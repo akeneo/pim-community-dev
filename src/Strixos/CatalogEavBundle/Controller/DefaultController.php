fa273cb6d55d2e73ffa2a43135dbfeb8a03b7910
Nicolas-Dupont <nicolas.dupont.pro@gmail.com>
2012-10-02 20:07:28 +0200

    first release of entity factory defined as service to create type, fields and entity, testable on route /eav/

--- src/Strixos/CatalogEavBundle/Controller/DefaultController.php
+++ src/Strixos/CatalogEavBundle/Controller/DefaultController.php
@@ -2,6 +2,14 @@
 
 namespace Strixos\CatalogEavBundle\Controller;
 
+use Strixos\CatalogEavBundle\Entity\Product;
+
+use Strixos\CatalogEavBundle\Entity\Attribute;
+
+use Strixos\CatalogEavBundle\Entity\ProductType;
+
+use Strixos\CatalogEavBundle\Factory\ProductTypeFactory;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -9,11 +17,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
+
+        $factory = $this->container->get('strixos_catalog_eav.typefactory');
+        $manager = $factory->getObjectManager();
+
+        $attCode = 'name';
+        $attribute = new Attribute();
+        $attribute->setCode($attCode);
+        $manager->persist($attribute);
+
+        $typCode = 'T-shirt';
+        $type = new ProductType();
+        $type->setCode($typCode);
+        $type->addAttribute($attribute);
+        $manager->persist($type);
+
+        $product = new Product();
+        $product->setType($type);
+        $manager->persist($product);
+
+        $manager->flush();
+
+        $name = 'pouet';
         return array('name' => $name);
     }
 }
