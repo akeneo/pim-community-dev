84a92047c0aa38cd4a4acc269dcf6d875d7abffe
Nicolas-Dupont <nicolas.dupont.pro@gmail.com>
2012-11-06 01:00:16 +0100

    add channel fixture

---
+++ src/Pim/Bundle/CatalogBundle/DataFixtures/ORM/LoadChannels.php
@@ -0,0 +1,51 @@
+<?php
+namespace Pim\Bundle\CatalogBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+use Pim\Bundle\CatalogBundle\Entity\ChannelLocale;
+
+/**
+ * Load channels
+ *
+ * Execute with "php app/console doctrine:fixtures:load"
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright Copyright (c) 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class LoadChannels extends AbstractFixture implements OrderedFixtureInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // add channel
+        $channel = new Channel();
+        $channel->setCode('Magento');
+        $channel->setIsDefault(true);
+        // add locale
+        $locale = new ChannelLocale();
+        $locale->setCode('fr_FR');
+        $locale->setIsDefault(true);
+        $channel->addLocale($locale);
+        // persist
+        $manager->persist($channel);
+        $manager->flush();
+    }
+
+    /**
+     * Executing order
+     * @see Doctrine\Common\DataFixtures.OrderedFixtureInterface::getOrder()
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+
+}
