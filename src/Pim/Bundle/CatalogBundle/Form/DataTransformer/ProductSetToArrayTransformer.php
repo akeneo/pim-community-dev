dd8515662af2095e60aa0e507fb8e421d0ddf82f
Nicolas-Dupont <nicolas.dupont.pro@gmail.com>
2012-11-18 04:19:26 +0100

    PIM-3 : first version of set to array transformer

---
+++ src/Pim/Bundle/CatalogBundle/Form/DataTransformer/ProductSetToArrayTransformer.php
@@ -0,0 +1,96 @@
+<?php
+namespace Pim\Bundle\CatalogBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Doctrine\Common\Persistence\ObjectManager;
+use Pim\Bundle\CatalogBundle\Doctrine\ProductManager;
+use Bap\Bundle\FlexibleEntityBundle\Model\EntitySet as ProductSet;
+
+/**
+ * Aims to transform array to product set and reverse operation
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright Copyright (c) 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class ProductSetToArrayTransformer implements DataTransformerInterface
+{
+    /**
+     * @var ProductManager
+     */
+    private $pm;
+
+    /**
+     * @param ProductManager $om
+     */
+    public function __construct(ProductManager $pm)
+    {
+        $this->pm = $pm;
+    }
+
+    /**
+     * Transforms an object (product set) to a array.
+     *
+     * @param  ProductSet $set
+     * @return array
+     */
+    public function transform($set)
+    {
+        $data = array();
+        // base data
+        $data['id']=    $set->getId();
+        $data['code']=  $set->getCode();
+        $data['title']= $set->getTitle();
+        // groups
+        $data['groups']= array();
+        foreach ($set->getGroups() as $group) {
+            if (!$group->getCode()) {
+                throw new TransformationFailedException(sprintf('A group of set "%s" has no code !', $set->getId()));
+            }
+            $groupData = array();
+            $groupData['id']=    $group->getId();
+            $groupData['code']=  $group->getCode();
+            $groupData['title']= $group->getTitle();
+            // attributes
+            $groupData['attributes']= array();
+            foreach ($group->getAttributes() as $attribute) {
+                if (!$attribute->getCode()) {
+                    throw new TransformationFailedException(sprintf('A attribute of set "%s" has no code !', $set->getId()));
+                }
+                $attributeData = array();
+                $attributeData['id']=    $attribute->getId();
+                $attributeData['code']=  $attribute->getCode();
+                $attributeData['title']= $attribute->getTitle();
+                $groupData['attributes'][$attribute->getCode()]= $attributeData;
+            }
+            $data['groups'][$group->getCode()]= $groupData;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Transforms a array to an object (product set).
+     *
+     * @param  array $data
+     * @return ProductSet
+     * @throws TransformationFailedException if object (set) is not found.
+     */
+    public function reverseTransform($data)
+    {
+        // get or create set
+        $setId = $data['id'];
+        $set = null;
+        if ($setId) {
+            $set = $this->pm->getSetRepository()->find($setId);
+        }
+        if (!$set) {
+            $set = $this->pm->getNewSetInstance();
+        }
+        // TODO
+
+        return $set;
+    }
+}
---
+++ src/Pim/Bundle/CatalogBundle/Tests/Form/DataTransformer/ProductSetToArrayTransformerTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Pim\Bundle\CatalogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Pim\Bundle\CatalogBundle\Doctrine\ProductManager;
+use Pim\Bundle\CatalogBundle\Tests\KernelAwareTest;
+use Pim\Bundle\CatalogBundle\Form\DataTransformer\ProductSetToArrayTransformer;
+
+/**
+ * Test related class
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright Copyright (c) 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductSetToArrayTransformerTest extends KernelAwareTest
+{
+
+    /**
+     * Test related method
+     */
+    public function testTransform()
+    {
+        // get first set
+        $productManager = $this->container->get('pim.catalog.product_manager');
+        $set = $productManager->getSetRepository()->findAll()->getSingleResult();
+        $this->assertNotNull($set);
+        // transform to array
+        $transformer = new ProductSetToArrayTransformer($productManager);
+        $data = $transformer->transform($set);
+        // assert
+        $this->assertEquals($set->getCode(), $data['code']);
+        $this->assertEquals($set->getTitle(), $data['title']);
+        $this->assertEquals($set->getGroups()->count(), count($data['groups']));
+    }
+
+    /**
+     * Test related method
+     */
+    public function testReverseTransform()
+    {
+        $productManager = $this->container->get('pim.catalog.product_manager');
+
+        // transform array to new set
+        $transformer = new ProductSetToArrayTransformer($productManager);
+        $data = array (
+            'id'     => null,
+            'code'   => 'new-set',
+            'title'  => 'my title',
+            'groups' => array(
+                'new-group' => array(
+                    'id'    => null,
+                    'code'  => 'new-group',
+                    'title' => 'new-title',
+                    'attributes' => array()
+                )
+            )
+        );
+        $set = $transformer->reverseTransform($data);
+        // assert
+        $this->assertEquals($set->getCode(), $data['code']);
+        $this->assertEquals($set->getTitle(), $data['title']);
+        $this->assertEquals($set->getGroupByCode('new-group')->getTitle(), $data['groups']['new-group']['title']);
+    }
+
+}
