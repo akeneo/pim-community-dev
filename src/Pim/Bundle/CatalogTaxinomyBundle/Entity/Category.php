b272638ffe3ac078eeec0a48aceaf5e2dc4dff6a
Romain Monceau <romain@akeneo.com>
2012-11-27 17:05:56 +0100

    PIM-17 : Paste strictely the same code than in example

--- src/Pim/Bundle/CatalogTaxinomyBundle/Entity/Category.php
+++ src/Pim/Bundle/CatalogTaxinomyBundle/Entity/Category.php
@@ -17,11 +17,9 @@ use Doctrine\ORM\Mapping as ORM;
 class Category
 {
     /**
-     * @var integer $id
-     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue
      */
     private $id;
 
@@ -32,21 +30,21 @@ class Category
 
     /**
      * @Gedmo\TreeLeft
-     * @ORM\Column(name="left", type="integer")
+     * @ORM\Column(name="lft", type="integer")
      */
-    private $left;
+    private $lft;
 
     /**
      * @Gedmo\TreeLevel
-     * @ORM\Column(name="level", type="integer")
+     * @ORM\Column(name="lvl", type="integer")
      */
-    private $level;
+    private $lvl;
 
     /**
      * @Gedmo\TreeRight
-     * @ORM\Column(name="right", type="integer")
+     * @ORM\Column(name="rgt", type="integer")
      */
-    private $right;
+    private $rgt;
 
     /**
      * @Gedmo\TreeRoot
@@ -63,195 +61,32 @@ class Category
 
     /**
      * @ORM\OneToMany(targetEntity="Category", mappedBy="parent")
-     * @ORM\OrderBy({"left" = "ASC"})
+     * @ORM\OrderBy({"lft" = "ASC"})
      */
     private $children;
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->children = new \Doctrine\Common\Collections\ArrayCollection();
-    }
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set title
-     * @param string $title
-     *
-     * @return Category
-     */
     public function setTitle($title)
     {
         $this->title = $title;
-
-        return $this;
     }
 
-    /**
-     * Get title
-     *
-     * @return string
-     */
     public function getTitle()
     {
         return $this->title;
     }
 
-    /**
-     * Set left
-     * @param integer $left
-     *
-     * @return Category
-     */
-    public function setLeft($left)
-    {
-        $this->left = $left;
-
-        return $this;
-    }
-
-    /**
-     * Get left
-     *
-     * @return integer
-     */
-    public function getLeft()
-    {
-        return $this->left;
-    }
-
-    /**
-     * Set level
-     * @param integer $level
-     *
-     * @return Category
-     */
-    public function setLevel($level)
-    {
-        $this->level = $level;
-
-        return $this;
-    }
-
-    /**
-     * Get level
-     *
-     * @return integer
-     */
-    public function getLevel()
-    {
-        return $this->level;
-    }
-
-    /**
-     * Set right
-     * @param integer $right
-     *
-     * @return Category
-     */
-    public function setRight($right)
-    {
-        $this->right = $right;
-
-        return $this;
-    }
-
-    /**
-     * Get right
-     *
-     * @return integer
-     */
-    public function getRight()
-    {
-        return $this->right;
-    }
-
-    /**
-     * Set root
-     * @param integer $root
-     *
-     * @return Category
-     */
-    public function setRoot($root)
-    {
-        $this->root = $root;
-
-        return $this;
-    }
-
-    /**
-     * Get root
-     *
-     * @return integer
-     */
-    public function getRoot()
-    {
-        return $this->root;
-    }
-
-    /**
-     * Set parent
-     * @param Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $parent
-     *
-     * @return Category
-     */
-    public function setParent(\Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $parent = null)
+    public function setParent(Category $parent = null)
     {
         $this->parent = $parent;
-
-        return $this;
     }
 
-    /**
-     * Get parent
-     *
-     * @return Pim\Bundle\CatalogTaxinomyBundle\Entity\Category
-     */
     public function getParent()
     {
         return $this->parent;
-    }
-
-    /**
-     * Add children
-     * @param Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children
-     *
-     * @return Category
-     */
-    public function addChildren(\Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children)
-    {
-        $this->children[] = $children;
-
-        return $this;
-    }
-
-    /**
-     * Remove children
-     *
-     * @param Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children
-     */
-    public function removeChildren(\Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children)
-    {
-        $this->children->removeElement($children);
-    }
-
-    /**
-     * Get children
-     *
-     * @return Doctrine\Common\Collections\Collection
-     */
-    public function getChildren()
-    {
-        return $this->children;
     }
 }
