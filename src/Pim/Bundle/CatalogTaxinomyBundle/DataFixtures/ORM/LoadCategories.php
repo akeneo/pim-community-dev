e93a42932c5c6e2a9e17e00f71305eea990d78f6
Romain Monceau <romain@akeneo.com>
2012-11-28 16:11:16 +0100

    PIM-10 : Comment load fixtures for categories. Actual implementation must be replace by a call of the ImportCategoriesCommand

--- src/Pim/Bundle/CatalogTaxinomyBundle/DataFixtures/ORM/LoadCategories.php
+++ src/Pim/Bundle/CatalogTaxinomyBundle/DataFixtures/ORM/LoadCategories.php
@@ -10,17 +10,24 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 /**
- * Enter description here ...
+ * Load category fixtures
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
+ * TODO : Must call the command "ImportCategoriesCommand"
  */
 class LoadCategories extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
     protected $container;
 
+    /**
+     * @var ObjectManager
+     */
     protected $manager;
 
     /**
@@ -38,21 +45,21 @@ class LoadCategories extends AbstractFixture implements OrderedFixtureInterface,
     {
         $this->manager = $manager;
 
-        $food = new Category();
-        $food->setTitle('food');
+//         $food = new Category();
+//         $food->setTitle('food');
 
-        $fruits = new Category();
-        $fruits->setTitle('fruits');
-        $fruits->setParent($food);
+//         $fruits = new Category();
+//         $fruits->setTitle('fruits');
+//         $fruits->setParent($food);
 
 
-        $vegetables = new Category();
-        $vegetables->setTitle('vegetables');
-        $vegetables->setParent($food);
+//         $vegetables = new Category();
+//         $vegetables->setTitle('vegetables');
+//         $vegetables->setParent($food);
 
-        $carrots = new Category();
-        $carrots->setTitle('carrots');
-        $carrots->setParent($vegetables);
+//         $carrots = new Category();
+//         $carrots->setTitle('carrots');
+//         $carrots->setParent($vegetables);
 
 
 //         $food->setLeft(1);
@@ -69,12 +76,12 @@ class LoadCategories extends AbstractFixture implements OrderedFixtureInterface,
 
 
 
-        $this->manager->persist($food);
-        $this->manager->persist($fruits);
-        $this->manager->persist($vegetables);
-        $this->manager->persist($carrots);
+//         $this->manager->persist($food);
+//         $this->manager->persist($fruits);
+//         $this->manager->persist($vegetables);
+//         $this->manager->persist($carrots);
 
-        $this->manager->flush();
+//         $this->manager->flush();
     }
 
     /**
