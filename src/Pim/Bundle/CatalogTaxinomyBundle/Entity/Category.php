011b31e4ef838ba7d7da5dd927812d3773c3d6ff
Romain Monceau <romain@akeneo.com>
2012-11-28 16:12:02 +0100

    PIM-10 : Create Category entity

--- src/Pim/Bundle/CatalogTaxinomyBundle/Entity/Category.php
+++ src/Pim/Bundle/CatalogTaxinomyBundle/Entity/Category.php
@@ -17,6 +17,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Category
 {
     /**
+     * @var integer $id
+     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -24,35 +26,47 @@ class Category
     private $id;
 
     /**
+     * @var string $title
+     *
      * @ORM\Column(name="title", type="string", length=64)
      */
     private $title;
 
     /**
+     * @var integer $left
+     *
      * @Gedmo\TreeLeft
      * @ORM\Column(name="lft", type="integer")
      */
-    private $lft;
+    private $left;
 
     /**
+     * @var integer $level
+     *
      * @Gedmo\TreeLevel
      * @ORM\Column(name="lvl", type="integer")
      */
-    private $lvl;
+    private $level;
 
     /**
+     * @var integer $right
+     *
      * @Gedmo\TreeRight
      * @ORM\Column(name="rgt", type="integer")
      */
-    private $rgt;
+    private $right;
 
     /**
+     * @var integer $root
+     *
      * @Gedmo\TreeRoot
      * @ORM\Column(name="root", type="integer", nullable=true)
      */
     private $root;
 
     /**
+     * @var Category $parent
+     *
      * @Gedmo\TreeParent
      * @ORM\ManyToOne(targetEntity="Category", inversedBy="children")
      * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="SET NULL")
@@ -61,32 +75,196 @@ class Category
 
     /**
      * @ORM\OneToMany(targetEntity="Category", mappedBy="parent")
-     * @ORM\OrderBy({"lft" = "ASC"})
+     * @ORM\OrderBy({"left" = "ASC"})
      */
     private $children;
 
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->children = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Set title
+     * @param string $title
+     *
+     * @return Category
+     */
     public function setTitle($title)
     {
         $this->title = $title;
+
+        return $this;
     }
 
+    /**
+     * Get title
+     *
+     * @return string
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * Set left
+     * @param integer $left
+     *
+     * @return Category
+     */
+    public function setLeft($left)
+    {
+        $this->left = $left;
+
+        return $this;
+    }
+
+    /**
+     * Get left
+     *
+     * @return integer
+     */
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    /**
+     * Set level
+     * @param integer $level
+     *
+     * @return Category
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set right
+     * @param integer $right
+     *
+     * @return Category
+     */
+    public function setRight($right)
+    {
+        $this->right = $right;
+
+        return $this;
+    }
+
+    /**
+     * Get right
+     *
+     * @return integer
+     */
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    /**
+     * Set root
+     * @param integer $root
+     *
+     * @return Category
+     */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+
+        return $this;
+    }
+
+    /**
+     * Get root
+     *
+     * @return integer
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * Set parent
+     * @param Category $parent
+     *
+     * @return Category
+     */
     public function setParent(Category $parent = null)
     {
         $this->parent = $parent;
+
+        return $this;
     }
 
+    /**
+     * Get parent
+     *
+     * @return Pim\Bundle\CatalogTaxinomyBundle\Entity\Category
+     */
     public function getParent()
     {
         return $this->parent;
+    }
+
+    /**
+     * Add children
+     * @param Category $children
+     *
+     * @return Category
+     */
+    public function addChildren(Category $children)
+    {
+        $this->children[] = $children;
+
+        return $this;
+    }
+
+    /**
+     * Remove children
+     *
+     * @param Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children
+     */
+    public function removeChildren(\Pim\Bundle\CatalogTaxinomyBundle\Entity\Category $children)
+    {
+        $this->children->removeElement($children);
+    }
+
+    /**
+     * Get children
+     *
+     * @return Doctrine\Common\Collections\Collection
+     */
+    public function getChildren()
+    {
+        return $this->children;
     }
 }
