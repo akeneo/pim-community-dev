9e0dfabd2a8af938e129da673b0907f78a23a825
Romain Monceau <romain@akeneo.com>
2012-12-04 14:19:00 +0100

    PIM-63 : Add some tests

---
+++ src/Pim/Bundle/CatalogTaxinomyBundle/Tests/Model/CategoryManagerTest.php
@@ -0,0 +1,175 @@
+<?php
+namespace Pim\Bundle\CatalogTaxinomyBundle\Tests\Model;
+
+use Pim\Bundle\CatalogBundle\Tests\KernelAwareTest;
+/**
+ * Test related class
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class CategoryManagerTest extends KernelAwareTest
+{
+    /**
+     * @return \Pim\Bundle\CatalogTaxinomyBundle\Model\CategoryManager
+     */
+    protected function getManager()
+    {
+        return $this->container->get('pim.catalog_taxinomy.category_manager');
+    }
+
+    /**
+     * Assert entity is a Category entity
+     * @param object $entity
+     */
+    protected function assertInstanceOfCategory($entity)
+    {
+        $this->assertInstanceOf('\Pim\Bundle\CatalogTaxinomyBundle\Entity\Category', $entity);
+    }
+
+    /**
+     * test related method
+     */
+    public function testCopy()
+    {
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * test related method
+     */
+    public function testCreateNewInstance()
+    {
+        $category = $this->getManager()->createNewInstance();
+        $this->assertInstanceOfCategory($category);
+    }
+
+    /**
+     * test related method
+     */
+    public function testGetCategories()
+    {
+        $categories = $this->getManager()->getCategories();
+
+        $this->assertCount(20, $categories);
+        foreach ($categories as $category) {
+            $this->assertInstanceOfCategory($category);
+        }
+
+        // TODO : Assert ordering by title
+    }
+
+    /**
+     * test related method
+     */
+    public function testGetCategory()
+    {
+        $category = $this->getManager()->getCategory(1);
+
+        $this->assertInstanceOfCategory($category);
+        $this->assertEquals('computers', $category->getTitle());
+    }
+
+    /**
+     * Test related method
+     */
+    public function testGetChildren()
+    {
+        // initialize variables
+        $parentId = 1;
+        $listChildren = array('desktop', 'laptop', 'server', 'tablet');
+        $index = 0;
+
+        // recover objects
+        $parent = $this->getManager()->getCategory($parentId);
+        $categories = $this->getManager()->getChildren($parentId);
+
+        // asserts
+        $this->assertCount(count($parent->getChildren()), $categories);
+        foreach ($categories as $category) {
+            $this->assertInstanceOfCategory($category);
+            $this->assertEquals($listChildren[$index++], $category->getTitle());
+        }
+    }
+
+    /**
+     * test related method
+     */
+    public function testMove()
+    {
+
+    }
+
+    /**
+     * test related method
+     */
+    public function testPersist()
+    {
+
+    }
+
+    /**
+     * test related method
+     */
+    public function testRemove()
+    {
+
+    }
+
+    /**
+     * test related method
+     */
+    public function testRemoveFromId()
+    {
+
+    }
+
+    /**
+     * test related method
+     */
+    public function testRename()
+    {
+
+    }
+
+    /**
+     * test related method
+     */
+    public function testSearch()
+    {
+        $criterias = array('title' => 'hard drive');
+
+        $categories = $this->getManager()->search($criterias);
+
+        // asserts
+        $this->assertCount(2, $categories);
+        foreach ($categories as $category) {
+            $this->assertInstanceOfCategory($category);
+        }
+
+        // TODO : assert the 2 categories contains "hard drive" in their title
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
