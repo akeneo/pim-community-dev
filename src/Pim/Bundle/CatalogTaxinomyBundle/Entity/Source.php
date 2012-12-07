67ee043345d29ccb0323fab122ce19823ae6792c
Nicolas Dupont <nicolas@akeneo.com>
2012-12-07 16:26:35 +0100

    PIM-70 : add source model, and access from dashboard

---
+++ src/Pim/Bundle/CatalogTaxinomyBundle/Entity/Source.php
@@ -0,0 +1,80 @@
+<?php
+namespace Pim\Bundle\CatalogTaxinomyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Catalog source, where data come from
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @ORM\Table(name="Akeneo_PimCatalogTaxinomy_Source")
+ * @ORM\Entity
+ */
+class Source
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string $code
+     * @ORM\Column(name="code", type="string", length=255, unique=true)
+     */
+    protected $code;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id
+     * @param integer $id
+     *
+     * @return Channel
+     */
+    public function setId($id)
+    {
+        return $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Channel
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+}
---
+++ src/Pim/Bundle/CatalogTaxinomyBundle/Model/SourceManager.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pim\Bundle\CatalogBundle\Doctrine;
+
+use Oro\Bundle\FlexibleEntityBundle\Doctrine\BaseEntityManager;
+
+/**
+ * Manage sources
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ChannelManager extends BaseEntityManager
+{
+
+    /**
+     * Return shortname that can be used to get the repository or instance
+     * @return string
+     */
+    public function getEntityShortname()
+    {
+        return 'PimCatalogTaxinomyBundle:Source';
+    }
+
+}
