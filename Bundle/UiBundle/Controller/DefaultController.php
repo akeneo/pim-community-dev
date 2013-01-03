435651fabb74eb0514000879723334bc93f8b6aa
vladimir <vladimire@magecore.com>
2013-01-03 17:32:04 +0200

    add start files for symfony

---
+++ Bundle/UiBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Oro\Bundle\UiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/1column")
+     * @Template()
+     */
+    public function oneColumnAction()
+    {
+        return array();
+    }
+}
---
+++ Bundle/UiBundle/OroUiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\UiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroUiBundle extends Bundle
+{
+}
