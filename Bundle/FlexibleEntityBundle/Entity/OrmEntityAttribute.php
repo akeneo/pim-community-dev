7f87ad3d1bf1661fed719e6470649f341c4040f9
Nicolas Dupont <nicolas@akeneo.com>
2013-01-08 09:53:20 +0100

    BAP-17 : change name of tables as bundlename_entityname

--- Bundle/FlexibleEntityBundle/Entity/OrmEntityAttribute.php
+++ Bundle/FlexibleEntityBundle/Entity/OrmEntityAttribute.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @license   http://opensource.org/licenses/MIT MIT
  *
  * @ORM\Table(
- *     name="flexibleentity_entity_attribute", indexes={@ORM\Index(name="searchcode_idx", columns={"code"})},
+ *     name="oroflexibleentity_entity_attribute", indexes={@ORM\Index(name="searchcode_idx", columns={"code"})},
  *     uniqueConstraints={@ORM\UniqueConstraint(name="searchunique_idx", columns={"code", "entity_type"})}
  * )
  * @ORM\Entity
