edc33ba5ff48329c4f0c5651cb8d27f5002e5b90
Nicolas Dupont <nicolas@akeneo.com>
2013-01-14 22:22:02 +0100

    PIM-61 : remove old bap UI bundle (merge layout in pim ui, move form custom in pim ui)

--- src/Pim/Bundle/UIBundle/PimUIBundle.php
+++ src/Pim/Bundle/UIBundle/PimUIBundle.php
@@ -4,6 +4,15 @@ namespace Pim\Bundle\UIBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * UI bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
 class PimUIBundle extends Bundle
 {
+
 }
