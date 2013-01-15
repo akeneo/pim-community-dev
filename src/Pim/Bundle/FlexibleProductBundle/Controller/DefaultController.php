741afad68ea20ad3fbb2fcec26409a0f52f57f01
Romain Monceau <romain@akeneo.com>
2013-01-15 10:58:00 +0100

    PIM-83 : Clean up

--- src/Pim/Bundle/FlexibleProductBundle/Controller/DefaultController.php
+++ src/Pim/Bundle/FlexibleProductBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ use Oro\Bundle\FlexibleEntityBundle\Model\Attribute\Type\AbstractAttributeType;
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  *
  * @Route("/default")
  */
@@ -26,7 +26,6 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $pm = $this->container->get('pim_flexibleproduct.product_manager');
-        //var_dump($pm->getEntityRepository());
 
         $attributeCode = 'moncode';
         $attribute = $pm->getEntityRepository()->findAttributeByCode($attributeCode);
