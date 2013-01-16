28b38d811e604faf5918abe6771489e3ad1ccb38
Romain Monceau <romain@akeneo.com>
2013-01-16 10:05:50 +0100

    PIM-83 : Add php doc

--- src/Pim/Bundle/TestBundle/PimTestBundle.php
+++ src/Pim/Bundle/TestBundle/PimTestBundle.php
@@ -4,6 +4,14 @@ namespace Pim\Bundle\TestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Test bundle for abstract unit test classes
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
 class PimTestBundle extends Bundle
 {
 }
