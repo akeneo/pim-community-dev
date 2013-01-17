a838ba426d87026f2b9f7e234b25d6835ccc151c
Romain Monceau <romain@akeneo.com>
2013-01-17 18:43:38 +0100

    PIM-83 : Add generic type options and option values

---
+++ src/Pim/Bundle/FlexibleProductBundle/Form/Type/AttributeOptionType.php
@@ -0,0 +1,97 @@
+<?php
+namespace Pim\Bundle\FlexibleProductBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * Type for option attribute form (independent of persistence)
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class AttributeOptionType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->addFieldId($builder);
+
+        $this->addFieldSortOrder($builder);
+
+        $this->addFieldTranslatable($builder);
+
+        $this->addFieldOptionValues($builder);
+    }
+
+    /**
+     * Add field id to form builder
+     * @param FormBuilderInterface $builder
+     */
+    protected function addFieldId(FormBuilderInterface $builder)
+    {
+        $builder->add('id', 'hidden');
+    }
+
+    /**
+     * Add field sort_order to form builder
+     * @param FormBuilderInterface $builder
+     */
+    protected function addFieldSortOrder(FormBuilderInterface $builder)
+    {
+        $builder->add('sort_order');
+    }
+
+    /**
+     * Add field translatable to form builder
+     * @param FormBuilderInterface $builder
+     */
+    protected function addFieldTranslatable(FormBuilderInterface $builder)
+    {
+        $builder->add('translatable');
+    }
+
+    /**
+     * Add options values to form builder
+     * @param FormBuilderInterface $builder
+     */
+    protected function addFieldOptionValues(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            'optionValues', 'collection', array(
+                'type'         => new AttributeOptionValueType(),
+                'allow_add'    => true,
+                'allow_delete' => true,
+                'by_reference' => false
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'Oro\Bundle\FlexibleEntityBundle\Entity\AttributeOption'
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_flexibleproduct_productattribute';
+    }
+}
