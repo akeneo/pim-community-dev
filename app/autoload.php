40f3762df54262a16270d553127c353187b0aa6d
Nicolas Dupont <nicolas@akeneo.com>
2013-02-08 18:36:24 +0100

    PIM-61 : ignore web bundles and rollback autoload to standard one

--- app/autoload.php
+++ app/autoload.php
@@ -7,13 +7,7 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
-
-AnnotationRegistry::registerFile(
-    __DIR__.'/../vendor/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php'
-);
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
