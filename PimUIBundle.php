bdb445d8d771df04548a90bd10ace575c75b2a36
Romain Monceau <romain@akeneo.com>
2013-02-12 11:03:29 +0100

    PIM-138 : Add php documentations to define bundle class and change license name

--- PimUIBundle.php
+++ PimUIBundle.php
@@ -4,6 +4,14 @@ namespace Pim\Bundle\UIBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Pim User Interface Bundle
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
 class PimUIBundle extends Bundle
 {
 }
