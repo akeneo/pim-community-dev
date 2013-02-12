873caf464987cfffe067d68029ed2effb24a372c
Romain Monceau <romain@akeneo.com>
2013-02-12 09:34:26 +0100

    PIM-138 : Add Entity for Product management

---
+++ Entity/ProductAttribute.php
@@ -0,0 +1,154 @@
+<?php
+namespace Pim\Bundle\ProductBundle\Entity;
+
+use Oro\Bundle\FlexibleEntityBundle\Entity\Mapping\AbstractEntityAttributeExtended;
+use Oro\Bundle\FlexibleEntityBundle\Entity\Attribute;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Custom properties for a product attribute
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ * @ORM\Table(name="acmedemoflexibleentity_product_attribute")
+ * @ORM\Entity
+ */
+class ProductAttribute extends AbstractEntityAttributeExtended
+{
+    const SCOPE_ECOMMERCE = 'ecommerce';
+    const SCOPE_MOBILE    = 'mobile';
+
+    /**
+     * @var Oro\Bundle\FlexibleEntityBundle\Entity\Attribute $attribute
+     *
+     * @ORM\OneToOne(targetEntity="Oro\Bundle\FlexibleEntityBundle\Entity\Attribute", cascade={"persist", "merge", "remove"})
+     * @ORM\JoinColumn(name="attribute_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $attribute;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var string $description
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    protected $description;
+
+    /**
+     * @var string $variant
+     *
+     * @ORM\Column(name="variant", type="string", length=255)
+     */
+    protected $variant;
+
+    /**
+     * @ORM\Column(name="is_smart", type="boolean")
+     */
+    protected $smart;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->description = '';
+        $this->smart       = false;
+    }
+
+    /**
+     * Get name
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get searchable
+     *
+     * @return boolean $smart
+     */
+    public function getSmart()
+    {
+        return $this->smart;
+    }
+
+    /**
+     * Set smart
+     *
+     * @param boolean $smart
+     *
+     * @return ProductAttribute
+     */
+    public function setSmart($smart)
+    {
+        $this->smart = $smart;
+
+        return $this;
+    }
+
+    /**
+     * Get variant
+     *
+     * @return string $variant
+     */
+    public function getVariant()
+    {
+        return $this->variant;
+    }
+
+    /**
+     * Set variant
+     *
+     * @param string $variant
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ProductAttribute
+     */
+    public function setVariant($variant)
+    {
+        $this->variant = $variant;
+
+        return $this;
+    }
+
+}
