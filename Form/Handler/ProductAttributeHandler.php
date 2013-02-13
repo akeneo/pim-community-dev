5a496ca1d3392da8b9dbdc75814b114f393c85a0
Romain Monceau <romain@akeneo.com>
2013-02-13 11:04:16 +0100

    PIM-169 : Change license name

--- Form/Handler/ProductAttributeHandler.php
+++ Form/Handler/ProductAttributeHandler.php
@@ -14,7 +14,7 @@ use Symfony\Component\Form\FormInterface;
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/MIT MIT
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  */
 class ProductAttributeHandler
