4b8ca0d0facb8a0f85870f359f8bfee5e55faed3
Romain Monceau <romain@akeneo.com>
2013-02-21 14:23:43 +0100

    PIM-176 : Add data fixtures for demonstration environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -46,6 +46,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            $bundles[] = new Pim\Bundle\DemoBundle\PimDemoBundle();
         }
 
         return $bundles;
---
+++ src/Pim/Bundle/DemoBundle/PimDemoBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pim\Bundle\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Demo bundle
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class PimDemoBundle extends Bundle
+{
+}
