7f2b28d178ec97623edeff3b6e9f4a9adf347f64
Romain Monceau <romain@akeneo.com>
2013-02-21 15:43:12 +0100

    PIM-176 : Update bundle configuration

--- PimDemoBundle.php
+++ PimDemoBundle.php
@@ -12,6 +12,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  */
-class PimProductBundle extends Bundle
+class PimDemoBundle extends Bundle
 {
 }
