fbb56fa256d30f2007c1fba1b98a3ae6127557bd
Romain Monceau <romain@akeneo.com>
2013-02-21 15:41:31 +0100

    PIM-176 : Add demonstration content

---
+++ PimDemoBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pim\Bundle\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Pim Demo Bundle to load fixtures
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class PimProductBundle extends Bundle
+{
+}
