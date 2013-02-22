10a2d32b6da225de13f3ced34d73aaa717bccafe
Romain Monceau <romain@akeneo.com>
2013-02-22 15:01:31 +0100

    PIM-173 : Remove bootstrap file useless

--- Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-$loader = require_once __DIR__.'/../vendor/autoload.php';
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$loader->add('Doctrine\\Tests', __DIR__.'/../vendor/doctrine/orm/tests');
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
