36a3735be8aa5734986f087b3d1516aa951ce421
Nicolas Dupont <nicolas@akeneo.com>
2013-02-25 18:17:31 +0100

    PIM-65 : fix autoload to run doctrine tests

--- app/autoload.php
+++ app/autoload.php
@@ -9,6 +9,9 @@ if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 }
 
+// add possibility to extends doctrine unit test and use mocks
+$loader->add( 'Doctrine\\Tests', __DIR__.'/../vendor/doctrine/orm/tests' );
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
