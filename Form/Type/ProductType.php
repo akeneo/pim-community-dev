20969f1c89d6a4b3e2a3663f0e1904a04bd302e9
Nicolas Dupont <nicolas@akeneo.com>
2013-02-26 09:18:44 +0100

    BAP-179 : fix licence

--- Form/Type/ProductType.php
+++ Form/Type/ProductType.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/MIT MIT
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  */
 class ProductType extends FlexibleType
