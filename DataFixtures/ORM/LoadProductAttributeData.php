e4b3d1631ae0381f98e4a7a21bc1171e70462b1a
Romain Monceau <romain@akeneo.com>
2013-02-27 15:55:36 +0100

    PIM-185 : Set all attribute loading in ProductAttribute file

--- DataFixtures/ORM/LoadProductAttributeData.php
+++ DataFixtures/ORM/LoadProductAttributeData.php
@@ -1,9 +1,11 @@
 <?php
 namespace Pim\Bundle\DemoBundle\DataFixtures\ORM;
 
-use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\DateType;
-
 use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\MoneyType;
+
+use Oro\Bundle\FlexibleEntityBundle\Model\AbstractAttributeType;
+
+use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\MetricType;
 
 use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\OptionSimpleSelectType;
 
@@ -11,21 +13,19 @@ use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\OptionMultiCheckboxType;
 
 use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\TextAreaType;
 
+use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\DateType;
+
 use Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\TextType;
 
-use Oro\Bundle\FlexibleEntityBundle\Model\AbstractAttributeType;
-
-use Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
+use Doctrine\Common\Persistence\ObjectManager;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-use Doctrine\Common\DataFixtures\AbstractFixture;
-
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 
 /**
  * Load fixtures for Product attributes
@@ -45,7 +45,7 @@ class LoadProductAttributeData extends AbstractFixture implements OrderedFixture
 
     /**
      * Product manager
-     * @var Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
+     * @var Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManager
      */
     protected $manager;
 
@@ -59,7 +59,7 @@ class LoadProductAttributeData extends AbstractFixture implements OrderedFixture
 
     /**
      * Get entity manager
-     * @return FlexibleManager
+     * @return Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManager
      */
     protected function getProductManager()
     {
@@ -114,6 +114,85 @@ class LoadProductAttributeData extends AbstractFixture implements OrderedFixture
         }
         $this->getProductManager()->getStorageManager()->persist($attribute);
         $this->setReference('product-attribute.generic-size', $attribute);
+
+
+        // force in english
+        $this->getProductManager()->setLocale('en_US');
+
+        // attribute name (if not exists)
+        $attributeCode = 'name';
+        $productAttribute = $this->getProductManager()->createAttributeExtended(new TextType());
+        $productAttribute->setName('Name');
+        $productAttribute->setCode($attributeCode);
+        $productAttribute->setTranslatable(true);
+        $productAttribute->setRequired(true);
+        $productAttribute->setVariant(0);
+
+        // persists and add to references
+        $this->getProductManager()->getStorageManager()->persist($productAttribute);
+        $this->addReference('product-attribute.'. $attributeCode, $productAttribute);
+
+
+        // attribute price (if not exists)
+        $attributeCode = 'price';
+        $productAttribute = $this->getProductManager()->createAttributeExtended(new MoneyType());
+        $productAttribute->setName('Price');
+        $productAttribute->setCode($attributeCode);
+        $productAttribute->setVariant(0);
+
+        // persists and add to references
+        $this->getProductManager()->getStorageManager()->persist($productAttribute);
+        $this->addReference('product-attribute.'. $attributeCode, $productAttribute);
+
+
+        // attribute description (if not exists)
+        $attributeCode = 'description';
+        $productAttribute = $this->getProductManager()->createAttributeExtended(new TextAreaType());
+        $productAttribute->setName('Description');
+        $productAttribute->setCode($attributeCode);
+        $productAttribute->setTranslatable(true);
+        $productAttribute->setScopable(true);
+        $productAttribute->setVariant(0);
+
+        // persists and add to references
+        $this->getProductManager()->getStorageManager()->persist($productAttribute);
+        $this->addReference('product-attribute.'. $attributeCode, $productAttribute);
+
+
+        // attribute size (if not exists)
+        $attributeCode= 'size';
+        $productAttribute = $this->getProductManager()->createAttributeExtended(new MetricType());
+        $productAttribute->setName('Size');
+        $productAttribute->setCode($attributeCode);
+        $productAttribute->setVariant(0);
+
+        // persists and add to references
+        $this->getProductManager()->getStorageManager()->persist($productAttribute);
+        $this->addReference('product-attribute.'. $attributeCode, $productAttribute);
+
+
+        // attribute color (if not exists)
+        $attributeCode= 'color';
+        $productAttribute = $this->getProductManager()->createAttributeExtended(new OptionMultiCheckboxType());
+        $productAttribute->setName('Color');
+        $productAttribute->setCode($attributeCode);
+        $productAttribute->setTranslatable(false); // only one value but option can be translated in option values
+        $productAttribute->setVariant(0);
+
+        // add translatable option and related value "Red", "Blue", "Green"
+        $colors = array("Red", "Blue", "Green");
+        foreach ($colors as $color) {
+            $option = $this->getProductManager()->createAttributeOption();
+            $option->setTranslatable(true);
+            $optionValue = $this->getProductManager()->createAttributeOptionValue();
+            $optionValue->setValue($color);
+            $option->addOptionValue($optionValue);
+            $productAttribute->addOption($option);
+        }
+
+        // persists and add to references
+        $this->getProductManager()->getStorageManager()->persist($productAttribute);
+        $this->addReference('product-attribute.'. $attributeCode, $productAttribute);
 
         // flush
         $this->getProductManager()->getStorageManager()->flush();
