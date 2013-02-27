545a45cb891cd5f048f58e9566508d13d5b853c4
Romain Monceau <romain@akeneo.com>
2013-02-27 17:44:09 +0100

    PIM-133 : Define currency field

--- src/Pim/Bundle/ConfigBundle/Form/Type/LanguageType.php
+++ src/Pim/Bundle/ConfigBundle/Form/Type/LanguageType.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pim\Bundle\ConfigBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
+
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use Symfony\Component\Form\FormBuilderInterface;
@@ -27,11 +29,42 @@ class LanguageType extends AbstractType
 
         $builder->add('id', 'hidden');
 
-        $builder->add('code', 'locale');
+        $builder->add('code', 'locale');    // TODO : purge all useless locales
 
         $builder->add('fallback', 'locale');
 
+        // add currency field
+        $this->addCurrencyField($builder);
+
         $builder->add('activated', 'checkbox', array('required' => false));
+    }
+
+    /**
+     * Add currency field
+     * @param FormBuilderInterface $builder
+     */
+    protected function addCurrencyField(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            'currencies',
+            'entity',
+            array(
+                'class' => 'PimConfigBundle:Currency',
+                'property' => 'code',
+                'multiple' => true,
+                'query_builder' => function (EntityRepository $repository) {
+                    // prepare query to get activated currencies ordered by code
+                    $query = $repository->createQueryBuilder('c');
+                    $query->andwhere(
+                        $query->expr()->eq('c.activated', true)
+                    )
+                    ->orderBy('c.code');
+
+                    return $query;
+                },
+                'required' => true
+            )
+        );
     }
 
     /**
