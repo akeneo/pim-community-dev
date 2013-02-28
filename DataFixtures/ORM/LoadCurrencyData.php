bc401d2545aeb7eefcbdad0d5e0fde400fe32ba2
Romain Monceau <romain@akeneo.com>
2013-02-28 11:33:41 +0100

    PIM-185 & PIM-132 & PIM-133 : Add data fixtures loading for currencies and languages

---
+++ DataFixtures/ORM/LoadCurrencyData.php
@@ -0,0 +1,88 @@
+<?php
+namespace Pim\Bundle\DemoBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+/**
+ * Load fixtures for currencies
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class LoadCurrencyData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Currency manager
+     * @var Oro\Bundle\FlexibleEntityBundle\Manager\SimpleManager;
+     */
+    protected $manager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Get currency manager
+     * @return \Oro\Bundle\FlexibleEntityBundle\Manager\SimpleManager
+     */
+    protected function getCurrencyManager()
+    {
+        return $this->container->get('currency_manager');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // create currencies
+        $this->createCurrency('EUR');
+        $this->createCurrency('USD');
+        $this->createCurrency('GBP');
+        $this->createCurrency('CHF', false);
+
+        $this->getCurrencyManager()->getStorageManager()->flush();
+    }
+
+    /**
+     * Create currency entity and persist it
+     * @param string  $code      Currency code
+     * @param boolean $activated Define if currency is activated or not
+     */
+    protected function createCurrency($code, $activated = true)
+    {
+        $currency = $this->getCurrencyManager()->createEntity();
+        $currency->setCode($code);
+        $currency->setActivated($activated);
+        $this->getCurrencyManager()->getStorageManager()->persist($currency);
+        $this->setReference('currency.'. $code, $currency);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
---
+++ DataFixtures/ORM/LoadLanguageData.php
@@ -0,0 +1,98 @@
+<?php
+namespace Pim\Bundle\DemoBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+/**
+ * Load fixtures for languages
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class LoadLanguageData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Currency manager
+     * @var Oro\Bundle\FlexibleEntityBundle\Manager\SimpleManager;
+     */
+    protected $manager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Get language manager
+     * @return \Oro\Bundle\FlexibleEntityBundle\Manager\SimpleManager
+     */
+    protected function getLanguageManager()
+    {
+        return $this->container->get('language_manager');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->createLanguage('fr', 'fr', array('EUR'));
+        $this->createLanguage('en_US', 'en', array('USD', 'EUR'));
+        $this->createLanguage('en', 'en', array('GBP', 'EUR', 'USD'));
+        $this->createLanguage('fr_CH', 'fr', array('CHF', 'EUR'), false);
+
+        $this->getLanguageManager()->getStorageManager()->flush();
+    }
+
+    /**
+     * Create language entity and persist it
+     * @param string  $code       Language code
+     * @param string  $fallback   Language fallback
+     * @param array   $currencies Currencies used
+     * @param boolean $activated  Define if language is activated or not
+     */
+    protected function createLanguage($code, $fallback, $currencies = array(), $activated = true)
+    {
+        $language = $this->getLanguageManager()->createEntity();
+        $language->setCode($code);
+        $language->setFallback($fallback);
+        $language->setActivated($activated);
+
+        // prepare currencies
+        $langCurrencies = array();
+        foreach ($currencies as $code) {
+            $langCurrencies[] = $this->getReference('currency.'. $code);
+        }
+        $language->setCurrencies($langCurrencies);
+
+        $this->getLanguageManager()->getStorageManager()->persist($language);
+        $this->setReference('language.'. $code, $language);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
