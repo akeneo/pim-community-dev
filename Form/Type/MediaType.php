489b4fc63b54d14c354d1a682f7c40dea220602f
Romain Monceau <romain@akeneo.com>
2013-03-15 10:56:41 +0100

    PIM-120 : Redefine MediaType for PIM

---
+++ Form/Type/MediaType.php
@@ -0,0 +1,30 @@
+<?php
+namespace Pim\Bundle\ProductBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Oro\Bundle\FlexibleEntityBundle\Form\Type\MediaType as FlexibleMediaType;
+
+/**
+ * Extended MediaType
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class MediaType extends FlexibleMediaType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('id', 'hidden');
+
+        $builder->add('file', 'file', array('required' => false));
+
+        $builder->add('filename', 'text', array('required' => false, 'disabled' => false));
+    }
+}
