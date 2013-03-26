d5bb2188603c276482d3aca28cb21db1b2add1be
Romain Monceau <romain@akeneo.com>
2013-03-26 16:14:31 +0100

    PIM-227 ; Update functional test architecture and attribute group controller

--- Tests/Functional/Controller/AttributeGroupControllerTest.php
+++ Tests/Functional/Controller/AttributeGroupControllerTest.php
@@ -11,6 +11,32 @@ namespace Pim\Bundle\ProductBundle\Tests\Functional\Controller;
  */
 class AttributeGroupControllerTest extends ControllerTest
 {
+
+    /**
+     * @staticvar string
+     */
+    const GROUP_NAME = 'group name';
+
+    /**
+     * @staticvar integer
+     */
+    const GROUP_ORDER = 5;
+
+    /**
+     * @staticvar string
+     */
+    const GROUP_EDITED_NAME = 'group edited name';
+
+    /**
+     * @staticvar string
+     */
+    const GROUP_SAVED_MSG = 'Group successfully saved';
+
+    /**
+     * @staticvar string
+     */
+    const GROUP_REMOVED_MSG = 'Group successfully removed';
+
     /**
      * Test related action
      * @param string $locale
@@ -22,12 +48,13 @@ class AttributeGroupControllerTest extends ControllerTest
         $uri = '/'. $locale .'/product/attribute-group/index';
 
         // assert without authentication
-        $crawler = $this->client->request('GET', $uri);
+        $this->client->request('GET', $uri);
         $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
 
         // assert with authentication
         $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(1, $crawler->filter('table.table:contains("SEO")'));
     }
 
     /**
@@ -35,18 +62,47 @@ class AttributeGroupControllerTest extends ControllerTest
      * @param string $locale
      *
      * @dataProvider localeProvider
+     *
+     * @return null
      */
     public function testCreate($locale)
     {
         $uri = '/'. $locale .'/product/attribute-group/create';
 
         // assert without authentication
-        $crawler = $this->client->request('GET', $uri);
+        $this->client->request('GET', $uri);
         $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
 
         // assert with authentication
         $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        // assert attribute group form well works
+        $form = $crawler->filter('form')->reduce(
+            function ($node, $i) {
+                if ($node->hasAttribute('action')) {
+                    $action = $node->getAttribute('action');
+                    if (preg_match('#\/product\/attribute-group\/create$#', $action)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        )->first()->form();
+
+        $values = array(
+            'pim_attribute_group_form[name]'  => self::GROUP_NAME,
+            'pim_attribute_group_form[sort_order]' => self::GROUP_ORDER
+        );
+
+        $this->submitFormAndAssertFlashbag($form, $values, self::GROUP_SAVED_MSG);
+
+        // assert entity well inserted
+        $attributeGroup = $this->getRepository()->findOneBy(array('name' => self::GROUP_NAME));
+        $this->assertInstanceOf('Pim\Bundle\ProductBundle\Entity\AttributeGroup', $attributeGroup);
+        $this->assertEquals(self::GROUP_NAME, $attributeGroup->getName());
+        $this->assertEquals(self::GROUP_ORDER, $attributeGroup->getSortOrder());
     }
 
     /**
@@ -54,24 +110,55 @@ class AttributeGroupControllerTest extends ControllerTest
      * @param string $locale
      *
      * @dataProvider localeProvider
+     * @depends testCreate
+     *
+     * @return null
      */
     public function testEdit($locale)
     {
         // initialize authentication to call container and get attribute group entity
-        $attributeGroup = $this->getRepository()->findOneBy(array());
+        $attributeGroup = $this->getRepository()->findOneBy(array('name' => self::GROUP_NAME));
         $uri = '/'. $locale .'/product/attribute-group/edit/'. $attributeGroup->getId();
 
         // assert without authentication
-        $crawler = $this->client->request('GET', $uri);
+        $this->client->request('GET', $uri);
         $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
 
         // assert with authentication
         $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
 
+        // assert attribute group form well works
+        $form = $crawler->filter('form')->reduce(
+            function ($node, $i) {
+                if ($node->hasAttribute('action')) {
+                    $action = $node->getAttribute('action');
+                    if (preg_match('#\/product\/attribute-group\/edit/[0-9]*$#', $action)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        )->first()->form();
+
+        $values = array(
+            'pim_attribute_group_form[id]'  => $attributeGroup->getId(),
+            'pim_attribute_group_form[name]'  => self::GROUP_EDITED_NAME,
+            'pim_attribute_group_form[sort_order]' => self::GROUP_ORDER
+        );
+
+        $this->submitFormAndAssertFlashbag($form, $values, self::GROUP_SAVED_MSG);
+
+        // assert entity well inserted
+        $attributeGroup = $this->getRepository()->findOneBy(array('name' => self::GROUP_EDITED_NAME));
+        $this->assertInstanceOf('Pim\Bundle\ProductBundle\Entity\AttributeGroup', $attributeGroup);
+        $this->assertEquals(self::GROUP_EDITED_NAME, $attributeGroup->getName());
+        $this->assertEquals(self::GROUP_ORDER, $attributeGroup->getSortOrder());
+
         // assert with unknown attribute group id and authentication
         $uri = '/'. $locale .'/product/attribute-group/edit/0';
-        $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
+        $this->client->request('GET', $uri, array(), array(), $this->server);
         $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
     }
 
@@ -80,6 +167,7 @@ class AttributeGroupControllerTest extends ControllerTest
      * @param string $locale
      *
      * @dataProvider localeProvider
+     * @depends testEdit
      */
     public function testRemove($locale)
     {
@@ -91,14 +179,13 @@ class AttributeGroupControllerTest extends ControllerTest
         $crawler = $this->client->request('GET', $uri);
         $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
 
-        // TODO: @Romain fix mapping problem between group and attributes
         // assert with authentication
-        // $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
-        // $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
 
         // assert with unknown attribute group id (last removed) and authentication
-        //$crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
-        //$this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+        $crawler = $this->client->request('GET', $uri, array(), array(), $this->server);
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
     }
 
     /**
