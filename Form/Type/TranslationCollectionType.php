174a5108ec54801a2a7d69843434df9c6898740b
Romain Monceau <romain@akeneo.com>
2013-04-11 12:58:19 +0200

    PIM-155 & PIM-282 & PIM-136 : Implements TranslationFormType cleaner

---
+++ Form/Type/TranslationCollectionType.php
@@ -0,0 +1,67 @@
+<?php
+namespace Pim\Bundle\TranslationBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\OptionsResolver\Options;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\FormView;
+
+use Symfony\Component\Form\FormInterface;
+
+use Symfony\Component\Form\Extension\Core\EventListener\ResizeFormListener;
+
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Translation collection type
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class TranslationCollectionType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $resizeListener = new ResizeFormListener(
+            $builder->getFormFactory(),
+            $options['type'],
+            $options['options']
+        );
+
+        $builder->addEventSubscriber($resizeListener);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $optionsNormalizer = function (Options $options, $value) {
+            $value['block_name'] = 'entry';
+
+            return $value;
+        };
+
+        $resolver->setDefaults(array('type' => 'text', 'options' => array()));
+
+        $resolver->setNormalizers(array('options' => $optionsNormalizer));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_translation_collection';
+    }
+}
--- Form/Type/TranslationType.php
+++ Form/Type/TranslationType.php
@@ -1,9 +1,11 @@
 <?php
 namespace Pim\Bundle\TranslationBundle\Form\Type;
 
-use Symfony\Component\Form\FormBuilderInterface;
-
 use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  * Abstract translation type
@@ -11,10 +13,8 @@ use Symfony\Component\Form\AbstractType;
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
- * @abstract
  */
-abstract class TranslationType extends AbstractType
+class TranslationType extends AbstractType
 {
 
     /**
@@ -27,5 +27,25 @@ abstract class TranslationType extends AbstractType
         $builder->add('content', 'text');
 
         $builder->add('locale', 'text');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation'
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_translation';
     }
 }
