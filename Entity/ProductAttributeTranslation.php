c8450b425a7c1f5609b9540b64eff0d05a9ec12b
Romain Monceau <romain@akeneo.com>
2013-04-16 16:56:31 +0200

    PIM-136 : Implements translation for product attribute

---
+++ Entity/ProductAttributeTranslation.php
@@ -0,0 +1,40 @@
+<?php
+namespace Pim\Bundle\ProductBundle\Entity;
+
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractTranslation;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProductAttribute translation entity
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @ORM\Entity()
+ * @ORM\Table(
+ *     name="pim_product_attribute_translations",
+ *     indexes={
+ *         @ORM\Index(
+ *             name="pim_product_attribute_translations_idx",
+ *             columns={"locale", "object_class", "field", "foreign_key"}
+ *         )
+ *     }
+ * )
+ *
+ */
+class ProductAttributeTranslation extends AbstractTranslation
+{
+    /**
+     * All required columns are mapped through inherited superclass
+     */
+
+    /**
+     * Change foreign key to add constraint and work with basic entity
+     *
+     * @ORM\ManyToOne(targetEntity="ProductAttribute", inversedBy="translations")
+     * @ORM\JoinColumn(name="foreign_key", referencedColumnName="id")
+     */
+    protected $foreignKey;
+}
