c79b30c317e3bdd7a9ff3dc4ac2be1f4d2375075
Romain Monceau <romain@akeneo.com>
2013-04-19 19:08:37 +0200

    PIM-147 : Add entity and UT related to

---
+++ Entity/ExportProfile.php
@@ -0,0 +1,214 @@
+<?php
+namespace Pim\Bundle\ProductBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+use Gedmo\Translatable\Translatable;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Export profile entity
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="pim_export_profile")
+ * @Gedmo\TranslationEntity(class="Pim\Bundle\ProductBundle\Entity\ExportProfileTranslation")
+ */
+class ExportProfile implements Translatable
+{
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string $code
+     *
+     * @ORM\Column(name="code", type="string", length=100)
+     */
+    protected $code;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     * @Gedmo\Translatable
+     */
+    protected $name;
+
+    /**
+     * Used locale to override Translation listener`s locale
+     * this is not a mapped field of entity metadata, just a simple property
+     *
+     * @var string $locale
+     *
+     * @Gedmo\Locale
+     */
+    protected $locale;
+
+    /**
+     * @var ArrayCollection $translations
+     *
+     * @ORM\OneToMany(
+     *     targetEntity="ExportProfileTranslation",
+     *     mappedBy="foreignKey",
+     *     cascade={"persist", "remove"}
+     * )
+     */
+    protected $translations;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get sort order
+     *
+     * @return integer
+     */
+    public function getSortOrder()
+    {
+        return $this->sortOrder;
+    }
+
+    /**
+     * Set sort order
+     *
+     * @param string $sortOrder
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function setSortOrder($sortOrder)
+    {
+        $this->sortOrder = $sortOrder;
+
+        return $this;
+    }
+
+    /**
+     * Define locale used by entity
+     *
+     * @param string $locale
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function setTranslatableLocale($locale)
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * Get translations
+     *
+     * @return ArrayCollection
+     */
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
+
+    /**
+     * Add translation
+     *
+     * @param ExportProfileTranslation $translation
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function addTranslation(ExportProfileTranslation $translation)
+    {
+        if (!$this->translations->contains($translation)) {
+            $this->translations->add($translation);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove translation
+     *
+     * @param ExportProfileTranslation $translation
+     *
+     * @return \Pim\Bundle\ProductBundle\Entity\ExportProfile
+     */
+    public function removeTranslation(ExportProfileTranslation $translation)
+    {
+        $this->translations->removeElement($translation);
+
+        return $this;
+    }
+}
