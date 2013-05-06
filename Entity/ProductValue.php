905446efe560125b109f5bc55b73c63e55cc1218
Nicolas Dupont <nicolas@akeneo.com>
2013-05-06 14:04:09 +0200

    BAP-677 : add media, metric and price

--- Entity/ProductValue.php
+++ Entity/ProductValue.php
@@ -54,4 +54,106 @@ class ProductValue extends AbstractEntityFlexibleValue
      * @ORM\JoinColumn(name="option_id", referencedColumnName="id", onDelete="SET NULL")
      */
     protected $option;
+
+    /**
+     * Store upload values
+     *
+     * @var Media $media
+     *
+     * @ORM\OneToOne(targetEntity="Oro\Bundle\FlexibleEntityBundle\Entity\Media", cascade="persist")
+     * @ORM\JoinColumn(name="media_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $media;
+
+    /**
+     * Store metric value
+     *
+     * @var Metric $metric
+     *
+     * @ORM\OneToOne(targetEntity="Oro\Bundle\FlexibleEntityBundle\Entity\Metric", cascade="persist")
+     * @ORM\JoinColumn(name="metric_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $metric;
+
+    /**
+     * Store price value
+     *
+     * @var Price $price
+     *
+     * @ORM\OneToOne(targetEntity="Oro\Bundle\FlexibleEntityBundle\Entity\Price", cascade="persist")
+     * @ORM\JoinColumn(name="price_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $price;
+
+    /**
+     * Get media
+     *
+     * @return \Oro\Bundle\FlexibleEntityBundle\Entity\Media
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * Set media
+     *
+     * @param \Oro\Bundle\FlexibleEntityBundle\Entity\Media $media
+     *
+     * @return \Oro\Bundle\FlexibleEntityBundle\Entity\ProductValue
+     */
+    public function setMedia($media)
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    /**
+     * Get metric
+     *
+     * @return Metric
+     */
+    public function getMetric()
+    {
+        return $this->metric;
+    }
+
+    /**
+     * Set metric
+     *
+     * @param Metric $metric
+     *
+     * @return AbstractEntityFlexibleValue
+     */
+    public function setMetric($metric)
+    {
+        $this->metric = $metric;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return Price
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set price
+     *
+     * @param Price $price
+     *
+     * @return AbstractEntityFlexibleValue
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
 }
