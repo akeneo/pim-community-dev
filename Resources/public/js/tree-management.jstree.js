fab36e42e1ff2c04f131e27e8043d3a80d5c0cec
Benoit Jacquemont <benoit@akeneo.com>
2013-05-24 11:20:04 +0200

    PIM-415 : Use title for node label

--- Resources/public/js/tree-management.jstree.js
+++ Resources/public/js/tree-management.jstree.js
@@ -9,7 +9,8 @@ var jstree = $('#tree').jstree({
         "ajax" : {
             "url" : urlListTree
         },
-        "auto_open_root" : true
+        "auto_open_root" : true,
+        "node_label_field" : "title"
     },
     "themes" : {
         "dots" : true,
