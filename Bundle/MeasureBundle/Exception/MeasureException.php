719f5011051ba5567aaeae53c932cd57458e019c
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-05-27 22:48:45 +0300

    Initial repository

---
+++ Bundle/MeasureBundle/Exception/MeasureException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Oro\Bundle\MeasureBundle\Exception;
+
+/**
+ * Measure exception
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ * @abstract
+ */
+abstract class MeasureException extends \Exception
+{
+}
---
+++ Bundle/MeasureBundle/Tests/Manager/MeasureManagerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oro\Bundle\MeasureBundle\Tests\Manager;
+
+use Oro\Bundle\MeasureBundle\Manager\MeasureManager;
+
+/**
+ * @author    Gildas Quemener <gildas.quemener@gmail.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MeasureManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->measureManager = new MeasureManager;
+        $this->measureManager->setMeasureConfig(array(
+            'WEIGHT' => array(
+                'units' => array(
+                    'KILOGRAM' => array(
+                        'symbol' => 'kg',
+                    ),
+                    'GRAM' => array(
+                        'symbol' => 'g',
+                    ),
+                )
+            )
+        ));
+    }
+
+    public function testGetUnitForFamily()
+    {
+        $this->assertEquals(array(
+            'KILOGRAM' => 'kg',
+            'GRAM'     => 'g',
+        ), $this->measureManager->getUnitSymbolsForFamily('WEIGHT'));
+    }
+
+    public function testInvalidFamilyWhenGettingUnitForFamily()
+    {
+        try {
+            $this->measureManager->getUnitSymbolsForFamily('LENGTH');
+        }
+        catch (\InvalidArgumentException $e) {
+            $this->assertEquals('Undefined measure family "LENGTH"', $e->getMessage());
+            return;
+        }
+
+        $this->fail('An InvalidArgumentException has not been raised.');
+    }
+}
