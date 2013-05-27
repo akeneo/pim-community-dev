84c4d6390c0d7a82c46401b1d32f125337ce7611
Romain Monceau <romain@akeneo.com>
2013-05-27 15:43:42 +0200

    PIM-222-223 : Update UT and add CurrencyManager FT

---
+++ src/Pim/Bundle/ConfigBundle/Tests/Functional/Manager/CurrencyManagerTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace Pim\Bundle\ConfigBundle\Tests\Functional\Manager;
+
+use Pim\Bundle\ConfigBundle\Manager\CurrencyManager;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test related class
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class CurrencyManagerTest extends WebTestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var CurrencyManager
+     */
+    protected $currencyManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        if (null !== static::$kernel) {
+            static::$kernel->shutdown();
+        }
+
+        static::$kernel = static::createKernel(array("debug" => true));
+        static::$kernel->boot();
+
+        $this->container = static::$kernel->getContainer();
+
+        $this->em = $this->container->get('doctrine.orm.entity_manager');
+
+        $this->currencyManager = new CurrencyManager($this->em);
+    }
+
+    /**
+     * Test related method
+     */
+    public function testGetActiveCurrencies()
+    {
+        $currencies = $this->currencyManager->getActiveCurrencies();
+        $expectedCurrencies = array('EUR', 'USD', 'GBP');
+
+        $this->assertCount(3, $currencies);
+        foreach ($currencies as $currency) {
+            $this->assertContains($currency->getCode(), $expectedCurrencies);
+        }
+    }
+
+    /**
+     * Test related method
+     */
+    public function testGetActiveCodes()
+    {
+        $currencies = $this->currencyManager->getActiveCodes();
+        $expectedCurrencies = array('EUR', 'USD', 'GBP');
+
+        var_dump($currencies);
+
+        $this->assertCount(3, $currencies);
+        foreach ($currencies as $currency) {
+            $this->assertContains($currency, $expectedCurrencies);
+        }
+    }
+
+    /**
+     * Test related class
+     */
+    public function testGetCurrencies()
+    {
+        $currencies = $this->currencyManager->getCurrencies();
+        $expectedCurrencies = array('EUR', 'USD', 'GBP');
+
+        $this->assertGreaterThan(3, $currencies);
+    }
+}
