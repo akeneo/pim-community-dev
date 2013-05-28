411350d4e879723f596a0ae871b26bc5edc211f4
Filips Alpe <filips@akeneo.com>
2013-05-28 13:37:40 +0300

    Keep dataLocale after adding attributes to a product

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -194,7 +194,10 @@ class ProductController extends Controller
         $this->addFlash('success', 'Attributes are added to the product form.');
 
         return $this->redirect(
-            $this->generateUrl('pim_product_product_edit', array('id' => $product->getId()))
+            $this->generateUrl('pim_product_product_edit', array(
+                'id' => $product->getId(),
+                'dataLocale' => $this->getDataLocale()
+            ))
         );
     }
 
