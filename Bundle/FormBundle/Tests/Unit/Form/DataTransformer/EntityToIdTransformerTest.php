e26e61d53727ca92fcebc86d18f581992e4c88e1
Dmitry Khrysev <dkhrysev@magecore.com>
2013-06-03 19:48:21 +0300

    CRM-110: unit tests

---
+++ Bundle/FormBundle/Tests/Unit/Form/DataTransformer/EntityToIdTransformerTest.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Tests\Unit\Form\Type;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Oro\Bundle\FormBundle\Form\DataTransformer\EntityToIdTransformer;
+use Doctrine\ORM\Mapping\MappingException;
+
+class EntityToIdTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $entityManager;
+
+    protected $repository;
+
+    /**
+     * @dataProvider transformDataProvider
+     *
+     * @param string $property
+     * @param mixed $value
+     * @param mixed $expectedValue
+     */
+    public function testTransform($property, $value, $expectedValue)
+    {
+        $transformer = new EntityToIdTransformer($this->getMockEntityManager(), 'TestClass', $property, null);
+        $this->assertEquals($expectedValue, $transformer->transform($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function transformDataProvider()
+    {
+        return array(
+            'default' => array(
+                'id',
+                $this->createMockEntity('id', 1),
+                1
+            ),
+            'empty' => array(
+                'id',
+                null,
+                null
+            ),
+        );
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @expectedExceptionMessageExpected Expected argument of type "array", "string" given
+     */
+    public function testTransformFailsWhenValueInNotAnArray()
+    {
+        $transformer = new EntityToIdTransformer($this->getMockEntityManager(), 'TestClass', 'id', null);
+        $transformer->transform('invalid value');
+    }
+
+    public function testReverseTransformEmpty()
+    {
+        $transformer = new EntityToIdTransformer($this->getMockEntityManager(), 'TestClass', 'id', null);
+        $this->assertNull($transformer->reverseTransform(''));
+    }
+
+    public function testReverseTransform()
+    {
+        $entity = $this->createMockEntity('id', 1);
+
+        $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repository->expects($this->once())
+            ->method('find')
+            ->with(1)
+            ->will($this->returnValue($entity));
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->with('TestClass')
+            ->will($this->returnValue($repository));
+
+        $transformer = new EntityToIdTransformer($em, 'TestClass', 'id', null);
+        $this->assertEquals($entity, $transformer->reverseTransform(1));
+    }
+
+    public function testReverseTransformQueryBuilder()
+    {
+        $entity = $this->createMockEntity('id', 1);
+
+        $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $self= $this;
+        $callback = function ($pRepository, $pId) use ($self, $repository, $entity) {
+            $self->assertEquals($repository, $pRepository);
+            $self->assertEquals(1, $pId);
+
+            $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
+                ->disableOriginalConstructor()
+                ->setMethods(array('execute'))
+                ->getMockForAbstractClass();
+            $query->expects($this->once())
+                ->method('execute')
+                ->will($this->returnValue($entity));
+
+            $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+                ->disableOriginalConstructor()
+                ->getMock();
+            $qb->expects($this->once())
+                ->method('getQuery')
+                ->will($this->returnValue($query));
+            return $qb;
+        };
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->with('TestClass')
+            ->will($this->returnValue($repository));
+
+        $transformer = new EntityToIdTransformer($em, 'TestClass', 'id', $callback);
+        $this->assertEquals($entity, $transformer->reverseTransform(1));
+    }
+
+    /**
+     * @expectedException Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @expectedExceptionMessage Expected argument of type "Doctrine\ORM\QueryBuilder", "NULL" given
+     */
+    public function testReverseTransformQueryBuilderException()
+    {
+        $entity = $this->createMockEntity('id', 1);
+
+        $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $callback = function () {
+            return null;
+        };
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->with('TestClass')
+            ->will($this->returnValue($repository));
+
+        $transformer = new EntityToIdTransformer($em, 'TestClass', 'id', $callback);
+        $this->assertEquals($entity, $transformer->reverseTransform(1));
+    }
+
+    public function testPropertyConstruction()
+    {
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $metadata = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metadata->expects($this->once())
+            ->method('getSingleIdentifierFieldName')
+            ->will($this->returnValue('id'));
+        $em->expects($this->once())
+            ->method('getClassMetadata')
+            ->will($this->returnValue($metadata));
+
+        new EntityToIdTransformer($em, 'TestClass', null, null);
+    }
+
+    /**
+     * @expectedException Symfony\Component\Form\Exception\FormException
+     * @expectedExceptionMessage Cannot get id property path of entity. "TestClass" has composite primary key.
+     */
+    public function testPropertyConstructionException()
+    {
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $metadata = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metadata->expects($this->once())
+            ->method('getSingleIdentifierFieldName')
+            ->will(
+                $this->returnCallback(
+                    function () {
+                        throw new MappingException('Exception');
+                    }
+                )
+            );
+        $em->expects($this->once())
+            ->method('getClassMetadata')
+            ->will($this->returnValue($metadata));
+
+        new EntityToIdTransformer($em, 'TestClass', null, null);
+    }
+
+    /**
+     * @expectedException Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @expectedExceptionMessage Expected argument of type "callable", "string" given
+     */
+    public function testCallbackException()
+    {
+        new EntityToIdTransformer($this->getMockEntityManager(), 'TestClass', 'id', 'uncallable');
+    }
+
+    /**
+     * @return EntityManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockEntityManager()
+    {
+        if (!$this->entityManager) {
+            $this->entityManager = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+                ->disableOriginalConstructor()
+                ->setMethods(array('getClassMetadata', 'getRepository'))
+                ->getMockForAbstractClass();
+        }
+
+        return $this->entityManager;
+    }
+
+    /**
+     * Create mock entity by id property name and value
+     *
+     * @param string $property
+     * @param mixed $value
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createMockEntity($property, $value)
+    {
+        $getter = 'get' . ucfirst($property);
+        $result = $this->getMock('MockEntity', array($getter));
+        $result->expects($this->any())->method($getter)->will($this->returnValue($value));
+        return $result;
+    }
+}
---
+++ Bundle/FormBundle/Tests/Unit/Form/Type/OroJquerySelect2HiddenTypeTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Tests\Unit\Form\Type;
+
+use Oro\Bundle\FormBundle\Form\Type\OroJquerySelect2HiddenType;
+
+class OroJquerySelect2HiddenTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var OroJquerySelect2HiddenType
+     */
+    protected $type;
+
+    protected $em;
+
+    protected $entityTransformer;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->entityTransformer = $this->getMockBuilder('Oro\Bundle\FormBundle\DataTransformer\EntityTransformerInterface')
+            ->getMock();
+        $this->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->type = new OroJquerySelect2HiddenType($this->entityTransformer, $this->em);
+    }
+
+    public function testSetDefaultOptions()
+    {
+        $resolver = $this->getMockBuilder('Symfony\Component\OptionsResolver\OptionsResolver')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $resolver->expects($this->once())
+            ->method('setRequired')
+            ->with(array('autocompleter_alias', 'class'))
+            ->will($this->returnSelf());
+
+        $resolver->expects($this->once())
+            ->method('setDefaults')
+            ->with(
+                array(
+                    'empty_value' => '',
+                    'empty_data' => null,
+                    'data_class' => null
+                )
+            )
+            ->will($this->returnSelf());
+
+        $this->type->setDefaultOptions($resolver);
+    }
+
+    public function testBuildForm()
+    {
+        $builder = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $builder->expects($this->once())
+            ->method('addModelTransformer')
+            ->with($this->isInstanceOf('Oro\Bundle\FormBundle\Form\DataTransformer\EntityToIdTransformer'))
+            ->will($this->returnSelf());
+
+        $metadata = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metadata->expects($this->once())
+            ->method('getSingleIdentifierFieldName')
+            ->will($this->returnValue('id'));
+        $this->em->expects($this->once())
+            ->method('getClassMetadata')
+            ->will($this->returnValue($metadata));
+        $this->type->buildForm($builder, array('class' => 'TestBundle:Test'));
+    }
+
+    public function testBuildView()
+    {
+        $data = null;
+        $options = array('autocompleter_alias' => 'test');
+        $title = 'Test Value';
+        $view = $this->getMockBuilder('Symfony\Component\Form\FormView')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $form = $this->getMockBuilder('Symfony\Component\Form\Form')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->entityTransformer->expects($this->once())
+            ->method('transform')
+            ->with($options['autocompleter_alias'], $data)
+            ->will($this->returnValue($title));
+        $this->type->buildView($view, $form, $options);
+
+        $this->assertInternalType('array', $view->vars);
+        $this->assertArrayHasKey('attr', $view->vars);
+        $this->assertInternalType('array', $view->vars['attr']);
+        $this->assertArrayHasKey('data-title', $view->vars['attr']);
+        $this->assertEquals($title, $view->vars['attr']['data-title']);
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('oro_jqueryselect2_hidden', $this->type->getName());
+    }
+
+    public function testGetParent()
+    {
+        $this->assertEquals('genemu_jqueryselect2_hidden', $this->type->getParent());
+    }
+}
