a5d2b566d58d0c62c28fc95ad3e2ff5a9df0808e
Gildas Quéméner <gildas.quemener@gmail.com>
2013-06-10 11:32:22 +0200

    PIM-436 Tested option attribute types

---
+++ Bundle/FlexibleEntityBundle/Tests/Unit/AttributeType/OptionSimpleSelectTypeTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\FlexibleEntityBundle\Tests\Unit\AttributeType;
+
+use Oro\Bundle\FlexibleEntityBundle\AttributeType\OptionSimpleSelectType;
+
+class OptionSimpleSelectTypeTest extends AttributeTypeTest
+{
+    protected $name = 'oro_flexibleentity_simpleselect';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->target = new OptionSimpleSelectType('text', 'email', $this->guesser);
+    }
+
+    public function testBuildValueFormType()
+    {
+        $factory = $this->getFormFactoryMock();
+        $value = $this->getFlexibleValueMock(array(
+            'data'        => 'bar',
+            'backendType' => 'foo',
+        ));
+
+        $factory->expects($this->once())
+            ->method('createNamed')
+            ->with('foo', 'email', 'bar', array(
+                'constraints' => array('constraints'),
+                'label'       => null,
+                'required'    => null,
+                'empty_value' => false,
+                'class'       => 'OroFlexibleEntityBundle:AttributeOption',
+                'expanded'    => false,
+                'multiple'    => false,
+                'query_builder' => function () {},
+            ));
+
+        $this->target->buildValueFormType($factory, $value);
+    }
+
+    public function testGetBackendType()
+    {
+        $this->assertEquals('text', $this->target->getBackendType());
+    }
+
+    public function testGetFormType()
+    {
+        $this->assertEquals('email', $this->target->getFormType());
+    }
+
+    public function testBuildAttributeFormType()
+    {
+        $this->assertNull($this->target->buildAttributeFormType(
+            $this->getFormFactoryMock(),
+            $this->getAttributeMock(null, null)
+        ));
+    }
+}
