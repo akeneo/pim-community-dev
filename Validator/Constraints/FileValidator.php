1f54ba85023b41ea2ed07945e775a1b1fb433951
Gildas Quéméner <gildas.quemener@gmail.com>
2013-06-10 18:30:41 +0200

    PIM-436 Extended the file validator to check file extension

---
+++ Validator/Constraints/FileValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pim\Bundle\ProductBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraints\FileValidator as BaseFileValidator;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @author    Gildas Quemener <gildas.quemener@gmail.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FileValidator extends BaseFileValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        parent::validate($value, $constraint);
+
+        $file = new \SplFileInfo($value);
+        if (!in_array($file->getExtension(), $constraint->allowedExtensions)) {
+            $this->context->addViolation($constraint->allowedExtensionsMessage, array(
+                '{{ extensions }}' => $constraint->allowedExtensions,
+            ));
+        }
+    }
+}
+
