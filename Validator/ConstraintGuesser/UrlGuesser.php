40ad2d1cf5790e081d9fdd5c9b4b0a548ee2ad84
Gildas Quéméner <gildas.quemener@gmail.com>
2013-06-11 10:47:03 +0200

    PIM-436 Implemented the url constraint guesser
    Only support varchar attribute which validation rule is email.

---
+++ Validator/ConstraintGuesser/UrlGuesser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pim\Bundle\ProductBundle\Validator\ConstraintGuesser;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Oro\Bundle\FlexibleEntityBundle\Form\Validator\ConstraintGuesserInterface;
+use Oro\Bundle\FlexibleEntityBundle\Model\AbstractAttribute;
+use Oro\Bundle\FlexibleEntityBundle\AttributeType\AbstractAttributeType;
+
+/**
+ * @author    Gildas Quemener <gildas.quemener@gmail.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class UrlGuesser implements ConstraintGuesserInterface
+{
+    public function supportAttribute(AbstractAttribute $attribute)
+    {
+        return AbstractAttributeType::BACKEND_TYPE_VARCHAR === $attribute->getBackendType();
+    }
+
+    public function guessConstraints(AbstractAttribute $attribute)
+    {
+        $constraints = array();
+
+        if ('url' === $attribute->getValidationRule()) {
+            $constraints[] = new Assert\Url;
+        }
+
+        return $constraints;
+    }
+}
+
