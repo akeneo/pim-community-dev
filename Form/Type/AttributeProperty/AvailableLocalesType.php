08715367c788e06543b66e5e93c0202fca645df3
Filips Alpe <filips@akeneo.com>
2013-06-12 11:03:11 +0200

    Create custom attribute property fields

---
+++ Form/Type/AttributeProperty/AvailableLocalesType.php
@@ -0,0 +1,49 @@
+<?php
+namespace Pim\Bundle\ProductBundle\Form\Type\AttributeProperty;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Form type related to availableLocales property of ProductAttribute
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AvailableLocalesType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'required' => false,
+            'multiple' => true,
+            'class' => 'Pim\Bundle\ConfigBundle\Entity\Locale',
+            'query_builder' => function (EntityRepository $repository) {
+                return $repository->createQueryBuilder('l')->where('l.activated = 1')->orderBy('l.code');
+            }
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_product_available_locales';
+    }
+}
