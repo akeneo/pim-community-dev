5e668673dc71f2e88c3b12fa3e66cc4b6caa6ba2
Alexander Nezdoiminoga <ale.n.2k7@gmail.com>
2013-06-20 17:26:01 +0300

    Generator refactoring

--- Bundle/EntityExtendBundle/Tools/Generator/Generator.php
+++ Bundle/EntityExtendBundle/Tools/Generator/Generator.php
@@ -70,111 +70,6 @@ class Generator
         die('generator');
     }
 
-    protected function generateYaml(){}
-
-
-    /**
-     * Prepare Dynamic class
-     *
-     * @param $entityName
-     * @param $className
-     * @return $this
-     */
-    protected function generateDynamicClass($entityName, $className)
-    {
-        $this->writer = new Writer();
-
-        $class = PhpClass::create($this->generateClassName($entityName))
-            ->setName($className)
-            ->setInterfaceNames(array('Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityInterface'))
-            ->setProperty(PhpProperty::create('id')->setVisibility('protected'))
-            ->setProperty(PhpProperty::create('parent')->setVisibility('protected'));
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('getId')->setBody(
-            $this->writer
-                ->write('return $this->id;')
-                ->getContent()
-            )
-        );
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('getParent')->setBody(
-            $this->writer
-                ->write('return $this->parent;')
-                ->getContent()
-            )
-        );
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('setParent')->setBody(
-            $this->writer
-                ->writeln('$this->parent = $parent;')
-                ->writeln('return $this;')
-                ->getContent()
-            )
-            ->addParameter(PhpParameter::create('parent'))
-        );
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('set')->setBody(
-            $this->writer
-                ->writeln('return $this->{\'set\' . ucfirst($key)}($value);')
-                ->getContent()
-            )
-            ->addParameter(PhpParameter::create('key'))
-            ->addParameter(PhpParameter::create('value'))
-        );
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('get')->setBody(
-            $this->writer
-                ->writeln('return $this->{\'get\' . ucfirst($key)}();')
-                ->getContent()
-            )
-            ->addParameter(PhpParameter::create('key'))
-        );
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('__fromArray')->setBody(
-            $this->writer
-                ->writeln('foreach($values as $key => $value) {')
-                ->writeln('    $this->set($key, $value);')
-                ->writeln('}')
-                ->getContent()
-            )
-            ->addParameter(PhpParameter::create('values'))
-        );
-
-        $fields = $this->configProvider->getConfig($entityName)->getFields();
-        $toArray = '';
-        if($fields) {
-            foreach ($fields as $field => $options) {
-
-                if ($this->configProvider->getFieldConfig($entityName, $field)->is('is_extend')) {
-                    $toArray .= '    \''.$field.'\' => $this->'.$field.','."\n";
-                }
-            }
-        }
-
-        $this->writer->reset();
-        $class->setMethod(PhpMethod::create('__toArray')->setBody(
-            $this->writer
-                ->writeln('return array(')
-                ->writeln($toArray)
-                ->writeln(');')
-                ->getContent()
-            )
-        );
-
-        $strategy = new DefaultGeneratorStrategy();
-
-        return $strategy->generate($class);
-    }
-
-    
-
-
     public function generateExtendClassName($entityName)
     {
         return 'Extend\\Entity\\' . $this->mode . '\\' . $this->generateClassName($entityName);
@@ -188,5 +83,107 @@ class Generator
     protected function generateClassName($entityName)
     {
         return str_replace('\\', '', $entityName);
+    }
+
+    protected function generateClassMethod($methodName, $methodBody, $methodArgs = array())
+    {
+        $this->writer->reset();
+        $method = PhpMethod::create($methodName)->setBody(
+            $this->writer->write($methodBody)->getContent()
+        );
+
+        if (count($methodArgs)) {
+            foreach ($methodArgs as $arg) {
+                $method->addParameter(PhpParameter::create($arg));
+            }
+        }
+
+        return $method;
+    }
+
+    /**
+     * Generate Dynamic class
+     *
+     * @param $entityName
+     * @param $className
+     * @return $this
+     */
+    protected function generateDynamicClass($entityName, $className)
+    {
+        $this->writer = new Writer();
+
+        $class = PhpClass::create($this->generateClassName($entityName))
+            ->setName($className)
+            ->setInterfaceNames(array('Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityInterface'))
+            ->setProperty(PhpProperty::create('id')->setVisibility('protected'))
+            ->setProperty(PhpProperty::create('parent')->setVisibility('protected'))
+            ->setMethod($this->generateClassMethod(
+                'getId',
+                'return $this->id;'
+            ))
+            ->setMethod($this->generateClassMethod(
+                'getParent',
+                'return $this->parent;'
+            ))
+            ->setMethod($this->generateClassMethod(
+                'setParent',
+                '$this->parent = $parent;return $this;', array('parent')
+            ))
+            ->setMethod($this->generateClassMethod(
+                'set',
+                'return $this->{\'set\'.ucfirst($key)}($value);',
+                array('key', 'value')
+            ))
+            ->setMethod($this->generateClassMethod(
+                'get',
+                'return $this->{\'get\'.ucfirst($key)}();',
+                array('key')
+            ))
+            ->setMethod($this->generateClassMethod(
+                '__fromArray',
+                'foreach($values as $key => $value){$this->set($key, $value);}',
+                array('values')
+            ));
+
+        $fields = $this->configProvider->getConfig($entityName)->getFields();
+        $toArray = '';
+        if($fields) {
+            foreach ($fields as $field => $options) {
+                if ($this->configProvider->getFieldConfig($entityName, $field)->is('is_extend')) {
+                    $toArray .= "\n".'    \''.$field.'\' => $this->'.$field.',';
+                }
+            }
+        }
+
+        $class->setMethod($this->generateClassMethod(
+            '__toArray',
+            'return array('.$toArray."\n".');'
+        ));
+
+        $strategy = new DefaultGeneratorStrategy();
+
+        return $strategy->generate($class);
+    }
+
+    /**
+     * Generate Proxy class
+     *
+     * @param $entityName
+     * @param $className
+     * @return $this
+     */
+    protected function generateProxyClass($entityName, $className)
+    {
+        $this->writer = new Writer();
+
+        $class = PhpClass::create($this->generateClassName($entityName))
+            ->setName($className)
+            ->setInterfaceNames(array('Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityInterface'))
+            ->setProperty(PhpProperty::create('id')->setVisibility('protected'))
+            ->setProperty(PhpProperty::create('parent')->setVisibility('protected'));
+
+        $strategy = new DefaultGeneratorStrategy();
+
+        return $strategy->generate($class);
     }
 }
