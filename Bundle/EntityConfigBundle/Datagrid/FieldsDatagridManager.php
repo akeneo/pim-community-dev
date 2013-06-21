cca2dd559eb9626d7dabed407397927786979272
Alexander Nezdoiminoga <ale.n.2k7@gmail.com>
2013-06-21 15:32:23 +0300

    Datagrid templates

---
+++ Bundle/EntityConfigBundle/Datagrid/FieldsDatagridManager.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Oro\Bundle\EntityConfigBundle\Datagrid;
+
+use Doctrine\ORM\Query;
+
+use Oro\Bundle\GridBundle\Datagrid\ProxyQueryInterface;
+
+use Oro\Bundle\EntityConfigBundle\ConfigManager;
+
+use Oro\Bundle\GridBundle\Action\ActionInterface;
+use Oro\Bundle\GridBundle\Datagrid\DatagridManager;
+use Oro\Bundle\GridBundle\Field\FieldDescription;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionInterface;
+use Oro\Bundle\GridBundle\Filter\FilterInterface;
+use Oro\Bundle\GridBundle\Property\UrlProperty;
+
+class FieldsDatagridManager extends DatagridManager
+{
+    /**
+     * @var FieldDescriptionCollection
+     */
+    protected $fieldsCollection;
+
+    /**
+     * @var ConfigManager
+     */
+    protected $configManager;
+
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getProperties()
+    {
+        return array(
+            //new UrlProperty('view_link', $this->router, 'oro_entityconfig_update', array('className')),
+            //new UrlProperty('update_link', $this->router, 'oro_entityconfig_fields_update', array('id')),
+            //new UrlProperty('fields_link', $this->router, 'oro_entityconfig_fields', array('id')),
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureFields(FieldDescriptionCollection $fieldsCollection)
+    {
+        $fieldObjectId = new FieldDescription();
+        $fieldObjectId->setName('Id');
+        $fieldObjectId->setOptions(
+            array(
+                'type'        => FieldDescriptionInterface::TYPE_INTEGER,
+                'label'       => 'Id',
+                'field_name'  => 'id',
+                'filter_type' => FilterInterface::TYPE_NUMBER,
+                'required'    => false,
+                'sortable'    => true,
+                'filterable'  => false,
+                'show_filter' => true,
+            )
+        );
+        $fieldsCollection->add($fieldObjectId);
+
+//        $fieldObjectName = new FieldDescription();
+//        $fieldObjectName->setName('className');
+//        $fieldObjectName->setOptions(
+//            array(
+//                'type'        => FieldDescriptionInterface::TYPE_TEXT,
+//                'label'       => 'Class Name',
+//                'field_name'  => 'className',
+//                'filter_type' => FilterInterface::TYPE_STRING,
+//                'required'    => false,
+//                'sortable'    => true,
+//                'filterable'  => false,
+//                'show_filter' => false,
+//            )
+//        );
+//        $fieldsCollection->add($fieldObjectName);
+
+//        foreach ($this->configManager->getProviders() as $provider) {
+//            foreach ($provider->getConfigContainer()->getEntityItems() as $code => $item) {
+//                if (isset($item['grid'])) {
+//                    $fieldObjectName = new FieldDescription();
+//                    $fieldObjectName->setName($code);
+//                    $fieldObjectName->setOptions(array_merge($item['grid'], array(
+//                        'expression' => 'cev' . $code . '.value'
+//                    )));
+//                    $fieldsCollection->add($fieldObjectName);
+//                }
+//            }
+//        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getRowActions()
+    {
+        $viewAction = array(
+            'name'         => 'view',
+            'type'         => ActionInterface::TYPE_REDIRECT,
+            'acl_resource' => 'root',
+            'options'      => array(
+                'label' => 'View',
+                'icon'  => 'book',
+                'link'  => 'view_link',
+            )
+        );
+
+        $updateAction = array(
+            'name'         => 'update',
+            'type'         => ActionInterface::TYPE_REDIRECT,
+            'acl_resource' => 'root',
+            'options'      => array(
+                'label' => 'Edit',
+                'icon'  => 'edit',
+                'link'  => 'update_link',
+            )
+        );
+
+        $deleteAction = array(
+            'name'         => 'delete',
+            'type'         => ActionInterface::TYPE_DELETE,
+            'acl_resource' => 'root',
+            'options'      => array(
+                'label' => 'Delete',
+                'icon'  => 'trash',
+                'link'  => 'delete_link',
+            )
+        );
+
+        return array($viewAction, $updateAction, $deleteAction);
+    }
+
+    /**
+     * @return ProxyQueryInterface
+     */
+//    protected function createQuery()
+//    {
+//        /** @var ProxyQueryInterface|Query $query */
+//        $query = parent::createQuery();
+//
+//        foreach ($this->configManager->getProviders() as $provider) {
+//            foreach ($provider->getConfigContainer()->getEntityItems() as $code => $item) {
+//                $alias = 'cev'. $code;
+//                $query->leftJoin('ce.values', $alias, 'WITH', $alias . ".code='".$code . "' AND " . $alias . ".scope='" . $provider->getScope() . "'");
+//                $query->addSelect($alias . '.value as '. $code, true);
+//            }
+//        }
+//
+//        return $query;
+//    }
+}
