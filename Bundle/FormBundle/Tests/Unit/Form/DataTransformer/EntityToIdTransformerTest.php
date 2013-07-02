d63580a0cd9ae3f874774740ecc1156566e79070
Aleksandr Smaga <asmaga@magecore.com>
2013-07-02 15:11:56 +0200

    BAP-1040: Fix Oro/OroCRM unit tests    - Fix expected exception types in tests

--- Bundle/FormBundle/Tests/Unit/Form/DataTransformer/EntityToIdTransformerTest.php
+++ Bundle/FormBundle/Tests/Unit/Form/DataTransformer/EntityToIdTransformerTest.php
@@ -46,7 +46,7 @@ class EntityToIdTransformerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException \Symfony\Component\PropertyAccess\Exception\UnexpectedTypeException
+     * @expectedException \Symfony\Component\Form\Exception\UnexpectedTypeException
      * @expectedExceptionMessageExpected Expected argument of type "array", "string" given
      */
     public function testTransformFailsWhenValueInNotAnArray()
@@ -128,7 +128,7 @@ class EntityToIdTransformerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException Symfony\Component\PropertyAccess\Exception\UnexpectedTypeException
+     * @expectedException Symfony\Component\Form\Exception\UnexpectedTypeException
      * @expectedExceptionMessage Expected argument of type "Doctrine\ORM\QueryBuilder", "NULL" given
      */
     public function testReverseTransformQueryBuilderException()
@@ -204,7 +204,7 @@ class EntityToIdTransformerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException Symfony\Component\PropertyAccess\Exception\UnexpectedTypeException
+     * @expectedException Symfony\Component\Form\Exception\UnexpectedTypeException
      * @expectedExceptionMessage Expected argument of type "callable", "string" given
      */
     public function testCallbackException()
