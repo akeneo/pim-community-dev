ae9d090aea4ed8315b72668d0db687d1e1f2ecab
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-07-03 13:07:50 +0300

    CRM-221: Update form type for TypedAddress - added translation of validation constraints

--- Bundle/AddressBundle/Validator/Constraints/UniqueAddressTypes.php
+++ Bundle/AddressBundle/Validator/Constraints/UniqueAddressTypes.php
@@ -4,9 +4,6 @@ namespace Oro\Bundle\AddressBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 
-/**
- * @Annotation
- */
 class UniqueAddressTypes extends Constraint
 {
     public $message = 'Different addresses cannot have same type.';
