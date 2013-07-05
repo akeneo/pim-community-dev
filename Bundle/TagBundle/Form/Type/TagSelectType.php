3d89d08a68dd54d295b18539f4b7ee711e713293
Ivan Shakuta <ishakuta@gmail.com>
2013-07-05 17:05:31 +0000

    BAP-1104: UI for create/assign tags on entities - macro for rendering tag input field - autocomplete form tag select type - tag search config

---
+++ Bundle/TagBundle/Form/Type/TagSelectType.php
@@ -0,0 +1,33 @@
+<?php
+namespace Oro\Bundle\TagBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TagSelectType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'configs' => array(
+                    'placeholder' => 'oro.tag.form.choose_tag'
+                ),
+                'autocomplete_alias' => 'tags'
+            )
+        );
+    }
+
+    public function getParent()
+    {
+        return 'oro_jqueryselect2_hidden';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'oro_tag_select';
+    }
+}
