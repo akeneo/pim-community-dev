68c87063c3e383b8166f2897657f7362abe4380a
kotfalya <kotfalya@gmail.com>
2013-07-05 19:02:22 +0300

    test for ConfigSubscriber.php

--- Bundle/EntityConfigBundle/Tests/Unit/Form/EventListener/ConfigSubscriberTest.php
+++ Bundle/EntityConfigBundle/Tests/Unit/Form/EventListener/ConfigSubscriberTest.php
@@ -2,6 +2,10 @@
 
 namespace Oro\Bundle\EntityConfigBundle\Tests\Unit\Form\EventListener;
 
+use Oro\Bundle\EntityConfigBundle\Config\EntityConfig;
+use Oro\Bundle\EntityConfigBundle\Config\FieldConfig;
+use Oro\Bundle\EntityConfigBundle\DependencyInjection\EntityConfigContainer;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\EntityConfigBundle\Tests\Unit\ConfigManagerTest;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -23,18 +27,24 @@ class ConfigSubscriberTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        $entityConfig = new EntityConfig(ConfigManagerTest::DEMO_ENTITY, 'test');
+        $entityConfig->addField(new FieldConfig(ConfigManagerTest::DEMO_ENTITY, 'testField', 'string', 'scope'));
+
         $this->configManager = $this->getMockBuilder('Oro\Bundle\EntityConfigBundle\ConfigManager')
             ->disableOriginalConstructor()
             ->getMock();
 
+        $configProvider = new ConfigProvider($this->configManager, new EntityConfigContainer(array('scope' => 'test')));
+
+        $this->configManager->expects($this->any())->method('getConfig')->will($this->returnValue($entityConfig));
         $this->configManager->expects($this->any())->method('hasConfig')->will($this->returnValue(true));
         $this->configManager->expects($this->any())->method('flush')->will($this->returnValue(true));
-        $this->configManager->expects($this->any())->method('getProviders')->will($this->returnValue(array()));
+        $this->configManager->expects($this->any())->method('getProviders')->will($this->returnValue(array($configProvider)));
 
         $this->subscriber = new ConfigSubscriber($this->configManager);
     }
 
-    public function testPostBind()
+    public function testPostBindEntity()
     {
         $form       = $this->getMock('Symfony\Component\Form\Tests\FormInterface');
         $formConfig = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
@@ -47,10 +57,29 @@ class ConfigSubscriberTest extends \PHPUnit_Framework_TestCase
 
         $form->expects($this->any())->method('getConfig')->will($this->returnValue($formConfig));
 
-        $event = new FormEvent($form, array());
+        $event = new FormEvent($form, array('test' => array('first' => 'data')));
 
         $this->subscriber->postBind($event);
     }
+
+    public function testPostBindField()
+    {
+        $form       = $this->getMock('Symfony\Component\Form\Tests\FormInterface');
+        $formConfig = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $formConfig->expects($this->any())->method('getOptions')->will(
+            $this->returnValue(array('class_name' => ConfigManagerTest::DEMO_ENTITY, 'field_name' => 'testField'))
+        );
+
+        $form->expects($this->any())->method('getConfig')->will($this->returnValue($formConfig));
+
+        $event = new FormEvent($form, array('test' => array('testField' => 'data')));
+
+        $this->subscriber->postBind($event);
+    }
+
 
     public function testGetSubscribedEvents()
     {
