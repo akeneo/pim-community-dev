acc0062b77a2e2a7d24919d0cd1da4eb5058ec1f
Alexander Nezdoiminoga <alex@magecore.com>
2013-07-08 14:06:16 +0300

    test fix

---
+++ Bundle/EntityConfigBundle/Tests/Unit/DependencyInjection/Compiler/EntityConfigPassTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oro\Bundle\EntityConfigBundle\Tests\Unit\DependencyInjection\Compiler;
+
+class EntityConfigPassTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+
+    }
+
+    public function test()
+    {
+
+    }
+}
