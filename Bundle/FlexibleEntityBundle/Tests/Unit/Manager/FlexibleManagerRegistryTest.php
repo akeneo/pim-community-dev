e0eb48baba5e66863ba27f26aad62dce0fe5d2a2
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-07-09 16:30:43 +0300

    CRM-232: Add columns and filters for contact group, lead source and primary postal code  - revert small change in tests

--- Bundle/FlexibleEntityBundle/Tests/Unit/Manager/FlexibleManagerRegistryTest.php
+++ Bundle/FlexibleEntityBundle/Tests/Unit/Manager/FlexibleManagerRegistryTest.php
@@ -36,7 +36,7 @@ class FlexibleManagerRegistryTest extends \PHPUnit_Framework_TestCase
     /**
      * @var string
      */
-    protected $entityFQCN = 'Oro\\Bundle\\FlexibleEntityBundle\\Tests\\Unit\\Entity\\Demo\\Flexible';
+    protected $entityFQCN = 'Oro\\Bundle\\FlexibleEntityBundle\\Test\\Entity\\Demo';
 
     /**
      * @var FlexibleManager
