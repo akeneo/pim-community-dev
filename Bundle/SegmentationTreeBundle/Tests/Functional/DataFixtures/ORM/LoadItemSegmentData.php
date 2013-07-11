07d1da102c12cc9459bc04cda59cc19725a81e96
Benoit Jacquemont <benoit@akeneo.com>
2013-07-11 21:55:16 +0200

    Removing bogus class header

--- Bundle/SegmentationTreeBundle/Tests/Functional/DataFixtures/ORM/LoadItemSegmentData.php
+++ Bundle/SegmentationTreeBundle/Tests/Functional/DataFixtures/ORM/LoadItemSegmentData.php
@@ -14,9 +14,6 @@ use Doctrine\Common\Persistence\ObjectManager;
 /**
  * Load tests items and items segment
  *
- * @author    Benoit Jacquemont <benoit@akeneo.com>
- * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/MIT MIT
  */
 class LoadItemSegmentData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
