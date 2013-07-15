d2a62971d02d58329cc52d6db187d8e6625f7d1b
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-07-15 19:54:51 +0300

    BAP-1122: Add ability to copy information from the Grid - added possibility to select text in grid cell when row action is bind

--- Bundle/GridBundle/Resources/public/js/app/datagrid/row.js
+++ Bundle/GridBundle/Resources/public/js/app/datagrid/row.js
@@ -18,6 +18,13 @@ Oro.Datagrid.Row = Backgrid.Row.extend({
         "click": "onClick"
     },
 
+    /** @property */
+    clickData: {
+        counter: 0,
+        timeout: 300,
+        hasSelectedText: false
+    },
+
     /**
      * jQuery event handler for row click, trigger "clicked" event if row element was clicked
      *
@@ -27,9 +34,35 @@ Oro.Datagrid.Row = Backgrid.Row.extend({
         var targetElement = e.target;
         var targetParentElement = $(e.target).parent().get(0);
 
-        if (this.el == targetElement || this.el == targetParentElement) {
-            this.trigger('clicked', this, e);
+        if (!this.el == targetElement && !this.el == targetParentElement) {
+            return;
         }
+
+        if (this.clickData.counter == 0 && !this._hasSelectedText()) {
+            this.clickData.counter++;
+            setTimeout(_.bind(function() {
+                if (!this._hasSelectedText() && this.clickData.counter == 1) {
+                    this.trigger('clicked', this, e);
+                }
+                this.clickData.counter = 0;
+            }, this), this.clickData.timeout);
+        }
+    },
+
+    /**
+     * Checks if selected text is available
+     *
+     * @returns {string}
+     * @return {boolean}
+     */
+    _hasSelectedText: function() {
+        var text = "";
+        if (typeof window.getSelection != "undefined") {
+            text = window.getSelection().toString();
+        } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+            text = document.selection.createRange().text;
+        }
+        return !_.isEmpty(text);
     },
 
     /**
