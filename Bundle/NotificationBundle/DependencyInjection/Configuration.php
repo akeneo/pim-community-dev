869f87665f5d9443d7d03742ce82efe7d5913a65
Aleksandr Smaga <asmaga@magecore.com>
2013-07-15 17:32:11 +0200

    BAP-1097: Notification rules grid    - Rules CRUD

--- Bundle/NotificationBundle/DependencyInjection/Configuration.php
+++ Bundle/NotificationBundle/DependencyInjection/Configuration.php
@@ -18,11 +18,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('oro_notification');
+        $treeBuilder->root('oro_notification');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
         return $treeBuilder;
     }
 }
