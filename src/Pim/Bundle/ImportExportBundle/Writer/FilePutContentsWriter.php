906884d4850960addeab1177e336569d1eef4aa4
Gildas Quéméner <gildas.quemener@gmail.com>
2013-07-17 15:26:27 +0200

    PIM-692 Implemented a simple file writer

--- src/Pim/Bundle/ImportExportBundle/Writer/FilePutContentsWriter.php
+++ src/Pim/Bundle/ImportExportBundle/Writer/FilePutContentsWriter.php
@@ -2,16 +2,22 @@
 
 namespace Pim\Bundle\ImportExportBundle\Writer;
 
+use Pim\Bundle\ImportExportBundle\Exception\FileExistsException;
+
 class FilePutContentsWriter implements WriterInterface
 {
     protected $path;
 
     public function __construct($path)
     {
+        if (file_exists($path)) {
+            throw new FileExistsException($path);
+        }
         $this->path = $path;
     }
-    public function write($data, array $options)
+
+    public function write($data)
     {
-        file_put_contents($this->path, $data);
+        return file_put_contents($this->path, $data, FILE_APPEND);
     }
 }
