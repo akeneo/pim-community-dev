01e56f84ddc4bdaf9cefc5ddb7bfa33ebd1aeaad
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-07-18 15:45:55 +0300

    BAP-1194: Implement wrapper for Translator.get() call   - CR-changes

--- Bundle/TranslationBundle/Resources/public/js/translator.js
+++ Bundle/TranslationBundle/Resources/public/js/translator.js
@@ -1,10 +1,12 @@
 (function(Translator, _, Oro) {
     var dict = {},
-        add = Translator.add;
+        add = Translator.add,
+        get = Translator.get;
 
     /**
-     * Store all translation ids which were added to Translator
-     * @param id
+     * Adds a translation to Translator object and stores
+     * translation id in protected dictionary
+     * @param {string} id
      */
     Translator.add = function(id) {
         dict[id] = 1;
@@ -12,9 +14,20 @@
     };
 
     /**
+     * Fetches translation by its id,
+     * but before checks if the id was registered in dictionary
+     * @param {string} id
+     * @returns {string}
+     */
+    Translator.get = function(id) {
+        checkTranslation(id);
+        return get.apply(Translator, arguments);
+    };
+
+    /**
      * Checks if translation for passed id exist, if it's Oro.debug mode
      * and there's no translation - output error message in console
-     * @param id
+     * @param {string} id
      */
     function checkTranslation(id) {
         if (!Oro.debug) {
@@ -26,7 +39,7 @@
             };
         domains = _.union([undefined], _.isArray(domains) ? domains : [domains]);
         if (!_.some(domains, checker)) {
-            console.error('Translation "%s" does not exist!', id);
+            console.error('Untranslated: %s', id);
         }
     }
 
@@ -34,12 +47,8 @@
         /**
          * Shortcut for Translator.get() method call,
          * Due to it's underscore mixin, it can be used inside templates
-         * @param id
          * @returns {string}
          */
-        __: function(id) {
-            checkTranslation(id);
-            return Translator.get.apply(Translator, arguments);
-        }
+        __: _.bind(Translator.get, Translator)
     });
 }) (Translator, _, Oro);
