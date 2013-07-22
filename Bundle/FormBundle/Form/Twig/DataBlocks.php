a4c83152f524aaedcbbf666e512c55fe8e3a8425
Alexander Nezdoiminoga <alex@magecore.com>
2013-07-22 14:09:04 +0300

    BAP-1024 forms

---
+++ Bundle/FormBundle/Form/Twig/DataBlocks.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Form\Twig;
+
+use Oro\Bundle\FormBundle\Config\SubBlockConfig;
+use Symfony\Component\Form\FormView;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+use Oro\Bundle\FormBundle\Config\BlockConfig;
+use Oro\Bundle\FormBundle\Config\FormConfig;
+
+class DataBlocks
+{
+    /**
+     * @var FormConfig
+     */
+    protected $formConfig;
+
+    /**
+     * @var string
+     */
+    protected $formVariableName;
+
+    /**
+     * @var mixed
+     */
+    protected $context;
+
+    /**
+     * @var \Twig_Environment
+     */
+    protected $env;
+
+    /**
+     * @var PropertyAccessor
+     */
+    protected $accessor;
+
+    public function __construct()
+    {
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
+     * @param  \Twig_Environment $env
+     * @param                    $context
+     * @param  FormView          $form
+     * @param  string            $formVariableName
+     * @return array
+     */
+    public function render(\Twig_Environment $env, $context, FormView $form, $formVariableName = 'form')
+    {
+        $this->formVariableName = $formVariableName;
+        $this->formConfig       = new FormConfig;
+        $this->context          = $context;
+        $this->env              = $env;
+
+        $tmpLoader = $env->getLoader();
+        $env->setLoader(new \Twig_Loader_Chain(array($tmpLoader, new \Twig_Loader_String())));
+
+        $this->renderBlock($form);
+
+        $env->setLoader($tmpLoader);
+
+        return $this->formConfig->toArray();
+    }
+
+    /**
+     * @param FormView $form
+     */
+    protected function renderBlock(FormView $form)
+    {
+        if (isset($form->vars['block_config'])) {
+            foreach ($form->vars['block_config'] as $code => $blockConfig) {
+                $this->createBlock($code, $blockConfig);
+            }
+        }
+
+        foreach ($form->children as $name => $child) {
+            if (isset($child->vars['block']) || isset($child->vars['subblock'])) {
+
+                $block = null;
+                if ($this->formConfig->hasBlock($child->vars['block'])) {
+                    $block = $this->formConfig->getBlock($child->vars['block']);
+                }
+
+                if (!$block) {
+                    $blockCode = $child->vars['block'];
+                    $block     = $this->createBlock($blockCode);
+
+                    $this->formConfig->addBlock($block);
+                }
+
+                $subBlock = $this->getSubBlock($name, $child, $block);
+
+                $tmpChild = $child;
+                $formPath = '';
+
+                while ($tmpChild->parent) {
+                    $formPath = sprintf('.children[\'%s\']', $tmpChild->vars['name']) . $formPath;
+                    $tmpChild = $tmpChild->parent;
+                }
+
+                $subBlock->addData($this->env->render(
+                    '{{ form_row(' . $this->formVariableName . $formPath . ') }}',
+                    $this->context
+                ));
+            }
+
+            $this->renderBlock($child);
+        }
+    }
+
+    protected function getSubBlock($name, FormView $child, BlockConfig $block)
+    {
+        $subBlock = null;
+        if (isset($child->vars['subblock']) && $block->hasSubBlock($child->vars['subblock'])) {
+            $subBlock = $block->getSubBlock($child->vars['subblock']);
+        } elseif (!isset($child->vars['subblock'])) {
+            $subBlocks = $block->getSubBlocks();
+            $subBlock  = reset($subBlocks);
+        }
+
+        if (!$subBlock) {
+            if (isset($child->vars['subblock'])) {
+                $subBlockCode = $child->vars['subblock'];
+            } else {
+                $subBlockCode = $name . '__subblock';
+            }
+
+            $subBlock = $this->createSubBlock($subBlockCode, array('title' => ''));
+            $block->addSubBlock($subBlock);
+        }
+
+        return $subBlock;
+    }
+
+    /**
+     * @param        $code
+     * @param  array $blockConfig
+     * @return BlockConfig
+     */
+    protected function createBlock($code, $blockConfig = array())
+    {
+        $block = new BlockConfig($code);
+        $block->setClass($this->accessor->getValue($blockConfig, '[class]'));
+        $block->setPriority($this->accessor->getValue($blockConfig, '[priority]'));
+
+        $title = $this->accessor->getValue($blockConfig, '[title]')
+            ? $this->accessor->getValue($blockConfig, '[title]')
+            : ucfirst($code);
+        $block->setTitle($title);
+
+        foreach ((array)$this->accessor->getValue($blockConfig, '[subblocks]') as $subCode => $subBlockConfig) {
+            $block->addSubBlock($this->createSubBlock($subCode, (array)$subBlockConfig));
+        }
+
+        $this->formConfig->addBlock($block);
+
+        return $block;
+    }
+
+    /**
+     * @param $code
+     * @param $config
+     * @return SubBlockConfig
+     */
+    protected function createSubBlock($code, $config)
+    {
+        $subBlock = new SubBlockConfig($code);
+        $subBlock->setTitle($this->accessor->getValue($config, '[title]'));
+        $subBlock->setPriority($this->accessor->getValue($config, '[priority]'));
+
+        return $subBlock;
+    }
+}
---
+++ Bundle/FormBundle/Tests/Unit/Form/Twig/DataBlocksTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Tests\Unit\Form\Twig;
+
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\Forms;
+
+use Oro\Bundle\FormBundle\Form\Extension\DataBlockExtension;
+use Oro\Bundle\FormBundle\Form\Twig\DataBlocks;
+
+class DataBlocksTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var FormFactory
+     */
+    private $factory;
+
+    /**
+     * @var  DataBlocks
+     */
+    private $dataBlocks;
+
+    /**
+     * @var array
+     */
+    private $testFormConfig = array(
+        'second' => array(
+            'title'     => 'Second',
+            'class'     => null,
+            'subblocks' => array(
+                'text_3__subblock' => array(
+                    'code'  => 'text_3__subblock',
+                    'title' => null,
+                    'data'  => array(null),
+                ),
+            ),
+        ),
+        'first'  => array(
+            'title'     => 'First Block',
+            'class'     => null,
+            'subblocks' => array(
+                'first'  => array(
+                    'code'  => 'first',
+                    'title' => null,
+                    'data'  => array(null),
+                ),
+                'second' => array(
+                    'code'  => 'second',
+                    'title' => 'Second SubBlock',
+                    'data'  => array(null),
+                ),
+            ),
+        ),
+        'third'  => array(
+            'title'     => 'Third',
+            'class'     => null,
+            'subblocks' => array(
+                'text_4__subblock' => array(
+                    'code'  => 'text_4__subblock',
+                    'title' => null,
+                    'data'  => array(null),
+                ),
+            ),
+        ),
+    );
+
+    public function setUp()
+    {
+        $this->dataBlocks = new DataBlocks();
+
+        $this->factory = Forms::createFormFactoryBuilder()
+            ->addTypeExtension(new DataBlockExtension())
+            ->getFormFactory();
+
+        $this->twig = $this->getMockBuilder('\Twig_Environment')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->twig->expects($this->any())
+            ->method('render')
+            ->will($this->returnValue(null));
+        $this->twig->expects($this->any())
+            ->method('getLoader')
+            ->will($this->returnValue($this->getMockForAbstractClass('\Twig_LoaderInterface')));
+    }
+
+    public function testConstruct()
+    {
+        $this->assertInstanceOf(
+            'Symfony\Component\PropertyAccess\PropertyAccessor',
+            $this->readAttribute($this->dataBlocks, 'accessor')
+        );
+    }
+
+    public function testRender()
+    {
+        $options = array('block_config' =>
+              array(
+                  'first'  => array(
+                      'priority'  => 1,
+                      'title'     => 'First Block',
+                      'subblocks' => array(
+                          'first'  => array(),
+                          'second' => array(
+                              'title' => 'Second SubBlock'
+                          ),
+                      ),
+                  ),
+                  'second' => array(
+                      'priority' => 2,
+                  )
+              )
+        );
+        $builder = $this->factory->createNamedBuilder('test', 'form', null, $options);
+        $builder->add('text_1', null, array('block' => 'first', 'subblock' => 'second'));
+        $builder->add('text_2', null, array('block' => 'first'));
+        $builder->add('text_3', null, array('block' => 'second'));
+        $builder->add('text_4', null, array('block' => 'third'));
+        $builder->add('text_5', null);
+
+        $formView = $builder->getForm()->createView();
+
+        $result = $this->dataBlocks->render($this->twig, array('form' => $formView), $formView);
+
+        $this->assertEquals($this->testFormConfig, $result);
+    }
+}
