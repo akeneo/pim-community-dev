183852cbb378a5a72e0067fc51e5ff291451edeb
Falko Konstantin <kotfalya@gmail.com>
2013-07-22 17:53:42 +0300

    clear code

--- Bundle/EntityBundle/OroEntityBundle.php
+++ Bundle/EntityBundle/OroEntityBundle.php
@@ -2,16 +2,8 @@
 
 namespace Oro\Bundle\EntityBundle;
 
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use Oro\Bundle\EntityBundle\DependencyInjection\Compiler\ExtendCompilerPass;
 
 class OroEntityBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        //$container->addCompilerPass(new ExtendCompilerPass());
-    }
 }
