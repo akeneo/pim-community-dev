96e31fcd0a475c66c9df58f86e5b575793c583a5
Dima Soroka <dima@magecore.com>
2013-07-24 15:51:42 -0700

    BAP-1258: Added ability to enable debug for all CSS and JS assets in oro assetic

--- Bundle/AsseticBundle/Tests/Unit/DependencyInjection/OroAsseticExtensionTest.php
+++ Bundle/AsseticBundle/Tests/Unit/DependencyInjection/OroAsseticExtensionTest.php
@@ -21,9 +21,12 @@ class OroAsseticExtensionTest extends \PHPUnit_Framework_TestCase
                 )
             );
 
-        $assets = $extension->getAssets($container, array('uncompress_css' => array(), 'uncompress_js' => array()));
+        $assets = $extension->getAssets(
+            $container,
+            array('css_debug' => array(), 'js_debug' => array(), 'css_debug_all' => false, 'js_debug_all' => true)
+        );
 
         $this->assertEquals('second.css', $assets['css']['compress'][0][1]);
-        $this->assertEquals('first.js', $assets['js']['compress'][0][0]);
+        $this->assertEquals('first.js', $assets['js']['uncompress'][0][0]);
     }
 }
