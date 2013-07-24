3b6fe98ca0c370cbcc06feeae0051c1715ff537d
Gildas Quéméner <gildas.quemener@gmail.com>
2013-07-24 17:37:43 +0200

    Remove the connector compiler pass and the associated service definitions

--- src/Pim/Bundle/BatchBundle/PimBatchBundle.php
+++ src/Pim/Bundle/BatchBundle/PimBatchBundle.php
@@ -17,13 +17,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimBatchBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        //$container->addCompilerPass(new ConnectorCompilerPass());
-    }
 }
