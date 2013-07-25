ec0dba995031fee26292ea08e477aec0a6159454
Filips Alpe <filips@akeneo.com>
2013-07-25 18:09:31 +0200

    Update some symfony 2.3 stuff

--- app/autoload.php
+++ app/autoload.php
@@ -1,13 +1,12 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
+/**
+ * @var $loader ClassLoader
+ */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-}
 
 // add possibility to extends doctrine unit test and use mocks
 $loader->add( 'Doctrine\\Tests', __DIR__.'/../vendor/doctrine/orm/tests' );
--- web/app_behat.php
+++ web/app_behat.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
@@ -10,16 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        '127.0.0.1',
-        '::1',
-    ))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
+
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('behat', true);
