cf235c0cecc00237337a6090aa2bbb4c42832a83
Michael Banin <mbanin@magecore.com>
2013-07-25 12:43:20 +0300

    BAP-1156 Functional Selenium Tests for Tag CRUD - also here is BAP-1171

---
+++ Bundle/TestFrameworkBundle/Pages/Objects/Tag.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\TestFrameworkBundle\Pages\Objects;
+
+use Oro\Bundle\TestFrameworkBundle\Pages\AbstractEntity;
+use Oro\Bundle\TestFrameworkBundle\Pages\Entity;
+
+class Tag extends AbstractEntity implements Entity
+{
+    protected $tagname;
+
+    public function __construct($testCase, $redirect = true)
+    {
+        parent::__construct($testCase, $redirect);
+        $this->tagname = $this->byId('oro_tag_tag_form_name');
+    }
+
+    public function setTagname($accountname)
+    {
+        $this->tagname->clear();
+        $this->tagname->value($accountname);
+        return $this;
+    }
+
+    public function getTagname()
+    {
+        return $this->tagname->value();
+    }
+
+    public function save()
+    {
+        $this->byXPath("//button[contains(., 'Save')]")->click();
+        $this->waitPageToLoad();
+        $this->waitForAjax();
+        return new Tags($this->test);
+    }
+
+    public function close()
+    {
+        return new Tags($this->test);
+    }
+}
