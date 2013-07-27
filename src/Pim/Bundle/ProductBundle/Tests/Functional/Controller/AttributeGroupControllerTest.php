4a3d0aaabf2dd03d2f1191d874b3c003c9faa85b
Filips Alpe <filips@akeneo.com>
2013-07-27 11:44:43 +0200

    Skip asserting flash message in functional tests - issue with javascript generated message

--- src/Pim/Bundle/ProductBundle/Tests/Functional/Controller/AttributeGroupControllerTest.php
+++ src/Pim/Bundle/ProductBundle/Tests/Functional/Controller/AttributeGroupControllerTest.php
@@ -35,12 +35,12 @@ class AttributeGroupControllerTest extends ControllerTest
     /**
      * @staticvar string
      */
-    const GROUP_SAVED_MSG = 'Group successfully saved';
+    const GROUP_SAVED_MSG = 'Attribute group successfully saved';
 
     /**
      * @staticvar string
      */
-    const GROUP_REMOVED_MSG = 'Group successfully removed';
+    const GROUP_REMOVED_MSG = 'Attribute group successfully removed';
 
     /**
      * Test related action
