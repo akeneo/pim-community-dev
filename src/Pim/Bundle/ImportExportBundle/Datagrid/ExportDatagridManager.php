8c163cf44cf40914e43cd7e154ea5c64d2d20c19
Filips Alpe <filips@akeneo.com>
2013-07-29 09:46:40 +0200

    Fix export edit action in grid

--- src/Pim/Bundle/ImportExportBundle/Datagrid/ExportDatagridManager.php
+++ src/Pim/Bundle/ImportExportBundle/Datagrid/ExportDatagridManager.php
@@ -26,6 +26,17 @@ class ExportDatagridManager extends DatagridManager
     /**
      * {@inheritdoc}
      */
+    protected function getProperties()
+    {
+        return array(
+            new UrlProperty('edit_link', $this->router, 'pim_importexport_export_edit', array('id')),
+            new UrlProperty('delete_link', $this->router, 'pim_product_exportprofile_remove', array('id'))
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configureFields(FieldDescriptionCollection $fieldsCollection)
     {
         $field = new FieldDescription();
