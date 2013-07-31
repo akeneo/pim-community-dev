3d9566f3789d533a8a1a49b79d739308b29d3c51
Aleksandr Smaga <asmaga@magecore.com>
2013-07-31 19:26:20 +0200

    BAP-1233: Tags refactoring

---
+++ Bundle/TagBundle/Form/Type/TagAutocompleteType.php
@@ -0,0 +1,46 @@
+<?php
+namespace Oro\Bundle\TagBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Oro\Bundle\TagBundle\Entity\TagManager;
+
+class TagAutocompleteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'configs' => array(
+                    'placeholder'    => 'oro.tag.form.choose_tag',
+                    'extra_config'   => 'multi_autocomplete',
+                    'multiple'       => true
+                ),
+                'autocomplete_alias' => 'tags',
+                'inherit_data'       => true
+            )
+        );
+
+        $resolver->setNormalizers(array());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'oro_tag_autocomplete';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'oro_jqueryselect2_hidden';
+    }
+}
--- Bundle/TagBundle/Form/Type/TagSelectType.php
+++ Bundle/TagBundle/Form/Type/TagSelectType.php
@@ -3,11 +3,8 @@ namespace Oro\Bundle\TagBundle\Form\Type;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Oro\Bundle\TagBundle\Entity\TagManager;
-use Oro\Bundle\TagBundle\Form\TagsTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class TagSelectType extends AbstractType
@@ -32,18 +29,16 @@ class TagSelectType extends AbstractType
         $this->tagManager = $tagManager;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(
             array(
-                'configs' => array(
-                    'placeholder'  => 'oro.tag.form.choose_tag',
-                    'multiple'     => true,
-                    'tokenSeparators' => array(',', ' '),
-                    'tags' => true,
-                    'extra_config' => 'multi_autocomplete',
-                ),
-                'autocomplete_alias' => 'tags',
+                'fields'       => array(), // ?
+                'form'         => array(), // ?
+                'inherit_data' => true,
             )
         );
     }
@@ -53,15 +48,10 @@ class TagSelectType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $transformer = new TagsTransformer($this->om, 'Oro\Bundle\TagBundle\Entity\Tag');
-        $transformer->setTagManager($this->tagManager);
-
-        $builder->addModelTransformer($transformer);
-    }
-
-    public function getParent()
-    {
-        return 'oro_jqueryselect2_hidden';
+        $builder->add(
+            'autocomplete',
+            'oro_tag_autocomplete'
+        );
     }
 
     /**
