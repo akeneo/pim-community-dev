d6cc353f44262429d503c99031ac019d5e6a2363
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-07-31 19:14:43 +0300

    Implemented  BAP-1289 & BAP-1290: Remember selection on pagination; "All" and "All Visible"

---
+++ Bundle/GridBundle/Resources/public/js/app/datagrid/cell/selectrowcell.js
@@ -0,0 +1,267 @@
+/* jshint browser:true */
+/* global define, Oro, $, _, Backbone, Backgrid */
+(function (factory) {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
+        define(['Oro', 'jQuery', '_', 'Backbone', 'Backgrid'], factory);
+    } else {
+        factory();
+    }
+}(function () {
+    "use strict";
+    Oro.Datagrid = Oro.Datagrid || {};
+    Oro.Datagrid.Cell = Oro.Datagrid.Cell || {};
+
+    /**
+     * Renders a checkbox for row selection.
+     *
+     * @class Oro.Datagrid.Cell.SelectRowCell
+     * @extends Backbone.View
+     */
+    Oro.Datagrid.Cell.SelectRowCell = Backbone.View.extend({
+
+        /** @property */
+        className: "select-row-cell",
+
+        /** @property */
+        tagName: "td",
+
+        /** @property */
+        events: {
+            "change :checkbox": "onChange",
+            "click": "enterEditMode"
+        },
+
+        /**
+         * Initializer. If the underlying model triggers a `select` event, this cell
+         * will change its checked value according to the event's `selected` value.
+         *
+         * @param {Object} options
+         * @param {Backgrid.Column} options.column
+         * @param {Backbone.Model} options.model
+         */
+        initialize: function (options) {
+            //Backgrid.requireOptions(options, ["model", "column"]);
+
+            this.column = options.column;
+            if (!(this.column instanceof Backgrid.Column)) {
+                this.column = new Backgrid.Column(this.column);
+            }
+
+            this.listenTo(this.model, "backgrid:select", function (model, checked) {
+                this.$el.find(":checkbox").prop("checked", checked).change();
+            });
+
+        },
+
+        /**
+         * Focuses the checkbox.
+         */
+        enterEditMode: function (e) {
+            var $checkbox = this.$el.find(":checkbox").focus();
+            if ($checkbox[0] !== e.target) {
+                $checkbox.prop("checked", !$checkbox.prop("checked")).change();
+            }
+            e.stopPropagation();
+        },
+
+        /**
+         * Unfocuses the checkbox.
+         */
+        exitEditMode: function () {
+            this.$el.find(":checkbox").blur();
+        },
+
+        /**
+         * When the checkbox's value changes, this method will trigger a Backbone
+         * `backgrid:selected` event with a reference of the model and the
+         * checkbox's `checked` value.
+         */
+        onChange: function (e) {
+            this.model.trigger("backgrid:selected", this.model, $(e.target).prop("checked"));
+        },
+
+        /**
+         * Renders a checkbox in a table cell.
+         */
+        render: function () {
+            // work around with trigger event to get current state of model (selected or not)
+            var state = {selected: false};
+            this.$el.empty().append('<input tabindex="-1" type="checkbox" />');
+            this.model.trigger('backgrid:isSelected', this.model, state);
+            if (state.selected) {
+                this.$el.find(':checkbox').prop('checked', 'checked');
+            }
+            this.delegateEvents();
+            return this;
+        }
+    });
+
+    /**
+     * Contains mass-selection logic
+     *  - watches models selection, keeps reference to selected
+     *  - provides mass-selection actions
+     *  - listening to models collection events,
+     *      fills in 'obj' with proper data for
+     *      `backgrid:isSelected` and `backgrid:getSelected`
+     *
+     * @class Oro.Datagrid.Cell.SelectAllHeaderCell
+     * @extends Oro.Datagrid.Cell.SelectRowCell
+     */
+    Oro.Datagrid.Cell.SelectAllHeaderCell = Oro.Datagrid.Cell.SelectRowCell.extend({
+        /** @property */
+        className: "select-all-header-cell",
+
+        /** @property */
+        tagName: "th",
+
+        events: {},
+
+        /**
+         * Initializer.
+         * Subscribers on events listening
+         *
+         * @param {Object} options
+         * @param {Backgrid.Column} options.column
+         * @param {Backbone.Collection} options.collection
+         */
+        initialize: function (options) {
+            //Backgrid.requireOptions(options, ["column", "collection"]);
+
+            this.column = options.column;
+            if (!(this.column instanceof Backgrid.Column)) {
+                this.column = new Backgrid.Column(this.column);
+            }
+
+            this.initialState();
+            this.listenTo(this.collection, {
+                remove: this.removeModel,
+                updateState: this.initialState,
+                'backgrid:selected': this.selectModel,
+                'backgrid:selectAll': this.selectAll,
+                'backgrid:selectAllVisible': this.selectAllVisible,
+
+                'backgrid:isSelected': _.bind(function (model, obj) {
+                    if ($.isPlainObject(obj)) {
+                        obj.selected = this.isSelectedModel(model);
+                    }
+                }, this),
+                'backgrid:getSelected': _.bind(function (obj) {
+                    if ($.isEmptyObject(obj)) {
+                        obj.selected = _.keys(this.selectedModels);
+                        obj.inset = this.inset;
+                    }
+                }, this)
+            });
+        },
+
+        /**
+         * Resets selection to initial conditions
+         *  - clear selected models set
+         *  - reset set type in-set/not-in-set
+         */
+        initialState: function () {
+            this.selectedModels = {};
+            this.inset = true;
+        },
+
+        /**
+         * Checks if passed model have to be marked as selected
+         *
+         * @param {Backbone.Model} model
+         * @returns {boolean}
+         */
+        isSelectedModel: function (model) {
+            return this.inset === _.has(this.selectedModels, model.id || model.cid);
+        },
+
+        /**
+         * Removes model from selected models set
+         *
+         * @param {Backbone.Model} model
+         */
+        removeModel: function (model) {
+            delete this.selectedModels[model.id || model.cid];
+        },
+
+        /**
+         * Adds/removes model to/from selected models set
+         *
+         * @param {Backbone.Model} model
+         * @param {boolean} selected
+         */
+        selectModel: function (model, selected) {
+            if (selected === this.inset) {
+                this.selectedModels[model.id || model.cid] = model;
+            } else {
+                this.removeModel(model);
+            }
+        },
+
+        /**
+         * Performs selection of all possible models:
+         *  - reset to initial state
+         *  - change type of set type as not-inset
+         *  - marks all models in collection as selected
+         *  start to collect models which have to be excluded
+         */
+        selectAll: function () {
+            this.initialState();
+            this.inset = false;
+            this._selectAll();
+        },
+
+        /**
+         * Performs selection of all visible models:
+         *  - if necessary reset to initial state
+         *  - marks all models in collection as selected
+         */
+        selectAllVisible: function () {
+            if (!this.inset) {
+                this.initialState();
+            }
+            this._selectAll();
+        },
+
+        /**
+         * Marks all models in collection as selected
+         *
+         * @private
+         */
+        _selectAll: function () {
+            this.collection.each(function (model) {
+                model.trigger("backgrid:select", model, true);
+            });
+        },
+
+        /**
+         *
+         *
+         * @returns {Oro.Datagrid.Cell.SelectAllHeaderCell}
+         */
+        render: function () {
+            /*jshint multistr:true */
+            /*jslint es5: true */
+            /* render method will detend on options or will be empty */
+            this.$el.empty().append('<div class="btn-group">\
+                <button type="button" class="btn btn-default" data-select-all>All</button>\
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">\
+                    <span class="caret"></span>\
+                </button>\
+                <ul class="dropdown-menu">\
+                    <li><a href="#" data-select-all-visible>All visiable</a></li>\
+                </ul>\
+            </div>');
+            this.$el.find('[data-select-all]').on('click', _.bind(function (e) {
+                this.collection.trigger('backgrid:selectAll');
+                e.preventDefault();
+            }, this));
+            this.$el.find('[data-select-all-visible]').on('click', _.bind(function (e) {
+                this.collection.trigger('backgrid:selectAllVisible');
+                e.preventDefault();
+            }, this));
+            /* */
+            return this;
+        }
+    });
+}));
