1099da397407a0cff81bdb12f24ec28f80c4921c
Filips Alpe <filips@akeneo.com>
2013-08-04 22:42:17 +0200

    Change 2012 to 2013

--- DataFixtures/ORM/LoadCategoryData.php
+++ DataFixtures/ORM/LoadCategoryData.php
@@ -14,7 +14,7 @@ use Pim\Bundle\ProductBundle\Entity\Category;
  * Load data for category tree
  *
  * @author    Romain Monceau <romain@akeneo.com>
- * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  */
--- DataFixtures/ORM/LoadProductData.php
+++ DataFixtures/ORM/LoadProductData.php
@@ -21,7 +21,7 @@ use Pim\Bundle\ProductBundle\Entity\ProductPrice;
 * Execute with "php app/console doctrine:fixtures:load"
 *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
- * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *
 */
