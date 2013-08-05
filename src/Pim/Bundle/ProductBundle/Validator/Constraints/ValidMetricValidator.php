cbeda7ba1dd77ef9e50b02c75709eb59a81534a5
Filips Alpe <filips@akeneo.com>
2013-08-05 18:45:26 +0200

    Change ValidMetricAttribute constraint to ValidMetric, some clean-up

--- src/Pim/Bundle/ProductBundle/Validator/Constraints/ValidMetricValidator.php
+++ src/Pim/Bundle/ProductBundle/Validator/Constraints/ValidMetricValidator.php
@@ -12,7 +12,7 @@ use Symfony\Component\Validator\ConstraintValidator;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  */
-class ValidMetricAttributeValidator extends ConstraintValidator
+class ValidMetricValidator extends ConstraintValidator
 {
     /**
      * @var array $measures
