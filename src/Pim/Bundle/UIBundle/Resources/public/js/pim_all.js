50aa5a1dc0184df036c639a17e48abf31819e682
Filips Alpe <filips@akeneo.com>
2013-08-07 15:46:59 +0200

    Remove some javascript from templates

--- src/Pim/Bundle/UIBundle/Resources/public/js/pim_all.js
+++ src/Pim/Bundle/UIBundle/Resources/public/js/pim_all.js
@@ -1,53 +1,16 @@
 $(function() {
-    // Disable the oro scrollable container
-    $('.scrollable-container').removeClass('scrollable-container').css('overflow', 'visible');
-
-    // Prevent UniformJS from breaking our stuff
-    $(document).uniform.restore();
-
-    // Apply Select2
-    $('select').select2({ allowClear: true });
-
-    // Apply Select2 multiselect
-    $('input.multiselect').select2({ tags: $(this).val() });
-
-    // Toogle accordion icon
-    $('.accordion').on('show hide', function(e) {
-        $(e.target).siblings('.accordion-heading').find('.accordion-toggle i').toggleClass('fa-icon-collapse-alt fa-icon-expand-alt');
-    });
-
-    // Activate the form tab specified in the url
-    if (/^#[a-zA-Z0-9-_]+$/i.test(location.hash)) {
-        var activeTab = $('[href=' + location.hash + ']');
-        if (activeTab) {
-            activeTab.tab('show');
-        }
-    }
-
-    // Remove bap 'Loading Application' progressbar and partially fix page title regression issue
-    document.title = $('#page-title').text();
-    if ($('#progressbar').is(':visible')) {
-        $('#progressbar').hide();
-        $('#page').show();
-    }
-
-    $('a[data-dialog]').on('click', function(event) {
-        event.preventDefault();
-        $el = $(this);
-        var message = $el.data('message');
-        var title = $el.data('title');
-        if ($el.data('dialog') ==  'confirm') {
-            var doAction = function() {
-                $el.off('click');
-                var $form = $('<form>', { method: 'POST', action: $el.attr('href')});
-                $('<input>', { type: 'hidden', name: '_method', value: $el.data('method')}).appendTo($form);
-                $form.appendTo('body').submit();
-            };
-
-            PimDialog.confirm(message, title, doAction);
-        } else {
-            PimDialog.alert(message, title);
-        }
-    });
+    Oro.Navigation.prototype.bind('route', init);
 });
 
+function init() {
+    // Place code that we need to run on every page load here
+}
+
+$(function() {
+    // Do global event binding here
+
+    // Toogle accordion icon
+    $(document).on('show hide', '.accordion', function(e) {
+        $(e.target).siblings('.accordion-heading').find('.accordion-toggle i').toggleClass('fa-icon-collapse-alt fa-icon-expand-alt');
+    });
+});
--- src/Pim/Bundle/UIBundle/Resources/public/js/pim_refactor.js
+++ src/Pim/Bundle/UIBundle/Resources/public/js/pim_refactor.js
@@ -1,6 +1,51 @@
-<script type="text/javascript">
-$(function() {
-    'use strict';
+// Activate the form tab specified in the url
+if (/^#[a-zA-Z0-9-_]+$/i.test(location.hash)) {
+    var activeTab = $('[href=' + location.hash + ']');
+    if (activeTab) {
+        activeTab.tab('show');
+    }
+}
+
+// Disable the oro scrollable container
+$('.scrollable-container').removeClass('scrollable-container').css('overflow', 'visible');
+
+// Prevent UniformJS from breaking our stuff
+$(document).uniform.restore();
+
+// Apply Select2
+$('select').select2({ allowClear: true });
+
+// Apply Select2 multiselect
+$('input.multiselect').select2({ tags: $(this).val() });
+
+// Remove bap 'Loading Application' progressbar and partially fix page title regression issue
+document.title = $('#page-title').text();
+if ($('#progressbar').is(':visible')) {
+    $('#progressbar').hide();
+    $('#page').show();
+}
+
+// DELETE request for delete buttons
+$(document).on('click', 'a[data-dialog]', function(event) {
+    event.preventDefault();
+    $el = $(this);
+    var message = $el.data('message');
+    var title = $el.data('title');
+    if ($el.data('dialog') ==  'confirm') {
+        var doAction = function() {
+            $el.off('click');
+            var $form = $('<form>', { method: 'POST', action: $el.attr('href')});
+            $('<input>', { type: 'hidden', name: '_method', value: $el.data('method')}).appendTo($form);
+            $form.appendTo('body').submit();
+        };
+
+        PimDialog.confirm(message, title, doAction);
+    } else {
+        PimDialog.alert(message, title);
+    }
+});
+
+/* Product edit form js */
 
     var formAction = "{{ path('pim_product_product_edit', { id: form.vars.value.id, dataLocale: dataLocale }) }}";
 
@@ -70,5 +115,24 @@ $(function() {
     });
 
     $('.wysihtml5-toolbar a').attr('tabIndex', -1);
-});
-</script>
+
+/* End product edit form js */
+
+/* Available attributes form js */
+    $('#{{ form.attributes.vars.id }}').select2('destroy');
+
+    $('#{{ form.attributes.vars.id }}').pimMultiselect({
+        title: "{{ 'Add attributes'|trans }}",
+        placeholder: "{{ 'Search'|trans }}",
+        emptyText: "{{ 'There are no more attributes to add'|trans }}",
+        appendTo: '#attribute-buttons',
+        buttons: {
+            "{{ 'Add'|trans }}": function() {
+                $('#{{ form.attributes.vars.id }}').multiselect('close');
+                if ($('#{{ form.attributes.vars.id }}').val() !== null) {
+                    $('#{{ form.vars.id }}').submit();
+                }
+            }
+        }
+    });
+/* End vailable attributes form js */
