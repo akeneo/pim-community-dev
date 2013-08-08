5b96c6324e3addc958fe596a5d362102ba8f2db3
Gildas Quéméner <gildas.quemener@gmail.com>
2013-08-08 18:11:22 +0200

    Renamed attribute requirement table

--- src/Pim/Bundle/ProductBundle/Entity/AttributeRequirement.php
+++ src/Pim/Bundle/ProductBundle/Entity/AttributeRequirement.php
@@ -13,6 +13,7 @@ use Pim\Bundle\ConfigBundle\Entity\Channel;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @ORM\Entity
+ * @ORM\Table(name="pim_attribute_requirement")
  */
 class AttributeRequirement
 {
