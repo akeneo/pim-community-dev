c24ee6b1b98fdaad0ab3faf9413662f8f371c43d
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-08-09 15:17:12 +0300

    BAP-1350: Functional tests for Email Transactions  - added controllers tests  - refactored tags and user tests

---
+++ Bundle/UserBundle/Tests/Functional/ControllersTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Tests\Functional;
+
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\TestFrameworkBundle\Test\ToolsAPI;
+use Oro\Bundle\TestFrameworkBundle\Test\Client;
+
+/**
+ * @outputBuffering enabled
+ * @db_isolation
+ * @db_reindex
+ */
+class ControllersTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient(array(), ToolsAPI::generateBasicHeader());
+    }
+
+    public function testIndex()
+    {
+        $this->client->request('GET', $this->client->generate('oro_user_index'));
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+    }
+
+    public function testCreate()
+    {
+        $crawler = $this->client->request('GET', $this->client->generate('oro_user_create'));
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form['oro_user_user_form[enabled]'] = 1;
+        $form['oro_user_user_form[username]'] = 'testUser1';
+        $form['oro_user_user_form[plainPassword][first]'] = 'password';
+        $form['oro_user_user_form[plainPassword][second]'] = 'password';
+        $form['oro_user_user_form[firstName]'] = 'First Name';
+        $form['oro_user_user_form[lastName]'] = 'Last Name';
+        $form['oro_user_user_form[birthday]'] = '7/1/13';
+        $form['oro_user_user_form[email]'] = 'test@test.com';
+        //$form['oro_user_user_form[tags][owner]'] = 'tags1';
+        //$form['oro_user_user_form[tags][all]'] = null;
+        $form['oro_user_user_form[groups][1]'] = 3;
+        $form['oro_user_user_form[rolesCollection][2]'] = 4;
+        $form['oro_user_user_form[values][company][varchar]'] = 'company';
+        $form['oro_user_user_form[values][gender][option]'] = 6;
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->submit($form);
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+        $this->assertContains("User successfully saved", $crawler->html());
+    }
+
+    public function testUpdate()
+    {
+        $this->client->request(
+            'GET',
+            $this->client->generate('oro_user_index', array('_format' =>'json')) .
+                    '?users[_filter][username][value]=testUser1',
+            array(
+                'users[_pager][_page]' => 1,
+                'users[_pager][_per_page]' => 10,
+                'users[_sort_by][username]' => 'ASC',
+                'users[_filter][username][type]' => '',
+            )
+        );
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200);
+
+        $result = ToolsAPI::jsonToArray($result->getContent());
+        $result = reset($result['data']);
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->client->generate('oro_user_update', array('id' => $result['id']))
+        );
+        /** @var Form $form */
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form['oro_user_user_form[enabled]'] = 1;
+        $form['oro_user_user_form[username]'] = 'testUser1';
+        $form['oro_user_user_form[firstName]'] = 'First Name';
+        $form['oro_user_user_form[lastName]'] = 'Last Name';
+        $form['oro_user_user_form[birthday]'] = '1/1/13';
+        $form['oro_user_user_form[email]'] = 'test@test.com';
+        $form['oro_user_user_form[groups][1]'] = 3;
+        $form['oro_user_user_form[rolesCollection][2]'] = 4;
+        $form['oro_user_user_form[values][company][varchar]'] = 'company_update';
+        $form['oro_user_user_form[values][gender][option]'] = 6;
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->submit($form);
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+        $this->assertContains("User successfully saved", $crawler->html());
+    }
+}
