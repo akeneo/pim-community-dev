af67882c1417506ef30e51406483249ae042273d
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-08-12 16:05:52 +0300

    Implemented BAP-1363: Develop client data update over AJAX (JS side)

---
+++ Bundle/SyncBundle/Resources/public/js/sync/ajax.js
@@ -0,0 +1,199 @@
+/* jshint browser:true */
+(function (factory) {
+    "use strict";
+    /* global define, jQuery, _, Oro */
+    if (typeof define === 'function' && define.amd) {
+        define(['JSON', 'jQuery', '_', 'OroSynchronizer'], factory);
+    } else {
+        factory(JSON, jQuery, _, Oro.Synchronizer);
+    }
+}(function (JSON, $, _, Synchronizer) {
+    "use strict";
+
+    var defaultOptions = {
+            waitUpdate: 5000,
+            waitSubscription: 500,
+            attemptsLimit: 10
+        },
+
+        /**
+         * Invokes doFetchUpdates function with a delay (which is configured via options)
+         */
+        fetchUpdates = function () {
+            this.updater = _.delay(_.bind(doFetchUpdates, this), this.options.waitUpdate);
+        },
+
+        /**
+         * Collects all subscribed channels and makes request for updates.
+         * If there's no subscribed channels for the moment, then just invoke fetchUpdates(),
+         * to be executed again after a while.
+         */
+        doFetchUpdates = function () {
+            if (_.isEmpty(this.channels) ) {
+                fetchUpdates.call(this);
+                return;
+            }
+            var channels = this.channels,
+                payload = _.chain(channels)
+                    .filter(function (obj) {
+                        return obj.token !== '' && obj.token !== '-';
+                    })
+                    .map(function (obj) {
+                        return obj.token;
+                    })
+                    .value();
+            $.ajax({
+                url: this.options.url,
+                type: "POST",
+                data: {
+                    action: 'fetchUpdates',
+                    payload: JSON.stringify(payload)
+                },
+                success: function (payload) {
+                    _.each(payload, function (obj) {
+                        if (!obj.channel || !obj.attributes) {
+                            return;
+                        }
+                        var channel = channels[obj.channel];
+                        if (channel) {
+                            _.each(channel.callbacks, function(callback){
+                                callback(obj.attributes);
+                            });
+                        }
+                    });
+                },
+                complete: _.bind(fetchUpdates, this)
+            });
+        },
+
+        /**
+         * Invokes doSubscribe function but with a delay (which is configured via options)
+         *
+         * @param {number=} attempt number of attempts which service have to do after error occurred
+         */
+        subscribe = function (attempt) {
+            if (!this.subscriber) {
+                attempt = attempt || 0;
+                this.subscriber = _.delay(_.bind(doSubscribe, this), this.options.waitSubscription, attempt);
+            }
+        },
+
+        /**
+         * Collect all channels which don't have tokens and makes subscribe request
+         *
+         * @param {number} attempt number of attempts which service have to do after error occurred
+         */
+        doSubscribe = function (attempt) {
+            var channels = this.channels,
+                payload = _.chain(channels)
+                    .filter(function (obj) {
+                        return obj.token === '' ? Boolean(obj.token = '-') : false;
+                    })
+                    .map(function (obj) {
+                        return obj.channel;
+                    })
+                    .value();
+            delete this.subscriber;
+            $.ajax({
+                url: this.options.url,
+                type: "POST",
+                data: {
+                    action: 'subscribe',
+                    payload: JSON.stringify(payload)
+                },
+                success: function (payload) {
+                    _.each(payload, function (obj) {
+                        if (!obj.channel) {
+                            return;
+                        }
+                        if (obj.token) {
+                            channels[obj.channel].token = obj.token;
+                        } else {
+                            // assumed user have no permission to listening to this channel
+                            delete channels[obj.channel];
+                        }
+                    });
+                },
+                error: function () {
+                    // on error remove all tokens for sent channels
+                    _.each(payload, function (channel) {
+                        channels[channel].token = '';
+                    });
+                },
+                complete: _.bind(function (xhr, status) {
+                    attempt = status === 'error' ? attempt + 1 : 0;
+                    if (attempt <= this.options.attemptsLimit && (attempt || _.some(channels, function (obj) {
+                        return obj.token === '';
+                    }))) {
+                        subscribe.call(this, attempt);
+                    }
+                }, this)
+            });
+        },
+
+        /**
+         * Synchronizer service build over AJAX
+         *
+         * @constructor
+         * @param {Object} options to configure service
+         * @param {string} options.url is required,
+         * @param {number=} options.waitUpdate default is 5000 (5s)
+         * @param {number=} options.waitSubscription is time before actual subscribe request after first
+         *      subscribe call, default is 500 (1/2s). During this time, service waits for more
+         *      subscribers and after it makes a request to subscribe them all ta ones
+         * @param {number=} options.attemptsLimit quantity of attempts before stop trying
+         *      to subscribe after an error response received, default is 10
+         *
+         * @var {Function} ajax protected shortcut for Synchronizer.Ajax
+         */
+        ajax = Synchronizer.Ajax = function (options) {
+            this.options = _.extend({}, defaultOptions, options);
+            this.channels = {};
+            fetchUpdates.call(this);
+        };
+
+    ajax.prototype = {
+        /**
+         * Subscribes update callback function on channel
+         *
+         * @param {string} channel is an URL which broadcasts updates
+         * @param {function(Object)} callback is a function which accepts JSON
+         *      with attributes' values and performs update
+         */
+        subscribe: function (channel, callback) {
+            var obj = this.channels[channel];
+            if (!obj) {
+                this.channels[channel] = {
+                    channel: channel,
+                    callbacks: [callback],
+                    token: ''
+                };
+                subscribe.call(this);
+            } else {
+                obj.callbacks.push(callback);
+            }
+        },
+        /**
+         * Removes subscription of update callback function for a channel
+         *
+         * @param {string} channel is an URL which broadcasts updates
+         * @param {function(Object)=} callback an optional parameter,
+         *      if was no function corresponded then removes all callbacks for a channel
+         */
+        unsubscribe: function (channel, callback) {
+            var obj = this.channels[channel];
+            if (obj) {
+                if (callback) {
+                    obj.callbacks = _.without(obj.callbacks, callback);
+                }
+                if (!obj.callbacks.length || !callback) {
+                    delete this.channels[channel];
+                }
+            }
+        }
+    };
+
+    return Synchronizer.Ajax;
+}));
+
+
