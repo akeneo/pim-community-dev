256397bcdc3b8fdc3b4b7d730978c0969cf094e3
Gildas Quéméner <gildas.quemener@gmail.com>
2013-08-14 10:07:09 +0200

    PIM-783 Renamed product creation processor test class

--- src/Pim/Bundle/ImportExportBundle/Tests/Unit/Processor/ValidProductCreationProcessorTest.php
+++ src/Pim/Bundle/ImportExportBundle/Tests/Unit/Processor/ValidProductCreationProcessorTest.php
@@ -11,7 +11,7 @@ use Pim\Bundle\ImportExportBundle\Processor\ValidProductCreationProcessor;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ValidProductCreatorProcessorTest extends \PHPUnit_Framework_TestCase
+class ValidProductCreationProcessorTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
