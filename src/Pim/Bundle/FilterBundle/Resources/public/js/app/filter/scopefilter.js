f11b4257d41d714652087138b032f8cbf0b26c82
Filips Alpe <filips@akeneo.com>
2013-08-21 16:28:19 +0200

    Remove unused class

--- src/Pim/Bundle/FilterBundle/Resources/public/js/app/filter/scopefilter.js
+++ src/Pim/Bundle/FilterBundle/Resources/public/js/app/filter/scopefilter.js
@@ -28,7 +28,7 @@ Pim.Filter.ScopeFilter = Oro.Filter.SelectFilter.extend({
      * @see Oro.Filter.SelectFilter
      */
     template: _.template(
-        '<div class="btn filter-select filter-criteria-selector pull-right scope-filter">' +
+        '<div class="btn filter-select filter-criteria-selector scope-filter">' +
             '<i class="fa-icon-eye-open" title="<%= label %>"></i>: ' +
             '<select>' +
                 '<% _.each(options, function (hint, value) { %>' +
