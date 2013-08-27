99a6540098992923def280da4e25f0c8a95be559
Gildas Quéméner <gildas.quemener@gmail.com>
2013-08-27 18:31:12 +0200

    PIM-1002 Fixed the data submitted by the valid product creation processor

--- src/Pim/Bundle/ImportExportBundle/Processor/ValidProductCreationProcessor.php
+++ src/Pim/Bundle/ImportExportBundle/Processor/ValidProductCreationProcessor.php
@@ -4,15 +4,17 @@ namespace Pim\Bundle\ImportExportBundle\Processor;
 
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\FormFactoryInterface;
-use Doctrine\ORM\EntityManager;
 use Pim\Bundle\BatchBundle\Item\ItemProcessorInterface;
 use Pim\Bundle\ImportExportBundle\AbstractConfigurableStepElement;
 use Pim\Bundle\ImportExportBundle\Exception\InvalidObjectException;
 use Pim\Bundle\ImportExportBundle\Validator\Constraints\Channel;
 use Pim\Bundle\ProductBundle\Entity\Product;
-use Pim\Bundle\ProductBundle\Entity\ProductAttribute;
 use Pim\Bundle\ProductBundle\Manager\ProductManager;
 use Pim\Bundle\ProductBundle\Manager\ChannelManager;
+use Pim\Bundle\ImportExportBundle\Converter\ProductEnabledConverter;
+use Pim\Bundle\ImportExportBundle\Converter\ProductFamilyConverter;
+use Pim\Bundle\ImportExportBundle\Converter\ProductValueConverter;
+use Pim\Bundle\ImportExportBundle\Converter\ProductCategoriesConverter;
 
 /**
  * Product form processor
@@ -24,14 +26,12 @@ use Pim\Bundle\ProductBundle\Manager\ChannelManager;
  */
 class ValidProductCreationProcessor extends AbstractConfigurableStepElement implements ItemProcessorInterface
 {
-    protected $entityManager;
     protected $formFactory;
     protected $productManager;
     protected $channelManager;
 
     protected $enabled             = true;
     protected $categoriesColumn    = 'categories';
-    protected $categoriesDelimiter = ',';
     protected $familyColumn        = 'family';
 
     /**
@@ -44,12 +44,10 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
     private $attributes = array();
 
     public function __construct(
-        EntityManager $entityManager,
         FormFactoryInterface $formFactory,
         ProductManager $productManager,
         ChannelManager $channelManager
     ) {
-        $this->entityManager  = $entityManager;
         $this->formFactory    = $formFactory;
         $this->productManager = $productManager;
         $this->channelManager = $channelManager;
@@ -86,7 +84,7 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
     }
 
     /**
-     * Get the categories delimiter
+     * Get the categories column
      *
      * @return string
      */
@@ -106,7 +104,7 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
     }
 
     /**
-     * Get the family delimiter
+     * Get the family column
      *
      * @return string
      */
@@ -116,27 +114,8 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
     }
 
     /**
-     * Set the categories delimiter
-     *
-     * @param string $categoriesDelimiter
-     */
-    public function setCategoriesDelimiter($categoriesDelimiter)
-    {
-        $this->categoriesDelimiter = $categoriesDelimiter;
-    }
-
-    /**
-     * Get the categories delimiter
-     *
-     * @return string
-     */
-    public function getCategoriesDelimiter()
-    {
-        return $this->categoriesDelimiter;
-    }
-
-    /**
      * Set channel
+     *
      * @param string $channel
      */
     public function setChannel($channel)
@@ -157,6 +136,7 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
      * Goal is to transform an array like this:
      * array(
      *     'sku'        => 'sku-001',
+     *     'family'     => 'vehicle'
      *     'name-en_US' => 'car',
      *     'name-fr_FR' => 'voiture,
      *     'categories' => 'cat_1,cat_2,cat3',
@@ -164,19 +144,11 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
      *
      * into this:
      * array(
-     *    'enabled' => '1',
-     *    'values'  => array(
-     *        'sku' => array(
-     *             'varchar' => 'sku-001',
-     *         ),
-     *         'name_en_US' => array(
-     *             'varchar' => 'car'
-     *         ),
-     *         'name_fr_FR' => array(
-     *             'varchar' => 'voiture'
-     *         ),
-     *     ),
-     *     'categories' => array(1, 2, 3)
+     *    '[enabled]'    => true,
+     *    '[family']'    => 'vehicle'
+     *    'name-en_US'   => 'car',
+     *    'name-fr_FR'   => 'voiture,
+     *    '[categories]' => 'cat_1,cat_2,cat3',
      * )
      *
      * and to bind it to the ProductType.
@@ -189,12 +161,8 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
      */
     public function process($item)
     {
-        foreach (array_keys($item) as $code) {
-            $attributes[$code] = $this->getAttribute($code);
-        }
-
-        $product = $this->getProduct($attributes, $item);
-        $form    = $this->createAndSubmitForm($product, $attributes, $item);
+        $product = $this->getProduct($item);
+        $form    = $this->createAndSubmitForm($product, $item);
 
         if (!$form->isValid()) {
             throw new InvalidObjectException($form);
@@ -213,7 +181,6 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
                 'type' => 'checkbox',
             ),
             'categoriesColumn'    => array(),
-            'categoriesDelimiter' => array(),
             'familyColumn'        => array(),
             'channel'             => array(
                 'type' => 'choice',
@@ -233,77 +200,14 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
      *
      * @return Product
      */
-    private function getProduct(array $attributes, array $item)
+    private function getProduct(array $item)
     {
         $product = $this->productManager->findByIdentifier(reset($item));
         if (!$product) {
             $product = $this->productManager->createFlexible();
-            foreach ($attributes as $code => $attribute) {
-                if (!$attribute || false !== $product->{'get'.ucfirst($code)}()) {
-                    continue;
-                }
-
-                if ($attribute->getScopable()) {
-                    $product->setScope($this->channel);
-                }
-
-                if ($attribute->getTranslatable()) {
-                    list($code, $locale) = explode('-', $code);
-                    $product->setLocale($locale);
-                }
-
-                $product->{'set'.ucfirst($code)}(null);
-            }
         }
 
         return $product;
-    }
-
-    private function getValue(ProductAttribute $attribute, $value)
-    {
-        switch ($attribute->getBackendType()) {
-            case 'prices':
-                foreach (explode(',', $value) as $price) {
-                    list($data, $currency) = explode(' ', $price);
-                    $prices[] = array(
-                        'data'     => $data,
-                        'currency' => $currency,
-                    );
-                }
-
-                return array(
-                    'prices' => $prices
-                );
-            case 'date':
-                $date = new \DateTime($value);
-
-                return array($attribute->getBackendType() => $date->format('m/d/Y'));
-            case 'option':
-                if ($option = $this->getOption($value)) {
-                    return array($attribute->getBackendType() => $option->getId());
-                }
-
-                return array();
-            case 'options':
-                $options = array();
-                foreach (explode(',', $value) as $val) {
-                    if ($option = $this->getOption($val)) {
-                        $options[] = $option->getId();
-                    }
-                }
-
-                return array($attribute->getBackendType() => $options);
-            default:
-                return array($attribute->getBackendType() => $value);
-        }
-    }
-
-    private function getAttributeCode(array $attributes, $code)
-    {
-        $attribute = $attributes[$code];
-        $suffix = $attribute->getScopable() ? sprintf('_%s', $this->channel) : '';
-
-        return str_replace('-', '_', $code).$suffix;
     }
 
     /**
@@ -314,21 +218,8 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
      *
      * @return FormInterface
      */
-    private function createAndSubmitForm(Product $product, array $attributes, array $item)
+    private function createAndSubmitForm(Product $product, array $item)
     {
-        $values     = array();
-        $categories = array();
-        $family     = null;
-        foreach ($item as $code => $value) {
-            if (isset($attributes[$code])) {
-                $values[$this->getAttributeCode($attributes, $code)] = $this->getValue($attributes[$code], $value);
-            } elseif ($code === $this->categoriesColumn) {
-                $categories = $this->getCategoryIds($value);
-            } elseif ($code === $this->familyColumn) {
-                $family = $this->getFamilyId($value);
-            }
-        }
-
         $form = $this->formFactory->create(
             'pim_product',
             $product,
@@ -338,71 +229,21 @@ class ValidProductCreationProcessor extends AbstractConfigurableStepElement impl
             )
         );
 
-        $data = array(
-            'enabled'    => (string) (int) $this->enabled,
-            'values'     => $values,
-            'categories' => $categories,
-            'family'     => $family,
-        );
-        $form->submit($data);
+        $item[ProductEnabledConverter::ENABLED_KEY] = $this->enabled;
+        $item[ProductValueConverter::SCOPE_KEY]     = $this->channel;
+
+        if (array_key_exists($this->familyColumn, $item)) {
+            $item[ProductFamilyConverter::FAMILY_KEY] = $item[$this->familyColumn];
+            unset($item[$this->familyColumn]);
+        }
+
+        if (array_key_exists($this->categoriesColumn, $item)) {
+            $item[ProductCategoriesConverter::CATEGORIES_KEY] = $item[$this->categoriesColumn];
+            unset($item[$this->categoriesColumn]);
+        }
+
+        $form->submit($item);
 
         return $form;
-    }
-
-    private function getCategoryIds($codes)
-    {
-        $ids = array();
-        foreach (explode($this->categoriesDelimiter, $codes) as $code) {
-            if ($category = $this->getCategory($code)) {
-                $ids[] = $category->getId();
-            }
-        }
-
-        return $ids;
-    }
-
-    public function getFamilyId($code)
-    {
-        if ($family = $this->getFamily($code)) {
-            return $family->getId();
-        }
-    }
-
-    private function getFamily($code)
-    {
-        return $this->entityManager
-            ->getRepository('PimProductBundle:Family')
-            ->findOneBy(array('code' => $code));
-    }
-
-    private function getAttribute($code)
-    {
-        $parts = explode('-', $code);
-
-        if (!array_key_exists($parts[0], $this->attributes)) {
-            $this->attributes[$parts[0]] = $this->entityManager
-                ->getRepository('PimProductBundle:ProductAttribute')
-                ->findOneBy(array('code' => $parts[0]));
-        }
-
-        return $this->attributes[$parts[0]];
-    }
-
-    private function getCategory($code)
-    {
-        if (!array_key_exists($code, $this->categories)) {
-            $this->categories[$code] = $this->entityManager
-                ->getRepository('PimProductBundle:Category')
-                ->findOneBy(array('code' => $code));
-        }
-
-        return $this->categories[$code];
-    }
-
-    private function getOption($code)
-    {
-        return $this->entityManager
-            ->getRepository('PimProductBundle:AttributeOption')
-            ->findOneBy(array('code' => $code));
     }
 }
