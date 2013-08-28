15116c3f58437a0a8d0b91c142e632f66a544b91
Gildas Quéméner <gildas.quemener@gmail.com>
2013-08-28 11:37:21 +0200

    PIM-1002 Provided an option to override the product data transformer

--- src/Pim/Bundle/ProductBundle/DependencyInjection/Configuration.php
+++ src/Pim/Bundle/ProductBundle/DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->booleanNode('record_mails')->defaultFalse()->end()
+                ->scalarNode('imported_product_data_transformer')->defaultNull()->end()
             ->end()
         ->end();
 
