20543be84a5db41873681505565f5e32ebab7d8b
Filips Alpe <filips@akeneo.com>
2013-08-28 16:32:24 +0200

    Add allowUpload property to CsvReader to specify whether the job may be run using an uploaded file

--- src/Pim/Bundle/ImportExportBundle/Reader/CsvReader.php
+++ src/Pim/Bundle/ImportExportBundle/Reader/CsvReader.php
@@ -39,46 +39,126 @@ class CsvReader extends AbstractConfigurableStepElement implements ItemReaderInt
      */
     protected $escape = '\\';
 
+    /**
+     * @var boolean
+     *
+     * @Assert\Type(type="bool")
+     */
+    protected $allowUpload = false;
+
+    /**
+     * @var SplFileObject
+     */
     private $csv;
 
+    /**
+     * Set file path
+     * @param string $filePath
+     *
+     * @return CsvReader
+     */
     public function setFilePath($filePath)
     {
         $this->filePath = $filePath;
+
+        return $this;
     }
 
+    /**
+     * Get file path
+     * @return string $filePath
+     */
     public function getFilePath()
     {
         return $this->filePath;
     }
 
+    /**
+     * Set delimiter
+     * @param string $delimiter
+     *
+     * @return CsvReader
+     */
     public function setDelimiter($delimiter)
     {
         $this->delimiter = $delimiter;
+
+        return $this;
     }
 
+    /**
+     * Get delimiter
+     * @return string $delimiter
+     */
     public function getDelimiter()
     {
         return $this->delimiter;
     }
 
+    /**
+     * Set enclosure
+     * @param string $enclosure
+     *
+     * @return CsvReader
+     */
     public function setEnclosure($enclosure)
     {
         $this->enclosure = $enclosure;
+
+        return $this;
     }
 
+    /**
+     * Get enclosure
+     * @return string $enclosure
+     */
     public function getEnclosure()
     {
         return $this->enclosure;
     }
 
+    /**
+     * Set escape
+     * @param string $escape
+     *
+     * @return CsvReader
+     */
     public function setEscape($escape)
     {
         $this->escape = $escape;
+
+        return $this;
     }
 
+    /**
+     * Get escape
+     * @return string $escape
+     */
     public function getEscape()
     {
         return $this->escape;
+    }
+
+    /**
+     * Set the allowUpload property
+     * @param boolean $allowUpload
+     *
+     * @return CsvReader
+     */
+    public function setAllowUpload($allowUpload)
+    {
+        $this->allowUpload = $allowUpload;
+
+        return $this;
+    }
+
+    /**
+     * Get the allowUpload property
+     * @return boolean $allowUpload
+     */
+    public function getAllowUpload()
+    {
+        return $this->allowUpload;
     }
 
     /**
@@ -128,10 +208,13 @@ class CsvReader extends AbstractConfigurableStepElement implements ItemReaderInt
     public function getConfigurationFields()
     {
         return array(
-            'filePath'   => array(),
-            'delimiter'  => array(),
-            'enclosure'  => array(),
-            'escape'     => array(),
+            'filePath'    => array(),
+            'allowUpload' => array(
+                'type' => 'checkbox',
+            ),
+            'delimiter'   => array(),
+            'enclosure'   => array(),
+            'escape'      => array(),
         );
     }
 }
