7ea92f20345cf59232eebde4b0e565de12242db2
Antoine Guigan <antoine@akeneo.com>
2013-08-29 09:55:52 +0200

    Fixed coding standards

--- src/Pim/Bundle/ProductBundle/Form/Subscriber/ChannelSubscriber.php
+++ src/Pim/Bundle/ProductBundle/Form/Subscriber/ChannelSubscriber.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Pim\Bundle\ProductBundle\Form\Subscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -9,30 +10,40 @@ use Symfony\Component\Form\FormEvent;
  * Form subscriber for Channel
  * 
  * Disables the code field in update mode
+ * 
  * @author Antoine Guigan <antoine@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 class ChannelSubscriber implements EventSubscriberInterface
 {
     public static function getSubscribedEvents()
     {
         return array(
-            FormEvents::POST_SET_DATA=>'disableCode'  
+            FormEvents::POST_SET_DATA => 'addCodeField'
         );
     }
+    
     /**
      * Adds the code field to the form
      * 
      * @param \Symfony\Component\Form\FormEvent $event
      * @return type
      */
-    public function disableCode(FormEvent $event)
+    public function addCodeField(FormEvent $event)
     {
         $channel = $event->getData();
-        if (null === $channel) return;
+        if (null === $channel) {
+            return;
+        }
         
         $form = $event->getForm();
-        $form->add('code', 'text', array(
-            'disabled' => (bool)$channel->getId()
-        ));
+        $form->add(
+            'code',
+            'text',
+            array(
+                    'disabled' => (bool)$channel->getId()
+                )
+        );
     }
 }
