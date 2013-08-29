78d9bf42c7aec2227bbb243b46464555d8cd13b9
Romain Monceau <romain@akeneo.com>
2013-08-29 16:21:48 +0200

    PIM-897 : Remove useless use statement

--- src/Pim/Bundle/ProductBundle/Command/CompletenessCalculatorCommand.php
+++ src/Pim/Bundle/ProductBundle/Command/CompletenessCalculatorCommand.php
@@ -4,7 +4,6 @@ namespace Pim\Bundle\ProductBundle\Command;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 /**
