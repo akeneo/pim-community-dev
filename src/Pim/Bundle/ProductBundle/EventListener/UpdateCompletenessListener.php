afeb4d13d46b9572107695c62a306db739b3aac3
Nicolas Dupont <nicolas@akeneo.com>
2013-08-29 10:22:32 +0200

    PHPDOC : fix license to OSL

--- src/Pim/Bundle/ProductBundle/EventListener/UpdateCompletenessListener.php
+++ src/Pim/Bundle/ProductBundle/EventListener/UpdateCompletenessListener.php
@@ -19,7 +19,7 @@ use Pim\Bundle\ProductBundle\Entity\PendingCompleteness;
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/MIT MIT
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class UpdateCompletenessListener implements EventSubscriber
 {
