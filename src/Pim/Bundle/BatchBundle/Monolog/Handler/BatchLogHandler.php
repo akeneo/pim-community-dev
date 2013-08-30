f46b1e5a6c011638b2c33d80ca5c1b9c359f3bc3
Gildas Quéméner <gildas.quemener@gmail.com>
2013-08-30 11:13:19 +0200

    Docuemented the BatchLogHandler class

--- src/Pim/Bundle/BatchBundle/Monolog/Handler/BatchLogHandler.php
+++ src/Pim/Bundle/BatchBundle/Monolog/Handler/BatchLogHandler.php
@@ -13,9 +13,19 @@ use Monolog\Handler\StreamHandler;
  */
 class BatchLogHandler extends StreamHandler
 {
-
+    /**
+     * @var string $filename
+     */
     protected $filename;
 
+    /**
+     * @var string $logDir
+     */
+    protected $logDir;
+
+    /**
+     * @param string $logDir
+     */
     public function __construct($logDir)
     {
         if (!is_dir($logDir)) {
@@ -28,16 +38,31 @@ class BatchLogHandler extends StreamHandler
         parent::__construct($this->getRealPath($this->filename));
     }
 
+    /**
+     * Get the filename of the log file
+     */
     public function getFilename()
     {
         return $this->filename;
     }
 
+    /**
+     * Get the real path of the log file
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
     public function getRealPath($filename)
     {
         return sprintf('%s/%s', $this->logDir, $filename);
     }
 
+    /**
+     * Generates a random filename
+     *
+     * @return string
+     */
     private function generateLogFilename()
     {
         return sprintf('batch_%s.log', sha1(uniqid(rand(), true)));
