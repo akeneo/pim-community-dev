3e3861c560eb19ccc0051c37bc9aa88dbb62ba1a
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-08-30 14:12:13 +0300

    Implemented BAP-1469: Wrap js-scripts of Аddress, Email, Tags, User & Windows bundles into define module call (4)   - User bundles done

--- Bundle/UserBundle/Resources/public/js/init-signin.js
+++ Bundle/UserBundle/Resources/public/js/init-signin.js
@@ -1,26 +1,34 @@
-$(function() {
-    $(window)
-        .resize(function() {
-            form = $('form.form-signin');
-            var thisHeight = $(window).height()/2 - form.height()/2;
-            if  (thisHeight > 40) {
-                thisHeight = thisHeight -40;
-            }
-            form.css('margin-top', thisHeight );
-        })
-        .trigger('resize');
+/* jshint browser:true */
+/* global require */
+require(['jquery'],
+function($) {
+    'use strict';
 
-    var hashUrl = window.location.hash;
-    var hashUrlTag = '#url=';
-    if (hashUrl.length && hashUrl.match(hashUrlTag)) {
-        if (hashUrl.indexOf('|') !== -1) {
-            hashUrl = hashUrl.substring(0, hashUrl.indexOf('|'));
+    $(function() {
+        $(window)
+            .resize(function() {
+                var form = $('form.form-signin'),
+                    thisHeight = $(window).height()/2 - form.height()/2;
+                if (thisHeight > 40) {
+                    thisHeight = thisHeight -40;
+                }
+                form.css('margin-top', thisHeight );
+            })
+            .trigger('resize');
+
+        var hashUrl = window.location.hash,
+            hashUrlTag = '#url=',
+            hashArray;
+        if (hashUrl.length && hashUrl.match(hashUrlTag)) {
+            if (hashUrl.indexOf('|') !== -1) {
+                hashUrl = hashUrl.substring(0, hashUrl.indexOf('|'));
+            }
+            hashUrl = hashUrl.replace(hashUrlTag, '');
+            hashArray = hashUrl.split('php');
+            if (hashArray[1]) {
+                hashUrl = hashArray[1];
+            }
+            $('input[name="_target_path"]').val(hashUrl);
         }
-        hashUrl = hashUrl.replace(hashUrlTag, '');
-        var hashArray = hashUrl.split('php');
-        if (hashArray[1]) {
-            hashUrl = hashArray[1];
-        }
-        $('input[name="_target_path"]').val(hashUrl);
-    }
+    });
 });
