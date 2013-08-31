f6443819f8b4b62020355b656a3ec58c6139fd8d
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-08-31 18:15:40 +0300

    Implemented BAP-1470: Wrap js-scripts of Translation, Search, Form, JSFormValidation & EntityExtend  bundles into define module call (4)   - form bundle

--- Bundle/FormBundle/Resources/public/js/select2-config.js
+++ Bundle/FormBundle/Resources/public/js/select2-config.js
@@ -1,87 +1,101 @@
-var OroSelect2Config = function (config, url, perPage) {
-    this.config = config;
-    this.url = url;
-    this.perPage = perPage;
-};
+/* global define */
+define(['jquery', 'underscore'],
+function($, _) {
+    'use strict';
 
-OroSelect2Config.prototype.getConfig = function () {
-    var self = this;
-    if (this.config.formatResult === undefined) {
-        this.config.formatResult = this.format(this.config.result_template !== undefined ? this.config.result_template : false);
-    }
-    if (this.config.formatSelection === undefined) {
-        this.config.formatSelection = this.format(this.config.selection_template !== undefined ? this.config.selection_template : false);
-    }
-    if (this.config.initSelection === undefined) {
-        this.config.initSelection = _.bind(this.initSelection, this);
-    }
-    if (this.config.ajax === undefined) {
-        this.config.ajax = {
-            'url': this.url,
-            'data': function (query, page) {
-                return {
-                    'page': page,
-                    'per_page': self.perPage,
-                    'query': query
-                };
-            },
-            'results': function (data, page) {
-                return data;
-            }
-        };
-    }
-    if (this.config.ajax.quietMillis === undefined) {
-        this.config.ajax.quietMillis = 700;
-    }
-    if (this.config.escapeMarkup === undefined) {
-        this.config.escapeMarkup = function (m) { return m; };
-    }
-    return this.config;
-};
-
-OroSelect2Config.prototype.format = function (jsTemplate) {
-    var self = this;
-    return function (object, container, query) {
-        if ($.isEmptyObject(object)) {
-            return undefined;
-        }
-        var result = '';
-        var highlight = function (str) {
-            return self.highlightSelection(str, query);
-        };
-        if (object._html !== undefined) {
-            result = object._html;
-        } else if (jsTemplate) {
-            object.highlight = highlight;
-            var tpl = _.template(jsTemplate);
-            result = tpl(object);
-        } else {
-            result = highlight(self.getTitle(object, self.config.properties));
-        }
-        return result;
+    /**
+     * @export oro/select2-config
+     */
+    var Select2Config = function (config, url, perPage) {
+        this.config = config;
+        this.url = url;
+        this.perPage = perPage;
     };
-};
 
-OroSelect2Config.prototype.initSelection = function (element, callback) {
-    if (!_.isUndefined(this.config.multiple) && this.config.multiple === true) {
-        callback(element.data('entities'));
-    } else {
-        callback(element.data('entities').pop());
-    }
-};
+    Select2Config.prototype = {
+        getConfig: function () {
+            var self = this;
+            if (this.config.formatResult === undefined) {
+                this.config.formatResult = this.format(this.config.result_template || false);
+            }
+            if (this.config.formatSelection === undefined) {
+                this.config.formatSelection = this.format(this.config.selection_template || false);
+            }
+            if (this.config.initSelection === undefined) {
+                this.config.initSelection = _.bind(this.initSelection, this);
+            }
+            if (this.config.ajax === undefined) {
+                this.config.ajax = {
+                    'url': this.url,
+                    'data': function (query, page) {
+                        return {
+                            'page': page,
+                            'per_page': self.perPage,
+                            'query': query
+                        };
+                    },
+                    'results': function (data, page) {
+                        return data;
+                    }
+                };
+            }
+            if (this.config.ajax.quietMillis === undefined) {
+                this.config.ajax.quietMillis = 700;
+            }
+            if (this.config.escapeMarkup === undefined) {
+                this.config.escapeMarkup = function (m) { return m; };
+            }
+            return this.config;
+        },
 
-OroSelect2Config.prototype.highlightSelection = function (str, selection) {
-    return str && selection && selection.term ? str.replace(new RegExp(selection.term, 'ig'), '<span class="select2-match">$&</span>') : str;
-};
+        format: function(jsTemplate) {
+            var self = this;
+            return function (object, container, query) {
+                if ($.isEmptyObject(object)) {
+                    return undefined;
+                }
+                var result = '', tpl,
+                    highlight = function (str) {
+                    return self.highlightSelection(str, query);
+                };
+                if (object._html !== undefined) {
+                    result = object._html;
+                } else if (jsTemplate) {
+                    object.highlight = highlight;
+                    tpl = _.template(jsTemplate);
+                    result = tpl(object);
+                } else {
+                    result = highlight(self.getTitle(object, self.config.properties));
+                }
+                return result;
+            };
+        },
 
-OroSelect2Config.prototype.getTitle = function (data, properties) {
-    var title = '';
-    if (data) {
-        var result = [];
-        for (var i = 0; i < properties.length; i++) {
-            result.push(data[properties[i]]);
+        initSelection: function(element, callback) {
+            if (this.config.multiple === true) {
+                callback(element.data('entities'));
+            } else {
+                callback(element.data('entities').pop());
+            }
+        },
+
+        highlightSelection: function(str, selection) {
+            return str && selection && selection.term ?
+                str.replace(new RegExp(selection.term, 'ig'), '<span class="select2-match">$&</span>') : str;
+        },
+
+        getTitle: function(data, properties) {
+            var title = '', result, i;
+            if (data) {
+                result = [];
+                _.each(properties, function(property) {
+                    result.push(data[property]);
+                });
+                title = result.join(' ');
+            }
+            return title;
         }
-        title = result.join(' ');
-    }
-    return title;
-};
+    };
+
+    return Select2Config;
+});
