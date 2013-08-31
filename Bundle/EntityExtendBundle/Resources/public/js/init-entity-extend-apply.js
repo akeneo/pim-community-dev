9a784ee25fa96fe5a03f903926d2544200fefeda
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-08-31 19:05:32 +0300

    Implemented BAP-1470: Wrap js-scripts of Translation, Search, Form, JSFormValidation & EntityExtend  bundles into define module call (6)   - EntityExtend bundle

--- Bundle/EntityExtendBundle/Resources/public/js/init-entity-extend-apply.js
+++ Bundle/EntityExtendBundle/Resources/public/js/init-entity-extend-apply.js
@@ -1,39 +1,42 @@
-$(function() {
-    $(document).on('click', '.entity-extend-apply', function (e) {
-        var el = $(this);
-        var message = el.data('message');
-        var doAction = function() {
-            confirmUpdate.preventClose(function(){});
+/* jshint browser:true */
+/* global require */
+require(['jquery', 'underscore', 'oro/translator', 'oro/modal'],
+function($, _, __, Modal) {
+    'use strict';
+    $(function() {
+        $(document).on('click', '.entity-extend-apply', function (e) {
+            var el = $(this),
+                message = el.data('message'),
+                doAction = function() {
+                    confirmUpdate.preventClose(function(){});
 
-            var url = $(el).attr('href').substr(21);
-            var progressbar = $('#progressbar').clone();
-            progressbar
-                .attr('id', 'confirmUpdateLoading')
-                .css({'display':'block', 'margin': '0 auto'})
-                .find('h3').remove();
+                    var url = $(el).attr('href').substr(21),
+                        progressbar = $('#progressbar').clone();
+                    progressbar
+                        .attr('id', 'confirmUpdateLoading')
+                        .css({'display':'block', 'margin': '0 auto'})
+                        .find('h3').remove();
 
-            confirmUpdate.$content.parent().find('a.cancel').hide();
-            confirmUpdate.$content.parent().find('a.close').hide();
-            confirmUpdate.$content.parent().find('a.btn-danger').replaceWith(progressbar);
+                    confirmUpdate.$content.parent().find('a.cancel').hide();
+                    confirmUpdate.$content.parent().find('a.close').hide();
+                    confirmUpdate.$content.parent().find('a.btn-danger').replaceWith(progressbar);
 
-            $('#confirmUpdateLoading').show();
-            window.location.href = url;
-        };
-
-        if (!_.isUndefined(Oro.BootstrapModal)) {
-            var confirmUpdate = new Oro.BootstrapModal({
-                allowCancel: true,
-                cancelText: _.__('Cancel'),
-                title: 'Schema update confirmation',
-                content: '<p>'+ _.__('Your config changes will be applied to schema.')+'</p></p>'+ _.__('It may take few minutes...')+'</p>',
-                okText: 'Yes, Proceed'
-            });
+                    $('#confirmUpdateLoading').show();
+                    window.location.href = url;
+                },
+                /** @type oro.Modal */
+                confirmUpdate = new Modal({
+                    allowCancel: true,
+                    cancelText: __('Cancel'),
+                    title: __('Schema update confirmation'),
+                    content: '<p>' + __('Your config changes will be applied to schema.') +
+                        '</p></p>' + __('It may take few minutes...') + '</p>',
+                    okText: __('Yes, Proceed')
+                });
             confirmUpdate.on('ok', doAction);
             confirmUpdate.open();
-        } else if (window.confirm(message)) {
-            doAction();
-        }
 
-        return false;
+            return false;
+        });
     });
 });
