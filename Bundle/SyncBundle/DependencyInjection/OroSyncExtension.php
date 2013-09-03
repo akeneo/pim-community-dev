bfbee4cf4d6ec3a78ace94e817b3bedc0fe954ee
Dan Yasnyuk <danil@magecore.com>
2013-09-03 16:01:06 +0300

    Fixed wrong filename

--- Bundle/SyncBundle/DependencyInjection/OroSyncExtension.php
+++ Bundle/SyncBundle/DependencyInjection/OroSyncExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\CronBundle\DependencyInjection;
+namespace Oro\Bundle\SyncBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -12,19 +12,15 @@ use Symfony\Component\Config\FileLocator;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class OroCronExtension extends Extension
+class OroSyncExtension extends Extension
 {
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
 
-        $container->setParameter('oro_cron.max_jobs', (int) $config['max_concurrent_jobs']);
+        $loader->load('services.yml');
     }
 }
