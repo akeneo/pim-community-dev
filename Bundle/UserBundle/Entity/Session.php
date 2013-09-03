a49c071217ffb53fd4f6f368a24fd042b1925bdc
Dan Yasnyuk <danil@magecore.com>
2013-09-03 19:25:14 +0300

    BAP-1365: Develop WAMP server application for broadcasting client data updates (PHP side) - session entity added

--- Bundle/UserBundle/Entity/Session.php
+++ Bundle/UserBundle/Entity/Session.php
@@ -5,98 +5,28 @@ namespace Oro\Bundle\UserBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Table(name="oro_user_api")
+ * @ORM\Table(name="oro_session")
  * @ORM\Entity
  */
-class UserApi
+class Session
 {
     /**
      * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="string", length=255)
      */
     protected $id;
 
     /**
-     * @var User
-     *
-     * @ORM\OneToOne(targetEntity="User", inversedBy="api", fetch="LAZY")
-     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
-     */
-    protected $user;
-
-    /**
      * @var string
      *
-     * @ORM\Column(name="api_key", type="string", unique=true, length=255, nullable=false)
+     * @ORM\Column(name="sess_data", type="text", nullable=false)
      */
-    protected $apiKey;
+    protected $value;
 
     /**
-     * Get id
+     * @var int
      *
-     * @return integer
+     * @ORM\Column(name="sess_time", type="integer", nullable=false)
      */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set apiKey
-     *
-     * @param string $apiKey
-     *
-     * @return UserApi
-     */
-    public function setApiKey($apiKey)
-    {
-        $this->apiKey = $apiKey;
-
-        return $this;
-    }
-
-    /**
-     * Get apiKey
-     *
-     * @return string
-     */
-    public function getApiKey()
-    {
-        return $this->apiKey;
-    }
-
-    /**
-     * Set user
-     *
-     * @param User $user
-     *
-     * @return UserApi
-     */
-    public function setUser(User $user)
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    /**
-     * Get user
-     *
-     * @return User
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * Generate random API key
-     *
-     * @return string
-     */
-    public function generateKey()
-    {
-        return bin2hex(hash('sha1', uniqid(mt_rand(), true), true));
-    }
+    protected $time;
 }
