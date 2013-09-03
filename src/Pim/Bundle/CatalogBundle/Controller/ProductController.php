b63e8f5fa3cf0ce9b151906122066c580346b2bf
Romain Monceau <romain@akeneo.com>
2013-09-03 18:03:31 +0200

    PIM-942 : Clean code in ProductController

--- src/Pim/Bundle/CatalogBundle/Controller/ProductController.php
+++ src/Pim/Bundle/CatalogBundle/Controller/ProductController.php
@@ -37,11 +37,8 @@ class ProductController extends Controller
         $gridManager->setFilterCategoryId($request->get('categoryId', 0));
         $datagrid = $gridManager->getDatagrid();
 
-        if ('json' == $request->getRequestFormat()) {
-            $view = 'OroGridBundle:Datagrid:list.json.php';
-        } else {
-            $view = 'PimCatalogBundle:Product:index.html.twig';
-        }
+        $view =  ('json' === $request->getRequestFormat()) ?
+            'OroGridBundle:Datagrid:list.json.php' : 'PimCatalogBundle:Product:index.html.twig';
 
         $params = array(
             'datagrid'   => $datagrid->createView(),
