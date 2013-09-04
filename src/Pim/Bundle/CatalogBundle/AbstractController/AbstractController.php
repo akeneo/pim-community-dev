7e8f35c397e614a5f51a17ed94293963a029e4a4
Antoine Guigan <antoine@akeneo.com>
2013-09-04 08:34:45 +0200

    Updated PHPDoc

--- src/Pim/Bundle/CatalogBundle/AbstractController/AbstractController.php
+++ src/Pim/Bundle/CatalogBundle/AbstractController/AbstractController.php
@@ -11,7 +11,8 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
- *
+ * Base abstract controller
+ * 
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
--- src/Pim/Bundle/CatalogBundle/AbstractController/AbstractDoctrineController.php
+++ src/Pim/Bundle/CatalogBundle/AbstractController/AbstractDoctrineController.php
@@ -14,7 +14,8 @@ use Doctrine\Common\Persistence\ObjectRepository;
 use Symfony\Component\Form\FormTypeInterface;
 
 /**
- *
+ * Base abstract controller for managing entities
+ * 
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
