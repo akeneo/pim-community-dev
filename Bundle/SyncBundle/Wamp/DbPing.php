90597279b5a2414f2eae4cad1248ead070d44fbc
Dan Yasnyuk <danil@magecore.com>
2013-09-04 13:31:51 +0300

    BAP-1365: Develop WAMP server application for broadcasting client data updates (PHP side) - added periodic service as a workaround for "2006 MySQL server has gone away" error

---
+++ Bundle/SyncBundle/Wamp/DbPing.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oro\Bundle\SyncBundle\Wamp;
+
+use JDare\ClankBundle\Periodic\PeriodicInterface;
+
+class DbPing implements PeriodicInterface
+{
+    /**
+     * @var \PDO PDO instance.
+     */
+    private $pdo;
+
+    /**
+     * @param \PDO $pdo A \PDO instance, same as used by session handler
+     */
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * This function is executed every 1 minute to make sure that WebSocket server has a DB connection.
+     */
+    public function tick()
+    {
+        $stmt = $this->pdo->prepare("SELECT 1");
+
+        $stmt->execute();
+    }
+}
