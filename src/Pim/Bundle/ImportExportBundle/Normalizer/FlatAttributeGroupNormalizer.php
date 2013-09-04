91d62f711e9be13d4af8b96586a0bf37c4cf69f1
Romain Monceau <romain@akeneo.com>
2013-09-04 12:13:26 +0200

    PIM-1060 & PIM-1061 : Create attribute group normalizers

---
+++ src/Pim/Bundle/ImportExportBundle/Normalizer/FlatAttributeGroupNormalizer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pim\Bundle\ImportExportBundle\Normalizer;
+
+use Pim\Bundle\CatalogBundle\Entity\AttributeGroup;
+
+/**
+ * Flat attribute group normalizer
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FlatAttributeGroupNormalizer extends AttributeGroupNormalizer
+{
+    /**
+     * @var string[]
+     */
+    protected $supportedFormats = array('csv');
+
+    /**
+     * @var array
+     */
+    private $results;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeName(AttributeGroup $group)
+    {
+        $names = parent::normalizeName($group);
+        $flat = array();
+        foreach ($names as $locale => $name) {
+            $flat[]= $locale.':'.$name;
+        }
+
+        return implode(', ', $flat);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeAttributes(AttributeGroup $group)
+    {
+        $attributes = parent::normalizeAttributes($group);
+
+        return implode(', ', $attributes);
+    }
+}
