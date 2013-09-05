b7c205494a64912a00b8924bee791663a2a49ac8
Romain Monceau <romain@akeneo.com>
2013-09-05 15:24:58 +0200

    PIM-695 : Clean code for datagrid and datagrid builder

--- src/Pim/Bundle/GridBundle/Builder/ORM/DatagridBuilder.php
+++ src/Pim/Bundle/GridBundle/Builder/ORM/DatagridBuilder.php
@@ -2,32 +2,48 @@
 
 namespace Pim\Bundle\GridBundle\Builder\ORM;
 
-use Oro\Bundle\GridBundle\Datagrid\ParametersInterface;
-
-use Oro\Bundle\GridBundle\Route\RouteGeneratorInterface;
-
-use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
-
-use Oro\Bundle\GridBundle\Datagrid\ProxyQueryInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Serializer\Serializer;
 
 use Oro\Bundle\GridBundle\Action\ActionFactoryInterface;
-
-use Oro\Bundle\GridBundle\Sorter\SorterFactoryInterface;
-
+use Oro\Bundle\GridBundle\Datagrid\ParametersInterface;
+use Oro\Bundle\GridBundle\Datagrid\ProxyQueryInterface;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
 use Oro\Bundle\GridBundle\Filter\FilterFactoryInterface;
-
+use Oro\Bundle\GridBundle\Route\RouteGeneratorInterface;
+use Oro\Bundle\GridBundle\Sorter\SorterFactoryInterface;
 use Oro\Bundle\UserBundle\Acl\ManagerInterface;
-
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
-use Symfony\Component\Form\FormFactoryInterface;
-
 use Oro\Bundle\GridBundle\Builder\ORM\DatagridBuilder as OroDatagridBuilder;
 
+/**
+ * Override of OroPlatform datagrid builder
+ * Inject serializer in datagrid builder
+ * The serializer is passed to the datagrid and allow quick export
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class DatagridBuilder extends OroDatagridBuilder
 {
+    /**
+     * @var Serializer
+     */
     protected $serializer;
 
+    /**
+     * Constructor
+     *
+     * @param FormFactoryInterface $formFactory
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param ManagerInterface $aclManager
+     * @param FilterFactoryInterface $filterFactory
+     * @param SorterFactoryInterface $sorterFactory
+     * @param ActionFactoryInterface $actionFactory
+     * @param string $className
+     * @param Serializer $serializer
+     */
     public function __construct(
         FormFactoryInterface $formFactory,
         EventDispatcherInterface $eventDispatcher,
@@ -36,13 +52,26 @@ class DatagridBuilder extends OroDatagridBuilder
         SorterFactoryInterface $sorterFactory,
         ActionFactoryInterface $actionFactory,
         $className,
-        $serializer = null
+        Serializer $serializer
     ) {
-        parent::__construct($formFactory, $eventDispatcher, $aclManager, $filterFactory, $sorterFactory, $actionFactory, $className);
+        parent::__construct(
+            $formFactory,
+            $eventDispatcher,
+            $aclManager,
+            $filterFactory,
+            $sorterFactory,
+            $actionFactory,
+            $className
+        );
 
         $this->serializer = $serializer;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * Add the serializer to the datagrid object
+     */
     public function getBaseDatagrid(
         ProxyQueryInterface $proxyQuery,
         FieldDescriptionCollection $fieldCollection,
@@ -52,6 +81,8 @@ class DatagridBuilder extends OroDatagridBuilder
     ) {
         $datagrid = parent::getBaseDatagrid($proxyQuery, $fieldCollection, $routeGenerator, $parameters, $name);
 
-        return $datagrid->setSerializer($this->serializer);
+        $datagrid->setSerializer($this->serializer);
+
+        return $datagrid;
     }
 }
--- src/Pim/Bundle/GridBundle/Datagrid/Datagrid.php
+++ src/Pim/Bundle/GridBundle/Datagrid/Datagrid.php
@@ -2,15 +2,34 @@
 
 namespace Pim\Bundle\GridBundle\Datagrid;
 
-use Oro\Bundle\GridBundle\Datagrid\DatagridInterface;
+use Symfony\Component\Serializer\Serializer;
 
+use Oro\Bundle\GridBundle\Datagrid\DatagridInterface;
 use Oro\Bundle\GridBundle\Datagrid\Datagrid as OroDatagrid;
 
+/**
+ * Override of OroPlatform datagrid
+ * DatagridBuilder set serializer to this class allowing quick export feature
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class Datagrid extends OroDatagrid
 {
+    /**
+     * @var Serializer
+     */
     protected $serializer;
 
-    public function setSerializer($serializer)
+    /**
+     * Setter serializer
+     *
+     * @param Serializer $serializer
+     *
+     * @return \Pim\Bundle\GridBundle\Datagrid\Datagrid
+     */
+    public function setSerializer(Serializer $serializer)
     {
         $this->serializer = $serializer;
 
