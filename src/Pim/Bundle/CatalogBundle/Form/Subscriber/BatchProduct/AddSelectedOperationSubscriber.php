5a88d55056e2db36e44d5b0472923409265bfbad
Gildas Quéméner <gildas.quemener@gmail.com>
2013-09-05 09:48:58 +0200

    PIM-944 Tested the add selected operation form subscriber

--- src/Pim/Bundle/CatalogBundle/Form/Subscriber/BatchProduct/AddSelectedOperationSubscriber.php
+++ src/Pim/Bundle/CatalogBundle/Form/Subscriber/BatchProduct/AddSelectedOperationSubscriber.php
@@ -9,7 +9,8 @@ use Pim\Bundle\CatalogBundle\BatchOperation\BatchOperation;
 use Pim\Bundle\CatalogBundle\Model\BatchProduct;
 
 /**
- * 
+ * Add selected operation field if one is set
+ *
  * @author    Gildas Quemener <gildas.quemener@gmail.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
