346b772c140f1c59cf7e018688ffe67e2ca9ca6f
Ivan Shakuta <ishakuta@gmail.com>
2013-09-06 17:17:23 +0000

    BAP-1384: System configuration form - refactoring

--- Bundle/ConfigBundle/Entity/Repository/ConfigValueRepository.php
+++ Bundle/ConfigBundle/Entity/Repository/ConfigValueRepository.php
@@ -12,4 +12,28 @@ use Doctrine\ORM\EntityRepository;
  */
 class ConfigValueRepository extends EntityRepository
 {
+    /**
+     * Remove values by params
+     *
+     * @param integer $configId
+     * @param string $section
+     * @param string $name
+     * @return array
+     */
+    public function removeValues($configId, $section, $name)
+    {
+        $builder = $this->getEntityManager()->createQueryBuilder();
+
+        $builder->getEntityManager()->beginTransaction();
+        $builder->delete('OroConfigBundle:ConfigValue', 'cv')
+            ->where('cv.config = :configId')
+            ->andWhere('cv.name = :name')
+            ->andWhere('cv.section = :section')
+            ->setParameter('configId', $configId)
+            ->setParameter('section', $section)
+            ->setParameter('name', $name);
+
+        $builder->getQuery()->execute();
+        $builder->getEntityManager()->commit();
+    }
 }
