e5a81ec6de10a35a9e692fa3090b3b38392d1343
Benoit Jacquemont <benoit@akeneo.com>
2013-09-09 11:32:16 +0200

    Initial BatchBundle import into Oro Platform

---
+++ Bundle/BatchBundle/Item/ItemReaderInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\BatchBundle\Item;
+
+use Oro\Bundle\BatchBundle\Entity\StepExecution;
+
+/**
+ * Interface to provide data.
+ *
+ * Implementation are expected to be stateful and will be called multiple times
+ * for each batch, with each call to read() returning a different value and
+ * finally returning null when all input data is exhausted.
+ *
+ * Inspired by Spring Batch  org.springframework.batch.item.ItemReader
+ *
+ */
+interface ItemReaderInterface
+{
+    /**
+     * Reads a piece of input data and advance to the next one. Implementations
+     * <strong>must</strong> return <code>null</code> at the end of the input
+     * data set.
+     *
+     * @throws ParseException if there is a problem parsing the current record
+     * (but the next one may still be valid)
+     * @throws NonTransientResourceException if there is a fatal exception in
+     * the underlying resource. After throwing this exception implementations
+     * should endeavour to return null from subsequent calls to read.
+     * @throws UnexpectedInputException if there is an uncategorised problem
+     * with the input data. Assume potentially transient, so subsequent calls to
+     * read might succeed.
+     * @throws Exception if an there is a non-specific error.
+     */
+    public function read(StepExecution $stepExecution);
+}
---
+++ Bundle/BatchBundle/Item/Support/ArrayReader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oro\Bundle\BatchBundle\Item\Support;
+
+use Oro\Bundle\BatchBundle\Item\ItemReaderInterface;
+use Oro\Bundle\BatchBundle\Entity\StepExecution;
+
+/**
+ * Simple reader that provides data from an array
+ *
+ */
+class ArrayReader implements ItemReaderInterface
+{
+    protected $readIndex = 0;
+    protected $items;
+
+    /**
+     * @param array $items
+     *
+     * @return $this
+     */
+    public function setItems(array $items)
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(StepExecution $stepExecution)
+    {
+        $item = null;
+
+        if ($this->readIndex < count($this->items)) {
+            $item = $this->items[$this->readIndex];
+            $this->readIndex++;
+        }
+
+        return $item;
+    }
+}
