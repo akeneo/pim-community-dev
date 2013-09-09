8d32a122efaf7b8b92a7ce9a46753e35cd282e8d
Benoit Jacquemont <benoit@akeneo.com>
2013-09-09 11:32:16 +0200

    Initial BatchBundle import into Oro Platform

---
+++ Connector/Connector.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oro\Bundle\BatchBundle\Connector;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Connector bundle base class
+ *
+ */
+class Connector extends Bundle
+{
+}
