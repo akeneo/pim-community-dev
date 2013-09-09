d6afaa6a53674bdc35b4f40145b642e16856d394
Romain Monceau <romain@akeneo.com>
2013-09-09 18:02:17 +0200

    PIM-620 : Re-add quickexportcollectionaction allowing to download file

---
+++ src/Pim/Bundle/GridBundle/Resources/public/js/app/datagrid/action/quickexportcollectionaction.js
@@ -0,0 +1,45 @@
+var Pim = Pim || {};
+Pim.Datagrid = Pim.Datagrid || {};
+Pim.Datagrid.Action = Pim.Datagrid.Action || {};
+
+Pim.Datagrid.Action.QuickExportCollectionAction = Oro.Datagrid.Action.NavigateAction.extend({
+
+    initialize: function(options) {
+        options = options || {};
+
+        if (!options.datagrid) {
+            throw new TypeError("'datagrid' is required");
+        }
+        this.collection = options.datagrid.collection;
+
+        this.on('preExecute', _.bind(this._preExecuteSubscriber, this));
+        this.launcherOptions = _.extend({
+            link: this.getLink(),
+            runAction: false
+        }, this.launcherOptions);
+
+        Oro.Datagrid.Action.AbstractAction.prototype.initialize.apply(this, arguments);
+    },
+
+    /**
+     * Execute refresh collection
+     */
+    execute: function() {
+        window.location.href = this.getLink();
+    },
+    
+    getLink: function() {
+        var data = {};
+        
+        data = this.collection.processQueryParams(data, this.collection.state);
+        data = this.collection.processFiltersParams(data, this.collection.state);
+        data = Oro.packToQueryString(data);
+        
+        var baseUrl = Routing.generate('pim_catalog_product_index', {'_format': 'csv'});
+        var url = baseUrl.concat('?'+data);
+        console.log('----> ');
+        console.log(url);
+        
+        return url;
+    }
+});
