fe2288ca412209c86519e9a259e08f8354cb2267
Nicolas Dupont <nicolas@akeneo.com>
2013-09-09 16:34:32 +0200

    PIM-1038 : update scripts with new commands

--- init-db.sh
+++ init-db.sh
@@ -7,18 +7,19 @@ else
   env="dev"
 fi
 
-php app/console doctrine:database:drop --force --env=$env
-php app/console doctrine:database:create  --env=$env
-php app/console doctrine:schema:update --force --env=$env
+php app/console doctrine:database:drop --force --env $env
+php app/console doctrine:database:create  --env $env
+php app/console doctrine:schema:create --env $env
 if [ $env != "behat" ]; then
-    php app/console oro:search:create-index --env=$env
-    php app/console doctrine:fixtures:load --no-interaction --env=$env --no-debug
-    php app/console oro:acl:load --env=$env
-    php app/console oro:entity-config:update --env=$env
-    #php app/console pim:search:reindex en_US --env=$env
-    php app/console pim:versioning:refresh --env=$env
-    php app/console pim:product:completeness-calculator --env=$env
-else
-    php app/console oro:entity-config:update --env=$env
-    php app/console cache:clear --env=$env
+    php app/console doctrine:fixture:load --no-debug --no-interaction --env $env
 fi
+php app/console oro:acl:load --env $env
+php app/console oro:entity-config:update --env $env
+php app/console oro:entity-extend:create --env $env
+php app/console cache:clear --env $env
+php app/console doctrine:schema:update --env $env --force
+php app/console oro:search:create-index --env $env
+php app/console pim:search:reindex en_US --env $env
+php app/console pim:versioning:refresh --env $env
+php app/console pim:product:completeness-calculator --env $env
+
--- install.sh
+++ install.sh
@@ -7,12 +7,9 @@ then
 fi
 
 php app/console doctrine:schema:create --env $ENV
-
-if [ $ENV != "behat" ]
-then
+if [ $ENV != "behat" ]; then
     php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
 fi
-
 php app/console oro:acl:load --env $ENV
 php app/console oro:navigation:init --env $ENV
 php app/console oro:entity-config:update --env $ENV
@@ -20,7 +17,7 @@ php app/console oro:entity-extend:create --env $ENV
 php app/console cache:clear --env $ENV
 php app/console doctrine:schema:update --env $ENV --force
 php app/console oro:search:create-index --env $ENV
-# php app/console pim:search:reindex en_US --env $ENV
+php app/console pim:search:reindex en_US --env $ENV
 php app/console pim:versioning:refresh --env $ENV
 php app/console pim:product:completeness-calculator --env $ENV
 php app/console assets:install web --env $ENV
