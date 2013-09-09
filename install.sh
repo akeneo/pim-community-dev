278e5e664f6eded8770e5828972474de0e95e606
Nicolas Dupont <nicolas@akeneo.com>
2013-09-09 12:10:49 +0200

    PIM-1115 : fix the installer script

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 ENV="prod"
+
 if [ $1 ]
 then
     ENV="$1"
 fi
-php app/console doctrine:database:drop --force --env $ENV
-php app/console doctrine:database:create --env $ENV
+
 php app/console doctrine:schema:create --env $ENV
-if [ $env != "behat" ]; then
+
+if [ $ENV != "behat" ]
+then
     php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
 fi
+
 php app/console oro:acl:load --env $ENV
 php app/console oro:navigation:init --env $ENV
 php app/console oro:entity-config:update --env $ENV
@@ -19,7 +22,7 @@ php app/console doctrine:schema:update --env $ENV --force
 php app/console oro:search:create-index --env $ENV
 # php app/console pim:search:reindex en_US --env $ENV
 php app/console pim:versioning:refresh --env $ENV
-php app/console pim:product:completeness-calculator--env $ENV
+php app/console pim:product:completeness-calculator --env $ENV
 php app/console assets:install web --env $ENV
 php app/console assetic:dump --env $ENV
 php app/console oro:assetic:dump
