f2cf09272c2917e80b1919f3fffed27ad97e317f
Alexander Nezdoiminoga <alex@magecore.com>
2013-09-10 20:42:17 +0300

    EntityConfigBundle: datagrids refactoring

---
+++ Bundle/EntityConfigBundle/Datagrid/BaseDatagrid.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Oro\Bundle\EntityConfigBundle\Datagrid;
+
+use Oro\Bundle\GridBundle\Action\ActionInterface;
+use Oro\Bundle\GridBundle\Datagrid\DatagridManager;
+use Oro\Bundle\GridBundle\Datagrid\ResultRecord;
+use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
+use Oro\Bundle\GridBundle\Property\ActionConfigurationProperty;
+use Oro\Bundle\GridBundle\Property\UrlProperty;
+
+use Oro\Bundle\EntityConfigBundle\Config\ConfigModelManager;
+use Oro\Bundle\EntityConfigBundle\Provider\PropertyConfigContainer;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
+
+class BaseDatagrid extends DatagridManager
+{
+    /**
+     * @var FieldDescriptionCollection
+     */
+    protected $fieldsCollection;
+
+    /**
+     * @var ConfigManager
+     */
+    protected $configManager;
+
+    /**
+     * @param ConfigManager $configManager
+     */
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * @param $gridActions
+     * @param $properties
+     * @param $actions
+     * @param $filters
+     */
+    protected function prepareProperties($gridActions, &$properties, &$actions, &$filters)
+    {
+        foreach ($gridActions as $config) {
+            $properties[] = new UrlProperty(
+                strtolower($config['name']) . '_link',
+                $this->router,
+                $config['route'],
+                (isset($config['args']) ? $config['args'] : array())
+            );
+
+            if (isset($config['filter'])) {
+                $filters[strtolower($config['name'])] = $config['filter'];
+            }
+
+            $actions[strtolower($config['name'])] = true;
+        }
+    }
+
+    /**
+     * @param $actions
+     * @param $type
+     */
+    protected function prepareRowActions(&$actions, $type = PropertyConfigContainer::TYPE_ENTITY)
+    {
+        foreach ($this->configManager->getProviders() as $provider) {
+            $gridActions = $provider->getPropertyConfig()->getGridActions($type);
+
+            foreach ($gridActions as $config) {
+                if (isset($config['acl_resource'])) {
+                    $acl = $config['acl_resource'];
+                } else {
+                    $acl = 'root';
+                }
+
+                $configItem = array(
+                    'name'         => strtolower($config['name']),
+                    'acl_resource' => $acl,
+                    'options'      => array(
+                        'label' => ucfirst($config['name']),
+                        'icon'  => isset($config['icon']) ? $config['icon'] : 'question-sign',
+                        'link'  => strtolower($config['name']) . '_link'
+                    )
+                );
+
+                if (isset($config['type'])) {
+                    switch ($config['type']) {
+                        case 'delete':
+                            $configItem['type'] = ActionInterface::TYPE_DELETE;
+                            break;
+                        case 'redirect':
+                            $configItem['type'] = ActionInterface::TYPE_REDIRECT;
+                            break;
+                        case 'ajax':
+                            $configItem['type'] = ActionInterface::TYPE_AJAX;
+                            break;
+                    }
+                } else {
+                    $configItem['type'] = ActionInterface::TYPE_REDIRECT;
+                }
+
+                $actions[] = $configItem;
+            }
+        }
+    }
+}
