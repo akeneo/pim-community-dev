1dcf2df57ca77682b7b2ad603971e62c9c675abd
Romain Monceau <romain@akeneo.com>
2013-09-10 15:56:09 +0200

    PIM-620 : Add comments on js file

--- src/Pim/Bundle/GridBundle/Resources/public/js/app/datagrid/action/quickexportcollectionaction.js
+++ src/Pim/Bundle/GridBundle/Resources/public/js/app/datagrid/action/quickexportcollectionaction.js
@@ -2,8 +2,23 @@ var Pim = Pim || {};
 Pim.Datagrid = Pim.Datagrid || {};
 Pim.Datagrid.Action = Pim.Datagrid.Action || {};
 
+/**
+ * Quick export collection action extending NavigateAction
+ * 
+ * @author  Romain Monceau <romain@akeneo.com>
+ * @class   Pim.Datagrid.Action.QuickExportCollectionAction
+ * @extends Oro.Datagrid.Action.NavigateAction
+ * @see     Oro.Datagrid.Action.AbstractAction
+ */
 Pim.Datagrid.Action.QuickExportCollectionAction = Oro.Datagrid.Action.NavigateAction.extend({
 
+    /**
+     * Initialize collection and launcher
+     * 
+     * @param {Object} options
+     * @param {Backbone.Collection} options.collection Collection
+     * @throws {TypeError} If collection is undefined
+     */
     initialize: function(options) {
         options = options || {};
 
@@ -20,10 +35,19 @@ Pim.Datagrid.Action.QuickExportCollectionAction = Oro.Datagrid.Action.NavigateAc
         Oro.Datagrid.Action.AbstractAction.prototype.initialize.apply(this, arguments);
     },
     
+    /**
+     * Execution when clicking on the button
+     * Open a new window to download the file come from the action called
+     */
     execute: function() {
         window.open(this.getLink());
     },
     
+    /**
+     * Get the link of the file
+     * 
+     * @return string
+     */
     getLink: function() {
         var data = {};
         data = this.collection.processQueryParams(data, this.collection.state);
@@ -31,8 +55,7 @@ Pim.Datagrid.Action.QuickExportCollectionAction = Oro.Datagrid.Action.NavigateAc
         data = Oro.packToQueryString(data);
         
         var baseUrl = Routing.generate('pim_catalog_product_index', {'_format': 'csv'});
-        var url = baseUrl.concat('?'+data);
         
-        return url;
+        return baseUrl.concat('?'+data);
     }
 });
