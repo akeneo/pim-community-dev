3e957334609948858b3e982ab04f28beae589b05
Dan Yasnyuk <danil@magecore.com>
2013-09-12 14:12:50 +0300

    BAP-1630: WebSocket exception during schema update - added WebSocket server connection check

--- Bundle/SyncBundle/Wamp/TopicPublisher.php
+++ Bundle/SyncBundle/Wamp/TopicPublisher.php
@@ -41,31 +41,52 @@ class TopicPublisher
     /**
      * Publish (broadcast) message
      *
-     * @param string       $topic Topic id (or channel), for example "acme/demo-channel"
-     * @param string|array $msg   Message
+     * @param  string       $topic Topic id (or channel), for example "acme/demo-channel"
+     * @param  string|array $msg   Message
+     * @return bool         True on success, false otherwise
      */
     public function send($topic, $msg)
     {
-        $this
-            ->getWs()
-            ->sendData(
-                json_encode(
-                    array(
-                        \Ratchet\Wamp\ServerProtocol::MSG_PUBLISH,
-                        $topic,
-                        $msg,
-                    )
+        $ws = $this->getWs();
+
+        if (!$ws) {
+            return false;
+        }
+
+        $ws->sendData(
+            json_encode(
+                array(
+                    \Ratchet\Wamp\ServerProtocol::MSG_PUBLISH,
+                    $topic,
+                    $msg,
                 )
-            );
+            )
+        );
+
+        return true;
     }
 
     /**
-     * @return WebSocket
+     * Check if WebSocket server is running
+     *
+     * @return bool True on success, false otherwise
+     */
+    public function check()
+    {
+        return !is_null($this->getWs());
+    }
+
+    /**
+     * @return WebSocket|null
      */
     protected function getWs()
     {
         if (!$this->ws) {
-            $this->ws = new WebSocket($this->host, $this->port);
+            try {
+                $this->ws = new WebSocket($this->host, $this->port);
+            } catch (\Exception $e) {
+                return null;
+            }
         }
 
         return $this->ws;
