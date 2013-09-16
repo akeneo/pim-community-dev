d0aa87a6a8488a04430ea9459c5dfe2cbd32a4ac
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-09-16 17:32:38 +0300

    CRM-469: Create normalizer for OroCRMContactBundle:ContactGroup - fixed issue with normalizers cache for collections

--- Bundle/ImportExportBundle/Serializer/Serializer.php
+++ Bundle/ImportExportBundle/Serializer/Serializer.php
@@ -3,7 +3,20 @@
 namespace Oro\Bundle\ImportExportBundle\Serializer;
 
 use Symfony\Component\Serializer\Serializer as BaseSerializer;
+use Doctrine\Common\Collections\Collection;
 
 class Serializer extends BaseSerializer
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($data, $format = null, array $context = array())
+    {
+        if ($data instanceof Collection) {
+            // Clear cache of normalizer for collections,
+            // because of wrong behaviour when selecting normalizer for collections of elements with different types
+            unset($this->normalizerCache[get_class($data)][$format]);
+        }
+        return parent::normalize($data, $format, $context);
+    }
 }
