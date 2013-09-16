3141173c9b6617636fa3789fdec73701cfb44e07
Dmitry Khrysev <x86demon@gmail.com>
2013-09-16 17:49:41 +0300

    CRM-430: Account Grid columns adjustment - added unit test for new form type

---
+++ Bundle/FormBundle/Tests/Unit/Form/Type/MultipleEntityTypeTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Tests\Unit\Form\Type;
+
+use Oro\Bundle\FormBundle\Form\Type\MultipleEntityType;
+use Symfony\Component\Form\FormView;
+
+class EntityIdentifierTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MultipleEntityType
+     */
+    private $type;
+
+    protected function setUp()
+    {
+        $this->type = new MultipleEntityType();
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('oro_multiple_entity', $this->type->getName());
+    }
+
+    public function testBuildForm()
+    {
+        $builder = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $builder->expects($this->at(0))
+            ->method('add')
+            ->with('added', 'oro_entity_identifier', array('class' => '\stdObject', 'multiple' => true))
+            ->will($this->returnSelf());
+        $builder->expects($this->at(1))
+            ->method('add')
+            ->with('removed', 'oro_entity_identifier', array('class' => '\stdObject', 'multiple' => true))
+            ->will($this->returnSelf());
+        $this->type->buildForm($builder, array('class' => '\stdObject'));
+    }
+
+    public function testSetDefaultOptions()
+    {
+        $optionsResolver = $this->getMockBuilder('Symfony\Component\OptionsResolver\OptionsResolver')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $optionsResolver->expects($this->once())
+            ->method('setRequired')
+            ->with(array('class'));
+        $optionsResolver->expects($this->once())
+            ->method('setDefaults')
+            ->with(
+                array(
+                    'class' => null,
+                    'mapped' => false,
+                    'grid_url' => null,
+                    'default_element' => null,
+                    'initial_elements' => null
+                )
+            );
+        $this->type->setDefaultOptions($optionsResolver);
+    }
+
+    /**
+     * @dataProvider optionsDataProvider
+     * @param array $options
+     * @param string $expectedKey
+     * @param mixed $expectedValue
+     */
+    public function testFinishView($options, $expectedKey, $expectedValue)
+    {
+        $form = $this->getMockBuilder('Symfony\Component\Form\Form')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $view = new FormView();
+        $this->type->finishView($view, $form, $options);
+        $this->assertArrayHasKey($expectedKey, $view->vars);
+        $this->assertEquals($expectedValue, $view->vars[$expectedKey]);
+    }
+
+    public function optionsDataProvider()
+    {
+        $view = new FormView();
+        $view->vars['id'] = 'test_id';
+        $defaultElementForm = $this->getMockBuilder('Symfony\Component\Form\Form')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $defaultElementForm->expects($this->any())
+            ->method('createView')
+            ->will($this->returnValue($view));
+
+        return array(
+            array(
+                array('grid_url' => '/test'), 'grid_url', '/test'
+            ),
+            array(
+                array(), 'grid_url', null
+            ),
+            array(
+                array('initial_elements' => array()), 'initial_elements', array()
+            ),
+            array(
+                array(), 'initial_elements', null
+            ),
+            array(
+                array('default_element' => $defaultElementForm), 'default_element', 'test_id'
+            )
+        );
+    }
+}
