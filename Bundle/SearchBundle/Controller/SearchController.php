2e9846ce42a5abf8cbb3785a71c8e90a82e3dd53
Romain Monceau <romain@akeneo.com>
2013-09-16 10:44:14 +0200

    BAP-1638 : Define ACL for Search feature

--- Bundle/SearchBundle/Controller/SearchController.php
+++ Bundle/SearchBundle/Controller/SearchController.php
@@ -12,10 +12,27 @@ use Oro\Bundle\SearchBundle\Datagrid\SearchDatagridManager;
 use Oro\Bundle\GridBundle\Datagrid\DatagridView;
 use Oro\Bundle\SearchBundle\Provider\ResultStatisticsProvider;
 
+use Oro\Bundle\UserBundle\Annotation\Acl;
+
+/**
+ * @Acl(
+ *     id="oro_search",
+ *     name="Search",
+ *     description="Search",
+ *     parent="oro_security"
+ * )
+ */
 class SearchController extends Controller
 {
     /**
      * @Route("/advanced-search", name="oro_search_advanced")
+     *
+     * @Acl(
+     *     id="oro_search_ajax_advanced",
+     *     name="Ajax advanced search",
+     *     description="Ajax advanced search",
+     *     parent="oro_search"
+     * )
      */
     public function ajaxAdvancedSearchAction()
     {
@@ -33,6 +50,13 @@ class SearchController extends Controller
      *
      * @Route("/search-bar", name="oro_search_bar")
      * @Template("OroSearchBundle:Search:searchBar.html.twig")
+     *
+     * @Acl(
+     *     id="oro_search_bar",
+     *     name="Search bar",
+     *     description="Search bar",
+     *     parent="oro_search"
+     * )
      */
     public function searchBarAction()
     {
@@ -47,6 +71,13 @@ class SearchController extends Controller
      * @param  string       $from
      * @param  string       $string
      * @return DatagridView
+     *
+     * @Acl(
+     *     id="oro_search_datagrid_results",
+     *     name="Search datagrid results",
+     *     description="Search datagrid results",
+     *     parent="oro_search"
+     * )
      */
     protected function getSearchResultsDatagridView($from, $string)
     {
@@ -70,6 +101,13 @@ class SearchController extends Controller
      *
      * @Route("/", name="oro_search_results")
      * @Template("OroSearchBundle:Search:searchResults.html.twig")
+     *
+     * @Acl(
+     *     id="oro_search_results",
+     *     name="Search results",
+     *     description="Search results",
+     *     parent="oro_search"
+     * )
      */
     public function searchResultsAction(Request $request)
     {
@@ -93,6 +131,13 @@ class SearchController extends Controller
      * Return search results in json for datagrid
      *
      * @Route("/ajax", name="oro_search_results_ajax")
+     *
+     * @Acl(
+     *     id="oro_search_ajax_results",
+     *     name="Ajax search results",
+     *     description="Ajax search results",
+     *     parent="oro_search"
+     * )
      */
     public function searchResultsAjaxAction(Request $request)
     {
