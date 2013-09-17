5ec092288532777d482e8d28f394753927fbb08f
Romain Monceau <romain@akeneo.com>
2013-09-17 16:00:46 +0200

    Add feature for refresh button in datagrid

--- features/Context/Page/Base/Grid.php
+++ features/Context/Page/Base/Grid.php
@@ -281,6 +281,7 @@ class Grid extends Index
 
     /**
      * Click on the reset button of the datagrid toolbar
+     * @throws \InvalidArgumentException
      */
     public function clickOnResetButton()
     {
@@ -293,6 +294,23 @@ class Grid extends Index
         }
 
         $resetBtn->click();
+    }
+
+    /**
+     * Click on the refresh button of the datagrid toolbar
+     * @throws \InvalidArgumentException
+     */
+    public function clickOnRefreshButton()
+    {
+        $refreshBtn = $this
+            ->getElement('Grid toolbar')
+            ->find('css', sprintf('a:contains("%s")', 'Refresh'));
+
+        if (!$refreshBtn) {
+            throw new \InvalidArgumentException('Refresh button not found');
+        }
+
+        $refreshBtn->click();
     }
 
     /**
