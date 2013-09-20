62969704e56d35ed0f8c727044848b2df1f5a374
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-09-20 18:31:51 +0300

    BAP-1648: Improve Contact object complexity  - changed class

--- Bundle/TestFrameworkBundle/Pages/AbstractEntity.php
+++ Bundle/TestFrameworkBundle/Pages/AbstractEntity.php
@@ -39,4 +39,53 @@ abstract class AbstractEntity extends Page
 
         return new $class($this->test, $redirect);
     }
+
+    public function verifyTag($tag)
+    {
+        if ($this->isElementPresent("//div[@id='s2id_orocrm_contact_form_tags_autocomplete']")) {
+            $tagsPath = $this->byXpath("//div[@id='s2id_orocrm_contact_form_tags_autocomplete']//input");
+            $tagsPath->click();
+            $tagsPath->value(substr($tag, 0, (strlen($tag)-1)));
+            $this->waitForAjax();
+            $this->assertElementPresent(
+                "//div[@id='select2-drop']//div[contains(., '{$tag}')]",
+                "Tag's autocoplete doesn't return entity"
+            );
+            $tagsPath->clear();
+        } else {
+            if ($this->isElementPresent("//div[@id='tags-holder']")) {
+                $this->assertElementPresent(
+                    "//div[@id='tags-holder']//li[contains(., '{$tag}')]",
+                    'Tag is not assigned to entity'
+                );
+            } else {
+                throw new \Exception("Tag field can't be found");
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param $tag
+     * @return $this
+     * @throws \Exception
+     */
+    public function setTag($tag)
+    {
+        if ($this->isElementPresent("//div[@id='s2id_orocrm_contact_form_tags_autocomplete']")) {
+            $tagsPath = $this->byXpath("//div[@id='s2id_orocrm_contact_form_tags_autocomplete']//input");
+            $tagsPath->click();
+            $tagsPath->value($tag);
+            $this->waitForAjax();
+            $this->assertElementPresent(
+                "//div[@id='select2-drop']//div[contains(., '{$tag}')]",
+                "Tag's autocoplete doesn't return entity"
+            );
+            $this->byXpath("//div[@id='select2-drop']//div[contains(., '{$tag}')]")->click();
+
+            return $this;
+        } else {
+            throw new \Exception("Tag field can't be found");
+        }
+    }
 }
