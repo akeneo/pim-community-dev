0ad0de05ae9580a99493f744311f11d1a99a8a79
Benoit Jacquemont <benoit@akeneo.com>
2013-09-20 14:45:41 +0200

    Remove useless function parameter

--- Bundle/GridBundle/Resources/public/js/datagrid/grid.js
+++ Bundle/GridBundle/Resources/public/js/datagrid/grid.js
@@ -131,7 +131,7 @@ function($, _, Backgrid, __, mediator, LoadingMask, GridHeader, GridBody, Toolba
             options.columns.unshift(this._getMassActionsColumn());
 
             this.loadingMask = this._createLoadingMask();
-            this.toolbar = this._createToolbar(this.toolbarOptions);
+            this.toolbar = this._createToolbar();
 
             Backgrid.Grid.prototype.initialize.apply(this, arguments);
 
