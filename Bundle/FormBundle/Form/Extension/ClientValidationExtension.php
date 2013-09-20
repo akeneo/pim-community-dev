827659eb1fd836cb7eb1a4838f2ed8cdc1327016
Dan Yasnyuk <danil@magecore.com>
2013-09-20 16:18:13 +0300

    BAP-1626: Customize multi-step setup forms - added "client_validation" form type option

---
+++ Bundle/FormBundle/Form/Extension/ClientValidationExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ClientValidationExtension extends AbstractTypeExtension
+{
+    /**
+     * Add the image_path option
+     *
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(array('client_validation'));
+    }
+
+    /**
+     * Pass the client validation flag to the view
+     *
+     * @param FormView      $view
+     * @param FormInterface $form
+     * @param array         $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        // set an "image_url" variable that will be available when rendering this field
+        $view->vars['client_validation'] = isset($options['client_validation'])
+            ? (bool) $options['client_validation']
+            : true;
+    }
+
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+}
