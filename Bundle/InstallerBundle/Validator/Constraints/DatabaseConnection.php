ce905613714a6a43ec400b0a3810931e96527461
Dan Yasnyuk <danil@magecore.com>
2013-09-20 16:46:22 +0300

    BAP-1626: Customize multi-step setup forms - database connection validator added

---
+++ Bundle/InstallerBundle/Validator/Constraints/DatabaseConnection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oro\Bundle\InstallerBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+class DatabaseConnection extends Constraint
+{
+    public $message = 'Unable to connect to database "%name%". Please check database connection parameters and user permissions.';
+}
