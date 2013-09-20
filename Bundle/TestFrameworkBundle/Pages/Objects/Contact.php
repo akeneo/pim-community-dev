2aa5b2628464235a2a3e598ad0570d32febda75e
Michael Banin <mbanin@magecore.com>
2013-09-20 13:21:20 +0300

    BAP-1648: Improove Contact object complexity - Refactor object Contact

--- Bundle/TestFrameworkBundle/Pages/Objects/Contact.php
+++ Bundle/TestFrameworkBundle/Pages/Objects/Contact.php
@@ -144,14 +144,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressTypes($values, $addressId = 0)
     {
+        $xpath = "//input[@name = 'orocrm_contact_form[addresses][{$addressId}][types][]'";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            foreach ($values as $type) {
-                $this->byXpath("//input[@name='orocrm_contact_address_form[types][]' and @value='{$type}']")->click();
-            }
-        } else {
-            foreach ($values as $type) {
-                $this->byXpath("//input[@name = 'orocrm_contact_form[addresses][{$addressId}][types][]' and @value = '{$type}']")->click();
-            }
+            $xpath = "//input[@name='orocrm_contact_address_form[types][]'";
+        }
+        foreach ($values as $type) {
+            $this->byXpath("{$xpath} and @value = '{$type}']")->click();
         }
 
         return $this;
@@ -159,13 +157,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressPrimary($value, $addressId = 0)
     {
+        $primary = "//input[@id='orocrm_contact_form_addresses_{$addressId}_primary']";
+        if ($this->isElementPresent("//div[@role='dialog']")) {
+            $primary = ("//input[@id='orocrm_contact_address_form_primary']");
+        }
         if ($value) {
-            if ($this->isElementPresent("//div[@role='dialog']")) {
-                $primary = $this->byId("orocrm_contact_address_form_primary");
-            } else {
-                $primary = $this->byId("orocrm_contact_form_addresses_{$addressId}_primary");
-            }
-            $primary->click();
+            $this->byXpath($primary)->click();
         }
 
         return $this;
@@ -191,13 +188,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressFirstName($value, $addressId = 0)
     {
+        $addressFirstName = "//input[@id='orocrm_contact_form_addresses_{$addressId}_firstName']";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $addressFirstName = $this->byId("orocrm_contact_address_form_firstName");
-        } else {
-            $addressFirstName = $this->byId("orocrm_contact_form_addresses_{$addressId}_firstName");
+            $addressFirstName = "//input[@id='orocrm_contact_address_form_firstName']";
         }
-        $addressFirstName->clear();
-        $addressFirstName->value($value);
+        $this->byXpath($addressFirstName)->clear();
+        $this->byXpath($addressFirstName)->value($value);
 
         return $this;
 
@@ -211,13 +207,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressLastName($value, $addressId = 0)
     {
+        $addressLastName = "//input[@id='orocrm_contact_form_addresses_{$addressId}_lastName']";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $addressLastName = $this->byId("orocrm_contact_address_form_lastName");
-        } else {
-            $addressLastName = $this->byId("orocrm_contact_form_addresses_{$addressId}_lastName");
+            $addressLastName = "//input[@id='orocrm_contact_address_form_lastName']";
         }
-        $addressLastName->clear();
-        $addressLastName->value($value);
+        $this->byXpath($addressLastName)->clear();
+        $this->byXpath($addressLastName)->value($value);
 
         return $this;
 
@@ -231,13 +226,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressStreet($value, $addressId = 0)
     {
+        $street = "//input[@id='orocrm_contact_form_addresses_{$addressId}_street']";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $street = $this->byId("orocrm_contact_address_form_street");
-        } else {
-            $street = $this->byId("orocrm_contact_form_addresses_{$addressId}_street");
+            $street = "//input[@id='orocrm_contact_address_form_street']";
         }
-        $street->clear();
-        $street->value($value);
+        $this->byXpath($street)->clear();
+        $this->byXpath($street)->value($value);
 
         return $this;
     }
@@ -250,13 +244,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressCity($value, $addressId = 0)
     {
+        $city = "//input[@id='orocrm_contact_form_addresses_{$addressId}_city']";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $city = $this->byId("orocrm_contact_address_form_city");
-        } else {
-            $city = $this->byId("orocrm_contact_form_addresses_{$addressId}_city");
+            $city = "//input[@id='orocrm_contact_address_form_city']";
         }
-        $city->clear();
-        $city->value($value);
+        $this->byXpath($city)->clear();
+        $this->byXpath($city)->value($value);
         return $this;
     }
 
@@ -268,13 +261,12 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressPostalCode($value, $addressId = 0)
     {
+        $zipcode = "//input[@id='orocrm_contact_form_addresses_{$addressId}_postalCode']";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $zipcode = $this->byId("orocrm_contact_address_form_postalCode");
-        } else {
-            $zipcode = $this->byId("orocrm_contact_form_addresses_{$addressId}_postalCode");
+            $zipcode = "//input[@id='orocrm_contact_address_form_postalCode']";
         }
-        $zipcode->clear();
-        $zipcode->value($value);
+        $this->byXpath($zipcode)->clear();
+        $this->byXpath($zipcode)->value($value);
         return $this;
     }
 
@@ -286,12 +278,11 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressCountry($value, $addressId = 0)
     {
+        $country = "//div[@id='s2id_orocrm_contact_form_addresses_{$addressId}_country']/a";
         if ($this->isElementPresent("//div[@role='dialog']")) {
-            $country = $this->byXpath("//div[@id='s2id_orocrm_contact_address_form_country']/a");
-        } else {
-            $country = $this->byXpath("//div[@id='s2id_orocrm_contact_form_addresses_{$addressId}_country']/a");
+            $country = "//div[@id='s2id_orocrm_contact_address_form_country']/a";
         }
-        $country->click();
+        $this->byXpath($country)->click();
         $this->waitForAjax();
         $this->byXpath("//div[@id='select2-drop']/div/input")->value($value);
         $this->waitForAjax();
@@ -309,37 +300,23 @@ class Contact extends AbstractEntity implements Entity
 
     public function setAddressState($state, $addressId = 0)
     {
-        $flag = 0;
-        if ($this->isElementPresent(
-            "orocrm_contact_form_addresses_{$addressId}_state_text"
-        ) && $this->byId(
-            "orocrm_contact_form_addresses_{$addressId}_state_text"
-        )->displayed()) {
-            $this->state = $this->byId("orocrm_contact_form_addresses_{$addressId}_state_text");
-        } elseif ($this->isElementPresent(
-            "orocrm_contact_address_form_state_text"
-        ) && $this->byId(
-            "orocrm_contact_address_form_state_text"
-        )->displayed()) {
-            $this->state = $this->byId("orocrm_contact_address_form_state_text");
-        } elseif ($this->isElementPresent("//div[@id='s2id_orocrm_contact_form_addresses_{$addressId}_state']/a")) {
-            $this->state = $this->byXpath("//div[@id='s2id_orocrm_contact_form_addresses_{$addressId}_state']/a");
-            $flag = 1;
-        } elseif ($this->isElementPresent("//div[@id='s2id_orocrm_contact_address_form_state']")) {
-            $this->state = $this->byXpath("//div[@id='s2id_orocrm_contact_address_form_state']/a");
-            $flag = 1;
+        //$xpath = "//input[@id='orocrm_contact_form_addresses_0_state_text']";
+        //if  ($this->isElementPresent("//div[@role='dialog']")) {
+        //    $xpath = "//input[@id='orocrm_contact_address_form_state_text']";
+        //}
+        //$this->byXpath($xpath)->clear();
+        //$this->byXpath($xpath)->value($state);
+
+        $xpath = "//div[@id='s2id_orocrm_contact_form_addresses_{$addressId}_state']/a";
+        if ($this->isElementPresent("//div[@role='dialog']")) {
+            $xpath = "//div[@id='s2id_orocrm_contact_address_form_state']/a";
         }
-        if ($flag == 1) {
-            $this->state->click();
-            $this->waitForAjax();
-            $this->byXpath("//div[@id='select2-drop']/div/input")->value($state);
-            $this->waitForAjax();
-            $this->assertElementPresent("//div[@id='select2-drop']//div[contains(., '{$state}')]", "Country's autocoplete doesn't return search value");
-            $this->byXpath("//div[@id='select2-drop']//div[contains(., '{$state}')]")->click();
-        } else {
-            $this->state->clear();
-            $this->state->value($state);
-        }
+        $this->byXpath($xpath)->click();
+        $this->waitForAjax();
+        $this->byXpath("//div[@id='select2-drop']/div/input")->value($state);
+        $this->waitForAjax();
+        $this->assertElementPresent("//div[@id='select2-drop']//div[contains(., '{$state}')]", "Country's autocopmlete doesn't return search value");
+        $this->byXpath("//div[@id='select2-drop']//div[contains(., '{$state}')]")->click();
 
         return $this;
     }
