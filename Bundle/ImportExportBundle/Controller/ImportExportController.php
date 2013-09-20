fd32e974ab350a33da62629e2ef3448c364bcf8d
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-09-20 19:16:22 +0300

    CRM-483: Create universal import/export controller  - updated controller  - added FileSystemOperator  - fixed templates

--- Bundle/ImportExportBundle/Controller/ImportExportController.php
+++ Bundle/ImportExportBundle/Controller/ImportExportController.php
@@ -7,9 +7,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\Routing\RouterInterface;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -20,6 +18,7 @@ use Oro\Bundle\BatchBundle\Entity\JobExecution;
 use Oro\Bundle\BatchBundle\Job\BatchStatus;
 use Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry;
 use Oro\Bundle\ImportExportBundle\Job\JobExecutor;
+use Oro\Bundle\ImportExportBundle\File\FileSystemOperator;
 
 class ImportExportController extends Controller
 {
@@ -52,17 +51,11 @@ class ImportExportController extends Controller
      */
     public function instantExportAction($processorAlias)
     {
-        /** @var ProcessorRegistry $processorRegistry */
-        $processorRegistry = $this->get('oro_importexport.processor.registry');
-        /** @var JobExecutor $jobExecutor */
-        $jobExecutor = $this->get('oro_importexport.job_executor');
-        /** @var TranslatorInterface $translator */
-        $translator = $this->get('translator');
-        /** @var RouterInterface $router */
-        $router = $this->get('router');
-
-        $fileName = $this->generateTmpFileName($processorAlias, 'csv');
-        $entityName = $processorRegistry->getProcessorEntityName(ProcessorRegistry::TYPE_EXPORT, $processorAlias);
+        $fileName = $this->getFileSystemOperator()->generateTemporaryFileName($processorAlias, 'csv');
+        $entityName = $this->getProcessorRegistry()->getProcessorEntityName(
+            ProcessorRegistry::TYPE_EXPORT,
+            $processorAlias
+        );
         $configuration = array(
             'export' => array(
                 'processorAlias' => $processorAlias,
@@ -74,21 +67,21 @@ class ImportExportController extends Controller
         $url = null;
         $messages = array();
 
-        $jobResult = $jobExecutor->executeJob(
+        $jobResult = $this->getJobExecutor()->executeJob(
             ProcessorRegistry::TYPE_EXPORT,
             JobExecutor::JOB_EXPORT_TO_CSV,
             $configuration
         );
 
         if ($jobResult->isSuccessful()) {
-            $url = $router->generate(
+            $url = $this->get('router')->generate(
                 'oro_importexport_export_download',
                 array('fileName' => basename($fileName))
             );
             foreach ($jobResult->getContexts() as $context) {
                 $messages[] = array(
                     'type' => 'success',
-                    'message' => $translator->trans(
+                    'message' => $this->get('translator')->trans(
                         'oro_importexport.export.exported_entities_count %count%',
                         array('%count%' => $context->getReadCount())
                     ),
@@ -102,7 +95,7 @@ class ImportExportController extends Controller
 
         return new JsonResponse(
             array(
-                'successful' => $jobResult->isSuccessful(),
+                'success' => $jobResult->isSuccessful(),
                 'url' => $url,
                 'messages' => $messages
             )
@@ -115,66 +108,35 @@ class ImportExportController extends Controller
      */
     public function downloadExportResultAction($fileName)
     {
-        $fullFileName = $this->getFullFileName($fileName);
+        $fullFileName = $this->getFileSystemOperator()->getTemporaryFile($fileName);
 
-        $response = new BinaryFileResponse($fullFileName, 200, array('Content-Type' => 'text/csv'));
+        $response = new BinaryFileResponse($fullFileName->getRealPath(), 200, array('Content-Type' => 'text/csv'));
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
 
         return $response;
     }
 
-    protected function generateTranslationLabel($type, $job, $alias)
+    /**
+     * @return ProcessorRegistry
+     */
+    protected function getProcessorRegistry()
     {
-        /** @var TranslatorInterface $translator */
-        $translator = $this->get('translator');
-        $actionId = sprintf('%s.%s.%s.%s', JobExecutor::CONNECTOR_NAME, $type, $job, $alias);
-
-        return $translator->trans($actionId);
+        return $this->get('oro_importexport.processor.registry');
     }
 
-    protected function getImportExportTmpDir()
+    /**
+     * @return JobExecutor
+     */
+    protected function getJobExecutor()
     {
-        $cacheDir = rtrim($this->container->getParameter("kernel.cache_dir"), DIRECTORY_SEPARATOR);
-        $importExportDir = $cacheDir . DIRECTORY_SEPARATOR . 'import_export';
-        if (!file_exists($importExportDir) && !is_dir($importExportDir)) {
-            mkdir($importExportDir);
-        }
-
-        if (!is_readable($importExportDir)) {
-            throw new \LogicException('Import/export directory is not readable');
-        }
-        if (!is_writable($importExportDir)) {
-            throw new \LogicException('Import/export directory is not writeable');
-        }
-
-        return $importExportDir;
+        return $this->get('oro_importexport.job_executor');
     }
 
-    protected function getFullFileName($fileName)
+    /**
+     * @return FileSystemOperator
+     */
+    protected function getFileSystemOperator()
     {
-        $importExportDir = $this->getImportExportTmpDir();
-        $fullFileName = $importExportDir . DIRECTORY_SEPARATOR . $fileName;
-        if (!file_exists($fullFileName) || !is_file($fullFileName) || !is_readable($fullFileName)) {
-            throw new \LogicException(sprintf('Can\'t read file %s', $fileName));
-        }
-
-        return $fullFileName;
-    }
-
-    protected function generateTmpFileName($filePrefix, $fileExtension = 'tmp')
-    {
-        $importExportDir = $this->getImportExportTmpDir();
-
-        do {
-            $fileName = sprintf(
-                '%s%s%s.%s',
-                $importExportDir,
-                DIRECTORY_SEPARATOR,
-                preg_replace('~\W~', '_', uniqid($filePrefix . '_', true)),
-                $fileExtension
-            );
-        } while (file_exists($fileName));
-
-        return $fileName;
+        return $this->get('oro_importexport.file.file_system_operator');
     }
 }
