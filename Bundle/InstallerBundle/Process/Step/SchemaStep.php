ea984457fc26c30c103fe18c244df89f39ea5f21
Dan Yasnyuk <danil@magecore.com>
2013-09-25 18:45:45 +0300

    BAP-1391: Installer - pre-release fixes

--- Bundle/InstallerBundle/Process/Step/SchemaStep.php
+++ Bundle/InstallerBundle/Process/Step/SchemaStep.php
@@ -2,6 +2,9 @@
 
 namespace Oro\Bundle\InstallerBundle\Process\Step;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+
 use Sylius\Bundle\FlowBundle\Process\Context\ProcessContextInterface;
 
 class SchemaStep extends AbstractStep
@@ -16,6 +19,16 @@ class SchemaStep extends AbstractStep
             ->runCommand('doctrine:fixtures:load', array('--no-interaction' => true))
             ->runCommand('oro:entity-config:init')
             ->runCommand('oro:entity-extend:init');
+
+        /**
+         * @todo Refactor isolate process execution
+         */
+        $finder  = new PhpExecutableFinder();
+        $kernel  = $this->get('kernel');
+        $console = escapeshellarg($finder->find()) . ' ' . escapeshellarg($kernel->getRootDir() . '/console');
+        $process = new Process($console . ' oro:entity-extend:init --env=' . $kernel->getEnvironment());
+
+        $process->run();
 
         return $this->complete();
     }
