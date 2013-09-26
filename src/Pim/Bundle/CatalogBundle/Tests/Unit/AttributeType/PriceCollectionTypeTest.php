47d1e39fe184e48c9e245119663b7d1c0affb349
Romain Monceau <romain@akeneo.com>
2013-09-26 13:28:45 +0200

    Add UT : Rename AttributeTypeTest by AttributeTypeTestCase and refactor it

--- src/Pim/Bundle/CatalogBundle/Tests/Unit/AttributeType/PriceCollectionTypeTest.php
+++ src/Pim/Bundle/CatalogBundle/Tests/Unit/AttributeType/PriceCollectionTypeTest.php
@@ -2,24 +2,32 @@
 
 namespace Pim\Bundle\CatalogBundle\Tests\Unit\AttributeType;
 
+use Oro\Bundle\FlexibleEntityBundle\AttributeType\AbstractAttributeType;
 use Pim\Bundle\CatalogBundle\AttributeType\PriceCollectionType;
 
 /**
+ * Test related class
+ *
  * @author    Gildas Quemener <gildas.quemener@gmail.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PriceCollectionTypeTest extends AttributeTypeTest
+class PriceCollectionTypeTest extends AttributeTypeTestCase
 {
     protected $name = 'pim_catalog_price_collection';
+    protected $backendType = AbstractAttributeType::BACKEND_TYPE_DECIMAL;
+    protected $formType = 'text';
 
-    public function setUp()
+    /**
+     * {@inheritdoc}
+     */
+    protected function createAttributeType()
     {
-        parent::setUp();
-
-        $currencyManager = $this->getMockBuilder('Pim\Bundle\CatalogBundle\Manager\CurrencyManager')
+        $currencyManager = $this
+            ->getMockBuilder('Pim\Bundle\CatalogBundle\Manager\CurrencyManager')
             ->disableOriginalConstructor()->getMock();
-        $this->target = new PriceCollectionType('decimal', 'text', $this->guesser, $currencyManager);
+
+        return new PriceCollectionType($this->backendType, $this->formType, $this->guesser, $currencyManager);
     }
 
     public function testBuildValueFormType()
@@ -38,39 +46,28 @@ class PriceCollectionTypeTest extends AttributeTypeTest
                 'foo',
                 'text',
                 'bar',
-                array('options'      => array('constraints' => array('constraints'),),
-                'label'        => null,
-                'required'     => null,
-                'type'         => 'pim_catalog_price',
-                'allow_add'    => true,
-                'allow_delete' => true,
-                'by_reference' => false,
-                'auto_initialize' => false
+                array(
+                    'options'      => array('constraints' => array('constraints')),
+                    'label'        => null,
+                    'required'     => null,
+                    'type'         => 'pim_catalog_price',
+                    'allow_add'    => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                    'auto_initialize' => false
                 )
             );
 
         $this->target->buildValueFormType($factory, $value);
     }
 
-    public function testGetBackendType()
-    {
-        $this->assertEquals('decimal', $this->target->getBackendType());
-    }
-
-    public function testGetFormType()
-    {
-        $this->assertEquals('text', $this->target->getFormType());
-    }
-
     public function testBuildAttributeFormTypes()
     {
-        $this->assertEquals(
+        $this->assertCount(
             9,
-            count(
-                $this->target->buildAttributeFormTypes(
-                    $this->getFormFactoryMock(),
-                    $this->getAttributeMock(null, null)
-                )
+            $this->target->buildAttributeFormTypes(
+                $this->getFormFactoryMock(),
+                $this->getAttributeMock(null, null)
             )
         );
     }
