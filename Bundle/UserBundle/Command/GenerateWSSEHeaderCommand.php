c9d2c5b092687b68fae82723e80548fea08f5b09
Dan Yasnyuk <danil@magecore.com>
2013-09-27 13:36:05 +0300

    Merge pull request #15 from liip-forks/add_wsse_header_generator_command
    - minor updates and code style fixes

--- Bundle/UserBundle/Command/GenerateWSSEHeaderCommand.php
+++ Bundle/UserBundle/Command/GenerateWSSEHeaderCommand.php
@@ -4,11 +4,8 @@ namespace Oro\Bundle\UserBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\Translation\MessageCatalogue;
 
 class GenerateWSSEHeaderCommand extends ContainerAwareCommand
 {
@@ -27,37 +24,40 @@ class GenerateWSSEHeaderCommand extends ContainerAwareCommand
     }
 
     /**
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param InputInterface  $input
+     * @param OutputInterface $output
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $username = $input->getArgument('username');
-        $em = $this->getContainer()->get('doctrine');
-        $repository =$em->getRepository('Oro\Bundle\UserBundle\Entity\User');
-        $user = $repository->findOneBy(array('username' => $username));
+        $user     = $this
+            ->getContainer()
+            ->get('oro_user.manager')
+            ->findUserByUsername($username);
 
         if (null === $user->getApi() || null === $user->getApi()->getApiKey()) {
             throw new \InvalidArgumentException(sprintf('User "%s" does not yet have an API key generated', $username));
         }
 
-        $created = new \DateTime();
-        $created = $created->format(DATE_ATOM);
+        $created = date('c');
 
         // http://stackoverflow.com/questions/18117695/how-to-calculate-wsse-nonce
-        $prefix = gethostname();
-        $nonce = base64_encode( substr( md5( uniqid( $prefix.'_', true)), 0, 16));
-        $passwordDigest = base64_encode(sha1(base64_decode($nonce).$created.$user->getApi()->getApiKey(), true));
+        $prefix         = gethostname();
+        $nonce          = base64_encode(substr(md5(uniqid($prefix . '_', true)), 0, 16));
+        $passwordDigest = base64_encode(sha1(base64_decode($nonce) . $created . $user->getApi()->getApiKey(), true));
 
+        $output->writeln('<info>To use WSSE authentication add following headers to the request:</info>');
+        $output->writeln('Authorization: WSSE profile="UsernameToken"');
         $output->writeln(
             sprintf(
-                'X-WSSE:UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"',
+                'X-WSSE: UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"',
                 $username,
                 $passwordDigest,
                 $nonce,
                 $created
             )
         );
+        $output->writeln('');
 
         return 0;
     }
