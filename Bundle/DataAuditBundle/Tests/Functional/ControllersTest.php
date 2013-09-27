9a927544e6998e27dcfc4c948d0e1974de252b1f
yurio <yurio@ua.fm>
2013-09-27 18:41:42 +0300

    task/BAP-1771: Fix tests

--- Bundle/DataAuditBundle/Tests/Functional/ControllersTest.php
+++ Bundle/DataAuditBundle/Tests/Functional/ControllersTest.php
@@ -51,7 +51,7 @@ class ControllersTest extends WebTestCase
         $form['oro_user_user_form[birthday]'] = $this->userData['birthday'];
         $form['oro_user_user_form[email]'] = $this->userData['email'];
         $form['oro_user_user_form[groups][1]'] = 2;
-        $form['oro_user_user_form[rolesCollection][2]'] = 4;
+        $form['oro_user_user_form[rolesCollection][1]'] = true;
         $form['oro_user_user_form[values][company][varchar]'] = $this->userData['company'];
         $form['oro_user_user_form[owner]'] = 1;
 
