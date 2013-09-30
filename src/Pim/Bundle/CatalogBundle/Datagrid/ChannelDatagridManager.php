d9bc9d0b3ffc9cdfbca9997e6889165aa68e02c8
Romain Monceau <romain@akeneo.com>
2013-09-30 17:21:38 +0200

    PIM-1246 : Reorder use statements

--- src/Pim/Bundle/CatalogBundle/Datagrid/ChannelDatagridManager.php
+++ src/Pim/Bundle/CatalogBundle/Datagrid/ChannelDatagridManager.php
@@ -2,14 +2,13 @@
 
 namespace Pim\Bundle\CatalogBundle\Datagrid;
 
-use Oro\Bundle\GridBundle\Property\TwigTemplateProperty;
-
 use Oro\Bundle\GridBundle\Action\ActionInterface;
 use Oro\Bundle\GridBundle\Datagrid\DatagridManager;
 use Oro\Bundle\GridBundle\Field\FieldDescription;
 use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
 use Oro\Bundle\GridBundle\Field\FieldDescriptionInterface;
 use Oro\Bundle\GridBundle\Filter\FilterInterface;
+use Oro\Bundle\GridBundle\Property\TwigTemplateProperty;
 use Oro\Bundle\GridBundle\Property\UrlProperty;
 use Oro\Bundle\GridBundle\Datagrid\ProxyQueryInterface;
 
