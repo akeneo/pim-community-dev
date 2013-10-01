47501e2cfafc03656702711320765a842bb6cb27
Filips Alpe <filips@akeneo.com>
2013-10-01 17:30:00 +0200

    Clean up AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -41,38 +41,7 @@ class AppKernel extends Kernel
             // PIM deps
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
 
-            // BAP bundles
-/*
-            new Oro\Bundle\FlexibleEntityBundle\OroFlexibleEntityBundle(),
-            new Oro\Bundle\UIBundle\OroUIBundle(),
-            new Oro\Bundle\FormBundle\OroFormBundle(),
-            new Oro\Bundle\JsFormValidationBundle\OroJsFormValidationBundle(),
-            new Oro\Bundle\SoapBundle\OroSoapBundle(),
-            new Oro\Bundle\SearchBundle\OroSearchBundle(),
-            new Oro\Bundle\UserBundle\OroUserBundle(),
-            new Oro\Bundle\MeasureBundle\OroMeasureBundle(),
-            new Oro\Bundle\SegmentationTreeBundle\OroSegmentationTreeBundle(),
-            new Oro\Bundle\NavigationBundle\OroNavigationBundle(),
-            new Oro\Bundle\ConfigBundle\OroConfigBundle(),
-            new Oro\Bundle\FilterBundle\OroFilterBundle(),
-            new Oro\Bundle\GridBundle\OroGridBundle(),
-            new Oro\Bundle\WindowsBundle\OroWindowsBundle(),
-            // new Oro\Bundle\AddressBundle\OroAddressBundle(),
-            new Oro\Bundle\DataAuditBundle\OroDataAuditBundle(),
-            new Oro\Bundle\TagBundle\OroTagBundle(),
-            new Oro\Bundle\AsseticBundle\OroAsseticBundle(),
-            new Oro\Bundle\TranslationBundle\OroTranslationBundle(),
-            new Oro\Bundle\OrganizationBundle\OroOrganizationBundle(),
-            new Oro\Bundle\NotificationBundle\OroNotificationBundle($this),
-            new Oro\Bundle\EmailBundle\OroEmailBundle(),
-            new Oro\Bundle\EntityBundle\OroEntityBundle(),
-            new Oro\Bundle\EntityConfigBundle\OroEntityConfigBundle(),
-            new Oro\Bundle\EntityExtendBundle\OroEntityExtendBundle(),
-            new Oro\Bundle\ImapBundle\OroImapBundle(),
-            new Oro\Bundle\CronBundle\OroCronBundle(),
-            new Oro\Bundle\BatchBundle\OroBatchBundle(),*/
-
-            // BAP overrided bundles
+            // BAP overriden bundles
             new Pim\Bundle\NavigationBundle\PimNavigationBundle(),
             new Pim\Bundle\FilterBundle\PimFilterBundle(),
             new Pim\Bundle\GridBundle\PimGridBundle(),
