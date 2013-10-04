176978ca2dd0906e28206989932cd1f232241fc7
Nicolas Dupont <nicolas@akeneo.com>
2013-10-04 20:49:18 +0200

    override loggable manager to disable audit

--- src/Pim/Bundle/DataAuditBundle/Loggable/LoggableManager.php
+++ src/Pim/Bundle/DataAuditBundle/Loggable/LoggableManager.php
@@ -3,13 +3,31 @@
 namespace Pim\Bundle\DataAuditBundle\Loggable;
 
 use Oro\Bundle\DataAuditBundle\Loggable\LoggableManager as OroLoggableManager;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
+
 use Doctrine\ORM\EntityManager;
 
 /**
  * Override loggable manager
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class LoggableManager extends OroLoggableManager
 {
+    /**
+     * @param                     $logEntityClass
+     * @param ConfigProvider      $auditConfigProvider
+     */
+    public function __construct(
+        $logEntityClass,
+        ConfigProvider $auditConfigProvider
+    ) {
+        $this->auditConfigProvider = $auditConfigProvider;
+        $this->logEntityClass      = $logEntityClass;
+    }
+
     /**
      * @param EntityManager $em
      */
@@ -20,7 +38,7 @@ class LoggableManager extends OroLoggableManager
 
         $collections = array_merge($uow->getScheduledCollectionUpdates(), $uow->getScheduledCollectionDeletions());
         foreach ($collections as $collection) {
-            if (!$this->isSkipped($entity)) {
+            if (!$this->isSkipped($collection)) {
                 $this->calculateCollectionData($collection);
             }
         }
@@ -42,7 +60,7 @@ class LoggableManager extends OroLoggableManager
     }
 
     /**
-     * Check if entity must be skipped
+     * Check if entity must be skipped, disable it for PIM entities
      *
      * @param object $entity
      *
@@ -51,9 +69,11 @@ class LoggableManager extends OroLoggableManager
     protected function isSkipped($entity)
     {
         $className = get_class($entity);
-        if (strpos($className, 'Pim') === 1) {
-die('SKIP');
+        if (strpos($className, 'Pim') !== 1) {
+
+            return true;
         }
 
+        return false;
     }
 }
