9fa45dac8af49d0e4d9b51361abff12f05e34352
Filips Alpe <filips@akeneo.com>
2013-10-10 18:20:17 +0200

    Rename variant group axis constraint

--- src/Pim/Bundle/CatalogBundle/Validator/Constraints/UniqueVariantAxis.php
+++ src/Pim/Bundle/CatalogBundle/Validator/Constraints/UniqueVariantAxis.php
@@ -5,13 +5,13 @@ namespace Pim\Bundle\CatalogBundle\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 
 /**
- * Constraint for valid variant group axis values
+ * Constraint for unique variant group axis values
  *
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class VariantGroupMatrix extends Constraint
+class UniqueVariantAxis extends Constraint
 {
     /**
      * Violation message for already existing axis combination
@@ -25,7 +25,7 @@ class VariantGroupMatrix extends Constraint
      */
     public function validatedBy()
     {
-        return 'pim_variant_group_matrix_validator';
+        return 'pim_unique_variant_axis_validator';
     }
 
     /**
--- src/Pim/Bundle/CatalogBundle/Validator/Constraints/UniqueVariantAxisValidator.php
+++ src/Pim/Bundle/CatalogBundle/Validator/Constraints/UniqueVariantAxisValidator.php
@@ -9,13 +9,13 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
 use Pim\Bundle\CatalogBundle\Entity\VariantGroup;
 
 /**
- * Validator for valid variant group axis values constraint
+ * Validator for unique variant group axis values constraint
  *
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class VariantGroupMatrixValidator extends ConstraintValidator
+class UniqueVariantAxisValidator extends ConstraintValidator
 {
     /**
      * @var ProductManager $manager
