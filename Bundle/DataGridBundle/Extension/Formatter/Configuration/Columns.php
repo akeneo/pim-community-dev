657827f56e9614abdc586fb008ef112b05d19399
Ivan Shakuta <ishakuta@gmail.com>
2013-10-10 16:32:58 +0000

    BAP-1837: Implement tree validation - validation params

--- Bundle/DataGridBundle/Extension/Formatter/Configuration/Columns.php
+++ Bundle/DataGridBundle/Extension/Formatter/Configuration/Columns.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\DataGridBundle\Extension\Formatter\Configuration;
 
+use Oro\Bundle\ImportExportBundle\Exception\InvalidConfigurationException;
+
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -18,11 +20,52 @@ class Columns implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('columns')
+                    ->children()
+                    ->end()
                 ->end()
                 ->arrayNode('properties')
+                    ->children()
+                        ->arrayNode('update_link')
+                            ->append($this->getLinkProperty('update_link'))
+                        ->end()
+                        ->arrayNode('delete_link')
+                            ->append($this->getLinkProperty('delete_link'))
+                        ->end()
+                    ->end()
                 ->end()
             ->end();
 
         return $builder;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @throws InvalidConfigurationException
+     */
+    public function getLinkProperty($name)
+    {
+        if (!in_array($name, array('update_link', 'delete_link'))) {
+            throw new InvalidConfigurationException(sprintf('Invalid property type "%s"', $name));
+        }
+
+        $builder = new TreeBuilder();
+
+        return $builder->root($name)
+            ->requiresAtLeastOneElement()
+            ->prototype('scalar')
+                ->children()
+                    ->scalarNode('type')
+                        ->isRequired()
+                        ->cannotBeEmpty()
+                    ->end()
+                    ->scalarNode('route')
+                        ->isRequired()
+                        ->cannotBeEmpty()
+                    ->end()
+                    ->arrayNode('params')
+                    ->end()
+                ->end()
+            ->end();
     }
 }
