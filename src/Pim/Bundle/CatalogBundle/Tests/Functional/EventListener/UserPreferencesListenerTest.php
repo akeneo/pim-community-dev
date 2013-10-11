b665de7a3685f6dbccf1b51ba32a5dd13d65278e
Romain Monceau <romain@akeneo.com>
2013-10-11 11:32:10 +0200

    Clean up on UserPreferencesListenerTest

--- src/Pim/Bundle/CatalogBundle/Tests/Functional/EventListener/UserPreferencesListenerTest.php
+++ src/Pim/Bundle/CatalogBundle/Tests/Functional/EventListener/UserPreferencesListenerTest.php
@@ -3,10 +3,10 @@
 namespace Pim\Bundle\CatalogBundle\Tests\Functional\EventListener;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Oro\Bundle\UserBundle\Entity\UserManager;
-use Pim\Bundle\CatalogBundle\Entity\Channel;
 use Doctrine\ORM\EntityManager;
+use Oro\Bundle\UserBundle\Entity\UserManager;
 use Oro\Bundle\UserBundle\Entity\User;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
 
 /**
  * Test related class
@@ -20,11 +20,12 @@ class UserPreferencesListenerTest extends WebTestCase
     /**
      * @var UserManager
      */
-    private $userManager;
+    protected $userManager;
+
     /**
      * @var EntityManager
      */
-    private $entityManager;
+    protected $entityManager;
 
     /**
      * {@inheritdoc}
@@ -70,7 +71,7 @@ class UserPreferencesListenerTest extends WebTestCase
             ->setUsername($prefix)
             ->setEmail($prefix . '@test.com')
             ->setPassword($prefix);
-        $value = $user->setCatalogscope($removedOption);
+        $user->setCatalogscope($removedOption);
         $this->userManager->updateUser($user);
         $this->entityManager->flush();
 
