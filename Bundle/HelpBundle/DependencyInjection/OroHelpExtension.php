9cccb25b2e17a170bcf5ab21d424f2f34ea579a6
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-10-11 18:13:28 +0300

    BAP-1604: Create Extension Class - updated OroHelpExtension - read configuration for help link provider

--- Bundle/HelpBundle/DependencyInjection/OroHelpExtension.php
+++ Bundle/HelpBundle/DependencyInjection/OroHelpExtension.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\HelpBundle\DependencyInjection;
 
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -9,6 +11,8 @@ use Symfony\Component\Config\FileLocator;
 
 class OroHelpExtension extends Extension
 {
+    const BUNDLE_CONFIG_FILE = 'oro_help.yml';
+
     /**
      * {@inheritDoc}
      */
@@ -16,5 +20,46 @@ class OroHelpExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->setConfigurationToLinkProvider($configs, $container);
+    }
+
+    /**
+     * Reads configuration from all bundles and from the application and injects to oro_help.twig.help_link_provider
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @return array
+     */
+    protected function setConfigurationToLinkProvider(array $configs, ContainerBuilder $container)
+    {
+        $applicationConfig = $this->processConfiguration(new ApplicationConfiguration(), $configs);
+        $bundleConfig = $this->processConfiguration(new BundleConfiguration(), $this->getBundleConfigs($container));
+
+        $configuration = array_merge_recursive($bundleConfig, $applicationConfig);
+
+        $linkProvider = $container->getDefinition('oro_help.twig.help_link_provider');
+        $linkProvider->addMethodCall('setConfiguration', $configuration);
+    }
+
+    /**
+     * Get a list of configs from all bundles
+     *
+     * @param ContainerBuilder $container
+     * @return array
+     */
+    protected function getBundleConfigs(ContainerBuilder $container)
+    {
+        $result = array();
+        foreach ($container->getParameter('kernel.bundles') as $bundle) {
+            $reflection = new \ReflectionClass($bundle);
+            $file = dirname($reflection->getFilename()) . '/Resources/config/' . self::BUNDLE_CONFIG_FILE;
+            if (is_file($file)) {
+                $file = realpath($file);
+                $result[] = Yaml::parse($file);
+                $container->addResource(new FileResource($file));
+            }
+        }
+        return $result;
     }
 }
---
+++ Bundle/HelpBundle/Tests/Unit/DependencyInjection/OroHelpExtensionTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Oro\Bundle\TranslationBundle\Tests\Unit\DependencyInjection;
+
+use Oro\Bundle\HelpBundle\DependencyInjection\OroHelpExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
+class OroHelpExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var OroHelpExtension
+     */
+    protected $extension;
+
+    protected function setUp()
+    {
+        $this->extension = new OroHelpExtension();
+    }
+
+    /**
+     * @dataProvider loadConfigurationDataProvider
+     */
+    public function testLoadConfiguration(array $configs, array $bundles, array $expectedConfiguration)
+    {
+        $container = $this->createContainer($bundles);
+
+        $this->extension->load(
+            $configs,
+            $container
+        );
+
+        $this->assertTrue($container->hasDefinition('oro_help.twig.help_link_provider'));
+        $linkProvider = $container->getDefinition('oro_help.twig.help_link_provider');
+        $this->assertEquals(
+            array(
+                array(
+                    'setConfiguration',
+                    $expectedConfiguration
+                )
+            ),
+            $linkProvider->getMethodCalls()
+        );
+    }
+
+    public function loadConfigurationDataProvider()
+    {
+        return array(
+            'without_bundles' => array(
+                'configs' => array(
+                    'oro_help' => array(
+                        'defaults' => array(
+                            'server' => 'http://server.com'
+                        )
+                    )
+                ),
+                'bundles' => array(),
+                'expectedConfiguration' => array(
+                    'defaults' => array(
+                        'server' => 'http://server.com',
+                        'vendors' => array(),
+                    ),
+                    'resources' => array(),
+                )
+            ),
+            'with_bundles' => array(
+                'configs' => array(
+                    'oro_help' => array(
+                        'defaults' => array(
+                            'server' => 'http://server.com'
+                        )
+                    )
+                ),
+                'bundles' => array(
+                    'Oro\Bundle\HelpBundle\Tests\Unit\DependencyInjection\Fixtures\BarBundle\BarBundle',
+                    'Oro\Bundle\HelpBundle\Tests\Unit\DependencyInjection\Fixtures\FooBundle\FooBundle',
+                ),
+                'expectedConfiguration' => array(
+                    'defaults' => array(
+                        'server' => 'http://server.com',
+                        'vendors' => array(),
+                    ),
+                    'resources' => array(
+                        'FooBundle' => array(
+                            'alias' => 'Foo'
+                        ),
+                        'BarBundle' => array(
+                            'alias' => 'BarOverride'
+                        ),
+                        'BarBundle:Bar' => array(
+                            'alias' => 'BarBundleBar'
+                        ),
+                        'BarBundle:Bar:foo' => array(
+                            'server' => 'http://server.com/',
+                            'prefix' => 'baz/prefix',
+                            'alias' => 'bar/alias',
+                            'uri' => 'bar/uri',
+                            'link' => 'http://server.com/foo/custom',
+                        )
+                    )
+                )
+            ),
+        );
+    }
+
+    protected function createContainer(array $bundles)
+    {
+        $container = new ContainerBuilder(
+            new ParameterBag(
+                array(
+                    'kernel.bundles'=> $bundles,
+                )
+            )
+        );
+
+        return $container;
+    }
+}
