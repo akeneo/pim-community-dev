4f750d8cd526bde036fbe9dc1e912d68ba02e36a
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-10-14 11:28:49 +0300

    Fixed BAP-1906: Comment tags breaks js-templates rendering
     - fixed Unit Tests

--- Bundle/UIBundle/Tests/Unit/Twig/Template/TestJs.php
+++ Bundle/UIBundle/Tests/Unit/Twig/Template/TestJs.php
@@ -4,15 +4,15 @@ namespace Oro\Bundle\UIBundle\Tests\Unit\Twig\Template;
 
 use Oro\Bundle\UIBundle\Twig\Template;
 
-class TestJSON extends Template
+class TestJS extends Template
 {
     public function getTemplateName()
     {
-        return 'data.json.twig';
+        return 'app.js.twig';
     }
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        echo json_encode(array('content' => "<p>test</p>\n"));
+        echo 'console.log("test")';
     }
 }
