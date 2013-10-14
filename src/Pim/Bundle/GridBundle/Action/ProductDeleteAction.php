33ba7e3979f1aba09a5fd730cc2bb0881d691d78
Antoine Guigan <antoine@akeneo.com>
2013-10-14 17:29:12 +0200

    Fixed PIM-1251

---
+++ src/Pim/Bundle/GridBundle/Action/ProductDeleteAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pim\Bundle\GridBundle\Action;
+
+use Oro\Bundle\GridBundle\Action\DeleteAction;
+
+/**
+ * Extends delete action to treat special case of products
+ * 
+ * @author    Antoine Guigan <antoine@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductDeleteAction extends DeleteAction implements ActionInterface
+{
+    /**
+     * @var string
+     */
+    protected $type = self::TYPE_PRODUCT_DELETE;
+}
