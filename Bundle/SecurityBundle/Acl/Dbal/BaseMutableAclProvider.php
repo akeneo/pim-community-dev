505a583cc859d498c2471496b228079a633ddc81
yurio <yurio@ua.fm>
2013-10-14 19:59:18 +0300

    BAP-1419: Fix codestyles

--- Bundle/SecurityBundle/Acl/Dbal/BaseMutableAclProvider.php
+++ Bundle/SecurityBundle/Acl/Dbal/BaseMutableAclProvider.php
@@ -560,14 +560,16 @@ QUERY;
             'SELECT id FROM %s WHERE class_id = %d AND %s AND %s AND ace_order = %d',
             $this->options['entry_table_name'],
             $classId,
-            null === $oid ?
+            (null === $oid ?
                 $this->connection->getDatabasePlatform()
                     ->getIsNullExpression('object_identity_id')
-                : 'object_identity_id = '.intval($oid),
-            null === $field ?
+                : 'object_identity_id = '.intval($oid)
+            ),
+            (null === $field ?
                 $this->connection->getDatabasePlatform()
                     ->getIsNullExpression('field_name')
-                : 'field_name = '.$this->connection->quote($field),
+                : 'field_name = '.$this->connection->quote($field)
+            ),
             $order
         );
     }
