fccc3997a159c8172d49b260cf4874fbaa7c6cd3
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-10-15 13:24:22 +0300

    Implemented BAP-1353: Update Backgrid lib to the latest version
     - removed "edit", "editing", "edited" cell events in favor of Backgrid events "backgrid:edit" "backgrid:editing" and "backgrid:edited" triggered on model
     - removed a chain of "cellEdited" events
     - oro.datagrid.BooleanCell functional has been re-factored

--- Bundle/GridBundle/Resources/public/js/datagrid/cell/integer-cell.js
+++ Bundle/GridBundle/Resources/public/js/datagrid/cell/integer-cell.js
@@ -6,11 +6,6 @@ function(Backgrid) {
     /**
      * Integer column cell. Added missing behaviour.
      *
-     * Triggers events:
-     *  - "edit" when a cell is entering edit mode and an editor
-     *  - "editing" when a cell has finished switching to edit mode
-     *  - "edited" when cell editing is finished
-     *
      * @export  oro/datagrid/integer-cell
      * @class   oro.datagrid.IntegerCell
      * @extends Backgrid.IntegerCell
@@ -24,16 +19,6 @@ function(Backgrid) {
                 e.stopPropagation();
             }
             return Backgrid.IntegerCell.prototype.enterEditMode.apply(this, arguments);
-        },
-
-        /**
-         * @inheritDoc
-         */
-        exitEditMode: function (e) {
-            if (this.column.get("editable")) {
-                this.trigger("edited", this);
-            }
-            return Backgrid.IntegerCell.prototype.exitEditMode.apply(this, arguments);
         }
     });
 });
