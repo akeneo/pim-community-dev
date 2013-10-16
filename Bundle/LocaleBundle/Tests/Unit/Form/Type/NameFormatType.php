346a938b9f09adba4b7c18675f06f7001250b18b
Dmitry Khrysev <x86demon@gmail.com>
2013-10-16 13:05:51 +0300

    BAP-1887: Update OroLocalBundle System Configuration - added name format form type

---
+++ Bundle/LocaleBundle/Tests/Unit/Form/Type/NameFormatType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Tests\Unit\Form\Type;
+
+class NameFormatTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFormType()
+    {
+        $provider = $this->getMockBuilder('')
+    }
+}
