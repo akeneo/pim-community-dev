02f9bcb3f92e219e80df655d16aeb8eb202a39d8
Nicolas Dupont <nicolas@akeneo.com>
2013-10-16 10:51:48 +0200

    PIM-1278 : add group controller with index action

---
+++ src/Pim/Bundle/CatalogBundle/Controller/GroupController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Controller;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ValidatorInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use Pim\Bundle\CatalogBundle\AbstractController\AbstractDoctrineController;
+use Pim\Bundle\CatalogBundle\Datagrid\DatagridWorkerInterface;
+use Pim\Bundle\CatalogBundle\Entity\Group;
+
+/**
+ * Group controller
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GroupController extends AbstractDoctrineController
+{
+    /**
+     * @var DatagridWorkerInterface
+     */
+    protected $datagridWorker;
+
+    /**
+     * Constructor
+     *
+     * @param Request                  $request
+     * @param EngineInterface          $templating
+     * @param RouterInterface          $router
+     * @param SecurityContextInterface $securityContext
+     * @param FormFactoryInterface     $formFactory
+     * @param ValidatorInterface       $validator
+     * @param TranslatorInterface      $translator
+     * @param RegistryInterface        $doctrine
+     * @param DatagridWorkerInterface  $datagridWorker
+     */
+    public function __construct(
+        Request $request,
+        EngineInterface $templating,
+        RouterInterface $router,
+        SecurityContextInterface $securityContext,
+        FormFactoryInterface $formFactory,
+        ValidatorInterface $validator,
+        TranslatorInterface $translator,
+        RegistryInterface $doctrine,
+        DatagridWorkerInterface $datagridWorker
+    ) {
+        parent::__construct(
+            $request,
+            $templating,
+            $router,
+            $securityContext,
+            $formFactory,
+            $validator,
+            $translator,
+            $doctrine
+        );
+
+        $this->datagridWorker = $datagridWorker;
+    }
+
+    /**
+     * List groups
+     *
+     * @param Request $request
+     *
+     * @Template
+     * @AclAncestor("pim_catalog_group_index")
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $this->getManager()->createQueryBuilder();
+        $queryBuilder
+            ->select('g')
+            ->from('PimCatalogBundle:Group', 'g');
+        $datagrid = $this->datagridWorker->getDatagrid('group', $queryBuilder);
+
+
+        $view = ('json' === $request->getRequestFormat())
+            ? 'OroGridBundle:Datagrid:list.json.php'
+            : 'PimCatalogBundle:Group:index.html.twig';
+
+        return $this->render($view, array('datagrid' => $datagrid->createView()));
+    }
+}
