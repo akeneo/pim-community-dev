c6b82d90c497beea810d5a2e9d772cd9765fdea5
Vova Soroka <vsoroka@magecore.com>
2013-10-16 13:05:34 +0300

    BAP-1829: Local Calendar

--- Bundle/PlatformBundle/OroPlatformBundle.php
+++ Bundle/PlatformBundle/OroPlatformBundle.php
@@ -48,6 +48,7 @@ class OroPlatformBundle extends Bundle
             new \Oro\Bundle\BatchBundle\OroBatchBundle(),
             new \Oro\Bundle\ImportExportBundle\OroImportExportBundle(),
             new \Oro\Bundle\RequireJSBundle\OroRequireJSBundle(),
+            new \Oro\Bundle\CalendarBundle\OroCalendarBundle(),
         );
     }
 }
