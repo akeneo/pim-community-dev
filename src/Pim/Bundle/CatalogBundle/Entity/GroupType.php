ccd1172ffeb83072bd8c43da9f7e4eb4abdbd737
Nicolas Dupont <nicolas@akeneo.com>
2013-10-17 21:08:19 +0200

    PIM-1348 : add group type repository and a method to retrieve types by used group entity

--- src/Pim/Bundle/CatalogBundle/Entity/GroupType.php
+++ src/Pim/Bundle/CatalogBundle/Entity/GroupType.php
@@ -16,7 +16,7 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Pim\Bundle\CatalogBundle\Entity\Repository\GroupTypeRepository")
  * @ORM\Table(name="pim_catalog_group_type")
  * @ORM\DiscriminatorColumn(name="discr", type="string")
  * @Config(
