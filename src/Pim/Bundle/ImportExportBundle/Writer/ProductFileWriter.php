653645a7d15d3292a1b0d040da5c5ab1642f3f07
Gildas Quéméner <gildas.quemener@gmail.com>
2013-10-17 17:02:37 +0200

    PIM-1008 Created a product file writer
    It writes products file and copy their media

---
+++ src/Pim/Bundle/ImportExportBundle/Writer/ProductFileWriter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Pim\Bundle\ImportExportBundle\Writer;
+
+use Pim\Bundle\ImportExportBundle\Writer\FileWriter;
+use Pim\Bundle\CatalogBundle\Manager\MediaManager;
+
+/**
+ * Product file writer
+ *
+ * This writer is specialized in writing product file
+ *
+ * @author    Gildas Quemener <gildas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductFileWriter extends FileWriter
+{
+    /** @var MediaManager */
+    protected $mediaManager;
+
+    /**
+     * Constructor
+     *
+     * @param MediaManager
+     */
+    public function __construct(MediaManager $mediaManager)
+    {
+        $this->mediaManager = $mediaManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(array $data)
+    {
+        parent::write($data['entries']);
+
+        foreach ($data['media'] as $media) {
+            $this->mediaManager->copy($media, $this->directoryName);
+        }
+    }
+}
