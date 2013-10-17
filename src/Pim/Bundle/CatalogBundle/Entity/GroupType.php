89783327cda08147ffabb88523c81ae8ccb086bd
Nicolas Dupont <nicolas@akeneo.com>
2013-10-17 19:01:23 +0200

    PIM-1300 : add group type

---
+++ src/Pim/Bundle/CatalogBundle/Entity/GroupType.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Pim\Bundle\TranslationBundle\Entity\AbstractTranslation;
+use Pim\Bundle\TranslationBundle\Entity\TranslatableInterface;
+use Pim\Bundle\CatalogBundle\Model\ProductInterface;
+
+/**
+ * Group type entity
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="pim_catalog_group_type")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @Config(
+ *  defaultValues={
+ *      "entity"={"label"="Group type", "plural_label"="Group types"},
+ *      "security"={
+ *          "type"="ACL",
+ *          "group_name"=""
+ *      }
+ *  }
+ * )
+ */
+class GroupType
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string $code
+     *
+     * @ORM\Column(name="code", type="string", length=50, unique=true)
+     */
+    protected $code;
+
+    /**
+     * @var string $entity
+     *
+     * @ORM\Column(name="entity", type="string", length=255)
+     */
+    protected $entity;
+
+    /**
+     * Get the id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string $code
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return GroupType
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get entity
+     *
+     * @return string $entity
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set entity
+     *
+     * @param string $entity
+     *
+     * @return GroupType
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    /**
+     * Returns the code
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getCode();
+    }
+}
+
