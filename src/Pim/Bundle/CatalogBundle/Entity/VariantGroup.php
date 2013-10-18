90241bb5e52d49e3608dfeadf5b51cf3d43e0e06
Nicolas Dupont <nicolas@akeneo.com>
2013-10-18 18:25:18 +0200

    PIM-1393 : remove STA

--- src/Pim/Bundle/CatalogBundle/Entity/VariantGroup.php
+++ src/Pim/Bundle/CatalogBundle/Entity/VariantGroup.php
@@ -17,6 +17,7 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @ORM\Entity
+ * @ORM\Table(name="pim_catalog_variant_group")
  * @Config(
  *  defaultValues={
  *      "entity"={"label"="Variant group", "plural_label"="Variant groups"},
