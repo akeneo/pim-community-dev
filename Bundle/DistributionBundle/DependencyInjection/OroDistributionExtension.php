bd42fb645ac5ed82cbb0bb7c868badd2843ab4fd
Dan Yasnyuk <danil@magecore.com>
2013-10-18 13:45:13 +0300

    BAP-1538: Routes declaration on Bundles level - added "automatic" route loader

---
+++ Bundle/DistributionBundle/DependencyInjection/OroDistributionExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oro\Bundle\CronBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class OroCronExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->setParameter('oro_cron.max_jobs', (int) $config['max_concurrent_jobs']);
+        $container->setParameter('oro_cron.jms_statistics', $config['jms_statistics']);
+    }
+}
