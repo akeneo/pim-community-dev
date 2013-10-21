119cc4a7c97b24321eaa7c93731a11551a5adb8b
Vova Soroka <vsoroka@magecore.com>
2013-10-21 09:42:00 +0300

    BAP-1829: Local Calendar

---
+++ Bundle/CalendarBundle/Tests/Unit/EventListener/EntitySubscriberTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Tests\Unit\EventListener;
+
+use Oro\Bundle\CalendarBundle\Entity\Calendar;
+use Oro\Bundle\CalendarBundle\Entity\CalendarConnection;
+use Oro\Bundle\CalendarBundle\EventListener\EntitySubscriber;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Oro\Bundle\UserBundle\Entity\User;
+
+class EntitySubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOnFlush()
+    {
+        $subscriber = new EntitySubscriber();
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $uow = $this->getMockBuilder('\Doctrine\ORM\UnitOfWork')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $args = new OnFlushEventArgs($em);
+
+        $user = new User();
+        $newCalendar = new Calendar();
+        $newCalendar->setOwner($user);
+        $newCalendar->addConnection(new CalendarConnection($newCalendar));
+
+        $em->expects($this->once())
+            ->method('getUnitOfWork')
+            ->will($this->returnValue($uow));
+        $uow->expects($this->once())
+            ->method('getScheduledEntityInsertions')
+            ->will($this->returnValue(array($user)));
+        $em->expects($this->once())
+            ->method('persist')
+            ->with($this->equalTo($newCalendar));
+        $uow->expects($this->once())
+            ->method('computeChangeSets');
+
+        $subscriber->onFlush($args);
+    }
+}
