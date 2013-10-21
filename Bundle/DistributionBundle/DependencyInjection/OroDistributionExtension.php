6192a28413c77e3fa46d171e8859e41075c1c652
Dan Yasnyuk <danil@magecore.com>
2013-10-21 18:20:53 +0300

    BAP-1924: Remove bundle dependencies on application - added hardcoded replacement for Assetic and Twig bundles config

--- Bundle/DistributionBundle/DependencyInjection/OroDistributionExtension.php
+++ Bundle/DistributionBundle/DependencyInjection/OroDistributionExtension.php
@@ -20,6 +20,34 @@ class OroDistributionExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('services.yml');
+
+        $bundles = array_merge($container->getParameter('assetic.bundles'), array(
+            'OroAsseticBundle',
+            'OroAddressBundle',
+            'OroUIBundle',
+            'OroUserBundle',
+            'OroGridBundle',
+            'OroFilterBundle',
+            'OroNavigationBundle',
+            'OroWindowsBundle',
+            'OroSegmentationTreeBundle',
+            'OroEntityExtendBundle',
+            'OroSecurityBundle',
+            'JDareClankBundle',
+        ));
+
+        $templates = array_merge($container->getParameter('twig.form.resources'), array(
+            'OroUIBundle:Form:fields.html.twig',
+            'OroFormBundle:Form:fields.html.twig',
+            'OroTranslationBundle:Form:fields.html.twig',
+            'OroAddressBundle:Include:fields.html.twig',
+            'OroOrganizationBundle:Form:fields.html.twig',
+            'OroSecurityBundle:Form:fields.html.twig',
+        ));
+
+        $container->setParameter('twig.form.resources', $templates);
+        $container->setParameter('assetic.bundles', $bundles);
     }
 }
