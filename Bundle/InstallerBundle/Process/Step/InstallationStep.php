a87298e9612d7cf4d5475f446ae11f56af638031
Dan Yasnyuk <danil@magecore.com>
2013-10-22 17:07:43 +0300

    CRM-566: CRM install demo fixtures timeout - updated  time limit for "heavy" demo fixtures loading operation (CRM) - demo fixtures loading moved to Installation step (AJAX)

--- Bundle/InstallerBundle/Process/Step/InstallationStep.php
+++ Bundle/InstallerBundle/Process/Step/InstallationStep.php
@@ -2,15 +2,36 @@
 
 namespace Oro\Bundle\InstallerBundle\Process\Step;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+
 use Sylius\Bundle\FlowBundle\Process\Context\ProcessContextInterface;
 
 class InstallationStep extends AbstractStep
 {
     public function displayAction(ProcessContextInterface $context)
     {
-        set_time_limit(600);
+        set_time_limit(900);
 
         switch ($this->getRequest()->query->get('action')) {
+            case 'fixtures':
+                $loader = new ContainerAwareLoader($this->container);
+
+                foreach ($this->get('kernel')->getBundles() as $bundle) {
+                    if (is_dir($path = $bundle->getPath() . '/DataFixtures/Demo')) {
+                        $loader->loadFromDirectory($path);
+                    }
+                }
+
+                $executor = new ORMExecutor($this->getDoctrine()->getManager());
+
+                $executor->execute($loader->getFixtures(), true);
+
+                return $this->getRequest()->isXmlHttpRequest()
+                    ? new JsonResponse(array('result' => true))
+                    : $this->redirect('');
             case 'schema':
                 return $this->handleAjaxAction('doctrine:schema:update', array('--force' => true));
             case 'search':
@@ -29,6 +50,11 @@ class InstallationStep extends AbstractStep
                 return $this->handleAjaxAction('oro:requirejs:build');
         }
 
-        return $this->render('OroInstallerBundle:Process/Step:installation.html.twig');
+        return $this->render(
+            'OroInstallerBundle:Process/Step:installation.html.twig',
+            array(
+                'loadFixtures' => $context->getStorage()->get('loadFixtures'),
+            )
+        );
     }
 }
