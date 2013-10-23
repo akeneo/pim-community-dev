329ad7a162a5ea9871afab4495e5203b4195d4ec
Benoit Jacquemont <benoit@akeneo.com>
2013-10-23 16:10:59 +0200

    PIM-1424 Fix configuration name for allowed upload

--- DataFixtures/ORM/LoadJobInstanceData.php
+++ DataFixtures/ORM/LoadJobInstanceData.php
@@ -62,7 +62,7 @@ class LoadJobInstanceData extends AbstractDemoFixture
                         'delimiter'   => ';',
                         'enclosure'   => '"',
                         'escape'      => '\\',
-                        'allowUpload' => true,
+                        'uploadAllowed' => true,
                     ),
                     'processor' => array(
                         'enabled'          => true,
