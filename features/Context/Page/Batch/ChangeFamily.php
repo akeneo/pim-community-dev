72fe9e05b1d639920adce7b26ae64931efe1ba14
Gildas Quéméner <gildas.quemener@gmail.com>
2013-10-24 15:09:54 +0200

    Fixed mass family edit scenario

--- features/Context/Page/Batch/ChangeFamily.php
+++ features/Context/Page/Batch/ChangeFamily.php
@@ -3,6 +3,7 @@
 namespace Context\Page\Batch;
 
 use Context\Page\Base\Wizard;
+use Behat\Mink\Element\Element;
 
 /**
  * Batch ChangeFamily page
@@ -13,4 +14,17 @@ use Context\Page\Base\Wizard;
  */
 class ChangeFamily extends Wizard
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function fillField($labelContent, $value, Element $element = null)
+    {
+        if ('Family' === $labelContent) {
+            return $this
+                ->find('css', '#pim_catalog_mass_edit_action_operation_family')
+                ->selectOption($value === 'None' ? '' : $value);
+        }
+
+        return parent::fillField($labelContent, $value, $element);
+    }
 }
