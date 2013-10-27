c07919518b978b5c010f1fe7d57a3fda78070ef4
Filips Alpe <filips@akeneo.com>
2013-10-27 12:31:44 +0100

    Create a context for behat assertions

---
+++ features/Context/AssertionContext.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Context;
+
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * Context for assertions
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AssertionContext extends RawMinkContext
+{
+}
