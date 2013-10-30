02afac6f956c1cd4c71c90fa5cc6e01b942cc9f4
Romain Monceau <romain@akeneo.com>
2013-10-30 11:41:45 +0100

    PIM-1437 : Define variant group controller

---
+++ src/Pim/Bundle/CatalogBundle/Controller/VariantGroupController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Controller;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ValidatorInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use Pim\Bundle\CatalogBundle\AbstractController\AbstractDoctrineController;
+use Pim\Bundle\CatalogBundle\Datagrid\DatagridWorkerInterface;
+use Pim\Bundle\CatalogBundle\Entity\Group;
+use Pim\Bundle\CatalogBundle\Form\Handler\GroupHandler;
+
+/**
+ * Variant group controller
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class VariantGroupController extends GroupController
+{
+    /**
+     * List variant groups
+     *
+     * @param Request $request
+     *
+     * @Template
+     * @AclAncestor("pim_catalog_group_index")
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $this->getManager()->createQueryBuilder();
+        $datagrid = $this->datagridWorker->getDatagrid('group', $queryBuilder);
+
+        $view = ('json' === $request->getRequestFormat())
+            ? 'OroGridBundle:Datagrid:list.json.php'
+            : 'PimCatalogBundle:Group:index.html.twig';
+
+        return $this->render($view, array('datagrid' => $datagrid->createView()));
+    }
+}
