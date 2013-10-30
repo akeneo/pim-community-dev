4a6b8aad2b2a3b26cc29d23b4d5962be92048095
Alexander Nezdoiminoga <alex@magecore.com>
2013-10-30 16:44:22 +0200

    MD

--- Bundle/EntityExtendBundle/Form/Type/RelationType.php
+++ Bundle/EntityExtendBundle/Form/Type/RelationType.php
@@ -48,56 +48,79 @@ class RelationType extends AbstractType
 
     public function preSubmitData(FormEvent $event)
     {
-        $data         = $event->getData() ? : $event->getForm()->getParent()->getData();
+        $data         = $event->getData();
+        if (!$data) {
+            $data = $event->getForm()->getParent()->getData();
+        }
         $form         = $event->getForm();
-        $targetEntity = isset($data['target_entity']) ? $data['target_entity'] : null;
+        $targetEntity = null;
+
+        if (isset($data['target_entity'])) {
+            $targetEntity = $data['target_entity'];
+        }
 
         $relationType = $this->config->getId()->getFieldType();
         if ($relationType == 'manyToOne') {
             //target_field
+            $targetField = null;
+            if (isset($data['target_field'])) {
+                $targetField = $data['target_field'];
+            }
             $form->add(
                 $this->formFactory->createNamed(
                     'target_field',
                     new TargetFieldType($this->configProvider, $targetEntity),
-                    isset($data['target_field']) ? $data['target_field'] : null
+                    $targetField
                 )
             );
         } else {
             //target_grid
+            $targetGrid = null;
+            if (isset($data['target_grid'])) {
+                $targetGrid = $data['target_grid'];
+            }
             $form->add(
                 $this->formFactory->createNamed(
                     'target_grid',
                     new TargetFieldType($this->configProvider, $targetEntity),
-                    isset($data['target_grid']) ? $data['target_grid'] : null,
-                    array(
+                    $targetGrid,
+                    [
                         'multiple' => true,
                         'label'    => 'Related entity data fields'
-                    )
+                    ]
                 )
             );
 
             //target_title
+            $targetTitle = null;
+            if (isset($data['target_title'])) {
+                $targetTitle = $data['target_title'];
+            }
             $form->add(
                 $this->formFactory->createNamed(
                     'target_title',
                     new TargetFieldType($this->configProvider, $targetEntity),
-                    isset($data['target_title']) ? $data['target_title'] : null,
-                    array(
+                    $targetTitle,
+                    [
                         'label'    => 'Related entity info title'
-                    )
+                    ]
                 )
             );
 
             //target_detailed
+            $targetDetailed = null;
+            if (isset($data['target_detailed'])) {
+                $targetDetailed = $data['target_detailed'];
+            }
             $form->add(
                 $this->formFactory->createNamed(
                     'target_detailed',
                     new TargetFieldType($this->configProvider, $targetEntity),
-                    isset($data['target_detailed']) ? $data['target_detailed'] : null,
-                    array(
+                    $targetDetailed,
+                    [
                         'multiple' => true,
                         'label'    => 'Related entity detailed'
-                    )
+                    ]
                 )
             );
         }
