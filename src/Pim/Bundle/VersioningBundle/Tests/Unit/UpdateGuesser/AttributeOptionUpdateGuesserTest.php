ea6f3402fd4e3f91bce41ad1fa4936e4ea8bcf15
Nicolas Dupont <nicolas@akeneo.com>
2013-10-30 19:54:46 +0100

    PIM-1325 : add attribute option update guesser UT

---
+++ src/Pim/Bundle/VersioningBundle/Tests/Unit/UpdateGuesser/AttributeOptionUpdateGuesserTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pim\Bundle\VersioningBundle\Tests\Unit\UpdateGuesser;
+
+use Pim\Bundle\VersioningBundle\UpdateGuesser\AttributeOptionUpdateGuesser;
+use Pim\Bundle\CatalogBundle\Entity\ProductAttribute;
+use Pim\Bundle\CatalogBundle\Entity\AttributeOption;
+
+/**
+ * Test related class
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeOptionUpdateGuesserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test related methods
+     */
+    public function testGuessUpdates()
+    {
+        $option    = new AttributeOption();
+        $attribute = new ProductAttribute();
+        $attribute->addOption($option);
+
+        $guesser   = new AttributeOptionUpdateGuesser();
+        $em        = $this->getEntityManagerMock();
+        $updates   = $guesser->guessUpdates($em, $option);
+        $this->assertEquals(1, count($updates));
+        $this->assertEquals($attribute, $updates[0]);
+    }
+
+    /**
+     * @return EntityManager
+     */
+    protected function getEntityManagerMock()
+    {
+        $mock = $this
+            ->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $mock;
+    }
+}
