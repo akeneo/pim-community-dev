28af3d005a7a6071615c6c4fba43d7bc6166eb9b
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-10-30 12:32:16 +0200

    BAP-498: Investigate failed builds and test  - return soap test with segmentation fault for AddressBundle

--- Bundle/AddressBundle/Tests/Functional/API/SoapAddressTypeApiTest.php
+++ Bundle/AddressBundle/Tests/Functional/API/SoapAddressTypeApiTest.php
@@ -16,8 +16,6 @@ class SoapAddressTypeApiTest extends WebTestCase
 
     public function setUp()
     {
-        $this->markTestSkipped("Skipped segmentation fault");
-
         $this->client = static::createClient(array(), ToolsAPI::generateWsseHeader());
         $this->client->soap(
             "http://localhost/api/soap",
