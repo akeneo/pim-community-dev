c34fbb5e4af0463ef1ee0d00a6f6380e1e0268e3
Nicolas Dupont <nicolas@akeneo.com>
2013-10-31 11:39:38 +0100

    PIM-1325 : make group versionable

--- src/Pim/Bundle/CatalogBundle/Entity/Group.php
+++ src/Pim/Bundle/CatalogBundle/Entity/Group.php
@@ -10,6 +10,7 @@ use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 use Pim\Bundle\TranslationBundle\Entity\AbstractTranslation;
 use Pim\Bundle\TranslationBundle\Entity\TranslatableInterface;
 use Pim\Bundle\CatalogBundle\Model\ProductInterface;
+use Pim\Bundle\VersioningBundle\Entity\VersionableInterface;
 
 /**
  * Group entity
@@ -31,7 +32,7 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
  *  }
  * )
  */
-class Group implements TranslatableInterface, GroupSequenceProviderInterface
+class Group implements TranslatableInterface, GroupSequenceProviderInterface, VersionableInterface
 {
     /**
      * @var integer $id
@@ -102,6 +103,14 @@ class Group implements TranslatableInterface, GroupSequenceProviderInterface
     protected $translations;
 
     /**
+     * @var integer $version
+     *
+     * @ORM\Column(name="version", type="integer")
+     * @ORM\Version
+     */
+    protected $version;
+
+    /**
      * Constructor
      */
     public function __construct()
@@ -119,6 +128,16 @@ class Group implements TranslatableInterface, GroupSequenceProviderInterface
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Get version
+     *
+     * @return string $version
+     */
+    public function getVersion()
+    {
+        return $this->version;
     }
 
     /**
