2208599f2af42cc3f3def3081968031e2180d892
Vova Soroka <vsoroka@magecore.com>
2013-10-31 11:09:37 +0200

    BAP-1945: Emails

--- Bundle/InstallerBundle/Form/Type/Configuration/MailerType.php
+++ Bundle/InstallerBundle/Form/Type/Configuration/MailerType.php
@@ -42,13 +42,13 @@ class MailerType extends AbstractType
             )
             ->add(
                 'oro_installer_mailer_port',
-                'text',
+                'integer',
                 array(
                     'label'         => 'form.configuration.mailer.port',
-                    'constraints'   => array(
-                        new Assert\Range(array('groups' => array('SMTP'), 'min' => 0, 'max' => 65535)),
-                    ),
                     'required'      => false,
+                    'constraints'   => array(
+                        new Assert\Type(array('groups' => array('SMTP'), 'type' => 'integer')),
+                    ),
                 )
             )
             ->add(
@@ -56,13 +56,13 @@ class MailerType extends AbstractType
                 'choice',
                 array(
                     'label'         => 'form.configuration.mailer.encryption',
+                    'required'      => false,
                     'preferred_choices' => array(''),
                     'choices'       => array(
                         ''          => 'None',
                         'ssl'       => 'SSL',
                         'tls'       => 'TLS',
                     ),
-                    'required'      => false,
                 )
             )
             ->add(
