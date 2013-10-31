1cb99e6785dbb84a4c5bc23ea801d6395e4bfea3
Aleksandr Smaga <asmaga@magecore.com>
2013-10-31 14:24:24 +0000

    BAP-1833: Refactoring of current code    - mass actions

--- Bundle/DataGridBundle/OroDataGridBundle.php
+++ Bundle/DataGridBundle/OroDataGridBundle.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 use Oro\Bundle\DataGridBundle\DependencyInjection\CompilerPass\ActionsPass;
+use Oro\Bundle\DataGridBundle\DependencyInjection\CompilerPass\MassActionsPass;
 use Oro\Bundle\DataGridBundle\DependencyInjection\CompilerPass\FormattersPass;
 use Oro\Bundle\DataGridBundle\DependencyInjection\CompilerPass\ConfigurationPass;
 
@@ -19,5 +20,6 @@ class OroDataGridBundle extends Bundle
         $container->addCompilerPass(new ConfigurationPass());
         $container->addCompilerPass(new FormattersPass());
         $container->addCompilerPass(new ActionsPass());
+        $container->addCompilerPass(new MassActionsPass());
     }
 }
