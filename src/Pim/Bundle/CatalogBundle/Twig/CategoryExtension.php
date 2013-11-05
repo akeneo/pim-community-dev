65072c613be7cb34eb05bcc6435f8eedd1166f2e
Romain Monceau <romain@akeneo.com>
2013-11-05 17:09:11 +0100

    PIM-1473 : Clean controller and add php documentation

--- src/Pim/Bundle/CatalogBundle/Twig/CategoryExtension.php
+++ src/Pim/Bundle/CatalogBundle/Twig/CategoryExtension.php
@@ -2,19 +2,37 @@
 
 namespace Pim\Bundle\CatalogBundle\Twig;
 
-use Entity\Category;
+use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
 
 use Doctrine\ORM\EntityManager;
 
+/**
+ * Twig extension to render category from twig templates
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class CategoryExtension extends \Twig_Extension
 {
+    /**
+     * @var EntityManager
+     */
     protected $em;
 
+    /**
+     * Constructor
+     *
+     * @param EntityManager $em
+     */
     public function __construct(EntityManager $em)
     {
         $this->em = $em;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getFilters()
     {
         return array(
@@ -22,16 +40,26 @@ class CategoryExtension extends \Twig_Extension
         );
     }
 
+    /**
+     * Count products for a defined category
+     * @param CategoryInterface $category
+     * @param bool $nested
+     *
+     * @return string
+     */
     public function countProducts($category, $nested)
     {
         // TODO : test instance of
-        if ($category instanceof Category) {
+        if ($category instanceof CategoryInterface) {
             return 'pouic';
         } else {
             throw \Twig_Error_Runtime('"count_products" filter is only allowed for CategoryInterface');
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'pim_category_extension';
