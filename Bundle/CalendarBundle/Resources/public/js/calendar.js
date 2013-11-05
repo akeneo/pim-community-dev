8eb8659fb91e9046d0c4e1cee4b29d1dca25a401
Dmitry Khrysev <x86demon@gmail.com>
2013-11-05 18:50:25 +0200

    BAP-2128: Fix calendar view to work correct with JS dateFormatter - decoupled calendar and calendar event views

--- Bundle/CalendarBundle/Resources/public/js/calendar.js
+++ Bundle/CalendarBundle/Resources/public/js/calendar.js
@@ -41,9 +41,11 @@ function(_, Backbone, __, app, messenger, LoadingMask,
                 collection: null,
                 itemFormTemplateSelector: null,
                 itemFormDeleteButtonSelector: null,
-                itemFormValidationScriptUrl: null
+                itemFormValidationScriptUrl: null,
+                calendar: null
             },
             connectionsOptions: {
+                collection: null,
                 containerTemplateSelector: null
             }
         },
@@ -53,10 +55,13 @@ function(_, Backbone, __, app, messenger, LoadingMask,
          *  @property {bool}
          */
         enableEventLoading: false,
+        fullCalendar: null,
+        eventView: null,
+        loadingMask: null,
 
         initialize: function() {
             // init event collection
-            this.options.collection = this.options.eventsOptions.collection || new EventCollection();
+            this.options.collection = this.options.collection || new EventCollection();
             this.options.collection.setCalendar(this.options.calendar);
             this.options.collection.subordinate = this.options.eventsOptions.subordinate;
 
@@ -64,125 +69,49 @@ function(_, Backbone, __, app, messenger, LoadingMask,
             this.options.newEventEditable = this.options.eventsOptions.editable;
             this.options.newEventRemovable = this.options.eventsOptions.removable;
 
-            // init views
-            this.initializeEventsView();
-            this.initializeConnectionsView();
-            this.initializeLoadingMaskView();
-
             // subscribe to event collection events
             this.listenTo(this.getCollection(), 'add', this.onEventAdded);
             this.listenTo(this.getCollection(), 'change', this.onEventChanged);
             this.listenTo(this.getCollection(), 'destroy', this.onEventDeleted);
+
             // subscribe to connection collection events
-            this.listenTo(this.connectionsView.getCollection(), 'add', this.onConnectionAddedOrDeleted);
-            this.listenTo(this.connectionsView.getCollection(), 'change', this.onConnectionChanged);
-            this.listenTo(this.connectionsView.getCollection(), 'destroy', this.onConnectionAddedOrDeleted);
-
-            // initialize jQuery FullCalendar control
-            this.initializeFullCalendar();
-
-            // remove no longer used options
-            delete this.options.calendar;
-            delete this.options.eventsOptions;
-            delete this.options.connectionsOptions;
+            var connectionsCollection = this.options.connectionsOptions.collection;
+            this.listenTo(connectionsCollection, 'add', this.onConnectionAddedOrDeleted);
+            this.listenTo(connectionsCollection, 'change', this.onConnectionChanged);
+            this.listenTo(connectionsCollection, 'destroy', this.onConnectionAddedOrDeleted);
         },
 
-        initializeEventsView: function () {
-            // create a view for event details
-            this.eventView = new EventView({
-                collection: this.getCollection(),
-                formTemplateSelector: this.options.eventsOptions.itemFormTemplateSelector,
-                formDeleteButtonSelector: this.options.eventsOptions.itemFormDeleteButtonSelector,
-                formValidationScriptUrl: this.options.eventsOptions.itemFormValidationScriptUrl,
-                timezoneOffset: this.options.eventsOptions.timezoneOffset
-            });
-
-            // init events container
-            var eventsContainer = this.$el.find(this.options.eventsOptions.containerSelector);
-            if (eventsContainer.length === 0) {
-                throw new Error("Cannot find '" + this.options.eventsOptions.containerSelector + "' element.");
+        getEventsView: function (model) {
+            if (!this.eventView) {
+                // create a view for event details
+                this.eventView = new EventView({
+                    model: model,
+                    calendar: this.options.calendar,
+                    formTemplateSelector: this.options.eventsOptions.itemFormTemplateSelector,
+                    formValidationScriptUrl: this.options.eventsOptions.itemFormValidationScriptUrl
+                });
+                // subscribe to event view collection events
+                this.listenTo(this.eventView, 'addEvent', this.handleEventViewAdd);
+                this.listenTo(this.eventView, 'remove', this.handleEventViewRemove);
             }
-            eventsContainer.empty();
-            eventsContainer.append($(this.eventsTemplate()));
+            return this.eventView;
         },
 
-        initializeConnectionsView: function () {
-            // init connections container
-            var connectionsContainer = this.$el.find(this.options.connectionsOptions.containerSelector);
-            if (connectionsContainer.length === 0) {
-                throw new Error("Cannot find '" + this.options.connectionsOptions.containerSelector + "' element.");
+        handleEventViewRemove: function() {
+            this.eventView = null;
+        },
+
+        /**
+         * Init and get a loading mask control
+         *
+         * @returns {Element}
+         */
+        getLoadingMask: function () {
+            if (!this.loadingMask) {
+                this.loadingMask = new LoadingMask();
+                this.$el.find(this.selectors.loadingMask).append(this.loadingMask.render().$el);
             }
-            connectionsContainer.empty();
-            var connectionsTemplate = _.template($(this.options.connectionsOptions.containerTemplateSelector).html());
-            connectionsContainer.append($(connectionsTemplate()));
-
-            // init connection collection
-            var connections = this.options.connectionsOptions.collection || new ConnectionCollection();
-            connections.setCalendar(this.options.calendar);
-
-            // create a view for a list of connections
-            this.connectionsView = new ConnectionView({
-                el: connectionsContainer,
-                collection: connections,
-                itemTemplateSelector: this.options.connectionsOptions.itemTemplateSelector
-            });
-        },
-
-        initializeLoadingMaskView: function () {
-            // init a loading mask control
-            this.loadingMask = new LoadingMask();
-            this.$el.find(this.selectors.loadingMask).append(this.loadingMask.render().$el);
-        },
-
-        initializeFullCalendar: function () {
-            // prepare options for jQuery FullCalendar control
-            var options = {
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay',
-                    ignoreTimezone: false,
-                    allDayDefault: false
-                },
-                selectHelper: true,
-                events: _.bind(this.loadEvents, this),
-                select: _.bind(this.select, this),
-                eventClick: _.bind(this.eventClick, this),
-                eventDrop: _.bind(this.eventDropOrResize, this),
-                eventResize: _.bind(this.eventDropOrResize, this),
-                loading: _.bind(this.showLoadingMask, this),
-                allDayText: __('all-day'),
-                buttonText: {
-                    today: __('today'),
-                    month: __('month'),
-                    week: __('week'),
-                    day: __('day')
-                }
-            };
-            var keys = ['date', 'defaultView', 'editable', 'selectable',
-                'titleFormat', 'columnFormat', 'timeFormat', 'axisFormat',
-                'firstDay', 'monthNames', 'monthNamesShort', 'dayNames', 'dayNamesShort'];
-            _.extend(options, _.pick(this.options.eventsOptions, keys));
-            if (!_.isUndefined(options.date)) {
-                if (_.isString(options.date)) {
-                    options.date = $.fullCalendar.parseISO8601(options.date, true);
-                }
-                options.year = options.date.getFullYear();
-                options.month = options.date.getMonth();
-                options.date = options.date.getDate();
-            }
-
-            //Fix aspect ration to prevent double scroll for week and day views.
-            options.viewRender = _.bind(function(view) {
-                if (view.name !== 'month') {
-                    this.getCalendarElement().fullCalendar('option', 'aspectRatio', 1.0);
-                } else {
-                    this.getCalendarElement().fullCalendar('option', 'aspectRatio', 1.35);
-                }
-            }, this);
-            // create jQuery FullCalendar control
-            this.getCalendarElement().fullCalendar(options);
-            this.enableEventLoading = true;
+            return this.loadingMask;
         },
 
         getCollection: function() {
@@ -190,20 +119,27 @@ function(_, Backbone, __, app, messenger, LoadingMask,
         },
 
         getCalendarElement: function() {
-            return this.$el.find(this.selectors.calendar);
+            if (!this.fullCalendar) {
+                this.fullCalendar = this.$el.find(this.selectors.calendar);
+            }
+            return this.fullCalendar;
         },
 
-        onEventAdded: function(event){
-            var fcEvent = event.toJSON();
+        handleEventViewAdd: function(eventModel) {
+            this.getCollection().add(eventModel);
+        },
+
+        onEventAdded: function(eventModel){
+            var fcEvent = eventModel.toJSON();
             this.prepareViewModel(fcEvent);
 
             this.getCalendarElement().fullCalendar('renderEvent', fcEvent);
         },
 
-        onEventChanged: function(event){
-            var fcEvent = this.getCalendarElement().fullCalendar('clientEvents', event.get('id'))[0];
+        onEventChanged: function(eventModel){
+            var fcEvent = this.getCalendarElement().fullCalendar('clientEvents', eventModel.get('id'))[0];
             // copy all fields, except id, from event to fcEvent
-            fcEvent = _.extend(fcEvent, _.pick(event.attributes, _.keys(_.omit(fcEvent, ['id']))));
+            fcEvent = _.extend(fcEvent, _.pick(eventModel.attributes, _.keys(_.omit(fcEvent, ['id']))));
             // convert start and end dates from RFC 3339 string to Date object
             fcEvent.start = this.convertToViewDateTime(fcEvent.start);
             fcEvent.end = this.convertToViewDateTime(fcEvent.end);
@@ -211,8 +147,8 @@ function(_, Backbone, __, app, messenger, LoadingMask,
             this.getCalendarElement().fullCalendar('updateEvent', fcEvent);
         },
 
-        onEventDeleted: function(event) {
-            this.getCalendarElement().fullCalendar('removeEvents', event.id);
+        onEventDeleted: function(eventModel) {
+            this.getCalendarElement().fullCalendar('removeEvents', eventModel.id);
         },
 
         onConnectionAddedOrDeleted: function () {
@@ -224,15 +160,15 @@ function(_, Backbone, __, app, messenger, LoadingMask,
         },
 
         select: function(start, end) {
-            if (!this.eventView.model) {
+            if (!this.eventView) {
                 try {
-                    this.eventView.model = new EventModel({
+                    var eventModel = new EventModel({
                         start: this.formatDateTimeForModel(start),
                         end: this.formatDateTimeForModel(end),
                         editable: this.options.newEventEditable,
                         removable: this.options.newEventRemovable
                     });
-                    this.eventView.render();
+                    this.getEventsView(eventModel).render();
                 } catch (err) {
                     this.showError(err);
                 }
@@ -240,10 +176,10 @@ function(_, Backbone, __, app, messenger, LoadingMask,
         },
 
         eventClick: function(fcEvent) {
-            if (!this.eventView.model) {
+            if (!this.eventView) {
                 try {
-                    this.eventView.model = this.getCollection().get(fcEvent.id);
-                    this.eventView.render();
+                    var eventModel = this.getCollection().get(fcEvent.id);
+                    this.getEventsView(eventModel).render();
                 } catch (err) {
                     this.showError(err);
                 }
@@ -251,7 +187,7 @@ function(_, Backbone, __, app, messenger, LoadingMask,
         },
 
         eventDropOrResize: function(fcEvent) {
-            this.showSavingMask(true);
+            this.showSavingMask();
             try {
                 this.getCollection()
                     .get(fcEvent.id)
@@ -261,16 +197,12 @@ function(_, Backbone, __, app, messenger, LoadingMask,
                             end: this.formatDateTimeForModel(!_.isNull(fcEvent.end) ? fcEvent.end : fcEvent.start)
                         },
                         {
-                            success: _.bind(function () {
-                                this.showSavingMask(false);
-                            }, this),
+                            success: _.bind(this._hideMask, this),
                             error: _.bind(function (model, response) {
-                                this.showSavingMask(false);
                                 this.showSaveEventError(response.responseJSON);
                             }, this)
                         });
             } catch (err) {
-                this.showSavingMask(false);
                 this.showLoadEventsError(err);
             }
         },
@@ -328,24 +260,27 @@ function(_, Backbone, __, app, messenger, LoadingMask,
             return dateTimeFormatter.unformatDateTime(d);
         },
 
-        showSavingMask: function(show) {
-            this._showMask(show, __('Saving...'));
+        showSavingMask: function() {
+            this._showMask(__('Saving...'));
         },
 
-        showLoadingMask: function(show) {
-            this._showMask(show, __('Loading...'));
+        showLoadingMask: function() {
+            this._showMask(__('Loading...'));
         },
 
-        _showMask: function(show, message) {
+        _showMask: function(message) {
             if (this.enableEventLoading) {
-                if (show) {
-                    this.loadingMask.$el
-                        .find(this.selectors.loadingMaskContent)
-                        .text(message);
-                    this.loadingMask.show();
-                } else {
-                    this.loadingMask.hide();
-                }
+                var loadingMaskInstance = this.getLoadingMask();
+                loadingMaskInstance.$el
+                    .find(this.selectors.loadingMaskContent)
+                    .text(message);
+                loadingMaskInstance.show();
+            }
+        },
+
+        _hideMask: function() {
+            if (this.loadingMask) {
+                this.loadingMask.hide();
             }
         },
 
@@ -362,6 +297,7 @@ function(_, Backbone, __, app, messenger, LoadingMask,
         },
 
         _showError: function (err, message) {
+            this._hideMask();
             if (!_.isUndefined(console)) {
                 console.error(_.isUndefined(err.stack) ? err : err.stack);
             }
@@ -376,6 +312,99 @@ function(_, Backbone, __, app, messenger, LoadingMask,
                 }
             }
             messenger.notificationFlashMessage('error', msg);
+        },
+
+        initCalendarContainer: function() {
+            // init events container
+            var eventsContainer = this.$el.find(this.options.eventsOptions.containerSelector);
+            if (eventsContainer.length === 0) {
+                throw new Error("Cannot find '" + this.options.eventsOptions.containerSelector + "' element.");
+            }
+            eventsContainer.empty();
+            eventsContainer.append($(this.eventsTemplate()));
+        },
+
+        initializeFullCalendar: function () {
+            // prepare options for jQuery FullCalendar control
+            var options = {
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay',
+                    ignoreTimezone: false,
+                    allDayDefault: false
+                },
+                selectHelper: true,
+                events: _.bind(this.loadEvents, this),
+                select: _.bind(this.select, this),
+                eventClick: _.bind(this.eventClick, this),
+                eventDrop: _.bind(this.eventDropOrResize, this),
+                eventResize: _.bind(this.eventDropOrResize, this),
+                loading: _.bind(this.showLoadingMask, this),
+                allDayText: __('all-day'),
+                buttonText: {
+                    today: __('today'),
+                    month: __('month'),
+                    week: __('week'),
+                    day: __('day')
+                }
+            };
+            var keys = ['date', 'defaultView', 'editable', 'selectable',
+                'titleFormat', 'columnFormat', 'timeFormat', 'axisFormat',
+                'firstDay', 'monthNames', 'monthNamesShort', 'dayNames', 'dayNamesShort'];
+            _.extend(options, _.pick(this.options.eventsOptions, keys));
+            if (!_.isUndefined(options.date)) {
+                if (_.isString(options.date)) {
+                    options.date = $.fullCalendar.parseISO8601(options.date, true);
+                }
+                options.year = options.date.getFullYear();
+                options.month = options.date.getMonth();
+                options.date = options.date.getDate();
+            }
+
+            //Fix aspect ration to prevent double scroll for week and day views.
+            options.viewRender = _.bind(function(view) {
+                if (view.name !== 'month') {
+                    this.getCalendarElement().fullCalendar('option', 'aspectRatio', 1.0);
+                } else {
+                    this.getCalendarElement().fullCalendar('option', 'aspectRatio', 1.35);
+                }
+            }, this);
+            // create jQuery FullCalendar control
+            this.getCalendarElement().fullCalendar(options);
+            this.enableEventLoading = true;
+        },
+
+        initializeConnectionsView: function () {
+            // init connections container
+            var connectionsContainer = this.$el.find(this.options.connectionsOptions.containerSelector);
+            if (connectionsContainer.length === 0) {
+                throw new Error("Cannot find '" + this.options.connectionsOptions.containerSelector + "' element.");
+            }
+            connectionsContainer.empty();
+            var connectionsTemplate = _.template($(this.options.connectionsOptions.containerTemplateSelector).html());
+            connectionsContainer.append($(connectionsTemplate()));
+
+            // init connection collection
+            var connections = this.options.connectionsOptions.collection || new ConnectionCollection();
+            connections.setCalendar(this.options.calendar);
+
+            // create a view for a list of connections
+            this.connectionsView = new ConnectionView({
+                el: connectionsContainer,
+                collection: connections,
+                itemTemplateSelector: this.options.connectionsOptions.itemTemplateSelector
+            });
+        },
+
+        render: function() {
+            // init views
+            this.initCalendarContainer();
+            this.initializeConnectionsView();
+            // initialize jQuery FullCalendar control
+            this.initializeFullCalendar();
+
+            return this;
         }
     });
 });
