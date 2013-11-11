58e5a2ff0490d1a170733a6d0859600374bfce1a
yurio <yurio@ua.fm>
2013-11-11 12:15:03 +0200

    BAP-2036: Update fixtures

--- Bundle/InstallerBundle/Command/InstallCommand.php
+++ Bundle/InstallerBundle/Command/InstallCommand.php
@@ -105,6 +105,11 @@ class InstallCommand extends ContainerAwareCommand
             ->getRepository('OroUserBundle:Role')
             ->findOneBy(array('role' => 'ROLE_ADMINISTRATOR'));
 
+        $businessUnit = $container
+            ->get('doctrine.orm.entity_manager')
+            ->getRepository('OroOrganizationBundle:BusinessUnit')
+            ->findOneBy(array('name' => 'Main'));
+
         $passValidator = function ($value) {
             if (strlen(trim($value)) < 2) {
                 throw new \Exception('The password must be at least 2 characters long');
@@ -134,6 +139,8 @@ class InstallCommand extends ContainerAwareCommand
                 ? $options['user-password']
                 : $dialog->askHiddenResponseAndValidate($output, '<question>Password:</question> ', $passValidator)
             )
+            ->setOwner($businessUnit)
+            ->addBusinessUnit($businessUnit)
             ->setEnabled(true)
             ->addRole($role);
 
