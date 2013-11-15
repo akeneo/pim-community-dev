62cf51f36828bf9426e257686d9e69de8fad807c
Nicolas Dupont <nicolas@akeneo.com>
2013-11-15 14:56:23 +0100

    PIM-918 : add flat attribute option normalizer

---
+++ src/Pim/Bundle/ImportExportBundle/Normalizer/FlatAttributeOptionNormalizer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pim\Bundle\ImportExportBundle\Normalizer;
+
+use Pim\Bundle\CatalogBundle\Entity\AttributeOption;
+
+/**
+ * Flat attribute option normalizer
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FlatAttributeOptionNormalizer extends AttributeOptionNormalizer
+{
+    /**
+     * @var array
+     */
+    protected $supportedFormats = array('csv');
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getNormalizedLabelsArray(AttributeOption $entity)
+    {
+        $values = array();
+        foreach ($entity->getOptionValues() as $translation) {
+            $values[sprintf('label-%s', $translation->getLocale())] = $translation->getValue();
+        }
+
+        return $values;
+    }
+}
