313edb6404d6e052b452a1bfacd49e6b200c69b6
Romain Monceau <romain@akeneo.com>
2013-11-18 18:09:08 +0100

    PIM-1439 : Redefine methods from channel normalizer to flat normalizer

--- src/Pim/Bundle/ImportExportBundle/Normalizer/FlatChannelNormalizer.php
+++ src/Pim/Bundle/ImportExportBundle/Normalizer/FlatChannelNormalizer.php
@@ -2,6 +2,8 @@
 
 namespace Pim\Bundle\ImportExportBundle\Normalizer;
 
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+
 /**
  * A normalizer to transform a channel entity into a flat array
  *
@@ -15,4 +17,24 @@ class FlatChannelNormalizer extends ChannelNormalizer
      * @var array
      */
     protected $supportedFormats = array('csv');
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeCurrencies(Channel $channel)
+    {
+        $currencies = parent::normalizeCurrencies($channel);
+
+        return implode(',', $currencies);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeLocales(Channel $channel)
+    {
+        $locales = parent::normalizeLocales($channel);
+
+        return implode(',', $locales);
+    }
 }
