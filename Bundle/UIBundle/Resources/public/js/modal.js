25528020f94a2fee9a8fe57aa054f839137c4d4d
Alexander Nezdoiminoga <alex@magecore.com>
2013-11-18 20:32:33 +0200

    BAP-2050 fixes

--- Bundle/UIBundle/Resources/public/js/modal.js
+++ Bundle/UIBundle/Resources/public/js/modal.js
@@ -15,47 +15,6 @@ function(_, Backbone, __) {
         /** @property {String} */
         className: 'modal oro-modal-danger',
 
-        /** @property {String} */
-        okButtonClass: 'btn-primary',
-
-        /** @property {Object} */
-        template: _.template('\
-            <% if (title) { %>\
-              <div class="modal-header">\
-                <% if (allowCancel) { %>\
-                  <a class="close">×</a>\
-                <% } %>\
-                <h3><%- title %></h3>\
-              </div>\
-            <% } %>\
-            <div class="modal-body"><%= content %></div>\
-            <div class="modal-footer">\
-              <% if (allowCancel && cancelText) { %>\
-                <a href="#" class="btn cancel"><%- cancelText %></a>\
-              <% } %>\
-              <a href="#" class="btn ok <%= okButtonClass %>"><%- okText %></a>\
-            </div>\
-        '),
-
-        /**
-         * @param {Object} options
-         */
-        initialize: function(options) {
-            options = _.extend({
-                cancelText: __('Cancel')
-            }, options);
-
-            if (!options.okButtonClass) {
-                options.okButtonClass = this.okButtonClass;
-            }
-            options = _.extend({
-                template: this.template,
-                className: this.className
-            }, options);
-
-            Backbone.BootstrapModal.prototype.initialize.apply(this, arguments);
-        },
-
         open: function() {
             Backbone.BootstrapModal.prototype.open.apply(this, arguments);
 
