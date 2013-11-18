fdb7be9174a2dbe45a80565a9c674fb589f950db
Nicolas Dupont <nicolas@akeneo.com>
2013-11-18 16:05:20 +0100

    PIM-918 : refactor attribute normalizer

--- src/Pim/Bundle/ImportExportBundle/Normalizer/AttributeNormalizer.php
+++ src/Pim/Bundle/ImportExportBundle/Normalizer/AttributeNormalizer.php
@@ -37,17 +37,11 @@ class AttributeNormalizer implements NormalizerInterface
      */
     public function normalize($attribute, $format = null, array $context = array())
     {
-        $dateMin = (is_null($attribute->getDateMin())) ? '' : $attribute->getDateMin()->format(\DateTime::ISO8601);
-        $dateMax = (is_null($attribute->getDateMax())) ? '' : $attribute->getDateMax()->format(\DateTime::ISO8601);
-
         $results = array(
             'type' => $attribute->getAttributeType(),
             'code' => $attribute->getCode()
         );
-        $results = array_merge(
-            $results,
-            $this->normalizeLabel($attribute)
-        );
+        $results = array_merge($results, $this->normalizeLabel($attribute));
         $results = array_merge(
             $results,
             array(
@@ -58,35 +52,8 @@ class AttributeNormalizer implements NormalizerInterface
             )
         );
         if (isset($context['versioning'])) {
-            $results = array_merge(
-                $results,
-                array(
-                    'available_locales'       => $this->normalizeAvailableLocales($attribute),
-                    'searchable'          => $attribute->getSearchable(),
-                    'localizable'         => $attribute->getTranslatable(),
-                    'scope'               => $attribute->getScopable() ? self::CHANNEL_SCOPE : self::GLOBAL_SCOPE,
-                    'options'             => $this->normalizeOptions($attribute),
-                    'default_options'     => $this->normalizeDefaultOptions($attribute),
-                    'sort_order'          => (int) $attribute->getSortOrder(),
-                    'required'            => (int) $attribute->getRequired(),
-                    'default_value'       => (string) $attribute->getDefaultValue(),
-                    'max_characters'      => (string) $attribute->getMaxCharacters(),
-                    'validation_rule'     => (string) $attribute->getValidationRule(),
-                    'validation_regexp'   => (string) $attribute->getValidationRegexp(),
-                    'wysiwyg_enabled'     => (string) $attribute->isWysiwygEnabled(),
-                    'number_min'          => (string) $attribute->getNumberMin(),
-                    'number_max'          => (string) $attribute->getNumberMax(),
-                    'decimals_allowed'    => (string) $attribute->isDecimalsAllowed(),
-                    'negative_allowed'    => (string) $attribute->isNegativeAllowed(),
-                    'date_min'            => $dateMin,
-                    'date_max'            => $dateMax,
-                    'date_type'           => (string) $attribute->getDateType(),
-                    'metric_family'       => (string) $attribute->getMetricFamily(),
-                    'default_metric_unit' => (string) $attribute->getDefaultMetricUnit(),
-                    'allowed_extensions'  => implode(self::ITEM_SEPARATOR, $attribute->getAllowedExtensions()),
-                    'max_file_size'       => (string) $attribute->getMaxFileSize(),
-                )
-            );
+            $results = array_merge($results, $this->getVersionedData($attribute));
+
         } else {
             $results = array_merge(
                 $results,
@@ -111,6 +78,46 @@ class AttributeNormalizer implements NormalizerInterface
     public function supportsNormalization($data, $format = null)
     {
         return $data instanceof ProductAttribute && in_array($format, $this->supportedFormats);
+    }
+
+    /**
+     * Get extra data to store in version
+     *
+     * @param ProductAttribute $attribute
+     *
+     * @return array
+     */
+    protected function getVersionedData(ProductAttribute $attribute)
+    {
+        $dateMin = (is_null($attribute->getDateMin())) ? '' : $attribute->getDateMin()->format(\DateTime::ISO8601);
+        $dateMax = (is_null($attribute->getDateMax())) ? '' : $attribute->getDateMax()->format(\DateTime::ISO8601);
+
+        return array(
+            'available_locales'   => $this->normalizeAvailableLocales($attribute),
+            'searchable'          => $attribute->getSearchable(),
+            'localizable'         => $attribute->getTranslatable(),
+            'scope'               => $attribute->getScopable() ? self::CHANNEL_SCOPE : self::GLOBAL_SCOPE,
+            'options'             => $this->normalizeOptions($attribute),
+            'default_options'     => $this->normalizeDefaultOptions($attribute),
+            'sort_order'          => (int) $attribute->getSortOrder(),
+            'required'            => (int) $attribute->getRequired(),
+            'default_value'       => (string) $attribute->getDefaultValue(),
+            'max_characters'      => (string) $attribute->getMaxCharacters(),
+            'validation_rule'     => (string) $attribute->getValidationRule(),
+            'validation_regexp'   => (string) $attribute->getValidationRegexp(),
+            'wysiwyg_enabled'     => (string) $attribute->isWysiwygEnabled(),
+            'number_min'          => (string) $attribute->getNumberMin(),
+            'number_max'          => (string) $attribute->getNumberMax(),
+            'decimals_allowed'    => (string) $attribute->isDecimalsAllowed(),
+            'negative_allowed'    => (string) $attribute->isNegativeAllowed(),
+            'date_min'            => $dateMin,
+            'date_max'            => $dateMax,
+            'date_type'           => (string) $attribute->getDateType(),
+            'metric_family'       => (string) $attribute->getMetricFamily(),
+            'default_metric_unit' => (string) $attribute->getDefaultMetricUnit(),
+            'allowed_extensions'  => implode(self::ITEM_SEPARATOR, $attribute->getAllowedExtensions()),
+            'max_file_size'       => (string) $attribute->getMaxFileSize(),
+        );
     }
 
     /**
