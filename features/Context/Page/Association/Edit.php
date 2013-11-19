ac011330929eb44ee42fa9f59ac415ff53f8a84d
Romain Monceau <romain@akeneo.com>
2013-11-19 17:32:54 +0100

    Add behat scenario for display history feature on association entity

--- features/Context/Page/Association/Edit.php
+++ features/Context/Page/Association/Edit.php
@@ -17,4 +17,27 @@ class Edit extends Form
      * @var string
      */
     protected $path = '/configuration/association/{id}/edit';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($session, $pageFactory, $parameters = array())
+    {
+        parent::__construct($session, $pageFactory, $parameters);
+
+        $this->elements = array_merge(
+            $this->elements,
+            array(
+                'Updates grid' => array('css' => '#history table.grid')
+            )
+        );
+    }
+
+    /**
+     * @return NodeElement[]
+     */
+    public function getHistoryRows()
+    {
+        return $this->getElement('Updates grid')->findAll('css', 'tbody tr');
+    }
 }
