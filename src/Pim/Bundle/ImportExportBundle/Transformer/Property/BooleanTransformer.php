3e2e46537fbdc4662936205afa52fc725d03b2b3
Nicolas Dupont <nicolas@akeneo.com>
2013-11-19 11:21:06 +0100

    PIM-1558 : add boolean transformer to import product value of type boolean

---
+++ src/Pim/Bundle/ImportExportBundle/Transformer/Property/BooleanTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pim\Bundle\ImportExportBundle\Transformer\Property;
+
+use Pim\Bundle\ImportExportBundle\Exception\InvalidValueException;
+
+/**
+ * Boolean attribute transformer
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class BooleanTransformer implements PropertyTransformerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value, array $options = array())
+    {
+        if (is_bool($value)) {
+            return $value;
+        } elseif ($value == 0) {
+            return false;
+        } elseif ($value == 1) {
+            return true;
+        }
+
+        throw new InvalidValueException('Invalid boolean');
+    }
+}
