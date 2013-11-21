75b5e13f771993b6699530e8fe54571ae6d94fee
Benoit Jacquemont <benoit@akeneo.com>
2013-11-21 16:24:03 +0100

    Restored ACL config

--- src/Pim/Bundle/CatalogBundle/Entity/Family.php
+++ src/Pim/Bundle/CatalogBundle/Entity/Family.php
@@ -17,6 +17,16 @@ use Pim\Bundle\TranslationBundle\Entity\AbstractTranslation;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
+ * @Config(
+ *     defaultValues={
+ *         "entity"={"label"="Family", "plural_label"="Families"},
+ *         "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *         }
+ *     }
+ * )
+ *
  * @ExclusionPolicy("all")
  */
 class Family implements TranslatableInterface
