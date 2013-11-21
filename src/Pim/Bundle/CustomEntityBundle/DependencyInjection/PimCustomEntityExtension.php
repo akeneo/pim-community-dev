1e495b4876b3ed920c036bc5ea59b69c37086719
Romain Monceau <romain@akeneo.com>
2013-11-21 15:46:59 +0100

    Change @inheritdoc tag

--- src/Pim/Bundle/CustomEntityBundle/DependencyInjection/PimCustomEntityExtension.php
+++ src/Pim/Bundle/CustomEntityBundle/DependencyInjection/PimCustomEntityExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\Loader;
 class PimCustomEntityExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
