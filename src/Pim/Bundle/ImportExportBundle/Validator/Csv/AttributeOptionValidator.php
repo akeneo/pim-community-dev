2e69981faf99972f875217eb8aeee6138f3ce817
Nicolas Dupont <nicolas@akeneo.com>
2013-11-21 09:01:22 +0100

    Add validation constraints for attribute, code and is_default on a row that describes an attribute option

--- src/Pim/Bundle/ImportExportBundle/Validator/Csv/AttributeOptionValidator.php
+++ src/Pim/Bundle/ImportExportBundle/Validator/Csv/AttributeOptionValidator.php
@@ -3,6 +3,9 @@
 namespace Pim\Bundle\ImportExportBundle\Validator\Csv;
 
 use Symfony\Component\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Oro\Bundle\BatchBundle\Item\InvalidItemException;
 
 /**
  * Validates a csv option row
@@ -19,6 +22,11 @@ class AttributeOptionValidator implements RowValidatorInterface
     protected $validator;
 
     /**
+     * @var array
+     */
+    protected $constraints = array();
+
+    /**
      * Constructor
      *
      * @param ValidatorInterface $validator
@@ -30,9 +38,49 @@ class AttributeOptionValidator implements RowValidatorInterface
 
     /**
      * {@inheritdoc}
+     *
+     * @throws InvalidItemException
      */
     public function validate(array $data)
     {
-        var_dump($data);
+        $fieldConstraints = $this->getFieldConstraints();
+        foreach ($fieldConstraints as $field => $constraints) {
+            foreach ($constraints as $constraint) {
+                $violations = $this->validator->validateValue($data[$field], $constraint);
+                if (count($violations) > 0) {
+                    $messages = array();
+                    foreach ($violations as $violation) {
+                        $messages[]= (string) $violation;
+                    }
+
+                    throw new InvalidItemException(implode(', ', $messages), $data);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get constraints to apply on each field
+     *
+     * @return array
+     */
+    protected function getFieldConstraints()
+    {
+        if (empty($this->constraints)) {
+            $notBlank = new NotBlank(array('message' => 'The value attribute should not be blank.'));
+            $this->constraints['attribute'] = array($notBlank);
+
+            $notBlank = new NotBlank(array('message' => 'The value code should not be blank.'));
+            $this->constraints['code'] = array($notBlank);
+            $authorized = new Regex(
+                array(
+                    'pattern' => '/^[0-1]$/',
+                    'message' => 'The value is_default must be 0 or 1.'
+                )
+            );
+            $this->constraints['is_default'] = array($authorized);
+        }
+
+        return $this->constraints;
     }
 }
