d3ee07589aa93ed06702cb445ee75bc5c4ce0413
Romain Monceau <romain@akeneo.com>
2013-11-26 10:21:09 +0100

    Add UT for listProducts

--- src/Pim/Bundle/CatalogBundle/Tests/Unit/Twig/CategoryExtensionTest.php
+++ src/Pim/Bundle/CatalogBundle/Tests/Unit/Twig/CategoryExtensionTest.php
@@ -108,7 +108,6 @@ class CategoryExtensionTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-
     /**
      * Test related method
      *
@@ -118,7 +117,7 @@ class CategoryExtensionTest extends \PHPUnit_Framework_TestCase
      *
      * @dataProvider dataProviderListTrees
      */
-    public function testListTreesResponse($trees, $selectedTreeId, $resultCount, $expectedResult)
+    public function testListTreesResponse(array $trees, $selectedTreeId, $resultCount, $expectedResult)
     {
         $this->defineCategoryCountResult($resultCount);
 
@@ -129,6 +128,52 @@ class CategoryExtensionTest extends \PHPUnit_Framework_TestCase
 
         $result = $this->categoryExtension->listTreesResponse($treeEntities, $selectedTreeId);
         $this->assertSame($expectedResult, $result);
+    }
+
+    /**
+     * Data provider for listProducts method
+     *
+     * @return array
+     *
+     * @static
+     */
+    public static function dataProviderListProducts()
+    {
+        return array(
+            array(
+                array(
+                    array('id' => 5, 'identifier' => 'sku-005', 'label' => 'MyDescription5'),
+                    array('id' => 3, 'identifier' => 'sku-003', 'label' => 'MyDescription3'),
+                    array('id' => 14, 'identifier' => 'sku-014', 'label' => 'MyDescription14')
+                )
+            )
+        );
+    }
+
+    /**
+     * Test related method
+     *
+     * @param array $products
+     *
+     * @dataProvider dataProviderListProducts
+     */
+    public function testListProducts(array $products)
+    {
+        $expectedResult = array();
+        $productMocks = array();
+
+        foreach ($products as $product) {
+            $expectedResult[] = array(
+                'id'          => $product['id'],
+                'name'        => $product['identifier'],
+                'description' => $product['label']
+            );
+
+            $productMocks[] = $this->getProductMock($product);
+        }
+
+        $result = $this->categoryExtension->listProducts($productMocks);
+        $this->assertEquals($expectedResult, $result);
     }
 
     /**
@@ -160,14 +205,11 @@ class CategoryExtensionTest extends \PHPUnit_Framework_TestCase
      *
      * @param array $properties
      *
-     * @return Category
+     * @return CategoryInterface
      */
     protected function getCategoryMock(array $properties = array())
     {
-        $category = $this
-            ->getMockBuilder('Pim\Bundle\CatalogBundle\Entity\Category')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $category = $this->getMock('Pim\Bundle\CatalogBundle\Entity\Category');
 
         if (isset($properties['id'])) {
             $category
@@ -197,5 +239,40 @@ class CategoryExtensionTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue($isRoot));
 
         return $category;
+    }
+
+    /**
+     * Get product mock
+     *
+     * @param array $properties
+     *
+     * @return ProductInterface
+     */
+    protected function getProductMock(array $properties = array())
+    {
+        $product = $this->getMock('Pim\Bundle\CatalogBundle\Entity\Product');
+
+        if (isset($properties['id'])) {
+            $product
+                ->expects($this->any())
+                ->method('getId')
+                ->will($this->returnValue($properties['id']));
+        }
+
+        if (isset($properties['identifier'])) {
+            $product
+                ->expects($this->any())
+                ->method('getIdentifier')
+                ->will($this->returnValue($properties['identifier']));
+        }
+
+        if (isset($properties['label'])) {
+            $product
+                ->expects($this->any())
+                ->method('__toString')
+                ->will($this->returnValue($properties['label']));
+        }
+
+        return $product;
     }
 }
