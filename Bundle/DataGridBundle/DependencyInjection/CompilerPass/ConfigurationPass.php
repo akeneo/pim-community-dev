fc07f5193a20fc5426c83cfa23e56f1fda522f46
Vova Soroka <vsoroka@magecore.com>
2013-11-26 03:40:17 +0200

    BAP-2130: Table reports creation wizard (simplified UI)

--- Bundle/DataGridBundle/DependencyInjection/CompilerPass/ConfigurationPass.php
+++ Bundle/DataGridBundle/DependencyInjection/CompilerPass/ConfigurationPass.php
@@ -9,11 +9,13 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
 class ConfigurationPass implements CompilerPassInterface
 {
-    const MANAGER_SERVICE_ID = 'oro_datagrid.datagrid.manager';
-    const BUILDER_SERVICE_ID = 'oro_datagrid.datagrid.builder';
+    const BUILDER_SERVICE_ID        = 'oro_datagrid.datagrid.builder';
+    const PROVIDER_SERVICE_ID       = 'oro_datagrid.configuration.provider';
+    const CHAIN_PROVIDER_SERVICE_ID = 'oro_datagrid.configuration.provider.chain';
 
     const SOURCE_TAG_NAME    = 'oro_datagrid.datasource';
     const EXTENSION_TAG_NAME = 'oro_datagrid.extension';
+    const PROVIDER_TAG_NAME  = 'oro_datagrid.configuration.provider';
 
     const CONFIG_FILE_NAME = 'datagrid.yml';
     const ROOT_PARAMETER   = 'datagrid';
@@ -23,41 +25,80 @@ class ConfigurationPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $config = [];
+        $this->registerConfigFiles($container);
+        $this->registerConfigProviders($container);
+        $this->registerDataSources($container);
+    }
 
-        /**
-         * Collect and pass datagrid configurations to manager
-         */
-        foreach ($container->getParameter('kernel.bundles') as $bundle) {
-            $reflection = new \ReflectionClass($bundle);
-            if (is_file($file = dirname($reflection->getFilename()) . '/Resources/config/' . self::CONFIG_FILE_NAME)) {
-                $bundleConfig = Yaml::parse(realpath($file));
+    /**
+     * Collect datagrid configurations files and pass them to the configuration provider
+     *
+     * @param ContainerBuilder $container
+     */
+    protected function registerConfigFiles(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition(self::PROVIDER_SERVICE_ID)) {
+            $config = [];
+            foreach ($container->getParameter('kernel.bundles') as $bundle) {
+                $reflection = new \ReflectionClass($bundle);
+                $file       = dirname($reflection->getFilename()) . '/Resources/config/' . self::CONFIG_FILE_NAME;
+                if (is_file($file)) {
+                    $bundleConfig = Yaml::parse(realpath($file));
+                    if (isset($bundleConfig[self::ROOT_PARAMETER]) && is_array($bundleConfig[self::ROOT_PARAMETER])) {
+                        $config = array_merge_recursive($config, $bundleConfig[self::ROOT_PARAMETER]);
+                    }
+                }
+            }
 
-                if (isset($bundleConfig[self::ROOT_PARAMETER]) && is_array($bundleConfig[self::ROOT_PARAMETER])) {
-                    $config = array_merge_recursive($config, $bundleConfig[self::ROOT_PARAMETER]);
+            $configProviderDef = $container->getDefinition(self::PROVIDER_SERVICE_ID);
+            $configProviderDef->replaceArgument(0, $config);
+        }
+    }
+
+    /**
+     * Register all datagrid configuration providers
+     *
+     * @param ContainerBuilder $container
+     */
+    protected function registerConfigProviders(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition(self::CHAIN_PROVIDER_SERVICE_ID)) {
+            $providers = array();
+            foreach ($container->findTaggedServiceIds(self::PROVIDER_TAG_NAME) as $id => $attributes) {
+                $priority = isset($attributes[0]['priority']) ? $attributes[0]['priority'] : 0;
+                $providers[$priority][] = new Reference($id);
+            }
+            if (!empty($providers)) {
+                // sort by priority and flatten
+                krsort($providers);
+                $providers = call_user_func_array('array_merge', $providers);
+                // add to chain provider
+                $chainConfigProviderDef = $container->getDefinition(self::CHAIN_PROVIDER_SERVICE_ID);
+                foreach ($providers as $provider) {
+                    $chainConfigProviderDef->addMethodCall('addProvider', [$provider]);
                 }
             }
         }
+    }
 
-        $manager = $container->getDefinition(self::MANAGER_SERVICE_ID);
-        if ($manager) {
-            $manager->replaceArgument(0, $config);
-        }
-
-        /**
-         * Find and add available datasources and extensions to grid builder
-         */
-        $builder = $container->getDefinition(self::BUILDER_SERVICE_ID);
-        if ($builder) {
+    /**
+     * Find and add available datasources and extensions to grid builder
+     *
+     * @param ContainerBuilder $container
+     */
+    protected function registerDataSources(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition(self::BUILDER_SERVICE_ID)) {
+            $builderDef = $container->getDefinition(self::BUILDER_SERVICE_ID);
             $sources = $container->findTaggedServiceIds(self::SOURCE_TAG_NAME);
             foreach ($sources as $serviceId => $tags) {
                 $tagAttrs = reset($tags);
-                $builder->addMethodCall('registerDatasource', [$tagAttrs['type'], new Reference($serviceId)]);
+                $builderDef->addMethodCall('registerDatasource', [$tagAttrs['type'], new Reference($serviceId)]);
             }
 
             $extensions = $container->findTaggedServiceIds(self::EXTENSION_TAG_NAME);
             foreach ($extensions as $serviceId => $tags) {
-                $builder->addMethodCall('registerExtension', [new Reference($serviceId)]);
+                $builderDef->addMethodCall('registerExtension', [new Reference($serviceId)]);
             }
         }
     }
---
+++ Bundle/QueryDesignerBundle/Tests/Unit/Provider/DatagridConfigurationBuilderTest.php
@@ -0,0 +1,309 @@
+<?php
+
+namespace Oro\Bundle\QueryDesignerBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\QueryDesignerBundle\Provider\DatagridConfigurationBuilder;
+use Oro\Bundle\QueryDesignerBundle\Tests\Unit\Fixtures\QueryDesignerModel;
+
+class DatagridConfigurationBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \Oro\Bundle\QueryDesignerBundle\Exception\InvalidConfigurationException
+     * @expectedExceptionMessage The "columns" definition does not exist.
+     */
+    public function testEmpty()
+    {
+        $model = new QueryDesignerModel();
+        $model->setDefinition(json_encode([]));
+        new DatagridConfigurationBuilder('test_grid', $model);
+    }
+
+    /**
+     * @expectedException \Oro\Bundle\QueryDesignerBundle\Exception\InvalidConfigurationException
+     * @expectedExceptionMessage The "columns" definition must not be empty.
+     */
+    public function testEmptyColumns()
+    {
+        $model = new QueryDesignerModel();
+        $model->setDefinition(json_encode(['columns' => []]));
+        new DatagridConfigurationBuilder('test_grid', $model);
+    }
+
+    public function testNoFilters()
+    {
+        $gridName   = 'test_grid';
+        $en         = 'Acme\Entity\TestEntity';
+        $definition = [
+            'columns' => [
+                ['name' => 'column1', 'label' => 'lbl1', 'sorting' => '']
+            ]
+        ];
+
+        $model = new QueryDesignerModel();
+        $model->setEntity($en);
+        $model->setDefinition(json_encode($definition));
+        $builder = new DatagridConfigurationBuilder($gridName, $model);
+        $result  = $builder->getConfiguration()->toArray();
+
+        $expected = [
+            'source'  => [
+                'type'         => 'orm',
+                'query'        => [
+                    'select' => ['t1.column1 as c1'],
+                    'from'   => [
+                        ['table' => $en, 'alias' => 't1']
+                    ]
+                ],
+                'query_config' => [
+                    'table_aliases'  => [
+                        '' => 't1'
+                    ],
+                    'column_aliases' => [
+                        'column1' => 'c1',
+                    ],
+                ],
+            ],
+            'columns' => [
+                'c1' => ['label' => 'lbl1'],
+            ],
+            'name'    => $gridName
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testNoJoins()
+    {
+        $gridName   = 'test_grid';
+        $en         = 'Acme\Entity\TestEntity';
+        $definition = [
+            'columns' => [
+                ['name' => 'column1', 'label' => 'lbl1', 'sorting' => '']
+            ],
+            'filters' => []
+        ];
+
+        $model = new QueryDesignerModel();
+        $model->setEntity($en);
+        $model->setDefinition(json_encode($definition));
+        $builder = new DatagridConfigurationBuilder($gridName, $model);
+        $result  = $builder->getConfiguration()->toArray();
+
+        $expected = [
+            'source'  => [
+                'type'         => 'orm',
+                'query'        => [
+                    'select' => ['t1.column1 as c1'],
+                    'from'   => [
+                        ['table' => $en, 'alias' => 't1']
+                    ]
+                ],
+                'query_config' => [
+                    'table_aliases'  => [
+                        '' => 't1'
+                    ],
+                    'column_aliases' => [
+                        'column1' => 'c1',
+                    ],
+                ],
+            ],
+            'columns' => [
+                'c1' => ['label' => 'lbl1'],
+            ],
+            'name'    => $gridName
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testJoinFromColumns()
+    {
+        $gridName   = 'test_grid';
+        $en         = 'Acme\Entity\TestEntity';
+        $en1        = 'Acme\Entity\TestEntity1';
+        $definition = [
+            'columns' => [
+                ['name' => 'column1', 'label' => 'lbl1', 'sorting' => ''],
+                ['name' => 'rc1,' . $en1 . '::column2', 'label' => 'lbl2', 'sorting' => ''],
+            ],
+            'filters' => []
+        ];
+
+        $model = new QueryDesignerModel();
+        $model->setEntity($en);
+        $model->setDefinition(json_encode($definition));
+        $builder = new DatagridConfigurationBuilder($gridName, $model);
+        $result  = $builder->getConfiguration()->toArray();
+
+        $expected = [
+            'source'  => [
+                'type'         => 'orm',
+                'query'        => [
+                    'select' => [
+                        't1.column1 as c1',
+                        't2.column2 as c2',
+                    ],
+                    'from'   => [
+                        ['table' => $en, 'alias' => 't1']
+                    ],
+                    'join'   => [
+                        'left' => [
+                            ['join' => 't1.rc1', 'alias' => 't2'],
+                        ]
+                    ]
+                ],
+                'query_config' => [
+                    'table_aliases'  => [
+                        ''            => 't1',
+                        $en . '::rc1' => 't2'
+                    ],
+                    'column_aliases' => [
+                        'column1'                   => 'c1',
+                        'rc1,' . $en1 . '::column2' => 'c2',
+                    ],
+                ],
+            ],
+            'columns' => [
+                'c1' => ['label' => 'lbl1'],
+                'c2' => ['label' => 'lbl2'],
+            ],
+            'name'    => $gridName
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testJoinFromFilters()
+    {
+        $gridName   = 'test_grid';
+        $en         = 'Acme\Entity\TestEntity';
+        $en1        = 'Acme\Entity\TestEntity1';
+        $definition = [
+            'columns' => [
+                ['name' => 'column1', 'label' => 'lbl1', 'sorting' => ''],
+            ],
+            'filters' => [
+                ['columnName' => 'rc1,' . $en1 . '::column2', 'criterion' => []],
+            ]
+        ];
+
+        $model = new QueryDesignerModel();
+        $model->setEntity($en);
+        $model->setDefinition(json_encode($definition));
+        $builder = new DatagridConfigurationBuilder($gridName, $model);
+        $result  = $builder->getConfiguration()->toArray();
+
+        $expected = [
+            'source'  => [
+                'type'         => 'orm',
+                'query'        => [
+                    'select' => [
+                        't1.column1 as c1',
+                    ],
+                    'from'   => [
+                        ['table' => $en, 'alias' => 't1']
+                    ],
+                    'join'   => [
+                        'left' => [
+                            ['join' => 't1.rc1', 'alias' => 't2'],
+                        ]
+                    ]
+                ],
+                'query_config' => [
+                    'table_aliases'  => [
+                        ''            => 't1',
+                        $en . '::rc1' => 't2'
+                    ],
+                    'column_aliases' => [
+                        'column1' => 'c1',
+                    ],
+                ],
+            ],
+            'columns' => [
+                'c1' => ['label' => 'lbl1'],
+            ],
+            'name'    => $gridName
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testComplexQuery()
+    {
+        $gridName   = 'test_grid';
+        $en         = 'Acme\Entity\TestEntity';
+        $en1        = 'Acme\Entity\TestEntity1';
+        $en2        = 'Acme\Entity\TestEntity2';
+        $en3        = 'Acme\Entity\TestEntity3';
+        $definition = [
+            'columns' => [
+                ['name' => 'column1', 'label' => 'lbl1', 'sorting' => 'DESC'],
+                ['name' => 'rc1,' . $en1 . '::column2', 'label' => 'lbl2', 'sorting' => ''],
+                ['name' => 'rc2,' . $en2 . '::column3', 'label' => 'lbl3', 'sorting' => 'ASC'],
+            ],
+            'filters' => [
+                ['columnName' => 'rc1,' . $en1 . '::c2', 'criterion' => []],
+                [
+                    'columnName' => 'rc1,' . $en1 . '::rc4,' . $en3 . '::c5',
+                    'criterion'  => []
+                ],
+            ]
+        ];
+
+        $model = new QueryDesignerModel();
+        $model->setEntity($en);
+        $model->setDefinition(json_encode($definition));
+        $builder = new DatagridConfigurationBuilder($gridName, $model);
+        $result  = $builder->getConfiguration()->toArray();
+
+        $expected = [
+            'source'  => [
+                'type'         => 'orm',
+                'query'        => [
+                    'select' => [
+                        't1.column1 as c1',
+                        't2.column2 as c2',
+                        't4.column3 as c3',
+                    ],
+                    'from'   => [
+                        ['table' => $en, 'alias' => 't1']
+                    ],
+                    'join'   => [
+                        'left' => [
+                            ['join' => 't1.rc1', 'alias' => 't2'],
+                            ['join' => 't2.rc4', 'alias' => 't3'],
+                            ['join' => 't1.rc2', 'alias' => 't4'],
+                        ]
+                    ]
+                ],
+                'query_config' => [
+                    'table_aliases'  => [
+                        ''                              => 't1',
+                        $en . '::rc1'                   => 't2',
+                        $en . '::rc1,' . $en1 . '::rc4' => 't3',
+                        $en . '::rc2'                   => 't4',
+                    ],
+                    'column_aliases' => [
+                        'column1'                   => 'c1',
+                        'rc1,' . $en1 . '::column2' => 'c2',
+                        'rc2,' . $en2 . '::column3' => 'c3',
+                    ],
+                ],
+            ],
+            'columns' => [
+                'c1' => ['label' => 'lbl1'],
+                'c2' => ['label' => 'lbl2'],
+                'c3' => ['label' => 'lbl3'],
+            ],
+            'sorters' => [
+                'default' => [
+                    'c1' => 'DESC',
+                    'c3' => 'ASC',
+                ]
+            ],
+            'name'    => $gridName
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+}
