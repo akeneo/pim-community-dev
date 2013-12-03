7bbbf671480aca0e9af9dc50b9f5e36aec971710
Filips Alpe <filips@akeneo.com>
2013-12-03 10:29:09 +0100

    Create a subscriber to remove duplicate configuration from job forms

---
+++ src/Pim/Bundle/ImportExportBundle/Form/Subscriber/RemoveDuplicateJobConfigurationSubscriber.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Pim\Bundle\ImportExportBundle\Form\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Subscriber to remove duplicated job configuration from the form
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class RemoveDuplicateJobConfigurationSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::POST_SET_DATA => 'postSetData',
+            FormEvents::SUBMIT        => 'submit'
+        );
+    }
+
+    /**
+     * Remove duplicated configuration from the form, it will
+     * be provided to the steps on the 'submit' event
+     *
+     * @param FormEvent $event
+     */
+    public function postSetData(FormEvent $event)
+    {
+        $job = $event->getData();
+        if (null === $job) {
+            return;
+        }
+        $form = $event->getForm();
+
+        $steps = $form->get('steps');
+
+        $existingConfig = array();
+        foreach (array_keys($steps->all()) as $step) {
+            $stepForm = $steps->get($step);
+            foreach (array_keys($stepForm->all()) as $stepElement) {
+                $stepElementForm = $stepForm->get($stepElement);
+                foreach (array_keys($stepElementForm->all()) as $stepElementConfig) {
+                    if (in_array($stepElementConfig, $existingConfig)) {
+                        $stepElementForm->remove($stepElementConfig);
+                    } else {
+                        $existingConfig[] = $stepElementConfig;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Set the current configuration to all the steps of the job
+     * to make it available to steps that duplicate configuration
+     *
+     * @param FormEvent $event
+     */
+    public function submit(FormEvent $event)
+    {
+        $job = $event->getData();
+        if (null === $job) {
+            return;
+        }
+
+        $job->setConfiguration($job->getConfiguration());
+    }
+}
