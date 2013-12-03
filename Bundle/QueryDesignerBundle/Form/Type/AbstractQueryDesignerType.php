64603694238b923f082a6ae2348e859f9123759e
yurio <yurio@ua.fm>
2013-12-03 16:59:25 +0200

    BAP-2130: Fix NPath complexity

--- Bundle/QueryDesignerBundle/Form/Type/AbstractQueryDesignerType.php
+++ Bundle/QueryDesignerBundle/Form/Type/AbstractQueryDesignerType.php
@@ -32,53 +32,34 @@ abstract class AbstractQueryDesignerType extends AbstractType
             );
 
         $factory = $builder->getFormFactory();
-
-        $addFields = function ($form, $entity = null) use ($factory) {
-            $form->add(
-                $factory->createNamed(
-                    'column',
-                    'oro_query_designer_column',
-                    null,
-                    array(
-                        'mapped'             => false,
-                        'column_choice_type' => $form->getConfig()->getOption('column_column_choice_type'),
-                        'entity'             => $entity ? $entity : null,
-                        'auto_initialize'    => false
-                    )
-                )
-            );
-            $form->add(
-                $factory->createNamed(
-                    'filter',
-                    'oro_query_designer_filter',
-                    null,
-                    array(
-                        'mapped'             => false,
-                        'column_choice_type' => $form->getConfig()->getOption('filter_column_choice_type'),
-                        'entity'             => $entity ? $entity : null,
-                        'auto_initialize'    => false
-                    )
-                )
-            );
-        };
-
+        $that    = $this;
         $builder->addEventListener(
             FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($addFields) {
+            function (FormEvent $event) use ($that, $factory) {
                 $form = $event->getForm();
                 /** @var AbstractQueryDesigner $data */
                 $data = $event->getData();
-                $addFields($form, $data ? $data->getEntity() : null);
+                if ($data) {
+                    $entity = $data->getEntity();
+                } else {
+                    $entity = null;
+                }
+                $that->addFields($form, $factory, $entity);
             }
         );
 
         $builder->addEventListener(
             FormEvents::PRE_SUBMIT,
-            function (FormEvent $event) use ($addFields) {
+            function (FormEvent $event) use ($that, $factory) {
                 $form = $event->getForm();
                 /** @var AbstractQueryDesigner $data */
                 $data = $event->getData();
-                $addFields($form, $data ? $data['entity'] : null);
+                if ($data) {
+                    $entity = $data['entity'];
+                } else {
+                    $entity = null;
+                }
+                $that->addFields($form, $factory, $entity);
             }
         );
 
@@ -131,5 +112,42 @@ abstract class AbstractQueryDesignerType extends AbstractType
                 'column_column_choice_type' => 'oro_entity_field_choice',
                 'filter_column_choice_type' => 'oro_entity_field_choice'
             );
+    }
+
+    /**
+     * Adds column and filters sub forms
+     *
+     * @param $form
+     * @param $factory
+     * @param null $entity
+     */
+    protected function addFields($form, $factory, $entity = null)
+    {
+        $form->add(
+            $factory->createNamed(
+                'column',
+                'oro_query_designer_column',
+                null,
+                array(
+                    'mapped'             => false,
+                    'column_choice_type' => $form->getConfig()->getOption('column_column_choice_type'),
+                    'entity'             => $entity ? $entity : null,
+                    'auto_initialize'    => false
+                )
+            )
+        );
+        $form->add(
+            $factory->createNamed(
+                'filter',
+                'oro_query_designer_filter',
+                null,
+                array(
+                    'mapped'             => false,
+                    'column_choice_type' => $form->getConfig()->getOption('filter_column_choice_type'),
+                    'entity'             => $entity ? $entity : null,
+                    'auto_initialize'    => false
+                )
+            )
+        );
     }
 }
