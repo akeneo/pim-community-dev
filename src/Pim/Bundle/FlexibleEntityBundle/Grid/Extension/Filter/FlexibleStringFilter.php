cf69c1757857d003a8cedcefe469cf38c095d524
Nicolas Dupont <nicolas@akeneo.com>
2013-12-03 15:09:34 +0100

    PIM-1595 : get flexible entity report grid from BAP

---
+++ src/Pim/Bundle/FlexibleEntityBundle/Grid/Extension/Filter/FlexibleStringFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\FlexibleEntityBundle\Grid\Extension\Filter;
+
+use Oro\Bundle\FilterBundle\Filter\StringFilter;
+use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+
+class FlexibleStringFilter extends StringFilter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(FilterDatasourceAdapterInterface $ds, $data)
+    {
+        $data = $this->parseData($data);
+        if (!$data) {
+            return false;
+        }
+
+        $operator = $this->getOperator($data['type']);
+
+        $fen = $this->get(FilterUtility::FEN_KEY);
+        $this->util->applyFlexibleFilter(
+            $ds,
+            $fen,
+            $this->get(FilterUtility::DATA_NAME_KEY),
+            $data['value'],
+            $operator
+        );
+
+        return true;
+    }
+}
