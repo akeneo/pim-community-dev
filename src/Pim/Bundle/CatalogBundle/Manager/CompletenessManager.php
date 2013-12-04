dfc7af64f86d0b5e336e341e5740aea12195ee9d
Antoine Guigan <antoine@akeneo.com>
2013-12-04 11:49:32 +0100

    Refactored completeness manager

--- src/Pim/Bundle/CatalogBundle/Manager/CompletenessManager.php
+++ src/Pim/Bundle/CatalogBundle/Manager/CompletenessManager.php
@@ -2,10 +2,11 @@
 
 namespace Pim\Bundle\CatalogBundle\Manager;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\QueryBuilder;
 use Pim\Bundle\CatalogBundle\Doctrine\CompletenessQueryBuilder;
+use Pim\Bundle\CatalogBundle\Entity\AttributeRequirement;
 use Pim\Bundle\CatalogBundle\Entity\Channel;
-use Pim\Bundle\CatalogBundle\Entity\Family;
 use Pim\Bundle\CatalogBundle\Model\ProductInterface;
 use Pim\Bundle\CatalogBundle\Validator\Constraints\ProductValueNotBlank;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -128,48 +129,67 @@ class CompletenessManager
         $localeCodes = $getCodes($locales);
         $completenesses = array_fill_keys($localeCodes, $channelTemplate);
 
-        if ($family) {
-            $allCompletenesses = $this->getCompletenessQB($product)->getQuery()->execute();
-            foreach ($allCompletenesses as $completeness) {
-                $locale = $completeness->getLocale();
-                $channel = $completeness->getChannel();
-                $completenesses[$locale->getCode()][$channel->getCode()]['completeness'] = $completeness;
-            }
-            $requirements = $this->doctrine
-                ->getRepository(get_class($family))
-                ->getFullRequirementsQB($family, $locale)
-                ->getQuery()
-                ->getResult();
+        if (!$family) {
+            return $completenesses;
+        }
 
-            $productValues = $product->getValues();
-            foreach ($requirements as $requirement) {
-                if ($requirement->isRequired()) {
-                    $attribute = $requirement->getAttribute();
-                    $channel = $requirement->getChannel();
-                    foreach ($localeCodes as $localeCode) {
-                        $constraint = new ProductValueNotBlank(array('channel' => $channel));
-                        $valueCode = $attribute->getCode();
-                        if ($attribute->getTranslatable()) {
-                            $valueCode .= '_' .$localeCode;
-                        }
-                        if ($attribute->getScopable()) {
-                            $valueCode .= '_' . $channel->getCode();
-                        }
-                        $missing = false;
-                        if (!isset($productValues[$valueCode])) {
-                            $missing = true;
-                        } elseif ($this->validator->validateValue($productValues[$valueCode], $constraint)->count()) {
-                            $missing = true;
-                        }
-                        if ($missing) {
-                            $completenesses[$localeCode][$channel->getCode()]['missing'][] = $attribute;
-                        }
-                    }
-                }
+        $allCompletenesses = $this->getCompletenessQB($product)->getQuery()->execute();
+        foreach ($allCompletenesses as $completeness) {
+            $locale = $completeness->getLocale();
+            $channel = $completeness->getChannel();
+            $completenesses[$locale->getCode()][$channel->getCode()]['completeness'] = $completeness;
+        }
+        $requirements = $this->doctrine
+            ->getRepository(get_class($family))
+            ->getFullRequirementsQB($family, $locale)
+            ->getQuery()
+            ->getResult();
+
+        $productValues = $product->getValues();
+        foreach ($requirements as $requirement) {
+            if ($requirement->isRequired()) {
+                $this->addRequirementToCompleteness($completenesses, $requirement, $productValues, $localeCodes);
             }
         }
 
         return $completenesses;
+    }
+
+    /**
+     * Adds a requirement to the completenesses
+     *
+     * @param array                $completenesses
+     * @param AttributeRequirement $requirement
+     * @param ArrayCollection      $productValues
+     * @param array                $localeCodes
+     */
+    protected function addRequirementToCompleteness(
+        array &$completenesses,
+        AttributeRequirement $requirement,
+        ArrayCollection $productValues,
+        array $localeCodes
+    ) {
+        $attribute = $requirement->getAttribute();
+        $channel = $requirement->getChannel();
+        foreach ($localeCodes as $localeCode) {
+            $constraint = new ProductValueNotBlank(array('channel' => $channel));
+            $valueCode = $attribute->getCode();
+            if ($attribute->getTranslatable()) {
+                $valueCode .= '_' .$localeCode;
+            }
+            if ($attribute->getScopable()) {
+                $valueCode .= '_' . $channel->getCode();
+            }
+            $missing = false;
+            if (!isset($productValues[$valueCode])) {
+                $missing = true;
+            } elseif ($this->validator->validateValue($productValues[$valueCode], $constraint)->count()) {
+                $missing = true;
+            }
+            if ($missing) {
+                $completenesses[$localeCode][$channel->getCode()]['missing'][] = $attribute;
+            }
+        }
     }
 
     /**
