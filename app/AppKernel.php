870eceb5510c770dce9ae8a5b3f328b8ba539d6b
Nicolas Dupont <nicolas@akeneo.com>
2013-12-04 14:31:50 +0100

    PIM-1616 : use OroKernel to register bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -2,93 +2,49 @@
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Oro\Bundle\DistributionBundle\OroKernel;
 
-class AppKernel extends Kernel
+/**
+ * PIM AppKernel
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class AppKernel extends OroKernel
 {
+    /**
+     * Register bundles
+     *
+     * @return array
+     */
     public function registerBundles()
     {
         $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        $bundles = array_merge(parent::registerBundles(), $bundles);
+
+        $pimDepBundles = array(
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-
-            // BAP deps
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new BeSimple\SoapBundle\BeSimpleSoapBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Escape\WSSEAuthenticationBundle\EscapeWSSEAuthenticationBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
-            new Bazinga\ExposeTranslationBundle\BazingaExposeTranslationBundle(),
-            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
-            new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
-            new JDare\ClankBundle\JDareClankBundle(),
-            new Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle(),
-            new Sylius\Bundle\FlowBundle\SyliusFlowBundle(),
-
-            // PIM deps
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new APY\JsFormValidationBundle\APYJsFormValidationBundle(),
         );
-
-        // BAP bundles
-        $bapBundles = array(
-            new Oro\Bundle\UIBundle\OroUIBundle(),
-            new Oro\Bundle\FormBundle\OroFormBundle(),
-            // new Oro\Bundle\JsFormValidationBundle\OroJsFormValidationBundle(),
-            new Oro\Bundle\SoapBundle\OroSoapBundle(),
-            new Oro\Bundle\SearchBundle\OroSearchBundle(),
-            new Oro\Bundle\SecurityBundle\OroSecurityBundle(),
-            new Oro\Bundle\UserBundle\OroUserBundle(),
-            new Oro\Bundle\MeasureBundle\OroMeasureBundle(),
-            new Oro\Bundle\SegmentationTreeBundle\OroSegmentationTreeBundle(),
-            new Oro\Bundle\NavigationBundle\OroNavigationBundle(),
-            new Oro\Bundle\ConfigBundle\OroConfigBundle(),
-            new Oro\Bundle\FilterBundle\OroFilterBundle(),
-            new Oro\Bundle\GridBundle\OroGridBundle(),
-            new Oro\Bundle\DataGridBundle\OroDataGridBundle(),
-            new Oro\Bundle\WindowsBundle\OroWindowsBundle(),
-            new Oro\Bundle\AddressBundle\OroAddressBundle(),
-            new Oro\Bundle\DataAuditBundle\OroDataAuditBundle(),
-            new Oro\Bundle\TagBundle\OroTagBundle(),
-            new Oro\Bundle\AsseticBundle\OroAsseticBundle(),
-            new Oro\Bundle\TranslationBundle\OroTranslationBundle(),
-            new Oro\Bundle\OrganizationBundle\OroOrganizationBundle(),
-            new Oro\Bundle\NotificationBundle\OroNotificationBundle($this),
-            new Oro\Bundle\EmailBundle\OroEmailBundle(),
-            new Oro\Bundle\EntityBundle\OroEntityBundle(),
-            new Oro\Bundle\EntityConfigBundle\OroEntityConfigBundle(),
-            new Oro\Bundle\EntityExtendBundle\OroEntityExtendBundle($this),
-            new Oro\Bundle\ImapBundle\OroImapBundle(),
-            new Oro\Bundle\CronBundle\OroCronBundle(),
-            new Oro\Bundle\BatchBundle\OroBatchBundle(),
-            new Oro\Bundle\LocaleBundle\OroLocaleBundle(),
-            new Oro\Bundle\WorkflowBundle\OroWorkflowBundle(),
-            new Oro\Bundle\InstallerBundle\OroInstallerBundle(),
-            new Oro\Bundle\ImportExportBundle\OroImportExportBundle(),
-            new Oro\Bundle\RequireJSBundle\OroRequireJSBundle(),
-            new Oro\Bundle\HelpBundle\OroHelpBundle(),
-        );
-
-        $bundles = array_merge($bundles, $bapBundles);
+        $bundles = array_merge($bundles, $pimDepBundles);
 
         $pimBundles = array(
             // BAP overriden bundles
             new Pim\Bundle\NavigationBundle\PimNavigationBundle(),
             new Pim\Bundle\FilterBundle\PimFilterBundle(),
             new Pim\Bundle\GridBundle\PimGridBundle(),
-            //new Pim\Bundle\UserBundle\PimUserBundle(),
+            new Pim\Bundle\UserBundle\PimUserBundle(),
             new Pim\Bundle\SearchBundle\PimSearchBundle(),
             new Pim\Bundle\JsFormValidationBundle\PimJsFormValidationBundle(),
             new Pim\Bundle\DataAuditBundle\PimDataAuditBundle(),
@@ -107,12 +63,6 @@ class AppKernel extends Kernel
         );
 
         $bundles = array_merge($bundles, $pimBundles);
-
-        if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
 
         return $bundles;
     }
