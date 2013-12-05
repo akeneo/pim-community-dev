3c7128b5f762acffd959c0faf86ddb6a0ff625e4
Nicolas Dupont <nicolas@akeneo.com>
2013-12-05 11:07:12 +0100

    PIM-1518 : add a new skip item method to refactor invalid exception raising and skip counter

--- src/Pim/Bundle/ImportExportBundle/Processor/AbstractEntityProcessor.php
+++ src/Pim/Bundle/ImportExportBundle/Processor/AbstractEntityProcessor.php
@@ -89,13 +89,24 @@ abstract class AbstractEntityProcessor extends AbstractConfigurableStepElement i
 
         $identifier = $this->getIdentifier($entity);
         if (in_array($identifier, $this->identifiers)) {
-            $this->stepExecution->incrementSummaryInfo('skip');
-            throw new InvalidItemException(
-                sprintf('Duplicate, the entity "%s" has already been processed', $identifier),
-                $item
-            );
+            $this->skipItem($item, sprintf('Duplicate, the entity "%s" has already been processed', $identifier));
         }
         $this->identifiers[] = $identifier;
+    }
+
+    /**
+     * Skip an item with a detail message
+     *
+     * @param mixed  $item
+     * @param string $message
+     *
+     * @throws InvalidItemException
+     */
+    protected function skipItem($item, $message)
+    {
+        $this->stepExecution->incrementSummaryInfo('skip');
+
+        throw new InvalidItemException($message, $item);
     }
 
     /**
