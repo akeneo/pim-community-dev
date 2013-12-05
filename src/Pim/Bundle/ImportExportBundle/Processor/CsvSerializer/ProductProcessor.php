108f753221a81628e72245a33ea15ec9e56cff14
Nicolas Dupont <nicolas@akeneo.com>
2013-12-05 11:58:32 +0100

    PIM-1518 : fix write counter to manage the with/without header case

--- src/Pim/Bundle/ImportExportBundle/Processor/CsvSerializer/ProductProcessor.php
+++ src/Pim/Bundle/ImportExportBundle/Processor/CsvSerializer/ProductProcessor.php
@@ -64,7 +64,8 @@ class ProductProcessor extends HeterogeneousProcessor
      */
     public function process($products)
     {
-        $this->stepExecution->addSummaryInfo('write', count($products));
+        $nbItems = count($products) - ($this->isWithHeader() ? 1 : 0);
+        $this->stepExecution->addSummaryInfo('write', $nbItems);
 
         $csv =  $this->serializer->serialize(
             $products,
