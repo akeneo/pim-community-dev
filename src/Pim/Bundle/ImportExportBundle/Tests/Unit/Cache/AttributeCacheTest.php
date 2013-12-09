0f52d4062691fb93efb9a2eda80df852b9736982
Benoit Jacquemont <benoit@akeneo.com>
2013-12-09 07:03:07 +0100

    Switch back to Entity for Group

--- src/Pim/Bundle/ImportExportBundle/Tests/Unit/Cache/AttributeCacheTest.php
+++ src/Pim/Bundle/ImportExportBundle/Tests/Unit/Cache/AttributeCacheTest.php
@@ -219,7 +219,7 @@ class AttributeCacheTest extends \PHPUnit_Framework_TestCase
         $groups = array();
         foreach ($categories as $groupCode => $groupAttributeCodes) {
             if (!isset($this->groups[$groupCode])) {
-                $this->groups[$groupCode] = $this->getMock('Pim\Bundle\CatalogBundle\Enity\Group');
+                $this->groups[$groupCode] = $this->getMock('Pim\Bundle\CatalogBundle\Entity\Group');
                 $this->addAttributeCollection($this->groups[$groupCode], $groupCode, $groupAttributeCodes);
             }
             $groups[] = $this->groups[$groupCode];
