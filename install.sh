1c5486d01a4d371f170973cf99d85939bae1df98
Filips Alpe <filips@akeneo.com>
2013-12-13 16:25:31 +0100

    Remove clearing entity extend from install script to prevent errors

--- install.sh
+++ install.sh
@@ -96,7 +96,6 @@ cd $APP_ROOT
 # Execute tasks
 if [ $TASK = 'db' ] || [ $TASK = 'all' ]; then
     # Ignoring the case where the DB does not exist yet
-    php app/console oro:entity-extend:clear
     php app/console doctrine:schema:drop --force --full-database > /dev/null 2>&1 || true
     php app/console doctrine:database:drop --force > /dev/null 2>&1 || true
     php app/console doctrine:database:create
