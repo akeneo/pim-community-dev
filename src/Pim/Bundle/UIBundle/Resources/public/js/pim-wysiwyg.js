b72015f64701d425935b49f92c1202e62c1021fa
Filips Alpe <filips@akeneo.com>
2013-12-18 16:05:57 +0100

    Apply tinymce to basic textareas

---
+++ src/Pim/Bundle/UIBundle/Resources/public/js/pim-wysiwyg.js
@@ -0,0 +1,31 @@
+define(
+    ['tinymce'],
+    function() {
+        var config = {
+            plugins:     'link contextmenu preview code paste',
+            statusbar:   true,
+            menubar:     false,
+            toolbar:     'bold italic underline strikethrough | bullist numlist | outdent indent | link | preview code',
+            contextmenu: 'undo redo link'
+        };
+        return {
+            init: function(id) {
+                var settings = config;
+                settings.selector = '#' + id;
+                tinymce.init(settings);
+
+                return this;
+            },
+            destroy: function(id) {
+                if (tinymce.editors[id]) {
+                    tinymce.editors[id].destroy();
+                }
+
+                return this;
+            },
+            reinit: function(id) {
+                return this.destroy(id).init(id);
+            }
+        };
+    }
+);
