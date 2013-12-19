774c73a9d53b395d984b52b219ff20c265a3c708
Romain Monceau <romain@akeneo.com>
2013-12-19 12:32:59 +0100

    Fix PMD error

--- src/Pim/Bundle/CatalogBundle/Entity/Repository/AttributeOptionRepository.php
+++ src/Pim/Bundle/CatalogBundle/Entity/Repository/AttributeOptionRepository.php
@@ -12,7 +12,8 @@ use Pim\Bundle\FlexibleEntityBundle\Entity\Repository\AttributeOptionRepository 
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AttributeOptionRepository extends FlexAttributeOptionRepository implements OptionRepositoryInterface,
+class AttributeOptionRepository extends FlexAttributeOptionRepository implements
+    OptionRepositoryInterface,
     ReferableEntityRepositoryInterface
 {
     /**
