94d265f6a84d1498bd82431f96ce3f78a936d6f7
Nicolas Dupont <nicolas@akeneo.com>
2013-12-23 00:03:47 +0100

    PIM-1711 : add ProductRepositoryInterface to prepare Mongo impl

---
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ProductRepositoryInterface.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Doctrine;
+
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+use Pim\Bundle\CatalogBundle\Entity\Group;
+
+/**
+ * Product repository interface
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface ProductRepositoryInterface
+{
+    /**
+     * @param string $scope
+     *
+     * @return QueryBuilder
+     */
+    public function buildByScope($scope);
+
+    /**
+     * @param Channel $channel
+     *
+     * @return QueryBuilder
+     */
+    public function buildByChannelAndCompleteness(Channel $channel);
+
+    /**
+     * Find products by existing family
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function findByExistingFamily();
+
+    /**
+     * @param array $ids
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function findByIds(array $ids);
+
+    /**
+     * @return integer[]
+     */
+    public function getAllIds();
+
+    /**
+     * Find all products in a variant group (by variant axis attribute values)
+     *
+     * @param Group $variantGroup the variant group
+     * @param array $criteria     the criteria
+     *
+     * @return array
+     */
+    public function findAllForVariantGroup(Group $variantGroup, array $criteria = array());
+
+    /**
+     * Find products with missing completeness
+     * @param Channel $channel
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function findByMissingCompleteness(Channel $channel);
+
+    /**
+     * Returns a full product with all relations
+     *
+     * @param int $id
+     *
+     * @return \Pim\Bundle\CatalogBundle\Model\ProductInterface
+     */
+    public function getFullProduct($id);
+}
