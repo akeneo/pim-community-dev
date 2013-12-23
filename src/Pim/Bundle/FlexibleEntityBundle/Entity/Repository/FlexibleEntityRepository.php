eb74c27f1827a42ad3f63b1465f9aa42ef452332
Nicolas Dupont <nicolas@akeneo.com>
2013-12-23 19:16:30 +0100

    Fix CS

--- src/Pim/Bundle/FlexibleEntityBundle/Entity/Repository/FlexibleEntityRepository.php
+++ src/Pim/Bundle/FlexibleEntityBundle/Entity/Repository/FlexibleEntityRepository.php
@@ -21,7 +21,9 @@ use Pim\Bundle\FlexibleEntityBundle\Model\AbstractFlexible;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class FlexibleEntityRepository extends EntityRepository implements
-    TranslatableInterface, ScopableInterface, FlexibleEntityRepositoryInterface
+    TranslatableInterface,
+    ScopableInterface,
+    FlexibleEntityRepositoryInterface
 {
     /**
      * Flexible entity config
