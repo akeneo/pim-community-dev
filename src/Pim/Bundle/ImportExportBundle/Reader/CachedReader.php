d5df46a67a5b64d446ccd91213c73a633fced8f6
Antoine Guigan <antoine@akeneo.com>
2013-12-30 10:04:25 +0100

    Added PHPDoc

--- src/Pim/Bundle/ImportExportBundle/Reader/CachedReader.php
+++ src/Pim/Bundle/ImportExportBundle/Reader/CachedReader.php
@@ -6,7 +6,10 @@ use Oro\Bundle\BatchBundle\Item\ItemReaderInterface;
 use Oro\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
 
 /**
- * Cache reader
+ * Cached reader
+ * 
+ * Can be used to have an item step which receives data from a previous step.
+ * 
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
