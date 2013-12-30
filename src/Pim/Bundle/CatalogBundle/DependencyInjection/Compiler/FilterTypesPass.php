f0e39e5d46972dc00409d43b192d8ed21b6a17d3
Nicolas Dupont <nicolas@akeneo.com>
2013-12-30 12:22:26 +0100

    PIM-1767 : ensure that filters are known by filter extension

--- src/Pim/Bundle/CatalogBundle/DependencyInjection/Compiler/FilterTypesPass.php
+++ src/Pim/Bundle/CatalogBundle/DependencyInjection/Compiler/FilterTypesPass.php
@@ -17,18 +17,22 @@ class FilterTypesPass implements CompilerPassInterface
 {
     /**
      * @var string
+     * TODO : use same filter for product datasource ?
      */
-    const FILTER_EXTENSION_ID = 'pim_catalog.grid.extension.filter.orm_product_filter';
-
-    /**
-     * @var string
-     */
-    const PRODUCT_TAG_NAME    = 'oro_filter.extension.orm_product_filter.filter';
+    protected $filterExtensionIds = array(
+        'pim_catalog.grid.extension.filter.orm_filter',
+        'pim_catalog.grid.extension.filter.orm_product_filter'
+    );
 
     /**
      * @®ar string
      */
     const TAG_NAME            = 'oro_filter.extension.orm_filter.filter';
+
+    /**
+     * @var string
+     */
+    const PRODUCT_TAG_NAME    = 'oro_filter.extension.orm_product_filter.filter';
 
     /**
      * {@inheritDoc}
@@ -38,19 +42,21 @@ class FilterTypesPass implements CompilerPassInterface
         /**
          * Find and add available filters to extension
          */
-        $extension = $container->getDefinition(self::FILTER_EXTENSION_ID);
-        if ($extension) {
-            // add base filters
-            $filters = $container->findTaggedServiceIds(self::TAG_NAME);
-            foreach ($filters as $serviceId => $tags) {
-                $tagAttrs = reset($tags);
-                $extension->addMethodCall('addFilter', array($tagAttrs['type'], new Reference($serviceId)));
-            }
-            // add flexible filters
-            $filters = $container->findTaggedServiceIds(self::PRODUCT_TAG_NAME);
-            foreach ($filters as $serviceId => $tags) {
-                $tagAttrs = reset($tags);
-                $extension->addMethodCall('addFilter', array($tagAttrs['type'], new Reference($serviceId)));
+        foreach ($this->filterExtensionIds as $extensionId) {
+            $extension = $container->getDefinition($extensionId);
+            if ($extension) {
+                // add base filters
+                $filters = $container->findTaggedServiceIds(self::TAG_NAME);
+                foreach ($filters as $serviceId => $tags) {
+                    $tagAttrs = reset($tags);
+                    $extension->addMethodCall('addFilter', array($tagAttrs['type'], new Reference($serviceId)));
+                }
+                // add flexible filters
+                $filters = $container->findTaggedServiceIds(self::PRODUCT_TAG_NAME);
+                foreach ($filters as $serviceId => $tags) {
+                    $tagAttrs = reset($tags);
+                    $extension->addMethodCall('addFilter', array($tagAttrs['type'], new Reference($serviceId)));
+                }
             }
         }
     }
