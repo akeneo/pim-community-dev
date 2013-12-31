5b53cfa5ade952a68ba82dc03b7685a425e53fb0
Benoit Jacquemont <benoit@akeneo.com>
2013-12-31 18:53:56 +0100

    PIM-1557 Remove useless newline

--- src/Pim/Bundle/CatalogBundle/Entity/Repository/CategoryRepository.php
+++ src/Pim/Bundle/CatalogBundle/Entity/Repository/CategoryRepository.php
@@ -16,7 +16,6 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 class CategoryRepository extends SegmentRepository implements ReferableEntityRepositoryInterface
 {
     /**
