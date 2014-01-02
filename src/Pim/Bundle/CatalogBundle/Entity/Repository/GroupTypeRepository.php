145fd839cce380bbed96e48c90358bbf22f74173
Nicolas Dupont <nicolas@akeneo.com>
2014-01-02 17:00:47 +0100

    PIM-1767 : add new group types grid implementation, remove useless dep

--- src/Pim/Bundle/CatalogBundle/Entity/Repository/GroupTypeRepository.php
+++ src/Pim/Bundle/CatalogBundle/Entity/Repository/GroupTypeRepository.php
@@ -2,6 +2,8 @@
 
 namespace Pim\Bundle\CatalogBundle\Entity\Repository;
 
+use Pim\Bundle\DataGridBundle\Model\DataGridRepositoryInterface;
+
 /**
  * Group type repository
  *
@@ -9,7 +11,7 @@ namespace Pim\Bundle\CatalogBundle\Entity\Repository;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class GroupTypeRepository extends ReferableEntityRepository
+class GroupTypeRepository extends ReferableEntityRepository implements DataGridRepositoryInterface
 {
     /**
      * @return \Doctrine\ORM\QueryBuilder
@@ -18,5 +20,28 @@ class GroupTypeRepository extends ReferableEntityRepository
     {
         return $this->build()
             ->addOrderBy('group_type.code', 'ASC');
+    }
+
+    /**
+     * {0inheritdoc}
+     */
+    public function createDatagridQueryBuilder()
+    {
+        $rootAlias = 'g';
+        $qb = $this->createQueryBuilder($rootAlias);
+
+        $labelExpr = sprintf(
+            "(CASE WHEN translation.label IS NULL THEN %s.code ELSE translation.label END)",
+            $rootAlias
+        );
+        $qb
+            ->addSelect($rootAlias)
+            ->addSelect(sprintf("%s AS label", $labelExpr))
+            ->addSelect('translation.label');
+
+        $qb
+            ->leftJoin($rootAlias .'.translations', 'translation', 'WITH', 'translation.locale = :localeCode');
+
+       return $qb;
     }
 }
