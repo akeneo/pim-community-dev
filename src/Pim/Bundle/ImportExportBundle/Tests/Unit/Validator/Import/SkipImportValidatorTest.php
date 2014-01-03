ed1f358b08fd5721dd2baf5ad17c3f89fc34b4f9
Antoine Guigan <antoine@akeneo.com>
2014-01-03 11:52:18 +0100

    Fixed CS

--- src/Pim/Bundle/ImportExportBundle/Tests/Unit/Validator/Import/SkipImportValidatorTest.php
+++ src/Pim/Bundle/ImportExportBundle/Tests/Unit/Validator/Import/SkipImportValidatorTest.php
@@ -6,7 +6,7 @@ use Pim\Bundle\ImportExportBundle\Validator\Import\SkipImportValidator;
 
 /**
  * Tests related class
- * 
+ *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
