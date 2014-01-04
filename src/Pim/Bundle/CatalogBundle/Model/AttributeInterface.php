0b38fe422393c493460c315f1bb1a0aec8534e2f
Nicolas Dupont <nicolas@akeneo.com>
2014-01-04 10:35:45 +0100

    PIM-1813 : rename product attribute

--- src/Pim/Bundle/CatalogBundle/Model/AttributeInterface.php
+++ src/Pim/Bundle/CatalogBundle/Model/AttributeInterface.php
@@ -3,12 +3,12 @@
 namespace Pim\Bundle\CatalogBundle\Model;
 
 /**
- * Interface for ProductAttribute
+ * Interface for Product Attribute
  *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-interface ProductAttributeInterface
+interface AttributeInterface
 {
 }
