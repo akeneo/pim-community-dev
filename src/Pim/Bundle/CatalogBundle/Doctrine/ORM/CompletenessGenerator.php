756e17b065913c8e07d44c73bb3e850b5023a53b
Benoit Jacquemont <benoit@akeneo.com>
2014-01-08 13:56:53 +0100

    PIM-1627 Dividing the query in 3 steps to help improve performance and maintenability

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/CompletenessGenerator.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/CompletenessGenerator.php
@@ -16,6 +16,8 @@ use Doctrine\ORM\Mapping\ClassMetadataInfo;
  */
 class CompletenessGenerator implements CompletenessGeneratorInterface
 {
+    const COMPLETE_PRICES_TABLE = 'complete_price';
+    const MISSING_TABLE = 'missing_completeness';
     /**
      * @var RegistryInterface
      */
@@ -47,9 +49,12 @@ class CompletenessGenerator implements CompletenessGeneratorInterface
     /**
      * {@inheritdoc}
      */
-    public function generate(array $criteria, $limit = null)
+    public function generate(array $criteria = array(), $limit = null)
     {
-        $sql = $this->getInsertCompletenessSQL($criteria, $limit);
+        $this->prepareCompletePrices($criteria);
+        $this->prepareMissingCompletenesses($criteria);
+
+        $sql = $this->getInsertCompletenessSQL($criteria);
 
         $stmt = $this->doctrine->getConnection()->prepare($sql);
 
@@ -60,123 +65,236 @@ class CompletenessGenerator implements CompletenessGeneratorInterface
     }
 
     /**
+     * Generate a temporary table that will contains
+     * a line for each required and complete prices.
+     *
+     * This temporary table allows to define indexes
+     * that speed up the linking with this table.
+     *
+     * @param array $criteria
+     */
+    protected function prepareCompletePrices($criteria = array())
+    {
+        $cleanupSql = "DROP TABLE IF EXISTS ".self::COMPLETE_PRICES_TABLE."\n";
+        $cleanupStmt = $this->doctrine->getConnection()->prepare($cleanupSql);
+        $cleanupStmt->execute();
+
+        $sql = $this->getCompletePricesSQL();
+        $sql = $this->applyCriteria($sql, $criteria);
+
+        $sql = "CREATE TEMPORARY TABLE ".
+            self::COMPLETE_PRICES_TABLE.
+            " (locale_id int, channel_id int, value_id int, primary key(locale_id, channel_id, value_id)) ".
+            $sql;
+
+        $sql = strtr($sql, $this->getTableReplacements()) .';';
+
+        $stmt = $this->doctrine->getConnection()->prepare($sql);
+
+        foreach ($criteria as $placeholder => $value) {
+            $stmt->bindValue($placeholder, $value);
+        }
+        $stmt->execute();
+    }
+
+    /**
+     * Generate a temporary table that will contains
+     * the list of completeness that needs to be regenerated
+     *
+     * @param array $criteria
+     */
+    protected function prepareMissingCompletenesses(array $criteria = array())
+    {
+        $cleanupSql = "DROP TABLE IF EXISTS ".self::MISSING_TABLE."\n";
+        $cleanupStmt = $this->doctrine->getConnection()->prepare($cleanupSql);
+        $cleanupStmt->execute();
+
+        $sql = $this->getMissingCompletenessesSQL();
+        $sql = $this->applyCriteria($sql, $criteria);
+
+        $sql = "CREATE TEMPORARY TABLE ".
+            self::MISSING_TABLE.
+            " (locale_id int, channel_id int, product_id int)"
+            .$sql;
+
+        $sql = strtr($sql, $this->getTableReplacements()) .';';
+
+        $stmt = $this->doctrine->getConnection()->prepare($sql);
+
+        foreach ($criteria as $placeholder => $value) {
+            $stmt->bindValue($placeholder, $value);
+        }
+        $stmt->execute();
+    }
+
+    /**
+     * Provides the SQL that allows to aggregate multiple price-currency
+     * tuple in one line only if all the tuples for one product value are complete.
+     *
+     * The price is complete when all  for
+     * all currency are present for the channel and locale.
+     * 
+     * This allow to link with only complete prices
+     *
+     * @param $criteria
+     */
+    protected function getCompletePricesSQL($criteria = array())
+    {
+        return <<<COMPLETE_PRICES_SQL
+            SELECT l.id AS locale_id, c.id AS channel_id, v.id AS value_id
+                FROM pim_catalog_attribute_requirement r
+                JOIN pim_catalog_attribute att ON att.id = r.attribute_id AND att.backend_type = "prices"
+                JOIN pim_catalog_channel c ON c.id = r.channel_id %channel_conditions%
+                JOIN pim_catalog_channel_locale cl ON cl.channel_id = c.id
+                JOIN pim_catalog_locale l ON l.id = cl.locale_id
+                JOIN pim_catalog_channel_currency ccur ON ccur.channel_id = c.id
+                JOIN pim_catalog_currency cur ON cur.id = ccur.currency_id
+                JOIN %product_interface% p ON p.family_id = r.family_id %product_conditions%
+                JOIN %product_value_interface% v
+                    ON (v.scope_code = c.code OR v.scope_code IS NULL)
+                    AND (v.locale_code = l.code OR v.locale_code IS NULL)
+                    AND v.attribute_id = att.id
+                    AND v.entity_id = p.id
+                LEFT JOIN pim_catalog_product_value_price price ON price.value_id = v.id AND price.currency_code = cur.code
+                GROUP BY l.id, c.id, v.id
+                HAVING COUNT(price.data) = COUNT(price.id)
+COMPLETE_PRICES_SQL;
+
+    }
+
+    /**
+     * Provides the SQL that generate the list of
+     * missing completeness from the existing completeness
+     * and the expected completeness.
+     * Note that we use a subquery to get only family in order to boost
+     * the process comparing to joining with all attributes from the requirements
+     * table
+     *
+     * @param $criteria
+     */
+    protected function getMissingCompletenessesSQL($criteria = array())
+    {
+        return <<<MISSING_SQL
+            SELECT l.id AS locale_id, c.id AS channel_id, p.id AS product_id
+            FROM 
+                (SELECT c.id, r.family_id
+                FROM pim_catalog_attribute_requirement r
+                JOIN pim_catalog_channel c ON c.id = r.channel_id %channel_conditions%
+                GROUP BY c.id, r.family_id) AS c
+            JOIN pim_catalog_channel_locale cl ON cl.channel_id = c.id
+            JOIN pim_catalog_locale l ON l.id = cl.locale_id
+            JOIN pim_catalog_product p ON p.family_id = c.family_id %product_conditions%
+            LEFT JOIN pim_catalog_completeness co ON co.product_id = p.id AND co.channel_id = c.id AND co.locale_id = l.id
+            WHERE co.id IS NULL
+MISSING_SQL;
+
+    }
+
+    /**
+     * Provide SQL that will use complete_price
+     * and missing_completeness temporary table
+     * to generate the needed completeness
+     *
+     * 
+
+    /**
+     * Apply criteria to the provided SQL.
+     *
+     * @param string $sql
+     * @param string $criteria
+     *
+     * @return string $sqlWithCriteria
+     */
+    protected function applyCriteria($sql, $criteria)
+    {
+        $productConditions = "";
+        $channelConditions = "";
+
+        if (array_key_exists('productId', $criteria)) {
+            $productConditions = "AND p.id = :productId";
+        }
+
+        if (array_key_exists('channelId', $criteria)) {
+            $channelConditions = "AND c.id = :channelId";
+        }
+
+        $sql = str_replace('%product_conditions%', $productConditions, $sql);
+        $sql = str_replace('%channel_conditions%', $channelConditions, $sql);
+
+        return $sql;
+    }
+
+
+    /**
      * Get the sql query to insert completeness
      *
      * @param array   $criteria
-     * @param inreger $limit
      *
      * @return string
      */
-    protected function getInsertCompletenessSQL(array $criteria, $limit)
+    protected function getInsertCompletenessSQL(array $criteria)
     {
-        $sql = $this->getInsertCompletenessSQLCondition($criteria) . ' GROUP BY p.id, c.id, l.id';
-
-        if ($limit) {
-            $sql .= ' LIMIT ' . $limit;
-        }
-
+        $sql = $this->getMainSqlPart();
+        
         $sql = strtr($sql, $this->getQueryPartReplacements());
 
         return strtr($sql, $this->getTableReplacements()) .';';
     }
 
     /**
-     * Returns the raw SQL query, containing placeholders
+     * Provides the main SQL part
      *
-     * @return string
+     * return string $selectPart
      */
-    protected function getRawQuery()
+    protected function getMainSqlPart()
     {
-        return <<<SQL
+        return <<<MAIN_SQL
             INSERT INTO pim_catalog_completeness (
                 locale_id, channel_id, product_id, ratio, missing_count, required_count
             )
-                SELECT
-                    l.id, c.id, p.id,
-                    (
-                        COUNT(distinct v.id)
-                        / (
-                            SELECT count(*)
-                                FROM pim_catalog_attribute_requirement
-                                WHERE family_id = p.family_id
-                                    AND channel_id = c.id
-                                    AND required = true
-                        )
-                        * 100
-                    ),
-                    (
-                        (
-                            SELECT count(*)
-                                FROM pim_catalog_attribute_requirement
-                                WHERE family_id = p.family_id
-                                    AND channel_id = c.id
-                                    AND required = true
-                        ) - COUNT(distinct v.id)
-                    ),
-                    (
+            SELECT
+                l.id AS locale_id, c.id AS channel_id, p.id AS product_id,
+                (
+                    COUNT(distinct v.id)
+                    / (
                         SELECT count(*)
                             FROM pim_catalog_attribute_requirement
                             WHERE family_id = p.family_id
                                 AND channel_id = c.id
                                 AND required = true
                     )
-                    FROM pim_catalog_attribute_requirement r
-                        JOIN pim_catalog_channel c ON c.id = r.channel_id
-                        JOIN pim_catalog_channel_locale cl ON cl.channel_id = c.id
-                        JOIN pim_catalog_locale l ON l.id = cl.locale_id
-                        JOIN pim_catalog_channel_currency ccur ON ccur.channel_id = c.id
-                        JOIN pim_catalog_currency cur ON cur.id = ccur.currency_id
-                        JOIN %product_interface% p ON p.family_id = r.family_id
-                        JOIN %product_value_interface% v ON v.attribute_id = r.attribute_id
-                            AND (v.scope_code = c.code OR v.scope_code IS NULL)
-                            AND (v.locale_code = l.code OR v.locale_code IS NULL)
-                            AND v.entity_id = p.id
-                        LEFT JOIN pim_catalog_completeness co ON co.product_id = p.id AND co.channel_id = c.id
-                            AND co.locale_id = l.id
-                        %product_value_joins%
-                    WHERE (%product_value_conditions%) AND r.required = true AND co.id IS NULL
-SQL;
-    }
-
-    /**
-     * Get the sql condition to insert completeness
-     *
-     * @param array $criteria
-     *
-     * @return string
-     */
-    protected function getInsertCompletenessSQLCondition(array $criteria)
-    {
-        $sql = $this->getRawQuery();
-
-        if (array_key_exists('product', $criteria) && array_key_exists('channel', $criteria)) {
-            $sql .= <<<SQL
-                        AND p.id = :product
-                        AND c.id = :channel
-SQL;
-        }
-
-        if (array_key_exists('product', $criteria) && !array_key_exists('channel', $criteria)) {
-            $sql .= <<<SQL
-                        AND p.id = :product
-SQL;
-        }
-
-        if (!array_key_exists('product', $criteria) && array_key_exists('channel', $criteria)) {
-            $sql .= <<<SQL
-                        AND p.id IN (
-                            SELECT p.id FROM pim_catalog_channel ch
-                                JOIN %product_interface% p
-                                LEFT JOIN pim_catalog_completeness c
-                                    ON c.product_id = p.id
-                                    AND c.channel_id = ch.id
-                                    WHERE ch.id = :channel AND c.id IS NULL
-                        )
-                        AND c.id = :channel
-SQL;
-        }
-
-        return $sql;
+                    * 100
+                ) AS ratio,
+                (
+                    (
+                        SELECT count(*)
+                            FROM pim_catalog_attribute_requirement
+                            WHERE family_id = p.family_id
+                                AND channel_id = c.id
+                                AND required = true
+                    ) - COUNT(distinct v.id)
+                ) AS missing_count,
+                (
+                    SELECT count(*)
+                        FROM pim_catalog_attribute_requirement
+                        WHERE family_id = p.family_id
+                            AND channel_id = c.id
+                            AND required = true
+                ) AS required_count
+            FROM missing_completeness m
+                JOIN pim_catalog_channel c ON c.id = m.channel_id
+                JOIN pim_catalog_locale l ON l.id = m.locale_id
+                JOIN %product_interface% p ON p.id = m.product_id
+                JOIN pim_catalog_attribute_requirement r ON r.family_id = p.family_id AND r.channel_id = c.id
+                JOIN %product_value_interface% v ON v.attribute_id = r.attribute_id
+                    AND (v.scope_code = c.code OR v.scope_code IS NULL)
+                    AND (v.locale_code = l.code OR v.locale_code IS NULL)
+                    AND v.entity_id = p.id
+                LEFT JOIN complete_price ON complete_price.value_id = v.id AND complete_price.channel_id = c.id AND complete_price.locale_id = l.id
+                %product_value_joins%
+            WHERE (%product_value_conditions% OR complete_price.value_id IS NOT NULL) AND r.required = true
+            GROUP BY p.id, c.id, l.id
+MAIN_SQL;
     }
 
     /**
@@ -294,7 +412,7 @@ SQL;
             case 'Pim\Bundle\CatalogBundle\Model\Metric':
                 return array(sprintf('%s.%s', $prefix, 'data'));
             case 'Pim\Bundle\CatalogBundle\Model\ProductPrice':
-                return array(sprintf('%s.%s', $prefix, 'VID'));
+                return array();
             case 'Pim\Bundle\CatalogBundle\Model\Media':
                 return array(sprintf('%s.%s', $prefix, 'filename'));
             default:
@@ -345,7 +463,7 @@ SQL;
         }
 
         if ($mapping['targetEntity'] === 'Pim\Bundle\CatalogBundle\Model\ProductPrice') {
-            return $this->getProductPriceJoin($mapping, $prefix);
+            return array();
         }
 
         switch ($mapping['type']) {
@@ -392,40 +510,6 @@ SQL;
             default:
                 return array();
         }
-    }
-
-    /**
-     * Returns the SQL joins for the price association
-     *
-     * @param array  $mapping
-     * @param string $prefix
-     *
-     * @return array
-     */
-    protected function getProductPriceJoin($mapping, $prefix)
-    {
-        $sql = <<<SQL
-            LEFT JOIN (
-                SELECT l.id AS LID, c.id AS CID, v.id AS VID
-                FROM pim_catalog_channel c
-                JOIN pim_catalog_channel_locale cl ON cl.channel_id = c.id
-                JOIN pim_catalog_locale l ON l.id = cl.locale_id
-                JOIN pim_catalog_channel_currency ccur ON ccur.channel_id = c.id
-                JOIN pim_catalog_currency cur ON cur.id = ccur.currency_id
-                JOIN %product_value_interface% v ON
-                    (v.scope_code = c.code OR v.scope_code IS NULL)
-                    AND (v.locale_code = l.code OR v.locale_code IS NULL)
-                JOIN pim_catalog_attribute att ON v.attribute_id = att.id AND att.backend_type = "prices"
-                LEFT JOIN pim_catalog_product_value_price %prefix%
-                    ON %prefix%.value_id = v.id AND %prefix%.currency_code = cur.code
-                GROUP BY l.id, c.id, v.id
-                HAVING COUNT(%prefix%.data) = COUNT(%prefix%.id))
-            AS %prefix% ON %prefix%.VID = v.id AND %prefix%.CID = c.id AND %prefix%.LID = l.id
-SQL;
-
-        $sql = str_replace('%prefix%', $prefix, $sql);
-
-        return array($sql);
     }
 
     /**
