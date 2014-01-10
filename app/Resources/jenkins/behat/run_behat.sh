33e8c07bd9ee82fc376bcd5ab4b57503099921b3
Benoit Jacquemont <benoit@akeneo.com>
2014-01-10 10:45:42 +0100

    Use a specific temp dir for behat instead of changing system

--- app/Resources/jenkins/behat/run_behat.sh
+++ app/Resources/jenkins/behat/run_behat.sh
@@ -86,9 +86,7 @@ for FEATURE in $FEATURES; do
                 # (/proc/PID should not exist)
                 PID_VARNAME=PID_$PROC
                 PID="${!PID_VARNAME}"
-                OLD_TMPDIR=$TMPDIR
-                NEW_TMPDIR=`mktemp -d`
-                export TMPDIR=$NEW_TMPDIR
+                export BEHAT_TMPDIR=`mktemp -d`
                 ls /proc/$PID > /dev/null 2>&1
                 if [ $? -ne 0 ]; then
                     export SYMFONY__DATABASE__NAME=$DB_PREFIX$PROC
@@ -98,8 +96,7 @@ for FEATURE in $FEATURES; do
                     eval PID_$PROC=$RESULT
                     FEATURE_NAME=""
                 fi
-                rm -rf $NEW_TMPDIR
-                export TMPDIR=$OLD_TMPDIR
+                rm -rf $BEHAT_TMPDIR
 
             fi
         done
