539a72a7d457be533aefded12d8df4d270996518
Nicolas Dupont <nicolas@akeneo.com>
2014-01-11 14:07:49 +0100

    PIM-1802 : move and rename the flexible grid event listener

--- src/Pim/Bundle/DataGridBundle/EventListener/ConfigureFlexibleGridListener.php
+++ src/Pim/Bundle/DataGridBundle/EventListener/ConfigureFlexibleGridListener.php
@@ -1,5 +1,5 @@
 <?php
-namespace Pim\Bundle\FlexibleEntityBundle\Grid;
+namespace Pim\Bundle\DataGridBundle\EventListener;
 
 use Doctrine\ORM\QueryBuilder;
 
@@ -30,7 +30,7 @@ use Symfony\Component\PropertyAccess\PropertyAccessor;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class EventListener
+class ConfigureFlexibleGridListener
 {
     const FLEXIBLE_ENTITY_PATH = '[flexible_entity]';
 
