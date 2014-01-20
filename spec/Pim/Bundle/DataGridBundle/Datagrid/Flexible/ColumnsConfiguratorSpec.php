2f29e0fe358e39e2b09768bbe688dfc634aacc45
Nicolas Dupont <nicolas@akeneo.com>
2014-01-20 18:57:19 +0100

    PIM-1911 : complete spec for column configurator

--- spec/Pim/Bundle/DataGridBundle/Datagrid/Flexible/ColumnsConfiguratorSpec.php
+++ spec/Pim/Bundle/DataGridBundle/Datagrid/Flexible/ColumnsConfiguratorSpec.php
@@ -4,18 +4,24 @@ namespace spec\Pim\Bundle\DataGridBundle\Datagrid\Flexible;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Pim\Bundle\CatalogBundle\Entity\Attribute;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Configuration as FormatterConfiguration;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
 use Pim\Bundle\DataGridBundle\Datagrid\Flexible\ConfigurationRegistry;
 
 class ColumnsConfiguratorSpec extends ObjectBehavior
 {
-    function let(DatagridConfiguration $configuration, ConfigurationRegistry $registry)
+    function let(DatagridConfiguration $configuration, ConfigurationRegistry $registry, Attribute $sku, Attribute $name)
     {
-        $sku = new Attribute();
-        $sku->setCode('sku');
-        $name = new Attribute();
-        $name->setCode('name');
+        $sku->isUseableAsGridColumn()->willReturn(true);
+        $sku->getAttributeType()->willReturn('pim_catalog_identifier');
+        $sku->getLabel()->willReturn('Sku');
+        $name->isUseableAsGridColumn()->willReturn(true);
+        $name->getAttributeType()->willReturn('pim_catalog_text');
+        $name->getLabel()->willReturn('Name');
+
+        $registry->getConfiguration('pim_catalog_identifier')->willReturn(array('column' => array('identifier_config')));
+        $registry->getConfiguration('pim_catalog_text')->willReturn(array('column' => array('text_config')));
 
         $attributes = array('sku' => $sku, 'name' => $name);
         $this->beConstructedWith($configuration, $registry, $attributes);
@@ -24,5 +30,28 @@ class ColumnsConfiguratorSpec extends ObjectBehavior
     function it_is_a_configurator()
     {
         $this->shouldBeAnInstanceOf('Pim\Bundle\DataGridBundle\Datagrid\Flexible\ConfiguratorInterface');
+    }
+
+    function it_configures_datagrid_columns( $configuration)
+    {
+        $columnConfPath = sprintf('[%s]', FormatterConfiguration::COLUMNS_KEY);
+        $columns = [
+            'sku' => [
+                'identifier_config',
+                'label' => "Sku"
+            ],
+            'family' => [
+                'family_config',
+            ],
+            'name' => [
+                'text_config',
+                'label' => "Name"
+            ]
+        ];
+
+        $configuration->offsetGetByPath($columnConfPath)->willReturn(array('family' => array('family_config')));
+
+        $configuration->offsetSetByPath($columnConfPath, $columns)->shouldBeCalled();
+        $this->configure();
     }
 }
