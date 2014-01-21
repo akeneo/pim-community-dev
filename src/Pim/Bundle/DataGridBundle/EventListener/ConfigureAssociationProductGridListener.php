7fc3937013441123392c763b778136e9bf0ba9b6
Filips Alpe <filips@akeneo.com>
2014-01-21 17:04:06 +0100

    Fix column order in product associations grid

---
+++ src/Pim/Bundle/DataGridBundle/EventListener/ConfigureAssociationProductGridListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\EventListener;
+
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Pim\Bundle\DataGridBundle\Datagrid\Flexible\AssociationProductColumnsConfigurator;
+
+/**
+ * Grid listener to configure columns for association product grid
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ConfigureAssociationProductGridListener extends ConfigureFlexibleGridListener
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getColumnsConfigurator(DatagridConfiguration $datagridConfig, $attributes)
+    {
+        return new AssociationProductColumnsConfigurator($datagridConfig, $this->confRegistry, $attributes);
+    }
+}
