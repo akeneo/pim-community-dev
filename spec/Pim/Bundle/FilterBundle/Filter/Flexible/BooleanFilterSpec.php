f67466acbd00953ed3a223e2382d5acda2bdf226
Gildas Quéméner <gildas.quemener@gmail.com>
2014-01-21 12:04:14 +0100

    Specified the getForm and getMethod of BooleanFilter

--- spec/Pim/Bundle/FilterBundle/Filter/Flexible/BooleanFilterSpec.php
+++ spec/Pim/Bundle/FilterBundle/Filter/Flexible/BooleanFilterSpec.php
@@ -5,9 +5,12 @@ namespace spec\Pim\Bundle\FilterBundle\Filter\Flexible;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Extension\Core\View\ChoiceView;
 use Oro\Bundle\FilterBundle\Form\Type\Filter\BooleanFilterType;
-use Pim\Bundle\FilterBundle\Filter\Flexible\FilterUtility;
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+use Pim\Bundle\FilterBundle\Filter\Flexible\FilterUtility;
 
 class BooleanFilterSpec extends ObjectBehavior
 {
@@ -71,6 +74,64 @@ class BooleanFilterSpec extends ObjectBehavior
         $this->apply($datasource, array('value' => null))->shouldReturn(false);
         $this->apply($datasource, array())->shouldReturn(false);
         $this->apply($datasource, 0)->shouldReturn(false);
+    }
+
+    function it_uses_the_boolean_filter_form_type(FormInterface $form, $factory)
+    {
+        $factory->create(BooleanFilterType::NAME, [], ['csrf_protection' => false])->willReturn($form);
+
+        $this->getForm()->shouldReturn($form);
+    }
+
+    function it_generates_choices_metadata(
+        FormInterface $form,
+        FormView $formView,
+        FormView $fieldView,
+        FormView $typeView,
+        ChoiceView $yesChoice,
+        ChoiceView $noChoice,
+        ChoiceView $maybeChoice,
+        $factory,
+        $utility
+    ) {
+        $utility->getParamMap()->willReturn([]);
+        $utility->getExcludeParams()->willReturn([]);
+        $factory->create(BooleanFilterType::NAME, [], ['csrf_protection' => false])->willReturn($form);
+        $form->createView()->willReturn($formView);
+
+        $formView->children = array('value' => $fieldView, 'type' => $typeView);
+        $formView->vars     = array('populate_default' => true);
+        $fieldView->vars    = array('multiple' => true, 'choices' => array($yesChoice, $noChoice));
+        $typeView->vars     = array('choices' => array($maybeChoice));
+
+        $yesChoice->label = 'Yes';
+        $yesChoice->value = 1;
+        $noChoice->label  = 'No';
+        $noChoice->value  = 0;
+
+        $this->getMetadata()->shouldReturn(
+            [
+                'name'                 => 'foo',
+                'label'                => 'Foo',
+                'choices'              => [
+                    [
+                        'label' => 'Yes',
+                        'value' => 1,
+                    ],
+                    [
+                        'label' => 'No',
+                        'value' => 0,
+                    ]
+                ],
+                'enabled'              => true,
+                'flexible_entity_name' => 'baz',
+                'data_name'            => 'bar',
+                'contextSearch'        => false,
+                'populateDefault'      => true,
+                'type'                 => 'multichoice',
+            ]
+        );
+
     }
 
     public function getMatchers()
