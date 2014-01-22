c2887ee33810e336eb32d8cdb7b52661422b4cdd
Nicolas Dupont <nicolas@akeneo.com>
2014-01-22 15:56:19 +0100

    PIM-1907 : avoid the use of acl helper in the pim grid

---
+++ src/Pim/Bundle/DataGridBundle/Extension/Pager/Orm/Pager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Extension\Pager\Orm;
+
+use Oro\Bundle\DataGridBundle\Extension\Pager\Orm\Pager as OroPager;
+use Oro\Bundle\BatchBundle\ORM\Query\QueryCountCalculator;
+
+/**
+ * Our custom pager to disable the use of acl helper
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Pager extends OroPager
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function computeNbResult()
+    {
+        $qb    = clone $this->getQueryBuilder();
+        $query = $qb->setFirstResult(null)
+            ->setMaxResults(null)
+            ->resetDQLPart('orderBy')
+            ->getQuery();
+
+        return QueryCountCalculator::calculateCount($query);
+    }
+}
