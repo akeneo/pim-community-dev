df283a39ccb92a88acdd37e4d026d4915016c12a
Nicolas Dupont <nicolas@akeneo.com>
2014-01-22 15:56:49 +0100

    PIM-1898 : add missing phpdoc

--- src/Pim/Bundle/FilterBundle/Filter/Flexible/EntityFilter.php
+++ src/Pim/Bundle/FilterBundle/Filter/Flexible/EntityFilter.php
@@ -7,6 +7,13 @@ use Oro\Bundle\FilterBundle\Filter\EntityFilter as OroEntityFilter;
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
 use Pim\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
 
+/**
+ * Flexible filter
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class EntityFilter extends OroEntityFilter
 {
     const BACKEND_TYPE_KEY = 'backend_type';
--- src/Pim/Bundle/FilterBundle/Filter/Flexible/NumberFilter.php
+++ src/Pim/Bundle/FilterBundle/Filter/Flexible/NumberFilter.php
@@ -5,6 +5,13 @@ namespace Pim\Bundle\FilterBundle\Filter\Flexible;
 use Oro\Bundle\FilterBundle\Filter\NumberFilter as OroNumberFilter;
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
 
+/**
+ * Flexible filter
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class NumberFilter extends OroNumberFilter
 {
     /**
