8605fa06a660b112659e6d58f2b62f456c9edd2d
Filips Alpe <filips@akeneo.com>
2014-01-22 11:08:07 +0100

    Use our own pager for pim grids

--- src/Pim/Bundle/DataGridBundle/Extension/Pager/OrmPagerExtension.php
+++ src/Pim/Bundle/DataGridBundle/Extension/Pager/OrmPagerExtension.php
@@ -2,10 +2,12 @@
 
 namespace Pim\Bundle\DataGridBundle\Extension\Pager;
 
-use Oro\Bundle\DataGridBundle\Extension\Pager\OrmPagerExtension as OroOrmPagerExtension;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Datagrid\Builder;
-use Pim\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
+use Oro\Bundle\DataGridBundle\Datasource\DatasourceInterface;
+use Oro\Bundle\DataGridBundle\Extension\Pager\OrmPagerExtension as OroOrmPagerExtension;
+use Oro\Bundle\DataGridBundle\Extension\Toolbar\ToolbarExtension;
+use Pim\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource as PimOrmDatasource;
 
 /**
  * Orm pager extension
@@ -21,7 +23,7 @@ class OrmPagerExtension extends OroOrmPagerExtension
      */
     public function isApplicable(DatagridConfiguration $config)
     {
-        return $this->matchDatasource($config) && !$this->isFlexible($config);
+        return $this->matchDatasource($config);
     }
 
     /**
@@ -31,16 +33,56 @@ class OrmPagerExtension extends OroOrmPagerExtension
      */
     protected function matchDatasource(DatagridConfiguration $config)
     {
-        return $config->offsetGetByPath(Builder::DATASOURCE_TYPE_PATH) == OrmDatasource::TYPE;
+        return $config->offsetGetByPath(Builder::DATASOURCE_TYPE_PATH) == PimOrmDatasource::TYPE;
     }
 
     /**
-     * @param DatagridConfiguration $config
-     *
-     * @return boolean
+     * {@inheritDoc}
      */
-    protected function isFlexible(DatagridConfiguration $config)
+    public function visitDatasource(DatagridConfiguration $config, DatasourceInterface $datasource)
     {
-        return isset($config['flexible_entity']);
+        $defaultPerPage = $config->offsetGetByPath(ToolbarExtension::PAGER_DEFAULT_PER_PAGE_OPTION_PATH, 10);
+
+        // override to fix paging results
+        $cloneQb = clone $datasource->getQueryBuilder();
+
+        // prepare query to get entity ids
+        $rootEntity = current($cloneQb->getRootEntities());
+        $rootAlias = $cloneQb->getRootAlias();
+        $rootField = $rootAlias.'.id';
+        $cloneQb->add(
+            'from',
+            new \Doctrine\ORM\Query\Expr\From($rootEntity, $rootAlias, $rootField),
+            false
+        );
+        $cloneQb->groupBy($rootField);
+
+        // configure pager
+        $this->pager->setQueryBuilder($cloneQb);
+        $this->pager->setPage($this->getOr(self::PAGE_PARAM, 1));
+        $this->pager->setMaxPerPage($this->getOr(self::PER_PAGE_PARAM, $defaultPerPage));
+        $this->pager->init();
+
+        // get entity ids
+        $results = $cloneQb->getQuery()
+            ->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
+        $ids = array_keys($results);
+
+        // update query selection
+        if (count($ids) > 0) {
+            $datasource->getQueryBuilder()
+                ->andWhere($rootField.' IN (:entityIds)')
+                ->setParameter('entityIds', $ids);
+        }
+    }
+
+    /**
+     * Should pass at the very end (after filters and sorters)
+     *
+     * {@inheritdoc}
+     */
+    public function getPriority()
+    {
+        return -300;
     }
 }
