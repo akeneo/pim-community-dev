518058f4588769b56e1d1e608aa90aa46613aeb3
Nicolas Dupont <nicolas@akeneo.com>
2014-01-26 13:52:03 +0100

    PIM-1718 : Fix cs

--- src/Pim/Bundle/FlexibleEntityBundle/DependencyInjection/PimFlexibleEntityExtension.php
+++ src/Pim/Bundle/FlexibleEntityBundle/DependencyInjection/PimFlexibleEntityExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Flexible entity extension
