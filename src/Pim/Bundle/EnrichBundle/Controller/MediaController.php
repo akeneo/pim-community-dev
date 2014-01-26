755e9fd696c93af4eb4cac6cd7d7269075dd18ed
Nicolas Dupont <nicolas@akeneo.com>
2014-01-26 17:09:11 +0100

    PIM-1927 : move media controller

--- src/Pim/Bundle/EnrichBundle/Controller/MediaController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/MediaController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\CatalogBundle\Controller;
+namespace Pim\Bundle\EnrichBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContextInterface;
