7372848677a26f2cd8fb49309957f69df32e2d7e
Nicolas Dupont <nicolas@akeneo.com>
2014-01-26 15:56:06 +0100

    PIM-1743 : introduce the new EnrichBundle

---
+++ src/Pim/Bundle/EnrichBundle/PimEnrichBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Enrich bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PimEnrichBundle extends Bundle
+{
+}
