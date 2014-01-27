1fab0b0dd5c57038711f450cdec26a286820dd4f
Filips Alpe <filips@akeneo.com>
2014-01-27 17:32:29 +0100

    PIM-1928 Move prepend config to EnrichBundle

--- src/Pim/Bundle/CatalogBundle/DependencyInjection/PimCatalogExtension.php
+++ src/Pim/Bundle/CatalogBundle/DependencyInjection/PimCatalogExtension.php
@@ -3,11 +3,9 @@
 namespace Pim\Bundle\CatalogBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Yaml\Parser as YamlParser;
 use Symfony\Component\Finder\Finder;
 
 /**
@@ -15,7 +13,7 @@ use Symfony\Component\Finder\Finder;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class PimCatalogExtension extends Extension implements PrependExtensionInterface
+class PimCatalogExtension extends Extension
 {
     /** @staticvar string */
     const DOCTRINE_ORM = 'doctrine/orm';
@@ -49,42 +47,6 @@ class PimCatalogExtension extends Extension implements PrependExtensionInterface
 
         $this->loadStorageDriver($config, $container);
         $this->loadValidationFiles($container);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        $bundles = $container->getParameter('kernel.bundles');
-
-        $prependConfig = array(
-            'TwigBundle'                     => 'twig',
-            'AsseticBundle'                  => 'assetic',
-            'DoctrineBundle'                 => 'doctrine',
-            'KnpPaginatorBundle'             => 'knp_paginator',
-            'FOSRestBundle'                  => 'fos_rest',
-            'FOSJsRoutingBundle'             => 'fos_js_routing',
-            'BeSimpleSoapBundle'             => 'be_simple_soap',
-            'StofDoctrineExtensionsBundle'   => 'stof_doctrine_extensions',
-            'EscapeWSSEAuthenticationBundle' => 'escape_wsse_authentication',
-            'LiipImagineBundle'              => 'liip_imagine',
-            'GenemuFormBundle'               => 'genemu_form',
-            'OroSearchBundle'                => 'oro_search',
-            'OroUIBundle'                    => 'oro_ui',
-            'OroTranslationBundle'           => 'oro_translation',
-            'JMSDiExtraBundle'               => 'jms_di_extra',
-            'OroEntityExtendBundle'          => 'oro_entity_extend',
-            'OroFilterBundle'                => 'oro_filter',
-            'OroBatchBundle'                 => 'oro_batch',
-            'KnpGaufretteBundle'             => 'knp_gaufrette',
-        );
-
-        foreach ($prependConfig as $bundle => $alias) {
-            if (isset($bundles[$bundle])) {
-                $this->prependExtensionConfig($container, $alias);
-            }
-        }
     }
 
     /**
@@ -145,57 +107,5 @@ class PimCatalogExtension extends Extension implements PrependExtensionInterface
         $container->setParameter($this->getAlias().'.storage_driver', $storageDriver);
         // Parameter defining if the mapping driver must be enabled or not
         $container->setParameter($this->getAlias().'.storage_driver.'.$storageDriver, true);
-    }
-
-    /**
-     * Prepend configuration of a bundle to the container
-     *
-     * @param ContainerBuilder $container
-     * @param string           $extensionAlias
-     */
-    private function prependExtensionConfig(ContainerBuilder $container, $extensionAlias)
-    {
-        $container->prependExtensionConfig(
-            $extensionAlias,
-            $this->getBundleConfig($extensionAlias)
-        );
-    }
-
-    /**
-     * Get the bundle configuration from a file
-     *
-     * @param string $extensionAlias
-     *
-     * @return array
-     */
-    private function getBundleConfig($extensionAlias)
-    {
-        $configFile = realpath(
-            sprintf('%s/../Resources/config/bundles/%s.yml', __DIR__, $extensionAlias)
-        );
-
-        if (!is_file($configFile)) {
-            throw new \InvalidArgumentException(
-                sprintf('Could not load file %s', $configFile)
-            );
-        }
-
-        $yamlParser = new YamlParser();
-        $config = $yamlParser->parse(file_get_contents($configFile));
-
-        if (!array_key_exists($extensionAlias, $config)) {
-            $configKeys = array_keys($config);
-
-            throw new \RuntimeException(
-                sprintf(
-                    'Found file %s but it didn\'t start with "%s", got "%s" instead.',
-                    $configFile,
-                    $extensionAlias,
-                    reset($configKeys)
-                )
-            );
-        }
-
-        return $config[$extensionAlias];
     }
 }
