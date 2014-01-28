16373d1bf5e7716cf8a85c71e20cf358f9b83da3
Nicolas Dupont <nicolas@akeneo.com>
2014-01-28 21:54:57 +0100

    PIM-1959 : move replace pim serializer compiler pass

--- src/Pim/Bundle/TransformBundle/DependencyInjection/Compiler/ReplacePimSerializerArgumentsPass.php
+++ src/Pim/Bundle/TransformBundle/DependencyInjection/Compiler/ReplacePimSerializerArgumentsPass.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\ImportExportBundle\DependencyInjection\Compiler;
+namespace Pim\Bundle\TransformBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
