12bdc3bd3c2e0326136ceb4127c2946d2f802bb5
Filips Alpe <filips@akeneo.com>
2014-01-29 12:54:03 +0100

    Improve UserContextSpec

--- spec/Pim/Bundle/UserBundle/Context/UserContextSpec.php
+++ spec/Pim/Bundle/UserBundle/Context/UserContextSpec.php
@@ -10,7 +10,10 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Oro\Bundle\SecurityBundle\SecurityFacade;
 use Pim\Bundle\CatalogBundle\Manager\LocaleManager;
 use Pim\Bundle\CatalogBundle\Manager\ChannelManager;
+use Pim\Bundle\CatalogBundle\Manager\CategoryManager;
 use Pim\Bundle\CatalogBundle\Entity\Locale;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+use Pim\Bundle\CatalogBundle\Entity\Category;
 
 class UserContextSpec extends ObjectBehavior
 {
@@ -19,16 +22,21 @@ class UserContextSpec extends ObjectBehavior
         SecurityFacade $securityFacade,
         LocaleManager $localeManager,
         ChannelManager $channelManager,
+        CategoryManager $categoryManager,
         TokenInterface $token,
         User $user,
         Locale $en,
         Locale $fr,
-        Locale $de
+        Locale $de,
+        Channel $ecommerce,
+        Channel $mobile,
+        Category $firstTree,
+        Category $secondTree
     ) {
         $securityContext->getToken()->willReturn($token);
         $token->getUser()->willReturn($user);
 
-        $en->getCode()->willReturn('en_EN');
+        $en->getCode()->willReturn('en_US');
         $fr->getCode()->willReturn('fr_FR');
         $de->getCode()->willReturn('de_DE');
 
@@ -36,15 +44,17 @@ class UserContextSpec extends ObjectBehavior
         $fr->isActivated()->willReturn(true);
         $de->isActivated()->willReturn(true);
 
-        $localeManager->getLocaleByCode('en_EN')->willReturn($en);
+        $localeManager->getLocaleByCode('en_US')->willReturn($en);
         $localeManager->getLocaleByCode('fr_FR')->willReturn($fr);
         $localeManager->getLocaleByCode('de_DE')->willReturn($de);
 
         $localeManager->getActiveLocales()->willReturn([$en, $fr, $de]);
+        $channelManager->getChannels()->willReturn([$mobile, $ecommerce]);
+        $categoryManager->getTrees()->willReturn([$firstTree, $secondTree]);
 
         $securityFacade->isGranted(Argument::any())->willReturn(true);
 
-        $this->beConstructedWith($securityContext, $securityFacade, $localeManager, $channelManager);
+        $this->beConstructedWith($securityContext, $securityFacade, $localeManager, $channelManager, $categoryManager);
     }
 
     function it_provides_locale_from_the_request_if_it_has_been_set(Request $request, $fr)
@@ -65,11 +75,69 @@ class UserContextSpec extends ObjectBehavior
     {
         $this->getCurrentLocale()->shouldReturn($en);
     }
+
+    function it_throws_an_exception_if_user_doesnt_have_access_to_any_activated_locales($securityFacade)
+    {
+        $securityFacade->isGranted(Argument::any())->willReturn(false);
+
+        $this->shouldThrow(new \Exception("User doesn't have access to any activated locales"))->duringGetCurrentLocale();
+    }
+
+    function its_get_current_locale_code_method_returns_a_locale_code()
+    {
+        $this->getCurrentLocaleCode()->shouldReturn('en_US');
+    }
+
+    function its_get_user_locales_method_returns_all_locales_available_to_the_user($en, $fr, $de)
+    {
+        $this->getUserLocales()->shouldReturn([$en, $fr, $de]);
+    }
+
+    function its_get_user_locale_codes_method_returns_all_locale_codes_available_to_the_user()
+    {
+        $this->getUserLocaleCodes()->shouldReturn(['en_US', 'fr_FR', 'de_DE']);
+    }
+
+    function its_get_user_channel_method_returns_user_channel_if_available(User $user, $ecommerce)
+    {
+        $user->getCatalogScope()->willReturn($ecommerce);
+        $this->getUserChannel()->shouldReturn($ecommerce);
+    }
+
+    function its_get_user_channel_method_returns_the_first_available_channel_if_user_channel_is_not_available($mobile)
+    {
+        $this->getUserChannel()->shouldReturn($mobile);
+    }
+
+    function its_get_user_channel_code_method_returns_a_channel_code($mobile)
+    {
+        $mobile->getCode()->willReturn('mobile');
+        $this->getUserChannelCode()->shouldReturn('mobile');
+    }
+
+    function its_get_user_tree_method_returns_user_tree_if_available(User $user, $secondTree)
+    {
+        $user->getDefaultTree()->willReturn($secondTree);
+        $this->getUserTree()->shouldReturn($secondTree);
+    }
+
+    function its_get_user_tree_method_returns_the_first_available_tree_if_user_tree_is_not_available($firstTree)
+    {
+        $this->getUserTree()->shouldReturn($firstTree);
+    }
 }
 
 class User
 {
     public function getCatalogLocale()
+    {
+    }
+
+    public function getCatalogScope()
+    {
+    }
+
+    public function getDefaultTree()
     {
     }
 }
