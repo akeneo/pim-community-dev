80ac8b1ce7525828a1b5ca60c2eb5110bce3da5b
Filips Alpe <filips@akeneo.com>
2014-01-29 12:21:48 +0100

    Move user related listeners to enrich and userbundle

--- src/Pim/Bundle/EnrichBundle/EventListener/UserContextListener.php
+++ src/Pim/Bundle/EnrichBundle/EventListener/UserContextListener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\CatalogBundle\EventListener;
+namespace Pim\Bundle\EnrichBundle\EventListener;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernel;
