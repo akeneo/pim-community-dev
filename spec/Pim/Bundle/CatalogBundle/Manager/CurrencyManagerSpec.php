f5ff1c07ebfff234b922915cd3cd5b8f83d3676e
Romain Monceau <romain@akeneo.com>
2014-01-31 13:52:53 +0100

    PIM-1870: Add php spec on CurrencyManager

--- spec/Pim/Bundle/CatalogBundle/Manager/CurrencyManagerSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Manager/CurrencyManagerSpec.php
@@ -2,6 +2,8 @@
 
 namespace spec\Pim\Bundle\CatalogBundle\Manager;
 
+use Pim\Bundle\CatalogBundle\Entity\Currency;
+
 use Pim\Bundle\CatalogBundle\Entity\Repository\CurrencyRepository;
 
 use PhpSpec\ObjectBehavior;
@@ -10,9 +12,10 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class CurrencyManagerSpec extends ObjectBehavior
 {
-    function let(ObjectManager $objectManager)
+    function let(ObjectManager $objectManager, CurrencyRepository $repository)
     {
         $this->beConstructedWith($objectManager);
+        $objectManager->getRepository('PimCatalogBundle:Currency')->willReturn($repository);
     }
 
     function it_is_initializable()
@@ -20,11 +23,51 @@ class CurrencyManagerSpec extends ObjectBehavior
         $this->shouldHaveType('Pim\Bundle\CatalogBundle\Manager\CurrencyManager');
     }
 
-    function it_provides_currencies(ObjectManager $objectManager, CurrencyRepository $repository)
-    {
-        $objectManager->getRepository('PimCatalogBundle:Currency')->willReturn($repository);
-        $repository->findBy(array())->willReturn(array('EUR', 'USD'));
-        $this->getCurrencies()->shouldBeArray();
-        $this->getCurrencies()->shouldHaveCount(2);
+    function it_provides_active_currencies(
+        $repository,
+        Currency $eur,
+        Currency $usd
+    ) {
+        $repository->findBy(array('activated' => true))->willReturn([$eur, $usd]);
+
+        $this->getActiveCurrencies()->shouldReturn([$eur, $usd]);
+    }
+
+    function it_provides_currencies(
+        $repository,
+        Currency $eur,
+        Currency $usd,
+        Currency $gbp
+    ) {
+        $repository->findBy(array())->willReturn([$eur, $usd, $gbp]);
+        $this->getCurrencies()->shouldReturn([$eur, $usd, $gbp]);
+
+        $criterias = ['foo' => 'bar'];
+        $repository->findBy($criterias)->willReturn([$eur, $gbp]);
+        $this->getCurrencies($criterias)->shouldReturn([$eur, $gbp]);
+    }
+
+    function it_provides_active_currency_codes(
+        $repository,
+        Currency $eur,
+        Currency $usd
+    ) {
+        $eur->getCode()->willReturn('EUR');
+        $usd->getCode()->willReturn('USD');
+
+        $repository->findBy(array('activated' => true))->willReturn([$eur, $usd]);
+        $this->getActiveCodes()->shouldReturn(['EUR', 'USD']);
+    }
+
+    function it_provides_active_currency_code_choices(
+        $repository,
+        Currency $eur,
+        Currency $usd
+    ) {
+        $eur->getCode()->willReturn('EUR');
+        $usd->getCode()->willReturn('USD');
+
+        $repository->findBy(array('activated' => true))->willReturn([$eur, $usd]);
+        $this->getActiveCodeChoices()->shouldReturn(['EUR' => 'EUR', 'USD' => 'USD']);
     }
 }
