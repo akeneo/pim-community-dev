ec4dadb1322040a37f7aa5c63a6c1a36160dc7a2
Nicolas Dupont <nicolas@akeneo.com>
2014-02-04 18:04:16 +0100

    PIM-1852 : retrieve a fix (ff7248350142d497dc1611983201b3dff357b5e4) from BAP master to be able to fix doctrine version

--- Bundle/EntityExtendBundle/OroEntityExtendBundle.php
+++ Bundle/EntityExtendBundle/OroEntityExtendBundle.php
@@ -9,13 +9,12 @@ use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
-
 use Oro\Bundle\EntityExtendBundle\Exception\RuntimeException;
 
 use Oro\Bundle\EntityExtendBundle\DependencyInjection\Compiler\EntityManagerPass;
 use Symfony\Component\Process\Process;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendClassLoadingUtils;
+use Oro\Bundle\EntityBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 
 class OroEntityExtendBundle extends Bundle
 {
