64944f2e62890d63a421dd9ee378ef6064d1b653
Nicolas Dupont <nicolas@akeneo.com>
2014-02-18 11:49:59 +0100

    PIM-1891 : add flexible single option formatter

---
+++ src/Pim/Bundle/DataGridBundle/Extension/Formatter/Property/FlexibleOptionProperty.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Extension\Formatter\Property;
+
+/**
+ * Flexible field property, able to render attribute type which use single option as backend
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FlexibleOptionProperty extends FlexibleFieldProperty
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function convertValue($value)
+    {
+        $data = $this->getBackendData($value);
+
+        $result= '['.$data['code'].']';
+
+        return $result;
+    }
+}
