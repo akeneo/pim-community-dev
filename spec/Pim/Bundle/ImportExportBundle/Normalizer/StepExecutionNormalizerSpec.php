791881342e1fdbb4a9dffaed0137d2c7ec88ac2a
Gildas Quéméner <gildas.quemener@gmail.com>
2014-02-19 15:28:01 +0100

    PIM-1773 Used normalization context to translate step execution

--- spec/Pim/Bundle/ImportExportBundle/Normalizer/StepExecutionNormalizerSpec.php
+++ spec/Pim/Bundle/ImportExportBundle/Normalizer/StepExecutionNormalizerSpec.php
@@ -84,4 +84,62 @@ class StepExecutionNormalizerSpec extends ObjectBehavior
             ]
         );
     }
+
+    function it_normalizes_a_step_execution_instance_using_context_parameters_to_translate(
+        StepExecution $stepExecution,
+        BatchStatus $status,
+        $translator
+    ) {
+        $stepExecution->getStepName()->willReturn('export');
+        $translator->trans('export', [], 'step', 'fr_FR')->willReturn('Export step');
+
+        $stepExecution->getSummary()->willReturn([]);
+
+        $stepExecution->getStatus()->willReturn($status);
+        $status->__toString()->willReturn('PENDING');
+
+        $stepExecution->getStartTime()->willReturn(null);
+        $stepExecution->getEndTime()->willReturn(null);
+
+        $stepExecution->getWarnings()->willReturn(
+            [
+                [
+                    'name'             => 'a_warning',
+                    'reason'           => 'warning_reason',
+                    'reasonParameters' => ['foo' => 'bar'],
+                    'item'             => ['a' => 'A', 'b' => 'B', 'c' => 'C'],
+                ]
+            ]
+        );
+        $translator->trans('a_warning', [], 'step', 'fr_FR')->willReturn('Reader');
+        $translator->trans('warning_reason', ['foo' => 'bar'], 'step', 'fr_FR')->willReturn('WARNING!');
+
+        $stepExecution->getFailureExceptions()->willReturn(
+            [
+                [
+                    'message'           => 'a_failure',
+                    'messageParameters' => ['foo' => 'bar'],
+                ]
+            ]
+        );
+        $translator->trans('a_failure', ['foo' => 'bar'], 'step', 'fr_FR')->willReturn('FAIL!');
+
+        $this->normalize($stepExecution, 'any', ['translationDomain' => 'step', 'translationLocale' => 'fr_FR'])->shouldReturn(
+            [
+               'label'     => 'Export step',
+               'status'    => 'PENDING',
+               'summary'   => [],
+               'startedAt' => null,
+               'endedAt'   => null,
+               'warnings'  => [
+                   [
+                       'label'  => 'Reader',
+                       'reason' => 'WARNING!',
+                       'item'   => ['a' => 'A', 'b' => 'B', 'c' => 'C'],
+                   ]
+               ],
+               'failures'  => ['FAIL!'],
+            ]
+        );
+    }
 }
