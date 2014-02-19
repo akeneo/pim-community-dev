8b064800ad4e39f10d80922751efca43141cef2c
Nicolas Dupont <nicolas@akeneo.com>
2014-02-19 18:00:32 +0100

    PIM-1891 : fix filter configurator

--- spec/Pim/Bundle/DataGridBundle/Datagrid/Flexible/FiltersConfiguratorSpec.php
+++ spec/Pim/Bundle/DataGridBundle/Datagrid/Flexible/FiltersConfiguratorSpec.php
@@ -8,10 +8,21 @@ use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\FilterBundle\Grid\Extension\Configuration as FilterConfiguration;
 use Pim\Bundle\DataGridBundle\Datagrid\Flexible\ConfigurationRegistry;
 use Pim\Bundle\CatalogBundle\Entity\Attribute;
+use Pim\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
 
 class FiltersConfiguratorSpec extends ObjectBehavior
 {
     function let(DatagridConfiguration $configuration, ConfigurationRegistry $registry)
+    {
+        $this->beConstructedWith($configuration, $registry, 'Pim/Catalog/Product');
+    }
+
+    function it_is_a_configurator()
+    {
+        $this->shouldBeAnInstanceOf('Pim\Bundle\DataGridBundle\Datagrid\Flexible\ConfiguratorInterface');
+    }
+
+    function it_configures_datagrid_filters(DatagridConfiguration $configuration, ConfigurationRegistry $registry)
     {
         $attributes = [
             'sku' => [
@@ -25,24 +36,10 @@ class FiltersConfiguratorSpec extends ObjectBehavior
                 'label' => 'Name',
                 'useableAsGridFilter' => 1,
                 'attributeType' => 'pim_catalog_text'
-            ],
-            'desc' => [
-                'code'  => 'desc',
-                'label' => 'Desc',
-                'useableAsGridFilter' => 0,
-                'attributeType' => 'pim_catalog_text'
-            ],
+            ]
         ];
-        $this->beConstructedWith($configuration, $registry, $attributes, 'Pim/Catalog/Product');
-    }
+        $configuration->offsetGetByPath(OrmDatasource::USEABLE_ATTRIBUTES_PATH)->willReturn($attributes);
 
-    function it_is_a_configurator()
-    {
-        $this->shouldBeAnInstanceOf('Pim\Bundle\DataGridBundle\Datagrid\Flexible\ConfiguratorInterface');
-    }
-
-    function it_configures_datagrid_filters(DatagridConfiguration $configuration, ConfigurationRegistry $registry)
-    {
         $registry->getConfiguration('pim_catalog_identifier')->willReturn(array('filter' => array('identifier_config')));
         $registry->getConfiguration('pim_catalog_text')->willReturn(array('filter' => array('text_config')));
 
@@ -54,35 +51,60 @@ class FiltersConfiguratorSpec extends ObjectBehavior
             "label" => "Sku",
             "enabled" => true
         ];
-        $configuration->offsetSetByPath($columnConfPath, $expectedConf);
-
+        $configuration->offsetSetByPath($columnConfPath, $expectedConf)->willReturn($configuration);
         $columnConfPath = sprintf('%s[%s]', FilterConfiguration::COLUMNS_PATH, 'name');
         $expectedConf = [
-            0 => "identifier_config",
+            0 => "text_config",
             "flexible_entity_name" => "Pim/Catalog/Product",
             "data_name" => "name",
             "label" => "Name",
-            "enabled" => true
+            "enabled" => false
         ];
-        $configuration->offsetSetByPath($columnConfPath, $expectedConf);
+        $configuration->offsetSetByPath($columnConfPath, $expectedConf)->willReturn($configuration);
 
         $this->configure();
     }
 
-    /*
-     * TODO : to fix, how to changes the attributes parameter
-    function it_cannot_handle_misconfigured_attribute_type(DatagridConfiguration $configuration, ConfigurationRegistry $registry, Attribute $sku, Attribute $name)
+    function it_cannot_handle_misconfigured_attribute_type(DatagridConfiguration $configuration, ConfigurationRegistry $registry)
     {
-        $sku->isUseableAsGridFilter()->willReturn(true);
-        $sku->getAttributeType()->willReturn('pim_catalog_identifier');
-        $sku->getLabel()->willReturn('Sku');
-        $name->isUseableAsGridFilter()->willReturn(true);
-        $name->getAttributeType()->willReturn('pim_catalog_text');
-        $name->getLabel()->willReturn('Name');
+        $attributes = [
+            'sku' => [
+                'code'  => 'sku',
+                'label' => 'Sku',
+                'useableAsGridFilter' => 1,
+                'attributeType' => 'pim_catalog_identifier'
+            ],
+            'name' => [
+                'code'  => 'name',
+                'label' => 'Name',
+                'useableAsGridFilter' => 1,
+                'attributeType' => 'pim_catalog_text'
+            ]
+        ];
+        $configuration->offsetGetByPath(OrmDatasource::USEABLE_ATTRIBUTES_PATH)->willReturn($attributes);
 
         $registry->getConfiguration('pim_catalog_identifier')->willReturn(array('filter' => array('identifier_config')));
         $registry->getConfiguration('pim_catalog_text')->willReturn(array());
 
+        $columnConfPath = sprintf('%s[%s]', FilterConfiguration::COLUMNS_PATH, 'sku');
+        $expectedConf = [
+            0 => "identifier_config",
+            "flexible_entity_name" => "Pim/Catalog/Product",
+            "data_name" => "sku",
+            "label" => "Sku",
+            "enabled" => true
+        ];
+        $configuration->offsetSetByPath($columnConfPath, $expectedConf)->willReturn($configuration);
+        $columnConfPath = sprintf('%s[%s]', FilterConfiguration::COLUMNS_PATH, 'name');
+        $expectedConf = [
+            0 => "text_config",
+            "flexible_entity_name" => "Pim/Catalog/Product",
+            "data_name" => "name",
+            "label" => "Name",
+            "enabled" => false
+        ];
+        $configuration->offsetSetByPath($columnConfPath, $expectedConf)->willReturn($configuration);
+
         $this->shouldThrow('\LogicException')->duringConfigure();
-    }*/
+    }
 }
