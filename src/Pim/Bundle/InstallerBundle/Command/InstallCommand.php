625771b3fdecfe3a5df02c3a620064aa0a973084
Romain Monceau <romain@akeneo.com>
2014-02-24 18:46:29 +0100

    PIM-2060: Exit with error code

--- src/Pim/Bundle/InstallerBundle/Command/InstallCommand.php
+++ src/Pim/Bundle/InstallerBundle/Command/InstallCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 
-use Oro\Bundle\InstallerBundle\CommandExecutor;
+use Pim\Bundle\InstallerBundle\CommandExecutor;
 
 /**
  * Override OroInstaller command to add PIM custom rules
