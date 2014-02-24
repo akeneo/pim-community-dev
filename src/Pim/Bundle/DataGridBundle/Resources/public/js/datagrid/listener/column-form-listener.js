c055a23519835c00066fdedc25deddbd8e4ebd25
Filips Alpe <filips@akeneo.com>
2014-02-24 20:00:27 +0100

    Add a dedicated column for entity association grids

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/listener/column-form-listener.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/listener/column-form-listener.js
@@ -8,8 +8,26 @@ define(
          * changing of field selectors dynamically using mediator
          */
         var ColumnFormListener = OroColumnFormListener.extend({
+            collection: null,
             initialize: function(options) {
                 OroColumnFormListener.prototype.initialize.apply(this, arguments);
+
+                mediator.on('datagrid_collection_set_after', function (collection, $grid) {
+                    if (collection.inputName === this.gridName) {
+                        this.collection = collection;
+                        this.$el = $grid.find('table.grid thead th:not([style])').first();
+
+                        var $input = $('<input type="checkbox">').css('margin', 0);
+                        this.$el.empty().html($input);
+
+                        $input.on('click', _.bind(function() {
+                            var state = $input.is(':checked');
+                            _.each(this.collection.models, function(model) {
+                                model.set(this.columnName, state);
+                            }, this);
+                        }, this));
+                    }
+                }, this);
 
                 mediator.bind('column_form_listener:set_selectors:' + this.gridName, function (selectors) {
                     this._clearState();
