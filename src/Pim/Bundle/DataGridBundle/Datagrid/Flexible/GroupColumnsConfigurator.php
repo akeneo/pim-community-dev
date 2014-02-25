0af9e599b5d77fee11ee23b4ba2926d38919fe19
Nicolas Dupont <nicolas@akeneo.com>
2014-02-25 18:17:49 +0100

    PIM-2035 : specializethe group columns configurator to add only axis

--- src/Pim/Bundle/DataGridBundle/Datagrid/Flexible/GroupColumnsConfigurator.php
+++ src/Pim/Bundle/DataGridBundle/Datagrid/Flexible/GroupColumnsConfigurator.php
@@ -22,6 +22,11 @@ class GroupColumnsConfigurator extends ColumnsConfigurator
     protected $group;
 
     /**
+     * @param array
+     */
+    protected $axisColumns;
+
+    /**
      * @param DatagridConfiguration $configuration the grid config
      * @param ConfigurationRegistry $registry      the conf registry
      * @param Group                 $group         the current group
@@ -41,53 +46,50 @@ class GroupColumnsConfigurator extends ColumnsConfigurator
      */
     public function configure()
     {
-        $attributes = $this->configuration->offsetGetByPath(OrmDatasource::USEABLE_ATTRIBUTES_PATH);
-        $propertiesColumns = $this->configuration->offsetGetByPath(
-            sprintf('[%s]', FormatterConfiguration::COLUMNS_KEY)
-        );
+        $this->preparePropertiesColumns();
+        $this->prepareAttributesColumns();
+        $this->prepareAxisColumns();
+        $this->sortColumns();
+        $this->addColumns();
+    }
 
-        $editableColumn = array();
-        $labelColumn = array();
-        foreach ($propertiesColumns as $columnCode => $columnData) {
-            if (isset($columnData['editable'])) {
-                $editableColumn[$columnCode] = $columnData;
-                unset($propertiesColumns[$columnCode]);
-            } elseif ($columnCode === 'label') {
-                $labelColumn[$columnCode] = $columnData;
-                unset($propertiesColumns[$columnCode]);
-            }
-        }
-        $identifierColumn = array();
+    /**
+     * {@inheritdoc}
+     */
+    protected function prepareAxisColumns()
+    {
+        $attributes = $this->configuration->offsetGetByPath(OrmDatasource::USEABLE_ATTRIBUTES_PATH);
         $axisCodes = array_map(
             function ($attribute) {
                 return $attribute->getCode();
             },
             $this->group->getAttributes()->toArray()
         );
-        $axisColumns = array();
+        $this->axisColumns = array();
 
         foreach ($attributes as $attributeCode => $attribute) {
             $attributeType     = $attribute['attributeType'];
             $attributeTypeConf = $this->registry->getConfiguration($attributeType);
 
             if ($attributeTypeConf && $attributeTypeConf['column']) {
-                if ($attributeType === 'pim_catalog_identifier') {
+                if (in_array($attributeCode, $axisCodes)) {
                     $columnConfig = $attributeTypeConf['column'];
                     $columnConfig = $columnConfig + array(
                         'label' => $attribute['label'],
                     );
-                    $identifierColumn[$attributeCode] = $columnConfig;
-                } elseif (in_array($attributeCode, $axisCodes)) {
-                    $columnConfig = $attributeTypeConf['column'];
-                    $columnConfig = $columnConfig + array(
-                        'label' => $attribute['label'],
-                    );
-                    $axisColumns[$attributeCode] = $columnConfig;
+                    $this->axisColumns[$attributeCode] = $columnConfig;
                 }
             }
         }
+    }
 
-        $columns = $editableColumn + $identifierColumn + $labelColumn + $axisColumns + $propertiesColumns;
-        $this->configuration->offsetSetByPath(sprintf('[%s]', FormatterConfiguration::COLUMNS_KEY), $columns);
+    /**
+     * Sort the columns
+     *
+     * @return null
+     */
+    protected function sortColumns()
+    {
+        $this->sortedColumns = $this->editableColumns + $this->identifierColumn + $this->axisColumns + $this->propertiesColumns;
     }
 }
