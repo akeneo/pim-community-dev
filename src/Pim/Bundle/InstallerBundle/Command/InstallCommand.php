9a1dbd40f29d6e6e5dcb9640fb00edc80101f4d7
Romain Monceau <romain@akeneo.com>
2014-02-25 12:27:02 +0100

    Clean install command moving assets and database loading outside

--- src/Pim/Bundle/InstallerBundle/Command/InstallCommand.php
+++ src/Pim/Bundle/InstallerBundle/Command/InstallCommand.php
@@ -29,11 +29,6 @@ class InstallCommand extends ContainerAwareCommand
     const TASK_CHECK  = 'check';
     const TASK_DB     = 'db';
 
-    const LOAD_ALL    = 'all';
-    const LOAD_ORO    = 'OroPlatform';
-
-    const LOAD_FIXTURES_TIMEOUT = 0;
-
     /**
      * @var CommandExecutor $commandExecutor
      */
@@ -54,13 +49,6 @@ class InstallCommand extends ContainerAwareCommand
                 InputOption::VALUE_REQUIRED,
                 'Determines tasks called for installation (can be all, check, db or assets)',
                 self::TASK_ALL
-            )
-            ->addOption(
-                'fixtures',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Determines fixtures to load (can be just OroPlatform or all)',
-                self::LOAD_ALL
             );
     }
 
@@ -131,8 +119,7 @@ class InstallCommand extends ContainerAwareCommand
      */
     protected function checkStep(InputInterface $input, OutputInterface $output)
     {
-        $this->commandExecutor
-            ->runCommand('pim:installer:check-requirements', $this->getDefaultParams($input));
+        $this->commandExecutor->runCommand('pim:installer:check-requirements', $this->getDefaultParams($input));
 
         return $this;
     }
@@ -147,131 +134,7 @@ class InstallCommand extends ContainerAwareCommand
      */
     protected function databaseStep(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Prepare database schema</info>');
-        $defaultParams = $this->getDefaultParams($input);
-
-        $this->commandExecutor
-            ->runCommand('doctrine:database:drop', $defaultParams + array('--force' => true))
-            ->runCommand('doctrine:database:create', $defaultParams);
-
-        $connection = $this->getContainer()->get('doctrine')->getConnection();
-
-        if ($connection->isConnected()) {
-            $connection->close();
-        }
-
-        $this->commandExecutor
-            ->runCommand('doctrine:schema:create', $defaultParams)
-            ->runCommand('oro:entity-config:init', $defaultParams)
-            ->runCommand('oro:entity-extend:init', $defaultParams)
-            ->runCommand(
-                'oro:entity-extend:update-config',
-                $defaultParams + array('--process-isolation' => true)
-            )
-            ->runCommand(
-                'doctrine:schema:update',
-                $defaultParams + array('--process-isolation' => true, '--force' => true, '--no-interaction' => true)
-            );
-
-        $this
-            ->loadFixturesStep($input, $output)
-            ->launchCommands($input, $output);
-
-        return $this;
-    }
-
-    /**
-     * Step where fixtures are loaded
-     *
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return InstallCommand
-     */
-    protected function loadFixturesStep(InputInterface $input, OutputInterface $output)
-    {
-        if ($input->getOption('env') === 'behat') {
-            $input->setOption('fixtures', self::LOAD_ORO);
-        }
-        $defaultParams = $this->getDefaultParams($input);
-
-        $output->writeln('<info>Load fixtures.</info>');
-
-        $params =
-            $defaultParams
-            + array(
-                '--process-isolation' => true,
-                '--no-interaction' => true,
-                '--append' => true,
-                '--process-timeout' => static::LOAD_FIXTURES_TIMEOUT
-            )
-            + $this->getFixturesList($input->getOption('fixtures'));
-
-        $this->commandExecutor->runCommand('doctrine:fixtures:load', $params);
-
-        $output->writeln('');
-
-        return $this;
-    }
-
-    /**
-     * Get fixtures to load list
-     *
-     * @param string $fixtureOpt
-     *
-     * @return array
-     */
-    protected function getFixturesList($fixtureOpt)
-    {
-        if ($fixtureOpt === self::LOAD_ORO) {
-            $bundles = $this->getContainer()->getParameter('kernel.bundles');
-
-            $basePath = realpath($this->getContainer()->getParameter('kernel.root_dir') . DIRECTORY_SEPARATOR .'..');
-            $finder = new Finder();
-
-            foreach ($bundles as $bundleName => $bundleNamespace) {
-                if (strpos($bundleNamespace, 'Oro\\') === 0) {
-                    $bundle = $this->getContainer()->get('kernel')->getBundle($bundleName);
-                    $finder->in($bundle->getPath());
-                }
-            }
-            // Oro User Bundle overriden by Pim User Bundle, but we still need the data fixtures inside OroUserBundle
-            $finder->in($basePath."/vendor/oro/platform/src/Oro/Bundle/UserBundle");
-            $directories = $finder
-                ->path('/^DataFixtures$/')
-                ->directories();
-
-            $oroFixtures = array();
-            foreach ($directories as $directory) {
-                $oroFixtures[] = $directory->getPathName();
-            }
-
-            return array('--fixtures' => $oroFixtures);
-        }
-
-        return array();
-    }
-
-    /**
-     * Launchs all commands needed after fixtures loading
-     *
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return InstallCommand
-     */
-    protected function launchCommands(InputInterface $input, OutputInterface $output)
-    {
-        $defaultParams = $this->getDefaultParams($input);
-
-        $this->commandExecutor
-            ->runCommand('oro:search:create-index')
-            ->runCommand(
-                'pim:search:reindex',
-                $defaultParams + array('locale' => $this->getContainer()->getParameter('locale'))
-            )
-            ->runCommand('pim:versioning:refresh', $defaultParams)
-            ->runCommand('pim:completeness:calculate', $defaultParams);
+        $this->commandExecutor->runCommand('pim:installer:db', $this->getDefaultParams($input));
 
         return $this;
     }
@@ -286,19 +149,7 @@ class InstallCommand extends ContainerAwareCommand
      */
     protected function assetsStep(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Preparing application.</info>');
-        $defaultParams = $this->getDefaultParams($input);
-
-        $this->commandExecutor
-            ->runCommand('oro:navigation:init', $defaultParams)
-            ->runCommand('fos:js-routing:dump', $defaultParams + array('--target' => 'web/js/routes.js'))
-            ->runCommand('oro:localization:dump', $defaultParams)
-            ->runCommand('assets:install', $defaultParams)
-            ->runCommand('assetic:dump', $defaultParams)
-            ->runCommand('oro:assetic:dump', $defaultParams)
-            ->runCommand('oro:translation:dump', $defaultParams);
-
-        $output->writeln('');
+        $this->commandExecutor->runCommand('pim:installer:assets', $this->getDefaultParams($input));
 
         return $this;
     }
@@ -320,23 +171,6 @@ class InstallCommand extends ContainerAwareCommand
         $params = $dumper->parse();
         $params['system']['installed'] = $installed;
         $dumper->dump($params);
-    }
-
-    /**
-     * Run clear cache command
-     *
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return CommandExecutor
-     */
-    protected function clearCache(InputInterface $input, OutputInterface $output)
-    {
-        $defaultParams = $this->getDefaultParams($input);
-
-        return $this->commandExecutor
-            ->runCommand('oro:entity-extend:clear', $defaultParams)
-            ->runCommand('cache:clear', $defaultParams);
     }
 
     /**
