3c2847bb187d961f534822ac22b6ea33b92db090
Filips Alpe <filips@akeneo.com>
2014-02-26 16:03:21 +0100

    Move attribute icon config to a dedicated parameters file

--- src/Pim/Bundle/EnrichBundle/DependencyInjection/PimEnrichExtension.php
+++ src/Pim/Bundle/EnrichBundle/DependencyInjection/PimEnrichExtension.php
@@ -35,6 +35,7 @@ class PimEnrichExtension extends Extension implements PrependExtensionInterface
         $loader->load('datagrid_listeners.yml');
         $loader->load('repositories.yml');
         $loader->load('colors.yml');
+        $loader->load('attribute_icons.yml');
 
         if ($config['record_mails']) {
             $loader->load('mail_recorder.yml');
