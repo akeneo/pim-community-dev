ded62fa70da9a78139d863950ce5d51945082ca2
Nicolas Dupont <nicolas@akeneo.com>
2014-02-27 18:33:11 +0100

    PIM-2035 : rename CompletenessFilter to ProductCompletenessFilter to be consistent

--- src/Pim/Bundle/FilterBundle/Filter/ProductCompletenessFilter.php
+++ src/Pim/Bundle/FilterBundle/Filter/ProductCompletenessFilter.php
@@ -16,7 +16,7 @@ use Pim\Bundle\CatalogBundle\Entity\Repository\ProductRepository;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CompletenessFilter extends BooleanFilter
+class ProductCompletenessFilter extends BooleanFilter
 {
     /**
      * @var ProductRepository
