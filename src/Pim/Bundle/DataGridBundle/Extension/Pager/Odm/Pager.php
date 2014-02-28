d2248bf525c3c394bd3db8e0843b70a99c060eec
Nicolas Dupont <nicolas@akeneo.com>
2014-03-01 00:23:44 +0100

    PIM-2127 : add dummy ODM pager

---
+++ src/Pim/Bundle/DataGridBundle/Extension/Pager/Odm/Pager.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Extension\Pager\Odm;
+
+use Doctrine\ODM\MongoDB\Query\Builder as QueryBuilder;
+use Oro\Bundle\DataGridBundle\Extension\Pager\PagerInterface;
+use Oro\Bundle\DataGridBundle\Extension\Pager\AbstractPager;
+
+
+/**
+ * Our custom orm pager
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Pager extends AbstractPager implements PagerInterface
+{
+    /** @var QueryBuilder */
+    protected $qb;
+
+    public function __construct($maxPerPage = 10, QueryBuilder $qb = null)
+    {
+        $this->qb = $qb;
+        parent::__construct($maxPerPage);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        // TODO : to implement
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResults($hydrationMode = Query::HYDRATE_OBJECT)
+    {
+        // TODO: to implement
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function retrieveObject($offset)
+    {
+        // TODO : to implement
+        return null;
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     *
+     * @return $this
+     */
+    public function setQueryBuilder(QueryBuilder $qb)
+    {
+        $this->qb = $qb;
+
+        return $this;
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    public function getQueryBuilder()
+    {
+        return $this->qb;
+    }
+}
