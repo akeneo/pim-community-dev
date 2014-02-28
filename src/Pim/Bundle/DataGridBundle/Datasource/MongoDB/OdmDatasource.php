2266bdc0c3bb6a708144915ec682b0e66067407e
Nicolas Dupont <nicolas@akeneo.com>
2014-02-28 23:30:36 +0100

    PIM-2127 : experiment simple mongo datasource

---
+++ src/Pim/Bundle/DataGridBundle/Datasource/MongoDB/OdmDatasource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Datasource\MongoDB;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\DataGridBundle\Datasource\DatasourceInterface;
+use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
+use Pim\Bundle\DataGridBundle\Datagrid\Flexible\ContextConfigurator;
+
+/**
+ * Basic PIM data source, allow to prepare query builder from repository
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class OdmDatasource implements DatasourceInterface
+{
+    /**
+     * @var string
+     */
+    const TYPE = 'pim_odm_mongodb';
+
+    /** @var QueryBuilder */
+    protected $qb;
+
+    /** @var ObjectManager */
+    protected $om;
+
+    /**
+     * @param ObjectManager $om
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(DatagridInterface $grid, array $config)
+    {
+        if (!isset($config['entity'])) {
+            throw new \Exception(get_class($this).' expects to be configured with entity');
+        }
+
+        $entity = $config['entity'];
+        $repository = $this->om->getRepository($entity);
+
+        if (isset($config['repository_method']) && $method = $config['repository_method']) {
+            $this->qb = $repository->$method();
+        } else {
+            $this->qb = $repository->createQueryBuilder('o');
+        }
+
+        $this->isFlexible = isset($config['is_flexible']) ? (bool) $config['is_flexible'] : false;
+        $localeKey = ContextConfigurator::DISPLAYED_LOCALE_KEY;
+        $this->localeCode = isset($config[$localeKey]) ? $config[$localeKey] : null;
+
+        $grid->setDatasource(clone $this);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResults()
+    {
+        $query = $this->qb
+            ->hydrate(false)
+            ->getQuery();
+        $results = $query->execute();
+
+        $rows    = [];
+        foreach ($results as $result) {
+            $result['id']= $result['_id']->__toString();
+            $result['dataLocale']= $this->localeCode;
+            $rows[] = new ResultRecord($result);
+        }
+
+        return $rows;
+    }
+}
