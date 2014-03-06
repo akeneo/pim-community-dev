70eaab1dd8692bcd9c9ce0007f420a767b1fb56d
Nicolas Dupont <nicolas@akeneo.com>
2014-03-06 12:18:05 +0100

    PIM-2127 : detect storage driver from container parameter and load hydrator config file

--- src/Pim/Bundle/DataGridBundle/DependencyInjection/PimDataGridExtension.php
+++ src/Pim/Bundle/DataGridBundle/DependencyInjection/PimDataGridExtension.php
@@ -6,12 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-
-// TODO : refactor this part to be able to load storage configuration from others bundles than catalog
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Pim\Bundle\CatalogBundle\DependencyInjection\PimCatalogExtension;
-use Pim\Bundle\CatalogBundle\DependencyInjection\Configuration;
-use Symfony\Component\Finder\Finder;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -34,74 +29,14 @@ class PimDataGridExtension extends Extension
         $loader->load('pagers.yml');
         $loader->load('extensions.yml');
         $loader->load('actions.yml');
+        $loader->load('hydrators.yml');
         $loader->load('mass_actions.yml');
         $loader->load('event_listeners.yml');
         $loader->load('attribute_types.yml');
 
-        // process configuration to validation and merge
-        $config = $this->processConfiguration(new Configuration(), $configs);
-        $this->loadStorageDriver($config, $container);
-        $this->loadValidationFiles($container);
-    }
-
-    /**
-     * Loads the validation files
-     *
-     * @param ContainerBuilder $container
-     */
-    protected function loadValidationFiles(ContainerBuilder $container)
-    {
-        // load validation files
-        $dirs = array();
-        foreach ($container->getParameter('kernel.bundles') as $bundle) {
-            $reflection = new \ReflectionClass($bundle);
-            $dir = dirname($reflection->getFileName()) . '/Resources/config/validation';
-            if (is_dir($dir)) {
-                $dirs[] = $dir;
-            }
-        }
-        $finder = new Finder();
-        $mappingFiles = array();
-        foreach ($finder->files()->in($dirs) as $file) {
-            $mappingFiles[$file->getBasename('.yml')] = $file->getRealPath();
-        }
-        $container->setParameter(
-            'validator.mapping.loader.yaml_files_loader.mapping_files',
-            array_merge(
-                $container->getParameter('validator.mapping.loader.yaml_files_loader.mapping_files'),
-                array_values($mappingFiles)
-            )
-        );
-    }
-
-    /**
-     * Provides the supported driver for product storage
-     * @return string[]
-     */
-    protected function getSupportedStorageDrivers()
-    {
-        return array(PimCatalogExtension::DOCTRINE_ORM, PimCatalogExtension::DOCTRINE_MONGODB_ODM);
-    }
-
-    /**
-     * Load the mapping for product and product storage
-     * @param array            $config
-     * @param ContainerBuilder $container
-     */
-    protected function loadStorageDriver(array $config, ContainerBuilder $container)
-    {
-        $storageDriver = $config['storage_driver'];
-
-        if (!in_array($storageDriver, $this->getSupportedStorageDrivers())) {
-            throw new RuntimeException("The storage driver $storageDriver is not a supported drivers");
-        }
-
+        // TODO: the catalog bundle must be declared first
+        $storageDriver = $container->getParameter('pim_catalog.storage_driver');
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load(sprintf('storage_driver/%s.yml', $storageDriver));
-
-        $container->setParameter($this->getAlias().'.storage_driver', $storageDriver);
-        // Parameter defining if the mapping driver must be enabled or not
-        $container->setParameter($this->getAlias().'.storage_driver.'.$storageDriver, true);
     }
-
 }
