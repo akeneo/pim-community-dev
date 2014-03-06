62af05486be0ab7fb16cd54fcf7e3f9bfef99818
Nicolas Dupont <nicolas@akeneo.com>
2014-03-06 12:18:05 +0100

    PIM-2127 : detect storage driver from container parameter and load hydrator config file
    Conflicts:
    	src/Pim/Bundle/DataGridBundle/DependencyInjection/PimDataGridExtension.php

--- src/Pim/Bundle/DataGridBundle/DependencyInjection/PimDataGridExtension.php
+++ src/Pim/Bundle/DataGridBundle/DependencyInjection/PimDataGridExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -28,8 +29,13 @@ class PimDataGridExtension extends Extension
         $loader->load('pagers.yml');
         $loader->load('extensions.yml');
         $loader->load('actions.yml');
+        $loader->load('hydrators.yml');
         $loader->load('mass_actions.yml');
         $loader->load('event_listeners.yml');
         $loader->load('attribute_types.yml');
+
+        $storageDriver = $container->getParameter('pim_catalog.storage_driver');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load(sprintf('storage_driver/%s.yml', $storageDriver));
     }
 }
