fd68915d61396e062fbd4b020ae2f130002b0c40
Filips Alpe <filips@akeneo.com>
2014-03-06 14:52:42 +0100

    Create DatagridView entity

---
+++ spec/Pim/Bundle/EnrichBundle/Entity/DatagridViewSpec.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace spec\Pim\Bundle\EnrichBundle\Entity;
+
+use PhpSpec\ObjectBehavior;
+use Oro\Bundle\UserBundle\Entity\User;
+
+class DatagridViewSpec extends ObjectBehavior
+{
+    function it_stores_the_owner_of_the_view(User $owner)
+    {
+        $this->setOwner($owner);
+        $this->getOwner()->shouldReturn($owner);
+    }
+
+    function it_stores_the_datagrid_alias()
+    {
+        $this->setDatagridAlias('foo-grid');
+        $this->getDatagridAlias()->shouldReturn('foo-grid');
+    }
+
+    function it_stores_the_displayed_columns()
+    {
+        $this->setColumns(['foo', 'bar', 'baz']);
+        $this->getColumns()->shouldReturn(['foo', 'bar', 'baz']);
+    }
+
+    function it_stores_the_displayed_filters()
+    {
+        $this->setFilters('sku=1');
+        $this->getFilters()->shouldReturn('sku=1');
+    }
+}
---
+++ src/Pim/Bundle/EnrichBundle/Entity/DatagridView.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Entity;
+
+use Oro\Bundle\UserBundle\Entity\User;
+
+/**
+ * Datagrid view entity
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class DatagridView
+{
+    /** @var integer */
+    protected $id;
+
+    /** @var User */
+    protected $owner;
+
+    /** @var string */
+    protected $datagridAlias;
+
+    /** @var array */
+    protected $columns = [];
+
+    /** @var string */
+    protected $filters;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param User $owner
+     *
+     * @return DatagridView
+     */
+    public function setOwner(User $owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return User
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set datagrid alias
+     *
+     * @param string $datagridAlias
+     *
+     * @return DatagridView
+     */
+    public function setDatagridAlias($datagridAlias)
+    {
+        $this->datagridAlias = $datagridAlias;
+
+        return $this;
+    }
+
+    /**
+     * Get datagrid alias
+     *
+     * @return string
+     */
+    public function getDatagridAlias()
+    {
+        return $this->datagridAlias;
+    }
+
+    /**
+     * Set columns
+     *
+     * @param array $columns
+     *
+     * @return DatagridView
+     */
+    public function setColumns(array $columns)
+    {
+        $this->columns = $columns;
+
+        return $this;
+    }
+
+    /**
+     * Get columns
+     *
+     * @return array
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+
+    /**
+     * Set filters
+     *
+     * @param string $filters
+     *
+     * @return DatagridView
+     */
+    public function setFilters($filters)
+    {
+        $this->filters = $filters;
+
+        return $this;
+    }
+
+    /**
+     * Get filters
+     *
+     * @return string
+     */
+    public function getFilters()
+    {
+        return $this->filters;
+    }
+}
