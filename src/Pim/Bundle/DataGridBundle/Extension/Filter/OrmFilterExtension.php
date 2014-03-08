30efc51984b1f7e5cfbab2e00358da32e71a85f7
Nicolas Dupont <nicolas@akeneo.com>
2014-03-08 11:21:42 +0100

    PIM-2156 : ORM and product filter extensions now inherit from abstract extension

--- src/Pim/Bundle/DataGridBundle/Extension/Filter/OrmFilterExtension.php
+++ src/Pim/Bundle/DataGridBundle/Extension/Filter/OrmFilterExtension.php
@@ -15,19 +15,15 @@ use Pim\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource as PimOrmDatasource;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class OrmFilterExtension extends OroOrmFilterExtension
+class OrmFilterExtension extends AbstractFilterExtension
 {
     /**
-     * {@inheritdoc}
+     * @param DatagridConfiguration $config
+     *
+     * @return boolean
      */
-    public function isApplicable(DatagridConfiguration $config)
+    protected function matchDatasource(DatagridConfiguration $config)
     {
-        $filters = $config->offsetGetByPath(Configuration::COLUMNS_PATH, []);
-
-        if (!$filters) {
-            return false;
-        }
-
         $datasourceType = $config->offsetGetByPath(Builder::DATASOURCE_TYPE_PATH);
 
         return ($datasourceType == PimOrmDatasource::TYPE);
