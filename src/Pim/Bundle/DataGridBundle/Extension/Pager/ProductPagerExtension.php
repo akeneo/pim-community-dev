f2241c95a6bef7843a5f767ab267fa56dfba810a
Nicolas Dupont <nicolas@akeneo.com>
2014-03-08 11:01:23 +0100

    PIM-2156 : ORM and product pager inherits from abstract extension

--- src/Pim/Bundle/DataGridBundle/Extension/Pager/ProductPagerExtension.php
+++ src/Pim/Bundle/DataGridBundle/Extension/Pager/ProductPagerExtension.php
@@ -20,81 +20,8 @@ use Pim\Bundle\DataGridBundle\Datasource\ProductDatasource;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductPagerExtension extends AbstractExtension
+class ProductPagerExtension extends AbstractPagerExtension
 {
-    /**
-     * Query params
-     */
-    const PAGER_ROOT_PARAM = '_pager';
-    const PAGE_PARAM       = '_page';
-    const PER_PAGE_PARAM   = '_per_page';
-    const TOTAL_PARAM      = 'totalRecords';
-
-    /** @var PagerInterface */
-    protected $pager;
-
-    /**
-     * @param PagerInterface    $pager
-     * @param RequestParameters $requestParams
-     */
-    public function __construct(PagerInterface $pager, RequestParameters $requestParams)
-    {
-        $this->pager = $pager;
-        parent::__construct($requestParams);
-    }
-
-    /**
-     * Prototype object
-     */
-    public function __clone()
-    {
-        $this->pager = clone $this->pager;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function isApplicable(DatagridConfiguration $config)
-    {
-        return $this->matchDatasource($config);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function visitDatasource(DatagridConfiguration $config, DatasourceInterface $datasource)
-    {
-        $defaultPerPage = $config->offsetGetByPath(ToolbarExtension::PAGER_DEFAULT_PER_PAGE_OPTION_PATH, 10);
-
-        $this->pager->setQueryBuilder($datasource->getQueryBuilder());
-        $this->pager->setPage($this->getOr(self::PAGE_PARAM, 1));
-        $this->pager->setMaxPerPage($this->getOr(self::PER_PAGE_PARAM, $defaultPerPage));
-        $this->pager->init();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function visitResult(DatagridConfiguration $config, ResultsObject $result)
-    {
-        $result->offsetAddToArray('options', [self::TOTAL_PARAM => $this->pager->getNbResults()]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function visitMetadata(DatagridConfiguration $config, MetadataObject $data)
-    {
-        $defaultPerPage = $config->offsetGetByPath(ToolbarExtension::PAGER_DEFAULT_PER_PAGE_OPTION_PATH, 10);
-
-        $state = [
-            'currentPage' => $this->getOr(self::PAGE_PARAM, 1),
-            'pageSize'    => $this->getOr(self::PER_PAGE_PARAM, $defaultPerPage)
-        ];
-
-        $data->offsetAddToArray('state', $state);
-    }
-
     /**
      * @param DatagridConfiguration $config
      *
@@ -105,30 +32,5 @@ class ProductPagerExtension extends AbstractExtension
         $datasourceType = $config->offsetGetByPath(Builder::DATASOURCE_TYPE_PATH);
 
         return ($datasourceType == ProductDatasource::TYPE);
-    }
-
-    /**
-     * Get param or return specified default value
-     *
-     * @param string $paramName
-     * @param null   $default
-     *
-     * @return mixed
-     */
-    protected function getOr($paramName, $default = null)
-    {
-        $pagerParameters = $this->requestParams->get(self::PAGER_ROOT_PARAM);
-
-        return isset($pagerParameters[$paramName]) ? $pagerParameters[$paramName] : $default;
-    }
-
-    /**
-     * Should pass at the very end (after filters and sorters)
-     *
-     * {@inheritdoc}
-     */
-    public function getPriority()
-    {
-        return -300;
     }
 }
