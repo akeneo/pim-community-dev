ea8ec22506684cbe0cd75c52288c759e566ed31f
Romain Monceau <romain@akeneo.com>
2014-03-10 17:53:54 +0100

    PIM-2190: Add php documentation for class

--- src/Pim/Bundle/DataGridBundle/Controller/ExportController.php
+++ src/Pim/Bundle/DataGridBundle/Controller/ExportController.php
@@ -16,8 +16,18 @@ use Pim\Bundle\CatalogBundle\Manager\ProductManager;
 use Pim\Bundle\DataGridBundle\Extension\MassAction\MassActionDispatcher;
 use Pim\Bundle\UserBundle\Context\UserContext;
 
+/**
+ * Datagrid controller for export action
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class ExportController
 {
+    /** @var DatagridManager $datagridManager */
+    protected $datagridManager;
+
     /** @var MassActionParametersParser $parametersParser */
     protected $parametersParser;
 
