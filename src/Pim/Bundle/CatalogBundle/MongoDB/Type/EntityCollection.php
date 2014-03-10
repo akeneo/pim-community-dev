78ff30f9be31c0c121f06acc8ded0bac3021f4b3
Gildas Quéméner <gildas.quemener@gmail.com>
2014-03-10 12:21:25 +0100

    PIM-1568 Convert collection of entities to an array of identifiers

--- src/Pim/Bundle/CatalogBundle/MongoDB/Type/EntityCollection.php
+++ src/Pim/Bundle/CatalogBundle/MongoDB/Type/EntityCollection.php
@@ -3,13 +3,13 @@
 namespace Pim\Bundle\CatalogBundle\MongoDB\Type;
 
 use Doctrine\ODM\MongoDB\Types\Type;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\PersistentCollection;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Mapping\ClassMetadataFactory;
+use Pim\Bundle\CatalogBundle\Doctrine\ReferencedCollection;
 
 /**
- * 
+ * Stores a collection of entity identifiers
+ *
+ * @see Pim\Bundle\CatalogBundle\EventListener\MongoDBODM\EntityCollectionSubscriber
+ *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -21,11 +21,16 @@ class EntityCollection extends Type
      */
     public function convertToDatabaseValue($value)
     {
-        return array_map(
-            function($val) {
-                return $val->getId();
-            },
-            $value
-        );
+        if (!$value instanceof ReferencedCollection) {
+            throw new \InvalidArgumentException('Expecting instance of Pim\Bundle\CatalogBundle\Doctrine\ReferencedCollection');
+        }
+
+        return $value
+            ->map(
+                function($val) {
+                    return $val->getId();
+                }
+            )
+            ->toArray();
     }
 }
