a06374402f77c1bdadc635921df2be836a769866
Nicolas Dupont <nicolas@akeneo.com>
2014-03-12 10:05:05 +0100

    PIM-2127 : rename flexible filter utility

--- src/Pim/Bundle/FilterBundle/Filter/ProductFilterUtility.php
+++ src/Pim/Bundle/FilterBundle/Filter/ProductFilterUtility.php
@@ -1,22 +1,20 @@
 <?php
 
-namespace Pim\Bundle\FilterBundle\Filter\Flexible;
+namespace Pim\Bundle\FilterBundle\Filter;
 
-use Oro\Bundle\FilterBundle\Datasource\Orm\OrmFilterDatasourceAdapter;
 use Oro\Bundle\FilterBundle\Filter\FilterUtility as BaseFilterUtility;
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
 use Pim\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
 use Pim\Bundle\FlexibleEntityBundle\Manager\FlexibleManagerRegistry;
-use Pim\Bundle\FlexibleEntityBundle\Entity\Repository\FlexibleEntityRepository;
 
 /**
- * Flexible filter utility
+ * Product filter utility
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class FilterUtility extends BaseFilterUtility
+class ProductFilterUtility extends BaseFilterUtility
 {
     const FEN_KEY         = 'flexible_entity_name';
     const PARENT_TYPE_KEY = 'parent_type';
