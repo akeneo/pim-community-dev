d22bd6a882f19916eee63a9304a72be59e9afdf2
Nicolas Dupont <nicolas@akeneo.com>
2014-03-13 20:20:23 +0100

    PIM-2127 : add product price normalizer

---
+++ src/Pim/Bundle/CatalogBundle/MongoDB/Normalizer/ProductPriceNormalizer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\MongoDB\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Pim\Bundle\CatalogBundle\Model\ProductPrice;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * Normalize a product price collection to store it as bson
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductPriceNormalizer implements NormalizerInterface, SerializerAwareInterface
+{
+    /** @var SerializerInterface */
+    protected $serializer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data = [
+            'data'     => $object->getData(),
+            'currency' => $object->getCurrency()
+        ];
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ProductPrice && 'bson' === $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+}
