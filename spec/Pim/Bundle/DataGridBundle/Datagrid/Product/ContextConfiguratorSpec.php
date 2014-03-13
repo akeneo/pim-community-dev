fec248d3bc8f5eb206b39d107a67f2ae62b6111a
Filips Alpe <filips@akeneo.com>
2014-03-13 19:48:57 +0100

    Fix spec

--- spec/Pim/Bundle/DataGridBundle/Datagrid/Product/ContextConfiguratorSpec.php
+++ spec/Pim/Bundle/DataGridBundle/Datagrid/Product/ContextConfiguratorSpec.php
@@ -8,13 +8,14 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Oro\Bundle\DataGridBundle\Datagrid\RequestParameters;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Pim\Bundle\DataGridBundle\Manager\DatagridViewManager;
 use Pim\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
 
 class ContextConfiguratorSpec extends ObjectBehavior
 {
-    function let(DatagridConfiguration $configuration, FlexibleManager $manager, RequestParameters $requestParams, Request $request, SecurityContextInterface $securityContext)
+    function let(DatagridConfiguration $configuration, FlexibleManager $manager, RequestParameters $requestParams, Request $request, SecurityContextInterface $securityContext, DatagridViewManager $viewManager)
     {
-        $this->beConstructedWith($configuration, $manager, $requestParams, $request, $securityContext);
+        $this->beConstructedWith($configuration, $manager, $requestParams, $request, $securityContext, $viewManager);
     }
 
     function it_is_a_configurator()
--- spec/Pim/Bundle/DataGridBundle/Entity/DatagridViewSpec.php
+++ spec/Pim/Bundle/DataGridBundle/Entity/DatagridViewSpec.php
@@ -1,37 +1,37 @@
 <?php
 
-namespace spec\Pim\Bundle\EnrichBundle\Entity;
+namespace spec\Pim\Bundle\DataGridBundle\Entity;
 
 use PhpSpec\ObjectBehavior;
 use Oro\Bundle\UserBundle\Entity\User;
 
 class DatagridViewSpec extends ObjectBehavior
 {
-    function it_stores_the_label_of_the_view(User $owner)
+    public function it_stores_the_label_of_the_view(User $owner)
     {
         $this->setLabel('random view');
         $this->getLabel()->shouldReturn('random view');
     }
 
-    function it_stores_the_owner_of_the_view(User $owner)
+    public function it_stores_the_owner_of_the_view(User $owner)
     {
         $this->setOwner($owner);
         $this->getOwner()->shouldReturn($owner);
     }
 
-    function it_stores_the_datagrid_alias()
+    public function it_stores_the_datagrid_alias()
     {
         $this->setDatagridAlias('foo-grid');
         $this->getDatagridAlias()->shouldReturn('foo-grid');
     }
 
-    function it_stores_the_displayed_columns()
+    public function it_stores_the_displayed_columns()
     {
         $this->setColumns(['foo', 'bar', 'baz']);
         $this->getColumns()->shouldReturn(['foo', 'bar', 'baz']);
     }
 
-    function it_stores_the_displayed_filters()
+    public function it_stores_the_displayed_filters()
     {
         $this->setFilters('sku=1');
         $this->getFilters()->shouldReturn('sku=1');
