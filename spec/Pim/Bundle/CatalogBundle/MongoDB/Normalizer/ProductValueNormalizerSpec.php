c8e7e1146a28b2def808ad7e1a2aea9593f10b8d
Nicolas Dupont <nicolas@akeneo.com>
2014-03-13 21:09:15 +0100

    PIM-2127 : add productvaluenormalizer specs

---
+++ spec/Pim/Bundle/CatalogBundle/MongoDB/Normalizer/ProductValueNormalizerSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace spec\Pim\Bundle\CatalogBundle\MongoDB\Normalizer;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\Common\Collections\Collection;
+use Pim\Bundle\CatalogBundle\Model\ProductValue;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
+
+class ProductValueNormalizerSpec extends ObjectBehavior
+{
+    function it_is_a_normalizer()
+    {
+        $this->shouldImplement('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+    }
+
+    function it_supports_normalization_in_bson_of_value(ProductValue $value)
+    {
+        $this->supportsNormalization($value, 'bson')->shouldBe(true);
+        $this->supportsNormalization($value, 'json')->shouldBe(false);
+        $this->supportsNormalization($value, 'xml')->shouldBe(false);
+    }
+
+    function it_normalizes_value_with_simple_data(SerializerInterface $serializer, ProductValue $value, Attribute $attribute)
+    {
+        $serializer->implement('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+        $this->setSerializer($serializer);
+
+        $attribute->getCode()->willReturn('code');
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+
+        $data = 'product title';
+        $value->getData()->willReturn($data);
+        $value->getAttribute()->willReturn($attribute);
+
+        $serializer->normalize($data, 'bson', [])->willReturn($data);
+
+        $this->normalize($value, 'bson', [])->shouldReturn(['code' => 'product title']);
+    }
+
+    function it_normalizes_value_with_collection_data(ProductValue $value, Attribute $attribute, Collection $collection, \Iterator $iterator)
+    {
+
+        $attribute->getCode()->willReturn('code');
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+
+        $collection->getIterator()->willReturn($iterator);
+        $value->getData()->willReturn($collection);
+        $value->getAttribute()->willReturn($attribute);
+
+        $this->normalize($value, 'bson', [])->shouldReturn(['code' => []]);
+    }
+}
