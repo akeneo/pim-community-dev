f075404a70a4a9c5aed4289ba1aff11471a26cf9
Romain Monceau <romain@akeneo.com>
2014-03-14 17:23:36 +0100

    PIM-2185: Rename Flexible as Product

--- src/Pim/Bundle/DataGridBundle/Controller/ProductExportController.php
+++ src/Pim/Bundle/DataGridBundle/Controller/ProductExportController.php
@@ -12,13 +12,13 @@ use Pim\Bundle\DataGridBundle\Extension\MassAction\MassActionDispatcher;
 use Pim\Bundle\UserBundle\Context\UserContext;
 
 /**
- * Override ExportController for flexible exports
+ * Override ExportController for product exports
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class FlexibleExportController extends ExportController
+class ProductExportController extends ExportController
 {
     /** @var ProductManager $productManager */
     protected $productManager;
