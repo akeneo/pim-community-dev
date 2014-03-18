15aeb0933d2f997a776883847e348cffdf1728c7
Benoit Jacquemont <benoit@akeneo.com>
2014-03-18 10:49:12 +0100

    WIP on AssociationFormType

---
+++ src/Pim/Bundle/CatalogBundle/Manager/AssociationManager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Manager;
+
+use Pim\Bundle\CatalogBundle\Model\AssociationRepositoryInterface;
+use Pim\Bundle\CatalogBundle\Entity\AssociationType;
+
+/**
+ * Association Manager
+ *
+ * @author    Benoit Jacquemont <benoit@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AssociationManager
+{
+    /** @var AssociationRepositoryInterface */
+    protected $repository;
+
+    /**
+     * @param AssociationRepositoryInterface $repository
+     */
+    public function __construct(AssociationRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Get association count by association type
+     *
+     * @return int
+     */
+    public function getCountByType(AssociationType $type)
+    {
+        return $this->repository->countByType($type);
+
+    }
+}
