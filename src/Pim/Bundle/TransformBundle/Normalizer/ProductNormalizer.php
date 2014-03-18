234da65f6b2f65ae2ba9f5e568d1d6501cabb3a3
Filips Alpe <filips@akeneo.com>
2014-03-18 18:24:08 +0100

    Normalize product enabled property, family, groups, associations and categories for REST

--- src/Pim/Bundle/TransformBundle/Normalizer/ProductNormalizer.php
+++ src/Pim/Bundle/TransformBundle/Normalizer/ProductNormalizer.php
@@ -17,6 +17,21 @@ use Pim\Bundle\CatalogBundle\Entity\Channel;
  */
 class ProductNormalizer implements NormalizerInterface, SerializerAwareInterface
 {
+    /** @staticvar string */
+    const FIELD_FAMILY = 'family';
+
+    /** @staticvar string */
+    const FIELD_GROUPS = 'groups';
+
+    /** @staticvar string */
+    const FIELD_CATEGORY = 'categories';
+
+    /** @staticvar string */
+    const FIELD_ENABLED = 'enabled';
+
+    /** @staticvar string */
+    const FIELD_ASSOCIATIONS = 'associations';
+
     /** @var SerializerInterface */
     protected $serializer;
 
@@ -42,15 +57,16 @@ class ProductNormalizer implements NormalizerInterface, SerializerAwareInterface
             throw new \LogicException('Serializer must be a normalizer');
         }
 
-        $locales  = isset($context['locales']) ? $context['locales'] : [];
-        $channels = isset($context['channels']) ? $context['channels'] : [];
-        $values = $this->filterValues($product->getValues(), $channels, $locales);
-
         $data = [];
 
-        foreach ($values as $value) {
-            $data[$value->getAttribute()->getCode()][] = $this->serializer->normalize($value, $format, $context);
-        }
+        $data[self::FIELD_FAMILY]   = $product->getFamily() ? $product->getFamily()->getCode() : null;
+        $data[self::FIELD_GROUPS]   = $product->getGroupCodes() ? explode(',', $product->getGroupCodes()) : null;
+        $data[self::FIELD_CATEGORY] = $product->getCategoryCodes() ? explode(',', $product->getCategoryCodes()) : null;
+        $data[self::FIELD_ENABLED]  = (int) $product->isEnabled();
+
+        $data[self::FIELD_ASSOCIATIONS] = $this->normalizeAssociations($product->getAssociations());
+
+        $data = $data + $this->normalizeValues($product->getValues(), $format, $context);
 
         if (isset($context['resource'])) {
             $data['resource'] = $context['resource'];
@@ -65,6 +81,30 @@ class ProductNormalizer implements NormalizerInterface, SerializerAwareInterface
     public function supportsNormalization($data, $format = null)
     {
         return $data instanceof ProductInterface && in_array($format, $this->supportedFormats);
+    }
+
+    /**
+     * Normalize the values of the product
+     *
+     * @param ArrayCollection $values
+     * @param string          $format
+     * @param array           $context
+     *
+     * @return ArrayCollection
+     */
+    protected function normalizeValues($values, $format, array $context = [])
+    {
+        $locales  = isset($context['locales'])  ? $context['locales']  : [];
+        $channels = isset($context['channels']) ? $context['channels'] : [];
+        $values   = $this->filterValues($values, $channels, $locales);
+
+        $data = [];
+
+        foreach ($values as $value) {
+            $data[$value->getAttribute()->getCode()][] = $this->serializer->normalize($value, $format, $context);
+        }
+
+        return $data;
     }
 
     /**
@@ -91,5 +131,31 @@ class ProductNormalizer implements NormalizerInterface, SerializerAwareInterface
         );
 
         return $values;
+    }
+
+    /**
+     * Normalize the associations of the product
+     *
+     * @param Association[] $associations
+     *
+     * @return array
+     */
+    protected function normalizeAssociations($associations = [])
+    {
+        $data = [];
+
+        foreach ($associations as $association) {
+            $code = $association->getAssociationType()->getCode();
+
+            foreach ($association->getGroups() as $group) {
+                $data[$code]['groups'][] = $group->getCode();
+            }
+
+            foreach ($association->getProducts() as $product) {
+                $data[$code]['products'][] = $product->getReference();
+            }
+        }
+
+        return $data;
     }
 }
