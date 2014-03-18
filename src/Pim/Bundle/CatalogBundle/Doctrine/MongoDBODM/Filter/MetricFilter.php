0d961ff1cdc4ada3f79c4d64fae3956edeccdada
Nicolas Dupont <nicolas@akeneo.com>
2014-03-18 12:25:28 +0100

    PIM-2187 : add a metric filter + spec (MongoDB support)

---
+++ src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/Filter/MetricFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\Filter;
+
+use Pim\Bundle\FlexibleEntityBundle\Model\AbstractAttribute;
+
+/**
+ * Metric filter
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MetricFilter extends BaseFilter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function add(AbstractAttribute $attribute, $operator, $value)
+    {
+        $data = (float) $value;
+
+        $field = $this->getNormalizedValueField($attribute);
+        $fieldData = sprintf('%s.baseData', $field);
+
+        switch ($operator) {
+            case '<':
+                $this->qb->field($fieldData)->lt($data);
+                break;
+            case '<=':
+                $this->qb->field($fieldData)->lte($data);
+                break;
+            case '>':
+                $this->qb->field($fieldData)->gt($data);
+                break;
+            case '>=':
+                $this->qb->field($fieldData)->gte($data);
+                break;
+            default:
+                $this->qb->field($fieldData)->equals($data);
+        }
+
+        return $this;
+    }
+}
