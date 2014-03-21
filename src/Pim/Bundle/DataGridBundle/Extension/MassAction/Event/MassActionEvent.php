60e38e40a727b615dd641102711fd007f9f5f1b1
Romain Monceau <romain@akeneo.com>
2014-03-21 23:38:18 +0100

    PIM-2288: Add php doc on MassActionEvent

--- src/Pim/Bundle/DataGridBundle/Extension/MassAction/Event/MassActionEvent.php
+++ src/Pim/Bundle/DataGridBundle/Extension/MassAction/Event/MassActionEvent.php
@@ -4,9 +4,13 @@ namespace Pim\Bundle\DataGridBundle\Extension\MassAction\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 
+/**
+ * Mass action event allows to do add easily some extra code
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class MassActionEvent extends Event
 {
-    public function __construct()
-    {
-    }
 }
