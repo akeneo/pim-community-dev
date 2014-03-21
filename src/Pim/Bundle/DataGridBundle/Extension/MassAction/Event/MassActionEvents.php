57b40ea98fbebf40f33a5e0b552b0619093c6f5e
Romain Monceau <romain@akeneo.com>
2014-03-21 23:30:05 +0100

    PIM-2288: Add phpdoc and remove useless event

--- src/Pim/Bundle/DataGridBundle/Extension/MassAction/Event/MassActionEvents.php
+++ src/Pim/Bundle/DataGridBundle/Extension/MassAction/Event/MassActionEvents.php
@@ -2,8 +2,22 @@
 
 namespace Pim\Bundle\DataGridBundle\Extension\MassAction\Event;
 
+/**
+ * Mass actions events
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 final class MassActionEvents
 {
-    const MASS_ACTION_POST_DISPATCH = 'pim_datagrid.mass_action.post_dispatch';
-    const MASS_ACTION_PRE_DISPATCH  = 'pim_datagrid.mass_action.pre_dispatch';
+    /**
+     * This event is thrown each time a mass action handler is called
+     *
+     * The event listener receives an
+     * Pim\Bundle\DataGridBundle\Extension\MassAction\Event\MassActionEvent instance
+     *
+     * @staticvar string
+     */
+    const MASS_ACTION_POST_HANDLER = 'pim_datagrid.mass_action.post_handler';
 }
