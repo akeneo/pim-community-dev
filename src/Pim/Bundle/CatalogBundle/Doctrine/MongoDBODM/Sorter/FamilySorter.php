1cedf3a179e8857c5d97408f0ac798d4cfffe2ce
Nicolas Dupont <nicolas@akeneo.com>
2014-03-22 09:43:39 +0100

    PIM-2274 : add a family sorter

---
+++ src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/Sorter/FamilySorter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\Sorter;
+
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+use Pim\Bundle\CatalogBundle\Doctrine\FieldSorterInterface;
+use Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\ProductQueryUtility;
+use Doctrine\ODM\MongoDB\Query\Builder as QueryBuilder;
+
+/**
+ * Base sorter
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FamilySorter implements FieldSorterInterface
+{
+    /** @var QueryBuilder */
+    protected $qb;
+
+    /** @var string */
+    protected $locale;
+
+    /** @var string */
+    protected $scope;
+
+    /**
+     * Instanciate a sorter
+     *
+     * @param QueryBuilder $qb
+     * @param string       $locale
+     * @param scope        $scope
+     */
+    public function __construct(QueryBuilder $qb, $locale, $scope)
+    {
+        $this->qb     = $qb;
+        $this->locale = $locale;
+        $this->scope  = $scope;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFieldSorter($field, $direction)
+    {
+        $field = sprintf(
+            "%s.%s.label.%s",
+            ProductQueryUtility::NORMALIZED_FIELD,
+            $field,
+            $this->locale
+        );
+
+        $this->qb->sort($field, $direction);
+
+        return $this;
+    }
+}
