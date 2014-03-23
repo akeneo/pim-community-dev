61867078cd278678c9877146dc3379d8002cbb77
Nicolas Dupont <nicolas@akeneo.com>
2014-03-23 20:52:50 +0100

    PIM-1621 : fix attribute option

--- src/Pim/Bundle/CatalogBundle/Entity/AttributeOption.php
+++ src/Pim/Bundle/CatalogBundle/Entity/AttributeOption.php
@@ -4,8 +4,8 @@ namespace Pim\Bundle\CatalogBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use JMS\Serializer\Annotation\ExclusionPolicy;
-use Pim\Bundle\FlexibleEntityBundle\Entity\Mapping\AbstractEntityAttributeOption;
 use Pim\Bundle\CatalogBundle\Model\ReferableInterface;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
 
 /**
  * Attribute options
@@ -16,8 +16,13 @@ use Pim\Bundle\CatalogBundle\Model\ReferableInterface;
  *
  * @ExclusionPolicy("all")
  */
-class AttributeOption extends AbstractEntityAttributeOption implements ReferableInterface
+class AttributeOption implements ReferableInterface
 {
+    /**
+     * @var integer $id
+     */
+    protected $id;
+
     /**
      * @var string $code
      */
@@ -41,6 +46,164 @@ class AttributeOption extends AbstractEntityAttributeOption implements Referable
      * @var boolean $default
      */
     protected $default = false;
+
+    /**
+     * @var boolean
+     */
+    protected $translatable;
+
+    /**
+     * Not persisted, allowe to define the value locale
+     * @var string $locale
+     */
+    protected $locale;
+
+    /**
+     * @var integer
+     */
+    protected $sortOrder;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->optionValues = new ArrayCollection();
+        $this->translatable = true;
+        $this->sortOrder    = 1;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return AttributeOption
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get attribute
+     *
+     * @return AbstractAttribute
+     */
+    public function getAttribute()
+    {
+        return $this->attribute;
+    }
+
+    /**
+     * Set attribute
+     *
+     * @param AbstractAttribute $attribute
+     *
+     * @return AttributeOption
+     */
+    public function setAttribute(AbstractAttribute $attribute = null)
+    {
+        $this->attribute = $attribute;
+
+        return $this;
+    }
+
+    /**
+     * Get values
+     *
+     * @return \ArrayAccess
+     */
+    public function getOptionValues()
+    {
+        return $this->optionValues;
+    }
+
+    /**
+     * Get used locale
+     *
+     * @return string $locale
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Set used locale
+     *
+     * @param string $locale
+     *
+     * @return LocalizableInterface
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * Set translatable
+     *
+     * @param boolean $translatable
+     *
+     * @return AttributeOption
+     */
+    public function setTranslatable($translatable)
+    {
+        $this->translatable = $translatable;
+
+        return $this;
+    }
+
+    /**
+     * Is translatable
+     *
+     * @return boolean $translatable
+     */
+    public function isTranslatable()
+    {
+        return $this->translatable;
+    }
+
+    /**
+     * Set sort order
+     *
+     * @param string $sortOrder
+     *
+     * @return AttributeOption
+     */
+    public function setSortOrder($sortOrder)
+    {
+        if ($sortOrder !== null) {
+            $this->sortOrder = $sortOrder;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get sort order
+     *
+     * @return integer
+     */
+    public function getSortOrder()
+    {
+        return $this->sortOrder;
+    }
 
     /**
      * Set code
@@ -126,4 +289,58 @@ class AttributeOption extends AbstractEntityAttributeOption implements Referable
 
         return $value;
     }
+
+    /**
+     * Add option value
+     *
+     * @param AttributeOptionValue $value
+     *
+     * @return AbstractAttribute
+     */
+    public function addOptionValue(AttributeOptionValue $value)
+    {
+        $this->optionValues[] = $value;
+        $value->setOption($this);
+
+        return $this;
+    }
+
+    /**
+     * Remove value
+     *
+     * @param AttributeOptionValue $value
+     *
+     * @return AttributeOption
+     */
+    public function removeOptionValue(AttributeOptionValue $value)
+    {
+        $this->optionValues->removeElement($value);
+
+        return $this;
+    }
+
+    /**
+     * Get localized value
+     *
+     * @return AbstractEntityAttributeOptionValue
+     */
+    public function getOptionValue()
+    {
+        $translatable = $this->translatable;
+        $locale = $this->getLocale();
+        $values = $this->getOptionValues()->filter(
+            function ($value) use ($translatable, $locale) {
+                // return relevant translated value
+                if ($translatable and $value->getLocale() == $locale) {
+                    return true;
+                } elseif (!$translatable) {
+                    return true;
+                }
+            }
+        );
+        $value = $values->first();
+
+        return $value;
+    }
+
 }
