9de2a93b8de22944ea563a97682681920433a277
Nicolas Dupont <nicolas@akeneo.com>
2014-03-25 19:35:03 +0100

    PIM-2317 : add "fake" but agnostic in group filter

---
+++ src/Pim/Bundle/FilterBundle/Filter/Product/InGroupFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pim\Bundle\FilterBundle\Filter\Product;
+
+use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+use Oro\Bundle\FilterBundle\Filter\ChoiceFilter;
+
+/**
+ * Product in group filter (used by group products grid)
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class InGroupFilter extends ChoiceFilter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(FilterDatasourceAdapterInterface $ds, $data)
+    {
+        $data = $this->parseData($data);
+
+        if (!$data) {
+            return false;
+        }
+
+        $qb = $ds->getQueryBuilder();
+        $value = current($data['value']);
+
+        $repository = $this->util->getProductRepository();
+        //$repository->applyFilterByField($qb, 'enabled', $value);
+
+        return true;
+    }
+}
