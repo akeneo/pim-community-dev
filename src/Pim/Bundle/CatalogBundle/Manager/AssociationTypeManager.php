f64a63e360287ce7274979f03a4732700bfb5df1
Romain Monceau <romain@akeneo.com>
2014-03-25 22:07:21 +0100

    PIM-2312: Add php doc on association type manager

--- src/Pim/Bundle/CatalogBundle/Manager/AssociationTypeManager.php
+++ src/Pim/Bundle/CatalogBundle/Manager/AssociationTypeManager.php
@@ -4,15 +4,35 @@ namespace Pim\Bundle\CatalogBundle\Manager;
 
 use Pim\Bundle\CatalogBundle\Entity\Repository\AssociationTypeRepository;
 
+/**
+ * Association type manager
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class AssociationTypeManager
 {
+    /**
+     * @var AssociationTypeRepository $repository
+     */
     protected $repository;
 
+    /**
+     * Constructor
+     *
+     * @param AssociationTypeRepository $repository
+     */
     public function __construct(AssociationTypeRepository $repository)
     {
         $this->repository = $repository;
     }
 
+    /**
+     * Get association types
+     *
+     * @return array
+     */
     public function getAssociationTypes()
     {
         return $this->repository->findAll();
