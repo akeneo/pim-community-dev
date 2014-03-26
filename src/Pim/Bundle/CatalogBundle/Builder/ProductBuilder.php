68949627d11e875fb2b9bfb93a906b4752790c3a
Gildas Quéméner <gildas.quemener@gmail.com>
2014-03-26 15:26:21 +0100

    PIM-2219 Do not rely on a query builder to remove an attribute from a product

--- src/Pim/Bundle/CatalogBundle/Builder/ProductBuilder.php
+++ src/Pim/Bundle/CatalogBundle/Builder/ProductBuilder.php
@@ -133,12 +133,10 @@ class ProductBuilder
      */
     public function removeAttributeFromProduct(ProductInterface $product, AbstractAttribute $attribute)
     {
-        $values = $this->objectManager
-            ->getRepository($this->getProductValueClass())
-            ->findBy(array('entity' => $product, 'attribute' => $attribute));
-
-        foreach ($values as $value) {
-            $this->objectManager->remove($value);
+        foreach ($product->getValues() as $value) {
+            if ($attribute === $value->getAttribute()) {
+                $product->removeValue($value);
+            }
         }
 
         $this->objectManager->flush();
