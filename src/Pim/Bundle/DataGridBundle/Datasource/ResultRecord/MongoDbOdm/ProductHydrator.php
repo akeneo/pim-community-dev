f70acc2a3f9c8475a296287b18f0a98bf6ba0b1a
Nicolas Dupont <nicolas@akeneo.com>
2014-03-27 20:51:18 +0100

    PIM-2317 : use family transformer in product hydrator

--- src/Pim/Bundle/DataGridBundle/Datasource/ResultRecord/MongoDbOdm/ProductHydrator.php
+++ src/Pim/Bundle/DataGridBundle/Datasource/ResultRecord/MongoDbOdm/ProductHydrator.php
@@ -6,6 +6,7 @@ use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
 use Pim\Bundle\DataGridBundle\Datasource\ResultRecord\HydratorInterface;
 use Pim\Bundle\DataGridBundle\Datasource\ResultRecord\MongoDbOdm\Product\FieldsTransformer;
 use Pim\Bundle\DataGridBundle\Datasource\ResultRecord\MongoDbOdm\Product\ValuesTransformer;
+use Pim\Bundle\DataGridBundle\Datasource\ResultRecord\MongoDbOdm\Product\FamilyTransformer;
 use Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\ProductQueryUtility;
 
 /**
@@ -38,9 +39,12 @@ class ProductHydrator implements HydratorInterface
         $rows = [];
         $fieldsTransformer = new FieldsTransformer();
         $valuesTransformer = new ValuesTransformer();
+        $familyTransformer = new FamilyTransformer();
         foreach ($results as $result) {
             $result = $fieldsTransformer->transform($result, $locale);
             $result = $valuesTransformer->transform($result, $attributes, $locale, $scope);
+            $result = $familyTransformer->transform($result, $locale);
+
             $result = $this->prepareLinkedData($result, $locale, $scope, $groupId);
 
             $rows[] = new ResultRecord($result);
@@ -66,25 +70,6 @@ class ProductHydrator implements HydratorInterface
             $result['ratio']= number_format($normalizedData['completenesses'][$completenessCode], 0);
         } else {
             $result['ratio'] = null;
-        }
-
-        if (isset($normalizedData['family'])) {
-            $family = $normalizedData['family'];
-            $result['familyLabel']= isset($family['label'][$locale]) ?
-                $family['label'][$locale] : '['.$family['code'].']';
-            if (isset($family['attributeAsLabel']) && $family['attributeAsLabel'] != null) {
-                $attributeCode = $family['attributeAsLabel'];
-                if (isset($result[$attributeCode])) {
-                    $attributeAsLabel = $result[$attributeCode];
-                    $backendType = $attributeAsLabel['attribute']['backendType'];
-                    $result['productLabel']= isset($attributeAsLabel[$backendType]) ?
-                        $attributeAsLabel[$backendType] : null;
-                } else {
-                    $result['productLabel']= null;
-                }
-            }
-        } else {
-            $result['familyLabel']= '-';
         }
 
         if ($groupId && isset($result['groups'])) {
