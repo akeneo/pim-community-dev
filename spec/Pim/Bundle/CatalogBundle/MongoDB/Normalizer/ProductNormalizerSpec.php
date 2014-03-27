8a43e491249855082b5dbdc8ef953fd1ecc5192a
Nicolas Dupont <nicolas@akeneo.com>
2014-03-27 16:52:32 +0100

    PIM-2317 : fix product normalizer spec

--- spec/Pim/Bundle/CatalogBundle/MongoDB/Normalizer/ProductNormalizerSpec.php
+++ spec/Pim/Bundle/CatalogBundle/MongoDB/Normalizer/ProductNormalizerSpec.php
@@ -47,6 +47,7 @@ class ProductNormalizerSpec extends ObjectBehavior
 
         $this->normalize($product, 'mongodb_json', [])->shouldReturn([
             ProductNormalizer::FAMILY_FIELD => 'family normalization',
+            ProductNormalizer::GROUPS_FIELD => [],
             ProductNormalizer::COMPLETENESSES_FIELD => array('completenessCode' => 'completeness normalization'),
             ProductNormalizer::ENABLED_FIELD => 1
         ]);
