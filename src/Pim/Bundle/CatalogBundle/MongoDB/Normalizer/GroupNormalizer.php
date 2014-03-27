6a027405bd8b6e32bcaa70f3e9c316434f295687
Nicolas Dupont <nicolas@akeneo.com>
2014-03-27 16:27:24 +0100

    PIM-2317 : add a product group normalizer

---
+++ src/Pim/Bundle/CatalogBundle/MongoDB/Normalizer/GroupNormalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\MongoDB\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Pim\Bundle\CatalogBundle\Entity\Group;
+use Pim\Bundle\TransformBundle\Normalizer\TranslationNormalizer;
+
+/**
+ * Group normalizer
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GroupNormalizer implements NormalizerInterface
+{
+    /** @var TranslationNormalizer $transNormalizer */
+    protected $transNormalizer;
+
+    /**
+     * Constructor
+     *
+     * @param TranslationNormalizer $transNormalizer
+     */
+    public function __construct(TranslationNormalizer $transNormalizer)
+    {
+        $this->transNormalizer = $transNormalizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data = ['code' => $object->getCode()] + $this->transNormalizer->normalize($object, $format, $context);
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof Group && 'mongodb_json' === $format;
+    }
+}
