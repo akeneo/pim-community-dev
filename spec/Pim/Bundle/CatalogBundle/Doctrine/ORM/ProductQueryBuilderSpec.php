425807909ce6836a4c169efbe67bd280028d3b45
Nicolas Dupont <nicolas@akeneo.com>
2014-03-29 17:38:23 +0100

    PIM-2341 : fix PQB spec

--- spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductQueryBuilderSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductQueryBuilderSpec.php
@@ -7,15 +7,4 @@ use Prophecy\Argument;
 
 class ProductQueryBuilderSpec extends ObjectBehavior
 {
-    function it_has_a_locale()
-    {
-        $this->setLocale('fr');
-        $this->getLocale()->shouldReturn('fr');
-    }
-
-    function it_has_a_scope()
-    {
-        $this->setScope('ecommerce');
-        $this->getScope()->shouldReturn('ecommerce');
-    }
 }
