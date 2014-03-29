93c63254f4146b25dbf6aa62f827c66fef7166d1
Nicolas Dupont <nicolas@akeneo.com>
2014-03-29 14:50:50 +0100

    PIM-2341 : introduce the catalog context object

---
+++ src/Pim/Bundle/CatalogBundle/Context/CatalogContext.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Context;
+
+/**
+ * Allows to configure the context to use in queries you can execute and to ensure that the expecte
+ * configuration is provided
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class CatalogContext
+{
+    /** @var array configuration */
+    protected $configuration = array();
+
+    /**
+     * @return string
+     */
+    public function getLocaleCode()
+    {
+        return $this->getConfiguration('localeCode');
+    }
+
+    /**
+     * @return string
+     */
+    public function getScopeCode()
+    {
+        return $this->getConfiguration('scopeCode');
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return catalogcontext
+     */
+    public function setLocaleCode($code)
+    {
+        return $this->setConfiguration('localeCode', $code);
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return catalogcontext
+     */
+    public function setScopeCode($code)
+    {
+        return $this->setConfiguration('scopeCode', $code);
+    }
+
+    /**
+     * @param string $key  the key
+     * @param mixed  $data the data
+     *
+     * @return CatalogContext
+     */
+    public function setConfiguration($key, $data)
+    {
+        $this->configuration[$key]= $data;
+
+        return $this;
+    }
+
+    /**
+     * Get configuration
+     *
+     * @param string $key
+     *
+     * @return mixed
+     *
+     * @throws \LogicException
+     */
+    public function getConfiguration($key)
+    {
+        if (!isset($this->configuration[$key]) || $this->configuration[$key] === null) {
+            throw new \LogicException(sprintf('"%s" expects to be configured with "%s"', get_class($this), $key));
+        }
+
+        return $this->configuration[$key];
+    }
+}
