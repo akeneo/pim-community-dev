edadf79d44d452f7c8240b63823038757622ebc2
Nicolas Dupont <nicolas@akeneo.com>
2014-03-30 16:58:40 +0200

    PIM-2341 : fix ORM filter and sorter specs

--- spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/BaseFilterSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/BaseFilterSpec.php
@@ -7,12 +7,15 @@ use Prophecy\Argument;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query\Expr;
 use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+use Pim\Bundle\CatalogBundle\Context\CatalogContext;
 
 class BaseFilterSpec extends ObjectBehavior
 {
-    function let(QueryBuilder $queryBuilder)
+    function let(QueryBuilder $queryBuilder, CatalogContext $context)
     {
-        $this->beConstructedWith($queryBuilder, 'en_US', 'mobile');
+        $context->getLocaleCode()->willReturn('en_US');
+        $context->getScopeCode()->willReturn('mobile');
+        $this->beConstructedWith($queryBuilder, $context);
     }
 
     function it_is_a_filter()
