2c7f9141b7349489a8812b18e83100aee96245b9
Romain Monceau <romain@akeneo.com>
2014-04-01 17:19:58 +0200

    PIM-2368: Rename persister by repository

--- src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/ProductMassActionRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/ProductMassActionRepository.php
@@ -2,15 +2,15 @@
 
 namespace Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM;
 
-use Pim\Bundle\CatalogBundle\Doctrine\ProductMassActionPersisterInterface;
+use Pim\Bundle\CatalogBundle\Doctrine\ProductMassActionRepositoryInterface;
 
 /**
- * Mass action persister for product documents
+ * Mass action repository for product documents
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductMassActionPersister implements ProductMassActionPersisterInterface
+class ProductMassActionRepository implements ProductMassActionRepositoryInterface
 {
 }
--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductMassActionRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductMassActionRepository.php
@@ -5,12 +5,12 @@ namespace Pim\Bundle\CatalogBundle\Doctrine\ORM;
 use Pim\Bundle\CatalogBundle\Doctrine\ProductMassActionPersisterInterface;
 
 /**
- * Mass action persister for product entities
+ * Mass action repository for product entities
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductMassActionPersister implements ProductMassActionPersisterInterface
+class ProductMassActionRepository implements ProductMassActionRepositoryInterface
 {
 }
