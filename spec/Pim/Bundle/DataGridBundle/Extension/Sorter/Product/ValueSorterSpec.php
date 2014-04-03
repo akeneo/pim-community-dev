fef5e2fa0f7a405ba2c9a4adad0a777bd33e096a
Nicolas Dupont <nicolas@akeneo.com>
2014-04-03 10:51:15 +0200

    PIM-2374 : update value sorter and add related spec

---
+++ spec/Pim/Bundle/DataGridBundle/Extension/Sorter/Product/ValueSorterSpec.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace spec\Pim\Bundle\DataGridBundle\Extension\Sorter\Product;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Doctrine\ORM\QueryBuilder;
+use Pim\Bundle\FilterBundle\Filter\ProductFilterUtility;
+use Pim\Bundle\CatalogBundle\Model\ProductRepositoryInterface;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
+use Pim\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
+use Pim\Bundle\CatalogBundle\Doctrine\ProductQueryBuilderInterface;
+use Pim\Bundle\DataGridBundle\Datasource\ProductDatasource;
+
+class ValueSorterSpec extends ObjectBehavior
+{
+    function let(ProductRepositoryInterface $repository, AttributeRepository $attributeRepository)
+    {
+        $this->beConstructedWith($repository, $attributeRepository);
+    }
+
+    function it_should_be_a_sorter()
+    {
+        $this->shouldBeAnInstanceOf('Pim\Bundle\DataGridBundle\Extension\Sorter\SorterInterface');
+    }
+
+    function it_applies_a_sort_on_product_sku(
+        ProductDatasource $datasource,
+        ProductRepositoryInterface $repository,
+        ProductQueryBuilderInterface $pqb,
+        QueryBuilder $qb,
+        CustomAttributeRepository $attributeRepository,
+        Attribute $sku
+    ) {
+        // TODO : how to mock the following magic method ?
+        $attributeRepository->findOneByCode('sku')->willReturn($sku);
+        $sku->getCode()->willReturn('sku');
+        $sku->__toString()->willReturn('sku');
+
+        $datasource->getQueryBuilder()->willReturn($qb);
+        $repository->getProductQueryBuilder($qb)->willReturn($pqb);
+        $pqb->addFieldSorter($sku, 'ASC')->shouldBeCalled();
+
+        $this->apply($datasource, $sku, 'ASC');
+    }
+}
+
+class CustomAttributeRepository extends AttributeRepository
+{
+    public function findOneByCode()
+    {
+        return null;
+    }
+}
