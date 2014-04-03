c2d204142b0dc6ae3c3e2be2c741e54827812b4c
Benoit Jacquemont <benoit@akeneo.com>
2014-04-03 10:25:18 +0200

    Firs step to refactor ProductRepository

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/AssociationRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/AssociationRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\CatalogBundle\Entity\Repository;
+namespace Pim\Bundle\CatalogBundle\Doctrine\ORM;
 
 use Pim\Bundle\CatalogBundle\Doctrine\EntityRepository;
 use Pim\Bundle\CatalogBundle\Entity\AssociationType;
@@ -13,7 +13,7 @@ use Pim\Bundle\CatalogBundle\Model\AssociationRepositoryInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AssociationRepository extends EntityRepository implements AssociationRepositoryInterface,
+class AssociationRepository implements AssociationRepositoryInterface,
  ReferableEntityRepositoryInterface
 {
     /**
