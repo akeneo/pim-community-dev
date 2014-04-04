2f500f9a086495d04752fba4b2ccb47301ca4d6f
Nicolas Dupont <nicolas@akeneo.com>
2014-04-04 16:01:10 +0200

    PIM-2342 : add a MongoDB product id filter

--- src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/Filter/ProductIdFilter.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/Filter/ProductIdFilter.php
@@ -4,19 +4,18 @@ namespace Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\Filter;
 
 use Doctrine\ODM\MongoDB\Query\Builder as QueryBuilder;
 use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
-use Pim\Bundle\CatalogBundle\Doctrine\AttributeFilterInterface;
 use Pim\Bundle\CatalogBundle\Doctrine\FieldFilterInterface;
 use Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\ProductQueryUtility;
 use Pim\Bundle\CatalogBundle\Context\CatalogContext;
 
 /**
- * Is asssociated filter
+ * Product id filter
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class IsAssociatedFilter implements FieldFilterInterface
+class ProductIdFilter implements FieldFilterInterface
 {
     /** @var QueryBuilder */
     protected $qb;
@@ -39,10 +38,16 @@ class IsAssociatedFilter implements FieldFilterInterface
      */
     public function addFieldFilter($field, $operator, $value)
     {
-        $product = $value['product'];
-        $associationType = $value['associationType'];
+        // TODO ADD = operator
 
-        // TODO : to implement
+        $field = '_id';
+        $value = is_array($value) ? $value : [$value];
+
+        if ($operator === 'NOT IN') {
+            $this->qb->field($field)->notIn($value);
+        } else {
+            $this->qb->field($field)->in($value);
+        }
 
         return $this;
     }
