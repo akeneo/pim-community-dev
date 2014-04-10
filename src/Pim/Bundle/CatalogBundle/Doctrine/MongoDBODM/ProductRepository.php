912c52e2237253d6b1ddab92c37515e5bf3b1874
Nicolas Dupont <nicolas@akeneo.com>
2014-04-10 10:22:34 +0200

    PIM-2368 : move methods from MongoDB product repo to mass action repo

--- src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/ProductRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/MongoDBODM/ProductRepository.php
@@ -770,21 +770,6 @@ class ProductRepository extends DocumentRepository implements
     /**
      * {@inheritdoc}
      */
-    public function applyMassActionParameters($qb, $inset, $values)
-    {
-        // manage inset for selected entities
-        if ($values) {
-            $qb->field('_id');
-            $inset ? $qb->in($values) : $qb->notIn($values);
-        }
-
-        // remove limit of the query
-        $qb->limit(null);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getAvailableAttributeIdsToExport(array $productIds)
     {
         $qb = $this->createQueryBuilder('p');
@@ -805,82 +790,6 @@ class ProductRepository extends DocumentRepository implements
     {
         $qb = $this->createQueryBuilder('p');
         $qb->field('_id')->in($productIds);
-
-        $cursor = $qb->getQuery()->execute();
-
-        return $cursor->toArray();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findCommonAttributeIds(array $productIds)
-    {
-        $results = $this->findValuesCommonAttributeIds($productIds);
-
-        $familyIds = $this->findFamiliesFromProductIds($productIds);
-        if (!empty($familyIds)) {
-            $families = $this->familyRepository->findAttributeIdsFromFamilies($familyIds);
-        }
-
-        $attIds = null;
-        foreach ($results as $result) {
-            $familyAttr = isset($result['_id']['family']) ? $families[$result['_id']['family']] : array();
-            $prodAttIds = array_unique(array_merge($result['attribute'], $familyAttr));
-            if (null === $attIds) {
-                $attIds = $prodAttIds;
-            } else {
-                $attIds = array_intersect($attIds, $prodAttIds);
-            }
-        }
-
-        return $attIds;
-    }
-
-    /**
-     * Find all common attribute ids with values from a list of product ids
-     * Only exists for ODM repository
-     *
-     * @param array $productIds
-     *
-     * @return array
-     */
-    protected function findValuesCommonAttributeIds(array $productIds)
-    {
-        $collection = $this->dm->getDocumentCollection($this->documentName);
-
-        $expr = new Expr($this->dm);
-        $expr->setClassMetadata($this->class);
-        $expr->field('_id')->in($productIds);
-
-        $pipeline = array(
-            array('$match' => $expr->getQuery()),
-            array('$unwind' => '$values'),
-            array(
-                '$group'  => array(
-                    '_id'       => array('id' => '$_id', 'family' => '$family'),
-                    'attribute' => array( '$addToSet' => '$values.attribute')
-                )
-            )
-        );
-
-        return $collection->aggregate($pipeline)->toArray();
-    }
-
-    /**
-     * Find family from list of products
-     *
-     * @param array $productIds
-     *
-     * @return array
-     */
-    protected function findFamiliesFromProductIds(array $productIds)
-    {
-        $qb = $this->createQueryBuilder('p');
-        $qb
-            ->field('_id')->in($productIds)
-            ->distinct('family')
-            ->hydrate(false);
 
         $cursor = $qb->getQuery()->execute();
 
