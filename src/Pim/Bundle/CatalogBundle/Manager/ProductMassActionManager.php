3bc4c45918fcbf1ad2e0e13f72eac7e47c661002
Nicolas Dupont <nicolas@akeneo.com>
2014-04-10 12:39:33 +0200

    PIM-2368 : introduce a mass action manager

---
+++ src/Pim/Bundle/CatalogBundle/Manager/ProductMassActionManager.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Manager;
+
+use Pim\Bundle\CatalogBundle\Repository\ProductMassActionRepositoryInterface;
+use Pim\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
+
+/**
+ * Product mass action manager
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductMassActionManager
+{
+    /**
+     * @var ProductMassActionRepositoryInterface $productRepository
+     */
+    protected $massActionRepository;
+
+    /**
+     * @var AttributeRepository $attributeRepository
+     */
+    protected $attributeRepository;
+
+    /**
+     * Constructor
+     *
+     * @param ProductMassActionRepositoryInterface $massActionRepository
+     * @param AttributeRepository                  $attributeRepository
+     */
+    public function __construct(
+        ProductMassActionRepositoryInterface $massActionRepository,
+        AttributeRepository $attributeRepository
+    )
+    {
+        $this->massActionRepository = $massActionRepository;
+        $this->attributeRepository  = $attributeRepository;
+    }
+
+    /**
+     * Find common attributes
+     * Common attributes are:
+     *   - not unique (and not identifier)
+     *   - without value AND link to family
+     *   - with value
+     *
+     * @param array $productIds
+     *
+     * @return \Pim\Bundle\CatalogBundle\Model\AbstractAttribute[]
+     */
+    public function findCommonAttributes(array $productIds)
+    {
+        $attributeIds = $this->massActionRepository->findCommonAttributeIds($productIds);
+
+        return $this
+            ->attributeRepository
+            ->findWithGroups(array_unique($attributeIds), array('unique' => 0));
+    }
+}
