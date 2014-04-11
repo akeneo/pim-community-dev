e26648df6c66a81a826054a8ad7364623ce94c21
Romain Monceau <romain@akeneo.com>
2014-04-11 14:45:46 +0200

    Fix bash script to build pim-docs for behat

--- app/Resources/jenkins/pim-docs/link-pim-docs.sh
+++ app/Resources/jenkins/pim-docs/link-pim-docs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# add Akeneo/CustomEntityBundle to composer.json
+php composer.phar require akeneo/custom-entity-bundle:dev-master
+
 # clone pim-docs repository
 git clone https://github.com/akeneo/pim-docs.git
 
@@ -9,4 +12,10 @@ ln -s ../pim-docs/src/Acme Acme
 cd ..
 
 # update AppKernel
-sed -i 's/PimEnrichBundle(),/PimEnrichBundle(),new Acme\\Bundle\\CatalogBundle\\AcmeCatalogBundle(),new Acme\\Bundle\\DemoConnectorBundle\\AcmeDemoConnectorBundle(),new Acme\\Bundle\\EnrichBundle\\AcmeEnrichBundle(),new Acme\\Bundle\\IcecatDemoBundle\\AcmeIcecatDemoBundle(),new Acme\\Bundle\\MyBundle\\AcmeMyBundle(),new Acme\\Bundle\\SpecificConnectorBundle\\AcmeSpecificConnectorBundle(),/' app/AppKernel.php
+sed -i 's/PimEnrichBundle(),/PimEnrichBundle(),new Pim\\Bundle\\CustomEntityBundle\\PimCustomEntityBundle(),new Acme\\Bundle\\CatalogBundle\\AcmeCatalogBundle(),new Acme\\Bundle\\DemoConnectorBundle\\AcmeDemoConnectorBundle(),new Acme\\Bundle\\EnrichBundle\\AcmeEnrichBundle(),new Acme\\Bundle\\InstallerBundle\\AcmeInstallerBundle(),new Acme\\Bundle\\SpecificConnectorBundle\\AcmeSpecificConnectorBundle(),/' app/AppKernel.php
+
+# update routing.yml
+echo "pim_customentity:" >> app/config/routing.yml
+echo "    prefix: /enrich" >> app/config/routing.yml
+echo "    resource: \"@PimCustomEntityBundle/Resources/config/routing.yml\"" >> app/config/routing.yml
+
