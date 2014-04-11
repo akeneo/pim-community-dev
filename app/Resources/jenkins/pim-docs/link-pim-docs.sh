d7ea9a3bc4572bb9e38bc8209aa5f5fc4a5e02d1
Romain Monceau <romain@akeneo.com>
2014-04-11 15:02:35 +0200

    Update script for pim-docs

--- app/Resources/jenkins/pim-docs/link-pim-docs.sh
+++ app/Resources/jenkins/pim-docs/link-pim-docs.sh
@@ -12,7 +12,7 @@ ln -s ../pim-docs/src/Acme Acme
 cd ..
 
 # update AppKernel
-sed -i 's/PimEnrichBundle(),/PimEnrichBundle(),new Pim\\Bundle\\CustomEntityBundle\\PimCustomEntityBundle(),new Acme\\Bundle\\CatalogBundle\\AcmeCatalogBundle(),new Acme\\Bundle\\DemoConnectorBundle\\AcmeDemoConnectorBundle(),new Acme\\Bundle\\EnrichBundle\\AcmeEnrichBundle(),new Acme\\Bundle\\InstallerBundle\\AcmeInstallerBundle(),new Acme\\Bundle\\SpecificConnectorBundle\\AcmeSpecificConnectorBundle(),/' app/AppKernel.php
+sed -i 's/PimEnrichBundle(),/PimEnrichBundle(),new Pim\\Bundle\\CustomEntityBundle\\PimCustomEntityBundle(),,new Acme\\Bundle\\CatalogBundle\\AcmeCatalogBundle(),new Acme\\Bundle\\DemoConnectorBundle\\AcmeDemoConnectorBundle(),new Acme\\Bundle\\EnrichBundle\\AcmeEnrichBundle(),new Acme\\Bundle\\InstallerBundle\\AcmeInstallerBundle(),new Acme\\Bundle\\SpecificConnectorBundle\\AcmeSpecificConnectorBundle(),/' app/AppKernel.php
 
 # update routing.yml
 echo "pim_customentity:" >> app/config/routing.yml
