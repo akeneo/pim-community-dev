e22821152d2f2b3e9f66be16ef3c8471271f8f58
Nicolas Dupont <nicolas@akeneo.com>
2014-04-11 11:08:48 +0200

    PIM-2408 : move methods from product repo to product cateory repo (ORM)

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductRepository.php
@@ -225,99 +225,6 @@ class ProductRepository extends EntityRepository implements
     /**
      * {@inheritdoc}
      */
-    public function getProductCountByTree(ProductInterface $product)
-    {
-        $productMetadata = $this->getClassMetadata();
-
-        $categoryAssoc = $productMetadata->getAssociationMapping('categories');
-
-        $categoryClass = $categoryAssoc['targetEntity'];
-        $categoryTable = $this->getEntityManager()->getClassMetadata($categoryClass)->getTableName();
-
-        $categoryAssocTable = $categoryAssoc['joinTable']['name'];
-
-        $sql = "SELECT".
-               "    tree.id AS tree_id,".
-               "    COUNT(category_product.product_id) AS product_count".
-               "  FROM $categoryTable tree".
-               "  JOIN $categoryTable category".
-               "    ON category.root = tree.id".
-               "  LEFT JOIN $categoryAssocTable category_product".
-               "    ON category_product.product_id = :productId".
-               "   AND category_product.category_id = category.id".
-               " GROUP BY tree.id";
-
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-        $stmt->bindValue('productId', $product->getId());
-
-        $stmt->execute();
-        $productCounts = $stmt->fetchAll();
-        $trees = array();
-        foreach ($productCounts as $productCount) {
-            $tree = array();
-            $tree['productCount'] = $productCount['product_count'];
-            $tree['tree'] = $this->getEntityManager()->getRepository($categoryClass)->find($productCount['tree_id']);
-            $trees[] = $tree;
-        }
-
-        return $trees;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getProductsCountInCategory(
-        CategoryInterface $category,
-        QueryBuilder $categoryQb = null
-    ) {
-        $qb = $this->createQueryBuilder('p');
-        $qb->select($qb->expr()->count('distinct p'));
-        $qb->join('p.categories', 'node');
-
-        if (null === $categoryQb) {
-            $qb->where('node.id = :nodeId');
-            $qb->setParameter('nodeId', $category->getId());
-        } else {
-            $qb->where($categoryQb->getDqlPart('where'));
-            $qb->setParameters($categoryQb->getParameters());
-        }
-
-        return $qb->getQuery()->getSingleScalarResult();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getProductIdsInCategory(
-        CategoryInterface $category,
-        QueryBuilder $categoryQb = null
-    ) {
-        $qb = $this->createQueryBuilder('p');
-        $qb->select('p.id');
-        $qb->join('p.categories', 'node');
-
-        if (null === $categoryQb) {
-            $qb->where('node.id = :nodeId');
-            $qb->setParameter('nodeId', $category->getId());
-        } else {
-            $qb->where($categoryQb->getDqlPart('where'));
-            $qb->setParameters($categoryQb->getParameters());
-        }
-
-        $products = $qb->getQuery()->execute(array(), AbstractQuery::HYDRATE_ARRAY);
-
-        $productIds = array();
-        foreach ($products as $product) {
-            $productIds[] = $product['id'];
-        }
-        $productIds = array_unique($productIds);
-
-        return $productIds;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function findByReference($code)
     {
         return $this->createQueryBuilder('p')
@@ -508,22 +415,6 @@ class ProductRepository extends EntityRepository implements
         );
 
         return $productIds;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function applyFilterByIds($qb, array $productIds, $include)
-    {
-        $rootAlias  = $qb->getRootAlias();
-        if ($include) {
-            $expression = $qb->expr()->in($rootAlias .'.id', $productIds);
-            $qb->andWhere($expression);
-
-        } else {
-            $expression = $qb->expr()->notIn($rootAlias .'.id', $productIds);
-            $qb->andWhere($expression);
-        }
     }
 
     /**
