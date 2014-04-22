94887fc68f428ec3ad4d1569e4546101c9de1698
Filips Alpe <filips@akeneo.com>
2014-04-22 21:27:24 +0200

    Render the navigation

--- src/Pim/Bundle/UxBundle/Resources/public/js/app.js
+++ src/Pim/Bundle/UxBundle/Resources/public/js/app.js
@@ -1,1 +1,4 @@
-angular.module('App', ['ui.router', 'ui.bootstrap', 'App.services', 'App.controllers', 'App.directives', 'App.filters']);
+angular.module('App', ['ui.router', 'ui.bootstrap', 'App.services', 'App.controllers', 'App.directives', 'App.filters']).
+config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+});
