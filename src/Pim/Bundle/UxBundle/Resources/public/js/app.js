f78ef9845c88f92e65ccee75b87df47cc53b9aba
Filips Alpe <filips@akeneo.com>
2014-04-24 00:23:53 +0200

    Implement very basic routing

--- src/Pim/Bundle/UxBundle/Resources/public/js/app.js
+++ src/Pim/Bundle/UxBundle/Resources/public/js/app.js
@@ -1,4 +1,16 @@
+'use strict';
+
 angular.module('App', ['ui.router', 'ui.bootstrap', 'App.services', 'App.controllers', 'App.directives', 'App.filters']).
 config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('dashboard', {
+            url: '/',
+            template: '<h3>Dashboard</h3>'
+        })
+        .state('pim_enrich_product', {
+            url: '/products',
+            template: '<h3>Products</h3>'
+        });
 });
