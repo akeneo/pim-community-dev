8827a8bc3afc20cdca4c8e64bd3a384155790f7f
Nicolas Dupont <nicolas@akeneo.com>
2014-04-24 19:29:18 +0200

    Overrice the version twig template to provide extra details about version in EE

---
+++ src/EnterprisePim/Bundle/DashboardBundle/EnterprisePimDashboardBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EnterprisePim\Bundle\DashboardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Dashboard bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class EnterprisePimDashboardBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'PimDashboardBundle';
+    }
+}
