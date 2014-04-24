963a7f225f83ca176d369292051c396243824567
Julien Sanchez <sumbobyboys@gmail.com>
2014-04-24 23:57:47 +0200

    add grid display with TDs

--- src/Pim/Bundle/UxBundle/Resources/public/js/directives.js
+++ src/Pim/Bundle/UxBundle/Resources/public/js/directives.js
@@ -16,4 +16,28 @@ angular.module('App.directives', [])
                 });
             }
         };
+    })
+    .directive('gridRow', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                row: '='
+            },
+            templateUrl: '/bundles/pimux/templates/grid/row.html',
+            controller: function($scope) {
+            }
+        };
+    })
+    .directive('gridCell', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                cell: '='
+            },
+            templateUrl: '/bundles/pimux/templates/grid/cell.html',
+            controller: function($scope) {
+            }
+        };
     });
