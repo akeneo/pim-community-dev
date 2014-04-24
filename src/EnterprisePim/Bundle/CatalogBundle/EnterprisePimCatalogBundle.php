d0e0cb7347a54597ea47f77dd866fd4ab0a93684
Nicolas Dupont <nicolas@akeneo.com>
2014-04-24 19:27:57 +0200

    Add a EnterprisePim CatalogBundle (to provide EE version)

---
+++ src/EnterprisePim/Bundle/CatalogBundle/EnterprisePimCatalogBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EnterprisePim\Bundle\CatalogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PIM Enterprise Catalog Bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class EnterprisePimCatalogBundle extends Bundle
+{
+}
---
+++ src/EnterprisePim/Bundle/CatalogBundle/Version.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace EnterprisePim\Bundle\CatalogBundle;
+
+/**
+ * PIM Enterprise Version
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class Version
+{
+    /** @staticvar string */
+    const VERSION = '1.0.0-DEV';
+
+    /** @staticvar string */
+    const VERSION_CODENAME = '';
+
+    /** @staticvar string */
+    const MAJOR_VERSION = '1';
+
+    /** @staticvar string */
+    const MINOR_VERSION = '0';
+
+    /** @staticvar string */
+    const PATCH_VERSION = '0';
+
+    /** @staticvar string */
+    const EXTRA_VERSION = '';
+}
