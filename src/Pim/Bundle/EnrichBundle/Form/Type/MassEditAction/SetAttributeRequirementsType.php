cbd2f3e664e2b9d497eefae227b5a2c341b8e7a7
Gildas Quéméner <gildas.quemener@gmail.com>
2014-04-24 18:11:03 +0200

    PIM-1703 Created the mass family requirements edition form type

---
+++ src/Pim/Bundle/EnrichBundle/Form/Type/MassEditAction/SetAttributeRequirementsType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Form\Type\MassEditAction;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Form type of the SetAttributeRequirements operation
+ *
+ * @author    Gildas Quemener <gildas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class SetAttributeRequirementsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('attributeRequirements', 'collection', ['type' => 'pim_enrich_attribute_requirement']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'Pim\\Bundle\\EnrichBundle\\MassEditAction\\Operation\\SetAttributeRequirements',
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_enrich_mass_set_attribute_requirements';
+    }
+}
