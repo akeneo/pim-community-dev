749c0c7e6732d193b6359c124518aa0fe6827456
Nicolas Dupont <nicolas@akeneo.com>
2014-04-28 20:03:31 +0200

    PIM-2498 : cosmetic on attribute group voter

--- src/EnterprisePim/Bundle/SecurityBundle/Voter/AttributeGroupVoter.php
+++ src/EnterprisePim/Bundle/SecurityBundle/Voter/AttributeGroupVoter.php
@@ -8,6 +8,12 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Pim\Bundle\CatalogBundle\Entity\AttributeGroup;
 
+/**
+ * Enterprise Security Bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
 class AttributeGroupVoter implements VoterInterface
 {
     /**
@@ -24,20 +30,31 @@ class AttributeGroupVoter implements VoterInterface
         $this->objectManager = $objectManager;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function supportsAttribute($attribute)
     {
-        return in_array($attribute, array('VIEW_PRODUCT_DATA', 'EDIT'));
+        return in_array($attribute, array('VIEW_PRODUCT_DATA', 'EDIT_PRODUCT_DATA'));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function supportsClass($class)
     {
         return $class instanceof AttributeGroup;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     function vote(TokenInterface $token, $object, array $attributes)
     {
         foreach ($attributes as $attribute) {
             if ($this->supportsAttribute($attribute) && $this->supportsClass($object)) {
+
+                // TODO : delegate this to a dedicated service / repository ?
                 $role = $this->objectManager->getRepository('OroUserBundle:Role')->findOneByRole('ROLE_ADMINISTRATOR');
                 $user = $token->getUser();
                 if ($user->hasRole($role)) {
@@ -49,4 +66,3 @@ class AttributeGroupVoter implements VoterInterface
         return VoterInterface::ACCESS_DENIED;
     }
 }
-
