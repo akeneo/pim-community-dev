56fead7f373cbff3b172394654f0d1c81660ec51
Filips Alpe <filips@akeneo.com>
2014-04-29 13:33:36 +0200

    Add the rights fields to the attribute group edit form

--- src/PimEnterprise/Bundle/EnrichBundle/Form/Type/AttributeGroupType.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Form/Type/AttributeGroupType.php
@@ -6,11 +6,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Pim\Bundle\EnrichBundle\Form\Type\AttributeGroupType as BaseAttributeGroupType;
 
 /**
- * Form yype for AttributeGroup with custom ACL configuration
+ * Form type for AttributeGroup with custom ACL configuration
  *
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class AttributeGroupType extends BaseAttributeGroupType
 {
@@ -20,5 +19,7 @@ class AttributeGroupType extends BaseAttributeGroupType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
+
+        $builder->add('rights', 'pim_enterprise_enrich_attribute_group_rights');
     }
 }
