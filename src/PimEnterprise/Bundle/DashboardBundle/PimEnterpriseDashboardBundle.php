ca5b1e205be534f3fe7735ae0b3d7fc7bdf758db
Filips Alpe <filips@akeneo.com>
2014-04-29 13:34:45 +0200

    Remove forgotten @license tag

--- src/PimEnterprise/Bundle/DashboardBundle/PimEnterpriseDashboardBundle.php
+++ src/PimEnterprise/Bundle/DashboardBundle/PimEnterpriseDashboardBundle.php
@@ -8,8 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * Dashboard bundle
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  */
 class PimEnterpriseDashboardBundle extends Bundle
 {
