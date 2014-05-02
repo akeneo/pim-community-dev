3dc612d55a84c42c6daa79169aa6f9d36b7e8fae
Romain Monceau <romain@akeneo.com>
2014-05-02 10:15:51 +0200

    PIM-2492: Clean OptionFilter class

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/OptionFilter.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/OptionFilter.php
@@ -18,12 +18,9 @@ class OptionFilter extends EntityFilter
      */
     public function addAttributeFilter(AbstractAttribute $attribute, $operator, $value)
     {
-        $backendType = $attribute->getBackendType();
         $joinAlias = 'filter'.$attribute->getCode().$this->aliasCounter++;
 
-        // inner join to value
-        $condition = $this->prepareAttributeJoinCondition($attribute, $joinAlias);
-
+        // prepare join value condition
         $optionAlias = $joinAlias .'.option';
         if (in_array('empty', $value)) {
             unset($value[array_search('empty', $value)]);
@@ -37,6 +34,8 @@ class OptionFilter extends EntityFilter
             $expr = $this->qb->expr()->in($optionAlias, $value);
         }
 
+        // inner join to value
+        $condition = $this->prepareAttributeJoinCondition($attribute, $joinAlias);
         $condition .= ' AND ( '. $expr .' ) ';
 
         $this->qb->innerJoin(
@@ -45,25 +44,6 @@ class OptionFilter extends EntityFilter
             'WITH',
             $condition
         );
-
-
-        // SELECT     p0_.*
-        // FROM       pim_catalog_product p0_
-        // INNER JOIN pim_catalog_product_value p1_ ON p0_.id = p1_.entity_id
-        //        AND (p1_.attribute_id = 4 AND (p1_.option_id IN ('11') OR p1_.option_id IS NULL))
-        // GROUP BY   p0_.id;
-
-//         $joinAliasOpt = 'filterO'.$attribute->getCode().$this->aliasCounter;
-//         $backendField = sprintf('%s.%s', $joinAliasOpt, 'id');
-
-//         if (in_array('empty', $value)) {
-//             $expr = $this->qb->expr()->isNull($joinAlias .'.option');
-//             $this->qb->andWhere($expr);
-//         } else {
-//             $condition = $this->prepareCriteriaCondition($backendField, $operator, $value);
-
-//             $this->qb->innerJoin($joinAlias.'.'.$backendType, $joinAliasOpt, 'WITH', $condition);
-//         }
 
         return $this;
     }
