90a550555ca2df24c781900d5b3d8512eb0b5db5
Gildas Quéméner <gildas.quemener@gmail.com>
2014-05-06 14:18:17 +0200

    PIM-2496 Fixed the entity changes provider spec

--- spec/PimEnterprise/Bundle/CatalogBundle/Persistence/ORM/EntityChangesProviderSpec.php
+++ spec/PimEnterprise/Bundle/CatalogBundle/Persistence/ORM/EntityChangesProviderSpec.php
@@ -11,47 +11,57 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
 use Pim\Bundle\CatalogBundle\Model\AbstractProductValue;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use PimEnterprise\Bundle\CatalogBundle\Persistence\Engine\ArrayDiff;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 
 class EntityChangesProviderSpec extends ObjectBehavior
 {
-    function let(ManagerRegistry $registry)
+    function let(ManagerRegistry $registry, NormalizerInterface $normalizer, ArrayDiff $engine)
     {
-        $this->beConstructedWith($registry);
+        $this->beConstructedWith($registry, $normalizer, $engine);
     }
 
-    function it_computes_new_values_through_the_unit_of_work(
+    function it_computes_new_values_through_the_normalized_values(
         $registry,
+        $normalizer,
+        $engine,
         ProductInterface $product,
         AbstractProductValue $sku,
         AbstractProductValue $name,
         AbstractProductValue $description,
-        EntityManager $manager,
-        UnitOfWork $uow,
-        ClassMetadata $metadata
+        EntityManager $manager
     ) {
         $registry->getManagerForClass(get_class($product->getWrappedObject()))->willReturn($manager);
-        $manager->getUnitOfWork()->willReturn($uow);
-        $manager->getClassMetadata(Argument::any())->willReturn($metadata);
 
         $product->getValues()->willReturn([$sku, $name, $description]);
-        $uow->computeChangeSet($metadata, $sku)->shouldBeCalled();
-        $uow->computeChangeSet($metadata, $name)->shouldBeCalled();
-        $uow->computeChangeSet($metadata, $description)->shouldBeCalled();
 
-        $uow->getEntityChangeSet($sku)->willReturn([]);
-        $uow->getEntityChangeSet($name)->willReturn(['varchar' => ['foo', 'bar']]);
-        $uow->getEntityChangeSet($description)->willReturn(['text' => ['long test is long', 'long text is long']]);
+        $normalizer->normalize($sku, 'json', Argument::any())->willReturn(['value' => 'foo'], ['value' => 'foo']);
+        $sku->getId()->willReturn(1);
 
-        $sku->getId()->shouldNotBeCalled();
-        $name->getId()->willReturn(1);
-        $description->getId()->willReturn(2);
+        $normalizer->normalize($name, 'json', Argument::any())->willReturn(['value' => 'bar'], ['value' => 'baz']);
+        $name->getId()->willReturn(2);
 
-        $this->computeNewValues($product)->shouldReturn(
+        $normalizer->normalize($description, 'json', Argument::any())->willReturn(['value' => ''], ['value' => 'desc']);
+        $description->getId()->willReturn(3);
+
+        $manager->refresh($sku)->shouldBeCalled();
+        $manager->refresh($name)->shouldBeCalled();
+        $manager->refresh($description)->shouldBeCalled();
+
+        $engine->diff(
             [
-                1 => ['varchar' => 'bar'],
-                2 => ['text' => 'long text is long'],
+                1 => ['value' => 'foo'],
+                2 => ['value' => 'baz'],
+                3 => ['value' => 'desc'],
+            ],
+            [
+                1 => ['value' => 'foo'],
+                2 => ['value' => 'bar'],
+                3 => ['value' => ''],
             ]
-        );
+        )->willReturn('differences');
+
+        $this->computeNewValues($product)->shouldReturn('differences');
     }
 }
