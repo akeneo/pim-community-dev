cef7da8685a6866aa3841f15e2213b6155b3b2d2
Gildas Quéméner <gildas.quemener@gmail.com>
2014-05-06 12:15:26 +0200

    PIM-2496 Removed licence header

--- src/PimEnterprise/Bundle/CatalogBundle/Entity/Revision.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Entity/Revision.php
@@ -10,7 +10,6 @@ use Pim\Bundle\CatalogBundle\Model\ProductInterface;
  *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Revision
 {
--- src/PimEnterprise/Bundle/CatalogBundle/Persistence/RevisionPersister.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Persistence/RevisionPersister.php
@@ -14,7 +14,6 @@ use PimEnterprise\Bundle\CatalogBundle\Factory\RevisionFactory;
  *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class RevisionPersister implements ProductPersister
 {
