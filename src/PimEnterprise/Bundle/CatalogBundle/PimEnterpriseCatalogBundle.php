21b1684c1690ba61812b8cf297d6230d1b5f682e
Gildas Quéméner <gildas.quemener@gmail.com>
2014-05-07 16:50:07 +0200

    PIM-2496 Defined a proposal mapping for mongodb odm

--- src/PimEnterprise/Bundle/CatalogBundle/PimEnterpriseCatalogBundle.php
+++ src/PimEnterprise/Bundle/CatalogBundle/PimEnterpriseCatalogBundle.php
@@ -3,6 +3,8 @@
 namespace PimEnterprise\Bundle\CatalogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Oro\Bundle\EntityBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 
 /**
  * PIM Enterprise Catalog Bundle
@@ -12,4 +14,35 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimEnterpriseCatalogBundle extends Bundle
 {
+    /** @staticvar string */
+    const DOCTRINE_MONGODB = '\Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $mappings = array(
+            realpath(__DIR__ . '/Resources/config/model/doctrine') => 'PimEnterprise\Bundle\CatalogBundle\Model'
+        );
+
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createYamlMappingDriver(
+                $mappings,
+                array('doctrine.orm.entity_manager'),
+                'pim_catalog.storage_driver.doctrine/orm'
+            )
+        );
+
+        if (class_exists(self::DOCTRINE_MONGODB)) {
+            $mongoDBClass = self::DOCTRINE_MONGODB;
+            $container->addCompilerPass(
+                $mongoDBClass::createYamlMappingDriver(
+                    $mappings,
+                    array('doctrine.odm.mongodb.document_manager'),
+                    'pim_catalog.storage_driver.doctrine/mongodb-odm'
+                )
+            );
+        }
+    }
 }
