6ffc3b44464deac294945ee48c5a00972bc6c71e
Filips Alpe <filips@akeneo.com>
2014-05-13 14:02:52 +0200

    Remove @licence doc tag

--- src/PimEnterprise/Bundle/EnrichBundle/Form/View/ProductFormView.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Form/View/ProductFormView.php
@@ -16,7 +16,6 @@ use PimEnterprise\Bundle\SecurityBundle\Voter\AttributeGroupVoter;
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class ProductFormView extends PimProductFormView
 {
