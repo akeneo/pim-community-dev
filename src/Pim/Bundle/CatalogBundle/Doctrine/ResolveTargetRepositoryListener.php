282822be47a729d0fc0fcf2d7845d22cf163e266
Romain Monceau <romain@akeneo.com>
2014-05-19 10:46:32 +0200

    PIM-2558: Add phpdoc and clean class

--- src/Pim/Bundle/CatalogBundle/Doctrine/ResolveTargetRepositoryListener.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ResolveTargetRepositoryListener.php
@@ -5,22 +5,35 @@ namespace Pim\Bundle\CatalogBundle\Doctrine;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 
 /**
- *
- * Enter description here ...
+ * Mechanism to overwrite repository class without redefine class mapping
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
  */
 class ResolveTargetRepositoryListener
 {
-    private $resolveTargetRepositories = array();
+    /**
+     * @var array
+     */
+    protected $resolveTargetRepositories = array();
 
+    /**
+     * Adds repository class for a class name
+     *
+     * @param string $entity
+     * @param string $newRepository
+     */
     public function addResolveTargetRepository($entity, $newRepository)
     {
         $this->resolveTargetRepositories[ltrim($entity)] = $newRepository;
     }
+
+    /**
+     * Processes event and resolves new object repository class
+     *
+     * @param LoadClassMetadataEventArgs $args
+     */
     public function loadClassMetadata(LoadClassMetadataEventArgs $args)
     {
         $cm = $args->getClassMetadata();
