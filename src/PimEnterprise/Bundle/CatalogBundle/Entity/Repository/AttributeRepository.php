c1a5188b6266fb0f0406d6e918e2bba95522ed8a
Romain Monceau <romain@akeneo.com>
2014-05-19 14:50:13 +0200

    PIM-2558: Override AttributeRepository

---
+++ src/PimEnterprise/Bundle/CatalogBundle/Entity/Repository/AttributeRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PimEnterprise\Bundle\CatalogBundle\Entity\Repository;
+
+use Pim\Bundle\CatalogBundle\Entity\Repository\AttributeRepository as PimAttributeRepository;
+
+/**
+ * Override attribute repository
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class AttributeRepository extends PimAttributeRepository
+{
+}
