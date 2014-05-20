10029d0db57664ac284f7663e69caae7658ed726
Romain Monceau <romain@akeneo.com>
2014-05-20 10:35:41 +0200

    PIM-2553: Redefine manager services

--- src/PimEnterprise/Bundle/CatalogBundle/DependencyInjection/PimEnterpriseCatalogExtension.php
+++ src/PimEnterprise/Bundle/CatalogBundle/DependencyInjection/PimEnterpriseCatalogExtension.php
@@ -21,6 +21,7 @@ class PimEnterpriseCatalogExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('managers.yml');
         $loader->load('repositories.yml');
     }
 }
