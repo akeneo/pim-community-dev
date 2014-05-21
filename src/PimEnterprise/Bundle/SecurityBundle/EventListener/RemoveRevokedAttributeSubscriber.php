4966aed85145d0f6f7238b7ac0b1f8f9fa66b836
Romain Monceau <romain@akeneo.com>
2014-05-21 11:35:40 +0200

    PIM-2554: Define subscriber to remove revoked attributes

---
+++ src/PimEnterprise/Bundle/SecurityBundle/EventListener/RemoveRevokedAttributeSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PimEnterprise\Bundle\SecurityBundle\EventListener;
+
+use Symfony\Component\Form\FormEvent;
+
+use Symfony\Component\Form\FormEvents;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ *
+ * Enter description here ...
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+class RemoveRevokedAttributeSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::PRE_SET_DATA => 'preSetData'
+        );
+    }
+
+    /**
+     * Remove revoked attribute
+     *
+     * @param FormEvent $event
+     */
+    public function preSetData(FormEvent $event)
+    {
+        //TODO: Implementation
+    }
+}
