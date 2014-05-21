ded4355da637e9cba9467b24932a21da246c59e4
Romain Monceau <romain@akeneo.com>
2014-05-21 15:03:34 +0200

    PIM-2554: Inject attribute group access repo

--- src/PimEnterprise/Bundle/EnrichBundle/Form/Type/AvailableAttributesType.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Form/Type/AvailableAttributesType.php
@@ -3,7 +3,11 @@
 namespace PimEnterprise\Bundle\EnrichBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 use Pim\Bundle\EnrichBundle\Form\Type\AvailableAttributesType as PimAvailableAttributesType;
+use Pim\Bundle\UserBundle\Context\UserContext;
+use PimEnterprise\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
+use PimEnterprise\Bundle\SecurityBundle\Entity\Repository\AttributeGroupAccessRepository;
 
 /**
  * Override available attributes type to remove attributes where rights are revoked
@@ -13,4 +17,27 @@ use Pim\Bundle\EnrichBundle\Form\Type\AvailableAttributesType as PimAvailableAtt
  */
 class AvailableAttributesType extends PimAvailableAttributesType
 {
+    /** @var AttributeGroupAccessRepository */
+    protected $attGroupAccessRepo;
+
+    /**
+     * Construct
+     *
+     * @param string                         $attributeClass
+     * @param AttributeRepository            $attributeRepository
+     * @param UserContext                    $userContext
+     * @param TranslatorInterface            $translator
+     * @param AttributeGroupAccessRepository $attGroupAccessRepo
+     */
+    public function __construct(
+        $attributeClass,
+        AttributeRepository $repository,
+        UserContext $userContext,
+        TranslatorInterface $translator,
+        AttributeGroupAccessRepository $attGroupAccessRepo
+    ) {
+        parent::__construct($attributeClass, $repository, $userContext, $translator);
+
+        $this->attGroupAccessRepo = $attGroupAccessRepo;
+    }
 }
