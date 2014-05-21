bb4d061cf7dfb0bd56afa23c027aa8f7d2575f54
Romain Monceau <romain@akeneo.com>
2014-05-21 11:38:46 +0200

    PIM-2554: Clean code

--- src/PimEnterprise/Bundle/SecurityBundle/EventListener/RemoveRevokedAttributeSubscriber.php
+++ src/PimEnterprise/Bundle/SecurityBundle/EventListener/RemoveRevokedAttributeSubscriber.php
@@ -3,19 +3,15 @@
 namespace PimEnterprise\Bundle\SecurityBundle\EventListener;
 
 use Symfony\Component\Form\FormEvent;
-
 use Symfony\Component\Form\FormEvents;
-
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- *
- * Enter description here ...
+ * Remove revoked attributes for AvailableAttributeType
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
  */
 class RemoveRevokedAttributeSubscriber implements EventSubscriberInterface
 {
