52d9676110a81c3334226d801f38a5a6c241b190
Gildas Quéméner <gildas.quemener@gmail.com>
2014-05-22 10:05:47 +0200

    PIM-2225 Removed license header

--- src/PimEnterprise/Bundle/WorkflowBundle/Form/Comparator/ChainedComparator.php
+++ src/PimEnterprise/Bundle/WorkflowBundle/Form/Comparator/ChainedComparator.php
@@ -9,7 +9,6 @@ use Pim\Bundle\CatalogBundle\Model\AbstractProductValue;
  *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class ChainedComparator implements ComparatorInterface
 {
--- src/PimEnterprise/Bundle/WorkflowBundle/Form/Subscriber/CollectProductValuesSubscriber.php
+++ src/PimEnterprise/Bundle/WorkflowBundle/Form/Subscriber/CollectProductValuesSubscriber.php
@@ -13,7 +13,6 @@ use PimEnterprise\Bundle\WorkflowBundle\Form\Comparator\ComparatorInterface;
  *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class CollectProductValuesSubscriber implements EventSubscriberInterface
 {
