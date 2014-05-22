ff5c7b7f65887b87ec2c0a28700cdafa4dabb43b
jjanvier <j.janvier@gmail.com>
2014-05-23 00:14:07 +0200

    PIM-2178: Import the fixtures with the batch jobs

--- src/Pim/Bundle/InstallerBundle/DataFixtures/ORM/LoadFixturesData.php
+++ src/Pim/Bundle/InstallerBundle/DataFixtures/ORM/LoadFixturesData.php
@@ -2,24 +2,28 @@
 
 namespace Pim\Bundle\InstallerBundle\DataFixtures\ORM;
 
+use Akeneo\Bundle\BatchBundle\Command\BatchCommand;
+use Akeneo\Bundle\BatchBundle\Entity\JobInstance;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Pim\Bundle\CatalogBundle\DependencyInjection\PimCatalogExtension;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Load fixtures data
  *
- * @author    Antoine Guigan <antoine@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class LoadFixturesData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
-    /**
-     * @var ContainerInterface
-     */
+    /** @var ContainerInterface */
     protected $container;
 
     /**
@@ -27,7 +31,9 @@ class LoadFixturesData extends AbstractFixture implements OrderedFixtureInterfac
      */
     public function load(ObjectManager $manager)
     {
-        $this->getLoader()->load($manager, $this->referenceRepository, $this->getFiles());
+        foreach ($this->getJobs($manager) as $job) {
+            $this->launchJob($job);
+        }
     }
 
     /**
@@ -47,36 +53,55 @@ class LoadFixturesData extends AbstractFixture implements OrderedFixtureInterfac
     }
 
     /**
-     * @return \Pim\Bundle\InstallerBundle\FixtureLoader\MultipleLoader
-     */
-    protected function getLoader()
-    {
-        return $this->container->get('pim_installer.fixture_loader.multiple_loader');
-    }
-
-    /**
-     * Returns an array of fixture files
+     * Get the list of stored jobs
+     *
+     * @param ObjectManager $manager
      *
      * @return array
      */
-    protected function getFiles()
+    protected function getJobs(ObjectManager $manager)
     {
-        $dataParam = $this->container->getParameter('installer_data');
-        preg_match('/^(?P<bundle>\w+):(?P<directory>\w+)$/', $dataParam, $matches);
-        $bundles    = $this->container->getParameter('kernel.bundles');
-        $reflection = new \ReflectionClass($bundles[$matches['bundle']]);
-        $dataPath   = dirname($reflection->getFilename()) . '/Resources/fixtures/' . $matches['directory'];
+        //TODO: use repository with class name as parameters
+        /** @var JobInstance[] $jobs */
+        $jobs = $manager->getRepository('AkeneoBatchBundle:JobInstance')->findAll();
 
-        $paths = glob($dataPath.'/*');
-        if ('doctrine/mongodb-odm' === $this->container->getParameter('pim_catalog.storage_driver')) {
-            // Do not load products and associations with the ORM fixtures when mongodb support is activated
-            foreach ($paths as $key => $path) {
-                if (false !== strpos($path, 'products.') || false !== strpos($path, 'associations.')) {
-                    unset($paths[$key]);
-                }
+        foreach ($jobs as $key => $job) {
+            // Do not load products and associations with the ORM fixtures when MongoDB support is activated
+            if (PimCatalogExtension::DOCTRINE_MONGODB_ODM === $this->container->getParameter('pim_catalog.storage_driver') &&
+                (false !== strpos($job->getCode(), 'fixtures_product') ||
+                    false !== strpos($job->getCode(), 'fixtures_association'))
+            ) {
+                unset($jobs[$key]);
+            }
+
+            // Do not load job when fixtures file is not available
+            if (!is_readable($job->getRawConfiguration()['filePath'])) {
+                unset($jobs[$key]);
             }
         }
 
-        return $paths;
+        return $jobs;
+    }
+
+    /**
+     * Launch a job
+     * TODO: refactor all this
+     *
+     * @param JobInstance $job
+     */
+    protected function launchJob(JobInstance $job)
+    {
+        $app = new Application($this->container->get('kernel'));
+
+        $cmd = new BatchCommand();
+        $cmd->setContainer($this->container);
+        $cmd->setApplication($app);
+        $cmd->run(
+            new ArrayInput(array(
+                'command'     => 'akeneo:batch:job',
+                'code'        => $job->getCode(),
+            )),
+            new ConsoleOutput()
+        );
     }
 }
