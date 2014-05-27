c10c94b55e9f3bc97535d6d40a54a2d67048593c
Antoine Guigan <antoine@akeneo.com>
2014-05-27 14:22:06 +0200

    Corrected copyright

--- src/Pim/Bundle/UIBundle/Form/Transformer/IntegerTransformer.php
+++ src/Pim/Bundle/UIBundle/Form/Transformer/IntegerTransformer.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\DataTransformerInterface;
  * Transforms numbers into integers
  * 
  * @author    Antoine Guigan <antoine@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class IntegerTransformer implements DataTransformerInterface
--- src/Pim/Bundle/UIBundle/Form/Type/NumberType.php
+++ src/Pim/Bundle/UIBundle/Form/Type/NumberType.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  * PIM number type
  * 
  * @author    Antoine Guigan <antoine@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class NumberType extends AbstractType
