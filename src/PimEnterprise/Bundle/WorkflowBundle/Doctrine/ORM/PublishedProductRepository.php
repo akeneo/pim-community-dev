38eaf901d95d4679734e157686f8ce555fc5511f
Nicolas Dupont <nicolas@akeneo.com>
2014-05-28 19:02:45 +0200

    PIM-2602 : fix CS

--- src/PimEnterprise/Bundle/WorkflowBundle/Doctrine/ORM/PublishedProductRepository.php
+++ src/PimEnterprise/Bundle/WorkflowBundle/Doctrine/ORM/PublishedProductRepository.php
@@ -2,21 +2,8 @@
 
 namespace PimEnterprise\Bundle\WorkflowBundle\Doctrine\ORM;
 
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
-use Doctrine\ORM\Tools\Pagination\Paginator;
-use Pim\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
-use Pim\Bundle\CatalogBundle\Repository\ProductRepositoryInterface;
-use Pim\Bundle\CatalogBundle\Repository\ReferableEntityRepositoryInterface;
-use Pim\Bundle\CatalogBundle\Entity\Channel;
-use Pim\Bundle\CatalogBundle\Entity\Group;
-use Pim\Bundle\CatalogBundle\Model\ProductValueInterface;
-use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
-
 use Pim\Bundle\CatalogBundle\Doctrine\ORM\ProductRepository;
 use PimEnterprise\Bundle\WorkflowBundle\Repository\PublishedProductRepositoryInterface;
-
-
 
 /**
  * Published products repository
