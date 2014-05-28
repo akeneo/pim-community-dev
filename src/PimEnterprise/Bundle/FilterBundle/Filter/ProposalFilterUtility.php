c70e4990325de3c7eb72d32f482a7e3661c12979
Romain Monceau <romain@akeneo.com>
2014-05-28 14:03:12 +0200

    PIM-2585: Create proposal filter utility

--- src/PimEnterprise/Bundle/FilterBundle/Filter/ProposalFilterUtility.php
+++ src/PimEnterprise/Bundle/FilterBundle/Filter/ProposalFilterUtility.php
@@ -3,6 +3,7 @@
 namespace PimEnterprise\Bundle\FilterBundle\Filter;
 
 use Oro\Bundle\FilterBundle\Filter\FilterUtility as BaseFilterUtility;
+use PimEnterprise\Bundle\WorkflowBundle\Model\Repository\ProposalRepositoryInterface;
 
 /**
  * Proposal filter utility
@@ -14,4 +15,17 @@ class ProposalFilterUtility extends BaseFilterUtility
 {
     /** @staticvar string */
     const PARENT_TYPE_KEY = 'parent_type';
+
+    /** @var ProposalRepositoryInterface */
+    protected $proposalRepository;
+
+    /**
+     * Constructor
+     *
+     * @param ProposalRepositoryInterface $proposalRepository
+     */
+    public function __construct(ProposalRepositoryInterface $proposalRepository)
+    {
+        $this->proposalRepository = $proposalRepository;
+    }
 }
