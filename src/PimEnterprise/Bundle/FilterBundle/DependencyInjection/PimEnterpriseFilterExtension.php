ce52f4c4efa6ecd2a059bb2bdd4d072e23f7042e
Romain Monceau <romain@akeneo.com>
2014-05-28 11:39:40 +0200

    PIM-2585: Create filter bundle

---
+++ src/PimEnterprise/Bundle/FilterBundle/DependencyInjection/PimEnterpriseFilterExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PimEnterprise\Bundle\FilterBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Enterprise filter extension
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class PimEnterpriseFilterExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+}
