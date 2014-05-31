b1dc91d50694379fd069569e275f55bb5521ebaf
Gildas Quéméner <gildas.quemener@gmail.com>
2014-05-31 17:34:51 +0200

    PIM-2620 Introduced the option comparator

---
+++ src/PimEnterprise/Bundle/WorkflowBundle/Form/Comparator/OptionComparator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PimEnterprise\Bundle\WorkflowBundle\Form\Comparator;
+
+use Pim\Bundle\CatalogBundle\Model\AbstractProductValue;
+
+/**
+ *
+ * @author    Gildas Quemener <gildas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class OptionComparator implements ComparatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsComparison(AbstractProductValue $value)
+    {
+        return 'pim_catalog_simpleselect' === $value->getAttribute()->getAttributeType();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getChanges(AbstractProductValue $value, $submittedData)
+    {
+        $option = $value->getOption();
+
+        if (!$option || $option->getId() != $submittedData['option']) {
+            return [
+                'id' => $submittedData['id'],
+                'option' => $submittedData['option'],
+            ];
+        }
+    }
+}
