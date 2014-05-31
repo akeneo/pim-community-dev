e6bfd4393e824e09a668f3c44ae9d6939399baaa
Filips Alpe <filips@akeneo.com>
2014-05-31 15:04:41 +0200

    Fix style

--- src/Pim/Bundle/UIBundle/Form/Transformer/IntegerTransformer.php
+++ src/Pim/Bundle/UIBundle/Form/Transformer/IntegerTransformer.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\DataTransformerInterface;
 
 /**
  * Transforms numbers into integers
- * 
+ *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
