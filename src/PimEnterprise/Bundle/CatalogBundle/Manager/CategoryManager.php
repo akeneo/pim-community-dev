a0c5da3ae37ad26de74925c55c4af409ec60c791
jjanvier <j.janvier@gmail.com>
2014-06-02 18:13:30 +0200

    PIM-2626: Display only accessible trees in the product grid

---
+++ src/PimEnterprise/Bundle/CatalogBundle/Manager/CategoryManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PimEnterprise\Bundle\CatalogBundle\Manager;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+use Pim\Bundle\CatalogBundle\Manager\CategoryManager as BaseCategoryManager;
+use PimEnterprise\Bundle\SecurityBundle\Voter\CategoryVoter;
+
+/**
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class CategoryManager extends BaseCategoryManager
+{
+    /**
+     * @var SecurityContextInterface
+     */
+    protected $securityContext;
+
+    /**
+     * Constructor
+     *
+     * @param SecurityContextInterface  $securityContext
+     * @param ObjectManager             $om
+     * @param string                    $categoryClass
+     */
+    public function __construct(SecurityContextInterface $securityContext, ObjectManager $om, $categoryClass)
+    {
+        parent::__construct($om, $categoryClass);
+
+        $this->securityContext = $securityContext;
+    }
+
+    public function getAccessibleTrees()
+    {
+        $trees = array();
+
+        foreach ($this->getTrees() as $tree) {
+            if ($this->securityContext->isGranted(CategoryVoter::VIEW_PRODUCTS, $tree)) {
+                $trees[] = $tree;
+            }
+        }
+
+        return $trees;
+    }
+} 
---
+++ src/PimEnterprise/Bundle/EnrichBundle/Controller/CategoryTreeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace PimEnterprise\Bundle\EnrichBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use Pim\Bundle\EnrichBundle\Controller\CategoryTreeController as BaseCategoryTreeController;
+use PimEnterprise\Bundle\CatalogBundle\Manager\CategoryManager;
+use PimEnterprise\Bundle\UserBundle\Context\UserContext;
+use PimEnterprise\Bundle\SecurityBundle\Voter\CategoryVoter;
+
+/**
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class CategoryTreeController extends BaseCategoryTreeController
+{
+    /**
+     * @var UserContext
+     */
+    protected $userContext;
+
+    /**
+     * @var CategoryManager
+     */
+    protected $categoryManager;
+
+    /**
+     * Find a category from its id
+     *
+     * @param integer $categoryId
+     *
+     * @return CategoryInterface
+     * @throws NotFoundHttpException
+     * @throws AccessDeniedException
+     */
+    protected function findAccessibleCategory($categoryId)
+    {
+        $category = $this->findCategory($categoryId);
+
+        if (false === $this->securityContext->isGranted(CategoryVoter::VIEW_PRODUCTS, $category)) {
+            throw new AccessDeniedException('You can not access this category');
+        }
+
+        return $category;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @Template("PimEnrichBundle:CategoryTree:listTree.json.twig")
+     * @AclAncestor("pim_enrich_category_list")
+     */
+    public function listTreeAction(Request $request)
+    {
+        $selectNodeId = $request->get('select_node_id', -1);
+        try {
+            $selectNode = $this->findAccessibleCategory($selectNodeId);
+        } catch (NotFoundHttpException $e) {
+            $selectNode = $this->userContext->getAccessibleUserTree();
+        } catch (AccessDeniedException $e) {
+            $selectNode = $this->userContext->getAccessibleUserTree();
+        }
+
+        return array(
+            'trees'          => $this->categoryManager->getAccessibleTrees(),
+            'selectedTreeId' => $selectNode->isRoot() ? $selectNode->getId() : $selectNode->getRoot(),
+            'include_sub'    => (bool) $this->getRequest()->get('include_sub', false),
+        );
+    }
+} 
