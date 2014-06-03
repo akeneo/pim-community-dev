6e6731a2baffc97e81022e6b256ff4f925f5e9bd
jjanvier <j.janvier@gmail.com>
2014-06-03 11:36:29 +0200

    PIM-2626: Display only accessible trees in the product edit view

---
+++ src/PimEnterprise/Bundle/CatalogBundle/Manager/ProductCategoryManager.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PimEnterprise\Bundle\CatalogBundle\Manager;
+
+use Pim\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use Pim\Bundle\CatalogBundle\Model\ProductInterface;
+use Pim\Bundle\CatalogBundle\Manager\ProductCategoryManager as BaseProductCategoryManager;
+use Pim\Bundle\CatalogBundle\Repository\ProductCategoryRepositoryInterface;
+use PimEnterprise\Bundle\SecurityBundle\Voter\CategoryVoter;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+/**
+ * Product category manager
+ *
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class ProductCategoryManager extends BaseProductCategoryManager
+{
+    /**
+     * @var SecurityContextInterface
+     */
+    protected $securityContext;
+
+    /**
+     * Constructor
+     *
+     * @param SecurityContextInterface           $securityContext   Security context
+     * @param ProductCategoryRepositoryInterface $productRepo       Product repository
+     * @param CategoryRepository                 $categoryRepo      Category repository
+     */
+    public function __construct(
+        SecurityContextInterface $securityContext,
+        ProductCategoryRepositoryInterface $productRepo,
+        CategoryRepository $categoryRepo
+    ) {
+        parent::__construct($productRepo, $categoryRepo);
+
+        $this->securityContext = $securityContext;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProductCountByTree(ProductInterface $product)
+    {
+        $trees =  $this->productRepository->getProductCountByTree($product);
+
+        foreach ($trees as $key => $tree) {
+            if (false === $this->securityContext->isGranted(CategoryVoter::VIEW_PRODUCTS, $tree['tree'])) {
+                unset($trees[$key]);
+            }
+        }
+
+        return $trees;
+    }
+} 
