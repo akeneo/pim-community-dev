33d86f1187f13fa1c1cb837983101541073eea93
Romain Monceau <romain@akeneo.com>
2014-06-09 15:22:35 +0200

    PIM-2677: Create repository for job profile access

---
+++ src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/JobProfileAccessRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PimEnterprise\Bundle\SecurityBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Job profile access repository
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class JobProfileAccessRepository extends EntityRepository
+{
+}
