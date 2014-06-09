7892e26d8396532fa97e4c5bcf4f7d194a501d71
Romain Monceau <romain@akeneo.com>
2014-06-09 17:42:59 +0200

    PIM-2677: Implements voter for job profiles

--- src/PimEnterprise/Bundle/SecurityBundle/Voter/JobProfileVoter.php
+++ src/PimEnterprise/Bundle/SecurityBundle/Voter/JobProfileVoter.php
@@ -2,9 +2,10 @@
 
 namespace PimEnterprise\Bundle\SecurityBundle\Voter;
 
-use PimEnterprise\Bundle\SecurityBundle\Manager\JobProfileAccessManager;
-
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Akeneo\Bundle\BatchBundle\Entity\JobInstance;
+use PimEnterprise\Bundle\SecurityBundle\Manager\JobProfileAccessManager;
 
 /**
  * Job profile voter, allows to know if a job profile can be executed or edited by
@@ -23,4 +24,74 @@ class JobProfileVoter implements VoterInterface
 
     /** @var JobProfileAccessManager */
     protected $accessManager;
+
+    /**
+     * Constructor
+     *
+     * @param JobProfileAccessManager $accessManager
+     */
+    public function __construct(JobProfileAccessManager $accessManager)
+    {
+        $this->accessManager = $accessManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsAttribute($attribute)
+    {
+        return in_array($attribute, array(self::EXECUTE_JOB_PROFILE, self::EDIT_JOB_PROFILE));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsClass($class)
+    {
+        return $class instanceof JobInstance;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        $result = VoterInterface::ACCESS_ABSTAIN;
+
+        if ($this->supportsClass($object)) {
+            foreach ($attributes as $attribute) {
+                if ($this->supportsAttribute($attribute)) {
+                    $result = VoterInterface::ACCESS_DENIED;
+                    $grantedRoles = $this->extractRoles($attribute, $object);
+                }
+
+                foreach ($grantedRoles as $role) {
+                    if ($token->getUser()->hasRole($role)) {
+                        return VoterInterface::ACCESS_GRANTED;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get roles for specific job profile
+     *
+     * @param string      $attribute
+     * @param JobInstance $object
+     *
+     * @return Role[]
+     */
+    protected function extractRoles($attribute, $object)
+    {
+        if ($attribute === self::EDIT_JOB_PROFILE) {
+            $grantedRoles = $this->accessManager->getEditRoles($object);
+        } else {
+            $grantedRoles = $this->accessManager->getExecuteRoles($object);
+        }
+
+        return $grantedRoles;
+    }
 }
