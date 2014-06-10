a7b6209f5c40ac2ef11658ab832567a888b01985
Filips Alpe <filips@akeneo.com>
2014-06-10 11:28:30 +0300

    Improve the select2 choice filter frontend behavior

--- src/Pim/Bundle/FilterBundle/Resources/public/js/datafilter/filter/select2-choice-filter.js
+++ src/Pim/Bundle/FilterBundle/Resources/public/js/datafilter/filter/select2-choice-filter.js
@@ -1,43 +1,120 @@
-define(['jquery', 'underscore', 'oro/translator', 'oro/app', 'oro/datafilter/text-filter', 'routing', 'pim/initselect2', 'jquery.select2'],
-function($, _, __, app, TextFilter, Routing, initSelect2) {
-    'use strict';
+define(
+    ['jquery', 'underscore', 'oro/datafilter/text-filter', 'routing', 'jquery.select2'],
+    function($, _, TextFilter, Routing) {
+        'use strict';
 
-    return TextFilter.extend({
-        choiceUrl: null,
-        choiceUrlParams: {},
+        return TextFilter.extend({
+            choiceUrl: null,
+            choiceUrlParams: {},
+            resultCache: {},
 
-        popupCriteriaTemplate: _.template(
-            '<div>' +
-                '<div class="btn-group">' +
-                    '<input type="text" name="value" value="" data-multiple="true" data-min-input-length="1" />' +
-                '</div>' +
-                '<div class="btn-group">' +
-                    '<button type="button" class="btn btn-primary filter-update"><%- _.__("Update") %></button>' +
-                '</div>' +
-            '</div>'
-        ),
+            popupCriteriaTemplate: _.template(
+                '<div class="choicefilter">' +
+                    '<div class="input-prepend">' +
+                        '<div class="btn-group">' +
+                            '<input type="text" name="value" value=""/>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="btn-group">' +
+                        '<button type="button" class="btn btn-primary filter-update"><%- _.__("Update") %></button>' +
+                    '</div>' +
+                '</div>'
+            ),
 
-        initialize: function(options) {
-            options = options || {};
-            if (_.has(options, 'choiceUrl')) {
-                this.choiceUrl = options.choiceUrl;
+            initialize: function(options) {
+                options = options || {};
+                if (_.has(options, 'choiceUrl')) {
+                    this.choiceUrl = options.choiceUrl;
+                }
+                if (_.has(options, 'choiceUrlParams')) {
+                    this.choiceUrlParams = options.choiceUrlParams;
+                }
+
+                TextFilter.prototype.initialize.apply(this, arguments);
+            },
+
+            _renderCriteria: function() {
+                TextFilter.prototype._renderCriteria.apply(this, arguments);
+
+                var self = this;
+                var $select = this.$(this.criteriaValueSelectors.value);
+
+                var options = {
+                    multiple: true,
+                    allowClear: false,
+                    width: '290px',
+                    minimumInputLength: 1
+                };
+                options.ajax = {
+                    url: Routing.generate(this.choiceUrl, this.choiceUrlParams),
+                    cache: true,
+                    data: function(term) {
+                        return {
+                            search: term
+                        };
+                    },
+                    results: function(data) {
+                        self._cacheResults(data.results);
+                        return data;
+                    }
+                };
+                $select.select2(options);
+            },
+
+            _cacheResults: function (results) {
+                _.each(results, function (result) {
+                    this.resultCache[result.id] = result.text;
+                }, this);
+            },
+
+            _getCachedResults: function(ids) {
+                var results = [],
+                    missingResults = [];
+
+                _.each(ids, function(id) {
+                    if (_.isUndefined(this.resultCache[id])) {
+                        missingResults.push(id);
+                    } else {
+                        results.push({ id: id, text: this.resultCache[id] });
+                    }
+                }, this);
+
+                _.each(missingResults, function(id) {
+                    // TODO: get the label from the server
+                    results.push({ id: id, text: id });
+                });
+
+                return results;
+            },
+
+            _getInputValue: function(input) {
+                return this.$(input).select2('val');
+            },
+
+            _setInputValue: function(input, value) {
+                this.$(input).select2('data', this._getCachedResults(value));
+
+                return this;
+            },
+
+            _updateDOMValue: function() {
+                return this._writeDOMValue(this.getValue());
+            },
+
+            _formatDisplayValue: function(value) {
+                if (_.isEmpty(value.value)) {
+                    return value;
+                }
+
+                return {
+                    value: _.pluck(this._getCachedResults(value.value), 'text').join(', ')
+                };
+            },
+
+            _getCriteriaHint: function() {
+                var value = (arguments.length > 0) ? this._getDisplayValue(arguments[0]) : this._getDisplayValue();
+                return !_.isEmpty(value.value) ? value.value : this.placeholder;
             }
-            if (_.has(options, 'choiceUrlParams')) {
-                this.choiceUrlParams = options.choiceUrlParams;
-            }
-
-            TextFilter.prototype.initialize.apply(this, arguments);
-        },
-
-        _renderCriteria: function() {
-            TextFilter.prototype._renderCriteria.apply(this, arguments);
-
-            var $select = this.$(this.criteriaValueSelectors.value);
-            $select.attr({ 'data-url': Routing.generate(this.choiceUrl, this.choiceUrlParams) }).css('width', '290px');
-
-            initSelect2.initSelect($select);
-
-            return this;
-        }
-    });
-});
+        });
+    }
+);
