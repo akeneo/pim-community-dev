2c57a66e504cec57ed5c49225a2c25660de4f2e8
jjanvier <j.janvier@gmail.com>
2014-06-13 12:05:36 +0200

    Creation of the PimEntityBundle that overrides OroEntityBundle

--- src/Pim/Bundle/EntityBundle/DependencyInjection/PimEntityExtension.php
+++ src/Pim/Bundle/EntityBundle/DependencyInjection/PimEntityExtension.php
@@ -1,25 +1,24 @@
 <?php
 
-namespace Pim\Bundle\NavigationBundle\DependencyInjection;
+namespace Pim\Bundle\EntityBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- *
- * @author    Romain Monceau <romain@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PimNavigationExtension extends Extension
+class PimEntityExtension extends Extension
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
