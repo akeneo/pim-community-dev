51aaddd0b31f66ffe9796c696c878955f5adf799
jjanvier <j.janvier@gmail.com>
2014-06-13 12:27:11 +0200

    Override EntityClassResolver to use the smart manager registry

--- src/Pim/Bundle/EntityBundle/DependencyInjection/PimEntityExtension.php
+++ src/Pim/Bundle/EntityBundle/DependencyInjection/PimEntityExtension.php
@@ -23,6 +23,6 @@ class PimEntityExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('orm.yml');
     }
 }
