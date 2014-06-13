77d2a4a51689170d39abe4f02d4d1814406a9d90
Gildas Quéméner <gildas.quemener@gmail.com>
2014-06-13 15:40:46 +0200

    PIM-2705 Added the modified icon in the form theme

---
+++ src/PimEnterprise/Bundle/UIBundle/PimEnterpriseUIBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PimEnterprise\Bundle\UIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PIM Enterprise UI Bundle
+ *
+ * @author    Gildas Quemener <gildas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class PimEnterpriseUIBundle extends Bundle
+{
+}
