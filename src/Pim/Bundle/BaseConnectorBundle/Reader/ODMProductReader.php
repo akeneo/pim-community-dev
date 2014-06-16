25bdc9bf908f67fdbde8cdb0facc85a2f66eeab1
Olivier Soulet <olivier.soulet@akeneo.com>
2014-06-16 17:51:13 +0200

    Added a product reader for ODM

---
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/ODMProductReader.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Pim\Bundle\BaseConnectorBundle\Reader;
+
+use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
+use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
+use Akeneo\Bundle\BatchBundle\Item\ItemReaderInterface;
+use Pim\Bundle\CatalogBundle\Repository\ProductRepositoryInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Pim\Bundle\TransformBundle\Converter\MetricConverter;
+use Pim\Bundle\BaseConnectorBundle\Validator\Constraints\Channel as ChannelConstraint;
+use Pim\Bundle\CatalogBundle\Manager\ChannelManager;
+use Pim\Bundle\CatalogBundle\Manager\CompletenessManager;
+
+/**
+ * Reads products for Mongodb
+ *
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ODMProductReader extends AbstractConfigurableStepElement implements ItemReaderInterface
+{
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(groups={"Execution"})
+     * @ChannelConstraint
+     */
+    protected $channel;
+
+    /**
+     * @var StepExecution
+     */
+    protected $stepExecution;
+
+    /**
+     * @var ChannelManager
+     */
+    protected $channelManager;
+
+    /**
+     * @var AbstractQuery
+     */
+    protected $query;
+
+    /**
+     * @var Cursor
+     */
+    protected $products;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * @var CompletenessManager
+     */
+    protected $completenessManager;
+
+    /**
+     * @var MetricConverter
+     */
+    protected $metricConverter;
+
+    /**
+     * @var boolean
+     */
+    protected $executed = false;
+
+    /**
+     * @param ProductRepositoryInterface $repository
+     * @param ChannelManager             $channelManager
+     * @param CompletenessManager        $completenessManager
+     * @param MetricConverter            $metricConverter
+     */
+    public function __construct(
+        ProductRepositoryInterface $repository,
+        ChannelManager $channelManager,
+        CompletenessManager $completenessManager,
+        MetricConverter $metricConverter
+    ) {
+        $this->repository          = $repository;
+        $this->channelManager      = $channelManager;
+        $this->completenessManager = $completenessManager;
+        $this->metricConverter     = $metricConverter;
+    }
+
+    /**
+     * Get query to execute
+     *
+     * @return Doctrine\ORM\AbstractQuery
+     *
+     * @throws ORMReaderException
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read()
+    {
+        if (!$this->executed) {
+            $this->executed = true;
+            if (!is_object($this->channel)) {
+                $this->channel = $this->channelManager->getChannelByCode($this->channel);
+            }
+
+            $this->completenessManager->generateMissingForChannel($this->channel);
+
+            $this->query = $this->repository
+                ->buildByChannelAndCompleteness($this->channel)
+                ->getQuery();
+
+            $this->products = $this->getQuery()->execute();
+
+            // MongoDB Cursor are not positioned on first element (whereas ArrayIterator is)
+            // as long as getNext() hasn't be called
+            $this->products->getNext();
+        }
+
+        $result = $this->products->current();
+
+        if ($result) {
+            $this->products->next();
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigurationFields()
+    {
+        return array(
+            'channel' => array(
+                'type'    => 'choice',
+                'options' => array(
+                    'choices'  => $this->channelManager->getChannelChoices(),
+                    'required' => true,
+                    'select2'  => true,
+                    'label'    => 'pim_base_connector.export.channel.label',
+                    'help'     => 'pim_base_connector.export.channel.help'
+                )
+            )
+        );
+    }
+
+    /**
+     * Set channel
+     *
+     * @param string $channel
+     */
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+    }
+
+    /**
+     * Get channel
+     *
+     * @return string
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+}
