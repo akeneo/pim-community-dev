7cf19d10f4fd98de2fb0b50a00f3ac2b3e42de79
Gildas Quéméner <gildas.quemener@gmail.com>
2014-06-16 18:33:33 +0200

    PIM-2685 Unmap product values before submission

--- src/PimEnterprise/Bundle/WorkflowBundle/Form/Subscriber/CollectProductValuesSubscriber.php
+++ src/PimEnterprise/Bundle/WorkflowBundle/Form/Subscriber/CollectProductValuesSubscriber.php
@@ -8,21 +8,30 @@ use Symfony\Component\Form\FormEvent;
 use PimEnterprise\Bundle\WorkflowBundle\Form\Comparator\ComparatorInterface;
 
 /**
- * PimEnterprise\Bundle\WorkflowBundle\Form\Subscriber
+ * A collector of changes that a client is sending to a product edit form
  *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  */
 class CollectProductValuesSubscriber implements EventSubscriberInterface
 {
+    /** @var ComparatorInterface */
     protected $comparator;
+
+    /** @var array */
     private $changes = [];
 
+    /**
+     * @param ComparatorInterface $comparator
+     */
     public function __construct(ComparatorInterface $comparator)
     {
         $this->comparator = $comparator;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public static function getSubscribedEvents()
     {
         return [
@@ -30,35 +39,37 @@ class CollectProductValuesSubscriber implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * Collect changes that client sent to the product values
+     *
+     * @param FormEvent $event
+     */
     public function collect(FormEvent $event)
     {
         $form = $event->getForm();
-        if ('pim_product_edit' !== $form->getName()) {
+        $data = $event->getData();
+        if ('pim_product_edit' !== $form->getName() || !array_key_exists('values', $data)) {
             return;
         }
 
-        $currentValues = $form->get('values')->getViewData();
-        $data = $event->getData();
         foreach ($data['values'] as $key => $value) {
+            // TODO (2014-06-16 15:45 by Gildas): Values that are sent and not available in the product
+            // values should be stored in the changeset, shouldn't they?
             if ($currentValues->containsKey($key)) {
                 if (null !== $changes = $this->comparator->getChanges($currentValues->get($key), $value)) {
                     $this->changes['values'][$key] = $changes;
                 }
             }
         }
-
-        $this->removeValuesFromFormData($event);
     }
 
+    /**
+     * Get the collected changes sent to the product values
+     *
+     * @return array
+     */
     public function getChanges()
     {
         return $this->changes;
-    }
-
-    protected function removeValuesFromFormData(FormEvent $event)
-    {
-        $data = $event->getData();
-        unset($data['values']);
-        $event->setData($data);
     }
 }
