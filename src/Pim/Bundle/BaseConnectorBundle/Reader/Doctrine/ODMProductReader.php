6d4b848da3b1723349835ae53206ce219c02c60d
Olivier Soulet <olivier.soulet@akeneo.com>
2014-06-17 11:37:40 +0200

    Refactor of reader classes

--- src/Pim/Bundle/BaseConnectorBundle/Reader/Doctrine/ODMProductReader.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/Doctrine/ODMProductReader.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\BaseConnectorBundle\Reader;
+namespace Pim\Bundle\BaseConnectorBundle\Reader\Doctrine;
 
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
