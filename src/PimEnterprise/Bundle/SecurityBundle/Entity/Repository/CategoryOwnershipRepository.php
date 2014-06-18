b866a804a4a4b2d75ca69f442d08610b2b09708a
Filips Alpe <filips@akeneo.com>
2014-06-18 12:25:08 +0300

    Add mapping and repository for CategoryOwnership entity

---
+++ src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/CategoryOwnershipRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PimEnterprise\Bundle\SecurityBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Category ownership repository
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class CategoryOwnershipRepository extends EntityRepository
+{
+}
