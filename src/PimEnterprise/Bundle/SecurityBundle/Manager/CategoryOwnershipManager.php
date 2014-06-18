a4a66ebafaf544129e7a85974808d41b49ed703e
Filips Alpe <filips@akeneo.com>
2014-06-18 12:26:14 +0300

    Create a manager for granting category ownership

---
+++ src/PimEnterprise/Bundle/SecurityBundle/Manager/CategoryOwnershipManager.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace PimEnterprise\Bundle\SecurityBundle\Manager;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\UserBundle\Entity\Role;
+use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
+use PimEnterprise\Bundle\SecurityBundle\Entity\Repository\CategoryOwnershipRepository;
+use PimEnterprise\Bundle\SecurityBundle\Entity\CategoryOwnership;
+
+/**
+ * Category ownership manager
+  *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class CategoryOwnershipManager
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * Constructor
+     *
+     * @param ObjectManager $manager
+     */
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * Grant ownership to a role for the specified categories
+     *
+     * @param Role              $role
+     * @param CategoryInterface $category
+     */
+    public function grantOwnership(Role $role, CategoryInterface $category)
+    {
+        $ownership = $this->findOwnership($role, $category);
+
+        if (!$ownership) {
+            $ownership = new CategoryOwnership();
+            $ownership->setRole($role)->setCategory($category);
+            $this->manager->persist($ownership);
+        }
+    }
+
+    /**
+     * Revoke ownership to a role for the specified categories
+     *
+     * @param Role              $role
+     * @param CategoryInterface $category
+     */
+    public function revokeOwnership(Role $role, CategoryInterface $category)
+    {
+        $ownership = $this->findOwnership($role, $category);
+
+        if ($ownership) {
+            $this->manager->remove($ownership);
+        }
+    }
+
+    /**
+     * Find ownership for a role and category
+     *
+     * @param Role              $role
+     * @param CategoryInterface $category
+     *
+     * @return CategoryOwnership|null
+     */
+    protected function findOwnership(Role $role, CategoryInterface $category)
+    {
+        return $this->getRepository()->findOneBy(['role' => $role, 'category' => $category]);
+    }
+
+    /**
+     * Get repository
+     *
+     * @return CategoryOwnershipRepository
+     */
+    protected function getRepository()
+    {
+        return $this->manager->getRepository('PimEnterpriseSecurityBundle:CategoryOwnership');
+    }
+}
