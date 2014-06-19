701d97dea236ef1e19db01b47da9250e2c7d43e4
Romain Monceau <romain@akeneo.com>
2014-06-19 12:26:27 +0200

    PIM-2696: Define widget repository as deprecated

--- src/Pim/Bundle/DashboardBundle/Entity/Repository/WidgetRepository.php
+++ src/Pim/Bundle/DashboardBundle/Entity/Repository/WidgetRepository.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\EntityManager;
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @deprecated since 1.2
  */
 class WidgetRepository
 {
