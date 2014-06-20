dfc8078cab5c5bdaa77a008cd4a799f23e258178
sumbobyboys <sumbobyboys@gmail.com>
2014-06-20 15:48:06 +0200

    add support for channel (partial), attribute and families

--- src/Pim/Bundle/CatalogBundle/EventListener/MongoDBODM/UpdateNormalizedProductDataSubscriber.php
+++ src/Pim/Bundle/CatalogBundle/EventListener/MongoDBODM/UpdateNormalizedProductDataSubscriber.php
@@ -3,10 +3,12 @@
 namespace Pim\Bundle\CatalogBundle\EventListener\MongoDBODM;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PostFlushEventArgs;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
 
 /**
  * Sets the normalized data of a Product document when related entities are modified
@@ -20,9 +22,6 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
     /** @var ManagerRegistry */
     protected $registry;
 
-    /** @var NormalizerInterface */
-    protected $normalizer;
-
     /** @var string */
     protected $productClass;
 
@@ -31,6 +30,15 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
 
     /** @var string */
     protected $familyTranslationClass;
+
+    /** @var string */
+    protected $channelClass;
+
+    /** @var string */
+    protected $attributeOptionClass;
+
+    /** @var string */
+    protected $attributeOptionValueClass;
 
     /**
      * Scheduled queries to apply
@@ -41,23 +49,30 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
 
     /**
      * @param ManagerRegistry     $registry
-     * @param NormalizerInterface $normalizer
      * @param string              $productClass
      * @param string              $familyClass
      * @param string              $familyTranslationClass
+     * @param string              $channelClass
+     * @param string              $attributeOptionClass
+     * @param string              $attributeOptionValueClass
      */
     public function __construct(
         ManagerRegistry $registry,
-        NormalizerInterface $normalizer,
+        EntityManager $entityManager,
         $productClass,
         $familyClass,
-        $familyTranslationClass
+        $familyTranslationClass,
+        $channelClass,
+        $attributeOptionClass,
+        $attributeOptionValueClass
     ) {
-        $this->registry               = $registry;
-        $this->normalizer             = $normalizer;
-        $this->productClass           = $productClass;
-        $this->familyClass            = $familyClass;
-        $this->familyTranslationClass = $familyTranslationClass;
+        $this->registry                  = $registry;
+        $this->productClass              = $productClass;
+        $this->familyClass               = $familyClass;
+        $this->familyTranslationClass    = $familyTranslationClass;
+        $this->channelClass              = $channelClass;
+        $this->attributeOptionClass      = $attributeOptionClass;
+        $this->attributeOptionValueClass = $attributeOptionValueClass;
     }
 
     /**
@@ -76,19 +91,19 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
         $uow = $args->getEntityManager()->getUnitOfWork();
 
         foreach ($uow->getScheduledEntityUpdates() as $entity) {
-            $this->scheduleQueries($entity, $uow->getEntityChangeSet($entity));
+            $this->scheduleQueriesAfterUpdate($entity, $uow->getEntityChangeSet($entity));
         }
 
         foreach ($uow->getScheduledEntityDeletions() as $entity) {
-            $this->scheduleQueries($entity, $uow->getEntityChangeSet($entity));
+            $this->scheduleQueriesAfterDelete($entity);
         }
 
         foreach ($uow->getScheduledCollectionDeletions() as $entity) {
-            $this->scheduleQueries($entity, $uow->getEntityChangeSet($entity));
+            $this->scheduleQueriesAfterDelete($entity);
         }
 
         foreach ($uow->getScheduledCollectionUpdates() as $entity) {
-            $this->scheduleQueries($entity, $uow->getEntityChangeSet($entity));
+            $this->scheduleQueriesAfterUpdate($entity, $uow->getEntityChangeSet($entity));
         }
     }
 
@@ -105,20 +120,45 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
      *
      * @param object $entity
      */
-    protected function scheduleQueries($entity, array $changes)
+    protected function scheduleQueriesAfterUpdate($entity, array $changes)
     {
         foreach ($changes as $field => $values) {
             list($oldValue, $newValue) = $values;
 
-            $query = $this->generateQuery($entity, $field, $oldValue, $newValue);
+            $queries = $this->generateQuery($entity, $field, $oldValue, $newValue);
 
-            if (null !== $query) {
-                $this->scheduledQueries[] = $this->generateQuery($entity, $field, $oldValue, $newValue);
+            if (null !== $queries) {
+                $this->scheduledQueries = array_merge(
+                    $this->scheduledQueries,
+                    $queries
+                );
             }
         }
     }
 
-    protected function generateQuery($entity, $field, $oldValue, $newValue)
+    /**
+     * Schedule products related to the entity for normalized data recalculation
+     *
+     * @param object $entity
+     */
+    protected function scheduleQueriesAfterDelete($entity)
+    {
+        $queries = $this->generateQuery($entity);
+
+        if (null !== $queries) {
+            $this->scheduledQueries = array_merge(
+                $this->scheduledQueries,
+                $queries
+            );
+        }
+    }
+
+    /**
+     * Get generators for the given entity and updated field
+     *
+     * @return array|null
+     */
+    protected function generateQuery($entity, $field = '', $oldValue = '', $newValue = '')
     {
         $generator = $this->getGenerator($entity, $field);
 
@@ -129,6 +169,11 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
         }
     }
 
+    /**
+     * Get generators for the given entity and updated field
+     *
+     * @return array|null
+     */
     protected function getGenerator($entity, $field) {
         foreach ($this->getQueriesGenerators() as $queriesGenerator) {
             if ($entity instanceof $queriesGenerator['class'] &&
@@ -140,6 +185,11 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
         return null;
     }
 
+    /**
+     * Get queries generators
+     *
+     * @return array
+     */
     protected function getQueriesGenerators()
     {
         return [
@@ -147,37 +197,132 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
                 'class'     => $this->familyClass,
                 'field'     => 'attributeAsLabel',
                 'generator' => function($entity, $field, $oldValue, $newValue) {
-                    return [
-                        [
-                            'family' => $entity->getId()
-                        ],
-                        [
-                            'normalizedData.family.attributeAsLabel' => (string) $newValue
-                        ],
-                        [
-                            'multi' => true
-                        ]
-                    ];
+                    return [[
+                        ['family' => $entity->getId()],
+                        ['$set' => ['normalizedData.family.attributeAsLabel' => (string) $newValue]],
+                        ['multi' => true]
+                    ]];
                 }
             ],
             [
                 'class'     => $this->familyTranslationClass,
                 'field'     => 'label',
                 'generator' => function($entity, $field, $oldValue, $newValue) {
-                    return [
+                    return [[
+                        ['family' => $entity->getId()],
                         [
-                            'family' => $entity->getId()
+                            '$set' => [
+                                sprintf('normalizedData.family.label.%s', $entity->getLocale()) => (string) $newValue
+                            ]
+                        ],
+                        ['multi' => true]
+                    ]];
+                }
+            ],
+            [
+                'class'     => $this->channelClass,
+                'field'     => '',
+                'generator' => function($entity, $field, $oldValue, $newValue) {
+                    $attributes = $this->getScopableAttributes();
+
+                    $queries = [];
+
+                    foreach ($attributes as $attribute) {
+                        $attributesToRemove = [];
+
+                        if ($attribute->isLocalizable()) {
+                            foreach ($entity->getLocales() as $locale) {
+                                 $attributesToRemove[] = sprintf(
+                                    'normalizedData.%s-%s-%s',
+                                    $attribute->getCode(),
+                                    $locale->getCode(),
+                                    $entity->getCode()
+                                );
+                            }
+                        } else {
+                            $attributesToRemove[] = sprintf(
+                                'normalizedData.%s-%s',
+                                $attribute->getCode(),
+                                $entity->getCode()
+                            );
+                        }
+
+                        $queries[] = [
+                            [sprintf('normalizedData.%s', $attribute->getCode()) => [ '$exists' => true ]],
+                            ['$unset' => $this->getAttributesToRemove()],
+                            ['multi' => true]
+                        ];
+                    }
+
+                    return [];
+                }
+            ],
+            [
+                'class'     => $this->attributeOptionClass,
+                'field'     => '',
+                'generator' => function($entity, $field, $oldValue, $newValue) {
+                    return [[
+                        [sprintf('normalizedData.%s', $entity->getAttribute()->getCode()) => [ '$exists' => true ]],
+                        ['$unset' => [sprintf('normalizedData.%s', $entity->getCode())]],
+                        ['multi' => true]
+                    ]];
+                }
+            ],
+            [
+                'class'     => $this->attributeOptionClass,
+                'field'     => 'code',
+                'generator' => function($entity, $field, $oldValue, $newValue) {
+                    return [[
+                        [sprintf('normalizedData.%s', $entity->getAttribute()->getCode()) => [ '$exists' => true ]],
+                        [
+                            '$set' => [
+                                sprintf('normalizedData.%s.code', $entity->getAttribute()->getCode()) => $newValue
+                            ]
+                        ],
+                        ['multi' => true]
+                    ]];
+                }
+            ],
+            [
+                'class'     => $this->attributeOptionValueClass,
+                'field'     => 'code',
+                'generator' => function($entity, $field, $oldValue, $newValue) {
+                    return [[
+                        [
+                            sprintf(
+                                'normalizedData.%s',
+                                $entity->getOption()->getAttribute()->getCode()
+                            ) => [ '$exists' => true ]
                         ],
                         [
-                            'normalizedData.family.label.' . $entity->getLocale() => (string) $newValue
+                            '$set' => [
+                                sprintf(
+                                    'normalizedData.%s.code.optionValues%s.value',
+                                    $entity->getOption()->getAttribute()->getCode(),
+                                    $entity->getLocale()
+                                ) => $newValue
+                            ]
                         ],
-                        [
-                            'multi' => true
-                        ]
-                    ];
+                        ['multi' => true]
+                    ]];
                 }
             ]
         ];
+    }
+
+    /**
+     * Get scopable attributes
+     *
+     * @return array
+     */
+    protected function getScopableAttributes()
+    {
+        $attributeManager = $this->registry->getManagerForClass($this->attributeClass);
+        $attributeRepository = $attributeManager->getRepository($this->attributeClass);
+
+        $attributes = $attributeRepository->findBy(['scopable' => true]);
+
+        return $attributes;
     }
 
     /**
@@ -185,23 +330,6 @@ class UpdateNormalizedProductDataSubscriber implements EventSubscriber
      */
     protected function executeQueries()
     {
-        $manager = $this->registry->getManagerForClass($this->productClass);
-
-        foreach ($this->pendingProducts as $productId) {
-            $product = $manager->getRepository($this->productClass)->find($productId);
-            if ($product) {
-                $product->setNormalizedData($this->normalizer->normalize($product, 'mongodb_json'));
-                $manager->persist($product);
-
-                $this->updatedProducts[] = $productId;
-            }
-        }
-
-        $this->pendingProducts = array();
-
-        $updatedCount = count($this->updatedProducts);
-        if ($updatedCount) {
-            $manager->flush();
-        }
+        error_log(print_r($this->scheduledQueries, true));
     }
 }
