817768453d850e498dfa27788d0aab89a796cfb3
Gildas Quéméner <gildas.quemener@gmail.com>
2014-06-24 18:24:56 +0200

    Fixed specs

--- spec/PimEnterprise/Bundle/WorkflowBundle/Form/Comparator/MetricComparatorSpec.php
+++ spec/PimEnterprise/Bundle/WorkflowBundle/Form/Comparator/MetricComparatorSpec.php
@@ -15,6 +15,7 @@ class MetricComparatorSpec extends ObjectBehavior
         $value->getAttribute()->willReturn($attribute);
         $value->getId()->willReturn(713705);
         $value->getScope()->willReturn('ecommerce');
+        $value->getLocale()->willReturn('fr_FR');
         $attribute->getId()->willReturn(1337);
     }
 
@@ -58,6 +59,7 @@ class MetricComparatorSpec extends ObjectBehavior
                 'attribute_id' => 1337,
                 'value_id' => 713705,
                 'scope' => 'ecommerce',
+                'locale' => 'fr_FR',
             ],
         ]);
     }
@@ -87,6 +89,7 @@ class MetricComparatorSpec extends ObjectBehavior
                 'attribute_id' => 1337,
                 'value_id' => 713705,
                 'scope' => 'ecommerce',
+                'locale' => 'fr_FR',
             ],
         ]);
     }
@@ -135,6 +138,7 @@ class MetricComparatorSpec extends ObjectBehavior
                 'attribute_id' => 1337,
                 'value_id' => 713705,
                 'scope' => 'ecommerce',
+                'locale' => 'fr_FR',
             ],
         ]);
     }
@@ -180,6 +184,7 @@ class MetricComparatorSpec extends ObjectBehavior
                 'attribute_id' => 1337,
                 'value_id' => 713705,
                 'scope' => 'ecommerce',
+                'locale' => 'fr_FR',
             ],
         ]);
     }
