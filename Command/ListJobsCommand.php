d42500b925d59affec0e593c69bb204a89f461f4
Antoine Guigan <antoine@akeneo.com>
2014-06-24 11:16:05 +0200

    Fixed PHPDoc

--- Command/ListJobsCommand.php
+++ Command/ListJobsCommand.php
@@ -12,13 +12,13 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Lists active batch jobs
  * 
  * @author    Antoine Guigan <antoine@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class ListJobsCommand extends ContainerAwareCommand
 {
     /**
-     * Option used to list all jobs
+     * @staticvar string Option used to list all jobs
      */
     const LIST_ALL = 'all';
 
