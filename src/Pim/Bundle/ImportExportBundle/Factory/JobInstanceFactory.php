5824d8d031c023a1b9e54d774e1de2b3cb9bfdcf
Romain Monceau <romain@akeneo.com>
2014-06-24 10:39:09 +0200

    PIM-2795: Remove useless line

--- src/Pim/Bundle/ImportExportBundle/Factory/JobInstanceFactory.php
+++ src/Pim/Bundle/ImportExportBundle/Factory/JobInstanceFactory.php
@@ -8,7 +8,6 @@ namespace Pim\Bundle\ImportExportBundle\Factory;
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
  */
 class JobInstanceFactory
 {
