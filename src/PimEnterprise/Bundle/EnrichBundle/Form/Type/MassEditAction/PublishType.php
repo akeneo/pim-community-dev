2a8a9e25781ad6109d36f0644831625febcc24c1
Nicolas Dupont <nicolas@akeneo.com>
2014-06-27 19:24:35 +0200

    PIM-2651 : add mass publish form type

---
+++ src/PimEnterprise/Bundle/EnrichBundle/Form/Type/MassEditAction/PublishType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PimEnterprise\Bundle\EnrichBundle\Form\Type\MassEditAction;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Form type of the Publish operation
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class PublishType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+
+        $resolver->setDefaults(
+            [
+                'data_class' => 'PimEnterprise\\Bundle\\EnrichBundle\\MassEditAction\\Operation\\Publish',
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pimee_enrich_mass_publish';
+    }
+}
