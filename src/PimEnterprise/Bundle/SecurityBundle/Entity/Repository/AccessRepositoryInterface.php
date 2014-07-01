fd2b27d636dfe179073d232083b284f49a2e40bc
Romain Monceau <romain@akeneo.com>
2014-07-01 09:15:49 +0200

    PIM-2803: Add phpdoc for the interface

--- src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/AccessRepositoryInterface.php
+++ src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/AccessRepositoryInterface.php
@@ -4,6 +4,12 @@ namespace PimEnterprise\Bundle\SecurityBundle\Entity\Repository;
 
 use Oro\Bundle\UserBundle\Entity\User;
 
+/**
+ * Interface for access repository
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
 interface AccessRepositoryInterface
 {
     /**
