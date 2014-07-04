9bed4771d379cbaf101fade44a001286a1061a8e
Romain Monceau <romain@akeneo.com>
2014-07-04 14:53:53 +0200

    PIM-2828: Forbid to remove a product if it has been published

---
+++ src/PimEnterprise/Bundle/WorkflowBundle/EventSubscriber/PublishedProduct/CheckPublishedProductOnRemovalSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PimEnterprise\Bundle\WorkflowBundle\EventSubscriber\PublishedProduct;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use Pim\Bundle\CatalogBundle\CatalogEvents;
+use PimEnterprise\Bundle\WorkflowBundle\Repository\PublishedProductRepositoryInterface;
+
+/**
+ * Check some pre remove events and forbid deletion if the entity is linked to a published product
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class CheckPublishedProductOnRemovalSubscriber implements EventSubscriberInterface
+{
+    /** @var PublishedProductRepositoryInterface */
+    protected $publishedRepository;
+
+    /**
+     * @param PublishedProductRepositoryInterface $publishedRepository
+     */
+    public function __construct(PublishedProductRepositoryInterface $publishedRepository)
+    {
+        $this->publishedRepository = $publishedRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            CatalogEvents::PRE_REMOVE_PRODUCT => 'checkProductHasBeenPublished'
+        ];
+    }
+
+    /**
+     * Check if the product is published
+     *
+     * @param GenericEvent $event
+     *
+     * @throws ConflictHttpException
+     */
+    public function checkProductHasBeenPublished(GenericEvent $event)
+    {
+        $product   = $event->getSubject();
+        $published = $this->publishedRepository->findOneByOriginalProductId($product->getId());
+
+        if ($published) {
+            throw new ConflictHttpException('Impossible to remove a published product');
+        }
+    }
+}
