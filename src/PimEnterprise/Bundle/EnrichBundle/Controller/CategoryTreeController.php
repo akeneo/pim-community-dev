200e9b038f81b1050935bd65c1bb20ab9bb34fd1
Nicolas Dupont <nicolas@akeneo.com>
2014-07-05 19:03:21 +0200

    PIM-2860 : update the category tree controller to use the context and apply different permission checks depending on this one

--- src/PimEnterprise/Bundle/EnrichBundle/Controller/CategoryTreeController.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Controller/CategoryTreeController.php
@@ -5,6 +5,7 @@ namespace PimEnterprise\Bundle\EnrichBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
@@ -15,29 +16,75 @@ use PimEnterprise\Bundle\UserBundle\Context\UserContext;
 use PimEnterprise\Bundle\SecurityBundle\Voter\CategoryVoter;
 
 /**
+ * Overriden category controller
+ *
  * @author    Julien Janvier <julien.janvier@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  */
 class CategoryTreeController extends BaseCategoryTreeController
 {
+    /** @staticvar string */
+    const CONTEXT_MANAGE = 'manage';
+
+    /** @staticvar string */
+    const CONTEXT_VIEW = 'view';
+
+    /** @staticvar string */
+    const CONTEXT_ASSOCIATE = 'associate';
+
+    /** @staticvar string */
+    const CONTEXT_OWNERSHIP = 'ownership';
+
     /**
-     * Find a category from its id
+     * Find a category from its id, trow exception is not found or not granted
      *
-     * @param integer $categoryId
+     * @param integer $categoryId the category id
+     * @param string  $context    the retrieving context
      *
      * @return CategoryInterface
+     *
      * @throws NotFoundHttpException
      * @throws AccessDeniedException
      */
-    protected function findAccessibleCategory($categoryId)
+    protected function findGrantedCategory($categoryId, $context)
     {
         $category = $this->findCategory($categoryId);
+        $allowed = [self::CONTEXT_MANAGE, self::CONTEXT_VIEW, self::CONTEXT_ASSOCIATE, self::CONTEXT_OWNERSHIP];
 
-        if (false === $this->securityContext->isGranted(CategoryVoter::VIEW_PRODUCTS, $category)) {
+        if (!in_array($context, $allowed)) {
+             throw new AccessDeniedException('You can not access this category');
+        }
+
+        if ($context === self::CONTEXT_MANAGE && !$this->securityFacade->isGranted('pim_enrich_category_edit')) {
+             throw new AccessDeniedException('You can not access this category');
+        } elseif (false === $this->securityContext->isGranted(CategoryVoter::VIEW_PRODUCTS, $category)) {
             throw new AccessDeniedException('You can not access this category');
         }
 
         return $category;
+    }
+
+    /**
+     * Find granted trees
+     *
+     * @param UserInterface $user    the user
+     * @param string        $context the retrieving context
+     *
+     * @return CategoryInterface[]
+     *
+     * @throws NotFoundHttpException
+     * @throws AccessDeniedException
+     */
+    protected function findGrantedTrees(UserInterface $user, $context)
+    {
+        $allTrees = ($context === self::CONTEXT_MANAGE || $context === self::CONTEXT_OWNERSHIP);
+
+        if ($allTrees && $this->securityFacade->isGranted('pim_enrich_category_edit')) {
+            return $this->categoryManager->getTrees($this->getUser());
+
+        } else {
+            return $this->categoryManager->getAccessibleTrees($this->getUser());
+        }
     }
 
     /**
@@ -49,8 +96,9 @@ class CategoryTreeController extends BaseCategoryTreeController
     public function listTreeAction(Request $request)
     {
         $selectNodeId = $request->get('select_node_id', -1);
+        $context      = $request->get('context', false);
         try {
-            $selectNode = $this->findAccessibleCategory($selectNodeId);
+            $selectNode = $this->findGrantedCategory($selectNodeId, $context);
         } catch (NotFoundHttpException $e) {
             $selectNode = $this->userContext->getAccessibleUserTree();
         } catch (AccessDeniedException $e) {
@@ -58,7 +106,7 @@ class CategoryTreeController extends BaseCategoryTreeController
         }
 
         return array(
-            'trees'          => $this->categoryManager->getAccessibleTrees($this->getUser()),
+            'trees'          => $this->findGrantedTrees($this->getUser(), $context),
             'selectedTreeId' => $selectNode->isRoot() ? $selectNode->getId() : $selectNode->getRoot(),
             'include_sub'    => (bool) $this->getRequest()->get('include_sub', false),
             'product_count'  => (bool) $this->getRequest()->get('with_products_count', true),
