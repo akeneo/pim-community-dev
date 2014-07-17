83dbce2db4060e38b7768a990d7302c0ba15ef4f
Filips Alpe <filips@akeneo.com>
2014-07-17 13:43:10 +0300

    Create helpers for preparing product values for product view page

---
+++ src/PimEnterprise/Bundle/WorkflowBundle/Helper/SortProductValuesHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PimEnterprise\Bundle\WorkflowBundle\Helper;
+
+/**
+ * Helper for sorting product values
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class SortProductValuesHelper
+{
+    /**
+     * Sorts the provided values by attribute group and sort order
+     *
+     * @param AbstractProductValue[] $values
+     *
+     * @return AbstractProductValue[]
+     */
+    public function sort($values)
+    {
+        usort(
+            $values,
+            function ($first, $second) {
+                $firstGroupOrder = $first->getAttribute()->getGroup()->getSortOrder();
+                $secondGroupOrder = $second->getAttribute()->getGroup()->getSortOrder();
+
+                if ($firstGroupOrder !== $secondGroupOrder) {
+                    return $firstGroupOrder > $secondGroupOrder ? 1 : -1;
+                }
+
+                $firstAttrOrder = $first->getAttribute()->getSortOrder();
+                $secondAttrOrder = $second->getAttribute()->getSortOrder();
+
+                return $firstAttrOrder === $secondAttrOrder ? 0 : ($firstAttrOrder > $secondAttrOrder ? 1 : -1);
+            }
+        );
+
+        $sortedValues = [];
+
+        foreach ($values as $value) {
+            $group = $value->getAttribute()->getGroup();
+
+            $sortedValues[$group->getCode()]['groupLabel'] = $group->getLabel();
+            $sortedValues[$group->getCode()]['values'][] = $value;
+        }
+
+        return $sortedValues;
+    }
+}
