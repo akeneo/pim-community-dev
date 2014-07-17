2c5a9be9a5206eb494dfea9e2c220bfb8852cc34
jjanvier <j.janvier@gmail.com>
2014-07-17 14:09:04 +0200

    PIM-2754: Defining the datasource pimee_datasource_product_history as eligible to mongo support

---
+++ src/PimEnterprise/Bundle/DataGridBundle/DependencyInjection/Compiler/DatasourceSupportPass.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PimEnterprise\Bundle\DataGridBundle\DependencyInjection\Compiler;
+
+use PimEnterprise\Bundle\DataGridBundle\Datasource\DatasourceTypes;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Datasource support pass
+ *
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class DatasourceSupportPass implements CompilerPassInterface
+{
+    /** @staticvar string */
+    const DATASOURCE_SUPPORT_RESOLVER_ID = 'pim_datagrid.datasource.support_resolver';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $psupportResolver = $container->getDefinition(self::DATASOURCE_SUPPORT_RESOLVER_ID);
+        $psupportResolver->addMethodCall(
+            'addMongoEligibleDatasources',
+            [ DatasourceTypes::DATASOURCE_PRODUCT_HISTORY ]
+        );
+    }
+}
