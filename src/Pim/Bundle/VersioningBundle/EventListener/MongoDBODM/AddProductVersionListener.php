1b3aa18058ce743abf73d814a5569edfe645f297
Benoit Jacquemont <benoit@akeneo.com>
2014-07-21 10:21:41 +0200

    Fixes phpDoc and function visibility

--- src/Pim/Bundle/VersioningBundle/EventListener/MongoDBODM/AddProductVersionListener.php
+++ src/Pim/Bundle/VersioningBundle/EventListener/MongoDBODM/AddProductVersionListener.php
@@ -124,8 +124,10 @@ class AddProductVersionListener implements EventSubscriber
 
     /**
      * @param object $versionable
+     *
+     * @return Version
      */
-    public function createVersion($versionable)
+    protected function createVersion($versionable)
     {
         $changeset = [];
         if (!$this->versionManager->isRealTimeVersioning()) {
