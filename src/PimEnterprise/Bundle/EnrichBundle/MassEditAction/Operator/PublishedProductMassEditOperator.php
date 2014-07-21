cd4c5739004a9f321af2145747e863e93e43d261
Nicolas Dupont <nicolas@akeneo.com>
2014-07-21 18:38:46 +0200

    Fix CS

--- src/PimEnterprise/Bundle/EnrichBundle/MassEditAction/Operator/PublishedProductMassEditOperator.php
+++ src/PimEnterprise/Bundle/EnrichBundle/MassEditAction/Operator/PublishedProductMassEditOperator.php
@@ -4,8 +4,6 @@ namespace PimEnterprise\Bundle\EnrichBundle\MassEditAction\Operator;
 
 use Oro\Bundle\SecurityBundle\SecurityFacade;
 use PimEnterprise\Bundle\WorkflowBundle\Manager\PublishedProductManager;
-use Pim\Bundle\CatalogBundle\Manager\ProductManager;
-use Pim\Bundle\EnrichBundle\MassEditAction\Operation\ProductMassEditOperation;
 use Pim\Bundle\EnrichBundle\MassEditAction\Operator\AbstractMassEditOperator;
 
 /**
