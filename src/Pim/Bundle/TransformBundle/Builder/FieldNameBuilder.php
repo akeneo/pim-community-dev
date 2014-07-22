6d8e22339319514330d91ad3b7044be91e198908
Romain Monceau <romain@akeneo.com>
2014-07-22 14:43:39 +0200

    PIM-2900: Make work field name builder with values

--- src/Pim/Bundle/TransformBundle/Builder/FieldNameBuilder.php
+++ src/Pim/Bundle/TransformBundle/Builder/FieldNameBuilder.php
@@ -3,6 +3,7 @@
 namespace Pim\Bundle\TransformBundle\Builder;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
 
 /**
  * Create field names for associations and product values
@@ -22,11 +23,13 @@ class FieldNameBuilder
     /**
      * @param ManagerRegistry $managerRegistry
      * @param string          $assocTypeClass
+     * @param string          $attributeClass
      */
-    public function __construct(ManagerRegistry $managerRegistry, $assocTypeClass)
+    public function __construct(ManagerRegistry $managerRegistry, $assocTypeClass, $attributeClass)
     {
         $this->managerRegistry = $managerRegistry;
         $this->assocTypeClass  = $assocTypeClass;
+        $this->attributeClass  = $attributeClass;
     }
 
     /**
@@ -43,6 +46,50 @@ class FieldNameBuilder
         }
 
         return $fieldNames;
+    }
+
+    /**
+     * @param $fieldName
+     *
+     * @return array
+     */
+    public function extractAttributeFieldNameInfos($fieldName)
+    {
+        $explodedFieldName = explode("-", $fieldName);
+        $attributeCode = $explodedFieldName[0];
+
+        $attribute = $this->getRepository($this->attributeClass)->findByReference($attributeCode);
+
+        return $this->extractAttributeInfos($attribute, $explodedFieldName);
+    }
+
+    protected function extractAttributeInfos(AbstractAttribute $attribute, array $explodedFieldName)
+    {
+        if ($attribute->isLocalizable() && $attribute->isScopable()) {
+            $localeCode = $explodedFieldName[1];
+            $scopeCode  = $explodedFieldName[2];
+            $priceCurrency = $attribute->getBackendType() === 'prices' ? $explodedFieldName[3] : null;
+        } elseif ($attribute->isLocalizable()) {
+            $localeCode = $explodedFieldName[1];
+            $scopeCode  = null;
+            $priceCurrency = $attribute->getBackendType() === 'prices' ? $explodedFieldName[2] : null;
+        } elseif ($attribute->isScopable()) {
+            $localeCode = null;
+            $scopeCode  = $explodedFieldName[1];
+            $priceCurrency = $attribute->getBackendType() === 'prices' ? $explodedFieldName[2] : null;
+        } else {
+            $localeCode = null;
+            $scopeCode  = null;
+            $priceCurrency = $attribute->getBackendType() === 'prices' ? $explodedFieldName[1] : null;
+        }
+
+        $priceArray = (null === $priceCurrency) ? [] : ['price_currency' => $priceCurrency];
+
+        return [
+            'attribute'   => $attribute,
+            'locale_code' => $localeCode,
+            'scope_code'  => $scopeCode,
+        ] + $priceArray;
     }
 
     /**
