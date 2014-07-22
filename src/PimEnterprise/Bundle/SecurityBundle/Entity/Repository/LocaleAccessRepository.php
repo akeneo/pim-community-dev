bbadab9284f1ba7c03407f3b9d5bedee86a1ede5
Nicolas Dupont <nicolas@akeneo.com>
2014-07-22 15:06:35 +0200

    PIM-2517 : add missing methods in locale access repository

--- src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/LocaleAccessRepository.php
+++ src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/LocaleAccessRepository.php
@@ -2,15 +2,8 @@
 
 namespace PimEnterprise\Bundle\SecurityBundle\Entity\Repository;
 
-use Symfony\Component\Security\Core\Role\RoleInterface;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
-use Doctrine\ORM\AbstractQuery;
-use Oro\Bundle\UserBundle\Entity\Role;
-use Oro\Bundle\UserBundle\Entity\User;
-use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
-use Pim\Bundle\CatalogBundle\Model\ProductInterface;
-use Pim\Bundle\CatalogBundle\Doctrine\TableNameBuilder;
+use Pim\Bundle\CatalogBundle\Entity\Locale;
 use PimEnterprise\Bundle\SecurityBundle\Attributes;
 
 /**
@@ -21,4 +14,71 @@ use PimEnterprise\Bundle\SecurityBundle\Attributes;
  */
 class LocaleAccessRepository extends EntityRepository
 {
+    /**
+     * Get roles that have the specified access to a locale
+     *
+     * @param Locale $locale
+     * @param string $accessLevel
+     *
+     * @return Role[]
+     */
+    public function getGrantedRoles(Locale $locale, $accessLevel)
+    {
+        $accessField = $this->getAccessField($accessLevel);
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('r')
+            ->innerJoin('OroUserBundle:Role', 'r', 'WITH', 'a.role = r.id')
+            ->where('a.locale = :locale')
+            ->andWhere($qb->expr()->eq(sprintf('a.%s', $accessField), true))
+            ->setParameter('locale', $locale);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * Revoke access to a locales
+     * If excluded roles are provided, access will not be revoked for these roles
+     *
+     * @param Locale locale
+     * @param Role[] $excludedRoles
+     *
+     * @return integer
+     */
+    public function revokeAccess(Locale $locale, array $excludedRoles = [])
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->delete()
+            ->where('a.locale = :locale')
+            ->setParameter('locale', $locale);
+
+        if (!empty($excludedRoles)) {
+            $qb
+                ->andWhere($qb->expr()->notIn('a.role', ':excludedRoles'))
+                ->setParameter('excludedRoles', $excludedRoles);
+        }
+
+        return $qb->getQuery()->execute();
+    }
+
+    /**
+     * Get the access field depending of access level sent
+     *
+     * @param string $accessLevel
+     *
+     * @return string
+     */
+    protected function getAccessField($accessLevel)
+    {
+        $mapping = [
+            Attributes::EDIT_PRODUCTS => 'editProducts',
+            Attributes::VIEW_PRODUCTS => 'viewProducts'
+        ];
+        if (!isset($mapping[$accessLevel])) {
+            throw new \LogicException(sprintf('%s access level not exists', $accessLevel));
+        }
+
+        return $mapping[$accessLevel];
+    }
 }
