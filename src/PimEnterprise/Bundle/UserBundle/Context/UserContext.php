2a626aad6f5347fed709b219cd94873ffc23141c
Filips Alpe <filips@akeneo.com>
2014-07-23 15:29:30 +0300

    Revert "Enforce locale permissions in UserContext"
    This reverts commit 31e80a2de7275172d2e68de7cf01a72d5d0a30ae.

--- src/PimEnterprise/Bundle/UserBundle/Context/UserContext.php
+++ src/PimEnterprise/Bundle/UserBundle/Context/UserContext.php
@@ -5,7 +5,6 @@ namespace PimEnterprise\Bundle\UserBundle\Context;
 use Pim\Bundle\UserBundle\Context\UserContext as BaseUserContext;
 use PimEnterprise\Bundle\CatalogBundle\Manager\CategoryManager;
 use PimEnterprise\Bundle\SecurityBundle\Attributes;
-use Pim\Bundle\CatalogBundle\Entity\Locale;
 
 /**
  * User context that provides access to user locale, channel and default category tree
@@ -19,64 +18,6 @@ class UserContext extends BaseUserContext
      * @var CategoryManager
      */
     protected $categoryManager;
-
-   /**
-     * {@inheritdoc}
-     *
-     * @throws \LogicException When user doesn't have access to any activated locales
-     */
-    public function getCurrentLocale()
-    {
-        try {
-            return parent::getCurrentLocale();
-        } catch (\LogicException $e) {
-            throw new \LogicException("User doesn't have access to any activated locales");
-        }
-    }
-
-    /**
-     * Returns active locales the user has access to
-     *
-     * {@inheritdoc}
-     */
-    public function getUserLocales()
-    {
-        if ($this->userLocales === null) {
-            $this->userLocales = array_filter(
-                parent::getUserLocales(),
-                function ($locale) {
-                    return $this->securityContext->isGranted(Attributes::VIEW_PRODUCTS, $locale);
-                }
-            );
-        }
-
-        return $this->userLocales;
-    }
-
-   /**
-     * Returns the default application locale if user has access to it
-     *
-     * {@inheritdoc}
-     */
-    protected function getDefaultLocale()
-    {
-        $locale = parent::getDefaultLocale();
-
-        if (null !== $locale && !$this->securityContext->isGranted(Attributes::VIEW_PRODUCTS, $locale)) {
-            return null;
-        }
-    }
-
-    /**
-     * Checks if a locale is activated and user has the right to access it
-     *
-     * {@inheritdoc}
-     */
-    protected function isLocaleAvailable(Locale $locale)
-    {
-        return parent::isLocaleAvailable($locale) &&
-               $this->securityContext->isGranted(Attributes::VIEW_PRODUCTS, $locale);
-    }
 
     /**
      * Get user category tree
