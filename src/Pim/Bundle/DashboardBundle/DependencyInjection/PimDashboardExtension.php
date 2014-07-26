88b7c553170f67ae4f618751b8e160983301ff2f
Nicolas Dupont <nicolas@akeneo.com>
2014-07-27 00:48:35 +0200

    PIM-2919 : re-organize configuration files in dashboard bundle

--- src/Pim/Bundle/DashboardBundle/DependencyInjection/PimDashboardExtension.php
+++ src/Pim/Bundle/DashboardBundle/DependencyInjection/PimDashboardExtension.php
@@ -20,6 +20,7 @@ class PimDashboardExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('controllers.yml');
+        $loader->load('widgets.yml');
     }
 }
