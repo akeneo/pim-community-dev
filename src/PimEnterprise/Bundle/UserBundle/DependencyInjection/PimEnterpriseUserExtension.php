1b65fa2f4287c6f575f72659139080189768dc52
Nicolas Dupont <nicolas@akeneo.com>
2014-07-27 10:44:57 +0200

    PIM-2919 : rename services.yml to context.yml in user bundle

--- src/PimEnterprise/Bundle/UserBundle/DependencyInjection/PimEnterpriseUserExtension.php
+++ src/PimEnterprise/Bundle/UserBundle/DependencyInjection/PimEnterpriseUserExtension.php
@@ -21,6 +21,6 @@ class PimEnterpriseUserExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('context.yml');
     }
 }
