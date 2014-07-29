69f5c77f09fa08202f73980ad3aeaf1f77a6c60d
jjanvier <j.janvier@gmail.com>
2014-07-29 12:30:39 +0200

    PIM-2590: Introducing DatagridViewAccessManager

---
+++ spec/PimEnterprise/Bundle/SecurityBundle/Manager/DatagridViewAccessManagerSpec.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace spec\PimEnterprise\Bundle\SecurityBundle\Manager;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
+use Pim\Bundle\CatalogBundle\Entity\AttributeGroup;
+use Pim\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
+use Pim\Bundle\DataGridBundle\Entity\DatagridView;
+use PimEnterprise\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
+use PimEnterprise\Bundle\SecurityBundle\Attributes;
+use PimEnterprise\Bundle\SecurityBundle\Manager\AttributeGroupAccessManager;
+use PimEnterprise\Bundle\SecurityBundle\Manager\CategoryAccessManager;
+use Prophecy\Argument;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DatagridViewAccessManagerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('PimEnterprise\Bundle\SecurityBundle\Manager\DatagridViewAccessManager');
+    }
+
+    public function let(
+        AttributeRepository $attributeRepository,
+        CategoryRepository $categoryRepository,
+        AttributeGroupAccessManager $attributeGroupAccessManager,
+        CategoryAccessManager $categoryAccessManager,
+        DatagridView $view,
+        UserInterface $user
+    ) {
+        $this->beConstructedWith(
+            $attributeRepository,
+            $categoryRepository,
+            $attributeGroupAccessManager,
+            $categoryAccessManager
+        );
+
+        $view->getColumns()->willReturn(['col1']);
+        $view->getFilters()->willReturn('f[filter1]=10,f[category][value][categoryId]=3');
+    }
+
+    function it_does_not_grant_access_if_user_has_not_access_to_an_column(
+        $view,
+        $user,
+        $attributeGroupAccessManager,
+        $attributeRepository,
+        Attribute $attribute,
+        AttributeGroup $group
+    ) {
+        $attribute->getGroup()->willReturn($group);
+        $attributeRepository->findOneBy(['code' => 'col1'])->willReturn($attribute);
+        $attributeGroupAccessManager->isUserGranted($user, $group, Attributes::VIEW_ATTRIBUTES)->willReturn(false);
+
+        $this->isUserGranted($user, $view, Attributes::VIEW_DATAGRID_VIEW)->shouldReturn(false);
+    }
+
+    function it_does_not_grant_access_if_user_has_not_access_to_a_filter(
+        $view,
+        $user,
+        $attributeGroupAccessManager,
+        $attributeRepository,
+        Attribute $attribute,
+        AttributeGroup $group
+    ) {
+        $attribute->getGroup()->willReturn($group);
+        $attributeRepository->findOneBy(['code' => 'col1'])->willReturn(null);
+        $attributeRepository->findOneBy(['code' => 'filter1'])->willReturn($attribute);
+        $attributeGroupAccessManager->isUserGranted($user, $group, Attributes::VIEW_ATTRIBUTES)->willReturn(false);
+
+        $this->isUserGranted($user, $view, Attributes::VIEW_DATAGRID_VIEW)->shouldReturn(false);
+    }
+
+    function it_does_not_grant_access_if_user_has_not_access_to_a_category_filter(
+        $view,
+        $user,
+        $categoryRepository,
+        $categoryAccessManager,
+        $attributeRepository,
+        CategoryInterface $category
+    ) {
+        $attributeRepository->findOneBy(['code' => 'col1'])->willReturn(null);
+        $attributeRepository->findOneBy(['code' => 'filter1'])->willReturn(null);
+        $attributeRepository->findOneBy(['code' => 'category'])->willReturn(null);
+
+        $categoryRepository->find('3')->willReturn($category);
+        $categoryAccessManager->isUserGranted($user, $category, Attributes::VIEW_PRODUCTS)->willReturn(false);
+
+        $this->isUserGranted($user, $view, Attributes::VIEW_DATAGRID_VIEW)->shouldReturn(false);
+    }
+
+    function it_grants_access(
+        $view,
+        $user,
+        $attributeGroupAccessManager,
+        $attributeRepository,
+        $categoryRepository,
+        $categoryAccessManager,
+        Attribute $attribute,
+        AttributeGroup $group,
+        CategoryInterface $category
+    ) {
+
+        $attribute->getGroup()->willReturn($group);
+        $attributeRepository->findOneBy(['code' => 'col1'])->willReturn($attribute);
+        $attributeRepository->findOneBy(['code' => 'filter1'])->willReturn($attribute);
+        $attributeRepository->findOneBy(['code' => 'category'])->willReturn(null);
+        $attributeGroupAccessManager->isUserGranted($user, $group, Attributes::VIEW_ATTRIBUTES)->willReturn(true);
+
+        $categoryRepository->find('3')->willReturn($category);
+        $categoryAccessManager->isUserGranted($user, $category, Attributes::VIEW_PRODUCTS)->willReturn(true);
+
+        $this->isUserGranted($user, $view, Attributes::VIEW_DATAGRID_VIEW)->shouldReturn(true);
+    }
+}
---
+++ src/PimEnterprise/Bundle/SecurityBundle/Manager/DatagridViewAccessManager.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace PimEnterprise\Bundle\SecurityBundle\Manager;
+
+use Pim\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use Pim\Bundle\DataGridBundle\Entity\DatagridView;
+use PimEnterprise\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
+use PimEnterprise\Bundle\FilterBundle\Filter\Product\CategoryFilter;
+use PimEnterprise\Bundle\SecurityBundle\Attributes;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Datagrid view access manager
+ *
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ */
+class DatagridViewAccessManager
+{
+
+    /** @var AttributeRepository */
+    protected $attributeRepository;
+
+    /** @var CategoryRepository */
+    protected $categoryRepository;
+
+    /** @var AttributeGroupAccessManager */
+    protected $attributeGroupAccessManager;
+
+    /** @var CategoryAccessManager */
+    protected $categoryAccessManager;
+
+    /**
+     * @param AttributeRepository         $attributeRepository
+     * @param CategoryRepository          $categoryRepository
+     * @param AttributeGroupAccessManager $attributeGroupAccessManager
+     * @param CategoryAccessManager       $categoryAccessManager
+     */
+    public function __construct(
+        AttributeRepository $attributeRepository,
+        CategoryRepository $categoryRepository,
+        AttributeGroupAccessManager $attributeGroupAccessManager,
+        CategoryAccessManager $categoryAccessManager
+    ) {
+        $this->attributeRepository = $attributeRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->attributeGroupAccessManager = $attributeGroupAccessManager;
+        $this->categoryAccessManager = $categoryAccessManager;
+    }
+
+    /**
+     * Chek is a user is granted on a datagrid view for the given attribute.
+     *
+     * @param UserInterface $user
+     * @param DatagridView  $view
+     * @param string        $attribute
+     *
+     * @return bool
+     *
+     * @throws \LogicException
+     */
+    public function isUserGranted(UserInterface $user, DatagridView $view, $attribute)
+    {
+        if (Attributes::VIEW_DATAGRID_VIEW !== $attribute) {
+            throw new \LogicException(sprintf('Attribute "%" is not supported.', $attribute));
+        }
+
+        foreach ($view->getColumns() as $column) {
+            if (false === $this->isAttributeGranted($user, $column)) {
+                return false;
+            }
+        }
+
+        foreach ($this->getViewFiltersAsArray($view) as $filter) {
+            if (false === $this->isAttributeGranted($user, $filter)) {
+                return false;
+            }
+        }
+
+        if (null !== $categoryId = $this->getCategoryIdFromViewFilters($view)) {
+            return $this->isCategoryGranted($user, $categoryId);
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if an attribute is granted for the current user (ie: if the attribute group is granted for view)
+     *
+     * @param UserInterface $user
+     * @param string        $code
+     *
+     * @return bool
+     */
+    protected function isAttributeGranted(UserInterface $user, $code)
+    {
+        /** @var \Pim\Bundle\CatalogBundle\Entity\Attribute $attribute */
+        if (null === $attribute = $this->attributeRepository->findOneBy(['code' => $code])) {
+            return true;
+        }
+
+        return $this->attributeGroupAccessManager->isUserGranted(
+            $user,
+            $attribute->getGroup(),
+            Attributes::VIEW_ATTRIBUTES
+        );
+    }
+
+    /**
+     * Check if a category is granted for the current user (ie: if the category is granted for view)
+     *
+     * @param UserInterface $user
+     * @param int           $categoryId
+     *
+     * @return bool
+     */
+    protected function isCategoryGranted(UserInterface $user, $categoryId)
+    {
+        if (CategoryFilter::ALL_CATEGORY == $categoryId || CategoryFilter::UNCLASSIFIED_CATEGORY == $categoryId) {
+            return true;
+        }
+
+        /** @var \Pim\Bundle\CatalogBundle\Model\CategoryInterface $category */
+        if (null === $category = $this->categoryRepository->find($categoryId)) {
+            return false;
+        }
+
+        return $this->categoryAccessManager->isUserGranted($user, $category, Attributes::VIEW_PRODUCTS);
+    }
+
+    /**
+     * TODO:  change the way view filters are stored in the DB and remove this ugly hack...
+     *
+     * @param DatagridView $view
+     *
+     * @return int|null
+     */
+    protected function getCategoryIdFromViewFilters(DatagridView $view)
+    {
+        $matches = [];
+        preg_match('/f\[category\]\[value\]\[categoryId\]=(\d|-\d)/', urldecode($view->getFilters()), $matches);
+
+        // no filter on categories
+        if (empty($matches[1])) {
+            return null;
+        }
+
+        if (is_string($matches[1])) {
+            return $matches[1];
+        }
+
+        return $matches[1][0];
+    }
+
+    /**
+     * TODO:  change the way view filters are stored in the DB and remove this ugly hack...
+     *
+     * @param DatagridView $view
+     *
+     * @return array
+     */
+    protected function getViewFiltersAsArray(DatagridView $view)
+    {
+        $matches = [];
+        preg_match_all('/f\[(.*?)\].*?=([\w\d]|\-\d)/', urldecode($view->getFilters()), $matches);
+
+        $filters = array_unique($matches[1]);
+
+        return array_map(
+            function ($filter) {
+                return str_replace('__', '', $filter);
+            },
+            $filters
+        );
+    }
+}
