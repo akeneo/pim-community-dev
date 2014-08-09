77b9c2b10b373504deebe3e1e4bb6b9e3278ae72
Nicolas Dupont <nicolas@akeneo.com>
2014-08-09 12:28:52 +0200

    PIM-2919 : refactor metric filter to avoid the base filter inheritance

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/MetricFilter.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/MetricFilter.php
@@ -3,7 +3,13 @@
 namespace Pim\Bundle\CatalogBundle\Doctrine\ORM\Filter;
 
 use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
 use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+use Pim\Bundle\CatalogBundle\Exception\ProductQueryException;
+use Pim\Bundle\CatalogBundle\Doctrine\AttributeFilterInterface;
+use Pim\Bundle\CatalogBundle\Doctrine\ORM\ValueJoin;
+use Pim\Bundle\CatalogBundle\Doctrine\ORM\CriteriaCondition;
+use Pim\Bundle\CatalogBundle\Context\CatalogContext;
 
 /**
  * Metric filter
@@ -12,8 +18,40 @@ use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class MetricFilter extends BaseFilter
+class MetricFilter implements AttributeFilterInterface
 {
+    /**
+     * @var QueryBuilder
+     */
+    protected $qb;
+
+    /** @var CatalogContext */
+    protected $context;
+
+    /**
+     * Alias counter, to avoid duplicate alias name
+     * @return integer
+     */
+    protected $aliasCounter = 1;
+
+    /**
+     * Instanciate the base filter
+     *
+     * @param CatalogContext $context
+     */
+    public function __construct(CatalogContext $context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setQueryBuilder($queryBuilder)
+    {
+        $this->qb = $queryBuilder;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -69,5 +107,39 @@ class MetricFilter extends BaseFilter
     public function supportsOperator($operator)
     {
         return in_array($operator, ['=', '<', '<=', '>', '>=', 'EMPTY']);
+    }
+
+    /**
+     * Prepare criteria condition with field, operator and value
+     *
+     * @param string|array $field    the backend field name
+     * @param string|array $operator the operator used to filter
+     * @param string|array $value    the value(s) to filter
+     *
+     * @return string
+     * @throws ProductQueryException
+     */
+    protected function prepareCriteriaCondition($field, $operator, $value)
+    {
+        $criteriaCondition = new CriteriaCondition($this->qb);
+
+        return $criteriaCondition->prepareCriteriaCondition($field, $operator, $value);
+    }
+
+    /**
+     * Prepare join to attribute condition with current locale and scope criterias
+     *
+     * @param AbstractAttribute $attribute the attribute
+     * @param string            $joinAlias the value join alias
+     *
+     * @throws ProductQueryException
+     *
+     * @return string
+     */
+    protected function prepareAttributeJoinCondition(AbstractAttribute $attribute, $joinAlias)
+    {
+        $joinHelper = new ValueJoin($this->qb, $this->context);
+
+        return $joinHelper->prepareCondition($attribute, $joinAlias);
     }
 }
