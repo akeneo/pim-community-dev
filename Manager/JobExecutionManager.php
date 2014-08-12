62df84e50e44e6e5e9c05cc6e2475e9198d19a29
sumbobyboys <sumbobyboys@gmail.com>
2014-08-12 14:49:40 +0200

    add job execution update

--- Manager/JobExecutionManager.php
+++ Manager/JobExecutionManager.php
@@ -5,6 +5,8 @@ namespace Akeneo\Bundle\BatchBundle\Manager;
 use Akeneo\Bundle\BatchBundle\Entity\JobExecution;
 use Symfony\Component\Process\Process;
 use Doctrine\ORM\EntityManager;
+use Akeneo\Bundle\BatchBundle\Job\BatchStatus;
+use Akeneo\Bundle\BatchBundle\Job\ExitStatus;
 
 /**
  * Job execution manager
@@ -16,6 +18,26 @@ use Doctrine\ORM\EntityManager;
 class JobExecutionManager
 {
     /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var string
+     */
+    protected $jobExecutionClass;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param string        $jobExecutionClass
+     */
+    public function __construct(EntityManager $entityManager, $jobExecutionClass)
+    {
+        $this->entityManager     = $entityManager;
+        $this->jobExecutionClass = $jobExecutionClass;
+    }
+
+    /**
      * CHeck if the given JoExecution is still running using his PID
      * @param JobExecution $jobExecution
      *
@@ -23,13 +45,43 @@ class JobExecutionManager
      */
     public function checkRunningStatus(JobExecution $jobExecution)
     {
-        // if ($pid = intval($jobExecution->getPid()) > 0) {
-        //     exec(sprintf('ps -p %s', $pid), $output, $returnCode);
-        // } else {
-        //     throw new \InvalidArgumentException('The job execution PID is not valid');
-        // }
+        if (ExitStatus::UNKNOWN === $jobExecution->getExitStatus()->getValue()) {
+            return $this->processIsRunning($jobExecution);
+        } else {
+            return true;
+        }
 
-        // return $returnCode === 0;
-        return true;
+    }
+
+    /**
+     * Test if the process is still running
+     * @param JobExecution $jobExecution
+     *
+     * @return bool
+     */
+    protected function processIsRunning(JobExecution $jobExecution)
+    {
+        if (($pid = intval($jobExecution->getPid())) > 0) {
+            exec(sprintf('ps -p %s', $pid), $output, $returnCode);
+        } else {
+            throw new \InvalidArgumentException('The job execution PID is not valid');
+        }
+
+        return $returnCode === 0;
+    }
+
+    /**
+     * Mark a job execution as failed
+     * @param JobExecution $jobExecution
+     */
+    public function markAsFailed(JobExecution $jobExecution)
+    {
+        $jobExecution->setStatus(new BatchStatus(BatchStatus::FAILED));
+        $jobExecution->setExitStatus(new ExitStatus(ExitStatus::FAILED));
+        $jobExecution->setEndTime(new \DateTime('now'));
+        $jobExecution->addFailureException(new \Exception('An exception occured during the job execution'));
+
+        $this->getManagerForClass($this->jobExecutionClass)->persist($jobExecution);
+        $this->getManagerForClass($this->jobExecutionClass)->flush();
     }
 }
