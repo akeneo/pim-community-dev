9b356a271a9c6104ce1d9fa68a38446e07c4d69b
Antoine Guigan <antoine@akeneo.com>
2014-08-12 17:07:11 +0200

    Added new units

---
+++ Family/VoltageFamilyInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Akeneo\Bundle\MeasureBundle\Family;
+
+/**
+ * Voltage family
+ * 
+ * @author    Antoine Guigan <antoine@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class VoltageFamilyInterface
+{
+    /**
+     * @staticvar string
+     */
+    const MILLIVOLT = 'MILLIVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const CENTIVOLT = 'CENTIVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const DECIVOLT = 'DECIVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const VOLT = 'VOLT';
+
+    /**
+     * @staticvar string
+     */
+    const DEKAVOLT = 'DEKAVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const HECTOVOLT = 'HECTOVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const KILOVOLT = 'KILOVOLT';
+
+    /**
+     * @staticvar string
+     */
+    const MEGAVOLT = 'MEGAVOLT';
+}
