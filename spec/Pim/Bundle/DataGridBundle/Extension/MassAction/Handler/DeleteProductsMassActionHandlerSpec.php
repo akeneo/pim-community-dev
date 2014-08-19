5d1a452a9fc3f3a2e03bbab102996db447f24ade
Romain Monceau <romain@akeneo.com>
2014-08-19 17:24:01 +0200

    PIM-3095: Make specs work

---
+++ spec/Pim/Bundle/DataGridBundle/Extension/MassAction/Handler/DeleteProductsMassActionHandlerSpec.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace spec\Pim\Bundle\DataGridBundle\Extension\MassAction\Handler;
+
+use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
+use Oro\Bundle\DataGridBundle\Extension\Action\ActionConfiguration;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Doctrine\ORM\ProductMassActionRepository;
+use Pim\Bundle\CatalogBundle\Event\ProductEvents;
+use Pim\Bundle\CatalogBundle\Manager\ProductManager;
+use Pim\Bundle\DataGridBundle\Datasource\DatasourceInterface;
+use Pim\Bundle\DataGridBundle\Datasource\ResultRecord\HydratorInterface;
+use Pim\Bundle\DataGridBundle\Extension\MassAction\Actions\Ajax\DeleteMassAction;
+use Pim\Bundle\DataGridBundle\Extension\MassAction\Event\MassActionEvents;
+use Prophecy\Argument;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class DeleteProductsMassActionHandlerSpec extends ObjectBehavior
+{
+    function let(
+        HydratorInterface $hydrator,
+        TranslatorInterface $translator,
+        EventDispatcherInterface $eventDispatcher,
+        ProductManager $productManager,
+        DatagridInterface $datagrid,
+        DatasourceInterface $datasource,
+        DeleteMassAction $massAction,
+        ActionConfiguration $options,
+        ProductMassActionRepository $massActionRepo
+    ) {
+        $this->beConstructedWith(
+            $hydrator,
+            $translator,
+            $eventDispatcher,
+            $productManager
+        );
+
+        $translator->trans('qux')->willReturn('qux');
+
+        $datagrid->getDatasource()->willReturn($datasource);
+        $datasource->setHydrator($hydrator)->shouldBeCalled();
+        $datasource->getMassActionRepository()->willReturn($massActionRepo);
+
+        // prepare mass action response
+        $massAction->getOptions()->willReturn($options);
+        $options->offsetGetByPath(Argument::cetera())->willReturn('qux');
+    }
+
+    function it_dispatches_events($eventDispatcher, $datasource, $massActionRepo, $datagrid, $massAction)
+    {
+        $objectIds    = array('foo', 'bar', 'baz');
+        $countRemoved = count($objectIds);
+
+        $datasource->getResults()->willReturn($objectIds);
+        $massActionRepo->deleteFromIds($objectIds)->willReturn($countRemoved);
+
+        $eventDispatcher->dispatch(
+            MassActionEvents::MASS_DELETE_PRE_HANDLER,
+            Argument::type('Pim\Bundle\DataGridBundle\Extension\MassAction\Event\MassActionEvent')
+        )->shouldBeCalled();
+        $eventDispatcher->dispatch(
+            MassActionEvents::MASS_DELETE_POST_HANDLER,
+            Argument::type('Pim\Bundle\DataGridBundle\Extension\MassAction\Event\MassActionEvent')
+        )->shouldBeCalled();
+        $eventDispatcher->dispatch(
+            ProductEvents::PRE_MASS_REMOVE,
+            new GenericEvent($objectIds)
+        )->shouldBeCalled();
+        $eventDispatcher->dispatch(
+            ProductEvents::POST_MASS_REMOVE,
+            new GenericEvent($objectIds)
+        )->shouldBeCalled();
+
+        $this->handle($datagrid, $massAction);
+    }
+}
