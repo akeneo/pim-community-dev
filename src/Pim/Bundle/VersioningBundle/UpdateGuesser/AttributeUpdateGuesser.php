c43e0865fccb7a1147288ec0428178b9e24dec18
Filips Alpe <filips@akeneo.com>
2014-08-26 09:36:39 +0300

    Version products when an attribute or attribute option is removed

---
+++ src/Pim/Bundle/VersioningBundle/UpdateGuesser/AttributeUpdateGuesser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Pim\Bundle\VersioningBundle\UpdateGuesser;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+
+/**
+ * Attribute update guesser
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeUpdateGuesser implements UpdateGuesserInterface
+{
+    /** @var ManagerRegistry */
+    protected $registry;
+
+    /** @var string */
+    protected $productClass;
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param string          $productClass
+     */
+    public function __construct(ManagerRegistry $registry, $productClass)
+    {
+        $this->registry     = $registry;
+        $this->productClass = $productClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportAction($action)
+    {
+        return $action === UpdateGuesserInterface::ACTION_DELETE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function guessUpdates(EntityManager $em, $entity, $action)
+    {
+        $pendings = array();
+        if ($entity instanceof AbstractAttribute) {
+            $products = $this
+                ->registry
+                ->getRepository($this->productClass)
+                ->findAllByAttributes([$entity]);
+
+            foreach ($products as $product) {
+                $pendings[] = $product;
+            }
+        }
+
+        return $pendings;
+    }
+}
