6f8f4e6a44fb2202efaf9e87b3130a3145f361eb
Rémy BETUS <remy.betus@gmail.com>
2014-09-02 11:12:59 +0200

    CS fixe

--- src/Pim/Bundle/CatalogBundle/Entity/SequentialEdit.php
+++ src/Pim/Bundle/CatalogBundle/Entity/SequentialEdit.php
@@ -12,7 +12,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 class SequentialEdit
 {
     /**
@@ -29,7 +28,6 @@ class SequentialEdit
      * @var String $id 
      */
     protected $user;
-
 
     /**
      * Constructor
@@ -110,5 +108,4 @@ class SequentialEdit
 
         return $this;
     }
-
 }
