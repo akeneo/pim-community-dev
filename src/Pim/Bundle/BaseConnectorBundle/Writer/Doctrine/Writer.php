263717e8b64abef60f73199a3f0f90525bab0fa8
Antoine Guigan <antoine@akeneo.com>
2014-09-03 11:08:18 +0200

    Fixed writer bugging with attribute group import

--- src/Pim/Bundle/BaseConnectorBundle/Writer/Doctrine/Writer.php
+++ src/Pim/Bundle/BaseConnectorBundle/Writer/Doctrine/Writer.php
@@ -74,7 +74,7 @@ class Writer extends AbstractConfigurableStepElement implements
             $manager->flush();
         }
 
-        $this->cacheClearer->clear();
+        $this->cacheClearer->clear(true);
     }
 
     /**
