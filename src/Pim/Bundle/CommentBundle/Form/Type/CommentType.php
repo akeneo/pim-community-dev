2bd695912c9c1add535895d9f3ae139dd291f91a
jjanvier <j.janvier@gmail.com>
2014-09-04 10:47:07 +0200

    PIM-3139: Add a new comment on a product

---
+++ src/Pim/Bundle/CommentBundle/Form/Type/CommentType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Pim\Bundle\CommentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Comment type
+ *
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('body', 'textarea')
+            ->add('resourceName', 'hidden')
+            ->add('resourceId', 'hidden')
+            ->add('save', 'submit')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'Pim\Bundle\CommentBundle\Entity\Comment',
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'pim_comment_comment';
+    }
+}
