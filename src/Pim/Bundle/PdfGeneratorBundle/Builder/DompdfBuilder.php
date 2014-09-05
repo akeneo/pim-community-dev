b50b9d3938bc7bad7e6c554bd1949d2271306084
Charles Pourcel <charles.pourcel@akeneo.com>
2014-09-05 10:08:21 +0200

    Fix merging problems on DOMPDF wrapper

--- src/Pim/Bundle/PdfGeneratorBundle/Builder/DompdfBuilder.php
+++ src/Pim/Bundle/PdfGeneratorBundle/Builder/DompdfBuilder.php
@@ -2,10 +2,8 @@
 
 namespace Pim\Bundle\PdfGeneratorBundle\Builder;
 
-use Slik\DompdfBundle\Wrapper\DompdfWrapper;
-
 /**
- * PDF builder using DOMPDF wrapper
+ * PDF builder using DOMPDF library
  *
  * @author    Charles Pourcel <charles.pourcel@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
@@ -14,16 +12,21 @@ use Slik\DompdfBundle\Wrapper\DompdfWrapper;
 class DompdfBuilder implements PdfBuilderInterface
 {
     /**
-     * @var DompdfWrapper
+     * @var string
      */
-    protected $dompdfWrapper;
+    protected $rootDir;
 
     /**
-     * @param DompdfWrapper $dompdfWrapper
+     * @var DOMPDF
      */
-    public function __construct(DompdfWrapper $dompdfWrapper)
+    protected $dompdf;
+
+    /**
+     * @param string $rootDir
+     */
+    public function __construct($rootDir)
     {
-        $this->dompdfWrapper = $dompdfWrapper;
+        $this->rootDir = $rootDir;
     }
 
     /**
@@ -31,8 +34,41 @@ class DompdfBuilder implements PdfBuilderInterface
      */
     public function buildPdfOutput($htmlInput)
     {
-        $this->dompdfWrapper->getpdf($htmlInput);
+        $this->render($htmlInput);
 
-        return $this->dompdfWrapper->output();
+        return $this->output();
+    }
+
+    /**
+     * Render a pdf document
+     *
+     * @param  string $html    The html to be rendered
+     * @param  string $docname The name of the document to be served
+     */
+    protected function render($html)
+    {
+        // DOMPDF doesn't follow PSR convention, it uses classmap in autoload
+        define('DOMPDF_ENABLE_AUTOLOAD', false);
+        $filePath = $this->rootDir."/../vendor/dompdf/dompdf/dompdf_config.inc.php";
+        if (file_exists($filePath)) {
+            require_once($filePath);
+        } else {
+            throw new \LogicException('DomPDF cannot be loaded');
+        }
+
+        $this->dompdf = new \DOMPDF();
+        $this->dompdf->set_paper(DOMPDF_DEFAULT_PAPER_SIZE);
+        $this->dompdf->load_html($html);
+        $this->dompdf->render();
+    }
+
+    /**
+     * Get the raw pdf output
+     *
+     * @return string
+     */
+    protected function output()
+    {
+        return $this->dompdf->output();
     }
 } 
