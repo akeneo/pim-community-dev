dc379d4c552b5f37223c48417080b7296440d321
sumbobyboys <sumbobyboys@gmail.com>
2014-09-05 13:28:17 +0200

    add behat feature

---
+++ spec/Pim/Bundle/PdfGeneratorBundle/Controller/ProductControllerSpec.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace spec\Pim\Bundle\PdfGeneratorBundle\Controller;
+
+use Pim\Bundle\CatalogBundle\Manager\ProductManager;
+use Pim\Bundle\PdfGeneratorBundle\Renderer\RendererRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Pim\Bundle\CatalogBundle\Model\AbstractProduct;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ProductControllerSpec extends ObjectBehavior
+{
+    function let(ProductManager $productManager, RendererRegistry $rendererRegistry)
+    {
+        $this->beConstructedWith($productManager, $rendererRegistry);
+    }
+
+    function it_generates_a_pdf_for_a_given_product(Request $request, AbstractProduct $blender, $rendererRegistry, $productManager)
+    {
+        $productManager->find(12)->willReturn($blender);
+
+        $request->get('dataLocale', null)->willReturn('fr_FR');
+        $request->get('dataScope', null)->willReturn('mobile');
+
+        $now = new \DateTime('now');
+
+        $rendererRegistry->render($blender, 'full', [
+            'locale'        => 'fr_FR',
+            'renderingDate' => $now,
+            'scope'         => 'mobile',
+        ])->shouldBeCalled();
+
+        $blender->getIdentifier()->shouldBeCalled();
+
+        $this->generatePdfAction($request, 12);
+    }
+
+    function it_throws_an_exception_if_there_is_no_compatible_renderer(Request $request, AbstractProduct $blender, $rendererRegistry, $productManager)
+    {
+        $productManager->find(12)->willReturn($blender);
+
+        $request->get('dataLocale', null)->willReturn('fr_FR');
+        $request->get('dataScope', null)->willReturn('mobile');
+
+        $now = new \DateTime('now');
+
+        $rendererRegistry->render($blender, 'full', [
+            'locale'        => 'fr_FR',
+            'renderingDate' => $now,
+            'scope'         => 'mobile',
+        ])->willThrow('Pim\Bundle\PdfGeneratorBundle\Exception\RendererRequiredException');
+
+        $this->shouldThrow('Symfony\Component\HttpKernel\Exception\HttpException')->during('generatePdfAction', [$request, 12]);
+    }
+
+    function it_throws_an_exception_if_the_product_doesnt_exist(Request $request, AbstractProduct $blender, $rendererRegistry, $productManager)
+    {
+        $productManager->find(12)->willReturn(null);
+
+        $this->shouldThrow('Symfony\Component\HttpKernel\Exception\NotFoundHttpException')->during('generatePdfAction', [$request, 12]);
+    }
+}
