0a9680527976b1aa5bbf3339477ae0bab6329656
Olivier Soulet <olivier.soulet@akeneo.com>
2014-09-12 11:36:58 +0200

    PIM-3054: Added php specs for the data transformer

---
+++ spec/Pim/Bundle/EnrichBundle/Form/DataTransformer/StringToBooleanTransformerSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace spec\Pim\Bundle\EnrichBundle\Form\DataTransformer;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class StringToBooleanTransformerSpec extends ObjectBehavior
+{
+
+    function it_is_a_form_data_transformer()
+    {
+        $this->shouldImplement('Symfony\Component\Form\DataTransformerInterface');
+    }
+
+    function it_transforms_strings_into_boolean()
+    {
+        $this->transform('0')->shouldReturn(false);
+        $this->transform('1')->shouldReturn(true);
+    }
+
+    function it_reverse_transforms_strings_into_boolean()
+    {
+        $this->transform('1')->shouldReturn(true);
+        $this->transform('0')->shouldReturn(false);
+    }
+}
