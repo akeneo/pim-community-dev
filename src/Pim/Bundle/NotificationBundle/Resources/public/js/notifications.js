941f9c05e713b5281873b2d0afcfa7e825b97f4c
Filips Alpe <filips@akeneo.com>
2014-09-16 15:51:08 +0300

    Improve notification UI

---
+++ src/Pim/Bundle/NotificationBundle/Resources/public/js/notifications.js
@@ -0,0 +1,152 @@
+define(
+    ['backbone', 'jquery', 'underscore', 'routing', 'pim/notification-list', 'pim/indicator'],
+    function(Backbone, $, _, Routing) {
+        'use strict';
+
+        var NotificationList = require('pim/notification-list'),
+            Indicator = require('pim/indicator');
+
+        var Notifications = Backbone.View.extend({
+            el: '#notifications',
+
+            options: {
+                imgUrl:                 '',
+                loadingText:            null,
+                noNotificationsMessage: null,
+                markAsReadMessage:      null
+            },
+
+            freezeCount: false,
+
+            template: _.template(
+                [
+                    '<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">',
+                        '<i class="icon-bullhorn"></i>',
+                        '<span class="indicator"></span>',
+                    '</a>',
+                    '<ul class="dropdown-menu"></ul>'
+                ].join('')
+            ),
+
+            footerTemplate: _.template(
+                [
+                    '<p class="text-center unspaced">',
+                        '<% if (loading) { %>',
+                            '<img src="<%= options.imgUrl %>" alt="<%= options.loadingText %>" />',
+                        '<% } %>',
+                        '<% if (!loading && !hasNotifications && !hasMore) { %>',
+                            '<span><%= options.noNotificationsMessage %></span>',
+                        '<% } %>',
+                        '<% if (hasNotifications && hasUnread) { %>',
+                            '<button class="btn btn-mini pull-right mark-as-read"><%= options.markAsReadMessage %></button>',
+                        '<% } %>',
+                    '</p>'
+                ].join('')
+            ),
+
+            events: {
+                'click a.dropdown-toggle':   'onOpen',
+                'click button.mark-as-read': 'markAllAsRead'
+            },
+
+            markAllAsRead: function(e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                $.ajax({
+                    type: 'POST',
+                    url: Routing.generate('pim_notification_notification_mark_viewed', {id: 'all'}),
+                    async: true
+                });
+
+                this.collection.trigger('mark_as_read', 'all');
+                _.each(this.collection.models, function (model) {
+                    model.set('viewed', true);
+                });
+            },
+
+            initialize: function(opts) {
+                this.options = _.extend({}, this.options, opts);
+                this.collection = new NotificationList();
+                this.indicator  = new Indicator({el: this.$('span.indicator'), value: 0});
+
+                this.collection.on('load:unreadCount', function(count, reset) {
+                    if (this.freezeCount) {
+                        this.freezeCount = false;
+                        return;
+                    }
+                    if (this.indicator.get('value') != count) {
+                        this.indicator.set('value', count);
+                        if (reset) {
+                            this.collection.hasMore = true;
+                            this.collection.reset();
+                            this.renderFooter();
+                        }
+                    }
+                }, this);
+
+                this.collection.on('mark_as_read', function(id) {
+                    var value = 'all' === id ? 0 : this.indicator.get('value') - 1;
+                    this.indicator.set('value', value);
+                    if (0 === value) {
+                        this.renderFooter();
+                    }
+                    if ('all' !== id) {
+                        this.freezeCount = true;
+                    }
+                }, this);
+
+                this.collection.on('loading:start loading:finish remove', this.renderFooter, this);
+
+                this.indicator.on('change:value', function(model, value) {
+                    model.set('type', value > 0 ? 'important': '');
+                }, this);
+
+                this.render();
+            },
+
+            onOpen: function() {
+                if (!this.collection.length) {
+                    this.collection.loadNotifications();
+                }
+            },
+
+            render: function() {
+                this.setElement($('#notifications'));
+                this.$el.html(this.template());
+                this.collection.setElement(this.$('ul'));
+                this.indicator.setElement(this.$('span.indicator'));
+                this.renderFooter();
+            },
+
+            renderFooter: function () {
+                this.$('p').remove();
+
+                this.$('ul').append(
+                    this.footerTemplate({
+                        options:          this.options,
+                        loading:          this.collection.loading,
+                        hasNotifications: this.collection.length > 0,
+                        hasMore:          this.collection.hasMore,
+                        hasUnread:        this.indicator.get('value') > 0
+                    })
+                );
+            }
+        });
+
+        var notifications;
+
+        return {
+            init: function(options) {
+                if (notifications) {
+                    if (_.has(options, 'unreadCount')) {
+                        notifications.collection.trigger('load:unreadCount', options.unreadCount, true);
+                    }
+                    notifications.render();
+                } else {
+                    notifications = new Notifications(options);
+                }
+            }
+        };
+    }
+);
