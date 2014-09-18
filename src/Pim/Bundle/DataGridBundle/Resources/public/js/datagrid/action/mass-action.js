c4455cd4e0a8e87800958ab4943188ac16a3e115
sumbobyboys <sumbobyboys@gmail.com>
2014-09-18 12:07:28 +0200

    refactor some javascript

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/mass-action.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/mass-action.js
@@ -15,7 +15,8 @@ function(_, MassAction) {
          */
         getActionParameters: function() {
             var selectionState = this.datagrid.getSelectionState();
-            var collection = this.datagrid.collection;
+            var collection     = this.datagrid.collection;
+
             var idValues = _.map(selectionState.selectedModels, function(model) {
                 return model.get(this.identifierFieldName);
             }, this);
@@ -24,27 +25,7 @@ function(_, MassAction) {
                 values: idValues.join(',')
             };
 
-            if (collection.state != undefined) {
-                if (collection.state.parameters != undefined) {
-                    params['product-grid'] = {
-                        '_parameters': {
-                            view: {
-                                columns: collection.state.parameters.view.columns
-                            }
-                        }
-                    };
-                }
-
-                for (var key in collection.state.sorters) {
-                    if (params['product-grid'] == undefined)  {
-                        params['product-grid'] = {};
-                    }
-
-                    params['product-grid']['_sort_by'] = {};
-                    params['product-grid']['_sort_by'][key] = collection.state.sorters[key] === 1 ? 'DESC' : 'ASC';
-                }
-            }
-
+            params = this.getExtraParameters(params, collection.state)
             params = collection.processFiltersParams(params, null, 'filters');
 
             var locale = decodeURIComponent(this.datagrid.collection.url).split('dataLocale]=').pop();
@@ -54,23 +35,34 @@ function(_, MassAction) {
 
             return params;
         },
-        getExtraParameters: function
+        getExtraParameters: function(params, state) {
+            params['product-grid'] = {};
+
+            if (state != undefined) {
+                params['product-grid']['_parameters'] = this.getActiveSorters(state);
+                params['product-grid']['_sort_by']    = this.getActiveColumns(state);
+            }
+
+            return params;
+        },
         getActiveSorters: function(state) {
+            var result = {};
+
             if (state.parameters != undefined) {
-                return {
-                    '_parameters': {
-                        view: {
-                            columns: state.parameters.view.columns
-                        }
-                    }
+                result['view'] = {
+                    columns: state.parameters.view.columns
                 };
             }
+
+            return result;
         },
         getActiveColumns: function(state) {
             var result = {};
 
-            for (var sorterKey in state.sorters) {
-                result.push({'sorterKey': state.sorters[sorterKey] === 1 ? 'DESC' : 'ASC'});
+            if (state.sorters != undefined) {
+                for (var sorterKey in state.sorters) {
+                    result[sorterKey] = state.sorters[sorterKey] === 1 ? 'DESC' : 'ASC';
+                }
             }
 
             return result;
