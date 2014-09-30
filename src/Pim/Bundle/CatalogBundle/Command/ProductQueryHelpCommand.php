512d912d6417e929735da3593b1abd92f2c61191
Nicolas Dupont <nicolas@akeneo.com>
2014-09-30 14:03:32 +0200

    PIM-3197 : add dumpers services to extract logic from help command

--- src/Pim/Bundle/CatalogBundle/Command/ProductQueryHelpCommand.php
+++ src/Pim/Bundle/CatalogBundle/Command/ProductQueryHelpCommand.php
@@ -2,8 +2,7 @@
 
 namespace Pim\Bundle\CatalogBundle\Command;
 
-use Pim\Bundle\CatalogBundle\Entity\Repository\AttributeRepository;
-use Pim\Bundle\CatalogBundle\Doctrine\Query\ProductQueryFilterRegistryInterface;
+use Pim\Bundle\CatalogBundle\Doctrine\Query\DumperInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -32,87 +31,24 @@ class ProductQueryHelpCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->printFieldFilters($output);
-        $this->printAttributeFilters($output);
+        $helperSet = $this->getHelperSet();
+        $this->getFieldFilterDumper()->dump($output, $helperSet);
+        $this->getAttributeFilterDumper()->dump($output, $helperSet);
     }
 
     /**
-     * Print the field filters
-     *
-     * @param OutputInterface $output
+     * @return DumperInterface
      */
-    protected function printFieldFilters(OutputInterface $output)
+    protected function getFieldFilterDumper()
     {
-        $output->writeln("<info>Useable field filters...<info>");
-        $registry = $this->getFilterRegistry();
-        $fields = ['id', 'created', 'updated', 'enabled', 'completeness', 'family', 'groups'];
-        $rows = [];
-        foreach ($fields as $field) {
-            $filter = $registry->getFieldFilter($field);
-            if ($filter) {
-                $class = get_class($filter);
-                $operators = implode(', ', $filter->getOperators());
-            } else {
-                $class = 'Not supported';
-                $operators = '';
-            }
-            $rows[]= [$field, $class, $operators];
-        }
-        $table = $this->getHelper('table');
-        $headers = ['field', 'filter_class', 'operators'];
-        $table->setHeaders($headers)->setRows($rows);
-        $table->render($output);
+        return $this->getContainer()->get('pim_catalog.doctrine.query.field_filter_dumper');
     }
 
     /**
-     * Print the attribute filters
-     *
-     * @param OutputInterface $output
+     * @return DumperInterface
      */
-    protected function printAttributeFilters(OutputInterface $output)
+    protected function getAttributeFilterDumper()
     {
-        $output->writeln("<info>Useable attributes filters...<info>");
-        $registry = $this->getFilterRegistry();
-        $attributes = $this->getAttributeRepository()->findAll();
-        $rows = [];
-        foreach ($attributes as $attribute) {
-            $field = $attribute->getCode();
-            $filter = $registry->getAttributeFilter($attribute);
-            if ($filter) {
-                $class = get_class($filter);
-                $operators = implode(', ', $filter->getOperators());
-            } else {
-                $class = 'Not supported';
-                $operators = '';
-            }
-            $rows[]= [
-                $field,
-                $attribute->isLocalizable() ? 'yes' : 'no',
-                $attribute->isScopable() ? 'yes' : 'no',
-                $attribute->getAttributeType(),
-                $class,
-                $operators
-            ];
-        }
-        $table = $this->getHelper('table');
-        $headers = ['attribute', 'localizable', 'scopable', 'attribute type', 'filter_class', 'operators'];
-        $table->setHeaders($headers)->setRows($rows);
-        $table->render($output);
-    }
-
-    /**
-     * @return ProductQueryFilterRegistryInterface
-     */
-    protected function getFilterRegistry()
-    {
-        return $this->getContainer()->get('pim_catalog.doctrine.query.product_filter_registry');
-    }
-
-    /**
-     * @return AttributeRepository
-     */
-    protected function getAttributeRepository()
-    {
-        return $this->getContainer()->get('pim_catalog.repository.attribute');
+        return $this->getContainer()->get('pim_catalog.doctrine.query.attribute_filter_dumper');
     }
 }
