67e9b62b5f7e99208ac691f31c9cbb531a8714e4
Olivier Soulet <olivier.soulet@akeneo.com>
2014-09-30 10:04:18 +0200

    Added specs for filters
    *completeness filter
    *date filter
    *price filter

--- spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/CompletenessFilterSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/CompletenessFilterSpec.php
@@ -2,6 +2,8 @@
 
 namespace spec\Pim\Bundle\CatalogBundle\Doctrine\ORM\Filter;
 
+use Doctrine\ORM\EntityManager;
+use \Doctrine\ORM\Mapping\ClassMetadata;
 use PhpSpec\ObjectBehavior;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query\Expr;
@@ -28,5 +30,95 @@ class CompletenessFilterSpec extends ObjectBehavior
         $this->getOperators()->shouldReturn(['=', '<']);
         $this->supportsOperator('=')->shouldReturn(true);
         $this->supportsOperator('FAKE')->shouldReturn(false);
+    }
+
+    function it_adds_an_equal_filter_on_a_field_in_the_query(
+        QueryBuilder $qb,
+        Expr $expr,
+        EntityManager $em,
+        ClassMetadata $cm,
+        Expr\Comparison $comparison
+    ) {
+        $qb->expr()->willReturn($expr);
+        $qb->getRootAlias()->willReturn('p');
+        $qb->getRootEntities()->willReturn([]);
+        $qb->getEntityManager()->willReturn($em);
+        $em->getClassMetadata(false)->willReturn($cm);
+        $comparison->__toString()->willReturn('filterCompleteness.ratio = 100');
+        $cm->getAssociationMapping('completenesses')->willReturn(['targetEntity' => 'test']);
+        $expr->eq('filterCompleteness.ratio', '100')
+            ->shouldBeCalled()
+            ->willReturn($comparison);
+        $this->setQueryBuilder($qb);
+        $qb->leftJoin(
+            'PimCatalogBundle:Locale',
+            'filterCompletenessLocale',
+            'WITH',
+            'filterCompletenessLocale'.'.code = :dataLocale'
+        )->shouldBeCalled()->willReturn($qb);
+        $qb->leftJoin(
+            'PimCatalogBundle:Channel',
+            'filterCompletenessChannel',
+            'WITH',
+            'filterCompletenessChannel'.'.code = :scopeCode'
+        )->shouldBeCalled()->willReturn($qb);
+        $qb->leftJoin(
+            'test',
+            'filterCompleteness',
+            'WITH',
+            'filterCompleteness.locale = filterCompletenessLocale.id AND filterCompleteness.channel = filterCompletenessChannel.id AND filterCompleteness.product = p.id'
+        )->shouldBeCalled()->willReturn($qb);
+
+        $qb->andWhere('filterCompleteness.ratio = 100')->shouldBeCalled();
+
+        $this->addFieldFilter('completeness', '=', '100');
+    }
+
+    function it_adds_a_filter_on_a_field_in_the_query(
+        QueryBuilder $qb,
+        Expr $expr,
+        EntityManager $em,
+        ClassMetadata $cm,
+        Expr\Comparison $comparison
+    ) {
+        $qb->expr()->willReturn($expr);
+        $qb->getRootAlias()->willReturn('p');
+        $qb->getRootEntities()->willReturn([]);
+        $qb->getEntityManager()->willReturn($em);
+        $em->getClassMetadata(false)->willReturn($cm);
+        $comparison->__toString()->willReturn('filterCompleteness.ratio < 100');
+        $cm->getAssociationMapping('completenesses')->willReturn(['targetEntity' => 'test']);
+        $expr->lt('filterCompleteness.ratio', '100')
+            ->shouldBeCalled()
+            ->willReturn($comparison);
+        $this->setQueryBuilder($qb);
+        $qb->leftJoin(
+            'PimCatalogBundle:Locale',
+            'filterCompletenessLocale',
+            'WITH',
+            'filterCompletenessLocale'.'.code = :dataLocale'
+        )->shouldBeCalled()->willReturn($qb);
+        $qb->leftJoin(
+            'PimCatalogBundle:Channel',
+            'filterCompletenessChannel',
+            'WITH',
+            'filterCompletenessChannel'.'.code = :scopeCode'
+        )->shouldBeCalled()->willReturn($qb);
+        $qb->leftJoin(
+            'test',
+            'filterCompleteness',
+            'WITH',
+            'filterCompleteness.locale = filterCompletenessLocale.id AND filterCompleteness.channel = filterCompletenessChannel.id AND filterCompleteness.product = p.id'
+        )->shouldBeCalled()->willReturn($qb);
+
+        $qb->andWhere('filterCompleteness.ratio < 100')->shouldBeCalled();
+
+        $this->addFieldFilter('completeness', '<', '100');
+    }
+
+    function it_checks_if_field_is_supported()
+    {
+        $this->supportsField('completeness')->shouldReturn(true);
+        $this->supportsField('groups')->shouldReturn(false);
     }
 }
