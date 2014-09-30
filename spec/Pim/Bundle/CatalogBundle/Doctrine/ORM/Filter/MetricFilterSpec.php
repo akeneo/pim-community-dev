ed19c31b799492893fbc5396bb289f73c909e5a8
jjanvier <j.janvier@gmail.com>
2014-09-30 15:31:53 +0200

    PIM-3197: add specs to the filters

--- spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/MetricFilterSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/MetricFilterSpec.php
@@ -7,15 +7,16 @@ use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query\Expr;
 use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
 use Pim\Bundle\CatalogBundle\Context\CatalogContext;
+use Prophecy\Argument;
 
 class MetricFilterSpec extends ObjectBehavior
 {
-    function let(QueryBuilder $queryBuilder, CatalogContext $context)
+    function let(QueryBuilder $qb, CatalogContext $context)
     {
         $context->getLocaleCode()->willReturn('en_US');
         $context->getScopeCode()->willReturn('mobile');
         $this->beConstructedWith($context);
-        $this->setQueryBuilder($queryBuilder);
+        $this->setQueryBuilder($qb);
     }
 
     function it_is_a_filter()
@@ -28,5 +29,57 @@ class MetricFilterSpec extends ObjectBehavior
         $this->getOperators()->shouldReturn(['<', '<=', '=', '>=', '>', 'EMPTY']);
         $this->supportsOperator('=')->shouldReturn(true);
         $this->supportsOperator('FAKE')->shouldReturn(false);
+    }
+
+    function it_supports_metric_attribute(AbstractAttribute $attribute)
+    {
+        $attribute->getAttributeType()->willReturn('pim_catalog_metric');
+        $this->supportsAttribute($attribute)->shouldReturn(true);
+
+        $attribute->getAttributeType()->willReturn(Argument::any());
+        $this->supportsAttribute($attribute)->shouldReturn(false);
+    }
+
+    function it_adds_a_filter_to_the_query($qb, AbstractAttribute $attribute)
+    {
+        $attribute->getId()->willReturn(42);
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+        $attribute->getBackendType()->willReturn('metric');
+        $attribute->getCode()->willReturn('metric_code');
+
+        $qb->getRootAlias()->willReturn('r');
+        $qb->expr()->willReturn(new Expr());
+
+        $qb->innerJoin('r.values', 'filtermetric_code', 'WITH', 'filtermetric_code.attribute = 42')->shouldBeCalled();
+        $qb
+            ->innerJoin(
+                'filtermetric_code.metric',
+                'filterMmetric_code',
+                'WITH',
+                'filterMmetric_code.baseData = \'my_value\''
+            )
+            ->shouldBeCalled()
+        ;
+
+        $this->addAttributeFilter($attribute, '=', 'my_value');
+    }
+
+    function it_adds_an_empty_filter_to_the_query($qb, AbstractAttribute $attribute)
+    {
+        $attribute->getId()->willReturn(42);
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+        $attribute->getBackendType()->willReturn('metric');
+        $attribute->getCode()->willReturn('metric_code');
+
+        $qb->getRootAlias()->willReturn('r');
+        $qb->expr()->willReturn(new Expr());
+
+        $qb->leftJoin('r.values', 'filtermetric_code', 'WITH', 'filtermetric_code.attribute = 42')->shouldBeCalled();
+        $qb->leftJoin('filtermetric_code.metric', 'filterMmetric_code')->shouldBeCalled();
+        $qb->andWhere('filterMmetric_code.baseData IS NULL')->shouldBeCalled();
+
+        $this->addAttributeFilter($attribute, 'EMPTY', '');
     }
 }
--- spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/OptionFilterSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/OptionFilterSpec.php
@@ -6,6 +6,8 @@ use PhpSpec\ObjectBehavior;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query\Expr;
 use Pim\Bundle\CatalogBundle\Context\CatalogContext;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+use Prophecy\Argument;
 
 class OptionFilterSpec extends ObjectBehavior
 {
@@ -24,8 +26,90 @@ class OptionFilterSpec extends ObjectBehavior
 
     function it_supports_operators()
     {
-        $this->getOperators()->shouldReturn(['IN', 'NOT IN']);
+        $this->getOperators()->shouldReturn(['IN']);
         $this->supportsOperator('IN')->shouldReturn(true);
-        $this->supportsOperator('FAKE')->shouldReturn(false);
+        $this->supportsOperator(Argument::any())->shouldReturn(false);
+    }
+
+    function it_supports_simple_select_attribute(AbstractAttribute $attribute)
+    {
+        $attribute->getAttributeType()->willReturn('pim_catalog_simpleselect');
+        $this->supportsAttribute($attribute)->shouldReturn(true);
+
+        $attribute->getAttributeType()->willReturn(Argument::any());
+        $this->supportsAttribute($attribute)->shouldReturn(false);
+    }
+
+    function it_adds_a_filter_to_the_query($qb, AbstractAttribute $attribute)
+    {
+        $attribute->getId()->willReturn(42);
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+        $attribute->getBackendType()->willReturn('option');
+        $attribute->getCode()->willReturn('option_code');
+
+        $qb->getRootAlias()->willReturn('r');
+        $qb->expr()->willReturn(new Expr());
+
+        $qb->innerJoin(
+            'r.values',
+            'filteroption_code',
+            'WITH',
+            'filteroption_code.attribute = 42 AND ( filteroption_code.option IN(\'my_value1\', \'my_value2\') ) '
+        )->shouldBeCalled();
+
+        $this->addAttributeFilter($attribute, 'IN', ['my_value1', 'my_value2']);
+    }
+
+    function it_adds_an_empty_filter_to_the_query($qb, AbstractAttribute $attribute)
+    {
+        $attribute->getId()->willReturn(42);
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+        $attribute->getBackendType()->willReturn('option');
+        $attribute->getCode()->willReturn('option_code');
+
+        $qb->getRootAlias()->willReturn('r');
+        $qb->expr()->willReturn(new Expr());
+
+        $qb->leftJoin(
+            'r.values',
+            'filteroption_code',
+            'WITH',
+            'filteroption_code.attribute = 42'
+        )->shouldBeCalled();
+        $qb->andWhere('filteroption_code.option IS NULL')->shouldBeCalled();
+
+        $this->addAttributeFilter($attribute, 'IN', ['empty']);
+    }
+
+    function it_adds_an_empty_filter_and_another_filter_to_the_query($qb, AbstractAttribute $attribute)
+    {
+        $attribute->getId()->willReturn(42);
+        $attribute->isLocalizable()->willReturn(false);
+        $attribute->isScopable()->willReturn(false);
+        $attribute->getBackendType()->willReturn('option');
+        $attribute->getCode()->willReturn('option_code');
+
+        $qb->getRootAlias()->willReturn('r');
+        $qb->expr()->willReturn(new Expr());
+
+        $qb
+            ->leftJoin(
+                'r.values',
+                'filteroption_code',
+                'WITH',
+                'filteroption_code.attribute = 42'
+            )
+            ->shouldBeCalled()
+        ;
+        $qb
+            ->andWhere(
+                'filteroption_code.option IS NULL OR filteroption_code.option IN(\'my_value2\', \'my_value3\')'
+            )
+            ->shouldBeCalled()
+        ;
+
+        $this->addAttributeFilter($attribute, 'IN', ['empty', 'my_value2', 'my_value3']);
     }
 }
