30b9840ec0f73ae70b5bec828c83588ed292d052
jjanvier <j.janvier@gmail.com>
2014-10-14 09:31:58 +0200

    PIM-3243 : Clean Kernel to not depend on Oro's one

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,14 +1,22 @@
 <?php
 
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2014 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * PIM AppKernel
  *
- * @author    Nicolas Dupont <nicolas@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author Nicolas Dupont <nicolas@akeneo.com>
  */
 class AppKernel extends Kernel
 {
