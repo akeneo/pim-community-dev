318c13a8b9c0faf182bf09e32422f42f69388686
sumbobyboys <sumbobyboys@gmail.com>
2014-10-14 17:45:12 +0200

    PIM-3279: Add an ajax filter
    Conflicts:
    	CHANGELOG.md
    	spec/Pim/Bundle/FilterBundle/Filter/Product/GroupsFilterSpec.php

--- spec/Pim/Bundle/FilterBundle/Filter/Product/GroupsFilterSpec.php
+++ spec/Pim/Bundle/FilterBundle/Filter/Product/GroupsFilterSpec.php
@@ -2,17 +2,17 @@
 
 namespace spec\Pim\Bundle\FilterBundle\Filter\Product;
 
-use Doctrine\ORM\QueryBuilder;
 use PhpSpec\ObjectBehavior;
 use Pim\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
 use Pim\Bundle\FilterBundle\Filter\ProductFilterUtility;
 use Symfony\Component\Form\FormFactoryInterface;
+use Pim\Bundle\UserBundle\Context\UserContext;
 
 class GroupsFilterSpec extends ObjectBehavior
 {
-    function let(FormFactoryInterface $factory, ProductFilterUtility $utility)
+    function let(FormFactoryInterface $factory, ProductFilterUtility $utility, UserContext $userContext)
     {
-        $this->beConstructedWith($factory, $utility);
+        $this->beConstructedWith($factory, $utility, $userContext, 'Group');
     }
 
     function it_is_an_oro_choice_filter()
