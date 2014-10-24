62e0c22b90a625d2084d7fba279bb033a8338687
Nicolas Dupont <nicolas@akeneo.com>
2014-10-24 14:27:47 +0200

    Prepare the release v1.0.3

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.0.x';
+    const VERSION = '1.0.3';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Dandelion';
