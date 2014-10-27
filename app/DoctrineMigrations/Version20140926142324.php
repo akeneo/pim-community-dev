00aa6c5ce5a5ce8513e42ed902b9056a10a43cc3
Filips Alpe <filips@akeneo.com>
2014-10-27 17:49:45 +0200

    PIM-3255 Remove hardcoded table names for migrations

--- app/DoctrineMigrations/Version20140926142324.php
+++ app/DoctrineMigrations/Version20140926142324.php
@@ -4,6 +4,8 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Migration 1.2 => 1.3
@@ -12,21 +14,33 @@ use Doctrine\DBAL\Schema\Schema;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Version20140926142324 extends AbstractMigration
+class Version20140926142324 extends AbstractMigration implements ContainerAwareInterface
 {
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE pim_catalog_attribute DROP useable_as_grid_column');
+
+        $this->addSql(sprintf('ALTER TABLE %s DROP useable_as_grid_column', $this->getTableName()));
     }
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE pim_catalog_attribute ADD useable_as_grid_column TINYINT(1) DEFAULT \'0\' NOT NULL');
+
+        $this->addSql(sprintf('ALTER TABLE %s ADD useable_as_grid_column TINYINT(1) DEFAULT \'0\' NOT NULL', $this->getTableName()));
+    }
+
+    protected function getTableName()
+    {
+        $class = $this->container->getParameter('pim_catalog.entity.attribute.class');
+
+        return $this->container->get('doctrine.orm.entity_manager')->getClassMetadata($class)->getTableName();
     }
 }
--- app/DoctrineMigrations/Version20141027152640.php
+++ app/DoctrineMigrations/Version20141027152640.php
@@ -4,6 +4,8 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Migration 1.2 => 1.3
@@ -12,19 +14,33 @@ use Doctrine\DBAL\Schema\Schema;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Version20141027152640 extends AbstractMigration
+class Version20141027152640 extends AbstractMigration implements ContainerAwareInterface
 {
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE pim_catalog_attribute DROP default_value');
+        $this->addSql(sprintf('ALTER TABLE %s DROP default_value', $this->getTableName()));
     }
 
     public function down(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE pim_catalog_attribute ADD default_value LONGTEXT DEFAULT NULL');
+        $this->addSql(sprintf('ALTER TABLE %s ADD default_value LONGTEXT DEFAULT NULL', $this->getTableName()));
+    }
+
+    protected function getTableName()
+    {
+        $class = $this->container->getParameter('pim_catalog.entity.attribute.class');
+
+        return $this->container->get('doctrine.orm.entity_manager')->getClassMetadata($class)->getTableName();
     }
 }
