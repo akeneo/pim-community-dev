be790dbf4f31c7fe6c156210f4cdaede26b8012c
jjanvier <j.janvier@gmail.com>
2014-10-31 13:56:05 +0100

    Fix static analysis

--- src/Pim/Bundle/TransformBundle/Tests/Unit/Normalizer/Structured/AttributeNormalizerTest.php
+++ src/Pim/Bundle/TransformBundle/Tests/Unit/Normalizer/Structured/AttributeNormalizerTest.php
@@ -64,7 +64,7 @@ class AttributeNormalizerTest extends NormalizerTestCase
             $this->normalizer->normalize(
                 $attribute,
                 $this->format,
-                array('versioning' => true, 'locales' => ['en_US', 'fr_FR'])
+                ['versioning' => true, 'locales' => ['en_US', 'fr_FR']]
             )
         );
     }
