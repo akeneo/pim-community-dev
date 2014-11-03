7872b889b2259167a92a11f3b6e0a63328fa27fc
Nicolas Dupont <nicolas@akeneo.com>
2014-11-03 19:52:57 +0100

    PIM-3298 : in product edit, don't display a value with a locale specific property on its attribute

---
+++ src/Pim/Bundle/EnrichBundle/Form/Subscriber/FilterLocaleSpecificValueSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Form\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+
+/**
+ * Filter locale specific value subscriber to remove value available in only a set of locales
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FilterLocaleSpecificValueSubscriber implements EventSubscriberInterface
+{
+    /** @var string $currentLocale */
+    protected $currentLocale;
+
+    /**
+     * @param string $currentLocale
+     */
+    public function __construct($currentLocale)
+    {
+        $this->currentLocale = $currentLocale;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::PRE_SET_DATA => 'preSetData',
+        );
+    }
+
+    /**
+     * @param FormEvent $event
+     *
+     * @return null
+     */
+    public function preSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data) {
+            return;
+        }
+
+        foreach ($data as $name => $value) {
+            if ($this->currentLocale && $value->getAttribute()->getAvailableLocales()) {
+                // TODO : should be provided by Attribute itself, we'll do in 1.3 to avoid BC Break
+                $availableCodes = $value->getAttribute()->getAvailableLocales()->map(
+                    function ($locale) {
+                        return $locale->getCode();
+                    }
+                )->toArray();
+                if (!in_array($this->currentLocale, $availableCodes)) {
+                    $form->remove($name);
+                }
+            }
+        }
+    }
+}
