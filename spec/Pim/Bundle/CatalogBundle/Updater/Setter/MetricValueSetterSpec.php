37598a9f307fbec85b9cac74ff647370903dd3f7
Olivier Soulet <olivier.soulet@akeneo.com>
2014-11-05 18:36:00 +0100

    PIM-3321: added some specs and value checks in setter and copier

---
+++ spec/Pim/Bundle/CatalogBundle/Updater/Setter/MetricValueSetterSpec.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace spec\Pim\Bundle\CatalogBundle\Updater\Setter;
+
+use Akeneo\Bundle\MeasureBundle\Manager\MeasureManager;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Builder\ProductBuilder;
+use Pim\Bundle\CatalogBundle\Factory\MetricFactory;
+use Pim\Bundle\CatalogBundle\Model\AbstractProduct;
+use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+use Pim\Bundle\CatalogBundle\Model\MetricInterface;
+use Pim\Bundle\CatalogBundle\Model\ProductValue;
+
+class MetricValueSetterSpec extends ObjectBehavior
+{
+    function let(ProductBuilder $builder, MetricFactory $factory, MeasureManager $measureManager)
+    {
+        $this->beConstructedWith($builder, $factory, $measureManager);
+    }
+
+    function it_is_a_setter()
+    {
+        $this->shouldImplement('Pim\Bundle\CatalogBundle\Updater\Setter\SetterInterface');
+    }
+
+    function it_supports_metric_attributes(
+        AttributeInterface $metrictAttribute,
+        AttributeInterface $textareaAttribute
+    ) {
+        $metrictAttribute->getAttributeType()->willReturn('pim_catalog_metric');
+        $this->supports($metrictAttribute)->shouldReturn(true);
+
+        $textareaAttribute->getAttributeType()->willReturn('pim_catalog_textarea');
+        $this->supports($textareaAttribute)->shouldReturn(false);
+    }
+
+    function it_throw_an_error_if_data_is_not_array(
+        AttributeInterface $attribute
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+
+        $data = 'Not an array';
+
+        $this->shouldThrow(
+            new \LogicException('$data have to be an array')
+        )->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_throw_an_error_if_there_is_no_data_key_in_array(
+        AttributeInterface $attribute
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+
+        $data = ['unit' => 'KILOGRAM'];
+
+        $this->shouldThrow(
+            new \LogicException('Missing "data" key in array')
+        )->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_throw_an_error_if_there_is_no_unit_key_in_array(
+        AttributeInterface $attribute
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+
+        $data = ['data' => 'data value'];
+
+        $this->shouldThrow(
+            new \LogicException('Missing "unit" key in array')
+        )->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_throw_an_error_if_data_is_not_a_number(
+        AttributeInterface $attribute
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+
+        $data = ['data' => 'text', 'unit' => 'KG'];
+
+        $this->shouldThrow(
+            new \LogicException('Attribute "attributeCode" expects a numeric data')
+        )->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_throw_an_error_if_unit_is_not_a_string(
+        AttributeInterface $attribute
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+
+        $data = ['data' => 42, 'unit' => 123];
+
+        $this->shouldThrow(new \LogicException('Attribute "attributeCode" expects a string unit'))->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_throw_an_error_if_unit_does_not_exist(
+        AttributeInterface $attribute,
+        $measureManager
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+        $attribute->getMetricFamily()->willReturn('Weight');
+
+        $data = ['data' => 42, 'unit' => 'incorrect unit'];
+
+        $measureManager->unitExistInFamily($data['unit'], 'Weight')->shouldBeCalled()->willReturn(false);
+
+        $this->shouldThrow(new \LogicException('"incorrect unit" does not exist in any attribute\'s families'))->during('setValue', [[], $attribute, $data, $locale, $scope]);
+    }
+
+    function it_sets_numeric_value_to_a_product_value(
+        AttributeInterface $attribute,
+        AbstractProduct $product1,
+        AbstractProduct $product2,
+        AbstractProduct $product3,
+        $builder,
+        $measureManager,
+        $factory,
+        MetricInterface $metric,
+        ProductValue $productValue
+    ) {
+        $locale = 'fr_FR';
+        $scope = 'mobile';
+        $data = ['data' => 107, 'unit' => 'KILOGRAM'];
+
+        $attribute->isLocalizable()->shouldBeCalled()->willReturn(true);
+        $attribute->isScopable()->shouldBeCalled()->willReturn(true);
+        $attribute->getCode()->willReturn('attributeCode');
+        $attribute->getMetricFamily()->willReturn('Weight');
+
+        $measureManager->unitExistInFamily($data['unit'], 'Weight')->shouldBeCalled()->willReturn(true);
+
+        $productValue->getMetric()->willReturn(null);
+        $productValue->setMetric($metric)->shouldBeCalled();
+
+        $metric->setUnit($data['unit'])->shouldBeCalled();
+        $metric->setData($data['data'])->shouldBeCalled();
+
+        $builder
+            ->addProductValue($product2, $attribute, $locale, $scope)
+            ->willReturn($productValue);
+
+        $factory->createMetric('Weight')->shouldBeCalled()->willReturn($metric);
+
+        $product1->getValue('attributeCode', $locale, $scope)->shouldBeCalled()->willReturn($productValue);
+        $product2->getValue('attributeCode', $locale, $scope)->willReturn(null);
+        $product3->getValue('attributeCode', $locale, $scope)->willReturn($productValue);
+
+        $products = [$product1, $product2, $product3];
+
+        $this->setValue($products, $attribute, $data, $locale, $scope);
+    }
+}
--- src/Pim/Bundle/CatalogBundle/Updater/Setter/MetricValueSetter.php
+++ src/Pim/Bundle/CatalogBundle/Updater/Setter/MetricValueSetter.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Bundle\CatalogBundle\Updater\Setter;
 
+use Akeneo\Bundle\MeasureBundle\Manager\MeasureManager;
 use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
 use Pim\Bundle\CatalogBundle\Builder\ProductBuilder;
 use Pim\Bundle\CatalogBundle\Updater\Util\AttributeUtility;
@@ -22,29 +23,57 @@ class MetricValueSetter implements SetterInterface
     /** @var MetricFactory */
     protected $factory;
 
+    /** @var MeasureManager */
+    protected $measureManager;
+
     /**
      * @param ProductBuilder $builder
      * @param MetricFactory  $factory
      */
-    public function __construct(ProductBuilder $builder, MetricFactory $factory)
+    public function __construct(ProductBuilder $builder, MetricFactory $factory, MeasureManager $measureManager)
     {
         $this->productBuilder = $builder;
         $this->factory = $factory;
+        $this->measureManager = $measureManager;
     }
 
     /**
      * {@inheritdoc}
      *
-     * setValue( '12 KG'
-     *           ['data' => 12, 'unit' => 'KG']
+     * setValue( '12 KILOGRAM'
+     *           ['data' => 12, 'unit' => 'KILOGRAM']
      */
     public function setValue(array $products, AttributeInterface $attribute, $data, $locale = null, $scope = null)
     {
+        //TODO: pmd is to hight need to split the method
         AttributeUtility::validateLocale($attribute, $locale);
         AttributeUtility::validateScope($attribute, $scope);
 
-        // TODO : check the unit belongs to the family
-        // TODO : check the data is a number
+        if (!is_array($data)) {
+            throw new \LogicException('$data have to be an array');
+        }
+
+        if (!array_key_exists('data', $data)) {
+            throw new \LogicException('Missing "data" key in array');
+        }
+
+        if (!array_key_exists('unit', $data)) {
+            throw new \LogicException('Missing "unit" key in array');
+        }
+
+        if (!is_numeric($data['data'])) {
+            throw new \LogicException(sprintf('Attribute "%s" expects a numeric data', $attribute->getCode()));
+        }
+
+        if (!is_string($data['unit'])) {
+            throw new \LogicException(sprintf('Attribute "%s" expects a string unit', $attribute->getCode()));
+        }
+
+        if (!$this->measureManager->unitExistInFamily($data['unit'], $attribute->getMetricFamily())) {
+            throw new \LogicException(
+                sprintf('"%s" does not exist in any attribute\'s families', $data['unit'])
+            );
+        }
 
         $unit = $data['unit'];
         $data = $data['data'];
@@ -54,12 +83,14 @@ class MetricValueSetter implements SetterInterface
             if (null === $value) {
                 $value = $this->productBuilder->addProductValue($product, $attribute, $locale, $scope);
             }
+            //Todo: change this cases because there is a case where metric is not setted
             if (null === $metric = $value->getMetric()) {
                 $metric = $this->factory->createMetric($attribute->getMetricFamily());
+                $metric->setUnit($unit);
+                $metric->setData($data);
+
                 $value->setMetric($metric);
             }
-            $metric->setUnit($unit);
-            $metric->setData($data);
         }
     }
 
