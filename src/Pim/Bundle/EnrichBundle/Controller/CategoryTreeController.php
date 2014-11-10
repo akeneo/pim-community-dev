99f108a6cde258f111772e7de051146f1c083edc
Nicolas Dupont <nicolas@akeneo.com>
2014-11-10 14:55:45 +0100

    PIM-3325 : category manager implements saver interface

--- src/Pim/Bundle/EnrichBundle/Controller/CategoryTreeController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/CategoryTreeController.php
@@ -132,12 +132,7 @@ class CategoryTreeController extends AbstractDoctrineController
         $parentId      = $request->get('parent');
         $prevSiblingId = $request->get('prev_sibling');
 
-        if ($request->get('copy') == 1) {
-            $this->categoryManager->copy($categoryId, $parentId, $prevSiblingId);
-        } else {
-            $this->categoryManager->move($categoryId, $parentId, $prevSiblingId);
-        }
-        $this->categoryManager->getObjectManager()->flush();
+        $this->categoryManager->move($categoryId, $parentId, $prevSiblingId);
 
         return new JsonResponse(array('status' => 1));
     }
@@ -238,7 +233,7 @@ class CategoryTreeController extends AbstractDoctrineController
             $form->bind($request);
 
             if ($form->isValid()) {
-                $this->persist($category, true);
+                $this->categoryManager->save($category);
                 $this->addFlash('success', sprintf('flash.%s.created', $category->getParent() ? 'category' : 'tree'));
                 $this->eventDispatcher->dispatch(CategoryEvents::POST_CREATE, new GenericEvent($category));
 
@@ -273,7 +268,7 @@ class CategoryTreeController extends AbstractDoctrineController
             $form->bind($request);
 
             if ($form->isValid()) {
-                $this->persist($category, true);
+                $this->categoryManager->save($category);
                 $this->addFlash('success', sprintf('flash.%s.updated', $category->getParent() ? 'category' : 'tree'));
                 $this->eventDispatcher->dispatch(CategoryEvents::POST_EDIT, new GenericEvent($category));
             }
