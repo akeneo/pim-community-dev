1b696c5ab2d3ad8fa73dd14c67453f3e18ddb809
Nicolas Dupont <nicolas@akeneo.com>
2014-11-10 14:16:45 +0100

    PIM-3325 : normalize association type manager

--- spec/Pim/Bundle/CatalogBundle/Manager/AssociationTypeManagerSpec.php
+++ spec/Pim/Bundle/CatalogBundle/Manager/AssociationTypeManagerSpec.php
@@ -20,6 +20,16 @@ class AssociationTypeManagerSpec extends ObjectBehavior
         $this->beConstructedWith($repository, $objectManager, $eventDispatcher);
     }
 
+    function it_is_a_saver()
+    {
+        $this->shouldImplement('Pim\Component\Resource\Model\SaverInterface');
+    }
+
+    function it_is_a_remover()
+    {
+        $this->shouldImplement('Pim\Component\Resource\Model\RemoverInterface');
+    }
+
     function it_provides_all_association_types($repository)
     {
         $repository->findAll()->willReturn(['foo', 'bar']);
@@ -41,5 +51,35 @@ class AssociationTypeManagerSpec extends ObjectBehavior
         $objectManager->flush()->shouldBeCalled();
 
         $this->remove($associationType);
+    }
+
+    function it_throws_exception_when_save_anything_else_than_an_assoccation_type()
+    {
+        $anythingElse = new \stdClass();
+        $this
+            ->shouldThrow(
+                new \InvalidArgumentException(
+                    sprintf(
+                        'Expects an AssociationType, "%s" provided',
+                        get_class($anythingElse)
+                    )
+                )
+            )
+            ->duringSave($anythingElse);
+    }
+
+    function it_throws_exception_when_remove_anything_else_than_an_assocation_type()
+    {
+        $anythingElse = new \stdClass();
+        $this
+            ->shouldThrow(
+                new \InvalidArgumentException(
+                    sprintf(
+                        'Expects an AssociationType, "%s" provided',
+                        get_class($anythingElse)
+                    )
+                )
+            )
+            ->duringRemove($anythingElse);
     }
 }
