4b861cfaecc94ba99d0c74fae08a35bfbcc63197
Filips Alpe <filips@akeneo.com>
2014-11-15 20:57:09 +0200

    PIM-3196 Improve the view element registry

--- src/Pim/Bundle/EnrichBundle/DependencyInjection/Compiler/RegisterViewElementsPass.php
+++ src/Pim/Bundle/EnrichBundle/DependencyInjection/Compiler/RegisterViewElementsPass.php
@@ -2,12 +2,13 @@
 
 namespace Pim\Bundle\EnrichBundle\DependencyInjection\Compiler;
 
+use Pim\Bundle\EnrichBundle\DependencyInjection\Reference\ReferenceFactory;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
- * Compiler pass to register tagged views in the view registry
+ * Compiler pass to register tagged view elements in the view element registry
  *
  * @author    Julien Sanchez <julien@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
@@ -15,14 +16,25 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class RegisterViewElementsPass implements CompilerPassInterface
 {
-    /** @staticvar int The default position for services */
+    /** @staticvar int The default view element position */
     const DEFAULT_POSITION = 100;
 
-    /** @staticvar int The registry id */
+    /** @staticvar string The registry id */
     const REGISTRY_ID = 'pim_enrich.view_element.registry';
 
     /** @staticvar string */
-    const TAB_TAG = 'pim_enrich_view_element.tab';
+    const VIEW_ELEMENT_TAG = 'pim_enrich.view_element';
+
+    /** @var ReferenceFactory */
+    protected $factory;
+
+    /**
+     * @param ReferenceFactory $factory
+     */
+    public function __construct(ReferenceFactory $factory = null)
+    {
+        $this->factory = $factory ?: new ReferenceFactory();
+    }
 
     /**
      * {@inheritdoc}
@@ -30,47 +42,26 @@ class RegisterViewElementsPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if (!$container->hasDefinition(static::REGISTRY_ID)) {
-            throw new \LogicException(
-                sprintf('Resolver "%s" is called on an incorrect registry service id', get_class($this))
-            );
+            return;
         }
 
-        $views = [];
-        $views['tab'] = $this->findAndSortTaggedServices(static::TAB_TAG, $container);
+        $definition = $container->getDefinition(static::REGISTRY_ID);
 
-        $container->getDefinition(static::REGISTRY_ID)->addMethodCall('setViews', [$views]);
-    }
-
-    /**
-     * Returns an array of service references for a specified tag name
-     *
-     * @param string           $tagName
-     * @param ContainerBuilder $container
-     *
-     * @return \Symfony\Component\DependencyInjection\Reference[]
-     */
-    protected function findAndSortTaggedServices($tagName, ContainerBuilder $container)
-    {
-        $services = $container->findTaggedServiceIds($tagName);
-
-        $sortedServices = [];
-        foreach ($services as $serviceId => $tags) {
+        foreach ($container->findTaggedServiceIds(static::VIEW_ELEMENT_TAG) as $serviceId => $tags) {
             foreach ($tags as $tag) {
-                $position = isset($tag['position']) ? $tag['position'] : self::DEFAULT_POSITION;
-
-                if (!isset($sortedServices[$tag['identifier']])) {
-                    $sortedServices[$tag['identifier']] = [];
+                if (!isset($tag['type'])) {
+                    throw new \LogicException(sprintf('No type provided for the "%s" view element', $serviceId));
                 }
-
-                $sortedServices[$tag['identifier']][$position][] = new Reference($serviceId);
+                $position = isset($tag['position']) ? $tag['position'] : static::DEFAULT_POSITION;
+                $definition->addMethodCall(
+                    'add',
+                    [
+                        $this->factory->createReference($serviceId),
+                        $tag['type'],
+                        $position
+                    ]
+                );
             }
         }
-
-        foreach ($sortedServices as $identifier => $services) {
-            ksort($sortedServices[$identifier]);
-            $sortedServices[$identifier] = call_user_func_array('array_merge', $sortedServices[$identifier]);
-        }
-
-        return $sortedServices;
     }
 }
