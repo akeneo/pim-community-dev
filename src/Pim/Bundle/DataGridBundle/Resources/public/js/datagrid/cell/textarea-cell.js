c2f37c7783876109a5e4916c2815e2340e16031e
willy-ahva <willy.mesnage@akeneo.com>
2014-11-17 10:35:23 +0100

    Verify if description text is defined to display popover

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/textarea-cell.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/textarea-cell.js
@@ -5,20 +5,24 @@ function(StringCell) {
 
     return StringCell.extend({
         render: function() {
-            this.$el.empty();
-            this.$el.html('<span id="' + this.model.get("sku") + '">' + this.formatter.fromRaw(this.model.get(this.column.get("name"))).text.substring(0, 40) + ' ... </span>');
-            this.delegateEvents();
+            var description = this.formatter.fromRaw(this.model.get(this.column.get("name")));
 
-            this.$el.popover({
-                title: this.formatter.fromRaw(this.column.get('label')),
-                content: this.formatter.fromRaw(this.model.get(this.column.get('name'))).text,
-                delay: {
-                    show: 500,
-                    hide: 100
-                },
-                selector: '#' + this.model.get("sku"),
-                trigger: 'hover'
-            });
+            if (description.length != 0) {
+                this.$el.empty();
+                this.$el.html('<span id="' + this.model.get("sku") + '">' + description.substring(0, 40) + ' ... </span>');
+                this.delegateEvents();
+
+                this.$el.popover({
+                    title: this.formatter.fromRaw(this.column.get('label')),
+                    content: description,
+                    delay: {
+                        show: 500,
+                        hide: 100
+                    },
+                    selector: '#' + this.model.get("sku"),
+                    trigger: 'hover'
+                });
+            }
 
             return this;
         }
