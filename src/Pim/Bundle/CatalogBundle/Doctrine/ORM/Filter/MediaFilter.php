eec087280d11fec80aa965c0599541a7eb96d947
Nicolas Dupont <nicolas@akeneo.com>
2014-11-19 10:36:10 +0100

    PIM-2438 : add a first implem of media filter based on string filter

---
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Filter/MediaFilter.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Doctrine\ORM\Filter;
+
+use Pim\Bundle\CatalogBundle\Doctrine\Query\Operators;
+use Pim\Bundle\CatalogBundle\Doctrine\Query\AttributeFilterInterface;
+use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+
+/**
+ * Media filter
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MediaFilter extends AbstractFilter implements AttributeFilterInterface
+{
+    /** @var array */
+    protected $supportedAttributes;
+
+    /**
+     * Instanciate the base filter
+     *
+     * @param array $supportedAttributes
+     * @param array $supportedOperators
+     */
+    public function __construct(
+        array $supportedAttributes = [],
+        array $supportedOperators = []
+    ) {
+        $this->supportedAttributes = $supportedAttributes;
+        $this->supportedOperators  = $supportedOperators;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAttributeFilter(AttributeInterface $attribute, $operator, $value, $locale = null, $scope = null)
+    {
+        $joinAlias = 'filter'.$attribute->getCode();
+        $backendField = sprintf('%s.%s', $joinAlias, $attribute->getBackendType());
+
+        if ($operator === Operators::IS_EMPTY) {
+            $this->qb->leftJoin(
+                $this->qb->getRootAlias().'.values',
+                $joinAlias,
+                'WITH',
+                $this->prepareAttributeJoinCondition($attribute, $joinAlias, $locale, $scope)
+            );
+            $this->qb->andWhere($this->prepareCriteriaCondition($backendField, $operator, $value));
+        } else {
+            $condition = $this->prepareAttributeJoinCondition($attribute, $joinAlias, $locale, $scope);
+            $condition .= ' AND ' . $this->prepareCondition($backendField, $operator, $value);
+
+            $this->qb->innerJoin(
+                $this->qb->getRootAlias() . '.values',
+                $joinAlias,
+                'WITH',
+                $condition
+            );
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsAttribute(AttributeInterface $attribute)
+    {
+        return in_array($attribute->getAttributeType(), $this->supportedAttributes);
+    }
+
+    /**
+     * Prepare conditions of the filter
+     * @param string|array $operator
+     * @param string|array $value
+     *
+     * @return string
+     */
+    protected function prepareCondition($backendField, $operator, $value)
+    {
+        switch ($operator) {
+            case Operators::STARTS_WITH:
+                $operator = 'LIKE';
+                $value    = $value . '%';
+                break;
+            case Operators::ENDS_WITH:
+                $operator = 'LIKE';
+                $value    = '%' . $value;
+                break;
+            case Operators::CONTAINS:
+                $operator = 'LIKE';
+                $value    = '%' . $value . '%';
+                break;
+            case Operators::DOES_NOT_CONTAIN:
+                $operator = 'NOT LIKE';
+                $value    = '%' . $value . '%';
+                break;
+            default:
+                break;
+        }
+
+        return $this->prepareCriteriaCondition($backendField, $operator, $value);
+    }
+}
