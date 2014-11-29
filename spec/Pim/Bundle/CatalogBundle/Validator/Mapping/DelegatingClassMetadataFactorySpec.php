71d6f9264da5fe325d2b681310e3d385d810172c
Nicolas Dupont <nicolas@akeneo.com>
2014-11-29 10:20:29 +0100

    PIM-3371 : re-work specs for delegating metadata factory

--- spec/Pim/Bundle/CatalogBundle/Validator/Mapping/DelegatingClassMetadataFactorySpec.php
+++ spec/Pim/Bundle/CatalogBundle/Validator/Mapping/DelegatingClassMetadataFactorySpec.php
@@ -11,11 +11,11 @@ use Symfony\Component\Validator\Exception\NoSuchMetadataException;
 class DelegatingClassMetadataFactorySpec extends ObjectBehavior
 {
     function let(
-        MetadataFactoryInterface $dupond,
-        MetadataFactoryInterface $dupont
+        MetadataFactoryInterface $customFactory,
+        MetadataFactoryInterface $defaultFactory
     ) {
-        $this->addMetadataFactory($dupond);
-        $this->addMetadataFactory($dupont);
+        $this->addMetadataFactory($customFactory);
+        $this->addMetadataFactory($defaultFactory);
     }
 
     function it_is_a_validator_metadata_factory()
@@ -25,24 +25,24 @@ class DelegatingClassMetadataFactorySpec extends ObjectBehavior
 
     function it_delegate_metadata_creation_to_the_first_factory_having_one_for_an_object(
         $object,
-        $dupond,
-        $dupont,
+        $customFactory,
+        $defaultFactory,
         ClassMetadata $metadata
     ) {
-        $dupond->hasMetadataFor($object)->willReturn(false);
-        $dupont->hasMetadataFor($object)->willReturn(true);
-        $dupont->getMetadataFor($object)->willReturn($metadata);
+        $customFactory->hasMetadataFor($object)->willReturn(false);
+        $defaultFactory->hasMetadataFor($object)->willReturn(true);
+        $defaultFactory->getMetadataFor($object)->willReturn($metadata);
 
         $this->getMetadataFor($object)->shouldReturn($metadata);
     }
 
    function its_getMetadataFor_method_throws_exception_when_no_factory_has_metadata_for_object(
         $object,
-        $dupond,
-        $dupont
+        $customFactory,
+        $defaultFactory
     ) {
-        $dupond->hasMetadataFor($object)->willReturn(false);
-        $dupont->hasMetadataFor($object)->willReturn(false);
+        $customFactory->hasMetadataFor($object)->willReturn(false);
+        $defaultFactory->hasMetadataFor($object)->willReturn(false);
 
         $this
             ->shouldThrow(new NoSuchMetadataException())
@@ -51,22 +51,22 @@ class DelegatingClassMetadataFactorySpec extends ObjectBehavior
 
     function it_has_metadata_if_at_least_one_factory_has_one(
         $object,
-        $dupond,
-        $dupont
+        $customFactory,
+        $defaultFactory
     ) {
-        $dupond->hasMetadataFor($object)->willReturn(false);
-        $dupont->hasMetadataFor($object)->willReturn(true);
+        $customFactory->hasMetadataFor($object)->willReturn(false);
+        $defaultFactory->hasMetadataFor($object)->willReturn(true);
 
         $this->hasMetadataFor($object)->shouldReturn(true);
     }
 
     function it_does_not_have_metadata_if_no_factory_has_one(
         $object,
-        $dupond,
-        $dupont
+        $customFactory,
+        $defaultFactory
     ) {
-        $dupond->hasMetadataFor($object)->willReturn(false);
-        $dupont->hasMetadataFor($object)->willReturn(false);
+        $customFactory->hasMetadataFor($object)->willReturn(false);
+        $defaultFactory->hasMetadataFor($object)->willReturn(false);
 
         $this->hasMetadataFor($object)->shouldReturn(false);
     }
