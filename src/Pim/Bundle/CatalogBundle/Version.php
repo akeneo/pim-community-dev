1148af201b7ba6b6796f87f1a1b279df15883196
Stephane Chapeau <stephane.chapeau@akeneo.com>
2014-12-03 15:45:49 +0100

    Preparing version 1.2.14

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.2.13';
+    const VERSION = '1.2.14';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Acrobatty Bunny';
