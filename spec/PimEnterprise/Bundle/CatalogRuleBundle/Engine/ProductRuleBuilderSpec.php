55dc00923a6e1e5388ddadf669e00a9ba1e99003
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2014-12-04 17:04:00 +0100

    PIM-3419: clean the product rule builder to make it validate the rule and use the rule content serializer

--- spec/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleBuilderSpec.php
+++ spec/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleBuilderSpec.php
@@ -3,20 +3,28 @@
 namespace spec\PimEnterprise\Bundle\CatalogRuleBundle\Engine;
 
 use PhpSpec\ObjectBehavior;
+use PimEnterprise\Bundle\CatalogRuleBundle\Serializer\ProductRuleContentSerializerInterface;
 use PimEnterprise\Bundle\RuleEngineBundle\Event\RuleEvents;
+use PimEnterprise\Bundle\RuleEngineBundle\Exception\BuilderException;
 use PimEnterprise\Bundle\RuleEngineBundle\Model\RuleDefinitionInterface;
 use Prophecy\Argument;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\ValidatorInterface;
 
 class ProductRuleBuilderSpec extends ObjectBehavior
 {
-    public function let(EventDispatcherInterface $eventDispatcher)
-    {
+    public function let(
+        EventDispatcherInterface $eventDispatcher,
+        ValidatorInterface $validator,
+        ProductRuleContentSerializerInterface $serializer
+    ) {
         $this->beConstructedWith(
             $eventDispatcher,
-            'PimEnterprise\Bundle\RuleEngineBundle\Model\Rule',
-            'PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueAction',
-            'PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction'
+            $validator,
+            $serializer,
+            'PimEnterprise\Bundle\RuleEngineBundle\Model\Rule'
         );
     }
 
@@ -25,45 +33,96 @@ class ProductRuleBuilderSpec extends ObjectBehavior
         $this->shouldHaveType('PimEnterprise\Bundle\CatalogRuleBundle\Engine\ProductRuleBuilder');
     }
 
-    public function it_is_a_rule_loader()
+    public function it_is_a_rule_builder()
     {
         $this->shouldHaveType('PimEnterprise\Bundle\RuleEngineBundle\Engine\BuilderInterface');
     }
 
-    public function it_builds_a_rule($eventDispatcher, RuleDefinitionInterface $definition)
+    public function it_builds_a_rule($eventDispatcher, $validator, $serializer, RuleDefinitionInterface $definition)
     {
-        $content =
-            <<<CONTENT
-            {"conditions":[{"field":"weight","operator":">","value":"123"},{"field":"sku","operator":"STARTS WITH","value":"sku-4372"}],"actions":[{"type":"set_value","field":"name","value":"toto"},{"type":"copy_value","from_field":"description","to_field":"description","from_scope":"tablet","from_locale":"fr_FR","to_scope":"mobile","to_locale":"fr_CH"}]}
-CONTENT;
+        $strContent = $this->buildRuleContent(true);
+        $content = $this->buildRuleContent();
 
-        $definition->getContent()->willReturn($content);
+        $definition->getContent()->shouldBeCalled()->willReturn($strContent);
+        $serializer->deserialize($strContent)->shouldBeCalled()->willReturn($content);
 
         $eventDispatcher->dispatch(RuleEvents::PRE_LOAD, Argument::any())->shouldBeCalled();
         $eventDispatcher->dispatch(RuleEvents::POST_LOAD, Argument::any())->shouldBeCalled();
+        $validator->validate(Argument::any())->shouldBeCalled()->willReturn([]);
 
         $this->build($definition)->shouldHaveType('PimEnterprise\Bundle\RuleEngineBundle\Model\RuleInterface');
     }
 
     public function it_does_not_build_a_rule_with_bad_content(
         $eventDispatcher,
-        RuleDefinitionInterface $definition1,
-        RuleDefinitionInterface $definition2
+        $serializer,
+        RuleDefinitionInterface $definition
     ) {
-        $definition1->getContent()->willReturn(json_encode(['actions' => []]));
-        $definition2->getContent()->willReturn(json_encode(['conditions' => []]));
-        $definition1->getCode()->willReturn('rule1');
-        $definition2->getCode()->willReturn('rule2');
+        $strContent = $this->buildRuleContent(true);
+
+        $definition->getCode()->willReturn('rule1');
+        $definition->getContent()->shouldBeCalled()->willReturn($strContent);
+        $serializer->deserialize($strContent)->willThrow(new \LogicException('Bad content!'));
 
         $eventDispatcher->dispatch(RuleEvents::PRE_LOAD, Argument::any())->shouldBeCalled();
 
         $this
-            ->shouldThrow(new \LogicException('Rule "rule1" should have a "conditions" key in its content.'))
-            ->during('build', [$definition1])
+            ->shouldThrow(new BuilderException('Impossible to build the rule "rule1". Bad content!'))
+            ->during('build', [$definition])
         ;
+    }
+
+    public function it_does_not_build_a_rule_with_that_is_not_valid(
+        $eventDispatcher,
+        $validator,
+        $serializer,
+        RuleDefinitionInterface $definition
+    ) {
+        $strContent = $this->buildRuleContent(true);
+        $content = $this->buildRuleContent();
+
+        $definition->getCode()->willReturn('rule1');
+        $definition->getContent()->shouldBeCalled()->willReturn($strContent);
+        $serializer->deserialize($strContent)->shouldBeCalled()->willReturn($content);
+
+        $eventDispatcher->dispatch(RuleEvents::PRE_LOAD, Argument::any())->shouldBeCalled();
+
+        $validator->validate(Argument::any())->willReturn(['errors']);
+
         $this
-            ->shouldThrow(new \LogicException('Rule "rule2" should have a "actions" key in its content.'))
-            ->during('build', [$definition2])
+            ->shouldThrow(
+                new BuilderException('Impossible to build the rule "rule1" as it does not appear to be valid.')
+            )
+            ->during('build', [$definition])
         ;
+    }
+
+    /**
+     * Do not delete it, this method is used to easily build the rule content that is
+     * used in those specs.
+     * In case we need to modify the specs, it will be useful.
+     *
+     * @param bool $encode
+     *
+     * @return string
+     */
+    private function buildRuleContent($encode = false)
+    {
+        $content = [
+            'conditions' => [
+                ['field' => 'sku', 'operator' => 'LIKE', 'value' => 'foo'],
+                ['field' => 'clothing_size', 'operator' => 'NOT LIKE', 'value' => 'XL', 'locale' => 'fr_FR', 'scope' => 'ecommerce'],
+            ],
+            'actions' => [
+                ['type' => 'set_value', 'field' => 'name', 'value' => 'awesome-jacket', 'locale' => 'en_US', 'scope' => 'tablet'],
+                ['type' => 'copy_value', 'fromField' => 'description', 'toField' => 'description', 'fromLocale' => 'fr_FR', 'toLocale' => 'fr_CH']
+            ]
+        ];
+
+        if (true === $encode) {
+            $content = json_encode($content);
+        }
+
+        return $content;
     }
 }
--- src/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleBuilder.php
+++ src/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleBuilder.php
@@ -11,15 +11,14 @@
 
 namespace PimEnterprise\Bundle\CatalogRuleBundle\Engine;
 
-use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCondition;
-use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction;
-use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueAction;
+use PimEnterprise\Bundle\CatalogRuleBundle\Serializer\ProductRuleContentSerializerInterface;
 use PimEnterprise\Bundle\RuleEngineBundle\Engine\BuilderInterface;
 use PimEnterprise\Bundle\RuleEngineBundle\Event\RuleEvent;
 use PimEnterprise\Bundle\RuleEngineBundle\Event\RuleEvents;
-use PimEnterprise\Bundle\RuleEngineBundle\Model\RuleInterface;
+use PimEnterprise\Bundle\RuleEngineBundle\Exception\BuilderException;
 use PimEnterprise\Bundle\RuleEngineBundle\Model\RuleDefinitionInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Validator\ValidatorInterface;
 
 /**
  * Loads product rules.
@@ -31,31 +30,31 @@ class ProductRuleBuilder implements BuilderInterface
     /** @var EventDispatcherInterface */
     protected $eventDispatcher;
 
+    /** @var ValidatorInterface */
+    protected $validator;
+
     /** @var string */
     protected $ruleClass;
 
-    /** @var string */
-    protected $setValueActionClass;
-
-    /** @var string */
-    protected $copyValueActionClass;
+    /** @var ProductRuleContentSerializerInterface */
+    protected $ruleContentSerializer;
 
     /**
-     * @param EventDispatcherInterface $eventDispatcher
-     * @param string                   $ruleClass            should implement \PimEnterprise\Bundle\RuleEngineBundle\Model\RuleInterface
-     * @param string                   $setValueActionClass  should implement \PimEnterprise\Bundle\RuleEngineBundle\Model\ActionInterface\ProductSetValueActionInterface
-     * @param string                   $copyValueActionClass should implement \PimEnterprise\Bundle\RuleEngineBundle\Model\ActionInterface\ProductCopyValueActionInterface
+     * @param EventDispatcherInterface              $eventDispatcher
+     * @param ValidatorInterface                    $validator
+     * @param ProductRuleContentSerializerInterface $ruleContentSerializer
+     * @param string                                $ruleClass should implement \PimEnterprise\Bundle\RuleEngineBundle\Model\RuleInterface
      */
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
-        $ruleClass,
-        $setValueActionClass,
-        $copyValueActionClass
+        ValidatorInterface $validator,
+        ProductRuleContentSerializerInterface $ruleContentSerializer,
+        $ruleClass
     ) {
         $this->eventDispatcher = $eventDispatcher;
+        $this->validator = $validator;
+        $this->ruleContentSerializer = $ruleContentSerializer;
         $this->ruleClass = $ruleClass;
-        $this->setValueActionClass = $setValueActionClass;
-        $this->copyValueActionClass = $copyValueActionClass;
     }
 
     /**
@@ -63,75 +62,32 @@ class ProductRuleBuilder implements BuilderInterface
      */
     public function build(RuleDefinitionInterface $definition)
     {
+        //TODO: change the name of the events PRE_LOAD and POST_LOAD
         $this->eventDispatcher->dispatch(RuleEvents::PRE_LOAD, new RuleEvent($definition));
 
         /** @var \PimEnterprise\Bundle\RuleEngineBundle\Model\Rule $rule */
         $rule = new $this->ruleClass($definition);
 
-        $content = json_decode($definition->getContent(), true);
-        if (!array_key_exists('conditions', $content)) {
-            throw new \LogicException(
-                sprintf('Rule "%s" should have a "conditions" key in its content.', $definition->getCode())
-            );
-        }
-        if (!array_key_exists('actions', $content)) {
-            throw new \LogicException(
-                sprintf('Rule "%s" should have a "actions" key in its content.', $definition->getCode())
+        try {
+            $content = $this->ruleContentSerializer->deserialize($definition->getContent());
+        } catch (\LogicException $e) {
+            throw new BuilderException(
+                sprintf('Impossible to build the rule "%s". %s', $definition->getCode(), $e->getMessage())
             );
         }
 
-        $this->loadConditions($rule, $content['conditions']);
-        $this->loadActions($rule, $content['actions']);
+        $rule->setConditions($content['conditions']);
+        $rule->setActions($content['actions']);
+
+        $errors = $this->validator->validate($rule);
+        if (count($errors)) {
+            throw new BuilderException(
+                sprintf('Impossible to build the rule "%s" as it does not appear to be valid.', $definition->getCode())
+            );
+        }
 
         $this->eventDispatcher->dispatch(RuleEvents::POST_LOAD, new RuleEvent($definition));
 
         return $rule;
-    }
-
-    /**
-     * Loads conditions into a rule.
-     *
-     * @param RuleInterface $rule
-     * @param array         $rawConditions
-     *
-     * @return ProductRuleBuilder
-     */
-    protected function loadConditions(RuleInterface $rule, array $rawConditions)
-    {
-        $conditions = [];
-        foreach ($rawConditions as $rawCondition) {
-            //TODO: catch exception and log it ? or do not catch and totally fail ?
-            $conditions[] = new ProductCondition($rawCondition);
-        }
-
-        $rule->setConditions($conditions);
-
-        return $this;
-    }
-
-    /**
-     * Loads actions into a rule.
-     *
-     * @param RuleInterface $rule
-     * @param array         $rawActions
-     *
-     * @return ProductRuleBuilder
-     */
-    protected function loadActions(RuleInterface $rule, array $rawActions)
-    {
-        $actions = [];
-        foreach ($rawActions as $rawAction) {
-            if (!isset($rawAction['type'])) {
-                // TODO: throw exception ? that should not happen? or simply log?
-            } elseif (ProductSetValueAction::TYPE === $rawAction['type']) {
-                $actions[] = new $this->setValueActionClass($rawAction);
-            } elseif (ProductCopyValueAction::TYPE === $rawAction['type']) {
-                $actions[] = new $this->copyValueActionClass($rawAction);
-            }
-        }
-
-        $rule->setActions($actions);
-
-        return $this;
     }
 }
