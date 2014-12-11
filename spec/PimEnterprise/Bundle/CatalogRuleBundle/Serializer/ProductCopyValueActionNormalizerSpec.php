418ce81bfe0f91f0858e874a5acb6acbafd6070d
Olivier Soulet <olivier.soulet@akeneo.com>
2014-12-11 11:17:29 +0100

    PIM-3063: Refactored normalizers for copy and set action

---
+++ spec/PimEnterprise/Bundle/CatalogRuleBundle/Serializer/ProductCopyValueActionNormalizerSpec.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace spec\PimEnterprise\Bundle\CatalogRuleBundle\Serializer;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueActionInterface;
+use Prophecy\Argument;
+
+class ProductCopyValueActionNormalizerSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('PimEnterprise\Bundle\CatalogRuleBundle\Serializer\ProductCopyValueActionNormalizer');
+    }
+
+    function it_implements()
+    {
+        $this->shouldHaveType('Symfony\Component\Serializer\Normalizer\DenormalizerInterface');
+        $this->shouldHaveType('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+    }
+
+    function it_normalizes(ProductCopyValueActionInterface $object)
+    {
+        $object->getFromField()->shouldBeCalled()->willReturn('description');
+        $object->getToField()->shouldBeCalled()->willReturn('description');
+        $object->getFromLocale()->shouldBeCalled()->willReturn('fr_FR');
+        $object->getToLocale()->shouldBeCalled()->willReturn('en_US');
+        $object->getFromScope()->shouldBeCalled()->willReturn('mobile');
+        $object->getToScope()->shouldBeCalled()->willReturn('ecommerce');
+
+        $this->normalize($object)->shouldReturn(
+            [
+                'type'        => ProductCopyValueActionInterface::TYPE,
+                'from_field'  => 'description',
+                'to_field'    => 'description',
+                'from_locale' => 'fr_FR',
+                'to_locale'   => 'en_US',
+                'from_scope'  => 'mobile',
+                'to_scope'    => 'ecommerce'
+            ]
+        );
+    }
+
+    function it_supports_normalization(ProductCopyValueActionInterface $data)
+    {
+        $this->supportsNormalization($data)->shouldReturn(true);
+    }
+
+    function it_does_not_support_normalization(AttributeInterface $data)
+    {
+        $this->supportsNormalization($data)->shouldReturn(false);
+    }
+
+    function it_denormalizes()
+    {
+        $data['type'] = ProductCopyValueActionInterface::TYPE;
+
+        $this->denormalize($data, 'PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction')
+            ->shouldHaveType('PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction');
+    }
+
+    function it_supports_denormalization()
+    {
+        $data['type'] = ProductCopyValueActionInterface::TYPE;
+        $type = '\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction';
+
+        $this->supportsDenormalization($data, $type)->shouldReturn(true);
+    }
+
+    function it_does_not_support_denormalization_for_wrong_object()
+    {
+        $data['type'] = ProductCopyValueActionInterface::TYPE;
+        $type = '\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCondition';
+
+        $this->supportsDenormalization($data, $type)->shouldReturn(false);
+    }
+}
---
+++ spec/PimEnterprise/Bundle/CatalogRuleBundle/Serializer/ProductSetValueActionNormalizerSpec.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace spec\PimEnterprise\Bundle\CatalogRuleBundle\Serializer;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueActionInterface;
+use Prophecy\Argument;
+
+class ProductSetValueActionNormalizerSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueAction');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('PimEnterprise\Bundle\CatalogRuleBundle\Serializer\ProductSetValueActionNormalizer');
+    }
+
+    function it_implements()
+    {
+        $this->shouldHaveType('Symfony\Component\Serializer\Normalizer\DenormalizerInterface');
+        $this->shouldHaveType('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+    }
+
+    function it_normalizes(ProductSetValueActionInterface $object)
+    {
+        $object->getField()->shouldBeCalled()->willReturn('description');
+        $object->getValue()->shouldBeCalled()->willReturn('My beautiful description');
+        $object->getLocale()->shouldBeCalled()->willReturn('fr_FR');
+        $object->getScope()->shouldBeCalled()->willReturn('mobile');
+
+        $this->normalize($object)->shouldReturn(
+            [
+                'type'  => ProductSetValueActionInterface::TYPE,
+                'field' => 'description',
+                'value' => 'My beautiful description',
+                'locale'=> 'fr_FR',
+                'scope' => 'mobile',
+            ]
+        );
+    }
+
+    function it_supports_normalization(ProductSetValueActionInterface $data)
+    {
+        $this->supportsNormalization($data)->shouldReturn(true);
+    }
+
+    function it_does_not_support_normalization(AttributeInterface $data)
+    {
+        $this->supportsNormalization($data)->shouldReturn(false);
+    }
+
+    function it_denormalizes()
+    {
+        $data['type'] = ProductSetValueActionInterface::TYPE;
+
+        $this->denormalize($data, 'PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction')
+            ->shouldHaveType('PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueAction');
+    }
+
+    function it_supports_denormalization()
+    {
+        $data['type'] = ProductSetValueActionInterface::TYPE;
+        $type = '\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueAction';
+
+        $this->supportsDenormalization($data, $type)->shouldReturn(true);
+    }
+
+    function it_does_not_support_denormalization_for_wrong_object()
+    {
+        $data['type'] = ProductSetValueActionInterface::TYPE;
+        $type = '\PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCondition';
+
+        $this->supportsDenormalization($data, $type)->shouldReturn(false);
+    }
+}
