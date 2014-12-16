d1ec01d43916720275e8784db4e8a14244b4afbe
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2014-12-16 17:27:25 +0100

    PIM-3448: avoid to allow to approve/refuse/delete a proposal if the user can't edit all values of this proposal

--- src/PimEnterprise/Bundle/DataGridBundle/Datagrid/ProductDraft/GridHelper.php
+++ src/PimEnterprise/Bundle/DataGridBundle/Datagrid/ProductDraft/GridHelper.php
@@ -11,8 +11,10 @@
 
 namespace PimEnterprise\Bundle\DataGridBundle\Datagrid\ProductDraft;
 
+use PimEnterprise\Bundle\SecurityBundle\Attributes;
 use PimEnterprise\Bundle\WorkflowBundle\Model\ProductDraft;
 use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Helper for product draft datagrid
@@ -21,6 +23,19 @@ use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
  */
 class GridHelper
 {
+    /** @var SecurityContextInterface  */
+    protected $securityContext;
+
+    /**
+     * @param SecurityContextInterface $securityContext
+     *
+     * TODO: make this parameter mandatory for 1.3
+     */
+    public function __construct(SecurityContextInterface $securityContext = null)
+    {
+        $this->securityContext = $securityContext;
+    }
+
     /**
      * Returns callback that will disable approve and refuse buttons
      * given product draft status
@@ -30,11 +45,17 @@ class GridHelper
     public function getActionConfigurationClosure()
     {
         return function (ResultRecordInterface $record) {
+            if (null !== $this->securityContext &&
+                false === $this->securityContext->isGranted(Attributes::EDIT_ATTRIBUTES, $record->getRootEntity())
+            ) {
+                return ['approve' => false, 'refuse' => false, 'remove' => false];
+            }
+
             if (ProductDraft::IN_PROGRESS === $record->getValue('status')) {
                 return ['approve' => false, 'refuse' => false];
-            } else {
-                return ['remove' => false];
             }
+
+            return ['remove' => false];
         };
     }
 
