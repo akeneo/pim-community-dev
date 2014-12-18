2c0e1872c4a1e2edc900566999b1dc5e8e3fcf74
Nicolas Dupont <nicolas@akeneo.com>
2014-12-18 16:34:19 +0100

    PIM-2451 : declare denormalizers, hot fix the product value and media

--- src/Pim/Bundle/TransformBundle/Denormalizer/Flat/ProductValue/MediaDenormalizer.php
+++ src/Pim/Bundle/TransformBundle/Denormalizer/Flat/ProductValue/MediaDenormalizer.php
@@ -34,6 +34,8 @@ class MediaDenormalizer extends AbstractValueDenormalizer
      */
     public function denormalize($data, $class, $format = null, array $context = array())
     {
+        // TODO : to fix
+        return;
         if ($data === null || $data === '') {
             return null;
         }
