099daab61a005ada7b223a52fade47529c0f4056
Nicolas Dupont <nicolas@akeneo.com>
2014-12-19 14:24:45 +0100

    PIM-3530 : split command

---
+++ src/Pim/Bundle/CatalogBundle/Command/CreateProductTemplateCommand.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Command;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Pim\Bundle\CatalogBundle\Entity\Group;
+use Pim\Bundle\CatalogBundle\Entity\GroupProductTemplate;
+use Pim\Bundle\CatalogBundle\Model\ProductInterface;
+use Pim\Bundle\CatalogBundle\Model\ProductValue;
+use Pim\Bundle\CatalogBundle\Model\ProductValueInterface;
+use Pim\Bundle\CatalogBundle\Util\ProductValueKeyGenerator;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Temporary command to create variant group product templates
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CreateProductTemplateCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('pim:variant-group:create-template');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Setup the template with an existing product
+        $referenceProduct = $this->getProduct('AKNTS_BPS');
+        $productValues = $referenceProduct->getValues()->toArray();
+        $productValuesData = $this->normalizeToDB($productValues);
+
+        $variantGroup = $this->getVariantGroup('akeneo_tshirt');
+        if ($variantGroup->getProductTemplate()) {
+            $template = $variantGroup->getProductTemplate();
+        } else {
+            $template = new GroupProductTemplate();
+        }
+        $template->setValuesData($productValuesData);
+        $template->setGroup($variantGroup);
+        // TODO : should use cascade on VG
+        $this->saveTemplate($template);
+
+        $output->writeln(
+            sprintf(
+                '<info>product template has been created for variant group "%s"<error>',
+                $variantGroup->getCode()
+            )
+        );
+    }
+
+    /**
+     * @param ProductValueInterface[] $productValues
+     *
+     * @return array
+     */
+    protected function normalizeToDB(array $productValues)
+    {
+        // TODO : as for versionning, we should really change for json/structured format
+        $normalizer = $this->getContainer()->get('pim_serializer');
+        $normalizedValues = [];
+        foreach ($productValues as $value) {
+            $normalizedValues += $normalizer->normalize($value, 'csv');
+        }
+
+        return $normalizedValues;
+    }
+
+    /**
+     * @param string $identifier
+     *
+     * @return ProductInterface
+     */
+    protected function getProduct($identifier)
+    {
+        $repository = $this->getContainer()->get('pim_catalog.repository.product');
+        $product    = $repository->findOneByIdentifier($identifier);
+
+        return $product;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return Group
+     */
+    protected function getVariantGroup($code)
+    {
+        $repository = $this->getContainer()->get('pim_catalog.repository.group');
+        $group      = $repository->findOneByCode($code);
+
+        return $group;
+    }
+
+    /**
+     * @param GroupProductTemplate $template
+     */
+    protected function saveTemplate(GroupProductTemplate $template)
+    {
+        $objectManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $objectManager->persist($template);
+        $objectManager->flush();
+    }
+}
