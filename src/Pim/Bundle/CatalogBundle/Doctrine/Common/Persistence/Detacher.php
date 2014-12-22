2c3719c5f2413cb6d184c45e97d02de7914bc93c
Nicolas Dupont <nicolas@akeneo.com>
2014-12-22 13:55:15 +0100

    PIM-3530 : re-work the product template manager and add specs

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Persistence/Detacher.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Persistence/Detacher.php
@@ -12,6 +12,8 @@ use Doctrine\Common\Util\ClassUtils;
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO : add an interface ?
  */
 class Detacher
 {
