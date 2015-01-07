b8ddd1e7d3ff46c7b5dc43d07d0ffcfaa85f9f2a
Nicolas Dupont <nicolas@akeneo.com>
2015-01-07 12:19:47 +0100

    PIM-3076 : update the variant group values denormalizer to support variant group without values

--- src/Pim/Bundle/TransformBundle/Denormalizer/Flat/VariantGroupValuesDenormalizer.php
+++ src/Pim/Bundle/TransformBundle/Denormalizer/Flat/VariantGroupValuesDenormalizer.php
@@ -11,6 +11,8 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
  * TODO : should be merged with ProductDenormalizer::denormalizeValues, + add an option to update products or create
  * new values (variant groups), then change the type to use a naming less variant group tinted
  *
+ * TODO : to re-work
+ *
  * @author Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -56,31 +58,35 @@ class VariantGroupValuesDenormalizer implements DenormalizerInterface
         foreach ($data as $attFieldName => $dataValue) {
 
             $attributeInfos = $this->fieldNameBuilder->extractAttributeFieldNameInfos($attFieldName);
-            $attribute = $attributeInfos['attribute'];
-            unset($attributeInfos['attribute']);
 
-            $valueKey = $attribute->getCode();
-            if ($attribute->isLocalizable()) {
-                $valueKey .= '-'.$attributeInfos['locale_code'];
-            }
-            if ($attribute->isScopable()) {
-                $valueKey .= '-'.$attributeInfos['scope_code'];
-            }
-            if (isset($productValues[$valueKey])) {
-                $value = $productValues[$valueKey];
-            } else {
-                $value = new $this->valueClass();
-                $value->setAttribute($attribute);
-                $value->setLocale($attributeInfos['locale_code']);
-                $value->setScope($attributeInfos['scope_code']);
-            }
+            if (null !== $attributeInfos) {
 
-            $productValues[$valueKey] = $this->valueDenormalizer->denormalize(
-                $dataValue,
-                $this->valueClass,
-                'csv',
-                ['entity' => $value] + $attributeInfos
-            );
+                $attribute = $attributeInfos['attribute'];
+                unset($attributeInfos['attribute']);
+
+                $valueKey = $attribute->getCode();
+                if ($attribute->isLocalizable()) {
+                    $valueKey .= '-' . $attributeInfos['locale_code'];
+                }
+                if ($attribute->isScopable()) {
+                    $valueKey .= '-' . $attributeInfos['scope_code'];
+                }
+                if (isset($productValues[$valueKey])) {
+                    $value = $productValues[$valueKey];
+                } else {
+                    $value = new $this->valueClass();
+                    $value->setAttribute($attribute);
+                    $value->setLocale($attributeInfos['locale_code']);
+                    $value->setScope($attributeInfos['scope_code']);
+                }
+
+                $productValues[$valueKey] = $this->valueDenormalizer->denormalize(
+                    $dataValue,
+                    $this->valueClass,
+                    'csv',
+                    ['entity' => $value] + $attributeInfos
+                );
+            }
         }
 
         return $productValues;
