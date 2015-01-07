708e5e6a18c05c8504979a353811ddaa1aa0c8f9
Nicolas Dupont <nicolas@akeneo.com>
2015-01-07 12:20:26 +0100

    PIM-3071 : re-work flat group denormalizer to be able to set the code, type, axis and labels

--- src/Pim/Bundle/CatalogBundle/Validator/Constraints/VariantGroupValuesValidator.php
+++ src/Pim/Bundle/CatalogBundle/Validator/Constraints/VariantGroupValuesValidator.php
@@ -49,7 +49,9 @@ class VariantGroupValuesValidator extends ConstraintValidator
     protected function validateProductValues(GroupInterface $variantGroup, Constraint $constraint)
     {
         $template = $variantGroup->getProductTemplate();
-        $forbiddenAttributes = $variantGroup->getAttributes()->toArray();
+        $forbiddenAttributes = is_array($variantGroup->getAttributes()) ?
+            $variantGroup->getAttributes() :
+            $variantGroup->getAttributes()->toArray();
         $forbiddenAttributes[] = $this->attributeRepository->getIdentifier();
 
         $notValidAttributes = [];
--- src/Pim/Bundle/TransformBundle/Denormalizer/Flat/GroupDenormalizer.php
+++ src/Pim/Bundle/TransformBundle/Denormalizer/Flat/GroupDenormalizer.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace Pim\Bundle\TransformBundle\Denormalizer\Flat;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Pim\Bundle\CatalogBundle\Entity\GroupTranslation;
+use Pim\Bundle\CatalogBundle\Entity\GroupType;
+use Pim\Bundle\CatalogBundle\Model\GroupInterface;
+use Pim\Bundle\CatalogBundle\Repository\ReferableEntityRepositoryInterface;
 
 /**
  * Group flat denormalizer
@@ -11,11 +16,114 @@ namespace Pim\Bundle\TransformBundle\Denormalizer\Flat;
  */
 class GroupDenormalizer extends AbstractEntityDenormalizer
 {
+    /** @var ReferableEntityRepositoryInterface */
+    protected $groupTypeRepository;
+
+    /** @var ReferableEntityRepositoryInterface */
+    protected $attributeRepository;
+
+    /**
+     * @param ManagerRegistry                    $managerRegistry
+     * @param string                             $entityClass
+     * @param ReferableEntityRepositoryInterface $groupTypeRepository
+     * @param ReferableEntityRepositoryInterface $attributeRepository
+     */
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        $entityClass,
+        ReferableEntityRepositoryInterface $groupTypeRepository,
+        ReferableEntityRepositoryInterface $attributeRepository
+    ) {
+        parent::__construct($managerRegistry, $entityClass);
+        $this->groupTypeRepository = $groupTypeRepository;
+        $this->attributeRepository = $attributeRepository;
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function doDenormalize($data, $format, array $context)
     {
-        return $this->getEntity($data, $context);
+        /** @var GroupInterface $group */
+        $group = $this->getEntity($data, $context);
+        if (null === $group->getId()) {
+            $this->setCode($group, $data);
+            $this->setGroupType($group, $data);
+            $this->setAxis($group, $data);
+        }
+        $this->setLabels($group, $data);
+
+        return $group;
+    }
+
+    /**
+     * @param GroupInterface $group
+     * @param array          $data
+     */
+    protected function setCode(GroupInterface $group, $data)
+    {
+        if (isset($data['code'])) {
+            $group->setCode($data['code']);
+        }
+    }
+
+    /**
+     * @param GroupInterface $group
+     * @param array          $data
+     */
+    protected function setGroupType(GroupInterface $group, $data)
+    {
+        if (isset($data['type'])) {
+            $typeCode = $data['type'];
+            /** @var GroupType|null $type */
+            $type = $this->groupTypeRepository->findByReference($typeCode);
+            if (!$type) {
+                throw new \LogicException(
+                    sprintf('Group Type with identifier "%s" not found', $typeCode)
+                );
+            }
+            $group->setType($type);
+        }
+    }
+
+    /**
+     * @param GroupInterface $group
+     * @param array          $data
+     */
+    protected function setAxis(GroupInterface $group, $data)
+    {
+        if (isset($data['axis'])) {
+            $axisCodes = explode(',', $data['axis']);
+            $attributes = [];
+            foreach ($axisCodes as $code) {
+                $attribute = $this->attributeRepository->findByReference($code);
+                if (!$attribute) {
+                    throw new \LogicException(
+                        sprintf('Attribute with identifier "%s" not found', $code)
+                    );
+                }
+                $attributes[] = $attribute;
+            }
+            $group->setAttributes($attributes);
+        }
+    }
+
+    /**
+     * @param GroupInterface $group
+     * @param array          $data
+     */
+    protected function setLabels(GroupInterface $group, $data)
+    {
+        foreach ($data as $field => $value) {
+            $isLabel = false !== strpos($field, 'label-', 0);
+            if ($isLabel) {
+                $labelTokens = explode('-', $field);
+                $localeCode = $labelTokens[1];
+                /** @var GroupTranslation $translation */
+                $translation = $group->getTranslation($localeCode);
+                $translation->setLabel($value);
+                $group->addTranslation($translation);
+            }
+        }
     }
 }
