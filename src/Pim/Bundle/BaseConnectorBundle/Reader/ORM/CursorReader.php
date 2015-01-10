1edb9def67060f9058320975740e45085aa11ed5
Nicolas Dupont <nicolas@akeneo.com>
2015-01-10 16:25:40 +0100

    PIM-3470 : mark readers as deprecated and prepare the variant group export

--- src/Pim/Bundle/BaseConnectorBundle/Reader/ORM/CursorReader.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/ORM/CursorReader.php
@@ -12,6 +12,8 @@ use Pim\Bundle\BaseConnectorBundle\Exception\ORMReaderException;
 /**
  * ORM cursor reader
  *
+ * @deprecated, will be removed in 1.4
+ *
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
