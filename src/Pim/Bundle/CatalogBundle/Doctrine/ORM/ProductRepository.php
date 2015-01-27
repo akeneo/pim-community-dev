81c7309f44ce9b703fa800d48b3322a940a93829
Stephane Chapeau <stephane.chapeau@akeneo.com>
2015-01-27 14:39:46 +0100

    PIM-3593: Add a deprecated warning repositories of CatalogBundle\Doctrine should be moved in Doctrine\MongoDBODM\Repository and Doctrine\ORM\Repository

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/ProductRepository.php
@@ -22,6 +22,8 @@ use Pim\Bundle\CatalogBundle\Repository\ReferableEntityRepositoryInterface;
  * @author    Gildas Quemener <gildas@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @deprecated will be moved to Pim\Bundle\CatalogBundle\Doctrine\ORM\Repository in 1.4
  */
 class ProductRepository extends EntityRepository implements
     ProductRepositoryInterface,
