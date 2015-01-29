8ce928f6c1e28afda2066b56022cf0b475cb933d
Nicolas Dupont <nicolas@akeneo.com>
2015-01-29 17:06:13 +0100

    PIM-3695: freeze deps to prepare the 1.3 RC

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,7 +31,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-//            $bundles[] = new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
+            $bundles[] = new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
         }
 
         $bundles = array_merge(
