31a1b6c2e4ee5337f140f73de42c87da3367800d
Filips Alpe <filips@akeneo.com>
2015-01-29 22:47:36 +0200

    PIM-2389 Create a dedicated boolean product value denormalizer

--- src/Pim/Bundle/TransformBundle/Denormalizer/Structured/ProductValue/BooleanDenormalizer.php
+++ src/Pim/Bundle/TransformBundle/Denormalizer/Structured/ProductValue/BooleanDenormalizer.php
@@ -3,23 +3,20 @@
 namespace Pim\Bundle\TransformBundle\Denormalizer\Structured\ProductValue;
 
 /**
- * Base value denormalizer used following attribute types:
- * - pim_catalog_identifier
- * - pim_catalog_number
- * - pim_catalog_text
- * - pim_catalog_textarea
+ * Boolean denormalizer used for the following attribute type:
+ * - pim_catalog_boolean
  *
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class BaseValueDenormalizer extends AbstractValueDenormalizer
+class BooleanDenormalizer extends AbstractValueDenormalizer
 {
     /**
      * {@inheritdoc}
      */
     public function denormalize($data, $class, $format = null, array $context = [])
     {
-        return '' === $data ? null : $data;
+        return null === $data ? null : (bool) $data;
     }
 }
