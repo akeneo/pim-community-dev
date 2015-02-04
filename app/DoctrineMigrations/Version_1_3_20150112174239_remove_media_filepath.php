6875c0a2a52684dc58335dc5e7be575ac604e88c
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-02-04 15:59:08 +0100

    PIM-3616: handle mongo upgrades as real Doctrine migrations

--- app/DoctrineMigrations/Version_1_3_20150112174239_remove_media_filepath.php
+++ app/DoctrineMigrations/Version_1_3_20150112174239_remove_media_filepath.php
@@ -4,11 +4,12 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Pim\Upgrade\UpgradeHelper;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Migration 1.2 => 1.3
+ * Remove absolute filepath from product medias
  *
  * @author    Olivier Soulet <olivier.soulet@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
@@ -16,6 +17,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class Version_1_3_20150112174239_remove_media_filepath extends AbstractMigration implements ContainerAwareInterface
 {
+    const PRODUCT_COLLECTION = 'pim_catalog_product';
+
     /** @var ContainerInterface */
     protected $container;
 
@@ -29,18 +32,57 @@ class Version_1_3_20150112174239_remove_media_filepath extends AbstractMigration
 
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql(sprintf('ALTER TABLE %s DROP file_path', $this->getTableName()));
+        $helper = new UpgradeHelper($this->container);
+        if (!$helper->areProductsStoredInMongo()) {
+            $this->addSql(sprintf('ALTER TABLE %s DROP file_path', $this->getOrmTableName()));
+        }
     }
 
     public function down(Schema $schema)
     {
-        return;
+        throw new \RuntimeException('No revert is provided for the migrations.');
     }
 
-    protected function getTableName()
+    public function postUp(Schema $schema)
+    {
+        $helper = new UpgradeHelper($this->container);
+        if ($helper->areProductsStoredInMongo()) {
+            $database = $helper->getMongoInstance();
+            $this->removeFilePathFromProductMedias($database);
+        }
+    }
+
+    protected function removeFilePathFromProductMedias(\MongoDB $database)
+    {
+        $productCollection = new \MongoCollection($database, self::PRODUCT_COLLECTION);
+        $products = $productCollection->find();
+
+        echo sprintf("Removing filePath from %s medias...\n", $products->count());
+
+        foreach ($products as $product) {
+            if (array_key_exists('values', $product)) {
+                $countValues = count($product['values']);
+
+                for ($i = 0; $i <= $countValues; $i++) {
+                    $result = $productCollection->update(
+                        ['_id' => $product['_id']],
+                        ['$unset' => [sprintf('values.%s.media.filePath', $i) => true]],
+                        ['w' => true]
+                    );
+
+                    if ($result['ok'] != 1) {
+                        echo "ERROR on migrating media value:";
+                        print_r($result);
+                        print_r($product);
+                    }
+                }
+            }
+        }
+
+        echo sprintf("FilePath removed from %s medias: <info>done</info>.\n", $products->count());
+    }
+
+    protected function getOrmTableName()
     {
         $class = $this->container->getParameter('pim_catalog.entity.product_media.class');
 
