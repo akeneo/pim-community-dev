ca5b54cd09c51a4f98932fe0b3cf9473220f1f67
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-02-11 19:41:22 +0100

    PIM-3707: introduce saving options resolver for group saver

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/GroupSavingOptionsResolver.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/GroupSavingOptionsResolver.php
@@ -7,13 +7,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
- * Resolve completeness saving options
- *
- * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * Resolve group saving options
+ * @author    Julien Janvier <jjanvier@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CompletenessSavingOptionsResolver implements SavingOptionsResolverInterface
+class GroupSavingOptionsResolver implements SavingOptionsResolverInterface
 {
     /**
      * {@inheritdoc}
@@ -23,8 +22,10 @@ class CompletenessSavingOptionsResolver implements SavingOptionsResolverInterfac
         $resolver = $this->createOptionsResolver();
         $resolver->setDefaults(
             [
-                'flush'    => true,
-                'schedule' => true
+                'flush'                   => true,
+                'copy_values_to_products' => false,
+                'add_products'            => [],
+                'remove_products'         => [],
             ]
         );
         $options = $resolver->resolve($options);
@@ -37,16 +38,7 @@ class CompletenessSavingOptionsResolver implements SavingOptionsResolverInterfac
      */
     public function resolveSaveAllOptions(array $options)
     {
-        $resolver = $this->createOptionsResolver();
-        $resolver->setDefaults(
-            [
-                'flush'    => true,
-                'schedule' => true,
-            ]
-        );
-        $options = $resolver->resolve($options);
-
-        return $options;
+        return $this->resolveSaveOptions($options);
     }
 
     /**
@@ -55,11 +47,13 @@ class CompletenessSavingOptionsResolver implements SavingOptionsResolverInterfac
     protected function createOptionsResolver()
     {
         $resolver = new OptionsResolver();
-        $resolver->setOptional(['flush', 'schedule']);
+        $resolver->setOptional(['flush', 'copy_values_to_products', 'add_products', 'remove_products']);
         $resolver->setAllowedTypes(
             [
-                'flush'    => 'bool',
-                'schedule' => 'bool'
+                'flush'                   => 'bool',
+                'copy_values_to_products' => 'bool',
+                'add_products'            => 'array',
+                'remove_products'         => 'array',
             ]
         );
 
