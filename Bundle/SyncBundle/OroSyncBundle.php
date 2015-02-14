8ed6193ef565f9729ec4b42fcfe6fd2396484ccd
Nicolas Dupont <nicolas@akeneo.com>
2015-02-14 15:40:08 +0100

    Remove SyncBundle

--- Bundle/SyncBundle/OroSyncBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\SyncBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OroSyncBundle extends Bundle
-{
-}
--- Bundle/SyncBundle/Resources/public/js/when-setup.js
+++
@@ -1,8 +0,0 @@
-/* jshint browser:true */
-/* global define */
-define(['when'],
-function(when) {
-    'use strict';
-    window.when = when;
-    return when;
-});
