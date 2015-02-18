1190917a2c57facd9f946e4882d566f45aebe444
Filips Alpe <filips@akeneo.com>
2015-02-18 19:23:37 +0200

    PIM-3802 Fix api key generation

--- Bundle/UserBundle/Resources/public/js/init-user.js
+++ Bundle/UserBundle/Resources/public/js/init-user.js
@@ -1,8 +1,7 @@
 /* jshint browser:true */
 /* global require */
-require(['jquery', 'underscore', 'oro/translator', 'oro/app', 'oro/mediator', 'oro/messenger',
-    'oro/dialog-widget', 'jquery.dialog.extended'],
-function($, _, __, app, mediator, messenger, DialogWidget) {
+require(['jquery', 'underscore', 'oro/translator', 'oro/app', 'oro/mediator', 'oro/messenger'],
+function($, _, __, app, mediator, messenger) {
     'use strict';
 
     /* ============================================================
@@ -47,32 +46,6 @@ function($, _, __, app, mediator, messenger, DialogWidget) {
         $(document).on('change', '#btn-enable input', function(e) {
             $('.status-enabled').toggleClass('hide');
             $('.status-disabled').toggleClass('hide');
-        });
-
-        $(document).on('click', '#view-activity-btn', function (e) {
-            e.stopImmediatePropagation();
-            var $el = $(this),
-                dialog = /** @var oro.DialogWidget */ $el.data('dialog');
-            if (dialog) {
-                // dialog already is opened
-                return false;
-            }
-
-            $el.data('dialog', dialog = new DialogWidget({
-                url: $el.attr('href'),
-                dialogOptions: {
-                    allowMaximize: true,
-                    allowMinimize: true,
-                    dblclick: 'maximize',
-                    maximizedHeightDecreaseBy: 'minimize-bar',
-                    width : 1000,
-                    title: $el.attr('title')
-                }
-            }));
-            dialog.once('widgetRemove', _.bind($el.removeData, $el, 'dialog'));
-            dialog.render();
-
-            return false;
         });
     });
 
