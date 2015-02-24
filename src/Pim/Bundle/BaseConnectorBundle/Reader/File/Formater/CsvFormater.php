388a6ec77789a6ed13fb914c9788b89e96ad9939
sumbobyboys <sumbobyboys@gmail.com>
2015-02-24 17:02:25 +0100

    PIM-3796: Add a CSV formater to convert flat format to structured

--- src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
@@ -2,7 +2,7 @@
 
 namespace Pim\Bundle\BaseConnectorBundle\Reader\File\Formater;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Pim\Bundle\TransformBundle\Builder\FieldNameBuilder;
 
 /**
  * Csv formater
@@ -13,38 +13,236 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  */
 class CsvFormater
 {
-    const ARRAY_SEPARATOR            = ',';
-    const FIELD_SEPARATOR            = '-';
-    const GROUP_ASSOCIATION_SUFFIX   = '-groups';
-    const PRODUCT_ASSOCIATION_SUFFIX = '-products';
-
-    /** @var ManagerRegistry */
-    protected $managerRegistry;
-
-    /** @var string */
-    protected $associationTypeClass;
+    /** @var FieldNameBuilder */
+    protected $fieldNameBuilder;
 
     /**
      * Constructor
      *
-     * @param ManagerRegistry $managerRegistry
-     * @param string          $associationTypeClass
+     * @param FieldNameBuilder $fieldNameBuilder
      */
-    public function __construct(ManagerRegistry $managerRegistry, $associationTypeClass)
+    public function __construct(FieldNameBuilder $fieldNameBuilder)
     {
-        $this->managerRegistry      = $managerRegistry;
-        $this->associationTypeClass = $associationTypeClass;
+        $this->fieldNameBuilder = $fieldNameBuilder;
     }
 
-    public function getAssociationFieldNames()
+    /**
+     * Convert flat array to structured array:
+     *
+     * Before:
+     * [
+     *     'description-en_US-mobile': 'My description',
+     *     'price': '10 EUR, 24 USD',
+     *     'price-CHF': '20',
+     *     'length': '10 CENTIMETER',
+     *     'enabled': '1',
+     *     'categories': 'tshirt,men'
+     *     'XSELL-groups': 'akeneo_tshirt, oro_tshirt',
+     *     'XSELL-product': 'AKN_TS, ORO_TSH'
+     * ]
+     *
+     * After:
+     * [
+     *     'description' : {
+     *         'locale': 'en_US',
+     *         'scope':  'mobile',
+     *         'data':   'My description'
+     *     },
+     *     'price' : {
+     *         'locale': null,
+     *         'scope':  null,
+     *         'data':   [
+     *             {'data': '10', 'currency': 'EUR'},
+     *             {'data': '24', 'currency': 'USD'},
+     *             {'data': '20', 'currency': 'CHF'}
+     *         ]
+     *     },
+     *     'length' : {
+     *         'locale': 'en_US',
+     *         'scope':  'mobile',
+     *         'data':   {'data': '10', 'unit': 'CENTIMETER'}
+     *     },
+     *     'enabled': '1',
+     *     'categories': ['tshirt', 'men']
+     *     'XSELL': {
+     *         'groups': ['akeneo_tshirt', 'oro_tshirt'],
+     *         'product': ['AKN_TS', 'ORO_TSH']
+     *     }
+     * ]
+     *
+     * @param array $product Representing a flat product
+     *
+     * @return array structured product
+     */
+    public function convertToStructured(array $product)
     {
-        $fieldNames = [];
-        $associationTypes = $this->managerRegistry->getRepository($this->associationTypeClass)->findAll();
-        foreach ($associationTypes as $associationType) {
-            $fieldNames[] = $associationType->getCode() . self::GROUP_ASSOCIATION_SUFFIX;
-            $fieldNames[] = $associationType->getCode() . self::PRODUCT_ASSOCIATION_SUFFIX;
+        $result = [];
+        foreach ($product as $column => $value) {
+            $value = $this->convertToStructuredField($column, $value);
+            $result = $this->addFieldToCollection($result, $value);
         }
 
-        return $fieldNames;
+        return $result;
+    }
+
+    /**
+     * Convert a flat field to a structured one
+     * @param string $column The column name
+     * @param string $value  The value in the cell
+     *
+     * @return array
+     */
+    protected function convertToStructuredField($column, $value)
+    {
+        $associationFields = $this->fieldNameBuilder->getAssociationFieldNames();
+
+        if (in_array($column, $associationFields)) {
+            $value = FieldNameBuilder::splitCollection($value);
+            list($associationTypeCode, $associatedWith) = FieldNameBuilder::splitFieldName($column);
+
+            return [$associationTypeCode => [$associatedWith => $value]];
+        } elseif (in_array($column, ['categories', 'groups'])) {
+            return [$column => FieldNameBuilder::splitCollection($value)];
+        } elseif (in_array($column, ['enabled', 'family'])) {
+            return [$column => $value];
+        } else {
+            return $this->formatValue($column, $value);
+        }
+
+        return [];
+    }
+
+    /**
+     * Format a value cell
+     * @param string $column The column name
+     * @param string $value  The value in the cell
+     *
+     * @return structured value
+     */
+    protected function formatValue($column, $value)
+    {
+        $fieldNameInfos = $this->fieldNameBuilder->extractAttributeFieldNameInfos($column);
+
+        $data = $this->formatValueData($value, $fieldNameInfos);
+
+        return [$fieldNameInfos['attribute']->getCode() => [[
+            'locale' => $fieldNameInfos['locale_code'],
+            'scope'  => $fieldNameInfos['scope_code'],
+            'data'   => $data,
+        ]]];
+    }
+
+    /**
+     * Format the value data of a cell into a structured format
+     *
+     * prices:      '10 EUR, 24 USD'   => [{'data': '10', 'currency': 'EUR'}, {'data': '24', 'currency': 'USD'}]
+     * metric:      '10 METER'         => {'data': '10', 'unit': 'METER'}
+     * multiselect: 'red, blue, black' => ['red', 'blue', 'black']
+     *
+     * @param string $value          The value content
+     * @param array  $fieldNameInfos The field informations
+     *
+     * @return array
+     */
+    protected function formatValueData($value, $fieldNameInfos)
+    {
+        switch ($fieldNameInfos['attribute']->getAttributeType()) {
+            case 'pim_catalog_price_collection':
+                $value = FieldNameBuilder::splitCollection($value);
+
+                array_map(function ($value) use ($fieldNameInfos) {
+                    return $this->formatPrice($value, $fieldNameInfos);
+                }, $value);
+                break;
+            case 'pim_catalog_metric':
+                $value = $this->formatMetric($value, $fieldNameInfos);
+                break;
+            case 'pim_catalog_multiselect':
+                $value = FieldNameBuilder::splitCollection($value);
+                break;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Format price cell
+     * @param string $value          The value content
+     * @param array  $fieldNameInfos The field informations
+     *
+     * @return array
+     */
+    protected function formatPrice($value, $fieldNameInfos)
+    {
+        if ('' === $value) {
+            return [];
+        }
+
+        //Due to the multiple column for price collections
+        if (isset($fieldNameInfos['price_currency'])) {
+            $currency = $fieldNameInfos['price_currency'];
+        } else {
+            list($value, $currency) = FieldNameBuilder::splitUnitValue($value);
+        }
+
+        return ['data' => $value, 'currency' => $currency];
+    }
+
+    /**
+     * Format metric cell
+     * @param string $value          The value content
+     * @param array  $fieldNameInfos The field informations
+     *
+     * @return array
+     */
+    protected function formatMetric($value, $fieldNameInfos)
+    {
+        if ('' === $value) {
+            return [];
+        }
+
+        //Due to the multi column format for metrics
+        if (isset($fieldNameInfos['metric_unit'])) {
+            $unit = $fieldNameInfos['metric_unit'];
+        } else {
+            list($value, $unit) = FieldNameBuilder::splitUnitValue($value);
+        }
+
+        return ['data' => $value, 'unit' => $unit];
+    }
+
+    /**
+     * Method to make the array_merge_recursive more "smart" for price collections
+     *
+     * @param array $collection The collection in which we add the element
+     * @param array $value      The structured value to add to the collection
+     *
+     * @return array
+     */
+    protected function addFieldToCollection($collection, $value)
+    {
+        $field = key($value);
+
+        //Needed for prices collections in multiple columns
+        if (isset($collection[$field]) &&
+            isset($collection[$field][0]['data']) &&
+            is_array($collection[$field][0]['data'])
+        ) {
+            $newFieldValue = reset($value[$field]);
+
+            foreach ($collection[$field] as $key => $fieldValue) {
+                if (array_key_exists('locale', $newFieldValue) &&
+                    array_key_exists('scope', $newFieldValue) &&
+                    $newFieldValue['locale'] === $fieldValue['locale'] &&
+                    $newFieldValue['scope'] === $fieldValue['scope']
+                ) {
+                    $collection[$field][$key]['data'] = array_merge($fieldValue['data'], $newFieldValue['data']);
+                }
+            }
+        } else {
+            $collection = array_merge_recursive($collection, $value);
+        }
+
+        return $collection;
     }
 }
