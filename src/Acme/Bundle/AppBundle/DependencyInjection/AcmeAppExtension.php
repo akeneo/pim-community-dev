d045dc926a7e17f6c99541085ea53a922262ecd4
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-02-24 18:45:58 +0100

    PIM-3797: [DELETE] override ProductValue in AcmeAppBundle

--- src/Acme/Bundle/AppBundle/DependencyInjection/AcmeAppExtension.php
+++ src/Acme/Bundle/AppBundle/DependencyInjection/AcmeAppExtension.php
@@ -24,5 +24,6 @@ class AcmeAppExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('entities.yml');
     }
 }
---
+++ src/Acme/Bundle/AppBundle/Entity/ProductValue.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Acme\Bundle\AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Pim\Bundle\CatalogBundle\Model\AbstractProductValue as PimProductValue;
+
+class ProductValue extends PimProductValue
+{
+}
