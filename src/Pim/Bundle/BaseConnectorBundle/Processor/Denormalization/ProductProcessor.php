d63b27f953b01afc8d2936889f498011419ebd05
sumbobyboys <sumbobyboys@gmail.com>
2015-02-25 16:34:01 +0100

    PIM-3796: fix some bugs with the csv formater

--- src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/ProductProcessor.php
+++ src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/ProductProcessor.php
@@ -7,6 +7,7 @@ use Akeneo\Component\StorageUtils\Detacher\ObjectDetacherInterface;
 use Pim\Bundle\BaseConnectorBundle\Reader\File\Formater\CsvFormater;
 use Pim\Bundle\CatalogBundle\Manager\ProductManager;
 use Pim\Bundle\CatalogBundle\Model\GroupInterface;
+use Pim\Bundle\CatalogBundle\Updater\ProductUpdaterInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Validator\ValidatorInterface;
 
@@ -25,18 +26,22 @@ class ProductProcessor extends AbstractProcessor
     /** @var CsvFormater */
     protected $csvFormater;
 
+    /** @var ProductUpdaterInterface */
+    protected $productUpdater;
+
     /** @var string */
     protected $format;
 
     /**
-     * @param IdentifiableObjectRepositoryInterface $repository   repository to search the object in
-     * @param DenormalizerInterface                 $denormalizer denormalizer used to transform array to object
-     * @param ValidatorInterface                    $validator    validator of the object
-     * @param ObjectDetacherInterface               $detacher     detacher to remove it from UOW when skip
-     * @param ProductManager                        $manager      product manager
-     * @param CsvFormater                           $csvFormater      product manager
-     * @param string                                $class        class of the object to instanciate in case if need
-     * @param string                                $format       format use to denormalize
+     * @param IdentifiableObjectRepositoryInterface $repository     repository to search the object in
+     * @param DenormalizerInterface                 $denormalizer   denormalizer used to transform array to object
+     * @param ValidatorInterface                    $validator      validator of the object
+     * @param ObjectDetacherInterface               $detacher       detacher to remove it from UOW when skip
+     * @param ProductManager                        $manager        product manager
+     * @param CsvFormater                           $csvFormater    product manager
+     * @param ProductUpdaterInterface               $productUpdater product manager
+     * @param string                                $class          class of the object to instanciate in case if need
+     * @param string                                $format         format use to denormalize
      */
     public function __construct(
         IdentifiableObjectRepositoryInterface $repository,
@@ -45,14 +50,16 @@ class ProductProcessor extends AbstractProcessor
         ObjectDetacherInterface $detacher,
         ProductManager $manager,
         CsvFormater $csvFormater,
+        ProductUpdaterInterface $productUpdater,
         $class,
         $format
     ) {
         parent::__construct($repository, $denormalizer, $validator, $detacher, $class);
 
-        $this->manager     = $manager;
-        $this->csvFormater = $csvFormater;
-        $this->format      = $format;
+        $this->manager        = $manager;
+        $this->csvFormater    = $csvFormater;
+        $this->productUpdater = $productUpdater;
+        $this->format         = $format;
     }
 
     /**
@@ -60,10 +67,21 @@ class ProductProcessor extends AbstractProcessor
      */
     public function process($item)
     {
-        $identifier = $item[$this->repository->getIdentifierProperties()[0]];
+        $item = $this->csvFormater->convertToStructured($item);
+        $identifier = $item[$this->repository->getIdentifierProperties()[0]][0]['data'];
         $product    = $this->findOrCreateProduct($identifier);
 
-        $product = $this->denormalizer->denormalize($item, $this->class, $this->format, ['entity' => $product]);
+        unset($item[$this->repository->getIdentifierProperties()[0]]);
+        unset($item['associations']);
+        unset($item['family']);
+        unset($item['categories']);
+        unset($item['enabled']);
+
+        foreach ($item as $field => $values) {
+            foreach ($values as $value) {
+                $this->productUpdater->setValue([$product], $field, $value['data'], $value['locale'], $value['scope']);
+            }
+        }
 
         return $product;
     }
