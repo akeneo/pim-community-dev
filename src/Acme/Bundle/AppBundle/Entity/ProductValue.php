867c4405b39b2199ea13d6a08ef2da4077accf31
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-02-25 11:12:35 +0100

    PIM-3797: [DELETE] link Car and Moto referentials to ProductValue

--- src/Acme/Bundle/AppBundle/Entity/ProductValue.php
+++ src/Acme/Bundle/AppBundle/Entity/ProductValue.php
@@ -7,4 +7,63 @@ use Pim\Bundle\CatalogBundle\Model\AbstractProductValue as PimProductValue;
 
 class ProductValue extends PimProductValue
 {
+    /** @var ArrayCollection */
+    protected $cars;
+
+    /** @var Moto */
+    protected $moto;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->cars = new ArrayCollection();
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCars()
+    {
+        return $this->cars;
+    }
+
+    /**
+     * @param ArrayCollection $cars
+     */
+    public function setCars(ArrayCollection $cars)
+    {
+        $this->cars = $cars;
+    }
+
+    /**
+     * @param Car $car
+     */
+    public function addCar(Car $car)
+    {
+        $this->cars->add($car);
+    }
+
+    /**
+     * @param Car $car
+     */
+    public function removeCar(Car $car)
+    {
+        $this->cars->removeElement($car);
+    }
+
+    /**
+     * @return Moto
+     */
+    public function getMoto()
+    {
+        return $this->moto;
+    }
+
+    /**
+     * @param Moto $moto
+     */
+    public function setMoto($moto)
+    {
+        $this->moto = $moto;
+    }
 }
