11007ae36afe363eb669b912a55c4b94c09669e9
Olivier Soulet <olivier.soulet@akeneo.com>
2015-02-26 15:30:53 +0100

    PIM-2026: Added oro to pim filter convertion

---
+++ src/Pim/Bundle/DataGridBundle/Adapter/OroToPimGridFilterAdapter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Adapter;
+
+use Pim\Bundle\DataGridBundle\Extension\MassAction\MassActionDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Transform Oro filters into Akeneo PIM filters
+ *
+ * todo: make this class cleaner by transforming for real the oro filters to pim filters
+ *
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class OroToPimGridFilterAdapter implements GridFilterAdapterInterface
+{
+    /** @var MassActionDispatcher */
+    protected $massActionDispatcher;
+
+    /**
+     * @param MassActionDispatcher $massActionDispatcher
+     */
+    public function __construct(MassActionDispatcher $massActionDispatcher)
+    {
+        $this->massActionDispatcher = $massActionDispatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(Request $request)
+    {
+        $products =  $this->massActionDispatcher->dispatch($request);
+
+        $productIds = [];
+        foreach ($products as $product) {
+            $productIds[] = $product->getId();
+        }
+
+        $filter = ['field' => 'id', 'operator' => 'IN', 'value' => $productIds];
+
+        return [$filter];
+    }
+}
