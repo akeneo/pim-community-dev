d37a0f2789754428ee7ee71f565805786e39d3cd
Adrien Pétremann <petremann.adrien@gmail.com>
2015-02-26 16:24:49 +0100

    PIM-2026: Clean mass edit command

--- src/Pim/Bundle/EnrichBundle/Command/MassEditStatusCommand.php
+++ src/Pim/Bundle/EnrichBundle/Command/MassEditStatusCommand.php
@@ -3,18 +3,13 @@
 namespace Pim\Bundle\EnrichBundle\Command;
 
 use Akeneo\Bundle\BatchBundle\Entity\JobExecution;
-use Akeneo\Bundle\BatchBundle\Event\JobExecutionEvent;
 use Akeneo\Bundle\BatchBundle\Job\BatchStatus;
 use Akeneo\Bundle\BatchBundle\Job\ExitStatus;
-use Akeneo\Component\StorageUtils\Cursor\CursorInterface;
-use Pim\Bundle\CatalogBundle\Query\ProductQueryBuilderInterface;
 use Pim\Bundle\EnrichBundle\MassEditAction\Operation\ProductMassEditOperation;
-use Sortable\Fixture\Event;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
@@ -120,68 +115,6 @@ class MassEditStatusCommand extends ContainerAwareCommand
         $operator->setOperationAlias($rawConfiguration['operationAlias']);
 
         return $operator->getOperation();
-    }
-
-    /**
-     * @param array $products
-     */
-    protected function saveAll(array $products)
-    {
-        $saver = $this->getContainer()->get('pim_catalog.saver.product');
-        $saver->saveAll($products);
-    }
-
-    /**
-     * @param CursorInterface $products
-     *
-     * @return \Akeneo\Component\StorageUtils\Cursor\PaginatorInterface
-     */
-    protected function getPaginator(CursorInterface $products)
-    {
-        $paginatorFactory = $this->getContainer()->get('pim_enrich.product.paginator');
-
-        return $paginatorFactory->createPaginator($products);
-    }
-
-    /**
-     *
-     */
-    protected function getCacheClearer()
-    {
-        return $this->getContainer()->get('pim_transform.cache.product_cache_clearer');
-    }
-
-    /**
-     * @param array $filters
-     *
-     * @return CursorInterface
-     */
-    protected function getProducts(array $filters)
-    {
-        $productQueryBuilder = $this->getProductQueryBuilder();
-
-        $resolver = new OptionsResolver();
-        $resolver->setRequired(['field', 'operator', 'value']);
-        $resolver->setOptional(['locale', 'scope']);
-        $resolver->setDefaults(['locale' => null, 'scope' => null]);
-
-        foreach ($filters as $filter) {
-            $filter = $resolver->resolve($filter);
-            $context = ['locale' => $filter['locale'], 'scope' => $filter['scope']];
-            $productQueryBuilder->addFilter($filter['field'], $filter['operator'], $filter['value'], $context);
-        }
-
-        return $productQueryBuilder->execute();
-    }
-
-    /**
-     * @return ProductQueryBuilderInterface
-     */
-    protected function getProductQueryBuilder()
-    {
-        $factory = $this->getContainer()->get('pim_catalog.query.product_query_builder_factory');
-
-        return $factory->create();
     }
 
     /**
