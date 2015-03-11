69b933b87e4f90b9ef72d55d3c71bb154b1dcabd
Nicolas Dupont <nicolas@akeneo.com>
2015-03-11 11:02:53 +0100

    PIM-3832: use the new updater method to handle enabled, categories, families and groups cases

--- src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/File/Formater/CsvFormater.php
@@ -10,6 +10,8 @@ use Pim\Bundle\TransformBundle\Builder\FieldNameBuilder;
  * @author    Julien Sanchez <julien@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: too "global" naming, content is product tinted
  */
 class CsvFormater
 {
@@ -91,7 +93,7 @@ class CsvFormater
      *          "scope":  "mobile",
      *          "data":   {"data": "10", "unit": "CENTIMETER"}
      *      }],
-     *      "enabled": "1",
+     *      "enabled": true,
      *      "categories": ["tshirt", "men"],
      *      "associations": {
      *          "XSELL": {
@@ -111,6 +113,10 @@ class CsvFormater
         foreach ($product as $column => $value) {
             $value  = $this->convertToStructuredField($column, $value);
             $result = $this->addFieldToCollection($result, $value);
+
+            // TODO: filter empty values, for instance a simple select with "" should not be kept as an update
+            // TODO: does not work with media
+            // TODO: does not work with no groups
         }
 
         return $result;
@@ -134,7 +140,9 @@ class CsvFormater
             return ['associations' => [$associationTypeCode => [$associatedWith => $value]]];
         } elseif (in_array($column, ['categories', 'groups'])) {
             return [$column => FieldNameBuilder::splitCollection($value)];
-        } elseif (in_array($column, ['enabled', 'family'])) {
+        } elseif ('enabled' === $column) {
+            return [$column => (bool) $value];
+        } elseif ('family' === $column) {
             return [$column => $value];
         } else {
             return $this->formatValue($column, $value);
