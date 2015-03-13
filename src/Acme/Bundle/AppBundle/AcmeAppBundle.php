5b3aca36e0009dcca82ba302f34edbb8f6250d83
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-03-13 11:36:53 +0100

    PIM-3857: MongoDB implementation of the ProductValue

--- src/Acme/Bundle/AppBundle/AcmeAppBundle.php
+++ src/Acme/Bundle/AppBundle/AcmeAppBundle.php
@@ -2,6 +2,9 @@
 
 namespace Acme\Bundle\AppBundle;
 
+use Akeneo\Bundle\StorageUtilsBundle\AkeneoStorageUtilsBundle;
+use Oro\Bundle\EntityBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -13,4 +16,29 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class AcmeAppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $productMappings = array(
+            realpath(__DIR__ . '/Resources/config/model/doctrine') => 'Acme\Bundle\AppBundle\Model'
+        );
+
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createYamlMappingDriver(
+                $productMappings,
+                ['doctrine.orm.entity_manager'],
+                'akeneo_storage_utils.storage_driver.doctrine/orm'
+            )
+        );
+
+        if (class_exists(AkeneoStorageUtilsBundle::DOCTRINE_MONGODB)) {
+            $mongoDBClass = AkeneoStorageUtilsBundle::DOCTRINE_MONGODB;
+            $container->addCompilerPass(
+                $mongoDBClass::createYamlMappingDriver(
+                    $productMappings,
+                    ['doctrine.odm.mongodb.document_manager'],
+                    'akeneo_storage_utils.storage_driver.doctrine/mongodb-odm'
+                )
+            );
+        }
+    }
 }
