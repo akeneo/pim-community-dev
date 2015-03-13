93f8029e8492cd0b0e4b25c9437ed4575518be91
willy-ahva <willy.mesnage@akeneo.com>
2015-03-13 15:06:21 +0100

    PIM-3896: Replace BaseConnector Processor Unit Test by PHPSpec

---
+++ spec/Pim/Bundle/BaseConnectorBundle/Reader/File/FileReaderSpec.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: willy
+ * Date: 13/03/15
+ * Time: 15:35
+ */
+
+namespace spec\Pim\Bundle\BaseConnectorBundle\Reader\File;
+
+
+class FileReaderSpec {
+
+}
