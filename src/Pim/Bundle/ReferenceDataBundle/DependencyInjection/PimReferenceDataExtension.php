80623d14edfec2b9bf8b0cfe7f1eb05c5c888a03
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-03-13 14:21:40 +0100

    PIM-3857: allow ReferenceData to be normalized for Mongo

--- src/Pim/Bundle/ReferenceDataBundle/DependencyInjection/PimReferenceDataExtension.php
+++ src/Pim/Bundle/ReferenceDataBundle/DependencyInjection/PimReferenceDataExtension.php
@@ -2,10 +2,10 @@
 
 namespace Pim\Bundle\ReferenceDataBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -29,5 +29,22 @@ class PimReferenceDataExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('attribute_types.yml');
+
+        $this->loadStorageDriverFiles($container);
+    }
+
+    /**
+     * Load the services dedicated to the storage driver
+     *
+     * @param ContainerBuilder $container
+     */
+    protected function loadStorageDriverFiles(ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $storageDriver = $container->getParameter('pim_catalog_product_storage_driver');
+        $storageConfig = sprintf('storage_driver/%s.yml', $storageDriver);
+        if (file_exists(__DIR__ . '/../Resources/config/' . $storageConfig)) {
+            $loader->load($storageConfig);
+        }
     }
 }
---
+++ src/Pim/Bundle/ReferenceDataBundle/MongoDB/Normalizer/ReferenceDataNormalizer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pim\Bundle\ReferenceDataBundle\MongoDB\Normalizer;
+
+use Pim\Component\ReferenceData\Model\ReferenceDataInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Normalize a reference data very simply to store it as mongodb_json
+ * @author    Julien Janvier <nicolas@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ReferenceDataNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data = ['code' => $object->getCode()];
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ReferenceDataInterface && 'mongodb_json' === $format;
+    }
+}
