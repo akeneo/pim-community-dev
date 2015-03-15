0883060f628d8349f3beb0332811607dd4f62b1c
Nicolas Dupont <nicolas@akeneo.com>
2015-03-15 17:55:34 +0100

    PIM-3832: rename formatter classes and update references

--- src/Pim/Bundle/BaseConnectorBundle/DependencyInjection/PimBaseConnectorExtension.php
+++ src/Pim/Bundle/BaseConnectorBundle/DependencyInjection/PimBaseConnectorExtension.php
@@ -33,6 +33,7 @@ class PimBaseConnectorExtension extends Extension
         $loader->load('models.yml');
         $loader->load('factories.yml');
         $loader->load('repositories.yml');
+        $loader->load('converters.yml');
 
         $storageDriver = $container->getParameter('pim_catalog_product_storage_driver');
         $storageConfig = sprintf('storage_driver/%s.yml', $storageDriver);
