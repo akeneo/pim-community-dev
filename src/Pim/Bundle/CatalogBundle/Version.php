24d220cc5928c53c38b3db8125462188853cb4ef
Jean-Marie Leroux <jmleroux.pro@gmail.com>
2015-03-16 10:21:23 +0100

    Preparing version 1.2.33

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.2.32';
+    const VERSION = '1.2.33';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Acrobatty Bunny';
