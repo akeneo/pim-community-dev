0e54d2bc9c0528643d633feafc8bfe771d94e8e0
Adrien Pétremann <petremann.adrien@gmail.com>
2015-03-17 15:47:37 +0100

    PIM-3826: PHPspecs

--- spec/Pim/Bundle/EnrichBundle/MassEditAction/Operation/AddToGroupsSpec.php
+++ spec/Pim/Bundle/EnrichBundle/MassEditAction/Operation/AddToGroupsSpec.php
@@ -2,27 +2,33 @@
 
 namespace spec\Pim\Bundle\EnrichBundle\MassEditAction\Operation;
 
-use Akeneo\Component\StorageUtils\Saver\BulkSaverInterface;
-use Doctrine\ORM\AbstractQuery;
+use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
 use Pim\Bundle\CatalogBundle\Model\GroupInterface;
-use Pim\Bundle\CatalogBundle\Model\ProductInterface;
-use Pim\Bundle\CatalogBundle\Repository\GroupRepositoryInterface;
+use Prophecy\Argument;
 
 class AddToGroupsSpec extends ObjectBehavior
 {
-    function let(
-        GroupRepositoryInterface $groupRepository,
-        BulkSaverInterface $productSaver,
-        GroupInterface $shirts,
-        GroupInterface $pants
-    ) {
-        $this->beConstructedWith($groupRepository, $productSaver);
-    }
-
-    function it_is_a_mass_edit_action()
+    function it_is_a_mass_edit_operation()
     {
         $this->shouldImplement('Pim\Bundle\EnrichBundle\MassEditAction\Operation\MassEditOperationInterface');
+        $this->shouldImplement('Pim\Bundle\EnrichBundle\MassEditAction\Operation\ConfigurableOperationInterface');
+        $this->shouldImplement('Pim\Bundle\EnrichBundle\MassEditAction\Operation\BatchableOperationInterface');
+    }
+
+    function it_stores_the_groups_to_add_the_products_to(
+        ArrayCollection $collection,
+        GroupInterface $office,
+        GroupInterface $bedroom
+    ) {
+        $this->getGroups()->shouldReturnAnInstanceOf('Doctrine\Common\Collections\ArrayCollection');
+
+        $collection->add($office);
+        $collection->add($bedroom);
+
+        $this->setGroups($collection);
+
+        $this->getGroups()->shouldReturn($collection);
     }
 
     function it_provides_a_form_type()
@@ -30,29 +36,72 @@ class AddToGroupsSpec extends ObjectBehavior
         $this->getFormType()->shouldReturn('pim_enrich_mass_add_to_groups');
     }
 
-    function it_provides_form_options($groupRepository, $shirts, $pants)
+    function it_provides_form_options()
     {
-        $groupRepository->getAllGroupsExceptVariant()->willReturn([$shirts, $pants]);
-
-        $this->getFormOptions()->shouldReturn(['groups' => [$shirts, $pants]]);
+        $this->getFormOptions()->shouldReturn([]);
     }
 
-    function it_adds_products_to_groups_when_performing_the_operation(
-        AbstractQuery $query,
-        ProductInterface $product1,
-        ProductInterface $product2,
-        $shirts,
-        $pants
+    function it_provides_items_name_it_works_on()
+    {
+        $this->getItemsName()->shouldReturn('product');
+    }
+
+    function it_provides_an_alias()
+    {
+        $this->getAlias()->shouldReturn('add-to-groups');
+    }
+
+    function it_provides_correct_actions_to_apply_on_products(
+        GroupInterface $office,
+        GroupInterface $bedroom,
+        ArrayCollection $collection
     ) {
-        $this->setObjectsToMassEdit([$product1, $product2]);
+        $office->getCode()->willReturn('office_room');
+        $bedroom->getCode()->willReturn('bedroom');
 
-        $this->setGroups([$shirts, $pants]);
+        $collection->add($office);
+        $collection->add($bedroom);
 
-        $shirts->addProduct($product1)->shouldBeCalled();
-        $shirts->addProduct($product2)->shouldBeCalled();
-        $pants->addProduct($product1)->shouldBeCalled();
-        $pants->addProduct($product2)->shouldBeCalled();
+        $this->setGroups($collection);
+        $collection->map(Argument::type('closure'))->willReturn($collection);
+        $collection->toArray()->willReturn(['office_room', 'bedroom']);
 
-        $this->perform();
+        $this->getActions()->shouldReturn(
+            [
+                [
+                    'field' => 'groups',
+                    'value' => ['office_room', 'bedroom']
+                ]
+            ]
+        );
+    }
+
+    function it_provides_a_batch_job_code()
+    {
+        $this->getBatchJobCode()->shouldReturn('add_product_value');
+    }
+
+    function it_provides_formatted_batch_config_for_the_job(
+        GroupInterface $office,
+        GroupInterface $bedroom,
+        ArrayCollection $collection
+    ) {
+        $office->getCode()->willReturn('office_room');
+        $bedroom->getCode()->willReturn('bedroom');
+
+        $collection->add($office);
+        $collection->add($bedroom);
+
+        $this->setGroups($collection);
+        $collection->map(Argument::type('closure'))->willReturn($collection);
+        $collection->toArray()->willReturn(['office_room', 'bedroom']);
+
+        $this->setFilters([
+            ['id', 'IN', ['22', '7']]
+        ]);
+
+        $this->getBatchConfig()->shouldReturn(
+            '{\"filters\":[[\"id\",\"IN\",[\"22\",\"7\"]]],\"actions\":[{\"field\":\"groups\",\"value\":[\"office_room\",\"bedroom\"]}]}'
+        );
     }
 }
