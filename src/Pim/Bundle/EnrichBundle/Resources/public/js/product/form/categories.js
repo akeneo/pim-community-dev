232c3a3ecd9f6c9934046d35c12278db3ea85143
Filips Alpe <filips@akeneo.com>
2015-03-18 12:10:39 +0200

    PIM-3821: First implementation of the categories tab

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/categories.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/categories.js
@@ -3,15 +3,28 @@
 define(
     [
         'underscore',
+        'backbone',
         'pim/form',
         'text!pim/template/product/tab/categories',
         'pim/tree/associate'
     ],
-    function(_, BaseForm, formTemplate, TreeAssociate) {
+    function(_, Backbone, BaseForm, formTemplate, TreeAssociate) {
         return BaseForm.extend({
             template: _.template(formTemplate),
             className: 'tab-pane',
             id: 'product-categories',
+            events: {
+                'click .nav-tabs li': 'changeTree',
+                'change #hidden-tree-input': 'updateModel'
+            },
+            treeAssociate: null,
+            initialize: function () {
+                this.state = new Backbone.Model();
+
+                this.state.set('selectedCategories', [116, 128]);
+
+                BaseForm.prototype.initialize.apply(this, arguments);
+            },
             configure: function () {
                 this.getRoot().addTab('categories', 'Categories');
 
@@ -20,13 +33,34 @@ define(
             render: function () {
                 this.$el.html(
                     this.template({
-                        state: this.getRoot().state.toJSON()
+                        product: this.getData(),
+                        locale: this.getRoot().state.get('locale'),
+                        state: this.state.toJSON(),
+                        trees: this.loadTrees()
                     })
                 );
 
                 this.$el.appendTo(this.getRoot().$('.form-container .tab-pane[data-tab="categories"]'));
 
+                if (!this.treeAssociate) {
+                    this.treeAssociate = new TreeAssociate('#trees', '#hidden-tree-input');
+                }
+
                 return this;
+            },
+            loadTrees: function () {
+                return [
+                    {id: 1, code: 'master', label: 'master', associated: true},
+                    {id: 2, code: 'sales', label: 'sales', associated: false}
+                ];
+            },
+            changeTree: function (event) {
+                this.state.set('currentTree', event.currentTarget.dataset.tree);
+
+                this.treeAssociate.switchTree(event.currentTarget.dataset.treeId);
+            },
+            updateModel: function (event) {
+                this.state.set('selectedCategories', event.currentTarget.value.split(','));
             }
         });
     }
