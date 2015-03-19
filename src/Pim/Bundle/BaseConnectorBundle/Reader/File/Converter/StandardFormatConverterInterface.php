381c4e924109246f65048eda7afcbc95f4904d5c
Nicolas Dupont <nicolas@akeneo.com>
2015-03-19 09:58:11 +0100

    PIM-3832: re-work attribute option import

--- src/Pim/Bundle/BaseConnectorBundle/Reader/File/Converter/StandardFormatConverterInterface.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/File/Converter/StandardFormatConverterInterface.php
@@ -8,6 +8,8 @@ namespace Pim\Bundle\BaseConnectorBundle\Reader\File\Converter;
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: rename to ArrayConverter
  */
 interface StandardFormatConverterInterface
 {
