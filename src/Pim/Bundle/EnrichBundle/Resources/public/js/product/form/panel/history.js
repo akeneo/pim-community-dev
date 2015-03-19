d13b0779513131e77c2f5e200a8944e550ae0dfe
Filips Alpe <filips@akeneo.com>
2015-03-19 15:20:34 +0200

    PIM-3821: Add modal for the history

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/panel/history.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/panel/history.js
@@ -3,16 +3,23 @@
 define(
     [
         'underscore',
+        'backbone',
         'pim/form',
         'text!pim/template/product/panel/history',
+        'text!pim/template/product/panel/history-modal',
         'routing',
+        'backbone/bootstrap-modal'
     ],
-    function(_, BaseForm, template, Routing) {
+    function(_, Backbone, BaseForm, template, modalTemplate, Routing) {
         return BaseForm.extend({
             template: _.template(template),
+            modalTemplate: _.template(modalTemplate),
             className: 'panel-pane',
             code: 'history',
             versions: [],
+            events: {
+                'click #expand-history': 'showHistoryModal'
+            },
             configure: function () {
                 this.getRoot().addPanel('history', 'History');
 
@@ -25,9 +32,10 @@ define(
 
                         this.$el.html(
                             this.template({
-                                versions: this.versions
+                                versions: this.versions.slice(0, 10)
                             })
                         );
+                        this.delegateEvents();
                     }, this));
                 }
 
@@ -42,6 +50,38 @@ define(
                         }
                     )
                 );
+            },
+            showHistoryModal: function () {
+                var modal = new Backbone.BootstrapModal({
+                    className: 'modal modal-large history-modal',
+                    modalOptions: {
+                        backdrop: 'static',
+                        keyboard: false
+                    },
+                    allowCancel: true,
+                    okCloses: false,
+                    cancelText: _.__('pim_datagrid.column_configurator.cancel'),
+                    title: _.__('pim_datagrid.column_configurator.title'),
+                    content: this.modalTemplate({ versions: this.versions }),
+                    okText: _.__('pim_datagrid.column_configurator.apply')
+                });
+
+
+                modal.open();
+
+                modal.$el.on('click', 'tbody tr:not(.changeset)', function (event) {
+                    var $row = $(event.currentTarget);
+                    var $body = $row.parent();
+                    $body.find('tr.changeset').addClass('hide');
+                    $body.find('i.icon-chevron-down').toggleClass('icon-chevron-right icon-chevron-down');
+
+                    if (!$row.hasClass('expanded')) {
+                        $row.next('tr.changeset').removeClass('hide');
+                        $row.find('i').toggleClass('icon-chevron-right icon-chevron-down');
+                    }
+                    $row.siblings().removeClass('expanded');
+                    $row.toggleClass('expanded');
+                });
             }
         });
     }
