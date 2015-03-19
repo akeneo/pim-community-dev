64982c61e2ecd88f9609ca6778f8d1ad5aa37255
skeleton <marie.bochu@akeneo.com>
2015-03-19 10:20:52 +0100

    PIM-3857: fix author email

--- src/Pim/Bundle/ReferenceDataBundle/MongoDB/Normalizer/ReferenceDataNormalizer.php
+++ src/Pim/Bundle/ReferenceDataBundle/MongoDB/Normalizer/ReferenceDataNormalizer.php
@@ -7,7 +7,8 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * Normalize a reference data very simply to store it as mongodb_json
- * @author    Julien Janvier <nicolas@akeneo.com>
+ *
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
