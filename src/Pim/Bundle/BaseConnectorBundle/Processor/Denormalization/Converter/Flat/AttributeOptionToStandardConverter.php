11306ff63177287eca4afa91786ec8a0a950078f
Nicolas Dupont <nicolas@akeneo.com>
2015-03-19 10:34:52 +0100

    PIM-3832: cosmetic

--- src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/Converter/Flat/AttributeOptionToStandardConverter.php
+++ src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/Converter/Flat/AttributeOptionToStandardConverter.php
@@ -14,8 +14,6 @@ use Symfony\Component\Validator\Exception\InvalidOptionsException;
  * @author    Nicolas Dupont <nicola@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
- * TODO: rename to ArrayConverter + use Flat or Structured
  */
 class AttributeOptionToStandardConverter implements StandardArrayConverterInterface
 {
