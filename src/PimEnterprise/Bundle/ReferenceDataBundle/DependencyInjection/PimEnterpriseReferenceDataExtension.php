1e86d0acea7f83912035291c979f63e15ccc044a
skeleton <marie.bochu@akeneo.com>
2015-03-24 18:22:45 +0100

    PIM-3858: add ReferenceDataPublisher

--- src/PimEnterprise/Bundle/ReferenceDataBundle/DependencyInjection/PimEnterpriseReferenceDataExtension.php
+++ src/PimEnterprise/Bundle/ReferenceDataBundle/DependencyInjection/PimEnterpriseReferenceDataExtension.php
@@ -21,5 +21,6 @@ class PimEnterpriseReferenceDataExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('publishers.yml');
     }
 }
