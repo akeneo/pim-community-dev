42a526da1eb4857255993ec4c4189b2fcc930a3e
Filips Alpe <filips@akeneo.com>
2015-03-26 19:17:45 +0200

    PIM-3821: Full page view for inline edit

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/inline-edit-action.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/inline-edit-action.js
@@ -4,18 +4,21 @@ define(
     [
         'jquery',
         'underscore',
-        'backbone',
         'oro/datagrid/model-action',
         'oro/loading-mask',
         'pim/form-builder',
-        'pim/product-manager',
-        'backbone/bootstrap-modal'
+        'pim/product-manager'
     ],
-    function($, _, Backbone, ModelAction, LoadingMask, FormBuilder, ProductManager) {
+    function($, _, ModelAction, LoadingMask, FormBuilder, ProductManager) {
         return ModelAction.extend({
             template: _.template(
                 '<div id="product-edit-form"></div>'
             ),
+            initialize: function () {
+                _.bindAll(this, 'close', 'onEscape');
+
+                return ModelAction.prototype.initialize.apply(this, arguments);
+            },
             run: function() {
                 var loadingMask = new LoadingMask();
                 loadingMask.render().$el.appendTo($('#container'));
@@ -25,35 +28,48 @@ define(
                     FormBuilder.build('pim/product-edit-form'),
                     ProductManager.get(this.model.get('id'))
                 ).done(_.bind(function(form, product) {
-                    var modal = new Backbone.BootstrapModal({
-                        className: 'modal modal-large',
-                        modalOptions: {
-                            backdrop: 'static',
-                            keyboard: false
-                        },
-                        content: this.template()
-                    });
-                    modal.open();
+                    this.$el.html(this.template())
+                        .appendTo('body')
+                        .css({
+                            position:   'fixed',
+                            top:        0,
+                            left:       0,
+                            bottom:     0,
+                            right:      0,
+                            background: 'white',
+                            'z-index':  999
+                        });
 
                     form.setData(product);
                     form.setElement('#product-edit-form').render();
-                    form.$el.parent().before(
+                    form.$el.parent().prepend(
                         $('<i class="icon-remove"></i>').css({
-                            'font-size': '30px',
-                            'color': 'white',
-                            'position': 'absolute',
-                            'left': '-32px',
-                            'top': '-30px',
-                            'opacity': '0.8',
-                            'cursor': 'pointer'
-                        }).on('click', function () {
-                            modal.close();
-                        })
+                            'font-size': '22px',
+                            color:       '#999',
+                            position:    'absolute',
+                            left:        '2px',
+                            top:         '2px',
+                            cursor:      'pointer',
+                            'z-index':   999
+                        }).on('click', this.close)
                     );
+
+                    $(document).on('keyup', this.onEscape);
 
                     loadingMask.hide();
                     loadingMask.$el.remove();
                 }, this));
+            },
+
+            onEscape: function (e) {
+                if (27 === e.which) {
+                    this.close();
+                }
+            },
+
+            close: function () {
+                $(document).off('keyup', this.onEscape);
+                this.$el.remove();
             }
         });
     }
