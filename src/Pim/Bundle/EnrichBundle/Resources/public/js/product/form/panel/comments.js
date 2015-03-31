d2c209243739827ddf337eba3a502e3341c43e13
sumbobyboys <sumbobyboys@gmail.com>
2015-03-31 14:50:36 +0200

    PIM-3983: comment style revamp

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/panel/comments.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/panel/comments.js
@@ -13,17 +13,15 @@ define(
     function (_, Backbone, BaseForm, template, replyForm, Routing, messenger) {
         return BaseForm.extend({
             template: _.template(template),
-            replyFormTemplate: _.template(replyForm),
             className: 'panel-pane',
             code: 'history',
             comments: [],
             events: {
-                'keyup .create-comment textarea': 'toggleButtons',
-                'click .create-comment .comment-btn.btn-primary': 'saveComment',
+                'keyup .comment-create textarea, .reply-to-comment textarea': 'toggleButtons',
+                'click .comment-create .comment-btn': 'saveComment',
                 'click .remove-comment' : 'removeComment',
-                'click .reply-to-comment' : 'showReplyForm',
-                'click .comment-reply input[type="reset"]' : 'hideReplyForm',
-                'click .comment-reply .comment-btn.btn-primary': 'saveReply'
+                'click .comment-thread .send-comment': 'saveReply',
+                'click .comment-thread .cancel-comment': 'cancelComment'
             },
             initialize: function () {
                 this.comment = new Backbone.Model();
@@ -32,8 +30,6 @@ define(
             },
             configure: function () {
                 this.getRoot().addPanel('comments', 'Comments');
-
-                this.$replyForm = $(this.replyFormTemplate());
 
                 return BaseForm.prototype.configure.apply(this, arguments);
             },
@@ -65,19 +61,25 @@ define(
                     )
                 );
             },
-            toggleButtons: function () {
-                if (this.$('form.create-comment textarea').val()) {
-                    this.$('form.create-comment .comment-buttons').show();
+            toggleButtons: function (event) {
+                var $element = $(event.currentTarget).parents('.comment-thread, .comment-create');
+                if ($element.find('textarea').val()) {
+                    $element.addClass('active');
                 } else {
-                    this.$('form.create-comment .comment-buttons').hide();
+                    $element.removeClass('active');
                 }
+            },
+            cancelComment: function (event) {
+                var $element = $(event.currentTarget).parents('.comment-thread, .comment-create');
+                $element.find('textarea').val('');
+                $element.removeClass('active');
             },
             saveComment: function () {
                 $.ajax({
                     type: 'POST',
                     url: Routing.generate('pim_enrich_product_comments_rest_post', { id: this.getData().meta.id }),
                     contentType: 'application/json',
-                    data: JSON.stringify({ 'body': this.$('form.create-comment textarea').val() })
+                    data: JSON.stringify({ 'body': this.$('.comment-create textarea').val() })
                 }).done(_.bind(function () {
                     this.render();
                     messenger.notificationFlashMessage('success', 'Your comment has been created successfully.');
@@ -98,28 +100,22 @@ define(
                     messenger.notificationFlashMessage('error', 'An error occured during the deletion of your comment.');
                 });
             },
-            showReplyForm: function (event) {
-                this.$replyForm.data('comment-id', event.currentTarget.dataset.commentId);
-                this.$replyForm.appendTo($(event.currentTarget).closest('.comment-thread').find('.comment-topic')).show();
-            },
-            hideReplyForm: function () {
-                this.$replyForm.find('textarea').val('');
-                this.$replyForm.hide();
-            },
-            saveReply: function () {
+            saveReply: function (event) {
+                var $thread = $(event.currentTarget).parents('.comment-thread');
+
                 $.ajax({
                     type: 'POST',
                     url: Routing.generate(
                         'pim_enrich_product_comment_reply_rest_post',
                         {
                             id: this.getData().meta.id,
-                            commentId: this.$replyForm.data('comment-id')
+                            commentId: $thread.data('comment-id')
                         }
                     ),
                     contentType: 'application/json',
-                    data: JSON.stringify({ 'body': this.$replyForm.find('textarea').val()})
+                    data: JSON.stringify({ 'body': $thread.find('textarea').val()})
                 }).done(_.bind(function () {
-                    this.$replyForm.find('textarea').val('');
+                    $thread.find('textarea').val('');
                     this.render();
                     messenger.notificationFlashMessage('success', 'Your reply has been created successfully.');
                 }, this)).fail(function () {
