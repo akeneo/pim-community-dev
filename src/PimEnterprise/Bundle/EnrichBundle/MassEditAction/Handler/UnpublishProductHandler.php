31509273e8451420c6a309498c0aa8eb97cb7223
Adrien Pétremann <petremann.adrien@gmail.com>
2015-04-02 18:21:51 +0200

    PIM-3829: Use QueryBuilder for unpublish handler

--- src/PimEnterprise/Bundle/EnrichBundle/MassEditAction/Handler/UnpublishProductHandler.php
+++ src/PimEnterprise/Bundle/EnrichBundle/MassEditAction/Handler/UnpublishProductHandler.php
@@ -4,6 +4,7 @@ namespace PimEnterprise\Bundle\EnrichBundle\MassEditAction\Handler;
 
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
+use Akeneo\Bundle\BatchBundle\Step\StepExecutionAwareInterface;
 use Akeneo\Component\StorageUtils\Cursor\PaginatorFactoryInterface;
 use Pim\Bundle\CatalogBundle\Query\ProductQueryBuilderFactoryInterface;
 use PimEnterprise\Bundle\WorkflowBundle\Manager\PublishedProductManager;
@@ -15,11 +16,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UnpublishProductHandler extends AbstractConfigurableStepElement
+class UnpublishProductHandler extends AbstractConfigurableStepElement implements StepExecutionAwareInterface
 {
-    /** @var ProductQueryBuilderFactoryInterface */
-    protected $pqbFactory;
-
     /** @var PublishedProductManager */
     protected $manager;
 
@@ -32,24 +30,27 @@ class UnpublishProductHandler extends AbstractConfigurableStepElement
     /** @var StepExecution */
     protected $stepExecution;
 
+    /** @var ProductQueryBuilderFactoryInterface */
+    protected $publishedPqbFactory;
+
     /**
      * Constructor.
      *
-     * @param ProductQueryBuilderFactoryInterface $pqbFactory
+     * @param ProductQueryBuilderFactoryInterface $publishedPqbFactory
      * @param PublishedProductManager             $manager
      * @param PublishedProductRepositoryInterface $repository
      * @param PaginatorFactoryInterface           $paginatorFactory
      */
     public function __construct(
-        ProductQueryBuilderFactoryInterface $pqbFactory,
+        ProductQueryBuilderFactoryInterface $publishedPqbFactory,
         PublishedProductManager $manager,
         PublishedProductRepositoryInterface $repository,
         PaginatorFactoryInterface $paginatorFactory
     ) {
-        $this->pqbFactory = $pqbFactory;
         $this->manager = $manager;
         $this->repository = $repository;
         $this->paginatorFactory = $paginatorFactory;
+        $this->publishedPqbFactory = $publishedPqbFactory;
     }
 
     /**
@@ -57,18 +58,15 @@ class UnpublishProductHandler extends AbstractConfigurableStepElement
      */
     public function execute(array $configuration)
     {
-        $filter = current($configuration['filters']);
+        $cursor = $this->getPublishedProductsCursor($configuration['filters']);
+        $paginator = $this->paginatorFactory->createPaginator($cursor);
 
-        if ('id' === $filter['field'] && 'IN' === $filter['operator'] && 1 === count($configuration['filters'])) {
-            $publishedProducts = $this->getPublishedProducts($filter);
-        } else {
-            $publishedProducts = $this->getPublishedProductsThroughOriginals($configuration['filters']);
-        }
-
-        foreach ($publishedProducts as $publishedProduct) {
-            $this->manager->unpublish($publishedProduct);
-            $this->stepExecution->incrementSummaryInfo('mass_unpublished');
-            // TODO: validation & skip
+        foreach ($paginator as $publishedProductsPage) {
+            foreach ($publishedProductsPage as $publishedProduct) {
+                $this->manager->unpublish($publishedProduct);
+                $this->stepExecution->incrementSummaryInfo('mass_published');
+                // TODO: validation & skip
+            }
         }
     }
 
@@ -81,7 +79,7 @@ class UnpublishProductHandler extends AbstractConfigurableStepElement
     }
 
     /**
-     * @param StepExecution $stepExecution
+     * {@inheritdoc}
      */
     public function setStepExecution(StepExecution $stepExecution)
     {
@@ -91,47 +89,11 @@ class UnpublishProductHandler extends AbstractConfigurableStepElement
     }
 
     /**
-     * Retrieve published products through original products.
-     * $filter should be ready to be used with the ProductQueryBuilder.
-     *
-     * @param array $filters
-     *
-     * @return array
-     */
-    protected function getPublishedProductsThroughOriginals(array $filters)
-    {
-        $cursor = $this->getProductsCursor($filters);
-        $paginator = $this->paginatorFactory->createPaginator($cursor);
-        $publishedProducts = [];
-
-        foreach ($paginator as $productsPage) {
-            foreach ($productsPage as $product) {
-                $publishedProducts[] = $this->manager->findPublishedProductByOriginal($product);
-            }
-        }
-
-        return $publishedProducts;
-    }
-
-    /**
-     * Retrieve published product through $filter.
-     * Warning: Only ids supported !
-     *
-     * @param array $filter
-     *
-     * @return \Doctrine\Common\Collections\ArrayCollection
-     */
-    protected function getPublishedProducts(array $filter)
-    {
-        return $this->repository->findByIds($filter['value']);
-    }
-
-    /**
      * @return ProductQueryBuilderInterface
      */
-    protected function getProductQueryBuilder()
+    protected function getPublishedProductQueryBuilder()
     {
-        return $this->pqbFactory->create();
+        return $this->publishedPqbFactory->create();
     }
 
     /**
@@ -139,9 +101,9 @@ class UnpublishProductHandler extends AbstractConfigurableStepElement
      *
      * @return \Akeneo\Component\StorageUtils\Cursor\CursorInterface
      */
-    protected function getProductsCursor(array $filters)
+    protected function getPublishedProductsCursor(array $filters)
     {
-        $productQueryBuilder = $this->getProductQueryBuilder();
+        $productQueryBuilder = $this->getPublishedProductQueryBuilder();
 
         $resolver = new OptionsResolver();
         $resolver->setRequired(['field', 'operator', 'value']);
