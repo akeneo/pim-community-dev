54752634461b69f835fa1331c63600c1dc6061b2
Filips Alpe <filips@akeneo.com>
2015-04-09 08:56:18 +0300

    PIM-3973: Add jscs to check js code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,23 @@ module.exports = function (grunt) {
             options: {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
-            },
+            }
+        },
+        jscs: {
+            all: [
+                'Gruntfile.js',
+                'src/**/*.js',
+                '!src/**/lib/**/*.js',
+                '!src/**/DataGridBundle/**/*.js',
+                '!src/**/NavigationBundle/**/*.js'
+            ],
+            options: {
+                config: '.jscsrc',
+                reporter: require('jscs-stylish').path
+            }
         },
         jasmine: {
-            requirejs: {
+            specs: {
                 src: '/bundles/ororequirejs/lib/require.js',
                 options: {
                     specs: 'src/**/spec/**/*Spec.js',
@@ -57,8 +70,10 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-jscs');
 
     grunt.registerTask('test', ['jasmine']);
-    grunt.registerTask('travis', ['jshint','jasmine']);
-    grunt.registerTask('default', ['jshint', 'jasmine']);
+    grunt.registerTask('codestyle', ['jshint', 'jscs']);
+    grunt.registerTask('travis', ['codestyle', 'test']);
+    grunt.registerTask('default', ['codestyle', 'test']);
 };
