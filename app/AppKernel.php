c7c9e0e2cbb8270dce09c3a786e59ef3efc53459
Nicolas Dupont <nicolas@akeneo.com>
2015-04-12 12:07:32 +0200

    PIM-4034: enable debug bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
         ];
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
