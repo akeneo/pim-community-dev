4fd8277487975471c6a20dc6fcf90ab75e0a8da3
sumbobyboys <sumbobyboys@gmail.com>
2015-04-13 18:05:45 +0200

    PIM-3984: created meta extensions for updated and created and added i18n

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/meta/created.js
@@ -0,0 +1,27 @@
+ 'use strict';
+
+define(
+    [
+        'jquery',
+        'underscore',
+        'pim/form',
+        'text!pim/template/product/meta/created'
+    ],
+    function ($, _, BaseForm, formTemplate) {
+        var FormView = BaseForm.extend({
+            tagName: 'span',
+            template: _.template(formTemplate),
+            render: function () {
+                this.$el.html(
+                    this.template({
+                        product: this.getData()
+                    })
+                );
+
+                return this;
+            }
+        });
+
+        return FormView;
+    }
+);
