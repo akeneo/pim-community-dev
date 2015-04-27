0b1a1d8d4020002fe4122f8bf939f14a3e865f2e
Filips Alpe <filips@akeneo.com>
2015-04-27 13:22:57 +0300

    Fix wrong class doc

--- src/PimEnterprise/Bundle/ReferenceDataBundle/Workflow/Comparator/ReferenceDataCollectionComparator.php
+++ src/PimEnterprise/Bundle/ReferenceDataBundle/Workflow/Comparator/ReferenceDataCollectionComparator.php
@@ -18,9 +18,7 @@ use PimEnterprise\Bundle\WorkflowBundle\Comparator\ComparatorInterface;
 /**
  * Comparator which calculate change set for multi select reference data
  *
- * @author    Marie Bochu <marie.bochu@akeneo.com>
- * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author Marie Bochu <marie.bochu@akeneo.com>
  *
  * @see    PimEnterprise\Bundle\WorkflowBundle\Form\ComparatorInterface
  */
