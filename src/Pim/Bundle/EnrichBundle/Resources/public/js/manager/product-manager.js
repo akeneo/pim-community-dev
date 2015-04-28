49b0c43d6b94917143e78066d797c997dc192bac
Filips Alpe <filips@akeneo.com>
2015-04-28 12:04:15 +0300

    PIM-4001 Cache loaded products in product manager

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/manager/product-manager.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/manager/product-manager.js
@@ -3,19 +3,24 @@
 define(['jquery', 'underscore', 'routing', 'pim/attribute-manager'], function ($, _, Routing, AttributeManager) {
     return {
         productValues: null,
+        productPromises: {},
         get: function (id) {
-            var deferred = $.Deferred();
+            if (!(id in this.productPromises)) {
+                var deferred = $.Deferred();
 
-            $.ajax(
-                Routing.generate('pim_enrich_product_rest_get', {id: id}),
-                {
-                    method: 'GET'
-                }
-            ).done(function (product) {
-                deferred.resolve(product);
-            });
+                $.ajax(
+                    Routing.generate('pim_enrich_product_rest_get', {id: id}),
+                    {
+                        method: 'GET'
+                    }
+                ).done(function (product) {
+                    deferred.resolve(product);
+                });
 
-            return deferred.promise();
+                this.productPromises[id] = deferred.promise();
+            }
+
+            return this.productPromises[id];
         },
         save: function (id, data) {
             return $.ajax({
@@ -23,7 +28,9 @@ define(['jquery', 'underscore', 'routing', 'pim/attribute-manager'], function ($
                 url: Routing.generate('pim_enrich_product_rest_get', {id: id}),
                 contentType: 'application/json',
                 data: JSON.stringify(data)
-            }).promise();
+            }).then(_.bind(function (data) {
+                this.productPromises = $.Deferred().resolve(data).promise();
+            }, this)).promise();
         },
         remove: function (id) {
             return $.ajax({
@@ -31,7 +38,9 @@ define(['jquery', 'underscore', 'routing', 'pim/attribute-manager'], function ($
                 url: Routing.generate('pim_enrich_product_remove', {id: id}),
                 headers: { accept: 'application/json' },
                 data: { _method: 'DELETE' }
-            }).promise();
+            }).then(_.bind(function () {
+                delete this.productPromises[id];
+            }), this).promise();
         },
         getValues: function (product) {
             var deferred = $.Deferred();
