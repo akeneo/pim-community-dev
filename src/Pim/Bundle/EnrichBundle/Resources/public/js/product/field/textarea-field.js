f69ea557a681bae8ded6406fff1e46e882163556
sumbobyboys <sumbobyboys@gmail.com>
2015-04-29 17:06:59 +0200

    PIM-3991: WYSIWYG come back

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/field/textarea-field.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/field/textarea-field.js
@@ -1,18 +1,32 @@
 'use strict';
 
-define(['pim/field', 'underscore', 'text!pim/template/product/field/textarea'], function (Field, _, fieldTemplate) {
+define(['pim/field', 'underscore', 'text!pim/template/product/field/textarea', 'summernote'], function (Field, _, fieldTemplate) {
     return Field.extend({
         fieldTemplate: _.template(fieldTemplate),
         fieldType: 'textarea',
         events: {
             'change textarea': 'updateModel'
         },
+        render: function () {
+            Field.prototype.render.apply(this, arguments);
+
+            this.$('textarea').summernote({
+                disableResizeEditor: true,
+                height: 200,
+                iconPrefix: 'incon icon-',
+                toolbar: [
+                    ['font', ['bold', 'italic', 'underline', 'clear']],
+                    ['para', ['ul', 'ol']],
+                    ['insert', ['link']],
+                    ['view', ['codeview']]
+                ]
+            }).on('summernote.blur', _.bind(this.updateModel, this));
+        },
         renderInput: function (context) {
             return this.fieldTemplate(context);
         },
-        updateModel: function (event) {
-            var data = event.currentTarget.value;
-            this.setCurrentValue(data);
+        updateModel: function () {
+            this.setCurrentValue(this.$('textarea').code());
         },
         setFocus: function () {
             this.$('textarea').first().focus();
