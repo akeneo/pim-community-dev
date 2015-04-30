aabea171f7008b0ba1f3387a1c1422660016be4e
Adrien Pétremann <petremann.adrien@gmail.com>
2015-04-30 14:57:19 +0200

    PIM-4007: Add a family reader

---
+++ src/Pim/Bundle/EnrichBundle/Reader/MassEdit/FilteredFamilyReader.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Reader\MassEdit;
+
+use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
+use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
+use Akeneo\Bundle\BatchBundle\Item\ItemReaderInterface;
+use Akeneo\Bundle\BatchBundle\Step\StepExecutionAwareInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityNotFoundException;
+use Pim\Bundle\CatalogBundle\Repository\FamilyRepositoryInterface;
+use Pim\Bundle\EnrichBundle\Entity\Repository\MassEditRepositoryInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FilteredFamilyReader extends AbstractConfigurableStepElement implements
+    ItemReaderInterface,
+    StepExecutionAwareInterface
+{
+    /** @var StepExecution */
+    protected $stepExecution;
+
+    /** @var bool */
+    protected $isExecuted = false;
+
+    /** @var ArrayCollection */
+    protected $families;
+
+    /** @var FamilyRepositoryInterface */
+    protected $familyRepository;
+
+    public function __construct(
+        MassEditRepositoryInterface $massEditRepository,
+        FamilyRepositoryInterface $familyRepository
+    ) {
+        $this->massEditRepository = $massEditRepository;
+        $this->familyRepository   = $familyRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read()
+    {
+        $configuration = $this->getJobConfiguration();
+
+        if (!$this->isExecuted) {
+            $this->isExecuted = true;
+            $this->families = $this->getFamilies($configuration['filters']);
+        }
+
+        $result = $this->families->current();
+
+        if (!empty($result)) {
+            $this->stepExecution->incrementSummaryInfo('read');
+            $this->families->next();
+        } else {
+            $result = null;
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigurationFields()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setStepExecution(StepExecution $stepExecution)
+    {
+        $this->stepExecution = $stepExecution;
+    }
+
+    /**
+     * @param array $filters
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    protected function getFamilies(array $filters)
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setRequired(['field', 'operator', 'value']);
+
+        // In the case of the Family reader, we only have 1 filter for IDS
+        $filter = current($filters);
+        $filter = $resolver->resolve($filter);
+
+        $familiesIds = $filter['value'];
+
+        return new ArrayCollection($this->familyRepository->findByIds($familiesIds));
+    }
+
+    /**
+     * Return the job configuration
+     *
+     * @return array
+     *
+     * @throws EntityNotFoundException
+     */
+    protected function getJobConfiguration()
+    {
+        $jobExecution    = $this->stepExecution->getJobExecution();
+        $massEditJobConf = $this->massEditRepository->findOneBy(['jobExecution' => $jobExecution]);
+
+        if (null === $massEditJobConf) {
+            throw new EntityNotFoundException(sprintf(
+                'No JobConfiguration found for jobExecution with id %s',
+                $jobExecution->getId()
+            ));
+        }
+
+        return json_decode(stripcslashes($massEditJobConf->getConfiguration()), true);
+    }
+}
