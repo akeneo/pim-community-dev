53c8f5ced0b2482d6ef7542c4842ab0e1a10559b
Nicolas Dupont <nicolas@akeneo.com>
2015-05-13 14:44:01 +0200

    PIM-3832: add DI and register the ConnectorBundle

---
+++ src/Pim/Bundle/ConnectorBundle/PimConnectorBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pim\Bundle\ConnectorBundle;
+
+use Akeneo\Bundle\BatchBundle\Connector\Connector;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Connector bundle
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PimConnectorBundle extends Connector
+{
+}
