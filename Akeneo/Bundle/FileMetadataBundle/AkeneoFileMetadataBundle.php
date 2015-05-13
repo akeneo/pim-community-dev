804f38bf2946b9350da486f93fff6a672d729457
Adrien Pétremann <petremann.adrien@gmail.com>
2015-05-13 15:16:50 +0200

    PIM-4155: Create FileMetadataBundle for DI

---
+++ Akeneo/Bundle/FileMetadataBundle/AkeneoFileMetadataBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Akeneo\Bundle\FileMetadataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ */
+class AkeneoFileMetadataBundle extends Bundle
+{
+}
---
+++ Akeneo/Bundle/FileMetadataBundle/DependencyInjection/AkeneoFileMetadataExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Akeneo\Bundle\FileMetadataBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ */
+class AkeneoFileMetadataExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('adapters.yml');
+        $loader->load('readers.yml');
+    }
+}
