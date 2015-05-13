13c10061067b44a569721919fb5a40fda5f0278e
Nicolas Dupont <nicolas@akeneo.com>
2015-05-13 09:34:16 +0200

    PIM-3832: add TODOs

--- src/Pim/Bundle/ConnectorBundle/Doctrine/ORM/Repository/JobConfigurationRepository.php
+++ src/Pim/Bundle/ConnectorBundle/Doctrine/ORM/Repository/JobConfigurationRepository.php
@@ -11,6 +11,8 @@ use Pim\Component\Connector\Repository\JobConfigurationRepositoryInterface;
  * @author    Olivier Soulet <olivier.soulet@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: move this in a Doctrine folder
  */
 class JobConfigurationRepository extends EntityRepository implements JobConfigurationRepositoryInterface
 {
