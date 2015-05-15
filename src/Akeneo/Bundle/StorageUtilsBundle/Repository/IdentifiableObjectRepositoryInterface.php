40d26acd49c80687d2aa865b2d8bdcc49d7ec6e2
Nicolas Dupont <nicolas@akeneo.com>
2015-05-15 17:46:44 +0200

    PIM-3832: add notice in identifiable object repository interface

--- src/Akeneo/Bundle/StorageUtilsBundle/Repository/IdentifiableObjectRepositoryInterface.php
+++ src/Akeneo/Bundle/StorageUtilsBundle/Repository/IdentifiableObjectRepositoryInterface.php
@@ -8,6 +8,8 @@ namespace Akeneo\Bundle\StorageUtilsBundle\Repository;
  * @author    Julien Janvier <jjanvier@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: we should move this class in StorageUtils component
  */
 interface IdentifiableObjectRepositoryInterface
 {
