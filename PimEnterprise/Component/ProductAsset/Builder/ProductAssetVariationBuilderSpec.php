f343810a799b15adbef07a2906271a8c4118901b
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-05-18 23:18:15 +0200

    PIM-4168: Build missing references and variations of an asset

--- PimEnterprise/Component/ProductAsset/Builder/ProductAssetVariationBuilderSpec.php
+++ PimEnterprise/Component/ProductAsset/Builder/ProductAssetVariationBuilderSpec.php
@@ -6,7 +6,7 @@ use PhpSpec\ObjectBehavior;
 use Pim\Bundle\CatalogBundle\Model\ChannelInterface;
 use Pim\Bundle\CatalogBundle\Model\LocaleInterface;
 use Pim\Bundle\CatalogBundle\Repository\ChannelRepositoryInterface;
-use PimEnterprise\Component\ProductAsset\Model\ProductAssetInterface;
+use PimEnterprise\Component\ProductAsset\Model\ProductAssetReferenceInterface;
 use PimEnterprise\Component\ProductAsset\Model\ProductAssetVariationInterface;
 use Prophecy\Argument;
 
@@ -16,43 +16,127 @@ class ProductAssetVariationBuilderSpec extends ObjectBehavior
         ChannelRepositoryInterface $repository,
         ChannelInterface $ecommerce,
         ChannelInterface $print,
+        ChannelInterface $mobile,
         LocaleInterface $en_US,
         LocaleInterface $fr_FR,
         LocaleInterface $de_DE
     ) {
-        $ecommerce->getLocales()->willReturn([$fr_FR]);
-        $print->getLocales()->willReturn([$de_DE, $en_US]);
-        $channels = [$ecommerce, $print];
+        $en_US->isActivated()->willReturn(true);
+        $fr_FR->isActivated()->willReturn(true);
+        $de_DE->isActivated()->willReturn(true);
+
+        $channels = [$ecommerce, $print, $mobile];
         $repository->getFullChannels()->willReturn($channels);
 
         $this->beConstructedWith($repository);
     }
 
-    function it_builds_missing_variations($ecommerce, $print, $en_US, $fr_FR, $de_DE, ProductAssetInterface $asset)
-    {
-        $asset->hasVariation($ecommerce, $fr_FR)->willReturn(false);
-        $asset->hasVariation($print, $en_US)->willReturn(true);
-        $asset->hasVariation($print, $de_DE)->willReturn(false);
+    function it_builds_missing_variations_on_a_non_localized_reference(
+        $ecommerce,
+        $print,
+        $mobile,
+        ProductAssetReferenceInterface $reference
+    ) {
+        $reference->getLocale()->willReturn(null);
 
-        $missings = $this->buildMissing($asset);
+        $reference->hasVariation($ecommerce)->willReturn(false);
+        $reference->hasVariation($print)->willReturn(true);
+        $reference->hasVariation($mobile)->willReturn(false);
+
+        $missings = $this->buildMissing($reference);
         $missings->shouldHaveCount(2);
         $missings->shouldBeArrayOfVariations();
     }
 
-    function it_builds_a_variation(ProductAssetInterface $asset, $en_US, $ecommerce)
-    {
-        $variation = $this->buildOne($asset, $ecommerce, $en_US);
+    function it_builds_missing_variations_on_a_localized_reference(
+        $en_US,
+        $ecommerce,
+        $print,
+        $mobile,
+        ProductAssetReferenceInterface $reference
+    ) {
+        $reference->getLocale()->willReturn($en_US);
+        $ecommerce->hasLocale($en_US)->willReturn(true);
+        $print->hasLocale($en_US)->willReturn(false);
+        $mobile->hasLocale($en_US)->willReturn(true);
 
-        $variation->getAsset()->shouldBe($asset);
-        $variation->getChannel()->shouldBe($ecommerce);
-        $variation->getLocale()->shouldBe($en_US);
+        $reference->hasVariation($ecommerce)->willReturn(false);
+        $reference->hasVariation($mobile)->willReturn(false);
+
+        $missings = $this->buildMissing($reference);
+        $missings->shouldHaveCount(2);
+        $missings->shouldBeArrayOfVariations();
     }
 
-    function it_builds_all_variations(ProductAssetInterface $asset)
+    function it_builds_a_variation($ecommerce, ProductAssetReferenceInterface $reference)
     {
-        $missings = $this->buildAll($asset);
-        $missings->shouldHaveCount(3);
-        $missings->shouldBeArrayOfVariations();
+        $variation = $this->buildOne($reference, $ecommerce);
+
+        $variation->getReference()->shouldBe($reference);
+        $variation->getChannel()->shouldBe($ecommerce);
+    }
+
+    function it_does_not_build_a_variation_if_the_channel_has_not_the_locale_of_the_reference(
+        ProductAssetReferenceInterface $reference,
+        ChannelInterface $tablet,
+        LocaleInterface $it_IT
+    ) {
+        $it_IT->getCode()->willReturn('it_IT');
+        $it_IT->isActivated()->willReturn(true);
+        $tablet->hasLocale($it_IT)->willReturn(false);
+        $tablet->getCode()->willReturn('tablet');
+        $reference->getLocale()->willReturn($it_IT);
+
+        $this->shouldThrow(
+            new \LogicException(
+                'Impossible to build a variation on channel "tablet" for the reference with locale "it_IT".'
+            )
+        )->during('buildOne', [$reference, $tablet]);
+    }
+
+    function it_does_not_build_a_variation_if_the_channel_of_the_reference_is_not_activated(
+        ProductAssetReferenceInterface $reference,
+        ChannelInterface $tablet,
+        LocaleInterface $it_IT
+    ) {
+        $it_IT->getCode()->willReturn('it_IT');
+        $it_IT->isActivated()->willReturn(false);
+        $tablet->hasLocale($it_IT)->willReturn(true);
+        $tablet->getCode()->willReturn('tablet');
+        $reference->getLocale()->willReturn($it_IT);
+
+        $this->shouldThrow(
+            new \LogicException(
+                'Impossible to build a variation on channel "tablet" for the reference with locale "it_IT".'
+            )
+        )->during('buildOne', [$reference, $tablet]);
+    }
+
+    function it_builds_all_variations_on_a_non_localized_reference(
+        ProductAssetReferenceInterface $reference
+    ) {
+        $reference->getLocale()->willReturn(null);
+
+        $all = $this->buildAll($reference);
+        $all->shouldHaveCount(3);
+        $all->shouldBeArrayOfVariations();
+    }
+
+    function it_builds_all_variations_on_a_localized_reference(
+        $en_US,
+        $ecommerce,
+        $print,
+        $mobile,
+        ProductAssetReferenceInterface $reference
+    ) {
+        $reference->getLocale()->willReturn($en_US);
+        $ecommerce->hasLocale($en_US)->willReturn(true);
+        $print->hasLocale($en_US)->willReturn(false);
+        $mobile->hasLocale($en_US)->willReturn(true);
+
+        $all = $this->buildAll($reference);
+        $all->shouldHaveCount(2);
+        $all->shouldBeArrayOfVariations();
     }
 
     public function getMatchers()
