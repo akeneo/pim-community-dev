475eb173722036fbe574fbf75befde1827d1f150
skeleton <marie.bochu@akeneo.com>
2015-05-20 15:02:40 +0200

    PIM-4119: fix spec

--- spec/PimEnterprise/Bundle/WorkflowBundle/Doctrine/Common/Saver/DelegatingProductSaverSpec.php
+++ spec/PimEnterprise/Bundle/WorkflowBundle/Doctrine/Common/Saver/DelegatingProductSaverSpec.php
@@ -2,31 +2,34 @@
 
 namespace spec\PimEnterprise\Bundle\WorkflowBundle\Doctrine\Common\Saver;
 
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use PhpSpec\ObjectBehavior;
-use Pim\Bundle\CatalogBundle\Doctrine\Common\Saver\ProductSaver;
 use Pim\Bundle\CatalogBundle\Doctrine\Common\Saver\ProductSavingOptionsResolver;
 use Pim\Bundle\CatalogBundle\Model\ProductInterface;
 use PimEnterprise\Bundle\SecurityBundle\Attributes;
-use PimEnterprise\Bundle\WorkflowBundle\Doctrine\Common\Saver\ProductDraftSaver;
+use PimEnterprise\Bundle\WorkflowBundle\Builder\ProductDraftBuilderInterface;
+use PimEnterprise\Bundle\WorkflowBundle\Model\ProductDraft;
 use Prophecy\Argument;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class DelegatingProductSaverSpec extends ObjectBehavior
 {
     function let(
-        ProductSaver $workingCopySaver,
-        ProductDraftSaver $draftSaver,
+        SaverInterface $workingCopySaver,
+        SaverInterface $draftSaver,
         ObjectManager $objectManager,
         ProductSavingOptionsResolver $optionsResolver,
-        SecurityContextInterface $securityContext
+        SecurityContextInterface $securityContext,
+        ProductDraftBuilderInterface $productDraftBuilder
     ) {
         $this->beConstructedWith(
             $workingCopySaver,
             $draftSaver,
             $objectManager,
             $optionsResolver,
-            $securityContext
+            $securityContext,
+            $productDraftBuilder
         );
     }
 
@@ -76,10 +79,11 @@ class DelegatingProductSaverSpec extends ObjectBehavior
         $this->save($product, ['recalculate' => true, 'flush' => true, 'schedule' => true]);
     }
 
-    function it_delegates_to_draft_saver_when_user_is_not_the_owner_and_product_exists(
+    function it_delegates_to_draft_saver_when_user_is_not_the_owner_and_product_exists_without_changes(
         ProductInterface $product,
         $optionsResolver,
         $securityContext,
+        $productDraftBuilder,
         $draftSaver
     ) {
         $optionsResolver->resolveSaveOptions(['recalculate' => true, 'flush' => true, 'schedule' => true])
@@ -90,8 +94,37 @@ class DelegatingProductSaverSpec extends ObjectBehavior
             ->willReturn(false);
         $securityContext->getToken()->willReturn('token');
 
-        $draftSaver->save($product, ['recalculate' => true, 'flush' => true, 'schedule' => true])
+        $productDraftBuilder->build($product)
+            ->willReturn(null)
             ->shouldBeCalled();
+
+        $draftSaver->save()->shouldNotBeCalled();
+
+        $this->save($product, ['recalculate' => true, 'flush' => true, 'schedule' => true]);
+    }
+
+    function it_delegates_to_draft_saver_when_user_is_not_the_owner_and_product_exists_with_changes(
+        ProductInterface $product,
+        $optionsResolver,
+        $securityContext,
+        $productDraftBuilder,
+        $draftSaver
+    ) {
+        $optionsResolver->resolveSaveOptions(['recalculate' => true, 'flush' => true, 'schedule' => true])
+            ->shouldBeCalled()
+            ->willReturn(['recalculate' => true, 'flush' => true, 'schedule' => true]);
+
+        $product->getId()->willReturn(42);
+        $securityContext->isGranted(Attributes::OWN, $product)
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $productDraft = new ProductDraft();
+        $productDraftBuilder->build($product)
+            ->willReturn($productDraft)
+            ->shouldBeCalled();
+
+        $draftSaver->save($productDraft, ['recalculate' => true, 'flush' => true, 'schedule' => true])->shouldBeCalled();
 
         $this->save($product, ['recalculate' => true, 'flush' => true, 'schedule' => true]);
     }
