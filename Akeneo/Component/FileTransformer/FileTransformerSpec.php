7088cfef97723ae28f597b3f59905b075c80e7c8
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-05-20 17:39:26 +0200

    PIM-4062: Fix file transformer to get the output file after a pool transformations

--- Akeneo/Component/FileTransformer/FileTransformerSpec.php
+++ Akeneo/Component/FileTransformer/FileTransformerSpec.php
@@ -26,86 +26,53 @@ class FileTransformerSpec extends ObjectBehavior
         $this->shouldImplement('\Akeneo\Component\FileTransformer\FileTransformerInterface');
     }
 
-    function it_transforms_a_file_following_options(
+    function it_transforms_a_file_with_the_following_options(
         $registry,
         \SplFileInfo $file,
         TransformationInterface $thumbnailTransformation,
-        TransformationInterface $colorSpaceTransformation,
-        TransformationInterface $resizeTransformation
+        TransformationInterface $colorSpaceTransformation
     ) {
-        $transformationsPipeline = [
-            [
-                'outputFile' => null,
-                'pipeline'   => [
-                    'thumbnail'  => ['width' => 100, 'height' => 100],
-                    'colorspace' => ['colorspace' => 'gray']
-                ]
-            ],
-            [
-                'outputFile' => null,
-                'pipeline'   => [
-                    'resize' => ['width' => 400, 'height' => 50]
-                ]
-            ]
+        $rawTransformations = [
+            'thumbnail'  => ['width' => 100, 'height' => 100],
+            'colorspace' => ['colorspace' => 'gray']
         ];
 
         $file->getPathname()->willReturn(__FILE__);
 
         $registry->get('thumbnail', 'text/x-php')->willReturn($thumbnailTransformation);
         $registry->get('colorspace', 'text/x-php')->willReturn($colorSpaceTransformation);
-        $registry->get('resize', 'text/x-php')->willReturn($resizeTransformation);
 
         $thumbnailTransformation->transform($file, ['width' => 100, 'height' => 100])->shouldBeCalled();
         $colorSpaceTransformation->transform($file, ['colorspace' => 'gray'])->shouldBeCalled();
-        $resizeTransformation->transform($file, ['width' => 400, 'height' => 50])->shouldBeCalled();
 
-        $this->transform($file, $transformationsPipeline);
+        $this->transform($file, $rawTransformations)->shouldReturn($file);
     }
 
-    function it_throws_an_exception_if_given_options_are_wrong(\SplFileInfo $file)
-    {
+    function it_transforms_a_file_with_the_following_options_and_save_it_to_a_different_output(
+        $registry,
+        \SplFileInfo $file,
+        TransformationInterface $thumbnailTransformation,
+        TransformationInterface $colorSpaceTransformation
+    ) {
+        $outputFilename = uniqid();
+
+        $rawTransformations = [
+            'thumbnail'  => ['width' => 100, 'height' => 100],
+            'colorspace' => ['colorspace' => 'gray']
+        ];
+
         $file->getPathname()->willReturn(__FILE__);
+        $file->getPath()->willReturn(__DIR__);
 
-        $wrongPipeline1 = [
-            [
-                'outputFile' => 123,
-                'pipeline'   => [
-                    'thumbnail'  => ['width' => 100, 'height' => 100],
-                    'colorspace' => ['colorspace' => 'gray']
-                ]
-            ]
-        ];
-        $this->shouldThrow(
-            new InvalidFileTransformerOptionsException(
-                'Your options does not fulfil the requirements of the transformation.'
-            )
-        )->duringTransform($file, $wrongPipeline1);
+        $registry->get('thumbnail', 'text/x-php')->willReturn($thumbnailTransformation);
+        $registry->get('colorspace', 'text/x-php')->willReturn($colorSpaceTransformation);
 
-        $wrongPipeline2 = [
-            [
-                'outputFile'     => null,
-                'somethingWrong' => [
-                    'thumbnail'  => ['width' => 100, 'height' => 100],
-                    'colorspace' => ['colorspace' => 'gray']
-                ]
-            ]
-        ];
-        $this->shouldThrow(
-            new InvalidFileTransformerOptionsException(
-                'Your options does not fulfil the requirements of the transformation.'
-            )
-        )->duringTransform($file, $wrongPipeline2);
+        $thumbnailTransformation->transform(Argument::any(), ['width' => 100, 'height' => 100])->shouldBeCalled();
+        $colorSpaceTransformation->transform(Argument::any(), ['colorspace' => 'gray'])->shouldBeCalled();
 
-        $wrongPipeline3 = [
-            [
-                'outputFile' => null,
-                'pipeline'   => 'somethingWrong'
-            ]
-        ];
-        $this->shouldThrow(
-            new InvalidFileTransformerOptionsException(
-                'Your options does not fulfil the requirements of the transformation.'
-            )
-        )->duringTransform($file, $wrongPipeline3);
+        $outputFile = $this->transform($file, $rawTransformations, $outputFilename);
+        $outputFile->getFilename()->shouldBe($outputFilename);
+
+        unlink(__DIR__ . '/' . $outputFilename);
     }
 }
