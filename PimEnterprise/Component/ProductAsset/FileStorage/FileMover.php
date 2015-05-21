a9a033683142bfae5b1539b10a684ad85dee6925
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-05-21 18:35:36 +0200

    PIM-4062: Introduce file mover

---
+++ PimEnterprise/Component/ProductAsset/FileStorage/FileMover.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2015 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Component\ProductAsset\FileStorage;
+
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use League\Flysystem\MountManager;
+use PimEnterprise\Component\ProductAsset\Exception\TransferFileException;
+use PimEnterprise\Component\ProductAsset\Model\FileInterface;
+
+/**
+ * Move a file from a virtual filesystem to another
+ *
+ * @author Julien Janvier <jjanvier@akeneo.com>
+ */
+class FileMover implements FileMoverInterface
+{
+    /** @var MountManager */
+    protected $mountManager;
+
+    /** @var SaverInterface */
+    protected $saver;
+
+    /**
+     * @param MountManager $mountManager
+     * @param SaverInterface $saver
+     */
+    public function __construct(MountManager $mountManager, SaverInterface $saver)
+    {
+        $this->mountManager = $mountManager;
+        $this->saver        = $saver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function move(FileInterface $file, $srcFsAlias, $destFsAlias)
+    {
+        $isFileMoved = $this->mountManager->move(
+            sprintf('%s://%s', $srcFsAlias, $file->getPathname()),
+            sprintf('%s://%s', $destFsAlias, $file->getPathname())
+        );
+
+        if (!$isFileMoved) {
+            throw new TransferFileException(
+                sprintf(
+                    'Impossible to move the file "%s" from "%s" to "%s".',
+                    $file->getPathname(),
+                    $srcFsAlias,
+                    $destFsAlias
+                )
+            );
+        }
+
+        $file->setStorage($destFsAlias);
+        $this->saver->save($file);
+    }
+
+}
