f4c24a35ca5fa592d4f3b734d71cb2aca5c43c88
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-05-21 18:32:38 +0200

    PIM-4062: throw business exceptions in file storers

--- PimEnterprise/Component/ProductAsset/FileStorage/RawFile/UploadedStorerSpec.php
+++ PimEnterprise/Component/ProductAsset/FileStorage/RawFile/UploadedStorerSpec.php
@@ -1,32 +1,41 @@
 <?php
 
-namespace spec\PimEnterprise\Component\ProductAsset\FileStorage\FileHandler;
+namespace spec\PimEnterprise\Component\ProductAsset\FileStorage\RawFile;
 
 use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use League\Flysystem\Filesystem;
 use League\Flysystem\MountManager;
 use PhpSpec\ObjectBehavior;
 use PimEnterprise\Component\ProductAsset\FileStorage\PathGeneratorInterface;
 use PimEnterprise\Component\ProductAsset\Model\File;
-use PimEnterprise\Component\ProductAsset\Model\FileInterface;
 use Prophecy\Argument;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class LocalFileHandlerSpec extends ObjectBehavior
+class UploadedStorerSpec extends ObjectBehavior
 {
     function let(PathGeneratorInterface $pathGenerator, MountManager $mountManager, SaverInterface $saver)
     {
-        $this->beConstructedWith($pathGenerator, $mountManager, $saver, 'src', 'dest');
+        $this->beConstructedWith($pathGenerator, $mountManager, $saver);
     }
 
-    function it_handles_a_local_file($pathGenerator, $mountManager, $saver, CustomFileInfo $file)
+    function it_stores_an_uploaded_file($pathGenerator, $mountManager, $saver, CustomUploadedFile $file, Filesystem $fs)
     {
-        $file->getPathname()->willReturn(__FILE__);
-        $file->getFilename()->willReturn('my file.php');
+        touch('/tmp/x2f48h1');
+
+        $file->getPathname()->willReturn('/tmp/x2f48h1');
+        $file->getFilename()->willReturn('x2f48h1');
+        $file->getMimeType()->willReturn('text/x-php');
+        $file->getClientOriginalName()->willReturn('my file.php');
+        $file->getClientSize()->willReturn(filesize(__FILE__));
+        $file->getExtension()->willReturn('php');
+
+        $mountManager->getFilesystem('destination')->willReturn($fs);
 
         $pathGenerator->generate($file)->willReturn(
             [
                 'guid'      => '12345',
                 'file_name' => '12345_my_file.php',
-                'path'      => '1/2/3/4/',
+                'path'       => '1/2/3/4/',
                 'file_path' => '1/2/3/4/12345_my_file.php',
             ]
         );
@@ -38,33 +47,34 @@ class LocalFileHandlerSpec extends ObjectBehavior
         $expected->setGuid('12345');
         $expected->setPath('1/2/3/4/');
         $expected->setMimeType('text/x-php');
+        $expected->setExtension('php');
 
-        $mountManager->move('src://' . __FILE__, 'dest://1/2/3/4/12345_my_file.php')->shouldBeCalled();
+        $fs->writeStream('1/2/3/4/12345_my_file.php', Argument::any())->shouldBeCalled();
 
         $saver->save(Argument::any())->shouldBeCalled();
-        $this->handle($file)->shouldBeFileLike($expected);
+        $this->store($file, 'destination')->shouldBeFileLike($expected);
     }
 
     public function getMatchers()
     {
         return [
             'beFileLike' => function ($subject, $expected) {
-                return $subject instanceof FileInterface &&
-                    $subject->getSize() === $expected->getSize() &&
+                return $subject->getSize() === $expected->getSize() &&
                     $subject->getOriginalFilename() === $expected->getOriginalFilename() &&
                     $subject->getFilename() === $expected->getFilename() &&
                     $subject->getGuid() === $expected->getGuid() &&
                     $subject->getPath() === $expected->getPath() &&
-                    $subject->getMimeType() === $expected->getMimeType();
+                    $subject->getMimeType() === $expected->getMimeType() &&
+                    $subject->getExtension() === $subject->getExtension();
             },
         ];
     }
 }
 
-class CustomFileInfo extends \SplFileInfo
+class CustomUploadedFile extends UploadedFile
 {
     public function __construct()
     {
-        parent::__construct(__FILE__);
+        parent::__construct(__FILE__, 'my file.php');
     }
 }
