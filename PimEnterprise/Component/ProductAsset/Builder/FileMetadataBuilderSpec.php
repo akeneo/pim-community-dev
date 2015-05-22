a85ac15163756e5c0a99899c56238437f7d50ccc
Adrien Pétremann <petremann.adrien@gmail.com>
2015-05-22 16:16:52 +0200

    PIM-4160: Fix and add colorSpace metadata

--- PimEnterprise/Component/ProductAsset/Builder/FileMetadataBuilderSpec.php
+++ PimEnterprise/Component/ProductAsset/Builder/FileMetadataBuilderSpec.php
@@ -2,38 +2,29 @@
 
 namespace spec\PimEnterprise\Component\ProductAsset\Builder;
 
-use Akeneo\Component\FileMetadata\FileMetadataBagInterface;
-use Akeneo\Component\FileMetadata\FileMetadataReader;
-use Akeneo\Component\FileMetadata\FileMetadataReaderFactoryInterface;
 use PhpSpec\ObjectBehavior;
 
 class FileMetadataBuilderSpec extends ObjectBehavior
 {
-    function let(FileMetadataReaderFactoryInterface $fileMetadataReaderFactory)
+    function let()
     {
-        $this->beConstructedWith($fileMetadataReaderFactory);
+        $this->beConstructedWith();
     }
 
-    function it_is_a_filemetadata_builder()
+    function it_is_a_metadata_builder()
     {
-        $this->shouldImplement('PimEnterprise\Component\ProductAsset\Builder\FileMetadataBuilderInterface');
+        $this->shouldImplement('PimEnterprise\Component\ProductAsset\Builder\MetadataBuilderInterface');
     }
 
-    function it_builds_a_file_metadata(
-        $fileMetadataReaderFactory,
-        FileMetadataReader $fileMetaReader,
-        FileMetadataBagInterface $metadata
-    ) {
-        $file = new \SplFileInfo(__FILE__);
-        $fileMetadataReaderFactory->create($file)->willReturn($fileMetaReader);
-        $fileMetaReader->all($file)->shouldBeCalled();
-        $fileMetaReader->getMetadata()->willReturn($metadata);
-
-        $metadata->get('exif.FILE.FileDateTime')->willReturn(123123123);
+    function it_builds_a_file_metadata(\SplFileInfo $file)
+    {
+        $timestamp = 123456789;
+        $file->getMTime()->willReturn($timestamp);
+        $expectedDateTime = new \DateTime(sprintf('@%s', $timestamp));
 
         $fileMetadata = $this->build($file);
 
         $fileMetadata->shouldImplement('PimEnterprise\Component\ProductAsset\Model\FileMetadataInterface');
-        $fileMetadata->getFileDatetime()->shouldReturn(123123123);
+        $fileMetadata->getModificationDatetime()->getTimestamp()->shouldReturn($expectedDateTime->getTimestamp());
     }
 }
--- PimEnterprise/Component/ProductAsset/Builder/ImageMetadataBuilderSpec.php
+++ PimEnterprise/Component/ProductAsset/Builder/ImageMetadataBuilderSpec.php
@@ -6,21 +6,21 @@ use Akeneo\Component\FileMetadata\FileMetadataBagInterface;
 use Akeneo\Component\FileMetadata\FileMetadataReader;
 use Akeneo\Component\FileMetadata\FileMetadataReaderFactoryInterface;
 use PhpSpec\ObjectBehavior;
-use PimEnterprise\Component\ProductAsset\Builder\FileMetadataBuilderInterface;
+use PimEnterprise\Component\ProductAsset\Builder\MetadataBuilderInterface;
 use PimEnterprise\Component\ProductAsset\Model\FileMetadataInterface;
 
 class ImageMetadataBuilderSpec extends ObjectBehavior
 {
     function let(
         FileMetadataReaderFactoryInterface $fileMetadataReaderFactory,
-        FileMetadataBuilderInterface $fileMetaBuidler
+        MetadataBuilderInterface $fileMetaBuidler
     ) {
         $this->beConstructedWith($fileMetadataReaderFactory, $fileMetaBuidler);
     }
 
-    function it_is_a_filemetadata_builder()
+    function it_is_a_metadata_builder()
     {
-        $this->shouldImplement('PimEnterprise\Component\ProductAsset\Builder\ImageMetadataBuilderInterface');
+        $this->shouldImplement('PimEnterprise\Component\ProductAsset\Builder\MetadataBuilderInterface');
     }
 
     function it_builds_an_image_metadata(
@@ -28,17 +28,16 @@ class ImageMetadataBuilderSpec extends ObjectBehavior
         $fileMetaBuidler,
         FileMetadataReader $fileMetaReader,
         FileMetadataBagInterface $metadata,
-        FileMetadataInterface $fileMetadata
+        FileMetadataInterface $fileMetadata,
+        \SplFileInfo $file
     ) {
-        $file = new \SplFileInfo(__FILE__);
-        $fileMetadataReaderFactory->create($file)->willReturn($fileMetaReader);
-        $fileMetaReader->all($file)->shouldBeCalled();
-        $fileMetaReader->getMetadata()->willReturn($metadata);
+        $timestamp = 123456789;
+        $file->getMTime()->willReturn($timestamp);
+        $expectedDateTime = new \DateTime(sprintf('@%s', $timestamp));
 
         $fileMetaBuidler->build($file)->willReturn($fileMetadata);
-        $fileMetadata->getFileDatetime()->willReturn(123123123);
+        $fileMetadata->getModificationDatetime()->willReturn($expectedDateTime);
 
-        $metadata->get('exif.FILE.FileDateTime')->willReturn(123123123);
         $metadata->get('exif.EXIF.DateTimeOriginal')->willReturn('2012-05-08 12:20:33');
         $metadata->get('exif.IFD0.Make')->willReturn('Canon');
         $metadata->get('exif.IFD0.Model')->willReturn('LTH 5660r');
@@ -49,16 +48,21 @@ class ImageMetadataBuilderSpec extends ObjectBehavior
         $metadata->get('exif.IFD0.Keywords')->willReturn('fisherman');
         $metadata->get('exif.IFD0.Title')->willReturn('Fishing boat');
         $metadata->get('exif.IFD0.Subject')->willReturn('A beatiful boat taken in Iceland');
+        $metadata->get('exif.EXIF.ColorSpace')->willReturn(2);
         $metadata->get('iptc.Keywords', [])->willReturn(['boat', 'fish']);
         $metadata->get('iptc.LocationName')->willReturn('Iceland');
         $metadata->get('iptc.City')->willReturn('Reykjavik');
         $metadata->get('exif.IFD0.XResolution')->willReturn('300/1');
         $metadata->get('exif.IFD0.ResolutionUnit')->willReturn(2);
 
+        $fileMetadataReaderFactory->create($file)->willReturn($fileMetaReader);
+        $fileMetaReader->all($file)->shouldBeCalled();
+        $fileMetaReader->getMetadata()->willReturn($metadata);
+
         $imageMetadata = $this->build($file);
 
         $imageMetadata->shouldImplement('PimEnterprise\Component\ProductAsset\Model\ImageMetadataInterface');
-        $imageMetadata->getFileDatetime()->shouldReturn(123123123);
+        $imageMetadata->getModificationDatetime()->getTimestamp()->shouldReturn($expectedDateTime->getTimestamp());
         $imageMetadata->getExifDateTimeOriginal()->shouldReturn('2012-05-08 12:20:33');
         $imageMetadata->getExifCameraMake()->shouldReturn('Canon');
         $imageMetadata->getExifCameraModel()->shouldReturn('LTH 5660r');
@@ -69,6 +73,7 @@ class ImageMetadataBuilderSpec extends ObjectBehavior
         $imageMetadata->getExifKeywords()->shouldReturn('fisherman');
         $imageMetadata->getExifTitle()->shouldReturn('Fishing boat');
         $imageMetadata->getExifDescription()->shouldReturn('A beatiful boat taken in Iceland');
+        $imageMetadata->getExifColorSpace()->shouldReturn(2);
         $imageMetadata->getIptcKeywords()->shouldReturn('boat,fish');
         $imageMetadata->getIptcLocationCountry()->shouldReturn('Iceland');
         $imageMetadata->getIptcLocationCity()->shouldReturn('Reykjavik');
