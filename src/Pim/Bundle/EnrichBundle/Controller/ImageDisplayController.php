ec79f0b8cc01fb2d36b5a2f6857c2e27db5f2714
Adrien Pétremann <petremann.adrien@gmail.com>
2015-05-28 10:56:40 +0200

    PIM-4167: Change the new media controller name

--- src/Pim/Bundle/EnrichBundle/Controller/ImageDisplayController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/ImageDisplayController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class EnhancedMediaController extends Controller
+class ImageDisplayController extends Controller
 {
     /** @var ImagineController */
     protected $imagineController;
