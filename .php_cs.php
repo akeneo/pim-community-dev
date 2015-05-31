20ffa01eb61d91d8a1576a045467082c3e09b3ee
jmleroux <jmleroux.pro@gmail.com>
2015-05-31 13:41:57 +0200

    Travis : scripting tests

--- .php_cs.php
+++ .php_cs.php
@@ -1,12 +1,24 @@
 <?php
 
 $branch = getenv('TRAVIS_BRANCH');
+$phpVersion = getenv('TRAVIS_PHP_VERSION');
 
 printf('Current branch inspected : %s' . PHP_EOL, $branch);
+
+$parsedDirectories = [
+    '5.5' => [
+        __DIR__ . '/spec',
+        __DIR__ . '/features',
+    ],
+    '5.6' => [__DIR__ . '/src'],
+];
 
 $finder = Symfony\CS\Finder\DefaultFinder::create()->files();
 
 if (in_array($branch, ['master', 'HEAD'])) {
+    if (!array_key_exists($phpVersion, $parsedDirectories)) {
+        return null;
+    }
     $finder
         ->name('*.php')
         ->in(__DIR__ . '/src');
