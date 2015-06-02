521aa0b6a747404e5b85e7aebde62a15e8c6f02d
sumbobyboys <sumbobyboys@gmail.com>
2015-06-02 19:01:58 +0200

    PIM-4258: add upload context to handle context switch on upload

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/field/media-field.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/field/media-field.js
@@ -18,6 +18,7 @@ define([
                 'click  .clear-field': 'clearField',
                 'click  .open-media': 'previewImage'
             },
+            uploadContext: {},
             renderInput: function (context) {
                 return this.fieldTemplate(context);
             },
@@ -53,12 +54,24 @@ define([
                 return Field.prototype.renderCopyInput.apply(this, arguments);
             },
             updateModel: function (event) {
+                if (!this.getReady()) {
+                    alert(_.__(
+                        'pim_enrich.entity.product.info.already_in_upload',
+                        {'locale': this.context.locale, 'scope': this.context.scope}
+                    ))
+                }
+
                 var input = event.currentTarget;
 
                 var formData = new FormData();
                 formData.append('file', input.files[0]);
 
                 this.setReady(false);
+                this.uploadContext = {
+                    'locale': this.context.locale,
+                    'scope':  this.context.scope
+                };
+
                 $.ajax({
                     url: Routing.generate('pim_enrich_media_rest_post'),
                     type: 'POST',
@@ -75,11 +88,12 @@ define([
                         return myXhr;
                     }, this)
                 }).done(_.bind(function (data) {
-                    this.setCurrentValue(data);
+                    this.setUploadContextValue(data);
                     this.render();
                 }, this)).then(_.bind(function () {
                     this.$('> .media-field .progress').css({opacity: 0});
                     this.setReady(true);
+                    this.uploadContext = {};
                 }, this));
             },
             clearField: function () {
@@ -88,16 +102,32 @@ define([
                 this.render();
             },
             handleProcess: function (e) {
-                this.$('> .media-field .progress').css({opacity: 1});
-                this.$('> .media-field .progress .bar').css({
-                    width: ((e.loaded / e.total) * 100) + '%'
-                });
+                if (this.uploadContext.locale === this.context.locale &&
+                    this.uploadContext.scope === this.context.scope
+                ) {
+                    this.$('> .media-field .progress').css({opacity: 1});
+                    this.$('> .media-field .progress .bar').css({
+                        width: ((e.loaded / e.total) * 100) + '%'
+                    });
+                } else {
+                    this.$('> .media-field .progress').css({opacity: 0});
+                }
             },
             previewImage: function () {
                 var mediaUrl = this.getMediaUrl(this.getCurrentValue().value);
                 if (mediaUrl) {
                     $.slimbox(mediaUrl, '', {overlayOpacity: 0.3});
                 }
+            },
+            setUploadContextValue: function (value) {
+                var productValue = AttributeManager.getValue(
+                    this.model.get('values'),
+                    this.attribute,
+                    this.uploadContext.locale,
+                    this.uploadContext.scope
+                );
+
+                productValue.value = value;
             }
         });
     }
