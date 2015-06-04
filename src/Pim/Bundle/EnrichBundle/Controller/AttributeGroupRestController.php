0a08b2972969e7d898cb9c468983b1a0b7032f21
sumbobyboys <sumbobyboys@gmail.com>
2015-06-04 17:46:52 +0200

    PIM-4186: cleanup

--- src/Pim/Bundle/EnrichBundle/Controller/AttributeGroupRestController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/AttributeGroupRestController.php
@@ -4,7 +4,6 @@ namespace Pim\Bundle\EnrichBundle\Controller;
 
 use Doctrine\ORM\EntityRepository;
 use Pim\Bundle\CatalogBundle\Filter\CollectionFilterInterface;
-use Pim\Bundle\CatalogBundle\Filter\ObjectFilterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -27,25 +26,19 @@ class AttributeGroupRestController
     /** @var CollectionFilterInterface */
     protected $collectionFilter;
 
-    /** @var ObjectFilterInterface */
-    protected $objectFilter;
-
     /**
      * @param EntityRepository          $attributeGroupRepo
      * @param NormalizerInterface       $normalizer
      * @param CollectionFilterInterface $collectionFilter
-     * @param ObjectFilterInterface     $objectFilter
      */
     public function __construct(
         EntityRepository $attributeGroupRepo,
         NormalizerInterface $normalizer,
-        CollectionFilterInterface $collectionFilter,
-        ObjectFilterInterface $objectFilter
+        CollectionFilterInterface $collectionFilter
     ) {
         $this->attributeGroupRepo = $attributeGroupRepo;
         $this->normalizer         = $normalizer;
         $this->collectionFilter   = $collectionFilter;
-        $this->objectFilter       = $objectFilter;
     }
 
     /**
@@ -67,28 +60,5 @@ class AttributeGroupRestController
         }
 
         return new JsonResponse($normalizedAttrGroups);
-    }
-
-    /**
-     * Get a single attribute group
-     *
-     * @param int $id
-     *
-     * @return JsonResponse
-     */
-    public function getAction($id)
-    {
-        $attributeGroup = $this->attributeGroupGroupRepo->findOneById($id);
-
-        if (null === $attributeGroup ||
-            (
-                null !== $attributeGroup &&
-                $this->objectFilter->filterObject($attributeGroup, 'pim:internal_api:attribute_group:view')
-            )
-        ) {
-            throw new NotFoundHttpException(sprintf('No attribute group found for id "%d"', $id));
-        }
-
-        return new JsonResponse($this->normalizer->normalize($attributeGroup, 'json'));
     }
 }
