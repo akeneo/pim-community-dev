395a899618474246d5201b5c41796ac5bef0278d
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-06-05 10:30:01 +0200

    PIM-4299: move the file storage classes into a dedicated component

---
+++ AkeneoFileStorageBundle.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2014 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Bundle\FileStorageBundle;
+
+use Akeneo\Bundle\FileStorageBundle\DependencyInjection\Compiler\ResolveDoctrineTargetModelPass;
+use Oro\Bundle\EntityBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Akeneo File Storage Bundle
+ *
+ * @author Julien Janvier <jjanvier@akeneo.com>
+ */
+class AkeneoFileStorageBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container
+            ->addCompilerPass(new ResolveDoctrineTargetModelPass());
+
+        $mappings = [
+            realpath(__DIR__ . '/Resources/config/model/doctrine') => 'Akeneo\Component\FileStorage\Model'
+        ];
+
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createYamlMappingDriver(
+                $mappings,
+                ['doctrine.orm.entity_manager'],
+                false
+            )
+        );
+    }
+}
