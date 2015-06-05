3a3b95f1d74dc60014a659786dd79f1720290d26
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-06-05 12:03:24 +0200

    PIM-4299: introduce the file repository

---
+++ Repository/FileRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2015 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Component\ProductAsset\Repository;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+/**
+ * File repository interface
+ *
+ * @author Julien Janvier <jjanvier@akeneo.com>
+ */
+interface FileRepositoryInterface extends ObjectRepository
+{
+}
