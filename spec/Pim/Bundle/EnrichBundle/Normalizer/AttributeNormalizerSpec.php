43641a899e68dff53654a7c48cbb181de104d525
Jean-Marie Leroux <jmleroux.pro@gmail.com>
2015-06-12 16:53:08 +0200

    Attribute empty value comes from backend

--- spec/Pim/Bundle/EnrichBundle/Normalizer/AttributeNormalizerSpec.php
+++ spec/Pim/Bundle/EnrichBundle/Normalizer/AttributeNormalizerSpec.php
@@ -18,8 +18,88 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'price']);
         $price->getId()->willReturn(12);
         $price->isWysiwygEnabled()->willReturn(false);
+        $price->getAttributeType()->willReturn('pim_catalog_text');
 
-        $this->normalize($price, 'internal_api', [])->shouldReturn(['code' => 'price', 'id' => 12, 'wysiwyg_enabled' => false]);
+        $this->normalize($price, 'internal_api', [])->shouldReturn(['code' => 'price', 'id' => 12, 'wysiwyg_enabled' => false, 'empty_value' => '']);
+    }
+
+    public function it_adds_the_attribute_empty_value_to_the_normalized_attribute($normalizer, AttributeInterface $price)
+    {
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'text']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(true);
+        $price->getAttributeType()->willReturn('pim_catalog_textarea');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'text',
+            'id'              => 12,
+            'wysiwyg_enabled' => true,
+            'empty_value'     => ''
+        ]);
+
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'boolean']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(true);
+        $price->getAttributeType()->willReturn('pim_catalog_boolean');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'boolean',
+            'id'              => 12,
+            'wysiwyg_enabled' => true,
+            'empty_value'     => false
+        ]);
+
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'collection']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(false);
+        $price->getAttributeType()->willReturn('pim_catalog_price_collection');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'collection',
+            'id'              => 12,
+            'wysiwyg_enabled' => false,
+            'empty_value'     => []
+        ]);
+
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'collection']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(false);
+        $price->getAttributeType()->willReturn('pim_catalog_multiselect');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'collection',
+            'id'              => 12,
+            'wysiwyg_enabled' => false,
+            'empty_value'     => []
+        ]);
+
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'metric']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(false);
+        $price->getAttributeType()->willReturn('pim_catalog_metric');
+        $price->getDefaultMetricUnit()->willReturn('kg');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'metric',
+            'id'              => 12,
+            'wysiwyg_enabled' => false,
+            'empty_value'     => [
+                'data' => null,
+                'unit' => 'kg'
+            ]
+        ]);
+
+        $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'default']);
+        $price->getId()->willReturn(12);
+        $price->isWysiwygEnabled()->willReturn(false);
+        $price->getAttributeType()->willReturn('unknown');
+
+        $this->normalize($price, 'internal_api', [])->shouldReturn([
+            'code'            => 'default',
+            'id'              => 12,
+            'wysiwyg_enabled' => false,
+            'empty_value'     => null
+        ]);
     }
 
     public function it_supports_attributes_and_internal_api(AttributeInterface $price)
--- src/Pim/Bundle/EnrichBundle/Resources/public/js/manager/attribute-manager.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/manager/attribute-manager.js
@@ -34,41 +34,14 @@ define([
             isOptional: function (attribute, product, families) {
                 return !product.family ? true : !_.contains(families[product.family].attributes, attribute);
             },
-            getEmptyValue: function (attribute) {
-                switch (attribute.type) {
-                    case 'pim_catalog_date':
-                    case 'pim_catalog_file':
-                    case 'pim_catalog_image':
-                    case 'pim_catalog_simpleselect':
-                    case 'pim_reference_data_simpleselect':
-                    case 'pim_catalog_identifier':
-                    case 'pim_catalog_number':
-                        return null;
-                    case 'pim_catalog_metric':
-                        return {
-                            'data': null,
-                            'unit': attribute.default_metric_unit
-                        };
-                    case 'pim_catalog_multiselect':
-                    case 'pim_reference_data_multiselect':
-                        return [];
-                    case 'pim_catalog_text':
-                    case 'pim_catalog_textarea':
-                        return '';
-                    case 'pim_catalog_boolean':
-                        return false;
-                    case 'pim_catalog_price_collection':
-                        return [];
-                    default:
-                        throw new Error(JSON.stringify(attribute));
-                }
-            },
+
             getValue: function (values, attribute, locale, scope) {
                 locale = attribute.localizable ? locale : null;
                 scope  = attribute.scopable ? scope : null;
 
                 return _.findWhere(values, {scope: scope, locale: locale});
             },
+
             generateValue: function (attribute, locale, scope) {
                 locale = attribute.localizable ? locale : null;
                 scope  = attribute.scopable ? scope : null;
@@ -76,9 +49,19 @@ define([
                 return {
                     'locale': locale,
                     'scope':  scope,
-                    'value':  this.getEmptyValue(attribute)
+                    'value':  attribute.empty_value
                 };
             },
+
+            /**
+             * Generate all missing values for an attribute
+             * @param values
+             * @param attribute
+             * @param locales
+             * @param channels
+             * @param currencies
+             * @returns {*}
+             */
             generateMissingValues: function (values, attribute, locales, channels, currencies) {
                 _.each(locales, _.bind(function (locale) {
                     _.each(channels, _.bind(function (channel) {
@@ -103,6 +86,7 @@ define([
 
                 return values;
             },
+
             generateMissingPrices: function (prices, currencies) {
                 _.each(currencies, function (currency) {
                     var price = _.findWhere(prices, {currency: currency.code});
