d2459a1b2edf6a915af0f15908c078a34672eef9
Yohan BLAIN <yohan.blain@gmail.com>
2015-06-16 15:12:55 +0200

    Add a User repository

---
+++ src/Pim/Bundle/UserBundle/Entity/Repository/UserRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pim\Bundle\UserBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * User repository
+ *
+ * @author    Yohan Blain <yohan.blain@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class UserRepository extends EntityRepository implements UserRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifierProperties()
+    {
+        return ['username', 'email'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findOneByIdentifier($identifier)
+    {
+        $qb = $this->createQueryBuilder('u');
+        $qb->where($qb->expr()->orX(
+               $qb->expr()->eq('u.username', ':identifier'),
+               $qb->expr()->eq('u.email', ':identifier')
+           ))
+           ->setParameter(':identifier', $identifier);
+
+        return $qb->getQuery()->getSingleResult();
+    }
+}
