f8f75f6419d805f052e57f76570748ad677f5a32
Yohan BLAIN <yohan.blain@gmail.com>
2015-06-16 13:06:12 +0200

    PIM-3338: Add copyright and licence

--- src/Pim/Bundle/BaseConnectorBundle/Step/TaskletInterface.php
+++ src/Pim/Bundle/BaseConnectorBundle/Step/TaskletInterface.php
@@ -7,7 +7,9 @@ use Akeneo\Bundle\BatchBundle\Step\StepExecutionAwareInterface;
 /**
  * Simple task to be executed from a TaskletStep.
  *
- * @author Yohan Blain <yohan.blain@akeneo.com>
+ * @author    Yohan Blain <yohan.blain@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 interface TaskletInterface extends StepExecutionAwareInterface
 {
