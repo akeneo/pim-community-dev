d8d8632b75e7473ea69b38140cf6d5491c69becf
Adrien Pétremann <petremann.adrien@gmail.com>
2015-06-26 11:27:12 +0200

    PIM-4073: Beaht, be able to switch locale in the asset edit page

--- features/Context/Page/Asset/Edit.php
+++ features/Context/Page/Asset/Edit.php
@@ -17,6 +17,21 @@ class Edit extends Form
     protected $path = '/enrich/asset/{id}/edit';
 
     /**
+     * {@inheritdoc}
+     */
+    public function __construct($session, $pageFactory, $parameters = [])
+    {
+        parent::__construct($session, $pageFactory, $parameters);
+
+        $this->elements = array_merge(
+            $this->elements,
+            [
+                'Locales dropdown' => ['css' => '#locale-switcher'],
+            ]
+        );
+    }
+
+    /**
      * Fill a new date in the End of use at date picker
      *
      * @param string $date YEAR-MONTH-DAY e.g. 2015-06-20
@@ -25,5 +40,25 @@ class Edit extends Form
     {
         $field = $this->find('css', 'label:contains("End of use at")');
         $this->fillDateField($field, $date);
+    }
+
+    /**
+     * @param string $locale
+     *
+     * @throws \Exception
+     */
+    public function switchLocale($locale)
+    {
+        $elt = $this->getElement('Locales dropdown')->find('css', 'span.dropdown-toggle');
+        if (!$elt) {
+            throw new \Exception('Could not find locale switcher.');
+        }
+        $elt->click();
+
+        $elt = $this->getElement('Locales dropdown')->find('css', sprintf('a[title="%s"]', $locale));
+        if (!$elt) {
+            throw new \Exception(sprintf('Could not find locale "%s" in switcher.', $locale));
+        }
+        $elt->click();
     }
 }
