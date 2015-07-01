f721e5453b154f5d5deefd9a08824569a805a70f
Jean-Marie Leroux <jmleroux.pro@gmail.com>
2015-07-01 15:10:03 +0200

    PIM-4474 : add hooks for extra joins and extra conditions in completeness request

--- src/PimEnterprise/Bundle/CatalogBundle/Doctrine/ORM/CompletenessGenerator.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Doctrine/ORM/CompletenessGenerator.php
@@ -20,18 +20,21 @@ class CompletenessGenerator extends CommunityCompletenessGenerator
      * @param string                 $attributeClass
      * @param string                 $assetClass
      */
-    public function __construct(
-        EntityManagerInterface $manager,
-        $productClass,
-        $productValueClass,
-        $attributeClass,
-        $assetClass
-    ) {
-        parent::__construct($manager, $productClass, $productValueClass, $attributeClass);
+//    public function __construct(
+//        EntityManagerInterface $manager,
+//        $productClass,
+//        $productValueClass,
+//        $attributeClass,
+//        $assetClass
+//    ) {
+//        parent::__construct($manager, $productClass, $productValueClass, $attributeClass);
+//
+//        $this->assetClass = $assetClass;
+//    }
 
-        $this->assetClass = $assetClass;
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     protected function generate(array $criteria = [])
     {
         $this->prepareCompleteAssets();
@@ -39,72 +42,7 @@ class CompletenessGenerator extends CommunityCompletenessGenerator
     }
 
     /**
-     * Provides the main SQL part
-     *
-     * @return string
-     */
-    protected function getMainSqlPart()
-    {
-        return <<<MAIN_SQL
-            INSERT INTO pim_catalog_completeness (
-                locale_id, channel_id, product_id, ratio, missing_count, required_count
-            )
-            SELECT
-                l.id AS locale_id, c.id AS channel_id, p.id AS product_id,
-                (
-                    COUNT(distinct v.id)
-                    / (
-                        SELECT count(*)
-                            FROM pim_catalog_attribute_requirement
-                            WHERE family_id = p.family_id
-                                AND channel_id = c.id
-                                AND required = true
-                    )
-                    * 100
-                ) AS ratio,
-                (
-                    (
-                        SELECT count(*)
-                            FROM pim_catalog_attribute_requirement
-                            WHERE family_id = p.family_id
-                                AND channel_id = c.id
-                                AND required = true
-                    ) - COUNT(distinct v.id)
-                ) AS missing_count,
-                (
-                    SELECT count(*)
-                        FROM pim_catalog_attribute_requirement
-                        WHERE family_id = p.family_id
-                            AND channel_id = c.id
-                            AND required = true
-                ) AS required_count
-            FROM missing_completeness m
-                JOIN pim_catalog_channel c ON c.id = m.channel_id
-                JOIN pim_catalog_locale l ON l.id = m.locale_id
-                JOIN %product_table% p ON p.id = m.product_id
-                JOIN pim_catalog_attribute_requirement r ON r.family_id = p.family_id AND r.channel_id = c.id
-                JOIN %product_value_table% v ON v.attribute_id = r.attribute_id
-                    AND (v.scope_code = c.code OR v.scope_code IS NULL)
-                    AND (v.locale_code = l.code OR v.locale_code IS NULL)
-                    AND v.entity_id = p.id
-                LEFT JOIN complete_price
-                    ON complete_price.value_id = v.id
-                    AND complete_price.channel_id = c.id
-                    AND complete_price.locale_id = l.id
-                LEFT JOIN complete_asset
-                    ON complete_asset.value_id = v.id
-                    AND complete_asset.channel_id = c.id
-                    AND complete_asset.locale_id = l.id
-                %product_value_joins%
-            WHERE (%product_value_conditions% OR complete_price.value_id IS NOT NULL OR complete_asset.value_id IS NOT NULL) AND r.required = true
-            GROUP BY p.id, c.id, l.id
-MAIN_SQL;
-    }
-
-    /**
-     * Returns an array of SQL conditions for the ProductValue entity
-     *
-     * @return array
+     * {@inheritdoc}
      */
     protected function getProductValueConditions()
     {
@@ -166,5 +104,34 @@ MAIN_SQL;
         $skippedByParent = parent::getSkippedMappings();
 
         return array_merge($skippedByParent, ['asset']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getExtraJoins()
+    {
+        $assetsJoin = 'LEFT JOIN %s AS complete_asset
+            ON complete_asset.value_id = v.id
+            AND complete_asset.channel_id = c.id
+            AND complete_asset.locale_id = l.id';
+
+        $assetsJoin = sprintf($assetsJoin, static::ASSETS_VALUES_TABLE);
+
+        $extraJoins = array_merge(parent::getExtraJoins(), [$assetsJoin]);
+
+        return $extraJoins;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getExtraConditions()
+    {
+        $assetsConditions = sprintf('OR %s.value_id IS NOT NULL', static::ASSETS_VALUES_TABLE);
+
+        $extraConditions = array_merge(parent::getExtraConditions(), [$assetsConditions]);
+
+        return $extraConditions;
     }
 }
