6380322a866a5368a716731836090ea157d66688
Julien Sanchez <sumbobyboys@gmail.com>
2015-07-09 14:11:09 +0000

    Preparing version 1.3.18

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.17';
+    const VERSION = '1.3.18';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Strawberry';
