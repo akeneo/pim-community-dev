9b5f4b3248bf6165d7649c6cb5028883e560cd46
Nicolas Dupont <nicolas@akeneo.com>
2015-08-03 19:36:08 +0200

    PIM-4437: add deprecated notices

--- src/Pim/Bundle/BaseConnectorBundle/Reader/DummyReader.php
+++ src/Pim/Bundle/BaseConnectorBundle/Reader/DummyReader.php
@@ -11,6 +11,8 @@ use Akeneo\Bundle\BatchBundle\Item\ItemReaderInterface;
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @deprecated please use Pim\Component\Connector\Reader\DummyItemReader
  */
 class DummyReader extends AbstractConfigurableStepElement implements ItemReaderInterface
 {
