1d2f4f83c8f8dae478411bca4fecbba00487b986
Olivier Soulet <olivier.soulet@akeneo.com>
2015-08-05 10:39:27 +0200

    PIM-4690: Changed interfaces
    Interfaces has changed in order to also use the assets categories

--- spec/Pim/Bundle/TransformBundle/Normalizer/Structured/CategoryNormalizerSpec.php
+++ spec/Pim/Bundle/TransformBundle/Normalizer/Structured/CategoryNormalizerSpec.php
@@ -3,7 +3,7 @@
 namespace spec\Pim\Bundle\TransformBundle\Normalizer\Structured;
 
 use PhpSpec\ObjectBehavior;
-use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
+use Pim\Component\Classification\Model\CategoryInterface;
 use Pim\Bundle\TransformBundle\Normalizer\Flat\TranslationNormalizer;
 use Prophecy\Argument;
 
