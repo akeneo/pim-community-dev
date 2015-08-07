8c03df04ce265c00a9f0be21e418ca904b6e9d5a
Olivier Soulet <olivier.soulet@akeneo.com>
2015-08-07 15:35:04 +0200

    PIM-4341: specs for asset normalizer

---
+++ spec/PimEnterprise/Bundle/EnrichBundle/Normalizer/AssetNormalizerSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\PimEnterprise\Bundle\EnrichBundle\Normalizer;
+
+use Akeneo\Component\FileStorage\Model\FileInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Model\LocaleInterface;
+use PimEnterprise\Component\ProductAsset\Model\AssetInterface;
+use PimEnterprise\Component\ProductAsset\Model\ReferenceInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class AssetNormalizerSpec extends ObjectBehavior
+{
+    function let(NormalizerInterface $normalizer)
+    {
+        $this->beConstructedWith($normalizer);
+    }
+    function it_is_a_normalizer()
+    {
+        $this->shouldImplement('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+    }
+
+    function it_should_normalize_with_no_references_and_no_categories($normalizer, AssetInterface $asset)
+    {
+        $normalizedValues = [
+            'code'        => 'code',
+            'description' => 'my description',
+            'enabled'     => true,
+            'end_of_use'  => '2010-10-10',
+            'created_at'  => '2017-10-11',
+            'updated_at'  => '2013-10-12',
+        ];
+
+        $result = $normalizedValues + ['references' => [], 'categories' => []];
+
+        $normalizer->normalize($asset, null, [])->willReturn($normalizedValues);
+        $asset->getCode()->willReturn('code');
+        $asset->getDescription()->willReturn('my description');
+        $asset->isEnabled()->willReturn(true);
+        $asset->getEndOfUseAt()->willReturn(new \Datetime('2010-10-10'));
+        $asset->getCreatedAt()->willReturn(new \Datetime('2017-10-11'));
+        $asset->getUpdatedAt()->willReturn(new \Datetime('2013-10-12'));
+        $asset->getTagCodes()->willReturn('tag1,tag2,tag3');
+        $asset->getCategoryCodes()->willReturn('cat1,cat2,cat3');
+        $asset->getReferences()->willReturn(new ArrayCollection([]));
+        $asset->getCategories()->willReturn(new ArrayCollection([]));
+
+        $this->normalize($asset)->shouldReturn($result);
+    }
+}
---
+++ spec/PimEnterprise/Component/ProductAsset/Connector/Processor/Normalization/AssetProcessorSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace spec\PimEnterprise\Component\ProductAsset\Connector\Processor\Normalization;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Manager\LocaleManager;
+use PimEnterprise\Component\ProductAsset\Model\AssetInterface;
+use Prophecy\Argument;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class AssetProcessorSpec extends ObjectBehavior
+{
+    function let(
+        SerializerInterface $serializer,
+        LocaleManager $localeManager,
+        NormalizerInterface $assetNormalizer
+    ) {
+        $this->beConstructedWith($serializer, $localeManager, $assetNormalizer);
+    }
+
+    function it_is_a_configurable_step_execution_aware_processor()
+    {
+        $this->shouldImplement('Pim\Bundle\BaseConnectorBundle\Processor\CsvSerializer\Processor');
+    }
+
+    function it_processes(
+        $assetNormalizer,
+        $serializer,
+        $localeManager,
+        AssetInterface $asset
+    ) {
+
+        $values =
+            [
+                'code'          => 'mycode',
+                'localized'     => 0,
+                'description'   => 'My awesome description',
+                'categories'    => 'cat1,cat2,cat3',
+                'qualification' => 'dog,flowers',
+                'end_of_use'    => '2018/02/01',
+            ];
+
+        $result = [
+            'code,localized,description,categories,qualification,end_of_use',
+            'mycode;0;"My awesome description";cat1,cat2,cat3;dog,flowers;2018/02/01'
+        ];
+        $assetNormalizer->normalize($asset)->willReturn($values);
+        $serializer->serialize($values, 'csv',
+            [
+                'delimiter'     => ';',
+                'enclosure'     => '"',
+                'withHeader'    => true,
+                'heterogeneous' => false,
+                'locales'       => 'en_US',
+            ])->willReturn($result);
+
+        $localeManager->getActiveCodes()->willReturn('en_US');
+
+        $this
+            ->process($asset)
+            ->shouldReturn($result);
+    }
+}
