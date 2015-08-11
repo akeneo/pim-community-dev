48ec8f8b58d23e254478ab1e7f511b484f2c786e
skeleton <marie.bochu@akeneo.com>
2015-08-11 11:18:32 +0200

    PIM-4292: call itemCategoryRepository for asset category

--- src/PimEnterprise/Component/ProductAsset/Repository/AssetCategoryRepositoryInterface.php
+++ src/PimEnterprise/Component/ProductAsset/Repository/AssetCategoryRepositoryInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2015 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PimEnterprise\Component\ProductAsset\Repository;
 
 use Pim\Component\Classification\Repository\CategoryFilterableRepositoryInterface;
@@ -10,8 +19,6 @@ use Pim\Component\Classification\Repository\ItemCategoryRepositoryInterface;
  *
  * @author Adrien Pétremann <adrien.petremann@akeneo.com>
  */
-interface AssetCategoryRepositoryInterface extends
-    ItemCategoryRepositoryInterface,
-    CategoryFilterableRepositoryInterface
+interface AssetCategoryRepositoryInterface
 {
 }
