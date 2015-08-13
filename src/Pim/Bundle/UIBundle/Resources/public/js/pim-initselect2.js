2919d2649caa4619c011b35cecf95d78c5c01636
willy-ahva <willy.mesnage@akeneo.com>
2015-08-13 11:11:22 +0200

    PIM-4737: Fix a bug with the select2 cache.

--- src/Pim/Bundle/UIBundle/Resources/public/js/pim-initselect2.js
+++ src/Pim/Bundle/UIBundle/Resources/public/js/pim-initselect2.js
@@ -4,14 +4,15 @@ define(
     function ($, _) {
         'use strict';
         return {
-            cacheDataSource: [],
             resultsPerPage: 20,
             init: function ($target) {
                 var self = this;
+
                 $target.find('input.select2:not(.select2-offscreen)').each(function () {
                     var $el   = $(this);
                     var value = _.map(_.compact($el.val().split(',')), $.trim);
                     var tags  = _.map(_.compact($el.attr('data-tags').split(',')), $.trim);
+
                     tags = _.union(tags, value).sort();
                     $el.select2({ tags: tags, tokenSeparators: [',', ' '] });
                 });
@@ -19,6 +20,7 @@ define(
                 $target.find('select.select2:not(.select2-offscreen)').each(function () {
                     var $el    = $(this);
                     var $empty = $el.children('[value=""]');
+
                     if ($empty.length && $empty.html()) {
                         $el.attr('data-placeholder', $empty.html());
                         $empty.html('');
@@ -31,12 +33,12 @@ define(
                 });
             },
             initSelect: function ($select) {
-                var selectId = $select.context.id;
                 var options = {
                     multiple: false,
                     allowClear: false
                 };
                 var self = this;
+
                 if ($select.attr('data-multiple')) {
                     options.multiple = true;
                 }
@@ -46,54 +48,40 @@ define(
                     }
                     options.placeholder = ' ';
                 }
+
                 options.minimumInputLength = $select.attr('data-min-input-length');
                 options.query = function (options) {
-
                     var page = 1;
 
                     if (options.context && options.context.page) {
                         page = options.context.page;
                     }
 
-                    var key = [options.term, page, selectId].join('_');
-                    var cachedData = self.cacheDataSource[key];
-
-                    if (cachedData) {
-                        options.callback({
-                            results: cachedData.results,
-                            more: cachedData.results.length === self.resultsPerPage,
-                            context: {
-                                page: page + 1
+                    $.ajax({
+                        url: $select.attr("data-url"),
+                        data: {
+                            search: options.term,
+                            options: {
+                                limit: self.resultsPerPage,
+                                page: page
                             }
-                        });
-                    } else {
-                        $.ajax({
-                            url: $select.attr('data-url'),
-                            data: {
-                                search: options.term,
-                                options: {
-                                    limit: self.resultsPerPage,
-                                    page: page
+                        },
+                        dataType: 'json',
+                        type: 'GET',
+                        success: function (data) {
+                            options.callback({
+                                results: data.results,
+                                more: data.results.length === self.resultsPerPage,
+                                context: {
+                                    page: page + 1
                                 }
-                            },
-                            dataType: 'json',
-                            type: 'GET',
-                            success: function (data) {
-                                self.cacheDataSource[key] = data;
-
-                                options.callback({
-                                    results: data.results,
-                                    more: data.results.length === self.resultsPerPage,
-                                    context: {
-                                        page: page + 1
-                                    }
-                                });
-                            }
-                        });
-                    }
+                            });
+                        }
+                    });
                 };
-                options.initSelection = function (element, callback) {
+                options.initSelection = function(element, callback) {
                     var choices = $.parseJSON($select.attr('data-choices'));
+
                     callback(choices);
                 };
                 $select.select2(options);
