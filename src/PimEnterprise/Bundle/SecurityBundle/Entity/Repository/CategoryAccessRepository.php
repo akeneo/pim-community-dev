93b14ba6309187d223129242ecf904bc228fda3b
Nicolas Dupont <nicolas@akeneo.com>
2015-08-19 09:21:30 +0200

    PIM-4765: fix cs

--- src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/CategoryAccessRepository.php
+++ src/PimEnterprise/Bundle/SecurityBundle/Entity/Repository/CategoryAccessRepository.php
@@ -16,9 +16,9 @@ use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Oro\Bundle\UserBundle\Entity\Group;
-use Pim\Bundle\UserBundle\Entity\User;
 use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
 use Pim\Bundle\CatalogBundle\Model\ProductInterface;
+use Pim\Bundle\UserBundle\Entity\User;
 use PimEnterprise\Bundle\SecurityBundle\Attributes;
 
 /**
