903f77c775bfddc7e3c5c3aa65b736e1f3173bec
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-08-19 15:49:44 +0200

    PIM-4759: fix specs

--- spec/PimEnterprise/Bundle/EnrichBundle/Normalizer/AssetNormalizerSpec.php
+++ spec/PimEnterprise/Bundle/EnrichBundle/Normalizer/AssetNormalizerSpec.php
@@ -29,19 +29,15 @@ class AssetNormalizerSpec extends ObjectBehavior
             'description' => 'my description',
             'enabled'     => true,
             'end_of_use'  => '2010-10-10',
-            'created_at'  => '2017-10-11',
-            'updated_at'  => '2013-10-12',
         ];
 
         $result = $normalizedValues + ['references' => [], 'categories' => []];
 
-        $normalizer->normalize($asset, null, [])->willReturn($normalizedValues);
+        $normalizer->normalize($asset, 'structured', [])->willReturn($normalizedValues);
         $asset->getCode()->willReturn('code');
         $asset->getDescription()->willReturn('my description');
         $asset->isEnabled()->willReturn(true);
         $asset->getEndOfUseAt()->willReturn(new \Datetime('2010-10-10'));
-        $asset->getCreatedAt()->willReturn(new \Datetime('2017-10-11'));
-        $asset->getUpdatedAt()->willReturn(new \Datetime('2013-10-12'));
         $asset->getTagCodes()->willReturn('tag1,tag2,tag3');
         $asset->getCategoryCodes()->willReturn('cat1,cat2,cat3');
         $asset->getReferences()->willReturn(new ArrayCollection([]));
