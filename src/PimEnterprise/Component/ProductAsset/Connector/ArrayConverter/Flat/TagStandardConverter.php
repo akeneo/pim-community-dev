b1ab98b3d5e69f3f6d3a235dea85c173fd1bbc2f
Nicolas Dupont <nicolas@akeneo.com>
2015-08-21 11:23:32 +0200

    PIM-4784: add a tag converter to import only tags in a simplified file

---
+++ src/PimEnterprise/Component/ProductAsset/Connector/ArrayConverter/Flat/TagStandardConverter.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2015 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Component\ProductAsset\Connector\ArrayConverter\Flat;
+
+use Pim\Component\Connector\ArrayConverter\StandardArrayConverterInterface;
+use Pim\Component\Connector\Exception\ArrayConversionException;
+
+/**
+ * Product Asset Tag Converter
+ *
+ * @author Nicolas Dupont <nicolas@akeneo.com>
+ */
+class TagStandardConverter implements StandardArrayConverterInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * Converts flat csv array to standard structured array:
+     *
+     * Before:
+     * [
+     *      'qualification' => 'dog,flowers,cities,animal,sunset',
+     * ]
+     *
+     * After:
+     * [
+     *      'tags'        => [
+     *          'dog',
+     *          'flowers',
+     *          'cities',
+     *          'animal',
+     *          'sunset',
+     *      ]
+     * ]
+     */
+    public function convert(array $item, array $options = [])
+    {
+        $this->validate($item);
+
+        $convertedItem = ['tags' => []];
+        foreach ($item as $field => $data) {
+            if ('' !== $data) {
+                $convertedItem = $this->convertField($convertedItem, $field, $data);
+            }
+        }
+
+        return $convertedItem;
+    }
+
+    /**
+     * @param array  $convertedItem
+     * @param string $field
+     * @param mixed  $data
+     *
+     * @return array
+     *
+     * TODO: qualification when import, tags when export ... + localized field, we should be able to import what we export
+     */
+    protected function convertField(array $convertedItem, $field, $data)
+    {
+        switch ($field) {
+            case 'qualification':
+                $convertedItem['tags'] = array_unique(explode(',', $data));
+                break;
+        }
+
+        return $convertedItem;
+    }
+
+    /**
+     * @param array $item
+     */
+    protected function validate(array $item)
+    {
+        $this->validateRequiredFields($item, ['qualification']);
+    }
+
+    /**
+     * @param array $item
+     * @param array $requiredFields
+     *
+     * @throws ArrayConversionException
+     */
+    protected function validateRequiredFields(array $item, array $requiredFields)
+    {
+        foreach ($requiredFields as $requiredField) {
+            if (!in_array($requiredField, array_keys($item))) {
+                throw new ArrayConversionException(
+                    sprintf(
+                        'Field "%s" is expected, provided fields are "%s"',
+                        $requiredField,
+                        implode(', ', array_keys($item))
+                    )
+                );
+            }
+        }
+    }
+}
