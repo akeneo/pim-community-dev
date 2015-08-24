c584ab3451c7f40c0fbba276251d2cc21c8bad00
Adrien Pétremann <petremann.adrien@gmail.com>
2015-08-24 16:02:03 +0200

    PIM-4536: Introduce localizable restriction as an extension

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/attributes/localizable.js
@@ -0,0 +1,80 @@
+'use strict';
+/**
+ * Localizable field extension
+ *
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(
+    [
+        'jquery',
+        'underscore',
+        'pim/form',
+        'pim/fetcher-registry',
+        'oro/mediator',
+    ],
+    function ($, _, BaseForm, FetcherRegistry, mediator) {
+        return BaseForm.extend({
+            configure: function () {
+                this.listenTo(mediator, 'pim_enrich:form:field:extension:add', this.addFieldExtension);
+
+                return BaseForm.prototype.configure.apply(this, arguments);
+            },
+
+            /**
+             * Add this field extension to the given field event
+             *
+             * @param {Object} event
+             *
+             * @returns {Promise}
+             */
+            addFieldExtension: function (event) {
+                var field = event.field;
+
+                if (!field.attribute.localizable) {
+                    return;
+                }
+
+                var retrieveChannel = FetcherRegistry.getFetcher('channel')
+                    .fetch(field.context.scope)
+                    .then(function (channel) {
+                        if (!this.channelHasLocale(channel, field.context.locale)) {
+                            this.updateInput(field.context, field);
+                        }
+                    }.bind(this));
+
+                event.promises.push(retrieveChannel);
+
+                return this;
+            },
+
+            /**
+             * Return whether the given channel has the given locale
+             *
+             * @param {string} channel
+             *
+             * @returns {boolean}
+             */
+            channelHasLocale: function (channel, locale) {
+                return _.contains(channel.locales, locale);
+            },
+
+            /**
+             * Update the given field by adding element to it
+             *
+             * @param {Object} field
+             */
+            updateInput: function (context, field) {
+                field.addElement(
+                    'field-input',
+                    'input_placeholder',
+                    _.__('pim_enrich.entity.product.localizable.channel_locale_unavailable', {
+                        channel: context.scope,
+                        locale: context.locale
+                    })
+                );
+            }
+        });
+    }
+);
