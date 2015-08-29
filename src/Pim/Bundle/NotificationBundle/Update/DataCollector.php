847e5b8d9eb70facb29750d8d4750aa950e75ebd
Nicolas Dupont <nicolas@akeneo.com>
2015-08-29 13:30:28 +0200

    PIM-4345: get last patch from update server

---
+++ src/Pim/Bundle/NotificationBundle/Update/DataCollector.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pim\Bundle\NotificationBundle\Update;
+
+/**
+ * Data collector
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class DataCollector
+{
+    /**
+     * @return array
+     */
+    public function collect()
+    {
+
+    }
+}
