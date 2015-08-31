5f539e2009c834c644a42fa8142cc3a001383476
Nicolas Dupont <nicolas@akeneo.com>
2015-08-31 19:51:45 +0200

    PIM-4345: store last available patch in session storage to limit calls

--- src/Pim/Bundle/AnalyticsBundle/Resources/public/js/patch-fetcher.js
+++ src/Pim/Bundle/AnalyticsBundle/Resources/public/js/patch-fetcher.js
@@ -5,19 +5,33 @@ define(
 
         return {
             /**
-             * @return {Object}
+             * @return {Promise}
              */
             fetch: function (updateServerUrl) {
-                return DataCollector.collect('pim_analytics_data_collect').then(function (collectedData) {
-                    var version = collectedData['pim_version'];
-                    var minorVersion = _.first(version.match(/^\d+.\d+/g));
-                    var lastPatchUrl = updateServerUrl
-                        + '/' + collectedData['pim_edition']
-                        + '-' + minorVersion
-                        + '.json';
-                    // TODO: trim the "v" in the version code
-                    return $.getJSON(lastPatchUrl, collectedData);
-                });
+                var storageEnabled = typeof Storage !== 'undefined' && sessionStorage;
+                var lastPatchKey = 'last-available-patch';
+                if (storageEnabled && null !== sessionStorage.getItem(lastPatchKey)) {
+                    var deferred = $.Deferred();
+                    deferred.resolve(sessionStorage.getItem(lastPatchKey));
+                    return deferred.promise();
+
+                } else {
+                    return DataCollector.collect('pim_analytics_data_collect').then(function (collectedData) {
+                        var version = collectedData.pim_version;
+                        var minorVersion = _.first(version.match(/^\d+.\d+/g));
+                        var lastPatchUrl = updateServerUrl
+                            + '/' + collectedData.pim_edition
+                            + '-' + minorVersion
+                            + '.json';
+
+                        return $.getJSON(lastPatchUrl, collectedData).then(function (patchData) {
+                            var patch = patchData.last_patch.name;
+                            var cleanedPatch = patch.replace(/^v/g, '');
+                            sessionStorage.setItem(lastPatchKey, cleanedPatch);
+                            return cleanedPatch;
+                        });
+                    });
+                }
             }
         };
     }
