d2602a049f5e5be469ab4ab9c42b9876e13f16d2
Nicolas Dupont <nicolas@akeneo.com>
2015-09-02 09:30:47 +0200

    PIM-4345: add environment in analytics data collector

--- src/Pim/Bundle/AnalyticsBundle/DataCollector/VersionDataCollector.php
+++ src/Pim/Bundle/AnalyticsBundle/DataCollector/VersionDataCollector.php
@@ -20,14 +20,19 @@ class VersionDataCollector implements DataCollectorInterface
     /** @var string */
     protected $catalogStorage;
 
+    /** @var string */
+    protected $environment;
+
     /**
      * @param VersionProviderInterface $versionProvider
      * @param string                   $catalogStorage
+     * @param string                   $environment
      */
-    public function __construct(VersionProviderInterface $versionProvider, $catalogStorage)
+    public function __construct(VersionProviderInterface $versionProvider, $catalogStorage, $environment)
     {
         $this->versionProvider = $versionProvider;
         $this->catalogStorage  = $catalogStorage;
+        $this->environment     = $environment;
     }
 
     /**
@@ -38,7 +43,8 @@ class VersionDataCollector implements DataCollectorInterface
         return [
             'pim_edition'        => $this->versionProvider->getEdition(),
             'pim_version'        => $this->versionProvider->getPatch(),
-            'pim_storage_driver' => $this->catalogStorage
+            'pim_storage_driver' => $this->catalogStorage,
+            'pim_environment'    => $this->environment
         ];
     }
 }
