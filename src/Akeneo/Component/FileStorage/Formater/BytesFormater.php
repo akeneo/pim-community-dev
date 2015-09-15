8f3117effd0ffa15289b4ecbf877326834a55503
Olivier Soulet <olivier.soulet@akeneo.com>
2015-09-15 14:06:41 +0200

    PIM-4816: add license

--- src/Akeneo/Component/FileStorage/Formater/BytesFormater.php
+++ src/Akeneo/Component/FileStorage/Formater/BytesFormater.php
@@ -3,9 +3,9 @@
 namespace Akeneo\Component\FileStorage\Formater;
 
 /**
- * Convert bytes into human readable value
- *
- * @author Olivier Soulet <olivier.soulet@akeneo.com>
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class BytesFormater
 {
