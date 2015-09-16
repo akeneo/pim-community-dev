0e1aa7a8f3c4dae4019ffb8c775e02b1369d45a3
skeleton <marie.bochu@akeneo.com>
2015-09-16 16:38:41 +0200

    Preparing version 1.3.26

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.25';
+    const VERSION = '1.3.26';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Force';
