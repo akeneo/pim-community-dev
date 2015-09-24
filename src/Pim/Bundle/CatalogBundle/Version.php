892b7624441078fe089a4d18127e6bdbab31ce7e
skeleton <marie.bochu@akeneo.com>
2015-09-24 14:12:04 +0200

    Preparing version 1.4.1

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
