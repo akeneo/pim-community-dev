8f4f40d0521d942b9c65988f470211316f52c0f1
Olivier Soulet <olivier.soulet@akeneo.com>
2015-09-24 11:51:21 +0200

    PIM-4869: add specs

--- src/Pim/Bundle/UserBundle/EventListener/LocaleListener.php
+++ src/Pim/Bundle/UserBundle/EventListener/LocaleListener.php
@@ -37,7 +37,6 @@ class LocaleListener implements EventSubscriberInterface
         if (!$request) {
             return;
         }
-
         if (!$request->attributes->get('_locale')) {
             $request->setLocale($this->localeSettings->getLanguage());
         }
