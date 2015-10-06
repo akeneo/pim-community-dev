c52391a72087656f56c103b1642b8a43b3c09edc
skeleton <marie.bochu@akeneo.com>
2015-10-06 11:22:42 +0200

    PIM-3305: add LocalizationBundle

---
+++ src/Pim/Bundle/LocalizationBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pim\Bundle\LocalizationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('pim_localization');
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Pim/Bundle/LocalizationBundle/PimLocalizationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pim\Bundle\LocalizationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PimLocalizationBundle extends Bundle
+{
+}
