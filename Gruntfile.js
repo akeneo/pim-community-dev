a1769cbae347569c43c1a01e56bed2f80a66b8e7
Nicolas Dupont <nicolas@akeneo.com>
2015-10-08 14:08:06 +0200

    Exclude Oro js files from jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function (grunt) {
                 'src/**/*.js',
                 '!src/**/lib/**/*.js',
                 '!src/**/DataGridBundle/**/*.js',
-                '!src/**/NavigationBundle/**/*.js'
+                '!src/**/NavigationBundle/**/*.js',
+                '!src/Oro/**/**/*.js'
             ],
             options: {
                 jshintrc: '.jshintrc',
