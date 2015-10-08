894b1dd278561ce3ab21587ba51e6ae4d903385f
willy-ahva <willy.mesnage@akeneo.com>
2015-10-08 15:11:26 +0200

    PIM-2736: Add possibility to comment a notification when a draft is send for approval

---
+++ src/PimEnterprise/Bundle/WorkflowBundle/Resources/public/js/product/form/comment-for-approval.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Form to comment when product is send for approval
+ *
+ * @author Willy MESNAGE <willy.mesnage@akeneo.com>
+ */
+define(
+    [
+        'underscore',
+        'pim/form',
+        'text!pimee/template/product/meta/comment'
+    ],
+    function (_, BaseForm, template) {
+        return BaseForm.extend({
+            template: _.template(template),
+            render: function () {
+                this.$el.html(this.template());
+
+                return this;
+            }
+        });
+    }
+);
