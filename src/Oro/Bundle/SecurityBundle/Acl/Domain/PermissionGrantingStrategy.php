e517424c3c7672dc46932e7cf955081a05a7200a
Nicolas Dupont <nicolas@akeneo.com>
2015-10-12 11:43:32 +0200

    PIM-5001: fix cs

--- src/Oro/Bundle/SecurityBundle/Acl/Domain/PermissionGrantingStrategy.php
+++ src/Oro/Bundle/SecurityBundle/Acl/Domain/PermissionGrantingStrategy.php
@@ -26,7 +26,7 @@ class PermissionGrantingStrategy implements PermissionGrantingStrategyInterface
     protected $auditLogger;
 
     /**
-     * @var \Oro\Bundle\SecurityBundle\DependencyInjection\Utils\ServiceLink
+     * @var ServiceLink
      */
     private $contextLink;
 
@@ -43,7 +43,7 @@ class PermissionGrantingStrategy implements PermissionGrantingStrategyInterface
     /**
      * Sets the accessor to the context data of this strategy
      *
-     * @param \Oro\Bundle\SecurityBundle\DependencyInjection\Utils\ServiceLink $contextLink The link to a service implementing PermissionGrantingStrategyContextInterface
+     * @param ServiceLink $contextLink The link to a service implementing PermissionGrantingStrategyContextInterface
      */
     public function setContext(ServiceLink $contextLink)
     {
