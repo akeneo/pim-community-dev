8740e5df97eba941f141565b8010f0420d146096
Julien Sanchez <sumbobyboys@gmail.com>
2015-10-12 17:55:00 +0200

    PIM-4945: rework the ProductsUpdater to add an action applier registry

--- CHANGELOG-1.5.md
+++ CHANGELOG-1.5.md
@@ -6,3 +6,4 @@
 
 - Rename service `pimee_product_asset.extension.formatter.property.product_value.product_asset_property` to `pimee_product_asset.datagrid.extension.formatter.property.product_value.product_asset_property`
 - Column 'comment' has been added on the `pim_notification_notification` table.
+- PropertySetterInterface and PropertyCopierInterface were removed from the PimEnterprise\Bundle\CatalogRuleBundle\Engine\ProductRuleApplier\ProductsUpdater and replaced by Akeneo\Component\RuleEngine\ActionApplier\ActionApplierRegistryInterface
--- spec/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleApplier/ProductsUpdaterSpec.php
+++ spec/PimEnterprise/Bundle/CatalogRuleBundle/Engine/ProductRuleApplier/ProductsUpdaterSpec.php
@@ -3,29 +3,26 @@
 namespace spec\PimEnterprise\Bundle\CatalogRuleBundle\Engine\ProductRuleApplier;
 
 use Akeneo\Bundle\RuleEngineBundle\Model\RuleInterface;
+use Akeneo\Component\RuleEngine\ActionApplier\ActionApplierInterface;
+use Akeneo\Component\RuleEngine\ActionApplier\ActionApplierRegistryInterface;
 use Akeneo\Component\StorageUtils\Updater\PropertyCopierInterface;
 use Akeneo\Component\StorageUtils\Updater\PropertySetterInterface;
 use PhpSpec\ObjectBehavior;
+use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction;
+use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueActionInterface;
 use Pim\Bundle\CatalogBundle\Model\GroupInterface;
 use Pim\Bundle\CatalogBundle\Model\ProductInterface;
 use Pim\Bundle\CatalogBundle\Model\ProductTemplateInterface;
 use Pim\Component\Catalog\Updater\ProductTemplateUpdaterInterface;
-use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductCopyValueAction;
-use PimEnterprise\Bundle\CatalogRuleBundle\Model\ProductSetValueActionInterface;
 use Prophecy\Argument;
 
 class ProductsUpdaterSpec extends ObjectBehavior
 {
     function let(
-        PropertySetterInterface $propertySetter,
-        PropertyCopierInterface $propertyCopier,
+        ActionApplierRegistryInterface $applierRegistry,
         ProductTemplateUpdaterInterface $templateUpdater
     ) {
-        $this->beConstructedWith(
-            $propertySetter,
-            $propertyCopier,
-            $templateUpdater
-        );
+        $this->beConstructedWith($applierRegistry, $templateUpdater);
     }
 
     function it_is_initializable()
@@ -34,16 +31,14 @@ class ProductsUpdaterSpec extends ObjectBehavior
     }
 
     function it_does_not_update_products_when_no_actions(
-        $propertySetter,
-        $propertyCopier,
+        $applierRegistry,
         $templateUpdater,
         RuleInterface $rule,
         ProductInterface $product
     ) {
         $rule->getActions()->willReturn([]);
 
-        $propertySetter->setData(Argument::any())->shouldNotBeCalled();
-        $propertyCopier->copyData(Argument::any())->shouldNotBeCalled();
+        $applierRegistry->getActionApplier(Argument::any())->shouldNotBeCalled();
 
         $templateUpdater->update(Argument::any(), Argument::any())->shouldNotBeCalled();
 
@@ -51,11 +46,12 @@ class ProductsUpdaterSpec extends ObjectBehavior
     }
 
     function it_updates_product_when_the_rule_has_a_set_action(
-        $propertySetter,
+        $applierRegistry,
         $templateUpdater,
         RuleInterface $rule,
         ProductInterface $product,
-        ProductSetValueActionInterface $action
+        ProductSetValueActionInterface $action,
+        ActionApplierInterface $actionApplier
     ) {
         $action->getField()->willReturn('sku');
         $action->getValue()->willReturn('foo');
@@ -63,65 +59,24 @@ class ProductsUpdaterSpec extends ObjectBehavior
         $action->getLocale()->willReturn('en_US');
         $rule->getActions()->willReturn([$action]);
 
-        $propertySetter->setData(Argument::any(), 'sku', 'foo', ['locale' => 'en_US', 'scope' => 'ecommerce'])
-            ->shouldBeCalled();
+        $applierRegistry->getActionApplier($action)->willReturn($actionApplier);
+        $actionApplier->applyAction($action, [$product]);
 
         $templateUpdater->update(Argument::any(), Argument::any())
             ->shouldNotBeCalled();
 
         $this->update($rule, [$product]);
-    }
-
-    function it_updates_product_when_the_rule_has_a_copy_action(
-        $propertyCopier,
-        $templateUpdater,
-        RuleInterface $rule,
-        ProductInterface $product,
-        ProductCopyValueAction $action
-    ) {
-        $action->getFromField()->willReturn('sku');
-        $action->getToField()->willReturn('description');
-        $action->getFromLocale()->willReturn('fr_FR');
-        $action->getToLocale()->willReturn('fr_CH');
-        $action->getFromScope()->willReturn('ecommerce');
-        $action->getToScope()->willReturn('tablet');
-        $rule->getActions()->willReturn([$action]);
-
-        $propertyCopier
-            ->copyData(
-                $product,
-                $product,
-                'sku',
-                'description',
-                ['from_locale' => 'fr_FR', 'to_locale' => 'fr_CH', 'from_scope' => 'ecommerce', 'to_scope' => 'tablet']
-            )
-            ->shouldBeCalled();
-
-        $templateUpdater->update(Argument::any(), Argument::any())
-            ->shouldNotBeCalled();
-
-        $this->update($rule, [$product]);
-    }
-
-    function it_throws_exception_when_update_a_product_with_an_unknown_action(
-        RuleInterface $rule,
-        ProductInterface $product
-    ) {
-        $rule->getActions()->willReturn([new \stdClass()]);
-        $rule->getCode()->willReturn('test_rule');
-
-        $this->shouldThrow(new \LogicException('The action "stdClass" is not supported yet.'))
-            ->during('update', [$rule, [$product]]);
     }
 
     function it_ensures_priority_of_variant_group_values_over_the_rule(
-        $propertyCopier,
+        $applierRegistry,
         $templateUpdater,
         RuleInterface $rule,
         ProductInterface $product,
         ProductCopyValueAction $action,
         GroupInterface $group,
-        ProductTemplateInterface $productTemplate
+        ProductTemplateInterface $productTemplate,
+        ActionApplierInterface $actionApplier
     ) {
         $action->getFromField()->willReturn('sku');
         $action->getToField()->willReturn('description');
@@ -131,15 +86,8 @@ class ProductsUpdaterSpec extends ObjectBehavior
         $action->getToScope()->willReturn('tablet');
         $rule->getActions()->willReturn([$action]);
 
-        $propertyCopier
-            ->copyData(
-                $product,
-                $product,
-                'sku',
-                'description',
-                ['from_locale' => 'fr_FR', 'to_locale' => 'fr_CH', 'from_scope' => 'ecommerce', 'to_scope' => 'tablet']
-            )
-            ->shouldBeCalled();
+        $applierRegistry->getActionApplier($action)->willReturn($actionApplier);
+        $actionApplier->applyAction($action, [$product]);
 
         $product->getVariantGroup()->willReturn($group);
         $group->getProductTemplate()->willReturn($productTemplate);
