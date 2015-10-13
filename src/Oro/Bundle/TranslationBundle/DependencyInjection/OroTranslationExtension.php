4648c858791d08774bef7c84887c5a86666d61c2
Nicolas Dupont <nicolas@akeneo.com>
2015-10-13 19:11:08 +0200

    PIM-5001: remove useless classes from OroTranslationBundle

--- src/Oro/Bundle/TranslationBundle/DependencyInjection/OroTranslationExtension.php
+++ src/Oro/Bundle/TranslationBundle/DependencyInjection/OroTranslationExtension.php
@@ -18,7 +18,6 @@ class OroTranslationExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('form_types.yml');
         $loader->load('services.yml');
 
         $container
