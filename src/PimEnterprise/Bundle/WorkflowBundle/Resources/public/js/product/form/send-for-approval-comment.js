68f2420a1915f29b74dacc960f217c8148419179
willy-ahva <willy.mesnage@akeneo.com>
2015-10-13 18:09:48 +0200

    PIM-2736: Rework the way comment are sent in notifications

---
+++ src/PimEnterprise/Bundle/WorkflowBundle/Resources/public/js/product/form/send-for-approval-comment.js
@@ -0,0 +1,35 @@
+'use strict';
+/**
+ * Form to add a comment in a notification when the proposal is sent for approval
+ *
+ * @author Willy Mesnage <willy.mesnage@akeneo.com>
+ */
+define(
+    [
+        'underscore',
+        'backbone',
+        'pim/form',
+        'text!pimee/template/product/meta/notification-comment'
+    ],
+    function (_, Backbone, BaseForm, template) {
+        return BaseForm.extend({
+            template: _.template(template),
+            events: {
+                'change textarea': 'updateModel'
+            },
+            initialize: function () {
+                this.model = new Backbone.Model();
+
+                BaseForm.prototype.initialize.apply(this, arguments);
+            },
+            updateModel: function () {
+                this.model.set('comment', this.$('textarea[id="modal-comment"]').val());
+            },
+            render: function () {
+                this.$el.html(this.template({title: _.__('pimee_enrich.entity.product_draft.modal.title')}));
+
+                return this;
+            }
+        });
+    }
+);
