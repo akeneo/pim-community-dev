3675800f7857d07932a69fc314bc62e858b9af5b
Nicolas Dupont <nicolas@akeneo.com>
2015-10-14 18:31:31 +0200

    PIM-5001: fix cs

--- src/Oro/Bundle/UIBundle/Route/Router.php
+++ src/Oro/Bundle/UIBundle/Route/Router.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-
 /**
  * Class Router
  *
