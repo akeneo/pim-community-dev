04c04d8d08eeee855a6fbab1fa5664e646ac0370
pierallard <pierallard@gmail.com>
2015-10-14 10:33:44 +0200

    PIM-4940: Restrict PIM locale select with min translation progress

--- src/Oro/Bundle/LocaleBundle/Form/Type/LocaleType.php
+++ src/Oro/Bundle/LocaleBundle/Form/Type/LocaleType.php
@@ -2,22 +2,30 @@
 
 namespace Oro\Bundle\LocaleBundle\Form\Type;
 
+use Pim\Component\Localization\Provider\LocaleProviderInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Intl\Intl;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LocaleType extends AbstractType
 {
+    /** @var LocaleProviderInterface */
+    protected $localeProvider;
+
+    /**
+     * @param LocaleProviderInterface $localeProvider
+     */
+    public function __construct(LocaleProviderInterface $localeProvider)
+    {
+        $this->localeProvider = $localeProvider;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(
-            array(
-                'choices' => Intl::getLocaleBundle()->getLocaleNames('en'),
-            )
-        );
+        $choices = $this->localeProvider->getLocales();
+        $resolver->setDefaults(['choices' => $choices]);
     }
 
     /**
