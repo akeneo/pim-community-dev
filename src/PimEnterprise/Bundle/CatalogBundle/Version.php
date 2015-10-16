c271388712b25299f60380a52cd35594b4ee9501
Yohan BLAIN <yohan.blain@gmail.com>
2015-10-16 15:00:39 +0200

    Preparing version 1.4.4

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.3';
+    const VERSION = '1.4.4';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Samara';
