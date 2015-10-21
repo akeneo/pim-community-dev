744c97e0427b25792f213f2b0713a53f008d121a
Nicolas Dupont <nicolas@akeneo.com>
2015-10-21 09:15:59 +0200

    Add deprecated ProductInterface

---
+++ src/Pim/Bundle/CatalogBundle/Model/ProductInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Model;
+
+use Pim\Component\Catalog\Model\ProductInterface as BaseProductInterface;
+
+/**
+ * Deprecated product interface
+ *
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @deprecated please use Pim\Component\Catalog\Model\ProductInterface
+ */
+interface ProductInterface extends BaseProductInterface
+{
+}
