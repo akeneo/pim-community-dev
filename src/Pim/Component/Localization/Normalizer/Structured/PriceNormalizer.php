c9f61f412a781e5966dbbd325a661e5aabcf6098
skeleton <marie.bochu@akeneo.com>
2015-10-27 17:08:54 +0100

    PIM-4980: add metric, price and number structured normalizers

---
+++ src/Pim/Component/Localization/Normalizer/Structured/PriceNormalizer.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pim\Component\Localization\Normalizer\Structured;
+
+use Pim\Bundle\CatalogBundle\Model\ProductPriceInterface;
+use Pim\Component\Localization\Localizer\LocalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Normalize a price with a localized format
+ *
+ * @author    Marie Bochu <marie.bochu@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PriceNormalizer implements NormalizerInterface
+{
+    /** @var array */
+    protected $supportedFormats = ['json', 'xml'];
+
+    /** @var NormalizerInterface */
+    protected $priceNormalizer;
+
+    /** @var LocalizerInterface */
+    protected $localizer;
+
+    /**
+     * @param NormalizerInterface $priceNormalizer
+     * @param LocalizerInterface  $localizer
+     */
+    public function __construct(NormalizerInterface $priceNormalizer, LocalizerInterface $localizer)
+    {
+        $this->priceNormalizer = $priceNormalizer;
+        $this->localizer       = $localizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($price, $format = null, array $context = [])
+    {
+        $price = $this->priceNormalizer->normalize($price, $format, $context);
+
+        $price['data'] = $this->localizer->convertDefaultToLocalized($price['data'], $context);
+
+        return $price;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ProductPriceInterface && in_array($format, $this->supportedFormats);
+    }
+}
