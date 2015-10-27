68a56767aa43c0fde72849791adfcace34b5d272
Jean-Marie Leroux <jmleroux.pro@gmail.com>
2015-10-27 15:16:06 +0100

    Preparing version 1.4.6

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.5';
+    const VERSION = '1.4.6';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
