448376a99390ef6120f922ac76c504ef38cbde81
skeleton <marie.bochu@akeneo.com>
2015-10-28 10:22:50 +0100

    PIM-4980: add denormalizers

---
+++ spec/Pim/Component/Localization/Denormalizer/Structured/NumberDenormalizerSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace spec\Pim\Component\Localization\Denormalizer\Structured;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+use Pim\Component\Localization\Localizer\LocalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class NumberDenormalizerSpec extends ObjectBehavior
+{
+    function let(DenormalizerInterface $valuesDenormalizer, LocalizerInterface $localizer)
+    {
+        $this->beConstructedWith($valuesDenormalizer, $localizer, ['pim_catalog_number']);
+    }
+
+    function it_is_a_denormalizer()
+    {
+        $this->shouldBeAnInstanceOf('Symfony\Component\Serializer\Normalizer\DenormalizerInterface');
+    }
+
+    function it_supports_denormalization_of_number_values_from_json()
+    {
+        $this->supportsDenormalization([], 'pim_catalog_number', 'json')->shouldReturn(true);
+        $this->supportsDenormalization([], 'pim_catalog_number', 'csv')->shouldReturn(false);
+        $this->supportsDenormalization([], 'pim_catalog_text', 'json')->shouldReturn(false);
+    }
+
+    function it_returns_null_if_data_is_empty()
+    {
+        $this->denormalize('', 'pim_catalog_number', 'json')->shouldReturn(null);
+        $this->denormalize(null, 'pim_catalog_number', 'json')->shouldReturn(null);
+        $this->denormalize([], 'pim_catalog_number', 'json')->shouldReturn(null);
+    }
+
+    function it_denormalizes_data_into_number_with_english_format(
+        $valuesDenormalizer,
+        $localizer,
+        AttributeInterface $attribute
+    ) {
+        $attribute->getAttributeType()->willReturn('pim_catalog_number');
+        $options = ['attribute' => $attribute, 'locale' => 'en_US'];
+
+        $valuesDenormalizer->denormalize(3.85, 'Pim\Bundle\CatalogBundle\Model\ProductValue', 'json', $options)
+            ->willReturn(3.85);
+
+        $localizer->localize(3.85, $options)->willReturn(3.85);
+
+        $this->denormalize(3.85, 'Pim\Bundle\CatalogBundle\Model\ProductValue', 'json', $options)
+            ->shouldReturn(3.85);
+    }
+
+    function it_denormalizes_data_into_number_with_french_format(
+        $valuesDenormalizer,
+        $localizer,
+        AttributeInterface $attribute
+    ) {
+        $attribute->getAttributeType()->willReturn('pim_catalog_number');
+        $options = ['attribute' => $attribute, 'locale' => 'fr_FR'];
+
+        $valuesDenormalizer->denormalize(3.85, 'Pim\Bundle\CatalogBundle\Model\ProductValue', 'json', $options)
+            ->willReturn(3.85);
+
+        $localizer->localize(3.85, $options)->willReturn('3,85');
+
+        $this->denormalize(3.85, 'Pim\Bundle\CatalogBundle\Model\ProductValue', 'json', $options)
+            ->shouldReturn('3,85');
+    }
+}
---
+++ src/Pim/Component/Localization/Denormalizer/Structured/NumberDenormalizer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Pim\Component\Localization\Denormalizer\Structured;
+
+use Pim\Component\Localization\Localizer\LocalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+/**
+ * Denormalize number to localize data
+ *
+ * @author    Marie Bochu <marie.bochu@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class NumberDenormalizer implements DenormalizerInterface
+{
+    /** @var string[] */
+    protected $supportedFormats = ['json'];
+
+    /** @var DenormalizerInterface */
+    protected $valuesDenormalizer;
+
+    /** @var LocalizerInterface */
+    protected $localizer;
+
+    /** @var string[] */
+    protected $supportedTypes;
+
+    /**
+     * @param DenormalizerInterface $valuesDenormalizer
+     * @param LocalizerInterface    $localizer
+     * @param string[]              $supportedTypes
+     */
+    public function __construct(
+        DenormalizerInterface $valuesDenormalizer,
+        LocalizerInterface $localizer,
+        array $supportedTypes
+    ) {
+        $this->valuesDenormalizer = $valuesDenormalizer;
+        $this->localizer          = $localizer;
+        $this->supportedTypes     = $supportedTypes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $data = $this->valuesDenormalizer->denormalize($data, $class, $format, $context);
+
+        if (null !== $data) {
+            $data = $this->localizer->convertDefaultToLocalized($data, $context);
+        }
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return in_array($type, $this->supportedTypes) && in_array($format, $this->supportedFormats);
+    }
+}
