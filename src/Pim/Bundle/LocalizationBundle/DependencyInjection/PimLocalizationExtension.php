f85ebd0ae3dcdbefd2572b0aac2c543b3f25b8ae
skeleton <marie.bochu@akeneo.com>
2015-10-28 10:22:50 +0100

    PIM-4980: add denormalizers

--- src/Pim/Bundle/LocalizationBundle/DependencyInjection/PimLocalizationExtension.php
+++ src/Pim/Bundle/LocalizationBundle/DependencyInjection/PimLocalizationExtension.php
@@ -37,6 +37,7 @@ class PimLocalizationExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('controllers.yml');
         $loader->load('converters.yml');
+        $loader->load('denormalizers.yml');
         $loader->load('localizers.yml');
         $loader->load('normalizers.yml');
         $loader->load('providers.yml');
