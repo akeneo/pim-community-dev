bc5416ab1d6920f3eaf38a25275371ff1d4dba1e
skeleton <marie.bochu@akeneo.com>
2015-11-03 18:28:26 +0100

    PIM-5271: remove useless files

--- src/Oro/Bundle/FormBundle/Resources/public/js/validator/range.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/validator/range.js
@@ -1,5 +1,5 @@
 /* global define */
-define(['underscore', 'oro/translator', 'oro/formatter/number'],
+define(['underscore', 'oro/translator'],
 function (_, __, numberFormatter) {
     'use strict';
 
@@ -15,15 +15,13 @@ function (_, __, numberFormatter) {
     return [
         'Range',
         function (value, element, param) {
-            value = numberFormatter.unformat(value);
             return this.optional(element) ||
                 !(isNaN(value) ||
                     (param.min !== null && value < Number(param.min)) ||
                     (param.max !== null && value > Number(param.max)));
         },
         function (param, element) {
-            var message, placeholders = {},
-                value = numberFormatter.unformat(this.elementValue(element));
+            var message, placeholders = {}
             param = _.extend({}, defaultParam, param);
             if (isNaN(value)) {
                 message = param.invalidMessage;
--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/datetime-cell.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/datetime-cell.js
@@ -1,6 +1,6 @@
 /* global define */
-define(['oro/datagrid/string-cell', 'oro/datagrid/datetime-formatter'],
-function(StringCell, DatagridDateTimeFormatter) {
+define(['oro/datagrid/string-cell'],
+function(StringCell) {
     'use strict';
 
     /**
@@ -10,32 +10,5 @@ function(StringCell, DatagridDateTimeFormatter) {
      * @class   oro.datagrid.DateTimeCell
      * @extends oro.datagrid.StringCell
      */
-    return StringCell.extend({
-        /**
-         * @property {oro.datagrid.DateTimeFormatter}
-         */
-        formatterPrototype: DatagridDateTimeFormatter,
-
-        /**
-         * @property {string}
-         */
-        type: 'dateTime',
-
-        /**
-         * @inheritDoc
-         */
-        initialize: function (options) {
-            StringCell.prototype.initialize.apply(this, arguments);
-            this.formatter = this.createFormatter();
-        },
-
-        /**
-         * Creates number cell formatter
-         *
-         * @return {oro.datagrid.DateTimeFormatter}
-         */
-        createFormatter: function() {
-            return new this.formatterPrototype({type: this.type});
-        }
-    });
+    return StringCell.extend({type: 'dateTime'});
 });
