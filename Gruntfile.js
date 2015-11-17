798e8c7d427dcbc0c3502e68c588d17836a3bcb6
pierallard <pierallard@gmail.com>
2015-11-17 10:27:52 +0100

    Update Travis and Grunt to manage imports

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,14 +34,14 @@ module.exports = function (grunt) {
         },
         recess: {
             all: [
-                'src/**/*.css',
-                'src/**/*.less',
-                '!src/**/lib/**/*.css',
-                '!src/**/lib/**/*.less',
-                '!src/Pim/Bundle/UIBundle/Resources/public/css/less/oro.less',
-                '!src/Pim/Bundle/UIBundle/Resources/public/css/pim.less',
-                '!src/Pim/Bundle/UIBundle/Resources/public/css/form.less',
-                '!src/Pim/Bundle/UIBundle/Resources/public/css/flags.less'
+                'web/bundles/pim*/**/*.css',
+                'web/bundles/pim*/**/*.less',
+                '!web/bundles/**/lib/**/*.css',
+                '!web/bundles/**/lib/**/*.less',
+                '!web/bundles/pimui/css/flags.less',
+                '!web/bundles/pimui/css/form.less',
+                '!web/bundles/pimui/css/less/oro.less',
+                '!web/bundles/pimui/css/pim.less'
             ],
             options: {
                 strictPropertyOrder: false,
