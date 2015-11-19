02b89cb0d4963cd740993b91b36deac46d9581e1
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-11-19 14:12:34 +0100

    Preparing version 1.3.32

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.31';
+    const VERSION = '1.3.32';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Strawberry';
