af60001a8b6f769e1b7414d46a453257ac0374c4
skeleton <marie.bochu@akeneo.com>
2015-11-23 14:45:18 +0100

    PIM-4999: add rest action to have date format

---
+++ src/Pim/Bundle/LocalizationBundle/Controller/FormatController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Pim\Bundle\LocalizationBundle\Controller;
+
+use Pim\Component\Localization\Factory\DateFactory;
+use Pim\Component\Localization\LocaleResolver;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Format controller
+ *
+ * @author    Marie Bochu <marie.bochu@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FormatController
+{
+    /** @var DateFactory */
+    protected $factory;
+
+    /** @var LocaleResolver */
+    protected $localeResolver;
+
+    /**
+     * @param DateFactory    $factory
+     * @param LocaleResolver $localeResolver
+     */
+    public function __construct(DateFactory $factory, LocaleResolver $localeResolver)
+    {
+        $this->factory        = $factory;
+        $this->localeResolver = $localeResolver;
+    }
+
+    /**
+     * Date format action (show pattern expected for current locale)
+     *
+     * @return JsonResponse
+     */
+    public function dateAction()
+    {
+        $locale    = $this->localeResolver->getCurrentLocale();
+        $formatter = $this->factory->create(['locale' => $locale]);
+
+        return new JsonResponse(
+            [
+                'format'   => $formatter->getPattern(),
+                'language' => $locale,
+            ]
+        );
+    }
+}
