9c1e60689aa8f0a4865fc0d593220fa894be57f4
Yohan BLAIN <yohan.blain@gmail.com>
2015-11-24 15:11:33 +0100

    PIM-5217: Replace deprecated file writers by new ones

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -152,6 +152,7 @@ class AppKernel extends Kernel
             new Oneup\FlysystemBundle\OneupFlysystemBundle(),
             new Akeneo\Bundle\MeasureBundle\AkeneoMeasureBundle(),
             new Akeneo\Bundle\BatchBundle\AkeneoBatchBundle(),
+            new Akeneo\Bundle\BufferBundle\AkeneoBufferBundle(),
             new Akeneo\Bundle\StorageUtilsBundle\AkeneoStorageUtilsBundle(),
             new Akeneo\Bundle\FileStorageBundle\AkeneoFileStorageBundle(),
         ];
