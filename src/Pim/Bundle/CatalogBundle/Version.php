f36a4f3bf0e1710a54975d6512a27a3c6406b087
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-11-25 10:15:08 +0100

    Preparing version 1.3.33

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.32';
+    const VERSION = '1.3.33';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Force';
