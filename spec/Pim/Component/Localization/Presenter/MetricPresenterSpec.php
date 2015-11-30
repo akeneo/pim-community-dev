4c89179202cf78c92d593afedf2624db25365d1c
pierallard <pierallard@gmail.com>
2015-11-30 14:46:13 +0100

    PIM-5033: Add DatePresenter

--- spec/Pim/Component/Localization/Presenter/MetricPresenterSpec.php
+++ spec/Pim/Component/Localization/Presenter/MetricPresenterSpec.php
@@ -4,6 +4,7 @@ namespace spec\Pim\Component\Localization\Presenter;
 
 use PhpSpec\ObjectBehavior;
 use Pim\Component\Localization\Factory\NumberFactory;
+use Prophecy\Argument;
 
 class MetricPresenterSpec extends ObjectBehavior
 {
@@ -24,6 +25,7 @@ class MetricPresenterSpec extends ObjectBehavior
     ) {
         $numberFactory->create([])->willReturn($numberFormatter);
         $numberFormatter->format(12000.34)->willReturn('12,000.34');
+        $numberFormatter->setAttribute(Argument::any(), Argument::any())->willReturn(null);
         $this
             ->present(['data' => 12000.34, 'unit' => 'KILOGRAM'])
             ->shouldReturn('12,000.34 KILOGRAM');
@@ -35,6 +37,7 @@ class MetricPresenterSpec extends ObjectBehavior
     ) {
         $numberFactory->create(['locale' => 'fr_FR'])->willReturn($numberFormatter);
         $numberFormatter->format(12000.34)->willReturn('12 000,34');
+        $numberFormatter->setAttribute(Argument::any(), Argument::any())->willReturn(null);
         $this
             ->present(['data' => 12000.34, 'unit' => 'KILOGRAM'], ['locale' => 'fr_FR'])
             ->shouldReturn('12 000,34 KILOGRAM');
@@ -47,6 +50,7 @@ class MetricPresenterSpec extends ObjectBehavior
         $numberFactory->create(['disable_grouping_separator' => true])->willReturn($numberFormatter);
         $numberFormatter->setSymbol(\NumberFormatter::GROUPING_SEPARATOR_SYMBOL, '')->willReturn(null);
         $numberFormatter->format(12000.34)->willReturn('12000.34');
+        $numberFormatter->setAttribute(Argument::any(), Argument::any())->willReturn(null);
         $this
             ->present(['data' => 12000.34, 'unit' => 'KILOGRAM'], ['disable_grouping_separator' => true])
             ->shouldReturn('12000.34 KILOGRAM');
