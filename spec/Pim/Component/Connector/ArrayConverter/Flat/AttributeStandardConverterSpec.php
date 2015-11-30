b55ed2e33843dc6c3eb7638c5795b5a0bd53cdc8
Yohan BLAIN <yohan.blain@gmail.com>
2015-11-30 15:32:08 +0100

    PIM-5235: Fix empty reference data name after attributes import

--- spec/Pim/Component/Connector/ArrayConverter/Flat/AttributeStandardConverterSpec.php
+++ spec/Pim/Component/Connector/ArrayConverter/Flat/AttributeStandardConverterSpec.php
@@ -27,26 +27,66 @@ class AttributeStandardConverterSpec extends ObjectBehavior
             'allowed_extensions'     => '',
             'metric_family'          => '',
             'default_metric_unit'    => '',
+            'reference_data_name'    => 'color',
+            'localizable'            => '0',
+            'scopable'               => '0',
+        ];
+
+        $result = [
+            'labels' => [
+                'de_DE' => 'SKU',
+                'en_US' => 'SKU',
+                'fr_FR' => 'SKU',
+            ],
+            'attributeType'          => 'pim_catalog_identifier',
+            'code'                   => 'sku',
+            'group'                  => 'marketing',
+            'unique'                 => true,
+            'useable_as_grid_filter' => true,
+            'allowed_extensions'     => '',
+            'metric_family'          => '',
+            'default_metric_unit'    => '',
+            'reference_data_name'    => 'color',
+            'localizable'            => false,
+            'scopable'               => false,
+        ];
+
+        $this->convert($item)->shouldReturn($result);
+    }
+
+    function it_does_not_convert_empty_reference_data_name()
+    {
+        $item = [
+            'type'                   => 'pim_catalog_identifier',
+            'code'                   => 'sku',
+            'label-de_DE'            => 'SKU',
+            'label-en_US'            => 'SKU',
+            'label-fr_FR'            => 'SKU',
+            'group'                  => 'marketing',
+            'unique'                 => '1',
+            'useable_as_grid_filter' => '1',
+            'allowed_extensions'     => '',
+            'metric_family'          => '',
+            'default_metric_unit'    => '',
             'reference_data_name'    => '',
             'localizable'            => '0',
             'scopable'               => '0',
         ];
 
         $result = [
-            'labels'                 => [
+            'labels' => [
                 'de_DE' => 'SKU',
                 'en_US' => 'SKU',
                 'fr_FR' => 'SKU',
             ],
-            'attributeType'          => "pim_catalog_identifier",
-            'code'                   => "sku",
-            'group'                  => "marketing",
+            'attributeType'          => 'pim_catalog_identifier',
+            'code'                   => 'sku',
+            'group'                  => 'marketing',
             'unique'                 => true,
             'useable_as_grid_filter' => true,
-            'allowed_extensions'     => "",
-            'metric_family'          => "",
-            'default_metric_unit'    => "",
-            'reference_data_name'    => "",
+            'allowed_extensions'     => '',
+            'metric_family'          => '',
+            'default_metric_unit'    => '',
             'localizable'            => false,
             'scopable'               => false,
         ];
