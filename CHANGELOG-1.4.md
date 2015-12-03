fecfaa070b544a30e87c24844f896cbf6fd4db24
Clement Gautier <clement.gautier@akeneo.com>
2015-12-03 16:08:46 +0100

    Preparing version 1.4.12

--- CHANGELOG-1.4.md
+++ CHANGELOG-1.4.md
@@ -1,4 +1,4 @@
-# 1.4.x
+# 1.4.12 (2015-12-03)
 
 ## Bug fixes
 - PIM-5136: Fixed completeness of published products
--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.11';
+    const VERSION = '1.4.12';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Samara';
