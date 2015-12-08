d9f4348ae955f3781114e35f1d25ef994222bf2d
Nicolas Dupont <nicolas@akeneo.com>
2015-12-08 22:34:13 +0100

    [DX] add imutability notice

--- src/Akeneo/Component/Versioning/Model/VersionInterface.php
+++ src/Akeneo/Component/Versioning/Model/VersionInterface.php
@@ -8,6 +8,8 @@ namespace Akeneo\Component\Versioning\Model;
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * TODO: use constructor and remove setters to make it immutable
  */
 interface VersionInterface
 {
