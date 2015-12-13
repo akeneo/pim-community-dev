67c938350a8fb9cc12785a17f3c5c69bc31ffd2a
Nicolas Dupont <nicolas@akeneo.com>
2015-12-13 20:07:41 +0100

    [DX] Replace JobFactory php unit test by phpspec

---
+++ spec/Akeneo/Bundle/BatchBundle/Job/JobFactorySpec.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace spec\Akeneo\Bundle\BatchBundle\Job;
+
+use Akeneo\Component\Batch\Job\JobRepositoryInterface;
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class JobFactorySpec extends ObjectBehavior
+{
+    const TESTED_CLASS = 'Akeneo\Component\Batch\Job\Job';
+
+    function let(EventDispatcherInterface $dispatcher, JobRepositoryInterface $repository)
+    {
+        $this->beConstructedWith($dispatcher, $repository);
+    }
+
+    function it_creates_job()
+    {
+        $this->createJob('myJobTitle')->shouldReturnAnInstanceOf(self::TESTED_CLASS);
+    }
+}
