f93aa2ef6c88ee7795588c14924bb182e9b0e533
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2015-12-23 15:18:51 +0100

    PIM-5213: normalize attribute group for enrich

--- spec/Pim/Bundle/EnrichBundle/Normalizer/AttributeNormalizerSpec.php
+++ spec/Pim/Bundle/EnrichBundle/Normalizer/AttributeNormalizerSpec.php
@@ -4,19 +4,27 @@ namespace spec\Pim\Bundle\EnrichBundle\Normalizer;
 
 use PhpSpec\ObjectBehavior;
 use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
+use Pim\Bundle\CatalogBundle\Model\GroupInterface;
 use Pim\Bundle\EnrichBundle\Provider\Field\FieldProviderInterface;
 use Pim\Bundle\EnrichBundle\Provider\EmptyValue\EmptyValueProviderInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AttributeNormalizerSpec extends ObjectBehavior
 {
-    public function let(NormalizerInterface $normalizer, FieldProviderInterface $fieldProvider, EmptyValueProviderInterface $emptyValueProvider)
-    {
+    public function let(
+        NormalizerInterface $normalizer,
+        FieldProviderInterface $fieldProvider,
+        EmptyValueProviderInterface $emptyValueProvider
+    ) {
         $this->beConstructedWith($normalizer, $fieldProvider, $emptyValueProvider);
     }
 
-    public function it_adds_the_attribute_id_to_the_normalized_attribute($normalizer, $fieldProvider, $emptyValueProvider, AttributeInterface $price)
-    {
+    function it_adds_the_attribute_id_to_the_normalized_attribute(
+        $normalizer,
+        $fieldProvider,
+        $emptyValueProvider,
+        AttributeInterface $price
+    ) {
         $normalizer->normalize($price, 'json', [])->willReturn(['code' => 'price']);
         $price->getId()->willReturn(12);
         $price->isWysiwygEnabled()->willReturn(false);
@@ -37,9 +45,11 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $price->getDefaultMetricUnit()->willReturn('');
         $price->getMaxFileSize()->willReturn('');
         $price->getSortOrder()->willReturn(2);
+        $price->getGroup()->willReturn(null);
 
         $fieldProvider->getField($price)->willReturn('akeneo-text-field');
         $emptyValueProvider->getEmptyValue($price)->willReturn([]);
+
 
         $this->normalize($price, 'internal_api', [])->shouldReturn(
             [
@@ -63,12 +73,18 @@ class AttributeNormalizerSpec extends ObjectBehavior
                 'default_metric_unit'   => '',
                 'max_file_size'         => '',
                 'sort_order'            => 2,
+                'group'                 => null,
             ]
         );
     }
 
-    public function it_adds_the_attribute_empty_value_to_the_normalized_attribute($normalizer, $fieldProvider, $emptyValueProvider, AttributeInterface $attribute)
-    {
+    function it_adds_the_attribute_empty_value_to_the_normalized_attribute(
+        $normalizer,
+        $fieldProvider,
+        $emptyValueProvider,
+        AttributeInterface $attribute,
+        GroupInterface $group
+    ) {
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'text']);
         $attribute->getId()->willReturn(12);
         $attribute->isWysiwygEnabled()->willReturn(true);
@@ -89,116 +105,129 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $attribute->getDefaultMetricUnit()->willReturn('');
         $attribute->getMaxFileSize()->willReturn('');
         $attribute->getSortOrder()->willReturn(2);
+        $attribute->getGroup()->willReturn(null);
 
         $fieldProvider->getField($attribute)->willReturn('akeneo-text-field');
         $emptyValueProvider->getEmptyValue($attribute)->willReturn([]);
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'text',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => true,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => '',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'text',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => true,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => '',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => null,
+            ]
+        );
 
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'boolean']);
         $attribute->getId()->willReturn(12);
         $attribute->isWysiwygEnabled()->willReturn(true);
         $attribute->getAttributeType()->willReturn('pim_catalog_boolean');
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'boolean',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => true,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => '',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'boolean',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => true,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => '',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => null,
+            ]
+        );
 
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'collection']);
         $attribute->getId()->willReturn(12);
         $attribute->isWysiwygEnabled()->willReturn(false);
         $attribute->getAttributeType()->willReturn('pim_catalog_attribute_collection');
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'collection',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => false,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => '',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'collection',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => false,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => '',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => null,
+            ]
+        );
 
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'collection']);
         $attribute->getId()->willReturn(12);
         $attribute->isWysiwygEnabled()->willReturn(false);
         $attribute->getAttributeType()->willReturn('pim_catalog_multiselect');
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'collection',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => false,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => '',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'collection',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => false,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => '',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => null,
+            ]
+        );
 
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'metric']);
         $attribute->getId()->willReturn(12);
@@ -206,59 +235,67 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $attribute->getAttributeType()->willReturn('pim_catalog_metric');
         $attribute->getDefaultMetricUnit()->willReturn('kg');
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'metric',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => false,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => 'kg',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'metric',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => false,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => 'kg',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => null,
+            ]
+        );
 
         $normalizer->normalize($attribute, 'json', [])->willReturn(['code' => 'default']);
         $attribute->getId()->willReturn(12);
         $attribute->isWysiwygEnabled()->willReturn(false);
         $attribute->getAttributeType()->willReturn('unknown');
+        $attribute->getGroup()->willReturn($group);
+        $normalizer->normalize($group, 'json', [])->willReturn(['code' => 'the_group_is_normalized']);
 
-        $this->normalize($attribute, 'internal_api', [])->shouldReturn([
-            'code'                  => 'default',
-            'id'                    => 12,
-            'wysiwyg_enabled'       => false,
-            'empty_value'           => [],
-            'field_type'            => 'akeneo-text-field',
-            'is_locale_specific'    => 0,
-            'locale_specific_codes' => [],
-            'max_characters'        => '2048',
-            'validation_rule'       => '',
-            'validation_regexp'     => '',
-            'number_min'            => '',
-            'number_max'            => '',
-            'decimals_allowed'      => '',
-            'negative_allowed'      => '',
-            'date_min'              => '',
-            'date_max'              => '',
-            'metric_family'         => '',
-            'default_metric_unit'   => 'kg',
-            'max_file_size'         => '',
-            'sort_order'            => 2,
-        ]);
+        $this->normalize($attribute, 'internal_api', [])->shouldReturn(
+            [
+                'code'                  => 'default',
+                'id'                    => 12,
+                'wysiwyg_enabled'       => false,
+                'empty_value'           => [],
+                'field_type'            => 'akeneo-text-field',
+                'is_locale_specific'    => 0,
+                'locale_specific_codes' => [],
+                'max_characters'        => '2048',
+                'validation_rule'       => '',
+                'validation_regexp'     => '',
+                'number_min'            => '',
+                'number_max'            => '',
+                'decimals_allowed'      => '',
+                'negative_allowed'      => '',
+                'date_min'              => '',
+                'date_max'              => '',
+                'metric_family'         => '',
+                'default_metric_unit'   => 'kg',
+                'max_file_size'         => '',
+                'sort_order'            => 2,
+                'group'                 => ['code' => 'the_group_is_normalized'],
+            ]
+        );
     }
 
-    public function it_supports_attributes_and_internal_api(AttributeInterface $price)
+    function it_supports_attributes_and_internal_api(AttributeInterface $price)
     {
         $this->supportsNormalization($price, 'internal_api')->shouldReturn(true);
         $this->supportsNormalization($price, 'json')->shouldReturn(false);
