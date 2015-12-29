2910ce314ecb0c94348efdef2a4fe762ca07c86e
Julien Sanchez <julien@akeneo.com>
2015-12-29 10:56:34 +0100

    Preparing version 1.3.35

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.34';
+    const VERSION = '1.3.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Force';
