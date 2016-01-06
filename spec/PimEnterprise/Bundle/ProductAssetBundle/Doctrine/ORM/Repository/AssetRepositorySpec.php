5c5273ed5d94ba81bfdbee93c8b4de69c41b3d78
Nicolas Dupont <nicolas@akeneo.com>
2016-01-06 23:49:16 +0100

    [CI] Fix AssetRepositorySpec

--- spec/PimEnterprise/Bundle/ProductAssetBundle/Doctrine/ORM/Repository/AssetRepositorySpec.php
+++ spec/PimEnterprise/Bundle/ProductAssetBundle/Doctrine/ORM/Repository/AssetRepositorySpec.php
@@ -94,7 +94,7 @@ class AssetRepositorySpec extends ObjectBehavior
 
         $qb->select('asset')->willReturn($qb);
         $qb->from('PimEnterprise\Component\ProductAsset\Model\Asset', 'asset')->willReturn($qb);
-        $qb->groupBy('asset.id')->willReturn($qb);
+        $qb->addGroupBy('asset.id')->willReturn($qb);
 
         $this->createAssetDatagridQueryBuilder([]);
     }
