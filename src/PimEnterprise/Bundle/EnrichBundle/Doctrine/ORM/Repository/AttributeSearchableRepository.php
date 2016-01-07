26151121e469355773f2f2e4e734e2df127dfef6
Olivier Soulet <olivier.soulet@akeneo.com>
2016-01-07 16:12:20 +0100

    PIM-5213: add option resolver

--- src/PimEnterprise/Bundle/EnrichBundle/Doctrine/ORM/Repository/AttributeSearchableRepository.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Doctrine/ORM/Repository/AttributeSearchableRepository.php
@@ -5,6 +5,7 @@ namespace PimEnterprise\Bundle\EnrichBundle\Doctrine\ORM\Repository;
 use Doctrine\ORM\QueryBuilder;
 use Pim\Bundle\CatalogBundle\Model\AttributeInterface;
 use Pim\Bundle\EnrichBundle\Doctrine\ORM\Repository\AttributeSearchableRepository as BaseAttributeSearchableRepository;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Attribute searchable repository
@@ -16,25 +17,28 @@ class AttributeSearchableRepository extends BaseAttributeSearchableRepository
     /**
      * {@inheritdoc}
      *
-     * @return AttributeInterface[]
+     * @return QueryBuilder
      */
-    public function findBySearch($search = null, array $options = [])
+    public function findBySearchQb($search = null, array $options = [])
     {
-        $qb = $this->findBySearchQb($search, $options);
+        $qb = parent::findBySearchQb($search, $options);
+        $options = $this->resolveOptions($options);
+
+        $qb->leftJoin(
+            'PimEnterprise\Bundle\SecurityBundle\Entity\AttributeGroupAccess',
+            'aga',
+            'WITH',
+            'ag.id = aga.attributeGroup'
+        );
+        $qb->groupBy('a.id');
+        $qb->andWhere('aga.userGroup IN (:userGroupsIds)')
+            ->setParameter('userGroupsIds', $options['user_groups_ids']);
+        $qb->andWhere('aga.viewAttributes = 1');
 
         if ($options['editable']) {
-            $qb->leftJoin(
-                'PimEnterprise\Bundle\SecurityBundle\Entity\AttributeGroupAccess',
-                'aga',
-                'WITH',
-                'ag.id = aga.attributeGroup'
-            );
-            $qb->groupBy('a.id');
-            $qb->andWhere('aga.userGroup IN (:userGroupsIds)')
-                ->setParameter('userGroupsIds', $options['user_groups_ids']);
             $qb->andWhere('aga.editAttributes = 1');
         }
 
-        return $qb->getQuery()->getResult();
+        return $qb;
     }
 }
