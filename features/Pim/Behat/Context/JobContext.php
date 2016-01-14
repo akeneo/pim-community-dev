1eca8e41e958c43534558bb994fb8d8c89001948
Olivier Soulet <olivier.soulet@akeneo.com>
2016-01-14 14:46:48 +0100

    Fix missing import after merge

--- features/Pim/Behat/Context/JobContext.php
+++ features/Pim/Behat/Context/JobContext.php
@@ -2,7 +2,7 @@
 
 namespace Pim\Behat\Context;
 
-use Akeneo\Bundle\BatchBundle\Entity\JobInstance;
+use Akeneo\Component\Batch\Model\JobInstance;
 use Behat\Behat\Context\Step;
 use Behat\Gherkin\Node\TableNode;
 
