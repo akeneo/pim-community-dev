3ff7d4e8878ca8274ca6cad1e120b807c626be09
Olivier Soulet <olivier.soulet@akeneo.com>
2016-01-26 13:55:02 +0100

    PIM-5376: change version and update changelog

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,10 +19,10 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.16';
+    const VERSION = '1.5.0-ALPHA1';
 
     /** @staticvar string */
-    const VERSION_CODENAME = 'Samara';
+    const VERSION_CODENAME = '';
 
     /** @staticvar string */
     const EDITION = 'EE';
