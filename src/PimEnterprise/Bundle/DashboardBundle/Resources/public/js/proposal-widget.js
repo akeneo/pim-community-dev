1f7f97b6d524305ec3bf089eb41d818d23931e37
Olivier Soulet <olivier.soulet@akeneo.com>
2016-02-03 16:16:06 +0100

    PIM-5415: extract html to a proper template

--- src/PimEnterprise/Bundle/DashboardBundle/Resources/public/js/proposal-widget.js
+++ src/PimEnterprise/Bundle/DashboardBundle/Resources/public/js/proposal-widget.js
@@ -1,6 +1,6 @@
 define(
-    ['jquery', 'underscore', 'routing', 'oro/navigation', 'pim/dashboard/abstract-widget'],
-    function ($, _, Routing, Navigation, AbstractWidget) {
+    ['jquery', 'underscore', 'routing', 'oro/navigation', 'pim/dashboard/abstract-widget', 'text!pimee/templates/dashboard/proposal-widget'],
+    function ($, _, Routing, Navigation, AbstractWidget, widgetTemplate) {
         'use strict';
 
         return AbstractWidget.extend({
@@ -16,45 +16,7 @@ define(
 
             $viewAllLink: null,
 
-            template: _.template(
-                [
-                    '<% if (!_.isEmpty(data)) { %>',
-                        '<thead>',
-                            '<tr>',
-                                '<th class="center"><%= _.__("pimee_dashboard.widget.product_drafts.date") %></th>',
-                                '<th class="center"><%= _.__("pimee_dashboard.widget.product_drafts.author") %></th>',
-                                '<th class="center"><%= _.__("pimee_dashboard.widget.product_drafts.product") %></th>',
-                                '<th></th>',
-                            '</tr>',
-                        '</thead>',
-                        '<tbody>',
-                            '<% _.each(data, function (proposal) { %>',
-                                '<tr>',
-                                    '<td>',
-                                        '<%= proposal.createdAt %>',
-                                    '</td>',
-                                    '<td><%= proposal.author %></td>',
-                                    '<td>',
-                                        '<a href="javascript:void(0);" data-id="<%= proposal.productId %>">',
-                                            '<%= proposal.productLabel %>',
-                                        '</a>',
-                                    '</td>',
-                                    '<td>',
-                                        '<a class="btn btn-mini" href="javascript:void(0);" ',
-                                            'data-author="<%= proposal.author %>" data-proposalid="<%= proposal.id %>">',
-                                            '<%= _.__("pimee_dashboard.widget.product_drafts.review") %>',
-                                        '</a>',
-                                    '</td>',
-                                '</tr>',
-                            '<% }); %>',
-                        '</tbody>',
-                    '<% } else if (options.contentLoaded) {%>',
-                        '<span class="label text-center buffer-small-top buffer-small-bottom fullwidth">',
-                            '<%= _.__("pimee_dashboard.widget.product_drafts.empty") %>',
-                        '</span>',
-                    '<% } %>'
-                ].join('')
-            ),
+            template: _.template(widgetTemplate),
 
             viewAllLinkTemplate: _.template(
                 [
