a7318260dad6428c7cb03dcac00dd1f8861d7c17
Romain Monceau <fitn@users.noreply.github.com>
2016-02-08 17:11:38 +0100

    Update pqb to use cached method in filter registry

--- src/Pim/Bundle/CatalogBundle/Query/ProductQueryBuilder.php
+++ src/Pim/Bundle/CatalogBundle/Query/ProductQueryBuilder.php
@@ -105,13 +105,7 @@ class ProductQueryBuilder implements ProductQueryBuilderInterface
      */
     public function addFilter($field, $operator, $value, array $context = [])
     {
-        $attribute = $this->attributeRepository->findOneBy(['code' => FieldFilterHelper::getCode($field)]);
-
-        if ($attribute !== null) {
-            $filter = $this->filterRegistry->getAttributeFilter($attribute);
-        } else {
-            $filter = $this->filterRegistry->getFieldFilter($field);
-        }
+        $filter = $this->filterRegistry->getFilter($field);
 
         if ($filter === null) {
             throw new \LogicException(
