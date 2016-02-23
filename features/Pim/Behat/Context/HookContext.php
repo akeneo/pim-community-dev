5acc4586b778d592405c5b0d83ff44cc0b84500f
Julien Sanchez <sumbobyboys@gmail.com>
2016-02-23 21:28:34 +0100

    Logout before a scenario execution

--- features/Pim/Behat/Context/HookContext.php
+++ features/Pim/Behat/Context/HookContext.php
@@ -57,6 +57,8 @@ class HookContext extends PimContext
      */
     public function purgeDatabase()
     {
+        $this->getSession()->visit($this->locatePath('/user/logout'));
+
         $excludedTablesFile = __DIR__ . '/' . $this->excludedTablesFile;
         if (file_exists($excludedTablesFile)) {
             $parser         = new Parser();
