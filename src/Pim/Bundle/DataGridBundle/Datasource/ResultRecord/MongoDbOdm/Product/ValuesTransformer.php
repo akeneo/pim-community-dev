df50517596dd1d0dbac3c8fe0f15b4096ab3c979
Adrien Pétremann <petremann.adrien@gmail.com>
2016-03-03 17:03:56 +0100

    PIM-5532: Fix localizable media on product grid

--- src/Pim/Bundle/DataGridBundle/Datasource/ResultRecord/MongoDbOdm/Product/ValuesTransformer.php
+++ src/Pim/Bundle/DataGridBundle/Datasource/ResultRecord/MongoDbOdm/Product/ValuesTransformer.php
@@ -3,6 +3,7 @@
 namespace Pim\Bundle\DataGridBundle\Datasource\ResultRecord\MongoDbOdm\Product;
 
 use Pim\Bundle\CatalogBundle\AttributeType\AttributeTypes;
+use Pim\Bundle\CatalogBundle\Doctrine\MongoDBODM\ProductQueryUtility;
 use Pim\Bundle\ReferenceDataBundle\DataGrid\Datasource\ResultRecord\MongoDbOdm\Product\ReferenceDataTransformer;
 
 /**
@@ -41,6 +42,7 @@ class ValuesTransformer
                     $result[$attributeCode] = $optionsTransformer->transform($result, $attribute, $locale, $scope);
                     $result[$attributeCode] = $refDataTransformer->transform($result, $attribute, $locale, $scope);
                     $result[$attributeCode] = $this->prepareDateData($result, $attribute);
+                    $result[$attributeCode] = $this->prepareMediaData($result, $attribute, $locale, $scope);
                 }
             }
 
@@ -74,16 +76,25 @@ class ValuesTransformer
     /**
      * @param array $result
      * @param array $attribute
+     * @param string $locale
+     * @param string $scope
      *
      * @return array
      */
-    protected function prepareMediaData(array $result, array $attribute)
+    protected function prepareMediaData(array $result, array $attribute, $locale, $scope)
     {
         $attributeCode = $attribute['code'];
         $backendType = $attribute['backendType'];
         $value = $result[$attributeCode];
         if (AttributeTypes::IMAGE === $attribute['attributeType'] && isset($value[$backendType])) {
             $normalizedData = $result['normalizedData'];
+            $attributeCode = ProductQueryUtility::getNormalizedValueField(
+                $attributeCode,
+                $attribute['localizable'],
+                $attribute['scopable'],
+                $locale,
+                $scope
+            );
             $value[$backendType] = $normalizedData[$attributeCode];
         }
 
