cc96f3a41db915f472ccb0bcdc5d6214a7932857
Adrien Pétremann <petremann.adrien@gmail.com>
2016-03-03 10:52:23 +0100

    Preparing version 1.3.39

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.38';
+    const VERSION = '1.3.39';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Force';
