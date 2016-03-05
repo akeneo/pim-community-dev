935886404641a3b26516626a4bfd7f61d0114aef
Nicolas Dupont <nicolas@akeneo.com>
2016-03-05 17:19:20 +0100

    Fix 1.5 merge

--- spec/Pim/Component/Connector/ArrayConverter/Flat/ProductStandardConverterSpec.php
+++ spec/Pim/Component/Connector/ArrayConverter/Flat/ProductStandardConverterSpec.php
@@ -27,7 +27,6 @@ class ProductStandardConverterSpec extends ObjectBehavior
         FieldConverter $fieldConverter,
         ColumnsMerger $columnsMerger,
         ColumnsMapper $columnsMapper,
-        LocalizerRegistryInterface $localizerRegistry,
         FieldsRequirementValidator $validator
     ) {
         $this->beConstructedWith(
@@ -38,7 +37,6 @@ class ProductStandardConverterSpec extends ObjectBehavior
             $fieldConverter,
             $columnsMerger,
             $columnsMapper,
-            $localizerRegistry,
             $validator
         );
     }
