b1eacb6da31cb432ea55be9561a76c1a4fec14ce
Clement Gautier <clement.gautier@akeneo.com>
2016-03-08 09:30:02 +0100

    Fixed regression during merge process

--- src/Pim/Bundle/BaseConnectorBundle/Archiver/FileWriterArchiver.php
+++ src/Pim/Bundle/BaseConnectorBundle/Archiver/FileWriterArchiver.php
@@ -61,11 +61,7 @@ class FileWriterArchiver extends AbstractFilesystemArchiver
      */
     protected function isUsableWriter(ItemWriterInterface $writer)
     {
-        if ($writer instanceof ArchivableWriterInterface && count($writer->getWrittenFiles()) > 1) {
-            return false;
-        }
-
-        return ($writer instanceof FileWriter || $writer instanceof AbstractFileWriter) && is_file($writer->getPath());
+        return $writer instanceof FileWriter && is_file($writer->getPath());
     }
 
     /**
