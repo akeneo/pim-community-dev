b482721c6d4246d1eb29517a5a4b2845d22a3c31
Olivier Soulet <olivier.soulet@akeneo.com>
2016-03-16 15:43:51 +0100

    Preparing version 1.3.40

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.3.39';
+    const VERSION = '1.3.40';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Force';
