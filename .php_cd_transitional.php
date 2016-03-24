52c2eb0a9c4f86a46ff9b85859f5536dd0770ec6
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-03-24 14:50:02 +0100

    TIP-221: sanitize coupling rules

--- .php_cd_transitional.php
+++ .php_cd_transitional.php
@@ -35,23 +35,18 @@ $cAkeneoRules = [
     new Rule('Akeneo\Component\Buffer', $cDeps, RuleInterface::TYPE_ONLY),
     new Rule(
         'Akeneo\Component\Classification',
-        array_merge($cDeps, ['Gedmo\Tree\RepositoryInterface']),
-        RuleInterface::TYPE_ONLY,
-        'The component classification should depends on nothing particular except Gedmo\Tree that is our base tree library.'
+        array_merge($cDeps, [
+            'Gedmo\Tree\RepositoryInterface', // used as base tree library
+        ]),
+        RuleInterface::TYPE_ONLY
     ),
-    new Rule('Akeneo\Component\Console', $cDeps, RuleInterface::TYPE_ONLY),
+    new Rule('Akeneo\Component\Console', $cDeps, RuleInterface::TYPE_ONLY, 'have no real existence, should be merged into Batch'),
     new Rule(
         'Akeneo\Component\FileStorage',
-        array_merge(
-            $cDeps,
-            [
-                'League\Flysystem',
-                'Symfony\Component\HttpFoundation', // will be removed when we don't use Symfony uploaded files anymore
-            ]
-        ),
-        RuleInterface::TYPE_ONLY,
-        'The component file storage should depends on nothing particular except League\FlySystem that is our storage library.' .
-        'For the moment we authorize HttpFoundation as we need it for file uploads :(.'
+        array_merge($cDeps, [
+            'League\Flysystem',  // used as base file storage system
+        ]),
+        RuleInterface::TYPE_ONLY
     ),
     new Rule('Akeneo\Component\Localization', $cDeps, RuleInterface::TYPE_ONLY),
     new Rule('Akeneo\Component\StorageUtils', $cDeps, RuleInterface::TYPE_ONLY),
@@ -62,14 +57,13 @@ $cPimRules = [
     new Rule(
         'Pim\Component\Catalog',
         array_merge($cDeps, [
-            'Akeneo\Component\Localization',
-            'Akeneo\Component\FileStorage',
-            'Akeneo\Component\Classification',
-            'Akeneo\Component\Versioning',
-            'Pim\Component\ReferenceData', //maybe should be merged inside catalog
+            'Akeneo\Component\Localization',   // to localize product's data
+            'Akeneo\Component\FileStorage',    // for product categories
+            'Akeneo\Component\Classification', // to handle product's media
+            'Akeneo\Component\Versioning',     // for the history of all models
         ]),
         RuleInterface::TYPE_ONLY
-    )
+    ),
 ];
 
 $rules  = array_merge($cAkeneoRules, $cPimRules);
