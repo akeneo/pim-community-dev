cae26bc84b6b3929be5b28b24c6adc1901ea19b7
Alexandr Jeliuc <a2xchip@gmail.com>
2016-04-02 01:10:38 +0300

    AttributeNormalizer complimented to normalize all available attribute fields and options #4220
    New attribute properties normalizations added:
    
    * available_locales
    * max_characters
    * validation_rule
    * validation_regexp
    * date_min
    * date_max
    * max_file_size
    * minimum_input_length

--- spec/Pim/Component/Connector/Normalizer/Flat/AttributeNormalizerSpec.php
+++ spec/Pim/Component/Connector/Normalizer/Flat/AttributeNormalizerSpec.php
@@ -37,6 +37,20 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $attribute->isNegativeAllowed()->willReturn(false);
         $attribute->getNumberMin()->willReturn('');
         $attribute->getNumberMax()->willReturn('');
+
+        $attribute->getLocaleSpecificCodes()->willReturn(['en_US', 'fr_FR']);
+        $attribute->getMaxCharacters()->willReturn(null);
+        $attribute->getValidationRule()->willReturn(null);
+        $attribute->getValidationRegexp()->willReturn(null);
+        $attribute->isWysiwygEnabled()->willReturn(false);
+        $attribute->getNumberMin()->willReturn('');
+        $attribute->getNumberMax()->willReturn('');
+        $attribute->isDecimalsAllowed()->willReturn(false);
+        $attribute->isNegativeAllowed()->willReturn(false);
+        $attribute->getDateMin()->willReturn(null);
+        $attribute->getDateMax()->willReturn(null);
+        $attribute->getMaxFileSize()->willReturn(null);
+        $attribute->getMinimumInputLength()->willReturn(null);
     }
 
     function it_is_initializable()
@@ -69,11 +83,19 @@ class AttributeNormalizerSpec extends ObjectBehavior
                 'metric_family'          => 'Length',
                 'default_metric_unit'    => 'Centimenter',
                 'reference_data_name'    => 'color',
+                'available_locales'      => 'en_US,fr_FR',
+                'max_characters'         => '',
+                'validation_rule'        => '',
+                'validation_regexp'      => '',
                 'wysiwyg_enabled'        => '',
                 'number_min'             => '',
                 'number_max'             => '',
                 'decimals_allowed'       => '',
                 'negative_allowed'       => '',
+                'date_min'               => '',
+                'date_max'               => '',
+                'max_file_size'          => '',
+                'minimum_input_length'   => '',
                 'localizable'            => 1,
                 'scopable'               => 0
             ]
@@ -120,25 +142,26 @@ class AttributeNormalizerSpec extends ObjectBehavior
                 'metric_family'          => 'Length',
                 'default_metric_unit'    => 'Centimenter',
                 'reference_data_name'    => 'color',
+                'available_locales'      => 'en_US,fr_FR',
+                'max_characters'         => '',
+                'validation_rule'        => '',
+                'validation_regexp'      => '',
                 'wysiwyg_enabled'        => '',
                 'number_min'             => '1',
                 'number_max'             => '10',
                 'decimals_allowed'       => '',
                 'negative_allowed'       => '',
-                'available_locales'      => 'en_US,fr_FR',
+                'date_min'               => '',
+                'date_max'               => '',
+                'max_file_size'          => '0',
+                'minimum_input_length'   => '',
                 'localizable'            => true,
                 'scope'                  => 'Channel',
                 'options'                => 'Code:size,en_US:big,fr_FR:grand',
                 'sort_order'             => 1,
                 'required'               => 0,
-                'max_characters'         => '',
-                'validation_rule'        => '',
-                'validation_regexp'      => '',
-                'date_min'               => '',
-                'date_max'               => '',
                 'metric_family'          => 'Length',
                 'default_metric_unit'    => 'Centimenter',
-                'max_file_size'          => '0'
             ]
         );
     }
