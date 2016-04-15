bc48090e4d72338658cdf5beb9b9b0ff48e5c2ea
skeleton <marie.bochu@akeneo.com>
2016-04-15 11:33:22 +0200

    PIM-5684: get granted categories without getAllChildrenQueryBuilder()

--- src/PimEnterprise/Bundle/EnrichBundle/Doctrine/Counter/GrantedCategoryItemsCounter.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Doctrine/Counter/GrantedCategoryItemsCounter.php
@@ -14,7 +14,6 @@ namespace PimEnterprise\Bundle\EnrichBundle\Doctrine\Counter;
 use Akeneo\Component\Classification\Model\CategoryInterface;
 use Akeneo\Component\Classification\Repository\CategoryRepositoryInterface;
 use Akeneo\Component\Classification\Repository\ItemCategoryRepositoryInterface;
-use Doctrine\ORM\QueryBuilder;
 use Pim\Bundle\EnrichBundle\Doctrine\Counter\CategoryItemsCounter;
 use PimEnterprise\Bundle\SecurityBundle\Entity\Repository\CategoryAccessRepository;
 use PimEnterprise\Component\Security\Attributes;
@@ -28,12 +27,6 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
  */
 class GrantedCategoryItemsCounter extends CategoryItemsCounter
 {
-    /** @var ItemCategoryRepositoryInterface */
-    protected $itemRepository;
-
-    /** @var CategoryRepositoryInterface */
-    protected $categoryRepository;
-
     /** @var CategoryAccessRepository */
     protected $categoryAccessRepo;
 
@@ -45,7 +38,7 @@ class GrantedCategoryItemsCounter extends CategoryItemsCounter
 
     /**
      * @param ItemCategoryRepositoryInterface $itemRepository       Item repository
-     * @param CategoryRepositoryInterface     $categoryRepo         Category repository
+     * @param CategoryRepositoryInterface     $categoryRepository   Category repository
      * @param CategoryAccessRepository        $categoryAccessRepo   Category Access repository
      * @param AuthorizationCheckerInterface   $authorizationChecker Authorization checker
      * @param TokenStorageInterface           $tokenStorage         Token storage
@@ -57,8 +50,8 @@ class GrantedCategoryItemsCounter extends CategoryItemsCounter
         AuthorizationCheckerInterface $authorizationChecker,
         TokenStorageInterface $tokenStorage
     ) {
-        $this->itemRepository       = $itemRepository;
-        $this->categoryRepository   = $categoryRepository;
+        parent::__construct($itemRepository, $categoryRepository);
+
         $this->categoryAccessRepo   = $categoryAccessRepo;
         $this->authorizationChecker = $authorizationChecker;
         $this->tokenStorage         = $tokenStorage;
@@ -69,45 +62,22 @@ class GrantedCategoryItemsCounter extends CategoryItemsCounter
      *
      * @see getItemsCountInCategory same logic with applying permissions
      */
-    public function getItemsCountInCategory(
-        CategoryInterface $category,
-        $inChildren = false,
-        $inProvided = true
-    ) {
-        if (false === $this->authorizationChecker->isGranted(Attributes::VIEW_ITEMS, $category)) {
+    public function getItemsCountInCategory(CategoryInterface $category, $inChildren = false, $inProvided = true)
+    {
+        if (!$this->authorizationChecker->isGranted(Attributes::VIEW_ITEMS, $category)) {
             return 0;
         }
 
-        $grantedQb = null;
         if ($inChildren) {
-            $categoryQb = $this->categoryRepository->getAllChildrenQueryBuilder($category, $inProvided);
-            $grantedQb = $this->getAllGrantedChildrenQueryBuilder($categoryQb);
+            $categoryIds = $this->categoryAccessRepo->getGrantedChildrenIds(
+                $category,
+                $this->tokenStorage->getToken()->getUser(),
+                Attributes::VIEW_ITEMS
+            );
+        } else {
+            $categoryIds = [$category->getId()];
         }
 
-        return $this->itemRepository->getItemsCountInCategory($category, $grantedQb);
-    }
-
-    /**
-     * Build a new query builder based on children QB to let only granted children
-     *
-     * @param QueryBuilder $childrenQb
-     *
-     * @return QueryBuilder
-     */
-    protected function getAllGrantedChildrenQueryBuilder(QueryBuilder $childrenQb)
-    {
-        $categoryIds = $this->categoryAccessRepo->getGrantedCategoryIdsFromQB(
-            $childrenQb,
-            $this->tokenStorage->getToken()->getUser(),
-            Attributes::VIEW_ITEMS
-        );
-
-        $rootAlias = current($childrenQb->getRootAliases());
-        $grantedQb = $this->categoryRepository->createQueryBuilder($rootAlias);
-        $grantedQb->select($rootAlias.'.id');
-        $grantedQb->where($grantedQb->expr()->in($rootAlias.'.id', ':categoryIds'));
-        $grantedQb->setParameter('categoryIds', $categoryIds);
-
-        return $grantedQb;
+        return $this->itemRepository->getItemsCountInCategory($categoryIds);
     }
 }
