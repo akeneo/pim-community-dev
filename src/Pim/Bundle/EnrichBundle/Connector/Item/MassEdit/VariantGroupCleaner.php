69353f9c7617675f8e3f271319300cb4775c3608
Adrien Pétremann <petremann.adrien@gmail.com>
2016-04-26 16:20:48 +0200

    PIM-5364: Use new clean translation keys for mass edit jobs

--- src/Pim/Bundle/EnrichBundle/Connector/Item/MassEdit/VariantGroupCleaner.php
+++ src/Pim/Bundle/EnrichBundle/Connector/Item/MassEdit/VariantGroupCleaner.php
@@ -258,7 +258,9 @@ class VariantGroupCleaner
                     $stepExecution
                         ->addWarning(
                             'duplicated',
-                            $this->translator->trans('add_to_variant_group.steps.cleaner.warning.description'),
+                            $this->translator->trans(
+                                'pim_enrich.jobs.add_to_variant_group.steps.cleaner.warning.description'
+                            ),
                             [],
                             $product
                         );
