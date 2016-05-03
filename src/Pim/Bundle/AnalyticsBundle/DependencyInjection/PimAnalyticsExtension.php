7ce742aa493735294cea10b2486a6a7d972bc7d0
Damien Carcel <damien.carcel@akeneo.com>
2016-05-03 14:58:02 +0200

    PIM-5781: Remove useless service There is no class for it, and it is never used

--- src/Pim/Bundle/AnalyticsBundle/DependencyInjection/PimAnalyticsExtension.php
+++ src/Pim/Bundle/AnalyticsBundle/DependencyInjection/PimAnalyticsExtension.php
@@ -29,6 +29,5 @@ class PimAnalyticsExtension extends Extension
         $loader->load('controllers.yml');
         $loader->load('data_collectors.yml');
         $loader->load('twig.yml');
-        $loader->load('url_generators.yml');
     }
 }
