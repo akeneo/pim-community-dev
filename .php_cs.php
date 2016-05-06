28a1800a5dddde8ed9c5e49996cf1fc074b6512c
aRn0D <arnaud.langlade@akeneo.com>
2016-05-06 16:05:17 +0200

    TIP-395: php-cs-fixer configuration

--- .php_cs.php
+++ .php_cs.php
@@ -13,9 +13,9 @@ $fixers = require __DIR__ . '/.php_cs-fixers.php';
 if (is_numeric(getenv('TRAVIS_PULL_REQUEST'))) {
     $commitRange = str_replace('...', '..', getenv('TRAVIS_COMMIT_RANGE'));
     printf('Commit range = %s' . PHP_EOL, $commitRange);
-    exec('git diff ' . $commitRange . ' --name-only --diff-filter=AMR | grep -v ^spec/ | grep php$', $diff);
+    exec('git diff ' . $commitRange . ' --name-only --diff-filter=AMR | grep -v Spec.php$ | grep php$', $diff);
 } else {
-    exec('git show --name-only --oneline --pretty="format:" --diff-filter=AMR | grep -v ^spec/ | grep php$', $diff);
+    exec('git show --name-only --oneline --pretty="format:" --diff-filter=AMR | grep -v Spec.php$ | grep php$', $diff);
     $diff = array_filter($diff);
 }
 
