ba5f013d8534f45a07b7b1afc0a8cc5ff32fda3e
Philippe Mossière <philippe.mossiere@akeneo.com>
2016-05-06 14:45:06 +0200

    PIM-5749 : Add default column sorter and product column sorter with spec

---
+++ src/Pim/Component/Connector/spec/Writer/File/ProductColumnSorterSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace spec\Pim\Component\Connector\Writer\File;
+
+use PhpSpec\ObjectBehavior;
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Pim\Component\Connector\ArrayConverter\Flat\Product\FieldSplitter;
+
+class ProductColumnSorterSpec extends ObjectBehavior
+{
+    function let(FieldSplitter $fieldSplitter, IdentifiableObjectRepositoryInterface $productRepository)
+    {
+        $this->beConstructedWith($fieldSplitter, $productRepository, ['label']);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Pim\Component\Connector\Writer\File\DefaultColumnSorter');
+    }
+
+    function it_is_a_sorter()
+    {
+        $this->shouldImplement('Pim\Component\Connector\Writer\File\ColumnSorterInterface');
+    }
+
+    function it_sort_headers_columns($productRepository)
+    {
+        $productRepository->getIdentifierProperties()->willReturn([0 => 'sku']);
+
+        $this->sort([
+            'sku',
+            'code',
+            'label'
+        ])->shouldReturn([
+            'sku',
+            'label',
+            'code'
+        ]);
+    }
+}
