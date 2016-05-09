58d8a79be2f8be25fba1805d8965af466b163d81
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-05-09 15:44:00 +0200

    PIM-5428: Filter on product status during exports

--- src/Pim/Component/Connector/spec/Reader/ProductReaderSpec.php
+++ src/Pim/Component/Connector/spec/Reader/ProductReaderSpec.php
@@ -46,7 +46,7 @@ class ProductReaderSpec extends ObjectBehavior
         $this->getChannel()->shouldReturn('mobile');
     }
 
-    function it_reads_products(
+    function it_reads_enabled_products(
         $pqbFactory,
         $channelRepository,
         $metricConverter,
@@ -97,6 +97,112 @@ class ProductReaderSpec extends ObjectBehavior
         $this->read()->shouldReturn(null);
     }
 
+    function it_reads_disabled_products(
+        $pqbFactory,
+        $channelRepository,
+        $metricConverter,
+        $objectDetacher,
+        $stepExecution,
+        ChannelInterface $channel,
+        CategoryInterface $channelRoot,
+        ProductQueryBuilderInterface $pqb,
+        CursorInterface $cursor,
+        ProductInterface $product1,
+        ProductInterface $product2,
+        ProductInterface $product3
+    ) {
+        $this->setEnabled('disabled');
+
+        $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
+        $channel->getCategory()->willReturn($channelRoot);
+        $channelRoot->getId()->willReturn(42);
+        $channel->getCode()->willReturn('mobile');
+
+        $pqbFactory->create(['default_scope' => 'mobile'])
+            ->shouldBeCalled()
+            ->willReturn($pqb);
+        $pqb->addFilter('enabled', '=', false, [])->shouldBeCalled();
+        $pqb->addFilter('completeness', '=', 100, [])->shouldBeCalled();
+        $pqb->addFilter('categories.id', 'IN CHILDREN', [42], [])->shouldBeCalled();
+        $pqb->execute()
+            ->shouldBeCalled()
+            ->willReturn($cursor);
+
+        $products = [$product1, $product2, $product3];
+        $productsCount = count($products);
+        $cursor->valid()->will(
+            function () use (&$productsCount) {
+                return $productsCount-- > 0;
+            }
+        );
+        $cursor->next()->shouldBeCalled();
+        $cursor->current()->will(new ReturnPromise($products));
+
+        $stepExecution->incrementSummaryInfo('read')->shouldBeCalledTimes(3);
+        $objectDetacher->detach(Argument::any())->shouldBeCalledTimes(3);
+        $metricConverter->convert(Argument::any(), $channel)->shouldBeCalledTimes(3);
+
+        $this->setChannel('mobile');
+        $this->initialize();
+        $this->read()->shouldReturn($product1);
+        $this->read()->shouldReturn($product2);
+        $this->read()->shouldReturn($product3);
+        $this->read()->shouldReturn(null);
+    }
+
+    function it_reads_all_products(
+        $pqbFactory,
+        $channelRepository,
+        $metricConverter,
+        $objectDetacher,
+        $stepExecution,
+        ChannelInterface $channel,
+        CategoryInterface $channelRoot,
+        ProductQueryBuilderInterface $pqb,
+        CursorInterface $cursor,
+        ProductInterface $product1,
+        ProductInterface $product2,
+        ProductInterface $product3
+    ) {
+        $this->setEnabled('all');
+
+        $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
+        $channel->getCategory()->willReturn($channelRoot);
+        $channelRoot->getId()->willReturn(42);
+        $channel->getCode()->willReturn('mobile');
+
+        $pqbFactory->create(['default_scope' => 'mobile'])
+            ->shouldBeCalled()
+            ->willReturn($pqb);
+        $pqb->addFilter('enabled', Argument::cetera())->shouldNotBeCalled();
+        $pqb->addFilter('completeness', '=', 100, [])->shouldBeCalled();
+        $pqb->addFilter('categories.id', 'IN CHILDREN', [42], [])->shouldBeCalled();
+        $pqb->execute()
+            ->shouldBeCalled()
+            ->willReturn($cursor);
+
+        $products = [$product1, $product2, $product3];
+        $productsCount = count($products);
+        $cursor->valid()->will(
+            function () use (&$productsCount) {
+                return $productsCount-- > 0;
+            }
+        );
+        $cursor->next()->shouldBeCalled();
+        $cursor->current()->will(new ReturnPromise($products));
+
+        $stepExecution->incrementSummaryInfo('read')->shouldBeCalledTimes(3);
+        $objectDetacher->detach(Argument::any())->shouldBeCalledTimes(3);
+        $metricConverter->convert(Argument::any(), $channel)->shouldBeCalledTimes(3);
+
+        $this->setChannel('mobile');
+        $this->initialize();
+        $this->read()->shouldReturn($product1);
+        $this->read()->shouldReturn($product2);
+        $this->read()->shouldReturn($product3);
+        $this->read()->shouldReturn(null);
+    }
+
     function it_generates_the_completeness_on_initialization(
         $pqbFactory,
         $channelRepository,
@@ -116,7 +222,7 @@ class ProductReaderSpec extends ObjectBehavior
         $this->initialize();
     }
 
-    function it_exposes_the_channel_field($channelRepository)
+    function it_exposes_the_channel_and_status_fields($channelRepository)
     {
         $channelRepository->getLabelsIndexedByCode()->willReturn(
             [
@@ -139,7 +245,21 @@ class ProductReaderSpec extends ObjectBehavior
                         'label'    => 'pim_connector.export.channel.label',
                         'help'     => 'pim_connector.export.channel.help'
                     ]
-                ]
+                ],
+                'enabled' => [
+                    'type'    => 'choice',
+                    'options' => [
+                        'choices'  => [
+                            'enabled' => 'pim_connector.export.status.choice.enabled',
+                            'disabled' => 'pim_connector.export.status.choice.disabled',
+                            'all' => 'pim_connector.export.status.choice.all'
+                        ],
+                        'required' => true,
+                        'select2'  => true,
+                        'label'    => 'pim_connector.export.status.label',
+                        'help'     => 'pim_connector.export.status.help',
+                    ]
+                ],
             ]
         );
     }
