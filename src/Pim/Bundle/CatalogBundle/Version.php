da5296f56d64b4174f4e5d33982e13275e5de952
Clement Gautier <clement.gautier@akeneo.com>
2016-05-10 14:21:48 +0200

    Preparing version 1.4.24

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.23';
+    const VERSION = '1.4.24';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
