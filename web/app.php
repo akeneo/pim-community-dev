88ce8b843dad5e1769c71e2439a6cb3389407c5e
skeleton <marie.bochu@akeneo.com>
2016-05-10 13:41:51 +0200

    TIP-414: updade configuration

--- web/app.php
+++ web/app.php
@@ -1,24 +1,19 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
-
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- web/app_behat.php
+++ web/app_behat.php
@@ -1,33 +1,19 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-// Use APC for autoloading to improve performance.
-// Use the HOST variable if available to define prefix
-/*
-$prefix = 'pim-behat';
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../app/bootstrap.php.cache';
 
-if (isset($_SERVER['HTTP_HOST'])) {
-    $prefix .= '-'.$_SERVER['HTTP_HOST'];
-}
-$loader = new ApcClassLoader($prefix, $loader);
-$loader->register(true);
-*/
-
-// if env defined outside (by vhost for example)
-// use it
-$env = getenv('BEHAT_ENV');
-
-if ($env == null) {
-    $env = 'behat';
-}
-
-require_once __DIR__.'/../app/AppKernel.php';
-
-$kernel = new AppKernel($env, false);
+$kernel = new AppKernel('behat', false);
 $kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
