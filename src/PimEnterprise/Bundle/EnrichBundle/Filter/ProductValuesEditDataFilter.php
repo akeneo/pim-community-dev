25d39ce13fc91e7f03446bcce5bcd54a91385292
Olivier Soulet <olivier.soulet@akeneo.com>
2016-05-12 11:01:27 +0200

    PIM-5582: Add the is read only option in converter

--- src/PimEnterprise/Bundle/EnrichBundle/Filter/ProductValuesEditDataFilter.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Filter/ProductValuesEditDataFilter.php
@@ -25,71 +25,15 @@ use Pim\Component\Catalog\Repository\LocaleRepositoryInterface;
  */
 class ProductValuesEditDataFilter extends BaseProductValuesEditDataFilter
 {
-     /**
-     * @param ObjectFilterInterface        $objectFilter
-     * @param AttributeRepositoryInterface $attributeRepository
-     * @param LocaleRepositoryInterface    $localeRepository
-     * @param ChannelRepositoryInterface   $channelRepository
-     */
-    public function __construct(
-        ObjectFilterInterface $objectFilter,
-        AttributeRepositoryInterface $attributeRepository,
-        LocaleRepositoryInterface $localeRepository,
-        ChannelRepositoryInterface $channelRepository
-    ) {
-        parent::__construct($objectFilter, $attributeRepository, $localeRepository, $channelRepository);
-    }
-
     /**
-     * Test if a value is accepted or not
-     *
-     * @param AttributeInterface $attribute
-     * @param array              $value
-     *
-     * @return boolean
+     * {@inheritdoc}
      */
     protected function acceptValue(AttributeInterface $attribute, $value, array $options = [])
     {
-        if (null !== $value['locale']) {
-            $locale = $this->getLocale($value['locale']);
-            if (null === $locale) {
-                return false;
-            }
-
-            if (!$attribute->isLocalizable()) {
-                return false;
-            }
-
-            if (!$locale->isActivated()) {
-                return false;
-            }
-
-            if ($attribute->isReadOnly()) {
-                return false;
-            }
-
-            if ($this->objectFilter->filterObject(
-                $this->getLocale($value['locale']),
-                'pim.internal_api.locale.edit',
-                $options
-            )) {
-                return false;
-            }
-
-            if ($attribute->isLocaleSpecific() && !in_array($value['locale'], $attribute->getLocaleSpecificCodes())) {
-                return false;
-            }
+        if ($attribute->isReadOnly()) {
+            return false;
         }
 
-        if (null !== $value['scope']) {
-            if (!$attribute->isScopable()) {
-                return false;
-            }
-            if (null === $this->getChannel($value['scope'])) {
-                return false;
-            }
-        }
-
-        return true;
+        return parent::acceptValue($attribute, $value, $options);
     }
 }
--- src/PimEnterprise/Bundle/EnrichBundle/Normalizer/AttributeNormalizer.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Normalizer/AttributeNormalizer.php
@@ -24,61 +24,10 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 class AttributeNormalizer extends BaseAttributeNormalizer
 {
     /**
-     * @param NormalizerInterface         $normalizer
-     * @param FieldProviderInterface      $fieldProvider
-     * @param EmptyValueProviderInterface $emptyValueProvider
-     */
-    public function __construct(
-        NormalizerInterface $normalizer,
-        FieldProviderInterface $fieldProvider,
-        EmptyValueProviderInterface $emptyValueProvider
-    ) {
-        parent::__construct($normalizer, $fieldProvider, $emptyValueProvider);
-
-        $this->normalizer = $normalizer;
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function normalize($attribute, $format = null, array $context = [])
     {
-        $dateMin = (null === $attribute->getDateMin()) ? '' : $attribute->getDateMin()->format(\DateTime::ISO8601);
-        $dateMax = (null === $attribute->getDateMax()) ? '' : $attribute->getDateMax()->format(\DateTime::ISO8601);
-        $groupCode = (null === $attribute->getGroup()) ? null : $attribute->getGroup()->getCode();
-
-        $normalizedAttribute = $this->normalizer->normalize($attribute, 'json', $context) + [
-                'id'                    => $attribute->getId(),
-                'wysiwyg_enabled'       => $attribute->isWysiwygEnabled(),
-                'empty_value'           => $this->emptyValueProvider->getEmptyValue($attribute),
-                'field_type'            => $this->fieldProvider->getField($attribute),
-                'is_locale_specific'    => (int) $attribute->isLocaleSpecific(),
-                'locale_specific_codes' => $attribute->getLocaleSpecificCodes(),
-                'max_characters'        => $attribute->getMaxCharacters(),
-                'validation_rule'       => $attribute->getValidationRule(),
-                'validation_regexp'     => $attribute->getValidationRegexp(),
-                'number_min'            => $attribute->getNumberMin(),
-                'number_max'            => $attribute->getNumberMax(),
-                'decimals_allowed'      => $attribute->isDecimalsAllowed(),
-                'negative_allowed'      => $attribute->isNegativeAllowed(),
-                'date_min'              => $dateMin,
-                'date_max'              => $dateMax,
-                'metric_family'         => $attribute->getMetricFamily(),
-                'default_metric_unit'   => $attribute->getDefaultMetricUnit(),
-                'max_file_size'         => $attribute->getMaxFileSize(),
-                'sort_order'            => $attribute->getSortOrder(),
-                'group_code'            => $groupCode,
-                'is_read_only'          => $attribute->isReadOnly()
-            ];
-
-        // This normalizer is used in the PEF attributes loading and in the add_attributes widget. The attributes
-        // loading does not need complete group normalization. This has to be cleaned.
-        if (isset($context['include_group']) && $context['include_group'] && null !== $attribute->getGroup()) {
-            $normalizedAttribute['group'] = $this->normalizer->normalize($attribute->getGroup(), 'json', $context);
-        } else {
-            $normalizedAttribute['group'] = null;
-        }
-
-        return $normalizedAttribute;
+        return parent::normalize($attribute, $format, $context) + ['is_read_only' => $attribute->isReadOnly()];
     }
 }
