fe9d09190dc15538e36f9214b840e31ee5e8bf13
Clement Gautier <clement.gautier@akeneo.com>
2016-05-13 14:12:37 +0200

    Preparing version 1.5.3

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.2';
+    const VERSION = '1.5.3';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
