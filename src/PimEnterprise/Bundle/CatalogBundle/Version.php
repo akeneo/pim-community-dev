854d408a3da65b7a1510d72651ba1579cc49c480
Clement Gautier <clement.gautier@akeneo.com>
2016-05-13 14:14:52 +0200

    Preparing version 1.5.3

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.2';
+    const VERSION = '1.5.3';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Sunflower';
