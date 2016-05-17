c190da39406ae3a8af32778e29dea267d9984de7
Nicolas Dupont <nicolas@akeneo.com>
2016-05-17 18:31:03 +0200

    TIP-424: update BatchBundle to support JobParameters

--- src/Akeneo/Bundle/BatchBundle/spec/Launcher/SimpleJobLauncherSpec.php
+++ src/Akeneo/Bundle/BatchBundle/spec/Launcher/SimpleJobLauncherSpec.php
@@ -2,14 +2,20 @@
 
 namespace spec\Akeneo\Bundle\BatchBundle\Launcher;
 
+use Akeneo\Component\Batch\Job\JobParametersFactory;
 use Akeneo\Component\Batch\Job\JobRepositoryInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SimpleJobLauncherSpec extends ObjectBehavior
 {
-    function let(JobRepositoryInterface $jobRepository) {
-        $this->beConstructedWith($jobRepository, '/', 'prod');
+    function let(
+        JobRepositoryInterface $jobRepository,
+        JobParametersFactory $jobParametersFactory,
+        ContainerInterface $container
+    ) {
+        $this->beConstructedWith($jobRepository, $jobParametersFactory, $container, '/', 'prod');
     }
 
     function it_is_a_job_launcher()
