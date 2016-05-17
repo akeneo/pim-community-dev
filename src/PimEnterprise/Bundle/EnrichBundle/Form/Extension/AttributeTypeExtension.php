f0582a0c764aba67f3eb7c05d486c75097e9dbfc
aRn0D <arnaud.langlade@akeneo.com>
2016-05-17 18:18:31 +0200

    PIM-5582: backend edition fixes after review

---
+++ src/PimEnterprise/Bundle/EnrichBundle/Form/Extension/AttributeTypeExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PimEnterprise\Bundle\EnrichBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AttributeTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder->add('isReadOnly', 'switch', [
+            'required' => false,
+            'property_path' => 'properties[is_read_only]',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'pim_enrich_attribute';
+    }
+}
