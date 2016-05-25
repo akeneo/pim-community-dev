2d5faa3afd22395e55c26876e936fef3fc5b281b
Olivier Soulet <olivier.soulet@akeneo.com>
2016-05-25 16:19:54 +0200

    PIM-5427: new family form for export

--- src/Pim/Bundle/EnrichBundle/Form/Type/ChangeFamilyType.php
+++ src/Pim/Bundle/EnrichBundle/Form/Type/ChangeFamilyType.php
@@ -2,34 +2,24 @@
 
 namespace Pim\Bundle\EnrichBundle\Form\Type;
 
-use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
-use Pim\Bundle\EnrichBundle\Form\Factory\IdentifiableModelTransformerFactory;
 use Pim\Component\Catalog\Model\FamilyInterface;
 use Pim\Component\Catalog\Repository\FamilyRepositoryInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Select form type
  * A form type to display an asynchronous dropdown
  *
- * @author    JM Leroux <jean-marie.leroux@akeneo.com>
- * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AsyncSelectType extends AbstractType
+class ChangeFamilyType extends AbstractType
 {
-    /** @var RouterInterface */
-    protected $router;
-
-    /** @var IdentifiableModelTransformerFactory */
-    protected $transformerFactory;
-
     /** @var FamilyRepositoryInterface */
     protected $familyRepository;
 
@@ -37,10 +27,8 @@ class AsyncSelectType extends AbstractType
      * @param RouterInterface           $router
      * @param FamilyRepositoryInterface $familyRepository
      */
-    public function __construct(
-        RouterInterface $router,
-        FamilyRepositoryInterface $familyRepository
-    ) {
+    public function __construct(RouterInterface $router, FamilyRepositoryInterface $familyRepository)
+    {
         $this->router = $router;
         $this->familyRepository = $familyRepository;
     }
@@ -50,7 +38,7 @@ class AsyncSelectType extends AbstractType
      */
     public function getName()
     {
-        return 'pim_async_select';
+        return 'change_family_type';
     }
 
     /**
@@ -58,34 +46,7 @@ class AsyncSelectType extends AbstractType
      */
     public function getParent()
     {
-        return 'hidden';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver
-            ->setDefaults(
-                [
-                    'repository_options' => [],
-                    'route_parameters'   => [],
-                    'required'           => false,
-                    'multiple'           => false,
-                    'min-input-length'   => 0,
-                    'help'   => 'pute',
-                    'label'   => 'pute',
-                ]
-            )
-            ->setAllowedTypes('repository_options', ['array'])
-            ->setAllowedTypes('route_parameters', ['array'])
-            ->setAllowedTypes('required', ['bool'])
-            ->setAllowedTypes('multiple', ['bool'])
-            ->setAllowedTypes('min-input-length', ['int'])
-            ->setAllowedTypes('label', 'string')
-            ->setAllowedTypes('help', 'string')
-            ->setRequired(['route', 'repository']);
+        return 'pim_async_select';
     }
 
     /**
@@ -93,14 +54,7 @@ class AsyncSelectType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $repository = $options['repository'];
-
-        if (!$repository instanceof IdentifiableObjectRepositoryInterface) {
-            throw new UnexpectedTypeException(
-                $repository,
-                '\Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface'
-            );
-        }
+        $builder->resetViewTransformers();
     }
 
     /**
@@ -111,23 +65,22 @@ class AsyncSelectType extends AbstractType
         if ($options['multiple']) {
             $view->vars['attr']['data-multiple'] = 'multiple';
         }
-        $view->vars['help'] = $options['label'];
-        $view->vars['attr']['data-url']              = $this->getUrl($options);
-        $view->vars['attr']['data-min-input-length'] = $options['min-input-length'];
         $view->vars['attr']['data-choices'] = json_encode($this->normalizeFamily($form->getData()));
-        if ($options['required']) {
-            $view->vars['attr']['data-required'] = 'required';
-        }
-        $view->vars['attr']['class'] = 'pim-ajax-entity';
     }
 
+    /**
+     * Normalizes families for the select2
+     *
+     * @param $familyCodes
+     *
+     * @return array
+     */
     protected function normalizeFamily($familyCodes)
     {
         $familyCodes = explode(',', $familyCodes);
 
         $result = [];
         foreach ($familyCodes as $familyCode) {
-            /** @var FamilyInterface $family */
             $family = $this->familyRepository->findOneByIdentifier($familyCode);
             if ($family instanceof FamilyInterface) {
                 $familyLabel = $family->getLabel();
@@ -136,15 +89,5 @@ class AsyncSelectType extends AbstractType
         }
 
         return $result;
-    }
-
-    /**
-     * @param array $options
-     *
-     * @return string
-     */
-    protected function getUrl(array $options)
-    {
-        return $this->router->generate($options['route']);
     }
 }
