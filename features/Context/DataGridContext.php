b2526c16ed45ed60b42fd39793c5c13e9be5aa6a
Julien Sanchez <sumbobyboys@gmail.com>
2016-05-26 10:14:53 +0200

    PIM-5558: add spin on check row

--- features/Context/DataGridContext.php
+++ features/Context/DataGridContext.php
@@ -734,16 +734,16 @@ class DataGridContext extends RawMinkContext implements PageObjectAwareInterface
         $rows = $this->getMainContext()->listToArray($rows);
 
         foreach ($rows as $row) {
-            $gridRow  = $this->datagrid->getRow($row);
-            $checkbox = $gridRow->find('css', 'td.boolean-cell input[type="checkbox"]:not(:disabled)');
+            $this->spin(function () use ($row) {
+                $gridRow  = $this->datagrid->getRow($row);
+                $checkbox = $gridRow->find('css', 'td.boolean-cell input[type="checkbox"]:not(:disabled)');
 
-            if (!$checkbox) {
-                throw $this->createExpectationException(sprintf('Unable to find a checkbox for row %s', $row));
-            }
+                if (!$checkbox) {
+                    return false;
+                }
 
-            if (!$checkbox->isChecked()) {
-                throw $this->createExpectationException(sprintf('Expecting row %s to be checked', $row));
-            }
+                return $checkbox->isChecked();
+            }, sprintf('Fail asserting that "%s" row was checked', $row));
         }
     }
 
