800834a4626ae1b5bf39786482d582cba866c13b
Julien Sanchez <sumbobyboys@gmail.com>
2016-06-04 14:21:11 +0200

    PIM-5809: add locale and scope selects

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/export/product/edit/content/structure/locales.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/export/product/edit/content/structure/locales.js
@@ -9,33 +9,81 @@
 define(
     [
         'text!pim/template/export/product/edit/content/structure/locales',
-        'pim/form'
+        'pim/form',
+        'pim/fetcher-registry',
+        'pim/initselect2'
     ],
     function (
         template,
-        BaseForm
+        BaseForm,
+        fetcherRegistry
     ) {
         return BaseForm.extend({
             template: _.template(template),
-            events: {
-                'change input': 'updateModel'
+            configure: function () {
+                this.listenTo(this.getRoot(), 'channel:update:after', this.channelUpdated.bind(this));
+
+                return BaseForm.prototype.configure.apply(this, arguments);
             },
             render: function () {
                 if (!this.configured) {
                     return this;
                 }
-                this.$el.html(
-                    this.template({
-                        locales: this.getFormData().structure.locales || []
-                    })
-                );
 
-                this.renderExtensions();
+                var defaultLocalesPromise = (new $.Deferred()).resolve();
+                if (_.isEmpty(this.getLocales())) {
+                    defaultLocalesPromise = this.setDefaultLocales();
+                }
+
+                $.when(
+                    fetcherRegistry.getFetcher('channel').fetch(this.getFormData().structure.scope),
+                    defaultLocalesPromise
+                ).then(function (scope) {
+                    this.$el.html(
+                        this.template({
+                            locales: this.getLocales(),
+                            availableLocales: scope.locales
+                        })
+                    );
+
+                    this.$('.select2').select2().on('change', this.updateModel.bind(this));
+
+                    this.renderExtensions();
+                }.bind(this));
+
+                return this;
             },
-            updateModel: function() {
+            updateModel: function (event) {
+                this.setLocales($(event.target).val());
+            },
+            setLocales: function (codes) {
                 var data = this.getFormData();
-                data.structure.locales = JSON.parse(event.target.value);
+                var before = data.structure.locales;
+
+                data.structure.locales = codes;
                 this.setData(data);
+
+                if (before !== codes) {
+                    this.getRoot().trigger('locales:update:after', codes);
+                }
+            },
+            getLocales: function () {
+                return this.getFormData().structure.locales;
+            },
+            channelUpdated: function () {
+                this.setDefaultLocales()
+                    .then(function () {
+                        this.render();
+                    }.bind(this));
+            },
+            setDefaultLocales: function () {
+                return fetcherRegistry.getFetcher('channel')
+                    .fetch(this.getFormData().structure.scope)
+                    .then(function (scope) {
+                        this.setLocales(scope.locales);
+
+                        return;
+                    }.bind(this));
             }
         });
     }
