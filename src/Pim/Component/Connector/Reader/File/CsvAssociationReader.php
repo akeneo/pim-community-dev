582660b38f0c62830f99c4e25cc94e4f05c454d4
pierallard <pierallard@gmail.com>
2016-06-06 17:39:22 +0200

    TIP-458: Move converter from processor to reader (ProductAssociationProcessor)

---
+++ src/Pim/Component/Connector/Reader/File/CsvAssociationReader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pim\Component\Connector\Reader\File;
+
+/**
+ * Product Association CSV reader
+ *
+ * @author    Pierre Allard <pierre.allard@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CsvAssociationReader extends CsvReader
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getArrayConverterOptions()
+    {
+        return ['with_associations' => true];
+    }
+}
