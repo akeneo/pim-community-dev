fa3dd9050aa3942ffc4645289bf2ab9c248e33a8
Adrien Pétremann <petremann.adrien@gmail.com>
2016-06-14 15:27:52 +0200

    TIP-450: Add a test about register priority

--- src/Pim/Component/Connector/spec/ArrayConverter/StandardToFlat/Product/ValueConverter/ValueConverterRegistrySpec.php
+++ src/Pim/Component/Connector/spec/ArrayConverter/StandardToFlat/Product/ValueConverter/ValueConverterRegistrySpec.php
@@ -7,6 +7,19 @@ use Pim\Component\Connector\ArrayConverter\StandardToFlat\Product\ValueConverter
 
 class ValueConverterRegistrySpec extends ObjectBehavior
 {
+    function it_increments_priority_if_service_already_registered(
+        AbstractValueConverter $converter1,
+        AbstractValueConverter $converter2
+    ) {
+        $converter1->supportsField('pim_catalog_identifier')->willReturn(true);
+        $converter2->supportsField('pim_catalog_identifier')->willReturn(true);
+
+        $this->register($converter1, 100);
+        $this->register($converter2, 100);
+
+        $this->getConverter('pim_catalog_identifier')->shouldReturn($converter1);
+    }
+
     function it_gets_converters(AbstractValueConverter $converter)
     {
         $converter->supportsField('pim_catalog_identifier')->willReturn(true);
