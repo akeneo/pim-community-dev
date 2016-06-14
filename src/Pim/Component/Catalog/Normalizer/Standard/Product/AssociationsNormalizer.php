bc8b24261228dfdefb184739c6558cee46916690
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-06-14 15:11:27 +0200

    TIP-480: Normalizers for the standard format

---
+++ src/Pim/Component/Catalog/Normalizer/Standard/Product/AssociationsNormalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Pim\Component\Catalog\Normalizer\Standard\Product;
+
+use Pim\Component\Catalog\Model\ProductInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Normalize associations into an array
+ *
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AssociationsNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($product, $format = null, array $context = [])
+    {
+        $data = [];
+
+        foreach ($product->getAssociations() as $association) {
+            $code = $association->getAssociationType()->getCode();
+
+            foreach ($association->getGroups() as $group) {
+                $data[$code]['groups'][] = $group->getCode();
+            }
+
+            foreach ($association->getProducts() as $product) {
+                $data[$code]['products'][] = $product->getReference();
+            }
+        }
+
+        ksort($data);
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ProductInterface && 'standard' === $format;
+    }
+}
