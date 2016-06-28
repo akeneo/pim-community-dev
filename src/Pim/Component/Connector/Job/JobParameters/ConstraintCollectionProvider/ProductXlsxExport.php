0a645a8f4a93150f162368a2c3e66a571537617e
Julien Sanchez <sumbobyboys@gmail.com>
2016-06-28 10:47:26 +0200

    PIM-5853: add compatibility for excel

--- src/Pim/Component/Connector/Job/JobParameters/ConstraintCollectionProvider/ProductXlsxExport.php
+++ src/Pim/Component/Connector/Job/JobParameters/ConstraintCollectionProvider/ProductXlsxExport.php
@@ -46,45 +46,7 @@ class ProductXlsxExport implements ConstraintCollectionProviderInterface
         $constraintFields = $baseConstraint->fields;
         $constraintFields['decimalSeparator'] = new NotBlank();
         $constraintFields['dateFormat'] = new NotBlank();
-        $constraintFields['channel'] = [
-            new NotBlank(['groups' => 'Execution']),
-            new Channel()
-        ];
-
-        $constraintFields['locales'] = new NotBlank([
-            'groups'  => 'Execution',
-            'message' => 'pim_connector.export.locales.validation.not_blank'
-        ]);
-
-        $constraintFields['enabled'] = new NotBlank(['groups' => 'Execution']);
-        $constraintFields['completeness'] = new NotBlank(['groups' => 'Execution']);
-        $constraintFields['updated_since_strategy'] = [
-            new NotBlank(['groups' => 'Execution']),
-            new Choice(['choices' => [
-                    'all',
-                    'last_export',
-                    'since_date',
-                    'since_n_days',
-                ]
-            ])
-        ];
-        $constraintFields['updated_since_date'] = new DateTime(['groups' => 'Execution']);
-        $constraintFields['updated_since_n_days'] = new Range(['min' => 0, 'groups' => 'Execution']);
-        $constraintFields['linesPerFile'] = [
-            new NotBlank(),
-            new GreaterThan(1)
-        ];
-        $constraintFields['families'] = [];
-        $constraintFields['product_identifier'] = [];
-        $constraintFields['completeness'] = [
-            new NotBlank(['groups' => 'Execution']),
-            new Choice(['choices' => [
-                'at_least_one_complete',
-                'all_complete',
-                'all_incomplete',
-                'all',
-            ], 'groups' => 'Execution'])
-        ];
+        $constraintFields['filters'] = [];
 
         return new Collection(['fields' => $constraintFields]);
     }
