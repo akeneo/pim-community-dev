0ff79c4d5cdeddd81e25890985327cdfe17d2152
Olivier Soulet <olivier.soulet@akeneo.com>
2016-06-29 16:26:49 +0200

    PIM-5488: Add abstract controller for mass edit

---
+++ src/Pim/Bundle/EnrichBundle/Controller/MassEdit/AbstractMassEditController.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Controller\MassEdit;
+
+use Oro\Bundle\DataGridBundle\Extension\MassAction\MassActionParametersParser;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Pim\Bundle\DataGridBundle\Adapter\GridFilterAdapterInterface;
+use Pim\Bundle\EnrichBundle\Flash\Message;
+use Pim\Bundle\EnrichBundle\MassEditAction\MassEditFormResolver;
+use Pim\Bundle\EnrichBundle\MassEditAction\Operation\OperationRegistryInterface;
+use Pim\Bundle\EnrichBundle\MassEditAction\OperationJobLauncher;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+
+/**
+ * Abstract Mass Edit controller that contains base methods
+ * Handle all the steps from choosing action to run to the launching of the action.
+ *
+ * @author    Olivier Soulet <olivier.soulet@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+abstract class AbstractMassEditController
+{
+    /** @var RouterInterface */
+    protected $router;
+
+    /** @var EngineInterface */
+    protected $templating;
+
+    /** @var MassActionParametersParser */
+    protected $parametersParser;
+
+    /** @var GridFilterAdapterInterface */
+    protected $gridFilterAdapter;
+
+    /** @var MassEditFormResolver */
+    protected $massEditFormResolver;
+
+    /** @var OperationRegistryInterface */
+    protected $operationRegistry;
+
+    /** @var OperationJobLauncher */
+    protected $operationLauncher;
+
+    /**
+     * @param RouterInterface            $router
+     * @param EngineInterface            $templating
+     * @param MassActionParametersParser $parametersParser
+     * @param GridFilterAdapterInterface $gridFilterAdapter
+     * @param MassEditFormResolver       $massEditFormResolver
+     * @param OperationRegistryInterface $operationRegistry
+     * @param OperationJobLauncher       $operationLauncher
+     */
+    public function __construct(
+        RouterInterface $router,
+        EngineInterface $templating,
+        MassActionParametersParser $parametersParser,
+        GridFilterAdapterInterface $gridFilterAdapter,
+        MassEditFormResolver $massEditFormResolver,
+        OperationRegistryInterface $operationRegistry,
+        OperationJobLauncher $operationLauncher
+    ) {
+        $this->router = $router;
+        $this->templating = $templating;
+        $this->parametersParser = $parametersParser;
+        $this->gridFilterAdapter = $gridFilterAdapter;
+        $this->massEditFormResolver = $massEditFormResolver;
+        $this->operationRegistry = $operationRegistry;
+        $this->operationLauncher = $operationLauncher;
+    }
+
+    /**
+     * Display the form to choose the mass edit action to execute.
+     *
+     * @AclAncestor("pim_enrich_mass_edit")
+     *
+     * @param Request $request
+     * @param string  $operationGroup
+     *
+     * @return RedirectResponse|Response
+     */
+    public function chooseAction(Request $request, $operationGroup)
+    {
+        $form = $this
+            ->massEditFormResolver
+            ->getAvailableOperationsForm('family-grid', $operationGroup);
+
+        $queryParams = $this->getQueryParams($request);
+
+        if ($request->isMethod('POST')) {
+            $form->submit($request);
+
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $queryParams += ['operationAlias' => $data['operationAlias']];
+
+                $configureRoute = $this
+                    ->router
+                    ->generate($this->getChooseRoute(), $queryParams);
+
+                return new RedirectResponse($configureRoute);
+            }
+        }
+
+        $itemsCount = $request->get('itemsCount');
+
+        return $this->templating->renderResponse($this->getChooseTemplate(), [
+            'form'        => $form->createView(),
+            'itemsCount'  => $itemsCount,
+            'queryParams' => array_merge($queryParams, ['operationGroup' => $operationGroup]),
+        ]);
+    }
+
+    /**
+     * Display the form to configure the mass edit action to execute
+     *
+     * @AclAncestor("pim_enrich_mass_edit")
+     *
+     * @param Request $request
+     * @param string  $operationAlias
+     *
+     * @return Response
+     */
+    public function configureAction(Request $request, $operationAlias)
+    {
+        $operation = $this
+            ->operationRegistry
+            ->get($operationAlias);
+
+        $form = $this
+            ->massEditFormResolver
+            ->getConfigurationForm($operationAlias);
+
+        $itemsCount = $request->get('itemsCount');
+        $configureTemplate = sprintf($this->getConfigureTemplate(), $operationAlias);
+
+        return $this->templating->renderResponse(
+            $configureTemplate,
+            [
+                'form'           => $form->createView(),
+                'operationAlias' => $operationAlias,
+                'operation'      => $operation,
+                'queryParams'    => $this->getQueryParams($request),
+                'itemsCount'     => $itemsCount,
+            ]
+        );
+    }
+
+    /**
+     * Launch the background process related to the mass edit action
+     *
+     * @AclAncestor("pim_enrich_mass_edit")
+     *
+     * @param Request $request
+     * @param string  $operationAlias
+     *
+     * @throws NotFoundResourceException
+     *
+     * @return Response
+     */
+    public function performAction(Request $request, $operationAlias)
+    {
+        $form = $this
+            ->massEditFormResolver
+            ->getConfigurationForm($operationAlias);
+
+        $itemsCount = $request->get('itemsCount');
+        $configureTemplate = sprintf($this->getPerformTemplate(), $operationAlias);
+
+        $form->remove('operationAlias');
+        $form->submit($request);
+
+        $queryParams = $this->getQueryParams($request);
+
+        if ($form->isValid()) {
+            $pimFilters = $this
+                ->gridFilterAdapter
+                ->adapt($request);
+
+            $operation = $form->getData();
+            $operation->setFilters($pimFilters);
+
+            $this
+                ->operationLauncher
+                ->launch($operation);
+
+            $request
+                ->getSession()
+                ->getFlashBag()
+                ->add(
+                    'success',
+                    new Message(sprintf($this->getPerformRoute(), $operationAlias))
+                );
+
+            $redirectRoute = $this->getPerformRedirectRoute();
+
+            return new RedirectResponse(
+                $this->router->generate($redirectRoute, ['dataLocale' => $queryParams['dataLocale']])
+            );
+        }
+
+        return $this->templating->renderResponse(
+            $configureTemplate,
+            [
+                'form'           => $form->createView(),
+                'operationAlias' => $operationAlias,
+                'itemsCount'     => $itemsCount,
+                'queryParams'    => $queryParams
+            ]
+        );
+    }
+
+    /**
+     * Get the datagrid query parameters
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    protected function getQueryParams(Request $request)
+    {
+        $params = $this
+            ->parametersParser
+            ->parse($request);
+
+        $params['gridName']   = $request->get('gridName');
+        $params['actionName'] = $request->get('actionName');
+        $params['values']     = implode(',', $params['values']);
+        $params['filters']    = json_encode($params['filters']);
+        $params['dataLocale'] = $request->get('dataLocale', null);
+        $params['itemsCount'] = $request->get('itemsCount');
+
+        return $params;
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getChooseRoute();
+
+    /**
+     * @return string
+     */
+    abstract protected function getConfigureTemplate();
+
+    /**
+     * @return string
+     */
+    abstract protected function getChooseTemplate();
+
+    /**
+     * @return string
+     */
+    abstract protected function getPerformRoute();
+
+    /**
+     * @return string
+     */
+    abstract protected function getPerformTemplate();
+
+    /**
+     * @return string
+     */
+    abstract protected function getPerformRedirectRoute();
+}
