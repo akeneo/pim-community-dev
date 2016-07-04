fc3941dee63c9bbe30b604a78d3bd6b6352f11e3
Philippe Mossière <philippe.mossiere@akeneo.com>
2016-07-04 16:59:58 +0200

    TIP-418: add missing class descriptions

--- src/Akeneo/Component/Batch/Job/UndefinedJobException.php
+++ src/Akeneo/Component/Batch/Job/UndefinedJobException.php
@@ -3,6 +3,8 @@
 namespace Akeneo\Component\Batch\Job;
 
 /**
+ * Throw an exception if job is undefined
+ *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
