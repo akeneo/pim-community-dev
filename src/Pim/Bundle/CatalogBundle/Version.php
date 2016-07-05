8669895a1d03a5dc4e03abf1986f63838ecd86fc
aRn0D <arnaud.langlade@akeneo.com>
2016-07-05 15:44:26 +0200

    Preparing version 1.4.26

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.25';
+    const VERSION = '1.4.26';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
