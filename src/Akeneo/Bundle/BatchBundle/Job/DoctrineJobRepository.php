ed0ab0a1ed509c0aa76ed3082e26b7395c7efcd1
Cristian Quiroz <cris@qrz.io>
2016-07-05 09:40:24 +0100

    Ensure the server is available before update job and step execution data.

--- src/Akeneo/Bundle/BatchBundle/Job/DoctrineJobRepository.php
+++ src/Akeneo/Bundle/BatchBundle/Job/DoctrineJobRepository.php
@@ -9,6 +9,7 @@ use Akeneo\Component\Batch\Model\JobExecution;
 use Akeneo\Component\Batch\Model\JobInstance;
 use Akeneo\Component\Batch\Model\StepExecution;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityManager;
 
 /**
@@ -123,6 +124,7 @@ class DoctrineJobRepository implements JobRepositoryInterface
      */
     public function updateJobExecution(JobExecution $jobExecution)
     {
+        $this->checkConnection();
         $this->jobManager->persist($jobExecution);
         $this->jobManager->flush($jobExecution);
     }
@@ -132,6 +134,7 @@ class DoctrineJobRepository implements JobRepositoryInterface
      */
     public function updateStepExecution(StepExecution $stepExecution)
     {
+        $this->checkConnection();
         $this->jobManager->persist($stepExecution);
         $this->jobManager->flush($stepExecution);
     }
@@ -192,5 +195,37 @@ class DoctrineJobRepository implements JobRepositoryInterface
             $this->jobManager->remove($jobsExecution);
         }
         $this->jobManager->flush();
+    }
+
+    /**
+     * Ping the Server, if not available then reset the connection.
+     * @author Cristian Quiroz <cq@amp.co>
+     */
+    private function checkConnection()
+    {
+        $connection = $this->jobManager->getConnection();
+        if ($this->pingConnection($connection) === false) {
+            $connection->close();
+            $connection->connect();
+        }
+    }
+
+    /**
+     * Pings the server, returns false if it's not available.
+     * There is a ping() method in Doctrine\DBAL\Connection in the doctrine/dbal package
+     * as of 2.5.0, but  we are currently on 2.4.x
+     * @return bool
+     * @author Cristian Quiroz <cq@amp.co>
+     */
+    private function pingConnection()
+    {
+        $connection = $this->jobManager->getConnection();
+        $connection->connect();
+        try {
+            $connection->query($connection->getDatabasePlatform()->getDummySelectSQL());
+            return true;
+        } catch (DBALException $e) {
+            return false;
+        }
     }
 }
