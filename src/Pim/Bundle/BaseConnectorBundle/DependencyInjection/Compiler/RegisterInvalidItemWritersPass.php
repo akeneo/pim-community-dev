d4ce1c3a91983fc4fd23cbd9b1001a5dd8ad3e24
Adrien Pétremann <petremann.adrien@gmail.com>
2016-07-08 16:35:08 +0200

    PIM-5094: WIP put the register pass for writers

---
+++ src/Pim/Bundle/BaseConnectorBundle/DependencyInjection/Compiler/RegisterInvalidItemWritersPass.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pim\Bundle\BaseConnectorBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class RegisterInvalidItemWritersPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('pim_base_connector.event_listener.invalid_items_writer_register')) {
+            return;
+        }
+
+        $service = $container->getDefinition('pim_base_connector.event_listener.invalid_items_writer_register');
+
+        $taggedServices = $container->findTaggedServiceIds('pim_base_connector.invalid_items_writer');
+
+        foreach (array_keys($taggedServices) as $id) {
+            $service->addMethodCall('registerWriter', [new Reference($id)]);
+        }
+    }
+}
---
+++ src/Pim/Bundle/BaseConnectorBundle/EventListener/InvalidItemWriterResolver.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Pim\Bundle\BaseConnectorBundle\EventListener;
+
+use Akeneo\Component\Batch\Event\EventInterface;
+use Akeneo\Component\Batch\Event\JobExecutionEvent;
+use Pim\Bundle\BaseConnectorBundle\Archiver\ArchiverInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class InvalidItemWriterResolver implements EventSubscriberInterface
+{
+    /** @var array */
+    protected $writers = [];
+
+    /** @var InvalidItemsCollector */
+    protected $invalidItemsCollector;
+
+    /**
+     * @param InvalidItemsCollector $invalidItemsCollector
+     */
+    public function __construct(InvalidItemsCollector $invalidItemsCollector)
+    {
+        $this->invalidItemsCollector = $invalidItemsCollector;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            EventInterface::BEFORE_JOB_STATUS_UPGRADE => 'beforeStatusUpgrade',
+        ];
+    }
+
+    /**
+     * Register a writer
+     *
+     * TODO: Update the $writer interface
+     *
+     * @param ArchiverInterface $writer
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function registerWriter(ArchiverInterface $writer)
+    {
+        if (array_key_exists($writer->getName(), $this->writers)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'There is already a registered writer named "%s": %s',
+                    $writer->getName(),
+                    get_class($this->writers[$writer->getName()])
+                )
+            );
+        }
+
+        $this->writers[$writer->getName()] = $writer;
+    }
+
+    /**
+     * Delegate archiving to the registered archivers
+     *
+     * @param JobExecutionEvent $event
+     */
+    public function beforeStatusUpgrade(JobExecutionEvent $event)
+    {
+        $jobExecution = $event->getJobExecution();
+
+        $items = $this->invalidItemsCollector->getInvalidItems();
+
+        foreach ($this->writers as $writer) {
+            if ($writer->supports($item)) {
+                $writer->archive($jobExecution);
+            }
+        }
+    }
+}
