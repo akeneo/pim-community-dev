a1ae2ca64380015357074224338b37f43b5c86a7
Julien Sanchez <sumbobyboys@gmail.com>
2016-07-14 13:02:38 +0200

    PIM-5853: fix sdtClass error

--- src/Pim/Component/Connector/Job/JobParameters/DefaultValuesProvider/ProductXlsxExport.php
+++ src/Pim/Component/Connector/Job/JobParameters/DefaultValuesProvider/ProductXlsxExport.php
@@ -40,7 +40,7 @@ class ProductXlsxExport implements DefaultValuesProviderInterface
         $parameters['decimalSeparator'] = LocalizerInterface::DEFAULT_DECIMAL_SEPARATOR;
         $parameters['dateFormat'] = LocalizerInterface::DEFAULT_DATE_FORMAT;
         $parameters['linesPerFile'] = 10000;
-        $parameters['filters'] = ['data' => [], 'structure' => new \stdClass()];
+        $parameters['filters'] = ['data' => [], 'structure' => (object) []];
 
         return $parameters;
     }
