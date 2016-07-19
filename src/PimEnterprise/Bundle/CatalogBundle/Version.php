8dd714c4a3cccf94e32f4a03600e764a612682cd
aRn0D <arnaud.langlade@akeneo.com>
2016-07-19 10:44:27 +0200

    Preparing version 1.5.7

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.6';
+    const VERSION = '1.5.7';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Sunflower';
