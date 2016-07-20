094d69fd4ae359e8955dc61fd4a5022b8e22bd38
Olivier Soulet <olivier.soulet@akeneo.com>
2016-07-20 17:11:21 +0200

    PIM-5853: add text filter

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/filter/attribute/text.js
@@ -0,0 +1,100 @@
+'use strict';
+
+define([
+    'jquery',
+    'underscore',
+    'oro/translator',
+    'pim/filter/filter',
+    'pim/fetcher-registry',
+    'pim/user-context',
+    'pim/i18n',
+    'text!pim/template/filter/attribute/text',
+    'jquery.select2'
+], function (
+    $,
+    _,
+    __,
+    BaseFilter,
+    FetcherRegistry,
+    UserContext,
+    i18n,
+    template
+) {
+    return BaseFilter.extend({
+        shortname: 'text',
+        template: _.template(template),
+        events: {
+            'change [name="filter-operator"], [name="filter-data"]': 'updateState'
+        },
+
+        /**
+         * {@inherit}
+         */
+        initialize: function (config) {
+            this.config = config.config;
+
+            return BaseFilter.prototype.initialize.apply(this, arguments);
+        },
+
+        /**
+         * {@inherit}
+         */
+        isEmpty: function () {
+            return 'EMPTY' !== this.getOperator() &&
+                (undefined === this.getValue() || '' === this.getValue())
+                ;
+        },
+
+        /**
+         * {@inherit}
+         */
+        renderInput: function (templateContext) {
+            if (undefined === this.getValue()) {
+                this.setValue('')
+            }
+
+            return this.template(_.extend({}, templateContext, {
+                __: __,
+                value: this.getValue(),
+                field: this.getField(),
+                operator: this.getOperator(),
+                operators: this.config.operators
+            }));
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        postRender: function () {
+            this.$('select.select2').select2({minimumResultsForSearch: -1});
+        },
+
+        /**
+         * {@inherit}
+         */
+        getTemplateContext: function () {
+            return $.when(
+                BaseFilter.prototype.getTemplateContext.apply(this, arguments),
+                FetcherRegistry.getFetcher('attribute').fetch(this.getField())
+            ).then(function (templateContext, attribute) {
+                return _.extend({}, templateContext, {
+                    label: i18n.getLabel(attribute.labels, UserContext.get('uiLocale'), attribute.code)
+                });
+            }.bind(this));
+        },
+
+        /**
+         * {@inherit}
+         */
+        updateState: function () {
+            var value = this.$('[name="filter-data"]').val();
+            var operator = this.$('[name="filter-operator"]').val();
+
+            this.setData({
+                field: this.getField(),
+                operator: operator,
+                value: value
+            });
+        }
+    });
+});
