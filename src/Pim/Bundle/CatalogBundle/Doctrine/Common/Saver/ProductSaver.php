1c6e5ee2db2aad01fdcff99e1041cb9207b03b55
Iulian Popa <iulyanpopa@gmail.com>
2016-07-22 14:02:38 +0300

    Example of eventstore events

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
@@ -83,7 +83,13 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
         if (true === $options['recalculate']) {
             $this->completenessManager->generateMissingForProduct($product);
         }
-
+        /*
+         * A.
+         * If I listen for this event on mass edit I will not have in the $event->getEventType() the real changeset
+         * because the object was not flushed into the database.
+         * Also it's still not so clear for me why the post_save event should be dispatched if you didn't flush the
+         * object.
+         * */
         $this->eventDispatcher->dispatch(StorageEvents::POST_SAVE, new GenericEvent($product, $options));
     }
 
@@ -103,13 +109,34 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
         $itemOptions['flush'] = false;
 
         foreach ($products as $product) {
+            /*
+            B.
+            Here is where the save function is called multiple times for a batch when we use mass edit.
+            The function save will dispatch post_save events without flushing the changes into the database.
+
+            If saveAll function would not base on save function, everything should work as I expect:
+            - saveAll will flush every batch and dispatch the post_save_all event, which I am listening when I do imports
+            or mass edits.
+            - save will flush every time and dispatch the events with the correct change set.
+            */
             $this->save($product, $itemOptions);
         }
 
         if (true === $options['flush']) {
             $this->objectManager->flush();
         }
-
+        /*
+         * C.
+         * 1. If we listen for both post_save and post_save_all events all the events are duplicated right now.
+         * - From post_save I have the event with a wrong change set.
+         * - From post_save_all I have the correct event.
+         *
+         * 2. If I change the save function to dispatch the event post_save only when the object is flushed (as in my
+         * previous PR) and listen for both post_save and post_save_all, everything is awesome.
+         * The only issue in that case is that on mass edit "Edit common attributes"
+         * "PimEnterprise\Bundle\WorkflowBundle\Doctrine\Common\Saver\DelegatingProductSaver" does not raise
+         * "pre/post_save_all" events at all on saveAll function.
+         * */
         $this->eventDispatcher->dispatch(StorageEvents::POST_SAVE_ALL, new GenericEvent($products, $options));
     }
 }
