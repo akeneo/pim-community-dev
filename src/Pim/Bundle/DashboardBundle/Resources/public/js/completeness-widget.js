7a74a9e76c0ea9caa027458345462bd972e5d375
pierallard <pierallard@gmail.com>
2016-07-29 09:54:36 +0200

    PIM-5900: Move completeness widget into template

--- src/Pim/Bundle/DashboardBundle/Resources/public/js/completeness-widget.js
+++ src/Pim/Bundle/DashboardBundle/Resources/public/js/completeness-widget.js
@@ -1,6 +1,6 @@
 define(
-    ['jquery', 'underscore', 'pim/dashboard/abstract-widget'],
-    function ($, _, AbstractWidget) {
+    ['jquery', 'underscore', 'pim/dashboard/abstract-widget', 'text!pimdashboard/templates/completeness-widget'],
+    function ($, _, AbstractWidget, template) {
         'use strict';
 
         return AbstractWidget.extend({
@@ -13,43 +13,7 @@ define(
                 inCompleteBar: 'bar-warning'
             },
 
-            template: _.template(
-                [
-                    '<% _.each(data, function (channelResult, channel) { %>',
-                        '<tr class="channel">',
-                            '<td>',
-                                '<a href="#" data-toggle-channel="<%= channel %>">',
-                                    '<i class="icon-caret-down"></i>',
-                                '</a>',
-                            '</td>',
-                            '<td>',
-                                '<a href="#" data-toggle-channel="<%= channel %>">',
-                                    '<b><%= channel %></b>',
-                                '</a>',
-                            '</td>',
-                            '<td>',
-                                '<b><%= channelResult.percentage %>%</b>',
-                            '</td>',
-                            '<td>&nbsp;</td>',
-                        '</tr>',
-                        '<% _.each(channelResult.locales, function (localeResult, locale) { %>',
-                            '<tr data-channel="<%= channel %>">',
-                                '<td>&nbsp;</td>',
-                                '<td><%= locale %></td>',
-                                '<td><%= localeResult.ratio %>%</td>',
-                                '<td class="progress-cell">',
-                                    '<div class="progress">',
-                                        '<div class="bar ' + '<%= localeResult.ratio === 100 ? ' +
-                                            'options.completeBar : options.inCompleteBar %>" ' +
-                                            'style="width: <%= localeResult.ratio %>%;"></div>',
-                                    '</div>',
-                                    '<small><%= localeResult.complete %>/<%= channelResult.total %></small>',
-                                '</td>',
-                            '</tr>',
-                        '<% }); %>',
-                    '<% }); %>'
-                ].join('')
-            ),
+            template: _.template(template),
 
             events: {
                 'click a[data-toggle-channel]': 'toggleChannel'
