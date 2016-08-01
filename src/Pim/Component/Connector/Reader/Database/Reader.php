3b725e6c2e8511a1cb3479e774e4a7e502b21231
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-08-01 17:55:31 +0200

    TIP-560: rename database base reader

--- src/Pim/Component/Connector/Reader/Database/Reader.php
+++ src/Pim/Component/Connector/Reader/Database/Reader.php
@@ -13,7 +13,7 @@ use Doctrine\Common\Persistence\ObjectRepository;
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class BaseReader extends AbstractReader implements ItemReaderInterface, StepExecutionAwareInterface
+class Reader extends AbstractReader implements ItemReaderInterface, StepExecutionAwareInterface
 {
     /** @var ObjectRepository */
     protected $repository;
