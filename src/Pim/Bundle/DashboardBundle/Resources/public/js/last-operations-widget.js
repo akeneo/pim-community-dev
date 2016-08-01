bc66acc753781e01adc778cd623318a5d3e1a8ee
pierallard <pierallard@gmail.com>
2016-08-01 10:36:09 +0200

    PIM-5900: Move last operations into template

--- src/Pim/Bundle/DashboardBundle/Resources/public/js/last-operations-widget.js
+++ src/Pim/Bundle/DashboardBundle/Resources/public/js/last-operations-widget.js
@@ -1,6 +1,13 @@
 define(
-    ['jquery', 'underscore', 'routing', 'oro/navigation', 'pim/dashboard/abstract-widget'],
-    function ($, _, Routing, Navigation, AbstractWidget) {
+    [
+        'jquery',
+        'underscore',
+        'routing',
+        'oro/navigation',
+        'pim/dashboard/abstract-widget',
+        'text!pimdashboard/templates/last-operations-widget'
+    ],
+    function ($, _, Routing, Navigation, AbstractWidget, template) {
         'use strict';
 
         return AbstractWidget.extend({
@@ -30,58 +37,7 @@ define(
                 '</a>'
             ),
 
-            template: _.template(
-                [
-                    '<% if (!_.isEmpty(data)) { %>',
-                        '<thead>',
-                            '<tr>',
-                                '<th class="center">',
-                                    '<%= _.__("pim_dashboard.widget.last_operations.date") %>',
-                                '</th>',
-                                '<th class="center">',
-                                    '<%= _.__("pim_dashboard.widget.last_operations.type") %>',
-                                '</th>',
-                                '<th class="center">',
-                                    '<%= _.__("pim_dashboard.widget.last_operations.profile name") %>',
-                                '</th>',
-                                '<th class="center">',
-                                    '<%= _.__("pim_dashboard.widget.last_operations.status") %>',
-                                '</th>',
-                                '<th></th>',
-                            '</tr>',
-                        '</thead>',
-                        '<tbody>',
-                            '<% _.each(data, function (operation) { %>',
-                                '<tr>',
-                                    '<td>',
-                                        '<%= operation.date %>',
-                                    '</td>',
-                                    '<td>',
-                                        '<%= _.__("pim_dashboard.widget.last_operations.job_type."+operation.type) %>',
-                                    '</td>',
-                                    '<td><%= operation.label %></td>',
-                                    '<td>',
-                                        '<span class="label <%= operation.labelClass %> fullwidth">',
-                                            '<%= operation.statusLabel %>',
-                                        '</span>',
-                                    '</td>',
-                                    '<td>',
-                                        '<a class="btn btn-mini" href="javascript:void(0);" ',
-                                            'data-id="<%= operation.id %>" ',
-                                            'data-operation-type="<%= operation.type %>">',
-                                            '<%= _.__("pim_dashboard.widget.last_operations.details") %>',
-                                        '</a>',
-                                    '</td>',
-                                '</tr>',
-                            '<% }); %>',
-                        '</tbody>',
-                    '<% } else if (options.contentLoaded) {%>',
-                        '<span class="label text-center buffer-small-top buffer-small-bottom fullwidth">',
-                            '<%= _.__("pim_dashboard.widget.last_operations.empty") %>',
-                        '</span>',
-                    '<% } %>'
-                ].join('')
-            ),
+            template: _.template(template),
 
             events: {
                 'click a.btn': 'followLink',
