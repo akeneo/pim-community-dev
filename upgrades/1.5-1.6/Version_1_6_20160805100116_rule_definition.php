a32696d252e2e8aeea081c5c6d98a84a61ff11a1
aRn0D <arnaud.langlade@akeneo.com>
2016-08-05 10:39:43 +0200

    PIM-5788: Missing migration

---
+++ upgrades/1.5-1.6/Version_1_6_20160805100116_rule_definition.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+/**
+ * @author Arnaud Langlade <arnaud.langlade@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Version20160805100116 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE akeneo_rule_engine_rule_definition ADD impacted_subject_count INT DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE akeneo_rule_engine_rule_definition DROP impacted_subject_count');
+    }
+}
