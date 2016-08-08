55c98a4ab7c60d6b42d69e9a21057284ea7a1092
aRn0D <arnaud.langlade@akeneo.com>
2016-08-08 12:13:38 +0200

    PIM-5788: Wrong classname

--- upgrades/1.5-1.6/Version_1_6_20160804181154_versioning_index.php
+++ upgrades/1.5-1.6/Version_1_6_20160804181154_versioning_index.php
@@ -12,7 +12,7 @@ use Doctrine\DBAL\Schema\Schema;
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Version20160804181154 extends AbstractMigration
+class Version_1_6_20160804181154_versioning_index extends AbstractMigration
 {
     /**
      * @param Schema $schema
