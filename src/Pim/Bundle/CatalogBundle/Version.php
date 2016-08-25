da73831c8ee87a3f720c8c23d0258cb3ab8d3f8c
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2016-08-25 14:55:16 +0200

    Preparing version 1.5.8

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.7';
+    const VERSION = '1.5.8';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
