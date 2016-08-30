44dadbad4fd5ea1eb94f417c04c66574bb3fda7c
Adrien Pétremann <petremann.adrien@gmail.com>
2016-08-30 10:44:32 +0200

    PIM-5790: prepare 1.6.0 release

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,10 +12,10 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.6.0-RC1';
+    const VERSION = '1.6.0';
 
     /** @staticvar string */
-    const VERSION_CODENAME = '';
+    const VERSION_CODENAME = 'Wackiki Wabbit';
 
     /** @staticvar string */
     const EDITION = 'CE';
