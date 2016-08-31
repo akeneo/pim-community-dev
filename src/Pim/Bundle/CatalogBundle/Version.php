0e6a061febf765a12a66e1ecef1983f7748ae711
Olivier Soulet <olivier.soulet@akeneo.com>
2016-08-31 14:42:30 +0200

    Preparing version 1.4.27

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.26';
+    const VERSION = '1.4.27';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
