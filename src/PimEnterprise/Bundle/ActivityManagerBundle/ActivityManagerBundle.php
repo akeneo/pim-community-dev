9ba093f829411f4ad28b6b91a65a52e3ea8cb339
aRn0D <arnaud.langlade@akeneo.com>
2016-09-12 11:59:50 +0200

    Create a default doctrine mapping driver

--- src/PimEnterprise/Bundle/ActivityManagerBundle/ActivityManagerBundle.php
+++ src/PimEnterprise/Bundle/ActivityManagerBundle/ActivityManagerBundle.php
@@ -11,6 +11,9 @@
 
 namespace Akeneo\ActivityManager\Bundle;
 
+use Akeneo\Bundle\StorageUtilsBundle\AkeneoStorageUtilsBundle;
+use Akeneo\Bundle\StorageUtilsBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -21,4 +24,29 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ActivityManagerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $productMappings = [
+            realpath(__DIR__.'/Resources/config/model/doctrine') => 'Akeneo\ActivityManager\Component\Model'
+        ];
+
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createYamlMappingDriver(
+                $productMappings,
+                ['doctrine.orm.entity_manager'],
+                'akeneo_storage_utils.storage_driver.doctrine/orm'
+            )
+        );
+
+        if (class_exists(AkeneoStorageUtilsBundle::DOCTRINE_MONGODB)) {
+            $mongoDBClass = AkeneoStorageUtilsBundle::DOCTRINE_MONGODB;
+            $container->addCompilerPass(
+                $mongoDBClass::createYamlMappingDriver(
+                    $productMappings,
+                    ['doctrine.odm.mongodb.document_manager'],
+                    'akeneo_storage_utils.storage_driver.doctrine/mongodb-odm'
+                )
+            );
+        }
+    }
 }
