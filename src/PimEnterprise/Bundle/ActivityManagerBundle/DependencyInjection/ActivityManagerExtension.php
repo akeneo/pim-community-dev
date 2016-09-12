ef801390f21954740f850896af7146b54c7f68aa
aRn0D <arnaud.langlade@akeneo.com>
2016-09-12 12:12:42 +0200

    Typo

--- src/PimEnterprise/Bundle/ActivityManagerBundle/DependencyInjection/ActivityManagerExtension.php
+++ src/PimEnterprise/Bundle/ActivityManagerBundle/DependencyInjection/ActivityManagerExtension.php
@@ -18,9 +18,9 @@ use Symfony\Component\Config\FileLocator;
 
 /**
  * @author   Arnaud Langlade <arnaud.langlade@akeneo.com>
-* @copyright 2016 Akeneo SAS (http://www.akeneo.com)
-* @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*/
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class ActivityManagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder)
