b6091497c75dec5fca8d4f43ae6fdb2201b45f52
skeleton <marie.bochu@akeneo.com>
2016-10-03 18:17:04 +0200

    TIP-576: add standard normalizer for Asset

---
+++ spec/PimEnterprise/Component/ProductAsset/Normalizer/Standard/AssetNormalizerSpec.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace spec\PimEnterprise\Component\ProductAsset\Normalizer\Standard;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Component\Catalog\Normalizer\Standard\DateTimeNormalizer;
+use PimEnterprise\Component\ProductAsset\Model\AssetInterface;
+
+class AssetNormalizerSpec extends ObjectBehavior
+{
+    function let(DateTimeNormalizer $dateTimeNormalizer)
+    {
+        $this->beConstructedWith($dateTimeNormalizer);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('PimEnterprise\Component\ProductAsset\Normalizer\Standard\AssetNormalizer');
+    }
+
+    function it_is_a_normalizer()
+    {
+        $this->shouldImplement('Symfony\Component\Serializer\Normalizer\NormalizerInterface');
+    }
+
+    function it_supports_standard_normalization(AssetInterface $asset)
+    {
+        $this->supportsNormalization($asset, 'standard')->shouldBe(true);
+        $this->supportsNormalization($asset, 'json')->shouldBe(false);
+        $this->supportsNormalization($asset, 'xml')->shouldBe(false);
+    }
+
+    function it_normalizes_empty_asset($dateTimeNormalizer, AssetInterface $asset)
+    {
+        $dateTimeNormalizer->normalize(null, 'standard', [])->willReturn(null);
+
+        $asset->getCode()->willReturn('code');
+        $asset->getDescription()->willReturn('');
+        $asset->getEndOfUseAt()->willReturn(null);
+        $asset->isLocalizable()->willReturn(false);
+        $asset->getTagCodes()->willReturn([]);
+        $asset->getCategoryCodes()->willReturn([]);
+
+        $this->normalize($asset)->shouldReturn([
+            'code'        => 'code',
+            'localized'   => false,
+            'description' => null,
+            'end_of_use'  => null,
+            'tags'        => [],
+            'categories'  => []
+        ]);
+    }
+
+    function it_normalizes_asset($dateTimeNormalizer, AssetInterface $asset)
+    {
+        $date = new \DateTime('2016-08-22 15:54:22');
+        $dateTimeNormalizer->normalize($date, 'standard', [])->willReturn('2016-08-22T15:54:22+01:00');
+
+        $asset->getCode()->willReturn('code');
+        $asset->getDescription()->willReturn('my description');
+        $asset->getEndOfUseAt()->willReturn($date);
+        $asset->isLocalizable()->willReturn(true);
+        $asset->getTagCodes()->willReturn(['cats', 'dogs']);
+        $asset->getCategoryCodes()->willReturn(['pets']);
+
+        $this->normalize($asset)->shouldReturn([
+            'code'        => 'code',
+            'localized'   => true,
+            'description' => 'my description',
+            'end_of_use'  => '2016-08-22T15:54:22+01:00',
+            'tags'        => ['cats', 'dogs'],
+            'categories'  => ['pets']
+        ]);
+    }
+}
---
+++ src/PimEnterprise/Component/ProductAsset/Normalizer/Standard/AssetNormalizer.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2016 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Component\ProductAsset\Normalizer\Standard;
+
+use PimEnterprise\Component\ProductAsset\Model\AssetInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * @author Marie Bochu <marie.bochu@akeneo.com>
+ */
+class AssetNormalizer implements NormalizerInterface
+{
+    /** @var NormalizerInterface */
+    protected $datetimeNormalizer;
+
+    /**
+     * @param NormalizerInterface $datetimeNormalizer
+     */
+    public function __construct(NormalizerInterface $datetimeNormalizer)
+    {
+        $this->datetimeNormalizer = $datetimeNormalizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($asset, $format = null, array $context = [])
+    {
+        return [
+            'code'        => $asset->getCode(),
+            'localized'   => (bool) $asset->isLocalizable(),
+            'description' => '' === $asset->getDescription() ? null : $asset->getDescription(),
+            'end_of_use'  => $this->datetimeNormalizer->normalize($asset->getEndOfUseAt(), 'standard', $context),
+            'tags'        => $asset->getTagCodes(),
+            'categories'  => $asset->getCategoryCodes(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof AssetInterface && 'standard' === $format;
+    }
+}
