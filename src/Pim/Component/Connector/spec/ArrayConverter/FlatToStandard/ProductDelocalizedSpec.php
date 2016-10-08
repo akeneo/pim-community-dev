46f0d938215d45c8883fc70e8660f40f28c6e5af
skeleton <marie.bochu@akeneo.com>
2016-10-08 11:40:57 +0200

    TIP-496: send only product values to localizer

--- src/Pim/Component/Connector/spec/ArrayConverter/FlatToStandard/ProductDelocalizedSpec.php
+++ src/Pim/Component/Connector/spec/ArrayConverter/FlatToStandard/ProductDelocalizedSpec.php
@@ -17,14 +17,15 @@ class ProductDelocalizedSpec extends ObjectBehavior
 
     function it_converts($converter, $delocalizer, ConstraintViolationListInterface $violations)
     {
-        $converter->convert(['the item'], ['the options'])->willReturn(['the item standardized']);
+        $converter->convert(['values' => ['the item']], ['the options'])
+            ->willReturn(['values' => ['the item standardized']]);
         $delocalizer->convertToDefaultFormats(['the item standardized'], ['the options'])
             ->willReturn(['the item standardized and delocalized']);
 
         $delocalizer->getViolations()->willReturn($violations);
         $violations->count()->willReturn(0);
 
-        $this->convert(['the item'], ['the options'])->shouldReturn(['the item standardized and delocalized']);
+        $this->convert(['values' => ['the item']], ['the options'])->shouldReturn(['values' => ['the item standardized and delocalized']]);
     }
 
     function it_throws_an_exception_in_case_of_conversion_error(
