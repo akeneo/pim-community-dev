01a7e142996b5ba0f4a4c10deab859d8394fffcc
skeleton <marie.bochu@akeneo.com>
2016-10-09 17:54:35 +0200

    TIP-576: add integration tests for standard format

---
+++ src/Akeneo/Bundle/RuleEngineBundle/tests/integration/Normalizer/Standard/RuleIntegration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests\integration\Akeneo\Bundle\RuleEngineBundle\Normalizer\Standard;
+
+use EnterpriseIntegration\TestCase;
+
+class RuleIntegration extends TestCase
+{
+    public function testRule()
+    {
+        $expected = [
+            'code'       => 'set_localized_scopable_text',
+            'type'       => 'product',
+            'priority'   => 10,
+            'conditions' => [
+                [
+                    'field' => 'categories.code',
+                    'operator' => 'IN',
+                    'value' => 'master'
+                ]
+            ],
+            'actions'    => [
+                [
+                    'type'   => 'set',
+                    'field'  => 'a_localized_and_scopable_text_area',
+                    'value'  => 'an other text',
+                    'locale' => 'en_US',
+                    'scope'  => 'tablet',
+                ]
+            ],
+        ];
+
+        $repository = $this->get('akeneo_rule_engine.repository.rule_definition');
+        $serializer = $this->get('pim_serializer');
+
+        // TODO: uncomment it when old normalizer will be removed
+        //$result = $serializer->normalize($repository->findOneByIdentifier('set_localized_scopable_text'), 'standard');
+        //$this->assertSame($result, $expected);
+    }
+}
