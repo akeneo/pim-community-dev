7057d57eece7cb4e7fd0815dd35a12a24b2974d0
Adrien Pétremann <petremann.adrien@gmail.com>
2016-10-14 09:40:20 +0200

    Preparing version 1.5.10

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.9';
+    const VERSION = '1.5.10';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
