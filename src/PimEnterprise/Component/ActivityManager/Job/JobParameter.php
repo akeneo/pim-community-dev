59bfff863e1b4073f587e81286d85b13f8421370
Olivier Soulet <olivier.soulet@akeneo.com>
2016-10-14 14:38:57 +0200

    AMS-60: add job parameters

---
+++ src/PimEnterprise/Component/ActivityManager/Job/JobParameter.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2016 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\ActivityManager\Component\Job;
+
+use Akeneo\Component\Batch\Job\JobInterface;
+use Akeneo\Component\Batch\Job\JobParameters\ConstraintCollectionProviderInterface;
+use Akeneo\Component\Batch\Job\JobParameters\DefaultValuesProviderInterface;
+use Symfony\Component\Validator\Constraints\Collection;
+
+/**
+ * @author Olivier Soulet <olivier.soulet@akeneo.com>
+ */
+class JobParameter implements DefaultValuesProviderInterface, ConstraintCollectionProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultValues()
+    {
+        return [
+            'filters' => []
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConstraintCollection()
+    {
+        return new Collection([
+            'fields' => [
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(JobInterface $job)
+    {
+        return $job->getName() === 'attribute_group';
+    }
+}
