8c8d59400f5d100b123a58d8e50673d0dbd3cb66
willy-ahva <willy.mesnage@akeneo.com>
2016-10-17 11:25:06 +0200

    AMS-10: Make the create button from view selector extensible

--- features/Context/DataGridContext.php
+++ features/Context/DataGridContext.php
@@ -1019,7 +1019,7 @@ class DataGridContext extends RawMinkContext implements PageObjectAwareInterface
     public function iCreateTheView(TableNode $table)
     {
         $this->getCurrentPage()->getViewSelector()->click();
-        $this->getCurrentPage()->clickCreateOnButton("Create view");
+        $this->getCurrentPage()->clickOnCreateButton("Create view");
 
         return [
             new Step\Then('I fill in the following information in the popin:', $table),
