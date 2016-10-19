fa03e56d95896acbb5f0c3b78d10b6fde7b3819e
Damien Carcel <damien.carcel@akeneo.com>
2016-10-19 16:44:12 +0200

    Fix product filtering scenarios

--- features/Context/DataGridContext.php
+++ features/Context/DataGridContext.php
@@ -530,10 +530,8 @@ class DataGridContext extends RawMinkContext implements PageObjectAwareInterface
             }
             $filter = $item['filter'];
             $isCategoryFilter = false !== strpos(strtolower($filter), 'category');
-            $countBeforeFilter = null;
 
             if (!$isCategoryFilter) {
-                $countBeforeFilter = $this->datagrid->getToolbarCount();
                 $steps[] = new Step\Then(sprintf('I show the filter "%s"', $filter));
             }
             $steps[] = new Step\Then(sprintf(
@@ -549,23 +547,6 @@ class DataGridContext extends RawMinkContext implements PageObjectAwareInterface
             }
             if (!$isCategoryFilter) {
                 $steps[] = new Step\Then(sprintf('I hide the filter "%s"', $filter));
-                if (null !== $countBeforeFilter) {
-                    /**
-                     * At the end of the loop, we ensure we get back to the initial state of the loop.
-                     * To do this, we add a validation step checking if the number of initial elements in the datagrid
-                     * is the same than the number of elements in the datagrid after removing the filter.
-                     *
-                     * This has a second effect: it ensure the fact that the datagrid is well loaded. It fixes some
-                     * random features, where Metric and Price filters were closed during behat execution, when the
-                     * datagrid finished to be load.
-                     *
-                     * // TODO Refactor this using a comparison with content of the datagrid.
-                     */
-                    $steps[] = new Step\Then(sprintf(
-                        'the grid toolbar count should be %s elements',
-                        $countBeforeFilter
-                    ));
-                }
             }
         }
 
