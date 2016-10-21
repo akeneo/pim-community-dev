017588fd83aaba0045974b3e1614a968f7f69058
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2016-10-21 15:38:52 +0200

    TIP-607:  Fix media fied update

---
+++ src/Pim/Component/Catalog/tests/integration/Updater/Setter/MediaAttributeSetterIntegration.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace tests\integration\Pim\Component\Catalog\Updater\Setter;
+
+use Test\Integration\TestCase;
+
+/**
+ * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MediaAttributeSetterIntegration extends TestCase
+{
+    const MEDIA_ATTRIBUTE_DATA_PATTERN = '#[0-9a-z]/[0-9a-z]/[0-9a-z]/[0-9a-z]/[0-9a-z]{40}_\w+\.[a-zA-Z]+$#';
+    const MEDIA_ATTRIBUTE_DATA_COMPARISON = 'this is a media identifier';
+
+    protected $purgeDatabaseForEachTest = false;
+
+    protected function assertCommandMedia(array $parameters, array $result, $attributeName)
+    {
+        $productUpdater = $this->get('pim_catalog.updater.product');
+
+        $product = $this->get('pim_catalog.builder.product')->createProduct('product_media');
+        $productUpdater->update($product, $parameters);
+
+        $this->get('pim_catalog.saver.product')->save($product);
+
+        $standardProduct = $this->get('pim_serializer')->normalize($product, 'standard');
+
+        $result = $this->sanitizeMediaAttributeData($result);
+        $standardValues = $this->sanitizeMediaAttributeData($standardProduct['values'][$attributeName]);
+
+        $this->assertEquals($result, $standardValues);
+    }
+
+    public function testLocalisableMedia()
+    {
+        $attributeName = 'a_localizable_image';
+
+        $parameters = [
+            'values' => [
+                $attributeName => [
+                    [
+                        'data'   => $this->getParameter('kernel.root_dir').'/../features/Context/fixtures/SNKRS-1R.png',
+                        'locale' => 'fr_FR',
+                        'scope'  => null,
+                    ],
+                ],
+            ],
+        ];
+
+        $result = [
+            [
+                'locale' => 'fr_FR',
+                'scope'  => null,
+                'data'   => 'd/5/e/1/d5e1aeb5149a8a721e567952c895d20ffef8c6d9_SNKRS_1R.png',
+            ],
+        ];
+
+        $this->assertCommandMedia($parameters, $result, $attributeName);
+    }
+
+    public function testMediaWithChannel()
+    {
+        $attributeName = 'a_scopable_image';
+
+        $parameters = [
+            'values' => [
+                $attributeName => [
+                    [
+                        'data'   => $this->getParameter('kernel.root_dir').'/../features/Context/fixtures/SNKRS-1C-t.png',
+                        'locale' => null,
+                        'scope'  => 'tablet',
+                    ],
+                ],
+            ],
+        ];
+
+        $result = [
+            [
+                'locale' => null,
+                'scope'  => 'tablet',
+                'data'   => 'd/5/e/1/d5e1aeb5149a8a721e567952c895d20ffef8c6d9_SNKRS_1R.png',
+            ],
+        ];
+
+        $this->assertCommandMedia($parameters, $result, $attributeName);
+    }
+
+    public function testMediaWithLocaleAndChannel()
+    {
+        $attributeName = 'a_localizable_scopable_image';
+
+        $parameters = [
+            'values' => [
+                $attributeName => [
+                    [
+                        'data'   => $this->getParameter('kernel.root_dir').'/../features/Context/fixtures/SNKRS-1R.png',
+                        'locale' => 'fr_FR',
+                        'scope'  => 'tablet',
+                    ],
+                ],
+            ],
+        ];
+
+        $result = [
+            [
+                'locale' => 'fr_FR',
+                'scope'  => 'tablet',
+                'data'   => 'd/5/e/1/d5e1aeb5149a8a721e567952c895d20ffef8c6d9_SNKRS_1R.png',
+            ],
+        ];
+
+        $this->assertCommandMedia($parameters, $result, $attributeName);
+    }
+
+    public function estSameMedia()
+    {
+
+        $attributeName = 'a_localizable_scopable_image';
+
+        $parameters = [
+            'values' => [
+                $attributeName => [
+                    [
+                        'data'   => $this->getParameter('kernel.root_dir').'/../features/Context/fixtures/SNKRS-1R.png',
+                        'locale' => 'fr_FR',
+                        'scope'  => 'tablet',
+                    ],
+                ],
+                $attributeName => [
+                    [
+                        'data'   => $this->getParameter('kernel.root_dir').'/../features/Context/fixtures/SNKRS-1R.png',
+                        'locale' => 'fr_FR',
+                        'scope'  => 'tablet',
+                    ],
+                ],
+            ],
+        ];
+
+        $result = [
+            [
+                'locale' => 'fr_FR',
+                'scope'  => 'tablet',
+                'data'   => 'd/5/e/1/d5e1aeb5149a8a721e567952c895d20ffef8c6d9_SNKRS_1R.png',
+            ],
+        ];
+
+        $this->assertCommandMedia($parameters, $result, $attributeName);
+    }
+
+
+    /**
+     * Replaces media attributes data in the $data array by self::MEDIA_ATTRIBUTE_DATA_COMPARISON.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    private function sanitizeMediaAttributeData(array $data)
+    {
+        print_r($data);
+        foreach ($data as $index => $value) {
+            if ($this->assertMediaAttributeDataPattern($value['data'])) {
+                $data[$index]['data'] = self::MEDIA_ATTRIBUTE_DATA_COMPARISON;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return bool
+     */
+    protected function assertMediaAttributeDataPattern($data)
+    {
+        return 1 === preg_match(self::MEDIA_ATTRIBUTE_DATA_PATTERN, $data);
+    }
+}
