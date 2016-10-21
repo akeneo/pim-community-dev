f2139e59ab59cb7624cbda8af1c5d8b2e20bee13
aRn0D <arnaud.langlade@akeneo.com>
2016-10-21 12:24:23 +0200

    AMS-61: add datagrid updater

---
+++ src/Pim/Bundle/DataGridBundle/Updater/DatagridViewUpdater.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Pim\Bundle\DataGridBundle\Updater;
+
+use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Doctrine\Common\Util\ClassUtils;
+use Pim\Bundle\DataGridBundle\Entity\DatagridView;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+/**
+ * Update the datagrid view properties
+ *
+ * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class DatagridViewUpdater implements ObjectUpdaterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function update($datagridView, array $data, array $options = [])
+    {
+        if (!$datagridView instanceof DatagridView) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Expects a "%s", "%s" provided.',
+                    DatagridView::class,
+                    ClassUtils::getClass($datagridView)
+                )
+            );
+        }
+
+        foreach ($data as $field => $value) {
+            $this->setData($datagridView, $field, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set the value to an object property
+     *
+     * @param DatagridView $datagridView
+     * @param string       $field
+     * @param mixed        $value
+     */
+    protected function setData(DatagridView $datagridView, $field, $value)
+    {
+        switch ($field) {
+            case 'label':
+                $datagridView->setLabel($value);
+                break;
+            case 'owner':
+                $datagridView->setOwner($value);
+                break;
+            case 'datagrid_alias':
+                $datagridView->setDatagridAlias($value);
+                break;
+            case 'type':
+                $datagridView->setType($value);
+                break;
+            case 'columns':
+                $datagridView->setColumns(array_map('trim', explode(',', $value)));
+                break;
+            case 'filters':
+                $datagridView->setFilters($value);
+                break;
+        }
+    }
+}
