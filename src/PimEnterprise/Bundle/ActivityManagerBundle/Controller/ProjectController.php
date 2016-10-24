18a13c97936ff1afda28c01d1ec09f028452cba5
Olivier Soulet <olivier.soulet@akeneo.com>
2016-10-24 17:58:40 +0200

    AMS-64: Pass filters to the job
    It passes the filters to the job process

--- src/PimEnterprise/Bundle/ActivityManagerBundle/Controller/ProjectController.php
+++ src/PimEnterprise/Bundle/ActivityManagerBundle/Controller/ProjectController.php
@@ -11,7 +11,7 @@
 
 namespace Akeneo\ActivityManager\Bundle\Controller;
 
-use Akeneo\ActivityManager\Component\Job\ProjectCalculationJobParameters;
+use PimEnterprise\Bundle\DataGridBundle\Adapter\OroToPimGridFilterAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,6 +42,11 @@ class ProjectController extends Controller
     public function createAction(Request $request)
     {
         $projectData = $request->request->get('project');
+        $projectData['owner'] = $this->getUser();
+        parse_str($projectData['datagrid_view']['filters'], $output);
+        $filters = json_encode($output['f']);
+
+        $this->addParametersToRequest($request, $filters);
 
         $project = $this->container->get('activity_manager.factory.project')
             ->create();
@@ -56,7 +61,7 @@ class ProjectController extends Controller
             $this->container->get('activity_manager.saver.project')
                 ->save($project);
 
-            $this->test($request, $project->getId());
+            $this->executeJob($request, $project->getId());
 
             $normalizedProject = $this->container->get('activity_manager.normalizer.project')
                 ->normalize($project, 'internal_api');
@@ -76,21 +81,38 @@ class ProjectController extends Controller
     }
 
     /**
-     * TODO: Will be reworked to add filters and maybe moved somewhere else.
-     *
      * @param Request $request
-     * @param         $id
+     * @param string  $id
      */
-    private function test(Request $request, $id)
+    private function executeJob(Request $request, $id)
     {
         $simpleJobLauncher = $this->container->get('akeneo_batch.launcher.simple_job_launcher');
         $jobInstanceRepo = $this->container->get('pim_import_export.repository.job_instance');
+        $filterAdapter = $this->container->get('pim_datagrid.adapter.oro_to_pim_grid_filter');
 
-        // TODO: add filters
-        $params['values'] = [];
-        $jobInstance = $jobInstanceRepo->findOneByIdentifier(ProjectCalculationJobParameters::JOB_NAME);
-        $configuration = ['filters' => $params['values'], 'project_id' => $id];
+        $filters = $filterAdapter->adapt($request);
+
+        $jobInstance = $jobInstanceRepo->findOneByIdentifier('project_calculation');
+        $configuration = ['filters' => $filters, 'project_id' => $id];
 
         $simpleJobLauncher->launch($jobInstance, $this->getUser(), $configuration);
+    }
+
+    /**
+     * It adds values to the request as it's needed by the adapter to transform oro grid filters into PQB filter.
+     *
+     * @param Request $request
+     * @param string  $filters
+     */
+    private function addParametersToRequest(Request $request, $filters)
+    {
+        $request->query->add(
+            [
+                'gridName'   => OroToPimGridFilterAdapter::PRODUCT_GRID_NAME,
+                'actionName' => 'mass_edit', //Fake mass action, needed for the grid filter adapter.
+                'inset'      => false,
+                'filters'    => $filters,
+            ]
+        );
     }
 }
