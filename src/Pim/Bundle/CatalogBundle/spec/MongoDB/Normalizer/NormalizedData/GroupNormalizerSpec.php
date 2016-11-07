7e968b2a534f7a3751c53167f55e6b32db6c0b51
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2016-11-07 09:51:15 +0100

    TIP-475: Fix Group normalization to MongoDB

--- src/Pim/Bundle/CatalogBundle/spec/MongoDB/Normalizer/NormalizedData/GroupNormalizerSpec.php
+++ src/Pim/Bundle/CatalogBundle/spec/MongoDB/Normalizer/NormalizedData/GroupNormalizerSpec.php
@@ -30,11 +30,11 @@ class GroupNormalizerSpec extends ObjectBehavior
         GroupInterface $group
     ) {
         $group->getCode()->willReturn('mongo');
-        $normalizer->normalize($group, 'mongodb_json', [])->willReturn(['label' => 'translations']);
+        $normalizer->normalize($group, 'mongodb_json', [])->willReturn(['en_US' => 'US Label']);
 
         $this->normalize($group, 'mongodb_json', [])->shouldReturn([
-            'code' => 'mongo',
-            'label' => 'translations'
+            'code'   => 'mongo',
+            'labels' => ['en_US' => 'US Label']
         ]);
     }
 }
