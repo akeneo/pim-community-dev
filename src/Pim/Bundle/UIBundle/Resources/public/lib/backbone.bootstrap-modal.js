a03c441b12f3a8e50be1f69c0b2acea90ca224fd
Pierre Allard <pierallard@gmail.com>
2016-11-30 10:53:28 +0100

    [TIP-645] Remove btn classes for import export

--- src/Pim/Bundle/UIBundle/Resources/public/lib/backbone.bootstrap-modal.js
+++ src/Pim/Bundle/UIBundle/Resources/public/lib/backbone.bootstrap-modal.js
@@ -31,13 +31,19 @@
       </div>\
     <% } %>\
     <div class="modal-body">{{content}}</div>\
-    <div class="modal-footer">\
+    <div class="AknButtonsList AknButtonsList--withSpace AknButtonsList--right modal-footer">\
       <% if (allowCancel) { %>\
         <% if (cancelText) { %>\
-          <a href="#" class="btn cancel icons-holder-text"><i class="icon-chevron-left"></i>{{cancelText}}</a>\
+          <a href="#" class="AknButtonsList-item AknButton AknButton--withIcon AknButton--grey cancel icons-holder-text">\
+            <i class="AknButton-icon icon-chevron-left"></i>\
+            {{cancelText}}\
+          </a>\
         <% } %>\
       <% } %>\
-      <a href="#" class="btn btn-primary ok icons-holder-text"><i class="icon-ok"></i>{{okText}}</a>\
+      <a href="#" class="AknButtonsList-item AknButton AknButton--withIcon AknButton--apply ok icons-holder-text">\
+        <i class="AknButton-icon icon-ok"></i>\
+        {{okText}}\
+      </a>\
     </div>\
   ');
 
