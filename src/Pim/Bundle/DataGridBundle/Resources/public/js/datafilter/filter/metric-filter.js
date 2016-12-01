762b855aa8a2441117d90004e9667abaf1ae35b8
pierallard <pierallard@gmail.com>
2016-12-01 12:08:43 +0100

    [TIP-645] Add metric filter as template

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datafilter/filter/metric-filter.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datafilter/filter/metric-filter.js
@@ -1,6 +1,12 @@
 define(
-    ['jquery', 'underscore', 'oro/datafilter/number-filter', 'oro/app'],
-    function ($, _, NumberFilter, app) {
+    [
+        'jquery',
+        'underscore',
+        'oro/datafilter/number-filter',
+        'oro/app',
+        'text!pim/template/datagrid/filter/metric-filter'
+    ],
+    function ($, _, NumberFilter, app, template) {
         'use strict';
 
         /**
@@ -88,38 +94,7 @@ define(
             /**
              * @inheritDoc
              */
-            popupCriteriaTemplate: _.template(
-                '<div class="AknFilterMetric metricfilter choicefilter">' +
-                    '<div class="AknDropdown btn-group">' +
-                        '<button class="AknActionButton AknActionButton--noRightBorder dropdown-toggle" data-toggle="dropdown">' +
-                            '<%= _.__("Action") %>' +
-                            '<span class="AknActionButton-caret caret"></span>' +
-                        '</button>' +
-                        '<ul class="dropdown-menu">' +
-                            '<% _.each(choices, function (choice) { %>' +
-                                '<li><a class="choice_value" href="#" data-value="<%= choice.value %>"><%= choice.label %></a></li>' +
-                            '<% }); %>' +
-                        '</ul>' +
-                        '<input class="name_input" type="hidden" name="metric_type" value=""/>' +
-                    '</div>' +
-
-                    '<input class="AknFilterMetric-field" type="text" name="value" value="">' +
-
-                    '<div class="AknDropdown">' +
-                        '<button class="AknActionButton AknActionButton--noRightBorder AknActionButton--noLeftBorder dropdown-toggle" data-toggle="dropdown">' +
-                            '<%= _.__("Unit") %>' +
-                            '<span class="AknActionButton-caret caret"></span>' +
-                        '</button>' +
-                        '<ul class="dropdown-menu">' +
-                            '<% _.each(units, function (symbol, code) { %>' +
-                                '<li><a class="choice_value" href="#" data-value="<%= code %>"><%= _.__(code) %></a></li>' +
-                            '<% }); %>' +
-                        '</ul>' +
-                        '<input class="name_input" type="hidden" name="metric_unit" value=""/>' +
-                    '</div>' +
-                    '<button class="AknButton AknButton--apply AknButton--little AknButton--noLeftRadius filter-update" type="button"><%= _.__("Update") %></button>' +
-                '</div>'
-            ),
+            popupCriteriaTemplate: _.template(template),
 
             /**
              * Selectors for filter criteria elements
