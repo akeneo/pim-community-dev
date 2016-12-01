bd27d721c24ecdac2f1faa088bf6cdd1294263ec
jean-marie leroux <jmleroux.pro@gmail.com>
2016-12-01 10:47:49 +0100

    Family classes to interfaces (#5175)

--- src/Akeneo/Bundle/MeasureBundle/Family/DecibelFamilyInterface.php
+++ src/Akeneo/Bundle/MeasureBundle/Family/DecibelFamilyInterface.php
@@ -9,7 +9,7 @@ namespace Akeneo\Bundle\MeasureBundle\Family;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DecibelFamilyInterface
+interface DecibelFamilyInterface
 {
     /**
      * Family measure name
