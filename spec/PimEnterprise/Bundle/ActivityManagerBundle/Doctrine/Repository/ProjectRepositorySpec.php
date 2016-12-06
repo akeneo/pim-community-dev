0a1f67a0048ee3b417b9c775597d03ea2d650c93
Olivier Soulet <olivier.soulet@akeneo.com>
2016-12-06 17:57:12 +0100

    AMS-83: add missing spec

--- spec/PimEnterprise/Bundle/ActivityManagerBundle/Doctrine/Repository/ProjectRepositorySpec.php
+++ spec/PimEnterprise/Bundle/ActivityManagerBundle/Doctrine/Repository/ProjectRepositorySpec.php
@@ -5,6 +5,7 @@ namespace spec\Akeneo\ActivityManager\Bundle\Doctrine\Repository;
 use Akeneo\ActivityManager\Bundle\Doctrine\Repository\ProjectRepository;
 use Akeneo\ActivityManager\Component\Repository\ProjectRepositoryInterface;
 use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Component\StorageUtils\Repository\SearchableRepositoryInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use PhpSpec\ObjectBehavior;
@@ -31,6 +32,11 @@ class ProjectRepositorySpec extends ObjectBehavior
     function it_is_an_object_identifiable_repository()
     {
         $this->shouldImplement(IdentifiableObjectRepositoryInterface::class);
+    }
+
+    function it_is_a_searchable_repository()
+    {
+        $this->shouldImplement(SearchableRepositoryInterface::class);
     }
 
     function its_identifier_is_id()
