87da11b556cdbceee22b9540b445a5230aaf9966
Damien Carcel <damien.carcel@akeneo.com>
2016-12-06 17:54:39 +0100

    PIM-6018: Import identifier attribute only if useble as grid filter If not, violation will be raised and identifier attribute will be skipped

---
+++ src/Pim/Component/Catalog/spec/Validator/Constraints/IsIdentifierUsableAsGridFilterSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace spec\Pim\Component\Catalog\Validator\Constraints;
+
+use Pim\Component\Catalog\Validator\Constraints\IsIdentifierUsableAsGridFilter;
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Validator\Constraint;
+
+class IsIdentifierUsableAsGridFilterSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(IsIdentifierUsableAsGridFilter::class);
+    }
+
+    function it_has_message()
+    {
+        $this->message->shouldBe('"%code%" is an identifier attribute, it must be usable as grid filter');
+    }
+
+    function it_is_a_validator_constraint()
+    {
+        $this->shouldBeAnInstanceOf(Constraint::class);
+    }
+}
