0b2ef0c6889903fb29fa217baae3369fa016b96d
Damien Carcel <damien.carcel@akeneo.com>
2016-12-13 10:30:30 +0100

    Preparing version 1.4.28

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.4.27';
+    const VERSION = '1.4.28';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rhapsody Rabbit';
