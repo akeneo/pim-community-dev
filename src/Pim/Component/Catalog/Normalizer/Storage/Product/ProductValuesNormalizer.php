3ed09ddeba672990e6c2c657845aaf557ee9cd34
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2016-12-19 14:58:26 +0100

    Normalizer the values to be able to query products from ES

--- src/Pim/Component/Catalog/Normalizer/Storage/Product/ProductValuesNormalizer.php
+++ src/Pim/Component/Catalog/Normalizer/Storage/Product/ProductValuesNormalizer.php
@@ -23,6 +23,21 @@ class ProductValuesNormalizer extends SerializerAwareNormalizer implements Norma
     {
         $result = [];
 
+        $esMappingSuffixes = [
+             'pim_catalog_identifier' => '-ident-',
+             'pim_catalog_text' => '-text-',
+             'pim_catalog_textarea' => '-text_area-',
+             'pim_catalog_metric' => '-metric-',
+             'pim_catalog_boolean' => '-bool-',
+             'pim_catalog_simpleselect' => '-option-',
+             'pim_catalog_number' => '-number-',
+             'pim_catalog_multiselect' => '-options-',
+             'pim_catalog_date' => '-date-',
+             'pim_catalog_price_collection' => '-prices-',
+             'pim_catalog_image' => '-media-',
+             'pim_catalog_file' => '-media-',
+        ];                              
+        
         foreach ($data as $value) {
             if (!$value instanceof ProductValueInterface) {
                 throw new \InvalidArgumentException('This normalizer only handles "Pim\Component\Catalog\Model\ProductValueInterface".');
@@ -30,11 +45,13 @@ class ProductValuesNormalizer extends SerializerAwareNormalizer implements Norma
 
             $stdValue = $this->serializer->normalize($value, 'standard', $context);
 
-            $attribute = $value->getAttribute()->getCode();
+            $attribute = $value->getAttribute();
             $channel = null !== $stdValue['scope'] ? $stdValue['scope']->getCode() : '<all_channels>';
             $locale = null !== $stdValue['locale'] ? $stdValue['locale']->getCode() : '<all_locales>';
 
-            $result[$attribute][$channel][$locale] = $stdValue['data'];
+            $attributeCode = $attribute->getCode() . $esMappingSuffixes[$attribute->getAttributeType()];
+
+            $result[$attributeCode][$channel][$locale] = $stdValue['data'];
         }
 
         return $result;
