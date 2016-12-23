cf39333a1ed441472a96db9d9d9fb651a58899be
Samir <samir.boulil@gmail.com>
2016-12-23 15:21:10 +0100

    Add missing php file copyright (#2336)

--- src/PimEnterprise/Bundle/ProductAssetBundle/EventSubscriber/ORM/RemoveChannelSubscriber.php
+++ src/PimEnterprise/Bundle/ProductAssetBundle/EventSubscriber/ORM/RemoveChannelSubscriber.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2016 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PimEnterprise\Bundle\ProductAssetBundle\EventSubscriber\ORM;
 
 use Akeneo\Component\StorageUtils\Remover\RemoverInterface;
