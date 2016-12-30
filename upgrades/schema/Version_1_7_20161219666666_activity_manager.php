75934c9511335b8ec5b79badf7f9372fdc014151
Nicolas Dupont <nicolas@akeneo.com>
2016-12-30 10:39:46 +0100

    Rename activity manager tables for consistency

--- upgrades/schema/Version_1_7_20161219666666_activity_manager.php
+++ upgrades/schema/Version_1_7_20161219666666_activity_manager.php
@@ -29,7 +29,7 @@ class Version_1_7_20161219666666_activity_manager extends AbstractMigration impl
         $storage = $this->container->getParameter('pim_catalog_product_storage_driver');
         if (AkeneoStorageUtilsExtension::DOCTRINE_ORM === $storage) {
             $sql = <<<'SQL'
-CREATE TABLE `akeneo_activity_manager_completeness_per_attribute_group` (
+CREATE TABLE `pimee_activity_manager_completeness_per_attribute_group` (
     `locale_id` INT NOT NULL,
     `channel_id` INT NOT NULL, 
     `product_id` INT NOT NULL, 
@@ -40,29 +40,29 @@ CREATE TABLE `akeneo_activity_manager_completeness_per_attribute_group` (
     PRIMARY KEY(`locale_id`, `channel_id`, `product_id`, `attribute_group_id`)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group`
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC7820E559DFD1 FOREIGN KEY (`locale_id`) REFERENCES `pim_catalog_locale` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC782072F5A1AA FOREIGN KEY (`channel_id`) REFERENCES `pim_catalog_channel` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC78204584665A FOREIGN KEY (`product_id`) REFERENCES `pim_catalog_product` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC782062D643B7 FOREIGN KEY (`attribute_group_id`) REFERENCES `pim_catalog_attribute_group` (id);
 
-CREATE TABLE `akeneo_activity_manager_project_product` (
+CREATE TABLE `pimee_activity_manager_project_product` (
   `project_id` INT NOT NULL,
   `product_id` INT NOT NULL,
   PRIMARY KEY (`project_id`,`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `akeneo_activity_manager_project_product`
-    ADD CONSTRAINT FK_E004BCB8166D1F9C FOREIGN KEY (`project_id`) REFERENCES `akeneo_activity_manager_project` (id);
-ALTER TABLE `akeneo_activity_manager_project_product`
+ALTER TABLE `pimee_activity_manager_project_product`
+    ADD CONSTRAINT FK_E004BCB8166D1F9C FOREIGN KEY (`project_id`) REFERENCES `pimee_activity_manager_project` (id);
+ALTER TABLE `pimee_activity_manager_project_product`
     ADD CONSTRAINT FK_E004BCB84584665A FOREIGN KEY (`product_id`) REFERENCES `pim_catalog_product` (id);
 SQL;
         } else {
             $sql = <<<'SQL'
-CREATE TABLE `akeneo_activity_manager_completeness_per_attribute_group` (
+CREATE TABLE `pimee_activity_manager_completeness_per_attribute_group` (
     `locale_id` INT NOT NULL,
     `channel_id` INT NOT NULL, 
     `product_id` VARCHAR(36) NOT NULL, 
@@ -73,24 +73,24 @@ CREATE TABLE `akeneo_activity_manager_completeness_per_attribute_group` (
     PRIMARY KEY(`locale_id`, `channel_id`, `product_id`, `attribute_group_id`)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group`
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC7820E559DFD1 FOREIGN KEY (`locale_id`) REFERENCES `pim_catalog_locale` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC782072F5A1AA FOREIGN KEY (`channel_id`) REFERENCES `pim_catalog_channel` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT FK_BEBC782062D643B7 FOREIGN KEY (`attribute_group_id`) REFERENCES `pim_catalog_attribute_group` (id);
-ALTER TABLE `akeneo_activity_manager_completeness_per_attribute_group` 
+ALTER TABLE `pimee_activity_manager_completeness_per_attribute_group`
     ADD CONSTRAINT completeness_fake_product_id_foreign_key UNIQUE (`locale_id`, `channel_id`, `product_id`, `attribute_group_id`);
 
-CREATE TABLE `akeneo_activity_manager_project_product` (
+CREATE TABLE `pimee_activity_manager_project_product` (
   `project_id` INT NOT NULL,
   `product_id` VARCHAR(36) NOT NULL,
   PRIMARY KEY (`project_id`,`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `akeneo_activity_manager_project_product`
-    ADD CONSTRAINT FK_E004BCB8166D1F9C FOREIGN KEY (`project_id`) REFERENCES `akeneo_activity_manager_project` (id);
-ALTER TABLE `akeneo_activity_manager_project_product` 
+ALTER TABLE `pimee_activity_manager_project_product`
+    ADD CONSTRAINT FK_E004BCB8166D1F9C FOREIGN KEY (`project_id`) REFERENCES `pimee_activity_manager_project` (id);
+ALTER TABLE `pimee_activity_manager_project_product`
     ADD CONSTRAINT project_fake_product_id_foreign_key UNIQUE (`project_id`, `product_id`)
 SQL;
         }
