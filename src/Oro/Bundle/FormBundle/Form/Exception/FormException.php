31f07f747019ba9ad4980ab0b3e56cd4ef09a3ae
Nicolas Dupont <nicolas@akeneo.com>
2016-12-31 18:36:58 +0100

    Remove OroFormBundle

--- src/Oro/Bundle/FormBundle/Form/Exception/FormException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\FormBundle\Form\Exception;
-
-use Symfony\Component\Form\Exception\ExceptionInterface;
-
-class FormException extends \Exception implements ExceptionInterface
-{
-}
