5b163dbc11000be452ed17a2a9742023c5dad3be
Damien Carcel <damien.carcel@akeneo.com>
2017-01-03 12:02:33 +0100

    TIP-670: Update MetricFactory to use reworked Metric class

--- src/Pim/Component/Catalog/Factory/MetricFactory.php
+++ src/Pim/Component/Catalog/Factory/MetricFactory.php
@@ -2,8 +2,13 @@
 
 namespace Pim\Component\Catalog\Factory;
 
+use Akeneo\Bundle\MeasureBundle\Convert\MeasureConverter;
+use Akeneo\Bundle\MeasureBundle\Exception\MeasureException;
+use Akeneo\Bundle\MeasureBundle\Manager\MeasureManager;
+use Pim\Component\Catalog\Model\MetricInterface;
+
 /**
- * Metric factory
+ * Creates and configures a metric instance.
  *
  * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
@@ -11,29 +16,84 @@ namespace Pim\Component\Catalog\Factory;
  */
 class MetricFactory
 {
+    /** @var MeasureConverter */
+    protected $measureConverter;
+
+    /** @var MeasureManager */
+    protected $measureManager;
+
     /** @var string */
     protected $metricClass;
 
     /**
-     * @param string $metricClass
+     * @param MeasureConverter $measureConverter
+     * @param MeasureManager   $measureManager
+     * @param string           $metricClass
      */
-    public function __construct($metricClass)
+    public function __construct(MeasureConverter $measureConverter, MeasureManager $measureManager, $metricClass)
     {
+        $this->measureConverter = $measureConverter;
+        $this->measureManager = $measureManager;
         $this->metricClass = $metricClass;
     }
 
     /**
-     * Create and configure a metric instance
+     * @param string $family
+     * @param string $unit
+     * @param double $data
+     *
+     * @return MetricInterface
+     */
+    public function createMetric($family, $unit, $data)
+    {
+        $baseData = null !== $data ? $this->convertDataToBaseData($family, $unit, $data) : null;
+        $baseUnit = $this->getBaseUnit($family);
+
+        $metric = new $this->metricClass($family, $unit, $data, $baseUnit, $baseData);
+
+        return $metric;
+    }
+
+    /**
+     * Converts the provided data to the metric base data, according to its family standard unit (base unit).
+     *
+     * @param string $family
+     * @param string $unit
+     * @param double $data
+     *
+     * @throws \LogicException
+     * @return double
+     */
+    protected function convertDataToBaseData($family, $unit, $data)
+    {
+        try {
+            $convertedData = $this->measureConverter->setFamily($family)->convertBaseToStandard($unit, $data);
+        } catch (MeasureException $e) {
+            // TODO: This should be a dedicated exception. Will be addressed in a coming PR.
+            throw new \LogicException(
+                sprintf(
+                    'Metric data %d cannot be converted to base data for unit %s and family %s',
+                    $data,
+                    $unit,
+                    $family
+                ),
+                $e->getCode(),
+                $e
+            );
+        }
+
+        return $convertedData;
+    }
+
+    /**
+     * Returns the standard unit of a metric family.
      *
      * @param string $family
      *
-     * @return \Pim\Component\Catalog\Model\MetricInterface
+     * @return string
      */
-    public function createMetric($family)
+    protected function getBaseUnit($family)
     {
-        $metric = new $this->metricClass();
-        $metric->setFamily($family);
-
-        return $metric;
+        return $this->measureManager->getStandardUnitForFamily($family);
     }
 }
