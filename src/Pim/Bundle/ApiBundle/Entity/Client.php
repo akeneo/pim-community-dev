e86ec8705858cde2bdb9440ecf9412ae17c59d1b
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2017-01-11 17:45:51 +0100

    API-48: Add a client creation command

--- src/Pim/Bundle/ApiBundle/Entity/Client.php
+++ src/Pim/Bundle/ApiBundle/Entity/Client.php
@@ -11,4 +11,22 @@ use FOS\OAuthServerBundle\Entity\Client as BaseClient;
  */
 class Client extends BaseClient
 {
+    /** @var string */
+    protected $label;
+
+    /**
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
 }
