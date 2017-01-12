6cdf24e9758d391d3c78d500cd5fd982bd7269f3
Philippe Mossière <philippe.mossiere@akeneo.com>
2017-01-12 15:17:30 +0100

    API-24: Get a single attribute

---
+++ src/Pim/Bundle/ApiBundle/tests/integration/Controller/Rest/Attribute/GetAttributeIntegration.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace tests\integration\Pim\Bundle\ApiBundle\Controller\Rest\Attribute;
+
+use Symfony\Component\HttpFoundation\Response;
+use Test\Integration\TestCase;
+
+class GetAttributeIntegration extends TestCase
+{
+    public function testGetAnAttribute()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', 'api/rest/v1/attributes/sku');
+
+        $standardAttribute = [
+            'code'                   => 'sku',
+            'type'                   => 'pim_catalog_identifier',
+            'group'                  => 'attributeGroupA',
+            'unique'                 => true,
+            'useable_as_grid_filter' => true,
+            'allowed_extensions'     => [],
+            'metric_family'          => null,
+            'default_metric_unit'    => null,
+            'reference_data_name'    => null,
+            'available_locales'      => [],
+            'max_characters'         => null,
+            'validation_rule'        => null,
+            'validation_regexp'      => null,
+            'wysiwyg_enabled'        => false,
+            'number_min'             => null,
+            'number_max'             => null,
+            'decimals_allowed'       => false,
+            'negative_allowed'       => false,
+            'date_min'               => null,
+            'date_max'               => null,
+            'max_file_size'          => null,
+            'minimum_input_length'   => 0,
+            'sort_order'             => 0,
+            'localizable'            => false,
+            'scopable'               => false,
+            'labels'                 => [],
+        ];
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertSame($standardAttribute, json_decode($response->getContent(), true));
+    }
+
+    public function testNotFoundAnAttribute()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', 'api/rest/v1/attributes/not_found');
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $content = json_decode($response->getContent(), true);
+        $this->assertCount(2, $content, 'response contains 2 items');
+        $this->assertSame(Response::HTTP_NOT_FOUND, $content['code']);
+        $this->assertSame('Attribute "not_found" does not exist.', $content['message']);
+    }
+}
