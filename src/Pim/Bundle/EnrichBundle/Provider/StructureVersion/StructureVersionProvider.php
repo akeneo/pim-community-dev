612761d35260dd8724ec473f210a3da9615c8c02
pierallard <pierallard@gmail.com>
2017-01-17 10:48:47 +0100

    [1.5->1.6] Move StructureVersionProvider [3/3]    Rename classes

--- src/Pim/Bundle/EnrichBundle/Provider/StructureVersion/StructureVersionProvider.php
+++ src/Pim/Bundle/EnrichBundle/Provider/StructureVersion/StructureVersionProvider.php
@@ -5,13 +5,13 @@ namespace Pim\Bundle\EnrichBundle\Provider\StructureVersion;
 use Pim\Bundle\VersioningBundle\Repository\VersionRepositoryInterface;
 
 /**
- * Product structure version provider
+ * Structure version provider
  *
  * @author    Julien Sanchez <julien@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductStructureVersionProvider implements StructureVersionProviderInterface
+class StructureVersionProvider implements StructureVersionProviderInterface
 {
     /** @var array */
     protected $resourceNames = [];
