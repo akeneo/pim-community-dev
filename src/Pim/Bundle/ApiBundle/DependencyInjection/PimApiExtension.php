fc86d91b9d1d8cea82b49e97850e2661d98404e9
skeleton <marie.bochu@akeneo.com>
2017-01-19 13:25:46 +0100

    API-104: change the default JMS serializer in fosrestbundle to use symfony serializer

--- src/Pim/Bundle/ApiBundle/DependencyInjection/PimApiExtension.php
+++ src/Pim/Bundle/ApiBundle/DependencyInjection/PimApiExtension.php
@@ -24,5 +24,6 @@ class PimApiExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('controllers.yml');
+        $loader->load('normalizers.yml');
     }
 }
