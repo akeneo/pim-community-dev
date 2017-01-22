7e8f8cf6286df824249e1b7f4e8344c5af5cc902
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-01-22 20:10:41 +0100

    API-19: add category creation in the web API

--- src/Pim/Bundle/ApiBundle/Controller/Rest/CategoryController.php
+++ src/Pim/Bundle/ApiBundle/Controller/Rest/CategoryController.php
@@ -3,11 +3,24 @@
 namespace Pim\Bundle\ApiBundle\Controller\Rest;
 
 use Akeneo\Component\Classification\Repository\CategoryRepositoryInterface;
+use Akeneo\Component\StorageUtils\Exception\ObjectUpdaterException;
+use Akeneo\Component\StorageUtils\Exception\UnknownPropertyException;
+use Akeneo\Component\StorageUtils\Factory\SimpleFactoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
 use Pim\Bundle\CatalogBundle\Version;
+use Pim\Component\Api\Exception\DocumentedHttpException;
+use Pim\Component\Api\Exception\ViolationHttpException;
+use Pim\Component\Catalog\Model\CategoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @author    Marie Bochu <marie.bochu@akeneo.com>
@@ -22,21 +35,51 @@ class CategoryController
     /** @var NormalizerInterface */
     protected $normalizer;
 
+    /** @var  ValidatorInterface */
+    protected $validator;
+
+    /** @var SimpleFactoryInterface */
+    protected $factory;
+
+    /** @var ObjectUpdaterInterface */
+    protected $updater;
+
+    /** @var SaverInterface */
+    protected $saver;
+
+    /** @var RouterInterface */
+    protected $router;
+
     /** @var string */
     protected $urlDocumentation;
 
     /**
      * @param CategoryRepositoryInterface $repository
      * @param NormalizerInterface         $normalizer
+     * @param SimpleFactoryInterface      $factory
+     * @param ObjectUpdaterInterface      $updater
+     * @param ValidatorInterface          $validator
+     * @param SaverInterface              $saver
+     * @param RouterInterface             $router
      * @param string                      $urlDocumentation
      */
     public function __construct(
         CategoryRepositoryInterface $repository,
         NormalizerInterface $normalizer,
+        SimpleFactoryInterface $factory,
+        ObjectUpdaterInterface $updater,
+        ValidatorInterface $validator,
+        SaverInterface $saver,
+        RouterInterface $router,
         $urlDocumentation
     ) {
         $this->repository = $repository;
         $this->normalizer = $normalizer;
+        $this->factory = $factory;
+        $this->updater = $updater;
+        $this->validator = $validator;
+        $this->saver = $saver;
+        $this->router = $router;
         $this->urlDocumentation = sprintf($urlDocumentation, substr(Version::VERSION, 0, 3));
     }
 
@@ -82,5 +125,88 @@ class CategoryController
         //@TODO use paginate method before return results
 
         return new JsonResponse($categoriesStandard);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws BadRequestHttpException
+     * @throws UnprocessableEntityHttpException
+     *
+     * @return JsonResponse
+     */
+    public function createAction(Request $request)
+    {
+        $data = $this->getDecodedContent($request->getContent());
+
+        if (isset($data['code']) && null !== $this->repository->findOneByIdentifier($data['code'])) {
+            throw new UnprocessableEntityHttpException(sprintf('Category "%s" already exists.', $data['code']));
+        }
+
+        $category = $this->factory->create();
+        $this->updateCategory($category, $data);
+
+        $violations = $this->validator->validate($category);
+        if (0 !== $violations->count()) {
+            throw new ViolationHttpException($violations);
+        }
+
+        $this->saver->save($category);
+
+        $response = new JsonResponse(null, Response::HTTP_CREATED);
+        $route = $this->router->generate('pim_api_rest_category_get', ['code' => $category->getCode()], true);
+        $response->headers->set('Location', $route);
+
+        return $response;
+    }
+
+    /**
+     * Get the JSON decoded content. If the content is not a valid JSON, it throws an error 400.
+     *
+     * @param string $content content of a request to decode
+     *
+     * @throws BadRequestHttpException
+     *
+     * @return array
+     */
+    protected function getDecodedContent($content)
+    {
+        $decodedContent = json_decode($content, true);
+
+        if (null === $decodedContent) {
+            throw new BadRequestHttpException('JSON is not valid.');
+        }
+
+        return $decodedContent;
+    }
+
+    /**
+     * Update a category. It throws an error 422 if a problem occurred during the update.
+     *
+     * @param CategoryInterface $category category to update
+     * @param array             $data     data of the request already decoded
+     *
+     * @throws UnprocessableEntityHttpException
+     */
+    protected function updateCategory(CategoryInterface $category, $data)
+    {
+        try {
+            $this->updater->update($category, $data);
+        } catch (UnknownPropertyException $exception) {
+            throw new DocumentedHttpException(
+                $this->urlDocumentation,
+                sprintf(
+                    'Property "%s" does not exist. Check the standard format documentation.',
+                    $exception->getPropertyName()
+                ),
+                $exception
+            );
+        } catch (ObjectUpdaterException $exception) {
+            throw new DocumentedHttpException(
+                $this->urlDocumentation,
+                sprintf('%s Check the standard format documentation.', $exception->getMessage()),
+                $exception
+            );
+        }
     }
 }
