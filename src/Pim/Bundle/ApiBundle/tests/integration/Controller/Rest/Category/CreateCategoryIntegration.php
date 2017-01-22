0a04ebb9da808a60669a81c1ff62fa4841c03c8b
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-01-22 20:10:41 +0100

    API-19: add category creation in the web API

--- src/Pim/Bundle/ApiBundle/tests/integration/Controller/Rest/Category/CreateCategoryIntegration.php
+++ src/Pim/Bundle/ApiBundle/tests/integration/Controller/Rest/Category/CreateCategoryIntegration.php
@@ -1,7 +1,222 @@
 <?php
+
+namespace tests\integration\Pim\Bundle\ApiBundle\Controller\Rest\Category;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use Pim\Bundle\CatalogBundle\Version;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateCategoryIntegration extends TestCase
+{
+    public function testHttpHeadersInResponseWhenACategoryIsCreated()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "new_category_headers"
+    }
+JSON;
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertArrayHasKey('location', $response->headers->all());
+        $this->assertSame('http://localhost/api/rest/v1/categories/new_category_headers', $response->headers->get('location'));
+        $this->assertSame([], json_decode($response->getContent(), true));
+    }
+
+    public function testFormatStandardWhenACategoryIsCreatedButUncompleted()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "new_category_uncompleted"
+    }
+JSON;
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $category = $this->get('pim_catalog.repository.category')->findOneByIdentifier('new_category_uncompleted');
+        $categoryStandard = [
+            'code'   => 'new_category_uncompleted',
+            'parent' => null,
+            'labels' => [],
+        ];
+        $normalizer = $this->get('pim_catalog.normalizer.standard.category');
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame($categoryStandard, $normalizer->normalize($category));
+    }
+
+    public function testCompleteCategoryCreation()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "categoryC",
+        "parent": "master",
+        "labels": {
+            "en_US": "Category C",
+            "fr_FR": "Catégorie C"
+        }
+    }
+JSON;
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $category = $this->get('pim_catalog.repository.category')->findOneByIdentifier('categoryC');
+        $categoryStandard = [
+            'code'   => 'categoryC',
+            'parent' => 'master',
+            'labels' => [
+                'en_US' => 'Category C',
+                'fr_FR' => 'Catégorie C',
+            ],
+        ];
+        $normalizer = $this->get('pim_catalog.normalizer.standard.category');
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame($categoryStandard, $normalizer->normalize($category));
+    }
+
+    public function testResponseWhenContentIsNotValid()
+    {
+        $client = static::createClient();
+        $data = '';
+
+        $expectedContent = [
+            'code'    => 400,
+            'message' => 'JSON is not valid.',
+        ];
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenCategoryCodeAlreadyExists()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "categoryA"
+    }
+JSON;
+
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Category "categoryA" already exists.',
+        ];
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+
+    public function testResponseWhenValidationFailed()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": ""
+    }
+JSON;
+
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Validation failed.',
+            'errors'  => [
+                [
+                    'field'   => 'code',
+                    'message' => 'This value should not be blank.',
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenAPropertyIsNotExpected()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "sales",
+        "extra_property": ""
+    }
+JSON;
+
+        $version = substr(Version::VERSION, 0, 3);
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Property "extra_property" does not exist. Check the standard format documentation.',
+            '_links'  => [
+                'documentation' => [
+                    'href' => sprintf('https://docs.akeneo.com/%s/reference/standard_format/other_entities.html#category', $version),
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenLabelsIsNull()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "labels": null
+    }
+JSON;
+
+        $version = substr(Version::VERSION, 0, 3);
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Property "labels" expects an array (for update category). Check the standard format documentation.',
+            '_links'  => [
+                'documentation' => [
+                    'href' => sprintf('https://docs.akeneo.com/%s/reference/standard_format/other_entities.html#category', $version),
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/categories', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
     /**
-     * Created by PhpStorm.
-     * User: ahocquard
-     * Date: 19/01/17
-     * Time: 17:42
+     * {@inheritdoc}
      */
+    protected function getConfiguration()
+    {
+        return new Configuration(
+            [Configuration::getTechnicalCatalogPath()],
+            false
+        );
+    }
+}
