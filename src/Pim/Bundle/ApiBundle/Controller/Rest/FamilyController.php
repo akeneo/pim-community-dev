f4675697094ae482c846aaa760ed89f6af918363
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-01-25 11:33:45 +0100

    API-23: add the creation of a family in the API

--- src/Pim/Bundle/ApiBundle/Controller/Rest/FamilyController.php
+++ src/Pim/Bundle/ApiBundle/Controller/Rest/FamilyController.php
@@ -2,11 +2,25 @@
 
 namespace Pim\Bundle\ApiBundle\Controller\Rest;
 
+use Akeneo\Component\StorageUtils\Exception\ObjectUpdaterException;
+use Akeneo\Component\StorageUtils\Exception\UnknownPropertyException;
+use Akeneo\Component\StorageUtils\Factory\SimpleFactoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Pim\Bundle\CatalogBundle\Version;
+use Pim\Component\Api\Exception\DocumentedHttpException;
+use Pim\Component\Api\Exception\ViolationHttpException;
+use Pim\Component\Catalog\Model\FamilyInterface;
 use Pim\Component\Catalog\Repository\FamilyRepositoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @author    Philippe Mossière <philippe.mossiere@akeneo.com>
@@ -21,16 +35,52 @@ class FamilyController
     /** @var NormalizerInterface */
     protected $normalizer;
 
+    /** @var  ValidatorInterface */
+    protected $validator;
+
+    /** @var SimpleFactoryInterface */
+    protected $factory;
+
+    /** @var ObjectUpdaterInterface */
+    protected $updater;
+
+    /** @var SaverInterface */
+    protected $saver;
+
+    /** @var RouterInterface */
+    protected $router;
+
+    /** @var string */
+    protected $documentationUrl;
+
     /**
      * @param FamilyRepositoryInterface $repository
      * @param NormalizerInterface       $normalizer
+     * @param SimpleFactoryInterface    $factory
+     * @param ObjectUpdaterInterface    $updater
+     * @param ValidatorInterface        $validator
+     * @param SaverInterface            $saver
+     * @param RouterInterface           $router
+     * @param string                    $documentationUrl
      */
     public function __construct(
         FamilyRepositoryInterface $repository,
-        NormalizerInterface $normalizer
+        NormalizerInterface $normalizer,
+        SimpleFactoryInterface $factory,
+        ObjectUpdaterInterface $updater,
+        ValidatorInterface $validator,
+        SaverInterface $saver,
+        RouterInterface $router,
+        $documentationUrl
     ) {
-        $this->repository = $repository;
-        $this->normalizer = $normalizer;
+        $this->repository       = $repository;
+        $this->normalizer       = $normalizer;
+        $this->factory          = $factory;
+        $this->updater          = $updater;
+        $this->validator        = $validator;
+        $this->saver            = $saver;
+        $this->router           = $router;
+        $this->documentationUrl = sprintf($documentationUrl, substr(Version::VERSION, 0, 3));
     }
 
     /**
@@ -75,5 +125,110 @@ class FamilyController
         //@TODO use paginate method before return results
 
         return new JsonResponse($familiesStandard);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws BadRequestHttpException
+     * @throws UnprocessableEntityHttpException
+     *
+     * @return JsonResponse
+     */
+    public function createAction(Request $request)
+    {
+        $data = $this->getDecodedContent($request->getContent());
+
+        $family = $this->factory->create();
+        $this->updateFamily($family, $data);
+        $this->validateFamily($family);
+
+        $this->saver->save($family);
+
+        $response = $this->getCreateResponse($family);
+
+        return $response;
+    }
+
+    /**
+     * Get the JSON decoded content. If the content is not a valid JSON, it throws an error 400.
+     *
+     * @param string $content content of a request to decode
+     *
+     * @throws BadRequestHttpException
+     *
+     * @return array
+     */
+    protected function getDecodedContent($content)
+    {
+        $decodedContent = json_decode($content, true);
+
+        if (null === $decodedContent) {
+            throw new BadRequestHttpException('JSON is not valid.');
+        }
+
+        return $decodedContent;
+    }
+
+    /**
+     * Update a family. It throws an error 422 if a problem occurred during the update.
+     *
+     * @param FamilyInterface $family family to update
+     * @param array           $data   data of the request already decoded, it should be the standard format
+     *
+     * @throws UnprocessableEntityHttpException
+     */
+    protected function updateFamily(FamilyInterface $family, array $data)
+    {
+        try {
+            $this->updater->update($family, $data);
+        } catch (UnknownPropertyException $exception) {
+            throw new DocumentedHttpException(
+                $this->documentationUrl,
+                sprintf(
+                    'Property "%s" does not exist. Check the standard format documentation.',
+                    $exception->getPropertyName()
+                ),
+                $exception
+            );
+        } catch (ObjectUpdaterException $exception) {
+            throw new DocumentedHttpException(
+                $this->documentationUrl,
+                sprintf('%s Check the standard format documentation.', $exception->getMessage()),
+                $exception
+            );
+        }
+    }
+
+    /**
+     * Validate a family. It throws an error 422 with every violated constraints if
+     * the validation failed.
+     *
+     * @param FamilyInterface $family
+     *
+     * @throws ViolationHttpException
+     */
+    protected function validateFamily(FamilyInterface $family)
+    {
+        $violations = $this->validator->validate($family);
+        if (0 !== $violations->count()) {
+            throw new ViolationHttpException($violations);
+        }
+    }
+
+    /**
+     * Get a response with HTTP code 201 when an object is created.
+     *
+     * @param FamilyInterface $family
+     *
+     * @return Response
+     */
+    protected function getCreateResponse(FamilyInterface $family)
+    {
+        $response = new Response(null, Response::HTTP_CREATED);
+        $route = $this->router->generate('pim_api_rest_family_get', ['code' => $family->getCode()], true);
+        $response->headers->set('Location', $route);
+
+        return $response;
     }
 }
---
+++ src/Pim/Bundle/ApiBundle/tests/integration/Controller/Rest/Family/CreateFamilyIntegration.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace tests\integration\Pim\Bundle\ApiBundle\Controller\Rest\Family;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use Pim\Bundle\CatalogBundle\Version;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateFamilyIntegration extends TestCase
+{
+    public function testHttpHeadersInResponseWhenAFamilyIsCreated()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "new_family_headers"
+    }
+JSON;
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertArrayHasKey('location', $response->headers->all());
+        $this->assertSame('http://localhost/api/rest/v1/families/new_family_headers', $response->headers->get('location'));
+        $this->assertSame('', $response->getContent());
+    }
+
+    public function testFormatStandardWhenAFamilyIsCreatedButUncompleted()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "new_family_uncompleted"
+    }
+JSON;
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $family = $this->get('pim_catalog.repository.family')->findOneByIdentifier('new_family_uncompleted');
+        $familyStandard = [
+            'code'                   => 'new_family_uncompleted',
+            'attributes'             => ['sku'],
+            'attribute_as_label'     => 'sku',
+            'attribute_requirements' => [
+                'ecommerce' => ['sku'],
+                'tablet'    => ['sku'],
+            ],
+            'labels'                 => [],
+        ];
+        $normalizer = $this->get('pim_catalog.normalizer.standard.family');
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame($familyStandard, $normalizer->normalize($family));
+    }
+
+    public function testCompleteFamilyCreation()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "complete_family_creation",
+        "attributes": ["an_image", "a_metric", "a_price"],
+        "attribute_as_label": "sku",
+        "attribute_requirements": {
+            "ecommerce": ["sku", "a_metric"],
+            "tablet": ["sku", "a_price"]
+        },
+        "labels": {
+            "en_US": "Complete Family creation",
+            "fr_FR": "Création complète famille"
+        }
+    }
+JSON;
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $family = $this->get('pim_catalog.repository.family')->findOneByIdentifier('complete_family_creation');
+        $familyStandard = [
+            'code'                   => 'complete_family_creation',
+            'attributes'             => ['a_metric', 'a_price', 'an_image', 'sku'],
+            'attribute_as_label'     => 'sku',
+            'attribute_requirements' => [
+                'ecommerce' => ['a_metric', 'sku'],
+                'tablet'    => ['a_price', 'sku'],
+            ],
+            'labels'                 => [
+                'en_US' => 'Complete Family creation',
+                'fr_FR' => 'Création complète famille',
+            ],
+        ];
+        $normalizer = $this->get('pim_catalog.normalizer.standard.family');
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame($familyStandard, $normalizer->normalize($family));
+    }
+
+    public function testResponseWhenContentIsNotValid()
+    {
+        $client = static::createClient();
+        $data = '';
+
+        $expectedContent = [
+            'code'    => 400,
+            'message' => 'JSON is not valid.',
+        ];
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenFamilyCodeAlreadyExists()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "familyA"
+    }
+JSON;
+
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Validation failed.',
+            'errors'  => [
+                [
+                    'field'   => 'code',
+                    'message' => 'This value is already used.',
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenValidationFailed()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": ""
+    }
+JSON;
+
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Validation failed.',
+            'errors'  => [
+                [
+                    'field'   => 'code',
+                    'message' => 'This value should not be blank.',
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenAPropertyIsNotExpected()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "code": "new_family",
+        "extra_property": ""
+    }
+JSON;
+
+        $version = substr(Version::VERSION, 0, 3);
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Property "extra_property" does not exist. Check the standard format documentation.',
+            '_links'  => [
+                'documentation' => [
+                    'href' => sprintf('https://docs.akeneo.com/%s/reference/standard_format/other_entities.html#family', $version),
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    public function testResponseWhenLabelsIsNull()
+    {
+        $client = static::createClient();
+        $data =
+<<<JSON
+    {
+        "labels": null
+    }
+JSON;
+
+        $version = substr(Version::VERSION, 0, 3);
+        $expectedContent = [
+            'code'    => 422,
+            'message' => 'Property "labels" expects an array (for update family). Check the standard format documentation.',
+            '_links'  => [
+                'documentation' => [
+                    'href' => sprintf('https://docs.akeneo.com/%s/reference/standard_format/other_entities.html#family', $version),
+                ],
+            ],
+        ];
+
+        $client->request('POST', 'api/rest/v1/families', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertSame($expectedContent, json_decode($response->getContent(), true));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration()
+    {
+        return new Configuration(
+            [Configuration::getTechnicalCatalogPath()],
+            false
+        );
+    }
+}
