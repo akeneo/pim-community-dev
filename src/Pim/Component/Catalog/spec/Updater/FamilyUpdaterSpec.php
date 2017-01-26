08798643fa8d20bd09cfa73e1832c9364e425217
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-01-26 14:38:46 +0100

    API-27: Fix exception for attribute as label property in family updater

--- src/Pim/Component/Catalog/spec/Updater/FamilyUpdaterSpec.php
+++ src/Pim/Component/Catalog/spec/Updater/FamilyUpdaterSpec.php
@@ -346,8 +346,7 @@ class FamilyUpdaterSpec extends ObjectBehavior
 
     public function it_throws_an_exception_if_attribute_as_label_does_not_exist(
         $attributeRepository,
-        FamilyInterface $family,
-        AttributeInterface $priceAttribute
+        FamilyInterface $family
     ) {
         $data = [
             'attribute_as_label'     => 'unknown',
@@ -367,7 +366,7 @@ class FamilyUpdaterSpec extends ObjectBehavior
 
         $this->shouldThrow(
             InvalidPropertyException::validEntityCodeExpected(
-                'attributes',
+                'attribute_as_label',
                 'code',
                 'The attribute does not exist',
                 'Pim\Component\Catalog\Updater\FamilyUpdater',
