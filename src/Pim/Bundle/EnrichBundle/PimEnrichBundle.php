108522899780c1ea7f75ec0464633724d6e68547
Damien Carcel <damien.carcel@akeneo.com>
2017-01-27 16:04:16 +0100

    TIP-686: Remove useless variant view updater

--- src/Pim/Bundle/EnrichBundle/PimEnrichBundle.php
+++ src/Pim/Bundle/EnrichBundle/PimEnrichBundle.php
@@ -25,7 +25,6 @@ class PimEnrichBundle extends Bundle
         $container
             ->addCompilerPass(new Compiler\RegisterMassEditOperationsPass(new ReferenceFactory()))
             ->addCompilerPass(new Compiler\RegisterViewElementsPass(new ReferenceFactory()))
-            ->addCompilerPass(new Compiler\RegisterViewUpdatersPass(new ReferenceFactory()))
             ->addCompilerPass(new Compiler\RegisterFormExtensionsPass())
             ->addCompilerPass(new Compiler\RegisterGenericProvidersPass(new ReferenceFactory(), 'field'))
             ->addCompilerPass(new Compiler\RegisterGenericProvidersPass(new ReferenceFactory(), 'empty_value'))
