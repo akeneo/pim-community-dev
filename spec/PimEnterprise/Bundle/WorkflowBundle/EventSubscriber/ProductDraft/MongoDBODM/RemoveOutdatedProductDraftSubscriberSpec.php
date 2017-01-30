a030010da41620787a5ab41e2340cc6a15e84ad8
Damien Carcel <damien.carcel@akeneo.com>
2017-01-30 11:46:15 +0100

    TIP-686: Remove useless view updaters

--- spec/PimEnterprise/Bundle/WorkflowBundle/EventSubscriber/ProductDraft/MongoDBODM/RemoveOutdatedProductDraftSubscriberSpec.php
+++ spec/PimEnterprise/Bundle/WorkflowBundle/EventSubscriber/ProductDraft/MongoDBODM/RemoveOutdatedProductDraftSubscriberSpec.php
@@ -13,6 +13,7 @@ use PimEnterprise\Component\Workflow\Repository\ProductDraftRepositoryInterface;
 
 /**
  * @require Doctrine\ODM\MongoDB\DocumentManager
+ * @require \MongoId
  */
 class RemoveOutdatedProductDraftSubscriberSpec extends ObjectBehavior
 {
