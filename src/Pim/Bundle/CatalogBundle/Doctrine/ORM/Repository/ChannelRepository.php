21ffdacd161f897fb0e54b87f8b97430ac635d23
pierallard <pierallard@gmail.com>
2017-01-31 16:39:23 +0100

    [remove-deprecated] Remove deprecated ChannelRepositoryInterface methods

--- src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Repository/ChannelRepository.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Repository/ChannelRepository.php
@@ -54,48 +54,6 @@ class ChannelRepository extends EntityRepository implements ChannelRepositoryInt
     /**
      * {@inheritdoc}
      */
-    public function getDeletedLocaleIdsForChannel(ChannelInterface $channel)
-    {
-        $currentLocaleIds = array_map(
-            function ($locale) {
-                return $locale->getId();
-            },
-            $channel->getLocales()->toArray()
-        );
-
-        $sql = <<<SQL
-    SELECT cl.locale_id
-    FROM pim_catalog_channel_locale cl
-    WHERE cl.channel_id = :channel_id
-      AND cl.locale_id NOT IN (:current_locale_ids)
-SQL;
-
-        $stmt = $this->getEntityManager()->getConnection()->executeQuery(
-            $sql,
-            [
-                ':channel_id'         => $channel->getId(),
-                ':current_locale_ids' => $currentLocaleIds,
-            ],
-            [
-                ':current_locale_ids' => Connection::PARAM_INT_ARRAY,
-            ]
-        );
-
-        $rows = $stmt->fetchAll();
-
-        $locales = array_map(
-            function ($row) {
-                return (int) $row['locale_id'];
-            },
-            $rows
-        );
-
-        return $locales;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getChannelCodes()
     {
         $qb = $this->createQueryBuilder('c');
