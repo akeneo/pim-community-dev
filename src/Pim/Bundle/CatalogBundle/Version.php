367476d6ad479697763cc8366fe97996ba5f7bb5
Willy <willy.mesnage@akeneo.com>
2017-02-06 16:30:09 +0100

    Preparing version 1.5.19

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.18';
+    const VERSION = '1.5.19';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
