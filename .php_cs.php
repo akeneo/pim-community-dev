2f724bb3b36867b066b796d21152cfd431e0e167
skeleton <marie.bochu@akeneo.com>
2017-02-13 19:34:21 +0100

    Remove integration tests from php-cs-fixer

--- .php_cs.php
+++ .php_cs.php
@@ -6,6 +6,7 @@ $fixers = require __DIR__ . '/.php_cs-fixers.php';
 $finder->name('*.php')
     ->in(__DIR__ . '/features')
     ->in(__DIR__ . '/src')
+    ->files()->notName('*Integration.php')
     ->files()->notName('*Spec.php');
 
 return \Symfony\CS\Config\Config::create()
