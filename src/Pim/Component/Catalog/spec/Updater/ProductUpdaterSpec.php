89e0340dfc0a53a3e61af2205abb47f6ab8a4ad5
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-02-15 18:21:51 +0100

    API-13: add data validation on the product values of the standard format

--- src/Pim/Component/Catalog/spec/Updater/ProductUpdaterSpec.php
+++ src/Pim/Component/Catalog/spec/Updater/ProductUpdaterSpec.php
@@ -3,11 +3,13 @@
 namespace spec\Pim\Component\Catalog\Updater;
 
 use Akeneo\Component\StorageUtils\Exception\InvalidObjectException;
+use Akeneo\Component\StorageUtils\Exception\InvalidPropertyTypeException;
 use Akeneo\Component\StorageUtils\Exception\UnknownPropertyException;
 use Akeneo\Component\StorageUtils\Updater\PropertySetterInterface;
 use PhpSpec\ObjectBehavior;
 use Pim\Component\Catalog\Model\ProductInterface;
 use Pim\Component\Catalog\Updater\ProductTemplateUpdaterInterface;
+use Pim\Component\Catalog\Updater\ProductUpdater;
 
 class ProductUpdaterSpec extends ObjectBehavior
 {
@@ -69,7 +71,7 @@ class ProductUpdaterSpec extends ObjectBehavior
         $this->update($product, $updates, []);
     }
 
-    function it_ignores_fields__when_updating_a_product($propertySetter, ProductInterface $product)
+    function it_ignores_fields_when_updating_a_product($propertySetter, ProductInterface $product)
     {
         $propertySetter
             ->setData($product, 'groups', ['related1', 'related2'])
@@ -80,6 +82,14 @@ class ProductUpdaterSpec extends ObjectBehavior
         $propertySetter
             ->setData($product, 'desc', 'newdescUS', ['locale' => 'en_US', 'scope' => null])
             ->shouldBeCalled();
+
+        $propertySetter
+            ->setData($product, 'created', '2016-06-14T13:12:50+02:00')
+            ->shouldNotBeCalled();
+
+        $propertySetter
+            ->setData($product, 'updated', '2016-06-14T13:12:50+02:00')
+            ->shouldNotBeCalled();
 
         $updates = [
             'created'    => '2016-06-14T13:12:50+02:00',
@@ -103,6 +113,118 @@ class ProductUpdaterSpec extends ObjectBehavior
 
         $this
             ->shouldThrow(UnknownPropertyException::unknownProperty('unknown_property'))
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_values_is_not_an_array(ProductInterface $product)
+    {
+        $updates = [
+            'values' => 'foo',
+        ];
+
+        $this
+            ->shouldThrow(InvalidPropertyTypeException::arrayExpected('values', ProductUpdater::class, 'foo'))
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_it_is_not_an_array_of_product_values(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => 'foo',
+            ],
+        ];
+
+        $this
+            ->shouldThrow(InvalidPropertyTypeException::arrayExpected('name', ProductUpdater::class, 'foo'))
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_it_a_product_value_is_not_an_array(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => ['foo'],
+            ],
+        ];
+
+        $this
+            ->shouldThrow(
+                InvalidPropertyTypeException::validArrayStructureExpected(
+                    'name',
+                    'one of the product values is not an array.',
+                    ProductUpdater::class,
+                    ['foo']
+                )
+            )
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_locale_is_missing_in_a_product_value(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => [['scope' => null, 'data' => null]],
+            ],
+        ];
+
+        $this
+            ->shouldThrow(InvalidPropertyTypeException::arrayKeyExpected('name', 'locale', ProductUpdater::class, ['scope' => null, 'data' => null]))
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_scope_is_missing_in_a_product_value(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => [['locale' => null, 'data' => null]],
+            ],
+        ];
+
+        $this
+            ->shouldThrow(InvalidPropertyTypeException::arrayKeyExpected('name', 'scope', ProductUpdater::class, ['locale' => null, 'data' => null]))
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_locale_is_not_a_string_in_the_product_value(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => [['locale' => [], 'scope' => null, 'data' => null]],
+            ],
+        ];
+
+        $this
+            ->shouldThrow(
+                 new InvalidPropertyTypeException(
+                    'name',
+                    [],
+                    ProductUpdater::class,
+                    'Property "name" expects a product value with a string as locale, "array" given.',
+                    InvalidPropertyTypeException::STRING_EXPECTED_CODE
+                )
+            )
+            ->during('update', [$product, $updates, []]);
+    }
+
+    function it_throws_an_exception_when_scope_is_not_a_string_in_the_product_value(ProductInterface $product)
+    {
+        $updates = [
+            'values'     => [
+                'name' => [['locale' => null, 'scope' => [], 'data' => null]],
+            ],
+        ];
+
+        $this
+            ->shouldThrow(
+                new InvalidPropertyTypeException(
+                    'name',
+                    [],
+                    ProductUpdater::class,
+                    'Property "name" expects a product value with a string as scope, "array" given.',
+                    InvalidPropertyTypeException::STRING_EXPECTED_CODE
+                )
+            )
             ->during('update', [$product, $updates, []]);
     }
 }
