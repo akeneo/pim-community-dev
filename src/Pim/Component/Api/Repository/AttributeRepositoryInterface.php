8a939503707997e3add91351d4046008336cb385
skeleton <marie.bochu@akeneo.com>
2017-02-23 18:22:02 +0100

    API-114: add PageableRepositoryInterface

--- src/Pim/Component/Api/Repository/AttributeRepositoryInterface.php
+++ src/Pim/Component/Api/Repository/AttributeRepositoryInterface.php
@@ -5,35 +5,14 @@ namespace Pim\Component\Api\Repository;
 use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
 
 /**
- * Repository interface to access to attributes resources
+ * Repository interface for attributes resources
  *
  * @author    Marie Bochu <marie.bochu@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-interface AttributeRepositoryInterface extends IdentifiableObjectRepositoryInterface
+interface AttributeRepositoryInterface extends PageableRepositoryInterface, IdentifiableObjectRepositoryInterface
 {
-    /**
-     * Find resources with offset >= $offset and filtered by $criteria
-     *
-     * @param array $criteria
-     * @param array $orders
-     * @param int   $limit
-     * @param int   $offset
-     *
-     * @return array
-     */
-    public function searchAfterOffset(array $criteria, array $orders, $limit, $offset);
-
-    /**
-     * Return the count of products filtered by $criteria
-     *
-     * @param array $criteria
-     *
-     * @return int
-     */
-    public function count(array $criteria);
-
     /**
      * Get identifier code
      *
