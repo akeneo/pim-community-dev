24d9984d0e1c38f48c4bd4bf7392194cd4c834ca
Olivier Soulet <olivier.soulet@akeneo.com>
2017-02-28 10:24:24 +0100

    Preparing version 1.6.12

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.6.11';
+    const VERSION = '1.6.12';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Wackiki Wabbit';
