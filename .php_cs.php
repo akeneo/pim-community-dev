8ac057842f53702ab34dbc2636b142b917cf8676
Damien Carcel <damien.carcel@akeneo.com>
2017-03-02 18:10:08 +0100

    Remove old CI configuration files

--- .php_cs.php
+++ .php_cs.php
@@ -1,36 +1,15 @@
 <?php
 
-$branch     = getenv('TRAVIS_BRANCH');
-$phpVersion = getenv('TRAVIS_PHP_VERSION');
-
-printf('Current branch inspected : %s' . PHP_EOL, $branch);
-
-$finder = \Symfony\CS\Finder\DefaultFinder::create()
-    ->files()
-    ->exclude('app/check.php');
-$fixers = require __DIR__ . '/.php_cs-fixers.php';
-
-if (is_numeric(getenv('TRAVIS_PULL_REQUEST'))) {
-    $commitRange = str_replace('...', '..', getenv('TRAVIS_COMMIT_RANGE'));
-    printf('Commit range = %s' . PHP_EOL, $commitRange);
-    exec('git diff ' . $commitRange . ' --name-only --diff-filter=AMR | grep -v ^spec/ | grep php$', $diff);
-} else {
-    exec('git show --name-only --oneline --pretty="format:" --diff-filter=AMR | grep -v ^spec/ | grep php$', $diff);
-    $diff = array_filter($diff);
-}
-
-foreach ($diff as $idx => $filename) {
-    if ($filename !== 'app/check.php') {
-        printf('Parsed file : %s' . PHP_EOL, $filename);
-    } else {
-        printf('Excluded file : %s' . PHP_EOL, $filename);
-        unset($diff[$idx]);
-    }
-}
-
-$finder->append($diff);
-
-return \Symfony\CS\Config\Config::create()
-    ->level(Symfony\CS\FixerInterface::PSR2_LEVEL)
-    ->fixers($fixers)
-    ->finder($finder);
+return PhpCsFixer\Config::create()
+    ->setRules(array(
+        '@PSR2' => true,
+        'linebreak_after_opening_tag' => true,
+        'ordered_imports' => true,
+    ))
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->name('*.php')
+            ->in(__DIR__ . '/features')
+            ->in(__DIR__ . '/src')
+    )
+;
--- app/Resources/jenkins/behat_coverage/auto_append.php
+++
@@ -1,6 +0,0 @@
-<?php
-$coverage->stop();
-
-$writer = new PHP_CodeCoverage_Report_PHP;
-$objectName = 'proc_'.getmypid().'_'.uniqid();
-$writer->process($coverage, __DIR__. '/../../../build/coverage-parts/'.$objectName.'.cov');
