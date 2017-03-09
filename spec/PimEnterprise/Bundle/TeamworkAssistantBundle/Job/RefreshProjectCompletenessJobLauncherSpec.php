0b736cc47536ccc9c173ec7693b1a2b75d286e1c
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-03-09 15:10:22 +0100

    AMS-196: Remove useless spec

--- spec/PimEnterprise/Bundle/TeamworkAssistantBundle/Job/RefreshProjectCompletenessJobLauncherSpec.php
+++ spec/PimEnterprise/Bundle/TeamworkAssistantBundle/Job/RefreshProjectCompletenessJobLauncherSpec.php
@@ -12,24 +12,12 @@ class RefreshProjectCompletenessJobLauncherSpec extends ObjectBehavior
 {
     function let(JobInstanceRepository $jobInstanceRepository)
     {
-        $this->beConstructedWith($jobInstanceRepository, 'job_name', '/', 'prod');
+        $this->beConstructedWith($jobInstanceRepository, 'job_name', '/tmp', 'prod');
     }
 
     function it_is_initializable()
     {
         $this->shouldHaveType(RefreshProjectCompletenessJobLauncher::class);
-    }
-
-    function it_launch_the_attribute_groups_completeness_calculation(
-        $jobInstanceRepository,
-        JobInstance $jobInstance,
-        ProductInterface $product
-    ) {
-        $jobInstanceRepository->findOneByIdentifier('job_name')->willReturn($jobInstance);
-
-        $product->getId()->willReturn(40);
-
-        $this->launch($product, 'ecommerce', 'en_US')->shouldReturn(null);
     }
 
     function it_throws_an_exception_if_the_job_instance_does_not_exist(
