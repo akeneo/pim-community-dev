da1154d11eaed71fc8215cb2e2f328a7aa63beeb
Damien Carcel <damien.carcel@akeneo.com>
2017-03-10 16:01:20 +0100

    TIP-694: Delete previous completenesses by batch

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Saver/ProductSaver.php
@@ -5,7 +5,6 @@ namespace Pim\Bundle\CatalogBundle\Doctrine\Common\Saver;
 use Akeneo\Component\StorageUtils\Saver\BulkSaverInterface;
 use Akeneo\Component\StorageUtils\Saver\SaverInterface;
 use Akeneo\Component\StorageUtils\StorageEvents;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Util\ClassUtils;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
@@ -58,6 +57,7 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
 
         $this->eventDispatcher->dispatch(StorageEvents::PRE_SAVE, new GenericEvent($product, $options));
 
+        $this->dropCompletenesses([$product]);
         $this->calculateProductCompletenesses($product);
 
         $this->entityManager->persist($product);
@@ -75,13 +75,17 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
             return;
         }
 
+        foreach ($products as $product) {
+            $this->validateProduct($product);
+        }
+
         $options['unitary'] = false;
 
         $this->eventDispatcher->dispatch(StorageEvents::PRE_SAVE_ALL, new GenericEvent($products, $options));
 
-        foreach ($products as $product) {
-            $this->validateProduct($product);
+        $this->dropCompletenesses($products);
 
+        foreach ($products as $product) {
             $this->eventDispatcher->dispatch(StorageEvents::PRE_SAVE, new GenericEvent($product, $options));
 
             $this->calculateProductCompletenesses($product);
@@ -114,6 +118,29 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
     }
 
     /**
+     * Drops the current completenesses from the database for a list of products.
+     *
+     * @param ProductInterface[] $products
+     */
+    protected function dropCompletenesses(array $products)
+    {
+        $completenessIDs = [];
+        foreach ($products as $product) {
+            $completenesses = $product->getCompletenesses();
+            foreach ($completenesses as $completeness) {
+                $completenessIDs[] = $completeness->getId();
+            }
+        }
+
+        $stmt = $this->entityManager->getConnection()->executeQuery(
+            'DELETE c FROM pim_catalog_completeness c WHERE c.id IN (?)',
+            [$completenessIDs],
+            [Connection::PARAM_INT_ARRAY]
+        );
+        $stmt->execute();
+    }
+
+    /**
      * Calculates current product completenesses.
      *
      * The current completenesses collection is first cleared, then newly calculated ones are set to the product.
@@ -125,7 +152,7 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
         $completenessesCollection = $product->getCompletenesses();
 
         if (!$completenessesCollection->isEmpty()) {
-            $this->dropCompletenesses($completenessesCollection);
+            $completenessesCollection->clear();
         }
 
         $newCompletenesses = $this->completenessCalculator->calculate($product);
@@ -133,27 +160,5 @@ class ProductSaver implements SaverInterface, BulkSaverInterface
         foreach ($newCompletenesses as $completeness) {
             $completenessesCollection->add($completeness);
         }
-    }
-
-    /**
-     * Drops the current completenesses and missing attributes from the database and clear them from the product.
-     *
-     * @param Collection $completenessesCollection
-     */
-    protected function dropCompletenesses(Collection $completenessesCollection)
-    {
-        $completenessesIDs = [];
-        foreach ($completenessesCollection->getValues() as $completeness) {
-            $completenessesIDs[] = $completeness->getId();
-        }
-
-        $stmt = $this->entityManager->getConnection()->executeQuery(
-            'DELETE c FROM pim_catalog_completeness c WHERE c.id IN (?)',
-            [$completenessesIDs],
-            [Connection::PARAM_INT_ARRAY]
-        );
-        $stmt->execute();
-
-        $completenessesCollection->clear();
     }
 }
