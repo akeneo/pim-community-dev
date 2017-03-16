9fc331018659662533e6a79a40c84b709f23f1c2
pierallard <pierallard@gmail.com>
2017-03-16 16:00:20 +0100

    Revert "GITHUB-5538: Review fixes"
    This reverts commit b6499b7f8a04ea2d4b687557f8341735ce51b538.
    
    Conflicts:
    	CHANGELOG-1.7.md

--- features/Context/AssertionContext.php
+++ features/Context/AssertionContext.php
@@ -258,16 +258,13 @@ class AssertionContext extends RawMinkContext
     {
         return;
 
-        $this->spin(function () use ($text) {
-            $flashes = $this->getCurrentPage()->findAll('css', '.flash-messages-holder > div');
-            foreach ($flashes as $flash) {
-                if (false !== strpos($flash->getText(), $text)) {
-                    return true;
-                }
-            }
-
-            return null;
-        }, sprintf('Can not find flash message with text "%s"', $text));
+        // see PR #5817
+        return $this->spin(function () use ($text) {
+            return $this->getCurrentPage()->find(
+                'css',
+                sprintf('.message:contains("%s")', $text)
+            );
+        }, sprintf('Flash message "%s" was not found', $text));
     }
 
     /**
