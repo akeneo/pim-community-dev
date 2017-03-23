18f1d46a3c1c87ff97411bcb1ee1fde5ad9ab30a
Adrien Pétremann <petremann.adrien@gmail.com>
2017-03-23 11:28:03 +0100

    Preparing version 1.7.1

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.0';
+    const VERSION = '1.7.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
