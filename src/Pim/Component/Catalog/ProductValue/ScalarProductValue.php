017ac4b7c3d389fa74fbb105a00be6723344ce08
skeleton <marie.bochu@akeneo.com>
2017-03-23 18:24:24 +0100

    TIP-674: rename ProductValue to ScalarProductValue

--- src/Pim/Component/Catalog/ProductValue/ScalarProductValue.php
+++ src/Pim/Component/Catalog/ProductValue/ScalarProductValue.php
@@ -18,7 +18,7 @@ use Pim\Component\Catalog\Model\ProductValueInterface;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ProductValue extends AbstractProductValue implements ProductValueInterface
+class ScalarProductValue extends AbstractProductValue implements ProductValueInterface
 {
     /** @var string */
     protected $data;
