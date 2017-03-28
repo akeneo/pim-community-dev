1d775818e44f25344900c1aad855166ec079d2d9
Nicolas Dupont <nicolas@akeneo.com>
2017-03-28 10:43:20 +0200

    Add base php coupling detector rules

--- .php_cd.php
+++ .php_cd.php
@@ -34,14 +34,54 @@ $cUtilsDeps    = [
 
 $cDeps = array_merge($cBusinessDeps, $cUtilsDeps);
 
+$cAkeneoEERules = [
+    new Rule(
+        'Akeneo\Component\FileMetadata',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+    new Rule(
+        'Akeneo\Component\FileTransformer',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+    new Rule(
+        'Akeneo\Component\RuleEngine',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+];
+
 $cPimEERules = [
+    /*
+    new Rule(
+        'PimEnterprise\Component\CatalogRule',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+    new Rule(
+        'PimEnterprise\Component\ProductAsset',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+    new Rule(
+        'PimEnterprise\Component\Security',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
+    new Rule(
+        'PimEnterprise\Component\TeamworkAssistant',
+        $cDeps,
+        RuleInterface::TYPE_ONLY
+    ),
     new Rule(
         'PimEnterprise\Component\Workflow',
         $cDeps,
         RuleInterface::TYPE_ONLY
-    )
+    )*/
 ];
 
+$rules  = array_merge($cAkeneoEERules, $cPimEERules);
 $config = new Configuration($cPimEERules, $finder);
 
 return $config;
