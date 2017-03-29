9cd80c6b71860268bf7db8857fc5b0e36c794909
Damien Carcel <damien.carcel@akeneo.com>
2017-03-29 17:01:23 +0200

    TIP-613: Fix behat catalog install for null option value

--- src/Pim/Component/Catalog/Normalizer/Indexing/Product/DateNormalizer.php
+++ src/Pim/Component/Catalog/Normalizer/Indexing/Product/DateNormalizer.php
@@ -18,24 +18,24 @@ class DateNormalizer extends AbstractProductValueNormalizer implements Normalize
     /**
      * {@inheritdoc}
      */
-    protected function getNormalizedData(ProductValueInterface $productValue)
-    {
-        $date = $productValue->getData();
-
-        if (null !== $date && $date instanceof \DateTimeInterface) {
-            return $date->format('Y-m-d');
-        }
-
-        return null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function supportsNormalization($data, $format = null)
     {
         return $data instanceof ProductValueInterface &&
             AttributeTypes::BACKEND_TYPE_DATE === $data->getAttribute()->getBackendType() &&
             'indexing' === $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getNormalizedData(ProductValueInterface $productValue)
+    {
+        $date = $productValue->getData();
+
+        if ($date instanceof \DateTimeInterface) {
+            return $date->format('Y-m-d');
+        }
+
+        return null;
     }
 }
