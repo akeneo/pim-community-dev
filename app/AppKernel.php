13e9f88fe41be614b998e4457f4cd6f70b8c280f
Julien Sanchez <sumbobyboys@gmail.com>
2017-04-05 18:02:11 +0200

    Update AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\Kernel;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AppKernel extends Kernel
+class AppKernel extends KernelTest
 {
     /**
      * Registers your custom bundles
