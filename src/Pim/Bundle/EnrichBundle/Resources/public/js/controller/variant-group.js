a94256ccccd5a3288cb395a6c5f7e58ff87d64f6
Julien Sanchez <sumbobyboys@gmail.com>
2017-04-18 11:59:49 +0200

    fix variant group controller

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/controller/variant-group.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/controller/variant-group.js
@@ -2,55 +2,9 @@
 
 define(
     [
-        'underscore',
-        'pim/controller/base',
-        'pim/form-builder',
-        'pim/fetcher-registry',
-        'pim/user-context',
-        'pim/dialog',
-        'pim/page-title',
-        'pim/error',
-        'pim/i18n',
-        'module'
+        'pim/controller/group'
     ],
-    function (_, BaseController, FormBuilder, FetcherRegistry, UserContext, Dialog, PageTitle, Error, i18n, module) {
-        return BaseController.extend({
-            initialize: function () {
-                this.config = module.config();
-            },
-
-            /**
-             * {@inheritdoc}
-             */
-            renderRoute: function (route) {
-                return FetcherRegistry.getFetcher(this.config.fetcher).fetch(route.params.code)
-                    .then(function (group) {
-                        if (!this.active) {
-                            return;
-                        }
-
-                        var label = _.escape(
-                            i18n.getLabel(
-                                group.labels,
-                                UserContext.get('catalogLocale'),
-                                group.code
-                            )
-                        );
-
-                        PageTitle.set({'group.label': label });
-
-                        FormBuilder.build(group.meta.form)
-                            .then(function (form) {
-                                form.setData(group);
-                                form.trigger('pim_enrich:form:entity:post_fetch', group);
-                                form.setElement(this.$el).render();
-                            }.bind(this));
-                    }.bind(this))
-                .fail(function (response) {
-                    var errorView = new Error(response.responseJSON.message, response.status);
-                    errorView.setElement(this.$el).render();
-                });
-            }
-        });
+    function (BaseController) {
+        return BaseController.extend({});
     }
 );
