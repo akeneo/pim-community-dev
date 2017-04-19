bbbddfc54a4213b0a2dd2eeb01b05bb0de0c9163
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2017-04-19 17:18:46 +0200

    TIP-692: make completeness calculator usable by published

--- src/Pim/Component/Catalog/spec/Completeness/CompletenessCalculatorSpec.php
+++ src/Pim/Component/Catalog/spec/Completeness/CompletenessCalculatorSpec.php
@@ -12,6 +12,7 @@ use Pim\Component\Catalog\Factory\ProductValueFactory;
 use Pim\Component\Catalog\Model\AttributeInterface;
 use Pim\Component\Catalog\Model\AttributeRequirementInterface;
 use Pim\Component\Catalog\Model\ChannelInterface;
+use Pim\Component\Catalog\Model\Completeness;
 use Pim\Component\Catalog\Model\CompletenessInterface;
 use Pim\Component\Catalog\Model\FamilyInterface;
 use Pim\Component\Catalog\Model\LocaleInterface;
@@ -32,7 +33,8 @@ class CompletenessCalculatorSpec extends ObjectBehavior
             $productValueFactory,
             $channelRepository,
             $localeRepository,
-            $productValueCompleteChecker
+            $productValueCompleteChecker,
+            Completeness::class
         );
     }
 
