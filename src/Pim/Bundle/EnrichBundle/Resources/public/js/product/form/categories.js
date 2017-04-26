ca2f0c620a96a0f7195f85defba9124ad854b11b
Pierre Allard <pierallard@gmail.com>
2017-04-26 11:24:33 +0200

    [PIM-6374] Clean categories JS

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/categories.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/categories.js
@@ -11,25 +11,28 @@ define(
     [
         'jquery',
         'underscore',
+        'oro/translator',
         'backbone',
         'pim/form',
         'pim/template/product/tab/categories',
-        'pim/template/product/tab/category-switcher',
+        'pim/template/product/tab/catalog-switcher',
         'pim/user-context',
         'routing',
         'pim/tree/associate',
         'oro/mediator'
     ],
-    function ($,
-              _,
-              Backbone,
-              BaseForm,
-              formTemplate,
-              switcherTemplate,
-              UserContext,
-              Routing,
-              TreeAssociate,
-              mediator
+    function (
+        $,
+        _,
+        __,
+        Backbone,
+        BaseForm,
+        formTemplate,
+        switcherTemplate,
+        UserContext,
+        Routing,
+        TreeAssociate,
+        mediator
     ) {
         return BaseForm.extend({
             template: _.template(formTemplate),
@@ -44,6 +47,13 @@ define(
             },
             treeAssociate: null,
             cache: {},
+            trees: [],
+
+            /**
+             * Associates the tree code to the number of selected categories
+             * Example: { master: 1, sales: 12 }
+             */
+            categoriesCount: {},
 
             /**
              * {@inheritdoc}
@@ -63,7 +73,7 @@ define(
                 this.trigger('tab:register', {
                     code: this.code,
                     isVisible: this.isVisible.bind(this),
-                    label: _.__('pim_enrich.form.product.tab.categories.title')
+                    label: __('pim_enrich.form.product.tab.categories.title')
                 });
 
                 return BaseForm.prototype.configure.apply(this, arguments);
@@ -74,29 +84,19 @@ define(
              */
             render: function () {
                 this.loadTrees().done(function (trees) {
-                    var categoriesCount = {};
-                    _.each(_.pluck(trees, 'id'), function (id) {
-                        categoriesCount[id] = 0;
-                    });
+                    this.trees = trees;
 
-                    this.state.set('currentTree', _.first(trees).code);
-                    this.state.set('currentTreeId', _.first(trees).id);
+                    this.state.set('currentTree', _.first(this.trees).code);
+                    this.state.set('currentTreeId', _.first(this.trees).id);
 
                     this.$el.html(
                         this.template({
                             product: this.getFormData(),
                             locale: UserContext.get('catalogLocale'),
                             state: this.state.toJSON(),
-                            trees: trees
+                            trees: this.trees
                         })
                     );
-
-                    this.$el.find('.category-switcher').html(this.switcherTemplate({
-                        state: this.state.toJSON(),
-                        trees: trees,
-                        categoriesCount: categoriesCount,
-                        treeLinkSelector: this.treeLinkSelector
-                    }));
 
                     this.treeAssociate = new TreeAssociate('#trees', '#hidden-tree-input', {
                         list_categories: 'pim_enrich_product_listcategories',
@@ -104,10 +104,25 @@ define(
                     });
 
                     this.delegateEvents();
-                    this.initCategoryCount(trees);
+
+                    this.initCategoryCount();
+                    this.renderCategorySwitcher();
                 }.bind(this));
 
                 return this;
+            },
+
+            /**
+             * Renders the category switcher in the main template
+             */
+            renderCategorySwitcher: function () {
+                this.$el.find('.catalog-switcher:first').html(this.switcherTemplate({
+                    state: this.state.toJSON(),
+                    trees: this.trees,
+                    categoriesCount: this.categoriesCount,
+                    treeLinkSelector: this.treeLinkSelector,
+                    label: __('pim_enrich.form.product.tab.categories.catalog-switcher')
+                }));
             },
 
             /**
@@ -117,7 +132,7 @@ define(
              */
             loadTrees: function () {
                 return $.getJSON(
-                    Routing.generate('pim_enrich_product_category_rest_list', {id: this.getFormData().meta.id })
+                    Routing.generate('pim_enrich_product_category_rest_list', { id: this.getFormData().meta.id })
                 ).then(function (data) {
                     _.each(data.categories, function (category) {
                         this.cache[category.id] = category;
@@ -131,26 +146,32 @@ define(
                 }.bind(this));
             },
 
+            /**
+             * Displays the current tree when the user choose another one
+             *
+             * @param {Event} event
+             */
             changeTree: function (event) {
                 this.state.set('currentTree', event.currentTarget.dataset.tree);
                 this.state.set('currentTreeId', event.currentTarget.dataset.treeId);
                 this.treeAssociate.switchTree(event.currentTarget.dataset.treeId);
 
-                // TODO Use category switcher template
-                $(event.currentTarget)
-                    .find('.AknDropdown-menuLink').addClass('AknDropdown-menuLink--active').end()
-                    .siblings('[data-tree]')
-                    .each(function (i, link) {
-                        $(link).find('.AknDropdown-menuLink').removeClass('AknDropdown-menuLink--active')
-                    }).end();
-                this.$el.find('.current-category:first').html($(event.currentTarget).find('.tree-label').text());
+                this.renderCategorySwitcher();
             },
 
+            /**
+             * Change the current model when categories are checked/unchecked
+             *
+             * @param {Event} event
+             */
             updateModel: function (event) {
                 var selectedIds = _.filter(event.currentTarget.value.split(','), _.identity);
                 this.state.set('selectedCategories', selectedIds);
 
-                this.updateCategoryCount(this.state.get('currentTree'), this.state.get('currentTreeId'));
+                var rootTreeCode = this.state.get('currentTree');
+                this.categoriesCount[rootTreeCode] =
+                    this.$('li[data-code=' + rootTreeCode +  '] .jstree-checked').length;
+                this.renderCategorySwitcher();
 
                 var categoryCodes = _.map(selectedIds, this.getCategoryCode.bind(this));
                 this.getFormModel().set('categories', categoryCodes);
@@ -159,59 +180,18 @@ define(
 
             /**
              * Initialize category count with hidden values
-             *
-             * @param {Array} trees
              */
-            initCategoryCount: function (trees) {
-                _.each(trees, function (tree) {
+            initCategoryCount: function () {
+                _.each(this.trees, function (tree) {
                     var selectedCategories = [];
                     var hiddenSelection = this.$('#hidden-tree-input').val();
                     hiddenSelection = hiddenSelection.length > 0 ? hiddenSelection.split(',') : [];
                     _.each(hiddenSelection, function (categoryId) {
                         selectedCategories.push(this.cache[categoryId]);
                     }.bind(this));
-                    var categoryCount = _.where(selectedCategories, {rootId: tree.id}).length;
-                    if (categoryCount > 0) {
-                        $('#' + this.treeLinkSelector + tree.id).addClass(this.treeHasItemClass);
-                    }
-                    this.updateCategoryBadge(tree.code, categoryCount);
+
+                    this.categoriesCount[tree.code] = _.where(selectedCategories, {rootId: tree.id}).length;
                 }.bind(this));
-            },
-
-            /**
-             * count selected leaves in the category jstree
-             *
-             * @param {String}  rootTreeCode
-             * @param {integer} treeId
-             */
-            updateCategoryCount: function (rootTreeCode, treeId) {
-                var $rootTreeContainer = this.$('li[data-code=' + rootTreeCode +  ']');
-                var selected = $rootTreeContainer.find('.jstree-checked');
-
-                if (selected.length > 0) {
-                    $('#' + this.treeLinkSelector + treeId).addClass(this.treeHasItemClass);
-                } else {
-                    $('#' + this.treeLinkSelector + treeId).removeClass(this.treeHasItemClass);
-                }
-
-                this.updateCategoryBadge(rootTreeCode, selected.length);
-            },
-
-            /**
-             * Update the category count badge
-             *
-             * @param {string} rootTreeCode
-             * @param {integer} categoryCount
-             */
-            updateCategoryBadge: function (rootTreeCode, categoryCount) {
-                var badge = this.$('li[data-tree=' + rootTreeCode +  ']').find('.AknBadge');
-                badge.html(categoryCount);
-
-                if (categoryCount > 0) {
-                    badge.addClass('AknBadge--enabled').removeClass('AknBadge--grey');
-                } else {
-                    badge.removeClass('AknBadge--enabled').addClass('AknBadge--grey');
-                }
             },
 
             /**
