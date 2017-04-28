5b4e99f463ab841a8a6e0714d5678ff77c8dc129
Pierre Allard <pierallard@gmail.com>
2017-04-28 16:33:37 +0200

    [PIM-6397] Set real search in background

--- src/Oro/Bundle/FilterBundle/Filter/SearchFilter.php
+++ src/Oro/Bundle/FilterBundle/Filter/SearchFilter.php
@@ -2,7 +2,55 @@
 
 namespace Oro\Bundle\FilterBundle\Filter;
 
-class SearchFilter extends StringFilter
-{
+use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+use Oro\Bundle\FilterBundle\Form\Type\Filter\TextFilterType;
+use Pim\Component\Catalog\Query\Filter\Operators;
 
+class SearchFilter extends AbstractFilter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function apply(FilterDatasourceAdapterInterface $ds, $data)
+    {
+        $data = $this->parseData($data);
+        if (!$data) {
+            return false;
+        }
+
+        $parameterName = $ds->generateParameterName($this->getName());
+
+        $this->applyFilterToClause(
+            $ds,
+            $ds->expr()->comparison($this->get(FilterUtility::DATA_NAME_KEY), Operators::IS_LIKE, $parameterName, true)
+        );
+
+        $ds->setParameter($parameterName, $data['value']);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFormType()
+    {
+        return TextFilterType::NAME;
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return array|bool
+     */
+    protected function parseData($data)
+    {
+        if (!is_array($data) || !array_key_exists('value', $data) || !$data['value']) {
+            return false;
+        }
+
+        $data['value'] = sprintf('%%%s%%', $data['value']);
+
+        return $data;
+    }
 }
--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datafilter/filter/search-filter.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datafilter/filter/search-filter.js
@@ -1,7 +1,62 @@
 /* global define */
-define(['jquery', 'underscore', 'oro/translator', 'oro/datafilter/text-filter'],
-function($, _, __, TextFilter) {
+define(['jquery', 'underscore', 'oro/translator', 'oro/datafilter/abstract-filter'],
+function($, _, __, AbstractFilter) {
     'use strict';
 
-    return TextFilter.extend({});
+    return AbstractFilter.extend({
+        inputValueSelector: 'input[name="value"]',
+
+        events: {
+            'keydown input[name="value"]': 'keyPress',
+            'keypress input[name="value"]': 'keyPress'
+        },
+
+        emptyValue: {
+            value: ''
+        },
+
+        timer: null,
+
+        timerDelay: 500,
+
+        render: function () {
+            this.$el.html(
+                '<input type="text" name="value" value="" placeholder="Search">'
+            );
+        },
+
+        /**
+         * @inheritDoc
+         */
+        _writeDOMValue: function(value) {
+            this._setInputValue(this.inputValueSelector, value.value);
+            return this;
+        },
+
+        /**
+         * @inheritDoc
+         */
+        _readDOMValue: function() {
+            return {
+                value: this._getInputValue(this.inputValueSelector)
+            };
+        },
+
+        keyPress: function(event) {
+            if (null !== this.timer) {
+                clearTimeout(this.timer);
+            }
+
+            if (13 === event.keyCode) {
+                // Enter key
+                this.doSearch();
+            } else {
+                this.timer = setTimeout(this.doSearch.bind(this), this.timerDelay);
+            }
+        },
+
+        doSearch: function() {
+            this.setValue(this._readDOMValue());
+        }
+    });
 });
