6b995b5843ce79fd8bd43fcd85203640c00a36b5
Julien Sanchez <sumbobyboys@gmail.com>
2017-05-05 12:29:58 +0200

    TIP-734: Rework menu to use PEF architecture

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/menu/menu.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Base extension for menu
+ *
+ * @author    Julien Sanchez <julien@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(
+    [
+        'underscore',
+        'pim/form'
+    ],
+    function (
+        _,
+        BaseForm
+    ) {
+        return BaseForm.extend({
+            tagName: 'ul',
+            className: 'AknMainMenu',
+
+            /**
+             * {@inheritdoc}
+             */
+            initialize: function (config) {
+                this.config = config.config;
+                
+                BaseForm.prototype.initialize.apply(this, arguments);
+            }
+        });
+    });
