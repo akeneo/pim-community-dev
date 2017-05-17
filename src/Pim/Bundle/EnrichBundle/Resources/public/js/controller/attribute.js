12571411e937c0666fb11be0a082e23b440e24ba
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2017-05-17 10:50:27 +0200

    TIP-732: Set a front controller for attribute form

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/controller/attribute.js
@@ -0,0 +1,66 @@
+/**
+ * @author    Yohan Blain <yohan.blain@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+'use strict';
+
+define(
+    [
+        'underscore',
+        'pim/controller/base',
+        'pim/form-builder',
+        'pim/fetcher-registry',
+        'pim/user-context',
+        'pim/dialog',
+        'pim/page-title',
+        'pim/error',
+        'pim/i18n'
+    ],
+    function (_, BaseController, FormBuilder, FetcherRegistry, UserContext, Dialog, PageTitle, Error, i18n) {
+        return BaseController.extend({
+            /**
+             * {@inheritdoc}
+             */
+            renderRoute: function (route) {
+                return FetcherRegistry.getFetcher('attribute').fetch(route.params.code, {cached: false})
+                    .then(function (attribute) {
+                        if (!this.active) {
+                            return;
+                        }
+
+                        var label = _.escape(
+                            i18n.getLabel(
+                                attribute.labels,
+                                UserContext.get('catalogLocale'),
+                                attribute.code
+                            )
+                        );
+
+                        PageTitle.set({'attribute.label': _.escape(label) });
+
+                        FormBuilder.buildForm(attribute.meta.form)
+                            .then(function (form) {
+                                form.setAdditionalView('type-specific', attribute.meta.additional_form);
+
+                                return form.configure().then(function () {
+                                    return form;
+                                });
+                            })
+                            .then(function (form) {
+                                this.on('pim:controller:can-leave', function (event) {
+                                    form.trigger('pim_enrich:form:can-leave', event);
+                                });
+                                form.setData(attribute);
+                                form.trigger('pim_enrich:form:entity:post_fetch', attribute);
+                                form.setElement(this.$el).render();
+                            }.bind(this));
+                    }.bind(this))
+                .fail(function (response) {
+                    var errorView = new Error(response.responseJSON.message, response.status);
+                    errorView.setElement(this.$el).render();
+                });
+            }
+        });
+    }
+);
