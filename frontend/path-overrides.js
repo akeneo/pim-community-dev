fac57db6b4db9cb4c19f6ad580332d369a03b7fa
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-05-17 16:04:31 +0200

    Add .js to summernote path alias

--- frontend/path-overrides.js
+++ frontend/path-overrides.js
@@ -1,7 +1,7 @@
 module.exports = {
     'pimuser/js/init-signin': 'src/Pim/Bundle/UserBundle/Resources/public/js/init-signin',
     'bootstrap-modal': 'src/Pim/Bundle/UIBundle/Resources/public/lib/bootstrap-modal',
-    summernote: 'node_modules/summernote/dist/summernote.min',
+    summernote: 'node_modules/summernote/dist/summernote.min.js',
     'translator-lib': 'src/Pim/Bundle/EnrichBundle/Resources/public/lib/translator',
     translator: 'src/Pim/Bundle/EnrichBundle/Resources/public/js/translator',
     routing: 'src/Pim/Bundle/EnrichBundle/Resources/public/js/fos-routing-wrapper',
