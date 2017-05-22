c65ced1838bb546edad4afecef4d175d03abfc81
Samir Boulil <samir.boulil@akeneo.com>
2017-05-22 15:10:12 +0200

    Preparing version 1.5.22

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.21';
+    const VERSION = '1.5.22';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Sunflower';
