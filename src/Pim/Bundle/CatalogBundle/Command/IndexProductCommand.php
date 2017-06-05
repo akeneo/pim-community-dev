552a95af36b0f6d123b514a35b2904ce4c86d97e
Anael CHARDAN <anael.chardan@gmail.com>
2017-06-05 16:32:45 +0200

    TIP-760: Index command

---
+++ src/Pim/Bundle/CatalogBundle/Command/IndexProductCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Index products into Elasticsearch
+ *
+ * @author    Anael Chardan <anael.chardan@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class IndexProductCommand extends ContainerAwareCommand
+{
+    /** @var integer */
+    const DEFAULT_PAGE_SIZE = 100;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('pim:product:index')
+            ->addOption(
+                'page-size',
+                false,
+                InputOption::VALUE_OPTIONAL,
+                'Number of products per page',
+                self::DEFAULT_PAGE_SIZE
+            )
+            ->setDescription('Index all products into Elasticsearch');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $productRepository = $this->getContainer()->get('pim_catalog.repository.product');
+        $productIndexer = $this->getContainer()->get('pim_catalog.elasticsearch.product_indexer');
+
+        $pageSize = $input->getOption('page-size');
+        $totalElements = $productRepository->countAll();
+        $numberOfPage = ceil($totalElements / $pageSize);
+
+        $output->writeln(sprintf('<info>%s products to index</info>', $totalElements));
+
+        for($currentPage = 1; $currentPage <= $numberOfPage; $currentPage++) {
+            $offset = $pageSize * ($currentPage - 1);
+            $output->writeln(
+                sprintf(
+                    'Indexing products %d to %d',
+                    $offset,
+                    ($offset + $pageSize) < $totalElements ? ($offset + $pageSize) : $totalElements
+                )
+            );
+
+            $productIndexer->indexAll($productRepository->findAllWithOffsetAndSize($offset, $pageSize));
+        }
+
+        $message = sprintf('<info>%d products indexed</info>', $totalElements);
+
+        $output->writeln($message);
+    }
+}
---
+++ src/Pim/Bundle/CatalogBundle/spec/Command/IndexProductCommandSpec.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace spec\Pim\Bundle\CatalogBundle\Command;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Doctrine\ORM\Repository\ProductRepository;
+use Pim\Bundle\CatalogBundle\Elasticsearch\ProductIndexer;
+use Prophecy\Argument;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class IndexProductCommandSpec extends ObjectBehavior
+{
+    function it_has_a_name()
+    {
+        $this->getName()->shouldReturn('pim:product:index');
+    }
+
+    function it_is_a_command()
+    {
+        $this->shouldBeAnInstanceOf(ContainerAwareCommand::class);
+    }
+
+    function it_indexes_all_products(
+        ContainerInterface $container,
+        ProductRepository $productRepository,
+        ProductIndexer $productIndexer,
+        InputInterface $input,
+        OutputInterface $output,
+        Application $application,
+        HelperSet $helperSet,
+        InputDefinition $definition
+    ) {
+        $container->get('pim_catalog.repository.product')->willReturn($productRepository);
+        $container->get('pim_catalog.elasticsearch.product_indexer')->willReturn($productIndexer);
+
+        $productRepository->countAll()->willReturn(6);
+        $productRepository->findAllWithOffsetAndSize(0, 5)->willReturn([]);
+        $productRepository->findAllWithOffsetAndSize(5, 5)->willReturn([]);
+
+        $output->writeln('<info>6 products to index</info>')->shouldBeCalled();
+        $output->writeln('Indexing products 0 to 5')->shouldBeCalled();
+        $output->writeln('Indexing products 5 to 6')->shouldBeCalled();
+        $output->writeln('<info>6 products indexed</info>')->shouldBeCalled();
+
+        $commandInput = new ArrayInput([
+            'command' => 'pim:product:index',
+            '--page-size' => 5,
+            '--no-debug' => true,
+        ]);
+        $application->run($commandInput, $output)->willReturn(0);
+
+        $definition->getOptions()->willReturn([]);
+        $definition->getArguments()->willReturn([]);
+
+        $application->getHelperSet()->willReturn($helperSet);
+        $application->getDefinition()->willReturn($definition);
+
+        $this->setApplication($application);
+        $this->setContainer($container);
+        $input->bind(Argument::any())->shouldBeCalled();
+        $input->isInteractive()->shouldBeCalled();
+        $input->hasArgument(Argument::any())->shouldBeCalled();
+        $input->validate()->shouldBeCalled();
+        $input->getOption('page-size')->willReturn(5);
+        $this->run($input, $output);
+    }
+}
