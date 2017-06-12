27582c5dfe2744d1fbd2576c2a044fad8ddd329b
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2017-06-12 11:56:25 +0200

    TIP-732: Change attribute controller declaration according to CE

--- src/PimEnterprise/Component/Catalog/Normalizer/Standard/AttributeNormalizer.php
+++ src/PimEnterprise/Component/Catalog/Normalizer/Standard/AttributeNormalizer.php
@@ -16,8 +16,6 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * @author    Marie Bochu <marie.bochu@akeneo.com>
- * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class AttributeNormalizer implements NormalizerInterface
 {
