778c2191b21f030e5a03fcf558b92d4211ac923c
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-06-13 14:47:04 +0200

    PIM-6442: Use ValueContainerBuilder instead ProductBuilder

--- src/Pim/Component/Catalog/Builder/ValuesContainerBuilderInterface.php
+++ src/Pim/Component/Catalog/Builder/ValuesContainerBuilderInterface.php
@@ -5,6 +5,11 @@ namespace Pim\Component\Catalog\Builder;
 use Pim\Component\Catalog\Model\AttributeInterface;
 use Pim\Component\Catalog\Model\ValuesContainerInterface;
 
+/**
+ * @author    Julien Janvier <julien.janvier@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
 interface ValuesContainerBuilderInterface
 {
     /**
