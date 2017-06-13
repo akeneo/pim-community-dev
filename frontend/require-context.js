1d5942c9f7fb00f0eaa15dccca0bbd4669ea17d7
Tamara Robichet <tamarasaurus@users.noreply.github.com>
2017-06-13 16:54:55 +0200

    TIP-731: Fix uglify/minify + es6 (#6286)
    * Get es6 to work with uglify
    
    * TIP-731: Get es6 to work with uglify

--- frontend/require-context.js
+++ frontend/require-context.js
@@ -2,6 +2,11 @@ define([], function() {
     return function(moduleName) {
         var modulePath = __contextPaths[moduleName]
         var grab = require.context('./dynamic/', true, __contextPlaceholder)
+
+        if (typeof modulePath === 'undefined') {
+            console.error('Cannot fetch module', moduleName, ' - it needs to be defined in the requirejs.yml')
+        }
+
         modulePath = modulePath.replace(/.js$/, '')
 
         return grab(modulePath)
