b0345d8240ca8373db59d4964cb9d3fd244b4e8f
Adrien Pétremann <petremann.adrien@gmail.com>
2017-06-13 17:19:45 +0200

    PIM-6442: Add doc for new Exception

--- src/Akeneo/Component/StorageUtils/Exception/ResourceNotFoundException.php
+++ src/Akeneo/Component/StorageUtils/Exception/ResourceNotFoundException.php
@@ -3,6 +3,8 @@
 namespace Akeneo\Component\StorageUtils\Exception;
 
 /**
+ * Exception thrown when an expected resource has not been found.
+ *
  * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
