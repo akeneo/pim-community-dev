6e301d7fa1ffbd360797efee1c933568e304191d
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-06-13 14:47:04 +0200

    PIM-6442: Use ValueContainerBuilder instead ProductBuilder

--- src/Pim/Component/Catalog/Updater/Setter/AssociationFieldSetter.php
+++ src/Pim/Component/Catalog/Updater/Setter/AssociationFieldSetter.php
@@ -24,7 +24,7 @@ class AssociationFieldSetter extends AbstractFieldSetter
     /** @var IdentifiableObjectRepositoryInterface */
     protected $groupRepository;
 
-    /** @var \Pim\Component\Catalog\Builder\ValuesContainerBuilderInterface */
+    /** @var ValuesContainerBuilderInterface */
     protected $productBuilder;
 
     /**
