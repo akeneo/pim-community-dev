894ceecf1a3ccef3ba75b45b508bc4e0fa8cc9e4
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-06-14 16:47:35 +0200

    PIM-6442: Rename rename AbstractProductValueDataNormalizer

--- src/Pim/Bundle/VersioningBundle/Normalizer/Flat/AbstractValueDataNormalizer.php
+++ src/Pim/Bundle/VersioningBundle/Normalizer/Flat/AbstractValueDataNormalizer.php
@@ -11,7 +11,7 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-abstract class AbstractProductValueDataNormalizer implements NormalizerInterface
+abstract class AbstractValueDataNormalizer implements NormalizerInterface
 {
     /**
      * {@inheritdoc}
