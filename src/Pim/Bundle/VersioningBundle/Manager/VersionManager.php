baff2960fb389c410f6ecad32df09460b6f3a529
Marie <marie.bochu@akeneo.com>
2017-06-26 15:43:42 +0200

    [TEST] Remove refresh on versioning

--- src/Pim/Bundle/VersioningBundle/Manager/VersionManager.php
+++ src/Pim/Bundle/VersioningBundle/Manager/VersionManager.php
@@ -120,7 +120,7 @@ class VersionManager
 
         if ($this->realTimeVersioning) {
             $manager = $this->registry->getManagerForClass(ClassUtils::getClass($versionable));
-            $manager->refresh($versionable);
+            //$manager->refresh($versionable);
 
             $createdVersions = $this->buildPendingVersions($versionable);
 
