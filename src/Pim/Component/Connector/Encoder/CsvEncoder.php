1971ca1524262a53413b9ae74a5a7c93b6373f72
Nicolas Dupont <nicolas@akeneo.com>
2017-07-11 12:51:44 +0200

    change connector

--- src/Pim/Component/Connector/Encoder/CsvEncoder.php
+++ src/Pim/Component/Connector/Encoder/CsvEncoder.php
@@ -11,7 +11,7 @@ use Symfony\Component\Serializer\Encoder\EncoderInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CsvEncoder implements EncoderInterface
+class CsveeEncoder implements EncoderInterface
 {
     /** @staticvar string */
     const FORMAT = 'csv';
