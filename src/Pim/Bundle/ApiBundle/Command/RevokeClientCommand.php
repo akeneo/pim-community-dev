0eece26aa1142e68b87798e4463ad12b8d9d35d8
Nicolas Dupont <nicolas@akeneo.com>
2017-07-11 10:12:59 +0200

    JeanBille

--- src/Pim/Bundle/ApiBundle/Command/RevokeClientCommand.php
+++ src/Pim/Bundle/ApiBundle/Command/RevokeClientCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
  * @author    Yohan Blain <yohan.blain@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * baba
  */
 class RevokeClientCommand extends ContainerAwareCommand
 {
