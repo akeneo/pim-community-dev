ea4f053173a65e895603bef1c8393f91a162b763
Nicolas Dupont <nicolas@akeneo.com>
2017-07-11 11:16:46 +0200

    Revert

--- src/Pim/Bundle/CatalogBundle/Doctrine/Common/Filter/CategoryFilter.php
+++ src/Pim/Bundle/CatalogBundle/Doctrine/Common/Filter/CategoryFilter.php
@@ -10,7 +10,7 @@ use Pim\Component\Catalog\Query\Filter\FieldFilterInterface;
 use Pim\Component\Catalog\Query\Filter\Operators;
 
 /**
- * Product category filter Michel
+ * Product category filter
  *
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
