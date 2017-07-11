8498f2fac876a8c4bc6747f959b2a7643eff154f
pierallard <pierallard@gmail.com>
2017-07-11 15:37:17 +0200

    [PIM-6495] Fix review

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/product-completeness.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/product-completeness.js
@@ -30,7 +30,8 @@ define(
 
                 this.$el.append(this.template({
                     label: __('pim_enrich.entity.product.completeness'),
-                    completeness: this.getRatio()
+                    completeness: this.getRatio(),
+                    badgeClass: this.getBadgeClass()
                 }));
 
                 return this;
@@ -48,6 +49,25 @@ define(
                 );
 
                 return completeness.channels[UserContext.get('catalogScope')].completeness.ratio;
+            },
+
+            /**
+             * Returns the HTML class for the badge from the completeness ratio
+             *
+             * @returns string
+             */
+            getBadgeClass: function() {
+                var ratio = this.getRatio();
+
+                if (ratio <= 0) {
+                    return 'AknBadge--important';
+                }
+
+                if (ratio >= 100) {
+                    return 'AknBadge--enabled';
+                }
+
+                return 'AknBadge--warning';
             }
         });
     }
