861a7ce859a4ce4a198fcc8dfc27b93b13045f0a
Nicolas Dupont <nicolas@akeneo.com>
2017-07-11 15:58:24 +0200

    wapi

--- src/Pim/Bundle/ApiBundle/Controller/AttributeController.php
+++ src/Pim/Bundle/ApiBundle/Controller/AttributeController.php
@@ -32,7 +32,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AttributeController
+class AttributeControllerMichel
 {
     /** @var AttributeRepositoryInterface */
     protected $repository;
