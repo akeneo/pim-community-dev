11cab4f2e4ebb341eb422e01e80f0a7dd02432ab
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-07-12 18:12:51 +0200

    PIM-6448: Add Product model validation

--- src/Pim/Component/Catalog/Updater/ProductModelUpdater.php
+++ src/Pim/Component/Catalog/Updater/ProductModelUpdater.php
@@ -3,7 +3,9 @@
 namespace Pim\Component\Catalog\Updater;
 
 use Akeneo\Component\StorageUtils\Exception\InvalidObjectException;
+use Akeneo\Component\StorageUtils\Exception\InvalidPropertyException;
 use Akeneo\Component\StorageUtils\Exception\UnknownPropertyException;
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
 use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
 use Akeneo\Component\StorageUtils\Updater\PropertySetterInterface;
 use Doctrine\Common\Util\ClassUtils;
@@ -29,20 +31,26 @@ class ProductModelUpdater implements ObjectUpdaterInterface
     /** @var array */
     private $ignoredFields;
 
+    /** @var IdentifiableObjectRepositoryInterface */
+    private $familyVariantRepository;
+
     /**
-     * @param PropertySetterInterface         $propertySetter
-     * @param ObjectUpdaterInterface          $valuesUpdater
-     * @param array                           $supportedFields
-     * @param array                           $ignoredFields
+     * @param PropertySetterInterface               $propertySetter
+     * @param ObjectUpdaterInterface                $valuesUpdater
+     * @param IdentifiableObjectRepositoryInterface $familyVariantRepository
+     * @param array                                 $supportedFields
+     * @param array                                 $ignoredFields
      */
     public function __construct(
         PropertySetterInterface $propertySetter,
         ObjectUpdaterInterface $valuesUpdater,
+        IdentifiableObjectRepositoryInterface $familyVariantRepository,
         array $supportedFields,
         array $ignoredFields
     ) {
         $this->propertySetter = $propertySetter;
         $this->valuesUpdater = $valuesUpdater;
+        $this->familyVariantRepository = $familyVariantRepository;
         $this->supportedFields = $supportedFields;
         $this->ignoredFields = $ignoredFields;
     }
@@ -59,13 +67,25 @@ class ProductModelUpdater implements ObjectUpdaterInterface
             );
         }
 
-        foreach ($data as $code => $values) {
+        foreach ($data as $code => $value) {
             if (in_array($code, $this->supportedFields)) {
-                $this->propertySetter->setData($productModel, $code, $values);
+                $this->propertySetter->setData($productModel, $code, $value);
             } elseif ('values' === $code) {
-                $this->valuesUpdater->update($productModel, $values, $options);
+                $this->valuesUpdater->update($productModel, $value, $options);
             } elseif ('identifier' === $code) {
-                $productModel->setIdentifier($values);
+                $productModel->setIdentifier($value);
+            } elseif ('family_variant' === $code) {
+                if (null === $familyVariant = $this->familyVariantRepository->findOneByIdentifier($value)) {
+                    throw InvalidPropertyException::validEntityCodeExpected(
+                        'family_variant',
+                        'family variant code',
+                        'The family variant does not exist',
+                        static::class,
+                        $value
+                    );
+                }
+
+                $productModel->setFamilyVariant($familyVariant);
             } elseif (!in_array($code, $this->ignoredFields)) {
                 throw UnknownPropertyException::unknownProperty($code);
             }
