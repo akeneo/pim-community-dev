e3dffde8cc8e2f7f569a3835751d9a65223ef55c
skeleton <marie.bochu@akeneo.com>
2017-07-18 23:37:39 +0200

    TIP-747: fix form type in import/export

--- src/Pim/Bundle/ImportExportBundle/Form/Type/JobInstanceFormType.php
+++ src/Pim/Bundle/ImportExportBundle/Form/Type/JobInstanceFormType.php
@@ -203,7 +203,7 @@ class JobInstanceFormType extends AbstractType
         $choices = [];
         foreach ($this->jobRegistry->allByTypeGroupByConnector($this->jobType) as $connector => $jobs) {
             foreach ($jobs as $key => $job) {
-                $choices[$connector][$key] = $this->jobLabelProvider->getJobLabel($job->getName());
+                $choices[$connector][$this->jobLabelProvider->getJobLabel($job->getName())] = $key;
             }
         }
 
