6e2d3e77cdc63d7eb062dc0b225d34a004d06626
Olivier Soulet <olivier.soulet@akeneo.com>
2017-07-18 11:37:13 +0200

    API-259: Add published repo

---
+++ src/PimEnterprise/Bundle/ApiBundle/Doctrine/ORM/Repository/PublishedProductRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2017 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Bundle\ApiBundle\Doctrine\ORM\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use PimEnterprise\Component\Api\Repository\PublishedProductRepositoryInterface;
+use PimEnterprise\Component\Workflow\Repository\PublishedProductRepositoryInterface as WorkflowProductRepositoryInterface;
+
+/**
+ * @author Olivier Soulet <olivier.soulet@akeneo.com>
+ */
+class PublishedProductRepository extends EntityRepository implements PublishedProductRepositoryInterface
+{
+    /** @var WorkflowProductRepositoryInterface */
+    private $publishedProductRepository;
+
+    /**
+     * @param EntityManager                      $em
+     * @param string                             $className
+     * @param WorkflowProductRepositoryInterface $publishedProductRepository
+     */
+    public function __construct(EntityManager $em, $className, WorkflowProductRepositoryInterface $publishedProductRepository)
+    {
+        parent::__construct($em, $em->getClassMetadata($className));
+
+        $this->publishedProductRepository = $publishedProductRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findOneByIdentifier($identifier)
+    {
+        return $this->publishedProductRepository->findOneByIdentifier($identifier);
+    }
+}
