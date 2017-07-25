77c104992aef2bfd406b58294655f899ed601acf
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-07-25 15:30:46 +0200

    PIM-6335: Missing config

--- src/Pim/Component/Connector/spec/ArrayConverter/FlatToStandard/ProductModelSpec.php
+++ src/Pim/Component/Connector/spec/ArrayConverter/FlatToStandard/ProductModelSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Pim\Component\Connector\ArrayConverter\FlatToStandard;
 
+use Pim\Component\Connector\ArrayConverter\ArrayConverterInterface;
 use Pim\Component\Connector\ArrayConverter\FlatToStandard\ProductModel;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -11,5 +12,25 @@ class ProductModelSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(ProductModel::class);
+    }
+
+    function it_is_an_array_converter()
+    {
+        $this->shouldImplement(ArrayConverterInterface::class);
+    }
+
+    function it_convert_flat_product_model_to_standard_format()
+    {
+        // Mapping (categories, family variant) (pas group et family)
+
+        // Exclude association field (pas group)
+
+        // Validation du tableau de données
+
+        $this->convert([
+
+        ])->shouldReturn([
+
+        ]);
     }
 }
