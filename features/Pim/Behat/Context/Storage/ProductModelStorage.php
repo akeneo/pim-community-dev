82096b79302411fb303d3512fd8a4bbe671aa2fc
Damien Carcel <damien.carcel@akeneo.com>
2017-07-28 11:59:30 +0200

    PIM-6333: Split behat step checking product model content

--- features/Pim/Behat/Context/Storage/ProductModelStorage.php
+++ features/Pim/Behat/Context/Storage/ProductModelStorage.php
@@ -1,9 +1,11 @@
 <?php
+declare(strict_types=1);
 
 namespace Pim\Behat\Context\Storage;
 
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\RawMinkContext;
+use Pim\Component\Catalog\Model\ProductModelInterface;
 use Pim\Component\Catalog\Repository\FamilyVariantRepositoryInterface;
 use Pim\Component\Catalog\Repository\ProductModelRepositoryInterface;
 use Pim\Component\Connector\ArrayConverter\FlatToStandard\Product\AttributeColumnInfoExtractor;
@@ -38,6 +40,8 @@ class ProductModelStorage extends RawMinkContext
     }
 
     /**
+     * @param TableNode $properties
+     *
      * @Then /^there should be the following (?:|root product model|product model):$/
      */
     public function theProductShouldHaveTheFollowingValues(TableNode $properties)
@@ -47,64 +51,77 @@ class ProductModelStorage extends RawMinkContext
 
             foreach ($rawPoductModel as $propertyName => $value) {
                 if (in_array($propertyName, $this->productModelFields)) {
-                    switch ($propertyName) {
-                        case 'parent':
-                            $actualParent = $productModel->getParent();
-                            $expectedParent = $this->productModelRepository->findOneByIdentifier($value);
-
-                            assertSame($actualParent, $expectedParent);
-                            break;
-                        case 'family_variant':
-                            $actualFamilyVariant = $productModel->getFamilyVariant();
-                            $expectedFamilyVariant = $this->familyVariantRepository->findOneByIdentifier($value);
-
-                            assertSame($actualFamilyVariant, $expectedFamilyVariant);
-                            break;
-                        case 'categories':
-                            $actualCategoryCodes = $productModel->getCategoryCodes();
-                            $expectedCategoryCodes = explode(',', $value);
-
-                            assertSame($actualCategoryCodes, $expectedCategoryCodes);
-                            break;
-                    }
+                    $this->checkProductModelField($productModel, $propertyName, $value);
                 } else {
-                    $infos = $this->attributeColumnInfoExtractor->extractColumnInfo($propertyName);
-
-                    $attribute = $infos['attribute'];
-                    $priceCurrency = isset($infos['price_currency']) ? $infos['price_currency'] : null;
-                    $productValue = $productModel->getValue(
-                        $attribute->getCode(),
-                        $infos['locale_code'],
-                        $infos['scope_code']
-                    );
-
-                    if ('' === $value) {
-                        assertEmpty((string)$productValue);
-                    } elseif ('media' === $attribute->getBackendType()) {
-                        // media filename is auto generated during media handling and cannot be guessed
-                        // (it contains a timestamp)
-                        if ('**empty**' === $value) {
-                            assertEmpty((string)$productValue);
-                        } else {
-                            assertTrue(
-                                null !== $productValue->getData() &&
-                                false !== strpos($productValue->getData()->getOriginalFilename(), $value)
-                            );
-                        }
-                    } elseif ('prices' === $attribute->getBackendType() && null !== $priceCurrency) {
-                        // $priceCurrency can be null if we want to test all the currencies at the same time
-                        // in this case, it's a simple string comparison
-                        // example: 180.00 EUR, 220.00 USD
-
-                        $price = $productValue->getPrice($priceCurrency);
-                        assertEquals($value, $price->getData());
-                    } elseif ('date' === $attribute->getBackendType()) {
-                        assertEquals($value, $productValue->getData()->format('Y-m-d'));
-                    } else {
-                        assertEquals($value, (string)$productValue);
-                    }
+                    $this->checkProductModelValue($productModel, $propertyName, $value);
                 }
             }
+        }
+    }
+
+    /**
+     * @param ProductModelInterface $productModel
+     * @param string                $propertyName
+     * @param mixed                 $value
+     *
+     * @throws \PHPUnit_Framework_Exception
+     */
+    private function checkProductModelField(ProductModelInterface $productModel, string $propertyName, $value): void
+    {
+        switch ($propertyName) {
+            case 'parent':
+                $actualParent = $productModel->getParent();
+                $expectedParent = $this->productModelRepository->findOneByIdentifier($value);
+
+                assertSame($actualParent, $expectedParent);
+                break;
+            case 'family_variant':
+                $actualFamilyVariant = $productModel->getFamilyVariant();
+                $expectedFamilyVariant = $this->familyVariantRepository->findOneByIdentifier($value);
+
+                assertSame($actualFamilyVariant, $expectedFamilyVariant);
+                break;
+            case 'categories':
+                $actualCategoryCodes = $productModel->getCategoryCodes();
+                $expectedCategoryCodes = explode(',', $value);
+
+                assertSame($actualCategoryCodes, $expectedCategoryCodes);
+                break;
+        }
+    }
+
+    /**
+     * @param ProductModelInterface $productModel
+     * @param string                $propertyName
+     * @param mixed                 $value
+     *
+     * @throws \PHPUnit_Framework_Exception
+     */
+    private function checkProductModelValue(ProductModelInterface $productModel, string $propertyName, $value): void
+    {
+        $infos = $this->attributeColumnInfoExtractor->extractColumnInfo($propertyName);
+
+        $attribute = $infos['attribute'];
+        $priceCurrency = isset($infos['price_currency']) ? $infos['price_currency'] : null;
+        $productValue = $productModel->getValue(
+            $attribute->getCode(),
+            $infos['locale_code'],
+            $infos['scope_code']
+        );
+
+        if ('' === $value) {
+            assertEmpty((string)$productValue);
+        } elseif ('prices' === $attribute->getBackendType() && null !== $priceCurrency) {
+            // $priceCurrency can be null if we want to test all the currencies at the same time
+            // in this case, it's a simple string comparison
+            // example: 180.00 EUR, 220.00 USD
+
+            $price = $productValue->getPrice($priceCurrency);
+            assertEquals($value, $price->getData());
+        } elseif ('date' === $attribute->getBackendType()) {
+            assertEquals($value, $productValue->getData()->format('Y-m-d'));
+        } else {
+            assertEquals($value, (string)$productValue);
         }
     }
 }
