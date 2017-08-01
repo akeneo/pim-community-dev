7085e9b1ff7e7be2502b7af175061b115066391f
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-08-01 15:29:05 +0200

    Fix PimFormatterExtension php-cs

--- features/Pim/Behat/Extension/PimFormatter/PimFormatterExtension.php
+++ features/Pim/Behat/Extension/PimFormatter/PimFormatterExtension.php
@@ -15,25 +15,29 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PimFormatterExtension implements ExtensionInterface {
+class PimFormatterExtension implements ExtensionInterface
+{
 
     /**
      * {@inheritdoc}
      */
-    public function process(ContainerBuilder $container) {
+    public function process(ContainerBuilder $container)
+    {
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getConfigKey() {
+    public function getConfigKey()
+    {
         return "pim_formatter";
     }
 
     /**
      * {@inheritdoc}
      */
-    public function initialize(ExtensionManager $extensionManager) {
+    public function initialize(ExtensionManager $extensionManager)
+    {
         $outputExtension = $extensionManager->getExtension('formatters');
         $outputExtension->registerFormatterFactory(new PimFormatterFactory());
     }
@@ -41,12 +45,14 @@ class PimFormatterExtension implements ExtensionInterface {
     /**
      * {@inheritdoc}
      */
-    public function configure(ArrayNodeDefinition $builder) {
+    public function configure(ArrayNodeDefinition $builder)
+    {
     }
 
     /**
      * {@inheritdoc}
      */
-    public function load(ContainerBuilder $container, array $config) {
+    public function load(ContainerBuilder $container, array $config)
+    {
     }
 }
