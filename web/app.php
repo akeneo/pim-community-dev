727a7fc1693332a759d34969a4eda0af000ad652
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-08-01 11:48:08 +0200

    change debugmode to false in prod

--- web/app.php
+++ web/app.php
@@ -7,7 +7,7 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
