bc79225ae0fde631c55eda2bc0e08f732200e202
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-08-03 09:32:00 +0200

    Preparing version 1.5.26

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.25';
+    const VERSION = '1.5.26';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
