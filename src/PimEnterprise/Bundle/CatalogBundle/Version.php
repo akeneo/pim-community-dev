7e411c817098c3dee7ceda539bd39936c1664018
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-08-03 09:45:39 +0200

    Preparing version 1.5.26

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.25';
+    const VERSION = '1.5.26';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Sunflower';
