e549b80ca3dab6c63b881d7ce3d9b34a45c708c7
Olivier Soulet <olivier.soulet@akeneo.com>
2017-08-07 11:46:03 +0200

    Fix style conf

--- .php_cs.php
+++ .php_cs.php
@@ -1,11 +1,16 @@
 <?php
 
 return PhpCsFixer\Config::create()
-    ->setRules(array(
-        '@PSR2' => true,
-        'linebreak_after_opening_tag' => true,
-        'ordered_imports' => true,
-    ))
+    ->setRules(
+        [
+            '@PSR2' => true,
+            'linebreak_after_opening_tag' => true,
+            'ordered_imports' => true,
+            'method_argument_space' => [
+                'ensure_fully_multiline' => false
+            ],
+        ]
+    )
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->name('*.php')
