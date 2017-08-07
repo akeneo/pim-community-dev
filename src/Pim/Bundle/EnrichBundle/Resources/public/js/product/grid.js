5b2df75e57d07d32fd9f348b78cf157c3c5767ec
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-08-07 16:34:01 +0200

    TIP-733: Add the parent form extension for product grid

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/grid.js
@@ -0,0 +1,11 @@
+/**
+ * Product grid parent view
+ *
+ * @author    Tamara Robichet <tamara.robichet@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(['backbone'], function (Backbone) {
+    console.log('Product grid parent component');
+    return Backbone.View.extend();
+});
