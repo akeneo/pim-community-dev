75ce9499ba2064e21bce24dcf9d6acc06703e894
tamarasaurus <tamara@robichet.com>
2017-08-08 15:37:06 +0200

    TIP-733: Add product grid locale-switcher extension

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/grid/locale-switcher.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/grid/locale-switcher.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Extension to switch locales on the product grid
+ * Extension to render a list of activated locales used for the product grid. 
  *
  * @author    Tamara Robichet <tamara.robichet@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
@@ -12,31 +12,80 @@ define([
         'underscore',
         'pim/form',
         'pim/template/product/grid/locale-switcher',
+        'pim/fetcher-registry',
+        'pim/i18n',
+        'routing'
     ],
     function (
         $,
         _,
         BaseForm,
         template,
+        FetcherRegistry,
+        i18n,
+        Routing
     ) {
         return BaseForm.extend({
             template: _.template(template),
+            config: {},
 
-            initialize: function (config) {
+            initialize(config) {
                 this.config = config.config;
 
                 return BaseForm.prototype.initialize.apply(this, arguments);
             },
 
-            configure: function () {
-                return BaseForm.prototype.configure.apply(this, arguments);
-            },
-
             /**
              * {@inheritdoc}
              */
-            render: function () {
-                this.$el.empty().append(this.template({}));
+            render() {
+                this.fetchLocales().then((locales) => {
+                    const currentLocaleCode = this.getCurrentLocale() || _.first(locales).code;
+
+                    this.$el.empty().append(this.template({
+                        locales,
+                        currentLocaleCode,
+                        i18n,
+                        getDisplayName: this.getDisplayName,
+                        generateUrl: this.generateUrl.bind(this)
+                    }));
+                });
             },
+
+            /**
+             * Fetch the activated locales to render in the list
+             * @return {Array} An array of activated locales
+             */
+            fetchLocales() {
+                return FetcherRegistry.getFetcher('locale').fetchActivated();
+            },
+
+            /**
+             * Transforms the locale code
+             * @param  {String} localeCode The original localeCode like fr_FR
+             * @return {String}            The shortened localeCode like fr
+             */
+            getDisplayName(localeCode) {
+                return localeCode.split('_')[0];
+            },
+
+            /**
+             * Generates a product grid url with the locale code
+             * @param  {String} localeCode The locale code - e.g. en_US
+             * @return {String}        A url like #/enrich/product/?dataLocale=en_US
+             */
+            generateUrl(localeCode) {
+                const { localeParamName } = this.config;
+
+                return Routing.generate(this.config.routeName, { [localeParamName]: localeCode });
+            },
+
+            /**
+             * @TODO - Get this information elsewhere
+             * @return {String} Returns a string with the locale code e.g. en_US
+             */
+            getCurrentLocale() {
+                return window.location.hash.split(`?${this.config.localeParamName}=`)[1];
+            }
         });
     });
