66f12ca49c7667b2e702b71522f2e5e61579b98f
Damien Carcel <damien.carcel@akeneo.com>
2017-08-16 15:42:37 +0200

    Update Docker helper scripts

--- bin/docker/pim-front.sh
+++ bin/docker/pim-front.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-currentDir=$(dirname "$0")
-
-echo "Clean previous assets"
-
-rm -rf ${currentDir}/../../app/cache/*
-
-echo "Install the assets"
+docker-composer exec akeneo --env=prod cache:clear --no-warmup
+docker-composer exec akeneo --env=dev cache:clear --no-warmup
+docker-composer exec akeneo-behat --env=behat cache:clear --no-warmup
+docker-composer exec akeneo-behat --env=test cache:clear --no-warmup
 
 docker-compose exec akeneo app/console --env=prod pim:installer:assets --symlink
