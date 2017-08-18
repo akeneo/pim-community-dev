87e45796f83dcecebdd771a2bc6f76e935cca813
skeleton <marie.bochu@akeneo.com>
2017-08-18 15:45:33 +0200

    API-72: add integration tests on products values

--- src/PimEnterprise/Component/Catalog/tests/integration/Security/Product/ValuesIntegration.php
+++ src/PimEnterprise/Component/Catalog/tests/integration/Security/Product/ValuesIntegration.php
@@ -1,11 +1,164 @@
 <?php
 
-namespace PimEnterprise\Component\Catalog\tests\integration\Security\Updater;
+namespace PimEnterprise\Component\Catalog\tests\integration\Security\Product;
 
-use PimEnterprise\Component\Catalog\tests\integration\Security\Product\AbstractSecurityTestCase;
-
-class ProductUpdaterIntegration extends AbstractSecurityTestCase
+/**
+ * +------------------------------------------+-----------------------+
+ * |              Attributes                  |         Roles         |
+ * |                                          |  Redactor |  Manager  |
+ * +------------------------------------------+-----------------------+
+ * | a_text                                   | View,Edit | View,Edit |
+ * | a_number_float                           |    View   | View,Edit |
+ * | a_localized_and_scopable_text_area-fr_FR |    View   | View,Edit |
+ * | a_localized_and_scopable_text_area-en_US | View,Edit | View,Edit |
+ * | a_localized_and_scopable_text_area-de_DE |     -     | View,Edit |
+ * +------------------------------------------+-----------------------+
+ */
+class ValuesIntegration extends AbstractSecurityTestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->saveProduct('product_a', [
+            'values' => [
+                'a_text' => [
+                    ['data' => 'text', 'locale' => null, 'scope' => null]
+                ],
+                'a_number_float' => [
+                    ['data' => 15, 'locale' => null, 'scope' => null]
+                ],
+                'a_localized_and_scopable_text_area' => [
+                    ['data' => 'mon text', 'locale' => 'fr_FR', 'scope' => 'ecommerce'],
+                    ['data' => 'mein text', 'locale' => 'de_DE', 'scope' => 'ecommerce'],
+                ]
+            ]
+        ]);
+    }
+
+    public function testAddNewAttributeOnProduct()
+    {
+        $this->generateToken('mary');
+        $product = $this->getProduct('product_a');
+
+        $this->updateProduct($product, [
+            'values' => [
+                'a_date' => [
+                    ['data' => '2017-03-03', 'locale' => null, 'scope' => null]
+                ]
+            ]
+        ]);
+        $this->get('pim_catalog.saver.product')->save($product);
+
+        $this->assertSame($product->getRawValues(), [
+            'sku' => [
+                '<all_channels>' => [
+                    '<all_locales>' => 'product_a'
+                ]
+            ],
+            'a_text' => [
+                '<all_channels>' => [
+                    '<all_locales>' => 'text'
+                ]
+            ],
+            'a_number_float' => [
+                '<all_channels>' => [
+                    '<all_locales>' => '15.0000'
+                ]
+            ],
+            'a_localized_and_scopable_text_area' => [
+                'ecommerce' => [
+                    'fr_FR' => 'mon text',
+                    'de_DE' => 'mein text',
+                ]
+            ],
+            'a_date' => [
+                '<all_channels>' => [
+                    '<all_locales>' => '2017-03-03T00:00:00+01:00'
+                ]
+            ]
+        ]);
+    }
+
+    public function testDeleteAttributeOnProduct()
+    {
+        $this->generateToken('mary');
+        $product = $this->getProduct('product_a');
+
+        $this->updateProduct($product, [
+            'values' => [
+                'a_text' => [
+                    ['data' => null, 'locale' => null, 'scope' => null]
+                ]
+            ]
+        ]);
+        $this->get('pim_catalog.saver.product')->save($product);
+
+        $this->assertSame($product->getRawValues(), [
+            'sku' => [
+                '<all_channels>' => [
+                    '<all_locales>' => 'product_a'
+                ]
+            ],
+            'a_number_float' => [
+                '<all_channels>' => [
+                    '<all_locales>' => '15.0000'
+                ]
+            ],
+            'a_localized_and_scopable_text_area' => [
+                'ecommerce' => [
+                    'fr_FR' => 'mon text',
+                    'de_DE' => 'mein text',
+                ]
+            ],
+            'a_text' => [
+                '<all_channels>' => [
+                    '<all_locales>' => null
+                ]
+            ]
+        ]);
+    }
+
+    public function testAddNewLocalizableAttributeOnProduct()
+    {
+        $this->generateToken('mary');
+        $product = $this->getProduct('product_a');
+
+        $this->updateProduct($product, [
+            'values' => [
+                'a_localized_and_scopable_text_area' => [
+                    ['data' => 'my text', 'locale' => 'en_US', 'scope' => 'ecommerce']
+                ]
+            ]
+        ]);
+        $this->get('pim_catalog.saver.product')->save($product);
+
+        $this->assertSame($product->getRawValues(), [
+            'sku' => [
+                '<all_channels>' => [
+                    '<all_locales>' => 'product_a'
+                ]
+            ],
+            'a_text' => [
+                '<all_channels>' => [
+                    '<all_locales>' => 'text'
+                ]
+            ],
+            'a_number_float' => [
+                '<all_channels>' => [
+                    '<all_locales>' => '15.0000'
+                ]
+            ],
+            'a_localized_and_scopable_text_area' => [
+                'ecommerce' => [
+                    'fr_FR' => 'mon text',
+                    'de_DE' => 'mein text',
+                    'en_US' => 'my text',
+                ]
+            ]
+        ]);
+    }
+
     /**
      * @expectedException \Akeneo\Component\StorageUtils\Exception\UnknownPropertyException
      * @expectedExceptionMessage Property "not_found" does not exist.
