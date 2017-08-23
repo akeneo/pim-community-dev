3343958cf2b64628ea294f7433d025f2942c955f
Damien Carcel <damien.carcel@akeneo.com>
2017-08-23 16:49:27 +0200

    Fix container name in docker helper scripts

--- bin/docker/pim-front.sh
+++ bin/docker/pim-front.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-docker-compose exec akeneo app/console --env=prod cache:clear --no-warmup
-docker-compose exec akeneo app/console --env=dev cache:clear --no-warmup
-docker-compose exec akeneo-behat app/console --env=behat cache:clear --no-warmup
-docker-compose exec akeneo-behat app/console --env=test cache:clear --no-warmup
+docker-compose exec fpm bin/console --env=prod cache:clear --no-warmup
+docker-compose exec fpm bin/console --env=dev cache:clear --no-warmup
+docker-compose exec fpm bin/console --env=behat cache:clear --no-warmup
+docker-compose exec fpm bin/console --env=test cache:clear --no-warmup
 
-docker-compose exec akeneo app/console --env=prod pim:installer:assets --symlink --clean
+docker-compose exec fpm bin/console --env=prod pim:installer:assets --symlink --clean
 
 docker-compose run --rm node npm install
 docker-compose run --rm node npm run webpack
