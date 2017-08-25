e8b3527dab46e0e0fe6afe2db7815fd40ae63f93
Julien Sanchez <sumbobyboys@gmail.com>
2017-08-25 16:20:53 +0200

    PIM-6670: remove old js specs

--- frontend/jasmine-runner.js
+++
@@ -1,7 +0,0 @@
-var requireAll = function(requireContext) {
-    requireContext.keys().map(requireContext)
-}
-
-requireAll(require.context('./dynamic/', true, /^.*\/Bundle\/.*\/Resources\/public\/spec\/.*\/.*\.js$/))
-
-Backbone.$ = $
