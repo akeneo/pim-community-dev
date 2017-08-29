bc3e5468de1a16c1a7375ed5482877c3c0357184
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2017-08-29 14:36:38 +0200

    PIM-6352: introduce cursor factory for products and product models

---
+++ src/Pim/Bundle/EnrichBundle/Elasticsearch/CursorFactory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Pim\Bundle\EnrichBundle\Elasticsearch;
+
+use Akeneo\Bundle\ElasticsearchBundle\Client;
+use Akeneo\Component\StorageUtils\Cursor\CursorFactoryInterface;
+use Akeneo\Component\StorageUtils\Repository\CursorableRepositoryInterface;
+
+/**
+ * @author    Julien Janvier <j.janvier@gmail.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CursorFactory implements CursorFactoryInterface
+{
+    /** @var Client */
+    protected $searchEngine;
+
+    /** @var CursorableRepositoryInterface */
+    private $productRepository;
+
+    /** @var CursorableRepositoryInterface */
+    private $productModelRepository;
+
+    /** @var string */
+    protected $cursorClassName;
+
+    /** @var int */
+    protected $pageSize;
+
+    /** @var string */
+    protected $indexType;
+
+    /**
+     * @param Client                        $searchEngine
+     * @param CursorableRepositoryInterface $productRepository
+     * @param CursorableRepositoryInterface $productModelRepository
+     * @param int                           $pageSize
+     * @param string                        $indexType
+     */
+    public function __construct(
+        Client $searchEngine,
+        CursorableRepositoryInterface $productRepository,
+        CursorableRepositoryInterface $productModelRepository,
+        int $pageSize,
+        string $indexType
+    ) {
+        $this->searchEngine = $searchEngine;
+        $this->productRepository = $productRepository;
+        $this->productModelRepository = $productModelRepository;
+        $this->pageSize = $pageSize;
+        $this->indexType = $indexType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createCursor($queryBuilder, array $options = [])
+    {
+        $pageSize = !isset($options['page_size']) ? $this->pageSize : $options['page_size'];
+
+        $queryBuilder['_source'] = array_merge($queryBuilder['_source'], ['product_type']);
+
+        return new Cursor(
+            $this->searchEngine,
+            $this->productRepository,
+            $this->productModelRepository,
+            $queryBuilder,
+            $this->indexType,
+            $pageSize
+        );
+    }
+}
