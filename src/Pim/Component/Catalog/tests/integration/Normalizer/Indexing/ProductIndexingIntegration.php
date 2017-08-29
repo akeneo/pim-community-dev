7c2b9a65fa6b0b5c17dc4efb1afa20b702cd47f5
Julien Sanchez <sumbobyboys@gmail.com>
2017-08-29 14:22:11 +0200

    PIM-6621: fix integration tests

--- src/Pim/Component/Catalog/tests/integration/Normalizer/Indexing/ProductIndexingIntegration.php
+++ src/Pim/Component/Catalog/tests/integration/Normalizer/Indexing/ProductIndexingIntegration.php
@@ -32,7 +32,6 @@ class ProductIndexingIntegration extends TestCase
         $expected = [
             'id'            => '47',
             'identifier'    => 'bar',
-            'label'         => 'bar',
             'created'       => $date->format('c'),
             'updated'       => $date->format('c'),
             'family'        => null,
@@ -58,7 +57,6 @@ class ProductIndexingIntegration extends TestCase
         $expected = [
             'id'            => '48',
             'identifier'    => 'baz',
-            'label'         => 'baz',
             'created'       => $date->format('c'),
             'updated'       => $date->format('c'),
             'family'        => null,
@@ -84,7 +82,6 @@ class ProductIndexingIntegration extends TestCase
         $expected = [
             'id'            => '49',
             'identifier'    => 'foo',
-            'label'         => 'foo',
             'created'       => $date->format('c'),
             'updated'       => $date->format('c'),
             'family'        => [
