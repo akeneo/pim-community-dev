51c523e34efb7ea994bb41bf52d9287c34a78233
skeleton <marie.bochu@akeneo.com>
2017-08-30 17:54:24 +0200

    API-246: throw an exception if data have changed on a viewable product

--- src/PimEnterprise/Bundle/ApiBundle/tests/integration/Controller/Product/UpsertProductWithPermissionsIntegration.php
+++ src/PimEnterprise/Bundle/ApiBundle/tests/integration/Controller/Product/UpsertProductWithPermissionsIntegration.php
@@ -188,11 +188,12 @@ JSON;
         $this->assertJsonStringEqualsJsonString($expected, $client->getResponse()->getContent());
     }
 
-    public function testToUpdateAViewableProduct()
+    public function testToUpdateAViewableProductWithChange()
     {
         $client = $this->createAuthenticatedClient([], [], null, null, 'mary', 'mary');
 
-        $client->request('PATCH', 'api/rest/v1/products/product_viewable_by_everybody_1', [], [], [], '{}');
+        $data = '{"enabled": false}';
+        $client->request('PATCH', 'api/rest/v1/products/product_viewable_by_everybody_1', [], [], [], $data);
         $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
 
         $expected = <<<JSON
