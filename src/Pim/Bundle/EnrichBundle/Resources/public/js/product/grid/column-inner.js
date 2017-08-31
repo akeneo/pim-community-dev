d23c320db0449d8fe42639b05169c12e6df20a04
pierallard <pierallard@gmail.com>
2017-08-31 14:53:17 +0200

    [PIM-6574] Set temporary template for product grid column

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/grid/column-inner.js
@@ -0,0 +1,30 @@
+/**
+ * Displays the inner of the products grid column
+ *
+ * @author    Pierre Allard <pierre.allard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(
+    [
+        'underscore',
+        'pim/form',
+        'pim/template/product/grid/column-inner'
+    ],
+    function(
+        _,
+        BaseForm,
+        template
+    ) {
+        return BaseForm.extend({
+            template: _.template(template),
+
+            /**
+             * {@inheritdoc}
+             */
+            render() {
+                this.$el.html(this.template);
+            }
+        });
+    }
+);
