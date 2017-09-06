e87dd9dfe5938c542cf24ce54a9dbe24ef5ba2de
Pierre Allard <pierallard@gmail.com>
2017-09-06 18:22:44 +0200

    [PIM-6555] Do some cleaning on AknBadge

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/enabled-cell.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/cell/enabled-cell.js
@@ -16,7 +16,7 @@ define(['oro/datagrid/string-cell', 'oro/translator'],
                 var value = this.formatter.fromRaw(this.model.get(this.column.get("name")));
                 var enabled = true === value ? 'enabled' : 'disabled';
 
-                this.$el.empty().html('<div class="AknBadge AknBadge--round AknBadge--' + enabled + ' status-' + enabled + '">' +
+                this.$el.empty().html('<div class="AknBadge AknBadge--' + enabled + ' status-' + enabled + '">' +
                     '<i class="AknBadge-icon icon-status-' + enabled + ' icon-circle"></i>' + __(enabled) + '</div>');
 
                 return this;
