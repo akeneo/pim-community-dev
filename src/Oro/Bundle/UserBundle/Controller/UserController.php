eb6600a47cb8c7c1f70ae0e3eac09150b719e041
Olivier Soulet <olivier.soulet@akeneo.com>
2017-09-08 15:29:59 +0200

    PIM-6258: Fix issues onuser profile permission

--- src/Oro/Bundle/UserBundle/Controller/UserController.php
+++ src/Oro/Bundle/UserBundle/Controller/UserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserController extends Controller
@@ -63,11 +64,20 @@ class UserController extends Controller
      * Edit user form
      *
      * @Template
-     * @AclAncestor("pim_user_user_edit")
      */
     public function updateAction($id)
     {
-        return $this->update($id);
+        $user = $this->get('pim_user.repository.user')->findOneBy(['id' => $id]);
+
+        if ($this->getUser()->getUsername() !== $user->getUsername()) {
+            $authorizationChecker = $this->get('security.authorization_checker');
+
+            if (false === $authorizationChecker->isGranted('EDIT', $user)) {
+                throw new AccessDeniedException();
+            }
+        }
+
+        return $this->update($user);
     }
 
     /**
@@ -105,7 +115,6 @@ class UserController extends Controller
             return new JsonResponse('', 403);
         }
     }
-
 
     /**
      * @param mixed  $user
