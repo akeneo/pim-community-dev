4f965b1c7d6ee684df30c7db92fa7ab9449cf8d4
Nicolas Dupont <nicolas@akeneo.com>
2017-09-22 15:21:36 +0200

    PIM-6737: remove categories field from the product model export

--- src/Pim/Component/Connector/ArrayConverter/StandardToFlat/ProductModel.php
+++ src/Pim/Component/Connector/ArrayConverter/StandardToFlat/ProductModel.php
@@ -31,9 +31,6 @@ class ProductModel extends AbstractSimpleArrayConverter implements ArrayConverte
     protected function convertProperty($property, $data, array $convertedItem, array $options)
     {
         switch ($property) {
-            case 'categories':
-                $convertedItem[$property] = implode(',', $data);
-                break;
             case 'code':
             case 'family_variant':
             case 'parent':
@@ -44,6 +41,7 @@ class ProductModel extends AbstractSimpleArrayConverter implements ArrayConverte
                     $convertedItem = $convertedItem + $this->valueConverter->convertAttribute($code, $attribute);
                 }
                 break;
+            case 'categories':
             case 'created':
             case 'updated':
                 break;
