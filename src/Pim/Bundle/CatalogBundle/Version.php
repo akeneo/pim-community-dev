d094602dc9fc3383937e9150aeeae6e86eff53a9
Damien Carcel <damien.carcel@akeneo.com>
2017-09-22 09:25:24 +0200

    Preparing version 1.5.27

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.5.26';
+    const VERSION = '1.5.27';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Fresh Hare';
