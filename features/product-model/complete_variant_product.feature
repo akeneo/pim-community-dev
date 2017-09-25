c2f7f9fb1098719219f46fdc4fd1daba9e5489f8
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-09-25 14:23:08 +0200

    PIM-6560: Display complete variant product on the product model edit form

--- features/product-model/complete_variant_product.feature
+++ features/product-model/complete_variant_product.feature
@@ -7,11 +7,22 @@ Feature: Complete variant product
   Background:
     Given a "catalog_modeling" catalog configuration
 
+  Scenario: Update the complete variant product when I change the locale and the channel
+    When I am logged in as "Mary"
+    And I edit the "model-braided-hat" product model
+    Then I should see the text "2 / 2 variant products"
+    When I switch the locale to "fr_FR"
+    Then I should see the text "1 / 2 variant product"
+    And I switch the scope to "mobile"
+    And I switch the locale to "de_DE"
+    Then I should see the text "0 / 2 variant product"
+
   Scenario: Try to display the complete variant product for a product model without child
     When I am logged in as "Mary"
     And I am on the products page
     And I show the filter "Model name"
     And I filter by "Model name" with operator "contains" and value "minerva"
+    And I should see the text "0/0"
 
   Scenario: Try to display the complete variant product for a product
     When I am logged in as "Mary"
---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product-model/form/complete-variant-product.js
@@ -0,0 +1,95 @@
+/**
+ * Complete variant product extension
+ *
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(
+    [
+        'underscore',
+        'oro/translator',
+        'pim/form',
+        'pim/template/product-model/complete-variant-product',
+        'pim/user-context'
+    ],
+    function (
+        _,
+        __,
+        BaseForm,
+        template,
+        UserContext
+    ) {
+        return BaseForm.extend({
+            template: _.template(template),
+
+            /**
+             * {@inheritdoc}
+             */
+            initialize: function (config) {
+                UserContext.off('change:catalogLocale change:catalogScope', this.render);
+
+                this.listenTo(UserContext, 'change:catalogLocale change:catalogScope', this.render);
+
+                return BaseForm.prototype.configure.apply(this, arguments);
+            },
+
+            /**
+             * {@inheritdoc}
+             */
+            render: function () {
+                const variantProductCompleteness = this.getFormData().meta.variant_product_completenesses;
+                const channel = UserContext.get('catalogScope');
+                const locale = UserContext.get('catalogLocale');
+                const completeProducts = variantProductCompleteness.completenesses[channel][locale];
+                const totalProducts = variantProductCompleteness.total;
+
+                this.$el.html(
+                    this.template({
+                        complete: completeProducts,
+                        total: totalProducts,
+                        color: this.badgeColor(completeProducts, totalProducts),
+                        label: this.badgeLabel(completeProducts)
+                    })
+                );
+            },
+
+            /**
+             * Return the color of the badge
+             *
+             * @param {int} completeProducts
+             * @param {int} totalProducts
+             *
+             * @returns {string}
+             */
+            badgeColor: function (completeProducts, totalProducts) {
+                const ratio = completeProducts / totalProducts;
+                let color = 'warning';
+
+                if (1 === ratio) {
+                    color = 'success';
+                } else if (0 === ratio || 0 === totalProducts) {
+                    color = 'important';
+                }
+
+                return color;
+            },
+
+            /**
+             * Return the label of the badge
+             *
+             * @param {object} completeProducts
+             *
+             * @returns {string}
+             */
+            badgeLabel: function (completeProducts) {
+                let label = __('pim_enrich.form.product_model.complete_variant_product');
+
+                if (1 < completeProducts) {
+                    label = __('pim_enrich.form.product_model.complete_variant_products')
+                }
+
+                return label;
+            }
+        });
+    }
+);
