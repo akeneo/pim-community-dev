8886ad402d83c80b581f5bd3bbbb1ab9958bb18c
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-09-26 15:34:10 +0200

    Rename 1.8 into 2.0 for the migratin files

--- upgrades/1.7-2.0/Version_2_0_20170821122627_add_client_in_access_token_table.php
+++ upgrades/1.7-2.0/Version_2_0_20170821122627_add_client_in_access_token_table.php
@@ -5,7 +5,7 @@ namespace Pim\Upgrade\Schema;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-class Version_1_8_20170821122627_add_client_in_access_token_table extends AbstractMigration
+class Version_2_0_20170821122627_add_client_in_access_token_table extends AbstractMigration
 {
     /**
      * @param Schema $schema
