3dde51a6610e9e2cffbece4e9b44e87a7ff96f71
Samir Boulil <samir.boulil@akeneo.com>
2017-09-27 16:58:58 +0200

    PIM-6773: Disable toFillFields header for product models

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/form/common/attributes.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/form/common/attributes.js
@@ -202,6 +202,12 @@ define(
                             AttributeGroupManager.getAttributeGroupsForObject(data),
                             toFillFieldProvider.getFields(this.getRoot(), data.values)
                         ).then((attributeGroups, fieldsToFill) => {
+
+                            // PIM-6773: Remove to activate fields to fill in product models.
+                            if (this.getFormData().meta.model_type === 'product_model') {
+                                fieldsToFill = [];
+                            }
+
                             const sections = _.values(
                                 fields.reduce(groupFieldsBySection(attributeGroups, fieldsToFill), {})
                             );
