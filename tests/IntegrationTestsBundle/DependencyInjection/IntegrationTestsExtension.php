dc82f89ff868dbf7e2cfd407f15bc4f9238796b5
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-09-29 21:21:01 +0200

    Initialise a bundle and a kernel for the tests

---
+++ tests/IntegrationTestsBundle/DependencyInjection/IntegrationTestsExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Akeneo\Test\IntegrationTestsBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class IntegrationTestsExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ tests/IntegrationTestsBundle/IntegrationTestsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Akeneo\Test\IntegrationTestsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle for the integration tests, allowing to have a DI in the integration tests.
+ *
+ * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class IntegrationTestsBundle extends Bundle
+{
+}
