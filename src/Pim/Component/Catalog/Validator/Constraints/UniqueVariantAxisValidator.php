bf00d760eba22f9b8fa845d55a9343d95982b822
Arnaud Langlade <arnaud.langlade@akeneo.com>
2017-10-02 17:36:26 +0200

    PIM-6805: Rename SiblingUniqueVariantAxesValidator to UniqueVariantAxisValidator

--- src/Pim/Component/Catalog/Validator/Constraints/UniqueVariantAxisValidator.php
+++ src/Pim/Component/Catalog/Validator/Constraints/UniqueVariantAxisValidator.php
@@ -12,13 +12,11 @@ use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 /**
- * TODO: Rename to UniqueVariantAxisValidator once VariantGroup are dropped.
- *
  * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class SiblingUniqueVariantAxesValidator extends ConstraintValidator
+class UniqueVariantAxisValidator extends ConstraintValidator
 {
     /** @var EntityWithFamilyVariantAttributesProvider */
     private $axesProvider;
