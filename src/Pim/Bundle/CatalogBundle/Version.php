4a66c39bcd9df66a10f48b3a812dc99dfeb2b767
Julien Sanchez <sumbobyboys@gmail.com>
2017-10-05 16:13:02 +0200

    Preparing version 2.0.1

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.0';
+    const VERSION = '2.0.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
