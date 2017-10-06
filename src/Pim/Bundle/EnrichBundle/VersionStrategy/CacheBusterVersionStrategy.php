7a17f13413bc96a4e2fb1a19e701d52cc4b9aec8
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-10-06 11:44:28 +0200

    TIP-808: Allow passing in version provider for other editions

--- src/Pim/Bundle/EnrichBundle/VersionStrategy/CacheBusterVersionStrategy.php
+++ src/Pim/Bundle/EnrichBundle/VersionStrategy/CacheBusterVersionStrategy.php
@@ -2,16 +2,32 @@
 
 namespace Pim\Bundle\EnrichBundle\VersionStrategy;
 
-use Pim\Bundle\CatalogBundle\Version;
+use Pim\Bundle\CatalogBundle\VersionProviderInterface;
 use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
 
 class CacheBusterVersionStrategy implements VersionStrategyInterface
 {
-    public function getVersion($path)
-    {
-        return Version::VERSION;
+    /** @var VersionProviderInterface */
+    protected $versionProvider;
+
+    /**
+     * @param VersionProviderInterface $versionProvider
+     */
+    public function __construct(VersionProviderInterface $versionProvider) {
+        $this->versionProvider = $versionProvider;
     }
 
+    /**
+     * @param string $path
+     */
+    public function getVersion($path)
+    {
+        return $this->versionProvider->getPatch();
+    }
+
+    /**
+     * @param string $path
+     */
     public function applyVersion($path)
     {
         $version = $this->getVersion($path);
