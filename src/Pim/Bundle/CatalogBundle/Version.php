0443c3e01e69eabba50a8bc700792a781168db62
pierallard <pierallard@gmail.com>
2017-10-10 11:13:55 +0200

    Preparing version 1.7.10

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.9';
+    const VERSION = '1.7.10';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
