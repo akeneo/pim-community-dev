b6f4241b764ddcc7ee330e62a811799478c29655
skeleton <marie.bochu@akeneo.com>
2017-10-16 11:41:00 +0200

    API-352: add filter for values

---
+++ src/PimEnterprise/Component/Catalog/Security/Filter/NotGrantedValuesFilter.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2017 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PimEnterprise\Component\Catalog\Security\Filter;
+
+use Akeneo\Component\StorageUtils\Exception\InvalidObjectException;
+use Akeneo\Component\StorageUtils\Repository\CachedObjectRepositoryInterface;
+use Doctrine\Common\Util\ClassUtils;
+use Pim\Component\Catalog\Model\EntityWithValuesInterface;
+use PimEnterprise\Component\Security\Attributes;
+use PimEnterprise\Component\Security\NotGrantedDataFilterInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+/**
+ * Filter not granted values
+ *
+ * @author Marie Bochu <marie.bochu@akeneo.com>
+ */
+class NotGrantedValuesFilter implements NotGrantedDataFilterInterface
+{
+    /** @var AuthorizationCheckerInterface */
+    private $authorizationChecker;
+
+    /** @var CachedObjectRepositoryInterface */
+    private $localeRepository;
+
+    /**
+     * @param AuthorizationCheckerInterface   $authorizationChecker
+     * @param CachedObjectRepositoryInterface $localeRepository
+     */
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        CachedObjectRepositoryInterface $localeRepository
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->localeRepository = $localeRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter($entityWithValues)
+    {
+        if (!$entityWithValues instanceof EntityWithValuesInterface) {
+            throw InvalidObjectException::objectExpected(ClassUtils::getClass($entityWithValues), EntityWithValuesInterface::class);
+        }
+
+        foreach ($entityWithValues->getValues() as $value) {
+            if (!$this->authorizationChecker->isGranted(Attributes::VIEW_ATTRIBUTES, $value->getAttribute())) {
+                $entityWithValues->removeValue($value);
+
+                continue;
+            }
+
+            if (null === $value->getLocale()) {
+                continue;
+            }
+
+            $locale = $this->localeRepository->findOneByIdentifier($value->getLocale());
+            if (!$this->authorizationChecker->isGranted(Attributes::VIEW_ITEMS, $locale)) {
+                $entityWithValues->removeValue($value);
+            }
+        }
+
+        return $entityWithValues;
+    }
+}
