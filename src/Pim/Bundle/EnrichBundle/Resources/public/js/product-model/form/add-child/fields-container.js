ce8f935d0e9887df7ef72eac93f7ba096eea58f1
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2017-10-23 10:39:24 +0200

    PIM-6449: Fix fields rendering

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product-model/form/add-child/fields-container.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product-model/form/add-child/fields-container.js
@@ -40,7 +40,7 @@ define(
             initialize(meta) {
                 this.config = _.defaults(meta.config, {fieldModules: {}});
 
-                BaseForm.prototype.initialize.apply(arguments);
+                BaseForm.prototype.initialize.apply(this, arguments);
             },
 
             /**
@@ -50,13 +50,15 @@ define(
                 const familyVariantCode = this.getFormData().family_variant;
                 const parentCode = this.getFormData().parent;
 
+                this.$el.html(this.template());
+
                 $.when(
                     FetcherRegistry.getFetcher('family-variant').fetch(familyVariantCode),
                     FetcherRegistry.getFetcher('product-model-by-code').fetch(parentCode)
                 ).then((familyVariant, parent) => {
                     this.getAxesAttributes(familyVariant, parent.meta.level + 1)
                         .then((axesAttributes) => {
-                            return $.when(axesAttributes.map((attribute) => this.createField(attribute)));
+                            return $.when.apply($, axesAttributes.map((attribute) => this.createField(attribute)));
                         })
                         .then((...fields) => {
                             let position = 100;
@@ -64,23 +66,46 @@ define(
                                 this.addExtension(
                                     field.code,
                                     field,
-                                    'self',
+                                    'content',
                                     position++
                                 );
                             });
+
+                            this.renderExtensions();
                         });
                 });
             },
 
+            /**
+             * Looks for the attributes set corresponding to the specified level of the family variant
+             * and fetches its axes attributes.
+             *
+             * @param {Object} familyVariant
+             * @param {Number} level
+             *
+             * @returns {Promise}
+             */
             getAxesAttributes(familyVariant, level) {
                 const variantAttributeSets = familyVariant.variant_attribute_sets;
                 const variantAttributeSetForLevel = variantAttributeSets.find(
                     (variantAttributeSet) => variantAttributeSet.level === level
                 );
 
-                return FetcherRegistry.getFetcher('attribute').fetchByIdentifiers(variantAttributeSetForLevel.axes);
+                return FetcherRegistry
+                    .getFetcher('attribute')
+                    .fetchByIdentifiers(variantAttributeSetForLevel.axes);
             },
 
+            /**
+             * Instantiate a field view corresponding to an attribute.
+             * The "field_type" of the attribute must be mapped to a view key in the config of this module.
+             * The meta under this key is then modified on-the-fly before instantiation using attribute
+             * code to make the field unique.
+             *
+             * @param {Object} attribute
+             *
+             * @returns {Promise}
+             */
             createField(attribute) {
                 const fieldModuleName = this.config.fieldModules[attribute.field_type];
 
@@ -89,13 +114,22 @@ define(
                 }
 
                 return FormBuilder
-                    .buildForm(fieldModuleName)
+                    .getFormMeta(fieldModuleName)
+                    .then((formMeta) => {
+                        const newFormMeta = Object.assign({}, formMeta);
+                        newFormMeta.code += '-' + attribute.code;
+                        newFormMeta.config.fieldName = attribute.code;
+
+                        return FormBuilder.buildForm(newFormMeta);
+                    })
                     .then((field) => {
                         if ('pim_catalog_metric' === attribute.type) {
                             field.setMetricFamily(attribute.metric_family);
                         }
 
-                        return field;
+                        return field.configure().then(() => {
+                            return field;
+                        });
                     });
             }
         });
