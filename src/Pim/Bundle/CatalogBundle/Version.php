19a7e9c8e4e189e23213f7d2877fa2c7a00698c3
Laurent Petard <laurent.petard@akeneo.com>
2017-10-26 14:14:03 +0200

    Preparing version 2.0.5

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.4';
+    const VERSION = '2.0.5';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
