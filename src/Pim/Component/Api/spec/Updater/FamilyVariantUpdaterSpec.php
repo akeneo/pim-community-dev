f5070d322cf75423d03a4542b2805c030b30591e
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-11-03 16:03:04 +0100

    API-400: update a family variant trhough the API

--- src/Pim/Component/Api/spec/Updater/FamilyVariantUpdaterSpec.php
+++ src/Pim/Component/Api/spec/Updater/FamilyVariantUpdaterSpec.php
@@ -2,7 +2,12 @@
 
 namespace spec\Pim\Component\Api\Updater;
 
+use Akeneo\Component\StorageUtils\Exception\ImmutablePropertyException;
+use Akeneo\Component\StorageUtils\Exception\MandatoryPropertyException;
+use Akeneo\Component\StorageUtils\Exception\PropertyException;
 use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Gedmo\Exception;
 use PhpSpec\ObjectBehavior;
 use Pim\Component\Api\Exception\DocumentedHttpException;
 use Pim\Component\Api\Updater\FamilyVariantUpdater;
@@ -58,14 +63,66 @@ class FamilyVariantUpdaterSpec extends ObjectBehavior
             ],
         ];
         $options = ['familyCode' => 'boots'];
+        $familyVariant->getId()->willReturn(1);
+        $familyVariant->getNumberOfLevel()->willReturn(1);
         $familyUpdater->update($familyVariant, $data, $options)->shouldBeCalled();
 
         $this
-            ->shouldNotThrow(DocumentedHttpException::class)
+            ->shouldNotThrow(\Exception::class)
             ->during('update', [$familyVariant, $dataToUpdate, $options]);
     }
 
-    function it_throws_a_documented_exception_if_family_field_is_filled(
+    function it_throws_an_exception_if_not_same_number_of_level_when_updating($familyUpdater, FamilyVariantInterface $familyVariant)
+    {
+        $dataToUpdate = [
+            'code' => 'family_variant',
+            'labels' => [
+                'en_US' => 'My family variant'
+            ],
+            'variant_attribute_sets' => [
+                [
+                    'axes' => ['color'],
+                    'attributes' => ['description'],
+                    'level'=> 1,
+                ],
+
+            ],
+        ];
+        $options = ['familyCode' => 'boots'];
+        $familyVariant->getId()->willReturn(1);
+        $familyVariant->getNumberOfLevel()->willReturn(2);
+        $familyUpdater->update(Argument::cetera())->shouldNotBeCalled();
+
+        $this
+            ->shouldThrow(ImmutablePropertyException::class)
+            ->during('update', [$familyVariant, $dataToUpdate, $options]);
+    }
+
+    function it_throws_an_exception_if_level_field_is_missing_in_attribute_set($familyUpdater, FamilyVariantInterface $familyVariant)
+    {
+        $dataToUpdate = [
+            'code' => 'family_variant',
+            'labels' => [
+                'en_US' => 'My family variant'
+            ],
+            'variant_attribute_sets' => [
+                [
+                    'axes' => ['color'],
+                    'attributes' => ['description'],
+                ],
+
+            ],
+        ];
+        $options = ['familyCode' => 'boots'];
+        $familyVariant->getId()->willReturn(1);
+        $familyUpdater->update(Argument::cetera())->shouldNotBeCalled();
+
+        $this
+            ->shouldThrow(MandatoryPropertyException::class)
+            ->during('update', [$familyVariant, $dataToUpdate, $options]);
+    }
+
+    function it_throws_a_property_exception_if_family_field_is_filled(
         $familyUpdater,
         FamilyVariantInterface $familyVariant
     ) {
@@ -88,7 +145,7 @@ class FamilyVariantUpdaterSpec extends ObjectBehavior
         $familyUpdater->update(Argument::cetera())->shouldNotBeCalled();
 
         $this
-            ->shouldThrow(DocumentedHttpException::class)
+            ->shouldThrow(PropertyException::class)
             ->during('update', [$familyVariant, $data, $options]);
     }
 }
