43b1cb02a9914f76b61d411f9558aa2ebc168d44
Samir Boulil <samir.boulil@akeneo.com>
2017-11-21 11:41:54 +0100

    Preparing version 1.7.14

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.13';
+    const VERSION = '1.7.14';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Artichoke';
