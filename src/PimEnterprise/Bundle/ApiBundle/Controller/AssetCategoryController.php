a61b98c33ad0591f285fd81ebdebdde414338278
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-11-28 21:07:26 +0100

    API-439: add list asset categories endpoint in the API

--- src/PimEnterprise/Bundle/ApiBundle/Controller/AssetCategoryController.php
+++ src/PimEnterprise/Bundle/ApiBundle/Controller/AssetCategoryController.php
@@ -14,11 +14,15 @@ declare(strict_types=1);
 namespace PimEnterprise\Bundle\ApiBundle\Controller;
 
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Pim\Component\Api\Exception\PaginationParametersException;
+use Pim\Component\Api\Pagination\PaginatorInterface;
+use Pim\Component\Api\Pagination\ParameterValidatorInterface;
 use Pim\Component\Api\Repository\ApiResourceRepositoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
@@ -32,14 +36,34 @@ class AssetCategoryController
     /** @var NormalizerInterface */
     protected $normalizer;
 
+    /** @var ParameterValidatorInterface */
+    protected $parameterValidator;
+
+    /** @var PaginatorInterface */
+    protected $paginator;
+
+    /** @var array */
+    protected $apiConfiguration;
+
     /**
      * @param ApiResourceRepositoryInterface $repository
      * @param NormalizerInterface            $normalizer
+     * @param ParameterValidatorInterface    $parameterValidator
+     * @param PaginatorInterface             $paginator
+     * @param array                          $apiConfiguration
      */
-    public function __construct(ApiResourceRepositoryInterface $repository, NormalizerInterface $normalizer)
-    {
+    public function __construct(
+        ApiResourceRepositoryInterface $repository,
+        NormalizerInterface $normalizer,
+        ParameterValidatorInterface $parameterValidator,
+        PaginatorInterface $paginator,
+        array $apiConfiguration
+    ) {
         $this->repository = $repository;
         $this->normalizer = $normalizer;
+        $this->parameterValidator = $parameterValidator;
+        $this->paginator = $paginator;
+        $this->apiConfiguration = $apiConfiguration;
     }
 
     /**
@@ -62,5 +86,50 @@ class AssetCategoryController
         $assetCategoryApi = $this->normalizer->normalize($assetCategory, 'external_api');
 
         return new JsonResponse($assetCategoryApi);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws UnprocessableEntityHttpException
+     *
+     * @return Response
+     *
+     * @AclAncestor("pim_api_asset_category_list")
+     */
+    public function listAction(Request $request)
+    {
+        try {
+            $this->parameterValidator->validate($request->query->all());
+        } catch (PaginationParametersException $e) {
+            throw new UnprocessableEntityHttpException($e->getMessage(), $e);
+        }
+
+        $defaultParameters = [
+            'page'       => 1,
+            'limit'      => $this->apiConfiguration['pagination']['limit_by_default'],
+            'with_count' => 'false',
+        ];
+
+        $queryParameters = array_merge($defaultParameters, $request->query->all());
+
+        $offset = $queryParameters['limit'] * ($queryParameters['page'] - 1);
+        $order = ['root' => 'ASC', 'left' => 'ASC'];
+        $assetCategories = $this->repository->searchAfterOffset([], $order, $queryParameters['limit'], $offset);
+
+        $parameters = [
+            'query_parameters' => $queryParameters,
+            'list_route_name'  => 'pimee_api_category_asset_list',
+            'item_route_name'  => 'pimee_api_category_asset_get',
+        ];
+
+        $count = true === $request->query->getBoolean('with_count') ? $this->repository->count() : null;
+        $paginatedCategories = $this->paginator->paginate(
+            $this->normalizer->normalize($assetCategories, 'external_api'),
+            $parameters,
+            $count
+        );
+
+        return new JsonResponse($paginatedCategories);
     }
 }
