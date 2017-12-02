68d173900a0fb991cc9dd926f1c1441dc0cb16d8
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-12-02 13:43:22 +0100

    API-442: partial update a list of asset categories

--- src/PimEnterprise/Bundle/ApiBundle/tests/EndToEnd/Security/AssetCategoryAuthorizationIntegration.php
+++ src/PimEnterprise/Bundle/ApiBundle/tests/EndToEnd/Security/AssetCategoryAuthorizationIntegration.php
@@ -2,6 +2,7 @@
 
 namespace PimEnterprise\Bundle\ApiBundle\tests\EndToEnd\Security;
 
+use Pim\Bundle\ApiBundle\Stream\StreamResourceResponse;
 use Pim\Bundle\ApiBundle\tests\integration\ApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -123,6 +124,78 @@ JSON;
 JSON;
 
         $client->request('POST', '/api/rest/v1/asset-categories', [], [], [], $data);
+
+        $expectedResponse = <<<JSON
+{
+    "code": 403,
+    "message": "Access forbidden. You are not allowed to create or update asset categories."
+}
+JSON;
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+        $this->assertJsonStringEqualsJsonString($expectedResponse, $response->getContent());
+    }
+
+    public function testAccessGrantedForPartialUpdatingAnAssetCategory()
+    {
+        $client = $this->createAuthenticatedClient();
+
+        $data = '{}';
+
+        $client->request('PATCH', '/api/rest/v1/asset-categories/asset_main_catalog', [], [], [], $data);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+    }
+
+    public function testAccessDeniedForPartialUpdatingAnAssetCategory()
+    {
+        $client = $this->createAuthenticatedClient([], [], null, null, 'julia', 'julia');
+
+        $data = '{}';
+
+        $client->request('PATCH', '/api/rest/v1/asset-categories/asset_main_catalog', [], [], [], $data);
+
+        $expectedResponse = <<<JSON
+{
+    "code": 403,
+    "message": "Access forbidden. You are not allowed to create or update asset categories."
+}
+JSON;
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+        $this->assertJsonStringEqualsJsonString($expectedResponse, $response->getContent());
+    }
+
+    public function testAccessGrantedForPartialUpdatingAListOfAssetCategories()
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->setServerParameter('CONTENT_TYPE', StreamResourceResponse::CONTENT_TYPE);
+
+        $data = <<<JSON
+{"code": "a_category"}
+JSON;
+
+        ob_start(function() { return ''; });
+        $client->request('PATCH', '/api/rest/v1/asset-categories', [], [], [], $data);
+        ob_end_flush();
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testAccessDeniedForPartialUpdatingAListOfAssetCategories()
+    {
+        $client = $this->createAuthenticatedClient([], [], null, null, 'julia', 'julia');
+        $client->setServerParameter('CONTENT_TYPE', StreamResourceResponse::CONTENT_TYPE);
+
+        $data = <<<JSON
+{"code": "a_category"}
+JSON;
+
+        $client->request('PATCH', '/api/rest/v1/asset-categories', [], [], [], $data);
 
         $expectedResponse = <<<JSON
 {
