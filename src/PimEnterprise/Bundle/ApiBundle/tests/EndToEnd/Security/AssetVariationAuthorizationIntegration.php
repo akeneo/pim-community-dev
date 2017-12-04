0b229cca7ab26e1e9a97c0444d0b779d10922387
Damien Carcel <damien.carcel@akeneo.com>
2017-12-04 14:51:24 +0100

    API-513: Test the download of a variation file

--- src/PimEnterprise/Bundle/ApiBundle/tests/EndToEnd/Security/AssetVariationAuthorizationIntegration.php
+++ src/PimEnterprise/Bundle/ApiBundle/tests/EndToEnd/Security/AssetVariationAuthorizationIntegration.php
@@ -51,11 +51,55 @@ JSON;
     /**
      * Should be an integration test.
      */
+    public function testAccessGrantedForDownloadingAnAssetReferenceFile()
+    {
+        $this->createAsset(['code' => 'an_asset', 'localized' => false]);
+
+        $client = $this->createAuthenticatedClient();
+
+        //  The file is streamed so the console output is not polluted by it.
+        $contentFile = '';
+        ob_start(function ($streamedFile) use (&$contentFile) {
+            $contentFile .= $streamedFile;
+
+            return '';
+        });
+        $client->request('GET', '/api/rest/v1/assets/an_asset/variation-files/ecommerce/no_locale/download');
+        ob_end_clean();
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    /**
+     * Should be an integration test.
+     */
     public function testAccessDeniedForGettingAnAssetReference()
     {
         $client = $this->createAuthenticatedClient([], [], null, null, 'julia', 'julia');
 
         $client->request('GET', '/api/rest/v1/assets/cat/variation-files/ecommerce/en_US');
+
+        $expectedResponse = <<<JSON
+{
+    "code": 403,
+    "message": "Access forbidden. You are not allowed to list asset variations."
+}
+JSON;
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+        $this->assertJsonStringEqualsJsonString($expectedResponse, $response->getContent());
+    }
+
+    /**
+     * Should be an integration test.
+     */
+    public function testAccessDeniedForDownloadingAnAssetReferenceFile()
+    {
+        $client = $this->createAuthenticatedClient([], [], null, null, 'julia', 'julia');
+
+        $client->request('GET', '/api/rest/v1/assets/cat/variation-files/ecommerce/en_US/download');
 
         $expectedResponse = <<<JSON
 {
