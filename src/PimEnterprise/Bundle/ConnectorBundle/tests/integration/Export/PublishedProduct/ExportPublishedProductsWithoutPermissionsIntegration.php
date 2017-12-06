e3121049882c3382ca9f039d66b4151ec65689b1
Elodie Raposo <raposo.elodie@gmail.com>
2017-12-06 17:50:27 +0100

    PIM-6996: correct test

--- src/PimEnterprise/Bundle/ConnectorBundle/tests/integration/Export/PublishedProduct/ExportPublishedProductsWithoutPermissionsIntegration.php
+++ src/PimEnterprise/Bundle/ConnectorBundle/tests/integration/Export/PublishedProduct/ExportPublishedProductsWithoutPermissionsIntegration.php
@@ -19,11 +19,11 @@ class ExportPublishedProductsWithoutPermissionsIntegration extends AbstractPubli
     public function testPublishedProductViewableByRedactorWithoutPermissionApplied()
     {
         $expectedCsv = <<<CSV
-sku;categories;enabled;family;groups;a_localizable_image-de_DE;a_localizable_image-en_US;a_localizable_image-fr_FR;a_localized_and_scopable_text_area-de_DE-tablet;a_localized_and_scopable_text_area-en_US-tablet;a_localized_and_scopable_text_area-fr_FR-tablet;a_metric_without_decimal_negative;a_metric_without_decimal_negative-unit;a_number_float;PACK-groups;PACK-product_models;PACK-products;SUBSTITUTION-groups;SUBSTITUTION-product_models;SUBSTITUTION-products;UPSELL-groups;UPSELL-product_models;UPSELL-products;X_SELL-groups;X_SELL-product_models;X_SELL-products
+sku;categories;enabled;family;groups;a_localizable_image-de_DE;a_localizable_image-en_US;a_localizable_image-fr_FR;a_localized_and_scopable_text_area-de_DE-tablet;a_localized_and_scopable_text_area-en_US-tablet;a_localized_and_scopable_text_area-fr_FR-tablet;a_metric_without_decimal_negative;a_metric_without_decimal_negative-unit;a_number_float;PACK-groups;PACK-products;PACK-product_models;SUBSTITUTION-groups;SUBSTITUTION-products;SUBSTITUTION-product_models;UPSELL-groups;UPSELL-products;UPSELL-product_models;X_SELL-groups;X_SELL-products;X_SELL-product_models
 product_viewable_by_everybody_1;categoryA2;1;;;files/product_viewable_by_everybody_1/a_localizable_image/de_DE/akeneo.jpg;files/product_viewable_by_everybody_1/a_localizable_image/en_US/akeneo.jpg;files/product_viewable_by_everybody_1/a_localizable_image/fr_FR/akeneo.jpg;"DE tablet";"EN tablet";"FR tablet";-10;CELSIUS;12.0500;;;;;;;;;;;;
 product_viewable_by_everybody_2;categoryA2,categoryB;1;;;;;;;;;;;;;;;;;;;;;;;
 product_not_viewable_by_redactor;categoryB;1;;;;;;;;;;;;;;;;;;;;;;;
-product_without_category;;1;;;;;;;;;;;;;;;;;;;;;;;product_viewable_by_everybody_2,product_not_viewable_by_redactor
+product_without_category;;1;;;;;;;;;;;;;;;;;;;;;;product_viewable_by_everybody_2,product_not_viewable_by_redactor;
 
 CSV;
         $config = [
@@ -56,11 +56,11 @@ CSV;
         $user = $this->get('pim_user.provider.user')->loadUserByUsername('mary');
 
         $expectedCsv = <<<CSV
-sku;categories;enabled;family;groups;a_localizable_image-de_DE;a_localizable_image-en_US;a_localizable_image-fr_FR;a_localized_and_scopable_text_area-de_DE-tablet;a_localized_and_scopable_text_area-en_US-tablet;a_localized_and_scopable_text_area-fr_FR-tablet;a_metric_without_decimal_negative;a_metric_without_decimal_negative-unit;a_number_float;PACK-groups;PACK-product_models;PACK-products;SUBSTITUTION-groups;SUBSTITUTION-product_models;SUBSTITUTION-products;UPSELL-groups;UPSELL-product_models;UPSELL-products;X_SELL-groups;X_SELL-product_models;X_SELL-products
+sku;categories;enabled;family;groups;a_localizable_image-de_DE;a_localizable_image-en_US;a_localizable_image-fr_FR;a_localized_and_scopable_text_area-de_DE-tablet;a_localized_and_scopable_text_area-en_US-tablet;a_localized_and_scopable_text_area-fr_FR-tablet;a_metric_without_decimal_negative;a_metric_without_decimal_negative-unit;a_number_float;PACK-groups;PACK-products;PACK-product_models;SUBSTITUTION-groups;SUBSTITUTION-products;SUBSTITUTION-product_models;UPSELL-groups;UPSELL-products;UPSELL-product_models;X_SELL-groups;X_SELL-products;X_SELL-product_models
 product_viewable_by_everybody_1;categoryA2;1;;;files/product_viewable_by_everybody_1/a_localizable_image/de_DE/akeneo.jpg;files/product_viewable_by_everybody_1/a_localizable_image/en_US/akeneo.jpg;files/product_viewable_by_everybody_1/a_localizable_image/fr_FR/akeneo.jpg;"DE tablet";"EN tablet";"FR tablet";-10;CELSIUS;12.0500;;;;;;;;;;;;
 product_viewable_by_everybody_2;categoryA2,categoryB;1;;;;;;;;;;;;;;;;;;;;;;;
 product_not_viewable_by_redactor;categoryB;1;;;;;;;;;;;;;;;;;;;;;;;
-product_without_category;;1;;;;;;;;;;;;;;;;;;;;;;;product_viewable_by_everybody_2,product_not_viewable_by_redactor
+product_without_category;;1;;;;;;;;;;;;;;;;;;;;;;product_viewable_by_everybody_2,product_not_viewable_by_redactor;
 
 CSV;
         $config = [
