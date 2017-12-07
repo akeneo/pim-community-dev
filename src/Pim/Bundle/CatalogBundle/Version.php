32c2d9916229b3b3b9a93405b1ae3eb1d3b753f5
Philippe Mossière <philippe.mossiere@akeneo.com>
2017-12-07 15:19:02 +0100

    Preparing version 2.0.8

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.7';
+    const VERSION = '2.0.8';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
