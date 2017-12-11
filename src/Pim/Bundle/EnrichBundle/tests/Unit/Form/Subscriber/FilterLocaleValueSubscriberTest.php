2cfa0815ebf941f33cc2a5340e9e4eb511faa47f
Samir Boulil <samir.boulil@akeneo.com>
2017-12-11 14:19:03 +0100

    Fix phpspec

--- src/Pim/Bundle/EnrichBundle/tests/Unit/Form/Subscriber/FilterLocaleValueSubscriberTest.php
+++ src/Pim/Bundle/EnrichBundle/tests/Unit/Form/Subscriber/FilterLocaleValueSubscriberTest.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Bundle\EnrichBundle\tests\Unit\Form\Subscriber;
 
+use PHPUnit\Framework\TestCase;
 use Pim\Bundle\EnrichBundle\Form\Subscriber\FilterLocaleValueSubscriber;
 use Pim\Component\Catalog\Model\AttributeInterface;
 use Pim\Component\Catalog\Model\ValueInterface;
@@ -13,7 +14,7 @@ use Pim\Component\Catalog\Model\ValueInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class FilterLocaleValueSubscriberTest extends \PHPUnit_Framework_TestCase
+class FilterLocaleValueSubscriberTest extends TestCase
 {
     const CURRENT_LOCALE = 'fr_FR';
     const COMPARISON_LOCALE = 'fr_BE';
