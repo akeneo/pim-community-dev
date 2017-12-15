20dc5b47b03c800c8a2a8ac4f5684ec25049faa7
Damien Carcel <damien.carcel@akeneo.com>
2017-12-15 10:13:57 +0100

    Preparing version 2.0.9

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.8';
+    const VERSION = '2.0.9';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
