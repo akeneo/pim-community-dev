3dd8a5ec8bb4a534ed9d01cd44285cf589e705fd
Elodie Raposo <raposo.elodie@gmail.com>
2017-12-21 14:58:31 +0100

    PIM-7062: fix reviews

--- CHANGELOG-2.0.md
+++ CHANGELOG-2.0.md
@@ -4,7 +4,7 @@
 
 - PIM-7032: Fix close button when clicking on "Compare/Translate" option on the product edit form
 - PIM-7063: Add validation for AttributeGroup - cannot remove AttributeGroup containing attributes and cannot remove AttributGroup "other"
-- PIM-7062: Allow to delete attributes contains in a family variant
+- PIM-7062: Allow to delete attributes contained in a family variant
 
 # 2.0.9 (2017-12-15)
 
--- upgrades/schema/Version_2_0_20171221083904_add_delete_cascade_on_variant_attribute_set.php
+++ upgrades/schema/Version_2_0_20171221083904_add_delete_cascade_on_variant_attribute_set.php
@@ -6,7 +6,8 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add a delete cascade to variant_attribute_set_has_hattributes on attribue_id
+ * Allow to delete an attribute contains in a variant family (not an axis)
  */
 class Version_2_0_20171221083904_add_delete_cascade_on_variant_attribute_set extends AbstractMigration
 {
@@ -24,7 +25,6 @@ class Version_2_0_20171221083904_add_delete_cascade_on_variant_attribute_set ext
      */
     public function down(Schema $schema)
     {
-        $this->addSql('ALTER TABLE pim_catalog_variant_attribute_set_has_attributes DROP FOREIGN KEY FK_E9C4264ABAAF4009');
-        $this->addSql('ALTER TABLE pim_catalog_variant_attribute_set_has_attributes ADD CONSTRAINT FK_E9C4264ABAAF4009 FOREIGN KEY (attributes_id) REFERENCES pim_catalog_attribute (id)');
+        $this->throwIrreversibleMigrationException();
     }
 }
