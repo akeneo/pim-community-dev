69a01f7d74261dc31d54d310a019696455f66ffc
Elodie Raposo <raposo.elodie@gmail.com>
2017-12-21 10:55:21 +0100

    PIM-7063: fix reviews

--- src/Pim/Bundle/EnrichBundle/Controller/Rest/AttributeGroupController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/Rest/AttributeGroupController.php
@@ -338,19 +338,18 @@ class AttributeGroupController
 
         if ('other' === $attributeGroup->getCode()) {
             return new JsonResponse(
-                array(
+                [
                     'message' => 'Attribute group "other" cannot be removed.'
-                ),
+                ],
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
         }
 
         if (0 < $attributeGroup->getAttributes()->count()) {
             return new JsonResponse(
-                array(
-                    'message' => 'Attribute group containing attributes cannot be removed. 
-                    Please remove its attributes prior to delete it.'
-                ),
+                [
+                    'message' => 'Attribute group containing attributes cannot be removed. Please remove its attributes prior to delete it.'
+                ],
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
         }
