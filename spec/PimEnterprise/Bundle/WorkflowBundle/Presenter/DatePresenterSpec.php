dcf0b09d28c9de769c1dc45b70f3d8cdfb608f70
Samir Boulil <samir.boulil@akeneo.com>
2017-12-27 16:21:32 +0100

    Merge 2.0 -> 2.1: Fix specs

--- spec/PimEnterprise/Bundle/WorkflowBundle/Presenter/DatePresenterSpec.php
+++ spec/PimEnterprise/Bundle/WorkflowBundle/Presenter/DatePresenterSpec.php
@@ -32,9 +32,9 @@ class DatePresenterSpec extends ObjectBehavior
         $localeResolver,
         RendererInterface $renderer,
         ValueInterface $value,
-        AttributeInterface $attribute,
-        \DateTime $date
+        AttributeInterface $attribute
     ) {
+        $date = new \Datetime('now', new \DateTimeZone('UTC'));
         $localeResolver->getCurrentLocale()->willReturn('en_US');
         $datePresenter->present($date, ['locale' => 'en_US'])->willReturn('01/20/2012');
         $datePresenter->present('2012-04-25', ['locale' => 'en_US'])->willReturn('04/25/2012');
@@ -73,9 +73,9 @@ class DatePresenterSpec extends ObjectBehavior
         $localeResolver,
         RendererInterface $renderer,
         ValueInterface $value,
-        AttributeInterface $attribute,
-        \DateTime $date
+        AttributeInterface $attribute
     ) {
+        $date = new \Datetime('now', new \DateTimeZone('UTC'));
         $localeResolver->getCurrentLocale()->willReturn('en_US');
         $datePresenter->present($date, ['locale' => 'en_US'])->willReturn('2012/20/01');
         $datePresenter->present(null, ['locale' => 'en_US'])->willReturn('');
