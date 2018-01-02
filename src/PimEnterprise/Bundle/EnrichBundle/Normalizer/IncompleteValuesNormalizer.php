0765ff2efcdf7f98d4adacba248ad72749b584f5
Adrien Pétremann <hello@grena.fr>
2018-01-02 11:02:25 +0100

    PIM-7066: inherits the normalizer of the CE to avoid duplicate code + specs

--- src/PimEnterprise/Bundle/EnrichBundle/Normalizer/IncompleteValuesNormalizer.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Normalizer/IncompleteValuesNormalizer.php
@@ -2,35 +2,22 @@
 
 namespace PimEnterprise\Bundle\EnrichBundle\Normalizer;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use Pim\Bundle\EnrichBundle\Normalizer\IncompleteValuesNormalizer as BaseIncompleteValuesNormalizer;
 use Pim\Component\Catalog\EntityWithFamily\IncompleteValueCollectionFactory;
 use Pim\Component\Catalog\EntityWithFamily\RequiredValueCollectionFactory;
 use Pim\Component\Catalog\Model\AttributeInterface;
-use Pim\Component\Catalog\Model\ChannelInterface;
 use Pim\Component\Catalog\Model\EntityWithFamilyInterface;
-use Pim\Component\Catalog\Model\FamilyInterface;
 use Pim\Component\Catalog\Model\LocaleInterface;
 use PimEnterprise\Component\Security\Attributes;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
- * This class is almost a copy past for the Pim\Bundle\EnrichBundle\Normalizer\IncompleteValuesNormalizer
- * in the CE, except that we need to handle permission of what the current user is able to see
- * regarding its permissions.
+ * This class extends the Pim\Bundle\EnrichBundle\Normalizer\IncompleteValuesNormalizer
+ * of the CE to handle permission of what the current user is able to edit regarding its rights.
  */
-class IncompleteValuesNormalizer implements NormalizerInterface
+class IncompleteValuesNormalizer extends BaseIncompleteValuesNormalizer
 {
-    /** @var NormalizerInterface */
-    private $normalizer;
-
-    /** @var \Pim\Component\Catalog\EntityWithFamily\RequiredValueCollectionFactory */
-    private $requiredValueCollectionFactory;
-
-    /** @var IncompleteValueCollectionFactory */
-    private $incompleteValueCollectionFactory;
-
     /** @var AuthorizationCheckerInterface */
     private $authorizationChecker;
 
@@ -38,6 +25,7 @@ class IncompleteValuesNormalizer implements NormalizerInterface
      * @param NormalizerInterface              $normalizer
      * @param RequiredValueCollectionFactory   $requiredValueCollectionFactory
      * @param IncompleteValueCollectionFactory $incompleteValueCollectionFactory
+     * @param AuthorizationCheckerInterface    $authorizationChecker
      */
     public function __construct(
         NormalizerInterface $normalizer,
@@ -45,129 +33,32 @@ class IncompleteValuesNormalizer implements NormalizerInterface
         IncompleteValueCollectionFactory $incompleteValueCollectionFactory,
         AuthorizationCheckerInterface $authorizationChecker
     ) {
-        $this->normalizer = $normalizer;
-        $this->requiredValueCollectionFactory = $requiredValueCollectionFactory;
-        $this->incompleteValueCollectionFactory = $incompleteValueCollectionFactory;
+        parent::__construct($normalizer, $requiredValueCollectionFactory, $incompleteValueCollectionFactory);
+
         $this->authorizationChecker = $authorizationChecker;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function normalize($entityWithFamily, $format = null, array $context = []): array
+    protected function isEntityGranted(EntityWithFamilyInterface $entityWithFamily)
     {
-        $family = $entityWithFamily->getFamily();
-        if (null === $family) {
-            return [];
-        }
-
-        if (!$this->authorizationChecker->isGranted(Attributes::EDIT, $entityWithFamily)) {
-            return [];
-        }
-
-        $kindOfCompletenesses = [];
-
-        foreach ($this->getChannelsFromRequirements($family) as $channel) {
-            $requiredValuesForChannel = $this->requiredValueCollectionFactory->forChannel($family, $channel);
-
-            $kindOfCompleteness = [
-                'channel' => $channel->getCode(),
-                'labels' => $this->getChannelLabels($channel),
-                'locales' => []
-            ];
-
-            foreach ($channel->getLocales() as $locale) {
-                $requiredValues = $requiredValuesForChannel->filterByChannelAndLocale($channel, $locale);
-                $incompleteValues = $this->incompleteValueCollectionFactory->forChannelAndLocale(
-                    $requiredValues,
-                    $channel,
-                    $locale,
-                    $entityWithFamily
-                );
-
-                $missingAttributes = [];
-                foreach ($incompleteValues->attributes() as $attribute) {
-                    if (!$this->authorizationChecker->isGranted(Attributes::EDIT_ATTRIBUTES, $attribute->getGroup())) {
-                        continue;
-                    }
-
-                    if ($attribute->isLocalizable() &&
-                        !$this->authorizationChecker->isGranted(Attributes::EDIT_ITEMS, $locale)
-                    ) {
-                        continue;
-                    }
-
-                    $missingAttributes[] = [
-                        'code' => $attribute->getCode(),
-                        'labels' => $this->normalizeAttributeLabels($attribute, $channel->getLocales()->toArray())
-                    ];
-                }
-
-                $kindOfCompleteness['locales'][$locale->getCode()] = [
-                    'missing' => $missingAttributes,
-                    'label' => $locale->getName(),
-                ];
-            }
-
-            $kindOfCompletenesses[] = $kindOfCompleteness;
-        }
-
-        return $kindOfCompletenesses;
+        return $this->authorizationChecker->isGranted(Attributes::EDIT, $entityWithFamily);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function supportsNormalization($data, $format = null)
+    protected function isAttributeGranted(AttributeInterface $attribute)
     {
-        return $data instanceof EntityWithFamilyInterface && 'internal_api' === $format;
+        return $this->authorizationChecker->isGranted(Attributes::EDIT_ATTRIBUTES, $attribute->getGroup());
     }
 
     /**
-     * @param FamilyInterface $family
-     *
-     * @return Collection
+     * {@inheritdoc}
      */
-    private function getChannelsFromRequirements(FamilyInterface $family)
+    protected function isLocaleGranted(LocaleInterface $locale)
     {
-        $channels = new ArrayCollection();
-        foreach ($family->getAttributeRequirements() as $attributeRequirement) {
-            $channel = $attributeRequirement->getChannel();
-            if (!$channels->contains($channel)) {
-                $channels->add($channel);
-            }
-        }
-
-        return $channels;
-    }
-
-    /**
-     * @param ChannelInterface $channel
-     *
-     * @return string[]
-     */
-    private function getChannelLabels(ChannelInterface $channel): array
-    {
-        return array_reduce($channel->getLocales()->toArray(), function ($result, LocaleInterface $locale) use ($channel) {
-            $result[$locale->getCode()] = $channel->getTranslation($locale->getCode())->getLabel();
-
-            return $result;
-        }, []);
-    }
-
-    /**
-     * @param AttributeInterface $attribute
-     * @param LocaleInterface[]  $locales
-     *
-     * @return array
-     */
-    private function normalizeAttributeLabels(AttributeInterface $attribute, array $locales): array
-    {
-        $result = [];
-        foreach ($locales as $locale) {
-            $result[$locale->getCode()] = $attribute->getTranslation($locale->getCode())->getLabel();
-        }
-
-        return $result;
+        return $this->authorizationChecker->isGranted(Attributes::EDIT_ITEMS, $locale);
     }
 }
