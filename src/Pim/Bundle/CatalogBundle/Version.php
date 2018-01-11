aaaa63a904a1a1f27ddde8149f3776148d6fc767
Tamara Robichet <v0ld3m0r7@gmail.com>
2018-01-11 10:12:08 +0100

    Preparing version 1.6.21

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.6.20';
+    const VERSION = '1.6.21';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Wackiki Wabbit';
