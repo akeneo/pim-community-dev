8d8589e9ba83c9148ab224e829507edcf1223d4a
pierallard <pierallard@gmail.com>
2018-01-12 12:15:05 +0100

    Preparing version 2.0.12

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.11';
+    const VERSION = '2.0.12';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
