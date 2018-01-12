565562db9c4350c3c1f1f567abc53fb1836d706e
pierallard <pierallard@gmail.com>
2018-01-12 12:03:31 +0100

    Preparing version 2.0.12

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.11';
+    const VERSION = '2.0.12';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
