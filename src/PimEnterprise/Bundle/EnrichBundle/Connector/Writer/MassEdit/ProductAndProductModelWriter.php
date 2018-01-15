5a83290071aa3e5c60a593fd1b1bab588e5df1cb
Samir Boulil <samir.boulil@akeneo.com>
2018-01-15 18:53:47 +0100

    Rename CacheClearerInterface for EntityManagerClearer interface

--- src/PimEnterprise/Bundle/EnrichBundle/Connector/Writer/MassEdit/ProductAndProductModelWriter.php
+++ src/PimEnterprise/Bundle/EnrichBundle/Connector/Writer/MassEdit/ProductAndProductModelWriter.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace PimEnterprise\Bundle\EnrichBundle\Connector\Writer\MassEdit;
 
-use Akeneo\Component\StorageUtils\Cache\CacheClearerInterface;
+use Akeneo\Component\StorageUtils\Cache\EntityManagerClearerInterface;
 use Akeneo\Component\StorageUtils\Saver\BulkSaverInterface;
 use Pim\Bundle\EnrichBundle\Connector\Writer\MassEdit\ProductAndProductModelWriter as BaseWriter;
 use Pim\Bundle\VersioningBundle\Manager\VersionManager;
@@ -37,14 +37,14 @@ class ProductAndProductModelWriter extends BaseWriter
      * @param BulkSaverInterface            $productSaver
      * @param BulkSaverInterface            $productModelSaver
      * @param VersionManager                $versionManager
-     * @param CacheClearerInterface         $cacheClearer
+     * @param EntityManagerClearerInterface $cacheClearer
      * @param AuthorizationCheckerInterface $authorizationChecker
      */
     public function __construct(
         BulkSaverInterface $productSaver,
         BulkSaverInterface $productModelSaver,
         VersionManager $versionManager,
-        CacheClearerInterface $cacheClearer,
+        EntityManagerClearerInterface $cacheClearer,
         AuthorizationCheckerInterface $authorizationChecker
     ) {
         parent::__construct(
