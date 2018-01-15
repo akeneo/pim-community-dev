3627e86c5e804d99b6eec0b01de675fe41da9228
Samir Boulil <samir.boulil@akeneo.com>
2018-01-15 13:21:18 +0100

    Hotfix: reorder migration scripts of 2.0 and 2.1

--- upgrades/schema/Version_2_1_171129131942_product_model_associations.php
+++ upgrades/schema/Version_2_1_171129131942_product_model_associations.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171129131942 extends AbstractMigration
+class Version_2_1_171129131942_product_model_associations extends AbstractMigration
 {
     /**
      * @param Schema $schema
