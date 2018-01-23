1593ffcf6a4f48100ede0bdef4254bb0737eab3d
Adrien Pétremann <hello@grena.fr>
2018-01-23 13:42:58 +0100

    Preparing version 2.0.13

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.12';
+    const VERSION = '2.0.13';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
