227a22ef7e2741454620b413e9630bac2a90b42c
Laurent Petard <laurent.petard@akeneo.com>
2018-02-01 11:20:47 +0100

    Preparing version 1.7.17

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.16';
+    const VERSION = '1.7.17';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Artichoke';
