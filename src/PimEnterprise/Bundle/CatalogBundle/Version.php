08a6149c3363ea8159059184bea78ee4516f8516
Laurent Petard <laurent.petard@akeneo.com>
2018-02-01 16:58:38 +0100

    Preparing version 2.0.15

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.14';
+    const VERSION = '2.0.15';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
