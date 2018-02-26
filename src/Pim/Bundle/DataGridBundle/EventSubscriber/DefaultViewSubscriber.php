19565cdcdbc46b47922f55f9d2ef2ec241293374
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-02-26 09:34:29 +0100

    TIP-236: Move form event to the namespace form

--- src/Pim/Bundle/DataGridBundle/EventSubscriber/DefaultViewSubscriber.php
+++ src/Pim/Bundle/DataGridBundle/EventSubscriber/DefaultViewSubscriber.php
@@ -5,7 +5,7 @@ namespace Pim\Bundle\DataGridBundle\EventSubscriber;
 use Pim\Bundle\DataGridBundle\DataTransformer\DefaultViewDataTransformer;
 use Pim\Bundle\DataGridBundle\Repository\DatagridViewRepositoryInterface;
 use Pim\Bundle\UserBundle\Entity\UserInterface;
-use Pim\Bundle\UserBundle\Event\UserFormBuilderEvent;
+use Pim\Bundle\UserBundle\Form\Event\UserFormBuilderEvent;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
