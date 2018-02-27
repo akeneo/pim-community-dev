c454b985b6aa411dbd309b35dbbaca4d5349fc79
Aymeric Planche <aymeric.planche@akeneo.com>
2018-02-27 10:42:24 +0100

    AOB-97 add passing simple job launcher test

--- src/Akeneo/Bundle/BatchBundle/spec/Launcher/SimpleJobLauncherSpec.php
+++ src/Akeneo/Bundle/BatchBundle/spec/Launcher/SimpleJobLauncherSpec.php
@@ -37,6 +37,38 @@ class SimpleJobLauncherSpec extends ObjectBehavior
         $this->shouldHaveType(JobLauncherInterface::class);
     }
 
+    function it_launches_a_job_by_pushing_it_to_the_queue(
+        $jobRegistry,
+        $jobParametersFactory,
+        $jobParametersValidator,
+        $jobRepository,
+        JobInstance $jobInstance,
+        UserInterface $user,
+        JobExecution $jobExecution,
+        Job $job,
+        JobParameters $jobParameters,
+        ConstraintViolationListInterface $constraintViolationList,
+        $eventDispatcher
+    ) {
+        $jobInstance->getJobName()->willReturn('job_instance_name');
+        $jobInstance->getCode()->willReturn('job_instance_code');
+        $jobInstance->getRawParameters()->willReturn(['foo' => 'bar']);
+        $user->getUsername()->willReturn('julia');
+        $jobExecution->getId()->willReturn(1);
+        $constraintViolationList->count()->willReturn(0);
+
+        $jobRegistry->get('job_instance_name')->willReturn($job);
+        $jobParametersFactory->create($job, ['foo' => 'bar', 'baz' => 'foz'])->willReturn($jobParameters);
+        $jobParametersValidator->validate($job, $jobParameters, ['Default', 'Execution'])->willReturn($constraintViolationList);
+        $jobRepository->createJobExecution($jobInstance, $jobParameters)->willReturn($jobExecution);
+        $jobExecution->setUser('julia')->shouldBeCalled();
+        $jobRepository->updateJobExecution($jobExecution)->shouldBeCalled();
+
+        $eventDispatcher->dispatch(EventInterface::JOB_EXECUTION_CREATED, Argument::type(JobExecutionEvent::class))->shouldBeCalled();
+
+        $this->launch($jobInstance, $user, ['baz' => 'foz'])->shouldReturn($jobExecution);
+    }
+
     function it_throws_an_exception_if_job_parameters_are_invalid(
         $jobRegistry,
         $jobParametersFactory,
