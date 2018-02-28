748738dbc5d46dc9a4bbcb12f0aae0f65af65b65
Damien Carcel <damien.carcel@gmail.com>
2018-02-28 10:40:40 +0100

    Fix an error on docker scripts

--- bin/docker/pim-initialize.sh
+++ bin/docker/pim-initialize.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose exec akeneo rm -rf var/cache/*
+docker-compose exec fpm rm -rf var/cache/*
 
 docker-compose exec fpm bin/console --env=prod pim:install --force --symlink --clean
 docker-compose exec fpm bin/console --env=behat pim:installer:db
