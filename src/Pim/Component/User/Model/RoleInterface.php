9fd8b6f264e516da1c028d85897a800ad17ed30e
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-03-01 11:34:33 +0100

    TIP-236: Fix some issue on the role model

--- src/Pim/Component/User/Model/RoleInterface.php
+++ src/Pim/Component/User/Model/RoleInterface.php
@@ -2,17 +2,12 @@
 
 namespace Pim\Component\User\Model;
 
-use Symfony\Component\Security\Core\Role\RoleInterface as SymfonyRoleInterface;
-
 /**
  * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
- * @todo This "write" model should not extends Symfony\Component\Security\Core\Role\RoleInterface.
- * We should create a "read" model that implement that class.
  */
-interface RoleInterface extends SymfonyRoleInterface
+interface RoleInterface
 {
     /**
      * Return the role id
