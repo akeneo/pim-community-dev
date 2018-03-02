463376e1ef6c9f9e77650d58852ae3ae4255d60f
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-03-02 17:48:35 +0100

    Create query and read model

--- src/Pim/Bundle/UserBundle/spec/Security/UserProviderSpec.php
+++ src/Pim/Bundle/UserBundle/spec/Security/UserProviderSpec.php
@@ -3,32 +3,34 @@
 namespace spec\Pim\Bundle\UserBundle\Security;
 
 use PhpSpec\ObjectBehavior;
+use Pim\Bundle\UserBundle\Persistence\ORM\Query\FindUserForSecurity;
 use Pim\Component\User\User\UserRepositoryInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserProviderSpec extends ObjectBehavior
 {
-    function let(UserRepositoryInterface $userRepository)
+    function let(FindUserForSecurity $findUserForSecurityQuery)
     {
-        $this->beConstructedWith($userRepository);
+        $this->beConstructedWith($findUserForSecurityQuery);
     }
 
-    function it_loads_a_user_by_its_username($userRepository, UserInterface $julia)
+    function it_loads_a_user_by_its_username($findUserForSecurityQuery, UserInterface $julia)
     {
-        $userRepository->findOneByIdentifier('julia')->willReturn($julia);
+        $findUserForSecurityQuery->__invoke('julia')->willReturn($julia);
         $this->loadUserByUsername('julia')->shouldReturn($julia);
     }
 
-    function it_refreshes_a_user($userRepository, UserInterface $julia)
+    function it_refreshes_a_user($findUserForSecurityQuery, UserInterface $julia)
     {
-        $userRepository->findOneByIdentifier('julia')->willReturn($julia);
+        $findUserForSecurityQuery->__invoke('julia')->willReturn($julia);
         $julia->getUsername()->willReturn('julia');
         $this->refreshUser($julia)->shouldReturn($julia);
     }
 
-    function it_throws_an_exception_if_user_does_not_exist($userRepository)
+    function it_throws_an_exception_if_user_does_not_exist($findUserForSecurityQuery)
     {
-        $userRepository->findOneByIdentifier('jean-pacôme')->willReturn(null);
+        $findUserForSecurityQuery->__invoke('jean-pacôme')->willThrow(UsernameNotFoundException::class);
         $this->shouldThrow('Symfony\Component\Security\Core\Exception\UsernameNotFoundException')
              ->during('loadUserByUsername', ['jean-pacôme']);
     }
