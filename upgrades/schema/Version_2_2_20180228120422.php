ad1bed3e6c3c0911666e2fdd92011b9d933e829e
Adrien Pétremann <hello@grena.fr>
2018-03-06 11:17:32 +0100

    PIM-6389: simplify migration

--- upgrades/schema/Version_2_2_20180228120422.php
+++ upgrades/schema/Version_2_2_20180228120422.php
@@ -12,9 +12,13 @@ class Version_2_2_20180228120422 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $newJobCodes = $this->getNewJobs();
-        $jobInstancesToCreate = $this->jobInstancesToCreate($newJobCodes);
-        $this->insertJobInstances($jobInstancesToCreate);
+        $sql = <<<SQL
+REPLACE INTO `akeneo_batch_job_instance` (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
+VALUES
+	('add_attribute_value', 'Mass add attribute value', 'add_attribute_value', 0, 'Akeneo Mass Edit Connector', 'a:0:{}', 'mass_edit');
+SQL;
+
+        $this->addSql($sql);
     }
 
     /**
@@ -23,69 +27,5 @@ class Version_2_2_20180228120422 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->throwIrreversibleMigrationException();
-    }
-
-    /**
-     * @return array
-     *
-     */
-    private function getNewJobs(): array
-    {
-        return [
-            'add_attribute_value' => '(\'add_attribute_value\', \'Mass add attribute value\', \'add_attribute_value\', 0, \'Akeneo Mass Edit Connector\', \'a:0:{}\', \'mass_edit\')',
-        ];
-    }
-
-    /**
-     * @param array $newJobCodes
-     *
-     * @return array
-     *
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    private function jobInstancesToCreate(array $newJobCodes): array
-    {
-        $jobInstancesToCreate = [];
-        foreach ($newJobCodes as $jobCode => $jobInstance) {
-            if (!$this->jobExists($jobCode)) {
-                $jobInstancesToCreate[] = $jobInstance;
-            }
-        }
-
-        return $jobInstancesToCreate;
-    }
-
-    /**
-     * @param string $jobCode
-     *
-     * @return bool
-     *
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    private function jobExists(string $jobCode): bool
-    {
-        $stmt = $this->connection->executeQuery(
-            'SELECT * FROM akeneo_batch_job_instance WHERE code = \'' . $jobCode . '\''
-        );
-
-        return 1 <= $stmt->rowCount();
-    }
-
-    /**
-     * @param array $jobInstancesToCreate
-     */
-    private function insertJobInstances(array $jobInstancesToCreate): void
-    {
-        if (empty($jobInstancesToCreate)) {
-            return;
-        }
-        $jobInstancesSQL = implode(',', $jobInstancesToCreate);
-        $sql = <<<SQL
-INSERT INTO `akeneo_batch_job_instance` (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
-VALUES
-$jobInstancesSQL
-;
-SQL;
-        $this->addSql($sql);
     }
 }
