b99c227ef3d56716ca30df989c2319fd3399f990
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-03-21 11:52:50 +0100

    TIP-833: Fix DI service definitions

--- src/PimEnterprise/Bundle/FilterBundle/DependencyInjection/PimEnterpriseFilterExtension.php
+++ src/PimEnterprise/Bundle/FilterBundle/DependencyInjection/PimEnterpriseFilterExtension.php
@@ -30,6 +30,5 @@ class PimEnterpriseFilterExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('filters.yml');
-        $loader->load('utility.yml');
     }
 }
