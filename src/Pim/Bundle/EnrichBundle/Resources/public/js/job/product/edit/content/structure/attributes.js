40b7f465645dcebbc4ace0ca2aa8dee69f43b8de
pierallard <pierallard@gmail.com>
2018-03-23 11:11:35 +0100

    [TIP-854] Remove occurences of 'Attributes'

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/job/product/edit/content/structure/attributes.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/job/product/edit/content/structure/attributes.js
@@ -63,7 +63,7 @@ define(
                     this.template({
                         __: __,
                         isEditable: this.isEditable(),
-                        titleEdit: __('pim_enrich.export.product.filter.attributes.title'),
+                        titleEdit: __('pim_common.attributes'),
                         labelEdit: __('pim_enrich.export.product.filter.attributes.edit'),
                         labelInfo: __(
                             'pim_enrich.export.product.filter.attributes.label',
