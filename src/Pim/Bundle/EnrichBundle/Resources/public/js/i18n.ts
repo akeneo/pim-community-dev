fddae672063415d11ffa36589bee0a419022dcce
christophe-chausseray <chausseray.christophe@gmail.com>
2018-03-29 17:23:04 +0200

    Update i18n module to be in typescript

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/i18n.ts
@@ -0,0 +1,21 @@
+const flagTemplate = (country: string, language: string, displayLanguage: boolean) => {
+  return `
+<span class="flag-language">
+  <i class="flag flag-${country}"></i>
+  ${displayLanguage ? `<span class="language">${language}</span>` : ''}
+</span>`;
+};
+
+export const getFlag = (locale: string, displayLanguage: boolean = true): string => {
+  if (!locale) {
+    return '';
+  }
+
+  const [language, country] = locale.split('_');
+
+  return flagTemplate(country.toLowerCase(), language, displayLanguage);
+};
+
+export const getLabel = (labels: {[locale: string]: string}, locale: string, fallback: string): string => {
+  return labels[locale] ? labels[locale] : `[${fallback}]`;
+};
