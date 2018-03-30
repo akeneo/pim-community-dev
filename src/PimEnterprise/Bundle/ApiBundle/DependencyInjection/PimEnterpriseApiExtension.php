fc556d858d3807cc9fe9ef5f7fe89680b472cd1d
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-03-30 14:37:28 +0200

    TIP-833: Remove bundle configuration

--- src/PimEnterprise/Bundle/ApiBundle/DependencyInjection/PimEnterpriseApiExtension.php
+++ src/PimEnterprise/Bundle/ApiBundle/DependencyInjection/PimEnterpriseApiExtension.php
@@ -28,9 +28,6 @@ class PimEnterpriseApiExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('filters.yml');
     }
