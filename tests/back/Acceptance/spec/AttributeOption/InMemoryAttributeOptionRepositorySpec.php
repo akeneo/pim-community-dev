ad44bb79d45a99927d5ddfa4abb0297deae5434b
Damien Carcel <damien.carcel@akeneo.com>
2018-04-18 10:05:39 +0200

    Add an in memory attribute option repository

---
+++ tests/back/Acceptance/spec/AttributeOption/InMemoryAttributeOptionRepositorySpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace spec\Akeneo\Test\Acceptance\AttributeOption;
+
+use Akeneo\Test\Acceptance\AttributeOption\InMemoryAttributeOptionRepository;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
+use Pim\Bundle\CatalogBundle\Entity\AttributeOption;
+use Pim\Component\Catalog\Model\AttributeInterface;
+use Pim\Component\Catalog\Model\AttributeOptionInterface;
+use Prophecy\Argument;
+
+class InMemoryAttributeOptionRepositorySpec extends ObjectBehavior
+{
+    function it_returns_an_identifier_property()
+    {
+        $this->getIdentifierProperties()->shouldReturn(['code']);
+    }
+
+    function it_find_one_attribute_option_by_identifier()
+    {
+        $attribute = new Attribute();
+        $attributeOption = $this->createAttributeOption('attribute_option_1', $attribute);
+        $this->beConstructedWith([$attributeOption->getCode() => $attributeOption]);
+
+        $this->findOneByIdentifier('attribute_option_1')->shouldReturn($attributeOption);
+    }
+
+    function it_saves_an_attribute_option()
+    {
+        $attribute = new Attribute();
+        $attributeOption = $this->createAttributeOption('attribute_option_1', $attribute);
+        $this->save($attributeOption);
+
+        $this->findOneByIdentifier('attribute_option_1')->shouldReturn($attributeOption);
+    }
+
+    function it_find_attribute_options_by_criteria()
+    {
+        $attribute = new Attribute();
+        $attributeOption = $this->createAttributeOption('attribute_option_1', $attribute);
+        $this->beConstructedWith([$attributeOption->getCode() => $attributeOption]);
+
+        $this->findBy(['code' => 'attribute_option_1'])->shouldReturn([$attributeOption]);
+    }
+
+    private function createAttributeOption(string $code, AttributeInterface $attribute): AttributeOptionInterface
+    {
+        $attributeOption = new AttributeOption();
+        $attributeOption->setCode($code);
+        $attributeOption->setAttribute($attribute);
+
+        return $attributeOption;
+    }
+}
