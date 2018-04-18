0c3f619001138e9aef77f920238e8da1d93edd55
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2018-04-18 11:13:28 +0200

    In memory family variant repository

---
+++ tests/back/Acceptance/FamilyVariant/InMemoryFamilyVariantRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Akeneo\Test\Acceptance\FamilyVariant;
+
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\Test\Acceptance\Common\NotImplementedException;
+use Doctrine\Common\Collections\ArrayCollection;
+use Pim\Component\Catalog\Model\FamilyVariant;
+use Pim\Component\Catalog\Repository\FamilyVariantRepositoryInterface;
+
+class InMemoryFamilyVariantRepository implements SaverInterface, IdentifiableObjectRepositoryInterface, FamilyVariantRepositoryInterface
+{
+    /** @var ArrayCollection|FamilyVariant[] */
+    private $familyVariants;
+
+    public function __construct(array $familyVariants = [])
+    {
+        $this->familyVariants = new ArrayCollection($familyVariants);
+    }
+
+    public function getIdentifierProperties()
+    {
+        return ['code'];
+    }
+
+    public function findOneByIdentifier($identifier)
+    {
+        return $this->familyVariants->get($identifier);
+    }
+
+    public function save($object, array $options = [])
+    {
+        if (!$object instanceof FamilyVariant) {
+            throw new \InvalidArgumentException('The object argument should be a family variant');
+        }
+
+        $this->familyVariants->set($object->getCode(), $object);
+    }
+
+    public function find($id)
+    {
+        throw new NotImplementedException(__METHOD__);
+    }
+
+    public function findAll()
+    {
+        throw new NotImplementedException(__METHOD__);
+    }
+
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        throw new NotImplementedException(__METHOD__);
+    }
+
+    public function findOneBy(array $criteria)
+    {
+        throw new NotImplementedException(__METHOD__);
+    }
+
+    public function getClassName()
+    {
+        throw new NotImplementedException(__METHOD__);
+    }
+}
---
+++ tests/back/Acceptance/spec/FamilyVariant/InMemoryFamilyVariantRepositorySpec.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace spec\Akeneo\Test\Acceptance\FamilyVariant;
+
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\Test\Acceptance\Common\NotImplementedException;
+use Akeneo\Test\Acceptance\FamilyVariant\InMemoryFamilyVariantRepository;
+use PhpSpec\ObjectBehavior;
+use Pim\Component\Catalog\Model\FamilyVariant;
+use Pim\Component\Catalog\Repository\FamilyVariantRepositoryInterface;
+use Prophecy\Argument;
+
+class InMemoryFamilyVariantRepositorySpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(InMemoryFamilyVariantRepository::class);
+    }
+
+    function it_is_an_identifiable_repository()
+    {
+        $this->shouldBeAnInstanceOf(IdentifiableObjectRepositoryInterface::class);
+    }
+
+    function it_is_a_familiy_variant_repository()
+    {
+        $this->shouldBeAnInstanceOf(FamilyVariantRepositoryInterface::class);
+    }
+
+    function it_is_a_saver()
+    {
+        $this->shouldBeAnInstanceOf(SaverInterface::class);
+    }
+
+    function it_asserts_the_identifier_property_is_the_code()
+    {
+        $this->getIdentifierProperties()->shouldReturn(['code']);
+    }
+
+    function it_finds_a_family_variant_by_identifier()
+    {
+        $familyVariant = new FamilyVariant();
+        $familyVariant->setCode('a-family-variant');
+        $this->beConstructedWith([$familyVariant->getCode() => $familyVariant]);
+
+        $this->findOneByIdentifier('a-family-variant')->shouldReturn($familyVariant);
+    }
+
+    function it_saves_a_family_variant()
+    {
+        $familyVariant = new FamilyVariant();
+        $familyVariant->setCode('a-family-variant');
+
+        $this->save($familyVariant)->shouldReturn(null);
+
+        $this->findOneByIdentifier($familyVariant->getCode())->shouldReturn($familyVariant);
+    }
+
+    function it_saves_only_family_variants()
+    {
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('save', ['a_thing']);
+    }
+
+    function it_asserts_that_the_other_methods_are_not_implemented_yet()
+    {
+        $this->shouldThrow(NotImplementedException::class)->during('find', ['a-family']);
+        $this->shouldThrow(NotImplementedException::class)->during('findAll', []);
+        $this->shouldThrow(NotImplementedException::class)->during('findBy', [[]]);
+        $this->shouldThrow(NotImplementedException::class)->during('findOneBy', [[]]);
+        $this->shouldThrow(NotImplementedException::class)->during('getClassName', []);
+    }
+}
