5c56a4fe4340c6ed7b6fe8062f5c54da6e16f2a8
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-04-18 10:43:29 +0200

    Add in memory repository for acceptance tests

---
+++ tests/back/Acceptance/User/InMemoryRoleRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Test\Acceptance\User;
+
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Oro\Bundle\UserBundle\Entity\Role;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+/**
+ * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class InMemoryRoleRepository implements IdentifiableObjectRepositoryInterface, SaverInterface
+{
+    /** @var Role[] */
+    private $roles;
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+    }
+
+    public function save($role, array $options = [])
+    {
+        if(!$role instanceof RoleInterface) {
+            throw new \InvalidArgumentException('Only user role objects are supported.');
+        }
+        $index = $role->getRole();
+        $index = strtolower(str_replace('ROLE_', '', $index));
+        $this->roles->set($index, $role);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifierProperties()
+    {
+        return ['role'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findOneByIdentifier($identifier)
+    {
+        return $this->roles->get($identifier);
+    }
+
+}
---
+++ tests/back/Acceptance/spec/User/InMemoryRoleRepositorySpec.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace spec\Akeneo\Test\Acceptance\User;
+
+use Akeneo\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\Test\Acceptance\User\InMemoryRoleRepository;
+use Oro\Bundle\UserBundle\Entity\Role;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class InMemoryRoleRepositorySpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(InMemoryRoleRepository::class);
+    }
+    
+    function it_is_a_identifiable_object_repository()
+    {
+        $this->shouldImplement(IdentifiableObjectRepositoryInterface::class);
+    }
+
+    function it_is_a_saver()
+    {
+        $this->shouldImplement(SaverInterface::class);
+    }
+
+    function it_saves_a_role()
+    {
+        $this->save(new Role())->shouldReturn(null);
+    }
+
+    function it_only_saves_roles()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->during('save', ['wrong_object']);
+    }
+
+    function it_finds_a_role_by_its_identifier()
+    {
+        $role = new Role();
+        $role->setRole('role');
+        $this->save($role);
+        $this->findOneByIdentifier('role')->shouldReturn($role);
+    }
+
+    function it_returns_null_if_the_role_does_not_exist()
+    {
+        $this->findOneByIdentifier('role')->shouldReturn(null);
+    }
+
+    function it_has_identifier_properties()
+    {
+        $this->getIdentifierProperties()->shouldReturn(['role']);
+    }
+}
