5237d9058df2b3afe9f512290d615bdc575b5bf9
Tamara Robichet <v0ld3m0r7@gmail.com>
2018-04-27 10:38:55 +0200

    Stabilise cucumber and puppeteer
    stabilise puppeteer
    
    fix require order
    
    fix log order
    
    cleanup
    
    cleanup

--- tests/front/acceptance/cucumber/world.js
+++ tests/front/acceptance/cucumber/world.js
@@ -3,27 +3,26 @@ const puppeteer = require('puppeteer');
 const extensions = require(`${process.cwd()}/web/js/extensions.json`);
 const fs = require('fs');
 const path = require('path');
-
+const htmlTemplate =  fs.readFileSync(process.cwd() + '/web/test_dist/index.html', 'utf-8');
+const translations = fs.readFileSync(path.join(process.cwd(), './web/js/translation/en.js'), 'utf-8');
 const userBuilder = new UserBuilder();
 
 module.exports = function(cucumber) {
     const {Before, After, Status} = cucumber;
 
     Before({timeout: 10 * 1000}, async function() {
-        console.log('Step 1: Before');
         this.baseUrl = 'http://pim.com/';
         this.browser = await puppeteer.launch({
             ignoreHTTPSErrors: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
             headless: !this.parameters.debug,
-            slowMo: 0
+            slowMo: 0,
+            pipe: true
         });
 
-        console.log('Step 2: Browser');
         this.page = await this.browser.newPage();
         await this.page.setRequestInterception(true);
 
-        console.log('Step 3: Load browser and begin interception');
         this.consoleLogs = [];
         this.page.on('console', message => {
             if (['error', 'warning'].includes(message.type())) {
@@ -35,7 +34,7 @@ module.exports = function(cucumber) {
             if (request.url() === this.baseUrl) {
                 request.respond({
                     contentType: 'text/html',
-                    body: fs.readFileSync(process.cwd() + '/web/test_dist/index.html', 'utf-8')
+                    body: htmlTemplate
                 });
             }
             if (request.url().includes('/rest/user/')) {
@@ -53,34 +52,28 @@ module.exports = function(cucumber) {
             }
 
             if (request.url().includes('/js/translation')) {
-                const language = path.basename(request.url());
-                const languageContents = fs.readFileSync(path.join(process.cwd(), `./web/js/translation/${language}`), 'utf-8');
-
                 request.respond({
                     contentType: 'application/json',
-                    body: `${JSON.stringify(languageContents)}`
+                    body: `${JSON.stringify(translations)}`
                 });
             }
         });
 
-        console.log('Step 4: Go to the pim');
         await this.page.goto(this.baseUrl);
-        console.log('Step 5: Set up the page');
         await this.page.evaluate(async () => await require('pim/fetcher-registry').initialize());
         await this.page.evaluate(async () => await require('pim/user-context').initialize());
         await this.page.evaluate(async () => await require('pim/init-translator').fetch());
-        console.log('Done');
-        console.log('-------');
     });
 
     After(async function(scenario) {
+        this.consoleLogs = this.consoleLogs || [];
         if (Status.FAILED === scenario.result.status) {
             if (0 < this.consoleLogs.length) {
                 const logMessages = this.consoleLogs.reduce(
                     (result, message) => `${result}\nError logged: ${message}`, ''
                 );
 
-                this.attach(logMessages, 'text/plain');
+                await this.attach(logMessages, 'text/plain');
                 console.log(logMessages);
             }
         }
