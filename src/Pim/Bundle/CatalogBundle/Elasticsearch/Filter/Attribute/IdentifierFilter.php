c4b66e42c4e3658507fe0c86ebdd3de6be915316
Samir Boulil <samir.boulil@akeneo.com>
2018-05-02 17:07:19 +0200

    PIM-6784: Fix identifier filter
    Context:
    
    Before this PR, the identifier filter would only use the es document
    property "identifier" to filter on product and product models
    identifier.
    
    This means that whenever you would filter on an identifier attribute
    like "Sku", this filter would only use the "identifier" property of
    the ES document to find the right node to return.
    
    As mentionned above, the product model codes are also indexed in
    the "identifier" property of the ES document.
    The consequence of that is, if you are searching for a document in
    ES which has an identifier equals to "my_identifier", ES would indeed
    match the product model having this a code "my_identifier" as well as
    the product having a sku "my_identifier".
    
    Still, this way of searching was functioning very well in the datagrid (smart search or aggregated search) since along with
    the identifier to search we would tell elasticsearch to match only the documents that would have the "sku" attribute in the document level.
    
    This last clause would disqualify all product models from appearing in the search (which is what we want when you look for a not having the sku "toto" for instance).
    
    Problem:
    The new feature is implemented in such a way that we don't look if the
    attributes are in the level of the document we match. Instead, we
    just see if the node has the attribute in his ancestors (if not,
    therefore he should have it, more on that in this PR:
    https://github.com/akeneo/pim-community-dev/pull/7523)
    
    Long story short: the product models are not disqualified anymore when
    we filter on the attribute sku as it is implemented.
    
    Solution:
    Stop relying only on the "identifier" property when we search on
    attribute identifiers like "sku"
    
    1. Make sure the product values identifiers are indexed in the "values"
    of the ES documents.
    2. When using the PQB:
    
    ```
    $pqb->addFilter('identifier', Operators:Equals, 'toto') => will use the
    "identifier" property of the ES document
    
    $pqb->addFilter('sku', Operators:Equals, 'toto') => will use the
    indexed product value stored in
    values.sku-text.<all_channels>.<all_locales> property of the ES
    document.
    ```
    
    - This solution also involves splitting the IdentifierFilter which is
    today an AttributeFilterInterface as well as a FieldFilterInterface.

---
+++ src/Pim/Bundle/CatalogBundle/Elasticsearch/Filter/Attribute/IdentifierFilter.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\Elasticsearch\Filter\Attribute;
+
+use Akeneo\Component\StorageUtils\Exception\InvalidPropertyTypeException;
+use Pim\Component\Catalog\Exception\InvalidOperatorException;
+use Pim\Component\Catalog\Model\AttributeInterface;
+use Pim\Component\Catalog\Query\Filter\AttributeFilterInterface;
+use Pim\Component\Catalog\Query\Filter\FieldFilterHelper;
+use Pim\Component\Catalog\Query\Filter\Operators;
+
+/**
+ * Identifier filter that filters on the attribute identifier.
+ *
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class IdentifierFilter extends AbstractAttributeFilter implements AttributeFilterInterface
+{
+    /**
+     * @param array $supportedAttributeTypes
+     * @param array $supportedOperators
+     */
+    public function __construct(
+        array $supportedAttributeTypes = [],
+        array $supportedOperators = []
+    ) {
+        $this->supportedOperators = $supportedOperators;
+        $this->supportedAttributeTypes = $supportedAttributeTypes;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Akeneo\Component\StorageUtils\Exception\InvalidPropertyTypeException
+     */
+    public function addAttributeFilter(
+        AttributeInterface $attribute,
+        $operator,
+        $value,
+        $locale = null,
+        $channel = null,
+        $options = []
+    ) {
+        if (null === $this->searchQueryBuilder) {
+            throw new \LogicException('The search query builder is not initialized in the filter.');
+        }
+
+        $this->checkValue($attribute->getCode(), $operator, $value);
+        $this->applyFilter($attribute, $operator, $value);
+
+        return $this;
+    }
+
+    /**
+     * Checks the identifier is a string or an array depending on the operator
+     *
+     * @param string $property
+     * @param string $operator
+     * @param mixed  $value
+     *
+     * @throws InvalidPropertyTypeException
+     */
+    protected function checkValue($property, $operator, $value)
+    {
+        if (Operators::IN_LIST === $operator || Operators::NOT_IN_LIST === $operator) {
+            FieldFilterHelper::checkArray($property, $value, self::class);
+        } else {
+            FieldFilterHelper::checkString($property, $value, self::class);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsAttribute(AttributeInterface $attribute)
+    {
+        return in_array($attribute->getType(), $this->supportedAttributeTypes);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttributeTypes()
+    {
+        return $this->supportedAttributeTypes;
+    }
+
+    /**
+     * Apply the filtering conditions to the search query builder
+     *
+     * @param AttributeInterface $attribute
+     * @param string             $operator
+     * @param string|array       $value
+     */
+    protected function applyFilter(AttributeInterface $attribute, string $operator, $value)
+    {
+        $attributePath = $this->getAttributePath($attribute, null, null);
+        switch ($operator) {
+            case Operators::STARTS_WITH:
+                $clause = [
+                    'query_string' => [
+                        'default_field' => $attributePath,
+                        'query'         => $value . '*',
+                    ],
+                ];
+                $this->searchQueryBuilder->addFilter($clause);
+                break;
+
+            case Operators::CONTAINS:
+                $clause = [
+                    'query_string' => [
+                        'default_field' => $attributePath,
+                        'query'         => '*' . $value . '*',
+                    ],
+                ];
+                $this->searchQueryBuilder->addFilter($clause);
+                break;
+
+            case Operators::DOES_NOT_CONTAIN:
+                $mustNotClause = [
+                    'query_string' => [
+                        'default_field' => $attributePath,
+                        'query'         => '*' . $value . '*',
+                    ],
+                ];
+
+                $filterClause = [
+                    'exists' => ['field' => $attributePath],
+                ];
+
+                $this->searchQueryBuilder->addMustNot($mustNotClause);
+                $this->searchQueryBuilder->addFilter($filterClause);
+                break;
+
+            case Operators::EQUALS:
+                $clause = [
+                    'term' => [
+                        $attributePath => $value,
+                    ],
+                ];
+                $this->searchQueryBuilder->addFilter($clause);
+                break;
+
+            case Operators::NOT_EQUAL:
+                $mustNotClause = [
+                    'term' => [
+                        $attributePath => $value,
+                    ],
+                ];
+
+                $filterClause = [
+                    'exists' => [
+                        'field' => $attributePath,
+                    ],
+                ];
+                $this->searchQueryBuilder->addMustNot($mustNotClause);
+                $this->searchQueryBuilder->addFilter($filterClause);
+                break;
+
+            case Operators::IN_LIST:
+                $clause = [
+                    'terms' => [
+                        $attributePath => $value,
+                    ],
+                ];
+
+                $this->searchQueryBuilder->addFilter($clause);
+                break;
+
+            case Operators::NOT_IN_LIST:
+                $clause = [
+                    'terms' => [
+                        $attributePath => $value,
+                    ],
+                ];
+
+                $this->searchQueryBuilder->addMustNot($clause);
+                break;
+
+            default:
+                throw InvalidOperatorException::notSupported($operator, static::class);
+        }
+    }
+}
