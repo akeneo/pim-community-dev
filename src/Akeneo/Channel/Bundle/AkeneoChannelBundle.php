53cb353104def4e55c1e36672e170702c22e1d87
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-05-02 11:07:23 +0200

    TIP-839: create empty AkeneoChannelBundle

---
+++ src/Akeneo/Channel/Bundle/AkeneoChannelBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Akeneo\Channel\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author    Julien Janvier <j.janvier@gmail.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AkeneoChannelBundle extends Bundle
+{
+}
