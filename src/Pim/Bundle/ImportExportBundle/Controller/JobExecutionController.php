10c3551809a2d799ac91863346036ca41e2ab283
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-05-04 17:23:50 +0200

    TIP-856: Fix CS

--- src/Pim/Bundle/ImportExportBundle/Controller/JobExecutionController.php
+++ src/Pim/Bundle/ImportExportBundle/Controller/JobExecutionController.php
@@ -2,9 +2,9 @@
 
 namespace Pim\Bundle\ImportExportBundle\Controller;
 
-use Akeneo\Tool\Component\FileStorage\StreamedFileResponse;
 use Akeneo\Tool\Bundle\BatchBundle\Manager\JobExecutionManager;
 use Akeneo\Tool\Bundle\BatchBundle\Monolog\Handler\BatchLogHandler;
+use Akeneo\Tool\Component\FileStorage\StreamedFileResponse;
 use Pim\Bundle\ConnectorBundle\EventListener\JobExecutionArchivist;
 use Pim\Bundle\EnrichBundle\Doctrine\ORM\Repository\JobExecutionRepository;
 use Pim\Bundle\ImportExportBundle\Event\JobExecutionEvents;
