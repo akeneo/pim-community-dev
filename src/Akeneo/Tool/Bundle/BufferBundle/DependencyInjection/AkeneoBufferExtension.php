f1a3e0d8f687e5094c48a975cb6738e7899c3ded
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-05-07 13:02:15 +0200

    TIP-856: Move Pim\BufferBundle classes to Akeneo\Tool

--- src/Akeneo/Tool/Bundle/BufferBundle/DependencyInjection/AkeneoBufferExtension.php
+++ src/Akeneo/Tool/Bundle/BufferBundle/DependencyInjection/AkeneoBufferExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Akeneo\Bundle\BufferBundle\DependencyInjection;
+namespace Akeneo\Tool\Bundle\BufferBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
