41eb954940ac5840894c4c832f98882c10525511
Julian Prud'homme <julian.prudhomme@akeneo.com>
2018-05-14 16:40:58 +0200

    PIM-6350: add change parent processor

---
+++ src/Pim/Bundle/EnrichBundle/Connector/Processor/MassEdit/Product/ChangeParentProcessor.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Bundle\EnrichBundle\Connector\Processor\MassEdit\Product;
+
+use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Pim\Bundle\EnrichBundle\Connector\Processor\AbstractProcessor;
+use Pim\Component\Catalog\Model\EntityWithFamilyVariantInterface;
+use Pim\Component\Catalog\Model\ProductModelInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ChangeParentProcessor extends AbstractProcessor
+{
+    /** @var ValidatorInterface */
+    protected $productValidator;
+
+    /** @var ValidatorInterface */
+    protected $productModelValidator;
+
+    /** @var ObjectUpdaterInterface */
+    protected $productUpdater;
+
+    /** @var ObjectUpdaterInterface */
+    protected $productModelUpdater;
+
+    public function __construct(
+        ValidatorInterface $productValidator,
+        ValidatorInterface $productModelValidator,
+        ObjectUpdaterInterface $productUpdater,
+        ObjectUpdaterInterface $productModelUpdater
+    )
+    {
+        $this->productValidator = $productValidator;
+        $this->productModelValidator = $productModelValidator;
+        $this->productUpdater = $productUpdater;
+        $this->productModelUpdater = $productModelUpdater;
+    }
+
+    public function process($product)
+    {
+        $this->validateProduct($product);
+
+        $newParentCode = $this->getNewParentCode();
+        $this->updateEntity($product, $newParentCode);
+
+        if(! $this->isProductValid($product)) {
+            return null;
+        }
+
+        return $product;
+    }
+
+    private function validateProduct($product)
+    {
+        if(! $product instanceof EntityWithFamilyVariantInterface)
+        {
+            throw new \InvalidArgumentException('The product is not correct');
+        }
+    }
+
+    private function getNewParentCode()
+    {
+        $actions = $this->getConfiguredActions();
+
+        return $actions[0]['value'];
+    }
+
+    private function updateEntity(EntityWithFamilyVariantInterface $product, string $newParentCode): void
+    {
+        $updater = $this->productUpdater;
+
+        if ($product instanceof ProductModelInterface) {
+            $updater = $this->productModelUpdater;
+        }
+
+        $updater->update($product, ['values' => $newParentCode]);
+    }
+
+    private function isProductValid(EntityWithFamilyVariantInterface $product): bool
+    {
+        $validator = $this->productValidator;
+
+        if ($product instanceof ProductModelInterface) {
+            $validator = $this->productModelValidator;
+        }
+
+        $violations = $validator->validate($product);
+        $this->addWarningMessage($violations, $product);
+
+        return 0 === $violations->count();
+    }
+}
---
+++ src/Pim/Bundle/EnrichBundle/spec/Connector/Processor/MassEdit/Product/ChangeParentProcessorSpec.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace spec\Pim\Bundle\EnrichBundle\Connector\Processor\MassEdit\Product;
+
+use Akeneo\Component\Batch\Job\JobParameters;
+use Akeneo\Component\Batch\Model\JobExecution;
+use Akeneo\Component\Batch\Model\StepExecution;
+use Akeneo\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use PhpSpec\ObjectBehavior;
+use Pim\Component\Catalog\Model\EntityWithFamilyVariantInterface;
+use Prophecy\Argument;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ChangeParentProcessorSpec extends ObjectBehavior
+{
+    function let(
+        ValidatorInterface $productValidator,
+        ValidatorInterface $productModelValidator,
+        ObjectUpdaterInterface $productUpdater,
+        ObjectUpdaterInterface $productModelUpdater
+    )
+    {
+        $this->beConstructedWith($productValidator, $productModelValidator, $productUpdater, $productModelUpdater);
+    }
+
+    public function it_throws_an_exception_if_product_is_not_a_correct_type()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)->duringProcess(new \stdClass());
+    }
+
+    public function it_changes_the_parent_of_a_variant_product(
+        $productValidator,
+        $productUpdater,
+        EntityWithFamilyVariantInterface $product,
+        StepExecution $stepExecution,
+        JobExecution $jobExecution,
+        JobParameters $jobParameters
+    )
+    {
+        $this->setStepExecution($stepExecution);
+        $stepExecution->getJobExecution()->willReturn($jobExecution);
+        $jobParameters->get('actions')->willReturn([['value' => '42']]);
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+
+        $violations = new ConstraintViolationList([]);
+        $productValidator->validate($product)->willReturn($violations);
+
+        $productUpdater->update($product, ['values' => '42'])->shouldBeCalled();
+
+        $this->process($product)->shouldReturn($product);
+    }
+
+    public function it_fails_to_update_an_invalid_product(
+        $productValidator,
+        $productUpdater,
+        EntityWithFamilyVariantInterface $product,
+        StepExecution $stepExecution,
+        JobExecution $jobExecution,
+        JobParameters $jobParameters
+    )
+    {
+        $this->setStepExecution($stepExecution);
+        $stepExecution->getJobExecution()->willReturn($jobExecution);
+        $jobParameters->get('actions')->willReturn([['value' => '42']]);
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+
+        $violation = new ConstraintViolation('error1', '', [], '', '', '');
+        $violations = new ConstraintViolationList([$violation]);
+        $productValidator->validate($product)->willReturn($violations);
+
+        $stepExecution->addWarning(Argument::cetera())->shouldBeCalled();
+
+        $productUpdater->update($product, ['values' => '42'])->shouldBeCalled();
+
+        $this->process($product)->shouldReturn(null);
+    }
+}
