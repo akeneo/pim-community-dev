36a3de88bb5957b173cdca47d79ca27ec3f80280
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-05-15 14:18:51 +0200

    TIP-841: Move the channel translation doctrine config to Akeneo\Channel

--- src/Akeneo/Channel/Bundle/DependencyInjection/CompilerPass/ResolveDoctrineTargetModelPass.php
+++ src/Akeneo/Channel/Bundle/DependencyInjection/CompilerPass/ResolveDoctrineTargetModelPass.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Channel\Bundle\DependencyInjection\CompilerPass;
 
 use Akeneo\Channel\Component\Model\ChannelInterface;
+use Akeneo\Channel\Component\Model\ChannelTranslationInterface;
 use Akeneo\Channel\Component\Model\LocaleInterface;
 use Akeneo\Tool\Bundle\StorageUtilsBundle\DependencyInjection\Compiler\AbstractResolveDoctrineTargetModelPass;
 
@@ -25,6 +26,7 @@ class ResolveDoctrineTargetModelPass extends AbstractResolveDoctrineTargetModelP
         return [
             LocaleInterface::class => 'pim_catalog.entity.locale.class',
             ChannelInterface::class => 'pim_catalog.entity.channel.class',
+            ChannelTranslationInterface::class => 'pim_catalog.entity.channel_translation.class',
         ];
     }
 }
