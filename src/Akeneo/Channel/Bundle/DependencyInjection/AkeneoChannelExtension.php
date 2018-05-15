b3fb14159ed122c111846fd89deaddd6cb0c92a0
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-05-15 09:25:02 +0200

    TIP-839: remove useless configuration for ChannelBundle

--- src/Akeneo/Channel/Bundle/DependencyInjection/AkeneoChannelExtension.php
+++ src/Akeneo/Channel/Bundle/DependencyInjection/AkeneoChannelExtension.php
@@ -21,9 +21,6 @@ class AkeneoChannelExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('controllers.yml');
         $loader->load('processors.yml');
