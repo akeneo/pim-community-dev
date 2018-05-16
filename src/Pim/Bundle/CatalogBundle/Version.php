1e05e34c7858d79dbbfda090659a72b0cbc9afe1
Philippe Mossière <philippe.mossiere@akeneo.com>
2018-05-16 15:56:15 +0200

    Preparing version 2.2.5

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.2.4';
+    const VERSION = '2.2.5';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bonanza Bunny';
