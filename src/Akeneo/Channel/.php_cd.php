d90e8c9369ea08c76f034b328ada6275ce1c142b
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-05-23 14:09:17 +0200

    TIP-841: Enable php coupling detector for user management

---
+++ src/Akeneo/Channel/.php_cd.php
@@ -0,0 +1,38 @@
+<?php
+
+use Akeneo\CouplingDetector\Configuration\Configuration;
+use Akeneo\CouplingDetector\Configuration\DefaultFinder;
+use Akeneo\CouplingDetector\RuleBuilder;
+
+$finder = new DefaultFinder();
+$finder->notPath('spec');
+$finder->notPath('tests');
+
+$builder = new RuleBuilder();
+
+$rules = [
+    $builder->only([
+        'Doctrine',
+        'Symfony\Component',
+        'Akeneo\Tool',
+        'Akeneo\Channel',
+        'Oro\Bundle\SecurityBundle\Annotation\AclAncestor',
+        'Pim\Bundle\CatalogBundle\Filter\CollectionFilterInterface', // TODO: we should remove this dependencies, related to permissions
+        'Pim\Component\Catalog\Query\Filter\Operators', // TODO: It should be moved elsewhere
+    ])->in('Akeneo\Channel\Bundle'),
+    $builder->only([
+        'Symfony\Component',
+        'Doctrine\Common',
+        'Akeneo\Tool\Component',
+        'Akeneo\Channel\Component',
+        'Pim\Component\Catalog\Model\CategoryInterface', // TODO: The channel is linked by reference instead of id
+        'Pim\Component\Connector', // TODO: Generic classes/interfaces like be moved to Akeneo/Tool
+        'Pim\Bundle\VersioningBundle\Repository\VersionRepositoryInterface', // TODO: The versioning bundle will be moved to Akeneo\Tool
+        'Pim\Bundle\CatalogBundle\Filter\CollectionFilterInterface', // TODO: we should remove this dependencies, related to permissions
+        'Akeneo\UserManagement\Bundle\Context\UserContext', // TODO: The current local should be given by $context (third parameter of normalize) instead of depending on UserContext
+    ])->in('Akeneo\Channel\Component'),
+];
+
+$config = new Configuration($rules, $finder);
+
+return $config;
