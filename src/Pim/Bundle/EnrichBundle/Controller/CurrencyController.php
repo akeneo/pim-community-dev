18ccfce73dff2f94c7ce9ae8fa0f09173596a640
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-05-23 14:01:32 +0200

    TIP-858: Move Linked Exception to Akeneo\Channel

--- src/Pim/Bundle/EnrichBundle/Controller/CurrencyController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/CurrencyController.php
@@ -6,7 +6,7 @@ use Akeneo\Channel\Component\Model\Currency;
 use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Pim\Bundle\EnrichBundle\Flash\Message;
-use Pim\Component\Catalog\Exception\LinkedChannelException;
+use Akeneo\Channel\Component\Exception\LinkedChannelException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\RouterInterface;
