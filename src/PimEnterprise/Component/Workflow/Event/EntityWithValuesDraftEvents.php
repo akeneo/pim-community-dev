ce8a519d02ee0fdb4252aae7197b2b5ffcdeb18f
Philippe Mossière <philippe.mossiere@akeneo.com>
2018-05-25 16:58:38 +0200

    AOB-62: Draft Builder/Applier/Manager/Events - add strict type and method type hinting - remove `product` word from - remove useless php doc

--- src/PimEnterprise/Component/Workflow/Event/EntityWithValuesDraftEvents.php
+++ src/PimEnterprise/Component/Workflow/Event/EntityWithValuesDraftEvents.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Akeneo PIM Enterprise Edition.
  *
@@ -23,96 +25,96 @@ class EntityWithValuesDraftEvents
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_APPLY = 'pimee_workflow.product_draft.pre_apply';
+    const PRE_APPLY = 'pimee_workflow.draft.pre_apply';
 
     /**
      * This event is dispatched after draft is applied on an entity with values
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_APPLY = 'pimee_workflow.product_draft.post_apply';
+    const POST_APPLY = 'pimee_workflow.draft.post_apply';
 
     /**
      * This event is dispatched before draft is approved
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_APPROVE = 'pimee_workflow.product_draft.pre_approve';
+    const PRE_APPROVE = 'pimee_workflow.draft.pre_approve';
 
     /**
      * This event is dispatched after draft is approved
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_APPROVE = 'pimee_workflow.product_draft.post_approve';
+    const POST_APPROVE = 'pimee_workflow.draft.post_approve';
 
     /**
      * This event is dispatched before draft is refused
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_REFUSE = 'pimee_workflow.product_draft.pre_refuse';
+    const PRE_REFUSE = 'pimee_workflow.draft.pre_refuse';
 
     /**
      * This event is dispatched after draft is refused
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_REFUSE = 'pimee_workflow.product_draft.post_refuse';
+    const POST_REFUSE = 'pimee_workflow.draft.post_refuse';
 
     /**
      * This event is dispatched before draft is partially approved
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_PARTIAL_APPROVE = 'pimee_workflow.product_draft.pre_partial_approve';
+    const PRE_PARTIAL_APPROVE = 'pimee_workflow.draft.pre_partial_approve';
 
     /**
      * This event is dispatched after draft is partially approved
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_PARTIAL_APPROVE = 'pimee_workflow.product_draft.post_partial_approve';
+    const POST_PARTIAL_APPROVE = 'pimee_workflow.draft.post_partial_approve';
 
     /**
      * This event is dispatched before draft is partially refused
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_PARTIAL_REFUSE = 'pimee_workflow.product_draft.pre_partial_refuse';
+    const PRE_PARTIAL_REFUSE = 'pimee_workflow.draft.pre_partial_refuse';
 
     /**
      * This event is dispatched after draft is partially refused
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_PARTIAL_REFUSE = 'pimee_workflow.product_draft.post_partial_refuse';
+    const POST_PARTIAL_REFUSE = 'pimee_workflow.draft.post_partial_refuse';
 
     /**
      * This event is dispatched before draft is removed
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_REMOVE = 'pimee_workflow.product_draft.pre_remove';
+    const PRE_REMOVE = 'pimee_workflow.draft.pre_remove';
 
     /**
      * This event is dispatched after draft is removed
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_REMOVE = 'pimee_workflow.product_draft.post_remove';
+    const POST_REMOVE = 'pimee_workflow.draft.post_remove';
 
     /**
      * This event is dispatched before draft is marked as ready
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const PRE_READY = 'pimee_workflow.product_draft.pre_ready';
+    const PRE_READY = 'pimee_workflow.draft.pre_ready';
 
     /**
      * This event is dispatched after draft is marked as ready
      *
      * The event listener receives a Symfony\Component\EventDispatcher\GenericEvent instance
      */
-    const POST_READY = 'pimee_workflow.product_draft.post_ready';
+    const POST_READY = 'pimee_workflow.draft.post_ready';
 }
