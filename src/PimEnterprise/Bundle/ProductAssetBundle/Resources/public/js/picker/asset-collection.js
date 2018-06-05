cab03ae2cd796472ea6dd153c802fbc080eac0d8
pierallard <pierallard@gmail.com>
2018-06-05 16:17:01 +0200

    [PIM-7397] Add description and title

--- src/PimEnterprise/Bundle/ProductAssetBundle/Resources/public/js/picker/asset-collection.js
+++ src/PimEnterprise/Bundle/ProductAssetBundle/Resources/public/js/picker/asset-collection.js
@@ -36,7 +36,7 @@ define(
             template: _.template(template),
             events: {
                 'click .add-asset': 'updateAssets',
-                'click .asset-thumbnail': 'openModal'
+                'click .asset-thumbnail': 'openPreviewModal'
             },
             modalTemplate: _.template(templateModal),
 
@@ -167,60 +167,70 @@ define(
                 return deferred.promise();
             },
 
-            openModal(clickEvent) {
-                const modal = new Backbone.BootstrapModal({
-                    className: 'modal modal--fullPage modal--topButton',
-                    modalOptions: {
-                        backdrop: 'static',
-                        keyboard: false
-                    },
-                    allowCancel: true,
-                    okCloses: false,
-                    template: this.modalTemplate,
-                    assets: this.data,
-                    locale: this.context.locale,
-                    scope: this.context.scope,
-                    content: '',
-                    thumbnailFilter: 'thumbnail'
-                });
-                modal.open();
-
-                const switchModalItem = function (item) {
-                    modal.$('.AknAssetCollectionField-listItem')
-                        .addClass('AknAssetCollectionField-listItem--transparent');
-                    item.removeClass('AknAssetCollectionField-listItem--transparent');
-                    $('.main-preview').attr('src', item.data('url'));
-                    $('.AknAssetPreview-buttons').stop(true, true).animate({
-                        scrollLeft: item.position().left
-                        - ($('.AknAssetPreview-buttons').width() - 140) / 2
-                    }, 400);
-                };
-
-                const switchWithGap = function (gap) {
-                    const thumbnails = modal.$('.asset-thumbnail-item');
-                    let clickedIndex = null;
-                    thumbnails.each(function (i, thumbnail) {
-                        if (!($(thumbnail).hasClass('AknAssetCollectionField-listItem--transparent'))) {
-                            clickedIndex = i;
-                        }
+            /**
+             * Opens a modal to show the preview
+             *
+             * @param {Event} clickEvent
+             */
+            openPreviewModal(clickEvent) {
+                FetcherRegistry.getFetcher('asset').fetchByIdentifiers(this.data).then(function (assets) {
+                    const modal = new Backbone.BootstrapModal({
+                        className: 'modal modal--fullPage modal--topButton',
+                        modalOptions: {
+                            backdrop: 'static',
+                            keyboard: false
+                        },
+                        allowCancel: true,
+                        okCloses: false,
+                        template: this.modalTemplate,
+                        assets: assets,
+                        locale: this.context.locale,
+                        scope: this.context.scope,
+                        content: '',
+                        thumbnailFilter: 'thumbnail',
+                        assetCollectionPreviewTitle: __('pimee_product_asset.form.product.asset.preview_title')
                     });
-                    switchModalItem($(thumbnails[(clickedIndex + gap + thumbnails.length) % thumbnails.length]));
-                };
+                    modal.open();
 
-                modal.$('.AknAssetCollectionField-listItem').click(function () {
-                    switchModalItem($(this));
-                });
+                    const switchModalItem = function (item) {
+                        modal.$('.AknAssetCollectionField-listItem')
+                            .addClass('AknAssetCollectionField-listItem--transparent');
+                        item.removeClass('AknAssetCollectionField-listItem--transparent');
+                        $('.main-preview').attr('src', item.data('url'));
+                        $('.AknAssetPreview-buttons').stop(true, true).animate({
+                            scrollLeft: item.position().left
+                            - ($('.AknAssetPreview-buttons').width() - 140) / 2
+                        }, 400);
+                        modal.$('.description').html(item.data('description'));
+                    };
 
-                modal.$('.browse-left').click(function () {
-                    switchWithGap(-1);
-                });
+                    const switchWithGap = function (gap) {
+                        const thumbnails = modal.$('.asset-thumbnail-item');
+                        let clickedIndex = null;
+                        thumbnails.each(function (i, thumbnail) {
+                            if (!($(thumbnail).hasClass('AknAssetCollectionField-listItem--transparent'))) {
+                                clickedIndex = i;
+                            }
+                        });
+                        switchModalItem($(thumbnails[(clickedIndex + gap + thumbnails.length) % thumbnails.length]));
+                    };
 
-                modal.$('.browse-right').click(function () {
-                    switchWithGap(1);
-                });
+                    modal.$('.AknAssetCollectionField-listItem').click(function () {
+                        switchModalItem($(this));
+                    });
 
-                const clickedAsset = $(clickEvent.currentTarget).closest('.asset-thumbnail-item').data('asset');
-                switchModalItem(modal.$('.asset-thumbnail-item[data-asset="' + clickedAsset + '"]'));
+                    modal.$('.browse-left').click(function () {
+                        switchWithGap(-1);
+                    });
+
+                    modal.$('.browse-right').click(function () {
+                        switchWithGap(1);
+                    });
+
+                    const clickedAsset = $(clickEvent.currentTarget).closest('.asset-thumbnail-item').data('asset');
+                    switchModalItem(modal.$('.asset-thumbnail-item[data-asset="' + clickedAsset + '"]'));
+                    this.delegateEvents();
+                }.bind(this));
             }
         });
     }
