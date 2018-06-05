6e6e74da50d27aa51c4e03f4e6045b95408092d8
Laurent Petard <laurent.petard@akeneo.com>
2018-06-05 14:50:13 +0200

    Preparing version 1.7.22

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.21';
+    const VERSION = '1.7.22';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
