628f726378cb6e5f053220b64d78fdf4c4bb0132
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-06-12 09:43:42 +0200

    TIP-842: Move attribute normalizer to Akeneo\Pim\Structure (EE)

--- src/PimEnterprise/Bundle/CatalogBundle/DependencyInjection/PimEnterpriseCatalogExtension.php
+++ src/PimEnterprise/Bundle/CatalogBundle/DependencyInjection/PimEnterpriseCatalogExtension.php
@@ -37,8 +37,6 @@ class PimEnterpriseCatalogExtension extends Extension
         $loader->load('security/query_builders.yml');
         $loader->load('security/savers.yml');
         $loader->load('security/updaters.yml');
-        $loader->load('serializers.yml');
-        $loader->load('serializers_standard.yml');
         $loader->load('updaters.yml');
         $loader->load('validators.yml');
         $loader->load('versions.yml');
