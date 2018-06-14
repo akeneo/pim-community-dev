64b2c004d210e30530940a539e191bc7d57126b5
christophe-chausseray <chausseray.christophe@gmail.com>
2018-06-14 10:56:17 +0200

    Remove loading ee decorator in the test-helper

--- tests/front/acceptance/test-helpers.js
+++ tests/front/acceptance/test-helpers.js
@@ -1,6 +1,5 @@
 const glob = require('glob');
 const path = require('path');
-const decoratorFiles = glob.sync(path.resolve(process.cwd(), './cucumber/decorators/**/*.decorator.js'));
 const localeDecoratorFiles = glob.sync(path.resolve(__dirname, './cucumber/decorators/**/*.decorator.js'));
 const createDecoratorFile = path.resolve(__dirname, './cucumber/decorators/common/create-element-decorator.js');
 const decorators = {};
@@ -8,9 +7,8 @@ const decorators = {};
 const requireDecorator = loadedDecorators => file => {
     const decorator = require(file);
     loadedDecorators[Object.keys(decorator)[0] || decorator.name] = decorator;
-}
+};
 
-decoratorFiles.forEach(requireDecorator(decorators));
 localeDecoratorFiles.forEach(requireDecorator(decorators));
 
 module.exports = {
