9b7c6f2552b78bae17ab8b85c7fd246e7893ff80
jmleroux <jmleroux.pro@gmail.com>
2018-06-15 11:01:09 +0200

    PIM-7430: new `AssociationsUpdateGuesser` for product versioning

--- src/Pim/Bundle/VersioningBundle/UpdateGuesser/AssociationsUpdateGuesser.php
+++ src/Pim/Bundle/VersioningBundle/UpdateGuesser/AssociationsUpdateGuesser.php
@@ -2,18 +2,17 @@
 
 namespace Pim\Bundle\VersioningBundle\UpdateGuesser;
 
-use Akeneo\Component\Versioning\Model\VersionableInterface;
-use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManager;
+use Pim\Component\Catalog\Model\AssociationInterface;
 
 /**
- * Fields update guesser
+ * Guess for associations updates and add the owner product entity to pendings versionnning if needed
  *
- * @author    Nicolas Dupont <nicolas@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @author    jm leroux <jean-marie.leroux@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class VersionableUpdateGuesser implements UpdateGuesserInterface
+class AssociationsUpdateGuesser implements UpdateGuesserInterface
 {
     /**
      * Entities configured as versionable without implementing interface because coming
@@ -47,10 +46,8 @@ class VersionableUpdateGuesser implements UpdateGuesserInterface
     public function guessUpdates(EntityManager $em, $entity, $action)
     {
         $pendings = [];
-        if ($entity instanceof VersionableInterface ||
-            in_array(ClassUtils::getClass($entity), $this->versionableEntities)
-        ) {
-            $pendings[] = $entity;
+        if ($entity instanceof AssociationInterface) {
+            $pendings[] = $entity->getOwner();
         }
 
         return $pendings;
