3093bf21988385076b929574b889ccf14695bd47
christophe-chausseray <chausseray.christophe@gmail.com>
2018-06-19 10:54:42 +0200

    PIM-7381 : Update normalize to have the enriched entity to do it itself.

--- src/Akeneo/EnrichedEntity/front/infrastructure/saver/enriched-entity.ts
+++ src/Akeneo/EnrichedEntity/front/infrastructure/saver/enriched-entity.ts
@@ -1,6 +1,5 @@
 import Saver from 'akeneoenrichedentity/domain/saver/saver';
 import EnrichedEntity from 'akeneoenrichedentity/domain/model/enriched-entity/enriched-entity';
-import enrichedEntityNormalizer from 'akeneoenrichedentity/infrastructure/normalizer/enriched-entity';
 import {postJSON} from 'akeneoenrichedentity/tools/fetch';
 import hydrator from 'akeneoenrichedentity/application/hydrator/enriched-entity';
 
@@ -18,7 +17,7 @@ export class EnrichedEntitySaverImplementation implements EnrichedEntitySaver {
       routing.generate('akeneo_enriched_entities_enriched_entities_edit_rest', {
         identifier: enrichedEntity.getIdentifier().stringValue(),
       }),
-      enrichedEntityNormalizer.normalize(enrichedEntity)
+      enrichedEntity.normalize()
     );
 
     return this.hydrator(backendEnrichedEntity);
