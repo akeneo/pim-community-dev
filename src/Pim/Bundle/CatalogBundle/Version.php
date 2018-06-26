bb61e941f0d4de466d2db6cf5b3e5c5bb00aa82a
Klein Florian <florian.klein@free.fr>
2018-06-26 12:00:25 +0200

    Preparing version 2.0.28

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.27';
+    const VERSION = '2.0.28';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
