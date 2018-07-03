2069779a4677032e5dee401e5b30e47e12a9c5ea
christophe-chausseray <chausseray.christophe@gmail.com>
2018-07-03 09:24:54 +0200

    PIM-7381 : Add notification message

---
+++ src/Akeneo/EnrichedEntity/front/infrastructure/middleware/form-notifier.ts
@@ -0,0 +1,31 @@
+import __ from 'akeneoenrichedentity/tools/translator';
+
+const messenger = require('oro/messenger');
+
+export default () => () => (next: any) => (action: any) => {
+  if ('POST_SAVE' === action.type) {
+    messenger.notify(
+      'success',
+      __('pim_enrich.entity.fallback.flash.update.success')
+    );
+
+    return;
+  }
+
+  if ('FAIL_SAVE' === action.type) {
+    if (action.response && 500 == action.response.status) {
+      const message = action.response.responseJSON ? action.response.responseJSON : action.response;
+
+      console.error('Errors:', message);
+    }
+
+    messenger.notify(
+      'error',
+      __('pim_enrich.entity.fallback.flash.update.fail')
+    );
+
+    return;
+  }
+
+  return next(action);
+};
