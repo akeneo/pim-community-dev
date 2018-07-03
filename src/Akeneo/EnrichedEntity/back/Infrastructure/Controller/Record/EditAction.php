ba95326b33ecf3f72c8fc29f6ab52fa58a403914
Adrien Pétremann <hello@grena.fr>
2018-07-03 17:55:53 +0200

    PIM-7435: add action to edit a record

---
+++ src/Akeneo/EnrichedEntity/back/Infrastructure/Controller/Record/EditAction.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\EnrichedEntity\Infrastructure\Controller\Record;
+
+use Akeneo\EnrichedEntity\Application\Record\EditRecord\EditRecordCommand;
+use Akeneo\EnrichedEntity\Application\Record\EditRecord\EditRecordHandler;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Validate & save a record
+ *
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2018 Akeneo SAS (https://www.akeneo.com)
+ */
+class EditAction
+{
+    /** @var EditRecordHandler */
+    private $editRecordHandler;
+
+    /** @var Serializer */
+    private $serializer;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    public function __construct(
+        EditRecordHandler $editRecordHandler,
+        Serializer $serializer,
+        ValidatorInterface $validator
+    ) {
+        $this->editRecordHandler = $editRecordHandler;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return new RedirectResponse('/');
+        }
+
+        $command = $this->serializer->deserialize($request->getContent(), EditRecordCommand::class, 'json');
+        $violations = $this->validator->validate($command);
+
+        if ($violations->count() > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                // TODO: format the error the way we want for the front
+                $errors[] = $violation->getPropertyPath() .' '. $violation->getMessage();
+            }
+
+            return new JsonResponse(['errors' => json_encode($errors)], Response::HTTP_BAD_REQUEST);
+        }
+
+        ($this->editRecordHandler)($command);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ src/Akeneo/EnrichedEntity/tests/back/spec/Application/Record/EditRecord/EditRecordHandlerSpec.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace spec\Akeneo\EnrichedEntity\Application\Record\EditRecord;
+
+use Akeneo\EnrichedEntity\Application\Record\EditRecord\EditRecordCommand;
+use Akeneo\EnrichedEntity\Application\Record\EditRecord\EditRecordHandler;
+use Akeneo\EnrichedEntity\Domain\Model\EnrichedEntity\EnrichedEntityIdentifier;
+use Akeneo\EnrichedEntity\Domain\Model\LabelCollection;
+use Akeneo\EnrichedEntity\Domain\Model\Record\Record;
+use Akeneo\EnrichedEntity\Domain\Model\Record\RecordIdentifier;
+use Akeneo\EnrichedEntity\Domain\Repository\RecordRepositoryInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class EditRecordHandlerSpec extends ObjectBehavior
+{
+    function let(RecordRepositoryInterface $recordRepository)
+    {
+        $this->beConstructedWith($recordRepository);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(EditRecordHandler::class);
+    }
+
+    function it_edits_a_record(
+        RecordRepositoryInterface $recordRepository,
+        EditRecordCommand $editRecordCommand,
+        Record $record
+    ) {
+        $editRecordCommand->identifier = 'sony';
+        $editRecordCommand->enrichedEntityIdentifier = 'brand';
+        $editRecordCommand->labels = [
+            'fr_FR' => 'Sony',
+            'en_US' => 'Sony',
+        ];
+
+        $recordRepository->getByIdentifier(
+            Argument::type(EnrichedEntityIdentifier::class),
+            Argument::type(RecordIdentifier::class)
+        )->willReturn($record);
+
+        $record->updateLabels(Argument::type(LabelCollection::class))->shouldBeCalled();
+        $recordRepository->save($record)->shouldBeCalled();
+
+        $this->__invoke($editRecordCommand);
+    }
+}
