280b35f60dacbc70130af295c9d15612a5946d4a
Samir Boulil <samir.boulil@akeneo.com>
2018-07-04 11:33:05 +0200

    PIM-7433: Create enriched entity application service

---
+++ src/Akeneo/EnrichedEntity/back/Application/EnrichedEntity/CreateEnrichedEntity/CreateEnrichedEntityHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\EnrichedEntity\Application\EnrichedEntity\CreateEnrichedEntity;
+
+use Akeneo\EnrichedEntity\Domain\Model\EnrichedEntity\EnrichedEntity;
+use Akeneo\EnrichedEntity\Domain\Model\EnrichedEntity\EnrichedEntityIdentifier;
+use Akeneo\EnrichedEntity\Domain\Repository\EnrichedEntityRepository;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2018 Akeneo SAS (https://www.akeneo.com)
+ */
+class CreateEnrichedEntityHandler
+{
+    /** @var EnrichedEntityRepository */
+    private $enrichedEntityRepository;
+
+    public function __construct(EnrichedEntityRepository $enrichedEntityRepository)
+    {
+        $this->enrichedEntityRepository = $enrichedEntityRepository;
+    }
+
+    public function __invoke(CreateEnrichedEntityCommand $createEnrichedEntityCommand): void
+    {
+        $enrichedEntity = EnrichedEntity::create(
+            EnrichedEntityIdentifier::fromString($createEnrichedEntityCommand->identifier),
+            $createEnrichedEntityCommand->labels
+        );
+
+        $this->enrichedEntityRepository->save($enrichedEntity);
+    }
+}
