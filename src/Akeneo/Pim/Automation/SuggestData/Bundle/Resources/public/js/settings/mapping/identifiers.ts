3a7f2ce7bef0efdfb6cf623212231b73eda27704
Julian Prud'homme <julian.prudhomme@akeneo.com>
2018-07-06 17:52:56 +0200

    ASI-14: display pim.ai identifiers mapping

--- src/Akeneo/Pim/Automation/SuggestData/Bundle/Resources/public/js/settings/mapping/identifiers.ts
+++ src/Akeneo/Pim/Automation/SuggestData/Bundle/Resources/public/js/settings/mapping/identifiers.ts
@@ -1,5 +1,6 @@
 import * as _ from "underscore";
 import BaseView = require('pimenrich/js/view/base');
+import {getIdentifiersMapping} from 'akeneosuggestdata/js/settings/mapping/fetcher/mapping-fetcher';
 
 const __ = require('oro/translator');
 const template = require('pimee/template/settings/mapping/identifiers');
@@ -15,45 +16,48 @@ interface EditIdentifiersMappingConfig {
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class EditIdentifiersMappingView extends BaseView {
-    readonly template = _.template(template);
+  readonly template = _.template(template);
 
-    readonly identifiers = [
-        {'label':__('akeneo_suggest_data.settings.index.tab.identifiers.headers.brand_label')},
-        {'label':'MPN'},
-        {'label':'UPC'},
-        {'label':'ASIN'},
-    ];
+  readonly headers = {
+    'identifiersLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.identifiers_label'),
+    'attributeLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.attribute_label'),
+    'suggestDataLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.suggest_data_label'),
+  };
 
-    readonly headers = {
-        'identifiersLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.identifiers_label'),
-        'attributeGroupLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.attribute_group_label'),
-        'attributeLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.attribute_label'),
-        'suggestDataLabel': __('akeneo_suggest_data.settings.index.tab.identifiers.headers.suggest_data_label'),
-    };
+  readonly config: EditIdentifiersMappingConfig = {};
 
-    readonly config: EditIdentifiersMappingConfig = {
-    };
+  /**
+   * {@inheritdoc}
+   */
+  constructor(options: { config: EditIdentifiersMappingConfig }) {
+    super(options);
 
-    /**
-     * {@inheritdoc}
-     */
-    constructor(options: { config: EditIdentifiersMappingConfig }) {
-        super(options);
+    this.config = {...this.config, ...options.config};
+  };
 
-        this.config = {...this.config, ...options.config};
-    };
+  /**
+   * {@inheritdoc}
+   */
+  public render(): BaseView {
+    getIdentifiersMapping().then((identifiersMapping: any) => {
+      const translatedIdentifiersMapping: any = {};
 
-    /**
-     * {@inheritdoc}
-     */
-    public render(): BaseView {
-        this.$el.html(this.template({
-            headers: this.headers,
-            identifiers: this.identifiers,
-        }));
+      Object.keys(identifiersMapping).forEach((pimAiAttributeCode: string) => {
+        const pimAiAttributeLabel: string = __(
+          `akeneo_suggest_data.settings.index.tab.identifiers.headers.${pimAiAttributeCode}_label`
+        );
 
-        return this;
-    }
+        translatedIdentifiersMapping[pimAiAttributeLabel] = identifiersMapping[pimAiAttributeCode];
+      });
+
+      this.$el.html(this.template({
+        headers: this.headers,
+        identifiers: translatedIdentifiersMapping,
+      }));
+    });
+
+    return this;
+  }
 }
 
 export = EditIdentifiersMappingView;
