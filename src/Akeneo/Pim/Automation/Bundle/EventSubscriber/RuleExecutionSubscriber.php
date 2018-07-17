671d94a67bc604898241b1bc37cc52bb870111d6
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-07-17 10:03:16 +0200

    TIP-866: Move rule engine bundle to Akeneo\Pim\Automation

---
+++ src/Akeneo/Pim/Automation/Bundle/EventSubscriber/RuleExecutionSubscriber.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2016 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\Bundle\EventSubscriber;
+
+use Akeneo\Tool\Bundle\RuleEngineBundle\Event\RuleEvents;
+use Pim\Bundle\NotificationBundle\Entity\NotificationInterface;
+use Pim\Bundle\NotificationBundle\NotifierInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\ChainUserProvider;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @author Damien Carcel <damien.carcel@gmail.com>
+ */
+class RuleExecutionSubscriber implements EventSubscriberInterface
+{
+    /** @var ChainUserProvider */
+    protected $chainUserProvider;
+
+    /** @var NotifierInterface */
+    protected $notifier;
+
+    /** @var string */
+    protected $notificationClass;
+
+    /**
+     * @param ChainUserProvider     $chainUserProvider
+     * @param NotifierInterface     $notifier
+     * @param string                $notificationClass
+     */
+    public function __construct(
+        ChainUserProvider $chainUserProvider,
+        NotifierInterface $notifier,
+        $notificationClass
+    ) {
+        $this->chainUserProvider = $chainUserProvider;
+        $this->notifier = $notifier;
+        $this->notificationClass = $notificationClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RuleEvents::POST_EXECUTE_ALL => 'afterJobExecution',
+        ];
+    }
+
+    /**
+     * Notify a user of the end of the command.
+     *
+     * @param GenericEvent $event
+     */
+    public function afterJobExecution(GenericEvent $event): void
+    {
+        $rules = $event->getSubject();
+        $user = $this->getUserFromEvent($event);
+
+        if (null === $user || 0 === count($rules)) {
+            return;
+        }
+
+        $notification = $this->createNotification();
+        $this->notifier->notify($notification, [$user]);
+    }
+
+    /**
+     * @return NotificationInterface
+     */
+    protected function createNotification(): NotificationInterface
+    {
+        $notification = new $this->notificationClass();
+        $notification
+            ->setType('success')
+            ->setMessage('pimee_catalog_rule.notification.rule.executed')
+            ->setRoute('pimee_catalog_rule_rule_index')
+            ->setContext([
+                'actionType'       => 'rule',
+                'showReportButton' => false,
+            ]);
+
+        return $notification;
+    }
+
+    /**
+     * @param GenericEvent $event
+     *
+     * @return null|UserInterface
+     */
+    protected function getUserFromEvent(GenericEvent $event): ?UserInterface
+    {
+        try {
+            $userToNotify = $event->getArgument('username');
+        } catch (\InvalidArgumentException $e) {
+            return null;
+        }
+
+        try {
+            return $this->chainUserProvider->loadUserByUsername($userToNotify);
+        } catch (UsernameNotFoundException $e) {
+            return null;
+        }
+    }
+}
---
+++ src/Akeneo/Pim/Automation/Bundle/Repository/RuleDefinitionMassEditRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2016 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\Bundle\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Pim\Bundle\DataGridBundle\Doctrine\ORM\Repository\MassActionRepositoryInterface;
+
+/**
+ * @author Marie Bochu <marie.bochu@akeneo.com>
+ */
+class RuleDefinitionMassEditRepository implements MassActionRepositoryInterface
+{
+    /** @var string */
+    protected $entityName;
+
+    /** @var EntityManager */
+    protected $entityManager;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param string        $entityName
+     */
+    public function __construct(EntityManager $entityManager, $entityName)
+    {
+        $this->entityManager = $entityManager;
+        $this->entityName = $entityName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function applyMassActionParameters($qb, $inset, array $values)
+    {
+        if (!empty($values)) {
+            $rootAlias = $qb->getRootAlias();
+            $valueWhereCondition =
+                $inset
+                    ? $qb->expr()->in($rootAlias, $values)
+                    : $qb->expr()->notIn($rootAlias, $values);
+            $qb->andWhere($valueWhereCondition);
+        }
+
+        $qb
+            ->resetDQLPart('orderBy')
+            ->setMaxResults(null);
+    }
+
+    /**
+     * Delete a list of rules
+     *
+     * @param mixed[] $ids
+     *
+     * @return int number of impacted rows
+     */
+    public function deleteFromIds(array $ids)
+    {
+        if (empty($ids)) {
+            return 0;
+        }
+
+        return $this->entityManager->createQueryBuilder()
+            ->delete($this->entityName, 'r')
+            ->where('r.id IN (:ids)')
+            ->setParameter('ids', $ids)
+            ->getQuery()
+            ->execute();
+    }
+}
