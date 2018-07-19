6406dd1175222d3d2b1e047df87a3db948b1147a
Mathias Métayer <mathias.metayer@akeneo.com>
2018-07-19 17:29:36 +0200

    APAI-41: Create ProductSubscriptionResponse

---
+++ src/Akeneo/Pim/Automation/SuggestData/Component/Model/ProductSubscriptionResponse.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\SuggestData\Component\Model;
+
+/**
+ * Represents a standard response from a subscription request
+ * Holds a subscription id and optional suggested data
+ *
+ * @author Mathias METAYER <mathias.metayer@akeneo.com>
+ */
+final class ProductSubscriptionResponse
+{
+    /** @var string */
+    private $subscriptionId;
+
+    /** @var array */
+    private $suggestedData;
+
+    /**
+     * @param string $subscriptionId
+     * @param array $suggestedData
+     */
+    public function __construct(string $subscriptionId, array $suggestedData)
+    {
+        $this->validate($subscriptionId, $suggestedData);
+
+        $this->subscriptionId = $subscriptionId;
+        $this->suggestedData = $suggestedData;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubscriptionId(): string
+    {
+        return $this->subscriptionId;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSuggestedData(): array
+    {
+        return $this->suggestedData;
+    }
+
+    /**
+     * @param string $subscriptionId
+     * @param array $suggestedData
+     */
+    private function validate(string $subscriptionId, array $suggestedData): void
+    {
+        if ('' === $subscriptionId) {
+            throw new \InvalidArgumentException('subscription id cannot be empty');
+        }
+        // TODO: validate suggested data format?
+    }
+}
