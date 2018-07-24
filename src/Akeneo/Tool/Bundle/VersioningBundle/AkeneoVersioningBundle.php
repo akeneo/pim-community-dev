b3595940b9b3b393040ffa8304235606d810d31b
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-07-24 18:07:05 +0200

    TIP-869: Rename the bundle

--- src/Akeneo/Tool/Bundle/VersioningBundle/AkeneoVersioningBundle.php
+++ src/Akeneo/Tool/Bundle/VersioningBundle/AkeneoVersioningBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PimVersioningBundle extends Bundle
+class AkeneoVersioningBundle extends Bundle
 {
     /**
      * {@inheritdoc}
