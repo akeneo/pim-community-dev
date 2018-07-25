00e3cfd5832a804f2e4ed6d0de671ae8da8fc568
Samir Boulil <samir.boulil@akeneo.com>
2018-07-25 15:27:21 +0200

    PIM-7492: Add ImageAttributeAllowedExtension VO

---
+++ src/Akeneo/EnrichedEntity/back/Domain/Model/Attribute/AttributeAllowedExtensions.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\EnrichedEntity\Domain\Model\Attribute;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeAllowedExtensions
+{
+    /** @var string[] */
+    private $allowedExtensions;
+
+    public function __construct(array $allowedExtensions)
+    {
+        array_map(function ($allowedExtension) {
+            Assert::string($allowedExtension, 'Expected allowed extension to be a string');
+        }, $allowedExtensions);
+        $this->allowedExtensions = $allowedExtensions;
+    }
+
+    public static function fromList(array $allowedExtensions) : self
+    {
+        return new self($allowedExtensions);
+    }
+}
