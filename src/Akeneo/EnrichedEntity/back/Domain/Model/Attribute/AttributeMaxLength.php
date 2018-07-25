e07118c6201c35d0312a00deee30453e7e104192
Samir Boulil <samir.boulil@akeneo.com>
2018-07-25 14:36:25 +0200

    PIM-7492: Add AttributeMaxLength VO

---
+++ src/Akeneo/EnrichedEntity/back/Domain/Model/Attribute/AttributeMaxLength.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\EnrichedEntity\Domain\Model\Attribute;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeMaxLength
+{
+    private const LIMIT = 65535;
+
+    /*** @var int */
+    private $maxLength;
+
+    public function __construct(int $maxLength)
+    {
+        Assert::natural($maxLength, sprintf('The maximum length should be positive, %d given', $maxLength));
+        Assert::lessThanEq(
+            $maxLength,
+            self::LIMIT,
+            sprintf('The maximum length authorized is %d, %d given', self::LIMIT, $maxLength)
+        );
+        $this->maxLength = $maxLength;
+    }
+
+    public static function fromInteger(int $maxLength) : self
+    {
+        return new self($maxLength);
+    }
+}
---
+++ src/Akeneo/EnrichedEntity/tests/back/spec/Domain/Model/Attribute/AttributeMaxLengthSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec\Akeneo\EnrichedEntity\Domain\Model\Attribute;
+
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeMaxLength;
+use PhpSpec\ObjectBehavior;
+
+class AttributeMaxLengthSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedThrough('fromInteger', [300]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(AttributeMaxLength::class);
+    }
+
+    function it_cannot_be_greater_than_65535()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)->during('fromInteger', [65536]);
+    }
+
+    function it_cannot_be_negative()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)->during('fromInteger', [-1]);
+    }
+}
