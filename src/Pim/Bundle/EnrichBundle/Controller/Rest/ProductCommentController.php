4075c925364cbc81c19983ad1d1677b3bb35569b
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-07-27 15:07:13 +0200

    TIP-871: Fix review feedback

--- src/Pim/Bundle/EnrichBundle/Controller/Rest/ProductCommentController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/Rest/ProductCommentController.php
@@ -63,16 +63,16 @@ class ProductCommentController
     protected $localeResolver;
 
     /**
-     * @param TokenStorageInterface                                           $tokenStorage
-     * @param FormFactoryInterface                                            $formFactory
-     * @param ProductRepositoryInterface                                      $productRepository
-     * @param CommentRepositoryInterface                                      $commentRepository
-     * @param SaverInterface                                                  $commentSaver
-     * @param \Akeneo\Pim\Enrichment\Component\Comment\Builder\CommentBuilder $commentBuilder
-     * @param NormalizerInterface                                             $normalizer
-     * @param ValidatorInterface                                              $validator
-     * @param PresenterInterface                                              $datetimePresenter
-     * @param LocaleResolver                                                  $localeResolver
+     * @param TokenStorageInterface      $tokenStorage
+     * @param FormFactoryInterface       $formFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param CommentRepositoryInterface $commentRepository
+     * @param SaverInterface             $commentSaver
+     * @param CommentBuilder             $commentBuilder
+     * @param NormalizerInterface        $normalizer
+     * @param ValidatorInterface         $validator
+     * @param PresenterInterface         $datetimePresenter
+     * @param LocaleResolver             $localeResolver
      */
     public function __construct(
         TokenStorageInterface $tokenStorage,
