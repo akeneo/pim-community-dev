91d0e9577238fdb5ebfe267d4f08423e78a15c34
Tamara Robichet <tamara.robichet@akeneo.com>
2018-07-30 14:58:53 +0200

    PIM-7538: Fix view
    PIM-7538: Fix view
    
    PIM-7538: Fix tests

--- src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/reset-collection-action.js
+++ src/Pim/Bundle/DataGridBundle/Resources/public/js/datagrid/action/reset-collection-action.js
@@ -39,14 +39,14 @@ function(_, AbstractAction, DatagridState) {
          */
         execute: function() {
             var initialState = this.collection.initialState;
+            var datagridState = DatagridState.get(this.datagrid.name, ['initialViewState'])
             var view = initialState.parameters.view
 
             if (_.has(initialState, 'filters')) {
                 initialState.filters = _.omit(initialState.filters, 'scope');
             }
 
-            if (view.id !== '0') {
-                var datagridState = DatagridState.get(this.datagrid.name, ['initialViewState'])
+            if (view && view.id !== '0' && datagridState.initialViewState.length !== 0) {
                 initialState = this.collection.decodeStateData(datagridState.initialViewState)
             }
 
