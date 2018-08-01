5d1700643c8f45161dc20afb28150b64f819de50
Samir Boulil <samir.boulil@akeneo.com>
2018-08-01 17:01:56 +0200

    PIM-7433: Make the 'test_fake' env kernel compile

--- tests/back/Acceptance/User/InMemoryGroupRepository.php
+++ tests/back/Acceptance/User/InMemoryGroupRepository.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace Akeneo\Test\Acceptance\User;
 
+use Akeneo\Test\Acceptance\Common\NotImplementedException;
 use Akeneo\Tool\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
 use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
 use Akeneo\UserManagement\Component\Model\GroupInterface;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Persistence\ObjectRepository;
 
 /**
  * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class InMemoryGroupRepository implements IdentifiableObjectRepositoryInterface, SaverInterface
+class InMemoryGroupRepository implements IdentifiableObjectRepositoryInterface, SaverInterface, ObjectRepository
 {
     /** @var GroupInterface[] */
     private $groups;
@@ -46,5 +48,45 @@ class InMemoryGroupRepository implements IdentifiableObjectRepositoryInterface, 
     public function findOneByIdentifier($identifier)
     {
         return $this->groups->get($identifier);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function find($id)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findOneBy(array $criteria)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassName()
+    {
+        throw new NotImplementedException();
     }
 }
