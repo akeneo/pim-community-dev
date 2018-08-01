ec031e84d8a795825a65d4c012969867bcba33ff
pierallard <pierallard@gmail.com>
2018-08-01 14:05:39 +0200

    [PEFize users] Fix user factory

--- src/Akeneo/UserManagement/Bundle/Factory/UserFactory.php
+++ src/Akeneo/UserManagement/Bundle/Factory/UserFactory.php
@@ -9,6 +9,7 @@ use Akeneo\Channel\Component\Repository\LocaleRepositoryInterface;
 use Akeneo\Pim\Enrichment\Component\Category\Model\CategoryInterface;
 use Akeneo\Tool\Component\Classification\Repository\CategoryRepositoryInterface;
 use Akeneo\Tool\Component\StorageUtils\Factory\SimpleFactoryInterface;
+use Akeneo\UserManagement\Component\Model\Group;
 use Akeneo\UserManagement\Component\Model\User;
 use Akeneo\UserManagement\Component\Repository\GroupRepositoryInterface;
 use Pim\Bundle\LocalizationBundle\Provider\UiLocaleProvider;
@@ -58,36 +59,56 @@ class UserFactory implements SimpleFactoryInterface
     public function create()
     {
         $user = new User();
-        $user->setUiLocale($this->getDefaultUiLocale());
-        $user->setCatalogLocale($this->getDefaultCatalogLocale());
-        $user->setCatalogScope($this->getDefaultCatalogScope());
-        $user->setDefaultTree($this->getDefaultCategoryTree());
-        $user->addGroup($this->getDefaultGroup());
+        if (null !== $uiLocale = $this->getDefaultUiLocale()) {
+            $user->setUiLocale($uiLocale);
+        }
+        if (null !== $catalogLocale = $this->getDefaultCatalogLocale()) {
+            $user->setCatalogLocale($catalogLocale);
+        }
+        if (null !== $catalogScope = $this->getDefaultCatalogScope()) {
+            $user->setCatalogScope($catalogScope);
+        }
+        if (null !== $categoryTree = $this->getDefaultCategoryTree()) {
+            $user->setDefaultTree($categoryTree);
+        }
+        if (null !== $group = $this->getDefaultGroup()) {
+            $user->addGroup($group);
+        }
 
         return $user;
     }
 
-    private function getDefaultCatalogLocale(): LocaleInterface
+    private function getDefaultCatalogLocale(): ?LocaleInterface
     {
-        return $this->localeRepository->getActivatedLocales()[0];
+        $locales = $this->localeRepository->getActivatedLocales();
+        if (count($locales) === 0) {
+            return null;
+        }
+
+        return $locales[0];
     }
 
-    private function getDefaultUiLocale(): LocaleInterface
+    private function getDefaultUiLocale(): ?LocaleInterface
     {
         return $this->localeRepository->findOneBy(['code' => UiLocaleProvider::MAIN_LOCALE]);
     }
 
-    private function getDefaultCatalogScope(): ChannelInterface
+    private function getDefaultCatalogScope(): ?ChannelInterface
     {
         return $this->channelRepository->findOneBy([]);
     }
 
-    private function getDefaultCategoryTree(): CategoryInterface
+    private function getDefaultCategoryTree(): ?CategoryInterface
     {
-        return $this->categoryRepository->getTrees()[0];
+        $trees = $this->categoryRepository->getTrees();
+        if (count($trees) === 0) {
+            return null;
+        }
+
+        return $trees[0];
     }
 
-    private function getDefaultGroup()
+    private function getDefaultGroup(): ?Group
     {
         return $this->groupRepository->findOneByIdentifier('all');
     }
