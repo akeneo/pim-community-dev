58c7c58ef7ba331ab8918cc99c31644405a24516
Willy <willy.mesnage@akeneo.com>
2018-08-01 17:09:54 +0200

    SuggestData: move js to ts. Fix indentation.

--- src/Akeneo/Pim/Automation/SuggestData/Bundle/Resources/public/js/controllers/attributes_mapping/index.ts
+++ src/Akeneo/Pim/Automation/SuggestData/Bundle/Resources/public/js/controllers/attributes_mapping/index.ts
@@ -5,7 +5,7 @@ const Router = require('pim/router');
 const Routing = require('routing');
 
 interface Families {
-    [index: number]: { code: string; enabled: boolean; labels: Object };
+  [index: number]: { code: string; enabled: boolean; labels: Object };
 }
 
 /**
@@ -15,16 +15,16 @@ interface Families {
  * @author Pierre Allard <pierre.allard@akeneo.com>
  */
 class IndexAttributeMappingController extends BaseController {
-    public renderForm(): Object {
-        return $.getJSON(Routing.generate('akeneo_sugggest_data_family_mapping_index', {limit: 1}))
-            .then((data: Families) => {
-                const firstFamily = data[0];
+  public renderForm(): Object {
+    return $.getJSON(Routing.generate('akeneo_sugggest_data_family_mapping_index', {limit: 1}))
+      .then((data: Families) => {
+        const firstFamily = data[0];
 
-                Router.redirectToRoute('akeneo_suggest_data_family_mapping_edit', {
-                    identifier: firstFamily.code
-                });
-            })
-    }
+        Router.redirectToRoute('akeneo_suggest_data_family_mapping_edit', {
+          identifier: firstFamily.code
+        });
+      })
+  }
 }
 
 export = IndexAttributeMappingController
