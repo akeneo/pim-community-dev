3b9deb5aeaa9646614c669ea2b7642f5673b6b28
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-08-07 22:27:44 +0200

    TIP-843: Fix spec

--- spec/Akeneo/Pim/Permission/Component/Connector/ArrayConverter/FlatToStandard/ProductCategoryAccessesSpec.php
+++ spec/Akeneo/Pim/Permission/Component/Connector/ArrayConverter/FlatToStandard/ProductCategoryAccessesSpec.php
@@ -15,7 +15,7 @@ class ProductCategoryAccessesSpec extends ObjectBehavior
     function it_is_a_standard_array_converter()
     {
         $this->shouldImplement(
-            'Pim\Component\Connector\ArrayConverter\ArrayConverterInterface'
+            'Akeneo\Tool\Component\Connector\ArrayConverter\ArrayConverterInterface'
         );
     }
 
