ce16f54f918267a75bdc72bf32c9ab0489d99bdb
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-08-07 22:59:33 +0200

    TIP-875: Fix spec

--- src/Akeneo/Tool/Bundle/ConnectorBundle/spec/EventListener/JobExecutionArchivistSpec.php
+++ src/Akeneo/Tool/Bundle/ConnectorBundle/spec/EventListener/JobExecutionArchivistSpec.php
@@ -12,7 +12,7 @@ class JobExecutionArchivistSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Pim\Bundle\ConnectorBundle\EventListener\JobExecutionArchivist');
+        $this->shouldHaveType('Akeneo\Tool\Bundle\ConnectorBundle\EventListener\JobExecutionArchivist');
     }
 
     function it_is_an_event_subscriber()
