3e055bb1e8b70ff9e3effe6cfefe94b1830743e2
Samir Boulil <samir.boulil@akeneo.com>
2018-08-07 17:11:32 +0200

    PIM-7509: Calculate completeness of variant products on rule execution
    **Context:**
    
    In the PIM, variant products are directly updated on unitary save of
    their parent.
    Meaning if you use the UI to update a product model all his children
    will be udpated (to calculate the completeness, and to reindex).
    
    This is not the case on bulk save (due to performance mainly). So for
    imports of product models, the children of the updated product models
    are updated in a separate step, and only on this step.
    
    **Problem:**
    
    There is no concept of "step" whatsoever in the context of rules
    execution. This means that until today, the children weren't updated at
    all if one of their parent was updated by a rule.
    
    **Proposed Solution:**
    
    As the rules are already executed in an asynchronous context, we can
    directly update the descendants of all the product models
    impacted/updated by a rule right after the save of the product models.
    
    The logic is the same as what's done in the 'Pim\Component\Catalog\Job\ComputeProductModelsDescendantsTasklet'
    with the use of the productModelDescendantsSaver.

--- src/PimEnterprise/Component/CatalogRule/Engine/ProductRuleApplier/ProductsSaver.php
+++ src/PimEnterprise/Component/CatalogRule/Engine/ProductRuleApplier/ProductsSaver.php
@@ -14,11 +14,14 @@ declare(strict_types=1);
 namespace PimEnterprise\Component\CatalogRule\Engine\ProductRuleApplier;
 
 use Akeneo\Bundle\RuleEngineBundle\Model\RuleInterface;
+use Akeneo\Component\StorageUtils\Cache\EntityManagerClearerInterface;
 use Akeneo\Component\StorageUtils\Saver\BulkSaverInterface;
+use Akeneo\Component\StorageUtils\Saver\SaverInterface;
 use Pim\Bundle\VersioningBundle\Manager\VersionContext;
 use Pim\Bundle\VersioningBundle\Manager\VersionManager;
 use Pim\Component\Catalog\Model\EntityWithValuesInterface;
 use Pim\Component\Catalog\Model\ProductInterface;
+use Pim\Component\Catalog\Model\ProductModel;
 use Pim\Component\Catalog\Model\ProductModelInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -44,25 +47,37 @@ class ProductsSaver
     /** @var VersionContext */
     protected $versionContext;
 
+    /** @var SaverInterface|null */
+    private $productModelDescendantsSaver;
+
+    /** @var EntityManagerClearerInterface|null */
+    private $cacheClearer;
+
     /**
-     * @param BulkSaverInterface  $productSaver
-     * @param BulkSaverInterface  $productModelSaver
-     * @param VersionManager      $versionManager
-     * @param VersionContext      $versionContext
-     * @param TranslatorInterface $translator
+     * @param BulkSaverInterface                 $productSaver
+     * @param BulkSaverInterface                 $productModelSaver
+     * @param VersionManager                     $versionManager
+     * @param VersionContext                     $versionContext
+     * @param TranslatorInterface                $translator
+     * @param SaverInterface|null                $productModelDescendantsSaver
+     * @param EntityManagerClearerInterface|null $cacheClearer
      */
     public function __construct(
         BulkSaverInterface $productSaver,
         BulkSaverInterface $productModelSaver,
         VersionManager $versionManager,
         VersionContext $versionContext,
-        TranslatorInterface $translator
+        TranslatorInterface $translator,
+        SaverInterface $productModelDescendantsSaver = null, // TODO: @merge - remove '=null'
+        EntityManagerClearerInterface $cacheClearer = null // TODO: @merge - remove '=null'
     ) {
         $this->productSaver = $productSaver;
         $this->productModelSaver = $productModelSaver;
         $this->versionManager = $versionManager;
         $this->translator = $translator;
         $this->versionContext = $versionContext;
+        $this->productModelDescendantsSaver = $productModelDescendantsSaver;
+        $this->cacheClearer = $cacheClearer;
     }
 
     /**
@@ -90,8 +105,24 @@ class ProductsSaver
 
         $this->productSaver->saveAll($products);
         $this->productModelSaver->saveAll($productModels);
+        $this->computeProductModelDescendants($productModels);
 
         $this->versionManager->setRealTimeVersioning($versioningState);
         $this->versionContext->unsetContextInfo('default');
+    }
+
+    /**
+     * Explicitly updates the product models descendants of a product.
+     *
+     * @param ProductModel[] $productModels
+     */
+    private function computeProductModelDescendants(array $productModels): void
+    {
+        foreach ($productModels as $productModel) {
+            if (null !== $this->productModelDescendantsSaver && null !== $this->cacheClearer) {
+                $this->productModelDescendantsSaver->save($productModel);
+                $this->cacheClearer->clear();
+            }
+        }
     }
 }
