d5b2e2d9c842a08314e03f05875d662e42e99fae
pierallard <pierallard@gmail.com>
2018-08-09 13:48:25 +0200

    [PIM-7570] Fix comments

--- src/Pim/Bundle/VersioningBundle/UpdateGuesser/LocaleSpecificAttributeUpdateGuesser.php
+++ src/Pim/Bundle/VersioningBundle/UpdateGuesser/LocaleSpecificAttributeUpdateGuesser.php
@@ -14,7 +14,7 @@ use Pim\Component\Catalog\Model\AttributeInterface;
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AttributeLocaleUpdateGuesser implements UpdateGuesserInterface
+class LocaleSpecificAttributeUpdateGuesser implements UpdateGuesserInterface
 {
     /**
      * {@inheritdoc}
--- src/Pim/Bundle/VersioningBundle/spec/UpdateGuesser/LocaleSpecificAttributeUpdateGuesserSpec.php
+++ src/Pim/Bundle/VersioningBundle/spec/UpdateGuesser/LocaleSpecificAttributeUpdateGuesserSpec.php
@@ -9,11 +9,11 @@ use PhpSpec\ObjectBehavior;
 use Pim\Bundle\CatalogBundle\Entity\Attribute;
 use Pim\Bundle\VersioningBundle\UpdateGuesser\UpdateGuesserInterface;
 
-class AttributeLocaleUpdateGuesserSpec extends ObjectBehavior
+class LocaleSpecificAttributeUpdateGuesserSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Pim\Bundle\VersioningBundle\UpdateGuesser\AttributeLocaleUpdateGuesser');
+        $this->shouldHaveType('Pim\Bundle\VersioningBundle\UpdateGuesser\LocaleSpecificAttributeUpdateGuesser');
     }
 
     function it_is_an_update_guesser()
