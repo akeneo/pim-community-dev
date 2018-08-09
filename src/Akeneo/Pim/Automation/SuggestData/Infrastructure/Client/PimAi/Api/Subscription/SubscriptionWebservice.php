f8d81aa05d5701951c191a1a7a91129bea389128
Julian Prud'homme <julian.prudhomme@akeneo.com>
2018-08-09 09:57:52 +0200

    pim.ai client now throws specific exceptions to abstract the guzzle ones

--- src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Client/PimAi/Api/Subscription/SubscriptionWebservice.php
+++ src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Client/PimAi/Api/Subscription/SubscriptionWebservice.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\PimAi\Api\Subscription;
 
+use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\Exceptions\BadRequestException;
+use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\Exceptions\DataProviderServerException;
+use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\Exceptions\InsufficientCreditsException;
+use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\Exceptions\InvalidTokenException;
 use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\PimAi\Api\ApiResponse;
 use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\PimAi\Client;
 use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\PimAi\UriGenerator;
 use Akeneo\Pim\Automation\SuggestData\Infrastructure\Client\PimAi\ValueObject\SubscriptionCollection;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Symfony\Component\HttpFoundation\Response;
 
 class SubscriptionWebservice implements SubscriptionApiInterface
 {
@@ -28,13 +35,26 @@ class SubscriptionWebservice implements SubscriptionApiInterface
     {
         $route = $this->uriGenerator->generate('/subscriptions');
 
-        $response = $this->httpClient->request('POST', $route, [
-            'form_params' => [$identifiers],
-        ]);
+        try {
+            $response = $this->httpClient->request('POST', $route, [
+                'form_params' => [$identifiers],
+            ]);
 
-        return new ApiResponse(
-            $response->getStatusCode(),
-            new SubscriptionCollection(json_decode($response->getBody()->getContents(), true))
-        );
+            return new ApiResponse(
+                $response->getStatusCode(),
+                new SubscriptionCollection(json_decode($response->getBody()->getContents(), true))
+            );
+        } catch (ServerException $e) {
+            throw new DataProviderServerException(sprintf('Something went wrong on pim.ai side during product subscription : ', $e->getMessage()));
+        } catch (ClientException $e) {
+            if ($e->getCode() === Response::HTTP_PAYMENT_REQUIRED) {
+                throw new InsufficientCreditsException('Not enough credits on pim.ai to subscribe');
+            }
+            if ($e->getCode() === Response::HTTP_FORBIDDEN) {
+                throw new InvalidTokenException('The pim.ai token is missing or invalid');
+            }
+
+            throw new BadRequestException(sprintf('Something went wrong during product subscription : ', $e->getMessage()));
+        }
     }
 }
