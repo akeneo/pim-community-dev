52ac6f79ae0778623db2fe38c385e2610fad2f4c
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-08-09 19:41:05 +0200

    TIP-875: Fix CI failure

--- src/Akeneo/Pim/Enrichment/Bundle/tests/Integration/Completeness/CompletenessForScopableAndLocalisableAttributeIntegration.php
+++ src/Akeneo/Pim/Enrichment/Bundle/tests/Integration/Completeness/CompletenessForScopableAndLocalisableAttributeIntegration.php
@@ -2,7 +2,7 @@
 
 namespace Pim\Bundle\CatalogBundle\tests\integration\Completeness;
 
-use Akeneo\Test\Integration\Configuration;
+use Akeneo\Pim\Enrichment\Bundle\tests\Integration\Completeness\AbstractCompletenessTestCase;
 use Akeneo\Pim\Enrichment\Component\Product\Model\CompletenessInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
 
