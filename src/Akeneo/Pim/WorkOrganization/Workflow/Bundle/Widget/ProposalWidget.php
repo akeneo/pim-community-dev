7762e683b2f1e24430da734d12f8877fd793e1e8
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-08-13 17:48:53 +0200

    TIP-877: Move dashboard bundle spec to platform

--- src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Widget/ProposalWidget.php
+++ src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Widget/ProposalWidget.php
@@ -16,7 +16,7 @@ use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\ProductDraft;
 use Akeneo\Pim\WorkOrganization\Workflow\Component\Repository\EntityWithValuesDraftRepositoryInterface;
 use Akeneo\Tool\Component\Localization\Presenter\PresenterInterface;
 use Akeneo\UserManagement\Bundle\Manager\UserManager;
-use Pim\Bundle\DashboardBundle\Widget\WidgetInterface;
+use Akeneo\Platform\Bundle\DashboardBundle\Widget\WidgetInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
