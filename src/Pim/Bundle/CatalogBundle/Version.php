1f11fea3bca89d9ef8bc78640d9ddf0aeaaa90e1
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2018-08-16 16:07:02 +0200

    Preparing version 2.0.33

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.32';
+    const VERSION = '2.0.33';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
