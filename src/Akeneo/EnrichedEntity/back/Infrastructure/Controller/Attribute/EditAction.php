2046bd345586341aec4d007cd9dc1c2a390dbd40
Samir Boulil <samir.boulil@akeneo.com>
2018-08-20 19:08:49 +0200

    PIM-7550: Infra - add controller + tests

--- src/Akeneo/EnrichedEntity/back/Infrastructure/Controller/Attribute/EditAction.php
+++ src/Akeneo/EnrichedEntity/back/Infrastructure/Controller/Attribute/EditAction.php
@@ -4,14 +4,89 @@ declare(strict_types=1);
 
 namespace Akeneo\EnrichedEntity\Infrastructure\Controller\Attribute;
 
+use Akeneo\EnrichedEntity\Application\Attribute\EditAttribute\CommandFactory\AbstractEditAttributeCommand;
+use Akeneo\EnrichedEntity\Application\Attribute\EditAttribute\CommandFactory\EditAttributeCommandFactoryInterface;
+use Akeneo\EnrichedEntity\Application\Attribute\EditAttribute\CommandFactory\EditAttributeCommandFactoryRegistryInterface;
+use Akeneo\EnrichedEntity\Application\Attribute\EditAttribute\EditAttributeHandler;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 /**
- * {description}
- *
  * @author    Samir Boulil <samir.boulil@akeneo.com>
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class EditAction
 {
+    /** @var EditAttributeCommandFactoryRegistryInterface */
+    private $editAttributeCommandFactory;
 
+    /** @var EditAttributeHandler */
+    private $editAttributeHandler;
+
+    /** @var NormalizerInterface */
+    private $normalizer;
+
+    /** @var ValidatorInterface  */
+    private $validator;
+
+    public function __construct(
+        EditAttributeCommandFactoryInterface $editAttributeCommandFactory,
+        EditAttributeHandler $editAttributeHandler,
+        NormalizerInterface $normalizer,
+        ValidatorInterface $validator
+    ) {
+        $this->editAttributeCommandFactory = $editAttributeCommandFactory;
+        $this->editAttributeHandler = $editAttributeHandler;
+        $this->normalizer = $normalizer;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return new RedirectResponse('/');
+        }
+        if ($this->hasDesynchronizedIdentifier($request)) {
+            return new JsonResponse(
+                'The identifier provided in the route and the one given in the body of the request are different',
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $command = $this->getEditCommand($request);
+        $violations = $this->validator->validate($command);
+
+        if ($violations->count() > 0) {
+            return new JsonResponse(
+                $this->normalizer->normalize($violations, 'internal_api'),
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        ($this->editAttributeHandler)($command);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Checks whether the identifier given in the url parameter and in the body are the same.
+     */
+    private function hasDesynchronizedIdentifier(Request $request): bool
+    {
+        $normalizedCommand = json_decode($request->getContent(), true);
+
+        return $normalizedCommand['identifier'] !== $request->get('identifier');
+    }
+
+    private function getEditCommand(Request $request): AbstractEditAttributeCommand
+    {
+        $normalizedCommand = json_decode($request->getContent(), true);
+        $command = $this->editAttributeCommandFactory->create($normalizedCommand);
+
+        return $command;
+    }
 }
--- src/Akeneo/EnrichedEntity/tests/back/Integration/UI/Web/Attribute/EditActionTest.php
+++ src/Akeneo/EnrichedEntity/tests/back/Integration/UI/Web/Attribute/EditActionTest.php
@@ -4,14 +4,137 @@ declare(strict_types=1);
 
 namespace Akeneo\EnrichedEntity\tests\back\Integration\UI\Web\Attribute;
 
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeAllowedExtensions;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeCode;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeIdentifier;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeMaxFileSize;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeMaxLength;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeOrder;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeRequired;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeValuePerChannel;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\AttributeValuePerLocale;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\ImageAttribute;
+use Akeneo\EnrichedEntity\Domain\Model\Attribute\TextAttribute;
+use Akeneo\EnrichedEntity\Domain\Model\EnrichedEntity\EnrichedEntity;
+use Akeneo\EnrichedEntity\Domain\Model\EnrichedEntity\EnrichedEntityIdentifier;
+use Akeneo\EnrichedEntity\Domain\Model\LabelCollection;
+use Akeneo\EnrichedEntity\Domain\Repository\AttributeRepositoryInterface;
+use Akeneo\EnrichedEntity\tests\back\Common\Helper\AuthenticatedClientFactory;
+use Akeneo\EnrichedEntity\tests\back\Common\Helper\WebClientHelper;
+use Akeneo\EnrichedEntity\tests\back\Integration\ControllerIntegrationTestCase;
+use PHPUnit\Framework\Assert;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
- * {description}
- *
  * @author    Samir Boulil <samir.boulil@akeneo.com>
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class EditActionTest
+class EditActionTest extends ControllerIntegrationTestCase
 {
+    private const EDIT_ATTRIBUTE_ROUTE = 'akeneo_enriched_entities_attribute_edit_rest';
 
+    /** @var Client */
+    private $client;
+
+    /** @var WebClientHelper */
+    private $webClientHelper;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadFixtures();
+        $this->client = (new AuthenticatedClientFactory($this->get('pim_user.repository.user'), $this->testKernel))
+            ->logIn('julia');
+        $this->webClientHelper = $this->get('akeneoenriched_entity.tests.helper.web_client_helper');
+    }
+
+    /**
+     * @test
+     */
+    public function it_edits_an_attribute_labels()
+    {
+        $updateLabel = [
+            'identifier'                 => [
+                'enriched_entity_identifier' => 'designer',
+                'identifier'                 => 'name',
+            ],
+            'enriched_entity_identifier' => 'designer',
+            'code'                       => 'name',
+            'labels'                     => ['fr_FR' => 'LABEL UPDATED', 'en_US' => 'Name'],
+            'order'                      => 0,
+            'required'                   => false,
+            'value_per_channel'          => true,
+            'value_per_locale'           => true,
+            'type'                       => 'text',
+            'max_length'                 => 300,
+        ];
+
+        $this->webClientHelper->callRoute(
+            $this->client,
+            self::EDIT_ATTRIBUTE_ROUTE,
+            ['identifier' => 'designer'],
+            'POST',
+            [
+                'HTTP_X-Requested-With' => 'XMLHttpRequest',
+                'CONTENT_TYPE'          => 'application/json',
+            ],
+            $updateLabel
+        );
+
+        $this->webClientHelper->assertResponse($this->client->getResponse(), Response::HTTP_NO_CONTENT);
+
+        $repository = $this->getAttributeRepository();
+        $entityItem = $repository->getByIdentifier(AttributeIdentifier::create(
+            $updateLabel['identifier']['enriched_entity_identifier'],
+            $updateLabel['identifier']['identifier']
+        ));
+
+        Assert::assertEquals(array_keys($updateLabel['labels']), $entityItem->getLabelCodes());
+        Assert::assertEquals($updateLabel['labels']['en_US'], $entityItem->getLabel('en_US'));
+        Assert::assertEquals($updateLabel['labels']['fr_FR'], $entityItem->getLabel('fr_FR'));
+    }
+
+    private function loadFixtures(): void
+    {
+        $securityFacadeStub = $this->get('oro_security.security_facade');
+        $securityFacadeStub->setIsGranted('akeneo_enrichedentity_attribute_edit', true);
+
+        $enrichedEntityRepository = $this->get('akeneo_enrichedentity.infrastructure.persistence.enriched_entity');
+        $enrichedEntityRepository->create(EnrichedEntity::create(EnrichedEntityIdentifier::fromString('designer'), []));
+        $enrichedEntityRepository->create(EnrichedEntity::create(EnrichedEntityIdentifier::fromString('brand'), []));
+
+        $attributeRepository = $this->get('akeneo_enrichedentity.infrastructure.persistence.attribute');
+        $name = TextAttribute::create(
+            AttributeIdentifier::create('designer', 'name'),
+            EnrichedEntityIdentifier::fromString('designer'),
+            AttributeCode::fromString('name'),
+            LabelCollection::fromArray(['fr_FR' => 'Nom', 'en_US' => 'Name']),
+            AttributeOrder::fromInteger(0),
+            AttributeRequired::fromBoolean(true),
+            AttributeValuePerChannel::fromBoolean(false),
+            AttributeValuePerLocale::fromBoolean(true),
+            AttributeMaxLength::fromInteger(200)
+        );
+        $portrait = ImageAttribute::create(
+            AttributeIdentifier::create('designer', 'portrait'),
+            EnrichedEntityIdentifier::fromString('designer'),
+            AttributeCode::fromString('portrait'),
+            LabelCollection::fromArray(['fr_FR' => 'Image autobiographique', 'en_US' => 'Portrait']),
+            AttributeOrder::fromInteger(1),
+            AttributeRequired::fromBoolean(true),
+            AttributeValuePerChannel::fromBoolean(false),
+            AttributeValuePerLocale::fromBoolean(false),
+            AttributeMaxFileSize::fromString('200.10'),
+            AttributeAllowedExtensions::fromList(['png'])
+        );
+        $attributeRepository->create($name);
+        $attributeRepository->create($portrait);
+    }
+
+    private function getAttributeRepository(): AttributeRepositoryInterface
+    {
+        return $this->get('akeneo_enrichedentity.infrastructure.persistence.attribute');
+    }
 }
