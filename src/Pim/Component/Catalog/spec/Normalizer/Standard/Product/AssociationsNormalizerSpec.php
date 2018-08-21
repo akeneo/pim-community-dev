3398e1d20be22155fcd142a216da31e8b36b985a
Julian Prud'homme <julian.prudhomme@akeneo.com>
2018-08-21 14:11:58 +0200

    complete association normalizer specs

--- src/Pim/Component/Catalog/spec/Normalizer/Standard/Product/AssociationsNormalizerSpec.php
+++ src/Pim/Component/Catalog/spec/Normalizer/Standard/Product/AssociationsNormalizerSpec.php
@@ -85,68 +85,49 @@ class AssociationsNormalizerSpec extends ObjectBehavior
     function it_normalizes_a_product_associations_with_query_to_find_associated_products_codes(
         GetAssociatedProductCodesByProduct $getAssociatedProductCodesByProduct,
         ProductInterface $product,
+        ProductModelInterface $productModel,
         AssociationInterface $association1,
         AssociationInterface $association2,
         AssociationTypeInterface $associationType1,
         AssociationTypeInterface $associationType2,
         GroupInterface $group1,
-        ProductInterface $productAssociated,
-        \ArrayIterator $association1Iterator,
-        \ArrayIterator $association2Iterator
+        ProductInterface $productAssociated
     ) {
         $this->beConstructedWith($getAssociatedProductCodesByProduct);
-        $product->getId()->willReturn(1);
 
         $group1->getCode()->willReturn('group_code');
         $associationType1->getCode()->willReturn('XSELL');
-        $associationType1->getId()->willReturn(5);
         $association1->getAssociationType()->willReturn($associationType1);
-        $association1->getGroups()->willReturn($association1Iterator);
+        $association1->getGroups()->willReturn([$group1]);
         $association1->getProducts()->willReturn(new ArrayCollection());
-        $getAssociatedProductCodesByProduct->getCodes(1, 5)->willReturn([]);
+        $association1->getProductModels()->willReturn(new ArrayCollection());
+        $getAssociatedProductCodesByProduct->getCodes($association1)->willReturn([]);
 
-        $association1Iterator->rewind()->willReturn($group1);
-        $valueCount = 1;
-        $association1Iterator->valid()->will(
-            function () use (&$valueCount) {
-                return $valueCount-- > 0;
-            }
-        );
-        $association1Iterator->current()->willReturn($group1);
-        $association1Iterator->next()->willReturn(null);
-        $association1Iterator->count()->willReturn(1);
 
         $productAssociated->getReference()->willReturn('product_code');
+        $productModel->getCode()->willReturn('product_model_code');
         $associationType2->getCode()->willReturn('PACK');
         $associationType2->getId()->willReturn(7);
         $association2->getAssociationType()->willReturn($associationType2);
         $association2->getGroups()->willReturn(new ArrayCollection());
-        $association2->getProducts()->willReturn($association2Iterator);
+        $association2->getProducts()->willReturn([$productAssociated->getWrappedObject()]);
+        $association2->getProductModels()->willReturn(new ArrayCollection([$productModel->getWrappedObject()]));
 
-        $getAssociatedProductCodesByProduct->getCodes(1, 7)->willReturn(['product_code']);
+        $getAssociatedProductCodesByProduct->getCodes($association2)->willReturn(['product_code']);
 
-        $association2Iterator->rewind()->willReturn($productAssociated);
-        $valueCount2 = 1;
-        $association2Iterator->valid()->will(
-            function () use (&$valueCount2) {
-                return $valueCount2-- > 0;
-            }
-        );
-        $association2Iterator->current()->willReturn($productAssociated);
-        $association2Iterator->next()->willReturn(null);
-        $association2Iterator->count()->willReturn(1);
-
-        $product->getAssociations()->willReturn([$association1, $association2]);
+        $product->getAllAssociations()->willReturn([$association1, $association2]);
 
         $this->normalize($product, 'standard')->shouldReturn(
             [
                 'PACK' => [
                     'groups' => [],
-                    'products' => ['product_code']
+                    'products' => ['product_code'],
+                    'product_models' => ['product_model_code'],
                 ],
                 'XSELL' => [
                     'groups' => ['group_code'],
-                    'products' => []
+                    'products' => [],
+                    'product_models' => [],
                 ]
             ]
         );
