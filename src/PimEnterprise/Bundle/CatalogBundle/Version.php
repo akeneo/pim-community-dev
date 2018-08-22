b185f65ab41fac34e7f92d5bdbb1fa9ccd0cea1d
Julian Prud'homme <julian.prudhomme@akeneo.com>
2018-08-22 15:00:43 +0200

    Preparing version 1.7.31

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.30';
+    const VERSION = '1.7.31';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Artichoke';
