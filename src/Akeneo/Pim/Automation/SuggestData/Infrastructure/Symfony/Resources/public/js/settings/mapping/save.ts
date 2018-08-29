bad688649a547dff72f84801df90445b3b89f2ce
Damien Carcel <damien.carcel@akeneo.com>
2018-08-29 17:39:32 +0200

    APAI-19: Use permanent message for identifiers mapping first save

--- src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/Resources/public/js/settings/mapping/save.ts
+++ src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/Resources/public/js/settings/mapping/save.ts
@@ -1,6 +1,11 @@
 const __ = require('oro/translator');
+const fetcherRegistry = require('pim/fetcher-registry');
 const BaseSave = require('pim/form/common/save');
 const MappingSaver = require('pimee/saver/identifier-mapping');
+
+interface MappingInterface {
+  [key: string]: (string | null);
+}
 
 /**
  * Save extension identifiers mapping
@@ -8,13 +13,33 @@ const MappingSaver = require('pimee/saver/identifier-mapping');
  * @author Willy MESNAGE <willy.mesnage@akeneo.com>
  */
 class MappingSave extends BaseSave {
-  readonly updateSuccessMessage = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.success');
-  readonly updateFailureMessage = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.fail');
+  readonly updateFailureMessage: string = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.fail');
+  protected updateSuccessMessage: string = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.success');
+  protected isFlash: boolean = true;
+
+  /**
+   * {@inheritdoc}
+   *
+   * Check if there is already an existing mapping. The success message will be set accordingly.
+   */
+  public configure() {
+    return $.when(
+      fetcherRegistry.getFetcher('identifiers-mapping').fetchAll().then(
+        (identifiersMapping: MappingInterface) => {
+          if (this.isMappingEmpty(identifiersMapping)) {
+            this.updateSuccessMessage = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.first');
+            this.isFlash = false;
+          }
+        }
+      ),
+      BaseSave.prototype.configure.apply(this, arguments)
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
-  save() {
+  public save(): void {
     let identifiersMapping = $.extend(true, {}, this.getFormData());
     this.showLoadingMask();
     this.getRoot().trigger('pim_enrich:form:entity:pre_save');
@@ -25,17 +50,38 @@ class MappingSave extends BaseSave {
       .then((savedMapping: string) => {
         this.postSave();
         this.setData(JSON.parse(savedMapping));
+        this.updateSuccessMessage = __('akeneo_suggest_data.entity.identifier_mapping.flash.update.success');
+        this.isFlash = true;
       })
       .fail(this.fail.bind(this))
       .always(this.hideLoadingMask.bind(this));
   }
 
   /**
+   * Checks if at least one Franklin identifier is mapped to an Akeneo attribute.
+   *
+   * @param {MappingInterface} identifiersMapping
+   *
+   * @return {boolean}
+   */
+  private isMappingEmpty(identifiersMapping: MappingInterface): boolean {
+    const mappedAttributes = Object.keys(identifiersMapping).filter(
+      (franklinIdentifier: string) => null !== identifiersMapping[franklinIdentifier]
+    );
+
+    return $.isEmptyObject(mappedAttributes);
+  }
+
+  /**
    * When you clear data in select2 choice it puts an empty string instead of null.
    * This function put null instead of empty string in mapping values.
+   *
+   * @param {Object} identifiersMapping
+   *
+   * @return {MappingInterface}
    */
-  private cleanMapping(identifiersMapping: { [key: string]: string }): { [key: string]: (string | null) } {
-    return Object.keys(identifiersMapping).reduce((accumulator: { [key: string]: (string | null) }, index: string) => {
+  private cleanMapping(identifiersMapping: { [key: string]: string }): MappingInterface {
+    return Object.keys(identifiersMapping).reduce((accumulator: MappingInterface, index: string) => {
       accumulator[index] = '' !== identifiersMapping[index] ? identifiersMapping[index] : null;
 
       return accumulator;
