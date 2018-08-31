bf6e0540f1d8fd33131e4fc219f732a65404e11a
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-08-31 14:47:17 +0200

    TIP-882: move normalizer

--- src/Pim/Bundle/ReferenceDataBundle/Normalizer/ReferenceDataConfigurationNormalizer.php
+++ src/Pim/Bundle/ReferenceDataBundle/Normalizer/ReferenceDataConfigurationNormalizer.php
@@ -12,7 +12,7 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ConfigurationNormalizer implements NormalizerInterface
+class ReferenceDataConfigurationNormalizer implements NormalizerInterface
 {
     /** @var string[] */
     protected $supportedFormats = ['internal_api'];
