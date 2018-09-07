86d15796babf9e4ec865a2b258082ca3a8dd8901
pierallard <pierallard@gmail.com>
2018-09-07 11:09:08 +0200

    [APAI-203] Add empty message

--- src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/Resources/public/js/settings/mapping/attributes-mapping.ts
+++ src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/Resources/public/js/settings/mapping/attributes-mapping.ts
@@ -3,6 +3,7 @@ import BaseForm = require('pimenrich/js/view/base');
 const __ = require('oro/translator');
 const SimpleSelectAttribute = require('pimee/settings/mapping/simple-select-attribute');
 const template = require('pimee/template/settings/mapping/attributes-mapping');
+const noDataTemplate = require('pim/template/common/no-data');
 
 /**
  * This module will allow user to map the attributes from PIM.ai to the catalog attributes.
@@ -49,6 +50,7 @@ class InterfaceNormalizedAttributeMapping {
 
 class AttributeMapping extends BaseForm {
   readonly template = _.template(template);
+  readonly noDataTemplate = _.template(noDataTemplate);
   readonly config: Config = {
     labels: {
       pending: '',
@@ -84,45 +86,52 @@ class AttributeMapping extends BaseForm {
   public render(): BaseForm {
     this.$el.html('');
     const familyMapping: InterfaceNormalizedAttributeMapping = this.getFormData();
-    if (familyMapping.hasOwnProperty('mapping') && Object.keys(familyMapping.mapping).length) {
-      const mapping = familyMapping.mapping;
-      const statuses: { [key: number]: string } = {};
-      statuses[ATTRIBUTE_PENDING] = __(this.config.labels.pending);
-      statuses[ATTRIBUTE_MAPPED] = __(this.config.labels.mapped);
-      statuses[ATTRIBUTE_UNMAPPED] = __(this.config.labels.unmapped);
-      this.$el.html(this.template({
-        mapping,
-        statuses,
-        pim_ai_attribute: __(this.config.labels.pim_ai_attribute),
-        catalog_attribute: __(this.config.labels.catalog_attribute),
-        suggest_data: __(this.config.labels.suggest_data)
-      }));
-      Object.keys(mapping).forEach((pim_ai_attribute_code) => {
-        const $dom = this.$el.find(
-          '.attribute-selector[data-pim-ai-attribute-code="' + pim_ai_attribute_code + '"]'
-        );
-        const attributeSelector = new SimpleSelectAttribute({
-          config: {
-            /**
-             * The normalized managed object looks like:
-             * { mapping: {
-             *     pim_ai_attribute_code_1: { attribute: 'foo' ... },
-             *     pim_ai_attribute_code_2: { attribute: 'bar' ... }
-             * } }
-             */
-            fieldName: 'mapping.' + pim_ai_attribute_code + '.attribute',
-            label: '',
-            choiceRoute: 'pim_enrich_attribute_rest_index',
-            types: VALID_MAPPING[mapping[pim_ai_attribute_code].pim_ai_attribute.type],
-          },
-          className: 'AknFieldContainer AknFieldContainer--withoutMargin AknFieldContainer--inline'
-        });
-        attributeSelector.configure().then(() => {
-          attributeSelector.setParent(this);
-          $dom.html(attributeSelector.render().$el);
-        });
+    const mapping = familyMapping.hasOwnProperty('mapping') ? familyMapping.mapping : {};
+    const statuses: { [key: number]: string } = {};
+    statuses[ATTRIBUTE_PENDING] = __(this.config.labels.pending);
+    statuses[ATTRIBUTE_MAPPED] = __(this.config.labels.mapped);
+    statuses[ATTRIBUTE_UNMAPPED] = __(this.config.labels.unmapped);
+    this.$el.html(this.template({
+      mapping,
+      statuses,
+      pim_ai_attribute: __(this.config.labels.pim_ai_attribute),
+      catalog_attribute: __(this.config.labels.catalog_attribute),
+      suggest_data: __(this.config.labels.suggest_data)
+    }) + this.noDataTemplate({
+      __,
+      imageClass: '',
+      hint: __('pim_datagrid.no_results', {
+        entityHint: __('akeneo_suggest_data.entity.attributes_mapping.fields.pim_ai_attribute')
+      }),
+      subHint: 'pim_datagrid.no_results_subtitle'
+    }));
+    Object.keys(mapping).forEach((pim_ai_attribute_code) => {
+      const $dom = this.$el.find(
+        '.attribute-selector[data-pim-ai-attribute-code="' + pim_ai_attribute_code + '"]'
+      );
+      const attributeSelector = new SimpleSelectAttribute({
+        config: {
+          /**
+           * The normalized managed object looks like:
+           * { mapping: {
+           *     pim_ai_attribute_code_1: { attribute: 'foo' ... },
+           *     pim_ai_attribute_code_2: { attribute: 'bar' ... }
+           * } }
+           */
+          fieldName: 'mapping.' + pim_ai_attribute_code + '.attribute',
+          label: '',
+          choiceRoute: 'pim_enrich_attribute_rest_index',
+          types: VALID_MAPPING[mapping[pim_ai_attribute_code].pim_ai_attribute.type],
+        },
+        className: 'AknFieldContainer AknFieldContainer--withoutMargin AknFieldContainer--inline'
       });
-    }
+      attributeSelector.configure().then(() => {
+        attributeSelector.setParent(this);
+        $dom.html(attributeSelector.render().$el);
+      });
+    });
+
+    this.toggleNoDataMessage();
 
     this.renderExtensions();
 
@@ -158,7 +167,16 @@ class AttributeMapping extends BaseForm {
       $(row).data('active-filters', filters);
 
       filters.length > 0 ? $(row).hide() : $(row).show();
+
+      this.toggleNoDataMessage();
     });
+  }
+
+  /**
+   * Toggle the "there is no data" message regarding the number of visible rows.
+   */
+  private toggleNoDataMessage() {
+    $('.searchable-row:visible').length ? this.$el.find('.no-data').hide() : this.$el.find('.no-data').show();
   }
 
   /**
