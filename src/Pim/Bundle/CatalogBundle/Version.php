b065c22169c12b63e4b01fe12886b21965350718
Elodie Raposo <raposo.elodie@gmail.com>
2018-09-07 11:28:22 +0200

    Preparing version 1.7.32

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.31';
+    const VERSION = '1.7.32';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
