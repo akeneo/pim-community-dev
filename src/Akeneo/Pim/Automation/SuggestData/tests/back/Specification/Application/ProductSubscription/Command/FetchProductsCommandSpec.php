4c369414d99fcbfac06999ea80c478f69c4c7ee7
romain <romain@akeneo.com>
2018-09-11 14:50:38 +0200

    Fix phpspec namespaces

--- src/Akeneo/Pim/Automation/SuggestData/tests/back/Specification/Application/ProductSubscription/Command/FetchProductsCommandSpec.php
+++ src/Akeneo/Pim/Automation/SuggestData/tests/back/Specification/Application/ProductSubscription/Command/FetchProductsCommandSpec.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Specification\Akeneo\Pim\Automation\SuggestData\Component\Command;
+namespace Specification\Akeneo\Pim\Automation\SuggestData\Application\ProductSubscription\Command;
 
 use Akeneo\Pim\Automation\SuggestData\Application\ProductSubscription\Command\FetchProductsCommand;
 use PhpSpec\ObjectBehavior;
