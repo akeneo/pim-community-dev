3bd7a4a07a85a64f1dcd5d5ceaf0fd5075b1c7f0
Elodie Raposo <raposo.elodie@gmail.com>
2018-09-13 15:09:14 +0200

    Preparing version 2.0.38

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.37';
+    const VERSION = '2.0.38';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
