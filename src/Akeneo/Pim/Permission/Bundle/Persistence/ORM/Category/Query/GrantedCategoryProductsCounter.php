a13eae95cb07a7156c739087137fb2dedc420c35
Arnaud Langlade <arnaud.langlade@akeneo.com>
2018-09-14 11:39:23 +0200

    TIP-860: Move repositories from enrich bundle to the right BC TIP-860: Move repositories/queries from enrich bundle to the right BC

--- src/Akeneo/Pim/Permission/Bundle/Persistence/ORM/Category/Query/GrantedCategoryProductsCounter.php
+++ src/Akeneo/Pim/Permission/Bundle/Persistence/ORM/Category/Query/GrantedCategoryProductsCounter.php
@@ -19,7 +19,7 @@ use Akeneo\Pim\Permission\Bundle\Entity\Repository\CategoryAccessRepository;
 use Akeneo\Pim\Permission\Component\Attributes;
 use Akeneo\Tool\Component\Classification\Model\CategoryInterface;
 use Akeneo\Tool\Component\Classification\Repository\CategoryRepositoryInterface;
-use Pim\Bundle\EnrichBundle\Doctrine\Counter\CategoryItemsCounterInterface;
+use Akeneo\Pim\Enrichment\Bundle\Doctrine\ORM\Counter\CategoryItemsCounterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
