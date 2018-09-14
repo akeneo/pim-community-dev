a7164d902dbf599c6d2d2abfb6710f4a2a799e60
christophe-chausseray <chausseray.christophe@gmail.com>
2018-09-14 15:03:17 +0200

    PIM-7435 : Fix DataHydrator definition

--- src/Akeneo/EnrichedEntity/back/Infrastructure/Persistence/Sql/Record/Hydrator/FileDataHydrator.php
+++ src/Akeneo/EnrichedEntity/back/Infrastructure/Persistence/Sql/Record/Hydrator/FileDataHydrator.php
@@ -29,7 +29,7 @@ class FileDataHydrator implements DataHydratorInterface
         return $attribute instanceof ImageAttribute;
     }
 
-    public function hydrate($normalizedData, AbstractAttribute $attribute): ValueDataInterface
+    public function hydrate($normalizedData): ValueDataInterface
     {
         return FileData::createFromNormalize($normalizedData);
     }
