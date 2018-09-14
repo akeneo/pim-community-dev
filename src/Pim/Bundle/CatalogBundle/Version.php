dc7463c992a66cb3a2bd701c8dc719b26c77d097
Elodie Raposo <raposo.elodie@gmail.com>
2018-09-14 10:14:07 +0200

    Preparing version 2.3.8

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.7';
+    const VERSION = '2.3.8';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
