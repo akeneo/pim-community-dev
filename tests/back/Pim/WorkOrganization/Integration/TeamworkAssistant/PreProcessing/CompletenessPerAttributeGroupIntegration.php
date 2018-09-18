9204e4578c776c2bed820b734a85159e88db6546
arnolanglade <arnaud.langlade@akeneo.com>
2018-09-18 10:57:53 +0200

    Twa critical use cases

--- tests/back/Pim/WorkOrganization/Integration/TeamworkAssistant/PreProcessing/CompletenessPerAttributeGroupIntegration.php
+++ tests/back/Pim/WorkOrganization/Integration/TeamworkAssistant/PreProcessing/CompletenessPerAttributeGroupIntegration.php
@@ -26,8 +26,6 @@ class CompletenessPerAttributeGroupIntegration extends TeamworkAssistantTestCase
      *     - ecommerce / fr_FR
      *     - tablet / en_US
      *     - tablet / fr_FR
-     *
-     * @critical
      */
     public function testProjectCalculationOnTshirtSkyrim()
     {
@@ -176,8 +174,6 @@ class CompletenessPerAttributeGroupIntegration extends TeamworkAssistantTestCase
      * 1 created project with the product 'empty-technical-product':
      *     - Channel: ecommerce
      *     - Locale: en_US
-     *
-     * @critical
      */
     public function testProjectCalculationWhenTheProductPropertiesAreEmpties()
     {
@@ -252,9 +248,6 @@ class CompletenessPerAttributeGroupIntegration extends TeamworkAssistantTestCase
         ]);
     }
 
-    /**
-     * @critical
-     */
     public function testTheAllPermissionCategory()
     {
         $category = $this->get('pim_catalog.repository.category')->findOneByIdentifier('car');
