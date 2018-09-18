f437441dd6071d6ed0144785d7b46725eec69c38
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-09-18 14:38:47 +0200

    TIP-873: remove overidden adapter

--- src/PimEnterprise/Bundle/DataGridBundle/DependencyInjection/PimEnterpriseDataGridExtension.php
+++ src/PimEnterprise/Bundle/DataGridBundle/DependencyInjection/PimEnterpriseDataGridExtension.php
@@ -29,7 +29,6 @@ class PimEnterpriseDataGridExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('adapters.yml');
         $loader->load('configurators.yml');
         $loader->load('data_sources.yml');
         $loader->load('extensions.yml');
