54cda50d270574496372f790ba3a08f16aba6e45
Mathias Métayer <mathias.metayer@akeneo.com>
2018-09-26 09:37:50 +0200

    APAI-157: Initialize mass subscription job profile

--- src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/DependencyInjection/AkeneoSuggestDataExtension.php
+++ src/Akeneo/Pim/Automation/SuggestData/Infrastructure/Symfony/DependencyInjection/AkeneoSuggestDataExtension.php
@@ -38,8 +38,10 @@ class AkeneoSuggestDataExtension extends Extension
         $loader->load('data_provider/pim_ai.yml');
         $loader->load('event_subscribers.yml');
         $loader->load('handlers.yml');
+        $loader->load('jobs.yml');
         $loader->load('normalizers.yml');
         $loader->load('repositories.yml');
         $loader->load('services.yml');
+        $loader->load('steps.yml');
     }
 }
