d56cb1642a9c3dcc8963487864c87c71aec8c491
Julien Sanchez <sumbobyboys@gmail.com>
2018-10-01 13:30:53 +0200

    Preparing version 2.3.10

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.9';
+    const VERSION = '2.3.10';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
