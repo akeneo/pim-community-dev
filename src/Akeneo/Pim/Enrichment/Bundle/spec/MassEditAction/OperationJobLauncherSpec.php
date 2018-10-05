02d6645ea830ac5eea52e3add11c20e350ae79a1
arnolanglade <arnaud.langlade@akeneo.com>
2018-10-05 11:39:14 +0200

    TIP-860: Move missing spec

--- src/Akeneo/Pim/Enrichment/Bundle/spec/MassEditAction/OperationJobLauncherSpec.php
+++ src/Akeneo/Pim/Enrichment/Bundle/spec/MassEditAction/OperationJobLauncherSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec\Pim\Bundle\EnrichBundle\MassEditAction;
+namespace spec\Akeneo\Pim\Enrichment\Bundle\MassEditAction;
 
 use Akeneo\Pim\Enrichment\Bundle\MassEditAction\Operation\BatchableOperationInterface;
 use Akeneo\Tool\Bundle\BatchBundle\Launcher\SimpleJobLauncher;
