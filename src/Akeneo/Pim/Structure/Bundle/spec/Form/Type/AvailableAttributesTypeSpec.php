d2d90381410bb9a324e17681c90d74da164e3b8a
arnolanglade <arnaud.langlade@akeneo.com>
2018-10-08 12:19:26 +0200

    TIP-860: Move available attribute model to structure

--- src/Akeneo/Pim/Structure/Bundle/spec/Form/Type/AvailableAttributesTypeSpec.php
+++ src/Akeneo/Pim/Structure/Bundle/spec/Form/Type/AvailableAttributesTypeSpec.php
@@ -3,10 +3,10 @@
 namespace spec\Akeneo\Pim\Structure\Bundle\Form\Type;
 
 use Akeneo\Pim\Structure\Component\Model\Attribute;
+use Akeneo\Pim\Structure\Component\Model\AvailableAttributes;
 use Akeneo\Platform\Bundle\UIBundle\Form\Type\LightEntityType;
 use Akeneo\Platform\Bundle\UIBundle\Provider\TranslatedLabelsProviderInterface;
 use PhpSpec\ObjectBehavior;
-use Pim\Component\Enrich\Model\AvailableAttributes;
 use Prophecy\Argument;
 use Symfony\Component\Form\Test\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
--- src/Pim/Bundle/EnrichBundle/DependencyInjection/PimEnrichExtension.php
+++ src/Pim/Bundle/EnrichBundle/DependencyInjection/PimEnrichExtension.php
@@ -39,7 +39,6 @@ class PimEnrichExtension extends Extension
         $loader->load('controllers.yml');
         $loader->load('converters.yml');
         $loader->load('datagrid_listeners.yml');
-        $loader->load('entities.yml');
         $loader->load('forms.yml');
         $loader->load('handlers.yml');
         $loader->load('job_constraints.yml');
