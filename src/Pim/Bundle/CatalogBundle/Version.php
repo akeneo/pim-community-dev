8a12c771e1b67ddcd8efbba5bc8d9e836698d8a7
Adrien Pétremann <hello@grena.fr>
2018-10-10 11:00:42 +0200

    Preparing version 1.7.33

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.32';
+    const VERSION = '1.7.33';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
