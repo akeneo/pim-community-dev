22f61845a18f4acad36cd3c3afec4df57580ca76
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-10-11 11:14:02 +0200

    TIP-860: fix rules in coupling detector

--- src/Akeneo/UserManagement/.php_cd.php
+++ src/Akeneo/UserManagement/.php_cd.php
@@ -27,6 +27,9 @@ $rules = [
         'Akeneo\Tool',
         'Akeneo\UserManagement',
         'Oro\Bundle\SecurityBundle',
+        'FOS\OAuthServerBundle\Entity\ClientManager', // used by API client controller
+        'OAuth2\OAuth2', // used by API client controller
+        'Akeneo\Platform\Bundle\UIBundle\ViewElement\Checker\NonEmptyPropertyVisibilityChecker', // TODO: we should not rely on the platform here
         'Akeneo\Channel\Component\Model\ChannelInterface', // TODO: The channel is linked by reference instead of id
         'Akeneo\Channel\Component\Model\Channel', // TODO: Use for entity form
         'Akeneo\Channel\Component\Model\LocaleInterface', // TODO: The locale is linked by reference instead of id
