4706d248057a00979c27650e374adfbff42b27a1
Samir Boulil <samir.boulil@akeneo.com>
2018-10-17 18:17:47 +0200

    PIM-7754: Add DI

--- src/Akeneo/ReferenceEntity/back/Infrastructure/Search/Elasticsearch/Record/Subscriber/IndexRecordSubscriber.php
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/Search/Elasticsearch/Record/Subscriber/IndexRecordSubscriber.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Akeneo\ReferenceEntity\Infrastructure\Search\Elasticsearch\Record\Subscriber;
 
+use Akeneo\ReferenceEntity\Infrastructure\Persistence\Sql\Attribute\Event\AttributeDeletedEvent;
 use Akeneo\ReferenceEntity\Infrastructure\Persistence\Sql\Record\Event\RecordUpdatedEvent;
+use Akeneo\ReferenceEntity\Infrastructure\Persistence\Sql\ReferenceEntity\SqlGetReferenceEntityIdentifierForAttribute;
 use Akeneo\ReferenceEntity\Infrastructure\Search\Elasticsearch\Record\RecordIndexerInterface;
+use Akeneo\ReferenceEntity\Infrastructure\Symfony\Command\IndexRecordsCommand;
+use Akeneo\Tool\Component\Console\CommandLauncher;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -17,9 +21,20 @@ class IndexRecordSubscriber implements EventSubscriberInterface
     /** @var RecordIndexerInterface */
     private $recordIndexer;
 
-    public function __construct(RecordIndexerInterface $recordIndexer)
-    {
+    /** @var CommandLauncher */
+    private $commandLauncher;
+
+    /** @var SqlGetReferenceEntityIdentifierForAttribute */
+    private $getReferenceEntityIdentifierForAttribute;
+
+    public function __construct(
+        RecordIndexerInterface $recordIndexer,
+        SqlGetReferenceEntityIdentifierForAttribute $getReferenceEntityIdentifierForAttribute,
+        CommandLauncher $commandLauncher
+    ) {
         $this->recordIndexer = $recordIndexer;
+        $this->commandLauncher = $commandLauncher;
+        $this->getReferenceEntityIdentifierForAttribute = $getReferenceEntityIdentifierForAttribute;
     }
 
     /**
@@ -27,11 +42,25 @@ class IndexRecordSubscriber implements EventSubscriberInterface
      */
     public static function getSubscribedEvents()
     {
-        return [RecordUpdatedEvent::class => 'whenRecordUpdated'];
+        return [
+            RecordUpdatedEvent::class    => 'whenRecordUpdated',
+            AttributeDeletedEvent::class => 'whenAttributeIsDeleted',
+        ];
     }
 
     public function whenRecordUpdated(RecordUpdatedEvent $recordUpdatedEvent): void
     {
         $this->recordIndexer->index($recordUpdatedEvent->getRecordIdentifier());
+    }
+
+    public function whenAttributeIsDeleted(AttributeDeletedEvent $attributeDeletedEvent): void
+    {
+        $cmd = sprintf(
+            '%s %s',
+            IndexRecordsCommand::INDEX_RECORDS_COMMAND_NAME,
+            (string) $attributeDeletedEvent->referenceEntityIdentifier
+        );
+
+        $this->commandLauncher->executeBackground($cmd, '/dev/null');
     }
 }
