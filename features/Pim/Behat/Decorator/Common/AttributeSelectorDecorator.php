a31f154cb5847dc6ba8d063a65c9cddf9e679213
Aymeric Planche <aymeric.planche@akeneo.com>
2018-10-18 16:42:57 +0200

    Update features/Pim/Behat/Decorator/Common/AttributeSelectorDecorator.php

--- features/Pim/Behat/Decorator/Common/AttributeSelectorDecorator.php
+++ features/Pim/Behat/Decorator/Common/AttributeSelectorDecorator.php
@@ -70,7 +70,7 @@ class AttributeSelectorDecorator extends ElementDecorator
                 '.selected-attributes .AknColumnConfigurator-listContainer .AknVerticalList'
             );
             return false === $selectedAttributes->has('css', '.AknVerticalList-item');
-        }, 'Cannot clear the selected atributes');
+span class="pl-s1">         }, 'Cannot clear the selected attributes');
     }
 
     /**
