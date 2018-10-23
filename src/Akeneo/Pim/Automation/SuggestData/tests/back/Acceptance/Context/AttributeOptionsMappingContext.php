5f6dbe8d0bc610c9200843787fc3a54b1ccd256f
romain <romain@akeneo.com>
2018-10-23 19:41:01 +0200

    APAI-165: Adds acceptance test

---
+++ src/Akeneo/Pim/Automation/SuggestData/tests/back/Acceptance/Context/AttributeOptionsMappingContext.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\SuggestData\tests\back\Acceptance\Context;
+
+use Akeneo\Pim\Automation\SuggestData\Application\Mapping\Query\GetAttributeOptionsMappingByAttributeAndFamilyHandler;
+use Behat\Behat\Context\Context;
+
+/**
+ * @author Romain Monceau <romain@akeneo.com>
+ */
+class AttributeOptionsMappingContext implements Context
+{
+    /** @var GetAttributeOptionsMappingByAttributeAndFamilyHandler */
+    private $getAttributeOptionsMappingByAttributeAndFamilyHandler;
+
+    /**
+     * @param GetAttributeOptionsMappingByAttributeAndFamilyHandler $getAttributeOptionsMappingByAttributeAndFamilyHandler
+     */
+    public function __construct(
+        GetAttributeOptionsMappingByAttributeAndFamilyHandler $getAttributeOptionsMappingByAttributeAndFamilyHandler
+    ) {
+        $this->getAttributeOptionsMappingByAttributeAndFamilyHandler
+            = $getAttributeOptionsMappingByAttributeAndFamilyHandler;
+    }
+}
