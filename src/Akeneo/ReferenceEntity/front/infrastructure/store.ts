8c39f6a104b66c62b39cf7c0916b772b18f33fe8
christophe-chausseray <chausseray.christophe@gmail.com>
2018-10-24 14:48:50 +0200

    Fix add midleware to set the user context value(channel and locale)

--- src/Akeneo/ReferenceEntity/front/infrastructure/store.ts
+++ src/Akeneo/ReferenceEntity/front/infrastructure/store.ts
@@ -3,6 +3,7 @@ import {applyMiddleware, createStore, Store, combineReducers} from 'redux';
 import routerMiddleware from 'akeneoreferenceentity/infrastructure/middleware/router';
 import formNotifier from 'akeneoreferenceentity/infrastructure/middleware/form-notifier';
 import grid from 'akeneoreferenceentity/infrastructure/middleware/grid';
+import userContext from 'akeneoreferenceentity/infrastructure/middleware/user-context';
 import {composeWithDevTools} from 'redux-devtools-extension';
 const router = require('pim/router');
 
@@ -10,7 +11,7 @@ export default (debug: boolean = true) => (reducer: any): Store<any> => {
   return createStore(
     combineReducers(reducer),
     true === debug
-      ? composeWithDevTools(applyMiddleware(thunkMiddleware, routerMiddleware(router), formNotifier(), grid()))
+      ? composeWithDevTools(applyMiddleware(thunkMiddleware, routerMiddleware(router), formNotifier(), grid(), userContext()))
       : applyMiddleware(thunkMiddleware, routerMiddleware(router), formNotifier())
   );
 };
