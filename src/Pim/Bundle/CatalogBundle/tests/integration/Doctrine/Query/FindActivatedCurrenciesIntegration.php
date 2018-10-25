ada436b86bf46092a2f79e52d897b060b1077f4b
Samir Boulil <samir.boulil@akeneo.com>
2018-10-25 12:45:14 +0200

    PIM-7765: Add query function to find activated currencies for locale

---
+++ src/Pim/Bundle/CatalogBundle/tests/integration/Doctrine/Query/FindActivatedCurrenciesIntegration.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Pim\Bundle\CatalogBundle\tests\integration\Doctrine\Query;
+
+use Akeneo\Test\Integration\TestCase;
+use PHPUnit\Framework\Assert;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+
+/**
+ * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FindActivatedCurrenciesIntegration extends TestCase
+{
+    public function testThatItReturnsTheActivatedCurrencyOfAChannel()
+    {
+        Assert::assertSame(
+            $this->get('pim_catalog.query.find_activated_currencies')->forChannel('ecommerce'),
+            ['USD']
+        );
+    }
+
+    public function testThatItReturnsAnEmptyArrayForUnkownChannels()
+    {
+        Assert::assertEmpty(
+            $this->get('pim_catalog.query.find_activated_currencies')->forChannel('unknown_channel')
+        );
+    }
+
+    public function testReturnsMultipleActivatedCurrenciesForAChannel()
+    {
+        $this->addEURToEcommerce();
+        Assert::assertSame(
+            $this->get('pim_catalog.query.find_activated_currencies')->forChannel('ecommerce'),
+            ['USD', 'EUR']
+        );
+    }
+
+    public function testReturnsAllActivatedCurrenciesForAllChannels()
+    {
+        $this->addAdditionalCurrenciesToMobile();
+        Assert::assertSame(
+            $this->get('pim_catalog.query.find_activated_currencies')->forAllChannels('ecommerce'),
+            ['USD', 'EUR', 'ADP', 'AFA']
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration()
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+
+    private function addEURToEcommerce(): void
+    {
+        $eur = $this->get('pim_catalog.repository.currency')->findOneByIdentifier('EUR');
+        $channelRepository = $this->get('pim_catalog.repository.channel');
+        $ecommerce = $channelRepository->findOneBy(['code' => 'ecommerce']);
+        $ecommerce->addCurrency($eur);
+        $this->get('pim_catalog.saver.channel')->save($ecommerce);
+    }
+
+    private function addAdditionalCurrenciesToMobile()
+    {
+        $eur = $this->get('pim_catalog.repository.currency')->findOneByIdentifier('EUR');
+        $adp = $this->get('pim_catalog.repository.currency')->findOneByIdentifier('ADP');
+        $afa = $this->get('pim_catalog.repository.currency')->findOneByIdentifier('AFA');
+        $adp->setActivated(true);
+        $afa->setActivated(true);
+        $this->get('pim_catalog.saver.currency')->saveAll([$adp, $afa]);
+
+        $master = $this->get('pim_catalog.repository.category')->findOneByIdentifier('master');
+
+        $mobile = new Channel();
+        $mobile->setCode('mobile');
+        $mobile->setCurrencies([$eur, $adp, $afa]);
+        $mobile->setCategory($master);
+        $this->get('pim_catalog.saver.channel')->save($mobile);
+    }
+}
