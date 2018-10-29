057bd5eeefa2e9418eb3d8cf5c85a78180a09682
christophe-chausseray <chausseray.christophe@gmail.com>
2018-10-29 12:00:29 +0100

    PIM-7765: Fix integration backend tests

--- src/PimEnterprise/Bundle/ConnectorBundle/tests/integration/Export/ProductModel/ExportProductModelsWithoutPermissionIntegration.php
+++ src/PimEnterprise/Bundle/ConnectorBundle/tests/integration/Export/ProductModel/ExportProductModelsWithoutPermissionIntegration.php
@@ -19,9 +19,9 @@ class ExportProductModelsWithoutPermissionIntegration extends AbstractProductMod
     public function testProductModelsWithoutPermissionApplied()
     {
         $expectedCsv = <<<CSV
-code;family_variant;parent;categories;an_image;a_date;a_file;a_localizable_image-en_US;a_localized_and_scopable_text_area-en_US-ecommerce;a_metric;a_metric-unit;a_multi_select;a_number_float;a_number_float_negative;a_number_integer;a_price-CNY;a_price-EUR;a_price-USD;a_ref_data_multi_select;a_ref_data_simple_select;a_scopable_price-ecommerce-CNY;a_scopable_price-ecommerce-EUR;a_scopable_price-ecommerce-USD
-root_product_model_visible_for_redactor;familyVariantA1;;categoryA2;;;;;;;;;;;;;;;;;;;
-root_product_model_visible_for_manager_only;familyVariantA1;;categoryB;;;;;;;;;;;;;;;;;;;
+code;family_variant;parent;categories;an_image;a_date;a_file;a_localizable_image-en_US;a_localized_and_scopable_text_area-en_US-ecommerce;a_metric;a_metric-unit;a_multi_select;a_number_float;a_number_float_negative;a_number_integer;a_price-CNY;a_price-EUR;a_price-USD;a_ref_data_multi_select;a_ref_data_simple_select;a_scopable_price-ecommerce-EUR;a_scopable_price-ecommerce-USD
+root_product_model_visible_for_redactor;familyVariantA1;;categoryA2;;;;;;;;;;;;;;;;;;
+root_product_model_visible_for_manager_only;familyVariantA1;;categoryB;;;;;;;;;;;;;;;;;;
 
 CSV;
 
@@ -46,9 +46,9 @@ CSV;
         $user = $this->get('pim_user.provider.user')->loadUserByUsername('mary');
 
         $expectedCsv = <<<CSV
-code;family_variant;parent;categories;an_image;a_date;a_file;a_localizable_image-en_US;a_localized_and_scopable_text_area-en_US-ecommerce;a_metric;a_metric-unit;a_multi_select;a_number_float;a_number_float_negative;a_number_integer;a_price-CNY;a_price-EUR;a_price-USD;a_ref_data_multi_select;a_ref_data_simple_select;a_scopable_price-ecommerce-CNY;a_scopable_price-ecommerce-EUR;a_scopable_price-ecommerce-USD
-root_product_model_visible_for_redactor;familyVariantA1;;categoryA2;;;;;;;;;;;;;;;;;;;
-root_product_model_visible_for_manager_only;familyVariantA1;;categoryB;;;;;;;;;;;;;;;;;;;
+code;family_variant;parent;categories;an_image;a_date;a_file;a_localizable_image-en_US;a_localized_and_scopable_text_area-en_US-ecommerce;a_metric;a_metric-unit;a_multi_select;a_number_float;a_number_float_negative;a_number_integer;a_price-CNY;a_price-EUR;a_price-USD;a_ref_data_multi_select;a_ref_data_simple_select;a_scopable_price-ecommerce-EUR;a_scopable_price-ecommerce-USD
+root_product_model_visible_for_redactor;familyVariantA1;;categoryA2;;;;;;;;;;;;;;;;;;
+root_product_model_visible_for_manager_only;familyVariantA1;;categoryB;;;;;;;;;;;;;;;;;;
 
 CSV;
 
