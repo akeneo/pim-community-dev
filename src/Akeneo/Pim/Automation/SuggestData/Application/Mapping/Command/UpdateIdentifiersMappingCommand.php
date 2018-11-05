525b492930304ff5e7ea80c7065e9598c5e5ffe1
pierallard <pierallard@gmail.com>
2018-11-05 16:04:50 +0100

    [APAI-378] Fix constraint messages #138

--- src/Akeneo/Pim/Automation/SuggestData/Application/Mapping/Command/UpdateIdentifiersMappingCommand.php
+++ src/Akeneo/Pim/Automation/SuggestData/Application/Mapping/Command/UpdateIdentifiersMappingCommand.php
@@ -83,8 +83,6 @@ class UpdateIdentifiersMappingCommand
         foreach ($values as $attributeCode => $frequency) {
             if ($frequency > 1) {
                 throw InvalidMappingException::duplicateAttributeCode(
-                    $frequency,
-                    $attributeCode,
                     static::class,
                     array_search($attributeCode, $filteredMapping)
                 );
