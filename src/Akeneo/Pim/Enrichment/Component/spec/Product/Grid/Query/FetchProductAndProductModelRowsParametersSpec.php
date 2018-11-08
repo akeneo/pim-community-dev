47d28db0ef60babaea9249f660c8f810fddb41c9
ahocquard <alexandre.hocquard@akeneo.com>
2018-11-08 17:55:01 +0100

    PIM-7660: create a query to fetch the product and product rows in the grid

---
+++ src/Akeneo/Pim/Enrichment/Component/spec/Product/Grid/Query/FetchProductAndProductModelRowsParametersSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Akeneo\Pim\Enrichment\Component\Product\Grid\Query;
+
+use Akeneo\Pim\Enrichment\Component\Product\Model\ValueCollection;
+use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Value\MediaValue;
+use Akeneo\Pim\Enrichment\Component\Product\Value\ScalarValue;
+use Akeneo\Pim\Structure\Component\Model\Attribute;
+use Akeneo\Tool\Component\FileStorage\Model\FileInfo;
+use PhpSpec\ObjectBehavior;
+
+class FetchProductAndProductModelRowsParametersSpec extends ObjectBehavior
+{
+    function let(ProductQueryBuilderInterface $builder)
+    {
+        $this->beConstructedWith($builder, ['attribute_1'], 'channel_code', 'locale_code', 123);
+    }
+
+    function it_has_the_attribute_codes()
+    {
+        $this->attributeCodes()->shouldReturn(['attribute_1']);
+    }
+
+    function it_has_the_channel_code()
+    {
+        $this->channelCode()->shouldReturn('channel_code');
+    }
+
+    function it_has_the_locale_code()
+    {
+        $this->localeCode()->shouldReturn('locale_code');
+    }
+
+    function it_has_the_user_id()
+    {
+        $this->userId()->shouldReturn(123);
+    }
+}
