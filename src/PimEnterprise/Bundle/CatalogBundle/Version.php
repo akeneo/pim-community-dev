888cbd893a3bd241c234dc45a303940796fa8129
Elodie Raposo <raposo.elodie@gmail.com>
2018-11-12 11:16:11 +0100

    Preparing version 1.7.34

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.33';
+    const VERSION = '1.7.34';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Artichoke';
