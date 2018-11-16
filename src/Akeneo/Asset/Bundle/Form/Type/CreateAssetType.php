88892a56b759c89b421d07d81f7fd0203bce15d3
Elodie Raposo <raposo.elodie@gmail.com>
2018-11-16 11:27:26 +0100

    Remove unused translations

--- src/Akeneo/Asset/Bundle/Form/Type/CreateAssetType.php
+++ src/Akeneo/Asset/Bundle/Form/Type/CreateAssetType.php
@@ -68,4 +68,16 @@ class CreateAssetType extends AbstractType
     {
         return 'pimee_product_asset_create';
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'validation_groups' => ['upload'],
+            ]
+        );
+    }
 }
