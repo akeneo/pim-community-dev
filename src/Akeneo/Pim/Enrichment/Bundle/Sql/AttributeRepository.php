59f95e29389ff0cc893b2ab4b536a3ca31b33604
ahocquard <alexandre.hocquard@akeneo.com>
2018-11-25 19:03:28 +0100

    Refactor the unitary call

--- src/Akeneo/Pim/Enrichment/Bundle/Sql/AttributeRepository.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Sql/AttributeRepository.php
@@ -24,57 +24,9 @@ class AttributeRepository
 
     public function findOneByIdentifier(string $code): ?Attribute
     {
-        $sql = <<<SQL
-          SELECT
-            a.*,
-            ag.code AS group_code,
-            JSON_ARRAYAGG(l.code) AS available_locales
-          FROM
-            pim_catalog_attribute a
-            JOIN pim_catalog_attribute_group ag ON ag.id = a.group_id
-            LEFT JOIN pim_catalog_attribute_locale al ON al.attribute_id = a.id
-            LEFT JOIN pim_catalog_locale l on l.id = al.locale_id
-          WHERE 
-            a.code = :code
-		  GROUP BY a.id
-SQL;
+        $rows =  $this->findSeveralByIdentifiers([$code]);
 
-        $row = $this->connection->executeQuery($sql, ['code' => $code])->fetch();
-        if (empty($row)) {
-            return null;
-        }
-
-        $attribute = new Attribute(
-            (int) $row['id'],
-            $row['code'],
-            $row['attribute_type'],
-            $row['backend_type'],
-            (bool) $row['is_required'],
-            (bool) $row['is_unique'],
-            (bool) $row['is_localizable'],
-            (bool) $row['is_scopable'],
-            new ArrayCollection(unserialize($row['properties'])),
-            // options
-            $row['group_code'],
-            (bool) $row['useable_as_grid_filter'],
-            new ArrayCollection(array_filter(json_decode($row['available_locales'], true))),
-            (int) $row['max_characters'],
-            $row['validation_rule'],
-            $row['validation_regexp'],
-            (int) $row['number_min'],
-            (int) $row['number_max'],
-            (bool) $row['decimals_allowed'],
-            (bool) $row['negative_allowed'],
-            Type::getType(Type::DATETIME)->convertToPHPValue($row['date_min'], $this->connection->getDatabasePlatform()),
-            Type::getType(Type::DATETIME)->convertToPHPValue($row['date_max'], $this->connection->getDatabasePlatform()),
-            $row['metric_family'],
-            $row['default_metric_unit'],
-            (int) $row['max_file_size'],
-            explode(',', $row['allowed_extensions']),
-            (int) $row['minimumInputLength']
-        );
-
-        return $attribute;
+        return $rows[$code];
     }
 
     public function findSeveralByIdentifiers(array $codes): array
@@ -137,7 +89,7 @@ SQL;
                 $row['metric_family'],
                 $row['default_metric_unit'],
                 (int) $row['max_file_size'],
-                explode(',', $row['allowed_extensions']),
+                null === $row['allowed_extensions'] ? [] : explode(',', $row['allowed_extensions']),
                 (int) $row['minimumInputLength']
             );
         }
