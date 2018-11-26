8c93da2d88b5d3b1725ea93ee8d7eb1f394a08fb
Philippe Mossière <philippe.mossiere@akeneo.com>
2018-11-26 14:06:26 +0100

    Preparing version 1.7.35

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.34';
+    const VERSION = '1.7.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
