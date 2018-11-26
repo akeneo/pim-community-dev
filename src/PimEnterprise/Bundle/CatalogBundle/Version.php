be436d859e798d6e4afa640dbafbf2d738e30bfa
Philippe Mossière <philippe.mossiere@akeneo.com>
2018-11-26 14:15:39 +0100

    Preparing version 1.7.35

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.34';
+    const VERSION = '1.7.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Artichoke';
