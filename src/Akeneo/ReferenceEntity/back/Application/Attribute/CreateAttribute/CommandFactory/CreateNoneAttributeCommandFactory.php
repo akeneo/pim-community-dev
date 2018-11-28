ccc30f98fa30f77cecb5a6f991c3d909d63e3720
christophe-chausseray <chausseray.christophe@gmail.com>
2018-11-28 10:21:57 +0100

    Add none attribute command

---
+++ src/Akeneo/ReferenceEntity/back/Application/Attribute/CreateAttribute/CommandFactory/CreateNoneAttributeCommandFactory.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Akeneo\ReferenceEntity\Application\Attribute\CreateAttribute\CommandFactory;
+
+use Akeneo\ReferenceEntity\Application\Attribute\CreateAttribute\CreateNoneAttributeCommand;
+use Akeneo\ReferenceEntity\Application\Attribute\CreateAttribute\AbstractCreateAttributeCommand;
+
+/**
+ * @author    Christophe Chausseray <christophe.chausseray@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ */
+class CreateNoneAttributeCommandFactory extends AbstractCreateAttributeCommandFactory
+{
+    public function supports(array $normalizedCommand): bool
+    {
+        return isset($normalizedCommand['type']) && 'none' === $normalizedCommand['type'];
+    }
+
+    public function create(array $normalizedCommand): AbstractCreateAttributeCommand
+    {
+        $command = new CreateNoneAttributeCommand();
+        $command->type = $normalizedCommand['type'];
+
+        return $command;
+    }
+}
