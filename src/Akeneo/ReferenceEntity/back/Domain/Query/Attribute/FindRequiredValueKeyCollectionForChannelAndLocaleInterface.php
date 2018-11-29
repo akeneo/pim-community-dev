4a25e62ff23d26bc7340bcbb90fe480dcb8f8fc7
Adrien Pétremann <hello@grena.fr>
2018-11-29 10:27:28 +0100

    PIM-7833: Add query for required value keys

---
+++ src/Akeneo/ReferenceEntity/back/Domain/Query/Attribute/FindRequiredValueKeyCollectionForChannelAndLocaleInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\ReferenceEntity\Domain\Query\Attribute;
+
+use Akeneo\ReferenceEntity\Domain\Model\ChannelIdentifier;
+use Akeneo\ReferenceEntity\Domain\Model\LocaleIdentifier;
+use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
+
+/**
+ * Find the list of required keys for the given reference entity, on the given channel & locale.
+ *
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2018 Akeneo SAS (https://www.akeneo.com)
+ */
+interface FindRequiredValueKeyCollectionForChannelAndLocaleInterface
+{
+    public function __invoke(
+        ReferenceEntityIdentifier $referenceEntityIdentifier,
+        ChannelIdentifier $channelIdentifier,
+        LocaleIdentifier $localeIdentifier
+    ): ValueKeyCollection;
+}
