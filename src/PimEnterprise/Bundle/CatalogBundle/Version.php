c3b38d4f2204857f4aafb9bfbdf5a99962535cc6
Mathias Métayer <mathias.metayer@akeneo.com>
2018-11-29 17:45:10 +0100

    Preparing version 2.0.44

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.43';
+    const VERSION = '2.0.44';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
