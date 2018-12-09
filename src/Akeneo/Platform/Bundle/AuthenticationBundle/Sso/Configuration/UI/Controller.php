2393ef013581245344e54aa6752eb82df2307d36
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2018-12-09 21:17:10 +0100

    AOB-283: Add controller, command and handler

---
+++ src/Akeneo/Platform/Bundle/AuthenticationBundle/Sso/Configuration/UI/Controller.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Platform\Bundle\AuthenticationBundle\Sso\Configuration\UI;
+
+use Akeneo\Platform\Component\Authentication\Sso\Configuration\CreateOrUpdateConfiguration;
+use Akeneo\Platform\Component\Authentication\Sso\Configuration\CreateOrUpdateConfigurationHandler;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Controller for SSO configuration.
+ *
+ * @author Yohan Blain <yohan.blain@akeneo.com>
+ */
+final class Controller
+{
+    private const CONFIGURATION_CODE = 'authentication_sso';
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var NormalizerInterface */
+    private $normalizer;
+
+    /** @var CreateOrUpdateConfigurationHandler */
+    private $createOrUpdateConfigHandler;
+
+    public function __construct(
+        ValidatorInterface $validator,
+        NormalizerInterface $normalizer,
+        CreateOrUpdateConfigurationHandler $createOrUpdateConfigHandler
+    ) {
+        $this->validator = $validator;
+        $this->normalizer = $normalizer;
+        $this->createOrUpdateConfigHandler = $createOrUpdateConfigHandler;
+    }
+
+    public function saveAction(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $createOrUpdateConfig = new CreateOrUpdateConfiguration(
+            self::CONFIGURATION_CODE,
+            $data['enabled'],
+            $data['identityProviderEntityId'],
+            $data['identityProviderUrl'],
+            $data['identityProviderPublicCertificate'],
+            $data['serviceProviderEntityId'],
+            $data['serviceProviderPublicCertificate'],
+            $data['serviceProviderPrivateCertificate']
+        );
+
+        $errors = $this->validator->validate($createOrUpdateConfig);
+
+        if (0 < $errors->count()) {
+            $normalizedErrors = $this->normalizer->normalize(
+                $errors,
+                'internal_api'
+            );
+
+            return new JsonResponse($normalizedErrors, 400);
+        }
+
+        $this->createOrUpdateConfigHandler->handle($createOrUpdateConfig);
+
+        return new JsonResponse();
+    }
+}
---
+++ src/Akeneo/Platform/Component/Authentication/Sso/Configuration/CreateOrUpdateConfiguration.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Platform\Component\Authentication\Sso\Configuration;
+
+/**
+ * DTO holding configuration data waiting to be validated and persisted.
+ *
+ * @author Yohan Blain <yohan.blain@akeneo.com>
+ */
+final class CreateOrUpdateConfiguration
+{
+    /** @var string */
+    public $code;
+
+    /** @var bool */
+    public $enabled;
+
+    /** @var string */
+    public $identityProviderEntityId;
+
+    /** @var string */
+    public $identityProviderUrl;
+
+    /** @var string */
+    public $identityProviderPublicCertificate;
+
+    /** @var string */
+    public $serviceProviderEntityId;
+
+    /** @var string */
+    public $serviceProviderPublicCertificate;
+
+    /** @var string */
+    public $serviceProviderPrivateCertificate;
+
+    public function __construct(
+        string $code,
+        bool $enabled,
+        string $identityProviderEntityId,
+        string $identityProviderUrl,
+        string $identityProviderPublicCertificate,
+        string $serviceProviderEntityId,
+        string $serviceProviderPublicCertificate,
+        string $serviceProviderPrivateCertificate
+    ) {
+
+        $this->code = $code;
+        $this->enabled = $enabled;
+        $this->identityProviderEntityId = $identityProviderEntityId;
+        $this->identityProviderUrl = $identityProviderUrl;
+        $this->identityProviderPublicCertificate = $identityProviderPublicCertificate;
+        $this->serviceProviderEntityId = $serviceProviderEntityId;
+        $this->serviceProviderPublicCertificate = $serviceProviderPublicCertificate;
+        $this->serviceProviderPrivateCertificate = $serviceProviderPrivateCertificate;
+    }
+}
