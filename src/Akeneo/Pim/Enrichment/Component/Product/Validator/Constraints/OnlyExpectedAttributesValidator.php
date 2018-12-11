84422c6a2afc02232df01b8941f33c77efe9dca3
jean-marie leroux <jmleroux.pro@gmail.com>
2018-12-11 08:42:31 +0100

    Fix OnlyExpectedAttributesValidator
    This PR prevent `Nesting level too deep – recursive dependency` exception.
    Explained in #9023 issue

--- src/Akeneo/Pim/Enrichment/Component/Product/Validator/Constraints/OnlyExpectedAttributesValidator.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Validator/Constraints/OnlyExpectedAttributesValidator.php
@@ -54,7 +54,7 @@ class OnlyExpectedAttributesValidator extends ConstraintValidator
         );
 
         foreach ($entity->getValuesForVariation()->getAttributeCodes() as $modelAttributeCode) {
-            if (!in_array($modelAttributeCode, $familyAttributeCodes)) {
+            if (!in_array($modelAttributeCode, $familyAttributeCodes, true)) {
                 $this->context->buildViolation(
                     OnlyExpectedAttributes::ATTRIBUTE_DOES_NOT_BELONG_TO_FAMILY, [
                     '%attribute%' => $modelAttributeCode,
@@ -64,7 +64,7 @@ class OnlyExpectedAttributesValidator extends ConstraintValidator
                 continue;
             }
 
-            if (!in_array($modelAttributeCode, $levelAttributeCodes)) {
+            if (!in_array($modelAttributeCode, $levelAttributeCodes, true)) {
                 $this->context->buildViolation(
                     OnlyExpectedAttributes::ATTRIBUTE_UNEXPECTED, [
                     '%attribute%' => $modelAttributeCode
