c7f4058e85558d58ae14a5b3759bba97f2130042
ahocquard <alexandre.hocquard@akeneo.com>
2018-12-12 14:23:30 +0100

    Naming of the interface of the permissions

---
+++ src/Akeneo/Pim/Permission/Component/Authorization/Query/IsProductOnlyEditableAsDraftByUser.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Permission\Component\Authorization\Query;
+
+interface IsProductOnlyEditableAsDraftByUser
+{
+    public function __invoke(string $productCode, int $userId): bool;
+}
