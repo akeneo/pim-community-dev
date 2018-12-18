c96021c0cdad6bd7ed7f6c513f56307e6e6a10c2
Willy <willy.mesnage@akeneo.com>
2018-12-18 17:04:36 +0100

    Preparing version 2.0.45

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.44';
+    const VERSION = '2.0.45';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rebel Rabbit';
