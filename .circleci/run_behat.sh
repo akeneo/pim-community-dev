9229d2183a7e7b54ebd368e27ed216db498db21b
ahocquard <alexandre.hocquard@akeneo.com>
2018-12-19 14:39:49 +0100

    Pim formatter for behat tests

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -5,5 +5,5 @@ TESTFILES=$@
 
 for TESTFILE in $TESTFILES; do
     echo $TESTFILE
-    docker-compose exec -T fpm ./vendor/bin/behat --format junit --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
 done
