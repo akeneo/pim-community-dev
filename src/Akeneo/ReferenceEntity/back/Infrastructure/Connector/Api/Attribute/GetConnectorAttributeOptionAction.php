95a401c4f5c8315b539fd23d463e7455865c5b3b
Tamara Robichet <tamara.robichet@akeneo.com>
2018-12-19 10:13:44 +0100

    PIM-7807: Add missing copyright docs

--- src/Akeneo/ReferenceEntity/back/Infrastructure/Connector/Api/Attribute/GetConnectorAttributeOptionAction.php
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/Connector/Api/Attribute/GetConnectorAttributeOptionAction.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Akeneo\ReferenceEntity\Infrastructure\Connector\Api\Attribute;
 
 use Akeneo\ReferenceEntity\Domain\Model\Attribute\AttributeCode;
