b15f8ad5bf7d5dfe0866f0dfaf855bd525d76586
ahocquard <alexandre.hocquard@akeneo.com>
2018-12-20 14:41:57 +0100

    Mark legacy behat scenarios step as failed when at least one test fail

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -3,7 +3,11 @@
 # We need the TESTFILES var in $1
 TESTFILES=$@
 
+fail=0
 for TESTFILE in $TESTFILES; do
     echo $TESTFILE
     docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
+    fail=$(($fail + $?))
 done
+
+return $fail
