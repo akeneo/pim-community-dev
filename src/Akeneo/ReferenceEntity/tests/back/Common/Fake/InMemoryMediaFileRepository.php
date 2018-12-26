bbb23f4a77a3fa5c987197c4272fd43079e010a0
Laurent Petard <laurent.petard@akeneo.com>
2018-12-26 17:31:02 +0100

    PIM-7786: Add repository for the media files

---
+++ src/Akeneo/ReferenceEntity/tests/back/Common/Fake/InMemoryMediaFileRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\ReferenceEntity\Common\Fake;
+
+use Akeneo\ReferenceEntity\Domain\Repository\MediaFileNotFoundException;
+use Akeneo\ReferenceEntity\Domain\Repository\MediaFileRepositoryInterface;
+use Akeneo\Tool\Component\FileStorage\Model\FileInfo;
+
+/**
+ * @author    Laurent Petard <laurent.petard@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ */
+class InMemoryMediaFileRepository implements MediaFileRepositoryInterface
+{
+    /** @var FileInfo[] */
+    private $mediaFiles = [];
+
+    public function getByIdentifier(string $identifier): FileInfo
+    {
+        if (!array_key_exists($identifier, $this->mediaFiles)) {
+            throw MediaFileNotFoundException::withIdentifier($identifier);
+        }
+
+        return $this->mediaFiles[$identifier];
+    }
+
+    public function save(FileInfo $mediaFile): void
+    {
+        $this->mediaFiles[$mediaFile->getKey()] = $mediaFile;
+    }
+}
