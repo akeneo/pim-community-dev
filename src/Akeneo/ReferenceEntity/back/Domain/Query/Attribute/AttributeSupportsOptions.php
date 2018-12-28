527789adbb5ba2b49665fc19147db213e2c2e04f
Tamara Robichet <tamara.robichet@akeneo.com>
2018-12-28 10:49:43 +0100

    PIM-7806: Implement AttributeSupportsOptions

--- src/Akeneo/ReferenceEntity/back/Domain/Query/Attribute/AttributeSupportsOptions.php
+++ src/Akeneo/ReferenceEntity/back/Domain/Query/Attribute/AttributeSupportsOptions.php
@@ -6,12 +6,29 @@ use Akeneo\ReferenceEntity\Domain\Model\Attribute\AttributeCode;
 use Akeneo\ReferenceEntity\Domain\Model\Attribute\OptionAttribute;
 use Akeneo\ReferenceEntity\Domain\Model\Attribute\OptionCollectionAttribute;
 use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
+use Akeneo\ReferenceEntity\Domain\Repository\AttributeRepositoryInterface;
 
 class AttributeSupportsOptions
 {
-    public function __invoke(AttributeCode $attributeCode, ReferenceEntityIdentifier $referenceEntityIdentifier): bool
+    /** @var GetAttributeIdentifierInterface */
+    private $getAttributeIdentifier;
+
+    /** @var AttributeRepositoryInterface */
+    private $attributeRepository;
+
+    public function __construct(
+        GetAttributeIdentifierInterface $getAttributeIdentifier,
+        AttributeRepositoryInterface $attributeRepository
+    )
     {
-        $attribute = [];
+        $this->getAttributeIdentifier = $getAttributeIdentifier;
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    public function __invoke(ReferenceEntityIdentifier $referenceEntityIdentifier, AttributeCode $attributeCode): bool
+    {
+        $identifier = ($this->getAttributeIdentifier)($referenceEntityIdentifier, $attributeCode);
+        $attribute = $this->attributeRepository->getByIdentifier($identifier);
 
         return ($attribute instanceof OptionCollectionAttribute || $attribute instanceof OptionAttribute);
     }
