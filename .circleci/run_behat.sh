574e870057ca7f606161305ca5411868354e9a46
pierallard <pierallard@gmail.com>
2018-12-31 12:36:18 +0100

    try with retry 1

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -6,7 +6,7 @@ TESTFILES=$@
 fail=0
 for TESTFILE in $TESTFILES; do
     echo $TESTFILE
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE || docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
     fail=$(($fail + $?))
 done
 
