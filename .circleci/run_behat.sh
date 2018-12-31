3ea37011f4c34a8f5006a73a07fc680e11498401
pierallard <pierallard@gmail.com>
2018-12-31 14:30:38 +0100

    Generate same UUID for behats

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -9,7 +9,9 @@ total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
 
 for TESTFILE in $TESTFILES; do
     echo "$TESTFILE ($counter/$total):"
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE || docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_$(uuidgen) --format pretty --out std -p legacy $TESTFILE
+    uuid=$(uuidgen)
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_${uuid} --format pretty --out std -p legacy $TESTFILE || \
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_${uuid} --format pretty --out std -p legacy $TESTFILE
     fail=$(($fail + $?))
     counter=$(($counter + 1))
 done
