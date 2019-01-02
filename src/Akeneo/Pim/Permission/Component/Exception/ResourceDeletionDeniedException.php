2e7b0f68d87c64f39ec16a7aeaf07f10c97c7930
Adrien Pétremann <hello@grena.fr>
2019-01-02 11:12:44 +0100

    PIM-7915: Event subscriber on user group deletion

---
+++ src/Akeneo/Pim/Permission/Component/Exception/ResourceDeletionDeniedException.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2017 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Permission\Component\Exception;
+
+/**
+ * Deletion denied for a resource.
+ *
+ * @author JM Leroux <jean-marie.leroux@akeneo.com>
+ */
+class ResourceDeletionDeniedException extends \LogicException
+{
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+}
