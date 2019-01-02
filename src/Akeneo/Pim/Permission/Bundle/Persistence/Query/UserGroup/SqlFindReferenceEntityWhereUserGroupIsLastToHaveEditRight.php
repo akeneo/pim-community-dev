8b97578eadd127953e6f56782cd650a7f749e774
Adrien Pétremann <hello@grena.fr>
2019-01-02 11:12:13 +0100

    PIM-7915: Add query to find rights on ref entities

---
+++ src/Akeneo/Pim/Permission/Bundle/Persistence/Query/UserGroup/SqlFindReferenceEntityWhereUserGroupIsLastToHaveEditRight.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Permission\Bundle\Persistence\Query\UserGroup;
+
+use Doctrine\DBAL\Connection;
+use PDO;
+
+/**
+ * This query finds the Reference Entity identifiers for which the given user group is the last one
+ * to have the edit permission on.
+ *
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2019 Akeneo SAS (https://www.akeneo.com)
+ */
+class SqlFindReferenceEntityWhereUserGroupIsLastToHaveEditRight
+{
+    /** @var Connection */
+    private $sqlConnection;
+
+    public function __construct(Connection $sqlConnection)
+    {
+        $this->sqlConnection = $sqlConnection;
+    }
+
+    public function __invoke(int $userGroupId): array
+    {
+        $sql = "SELECT reference_entity_identifier
+                FROM akeneo_reference_entity_reference_entity_permissions
+                WHERE right_level = 'edit'
+                GROUP BY reference_entity_identifier, user_group_identifier
+                HAVING user_group_identifier = :userGroupIdentifier
+                AND COUNT(*) = 1";
+
+        $statement = $this->sqlConnection->executeQuery(
+            $sql,
+            ['userGroupIdentifier' => $userGroupId],
+            ['userGroupId' => PDO::PARAM_INT]
+        );
+
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $referenceEntityIdentifiers = array_map(function (array $result) {
+            return $result['reference_entity_identifier'];
+        }, $results);
+
+        return $referenceEntityIdentifiers;
+    }
+}
