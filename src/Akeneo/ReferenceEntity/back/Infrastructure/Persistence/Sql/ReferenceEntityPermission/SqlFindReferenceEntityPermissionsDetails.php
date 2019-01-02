34e4aa5b8c8d4ce7457dfd6795380dfac8bebd94
Samir Boulil <samir.boulil@akeneo.com>
2019-01-02 17:06:25 +0100

    PIM-7915: Fix show of ref-permissions

--- src/Akeneo/ReferenceEntity/back/Infrastructure/Persistence/Sql/ReferenceEntityPermission/SqlFindReferenceEntityPermissionsDetails.php
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/Persistence/Sql/ReferenceEntityPermission/SqlFindReferenceEntityPermissionsDetails.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Akeneo\ReferenceEntity\Infrastructure\Persistence\Sql\ReferenceEntityPermission;
 
+use Akeneo\ReferenceEntity\Domain\Model\Permission\RightLevel;
 use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
 use Akeneo\ReferenceEntity\Domain\Query\ReferenceEntityPermission\FindReferenceEntityPermissionsDetailsInterface;
 use Akeneo\ReferenceEntity\Domain\Query\ReferenceEntityPermission\PermissionDetails;
@@ -11,6 +12,10 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
 
 /**
+ * If there are no permissions set for the reference entity, then this query function returns an empty list.
+ * However, if there are permissions, we need to merge those with all the user group (except 'All') defined in the PIM
+ * with the default right level: "view"
+ *
  * @author    Samir Boulil <samir.boulil@akeneo.com>
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  */
@@ -29,12 +34,29 @@ class SqlFindReferenceEntityPermissionsDetails implements FindReferenceEntityPer
      */
     public function __invoke(ReferenceEntityIdentifier $referenceEntityIdentifier): array
     {
-        $permissionDetails = $this->fetch($referenceEntityIdentifier);
+        $permissionDetails = $this->fetchPermissions($referenceEntityIdentifier);
+        if (empty($permissionDetails)) {
+            return [];
+        }
+        $userGroups = $this->fetchUserGroups();
 
-        return $this->hydratePermissionDetails($permissionDetails);
+        return $this->hydrate($userGroups, $permissionDetails);
     }
 
-    private function fetch(ReferenceEntityIdentifier $referenceEntityIdentifier): array
+    private function fetchUserGroups(): array
+    {
+        $query = <<<SQL
+SELECT ug.id as user_group_identifier, ug.name as user_group_name
+FROM oro_access_group ug
+WHERE ug.name <> 'All';
+SQL;
+        $statement = $this->sqlConnection->executeQuery($query);
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return false !== $result ? $result : [];
+    }
+
+    private function fetchPermissions(ReferenceEntityIdentifier $referenceEntityIdentifier): array
     {
         $query = <<<SQL
 SELECT ug.id as user_group_identifier, ug.name as user_group_name, rp.right_level
@@ -53,23 +75,47 @@ SQL;
     /**
      * @return PermissionDetails[]
      */
-    private function hydratePermissionDetails(array $permissionDetails): array
+    private function hydrate(array $userGroups, array $normalizedpermissionDetails): array
+    {
+        return array_map(
+            function (array $userGroup) use ($normalizedpermissionDetails) {
+                $rightLevelForGroup = $this->getRightLevel($userGroup, $normalizedpermissionDetails);
+
+                return $this->createPermissionDetails($userGroup, $rightLevelForGroup);
+            },
+            $userGroups
+        );
+    }
+
+    private function getRightLevel(array $userGroup, array $normalizedPermissionDetails): string
+    {
+        $rightLevel = RightLevel::VIEW;
+        $platform = $this->sqlConnection->getDatabasePlatform();
+        $userGroupIdentifier = Type::getType(Type::INTEGER)
+            ->convertToPhpValue($userGroup['user_group_identifier'], $platform);
+
+        foreach ($normalizedPermissionDetails as $normalizedPermissionDetail) {
+            $searchedUserGroup = Type::getType(Type::INTEGER)
+                ->convertToPhpValue($normalizedPermissionDetail['user_group_identifier'], $platform);
+            if ($userGroupIdentifier === $searchedUserGroup) {
+                $rightLevel = Type::getType(Type::STRING)
+                    ->convertToPhpValue($normalizedPermissionDetail['right_level'], $platform);
+            }
+        }
+
+        return $rightLevel;
+    }
+
+    private function createPermissionDetails(array $userGroup, string $rightLevel): PermissionDetails
     {
         $platform = $this->sqlConnection->getDatabasePlatform();
+        $permissionDetails = new PermissionDetails();
+        $permissionDetails->userGroupIdentifier = Type::getType(Type::INTEGER)
+            ->convertToPhpValue($userGroup['user_group_identifier'], $platform);
+        $permissionDetails->userGroupName = Type::getType(Type::STRING)
+            ->convertToPhpValue($userGroup['user_group_name'], $platform);
+        $permissionDetails->rightLevel = $rightLevel;
 
-        return array_map(
-            function (array $normalizedPermissionDetails) use ($platform) {
-                $permissionDetails = new PermissionDetails();
-                $permissionDetails->userGroupIdentifier = Type::getType(Type::INTEGER)
-                    ->convertToPhpValue($normalizedPermissionDetails['user_group_identifier'], $platform);
-                $permissionDetails->userGroupName = Type::getType(Type::STRING)
-                    ->convertToPhpValue($normalizedPermissionDetails['user_group_name'], $platform);
-                $permissionDetails->rightLevel = Type::getType(Type::STRING)
-                    ->convertToPhpValue($normalizedPermissionDetails['right_level'], $platform);
-
-                return $permissionDetails;
-            },
-            $permissionDetails
-        );
+        return $permissionDetails;
     }
 }
