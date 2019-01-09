101c9ad94ced4661a6af6519eeddeabf1a590475
Elodie Raposo <raposo.elodie@gmail.com>
2019-01-09 17:13:21 +0100

    PIM-7964: Fix database migration

--- upgrades/schema/Version_2_3_20190103095151_published_product_completeness_missing_attribute.php
+++ upgrades/schema/Version_2_3_20190103095151_published_product_completeness_missing_attribute.php
@@ -10,9 +10,8 @@ class Version_2_3_20190103095151_published_product_completeness_missing_attribut
     public function up(Schema $schema)
     {
         $sql = <<<SQL
-ALTER TABLE pimee_workflow_published_product_completeness_missing_attribute
-    DROP FOREIGN KEY FK_B0FD5518762147F6,
-    ADD CONSTRAINT FK_B0FD5518762147F6 FOREIGN KEY (missing_attribute_id) REFERENCES pim_catalog_attribute (id) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE pimee_workflow_published_product_completeness_missing_attribute DROP FOREIGN KEY FK_B0FD5518762147F6;
+ALTER TABLE pimee_workflow_published_product_completeness_missing_attribute ADD CONSTRAINT FK_B0FD5518762147F6 FOREIGN KEY (missing_attribute_id) REFERENCES pim_catalog_attribute (id) ON DELETE CASCADE;
 SQL;
         $this->addSql($sql);
     }
