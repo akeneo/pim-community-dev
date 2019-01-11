2ef9a4f79a1c91859962e38b7ba74f8065f21600
Tamara Robichet <tamara.robichet@akeneo.com>
2019-01-11 10:35:54 +0100

    PIM-7809: Add query validation

--- src/Akeneo/ReferenceEntity/back/Infrastructure/Connector/Api/Attribute/CreateOrUpdateAttributeOptionAction.php
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/Connector/Api/Attribute/CreateOrUpdateAttributeOptionAction.php
@@ -2,24 +2,74 @@
 
 namespace Akeneo\ReferenceEntity\Infrastructure\Connector\Api\Attribute;
 
+use Akeneo\ReferenceEntity\Domain\Model\Attribute\AttributeCode;
+use Akeneo\ReferenceEntity\Domain\Model\Attribute\AttributeOption\OptionCode;
+use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 class CreateOrUpdateAttributeOptionAction
 {
 
     public function __invoke(Request $request, string $referenceEntityIdentifier, string $attributeCode, string $optionCode)
     {
-        // Does reference entity exist
+        try {
+            $referenceEntityIdentifier = ReferenceEntityIdentifier::fromString($referenceEntityIdentifier);
+        } catch (\Exception $e) {
+            throw new UnprocessableEntityHttpException($e->getMessage());
+        }
 
-        // Does attribute exist
+        $referenceEntityExists = $this->referenceEntityExists->withIdentifier($referenceEntityIdentifier);
 
-        // AttributeSupportsOptions? if not return an error
+        if (false === $referenceEntityExists) {
+            throw new NotFoundHttpException(sprintf('Reference entity "%s" does not exist.', $referenceEntityIdentifier));
+        }
 
-        // Does option exist ?
-            // Create or update
+        try {
+            $attributeCode = AttributeCode::fromString($attributeCode);
+        } catch (\Exception $e) {
+            throw new UnprocessableEntityHttpException($e->getMessage());
+        }
+
+        $attributeExists = $this->attributeExists->withReferenceEntityAndCode($referenceEntityIdentifier, $attributeCode);
+
+        if (false === $attributeExists) {
+            throw new NotFoundHttpException(sprintf(
+                'Attribute "%s" does not exist for reference entity "%s".',
+                (string) $attributeCode,
+                (string) $referenceEntityIdentifier
+            ));
+        }
+
+        $attributeSupportsOptions = ($this->attributeSupportsOptions)($referenceEntityIdentifier, $attributeCode);
+
+        if (false === $attributeSupportsOptions) {
+            throw new NotFoundHttpException(sprintf('Attribute "%s" does not support options.', $attributeCode));
+        }
+
+        $attribute = ($this->findConnectorAttributeQuery)($referenceEntityIdentifier, $attributeCode);
+
+        try {
+            $optionCode = OptionCode::fromString($optionCode);
+        } catch (\Exception $e) {
+            throw new UnprocessableEntityHttpException($e->getMessage());
+        }
+
+        $optionExists = $attribute->hasAttributeOption($optionCode);
+
+        $optionExists ?
+            $this->editOption($referenceEntityIdentifier, $attributeCode, $optionCode, $request) :
+            $this->createOption($referenceEntityIdentifier, $attributeCode, $optionCode, $request);
+
     }
 
-    public function editOption(string $referenceEntityIdentifier, string $attributeCode, string $optionCode)
+    public function editOption(
+        ReferenceEntityIdentifier $referenceEntityIdentifier,
+        AttributeCode $attributeCode,
+        OptionCode $optionCode,
+        Request $request
+    )
     {
         // Validate the data with json schema validator
         // Use the handler to update
@@ -28,7 +78,12 @@ class CreateOrUpdateAttributeOptionAction
         // Normalize the attribute
     }
 
-    public function createOption(string $referenceEntityIdentifier, string $attributeCode, string $optionCode)
+    public function createOption(
+        ReferenceEntityIdentifier $referenceEntityIdentifier,
+        AttributeCode $attributeCode,
+        OptionCode $optionCode,
+        Request $request
+    )
     {
 
     }
