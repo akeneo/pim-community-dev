49216c497188a196317ae57d6ca57bc7063bf70d
romain <romain@akeneo.com>
2019-01-11 15:30:15 +0100

    APAI-170: Add phpspec for suggested data reader

--- src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification/Infrastructure/Connector/Reader/SuggestedDataReaderSpec.php
+++ src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification/Infrastructure/Connector/Reader/SuggestedDataReaderSpec.php
@@ -14,8 +14,11 @@ declare(strict_types=1);
 namespace Specification\Akeneo\Pim\Automation\FranklinInsights\Infrastructure\Connector\Reader;
 
 use Akeneo\Pim\Automation\FranklinInsights\Application\DataProvider\SubscriptionProviderInterface;
+use Akeneo\Pim\Automation\FranklinInsights\Infrastructure\Persistence\Query\Doctrine\SelectLastCompletedFetchExecutionDatetimeQuery;
 use Akeneo\Tool\Component\Batch\Item\InitializableInterface;
 use Akeneo\Tool\Component\Batch\Item\ItemReaderInterface;
+use Akeneo\Tool\Component\Batch\Job\JobParameters;
+use Akeneo\Tool\Component\Batch\Model\StepExecution;
 use Akeneo\Tool\Component\Batch\Step\StepExecutionAwareInterface;
 use PhpSpec\ObjectBehavior;
 
@@ -25,9 +28,12 @@ use PhpSpec\ObjectBehavior;
 class SuggestedDataReaderSpec extends ObjectBehavior
 {
     public function let(
-        SubscriptionProviderInterface $subscriptionProvider
+        SubscriptionProviderInterface $subscriptionProvider,
+        SelectLastCompletedFetchExecutionDatetimeQuery $query,
+        StepExecution $stepExecution
     ): void {
-        $this->beConstructedWith($subscriptionProvider);
+        $this->beConstructedWith($subscriptionProvider, $query);
+        $this->setStepExecution($stepExecution);
     }
 
     public function it_is_a_reader(): void
@@ -43,5 +49,38 @@ class SuggestedDataReaderSpec extends ObjectBehavior
     public function it_is_step_execution_aware(): void
     {
         $this->shouldImplement(StepExecutionAwareInterface::class);
+    }
+
+    public function it_initializes_the_reader_with_the_updated_since_parameter($stepExecution, $subscriptionProvider): void
+    {
+        $datetime = new \DateTime('2013-01-16');
+        $jobParameters = new JobParameters(['updated_since' => $datetime]);
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+
+        $subscriptionProvider->fetch($datetime)->shouldBeCalled();
+
+        $this->initialize();
+    }
+
+    public function it_takes_the_last_execution_datetime_if_no_parameter($stepExecution, $subscriptionProvider, $query): void
+    {
+        $jobParameters = new JobParameters(['updated_since' => null]);
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+        $query->execute()->willReturn('2017-08-28 11:26:32');
+
+        $subscriptionProvider->fetch(new \Datetime('2017-08-28 11:26:32'))->shouldBeCalled();
+
+        $this->initialize();
+    }
+
+    public function it_generates_an_old_date_for_the_first_execution($stepExecution, $subscriptionProvider, $query): void
+    {
+        $jobParameters = new JobParameters(['updated_since' => null]);
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+        $query->execute()->willReturn('2012-01-01');
+
+        $subscriptionProvider->fetch(new \Datetime('2012-01-01'))->shouldBeCalled();
+
+        $this->initialize();
     }
 }
