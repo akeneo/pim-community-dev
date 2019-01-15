e49ba4aef59c3cf3bf057992158962fdbdee1747
Julien Sanchez <sumbobyboys@gmail.com>
2019-01-15 15:28:08 +0100

    PIM-7916: fix acceptance tests

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/fetcher/locale-fetcher.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/fetcher/locale-fetcher.js
@@ -1,56 +1,52 @@
 'use strict';
 
-define(
-    [
-        'jquery',
-        'underscore',
-        'pim/base-fetcher',
-        'routing'
+define(['jquery', 'underscore', 'pim/base-fetcher', 'routing'], function($, _, BaseFetcher, Routing) {
+  return BaseFetcher.extend({
+    entityActivatedListPromise: null,
+    /**
+     * @param {Object} options
+     */
+    initialize: function(options) {
+      this.options = options || {};
+    },
 
-    ],
-    function (
-        $,
-        _,
-        BaseFetcher,
-        Routing
-    ) {
-        return BaseFetcher.extend({
-            entityActivatedListPromise: null,
-            /**
-             * @param {Object} options
-             */
-            initialize: function (options) {
-                this.options = options || {};
+    /**
+     * Fetch all activated locales.
+     *
+     * @return {Promise}
+     */
+    fetchActivated: function(searchOptions) {
+      if (!this.entityActivatedListPromise) {
+        if (!_.has(this.options.urls, 'list')) {
+          return $.Deferred()
+            .reject()
+            .promise();
+        }
+
+        this.entityActivatedListPromise = $.getJSON(
+          Routing.generate(this.options.urls.list),
+          Object.assign(
+            {},
+            {
+              activated: true,
             },
+            searchOptions
+          )
+        )
+          .then(_.identity)
+          .promise();
+      }
 
-            /**
-             * Fetch all activated locales.
-             *
-             * @return {Promise}
-             */
-            fetchActivated: function () {
-                if (!this.entityActivatedListPromise) {
-                    if (!_.has(this.options.urls, 'list')) {
-                        return $.Deferred().reject().promise();
-                    }
+      return this.entityActivatedListPromise;
+    },
 
-                    this.entityActivatedListPromise = $.getJSON(
-                        Routing.generate(this.options.urls.list),
-                        {activated: true}
-                    ).then(_.identity).promise();
-                }
+    /**
+     * {inheritdoc}
+     */
+    clear: function() {
+      this.entityActivatedListPromise = null;
 
-                return this.entityActivatedListPromise;
-            },
-
-            /**
-             * {inheritdoc}
-             */
-            clear: function () {
-                this.entityActivatedListPromise = null;
-
-                BaseFetcher.prototype.clear.apply(this, arguments);
-            }
-        });
-    }
-);
+      BaseFetcher.prototype.clear.apply(this, arguments);
+    },
+  });
+});
