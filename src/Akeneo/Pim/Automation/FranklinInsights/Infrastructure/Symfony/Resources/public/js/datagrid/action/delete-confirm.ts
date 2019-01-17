899670ac5c4542efd22cb9f7146199901de1c657
pierallard <pierallard@gmail.com>
2019-01-17 16:18:25 +0100

    APAI-471: Add warning on attribute deletion

---
+++ src/Akeneo/Pim/Automation/FranklinInsights/Infrastructure/Symfony/Resources/public/js/datagrid/action/delete-confirm.ts
@@ -0,0 +1,59 @@
+import {getConnectionStatus} from "../../fetcher/franklin-connection";
+import ConnectionStatus from "../../model/connection-status";
+
+/**
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const __ = require('oro/translator');
+const Dialog = require('pim/dialog');
+
+/**
+ * Overrides the delete Confirm modal for datagrid to check Franklin subscriptions
+ *
+ * @author Pierre Allard <pierre.allard@akeneo.com>
+ */
+class DeleteConfirm {
+  /**
+   * Returns a confirm modal
+   *
+   * @param {string} entityCode
+   * @param {any}    callback
+   * @param {string} entityHint
+   * @return {Promise}
+   */
+  public static getConfirmDialog(
+    entityCode: string,
+    callback: any,
+    entityHint: string
+  ) {
+    getConnectionStatus(false).then((connectionStatus: ConnectionStatus) => {
+      if (connectionStatus.productSubscriptionCount > 0) {
+        return Dialog.confirmDelete(
+          __(
+            'pim_enrich.entity.attribute.module.save.warning',
+            {count: connectionStatus.productSubscriptionCount},
+            connectionStatus.productSubscriptionCount,
+          ),
+          __('pim_enrich.entity.attribute.module.save.title'),
+          callback,
+          entityHint
+        );
+      }
+
+      return Dialog.confirmDelete(
+        __(`pim_enrich.entity.${entityCode}.module.delete.confirm`),
+        __('pim_common.confirm_deletion'),
+        callback,
+        entityHint
+      );
+    });
+  }
+}
+
+export = DeleteConfirm;
