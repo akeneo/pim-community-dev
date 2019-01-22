cd0b7b5c58a9819d65348aff7236db5898d2bc6a
ahocquard <alexandre.hocquard@akeneo.com>
2019-01-22 14:31:06 +0100

    make check pullup fail

---
+++ src/Pim/Bundle/ApiBundle/Toto.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Bundle\ApiBundle;
+
+/**
+ * Class Toto
+ *
+ * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Toto
+{
+}
