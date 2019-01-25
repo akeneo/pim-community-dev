c6a7822fdf67278dd1e7d69acb337697b1de9211
Samir Boulil <samir.boulil@akeneo.com>
2019-01-25 15:10:26 +0100

    PIM-8006: Add changelog

--- src/PimEnterprise/Bundle/ProductAssetBundle/tests/EndToEnd/Command/GenerateMissingVariationFilesCommandIntegration.php
+++ src/PimEnterprise/Bundle/ProductAssetBundle/tests/EndToEnd/Command/GenerateMissingVariationFilesCommandIntegration.php
@@ -16,10 +16,8 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
 /**
- *
  * @author    Samir Boulil <samir.boulil@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class GenerateMissingVariationFilesCommandIntegration extends TestCase
 {
