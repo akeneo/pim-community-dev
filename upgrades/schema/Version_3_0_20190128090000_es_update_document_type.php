476c6d8ceaa274c5040ab7e7dac3a0494937cbf8
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-01-28 15:50:19 +0100

    Update document type

---
+++ upgrades/schema/Version_3_0_20190128090000_es_update_document_type.php
@@ -0,0 +1,56 @@
+<?php
+
+if (!file_exists(__DIR__ . '/../../app/AppKernel.php')) {
+    die("Please run this command from your Symfony application root.");
+}
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../app/AppKernel.php';
+
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    (new Symfony\Component\Dotenv\Dotenv())->load($envFile);
+}
+
+$kernel = new AppKernel('prod', false);
+$kernel->boot();
+$container = $kernel->getContainer();
+
+$builder = $container->get('akeneo_elasticsearch.client_builder');
+$hosts = [$container->getParameter('index_hosts')];
+
+$client = $builder->setHosts($hosts)->build();
+
+$client->updateByQuery(
+    [
+        'index' => $container->getParameter('product_and_product_model_index_name'),
+        'type'  => 'pim_catalog_product',
+        'body'  => [
+            'script' => [
+                'inline' => "ctx._source.document_type = 'Akeneo\\\\Pim\\\\Enrichment\\\\Component\\\\Product\\\\Model\\\\ProductInterface'",
+            ],
+            'query'  => [
+                'term' => [
+                    'document_type' => "Pim\\Component\\Catalog\\Model\\ProductInterface"
+                ]
+            ]
+        ]
+    ]
+);
+
+$client->updateByQuery(
+    [
+        'index' => $container->getParameter('product_and_product_model_index_name'),
+        'type'  => 'pim_catalog_product',
+        'body'  => [
+            'script' => [
+                'inline' => "ctx._source.document_type = 'Akeneo\\\\Pim\\\\Enrichment\\\\Component\\\\Product\\\\Model\\\\ProductModelInterface'",
+            ],
+            'query'  => [
+                'term' => [
+                    'document_type' => "Pim\\Component\\Catalog\\Model\\ProductModelInterface"
+                ]
+            ]
+        ]
+    ]
+);
