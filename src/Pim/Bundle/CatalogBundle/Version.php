8bfb1e5ff1c1847d5a83980ad2cfca23f486d83b
Yohan BLAIN (phaseinducer) <yohan.blain@gmail.com>
2019-01-29 15:24:46 +0100

    Preparing version 2.3.27

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.26';
+    const VERSION = '2.3.27';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
