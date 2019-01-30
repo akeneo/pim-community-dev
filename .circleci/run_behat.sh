37ddfe8f1b15543e740e85245ce91e3a39f19613
Benoit Jacquemont <benoit@akeneo.com>
2019-01-30 17:29:14 +0100

    #9469 CircleCI configuration for 2.0 (#9454)
    *  #9469 Circle CI configuration for 2.0

---
+++ .circleci/run_behat.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# We need the TESTFILES var in $1
+TESTFILES=$@
+
+fail=0
+counter=1
+total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
+
+for TESTFILE in $TESTFILES; do
+    echo "$TESTFILE ($counter/$total):"
+    uuid=$(uuidgen)
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_${uuid} --format pretty --out std --colors -p default $TESTFILE || \
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/behat_${uuid} --format pretty --out std --colors -p default $TESTFILE
+    fail=$(($fail + $?))
+    counter=$(($counter + 1))
+done
+
+return $fail
