de838a2e81b0785e2bee8e66969238f9ebf826a8
Philippe Mossière <philippe.mossiere@akeneo.com>
2019-01-30 16:28:49 +0100

    Fix ResetHandler it fixes the reset action

--- src/Akeneo/UserManagement/Bundle/Form/Handler/ResetHandler.php
+++ src/Akeneo/UserManagement/Bundle/Form/Handler/ResetHandler.php
@@ -5,6 +5,8 @@ namespace Akeneo\UserManagement\Bundle\Form\Handler;
 use Akeneo\UserManagement\Bundle\Manager\UserManager;
 use Akeneo\UserManagement\Component\Model\UserInterface;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Overridden ResetHandler in order to manage the reset password
@@ -18,22 +20,32 @@ class ResetHandler
     /** @var FormInterface */
     private $form;
 
+    /** @var RequestStack */
+    private $requestStack;
+
     /** @var UserManager */
     private $manager;
 
-    /**
-     * @param FormInterface $form
-     * @param UserManager   $manager
-     */
-    public function __construct(FormInterface $form, UserManager $manager)
+    public function __construct(FormInterface $form, RequestStack $requestStack, UserManager $manager)
     {
         $this->form = $form;
+        $this->requestStack = $requestStack;
         $this->manager = $manager;
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    public function process(UserInterface $user): bool
+    {
+        $this->form->setData($user);
+        if (in_array($this->getRequest()->getMethod(), ['POST', 'PUT'])) {
+            $this->form->handleRequest($this->getRequest());
+            if ($this->form->isValid()) {
+                $this->onSuccess($user);
+                return true;
+            }
+        }
+        return false;
+    }
+
     protected function onSuccess(UserInterface $user)
     {
         $user
@@ -43,5 +55,10 @@ class ResetHandler
             ->setEnabled(true);
 
         $this->manager->updateUser($user);
+    }
+
+    protected function getRequest(): ?Request
+    {
+        return $this->requestStack->getCurrentRequest();
     }
 }
