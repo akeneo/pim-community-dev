b85d51c29929a3d3d0aefd4fe671a59aaf723ca7
Adrien Pétremann <hello@grena.fr>
2019-01-31 10:40:35 +0100

    Rename abstract to not be interpreted as a test

--- src/Pim/Bundle/CatalogBundle/tests/integration/Elasticsearch/IndexConfiguration/SearchAncestorsIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/Elasticsearch/IndexConfiguration/SearchAncestorsIntegration.php
@@ -9,7 +9,7 @@ namespace Pim\Bundle\CatalogBundle\tests\integration\Elasticsearch\IndexConfigur
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class SearchAncestorsIntegration extends AbstractPimCatalogProductModelIntegration
+class SearchAncestorsIntegration extends AbstractPimCatalogProductModel
 {
     public function testFindAllProductsAndProductModelsAncestorsOfGrandParentWithCode()
     {
