5d1667a5c1f8604bc787c7da374674b0ce3aafa8
Mathias Métayer <mathias.metayer@akeneo.com>
2019-01-31 11:55:51 +0100

    APAI: Fix Franklin migration scripts

--- upgrades/schema/Version_3_0_20190114153056_job_profiles_franklin.php
+++ upgrades/schema/Version_3_0_20190114153056_job_profiles_franklin.php
@@ -30,9 +30,11 @@ class Version_3_0_20190114153056_job_profiles_franklin extends AbstractMigration
         $output = new BufferedOutput();
 
         $application->setAutoExit(false);
-        $application->run($input, $output);
+        $status = $application->run($input, $output);
 
-        $this->write($output->fetch());
+        if (0 !== $status) {
+            throw new \Exception($output->fetch());
+        }
     }
 
     /**
