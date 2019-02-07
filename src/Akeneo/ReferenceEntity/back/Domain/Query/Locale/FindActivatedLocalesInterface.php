088ea9d24fe274a7f41d7a875d9c9febd583932b
christophe-chausseray <chausseray.christophe@gmail.com>
2019-02-07 15:36:01 +0100

    PIM-8030 : Update comments

--- src/Akeneo/ReferenceEntity/back/Domain/Query/Locale/FindActivatedLocalesInterface.php
+++ src/Akeneo/ReferenceEntity/back/Domain/Query/Locale/FindActivatedLocalesInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Akeneo\ReferenceEntity\Domain\Query\Locale;
 
 /**
- * Find the list of activated locale identifiers
+ * Find the list of activated locale identifiers (that's not belong to our bounded context)
  *
  * @author    Christophe Chausseray <christophe.chausseray@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
