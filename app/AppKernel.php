addddccbff846c33401a80eb830822373038e3fd
arnolanglade <arnaud.langlade@akeneo.com>
2019-02-12 15:01:12 +0100

    Update dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,6 @@ class AppKernel extends Kernel
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
