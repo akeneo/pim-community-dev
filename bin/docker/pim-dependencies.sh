af876ab76a75b9dd5d08f00d93fb32fc7d508f3b
jmleroux <jmleroux.pro@gmail.com>
2019-02-13 11:01:01 +0100

    Update composer dependencies

--- bin/docker/pim-dependencies.sh
+++ bin/docker/pim-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose exec fpm php -d memory_limit=3G /usr/local/bin/composer update && \
+docker-compose exec fpm php -d memory_limit=3G /usr/local/bin/composer install && \
 docker-compose run --rm node yarn install
