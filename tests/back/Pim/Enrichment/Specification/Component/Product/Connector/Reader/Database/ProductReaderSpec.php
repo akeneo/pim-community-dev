99802988f79e348f70038ec9eac86ae14b1f165a
Willy <willy.mesnage@akeneo.com>
2019-02-15 12:12:00 +0100

    PIM-8127: Be able to save and launch job even if products filters are broken

--- tests/back/Pim/Enrichment/Specification/Component/Product/Connector/Reader/Database/ProductReaderSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Connector/Reader/Database/ProductReaderSpec.php
@@ -5,7 +5,9 @@ namespace Specification\Akeneo\Pim\Enrichment\Component\Product\Connector\Reader
 use Akeneo\Channel\Component\Model\ChannelInterface;
 use Akeneo\Channel\Component\Repository\ChannelRepositoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Converter\MetricConverter;
+use Akeneo\Pim\Enrichment\Component\Product\Exception\ObjectNotFoundException;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\Operators;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderFactoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
 use Akeneo\Tool\Component\Batch\Job\JobParameters;
@@ -83,9 +85,12 @@ class ProductReaderSpec extends ObjectBehavior
         $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
         $channel->getCode()->willReturn('mobile');
 
-        $pqbFactory->create(['filters' => $filters['data'], 'default_scope' => 'mobile'])
+        $pqbFactory->create(['default_scope' => 'mobile'])
             ->shouldBeCalled()
             ->willReturn($pqb);
+        $pqb->addFilter('enabled', Operators::EQUALS, true, [])->shouldBeCalled();
+        $pqb->addFilter('family', Operators::IN_LIST, ['camcorder'], [])->shouldBeCalled();
+        $pqb->addFilter('completeness', Operators::GREATER_OR_EQUAL_THAN, 100, [])->shouldBeCalled();
         $pqb->execute()
             ->shouldBeCalled()
             ->willReturn($cursor);
@@ -144,9 +149,10 @@ class ProductReaderSpec extends ObjectBehavior
         $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
         $channel->getCode()->willReturn('mobile');
 
-        $pqbFactory->create(['filters' => $filters['data'], 'default_scope' => 'mobile'])
+        $pqbFactory->create(['default_scope' => 'mobile'])
             ->shouldBeCalled()
             ->willReturn($pqb);
+        $pqb->addFilter('completeness', Operators::GREATER_OR_EQUAL_THAN, 100, [])->shouldBeCalled();
         $pqb->execute()
             ->shouldBeCalled()
             ->willReturn($cursor);
@@ -187,7 +193,7 @@ class ProductReaderSpec extends ObjectBehavior
             'data' => [
                 [
                     'field'    => 'completeness',
-                    'operator' => 'ALL',
+                    'operator' => '>=',
                     'value'    => 100
                 ]
             ],
@@ -206,9 +212,77 @@ class ProductReaderSpec extends ObjectBehavior
         $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
         $channel->getCode()->willReturn('mobile');
 
-        $pqbFactory->create(['filters' => $filters['data'], 'default_scope' => 'mobile'])
+        $pqbFactory->create(['default_scope' => 'mobile'])
             ->shouldBeCalled()
             ->willReturn($pqb);
+        $pqb->addFilter('completeness', Operators::GREATER_OR_EQUAL_THAN, 100, [])->shouldBeCalled();
+        $pqb->execute()
+            ->shouldBeCalled()
+            ->willReturn($cursor);
+
+        $cursor->valid()->will(
+            function () use (&$productsCount) {
+                return $productsCount-- > 0;
+            }
+        );
+        $cursor->current()->will(new ReturnPromise($products));
+        $cursor->next()->shouldBeCalled();
+
+        $stepExecution->incrementSummaryInfo('read')->shouldBeCalledTimes(3);
+        $metricConverter->convert(Argument::any(), $channel)->shouldBeCalledTimes(3);
+
+        $this->initialize();
+        $this->read()->shouldReturn($product1);
+        $this->read()->shouldReturn($product2);
+        $this->read()->shouldReturn($product3);
+        $this->read()->shouldReturn(null);
+    }
+
+    function it_ignores_object_not_found_in_filter(
+        $pqbFactory,
+        $channelRepository,
+        $metricConverter,
+        $stepExecution,
+        ChannelInterface $channel,
+        ProductQueryBuilderInterface $pqb,
+        CursorInterface $cursor,
+        ProductInterface $product1,
+        ProductInterface $product2,
+        ProductInterface $product3,
+        JobParameters $jobParameters
+    ) {
+        $filters = [
+            'data' => [
+                [
+                    'field'    => 'family',
+                    'operator' => 'IN',
+                    'value'    => [
+                        'does_not_exist'
+                    ]
+                ]
+            ],
+            'structure' => [
+                'scope'   => 'mobile',
+                'locales' => ['fr_FR', 'en_US'],
+            ]
+        ];
+
+        $products = [$product1, $product2, $product3];
+        $productsCount = count($products);
+
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+        $jobParameters->get('filters')->willReturn($filters);
+
+        $channelRepository->findOneByIdentifier('mobile')->willReturn($channel);
+        $channel->getCode()->willReturn('mobile');
+
+        $pqbFactory->create(['default_scope' => 'mobile'])
+            ->shouldBeCalled()
+            ->willReturn($pqb);
+        $pqb
+            ->addFilter('family', Operators::IN_LIST, ['does_not_exist'], [])
+            ->shouldBeCalled()
+            ->willThrow(new ObjectNotFoundException());
         $pqb->execute()
             ->shouldBeCalled()
             ->willReturn($cursor);
