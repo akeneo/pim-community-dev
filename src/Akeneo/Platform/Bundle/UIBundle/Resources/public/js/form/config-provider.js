4a0ebac781cde4902bf322ffab44c8427a45368e
Tamara Robichet <tamara.robichet@akeneo.com>
2019-02-19 17:15:07 +0100

    Filter by ACL on the frontend

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/form/config-provider.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/form/config-provider.js
@@ -1,16 +1,30 @@
 'use strict';
 
 define(
-    ['jquery', 'underscore', 'routing'],
-    function ($, _, Routing) {
+    ['jquery', 'underscore', 'pim/security-context'],
+    function ($, _, SecurityContext) {
         var promise = null;
+
+        const filterByGranted = (extensions) => {
+            const filtered = _.filter(extensions, extension => {
+                return null === extension.aclResourceId || SecurityContext.isGranted(extension.aclResourceId)
+            });
+
+            return filtered
+        }
 
         var loadConfig = function () {
             if (null === promise) {
-                promise = $.get('/js/extensions.json').fail(() => {
-                    throw Error('It seems that your web server is not well configured as we were not able ' +
-                        'to load the frontend configuration. The most likely reason is that the mod_rewrite ' +
-                        'module is not installed/enabled.')
+                promise = $.when(
+                    $.get('/js/extensions.json'),
+                    SecurityContext.initialize()
+                ).then((formExtensions) => {
+                    const test = formExtensions[0]
+                    test.extensions = filterByGranted(test.extensions);
+
+                    return test;
+                }).fail(() => {
+                    throw Error('It seems that your web server is not well configured as we were not able to load the frontend configuration. The most likely reason is that the mod_rewrite module is not installed/enabled.')
                 });
             }
 
@@ -25,7 +39,6 @@ define(
              */
             getExtensionMap: function () {
                 return loadConfig().then(function (config) {
-                    console.log(Object.values(config.extensions))
                     return Object.values(config.extensions);
                 });
             },
