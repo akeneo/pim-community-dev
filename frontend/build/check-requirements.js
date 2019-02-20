5f21ad88dcbdb921d3ece21f3e0134adfd12c603
Tamara Robichet <tamara.robichet@akeneo.com>
2019-02-20 11:40:28 +0100

    Cleanup

--- frontend/build/check-requirements.js
+++ frontend/build/check-requirements.js
@@ -2,7 +2,6 @@ require('colors')
 const { existsSync } = require('fs')
 const BUNDLE_REQUIRE_PATH = './web/js/require-paths.js'
 const ROUTES_PATH = './web/js/routes.js'
-const EXTENSIONS_PATH = './web/js/extensions.json'
 
 console.log('Checking PIM frontend requirements'.blue)
 
@@ -13,10 +12,5 @@ if (!existsSync(BUNDLE_REQUIRE_PATH)) {
 
 if (!existsSync(ROUTES_PATH)) {
     console.log(`${ROUTES_PATH} does not exist - Run "bin/console --ansi fos:js-routing:dump --target=web/js/routes.js" and try again.`.red)
-    process.exit(1)
-}
-
-if (!existsSync(EXTENSIONS_PATH)) {
-    console.log(`${EXTENSIONS_PATH} does not exist - Run "yarn update-extensions" and try again.`.red)
     process.exit(1)
 }
