4943fe12590b5578ca00aa81eba673aeb4b1b0f1
jmleroux <jmleroux.pro@gmail.com>
2019-02-25 11:09:48 +0100

    Preparing version 3.0.5

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.4';
+    const VERSION = '3.0.5';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Super-Rabbit';
