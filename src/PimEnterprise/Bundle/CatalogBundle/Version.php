4912442494fc0c9edf6861c4e7a15fa7eb128da4
jmleroux <jmleroux.pro@gmail.com>
2019-02-25 14:50:23 +0100

    Preparing version 2.0.49

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.0.48';
+    const VERSION = '2.0.49';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hazel';
