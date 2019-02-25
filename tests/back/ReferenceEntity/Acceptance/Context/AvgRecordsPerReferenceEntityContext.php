07e51226a6dff1047b7d93ca02fa9fabe3acb308
Samir Boulil <samir.boulil@akeneo.com>
2019-02-25 15:14:16 +0100

    PIM-7863: Add acceptance tests

---
+++ tests/back/ReferenceEntity/Acceptance/Context/AvgRecordsPerReferenceEntityContext.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkeneoTestEnterprise\ReferenceEntity\Acceptance\Context;
+
+use AkeneoTest\Platform\Acceptance\CatalogVolumeMonitoring\Context\ReportContext;
+use AkeneoTest\Platform\Acceptance\CatalogVolumeMonitoring\Persistence\Query\InMemory\InMemoryCountQuery;
+use Behat\Behat\Context\Context;
+use Webmozart\Assert\Assert;
+
+final class AvgRecordsPerReferenceEntityContext implements Context
+{
+    /** @var ReportContext */
+    private $reportContext;
+
+    /** @var InMemoryCountQuery */
+    private $inMemoryQuery;
+
+    /**
+     * @param ReportContext      $reportContext
+     * @param InMemoryCountQuery $inMemoryQuery
+     */
+    public function __construct(ReportContext $reportContext, InMemoryCountQuery $inMemoryQuery)
+    {
+        $this->reportContext = $reportContext;
+        $this->inMemoryQuery = $inMemoryQuery;
+    }
+
+    /**
+     * @Given a catalog with on average :recordsPerReferenceEntity records per reference entity
+     */
+    public function aCatalogWithReferenceEntities(int $recordsPerReferenceEntity): void
+    {
+        $this->inMemoryQuery->setVolume($recordsPerReferenceEntity);
+    }
+
+    /**
+     * @Given the average limit of records per reference entity is set to :limit
+     */
+    public function theLimitOfRecordsPerReferenceEntityIsSetTo(int $limit): void
+    {
+        $this->inMemoryQuery->setLimit($limit);
+    }
+
+    /**
+     * @Then the report returns that the average number of records per reference entityis :recordsPerReferenceEntity
+     */
+    public function theReportReturnsThatTheRecordsPerReferenceEntityIs(int $recordsPerReferenceEntity): void
+    {
+        $volumes = $this->reportContext->getVolumes();
+
+        Assert::eq($recordsPerReferenceEntity, $volumes['average_records_per_reference_entities']['value']);
+    }
+
+    /**
+     * @Then the report warns the users that the average number of records per reference entity is high
+     */
+    public function theReportWarnsTheUsersThatTheRecordsPerReferenceEntityIsHigh(): void
+    {
+        $volumes = $this->reportContext->getVolumes();
+
+        Assert::true($volumes['average_records_per_reference_entities']['has_warning']);
+    }
+
+    /**
+     * @Then the report does not warn the users that the average number of records per reference entity is high
+     */
+    public function theReportDoesNotWarnTheUsersThatTherecordsPerReferenceEntityIsHigh(): void
+    {
+        $volumes = $this->reportContext->getVolumes();
+
+        Assert::false($volumes['average_records_per_reference_entities']['has_warning']);
+    }
+}
