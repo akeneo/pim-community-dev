ec6f01527fb6f24ef1802d00a1da02d58f10a9fc
Tamara Robichet <tamarasaurus@users.noreply.github.com>
2019-02-26 13:50:19 +0100

    Remove unused js and twig extensions (#9661)

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/layout.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/layout.js
@@ -1,34 +1,5 @@
 define(['jquery', 'bootstrap', 'jquery-ui'], function ($) {
     'use strict';
-
-    // todo: remove this or move somewhere else
-    /**
-     * Fix for IE8 compatibility
-     */
-    if (!Date.prototype.toISOString) {
-        (function () {
-            function pad(number) {
-                var r = String(number);
-                if (r.length === 1) {
-                    r = '0' + r;
-                }
-
-                return r;
-            }
-
-            Date.prototype.toISOString = function () {
-                return this.getUTCFullYear() +
-                    '-' + pad(this.getUTCMonth() + 1) +
-                    '-' + pad(this.getUTCDate()) +
-                    'T' + pad(this.getUTCHours()) +
-                    ':' + pad(this.getUTCMinutes()) +
-                    ':' + pad(this.getUTCSeconds()) +
-                    '.' + String((this.getUTCMilliseconds() / 1000).toFixed(3)).slice(2, 5) +
-                    'Z';
-            };
-        }());
-    }
-
     var layout = {};
 
     layout.init = function (container) {
