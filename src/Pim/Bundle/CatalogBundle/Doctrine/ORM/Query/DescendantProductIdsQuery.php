e0b6a4a554d5bfa4d9e79dd8de6ae37237a1e361
romain <romain@akeneo.com>
2019-03-06 23:50:33 +0100

    PIM-8187: Adds queries to get descendants from product models

---
+++ src/Pim/Bundle/CatalogBundle/Doctrine/ORM/Query/DescendantProductIdsQuery.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Bundle\CatalogBundle\Doctrine\ORM\Query;
+
+use Doctrine\DBAL\Connection;
+use Pim\Component\Catalog\ProductModel\Query\DescendantProductIdsQueryInterface;
+
+/**
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class DescendantProductIdsQuery implements DescendantProductIdsQueryInterface
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function fetchFromProductModelIds(array $productModelIds): array
+    {
+        if (empty($productModelIds)) {
+            return [];
+        }
+
+        $sql = <<<SQL
+SELECT id FROM pim_catalog_product
+WHERE product_model_id IN (:productModelIds)
+SQL;
+
+        $resultRows = $this->connection->executeQuery(
+            $sql,
+            ['productModelIds' => $productModelIds],
+            ['productModelIds' => Connection::PARAM_INT_ARRAY]
+        )->fetchAll();
+
+        return array_map(function ($rowData) {
+            return (int) $rowData['id'];
+        }, $resultRows);
+    }
+}
