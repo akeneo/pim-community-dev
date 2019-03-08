fbc8a1ef113d7bd3df2ebf43f568388c00cd2363
romain <romain@akeneo.com>
2019-03-08 17:26:53 +0100

    PIM-8187: Improve integration test

--- src/Pim/Bundle/CatalogBundle/tests/integration/Doctrine/ORM/Query/DescendantProductIdsIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/Doctrine/ORM/Query/DescendantProductIdsIntegration.php
@@ -13,17 +13,29 @@ class DescendantProductIdsIntegration extends TestCase
     {
         $query = $this->get('pim_catalog.query.descendant_product_ids');
 
-        $resultRows = $query->fetchFromProductModelIds([1]);
-        $this->assertCount(3, $resultRows);
-        $this->assertSame([1, 2, 3], $resultRows);
+        $productModel = $this->get('pim_catalog.repository.product_model')->findOneByIdentifier('amor');
+        $productModel2 = $this->get('pim_catalog.repository.product_model')->findOneByIdentifier('aphrodite');
 
-        $resultRows = $query->fetchFromProductModelIds([1, 2]);
-        $this->assertCount(8, $resultRows);
-        $this->assertSame([1, 2, 3, 4, 5, 6, 7, 8], $resultRows);
+        $expectedProductIds = array_map(function ($product) {
+            return (int) $product->getId();
+        }, $productModel->getProducts()->toArray());
 
+        $resultRows = $query->fetchFromProductModelIds([$productModel->getId()]);
+        $this->assertCount(count($expectedProductIds), $resultRows);
+        $this->assertSame($expectedProductIds, $resultRows);
+
+        $products = array_merge($productModel->getProducts()->toArray(), $productModel2->getProducts()->toArray());
+        $expectedProductIds = array_map(function ($product) {
+            return (int) $product->getId();
+        }, $products);
+        
+        $resultRows = $query->fetchFromProductModelIds([$productModel->getId(), $productModel2->getId()]);
+        $this->assertCount(count($expectedProductIds), $resultRows);
+        $this->assertSame($expectedProductIds, $resultRows);
+
+        $this->assertNull($this->get('pim_catalog.repository.product_model')->find(81));
         $resultRows = $query->fetchFromProductModelIds([81]);
         $this->assertCount(0, $resultRows);
-        $this->assertSame([], $resultRows);
     }
 
     /**
