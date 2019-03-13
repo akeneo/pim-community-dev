d61eca0966f9d3cf557ff08247a1f2d44f2ef4f1
Damien Carcel <damien.carcel@akeneo.com>
2019-03-13 16:11:27 +0100

    AOB-404: Store data in Google Cloud Storage Create a dedicate bundle and configure oneup adapters in it

---
+++ src/Poc/Bundle/StorageBundle/PocStorageBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Poc\Bundle\StorageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author    Damien Carcel <damien.carcel@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PocStorageBundle extends Bundle
+{
+}
