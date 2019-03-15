88cf8c4947f38fd2376f546b37ed61eaaa467b92
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2019-03-15 09:35:35 +0100

    Fix test

--- tests/back/Platform/Integration/Version/VersionProviderIntegration.php
+++ tests/back/Platform/Integration/Version/VersionProviderIntegration.php
@@ -13,7 +13,7 @@ class VersionProviderIntegration extends KernelTestCase
     /**
      * {@inheritdoc}
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         static::bootKernel(['debug' => false]);
     }
