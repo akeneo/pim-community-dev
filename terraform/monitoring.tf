5d6dbd8a654b2e852f7bfa8d80387e61c19059ca
franckbrouard <48205166+franckbrouard@users.noreply.github.com>
2019-03-15 16:35:06 +0100

    [CLOUD-972] Disk policy (#103)
    * add policy mysql, nfs and elasticsearch in terraform
    
    * add policy mysql, nfs and elasticsearch in terraform - change param
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add param projet
    
    * add policy mysql, nfs and elasticsearch in terraform - add comments
    
    * Update infra/terraform/monitoring.tf
    
    Co-Authored-By: franckbrouard <48205166+franckbrouard@users.noreply.github.com>
    
    * Update infra/terraform/monitoring.tf
    
    Co-Authored-By: franckbrouard <48205166+franckbrouard@users.noreply.github.com>
    
    * Update infra/terraform/monitoring.tf
    
    Co-Authored-By: franckbrouard <48205166+franckbrouard@users.noreply.github.com>
    
    * add comments - tests next to reviews
    
    * add comments - tests next to reviews
    
    * add comments - tests next to reviews
    
    * add comments - tests next to reviews
    
    * add comments - tests next to reviews
    
    * change CHANGELOG & UPGRADE
    
    * upgrade UPGRADE file
    
    * change filter syntax
    
    * change UPGRADE.md with pim source details
    
    * Update UPGRADE.md
    
    Co-Authored-By: franckbrouard <48205166+franckbrouard@users.noreply.github.com>
    
    * change UPGRADE.md with pim source details
    
    * change UPGRADE.md with new order
    
    * change terraform dependencies

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -21,6 +21,119 @@ resource "local_file" "metric-rendered" {
   filename = "metrics/metric-${local.metrics[count.index]}.json"
 }
 
+resource "google_monitoring_uptime_check_config" "https" {
+  display_name = "${replace(var.dns_external, "/\\.$/", "")}"
+  timeout      = "30s"
+  period       = "60s"
+  project      = "${var.google_project_name}"
+
+  http_check = {
+    path    = "/user/login"
+    port    = "443"
+    use_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+
+    labels = {
+      project_id = "${var.google_project_name}"
+      host       = "${replace(var.dns_external, "/\\.$/", "")}"
+    }
+  }
+}
+
+resource "google_monitoring_notification_channel" "pagerduty" {
+  project      = "${var.google_project_name}"
+  display_name = "Notification channel of ${var.pfid}"
+  type         = "pagerduty"
+
+  labels = {
+    service_key = "${var.pager_duty_service_key}"
+  }
+}
+
+resource "google_monitoring_alert_policy" "alert_policy" {
+  display_name = "${var.pfid}"
+  combiner = "OR"
+  project = "${var.google_project_name}"
+  depends_on = ["null_resource.metric"]
+  conditions = [
+  # Basically it should ring if the volume utilization is > 90% during 15min
+  {
+    display_name = "Volume utilization for mysql of ${var.pfid}"
+    condition_threshold {
+      filter = "metric.type=\"kubernetes.io/pod/volume/utilization\" AND resource.type=\"k8s_pod\" AND resource.label.namespace_name=\"${var.pfid}\" AND resource.label.pod_name=monitoring.regex.full_match(\"^${var.pfid}-mysql-server[a-z0-9-]*$\") AND metric.label.volume_name=\"pvc-data\""
+      duration = "900s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 0.9
+      aggregations {
+        alignment_period = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  },
+  {
+    display_name = "Volume utilization for elasticsearch of ${var.pfid}"
+    condition_threshold {
+      filter = "metric.type=\"kubernetes.io/pod/volume/utilization\" AND resource.type=\"k8s_pod\" AND resource.label.namespace_name=\"${var.pfid}\" AND resource.label.pod_name=monitoring.regex.full_match(\"^elasticsearch-[a-z0-9-]*$\") AND metric.label.volume_name=\"data\""
+      duration = "900s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 0.9
+      aggregations {
+        alignment_period = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  },
+  {
+    display_name = "Volume utilization for NFS of ${var.pfid}"
+    condition_threshold {
+      filter = "metric.type=\"kubernetes.io/pod/volume/utilization\" AND resource.type=\"k8s_pod\" AND resource.label.namespace_name=\"${var.pfid}\" AND resource.label.pod_name=monitoring.regex.full_match(\"^${var.pfid}-nfs-[a-z0-9-]*$\") AND metric.label.volume_name=\"data\""
+      duration = "900s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 0.9
+      aggregations {
+        alignment_period = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  },
+  # Basically it should ring if the uptime don't respond or the response time is > 4sec for 15 minutes
+  {
+    display_name = "Response time of successful requests on /user/login of ${var.pfid}"
+    condition_threshold {
+      filter          = "metric.type=\"logging.googleapis.com/user/${var.pfid}-login-response-time-distribution\" AND resource.type=k8s_container AND metric.label.response_code=200"
+      duration        = "900s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 4000000
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner   = "ALIGN_SUM"
+        cross_series_reducer = "REDUCE_PERCENTILE_95"
+      }
+    }
+  },
+  {
+    display_name = "Uptime Health Check on /user/login of ${var.pfid}"
+    condition_threshold {
+      filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" AND resource.type=uptime_url AND resource.label.host=\"${replace(var.dns_external, "/\\.$/", "")}\""
+      duration        = "900s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner   = "ALIGN_NEXT_OLDER"
+        cross_series_reducer = "REDUCE_COUNT_FALSE"
+      }
+    }
+  },
+  ]
+  notification_channels = [
+    "${google_monitoring_notification_channel.pagerduty.name}",
+  ]
+}
+
 resource "null_resource" "metric" {
   count = "${length(local.metrics)}"
 
@@ -45,89 +158,5 @@ EOF
         --project ${var.google_project_name} \
         --quiet
 EOF
-  }
-}
-
-resource "google_monitoring_uptime_check_config" "https" {
-  display_name = "${replace(var.dns_external, "/\\.$/", "")}"
-  timeout      = "30s"
-  period       = "60s"
-  project      = "${var.google_project_name}"
-
-  http_check = {
-    path    = "/user/login"
-    port    = "443"
-    use_ssl = true
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-
-    labels = {
-      project_id = "${var.google_project_name}"
-      host       = "${replace(var.dns_external, "/\\.$/", "")}"
-    }
-  }
-}
-
-# Basically it should ring if the uptime don't respond or the response time is > 4sec for 15 minutes
-resource "google_monitoring_alert_policy" "availability" {
-  display_name = "Availability of the login page of ${var.pfid}"
-  combiner     = "OR"
-  enabled      = true
-  project      = "${var.google_project_name}"
-  depends_on   = ["null_resource.metric"]
-
-  conditions = [
-    {
-      display_name = "Response time of successful requests on /user/login of ${var.pfid}"
-
-      condition_threshold {
-        filter          = "metric.type=\"logging.googleapis.com/user/${var.pfid}-login-response-time-distribution\" AND resource.type=k8s_container AND metric.label.response_code=200"
-        duration        = "900s"
-        comparison      = "COMPARISON_GT"
-        threshold_value = 4000000
-
-        aggregations = [
-          {
-            alignment_period     = "60s"
-            per_series_aligner   = "ALIGN_SUM"
-            cross_series_reducer = "REDUCE_PERCENTILE_95"
-          },
-        ]
-      }
-    },
-    {
-      display_name = "Uptime Health Check on /user/login of ${var.pfid}"
-
-      condition_threshold {
-        filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" AND resource.type=uptime_url AND resource.label.host=\"${replace(var.dns_external, "/\\.$/", "")}\""
-        duration        = "900s"
-        comparison      = "COMPARISON_GT"
-        threshold_value = 0
-
-        aggregations = [
-          {
-            alignment_period     = "60s"
-            per_series_aligner   = "ALIGN_NEXT_OLDER"
-            cross_series_reducer = "REDUCE_COUNT_FALSE"
-          },
-        ]
-      }
-    },
-  ]
-
-  notification_channels = [
-    "${google_monitoring_notification_channel.default.name}",
-  ]
-}
-
-resource "google_monitoring_notification_channel" "default" {
-  project      = "${var.google_project_name}"
-  display_name = "Notification channel of ${var.pfid}"
-  type         = "pagerduty"
-
-  labels = {
-    service_key = "${var.pager_duty_service_key}"
   }
 }
