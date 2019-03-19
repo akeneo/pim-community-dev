e1cea901349c9365e4a93f26d11014431cb7834a
christophe-chausseray <chausseray.christophe@gmail.com>
2019-03-19 15:05:30 +0100

    PIM-8181: Add single option and multiple option in the record grid

---
+++ src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option-collection.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {NormalizedValue} from 'akeneoreferenceentity/domain/model/record/value';
+import {CellView} from 'akeneoreferenceentity/application/configuration/value';
+import {Column} from 'akeneoreferenceentity/application/reducer/grid';
+import {NormalizedOption} from 'akeneoreferenceentity/domain/model/attribute/type/option/option';
+import {NormalizedOptionCollectionAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/option-collection';
+
+const memo = (React as any).memo;
+
+const OptionCollectionCellView: CellView = memo(({column, value}: {column: Column, value: NormalizedValue}) => {
+  const selectedOptionCollectionCode = value.data;
+  const normalizedOptionCollectionAttribute = column.attribute as NormalizedOptionCollectionAttribute;
+  const selectedOptionCollectionLabel = normalizedOptionCollectionAttribute.options
+    .filter((option: NormalizedOption) => selectedOptionCollectionCode.includes(option.code))
+    .map((selectedOption: NormalizedOption) => {
+      return selectedOption.labels[column.locale] ? selectedOption.labels[column.locale] : `[${selectedOption.code}]`;
+    })
+    .join(', ');
+
+  return (
+    <div className="AknGrid-bodyCellContainer" title={selectedOptionCollectionLabel}>
+      {selectedOptionCollectionLabel}
+    </div>
+  );
+});
+
+export const cell = OptionCollectionCellView;
---
+++ src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {NormalizedValue} from 'akeneoreferenceentity/domain/model/record/value';
+import {CellView} from 'akeneoreferenceentity/application/configuration/value';
+import {Column} from 'akeneoreferenceentity/application/reducer/grid';
+import {NormalizedOption} from 'akeneoreferenceentity/domain/model/attribute/type/option/option';
+import {NormalizedOptionAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/option';
+
+const memo = (React as any).memo;
+
+const OptionCellView: CellView = memo(({column, value}: {column: Column, value: NormalizedValue}) => {
+  const selectedOptionCode = value.data;
+  const normalizedOptionAttribute = column.attribute as NormalizedOptionAttribute;
+  const selectedOption = normalizedOptionAttribute.options.find((option: NormalizedOption) => option.code === selectedOptionCode);
+  const selectedOptionLabel = (selectedOption !== undefined && selectedOption.labels[column.locale]) ?
+    selectedOption.labels[column.locale] :
+    `[${selectedOptionCode}]`
+  ;
+
+  return (
+    <div className="AknGrid-bodyCellContainer" title={selectedOptionLabel}>
+      {selectedOptionLabel}
+    </div>
+  );
+});
+
+export const cell = OptionCellView;
