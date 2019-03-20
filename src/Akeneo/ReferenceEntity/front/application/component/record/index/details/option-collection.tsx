dddd0fa28b7137874c32e218ca06d2ca17c6bc47
christophe-chausseray <chausseray.christophe@gmail.com>
2019-03-20 11:33:18 +0100

    PIM-8181: Fix comments

--- src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option-collection.tsx
+++ src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option-collection.tsx
@@ -4,22 +4,23 @@ import {CellView} from 'akeneoreferenceentity/application/configuration/value';
 import {Column} from 'akeneoreferenceentity/application/reducer/grid';
 import {NormalizedOption} from 'akeneoreferenceentity/domain/model/attribute/type/option/option';
 import {NormalizedOptionCollectionAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/option-collection';
+import {getLabel} from 'pimui/js/i18n';
 
 const memo = (React as any).memo;
 
 const OptionCollectionCellView: CellView = memo(({column, value}: {column: Column; value: NormalizedValue}) => {
-  const selectedOptionCollectionCode = value.data;
+  const selectedOptionCollectionCodes = value.data;
   const normalizedOptionCollectionAttribute = column.attribute as NormalizedOptionCollectionAttribute;
-  const selectedOptionCollectionLabel = normalizedOptionCollectionAttribute.options
-    .filter((option: NormalizedOption) => selectedOptionCollectionCode.includes(option.code))
+  const selectedOptionCollectionLabels = normalizedOptionCollectionAttribute.options
+    .filter((option: NormalizedOption) => selectedOptionCollectionCodes.includes(option.code))
     .map((selectedOption: NormalizedOption) => {
-      return selectedOption.labels[column.locale] ? selectedOption.labels[column.locale] : `[${selectedOption.code}]`;
+      return getLabel(selectedOption.labels, column.locale, selectedOption.code);
     })
     .join(', ');
 
   return (
-    <div className="AknGrid-bodyCellContainer" title={selectedOptionCollectionLabel}>
-      {selectedOptionCollectionLabel}
+    <div className="AknGrid-bodyCellContainer" title={selectedOptionCollectionLabels}>
+      {selectedOptionCollectionLabels}
     </div>
   );
 });
--- src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option.tsx
+++ src/Akeneo/ReferenceEntity/front/application/component/record/index/details/option.tsx
@@ -4,19 +4,21 @@ import {CellView} from 'akeneoreferenceentity/application/configuration/value';
 import {Column} from 'akeneoreferenceentity/application/reducer/grid';
 import {NormalizedOption} from 'akeneoreferenceentity/domain/model/attribute/type/option/option';
 import {NormalizedOptionAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/option';
+import {getLabel} from 'pimui/js/i18n';
 
 const memo = (React as any).memo;
 
 const OptionCellView: CellView = memo(({column, value}: {column: Column; value: NormalizedValue}) => {
   const selectedOptionCode = value.data;
   const normalizedOptionAttribute = column.attribute as NormalizedOptionAttribute;
-  const selectedOption = normalizedOptionAttribute.options.find(
+  const selectedOption = normalizedOptionAttribute.options.filter(
     (option: NormalizedOption) => option.code === selectedOptionCode
   );
-  const selectedOptionLabel =
-    selectedOption !== undefined && selectedOption.labels[column.locale]
-      ? selectedOption.labels[column.locale]
-      : `[${selectedOptionCode}]`;
+  // const selectedOptionLabel =
+  //   selectedOption !== undefined && selectedOption.labels[column.locale]
+  //     ? selectedOption.labels[column.locale]
+  //     : `[${selectedOptionCode}]`;
+  const selectedOptionLabel = getLabel(selectedOption[0].labels, column.locale, selectedOption[0].code);
 
   return (
     <div className="AknGrid-bodyCellContainer" title={selectedOptionLabel}>
