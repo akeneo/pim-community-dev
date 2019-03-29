bee19995531477fa483ec5d29373b0089b627220
franckbrouard <48205166+franckbrouard@users.noreply.github.com>
2019-03-29 18:05:20 +0100

    Cloud 1006 - Add triggers on mailgun (Terraform) (#115)
    * add policy mysql, nfs and elasticsearch in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,13 +17,25 @@ resource "random_string" "mailgun_password" {
   special = false
 }
 
+locals {
+  mailgun_login_email = "${data.template_file.mailgun_login.rendered}@${var.mailgun_domain}"
+}
+
 resource "null_resource" "mailgun_credential" {
+
+  triggers {
+    mailgun_password = "${random_string.mailgun_password.result}",
+    mailgun_login_email = "${local.mailgun_login_email}"
+    mailgun_domain = "${var.mailgun_domain}"
+  }
+
   provisioner "local-exec" {
     command = <<EOF
 curl -s --user 'api:${var.mailgun_api_key}' \
 		https://api.mailgun.net/v3/domains/${var.mailgun_domain}/credentials \
-		-F login='${data.template_file.mailgun_login.rendered}@${var.mailgun_domain}' \
+		-F login='${local.mailgun_login_email}' \
 		-F password='${random_string.mailgun_password.result}' ; \
+ \
 EOF
   }
 
@@ -31,8 +43,10 @@ EOF
     when = "destroy"
 
     command = <<EOF
+# If you've changed mailgun_email, this command will fail
+# Thereby, you should first do a terraform destroy of this resource with the previous mailgun_email value
 curl -s --user 'api:${var.mailgun_api_key}' -X DELETE \
-		https://api.mailgun.net/v3/domains/${var.mailgun_domain}/credentials/${data.template_file.mailgun_login.rendered}@${var.mailgun_domain}
+		https://api.mailgun.net/v3/domains/${var.mailgun_domain}/credentials/${local.mailgun_login_email}
 EOF
   }
 }
