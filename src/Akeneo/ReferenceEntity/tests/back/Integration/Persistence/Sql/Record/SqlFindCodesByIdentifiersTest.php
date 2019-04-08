b7a14a1ae25f3857cc0a02f172191d5df5dfe474
Adrien Pétremann <hello@grena.fr>
2019-04-08 10:25:34 +0200

    PIM-8183: add QF to retrieve codes by ids

---
+++ src/Akeneo/ReferenceEntity/tests/back/Integration/Persistence/Sql/Record/SqlFindCodesByIdentifiersTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\ReferenceEntity\Integration\Persistence\Sql\Record;
+
+use Akeneo\ReferenceEntity\Domain\Model\Attribute\AttributeIdentifier;
+use Akeneo\ReferenceEntity\Domain\Model\Image;
+use Akeneo\ReferenceEntity\Domain\Model\LocaleIdentifier;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Record;
+use Akeneo\ReferenceEntity\Domain\Model\Record\RecordCode;
+use Akeneo\ReferenceEntity\Domain\Model\Record\RecordIdentifier;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Value\ChannelReference;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Value\LocaleReference;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Value\TextData;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Value\Value;
+use Akeneo\ReferenceEntity\Domain\Model\Record\Value\ValueCollection;
+use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntity;
+use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
+use Akeneo\ReferenceEntity\Domain\Query\Record\FindCodesByIdentifiersInterface;
+use Akeneo\ReferenceEntity\Integration\SqlIntegrationTestCase;
+
+class SqlFindCodesByIdentifiersTest extends SqlIntegrationTestCase
+{
+    /** @var FindCodesByIdentifiersInterface */
+    private $query;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->query = $this->get('akeneo_referenceentity.infrastructure.persistence.query.find_codes_by_identifiers');
+        $this->resetDB();
+        $this->loadReferenceEntityDesigner();
+        $this->loadRecords();
+    }
+
+    private function resetDB(): void
+    {
+        $this->get('akeneoreference_entity.tests.helper.database_helper')->resetDatabase();
+    }
+
+    /**
+     * @test
+     */
+    public function it_finds_record_codes_given_their_identifiers()
+    {
+
+    }
+
+    private function loadReferenceEntityDesigner(): void
+    {
+        $referenceEntityRepository = $this->get('akeneo_referenceentity.infrastructure.persistence.repository.reference_entity');
+        $referenceEntity = ReferenceEntity::create(
+            ReferenceEntityIdentifier::fromString('designer'),
+            [
+                'fr_FR' => 'Concepteur',
+                'en_US' => 'Designer',
+            ],
+            Image::createEmpty()
+        );
+        $referenceEntityRepository->create($referenceEntity);
+    }
+
+    public function loadRecords(): void
+    {
+        $recordRepository = $this->get('akeneo_referenceentity.infrastructure.persistence.repository.record');
+        $designerIdentifier = ReferenceEntityIdentifier::fromString('designer');
+
+        $starkCode = RecordCode::fromString('starck');
+        $starkIdentifier = RecordIdentifier::create('designer', 'stark', 'fingerprint');
+        $recordRepository->create(
+            Record::create(
+                $starkIdentifier,
+                $designerIdentifier,
+                $starkCode,
+                ValueCollection::fromValues([])
+            )
+        );
+
+        $jacobsCode = RecordCode::fromString('jacobs');
+        $jacobsIdentifier = RecordIdentifier::create('designer', 'jacobs', 'fingerprint');
+        $recordRepository->create(
+            Record::create(
+                $jacobsIdentifier,
+                $designerIdentifier,
+                $jacobsCode,
+                ValueCollection::fromValues([])
+            )
+        );
+    }
+}
