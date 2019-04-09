1cc2f2fd2a77a55897a6f9452e63eb3f925b39af
christophe-chausseray <chausseray.christophe@gmail.com>
2019-04-09 14:32:58 +0200

    Preparing version 3.0.12

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.11';
+    const VERSION = '3.0.12';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Super-Rabbit';
