6e758536780f401d7af2dc68cf11873d12916501
Tamara Robichet <tamarasaurus@users.noreply.github.com>
2019-04-09 16:38:57 +0200

    TIP-1083: Add query to get viewable attributes by a user (#5978)
    * TIP-1083: Add query to get viewable attributes by a user
    
    TIP-1083: Rename class and functions
    
    TIP-1083: Remove useless join
    
    TIP-1083: Check for attribute codes in query
    
    TIP-1083: Update query and return
    
    TIP-1083: Add docs
    
    * TIP-1083: Fix phpcs

---
+++ src/Akeneo/Pim/Permission/Bundle/Persistence/Sql/GetViewableAttributeCodesForUser.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Permission\Bundle\Persistence\Sql;
+
+use Akeneo\Pim\Permission\Component\Query\GetViewableAttributeCodesForUserInterface;
+use Doctrine\DBAL\Connection;
+
+/**
+ * Filters a list of attribute by code based on whether the given user can view them
+ *
+ * @author    Tamara Robichet <tamara.robichet@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GetViewableAttributeCodesForUser implements GetViewableAttributeCodesForUserInterface
+{
+    /** @var Connection */
+    private $sqlConnection;
+
+    public function __construct(Connection $sqlConnection)
+    {
+        $this->sqlConnection = $sqlConnection;
+    }
+
+    public function forAttributeCodes(array $attributeCodes, int $userId): array
+    {
+        if (empty($attributeCodes)) {
+            return [];
+        }
+
+        $query = <<<SQL
+        SELECT attribute.code
+        FROM pim_catalog_attribute attribute
+        WHERE EXISTS (
+            SELECT *
+            FROM pimee_security_attribute_group_access attribute_access
+            INNER JOIN oro_user_access_group user_access_group on attribute_access.user_group_id = user_access_group.group_id AND user_access_group.user_id = :userId
+            WHERE attribute_access.attribute_group_id = attribute.group_id
+        )
+        AND attribute.code IN (:attributeCodes)
+SQL;
+
+        $statement = $this->sqlConnection->executeQuery($query, [
+            'userId' => $userId,
+            'attributeCodes' => $attributeCodes
+        ], ['attributeCodes' => Connection::PARAM_STR_ARRAY]);
+
+        $allowedAttributes = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return array_map(
+            function ($row) {
+                return $row['code'];
+            },
+            $allowedAttributes
+        );
+    }
+}
---
+++ tests/back/Pim/Permission/Integration/Persistence/Sql/GetViewableAttributeCodesForUserIntegration.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AkeneoTestEnterprise\Pim\Permission\Integration\Persistence\Sql;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use PHPUnit\Framework\Assert;
+use Akeneo\Pim\Permission\Bundle\Persistence\Sql\GetViewableAttributeCodesForUser;
+
+class GetViewableAttributeCodesForUserIntegration extends TestCase
+{
+    /**
+     * @test
+    */
+    public function it_filters_a_list_of_attribute_codes_by_user_permission()
+    {
+        $query = $this->getQuery();
+
+        $userId = $this->get('database_connection')
+                        ->fetchColumn('SELECT id FROM oro_user WHERE username = "mary"', [], 0);
+
+        $filteredAttributes = $query->forAttributeCodes(['a_date', 'a_multi_select'], $userId);
+
+        Assert::assertSame($filteredAttributes, ['a_date']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useTechnicalCatalog();
+    }
+
+    private function getQuery(): GetViewableAttributeCodesForUser
+    {
+        return $this->get('pimee_security.query.get_viewable_attribute_codes_for_user');
+    }
+}
