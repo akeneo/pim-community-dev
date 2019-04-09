c781d9dd2ae906527cba07cf2dc8fe58fe5295e9
Julian Prud'homme <julian.prudhomme@akeneo.com>
2019-04-09 16:36:58 +0200

    DAPI-183: add a value object for attribute option code

---
+++ src/Akeneo/Pim/Automation/FranklinInsights/Domain/Common/ValueObject/AttributeOptionCode.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject;
+
+/**
+ * @author Julian Prud'homme <julian.prudhomme@akeneo.com>
+ */
+class AttributeOptionCode
+{
+    /** @var string */
+    private $code;
+
+    /**
+     * @param string $code
+     */
+    public function __construct(string $code)
+    {
+        if (empty($code)) {
+            throw new \InvalidArgumentException('Attribute option code cannot be an empty string');
+        }
+
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->code;
+    }
+}
---
+++ src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification/Domain/Common/ValueObject/AttributeOptionCodeSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2018 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Specification\Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject;
+
+use Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject\AttributeOptionCode;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @author Julian Prud'homme <julian.prudhomme@akeneo.com>
+ */
+class AttributeOptionCodeSpec extends ObjectBehavior
+{
+    public function it_is_an_attribute_option_code(): void
+    {
+        $this->beConstructedWith('foo');
+        $this->shouldBeAnInstanceOf(AttributeOptionCode::class);
+    }
+
+    public function it_throws_an_exception_when_attribute_option_code_is_empty(): void
+    {
+        $this->beConstructedWith('');
+        $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
+    }
+
+    public function it_returns_the_attribute_option_code(): void
+    {
+        $this->beConstructedWith('foo');
+        $this->__toString()->shouldReturn('foo');
+    }
+}
