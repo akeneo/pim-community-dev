707001d5bff3e4dd64f212cb9e6d13b52d6d76e4
arnaud <arnaud.langlade@akeneo.com>
2019-04-09 13:50:18 +0200

    #1062: Hide the file path property for cloud edition

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/edit/field/file-path.ts
@@ -0,0 +1,14 @@
+const BaseField = require('pimui/js/job/common/edit/field/text');
+const editionProvider = require('pim/edition');
+
+class FilePath extends BaseField {
+    render() {
+        if (editionProvider.isCloudEdition() === false) {
+            super.render(); // on pourra faire super
+        }
+
+        return this;
+    }
+}
+
+export = FilePath;
---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/pim-edition.ts
@@ -0,0 +1,7 @@
+const pimEdition = {
+    isCloudEdition: () => {
+        return process.env.IS_CLOUD_EDITION
+    }
+};
+
+export=pimEdition;
