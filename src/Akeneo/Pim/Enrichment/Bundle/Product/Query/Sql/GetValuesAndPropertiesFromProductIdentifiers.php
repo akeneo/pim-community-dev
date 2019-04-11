9326d5fe2a7cff2aefb7297038a43459c12aa24a
pierallard <pierallard@gmail.com>
2019-04-11 11:16:06 +0200

    TIP-1051: Add GetValuesAndPropertiesFromProductIdentifiers

---
+++ src/Akeneo/Pim/Enrichment/Bundle/Product/Query/Sql/GetValuesAndPropertiesFromProductIdentifiers.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Bundle\Product\Query\Sql;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * SQL Query to get the properties and the values from a set of product identifiers:
+ * identifier, is_enabled, product_model_code, created, updated, family_code, group_codes and raw_data.
+ *
+ * @author    Pierre Allard <pierre.allard@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class GetValuesAndPropertiesFromProductIdentifiers
+{
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function fetchByProductIdentifiers(array $productIdentifiers)
+    {
+        $query = <<<SQL
+SELECT
+    p.identifier,
+    p.is_enabled,
+    pm1.code AS product_model_code,
+    p.created,
+    p.updated,
+    f.code AS family_code,
+    JSON_ARRAYAGG(g.code) AS group_codes,
+    JSON_MERGE(COALESCE(pm1.raw_values, '{}'), COALESCE(pm2.raw_values, '{}'), p.raw_values) as raw_values
+FROM pim_catalog_product p
+LEFT JOIN pim_catalog_family f ON p.family_id = f.id
+LEFT JOIN pim_catalog_group_product pg ON p.id = pg.product_id
+LEFT JOIN pim_catalog_group g ON pg.group_id = g.id
+LEFT JOIN pim_catalog_product_model pm1 ON p.product_model_id = pm1.id
+LEFT JOIN pim_catalog_product_model pm2 ON pm1.parent_id = pm2.id
+WHERE p.identifier IN (?)
+GROUP BY p.identifier
+SQL;
+
+        $rows = $this->connection->fetchAll(
+            $query,
+            [$productIdentifiers],
+            [Connection::PARAM_STR_ARRAY]
+        );
+
+        $results = [];
+        foreach ($rows as $row) {
+            $results[$row['identifier']] = [
+                'identifier' => $row['identifier'],
+                'is_enabled' => $row['is_enabled'] === '1',
+                'product_model_code' => $row['product_model_code'],
+                'created' => \DateTime::createFromFormat('Y-m-d H:i:s', $row['created']),
+                'updated' => \DateTime::createFromFormat('Y-m-d H:i:s', $row['updated']),
+                'family_code' => $row['family_code'],
+                'group_codes' => array_filter(json_decode($row['group_codes']), function ($groupCode) {
+                    return null !== $groupCode;
+                }),
+                'raw_values' => json_decode($row['raw_values'], true)
+            ];
+        }
+
+        return $results;
+    }
+}
