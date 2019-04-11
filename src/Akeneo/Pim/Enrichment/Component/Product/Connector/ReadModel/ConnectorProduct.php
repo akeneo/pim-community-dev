9c6c42ab9561629e1985aa1c671abe2ddc11f26e
ahocquard <alexandre.hocquard@akeneo.com>
2019-04-11 17:09:29 +0200

    TIP-1050: initialize the read model

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Connector/ReadModel/ConnectorProduct.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Connector\ReadModel;
+
+use Akeneo\Pim\Enrichment\Component\Product\Model\ValueCollectionInterface;
+
+/**
+ * This read model is dedicated to export product data for the connector, such as the API.
+ *
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class ConnectorProduct
+{
+    /** @var string */
+    private $identifier;
+
+    /** @var \DateTimeImmutable */
+    private $createdDate;
+
+    /** @var \DateTimeImmutable */
+    private $updatedDate;
+
+    /** @var bool */
+    private $enabled;
+
+    /** @var string */
+    private $familyCode;
+
+    /** @var array */
+    private $categoryCodes;
+
+    /** @var array */
+    private $groupCodes;
+
+    /** @var null|string */
+    private $parentProductModelCode;
+
+    /** @var array */
+    private $associations;
+
+    /** @var array medata are for the status of the product in enterprise edition */
+    private $metadata;
+
+    /** @var ValueCollectionInterface */
+    private $values;
+
+    public function __construct(
+        string $identifier,
+        \DateTimeImmutable $createdDate,
+        \DateTimeImmutable $updatedDate,
+        bool $enabled,
+        string $familyCode,
+        array $categoryCodes,
+        array $groups,
+        ?string $parentProductModelCode,
+        array $associations,
+        array $metadata,
+        ValueCollectionInterface $values
+    ) {
+        $this->identifier = $identifier;
+        $this->createdDate = $createdDate;
+        $this->updatedDate = $updatedDate;
+        $this->enabled = $enabled;
+        $this->familyCode = $familyCode;
+        $this->categoryCodes = $categoryCodes;
+        $this->groupCodes = $groups;
+        $this->parentProductModelCode = $parentProductModelCode;
+        $this->values = $values;
+        $this->associations = $associations;
+        $this->metadata = $metadata;
+    }
+
+    public function identifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function createdDate(): \DateTimeImmutable
+    {
+        return $this->createdDate;
+    }
+
+    public function updatedDate(): \DateTimeImmutable
+    {
+        return $this->updatedDate;
+    }
+
+    public function enabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public function familyCode(): string
+    {
+        return $this->familyCode;
+    }
+
+    public function categoryCodes(): array
+    {
+        return $this->categoryCodes;
+    }
+
+    public function groupCodes(): array
+    {
+        return $this->groupCodes;
+    }
+
+    public function parentProductModelCode(): ?string
+    {
+        return $this->parentProductModelCode;
+    }
+
+    public function associations(): array
+    {
+        return $this->associations;
+    }
+
+    public function metadata(): array
+    {
+        return $this->metadata;
+    }
+
+    public function values(): ValueCollectionInterface
+    {
+        return $this->values;
+    }
+}
---
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Connector/ReadModel/ConnectorProductSpec.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Specification\Akeneo\Pim\Enrichment\Component\Product\Connector\ReadModel;
+
+use Akeneo\Pim\Enrichment\Component\Product\Connector\ReadModel\ConnectorProduct;
+use Akeneo\Pim\Enrichment\Component\Product\Model\ValueCollection;
+use PhpSpec\ObjectBehavior;
+
+class ConnectorProductSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(
+            'identifier',
+            new \DateTimeImmutable('2019-04-23 15:55:50', new \DateTimeZone('UTC')),
+            new \DateTimeImmutable('2019-04-25 15:55:50', new \DateTimeZone('UTC')),
+            true,
+            'family_code',
+            ['category_code_1', 'category_code_2'],
+            ['group_code_1', 'group_code_2'],
+            'parent_product_model_code',
+            [
+                'X_SELL' => [
+                    'products' => ['product_code_1'],
+                    'product_models' => [],
+                    'groups' => ['group_code_2']
+                ],
+                'UPSELL' => [
+                    'products' => ['product_code_4'],
+                    'product_models' => ['product_model_5'],
+                    'groups' => ['group_code_3']
+                ]
+            ],
+            [],
+            new ValueCollection()
+        );
+    }
+
+    function it_is_a_connector_product()
+    {
+        $this->shouldBeAnInstanceOf(ConnectorProduct::class);
+    }
+}
