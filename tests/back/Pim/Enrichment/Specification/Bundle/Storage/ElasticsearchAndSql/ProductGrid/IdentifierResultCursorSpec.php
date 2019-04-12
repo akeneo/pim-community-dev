a177fbe685105f525aa862ab606d0b2a7501a162
ahocquard <alexandre.hocquard@akeneo.com>
2019-04-12 17:21:19 +0200

    TIP-1050: fix spec due to renaming

--- tests/back/Pim/Enrichment/Specification/Bundle/Storage/ElasticsearchAndSql/ProductGrid/IdentifierResultCursorSpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/Storage/ElasticsearchAndSql/ProductGrid/IdentifierResultCursorSpec.php
@@ -6,14 +6,14 @@ use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\IdentifierResult;
 use Akeneo\Tool\Component\StorageUtils\Cursor\CursorInterface;
 use PhpSpec\ObjectBehavior;
 
-class ProductAndProductModelIdentifierCursorSpec extends ObjectBehavior
+class IdentifierResultCursorSpec extends ObjectBehavior
 {
     function let(IdentifierResult $identifierResult1, IdentifierResult $identifierResult2)
     {
         $this->beConstructedWith([$identifierResult1, $identifierResult2], 42);
     }
 
-    function it_is_a_Cursor()
+    function it_is_a_cursor()
     {
         $this->shouldImplement(CursorInterface::class);
     }
