7b59b7471846902260cc9af8589ddc92fd232ee2
Adrien Pétremann <hello@grena.fr>
2019-04-15 14:49:02 +0200

    Preparing version 2.3.37

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.36';
+    const VERSION = '2.3.37';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
