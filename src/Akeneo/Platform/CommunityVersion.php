9955fe79f77692b0edf96afbac55a2d1d60f5fb0
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-04-29 16:53:36 +0200

    Fix version

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.x';
+    const VERSION = '3.2.0';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Slick Hare';
