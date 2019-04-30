e4992d8991a5bf1ad4364f8f88a27b49380867c9
christophe-chausseray <chausseray.christophe@gmail.com>
2019-04-30 11:26:13 +0200

    PIM-8317: Define new model for the number attribute

---
+++ src/Akeneo/ReferenceEntity/back/Domain/Model/Attribute/NumberAttribute.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\ReferenceEntity\Domain\Model\Attribute;
+
+use Akeneo\ReferenceEntity\Domain\Model\LabelCollection;
+use Akeneo\ReferenceEntity\Domain\Model\ReferenceEntity\ReferenceEntityIdentifier;
+
+/**
+ * @author    Christophe Chausseray <christophe.chausseray@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ */
+class NumberAttribute extends AbstractAttribute
+{
+    public static function create(
+        AttributeIdentifier $identifier,
+        ReferenceEntityIdentifier $referenceEntityIdentifier,
+        AttributeCode $code,
+        LabelCollection $labelCollection,
+        AttributeOrder $order,
+        AttributeIsRequired $isRequired,
+        AttributeValuePerChannel $valuePerChannel,
+        AttributeValuePerLocale $valuePerLocale
+    ) {
+        return new self(
+            $identifier,
+            $referenceEntityIdentifier,
+            $code,
+            $labelCollection,
+            $order,
+            $isRequired,
+            $valuePerChannel,
+            $valuePerLocale
+        );
+    }
+
+    public function getType(): string
+    {
+        return 'number';
+    }
+}
