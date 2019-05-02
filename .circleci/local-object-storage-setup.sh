aaa707e27e85bdb2008fe8b89f0d00773ab3236d
Benoit Jacquemont <benoit@akeneo.com>
2019-05-02 00:54:38 -0700

    TIP-1118: Uses Flysystem and local object storage to run tests on the CI (#6073)
    * TIP-1118 Run tests on the CI with object storage
    
    * TIP-1118 Uses Flysystem to check variation content

---
+++ .circleci/local-object-storage-setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname $0)
+
+
+cat >> $SCRIPT_DIR/../app/config/parameters_test.yml <<OBJECT_STORAGE_CONF
+
+services:
+    acme.s3_client:
+        class: Aws\S3\S3Client
+        factory_class: Aws\S3\S3Client
+        factory_method: factory
+        arguments:
+            -
+                endpoint: 'http://object-storage:9000/asset/'
+                version: 'latest'
+                region: 'us-west-1'
+                use_path_style_endpoint: true
+                credentials:
+                    key: "AKENEO_OBJECT_STORAGE_ACCESS_KEY"
+                    secret: "AKENEO_OBJECT_STORAGE_SECRET_KEY"
+
+oneup_flysystem:
+    adapters:
+        asset_storage_adapter:
+            awss3v3:
+                client: acme.s3_client
+                bucket: asset
+        catalog_storage_adapter:
+            awss3v3:
+                client: acme.s3_client
+                bucket: catalog
+
+OBJECT_STORAGE_CONF
+
