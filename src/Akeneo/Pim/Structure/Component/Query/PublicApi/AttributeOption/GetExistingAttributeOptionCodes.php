c29b956d488618b7e49f936c3037d1a052525b68
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2019-05-02 10:12:08 +0200

    TIP-1158: Fix tests on existing attriute option codes

--- src/Akeneo/Pim/Structure/Component/Query/PublicApi/AttributeOption/GetExistingAttributeOptionCodes.php
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/AttributeOption/GetExistingAttributeOptionCodes.php
@@ -13,5 +13,5 @@ namespace Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeOption;
  */
 interface GetExistingAttributeOptionCodes
 {
-    public function fromOptionCodes(array $optionCodes): array;
+    public function fromOptionCodesByAttributeCode(array $attributeCodesToOptionCodes): array;
 }
