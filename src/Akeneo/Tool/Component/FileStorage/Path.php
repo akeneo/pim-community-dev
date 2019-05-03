0fb2d6258a6b01e7407684e6915e8229f3cf07ba
arnaud <arnaud.langlade@akeneo.com>
2019-05-03 11:05:38 +0200

    TIP-1126: Fix typo

--- src/Akeneo/Tool/Component/FileStorage/Path.php
+++ src/Akeneo/Tool/Component/FileStorage/Path.php
@@ -2,17 +2,27 @@
 
 namespace Akeneo\Tool\Component\FileStorage;
 
+/**
+ * Represent a file or a directory path.
+ *
+ * Example: to build this path: `local://path/to/file.png` you can use `new Path('local, 'path', 'to', 'file.png');`
+ * Example: to build this path: `path/to/file.png` you can use `Path::withoutStorage('path', 'to', 'file.png');`
+ *
+ * @author    Arnaud Langlade <arnaud.langlade@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 final class Path
 {
     /** @var array */
-    private $directories;
+    private $pathFragment;
 
     /**  @var string */
     private $storage;
 
-    public function __construct(string $storage, string ...$directories)
+    public function __construct(string $storage, string ...$pathFragment)
     {
-        $this->directories = $directories;
+        $this->pathFragment = $pathFragment;
         $this->storage = $storage;
     }
 
@@ -23,14 +33,14 @@ final class Path
 
     public function __toString(): string
     {
-        $path = array_reduce($this->directories, function ($path, $directory) {
-            $directory = trim($directory, "/");
+        $path = array_reduce($this->pathFragment, function ($path, $fragment) {
+            $fragment = trim($fragment, "/");
 
             if ('' === $path) {
-                return $directory;
+                return $fragment;
             }
 
-            return $path.DIRECTORY_SEPARATOR.$directory;
+            return $path.DIRECTORY_SEPARATOR.$fragment;
         }, '');
 
         if ('' !== $this->storage) {
