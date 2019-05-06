43f2d77cda15ac6defe5f10c2e19fdd173087095
jmleroux <jmleroux.pro@gmail.com>
2019-05-06 16:25:16 +0200

    Preparing version 2.3.42

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.41';
+    const VERSION = '2.3.42';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Lychee';
