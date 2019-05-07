c46ba2d05c8f80987057b9f1fba1178fbc68c98c
Adrien Pétremann <hello@grena.fr>
2019-05-07 11:23:21 +0200

    PIM-8317: front coverage 100%

--- src/Akeneo/ReferenceEntity/tests/front/unit/akeneoreferenceentity/domain/model/attribute/type/number.unit.ts
+++ src/Akeneo/ReferenceEntity/tests/front/unit/akeneoreferenceentity/domain/model/attribute/type/number.unit.ts
@@ -1,4 +1,10 @@
 import {ConcreteNumberAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/number';
+import Identifier, {createIdentifier} from 'akeneoreferenceentity/domain/model/attribute/identifier';
+import ReferenceEntityIdentifier, {
+  createIdentifier as createReferenceEntityIdentifier,
+} from 'akeneoreferenceentity/domain/model/reference-entity/identifier';
+import LabelCollection, {createLabelCollection} from 'akeneoreferenceentity/domain/model/label-collection';
+import AttributeCode, {createCode} from 'akeneoreferenceentity/domain/model/attribute/code';
 
 const normalizedArea = {
   identifier: 'area_city_fingerprint',
@@ -16,5 +22,21 @@ const normalizedArea = {
 describe('akeneo > attribute > domain > model > attribute > type --- NumberAttribute', () => {
   test('I can create a ConcreteNumberAttribute from normalized', () => {
     expect(ConcreteNumberAttribute.createFromNormalized(normalizedArea).normalize()).toEqual(normalizedArea);
+  });
+
+  test('I cannot create an invalid ConcreteNumberAttribute', () => {
+    expect(() => {
+      new ConcreteNumberAttribute(
+        createIdentifier('designer', 'age'),
+        createReferenceEntityIdentifier('designer'),
+        createCode('age'),
+        createLabelCollection({en_US: 'Age'}),
+        false,
+        false,
+        0,
+        true,
+        false
+      );
+    }).toThrow('Attribute expects a IsDecimal as isDecimal');
   });
 });
