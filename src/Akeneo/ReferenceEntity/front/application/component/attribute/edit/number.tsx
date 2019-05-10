01a021ea7db4df6347afef45e965965a29395143
Adrien Pétremann <hello@grena.fr>
2019-05-10 16:30:07 +0200

    PIM-8317: add min & max in frontend

--- src/Akeneo/ReferenceEntity/front/application/component/attribute/edit/number.tsx
+++ src/Akeneo/ReferenceEntity/front/application/component/attribute/edit/number.tsx
@@ -3,17 +3,22 @@ import __ from 'akeneoreferenceentity/tools/translator';
 import ValidationError from 'akeneoreferenceentity/domain/model/validation-error';
 import {getErrorsView} from 'akeneoreferenceentity/application/component/app/validation-error';
 import Checkbox from 'akeneoreferenceentity/application/component/app/checkbox';
-import {IsDecimal} from 'akeneoreferenceentity/domain/model/attribute/type/number/is-decimal';
 import {NumberAdditionalProperty, NumberAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/number';
+import {IsDecimal} from 'akeneoreferenceentity/domain/model/attribute/type/number/is-decimal';
+import {MinValue} from 'akeneoreferenceentity/domain/model/attribute/type/number/min-value';
+import {MaxValue} from 'akeneoreferenceentity/domain/model/attribute/type/number/max-value';
+import Key from 'akeneoreferenceentity/tools/key';
 
 const NumberView = ({
   attribute,
   onAdditionalPropertyUpdated,
+  onSubmit,
   errors,
   rights,
 }: {
   attribute: NumberAttribute;
   onAdditionalPropertyUpdated: (property: string, value: NumberAdditionalProperty) => void;
+  onSubmit: () => void;
   errors: ValidationError[];
   rights: {
     attribute: {
@@ -23,6 +28,10 @@ const NumberView = ({
     };
   };
 }) => {
+  const inputTextClassName = `AknTextField AknTextField--light ${
+    !rights.attribute.edit ? 'AknTextField--disabled' : ''
+  }`;
+
   return (
     <React.Fragment>
       <div className="AknFieldContainer AknFieldContainer--packed" data-code="isDecimal">
@@ -54,6 +63,68 @@ const NumberView = ({
           </label>
         </div>
         {getErrorsView(errors, 'isDecimal')}
+      </div>
+      <div className="AknFieldContainer" data-code="minValue">
+        <div className="AknFieldContainer-header AknFieldContainer-header--light">
+          <label className="AknFieldContainer-label" htmlFor="pim_reference_entity.attribute.edit.input.min_value">
+            {__('pim_reference_entity.attribute.edit.input.min_value')}
+          </label>
+        </div>
+        <div className="AknFieldContainer-inputContainer">
+          <input
+            type="text"
+            autoComplete="off"
+            className={inputTextClassName}
+            id="pim_reference_entity.attribute.edit.input.min_value"
+            name="min_value"
+            value={attribute.minValue.stringValue()}
+            onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
+              if (Key.Enter === event.key) onSubmit();
+            }}
+            onChange={(event: React.FormEvent<HTMLInputElement>) => {
+              if (!MinValue.isValid(event.currentTarget.value)) {
+                event.currentTarget.value = attribute.minValue.stringValue();
+                event.preventDefault();
+                return;
+              }
+
+              onAdditionalPropertyUpdated('min_value', MinValue.createFromString(event.currentTarget.value));
+            }}
+            readOnly={!rights.attribute.edit}
+          />
+        </div>
+        {getErrorsView(errors, 'minValue')}
+      </div>
+      <div className="AknFieldContainer" data-code="maxValue">
+        <div className="AknFieldContainer-header AknFieldContainer-header--light">
+          <label className="AknFieldContainer-label" htmlFor="pim_reference_entity.attribute.edit.input.max_value">
+            {__('pim_reference_entity.attribute.edit.input.max_value')}
+          </label>
+        </div>
+        <div className="AknFieldContainer-inputContainer">
+          <input
+            type="text"
+            autoComplete="off"
+            className={inputTextClassName}
+            id="pim_reference_entity.attribute.edit.input.max_value"
+            name="max_value"
+            value={attribute.maxValue.stringValue()}
+            onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
+              if (Key.Enter === event.key) onSubmit();
+            }}
+            onChange={(event: React.FormEvent<HTMLInputElement>) => {
+              if (!MaxValue.isValid(event.currentTarget.value)) {
+                event.currentTarget.value = attribute.maxValue.stringValue();
+                event.preventDefault();
+                return;
+              }
+
+              onAdditionalPropertyUpdated('max_value', MaxValue.createFromString(event.currentTarget.value));
+            }}
+            readOnly={!rights.attribute.edit}
+          />
+        </div>
+        {getErrorsView(errors, 'maxValue')}
       </div>
     </React.Fragment>
   );
--- src/Akeneo/ReferenceEntity/front/application/reducer/attribute/type/number.ts
+++ src/Akeneo/ReferenceEntity/front/application/reducer/attribute/type/number.ts
@@ -1,6 +1,8 @@
 import {NormalizedNumberAttribute} from 'akeneoreferenceentity/domain/model/attribute/type/number';
 import {NormalizedNumberAdditionalProperty} from 'akeneoreferenceentity/domain/model/attribute/type/number';
 import {NormalizedIsDecimal} from 'akeneoreferenceentity/domain/model/attribute/type/number/is-decimal';
+import {NormalizedMinValue} from 'akeneoreferenceentity/domain/model/attribute/type/number/min-value';
+import {NormalizedMaxValue} from 'akeneoreferenceentity/domain/model/attribute/type/number/max-value';
 
 const numberAttributeReducer = (
   normalizedAttribute: NormalizedNumberAttribute,
@@ -11,6 +13,12 @@ const numberAttributeReducer = (
     case 'is_decimal':
       const is_decimal = propertyValue as NormalizedIsDecimal;
       return {...normalizedAttribute, is_decimal};
+    case 'min_value':
+      const min_value = propertyValue as NormalizedMinValue;
+      return {...normalizedAttribute, min_value};
+    case 'max_value':
+      const max_value = propertyValue as NormalizedMaxValue;
+      return {...normalizedAttribute, max_value};
     default:
       break;
   }
