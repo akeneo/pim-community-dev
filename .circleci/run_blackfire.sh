88384f61d9bfb1bdf986658b3b8795f36792abf9
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-13 17:30:38 +0200

    [POC] fix env variables
    [POC] export env variables to bash env
    
    [POC] pass vars to data generator
    
    [POC] assign env variable values
    
    [POC] assign env variable values

--- .circleci/run_blackfire.sh
+++ .circleci/run_blackfire.sh
@@ -33,13 +33,17 @@ generate_reference_catalog()
     message "Generates an API user for the benchmarks in test environment"
     cd $PIM_PATH
     export ES_JAVA_OPTS='-Xms2g -Xmx2g'
+    PUBLIC_PIM_HTTP_PORT=$(docker-compose port fpm 80 | cut -d ':' -f 2)
     CREDENTIALS=$(docker-compose exec -T fpm bin/console pim:oauth-server:create-client --no-ansi -e behat generator | tr -d '\r ')
 
-    export API_CLIENT=$(echo $CREDENTIALS | cut -d " " -f 2 | cut -d ":" -f 2)
-    export API_SECRET=$(echo $CREDENTIALS | cut -d " " -f 3 | cut -d ":" -f 2)
-    export API_URL="http://$DOCKER_BRIDGE_IP:8080"
-    export API_USER="admin"
-    export API_PASSWORD="admin"
+    echo 'export API_CLIENT=$(echo $CREDENTIALS | cut -d " " -f 2 | cut -d ":" -f 2)' >> $BASH_ENV
+    echo 'export API_SECRET=$(echo $CREDENTIALS | cut -d " " -f 3 | cut -d ":" -f 2)' >> $BASH_ENV
+    echo 'export API_URL="http://$DOCKER_BRIDGE_IP:8080"' >> $BASH_ENV
+    echo 'export API_USER="admin"' >> $BASH_ENV
+    echo 'export API_PASSWORD="admin"' >> $BASH_ENV
+
+    which bash
+    source $BASH_ENV
 
     docker pull akeneo/data-generator:3.0
 
