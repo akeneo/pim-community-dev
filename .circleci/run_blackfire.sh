3b906bb1cb5dc2ce3ea37663e064b09a09ef4d7e
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-14 11:24:02 +0200

    add steps to install blackfire in fpm

--- .circleci/run_blackfire.sh
+++ .circleci/run_blackfire.sh
@@ -31,6 +31,7 @@ message()
 generate_reference_catalog()
 {
     message "Generates an API user for the benchmarks in test environment"
+
     cd $PIM_PATH
     export ES_JAVA_OPTS='-Xms2g -Xmx2g'
     PUBLIC_PIM_HTTP_PORT='$(docker-compose port httpd-behat 80 | cut -d ':' -f 2)'
@@ -40,6 +41,7 @@ generate_reference_catalog()
     export API_URL="http://$DOCKER_BRIDGE_IP:8081"
     export API_USER="admin"
     export API_PASSWORD="admin"
+    export API_AUTH="echo -n $API_CLIENT:$API_SECRET | base64"
 
     message "Generate the catalog"
 
@@ -56,20 +58,30 @@ generate_reference_catalog()
 
 setup_blackfire()
 {
-    # install blackfire in the container
-    message "Install blackfire in the container"
+    message "Install blackfire"
+
+    docker-compose exec -T fpm bash -c "wget -q -O - https://packages.blackfire.io/gpg.key | sudo apt-key add -"
+    docker-compose exec -T fpm bash -c 'echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list'
+    docker-compose exec -T fpm sudo apt-get update
+    docker-compose exec -T fpm sudo apt-get install -y --allow-unauthenticated blackfire-agent
+    docker-compose exec -T fpm bash -c "printf '$BLACKFIRE_SERVER_ID\n$BLACKFIRE_SERVER_TOKEN\n' | sudo blackfire-agent --register"
+    docker-compose exec -T fpm sudo /etc/init.d/blackfire-agent restart
+    docker-compose exec -T fpm bash -c "printf '$BLACKFIRE_CLIENT_ID\n$BLACKFIRE_CLIENT_TOKEN\n' | blackfire config -h"
+    docker-compose exec -T fpm sudo apt-get install -y --allow-unauthenticated blackfire-php
+    docker-compose restart fpm
+    docker-compose exec -T fpm sudo /etc/init.d/blackfire-agent restart
 }
 
 launch_bench()
 {
     message "Start benchmarks"
-    # blackfire curl
-    docker run -t -e API_CLIENT -e API_SECRET -e API_URL -e API_USER -e API_PASSWORD fpm bash echo 'Bench stuff'
+
+    docker-compose exec -T fpm blackfire --samples 10 curl -X GET 'http://$API_URL/api/rest/v1/products?limit=100' -H 'authorization: Bearer $API_AUTH' -H 'cache-control: no-cache' -H 'content-type: application/json'
 }
 
 generate_reference_catalog
 setup_blackfire
-launch_bench "get_many_products"
+launch_bench
 
 cd $PIM_PATH
 PRODUCT_SIZE=$(docker-compose exec -T mysql-behat mysql -uakeneo_pim -pakeneo_pim akeneo_pim -N -s -e "SELECT AVG(JSON_LENGTH(JSON_EXTRACT(raw_values, '$.*.*.*'))) avg_product_values FROM pim_catalog_product;" | tail -n 1 | tr -d '\r \n')
