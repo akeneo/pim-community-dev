c6b06d251a7365718c617326874e9992d1dcd488
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-14 14:48:58 +0200

    echo api response

--- .circleci/run_blackfire.sh
+++ .circleci/run_blackfire.sh
@@ -64,12 +64,13 @@ launch_bench()
 {
     echo "Start benchmarks"
 
-    echo 'auth\n'
-    echo $API_AUTH
-
-    API_TOKEN=$(docker-compose exec -T fpm curl -X POST $API_URL/api/oauth/v1/token -H "authorization: Basic ${API_AUTH}" -H 'content-type: application/json' -d '{ "grant_type": "password", "username": "admin", "password": "admin" }' | jq -r '.access_token')
+    API_RESPONSE=$(docker-compose exec -T fpm curl -X POST $API_URL/api/oauth/v1/token -H "authorization: Basic ${API_AUTH}" -H 'content-type: application/json' -d '{ "grant_type": "password", "username": "admin", "password": "admin" }')
 
     echo 'token\n'
+    echo $API_RESPONSE
+
+    API_TOKEN=$(echo $API_RESPONSE | jq -r '.access_token')
+
     echo $API_TOKEN
 
     docker-compose exec -T fpm curl "${API_URL}/api/rest/v1/products?limit=100" -H "authorization: Bearer ${API_TOKEN}" -H 'content-type: application/json'
