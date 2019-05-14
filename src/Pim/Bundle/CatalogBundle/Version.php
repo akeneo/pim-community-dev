f9cefc404c37daa98c37b95262c1f585b41c17ef
jmleroux <jmleroux.pro@gmail.com>
2019-05-14 14:28:13 +0200

    Preparing version 2.3.43

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.42';
+    const VERSION = '2.3.43';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
