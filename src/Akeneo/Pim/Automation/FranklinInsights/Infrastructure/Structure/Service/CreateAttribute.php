d9d4ab903aefd4f0ec5deea166d4d150b09487ad
romain <romain@akeneo.com>
2019-05-23 15:50:50 +0200

    Adds copyright templates

--- src/Akeneo/Pim/Automation/FranklinInsights/Infrastructure/Structure/Service/CreateAttribute.php
+++ src/Akeneo/Pim/Automation/FranklinInsights/Infrastructure/Structure/Service/CreateAttribute.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Akeneo\Pim\Automation\FranklinInsights\Infrastructure\Structure\Service;
-
 
 use Akeneo\Pim\Automation\FranklinInsights\Application\Structure\Service\CreateAttributeInterface;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject\AttributeCode;
