6d1464ecf0d3fcf5d896dbf0b704757b275a6b3d
ahocquard <alexandre.hocquard@akeneo.com>
2019-05-28 08:57:37 +0200

    Add categorized events

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Model/Events/CategorizedProduct.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Model\Events;
+
+/**
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CategorizedProduct
+{
+    /** @var string */
+    private $categoryCode;
+
+    public function __construct(string $categoryCode)
+    {
+        $this->categoryCode = $categoryCode;
+    }
+
+    public function categoryCode(): string
+    {
+        return $this->categoryCode;
+    }
+}
