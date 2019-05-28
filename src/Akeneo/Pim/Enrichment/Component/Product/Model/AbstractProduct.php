e8dc77818ca4ed75579fbf62b29b297dd25fdd9a
ahocquard <alexandre.hocquard@akeneo.com>
2019-05-28 14:29:17 +0200

    TIP-1171: add product identifier in the event

--- src/Akeneo/Pim/Enrichment/Component/Product/Model/AbstractProduct.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Model/AbstractProduct.php
@@ -379,7 +379,7 @@ abstract class AbstractProduct implements ProductInterface
     {
         if (!$this->categories->contains($category) && !$this->hasAncestryCategory($category)) {
             $this->categories->add($category);
-            $this->events[] = new CategorizedProduct($category->getCode());
+            $this->events[] = new CategorizedProduct($this->identifier, $category->getCode());
         }
 
         return $this;
@@ -401,10 +401,10 @@ abstract class AbstractProduct implements ProductInterface
         $categorizedCategoryCodes = array_diff($newCategoryCodes, $previousCategoryCodes);
 
         foreach ($uncategorizedCategoryCodes as $uncategorizedCategoryCode) {
-            $this->events[] = new UncategorizedProduct($uncategorizedCategoryCode);
+            $this->events[] = new UncategorizedProduct($this->identifier, $uncategorizedCategoryCode);
         }
         foreach ($categorizedCategoryCodes as $categorizedCategoryCode) {
-            $this->events[] = new CategorizedProduct($categorizedCategoryCode);
+            $this->events[] = new CategorizedProduct($this->identifier, $categorizedCategoryCode);
         }
 
         $this->categories = $categories;
@@ -417,7 +417,7 @@ abstract class AbstractProduct implements ProductInterface
     {
         if ($this->categories->contains($category)) {
             $this->categories->removeElement($category);
-            $this->events[] = new UncategorizedProduct($category->getCode());
+            $this->events[] = new UncategorizedProduct($this->identifier, $category->getCode());
         }
 
         return $this;
