e333fbe85633b514dd194d3e74809eaf1516cedf
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2019-05-31 11:31:44 +0200

    TIP-1173: Avoid to query attributes

--- tests/back/Pim/Enrichment/ReferenceEntity/Specification/Component/Factory/Read/Value/ReferenceEntityCollectionValueFactorySpec.php
+++ tests/back/Pim/Enrichment/ReferenceEntity/Specification/Component/Factory/Read/Value/ReferenceEntityCollectionValueFactorySpec.php
@@ -68,6 +68,6 @@ final class ReferenceEntityCollectionValueFactorySpec extends ObjectBehavior
 
     private function getAttribute(bool $isLocalizable, bool $isScopable): Attribute
     {
-        return new Attribute('an_attribute', ReferenceEntityCollectionType::REFERENCE_ENTITY_COLLECTION, ['reference_data_name' => 'color'], $isLocalizable, $isScopable, null);
+        return new Attribute('an_attribute', ReferenceEntityCollectionType::REFERENCE_ENTITY_COLLECTION, ['reference_data_name' => 'color'], $isLocalizable, $isScopable, null, false);
     }
 }
