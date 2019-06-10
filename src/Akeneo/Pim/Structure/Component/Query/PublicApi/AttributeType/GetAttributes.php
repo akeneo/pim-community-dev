477a310b6bb41dd9b7b68dd7e4d75940b7c0635a
ahocquard <alexandre.hocquard@akeneo.com>
2019-06-10 17:01:28 +0200

    TIP-1173: return null when attribute is not found in the public API

--- src/Akeneo/Pim/Structure/Component/Query/PublicApi/AttributeType/GetAttributes.php
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/AttributeType/GetAttributes.php
@@ -10,6 +10,9 @@ namespace Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType;
 interface GetAttributes
 {
     /**
+     * It returns an array of attributes indexed by the code.
+     * If an attribute is not found, it returns a NULL value for this key.
+     *
      * @return Attribute[]
      */
     public function forCodes(array $attributeCodes): array;
