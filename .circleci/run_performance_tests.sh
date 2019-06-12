af80bb6e1f3e0265bcee6b4df43108d0eee1f0d9
ahocquard <alexandre.hocquard@akeneo.com>
2019-06-12 14:11:48 +0200

    TIP-1173: test the performance of the API with Blackfire with our improvements

--- .circleci/run_performance_tests.sh
+++ .circleci/run_performance_tests.sh
@@ -6,4 +6,7 @@ SCRIPT_DIR=$(dirname $0)
 $SCRIPT_DIR/../tests/back/Performance/install_blackfire.sh
 $SCRIPT_DIR/../tests/back/Performance/load_reference_catalog.sh
 
+# wait ES to index everything before starting anything, which can alter the result
+sleep 10
+
 docker-compose exec -T fpm ./vendor/bin/phpunit -c app/phpunit.xml.dist --log-junit var/tests/phpunit/phpunit_$(uuidgen).xml --testsuite PIM_Performance_Test
