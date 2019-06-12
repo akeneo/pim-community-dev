7629e9468507632f39afd3b47967ee550a220dd7
christophe-chausseray <chausseray.christophe@gmail.com>
2019-06-12 17:03:33 +0200

    PIM-8409: Add PreviewGenerators

---
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/PreviewGenerator/PreviewGeneratorInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\ReferenceEntity\Infrastructure\PreviewGenerator;
+
+use Akeneo\ReferenceEntity\Domain\Model\Attribute\UrlAttribute;
+
+/**
+ * @author    Christophe Chausseray <christophe.chausseray@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ */
+interface PreviewGeneratorInterface
+{
+    public function supports(string $value, UrlAttribute $attribute, string $type): bool;
+
+    public function generate(string $value, UrlAttribute $attribute, string $type): string;
+}
---
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/PreviewGenerator/PreviewGeneratorRegistry.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\ReferenceEntity\Infrastructure\PreviewGenerator;
+
+use Akeneo\ReferenceEntity\Domain\Model\Attribute\UrlAttribute;
+
+/**
+ * @author    Christophe Chausseray <christophe.chausseray@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ */
+class PreviewGeneratorRegistry implements PreviewGeneratorInterface, PreviewGeneratorRegistryInterface
+{
+    /** @var PreviewGeneratorInterface[] */
+    private $previewGenerators;
+
+    public function registry(PreviewGeneratorInterface $previewGenerator): void
+    {
+        $this->previewGenerators[] = $previewGenerator;
+    }
+
+    public function supports(string $value, UrlAttribute $attribute, string $type): bool
+    {
+        foreach ($this->previewGenerators as $previewGenerator) {
+            if($previewGenerator->supports($value, $attribute, $type)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function generate(string $value, UrlAttribute $attribute, string $type): string
+    {
+        foreach ($this->previewGenerators as $previewGenerator) {
+            if($previewGenerator->supports($value, $attribute, $type)) {
+                return $previewGenerator->generate($value, $attribute, $type);
+            }
+        }
+
+        throw new \RuntimeException(
+            sprintf(
+                'There was no generator found to get the preview of attribute "%s"',
+                $attribute->getCode()
+            )
+        );
+    }
+}
