2d5e1ec33184c123fc701e11ef56ff66a8c2c7de
Tamara Robichet <tamara.robichet@akeneo.com>
2019-06-14 17:00:52 +0200

    TIP-1185: [POC] Use abstract for product and product model builders

--- src/Akeneo/Pim/Enrichment/Component/Product/Query/ProductAndProductModelQueryBuilder.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Query/ProductAndProductModelQueryBuilder.php
@@ -9,6 +9,8 @@ namespace Akeneo\Pim\Enrichment\Component\Product\Query;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
+// no longer use product query builder, copy paste the implementation
 class ProductAndProductModelQueryBuilder implements ProductQueryBuilderInterface
 {
     /** @var ProductQueryBuilderInterface */
