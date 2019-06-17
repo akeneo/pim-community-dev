2fe598e42ded43f74a94495ccf1dbe803a80fddc
Tamara Robichet <tamara.robichet@akeneo.com>
2019-06-17 12:08:56 +0200

    TIP-1185: [POC] Fix specs

--- tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/ProductAndProductModelQueryBuilderFactorySpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/ProductAndProductModelQueryBuilderFactorySpec.php
@@ -2,17 +2,33 @@
 
 namespace Specification\Akeneo\Pim\Enrichment\Bundle\Elasticsearch;
 
+use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\FilterRegistryInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderOptionsResolverInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\Sorter\SorterRegistryInterface;
+use Akeneo\Pim\Structure\Component\Repository\AttributeRepositoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Cursor\CursorFactoryInterface;
 use PhpSpec\ObjectBehavior;
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\ProductQueryBuilderFactory;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductAndProductModelQueryBuilder;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderFactoryInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
 
 class ProductAndProductModelQueryBuilderFactorySpec extends ObjectBehavior
 {
-    function let(ProductQueryBuilderFactory $factory)
+    function let(
+        AttributeRepositoryInterface $attributeRepository,
+        FilterRegistryInterface $filterRegistry,
+        SorterRegistryInterface $sorterRegistry,
+        CursorFactoryInterface $cursorFactory,
+        ProductQueryBuilderOptionsResolverInterface $optionsResolver
+    )
     {
-        $this->beConstructedWith(ProductAndProductModelQueryBuilder::class, $factory);
+        $this->beConstructedWith(
+            ProductAndProductModelQueryBuilder::class,
+            $attributeRepository,
+            $filterRegistry,
+            $sorterRegistry,
+            $cursorFactory,
+            $optionsResolver
+        );
     }
 
     function it_is_a_product_query_builder_factory()
@@ -20,10 +36,8 @@ class ProductAndProductModelQueryBuilderFactorySpec extends ObjectBehavior
         $this->shouldImplement(ProductQueryBuilderFactoryInterface::class);
     }
 
-    function it_creates_a_product_query_builder($factory, ProductQueryBuilderInterface $basePqb)
+    function it_creates_a_product_query_builder()
     {
-        $factory->create(['default_locale' => 'en_US', 'default_scope' => 'print'])->willReturn($basePqb);
-
         $this->create(['default_locale' => 'en_US', 'default_scope' => 'print'])->shouldBeAnInstanceOf(
             ProductAndProductModelQueryBuilder::class
         );
