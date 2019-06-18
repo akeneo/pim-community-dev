a1e675491392c70d30aac289e5a98a4fe027d399
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-06-18 10:45:57 +0200

    Simplify command

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -11,11 +11,14 @@ counter=1
 total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
 
 for TESTFILE in $TESTFILES; do
-    echo "docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE"
     echo "$TESTFILE ($counter/$total):"
     output=$(basename $TESTFILE)_$(uuidgen)
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE || \
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE
+
+    CMD=`docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE`
+
+    echo $CMD
+
+    $CMD || $CMD
     fail=$(($fail + $?))
     counter=$(($counter + 1))
 done
