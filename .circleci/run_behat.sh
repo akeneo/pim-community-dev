fe50ca2ea5326514e561346b2d692b7e2d7ba462
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-06-18 10:40:53 +0200

    Add suite

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -11,11 +11,11 @@ counter=1
 total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
 
 for TESTFILE in $TESTFILES; do
-    echo "docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy $TESTFILE"
+    echo "docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE"
     echo "$TESTFILE ($counter/$total):"
     output=$(basename $TESTFILE)_$(uuidgen)
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy $TESTFILE || \
-    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy $TESTFILE
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE || \
+    docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE
     fail=$(($fail + $?))
     counter=$(($counter + 1))
 done
