ba4ef7f99eff83f61987b1086c3680c4a328b5dd
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-06-18 10:13:49 +0200

    debug

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -4,11 +4,14 @@ SUITE=$1
 shift
 TESTFILES=$@
 
+echo $TESTFILES
+
 fail=0
 counter=1
 total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
 
 for TESTFILE in $TESTFILES; do
+    echo "docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy $TESTFILE"
     echo "$TESTFILE ($counter/$total):"
     output=$(basename $TESTFILE)_$(uuidgen)
     docker-compose exec -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${SUITE}/${output} --format pretty --out std --colors -p legacy $TESTFILE || \
