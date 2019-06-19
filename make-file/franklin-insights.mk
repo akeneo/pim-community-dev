8e1dc32312f6e87534dbe5be522e75c79b055489
Julian Prud'homme <julian.prudhomme@akeneo.com>
2019-06-19 17:28:25 +0200

    DAPI: add a new make target to fix CS

--- make-file/franklin-insights.mk
+++ make-file/franklin-insights.mk
@@ -32,5 +32,11 @@ franklin-insights-cs: vendor
 	$(YARN_EXEC) tslint -c src/Akeneo/Pim/Automation/FranklinInsights/tslint.json src/Akeneo/Pim/Automation/FranklinInsights/**/*.ts
 	$(DOCKER_COMPOSE) run --rm node ./node_modules/.bin/prettier --config src/Akeneo/Pim/Automation/FranklinInsights/.prettierrc.json --check src/Akeneo/Pim/Automation/FranklinInsights/**/*.ts
 
+.PHONY: franklin-insights-cs-fix
+franklin-insights-cs-fix:
+	$(PHP_EXEC) vendor/bin/php-cs-fixer fix --config=.php_cs.php
+	$(YARN_EXEC) tslint -c src/Akeneo/Pim/Automation/FranklinInsights/tslint.json src/Akeneo/Pim/Automation/FranklinInsights/**/*.ts --fix
+	$(DOCKER_COMPOSE) run --rm node ./node_modules/.bin/prettier --config src/Akeneo/Pim/Automation/FranklinInsights/.prettierrc.json --check src/Akeneo/Pim/Automation/FranklinInsights/**/*.ts --write
+
 .PHONY: franklin-insights-tests
 franklin-insights-tests: franklin-insights-coupling franklin-insights-cs franklin-insights-phpstan franklin-insights-unit franklin-insights-acceptance franklin-insights-integration franklin-insights-end-to-end
