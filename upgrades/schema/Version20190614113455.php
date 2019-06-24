e3087ca2310f0475fefcaebc17ec93587a899816
romain <romain@akeneo.com>
2019-06-24 15:19:22 +0200

    DAPI-338: Rework installation queries and fix issues on migration

--- upgrades/schema/Version20190614113455.php
+++ upgrades/schema/Version20190614113455.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Upgrade\Schema;
 
+use Akeneo\Pim\Automation\FranklinInsights\Infrastructure\Install\Query\CreateTableAttributeCreatedQuery;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
@@ -12,15 +13,7 @@ class Version20190614113455 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $sqlQuery = <<<'SQL'
-CREATE TABLE IF NOT EXISTS pimee_franklin_insights_attribute_created(
-    attribute_code VARCHAR(100) NOT NULL,
-    attribute_type VARCHAR(255) NOT NULL,
-    created DATETIME NOT NULL COMMENT '(DC2Type:datetime)' DEFAULT CURRENT_TIMESTAMP, 
-    INDEX IDX_FI_AATF_attribute_code (attribute_code)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
-SQL;
-        $this->connection->executeQuery($sqlQuery);
+        $this->connection->executeQuery(CreateTableAttributeCreatedQuery::QUERY);
     }
 
     public function down(Schema $schema)
