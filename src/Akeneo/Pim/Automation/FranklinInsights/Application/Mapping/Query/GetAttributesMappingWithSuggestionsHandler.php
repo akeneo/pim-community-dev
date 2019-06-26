231f03bfc05ece490f10ab4b83da804b8fc6e88a
Olivier Pontier <olivier.pontier@akeneo.com>
2019-06-26 16:45:27 +0200

    DAPI-296: refactor GetAttributesMappingWithSuggestionsHandler to use AttributeMappingCollectionDataProcessor service

--- src/Akeneo/Pim/Automation/FranklinInsights/Application/Mapping/Query/GetAttributesMappingWithSuggestionsHandler.php
+++ src/Akeneo/Pim/Automation/FranklinInsights/Application/Mapping/Query/GetAttributesMappingWithSuggestionsHandler.php
@@ -13,96 +13,37 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Query;
 
-use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\AttributeMappingStatus;
-use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\Read\AttributeMapping;
+use Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Service\DataProcessor\AttributeMappingCollectionDataProcessorInterface;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\Read\AttributeMappingCollection;
-use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Query\SelectExactMatchAttributeCodeQueryInterface;
-use Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject\AttributeCode;
-use Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject\FamilyCode;
 
 /**
  * @author Julian Prud'homme <julian.prudhomme@akeneo.com>
  */
 class GetAttributesMappingWithSuggestionsHandler
 {
-    /** @var SelectExactMatchAttributeCodeQueryInterface */
-    private $selectExactMatchAttributeCodeQuery;
-
     /** @var GetAttributesMappingByFamilyHandler */
     private $getAttributesMappingByFamilyHandler;
 
+    /** @var AttributeMappingCollectionDataProcessorInterface */
+    private $attributeMappingDataProcessor;
+
     public function __construct(
         GetAttributesMappingByFamilyHandler $getAttributesMappingByFamilyHandler,
-        SelectExactMatchAttributeCodeQueryInterface $selectExactMatchAttributeCodeQuery
+        AttributeMappingCollectionDataProcessorInterface $attributeMappingDataProcessor
     ) {
-        $this->selectExactMatchAttributeCodeQuery = $selectExactMatchAttributeCodeQuery;
         $this->getAttributesMappingByFamilyHandler = $getAttributesMappingByFamilyHandler;
+        $this->attributeMappingDataProcessor = $attributeMappingDataProcessor;
     }
 
     public function handle(GetAttributesMappingWithSuggestionsQuery $query): AttributeMappingCollection
     {
         $familyCode = $query->getFamilyCode();
         $attributeMappingCollection = $this->getAttributesMappingByFamilyHandler->handle(
-            new GetAttributesMappingByFamilyQuery($query->getFamilyCode())
+            new GetAttributesMappingByFamilyQuery($familyCode)
         );
 
-        $attributeMappingCollection = $this->applyExactMatches($familyCode, $attributeMappingCollection);
-
-        return $attributeMappingCollection;
-    }
-
-    /**
-     * @param FamilyCode $familyCode
-     * @param AttributeMappingCollection $familyAttributesMapping
-     * @return string[]
-     */
-    private function findPimAttributeCodeMatches(FamilyCode $familyCode, AttributeMappingCollection $familyAttributesMapping): array
-    {
-        $matchedPimAttributeCodes = $this->selectExactMatchAttributeCodeQuery->execute(
-            $familyCode,
-            $familyAttributesMapping->getPendingAttributesFranklinLabels()
-        );
-
-        return $this->filterNotMappedAttributeCodes($matchedPimAttributeCodes, $familyAttributesMapping);
-    }
-
-    private function filterNotMappedAttributeCodes(array $attributeCodes, AttributeMappingCollection $attributeMappingCollection): array
-    {
-        return array_filter($attributeCodes, function ($attributeCode) use ($attributeMappingCollection) {;
-            return null === $attributeCode || !$attributeMappingCollection->hasPimAttribute(new AttributeCode($attributeCode));
-        });
-    }
-
-    private function buildAttributeMappingCollectionWithMatchedAttributeCodes(array $matchedPimAttributeCodes, AttributeMappingCollection $attributeMappingCollection)
-    {
-        $newMapping = new AttributeMappingCollection();
-
-        foreach ($attributeMappingCollection as $attributeMapping) {
-            $pimAttributeCode = $attributeMapping->getPimAttributeCode();
-            if ($attributeMapping->getStatus() === AttributeMappingStatus::ATTRIBUTE_PENDING &&
-                array_key_exists($attributeMapping->getTargetAttributeLabel(), $matchedPimAttributeCodes)
-            ) {
-                $pimAttributeCode = $matchedPimAttributeCodes[$attributeMapping->getTargetAttributeLabel()];
-            }
-
-            $newAttributeMapping = new AttributeMapping(
-                $attributeMapping->getTargetAttributeCode(),
-                $attributeMapping->getTargetAttributeLabel(),
-                $attributeMapping->getTargetAttributeType(),
-                $pimAttributeCode,
-                $attributeMapping->getStatus(),
-                $attributeMapping->getSummary()
-            );
-            $newMapping->addAttribute($newAttributeMapping);
-        }
-
-        return $newMapping;
-    }
-
-    private function applyExactMatches(FamilyCode $familyCode, AttributeMappingCollection $attributeMappingCollection)
-    {
-        $matchedPimAttributeCodes = $this->findPimAttributeCodeMatches($familyCode, $attributeMappingCollection);
-
-        return $this->buildAttributeMappingCollectionWithMatchedAttributeCodes($matchedPimAttributeCodes, $attributeMappingCollection);
+        return $this->attributeMappingDataProcessor->process($attributeMappingCollection, [
+            'familyCode' => $familyCode,
+        ]);
     }
 }
