ade8ace4a50aac96c0bbbdcbb1a808d684ab2bf8
Olivier Pontier <olivier.pontier@akeneo.com>
2019-06-26 16:46:24 +0200

    DAPI-296: enrich ApplyAttributeExactMatches unit tests

--- src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification/Application/Mapping/Service/DataProcessor/ApplyAttributeExactMatchesSpec.php
+++ src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification/Application/Mapping/Service/DataProcessor/ApplyAttributeExactMatchesSpec.php
@@ -13,16 +13,12 @@
 namespace Specification\Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Service\DataProcessor;
 
 
-use Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Query\GetAttributesMappingByFamilyHandler;
-use Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Query\GetAttributesMappingByFamilyQuery;
-use Akeneo\Pim\Automation\FranklinInsights\Application\Mapping\Query\GetAttributesMappingWithSuggestionsQuery;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\AttributeMappingStatus;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\Read\AttributeMapping;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Model\Read\AttributeMappingCollection;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\AttributeMapping\Query\SelectExactMatchAttributeCodeQueryInterface;
 use Akeneo\Pim\Automation\FranklinInsights\Domain\Common\ValueObject\FamilyCode;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 /**
  * @author Olivier Pontier <olivier.pontier@akeneo.com>
@@ -33,6 +29,21 @@ class ApplyAttributeExactMatchesSpec extends ObjectBehavior
     {
         $this->beConstructedWith($selectExactMatchAttributeCodeQuery);
     }
+
+    public function it_returns_unmodified_collection_when_family_code_is_null(AttributeMappingCollection $attributeMappingCollection)
+    {
+        $context = ['familyCode' => null];
+
+        $this->process($attributeMappingCollection, $context)->shouldReturn($attributeMappingCollection);
+    }
+
+    public function it_returns_unmodified_collection_when_family_code_is_invalid_type(AttributeMappingCollection $attributeMappingCollection)
+    {
+        $context = ['familyCode' => 'family_code_as_string'];
+
+        $this->process($attributeMappingCollection, $context)->shouldReturn($attributeMappingCollection);
+    }
+
 
     public function it_applies_matched_pim_attributes($selectExactMatchAttributeCodeQuery)
     {
@@ -62,17 +73,52 @@ class ApplyAttributeExactMatchesSpec extends ObjectBehavior
         $this->process($attributeMappingCollection, $context)->shouldBeLike($expectedAttributeMappingCollection);
     }
 
-    public function it_returns_unmodified_collection_when_family_code_is_null(AttributeMappingCollection $attributeMappingCollection)
-    {
-        $context = ['familyCode' => null];
+    public function it_applies_matched_pim_attributes_only_for_pending_status(
+        SelectExactMatchAttributeCodeQueryInterface $selectExactMatchAttributeCodeQuery
+    ) {
+        $familyCode = new FamilyCode('router');
+        $context = ['familyCode' => $familyCode];
+        $matchedPimAttributeCodes = ['Color' => 'color'];
+        $pendingAttributesFranklinLabels = ['Color'];
 
-        $this->process($attributeMappingCollection, $context)->shouldReturn($attributeMappingCollection);
+        $attributeMappingCollection = new AttributeMappingCollection();
+        $attributeMappingCollection
+            ->addAttribute(new AttributeMapping('color', 'Color', 'text', null, AttributeMappingStatus::ATTRIBUTE_PENDING))
+            ->addAttribute(new AttributeMapping('weight', 'Weight', 'text', null, AttributeMappingStatus::ATTRIBUTE_INACTIVE))
+            ->addAttribute(new AttributeMapping('size', 'Size', 'text', 'pim_size', AttributeMappingStatus::ATTRIBUTE_ACTIVE))
+        ;
+
+        $expectedAttributeMappingCollection = new AttributeMappingCollection();
+        $expectedAttributeMappingCollection
+            ->addAttribute(new AttributeMapping('color', 'Color', 'text', 'color', AttributeMappingStatus::ATTRIBUTE_PENDING))
+            ->addAttribute(new AttributeMapping('weight', 'Weight', 'text', null, AttributeMappingStatus::ATTRIBUTE_INACTIVE))
+            ->addAttribute(new AttributeMapping('size', 'Size', 'text', 'pim_size', AttributeMappingStatus::ATTRIBUTE_ACTIVE))
+        ;
+
+        $selectExactMatchAttributeCodeQuery
+            ->execute($familyCode, $pendingAttributesFranklinLabels)
+            ->willReturn($matchedPimAttributeCodes);
+
+        $this->process($attributeMappingCollection, $context)->shouldBeLike($expectedAttributeMappingCollection);
     }
 
-    public function it_returns_unmodified_collection_when_family_code_is_invalid_type(AttributeMappingCollection $attributeMappingCollection)
-    {
-        $context = ['familyCode' => 'family_code_as_string'];
+    public function it_does_not_apply_matched_pim_attributes_if_the_attribute_is_already_mapped(
+        SelectExactMatchAttributeCodeQueryInterface $selectExactMatchAttributeCodeQuery
+    ) {
+        $familyCode = new FamilyCode('router');
+        $context = ['familyCode' => $familyCode];
+        $matchedPimAttributeCodes = ['Color' => 'color'];
+        $pendingAttributesFranklinLabels = ['Color'];
 
-        $this->process($attributeMappingCollection, $context)->shouldReturn($attributeMappingCollection);
+        $attributeMappingCollection = new AttributeMappingCollection();
+        $attributeMappingCollection
+            ->addAttribute(new AttributeMapping('color', 'Color', 'text', null, AttributeMappingStatus::ATTRIBUTE_PENDING))
+            ->addAttribute(new AttributeMapping('finish', 'Color/finish', 'text', 'color', AttributeMappingStatus::ATTRIBUTE_ACTIVE));
+
+        $selectExactMatchAttributeCodeQuery
+            ->execute($familyCode, $pendingAttributesFranklinLabels)
+            ->willReturn($matchedPimAttributeCodes);
+
+        $this->process($attributeMappingCollection, $context)->shouldBeLike($attributeMappingCollection);
     }
 }
