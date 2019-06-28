70913b6e6676c481f309af09e4b37cb4cb787179
Mathias Métayer <mathias.metayer@akeneo.com>
2019-06-28 10:47:39 +0200

    TIP-1159: Introduce product completeness projection model

---
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Model/Projection/ProductCompletenessSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Specification\Akeneo\Pim\Enrichment\Component\Product\Model\Projection;
+
+use Akeneo\Pim\Enrichment\Component\Product\Model\Projection\ProductCompleteness;
+use PhpSpec\ObjectBehavior;
+
+class ProductCompletenessSpec extends ObjectBehavior
+{
+    function it_is_a_product_completeness()
+    {
+        $this->beConstructedWith(
+            'ecommerce',
+            'fr_FR',
+            30,
+            ['name', 'brand', 'description', 'picture']
+        );
+        $this->shouldHaveType(ProductCompleteness::class);
+    }
+
+    function it_throws_an_exception_if_required_count_is_negative()
+    {
+        $this->beConstructedWith(
+            'ecommerce',
+            'fr_FR',
+            -5,
+            ['name', 'brand', 'description', 'picture']
+        );
+        $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
+    }
+
+    function it_calculates_the_completeness_ratio()
+    {
+        $this->beConstructedWith(
+            'ecommerce',
+            'fr_FR',
+            30,
+            ['name', 'brand', 'description', 'picture']
+        );
+        $this->ratio()->shouldReturn(86);
+    }
+
+    function it_calculates_the_completeness_ratio_when_required_count_is_zero()
+    {
+        $this->beConstructedWith(
+            'ecommerce',
+            'fr_FR',
+            0,
+            []
+        );
+        $this->ratio()->shouldReturn(100);
+    }
+}
