f0291612dd7f609a3fe1afbbea9389d4fde60bd7
jmleroux <jmleroux.pro@gmail.com>
2019-06-28 10:30:24 +0200

    Preparing version 3.1.8

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.1.7';
+    const VERSION = '3.1.8';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Slick Hare';
