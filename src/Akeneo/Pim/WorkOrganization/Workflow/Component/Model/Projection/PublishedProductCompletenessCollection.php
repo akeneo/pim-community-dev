1abbb7ff89c3249012541b19e832ebca2249d51a
Mathias Métayer <mathias.metayer@akeneo.com>
2019-07-02 17:28:49 +0200

    TIP-1159: Introduce PublishedProductCompletenessCollection

---
+++ src/Akeneo/Pim/WorkOrganization/Workflow/Component/Model/Projection/PublishedProductCompletenessCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\WorkOrganization\Workflow\Component\Model\Projection;
+
+/**
+ * @author Mathias METAYER <mathias.metayer@akeneo.com>
+ */
+class PublishedProductCompletenessCollection implements \IteratorAggregate
+{
+    /** @var int */
+    private $publishedProductId;
+
+    /** @var PublishedProductCompleteness[] */
+    private $completenesses = [];
+
+    public function __construct(int $publishedProductId, array $completenesses)
+    {
+        $this->publishedProductId = $publishedProductId;
+        foreach ($completenesses as $completeness) {
+            $this->add($completeness);
+        }
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->completenesses);
+    }
+
+    public function publishedProductId(): int
+    {
+        return $this->publishedProductId;
+    }
+
+    private function add(PublishedProductCompleteness $completeness): void
+    {
+        $key = sprintf('%s-%s', $completeness->channelCode(), $completeness->localeCode());
+        $this->completenesses[$key] = $completeness;
+    }
+}
---
+++ tests/back/Pim/WorkOrganization/Specification/Workflow/Component/Model/Projection/PublishedProductCompletenessCollectionSpec.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Specification\Akeneo\Pim\WorkOrganization\Workflow\Component\Model\Projection;
+
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\Projection\PublishedProductCompleteness;
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\Projection\PublishedProductCompletenessCollection;
+use PhpSpec\ObjectBehavior;
+
+class PublishedProductCompletenessCollectionSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(42, []);
+    }
+
+    function it_is_a_published_product_completeness_collection()
+    {
+        $this->shouldHaveType(PublishedProductCompletenessCollection::class);
+    }
+
+    function it_is_an_iterator_aggregate()
+    {
+        $this->shouldImplement(\IteratorAggregate::class);
+    }
+
+    function it_can_only_store_published_product_completenesses()
+    {
+        $this->beConstructedWith(42, [new \stdClass()]);
+        $this->shouldThrow(\TypeError::class)->duringInstantiation();
+    }
+
+    function it_exposes_a_published_product_id()
+    {
+        $this->publishedProductId()->shouldReturn(42);
+    }
+
+    function it_can_store_published_product_completenesses()
+    {
+        $completeness = new PublishedProductCompleteness('ecommerce', 'en_US', 4, []);
+        $otherCompleteness = new PublishedProductCompleteness('ecommerce', 'fr_FR', 4, []);
+        $this->beConstructedWith(42, [$completeness, $otherCompleteness]);
+
+        $this->getIterator()->count()->shouldReturn(2);
+        $this->getIterator()->getArrayCopy()->shouldReturn(
+            [
+                'ecommerce-en_US' => $completeness,
+                'ecommerce-fr_FR' => $otherCompleteness,
+            ]
+        );
+    }
+
+    function it_does_not_store_two_completenesses_with_the_same_channel_and_locale()
+    {
+        $completeness = new PublishedProductCompleteness('ecommerce', 'en_US', 4, []);
+        $otherCompleteness = new PublishedProductCompleteness('ecommerce', 'en_US', 5, ['description', 'price']);
+        $this->beConstructedWith(42, [$completeness, $otherCompleteness]);
+
+        $this->getIterator()->count()->shouldReturn(1);
+        $this->getIterator()->getArrayCopy()->shouldReturn(['ecommerce-en_US' => $otherCompleteness]);
+    }
+}
