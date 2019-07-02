5eddaceffea5436c22209209eb690d870e9dd789
jmleroux <jmleroux.pro@gmail.com>
2019-07-02 14:24:00 +0200

    Preparing version 3.0.28

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.27';
+    const VERSION = '3.0.28';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Super-Rabbit';
