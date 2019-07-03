a966494466699bf2e334256a8cd45dffc7201cb2
Samir Boulil <samir.boulil@akeneo.com>
2019-07-03 17:21:32 +0200

    PIM-8455: Update the subscriber + add specs

--- src/Akeneo/AssetManager/back/Application/Asset/Subscribers/LinkAssetSubscriber.php
+++ src/Akeneo/AssetManager/back/Application/Asset/Subscribers/LinkAssetSubscriber.php
@@ -6,10 +6,8 @@ namespace Akeneo\AssetManager\Application\Asset\Subscribers;
 
 use Akeneo\AssetManager\Domain\Event\AssetCreatedEvent;
 use Akeneo\AssetManager\Domain\Query\Asset\FindPropertyAccessibleAssetInterface;
-use Akeneo\AssetManager\Infrastructure\Rule\RuleExecutor;
+use Akeneo\AssetManager\Application\Asset\ExecuteRuleTemplates\RuleExecutor;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Akeneo\AssetManager\Domain\Model\AssetFamily\RuleTemplate;
-use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\Operators;
 
 /**
  * @author    Julien Sanchez <julien@akeneo.com>
@@ -20,15 +18,9 @@ class LinkAssetSubscriber implements EventSubscriberInterface
     /** @var RuleExecutor */
     private $ruleExecutor;
 
-    /** @var FindPropertyAccessibleAssetInterface */
-    private $findPropertyAccessibleAsset;
-
-    public function __construct(
-        RuleExecutor $ruleExecutor,
-        FindPropertyAccessibleAssetInterface $findPropertyAccessibleAsset
-    ) {
+    public function __construct(RuleExecutor $ruleExecutor)
+    {
         $this->ruleExecutor = $ruleExecutor;
-        $this->findPropertyAccessibleAsset = $findPropertyAccessibleAsset;
     }
 
     /**
@@ -43,23 +35,6 @@ class LinkAssetSubscriber implements EventSubscriberInterface
 
     public function whenAssetCreated(AssetCreatedEvent $assetCreatedEvent): void
     {
-        $asset = $this->findPropertyAccessibleAsset->find($assetCreatedEvent->getAssetFamilyIdentifier(), $assetCreatedEvent->getAssetCode());
-
-        $ruleTemplate = RuleTemplate::createFromNormalized([
-            'conditions' => [
-                [
-                    'field' => 'sku',
-                    'operator' => Operators::EQUALS,
-                    'value' => '1111111304'
-                ]
-            ],
-            'actions' => [[
-                'type' => 'add',
-                'field' => 'new_asset_multiple_link',
-                'items' => ['{{code}}']
-            ]]
-        ]);
-
-        $this->ruleExecutor->execute($ruleTemplate, $asset);
+        $this->ruleExecutor->execute($assetCreatedEvent->getAssetFamilyIdentifier(), $assetCreatedEvent->getAssetCode());
     }
 }
---
+++ src/Akeneo/AssetManager/tests/back/spec/Application/Asset/Subscribers/LinkAssetSubscriberSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Akeneo\AssetManager\Application\Asset\Subscribers;
+
+use Akeneo\AssetManager\Application\Asset\ExecuteRuleTemplates\RuleExecutor;
+use Akeneo\AssetManager\Application\Asset\Subscribers\LinkAssetSubscriber;
+use Akeneo\AssetManager\Domain\Event\AssetCreatedEvent;
+use Akeneo\AssetManager\Domain\Model\Asset\AssetCode;
+use Akeneo\AssetManager\Domain\Model\Asset\AssetIdentifier;
+use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamilyIdentifier;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
+ */
+class LinkAssetSubscriberSpec extends ObjectBehavior
+{
+    function let(RuleExecutor $ruleTemplateExecutor)
+    {
+        $this->beConstructedWith($ruleTemplateExecutor);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(LinkAssetSubscriber::class);
+    }
+
+    function it_subscribes_to_events()
+    {
+        $this::getSubscribedEvents()->shouldReturn(
+            [
+                AssetCreatedEvent::class => 'whenAssetCreated',
+            ]
+        );
+    }
+
+    function it_triggers_the_rule_template_executor(RuleExecutor $ruleTemplateExecutor)
+    {
+        $expectedAssetCode = 'starck';
+        $expectedAssetFamilyIdentifier = 'designer';
+
+        $ruleTemplateExecutor->execute(
+            Argument::that(
+                function (AssetFamilyIdentifier $actualFamilyIdentifier) use ($expectedAssetFamilyIdentifier)
+                {
+                    return $actualFamilyIdentifier->equals(
+                        AssetFamilyIdentifier::fromString($expectedAssetFamilyIdentifier)
+                    );
+                }
+            ),
+            Argument::that(
+                function (AssetCode $actualAssetCode) use ($expectedAssetCode)
+                {
+                    return $actualAssetCode->equals(AssetCode::fromString($expectedAssetCode));
+                }
+            )
+        )->shouldBeCalled();
+
+        $this->whenAssetCreated(
+            new AssetCreatedEvent(
+                AssetIdentifier::fromString('asset_identifier'),
+                AssetCode::fromString($expectedAssetCode),
+                AssetFamilyIdentifier::fromString($expectedAssetFamilyIdentifier)
+            )
+        );
+    }
+}
+
