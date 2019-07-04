283dad1566407173ea5bdfcdde699a7c90410ec5
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-07-04 17:44:28 +0200

    find critical phpunit

--- .circleci/find_critical_phpunit_bis.php
+++ .circleci/find_critical_phpunit_bis.php
@@ -1,18 +1,66 @@
 <?php
 
+use PHPUnit\Framework\TestSuite;
 use PHPUnit\Util\Configuration;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$usage = <<<USAGE
+
+USAGE;
+
+if (null === $argv[1] || null === $argv[2]) {
+    echo $usage;
+    exit -1;
+}
+
+$suiteName = $argv[1];
+$criticals = boolval($argv[2]);
 
 $configuration = Configuration::getInstance(__DIR__ . '/../app/phpunit.xml.dist');
-$testSuite = $configuration->getTestSuiteConfiguration();
+$testSuiteConfiguration = $configuration->getTestSuiteConfiguration();
 
-$suites = $testSuite->tests();
-/** @var \PHPUnit\Framework\TestSuite $suite */
-foreach ($suites as $suite) {
-    echo "\n\n\n-------------------\n";
-    $tests = $suite->tests();
-    foreach ($tests as $test) {
-        var_dump($test);
+$matchingSuites = array_filter(
+    $testSuiteConfiguration->tests(),
+    function ($suite) use ($suiteName) {
+        return $suiteName === $suite->getName();
     }
+);
+
+if (empty($matchingSuites)) {
+    throw new \Exception(sprintf('The suite "%s" does not exist!', $suiteName));
+}
+
+$suite = array_pop($matchingSuites);
+
+$testClasses = [];
+if ($criticals === true) {
+    $testClasses = classesInsideGroup($suite, 'critical');
+} else {
+    $criticalClasses = classesInsideGroup($suite, 'critical');
+    $defaultClasses = classesInsideGroup($suite, 'default');
+    $testClasses = array_diff($defaultClasses, $criticalClasses);
+}
+
+array_map(
+    function ($testClass) {
+        echo $testClass . "\n";
+    },
+    $testClasses
+);
+
+function classesInsideGroup(TestSuite $suite, string $groupName): array
+{
+    if (!in_array($groupName, $suite->getGroups())) {
+        throw new Exception(sprintf('The group "%s" does not exist in the suite "%s".', $groupName, $suite->getName()));
+    }
+
+    $group = $suite->getGroupDetails()[$groupName];
+
+    return array_map(
+        function ($suite) {
+            return $suite->getName();
+        },
+        $group
+    );
 }
