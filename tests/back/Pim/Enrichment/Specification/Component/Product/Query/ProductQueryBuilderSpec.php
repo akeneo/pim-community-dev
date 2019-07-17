54fca0f40653ca4c9582719c9a3aab264d5da6ee
Tamara Robichet <tamara.robichet@akeneo.com>
2019-07-17 15:46:19 +0200

    TIP-1185: Update PQB spec with entity_type filter

--- tests/back/Pim/Enrichment/Specification/Component/Product/Query/ProductQueryBuilderSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Query/ProductQueryBuilderSpec.php
@@ -2,7 +2,11 @@
 
 namespace Specification\Akeneo\Pim\Enrichment\Component\Product\Query;
 
+use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\FieldFilterInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\Operators;
 use Akeneo\Tool\Component\StorageUtils\Cursor\CursorFactoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Cursor\CursorInterface;
 use PhpSpec\ObjectBehavior;
 use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\SearchQueryBuilder;
 use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\FilterRegistryInterface;
@@ -10,6 +14,7 @@ use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderOptionsResolverInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\Sorter\SorterRegistryInterface;
 use Akeneo\Pim\Structure\Component\Repository\AttributeRepositoryInterface;
+use Prophecy\Argument;
 
 class ProductQueryBuilderSpec extends ObjectBehavior
 {
@@ -39,8 +44,27 @@ class ProductQueryBuilderSpec extends ObjectBehavior
         $this->shouldImplement(ProductQueryBuilderInterface::class);
     }
 
-    function it_adds_an_entity_type_filter()
+    function it_adds_an_entity_type_filter(
+        CursorFactoryInterface $cursorFactory,
+        CursorInterface $cursor,
+        FieldFilterInterface $filterField,
+        $filterRegistry
+    )
     {
+        $filterRegistry->getFieldFilter('entity_type', '=')->willReturn($filterField);
+        $cursorFactory->createCursor(Argument::any(), [] )->shouldBeCalled()->willReturn($cursor);
+        $filterField->setQueryBuilder(Argument::any())->shouldBeCalled();
+
+        $filterField->addFieldFilter(
+            "entity_type",
+            Operators::EQUALS,
+            ProductInterface::class,
+            "en_US",
+            "print",
+            ["locale" => "en_US", "scope" => "print"]
+        )->shouldBeCalled();
+
+        $this->execute()->shouldReturn($cursor);
 
     }
 }
