ad2abfa7e0032089bdf6999a7acf1fcd393ec696
Julian Prud'homme <julian.prudhomme@akeneo.com>
2019-07-18 15:36:07 +0200

    DAPI: fix end to end test launch in makefile

--- make-file/franklin-insights.mk
+++ make-file/franklin-insights.mk
@@ -3,31 +3,31 @@
 ##
 
 .PHONY: franklin-insights-coupling
-franklin-insights-coupling: vendor
+franklin-insights-coupling:
 	$(PHP_RUN) vendor/bin/php-coupling-detector detect --config-file=src/Akeneo/Pim/Automation/FranklinInsights/tests/back/.php_cd.php src/Akeneo/Pim/Automation/FranklinInsights
 
 .PHONY: franklin-insights-phpstan
-franklin-insights-phpstan: vendor
+franklin-insights-phpstan:
 	$(PHP_EXEC) vendor/bin/phpstan analyse src/Akeneo/Pim/Automation/FranklinInsights -l 1
 
 .PHONY: franklin-insights-unit
-franklin-insights-unit: vendor
+franklin-insights-unit:
 	$(PHP_EXEC) vendor/bin/phpspec run src/Akeneo/Pim/Automation/FranklinInsights/tests/back/Specification
 
 .PHONY: franklin-insights-acceptance
-franklin-insights-acceptance: vendor
+franklin-insights-acceptance:
 	$(PHP_EXEC) vendor/bin/behat -p acceptance -s franklin-insights
 
 .PHONY: franklin-insights-integration
-franklin-insights-integration: vendor
+franklin-insights-integration:
 	$(PHP_EXEC) vendor/bin/phpunit -c app --testsuite=Franklin_Insights
 
 .PHONY: franklin-insights-end-to-end
-franklin-insights-end-to-end: vendor
-	$(PHP_EXEC) vendor/bin/behat -p legacy -s franklin-insights
+franklin-insights-end-to-end:
+	$(PHP_EXEC) vendor/bin/behat -p legacy -s insights
 
 .PHONY: franklin-insights-cs
-franklin-insights-cs: vendor
+franklin-insights-cs:
 	$(PHP_EXEC) vendor/bin/php-cs-fixer fix --diff --dry-run --config=.php_cs.php
 	$(YARN_EXEC) tslint -c src/Akeneo/Pim/Automation/FranklinInsights/tslint.json src/Akeneo/Pim/Automation/FranklinInsights/**/*.{ts,tsx}
 	$(DOCKER_COMPOSE) run --rm node ./node_modules/.bin/prettier --config src/Akeneo/Pim/Automation/FranklinInsights/.prettierrc.json --check src/Akeneo/Pim/Automation/FranklinInsights/**/*.{ts,tsx}
