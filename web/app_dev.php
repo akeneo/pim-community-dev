b17fba710a9df45b963aae05da414d74a12b1901
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-07-19 11:11:53 +0200

    TO REMOVE, FOR DEBUG PURPOSE

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,13 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
 
 require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
