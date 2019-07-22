6be729ad71e80bb695eb11d77ce2bfab41fc2a09
Tamara Robichet <tamara.robichet@akeneo.com>
2019-07-22 12:03:43 +0200

    TIP-1185: Fix spec of ProductAndProductModelQueryBuilderWithSearchAggregatorFactory
    This reverts commit c276fb47bb07cfe25b788c27d72055616664afca.
    
    TIP-1185: Fix spec of ProductAndProductModelQueryBuilderWithSearchAggregatorFactory

--- tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/ProductAndProductModelQueryBuilderWithSearchAggregatorFactorySpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/ProductAndProductModelQueryBuilderWithSearchAggregatorFactorySpec.php
@@ -3,34 +3,19 @@
 namespace Specification\Akeneo\Pim\Enrichment\Bundle\Elasticsearch;
 
 use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\ProductAndProductModelSearchAggregator;
-use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\FilterRegistryInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Query\ProductAndProductModelQueryBuilder;
+use Akeneo\Pim\Enrichment\Bundle\ProductQueryBuilder\ProductAndProductModelQueryBuilder;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderFactoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderOptionsResolverInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Query\Sorter\SorterRegistryInterface;
-use Akeneo\Pim\Structure\Component\Repository\AttributeRepositoryInterface;
-use Akeneo\Tool\Component\StorageUtils\Cursor\CursorFactoryInterface;
 use PhpSpec\ObjectBehavior;
 
 class ProductAndProductModelQueryBuilderWithSearchAggregatorFactorySpec extends ObjectBehavior
 {
     function let(
-        AttributeRepositoryInterface $attributeRepository,
-        FilterRegistryInterface $filterRegistry,
-        SorterRegistryInterface $sorterRegistry,
-        CursorFactoryInterface $cursorFactory,
-        ProductQueryBuilderOptionsResolverInterface $optionsResolver,
         ProductQueryBuilderFactoryInterface $factory,
         ProductAndProductModelSearchAggregator $resultAggregator
     ) {
         $this->beConstructedWith(
             ProductAndProductModelQueryBuilder::class,
-            $attributeRepository,
-            $filterRegistry,
-            $sorterRegistry,
-            $cursorFactory,
-            $optionsResolver,
             $factory,
             $resultAggregator
         );
@@ -41,15 +26,12 @@ class ProductAndProductModelQueryBuilderWithSearchAggregatorFactorySpec extends 
         $this->shouldImplement(ProductQueryBuilderFactoryInterface::class);
     }
 
-    public function it_creates_a_product_and_product_model_query_builder(
-        $factory,
-        ProductQueryBuilderInterface $basePqb
-    )
+    public function it_creates_a_product_and_product_model_query_builder($factory, ProductQueryBuilderInterface $basePqb)
     {
         $factory->create(['default_locale' => 'en_US', 'default_scope' => 'print'])->willReturn($basePqb);
 
         $this->create(['default_locale' => 'en_US', 'default_scope' => 'print'])->shouldBeAnInstanceOf(
-            ProductAndProductModelQueryBuilder::class
+            ProductQueryBuilderInterface::class
         );
     }
 }
