ced327c2810934aa00a2559be08d2562d4a9acfd
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-07-22 20:03:26 +0200

    Preparing version 3.2.0-BETA3

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.0-BETA2';
+    const VERSION = '3.2.0-BETA3';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
