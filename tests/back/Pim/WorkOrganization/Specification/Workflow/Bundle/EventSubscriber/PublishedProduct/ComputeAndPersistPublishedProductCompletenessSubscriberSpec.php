d2fd092a27b0dc46c3ee629931d24a1096e6073d
pierallard <pierallard@gmail.com>
2019-07-23 10:19:25 +0200

    TIP-1159: Add spec

---
+++ tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/EventSubscriber/PublishedProduct/ComputeAndPersistPublishedProductCompletenessSubscriberSpec.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Specification\Akeneo\Pim\WorkOrganization\Workflow\Bundle\EventSubscriber\PublishedProduct;
+
+use Akeneo\Channel\Component\Model\Channel;
+use Akeneo\Channel\Component\Model\Locale;
+use Akeneo\Pim\Enrichment\Component\Product\Completeness\CompletenessCalculatorInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Model\Completeness;
+use Akeneo\Pim\Structure\Component\Model\Attribute;
+use Akeneo\Pim\WorkOrganization\Workflow\Bundle\EventSubscriber\PublishedProduct\ComputeAndPersistPublishedProductCompletenessSubscriber;
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\Projection\PublishedProductCompletenessCollection;
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\PublishedProduct;
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Query\SavePublishedProductCompletenesses;
+use Doctrine\Common\Collections\ArrayCollection;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class ComputeAndPersistPublishedProductCompletenessSubscriberSpec extends ObjectBehavior
+{
+    function let(
+        CompletenessCalculatorInterface $completenessCalculator,
+        SavePublishedProductCompletenesses $savePublishedProductCompletenesses
+    ) {
+        $this->beConstructedWith($completenessCalculator, $savePublishedProductCompletenesses);
+    }
+
+    function it_is_an_event_subscriber()
+    {
+        $this->shouldImplement(EventSubscriberInterface::class);
+    }
+
+    function it_is_a_compute_published_product_completeness_event_subscriber()
+    {
+        $this->shouldHaveType(ComputeAndPersistPublishedProductCompletenessSubscriber::class);
+    }
+
+    function it_computes_and_saves_completenesses_for_a_published_product(
+        CompletenessCalculatorInterface $completenessCalculator,
+        SavePublishedProductCompletenesses $savePublishedProductCompletenesses
+    ) {
+        $publishedProduct = new PublishedProduct();
+        $publishedProduct->setId(42);
+        $ecommerce = new Channel();
+        $ecommerce->setCode('ecommerce');
+        $frFr = new Locale();
+        $frFr->setCode('fr_FR');
+        $enUs = new Locale();
+        $enUs->setCode('en_US');
+        $description = new Attribute();
+        $description->setCode('description');
+        $picture = new Attribute();
+        $picture->setCode('picture');
+
+        $completenessCalculator->calculate($publishedProduct)->willReturn(
+            [
+                new Completeness($publishedProduct, $ecommerce, $enUs, new ArrayCollection([$description]), 1, 20),
+                new Completeness($publishedProduct, $ecommerce, $enUs, new ArrayCollection([$description, $picture]), 2, 25),
+            ]
+        )->shouldBeCalled();
+
+        $savePublishedProductCompletenesses->save(Argument::type(PublishedProductCompletenessCollection::class))->shouldBeCalled();
+
+        $this->computePublishedProductCompleteness(new GenericEvent($publishedProduct));
+    }
+
+    function it_does_nothing_for_anything_but_a_product(
+        CompletenessCalculatorInterface $completenessCalculator,
+        SavePublishedProductCompletenesses $savePublishedProductCompletenesses
+    ) {
+        $completenessCalculator->calculate(Argument::any())->shouldNotBeCalled();
+        $savePublishedProductCompletenesses->save(Argument::any())->shouldNotBeCalled();
+
+        $this->computePublishedProductCompleteness(new GenericEvent(new \stdClass()));
+    }
+}
