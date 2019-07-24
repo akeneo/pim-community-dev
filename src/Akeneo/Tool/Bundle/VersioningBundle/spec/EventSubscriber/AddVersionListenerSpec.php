d846c487121545addbfd62abda43d420cfc1cf77
ahocquard <alexandre.hocquard@akeneo.com>
2019-07-24 16:55:10 +0200

    TIP-901: Fix the spec

--- src/Akeneo/Tool/Bundle/VersioningBundle/spec/EventSubscriber/AddVersionListenerSpec.php
+++ src/Akeneo/Tool/Bundle/VersioningBundle/spec/EventSubscriber/AddVersionListenerSpec.php
@@ -2,23 +2,26 @@
 
 namespace spec\Akeneo\Tool\Bundle\VersioningBundle\EventSubscriber;
 
+use Akeneo\Tool\Bundle\VersioningBundle\EventSubscriber\AddVersionListener;
+use Akeneo\Tool\Bundle\VersioningBundle\Manager\VersionContext;
+use Akeneo\Tool\Bundle\VersioningBundle\Manager\VersionManager;
+use Akeneo\Tool\Bundle\VersioningBundle\UpdateGuesser\UpdateGuesserInterface;
 use PhpSpec\ObjectBehavior;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AddVersionListenerSpec extends ObjectBehavior
 {
-    function let(ContainerInterface $container)
-    {
-        $this->beConstructedWith($container);
+    function let(
+        VersionManager $versionManager,
+        NormalizerInterface $versioningNormalizer,
+        UpdateGuesserInterface $updateGuesser,
+        VersionContext $versionContext
+    ) {
+        $this->beConstructedWith($versionManager, $versioningNormalizer, $updateGuesser, $versionContext);
     }
 
-    function it_is_a_doctrine_event_listener()
+    function it_is_initializable()
     {
-        $this->shouldImplement('Doctrine\Common\EventSubscriber');
-    }
-
-    function it_subscribes_to_on_and_post_flush_events()
-    {
-        $this->getSubscribedEvents()->shouldReturn(['onFlush', 'postFlush']);
+        $this->shouldImplement(AddVersionListener::class);
     }
 }
