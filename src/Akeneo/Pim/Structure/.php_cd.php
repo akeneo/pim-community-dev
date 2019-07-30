05839218831c9774213a2b3d91ecf449030bb78f
Tamara Robichet <tamara@robichet.com>
2019-07-30 17:04:41 +0200

    TIP-1185: Fix integration test

--- src/Akeneo/Pim/Structure/.php_cd.php
+++ src/Akeneo/Pim/Structure/.php_cd.php
@@ -93,9 +93,7 @@ $rules = [
         'Akeneo\Pim\Enrichment\Component\Product\Value\MetricValueInterface',
 
         // TIP-914 FamilyNormalizer should not use PIM/Enrichment
-        'Akeneo\Pim\Enrichment\Component\Product\Normalizer\Indexing\Product\ProductNormalizer',
         'Akeneo\Pim\Enrichment\Component\Product\Normalizer\Indexing\ProductAndProductModel',
-        'Akeneo\Pim\Enrichment\Component\Product\Normalizer\Indexing\ProductModel',
     ])->in('Akeneo\Pim\Structure\Component'),
 ];
 
