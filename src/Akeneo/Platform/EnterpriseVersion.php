6a0a03436b6b7ef1fd7b8d98a5982c5f38e1651f
jmleroux <jmleroux.pro@gmail.com>
2019-07-31 16:31:02 +0200

    Preparing version 3.2.1

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.0';
+    const VERSION = '3.2.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Edelweiss';
