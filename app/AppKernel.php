50080ecfe4b7cca2faa239960b9b5e4c34233dda
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-07-31 18:39:31 +0200

    TIP-901: move bundles.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,7 +24,7 @@ class AppKernel extends Kernel
      */
     public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir().'/app/bundles.php';
+        $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
