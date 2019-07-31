9123279b450cc66103b59abb66bb59a0e76dff6b
pierallard <pierallard@gmail.com>
2019-07-31 10:38:34 +0200

    TIP-1159 [Product Masks]: Queries

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Completeness/Query/GetProducts.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Completeness\Query;
+
+use Akeneo\Pim\Enrichment\Component\Product\Completeness\Model\CompletenessProductMask;
+
+/**
+ * @author    Pierre Allard <pierre.allard@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface GetProducts
+{
+    /**
+     * @param string[] $productIdentifiers
+     *
+     * @return CompletenessProductMask[]
+     */
+    public function fromProductIdentifiers(array $productIdentifiers): array;
+}
