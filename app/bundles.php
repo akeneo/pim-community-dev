9a9d4e6927058d98c6d17ef5f9f2eb4529b4f82b
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-02 22:54:19 +0200

    TIP-901: Fix SF4 removal support of ACLs

--- app/bundles.php
+++ app/bundles.php
@@ -10,6 +10,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\AclBundle\AclBundle::class => ['all' => true],
 
     // ORO dependencies
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
