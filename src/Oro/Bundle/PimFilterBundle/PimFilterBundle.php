eb101e64346ac49f404afb5127a978aa7186b0f3
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-05 15:55:39 +0200

    TIP-901: remove bundle override

--- src/Oro/Bundle/PimFilterBundle/PimFilterBundle.php
+++ src/Oro/Bundle/PimFilterBundle/PimFilterBundle.php
@@ -13,11 +13,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimFilterBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getParent()
-    {
-        return 'OroFilterBundle';
-    }
 }
