1cc69aacc52cf93f57eb441aeb49bdecdcc48691
pierallard <pierallard@gmail.com>
2019-08-05 13:15:12 +0000

    Preparing version 3.0.35

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.34';
+    const VERSION = '3.0.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rose';
