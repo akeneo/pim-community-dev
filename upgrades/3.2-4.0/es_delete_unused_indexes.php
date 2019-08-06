51387ec56f78d8fb3fe9c5dcb8a613f66e57832c
Tamara Robichet <tamarasaurus@users.noreply.github.com>
2019-08-06 14:19:19 +0200

    TIP-1185: Add migration script (#10525)
    * TIP-1185: Add migration script
    
    TIP-1185: Delete old product index by alias
    
    TIP-1185: Use name without param
    
    TIP-1185: Delete index correctly
    
    TIP-1185: Delete index correctly
    
    TIP-1185: Fix delete params
    
    TIP-1185: Delete akeneo_pim_product_model alias and index
    
    Fix
    
    * TIP-1185: Fix review comments
    
    * TIP-1185: Make the output nicer
    
    * TIP-1185: Clean akeneo_pim_published_product_and_product_model
    
    * TIP-1185: Fix review comments

---
+++ upgrades/3.2-4.0/es_delete_unused_indexes.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!file_exists(__DIR__ . '/../../src/Kernel.php')) {
+    die("Please run this command from your Symfony application root.");
+}
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../src/Kernel.php';
+require __DIR__ . '/../../app/bootstrap.php';
+
+$kernel = new Kernel('prod', false);
+$kernel->boot();
+$container = $kernel->getContainer();
+
+$builder = $container->get('akeneo_elasticsearch.client_builder');
+$hosts = [$container->getParameter('index_hosts')];
+
+$client = $builder->setHosts($hosts)->build()->indices();
+
+function deleteAliasAndIndex($client, $aliasName) {
+    if ($client->existsAlias(['name' => $aliasName ])) {
+        echo sprintf("Deleting alias \"%s\" and index.\n", $aliasName);
+        $alias = $client->getAlias(['name' => $aliasName ]);
+        $index = array_keys($alias)[0];
+        $client->deleteAlias([ 'index' => $index, 'name' => $aliasName ]);
+        return $client->delete(['index' => $index]);
+    }
+
+    echo sprintf(
+        "The alias \"%s\" does not exist. This could be because it's part of the Enterprise Edition.\n",
+        $aliasName
+    );
+}
+
+deleteAliasAndIndex($client, 'akeneo_pim_product');
+deleteAliasAndIndex($client, 'akeneo_pim_product_model');
+deleteAliasAndIndex($client, 'akeneo_pim_published_product_and_product_model');
+
+echo "Done.\n";
