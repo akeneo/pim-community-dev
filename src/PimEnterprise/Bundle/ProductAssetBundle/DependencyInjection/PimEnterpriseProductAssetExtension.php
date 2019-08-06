e775afec6b53d91ec02b7e14d0efa786d425fa28
pierallard <pierallard@gmail.com>
2019-08-06 11:20:49 +0200

    PIM-8331: Fix association screen when associated products contain asset_collection

--- src/PimEnterprise/Bundle/ProductAssetBundle/DependencyInjection/PimEnterpriseProductAssetExtension.php
+++ src/PimEnterprise/Bundle/ProductAssetBundle/DependencyInjection/PimEnterpriseProductAssetExtension.php
@@ -63,6 +63,7 @@ class PimEnterpriseProductAssetExtension extends Extension
         $loader->load('normalizers.yml');
         $loader->load('providers.yml');
         $loader->load('queries.yml');
+        $loader->load('query_builders.yml');
         $loader->load('readers.yml');
         $loader->load('removers.yml');
         $loader->load('repositories.yml');
