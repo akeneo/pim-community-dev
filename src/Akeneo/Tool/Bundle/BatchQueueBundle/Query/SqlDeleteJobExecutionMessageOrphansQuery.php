0da613c3402154ec3d5710af7c2eb051f055970d
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-07 14:13:44 +0200

    Move BatchQueue lost classes

--- src/Akeneo/Tool/Bundle/BatchQueueBundle/Query/SqlDeleteJobExecutionMessageOrphansQuery.php
+++ src/Akeneo/Tool/Bundle/BatchQueueBundle/Query/SqlDeleteJobExecutionMessageOrphansQuery.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Bundle\BatchQueueBundle\Query;
+namespace Akeneo\Tool\Bundle\BatchQueueBundle\Query;
 
-use Akeneo\Component\BatchQueue\Query\DeleteJobExecutionMessageOrphansQueryInterface;
+use Akeneo\Tool\Component\BatchQueue\Query\DeleteJobExecutionMessageOrphansQueryInterface;
 use Doctrine\DBAL\Connection;
 
 /**
