050c7eebeae0ee3dd5975bb7699d88eb1c4ac5f5
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-08 13:46:31 +0200

    TIP-901: remove direct access to container

--- src/Oro/Bundle/DataGridBundle/Controller/GridController.php
+++ src/Oro/Bundle/DataGridBundle/Controller/GridController.php
@@ -2,20 +2,29 @@
 
 namespace Oro\Bundle\DataGridBundle\Controller;
 
+use Oro\Bundle\DataGridBundle\Datagrid\ManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GridController extends Controller
 {
+    /** @var ManagerInterface */
+    private $manager;
+
+    public function __construct(ManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
     /**
      * @param string $gridName
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function getAction($gridName)
+    public function get($gridName)
     {
-        $grid = $this->get('oro_datagrid.datagrid.manager')->getDatagrid($gridName);
+        $grid = $this->manager->getDatagrid($gridName);
         $result = $grid->getData();
 
         return new JsonResponse($result->toArray());
