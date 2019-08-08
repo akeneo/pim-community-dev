18208bb570d53e938ad2b2cd54422b020d403b3b
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-08 13:44:32 +0200

    TIP-901: remove dead code

--- src/Oro/Bundle/DataGridBundle/Controller/GridController.php
+++ src/Oro/Bundle/DataGridBundle/Controller/GridController.php
@@ -2,11 +2,8 @@
 
 namespace Oro\Bundle\DataGridBundle\Controller;
 
-use Oro\Bundle\DataGridBundle\Extension\MassAction\MassActionDispatcher;
-use Oro\Bundle\DataGridBundle\Extension\MassAction\MassActionParametersParser;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class GridController extends Controller
@@ -22,32 +19,5 @@ class GridController extends Controller
         $result = $grid->getData();
 
         return new JsonResponse($result->toArray());
-    }
-
-    /**
-     * @param string $gridName
-     * @param string $actionName
-     *
-     * @throws \LogicException
-     * @return Response
-     */
-    public function massActionAction(Request $request, $gridName, $actionName)
-    {
-        /** @var MassActionParametersParser $massActionParametersParser */
-        $parametersParser = $this->get('oro_datagrid.mass_action.parameters_parser');
-        $parameters = $parametersParser->parse($request);
-
-        $requestData = array_merge($request->query->all(), $request->request->all());
-
-        /** @var MassActionDispatcher $massActionDispatcher */
-        $massActionDispatcher = $this->get('oro_datagrid.mass_action.dispatcher');
-        $response = $massActionDispatcher->dispatch($gridName, $actionName, $parameters, $requestData);
-
-        $data = [
-            'successful' => $response->isSuccessful(),
-            'message'    => $response->getMessage(),
-        ];
-
-        return new JsonResponse(array_merge($data, $response->getOptions()));
     }
 }
