7497c40f7391288029a9de083e62d28272fca3e9
pierallard <pierallard@gmail.com>
2019-08-13 11:47:30 +0200

    TIP-1159: Fix CI

--- tests/back/Pim/Enrichment/Specification/Bundle/Doctrine/ORM/Query/CompleteFilterSpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/Doctrine/ORM/Query/CompleteFilterSpec.php
@@ -2,16 +2,16 @@
 
 namespace Specification\Akeneo\Pim\Enrichment\Bundle\Doctrine\ORM\Query;
 
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Connection;
 use PhpSpec\ObjectBehavior;
 use Akeneo\Pim\Enrichment\Bundle\Doctrine\ORM\Query\CompleteFilter;
 use Akeneo\Pim\Enrichment\Component\Product\ProductAndProductModel\Query\CompleteFilterInterface;
 
 class CompleteFilterSpec extends ObjectBehavior
 {
-    function let(EntityManagerInterface $entityManager)
+    function let(Connection $connection)
     {
-        $this->beConstructedWith($entityManager);
+        $this->beConstructedWith($connection);
     }
 
     function it_is_initializable()
