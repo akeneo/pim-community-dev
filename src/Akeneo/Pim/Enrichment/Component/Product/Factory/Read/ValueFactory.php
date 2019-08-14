7b87797911fdb5f76842483de5be4acb54f065a2
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2019-08-14 16:22:37 +0200

    TIP-1073: Fix tests

--- src/Akeneo/Pim/Enrichment/Component/Product/Factory/Read/ValueFactory.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Factory/Read/ValueFactory.php
@@ -13,7 +13,7 @@ use Webmozart\Assert\Assert;
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class ValueFactory
+class ValueFactory
 {
     /** @var array|SingleValueFactory[] */
     private $valueFactories;
