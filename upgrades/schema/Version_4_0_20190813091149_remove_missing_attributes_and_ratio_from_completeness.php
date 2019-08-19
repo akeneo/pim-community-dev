f8116d3234b053e045ea5301eae071863b5442f8
pierallard <pierallard@gmail.com>
2019-08-19 11:03:19 +0200

    TIP-1159: Rename upgrade file

--- upgrades/schema/Version_4_0_20190813091149_remove_missing_attributes_and_ratio_from_completeness.php
+++ upgrades/schema/Version_4_0_20190813091149_remove_missing_attributes_and_ratio_from_completeness.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
  * - Removes the table `pim_catalog_completeness_missing_attribute`
  * - Drops the column ratio from table `pim_catalog_completeness`
  */
-final class Version_3_2_20190813091149_remove_missing_attributes_and_ratio_from_completeness extends AbstractMigration
+final class Version_4_0_20190813091149_remove_missing_attributes_and_ratio_from_completeness extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
