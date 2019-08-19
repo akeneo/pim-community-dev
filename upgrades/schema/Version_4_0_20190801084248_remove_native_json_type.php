00e4144f1a10bc22dd9d0a5769e6ca3ec849899b
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-08-19 12:48:01 +0200

    TIP-901: database migration for native_json type removal

---
+++ upgrades/schema/Version_4_0_20190801084248_remove_native_json_type.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_4_0_20190801084248_remove_native_json_type extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("ALTER TABLE pimee_franklin_insights_subscription MODIFY raw_suggested_data JSON COMMENT ''");
+        $this->addSql("ALTER TABLE pimee_workflow_product_draft MODIFY raw_values JSON COMMENT ''");
+        $this->addSql("ALTER TABLE pimee_workflow_product_model_draft MODIFY raw_values JSON COMMENT ''");
+        $this->addSql("ALTER TABLE pimee_workflow_published_product MODIFY raw_values JSON COMMENT ''");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
