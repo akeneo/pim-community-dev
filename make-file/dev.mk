d2dfc50ba5cdae6c8636cc46be4c369be93b0662
arnaud <arnaud.langlade@akeneo.com>
2019-08-28 16:20:33 +0200

    TIP-1204 Fix review feedback

--- make-file/dev.mk
+++ make-file/dev.mk
@@ -3,7 +3,7 @@
 ##
 
 .PHONY: coupling
-coupling: twa-coupling asset-coupling franklin-insights-coupling reference-entity-coupling rule-engine-coupling workflow-coupling permission-coupling
+coupling: structure-coupling user-management-coupling channel-coupling enrichment-coupling
 
 .PHONY: phpspec
 phpspec:
@@ -17,8 +17,8 @@ acceptance: behat.yml
 phpunit:
 	${PHP_RUN} vendor/bin/phpunit -c phpunit.xml.dist ${F}
 
-.PHONY: end-to-end
-end-to-end: behat.yml
+.PHONY: behat-legacy
+behat-legacy: behat.yml
 	APP_ENV=behat $(PHP_EXEC) vendor/bin/behat -p legacy -s all ${F}
 
 ##
@@ -27,11 +27,11 @@ end-to-end: behat.yml
 
 ## Enable Xdebug
 .PHONY: xdebug-on
-xdebug-on: docker-compose.override.yml
+xdebug-on:
 	XDEBUG_ENABLED=1 $(MAKE) up
 
 ## Disable Xdebug
 .PHONY: xdebug-off
-xdebug-off: docker-compose.override.yml
+xdebug-off:
 	XDEBUG_ENABLED=0 $(MAKE) up
 
