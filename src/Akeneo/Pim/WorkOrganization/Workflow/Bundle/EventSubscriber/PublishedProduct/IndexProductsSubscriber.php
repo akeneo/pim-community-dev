9c420d815bc5669c716383549cb09ace2507eff5
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2019-08-29 11:23:26 +0200

    TIP-1220: remove index is now done in POST_REMOVE

--- src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/EventSubscriber/PublishedProduct/IndexProductsSubscriber.php
+++ src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/EventSubscriber/PublishedProduct/IndexProductsSubscriber.php
@@ -64,7 +64,7 @@ class IndexProductsSubscriber implements EventSubscriberInterface
         return [
             StorageEvents::POST_SAVE => ['indexProduct', 300],
             StorageEvents::POST_SAVE_ALL => ['bulkIndexProducts', 300],
-            StorageEvents::PRE_REMOVE => ['deleteProduct', 300],
+            StorageEvents::POST_REMOVE => ['deleteProduct', 300],
         ];
     }
 
