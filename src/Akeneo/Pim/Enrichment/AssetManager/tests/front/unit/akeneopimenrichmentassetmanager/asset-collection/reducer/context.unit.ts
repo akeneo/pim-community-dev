1daba5dafb66aaf1de1d8a8b6854ec515a91be80
christophe-chausseray <chausseray.christophe@gmail.com>
2019-08-29 18:02:06 +0200

    PIM-8552: Remove useless describe

--- src/Akeneo/Pim/Enrichment/AssetManager/tests/front/unit/akeneopimenrichmentassetmanager/asset-collection/reducer/context.unit.ts
+++ src/Akeneo/Pim/Enrichment/AssetManager/tests/front/unit/akeneopimenrichmentassetmanager/asset-collection/reducer/context.unit.ts
@@ -6,83 +6,81 @@ import {
   selectCurrentLocale,
 } from 'akeneopimenrichmentassetmanager/assets-collection/reducer/context';
 
-describe('akeneo > enrichment > asset collection > reducer > context', () => {
-  test('It ignore other commands', () => {
-    const state = {};
-    const newState = contextReducer(state, {
-      type: 'ANOTHER_ACTION',
-    });
-
-    expect(newState).toMatchObject(state);
+test('It ignore other commands', () => {
+  const state = {};
+  const newState = contextReducer(state, {
+    type: 'ANOTHER_ACTION',
   });
 
-  test('It should generate a default state', () => {
-    const newState = contextReducer(undefined, {
-      type: 'ANOTHER_ACTION',
-    });
+  expect(newState).toMatchObject(state);
+});
 
-    expect(newState).toMatchObject({locale: '', channel: ''});
+test('It should generate a default state', () => {
+  const newState = contextReducer(undefined, {
+    type: 'ANOTHER_ACTION',
   });
 
-  test('It should update the locale in the state', () => {
-    const state = {locale: '', channel: ''};
-    const newState = contextReducer(state, {
-      type: 'LOCALE_UPDATED',
-      locale: 'en_US',
-    });
+  expect(newState).toMatchObject({locale: '', channel: ''});
+});
 
-    expect(newState).toMatchObject({locale: 'en_US', channel: ''});
+test('It should update the locale in the state', () => {
+  const state = {locale: '', channel: ''};
+  const newState = contextReducer(state, {
+    type: 'LOCALE_UPDATED',
+    locale: 'en_US',
   });
 
-  test('It should update the channel in the state', () => {
-    const state = {locale: '', channel: ''};
-    const newState = contextReducer(state, {
-      type: 'CHANNEL_UPDATED',
-      channel: 'ecommerce',
-    });
+  expect(newState).toMatchObject({locale: 'en_US', channel: ''});
+});
 
-    expect(newState).toMatchObject({locale: '', channel: 'ecommerce'});
+test('It should update the channel in the state', () => {
+  const state = {locale: '', channel: ''};
+  const newState = contextReducer(state, {
+    type: 'CHANNEL_UPDATED',
+    channel: 'ecommerce',
   });
 
-  test('It should have an action to update the locale', () => {
-    const locale = 'en_US';
-    const expectedAction = {
-      type: 'LOCALE_UPDATED',
-      locale,
-    };
+  expect(newState).toMatchObject({locale: '', channel: 'ecommerce'});
+});
 
-    expect(localeUpdated(locale)).toMatchObject(expectedAction);
-  });
+test('It should have an action to update the locale', () => {
+  const locale = 'en_US';
+  const expectedAction = {
+    type: 'LOCALE_UPDATED',
+    locale,
+  };
 
-  test('It should have an action to update the channel', () => {
-    const channel = 'ecommerce';
-    const expectedAction = {
-      type: 'CHANNEL_UPDATED',
-      channel,
-    };
+  expect(localeUpdated(locale)).toMatchObject(expectedAction);
+});
 
-    expect(channelUpdated(channel)).toMatchObject(expectedAction);
-  });
+test('It should have an action to update the channel', () => {
+  const channel = 'ecommerce';
+  const expectedAction = {
+    type: 'CHANNEL_UPDATED',
+    channel,
+  };
 
-  test('It should be able to select the context from the state', () => {
-    const state = {
-      context: {channel: 'ecommerce', locale: 'en_US'},
-      structure: {attributes: [], channels: [], family: null},
-      values: [],
-    };
-    const expectedContext = {channel: 'ecommerce', locale: 'en_US'};
+  expect(channelUpdated(channel)).toMatchObject(expectedAction);
+});
 
-    expect(selectContext(state)).toMatchObject(expectedContext);
-  });
+test('It should be able to select the context from the state', () => {
+  const state = {
+    context: {channel: 'ecommerce', locale: 'en_US'},
+    structure: {attributes: [], channels: [], family: null},
+    values: [],
+  };
+  const expectedContext = {channel: 'ecommerce', locale: 'en_US'};
 
-  test('It should be able to select the current locale from the state', () => {
-    const state = {
-      context: {channel: 'ecommerce', locale: 'en_US'},
-      structure: {attributes: [], channels: [], family: null},
-      values: [],
-    };
-    const expectedCurrentLocale = 'en_US';
+  expect(selectContext(state)).toMatchObject(expectedContext);
+});
 
-    expect(selectCurrentLocale(state)).toEqual(expectedCurrentLocale);
-  });
+test('It should be able to select the current locale from the state', () => {
+  const state = {
+    context: {channel: 'ecommerce', locale: 'en_US'},
+    structure: {attributes: [], channels: [], family: null},
+    values: [],
+  };
+  const expectedCurrentLocale = 'en_US';
+
+  expect(selectCurrentLocale(state)).toEqual(expectedCurrentLocale);
 });
