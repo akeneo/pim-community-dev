3672ae061820e1bd1d57eb3796dbcfb94f969c12
jmleroux <jmleroux.pro@gmail.com>
2019-08-29 18:35:22 +0200

    PIM-7127: Hide version from CSS call

--- src/Pim/Bundle/EnrichBundle/spec/VersionStrategy/CacheBusterVersionStrategySpec.php
+++ src/Pim/Bundle/EnrichBundle/spec/VersionStrategy/CacheBusterVersionStrategySpec.php
@@ -2,15 +2,13 @@
 
 namespace spec\Pim\Bundle\EnrichBundle\VersionStrategy;
 
-use Pim\Bundle\EnrichBundle\VersionStrategy\CacheBusterVersionStrategy;
-use Pim\Bundle\CatalogBundle\VersionProviderInterface;
-use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use Pim\Bundle\CatalogBundle\VersionProviderInterface;
 
 class CacheBusterVersionStrategySpec extends ObjectBehavior
 {
-    public function let(VersionProviderInterface $versionProvider) {
+    public function let(VersionProviderInterface $versionProvider)
+    {
         $this->beConstructedWith($versionProvider);
     }
 
@@ -29,14 +27,16 @@ class CacheBusterVersionStrategySpec extends ObjectBehavior
     public function it_returns_the_versioned_asset_path($versionProvider)
     {
         $versionProvider->getPatch()->willReturn('2.0.2');
+        $hash = md5('2.0.2');
 
-        $this->applyVersion('css/pim.css')->shouldReturn('css/pim.css?2.0.2');
+        $this->applyVersion('css/pim.css')->shouldReturn('css/pim.css?' . $hash);
     }
 
     public function it_returns_the_versioned_asset_path_with_leading_slash($versionProvider)
     {
         $versionProvider->getPatch()->willReturn('1.7.8');
+        $hash = md5('1.7.8');
 
-        $this->applyVersion('/js/main.dist.js')->shouldReturn('/js/main.dist.js?1.7.8');
+        $this->applyVersion('/js/main.dist.js')->shouldReturn('/js/main.dist.js?' . $hash);
     }
 }
