2faaa87a764ba46b104c2d7889a6bad0d44985aa
christophe-chausseray <chausseray.christophe@gmail.com>
2019-08-29 14:02:04 +0200

    PIM-8552: Tests unitaires on the value generator

---
+++ src/Akeneo/Pim/Enrichment/AssetManager/tests/front/unit/akeneopimenrichmentassetmanager/asset-collection/application/value-generator.unit.ts
@@ -0,0 +1,411 @@
+import generate from 'akeneopimenrichmentassetmanager/assets-collection/application/value-generator';
+import {fetchAssetAttributes} from 'akeneopimenrichmentassetmanager/assets-collection/infrastructure/fetcher/attribute';
+import {fetchPermissions} from 'akeneopimenrichmentassetmanager/assets-collection/infrastructure/fetcher/permission';
+import {AttributeGroupPermission, LocalePermission} from 'akeneopimenrichmentassetmanager/assets-collection/infrastructure/fetcher/permission';
+import {AttributeCode} from 'akeneopimenrichmentassetmanager/platform/model/structure/attribute';
+import {CategoryCode} from 'akeneopimenrichmentassetmanager/enrich/domain/model/product';
+
+jest.mock('pim/fetcher-registry', () => {});
+fetchAssetAttributes = jest.fn();
+fetchPermissions = jest.fn();
+
+test('It should generate a value collection from the product with all attributes editable', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: true,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: true,
+  };
+  const isReadOnly = false;
+  const attributesForThisLevel = ['packshot', 'notices'];
+  const categoriesEditPermission = ['scanners'];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: true,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: true,
+    },
+  ];
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+test('It should generate a value collection from the product with an attribute group non editable', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: false,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: true,
+  };
+  const isReadOnly = false;
+  const attributesForThisLevel = ['packshot', 'notices'];
+  const categoriesEditPermission = ['scanners'];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: false,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: false,
+    },
+  ];
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+test('It should generate a value collection from the product with a locale non editable', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: true,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: false,
+  };
+  const isReadOnly = false;
+  const attributesForThisLevel = ['packshot', 'notices'];
+  const categoriesEditPermission = ['scanners'];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: false,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: true,
+    },
+  ];;
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+test('It should generate a value collection from the product with a read only attribute', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: true,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: true,
+  };
+  const isReadOnly = true;
+  const attributesForThisLevel = ['packshot', 'notices'];
+  const categoriesEditPermission = ['scanners'];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: true,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: false,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: true,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: false,
+    },
+  ];
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+test('It should generate a value collection from the product with a non editable parent attribute', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: true,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: true,
+  };
+  const isReadOnly = false;
+  const attributesForThisLevel = ['notices'];
+  const categoriesEditPermission = ['scanners'];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: false,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: true,
+    },
+  ];
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+test('It should generate a value collection from the product with a non editable category', async () => {
+  const attributeGroupPermission = {
+    code: 'marketing',
+    view: true,
+    edit: true,
+  }
+  const localePermission = {
+    code: 'en_US',
+    view: true,
+    edit: true,
+  };
+  const isReadOnly = false;
+  const attributesForThisLevel = ['packshot', 'notices'];
+  const categoriesEditPermission = [];
+
+  fetchAssetAttributes.mockImplementation(() => getMockAssetAttributes(isReadOnly));
+  fetchPermissions.mockImplementation(() => getMockPermissions(attributeGroupPermission, localePermission, categoriesEditPermission));
+  const product = getMockProduct(attributesForThisLevel);
+  const expectedValueCollection = [
+    {
+      attribute: {
+        code: 'packshot',
+        labels: {
+          en_US: 'Packshot',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'packshot',
+      },
+      locale: 'en_US',
+      channel: 'ecommerce',
+      data: ['iphone'],
+      editable: false,
+    },
+    {
+      attribute: {
+        code: 'notices',
+        labels: {
+          en_US: 'Notices',
+        },
+        group: 'marketing',
+        isReadOnly: false,
+        referenceDataName: 'notices',
+      },
+      locale: null,
+      channel: 'ecommerce',
+      data: [],
+      editable: false,
+    },
+  ];
+
+  const valueCollection = await generate(product);
+  expect(valueCollection).toEqual(expectedValueCollection);
+});
+
+const getMockAssetAttributes = (isReadOnly: boolean) => {
+  return [
+    {
+      code: 'packshot',
+      labels: {
+        en_US: 'Packshot',
+      },
+      group: 'marketing',
+      isReadOnly,
+      referenceDataName: 'packshot',
+    },
+    {
+      code: 'notices',
+      labels: {
+        en_US: 'Notices',
+      },
+      group: 'marketing',
+      isReadOnly,
+      referenceDataName: 'notices',
+    },
+  ];
+}
+
+const getMockPermissions = (attributeGroupPermission: AttributeGroupPermission, localePermission: LocalePermission, categoriesEditPermission: CategoryCode[]) => {
+  return {
+    attributeGroups: [attributeGroupPermission],
+    locales: [localePermission],
+    categories: {
+      EDIT_ITEMS: categoriesEditPermission,
+    },
+  };
+}
+
+const getMockProduct = (attributesForThisLevel: AttributeCode[]) => {
+  return {
+    meta: {
+      attributes_for_this_level: attributesForThisLevel,
+      level: 1,
+      completeness: {
+        channel: 'ecommerce',
+        labels: {en_US: 'E-commerce'},
+        locales: {
+          en_US: {
+            completeness: {
+              required: 2,
+              missing: 1,
+            },
+          },
+        },
+      },
+    },
+    values: {
+      packshot: [
+        {
+          data: ['iphone'],
+          scope: 'ecommerce',
+          locale: 'en_US',
+        },
+      ],
+      notices: [
+        {
+          data: [],
+          scope: 'ecommerce',
+          locale: null,
+        },
+      ],
+    },
+    categories: ['scanners', 'xerox'],
+  };
+}
