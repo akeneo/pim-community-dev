cd9a81df5b3f22cf53a42a3cbe9a3f034cc1d9bd
Samir Boulil <samir.boulil@akeneo.com>
2019-08-29 16:59:19 +0200

    PIM-8711: Add attribute type query & tests

---
+++ src/Akeneo/AssetManager/back/Domain/Query/Attribute/GetAttributeTypeInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\AssetManager\Domain\Query\Attribute;
+
+use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamilyIdentifier;
+use Akeneo\AssetManager\Domain\Model\Attribute\AttributeCode;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ */
+Interface GetAttributeTypeInterface
+{
+    public function fetch(AssetFamilyIdentifier $assetFamilyIdentifier, AttributeCode $attributeCode): string;
+}
