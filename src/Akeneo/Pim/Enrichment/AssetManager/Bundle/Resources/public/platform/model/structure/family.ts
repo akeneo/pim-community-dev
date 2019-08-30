4e0f3a0a377b3e9e0f1e94e594b93abab4b589fa
christophe-chausseray <chausseray.christophe@gmail.com>
2019-08-30 13:22:34 +0200

    PIM-8552: Fix after rebase

--- src/Akeneo/Pim/Enrichment/AssetManager/Bundle/Resources/public/platform/model/structure/family.ts
+++ src/Akeneo/Pim/Enrichment/AssetManager/Bundle/Resources/public/platform/model/structure/family.ts
@@ -1,11 +1,11 @@
 import {AttributeCode} from 'akeneopimenrichmentassetmanager/platform/model/structure/attribute';
 
 export type FamilyCode = string;
-type AttributeRequirements = {
+export type AttributeRequirements = {
   [key: string]: AttributeCode[];
 };
 
 export type Family = {
   code: FamilyCode;
-  attribute_requirements: AttributeRequirements;
+  attributeRequirements: AttributeRequirements;
 };
