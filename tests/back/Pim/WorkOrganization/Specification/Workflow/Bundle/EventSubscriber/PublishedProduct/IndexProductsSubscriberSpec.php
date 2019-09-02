4dabcb13a1ccb0f0f2996e695b578877494ccdcd
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2019-09-02 16:03:52 +0200

    clean code

--- tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/EventSubscriber/PublishedProduct/IndexProductsSubscriberSpec.php
+++ tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/EventSubscriber/PublishedProduct/IndexProductsSubscriberSpec.php
@@ -18,8 +18,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
- * Class IndexProductsSubscriberSpec
- *
  * @author    Nicolas Marniesse <nicolas.marniesse@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
