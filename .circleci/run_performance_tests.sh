701f63fd8d29433c6febd7785f5c04823758cc23
Mathias Métayer <mathias.metayer@akeneo.com>
2019-09-04 10:33:36 +0200

    Fix back performance scripts

--- .circleci/run_performance_tests.sh
+++ .circleci/run_performance_tests.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+# In order to execute this script locally, you will need to set the BLACKFIRE_xxx environment variables in your host, e.g:
+# export BLACKFIRE_SERVER_ID=my_blackfire_server_id
+# export BLACKFIRE_SERVER_TOKEN=my_blackfire_server_token
+# export BLACKFIRE_CLIENT_ID=my_blackfire_client_id
+# export BLACKFIRE_CLIENT_TOKEN=my_blackfire_client_token
+
 SCRIPT_DIR=$(dirname $0)
 $SCRIPT_DIR/../tests/back/Performance/install_blackfire.sh
 $SCRIPT_DIR/../tests/back/Performance/load_reference_catalog.sh
@@ -9,4 +15,5 @@ $SCRIPT_DIR/../tests/back/Performance/load_reference_catalog.sh
 # wait ES to index everything before starting anything, which can alter the result
 sleep 10
 
-docker-compose run -T php ./vendor/bin/phpunit -c app/phpunit.xml.dist --log-junit var/tests/phpunit/phpunit_$(uuidgen).xml --testsuite PIM_Performance_Test
+docker-compose exec -T -e BLACKFIRE_SERVER_ID -e BLACKFIRE_SERVER_TOKEN -e BLACKFIRE_CLIENT_ID -e BLACKFIRE_CLIENT_TOKEN \
+ -e APP_ENV=behat -u www-data fpm ./vendor/bin/phpunit -c phpunit.xml.dist --log-junit var/tests/phpunit/phpunit_$(uuidgen).xml --testsuite PIM_Performance_Test
--- tests/back/Performance/load_reference_catalog.sh
+++ tests/back/Performance/load_reference_catalog.sh
@@ -4,18 +4,19 @@ set -eu
 
 SCRIPT_DIR=$(dirname $0)
 DOCKER_BRIDGE_IP=$(ip address show | grep "global docker" | cut -c10- | cut -d '/' -f1)
-PUBLIC_PIM_HTTP_PORT=$(docker-compose port httpd-behat 80 | cut -d ':' -f 2)
+PUBLIC_PIM_HTTP_PORT=$(docker-compose port httpd 80 | cut -d ':' -f 2)
 REFERENCE_CATALOG_FILE="$SCRIPT_DIR/reference_catalog.yml"
+DOCKER_COMPOSE_EXEC='docker-compose exec -T -u www-data -e APP_ENV=behat'
 
 echo "Reset the database with the minimal catalog"
 
-docker-compose exec -T fpm bin/console pim:install:db -e behat
+$DOCKER_COMPOSE_EXEC fpm bin/console pim:install:db -e behat
 
 echo "Generates an API user for the benchmarks in test environment"
 
-docker-compose exec -T fpm bin/console pim:user:create -e behat --admin -n -- admin admin test@example.com John Doe en_US
+$DOCKER_COMPOSE_EXEC fpm bin/console pim:user:create -e behat --admin -n -- admin admin test@example.com John Doe en_US
 
-CREDENTIALS=$(docker-compose exec -T fpm bin/console pim:oauth-server:create-client --no-ansi -e behat generator | tr -d '\r ')
+CREDENTIALS=$($DOCKER_COMPOSE_EXEC fpm bin/console pim:oauth-server:create-client --no-ansi -e behat generator | tr -d '\r ')
 export API_CLIENT=$(echo $CREDENTIALS | cut -d " " -f 2 | cut -d ":" -f 2)
 export API_SECRET=$(echo $CREDENTIALS | cut -d " " -f 3 | cut -d ":" -f 2)
 export API_URL="http://$DOCKER_BRIDGE_IP:${PUBLIC_PIM_HTTP_PORT}"
