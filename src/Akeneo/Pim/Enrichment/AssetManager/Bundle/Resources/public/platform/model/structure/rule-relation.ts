096cd73da711156adfd721258772f2302d008840
christophe-chausseray <chausseray.christophe@gmail.com>
2019-09-06 17:04:47 +0200

    PIM-8500: Add rule-relation model

---
+++ src/Akeneo/Pim/Enrichment/AssetManager/Bundle/Resources/public/platform/model/structure/rule-relation.ts
@@ -0,0 +1,23 @@
+import {AttributeCode} from 'akeneopimenrichmentassetmanager/platform/model/structure/attribute';
+
+export type RuleCode = string;
+export type RuleRelation = {
+  attribute: AttributeCode;
+  rule: RuleCode;
+};
+
+export const isSmartAttribute = (attributeCode: AttributeCode, ruleRelations: RuleRelation[]): boolean => {
+  const isExistInRelationRule = ruleRelations.some(
+    (ruleRelation: RuleRelation) => attributeCode === ruleRelation.attribute
+  );
+
+  return isExistInRelationRule;
+};
+
+export const getRulesForAttribute = (attributeCode: AttributeCode, ruleRelations: RuleRelation[]): RuleCode[] => {
+  const rulesForAttribute = ruleRelations.filter(
+    (ruleRelation: RuleRelation) => attributeCode === ruleRelation.attribute
+  );
+
+  return rulesForAttribute.map((ruleRelation: RuleRelation) => ruleRelation.rule);
+};
