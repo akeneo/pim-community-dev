bf505d9ac7b8954ea7f204cd0ca99d819673f020
jmleroux <jmleroux.pro@gmail.com>
2019-09-13 15:38:36 -0400

    Preparing version 3.0.42

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.41';
+    const VERSION = '3.0.42';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rose';
