c92ffae20ef369e110b111199a92980f76b2f041
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2019-09-17 11:22:17 +0200

    PIM-8738: fix memory leak on purge job execution
    This is a backport of PIM-8424 done on 3.0

--- src/Akeneo/Bundle/BatchBundle/Command/PurgeJobExecutionCommand.php
+++ src/Akeneo/Bundle/BatchBundle/Command/PurgeJobExecutionCommand.php
@@ -2,7 +2,7 @@
 
 namespace Akeneo\Bundle\BatchBundle\Command;
 
-use Akeneo\Bundle\BatchBundle\Job\DoctrineJobRepository;
+use Akeneo\Bundle\BatchBundle\Persistence\Sql\DeleteJobExecution;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -43,13 +43,8 @@ class PurgeJobExecutionCommand extends ContainerAwareCommand
     {
         $days = $this->getNumberOfDaysOption($input);
 
-        $jobsExecutions = $this->getJobExecutionRepository()->findPurgeables($days);
-
-        if (!empty($jobsExecutions)) {
-            //TODO TIP-385: re-wite this implementation to use 'akeneo_batch.remover.job_execution'
-            $this->getJobExecutionRepository()->remove($jobsExecutions);
-            $output->write(sprintf("%s jobs execution deleted ...\n", count($jobsExecutions)));
-        }
+        $numberOfDeletedJobExecution = $this->getDeleteJobExecutionQuery()->olderThanDays($days);
+        $output->writeln(sprintf("%s jobs execution deleted ...", $numberOfDeletedJobExecution));
 
         $this->deleteJobExecutionMessageOrphans();
     }
@@ -69,11 +64,11 @@ class PurgeJobExecutionCommand extends ContainerAwareCommand
     }
 
     /**
-     * @return DoctrineJobRepository
+     * @return DeleteJobExecution
      */
-    protected function getJobExecutionRepository()
+    protected function getDeleteJobExecutionQuery(): DeleteJobExecution
     {
-        return $this->getContainer()->get('akeneo_batch.job_repository');
+        return $this->getContainer()->get('akeneo_batch.delete_job_execution');
     }
 
     private function deleteJobExecutionMessageOrphans(): void
---
+++ tests/back/Integration/integration/BatchBundle/Persistence/Sql/DeleteJobExecutionIntegration.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Test\Integration\integration\BatchBundle\Persistence\Sql;
+
+use Akeneo\Bundle\BatchBundle\Persistence\Sql\DeleteJobExecution;
+use Akeneo\Test\Integration\TestCase;
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\Assert;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class DeleteJobExecutionIntegration extends TestCase
+{
+    public function test_that_it_deletes_all_jobs_older_than_one_day_except_the_most_recent_one_for_each_job_instance()
+    {
+        $this->launchJob();
+        $this->launchJob();
+        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
+        Assert::assertSame(2, $numberOfJobs);
+
+        $this->getConnection()->executeUpdate('UPDATE akeneo_batch_job_execution SET end_time = Date_ADD(end_time, INTERVAL -10 day)');
+
+        $deleteJobExecution = $this->getDeleteQuery();
+        $deleteJobExecution->olderThanDays(1);
+
+        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
+        Assert::assertSame(1, $numberOfJobs);
+    }
+
+    private function getDeleteQuery(): DeleteJobExecution
+    {
+        return $this->get('akeneo_batch.delete_job_execution');
+    }
+
+    private function launchJob(array $arrayInput = []): BufferedOutput
+    {
+        $application = new Application(static::$kernel);
+        $application->setAutoExit(false);
+
+        $defaultArrayInput = [
+            'command'  => 'akeneo:batch:job',
+            'code'     => 'csv_product_export',
+        ];
+
+        $arrayInput = array_merge($defaultArrayInput, $arrayInput);
+        if (isset($arrayInput['--config'])) {
+            $arrayInput['--config'] = json_encode($arrayInput['--config']);
+        }
+
+        $input = new ArrayInput($arrayInput);
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        return $output;
+    }
+
+    private function getConnection(): Connection
+    {
+        return $this->get('database_connection');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration()
+    {
+        return $this->catalog->useTechnicalCatalog();
+    }
+}
