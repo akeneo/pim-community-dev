16b4eb4350a3a8c2cb7645087596a4444cf2c82b
arnaud <arnaud.langlade@akeneo.com>
2019-09-17 15:56:15 +0200

    TIP-1262: Turn PurgeJobExecutionCommand into a service

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Command/PurgeJobExecutionCommand.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Command/PurgeJobExecutionCommand.php
@@ -4,6 +4,7 @@ namespace Akeneo\Platform\Bundle\ImportExportBundle\Command;
 
 use Akeneo\Platform\Bundle\ImportExportBundle\Purge\PurgeJobExecution;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,16 +16,27 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PurgeJobExecutionCommand extends ContainerAwareCommand
+class PurgeJobExecutionCommand extends Command
 {
+    protected static $defaultName = 'akeneo:batch:purge-job-execution';
+
     const DEFAULT_NUMBER_OF_DAYS = 90;
+
+    /** @var PurgeJobExecution */
+    private $purgeJobExecution;
+
+    public function __construct(PurgeJobExecution $purgeJobExecution)
+    {
+        parent::__construct();
+
+        $this->purgeJobExecution = $purgeJobExecution;
+    }
 
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
-        $this->setName('akeneo:batch:purge-job-execution');
         $this->setDescription(
             'Purge jobs execution older than number of days you want except the last one, by default 90 days, minimum is 1 day'
         );
@@ -51,12 +63,7 @@ class PurgeJobExecutionCommand extends ContainerAwareCommand
             return;
         }
 
-        $numberOfDeletedJobExecutions = $this->purgeJobExecution()->olderThanDays($days);
+        $numberOfDeletedJobExecutions = $this->purgeJobExecution->olderThanDays($days);
         $output->write(sprintf("%s jobs execution deleted ...\n", $numberOfDeletedJobExecutions));
-    }
-
-    private function purgeJobExecution(): PurgeJobExecution
-    {
-        return  $this->getContainer()->get('akeneo.platform.import_export.purge_job_execution');
     }
 }
