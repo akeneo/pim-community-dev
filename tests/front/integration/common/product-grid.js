965d40347b00eb14226c351d18c2613d871cdff8
Tamara Robichet <tamara@robichet.com>
2019-09-26 17:25:10 +0200

    Pass in generated products to response

--- tests/front/integration/common/product-grid.js
+++ tests/front/integration/common/product-grid.js
@@ -3,21 +3,7 @@ const adminUser = new UserBuilder().withUsername('admin').build();
 const datagridLoad = require('./responses/datagrid-load.json');
 const { readFileSync } = require('fs');
 
-const datagridProducts = [];
-
-const productGridData = {
-  data: datagridProducts,
-  totalRecords: datagridProducts.length,
-  options: {
-    totalRecords: datagridProducts.length
-  }
-}
-
-const productLoadData = Object.assign(datagridLoad, {
-  data: JSON.stringify(productGridData)
-})
-
-const responses = {
+const mockResponses = {
   'http://pim.com/js/extensions.json': ['application/json', require(`${process.cwd()}/public/js/extensions.json`)],
   'http://pim.com/rest/security/': ['application/json', require('./responses/rest-security.json')],
   'http://pim.com/configuration/locale/rest?activated=true': ['application/json', require('./responses/activated-locales.json')],
@@ -32,15 +18,34 @@ const responses = {
   'http://pim.com/enrich/product-category-tree/product-grid/children.json?dataLocale=undefined&context=view&id=1&select_node_id=-2&with_items_count=1&include_sub=1': [
     'application/json', require('./responses/category-tree.json')
   ],
-  'http://pim.com/datagrid/product-grid/load?dataLocale=en_US&params%5BdataLocale%5D=en_US&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject+Object%5D': [
-    'application/json', productLoadData
-  ]
 }
 
-const matchResponses = (page) => {
+const matchResponses = (page, products, filters) => {
+  const datagridProducts = [];
+  const productGridFilters = [];
+
+  const productGridData = {
+    data: datagridProducts,
+    totalRecords: datagridProducts.length,
+    options: {
+      totalRecords: datagridProducts.length
+    }
+  }
+
+  const productLoadData = Object.assign(datagridLoad, {
+    data: JSON.stringify(productGridData)
+  })
+
+  const productGridResponses = {
+    'http://pim.com/datagrid/product-grid/load?dataLocale=en_US&params%5BdataLocale%5D=en_US&product-grid%5B_parameters%5D%5Bview%5D%5Bcolumns%5D=identifier%2Cimage%2Clabel%2Cfamily%2Cenabled%2Ccompleteness%2Ccreated%2Cupdated%2Ccomplete_variant_products%2Csuccess%2C%5Bobject+Object%5D': [
+    'application/json', productLoadData
+      ],
+      'http://pim.com/datagrid/product-grid/attributes-filters?page=1&locale=en_US': productGridFilters
+  };
+
   return page.on('request', (interceptedRequest) => {
     // console.log('url', interceptedRequest.url())
-    const response = responses[interceptedRequest.url()];
+    const response = Object.assign(mockResponses, productGridResponses)[interceptedRequest.url()];
 
     if (!response) {
       return interceptedRequest.continue();
@@ -56,8 +61,8 @@ const matchResponses = (page) => {
   })
 }
 
-const loadProductGrid = async (page) => {
-  await matchResponses(page);
+const loadProductGrid = async (page, products, filters) => {
+  await matchResponses(page, products, filters);
 
   await page.evaluate(async () => await require('pim/init')());
   await page.evaluate(async () => await require('pim/user-context').initialize());
