a64a02678bd13c227552ef91ebf736b5a76343af
Tamara Robichet <tamara.robichet@akeneo.com>
2019-09-27 16:47:30 +0200

    Increase puppeteer viewport, fix teardown

--- tests/front/integration/jest/setup.js
+++ tests/front/integration/jest/setup.js
@@ -9,9 +9,13 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 module.exports = async function() {
   const browser = await puppeteer.launch({
       ignoreHTTPSErrors: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080'],
       headless: false,
-      devtools: true
+      devtools: true,
+      defaultViewport: {
+        width: 1920,
+        height: 1080
+      }
     });
   // store the browser instance so we can teardown it later
   global.__BROWSER__ = browser;
