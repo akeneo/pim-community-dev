94dde9c645a5a4f8a662bfdb6624b1c4fee335c5
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2019-10-01 09:44:58 +0200

    TIP-1285: add subscriber post product model save and save all
    * TIP-1285: add subscriber post product model save and save all
    
    * TIP-1285: add changelog
    
    * TIP-1285: remove useless class
    
    * TIP-1285: fix random test
    
    * Update tests/back/Pim/Enrichment/Integration/Doctrine/Common/Saver/ComputeDescendantProductCompletenessesIntegration.php
    
    Co-Authored-By: Alexandre Hocquard <alexandre.hocquard@akeneo.com>
    
    * TIP-1285: fix test

--- tests/back/Pim/Enrichment/Specification/Bundle/EventSubscriber/IndexProductModelsSubscriberSpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/EventSubscriber/IndexProductModelsSubscriberSpec.php
@@ -26,36 +26,9 @@ class IndexProductModelsSubscriberSpec extends ObjectBehavior
     function it_subscribes_to_events()
     {
         $this->getSubscribedEvents()->shouldReturn([
-            StorageEvents::POST_SAVE => 'indexProductModel',
-            StorageEvents::POST_SAVE_ALL => 'bulkIndexProductModels',
             StorageEvents::POST_REMOVE => 'deleteProductModel',
             StorageEvents::POST_REMOVE_ALL => 'bulkDeleteProductModels',
         ]);
-    }
-
-    function it_indexes_a_single_product_model($productModelIndexer, ProductModelInterface $productModel)
-    {
-        $event = new GenericEvent($productModel->getWrappedObject(), ['unitary' => true]);
-        $productModel->getCode()->willReturn('identifier');
-
-        $productModelIndexer->indexFromProductModelCode('identifier')->shouldBeCalled();
-
-        $this->indexProductModel($event);
-    }
-
-    function it_bulk_indexes_products(
-        $productModelIndexer,
-        ProductModelInterface $productModel1,
-        ProductModelInterface $productModel2
-    ) {
-        $event = new GenericEvent([$productModel1->getWrappedObject(), $productModel2->getWrappedObject()]);
-
-        $productModel1->getCode()->willReturn('identifier1');
-        $productModel2->getCode()->willReturn('identifier2');
-
-        $productModelIndexer->indexFromProductModelCodes(['identifier1', 'identifier2'])->shouldBeCalled();
-
-        $this->bulkIndexProductModels($event);
     }
 
     function it_deletes_a_product_model_from_elasticsearch_index(
@@ -67,54 +40,6 @@ class IndexProductModelsSubscriberSpec extends ObjectBehavior
         $productModelIndexer->removeFromProductModelId(40)->shouldBeCalled();
 
         $this->deleteProductModel($event)->shouldReturn(null);
-    }
-
-    function it_does_not_index_a_non_product_model_entity($productModelIndexer, GenericEvent $event, \stdClass $subject)
-    {
-        $event->getSubject()->willReturn($subject);
-        $productModelIndexer->indexFromProductModelCode(Argument::cetera())->shouldNotBeCalled();
-
-        $this->indexProductModel($event);
-    }
-
-    function it_does_not_index_a_non_unitary_save_of_a_product_model(
-        $productModelIndexer,
-        ProductModelInterface $productModel
-    ) {
-        $event = new GenericEvent($productModel->getWrappedObject(), ['unitary' => false]);
-
-        $productModelIndexer->indexFromProductModelCode(Argument::any())->shouldNotBeCalled();
-
-        $this->indexProductModel($event);
-    }
-
-    function it_does_not_index_a_non_unitary_save_of_a_product_model_bis(
-        $productModelIndexer,
-        ProductModelInterface $productModel
-    ) {
-        $event = new GenericEvent($productModel->getWrappedObject());
-
-        $productModelIndexer->indexFromProductModelCode(Argument::any())->shouldNotBeCalled();
-
-        $this->indexProductModel($event);
-    }
-
-    function it_does_not_bulk_index_non_product_model_entities($productModelIndexer)
-    {
-        $event = new GenericEvent(new \stdClass);
-
-        $productModelIndexer->indexFromProductModelCodes(Argument::any())->shouldNotBeCalled();
-
-        $this->bulkIndexProductModels($event);
-    }
-
-    function it_does_not_bulk_index_non_collections($productModelIndexer)
-    {
-        $event = new GenericEvent(new \stdClass);
-
-        $productModelIndexer->indexFromProductModelCodes(Argument::any())->shouldNotBeCalled();
-
-        $this->bulkIndexProductModels($event);
     }
 
     function it_does_not_delete_non_product_model_entity_from_elasticsearch($productModelIndexer)
