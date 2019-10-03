715058df8649974bccd9cd400eede46fb65e640a
Mathias Métayer <mmetayer@users.noreply.github.com>
2019-10-03 08:31:26 +0200

    TIP-1159: Remove unnecessary service definition override (#6938)

--- src/Akeneo/Pim/Permission/Bundle/DependencyInjection/AkeneoPimPermissionExtension.php
+++ src/Akeneo/Pim/Permission/Bundle/DependencyInjection/AkeneoPimPermissionExtension.php
@@ -60,6 +60,5 @@ class AkeneoPimPermissionExtension extends Extension
         $loader->load('job_defaults.yml');
         $loader->load('jobs.yml');
         $loader->load('validators.yml');
-        $loader->load('cli_commands.yml');
     }
 }
