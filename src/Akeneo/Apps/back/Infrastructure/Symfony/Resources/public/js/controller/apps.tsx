41ce4747849362750dbd4edbcf6e20483185755b
Paul Chasle <paul.chasle@akeneo.com>
2019-10-03 18:03:22 +0200

    Fix menu display after reload

--- src/Akeneo/Apps/back/Infrastructure/Symfony/Resources/public/js/controller/apps.tsx
+++ src/Akeneo/Apps/back/Infrastructure/Symfony/Resources/public/js/controller/apps.tsx
@@ -5,9 +5,13 @@ import * as ReactDOM from 'react-dom';
 import {Apps} from '@akeneo-pim-ce/apps';
 
 const BaseController = require('pim/controller/base');
+const mediator = require('oro/mediator');
 
 class AppsController extends BaseController {
   renderRoute() {
+    mediator.trigger('pim_menu:highlight:tab', {extension: 'pim-menu-system'});
+    mediator.trigger('pim_menu:highlight:item', {extension: 'pim-menu-system-apps'});
+
     ReactDOM.render(<Apps />, this.el);
 
     return $.Deferred().resolve();
