275526f414b5b5cb0a01c18161ea18c7887c8be3
jmleroux <jean-marie.leroux@akeneo.com>
2019-10-09 07:07:47 +0000

    Preparing version 2.3.66

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.65';
+    const VERSION = '2.3.66';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
