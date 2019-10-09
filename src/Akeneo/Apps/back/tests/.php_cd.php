e2c6e1e1b2b1615bfba3018c0f969be71a4d290d
romain <romain@akeneo.com>
2019-10-09 17:30:59 +0200

    Fix comments

--- src/Akeneo/Apps/back/tests/.php_cd.php
+++ src/Akeneo/Apps/back/tests/.php_cd.php
@@ -13,24 +13,15 @@ $builder = new RuleBuilder();
 $rules = [
     $builder->only(
         [
-            'Symfony\Component\EventDispatcher\EventSubscriberInterface',
-            'Symfony\Component\EventDispatcher\GenericEvent',
-
-            'Symfony\Component\Filesystem\Exception\IOException',
-            'Symfony\Component\Filesystem\Filesystem',
-            'Symfony\Component\Finder\Finder',
+            'Symfony\Component',
 
             'Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvents',
-
         ]
     )->in('Akeneo\Apps\Infrastructure\Install'),
 
     $builder->only(
         [
-            'Symfony\Component\Config\FileLocator',
-            'Symfony\Component\DependencyInjection',
-            'Symfony\Component\HttpKernel\Bundle\Bundle',
-            'Symfony\Component\HttpKernel\DependencyInjection\Extension',
+            'Symfony\Component',
         ]
     )->in('Akeneo\Apps\Infrastructure\Symfony'),
 ];
