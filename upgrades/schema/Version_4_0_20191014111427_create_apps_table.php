84af657d0e68d43635bded0af20b9bced8bb82ba
romain <romain@akeneo.com>
2019-10-14 15:51:10 +0200

    Fixup migration

--- upgrades/schema/Version_4_0_20191014111427_create_apps_table.php
+++ upgrades/schema/Version_4_0_20191014111427_create_apps_table.php
@@ -1,26 +1,26 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Pim\Upgrade\Schema;
 
 use Akeneo\Apps\Infrastructure\Install\Query\CreateAppsTableQuery;
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Indexer\ProductModelDescendantsIndexer;
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Indexer\ProductModelIndexer;
-use Akeneo\Pim\Enrichment\Component\Product\Factory\EmptyValuesCleaner;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
 use Doctrine\Migrations\AbstractMigration;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * This migration will delete the empty values from raw values of product models.
- * For example, the value {attr: {<all_channels>: {<all_locales>: []}}} will be removed from the raw_values field.
+ * This migration will create the apps table
+ *
+ * @author Romain Monceau <romain@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 final class Version_4_0_20191014111427_create_apps_table
     extends AbstractMigration
     implements ContainerAwareInterface
 {
-    /** @var ContainerInterface */
     private $container;
 
     /**
