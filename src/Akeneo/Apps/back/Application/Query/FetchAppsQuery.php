c5f4876a09f05ea48e2f6edc1741f25990533bb0
romain <romain@akeneo.com>
2019-10-14 17:34:33 +0200

    API-788: Implement query handler to fetch apps

--- src/Akeneo/Apps/back/Application/Query/FetchAppsQuery.php
+++ src/Akeneo/Apps/back/Application/Query/FetchAppsQuery.php
@@ -2,18 +2,15 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Apps\Domain\Persistence\Repository;
+namespace Akeneo\Apps\Application\Query;
 
-use Akeneo\Apps\Domain\Model\App;
+use Akeneo\Apps\Domain\Persistence\Repository\AppRepository;
 
 /**
  * @author Romain Monceau <romain@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-interface AppRepository
+class FetchAppsQuery
 {
-    public function create(App $app): void;
-
-    public function fetchAll(): array;
 }
