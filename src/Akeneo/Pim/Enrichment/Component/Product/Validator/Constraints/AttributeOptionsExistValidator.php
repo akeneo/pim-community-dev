8ac1a8e33d9960de038a92c25e97118a902938f5
Mathias Métayer <mathias.metayer@akeneo.com>
2019-10-16 13:01:28 +0200

    PIM-8879: Skip empty values

--- src/Akeneo/Pim/Enrichment/Component/Product/Validator/Constraints/AttributeOptionsExistValidator.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Validator/Constraints/AttributeOptionsExistValidator.php
@@ -33,41 +33,47 @@ class AttributeOptionsExistValidator extends ConstraintValidator
             throw new UnexpectedTypeException($constraint, AttributeOptionsExist::class);
         }
 
-        if ($values instanceof WriteValueCollection) {
-            $optionValues = $values->filter(
-                function (ValueInterface $value): bool {
-                    return $value instanceof OptionValueInterface || $value instanceof OptionsValueInterface;
-                }
-            );
+        if (!($values instanceof WriteValueCollection)) {
+            return;
+        }
 
-            if ($optionValues->isEmpty()) {
-                return;
+        $optionValues = $values->filter(
+            function (ValueInterface $value): bool {
+                // @todo @merge master/4.0: remove the tests on values emptiness as there will be no more empty values
+                // in clear, just let:
+                // return $value instanceof OptionValueInterface || $value instanceof OptionsValueInterface;
+                return ($value instanceof OptionValueInterface && null !== $value->getData()) ||
+                    ($value instanceof OptionsValueInterface && [] !== $value->getData());
             }
+        );
 
-            $existingOptionCodes = $this->getExistingOptionCodesIndexedByAttributeCodes($optionValues);
+        if ($optionValues->isEmpty()) {
+            return;
+        }
 
-            foreach ($optionValues as $key => $value) {
-                if ($value instanceof OptionValueInterface) {
-                    if (!in_array($value->getData(), ($existingOptionCodes[$value->getAttributeCode()] ?? []))) {
-                        $this->context->buildViolation(
-                            $constraint->message,
-                            [
-                                '%attribute_code%' => $value->getAttributeCode(),
-                                '%invalid_option%' => $value->getData(),
-                            ]
-                        )->atPath(sprintf('[%s]', $key))->addViolation();
-                    }
-                } elseif ($value instanceof OptionsValueInterface) {
-                    $diff = array_diff($value->getData(), ($existingOptionCodes[$value->getAttributeCode()] ?? []));
-                    if (!empty($diff)) {
-                        $this->context->buildViolation(
-                            $constraint->messagePlural,
-                            [
-                                '%attribute_code%' => $value->getAttributeCode(),
-                                '%invalid_options%' => implode(', ', $diff),
-                            ]
-                        )->atPath(sprintf('[%s]', $key))->addViolation();
-                    }
+        $existingOptionCodes = $this->getExistingOptionCodesIndexedByAttributeCodes($optionValues);
+
+        foreach ($optionValues as $key => $value) {
+            if ($value instanceof OptionValueInterface) {
+                if (!in_array($value->getData(), ($existingOptionCodes[$value->getAttributeCode()] ?? []))) {
+                    $this->context->buildViolation(
+                        $constraint->message,
+                        [
+                            '%attribute_code%' => $value->getAttributeCode(),
+                            '%invalid_option%' => $value->getData(),
+                        ]
+                    )->atPath(sprintf('[%s]', $key))->addViolation();
+                }
+            } elseif ($value instanceof OptionsValueInterface) {
+                $notExistingOptionCodes = array_diff($value->getData(), ($existingOptionCodes[$value->getAttributeCode()] ?? []));
+                if (!empty($notExistingOptionCodes)) {
+                    $this->context->buildViolation(
+                        $constraint->messagePlural,
+                        [
+                            '%attribute_code%' => $value->getAttributeCode(),
+                            '%invalid_options%' => implode(', ', $notExistingOptionCodes),
+                        ]
+                    )->atPath(sprintf('[%s]', $key))->addViolation();
                 }
             }
         }
@@ -82,16 +88,15 @@ class AttributeOptionsExistValidator extends ConstraintValidator
             if (!isset($optionCodesIndexedByAttributeCode[$attributeCode])) {
                 $optionCodesIndexedByAttributeCode[$attributeCode] = [];
             }
-            $optionCodes = [];
+
             if ($value instanceof OptionValueInterface) {
-                $optionCodes = [$value->getData()];
+                $optionCodesIndexedByAttributeCode[$attributeCode][] = $value->getData();
             } elseif ($value instanceof OptionsValueInterface) {
-                $optionCodes = $value->getData();
+                $optionCodesIndexedByAttributeCode[$attributeCode] = array_merge(
+                    $optionCodesIndexedByAttributeCode[$attributeCode],
+                    $value->getData()
+                );
             }
-            $optionCodesIndexedByAttributeCode[$attributeCode] = array_merge(
-                $optionCodesIndexedByAttributeCode[$attributeCode],
-                $optionCodes
-            );
         }
 
         return $this->getExistingAttibuteOptionCodes->fromOptionCodesByAttributeCode(
