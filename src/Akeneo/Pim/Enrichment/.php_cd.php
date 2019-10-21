f2baa3ed155838d0da2bd42ae5c93b9da86375d1
jmleroux <jmleroux.pro@gmail.com>
2019-10-21 10:39:19 +0200

    Fix coupling rules

--- src/Akeneo/Pim/Enrichment/.php_cd.php
+++ src/Akeneo/Pim/Enrichment/.php_cd.php
@@ -18,6 +18,7 @@ $rules = [
         'Oro\Bundle\SecurityBundle\SecurityFacade',
         'Oro\Bundle\SecurityBundle\Annotation\AclAncestor',
         'Liip\ImagineBundle',
+        'Dompdf\Dompdf',
         'Webmozart\Assert\Assert',
         // TODO the feature use the datagrid
         'Oro\Bundle\DataGridBundle',
