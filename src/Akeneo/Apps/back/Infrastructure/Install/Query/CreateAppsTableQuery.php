7eebca6e777403498718f559575f8abd5638f7a0
romain <romain@akeneo.com>
2019-10-21 17:03:18 +0200

    API-652: Fix foreign key naming

--- src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppsTableQuery.php
+++ src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppsTableQuery.php
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS akeneo_app(
     label VARCHAR(100) NOT NULL,
     flow_type ENUM('data_destination', 'data_source', 'other') NOT NULL DEFAULT 'other',
     created DATETIME NOT NULL COMMENT '(DC2Type:datetime)' DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT client_id FOREIGN KEY (client_id) REFERENCES pim_api_client (id),
+    CONSTRAINT FK_APP_pim_api_client_app_client_id FOREIGN KEY (client_id) REFERENCES pim_api_client (id),
     INDEX IDX_APP_code (code)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
 SQL;
