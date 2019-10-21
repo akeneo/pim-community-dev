e1afefb04b1e35cf1e961c9b1cbef1f0a9689336
jean-marie leroux <jmleroux.pro@gmail.com>
2019-10-21 22:39:11 +0200

    Preparing version 2.9.1 (#10928)
    Preparing version 2.9.1

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.68';
+    const VERSION = '2.9.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
