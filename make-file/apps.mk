0b23d0ed7aeee73471355f9f121e60665466dfd3
romain <romain@akeneo.com>
2019-10-22 10:46:26 +0200

    API-650: Launch Apps integration test with phpunit

--- make-file/apps.mk
+++ make-file/apps.mk
@@ -1,7 +1,7 @@
 _APPS_YARN_RUN = $(YARN_EXEC) run --cwd=src/Akeneo/Apps/front/
 
 # Tests
-
+# Back
 apps-coupling:
 	$(PHP_RUN) vendor/bin/php-coupling-detector detect --config-file=src/Akeneo/Apps/back/tests/.php_cd.php src/Akeneo/Apps/back
 
@@ -11,6 +11,10 @@ apps-back-phpspec:
 apps-back-acceptance:
 	$(PHP_RUN) vendor/bin/behat --strict --config src/Akeneo/Apps/back/tests/Acceptance/behat.yml
 
+apps-back-integration:
+    $(PHP_RUN) vendor/bin/phpunit -c phpunit.xml.dist --testsuite=Akeneo_Apps_Integration
+
+# Front
 apps-front-tests:
 	$(_APPS_YARN_RUN) jest
 
