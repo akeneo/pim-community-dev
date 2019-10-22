7da123a1cadd6e187aa6fec949541d95acf13945
Mathias Métayer <mathias.metayer@akeneo.com>
2019-10-22 11:48:10 +0200

    Fix and improve back_performance job

--- .circleci/run_performance_tests.sh
+++ .circleci/run_performance_tests.sh
@@ -2,18 +2,12 @@
 
 set -eu
 
-# In order to execute this script locally, you will need to set the BLACKFIRE_xxx environment variables in your host, e.g:
-# export BLACKFIRE_SERVER_ID=my_blackfire_server_id
-# export BLACKFIRE_SERVER_TOKEN=my_blackfire_server_token
-# export BLACKFIRE_CLIENT_ID=my_blackfire_client_id
-# export BLACKFIRE_CLIENT_TOKEN=my_blackfire_client_token
-
 SCRIPT_DIR=$(dirname $0)
-$SCRIPT_DIR/../tests/back/Performance/install_blackfire.sh
+
 $SCRIPT_DIR/../tests/back/Performance/load_reference_catalog.sh
 
-# wait ES to index everything before starting anything, which can alter the result
+# wait ES to index everything before starting anything, which can alter the result, and warm it up with a first query
 sleep 10
+docker-compose run -T -u www-data --rm php curl -s http://elasticsearch:9200/akeneo_pim_product_and_product_model/_search >> /dev/null
 
-docker-compose exec -T -e BLACKFIRE_SERVER_ID -e BLACKFIRE_SERVER_TOKEN -e BLACKFIRE_CLIENT_ID -e BLACKFIRE_CLIENT_TOKEN \
- -e APP_ENV=behat -u www-data fpm ./vendor/bin/phpunit -c phpunit.xml.dist --log-junit var/tests/phpunit/phpunit_$(uuidgen).xml --testsuite PIM_Performance_Test
+docker-compose run -T -u www-data --rm php ./vendor/bin/phpunit -c phpunit.xml.dist --log-junit var/tests/phpunit/phpunit_$(uuidgen).xml --testsuite PIM_Performance_Test
