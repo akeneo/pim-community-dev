f83b35098c391b9940de2fedd475140183acfc98
Michel Tag <micheltag@akeneo.com>
2019-10-23 11:59:53 +0200

    Preparing version 3.0.48 (#7132)
    Preparing version 3.0.48
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.47';
+    const VERSION = '3.0.48';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rose';
