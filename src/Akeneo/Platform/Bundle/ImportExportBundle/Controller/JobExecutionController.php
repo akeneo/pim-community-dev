40c1f54b63d5308bb5884dc5f764b4fd98861cc5
pierallard <pierallard@gmail.com>
2019-10-31 15:19:36 +0100

    [Deadcode] Remove JobExecution Controllers useless parameters

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Controller/JobExecutionController.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Controller/JobExecutionController.php
@@ -4,21 +4,17 @@ namespace Akeneo\Platform\Bundle\ImportExportBundle\Controller;
 
 use Akeneo\Platform\Bundle\ImportExportBundle\Event\JobExecutionEvents;
 use Akeneo\Platform\Bundle\ImportExportBundle\Repository\InternalApi\JobExecutionRepository;
-use Akeneo\Tool\Bundle\BatchBundle\Manager\JobExecutionManager;
-use Akeneo\Tool\Bundle\BatchBundle\Monolog\Handler\BatchLogHandler;
 use Akeneo\Tool\Bundle\ConnectorBundle\EventListener\JobExecutionArchivist;
 use Akeneo\Tool\Component\Connector\LogKey;
 use Akeneo\Tool\Component\FileStorage\StreamedFileResponse;
 use League\Flysystem\FileNotFoundException;
 use League\Flysystem\FilesystemInterface;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Job execution controller
@@ -29,26 +25,11 @@ use Symfony\Component\Translation\TranslatorInterface;
  */
 class JobExecutionController
 {
-    /** @var BatchLogHandler */
-    protected $batchLogHandler;
-
     /** @var JobExecutionArchivist */
     protected $archivist;
 
-    /** @var string */
-    protected $jobType;
-
-    /** @var JobExecutionManager */
-    protected $jobExecutionManager;
-
-    /** @var EngineInterface */
-    protected $templating;
-
     /** @var EventDispatcherInterface */
     protected $eventDispatcher;
-
-    /** @var TranslatorInterface */
-    protected $translator;
 
     /** @var JobExecutionRepository */
     protected $jobExecutionRepo;
@@ -57,36 +38,21 @@ class JobExecutionController
     private $logFileSystem;
 
     /**
-     * @param EngineInterface          $templating
-     * @param TranslatorInterface      $translator
      * @param EventDispatcherInterface $eventDispatcher
-     * @param BatchLogHandler          $batchLogHandler
      * @param JobExecutionArchivist    $archivist
-     * @param JobExecutionManager      $jobExecutionManager
      * @param JobExecutionRepository   $jobExecutionRepo
      * @param FilesystemInterface      $logFileSystem
-     * @param string                   $jobType
      */
     public function __construct(
-        EngineInterface $templating,
-        TranslatorInterface $translator,
         EventDispatcherInterface $eventDispatcher,
-        BatchLogHandler $batchLogHandler,
         JobExecutionArchivist $archivist,
-        JobExecutionManager $jobExecutionManager,
         JobExecutionRepository $jobExecutionRepo,
-        FilesystemInterface $logFileSystem,
-        $jobType
+        FilesystemInterface $logFileSystem
     ) {
-        $this->templating = $templating;
-        $this->translator = $translator;
         $this->eventDispatcher = $eventDispatcher;
-        $this->batchLogHandler = $batchLogHandler;
         $this->archivist = $archivist;
-        $this->jobExecutionManager = $jobExecutionManager;
         $this->jobExecutionRepo = $jobExecutionRepo;
         $this->logFileSystem = $logFileSystem;
-        $this->jobType = $jobType;
     }
 
     /**
@@ -141,15 +107,5 @@ class JobExecutionController
         $stream = $this->archivist->getArchive($jobExecution, $archiver, $key);
 
         return new StreamedFileResponse($stream);
-    }
-
-    /**
-     * Return the job type of the controller
-     *
-     * @return string
-     */
-    protected function getJobType()
-    {
-        return $this->jobType;
     }
 }
