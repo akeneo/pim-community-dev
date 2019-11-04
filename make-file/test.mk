0c8f66ca79e807b8f24c07b9dd50f691497ba6e7
arnaud <arnaud.langlade@akeneo.com>
2019-11-04 15:18:03 +0100

    TIP-1327: Make simpler but longer...

--- make-file/test.mk
+++ make-file/test.mk
@@ -21,9 +21,12 @@ lint-front:
 ### Unit tests
 .PHONY: unit-back
 unit-back: apps-unit-back
-	$(call configure_ci_options, --format=junit > var/tests/phpspec/specs.xml)
+ifeq ($(CI),1)
+	${PHP_RUN} vendor/bin/phpspec run --format=junit > var/tests/phpspec/specs.xml
+	.circleci/find_non_executed_phpspec.sh
+else
 	${PHP_RUN} vendor/bin/phpspec run $(O)
-	$(call execute_on_ci_only, .circleci/find_non_executed_phpspec.sh)
+endif
 
 .PHONY: unit-front
 unit-front:
@@ -42,36 +45,3 @@ acceptance-front:
 .PHONY: integration-front
 integration-front:
 	$(YARN_RUN) integration
-
-# This function will set the var "O" with the given argument if the var CI is set to "1".
-# You need to use this function to configure your targets to run them on the CI:
-# $(call configure_ci_options, XXX) where XXX is the target options.
-#
-# Example: $(call configure_ci_options, --format=junit > spec.xml)
-#
-# How to run tests? Let's take an example: run asset manager unit tests.
-#
-# Locally, I want the default formatter, I use the following command:
-# make asset-manager-unit-back
-#
-# On the CI, I want to generate a JUnit file, I use the following command:
-# make asset-manager-unit-back CI=1
-#
-# CAUTION:
-#  - use this function if your test tools do not support mutiple output format
-#  - the command in your target must use the var O
-#
-# unit-back:
-#	$(PHP_RUN) vendor/bin/phpspec run $(O)
-
-define configure_ci_options
-    $(if $(filter $(CI),1),$(eval O=$(1)))
-endef
-
-# This function execution a command if the var CI is set to "1".
-#
-# Example: $(call execute_on_ci_only, my/script.sh) where my/script.sh is the script you want to run only in the CI.
-
-define execute_on_ci_only
-    $(if $(filter $(CI),1), $(1))
-endef
