43bfbc40c6fcb130184b7509c187ce79b2c94a8b
Samir Boulil <samir.boulil@akeneo.com>
2019-11-04 15:28:58 +0100

    PIM-8860: Fix CI

--- src/Akeneo/Pim/Enrichment/ReferenceEntity/Component/Filter/ReferenceEntityFilter.php
+++ src/Akeneo/Pim/Enrichment/ReferenceEntity/Component/Filter/ReferenceEntityFilter.php
@@ -149,7 +149,9 @@ class ReferenceEntityFilter extends AbstractAttributeFilter implements Attribute
             !empty($referenceEntityIdentifier) &&
             true === $this->referenceEntityExists->withIdentifier(ReferenceEntityIdentifier::fromString($referenceEntityIdentifier));
 
-        return $referenceEntityExists && in_array($attribute->getAttributeType(), $this->supportedAttributeTypes);
+        $isSupportedAttributeType = in_array($attribute->getAttributeType(), $this->supportedAttributeTypes);
+
+        return $referenceEntityExists && $isSupportedAttributeType;
     }
 
     /**
