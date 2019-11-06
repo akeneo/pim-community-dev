26d51f702bdf45ab943752bb283b6d1f1ce37d9a
Mathias Métayer <mathias.metayer@akeneo.com>
2019-11-06 15:55:55 +0100

    TIP-1174: Add possibility to add additional data to the Elasticsearch productmodel projection

--- tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/Model/ElasticsearchProductModelProjectionSpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/Elasticsearch/Model/ElasticsearchProductModelProjectionSpec.php
@@ -84,4 +84,48 @@ class ElasticsearchProductModelProjectionSpec extends ObjectBehavior
             'attributes_for_this_level' => ['attribute_for_this_level1', 'attribute_for_this_level2']
         ]);
     }
+
+    function it_adds_additional_data()
+    {
+        $this->addAdditionalData(['foo' => 'bar', 'baz' => ['42', '44']])
+            ->addAdditionalData(['other_additional_data' => 'some data'])
+            ->shouldBeLike(
+                new ElasticsearchProductModelProjection(
+                    1,
+                    'code',
+                    new \DateTimeImmutable('2019-04-23 15:55:50', new \DateTimeZone('UTC')),
+                    new \DateTimeImmutable('2019-04-25 15:55:50', new \DateTimeZone('UTC')),
+                    'family_code',
+                    ['family_label_1', 'family_label_2'],
+                    'family_variant_code',
+                    ['category_code_1', 'category_code_2'],
+                    ['category_code_of_ancestors_1', 'category_code_of_ancestors_2'],
+                    'parent_product_model_code',
+                    [
+                        'key1' => 'value1',
+                        'key2' => 'value2',
+                    ],
+                    [
+                        'ecommerce' => ['de_DE' => 1, 'fr_FR' => 0],
+                        'mobile' => ['de_DE' => 1, 'fr_FR' => 0],
+                    ],
+                    [
+                        'ecommerce' => ['de_DE' => 1, 'fr_FR' => 0],
+                        'mobile' => ['de_DE' => 0, 'fr_FR' => 1],
+                    ],
+                    2,
+                    ['<all_channels>' => ['en_US' => 'label']],
+                    ['ancestor_attribute_code_1', 'ancestor_attribute_code_2'],
+                    ['attribute_for_this_level1', 'attribute_for_this_level2'],
+                    [
+                        'foo' => 'bar',
+                        'baz' => [
+                            '42',
+                            '44',
+                        ],
+                        'other_additional_data' => 'some data',
+                    ]
+                )
+            );
+    }
 }
