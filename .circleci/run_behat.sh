44e9568db1fa3f318be2aeeb00bffe56adf0161a
Arnaud Langlade <arnaud.langlade@akeneo.com>
2019-11-08 14:09:52 +0100

    TIP-1327: Use make to run behat legacy tests (#11021)
    * TIP-1327: Use make to run behat legacy tests
    
    TIP-1327: Rename vars
    
    TIP-1327: Rename targets
    
    TIP-1327: improve doc
    
    TIP-1327: Use CI=1 by default
    
    * TIP-1327: Fix after rebasing
    
    * TIP-1327: Fix review comments

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-SUITE=$1
-shift
-TESTFILES=$@
+TEST_SUITE=$1
+
+TEST_FILES=$(docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings)
 
 fail=0
 counter=1
-total=$(($(echo $TESTFILES | grep -o ' ' | wc -l) + 1))
+total=$(($(echo $TEST_FILES | grep -o ' ' | wc -l) + 1))
 
-for TESTFILE in $TESTFILES; do
-    echo "$TESTFILE ($counter/$total):"
-    output=$(basename $TESTFILE)_$(uuidgen)
+for TEST_FILE in $TEST_FILES; do
+    echo "$TEST_FILE ($counter/$total):"
+    output=$(basename $TEST_FILE)_$(uuidgen)
 
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE ||
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $SUITE $TESTFILE
+    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE ||
+    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
     fail=$(($fail + $?))
     counter=$(($counter + 1))
 done
--- make-file/test.mk
+++ make-file/test.mk
@@ -21,11 +21,11 @@ lint-front:
 ### Unit tests
 .PHONY: unit-back
 unit-back: var/tests/phpspec
-ifeq ($(CI),1)
+ifeq ($(CI),true)
 	${PHP_RUN} vendor/bin/phpspec run --format=junit > var/tests/phpspec/specs.xml
 	.circleci/find_non_executed_phpspec.sh
 else
-	${PHP_RUN} vendor/bin/phpspec run $(O)
+	${PHP_RUN} vendor/bin/phpspec run
 endif
 
 .PHONY: unit-front
@@ -48,17 +48,36 @@ integration-front:
 
 .PHONY: integration-back
 integration-back: var/tests/phpunit apps-integration-back
-ifeq ($(CI),1)
+ifeq ($(CI),true)
 	.circleci/run_phpunit.sh . .circleci/find_phpunit.php PIM_Integration_Test
 else
-	${PHP_RUN} vendor/bin/phpunit -c . --testsuite PIM_Integration_Test $(O)
+	@echo Run integration test locally is too long, please use the target defined for your bounded context (ex: bounded-context-integration-back)
 endif
 
-### Integration tests
+### End to end tests
 .PHONY: end-to-end-back
 end-to-end-back: var/tests/phpunit
-ifeq ($(CI),1)
+ifeq ($(CI),true)
 	.circleci/run_phpunit.sh . .circleci/find_phpunit.php End_to_End
 else
-	${PHP_RUN} vendor/bin/phpunit -c . --testsuite End_to_End $(O)
+	@echo Run end to end test locally is too long, please use the target defined for your bounded context (ex: bounded-context-end-to-end-back)
 endif
+
+# How to debug a behat locally?
+# -----------------------------
+#
+# Run the following command:
+# make end-to-end-legacy O=my/feature/file.feature:23
+#
+# Don't forget to pass *O*ption to avoid to run the whole suite.
+# Please add dependencies to this tagert and let it die
+
+.PHONY: end-to-end-legacy
+end-to-end-legacy: var/tests/behat
+ifeq ($(CI),true)
+	.circleci/run_behat.sh $(SUITE)
+	.circleci/run_behat.sh critical
+else
+	${PHP_RUN} vendor/bin/behat -p legacy -s all ${0}
+endif
+
