46d0b8b18a8cba98ac42a57903e7d6034138ae7b
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-11-08 16:32:54 +0100

    TIP-1342: tests before deployment

--- make-file/deployment.mk
+++ make-file/deployment.mk
@@ -1,12 +1,5 @@
-.PHONY: tests
-tests:
-	echo "make phpspec"
-	echo "make acceptance"
-	echo "make phpunit"
-	echo "make behat-legacy"
-	echo "make FRONT tests"
-	echo "make PROD tests"
-	echo "and more stuff to do here..."
+.PHONY: test-prod
+test-prod:
 	exit 1
 
 .PHONY: release
