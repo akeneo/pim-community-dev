a3a923cce7f88603563fcc9ad1d30d7107107d2c
Mathias Métayer <mmetayer@users.noreply.github.com>
2019-11-15 11:53:23 +0100

    PIM-8956: Fix security vulnerability (#11060)
    * PIM-8956: Fix security vulnerability
    
    * PIM-8956: Implement InMemoryUserRepository::find() method

--- tests/back/UserManagement/Specification/Bundle/Security/UserProviderSpec.php
+++ tests/back/UserManagement/Specification/Bundle/Security/UserProviderSpec.php
@@ -2,10 +2,12 @@
 
 namespace Specification\Akeneo\UserManagement\Bundle\Security;
 
-use PhpSpec\ObjectBehavior;
+use Akeneo\UserManagement\Component\Model\UserInterface;
 use Akeneo\UserManagement\Component\Repository\UserRepositoryInterface;
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\User;
 
 class UserProviderSpec extends ObjectBehavior
 {
@@ -14,23 +16,40 @@ class UserProviderSpec extends ObjectBehavior
         $this->beConstructedWith($userRepository);
     }
 
-    function it_loads_a_user_by_its_username($userRepository, UserInterface $julia)
+    function it_loads_a_user_by_its_username(UserRepositoryInterface $userRepository, UserInterface $julia)
     {
         $userRepository->findOneByIdentifier('julia')->willReturn($julia);
         $this->loadUserByUsername('julia')->shouldReturn($julia);
     }
 
-    function it_refreshes_a_user($userRepository, UserInterface $julia)
-    {
-        $userRepository->findOneByIdentifier('julia')->willReturn($julia);
-        $julia->getUsername()->willReturn('julia');
-        $this->refreshUser($julia)->shouldReturn($julia);
-    }
-
-    function it_throws_an_exception_if_user_does_not_exist($userRepository)
+    function it_throws_an_exception_if_username_does_not_exist(UserRepositoryInterface $userRepository)
     {
         $userRepository->findOneByIdentifier('jean-pacôme')->willReturn(null);
         $this->shouldThrow(UsernameNotFoundException::class)
              ->during('loadUserByUsername', ['jean-pacôme']);
+    }
+
+    function it_refreshes_a_user($userRepository, UserInterface $julia)
+    {
+        $userRepository->find(42)->willReturn($julia);
+        $julia->getId()->willReturn(42);
+        $this->refreshUser($julia)->shouldReturn($julia);
+    }
+
+    function it_throw_an_exception_if_user_class_is_not_supported()
+    {
+        $julia = new User('julia', 'jambon');
+        $this->shouldThrow(UnsupportedUserException::class)->during('refreshUser', [$julia]);
+    }
+
+    function it_throws_an_exception_if_user_cannot_be_refreshed(
+        UserRepositoryInterface $userRepository,
+        UserInterface $julia
+    ) {
+        $julia->getId()->willReturn(42);
+        $userRepository->find(42)->willReturn(null);
+
+        $this->shouldThrow(UsernameNotFoundException::class)
+            ->during('refreshUser', [$julia]);
     }
 }
