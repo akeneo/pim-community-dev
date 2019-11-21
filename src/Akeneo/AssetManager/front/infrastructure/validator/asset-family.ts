e3a80dee52cd5eb14bf229b4938e646f9ba52e65
Adrien Pétremann <hello@grena.fr>
2019-11-21 17:52:02 +0100

    AST-60: change json path

--- src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
+++ src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
@@ -1,7 +1,7 @@
 import validateAgainstSchema from 'akeneoassetmanager/infrastructure/tools/validator';
 import {AssetFamily} from 'akeneoassetmanager/infrastructure/model/asset-family';
 
-const schema = require('akeneoassetmanager/infrastructure/model/backend-asset-family.schema.json');
+const schema = require('akeneoassetmanager/infrastructure/model/asset-family.schema.json');
 
 export default (data: any): AssetFamily => {
   return validateAgainstSchema(data, schema);
