bf44ff192f6fe6304ea216acf1f7b6d3b3b7378f
Mathias Métayer <mathias.metayer@akeneo.com>
2019-11-21 14:06:44 +0100

    ATR-30: Update json schema for asset families

--- src/Akeneo/AssetManager/back/Infrastructure/Connector/Api/AssetFamily/JsonSchema/AssetFamilyValidator.php
+++ src/Akeneo/AssetManager/back/Infrastructure/Connector/Api/AssetFamily/JsonSchema/AssetFamilyValidator.php
@@ -102,6 +102,62 @@ class AssetFamilyValidator
                         'additionalProperties' => false,
                     ],
                 ],
+                'transformations' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'object',
+                        'properties' => [
+                            'source' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'attribute' => [
+                                        'type' => 'string',
+                                    ],
+                                    'channel' => [
+                                        'type' => ['string', 'null'],
+                                    ],
+                                    'locale' => [
+                                        'type' => ['string', 'null'],
+                                    ],
+                                ],
+                                'required' => ['attribute', 'channel', 'locale'],
+                                'additionalProperties' => false,
+                            ],
+                            'target' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'attribute' => [
+                                        'type' => 'string',
+                                    ],
+                                    'channel' => [
+                                        'type' => ['string', 'null'],
+                                    ],
+                                    'locale' => [
+                                        'type' => ['string', 'null'],
+                                    ],
+                                ],
+                            ],
+                            'operations' => [
+                                'type' => 'array',
+                                'items' => [
+                                    'type' => 'object',
+                                    'properties' => [
+                                        'type' => [
+                                            'type' => 'string',
+                                        ],
+                                        'parameters' => [
+                                            'type' => 'object',
+                                        ],
+                                        'required' => ['type'],
+                                        'additionalProperties' => false,
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'required' => ['source', 'target', 'operations'],
+                        'additionalProperties' => false,
+                    ],
+                ],
                 '_links' => [
                     'type' => 'object'
                 ]
