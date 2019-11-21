c2fa00f212333f5e4aaa837e12b04132a143c09a
Adrien Pétremann <hello@grena.fr>
2019-11-21 17:39:34 +0100

    AST-60: fix interface name

--- src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
+++ src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
@@ -1,8 +1,8 @@
 import validateAgainstSchema from 'akeneoassetmanager/infrastructure/tools/validator';
-import {BackendAssetFamily} from 'akeneoassetmanager/infrastructure/model/backend-asset-family';
+import {AssetFamily} from 'akeneoassetmanager/infrastructure/model/asset-family';
 
 const schema = require('akeneoassetmanager/infrastructure/model/backend-asset-family.schema.json');
 
-export default (data: any): BackendAssetFamily => {
+export default (data: any): AssetFamily => {
   return validateAgainstSchema(data, schema);
 };
