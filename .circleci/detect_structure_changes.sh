4d672b019c1865ad0b9e333b14acbe452826f826
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-11-25 14:30:16 +0100

    TIP-1291: properly name index and db dumps

--- .circleci/detect_structure_changes.sh
+++ .circleci/detect_structure_changes.sh
@@ -50,15 +50,15 @@ echo "Install branch database and indexes..."
 APP_ENV=test make database
 
 echo "Dump branch database..."
-docker-compose exec -T mysql mysqldump --no-data --skip-opt --skip-comments --password=$APP_DATABASE_PASSWORD --user=$APP_DATABASE_USER $APP_DATABASE_NAME > /tmp/dump_master_database.sql
+docker-compose exec -T mysql mysqldump --no-data --skip-opt --skip-comments --password=$APP_DATABASE_PASSWORD --user=$APP_DATABASE_USER $APP_DATABASE_NAME > /tmp/dump_branch_database.sql
 
 echo "Dump branch index..."
-docker-compose exec -T elasticsearch curl -XGET "$APP_INDEX_HOSTS/_all/_mapping"|json_pp --json_opt=canonical,pretty > /tmp/dump_master_index.json
+docker-compose exec -T elasticsearch curl -XGET "$APP_INDEX_HOSTS/_all/_mapping"|json_pp --json_opt=canonical,pretty > /tmp/dump_branch_index.json
 
 echo "Compare database master+PR migrations from database PR..."
-diff /tmp/dump_master_database_with_migrations.sql /tmp/dump_master_database.sql
+diff /tmp/dump_master_database_with_migrations.sql /tmp/dump_branch_database.sql
 
 echo "Compare index master+PR migrations from index PR..."
 sed -i -r 's/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/uuid/g' /tmp/dump_master_index_with_migrations.json
-sed -i -r 's/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/uuid/g' /tmp/dump_master_index.json
-diff /tmp/dump_master_index_with_migrations.json /tmp/dump_master_index.json
+sed -i -r 's/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/uuid/g' /tmp/dump_branch_index.json
+diff /tmp/dump_master_index_with_migrations.json /tmp/dump_branch_index.json
