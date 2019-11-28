e2631b1c8c2c71d82fe1a4ead7a9b9957462f286
Benoit Jacquemont <benoit@akeneo.com>
2019-11-28 12:38:42 +0100

    TIP-1271 Adds ES checker

---
+++ src/Akeneo/Platform/Bundle/MonitoringBundle/ServiceStatusChecker/ElasticsearchChecker.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\Bundle\MonitoringBundle\ServiceStatusChecker;
+
+use Akeneo\Platform\Bundle\MonitoringBundle\ServiceStatus;
+use Akeneo\Platform\Bundle\MonitoringBundle\ServiceStatusChecker;
+use Akeneo\Tool\Bundle\ElasticsearchBundle\ClientRegistry;
+
+final class ElasticsearchChecker implements ServiceStatusChecker
+{
+    /** @var ClientRegistry */
+    private $clientRegistry;
+
+    public function __construct(ClientRegistry $clientRegistry)
+    {
+        $this->clientRegistry = $clientRegistry;
+    }
+
+    public function status(): ServiceStatus
+    {
+        $statusOk = true;
+        $failingIndexNames = [];
+
+        foreach ($this->clientRegistry->getClients() as $client) {
+            if (!$client->hasIndex()) {
+                $statusOk = false;
+                $failingIndexNames[] = $client->getIndexName();
+            }
+        }
+
+        if ($statusOk) {
+            $message = 'OK';
+        } else {
+            $message = 'Elasticsearch failing indexes: '.implode(',', $failingIndexNames);
+        }
+
+        return new ServiceStatus($statusOk, $message);
+    }
+}
--- tests/back/Platform/Specification/Bundle/MonitoringBundle/ServiceStatusChecker/ElasticsearchCheckerSpec.php
+++ tests/back/Platform/Specification/Bundle/MonitoringBundle/ServiceStatusChecker/ElasticsearchCheckerSpec.php
@@ -10,25 +10,60 @@ use PhpSpec\ObjectBehavior;
 
 class ElasticsearchCheckerSpec extends ObjectBehavior
 {
-    function it_returns_a_ok_status_with_all_working_es_clients(ClientRegistry $clientRegistry)
-    {
-        $connection->ping()->willReturn(true);
-        $this->beConstructedWith($connection);
-        $this->shouldHaveType(MysqlChecker::class);
+    function it_returns_a_ok_status_with_all_working_es_clients(
+        ClientRegistry $clientRegistry,
+        Client $productClient,
+        Client $draftClient
+    ) {
+        $productClient->hasIndex()->willReturn(true);
+        $draftClient->hasIndex()->willReturn(true);
+
+        $clientRegistry->getClients()->willReturn([$productClient, $draftClient]);
+
+        $this->beConstructedWith($clientRegistry);
+        $this->shouldHaveType(ElasticsearchChecker::class);
 
         $status = $this->status();
         $status->isOk()->shouldReturn(true);
         $status->getMessage()->shouldReturn("OK");
     }
 
-    function it_returns_a_ko_status_with_a_non_working_connection(Connection $connection)
-    {
-        $connection->ping()->willReturn(false);
-        $this->beConstructedWith($connection);
-        $this->shouldHaveType(MysqlChecker::class);
+    function it_returns_a_ko_status_with_a_non_working_es_client(
+        ClientRegistry $clientRegistry,
+        Client $productClient,
+        Client $draftClient
+    ) {
+        $productClient->hasIndex()->willReturn(true);
+        $draftClient->hasIndex()->willReturn(false);
+        $draftClient->getIndexName()->willReturn('pimee_index_draft');
+
+        $clientRegistry->getClients()->willReturn([$productClient, $draftClient]);
+
+        $this->beConstructedWith($clientRegistry);
+        $this->shouldHaveType(ElasticsearchChecker::class);
 
         $status = $this->status();
         $status->isOk()->shouldReturn(false);
-        $status->getMessage()->shouldReturn("Unable to ping the database.");
+        $status->getMessage()->shouldReturn("Elasticsearch failing indexes: pimee_index_draft");
+    }
+
+    function it_returns_a_ko_status_with_all_es_clients_non_working(
+        ClientRegistry $clientRegistry,
+        Client $productClient,
+        Client $draftClient
+    ) {
+        $productClient->hasIndex()->willReturn(false);
+        $productClient->getIndexName()->willReturn('pimee_index_product');
+        $draftClient->hasIndex()->willReturn(false);
+        $draftClient->getIndexName()->willReturn('pimee_index_draft');
+
+        $clientRegistry->getClients()->willReturn([$productClient, $draftClient]);
+
+        $this->beConstructedWith($clientRegistry);
+        $this->shouldHaveType(ElasticsearchChecker::class);
+
+        $status = $this->status();
+        $status->isOk()->shouldReturn(false);
+        $status->getMessage()->shouldReturn("Elasticsearch failing indexes: pimee_index_product,pimee_index_draft");
     }
 }
