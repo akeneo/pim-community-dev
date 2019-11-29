32d91998b6bb0383cca26231b3df47f5b556f4f1
Benoit Jacquemont <benoit@akeneo.com>
2019-11-29 13:12:56 +0100

    Makes behat fails on undefined steps (#11146)

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -15,8 +15,9 @@ for TEST_FILE in $TEST_FILES; do
     output=$(basename $TEST_FILE)_$(uuidgen)
 
     set +e
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE ||
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
+    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE ||
+    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
+
     fail=$(($fail + $?))
     counter=$(($counter + 1))
     set -eo pipefail
