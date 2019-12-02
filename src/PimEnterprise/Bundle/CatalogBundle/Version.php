07d7660f37fa9dee67c9dd96859836e531ee6d96
Michel Tag <micheltag@akeneo.com>
2019-12-02 11:39:25 +0100

    Preparing version 2.3.72 (#7414)
    Preparing version 2.3.72
    
    
    Co-authored-by: rouliane <julian.prudhomme@akeneo.com>

--- src/PimEnterprise/Bundle/CatalogBundle/Version.php
+++ src/PimEnterprise/Bundle/CatalogBundle/Version.php
@@ -19,7 +19,7 @@ namespace PimEnterprise\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.71';
+    const VERSION = '2.3.72';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Lychee';
