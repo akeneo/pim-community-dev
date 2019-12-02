6c180f3308877113880d4130ec3b6ff56be7f82d
Julian Prud'homme <julian.prudhomme@akeneo.com>
2019-12-02 10:34:50 +0000

    Preparing version 2.3.72

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '2.3.71';
+    const VERSION = '2.3.72';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'What’s up Doc?';
