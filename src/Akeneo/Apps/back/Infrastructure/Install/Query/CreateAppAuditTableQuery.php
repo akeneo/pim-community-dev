63a08b6c44367051c3b2df12ae700a6f75c5310a
Pierre Jolly <pierre.jolly@akeneo.com>
2019-12-02 16:31:22 +0100

    API-861: Remove useless quote

--- src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppAuditTableQuery.php
+++ src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppAuditTableQuery.php
@@ -9,16 +9,16 @@ namespace Akeneo\Apps\Infrastructure\Install\Query;
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-final class CreateAppAuditsTableQuery
+final class CreateAppAuditTableQuery
 {
-    const QUERY = <<<'SQL'
+    const QUERY = <<<SQL
 CREATE TABLE IF NOT EXISTS akeneo_app_audit(
     id INT NOT NULL AUTO_INCREMENT,
     app_code VARCHAR(100) NOT NULL,
-    event_date DATE NOT NULL COMMENT '(DC2Type:datetime)',
+    event_date DATE NOT NULL,
     count INT NOT NULL,
     event_type ENUM('product_created', 'product_updated') NOT NULL,
-    updated DATETIME NOT NULL COMMENT '(DC2Type:datetime)' DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_AUDIT_akeneo_app_audit_code FOREIGN KEY (app_code) REFERENCES akeneo_app (code),
     INDEX IDX_AUDIT_id (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
--- upgrades/schema/Version_4_0_20191202144736_create_app_audit_table.php
+++ upgrades/schema/Version_4_0_20191202144736_create_app_audit_table.php
@@ -6,24 +6,24 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * This migration will create the app audits table.
+ * This migration will create the app audit table.
  *
  * @author Pierre Jolly <pierre.jolly@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-final class Version_4_0_20191202144736_create_app_audits_table extends AbstractMigration
+final class Version_4_0_20191202144736_create_app_audit_table extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $createTableQuery = <<<'SQL'
+        $createTableQuery = <<<SQL
 CREATE TABLE IF NOT EXISTS akeneo_app_audit(
     id INT NOT NULL AUTO_INCREMENT,
     app_code VARCHAR(100) NOT NULL,
-    event_date DATE NOT NULL COMMENT '(DC2Type:datetime)',
+    event_date DATE NOT NULL,
     count INT NOT NULL,
     event_type ENUM('product_created', 'product_updated') NOT NULL,
-    updated DATETIME NOT NULL COMMENT '(DC2Type:datetime)' DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_AUDIT_akeneo_app_audit_code FOREIGN KEY (app_code) REFERENCES akeneo_app (code),
     INDEX IDX_AUDIT_id (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
