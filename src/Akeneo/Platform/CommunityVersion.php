6babf9566f37ac58cf59348e28fb596e1279c7fc
Julian Prud'homme <julian.prudhomme@akeneo.com>
2019-12-02 15:43:55 +0000

    Preparing version 3.0.57

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.56';
+    const VERSION = '3.0.57';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Super-Rabbit';
