9129e2eaac5bab826ccac582e495da27da938b3f
Paul Chasle <paul.chasle@akeneo.com>
2019-12-03 18:20:47 +0100

    API-794: Add withContexts HoC

--- src/Akeneo/Apps/back/Infrastructure/Symfony/Resources/public/js/react/react-controller.ts
+++ src/Akeneo/Apps/back/Infrastructure/Symfony/Resources/public/js/react/react-controller.ts
@@ -45,8 +45,6 @@ abstract class ReactController extends BaseController {
       return;
     }
 
-    console.log('ok');
-
     unmoundReactElementRef();
   }
 }
--- src/Akeneo/Apps/front/src/infrastructure/Dashboard.tsx
+++ src/Akeneo/Apps/front/src/infrastructure/Dashboard.tsx
@@ -1,35 +1,4 @@
-import React from 'react';
-import {ThemeProvider} from 'styled-components';
-import {theme} from '../application/common/theme';
-import {NotifyContext, NotifyInterface} from '../application/shared/notify';
-import {RouterContext, RouterInterface} from '../application/shared/router';
-import {TranslateContext, TranslateInterface} from '../application/shared/translate';
-import {LegacyContext} from './legacy-context';
-import {ViewBuilder} from './pim-view/view-builder';
+import {Fragment} from 'react';
+import {withContexts} from './with-contexts';
 
-interface Props {
-    router: RouterInterface;
-    translate: TranslateInterface;
-    viewBuilder: ViewBuilder;
-    notify: NotifyInterface;
-}
-
-export const Dashboard = ({router, translate, viewBuilder, notify}: Props) => {
-    return (
-        <RouterContext.Provider value={router}>
-            <TranslateContext.Provider value={translate}>
-                <NotifyContext.Provider value={notify}>
-                    <LegacyContext.Provider
-                        value={{
-                            viewBuilder,
-                        }}
-                    >
-                        <ThemeProvider theme={theme}>
-                            <></>
-                        </ThemeProvider>
-                    </LegacyContext.Provider>
-                </NotifyContext.Provider>
-            </TranslateContext.Provider>
-        </RouterContext.Provider>
-    );
-};
+export const Dashboard = withContexts(Fragment);
