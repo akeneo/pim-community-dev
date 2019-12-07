89db25cd00306f30dbe1a3649ebf77b40adf60c6
Samir Boulil <samir.boulil@akeneo.com>
2019-12-07 18:24:12 +0100

    AST-151: Introduce Tags component based on select2

--- src/Akeneo/AssetManager/front/domain/model/attribute/type/media-file/allowed-extensions.ts
+++ src/Akeneo/AssetManager/front/domain/model/attribute/type/media-file/allowed-extensions.ts
@@ -1,32 +1,17 @@
-export type AllowedExtensions = AllowedExtensionsOptions[];
-
-export enum AllowedExtensionsOptions {
-  gif = 'gif',
-  jfif = 'jfif',
-  jif = 'jif',
-  jpeg = 'jpeg',
-  jpg = 'jpg',
-  pdf = 'pdf',
-  png = 'png',
-  psd = 'psd',
-  tif = 'tif',
-  tiff = 'tiff',
-}
-
-export const validAllowedExtensionsOptions = Object.values(AllowedExtensionsOptions);
+export type AllowedExtensions = string[];
 
 export const isValidAllowedExtension = (allowedExtensions: string[]): allowedExtensions is AllowedExtensions => {
   if (!Array.isArray(allowedExtensions)) {
     return false;
   }
-  return !allowedExtensions.some(
-    (extension: string) => !Object.values(AllowedExtensionsOptions).includes(extension as any)
+  return allowedExtensions.every(
+    (extension: string) => typeof extension === "string"
   );
 };
 
 export const createAllowedExtensionFromNormalized = (allowedExtensions: string[]): AllowedExtensions => {
   if (!isValidAllowedExtension(allowedExtensions)) {
-    throw new Error(`AllowedExtension should be ${validAllowedExtensionsOptions.join(',')}`);
+    throw new Error(`AllowedExtension is not valid`);
   }
 
   return allowedExtensions;
