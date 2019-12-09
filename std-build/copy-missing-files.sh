96f7c5f4d7aa10f00569b84037538a8bbdfac6f5
Benoit Jacquemont <benoit@akeneo.com>
2019-12-09 14:30:59 +0100

    TIP-1346 Adds specific Kernel for standard

--- std-build/copy-missing-files.sh
+++ std-build/copy-missing-files.sh
@@ -8,11 +8,14 @@ set -e
 DEV_DISTRIB_DIR=$(dirname $0)/..
 STANDARD_DISTRIB_DIR=./
 
+mkdir -p $STANDARD_DISTRIB_DIR/bin $STANDARD_DISTRIB_DIR/public $STANDARD_DISTRIB_DIR/config/packages
+
 cp $DEV_DISTRIB_DIR/CHANGELOG*.md $STANDARD_DISTRIB_DIR
 cp $DEV_DISTRIB_DIR/UPGRADE*.md $STANDARD_DISTRIB_DIR
 cp -r $DEV_DISTRIB_DIR/upgrades $STANDARD_DISTRIB_DIR
+cp $DEV_DISTRIB_DIR/config/bootstrap.php $STANDARD_DISTRIB_DIR/config/
+cp $STANDARD_DISTRIB_DIR/bin/console $STANDARD_DISTRIB_DIR/bin/
+cp $STANDARD_DISTRIB_DIR/public/index.php $STANDARD_DISTRIB_DIR/public/
+cp $STANDARD_DISTRIB_DIR/std-build/src/Kernel.php $STANDARD_DISTRIB_DIR/src
 
-mkdir -p $STANDARD_DISTRIB_DIR/bin $STANDARD_DISTRIB_DIR/public
-[ -f $STANDARD_DISTRIB_DIR/bin/console ] || cp $DEV_DISTRIB_DIR/bin/console $STANDARD_DISTRIB_DIR/bin
-[ -f $STANDARD_DISTRIB_DIR/public/index.php ] || cp $DEV_DISTRIB_DIR/public/index.php $STANDARD_DISTRIB_DIR/public
-[ -f $STANDARD_DISTRIB_DIR/Makefile ] || cp $DEV_DISTRIB_DIR/std-build/Makefile $STANDARD_DISTRIB_DIR
+[ -f $STANDARD_DISTRIB_DIR/config/packages/prod/oneup_flysystem.yml ] || cp $DEV_DISTRIB_DIR/config/packages/prod_onprem_paas/oneup_flysystem.yml $STANDARD_DISTRIB_DIR/config/packages/prod/oneup_flysystem.yml
