a806198addecd05d62be0380bc4e2120ac6dcf7c
Benoit Jacquemont <benoit@akeneo.com>
2019-12-09 10:56:57 +0100

    TIP-1346 Adds script to build the standard distribution

---
+++ std-build/build-standard-edition.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Generate a standard edition on the provided directory
+#
+
+set -e
+
+if [ $# -eq 1 ]; then
+    echo "Usage: $0 <target-directory>" >&2
+    exit 1;
+fi
+
+TARGET_DIR="$1"
+if [ ! -d "$1" ] || [ ! -w "$1" ]; then
+    echo "Directory $1 does not exist or is not writable" >&2
+    exit 2
+fi
+
+
+SRC_DIR=$(dirname $0)/..
+
+cp $SRC_DIR/std-build/composer.json $TARGET_DIR/*
