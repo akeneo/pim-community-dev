8011346d6710f75a27f3ee3eee863e37a297dd84
anaisbaunelemaire <anais.lemaire@akeneo.com>
2019-12-09 10:35:53 +0100

    DAPI-691 - change of the type of the variable $blacklistedJobInstanceCodes and addition of precisions in the doc

--- src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionQueueInterface.php
+++ src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionQueueInterface.php
@@ -25,8 +25,8 @@ interface JobExecutionQueueInterface
      * This method loops until there is a message to consume into the queue.
      *
      * @param string $consumer name of the consumer
-     * @param string[] $whitelistedJobInstanceCodes name of the job instances
-     * @param array $blacklistedJobInstanceCodes
+     * @param string[] $whitelistedJobInstanceCodes name of the whitelisted job instances
+     * @param string[] $blacklistedJobInstanceCodes name of the blacklisted job instances
      *
      * @return JobExecutionMessage
      */
