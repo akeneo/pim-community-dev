b14548a199ba30164921f5886338da98e296ad93
romain <romain@akeneo.com>
2019-12-09 11:16:20 +0100

    API-862: Rename "count" column name

--- src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppAuditTableQuery.php
+++ src/Akeneo/Apps/back/Infrastructure/Install/Query/CreateAppAuditTableQuery.php
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS akeneo_app_audit(
     id INT NOT NULL AUTO_INCREMENT,
     app_code VARCHAR(100) NOT NULL,
     event_date DATE NOT NULL,
-    count INT NOT NULL,
+    event_count INT NOT NULL,
     event_type ENUM('product_created', 'product_updated') NOT NULL,
     updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_AUDIT_akeneo_app_audit_code FOREIGN KEY (app_code) REFERENCES akeneo_app (code),
