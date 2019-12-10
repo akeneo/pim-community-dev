05000a60d0485e19e75f1fb5c21fc0ba95060b5a
Quentin Favrie <quentin.favrie@akeneo.com>
2019-12-10 11:22:32 +0100

    AST-40: cleanup unrelated changes

--- src/Akeneo/AssetManager/front/application/hydrator/asset.ts
+++ src/Akeneo/AssetManager/front/application/hydrator/asset.ts
@@ -4,6 +4,7 @@ import denormalizeAsset from 'akeneoassetmanager/application/denormalizer/asset'
 
 export default (backendAsset: any): Asset => {
   const expectedKeys = ['identifier', 'asset_family_identifier', 'code', 'labels', 'image', 'values'];
+
   validateKeys(backendAsset, expectedKeys, 'The provided raw asset seems to be malformed.');
 
   return denormalizeAsset(backendAsset);
--- src/Akeneo/AssetManager/front/infrastructure/tools/validator.ts
+++ src/Akeneo/AssetManager/front/infrastructure/tools/validator.ts
@@ -4,11 +4,15 @@ const Ajv = require('ajv');
 const ajv = new Ajv();
 
 export const isValidAgainstSchema = <T>(data: any, schema: object): data is T => {
-  return ajv.validate(schema, data);
+  try {
+    return ajv.validate(schema, data);
+  } catch (e) {
+    throw Error(e.message);
+  }
 };
 
 export const validateAgainstSchema = <T>(data: any, schema: object): T => {
-  // const isValid = isValidAgainstSchema<T>(data, schema);
+  const isValid = isValidAgainstSchema<T>(data, schema);
 
   if (!isValid) {
     console.error('The data does not match the JSON schema', ajv.errors);
