ba905f108f47821620462e8d7126da029e4a2ec5
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2019-12-11 14:37:59 +0100

    ATR-52: fix test

--- src/Akeneo/AssetManager/tests/back/spec/Infrastructure/Validation/AssetFamily/Transformation/TransformationCanNotHaveSameOperationTwiceValidatorSpec.php
+++ src/Akeneo/AssetManager/tests/back/spec/Infrastructure/Validation/AssetFamily/Transformation/TransformationCanNotHaveSameOperationTwiceValidatorSpec.php
@@ -18,7 +18,9 @@ use Akeneo\AssetManager\Infrastructure\Validation\AssetFamily\Transformation\Tra
 use Akeneo\AssetManager\Infrastructure\Validation\AssetFamily\Transformation\TransformationCanNotHaveSameOperationTwiceValidator;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 class TransformationCanNotHaveSameOperationTwiceValidatorSpec extends ObjectBehavior
@@ -33,13 +35,16 @@ class TransformationCanNotHaveSameOperationTwiceValidatorSpec extends ObjectBeha
         $this->shouldBeAnInstanceOf(TransformationCanNotHaveSameOperationTwiceValidator::class);
     }
 
-    public function it_passes_the_validation(ExecutionContextInterface $context)
+    public function it_passes_the_validation(ExecutionContextInterface $context, ValidatorInterface $validator)
     {
         $operations = [
             ['type' => 'type1'],
             ['type' => 'type2'],
             ['type' => 'type3'],
         ];
+        $context->getValidator()->willReturn($validator);
+        $validator->inContext($context)->willReturn($validator);
+        $validator->validate($operations, new Assert\Type('array'))->shouldBeCalled();
         $context->buildViolation(Argument::any())->shouldNotBeCalled();
 
         $this->validate($operations, new TransformationCanNotHaveSameOperationTwice(AssetFamilyIdentifier::fromString('id')));
@@ -47,13 +52,17 @@ class TransformationCanNotHaveSameOperationTwiceValidatorSpec extends ObjectBeha
 
     public function it_builds_exception_with_same_operation_twice(
         ExecutionContextInterface $context,
-        ConstraintViolationBuilderInterface $constraintViolationBuilder
+        ConstraintViolationBuilderInterface $constraintViolationBuilder,
+        ValidatorInterface $validator
     ) {
         $operations = [
             ['type' => 'type1'],
             ['type' => 'type2'],
             ['type' => 'type1'],
         ];
+        $context->getValidator()->willReturn($validator);
+        $validator->inContext($context)->willReturn($validator);
+        $validator->validate($operations, new Assert\Type('array'))->shouldBeCalled();
         $context->buildViolation(TransformationCanNotHaveSameOperationTwice::ERROR_MESSAGE)
             ->willReturn($constraintViolationBuilder, $constraintViolationBuilder);
         $constraintViolationBuilder->setParameter('%asset_family_identifier%', 'id')->willReturn($constraintViolationBuilder);
