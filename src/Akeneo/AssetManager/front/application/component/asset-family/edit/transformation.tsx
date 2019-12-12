197b20b0f4eb935d47f35b78da4f26a46abfc5ce
pierallard <pierallard@gmail.com>
2019-12-12 11:38:22 +0100

    ATR-49: Add JsonEditor

--- src/Akeneo/AssetManager/front/application/component/asset-family/edit/transformation.tsx
+++ src/Akeneo/AssetManager/front/application/component/asset-family/edit/transformation.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
 import __ from 'akeneoassetmanager/tools/translator';
 import {breadcrumbConfiguration} from 'akeneoassetmanager/application/component/asset-family/edit';
 import Header from 'akeneoassetmanager/application/component/asset-family/edit/header';
@@ -10,6 +12,8 @@ import {
   assetFamilyTransformationsUpdated,
   saveAssetFamily,
 } from 'akeneoassetmanager/application/action/asset-family/edit';
+import Ajv from 'ajv';
+const ajv = new Ajv({ allErrors: true, verbose: true });
 
 interface StateProps {
   assetFamily: AssetFamily;
@@ -24,15 +28,102 @@ const AssetFamilyTransformationEditor = ({
   transformations,
   onAssetFamilyTransformationsChange,
 }: AssetFamilyTransformationEditorProps) => {
+  let transformationSchema = {
+    "title": "BackendAssetFamilyTransformation",
+    "type": "array",
+    "items": {
+      "type": "object",
+      "properties": {
+        "source": {
+          "type": "object",
+          "properties": {
+            "attribute": {
+              "type": "string",
+            },
+            "channel": {
+              "type": [
+                "string",
+                "null"
+              ]
+            },
+            "locale": {
+              "type": [
+                "string",
+                "null"
+              ]
+            }
+          },
+          "required": [
+            "attribute",
+            "channel",
+            "locale"
+          ],
+          "additionalProperties": false
+        },
+        "target": {
+          "type": "object",
+          "properties": {
+            "attribute": {
+              "type": "string"
+            },
+            "channel": {
+              "type": [
+                "string",
+                "null"
+              ]
+            },
+            "locale": {
+              "type": [
+                "string",
+                "null"
+              ]
+            }
+          },
+          "required": [
+            "attribute",
+            "channel",
+            "locale"
+          ],
+          "additionalProperties": false
+        },
+        "operations": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "required": [
+              "type"
+            ],
+            "additionalProperties": true
+          }
+        },
+        "filename_prefix": {
+          "type": "string"
+        },
+        "filename_suffix": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "source",
+        "target",
+        "operations",
+      ],
+      "additionalProperties": false
+    }
+  };
+
+  //https://github.com/vankop/jsoneditor-react/blob/HEAD/src/Editor.jsx
   return (
-    <textarea
-      value={transformations}
+    <Editor
+      value={JSON.parse(transformations)}
       onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
         onAssetFamilyTransformationsChange(event.target.value);
       }}
-      style={{width: 800, height: 450}}
+      mode='code'
+      schema={transformationSchema}
+      ajv={ajv}
     />
-  );
+  )
 };
 
 interface DispatchProps {
