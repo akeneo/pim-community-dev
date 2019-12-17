ef3ba6877db3c2e12fc170df2bbaf3331fdebdd7
Adrien Pétremann <hello@grena.fr>
2019-12-17 17:01:05 +0100

    AST-195: write our own constraint violation list normalizer

---
+++ src/Akeneo/AssetManager/back/Infrastructure/Normalizer/ConstraintViolationListNormalizer.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2019 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\AssetManager\Infrastructure\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2019 Akeneo SAS (https://www.akeneo.com)
+ */
+class ConstraintViolationListNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+{
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+
+    public function normalize($constraintViolationList, $format = null, array $context = [])
+    {
+        $normalizedViolations = [];
+
+        /** @var ConstraintViolationInterface $constraintViolation */
+        foreach ($constraintViolationList as $constraintViolation)
+        {
+            $normalizedViolations[] = [
+                'messageTemplate' => $constraintViolation->getMessageTemplate(),
+                'parameters' => $constraintViolation->getParameters(),
+                'message' => $constraintViolation->getMessage(),
+                'propertyPath' => $constraintViolation->getPropertyPath(),
+                'invalidValue' => $constraintViolation->getInvalidValue(),
+            ];
+        }
+
+        return $normalizedViolations;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ConstraintViolationList;
+    }
+}
---
+++ src/Akeneo/AssetManager/tests/back/spec/Infrastructure/Normalizer/ConstraintViolationListNormalizerSpec.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Akeneo\AssetManager\Infrastructure\Normalizer;
+
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+class ConstraintViolationListNormalizerSpec extends ObjectBehavior
+{
+    public function it_supports_normalization_of_constraint_violation_list(
+        ConstraintViolationList $constraintViolationList
+    ) {
+        $this->supportsNormalization(new \stdClass())->shouldReturn(false);
+        $this->supportsNormalization($constraintViolationList)->shouldReturn(true);
+    }
+
+    public function it_normalizes_a_constraint_violation_list(
+        ConstraintViolationInterface $constraintViolationA,
+        ConstraintViolationInterface $constraintViolationB
+    ) {
+        $constraintViolationA->getMessageTemplate()->willReturn('the_message_template_a');
+        $constraintViolationA->getParameters()->willReturn(['parameters_a']);
+        $constraintViolationA->getMessage()->willReturn('The message A');
+        $constraintViolationA->getPropertyPath()->willReturn('property.path.a');
+        $constraintViolationA->getInvalidValue()->willReturn('toto');
+
+        $constraintViolationB->getMessageTemplate()->willReturn('the_message_template_b');
+        $constraintViolationB->getParameters()->willReturn(['parameters_b']);
+        $constraintViolationB->getMessage()->willReturn('The message B');
+        $constraintViolationB->getPropertyPath()->willReturn('property.path.b');
+        $constraintViolationB->getInvalidValue()->willReturn('toto');
+
+        $constraintViolationList = [$constraintViolationA, $constraintViolationB];
+
+        $this->normalize($constraintViolationList)->shouldReturn(
+            [
+                [
+                    'messageTemplate' => 'the_message_template_a',
+                    'parameters' => ['parameters_a'],
+                    'message' => 'The message A',
+                    'propertyPath' => 'property.path.a',
+                    'invalidValue' => 'toto',
+                ],
+                [
+                    'messageTemplate' => 'the_message_template_b',
+                    'parameters' => ['parameters_b'],
+                    'message' => 'The message B',
+                    'propertyPath' => 'property.path.b',
+                    'invalidValue' => 'toto',
+                ],
+            ]
+        );
+
+    }
+}
