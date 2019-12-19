505f84b207ba50facae1a2ec05041d9e11da023e
Michel Tag <micheltag@akeneo.com>
2019-12-19 10:49:00 +0100

    Preparing version 3.0.61 (#7567)
    Preparing version 3.0.61
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.60';
+    const VERSION = '3.0.61';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rose';
