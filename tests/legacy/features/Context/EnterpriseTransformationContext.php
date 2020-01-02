4bab9bfd267ae4c4e367feacc41ecbf5bb2aa6d2
Adrien Pétremann <hello@grena.fr>
2020-01-02 16:59:07 +0100

    AST-36: revert change on transformation context

---
+++ tests/legacy/features/Context/EnterpriseTransformationContext.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Context;
+
+use Context\TransformationContext as BaseTransformationContext;
+
+/**
+ * Context for data transformations
+ *
+ * @author    Marie Bochu <marie.bochu@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ */
+class EnterpriseTransformationContext extends BaseTransformationContext
+{
+}
