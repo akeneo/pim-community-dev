42e8174d560858a51748bd988f479dc3db67ca4b
Adrien Pétremann <hello@grena.fr>
2020-01-02 18:02:04 +0100

    AST-36: revert deletion of behat context

--- tests/legacy/features/Context/EnterpriseItemPickerContext.php
+++ tests/legacy/features/Context/EnterpriseItemPickerContext.php
@@ -2,9 +2,9 @@
 
 namespace Context;
 
-use Context\TransformationContext as BaseTransformationContext;
+use Pim\Behat\Context\Domain\Enrich\ItemPickerContext;
 
-class EnterpriseTransformationContext extends BaseTransformationContext
+class EnterpriseItemPickerContext extends ItemPickerContext
 {
     // TODO: now useless with the PAM deletion, find a way to remove this class and use the one in the CE.
 }
