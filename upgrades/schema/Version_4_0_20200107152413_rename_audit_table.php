b5856f0263e8b41b620a46141c941b35966d7a30
romain <romain@akeneo.com>
2020-01-07 15:42:13 +0100

    API-934: Adds missing migration script

---
+++ upgrades/schema/Version_4_0_20200107152413_rename_audit_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * This migration will rename the audit table
+ *
+ * @author Romain Monceau <romain@akeneo.com>
+ * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+final class Version_4_0_20200107152413_rename_audit_table extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $alterTableQuery = <<<SQL
+ALTER TABLE akeneo_app_audit RENAME TO akeneo_connectivity_connection_audit
+SQL;
+
+        $this->addSql($alterTableQuery);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
