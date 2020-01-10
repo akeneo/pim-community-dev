d809c33e417157714a041371715064b1102d2475
Valentin Dijkstra <valentin.dijkstra@akeneo.com>
2020-01-10 16:12:51 +0100

    AST-19: Fix asset-card unit test

--- src/Akeneo/AssetManager/tests/front/unit/akeneoassetmanager/application/component/asset/list/mosaic/asset-card.unit.tsx
+++ src/Akeneo/AssetManager/tests/front/unit/akeneoassetmanager/application/component/asset/list/mosaic/asset-card.unit.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import '@testing-library/jest-dom/extend-expect';
-import {render, fireEvent, act} from '@testing-library/react';
+import {getByText, fireEvent, act} from '@testing-library/react';
 import {ThemeProvider} from 'styled-components';
 import {akeneoTheme} from 'akeneoassetmanager/application/component/app/theme';
 import AssetCard from 'akeneoassetmanager/application/component/asset/list/mosaic/asset-card';
@@ -22,117 +23,152 @@ jest.mock('akeneoassetmanager/tools/image-loader', () =>
   )
 );
 
-test('It displays an unselected asset card', async () => {
-  const isSelected = false;
-  const {getByText, container} = render(
-    <ThemeProvider theme={akeneoTheme}>
-      <AssetCard
-        asset={asset}
-        context={{locale: 'en_US', channel: 'ecommerce'}}
-        isSelected={isSelected}
-        onSelectionChange={() => {}}
-      />
-    </ThemeProvider>
-  );
+describe('Test Asset create modal component', () => {
+  let container: HTMLElement;
 
-  await setTimeout(() => new Promise(resolve => resolve), 10);
-
-  expect(container.querySelector('img').src).toEqual('');
-  expect(container.querySelector('[data-checked="false"]')).toBeInTheDocument();
-  expect(getByText(asset.labels.en_US)).toBeInTheDocument();
-});
-
-test('It displays selected asset card', async () => {
-  const isSelected = true;
-  const {getByText, container} = render(
-    <ThemeProvider theme={akeneoTheme}>
-      <AssetCard
-        asset={asset}
-        context={{locale: 'en_US', channel: 'ecommerce'}}
-        isSelected={isSelected}
-        onSelectionChange={() => {}}
-      />
-    </ThemeProvider>
-  );
-
-  await setTimeout(() => new Promise(resolve => resolve), 10);
-
-  expect(container.querySelector('img').src).toEqual('');
-  expect(container.querySelector('[data-checked="true"]')).toBeInTheDocument();
-  expect(getByText(asset.labels.en_US)).toBeInTheDocument();
-});
-
-test('it can be selected when clicking on the checkbox', () => {
-  let isSelected = false;
-  let selectedCode = null;
-  const {container} = render(
-    <ThemeProvider theme={akeneoTheme}>
-      <AssetCard
-        asset={asset}
-        context={{locale: 'en_US', channel: 'ecommerce'}}
-        isSelected={isSelected}
-        onSelectionChange={(code, value) => {
-          isSelected = value;
-          selectedCode = code;
-        }}
-      />
-    </ThemeProvider>
-  );
-
-  act(() => {
-    fireEvent.click(container.querySelector('[data-checked]'));
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
   });
 
-  expect(isSelected).toEqual(true);
-  expect(selectedCode).toEqual(asset.code);
-});
-
-test('it can be selected when clicking on the asset card', () => {
-  let isSelected = false;
-  let selectedCode = null;
-  const {container} = render(
-    <ThemeProvider theme={akeneoTheme}>
-      <AssetCard
-        asset={asset}
-        context={{locale: 'en_US', channel: 'ecommerce'}}
-        isSelected={isSelected}
-        onSelectionChange={(code, value) => {
-          isSelected = value;
-          selectedCode = code;
-        }}
-      />
-    </ThemeProvider>
-  );
-
-  act(() => {
-    fireEvent.click(container.querySelector('[data-test-id="asset-card-image"]'));
+  afterEach(() => {
+    document.body.removeChild(container);
   });
 
-  expect(isSelected).toEqual(true);
-  expect(selectedCode).toEqual(asset.code);
-});
+  test('It displays an unselected asset card', async () => {
+    const isSelected = false;
+    await act(async () => {
+      ReactDOM.render(
+        <ThemeProvider theme={akeneoTheme}>
+          <AssetCard
+            asset={asset}
+            context={{locale: 'en_US', channel: 'ecommerce'}}
+            isSelected={isSelected}
+            onSelectionChange={() => {}}
+          />
+        </ThemeProvider>,
+        container
+      );
+    });
 
-test('it cannot be selected if there is an on change property', () => {
-  let isSelected = false;
-  let selectedCode = null;
-  const {container} = render(
-    <ThemeProvider theme={akeneoTheme}>
-      <AssetCard
-        asset={asset}
-        context={{locale: 'en_US', channel: 'ecommerce'}}
-        isSelected={isSelected}
-        onClick={() => {}}
-        onSelectionChange={(code, value) => {
-          isSelected = value;
-          selectedCode = code;
-        }}
-      />
-    </ThemeProvider>
-  );
-  act(() => {
-    fireEvent.click(container.firstElementChild);
+    await setTimeout(() => new Promise(resolve => resolve), 10);
+
+    expect(container.querySelector('img').src).toEqual('');
+    expect(container.querySelector('[data-checked="false"]')).toBeInTheDocument();
+    expect(getByText(container, asset.labels.en_US)).toBeInTheDocument();
   });
 
-  expect(isSelected).toEqual(false);
-  expect(selectedCode).toEqual(null);
+  test('It displays selected asset card', async () => {
+    const isSelected = true;
+    await act(async () => {
+      ReactDOM.render(
+        <ThemeProvider theme={akeneoTheme}>
+          <AssetCard
+            asset={asset}
+            context={{locale: 'en_US', channel: 'ecommerce'}}
+            isSelected={isSelected}
+            onSelectionChange={() => {}}
+          />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    await setTimeout(() => new Promise(resolve => resolve), 10);
+
+    expect(container.querySelector('img').src).toEqual('');
+    expect(container.querySelector('[data-checked="true"]')).toBeInTheDocument();
+    expect(getByText(container, asset.labels.en_US)).toBeInTheDocument();
+  });
+
+  test('it can be selected when clicking on the checkbox', async () => {
+    let isSelected = false;
+    let selectedCode = null;
+    await act(async () => {
+      ReactDOM.render(
+        <ThemeProvider theme={akeneoTheme}>
+          <AssetCard
+            asset={asset}
+            context={{locale: 'en_US', channel: 'ecommerce'}}
+            isSelected={isSelected}
+            onSelectionChange={(code, value) => {
+              isSelected = value;
+              selectedCode = code;
+            }}
+          />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    act(() => {
+      fireEvent.click(container.querySelector('[data-checked]'));
+    });
+
+    expect(isSelected).toEqual(true);
+    expect(selectedCode).toEqual(asset.code);
+  });
+
+  test('it can be selected when clicking on the asset card', async () => {
+    let isSelected = false;
+    let selectedCode = null;
+    await act(async () => {
+      ReactDOM.render(
+        <ThemeProvider theme={akeneoTheme}>
+          <AssetCard
+            asset={asset}
+            context={{locale: 'en_US', channel: 'ecommerce'}}
+            isSelected={isSelected}
+            onSelectionChange={(code, value) => {
+              isSelected = value;
+              selectedCode = code;
+            }}
+          />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    act(() => {
+      fireEvent.click(container.querySelector('[data-test-id="asset-card-image"]'));
+    });
+
+    expect(isSelected).toEqual(true);
+    expect(selectedCode).toEqual(asset.code);
+  });
+
+  test('it cannot be selected if there is an on change property', async () => {
+    let isSelected = false;
+    let selectedCode = null;
+    let assetCode = '';
+    const onClick = jest.fn().mockImplementation((code: string) => {
+      assetCode = code;
+    });
+    await act(async () => {
+      ReactDOM.render(
+        <ThemeProvider theme={akeneoTheme}>
+          <AssetCard
+            asset={asset}
+            context={{locale: 'en_US', channel: 'ecommerce'}}
+            isSelected={isSelected}
+            onClick={onClick}
+            onSelectionChange={(code, value) => {
+              isSelected = value;
+              selectedCode = code;
+            }}
+          />
+        </ThemeProvider>,
+        container
+      );
+    });
+
+    act(() => {
+      fireEvent.click(container.querySelector('[data-test-id="asset-card-image"]'));
+    });
+
+    expect(isSelected).toEqual(false);
+    expect(selectedCode).toEqual(null);
+    expect(onClick).toBeCalled();
+    expect(assetCode).toEqual(asset.code);
+  });
 });
