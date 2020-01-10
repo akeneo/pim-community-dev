86286d578df35acf329435dfeeab604825b738ee
Adrien Pétremann <hello@grena.fr>
2020-01-10 15:22:47 +0100

    Add legacy asset bundle and asset collection value factory

---
+++ src/Akeneo/Asset/Bundle/AkeneoAssetBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Asset\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AkeneoAssetBundle extends Bundle
+{
+}
