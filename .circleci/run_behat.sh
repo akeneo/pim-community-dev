a2a2f6e0b23821c200d522405bc2c895eb37415b
Benoit Jacquemont <benoit@akeneo.com>
2020-01-14 17:29:50 +0100

    Fixes Behat run

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -6,5 +6,5 @@ TEST_SUITE=$1
 
 ID=$(uuidgen)
 
-TESTS=docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings > var/tests/behat/behat_tests_$ID.scenarios
-docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/behat_tests_$ID.results.xml --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
+docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings > var/tests/behat/behat_tests_$ID.scenarios
+docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/behat_tests_$ID.results.xml --format pretty --out std --colors -p legacy -s $TEST_SUITE var/tests/behat/behat_tests_$ID.scenarios
