0e70cd06e4f31bdf485ab2669abfc83693d36a50
Michel Tag <micheltag@akeneo.com>
2020-01-14 09:57:25 +0100

    Preparing version 3.2.31 (#11396)
    Preparing version 3.2.31
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.30';
+    const VERSION = '3.2.31';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
