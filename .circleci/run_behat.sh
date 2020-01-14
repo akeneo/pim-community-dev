957bff6ad04d231f27b715c950f8a73c5c0ff2f6
Benoit Jacquemont <benoit@akeneo.com>
2020-01-14 16:34:59 +0100

    GITHUB-11403 Tests scenarios

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -4,23 +4,7 @@ set -eo pipefail
 
 TEST_SUITE=$1
 
-TEST_FILES=$(docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings)
+ID=$(uuidgen)
 
-fail=0
-counter=1
-total=$(($(echo $TEST_FILES | grep -o ' ' | wc -l) + 1))
-
-for TEST_FILE in $TEST_FILES; do
-    echo "$TEST_FILE ($counter/$total):"
-    output=$(basename $TEST_FILE)_$(uuidgen)
-
-    set +e
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE ||
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
-
-    fail=$(($fail + $?))
-    counter=$(($counter + 1))
-    set -eo pipefail
-done
-
-exit $fail
+TESTS=docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings > var/tests/behat/behat_tests_$ID.scenarios
+docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/behat_tests_$ID.results.xml --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
