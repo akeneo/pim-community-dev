2b943b93cc3de638d7e5c4b963a365277338eb1f
jmleroux <jmleroux.pro@gmail.com>
2020-01-14 14:25:01 +0100

    PIM-9036: Fix schema

--- upgrades/schema/Version_3_0_20191227141338_fix_versioning_indexes.php
+++ upgrades/schema/Version_3_0_20191227141338_fix_versioning_indexes.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Upgrade\Schema;
 
+use Doctrine\DBAL\Exception\DriverException;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
@@ -10,24 +11,54 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version_3_0_20191227141338_fix_versioning_indexes extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
     public function up(Schema $schema)
     {
-        $sql = <<<SQL
-ALTER TABLE pim_versioning_version 
-    DROP INDEX resource_name_idx,
-    DROP INDEX version_idx,
-    ADD INDEX resource_name_logged_at_idx (resource_name, logged_at);
-SQL;
-
-        $this->addSql($sql);
+        $this->updateVersioningIndexes();
     }
 
     /**
-     * @param Schema $schema
+     * Try to drop and create versioning indexes.
+     * Does nothing if they are already dropped/created
+     *
+     * @throws DriverException
+     * @throws \Doctrine\DBAL\DBALException
      */
+    private function updateVersioningIndexes()
+    {
+        $sql = 'ALTER TABLE pim_versioning_version DROP INDEX resource_name_idx;';
+
+        try {
+            $this->connection->executeQuery($sql);
+        } catch (DriverException $dbalException) {
+            if ($dbalException->getErrorCode() !== 1091) {
+                throw $dbalException;
+            }
+        }
+
+        $sql = 'ALTER TABLE pim_versioning_version DROP INDEX resource_name_resource_id_idx';
+
+        try {
+            $this->connection->executeQuery($sql);
+        } catch (DriverException $dbalException) {
+            if ($dbalException->getErrorCode() !== 1091) {
+                throw $dbalException;
+            }
+        }
+
+        $sql = <<<SQL
+ALTER TABLE pim_versioning_version 
+    ADD INDEX resource_name_resource_id_version_idx (resource_name, resource_id, version);
+SQL;
+
+        try {
+            $this->connection->executeQuery($sql);
+        } catch (DriverException $dbalException) {
+            if ($dbalException->getErrorCode() !== 1061) {
+                throw $dbalException;
+            }
+        }
+    }
+
     public function down(Schema $schema)
     {
         $this->throwIrreversibleMigrationException();
