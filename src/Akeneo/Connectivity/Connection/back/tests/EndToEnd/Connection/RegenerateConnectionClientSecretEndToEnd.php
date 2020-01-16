0e391eae17b08634fd963bf031158a09085fb8c9
romain <romain@akeneo.com>
2020-01-16 12:10:02 +0100

    API-858: Rework secret regeneration tests

--- src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Connection/RegenerateConnectionClientSecretEndToEnd.php
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Connection/RegenerateConnectionClientSecretEndToEnd.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Akeneo\Connectivity\Connection\back\tests\EndToEnd\Connection;
 
-use Akeneo\Connectivity\Connection\Application\Settings\Command\CreateConnectionCommand;
 use Akeneo\Connectivity\Connection\Application\Settings\Command\RegenerateConnectionSecretCommand;
-use Akeneo\Connectivity\Connection\Domain\Settings\Model\ValueObject\FlowType;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Tool\Bundle\ApiBundle\tests\integration\ApiTestCase;
 use PHPUnit\Framework\Assert;
@@ -19,10 +17,9 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class RegenerateConnectionClientSecretEndToEnd extends ApiTestCase
 {
-    public function test_it_disables_the_client_secret_and_tokens()
+    public function test_it_disables_the_access_token()
     {
-        $createConnectionCommand = new CreateConnectionCommand('magento', 'Magento Connector', FlowType::DATA_DESTINATION);
-        $connectionWithCredentials = $this->get('akeneo_connectivity.connection.application.handler.create_connection')->handle($createConnectionCommand);
+        $connectionWithCredentials = $this->createConnection('magento');
 
         $apiClient = $this->createAuthenticatedClient(
             [],
@@ -32,7 +29,7 @@ class RegenerateConnectionClientSecretEndToEnd extends ApiTestCase
             $connectionWithCredentials->username(),
             $connectionWithCredentials->password()
         );
-        $apiClient->request('GET', 'api/rest/v1/attributes');
+        $apiClient->request('GET', 'api/rest/v1/currencies');
         Assert::assertEquals(Response::HTTP_OK, $apiClient->getResponse()->getStatusCode());
 
         $apiClient->reload();
@@ -42,7 +39,46 @@ class RegenerateConnectionClientSecretEndToEnd extends ApiTestCase
         $this->get('akeneo_connectivity.connection.application.handler.regenerate_connection_secret')->handle($regenerateConnectionSecretCommand);
 
         $apiClient->reload();
-        Assert::assertEquals(Response::HTTP_UNAUTHORIZED, $apiClient->getResponse()->getStatusCode());
+
+        $expectedResponseContent = <<<JSON
+{
+    "code": 401,
+    "message": "The access token provided is invalid."
+}
+JSON;
+        $this->assertJsonStringEqualsJsonString($expectedResponseContent, $apiClient->getResponse()->getContent());
+    }
+
+    public function test_it_disables_the_secret()
+    {
+        $connectionWithCredentials = $this->createConnection('magento');
+
+        $command = new RegenerateConnectionSecretCommand('magento');
+        $this->get('akeneo_connectivity.connection.application.handler.regenerate_connection_secret')->handle($command);
+
+        $client = static::createClient(['debug' => false]);
+        $client->request('POST', 'api/oauth/v1/token',
+            [
+                'username'   => $connectionWithCredentials->username(),
+                'password'   => $connectionWithCredentials->password(),
+                'grant_type' => 'password',
+            ],
+            [],
+            [
+                'PHP_AUTH_USER' => $connectionWithCredentials->clientId(),
+                'PHP_AUTH_PW'   => $connectionWithCredentials->secret(),
+                'CONTENT_TYPE'  => 'application/json',
+            ]
+        );
+
+        $expectedResponseContent = <<<JSON
+{
+    "code": 422,
+    "message": "Parameter \"client_id\" is missing or does not match any client, or secret is invalid"
+}
+JSON;
+        $this->assertJsonStringEqualsJsonString($expectedResponseContent, $client->getResponse()->getContent());
+
     }
 
     /**
