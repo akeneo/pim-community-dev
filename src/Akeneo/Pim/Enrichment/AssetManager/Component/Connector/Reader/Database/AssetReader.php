a75cc9e438cc7aed473482c653d2e157641ae0e7
Mathias Métayer <mathias.metayer@akeneo.com>
2020-01-23 10:42:33 +0100

    AST-78: Create Database Asset Reader

---
+++ src/Akeneo/Pim/Enrichment/AssetManager/Component/Connector/Reader/Database/AssetReader.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Enrichment\AssetManager\Component\Connector\Reader\Database;
+
+use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamilyIdentifier;
+use Akeneo\AssetManager\Domain\Query\Asset\FindAssetIdentifiersByAssetFamilyInterface;
+use Akeneo\AssetManager\Domain\Repository\AssetRepositoryInterface;
+use Akeneo\Tool\Component\Batch\Item\InitializableInterface;
+use Akeneo\Tool\Component\Batch\Item\ItemReaderInterface;
+use Akeneo\Tool\Component\Batch\Model\StepExecution;
+use Akeneo\Tool\Component\Batch\Step\StepExecutionAwareInterface;
+
+class AssetReader implements ItemReaderInterface, InitializableInterface, StepExecutionAwareInterface
+{
+    /** @var StepExecution */
+    private $stepExecution;
+
+    /** @var FindAssetIdentifiersByAssetFamilyInterface */
+    private $findAssetIdentifiersByAssetFamily;
+
+    /** @var AssetRepositoryInterface */
+    private $assetRepository;
+
+    /** @var \Iterator */
+    private $identifiers;
+
+    /** @var bool */
+    private $firstRead;
+
+    public function __construct(
+        FindAssetIdentifiersByAssetFamilyInterface $findAssetIdentifiersByAssetFamily,
+        AssetRepositoryInterface $assetRepository
+    ) {
+        $this->findAssetIdentifiersByAssetFamily = $findAssetIdentifiersByAssetFamily;
+        $this->assetRepository = $assetRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize()
+    {
+        $assetFamilyIdentifier = AssetFamilyIdentifier::fromString(
+            $this->stepExecution->getJobParameters()->get('asset_family_identifier')
+        );
+        $this->identifiers = $this->findAssetIdentifiersByAssetFamily->find($assetFamilyIdentifier);
+        $this->firstRead = true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read()
+    {
+        $asset = null;
+        if (!$this->firstRead) {
+            $this->identifiers->next();
+        }
+
+        if ($this->identifiers->valid()) {
+            $asset = $this->assetRepository->getByIdentifier($this->identifiers->current());
+            if (null !== $asset) {
+                $this->stepExecution->incrementSummaryInfo('read');
+            }
+        }
+
+        $this->firstRead = false;
+
+        return $asset;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setStepExecution(StepExecution $stepExecution)
+    {
+        $this->stepExecution = $stepExecution;
+    }
+}
---
+++ tests/back/Pim/Enrichment/AssetManager/Specification/Component/Connector/Reader/Database/AssetReaderSpec.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Specification\Akeneo\Pim\Enrichment\AssetManager\Component\Connector\Reader\Database;
+
+use Akeneo\AssetManager\Domain\Model\Asset\Asset;
+use Akeneo\AssetManager\Domain\Model\Asset\AssetCode;
+use Akeneo\AssetManager\Domain\Model\Asset\AssetIdentifier;
+use Akeneo\AssetManager\Domain\Model\Asset\Value\ValueCollection;
+use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamilyIdentifier;
+use Akeneo\AssetManager\Domain\Query\Asset\FindAssetIdentifiersByAssetFamilyInterface;
+use Akeneo\AssetManager\Domain\Repository\AssetRepositoryInterface;
+use Akeneo\Pim\Enrichment\AssetManager\Component\Connector\Reader\Database\AssetReader;
+use Akeneo\Tool\Component\Batch\Item\ItemReaderInterface;
+use Akeneo\Tool\Component\Batch\Job\JobParameters;
+use Akeneo\Tool\Component\Batch\Model\StepExecution;
+use PhpSpec\ObjectBehavior;
+
+class AssetReaderSpec extends ObjectBehavior
+{
+    function let(
+        FindAssetIdentifiersByAssetFamilyInterface $findAssetIdentifiersByAssetFamily,
+        AssetRepositoryInterface $assetRepository,
+        StepExecution $stepExecution,
+        JobParameters $parameters
+    ) {
+        $this->beConstructedWith($findAssetIdentifiersByAssetFamily, $assetRepository);
+
+        $parameters->get('asset_family_identifier')->willReturn('packshot');
+        $stepExecution->getJobParameters()->willReturn($parameters);
+        $this->setStepExecution($stepExecution);
+
+        $assetFamilyIdentifier = AssetFamilyIdentifier::fromString('packshot');
+        $findAssetIdentifiersByAssetFamily->find($assetFamilyIdentifier)->willReturn(
+            new \ArrayIterator(
+                [
+                    AssetIdentifier::fromString('asset_packshot_1'),
+                    AssetIdentifier::fromString('asset_packshot_2'),
+                    AssetIdentifier::fromString('asset_packshot_3'),
+                ]
+            )
+        );
+        $this->initialize();
+    }
+
+    function it_is_an_item_reader()
+    {
+        $this->shouldImplement(ItemReaderInterface::class);
+    }
+
+    function it_is_a_database_asset_reader()
+    {
+        $this->shouldHaveType(AssetReader::class);
+    }
+
+    function it_reads_items_from_database(
+        AssetRepositoryInterface $assetRepository,
+        StepExecution $stepExecution
+    ) {
+        $assetFamilyIdentifier = AssetFamilyIdentifier::fromString('packshot');
+        [$asset1, $asset2, $asset3] = [
+            $this->createAsset(AssetIdentifier::fromString('asset_packshot_1'), $assetFamilyIdentifier),
+            $this->createAsset(AssetIdentifier::fromString('asset_packshot_2'), $assetFamilyIdentifier),
+            $this->createAsset(AssetIdentifier::fromString('asset_packshot_3'), $assetFamilyIdentifier),
+        ];
+        $assetRepository->getByIdentifier(AssetIdentifier::fromString('asset_packshot_1'))->willReturn($asset1);
+        $assetRepository->getByIdentifier(AssetIdentifier::fromString('asset_packshot_2'))->willReturn($asset2);
+        $assetRepository->getByIdentifier(AssetIdentifier::fromString('asset_packshot_3'))->willReturn($asset3);
+
+        $stepExecution->incrementSummaryInfo('read')->shouldBeCalledTimes(3);
+
+        $this->read()->shouldReturn($asset1);
+        $this->read()->shouldReturn($asset2);
+        $this->read()->shouldReturn($asset3);
+        $this->read()->shouldReturn(null);
+    }
+
+    private function createAsset(AssetIdentifier $identifier, AssetFamilyIdentifier $familyIdentifier): Asset
+    {
+        return Asset::create(
+            $identifier,
+            $familyIdentifier,
+            AssetCode::fromString($identifier->__toString()),
+            ValueCollection::fromValues([])
+        );
+    }
+}
