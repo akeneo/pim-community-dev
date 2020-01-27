8995502edf1de21bc435f1f1a060a7591f78dee3
Pierre Jolly <pie.jolly@gmail.com>
2020-01-27 15:35:05 +0100

    API-945: add UpdateAuditDataCommand end to end test

--- src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Connection/CheckAuthenticationSecurityEndToEnd.php
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Connection/CheckAuthenticationSecurityEndToEnd.php
@@ -41,7 +41,7 @@ class CheckAuthenticationSecurityEndToEnd extends ApiTestCase
 
         // Assert DB content
         $arrayClientId = explode('_', $apiConnection->clientId());
-        $dbalConnection = $dbalConnection = $this->get('database_connection');
+        $dbalConnection = $this->get('database_connection');
         $results = $dbalConnection->fetchAll('SELECT id FROM pim_api_access_token WHERE client = '. $arrayClientId[0]);
         Assert::assertCount(1, $results);
 
@@ -115,7 +115,7 @@ class CheckAuthenticationSecurityEndToEnd extends ApiTestCase
 
         // Assert DB content
         $arrayClientId = explode('_', $apiConnection->clientId());
-        $dbalConnection = $dbalConnection = $this->get('database_connection');
+        $dbalConnection = $this->get('database_connection');
         $results = $dbalConnection->fetchAll('SELECT id FROM pim_api_refresh_token WHERE client = '. $arrayClientId[0]);
         Assert::assertCount(1, $results);
 
