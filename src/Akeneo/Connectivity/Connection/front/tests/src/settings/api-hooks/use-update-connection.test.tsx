0698ad7a474664f9fc3c69ac42ee5f5df01bd189
Paul Chasle <paul.chasle@akeneo.com>
2020-01-29 18:35:18 +0100

    API-953: Add unit tests

--- src/Akeneo/Connectivity/Connection/front/tests/src/settings/api-hooks/use-update-connection.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/settings/api-hooks/use-update-connection.test.tsx
@@ -1,26 +1,25 @@
 import React, {PropsWithChildren} from 'react';
 import {FlowType} from '@src/model/flow-type.enum';
 import {useUpdateConnection} from '@src/settings/api-hooks/use-update-connection';
-import {ok} from '@src/shared/fetch-result/result';
+import {ok, err} from '@src/shared/fetch-result/result';
 import {NotificationLevel, NotifyContext} from '@src/shared/notify';
 import {act, renderHook} from '@testing-library/react-hooks';
+
+const notify = jest.fn();
+const wrapper = ({children}: PropsWithChildren<{}>) => (
+    <NotifyContext.Provider value={notify}>{children}</NotifyContext.Provider>
+);
 
 describe('useUpdateConnection', () => {
     beforeEach(() => {
         fetchMock.resetMocks();
+        notify.mockClear();
     });
 
     it('updates a connection', async () => {
         fetchMock.mockResponseOnce(JSON.stringify('ok'));
 
-        const notify = jest.fn();
-        const wrapper = ({children}: PropsWithChildren<{}>) => (
-            <NotifyContext.Provider value={notify}>{children}</NotifyContext.Provider>
-        );
-
-        const {result} = renderHook(() => useUpdateConnection('franklin'), {
-            wrapper,
-        });
+        const {result} = renderHook(() => useUpdateConnection('franklin'), {wrapper});
 
         let updateConnectionResult;
         await act(async () => {
@@ -51,6 +50,53 @@ describe('useUpdateConnection', () => {
         expect(notify).toBeCalledWith(
             NotificationLevel.SUCCESS,
             'akeneo_connectivity.connection.edit_connection.flash.success'
+        );
+    });
+
+    it('handles a bad request', async () => {
+        const response = {errors: [{reason: 'reason 1'}, {reason: 'reason 2'}]};
+        fetchMock.mockResponseOnce(JSON.stringify(response), {status: 400});
+
+        const {result} = renderHook(() => useUpdateConnection('franklin'), {wrapper});
+
+        let updateConnectionResult;
+        await act(async () => {
+            updateConnectionResult = await result.current({
+                code: 'franklin',
+                label: 'Franklin',
+                flowType: FlowType.DATA_SOURCE,
+                image: null,
+                userRoleId: '1',
+                userGroupId: '2',
+            });
+        });
+
+        expect(updateConnectionResult).toStrictEqual(err(response));
+        expect(notify).toBeCalledWith(NotificationLevel.ERROR, 'reason 1');
+        expect(notify).toBeCalledWith(NotificationLevel.ERROR, 'reason 2');
+    });
+
+    it('handles a unknown error', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify('fail'), {status: 400});
+
+        const {result} = renderHook(() => useUpdateConnection('franklin'), {wrapper});
+
+        let updateConnectionResult;
+        await act(async () => {
+            updateConnectionResult = await result.current({
+                code: 'franklin',
+                label: 'Franklin',
+                flowType: FlowType.DATA_SOURCE,
+                image: null,
+                userRoleId: '1',
+                userGroupId: '2',
+            });
+        });
+
+        expect(updateConnectionResult).toStrictEqual(err('fail'));
+        expect(notify).toBeCalledWith(
+            NotificationLevel.ERROR,
+            'akeneo_connectivity.connection.edit_connection.flash.error'
         );
     });
 });
