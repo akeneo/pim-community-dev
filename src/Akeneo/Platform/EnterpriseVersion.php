d91679d0dbac3f68b7fd24dfb2879fcf01f46698
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2020-01-30 17:37:49 +0100

    Change SaaS version

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '';
+    const VERSION = 'v20200130151605';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Cirrus';
