194d89aa91e55a4be3811c96a8efac4c79362bf5
anaisbaunelemaire <57708349+anaisbaunelemaire@users.noreply.github.com>
2020-02-10 09:11:53 +0100

    DAPI-628: Progressive loading of the data quality rate badges in the sidebar (#8023)
    * DAPI-628: Adapt 'Rate' component for its "isLoading" display
    
    * DAPI-628: Update of files that contain Rate component (new prop isLoading)
    
    * DAPI-628: Less files update
    
    * DAPI-628: Remove useless useEffect()
    
    * DAPI-628: Correct isLoading prop

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Rate.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Rate.tsx
@@ -1,10 +1,20 @@
 import React, {FunctionComponent} from 'react';
 
 interface RateProps {
-  value?: string|null;
+  value?: string | null;
+  isLoading?: boolean;
 }
 
-const Rate: FunctionComponent<RateProps> = ({value}) => {
+const Rate: FunctionComponent<RateProps> = ({value, isLoading = false}) => {
+
+  if (isLoading) {
+    return (
+      <div className={'AknDataQualityInsightsRate AknDataQualityInsightsRate-Loading'}>
+        <img src={'bundles/akeneodataqualityinsights/images/AxisRateLoader.svg'}
+             className={' AknDataQualityInsightsRate-Loader'}/>
+      </div>
+    )
+  }
   return (
     <>
       {value ? (
