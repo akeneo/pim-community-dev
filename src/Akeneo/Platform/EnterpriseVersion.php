816d9279513e550baa52cfb0e6b236cdebcd2a0a
Michel Tag <micheltag@akeneo.com>
2020-02-12 16:20:58 +0100

    Preparing version 4.0.4 (#8089)
    Preparing version 4.0.4
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.3';
+    const VERSION = '4.0.4';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Daisy';
