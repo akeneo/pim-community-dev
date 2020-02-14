294cfad6ebe88ed9a19fd47b9b3c3324135bac4f
Quentin Favrie <quentin.favrie@akeneo.com>
2020-02-14 17:35:18 +0100

    AST-271: Increase too much files notification delay (#8108)

--- src/Akeneo/AssetManager/front/application/asset-upload/utils/upload-limit.ts
+++ src/Akeneo/AssetManager/front/application/asset-upload/utils/upload-limit.ts
@@ -4,7 +4,7 @@ const FILE_UPLOAD_LIMIT = 500;
 
 export default (files: File[], currentQuantity: number): File[] => {
   if (currentQuantity + files.length > FILE_UPLOAD_LIMIT) {
-    notify('warning', 'pim_asset_manager.asset.upload.files_limit', {limit: FILE_UPLOAD_LIMIT});
+    notify('warning', 'pim_asset_manager.asset.upload.files_limit', {limit: FILE_UPLOAD_LIMIT}, {delay: 8000});
   }
 
   if (currentQuantity >= FILE_UPLOAD_LIMIT) {
