3ce091046916c051e81e3b5d2d713dfef8e82579
Michel Tag <micheltag@akeneo.com>
2020-02-17 11:30:40 +0100

    Preparing version 3.2.39 (#8116)
    Preparing version 3.2.39
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.38';
+    const VERSION = '3.2.39';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Edelweiss';
