36918ed337695159b7a5a0fc83393eb62ed3f8e7
Samir Boulil <samir.boulil@akeneo.com>
2020-02-18 14:23:03 +0100

    MET-11: Add UnitCode spec

--- src/Akeneo/Tool/Bundle/MeasureBundle/spec/Model/UnitCodeSpec.php
+++ src/Akeneo/Tool/Bundle/MeasureBundle/spec/Model/UnitCodeSpec.php
@@ -2,12 +2,34 @@
 
 declare(strict_types=1);
 
-namespace spec\Akeneo\Tool\Bundle\MeasureBundle\Manager;
+namespace spec\Akeneo\Tool\Bundle\MeasureBundle\Model;
 
-class UnitCodeSpec
+use Akeneo\Tool\Bundle\MeasureBundle\Model\Operation;
+use Akeneo\Tool\Bundle\MeasureBundle\Model\UnitCode;
+use PhpSpec\ObjectBehavior;
+
+class UnitCodeSpec extends ObjectBehavior
 {
+    private const UNIT_CODE = 'meter';
+
+    public function let()
+    {
+        $this->beConstructedThrough('fromString', [self::UNIT_CODE]);
+    }
+
     function it_is_initializable()
     {
+        $this->shouldHaveType(UnitCode::class);
+    }
 
+    function it_is_normalizable()
+    {
+        $this->normalize()->shouldReturn(self::UNIT_CODE);
+    }
+
+    function it_cannot_be_constructed_with_an_empty_string()
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->during('fromString', ['']);
     }
 }
