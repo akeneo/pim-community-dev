16cd985ecce68c7a863a2aab3a92ef298151a721
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-02-19 18:10:48 +0100

    MET-12: Fix comment

--- src/Akeneo/Tool/Bundle/MeasureBundle/tests/Integration/Persistence/MeasurementFamilyRepositoryIntegration.php
+++ src/Akeneo/Tool/Bundle/MeasureBundle/tests/Integration/Persistence/MeasurementFamilyRepositoryIntegration.php
@@ -31,6 +31,14 @@ class MeasurementFamilyRepositoryIntegration extends SqlIntegrationTestCase
         parent::setUp();
 
         $this->repository = $this->get('akeneo_measure.persistence.measurement_family_repository');
+
+        $sql = <<<SQL
+INSERT INTO `akeneo_measurement` (`code`, `labels`, `standard_unit`, `units`)
+VALUES
+	('Area', '{\"en_US\": \"Area\", \"fr_FR\": \"Surface\"}', 'SQUARE_MILLIMETER', '[{\"code\": \"SQUARE_MILLIMETER\", \"labels\": {\"en_US\": \"Square millimeter\", \"fr_FR\": \"Millimètre carré\"}, \"symbol\": \"mm²\", \"convert\": [{\"value\": \"0.000001\", \"operator\": \"mul\"}]}, {\"code\": \"SQUARE_CENTIMETER\", \"labels\": {\"en_US\": \"Square centimeter\", \"fr_FR\": \"Centimètre carré\"}, \"symbol\": \"cm²\", \"convert\": [{\"value\": \"0.0001\", \"operator\": \"mul\"}]}]'),
+	('Binary', '{\"en_US\": \"Binary\", \"fr_FR\": \"Binaire\"}', 'BYTE', '[{\"code\": \"BIT\", \"labels\": {\"en_US\": \"Bit\", \"fr_FR\": \"Bit\"}, \"symbol\": \"b\", \"convert\": [{\"value\": \"0.125\", \"operator\": \"mul\"}]}, {\"code\": \"BYTE\", \"labels\": {\"en_US\": \"Byte\", \"fr_FR\": \"Octet\"}, \"symbol\": \"B\", \"convert\": [{\"value\": \"1\", \"operator\": \"mul\"}]}]');
+SQL;
+        $this->connection->executeQuery($sql);
     }
 
     /**
