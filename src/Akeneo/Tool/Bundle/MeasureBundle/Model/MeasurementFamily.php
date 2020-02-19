27a796693f1e09ee7abee4a34c44e59666acd218
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-02-19 10:50:05 +0100

    MET-35: Add labels in model

--- src/Akeneo/Tool/Bundle/MeasureBundle/Model/MeasurementFamily.php
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Model/MeasurementFamily.php
@@ -16,33 +16,40 @@ class MeasurementFamily
     /** @var string */
     private $code;
 
+    /** @var LabelCollection */
+    private $labels;
+
     /** @var UnitCode */
     private $standardUnitCode;
 
     /** @var array */
     private $units;
 
-    private function __construct(MeasurementFamilyCode $code, UnitCode $standardUnitCode, array $units)
+    private function __construct(MeasurementFamilyCode $code, array $labels, UnitCode $standardUnitCode, array $units)
     {
+        Assert::stringNotEmpty($code);
+        Assert::isInstanceOf($labels, LabelCollection::class);
         Assert::allIsInstanceOf($units, Unit::class);
         Assert::minCount($units, 1);
         $this->assertStandardUnitIsAlsoAUnit($standardUnitCode, $units);
         $this->assertNoDuplicatedUnits($units);
 
         $this->code = $code;
+        $this->labels = $labels;
         $this->standardUnitCode = $standardUnitCode;
         $this->units = $units;
     }
 
-    public static function create(MeasurementFamilyCode $code, UnitCode $standardUnitCode, array $units): self
+    public static function create(MeasurementFamilyCode $code, array $labels, UnitCode $standardUnitCode, array $units): self
     {
-        return new self($code, $standardUnitCode, $units);
+        return new self($code,  $labels, $standardUnitCode, $units);
     }
 
     public function normalize(): array
     {
         return [
             'code' => $this->code->normalize(),
+            'labels' => $this->labels->normalize(),
             'standard_unit_code' => $this->standardUnitCode->normalize(),
             'units' => array_map(
                 function (Unit $unit) {
