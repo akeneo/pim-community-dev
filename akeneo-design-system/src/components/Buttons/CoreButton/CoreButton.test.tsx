5790d83aa9ea6ace36295227474c35ade6d6c558
Nicolas Maligne <nicolas.maligne@akeneo.com>
2020-02-27 18:07:03 +0100

    add test and enter keyboard

--- akeneo-design-system/src/components/Buttons/CoreButton/CoreButton.test.tsx
+++ akeneo-design-system/src/components/Buttons/CoreButton/CoreButton.test.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { CoreButton } from "./CoreButton";
 import {
-    render,
+    render, fireEvent
 } from "@testing-library/react";
 
+//https://github.com/testing-library/dom-testing-library/blob/master/src/events.js
+
 describe("testing button", () => {
-    test("should do something", () => {
+    test("should a button with the given children", () => {
         // Given
         const onClick = jest.fn();
-        const myLabel = "Hello world";
+        const myLabel = "Click Here";
         // When
         const { getByText } = render(
             <CoreButton onClick={onClick}>{myLabel}</CoreButton>
         );
         // Then
-        expect(getByText("Hello world")).toBeInTheDocument();
+        expect(getByText("Click Here")).toBeInTheDocument();
+    });
+    test('should call the function callback when there is a left mouse click', () => {
+        // Given
+        const onClick = jest.fn();
+        const myLabel = "Click Here";
+        // When
+        const { getByText } = render(
+            <CoreButton onClick={onClick}>{myLabel}</CoreButton>
+        );
+        fireEvent.click(getByText("Click Here"), { bubbles: true, cancelable: true, button: 0 })
+        // Then
+        expect(onClick).toHaveBeenCalledTimes(1)
     });
 });
