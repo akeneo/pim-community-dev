7b1209c5d2c8a52437a4fb55d3468891491ec013
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-03-06 10:36:54 +0100

    DAPI-887: fix recommendations display for root product models

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Attribute.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Attribute.tsx
@@ -2,13 +2,14 @@ import React, {FunctionComponent, ReactElement} from 'react';
 import {DATA_QUALITY_INSIGHTS_SHOW_ATTRIBUTE} from "../../../../listener";
 import {Product} from "../../../../../domain";
 import {isSimpleProduct} from "../../../../helper/ProductEditForm/Product";
-import {PRODUCT_MODEL_ATTRIBUTES_TAB_NAME} from "../../../../constant";
+import {
+  PRODUCT_MODEL_ATTRIBUTES_TAB_NAME,
+  ROOT_PRODUCT_MODEL_LEVEL,
+  SUB_PRODUCT_MODEL_LEVEL
+} from "../../../../constant";
 import {ATTRIBUTE_TO_IMPROVE_SESSION_STORAGE_KEY} from "../AttributesTabContent";
 
 const Router = require('pim/router');
-
-const SUB_PRODUCT_MODEL_LEVEL = 1;
-const ROOT_PRODUCT_MODEL_LEVEL = 0;
 
 interface AttributeProps {
   attributeCode: string;
--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/TabContent/DataQualityInsights/VariantAttributesList.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/TabContent/DataQualityInsights/VariantAttributesList.tsx
@@ -1,6 +1,8 @@
 import {AttributeWithRecommendation, Product} from '../../../../../domain';
 import React from 'react';
 import AttributesList from './AttributesList';
+import {isRootProductModel} from "../../../../helper/ProductEditForm/Product";
+import {ROOT_PRODUCT_MODEL_LEVEL} from "../../../../constant";
 
 const __ = require('oro/translator');
 
@@ -34,19 +36,21 @@ const VariantAttributesList = ({product, criterionCode, attributes, locale}: Att
   };
 
   return (
-    <div className="CriterionVariantRecommendations">
-      {product.meta.variant_navigation
-        // @ts-ignore
-        .filter((_: any, index) => index <= product.meta.level)
-        .map((variant: any, index: number) => {
-          return (
-            <div key={`variant-attributes-list-${index}`} className="attributeList">
-              <span>{index > 0 ? variant.axes[locale] : __('pim_enrich.entity.product.module.variant_navigation.common')}</span>:&thinsp;
-              <AttributesList product={product} criterionCode={criterionCode} attributes={getLevelAttributes(attributes, index)}/>
-            </div>
-          )
-      })}
-    </div>
+    isRootProductModel(product) ?
+      <AttributesList product={product} criterionCode={criterionCode} attributes={getLevelAttributes(attributes, ROOT_PRODUCT_MODEL_LEVEL)}/> :
+      <div className="CriterionVariantRecommendations">
+        {product.meta.variant_navigation
+          // @ts-ignore
+          .filter((_: any, index) => index <= product.meta.level)
+          .map((variant: any, index: number) => {
+            return (
+              <div key={`variant-attributes-list-${index}`} className="attributeList">
+                <span>{index > 0 ? variant.axes[locale] : __('pim_enrich.entity.product.module.variant_navigation.common')}</span>:&thinsp;
+                <AttributesList product={product} criterionCode={criterionCode} attributes={getLevelAttributes(attributes, index)}/>
+              </div>
+            )
+        })}
+      </div>
   );
 };
 
