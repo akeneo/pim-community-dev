fa48dbf68c66e7c506fa1afcf76484f399bdf30d
Grégoire HUBERT <gregoire.hubert@akeneo.com>
2020-03-09 18:00:18 +0100

    fix CS

--- upgrades/schema/Version_5_0_20200309165300_remove_product_foreign_key_pim_catalog_completeness.php
+++ upgrades/schema/Version_5_0_20200309165300_remove_product_foreign_key_pim_catalog_completeness.php
@@ -14,7 +14,8 @@ final class Version_5_0_20200309165300_remove_product_foreign_key_pim_catalog_co
         $this->addSql('ALTER TABLE pim_catalog_completeness DROP FOREIGN KEY pim_catalog_completeness_ibfk_1');
     }
 
-    public function down(Schema $schema) : void {
+    public function down(Schema $schema) : void
+    {
         $this->addSql('ALTER TABLE pim_catalog_completeness ADD FOREIGN KEY pim_catalog_completeness_ibfk_1 (product_id) REFERENCES pim_catalog_product (id)');
     }
 }
