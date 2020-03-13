ed2d9afc24b218752b280b4d3d1518be9520cc01
EvrardCaron <pv25@laposte.net>
2020-03-13 12:04:01 +0100

    RUL-96 Create endpoint to get a rule

---
+++ tests/back/Pim/Automation/Integration/ControllerIntegrationTestCase.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AkeneoTestEnterprise\Pim\Automation\Integration;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * This class is used for running integration tests testing the web controllers.
+ *
+ * @author    Evrard Caron <evrard.caron@akeneo.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ */
+abstract class ControllerIntegrationTestCase extends WebTestCase
+{
+    /** @var KernelBrowser */
+    protected $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient(['environment' => 'test_fake', 'debug' => false]);
+        $this->client->disableReboot();
+    }
+
+    protected function get(string $service)
+    {
+        return self::$container->get($service);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown(): void
+    {
+        $connectionCloser = $this->get('akeneo_integration_tests.doctrine.connection.connection_closer');
+        $connectionCloser->closeConnections();
+
+        $this->ensureKernelShutdown();
+    }
+}
