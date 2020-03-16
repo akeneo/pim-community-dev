8189c58f881f3bd2cdec39dffa987411644dff90
romain <romain@akeneo.com>
2020-03-16 11:38:21 +0100

    API-1023: Adds migration test for audit product table creation

--- upgrades/schema/Version_5_0_20200313130000_create_audit_product_table.php
+++ upgrades/schema/Version_5_0_20200313130000_create_audit_product_table.php
@@ -16,7 +16,7 @@ final class Version_5_0_20200313130000_create_audit_product_table extends Abstra
 {
     public function up(Schema $schema): void
     {
-        $sql = <<<SQL
+        $createTableAuditProductSql = <<<SQL
 CREATE TABLE IF NOT EXISTS akeneo_connectivity_connection_audit_product(
     connection_code VARCHAR(100) NOT NULL,
     event_datetime DATETIME NOT NULL,
@@ -26,8 +26,8 @@ CREATE TABLE IF NOT EXISTS akeneo_connectivity_connection_audit_product(
     PRIMARY KEY (event_datetime, connection_code, event_type)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
 SQL;
+        $this->addSql($createTableAuditProductSql);
 
-        $this->addSql($sql);
         $this->addSql('DROP TABLE IF EXISTS akeneo_connectivity_connection_audit');
     }
 
