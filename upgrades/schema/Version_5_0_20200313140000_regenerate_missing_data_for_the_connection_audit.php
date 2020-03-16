32d31c011bd84c652426c08a417d1450056c1559
romain <romain@akeneo.com>
2020-03-16 16:21:30 +0100

    API-1023: Rework migration script to regenerate missing data

--- upgrades/schema/Version_5_0_20200313140000_regenerate_missing_data_for_the_connection_audit.php
+++ upgrades/schema/Version_5_0_20200313140000_regenerate_missing_data_for_the_connection_audit.php
@@ -5,62 +5,139 @@ declare(strict_types=1);
 namespace Pim\Upgrade\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class Version_5_0_20200313140000_regenerate_missing_data_for_the_connection_audit extends AbstractMigration
+final class Version_5_0_20200313140000_regenerate_missing_data_for_the_connection_audit extends AbstractMigration implements ContainerAwareInterface
 {
+    /** @var ContainerInterface */
+    private $container;
+
     public function up(Schema $schema): void
     {
-        $auditDataExists = $this->connection->executeQuery('SELECT COUNT(*) FROM akeneo_connectivity_connection_audit_product')
-            ->fetchColumn();
+        var_dump('oiudis');
+        $auditDataExists = $this->connection->executeQuery('SELECT COUNT(1) FROM akeneo_connectivity_connection_audit_product')->fetchColumn();
         if ($auditDataExists > 0) {
+            var_dump('Already data in audit_product table... No migration needed!');
             return;
         }
+        var_dump('No data in audit product table. Starting recalculation...');
 
+        $datetimeUTC = new \DateTime('now', new \DateTimeZone('UTC'));
+        $datetimeUTC->setTime((int) $datetimeUTC->format('H'), 0);
+
+        $hourInterval = new \DateInterval('PT1H');
+
+        for ($i = 24*8; $i > 0; $i--) {
+            $this->recalculateForDateTime($datetimeUTC->sub($hourInterval));
+        }
+    }
+
+    private function recalculateForDateTime(\DateTime $startDateTime): void
+    {
+        $endDateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $startDateTime->format('Y-m-d H:i:s'), new \DateTimeZone('UTC'));
+        $endDateTime->setTime((int) $endDateTime->format('H'), 59, 59);
+
+        // Recalculate created
+        $this->recalculateCreatedForDateTime($startDateTime, $endDateTime);
+        // Recalculate updated
+        $this->recalculateUpdatedForDateTime($startDateTime, $endDateTime);
+    }
+
+    private function recalculateCreatedForDateTime(\DateTime $startDateTime, \DateTime $endDateTime): void
+    {
         $selectEventCountByTime = <<<SQL
-SELECT conn.code, versioning.version != 1 as is_updated, count(versioning.version) as total
-FROM pim_versioning_version versioning USE INDEX(logged_at_idx)
-INNER JOIN oro_user u ON u.username = versioning.author AND u.user_type = 'api'
+SELECT conn.code AS connection_code, COUNT(resource_id) as event_count
+FROM (
+    SELECT author, resource_id
+    FROM pim_versioning_version USE INDEX(logged_at_idx)
+    WHERE logged_at >= :start_time AND logged_at < :end_time
+    AND resource_name = :resource_name
+    AND version = 1
+    GROUP BY author, resource_id
+) AS tmp_table
+INNER JOIN oro_user u ON u.username = author AND u.user_type = 'api'
 INNER JOIN akeneo_connectivity_connection conn ON conn.user_id = u.id
-WHERE resource_name = 'Akeneo\\Pim\\Enrichment\\Component\\Product\\Model\\Product'
-AND logged_at >= :start_time AND logged_at < :end_time
-GROUP BY conn.code, is_updated
+GROUP BY conn.code;
 SQL;
 
-        $now = new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
-        $now = $now->setTime((int) $now->format('H'), 0);
-        $tenDaysAgo = $now->modify('- 10 days');
+        $dateTimeParams = [
+            'start_time' => $startDateTime->format('Y-m-d H:i:s'),
+            'end_time'   => $endDateTime->format('Y-m-d H:i:s'),
+            'resource_name' => $this->container->getParameter('pim_catalog.entity.product.class'),
+        ];
+        $eventCounts = $this->connection->executeQuery($selectEventCountByTime, $dateTimeParams)->fetchAll();
 
-        $period = new \DatePeriod(
-            $tenDaysAgo,
-            new \DateInterval('PT1H'),
-            $now->modify('+ 3 hour')
-        );
-
-        foreach ($period as $endDateTime) {
-            $result = $this->connection->executeQuery(
-                $selectEventCountByTime,
-                [
-                    'start_time' => $endDateTime->sub(new \DateInterval('PT1H')),
-                    'end_time' => $endDateTime
-                ],
-                [
-                    'start_time' => Types::DATETIME_IMMUTABLE,
-                    'end_time' => Types::DATETIME_IMMUTABLE,
-                ]
-            )->fetchAll();
-
-            dump($result);
+        $totalCount = 0;
+        foreach ($eventCounts as $eventCount) {
+            $this->insertAuditProductRow($eventCount['connection_code'], $eventCount['event_count'], $startDateTime->format('Y-m-d H:i:s'), 'product_created');
+            $totalCount += $eventCount['event_count'];
         }
+        $this->insertAuditProductRow('<all>', $totalCount, $startDateTime->format('Y-m-d H:i:s'), 'product_created');
+    }
+
+    private function recalculateUpdatedForDateTime(\DateTime $startDateTime, \DateTime $endDateTime): void
+    {
+        $selectEventCountByTime = <<<SQL
+SELECT conn.code AS connection_code, COUNT(resource_id) as event_count
+FROM (
+    SELECT author, resource_id
+    FROM pim_versioning_version USE INDEX(logged_at_idx)
+    WHERE logged_at >= :start_time AND logged_at < :end_time
+    AND resource_name = :resource_name
+    AND version != 1
+    GROUP BY author, resource_id
+) AS tmp_table
+INNER JOIN oro_user u ON u.username = author AND u.user_type = 'api'
+INNER JOIN akeneo_connectivity_connection conn ON conn.user_id = u.id
+GROUP BY conn.code;
+SQL;
+
+        $dateTimeParams = [
+            'start_time' => $startDateTime->format('Y-m-d H:i:s'),
+            'end_time'   => $endDateTime->format('Y-m-d H:i:s'),
+            'resource_name' => $this->container->getParameter('pim_catalog.entity.product.class'),
+        ];
+        $eventCounts = $this->connection->executeQuery($selectEventCountByTime, $dateTimeParams)->fetchAll();
+
+        $totalCount = 0;
+        foreach ($eventCounts as $eventCount) {
+            $this->insertAuditProductRow($eventCount['connection_code'], $eventCount['event_count'], $startDateTime->format('Y-m-d H:i:s'), 'product_updated');
+            $totalCount += $eventCount['event_count'];
+        }
+        $this->insertAuditProductRow('<all>', $totalCount, $startDateTime->format('Y-m-d H:i:s'), 'product_updated');
+    }
+
+    private function insertAuditProductRow(string $connectionCode, int $eventCount, string $eventDateTime, string $eventType): void
+    {
+        $insertQuerySql = <<<SQL
+INSERT INTO akeneo_connectivity_connection_audit_product (connection_code, event_datetime, event_count, event_type, updated)
+VALUES(:connection_code, :event_datetime, :event_count, :event_type, UTC_TIMESTAMP())
+SQL;
+        $insertQueryParams = [
+            'connection_code' => $connectionCode,
+            'event_datetime'  => $eventDateTime,
+            'event_count'     => $eventCount,
+            'event_type'      => $eventType
+        ];
+        $this->connection->executeQuery($insertQuerySql, $insertQueryParams);
     }
 
     public function down(Schema $schema): void
     {
         $this->throwIrreversibleMigrationException();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }
