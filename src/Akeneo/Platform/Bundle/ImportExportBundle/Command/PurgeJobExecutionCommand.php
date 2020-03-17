4ce766d7fc30bcfad8a84103a3ff97b8260a7418
Olivier Pontier <olivier.pontier@akeneo.com>
2020-03-17 18:36:47 +0100

    Fix the dependency issue with the Symfony ConfirmationQuestion component

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Command/PurgeJobExecutionCommand.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Command/PurgeJobExecutionCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * Purge Jobs Execution history
