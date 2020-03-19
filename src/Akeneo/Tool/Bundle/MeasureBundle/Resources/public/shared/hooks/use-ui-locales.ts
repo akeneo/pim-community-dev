d66b689eeeeab7fa3d5e76d44a7c542acc4289ce
Julien Sanchez <sumbobyboys@gmail.com>
2020-03-19 15:21:28 +0100

    MET-83: add cache on UIlocales

--- src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/shared/hooks/use-ui-locales.ts
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/shared/hooks/use-ui-locales.ts
@@ -3,11 +3,20 @@ import {RouterContext} from 'akeneomeasure/context/router-context';
 import {Locale} from 'akeneomeasure/model/locale';
 import {baseFetcher} from 'akeneomeasure/shared/fetcher/base-fetcher';
 
+let uiLocalesPromise: Promise<Locale[]> | null = null;
+const fetchUiLocales = async (route: string): Promise<Locale[]> => {
+  if (null === uiLocalesPromise) {
+    uiLocalesPromise = (await baseFetcher(route)) as Promise<Locale[]>;
+  }
+
+  return uiLocalesPromise;
+};
+
 const useUiLocales = (): Locale[] | null => {
   const [locales, setLocales] = useState<Locale[] | null>(null);
   const route = useContext(RouterContext).generate('pim_localization_locale_index');
 
-  const fetchLocales = useCallback(async () => setLocales(await baseFetcher(route)), [route, setLocales]);
+  const fetchLocales = useCallback(async () => setLocales(await fetchUiLocales(route)), [route, setLocales]);
 
   useEffect(() => {
     (async () => fetchLocales())();
