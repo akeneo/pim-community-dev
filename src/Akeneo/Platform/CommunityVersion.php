f6907b6b6e43e918a1a0e82c1c818748ea4fa389
jmleroux <jmleroux.pro@gmail.com>
2020-03-20 13:24:57 +0000

    Preparing version 3.2.45

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.44';
+    const VERSION = '3.2.45';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
