657ebe1d9a5ce820735fa2d3581868f3adc56c87
Olivier Pontier <olivier.pontier@akeneo.com>
2020-03-20 17:06:11 +0100

    DAPI-931: trigger events before handling navigation in product model breadcrumb

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/variant-navigation.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/variant-navigation.js
@@ -462,7 +462,7 @@ define(
                     : 'pim_enrich_product_edit'
                 ;
 
-                this.getRoot().trigger('pim:product:variant-navigation:navigate-to-level:before');
+                this.getRoot().trigger('pim:product:variant-navigation:navigate-to-level:before', entity);
                 router.redirectToRoute(route, params);
             }
         });
