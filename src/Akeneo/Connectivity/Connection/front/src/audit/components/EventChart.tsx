b9570676a3d8d01339fe510fe74f1bfbb80f966a
Paul Chasle <paul.chasle@akeneo.com>
2020-03-24 16:18:32 +0100

    API-1036: Display weekly total on Audit charts

--- src/Akeneo/Connectivity/Connection/front/src/audit/components/EventChart.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/audit/components/EventChart.tsx
@@ -1,13 +1,13 @@
 import React, {FC, ReactNode, useEffect, useState} from 'react';
+import styled from 'styled-components';
 import {VictoryChartProps, VictoryThemeDefinition} from 'victory';
+import {PropsWithTheme} from '../../common/theme';
 import {AuditEventType} from '../../model/audit-event-type.enum';
 import {useDateFormatter} from '../../shared/formatter/use-date-formatter';
 import {useNumberFormatter} from '../../shared/formatter/use-number-formatter';
-import {useTranslate} from '../../shared/translate';
+import {Translate, useTranslate} from '../../shared/translate';
 import {useFetchConnectionsAuditData} from '../api-hooks/use-fetch-connections-audit-data';
 import {Chart} from './Chart';
-import styled from 'styled-components';
-import {PropsWithTheme} from '../../common/theme';
 
 type Props = {
     title: ReactNode;
@@ -24,15 +24,6 @@ type ChartEntry = {
     yLabel: string;
 };
 
-const Title = styled.div`
-    color: ${({theme}: PropsWithTheme) => theme.color.purple100};
-    font-size: ${({theme}: PropsWithTheme) => theme.fontSize.bigger};
-    line-height: 44px;
-    text-transform: uppercase;
-    font-weight: bold;
-    display: block;
-`;
-
 export const EventChart: FC<Props> = ({
     title,
     eventType,
@@ -46,6 +37,7 @@ export const EventChart: FC<Props> = ({
     const formatDate = useDateFormatter();
     const formatNumber = useNumberFormatter();
     const translate = useTranslate();
+
     useEffect(() => {
         setChartData(undefined);
         if (undefined === selectedConnectionCode || undefined === connectionsAuditData[selectedConnectionCode]) {
@@ -69,14 +61,44 @@ export const EventChart: FC<Props> = ({
         setChartData(chartData);
     }, [formatDate, translate, connectionsAuditData, selectedConnectionCode]);
 
+    const total = (selectedConnectionCode && connectionsAuditData[selectedConnectionCode]?.weekly_total) || 0;
+
     return (
         <>
             {chartData && (
                 <>
                     <Title>{title}</Title>
+                    <SubTitle>
+                        <Translate id='akeneo_connectivity.connection.dashboard.charts.legend.during_the_last_seven_days' />
+                        &nbsp;
+                        <Count>{formatNumber(total)}</Count>
+                    </SubTitle>
                     <Chart chartOptions={chartOptions} data={chartData} theme={theme} />
                 </>
             )}
         </>
     );
 };
+
+const Title = styled.div`
+    color: ${({theme}: PropsWithTheme) => theme.color.purple100};
+    display: block;
+    font-size: ${({theme}: PropsWithTheme) => theme.fontSize.bigger};
+    font-weight: bold;
+    line-height: 21px;
+    text-transform: uppercase;
+    padding-top: 20px;
+    padding-bottom: 5px;
+`;
+
+const SubTitle = styled.div`
+    color: ${({theme}: PropsWithTheme) => theme.color.grey140};
+    font-size: ${({theme}: PropsWithTheme) => theme.fontSize.bigger};
+    font-weight: bold;
+    line-height: 21px;
+    padding-bottom: 20px;
+`;
+
+const Count = styled.span`
+    color: ${({theme}: PropsWithTheme) => theme.color.purple100};
+`;
