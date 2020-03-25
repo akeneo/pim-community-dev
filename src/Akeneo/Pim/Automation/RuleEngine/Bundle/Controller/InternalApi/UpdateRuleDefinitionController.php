3822abd1f52f0984c777ffcf94110dd877d1543c
pierallard <pierallard@gmail.com>
2020-03-25 11:05:48 +0100

    RUL-93: Add routing for create/update rule definitions

---
+++ src/Akeneo/Pim/Automation/RuleEngine/Bundle/Controller/InternalApi/UpdateRuleDefinitionController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\RuleEngine\Bundle\Controller\InternalApi;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class UpdateRuleDefinitionController
+{
+    public function __invoke(string $ruleCode, Request $request)
+    {
+
+    }
+}
