75ec6bbf393174e3854c653201e0f086396272bd
Mathias Métayer <mathias.metayer@akeneo.com>
2020-03-25 15:44:30 +0100

    RUL-106: Add integration tests

---
+++ src/Akeneo/Pim/Automation/RuleEngine/tests/back/Integration/Context/AssociationContext.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Test\Pim\Automation\RuleEngine\Integration\Context;
+
+use Akeneo\Pim\Enrichment\Bundle\Doctrine\Common\Saver\ProductSaver;
+use Akeneo\Pim\Enrichment\Component\Product\Model\GroupInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Model\ProductModelInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductRepositoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Webmozart\Assert\Assert;
+
+class AssociationContext implements Context
+{
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
+    /** @var ObjectUpdaterInterface */
+    private $productUpdater;
+
+    /** @var ProductSaver */
+    private $productSaver;
+
+    public function __construct(
+        IdentifiableObjectRepositoryInterface $productRepository,
+        ObjectUpdaterInterface $productUpdater,
+        ProductSaver $productSaver
+    ) {
+        $this->productRepository = $productRepository;
+        $this->productUpdater = $productUpdater;
+        $this->productSaver = $productSaver;
+    }
+
+    /**
+     * @Given the following associations for product :identifier:
+     */
+    public function theFollowingAssociationsForProduct(string $identifier, TableNode $table)
+    {
+        $product = $this->productRepository->findOneByIdentifier($identifier);
+
+        $associations = [];
+        foreach ($table->getHash() as $row) {
+            $associationType = $row['association_type'];
+            $associations = [$associationType => []];
+            foreach (['products', 'product_models', 'groups'] as $key) {
+                if ('' !== $row[$key]) {
+                    $associations[$associationType][$key] = $this->listToArray($row[$key]);
+                }
+            }
+        }
+        $this->productUpdater->update($product, ['associations' => $associations]);
+        $this->productSaver->save($product);
+    }
+
+    /**
+     * @Then the :identifier product should have the following associations:
+     */
+    public function theProductShouldHaveTheFollowingAssociations(string $identifier, TableNode $table)
+    {
+        $product = $this->productRepository->findOneByIdentifier($identifier);
+        foreach ($table->getHash() as $row) {
+            $association = $product->getAssociationForTypeCode($row['association_type']);
+
+            $associatedProductIdentifiers = $association->getProducts()->map(
+                function (ProductInterface $product): string {
+                    return $product->getIdentifier();
+                }
+            )->toArray();
+            sort($associatedProductIdentifiers);
+            $expectedAssociatedProducts = $this->listToArray($row['products']);
+            sort($expectedAssociatedProducts);
+            Assert::eq(
+                $associatedProductIdentifiers,
+                $expectedAssociatedProducts,
+                sprintf(
+                    'Expected associated products to be "%s", "%s" found',
+                    $row['products'],
+                    implode(',', $associatedProductIdentifiers)
+                )
+            );
+
+            $associatedProductModelCodes = $association->getProductModels()->map(
+                function (ProductModelInterface $productModel): string {
+                    return $productModel->getCode();
+                }
+            )->toArray();
+            sort($associatedProductModelCodes);
+            $expectedAssociatedProductModels = $this->listToArray($row['product_models']);
+            sort($expectedAssociatedProductModels);
+            Assert::eq(
+                $associatedProductModelCodes,
+                $expectedAssociatedProductModels,
+                sprintf(
+                    'Expected associated product models to be "%s", "%s" found',
+                    $row['product_models'],
+                    implode(',', $associatedProductModelCodes)
+                )
+            );
+
+            $associatedGroupCodes = $association->getGroups()->map(
+                function (GroupInterface $group): string {
+                    return $group->getCode();
+                }
+            )->toArray();
+            sort($associatedGroupCodes);
+            $expectedAssociatedGroups = $this->listToArray($row['groups']);
+            sort($expectedAssociatedGroups);
+            Assert::eq(
+                $associatedGroupCodes,
+                $expectedAssociatedGroups,
+                sprintf(
+                    'Expected associated groups to be "%s", "%s" found',
+                    $row['groups'],
+                    implode(',', $associatedGroupCodes)
+                )
+            );
+        }
+    }
+
+    private function listToArray(string $list): array
+    {
+        return '' === trim($list) ? [] : array_map('trim', explode(',', $list));
+    }
+}
