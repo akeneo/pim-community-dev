e315b4afe932faa31ba6e24612d2d8f9465d2a6c
Tseho <quentin.favrie@gmail.com>
2020-03-27 14:20:24 +0100

    MET-136: Display global units errors in the Units tab

--- src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/edit/unit-tab/UnitTab.tsx
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/edit/unit-tab/UnitTab.tsx
@@ -11,6 +11,23 @@ import {ValidationError, filterErrors} from 'akeneomeasure/model/validation-erro
 import {UnitCode} from 'akeneomeasure/model/unit';
 import {UnitDetails} from 'akeneomeasure/pages/edit/unit-tab/UnitDetails';
 import {UnitRow} from 'akeneomeasure/pages/edit/unit-tab/UnitRow';
+import {ErrorFlashMessage} from 'akeneomeasure/shared/components/ErrorFlashMessage';
+
+const TabContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100%);
+  width: 100%;
+`;
+
+const TabColumns = styled.div`
+  display: flex;
+  overflow: hidden;
+`;
+
+const Errors = styled.div`
+  margin-bottom: 20px;
+`;
 
 const UnitList = styled.div`
   flex: 1;
@@ -38,47 +55,58 @@ const UnitTab = ({
 
   const filteredUnits = measurementFamily.units.filter(filterOnLabelOrCode(searchValue, locale));
 
+  const tabErrors = errors.filter((error: ValidationError) => error.propertyPath === '');
+
   return (
-    <>
-      <UnitList>
-        <SearchBar count={measurementFamily.units.length} searchValue={searchValue} onSearchChange={setSearchValue} />
-        {0 === filteredUnits.length && (
-          <NoDataSection>
-            <MeasurementFamilyIllustration size={256} />
-            <NoDataTitle>{__('measurements.family.no_result.title')}</NoDataTitle>
-          </NoDataSection>
-        )}
-        {0 < filteredUnits.length && (
-          <Table>
-            <thead>
-              <tr>
-                <HeaderCell title={__('pim_common.label')}>{__('pim_common.label')}</HeaderCell>
-                <HeaderCell title={__('pim_common.code')}>{__('pim_common.code')}</HeaderCell>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredUnits.map((unit, index) => (
-                <UnitRow
-                  key={unit.code}
-                  unit={unit}
-                  isStandardUnit={unit.code === measurementFamily.standard_unit_code}
-                  isSelected={unit.code === selectedUnitCode}
-                  isInvalid={0 < filterErrors(errors, `[${index}]`).length}
-                  onRowSelected={selectUnitCode}
-                />
-              ))}
-            </tbody>
-          </Table>
-        )}
-      </UnitList>
-      <UnitDetails
-        measurementFamily={measurementFamily}
-        selectedUnitCode={selectedUnitCode}
-        onMeasurementFamilyChange={onMeasurementFamilyChange}
-        selectUnitCode={selectUnitCode}
-        errors={filterErrors(errors, `[${getUnitIndex(measurementFamily, selectedUnitCode)}]`)}
-      />
-    </>
+    <TabContainer>
+      {0 < tabErrors.length && (
+        <Errors>
+          {tabErrors.map((error: ValidationError) => (
+            <ErrorFlashMessage>{error.message}</ErrorFlashMessage>
+          ))}
+        </Errors>
+      )}
+      <TabColumns>
+        <UnitList>
+          <SearchBar count={measurementFamily.units.length} searchValue={searchValue} onSearchChange={setSearchValue} />
+          {0 === filteredUnits.length && (
+            <NoDataSection>
+              <MeasurementFamilyIllustration size={256} />
+              <NoDataTitle>{__('measurements.family.no_result.title')}</NoDataTitle>
+            </NoDataSection>
+          )}
+          {0 < filteredUnits.length && (
+            <Table>
+              <thead>
+                <tr>
+                  <HeaderCell title={__('pim_common.label')}>{__('pim_common.label')}</HeaderCell>
+                  <HeaderCell title={__('pim_common.code')}>{__('pim_common.code')}</HeaderCell>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUnits.map((unit, index) => (
+                  <UnitRow
+                    key={unit.code}
+                    unit={unit}
+                    isStandardUnit={unit.code === measurementFamily.standard_unit_code}
+                    isSelected={unit.code === selectedUnitCode}
+                    isInvalid={0 < filterErrors(errors, `[${index}]`).length}
+                    onRowSelected={selectUnitCode}
+                  />
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </UnitList>
+        <UnitDetails
+          measurementFamily={measurementFamily}
+          selectedUnitCode={selectedUnitCode}
+          onMeasurementFamilyChange={onMeasurementFamilyChange}
+          selectUnitCode={selectUnitCode}
+          errors={filterErrors(errors, `[${getUnitIndex(measurementFamily, selectedUnitCode)}]`)}
+        />
+      </TabColumns>
+    </TabContainer>
   );
 };
 
