fd66403539efabae39dd0ca1f7b39efdd29dcc59
Nicolas Maligne <nicolas.maligne@akeneo.com>
2020-03-27 15:11:56 +0100

    DSM: Add primary button border color

--- akeneo-design-system/src/components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ akeneo-design-system/src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -5,17 +5,21 @@ import { CoreButton } from '../CoreButton'
 
 const PrimaryButton = styled(CoreButton)`
     color: white;
+    border-color: #67B373;
     background-color: #67B373;
     &:hover {
+        border-color: #528f5c;
         background-color: #528f5c;
     }
     &:active {
+        border-color: #3d6b45;
         background-color: #3d6b45;
     }
     &:focus {
         border-color: blue;
     }
     &:disabled {
+        border-color: #c2e1c7;
         background-color: #c2e1c7;
     }
 `
