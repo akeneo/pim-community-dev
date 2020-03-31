5311b5d28c0d99f0cd53e0ebcf71b01b761066ef
Tseho <quentin.favrie@gmail.com>
2020-03-31 15:24:34 +0200

    MET-137: Fix tests

--- src/Akeneo/Tool/Bundle/MeasureBundle/spec/Controller/ExternalApi/JsonSchema/MeasurementFamilyValidatorSpec.php
+++ src/Akeneo/Tool/Bundle/MeasureBundle/spec/Controller/ExternalApi/JsonSchema/MeasurementFamilyValidatorSpec.php
@@ -27,7 +27,7 @@ class MeasurementFamilyValidatorSpec extends ObjectBehavior
     {
         $asset = [
             'values' => null,
-            'foo'    => 'bar',
+            'foo' => 'bar',
         ];
 
         $errors = $this->validate($asset);
@@ -35,21 +35,21 @@ class MeasurementFamilyValidatorSpec extends ObjectBehavior
         $errors->shouldHaveCount(6);
     }
 
-    function it_returns_an_empty_array_if_all_the_asset_properties_are_valid()
+    function it_returns_an_empty_array_if_all_the_measurement_family_properties_are_valid()
     {
         $measurementFamily = [
-            'code'               => 'custom_metric_1',
-            'labels'             =>
+            'code' => 'custom_metric_1',
+            'labels' =>
                 [
                     'en_US' => 'Custom measurement 1',
                     'fr_FR' => 'Mesure personalisée 1',
                 ],
             'standard_unit_code' => 'CUSTOM_UNIT_1_1',
-            'units'              =>
+            'units' =>
                 [
-                    [
-                        'code'                  => 'CUSTOM_UNIT_1_1',
-                        'labels'                =>
+                    'CUSTOM_UNIT_1_1' => [
+                        'code' => 'CUSTOM_UNIT_1_1',
+                        'labels' =>
                             [
                                 'en_US' => 'Custom unit 1_1',
                                 'fr_FR' => 'Unité personalisée 1_1',
@@ -58,14 +58,14 @@ class MeasurementFamilyValidatorSpec extends ObjectBehavior
                             [
                                 [
                                     'operator' => 'mul',
-                                    'value'    => '0.000001',
+                                    'value' => '0.000001',
                                 ],
                             ],
-                        'symbol'                => 'mm²',
+                        'symbol' => 'mm²',
                     ],
                     [
-                        'code'                  => 'CUSTOM_UNIT_2_1',
-                        'labels'                =>
+                        'code' => 'CUSTOM_UNIT_2_1',
+                        'labels' =>
                             [
                                 'en_US' => 'Custom unit 2_1',
                                 'fr_FR' => 'Unité personalisée 2_1',
@@ -74,10 +74,10 @@ class MeasurementFamilyValidatorSpec extends ObjectBehavior
                             [
                                 [
                                     'operator' => 'mul',
-                                    'value'    => '0.0001',
+                                    'value' => '0.0001',
                                 ],
                             ],
-                        'symbol'                => 'cm²',
+                        'symbol' => 'cm²',
                     ],
                 ],
         ];
