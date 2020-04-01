e10d203597bcbba41595a58c8bcb7bcbc33a724d
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-04-01 21:01:09 +0200

    RUL-89: add clearers for associations, categories and groups (#11837)
    RUL-89: add clearers for associations, categories and groups

--- tests/back/Pim/Enrichment/Integration/Updater/PropertyClearerIntegration.php
+++ tests/back/Pim/Enrichment/Integration/Updater/PropertyClearerIntegration.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace AkeneoTest\Pim\Enrichment\Integration\Updater;
 
+use Akeneo\Pim\Enrichment\Bundle\Doctrine\Common\Saver\ProductSaver;
+use Akeneo\Pim\Enrichment\Component\Product\Model\EntityWithAssociationsInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductRepositoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Updater\PropertyClearer;
 use Akeneo\Test\Integration\TestCase;
+use Akeneo\Tool\Component\StorageUtils\Cache\EntityManagerClearerInterface;
 
 /**
  * @author    Nicolas Marniesse <nicolas.marniesse@akeneo.com>
@@ -18,10 +22,22 @@ class PropertyClearerIntegration extends TestCase
     /** @var PropertyClearer */
     private $propertyClearer;
 
+    /** @var ProductSaver */
+    private $productSaver;
+
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
+    /** @var EntityManagerClearerInterface */
+    private $cacheClearer;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->propertyClearer = $this->get('pim_catalog.updater.property_clearer');
+        $this->productSaver = $this->get('pim_catalog.saver.product');
+        $this->productRepository = $this->get('pim_catalog.repository.product');
+        $this->cacheClearer = $this->get('pim_connector.doctrine.cache_clearer');
     }
 
     public function test_it_clears_attribute_values(): void
@@ -78,6 +94,70 @@ class PropertyClearerIntegration extends TestCase
         $this->assertNotNull($product->getValue('a_localized_and_scopable_text_area', 'en_US', 'ecommerce'));
     }
 
+    public function test_it_clears_associations(): void
+    {
+        $this->createProduct('a_product', []);
+        $this->createProduct('another_product', []);
+
+        $parameters = [
+            'associations' => [
+                'X_SELL' => [
+                    'products' => ['a_product', 'another_product'],
+                    'product_models' => [],
+                    'groups' => [],
+                ],
+            ],
+        ];
+        $product = $this->createProduct('a_product_with_association', $parameters);
+
+        $this->assertGreaterThan(0, $this->getAssociationsCount($product));
+
+        $this->propertyClearer->clear($product, 'associations');
+        $this->assertSame(0, $this->getAssociationsCount($product));
+
+        // Save the product, clear the doctrine cache, reload the product and test the change is still good.
+        $this->productSaver->save($product);
+        $this->cacheClearer->clear();
+        $product = $this->productRepository->findOneByIdentifier('a_product_with_association');
+        $this->assertSame(0, $this->getAssociationsCount($product));
+    }
+
+    public function test_it_clears_categories(): void
+    {
+        $parameters = [
+            'categories' => ['categoryA', 'categoryB'],
+        ];
+        $product = $this->createProduct('a_product_with_categories', $parameters);
+        $this->assertGreaterThan(0, $product->getCategories()->count());
+
+        $this->propertyClearer->clear($product, 'categories');
+        $this->assertCount(0, $product->getCategories());
+
+        // Save the product, clear the doctrine cache, reload the product and test the change is still good.
+        $this->productSaver->save($product);
+        $this->cacheClearer->clear();
+        $product = $this->productRepository->findOneByIdentifier('a_product_with_categories');
+        $this->assertCount(0, $product->getCategories());
+    }
+
+    public function test_it_clears_groups(): void
+    {
+        $parameters = [
+            'groups' => ['groupA', 'groupB'],
+        ];
+        $product = $this->createProduct('a_product_with_groups', $parameters);
+        $this->assertGreaterThan(0, $product->getGroups()->count());
+
+        $this->propertyClearer->clear($product, 'groups');
+        $this->assertCount(0, $product->getGroups());
+
+        // Save the product, clear the doctrine cache, reload the product and test the change is still good.
+        $this->productSaver->save($product);
+        $this->cacheClearer->clear();
+        $product = $this->productRepository->findOneByIdentifier('a_product_with_groups');
+        $this->assertCount(0, $product->getGroups());
+    }
+
     public function getConfiguration()
     {
         return $this->catalog->useTechnicalCatalog();
@@ -101,5 +181,17 @@ class PropertyClearerIntegration extends TestCase
         $this->get('pim_catalog.validator.unique_value_set')->reset();
 
         return $product;
+    }
+
+    private function getAssociationsCount(EntityWithAssociationsInterface $entity): int
+    {
+        $count = 0;
+        foreach ($entity->getAssociations() as $association) {
+            $count += $association->getProducts()->count();
+            $count += $association->getProductModels()->count();
+            $count += $association->getGroups()->count();
+        };
+
+        return $count;
     }
 }
