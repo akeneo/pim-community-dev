8d809a629f862bd772817882b5f5bb12aea6a498
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2020-04-07 17:01:21 +0200

    TIP-1403: configuration for feature flags

--- src/Akeneo/Platform/Bundle/FeatureFlagBundle/DependencyInjection/AkeneoFeatureFlagExtension.php
+++ src/Akeneo/Platform/Bundle/FeatureFlagBundle/DependencyInjection/AkeneoFeatureFlagExtension.php
@@ -19,6 +19,9 @@ class AkeneoFeatureFlagExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+        var_dump($config);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
