5f82d96992b49b0b51f6ef9d3065422f5f1c66ed
pierallard <pierallard@gmail.com>
2020-04-09 15:44:23 +0200

    RUL-164: Add get in fetch

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/RuleDefinitionFetcher.ts
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/RuleDefinitionFetcher.ts
@@ -1,21 +1,11 @@
 import {denormalize} from "../models/rule-definition-denormalizer";
 import {Router} from "../dependenciesTools/provider/applicationDependenciesProvider.type";
 import {RuleDefinition} from "../models/RuleDefinition";
-
-function get(url: string) {
-  return fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-    },
-    credentials: 'include',
-    method: 'GET'
-  });
-}
+import {httpGet} from "./fetch";
 
 export const getByCode = async (ruleDefinitionCode: string, router: Router): Promise<RuleDefinition> => {
   const url = router.generate('pimee_enrich_rule_definition_get', { ruleCode: ruleDefinitionCode });
-  const response = await get(url);
+  const response = await httpGet(url);
   const json = await response.json();
 
   return denormalize(json);
--- src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/fetch.ts
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/fetch.ts
@@ -11,4 +11,17 @@ const httpPost = (url: string, params?: any) => {
   });
 };
 
-export { httpPost };
+const httpGet = (url: string, params?: any) => {
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+      ...params.header
+    },
+    credentials: "include",
+    method: "GET",
+    body: JSON.stringify(params.body)
+  });
+};
+
+export { httpPost, httpGet };
