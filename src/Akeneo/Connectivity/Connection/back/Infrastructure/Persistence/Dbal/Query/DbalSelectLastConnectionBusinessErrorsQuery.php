756d8bccf9e2321a8a6057e9a73dab4570982d79
Paul Chasle <paul.chasle@akeneo.com>
2020-04-14 21:44:46 +0200

    API-1042: GET Connection business errors endpoint

---
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Persistence/Dbal/Query/DbalSelectLastConnectionBusinessErrorsQuery.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Infrastructure\Persistence\Dbal\Query;
+
+use Akeneo\Connectivity\Connection\Domain\ErrorManagement\Model\Read\BusinessError;
+use Akeneo\Connectivity\Connection\Domain\ErrorManagement\Persistence\Query\SelectLastConnectionBusinessErrorsQuery;
+use Doctrine\DBAL\Connection as DbalConnection;
+use Doctrine\DBAL\FetchMode;
+use Doctrine\DBAL\Types\Types;
+
+/**
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class DbalSelectLastConnectionBusinessErrorsQuery implements SelectLastConnectionBusinessErrorsQuery
+{
+    /** @var DbalConnection */
+    private $dbalConnection;
+
+    public function __construct(DbalConnection $dbalConnection)
+    {
+        $this->dbalConnection = $dbalConnection;
+    }
+
+    public function execute(string $connectionCode, string $endDate = null, int $limit = 100): array
+    {
+        $to = new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
+        if (null !== $endDate) {
+            $to = \DateTimeImmutable::createFromFormat('Y-m-d', $endDate, new \DateTimeZone('UTC'));
+        }
+
+        $to = $to->setTime(0, 0)->add(new \DateInterval('P1D'));
+        $from = $to->sub(new \DateInterval('P7D'));
+
+        $sql = <<<SQL
+SELECT connection_code, error_datetime, content FROM akeneo_connectivity_connection_audit_business_error
+WHERE connection_code = :connection_code AND error_datetime BETWEEN :from AND :to
+ORDER BY error_datetime DESC
+LIMIT :limit
+SQL;
+        $result = $this->dbalConnection->executeQuery($sql, [
+            'connection_code' => $connectionCode,
+            'from' => $from,
+            'to' => $to,
+            'limit' => $limit
+        ], [
+            'from' => Types::DATETIME_IMMUTABLE,
+            'to' => Types::DATETIME_IMMUTABLE,
+            'limit' => Types::INTEGER
+        ])->fetchAll(FetchMode::ASSOCIATIVE);
+
+        $format = $this->dbalConnection->getDatabasePlatform()->getDateTimeFormatString();
+
+        return array_map(function (array $row) use ($format) {
+            return new BusinessError(
+                $row['connection_code'],
+                \DateTimeImmutable::createFromFormat($format, $row['error_datetime'], new \DateTimeZone('UTC')),
+                $row['content']
+            );
+        }, $result);
+    }
+}
---
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/ErrorManagement/GetConnectionBusinessErrorsEndToEnd.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\back\tests\EndToEnd\ErrorManagement;
+
+use Akeneo\Connectivity\Connection\back\tests\EndToEnd\WebTestCase;
+use Akeneo\Test\Integration\Configuration;
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class GetConnectionBusinessErrorsEndToEnd extends WebTestCase
+{
+    /** @var Connection */
+    private $connection;
+
+    public function test_it_gets_a_connection_business_errors(): void
+    {
+        $errors = [
+            ['erp', '2020-01-01 00:00:00', '{"message": "Error 1"}'],
+            ['erp', '2020-01-07 00:00:00', '{"message": "Error 2"}'],
+        ];
+        $this->insertBusinessErrors($errors);
+
+        $expectedResult = [
+            [
+                'connection_code' => 'erp',
+                'date_time' => '2020-01-07T00:00:00+00:00',
+                'content' => ['message' => 'Error 2']
+            ],
+            [
+                'connection_code' => 'erp',
+                'date_time' => '2020-01-01T00:00:00+00:00',
+                'content' => ['message' => 'Error 1']
+            ],
+        ];
+
+        $this->authenticateAsAdmin();
+        $this->client->request('GET', '/rest/connections/erp/business-errors', [
+            'end_date' => '2020-01-07',
+        ]);
+        $result = json_decode($this->client->getResponse()->getContent(), true);
+
+        Assert::assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        Assert::assertEquals($expectedResult, $result);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->connection = $this->get('database_connection');
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+
+    private function insertBusinessErrors(array $errors): void
+    {
+        foreach ($errors as [$connectionCode, $dateTime, $content]) {
+            $this->connection->insert('akeneo_connectivity_connection_audit_business_error', [
+                'connection_code' => $connectionCode,
+                'error_datetime' => $dateTime,
+                'content' => $content
+            ]);
+        }
+    }
+}
