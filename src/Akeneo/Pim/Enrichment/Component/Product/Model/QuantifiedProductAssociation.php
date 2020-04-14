485660f802e5caedcd43570805a3c9e5a6e435d2
Julien Sanchez <sumbobyboys@gmail.com>
2020-04-14 15:52:59 +0200

    MET-224: update doctrine configuration

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Model/QuantifiedProductAssociation.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Model;
+
+use Akeneo\Pim\Structure\Component\Model\AssociationTypeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * Product association entity
+ *
+ * @author    Filips Alpe <filips@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class QuantifiedProductAssociation
+{
+  /** @var int|string */
+  protected $id;
+
+  /** @var AssociationTypeInterface */
+  protected $associationType;
+
+  /** @var EntityWithAssociationsInterface */
+  protected $owner;
+
+  /** @var ProductInterface[] */
+  protected $products;
+
+  /** @var ProductModelInterface[] */
+  protected $productModels;
+
+  protected $quantifiedProductProductAssociation;
+
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+      $this->products = new ArrayCollection();
+      $this->productModels = new ArrayCollection();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getId()
+  {
+      return $this->id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAssociationType(AssociationTypeInterface $associationType)
+  {
+      $this->associationType = $associationType;
+
+      return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAssociationType()
+  {
+      return $this->associationType;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(EntityWithAssociationsInterface $owner)
+  {
+      // if (!$this->owner) {
+      //     $this->owner = $owner;
+      //     $owner->addAssociation($this);
+      // }
+
+      return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner()
+  {
+      return $this->owner;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setProducts($products)
+  {
+      $this->products = $products;
+
+      return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getProducts()
+  {
+      return $this->products;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addProduct(ProductInterface $product)
+  {
+      if (!$this->products->contains($product)) {
+          $this->products->add($product);
+      }
+
+      return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function hasProduct(ProductInterface $product)
+  {
+      return $this->products->contains($product);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function removeProduct(ProductInterface $product)
+  {
+      $this->products->removeElement($product);
+
+      return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getProductModels(): Collection
+  {
+      return $this->productModels;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addProductModel(ProductModelInterface $productModel): void
+  {
+      if (!$this->productModels->contains($productModel)) {
+          $this->productModels->add($productModel);
+      }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function removeProductModel(ProductModelInterface $productModel): void
+  {
+      $this->productModels->removeElement($productModel);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setProductModels($productModels): void
+  {
+      $this->productModels = $productModels;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReference()
+  {
+      return 'no_idea';
+      // return $this->owner ? $this->owner->getIdentifier() . '.' . $this->associationType->getCode() : null;
+  }
+}
