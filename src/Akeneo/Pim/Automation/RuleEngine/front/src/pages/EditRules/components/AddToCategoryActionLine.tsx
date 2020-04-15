992cc7b558329336ff0d1daf70e90e7fbcea2905
pierallard <pierallard@gmail.com>
2020-04-15 16:22:09 +0200

    RUL-127: Set fallback line

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/AddToCategoryActionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/AddToCategoryActionLine.tsx
@@ -8,17 +8,18 @@ type Props = {
   translate: Translate,
 }
 
-const FallbackActionLine: React.FC<Props> = ({ action, translate }) => {
+const AddToCategoryActionLine: React.FC<Props> = ({ translate, action }) => {
   return (
     <ActionTemplate
       translate={translate}
-      title="Unknown Action"
-      helper="This feature is under development. Please use the import to manage your rules."
-      srOnly="This feature is under development. Please use the import to manage your rules."
+      title={translate("pimee_catalog_rule.form.edit.add_to_category")}
+      helper={translate("pimee_catalog_rule.form.helper.add_to_category")}
+      srOnly={translate("pimee_catalog_rule.form.legend.add_to_category")}
     >
+      {/* Not coded yet */}
       {JSON.stringify(action.json)}
     </ActionTemplate>
   );
 };
 
-export { FallbackActionLine }
+export { AddToCategoryActionLine }
