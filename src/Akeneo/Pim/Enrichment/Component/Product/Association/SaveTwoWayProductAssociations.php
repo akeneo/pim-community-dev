5c84f4ae298041d26f892699ce21ba7bcb0b6f2d
Steven VAIDIE <steven.vaidie@gmail.com>
2020-04-17 09:22:07 +0200

    MET-182: fix: remove inverted products in only one query instead of loop on associations

--- src/Akeneo/Pim/Enrichment/Component/Product/Association/SaveTwoWayProductAssociations.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Association/SaveTwoWayProductAssociations.php
@@ -45,7 +45,7 @@ class SaveTwoWayProductAssociations implements EventSubscriberInterface
             return;
         }
 
-        $twoWayAssociations = array_filter($product->getAssociations()->toArray(), function(AssociationInterface $association) {
+        $twoWayAssociations = array_filter($product->getAssociations()->toArray(), function (AssociationInterface $association) {
             return $association->getAssociationType()->isTwoWay();
         });
 
@@ -53,16 +53,14 @@ class SaveTwoWayProductAssociations implements EventSubscriberInterface
             return;
         }
 
-        $twoWayAssociationTypeIds = array_map(function(AssociationInterface $association) {
+        $twoWayAssociationTypeIds = array_map(function (AssociationInterface $association) {
             return $association->getAssociationType()->getId();
         }, $twoWayAssociations);
 
         try {
             $this->connection->beginTransaction();
-            foreach ($twoWayAssociations as $twoWayAssociation) {
-                $this->removeInvertedProductAssociationsDeleted($twoWayAssociation, $product);
-                $this->removeInvertedProductModelAssociationsDeleted($twoWayAssociation, $product);
-            }
+            $this->removeInvertedProductAssociationsDeleted($twoWayAssociationTypeIds, $product->getId());
+            $this->removeInvertedProductModelAssociationsDeleted($twoWayAssociationTypeIds, $product->getId());
 
             $this->saveNewAssociation($twoWayAssociationTypeIds, $product->getId());
             $this->saveProductAssociation($twoWayAssociationTypeIds, $product->getId());
@@ -78,93 +76,68 @@ class SaveTwoWayProductAssociations implements EventSubscriberInterface
     }
 
     private function removeInvertedProductModelAssociationsDeleted(
-        AssociationInterface $association,
-        ProductInterface $product
+        array $twoWayAssociationTypeIds,
+        int $ownerProductId
     ): void {
-        $ownerProductModelIds = $association->getProductModels()->map(
-            function (ProductModelInterface $ownerProductModel) {
-                return $ownerProductModel->getId();
-            }
-        );
-
-        $types = [];
-        $params = [
-            'association_type_id' => $association->getAssociationType()->getId(),
-            'product_id' => $product->getId(),
-        ];
-
         $query = <<<SQL
 DELETE FROM pim_catalog_association_product_model_to_product
 WHERE association_id IN (
-	SELECT * FROM (
-		SELECT a.id
-	    FROM pim_catalog_product_model_association a INNER JOIN pim_catalog_association_product_model_to_product ap ON ap.association_id = a.id
-	    WHERE association_type_id = :association_type_id AND product_id = :product_id
-	) as association_id_to_delete
-)
+    SELECT * FROM (
+        SELECT a.id
+        FROM pim_catalog_product_model_association a
+        INNER JOIN pim_catalog_association_product_model_to_product ap
+          ON ap.association_id = a.id
+        WHERE association_type_id IN (:association_type_ids)
+        AND product_id = :owner_id
+        AND (association_type_id, product_id, owner_id) NOT IN (
+            SELECT existing_association.association_type_id, existing_association.owner_id, existing_product_model_association.product_model_id
+            FROM pim_catalog_association existing_association
+            INNER JOIN pim_catalog_association_product_model existing_product_model_association ON existing_product_model_association.association_id = existing_association.id
+        )
+    ) as association_id_to_delete
+);
 SQL;
 
-        if (!$ownerProductModelIds->isEmpty()) {
-            $query = <<<SQL
-DELETE FROM pim_catalog_association_product_model_to_product
-WHERE association_id IN (
-	SELECT * FROM (
-		SELECT a.id
-	    FROM pim_catalog_product_model_association a INNER JOIN pim_catalog_association_product_model_to_product ap ON ap.association_id = a.id
-	    WHERE association_type_id = :association_type_id AND product_id = :product_id AND owner_id NOT IN (:owner_product_model_ids_formatted)
-	) as association_id_to_delete
-)
-SQL;
-            $params['owner_product_model_ids_formatted'] = $ownerProductModelIds->toArray();
-            $types['owner_product_model_ids_formatted'] = Connection::PARAM_INT_ARRAY;
-        }
-
-        $this->connection->executeUpdate($query, $params, $types);
+        $this->connection->executeUpdate(
+            $query,
+            [
+                'association_type_ids' => $twoWayAssociationTypeIds,
+                'owner_id' => $ownerProductId
+            ],
+            ['association_type_ids' => Connection::PARAM_INT_ARRAY]
+        );
     }
 
     private function removeInvertedProductAssociationsDeleted(
-        AssociationInterface $association,
-        ProductInterface $product
+        array $twoWayAssociationTypeIds,
+        int $ownerProductId
     ): void {
-        $ownerProductIds = $association->getProducts()->map(
-            function (ProductInterface $ownerProduct) {
-                return $ownerProduct->getId();
-            }
-        );
-
-        $types = [];
-        $params = [
-            'association_type_id' => $association->getAssociationType()->getId(),
-            'product_id' => $product->getId(),
-        ];
-
         $query = <<<SQL
 DELETE FROM pim_catalog_association_product
 WHERE association_id IN (
-	SELECT * FROM (
-		SELECT a.id
-	    FROM pim_catalog_association a INNER JOIN pim_catalog_association_product ap ON ap.association_id = a.id
-	    WHERE association_type_id = :association_type_id AND product_id = :product_id
-	) as association_id_to_delete
-)
+    SELECT * FROM (
+        SELECT a.id
+        FROM pim_catalog_association a
+        INNER JOIN pim_catalog_association_product ap ON ap.association_id = a.id
+        WHERE association_type_id IN (:association_type_ids)
+        AND product_id = :owner_id
+        AND (association_type_id, product_id, owner_id) NOT IN (
+            SELECT existing_association.association_type_id, existing_association.owner_id, existing_product_association.product_id
+            FROM pim_catalog_association existing_association
+            INNER JOIN pim_catalog_association_product existing_product_association ON existing_product_association.association_id = existing_association.id
+        )
+    ) as association_id_to_delete
+);
 SQL;
 
-        if (!$ownerProductIds->isEmpty()) {
-            $query = <<<SQL
-DELETE FROM pim_catalog_association_product
-WHERE association_id IN (
-	SELECT * FROM (
-		SELECT a.id
-	    FROM pim_catalog_association a INNER JOIN pim_catalog_association_product ap ON ap.association_id = a.id
-	    WHERE association_type_id = :association_type_id AND product_id = :product_id AND owner_id NOT IN (:owner_product_ids_formatted)
-	) as association_id_to_delete
-)
-SQL;
-            $params['owner_product_ids_formatted'] = $ownerProductIds->toArray();
-            $types['owner_product_ids_formatted'] = Connection::PARAM_INT_ARRAY;
-        }
-
-        $this->connection->executeUpdate($query, $params, $types);
+        $this->connection->executeUpdate(
+            $query,
+            [
+                'association_type_ids' => $twoWayAssociationTypeIds,
+                'owner_id' => $ownerProductId
+            ],
+            ['association_type_ids' => Connection::PARAM_INT_ARRAY]
+        );
     }
 
     private function saveNewProductModelAssociation(array $twoWayAssociationTypeIds, int $ownerProductId): void
