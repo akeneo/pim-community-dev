094f95c00c2b5b3893ade164f8921621b9717838
Michel Tag <micheltag@akeneo.com>
2020-04-17 14:29:24 +0200

    Preparing version 4.0.17 (#11943)
    Preparing version 4.0.17
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.16';
+    const VERSION = '4.0.17';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Tonic';
