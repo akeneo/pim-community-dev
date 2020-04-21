61ce4f81a9c6a71918cac7373b087ca1d0b2fa54
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-04-21 18:19:09 +0200

    RUL-140: WIP style

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/TextAttributeConditionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/TextAttributeConditionLine.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import {TextAttributeCondition, TextAttributeOperators} from "../../models/TextAttributeCondition";
 import {InputText} from "../../components/InputText";
 import {Operator} from "../../models/Operator";
@@ -9,6 +10,37 @@ import {Scope} from "../../models";
 type Props = {
   condition: TextAttributeCondition,
 } & ConditionLineProps;
+
+const FieldColumn = styled.span`
+  width: 100px;
+  display: inline-block;
+  padding: 0 2px;
+  overflow: hidden;
+`;
+
+const OperatorColumn = styled.span`
+  width: 150px;
+  display: inline-block;
+  padding: 0 2px;
+`;
+
+const ValueColumn = styled.span`
+  width: 400px;
+  display: inline-block;
+  padding: 0 2px;
+`;
+
+const LocaleColumn = styled.span`
+  width: 150px;
+  display: inline-block;
+  padding: 0 2px;
+`;
+
+const ScopeColumn = styled.span`
+  width: 150px;
+  display: inline-block;
+  padding: 0 2px;
+`;
 
 const TextAttributeConditionLine: React.FC<Props> = ({
     register,
@@ -26,60 +58,47 @@ const TextAttributeConditionLine: React.FC<Props> = ({
     return scope.labels[currentCatalogLocale] || `[${scope.code}]`;
   }
 
-  const getLocales = (): Locale[] => {
-    if (!condition.attribute.scopable) {
-      return locales;
-    }
-
-    return scopeCode ? scopes[scopeCode].locales : [];
+  const computeLocales = (): Locale[] => {
+    return !condition.attribute.scopable
+      ? locales
+      : scopeCode ? scopes[scopeCode].locales : []
+      ;
   };
 
   return (
     <div>
-      <span>
+      <FieldColumn className={"AknGrid-bodyCell--highlight"}>
         {condition.attribute.labels[currentCatalogLocale] || '[' + condition.attribute.code + ']'}
         <input type="hidden" name={`content.conditions[${lineNumber}].field`} ref={register}/>
-      </span>
-      <select
-        style={{'width': 150, 'display': 'inline-block'}} //TODO Style
-        name={`content.conditions[${lineNumber}].operator`}
-        ref={register}
-        onChange={(event) => {
-          setOperator(event.target.value as Operator);
-        }}
-      >
-        {TextAttributeOperators.map((operator, i) => {
-          return <option key={i} value={operator}>
-            {translate('pimee_catalog_rule.form.edit.conditions.operators.' + operator)}
-          </option>
-        })}
-      </select>
-      {!([Operator.IS_EMPTY, Operator.IS_NOT_EMPTY] as Operator[]).includes(operator) && (
-        <span
-          style={{'width': 200, 'display': 'inline-block'}}
-        >
-          <InputText
-              id="edit-rules-input-toto"
-              name={`content.conditions[${lineNumber}].value`}
-              label={translate("pim_common.code")}
-              ref={register}
-          >&nbsp;</InputText>
-        </span>
-      )}
-      {condition.attribute.localizable &&
+      </FieldColumn>
+      <OperatorColumn>
         <select
-          style={{'width': 150, 'display': 'inline-block'}} //TODO Style
+          name={`content.conditions[${lineNumber}].operator`}
           ref={register}
-          name={`content.conditions[${lineNumber}].locale`}
+          onChange={(event) => {
+            setOperator(event.target.value as Operator);
+          }}
         >
-          {getLocales().map((locale: Locale, i: number) => {
-            return <option key={i} value={locale.code}>{locale.label}</option>
+          {TextAttributeOperators.map((operator, i) => {
+            return <option key={i} value={operator}>
+              {translate('pimee_catalog_rule.form.edit.conditions.operators.' + operator)}
+            </option>
           })}
         </select>
-      }
-      {condition.attribute.scopable &&
+      </OperatorColumn>
+      <ValueColumn>
+        {!([Operator.IS_EMPTY, Operator.IS_NOT_EMPTY] as Operator[]).includes(operator) && (
+          <InputText
+            id="edit-rules-input-toto"
+            name={`content.conditions[${lineNumber}].value`}
+            label={translate("pim_common.code")}
+            ref={register}
+          >&nbsp;</InputText>
+        )}
+      </ValueColumn>
+      <ScopeColumn>
+        {condition.attribute.scopable &&
         <select
-          style={{'width': 150, 'display': 'inline-block'}} //TODO Style
           ref={register}
           name={`content.conditions[${lineNumber}].scope`}
           onChange={(event) => {
@@ -90,7 +109,20 @@ const TextAttributeConditionLine: React.FC<Props> = ({
             return <option key={i} value={scope.code}>{getScopeLabel(scope)}</option>
           })}
         </select>
-      }
+        }
+      </ScopeColumn>
+      <LocaleColumn>
+        {condition.attribute.localizable &&
+        <select
+          ref={register}
+          name={`content.conditions[${lineNumber}].locale`}
+        >
+          {computeLocales().map((locale: Locale, i: number) => {
+            return <option key={i} value={locale.code}>{locale.label}</option>
+          })}
+        </select>
+        }
+      </LocaleColumn>
     </div>
   );
 };
