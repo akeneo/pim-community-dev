6cc60b9d8cd887595e7c3056eba5f388011ab0d2
Nicolas Maligne <nicolas.maligne@akeneo.com>
2020-04-22 10:40:20 +0200

    RUL-178: Add select2 wrapper

---
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/components/Select2Wrapper.tsx
@@ -0,0 +1,68 @@
+import $ from "jquery";
+import React, { useRef, useEffect } from "react";
+import { Label } from "./Label";
+
+type option = { id: number | string; text: string };
+
+type Select2Event = {
+  val: any;
+} & JQuery.Event;
+
+type Props = {
+  allowClear?: boolean;
+  containerCssClass?: string;
+  data: option[];
+  dropdownCssClass?: string;
+  formatResult?: (item: { id: string }) => string;
+  hiddenLabel?: boolean;
+  id: string;
+  label: string;
+  onChange: (event: Select2Event) => void;
+  placeholder?: string;
+  value: string;
+};
+
+const Select2Wrapper: React.FC<Props> = ({
+  allowClear,
+  containerCssClass,
+  data,
+  dropdownCssClass,
+  formatResult,
+  hiddenLabel = false,
+  id,
+  label,
+  onChange,
+  placeholder,
+  value,
+}) => {
+  let select2Ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (null === select2Ref.current) {
+      return;
+    }
+    const $select = $(select2Ref.current) as any;
+    $select.val(value);
+    $select.select2({
+      allowClear,
+      containerCssClass,
+      data,
+      dropdownCssClass,
+      formatResult,
+      placeholder,
+    });
+    $select.on("change", (event: Select2Event) => onChange(event));
+    return () => {
+      $select.off("change");
+      $select.select2("destroy");
+      $select.select2("container").remove();
+    };
+  }, [select2Ref]);
+  return (
+    <>
+      <Label label={label} hiddenLabel={hiddenLabel} htmlFor={id} />
+      <input id={id} type="hidden" ref={select2Ref} />
+    </>
+  );
+};
+
+export { Select2Wrapper };
