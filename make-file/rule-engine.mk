a2b4858b956e8171622d02afc996b3fa0c908b6e
Nicolas Maligne <nicolas.maligne@akeneo.com>
2020-04-23 17:35:05 +0200

    RULE-ENGINE: Add make file and yarn cmd

--- make-file/rule-engine.mk
+++ make-file/rule-engine.mk
@@ -75,3 +75,7 @@ rule-engine-integration-back: var/tests/behat/rule-engine-integration
 .PHONY: rule-engine-unit-front
 rule-engine-unit-front:
 	$(YARN_RUN) run --cwd=src/Akeneo/Pim/Automation/RuleEngine/front jest --ci
+
+.PHONY: rule-engine-lint-front
+rule-engine-lint-front:
+	$(YARN_RUN) run --cwd=src/Akeneo/Pim/Automation/RuleEngine/front lint
