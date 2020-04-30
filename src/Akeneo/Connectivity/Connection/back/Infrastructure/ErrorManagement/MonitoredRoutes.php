8f83603d51b7366f76b0afa49101393f2a0d8445
Paul Chasle <paul.chasle@akeneo.com>
2020-04-30 10:16:37 +0200

    API-1096: Add integration test to monitored routes

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/ErrorManagement/MonitoredRoutes.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/ErrorManagement/MonitoredRoutes.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Akeneo\Connectivity\Connection\Infrastructure\ErrorManagement;
 
+/**
+ * Monitored routes from the public API for the collection of business and technical errors.
+ */
 final class MonitoredRoutes
 {
     const ROUTES = [
