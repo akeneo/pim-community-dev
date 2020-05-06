d0e467a744f6b9b90249a8e64065ecee3577672a
jmleroux <jmleroux.pro@gmail.com>
2020-05-06 17:33:22 +0200

    PIM-9235: fix integration tests by extending the LRUCachedGetExistingAttributeOptions

--- tests/back/Pim/Enrichment/Integration/Product/LoadEntityWithValuesSubscriberIntegration.php
+++ tests/back/Pim/Enrichment/Integration/Product/LoadEntityWithValuesSubscriberIntegration.php
@@ -63,7 +63,7 @@ SQL;
         );
         $this->updateFooProduct();
 
-        self::$kernel->reboot(null);
+        $this->clearAttributeOptionsCache();
         $product = $this->findProductByIdentifier('foo');
         $this->assertProductHasValues($expectedValues, $product);
     }
@@ -76,10 +76,9 @@ SQL;
         $expectedValues = $this->getValuesFromStandardValues(
             $amputatedStandardValues
         );
-
         $this->updateFooProduct();
 
-        self::$kernel->reboot(null);
+        $this->clearAttributeOptionsCache();
         $product = $this->findProductByIdentifier('foo');
         $this->assertProductHasValues($expectedValues, $product);
     }
@@ -443,9 +442,6 @@ SQL;
     }
 
     /**
-     * @param string $attributeCode
-     * @param string $attributeOptionValueCode
-     *
      * @throws \InvalidArgumentException
      * @throws \LogicException
      */
@@ -464,5 +460,14 @@ SQL;
             );
         }
         $this->get('pim_catalog.remover.attribute_option')->remove($attributeOptionValue);
+    }
+
+    /**
+     * We need to clear the LRU cache because it's not meant to be used twice in the same request
+     */
+    private function clearAttributeOptionsCache(): void
+    {
+        $this->get('akeneo.pim.structure.query.cache.get_existing_attribute_option_codes_from_option_codes')
+            ->clear();
     }
 }
