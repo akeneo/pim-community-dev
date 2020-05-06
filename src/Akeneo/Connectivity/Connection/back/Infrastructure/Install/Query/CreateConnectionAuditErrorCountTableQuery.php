6e224e765ba291a4ee39dd9cbc543a4307a97ea1
Willy <willy.mesnage@akeneo.com>
2020-05-06 15:36:45 +0200

    API-1108: Create connection audit error count table

---
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Install/Query/CreateConnectionAuditErrorCountTableQuery.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Infrastructure\Install\Query;
+
+/**
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class CreateConnectionAuditErrorCountTableQuery
+{
+    const QUERY = <<<SQL
+CREATE TABLE IF NOT EXISTS akeneo_connectivity_connection_audit_error_count(
+    connection_code VARCHAR(100) NOT NULL,
+    error_datetime DATETIME NOT NULL,
+    error_count INT NOT NULL,
+    error_type VARCHAR(100) NOT NULL,
+    updated DATETIME NOT NULL,
+    PRIMARY KEY (error_datetime, connection_code, error_type)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
+SQL;
+}
