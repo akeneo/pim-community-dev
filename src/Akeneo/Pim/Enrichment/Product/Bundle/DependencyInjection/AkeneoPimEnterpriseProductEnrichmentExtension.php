0f1926c0c1559f3a369e8d7b7a3ee2d71042b6fb
christophe-chausseray <chausseray.christophe@gmail.com>
2020-05-13 09:12:59 +0930

    CHRIS-3: Rename bundle AkeneoPimEnterpriseProductEnrichmentBundle

--- src/Akeneo/Pim/Enrichment/Product/Bundle/DependencyInjection/AkeneoPimEnterpriseProductEnrichmentExtension.php
+++ src/Akeneo/Pim/Enrichment/Product/Bundle/DependencyInjection/AkeneoPimEnterpriseProductEnrichmentExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AkeneoPimEnterpriseEnrichmentExtension extends Extension
+class AkeneoPimEnterpriseProductEnrichmentExtension extends Extension
 {
     /**
      * {@inheritdoc}
