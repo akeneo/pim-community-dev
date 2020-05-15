1bcec7a8d527d9597b6e87787318733d6b6e5ea4
Pierre Jolly <pie.jolly@gmail.com>
2020-05-15 14:34:19 +0200

    CXP-55: Connection cards internal API E2E test

---
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Audit/ErrorCountPerConnectionEndToEnd.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\back\tests\EndToEnd\Audit;
+
+use Akeneo\Connectivity\Connection\back\tests\EndToEnd\WebTestCase;
+use Akeneo\Connectivity\Connection\back\tests\Integration\Fixtures\AuditErrorLoader;
+use Akeneo\Connectivity\Connection\back\tests\Integration\Fixtures\AuditLoader;
+use Akeneo\Connectivity\Connection\Domain\Audit\Model\AllConnectionCode;
+use Akeneo\Connectivity\Connection\Domain\ErrorManagement\Model\Write\HourlyErrorCount;
+use Akeneo\Connectivity\Connection\Domain\ValueObject\HourlyInterval;
+use Akeneo\Connectivity\Connection\Domain\Audit\Model\Write\HourlyEventCount;
+use Akeneo\Connectivity\Connection\Domain\Settings\Model\ValueObject\FlowType;
+use Akeneo\Test\Integration\Configuration;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Pierre Jolly <pierre.jolly@akeneo.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class ErrorCountPerConnectionEndToEnd extends WebTestCase
+{
+    public function test_it_get_error_count_per_connection()
+    {
+        $this->createHourlyErrorCounts([
+            ['bynder', 'business', '2019-12-31 23:00:00', 1], // ignored
+            ['bynder', 'business', '2020-01-01 00:00:00', 3],
+            ['bynder', 'business', '2020-01-04 15:00:00', 5],
+            ['bynder', 'business', '2020-01-04 16:00:00', 7],
+            ['bynder', 'technical', '2020-01-07 16:00:00', 7], // ignored
+            ['bynder', 'business', '2020-01-08 00:00:00', 9], // ignored
+            ['sap', 'business', '2020-01-04 14:00:00', 10],
+            ['sap', 'business', '2020-01-04 15:00:00', 30],
+            ['sap', 'business', '2020-01-08 23:00:00', 50],
+            ['sap', 'technical', '2020-01-08 23:00:00', 50], // ignored
+        ], 'Asia/Tokyo');
+
+        $expectedResult = [
+            'bynder' => 15,
+            'sap' => 90,
+        ];
+
+        $user = $this->authenticateAsAdmin();
+        $user->setTimezone('Asia/Tokyo');
+        $this->get('pim_user.saver.user')->save($user);
+
+        $this->client->request(
+            'GET',
+            '/rest/connections/audit/error-count-per-connection',
+            [
+                'error_type' => 'business',
+                'end_date' => '2020-01-07'
+            ],
+        );
+        $result = json_decode($this->client->getResponse()->getContent(), true);
+
+        Assert::assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        Assert::assertEquals($expectedResult, $result);
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+
+    private function getAuditErrorLoader(): AuditErrorLoader
+    {
+        return $this->get('akeneo_connectivity.connection.fixtures.audit_error_loader');
+    }
+
+    private function createHourlyErrorCounts(array $hourlyErrorCountData, string $userDateTimeZoneStr): void
+    {
+        foreach ($hourlyErrorCountData as [$connectionCode, $errorType, $userDateTimeStr, $errorCount]) {
+            $utcDateTime = (new \DateTimeImmutable($userDateTimeStr, new \DateTimeZone($userDateTimeZoneStr)))
+                ->setTimezone(new \DateTimeZone('UTC'));
+
+            $this->getAuditErrorLoader()
+                ->insert(
+                    $connectionCode,
+                    HourlyInterval::createFromDateTime($utcDateTime),
+                    $errorCount,
+                    $errorType
+                );
+        }
+    }
+}
