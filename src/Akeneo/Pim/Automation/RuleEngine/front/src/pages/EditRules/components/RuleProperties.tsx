4be61c871ff5ab6d12f44581ba2c3ec11e54c1f5
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-05-15 16:53:22 +0200

    RUL-193: add properties section

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/RuleProperties.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/RuleProperties.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Translate } from '../../../dependenciesTools';
 import { Locale } from '../../../models';
-import { InputNumber, InputText, SmallHelper } from '../../../components';
+import {
+  InputNumber,
+  InputText,
+  SmallHelper,
+  FormSubsection,
+} from '../../../components';
 
 type Props = {
   locales?: Locale[];
@@ -12,42 +17,54 @@ type Props = {
 const RuleProperties: React.FC<Props> = ({ locales, translate }) => {
   const { register } = useFormContext();
   return (
-    <div className='AknFormContainer'>
+    <>
       <SmallHelper>Page under construction</SmallHelper>
-      <div className='AknFormContainer'>
-        <div className='AknFieldContainer'>
-          <InputText
-            disabled
-            id='edit-rules-input-code'
-            name='code'
-            label={translate('pim_common.code')}
-            readOnly
-            ref={register}
-          />
+      <FormSubsection
+        title={translate(
+          'pimee_catalog_rule.form.edit.properties.section.general'
+        )}>
+        <div className='AknFormContainer'>
+          <div className='AknFieldContainer'>
+            <InputText
+              disabled
+              id='edit-rules-input-code'
+              name='code'
+              label={translate('pim_common.code')}
+              readOnly
+              ref={register}
+            />
+          </div>
+          <div className='AknFieldContainer'>
+            <InputNumber
+              name='priority'
+              id='edit-rules-input-priority'
+              label={translate('pimee_catalog_rule.form.edit.priority.label')}
+              ref={register}
+            />
+          </div>
         </div>
-        <div className='AknFieldContainer'>
-          <InputNumber
-            name='priority'
-            id='edit-rules-input-priority'
-            label={translate('pimee_catalog_rule.form.edit.priority.label')}
-            ref={register}
-          />
+      </FormSubsection>
+      <FormSubsection
+        title={translate(
+          'pimee_catalog_rule.form.edit.properties.section.labels'
+        )}>
+        <div className='AknFormContainer'>
+          {locales &&
+            locales.map(locale => {
+              return (
+                <div className='AknFieldContainer' key={locale.code}>
+                  <InputText
+                    name={`labels.${locale.code}`}
+                    id={`edit-rules-input-label-${locale.code}`}
+                    label={locale.label}
+                    ref={register}
+                  />
+                </div>
+              );
+            })}
         </div>
-        {locales &&
-          locales.map(locale => {
-            return (
-              <div className='AknFieldContainer' key={locale.code}>
-                <InputText
-                  name={`labels.${locale.code}`}
-                  id={`edit-rules-input-label-${locale.code}`}
-                  label={locale.label}
-                  ref={register}
-                />
-              </div>
-            );
-          })}
-      </div>
-    </div>
+      </FormSubsection>
+    </>
   );
 };
 
