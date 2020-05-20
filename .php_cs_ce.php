15230d4d8d4665f0a800bc7da38b58bc6c4ce917
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2020-05-20 10:16:57 +0200

    Launch CE static tests from an EE build

---
+++ .php_cs_ce.php
@@ -0,0 +1,22 @@
+<?php
+
+return PhpCsFixer\Config::create()
+    ->setRules(array(
+        '@PSR2' => true,
+        'linebreak_after_opening_tag' => true,
+        'ordered_imports' => true,
+        'method_argument_space' => [
+            'ensure_fully_multiline' => false
+        ],
+    ))
+    ->setCacheFile('var/php_cs.cache')
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->name('*.php')
+            ->notName('*Spec.php')
+            ->notName('*Integration.php')
+            ->in(__DIR__ . '/vendor/akeneo/pim-community-dev/tests/legacy/features')
+            ->in(__DIR__ . '/vendor/akeneo/pim-community-dev/tests/features')
+            ->in(__DIR__ . '/vendor/akeneo/pim-community-dev/tests/back/Acceptance')
+            ->in(__DIR__ . '/vendor/akeneo/pim-community-dev/src')
+    );
--- make-file/test.mk
+++ make-file/test.mk
@@ -22,9 +22,11 @@ lint-back:
 	$(DOCKER_COMPOSE) run -u www-data --rm php rm -rf var/cache/dev
 	APP_ENV=dev $(DOCKER_COMPOSE) run -e APP_DEBUG=1 -u www-data --rm php bin/console cache:warmup
 	$(DOCKER_COMPOSE) run -u www-data --rm php vendor/bin/phpstan analyse src/Akeneo/Pim -l 1
+	$(DOCKER_COMPOSE) run -u www-data --rm php vendor/bin/phpstan analyse vendor/akeneo/pim-community-dev/src/Akeneo/Pim -l 1
 	$(MAKE) data-quality-insights-lint-back reference-entity-lint-back asset-manager-lint-back
 	$(DOCKER_COMPOSE) run -u www-data --rm php rm -rf var/cache/dev
 	${PHP_RUN} vendor/bin/php-cs-fixer fix --diff --dry-run --config=.php_cs.php
+	${PHP_RUN} vendor/bin/php-cs-fixer fix --diff --dry-run --config=.php_cs_ce.php
 
 .PHONY: lint-front
 lint-front: franklin-insights-lint-front
