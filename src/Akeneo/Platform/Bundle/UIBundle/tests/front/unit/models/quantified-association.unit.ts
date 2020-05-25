ce8316606b3fcde25d96e231080d3e568a06cd43
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-05-25 10:38:14 +0200

    RAC-23: Fixing unit tests

--- src/Akeneo/Platform/Bundle/UIBundle/tests/front/unit/models/quantified-association.unit.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/tests/front/unit/models/quantified-association.unit.ts
@@ -1,51 +1,33 @@
 import {
-  isQuantifiedAssociationCollectionEmpty,
-  quantifiedAssociationCollectionToRowCollection,
-  rowCollectionToQuantifiedAssociationCollection,
+  quantifiedAssociationToRowCollection,
+  rowCollectionToQuantifiedAssociation,
 } from '../../../../Resources/public/js/product/form/quantified-associations/models/quantified-association';
 import {ProductType} from '../../../../Resources/public/js/product/form/quantified-associations/models';
 
-const quantifiedAssociationCollection = {
-  PACK: {
-    products: [{identifier: 'bag', quantity: 4}],
-    product_models: [],
-  },
-  CROSS_SELL: {
-    products: [{identifier: 'sock', quantity: 8}],
-    product_models: [{identifier: 'braided-hat', quantity: 12}],
-  },
+const quantifiedAssociation = {
+  products: [{identifier: 'bag', quantity: 4}],
+  product_models: [{identifier: 'braided-hat', quantity: 12}],
 };
 
 const rowCollection = [
-  {identifier: 'bag', quantity: 4, productType: ProductType.Product, associationTypeCode: 'PACK', product: null},
   {
-    identifier: 'sock',
-    quantity: 8,
+    quantifiedLink: {identifier: 'bag', quantity: 4},
     productType: ProductType.Product,
-    associationTypeCode: 'CROSS_SELL',
     product: null,
   },
   {
-    identifier: 'braided-hat',
-    quantity: 12,
+    quantifiedLink: {identifier: 'braided-hat', quantity: 12},
     productType: ProductType.ProductModel,
-    associationTypeCode: 'CROSS_SELL',
     product: null,
   },
 ];
 
 describe('quantified association', () => {
-  it('should tell if a quantified association collection is empty', () => {
-    expect(isQuantifiedAssociationCollectionEmpty(quantifiedAssociationCollection)).toEqual(false);
-    expect(isQuantifiedAssociationCollectionEmpty({})).toEqual(true);
-    expect(isQuantifiedAssociationCollectionEmpty({PACK: {products: [], product_models: []}})).toEqual(true);
-  });
-
   it('should create a row collection from a quantified association collection', () => {
-    expect(quantifiedAssociationCollectionToRowCollection(quantifiedAssociationCollection)).toEqual(rowCollection);
+    expect(quantifiedAssociationToRowCollection(quantifiedAssociation)).toEqual(rowCollection);
   });
 
   it('should create a quantified association collection from a row collection', () => {
-    expect(rowCollectionToQuantifiedAssociationCollection(rowCollection)).toEqual(quantifiedAssociationCollection);
+    expect(rowCollectionToQuantifiedAssociation(rowCollection)).toEqual(quantifiedAssociation);
   });
 });
