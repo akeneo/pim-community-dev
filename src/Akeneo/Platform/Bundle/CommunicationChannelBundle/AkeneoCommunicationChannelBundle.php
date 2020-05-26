d396b6a1ea0f7a202df8e971792c6eb990eea914
christophe-chausseray <chausseray.christophe@gmail.com>
2020-05-26 14:14:55 +0930

    GITHUB-APD-123: Add new bundle CommunicationChannel in Platform

---
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/AkeneoCommunicationChannelBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Akeneo\Platform\Bundle\CommunicationChannelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AkeneoCommunicationChannelBundle bundle
+ *
+ * @author    Christophe Chausseray <chausseray.christophe@gmail.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AkeneoCommunicationChannelBundle extends Bundle
+{
+}
+
