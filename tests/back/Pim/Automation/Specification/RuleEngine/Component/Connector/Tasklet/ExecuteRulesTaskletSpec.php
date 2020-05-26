e172e74fc8bdfbe330be6893c8bcdf9d0fff3791
Mathias Métayer <mathias.metayer@akeneo.com>
2020-05-26 16:56:13 +0200

    RUL-204: Add missing spec

---
+++ tests/back/Pim/Automation/Specification/RuleEngine/Component/Connector/Tasklet/ExecuteRulesTaskletSpec.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Specification\Akeneo\Pim\Automation\RuleEngine\Component\Connector\Tasklet;
+
+use Akeneo\Pim\Automation\RuleEngine\Component\Connector\Tasklet\ExecuteRulesTasklet;
+use Akeneo\Tool\Bundle\RuleEngineBundle\Model\RuleDefinition;
+use Akeneo\Tool\Bundle\RuleEngineBundle\Repository\RuleDefinitionRepositoryInterface;
+use Akeneo\Tool\Bundle\RuleEngineBundle\Runner\DryRunnerInterface;
+use Akeneo\Tool\Bundle\RuleEngineBundle\Runner\RunnerInterface;
+use Akeneo\Tool\Component\Batch\Job\JobParameters;
+use Akeneo\Tool\Component\Batch\Model\StepExecution;
+use Akeneo\Tool\Component\Connector\Step\TaskletInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class ExecuteRulesTaskletSpec extends ObjectBehavior
+{
+    function let(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        RunnerInterface $ruleRunner,
+        DryRunnerInterface $dryRuleRunner,
+        EventDispatcherInterface $eventDispatcher,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution
+    ) {
+        $this->beConstructedWith($ruleDefinitionRepository, $ruleRunner, $dryRuleRunner, $eventDispatcher);
+
+        $stepExecution->getJobParameters()->willReturn($jobParameters);
+        $this->setStepExecution($stepExecution);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldImplement(TaskletInterface::class);
+        $this->shouldHaveType(ExecuteRulesTasklet::class);
+    }
+
+    function it_executes_given_rules(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        RunnerInterface $ruleRunner,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution
+    ) {
+        $jobParameters->get('rule_codes')->willReturn(['rule1', 'rule2']);
+        $jobParameters->get('dry_run')->willReturn(false);
+
+        $ruleDefinitionRepository->findBy(['code' => ['rule1', 'rule2']], ['priority' => 'DESC'])
+            ->willReturn([new RuleDefinition(), new RuleDefinition()]);
+
+        $stepExecution->setSummary(Argument::type('array'))->shouldBeCalled();
+        $ruleRunner->run(Argument::type(RuleDefinition::class))->shouldBeCalledTimes(2);
+
+        $this->execute();
+    }
+
+    function it_executes_all_the_rules(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        RunnerInterface $ruleRunner,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution
+    ) {
+        $jobParameters->get('rule_codes')->willReturn([]);
+        $jobParameters->get('dry_run')->willReturn(false);
+
+        $ruleDefinitionRepository->findAllOrderedByPriority()
+                                 ->willReturn([new RuleDefinition(), new RuleDefinition(), new RuleDefinition()]);
+
+        $stepExecution->setSummary(Argument::type('array'))->shouldBeCalled();
+        $ruleRunner->run(Argument::type(RuleDefinition::class))->shouldBeCalledTimes(3);
+
+        $this->execute();
+    }
+
+    function it_dry_runs_a_rule(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        DryRunnerInterface $dryRuleRunner,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution
+    ) {
+        $jobParameters->get('rule_codes')->willReturn(['rule1']);
+        $jobParameters->get('dry_run')->willReturn(true);
+
+        $ruleDefinitionRepository->findBy(['code' => ['rule1']], ['priority' => 'DESC'])
+                                 ->willReturn([new RuleDefinition(), new RuleDefinition()]);
+
+        $stepExecution->setSummary(Argument::type('array'))->shouldBeCalled();
+        $dryRuleRunner->dryRun(Argument::type(RuleDefinition::class))->shouldBeCalled();
+
+        $this->execute();
+    }
+
+    function it_stops_rules_execution_when_an_error_occurs_in_strict_mode(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        RunnerInterface $ruleRunner,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution,
+        RuleDefinition $ruleDefinition1,
+        RuleDefinition $ruleDefinition2
+    ) {
+        $ruleDefinitionRepository->findAllOrderedByPriority()->willReturn([$ruleDefinition1, $ruleDefinition2]);
+        $jobParameters->get('rule_codes')->willReturn([]);
+        $jobParameters->get('dry_run')->willReturn(false);
+        $jobParameters->get('stop_on_error')->willReturn(true);
+
+        $stepExecution->setSummary(Argument::type('array'))->shouldBeCalled();
+
+        $exception = new \LogicException('error message');
+        $ruleRunner->run($ruleDefinition1)->willThrow($exception);
+
+        $stepExecution->addError('error message')->shouldBeCalled();
+        $stepExecution->incrementSummaryInfo('errored_rules')->shouldBeCalled();
+
+        $ruleRunner->run($ruleDefinition2)->shouldNotBeCalled();
+
+        $this->shouldThrow($exception)->during('execute');
+    }
+
+    function it_keeps_running_rules_execution_when_an_error_occurs_in_non_strict_mode(
+        RuleDefinitionRepositoryInterface $ruleDefinitionRepository,
+        RunnerInterface $ruleRunner,
+        JobParameters $jobParameters,
+        StepExecution $stepExecution,
+        RuleDefinition $ruleDefinition1,
+        RuleDefinition $ruleDefinition2
+    ) {
+        $ruleDefinitionRepository->findAllOrderedByPriority()->willReturn([$ruleDefinition1, $ruleDefinition2]);
+        $jobParameters->get('rule_codes')->willReturn([]);
+        $jobParameters->get('dry_run')->willReturn(false);
+        $jobParameters->get('stop_on_error')->willReturn(false);
+
+        $stepExecution->setSummary(Argument::type('array'))->shouldBeCalled();
+
+        $exception = new \LogicException('error message');
+        $ruleRunner->run($ruleDefinition1)->willThrow($exception);
+
+        $stepExecution->addError('error message')->shouldBeCalled();
+        $stepExecution->incrementSummaryInfo('errored_rules')->shouldBeCalled();
+
+        $ruleRunner->run($ruleDefinition2)->shouldBeCalled();
+
+        $this->execute();
+    }
+}
