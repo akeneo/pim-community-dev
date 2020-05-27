ed9b3d2f440f4ccacb42301ab225ef27c776204c
christophe-chausseray <chausseray.christophe@gmail.com>
2020-05-27 10:02:00 +0930

    GITHUB-APD-123: Add link to redirect to the help center regarding the pim edition and version

--- src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/public/menu/communication-channel.ts
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/public/menu/communication-channel.ts
@@ -1,21 +1,40 @@
 import * as Backbone from 'backbone';
 import * as _ from 'underscore';
 
+const DataCollector = require('pim/data-collector');
+const __ = require('oro/translator');
+
 const CommunicationChannelTemplate = require('akeneo/template/menu/communication-channel');
 
 class CommunicationChannel extends Backbone.View<any> {
-  constructor() {
-    super({tagName: 'a'});
-  }
+  private analyticsUrl: string = 'pim_analytics_data_collect';
+
+  private baseUrl: string =
+    'https://help.akeneo.com/pim/serenity/updates/index.html?utm_source=akeneo-app&utm_medium=communication-icon&utm_campaign=';
 
   public render(): Backbone.View {
     const template: any = _.template(CommunicationChannelTemplate);
-    this.$el.html(template());
-
-    this.$el.attr('href', 'https://help.akeneo.com/pim/v4/');
-    this.$el.attr('target', '_blank');
+    this.$el.empty().append(
+      template({
+        title: __('pim_communication_channel.link.title'),
+      })
+    );
 
     return Backbone.View.prototype.render.apply(this, arguments);
+  }
+
+  public refresh(): void {
+    this.getCampaign().then((campaign: string) => {
+      this.$('a').attr('href', `${this.baseUrl}${campaign}`);
+    });
+  }
+
+  private getCampaign() {
+    return DataCollector.collect(this.analyticsUrl).then((data: any) => {
+      const {pim_version, pim_edition} = data;
+
+      return `${pim_edition}${pim_version}`;
+    });
   }
 }
 
