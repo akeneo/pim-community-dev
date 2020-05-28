c04fb2705e039be31713812e18008fbf47a28f7e
Tseho <quentin.favrie@gmail.com>
2020-05-28 16:07:05 +0200

    RAC-64: Fix QuantifiedAssociationsNormalizerSpec

--- tests/back/Pim/Enrichment/Specification/Component/Product/Normalizer/Standard/Product/QuantifiedAssociationsNormalizerSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Normalizer/Standard/Product/QuantifiedAssociationsNormalizerSpec.php
@@ -6,11 +6,92 @@ use Akeneo\Pim\Enrichment\Component\Product\Model\EntityWithQuantifiedAssociatio
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductModelInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Normalizer\Standard\Product\QuantifiedAssociationsNormalizer;
+use Akeneo\Pim\Enrichment\Component\Product\QuantifiedAssociation\QuantifiedAssociationsMerger;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class QuantifiedAssociationsNormalizerSpec extends ObjectBehavior
 {
+    function let(
+        QuantifiedAssociationsMerger $quantifiedAssociationsMerger,
+        ProductModelInterface $product_model,
+        ProductModelInterface $variant_level_1,
+        ProductInterface $variant_level_2
+    ) {
+        $this->beConstructedWith(
+            $quantifiedAssociationsMerger
+        );
+
+        $product_model->getParent()->willReturn(null);
+        $product_model->normalizeQuantifiedAssociations()->willReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                    ],
+                ],
+            ]
+        );
+        $variant_level_1->getParent()->willReturn($product_model);
+        $variant_level_1->normalizeQuantifiedAssociations()->willReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'B', 'quantity' => 2],
+                    ],
+                ],
+            ]
+        );
+        $variant_level_2->getParent()->willReturn($variant_level_1);
+        $variant_level_2->normalizeQuantifiedAssociations()->willReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'C', 'quantity' => 3],
+                    ],
+                ],
+            ]
+        );
+
+        $quantifiedAssociationsMerger
+            ->normalizeAndMergeQuantifiedAssociationsFrom(
+                [
+                    $product_model,
+                    $variant_level_1,
+                ]
+            )
+            ->willReturn(
+                [
+                    'PACK' => [
+                        'products' => [
+                            ['identifier' => 'A', 'quantity' => 1],
+                            ['identifier' => 'B', 'quantity' => 2],
+                        ],
+                    ],
+                ]
+            );
+
+        $quantifiedAssociationsMerger
+            ->normalizeAndMergeQuantifiedAssociationsFrom(
+                [
+                    $product_model,
+                    $variant_level_1,
+                    $variant_level_2,
+                ]
+            )
+            ->willReturn(
+                [
+                    'PACK' => [
+                        'products' => [
+                            ['identifier' => 'A', 'quantity' => 1],
+                            ['identifier' => 'B', 'quantity' => 2],
+                            ['identifier' => 'C', 'quantity' => 3],
+                        ],
+                    ],
+                ]
+            );
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(QuantifiedAssociationsNormalizer::class);
@@ -30,91 +111,88 @@ class QuantifiedAssociationsNormalizerSpec extends ObjectBehavior
         $this->supportsNormalization(new \stdClass(), 'other_format')->shouldReturn(false);
     }
 
-    function it_normalizes_a_product_quantified_associations(EntityWithQuantifiedAssociationsInterface $product)
-    {
-        $product->normalizeQuantifiedAssociations()->willReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 2],
-                    ['identifier' => 'B', 'quantity' => 3],
-                    ['identifier' => 'C', 'quantity' => 4],
-                ],
-            ],
-        ]);
-
-        $this->normalize($product, 'standard')->shouldReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 2],
-                    ['identifier' => 'B', 'quantity' => 3],
-                    ['identifier' => 'C', 'quantity' => 4],
-                ],
-            ],
-        ]);
-    }
-
-    function it_normalizes_a_product_variant_quantified_associations(
-        ProductModelInterface $product_model,
-        ProductModelInterface $variant_level_1,
+    public function it_normalizes_a_product_without_its_parents_associations(
         ProductInterface $variant_level_2
     ) {
-        $product_model->getParent()->willReturn(null);
-        $variant_level_1->getParent()->willReturn($product_model);
-        $variant_level_2->getParent()->willReturn($variant_level_1);
+        $this->normalizeWithoutParentsAssociations($variant_level_2, 'standard', [])->shouldReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'C', 'quantity' => 3],
+                    ],
+                ],
+            ]
+        );
+    }
 
-        $product_model->normalizeQuantifiedAssociations()->willReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 2],
-                    ['identifier' => 'B', 'quantity' => 3],
-                    ['identifier' => 'C', 'quantity' => 4],
+    public function it_normalizes_a_product_with_only_its_parents_associations(
+        ProductInterface $variant_level_2
+    ) {
+        $this->normalizeOnlyParentsAssociations($variant_level_2, 'standard', [])->shouldReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                        ['identifier' => 'B', 'quantity' => 2],
+                    ],
                 ],
-            ],
-        ]);
-        $variant_level_1->normalizeQuantifiedAssociations()->willReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 5],
-                    ['identifier' => 'B', 'quantity' => 6],
-                ],
-            ],
-        ]);
-        $variant_level_2->normalizeQuantifiedAssociations()->willReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 7],
-                    ['identifier' => 'D', 'quantity' => 8],
-                ],
-            ],
-        ]);
+            ]
+        );
+    }
 
-        $this->normalize($product_model, 'standard')->shouldReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 2],
-                    ['identifier' => 'B', 'quantity' => 3],
-                    ['identifier' => 'C', 'quantity' => 4],
+    public function it_normalizes_a_product_with_its_parents_associations(
+        ProductInterface $variant_level_2
+    ) {
+        $this->normalizeWithParentsAssociations($variant_level_2, 'standard', [])->shouldReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                        ['identifier' => 'B', 'quantity' => 2],
+                        ['identifier' => 'C', 'quantity' => 3],
+                    ],
                 ],
-            ],
-        ]);
-        $this->normalize($variant_level_1, 'standard')->shouldReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 5],
-                    ['identifier' => 'B', 'quantity' => 6],
-                    ['identifier' => 'C', 'quantity' => 4],
+            ]
+        );
+    }
+
+    public function it_normalizes_a_product_nonvariant(
+        EntityWithQuantifiedAssociationsInterface $nonVariantProduct
+    ) {
+        $nonVariantProduct->normalizeQuantifiedAssociations()->willReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                    ],
                 ],
-            ],
-        ]);
-        $this->normalize($variant_level_2, 'standard')->shouldReturn([
-            'PACK' => [
-                'products' => [
-                    ['identifier' => 'A', 'quantity' => 7],
-                    ['identifier' => 'B', 'quantity' => 6],
-                    ['identifier' => 'C', 'quantity' => 4],
-                    ['identifier' => 'D', 'quantity' => 8],
+            ]
+        );
+
+        $this->normalize($nonVariantProduct, 'standard', [])->shouldReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                    ],
                 ],
-            ],
-        ]);
+            ]
+        );
+    }
+
+    public function it_normalizes_a_product_variant_and_merge_the_parents_associations_by_default(
+        ProductInterface $variant_level_2
+    ) {
+        $this->normalize($variant_level_2, 'standard', [])->shouldReturn(
+            [
+                'PACK' => [
+                    'products' => [
+                        ['identifier' => 'A', 'quantity' => 1],
+                        ['identifier' => 'B', 'quantity' => 2],
+                        ['identifier' => 'C', 'quantity' => 3],
+                    ],
+                ],
+            ]
+        );
     }
 }
