e70135523bceacc7be744d00947000f272bc241b
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-01 11:26:01 +0930

    GITHUB-APD-135: Update to open/close communication channel panel when click on updates button

--- src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/public/bridge/view/panel.tsx
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/public/bridge/view/panel.tsx
@@ -2,13 +2,53 @@ import React from 'react';
 import ReactView from 'akeneocommunicationchannel/bridge/react/react-view';
 import {Panel} from 'akeneocommunicationchannel/components/panel';
 
+const mediator = require('oro/mediator');
+
 class PanelView extends ReactView {
+  constructor() {
+    super({className: 'AknCommunicationChannelPanel'});
+  }
+
+  configure() {
+    this.listenTo(mediator, 'communication-channel:panel:open', this.openPanel);
+    this.listenTo(mediator, 'communication-channel:panel:close', this.closePanel);
+    this.listenTo(mediator, 'all', this.closePanelWhenEventTriggered);
+
+    return super.configure();
+  }
+
   reactElementToMount(): JSX.Element {
     return <Panel />;
   }
   
   render() {
     return super.render();
+  }
+
+  openPanel() {
+    this.$el.removeClass('AknCommunicationChannelPanel--collapsed');
+    mediator.trigger('pim-app:overlay:show');
+  }
+
+  closePanel() {
+    if (!this.isColapsed()) {
+      this.$el.addClass('AknCommunicationChannelPanel--collapsed');
+      mediator.trigger('pim-app:overlay:hide');
+    }
+  }
+
+  closePanelWhenEventTriggered(event: string) {
+    if (!this.isEventToOpenPanel(event)) {
+      this.closePanel();
+    }
+  }
+
+  isColapsed() {
+    return this.$el.hasClass('AknCommunicationChannelPanel--collapsed');
+  }
+
+  isEventToOpenPanel(event: string) {
+    return 'communication-channel:panel:open' === event || 'pim-app:overlay:show' === event;
   }
 }
 
--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/pim-app.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/pim-app.js
@@ -5,89 +5,102 @@
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-define(
-    [
-        'jquery',
-        'underscore',
-        'backbone',
-        'pim/form',
-        'oro/messenger',
-        'pim/fetcher-registry',
-        'pim/init',
-        'pim/init-translator',
-        'oro/init-layout',
-        'pimuser/js/init-signin',
-        'pim/page-title',
-        'pim/date-context',
-        'pim/user-context',
-        'pim/template/app',
-        'pim/template/common/flash',
-        'jquery.select2.placeholder'
-    ], function (
-        $,
-        _,
-        Backbone,
-        BaseForm,
-        messenger,
-        FetcherRegistry,
-        init,
-        initTranslator,
-        initLayout,
-        initSignin,
-        pageTitle,
-        DateContext,
-        UserContext,
-        template,
-        flashTemplate
-    ) {
-        return BaseForm.extend({
-            tagName: 'div',
-            className: 'app',
-            template: _.template(template),
-            flashTemplate: _.template(flashTemplate),
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'pim/form',
+  'oro/messenger',
+  'oro/mediator',
+  'pim/fetcher-registry',
+  'pim/init',
+  'pim/init-translator',
+  'oro/init-layout',
+  'pimuser/js/init-signin',
+  'pim/page-title',
+  'pim/date-context',
+  'pim/user-context',
+  'pim/template/app',
+  'pim/template/common/flash',
+  'jquery.select2.placeholder',
+], function(
+  $,
+  _,
+  Backbone,
+  BaseForm,
+  messenger,
+  mediator,
+  FetcherRegistry,
+  init,
+  initTranslator,
+  initLayout,
+  initSignin,
+  pageTitle,
+  DateContext,
+  UserContext,
+  template,
+  flashTemplate
+) {
+  return BaseForm.extend({
+    tagName: 'div',
+    className: 'app',
+    template: _.template(template),
+    flashTemplate: _.template(flashTemplate),
+    events: {
+      'click #overlay': 'hideOverlay',
+    },
 
-            /**
-             * {@inheritdoc}
-             */
-            initialize: function () {
-                initLayout();
-                initSignin();
+    /**
+     * {@inheritdoc}
+     */
+    initialize: function() {
+      initLayout();
+      initSignin();
 
-                return BaseForm.prototype.initialize.apply(this, arguments);
-            },
+      return BaseForm.prototype.initialize.apply(this, arguments);
+    },
 
-            /**
-             * {@inheritdoc}
-             */
-            configure: function () {
-                return $.when(
-                    FetcherRegistry.initialize(),
-                    DateContext.initialize(),
-                    UserContext.initialize()
-                )
-                    .then(initTranslator.fetch)
-                    .then(function () {
-                        messenger.showQueuedMessages();
+    /**
+     * {@inheritdoc}
+     */
+    configure: function() {
+      this.listenTo(mediator, 'pim-app:overlay:show', this.showOverlay);
+      this.listenTo(mediator, 'pim-app:overlay:hide', this.hideOverlay);
 
-                        init();
+      return $.when(FetcherRegistry.initialize(), DateContext.initialize(), UserContext.initialize())
+        .then(initTranslator.fetch)
+        .then(
+          function() {
+            messenger.showQueuedMessages();
 
-                        pageTitle.set('Akeneo PIM');
+            init();
 
-                        return BaseForm.prototype.configure.apply(this, arguments);
-                    }.bind(this));
-            },
+            pageTitle.set('Akeneo PIM');
 
-            /**
-             * {@inheritdoc}
-             */
-            render: function () {
-                this.$el.html(this.template({}));
+            return BaseForm.prototype.configure.apply(this, arguments);
+          }.bind(this)
+        );
+    },
 
-                if (!Backbone.History.started) {
-                    Backbone.history.start();
-                }
+    /**
+     * {@inheritdoc}
+     */
+    render: function() {
+      this.$el.html(this.template({}));
 
-                return BaseForm.prototype.render.apply(this, arguments);
-            }
-        });
-    });
+      if (!Backbone.History.started) {
+        Backbone.history.start();
+      }
+
+      return BaseForm.prototype.render.apply(this, arguments);
+    },
+
+    showOverlay: function() {
+      this.$('#overlay').addClass('AknOverlay--show');
+    },
+
+    hideOverlay: function() {
+      this.$('#overlay').removeClass('AknOverlay--show');
+    },
+  });
+});
