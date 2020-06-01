86a6208e865781ab311111db58b85090c80bcd90
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-01 10:04:42 +0930

    GITHUB-APD-135: Add React component to display a communication channel panel

---
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/tests/front/__mocks__/oro/translator.ts
@@ -0,0 +1,1 @@
+export = jest.fn((key: string) => key);
---
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/tests/front/unit/components/panel/Panel.unit.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {render, screen, fireEvent} from '@testing-library/react';
+import {Panel} from 'akeneocommunicationchannel/components/panel/Panel';
+
+const mediator = require('oro/mediator');
+
+test('it shows the panel', () => {
+  render(<Panel />);
+
+  expect(screen.getByText('akeneo_communication_channel.panel.title')).toBeInTheDocument();
+});
+
+test('it can close the panel', () => {
+  render(<Panel />);
+
+  fireEvent.click(screen.getByTitle('akeneo_communication_channel.panel.button.close'));
+
+  expect(mediator.trigger).toHaveBeenCalledWith('communication-channel:panel:close');
+})
