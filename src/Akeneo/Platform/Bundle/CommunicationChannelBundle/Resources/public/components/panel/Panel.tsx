28aac13a3528d1fb6106320eed7e0036a2289028
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-01 10:04:42 +0930

    GITHUB-APD-135: Add React component to display a communication channel panel

---
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/public/components/panel/Panel.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const mediator = require('oro/mediator');
+const __ = require('oro/translator');
+
+const Header = styled.div`
+  margin-bottom: 50px;
+`;
+
+const Title = styled.div`
+  color: #9452BA;
+  font-size: 28px;
+  line-height: 41px;
+  float: left;
+`;
+
+const CloseButton = styled.div`
+  background: url(/bundles/pimui/images/icon-delete-slategrey.svg) no-repeat 50% 50%;
+  cursor: pointer;
+  border: none;
+  float: right;
+  margin-right: 10px;
+  width: 20px;
+  height: 50px;
+`;
+
+const Panel = () => {
+  const closePanel = () => {
+    mediator.trigger('communication-channel:panel:close');
+  };
+
+  return (
+    <>
+      <Header>
+        <Title>{__('akeneo_communication_channel.panel.title')}</Title>
+        <CloseButton title={__('akeneo_communication_channel.panel.button.close')} onClick={closePanel} />
+      </Header>
+    </>
+  );
+};
+
+export {Panel};
