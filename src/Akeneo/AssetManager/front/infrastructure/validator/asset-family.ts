704daca3e5f6c8153ebce87ca046960eaef79b60
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-03 09:46:59 +0930

    GITHUB-APD-132: Update dependencies and generate models + add workspace for communication-channel

--- src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
+++ src/Akeneo/AssetManager/front/infrastructure/validator/asset-family.ts
@@ -1,4 +1,4 @@
-import {validateAgainstSchema} from 'akeneoassetmanager/infrastructure/tools/validator';
+import {validateAgainstSchema} from '@akeneo-pim-community/shared';
 import {BackendAssetFamily} from 'akeneoassetmanager/infrastructure/model/asset-family';
 import schema from 'akeneoassetmanager/infrastructure/model/asset-family.schema.json';
 
--- src/Akeneo/AssetManager/front/infrastructure/validator/attribute.ts
+++ src/Akeneo/AssetManager/front/infrastructure/validator/attribute.ts
@@ -1,4 +1,4 @@
-import {validateAgainstSchema} from 'akeneoassetmanager/infrastructure/tools/validator';
+import {validateAgainstSchema} from '@akeneo-pim-community/shared';
 import {BackendAttribute} from 'akeneoassetmanager/infrastructure/model/attribute';
 import schema from 'akeneoassetmanager/infrastructure/model/attribute.schema.json';
 
