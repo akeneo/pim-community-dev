44be5d64f676356b9888db3cdcebb50ee7f38baf
Paul Chasle <paul.chasle@akeneo.com>
2020-06-03 18:14:07 +0200

    CXP-321: Fix e2e tests name

--- src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/ErrorManagement/CollectApiError/CollectProductDomainErrorEndToEnd.php
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/ErrorManagement/CollectApiError/CollectProductDomainErrorEndToEnd.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\Response;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class CollectDomainErrorFromProductEndpointEndToEnd extends ApiTestCase
+class CollectProductDomainErrorEndToEnd extends ApiTestCase
 {
     /** @var AttributeLoader */
     private $attributeLoader;
