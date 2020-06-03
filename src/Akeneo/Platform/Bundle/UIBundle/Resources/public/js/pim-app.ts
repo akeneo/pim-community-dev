07f2a90472f0aa89476e86a9c2ed96beeb67fdbe
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-03 10:17:44 +0930

    GITHUB-APD-135: Transform pim-app.js to typescript

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/pim-app.ts
@@ -0,0 +1,71 @@
+import _ from 'underscore';
+import Backbone from 'backbone';
+import BaseView = require('pimui/js/view/base');
+
+const messenger = require('oro/messenger');
+const mediator = require('oro/mediator');
+const FetcherRegistry = require('pim/fetcher-registry');
+const init = require('pim/init');
+const initTranslator = require('pim/init-translator');
+const initLayout = require('oro/init-layout');
+const initSignin = require('pimuser/js/init-signin');
+const pageTitle = require('pim/page-title');
+const DateContext = require('pim/date-context');
+const UserContext = require('pim/user-context');
+const template = require('pim/template/app');
+
+class PimApp extends BaseView {
+  private readonly template = _.template(template);
+
+  public events() {
+    return {
+      'click #overlay': 'hideOverlay',
+    };
+  }
+
+  constructor() {
+    super({tagName: 'div', className: 'app'});
+  }
+
+  public initialize(): void {
+    initLayout();
+    initSignin();
+  }
+
+  public configure() {
+    this.listenTo(mediator, 'pim-app:overlay:show', this.showOverlay);
+    this.listenTo(mediator, 'pim-app:overlay:hide', this.hideOverlay);
+
+    return $.when(FetcherRegistry.initialize(), DateContext.initialize(), UserContext.initialize())
+      .then(initTranslator.fetch)
+      .then(() => {
+        messenger.showQueuedMessages();
+
+        init();
+
+        pageTitle.set('Akeneo PIM');
+
+        return super.configure();
+      });
+  }
+
+  public render(): BaseView {
+    this.$el.html(this.template({}));
+
+    if (!Backbone.History.started) {
+      Backbone.history.start();
+    }
+
+    return BaseView.prototype.render.apply(this, arguments);
+  }
+
+  private showOverlay(): void {
+    this.$('#overlay').addClass('AknOverlay--show');
+  }
+
+  private hideOverlay(): void {
+    this.$('#overlay').removeClass('AknOverlay--show');
+  }
+}
+
+export = PimApp;
