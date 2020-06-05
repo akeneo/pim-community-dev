f45465870d62a02bab785ca376e956be060b1b6c
Mathias Métayer <mathias.metayer@akeneo.com>
2020-06-05 09:45:16 +0200

    RUL-276: ObjectNotFoundException is a LogicException

--- src/Akeneo/Pim/Enrichment/Component/Product/Exception/ObjectNotFoundException.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Exception/ObjectNotFoundException.php
@@ -9,7 +9,7 @@ namespace Akeneo\Pim\Enrichment\Component\Product\Exception;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ObjectNotFoundException extends \Exception
+class ObjectNotFoundException extends \LogicException
 {
     /**
      * @param string     $message
