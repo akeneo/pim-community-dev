c02be323d4d7117b872f651db5d4f4ed483a8bf2
Nicolas Maligne <nicolas.maligne@akeneo.com>
2020-06-05 16:28:12 +0200

    RUL-144: Add Select your category

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/AddToCategoryActionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/AddToCategoryActionLine.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { AddToCategoryAction } from '../../../../models/actions';
+import React, { useState, useEffect } from 'react';
 import { ActionTemplate } from './ActionTemplate';
+import { AddToCategoryAction } from '../../../../models/actions';
 import { ActionLineProps } from './ActionLineProps';
+import { getCategoriesTrees } from '../../../../components/CategoryTree/category-tree.getters';
+import { NetworkLifeCycle } from '../../../../components/CategoryTree/hooks/NetworkLifeCycle.types';
+import { CategoryTreeModel } from '../../../../components/CategoryTree/category-tree.types';
+import { Select2SimpleSyncWrapper } from '../../../../components/Select2Wrapper';
+import { AkeneoSpinner } from '../../../../components/AkeneoSpinner';
 
 type Props = {
   action: AddToCategoryAction;
@@ -19,6 +24,33 @@ const AddToCategoryActionLine: React.FC<Props> = ({
   console.log('action', action);
   console.log('router', router);
   console.log('currentCatalogLocale', currentCatalogLocale);
+
+  const [categoriesTrees, setCategoriesTrees] = useState<
+    NetworkLifeCycle<CategoryTreeModel[]>
+  >({
+    status: 'PENDING',
+    data: [],
+  });
+
+  const [categoryTreesSelected, setCategoryTreesSelected] = useState<
+    Map<string, string[]>
+  >(new Map());
+
+  useEffect(() => {
+    const initAddToCategoryData = async () => {
+      await getCategoriesTrees(setCategoriesTrees);
+    };
+    initAddToCategoryData();
+  }, []);
+
+  const handleAddCategoryTree = (event: any) => {
+    const tmpCategoryTreesSelected = new Map(categoryTreesSelected);
+    const key: string = event.val;
+    if (!tmpCategoryTreesSelected.has(key)) {
+      tmpCategoryTreesSelected.set(key, []);
+    }
+    setCategoryTreesSelected(tmpCategoryTreesSelected);
+  };
 
   return (
     <ActionTemplate
@@ -38,14 +70,42 @@ const AddToCategoryActionLine: React.FC<Props> = ({
             style={{ color: '#9452BA', fontSize: '15px', padding: '10px 0' }}>
             Select your category trees
           </legend>
-          <div>Category tree (required)</div>
+          <label htmlFor='add_category_select_tree'>
+            Category tree (required)
+          </label>
+          {Array.from(categoryTreesSelected.keys()).map(code => {
+            console.log({ code });
+            return (
+              <button key={code} type='button'>
+                {code}
+              </button>
+            );
+          })}
+          {categoriesTrees.status === 'PENDING' ? (
+            // Replace with a proper select loader component
+            <AkeneoSpinner />
+          ) : (
+            <Select2SimpleSyncWrapper
+              id='add_category_select_tree'
+              label='Category tree (required)'
+              hiddenLabel
+              onSelecting={handleAddCategoryTree}
+              data={
+                categoriesTrees.data?.map(categoryTree => ({
+                  id: categoryTree.code,
+                  text: categoryTree.code,
+                })) || []
+              }
+              placeholder='Select you category tree'
+            />
+          )}
         </fieldset>
         <fieldset style={{ width: '50%' }}>
           <legend
             style={{ color: '#9452BA', fontSize: '15px', padding: '10px 0' }}>
             Select your categories for master catalog
           </legend>
-          <div>Categories (required)</div>
+          <label>Categories (required)</label>
         </fieldset>
       </div>
     </ActionTemplate>
