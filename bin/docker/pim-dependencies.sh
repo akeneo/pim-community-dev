766236a69fbd541db6341f98217ba52763ff8756
jmleroux <jmleroux.pro@gmail.com>
2020-06-09 15:45:58 +0200

    Do not download Chromium with yarn

--- bin/docker/pim-dependencies.sh
+++ bin/docker/pim-dependencies.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker-compose exec fpm php -d memory_limit=3G /usr/local/bin/composer install
-docker-compose run --rm -e PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 -e PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome node yarn install
+docker-compose run --rm -e PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 node yarn install
