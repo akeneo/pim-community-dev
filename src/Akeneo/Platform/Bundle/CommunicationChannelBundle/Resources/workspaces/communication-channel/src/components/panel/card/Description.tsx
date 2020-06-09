ea768c2db1c78d3856be50465c93952431b9fca2
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-09 10:18:48 +0930

    GITHUB-APD-133: Add react-markdown to convert the description written in markdown to html

--- src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/workspaces/communication-channel/src/components/panel/card/Description.tsx
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/Resources/workspaces/communication-channel/src/components/panel/card/Description.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
 import {AkeneoThemedProps} from '@akeneo-pim-community/shared';
 import {Tag} from './../../../components/panel/card/Tag';
 
@@ -9,7 +10,9 @@ type DesciptionProps = {
 };
 
 const Description = (props: DesciptionProps & any): JSX.Element => (
-  <StyledDescription {...props}>{props.description}</StyledDescription>
+  <StyledDescription {...props}>
+    <ReactMarkdown source={props.description} />
+  </StyledDescription>
 );
 
 const StyledDescription = styled.div`
