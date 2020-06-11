5586489ad7af44cbceeff1de0f1882d776a4e622
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-06-11 11:19:09 +0200

    DAPI: fix option deletion confirmation modal

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/DeleteConfirmationModal.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/DeleteConfirmationModal.tsx
@@ -19,20 +19,21 @@ const DeleteConfirmationModal = ({attributeOptionCode, confirmDelete, cancelDele
                     </div>
                     <div>
                         <div className="AknFullPage-titleContainer">
+                            <div className="AknFullPage-subTitle">{translate('pim_enrich.entity.attribute.plural_label')}</div>
                             <div className="AknFullPage-title">
-                                {translate('pim_enrich.entity.fallback.module.delete.title', {'itemName': attributeOptionCode})}
+                                {translate('pim_enrich.entity.attribute_option.module.edit.delete_confirmation_title_msg', {'optionCode': attributeOptionCode})}
                             </div>
                             <div className="AknFullPage-description">
-                                {translate('pim_enrich.entity.fallback.module.delete.item_placeholder', {'itemName': attributeOptionCode})}
+                                {translate('pim_enrich.entity.attribute_option.module.edit.delete_confirmation_subtitle_msg')}
                             </div>
                         </div>
                         <div className="modal-body"/>
                         <div className="AknButtonList">
-                            <div className="AknButton AknButton--grey AknButtonList-item cancel" onClick={cancelDelete} role="attribute-option-confirm-cancel-button">
-                                {translate('pim_common.cancel')}
-                            </div>
                             <div title="Delete" className="AknButton AknButtonList-item AknButton--apply AknButton--important ok" onClick={confirmDelete} role="attribute-option-confirm-delete-button">
                                 {translate('pim_common.delete')}
+                            </div>
+                            <div className="AknButton AknButton--grey AknButtonList-item cancel" onClick={cancelDelete} role="attribute-option-confirm-cancel-button">
+                                {translate('pim_common.cancel')}
                             </div>
                         </div>
                     </div>
