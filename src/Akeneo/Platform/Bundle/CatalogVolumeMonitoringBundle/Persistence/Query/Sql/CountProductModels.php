c92f289e9b0d76b4fcabe6ce75927dee93be1a00
Mathias Métayer <mathias.metayer@akeneo.com>
2020-06-18 10:16:39 +0200

    PIM-9306: Update service definitions

--- src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/Persistence/Query/Sql/CountProductModels.php
+++ src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/Persistence/Query/Sql/CountProductModels.php
@@ -12,6 +12,8 @@ use Doctrine\DBAL\Connection;
  * @author    Elodie Raposo <elodie.raposo@akeneo.com>
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @deprecated Please use Akeneo\Platform\Bundle\CatalogVolumeMonitoringBundle\Persistence\Query\Elasticsearch\CountProductModels
  */
 class CountProductModels implements CountQuery
 {
