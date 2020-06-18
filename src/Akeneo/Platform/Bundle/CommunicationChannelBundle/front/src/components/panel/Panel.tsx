3b5a464803763f8ff52543bdb6e622ed24ddba91
christophe-chausseray <chausseray.christophe@gmail.com>
2020-06-18 16:41:20 +0930

    GITHUB-APD-152: Add error message when we cannot get the announcements

--- src/Akeneo/Platform/Bundle/CommunicationChannelBundle/front/src/components/panel/Panel.tsx
+++ src/Akeneo/Platform/Bundle/CommunicationChannelBundle/front/src/components/panel/Panel.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import {useTranslate, useMediator} from '@akeneo-pim-community/legacy-bridge';
-import {useAnnouncements} from './../../hooks/useAnnouncements';
 import {usePimVersion} from '../../hooks/usePimVersion';
-import {HeaderPanel} from './../../components/panel/Header';
-import {AnnouncementComponent, EmptyAnnouncementList} from './announcement';
-import {Announcement} from './../../models/announcement';
+import {HeaderPanel} from './Header';
+import {ListAnnouncement} from './ListAnnouncement';
+import {EmptyAnnouncementList} from './announcement';
 import {formatCampaign} from '../../tools/formatCampaign';
-
-const ListAnnouncement = styled.ul`
-  margin: 74px 30px 0 30px;
-`;
 
 const Panel = (): JSX.Element => {
   const __ = useTranslate();
   const mediator = useMediator();
   const pimVersion = usePimVersion();
-  const announcements = useAnnouncements();
   const cloudEEVersion = 'serenity';
   const campaign = null !== pimVersion.data ? formatCampaign(pimVersion.data.edition, pimVersion.data.version) : '';
   const isSerenity = null !== pimVersion.data && cloudEEVersion === pimVersion.data.edition.toLowerCase();
@@ -29,11 +22,7 @@ const Panel = (): JSX.Element => {
     <>
       <HeaderPanel title={__('akeneo_communication_channel.panel.title')} onClickCloseButton={closePanel} />
       {isSerenity ? (
-        <ListAnnouncement>
-          {announcements.data.map((announcement: Announcement, index: number): JSX.Element =>
-            <AnnouncementComponent announcement={announcement} key={index} campaign={campaign} />)
-          }
-        </ListAnnouncement>
+        <ListAnnouncement campaign={campaign} />
       ) : (
         <EmptyAnnouncementList text={__('akeneo_communication_channel.panel.list.empty')} />
       )}
