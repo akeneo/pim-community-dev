922c5c47d17120c05c4992df045418eb6ebf36dc
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-06-22 22:33:07 +0200

    CXP-339 - add Locale Label

--- src/Akeneo/Connectivity/Connection/front/src/error-management/components/ErrorList/ErrorList.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/error-management/components/ErrorList/ErrorList.tsx
@@ -1,13 +1,15 @@
-import React, {FC, useState} from 'react';
-import {Table, TableHeaderCell, TableHeaderRow, TableRow} from '../../../common';
-import {Translate} from '../../../shared/translate';
-import {ConnectionError} from '../../model/ConnectionError';
-import {ErrorDateTimeCell} from './ErrorDateTimeCell';
-import {ErrorDetailsCell} from './ErrorDetailsCell';
-import {ErrorMessageCell} from './ErrorMessageCell';
-import {NoError} from './NoError';
-import {SearchFilter} from './SearchFilter';
-import {Order, SortButton} from './SortButton';
+import React, { FC, useState } from 'react';
+import { Table, TableHeaderCell, TableHeaderRow, TableRow } from '../../../common';
+import { Translate } from '../../../shared/translate';
+import { LocaleContext } from '../../../shared/locale/locale-context';
+import { useLocale } from '../../../shared/locale/use-locale';
+import { ConnectionError } from '../../model/ConnectionError';
+import { ErrorDateTimeCell } from './ErrorDateTimeCell';
+import { ErrorDetailsCell } from './ErrorDetailsCell';
+import { ErrorMessageCell } from './ErrorMessageCell';
+import { NoError } from './NoError';
+import { SearchFilter } from './SearchFilter';
+import { Order, SortButton } from './SortButton';
 
 const sortingByTimestamp = (sortOrder: Order) => {
     if ('asc' === sortOrder) {
@@ -25,9 +27,11 @@ type Props = {
     errors: ConnectionError[];
 };
 
-const ErrorList: FC<Props> = ({errors}) => {
+export const ErrorList: FC<Props> = ({errors}) => {
     const [sortOrder, setSortOrder] = useState<Order>('desc');
     const [searchValue, setSearchValue] = useState<string>('');
+
+    const {locales} = useLocale();
 
     const sortedAndfilteredErrors = errors
         .sort(sortingByTimestamp(sortOrder))
@@ -38,37 +42,37 @@ const ErrorList: FC<Props> = ({errors}) => {
             <SearchFilter value={searchValue} onSearch={setSearchValue} resultCount={sortedAndfilteredErrors.length} />
 
             {errors.length > 0 ? (
-                <Table>
-                    <thead>
-                        <TableHeaderRow>
-                            <TableHeaderCell>
-                                <SortButton order={sortOrder} onSort={setSortOrder}>
-                                    <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.date_time_column.title' />
-                                </SortButton>
-                            </TableHeaderCell>
-                            <TableHeaderCell>
-                                <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.content_column.title' />
-                            </TableHeaderCell>
-                            <TableHeaderCell>
-                                <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.details_column.title' />
-                            </TableHeaderCell>
-                        </TableHeaderRow>
-                    </thead>
-                    <tbody>
-                        {sortedAndfilteredErrors.map(error => (
-                            <TableRow key={error.id}>
-                                <ErrorDateTimeCell timestamp={error.timestamp} />
-                                <ErrorMessageCell content={error.content} />
-                                <ErrorDetailsCell content={error.content} />
-                            </TableRow>
-                        ))}
-                    </tbody>
-                </Table>
+                <LocaleContext.Provider value={locales}>
+                    <Table>
+                        <thead>
+                            <TableHeaderRow>
+                                <TableHeaderCell>
+                                    <SortButton order={sortOrder} onSort={setSortOrder}>
+                                        <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.date_time_column.title' />
+                                    </SortButton>
+                                </TableHeaderCell>
+                                <TableHeaderCell>
+                                    <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.content_column.title' />
+                                </TableHeaderCell>
+                                <TableHeaderCell>
+                                    <Translate id='akeneo_connectivity.connection.error_management.connection_monitoring.error_list.details_column.title' />
+                                </TableHeaderCell>
+                            </TableHeaderRow>
+                        </thead>
+                        <tbody>
+                            {sortedAndfilteredErrors.map(error => (
+                                <TableRow key={error.id}>
+                                    <ErrorDateTimeCell timestamp={error.timestamp} />
+                                    <ErrorMessageCell content={error.content} />
+                                    <ErrorDetailsCell content={error.content} />
+                                </TableRow>
+                            ))}
+                        </tbody>
+                    </Table>
+                </LocaleContext.Provider>
             ) : (
                 <NoError />
             )}
         </>
     );
 };
-
-export {ErrorList};
