f99b88d19254c1250cdb26429b5304ee819737a1
ahocquard <alexandre.hocquard@akeneo.com>
2020-06-22 12:27:22 +0200

    GITHUB-APD-158: remove the version name when creating an index, as it's useless

--- .circleci/detect_structure_changes.sh
+++ .circleci/detect_structure_changes.sh
@@ -139,6 +139,6 @@ echo "Compare database 40+PR migrations from database PR..."
 diff /tmp/structure_changes/dump_40_database_with_migrations.sql /tmp/structure_changes/dump_branch_database.sql --context=10
 
 echo "Compare index 40+PR migrations from index PR..."
-sed -i -r 's/[0-9]+_[0-9]+_[0-9]+_[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/version_uuid/g' /tmp/structure_changes/dump_40_index_with_migrations.json
-sed -i -r 's/[0-9]+_[0-9]+_[0-9]+_[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/version_uuid/g' /tmp/structure_changes/dump_branch_index.json
+sed -i -r 's/([0-9]+_[0-9]+_[0-9]+_)?[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/version_uuid/g' /tmp/structure_changes/dump_40_index_with_migrations.json
+sed -i -r 's/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/version_uuid/g' /tmp/structure_changes/dump_branch_index.json
 diff /tmp/structure_changes/dump_40_index_with_migrations.json /tmp/structure_changes/dump_branch_index.json --context=10
