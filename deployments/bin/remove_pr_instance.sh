4d200ac732fe3bdbbe0aa3eb109aa6c8c50db0a7
franckbrouard <48205166+franckbrouard@users.noreply.github.com>
2020-06-23 16:13:28 +0200

    TIP-1481 - delete environments in error (#9015)
    * TIP-1481
    Co-authored-by: matthieudelaro <matthieudelaro@users.noreply.github.com>

--- deployments/bin/remove_pr_instance.sh
+++ deployments/bin/remove_pr_instance.sh
@@ -1,10 +1,22 @@
-#!/bin/bash -eo pipefail
+#!/bin/bash 
 
 for namespace in $(kubectl get ns | egrep 'srnt-pimci|srnt-pimup' | awk '{print $1}'); do
     NS_INFO=($(kubectl get ns | grep ${namespace}))
+    NAMESPACE=$(echo ${NS_INFO[0]})
     NS_STATUS=$(echo ${NS_INFO[1]})
     NS_AGE=$(echo ${NS_INFO[2]})
     INSTANCE_NAME=$(echo ${NS_INFO[0]//srnt-/})
+    # delete environments that failed (not automatically removed by circleCI)
+    if [[ ${NAMESPACE} == *srnt-pimup* ]]; then
+        if [[ ${NS_AGE} == *h* ]]; then
+            echo "---[TODELETE] namespace ${namespace} with status ${NS_STATUS} since ${NS_AGE} (instance_name=${INSTANCE_NAME})"
+            if [ $(gsutil ls gs://akecld-terraform-dev/saas/akecld-saas-dev/europe-west3-a/srnt-${INSTANCE_NAME}/default.tflock 2>/dev/null) ]; then
+                gsutil rm gs://akecld-terraform-dev/saas/akecld-saas-dev/europe-west3-a/srnt-${INSTANCE_NAME}/default.tflock
+            fi
+            INSTANCE_NAME=${INSTANCE_NAME} make create-ci-release-files delete
+        fi
+    fi
+    # delete environments older than 24 hours
     if [[ ${NS_AGE} == *h* ]]; then
         if [[ ${#NS_AGE} -eq 3 ]]; then
             #ex : 23h
