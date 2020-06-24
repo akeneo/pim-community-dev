2b9085d7a0ca3630a220d704bd20532a138c8e99
Tseho <quentin.favrie@gmail.com>
2020-06-24 15:54:04 +0200

    RAC-86: New export job type for shared catalogs

---
+++ src/Akeneo/SharedCatalog/Resources/public/form/common/edit-form.js
@@ -0,0 +1,28 @@
+'use strict';
+
+define(
+  [
+    'pim/form',
+    'pim/router'
+  ],
+  function (
+    BaseForm,
+    router
+  ) {
+    return BaseForm.extend({
+      initialize: function (options) {
+        options = options || {};
+
+        this.config = options.config;
+      },
+      render: function () {
+        let jobInstance = $.extend(true, {}, this.getFormData());
+
+        router.redirectToRoute(
+          this.config.redirectPath,
+          {code: jobInstance.code}
+        );
+      }
+    });
+  }
+);
