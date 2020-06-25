43e78da2dc46373112cc9c20e3af98bfb71b87e4
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-06-25 11:44:35 +0200

    CXP-309 : type region

--- src/Akeneo/Connectivity/Connection/front/src/common/components/Flag.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/common/components/Flag.tsx
@@ -2,17 +2,15 @@ import React, {FC, useContext} from 'react';
 import {LocaleContext} from '../../shared/locale/locale-context';
 
 type Props = {
-    locale: string | undefined;
+    locale: string;
 };
 
 const Flag: FC<Props> = ({locale}) => {
     const region = locale?.split('_')[locale.split('_').length - 1];
-
     const locales = useContext(LocaleContext);
-
     const foundLocaleLabel = locales?.find(loc => loc.code === locale);
 
-    if (undefined !== foundLocaleLabel && undefined !== region) {
+    if (undefined !== foundLocaleLabel && 'string' === typeof region) {
         return (
             <>
                 <i className={`flag flag-${region.toLowerCase()}`} /> {foundLocaleLabel.language}
