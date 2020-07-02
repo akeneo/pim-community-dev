1f1f014eb379501fa218848495f0d17e27fb9d52
anaisbaunelemaire <anais.lemaire@akeneo.com>
2020-07-02 14:17:23 +0000

    Preparing version 3.2.62

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.61';
+    const VERSION = '3.2.62';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
