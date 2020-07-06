e1f04f74b6f018c2d67075a4a4668726f8c6346d
pierallard <pierallard@gmail.com>
2020-07-06 11:36:25 +0200

    RUL-292: Add ACL migration

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/IndexRules/create-button.ts
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/IndexRules/create-button.ts
@@ -1,28 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
 const BaseForm = require('pimui/js/view/base');
-import { EventsHash } from "backbone";
+import { EventsHash } from 'backbone';
 const Router = require('pim/router');
 const __ = require('oro/translator');
 
 class CreateButton extends BaseForm {
   private config: any;
 
-  constructor(options: {config: any}) {
-    super({ ...options, className: 'AknButton AknButtonList-item AknButton--apply' });
+  constructor(options: { config: any }) {
+    super({
+      ...options,
+      className: 'AknButton AknButtonList-item AknButton--apply',
+    });
     this.config = options.config;
   }
 
   public events(): EventsHash {
     return {
-      'click': (event: any) => {
+      click: (event: any) => {
         event.preventDefault();
         Router.redirectToRoute(this.config.url);
       },
     };
   }
 
-  public render(): BaseForm {
+  public render() {
     this.$el.html(__(this.config.title));
 
     return this;
