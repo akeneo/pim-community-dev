646f986c15d97eb51c2db49440ca3072524c0203
Steven VAIDIE <steven.vaidie@gmail.com>
2020-07-09 18:23:30 +0200

    RAC-119: fix product cannot be associated with product not already create

--- tests/back/Pim/Enrichment/Integration/Storage/Sql/ProductModel/QuantifiedAssociation/GetProductModelQuantifiedAssociationsByProductModelCodesIntegration.php
+++ tests/back/Pim/Enrichment/Integration/Storage/Sql/ProductModel/QuantifiedAssociation/GetProductModelQuantifiedAssociationsByProductModelCodesIntegration.php
@@ -79,6 +79,7 @@ class GetProductModelQuantifiedAssociationsByProductModelCodesIntegration extend
      */
     public function itReturnQuantifiedAssociationWithProductModelsOnMultipleProductModels()
     {
+        $this->getEntityBuilder()->createProductModel('productModelB', 'familyVariantWithTwoLevels', null, []);
         $rootProductModel = $this->getEntityBuilder()->createProductModel('root_product_model', 'familyVariantWithTwoLevels', null, [
             "quantified_associations" => [
                 'PRODUCT_SET' => [
@@ -91,7 +92,6 @@ class GetProductModelQuantifiedAssociationsByProductModelCodesIntegration extend
         $this->getEntityBuilder()->createProductModel('sub_product_model_1', 'familyVariantWithTwoLevels', $rootProductModel, []);
 
         $this->getEntityBuilder()->createProductModel('productModelA', 'familyVariantWithTwoLevels', null, []);
-        $this->getEntityBuilder()->createProductModel('productModelB', 'familyVariantWithTwoLevels', null, []);
         $this->getEntityBuilder()->createProductModel('productModelC', 'familyVariantWithTwoLevels', null, [
             'quantified_associations' => [
                 'PRODUCT_SET' => [
@@ -143,6 +143,7 @@ class GetProductModelQuantifiedAssociationsByProductModelCodesIntegration extend
      */
     public function itReturnsTheQuantifiedAssociationOfTheChildrenWhenDesynchronizedWithTheParent()
     {
+        $this->getEntityBuilder()->createProductModel('productModelA', 'familyVariantWithTwoLevels', null, []);
         $rootProductModel = $this->getEntityBuilder()->createProductModel('root_product_model', 'familyVariantWithTwoLevels', null, [
             "quantified_associations" => [
                 'PRODUCT_SET' => [
@@ -152,7 +153,6 @@ class GetProductModelQuantifiedAssociationsByProductModelCodesIntegration extend
                 ],
             ]
         ]);
-        $this->getEntityBuilder()->createProductModel('productModelA', 'familyVariantWithTwoLevels', null, []);
         $this->getEntityBuilder()->createProductModel('productModelB', 'familyVariantWithTwoLevels', $rootProductModel, [
             'quantified_associations' => [
                 'PRODUCT_SET' => [
