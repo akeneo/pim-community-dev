badf4eb1f73becc10f0814727fd884b1e77ade90
Julien Sanchez <sumbobyboys@gmail.com>
2020-07-10 15:32:22 +0200

    do not merge

--- src/Akeneo/Tool/Bundle/MeasureBundle/tests/front/unit/hooks/use-measurement-family-remover.unit.tsx
+++ src/Akeneo/Tool/Bundle/MeasureBundle/tests/front/unit/hooks/use-measurement-family-remover.unit.tsx
@@ -16,43 +16,43 @@ afterEach(() => {
   delete global.fetch;
 });
 
-test('It returns a success response when removing', async () => {
-  const fetchMock = jest.fn().mockImplementation(() => ({
-    status: 204,
-  }));
-  global.fetch = fetchMock;
+// test('It returns a success response when removing', async () => {
+//   const fetchMock = jest.fn().mockImplementation(() => ({
+//     status: 204,
+//   }));
+//   global.fetch = fetchMock;
 
-  const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
-  const remove = result.current;
+//   const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
+//   const remove = result.current;
 
-  expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.Success);
-  expect(fetchMock).toHaveBeenCalledWith('akeneo_measurements_measurement_family_delete_rest', {
-    headers: [['X-Requested-With', 'XMLHttpRequest']],
-    method: 'DELETE',
-  });
-});
+//   expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.Success);
+//   expect(fetchMock).toHaveBeenCalledWith('akeneo_measurements_measurement_family_delete_rest', {
+//     headers: [['X-Requested-With', 'XMLHttpRequest']],
+//     method: 'DELETE',
+//   });
+// });
 
-test('It returns a not found response when removing an unknown measurement family', async () => {
-  global.fetch = jest.fn().mockImplementation(() => ({
-    status: 404,
-  }));
+// test('It returns a not found response when removing an unknown measurement family', async () => {
+//   global.fetch = jest.fn().mockImplementation(() => ({
+//     status: 404,
+//   }));
 
-  const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
-  const remove = result.current;
+//   const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
+//   const remove = result.current;
 
-  expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.NotFound);
-});
+//   expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.NotFound);
+// });
 
-test('It returns a not allowed response when removing a read-only measurement family', async () => {
-  global.fetch = jest.fn().mockImplementation(() => ({
-    status: 422,
-  }));
+// test('It returns a not allowed response when removing a read-only measurement family', async () => {
+//   global.fetch = jest.fn().mockImplementation(() => ({
+//     status: 422,
+//   }));
 
-  const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
-  const remove = result.current;
+//   const {result} = renderHook(() => useMeasurementFamilyRemover(), {wrapper});
+//   const remove = result.current;
 
-  expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.Unprocessable);
-});
+//   expect(await remove('custom_metric')).toEqual(MeasurementFamilyRemoverResult.Unprocessable);
+// });
 
 test('An error is thrown if the server does not respond correctly', async () => {
   global.fetch = jest.fn().mockImplementation(() => ({
