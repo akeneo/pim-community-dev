0d8730cbfaa3e2463d205c0cb8f357c17b41304f
Steven VAIDIE <steven.vaidie@gmail.com>
2020-07-13 10:35:58 +0200

    fix: missing rename phpspec file name

--- tests/back/Pim/Enrichment/Specification/Component/Product/Model/QuantifiedAssociation/QuantifiedAssociationCollectionSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Model/QuantifiedAssociation/QuantifiedAssociationCollectionSpec.php
@@ -13,7 +13,7 @@ use PhpSpec\ObjectBehavior;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class QuantifiedAssociationsSpec extends ObjectBehavior
+class QuantifiedAssociationCollectionSpec extends ObjectBehavior
 {
     public function let()
     {
