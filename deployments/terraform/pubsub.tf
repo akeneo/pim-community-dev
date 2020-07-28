3f10cfae9eb0101919259275d2e1d8e7125e0c13
franckbrouard <franck.brouard@akeneo.fr>
2020-07-28 11:27:58 +0200

    BH-4 - add dependencies between resources & rights

--- deployments/terraform/pubsub.tf
+++ deployments/terraform/pubsub.tf
@@ -49,28 +49,60 @@ resource "google_pubsub_subscription" "job-queue" {
 // We do declare 2 kinds of applications, the one that writes and the one that
 // reads. It gives rights to the same service account: PIM.
 
+resource "google_service_account" "pimpubsub" {
+  account_id   = format("gs-%s", substr(md5(var.instance_name), 0, 25))
+  display_name = "Google access for pim container (${var.instance_name})"
+}
+
+resource "google_service_account_key" "pimpubsub" {
+  service_account_id = google_service_account.pimstorage.id
+  public_key_type    = "TYPE_X509_PEM_FILE"
+
+  depends_on = [google_service_account.pimpubsub]
+}
+
+
 
 resource "google_pubsub_topic_iam_member" "pubsub_publisher_business-event" {
   topic  = google_pubsub_topic.business-event.name
   role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_service_account.pim.email}"
+  member = "serviceAccount:${google_service_account.pimpubsub.email}"
+
+  depends_on = [
+    google_service_account.pimpubsub,
+    google_pubsub_topic.business-event,
+  ]
 }
 
 resource "google_pubsub_topic_iam_member" "pubsub_publisher_job_queue" {
   topic  = google_pubsub_topic.job-queue.name
   role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_service_account.pim.email}"
+  member = "serviceAccount:${google_service_account.pimpubsub.email}"
+
+  depends_on = [
+    google_service_account.pimpubsub,
+    google_pubsub_topic.job-queue,
+  ]
 }
 
 resource "google_pubsub_topic_iam_member" "pubsub_subscriber_webhook" {
   topic  = google_pubsub_subscription.webhook.name
   role   = "roles/pubsub.subsriber"
-  member = "serviceAccount:${google_service_account.pim.email}"
+  member = "serviceAccount:${google_service_account.pimpubsub.email}"
+
+  depends_on = [
+    google_service_account.pimpubsub,
+    google_pubsub_subscription.webhook,
+  ]
 }
 
 resource "google_pubsub_topic_iam_member" "pubsub_subscriber_job_queue" {
   topic  = google_pubsub_subscription.job-queue.name
   role   = "roles/pubsub.subsriber"
-  member = "serviceAccount:${google_service_account.pim.email}"
-}
+  member = "serviceAccount:${google_service_account.pimpubsub.email}"
 
+  depends_on = [
+    google_service_account.pimpubsub,
+    google_pubsub_subscription.job-queue,
+  ]
+}
