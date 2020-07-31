754894242793d9536131ab2563e4c1deb46f4488
Mathias Métayer <mathias.metayer@akeneo.com>
2020-07-31 16:47:40 +0200

    RUL-330: Error handling

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/conditions/StatusConditionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/conditions/StatusConditionLine.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Controller } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 import { ConditionLineProps } from './ConditionLineProps';
 import { StatusOperators } from '../../../../models/conditions';
 import { OperatorSelector } from '../../../../components/Selectors/OperatorSelector';
 import { Operator } from '../../../../models/Operator';
-import { FieldColumn, OperatorColumn, ValueColumn } from './style';
+import {
+  AttributeConditionLineFormAndErrorsContainer,
+  AttributeConditionLineFormContainer,
+  FieldColumn,
+  OperatorColumn,
+  ValueColumn,
+} from './style';
 import { StatusSelector } from '../../../../components/Selectors/StatusSelector';
 import { LineErrors } from '../LineErrors';
 import { useTranslate } from '../../../../dependenciesTools/hooks';
@@ -15,6 +21,7 @@ const INIT_OPERATOR = Operator.EQUALS;
 
 const StatusConditionLine: React.FC<ConditionLineProps> = ({ lineNumber }) => {
   const translate = useTranslate();
+  const { errors } = useFormContext();
   const {
     fieldFormName,
     operatorFormName,
@@ -24,59 +31,68 @@ const StatusConditionLine: React.FC<ConditionLineProps> = ({ lineNumber }) => {
     setValueFormValue,
   } = useControlledFormInputCondition<boolean>(lineNumber);
 
+  const isElementInError = (element: string): boolean =>
+    typeof errors?.content?.conditions?.[lineNumber]?.[element] === 'object';
+
   return (
-    <div className={'AknGrid-bodyCell'}>
-      <Controller
-        as={<input type='hidden' />}
-        name={fieldFormName}
-        defaultValue='enabled'
-      />
-      <FieldColumn
-        className={'AknGrid-bodyCell--highlight'}
-        title={translate('pimee_catalog_rule.form.edit.fields.enabled')}>
-        {translate('pimee_catalog_rule.form.edit.fields.enabled')}
-      </FieldColumn>
-      <OperatorColumn>
+    <AttributeConditionLineFormAndErrorsContainer
+      className={'AknGrid-bodyCell'}>
+      <AttributeConditionLineFormContainer>
         <Controller
-          as={OperatorSelector}
-          availableOperators={StatusOperators}
-          data-testid={`edit-rules-input-${lineNumber}-operator`}
-          hiddenLabel
-          name={operatorFormName}
-          defaultValue={getOperatorFormValue() ?? INIT_OPERATOR}
-          value={getOperatorFormValue()}
+          as={<input type='hidden' />}
+          name={fieldFormName}
+          defaultValue='enabled'
         />
-      </OperatorColumn>
-      <ValueColumn>
-        <Controller
-          name={valueFormName}
-          as={<span hidden />}
-          defaultValue={getValueFormValue()}
-          rules={{
-            validate: value => {
-              return (
-                typeof value === 'boolean' ||
-                translate('pimee_catalog_rule.exceptions.required_value')
-              );
-            },
-          }}
-        />
-        <StatusSelector
-          id={`edit-rules-input-${lineNumber}-value`}
-          name={valueFormName}
-          label={`${translate('pim_common.status')} ${translate(
-            'pim_common.required_label'
-          )}`}
-          placeholder={translate(
-            'pimee_catalog_rule.form.edit.actions.set_status.placeholder'
-          )}
-          hiddenLabel={true}
-          value={getValueFormValue()}
-          onChange={setValueFormValue}
-        />
-      </ValueColumn>
+        <FieldColumn
+          className={'AknGrid-bodyCell--highlight'}
+          title={translate('pimee_catalog_rule.form.edit.fields.enabled')}>
+          {translate('pimee_catalog_rule.form.edit.fields.enabled')}
+        </FieldColumn>
+        <OperatorColumn>
+          <Controller
+            as={OperatorSelector}
+            availableOperators={StatusOperators}
+            data-testid={`edit-rules-input-${lineNumber}-operator`}
+            hiddenLabel
+            name={operatorFormName}
+            defaultValue={getOperatorFormValue() ?? INIT_OPERATOR}
+            value={getOperatorFormValue()}
+          />
+        </OperatorColumn>
+        <ValueColumn
+          className={
+            isElementInError('value') ? 'select2-container-error' : ''
+          }>
+          <Controller
+            name={valueFormName}
+            as={<span hidden />}
+            defaultValue={getValueFormValue()}
+            rules={{
+              validate: value => {
+                return (
+                  typeof value === 'boolean' ||
+                  translate('pimee_catalog_rule.exceptions.required')
+                );
+              },
+            }}
+          />
+          <StatusSelector
+            id={`edit-rules-input-${lineNumber}-value`}
+            name={valueFormName}
+            label={`${translate('pim_common.status')} ${translate(
+              'pim_common.required_label'
+            )}`}
+            placeholder={translate(
+              'pimee_catalog_rule.form.edit.actions.set_status.placeholder'
+            )}
+            hiddenLabel={true}
+            value={getValueFormValue()}
+            onChange={setValueFormValue}
+          />
+        </ValueColumn>
+      </AttributeConditionLineFormContainer>
       <LineErrors lineNumber={lineNumber} type='conditions' />
-    </div>
+    </AttributeConditionLineFormAndErrorsContainer>
   );
 };
 
