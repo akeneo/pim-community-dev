3671a96b0c867023b8679473e597aba8b779f2b3
Julien Sanchez <sumbobyboys@gmail.com>
2020-07-31 15:03:37 +0200

    RAC-146: explude published products

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/product/edit/field/with-label.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/product/edit/field/with-label.js
@@ -27,6 +27,8 @@ define([
          * {@inheritdoc}
          */
         render: function () {
+            if (this.getFormData().code === 'csv_published_product_export' || this.getFormData().code === 'xlsx_published_product_export') return this;
+
             BaseField.prototype.render.apply(this, arguments);
 
             this.$('.switch').bootstrapSwitch();
