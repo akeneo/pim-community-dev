2946dac199ca649076fcd472f2b1d4bb39f532d8
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-07-31 12:05:01 +0200

    RAC-148: Improve style

--- src/Oro/Bundle/PimDataGridBundle/Resources/public/js/datagrid/quickexport/component/QuickExportConfigurator.tsx
+++ src/Oro/Bundle/PimDataGridBundle/Resources/public/js/datagrid/quickexport/component/QuickExportConfigurator.tsx
@@ -9,6 +9,10 @@ import {
   Key,
   ModalConfirmButton,
   AkeneoThemedProps,
+  IconProps,
+  useAkeneoTheme,
+  CSVFileIcon,
+  XLSXFileIcon,
 } from '@akeneo-pim-community/shared';
 import styled from 'styled-components';
 
@@ -39,7 +43,10 @@ const Title = styled.div`
 
 const OptionContainer = styled.div<{isSelected: boolean}>`
   width: 128px;
-  text-align: center;
+  padding: 24px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border: 1px solid;
   border-color: ${({theme, isSelected}: AkeneoThemedProps & {isSelected: boolean}) =>
     isSelected ? theme.color.blue100 : theme.color.grey80};
@@ -47,6 +54,7 @@ const OptionContainer = styled.div<{isSelected: boolean}>`
     isSelected ? theme.color.blue20 : theme.color.white};
   color: ${({theme, isSelected}: AkeneoThemedProps & {isSelected: boolean}) =>
     isSelected ? theme.color.blue100 : 'inherit'};
+  cursor: pointer;
 
   :not(:first-child) {
     margin-left: 20px;
@@ -64,20 +72,33 @@ const SelectContainer = styled.div`
 type OptionProps = {
   code: string;
   isSelected?: boolean;
+  children?: ReactNode;
   onSelect?: () => void;
 };
 
-const Option = ({isSelected, code, onSelect}: OptionProps) => {
+const Option = ({isSelected, code, children, onSelect}: OptionProps) => {
+  const theme = useAkeneoTheme();
+  const translate = useTranslate();
+
   return (
     <OptionContainer isSelected={!!isSelected} onClick={onSelect}>
-      {code}
+      {Children.map(children, child => {
+        if (!isValidElement<IconProps>(child)) {
+          return child;
+        }
+
+        return cloneElement<IconProps>(child, {
+          color: isSelected ? theme.color.blue100 : child.props.color,
+        });
+      })}
+      {translate(`pim_datagrid.mass_action.quick_export.configurator.${code}`)}
     </OptionContainer>
   );
 };
 
 type SelectProps = {
-  onChange: (value: string | null) => void;
   children?: ReactNode;
+  onChange: (value: string | null) => void;
 };
 
 const Select = ({onChange, children}: SelectProps) => {
@@ -87,22 +108,20 @@ const Select = ({onChange, children}: SelectProps) => {
     onChange(selectedOptionCode);
   }, [selectedOptionCode]);
 
-  const updatedChildren = Children.map(children, child => {
-    if (!isValidElement<OptionProps>(child)) {
-      return child;
-    }
+  return (
+    <SelectContainer>
+      {Children.map(children, child => {
+        if (!isValidElement<OptionProps>(child)) {
+          return child;
+        }
 
-    if (child.type === Option) {
-      return cloneElement<OptionProps>(child, {
-        isSelected: child.props.code === selectedOptionCode,
-        onSelect: () => setSelectedOptionCode(child.props.code),
-      });
-    } else {
-      return child;
-    }
-  });
-
-  return <SelectContainer>{updatedChildren}</SelectContainer>;
+        return cloneElement<OptionProps>(child, {
+          isSelected: child.props.code === selectedOptionCode,
+          onSelect: () => setSelectedOptionCode(child.props.code),
+        });
+      })}
+    </SelectContainer>
+  );
 };
 
 type QuickExportConfiguratorProps = {
@@ -141,37 +160,42 @@ const QuickExportConfiguratorContainer = ({onActionLaunch}: QuickExportConfigura
                 onClick={() => {
                   closeModal();
                   console.log(type, context, withLabel);
-                  onActionLaunch('yolo');
+                  onActionLaunch(
+                    `quick_export${'grid-context' === context ? `_grid_context` : ''}${
+                      'with-labels' === withLabel ? `_with_labels` : ''
+                    }_${type}`
+                  );
                 }}
                 disabled={null === type || null === context || null === withLabel}
               >
                 {translate('pim_common.export')}
               </ModalConfirmButton>
               <Subtitle>
-                {translate('pim_datagrid.mass_action.quick_export.configurator.subtitle')} |{' '}
-                {translate(
+                {`${translate('pim_datagrid.mass_action.quick_export.configurator.subtitle')} | ${translate(
                   'pim_datagrid.mass_action.quick_export.configurator.product_count',
-                  {
-                    count: productCount.toString(),
-                  },
+                  {count: productCount.toString()},
                   productCount
-                )}
+                )}`}
               </Subtitle>
               <Title>{translate('pim_datagrid.mass_action.quick_export.configurator.title')}</Title>
               <Select onChange={setType}>
-                <Option code="csv"></Option>
-                <Option code="xlsx"></Option>
+                <Option code="csv">
+                  <CSVFileIcon size={48} />
+                </Option>
+                <Option code="xlsx">
+                  <XLSXFileIcon size={48} />
+                </Option>
               </Select>
               {null !== type && (
                 <Select onChange={setContext}>
-                  <Option code="grid-context"></Option>
-                  <Option code="all"></Option>
+                  <Option code="grid-context" />
+                  <Option code="all-attributes" />
                 </Select>
               )}
               {null !== context && (
                 <Select onChange={setWithLabel}>
-                  <Option code="with-label"></Option>
-                  <Option code="without-label"></Option>
+                  <Option code="with-codes" />
+                  <Option code="with-labels" />
                 </Select>
               )}
             </Content>
