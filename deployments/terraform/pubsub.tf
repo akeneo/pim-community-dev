624fb2de9049adc31cfe32290a360d4dd7686740
franckbrouard <franck.brouard@akeneo.fr>
2020-08-04 15:23:37 +0200

    BH-4

--- deployments/terraform/pubsub.tf
+++ deployments/terraform/pubsub.tf
@@ -49,26 +49,13 @@ resource "google_pubsub_subscription" "job-queue" {
 // We do declare 2 kinds of applications, the one that writes and the one that
 // reads. It gives rights to the same service account: PIM.
 
-resource "google_service_account" "pimpubsub" {
-  account_id   = format("gs-pubsub-%s", substr(md5(var.instance_name), 0, 18))
-  display_name = "PubSub access for pim (${var.instance_name})"
-}
-
-resource "google_service_account_key" "pimpubsub" {
-  service_account_id = google_service_account.pimstorage.id
-  public_key_type    = "TYPE_X509_PEM_FILE"
-
-  depends_on = [google_service_account.pimpubsub]
-}
-
-
 resource "google_pubsub_topic_iam_member" "pubsub_publisher_business-event" {
   topic  = google_pubsub_topic.business-event.name
   role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_service_account.pimpubsub.email}"
+  member = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
-    google_service_account.pimpubsub,
+    google_service_account.pim_service_account,
     google_pubsub_topic.business-event,
   ]
 }
@@ -76,10 +63,10 @@ resource "google_pubsub_topic_iam_member" "pubsub_publisher_business-event" {
 resource "google_pubsub_topic_iam_member" "pubsub_publisher_job_queue" {
   topic  = google_pubsub_topic.job-queue.name
   role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_service_account.pimpubsub.email}"
+  member = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
-    google_service_account.pimpubsub,
+    google_service_account.pim_service_account,
     google_pubsub_topic.job-queue,
   ]
 }
@@ -87,10 +74,10 @@ resource "google_pubsub_topic_iam_member" "pubsub_publisher_job_queue" {
 resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_webhook" {
   subscription = google_pubsub_subscription.webhook.name
   role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:${google_service_account.pimpubsub.email}"
+  member       = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
-    google_service_account.pimpubsub,
+    google_service_account.pim_service_account,
     google_pubsub_subscription.webhook,
   ]
 }
@@ -98,10 +85,10 @@ resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_webhook" {
 resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_job_queue" {
   subscription  = google_pubsub_subscription.job-queue.name
   role          = "roles/pubsub.subscriber"
-  member        = "serviceAccount:${google_service_account.pimpubsub.email}"
+  member        = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
-    google_service_account.pimpubsub,
+    google_service_account.pim_service_account,
     google_pubsub_subscription.job-queue,
   ]
 }
--- deployments/terraform/storage.tf
+++ deployments/terraform/storage.tf
@@ -12,23 +12,10 @@ resource "google_storage_bucket" "srnt_bucket" {
 resource "google_storage_bucket_iam_member" "srnt_bucket_pimstorage" {
   bucket = google_storage_bucket.srnt_bucket.name
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.pimstorage.email}"
+  member = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
-    google_service_account.pimstorage,
+    google_service_account.pim_service_account,
     google_storage_bucket.srnt_bucket,
   ]
 }
-
-resource "google_service_account" "pimstorage" {
-  account_id   = format("gs-%s", substr(md5(var.instance_name), 0, 25))
-  display_name = "Google access for pim container (${var.instance_name})"
-}
-
-resource "google_service_account_key" "pimstorage" {
-  service_account_id = google_service_account.pimstorage.id
-  public_key_type    = "TYPE_X509_PEM_FILE"
-
-  depends_on = [google_service_account.pimstorage]
-}
-
