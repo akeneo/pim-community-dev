761a46df5c1e33a7b3d0200d79ea267657f1f79f
Michel Tag <micheltag@akeneo.com>
2020-08-04 10:58:23 +0200

    Preparing version 3.0.83 (#12536)
    Preparing version 3.0.83
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.0.82';
+    const VERSION = '3.0.83';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Super-Rabbit';
