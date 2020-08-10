dd0b13844ad9ba78b07f65cfa714a1beedac77cd
Julien Sanchez <sumbobyboys@gmail.com>
2020-08-10 23:42:47 +0200

    add bump version workflow

---
+++ .github/actions/extract/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+
+echo "Starts"
+FOLDER="$1"
+GITHUB_USERNAME="$2"
+GITHUB_REPO="$3"
+USER_EMAIL="$4"
+
+CLONE_DIR=$(mktemp -d)
+
+# Setup git
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$GITHUB_USERNAME"
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+
+# Copy files into the git and deletes all git
+find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
+
+cp -r "$FOLDER"/* "$CLONE_DIR"
+
+cd "$CLONE_DIR"
+
+sed '/\lib/d' .gitignore > .gitignore
+
+git add .
+git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+git push origin master
---
+++ akeneo-design-system/bump-version.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (typeof filePath === 'undefined') {
+  console.log(`usage: ${process.argv[0]} ${process.argv[1]} path_to_webhook_event.json`);
+
+  return;
+}
+
+// Let's define an enum for our bump levels
+const BumpLevelEnum = {
+  Patch: 0,
+  Minor: 1,
+  Major: 2,
+}
+Object.freeze(BumpLevelEnum);
+
+// Extract the bump level to extract from the commit message
+const getCommitMessageBumpLevel = (message) => {
+  if (message.includes(`#patch`)) return BumpLevelEnum.Patch;
+  if (message.includes('#minor')) return BumpLevelEnum.Minor;
+  if (message.includes('#major')) return BumpLevelEnum.Major;
+
+  return BumpLevelEnum.Patch;
+}
+
+const getBumpNameFromBumpLevel = (bumpLevel) => {
+  switch (bumpLevel) {
+    case BumpLevelEnum.Patch:
+      return 'patch';
+    case BumpLevelEnum.Minor:
+      return 'minor';
+    case BumpLevelEnum.Major:
+      return 'major';
+    default:
+      throw Error('Invalid bump level');
+  }
+}
+
+const rawdata = fs.readFileSync(filePath);
+const githubEvent = JSON.parse(rawdata);
+
+const levelToBump = githubEvent.commits.reduce((currentBumpLevel, commit) => {
+  const bumpLevel = getCommitMessageBumpLevel(commit.message);
+
+  return bumpLevel > currentBumpLevel ? bumpLevel : currentBumpLevel;
+}, BumpLevelEnum.Patch);
+
+exec(`npm --no-git-tag-version version ${getBumpNameFromBumpLevel(levelToBump)}`);
+
+console.log(JSON.parse(fs.readFileSync('./package.json')).version);
