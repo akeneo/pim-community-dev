7b916cca9b4d681441c51337b1dc2548a0e3f32c
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-08-10 11:15:12 +0200

    API-1260 : add blank line for reruning CI...

--- src/Akeneo/Pim/Structure/Bundle/Doctrine/ORM/Repository/ExternalApi/AttributeGroupRepository.php
+++ src/Akeneo/Pim/Structure/Bundle/Doctrine/ORM/Repository/ExternalApi/AttributeGroupRepository.php
@@ -16,6 +16,7 @@ use Symfony\Component\Validator\Validation;
  * @author    Willy Mesnage <willy.mesnage@akeneo.com>
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * 
  */
 class AttributeGroupRepository extends EntityRepository implements ApiResourceRepositoryInterface
 {
