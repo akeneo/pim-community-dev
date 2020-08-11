21b003c6e1d7bedbabade09c4e6f19bb7a5fde67
Julien Sanchez <sumbobyboys@gmail.com>
2020-08-11 20:38:39 +0200

    update button

--- akeneo-design-system/src/components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ akeneo-design-system/src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,23 +1,22 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import { CoreButton } from '../CoreButton'
-
+import {CoreButton} from '../CoreButton';
 
 const PrimaryButton = styled(CoreButton)`
-    color: white;
-    background-color: #67B373;
-    &:hover {
-        background-color: #528f5c;
-    }
-    &:active {
-        background-color: #3d6b45;
-    }
-    &:focus {
-        border-color: blue;
-    }
-    &:disabled {
-        background-color: #c2e1c7;
-    }
-`
+  color: red;
+  background-color: #67b373;
+  &:hover {
+    background-color: #528f5c;
+  }
+  &:active {
+    background-color: #3d6b45;
+  }
+  &:focus {
+    border-color: blue;
+  }
+  &:disabled {
+    background-color: #c2e1c7;
+  }
+`;
 
-export { PrimaryButton }
+export {PrimaryButton};
