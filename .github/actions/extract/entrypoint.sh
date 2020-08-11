1b2cf2432ac33016d35d8684f6a8715fe2cd8cd8
Julien Sanchez <sumbobyboys@gmail.com>
2020-08-11 20:16:17 +0200

    remove .github from ignore

--- .github/actions/extract/entrypoint.sh
+++ .github/actions/extract/entrypoint.sh
@@ -14,7 +14,7 @@ git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
 # Copy files into the git and deletes all git
-find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
+find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git\/" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
 
 cp -r "$FOLDER"/* "$CLONE_DIR"
 
