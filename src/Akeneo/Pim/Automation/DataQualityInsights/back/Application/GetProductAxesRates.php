6ee1c2b2da2ae188468bfc2b90e0f5efc2301936
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-08-17 16:47:41 +0200

    DAPI-1209: rename GetProductAxesRates in CE

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Application/GetProductAxesRates.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Application/GetProductAxesRates.php
@@ -19,7 +19,7 @@ use Akeneo\Pim\Automation\DataQualityInsights\Domain\Query\ProductEvaluation\Get
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\AxisCode;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductId;
 
-class GetProductAxesRates
+class GetProductAxesRates implements GetProductAxesRatesInterface
 {
     /** @var GetLatestAxesRatesQueryInterface */
     private $getLatestProductAxesRatesQuery;
