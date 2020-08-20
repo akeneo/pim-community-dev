9edd41686aebf09832a253f7c567309e4df224c8
Michel Tag <micheltag@akeneo.com>
2020-08-20 09:37:24 +0200

    Preparing version 4.0.50 (#9457)
    Preparing version 4.0.50
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.49';
+    const VERSION = '4.0.50';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Daisy';
