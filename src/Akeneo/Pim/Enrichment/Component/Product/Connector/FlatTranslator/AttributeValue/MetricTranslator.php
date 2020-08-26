154fec4273e7022e13063e4933100250eba55fe6
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-08-26 14:40:38 +0200

    RAC-131: Add metric attribute value flat translator

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Connector/FlatTranslator/AttributeValue/MetricTranslator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Connector\FlatTranslator\AttributeValue;
+
+use Akeneo\Pim\Enrichment\Component\Product\Connector\FlatTranslator\FlatTranslatorInterface;
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+use Akeneo\Tool\Bundle\MeasureBundle\PublicApi\SqlGetUnitTranslations;
+
+class MetricTranslator implements FlatAttributeValueTranslatorInterface
+{
+    const UNIT_SUFFIX = '-unit';
+
+    /** @var SqlGetUnitTranslations */
+    private $getUnitTranslations;
+
+    public function __construct(SqlGetUnitTranslations $getUnitTranslations)
+    {
+        $this->getUnitTranslations = $getUnitTranslations;
+    }
+
+    public function supports(string $attributeType, string $columnName): bool
+    {
+        $endsWithUnit = 0 === substr_compare($columnName, self::UNIT_SUFFIX, -strlen(self::UNIT_SUFFIX));
+
+        return $attributeType === AttributeTypes::METRIC && $endsWithUnit;
+    }
+
+    public function translate(string $attributeCode, array $properties, array $values, string $localeCode): array
+    {
+        if (!isset($properties['reference_data_name'])) {
+            return $values;
+        }
+
+        $measurementFamilyCode = $properties['reference_data_name'];
+        $unitTranslations = $this->getUnitTranslations->byMeasurementFamilyCodeAndLocale($measurementFamilyCode, $localeCode);
+
+        return array_map(function ($value) use ($unitTranslations) {
+            return $unitTranslations[$value] ?? sprintf(FlatTranslatorInterface::FALLBACK_PATTERN, $value);
+        }, $values);
+    }
+}
