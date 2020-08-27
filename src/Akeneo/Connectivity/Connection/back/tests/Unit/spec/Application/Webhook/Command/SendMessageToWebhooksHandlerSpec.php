c9dd19d08ce8e8578eeac9da6d44f413e037be1c
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-08-27 16:54:28 +0200

    finish SendMessageToWebhooksHandlerSpec

--- src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Application/Webhook/Command/SendMessageToWebhooksHandlerSpec.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Application/Webhook/Command/SendMessageToWebhooksHandlerSpec.php
@@ -7,10 +7,15 @@ namespace spec\Akeneo\Connectivity\Connection\Application\Webhook\Command;
 use Akeneo\Connectivity\Connection\Application\Webhook\Command\SendMessageToWebhooksCommand;
 use Akeneo\Connectivity\Connection\Application\Webhook\Command\SendMessageToWebhooksHandler;
 use Akeneo\Connectivity\Connection\Application\Webhook\WebhookEventBuilder;
+use Akeneo\Connectivity\Connection\Domain\Webhook\Model\ConnectionWebhook;
+use Akeneo\Connectivity\Connection\Domain\Webhook\Model\WebhookEvent;
+use Akeneo\Connectivity\Connection\Domain\Webhook\Model\WebhookRequest;
 use Akeneo\Connectivity\Connection\Domain\Webhook\Persistence\Query\SelectConnectionsWebhookQuery;
 use Akeneo\Connectivity\Connection\Infrastructure\Webhook\GuzzleWebhookClient;
 use Akeneo\Platform\Component\EventQueue\BusinessEventInterface;
 use PhpSpec\ObjectBehavior;
+use PHPUnit\Framework\Assert;
+use Prophecy\Argument;
 
 /**
  * @author Pierre Jolly <pierre.jolly@akeneo.com>
@@ -34,16 +39,41 @@ class SendMessageToWebhooksHandlerSpec extends ObjectBehavior
 
     public function it_sends_message_to_webhooks(
         $selectConnectionsWebhookQuery,
-        $client
+        $client,
+        $builder
     ): void {
+
+        $webhookBynder = new ConnectionWebhook('bynder','7', '5', 'secret_bynder','http://172.17.0.1:8000/webhook_bynder');
+        $webhookMagento = new ConnectionWebhook('magento','11', '5', 'secret_magento','http://172.17.0.1:8000/webhook_magento');
         $businessEvent = new BusinessEvent();
-        $webhookRequests = [];
+
+        $connectionWebhooks = [
+            $webhookBynder,
+            $webhookMagento
+        ];
+
+        $webhookEvent = new WebhookEvent(
+            $businessEvent->name(),
+            $businessEvent->uuid(),
+            date(\DateTimeInterface::ATOM, $businessEvent->timestamp()),
+            $businessEvent->data()
+        );
 
         $command = new SendMessageToWebhooksCommand($businessEvent);
 
-        $selectConnectionsWebhookQuery->execute()->shouldBeCalled()->willReturn($webhookRequests);
+        $selectConnectionsWebhookQuery->execute()->willReturn($connectionWebhooks);
 
-        $client->bulkSend($webhookRequests)->shouldBeCalled();
+        $builder->build($webhookMagento, $businessEvent)->willReturn($webhookEvent);
+        $builder->build($webhookBynder, $businessEvent)->willReturn($webhookEvent);
+
+        $client->bulkSend(Argument::that(function (array $params) {
+            Assert::assertCount(2, $params);
+            Assert::assertInstanceOf(WebhookRequest::class, $params[0]);
+            Assert::assertInstanceOf(WebhookRequest::class, $params[1]);
+            Assert::assertEquals($params[0]->webhook()->connectionCode(), 'bynder');
+            Assert::assertEquals($params[1]->webhook()->connectionCode(), 'magento');
+            return true;
+        }))->shouldBeCalled();
 
         $this->handle($command);
     }
@@ -53,7 +83,7 @@ class BusinessEvent implements BusinessEventInterface
 {
     public function name(): string
     {
-        return 'product';
+        return 'product.updated';
     }
 
     public function author(): string
