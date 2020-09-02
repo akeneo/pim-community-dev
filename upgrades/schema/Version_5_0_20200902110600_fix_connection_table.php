1947ff507054edad984d4a7e81c364abc454b7dc
jmleroux <jmleroux.pro@gmail.com>
2020-09-02 16:29:16 +0200

    PIM-9433: Fix akeneo_connectivity_connection migration from PIM-9398 (#12639)

--- upgrades/schema/Version_5_0_20200902110600_fix_connection_table.php
+++ upgrades/schema/Version_5_0_20200902110600_fix_connection_table.php
@@ -20,100 +20,67 @@ final class Version_5_0_20200902110600_fix_connection_table extends AbstractMigr
 
     private function fixIndexes()
     {
-        $this->addSql(<<<SQL
-IF EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND INDEX_NAME = 'IDX_APP_code'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection DROP INDEX IDX_APP_code;
-END IF;
-SQL
-        );
+        if ($this->indexExists('IDX_APP_code')) {
+            $this->addSql('ALTER TABLE akeneo_connectivity_connection DROP INDEX IDX_APP_code');
+        }
+        if ($this->indexExists('FK_APP_oro_user_app_user_id')) {
+            $this->addSql('ALTER TABLE akeneo_connectivity_connection DROP INDEX FK_APP_oro_user_app_user_id');
+        }
 
-        $this->addSql(<<<SQL
-IF EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND INDEX_NAME = 'FK_APP_oro_user_app_user_id'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection DROP INDEX FK_APP_oro_user_app_user_id;
-END IF;
-SQL
-        );
-
-        $this->addSql(<<<SQL
-IF NOT EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND INDEX_NAME = 'code'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection ADD PRIMARY KEY (code);
-END IF;
-SQL
-        );
+        if (!$this->indexExists('code')) {
+            $this->addSql('ALTER TABLE akeneo_connectivity_connection ADD PRIMARY KEY (code)');
+        }
     }
 
     private function fixConstraints()
     {
-        $this->addSql(<<<SQL
-IF EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND CONSTRAINT_NAME = 'FK_APP_oro_user_app_user_id'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection DROP CONSTRAINT FK_APP_oro_user_app_user_id;
-END IF;
-SQL
-        );
+        if ($this->constraintExists('FK_APP_oro_user_app_user_id')) {
+            $this->addSql('ALTER TABLE akeneo_connectivity_connection DROP CONSTRAINT FK_APP_oro_user_app_user_id');
+        }
+        if ($this->constraintExists('FK_APP_pim_api_client_app_client_id')) {
+            $this->addSql('ALTER TABLE akeneo_connectivity_connection DROP CONSTRAINT FK_APP_pim_api_client_app_client_id');
+        }
 
-        $this->addSql(<<<SQL
-IF EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND CONSTRAINT_NAME = 'FK_APP_pim_api_client_app_client_id'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection DROP CONSTRAINT FK_APP_pim_api_client_app_client_id;
-END IF;
-SQL
-        );
+        if (!$this->constraintExists('FK_CONNECTIVITY_CONNECTION_client_id')) {
+            $this->addSql(
+                'ALTER TABLE akeneo_connectivity_connection ADD CONSTRAINT 
+                FK_CONNECTIVITY_CONNECTION_client_id FOREIGN KEY (client_id) REFERENCES pim_api_client (id)'
+            );
+        }
+        if (!$this->constraintExists('FK_CONNECTIVITY_CONNECTION_user_id')) {
+            $this->addSql(
+                'ALTER TABLE akeneo_connectivity_connection ADD CONSTRAINT 
+                FK_CONNECTIVITY_CONNECTION_user_id FOREIGN KEY (user_id) REFERENCES oro_user (id)'
+            );
+        }
+    }
 
-        $this->addSql(<<<SQL
-IF NOT EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND CONSTRAINT_NAME = 'FK_CONNECTIVITY_CONNECTION_client_id'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection 
-        ADD CONSTRAINT FK_CONNECTIVITY_CONNECTION_client_id FOREIGN KEY (client_id) REFERENCES pim_api_client (id);
-END IF;
-SQL
-        );
+    private function indexExists(string $index)
+    {
+        $rows = $this->connection->executeQuery(
+            'SHOW INDEX FROM akeneo_connectivity_connection WHERE KEY_NAME = :index',
+            [
+                ':index' => $index,
+            ]
+        )->fetchAll();
 
-        $this->addSql(<<<SQL
-IF NOT EXISTS
-(
-    SELECT *
-    FROM information_schema.statistics
-    WHERE TABLE_NAME='akeneo_connectivity_connection' AND CONSTRAINT_NAME = 'FK_CONNECTIVITY_CONNECTION_user_id'
-)
-THEN
-    ALTER TABLE akeneo_connectivity_connection 
-        ADD CONSTRAINT FK_CONNECTIVITY_CONNECTION_user_id FOREIGN KEY (user_id) REFERENCES oro_user (id);
-END IF;
-SQL
-        );
+        return count($rows) > 0;
+    }
+
+    private function constraintExists(string $constraint): bool
+    {
+        $rows = $this->connection->executeQuery(
+            "SELECT table_name,column_name,referenced_table_name,referenced_column_name
+            FROM information_schema.key_column_usage
+            WHERE
+                REFERENCED_COLUMN_NAME IS NOT NULL
+                AND TABLE_NAME = 'akeneo_connectivity_connection'
+                AND CONSTRAINT_NAME = :constraint",
+            [
+                ':constraint' => $constraint,
+            ]
+        )->fetchAll();
+
+        return count($rows) > 0;
     }
 }
