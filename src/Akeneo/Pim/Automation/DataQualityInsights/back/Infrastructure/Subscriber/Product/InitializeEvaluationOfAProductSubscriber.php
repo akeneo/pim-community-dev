3f7f96a7840d01b760d6a744088ddd1dad1d2461
Laurent Petard <laurent.petard@akeneo.com>
2020-09-03 16:07:44 +0200

    Bypass DQI evaluation for benchmarks

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Subscriber/Product/InitializeEvaluationOfAProductSubscriber.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Subscriber/Product/InitializeEvaluationOfAProductSubscriber.php
@@ -10,6 +10,7 @@ use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductId;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
 use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
 use Akeneo\Tool\Component\StorageUtils\StorageEvents;
+use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -31,18 +32,23 @@ final class InitializeEvaluationOfAProductSubscriber implements EventSubscriberI
     /** @var ConsolidateAxesRates */
     private $consolidateProductAxisRates;
 
+    /** @var Connection */
+    private $db;
+
     public function __construct(
         FeatureFlag $dataQualityInsightsFeature,
         CreateCriteriaEvaluations $createProductsCriteriaEvaluations,
         LoggerInterface $logger,
         EvaluatePendingCriteria $evaluatePendingCriteria,
-        ConsolidateAxesRates $consolidateProductAxisRates
+        ConsolidateAxesRates $consolidateProductAxisRates,
+        Connection $db
     ) {
         $this->dataQualityInsightsFeature = $dataQualityInsightsFeature;
         $this->createProductsCriteriaEvaluations = $createProductsCriteriaEvaluations;
         $this->logger = $logger;
         $this->evaluatePendingCriteria = $evaluatePendingCriteria;
         $this->consolidateProductAxisRates = $consolidateProductAxisRates;
+        $this->db = $db;
     }
 
     public static function getSubscribedEvents()
@@ -67,6 +73,16 @@ final class InitializeEvaluationOfAProductSubscriber implements EventSubscriberI
         if (! $this->dataQualityInsightsFeature->isEnabled()) {
             return;
         }
+
+        $noEvaluation = (bool) $this->db->executeQuery(
+            "SELECT 1 FROM pim_configuration WHERE code = 'dqi_no_evaluation'"
+        )->fetchColumn();
+
+        if ($noEvaluation === true) {
+            return;
+        }
+
+        file_put_contents('/srv/pim/var/debug.txt', "Evaluate product\n", FILE_APPEND);
 
         $productId = intval($subject->getId());
         $this->initializeCriteria($productId);
