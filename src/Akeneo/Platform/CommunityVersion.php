f0a32b2fb3cc36a3ff4c00de0fa2843bc097f1eb
jmleroux <jmleroux.pro@gmail.com>
2020-09-03 09:22:40 +0000

    Preparing version 4.0.55

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.54';
+    const VERSION = '4.0.55';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Tonic';
