4a67a35efa568b8ae7a5bfa6c6792a51c4af9791
Julien Sanchez <sumbobyboys@gmail.com>
2020-09-04 16:49:47 +0200

    RAC-199: fix rule engine lint

--- src/Akeneo/Pim/Automation/RuleEngine/front/.eslintrc.js
+++ src/Akeneo/Pim/Automation/RuleEngine/front/.eslintrc.js
@@ -14,13 +14,21 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/unbound-method": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'object-curly-spacing': ['error', 'always'],
     'react/prop-types': ['off'],
     '@typescript-eslint/no-explicit-any': ['off'],
     'no-prototype-builtins': ['warn'],
-    "camelcase": "off",
-    "@typescript-eslint/camelcase": ["off"]
+    "camelcase": "off"
   },
 };
