c9943baabe12e317b8fd8bbae9d93847216a5cfe
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-09-07 15:00:03 +0200

    First commit

--- akeneo-design-system/.github/workflows/bump-version.js
+++ akeneo-design-system/.github/workflows/bump-version.js
@@ -46,7 +46,7 @@ const getBumpNameFromBumpLevel = (bumpLevel) => {
 const rawdata = fs.readFileSync(filePath);
 const githubEvent = JSON.parse(rawdata);
 
-const messages = (new String(execSync(`git rev-list --ancestry-path ${githubEvent.before}...${githubEvent.after} | xargs -n1 git log -n 1 --pretty=format:%s`))).split('\n');
+const messages = (new String(execSync(`git rev-list ${githubEvent.before}..HEAD | xargs -n1 git log -n 1 --pretty=format:%s`))).split('\n');
 
 const levelToBump = messages.reduce((currentBumpLevel, commit) => {
     const bumpLevel = getCommitMessageBumpLevel(commit);
