0c2605518ee58294fd2e626b8f828fa24e10ca80
Julien Sanchez <sumbobyboys@gmail.com>
2020-09-07 11:57:46 +0200

    RAC-202: update bump script

--- akeneo-design-system/.github/workflows/bump-version.js
+++ akeneo-design-system/.github/workflows/bump-version.js
@@ -45,8 +45,10 @@ const getBumpNameFromBumpLevel = (bumpLevel) => {
 const rawdata = fs.readFileSync(filePath);
 const githubEvent = JSON.parse(rawdata);
 
-const levelToBump = githubEvent.commits.reduce((currentBumpLevel, commit) => {
-    const bumpLevel = getCommitMessageBumpLevel(commit.message);
+const messages = execSync(`git rev-list --ancestry-path ${githubEvent.before}...${githubEvent.after} | xargs -n1 git log -n 1 --pretty=format:%s`).split('\n');
+
+const levelToBump = messages.reduce((currentBumpLevel, commit) => {
+    const bumpLevel = getCommitMessageBumpLevel(commit);
 
     return bumpLevel > currentBumpLevel ? bumpLevel : currentBumpLevel;
 }, BumpLevelEnum.Patch);
