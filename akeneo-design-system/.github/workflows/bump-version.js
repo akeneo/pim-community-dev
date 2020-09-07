2a3e031fae9a8db9307beed824d1194786224d0d
Julien Sanchez <sumbobyboys@gmail.com>
2020-09-07 12:00:36 +0200

    RAC-202: fix split

--- akeneo-design-system/.github/workflows/bump-version.js
+++ akeneo-design-system/.github/workflows/bump-version.js
@@ -45,8 +45,9 @@ const getBumpNameFromBumpLevel = (bumpLevel) => {
 const rawdata = fs.readFileSync(filePath);
 const githubEvent = JSON.parse(rawdata);
 
-const messages = execSync(`git rev-list --ancestry-path ${githubEvent.before}...${githubEvent.after} | xargs -n1 git log -n 1 --pretty=format:%s`).split('\n');
-
+const messages = execSync(`git rev-list --ancestry-path ${githubEvent.before}...${githubEvent.after} | xargs -n1 git log -n 1 --pretty=format:%s`);
+console.log(messages);
+console.log(new String(messages));
 const levelToBump = messages.reduce((currentBumpLevel, commit) => {
     const bumpLevel = getCommitMessageBumpLevel(commit);
 
