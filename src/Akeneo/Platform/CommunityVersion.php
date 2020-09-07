a86f7efe7f02aa378da6466c870b90b114db08d7
Michel Tag <micheltag@akeneo.com>
2020-09-07 16:19:51 +0200

    Preparing version 3.2.69 (#12691)
    Preparing version 3.2.69
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.68';
+    const VERSION = '3.2.69';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
