a00a99b7489a5996e93ca6861748bc25ec730820
Julien Sanchez <sumbobyboys@gmail.com>
2020-09-08 14:37:11 +0200

    RAC-202: rework extract action (#12687)
    * RAC-202: rework extract action
    
    * update package json bump method
    
    * RAC-202: fix artefact extract
    
    * RAC-202: split extract workflow
    
    * RAC-202: cleanup #minor
    
    * RAC-202: extract data from github event
    
    * RAC-202: first
    
    * RAC-202: second
    
    * RAC-202: third
    
    * RAC-202: update bump script
    
    * RaC-202: cleanup
    
    * RAC-202: fix split
    
    * RAC-202: fix split again
    
    * RAC-202: cleanup #minor
    
    * RAC-202: Test commit
    
    * RAC-202: Update extract action
    
    * RAC-202: Update bump version script
    
    * RAC-202: Update dsm extract
    
    * First commit
    
    * Second commit
    
    * Third commit
    
    * First commit
    
    * Second commit
    
    * Third commit
    
    * First commit
    
    * Seecccond commit
    
    * Thiiiiiiird commit
    
    * First commit
    
    * Seecccond commit
    
    * Thiiird commit
    
    * Thiiird commit
    
    * one commit
    
    * two commit
    
    * One commit
    
    * Two commit
    
    * Test
    saut
    de
    ligne
    
    * Onne commit
    
    * Twoo commit
    
    * Premier commit
    
    * Second commit
    
    * Trois commit
    
    * Commit 1
    
    * Commit 2
    
    * Commit 1
    
    * Commit 2
    
    * Commit 111
    
    * Commit 222
    
    * Commit 1
    
    * Commit 2
    
    * RAC-202: Cleanup
    
    * RAC-202: Cleanupd
    
    * RAC-202: Cleanup
    
    * RAC-202: Use repo full name
    
    * RAC-202: Reset dsm version
    
    Co-authored-by: Valentin Dijkstra <valentindijkstra@gmail.com>

--- akeneo-design-system/.github/workflows/bump-version.js
+++ akeneo-design-system/.github/workflows/bump-version.js
@@ -5,9 +5,11 @@ const {execSync} = require('child_process');
 const fs = require('fs');
 
 const filePath = process.argv[2];
+const externalPackageJson = process.argv[3];
+const commitMessagesFilepath = process.argv[4];
 
 if (typeof filePath === 'undefined') {
-    console.log(`usage: ${process.argv[0]} ${process.argv[1]} path_to_webhook_event.json`);
+    console.log(`usage: ${process.argv[0]} ${process.argv[1]} path_to_webhook_event.json external/package.json commit-messages.txt`);
 
     return;
 }
@@ -44,12 +46,17 @@ const getBumpNameFromBumpLevel = (bumpLevel) => {
 const rawdata = fs.readFileSync(filePath);
 const githubEvent = JSON.parse(rawdata);
 
-const levelToBump = githubEvent.commits.reduce((currentBumpLevel, commit) => {
-    const bumpLevel = getCommitMessageBumpLevel(commit.message);
+const lastCommits = (new String(execSync(`git rev-list ${githubEvent.before}..HEAD`))).split('\n');
+const messages = lastCommits.map(commit => new String(execSync(`git log -n 1 --pretty=format:%s ${commit}`)));
+fs.writeFileSync(commitMessagesFilepath, `Extract from https://github.com/${githubEvent.repository.full_name}/commit/${githubEvent.after}\nWith commits:\n${messages.join('\n')}`);
+
+const levelToBump = messages.reduce((currentBumpLevel, commit) => {
+    const bumpLevel = getCommitMessageBumpLevel(commit);
 
     return bumpLevel > currentBumpLevel ? bumpLevel : currentBumpLevel;
 }, BumpLevelEnum.Patch);
 
-execSync(`npm --no-git-tag-version version ${getBumpNameFromBumpLevel(levelToBump)}`);
+const externalVersion = JSON.parse(fs.readFileSync(externalPackageJson)).version
 
-console.log(JSON.parse(fs.readFileSync('./package.json')).version);
+execSync(`npm --no-git-tag-version version ${externalVersion}`);
+execSync(`npm --no-git-tag-version version ${getBumpNameFromBumpLevel(levelToBump)}`);
