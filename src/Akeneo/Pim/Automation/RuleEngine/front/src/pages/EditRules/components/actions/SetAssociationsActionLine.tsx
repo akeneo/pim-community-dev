cabf1fe01bce13bc8be0104644688a9e642c726f
pierallard <pierallard@gmail.com>
2020-09-08 15:26:39 +0200

    RUL-442: Move association types selector to component

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/SetAssociationsActionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/SetAssociationsActionLine.tsx
@@ -7,29 +7,9 @@ import {
 import { ActionLineProps } from './ActionLineProps';
 import { useControlledFormInputAction } from '../../hooks';
 import { ActionTemplate } from './ActionTemplate';
-import {
-  useBackboneRouter,
-  useTranslate,
-  useUserCatalogLocale,
-} from '../../../../dependenciesTools/hooks';
-import {
-  AssociationType,
-  AssociationTypeCode,
-  GroupCode,
-  ProductIdentifier,
-  ProductModelCode,
-} from '../../../../models';
-import {
-  ActionGrid,
-  ActionLeftSide,
-  ActionRightSide,
-  ActionTitle,
-} from './ActionLine';
-import { Label } from '../../../../components/Labels';
-import { getAssociationTypesFromQuantified } from '../../../../repositories/AssociationTypeRepository';
-import { AssociationsGroupsSelector } from './association/AssociationsGroupsSelector';
-import { AssociationsIdentifiersSelector } from './association/AssociationsIdentifiersSelector';
-import { AddAssociationTypeButton } from './association/AddAssociationTypeButton';
+import { useTranslate } from '../../../../dependenciesTools/hooks';
+import { AssociationTypeCode } from '../../../../models';
+import { AssociationTypesSelector } from './association/AssociationTypesSelector';
 
 type Props = {
   action?: SetAssociationsAction;
@@ -47,8 +27,6 @@ const SetAssociationsActionLine: React.FC<Props> = ({
   handleDelete,
 }) => {
   const translate = useTranslate();
-  const router = useBackboneRouter();
-  const currentCatalogLocale = useUserCatalogLocale();
 
   const {
     fieldFormName,
@@ -56,114 +34,6 @@ const SetAssociationsActionLine: React.FC<Props> = ({
     getValueFormValue,
     setValueFormValue,
   } = useControlledFormInputAction<AssociationValue>(lineNumber);
-
-  const [associationTypes, setAssociationTypes] = React.useState<
-    AssociationType[]
-  >();
-  const [associationValues, setAssociationValues] = React.useState<
-    Map<
-      AssociationTarget,
-      ProductIdentifier[] | GroupCode[] | ProductModelCode[]
-    >
-  >();
-  const [
-    currentAssociationTarget,
-    setCurrentAssociationTarget,
-  ] = React.useState<AssociationTarget>();
-
-  React.useEffect(() => {
-    getAssociationTypesFromQuantified(router, false).then(associationTypes => {
-      setAssociationTypes(associationTypes);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    const value = getValueFormValue() ?? {};
-    const associationValuesArray: any = [];
-    Object.keys(value).forEach((associationTypeCode: string) => {
-      (['products', 'product_models', 'groups'] as Target[]).forEach(target => {
-        if (typeof value[associationTypeCode][target] !== 'undefined') {
-          associationValuesArray.push([
-            { associationTypeCode, target },
-            value[associationTypeCode][target],
-          ]);
-        }
-      });
-    });
-    const associationValues = new Map<
-      AssociationTarget,
-      ProductIdentifier[] | GroupCode[] | ProductModelCode[]
-    >(associationValuesArray);
-    setAssociationValues(associationValues);
-    setCurrentAssociationTarget(Array.from(associationValues.keys())[0]);
-  }, []);
-
-  if (!associationValues || !associationTypes) {
-    return (
-      <img
-        src='/bundles/pimui/images//loader-V2.svg'
-        alt={translate('pim_common.loading')}
-      />
-    );
-  }
-
-  const isCurrentAssociationTargetOrDefault: (
-    associationTarget: AssociationTarget
-  ) => boolean = ({ associationTypeCode, target }) => {
-    return (
-      !!currentAssociationTarget &&
-      currentAssociationTarget.associationTypeCode === associationTypeCode &&
-      currentAssociationTarget.target === target
-    );
-  };
-
-  const getAssociationTypeLabel = (associationTypeCode: AssociationTypeCode) =>
-    associationTypes?.find(
-      associationType => associationType.code === associationTypeCode
-    )?.labels?.[currentCatalogLocale] || `[${associationTypeCode}]`;
-
-  const formatAssociationValues = () => {
-    return Array.from(associationValues.entries()).reduce(
-      (result, [associationTarget, value]) => {
-        if (!result[associationTarget.associationTypeCode]) {
-          result[associationTarget.associationTypeCode] = {};
-        }
-        result[associationTarget.associationTypeCode][
-          associationTarget.target
-        ] = value;
-        return result;
-      },
-      {} as AssociationValue
-    );
-  };
-
-  const handleAssociationTargetDelete = (
-    associationTarget: AssociationTarget
-  ) => {
-    associationValues.delete(associationTarget);
-    setAssociationValues(associationValues);
-    setValueFormValue(formatAssociationValues());
-  };
-
-  const handleChange = (
-    associationTarget: AssociationTarget,
-    value: GroupCode[] | ProductModelCode[] | ProductIdentifier[]
-  ) => {
-    associationValues.set(associationTarget, value);
-    setAssociationValues(new Map(associationValues));
-    setValueFormValue(formatAssociationValues());
-  };
-
-  const handleAddAssociationType = (
-    associationTypeCode: AssociationTypeCode,
-    target: Target
-  ) => {
-    const associationTarget = { associationTypeCode, target };
-    associationValues.set(associationTarget, []);
-    setAssociationValues(new Map(associationValues));
-    setCurrentAssociationTarget(associationTarget);
-    setValueFormValue(formatAssociationValues());
-  };
 
   return (
     <>
@@ -177,7 +47,6 @@ const SetAssociationsActionLine: React.FC<Props> = ({
         name={typeFormName}
         defaultValue='set'
       />
-
       <ActionTemplate
         title={translate(
           'pimee_catalog_rule.form.edit.actions.set_associations.title'
@@ -189,126 +58,12 @@ const SetAssociationsActionLine: React.FC<Props> = ({
           'pimee_catalog_rule.form.edit.actions.set_associations.helper'
         )}
         handleDelete={handleDelete}
-        lineNumber={lineNumber}>
-        <ActionGrid>
-          <ActionLeftSide>
-            <ActionTitle>
-              {translate(
-                'pimee_catalog_rule.form.edit.actions.set_associations.select_association_type'
-              )}
-            </ActionTitle>
-            <Label
-              className='AknFieldContainer-label control-label'
-              label={`${translate(
-                'pimee_catalog_rule.form.edit.actions.set_associations.association_types'
-              )} ${translate('pim_common.required_label')}`}
-            />
-            <ul>
-              {Array.from(associationValues.entries()).map(
-                ([associationTarget, value]) => {
-                  return (
-                    <li
-                      key={`${associationTarget.associationTypeCode}-${associationTarget.target}`}
-                      className={'AknBadgedSelector-item'}>
-                      <button
-                        data-testid={`association-type-selector-${associationTarget.associationTypeCode}-${associationTarget.target}`}
-                        className={`AknTextField AknBadgedSelector${
-                          isCurrentAssociationTargetOrDefault(associationTarget)
-                            ? ' AknBadgedSelector--selected'
-                            : ''
-                        }`}
-                        onClick={e => {
-                          e.preventDefault();
-                          setCurrentAssociationTarget(associationTarget);
-                        }}>
-                        {getAssociationTypeLabel(
-                          associationTarget.associationTypeCode
-                        )}
-                        <span className='AknBadgedSelector-helper'>
-                          {translate(
-                            `pimee_catalog_rule.form.edit.actions.set_associations.counts.${associationTarget.target}`,
-                            { count: value.length },
-                            value.length
-                          )}
-                        </span>
-                        <span
-                          className='AknBadgedSelector-delete'
-                          tabIndex={0}
-                          onClick={() =>
-                            handleAssociationTargetDelete(associationTarget)
-                          }
-                          role='button'
-                        />
-                      </button>
-                    </li>
-                  );
-                }
-              )}
-              <li className={'AknBadgedSelector-item'}>
-                <AddAssociationTypeButton
-                  onAddAssociationType={handleAddAssociationType}
-                  selectedTargets={Array.from(associationValues.keys())}
-                />
-              </li>
-            </ul>
-          </ActionLeftSide>
-          <ActionRightSide>
-            {currentAssociationTarget && (
-              <>
-                <ActionTitle>
-                  {translate(
-                    `pimee_catalog_rule.form.edit.actions.set_associations.select_title.${currentAssociationTarget.target}`
-                  )}
-                </ActionTitle>
-                <Label
-                  className='AknFieldContainer-label control-label'
-                  label={`${translate(
-                    `pimee_catalog_rule.form.edit.actions.set_associations.select.${currentAssociationTarget?.target}`
-                  )} ${translate('pim_common.required_label')}`}
-                />
-                {currentAssociationTarget.target === 'groups' && (
-                  <AssociationsGroupsSelector
-                    groupCodes={
-                      (associationValues.get(
-                        currentAssociationTarget
-                      ) as GroupCode[]) || []
-                    }
-                    currentCatalogLocale={currentCatalogLocale}
-                    onChange={groupCodes =>
-                      handleChange(currentAssociationTarget, groupCodes)
-                    }
-                  />
-                )}
-                {currentAssociationTarget.target === 'products' && (
-                  <AssociationsIdentifiersSelector
-                    entityType='product'
-                    identifiers={
-                      (associationValues.get(
-                        currentAssociationTarget
-                      ) as ProductIdentifier[]) || []
-                    }
-                    onChange={productIdentifiers =>
-                      handleChange(currentAssociationTarget, productIdentifiers)
-                    }
-                  />
-                )}
-                {currentAssociationTarget.target === 'product_models' && (
-                  <AssociationsIdentifiersSelector
-                    entityType='product_model'
-                    identifiers={
-                      (associationValues.get(
-                        currentAssociationTarget
-                      ) as ProductModelCode[]) || []
-                    }
-                    onChange={productModelCodes =>
-                      handleChange(currentAssociationTarget, productModelCodes)
-                    }
-                  />
-                )}
-              </>
-            )}
-          </ActionRightSide>
-        </ActionGrid>
+        lineNumber={lineNumber}
+      >
+        <AssociationTypesSelector
+          value={getValueFormValue() ?? {}}
+          onChange={setValueFormValue}
+        />
       </ActionTemplate>
     </>
   );
---
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/association/AssociationTypesSelector.tsx
@@ -0,0 +1,268 @@
+import React from 'react';
+import {
+  AssociationType,
+  AssociationTypeCode,
+  GroupCode,
+  ProductIdentifier,
+  ProductModelCode,
+} from '../../../../../models';
+import { getAssociationTypesFromQuantified } from '../../../../../repositories/AssociationTypeRepository';
+import { AssociationValue } from '../../../../../models/actions';
+import { AssociationTarget, Target } from '../SetAssociationsActionLine';
+import {
+  ActionGrid,
+  ActionLeftSide,
+  ActionRightSide,
+  ActionTitle,
+} from '../ActionLine';
+import { Label } from '../../../../../components/Labels';
+import { AddAssociationTypeButton } from './AddAssociationTypeButton';
+import { AssociationsGroupsSelector } from './AssociationsGroupsSelector';
+import { AssociationsIdentifiersSelector } from './AssociationsIdentifiersSelector';
+import {
+  useBackboneRouter,
+  useTranslate,
+  useUserCatalogLocale,
+} from '../../../../../dependenciesTools/hooks';
+
+type Props = {
+  value: AssociationValue;
+  onChange: (value: AssociationValue) => void;
+};
+
+const AssociationTypesSelector: React.FC<Props> = ({ value, onChange }) => {
+  const router = useBackboneRouter();
+  const currentCatalogLocale = useUserCatalogLocale();
+  const translate = useTranslate();
+
+  const [associationTypes, setAssociationTypes] = React.useState<
+    AssociationType[]
+  >();
+  const [associationValues, setAssociationValues] = React.useState<
+    Map<
+      AssociationTarget,
+      ProductIdentifier[] | GroupCode[] | ProductModelCode[]
+    >
+  >();
+  const [
+    currentAssociationTarget,
+    setCurrentAssociationTarget,
+  ] = React.useState<AssociationTarget>();
+
+  React.useEffect(() => {
+    getAssociationTypesFromQuantified(router, false).then(associationTypes => {
+      setAssociationTypes(associationTypes);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    const associationValuesArray: any = [];
+    Object.keys(value).forEach((associationTypeCode: string) => {
+      (['products', 'product_models', 'groups'] as Target[]).forEach(target => {
+        if (typeof value[associationTypeCode][target] !== 'undefined') {
+          associationValuesArray.push([
+            { associationTypeCode, target },
+            value[associationTypeCode][target],
+          ]);
+        }
+      });
+    });
+    const associationValues = new Map<
+      AssociationTarget,
+      ProductIdentifier[] | GroupCode[] | ProductModelCode[]
+    >(associationValuesArray);
+    setAssociationValues(associationValues);
+    setCurrentAssociationTarget(Array.from(associationValues.keys())[0]);
+  }, []);
+
+  if (!associationValues || !associationTypes) {
+    return (
+      <img
+        src='/bundles/pimui/images//loader-V2.svg'
+        alt={translate('pim_common.loading')}
+      />
+    );
+  }
+
+  const isCurrentAssociationTargetOrDefault: (
+    associationTarget: AssociationTarget
+  ) => boolean = ({ associationTypeCode, target }) => {
+    return (
+      !!currentAssociationTarget &&
+      currentAssociationTarget.associationTypeCode === associationTypeCode &&
+      currentAssociationTarget.target === target
+    );
+  };
+
+  const getAssociationTypeLabel = (associationTypeCode: AssociationTypeCode) =>
+    associationTypes?.find(
+      associationType => associationType.code === associationTypeCode
+    )?.labels?.[currentCatalogLocale] || `[${associationTypeCode}]`;
+
+  const formatAssociationValues = () => {
+    return Array.from(associationValues.entries()).reduce(
+      (result, [associationTarget, value]) => {
+        if (!result[associationTarget.associationTypeCode]) {
+          result[associationTarget.associationTypeCode] = {};
+        }
+        result[associationTarget.associationTypeCode][
+          associationTarget.target
+        ] = value;
+        return result;
+      },
+      {} as AssociationValue
+    );
+  };
+
+  const handleAssociationTargetDelete = (
+    associationTarget: AssociationTarget
+  ) => {
+    associationValues.delete(associationTarget);
+    setAssociationValues(associationValues);
+    onChange(formatAssociationValues());
+  };
+
+  const handleChange = (
+    associationTarget: AssociationTarget,
+    value: GroupCode[] | ProductModelCode[] | ProductIdentifier[]
+  ) => {
+    associationValues.set(associationTarget, value);
+    setAssociationValues(new Map(associationValues));
+    onChange(formatAssociationValues());
+  };
+
+  const handleAddAssociationType = (
+    associationTypeCode: AssociationTypeCode,
+    target: Target
+  ) => {
+    const associationTarget = { associationTypeCode, target };
+    associationValues.set(associationTarget, []);
+    setAssociationValues(new Map(associationValues));
+    setCurrentAssociationTarget(associationTarget);
+    onChange(formatAssociationValues());
+  };
+
+  return (
+    <ActionGrid>
+      <ActionLeftSide>
+        <ActionTitle>
+          {translate(
+            'pimee_catalog_rule.form.edit.actions.set_associations.select_association_type'
+          )}
+        </ActionTitle>
+        <Label
+          className='AknFieldContainer-label control-label'
+          label={`${translate(
+            'pimee_catalog_rule.form.edit.actions.set_associations.association_types'
+          )} ${translate('pim_common.required_label')}`}
+        />
+        <ul>
+          {Array.from(associationValues.entries()).map(
+            ([associationTarget, value]) => {
+              return (
+                <li
+                  key={`${associationTarget.associationTypeCode}-${associationTarget.target}`}
+                  className={'AknBadgedSelector-item'}>
+                  <button
+                    data-testid={`association-type-selector-${associationTarget.associationTypeCode}-${associationTarget.target}`}
+                    className={`AknTextField AknBadgedSelector${
+                      isCurrentAssociationTargetOrDefault(associationTarget)
+                        ? ' AknBadgedSelector--selected'
+                        : ''
+                    }`}
+                    onClick={e => {
+                      e.preventDefault();
+                      setCurrentAssociationTarget(associationTarget);
+                    }}>
+                    {getAssociationTypeLabel(
+                      associationTarget.associationTypeCode
+                    )}
+                    <span className='AknBadgedSelector-helper'>
+                      {translate(
+                        `pimee_catalog_rule.form.edit.actions.set_associations.counts.${associationTarget.target}`,
+                        { count: value.length },
+                        value.length
+                      )}
+                    </span>
+                    <span
+                      className='AknBadgedSelector-delete'
+                      tabIndex={0}
+                      onClick={() =>
+                        handleAssociationTargetDelete(associationTarget)
+                      }
+                      role='button'
+                    />
+                  </button>
+                </li>
+              );
+            }
+          )}
+          <li className={'AknBadgedSelector-item'}>
+            <AddAssociationTypeButton
+              onAddAssociationType={handleAddAssociationType}
+              selectedTargets={Array.from(associationValues.keys())}
+            />
+          </li>
+        </ul>
+      </ActionLeftSide>
+      <ActionRightSide>
+        {currentAssociationTarget && (
+          <>
+            <ActionTitle>
+              {translate(
+                `pimee_catalog_rule.form.edit.actions.set_associations.select_title.${currentAssociationTarget.target}`
+              )}
+            </ActionTitle>
+            <Label
+              className='AknFieldContainer-label control-label'
+              label={`${translate(
+                `pimee_catalog_rule.form.edit.actions.set_associations.select.${currentAssociationTarget?.target}`
+              )} ${translate('pim_common.required_label')}`}
+            />
+            {currentAssociationTarget.target === 'groups' && (
+              <AssociationsGroupsSelector
+                groupCodes={
+                  (associationValues.get(
+                    currentAssociationTarget
+                  ) as GroupCode[]) || []
+                }
+                currentCatalogLocale={currentCatalogLocale}
+                onChange={groupCodes =>
+                  handleChange(currentAssociationTarget, groupCodes)
+                }
+              />
+            )}
+            {currentAssociationTarget.target === 'products' && (
+              <AssociationsIdentifiersSelector
+                entityType='product'
+                identifiers={
+                  (associationValues.get(
+                    currentAssociationTarget
+                  ) as ProductIdentifier[]) || []
+                }
+                onChange={productIdentifiers =>
+                  handleChange(currentAssociationTarget, productIdentifiers)
+                }
+              />
+            )}
+            {currentAssociationTarget.target === 'product_models' && (
+              <AssociationsIdentifiersSelector
+                entityType='product_model'
+                identifiers={
+                  (associationValues.get(
+                    currentAssociationTarget
+                  ) as ProductModelCode[]) || []
+                }
+                onChange={productModelCodes =>
+                  handleChange(currentAssociationTarget, productModelCodes)
+                }
+              />
+            )}
+          </>
+        )}
+      </ActionRightSide>
+    </ActionGrid>
+  );
+};
+
+export { AssociationTypesSelector };
