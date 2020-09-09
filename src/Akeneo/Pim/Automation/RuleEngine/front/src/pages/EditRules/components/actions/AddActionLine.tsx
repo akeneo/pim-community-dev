57bc91d195fd9046b7f9a17defdc2146f86f2aa2
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-09-09 18:02:39 +0200

    RUL-363: remove useRegisterConsts from AddActionLine

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/AddActionLine.tsx
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/pages/EditRules/components/actions/AddActionLine.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { AddAction } from '../../../../models/actions/';
+import { Controller } from 'react-hook-form';
 import { ActionTemplate } from './ActionTemplate';
 import { ActionLineProps } from './ActionLineProps';
 import { FallbackField } from '../FallbackField';
-import { useRegisterConsts } from '../../hooks/useRegisterConst';
+import { useControlledFormInputAction } from '../../hooks';
 
-type Props = {
-  action: AddAction;
-} & ActionLineProps;
-
-const AddActionLine: React.FC<Props> = ({
+const AddActionLine: React.FC<ActionLineProps> = ({
   lineNumber,
-  action,
   handleDelete,
 }) => {
-  const values: any = {
-    type: 'add',
-    field: action.field,
-    items: action.items,
-  };
-
-  if (action.locale) {
-    values.locale = action.locale;
-  }
-
-  if (action.scope) {
-    values.scope = action.scope;
-  }
-
-  useRegisterConsts(values, `content.actions[${lineNumber}]`);
+  const { formName, getFormValue } = useControlledFormInputAction<boolean>(
+    lineNumber
+  );
+  const items = getFormValue('items');
 
   return (
     <ActionTemplate
@@ -40,16 +24,45 @@ const AddActionLine: React.FC<Props> = ({
       <div className='AknGrid AknGrid--unclickable'>
         <div className='AknGrid-bodyRow AknGrid-bodyRow--highlight'>
           <div className='AknGrid-bodyCell'>
+            <Controller
+              as={<span hidden />}
+              name={formName('type')}
+              defaultValue={'add'}
+            />
+            <Controller
+              as={<span hidden />}
+              name={formName('field')}
+              defaultValue={getFormValue('field')}
+            />
+            <Controller
+              as={<span hidden />}
+              name={formName('items')}
+              defaultValue={getFormValue('items')}
+            />
+            {getFormValue('locale') && (
+              <Controller
+                as={<span hidden />}
+                name={formName('locale')}
+                defaultValue={getFormValue('locale')}
+              />
+            )}
+            {getFormValue('scope') && (
+              <Controller
+                as={<span hidden />}
+                name={formName('scope')}
+                defaultValue={getFormValue('scope')}
+              />
+            )}
             {/* It is not translated since it is temporary. */}
-            The value{action.items.length > 1 && 's'}&nbsp;
-            <span className='AknRule-attribute'>{action.items.join(', ')}</span>
+            The value{items.length > 1 && 's'}&nbsp;
+            <span className='AknRule-attribute'>{items.join(', ')}</span>
             &nbsp;
-            {action.items.length > 1 ? ' are' : ' is'}
+            {items.length > 1 ? ' are' : ' is'}
             &nbsp;added to&nbsp;
             <FallbackField
-              field={action.field}
-              scope={action.scope}
-              locale={action.locale}
+              field={getFormValue('field')}
+              scope={getFormValue('scope')}
+              locale={getFormValue('locale')}
             />
             .
           </div>
