7047710a14b1960099b821167c67f1b05f359abc
Julien Sanchez <sumbobyboys@gmail.com>
2020-09-10 17:03:28 +0200

    RAC-223: fix lint problems

--- akeneo-design-system/src/all.visual.tsx
+++ akeneo-design-system/src/all.visual.tsx
@@ -1,20 +1,32 @@
 import 'expect-puppeteer';
-const fs = require('fs');
+import fs from 'fs';
 
 import {toMatchImageSnapshot} from 'jest-image-snapshot';
 
 expect.extend({toMatchImageSnapshot});
 
-console.log(Object.values(JSON.parse(fs.readFileSync('./stories.json').toString('utf8')).stories));
-const stories = Object.values(JSON.parse(fs.readFileSync('./stories.json').toString('utf8')).stories) as {id: string}[];
+type StoriesDump = {
+  stories: {
+    [storyKey: string]: {
+      id: string;
+      kind: string;
+      name: string;
+    };
+  };
+};
+
+const storyFileContent = fs.readFileSync('./stories.json').toString('utf8');
+const storiesDump = JSON.parse(storyFileContent) as StoriesDump;
+
+const stories = Object.values(storiesDump.stories);
 
 describe('Checkbox visual tests', () => {
-  stories.map((story) => {
+  stories.map(story => {
     if (story.id.indexOf('components') !== 0) {
       return;
     }
 
-    it(`Renders ${story.id} correctly`, async () => {
+    it(`Renders ${story.kind}/${story.name} correctly`, async () => {
       await page.goto(`http://localhost:6006/iframe.html?id=${story.id}`);
       const root = await page.$('#root');
       if (null === root) return;
