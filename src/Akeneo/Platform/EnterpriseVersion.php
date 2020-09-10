0cdd44c7c0cc3279e57bc4a46b823b1451cac307
Michel Tag <micheltag@akeneo.com>
2020-09-10 09:02:12 +0200

    Preparing version 3.2.70 (#9575)
    Preparing version 3.2.70
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.69';
+    const VERSION = '3.2.70';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Edelweiss';
