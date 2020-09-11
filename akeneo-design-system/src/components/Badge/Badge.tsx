be216931e7b5ddbfea774593fd277099ef08b693
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-09-11 17:20:15 +0200

    RAC-226: Fix Badge style

--- akeneo-design-system/src/components/Badge/Badge.tsx
+++ akeneo-design-system/src/components/Badge/Badge.tsx
@@ -3,12 +3,14 @@ import styled, {css} from 'styled-components';
 import {AkeneoThemedProps, Level} from '../../theme';
 
 const BadgeContainer = styled.span<BadgeProps & AkeneoThemedProps>`
+  display: inline-block;
   height: 18px;
   line-height: 16px;
   border: 1px solid;
   padding: 0 6px;
   border-radius: 2px;
   text-transform: uppercase;
+  box-sizing: border-box;
   background-color: ${({theme}: AkeneoThemedProps) => theme.color.white};
   font-size: ${({theme}: AkeneoThemedProps) => theme.fontSize.small};
 
