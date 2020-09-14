6a0c681df711d8e6f5d36cd541d1cdb2393f4117
pierallard <pierallard@gmail.com>
2020-09-14 16:10:15 +0200

    RUL-254: Fix rebase

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/models/actions/ActionModuleGuesser.ts
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/models/actions/ActionModuleGuesser.ts
@@ -61,7 +61,6 @@ const getActionModule: (
     getClearAttributeActionModule,
     getRemoveAttributeValueActionModule,
     // Fallbacks
-    getClearActionModule,
     getAddActionModule,
     getSetActionModule,
   ];
