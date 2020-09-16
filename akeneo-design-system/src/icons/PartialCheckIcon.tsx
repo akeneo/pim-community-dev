69dc53d61a8f73f5ae542084fb6efa84da6d6467
Valentin Mumble <valentin.dijkstra@akeneo.com>
2020-09-16 14:06:09 +0200

    RAC-262: Fix Checkbox animation (#12764)

--- akeneo-design-system/src/icons/PartialCheckIcon.tsx
+++ akeneo-design-system/src/icons/PartialCheckIcon.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const PartialCheckIcon = ({
-  width = 24,
-  height = 24,
+  width = 27,
+  height = 27,
   className = '',
 }: {
   width: number;
   height: number;
   className?: string;
 }) => (
-  <svg width={width} height={height} viewBox="0 0 24 24">
+  <svg width={width} height={height} viewBox="0 0 27 27">
     <path
       className={className}
       d="M2 12.5h20"
       stroke="#FFFFFF"
+      strokeWidth={2}
       fill="none"
       fillRule="evenodd"
       strokeLinecap="round"
