ea53ec632535208702dc55c43de6505b0ee1a2da
franckbrouard <franck.brouard@akeneo.fr>
2020-09-17 11:15:55 +0200

    webhook-octopus - add pubusub/viewer access

--- deployments/terraform/pubsub.tf
+++ deployments/terraform/pubsub.tf
@@ -71,6 +71,28 @@ resource "google_pubsub_topic_iam_member" "pubsub_publisher_job_queue" {
   ]
 }
 
+resource "google_pubsub_topic_iam_member" "pubsub_viewer_job_queue" {
+  topic  = google_pubsub_topic.job-queue.name
+  role   = "roles/pubsub.viewer"
+  member = "serviceAccount:${google_service_account.pim_service_account.email}"
+
+  depends_on = [
+    google_service_account.pim_service_account,
+    google_pubsub_topic.job-queue,
+  ]
+}
+
+resource "google_pubsub_topic_iam_member" "pubsub_viewer_business-event" {
+  topic  = google_pubsub_topic.business-event.name
+  role   = "roles/pubsub.viewer"
+  member = "serviceAccount:${google_service_account.pim_service_account.email}"
+
+  depends_on = [
+    google_service_account.pim_service_account,
+    google_pubsub_topic.business-event,
+  ]
+}
+
 resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_webhook" {
   subscription = google_pubsub_subscription.webhook.name
   role         = "roles/pubsub.subscriber"
@@ -85,6 +107,28 @@ resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_webhook" {
 resource "google_pubsub_subscription_iam_member" "pubsub_subscriber_job_queue" {
   subscription  = google_pubsub_subscription.job-queue.name
   role          = "roles/pubsub.subscriber"
+  member        = "serviceAccount:${google_service_account.pim_service_account.email}"
+
+  depends_on = [
+    google_service_account.pim_service_account,
+    google_pubsub_subscription.job-queue,
+  ]
+}
+
+resource "google_pubsub_subscription_iam_member" "pubsub_viewer_webhook" {
+  subscription = google_pubsub_subscription.webhook.name
+  role         = "roles/pubsub.viewer"
+  member       = "serviceAccount:${google_service_account.pim_service_account.email}"
+
+  depends_on = [
+    google_service_account.pim_service_account,
+    google_pubsub_subscription.webhook,
+  ]
+}
+
+resource "google_pubsub_subscription_iam_member" "pubsub_viewer_job_queue" {
+  subscription  = google_pubsub_subscription.job-queue.name
+  role          = "roles/pubsub.viewer"
   member        = "serviceAccount:${google_service_account.pim_service_account.email}"
 
   depends_on = [
