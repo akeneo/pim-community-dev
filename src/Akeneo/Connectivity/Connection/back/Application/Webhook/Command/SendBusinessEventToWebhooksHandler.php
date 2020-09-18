650c9e0b7f78cb8b4146a360464c7b7cd8ef1493
Paul Chasle <paul.chasle@akeneo.com>
2020-09-18 16:35:14 +0200

    API-1282: set user permission on webhook handling

--- src/Akeneo/Connectivity/Connection/back/Application/Webhook/Command/SendBusinessEventToWebhooksHandler.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Webhook/Command/SendBusinessEventToWebhooksHandler.php
@@ -9,7 +9,11 @@ use Akeneo\Connectivity\Connection\Domain\Webhook\Client\WebhookClient;
 use Akeneo\Connectivity\Connection\Domain\Webhook\Client\WebhookRequest;
 use Akeneo\Connectivity\Connection\Domain\Webhook\Persistence\Query\SelectActiveWebhooksQuery;
 use Akeneo\Connectivity\Connection\Domain\Webhook\WebhookEvent\WebhookEventBuildingFailedException;
+use Doctrine\Common\Persistence\ObjectRepository;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @author    Thomas Galvaing <thomas.galvaing@akeneo.com>
@@ -30,16 +34,26 @@ final class SendBusinessEventToWebhooksHandler
     /** @var LoggerInterface */
     private $logger;
 
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    /** @var ObjectRepository */
+    private $userRepository;
+
     public function __construct(
         SelectActiveWebhooksQuery $selectActiveWebhooksQuery,
         WebhookClient $client,
         WebhookEventBuilder $builder,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        TokenStorageInterface $tokenStorage,
+        ObjectRepository $userRepository
     ) {
         $this->selectActiveWebhooksQuery = $selectActiveWebhooksQuery;
         $this->client = $client;
         $this->builder = $builder;
         $this->logger = $logger;
+        $this->tokenStorage = $tokenStorage;
+        $this->userRepository = $userRepository;
     }
 
     public function handle(SendBusinessEventToWebhooksCommand $command): void
@@ -53,6 +67,11 @@ final class SendBusinessEventToWebhooksHandler
 
         $requests = function () use ($businessEvent, $webhooks) {
             foreach ($webhooks as $webhook) {
+                /** @var UserInterface $user */
+                if (null !== $user = $this->userRepository->find($webhook->userId())) {
+                    $this->tokenStorage->setToken(new UsernamePasswordToken($user, null, 'main', $user->getRoles()));
+                }
+
                 $context = [
                     'user_id' => $webhook->userId()
                 ];
