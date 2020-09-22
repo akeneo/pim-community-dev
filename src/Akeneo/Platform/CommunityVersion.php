1e5a5353d4dcdf14dd89f87c8356cdb5141ea4b0
ahocquard <alexandre.hocquard@akeneo.com>
2020-09-22 17:54:47 +0200

    32.72

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.71';
+    const VERSION = '3.2.72';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Rabbit Transit';
