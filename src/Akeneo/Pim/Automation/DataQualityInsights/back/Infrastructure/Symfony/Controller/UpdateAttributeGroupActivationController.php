1f2ff52c4e54386fbc45195505356faec4a37385
Laurent Petard <laurent.petard@akeneo.com>
2020-09-22 12:18:06 +0200

    DAPI-1281: Add controller to update an attribute group activation

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Symfony/Controller/UpdateAttributeGroupActivationController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\DataQualityInsights\Infrastructure\Symfony\Controller;
+
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\Model\AttributeGroupActivation;
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\Repository\AttributeGroupActivationRepositoryInterface;
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\AttributeGroupCode;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class UpdateAttributeGroupActivationController
+{
+    /** @var AttributeGroupActivationRepositoryInterface */
+    private $attributeGroupActivationRepository;
+
+    public function __construct(AttributeGroupActivationRepositoryInterface $attributeGroupActivationRepository)
+    {
+        $this->attributeGroupActivationRepository = $attributeGroupActivationRepository;
+    }
+
+    public function __invoke(Request $request)
+    {
+        try {
+            $attributeGroupCode = new AttributeGroupCode($request->request->get('attribute_group_code'));
+            $activated = $request->request->get('activated');
+            $attributeGroupActivation = new AttributeGroupActivation($attributeGroupCode, $activated);
+        } catch (\Throwable $e) {
+            return new Response(null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $this->attributeGroupActivationRepository->save($attributeGroupActivation);
+
+        return new Response(null, Response::HTTP_NO_CONTENT);
+    }
+}
