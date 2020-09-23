50205f0b03c060dc9d6cfe87635eb31d12461b88
Laurent Petard <laurent.petard@akeneo.com>
2020-09-23 11:32:57 +0200

    Improve log messages

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Subscriber/AttributeGroup/UpdateAttributeGroupActivationSubscriber.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Subscriber/AttributeGroup/UpdateAttributeGroupActivationSubscriber.php
@@ -74,9 +74,10 @@ final class UpdateAttributeGroupActivationSubscriber implements EventSubscriberI
             }
         } catch (\Throwable $exception) {
             $this->logger->error(
-                sprintf('Unable to create attribute group activation for "%s"', $attributeGroup->getCode()),
+                'Unable to create attribute group activation for "{attribute_group_code}"',
                 [
-                    'error_code' => 'unable_to_create_attribute_group_activation',
+                    'attribute_group_code' => $attributeGroup->getCode(),
+                    'error_code' => 'unable_to_remove_attribute_group_activation',
                     'error_message' => $exception->getMessage(),
                 ]
             );
@@ -99,9 +100,9 @@ final class UpdateAttributeGroupActivationSubscriber implements EventSubscriberI
             $this->attributeGroupActivationRepository->remove($attributeGroupCode);
         } catch (\Throwable $exception) {
             $this->logger->error(
-                'Unable to remove attribute group activation for "{attributegroup_code}"',
+                'Unable to remove attribute group activation for "{attribute_group_code}"',
                 [
-                    'attributegroup_code' => $attributeGroup->getCode(),
+                    'attribute_group_code' => $attributeGroup->getCode(),
                     'error_code' => 'unable_to_remove_attribute_group_activation',
                     'error_message' => $exception->getMessage(),
                 ]
