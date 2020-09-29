7edb05d2f2b306d44b9184ec4868bb1bac77a70b
Olivier Pontier <olivier.pontier@akeneo.com>
2020-09-29 17:51:10 +0200

    use styled components for Page Header

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/components/PageHeader.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/components/PageHeader.tsx
@@ -22,6 +22,82 @@ const ButtonCollection = styled.div.attrs(() => ({className: 'AknTitleContainer-
   }
 `;
 
+const LineContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const MainContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    max-width: 100%;
+`;
+
+const State = styled.div`
+  :not(:empty) {
+    order: 50;
+    align-self: center;
+    margin-right: 0;
+    margin-top: 6px;
+    text-align: right;
+    margin-left: 20px;
+  }
+`;
+
+const Title = styled.div`
+    color: ${({theme}) => theme.color.purple100};
+    font-size: ${({theme}) => theme.fontSize.title};
+    line-height: 34px;
+    margin: 0;
+    font-weight: normal;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    flex-grow: 1;
+
+    &:first-letter {
+      text-transform: uppercase;
+    }
+  }
+`;
+
+const Breadcrumbs = styled.div`
+    flex-grow: 1;
+    max-height: 32px;
+    overflow: hidden;
+    margin-right: 20px;
+    line-height: 16px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+`;
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    align-content: baseline;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 142px;
+  height: 142px;
+  border: 1px solid ${({theme}) => theme.color.grey80};
+  margin-right: 20px;
+  border-radius: 4px;
+  display: flex;
+  overflow: hidden;
+  flex-basis: 142px;
+  flex-shrink: 0;
+  
+  img {
+    max-height: 140px;
+    max-width: 140px;
+    width: auto;
+  }
+`;
+
+
 type PageHeaderProps = {
   breadcrumb?: ReactElement;
   buttons?: ReactElement[];
@@ -41,18 +117,18 @@ const PageHeader: FC<PageHeaderProps> = ({
   showPlaceholder
 }) => (
   <Header>
-    <div className="AknTitleContainer-line">
+    <LineContainer>
       {imageSrc && (
-        <div className="AknImage AknImage--readOnly">
+        <ImageContainer>
           <img className="AknImage-display" src={imageSrc} alt={title as string} />
-        </div>
+        </ImageContainer>
       )}
 
-      <div className="AknTitleContainer-mainContainer">
+      <MainContainer>
         <div>
-          <div className="AknTitleContainer-line">
-            <div className="AknTitleContainer-breadcrumbs">{breadcrumb}</div>
-            <div className="AknTitleContainer-buttonsContainer">
+          <LineContainer>
+            <Breadcrumbs>{breadcrumb}</Breadcrumbs>
+            <ButtonsContainer>
               {userButtons}
               {buttons && (
                 <ButtonCollection>
@@ -61,10 +137,10 @@ const PageHeader: FC<PageHeaderProps> = ({
                   ))}
                 </ButtonCollection>
               )}
-            </div>
-          </div>
-          <div className="AknTitleContainer-line">
-            <div className="AknTitleContainer-title">
+            </ButtonsContainer>
+          </LineContainer>
+          <LineContainer>
+            <Title>
               {showPlaceholder ? (
                   <LoadingPlaceholderContainer>
                     <PageHeaderPlaceholder />
@@ -72,12 +148,12 @@ const PageHeader: FC<PageHeaderProps> = ({
               ) : (
                   <>{title}</>
               )}
-            </div>
-            <div className="AknTitleContainer-state">{state}</div>
-          </div>
+            </Title>
+            <State>{state}</State>
+          </LineContainer>
         </div>
-      </div>
-    </div>
+      </MainContainer>
+    </LineContainer>
   </Header>
 );
 
