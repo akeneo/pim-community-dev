183289f57aa8fabf28bd833c58de403eadbe10c4
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-09-30 18:04:29 +0200

    DAPI-1288: set a fixed with to the drag header cell when the reorder is active

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/shared/datagrids/HeaderRow.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/shared/datagrids/HeaderRow.tsx
@@ -10,7 +10,7 @@ const HeaderRow: FC<Props> = ({children}) => {
   return (
     <TableHead>
       <TableHeadRow>
-        {isDraggable && <TableHeadCell isFilterable={isFilterable} />}
+        {isDraggable && <TableHeadCell isFilterable={isFilterable} style={{width: 40}}/>}
         {React.Children.map(children, element => (
           <TableHeadCell isFilterable={isFilterable}>{element}</TableHeadCell>
         ))}
