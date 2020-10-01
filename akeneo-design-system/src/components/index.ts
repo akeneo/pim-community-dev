0e27b833789841c4c5ad43ac489b916a519cecc1
Steven VAIDIE <steven.vaidie@gmail.com>
2020-10-01 11:27:19 +0200

    RAC-224: Split Helper into 3 component to simplify component, usage are totally different

--- akeneo-design-system/src/components/index.ts
+++ akeneo-design-system/src/components/index.ts
@@ -2,5 +2,7 @@ export * from './Card/Card';
 export * from './Checkbox/Checkbox';
 export * from './Badge/Badge';
 export * from './Button/Button';
-export * from './Link/Link';
 export * from './Helper/Helper';
+export * from './InformationHelper/InformationHelper';
+export * from './InlineHelper/InlineHelper';
+export * from './Link/Link';
