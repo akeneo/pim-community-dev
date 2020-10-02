c98d0e082203914ec0f2f0f47988710e4d624d3e
Valentin Mumble <valentin.dijkstra@akeneo.com>
2020-10-02 16:39:07 +0200

    RAC-306: Add useShortcut unit test with provided ref (#12917)

--- akeneo-design-system/src/hooks/useShortcut.unit.ts
+++ akeneo-design-system/src/hooks/useShortcut.unit.ts
@@ -12,10 +12,36 @@ test('It can register listener on keyboard events', () => {
   expect(callback).toHaveBeenCalled();
 });
 
-test('It does not listen on other event', () => {
+test('It listens only on the given Key events', () => {
   const callback = jest.fn();
 
   renderHook(() => useShortcut(Key.Enter, callback));
+
+  fireEvent.keyDown(document, {key: 'Space', code: 'Space'});
+  expect(callback).not.toHaveBeenCalled();
+});
+
+test('It can listen on a provided ref', () => {
+  const callback = jest.fn();
+
+  const ref = {
+    current: document.createElement('div'),
+  };
+
+  renderHook(() => useShortcut(Key.Space, callback, ref));
+
+  fireEvent.keyDown(ref.current, {key: 'Space', code: 'Space'});
+  expect(callback).toHaveBeenCalled();
+});
+
+test('It does not listen on document events when a ref is provided', () => {
+  const callback = jest.fn();
+
+  const ref = {
+    current: document.createElement('div'),
+  };
+
+  renderHook(() => useShortcut(Key.Space, callback, ref));
 
   fireEvent.keyDown(document, {key: 'Space', code: 'Space'});
   expect(callback).not.toHaveBeenCalled();
