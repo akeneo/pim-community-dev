957b69888f5a80e0d84b6f34fe1435496805c2ec
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-12 14:59:57 +0200

    DSM test

---
+++ akeneo-design-system/src/components/Coucou/Coucou.tsx
@@ -0,0 +1,35 @@
+import React, {Ref} from 'react';
+import styled from 'styled-components';
+
+//TODO be sure to select the appropriate container element here
+const CoucouContainer = styled.span<{level: string}>`
+  color: red;
+`;
+
+type CoucouProps = {
+  /**
+   * TODO
+   */
+  level?: string;
+
+  /**
+   * TODO
+   */
+  children?: string;
+};
+
+/**
+ * TODO
+ */
+const Coucou = React.forwardRef<HTMLSpanElement, CoucouProps>(
+  ({level = 'primary', children, ...rest}: CoucouProps, forwardedRef: Ref<HTMLSpanElement>) => {
+    return (
+      <CoucouContainer level={level} ref={forwardedRef} {...rest}>
+        DSM demo
+        {children}
+      </CoucouContainer>
+    );
+  }
+);
+
+export {Coucou};
