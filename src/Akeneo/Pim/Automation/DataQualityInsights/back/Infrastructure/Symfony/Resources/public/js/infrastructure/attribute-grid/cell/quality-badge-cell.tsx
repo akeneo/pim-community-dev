09ca8483e7d6a2498d178598f6c75870d67a4bae
Olivier Pontier <olivier.pontier@akeneo.com>
2020-10-13 20:28:52 +0200

    DAPI-1386: adapt Badges in DataQuality components to use DSM styles

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Symfony/Resources/public/js/infrastructure/attribute-grid/cell/quality-badge-cell.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Symfony/Resources/public/js/infrastructure/attribute-grid/cell/quality-badge-cell.tsx
@@ -1,11 +1,14 @@
+import {getQualityBadgeLevel} from "@akeneo-pim-ee/data-quality-insights/src/application/helper";
+
 const StringCell = require('oro/datagrid/string-cell');
 const __ = require('oro/translator');
 
 class QualityBadgeCell extends StringCell {
   render() {
     const attributeEvaluationResult = this.formatter.fromRaw(this.model.get(this.column.get('name')));
+    const badgeLevel = getQualityBadgeLevel(attributeEvaluationResult)
 
-    const cssClass = `AknDataQualityInsightsQualityBadge AknDataQualityInsightsQualityBadge--${attributeEvaluationResult.replace('_', '-')}`;
+    const cssClass = `AknBadge AknBadge--${badgeLevel}`;
 
     const content = `<span class="${cssClass}">${__('akeneo_data_quality_insights.attribute_grid.quality.' + attributeEvaluationResult)}</span>`;
     this.$el.empty().html(content);
