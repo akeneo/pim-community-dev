d627bc24b8d1f130380209d63a2e7094c44d0a0b
Mathias Métayer <mathias.metayer@akeneo.com>
2020-10-13 11:16:40 +0200

    PM2020-11: Add migration; update fixtures

---
+++ upgrades/test_schema/Version_5_0_20201013070505_add_convert_to_simple_products_job_Integration.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema\Tests;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+
+/**
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Version_5_0_20201013070505_add_convert_to_simple_products_job_Integration extends TestCase
+{
+    use ExecuteMigrationTrait;
+
+    private const JOB_CODE = 'convert_to_simple_product';
+    private const MIGRATION_LABEL = '_5_0_20201013070505_add_convert_to_simple_product_job';
+
+    /**
+     * @test
+     */
+    public function it_creates_the_job_instance()
+    {
+        $this->get('database_connection')->executeQuery(
+            'DELETE FROM akeneo_batch_job_instance WHERE code = :code',
+            ['code' => static::JOB_CODE]
+        );
+        $this->assertFalse($this->jobExists(self::JOB_CODE));
+
+        $this->reExecuteMigration(self::MIGRATION_LABEL);
+        $this->assertTrue($this->jobExists(self::JOB_CODE));
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useTechnicalCatalog();
+    }
+
+    private function jobExists(string $code): bool
+    {
+        $results = $this->get('database_connection')->executeQuery(
+            'SELECT code FROM akeneo_batch_job_instance WHERE code = :code',
+            ['code' => $code]
+        );
+
+        return 0 < $results->rowCount();
+    }
+}
