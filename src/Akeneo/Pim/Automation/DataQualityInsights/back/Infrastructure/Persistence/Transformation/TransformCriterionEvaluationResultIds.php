d7bdb84a736e1356f0118f2af3c8e471fe08d8cb
Laurent Petard <laurent.petard@akeneo.com>
2020-10-19 11:41:21 +0200

    DAPI-1377: Add specs

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Persistence/Transformation/TransformCriterionEvaluationResultIds.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Persistence/Transformation/TransformCriterionEvaluationResultIds.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\DataQualityInsights\Infrastructure\Persistence\Transformation;
 
-use Doctrine\DBAL\Connection;
-
 /**
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
