7dce4161405cc415dbf8a166586df2a899c038d6
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-20 18:10:31 +0200

    RAC: Add Link handling in Button

--- akeneo-design-system/src/components/Button/Button.tsx
+++ akeneo-design-system/src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
-import React, {ReactNode, Ref, SyntheticEvent} from 'react';
+import React, {isValidElement, ReactNode, Ref, RefAttributes, SyntheticEvent, useRef} from 'react';
 import styled, {css} from 'styled-components';
 import {AkeneoThemedProps, getColorForLevel, getFontSize, Level} from '../../theme';
-import {Key, Override} from '../../shared';
-import {useShortcut} from '../../hooks';
+import {Override} from '../../shared';
+import {Link, LinkProps} from 'components';
 
 type ButtonSize = 'small' | 'default';
 
@@ -33,7 +33,7 @@ type ButtonProps = Override<
     /**
      * Function called when the user clicks on the button or hit enter when focused.
      */
-    onClick: (event: SyntheticEvent) => void;
+    onClick?: (event: SyntheticEvent) => void;
 
     /**
      * Accessibility label to describe shortly the button.
@@ -117,9 +117,17 @@ const Container = styled.button<
   &:disabled {
     cursor: not-allowed;
   }
+
+  a,
+  a:hover {
+    color: inherit;
+    text-decoration: none;
+    cursor: inherit;
+  }
 `;
 
 /**
+ * TODO throw if using <a>
  * Buttons express what action will occur when the users clicks.
  * Buttons are used to initialize an action, either in the background or foreground of an experience.
  */
@@ -140,7 +148,33 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     }: ButtonProps,
     forwardedRef: Ref<HTMLButtonElement>
   ) => {
-    const ref = useShortcut(Key.Enter, disabled ? () => null : onClick, forwardedRef);
+    const linkRef = useRef<HTMLAnchorElement>(null);
+
+    const mappedChildren = React.Children.map(children, child => {
+      if (isValidElement<LinkProps & RefAttributes<HTMLAnchorElement>>(child) && child.type === Link) {
+        if (undefined !== onClick) {
+          throw new Error('Button can not have both `onClick` and a Link as child');
+        }
+
+        if (1 < React.Children.count(children)) {
+          throw new Error('Link can only be the single child of a Button');
+        }
+
+        return React.cloneElement(child, {ref: linkRef, disabled});
+      } else {
+        return child;
+      }
+    });
+
+    const handleAction = (event: SyntheticEvent) => {
+      if (disabled) return;
+
+      if (null !== linkRef.current) {
+        linkRef.current.click();
+      } else {
+        undefined !== onClick && onClick(event);
+      }
+    };
 
     return (
       <Container
@@ -152,13 +186,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         aria-disabled={disabled}
         aria-label={ariaLabel}
         aria-labelledby={ariaLabelledBy}
-        ref={ref}
+        ref={forwardedRef}
         role="button"
         type={type}
-        onClick={disabled ? null : onClick}
+        onClick={handleAction}
         {...rest}
       >
-        {children}
+        {mappedChildren}
       </Container>
     );
   }
