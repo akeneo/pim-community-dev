38c98060c07d42594dd797672c8cc214f7d5ab8a
Steven VAIDIE <steven.vaidie@gmail.com>
2020-10-20 12:55:07 +0200

    RAC-285: add indeterminate state, light props for DQI and manage width

--- akeneo-design-system/src/components/ProgressBar/ProgressBar.tsx
+++ akeneo-design-system/src/components/ProgressBar/ProgressBar.tsx
@@ -1,14 +1,28 @@
-import React, {Ref} from 'react';
-import styled, {css} from 'styled-components';
+import React, {Ref, useEffect, useRef, useState} from 'react';
+import styled, {css, keyframes} from 'styled-components';
 import {AkeneoThemedProps, getColor, getColorForLevel, getFontSize, Level} from '../../theme';
+import {useWindowSize} from "../../hooks/useWindowSize";
+import {uuid} from "../../shared";
 
 const ProgressBarContainer = styled.div``;
 
-const Header = styled.div`
+const progressBarAnimation = keyframes`
+  from { background-position: 0 0; }
+  to { background-position: 20px 0; }
+`;
+
+const Header = styled.div<{isMinimized: boolean} & AkeneoThemedProps>`
   display: flex;
   align-items: stretch;
   flex-direction: row;
   font-size: ${getFontSize('default')};
+  margin-bottom: -4px;
+  ${props =>
+  props.isMinimized &&
+  css`
+      flex-direction: column;
+      margin-bottom: 0px;
+    `}
 `;
 
 const Title = styled.div`
@@ -24,20 +38,21 @@ const Title = styled.div`
 const ProgressLabel = styled.div`
   color: ${getColor('grey120')};
   flex-shrink: 0;
+  padding-bottom: 4px;
 `;
 
-const ProgressBarBackground = styled.div<{height: ProgressBarHeight} & AkeneoThemedProps>`
+const ProgressBarBackground = styled.div<{size: ProgressBarSize} & AkeneoThemedProps>`
   background: ${getColor('grey80')};
-  height: ${props => getHeight(props.height)};
+  height: ${props => getHeightFromSize(props.size)};
   overflow: hidden;
   position: relative;
 `;
 
-const ProgressBarFill = styled.div.attrs<{width: number; level: Level}>(props => ({
+const ProgressBarFill = styled.div.attrs<{width: number; level: Level; indeterminate: boolean, light: boolean}>(props => ({
   style: {width: props.width + '%'},
-}))<{level: Level}>`
-  ${({level}: {level: Level} & AkeneoThemedProps) => css`
-    background: ${getColorForLevel(level, 100)};
+}))<{level: Level, light: boolean} & AkeneoThemedProps>`
+  ${({level, light}: {level: Level, light: boolean} & AkeneoThemedProps) => css`
+    background: ${getColorForLevel(level, light ? 60 : 100)};
   `}
 
   height: 100%;
@@ -45,10 +60,26 @@ const ProgressBarFill = styled.div.attrs<{width: number; level: Level}>(props =>
   position: absolute;
   top: 0;
   transition: width 0.3s;
+
+  ${props => props.indeterminate && css`
+    background-image: linear-gradient(
+      315deg,
+      rgba(255,255,255,0.6) 25%,
+      rgba(255,255,255,0.4) 25%,
+      rgba(255,255,255,0.4) 50%,
+      rgba(255,255,255,0.6) 50%,
+      rgba(255,255,255,0.6) 75%,
+      rgba(255,255,255,0.4) 75%,
+      rgba(255,255,255,0.4) 100%
+    );
+    background-size: 20px 20px;
+    transition: width 200ms ease;
+    animation: ${progressBarAnimation} 1s linear infinite;
+  `}
 `;
 
-const getHeight = (height: ProgressBarHeight): string => {
-  switch (height) {
+const getHeightFromSize = (size: ProgressBarSize): string => {
+  switch (size) {
     case 'large':
       return '10px';
     case 'small':
@@ -57,19 +88,19 @@ const getHeight = (height: ProgressBarHeight): string => {
   }
 };
 
-const sanitizePercent = (percent: number): number => {
-  if (percent < 0) {
-    return 0;
+const computeWidthFromPercent = (percent: number | 'indeterminate'): number => {
+  if (percent === 'indeterminate' || percent > 100) {
+    return 100;
   }
 
-  if (percent > 100) {
-    return 100;
+  if (percent < 0) {
+    return 0;
   }
 
   return percent;
 };
 
-type ProgressBarHeight = 'small' | 'large';
+type ProgressBarSize = 'small' | 'large';
 
 type ProgressBarProps = {
   /**
@@ -78,9 +109,9 @@ type ProgressBarProps = {
   level: Level;
 
   /**
-   * The progression of the progress bar in percentage (from 0 to 100).
+   * The progression of the progress bar in percentage (from 0 to 100) when type is determinate, when type is indeterminate use indeterminate.
    */
-  percent: number;
+  percent: number | 'indeterminate';
 
   /**
    * Is the style of the progress bar should be light
@@ -98,9 +129,9 @@ type ProgressBarProps = {
   progressLabel?: string;
 
   /**
-   * Define the height of the progress bar.
+   * Define the size of the progress bar.
    */
-  height?: ProgressBarHeight;
+  size?: ProgressBarSize;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 /**
@@ -108,25 +139,48 @@ type ProgressBarProps = {
  */
 const ProgressBar = React.forwardRef<HTMLDivElement, ProgressBarProps>(
   (
-    {level, percent, title, progressLabel, height = 'small', ...rest}: ProgressBarProps,
+    {level, percent, title, progressLabel, light, size = 'small', ...rest}: ProgressBarProps,
     forwardedRef: Ref<HTMLDivElement>
   ) => {
+    const [labelId] = useState<string>(`label_${uuid()}`);
+    const [progressBarId] = useState<string>(`progress_${uuid()}`);
+    const [isMinimized, setIsMinimized] = useState(false);
+    const ref = null === forwardedRef ? useRef<HTMLDivElement>(null) : forwardedRef;
+
+    const windowSize = useWindowSize();
+    useEffect(() => {
+      if(typeof ref !== 'function' && null !== ref.current) {
+        const element = (ref.current as unknown) as HTMLDivElement;
+        setIsMinimized(element.getBoundingClientRect().width <= 300);
+      }
+    }, [windowSize])
+
+    const progressBarProps = {};
+    if (percent !== 'indeterminate') {
+      progressBarProps['aria-valuenow'] = computeWidthFromPercent(percent);
+      progressBarProps['aria-valuemin'] = 0;
+      progressBarProps['aria-valuemax'] = 100;
+    }
+
+    if (title) {
+      progressBarProps['aria-labelledby'] = labelId;
+    }
+
     return (
-      <ProgressBarContainer ref={forwardedRef} {...rest}>
+      <ProgressBarContainer ref={ref} {...rest}>
         {(title || progressLabel) && (
-          <Header>
-            <Title>{title}</Title>
+          <Header isMinimized={isMinimized}>
+            <Title id={labelId} htmlFor={progressBarId}>{title}</Title>
             <ProgressLabel>{progressLabel}</ProgressLabel>
           </Header>
         )}
         <ProgressBarBackground
+          id={progressBarId}
           role="progressbar"
-          aria-valuenow={sanitizePercent(percent)}
-          aria-valuemin="0"
-          aria-valuemax="100"
-          height={height}
+          {...progressBarProps}
+          size={size}
         >
-          <ProgressBarFill level={level} width={sanitizePercent(percent)} />
+          <ProgressBarFill level={level} light={light} indeterminate={percent === 'indeterminate'} width={computeWidthFromPercent(percent)} />
         </ProgressBarBackground>
       </ProgressBarContainer>
     );
