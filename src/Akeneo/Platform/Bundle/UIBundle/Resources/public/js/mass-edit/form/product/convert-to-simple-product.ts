13f2c55409bfab2f8d82e5999c43f160373d7aa1
Mathias Métayer <mathias.metayer@akeneo.com>
2020-10-20 11:34:10 +0200

    PM2020-11: Fix reviews

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/mass-edit/form/product/convert-to-simple-product.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/mass-edit/form/product/convert-to-simple-product.ts
@@ -1,8 +1,8 @@
 const BaseOperation = require('pim/mass-edit-form/product/operation');
 
 class ConvertToSimpleProduct extends BaseOperation {
-  public render() {
-    this.$el.text('');
+  public render(): ConvertToSimpleProduct {
+    this.$el.empty();
 
     return this;
   }
--- upgrades/schema/Version_5_0_20201013070505_add_convert_to_simple_products_job.php
+++ upgrades/schema/Version_5_0_20201013070505_add_convert_to_simple_products_job.php
@@ -5,13 +5,13 @@ namespace Pim\Upgrade\Schema;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version_5_0_20201013070505_add_convert_to_simple_product_job extends AbstractMigration
+final class Version_5_0_20201013070505_add_convert_to_simple_products_job extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         $sql = <<<SQL
         REPLACE INTO `akeneo_batch_job_instance` (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
-        VALUES ('convert_to_simple_product', 'Convert to simple product', 'convert_to_simple_product', 0, 'Akeneo Mass Edit Connector', 'a:0:{}', 'mass_edit');
+        VALUES ('convert_to_simple_products', 'Convert to simple products', 'convert_to_simple_products', 0, 'Akeneo Mass Edit Connector', 'a:0:{}', 'mass_edit');
         SQL;
 
         $this->addSql($sql);
