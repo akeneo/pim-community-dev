b8e1fa88aa670b23bc1474b2b230920541d5545c
Olivier Pontier <olivier.pontier@akeneo.com>
2020-10-21 20:11:04 +0200

    initialize refactoring of the DQI tab on product page

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/ProductEditFormApp.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/ProductEditFormApp.tsx
@@ -1,7 +1,12 @@
 import React, {FunctionComponent} from 'react';
 import {Provider} from "react-redux";
 import {productEditFormStore} from "../infrastructure/store";
-import {Product} from "@akeneo-pim-community/data-quality-insights/src/domain";
+import {
+  CriterionEvaluationResult,
+  Family,
+  MAX_RATE,
+  Product
+} from "@akeneo-pim-community/data-quality-insights/src/domain";
 import {
   CatalogContextListener,
   PageContextListener,
@@ -10,17 +15,64 @@ import {
 import {fetchProduct} from "@akeneo-pim-community/data-quality-insights/src/infrastructure/fetcher";
 import AttributesTabContent from "./component/ProductEditForm/TabContent/AttributesTabContent";
 import {DataQualityInsightsTabContent} from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent";
+import AxisEvaluation
+  from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/AxisEvaluation";
+import Criterion
+  from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Criterion";
+import {Recommendation} from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Recommendation";
 import {AxisRatesOverviewPortal} from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm";
 import {fetchProductDataQualityEvaluation} from "@akeneo-pim-community/data-quality-insights/src";
 import {AxesContextProvider} from "@akeneo-pim-community/data-quality-insights/src/application/context/AxesContext";
+import {
+  redirectToAttributeGridFilteredByFamilyAndQuality,
+  redirectToAttributeGridFilteredByFamilyAndQualityAndSelectAttributeTypes
+} from "@akeneo-pim-community/data-quality-insights/src/infrastructure/AttributeGridRouter";
+import {CRITERION_DONE} from "@akeneo-pim-community/data-quality-insights/src/domain/Evaluation.interface";
 import {ThemeProvider} from "styled-components";
 import {pimTheme} from "akeneo-design-system";
+import {BACK_LINK_SESSION_STORAGE_KEY} from "../index";
+
+const translate = require('oro/translator');
 
 interface ProductEditFormAppProps {
   catalogChannel: string;
   catalogLocale: string;
   product: Product;
 }
+
+const followAttributeSpellingCriterion = (criterionEvaluation: CriterionEvaluationResult, family: Family|null, product: Product) => {
+  if (family === null || criterionEvaluation.status !== CRITERION_DONE && criterionEvaluation.rate.value === MAX_RATE) {
+    return;
+  }
+  window.sessionStorage.setItem(BACK_LINK_SESSION_STORAGE_KEY, JSON.stringify({
+    label: translate('akeneo_data_quality_insights.product_edit_form.back_to_products'),
+    route: 'pim_enrich_product_edit',
+    routeParams: {id: product.meta.id},
+    displayLinkRoutes: [
+      'pim_enrich_attribute_index',
+      'pim_enrich_attribute_edit',
+
+    ],
+  }));
+  redirectToAttributeGridFilteredByFamilyAndQuality(family.meta.id);
+};
+
+const followAttributeOptionSpellingCriterion = (criterionEvaluation: CriterionEvaluationResult, family: Family|null, product: Product) => {
+  if (family === null || criterionEvaluation.status !== CRITERION_DONE || criterionEvaluation.rate.value === MAX_RATE) {
+    return;
+  }
+  window.sessionStorage.setItem(BACK_LINK_SESSION_STORAGE_KEY, JSON.stringify({
+    label: translate('akeneo_data_quality_insights.product_edit_form.back_to_products'),
+    route: 'pim_enrich_product_edit',
+    routeParams: {id: product.meta.id},
+    displayLinkRoutes: [
+      'pim_enrich_attribute_index',
+      'pim_enrich_attribute_edit',
+
+    ],
+  }));
+  redirectToAttributeGridFilteredByFamilyAndQualityAndSelectAttributeTypes(family.meta.id);
+};
 
 const ProductEditFormApp: FunctionComponent<ProductEditFormAppProps> = ({product, catalogChannel, catalogLocale}) => {
   return (
@@ -33,7 +85,26 @@ const ProductEditFormApp: FunctionComponent<ProductEditFormAppProps> = ({product
         <AttributesTabContent product={product}/>
 
         <AxesContextProvider axes={['enrichment', 'consistency']}>
-          <DataQualityInsightsTabContent product={product} productEvaluationFetcher={fetchProductDataQualityEvaluation}/>
+          <DataQualityInsightsTabContent product={product} productEvaluationFetcher={fetchProductDataQualityEvaluation}>
+            <AxisEvaluation axis={'enrichment'}>
+              <Criterion code={'completeness_of_non_required_attributes'}/>
+              <Criterion code={'completeness_of_required_attributes'}/>
+              <Criterion code={'missing_image_attribute'}>
+                <Recommendation supports={(criterion => criterion.improvable_attributes.length === 0)}>
+                  <span className="NotApplicableAttribute">{translate('akeneo_data_quality_insights.product_evaluation.messages.add_image_attribute_recommendation')}</span>
+                </Recommendation>
+              </Criterion>
+            </AxisEvaluation>
+
+            <AxisEvaluation axis={"consistency"}>
+              <Criterion code={'consistency_spelling'}/>
+              <Criterion code={'consistency_textarea_lowercase_words'}/>
+              <Criterion code={'consistency_textarea_uppercase_words'}/>
+              <Criterion code={'consistency_text_title_formatting'}/>
+              <Criterion code={'consistency_attribute_spelling'} followCriterion={followAttributeSpellingCriterion}/>
+              <Criterion code={'consistency_attribute_option_spelling'} followCriterion={followAttributeOptionSpellingCriterion}/>
+            </AxisEvaluation>
+          </DataQualityInsightsTabContent>
           <AxisRatesOverviewPortal />
         </AxesContextProvider>
       </Provider>
--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/ProductModelEditFormApp.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/ProductModelEditFormApp.tsx
@@ -10,17 +10,64 @@ import {
 } from "@akeneo-pim-community/data-quality-insights/src/application/listener";
 import AttributesTabContent from "./component/ProductEditForm/TabContent/AttributesTabContent";
 import {DataQualityInsightsTabContent} from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent";
+import AxisEvaluation
+  from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/AxisEvaluation";
+import Criterion
+  from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Criterion";
+import {Recommendation} from "@akeneo-pim-community/data-quality-insights/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Recommendation";
+import {CriterionEvaluationResult, Family, MAX_RATE,} from "@akeneo-pim-community/data-quality-insights/src/domain";
+import {
+  redirectToAttributeGridFilteredByFamilyAndQuality,
+  redirectToAttributeGridFilteredByFamilyAndQualityAndSelectAttributeTypes
+} from "@akeneo-pim-community/data-quality-insights/src/infrastructure/AttributeGridRouter";
+import {CRITERION_DONE} from "@akeneo-pim-community/data-quality-insights/src/domain/Evaluation.interface";
 import {fetchProductModelEvaluation} from "@akeneo-pim-community/data-quality-insights/src";
 import fetchProductModel
   from "@akeneo-pim-community/data-quality-insights/src/infrastructure/fetcher/ProductEditForm/fetchProductModel";
 import {ThemeProvider} from "styled-components";
 import {pimTheme} from "akeneo-design-system";
+import {BACK_LINK_SESSION_STORAGE_KEY} from "../index";
+
+const translate = require('oro/translator');
 
 interface ProductModelEditFormAppProps {
   catalogChannel: string;
   catalogLocale: string;
   product: Product;
 }
+
+const followAttributeSpellingCriterion = (criterionEvaluation: CriterionEvaluationResult, family: Family|null, product: Product) => {
+  if (family === null || criterionEvaluation.status !== CRITERION_DONE && criterionEvaluation.rate.value === MAX_RATE) {
+    return;
+  }
+  window.sessionStorage.setItem(BACK_LINK_SESSION_STORAGE_KEY, JSON.stringify({
+    label: translate('akeneo_data_quality_insights.product_edit_form.back_to_products'),
+    routeParams: {id: product.meta.id},
+    displayLinkRoutes: [
+      'pim_enrich_attribute_index',
+      'pim_enrich_attribute_edit',
+
+    ],
+  }));
+  redirectToAttributeGridFilteredByFamilyAndQuality(family.meta.id);
+};
+
+const followAttributeOptionSpellingCriterion = (criterionEvaluation: CriterionEvaluationResult, family: Family|null, product: Product) => {
+  if (family === null || criterionEvaluation.status !== CRITERION_DONE || criterionEvaluation.rate.value === MAX_RATE) {
+    return;
+  }
+  window.sessionStorage.setItem(BACK_LINK_SESSION_STORAGE_KEY, JSON.stringify({
+    label: translate('akeneo_data_quality_insights.product_edit_form.back_to_products'),
+    route: 'pim_enrich_product_model_edit',
+    routeParams: {id: product.meta.id},
+    displayLinkRoutes: [
+      'pim_enrich_attribute_index',
+      'pim_enrich_attribute_edit',
+
+    ],
+  }));
+  redirectToAttributeGridFilteredByFamilyAndQualityAndSelectAttributeTypes(family.meta.id);
+};
 
 const ProductModelEditFormApp: FunctionComponent<ProductModelEditFormAppProps> = ({product, catalogChannel, catalogLocale}) => {
   return (
@@ -33,7 +80,26 @@ const ProductModelEditFormApp: FunctionComponent<ProductModelEditFormAppProps> =
         <AttributesTabContent product={product}/>
 
         <AxesContextProvider axes={['enrichment', 'consistency']}>
-          <DataQualityInsightsTabContent product={product} productEvaluationFetcher={fetchProductModelEvaluation} />
+          <DataQualityInsightsTabContent product={product} productEvaluationFetcher={fetchProductModelEvaluation}>
+            <AxisEvaluation axis={'enrichment'}>
+              <Criterion code={'completeness_of_non_required_attributes'}/>
+              <Criterion code={'completeness_of_required_attributes'}/>
+              <Criterion code={'missing_image_attribute'}>
+                <Recommendation supports={(criterion => criterion.improvable_attributes.length === 0)}>
+                  <span className="NotApplicableAttribute">{translate('akeneo_data_quality_insights.product_evaluation.messages.add_image_attribute_recommendation')}</span>
+                </Recommendation>
+              </Criterion>
+            </AxisEvaluation>
+
+            <AxisEvaluation axis={"consistency"}>
+              <Criterion code={'consistency_spelling'}/>
+              <Criterion code={'consistency_textarea_lowercase_words'}/>
+              <Criterion code={'consistency_textarea_uppercase_words'}/>
+              <Criterion code={'consistency_text_title_formatting'}/>
+              <Criterion code={'consistency_attribute_spelling'} followCriterion={followAttributeSpellingCriterion}/>
+              <Criterion code={'consistency_attribute_option_spelling'} followCriterion={followAttributeOptionSpellingCriterion}/>
+            </AxisEvaluation>
+          </DataQualityInsightsTabContent>
         </AxesContextProvider>
       </Provider>
     </ThemeProvider>
