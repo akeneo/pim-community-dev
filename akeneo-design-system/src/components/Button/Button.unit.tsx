eef1cc800ce1fb42f9b2a4d5c587a5a773496eee
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-21 19:11:12 +0200

    RAC: Tidy up

--- akeneo-design-system/src/components/Button/Button.unit.tsx
+++ akeneo-design-system/src/components/Button/Button.unit.tsx
@@ -3,7 +3,7 @@ import {fireEvent, render, screen} from '../../storybook/test-util';
 import {Button} from './Button';
 import userEvent from '@testing-library/user-event';
 
-it('it calls onClick handler when user clicks on button', () => {
+test('it calls onClick handler when user clicks on button', () => {
   const onClick = jest.fn();
   render(
     <Button size="small" onClick={onClick}>
@@ -17,7 +17,7 @@ it('it calls onClick handler when user clicks on button', () => {
   expect(onClick).toBeCalled();
 });
 
-it('it calls onClick handler when user hit enter key on button', async () => {
+test('it calls onClick handler when user hits enter key on button', async () => {
   const onClick = jest.fn();
   render(
     <Button ghost={true} onClick={onClick}>
@@ -32,7 +32,7 @@ it('it calls onClick handler when user hit enter key on button', async () => {
   expect(onClick).toBeCalled();
 });
 
-it('it does not call onClick handler when user clicks on a disabled button', () => {
+test('it does not call onClick handler when user clicks on a disabled button', () => {
   const onClick = jest.fn();
   render(
     <Button disabled={true} ghost={true} onClick={onClick}>
@@ -46,7 +46,7 @@ it('it does not call onClick handler when user clicks on a disabled button', () 
   expect(onClick).not.toBeCalled();
 });
 
-it('it does not call onClick handler when user hit enter key on button', () => {
+test('it does not call onClick handler when user hits enter key on a disabled button', () => {
   const onClick = jest.fn();
   render(
     <Button disabled={true} onClick={onClick}>
@@ -60,7 +60,7 @@ it('it does not call onClick handler when user hit enter key on button', () => {
   expect(onClick).not.toBeCalled();
 });
 
-it('it displays an anchor when providing a `href`', () => {
+test('it displays an anchor when providing a `href`', () => {
   render(<Button href="https://akeneo.com/">Hello</Button>);
 
   expect(screen.getByText('Hello').closest('a')).toHaveAttribute('href', 'https://akeneo.com/');
@@ -69,23 +69,12 @@ it('it displays an anchor when providing a `href`', () => {
 test('it throws when trying to pass href and onClick', () => {
   const mockConsole = jest.spyOn(console, 'error').mockImplementation();
 
-  const buttonRender = () =>
+  expect(() => {
     render(
-      <Button href="http://google.com" onClick={() => {}}>
-        invalid button
+      <Button onClick={() => {}} href="#nice">
+        Hello
       </Button>
     );
-
-  expect(buttonRender).toThrowError();
-
-  mockConsole.mockRestore();
-});
-
-test('it throws when trying to pass href and onClick', () => {
-  const mockConsole = jest.spyOn(console, 'error').mockImplementation();
-
-  expect(() => {
-    render(<Button onClick={() => {}} href="#nice">Hello</Button>);
   }).toThrowError();
 
   mockConsole.mockRestore();
@@ -104,13 +93,12 @@ test('it does not trigger onClick when disabled', () => {
 
   expect(onClick).not.toBeCalled();
 });
-test('it does not trigger onClick when onClick is undefined', () => {
-  render(
-    <Button>
-      Hello
-    </Button>
-  );
 
-  const button = screen.getByText('Hello');
-  fireEvent.click(button);
+test('it does not trigger onClick when onClick is undefined', () => {
+  const onClick = jest.fn();
+  render(<Button onClick={undefined}>Hello</Button>);
+
+  fireEvent.click(screen.getByText('Hello'));
+
+  expect(onClick).not.toBeCalled();
 });
