13bed1e1d985c9497dbba6ae371656a8272aefd6
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-23 17:13:00 +0200

    RAC-236: Change brand colors in themes

---
+++ akeneo-design-system/src/storybook/themes.ts
@@ -0,0 +1,11 @@
+import {pimTheme} from '../theme/pim';
+import {onboarderTheme} from '../theme/onboarder';
+import {sharedCatalogsTheme} from '../theme/shared-catalogs';
+
+const themes = {
+  PIM: pimTheme,
+  Onboarder: onboarderTheme,
+  'Shared Catalogs': sharedCatalogsTheme,
+};
+
+export {themes};
--- akeneo-design-system/src/theme/shared-catalogs/index.ts
+++ akeneo-design-system/src/theme/shared-catalogs/index.ts
@@ -1,17 +1,19 @@
 import {Theme} from '../theme';
-import {color, fontSize} from '../common';
+import {color, fontSize, palette} from '../common';
 
 const sharedCatalogsTheme: Theme = {
-  color,
-  fontSize,
-  palette: {
-    primary: 'green',
-    secondary: 'blue',
-    tertiary: 'grey',
-    warning: 'yellow',
-    danger: 'red',
-    brand: '#F9B53F',
+  color: {
+    ...color,
+    brand20: '#FDF0D8',
+    brand40: '#FCE1B2',
+    brand60: '#FBD28B',
+    brand80: '#FAC365',
+    brand100: '#F9B53F',
+    brand120: '#C79032',
+    brand140: '#956C25',
   },
+  fontSize,
+  palette,
 };
 
 export {sharedCatalogsTheme};
