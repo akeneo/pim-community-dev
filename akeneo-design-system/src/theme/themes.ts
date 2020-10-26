b8710d230c454a720c4943bfddd738d1893487d5
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-26 10:28:26 +0100

    RAC-236: Remove indexed themes

--- akeneo-design-system/src/theme/themes.ts
+++ akeneo-design-system/src/theme/themes.ts
@@ -2,12 +2,6 @@ import {pimTheme} from './pim';
 import {onboarderTheme} from './onboarder';
 import {sharedCatalogsTheme} from './shared-catalogs';
 
-const indexedThemes = {
-  PIM: pimTheme,
-  Onboarder: onboarderTheme,
-  'Shared Catalogs': sharedCatalogsTheme,
-};
-
 const themes = [pimTheme, onboarderTheme, sharedCatalogsTheme];
 
-export {themes, indexedThemes};
+export {themes};
