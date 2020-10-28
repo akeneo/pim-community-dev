b55b232674bcc8ab5a54907b43e1cb2e2019b4f5
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-10-28 19:22:49 +0100

    DAPI-1364: display EE specific key indicators on DQI dashboard

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Symfony/Resources/public/js/infrastructure/dqi-dashboard/section.tsx
@@ -0,0 +1,33 @@
+import ReactDOM from 'react-dom';
+import React from "react";
+import {DashboardHelper} from '@akeneo-pim-community/data-quality-insights/src/index';
+import {Dashboard} from "@akeneo-pim-ee/data-quality-insights/src/application/component/Dashboard/Dashboard";
+
+const UserContext = require('pim/user-context');
+const BaseDashboard = require('akeneo/data-quality-insights/view/dqi-dashboard/base-dashboard');
+
+class SectionView extends BaseDashboard {
+
+  render() {
+
+    const catalogLocale: string = UserContext.get('catalogLocale');
+    const catalogChannel: string = UserContext.get('catalogScope');
+
+    ReactDOM.render(
+      <div>
+        <DashboardHelper/>
+        <Dashboard
+          timePeriod={this.timePeriod}
+          catalogLocale={catalogLocale}
+          catalogChannel={catalogChannel}
+          familyCode={this.familyCode}
+          categoryCode={this.categoryCode}
+          axes={this.axes}
+        />
+      </div>,
+      this.el
+    );
+  }
+}
+
+export = SectionView;
---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Dashboard/Dashboard.tsx
@@ -0,0 +1,79 @@
+import React, {FunctionComponent} from "react";
+import {AssetCollectionIcon, EditIcon, pimTheme, SettingsIcon} from "akeneo-design-system";
+import {ThemeProvider} from "styled-components";
+import {DependenciesProvider} from "@akeneo-pim-community/legacy-bridge";
+import {
+  KeyIndicator,
+  KeyIndicators,
+  Overview,
+  Widgets
+} from "@akeneo-pim-community/data-quality-insights/src/application/component/Dashboard";
+import {AxesContextProvider} from "@akeneo-pim-community/data-quality-insights/src/application/context/AxesContext";
+import {keyIndicatorsTips} from "@akeneo-pim-community/data-quality-insights/src/application/helper/Dashboard/KeyIndicatorsTips";
+import {KeyIndicatorsProvider} from "@akeneo-pim-community/data-quality-insights/src/application/context/KeyIndicatorsContext";
+import {EEKeyIndicatorsTips} from "../../helper/Dashboard/EEKeyIndicatorsTips";
+
+interface DataQualityInsightsDashboardProps {
+  timePeriod: string;
+  catalogLocale: string;
+  catalogChannel: string;
+  familyCode: string | null;
+  categoryCode: string | null;
+  axes: string[];
+}
+
+const Dashboard: FunctionComponent<DataQualityInsightsDashboardProps> = ({timePeriod, catalogLocale, catalogChannel, familyCode, categoryCode, axes}) => {
+  return (
+    <DependenciesProvider>
+      <ThemeProvider theme={pimTheme}>
+        <AxesContextProvider axes={axes}>
+          <div id="data-quality-insights-activity-dashboard">
+            <div className="AknSubsection">
+              <Overview catalogLocale={catalogLocale} catalogChannel={catalogChannel} timePeriod={timePeriod} familyCode={familyCode} categoryCode={categoryCode}/>
+
+              <KeyIndicatorsProvider tips={{...EEKeyIndicatorsTips, ...keyIndicatorsTips}}>
+                <KeyIndicators channel={catalogChannel} locale={catalogLocale} family={familyCode} category={categoryCode}>
+                  <KeyIndicator
+                    type="has_image"
+                    title={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.list.has_image.title'}
+                    resultsMessage={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.products_to_work_on'}
+                  >
+                    <AssetCollectionIcon/>
+                  </KeyIndicator>
+
+                  <KeyIndicator
+                    type="values_perfect_spelling"
+                    title={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.list.values_perfect_spelling.title'}
+                    resultsMessage={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.products_to_work_on'}
+                  >
+                    <EditIcon/>
+                  </KeyIndicator>
+
+                  <KeyIndicator
+                    type="good_enrichment"
+                    title={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.list.good_enrichment.title'}
+                    resultsMessage={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.products_to_work_on'}
+                  >
+                    <EditIcon/>
+                  </KeyIndicator>
+
+                  <KeyIndicator
+                    type="attributes_perfect_spelling"
+                    title={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.list.attributes_perfect_spelling.title'}
+                    resultsMessage={'akeneo_data_quality_insights.dqi_dashboard.key_indicators.attributes_to_work_on'}
+                  >
+                    <SettingsIcon/>
+                  </KeyIndicator>
+                </KeyIndicators>
+              </KeyIndicatorsProvider>
+
+              <Widgets catalogLocale={catalogLocale} catalogChannel={catalogChannel}/>
+            </div>
+          </div>
+        </AxesContextProvider>
+      </ThemeProvider>
+    </DependenciesProvider>
+  )
+};
+
+export {Dashboard};
