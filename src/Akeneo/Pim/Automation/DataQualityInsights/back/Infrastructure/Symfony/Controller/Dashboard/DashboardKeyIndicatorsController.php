00f5c9eaa0d8fa59fc0784b3c787d28eb591d00f
Julian Prud'homme <julian.prudhomme@akeneo.com>
2020-10-28 18:22:51 +0100

    DAPI-1364: add stub controller for key indicators

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Symfony/Controller/Dashboard/DashboardKeyIndicatorsController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2020 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\Automation\DataQualityInsights\Infrastructure\Symfony\Controller\Dashboard;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+final class DashboardKeyIndicatorsController
+{
+    public function __invoke(Request $request, string $channel, string $locale)
+    {
+        return new JsonResponse([
+            'has_image' => [
+                'ratio' => 25.65,
+                'total' => 5641,
+            ],
+            'good_enrichment' => [
+                'ratio' => 35.48,
+                'total' => 15479,
+            ],
+        ]);
+    }
+}
