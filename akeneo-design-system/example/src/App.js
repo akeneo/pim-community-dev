3b41a56deac30f8e252fecbe3978a008ee11fee8
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-10-29 11:00:13 +0100

    RAC-236: Add Modal with Illustration variant

--- akeneo-design-system/example/src/App.js
+++ akeneo-design-system/example/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import {Badge, CheckIcon, pimTheme} from 'akeneo-design-system'
+import {Badge, CheckIcon, pimTheme} from 'akeneo-design-system';
 
 function App() {
   return (
     <div>
       <ThemeProvider theme={pimTheme}>
         <Badge>Success</Badge>
-        <hr/>
-        <CheckIcon size={24}/>
+        <hr />
+        <CheckIcon size={24} />
       </ThemeProvider>
     </div>
   );
