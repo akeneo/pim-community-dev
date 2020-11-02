1a6845e2d9ca408e81007633c459323aeb107fc6
Julien Sanchez <sumbobyboys@gmail.com>
2020-11-02 12:10:00 +0100

    RAC-271: add stop on import/export execution grid and acl on job tracker

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Button} from 'akeneo-design-system';
-import {useRoute, useTranslate} from '@akeneo-pim-community/legacy-bridge';
+import {useTranslate} from '@akeneo-pim-community/legacy-bridge';
 import {StopJobAction} from 'pimui/js/job/execution/StopJobAction';
 
 const ActionsContainer = styled.div`
@@ -14,16 +14,15 @@ type ActionsProps = {
   id: string;
   jobLabel: string;
   isStoppable: boolean;
+  showLink: string;
   refreshCollection: () => void;
 };
 
-const Actions = ({id, jobLabel, isStoppable, refreshCollection}: ActionsProps) => {
+const Actions = ({id, jobLabel, isStoppable, showLink, refreshCollection}: ActionsProps) => {
   const translate = useTranslate();
-  const showRoute = useRoute('pim_enrich_job_tracker_show', {id});
-
   return (
     <ActionsContainer>
-      <Button size="small" ghost={true} level="tertiary" href={`#${showRoute}`}>
+      <Button size="small" ghost={true} level="tertiary" href={`#${showLink}`}>
         {translate('pim_datagrid.action.show.title')}
       </Button>
       <StopJobAction
