684c79bb69ee7e3c3aa952e77e13710a8669791b
anaisbaunelemaire <anais.lemaire@akeneo.com>
2020-11-04 17:26:48 +0100

    PIM-9499: Improve code according to recent changes on master

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/execution/summary-table.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/execution/summary-table.js
@@ -6,21 +6,19 @@
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-define(
-    [
-        'jquery',
-        'underscore',
-        'oro/translator',
-        'pim/form',
-        'pim/template/job-execution/summary-table'
-    ],
-    function ($, _, __, BaseForm, template) {
-        return BaseForm.extend({
-            template: _.template(template),
-            events: {
-                'click .data': 'toggleDisplayWarning'
-            },
-            expandedWarnings: {},
+define(['jquery', 'underscore', 'oro/translator', 'pim/form', 'pim/template/job-execution/summary-table'], function (
+  $,
+  _,
+  __,
+  BaseForm,
+  template
+) {
+  return BaseForm.extend({
+    template: _.template(template),
+    events: {
+      'click .data': 'toggleDisplayWarning',
+    },
+    expandedWarnings: {},
 
             /**
              * Display or hide a warning details
@@ -38,60 +36,62 @@ define(
                 this.render();
             },
 
-            /**
-             * {@inheritdoc}
-             */
-            initialize: function (config) {
-                this.config = config.config;
-                BaseForm.prototype.initialize.apply(this, arguments);
-            },
+    /**
+     * {@inheritdoc}
+     */
+    initialize: function (config) {
+      this.config = config.config;
 
-            /**
-             * {@inheritdoc}
-             */
-            configure: function () {
-                this.expandedWarnings = {};
-                this.listenTo(this.getRoot(), 'pim_enrich:form:entity:post_update', this.render);
+      BaseForm.prototype.initialize.apply(this, arguments);
+    },
 
-                return BaseForm.prototype.configure.apply(this, arguments);
-            },
+    /**
+     * {@inheritdoc}
+     */
+    configure: function () {
+      this.expandedWarnings = {};
+      this.listenTo(this.getRoot(), 'pim_enrich:form:entity:post_update', this.render);
 
-            /**
-             * {@inheritdoc}
-             */
-            render: function () {
-                var model = this.getFormData();
-                this.$el.html(this.template({
-                    transAndUpperCase: function (str) {
-                        return __(str).toUpperCase();
-                    },
-                    __: __,
-                    stepExecutions: model.stepExecutions,
-                    status: model.status,
-                    failures: model.failures,
-                    id: model.meta.id,
-                    translateStepExecutionLabel: this.translateStepExecutionLabel,
-                    expandedWarnings: this.expandedWarnings
-                }));
+      return BaseForm.prototype.configure.apply(this, arguments);
+    },
 
-                return this;
-            },
+    /**
+     * {@inheritdoc}
+     */
+    render: function () {
+      var model = this.getFormData();
+      this.$el.html(
+        this.template({
+          transAndUpperCase: function (str) {
+            return __(str).toUpperCase();
+          },
+          __: __,
+          stepExecutions: model.stepExecutions,
+          status: model.status,
+          failures: model.failures,
+          id: model.meta.id,
+          translateStepExecutionLabel: this.translateStepExecutionLabel,
+          expandedWarnings: this.expandedWarnings
+        })
+      );
 
-            /**
-             * Get the translation of a stepExecution.
-             * If the translation exists for this specific job, returns it, else returns the default one.
-             *
-             * @param stepExecution
-             * @returns {string}
-             */
-            translateStepExecutionLabel: function(stepExecution) {
-                let key = 'batch_jobs.' + stepExecution.job + '.' + stepExecution.label + '.label';
-                if (__(key) === key) {
-                    key = 'batch_jobs.default_steps.' + stepExecution.label;
-                }
+      return this;
+    },
 
-                return __(key);
-            }
-        });
-    }
-);
+    /**
+     * Get the translation of a stepExecution.
+     * If the translation exists for this specific job, returns it, else returns the default one.
+     *
+     * @param stepExecution
+     * @returns {string}
+     */
+    translateStepExecutionLabel: function (stepExecution) {
+      let key = 'batch_jobs.' + stepExecution.job + '.' + stepExecution.label + '.label';
+      if (__(key) === key) {
+        key = 'batch_jobs.default_steps.' + stepExecution.label;
+      }
+
+      return __(key);
+    },
+  });
+});
