4b39615652dafba1529b1b8d6575dc4f272ee7aa
Steven VAIDIE <steven.vaidie@gmail.com>
2020-11-05 11:54:13 +0100

    RAC-343: feat make ComputeCompletenessOfFamilyProductsTasklet trackable

--- tests/back/Pim/Enrichment/Specification/Component/Product/Connector/Job/ComputeCompletenessOfFamilyProductsTaskletSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Connector/Job/ComputeCompletenessOfFamilyProductsTaskletSpec.php
@@ -11,8 +11,10 @@ use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\SaveProductCompletenesses;
 use Akeneo\Pim\Structure\Component\Model\FamilyInterface;
 use Akeneo\Tool\Component\Batch\Item\ItemReaderInterface;
+use Akeneo\Tool\Component\Batch\Item\TrackableTaskletInterface;
 use Akeneo\Tool\Component\Batch\Job\JobRepositoryInterface;
 use Akeneo\Tool\Component\Batch\Model\StepExecution;
+use Akeneo\Tool\Component\Connector\Reader\File\Csv\Reader;
 use Akeneo\Tool\Component\StorageUtils\Cache\EntityManagerClearerInterface;
 use Akeneo\Tool\Component\StorageUtils\Cursor\CursorInterface;
 use PhpSpec\ObjectBehavior;
@@ -45,6 +47,7 @@ class ComputeCompletenessOfFamilyProductsTaskletSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(ComputeCompletenessOfFamilyProductsTasklet::class);
+        $this->shouldImplement(TrackableTaskletInterface::class);
     }
 
     function it_does_nothing_if_there_is_no_family(
@@ -101,6 +104,8 @@ class ComputeCompletenessOfFamilyProductsTaskletSpec extends ObjectBehavior
         $saveProductCompletenesses->saveAll(['completeness_collection'])->shouldBeCalled();
 
         $stepExecution->incrementSummaryInfo('process', 4)->shouldBeCalled();
+        $stepExecution->incrementProcessedItems(4)->shouldBeCalledOnce();
+
         $jobRepository->updateStepExecution($stepExecution)->shouldBeCalled();
 
         $this->execute();
@@ -133,9 +138,41 @@ class ComputeCompletenessOfFamilyProductsTaskletSpec extends ObjectBehavior
 
         $stepExecution->incrementSummaryInfo('process', 1000)->shouldBeCalled();
         $stepExecution->incrementSummaryInfo('process', 6)->shouldBeCalled();
+        $stepExecution->incrementProcessedItems(1000)->shouldBeCalledOnce();
+        $stepExecution->incrementProcessedItems(6)->shouldBeCalledOnce();
+
         $jobRepository->updateStepExecution($stepExecution)->shouldBeCalledTimes(2);
 
         $this->execute();
+    }
+
+    function it_does_not_count_total_item_when_reader_is_not_rewindable()
+    {
+        $this->totalItems()->shouldReturn(0);
+    }
+
+    function it_return_the_total_items_when_reader_is_rewindable(
+        ProductQueryBuilderFactoryInterface $productQueryBuilderFactory,
+        ProductQueryBuilderInterface $productQueryBuilder,
+        Reader $familyReader,
+        FamilyInterface $familyShoes,
+        FamilyInterface $familyAccessories
+    ) {
+        $productQueryBuilderFactory->create()->willReturn($productQueryBuilder);
+
+        $familyReader->read()->shouldBeCalledTimes(3)->willReturn($familyShoes, $familyAccessories, null);
+        $familyReader->rewind()->shouldBeCalledOnce();
+        $familyShoes->getCode()->willReturn('Shoes');
+        $familyAccessories->getCode()->willReturn('Accessories');
+
+        $productQueryBuilder->addFilter('family', 'IN', ['Shoes', 'Accessories'])->shouldBeCalled();
+        $productQueryBuilder->execute()->shouldBeCalled()->willReturn(
+            new ProductCursor(array_map(function (int $i): ProductInterface {
+                return (new Product())->setIdentifier('product_' . $i);
+            }, range(1, 1006)))
+        );
+
+        $this->totalItems()->shouldReturn(1006);
     }
 }
 
