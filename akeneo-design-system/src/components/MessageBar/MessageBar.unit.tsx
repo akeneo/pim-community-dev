ecbd679c44695c793d2d0d6deea6d36e76a7b261
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-11-12 17:00:55 +0100

    RAC-327: Add level unit tests

--- akeneo-design-system/src/components/MessageBar/MessageBar.unit.tsx
+++ akeneo-design-system/src/components/MessageBar/MessageBar.unit.tsx
@@ -5,10 +5,31 @@ import {InfoIcon} from '../../icons';
 
 test('it renders its children properly', () => {
   render(
-    <MessageBar icon={<InfoIcon />} title="Title">
-      MessageBar content
+    <MessageBar level="info" icon={<InfoIcon />} title="Title">
+      MessageBar Info
     </MessageBar>
   );
 
-  expect(screen.getByText('MessageBar content')).toBeInTheDocument();
+  render(
+    <MessageBar level="success" icon={<InfoIcon />} title="Title">
+      MessageBar Success
+    </MessageBar>
+  );
+
+  render(
+    <MessageBar level="warning" icon={<InfoIcon />} title="Title">
+      MessageBar Warning
+    </MessageBar>
+  );
+
+  render(
+    <MessageBar level="danger" icon={<InfoIcon />} title="Title">
+      MessageBar Danger
+    </MessageBar>
+  );
+
+  expect(screen.getByText('MessageBar Info')).toBeInTheDocument();
+  expect(screen.getByText('MessageBar Success')).toBeInTheDocument();
+  expect(screen.getByText('MessageBar Warning')).toBeInTheDocument();
+  expect(screen.getByText('MessageBar Danger')).toBeInTheDocument();
 });
