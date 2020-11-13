d8d81abb56af91b9b9812208f0086fdf7fb6aa28
Olivier Pontier <olivier.pontier@akeneo.com>
2020-11-13 15:52:27 +0100

    follow Not applicable enrichment image recommendation redirects to the attributes tab on Family edition form

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/user-actions/followNotApplicableEnrichmentImageRecommendation.ts
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/user-actions/followNotApplicableEnrichmentImageRecommendation.ts
@@ -1,9 +1,13 @@
+import {FAMILY_EDIT_FORM_ATTRIBUTES_TAB} from '../constant';
+
 const router = require('pim/router');
 
 const followNotApplicableEnrichmentImageRecommendation = (family: string | null) => {
   if (family === null) {
     return;
   }
+
+  sessionStorage.setItem('current_form_tab', FAMILY_EDIT_FORM_ATTRIBUTES_TAB);
 
   router.redirectToRoute('pim_enrich_family_edit', {
     code: family,
