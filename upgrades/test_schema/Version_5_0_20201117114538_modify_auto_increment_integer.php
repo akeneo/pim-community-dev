6af36ebbc3bfe8c740bbfab7f5fa101412f087b8
Grégoire HUBERT <gregoire.hubert@akeneo.com>
2020-11-17 14:29:08 +0100

    add test for migration

---
+++ upgrades/test_schema/Version_5_0_20201117114538_modify_auto_increment_integer.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema\Tests;
+
+use Akeneo\Test\Integration\TestCase;
+
+final class Version_5_0_20201117114538_modify_auto_increment_integer extends TestCase
+{
+    use ExecuteMigrationTrait;
+
+    private const MIGRATION_LABEL = '_5_0_20201117114538_modify_auto_increment_integer';
+
+    public function completeness_id_has_changed_to_bigint(): void
+    {
+        $this->ensureCompletenessUsesNormalInt();
+        $this->reExecuteMigration(self::MIGRATION_LABEL);
+        $tableColumns = $this->schemaManager->listTableColumns('pim_catalog_completeness');
+
+        $found = false;
+        foreach($tableColumns as $column) {
+            if ($column->getName() === 'id') {
+                $this->assertEquals('bigint', $column->getType());
+                $found=true;
+            }
+        }
+
+        $this->assertTrue($found); // ← we need to ensure there is a ID column hence we have tested it correctly
+    }
+
+    private function ensureCompletenessUsesNormalInt(): void {
+        $sql = "ALTER TABLE pim_catalog_completeness MODIFY id int AUTO_INCREMENT";
+        $this->get('database_connection')->executeQuery($sql);
+    }
+}
