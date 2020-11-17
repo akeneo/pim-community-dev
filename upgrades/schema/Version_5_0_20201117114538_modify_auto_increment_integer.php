b7735ead8505b5afce5cf901913a768892310165
Grégoire HUBERT <gregoire.hubert@akeneo.com>
2020-11-17 11:50:43 +0100

    alter completion table

---
+++ upgrades/schema/Version_5_0_20201117114538_modify_auto_increment_integer.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_5_0_20201117114538_modify_auto_increment_integer.php extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $sql = "ALTER TABLE pim_catalog_completeness MODIFY id bigint AUTO_INCREMENT";
+        $this->addSql($sql);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
