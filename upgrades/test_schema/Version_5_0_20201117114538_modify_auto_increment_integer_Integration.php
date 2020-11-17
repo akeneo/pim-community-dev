d784607ec5098ef031726904c32fb360cc9fa684
jmleroux <jmleroux.pro@gmail.com>
2020-11-17 16:22:14 +0100

    Fix test for migration

--- upgrades/test_schema/Version_5_0_20201117114538_modify_auto_increment_integer_Integration.php
+++ upgrades/test_schema/Version_5_0_20201117114538_modify_auto_increment_integer_Integration.php
@@ -6,13 +6,13 @@ namespace Pim\Upgrade\Schema\Tests;
 
 use Akeneo\Test\Integration\TestCase;
 
-final class Version_5_0_20201117114538_modify_auto_increment_integer extends TestCase
+final class Version_5_0_20201117114538_modify_auto_increment_integer_Integration extends TestCase
 {
     use ExecuteMigrationTrait;
 
     private const MIGRATION_LABEL = '_5_0_20201117114538_modify_auto_increment_integer';
 
-    public function completeness_id_has_changed_to_bigint(): void
+    public function test_completeness_id_has_changed_to_bigint(): void
     {
         $this->ensureCompletenessUsesNormalInt();
         $this->reExecuteMigration(self::MIGRATION_LABEL);
@@ -32,5 +32,10 @@ final class Version_5_0_20201117114538_modify_auto_increment_integer extends Tes
     private function ensureCompletenessUsesNormalInt(): void {
         $sql = "ALTER TABLE pim_catalog_completeness MODIFY id int AUTO_INCREMENT";
         $this->get('database_connection')->executeQuery($sql);
+    }
+
+    protected function getConfiguration()
+    {
+        return $this->catalog->useMinimalCatalog();
     }
 }
