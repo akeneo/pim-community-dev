d53203f21a9594f38b36e0e65e80991b753d8c67
Steven VAIDIE <steven.vaidie@gmail.com>
2020-11-23 13:26:24 +0100

    RAC-315: fix Prettier

--- akeneo-design-system/src/components/Table/Table.unit.tsx
+++ akeneo-design-system/src/components/Table/Table.unit.tsx
@@ -25,6 +25,8 @@ test('it renders its children properly', () => {
 });
 
 test('it renders table without checkbox when it is selectable but not selected', () => {
+  const onSelectToggle = jest.fn();
+
   render(
     <Table isSelectable={true}>
       <Table.Header>
@@ -32,7 +34,7 @@ test('it renders table without checkbox when it is selectable but not selected',
         <Table.HeaderCell>Another header</Table.HeaderCell>
       </Table.Header>
       <Table.Body>
-        <Table.Row isSelected={false} onSelectToggle={() => {}}>
+        <Table.Row isSelected={false} onSelectToggle={onSelectToggle}>
           <Table.Cell>A cell</Table.Cell>
           <Table.Cell>Another cell</Table.Cell>
         </Table.Row>
@@ -44,6 +46,8 @@ test('it renders table without checkbox when it is selectable but not selected',
 });
 
 test('it renders table with checkbox when it is selectable and display checkbox', () => {
+  const onSelectToggle = jest.fn();
+
   render(
     <Table isSelectable={true} displayCheckbox={true}>
       <Table.Header>
@@ -51,7 +55,7 @@ test('it renders table with checkbox when it is selectable and display checkbox'
         <Table.HeaderCell>Another header</Table.HeaderCell>
       </Table.Header>
       <Table.Body>
-        <Table.Row isSelected={false} onSelectToggle={() => {}}>
+        <Table.Row isSelected={false} onSelectToggle={onSelectToggle}>
           <Table.Cell>A cell</Table.Cell>
           <Table.Cell>Another cell</Table.Cell>
         </Table.Row>
@@ -63,6 +67,8 @@ test('it renders table with checkbox when it is selectable and display checkbox'
 });
 
 test('it renders table with checkbox when it is selectable and row is selected', () => {
+  const onSelectToggle = jest.fn();
+
   render(
     <Table isSelectable={true}>
       <Table.Header>
@@ -70,7 +76,7 @@ test('it renders table with checkbox when it is selectable and row is selected',
         <Table.HeaderCell>Another header</Table.HeaderCell>
       </Table.Header>
       <Table.Body>
-        <Table.Row isSelected={true} onSelectToggle={() => {}}>
+        <Table.Row isSelected={true} onSelectToggle={onSelectToggle}>
           <Table.Cell>A cell</Table.Cell>
           <Table.Cell>Another cell</Table.Cell>
         </Table.Row>
