808b2e7ee75c8e8b900ca9e98ef165134c9fee8c
Philippe Mossiere <philippe.mossiere@akeneo.com>
2020-11-23 17:26:03 +0100

    jean-michel commit

--- akeneo-design-system/src/components/BreadcrumbTraining/BreadcrumbTraining.tsx
+++ akeneo-design-system/src/components/BreadcrumbTraining/BreadcrumbTraining.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import {AkeneoThemedProps, getColor} from '../../theme';
 
 //TODO be sure to select the appropriate container element here
-const BreadcrumbTrainingContainer = styled.div<{ color: string }>``;
+const BreadcrumbTrainingContainer = styled.nav<{ color: string }>``;
 
 const Level = styled.a<{ color: string, gradient: number } & AkeneoThemedProps>`
 color:${props => getColor(props.color, props.gradient)};
@@ -16,30 +16,35 @@ margin: 0 0.5rem;
 
 type BreadcrumbTrainingProps = {
   /**
-   * TODO.
+   * Color of the breadcrumb
    */
   color?: 'blue' | 'red' | 'yellow';
 
   /**
-   * TODO.
+   * Children of the breadcrumb (only accept Level elements)
    */
   children?: ReactNode;
 };
 
 /**
- * TODO.
+ * Breadcrumbs are an important navigation component that shows content hierarchy.
  */
 const BreadcrumbTraining = ({color = 'blue', children, ...rest}: BreadcrumbTrainingProps) => {
-  const decoratedChildren = React.Children.map(children, (child) => {
+  const decoratedChildren = React.Children.map(children, (child, index) => {
     if (!(isValidElement(child) && Level === child.type)) {
       return null;
     }
 
-    return <>{React.cloneElement(child, {color, gradient: 120})}<Separator>/</Separator></>;
+    const isLast = React.Children.count(children) === index + 1;
+    if (isLast) {
+      return React.cloneElement(child, {color, gradient: 100, 'aria-current': 'page'});
+    }
+
+    return <>{React.cloneElement(child, {color, gradient: 120})}<Separator aria-hidden={true}>/</Separator></>;
   });
 
   return (
-    <BreadcrumbTrainingContainer color={color} {...rest}>
+    <BreadcrumbTrainingContainer color={color} {...rest} aria-label="Breadcrumb">
       {decoratedChildren}
     </BreadcrumbTrainingContainer>
   );
