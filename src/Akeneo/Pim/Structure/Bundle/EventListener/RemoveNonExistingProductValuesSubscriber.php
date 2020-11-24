3e08fb46e8630ddd0c07c1f9d319fbbc04e2f4f1
jmleroux <jmleroux.pro@gmail.com>
2020-11-24 21:12:21 +0100

    PIM-9573: Create remove_non_existing_product_values job instance at runtime

--- src/Akeneo/Pim/Structure/Bundle/EventListener/RemoveNonExistingProductValuesSubscriber.php
+++ src/Akeneo/Pim/Structure/Bundle/EventListener/RemoveNonExistingProductValuesSubscriber.php
@@ -7,6 +7,8 @@ namespace Akeneo\Pim\Structure\Bundle\EventListener;
 use Akeneo\Pim\Structure\Component\Model\AttributeOptionInterface;
 use Akeneo\Tool\Bundle\BatchBundle\Job\JobInstanceRepository;
 use Akeneo\Tool\Bundle\BatchBundle\Launcher\JobLauncherInterface;
+use Akeneo\Tool\Component\Batch\Model\JobInstance;
+use Akeneo\Tool\Component\Batch\Query\SqlCreateJobInstanceInterface;
 use Akeneo\Tool\Component\StorageUtils\StorageEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -19,28 +21,24 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
  */
 final class RemoveNonExistingProductValuesSubscriber implements EventSubscriberInterface
 {
-    /** @var TokenStorageInterface */
-    private $tokenStorage;
-
-    /** @var JobInstanceRepository */
-    private $jobInstanceRepository;
-
-    /** @var JobLauncherInterface */
-    private $jobLauncher;
-
-    /** @var string */
-    private $jobName;
+    private TokenStorageInterface $tokenStorage;
+    private JobInstanceRepository $jobInstanceRepository;
+    private JobLauncherInterface $jobLauncher;
+    private string $jobName;
+    private ?SqlCreateJobInstanceInterface $createJobInstance;
 
     public function __construct(
         TokenStorageInterface $tokenStorage,
         JobInstanceRepository $jobInstanceRepository,
         JobLauncherInterface $jobLauncher,
-        string $jobName
+        string $jobName,
+        SqlCreateJobInstanceInterface $createJobInstance = null // @todo remove nullable on master
     ) {
         $this->tokenStorage = $tokenStorage;
         $this->jobInstanceRepository = $jobInstanceRepository;
         $this->jobLauncher = $jobLauncher;
         $this->jobName = $jobName;
+        $this->createJobInstance = $createJobInstance;
     }
 
     public static function getSubscribedEvents(): array
@@ -63,7 +61,27 @@ final class RemoveNonExistingProductValuesSubscriber implements EventSubscriberI
         ];
 
         $user = $this->tokenStorage->getToken()->getUser();
-        $jobInstance = $this->jobInstanceRepository->findOneByIdentifier($this->jobName);
+        $jobInstance = $this->getOrCreateJobInstance();
         $this->jobLauncher->launch($jobInstance, $user, $configuration);
+    }
+
+    private function getOrCreateJobInstance(): JobInstance
+    {
+        $jobInstance = $this->jobInstanceRepository->findOneByIdentifier($this->jobName);
+
+        // Create the job instance if the migration was not played
+        // @todo remove the whole block on master: the job should exists because we always play migrations
+        if (null === $jobInstance && null !== $this->createJobInstance) {
+            $this->createJobInstance->createJobInstance([
+                'code' => 'remove_non_existing_product_values',
+                'label' => 'Remove the non existing values of product and product models',
+                'job_name' => 'remove_non_existing_product_values',
+                'type' => 'remove_non_existing_product_values',
+            ]);
+
+            $jobInstance = $this->jobInstanceRepository->findOneByIdentifier($this->jobName);
+        }
+
+        return $jobInstance;
     }
 }
