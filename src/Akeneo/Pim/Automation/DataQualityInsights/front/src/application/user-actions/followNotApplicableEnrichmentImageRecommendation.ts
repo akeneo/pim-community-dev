4471c15165fdf25b7d24a36c08a321ce86659438
olivierpontier <50582517+olivierpontier@users.noreply.github.com>
2020-11-24 17:23:11 +0100

    DAPI-1435: Missing image - improvements when coming from link "no image attribute or asset collection for family" (#13324)
    * DAPI-1435: add back link to return on product edit form when the user follows the recommendation on missing image
    
    * DAPI-1435: use DSM button

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/user-actions/followNotApplicableEnrichmentImageRecommendation.ts
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/user-actions/followNotApplicableEnrichmentImageRecommendation.ts
@@ -1,11 +1,25 @@
-import {FAMILY_EDIT_FORM_ATTRIBUTES_TAB} from '../constant';
+import {BACK_LINK_SESSION_STORAGE_KEY, FAMILY_EDIT_FORM_ATTRIBUTES_TAB} from '../constant';
+import {Product} from '../../domain';
+import {isProductModel} from '../helper';
 
 const router = require('pim/router');
+const translate = require('oro/translator');
 
-const followNotApplicableEnrichmentImageRecommendation = (family: string | null) => {
+const followNotApplicableEnrichmentImageRecommendation = (product: Product) => {
+  const family: string | null = product.family;
   if (family === null) {
     return;
   }
+
+  window.sessionStorage.setItem(
+    BACK_LINK_SESSION_STORAGE_KEY,
+    JSON.stringify({
+      label: translate('akeneo_data_quality_insights.product_edit_form.back_to_products'),
+      route: isProductModel(product) ? 'pim_enrich_product_model_edit' : 'pim_enrich_product_edit',
+      routeParams: {id: product.meta.id},
+      displayLinkRoutes: ['pim_enrich_family_edit'],
+    })
+  );
 
   sessionStorage.setItem('current_form_tab', FAMILY_EDIT_FORM_ATTRIBUTES_TAB);
 
