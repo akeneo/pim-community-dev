3477be01a427d257a1635fa768e0f53a91e5ae80
Julien Sanchez <sumbobyboys@gmail.com>
2020-11-27 12:33:54 +0100

    RAC-university: re-display the menu item

--- src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/index.tsx
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import {List} from 'akeneomeasure/pages/list';
 import {Edit} from 'akeneomeasure/pages/edit';
@@ -7,8 +7,19 @@ import {UnsavedChangesContext} from 'akeneomeasure/context/unsaved-changes-conte
 
 // TODO: we should remove this and provide proper dependencies provider for config and unsavedChanges
 import {measurementsDependencies} from 'akeneomeasure/bridge/dependencies';
+import {useDependenciesContext} from '@akeneo-pim-community/legacy-bridge';
 
-const Index = () => (
+const Index = () => {
+  const {mediator} = useDependenciesContext();
+
+  useEffect(() => {
+    if (mediator) {
+      mediator.trigger('pim_menu:highlight:tab', {extension: 'pim-menu-settings'});
+      mediator.trigger('pim_menu:highlight:item', {extension: 'pim-menu-measurements-settings'});
+    }
+  }, [mediator]);
+
+  return (
   <ConfigContext.Provider value={measurementsDependencies.config}>
     <UnsavedChangesContext.Provider value={measurementsDependencies.unsavedChanges}>
       <Router basename="/configuration/measurement">
@@ -23,6 +34,6 @@ const Index = () => (
       </Router>
     </UnsavedChangesContext.Provider>
   </ConfigContext.Provider>
-);
+)};
 
 export {Index};
