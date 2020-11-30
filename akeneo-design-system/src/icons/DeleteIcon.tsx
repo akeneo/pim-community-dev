ac025b01755ab0ac3405df9893d1c337ce07d139
Valentin Mumble <valentin.dijkstra@akeneo.com>
2020-11-30 17:51:39 +0100

    RAC-412: Update illustrations to be animatable (#13385)

--- akeneo-design-system/src/icons/DeleteIcon.tsx
+++ akeneo-design-system/src/icons/DeleteIcon.tsx
@@ -4,14 +4,12 @@ import {IconProps} from './IconProps';
 const DeleteIcon = ({title, size = 24, color = 'currentColor', ...props}: IconProps) => (
   <svg viewBox="0 0 24 24" width={size} height={size} {...props}>
     {title && <title>{title}</title>}
-    <path
-      d="M5 8h14v14H5V8zm3.5 3v7.5M12 11v7.5m3.5-7.5v7.5M3 5h18v3H3V5zm5.5-2.5h7"
-      stroke={color}
-      fill="none"
-      fillRule="evenodd"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <g stroke={color} fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+      <path className="body" d="M5 8h14v14H5zM8.5 11v7.5M12 11v7.5M15.5 11v7.5" />
+      <g>
+        <path className="lid" d="M3 5h18v3H3zM8.5 2.5h7" />
+      </g>
+    </g>
   </svg>
 );
 
