e594c0d955eeaf219342183d4126396e3879044f
Michel Tag <micheltag@akeneo.com>
2020-11-30 09:34:11 +0100

    Preparing version 3.2.77 (#9999)
    Preparing version 3.2.77
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '3.2.76';
+    const VERSION = '3.2.77';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Edelweiss';
