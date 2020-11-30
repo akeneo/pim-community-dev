f72db428d125e8ca67e37bb9b2415ab534edceb8
gaël Prudhomme <gael@mrprud.me>
2020-11-30 10:20:11 +0100

    add memory for composer (#9998)

--- bin/docker/pim-dependencies.sh
+++ bin/docker/pim-dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-docker-compose exec fpm php -d memory_limit=3G /usr/local/bin/composer update
+docker-compose exec fpm php -d memory_limit=4G /usr/local/bin/composer update
 docker-compose run --rm -e PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 node yarn install
