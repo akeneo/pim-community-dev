fcb0c08a6c41364088dc947ded61cf45f360ca1f
Paul Chasle <pchasle@users.noreply.github.com>
2020-12-03 16:21:10 +0100

    CXP-494: add event subscriptions limit to the form

--- src/Akeneo/Connectivity/Connection/front/src/common/components/form/FormGroup.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/common/components/form/FormGroup.tsx
@@ -1,7 +1,5 @@
 import React, {cloneElement, ComponentProps, ReactElement, ReactNode} from 'react';
 import styled from 'styled-components';
-import {FormControlError} from './FormControlError';
-import {FormControlSuccess} from './FormControlSuccess';
 import {FormInput} from './FormInput';
 import {FormLabel} from './FormLabel';
 
@@ -9,36 +7,28 @@ interface Props {
     children: ReactElement<ComponentProps<typeof FormInput>, typeof FormInput>;
     controlId?: string;
     label?: string;
-    errors?: string[];
-    success?: string;
     required?: boolean;
-    helper?: ReactNode;
+    helpers?: ReactNode[];
 }
 
-export const FormGroup = ({children: control, controlId, label, errors, success, helper, required = false}: Props) => (
+export const FormGroup = ({children: control, controlId, label, helpers, required = false}: Props) => (
     <div className='AknFieldContainer'>
         {label && (
             <div className='AknFieldContainer-header'>
                 <FormLabel id={controlId} label={label} required={required || control.props.required} />
             </div>
         )}
+
         <InputContainer className='AknFieldContainer-inputContainer'>
             {cloneElement(control, {id: controlId})}
         </InputContainer>
-        {helper && (
-            <div className='AknFieldContainer-footer'>
-                <FormControlHelper>{helper}</FormControlHelper>
-            </div>
-        )}
-        {errors && errors.length > 0 && (
-            <div className='AknFieldContainer-footer'>
-                <FormControlErrors errors={errors} />
-            </div>
-        )}
-        {success && (
-            <div className='AknFieldContainer-footer'>
-                <FormControlSuccess success={success} />
-            </div>
+
+        {helpers && (
+            <HelperContainer>
+                {helpers.map(helper => (
+                    <div className='AknFieldContainer-footer'>{helper}</div>
+                ))}
+            </HelperContainer>
         )}
     </div>
 );
@@ -47,14 +37,6 @@ const InputContainer = styled.div`
     min-height: 28px;
 `;
 
-const FormControlErrors = ({errors}: {errors: string[]}) => (
-    <div className='AknFieldContainer-validationErrors'>
-        {errors.map(error => (
-            <FormControlError key={error} error={error} />
-        ))}
-    </div>
-);
-
-const FormControlHelper = styled.div`
+const HelperContainer = styled.div`
     margin-top: 6px;
 `;
