f625c71503b20a96e346c1932726dd680a906a2b
Mathias Métayer <mathias.metayer@akeneo.com>
2020-12-07 09:12:07 +0100

    CPM-93: Add field on boolean attribute edit form

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/attribute/form/properties/default-bool-value.ts
@@ -0,0 +1,29 @@
+const BaseSelect = require('pim/form/common/fields/select');
+
+type ModelValue = 'false'|'true'|'';
+type FieldValue = boolean|null;
+
+class DefaultBoolValue extends BaseSelect {
+
+  initialize(meta: any): void {
+    meta.config.choices = {
+      'false': 'pim_common.no',
+      'true': 'pim_common.yes',
+    }
+    super.initialize(meta);
+  }
+
+  getFieldValue(field: string): FieldValue {
+    const value = super.getFieldValue(field);
+
+    return 'false' === value ? false : ('true' === value ? true : null);
+  }
+
+  getModelValue(): ModelValue {
+    const modelValue = super.getModelValue();
+
+    return true === modelValue ? 'true' : (false === modelValue ? 'false' : '');
+  }
+}
+
+export = DefaultBoolValue;
