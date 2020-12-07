69d1d48a6057cdbafc869b7151ac6f4b800fdec8
Olivier Pontier <olivier.pontier@akeneo.com>
2020-12-07 12:53:43 +0100

    fix code style issues

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/tests/front/unit/hooks/locales/useActivatedLocales.unit.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/tests/front/unit/hooks/locales/useActivatedLocales.unit.tsx
@@ -7,42 +7,42 @@ import {act} from 'react-test-renderer';
 jest.mock('@akeneo-pim-community/settings-ui/src/infrastructure/fetchers/localesFetcher');
 
 describe('useInitialLocalesDataGridState', () => {
-    const renderUseInitialLocalesIndexState = () => {
-        return renderHookWithProviders(useActivatedLocales);
-    };
-    beforeEach(() => {
-        jest.clearAllMocks();
-        jest.resetAllMocks();
+  const renderUseInitialLocalesIndexState = () => {
+    return renderHookWithProviders(useActivatedLocales);
+  };
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  test('it initializes the state for Locales datagrid', () => {
+    const {result} = renderUseInitialLocalesIndexState();
+
+    expect(result.current.locales).toEqual([]);
+    expect(result.current.load).toBeDefined();
+    expect(result.current.isPending).toBeTruthy();
+  });
+
+  test('it loads the activated locales', async () => {
+    const activatedLocales = aListOfLocales(['en_US', 'fr_FR', 'en_US']);
+
+    // @ts-ignore
+    fetchActivatedLocales.mockResolvedValue(activatedLocales);
+
+    const {result} = renderUseInitialLocalesIndexState();
+
+    expect(result.current.isPending).toBeTruthy();
+
+    await act(async () => {
+      result.current.load();
     });
 
-    afterAll(() => {
-        jest.resetAllMocks();
-    });
+    expect(result.current.isPending).toBeFalsy();
 
-    test('it initializes the state for Locales datagrid', () => {
-        const {result} = renderUseInitialLocalesIndexState();
-
-        expect(result.current.locales).toEqual([]);
-        expect(result.current.load).toBeDefined();
-        expect(result.current.isPending).toBeTruthy();
-    });
-
-    test('it loads the activated locales', async () => {
-        const activatedLocales = aListOfLocales(['en_US', 'fr_FR', 'en_US']);
-
-        // @ts-ignore
-        fetchActivatedLocales.mockResolvedValue(activatedLocales);
-
-        const {result} = renderUseInitialLocalesIndexState();
-
-        expect(result.current.isPending).toBeTruthy();
-
-        await act(async () => {
-            result.current.load();
-        });
-
-        expect(result.current.isPending).toBeFalsy();
-
-        expect(result.current.locales).toBe(activatedLocales);
-    });
+    expect(result.current.locales).toBe(activatedLocales);
+  });
 });
