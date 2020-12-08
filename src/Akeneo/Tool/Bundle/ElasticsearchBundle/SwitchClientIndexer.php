ce453d85b7b892288b03aff5d9fc6200446be87f
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-12-08 17:29:46 +0100

    PIM-9584: rename MultipleClientIndexer to SwitchClientIndexer and add command

--- src/Akeneo/Tool/Bundle/ElasticsearchBundle/SwitchClientIndexer.php
+++ src/Akeneo/Tool/Bundle/ElasticsearchBundle/SwitchClientIndexer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Tool\Bundle\ElasticsearchBundle;
 
 use Akeneo\Platform\VersionProviderInterface;
+use Webmozart\Assert\Assert;
 
 /**
  * The purpose of this class is to provide index methods that impact current index + the next index.
@@ -16,16 +17,36 @@ use Akeneo\Platform\VersionProviderInterface;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class MultipleClientIndexer implements ClientIndexerInterface
+final class SwitchClientIndexer implements ClientIndexerInterface
 {
+    public const ONLY_CURRENT_INDEX_MODE = 'only_current_index';
+    public const BOTH_INDEXES_MODE = 'both_indexes';
+    public const ONLY_NEXT_INDEX_MODE = 'only_next_index';
+
     private Client $client;
     private VersionProviderInterface $versionProvider;
+    private string $mode;
     private ?Client $clientForNextIndex = null;
 
-    public function __construct(Client $client, VersionProviderInterface $versionProvider)
-    {
+    public function __construct(
+        Client $client,
+        VersionProviderInterface $versionProvider,
+        string $mode = 'only_current_index'
+    ) {
         $this->client = $client;
         $this->versionProvider = $versionProvider;
+        $this->setMode($mode);
+    }
+
+    public function setMode(string $mode): void
+    {
+        Assert::oneOf($mode, [
+            static::ONLY_CURRENT_INDEX_MODE,
+            static::BOTH_INDEXES_MODE,
+            static::ONLY_NEXT_INDEX_MODE,
+        ]);
+
+        $this->mode = $mode;
     }
 
     /**
@@ -33,13 +54,14 @@ final class MultipleClientIndexer implements ClientIndexerInterface
      */
     public function index(string $id, array $body, Refresh $refresh = null): array
     {
-        $result = $this->client->index($id, $body, $refresh);
-        $client = $this->getClientForNextIndex();
-        if (null !== $client) {
-            $client->index($id, $body, $refresh);
+        $results = [];
+        foreach ($this->getClientsToImpact() as $client) {
+            $results[] = $client->index($id, $body, $refresh);
         }
 
-        return $result;
+        Assert::notEmpty($results, 'No indexation is performed');
+
+        return $results[0];
     }
 
     /**
@@ -47,13 +69,14 @@ final class MultipleClientIndexer implements ClientIndexerInterface
      */
     public function bulkIndexes(array $documents, string $keyAsId = null, Refresh $refresh = null): array
     {
-        $result = $this->client->bulkIndexes($documents, $keyAsId, $refresh);
-        $client = $this->getClientForNextIndex();
-        if (null !== $client) {
-            $client->bulkIndexes($documents, $keyAsId, $refresh);
+        $results = [];
+        foreach ($this->getClientsToImpact() as $client) {
+            $results[] = $client->bulkIndexes($documents, $keyAsId, $refresh);
         }
 
-        return $result;
+        Assert::notEmpty($results, 'No indexation is performed');
+
+        return $results[0];
     }
 
     /**
@@ -61,10 +84,8 @@ final class MultipleClientIndexer implements ClientIndexerInterface
      */
     public function deleteByQuery(array $query): void
     {
-        $this->client->deleteByQuery($query);
-        $client = $this->getClientForNextIndex();
-        if (null !== $client) {
-            $client->deleteByQuery($query);
+        foreach ($this->getClientsToImpact() as $client) {
+            $results[] = $client->deleteByQuery($query);
         }
     }
 
@@ -73,13 +94,28 @@ final class MultipleClientIndexer implements ClientIndexerInterface
      */
     public function refreshIndex(): array
     {
-        $result = $this->client->refreshIndex();
-        $client = $this->getClientForNextIndex();
-        if (null !== $client) {
-            $client->refreshIndex();
+        $results = [];
+        foreach ($this->getClientsToImpact() as $client) {
+            $results[] = $client->refreshIndex();
         }
 
-        return $result;
+        Assert::notEmpty($results, 'No refresh is performed');
+
+        return $results[0];
+    }
+
+    private function getClientsToImpact(): array
+    {
+        $clients = [];
+        if (in_array($this->mode, [static::BOTH_INDEXES_MODE, static::ONLY_CURRENT_INDEX_MODE])) {
+            $clients[] = $this->client;
+        }
+
+        if (in_array($this->mode, [static::BOTH_INDEXES_MODE, static::ONLY_NEXT_INDEX_MODE])) {
+            $clients[] = $this->getClientForNextIndex();
+        }
+
+        return array_filter($clients);
     }
 
     private function getClientForNextIndex(): ?Client
