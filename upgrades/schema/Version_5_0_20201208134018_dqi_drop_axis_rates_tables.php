74906ff1380273b1a8cffcdad43b50a30ccd015b
Laurent Petard <laurent.petard@akeneo.com>
2020-12-08 14:50:34 +0100

    DAPI-1433: Add migration script to drop deprecated axis rates tables

---
+++ upgrades/schema/Version_5_0_20201208134018_dqi_drop_axis_rates_tables.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_5_0_20201208134018_dqi_drop_axis_rates_tables extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql(<<<SQL
+DROP TABLE IF EXISTS pim_data_quality_insights_product_axis_rates;
+DROP TABLE IF EXISTS pim_data_quality_insights_product_model_axis_rates;
+SQL
+        );
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
