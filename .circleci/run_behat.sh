f9e074dd06380a36ac19fdcc245bd7a389553746
jean-marie leroux <jmleroux.pro@gmail.com>
2020-12-09 15:10:27 +0100

    Fix legacy Behats run scripts on Circle CI (#13478)

--- .circleci/run_behat.sh
+++ .circleci/run_behat.sh
@@ -5,17 +5,17 @@ set -eo pipefail
 TEST_SUITE=$1
 
 TEST_FILES=$(docker-compose run --rm -T php vendor/bin/behat --list-scenarios -p legacy -s $TEST_SUITE | circleci tests split --split-by=timings)
+echo "TEST FILES ON THIS CONTAINER: $TEST_FILES"
 
 fail=0
 counter=1
-total=$(($(echo $TEST_FILES | grep -o ' ' | wc -l) + 1))
+total=$(echo $TEST_FILES | tr ' ' "\n" | wc -l)
 
 for TEST_FILE in $TEST_FILES; do
-    echo "$TEST_FILE ($counter/$total):"
+    echo -e "\nLAUNCHING $TEST_FILE ($counter/$total):"
     output=$(basename $TEST_FILE)_$(uuidgen)
 
     set +e
-    docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE ||
     docker-compose exec -u www-data -T fpm ./vendor/bin/behat --strict --format pim --out var/tests/behat/${output} --format pretty --out std --colors -p legacy -s $TEST_SUITE $TEST_FILE
 
     fail=$(($fail + $?))
