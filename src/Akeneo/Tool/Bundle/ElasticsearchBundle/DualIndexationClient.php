97c57db77a834999af14aa94009a5a350bfec7ae
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-12-10 10:49:10 +0100

    PIM-9584: add spec for DualIndexationClient

--- src/Akeneo/Tool/Bundle/ElasticsearchBundle/DualIndexationClient.php
+++ src/Akeneo/Tool/Bundle/ElasticsearchBundle/DualIndexationClient.php
@@ -4,37 +4,40 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Bundle\ElasticsearchBundle;
 
-use Akeneo\Tool\Bundle\ElasticsearchBundle\Exception\IndexationException;
 use Akeneo\Tool\Bundle\ElasticsearchBundle\IndexConfiguration\Loader;
 use Elasticsearch\ClientBuilder;
 
 /**
+ * This class extends behavior of "write" methods in order to index on 2 both aliases in same time:
+ *  - The alias specified by the $aliasName property
+ *  - The alias associated to the $dualClient property
+ *
  * @author    Nicolas Marniesse <nicolas.marniesse@akeneo.com>
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 final class DualIndexationClient extends Client
 {
-    private Client $temporaryClient;
+    private Client $dualClient;
 
     public function __construct(
         ClientBuilder $builder,
         Loader $configurationLoader,
         array $hosts,
         string $aliasName,
-        string $idPrefix = '',
-        Client $temporaryClient
+        string $idPrefix,
+        Client $dualClient
     ) {
         parent::__construct($builder, $configurationLoader, $hosts, $aliasName, $idPrefix);
-        $this->temporaryClient = $temporaryClient;
+        $this->dualClient = $dualClient;
     }
 
     /**
      * {@inheritDoc}
      */
-    public function index(string $id, array $body, Refresh $refresh = null): array
+    public function index($id, array $body, Refresh $refresh = null): array
     {
-        $this->temporaryClient->index($id, $body, $refresh);
+        $this->dualClient->index($id, $body, $refresh);
 
         return parent::index($id, $body, $refresh);
     }
@@ -42,9 +45,9 @@ final class DualIndexationClient extends Client
     /**
      * {@inheritDoc}
      */
-    public function bulkIndexes(array $documents, string $keyAsId = null, Refresh $refresh = null): array
+    public function bulkIndexes($documents, $keyAsId = null, Refresh $refresh = null): array
     {
-        $this->temporaryClient->bulkIndexes($documents, $keyAsId, $refresh);
+        $this->dualClient->bulkIndexes($documents, $keyAsId, $refresh);
 
         return parent::bulkIndexes($documents, $keyAsId, $refresh);
     }
@@ -55,7 +58,7 @@ final class DualIndexationClient extends Client
     public function deleteByQuery(array $query): void
     {
         parent::deleteByQuery($query);
-        $this->temporaryClient->deleteByQuery($query);
+        $this->dualClient->deleteByQuery($query);
     }
 
     /**
@@ -63,7 +66,7 @@ final class DualIndexationClient extends Client
      */
     public function refreshIndex(): array
     {
-        $this->temporaryClient->refreshIndex();
+        $this->dualClient->refreshIndex();
 
         return parent::refreshIndex();
     }
