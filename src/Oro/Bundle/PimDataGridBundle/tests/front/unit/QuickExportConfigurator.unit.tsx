adf49c5f1a3bb4e6506651eb5c61ba4bd4c3ffa7
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-12-16 17:14:47 +0100

    CPM-54: handle multiple media files in product and product model export (#13404)
    * CPM-54: handle multiple media files in archive
    
    * CPM-47: add "with media" option in quick exports (#13431)
    
    * CPM-47: add "with media" option in quick export
    
    * CPM-47: add changelog
    
    * CPM-54: display with media select only for product grid
    
    * CPM-54: fix behat
    
    * CPM-54: rename export option

--- src/Oro/Bundle/PimDataGridBundle/tests/front/unit/QuickExportConfigurator.unit.tsx
+++ src/Oro/Bundle/PimDataGridBundle/tests/front/unit/QuickExportConfigurator.unit.tsx
@@ -10,6 +10,7 @@ test('it displays a button and no modal initially', () => {
   const {getByTitle, queryByTitle} = renderWithProviders(
     <QuickExportConfigurator
       showWithLabelsSelect={true}
+      showWithMediaSelect={true}
       onActionLaunch={onActionLaunch}
       getProductCount={getProductCount}
     />
@@ -26,6 +27,7 @@ test('it does not call the action launch if an option is not set', () => {
   const {getByTitle} = renderWithProviders(
     <QuickExportConfigurator
       showWithLabelsSelect={true}
+      showWithMediaSelect={true}
       onActionLaunch={onActionLaunch}
       getProductCount={getProductCount}
     />
@@ -47,6 +49,7 @@ test('it does call the action launch if every option is set', () => {
   const {getByTitle, getByText} = renderWithProviders(
     <QuickExportConfigurator
       showWithLabelsSelect={true}
+      showWithMediaSelect={true}
       onActionLaunch={onActionLaunch}
       getProductCount={getProductCount}
     />
@@ -56,17 +59,29 @@ test('it does call the action launch if every option is set', () => {
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.csv'));
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.grid_context'));
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.with_labels'));
+  fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.with_media'));
   fireEvent.click(getByTitle('pim_common.export'));
 
-  expect(onActionLaunch).toHaveBeenCalledWith({context: 'grid-context', type: 'csv', 'with-labels': 'with-labels'});
+  expect(onActionLaunch).toHaveBeenCalledWith({
+    context: 'grid-context',
+    type: 'csv',
+    'with-labels': 'with-labels',
+    'with_media': 'true',
+  });
 
   fireEvent.click(getByTitle('pim_datagrid.mass_action_group.quick_export.label'));
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.xlsx'));
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.all_attributes'));
   fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.with_codes'));
+  fireEvent.click(getByText('pim_datagrid.mass_action.quick_export.configurator.without_media'));
   fireEvent.click(getByTitle('pim_common.export'));
 
-  expect(onActionLaunch).toHaveBeenCalledWith({context: 'all-attributes', type: 'xlsx', 'with-labels': 'with-codes'});
+  expect(onActionLaunch).toHaveBeenCalledWith({
+    context: 'all-attributes',
+    type: 'xlsx',
+    'with-labels': 'with-codes',
+    'with_media': 'false',
+  });
 });
 
 test('it does not display the with-labels select if specified', () => {
@@ -76,10 +91,27 @@ test('it does not display the with-labels select if specified', () => {
   const {queryByText} = renderWithProviders(
     <QuickExportConfigurator
       showWithLabelsSelect={false}
+      showWithMediaSelect={true}
       onActionLaunch={onActionLaunch}
       getProductCount={getProductCount}
     />
   );
 
   expect(queryByText('pim_datagrid.mass_action.quick_export.configurator.with_labels')).not.toBeInTheDocument();
+});
+
+test('it does not display the with-media select if specified', () => {
+  const onActionLaunch = jest.fn();
+  const getProductCount = jest.fn(() => 3);
+
+  const {queryByText} = renderWithProviders(
+    <QuickExportConfigurator
+      showWithLabelsSelect={true}
+      showWithMediaSelect={false}
+      onActionLaunch={onActionLaunch}
+      getProductCount={getProductCount}
+    />
+  );
+
+  expect(queryByText('pim_datagrid.mass_action.quick_export.configurator.with_media')).not.toBeInTheDocument();
 });
