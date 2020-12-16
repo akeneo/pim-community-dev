4f4012b45a62a8d236c4431754e3d0026bc86a07
Pierre Jolly <pierre.jolly@akeneo.com>
2020-12-16 04:04:42 +0100

    CXP-575: Remove the fake subscriptions mecanism

--- src/Akeneo/Connectivity/Connection/back/Domain/Webhook/Persistence/Query/GetConnectionUserForFakeSubscription.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Akeneo\Connectivity\Connection\Domain\Webhook\Persistence\Query;
-
-interface GetConnectionUserForFakeSubscription
-{
-    public function execute(): ?int;
-}
