03e5d7e767f9be46fbce3a26424d54ccc597fe68
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-12-16 11:14:15 +0100

    CXP-599: Limit the Events API requests sent

---
+++ src/Akeneo/Connectivity/Connection/back/Application/Webhook/Log/EventSubscriptionRequestsLimitReachedLog.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Application\Webhook\Log;
+
+/**
+ * @author    Thomas Galvaing <thomas.galvaing@akeneo.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class EventSubscriptionRequestsLimitReachedLog
+{
+    const TYPE = 'event_api.reach_requests_limit';
+    const MESSAGE = 'event subscription requests limit has been reached';
+
+    private int $limit;
+
+    private function __construct(int $limit)
+    {
+        $this->limit = $limit;
+    }
+
+    public static function fromLimit(int $limit): self
+    {
+        return new self($limit);
+    }
+
+    /**
+     * @return array{
+     *  type: string,
+     *  message: string,
+     *  limit: int
+     * }
+     */
+    public function toLog(): array
+    {
+        return [
+            'type' => self::TYPE,
+            'message' => self::MESSAGE,
+            'limit' => $this->limit,
+        ];
+    }
+}
---
+++ src/Akeneo/Connectivity/Connection/back/Domain/Audit/Persistence/Query/CountHourlyEventsApiRequestQuery.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Domain\Audit\Persistence\Query;
+
+/**
+ * @author    Thomas Galvaing <thomas.galvaing@akeneo.com>
+ * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface CountHourlyEventsApiRequestQuery
+{
+    public function execute(\DateTimeImmutable $eventDateTime): int;
+}
