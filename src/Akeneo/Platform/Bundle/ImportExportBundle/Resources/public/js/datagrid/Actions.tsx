cda24534e81314d20c66cf6d85fa0fa581789620
Tseho <quentin.favrie@gmail.com>
2020-12-17 15:38:15 +0100

    fix

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Button} from 'akeneo-design-system';
-import {useTranslate} from '@akeneo-pim-community/legacy-bridge';
+import {useSecurity, useTranslate} from '@akeneo-pim-community/legacy-bridge';
 import {StopJobAction} from 'pimui/js/job/execution/StopJobAction';
-
-const securityContext = require('pim/security-context');
 
 const ActionsContainer = styled.div`
   display: flex;
@@ -18,20 +16,21 @@ type ActionsProps = {
   isStoppable: boolean;
   showLink: string;
   refreshCollection: () => void;
-  isVisible: boolean;
 };
 
-const checkAuthorization = () => {
-    return securityContext.isGranted('pim_importexport_import_execution_show');
-};
-
-const Actions = ({id, jobLabel, isStoppable, showLink, refreshCollection, isVisible}: ActionsProps) => {
+const Actions = ({id, jobLabel, isStoppable, showLink, refreshCollection}: ActionsProps) => {
   const translate = useTranslate();
+  const security = useSecurity();
+
+  const isAllowedToViewJobDetails = security.isGranted('pim_importexport_import_execution_show');
+
   return (
     <ActionsContainer className="AknGrid-onHoverElement">
-      <Button size="small" ghost={true} level="tertiary" href={`#${showLink}`} isVisible={checkAuthorization} >
-        {translate('pim_datagrid.action.show.title')}
-      </Button>
+      {isAllowedToViewJobDetails &&
+        <Button size="small" ghost={true} level="tertiary" href={`#${showLink}`}>
+          {translate('pim_datagrid.action.show.title')}
+        </Button>
+      }
       <StopJobAction
         id={id}
         jobLabel={jobLabel}
