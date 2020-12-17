91923df8131639ef059260a5ddb9b6fc2461b08b
anaisbaunelemaire <anais.lemaire@akeneo.com>
2020-12-17 15:01:15 +0100

    PIM-9595: [WIP] Make the 'view' button invisible if no permission to access import details

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Resources/public/js/datagrid/Actions.tsx
@@ -4,6 +4,8 @@ import {Button} from 'akeneo-design-system';
 import {useTranslate} from '@akeneo-pim-community/legacy-bridge';
 import {StopJobAction} from 'pimui/js/job/execution/StopJobAction';
 
+const securityContext = require('pim/security-context');
+
 const ActionsContainer = styled.div`
   display: flex;
   gap: 10px;
@@ -16,13 +18,18 @@ type ActionsProps = {
   isStoppable: boolean;
   showLink: string;
   refreshCollection: () => void;
+  isVisible: boolean;
 };
 
-const Actions = ({id, jobLabel, isStoppable, showLink, refreshCollection}: ActionsProps) => {
+const checkAuthorization = () => {
+    return securityContext.isGranted('pim_importexport_import_execution_show');
+};
+
+const Actions = ({id, jobLabel, isStoppable, showLink, refreshCollection, isVisible}: ActionsProps) => {
   const translate = useTranslate();
   return (
     <ActionsContainer className="AknGrid-onHoverElement">
-      <Button size="small" ghost={true} level="tertiary" href={`#${showLink}`}>
+      <Button size="small" ghost={true} level="tertiary" href={`#${showLink}`} isVisible={checkAuthorization} >
         {translate('pim_datagrid.action.show.title')}
       </Button>
       <StopJobAction
