cd987b3b1512204b5fe3dad9998a9b6f0c0cd207
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2020-12-17 10:36:05 +0100

    CPM-101: fix test class name

--- src/Akeneo/Connectivity/Connection/back/tests/Integration/Persistence/Dbal/Query/DbalSelectConnectionsWebhookQueryIntegration.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Integration/Persistence/Dbal/Query/DbalSelectConnectionsWebhookQueryIntegration.php
@@ -20,7 +20,7 @@ use PHPUnit\Framework\Assert;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DbalSelectActiveWebhooksQueryIntegration extends TestCase
+class DbalSelectConnectionsWebhookQueryIntegration extends TestCase
 {
     /** @var ConnectionLoader */
     private $connectionLoader;
