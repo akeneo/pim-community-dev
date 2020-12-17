639f93db441e2dfdbac7c706aad13b102068850a
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-12-17 17:59:58 +0100

    RAC-437-flag: Fix type definition

--- akeneo-design-system/src/@types/index.d.ts
+++ akeneo-design-system/src/@types/index.d.ts
@@ -1,17 +1,18 @@
 declare module 'react-country-flag' {
   export interface ReactCountryFlagProps<T> extends React.DetailedHTMLProps<React.LabelHTMLAttributes<T>, T> {
     cdnSuffix?: string;
+    /** @default 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/4x3/'' */
     cdnUrl?: string;
     countryCode: string;
+    /** @default false */
     svg?: boolean;
     style?: React.CSSProperties;
   }
-  const ReactCountryFlag: React.FC<ReactCountryFlagProps<HTMLSpanElement | HTMLImageElement>> = ({
-    cdnSuffix = 'svg',
-    cdnUrl = 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/4x3/',
-    svg = false,
-    style = {},
-    countryCode,
-  }) => React.ReactNode; // return should be HTMLImageElement or HTMLSpanElement but close enough for me.
+
+  /**
+   * React component for emoji/svg country flags
+   */
+  declare const ReactCountryFlag: React.FC<ReactCountryFlagProps<HTMLSpanElement | HTMLImageElement>>;
+
   export default ReactCountryFlag;
 }
