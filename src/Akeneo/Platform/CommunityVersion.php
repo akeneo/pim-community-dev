859e840c75a2f6695d8b65ebfafeb980f5683347
Michel Tag <micheltag@akeneo.com>
2020-12-18 16:52:11 +0100

    Preparing version 4.0.79 (#13566)
    Preparing version 4.0.79
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.78';
+    const VERSION = '4.0.79';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Tonic';
