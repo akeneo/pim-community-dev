20c30fc701d043a1a36d9a6475d0817a267a706f
Ronan FAUGLAS <ronan.fauglas@gmail.com>
2020-12-23 18:56:36 +0100

    BH-218: copy migrations script (#10138)
    Co-authored-by: rfauglas <ronan.fauglas@akeneo.com>

--- std-build/install-required-files.sh
+++ std-build/install-required-files.sh
@@ -74,3 +74,8 @@ cp $DEV_DISTRIB_DIR/.env.dev $STANDARD_DISTRIB_DIR/
 
 # Skeleton .env file
 cp $DEV_DISTRIB_DIR/.gitignore $STANDARD_DISTRIB_DIR/
+
+# Database migrations
+mkdir -p $STANDARD_DISTRIB_DIR/upgrades/
+cp -R $DEV_DISTRIB_DIR/../pim-community-dev/upgrades/* $STANDARD_DISTRIB_DIR/upgrades/
+cp -R $DEV_DISTRIB_DIR/upgrades/* $STANDARD_DISTRIB_DIR/upgrades/
