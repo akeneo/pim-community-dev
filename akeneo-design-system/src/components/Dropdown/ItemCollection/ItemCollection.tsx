42799f71424f6ec02fd5f134144a40659a2d5a3c
Julien Sanchez <sumbobyboys@gmail.com>
2021-01-05 00:25:14 +0100

    RAC-375: fix checkstyle

--- akeneo-design-system/src/components/Dropdown/ItemCollection/ItemCollection.tsx
+++ akeneo-design-system/src/components/Dropdown/ItemCollection/ItemCollection.tsx
@@ -1,36 +1,7 @@
 import {Key, Override} from '../../../shared';
-import React, {ReactNode, useRef, Ref, useCallback} from 'react';
+import React, {ReactNode, useRef, Ref, useCallback, KeyboardEvent} from 'react';
 import styled from 'styled-components';
 import {useAutoFocus} from 'hooks';
-
-const decorateChildren = (children: ReactNode) => {
-  const firstItemRef = useRef<HTMLDivElement>(null);
-  const handleKeyDown = useCallback((event: KeyboardEvent) => {
-    if (null !== event.currentTarget) {
-      if (event.key === Key.ArrowDown) {
-        (event.currentTarget as any)?.nextSibling.focus();
-        event.preventDefault();
-      }
-      if (event.key === Key.ArrowUp) {
-        (event.currentTarget as any)?.previousSibling.focus();
-        event.preventDefault();
-      }
-    }
-  }, []);
-  const decoratedChildren = React.Children.map(children, (child, index) => {
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, {
-        ref: 0 === index ? firstItemRef : undefined,
-        onKeyDown: handleKeyDown,
-      });
-    }
-
-    return child;
-  });
-  useAutoFocus(firstItemRef);
-
-  return decoratedChildren;
-};
 
 const ItemCollectionContainer = styled.div`
   max-height: 320px;
@@ -47,7 +18,30 @@ type ItemCollectionProps = Override<
 
 const ItemCollection = React.forwardRef<HTMLDivElement, ItemCollectionProps>(
   ({children, ...rest}: ItemCollectionProps, forwardedRef: Ref<HTMLDivElement>): React.ReactElement => {
-    const decoratedChildren = decorateChildren(children);
+    const firstItemRef = useRef<HTMLDivElement>(null);
+    const handleKeyDown = useCallback((event: KeyboardEvent<HTMLDivElement>) => {
+      if (null !== event.currentTarget) {
+        if (event.key === Key.ArrowDown) {
+          ((event.currentTarget as HTMLElement).nextSibling as HTMLElement)?.focus();
+          event.preventDefault();
+        }
+        if (event.key === Key.ArrowUp) {
+          ((event.currentTarget as HTMLElement).previousSibling as HTMLElement)?.focus();
+          event.preventDefault();
+        }
+      }
+    }, []);
+    const decoratedChildren = React.Children.map(children, (child, index) => {
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child, {
+          ref: 0 === index ? firstItemRef : undefined,
+          onKeyDown: handleKeyDown,
+        });
+      }
+
+      return child;
+    });
+    useAutoFocus(firstItemRef);
 
     return (
       <ItemCollectionContainer {...rest} ref={forwardedRef}>
--- akeneo-design-system/src/hooks/useBooleanState.unit.ts
+++ akeneo-design-system/src/hooks/useBooleanState.unit.ts
@@ -2,33 +2,34 @@ import {act} from 'react-test-renderer';
 import {renderHook} from '@testing-library/react-hooks';
 import {useBooleanState} from './useBooleanState';
 
-test('It manages false default value', async () => {
+test('It manages false default value', () => {
   const {result} = renderHook(() => useBooleanState(true));
 
   const [isOpen] = result.current;
 
-  await expect(isOpen).toEqual(true);
+  expect(isOpen).toEqual(true);
 });
-test('It manages true default value', async () => {
+test('It manages true default value', () => {
   const {result} = renderHook(() => useBooleanState(false));
   const [isOpen] = result.current;
 
-  await expect(isOpen).toEqual(false);
+  expect(isOpen).toEqual(false);
 });
 
 test('It manages opening and closing', async () => {
-  let {result} = renderHook(() => useBooleanState());
+  const {result} = renderHook(() => useBooleanState());
 
-  let [isOpen, open, close] = result.current;
-  await expect(isOpen).toEqual(false);
-  act(() => {
+  let [isOpen] = result.current;
+  const [, open, close] = result.current;
+  expect(isOpen).toEqual(false);
+  await act(() => {
     open();
   });
   [isOpen] = result.current;
-  await expect(isOpen).toEqual(true);
-  act(() => {
+  expect(isOpen).toEqual(true);
+  await act(() => {
     close();
   });
   [isOpen] = result.current;
-  await expect(isOpen).toEqual(false);
+  expect(isOpen).toEqual(false);
 });
