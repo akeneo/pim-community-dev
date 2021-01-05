5b86f5532eba6387a7bc9bb454fd9d6ee9e74860
Mathias Métayer <mathias.metayer@akeneo.com>
2021-01-05 10:31:51 +0100

    CPM-124: Initialization

---
+++ src/Akeneo/Pim/Automation/TaskScheduling/back/Infrastructure/Symfony/AkeneoTaskSchedulingBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\TaskScheduling\back\Infrastructure\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AkeneoTaskSchedulingBundle extends Bundle
+{
+}
