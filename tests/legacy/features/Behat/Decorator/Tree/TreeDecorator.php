5816cb06924c0a3c7e169ae1ae319aa4132ccd6e
pierallard <pierallard@gmail.com>
2021-01-06 15:17:11 +0100

    CPM-117: Fix tests

---
+++ tests/legacy/features/Behat/Decorator/Tree/TreeDecorator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pim\Behat\Decorator\Tree;
+
+use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Exception\ElementNotFoundException;
+use Context\Spin\SpinCapableTrait;
+use Pim\Behat\Decorator\ElementDecorator;
+
+class TreeDecorator extends ElementDecorator
+{
+    use SpinCapableTrait;
+
+    public function findNodeInTree(string $nodeName): TreeDecorator
+    {
+        return $this->spin(function () use ($nodeName) {
+            $trees = $this->findAll('css', 'li[role=treeitem]');
+            foreach ($trees as $tree) {
+                if (strpos($tree->getText(), $nodeName) === 0) {
+                    return $this->decorate($tree, [TreeDecorator::class]);
+                }
+            }
+
+            return false;
+        }, sprintf('Node "%s" not found, found %s',
+                $nodeName,
+                join(', ', array_map(function ($tree) {
+                    return sprintf('"%s"', $tree->getText());
+                }, $this->findAll('css', 'li[role=treeitem]'))))
+        );
+    }
+
+    public function select()
+    {
+        $checkbox = $this->find('css', 'div[role=checkbox]');
+        if ($checkbox->getAttribute('aria-checked') === 'false') {
+            $checkbox->click();
+        }
+    }
+
+    public function expandNode($nodeName)
+    {
+        $tree = $this->findNodeInTree($nodeName);
+        if ($tree->getAttribute('aria-expanded') === 'false') {
+            $tree->find('css', 'button')->click();
+        }
+
+        $this->spin(function () use ($nodeName) {
+            return $this->findNodeInTree($nodeName)->getAttribute('aria-expanded') === 'true';
+        }, sprintf('Unable to open %s', $nodeName));
+    }
+}
