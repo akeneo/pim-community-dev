4efc826d8040a83f7901a5611dc6f3fbc370883c
pierallard <pierallard@gmail.com>
2021-01-06 15:00:57 +0100

    CPM-117: Replace tree on PEF

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/components/CategoryTree/RecursiveCategoryTree.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { CategoryTreeModel } from "./CategoryTree";
+import {Tree} from 'akeneo-design-system/lib/components/Tree/Tree';
+
+type RecursiveCategoryTreeProps = {
+  tree: CategoryTreeModel;
+  childrenCallback: (value: any) => Promise<CategoryTreeModel[]>;
+  onChange: (value: string, checked: boolean) => void;
+}
+
+type CategoryTreeValue = {
+  id: number;
+  code: string;
+}
+
+const RecursiveCategoryTree: React.FC<RecursiveCategoryTreeProps> = ({
+  tree,
+  childrenCallback,
+  onChange,
+}) => {
+  const [treeState, setTreeState] = React.useState<CategoryTreeModel>(tree);
+
+  const handleOpen = () => {
+    console.log(treeState.children);
+    if (typeof treeState.children === 'undefined') {
+      setTreeState({...treeState, loading: true});
+      childrenCallback(treeState.id).then((children) => {
+        setTreeState({...treeState, loading: false, children});
+      });
+    }
+  }
+
+  const handleChange = (value: CategoryTreeValue, checked: boolean) => {
+    setTreeState({ ...treeState, selected: checked });
+    onChange(value.code, checked);
+  }
+
+  return <Tree<CategoryTreeValue>
+    label={treeState.label}
+    value={{
+      id: treeState.id,
+      code: treeState.code
+    }}
+    selected={treeState.selected}
+    isLoading={treeState.loading}
+    readOnly={treeState.readOnly}
+    selectable={treeState.selectable}
+    isLeaf={Array.isArray(treeState.children) && treeState.children.length == 0}
+    onChange={handleChange}
+    onOpen={handleOpen}
+  >
+    {treeState.children &&
+    treeState.children.map(childNode => {
+      return (
+        <RecursiveCategoryTree
+          key={childNode.id}
+          tree={childNode}
+          onChange={onChange}
+          childrenCallback={childrenCallback}/>
+        )
+      })
+    }
+  </Tree>
+}
+
+export {RecursiveCategoryTree};
