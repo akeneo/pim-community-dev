de88822b553e810d2a8c850158643a162adc0d64
pierallard <pierallard@gmail.com>
2021-01-06 15:00:57 +0100

    CPM-117: Replace tree on PEF

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/components/CategoryTree/CategoryTree.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { RecursiveCategoryTree } from "./RecursiveCategoryTree";
+import {Tree} from 'akeneo-design-system/lib/components/Tree/Tree';
+
+type CategoryTreeModel = {
+  id: number;
+  code: string;
+  label: string;
+  children?: CategoryTreeModel[];
+  selectable: boolean;
+  loading?: boolean;
+  selected?: boolean;
+  readOnly?: boolean;
+}
+
+type CategoryTreeProps = {
+  init: () => Promise<CategoryTreeModel>;
+  childrenCallback: (value: any) => Promise<CategoryTreeModel[]>;
+  onChange: (value: string, checked: boolean) => void;
+};
+
+const CategoryTree: React.FC<CategoryTreeProps> = ({
+  init,
+  childrenCallback,
+  onChange,
+  ...rest
+}) => {
+  const [tree, setTree] = React.useState<CategoryTreeModel>();
+
+  React.useEffect(() => {
+    init().then(categoryTree => setTree(categoryTree));
+  }, []);
+
+  if (!tree) {
+    return <Tree value='' label='' isLoading={true} {...rest} />;
+  }
+
+  return (
+    <RecursiveCategoryTree
+      tree={tree}
+      childrenCallback={childrenCallback}
+      onChange={onChange}
+      {...rest}
+    />
+  );
+};
+
+export type {CategoryTreeModel};
+export {CategoryTree};
