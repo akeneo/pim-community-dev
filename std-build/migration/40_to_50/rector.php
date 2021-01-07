f8d41450ace15599653bc4fad7fc2ddb48a6e0ab
rfauglas <ronan.fauglas@akeneo.com>
2021-01-07 14:02:23 +0100

    documentation for install

--- std-build/migration/40_to_50/rector.php
+++ std-build/migration/40_to_50/rector.php
@@ -11,6 +11,7 @@ use Rector\Renaming\Rector\Name\RenameClassRector;
 
 
 /*
+ * Install:  docker-compose run  -u www-data --rm php composer require --dev rector/rector-prefixed
  * Test:  docker-compose run  -u www-data --rm php vendor/bin/rector -c std-build/migration/40_to_50/rector.php process ./std-build/migration/40_to_50/test-src
  */
 
--- std-build/migration/renamed_classes.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'oldName' => 'newName',
-    ];
