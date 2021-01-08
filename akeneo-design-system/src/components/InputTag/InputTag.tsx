7100fb0a1f87996f1fe99facc04c4d5c18ce6344
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-01-08 18:16:08 +0100

    DAPI-1471: allow tags to be removed using keyboard only

--- akeneo-design-system/src/components/InputTag/InputTag.tsx
+++ akeneo-design-system/src/components/InputTag/InputTag.tsx
@@ -1,126 +1,142 @@
-import React, {useState, useRef, ChangeEvent, FC} from 'react';
+import React, {useState, useRef, ChangeEvent, FC, RefObject} from 'react';
 import styled from 'styled-components';
 import {AkeneoThemedProps, getColor} from '../../theme';
 import {CloseIcon} from '../../icons';
-import {Key} from "../../shared";
+import {Key} from '../../shared';
 
 type InputTagProps = {
-    /**
-     * Specifies if the component will accept duplicated tags or not
-     */
-    allowDuplicates: boolean;
+  /**
+   * Specifies if the component will accept duplicated tags or not
+   */
+  allowDuplicates: boolean;
 
-    /**
-     * Default tags to display
-     */
-    defaultTags?: string[];
-}
+  /**
+   * Default tags to display
+   */
+  defaultTags?: string[];
+};
 
 const InputTag: FC<InputTagProps> = ({allowDuplicates, defaultTags = []}) => {
-    const [tags, setTags] = useState<string[]>(defaultTags);
-    const [isLastTagSelected, selectLastTag] = useState<boolean>(false);
-    const inputRef = useRef();
-    const containerRef = useRef();
-    const inputContainerRef = useRef();
+  const [tags, setTags] = useState<string[]>(defaultTags);
+  const [isLastTagSelected, selectLastTag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef();
+  const inputContainerRef = useRef();
 
-    const addTag = (event: ChangeEvent<HTMLInputElement>) => {
-        let tagsAsString = event.currentTarget.value;
-        if (tagsAsString !== '') {
-            let newTags = tagsAsString.split(/[\s]+/);
-            if (newTags.length === 1) {
-                return;
-            }
-            newTags = newTags.filter((word: string) => word.trim() !== '');
-            newTags = [...tags, ...newTags];
-            if (!allowDuplicates) {
-                newTags = arrayUnique(newTags);
-            }
-            setTags(newTags);
-            if (inputRef.current) {
-                inputRef.current.value = '';
-            }
-        }
+  const addTag = (event: ChangeEvent<HTMLInputElement>) => {
+    const tagsAsString = event.currentTarget.value;
+    if (tagsAsString !== '') {
+      let newTags = tagsAsString.split(/[\s]+/);
+      if (newTags.length === 1) {
+        return;
+      }
+      newTags = newTags.filter((word: string) => word.trim() !== '');
+      newTags = [...tags, ...newTags];
+      if (!allowDuplicates) {
+        newTags = arrayUnique(newTags);
+      }
+      setTags(newTags);
+      if (inputRef && inputRef.current) {
+        inputRef.current.value = '';
+      }
+    }
+  };
+
+  const removeWord = (wordToRemove: string) => {
+    const newTags = tags.filter((word: string) => word !== wordToRemove);
+    setTags(newTags);
+  };
+
+  const focusOnInputText = (event: MouseEvent, ref: RefObject<HTMLElement>) => {
+    if (ref && ref.current === event.target && inputRef && inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const onBackspaceKeyUp = (event: KeyboardEvent) => {
+    if (![Key.Backspace, Key.Delete].includes(event.key)) {
+      selectLastTag(false);
+      return;
     }
 
-    const removeWord = (wordToRemove: string) => {
-        const newTags = tags.filter((word: string) => word !== wordToRemove);
-        setTags(newTags);
+    if (tags.length === 0 || inputRef.current.value.trim() !== '') {
+      return;
     }
 
-    const focusOnInputText = (event: any, ref: any) => {
-        ref.current && ref.current === event.target && inputRef.current ? inputRef.current.focus() : null
+    if (!isLastTagSelected) {
+      selectLastTag(true);
+    } else {
+      const newTags = tags.slice(0, tags.length - 1);
+      setTags(newTags);
+      selectLastTag(false);
     }
+  };
 
-    const onBackspaceKeyUp = (event: KeyboardEvent) => {
-        if(event.key !== Key.Backspace)
-        {
-            return;
-        }
-
-        if(event.key === Key.Backspace && !isLastTagSelected){
-            selectLastTag(true)
-        }
-
-        //Delete the tag.
-    }
-
-    return <TagContainer ref={containerRef} onClick={(event: any) => focusOnInputText(event, containerRef)}>
-        {tags.map((tag, key) => {
-            return (
-              <Tag key={key} data-testid={'tag'}>
-                  <RemoveWordIcon onClick={() => removeWord(tag)} data-testid={`remove-${tag}`}/>
-                  {tag}
-              </Tag>
-            );
-        })}
-        <Tag key='inputer' ref={inputContainerRef} onClick={(event: any) => focusOnInputText(event, inputContainerRef)}>
-            <input type='text' data-testid={'tag-input'} ref={inputRef} onChange={addTag} onKeyUp={}/>
-        </Tag>
-    </TagContainer>;
-}
+  return (
+    <TagContainer ref={containerRef} onClick={(event: MouseEvent) => focusOnInputText(event, containerRef)}>
+      {tags.map((tag, key) => {
+        return (
+          <Tag key={key} data-testid={'tag'} isSelected={key === tags.length - 1 && isLastTagSelected}>
+            <RemoveWordIcon onClick={() => removeWord(tag)} data-testid={`remove-${tag}`} />
+            {tag}
+          </Tag>
+        );
+      })}
+      <Tag key="inputer" ref={inputContainerRef} onClick={(event: any) => focusOnInputText(event, inputContainerRef)}>
+        <input
+          type="text"
+          data-testid={'tag-input'}
+          ref={inputRef}
+          onKeyDownCapture={onBackspaceKeyUp}
+          onChange={addTag}
+        />
+      </Tag>
+    </TagContainer>
+  );
+};
 
 const arrayUnique = (array: string[]) => {
-    return Array.from(new Set(array))
-}
+  return Array.from(new Set(array));
+};
 
 const RemoveWordIcon = styled(CloseIcon)<AkeneoThemedProps>`
-    width: 15px;
-    height: 15px;
-    color: ${getColor('grey', 100)};
-    margin-right: 3px;
-    cursor: pointer;
+  width: 15px;
+  height: 15px;
+  color: ${getColor('grey', 100)};
+  margin-right: 3px;
+  cursor: pointer;
 `;
 
 const TagContainer = styled.ul<AkeneoThemedProps>`
-    border: 1px ${getColor('grey', 80)} solid;
-    border-radius: 2px;
-    padding: 5px 5px 0px 5px;
-    display: flex;
-    flex-wrap: wrap;
+  border: 1px ${getColor('grey', 80)} solid;
+  border-radius: 2px;
+  padding: 5px 5px 0px 5px;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
-const Tag = styled.li<AkeneoThemedProps>`
-    list-style-type: none;
-    padding: 9px 5px;
-    margin: 0 5px 5px 0;
-    border: 1px ${getColor('grey', 80)} solid;
-    background-color: ${getColor('grey', 20)};
-    text-transform: capitalize;
-    display: flex;
-    align-items: center;
+const Tag = styled.li<AkeneoThemedProps & {isSelected: boolean}>`
+  list-style-type: none;
+  padding: 9px 5px;
+  margin: 0 5px 5px 0;
+  border: 1px ${getColor('grey', 80)} solid;
+  background-color: ${props => (props.isSelected ? getColor('grey', 40) : getColor('grey', 20))};
+  text-transform: capitalize;
+  display: flex;
+  align-items: center;
+  color: ${getColor('grey', 120)};
+
+  :last-child {
+    background-color: ${getColor('white')};
+    border: 0;
+    flex: 1;
+  }
+
+  > input {
+    border: 0;
+    outline: 0;
     color: ${getColor('grey', 120)};
-
-    :last-child {
-        background-color: ${getColor('white')};
-        border: 0;
-        flex: 1;
-    }
-
-    > input {
-        border: 0;
-        outline: 0;
-        color: ${getColor('grey', 120)};
-    }
+  }
 `;
 
 export {InputTag};
--- akeneo-design-system/src/components/InputTag/InputTag.unit.tsx
+++ akeneo-design-system/src/components/InputTag/InputTag.unit.tsx
@@ -5,71 +5,70 @@ import {render, screen} from '../../storybook/test-util';
 import userEvent from '@testing-library/user-event';
 
 test('it renders an empty input tag', () => {
-    const result = render(<InputTag allowDuplicates={true}/>);
+  const result = render(<InputTag allowDuplicates={true} />);
 
-    expect(result.container.textContent).toBe(expectedTags([]));
+  expect(result.container.textContent).toBe(expectedTags([]));
 });
 
 test('it renders an input tag with default tags', () => {
-    const result = render(<InputTag allowDuplicates={true} defaultTags={['gucci', 'samsung', 'apple']}/>);
+  const result = render(<InputTag allowDuplicates={true} defaultTags={['gucci', 'samsung', 'apple']} />);
 
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
 });
 
 test('it renders a list of tags', () => {
-    const result = render(<InputTag allowDuplicates={true}/>);
+  const result = render(<InputTag allowDuplicates={true} />);
 
-    userEvent.type(screen.getByTestId('tag-input'), 'gucci{space}samsung{space}');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung']));
+  userEvent.type(screen.getByTestId('tag-input'), 'gucci{space}samsung{space}');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung']));
 });
 
 test('it handle a copy pasted input list of tags', () => {
-    const result = render(<InputTag allowDuplicates={true}/>);
+  const result = render(<InputTag allowDuplicates={true} />);
 
-    userEvent.paste(screen.getByTestId('tag-input'), ' gucci samsung    apple asus  ');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple', 'asus']));
+  userEvent.paste(screen.getByTestId('tag-input'), ' gucci samsung    apple asus  ');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple', 'asus']));
 });
 
 test('it handle a deletion of a tag', () => {
-    const result = render(<InputTag allowDuplicates={true}/>);
+  const result = render(<InputTag allowDuplicates={true} />);
 
-    userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
-    userEvent.click(screen.getByTestId('remove-samsung'));
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'apple']));
-    userEvent.click(screen.getByTestId('remove-apple'));
-    expect(result.container.textContent).toBe(expectedTags(['gucci']));
-    userEvent.click(screen.getByTestId('remove-gucci'));
-    expect(result.container.textContent).toBe(expectedTags([]));
+  userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
+  userEvent.click(screen.getByTestId('remove-samsung'));
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'apple']));
+  userEvent.click(screen.getByTestId('remove-apple'));
+  expect(result.container.textContent).toBe(expectedTags(['gucci']));
+  userEvent.click(screen.getByTestId('remove-gucci'));
+  expect(result.container.textContent).toBe(expectedTags([]));
 });
 
 test('it can keep duplicated tags', () => {
-    const result = render(<InputTag allowDuplicates={true}/>);
+  const result = render(<InputTag allowDuplicates={true} />);
 
-    userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple samsung gucci');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple', 'samsung', 'gucci']));
+  userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple samsung gucci');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple', 'samsung', 'gucci']));
 });
 
 test('it can remove duplicated tags', () => {
-    const result = render(<InputTag allowDuplicates={false}/>);
+  const result = render(<InputTag allowDuplicates={false} />);
 
-    userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple samsung gucci');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
+  userEvent.paste(screen.getByTestId('tag-input'), 'gucci samsung apple samsung gucci');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
 });
 
 test('it removes the tag on the left of the input on several user events', () => {
-    const result = render(<InputTag defaultTags={['gucci', 'samsung', 'apple']} allowDuplicates={false}/>);
+  const result = render(<InputTag defaultTags={['gucci', 'samsung', 'apple']} allowDuplicates={false} />);
 
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
-    userEvent.type(screen.getByTestId('tag-input'), '{backspace}');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
-    userEvent.type(screen.getByTestId('tag-input'), '{backspace}');
-    expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung']));
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
+  userEvent.type(screen.getByTestId('tag-input'), '{backspace}');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung', 'apple']));
+  userEvent.type(screen.getByTestId('tag-input'), '{del}');
+  expect(result.container.textContent).toBe(expectedTags(['gucci', 'samsung']));
 });
 
-
 const expectedTags = (tags: string[]) => {
-    expect(screen.queryAllByTestId('tag')).toHaveLength(tags.length);
+  expect(screen.queryAllByTestId('tag')).toHaveLength(tags.length);
 
-    return tags.join('');
-}
+  return tags.join('');
+};
