6950a055092787c160ed9bad260ce6601cbce40c
vgelot <vincent.gelot@akeneo.com>
2021-01-08 10:51:51 +0000

    Preparing version 5.0.1

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.0';
+    const VERSION = '5.0.1';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
