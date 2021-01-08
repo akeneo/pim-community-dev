3ef7a898fe8f9211f5e6f80911172b0961b523e4
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-01-08 14:17:10 +0100

    DAPI-1471: wip

--- akeneo-design-system/src/components/InputTag/InputTag.tsx
+++ akeneo-design-system/src/components/InputTag/InputTag.tsx
@@ -1,41 +1,87 @@
-import React, {KeyboardEvent, useState, useRef} from "react";
-import styled from "styled-components";
-
-const TagContainer = styled.ul`
-    border: 1px solid grey;
-`;
-
-const Tag = styled.li`
-    float: left;
-    list-style-type: none;
-    padding:5px;
-    margin: 5px;
-    border:1px solid grey;
-    background-color: #9e9e9e;
-`;
+import React, {KeyboardEvent, useState, useRef} from 'react';
+import styled from 'styled-components';
+import {AkeneoThemedProps, getColor} from '../../theme';
+import {CloseIcon} from '../../icons';
 
 const InputTag = () => {
     const [tags, setTags] = useState<string[]>([]);
     const inputRef = useRef();
 
+    const addWord = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(event.key===' ')
+        {
+            const word = event.currentTarget.value.trim();
+            if (word !== ''){
+                const newWords = [...tags, ...[word]];
+                setTags(arrayUnique(newWords));
+                if (inputRef.current) {
+                    inputRef.current.value = '';
+                }
+            }
+        }
+    }
+
+    const removeWord = (wordToRemove: string) => {
+        const newTags = tags.filter((word: string) => word !== wordToRemove);
+        setTags(newTags);
+    }
+
     return <TagContainer>
         {tags.map((tag, key) => {
-            return <Tag key={key} data-testid={'tag'}>{tag}</Tag>;
+            return (
+              <Tag key={key} data-testid={'tag'}>
+                  <RemoveWordIcon onClick={() => removeWord(tag)} />
+                  {tag}
+              </Tag>
+            );
         })}
         <Tag key='inputer'>
-            <input type="text" data-testid={'tag-input'} ref={inputRef} onKeyUp={(event: KeyboardEvent<HTMLInputElement>) => {
-                if(event.key===' ')
-                {
-                    const word = event.currentTarget.value.trim();
-                    if (word !== ''){
-                        setTags([...tags, ...[word]]);
-                        if (inputRef.current) {
-                            inputRef.current.value = '';
-                        }
-                    }
-                }
-            }}/></Tag>
+            <input type="text" data-testid={'tag-input'} ref={inputRef} onKeyUp={addWord}/>
+        </Tag>
     </TagContainer>;
 }
+
+const arrayUnique = (array: string[]) => {
+    return Array.from(new Set(array))
+}
+
+const RemoveWordIcon = styled(CloseIcon)<AkeneoThemedProps>`
+    width: 15px;
+    height: 15px;
+    color: ${getColor('grey', 100)};
+    margin-right: 3px;
+    cursor: pointer;
+`;
+
+const TagContainer = styled.ul<AkeneoThemedProps>`
+    border: 1px ${getColor('grey', 80)} solid;
+    border-radius: 2px;
+    padding: 6px 5px;
+    display: flex;
+`;
+
+const Tag = styled.li<AkeneoThemedProps>`
+    list-style-type: none;
+    padding: 9px 5px;
+    margin-right: 5px;
+    border: 1px ${getColor('grey', 80)} solid;
+    background-color: ${getColor('grey', 20)};
+    text-transform: capitalize;
+    display: flex;
+    align-items: center;
+    color: ${getColor('grey', 120)};
+    
+    :last-child {
+        background-color: ${getColor('white')};
+        border: 0;
+        flex: 1;
+    }
+    
+    > input {
+        border: 0;
+        outline: 0;
+        color: ${getColor('grey', 120)};
+    }
+`;
 
 export {InputTag};
