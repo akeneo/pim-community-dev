3c5d13145a62a167e3f94769860ee02290c2fcfd
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-01-11 12:19:22 +0100

    DAPI-1296: multiple minor fixes & improvements (PR comments)

--- akeneo-design-system/src/components/Pagination/Pagination.tsx
+++ akeneo-design-system/src/components/Pagination/Pagination.tsx
@@ -1,17 +1,17 @@
 import React, {FC} from 'react';
-import styled, {css} from 'styled-components';
-import {getColor, AkeneoThemedProps} from '../../theme';
+import styled from 'styled-components';
+import {PAGINATION_SEPARATOR, PaginationItem} from './PaginationItem';
 
 type PaginationProps = {
   /**
-   * The current page number (starting à 1).
+   * The current page number (starting at 1).
    */
   currentPage: number;
 
   /**
-   * The total number of pages.
+   * The total number of results.
    */
-  itemsTotal: number;
+  totalItems: number;
 
   /**
    * The number of items per page. Default to 25.
@@ -25,16 +25,19 @@ type PaginationProps = {
 };
 
 const MAX_PAGINATION_ITEMS_WITHOUT_SEPARATOR = 4;
-const PAGINATION_SEPARATOR = '...';
 
-const Pagination: FC<PaginationProps> = ({currentPage, itemsTotal, itemsPerPage = 25, onClick}) => {
+const Pagination: FC<PaginationProps> = ({currentPage, totalItems, itemsPerPage = 25, onClick}) => {
   if (itemsPerPage <= 0) {
     return <></>;
   }
 
-  const numberOfPages = Math.ceil(itemsTotal / itemsPerPage);
+  const numberOfPages = Math.ceil(totalItems / itemsPerPage);
 
-  if (numberOfPages <= 1 || currentPage > numberOfPages) {
+  if (currentPage > numberOfPages) {
+    throw new Error('');
+  }
+
+  if (numberOfPages <= 1) {
     return <></>;
   }
 
@@ -44,9 +47,7 @@ const Pagination: FC<PaginationProps> = ({currentPage, itemsTotal, itemsPerPage 
     <PaginationContainer>
       {pages.map((page: number | string, index: number) => {
         return (
-          <PaginationItem currentPage={page === currentPage} key={index} onClick={onClick}>
-            {page}
-          </PaginationItem>
+          <PaginationItem currentPage={page === currentPage} key={index} onClick={onClick} page={page as string} />
         );
       })}
     </PaginationContainer>
@@ -59,73 +60,7 @@ const PaginationContainer = styled.div`
   align-items: center;
   display: flex;
   justify-content: center;
-`;
-
-type PaginationItemProps = {
-  currentPage: boolean;
-  onClick: (page: number) => void;
-};
-
-const PaginationItem: FC<PaginationItemProps> = ({currentPage, children, onClick}) => {
-  const title = `No. ${children as string}`;
-
-  return (
-    <PaginationItemContainer
-      onClick={() => (children == PAGINATION_SEPARATOR ? null : onClick(children as number))}
-      data-testid="paginationItem"
-      title={children != PAGINATION_SEPARATOR ? title : ''}
-      disabled={children == PAGINATION_SEPARATOR}
-      currentPage={currentPage}
-    >
-      {children}
-    </PaginationItemContainer>
-  );
-};
-
-const currentPaginationItemMixin = css`
-  border: 1px ${getColor('brand', 100)} solid;
-  color: ${getColor('brand', 100)};
-`;
-
-const otherPaginationItemMixin = css`
-  border: 1px ${getColor('grey', 80)} solid;
-  color: ${getColor('grey', 100)};
-`;
-
-const disabledMixin = css`
-  cursor: default;
-  :hover {
-    background-color: ${getColor('white')};
-  }
-`;
-
-const PaginationItemContainer = styled.span<AkeneoThemedProps & PaginationItemProps & {disabled: boolean}>`
-  ${props => (props.currentPage ? currentPaginationItemMixin : otherPaginationItemMixin)}
-  display: inline-block;
-  border-width: 1px;
-  font-size: 13px;
-  font-weight: 400;
-  text-transform: uppercase;
-  border-radius: 16px;
-  padding: 0 10px;
-  height: 22px;
-  line-height: 21px;
-  cursor: pointer;
-  font-family: inherit;
-  transition: background-color 0.1s ease 0s;
-  min-width: 40px;
-  text-align: center;
-  box-sizing: border-box;
-
-  :not(:last-child) {
-    margin-right: 10px;
-  }
-
-  :hover {
-    background-color: ${getColor('grey', 20)};
-  }
-
-  ${props => (props.disabled ? disabledMixin : null)}
+  gap: 10px;
 `;
 
 function computePages(currentPage: number, numberOfPages: number) {
---
+++ akeneo-design-system/src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,81 @@
+import React, {FC, useCallback} from 'react';
+import styled, {css} from 'styled-components';
+import {AkeneoThemedProps, getColor} from '../../theme';
+
+const PAGINATION_SEPARATOR = '…';
+
+type PaginationItemProps = {
+  currentPage: boolean;
+  page: string;
+  onClick: (page: number) => void;
+};
+
+const PaginationItem: FC<PaginationItemProps> = ({currentPage, page, onClick}) => {
+  const handleClick = useCallback(() => {
+    if (page !== PAGINATION_SEPARATOR) {
+      onClick(parseInt(page));
+    }
+  }, [page]);
+
+  return (
+    <PaginationItemContainer
+      onClick={handleClick}
+      data-testid="paginationItem"
+      title={page !== PAGINATION_SEPARATOR ? `No. ${page}` : ''}
+      disabled={page === PAGINATION_SEPARATOR}
+      currentPage={currentPage}
+      page={page}
+    >
+      {page}
+    </PaginationItemContainer>
+  );
+};
+
+const currentPaginationItemMixin = css`
+  border: 1px ${getColor('brand', 100)} solid;
+  color: ${getColor('brand', 100)};
+`;
+
+const otherPaginationItemMixin = css`
+  border: 1px ${getColor('grey', 80)} solid;
+  color: ${getColor('grey', 100)};
+`;
+
+const disabledMixin = css`
+  cursor: default;
+  :hover {
+    background-color: ${getColor('white')};
+  }
+`;
+
+const PaginationItemContainer = styled.button<AkeneoThemedProps & PaginationItemProps & {disabled: boolean}>`
+  ${({currentPage}) => (currentPage ? currentPaginationItemMixin : otherPaginationItemMixin)}
+  display: inline-block;
+  border-width: 1px;
+  font-size: 13px;
+  font-weight: 400;
+  text-transform: uppercase;
+  border-radius: 16px;
+  padding: 0 10px;
+  height: 22px;
+  line-height: 21px;
+  cursor: pointer;
+  font-family: inherit;
+  transition: background-color 0.1s ease 0s;
+  min-width: 40px;
+  text-align: center;
+  box-sizing: border-box;
+  background-color: ${getColor('white')};
+
+  :hover {
+    background-color: ${getColor('grey', 20)};
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  ${props => (props.disabled ? disabledMixin : null)}
+`;
+
+export {PaginationItem, PAGINATION_SEPARATOR};
