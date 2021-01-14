1764fa9ee923643c7f10d3293e46101ae1eda1e1
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-01-14 17:53:58 +0100

    RAC-464: Add svg-jest-transformer

--- akeneo-design-system/jest.unit.config.js
+++ akeneo-design-system/jest.unit.config.js
@@ -5,8 +5,12 @@ module.exports = {
   roots: ['<rootDir>'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testMatch: ['**/?(*.)+(unit).ts?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '/generator/', 'src/illustrations/', 'src/icons/'],
-  transform: {'^.+\\.tsx?$': 'ts-jest', '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx'},
+  testPathIgnorePatterns: ['/node_modules/', '/generator/', 'src/illustrations/', 'src/icons/', '/static/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
+    "^.+\\.svg$": "jest-svg-transformer"
+  },
   transformIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts?(x)', '!**/*.visual.ts?(x)'],
