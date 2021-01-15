79b5c69a8b22aa28ebd44d25f783bcf414a9724a
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-01-15 10:54:44 +0100

    RAC-464: Fix import viewport

--- akeneo-design-system/src/illustrations/ImportIllustration.tsx
+++ akeneo-design-system/src/illustrations/ImportIllustration.tsx
@@ -3,7 +3,7 @@ import {IllustrationProps} from './IllustrationProps';
 import Import from '../../static/illustrations/Import.svg';
 
 const ImportIllustration = ({title, size = 256, ...props}: IllustrationProps) => (
-  <svg width={size} height={size} viewBox="0 0 243 177" {...props}>
+  <svg width={size} height={size} viewBox="0 0 256 256" {...props}>
     {title && <title>{title}</title>}
     <image href={Import} />
   </svg>
