f08ba09517821b397159778f15a6c3c5454487e6
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-01-19 11:20:12 +0100

    RAC-465: WIP

--- akeneo-design-system/src/icons/IconProps.ts
+++ akeneo-design-system/src/icons/IconProps.ts
@@ -1,10 +1,16 @@
-import {SVGProps} from 'react';
+import {RefObject, SVGProps} from 'react';
+import {Override} from '../shared';
 
-type IconProps = {
-  title?: string;
-  size?: number;
-  color?: string;
-  className?: string;
-} & SVGProps<SVGSVGElement>;
+type IconProps = Override<
+  SVGProps<SVGSVGElement>,
+  {
+    title?: string;
+    size?: number;
+    color?: string;
+    className?: string;
+    animateOnHover?: boolean;
+    ref?: RefObject<SVGSVGElement>;
+  }
+>;
 
 export type {IconProps};
