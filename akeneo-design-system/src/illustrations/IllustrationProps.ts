54e9c8a028e8fad850544539802011644300b817
Valentin Mumble <valentin.dijkstra@akeneo.com>
2021-01-22 10:05:46 +0100

    RAC-465: Move Icons & Illustrations animations to DSM (#13758)

--- akeneo-design-system/src/illustrations/IllustrationProps.ts
+++ akeneo-design-system/src/illustrations/IllustrationProps.ts
@@ -1,9 +1,15 @@
-import {SVGProps} from 'react';
+import {RefObject, SVGProps} from 'react';
+import {Override} from '../shared';
 
-type IllustrationProps = {
-  title?: string;
-  size?: number;
-  className?: string;
-} & SVGProps<SVGSVGElement>;
+type IllustrationProps = Override<
+  SVGProps<SVGSVGElement>,
+  {
+    title?: string;
+    size?: number;
+    className?: string;
+    animateOnHover?: boolean;
+    ref?: RefObject<SVGSVGElement>;
+  }
+>;
 
 export type {IllustrationProps};
