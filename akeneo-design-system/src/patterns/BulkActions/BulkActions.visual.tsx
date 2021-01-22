80894e6cd4f63756be5c5cb23ddbc332a5ffc1a7
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-01-22 15:31:24 +0100

    RAC-466: Add BulkActions with Cards stort

--- akeneo-design-system/src/patterns/BulkActions/BulkActions.visual.tsx
+++ akeneo-design-system/src/patterns/BulkActions/BulkActions.visual.tsx
@@ -2,15 +2,15 @@ import 'expect-puppeteer';
 import {toMatchImageSnapshot} from 'jest-image-snapshot';
 expect.extend({toMatchImageSnapshot});
 
-const storyIds = ['big-table', 'small-table'];
+const storyIds = ['big-table', 'small-table', 'with-cards'];
 
 test.each(storyIds)('Test Bulk Actions %s is displayed correctly', async storyId => {
   await page.goto(`http://localhost:6006/iframe.html?id=patterns-bulk-actions--${storyId}`);
   const root = await page.waitFor('#root');
   if (null === root) throw new Error('Cannot find root element');
 
-  const firstCheckbox = await page.$('tbody tr:first-child div[role="checkbox"]');
-  if (null === firstCheckbox) throw new Error('Cannot find first checkbox');
+  const firstCheckbox = await page.$('div[role="checkbox"]');
+  if (null === firstCheckbox) throw new Error('Cannot find checkbox');
   await firstCheckbox.click();
 
   await new Promise(resolve => setTimeout(resolve, 500));
