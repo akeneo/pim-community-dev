689b25e8515c2e883fbc68f1717f347959fe5846
Valentin Mumble <valentin.dijkstra@akeneo.com>
2021-01-25 07:35:58 +0100

    RAC-466: Small last fixes (#13779)

--- akeneo-design-system/src/hooks/useSelection.unit.ts
+++ akeneo-design-system/src/hooks/useSelection.unit.ts
@@ -4,31 +4,44 @@ import {useSelection} from './useSelection';
 test('It can generate a basic selection', () => {
   const {result} = renderHook(() => useSelection<string>(2));
 
-  const [, selectionState, isItemSelected, onSelectionChange] = result.current;
+  const [collection, selectionState, isItemSelected, onSelectionChange, , selectedCount] = result.current;
 
   expect(selectionState).toBe(false);
   expect(isItemSelected('donald')).toBe(false);
   expect(isItemSelected('melania')).toBe(false);
+  expect(selectedCount).toEqual(0);
+  expect(collection).toEqual([]);
 
   void act(() => {
     onSelectionChange('donald', true);
   });
 
-  const [, halfSelectionState, isHalfItemSelected] = result.current;
+  const [halfCollection, halfSelectionState, isHalfItemSelected, , , halfSelectedCount] = result.current;
 
   expect(isHalfItemSelected('donald')).toBe(true);
   expect(isHalfItemSelected('melania')).toBe(false);
   expect(halfSelectionState).toBe('mixed');
+  expect(halfSelectedCount).toEqual(1);
+  expect(halfCollection).toEqual(['donald']);
 
   void act(() => {
     onSelectionChange('melania', true);
   });
 
-  const [, completeSelectionState, isCompleteItemSelected] = result.current;
+  const [
+    completeCollection,
+    completeSelectionState,
+    isCompleteItemSelected,
+    ,
+    ,
+    completeSelectedCount,
+  ] = result.current;
 
   expect(isCompleteItemSelected('donald')).toBe(true);
   expect(isCompleteItemSelected('melania')).toBe(true);
   expect(completeSelectionState).toBe(true);
+  expect(completeSelectedCount).toEqual(2);
+  expect(completeCollection).toEqual(['donald', 'melania']);
 });
 
 test('It can handle unselection all', () => {
@@ -51,41 +64,54 @@ test('It can handle unselection all', () => {
 });
 
 test('It can handle selection all', () => {
-  const {result} = renderHook(() => useSelection<string>(2));
+  const {result} = renderHook(() => useSelection<string>(3));
 
-  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange] = result.current;
+  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange, selectedCount] = result.current;
 
   expect(selectionState).toBe(false);
   expect(isItemSelected('nice')).toBe(false);
+  expect(selectedCount).toEqual(0);
 
   void act(() => {
     onSelectionChange('nice', true);
     onSelectAllChange(true);
   });
 
-  const [, emptySelectionState, isEmptyItemSelected] = result.current;
+  const [emptyCollection, emptySelectionState, isEmptyItemSelected, , , emptySelectedCount] = result.current;
 
   expect(isEmptyItemSelected('nice')).toBe(true);
   expect(isEmptyItemSelected('donald')).toBe(true);
   expect(isEmptyItemSelected('melania')).toBe(true);
   expect(emptySelectionState).toBe(true);
+  expect(emptySelectedCount).toBe(3);
+  expect(emptyCollection).toEqual([]);
 
   void act(() => {
     onSelectionChange('donald', false);
   });
 
-  const [, unselectedSelectionState, isUnselectedItemSelected] = result.current;
+  const [
+    unselectedCollection,
+    unselectedSelectionState,
+    isUnselectedItemSelected,
+    ,
+    ,
+    unselectedCount,
+  ] = result.current;
   expect(isUnselectedItemSelected('donald')).toBe(false);
   expect(unselectedSelectionState).toBe('mixed');
+  expect(unselectedCollection).toEqual(['donald']);
+  expect(unselectedCount).toEqual(2);
 });
 
 test('It can handle selection after selection all', () => {
   const {result} = renderHook(() => useSelection<string>(2));
 
-  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange] = result.current;
+  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange, selectedCount] = result.current;
 
   expect(selectionState).toBe(false);
   expect(isItemSelected('nice')).toBe(false);
+  expect(selectedCount).toEqual(0);
 
   void act(() => {
     onSelectAllChange(true);
@@ -93,20 +119,22 @@ test('It can handle selection after selection all', () => {
     onSelectionChange('nice', true);
   });
 
-  const [, emptySelectionState, isEmptyItemSelected] = result.current;
+  const [, emptySelectionState, isEmptyItemSelected, , , emptySelectedCount] = result.current;
 
   expect(isEmptyItemSelected('nice')).toBe(true);
   expect(isEmptyItemSelected('donald')).toBe(true);
   expect(emptySelectionState).toBe(true);
+  expect(emptySelectedCount).toEqual(2);
 });
 
 test('It can handle unselection after unselect all', () => {
   const {result} = renderHook(() => useSelection<string>(2));
 
-  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange] = result.current;
+  const [, selectionState, isItemSelected, onSelectionChange, onSelectAllChange, selectedCount] = result.current;
 
   expect(selectionState).toBe(false);
   expect(isItemSelected('nice')).toBe(false);
+  expect(selectedCount).toEqual(0);
 
   void act(() => {
     onSelectAllChange(false);
@@ -114,9 +142,10 @@ test('It can handle unselection after unselect all', () => {
     onSelectionChange('nice', false);
   });
 
-  const [, emptySelectionState, isEmptyItemSelected] = result.current;
+  const [, emptySelectionState, isEmptyItemSelected, , , emptySelectedCount] = result.current;
 
   expect(isEmptyItemSelected('nice')).toBe(false);
   expect(isEmptyItemSelected('donald')).toBe(false);
   expect(emptySelectionState).toBe(false);
+  expect(emptySelectedCount).toEqual(0);
 });
