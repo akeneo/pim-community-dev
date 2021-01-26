bb206e1ac6f21445a6d90c2ae9c5aa6f958fcd68
Michel Tag <micheltag@akeneo.com>
2021-01-26 10:07:00 +0100

    Preparing version 4.0.87 (#10285)
    Preparing version 4.0.87
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.86';
+    const VERSION = '4.0.87';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Daisy';
