df00ea3c4b5776c37ababb2a7b5697cea4c9167e
olivierpontier <50582517+olivierpontier@users.noreply.github.com>
2021-01-26 15:38:06 +0100

    fix Helper link style issue on hover (#10289)

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Locale/Dictionary.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Locale/Dictionary.tsx
@@ -1,9 +1,16 @@
 import React, {FC} from 'react';
+import styled from "styled-components";
 import {Helper, Link} from 'akeneo-design-system';
 import {useTranslate} from '@akeneo-pim-community/legacy-bridge';
 import {AddWordsForm} from './Dictionary/AddWordsForm';
 import {useDictionaryState} from '../../../infrastructure';
 import {WordsGrid} from './Dictionary/WordsGrid';
+
+const PimHelperLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const Dictionary: FC = () => {
   const translate = useTranslate();
@@ -14,9 +21,9 @@ const Dictionary: FC = () => {
       <Helper level="info">
         {translate('akeneo_data_quality_insights.dictionary.helper.content')}
         <> </>
-        <Link href="https://help.akeneo.com/pim/serenity/articles/manage-your-data-quality.html#manage-your-dictionary" target="_blank">
+        <PimHelperLink href="https://help.akeneo.com/pim/serenity/articles/manage-your-data-quality.html#manage-your-dictionary" target="_blank">
           {translate('akeneo_data_quality_insights.dictionary.helper.link_label')}
-        </Link>
+        </PimHelperLink>
       </Helper>
 
       <AddWordsForm />
