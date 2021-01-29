91eefa3f4e6b2e5e714107af92c7877f8660f997
Michel Tag <micheltag@akeneo.com>
2021-01-29 12:35:38 +0100

    Preparing version 5.0.2 (#13823)
    Preparing version 5.0.2
    
    
    Co-authored-by: StevenVAIDIE <no-reply@akeneo.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.1';
+    const VERSION = '5.0.2';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
