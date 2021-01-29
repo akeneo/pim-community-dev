50bd2fc0024ab14afd65e2087ff656ff2ac287d5
Laurent Petard <laurent.petard@akeneo.com>
2021-01-29 11:34:33 +0100

    DAPI-1443: Init quality score filter (Front)

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/filter/product/quality-score.js
@@ -0,0 +1,72 @@
+'use strict';
+
+define([
+    'jquery',
+    'underscore',
+    'oro/translator',
+    'pim/filter/filter',
+    'pim/fetcher-registry',
+    'pim/user-context',
+    'pim/template/filter/product/quality-score',
+], function ($, _, __, BaseFilter, FetcherRegistry, UserContext, template) {
+    return BaseFilter.extend({
+        shortname: 'quality-score',
+        template: _.template(template),
+        events: {
+            'change [name="filter-value"]': 'updateState',
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        isEmpty: function () {
+            return _.isEmpty(this.getValue());
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        renderInput: function () {
+            return this.template({
+                __: __,
+                value: _.isArray(this.getValue()) ? this.getValue().join(', ') : '',
+                field: this.getField(),
+                isEditable: this.isEditable(),
+            });
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        getTemplateContext: function () {
+            return BaseFilter.prototype.getTemplateContext.apply(this, arguments).then(
+                function (templateContext) {
+                    return _.extend({}, templateContext, {
+                        removable: false,
+                    });
+                }.bind(this)
+            );
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        updateState: function () {
+            var value = this.$('[name="filter-value"]')
+                .val()
+                .split(/[\s,]+/);
+            var cleanedValues = _.reject(value, function (val) {
+                return '' === val;
+            });
+
+            this.setData({
+                field: this.getField(),
+                operator: 'IN ALL LOCALES',
+                value: cleanedValues,
+                context: {
+                    locales: this.getParentForm().getFilters().structure.locales
+                },
+            });
+        },
+    });
+});
