c3b0c8f1c90f66b29fc47cfa8bc757f1f0512df4
Bugsyaya <marina.blin@zenika.com>
2021-01-29 09:18:12 +0100

    WIP

--- akeneo-design-system/src/components/Scoring/Bar/ScoreBar.unit.tsx
+++ akeneo-design-system/src/components/Scoring/Bar/ScoreBar.unit.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {ScoreCell} from './ScoreCell';
+import {ScoreBar} from './ScoreBar';
 import {render, screen} from '../../../storybook/test-util';
 
 test('it renders its children properly', () => {
-  render(<ScoreCell />);
+  render(<ScoreBar />);
 
   expect(screen.getByText('a')).toBeInTheDocument();
   expect(screen.getByText('b')).toBeInTheDocument();
@@ -15,14 +15,14 @@ test('it renders its children properly', () => {
 // Those tests should pass directly if you follow the contributing guide.
 // If you add required props to your Component, these tests will fail
 // and you will need to add these required props here as well
-test('ScoreCell supports forwardRef', () => {
+test('ScoreBar supports forwardRef', () => {
   const ref = {current: null};
 
-  render(<ScoreCell ref={ref} />);
+  render(<ScoreBar ref={ref} />);
   expect(ref.current).not.toBe(null);
 });
 
-test('ScoreCell supports ...rest props', () => {
-  render(<ScoreCell data-testid="my_value" />);
+test('ScoreBar supports ...rest props', () => {
+  render(<ScoreBar data-testid="my_value" />);
   expect(screen.getByTestId('my_value')).toBeInTheDocument();
 });
---
+++ akeneo-design-system/src/components/Scoring/Cell/ScoreCell.tsx
@@ -0,0 +1,48 @@
+import React, {Ref} from 'react';
+import styled from 'styled-components';
+import {AkeneoThemedProps, getColorForScoring, getFontSize, Score} from '../../../theme';
+
+const ScoreCellContainer = styled.div<{score: Score} & ScoreCellProps & AkeneoThemedProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({score}) => getColorForScoring(score, score === 'a' || score === 'c' || score === 'd' ? 20 : 60)};
+  border-radius: 4px;
+  width: 20px;
+  height: 20px;
+`;
+
+const ScoreCellWrapper = styled.p<{score: Score} & ScoreCellProps & AkeneoThemedProps>`
+  font-size: ${getFontSize('default')};
+  color: ${({score}) =>
+    getColorForScoring(score, score === 'a' || score === 'c' ? 120 : score === 'b' || score === 'e' ? 140 : 100)};
+  margin: 0;
+  font-weight: 900;
+  text-transform: uppercase;
+`;
+
+type ScoreCellProps = {
+  /**
+   * Defines if Score should be highlighted.
+   */
+  score?: Score;
+};
+
+/**
+ * This component highlight to the users the level of quality of their product data.
+ */
+const ScoreCell = React.forwardRef<HTMLDivElement, ScoreCellProps>(
+  ({score, ...rest}: ScoreCellProps, forwardedRef: Ref<HTMLDivElement>) => {
+    return (
+      <div ref={forwardedRef} {...rest}>
+        {score && (
+          <ScoreCellContainer score={score}>
+            <ScoreCellWrapper score={score}>{score}</ScoreCellWrapper>
+          </ScoreCellContainer>
+        )}
+      </div>
+    );
+  }
+);
+
+export {ScoreCell};
