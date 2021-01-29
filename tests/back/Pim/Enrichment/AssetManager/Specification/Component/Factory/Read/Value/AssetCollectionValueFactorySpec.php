7b80765bd78ab22269b6652a50ffa9809cc67965
Steven VAIDIE <steven.vaidie@gmail.com>
2021-01-29 09:35:59 +0100

    RAC-507: fix test

--- tests/back/Pim/Enrichment/AssetManager/Specification/Component/Factory/Read/Value/AssetCollectionValueFactorySpec.php
+++ tests/back/Pim/Enrichment/AssetManager/Specification/Component/Factory/Read/Value/AssetCollectionValueFactorySpec.php
@@ -63,6 +63,7 @@ class AssetCollectionValueFactorySpec extends ObjectBehavior
             false,
             null,
             null,
+            null,
             'asset_collection',
             []
         );
