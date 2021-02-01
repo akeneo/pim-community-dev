4b416c0c4fa2a7e77f451e645c98c921bc01916c
Julien Sanchez <sumbobyboys@gmail.com>
2021-02-01 16:28:30 +0100

    RAC-481: add number input component

--- akeneo-design-system/src/components/Input/index.ts
+++ akeneo-design-system/src/components/Input/index.ts
@@ -2,3 +2,4 @@ export * from './BooleanInput/BooleanInput';
 export * from './InputProps';
 export * from './TextInput/TextInput';
 export * from './TagInput/TagInput';
+export * from './NumberInput/NumberInput';
