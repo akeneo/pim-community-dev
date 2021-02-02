544a39c334ec9d911356fb57479083f9d9966b2d
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-02-02 12:19:30 +0100

    DAPI-1307: minor fixes to TagInput component

--- akeneo-design-system/src/components/Input/TagInput/TagInput.tsx
+++ akeneo-design-system/src/components/Input/TagInput/TagInput.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useRef, ChangeEvent, FC, KeyboardEvent, useCallback} from 'react';
 import styled from 'styled-components';
 import {AkeneoThemedProps, getColor} from '../../../theme';
-import {CloseIcon} from '../../../icons';
+import {CloseIcon, LockIcon} from '../../../icons';
 import {arrayUnique, Key, Override} from '../../../shared';
 import {InputProps} from '../InputProps';
 
@@ -151,6 +151,7 @@ const TagInput: FC<TagInputProps> = ({onChange, placeholder, invalid, value = []
           readOnly={readOnly}
           {...inputProps}
         />
+        {readOnly && <ReadOnlyIcon size={16} />}
       </InputContainer>
     </TagContainer>
   );
@@ -167,13 +168,14 @@ const RemoveTagIcon = styled(CloseIcon)<AkeneoThemedProps>`
 const TagContainer = styled.ul<AkeneoThemedProps & {invalid: boolean}>`
   border: 1px solid ${({invalid}) => (invalid ? getColor('red', 100) : getColor('grey', 80))};
   border-radius: 2px;
-  padding: 5px;
+  padding: 4px;
   display: flex;
   flex-wrap: wrap;
   min-height: 40px;
   gap: 5px;
   box-sizing: border-box;
   background: ${({readOnly}) => (readOnly ? getColor('grey', 20) : getColor('white'))};
+  position: relative;
 
   &:focus-within {
     box-shadow: 0 0 0 2px ${getColor('blue', 40)};
@@ -209,6 +211,14 @@ const InputContainer = styled.li<AkeneoThemedProps>`
       font-family: 'Lato';
     }
   }
+`;
+
+const ReadOnlyIcon = styled(LockIcon)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 11px;
+  color: ${getColor('grey', 100)};
 `;
 
 export {TagInput};
