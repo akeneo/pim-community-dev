151edeedf522e15188b6b86cef4c7c6147ae13b0
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-02-02 10:04:37 +0100

    DAPI-1307: support Enter key to add words on a locale

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Locale/Dictionary/AddWordsForm.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/Locale/Dictionary/AddWordsForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, KeyboardEvent} from 'react';
 import {Button, Field, TagInput} from 'akeneo-design-system';
 import styled from 'styled-components';
 import {useTranslate} from '@akeneo-pim-community/legacy-bridge';
@@ -22,7 +22,13 @@ const AddWordsForm = () => {
       <Field label={translate('akeneo_data_quality_insights.dictionary.add_words')}>
         <FieldContent>
           <TagInputContainer>
-            <TagInput value={words} onChange={setWords} />
+            <TagInput value={words} onChange={setWords} onKeyDownCapture={async (event: KeyboardEvent) => {
+              if (event.key === 'Enter') {
+                event.preventDefault();
+                event.stopPropagation();
+                await onAddWords();
+              }
+            }} />
           </TagInputContainer>
           <Button ghost level="tertiary" onClick={onAddWords}>
             {translate('pim_common.add')}
