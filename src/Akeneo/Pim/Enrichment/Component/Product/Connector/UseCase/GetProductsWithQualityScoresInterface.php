c6eccb6d408d6b84a9a166a3cf673f83b2bcb3d8
Laurent Pétard <laurent.petard@akeneo.com>
2021-02-03 11:21:14 +0100

    DAPI-1488: Export products quality scores (Backend) (#13827)
    * DAPI-1488: Add ES filter on products quality scores for multiple locales
    
    * DAPI-1488: Add export columns for quality scores
    
    * DAPI-1488: Fix for published products

--- src/Akeneo/Pim/Enrichment/Component/Product/Connector/UseCase/GetProductsWithQualityScoresInterface.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Connector/UseCase/GetProductsWithQualityScoresInterface.php
@@ -9,7 +9,12 @@ use Akeneo\Pim\Enrichment\Component\Product\Connector\ReadModel\ConnectorProduct
 
 interface GetProductsWithQualityScoresInterface
 {
+    // @fixme: Find a better place to hold this constant
+    public const FLAT_FIELD_PREFIX = 'dqi_quality_score';
+
     public function fromConnectorProduct(ConnectorProduct $product): ConnectorProduct;
 
     public function fromConnectorProductList(ConnectorProductList $connectorProductList, ?string $channel = null, array $locales = []): ConnectorProductList;
+
+    public function fromNormalizedProduct(string $productIdentifier, array $normalizedProduct, ?string $channel = null, array $locales = []): array;
 }
