456d93f517650321c57404be176e07420f185839
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-02-11 14:54:28 +0100

    CPM-134: add roles export (#13842)
    * CPM-134: add config to export user roles
    
    * CPM-134: add permissions in export
    
    * CPM-134: fix translation
    
    * CPM-134: filter permissions
    
    * CPM-134: fix coupling by adding AclPrivilegeNormalizer
    
    * CPM-134: check privilige is authorized in normalizer
    
    * CPM-134: fix private

---
+++ tests/back/UserManagement/Specification/Component/Connector/ArrayConverter/StandardToFlat/RoleSpec.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Specification\Akeneo\UserManagement\Component\Connector\ArrayConverter\StandardToFlat;
+
+use Akeneo\Tool\Component\Connector\ArrayConverter\ArrayConverterInterface;
+use Akeneo\Tool\Component\Connector\ArrayConverter\FieldsRequirementChecker;
+use Akeneo\UserManagement\Component\Connector\ArrayConverter\StandardToFlat\Role;
+use PhpSpec\ObjectBehavior;
+
+class RoleSpec extends ObjectBehavior
+{
+    function let(FieldsRequirementChecker $fieldsRequirementChecker)
+    {
+        $this->beConstructedWith($fieldsRequirementChecker);
+    }
+
+    function it_is_an_array_converter()
+    {
+        $this->shouldBeAnInstanceOf(Role::class);
+        $this->shouldImplement(ArrayConverterInterface::class);
+    }
+
+    function it_converts_a_role_in_flat_format(FieldsRequirementChecker $fieldsRequirementChecker)
+    {
+        $role = [
+            'label' => 'Administrator',
+            'permissions' => [
+                ['id' => 'action:pim_enrich_product_create'],
+                ['id' => 'action:pim_enrich_product_index'],
+            ],
+        ];
+
+        $fieldsRequirementChecker->checkFieldsPresence($role, ['label'])->shouldBeCalled();
+
+        $convertedRole = $this->convert($role);
+        $convertedRole->shouldHaveKey('label');
+        $convertedRole['label']->shouldBe('Administrator');
+        $convertedRole->shouldHaveKey('permissions');
+        $convertedRole['permissions']->shouldBe('action:pim_enrich_product_create,action:pim_enrich_product_index');
+    }
+}
