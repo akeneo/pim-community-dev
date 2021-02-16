be7d0caf824eab244313a7f8a0d9ca6ef84cc7f8
Steven VAIDIE <steven.vaidie@gmail.com>
2021-02-16 10:12:03 +0100

    RAC-485: Review and add space between components

--- akeneo-design-system/src/components/Input/index.ts
+++ akeneo-design-system/src/components/Input/index.ts
@@ -1,6 +1,6 @@
 export * from './BooleanInput/BooleanInput';
 export * from './InputProps';
 export * from './NumberInput/NumberInput';
-export * from './TextInput/TextInput';
 export * from './SelectInput/SelectInput';
 export * from './TagInput/TagInput';
+export * from './TextInput/TextInput';
