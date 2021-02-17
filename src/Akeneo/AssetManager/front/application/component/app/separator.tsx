31cedbd3704040be3246767d5bfec2d322096d07
Julien Sanchez <sumbobyboys@gmail.com>
2021-02-17 11:22:01 +0100

    RAC-479: fix rebase

--- src/Akeneo/AssetManager/front/application/component/app/separator.tsx
+++ src/Akeneo/AssetManager/front/application/component/app/separator.tsx
@@ -4,10 +4,7 @@ import {ThemedProps} from 'akeneoassetmanager/application/component/app/theme';
 export const Separator = styled.div`
   border-left: 1px solid ${(props: ThemedProps<void>) => props.theme.color.grey100};
   margin: 0 10px;
-<<<<<<< HEAD
   height: 20px;
-=======
->>>>>>> 34d4702ae1 (RAC-486: move mass edit launcher into a dedicated component for unit test)
 
   &:first-child,
   &:last-child {
