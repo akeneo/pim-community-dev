8afca2fedcc312253325ca162165760ef83a8dee
Yanick Witschi <yanick.witschi@terminal42.ch>
2021-02-18 11:19:10 +0100

    Temporary directory configuration in jobs must be dynamic

--- src/Akeneo/Tool/Component/Connector/Job/JobParameters/DefaultValuesProvider/SimpleYamlExport.php
+++ src/Akeneo/Tool/Component/Connector/Job/JobParameters/DefaultValuesProvider/SimpleYamlExport.php
@@ -31,7 +31,7 @@ class SimpleYamlExport implements DefaultValuesProviderInterface
     public function getDefaultValues()
     {
         return [
-            'filePath'              => sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'export_%job_label%_%datetime%.yml',
+            'filePath'              => '%temp_dir%' . DIRECTORY_SEPARATOR . 'export_%job_label%_%datetime%.yml',
             'user_to_notify'        => null,
             'is_user_authenticated' => false,
         ];
