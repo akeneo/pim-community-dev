e101902de084e7f9f4232e6036f26c23646f152c
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-02-18 11:59:38 +0100

    DSM: Add disabled dropdown item state

--- akeneo-design-system/src/components/Dropdown/Item/Item.tsx
+++ akeneo-design-system/src/components/Dropdown/Item/Item.tsx
@@ -1,55 +1,62 @@
-import React, {KeyboardEvent, ReactNode, Ref, useCallback, useRef} from 'react';
-import styled from 'styled-components';
+import React, {isValidElement, KeyboardEvent, ReactNode, Ref, useCallback, useRef} from 'react';
+import styled, {css} from 'styled-components';
 import {AkeneoThemedProps, getColor} from '../../../theme';
 import {Image} from '../../../components/Image/Image';
 import {Checkbox} from '../../../components/Checkbox/Checkbox';
 import {Link} from '../../../components/Link/Link';
 import {Key, Override} from '../../../shared';
+import {LockIcon} from '../../../icons';
 
 const ItemLabel = styled.span`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   flex: 1;
-  color: ${getColor('grey', 120)};
   line-height: 34px;
 `;
 
-const ItemContainer = styled.div<{tall: boolean} & AkeneoThemedProps>`
+const ItemContainer = styled.div<{tall: boolean; disabled: boolean} & AkeneoThemedProps>`
   background: ${getColor('white')};
-  height: ${({tall}) => (tall ? '44px' : '34px')};
+  height: ${({tall}) => (tall ? 44 : 34)}px;
   padding: 0 20px;
   display: flex;
   align-items: center;
   gap: 10px;
-  cursor: pointer;
 
-  a {
-    color: ${getColor('grey', 120)};
-  }
-
-  &:focus ${ItemLabel} {
-    color: ${getColor('grey', 120)};
-  }
-  &:hover {
-    background: ${getColor('grey', 20)};
-  }
-  &:hover ${ItemLabel} {
-    color: ${getColor('brand', 140)};
-  }
-  &:active ${ItemLabel} {
-    color: ${getColor('brand', 100)};
-    font-style: italic;
-    font-weight: 700;
-  }
-  &:disabled ${ItemLabel} {
-    color: ${getColor('grey', 100)};
-  }
+  ${({disabled}) =>
+    disabled
+      ? css`
+          cursor: not-allowed;
+          color: ${getColor('grey', 100)};
+        `
+      : css`
+          cursor: pointer;
+          color: ${getColor('grey', 120)};
+          a {
+            color: ${getColor('grey', 120)};
+          }
+          :hover a,
+          &:hover {
+            background: ${getColor('grey', 20)};
+            color: ${getColor('brand', 140)};
+          }
+          :active a,
+          &:active {
+            color: ${getColor('brand', 100)};
+            font-style: italic;
+            font-weight: 700;
+          }
+        `}
 `;
 
 type ItemProps = Override<
   React.HTMLAttributes<HTMLDivElement>,
   {
+    /**
+     * Whether or not the item is disabled.
+     */
+    disabled?: boolean;
+
     /**
      * The content of the item.
      */
@@ -58,14 +65,17 @@ type ItemProps = Override<
 >;
 
 const Item = React.forwardRef<HTMLDivElement, ItemProps>(
-  ({children, onKeyDown, ...rest}: ItemProps, forwardedRef: Ref<HTMLDivElement>): React.ReactElement => {
+  (
+    {children, onKeyDown, disabled = false, ...rest}: ItemProps,
+    forwardedRef: Ref<HTMLDivElement>
+  ): React.ReactElement => {
     let tall = false;
     const actionableRef = useRef<HTMLAnchorElement>(null);
     const handleClick = useCallback(() => {
-      if (actionableRef.current !== null) {
+      if (null !== actionableRef.current && !disabled) {
         actionableRef.current.click();
       }
-    }, []);
+    }, [disabled]);
     const handleKeyDown = useCallback(
       (event: KeyboardEvent<HTMLDivElement>) => {
         if (Key.Enter === event.key || Key.Space === event.key) {
@@ -81,11 +91,16 @@ const Item = React.forwardRef<HTMLDivElement, ItemProps>(
 
     const decoratedChildren = React.Children.map(children, child => {
       if (typeof child === 'string') {
-        return <ItemLabel title={child}>{child}</ItemLabel>;
+        return (
+          <>
+            <ItemLabel title={child}>{child}</ItemLabel>
+            {disabled && <LockIcon size={18} />}
+          </>
+        );
       }
 
       // Change size of Image children
-      if (React.isValidElement(child) && child.type === Image) {
+      if (isValidElement(child) && child.type === Image) {
         tall = true;
 
         return React.cloneElement(child, {
@@ -94,22 +109,27 @@ const Item = React.forwardRef<HTMLDivElement, ItemProps>(
         });
       }
 
-      // Transmit onclick and space and enter to Link children
-      if (React.isValidElement(child) && child.type === Link) {
+      // Transmit onClick and space and enter to Link children
+      if (isValidElement(child) && child.type === Link) {
         return (
-          <ItemLabel>
-            {React.cloneElement(child, {
-              ref: actionableRef,
-              decorated: false,
-            })}
-          </ItemLabel>
+          <>
+            <ItemLabel>
+              {React.cloneElement(child, {
+                ref: actionableRef,
+                decorated: false,
+                disabled,
+              })}
+            </ItemLabel>
+            {disabled && <LockIcon size={18} />}
+          </>
         );
       }
 
-      // Same for checkboxes
-      if (React.isValidElement(child) && child.type === Checkbox) {
+      // Same for Checkboxes
+      if (isValidElement(child) && child.type === Checkbox) {
         return React.cloneElement(child, {
           ref: actionableRef,
+          readOnly: disabled,
         });
       }
 
@@ -119,9 +139,10 @@ const Item = React.forwardRef<HTMLDivElement, ItemProps>(
     return (
       <ItemContainer
         tall={tall}
-        tabIndex={actionableRef.current === null ? 0 : -1}
+        tabIndex={null === actionableRef.current ? 0 : -1}
         onClick={handleClick}
         onKeyDown={handleKeyDown}
+        disabled={disabled}
         {...rest}
         ref={forwardedRef}
       >
