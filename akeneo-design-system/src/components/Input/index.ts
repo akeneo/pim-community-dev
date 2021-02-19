079bfe3ee6402f40c96833cee5edad3f7f88b4fd
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-02-19 18:09:32 +0100

    CPM-138: add TextareaInput in DSM

--- akeneo-design-system/src/components/Input/index.ts
+++ akeneo-design-system/src/components/Input/index.ts
@@ -5,6 +5,8 @@ export * from './MediaFileInput/MediaFileInput';
 export * from './MediaLinkInput/MediaLinkInput';
 export * from './MultiSelectInput/MultiSelectInput';
 export * from './NumberInput/NumberInput';
+export * from './TextInput/TextInput';
+export * from './TextareaInput/TextareaInput';
 export * from './SelectInput/SelectInput';
 export * from './TagInput/TagInput';
 export * from './TextAreaInput/TextAreaInput';
