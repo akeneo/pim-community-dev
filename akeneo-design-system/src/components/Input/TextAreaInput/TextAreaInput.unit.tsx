fc3afa5e781b697c2cfbd963066644ff5e0e5306
Julien Sanchez <sumbobyboys@gmail.com>
2021-02-25 14:57:40 +0100

    RAC-480: fix types and eslint

--- akeneo-design-system/src/components/Input/TextAreaInput/TextAreaInput.unit.tsx
+++ akeneo-design-system/src/components/Input/TextAreaInput/TextAreaInput.unit.tsx
@@ -4,8 +4,10 @@ import {fireEvent, render, screen} from '../../../storybook/test-util';
 import {ContentBlock} from 'draft-js';
 
 jest.mock('draft-js', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const draftJS = jest.requireActual('draft-js');
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return {
     ...draftJS,
     convertFromHTML: (text: string) => ('WILL FAIL' === text ? undefined : {contentBlocks: [new ContentBlock({text})]}),
