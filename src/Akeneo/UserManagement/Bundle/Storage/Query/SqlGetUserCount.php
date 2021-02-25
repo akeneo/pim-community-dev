2b732c5a0155baebbabffe453d213552978f46b7
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-02-25 11:16:02 +0100

    PIM-9701: code style

--- src/Akeneo/UserManagement/Bundle/Storage/Query/SqlGetUserCount.php
+++ src/Akeneo/UserManagement/Bundle/Storage/Query/SqlGetUserCount.php
@@ -23,22 +23,22 @@ final class SqlGetUserCount implements GetUserCountInterface
     public function forUsersHavingOnlyRole(string $role): int
     {
         $sql = <<<SQL
-WITH
-user_with_role AS (
-    SELECT u.id, u.username
-    FROM oro_user u
-        JOIN oro_user_access_role ur ON u.id = ur.user_id
-        JOIN oro_access_role r ON ur.role_id = r.id AND r.role = :role
-),
-user_with_only_role AS (
-    SELECT uwr.username
-    FROM user_with_role uwr
-        JOIN oro_user_access_role r ON uwr.id = r.user_id
-    GROUP BY uwr.username
-    HAVING COUNT(r.role_id) = 1
-)
-SELECT count(username) FROM user_with_only_role
-SQL;
+            WITH
+            user_with_role AS (
+                SELECT u.id, u.username
+                FROM oro_user u
+                    JOIN oro_user_access_role ur ON u.id = ur.user_id
+                    JOIN oro_access_role r ON ur.role_id = r.id AND r.role = :role
+            ),
+            user_with_only_role AS (
+                SELECT uwr.username
+                FROM user_with_role uwr
+                    JOIN oro_user_access_role r ON uwr.id = r.user_id
+                GROUP BY uwr.username
+                HAVING COUNT(r.role_id) = 1
+            )
+            SELECT count(username) FROM user_with_only_role
+        SQL;
 
         return (int) $this->connection->executeQuery($sql, ['role' => $role])->fetchColumn();
     }
