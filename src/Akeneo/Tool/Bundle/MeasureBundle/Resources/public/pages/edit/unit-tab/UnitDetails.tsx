fbdb092ff1191531c8073ebce7da87ebbb288177
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-02-26 12:55:41 +0100

    RAC-527: Use Section in UnitDetails

--- src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/edit/unit-tab/UnitDetails.tsx
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/edit/unit-tab/UnitDetails.tsx
@@ -14,7 +14,7 @@ import {useUiLocales} from 'akeneomeasure/shared/hooks/use-ui-locales';
 import {UnitCode, getUnitLabel} from 'akeneomeasure/model/unit';
 import {ConfirmDeleteModal} from 'akeneomeasure/shared/components/ConfirmDeleteModal';
 import {useTranslate, useUserContext, useSecurity} from '@akeneo-pim-community/legacy-bridge';
-import {filterErrors, ValidationError, useToggleState, TextField} from '@akeneo-pim-community/shared';
+import {filterErrors, ValidationError, useToggleState, TextField, Section} from '@akeneo-pim-community/shared';
 import {Button, getColor, SectionTitle} from 'akeneo-design-system';
 
 const Container = styled.div`
@@ -73,54 +73,56 @@ const UnitDetails = ({
         onCancel={closeConfirmDeleteUnitModal}
       />
       <Container>
-        <SectionTitle sticky={0}>
-          <SectionTitle.Title>
-            {translate('measurements.unit.title', {unitLabel: getUnitLabel(selectedUnit, locale)})}
-          </SectionTitle.Title>
-        </SectionTitle>
-        <TextField
-          label={translate('pim_common.code')}
-          value={selectedUnit.code}
-          required={true}
-          readOnly={true}
-          errors={filterErrors(errors, '[code]')}
-        />
-        <TextField
-          label={translate('measurements.unit.symbol')}
-          value={selectedUnit.symbol}
-          readOnly={!isGranted('akeneo_measurements_measurement_unit_edit')}
-          onChange={value => onMeasurementFamilyChange(setUnitSymbol(measurementFamily, selectedUnit.code, value))}
-          errors={filterErrors(errors, '[symbol]')}
-        />
-        <OperationCollection
-          operations={selectedUnit.convert_from_standard}
-          readOnly={
-            !isGranted('akeneo_measurements_measurement_unit_edit') ||
-            measurementFamily.is_locked ||
-            selectedUnit.code === measurementFamily.standard_unit_code
-          }
-          onOperationsChange={(operations: Operation[]) =>
-            onMeasurementFamilyChange(setUnitOperations(measurementFamily, selectedUnit.code, operations))
-          }
-          errors={filterErrors(errors, '[convert_from_standard]')}
-        />
-        <SectionTitle sticky={0}>
-          <SectionTitle.Title>{translate('measurements.label_translations')}</SectionTitle.Title>
-        </SectionTitle>
-        {null !== locales &&
-          locales.map(locale => (
-            <TextField
-              label={locale.label}
-              key={locale.code}
-              locale={locale.code}
-              readOnly={!isGranted('akeneo_measurements_measurement_unit_edit')}
-              value={selectedUnit.labels[locale.code] || ''}
-              onChange={value =>
-                onMeasurementFamilyChange(setUnitLabel(measurementFamily, selectedUnitCode, locale.code, value))
-              }
-              errors={filterErrors(errors, `[labels][${locale.code}]`)}
-            />
-          ))}
+        <Section>
+          <SectionTitle sticky={0}>
+            <SectionTitle.Title>
+              {translate('measurements.unit.title', {unitLabel: getUnitLabel(selectedUnit, locale)})}
+            </SectionTitle.Title>
+          </SectionTitle>
+          <TextField
+            label={translate('pim_common.code')}
+            value={selectedUnit.code}
+            required={true}
+            readOnly={true}
+            errors={filterErrors(errors, '[code]')}
+          />
+          <TextField
+            label={translate('measurements.unit.symbol')}
+            value={selectedUnit.symbol}
+            readOnly={!isGranted('akeneo_measurements_measurement_unit_edit')}
+            onChange={value => onMeasurementFamilyChange(setUnitSymbol(measurementFamily, selectedUnit.code, value))}
+            errors={filterErrors(errors, '[symbol]')}
+          />
+          <OperationCollection
+            operations={selectedUnit.convert_from_standard}
+            readOnly={
+              !isGranted('akeneo_measurements_measurement_unit_edit') ||
+              measurementFamily.is_locked ||
+              selectedUnit.code === measurementFamily.standard_unit_code
+            }
+            onOperationsChange={(operations: Operation[]) =>
+              onMeasurementFamilyChange(setUnitOperations(measurementFamily, selectedUnit.code, operations))
+            }
+            errors={filterErrors(errors, '[convert_from_standard]')}
+          />
+          <SectionTitle sticky={0}>
+            <SectionTitle.Title>{translate('measurements.label_translations')}</SectionTitle.Title>
+          </SectionTitle>
+          {null !== locales &&
+            locales.map(locale => (
+              <TextField
+                label={locale.label}
+                key={locale.code}
+                locale={locale.code}
+                readOnly={!isGranted('akeneo_measurements_measurement_unit_edit')}
+                value={selectedUnit.labels[locale.code] || ''}
+                onChange={value =>
+                  onMeasurementFamilyChange(setUnitLabel(measurementFamily, selectedUnitCode, locale.code, value))
+                }
+                errors={filterErrors(errors, `[labels][${locale.code}]`)}
+              />
+            ))}
+        </Section>
         {isGranted('akeneo_measurements_measurement_unit_delete') &&
           !measurementFamily.is_locked &&
           selectedUnitCode !== measurementFamily.standard_unit_code && (
