bc476ef20f8e8bf98047e4aa8b5b85978774c147
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-03-02 11:12:55 +0100

    API-1455: with tests

--- src/Akeneo/Connectivity/Connection/back/Application/Webhook/Service/EventsApiDebugLogger.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Webhook/Service/EventsApiDebugLogger.php
@@ -15,7 +15,7 @@ use Akeneo\Platform\Component\EventQueue\EventInterface;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class EventsApiDebugLogger implements EventsApiDebugSendRequestErrorLogger
+class EventsApiDebugLogger implements EventsApiDebugResponseErrorLogger
 {
     private Clock $clock;
 
