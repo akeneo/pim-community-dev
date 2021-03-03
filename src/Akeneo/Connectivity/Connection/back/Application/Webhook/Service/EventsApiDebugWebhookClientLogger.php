9f30a511614a8da7fbcd675d3bbe2e298436953d
Thomas Galvaing <thomas.galvaing@akeneo.com>
2021-03-03 10:37:30 +0100

    Update src/Akeneo/Connectivity/Connection/back/Application/Webhook/Service/EventsApiDebugWebhookClientLogger.php
    Co-authored-by: Paul <pchasle@users.noreply.github.com>

--- src/Akeneo/Connectivity/Connection/back/Application/Webhook/Service/EventsApiDebugWebhookClientLogger.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Webhook/Service/EventsApiDebugWebhookClientLogger.php
@@ -10,7 +10,7 @@ use Akeneo\Connectivity\Connection\Domain\Webhook\Model\WebhookEvent;
  * @copyright 2020 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-interface EventsApiDebugWebhookClientLogger
+interface EventsApiRequestLogger
 {
     /**
      * @param array<WebhookEvent> $events
