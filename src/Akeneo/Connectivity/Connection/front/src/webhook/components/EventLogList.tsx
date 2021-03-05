4a3b5b98c9d8c2f47bd7045225e1d33a097b4e58
Paul Chasle <pchasle@users.noreply.github.com>
2021-03-05 19:12:53 +0100

    API-1491 display logs inside a table

--- src/Akeneo/Connectivity/Connection/front/src/webhook/components/EventLogList.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/webhook/components/EventLogList.tsx
@@ -1,4 +1,4 @@
-import {IconButton, RefreshIcon} from 'akeneo-design-system';
+import {ArrowRightIcon, Badge, Button, GraphIllustration, Information, RefreshIcon, Table} from 'akeneo-design-system';
 import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
 import {NoEventLogs} from './NoEventLogs';
 
@@ -47,7 +47,7 @@ const useFetchEventSubscriptionLogs = (connectionCode: string) => {
 
     const generator = useRef(fetchEventSubscriptionLogs(connectionCode, data.searchAfter));
     const fetchNextLogs = useCallback(() => {
-        const result = generator.current.next().value;
+        const result = generator.current.next().value; //= await fetchEventSubscriptionLogs(connectionCode, data.searchAfter)
         if (!result) {
             return;
         }
@@ -72,21 +72,47 @@ export const EventLogList: FC<{connectionCode: string}> = ({connectionCode}) => 
     if (logs.length > 0) {
         return (
             <>
-                Total: {total}
-                <ul>
-                    {logs.map(log => (
-                        <li key={log.id}>
-                            <code>{JSON.stringify(log)}</code>
-                        </li>
-                    ))}
-                </ul>
-                <IconButton
-                    icon={<RefreshIcon />}
-                    title='Load more!'
-                    level='tertiary'
-                    ghost='borderless'
-                    onClick={() => fetchNextLogs()}
-                />
+                <Information illustration={<GraphIllustration />} title={`There is ${total} logs.`}>
+                    {null}
+                </Information>
+                <Table>
+                    <Table.Header>
+                        <Table.HeaderCell></Table.HeaderCell>
+                        <Table.HeaderCell>Datetime</Table.HeaderCell>
+                        <Table.HeaderCell>Level</Table.HeaderCell>
+                        <Table.HeaderCell>Message</Table.HeaderCell>
+                    </Table.Header>
+                    <Table.Body>
+                        {logs.map(({id, timestamp, level, message}) => (
+                            <Table.Row key={id} onClick={() => undefined}>
+                                <Table.Cell>
+                                    <ArrowRightIcon />
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {new Intl.DateTimeFormat('en-US', {
+                                        year: 'numeric',
+                                        month: '2-digit',
+                                        day: '2-digit',
+                                    }).format(new Date(timestamp))}
+                                    <br />
+                                    {new Intl.DateTimeFormat('en-US', {
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                        second: '2-digit',
+                                    }).format(new Date(timestamp))}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <Badge level='warning'>{level.toUpperCase()}</Badge>
+                                </Table.Cell>
+                                <Table.Cell>{message}</Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+                <br />
+                <Button ghost level='tertiary' onClick={() => fetchNextLogs()}>
+                    <RefreshIcon /> Load more…
+                </Button>
             </>
         );
     }
