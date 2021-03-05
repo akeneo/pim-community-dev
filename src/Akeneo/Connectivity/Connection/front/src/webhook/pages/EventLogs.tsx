83dae8014df63e728f95ff735f3d5b16ae9f5631
Paul Chasle <pchasle@users.noreply.github.com>
2021-03-05 18:40:58 +0100

    API-1491 add infinite scroll

--- src/Akeneo/Connectivity/Connection/front/src/webhook/pages/EventLogs.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/webhook/pages/EventLogs.tsx
@@ -24,7 +24,7 @@ export const EventLogs: FC = () => {
                     <Loading />
                 ) : (
                     <EnabledEventSubscriptionGuard connectionCode={connection.code}>
-                        <EventLogList />
+                        <EventLogList connectionCode={connection.code} />
                     </EnabledEventSubscriptionGuard>
                 )}
             </PageContent>
