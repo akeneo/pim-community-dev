dcb968f433d8bd60244213864ba295d131d0441a
Michel Tag <micheltag@akeneo.com>
2021-03-09 16:46:06 +0100

    Preparing version 5.0.7 (#10511)
    Preparing version 5.0.7
    
    
    Co-authored-by: mr-prud  <gael.prudhomme@akeneo.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.6';
+    const VERSION = '5.0.7';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buttercup';
