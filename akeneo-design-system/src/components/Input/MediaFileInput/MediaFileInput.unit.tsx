91b96d06cc4921c34f40cebbd9c09a51739d4e07
Julien Sanchez <sumbobyboys@gmail.com>
2021-03-10 16:00:12 +0100

    RAC-483: refactor after discussions

--- akeneo-design-system/src/components/Input/MediaFileInput/MediaFileInput.unit.tsx
+++ akeneo-design-system/src/components/Input/MediaFileInput/MediaFileInput.unit.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {FileInput} from './FileInput';
+import {MediaFileInput} from './MediaFileInput';
 import {render, screen} from '../../storybook/test-util';
 
 test('it renders its children properly', () => {
-  render(<FileInput>FileInput content</FileInput>);
+  render(<MediaFileInput>MediaFileInput content</MediaFileInput>);
 
-  expect(screen.getByText('FileInput content')).toBeInTheDocument();
+  expect(screen.getByText('MediaFileInput content')).toBeInTheDocument();
 });
 
 // Those tests should pass directly if you follow the contributing guide.
 // If you add required props to your Component, these tests will fail
 // and you will need to add these required props here as well
-test('FileInput supports forwardRef', () => {
+test('MediaFileInput supports forwardRef', () => {
   const ref = {current: null};
 
-  render(<FileInput ref={ref} />);
+  render(<MediaFileInput ref={ref} />);
   expect(ref.current).not.toBe(null);
 });
 
-test('FileInput supports ...rest props', () => {
-  render(<FileInput data-testid="my_value" />);
+test('MediaFileInput supports ...rest props', () => {
+  render(<MediaFileInput data-testid="my_value" />);
   expect(screen.getByTestId('my_value')).toBeInTheDocument();
 });
