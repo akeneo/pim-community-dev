17cb22f690ebdd1b198a9593d581e5f5819e5b18
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-03-12 18:02:26 +0100

    CPM-155: add job message ui, import, export and backend

--- src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionMessage.php
+++ src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionMessage.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Component\BatchQueue\Queue;
 
+use Webmozart\Assert\Assert;
+
 /**
+ * @TODO: CPM-156: make it abstract
+ *
  * Object representing the message pushed into a queue to process a job execution asynchronously.
  *
  * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class JobExecutionMessage
+class JobExecutionMessage implements JobExecutionMessageInterface
 {
     /** @var int */
     private $id;
@@ -67,7 +71,7 @@ class JobExecutionMessage
     {
         $createTime = new \DateTime('now', new \DateTimeZone('UTC'));
 
-        return new self(null, $jobExecutionId, null, $createTime, null, $options);
+        return new static(null, $jobExecutionId, null, $createTime, null, $options);
     }
 
     /**
@@ -90,19 +94,19 @@ class JobExecutionMessage
         ?\DateTime $updatedTime,
         array $options
     ): JobExecutionMessage {
-        return new self($id, $jobExecutionId, $consumer, $createTime, $updatedTime, $options);
+        return new static($id, $jobExecutionId, $consumer, $createTime, $updatedTime, $options);
     }
 
-    public static function createJobExecutionMessageFromNormalized(
-        // @TODO CPM-152: replace id by a uuid? The id is only used for old queue.
-        ?int $id,
-        int $jobExecutionId,
-        ?string $consumer,
-        \DateTime $createTime,
-        ?\DateTime $updatedTime,
-        array $options
-    ): JobExecutionMessage {
-        return new self($id, $jobExecutionId, $consumer, $createTime, $updatedTime, $options);
+    public static function createJobExecutionMessageFromNormalized(array $normalized): JobExecutionMessageInterface
+    {
+        return new static(
+            $normalized['id'], // @TODO CPM-156: replace id by a uuid? The id is only used for old queue.
+            $normalized['job_execution_id'],
+            $normalized['consumer'],
+            new \DateTime($normalized['created_time']),
+            null !== $normalized['updated_time'] ? new \DateTime($normalized['updated_time']) : null,
+            $normalized['options']
+        );
     }
 
     /**
@@ -134,11 +138,9 @@ class JobExecutionMessage
      *
      * @return JobExecutionMessage
      */
-    public function consumedBy(string $consumer): JobExecutionMessage
+    public function consumedBy(string $consumer): void
     {
         $this->consumer = $consumer;
-
-        return $this;
     }
 
     /**
