ca13614b0a3910c983472b6b89d1eafcb14f776d
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-03-12 18:02:26 +0100

    CPM-155: add job message ui, import, export and backend

--- src/Akeneo/Tool/Component/BatchQueue/spec/Normalizer/JobExecutionMessageNormalizerSpec.php
+++ src/Akeneo/Tool/Component/BatchQueue/spec/Normalizer/JobExecutionMessageNormalizerSpec.php
@@ -3,13 +3,22 @@ declare(strict_types=1);
 
 namespace spec\Akeneo\Tool\Component\BatchQueue\Normalizer;
 
-use Akeneo\Tool\Component\BatchQueue\Queue\JobExecutionMessage;
+use Akeneo\Tool\Component\BatchQueue\Factory\JobExecutionMessageFactory;
+use Akeneo\Tool\Component\BatchQueue\Queue\BackendJobExecutionMessage;
+use Akeneo\Tool\Component\BatchQueue\Queue\ExportJobExecutionMessage;
+use Akeneo\Tool\Component\BatchQueue\Queue\ImportJobExecutionMessage;
+use Akeneo\Tool\Component\BatchQueue\Queue\UiJobExecutionMessage;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class JobExecutionMessageNormalizerSpec extends ObjectBehavior
 {
+    function let(JobExecutionMessageFactory $jobExecutionMessageFactory)
+    {
+        $this->beConstructedWith($jobExecutionMessageFactory);
+    }
+
     function it_is_a_normalizer_and_a_denormalizer()
     {
         $this->shouldImplement(NormalizerInterface::class);
@@ -18,16 +27,24 @@ class JobExecutionMessageNormalizerSpec extends ObjectBehavior
 
     function it_supports_job_messenger_normalization_only()
     {
-        $jobMessenger = JobExecutionMessage::createJobExecutionMessage(1, []);
-
+        $jobMessenger = UiJobExecutionMessage::createJobExecutionMessage(1, []);
         $this->supportsNormalization($jobMessenger, '')->shouldBe(true);
+
+        $jobMessenger = ImportJobExecutionMessage::createJobExecutionMessage(1, []);
+        $this->supportsNormalization($jobMessenger, '')->shouldBe(true);
+
+        $jobMessenger = ExportJobExecutionMessage::createJobExecutionMessage(1, []);
+        $this->supportsNormalization($jobMessenger, '')->shouldBe(true);
+
+        $jobMessenger = BackendJobExecutionMessage::createJobExecutionMessage(1, []);
+        $this->supportsNormalization($jobMessenger, '')->shouldBe(true);
+
         $this->supportsNormalization(new \StdClass(), '')->shouldBe(false);
     }
 
     function it_normalizes_a_simple_job_messenger()
     {
-        $createdAt = new \DateTime('2020-01-01');
-        $jobMessenger = JobExecutionMessage::createJobExecutionMessage(
+        $jobMessenger = UiJobExecutionMessage::createJobExecutionMessage(
             1,
             ['option1' => 'value1']
         );
@@ -46,7 +63,7 @@ class JobExecutionMessageNormalizerSpec extends ObjectBehavior
     {
         $createdAt = new \DateTime('2020-01-01');
         $updatedAt = new \DateTime('2020-02-01');
-        $jobMessenger = JobExecutionMessage::createJobExecutionMessageFromDatabase(
+        $jobMessenger = ImportJobExecutionMessage::createJobExecutionMessageFromDatabase(
             1,
             2,
             'consumer',
@@ -67,49 +84,19 @@ class JobExecutionMessageNormalizerSpec extends ObjectBehavior
 
     function it_supports_job_messenger_denormalization_only()
     {
-        $this->supportsDenormalization([], JobExecutionMessage::class)->shouldBe(true);
+        $this->supportsDenormalization([], UiJobExecutionMessage::class)->shouldBe(true);
+        $this->supportsDenormalization([], ImportJobExecutionMessage::class)->shouldBe(true);
+        $this->supportsDenormalization([], ExportJobExecutionMessage::class)->shouldBe(true);
+        $this->supportsDenormalization([], BackendJobExecutionMessage::class)->shouldBe(true);
         $this->supportsDenormalization([], 'Unknown')->shouldBe(false);
     }
 
-    function it_denormalizes_a_simple_job_messenger()
+    function it_denormalizes_a_job_messenger(JobExecutionMessageFactory $jobExecutionMessageFactory)
     {
-        $normalized = [
-            'id' => null,
-            'job_execution_id' => 1,
-            'consumer' => null,
-            'created_time' => '2021-03-08T15:37:23+01:00',
-            'updated_time' => null,
-            'options' => ['option1' => 'value1'],
-        ];
+        $message = UiJobExecutionMessage::createJobExecutionMessage(1, []);
+        $normalized = ['test'];
+        $jobExecutionMessageFactory->buildFromNormalized($normalized)->willReturn($message);
 
-        $jobMessenger = $this->denormalize($normalized, JobExecutionMessage::class);
-        $jobMessenger->shouldBeAnInstanceOf(JobExecutionMessage::class);
-        $jobMessenger->getId()->shouldBeNull();
-        $jobMessenger->getJobExecutionId()->shouldBe(1);
-        $jobMessenger->getConsumer()->shouldBeNull();
-        $jobMessenger->getCreateTime()->shouldBeLike(new \DateTime('2021-03-08T15:37:23+01:00'));
-        $jobMessenger->getUpdatedTime()->shouldBeNull();
-        $jobMessenger->getOptions()->shouldBe(['option1' => 'value1']);
-    }
-
-    function it_denormalizes_a_full_job_messenger()
-    {
-        $normalized = [
-            'id' => 1,
-            'job_execution_id' => 2,
-            'consumer' => 'consumer_name',
-            'created_time' => '2021-03-08T15:37:23+01:00',
-            'updated_time' => '2021-03-08T15:58:12+01:00',
-            'options' => ['option1' => 'value1'],
-        ];
-
-        $jobMessenger = $this->denormalize($normalized, JobExecutionMessage::class);
-        $jobMessenger->shouldBeAnInstanceOf(JobExecutionMessage::class);
-        $jobMessenger->getId()->shouldBe(1);
-        $jobMessenger->getJobExecutionId()->shouldBe(2);
-        $jobMessenger->getConsumer()->shouldBe('consumer_name');
-        $jobMessenger->getCreateTime()->shouldBeLike(new \DateTime('2021-03-08T15:37:23+01:00'));
-        $jobMessenger->getUpdatedTime()->shouldBeLike(new \DateTime('2021-03-08T15:58:12+01:00'));
-        $jobMessenger->getOptions()->shouldBe(['option1' => 'value1']);
+        $this->denormalize($normalized, UiJobExecutionMessage::class)->shouldBe($message);
     }
 }
