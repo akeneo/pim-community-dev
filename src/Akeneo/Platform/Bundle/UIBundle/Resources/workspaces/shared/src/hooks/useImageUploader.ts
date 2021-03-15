df5eb2ad5388888d5cf253940cae9f1ea97f4951
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-03-15 11:55:03 +0100

    RAC-483: Add useImageUploader unit test

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/hooks/useImageUploader.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/hooks/useImageUploader.ts
@@ -5,25 +5,27 @@ import {useCallback} from 'react';
 const useImageUploader = (uploadRoute: string) => {
   const router = useRouter();
 
-  return useCallback((file: File, onProgress: (ratio: number) => void): Promise<FileInfo> => {
-    return new Promise<FileInfo>((resolve, reject) => {
-      const formData = new FormData();
-      formData.append('file', file);
+  return useCallback(
+    (file: File, onProgress: (ratio: number) => void): Promise<FileInfo> =>
+      new Promise<FileInfo>((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('file', file);
 
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', router.generate(uploadRoute), true);
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      xhr.upload.addEventListener('progress', event => onProgress(event.loaded / event.total), false);
-      xhr.addEventListener('load', () => {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.response));
-        } else {
-          reject(xhr.response || []);
-        }
-      });
-      xhr.send(formData);
-    });
-  }, [router, uploadRoute]);
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', router.generate(uploadRoute), true);
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.upload.addEventListener('progress', event => onProgress(event.loaded / event.total), false);
+        xhr.addEventListener('load', () => {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response || []);
+          }
+        });
+        xhr.send(formData);
+      }),
+    [router, uploadRoute]
+  );
 };
 
 export {useImageUploader};
