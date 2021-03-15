142c7c42d9a4610d6a93cdabba7a89612e862e5e
Steven VAIDIE <steven.vaidie@gmail.com>
2021-03-15 11:31:02 +0100

    RAC-518: Add unit tests

--- akeneo-design-system/src/components/List/List.unit.tsx
+++ akeneo-design-system/src/components/List/List.unit.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import {List} from './List';
 import {render, screen} from '../../storybook/test-util';
+import {Helper} from '../Helper/Helper';
+import {IconButton} from '../IconButton/IconButton';
+import {Button} from '../Button/Button';
+import {CloseIcon} from '../../icons';
 
 test('it renders its children properly', () => {
-  render(<List>List content</List>);
+  render(
+    <List>
+      <List.Row>
+        <List.TitleCell width="auto">
+          A text
+        </List.TitleCell>
+        <List.Cell width={150}>
+          An information
+        </List.Cell>
+        <List.ActionCell>
+          <Button>First action</Button>
+          <Button>Second action</Button>
+          Not a button
+        </List.ActionCell>
+        <List.RemoveCell>
+          <IconButton ghost="borderless" level="tertiary" icon={<CloseIcon />} title="Remove first item" />
+        </List.RemoveCell>
+      </List.Row>
+      <List.Row>
+        <List.TitleCell width="auto">
+          Another text
+        </List.TitleCell>
+        <List.Cell width={150}>
+          Another information
+        </List.Cell>
+        <List.ActionCell>
+          <Button>Another action</Button>
+        </List.ActionCell>
+        <List.RemoveCell>
+          <IconButton ghost="borderless" level="tertiary" icon={<CloseIcon />} title="Remove second item" />
+        </List.RemoveCell>
+        <List.RowHelpers>
+          <Helper level="info">An helper</Helper>
+        </List.RowHelpers>
+      </List.Row>
+    </List>
+  );
 
-  expect(screen.getByText('List content')).toBeInTheDocument();
-});
-
-// Those tests should pass directly if you follow the contributing guide.
-// If you add required props to your Component, these tests will fail
-// and you will need to add these required props here as well
-test('List supports forwardRef', () => {
-  const ref = {current: null};
-
-  render(<List ref={ref} />);
-  expect(ref.current).not.toBe(null);
+  expect(screen.getByText('A text')).toBeInTheDocument();
+  expect(screen.getByText('An information')).toBeInTheDocument();
+  expect(screen.getByText('First action')).toBeInTheDocument();
+  expect(screen.getByText('Second action')).toBeInTheDocument();
+  expect(screen.getByText('Not a button')).toBeInTheDocument();
+  expect(screen.getByTitle('Remove first item')).toBeInTheDocument();
+  expect(screen.getByText('Another text')).toBeInTheDocument();
+  expect(screen.getByText('Another information')).toBeInTheDocument();
+  expect(screen.getByText('Another action')).toBeInTheDocument();
+  expect(screen.getByText('An helper')).toBeInTheDocument();
+  expect(screen.getByTitle('Remove first item')).toBeInTheDocument();
 });
 
 test('List supports ...rest props', () => {
