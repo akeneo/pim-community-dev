a5ecb1c9b0a7bd18c7d62fc1bd8c911c05bfebc3
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-03-15 17:19:40 +0100

    PLG-89: display the last operations widget on the dashboard

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/activity/src/components/DashboardIndex.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/activity/src/components/DashboardIndex.tsx
@@ -3,15 +3,20 @@ import {PageContent} from '@akeneo-pim-community/shared';
 import {Header} from './Header';
 import {CompletenessWidget} from './Widgets/CompletenessWidget';
 import {LastOperationsWidget} from './Widgets/LastOperationsWidget';
+import styled from 'styled-components';
+
+const StyledPageContent = styled(PageContent)`
+  height: calc(100vh - 240px);
+`;
 
 const DashboardIndex = () => {
   return (
     <>
       <Header />
-      <PageContent>
+      <StyledPageContent>
         <CompletenessWidget />
         <LastOperationsWidget />
-      </PageContent>
+      </StyledPageContent>
     </>
   );
 };
