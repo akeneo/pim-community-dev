906ae2965c528cf2433caa238a95b645ead69d40
Steven VAIDIE <steven.vaidie@gmail.com>
2021-03-18 13:56:30 +0100

    RAC-483: Add isUploading function to prevent closing while file is uploading

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/hooks/useImageUploader.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/hooks/useImageUploader.ts
@@ -1,21 +1,28 @@
 import {FileInfo} from 'akeneo-design-system';
 import {useRouter} from '@akeneo-pim-community/legacy-bridge';
-import {useCallback} from 'react';
+import {useCallback, useState} from 'react';
 
 const useImageUploader = (uploadRoute: string) => {
   const router = useRouter();
+  const [uploadCount, setUploadCount] = useState<number>(0);
+  const incrementUploadCount = () => setUploadCount(oldCount => oldCount + 1);
+  const decrementUploadCount = () => setUploadCount(oldCount => oldCount - 1);
+  const isUploading = () => uploadCount !== 0;
 
-  return useCallback(
+  const uploader = useCallback(
     (file: File, onProgress: (ratio: number) => void): Promise<FileInfo> =>
       new Promise<FileInfo>((resolve, reject) => {
         const formData = new FormData();
         formData.append('file', file);
+        incrementUploadCount();
 
         const xhr = new XMLHttpRequest();
         xhr.open('POST', router.generate(uploadRoute), true);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.upload.addEventListener('progress', event => onProgress(event.loaded / event.total), false);
         xhr.addEventListener('load', () => {
+          decrementUploadCount();
+
           if (xhr.status === 200) {
             resolve(JSON.parse(xhr.response));
           } else {
@@ -26,6 +33,8 @@ const useImageUploader = (uploadRoute: string) => {
       }),
     [router, uploadRoute]
   );
+
+  return [isUploading, uploader] as const;
 };
 
 export {useImageUploader};
--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/tests/front/unit/hooks/useImageUploader.unit.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/tests/front/unit/hooks/useImageUploader.unit.ts
@@ -1,3 +1,4 @@
+import {act} from '@testing-library/react-hooks';
 import {useImageUploader} from '../../../../src/hooks/useImageUploader';
 import {renderHookWithProviders} from '../utils';
 
@@ -18,23 +19,36 @@ const xhrMock: Partial<XMLHttpRequest> = {
   addEventListener: jest.fn((_eventName, callback: EventListener) => callback(new Event('load'))),
 };
 
+const timeoutXhrMock: Partial<XMLHttpRequest> = {
+  open: jest.fn(),
+  send: jest.fn(() => setTimeout(() => this.onreadystatechange(), 10000)),
+  setRequestHeader: jest.fn(),
+  readyState: 4,
+  status: 200,
+  response: JSON.stringify(fileInfo),
+  upload: {addEventListener: jest.fn((_eventName, callback) => callback({loaded: 50, total: 100}))} as any,
+  addEventListener: jest.fn((_eventName, callback: EventListener) => (this.onreadystatechange = callback)),
+};
+
 jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest);
 
 test('It returns an image uploader that can upload a file', async () => {
   const {result} = renderHookWithProviders(() => useImageUploader('fake_route'));
 
-  const uploader = result.current;
+  const [,uploader] = result.current;
   const onProgress = jest.fn();
 
-  const uploadedFile = await uploader(imageFile, onProgress);
+  await act(async () => {
+    const uploadedFile = await uploader(imageFile, onProgress);
 
-  expect(xhrMock.open).toBeCalledWith('POST', 'fake_route', true);
-  expect(xhrMock.setRequestHeader).toBeCalledWith('X-Requested-With', 'XMLHttpRequest');
-  expect(onProgress).toBeCalledWith(0.5);
-  expect(uploadedFile).toEqual(fileInfo);
+    expect(xhrMock.open).toBeCalledWith('POST', 'fake_route', true);
+    expect(xhrMock.setRequestHeader).toBeCalledWith('X-Requested-With', 'XMLHttpRequest');
+    expect(onProgress).toBeCalledWith(0.5);
+    expect(uploadedFile).toEqual(fileInfo);
+  });
 });
 
-test('It returns an image uploader that can handle failure', () => {
+test('It returns an image uploader that can handle failure', async () => {
   jest.spyOn(window, 'XMLHttpRequest').mockImplementationOnce(
     () =>
       ({
@@ -46,10 +60,37 @@ test('It returns an image uploader that can handle failure', () => {
 
   const {result} = renderHookWithProviders(() => useImageUploader('fake_route'));
 
-  const uploader = result.current;
+  const [,uploader] = result.current;
+  const onProgress = jest.fn();
+  await expect(async () => {
+    await act(async () => {
+      await uploader(imageFile, onProgress);
+    });
+  }).rejects.toBe('Internal server error');
+});
+
+test('It returns if an upload is in progress', async () => {
+  jest.spyOn(window, 'XMLHttpRequest').mockImplementationOnce(() => timeoutXhrMock as XMLHttpRequest);
+
+  jest.useFakeTimers();
+  const {result} = renderHookWithProviders(() => useImageUploader('fake_route'));
+
+  let [isUploading, uploader] = result.current;
   const onProgress = jest.fn();
 
-  expect(async () => {
-    await uploader(imageFile, onProgress);
-  }).rejects.toBe('Internal server error');
+  expect(isUploading()).toEqual(false);
+
+  void act(() => {
+    uploader(imageFile, onProgress);
+  });
+
+  [isUploading] = result.current;
+  expect(isUploading()).toEqual(true);
+
+  await act(() => {
+    jest.runAllTimers();
+  });
+
+  [isUploading] = result.current;
+  expect(isUploading()).toEqual(false);
 });
