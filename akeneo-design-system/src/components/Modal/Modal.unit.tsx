21e26aeb664e48bf588a7561b2a0a1e93bb88cc1
Julien Sanchez <sumbobyboys@gmail.com>
2021-03-18 17:07:53 +0100

    RAC-483: add modal provider and hook to know if we are inside a modal

--- akeneo-design-system/src/components/Modal/Modal.unit.tsx
+++ akeneo-design-system/src/components/Modal/Modal.unit.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Modal} from './Modal';
+import {Modal, useInModal} from './Modal';
 import {fireEvent, render, screen} from '../../storybook/test-util';
 
 test('it renders its children properly', () => {
@@ -49,4 +49,35 @@ test('it calls the onClose handler when hitting the Escape key', () => {
   fireEvent.keyDown(document, {key: 'Escape', code: 'Escape'});
 
   expect(onClose).toBeCalledTimes(1);
+});
+
+const Component = () => {
+  const isInModal = useInModal();
+  return <span onClick={() => {}}>An element {isInModal ? 'inside' : 'outside'} of a modal</span>;
+};
+
+test('it provides a hook to know if an element is inside a modal', () => {
+  const onClose = jest.fn();
+  render(
+    <Modal closeTitle="Close" onClose={onClose}>
+      <Component />
+    </Modal>
+  );
+
+  expect(screen.getByText('An element inside of a modal')).toBeInTheDocument();
+  expect(screen.queryByText('An element outside of a modal')).not.toBeInTheDocument();
+});
+
+test('it provides a hook to know if an element is outside a modal', () => {
+  const onClose = jest.fn();
+  render(
+    <>
+      <Modal closeTitle="Close" onClose={onClose}></Modal>
+
+      <Component />
+    </>
+  );
+
+  expect(screen.queryByText('An element inside of a modal')).not.toBeInTheDocument();
+  expect(screen.getByText('An element outside of a modal')).toBeInTheDocument();
 });
