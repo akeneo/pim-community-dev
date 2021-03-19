e7aba24f1d89695210ea04dd577c33836a81253e
Gaël Prudhomme <gael@mrprud.me>
2021-03-19 09:41:42 +0100

    remove oldest files for 3.2 and add 5.0 related script

--- std-build/migration/40_to_50/prepare_40_to_50.sh
+++ std-build/migration/40_to_50/prepare_40_to_50.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-#
-# Script to be launched from the standard distribution to add missing files
-# add installation files.
-#
-# This script is only meant for installation from scratch, not for migration
-#
 
 set -e
 
 DEV_DISTRIB_DIR=$(dirname $0)/../..
 STANDARD_DISTRIB_DIR=./
-
-echo "Preparing Akeneo PIM 4.0 file structure..."
-
-# Required directories
-mkdir -p $STANDARD_DISTRIB_DIR/src \
-         $STANDARD_DISTRIB_DIR/bin \
-         $STANDARD_DISTRIB_DIR/public \
-         $STANDARD_DISTRIB_DIR/config/packages/dev \
-         $STANDARD_DISTRIB_DIR/config/packages/prod \
-         $STANDARD_DISTRIB_DIR/config/services \
 
 # We use the same bootstrap.php to load .env file on standard as on CE-dev
 cp $DEV_DISTRIB_DIR/config/bootstrap.php $STANDARD_DISTRIB_DIR/config/
@@ -28,8 +12,10 @@ cp $DEV_DISTRIB_DIR/config/bootstrap.php $STANDARD_DISTRIB_DIR/config/
 cp $DEV_DISTRIB_DIR/config/packages/security.yml $STANDARD_DISTRIB_DIR/config/packages/security.yml
 
 # Partners are most likely to develop and deploy using local filesystem, not MinIO
-cp $DEV_DISTRIB_DIR/std-build/migration/32_to_40/oneup_flysystem.yml $STANDARD_DISTRIB_DIR/config/packages/dev/
-cp $DEV_DISTRIB_DIR/std-build/migration/32_to_40/oneup_flysystem.yml $STANDARD_DISTRIB_DIR/config/packages/prod/
+cp -r $DEV_DISTRIB_DIR/config/packages/dev $STANDARD_DISTRIB_DIR/config/packages/
+
+# Both production server mode must be available in std
+cp -r $DEV_DISTRIB_DIR/config/packages/prod_* $STANDARD_DISTRIB_DIR/config/packages/
 
 # We need a console and FPM entrypoint
 cp $DEV_DISTRIB_DIR/bin/console $STANDARD_DISTRIB_DIR/bin/
@@ -39,8 +25,11 @@ cp $DEV_DISTRIB_DIR/public/* $STANDARD_DISTRIB_DIR/public/
 # We provide a kernel that loads configuration from the CE dev and override it with the one in standard
 cp $DEV_DISTRIB_DIR/std-build/Kernel.php $STANDARD_DISTRIB_DIR/src
 
-# This is a skeleton file to encourage them to put their bundles inside it
-cp $DEV_DISTRIB_DIR/std-build/bundles.php $STANDARD_DISTRIB_DIR/config
+# Copy STD configuration
+# Uses same docker compose than the CE
+cp $DEV_DISTRIB_DIR/docker-compose.yml $STANDARD_DISTRIB_DIR/docker-compose.yml
+# Usable example Makefile
+cp $DEV_DISTRIB_DIR/std-build/Makefile $STANDARD_DISTRIB_DIR/Makefile
 
 # Front dependencies using workspace to depends on CE-dev and inherits its deps
 cp $DEV_DISTRIB_DIR/std-build/package.json $STANDARD_DISTRIB_DIR/package.json
@@ -52,13 +41,13 @@ cp $DEV_DISTRIB_DIR/std-build/tsconfig.json $STANDARD_DISTRIB_DIR/tsconfig.json
 # Needed to define Elasticsearch mapping file location inside CE-dev
 cp $DEV_DISTRIB_DIR/std-build/services.yml $STANDARD_DISTRIB_DIR/config/services/
 
-# Skeleton .env file
+# We use the same bootstrap.php to load .env file on standard as on CE-dev
 cp $DEV_DISTRIB_DIR/.env $STANDARD_DISTRIB_DIR/
 
 # Prepare database upgrades to run
 mkdir -p $STANDARD_DISTRIB_DIR/upgrades/
 cp -R $DEV_DISTRIB_DIR/upgrades/* $STANDARD_DISTRIB_DIR/upgrades/
 
-cp $DEV_DISTRIB_DIR/std-build/migration/32_to_40/rector.yaml $STANDARD_DISTRIB_DIR/
+cp $DEV_DISTRIB_DIR/std-build/upgrade/40_to_50/* $STANDARD_DISTRIB_DIR/
 
-echo "Done."
+printf "Done. \n"
