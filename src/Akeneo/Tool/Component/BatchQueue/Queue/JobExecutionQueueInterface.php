15678392b524a9a8c27be5f2e65f8c99c24ff789
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-03-19 15:04:40 +0100

    CPM-155: add todo

--- src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionQueueInterface.php
+++ src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionQueueInterface.php
@@ -21,6 +21,7 @@ interface JobExecutionQueueInterface
     public function publish(JobExecutionMessageInterface $jobExecutionMessage): void;
 
     /**
+     * @TODO CPM-152: to remove
      * Gets the last job execution message from the queue, that is not consumed yet.
      */
     public function consume(string $consumer, JobQueueConsumerConfiguration $configuration): ?JobExecutionMessageInterface;
