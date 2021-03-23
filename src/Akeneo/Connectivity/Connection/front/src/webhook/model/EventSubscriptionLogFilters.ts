56b0edbe26072e20a56a52ac09f05a69421d2673
Tseho <quentin.favrie@gmail.com>
2021-03-24 00:32:32 +0100

    API-1494: Use isEqual from lodash to better support

--- src/Akeneo/Connectivity/Connection/front/src/webhook/model/EventSubscriptionLogFilters.ts
+++ src/Akeneo/Connectivity/Connection/front/src/webhook/model/EventSubscriptionLogFilters.ts
@@ -1,4 +1,5 @@
 import {EventSubscriptionLogLevel} from './EventSubscriptionLogLevel';
+import {isEqual} from 'lodash';
 
 export type EventSubscriptionLogFilters = {
     levels: EventSubscriptionLogLevel[],
@@ -13,9 +14,6 @@ export const DEFAULT_EVENT_SUBSCRIPTION_LOG_FILTERS = {
     ],
 };
 
-// This is calculated only once, to avoid unecessary computing when comparing it.
-const DEFAULT_EVENT_SUBSCRIPTION_LOG_FILTERS_JSON = JSON.stringify(DEFAULT_EVENT_SUBSCRIPTION_LOG_FILTERS);
-
 export const isSameAsDefaultFiltersValues = (filters: EventSubscriptionLogFilters): boolean => {
-    return JSON.stringify(filters) === DEFAULT_EVENT_SUBSCRIPTION_LOG_FILTERS_JSON;
+    return isEqual(filters, DEFAULT_EVENT_SUBSCRIPTION_LOG_FILTERS);
 };
