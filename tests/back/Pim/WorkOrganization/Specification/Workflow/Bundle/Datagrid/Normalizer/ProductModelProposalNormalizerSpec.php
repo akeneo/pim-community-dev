0f963afcfc653b91867bc2a96c96ef59b842bbce
pierallard <pierallard@gmail.com>
2021-03-23 08:56:42 +0100

    CPM-157: Fix phpspec

--- tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/Datagrid/Normalizer/ProductModelProposalNormalizerSpec.php
+++ tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/Datagrid/Normalizer/ProductModelProposalNormalizerSpec.php
@@ -3,6 +3,7 @@
 namespace Specification\Akeneo\Pim\WorkOrganization\Workflow\Bundle\Datagrid\Normalizer;
 
 use Akeneo\Pim\Enrichment\Component\Product\Factory\ValueFactory;
+use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductModel;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductModelInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ValueInterface;
@@ -10,7 +11,9 @@ use Akeneo\Pim\Enrichment\Component\Product\Model\WriteValueCollection;
 use Akeneo\Pim\Enrichment\Component\Product\Value\ScalarValue;
 use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\Attribute;
 use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\GetAttributes;
+use Akeneo\Pim\WorkOrganization\Workflow\Bundle\Datagrid\Normalizer\ProposalChangesNormalizer;
 use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\EntityWithValuesDraftInterface;
+use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\ProductDraft;
 use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\ProductModelDraft;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -19,12 +22,10 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 class ProductModelProposalNormalizerSpec extends ObjectBehavior
 {
     function let(
-        NormalizerInterface $standardNormalizer,
         NormalizerInterface $datagridNormalizer,
-        ValueFactory $valueFactory,
-        GetAttributes $getAttributesQuery
+        ProposalChangesNormalizer $changesNormalizer
     ) {
-        $this->beConstructedWith($standardNormalizer, $datagridNormalizer, $valueFactory, $getAttributesQuery);
+        $this->beConstructedWith($datagridNormalizer, $changesNormalizer);
     }
 
     function it_should_implement()
@@ -32,166 +33,49 @@ class ProductModelProposalNormalizerSpec extends ObjectBehavior
         $this->shouldImplement(NormalizerInterface::class);
     }
 
-    function it_supports_product_proposal_normalization(ProductModelDraft $productModelProposal)
-    {
+    function it_supports_product_proposal_normalization(
+        ProductModelDraft $productModelProposal
+    ) {
         $this->supportsNormalization($productModelProposal, 'datagrid')->shouldReturn(true);
+        $this->supportsNormalization('Foobar', 'datagrid')->shouldReturn(false);
+
     }
 
     function it_normalizes_a_product_model_draft(
-        $standardNormalizer,
-        $datagridNormalizer,
-        ValueFactory $valueFactory,
-        GetAttributes $getAttributesQuery,
-        ProductModelDraft $productModelProposal,
-        WriteValueCollection $valueCollection,
-        ProductModelInterface $productModel,
-        ValueInterface $value
-    ) {
-        $context = [
-            'filter_types' => ['pim.transform.product_value.structured'],
-            'locales'      => ['en_US'],
-            'channels'     => ['ecommerce'],
-            'data_locale'  => 'en_US',
-        ];
-
-        $created = new \DateTime('2017-01-01T01:03:34+01:00');
-        $changes = [
-            'values' => [
-                'text' => [['locale' => null, 'scope' => null, 'data' => 'my text']]
-            ]
-        ];
-        $datagridNormalizer->normalize($created, 'datagrid', $context)->willReturn('2017-01-01');
-
-        $productModelProposal->getReviewStatusForChange('text', null, null)->willReturn('to_review');
-        $productModelProposal->getId()->willReturn(1);
-        $productModelProposal->getAuthor()->willReturn('Mary');
-        $productModelProposal->getAuthorLabel()->willReturn('Mary Smith');
-        $productModelProposal->getSource()->willReturn('pim');
-        $productModelProposal->getSourceLabel()->willReturn('PIM');
-        $productModelProposal->getStatus()->willReturn(1);
-        $productModelProposal->getEntityWithValue()->willReturn($productModel);
-        $productModelProposal->getCreatedAt()->willReturn($created);
-        $productModelProposal->getValues()->willReturn($valueCollection);
-
-        $productModelProposal->getChanges()->willReturn($changes);
-        $attribute = new Attribute('text', 'pim_catalog_text', [], false, false, null, null, true, 'pim_catalog_text', []);
-        $getAttributesQuery->forCode('text')->willReturn($attribute);
-        $valueFactory->createByCheckingData($attribute, null, null, 'my text')->willReturn($value);
-        $value->getAttributeCode()->willReturn('text');
-        $value->getScopeCode()->willReturn(null);
-        $value->getLocaleCode()->willReturn(null);
-        $valueCollection = new WriteValueCollection();
-        $valueCollection->add($value->getWrappedObject());
-        $standardNormalizer->normalize(
-            $valueCollection,
-            'standard',
-            $context
-        )->willReturn(
-            [
-                'text' => [
-                    [
-                        'locale' => null,
-                        'scope'  => null,
-                        'data'   => 'my text',
-                    ],
-                ]
-            ]
-        );
-
-        $productModel->getCode()->willReturn('fake-spec-model');
-
-        $this->normalize($productModelProposal, 'datagrid', $context)->shouldReturn(
-            [
-                'changes' => [
-                    'text' => [
-                        [
-                            'locale' => null,
-                            'scope'  => null,
-                            'data'   => 'my text',
-                        ],
-                    ],
-                ],
-                'createdAt' => '2017-01-01',
-                'product' => $productModel,
-                'author' => 'Mary',
-                'author_label' => 'Mary Smith',
-                'source' => 'pim',
-                'source_label' => 'PIM',
-                'status' => 1,
-                'proposal' => $productModelProposal,
-                'search_id' => 'fake-spec-model',
-                'id' => 'product_model_draft_1',
-                'document_type' => 'product_model_draft',
-                'proposal_id' => 1,
-            ]
-        );
-    }
-
-    function it_handles_empty_values_correctly(
-        NormalizerInterface $standardNormalizer,
         NormalizerInterface $datagridNormalizer,
-        ValueFactory $valueFactory,
-        GetAttributes $getAttributesQuery,
-        ProductModelDraft $productModelDraft
+        ProposalChangesNormalizer $changesNormalizer,
+        ProductModelDraft $productModelDraft,
+        ProductModelInterface $productModel
     ) {
-        $originalPM = new ProductModel();
-        $originalPM->setCode('original');
+        $context = ['locales' => ['en_US']];
+        $changes = [
+            'text' => [
+                ['locale' => 'en_US', 'scope' => null, 'before' => 'beforevalue', 'after' => 'aftervalue'],
+                ['locale' => 'fr_FR', 'scope' => null, 'before' => 'avantvalue', 'after' => 'apresvalue'],
+            ]
+        ];
+        $created = new \DateTime('2017-01-01T01:03:34+01:00');
 
-        $createdAt = \DateTime::createFromFormat('Y-m-d', '2020-05-07');
-        $datagridNormalizer->normalize($createdAt, 'datagrid', [])->willReturn('2020-05-07');
-
-        $productModelDraft->getReviewStatusForChange('name', 'fr_FR', null)->willReturn('to_review');
-        $productModelDraft->getCreatedAt()->willReturn($createdAt);
-        $productModelDraft->getEntityWithValue()->willReturn($originalPM);
-        $productModelDraft->getAuthor()->willReturn('john_doe');
-        $productModelDraft->getAuthorLabel()->willReturn('John Doe');
-        $productModelDraft->getSource()->willReturn('source');
-        $productModelDraft->getSourceLabel()->willReturn('Source label');
-        $productModelDraft->getStatus()->willReturn(EntityWithValuesDraftInterface::READY);
+        $productModelDraft->getEntityWithValue()->willReturn($productModel);
         $productModelDraft->getId()->willReturn(42);
+        $productModelDraft->getCreatedAt()->willReturn($created);
+        $productModelDraft->getAuthorLabel()->willReturn('Mary Smith');
+        $productModel->getId()->willReturn(69);
+        $productModel->getLabel()->willReturn('Banana');
 
-        $productModelDraft->getChanges()->willReturn([
-            'values' => [
-                'name' => [
-                    ['scope' => null, 'locale' => 'en_US', 'data' => null],
-                    ['scope' => null, 'locale' => 'fr_FR', 'data' => 'changed'],
-                ],
-            ],
-        ]);
+        $datagridNormalizer->normalize($created, 'datagrid', $context)->willReturn('2017-01-01');
+        $changesNormalizer->normalize($productModelDraft, $context)->willReturn($changes);
 
-        $nameAttribute = new Attribute('name', 'pim_catalog_text', [], true, false, null, null, null, 'string', []);
-        $getAttributesQuery->forCode('name')->willReturn($nameAttribute);
-
-        $valueFactory->createByCheckingData($nameAttribute, null, 'en_US', null)->shouldNotBeCalled();
-        $valueFactory->createByCheckingData($nameAttribute, null, 'fr_FR', 'changed')
-            ->shouldBeCalled()->willReturn(ScalarValue::localizableValue('name', 'changed', 'fr_FR'));
-
-        $standardNormalizer->normalize(Argument::type(WriteValueCollection::class), 'standard', [])
-            ->shouldBeCalled()->willReturn([
-                'name' => [
-                    ['scope' => null, 'locale' => 'fr_FR', 'data' => 'changed'],
-                ],
-            ]);
-
-        $this->normalize($productModelDraft, 'datagrid', [])->shouldReturn([
-            'changes' => [
-                'name' => [
-                    ['scope' => null, 'locale' => 'fr_FR', 'data' => 'changed'],
-                    ['data' => null, 'locale' => 'en_US', 'scope' => null],
-                ],
-            ],
-            'createdAt' => '2020-05-07',
-            'product' => $originalPM,
-            'author' => 'john_doe',
-            'author_label' => 'John Doe',
-            'source' => 'source',
-            'source_label' => 'Source label',
-            'status' => EntityWithValuesDraftInterface::READY,
-            'proposal' => $productModelDraft,
-            'search_id' => 'original',
-            'id' => 'product_model_draft_42',
-            'document_type' => 'product_model_draft',
-            'proposal_id' => 42,
-        ]);
+        $this->normalize($productModelDraft, 'datagrid', $context)->shouldReturn(
+            [
+                'proposal_id' => 42,
+                'createdAt' => '2017-01-01',
+                'author_label' => 'Mary Smith',
+                'document_id' => 69,
+                'document_label' => 'Banana',
+                'formatted_changes' => $changes,
+                'document_type' => 'product_model_draft',
+            ]
+        );
     }
 }
--- tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/Datagrid/Normalizer/ProductProposalNormalizerSpec.php
+++ tests/back/Pim/WorkOrganization/Specification/Workflow/Bundle/Datagrid/Normalizer/ProductProposalNormalizerSpec.php
@@ -2,26 +2,19 @@
 
 namespace Specification\Akeneo\Pim\WorkOrganization\Workflow\Bundle\Datagrid\Normalizer;
 
-use Akeneo\Pim\Enrichment\Component\Product\Factory\ValueFactory;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Model\ValueInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Model\WriteValueCollection;
-use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\Attribute;
-use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\GetAttributes;
+use Akeneo\Pim\WorkOrganization\Workflow\Bundle\Datagrid\Normalizer\ProposalChangesNormalizer;
 use Akeneo\Pim\WorkOrganization\Workflow\Component\Model\ProductDraft;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class ProductProposalNormalizerSpec extends ObjectBehavior
 {
     function let(
-        NormalizerInterface $standardNormalizer,
         NormalizerInterface $datagridNormalizer,
-        ValueFactory $valueFactory,
-        GetAttributes $getAttributes
+        ProposalChangesNormalizer $changesNormalizer
     ) {
-        $this->beConstructedWith($standardNormalizer, $datagridNormalizer, $valueFactory, $getAttributes);
+        $this->beConstructedWith($datagridNormalizer, $changesNormalizer);
     }
 
     function it_should_implement()
@@ -29,89 +22,47 @@ class ProductProposalNormalizerSpec extends ObjectBehavior
         $this->shouldImplement(NormalizerInterface::class);
     }
 
-    function it_supports_product_proposal_normalization(ProductDraft $productProposal)
-    {
+    function it_supports_product_proposal_normalization(
+        ProductDraft $productProposal
+    ) {
         $this->supportsNormalization($productProposal, 'datagrid')->shouldReturn(true);
+        $this->supportsNormalization('Foobar', 'datagrid')->shouldReturn(false);
     }
 
-    function it_normalizes(
-        $standardNormalizer,
-        $datagridNormalizer,
-        ValueFactory $valueFactory,
-        GetAttributes $getAttributes,
+    function it_normalizes_product_proposal(
+        NormalizerInterface $datagridNormalizer,
+        ProposalChangesNormalizer $changesNormalizer,
         ProductDraft $productProposal,
-        WriteValueCollection $valueCollection,
-        ProductInterface $product,
-        ValueInterface $value
+        ProductInterface $product
     ) {
-        $context = [
-            'filter_types' => ['pim.transform.product_value.structured'],
-            'locales'      => ['en_US'],
-            'channels'     => ['ecommerce'],
-            'data_locale'  => 'en_US',
-        ];
-
-        $created = new \DateTime('2017-01-01T01:03:34+01:00');
+        $context = ['locales' => ['en_US']];
         $changes = [
-            'values' => [
-                'text' => [['locale' => null, 'scope' => null, 'data' => 'my text']],
-                'description' => [['locale' => null, 'scope' => null, 'data' => null]]
+            'text' => [
+                ['locale' => 'en_US', 'scope' => null, 'before' => 'beforevalue', 'after' => 'aftervalue'],
+                ['locale' => 'fr_FR', 'scope' => null, 'before' => 'avantvalue', 'after' => 'apresvalue'],
             ]
         ];
-        $datagridNormalizer->normalize($created, 'datagrid', $context)->willReturn('2017-01-01');
+        $created = new \DateTime('2017-01-01T01:03:34+01:00');
 
-        $productProposal->getReviewStatusForChange('text', null, null)->willReturn('to_review');
-        $productProposal->getId()->willReturn(1);
-        $productProposal->getAuthor()->willReturn('Mary');
-        $productProposal->getAuthorLabel()->willReturn('Mary Smith');
-        $productProposal->getSource()->willReturn('pim');
-        $productProposal->getSourceLabel()->willReturn('PIM');
-        $productProposal->getStatus()->willReturn(1);
         $productProposal->getEntityWithValue()->willReturn($product);
+        $productProposal->getId()->willReturn(42);
         $productProposal->getCreatedAt()->willReturn($created);
-        $productProposal->getValues()->willReturn($valueCollection);
-        $productProposal->getChanges()->willReturn($changes);
+        $productProposal->getAuthorLabel()->willReturn('Mary Smith');
+        $product->getId()->willReturn(69);
+        $product->getLabel()->willReturn('Banana');
 
-        $textAttribute = new Attribute('text', 'pim_catalog_text', [], false, false, null, null, false, 'pim_catalog_text', []);
-        $descriptionAttribute = new Attribute('description', 'pim_catalog_text', [], false, false, null, null, false, 'pim_catalog_text', []);
-
-        $getAttributes->forCode('text')->willReturn($textAttribute);
-        $getAttributes->forCode('description')->willReturn($descriptionAttribute);
-
-        $valueFactory->createByCheckingData($textAttribute, null, null, 'my text')->willReturn($value);
-        $valueFactory->createByCheckingData($descriptionAttribute, Argument::cetera())->shouldNotBeCalled();
-        $value->getAttributeCode()->willReturn('text');
-        $value->getScopeCode()->willReturn(null);
-        $value->getLocaleCode()->willReturn(null);
-        $product->getIdentifier()->willReturn(2);
-        $valueCollection = new WriteValueCollection();
-        $valueCollection->add($value->getWrappedObject());
-        $standardNormalizer->normalize(
-            $valueCollection,
-            'standard',
-            $context
-        )->willReturn([
-            'text' => [['locale' => null, 'scope'  => null, 'data'   => 'my text']],
-        ]);
+        $datagridNormalizer->normalize($created, 'datagrid', $context)->willReturn('2017-01-01');
+        $changesNormalizer->normalize($productProposal, $context)->willReturn($changes);
 
         $this->normalize($productProposal, 'datagrid', $context)->shouldReturn(
             [
-                'changes' => [
-                    'text' => [['locale' => null, 'scope'  => null, 'data'   => 'my text']],
-                    'description' => [['data' => null, 'locale' => null, 'scope' => null]],
-                ],
+                'proposal_id' => 42,
                 'createdAt' => '2017-01-01',
-                'product' => $product,
-                'author' => 'Mary',
                 'author_label' => 'Mary Smith',
-                'source' => 'pim',
-                'source_label' => 'PIM',
-                'status' => 1,
-                'proposal' => $productProposal,
-                'search_id' => 2,
-                'id' => 'product_draft_1',
+                'document_id' => 69,
+                'document_label' => 'Banana',
+                'formatted_changes' => $changes,
                 'document_type' => 'product_draft',
-                'proposal_id' => 1,
             ]
         );
     }
