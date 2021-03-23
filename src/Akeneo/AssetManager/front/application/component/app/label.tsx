936a4d0d4044fef7100c86ce0a423ac5ee7c0310
Olivier Pontier <olivier.pontier@akeneo.com>
2021-03-23 09:03:51 +0100

    fix linter issue

--- src/Akeneo/AssetManager/front/application/component/app/label.tsx
+++ src/Akeneo/AssetManager/front/application/component/app/label.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import {AkeneoThemedProps, getColor, getFontSize} from 'akeneo-design-system';
 
-const Label = styled.span<{readOnly?: boolean, isCode?: boolean} & AkeneoThemedProps>`
+const Label = styled.span<{readOnly?: boolean; isCode?: boolean} & AkeneoThemedProps>`
   color: ${({readOnly}) => getColor('grey', readOnly ? 100 : 120)};
   font-size: ${getFontSize('default')};
   text-overflow: ellipsis;
