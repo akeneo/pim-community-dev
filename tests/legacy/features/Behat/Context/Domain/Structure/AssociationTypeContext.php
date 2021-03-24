33c1b07224f545f424d459bcf645f67cedaff995
Laurent Petard <laurent.petard@akeneo.com>
2021-03-24 16:09:26 +0100

    PLG-64: fix behat

--- tests/legacy/features/Behat/Context/Domain/Structure/AssociationTypeContext.php
+++ tests/legacy/features/Behat/Context/Domain/Structure/AssociationTypeContext.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Pim\Behat\Context\Domain\Structure;
 
+use Behat\Mink\Element\NodeElement;
 use Context\Spin\SpinCapableTrait;
 use Pim\Behat\Context\PimContext;
 
@@ -25,5 +26,36 @@ final class AssociationTypeContext extends PimContext
         }, 'Search input not found');
 
         $searchInput->setValue($searchWord);
+    }
+
+    /**
+     * @Then /^I should not see association type (.*)$/
+     */
+    public function iShouldNotSeeAssociationType(string $associationType)
+    {
+        $grid = $this->findDataGridElement();
+
+        $this->spin(function () use ($associationType, $grid) {
+            return !$grid->find('css', sprintf('tr td:contains("%s")', $associationType));
+        }, sprintf('Expected not to see association type "%s"', $associationType));
+    }
+
+    /**
+     * @Then /^I should see association type (.*)$/
+     */
+    public function iShouldSeeAssociationType(string $associationType)
+    {
+        $grid = $this->findDataGridElement();
+
+        $this->spin(function () use ($associationType, $grid) {
+            return $grid->find('css', sprintf('tr td:contains("%s")', $associationType));
+        }, sprintf('Expected to see association type "%s"', $associationType));
+    }
+
+    private function findDataGridElement(): NodeElement
+    {
+        return $this->spin(function () {
+            return $this->getCurrentPage()->find('css', 'table.grid') ?? false;
+        }, 'Association-types data-grid not found');
     }
 }
