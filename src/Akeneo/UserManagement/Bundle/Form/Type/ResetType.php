189061790556e0b41b3e718db42127c96f07e8a4
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-03-24 16:21:47 +0100

    PIM-9767: Add length constraint on password reset form

--- src/Akeneo/UserManagement/Bundle/Form/Type/ResetType.php
+++ src/Akeneo/UserManagement/Bundle/Form/Type/ResetType.php
@@ -7,9 +7,13 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ResetType extends AbstractType
 {
+    private const PASSWORD_MINIMUM_LENGTH = 8;
+    private const PASSWORD_MAXIMUM_LENGTH = 64;
+
     /**
      * @var string
      */
@@ -28,12 +32,21 @@ class ResetType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('plainPassword', RepeatedType::class, [
-            'type'            => PasswordType::class,
-            'required'        => true,
-            'first_options'   => ['label' => 'Password'],
-            'second_options'  => ['label' => 'Again'],
-        ]
+        $builder->add(
+            'plainPassword',
+            RepeatedType::class,
+            [
+                'type'            => PasswordType::class,
+                'required'        => true,
+                'first_options'   => ['label' => 'Password'],
+                'second_options'  => ['label' => 'Again'],
+                'constraints' => [
+                    new Length([
+                        'min' => self::PASSWORD_MINIMUM_LENGTH,
+                        'max' => self::PASSWORD_MAXIMUM_LENGTH
+                    ])
+                ],
+            ]
         );
     }
 
@@ -44,8 +57,8 @@ class ResetType extends AbstractType
     {
         $resolver->setDefaults(
             [
-            'data_class' => $this->class,
-            'intention'  => 'reset',
+                'data_class' => $this->class,
+                'intention'  => 'reset',
             ]
         );
     }
