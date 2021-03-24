99a0daf75d34fc50b40394f80c5769635f355934
Willy Mesnage <willy.mesnage@gmail.com>
2021-03-24 16:30:30 +0100

    API-1494: fix tests

--- src/Akeneo/Connectivity/Connection/front/src/shared/utils/debounce-callback.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/shared/utils/debounce-callback.tsx
@@ -2,6 +2,7 @@ export const debounceCallback = (callback: (...args: any[]) => any, delay: numbe
     let timer: number;
 
     return (...args: any[]) => {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const context = this;
 
         clearTimeout(timer);
