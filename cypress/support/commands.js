392017acb6296316a0dfa901c909b3474e02fe41
Julien Sanchez <sumbobyboys@gmail.com>
2021-03-25 17:27:25 +0100

    RAC-univ: add save on smoke test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,23 +32,67 @@ Cypress.Commands.add('login', () => {
 
   cy.findByLabelText('Password').type('admin');
 
+  // cy.intercept('/configuration/feature-flags').as('featureFlag');
+  // cy.intercept('/rest/user').as('user');
+  cy.intercept('/analytics/collect_data').as('analytics');
+
   cy.findByRole('button', 'Login').click();
+
+  // cy.wait('@featureFlag'); //4000ms
+  // cy.wait('@user'); //4000ms
+  cy.wait('@analytics', 20000); //4000ms
 });
 
 Cypress.Commands.add('goToProductsGridWait', () => {
   cy.findAllByText(/dashboard/i);
   cy.wait(2000);
 
+  cy.intercept('/datagrid/product-grid*').as('productDatagrid')
+  cy.intercept('/datagrid_view/rest/product-grid/default*').as('productDatagridViews')
+
   cy.findByText('Products').click();
+
+  cy.wait('@productDatagridViews')
+  cy.wait('@productDatagrid')
 });
 
 Cypress.Commands.add('goToProductsGridFindActivityItem', () => {
-  // We should rework the HTML to have proper role/aria selectors
+  //We should rework the HTML to have proper role/aria selectors
   cy.findByText('Activity').should('have.class', 'AknHeader-menuItem--active');
 
   cy.findByText('Products').click();
+
+  cy.intercept('/datagrid/product-grid*').as('productDatagrid')
+  cy.intercept('/datagrid_view/rest/product-grid/default*').as('productDatagridViews')
+  cy.wait('@productDatagridViews')
+  cy.wait('@productDatagrid')
 });
 
 Cypress.Commands.add('goToProductsGridUsingUrl', () => {
+  cy.intercept('/datagrid/product-grid*').as('productDatagrid')
+  cy.intercept('/datagrid_view/rest/product-grid/default*').as('productDatagridViews')
+
   cy.visit('/#/enrich/product/');
+
+  cy.wait('@productDatagridViews')
+  cy.wait('@productDatagrid')
 });
+
+Cypress.Commands.add('selectFirstProductInDatagrid', () => {
+  cy.findAllByRole('row').eq(1).click();
+
+  cy.intercept('GET', '/enrich/product/rest/*').as('getProduct')
+  cy.wait('@getProduct')
+})
+
+Cypress.Commands.add('saveProduct', () => {
+  cy.intercept('POST', '/enrich/product/rest/*').as('saveProduct')
+
+  cy.findByText('Save').click();
+
+  cy.wait('@saveProduct')
+})
+
+Cypress.Commands.add('updateField', (label, value) => {
+  cy.findByLabelText(label).clear().type(value)
+})
