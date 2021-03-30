bf70f1f0d66a68c6b5c8708d519275c049174c1b
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-03-30 12:20:48 +0200

    PIM-9767: Fixes after review

--- src/Akeneo/UserManagement/Bundle/Controller/Rest/UserController.php
+++ src/Akeneo/UserManagement/Bundle/Controller/Rest/UserController.php
@@ -471,9 +471,9 @@ class UserController
                 $propertyPath,
                 ''
             ));
-            // We have to use `strlen` here because Symfony's BasePasswordEncoder will check
-            // the actual character count when trying to encode it with salt.
-            // See: Symfony\Component\Security\Core\Encoder\BasePasswordEncoder
+        // We have to use `strlen` here because Symfony's BasePasswordEncoder will check
+        // the actual byte count when trying to encode it with salt.
+        // See: Symfony\Component\Security\Core\Encoder\BasePasswordEncoder
         } elseif (self::PASSWORD_MAXIMUM_LENGTH < strlen($password)) {
             $violations->add(new ConstraintViolation(
                 $this->translator->trans('pim_user.user.fields_errors.new_password.maximum_length'),
