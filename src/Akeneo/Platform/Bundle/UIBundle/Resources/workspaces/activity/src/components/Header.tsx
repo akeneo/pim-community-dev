87ce53d0f47cc79562b61dca730a6b7f71a4f38a
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-04-01 16:07:55 +0200

    PLG-162: replace the random welcome by a hardcoded title

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/activity/src/components/Header.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/activity/src/components/Header.tsx
@@ -2,19 +2,12 @@ import React from 'react';
 import {PageHeader} from '@akeneo-pim-community/shared';
 import {Breadcrumb} from 'akeneo-design-system';
 import {PimView, useTranslate, useUserContext} from '@akeneo-pim-community/legacy-bridge';
-import {generateRandomNumber} from '../helpers';
 
 const MediaUrlGenerator = require('pim/media-url-generator');
 
 const Header = () => {
   const translate = useTranslate();
   const userContext = useUserContext();
-
-  const getRandomWelcomeSentence = (): string => {
-    const welcomeSentences: string[] = ['pim_dashboard.welcome_sentence.message1'];
-
-    return welcomeSentences[generateRandomNumber(welcomeSentences.length - 1)];
-  };
 
   return (
     <PageHeader>
@@ -34,14 +27,7 @@ const Header = () => {
           className="AknTitleContainer-userMenuContainer AknTitleContainer-userMenu"
         />
       </PageHeader.UserActions>
-      <PageHeader.Title>
-        {translate('pim_dashboard.greetings', {
-          // @ts-ignore
-          name: userContext.get('first_name').charAt(0).toUpperCase() + userContext.get('first_name').slice(1),
-        })}
-        &nbsp;
-        {translate(getRandomWelcomeSentence())}
-      </PageHeader.Title>
+      <PageHeader.Title>{translate('pim_dashboard.title')}</PageHeader.Title>
     </PageHeader>
   );
 };
