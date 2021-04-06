d70f742c75d215f6f8f71b93cdc4cb28f3136a29
Michel Tag <micheltag@akeneo.com>
2021-04-06 17:48:13 +0200

    Preparing version 5.0.15 (#10646)
    Preparing version 5.0.15
    
    
    Co-authored-by: jmleroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.14';
+    const VERSION = '5.0.15';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buttercup';
