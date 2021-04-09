8179d299dbee3dbd18289d60c6e4037323ea2477
Benoit Jacquemont <benoit@akeneo.com>
2021-04-09 15:07:32 +0200

    BH-504 Makes the wait for ES more tolerant and more verbose (#10652)
    * BH-504 Makes the wait for ES more tolerant and more verbose

--- deployments/terraform/pim/scripts/hook-waiting-es.sh
+++ deployments/terraform/pim/scripts/hook-waiting-es.sh
@@ -5,11 +5,13 @@ MAX_COUNTER=120
 COUNTER=1
 SLEEP_TIME=5
 
-echo "We will check the connectivity with Elasticsearch"
-while ! curl -s -k --fail "elasticsearch-client:9200/_cluster/health?wait_for_status=green&timeout=1s" > /dev/null; do
+echo "Checking Elasticsearch connectivity..."
+while ! curl --fail "http://elasticsearch-client:9200/_cluster/health?wait_for_status=yellow&timeout=1s&pretty"; do
+    curl "http://elasticsearch-client:9200/_cluster/health?pretty" || true
     COUNTER=$((${COUNTER} + 1))
     if [ ${COUNTER} -gt ${MAX_COUNTER} ]; then
-        echo "We have been waiting for Elasticsearch too long already; failing." >&2
+        TIME_WAITED=$(( COUNTER*SLEEP_TIME ))
+        echo "We have been waiting for Elasticsearch for too long: $TIME_WAITED seconds; failing." >&2
         exit 1
     fi;
     sleep $SLEEP_TIME
