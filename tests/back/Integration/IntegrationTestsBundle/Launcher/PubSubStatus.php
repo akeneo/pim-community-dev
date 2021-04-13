87931d328f49a2bfe7c3954b27be43e27a2a39b1
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-04-13 17:19:40 +0200

    CPM-156: add migration

--- tests/back/Integration/IntegrationTestsBundle/Launcher/PubSubStatus.php
+++ tests/back/Integration/IntegrationTestsBundle/Launcher/PubSubStatus.php
@@ -19,6 +19,7 @@ use Google\Cloud\PubSub\Message;
 
 /**
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 final class PubSubStatus
 {
