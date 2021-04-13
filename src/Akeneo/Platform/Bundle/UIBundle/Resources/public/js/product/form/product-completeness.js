7f819980876ec2e3d3937f4f2807ebd16bda1dc4
Laurent Petard <laurent.petard@akeneo.com>
2021-04-13 17:10:12 +0200

    PLG-151: Display completeness

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/product-completeness.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/product-completeness.js
@@ -14,15 +14,9 @@ define([
   'pim/form',
   'pim/i18n',
   'pim/user-context',
-  'pim/template/product/form/product-completeness',
   '@akeneo-pim-community/enrichment'
-], function (_, __, router, BaseForm, i18n, UserContext, template, {ProductCurrentCompleteness, formatCurrentCompleteness}) {
+], function (_, __, router, BaseForm, i18n, UserContext, {ProductCurrentCompleteness, formatCurrentCompleteness}) {
   return BaseForm.extend({
-    className: 'AknDropdown AknButtonList-item',
-    template: _.template(template),
-    events: {
-      'click .missing-attribute': 'showAttribute',
-    },
 
     /**
      * {@inheritdoc}
@@ -78,34 +72,11 @@ define([
         event
       );
 
-      this.$el.empty();
+      const currentLocale = options.locale;
+      const rawCurrentCompleteness = this.getCurrentCompletenesses(options.scope);
+      const currentCompleteness = rawCurrentCompleteness ? formatCurrentCompleteness(rawCurrentCompleteness, currentLocale) : null;
 
-      const ratio = this.getCurrentRatio(options);
-      if (null !== ratio) {
-        const currentLocale = options.locale;
-        const rawCurrentCompleteness = this.getCurrentCompletenesses(options.scope);
-        const currentCompleteness = formatCurrentCompleteness(rawCurrentCompleteness, currentLocale);
-
-        this.renderReact(ProductCurrentCompleteness, {currentCompleteness}, this.el);
-
-        // this.$el
-        //   .append(
-        //     this.template({
-        //       __: __,
-        //       label: __('pim_enrich.entity.product.module.completeness.complete'),
-        //       ratio: ratio,
-        //       completenesses: currentCompleteness,
-        //       badgeClass: this.getBadgeClass(options),
-        //       currentLocale: options.locale,
-        //       missingValues: 'pim_enrich.entity.product.module.completeness.missing_values',
-        //       i18n: i18n,
-        //     })
-        //   )
-        //   .show();
-      } else {
-        // We hide the element for design issues, to avoid blank spaces.
-        this.$el.hide();
-      }
+      this.renderReact(ProductCurrentCompleteness, {currentCompleteness}, this.el);
 
       return this;
     },
@@ -119,104 +90,6 @@ define([
      */
     getCurrentCompletenesses: function (scope) {
       return _.findWhere(this.getFormData().meta.completenesses, {channel: scope});
-    },
-
-    /**
-     * Returns the ratio of the current scope and current locale
-     *
-     * @param options Object
-     * @param options.locale String
-     * @param options.scope  String
-     *
-     * @returns number|null
-     */
-    getCurrentRatio: function (options) {
-      const completenesses = this.getCurrentCompletenesses(options.scope);
-      if (undefined === completenesses) {
-        return null;
-      }
-
-      const completeness = completenesses.locales[options.locale];
-      if (undefined === completeness) {
-        return null;
-      }
-
-      return Math.round(completeness.completeness.ratio);
-    },
-
-    /**
-     * Returns the HTML class for the badge from the completeness ratio
-     *
-     * @param options Object
-     * @param options.locale String
-     * @param options.scope  String
-     *
-     * @returns string
-     */
-    getBadgeClass: function (options) {
-      const ratio = this.getCurrentRatio(options);
-      if (ratio <= 0) {
-        return 'AknBadge--important';
-      }
-
-      if (ratio >= 100) {
-        return 'AknBadge--enabled';
-      }
-
-      return 'AknBadge--warning';
-    },
-
-    /**
-     * Set focus to the attribute given by the event
-     *
-     * @param event Event
-     */
-    showAttribute: function (event) {
-      this.getRoot().trigger('pim_enrich:form:locale_switcher:change', {
-        localeCode: event.currentTarget.dataset.locale,
-        context: 'base_product',
-      });
-
-      const product = this.getFormData();
-      const familyVariant = product.meta.family_variant;
-      const attributeCode = event.currentTarget.dataset.attribute;
-
-      if (null !== familyVariant) {
-        if (!product.meta.attributes_for_this_level.includes(attributeCode)) {
-          let modelId = product.meta.variant_navigation[0].selected.id;
-          const comesFromParent = product.meta.parent_attributes.includes(attributeCode);
-          const hasTwoLevelsOfVariation = 3 === product.meta.variant_navigation.length;
-
-          if (comesFromParent && hasTwoLevelsOfVariation) {
-            modelId = product.meta.variant_navigation[1].selected.id;
-          }
-
-          this.redirectToModel(modelId);
-
-          return;
-        }
-      }
-
-      this.getRoot().trigger('pim_enrich:form:show_attribute', {
-        attribute: event.currentTarget.dataset.attribute,
-        locale: event.currentTarget.dataset.locale,
-        scope: UserContext.get('catalogScope'),
-      });
-      this.renderCompleteness();
-    },
-
-    /**
-     * Redirect to the product model with the modelId
-     *
-     * @param modelId
-     */
-    redirectToModel: function (modelId) {
-      const params = {id: modelId};
-      const route = 'pim_enrich_product_model_edit';
-
-      sessionStorage.setItem('filter_missing_required_attributes', true);
-
-      router.redirectToRoute(route, params);
-    },
+    }
   });
 });
