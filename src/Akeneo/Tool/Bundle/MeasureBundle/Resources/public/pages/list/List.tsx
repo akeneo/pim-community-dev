42fdc3a9517ca2eedcb56cbb37c24ab2a88920ac
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-04-13 12:58:47 +0200

    PLG-195: update breadcrumbs from settings pages to redirect to the new seetings index page

--- src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/list/List.tsx
+++ src/Akeneo/Tool/Bundle/MeasureBundle/Resources/public/pages/list/List.tsx
@@ -49,7 +49,7 @@ const List = () => {
   const [sortColumn, getSortDirection, toggleSortDirection] = useSorting('label');
   const [measurementFamilies] = useMeasurementFamilies();
   const [isCreateModalOpen, openCreateModal, closeCreateModal] = useToggleState(false);
-  const settingsHref = `#${useRoute('pim_enrich_attribute_index')}`;
+  const settingsHref = `#${useRoute('pim_settings_index')}`;
 
   const handleModalClose = useCallback(
     (createdMeasurementFamilyCode?: MeasurementFamilyCode) => {
@@ -89,7 +89,7 @@ const List = () => {
         }
         breadcrumb={
           <Breadcrumb>
-            <Breadcrumb.Step href={settingsHref}>{__('pim_menu.tab.settings')}</Breadcrumb.Step>
+            <Breadcrumb.Step href={`#${settingsHref}`}>{__('pim_menu.tab.settings')}</Breadcrumb.Step>
             <Breadcrumb.Step>{__('pim_menu.item.measurements')}</Breadcrumb.Step>
           </Breadcrumb>
         }
