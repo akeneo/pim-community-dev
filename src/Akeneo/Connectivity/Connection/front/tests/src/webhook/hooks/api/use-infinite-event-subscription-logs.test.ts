4f68cba028618246e46cae5cb2537547722e09a5
Tseho <quentin.favrie@gmail.com>
2021-04-15 16:08:27 +0200

    API-1564: Add tests & reduce amount of re-render in useInfiniteScroll

---
+++ src/Akeneo/Connectivity/Connection/front/tests/src/webhook/hooks/api/use-infinite-event-subscription-logs.test.ts
@@ -0,0 +1,83 @@
+import {renderHook} from '@testing-library/react-hooks';
+import useInfiniteEventSubscriptionLogs from '@src/webhook/hooks/api/use-infinite-event-subscription-logs';
+import {EventSubscriptionLogLevel} from '@src/webhook/model/EventSubscriptionLogLevel';
+import {mockFetchResponses} from '../../../../test-utils';
+
+beforeEach(() => {
+    document.body.innerHTML = `
+    <div>
+        <div id='content'></div>
+    </div>
+    `;
+});
+
+test('The first logs are fetched on mount', async () => {
+    const ref = {current: document.getElementById('content')};
+
+    mockFetchResponses({
+        'akeneo_connectivity_connection_events_api_debug_rest_search_event_subscription_logs?connection_code=alkemics': {
+            json: {
+                results: [
+                    {
+                        level: EventSubscriptionLogLevel.INFO,
+                        timestamp: 1615741520,
+                        connection_code: null,
+                        message: 'a log message',
+                        context: {
+                            foo: 'bar',
+                        },
+                    },
+                ],
+                total: 1,
+                search_after: 'search_after_1',
+            },
+        },
+    });
+
+    const filters = {
+        levels: [
+            EventSubscriptionLogLevel.INFO,
+            EventSubscriptionLogLevel.NOTICE,
+            EventSubscriptionLogLevel.WARNING,
+            EventSubscriptionLogLevel.ERROR,
+        ],
+        text: '',
+        dateTime: {},
+    };
+
+    const {waitForNextUpdate, result, unmount} = renderHook(() => useInfiniteEventSubscriptionLogs('alkemics', filters, ref));
+
+    expect(result.current).toEqual({
+        logs: [],
+        total: undefined,
+        page: 0,
+        maxScrollReached: false,
+        endScrollReached: false,
+        isLoading: true,
+        isInitialized: false
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current).toEqual({
+        logs: [
+            {
+                level: EventSubscriptionLogLevel.INFO,
+                timestamp: 1615741520,
+                connection_code: null,
+                message: 'a log message',
+                context: {
+                    foo: 'bar',
+                },
+            },
+        ],
+        total: 1,
+        page: 1,
+        maxScrollReached: false,
+        endScrollReached: false,
+        isLoading: false,
+        isInitialized: true
+    });
+
+    unmount();
+});
