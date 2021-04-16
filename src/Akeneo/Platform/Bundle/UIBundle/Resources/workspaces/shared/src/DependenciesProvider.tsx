ec8dc8b9ad93777c261a1640fae3141ae9a0a55a
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-04-16 18:00:57 +0200

    RAC-560: Fix lint

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/DependenciesProvider.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/shared/src/DependenciesProvider.tsx
@@ -3,7 +3,7 @@ import {DependenciesContext} from '@akeneo-pim-community/legacy-bridge';
 import {dependencies} from './dependencies';
 
 const DependenciesProvider: FC = ({children}) => {
-    return <DependenciesContext.Provider value={dependencies}>{children}</DependenciesContext.Provider>;
+  return <DependenciesContext.Provider value={dependencies}>{children}</DependenciesContext.Provider>;
 };
 
 export {DependenciesProvider};
