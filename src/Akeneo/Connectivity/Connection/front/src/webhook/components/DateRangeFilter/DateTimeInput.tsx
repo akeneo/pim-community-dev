74b4f1346982b063c58aaba2934493cb175f8426
Paul Chasle <pchasle@users.noreply.github.com>
2021-04-16 12:28:12 +0200

    API-1546 fix datetime input reset

--- src/Akeneo/Connectivity/Connection/front/src/webhook/components/DateRangeFilter/DateTimeInput.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/webhook/components/DateRangeFilter/DateTimeInput.tsx
@@ -1,6 +1,6 @@
 import {TextInput} from 'akeneo-design-system';
 import {DateTime} from 'luxon';
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {useUser} from '../../../shared/user';
 
@@ -9,6 +9,12 @@ const TIME_INPUT_FORMAT = 'HH:mm';
 
 const DATE_INPUT_PATTERN = /^\d{4}-\d{2}-\d{2}$/;
 const TIME_INPUT_PATTERN = /^\d{2}:\d{2}$/;
+
+const timestampToZonedInputDateString = (timeZone: string, timestamp?: Timestamp) =>
+    (timestamp && DateTime.fromSeconds(timestamp, {zone: timeZone}).toFormat(DATE_INPUT_FORMAT)) || '';
+
+const timestampToZonedInputTimeString = (timeZone: string, timestamp?: Timestamp) =>
+    (timestamp && DateTime.fromSeconds(timestamp, {zone: timeZone}).toFormat(TIME_INPUT_FORMAT)) || '';
 
 type Timestamp = number;
 type Props = {
@@ -20,12 +26,19 @@ type Props = {
 };
 
 export const DateTimeInput: FC<Props> = ({value, min, max, onChange, onError}) => {
-    const {timeZone: zone} = useUser();
+    const {timeZone} = useUser();
 
-    const timestampToZonedString = (timestamp: Timestamp, toFormat: string) =>
-        DateTime.fromSeconds(timestamp, {zone}).toFormat(toFormat);
+    const [values, setValues] = useState({
+        dateString: timestampToZonedInputDateString(timeZone, value),
+        timeString: timestampToZonedInputTimeString(timeZone, value),
+    });
+    useEffect(() => {
+        setValues({
+            dateString: timestampToZonedInputDateString(timeZone, value),
+            timeString: timestampToZonedInputTimeString(timeZone, value),
+        });
+    }, [timeZone, value]);
 
-    const [values, setValues] = useState({dateString: '', timeString: ''});
     const [errors, setErrors] = useState<{date?: boolean; time?: boolean}>({});
 
     const handleChange = (dateString: string, timeString: string) => {
@@ -45,7 +58,7 @@ export const DateTimeInput: FC<Props> = ({value, min, max, onChange, onError}) =
             return;
         }
 
-        const newZonedDateTime = DateTime.fromISO(`${dateString}T${timeString}`, {zone});
+        const newZonedDateTime = DateTime.fromISO(`${dateString}T${timeString}`, {zone: timeZone});
         if (!newZonedDateTime.isValid) {
             const error = newZonedDateTime.invalidExplanation as string;
             setErrors({date: true, time: true});
@@ -59,11 +72,11 @@ export const DateTimeInput: FC<Props> = ({value, min, max, onChange, onError}) =
     return (
         <FlexContainer>
             <TextInput
-                type='date'
-                defaultValue={(value && timestampToZonedString(value, DATE_INPUT_FORMAT)) || ''}
-                min={min && timestampToZonedString(min, DATE_INPUT_FORMAT)}
-                max={max && timestampToZonedString(max, DATE_INPUT_FORMAT)}
-                placeholder={timestampToZonedString(DateTime.utc().toSeconds(), DATE_INPUT_FORMAT)}
+                type='text'
+                value={values.dateString}
+                min={timestampToZonedInputDateString(timeZone, min)}
+                max={timestampToZonedInputDateString(timeZone, max)}
+                placeholder={timestampToZonedInputDateString(timeZone, DateTime.utc().toSeconds())}
                 onChange={dateString => {
                     setValues(values => ({...values, dateString}));
                     handleChange(dateString, values.timeString);
@@ -72,9 +85,9 @@ export const DateTimeInput: FC<Props> = ({value, min, max, onChange, onError}) =
                 aria-label='Date'
             />
             <TextInput
-                type='time'
-                defaultValue={(value && timestampToZonedString(value, TIME_INPUT_FORMAT)) || ''}
-                placeholder={timestampToZonedString(DateTime.utc().toSeconds(), TIME_INPUT_FORMAT)}
+                type='text'
+                value={values.timeString}
+                placeholder={timestampToZonedInputTimeString(timeZone, DateTime.utc().toSeconds())}
                 onChange={timeString => {
                     setValues(values => ({...values, timeString}));
                     handleChange(values.dateString, timeString);
