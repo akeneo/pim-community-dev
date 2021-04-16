9f8299bb849048df0391ba41ff1448e7ec603c8c
Olivier Pontier <olivier.pontier@akeneo.com>
2021-04-16 07:47:29 +0200

    Add unit tests to ensure that tree actions are triggered

--- akeneo-design-system/src/components/Tree/Tree.unit.tsx
+++ akeneo-design-system/src/components/Tree/Tree.unit.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Tree} from './Tree';
 import {render, screen, fireEvent} from '../../storybook/test-util';
+import {Button} from "../Button/Button";
 
 test('it renders tree with children properly and is loading', () => {
   render(
@@ -98,4 +99,49 @@ test('It throws with an unknown status', () => {
   }).toThrow();
 
   mockConsole.mockRestore();
+});
+
+test('it triggers actions', () => {
+  const handleOpen = jest.fn();
+  const handleClose = jest.fn();
+  const handleClick = jest.fn();
+  const handleAction = jest.fn();
+
+  render(
+    <Tree
+      value={'master'}
+      label={'Master'}
+      selectable={true}
+      onOpen={handleOpen}
+      onClose={handleClose}
+      onClick={handleClick}
+    >
+      <Tree.Actions>
+        <Button onClick={handleAction} data-testid="action">Action</Button>
+      </Tree.Actions>
+    </Tree>
+  );
+
+  fireEvent.click(screen.getByTestId('action'));
+
+  expect(handleOpen).toBeCalledTimes(0);
+  expect(handleClose).toBeCalledTimes(0);
+  expect(handleClick).toBeCalledTimes(0);
+  expect(handleAction).toBeCalledTimes(1);
+
+  jest.resetAllMocks();
+  fireEvent.click(screen.getAllByRole('button')[0]);
+
+  expect(handleOpen).toBeCalledTimes(1);
+  expect(handleClose).toBeCalledTimes(0);
+  expect(handleClick).toBeCalledTimes(0);
+  expect(handleAction).toBeCalledTimes(0);
+
+  jest.resetAllMocks();
+  fireEvent.click(screen.getAllByRole('button')[1]);
+
+  expect(handleOpen).toBeCalledTimes(0);
+  expect(handleClose).toBeCalledTimes(0);
+  expect(handleClick).toBeCalledTimes(1);
+  expect(handleAction).toBeCalledTimes(0);
 });
