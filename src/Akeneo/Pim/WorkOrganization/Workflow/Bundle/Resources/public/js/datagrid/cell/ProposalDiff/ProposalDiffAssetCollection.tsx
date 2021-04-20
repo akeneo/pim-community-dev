6f382edf165621ad60fe1ce8f7b182e366c946ba
pierallard <pierallard@gmail.com>
2021-04-20 12:06:16 +0200

    CPM-157: Move diff dependency from EE to CE

--- src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Resources/public/js/datagrid/cell/ProposalDiff/ProposalDiffAssetCollection.tsx
+++ src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Resources/public/js/datagrid/cell/ProposalDiff/ProposalDiffAssetCollection.tsx
@@ -82,7 +82,7 @@ const ProposalDiffAssetCollection: React.FC<ProposalDiffAssetCollectionProps> = 
             state={isDiff ? (accessor === 'before' ? 'removed' : 'added') : undefined}
             key={`${asset.code}-${i}`}
           >
-            <FullscreenPreview attribute={attribute} data={data} label={label} onClose={() => {}}/>
+            <FullscreenPreview attribute={attribute} data={data} label={label} onClose={() => {}} />
           </ImageCard>
         );
       })}
