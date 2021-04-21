7a1fa51f930feea7c7c4beba978dd30fbc2d0424
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-04-21 17:10:07 +0200

    RAC-560: Run packages unit test

--- front-packages/shared/jest.config.js
+++ front-packages/shared/jest.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
-  //   globals: {
-  //     'ts-jest': {
-  //       tsconfig: './tests/tsconfig.json',
-  //     },
-  //   },
-  //   moduleDirectories: ['<rootDir>/../../node_modules/'],
   moduleNameMapper: {
     '\\.(svg|css)$': '<rootDir>/tests/fileMock.ts',
   },
-  setupFilesAfterEnv: ['./tests/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
   testMatch: ['<rootDir>/src/**/?(*.)+(unit).ts?(x)'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
 };
