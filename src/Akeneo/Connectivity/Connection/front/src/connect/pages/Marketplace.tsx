d6378a24da89f4315a600e6161ab1068e6145909
Willy Mesnage <willy.mesnage@gmail.com>
2021-04-21 11:45:32 +0200

    CXP-658: Fix typescript

--- src/Akeneo/Connectivity/Connection/front/src/connect/pages/Marketplace.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/connect/pages/Marketplace.tsx
@@ -1,12 +1,12 @@
 import React, {FC} from 'react';
-import {Breadcrumb, ChannelsIllustration, getColor, getFontSize} from 'akeneo-design-system';
+import {AkeneoThemedProps, Breadcrumb, ChannelsIllustration, getColor, getFontSize} from 'akeneo-design-system';
 import {useTranslate} from '../../shared/translate';
 import {PageHeader} from '../../common';
 import {UserButtons} from '../../shared/user';
 import styled from 'styled-components';
 import {Caption, Heading} from '../../common/components/EmptyState';
 
-const LinkButton = styled.a`
+const LinkButton = styled.a<AkeneoThemedProps>`
     display: inline-flex;
     align-items: center;
     gap: 10px;
@@ -31,11 +31,11 @@ const LinkButton = styled.a`
     background-color: ${getColor('purple', 100)};
 
     &:hover {
-      background-color: ${getColor('purple', 120)};
+        background-color: ${getColor('purple', 120)};
     }
 
     &:active {
-      background-color: ${getColor('purple', 140)};
+        background-color: ${getColor('purple', 140)};
     }
     &:focus {
         box-shadow: 0 0 0 2px ${getColor('blue', 40)};
@@ -55,39 +55,33 @@ export const Marketplace: FC = () => {
     const translate = useTranslate();
     const breadcrumb = (
         <Breadcrumb>
-            <Breadcrumb.Step>
-                {translate('pim_menu.connect.title')}
-            </Breadcrumb.Step>
-            <Breadcrumb.Step>
-                {translate('pim_menu.connect.marketplace')}
-            </Breadcrumb.Step>
+            <Breadcrumb.Step>{translate('pim_menu.connect.title')}</Breadcrumb.Step>
+            <Breadcrumb.Step>{translate('pim_menu.connect.marketplace')}</Breadcrumb.Step>
         </Breadcrumb>
     );
 
-    return (<>
-        <PageHeader breadcrumb={breadcrumb} userButtons={<UserButtons />}>
-            {translate('pim_menu.connect.marketplace')}
-        </PageHeader>
+    return (
+        <>
+            <PageHeader breadcrumb={breadcrumb} userButtons={<UserButtons />}>
+                {translate('pim_menu.connect.marketplace')}
+            </PageHeader>
 
-        <PageContent>
-            <ChannelsIllustration size={256} />
+            <PageContent>
+                <ChannelsIllustration size={256} />
 
-            <Heading>
-                {translate('akeneo_connectivity.connection.connect.marketplace.title')}
-            </Heading>
+                <Heading>{translate('akeneo_connectivity.connection.connect.marketplace.title')}</Heading>
 
-            <Caption>
-                {translate('akeneo_connectivity.connection.connect.marketplace.sub_title')}
-            </Caption>
+                <Caption>{translate('akeneo_connectivity.connection.connect.marketplace.sub_title')}</Caption>
 
-            <LinkButton
-                href='https://marketplace.akeneo.com/extensions?edition=all&version=all&api_use=1&sort=date'
-                target='_blank'
-                role='link'
-                tabindex='0'
-            >
-                {translate('akeneo_connectivity.connection.connect.marketplace.link')}
-            </LinkButton>
-        </PageContent>
-    </>);
+                <LinkButton
+                    href='https://marketplace.akeneo.com/extensions?edition=all&version=all&api_use=1&sort=date'
+                    target='_blank'
+                    role='link'
+                    tabIndex='0'
+                >
+                    {translate('akeneo_connectivity.connection.connect.marketplace.link')}
+                </LinkButton>
+            </PageContent>
+        </>
+    );
 };
