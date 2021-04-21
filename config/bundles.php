68cb349f85476bcbf999d5716bd18faee7ccaf2f
Grégoire HUBERT <46669887+greg-akeneo@users.noreply.github.com>
2021-04-21 10:28:57 +0200

    pull up BH-461 changes

--- config/bundles.php
+++ config/bundles.php
@@ -83,6 +83,7 @@ return [
     AkeneoEnterprise\Connectivity\Connection\Infrastructure\Symfony\AkeneoEnterpriseConnectivityConnectionBundle::class =>  ['all' => true],
 
     // Dev related bundles
+    Akeneo\Tool\Bundle\DatabaseMetadataBundle\AkeneoDatabaseMetadataBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'behat' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'behat' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true, 'behat' => true],
