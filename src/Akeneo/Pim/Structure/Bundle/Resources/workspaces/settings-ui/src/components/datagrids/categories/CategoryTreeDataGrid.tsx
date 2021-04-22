27093bfeb8abdefc8c76425e44da9afe5077b252
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-04-22 14:44:23 +0200

    PLG-172: allow search on category trees

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/datagrids/categories/CategoryTreeDataGrid.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/datagrids/categories/CategoryTreeDataGrid.tsx
@@ -1,7 +1,10 @@
-import React, {FC, useCallback} from 'react';
-import {Table} from 'akeneo-design-system';
+import React, {FC, useCallback, useState} from 'react';
+import {Search, Table} from 'akeneo-design-system';
 import {useRouter, useSecurity, useTranslate} from '@akeneo-pim-community/legacy-bridge';
 import {CategoryTree} from '../../../models';
+import {useDebounceCallback} from '@akeneo-pim-community/shared';
+import styled from 'styled-components';
+import {NoResults} from "../../shared";
 
 type Props = {
   trees: CategoryTree[];
@@ -11,6 +14,8 @@ const CategoryTreesDataGrid: FC<Props> = ({trees}) => {
   const translate = useTranslate();
   const router = useRouter();
   const {isGranted} = useSecurity();
+  const [searchString, setSearchString] = useState('');
+  const [filteredTrees, setFilteredTrees] = useState<CategoryTree[]>(trees);
 
   const followCategoryTree = useCallback((tree: CategoryTree): void => {
     const url = router.generate('pim_enrich_categorytree_tree', {id: tree.id});
@@ -19,25 +24,65 @@ const CategoryTreesDataGrid: FC<Props> = ({trees}) => {
     return;
   }, []);
 
+  const search = useCallback(
+    (searchString: string) => {
+      setFilteredTrees(
+        trees.filter((tree: CategoryTree) => {
+          return (
+            tree.code.toLocaleLowerCase().includes(searchString.toLowerCase().trim()) ||
+            tree.label.toLocaleLowerCase().includes(searchString.toLowerCase().trim())
+          );
+        })
+      );
+    },
+    [trees]
+  );
+
+  const debouncedSearch = useDebounceCallback(search, 300);
+
+  const onSearch = (searchValue: string) => {
+    setSearchString(searchValue);
+    debouncedSearch(searchValue);
+  };
+
   return (
     <>
-      <Table>
-        <Table.Header>
-          <Table.HeaderCell>{translate('pim_enrich.entity.category.content.tree_list.columns.label')}</Table.HeaderCell>
-        </Table.Header>
-        <Table.Body>
-          {trees.map(tree => (
-            <Table.Row
-              key={tree.code}
-              onClick={isGranted('pim_enrich_product_category_list') ? () => followCategoryTree(tree) : undefined}
-            >
-              <Table.Cell rowTitle>{tree.label}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+      <StyledSearch searchValue={searchString} onSearchChange={onSearch} placeholder={translate('pim_common.search')}>
+        <Search.ResultCount>
+          {translate('pim_common.result_count', {itemsCount: filteredTrees.length.toString()}, filteredTrees.length)}
+        </Search.ResultCount>
+      </StyledSearch>
+      {filteredTrees.length === 0 && searchString !== '' &&
+        <NoResults
+          title={translate('pim_datagrid.no_results', {
+            entityHint: translate('pim_enrich.entity.category.label'),
+          })}
+          subtitle={translate('pim_datagrid.no_results_subtitle')}
+        />
+      }
+      {filteredTrees.length > 0 &&
+        <Table>
+          <Table.Header>
+            <Table.HeaderCell>{translate('pim_enrich.entity.category.content.tree_list.columns.label')}</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            {filteredTrees.map(tree => (
+              <Table.Row
+                key={tree.code}
+                onClick={isGranted('pim_enrich_product_category_list') ? () => followCategoryTree(tree) : undefined}
+              >
+                <Table.Cell rowTitle>{tree.label}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      }
     </>
   );
 };
+
+const StyledSearch = styled(Search)`
+  margin-bottom: 20px;
+`;
 
 export {CategoryTreesDataGrid};
