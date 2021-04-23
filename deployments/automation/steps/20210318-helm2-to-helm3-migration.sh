dbf3d6db4884b6133c983ef5a482e0cb5bc7cd99
franckbrouard <48205166+franckbrouard@users.noreply.github.com>
2021-04-23 19:51:52 +0200

    BH-525-3 : add kubeconfig (#10724)
    * change max release version
    * generate kubeconfig

--- deployments/automation/steps/20210318-helm2-to-helm3-migration.sh
+++ deployments/automation/steps/20210318-helm2-to-helm3-migration.sh
@@ -2,12 +2,12 @@
 set -eo pipefail
 set -x
 
-if [ ${STEP} == "PRE_INIT" ]; then
+if [ ${STEP} == "PRE_APPLY" ]; then
     if [ -n "$(helm3 list -n ${PFID} --max 1 -o json | jq -r '.[] | select(.name == "'${PFID}'") | .name')" ]; then
         echo "[INFO] ${PFID} is already HELM3 compatible"
     else
         echo "[INFO] ${PFID} is a HELM2 released, time to convert"
-        helm version
-        helm3 2to3 convert ${PFID} --delete-v2-releases --release-versions-max 1
+        terraform apply -input=false -auto-approve -target=module.pim.local_file.kubeconfig
+        KUBECONFIG=.kubeconfig helm3 2to3 convert ${PFID} --delete-v2-releases --release-versions-max 1
     fi
 fi
