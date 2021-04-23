678c691194d15f38b4151f3a01399b4bdd2206db
Thomas Galvaing <thomas.galvaing@akeneo.com>
2021-04-23 14:12:59 +0200

    CXP-650: fix review

--- src/Akeneo/Connectivity/Connection/front/src/connect/pages/RedirectConnectionDashboardToConnectMenu.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/connect/pages/RedirectConnectionDashboardToConnectMenu.tsx
@@ -1,11 +1,10 @@
-import {ClientErrorIllustration, Breadcrumb} from 'akeneo-design-system';
+import {Breadcrumb, ClientErrorIllustration} from 'akeneo-design-system';
 import React from 'react';
-import {Translate, useTranslate} from '../../shared/translate';
-import {HelperLink, PageHeader} from '../../common';
-import {UserButtons} from '../../shared/user';
 import styled from 'styled-components';
-import {Heading} from '../../common/components/EmptyState';
+import {EmptyState, HelperLink, PageHeader} from '../../common';
 import {useRouter} from '../../shared/router/use-router';
+import {Translate, useTranslate} from '../../shared/translate';
+import {UserButtons} from '../../shared/user';
 
 const PageContent = styled.div`
     text-align: center;
@@ -35,7 +34,9 @@ export const RedirectConnectionDashboardToConnectMenu = () => {
             <PageContent>
                 <ClientErrorIllustration width={500} height={250} />
 
-                <Heading>{translate('akeneo_connectivity.connection.connect.redirect.title')}</Heading>
+                <EmptyState.Heading>
+                    {translate('akeneo_connectivity.connection.connect.redirect.title')}
+                </EmptyState.Heading>
 
                 <HelperLink href={`#${generateUrl('akeneo_connectivity_connection_audit_index')}`}>
                     <Translate id='akeneo_connectivity.connection.connect.redirect.link' />
