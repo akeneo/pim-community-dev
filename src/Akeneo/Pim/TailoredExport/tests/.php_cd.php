41098f34d4551d9c647c3608b15fa90827e286c8
Steven VAIDIE <steven.vaidie@gmail.com>
2021-04-27 10:46:07 +0200

    RAC-603: add copyright in all files

--- src/Akeneo/Pim/TailoredExport/tests/.php_cd.php
+++ src/Akeneo/Pim/TailoredExport/tests/.php_cd.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Akeneo\CouplingDetector\Configuration\Configuration;
 use Akeneo\CouplingDetector\Configuration\DefaultFinder;
 use Akeneo\CouplingDetector\RuleBuilder;
