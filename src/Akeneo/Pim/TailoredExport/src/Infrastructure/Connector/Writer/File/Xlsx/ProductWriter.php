eace474329f2fca0dd77624f566379009493114b
Steven VAIDIE <steven.vaidie@gmail.com>
2021-04-28 09:29:42 +0200

    RAC-603: fix src/Akeneo/Pim also scan TailoredExportBundle avoid twice pass and avoid scan TailoredExportBundle test directory

--- src/Akeneo/Pim/TailoredExport/src/Infrastructure/Connector/Writer/File/Xlsx/ProductWriter.php
+++ src/Akeneo/Pim/TailoredExport/src/Infrastructure/Connector/Writer/File/Xlsx/ProductWriter.php
@@ -14,7 +14,8 @@ namespace Akeneo\Pim\TailoredExport\Infrastructure\Connector\Writer\File\Xlsx;
 
 use Akeneo\Pim\TailoredExport\Infrastructure\Connector\Writer\File\AbstractItemMediaWriter;
 
-class ProductWriter extends AbstractItemMediaWriter {
+class ProductWriter extends AbstractItemMediaWriter
+{
     /**
      * @return array<string, mixed>
      * {@inheritdoc}
