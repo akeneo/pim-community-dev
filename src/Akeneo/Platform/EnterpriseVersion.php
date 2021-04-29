6745dc561de26a7515cf33f34146e5c1ff8a7e81
Michel Tag <micheltag@akeneo.com>
2021-04-29 14:17:16 +0200

    Preparing version 4.0.106 (#10758)
    Preparing version 4.0.106
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.105';
+    const VERSION = '4.0.106';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Daisy';
