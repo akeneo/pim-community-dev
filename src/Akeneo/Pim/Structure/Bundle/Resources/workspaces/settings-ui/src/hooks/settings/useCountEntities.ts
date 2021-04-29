fcb30cd9996b70bd3b0b8a16b8a27608a16a6517
Laurent Pétard <laurent.petard@akeneo.com>
2021-04-29 10:50:47 +0200

    PLG-216: Display count entities figures in settings home page (#14352)
    * PLG-216: Add query to count settings entities
    
    * PLG-216: Add end-point to count settings entities
    
    * PLG-216: Move controller and query to UIBundle
    
    * PLG-216: Display count entities figures in settings home page
    
    * PLG-216: Fix route name

---
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/hooks/settings/useCountEntities.ts
@@ -0,0 +1,21 @@
+import {useEffect, useState} from 'react';
+import {baseFetcher, useRoute} from '@akeneo-pim-community/shared';
+
+type CountEntities = {
+  [key: string]: number;
+};
+
+const useCountEntities = (): CountEntities => {
+  const [countEntities, setCountEntities] = useState<CountEntities>({});
+  const url = useRoute('pim_settings_count_entities');
+
+  useEffect(() => {
+    (async () => {
+      setCountEntities(await baseFetcher(url));
+    })();
+  }, []);
+
+  return countEntities;
+};
+
+export {useCountEntities};
