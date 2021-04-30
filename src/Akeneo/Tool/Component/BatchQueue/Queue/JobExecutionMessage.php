112aab8f7205f9fa278dabf0d51f9b915328295b
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-04-30 17:09:54 +0200

    CPM-156: keep queue table if we have to rollback

--- src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionMessage.php
+++ src/Akeneo/Tool/Component/BatchQueue/Queue/JobExecutionMessage.php
@@ -21,6 +21,8 @@ abstract class JobExecutionMessage implements JobExecutionMessageInterface
     private \DateTime $createTime;
     private ?\DateTime $updatedTime;
     private array $options = [];
+    /** @TODO CPM-154: remove this property */
+    private string $consumer;
 
     private function __construct(
         UuidInterface $id,
