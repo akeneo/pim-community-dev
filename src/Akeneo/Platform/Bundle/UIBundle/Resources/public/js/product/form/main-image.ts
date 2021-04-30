bcee336a8aae48143c25a3f9ad0236129003af3a
rfauglas <ronan.fauglas@akeneo.com>
2021-04-30 16:10:27 +0200

    fix front linting

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/main-image.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/main-image.ts
@@ -8,34 +8,39 @@ class MainImage extends BaseMainImage {
     this.listenTo(this.getRoot(), 'pim_enrich:form:entity:update_state', this.render.bind(this));
 
     return BaseMainImage.prototype.configure.apply(this, arguments);
-  };
+  }
 
   render() {
     const fallbackUrl = '/bundles/pimui/img/image_default.png';
 
-    FetcherRegistry.getFetcher('family').fetch(this.getRoot().getFormData().family).then((family: any) => {
-      const attributeCode = family.attribute_as_image;
-      if (!attributeCode) {
+    FetcherRegistry.getFetcher('family')
+      .fetch(this.getRoot().getFormData().family)
+      .then((family: any) => {
+        const attributeCode = family.attribute_as_image;
+        if (!attributeCode) {
+          this.el.src = fallbackUrl;
+          return;
+        }
+
+        const imageValues = this.getRoot().getFormData().values[attributeCode];
+        if (imageValues.length === 0) {
+          this.el.src = fallbackUrl;
+          return;
+        }
+
+        const imageValue = imageValues[0]; // As the attribute_as_image is not localizable, it can only have 1 value max.
+        const imageData = imageValue.data;
+        if (imageData.hasOwnProperty('filePath') && imageData.hasOwnProperty('originalFilename')) {
+          this.el.src = Routing.generate('pim_enrich_media_show', {
+            filename: encodeURIComponent(imageData.filePath),
+            filter: 'thumbnail',
+          });
+          return;
+        }
+
         this.el.src = fallbackUrl;
-        return;
-      }
-
-      const imageValues = this.getRoot().getFormData().values[attributeCode];
-      if (imageValues.length === 0) {
-        this.el.src = fallbackUrl;
-        return;
-      }
-
-      const imageValue = imageValues[0]; // As the attribute_as_image is not localizable, it can only have 1 value max.
-      const imageData = imageValue.data;
-      if (imageData.hasOwnProperty('filePath') && imageData.hasOwnProperty('originalFilename')) {
-        this.el.src = Routing.generate('pim_enrich_media_show', { 'filename': encodeURIComponent(imageData.filePath), 'filter': 'thumbnail' });
-        return;
-      }
-
-      this.el.src = fallbackUrl;
-    });
-  };
+      });
+  }
 }
 
 export = MainImage;
