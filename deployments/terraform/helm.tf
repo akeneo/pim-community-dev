1b6b8f130c17bb45aab940abed98ac02112442a4
Rémy Bétus <3594268+rybus@users.noreply.github.com>
2021-05-04 17:01:13 +0200

    Remove traefik dependency

--- deployments/terraform/helm.tf
+++ deployments/terraform/helm.tf
@@ -63,7 +63,6 @@ resource "null_resource" "helm_release_pim" {
 yq w -i ${path.module}/pim/Chart.yaml version 0.0.0-${var.pim_version}
 yq w -i ${path.module}/pim/Chart.yaml appVersion ${var.pim_version}
 helm3 repo add akeneo-charts gs://akeneo-charts/
-helm3 repo add traefik-v2 https://helm.traefik.io/traefik
 helm3 dependencies update ${path.module}/pim/
 export KUBECONFIG="${local_file.kubeconfig.filename}"
 helm3 upgrade --atomic --cleanup-on-fail --history-max 5 --create-namespace --wait --install --timeout 20m ${local.pfid} --namespace ${local.pfid} ${path.module}/pim/ -f tf-helm-pim-values.yaml -f values.yaml
