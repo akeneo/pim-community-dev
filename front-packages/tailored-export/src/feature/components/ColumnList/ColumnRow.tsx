6fdee701d6a1f612459e07c02cf1bac7a8a78a12
Steven VAIDIE <steven.vaidie@gmail.com>
2021-05-04 15:17:36 +0200

    RAC-600: Add delete column modal

--- front-packages/tailored-export/src/feature/components/ColumnList/ColumnRow.tsx
+++ front-packages/tailored-export/src/feature/components/ColumnList/ColumnRow.tsx
@@ -1,7 +1,7 @@
 import React, {forwardRef, SyntheticEvent} from 'react';
 import styled from 'styled-components';
-import {CloseIcon, Helper, IconButton, List, TextInput} from 'akeneo-design-system';
-import {useTranslate} from '@akeneo-pim-community/shared';
+import {CloseIcon, Helper, IconButton, List, TextInput, useBooleanState} from 'akeneo-design-system';
+import {DeleteModal, useTranslate} from '@akeneo-pim-community/shared';
 import {ColumnConfiguration} from '../../models/ColumnConfiguration';
 import {useValidationErrors} from '../../contexts';
 
@@ -24,44 +24,61 @@ type ColumnRowProps = {
 const ColumnRow = forwardRef<HTMLInputElement, ColumnRowProps>(
   ({column, isSelected, onColumnChange, onFocusNext, onColumnSelected, onColumnRemoved}: ColumnRowProps, ref) => {
     const translate = useTranslate();
+    const [isDeleteModalOpen, openDeleteModal, closeDeleteModal] = useBooleanState();
 
-    const handleColumnRemove = (event: SyntheticEvent, uuid: string) => {
+    const handleColumnRemove = (event: SyntheticEvent) => {
       event.stopPropagation();
-      onColumnRemoved(uuid);
-      onFocusNext(uuid);
+      openDeleteModal();
     };
+
+    const handleConfirmColumnRemove = () => {
+      onColumnRemoved(column.uuid);
+      onFocusNext(column.uuid);
+    }
 
     const targetErrors = useValidationErrors(`[columns][${column.uuid}][target]`, true);
 
     return (
-      <List.Row key={column.uuid} onClick={() => onColumnSelected(column.uuid)} isSelected={isSelected}>
-        <List.Cell width={300}>
-          <Field>
-            <TextInput
-              ref={ref}
-              onChange={updatedValue => onColumnChange({...column, target: updatedValue})}
-              onSubmit={() => onFocusNext(column.uuid)}
-              placeholder={translate('akeneo.tailored_export.column_list.column_row.target_placeholder')}
-              value={column.target}
+      <>
+        <List.Row key={column.uuid} onClick={() => onColumnSelected(column.uuid)} isSelected={isSelected}>
+          <List.Cell width={300}>
+            <Field>
+              <TextInput
+                ref={ref}
+                onChange={updatedValue => onColumnChange({...column, target: updatedValue})}
+                onSubmit={() => onFocusNext(column.uuid)}
+                placeholder={translate('akeneo.tailored_export.column_list.column_row.target_placeholder')}
+                value={column.target}
+              />
+              {targetErrors.map((error, index) => (
+                <Helper key={index} inline={true} level="error">
+                  {translate(error.messageTemplate, error.parameters)}
+                </Helper>
+              ))}
+            </Field>
+          </List.Cell>
+          <List.Cell width="auto">{translate('akeneo.tailored_export.column_list.column_row.no_source')}</List.Cell>
+          <List.RemoveCell>
+            <IconButton
+              ghost="borderless"
+              level="tertiary"
+              icon={<CloseIcon />}
+              title={translate('akeneo.tailored_export.column_list.column_row.remove')}
+              onClick={handleColumnRemove}
             />
-            {targetErrors.map((error, index) => (
-              <Helper key={index} inline={true} level="error">
-                {translate(error.messageTemplate, error.parameters)}
-              </Helper>
-            ))}
-          </Field>
-        </List.Cell>
-        <List.Cell width="auto">{translate('akeneo.tailored_export.column_list.column_row.no_source')}</List.Cell>
-        <List.RemoveCell>
-          <IconButton
-            ghost="borderless"
-            level="tertiary"
-            icon={<CloseIcon />}
-            title={translate('akeneo.tailored_export.column_list.column_row.remove')}
-            onClick={event => handleColumnRemove(event, column.uuid)}
-          />
-        </List.RemoveCell>
-      </List.Row>
+          </List.RemoveCell>
+        </List.Row>
+        {isDeleteModalOpen && (
+          <DeleteModal
+            title={translate('akeneo.tailored_export.column_list.title')}
+            onConfirm={handleConfirmColumnRemove}
+            onCancel={closeDeleteModal}
+          >
+            {translate('akeneo.tailored_export.column.delete_message')}
+          </DeleteModal>
+        )}
+      </>
+
     );
   }
 );
