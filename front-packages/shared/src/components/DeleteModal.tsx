73e7329574d81c71da2aa5fc7f109d0eb25ac6a3
Julien Sanchez <sumbobyboys@gmail.com>
2021-05-05 11:04:47 +0200

    RAC-689: add focus on delete modal

--- front-packages/shared/src/components/DeleteModal.tsx
+++ front-packages/shared/src/components/DeleteModal.tsx
@@ -1,5 +1,5 @@
-import React, {ReactNode} from 'react';
-import {Button, DeleteIllustration, Modal} from 'akeneo-design-system';
+import React, {ReactNode, useRef} from 'react';
+import {Button, DeleteIllustration, Modal, useAutoFocus} from 'akeneo-design-system';
 import {useTranslate} from '../hooks';
 
 type DeleteModalProps = {
@@ -12,6 +12,8 @@ type DeleteModalProps = {
 
 const DeleteModal = ({children, title, canConfirmDelete = true, onConfirm, onCancel}: DeleteModalProps) => {
   const translate = useTranslate();
+  const cancelRef = useRef(null);
+  useAutoFocus(cancelRef);
 
   return (
     <Modal closeTitle={translate('pim_common.close')} onClose={onCancel} illustration={<DeleteIllustration />}>
@@ -19,7 +21,7 @@ const DeleteModal = ({children, title, canConfirmDelete = true, onConfirm, onCan
       <Modal.Title>{translate('pim_common.confirm_deletion')}</Modal.Title>
       {children}
       <Modal.BottomButtons>
-        <Button level="tertiary" onClick={onCancel}>
+        <Button level="tertiary" onClick={onCancel} ref={cancelRef}>
           {translate('pim_common.cancel')}
         </Button>
         <Button level="danger" disabled={!canConfirmDelete} onClick={onConfirm}>
