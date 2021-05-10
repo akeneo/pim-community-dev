fead42b93b1c6c469cfcdaeb335ac76918cc0df8
Mathias Métayer <mathias.metayer@akeneo.com>
2021-05-10 16:16:06 +0200

    CPM-225: First Domain model

---
+++ src/Akeneo/Pim/TableAttribute/back/Domain/Config/TextColumn.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Pim\TableAttribute\Domain\Config;
+
+use Webmozart\Assert\Assert;
+
+class TextColumn extends ColumnDefinition
+{
+    private const DATATYPE = 'text';
+
+    // validation for text (min, max chars)
+
+    public static function fromNormalized(array $normalized): self
+    {
+        Assert::keyExists($normalized, 'code');
+        Assert::stringNotEmpty($normalized['code']);
+
+        $labels = $normalized['labels'] ?? [];
+        Assert::isArray($labels);
+
+        return new self($normalized['code'], self::DATATYPE, $labels);
+    }
+
+
+}
