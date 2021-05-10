af233767f3428248dec5560ed551acecb3f24381
Thomas Galvaing <thomas.galvaing@akeneo.com>
2021-05-10 17:35:19 +0200

    API-1554: add index to migration

--- upgrades/schema/Version_6_0_20210505180000_add_updated_to_category.php
+++ upgrades/schema/Version_6_0_20210505180000_add_updated_to_category.php
@@ -17,6 +17,7 @@ final class Version_6_0_20210505180000_add_updated_to_category extends AbstractM
     {
         $this->addSql("ALTER TABLE pim_catalog_category ADD updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '(DC2Type:datetime)'");
         $this->addSql('UPDATE pim_catalog_category SET updated = created');
+        $this->addSql('CREATE INDEX updated_idx ON pim_catalog_category (updated)');
     }
 
     public function down(Schema $schema): void
