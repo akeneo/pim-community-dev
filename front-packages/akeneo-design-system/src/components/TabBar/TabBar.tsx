7f81ea2a00b736e32ad92d0d86cb8baf73dc5959
Steven VAIDIE <steven.vaidie@gmail.com>
2021-05-12 17:54:50 +0200

    RAC-715: add overflow management

--- front-packages/akeneo-design-system/src/components/TabBar/TabBar.tsx
+++ front-packages/akeneo-design-system/src/components/TabBar/TabBar.tsx
@@ -1,13 +1,32 @@
-import React, {HTMLAttributes, ReactNode} from 'react';
+import React, {
+  Children,
+  HTMLAttributes,
+  isValidElement,
+  ReactNode,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import styled from 'styled-components';
 import {AkeneoThemedProps, getColor, getFontSize} from '../../theme';
+import {Dropdown, IconButton} from '../../../lib';
+import {MoreIcon} from '../../icons';
+import {useBooleanState} from '../../hooks';
+
+const Container = styled.div`
+  display: flex;
+  border-bottom: 1px solid ${getColor('grey', 80)};
+`;
 
 const TabBarContainer = styled.div`
   display: flex;
   gap: 10px;
   margin-bottom: 20px;
+  flex-grow: 1;
   height: 44px;
-  border-bottom: 1px solid ${getColor('grey', 80)};
+  flex-wrap: wrap;
+  overflow: hidden;
 `;
 
 type TabProps = {
@@ -23,6 +42,10 @@ type TabProps = {
    * Content of the Tab.
    */
   children: ReactNode;
+
+  parentRef: RefObject<HTMLDivElement>;
+
+  onVisibilityChange: (newVisibility: boolean) => void;
 };
 
 const TabContainer = styled.div<TabProps & AkeneoThemedProps>`
@@ -35,15 +58,40 @@ const TabContainer = styled.div<TabProps & AkeneoThemedProps>`
   font-size: ${getFontSize('big')};
   cursor: pointer;
   white-space: nowrap;
+  height: 100%;
+  box-sizing: border-box;
 
   &:hover {
     color: ${getColor('brand', 100)};
   }
 `;
 
-const Tab = ({children, isActive, ...rest}: TabProps) => {
+const Tab = ({children, isActive, parentRef, onVisibilityChange, ...rest}: TabProps) => {
+  const ref = useRef<HTMLDivElement>();
+  useEffect(() => {
+    if (undefined === ref.current) return;
+    const tabElement = ref.current;
+    const tabBarElement = parentRef.current;
+
+    const options = {
+      root: tabBarElement,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(event => {
+      onVisibilityChange(event[0].isIntersecting);
+    }, options);
+
+    observer.observe(tabElement);
+
+    return () => {
+      observer.unobserve(tabElement);
+    };
+  }, [onVisibilityChange, parentRef, ref]);
+
   return (
-    <TabContainer tabIndex={0} role="tab" aria-selected={isActive} isActive={isActive} {...rest}>
+    <TabContainer ref={ref} tabIndex={0} role="tab" aria-selected={isActive} isActive={isActive} {...rest}>
       {children}
     </TabContainer>
   );
@@ -60,10 +108,50 @@ type TabBarProps = {
  * TabBar is used to move from one content to another within the same context.
  */
 const TabBar = ({children, ...rest}: TabBarProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [hiddenElements, setHiddenElements] = useState<number[]>([]);
+  const [isOpen, open, close] = useBooleanState();
+
+  const decoratedChildren = Children.map(children, (child, index) => {
+    if (!React.isValidElement<TabProps>(child)) return child;
+
+    return React.cloneElement(child, {
+      parentRef: ref,
+      onVisibilityChange: (isVisible: boolean) => {
+        setHiddenElements(previousHiddenElements =>
+          isVisible
+            ? previousHiddenElements.filter(hiddenElement => hiddenElement !== index)
+            : [index, ...previousHiddenElements]
+        );
+      },
+    });
+  });
+
   return (
-    <TabBarContainer role="tablist" {...rest}>
-      {children}
-    </TabBarContainer>
+    <Container>
+      <TabBarContainer ref={ref} role="tablist" {...rest}>
+        {decoratedChildren}
+      </TabBarContainer>
+      {hiddenElements.length > 0 && (
+        <Dropdown>
+          <IconButton level="tertiary" ghost="borderless" icon={<MoreIcon />} title={'Open'} onClick={open} />
+          {isOpen && (
+            <Dropdown.Overlay verticalPosition="down" onClose={close}>
+              <Dropdown.Header>
+                <Dropdown.Title>Elements</Dropdown.Title>
+              </Dropdown.Header>
+              <Dropdown.ItemCollection>
+                {decoratedChildren?.map((child, index) => {
+                  if (!hiddenElements.includes(index) || !isValidElement<TabProps>(child)) return;
+
+                  return <Dropdown.Item key={index}>{child.props.children}</Dropdown.Item>;
+                })}
+              </Dropdown.ItemCollection>
+            </Dropdown.Overlay>
+          )}
+        </Dropdown>
+      )}
+    </Container>
   );
 };
 
