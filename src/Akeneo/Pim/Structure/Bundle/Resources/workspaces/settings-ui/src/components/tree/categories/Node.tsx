3d65a41d194349314b909c684be24c0ff4b54d2f
Olivier Pontier <olivier.pontier@akeneo.com>
2021-05-12 18:25:28 +0200

    Handle placeholder, remove useless comment and fix some linter issues

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/tree/categories/Node.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/tree/categories/Node.tsx
@@ -1,19 +1,16 @@
-import React, {FC, useCallback} from 'react';
+import React, {FC} from 'react';
 import {Tree} from '../../shared';
 import {CategoryTreeModel as CategoryTreeModel} from '../../../models';
-import {useCategoryTreeNode} from '../../../hooks';
+import {useCategoryTreeNode, useCountProductsBeforeDeleteCategory} from '../../../hooks';
 import {MoveTarget} from '../../providers';
 import {Button} from 'akeneo-design-system';
 import {useTranslate} from '@akeneo-pim-community/shared';
-import {useCountProductsBeforeDeleteCategory} from '../../../hooks';
-import {NodePreview} from './NodePreview';
 
 type Props = {
   id: number;
   label: string;
   sortable?: boolean;
   followCategory?: (category: CategoryTreeModel) => void;
-  // @todo define onCategoryMoved arguments
   onCategoryMoved?: () => void;
   addCategory?: (parentCode: string, onCreate: () => void) => void;
   deleteCategory?: (identifier: number, label: string, numberOfProducts: number, onDelete: () => void) => void;
@@ -40,27 +37,6 @@ const Node: FC<Props> = ({id, label, followCategory, addCategory, deleteCategory
   const translate = useTranslate();
   const countProductsBeforeDeleteCategory = useCountProductsBeforeDeleteCategory(id);
 
-  const isValidPreviewPosition = useCallback(
-    (position: number): boolean => {
-      if (draggedCategory === null || moveTarget === null || draggedCategory.status !== 'ready') {
-        return false;
-      }
-
-      if (draggedCategory.identifier === moveTarget.identifier) {
-        return false;
-      }
-
-      const previewPosition =
-        moveTarget.position === 'before' ? position - 1 : moveTarget.position === 'after' ? position + 1 : position;
-      const isOriginalPosition =
-        moveTarget.identifier === draggedCategory.identifier ||
-        (draggedCategory.parentId === moveTarget.parentId && draggedCategory.position === previewPosition);
-
-      return !isOriginalPosition;
-    },
-    [draggedCategory, moveTarget]
-  );
-
   if (node === undefined) {
     return null;
   }
@@ -81,15 +57,24 @@ const Node: FC<Props> = ({id, label, followCategory, addCategory, deleteCategory
         draggedCategory !== null &&
         draggedCategory.identifier !== node.identifier
       }
+      placeholderPosition={
+        moveTarget !== null &&
+        node.identifier === moveTarget.identifier &&
+        draggedCategory !== null &&
+        draggedCategory.identifier !== node.identifier &&
+        moveTarget.position !== 'in'
+          ? moveTarget.position === 'before'
+            ? 'top'
+            : 'bottom'
+          : undefined
+      }
       onOpen={async () => {
-        // @todo handle when children have already loaded
         if (node.childrenStatus !== 'idle') {
           return;
         }
         return loadChildren();
       }}
       draggable={sortable && node.type !== 'root'}
-      /* @todo Tree is draggable if Node is draggable and the current node is not the root */
       onDragStart={() => {
         // Root is not draggable
         if (!node?.parentId) {
@@ -139,17 +124,7 @@ const Node: FC<Props> = ({id, label, followCategory, addCategory, deleteCategory
           position: hoveredCategoryPosition,
           identifier: node?.identifier,
         });
-
-        // @todo if is a valid target, update the category tree state with the droppable node position and parent id
-        // @todo if the hover element is a parent category the position will be 0 and the parent id is the hover element
       }}
-      // onDragEnter={() => {
-      // @todo if the target is a descendant of the dragged item, drop is not valid
-      // }}
-      // onDragLeave={() => {
-      // @todo update the category tree state with a null droppable node position and null parent id
-      // so that we can handle the
-      // }}
       onDrop={async () => {
         if (draggedCategory && moveTarget) {
           moveTo(draggedCategory.identifier, moveTarget, resetMove);
@@ -189,13 +164,8 @@ const Node: FC<Props> = ({id, label, followCategory, addCategory, deleteCategory
           )}
         </Tree.Actions>
       )}
-      {children.map((child, index) => (
+      {children.map(child => (
         <React.Fragment key={`category-node-${id}-${child.identifier}`}>
-          {draggedCategory &&
-            moveTarget &&
-            moveTarget.identifier === child.identifier &&
-            moveTarget.position === 'before' &&
-            isValidPreviewPosition(index) && <NodePreview id={draggedCategory.identifier} />}
           <Node
             id={child.identifier}
             label={child.label}
@@ -204,11 +174,6 @@ const Node: FC<Props> = ({id, label, followCategory, addCategory, deleteCategory
             deleteCategory={deleteCategory}
             sortable={sortable}
           />
-          {draggedCategory &&
-            moveTarget &&
-            moveTarget.identifier === child.identifier &&
-            moveTarget.position === 'after' &&
-            isValidPreviewPosition(index) && <NodePreview id={draggedCategory.identifier} />}
         </React.Fragment>
       ))}
     </Tree>
