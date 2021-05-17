f5a23faf49344170b9e2c1b8574a5655c9cde1b1
rfauglas <ronan.fauglas@akeneo.com>
2021-05-17 18:07:51 +0200

    setup AggregatedAverageMaxNumberOfValuePerAsset query

---
+++ src/Akeneo/ReferenceEntity/tests/back/Integration/PublicApi/Analytics/AggregatedAverageMaxNumberOfValuesPerRecordTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Akeneo\ReferenceEntity\Integration\PublicApi\Analytics;
+
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AggregatedAverageMaxNumberOfValuesPerRecordTest
+{
+
+}
