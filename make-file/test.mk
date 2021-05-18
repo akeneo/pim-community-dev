f7554124514bdb539df1f69e0d47a9bb15cc9f83
Mathias Métayer <mathias.metayer@akeneo.com>
2021-05-18 15:17:23 +0200

    CPM-217: Run acceptance tests in CI

--- make-file/test.mk
+++ make-file/test.mk
@@ -95,6 +95,7 @@ acceptance-back: var/tests/behat #Doc: launch Behat acceptance tests
 	PIM_CONTEXT=rule-engine $(MAKE) rule-engine-acceptance-back
 	PIM_CONTEXT=connectivity-connection $(MAKE) connectivity-connection-acceptance-back
 	PIM_CONTEXT=tailored-export $(MAKE) acceptance-back
+	PIM_CONTEXT=table-attribute $(MAKE) table-attribute-acceptance-back
 	${PHP_RUN} vendor/bin/behat -p acceptance --format pim --out var/tests/behat --format progress --out std --colors
 	${PHP_RUN} vendor/bin/behat --config vendor/akeneo/pim-community-dev/behat.yml -p acceptance --no-interaction --format=progress --strict
 
