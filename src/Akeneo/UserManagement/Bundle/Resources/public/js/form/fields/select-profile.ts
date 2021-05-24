dd482e6228b0bc210bb451e2dafb7d2cc79a9b7d
Willy Mesnage <willy.mesnage@gmail.com>
2021-05-24 16:22:44 +0200

    CXP-679: add profile field in user edit form

---
+++ src/Akeneo/UserManagement/Bundle/Resources/public/js/form/fields/select-profile.ts
@@ -0,0 +1,43 @@
+import * as $ from 'jquery';
+import * as _ from 'underscore';
+const __ = require('oro/translator');
+const BaseSelect = require('pim/form/common/fields/select');
+const FetcherRegistry = require('pim/fetcher-registry');
+const containerTemplate = require('pim/templates/user/form/fields/select-profile-container');
+
+type InterfaceNormalizedProfile = {
+  code: string;
+  label: string;
+};
+
+class SelectProfile extends BaseSelect {
+  private containerTemplate = _.template(containerTemplate);
+
+  /**
+   * {@inheritdoc}
+   */
+  configure() {
+    return $.when(
+      BaseSelect.prototype.configure.apply(this, arguments),
+
+      FetcherRegistry.getFetcher('user-profiles')
+        .fetchAll()
+        .then((profiles: InterfaceNormalizedProfile[]) => {
+          this.config.choices = profiles;
+        })
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  formatChoices(profiles: InterfaceNormalizedProfile[]): {[key: string]: string} {
+    return profiles.reduce((result: {[key: string]: string}, profile: InterfaceNormalizedProfile) => {
+      result[profile.code] = __(profile.label);
+
+      return result;
+    }, {});
+  }
+}
+
+export = SelectProfile;
