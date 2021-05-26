0e5800d3504d610936b45656ecf3ce741e2584ed
Michel Tag <micheltag@akeneo.com>
2021-05-26 16:38:58 +0200

    Preparing version 5.0.27 (#14574)
    Preparing version 5.0.27
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.26';
+    const VERSION = '5.0.27';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
