5ade3b3efe92e99d533933e3e6ce9cf62880dac4
Samir <samir.boulil@akeneo.com>
2021-05-26 16:14:51 +0200

    RAC-721: Add internal php api query to fetch all category trees

---
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/Category/CategoryTree.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Category;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CategoryTree
+{
+    public const CODE = 'code';
+    public const LABELS = 'labels';
+
+    public string $code;
+    public array $labels = [];
+
+    public function normalize(): array
+    {
+        return [
+            self::CODE => $this->code,
+            self::LABELS => $this->labels
+        ];
+    }
+}
---
+++ tests/back/Pim/Structure/Integration/Category/Query/SqlFindCategoryTreesIntegration.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkeneoTest\Pim\Structure\Integration\Category;
+
+use Akeneo\Pim\Structure\Bundle\Query\PublicApi\Category\Sql\SqlFindCategoryTrees;
+use Akeneo\Pim\Structure\Component\Query\PublicApi\Category\CategoryTree;
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+
+final class SqlFindCategoryTreesIntegration extends TestCase
+{
+    public SqlFindCategoryTrees $sqlFindCategoryTrees;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->sqlFindCategoryTrees = $this->get('akeneo.pim.structure.query.find_category_trees');
+    }
+
+    /** @test */
+    public function it_fetches_the_category_trees(): void
+    {
+        $masterTree = new CategoryTree();
+        $masterTree->code = 'master';
+        $masterTree->labels = ['en_US' => 'Master catalog'];
+
+        $actual = $this->sqlFindCategoryTrees->execute();
+
+        $expected = [$masterTree];
+        $this->assertEquals($expected, $actual);
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+}
