162de3e241de0ebc57182385e0af26b79a72d959
Willy Mesnage <willy.mesnage@gmail.com>
2021-05-28 14:58:30 +0200

    CXP-681: fix lint front

--- src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-fetch-marketing-url.ts
+++ src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-fetch-marketing-url.ts
@@ -5,15 +5,10 @@ export const useFetchMarketingUrl = () => {
     const url = useRoute('akeneo_connectivity_connection_marketplace_url_rest_get');
 
     return useCallback(async () => {
-        const response = await fetch(
-            url,
-            {
-                method: 'GET',
-                headers: [
-                    ['X-Requested-With', 'XMLHttpRequest'],
-                ]
-            }
-        );
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: [['X-Requested-With', 'XMLHttpRequest']],
+        });
 
         return response.json();
     }, [url]);
