ae3e9b998e2aa93aae3d8020d97e89cc313bad09
Riff <riff.ventura@gmail.com>
2021-05-28 11:03:32 +0200

    CXP-680: fixed front unit tests and linting

--- src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-save-user.ts
+++ src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-save-user.ts
@@ -4,16 +4,16 @@ import {useCallback} from 'react';
 export const useSaveUser = (username: string) => {
     const url = useRoute('pim_user_user_rest_post', {identifier: username});
 
-    return useCallback(async (data: {profile: string}) => {
-        const response = await fetch(
-            url,
-            {
+    return useCallback(
+        async (data: {profile: string}) => {
+            const response = await fetch(url, {
                 method: 'POST',
                 headers: [['X-Requested-With', 'XMLHttpRequest']],
                 body: JSON.stringify(data),
-            }
-        );
+            });
 
-        return response.json();
-    }, [url]);
+            return response.json();
+        },
+        [url]
+    );
 };
--- src/Akeneo/Connectivity/Connection/front/src/shared/user/user-context.ts
+++ src/Akeneo/Connectivity/Connection/front/src/shared/user/user-context.ts
@@ -2,7 +2,7 @@ import {createContext} from 'react';
 import {User} from './user.interface';
 
 export const UserContext = createContext<User>({
-    get: <T> (data: string) => data as unknown as T,
+    get: <T>(data: string) => (data as unknown) as T,
     set: () => undefined,
-    refresh: () => Promise.resolve()
+    refresh: () => Promise.resolve(),
 });
