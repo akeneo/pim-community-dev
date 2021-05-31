ee7dc1a3398a140dab836df31934e5cef91b825b
Ayas Oukache <ayas.oukache@akeneo.com>
2021-05-31 11:28:11 +0200

    CPM-175 added translation

--- src/Akeneo/Pim/TableAttribute/front/src/attribute/DeleteColumnModal.tsx
+++ src/Akeneo/Pim/TableAttribute/front/src/attribute/DeleteColumnModal.tsx
@@ -20,12 +20,10 @@ const DeleteColumnModal: React.FC<DeleteColumnModalProps> = ({close, onDelete, c
     const [typedColumnCode, setTypedColumnCode] =  React.useState<string>("");
 
     const handleCancel = ()=>{
-        // In construction
         close();
     }
 
     const handleDelete = ()=>{
-        // In construction
         close();
         onDelete();
     }
@@ -36,30 +34,26 @@ const DeleteColumnModal: React.FC<DeleteColumnModalProps> = ({close, onDelete, c
             <Modal.SectionTitle color='brand'>
                 {translate('pim_table_attribute.form.attribute.table_attribute')}
             </Modal.SectionTitle>
-            <Modal.Title>{/*translate('pim_table_attribute.form.attribute.delete_column')*/} !Confirm deletion</Modal.Title>
+            <Modal.Title>{translate('pim_common.confirm_deletion')}</Modal.Title>
             <FieldsList>
                 <div>
-                    Are you sure you want to delete this column
+                    {translate('pim_table_attribute.form.attribute.confirm_delete')}
                 </div>
                 <Helper
                     level="info"
                 >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </Helper>
-                <Field label={`TODO:Please Type ${columnCode}`}>
+                <Field label={translate('pim_table_attribute.form.attribute.please_type',{columnDefinitionCode:columnCode})}>
                     <TextInput onChange={setTypedColumnCode}  value={typedColumnCode} />
                 </Field>
             </FieldsList>
             <Modal.BottomButtons>
                 <Button level='tertiary' onClick={handleCancel}>
-                    {/*translate('pim_common.create')*/}
-                    {/*translate('pim_common.delete')*/}
-                    Cancel
+                    {translate('pim_common.cancel')}
                 </Button>
                 <Button level='danger' onClick={handleDelete} disabled={ typedColumnCode !== columnCode }>
-                    {/*translate('pim_common.create')*/}
-                    {/*translate('pim_common.delete')*/}
-                    Delete
+                    {translate('pim_common.delete')}
                 </Button>
             </Modal.BottomButtons>
         </Modal>
