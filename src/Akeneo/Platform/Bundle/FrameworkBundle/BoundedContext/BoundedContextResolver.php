108a8b43e75eed94ffb741510cd85338a56eea84
jean-marie leroux <jmleroux.pro@gmail.com>
2021-06-01 16:35:53 +0200

    BH-561: Add Bounded Context to HTTP Response and FPM logs (#10895)

---
+++ src/Akeneo/Platform/Bundle/FrameworkBundle/BoundedContext/BoundedContextResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2014 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Akeneo\Platform\Bundle\FrameworkBundle\BoundedContext;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+
+/**
+ * @author JM Leroux <jean-marie.leroux@akeneo.com>
+ */
+class BoundedContextResolver
+{
+    private ControllerResolverInterface $controllerResolver;
+    /** @var string[] */
+    private array $boundedContexts;
+
+    public function __construct(ControllerResolverInterface $controllerResolver, array $boundedContexts)
+    {
+        $this->controllerResolver = $controllerResolver;
+        $this->boundedContexts = $boundedContexts;
+    }
+
+    public function fromRequest(Request $request): string
+    {
+        $controller = $this->controllerResolver->getController($request);
+
+        if (false === $controller) {
+            return 'Unknown request context';
+        }
+
+        $namespace = is_array($controller) ? get_class($controller[0]) : get_class($controller);
+
+        foreach ($this->boundedContexts as $namespaceStart => $boundedContext) {
+            if (str_starts_with($namespace, $namespaceStart)) {
+                return $boundedContext;
+            }
+        }
+
+        return sprintf('Unknown namespace context: %s', str_replace('\\', '\\\\', $namespace));
+    }
+}
--- src/Akeneo/Platform/Bundle/FrameworkBundle/DependencyInjection/PimEnterpriseFrameworkExtension.php
+++ src/Akeneo/Platform/Bundle/FrameworkBundle/DependencyInjection/PimEnterpriseFrameworkExtension.php
@@ -27,5 +27,6 @@ class PimEnterpriseFrameworkExtension extends BasePimInstallerExtension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('bounded_contexts.yml');
     }
 }
