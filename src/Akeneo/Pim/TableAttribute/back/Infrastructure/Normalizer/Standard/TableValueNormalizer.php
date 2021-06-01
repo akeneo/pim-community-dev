507baf88699c9b4ed9bad468dc3fefb01f1c4761
Mathias Métayer <mathias.metayer@akeneo.com>
2021-06-01 12:00:11 +0200

    WIP

--- src/Akeneo/Pim/TableAttribute/back/Infrastructure/Normalizer/Standard/TableValueNormalizer.php
+++ src/Akeneo/Pim/TableAttribute/back/Infrastructure/Normalizer/Standard/TableValueNormalizer.php
@@ -13,7 +13,29 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\TableAttribute\Infrastructure\Normalizer\Standard;
 
-class TableValueNormalizer
-{
+use Akeneo\Pim\Enrichment\Component\Product\Model\ValueInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Normalizer\Indexing\Value\AbstractProductValueNormalizer;
+use Akeneo\Pim\TableAttribute\Infrastructure\Value\TableValue;
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Webmozart\Assert\Assert;
 
+class TableValueNormalizer extends AbstractProductValueNormalizer implements CacheableSupportsMethodInterface
+{
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return $data instanceof TableValue && 'standard' === $format;
+    }
+
+    protected function getNormalizedData(ValueInterface $value): array
+    {
+        Assert::isInstanceOf($value, TableValue::class);
+
+        return $value->getData()->normalize();
+    }
 }
