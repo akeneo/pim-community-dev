a1d129f2cb058a0fd923d05725f92801d42dee68
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-06-08 14:39:39 +0200

    RAC-689: Fix asset manager select input type

--- src/Akeneo/Pim/TailoredExport/src/Infrastructure/Connector/Writer/File/Xlsx/ProductWriter.php
+++ src/Akeneo/Pim/TailoredExport/src/Infrastructure/Connector/Writer/File/Xlsx/ProductWriter.php
@@ -10,11 +10,11 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Akeneo\Pim\TailoredExport\Infrastructure\Connector\Writer\File\Xlsx;
 
 use Akeneo\Pim\TailoredExport\Infrastructure\Connector\Writer\File\AbstractItemMediaWriter;
 
 class ProductWriter extends AbstractItemMediaWriter
 {
-
 }
