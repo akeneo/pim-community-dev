77622f8fa7c62def6c62c50969839806ee07b873
Thomas Galvaing <thomas.galvaing@akeneo.com>
2021-06-09 10:37:29 +0200

    CXP-736: Remove the System > Connections redirection page
    * CXP-736: Remove the System > Connections redirection page
    
    * CXP-737: Remove the New tag on the marketplace menu

--- src/Akeneo/Connectivity/Connection/front/src/index.ts
+++ src/Akeneo/Connectivity/Connection/front/src/index.ts
@@ -2,7 +2,6 @@ import {Audit} from './infrastructure/Audit';
 import {WebhookSettings} from './infrastructure/WebhookSettings';
 import {ErrorManagement} from './infrastructure/ErrorManagement';
 import {Settings} from './infrastructure/Settings';
-import {RedirectConnectionSettingsToConnectMenu} from './infrastructure/RedirectConnectionSettingsToConnectMenu';
 import {Marketplace} from './infrastructure/Marketplace';
 
-export {Settings, Audit, ErrorManagement, WebhookSettings, RedirectConnectionSettingsToConnectMenu, Marketplace};
+export {Settings, Audit, ErrorManagement, WebhookSettings, Marketplace};
