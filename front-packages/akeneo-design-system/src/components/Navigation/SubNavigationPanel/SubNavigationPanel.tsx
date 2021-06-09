66fda5cf2eb7cb01ac9a1c2d54d7257692c54430
Paul Chasle <pchasle@users.noreply.github.com>
2021-06-09 18:25:19 +0200

    CXP-715: add SubNavigationPanel component to DSM

---
+++ front-packages/akeneo-design-system/src/components/Navigation/SubNavigationPanel/SubNavigationPanel.tsx
@@ -0,0 +1,90 @@
+import {PanelCloseIcon, PanelOpenIcon} from 'icons';
+import React from 'react';
+import styled from 'styled-components';
+import {AkeneoThemedProps, getColor} from 'theme';
+
+const Panel = styled.div<{isOpen: boolean} & AkeneoThemedProps>`
+  background-color: ${getColor('grey', 20)};
+  border-right: 1px solid ${getColor('grey', 80)};
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  max-width: ${({isOpen}) => (isOpen ? '280px' : '40px')};
+`;
+
+const PanelContent = styled.div`
+  flex-grow: 1;
+  padding: 20px;
+`;
+
+const CloseButton = styled.button`
+  align-items: center;
+  background: none;
+  border: none;
+  border-top: 1px solid ${getColor('grey', 80)};
+  cursor: pointer;
+  display: flex;
+  height: 54px;
+  margin: 0 20px;
+  padding: 0;
+`;
+
+const CloseIcon = styled(PanelCloseIcon)`
+  color: ${getColor('grey', 100)};
+  width: 15px;
+`;
+
+const OpenButton = styled.button`
+  align-items: center;
+  background: none;
+  border: none;
+  border-top: 1px solid ${getColor('grey', 80)};
+  cursor: pointer;
+  display: flex;
+  height: 54px;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+`;
+
+const OpenIcon = styled(PanelOpenIcon)`
+  color: ${getColor('grey', 100)};
+  width: 15px;
+`;
+
+type SubNavigationPanelProps = {
+  /**
+   * The content of the panel.
+   */
+  children?: React.ReactNode;
+
+  /**
+   * Whether or not the panel is open.
+   */
+  isOpen?: boolean;
+};
+
+/**
+ * TODO.
+ */
+const SubNavigationPanel = React.forwardRef<HTMLDivElement, SubNavigationPanelProps>(
+  ({children, isOpen = true, ...rest}: SubNavigationPanelProps, forwardedRef: React.Ref<HTMLDivElement>) => {
+    return (
+      <Panel ref={forwardedRef} isOpen={isOpen} {...rest}>
+        <PanelContent>{isOpen && children}</PanelContent>
+
+        {isOpen ? (
+          <CloseButton>
+            <CloseIcon />
+          </CloseButton>
+        ) : (
+          <OpenButton>
+            <OpenIcon />
+          </OpenButton>
+        )}
+      </Panel>
+    );
+  }
+);
+
+export {SubNavigationPanel};
