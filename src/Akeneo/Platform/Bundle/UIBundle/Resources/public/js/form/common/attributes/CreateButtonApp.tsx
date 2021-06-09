2543745f77512a50cd8e51d9097137dbcf2e00cc
pierallard <pierallard@gmail.com>
2021-06-09 11:52:32 +0200

    CPM-234: Fix lint

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/form/common/attributes/CreateButtonApp.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/form/common/attributes/CreateButtonApp.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import {
-  Modal,
-  Tile,
-  Tiles,
-  useBooleanState,
-  Button,
-  AddAttributeIcon,
-  IconProps
-} from "akeneo-design-system";
-import { useRouter, useTranslate } from "@akeneo-pim-community/shared";
-import styled from "styled-components";
+import {Modal, Tile, Tiles, useBooleanState, Button, AddAttributeIcon, IconProps} from 'akeneo-design-system';
+import {baseFetcher, useRouter, useTranslate} from '@akeneo-pim-community/shared';
+import styled from 'styled-components';
 import * as icons from 'akeneo-design-system/lib/icons';
 
 const ModalContent = styled.div`
@@ -18,74 +10,78 @@ const ModalContent = styled.div`
   max-height: calc(100vh - 120px);
   overflow-x: hidden;
   overflow-y: auto;
-`
+`;
 
 type AttributeType = string;
 
 type CreateButtonAppProps = {
   buttonTitle: string;
-  iconsMap: {[attributeType: string]: string},
+  iconsMap: {[attributeType: string]: string};
   isModalOpen?: boolean;
   onClick: (attributeType: AttributeType) => void;
-}
+};
 
-const CreateButtonApp: React.FC<CreateButtonAppProps> = ({
-  buttonTitle,
-  iconsMap,
-  isModalOpen = false,
-  onClick,
-}) => {
+const CreateButtonApp: React.FC<CreateButtonAppProps> = ({buttonTitle, iconsMap, isModalOpen = false, onClick}) => {
   const [isOpen, open, close] = useBooleanState(isModalOpen);
+  const [attributeTypes, setAttributeTypes] = React.useState<AttributeType[] | undefined>();
   const translate = useTranslate();
   const Router = useRouter();
 
-  const [attributeTypes, setAttributeTypes] = React.useState<AttributeType[] | undefined>();
-
   React.useEffect(() => {
     if (isOpen && !attributeTypes) {
-      const url = Router.generate('pim_enrich_attribute_type_index');
-      fetch(url).then((response) => response.json().then(attributeTypes => {
+      baseFetcher(Router.generate('pim_enrich_attribute_type_index')).then(attributeTypes => {
         const newAttributeTypes = Object.keys(attributeTypes);
         const sortedAttributeTypes = newAttributeTypes.sort((a, b) => {
-          return translate(`pim_enrich.entity.attribute.property.type.${a}`).localeCompare(translate(`pim_enrich.entity.attribute.property.type.${b}`));
+          return translate(`pim_enrich.entity.attribute.property.type.${a}`).localeCompare(
+            translate(`pim_enrich.entity.attribute.property.type.${b}`)
+          );
         });
 
         setAttributeTypes(sortedAttributeTypes);
-      }));
+      });
     }
   }, [isOpen]);
 
   const handleClick = (attributeType: AttributeType) => {
     close();
     onClick(attributeType);
-  }
+  };
 
-  return <>
-    {isOpen && attributeTypes && <Modal closeTitle={translate('pim_common.close')} onClose={close}>
-      <Modal.SectionTitle color="brand">{translate('pim_enrich.entity.attribute.module.create.button')}</Modal.SectionTitle>
-      <Modal.Title>{translate('pim_enrich.entity.attribute.property.type.choose')}</Modal.Title>
-      <ModalContent>
-        <Tiles>
-          {attributeTypes.map(attributeType => {
-            const castIcons = icons as {[component: string]: React.FC<IconProps>};
-            const component = iconsMap[attributeType] || 'AddAttributeIcon';
-            const Icon = castIcons[component] || AddAttributeIcon
-            return <Tile
-              onClick={() => handleClick(attributeType)}
-              key={attributeType}
-              icon={<Icon/>}
-              title={translate(`pim_enrich.entity.attribute.property.type.${attributeType}`)}
-            >
-              {translate(`pim_enrich.entity.attribute.property.type.${attributeType}`)}
-            </Tile>
-          })}
-        </Tiles>
-      </ModalContent>
-    </Modal>}
-    <Button id="attribute-create-button" onClick={open}>
-      {buttonTitle}
-    </Button>
-  </>
-}
+  const castIcons = icons as {[component: string]: React.FC<IconProps>};
 
-export { CreateButtonApp }
+  return (
+    <>
+      {isOpen && attributeTypes && (
+        <Modal closeTitle={translate('pim_common.close')} onClose={close}>
+          <Modal.SectionTitle color="brand">
+            {translate('pim_enrich.entity.attribute.module.create.button')}
+          </Modal.SectionTitle>
+          <Modal.Title>{translate('pim_enrich.entity.attribute.property.type.choose')}</Modal.Title>
+          <ModalContent>
+            <Tiles>
+              {attributeTypes.map(attributeType => {
+                const component = iconsMap[attributeType] || 'AddAttributeIcon';
+                const Icon = castIcons[component] || AddAttributeIcon;
+                return (
+                  <Tile
+                    onClick={() => handleClick(attributeType)}
+                    key={attributeType}
+                    icon={<Icon />}
+                    title={translate(`pim_enrich.entity.attribute.property.type.${attributeType}`)}
+                  >
+                    {translate(`pim_enrich.entity.attribute.property.type.${attributeType}`)}
+                  </Tile>
+                );
+              })}
+            </Tiles>
+          </ModalContent>
+        </Modal>
+      )}
+      <Button id="attribute-create-button" onClick={open}>
+        {buttonTitle}
+      </Button>
+    </>
+  );
+};
+
+export {CreateButtonApp};
