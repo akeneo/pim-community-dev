d866e7fc6f0a46f5f5d5983079cf2e3ae42d5cb3
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-06-11 17:08:17 +0200

    PLG-275: Add a notify and translations, rename newInvitedUsers in InvitedUserProvider.tsx

--- front-packages/invite-user/src/feature/hooks/useInvitedUsers.ts
+++ front-packages/invite-user/src/feature/hooks/useInvitedUsers.ts
@@ -1,7 +1,11 @@
 import {InvitedUser} from '../models';
 import {useState} from 'react';
+import {NotificationLevel, useNotify, useTranslate} from "@akeneo-pim-community/shared";
 
 const useInvitedUsers = () => {
+  const notify = useNotify();
+  const translate = useTranslate();
+
   const [invitedUsers, setInvitedUsers] = useState<InvitedUser[]>([
     {email: 'test1@test1.com', status: 'invited'},
     {email: 'test2@test2.com', status: 'active'},
@@ -15,6 +19,12 @@ const useInvitedUsers = () => {
     });
 
     setInvitedUsers([...invitedUsers, ...tempInvitedUsers]);
+
+    notify(
+        NotificationLevel.SUCCESS,
+        translate('free_trial.invite_users.invite_messages.success.title'),
+        translate('free_trial.invite_users.invite_messages.success.description')
+    );
   }
 
   return {invitedUsers, addInvitedUsers};
--- front-packages/invite-user/src/feature/providers/InvitedUserProvider.tsx
+++ front-packages/invite-user/src/feature/providers/InvitedUserProvider.tsx
@@ -2,22 +2,22 @@ import React, {createContext, FC} from 'react';
 import {InvitedUser} from '../models';
 
 type InvitedUserState = {
-  inviteNewUsers: (emails: string[]) => void;
+  newInvitedUsers: (emails: string[]) => void;
   retrieveInvitedUsers: () => InvitedUser[];
 };
 
 const InvitedUserContext = createContext<InvitedUserState>({
-  inviteNewUsers: () => {},
+  newInvitedUsers: () => {},
   retrieveInvitedUsers: () => [],
 });
 
 type Props = {
-  inviteNewUsers: (emails: string[]) => void;
+  newInvitedUsers: (emails: string[]) => void;
   retrieveInvitedUsers: () => InvitedUser[];
 };
 
-const InvitedUserProvider: FC<Props> = ({children, inviteNewUsers, retrieveInvitedUsers}) => {
-  return <InvitedUserContext.Provider value={{inviteNewUsers, retrieveInvitedUsers}}>{children}</InvitedUserContext.Provider>;
+const InvitedUserProvider: FC<Props> = ({children, newInvitedUsers, retrieveInvitedUsers}) => {
+  return <InvitedUserContext.Provider value={{newInvitedUsers, retrieveInvitedUsers}}>{children}</InvitedUserContext.Provider>;
 };
 
 export {InvitedUserProvider, InvitedUserContext};
