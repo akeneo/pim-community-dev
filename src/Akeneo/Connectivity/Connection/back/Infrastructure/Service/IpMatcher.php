9b19247225d7d061377053d45c352ad3d19b1323
Tseho <quentin.favrie@gmail.com>
2021-06-11 17:21:24 +0200

    API-1602: missing license header

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Service/IpMatcher.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Service/IpMatcher.php
@@ -7,6 +7,10 @@ namespace Akeneo\Connectivity\Connection\Infrastructure\Service;
 use Akeneo\Connectivity\Connection\Application\Webhook\Service\IpMatcherInterface;
 use Symfony\Component\HttpFoundation\IpUtils;
 
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class IpMatcher implements IpMatcherInterface
 {
     /**
--- src/Akeneo/Connectivity/Connection/back/tests/Integration/Mock/FakeDnsLookup.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Integration/Mock/FakeDnsLookup.php
@@ -6,6 +6,10 @@ namespace Akeneo\Connectivity\Connection\Tests\Integration\Mock;
 
 use Akeneo\Connectivity\Connection\Application\Webhook\Service\DnsLookupInterface;
 
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class FakeDnsLookup implements DnsLookupInterface
 {
     public function ip(string $host): ?string
