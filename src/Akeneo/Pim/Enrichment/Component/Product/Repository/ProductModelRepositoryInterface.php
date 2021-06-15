7d8eb0d2a5f3b9a62549ef7cab236fd4bc3bc4fa
anaisbaunelemaire <anais.lemaire@akeneo.com>
2021-06-15 16:54:07 +0200

    PIM-9911: Fix CI

--- src/Akeneo/Pim/Enrichment/Component/Product/Repository/ProductModelRepositoryInterface.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Repository/ProductModelRepositoryInterface.php
@@ -72,7 +72,7 @@ interface ProductModelRepositoryInterface extends
      */
     public function findChildrenProducts(ProductModelInterface $productModel): array;
 
-    public function findFirstCreatedVariantProductModel(ProductModelInterface $productModel): ?ProductModelInterface
+    public function findFirstCreatedVariantProductModel(ProductModelInterface $productModel): ?ProductModelInterface;
 
     /**
      * Get root products models after the one provided. Mainly used to iterate
