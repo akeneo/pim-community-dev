0877fdae4002e5e13044072df4f60613220ec43a
pierallard <pierallard@gmail.com>
2021-06-15 12:05:12 +0200

    CPM-179: WIP border 1px solid red

---
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.unit.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {TableInput} from './TableInput';
+import {render, screen} from '../../storybook/test-util';
+
+test('it renders its children properly', () => {
+  render(<TableInput>Table content</TableInput>);
+
+  expect(screen.getByText('Table content')).toBeInTheDocument();
+});
+
+// Those tests should pass directly if you follow the contributing guide.
+// If you add required props to your Component, these tests will fail
+// and you will need to add these required props here as well
+test('TableInput supports forwardRef', () => {
+  const ref = {current: null};
+
+  render(<TableInput ref={ref} />);
+  expect(ref.current).not.toBe(null);
+});
+
+test('TableInput supports ...rest props', () => {
+  render(<TableInput data-testid="my_value" />);
+  expect(screen.getByTestId('my_value')).toBeInTheDocument();
+});
