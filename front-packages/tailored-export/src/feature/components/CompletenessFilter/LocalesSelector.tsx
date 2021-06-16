f29cfde2ca530eada02f745f38067b84c0dabcf7
Samir <samir.boulil@akeneo.com>
2021-06-16 16:39:13 +0200

    RAC-609: Add traductions

--- front-packages/tailored-export/src/feature/components/CompletenessFilter/LocalesSelector.tsx
+++ front-packages/tailored-export/src/feature/components/CompletenessFilter/LocalesSelector.tsx
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react';
 import {useTranslate, LocaleCode, Section, getAllLocalesFromChannels, Locale} from '@akeneo-pim-community/shared';
 import {Field, MultiSelectInput} from 'akeneo-design-system';
 import {useChannels} from '../../hooks';
 
 type LocalesSelectorProps = {
-    locales: LocaleCode[];
-    onChange: (newLocales: LocaleCode[]) => void;
-}
+  locales: LocaleCode[];
+  onChange: (newLocales: LocaleCode[]) => void;
+};
 const LocalesSelector = ({locales, onChange}: LocalesSelectorProps) => {
-    const translate = useTranslate();
-    const channels = useChannels();
-    const availableLocales = getAllLocalesFromChannels(channels);
+  const translate = useTranslate();
+  const channels = useChannels();
+  const availableLocales = getAllLocalesFromChannels(channels);
 
-    if (availableLocales.length === 0) return null;
+  if (availableLocales.length === 0) return null;
 
-    return (
-        <Section>
-            <Field label={translate('pim_connector.export.completeness.locale_selector.label')}>
-                <MultiSelectInput
-                    emptyResultLabel={translate('pim_common.no_result')}
-                    onChange={onChange}
-                    openLabel={translate('pim_common.open')}
-                    placeholder="Find traduction"
-                    removeLabel="Find traduction remove"
-                    value={locales}
-                >
-                    {availableLocales.map((locale: Locale) => (
-                        <MultiSelectInput.Option key={locale.code} value={locale.code}>
-                            {locale.label}
-                        </MultiSelectInput.Option>
-                    ))}
-                </MultiSelectInput>
-            </Field>
-        </Section>
-    );
+  return (
+    <Section>
+      <Field label={translate('akeneo.tailored_export.filters.completeness.locales.label')}>
+        <MultiSelectInput
+          emptyResultLabel={translate('pim_common.no_result')}
+          onChange={onChange}
+          openLabel={translate('pim_common.open')}
+          placeholder={translate('akeneo.tailored_export.filters.completeness.locales.placeholder')}
+          removeLabel={translate('akeneo.tailored_export.filters.completeness.locales.remove')}
+          value={locales}
+        >
+          {availableLocales.map((locale: Locale) => (
+            <MultiSelectInput.Option key={locale.code} value={locale.code}>
+              {locale.label}
+            </MultiSelectInput.Option>
+          ))}
+        </MultiSelectInput>
+      </Field>
+    </Section>
+  );
 };
 
 export {LocalesSelector};
--- front-packages/tailored-export/src/feature/components/CompletenessFilter/OperatorSelector.tsx
+++ front-packages/tailored-export/src/feature/components/CompletenessFilter/OperatorSelector.tsx
@@ -1,45 +1,48 @@
-import React from "react";
+import React from 'react';
 import {useTranslate, Section} from '@akeneo-pim-community/shared';
 import {Field, SelectInput} from 'akeneo-design-system';
 
 const availableOperators = [
-    'ALL',
-    'GREATER OR EQUALS THAN ON AT LEAST ONE LOCALE',
-    'GREATER OR EQUALS THAN ON ALL LOCALES',
-    'LOWER THAN ON ALL LOCALES'
+  'ALL',
+  'GREATER OR EQUALS THAN ON AT LEAST ONE LOCALE',
+  'GREATER OR EQUALS THAN ON ALL LOCALES',
+  'LOWER THAN ON ALL LOCALES',
 ];
 type Operator = typeof availableOperators[number];
 const isValidOperator = (operatorToCheck: unknown): operatorToCheck is Operator => {
-    return typeof operatorToCheck === 'string' && availableOperators.includes(operatorToCheck);
-}
+  return typeof operatorToCheck === 'string' && availableOperators.includes(operatorToCheck);
+};
 
 type OperatorSelectorProps = {
-    operator: Operator;
-    onChange: (newOperator: Operator) => void;
-}
+  operator: Operator;
+  onChange: (newOperator: Operator) => void;
+};
 const OperatorSelector = ({operator, onChange}: OperatorSelectorProps) => {
-    const translate = useTranslate();
+  const translate = useTranslate();
 
-    return (
-        <Section>
-            <Field label={translate('pim_connector.export.completeness.selector.label')}>
-                <SelectInput
-                    clearable={false}
-                    emptyResultLabel={translate('pim_common.no_result')}
-                    openLabel={translate('pim_common.open')}
-                    value={operator}
-                    onChange={newOperator => isValidOperator(newOperator) && onChange(newOperator)}
-                >
-                    {availableOperators.map((operator: string)=> (
-                        <SelectInput.Option key={operator} title={translate(`pim_enrich.export.product.filter.completeness.operators.${operator}`)} value={operator}>
-                            {translate(`pim_enrich.export.product.filter.completeness.operators.${operator}`)}
-                        </SelectInput.Option>
-                    ))}
-                </SelectInput>
-            </Field>
-        </Section>
-
-    )
+  return (
+    <Section>
+      <Field label="">
+        <SelectInput
+          clearable={false}
+          emptyResultLabel={translate('pim_common.no_result')}
+          openLabel={translate('pim_common.open')}
+          value={operator}
+          onChange={newOperator => isValidOperator(newOperator) && onChange(newOperator)}
+        >
+          {availableOperators.map((operator: string) => (
+            <SelectInput.Option
+              key={operator}
+              title={translate(`pim_enrich.export.product.filter.completeness.operators.${operator}`)}
+              value={operator}
+            >
+              {translate(`pim_enrich.export.product.filter.completeness.operators.${operator}`)}
+            </SelectInput.Option>
+          ))}
+        </SelectInput>
+      </Field>
+    </Section>
+  );
 };
 
 export {OperatorSelector};
