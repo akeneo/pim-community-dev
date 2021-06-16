811cf42435156c339485554eb315f66de0753c18
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-06-16 10:39:44 +0200

    PLG-275: Feature flag in routing.yml

--- src/Akeneo/FreeTrial/back/Infrastructure/Controller/RetrieveUsersController.php
+++ src/Akeneo/FreeTrial/back/Infrastructure/Controller/RetrieveUsersController.php
@@ -6,9 +6,7 @@ namespace Akeneo\FreeTrial\Infrastructure\Controller;
 
 use Akeneo\FreeTrial\Domain\Model\InvitedUser;
 use Akeneo\FreeTrial\Domain\Query\GetInvitedUsersQuery;
-use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
@@ -18,20 +16,13 @@ class RetrieveUsersController
 {
     private GetInvitedUsersQuery $getInvitedUsersQuery;
 
-    private FeatureFlag $freeTrialFeature;
-
-    public function __construct(GetInvitedUsersQuery $getInvitedUsersQuery, FeatureFlag $freeTrialFeature)
+    public function __construct(GetInvitedUsersQuery $getInvitedUsersQuery)
     {
         $this->getInvitedUsersQuery = $getInvitedUsersQuery;
-        $this->freeTrialFeature = $freeTrialFeature;
     }
 
     public function __invoke(): JsonResponse
     {
-        if (!$this->freeTrialFeature->isEnabled()) {
-            throw new NotFoundHttpException();
-        }
-
         $invitedUsers = $this->getInvitedUsersQuery->execute();
 
         $invitedUsers = array_map(fn (InvitedUser $invitedUser) => $invitedUser->toArray(), $invitedUsers);
--- src/Akeneo/FreeTrial/back/Infrastructure/Controller/SaveUsersController.php
+++ src/Akeneo/FreeTrial/back/Infrastructure/Controller/SaveUsersController.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Akeneo\FreeTrial\Infrastructure\Controller;
 
 use Akeneo\FreeTrial\Application\InviteUser;
-use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
@@ -18,20 +16,13 @@ class SaveUsersController
 {
     private InviteUser $inviteUser;
 
-    private FeatureFlag $freeTrialFeature;
-
-    public function __construct(InviteUser $inviteUser, FeatureFlag $freeTrialFeature)
+    public function __construct(InviteUser $inviteUser)
     {
         $this->inviteUser = $inviteUser;
-        $this->freeTrialFeature = $freeTrialFeature;
     }
 
     public function __invoke(Request $request): JsonResponse
     {
-        if (!$this->freeTrialFeature->isEnabled()) {
-            throw new NotFoundHttpException();
-        }
-
         $emails = json_decode($request->getContent());
 
         foreach ($emails as $email) {
