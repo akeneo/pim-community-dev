39dfc3a01225f78397eda3b6ef45ccdaa4929b2f
Riff <riff.ventura@gmail.com>
2021-06-16 09:53:58 +0200

    API-1659 Created framework bundle

--- src/Akeneo/Platform/Bundle/UIBundle/PimUIBundle.php
+++ src/Akeneo/Platform/Bundle/UIBundle/PimUIBundle.php
@@ -33,35 +33,4 @@ class PimUIBundle extends Bundle
             ->addCompilerPass(new RegisterGenericProvidersPass(new ReferenceFactory(), 'filter'))
         ;
     }
-
-    /**
-     * {@inheritdoc }
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        $this->setupRequestContext();
-    }
-
-    private function setupRequestContext(): void
-    {
-        $url = getenv('AKENEO_PIM_URL');
-        $scheme = parse_url($url, \PHP_URL_SCHEME);
-        $host = parse_url($url, \PHP_URL_HOST);
-        $port = parse_url($url, \PHP_URL_PORT);
-
-        /** @var RequestContext $requestContext */
-        $requestContext = $this->container->get('router')->getContext();
-        $requestContext->setScheme($scheme);
-        $requestContext->setHost($host);
-        switch (strtolower($scheme)) {
-            case 'https':
-                $requestContext->setHttpsPort($port);
-                break;
-            case 'http':
-                $requestContext->setHttpPort($port);
-                break;
-        }
-    }
 }
