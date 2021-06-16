f473c143886475cbbf7e9c658de4095d355a2359
pierallard <pierallard@gmail.com>
2021-06-16 09:57:16 +0200

    CPM-179: Fix border 1px solid red

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.tsx
@@ -1,17 +1,17 @@
 import React, {ReactNode} from 'react';
 import styled from 'styled-components';
 import { Override } from '../../../shared';
+import { TableInputHeader } from "./TableInputHeader/TableInputHeader";
+import { TableInputHeaderCell } from "./TableInputHeaderCell/TableInputHeaderCell";
+import { TableInputBody } from "./TableInputBody/TableInputBody";
+import { TableInputCell } from "./TableInputCell/TableInputCell";
+import { TableInputRow } from "./TableInputRow/TableInputRow";
+import { TableInputTextInput } from "./TableInputTextInput/TableInputTextInput";
+import { TableInputNumberInput } from "./TableInputNumberInput/TableInputNumberInput";
 
-//TODO be sure to select the appropriate container element here
-const TableInputContainer = styled.div<{level: string}>``;
-const TableInputCellContainer = styled.span`
-  display: inline-block;
-  width: 200px;
-  height: 50px;
-  border: 1px solid red;
-`;
-const TableInputRowContainer = styled.div`
-  display: flex;
+const TableInputContainer = styled.table`
+  border-spacing: 0 4px;
+  width: 100%;
 `;
 
 type TableInputProps = Override<
@@ -20,66 +20,27 @@ type TableInputProps = Override<
     /**
      * TODO.
      */
-    level?: 'primary' | 'warning' | 'danger';
-
-    /**
-     * TODO.
-     */
     children?: ReactNode;
   }
 >;
 
-const TableInputBody: React.FC<{}> = ({children}) => {
-  return <>
-    {children}
-  </>;
-};
-
-const TableInputRow: React.FC<{}> = ({children}) => {
-  return <TableInputRowContainer>
-    {children}
-  </TableInputRowContainer>;
-};
-
-const TableInputCell: React.FC<{}> = ({children}) => {
-  return <TableInputCellContainer>
-    {children}
-  </TableInputCellContainer>;
-};
-
-const TableInputHeaderCell: React.FC<{}> = ({children}) => {
-  return <TableInputCellContainer>
-    {children}
-  </TableInputCellContainer>;
-};
-
-const TableInputHeader: React.FC<{}> = ({children}) => {
-  return <TableInputRowContainer>
-    {children}
-  </TableInputRowContainer>;
-};
-
 /**
  * TODO.
  */
-const TableInput = ({ level = 'primary', children, ...rest }: TableInputProps) => {
+const TableInput = ({ children, ...rest }: TableInputProps) => {
   return (
-    <TableInputContainer level={level} {...rest}>
+    <TableInputContainer {...rest}>
       {children}
     </TableInputContainer>
   );
 };
-
-TableInputHeader.displayName = 'TableInput.Header';
-TableInputHeaderCell.displayName = 'TableInput.HeaderCell';
-TableInputBody.displayName = 'TableInput.Body';
-TableInputRow.displayName = 'TableInput.Row';
-TableInputCell.displayName = 'TableInput.Cell';
 
 TableInput.Header = TableInputHeader;
 TableInput.HeaderCell = TableInputHeaderCell;
 TableInput.Body = TableInputBody;
 TableInput.Row = TableInputRow;
 TableInput.Cell = TableInputCell;
+TableInput.TextInput = TableInputTextInput;
+TableInput.NumberInput = TableInputNumberInput;
 
 export {TableInput};
