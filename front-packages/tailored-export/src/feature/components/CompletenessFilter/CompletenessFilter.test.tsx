fdca151ac58f54fad695f9a87830be27930cf352
Julien Sanchez <sumbobyboys@gmail.com>
2021-06-17 15:41:24 +0200

    RAC-609: fix unit tests

--- front-packages/tailored-export/src/feature/components/CompletenessFilter/CompletenessFilter.test.tsx
+++ front-packages/tailored-export/src/feature/components/CompletenessFilter/CompletenessFilter.test.tsx
@@ -1,23 +1,134 @@
 import React from 'react';
-import {screen} from '@testing-library/react';
-import {renderWithProviders} from '@akeneo-pim-community/shared';
+import {screen, act} from '@testing-library/react';
+import {renderWithProviders, Channel} from '@akeneo-pim-community/shared';
 import {CompletenessFilter, Operator} from './CompletenessFilter';
+import {FetcherContext} from '../../contexts';
+import {Attribute} from '../../models';
+import userEvent from '@testing-library/user-event';
+
+const fetchers = {
+  attribute: {
+    fetchByIdentifiers: (): Promise<Attribute[]> =>
+      new Promise(resolve => {
+        act(() => {
+          resolve([]);
+        });
+      }),
+  },
+  channel: {
+    fetchAll: (): Promise<Channel[]> =>
+      new Promise(resolve => {
+        act(() => {
+          resolve([
+            {
+              code: 'Ecommerce',
+              labels: {},
+              locales: [
+                {
+                  code: 'en_US',
+                  label: 'English',
+                  region: '',
+                  language: '',
+                },
+                {
+                  code: 'fr_FR',
+                  label: 'French',
+                  region: '',
+                  language: '',
+                },
+                {
+                  code: 'br_FR',
+                  label: 'Breton',
+                  region: '',
+                  language: '',
+                },
+              ],
+            },
+          ]);
+        });
+      }),
+  },
+};
 
 const operatorsAndVisiblity = [
-    {operator: 'ALL', shouldAppear: false},
-    {operator: 'GREATER OR EQUALS THAN ON AT LEAST ONE LOCALE', shouldAppear: true},
-    {operator: 'GREATER OR EQUALS THAN ON ALL LOCALES', shouldAppear: true},
-    {operator: 'LOWER THAN ON ALL LOCALES', shouldAppear: true},
+  {operator: 'ALL', shouldAppear: false},
+  {operator: 'GREATER OR EQUALS THAN ON AT LEAST ONE LOCALE', shouldAppear: true},
+  {operator: 'GREATER OR EQUALS THAN ON ALL LOCALES', shouldAppear: true},
+  {operator: 'LOWER THAN ON ALL LOCALES', shouldAppear: true},
 ] as const;
-test.each(operatorsAndVisiblity)
-('it displays the locale selector depending on the operator', ({operator, shouldAppear}: {operator: Operator, shouldAppear: boolean}) => {
-    renderWithProviders(<CompletenessFilter operator={operator} locales={['fr_FR', 'en_US']} onOperatorChange={() => {}} onLocalesChange={() => {}}/>);
 
-    expect(screen.queryByText('pim_connector.export.completeness.selector.label')).toBeInTheDocument();
+test.each(operatorsAndVisiblity)(
+  'it displays the locale selector depending on the operator',
+  async ({operator, shouldAppear}: {operator: Operator; shouldAppear: boolean}) => {
+    await act(async () => {
+      renderWithProviders(
+        <FetcherContext.Provider value={fetchers}>
+          <CompletenessFilter
+            operator={operator}
+            locales={['fr_FR', 'en_US']}
+            onOperatorChange={() => {}}
+            onLocalesChange={() => {}}
+          />
+        </FetcherContext.Provider>
+      );
+    });
+
+    expect(screen.getByText(`pim_enrich.export.product.filter.completeness.operators.${operator}`)).toBeInTheDocument();
 
     if (shouldAppear) {
-        expect(screen.queryByText('pim_connector.export.completeness.locale_selector.label')).toBeInTheDocument();
+      expect(screen.getByText('akeneo.tailored_export.filters.completeness.locales.label')).toBeInTheDocument();
     } else {
-        expect(screen.queryByText('pim_connector.export.completeness.locale_selector.label')).not.toBeInTheDocument();
+      expect(screen.queryByText('akeneo.tailored_export.filters.completeness.locales.label')).not.toBeInTheDocument();
     }
+  }
+);
+
+test('it can switch operator', async () => {
+  const handleOperatorChange = jest.fn();
+
+  await act(async () => {
+    renderWithProviders(
+      <FetcherContext.Provider value={fetchers}>
+        <CompletenessFilter
+          operator="ALL"
+          locales={['fr_FR', 'en_US']}
+          onOperatorChange={handleOperatorChange}
+          onLocalesChange={jest.fn()}
+        />
+      </FetcherContext.Provider>
+    );
+  });
+
+  const openDropdownButton = screen.getByTitle(`pim_common.open`);
+  userEvent.click(openDropdownButton);
+  const greaterThanButton = screen.getByText(
+    `pim_enrich.export.product.filter.completeness.operators.GREATER OR EQUALS THAN ON ALL LOCALES`
+  );
+  await userEvent.click(greaterThanButton);
+
+  expect(handleOperatorChange).toHaveBeenCalledWith('GREATER OR EQUALS THAN ON ALL LOCALES');
+});
+
+test('it can  select locales', async () => {
+  const handleLocalesChange = jest.fn();
+
+  await act(async () => {
+    renderWithProviders(
+      <FetcherContext.Provider value={fetchers}>
+        <CompletenessFilter
+          operator="GREATER OR EQUALS THAN ON ALL LOCALES"
+          locales={[]}
+          onOperatorChange={jest.fn()}
+          onLocalesChange={handleLocalesChange}
+        />
+      </FetcherContext.Provider>
+    );
+  });
+
+  const openDropdownButton = screen.getAllByTitle('pim_common.open')[1];
+  userEvent.click(openDropdownButton);
+  const greaterThanButton = screen.getByText('English');
+  await userEvent.click(greaterThanButton);
+
+  expect(handleLocalesChange).toHaveBeenCalledWith(['en_US']);
 });
--- front-packages/tailored-export/src/feature/components/CompletenessFilter/LocalesSelector.test.tsx
+++ front-packages/tailored-export/src/feature/components/CompletenessFilter/LocalesSelector.test.tsx
@@ -1,58 +1,58 @@
 import {act, screen} from '@testing-library/react';
 import {Channel, renderWithProviders as baseRender} from '@akeneo-pim-community/shared';
-import {LocalesSelector} from "./LocalesSelector";
-import React, {ReactNode} from "react";
-import {FetcherContext} from "../../contexts";
-import userEvent from "@testing-library/user-event";
+import {LocalesSelector} from './LocalesSelector';
+import React, {ReactNode} from 'react';
+import {FetcherContext} from '../../contexts';
+import userEvent from '@testing-library/user-event';
 
 const channels: Channel[] = [
-    {
-        code: 'ecommerce',
-        labels: {},
-        locales: [
-            {
-                code: 'en_US',
-                label: 'English (American)',
-                region: 'US',
-                language: 'en',
-            },
-            {
-                code: 'fr_FR',
-                label: 'French',
-                region: 'FR',
-                language: 'fr',
-            },
-        ],
-        category_tree: '',
-        conversion_units: [],
-        currencies: [],
-        meta: {
-            created: '',
-            form: '',
-            id: 1,
-            updated: '',
-        },
+  {
+    code: 'ecommerce',
+    labels: {},
+    locales: [
+      {
+        code: 'en_US',
+        label: 'English (American)',
+        region: 'US',
+        language: 'en',
+      },
+      {
+        code: 'fr_FR',
+        label: 'French',
+        region: 'FR',
+        language: 'fr',
+      },
+    ],
+    category_tree: '',
+    conversion_units: [],
+    currencies: [],
+    meta: {
+      created: '',
+      form: '',
+      id: 1,
+      updated: '',
     },
+  },
 ];
 const fetchers = {
-    channel: {fetchAll: (): Promise<Channel[]> => Promise.resolve(channels)},
+  channel: {fetchAll: (): Promise<Channel[]> => Promise.resolve(channels)},
 };
 const renderWithProviders = async (node: ReactNode) =>
-    await act(async () => void baseRender(<FetcherContext.Provider value={fetchers}>{node})</FetcherContext.Provider>));
+  await act(async () => void baseRender(<FetcherContext.Provider value={fetchers}>{node})</FetcherContext.Provider>));
 
-test('it displays the selected locales', async () =>{
-    await renderWithProviders(<LocalesSelector locales={['en_US']} onChange={() => {}}/>);
+test('it displays the selected locales', async () => {
+  await renderWithProviders(<LocalesSelector locales={['en_US']} onChange={() => {}} />);
 
-    expect(screen.queryByText('pim_connector.export.completeness.locale_selector.label')).toBeInTheDocument();
-    expect(screen.queryByText('English (American)')).toBeInTheDocument();
-})
+  expect(screen.queryByText('akeneo.tailored_export.filters.completeness.locales.label')).toBeInTheDocument();
+  expect(screen.queryByText('English (American)')).toBeInTheDocument();
+});
 
 test('it notifies when a locale is added to the selection', async () => {
-    const onLocalesSelectionChange = jest.fn();
-    await renderWithProviders(<LocalesSelector locales={['fr_FR']} onChange={onLocalesSelectionChange} />);
+  const onLocalesSelectionChange = jest.fn();
+  await renderWithProviders(<LocalesSelector locales={['fr_FR']} onChange={onLocalesSelectionChange} />);
 
-    userEvent.click(screen.getByText('pim_connector.export.completeness.locale_selector.label'));
-    userEvent.click(screen.getByText('English (American)'));
+  userEvent.click(screen.getByText('akeneo.tailored_export.filters.completeness.locales.label'));
+  userEvent.click(screen.getByText('English (American)'));
 
-    expect(onLocalesSelectionChange).toHaveBeenCalledWith(['fr_FR', 'en_US']);
-})
+  expect(onLocalesSelectionChange).toHaveBeenCalledWith(['fr_FR', 'en_US']);
+});
