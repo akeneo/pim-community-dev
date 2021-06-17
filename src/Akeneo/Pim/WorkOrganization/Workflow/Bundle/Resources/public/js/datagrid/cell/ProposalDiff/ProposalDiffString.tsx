273172560a2cb441df30d9129bf45b18a7c3b0d3
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-06-17 16:14:45 +0200

    PIM-9913 Proposal UI: Do not calculate diff for long texts at first render (#11054)

--- src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Resources/public/js/datagrid/cell/ProposalDiff/ProposalDiffString.tsx
+++ src/Akeneo/Pim/WorkOrganization/Workflow/Bundle/Resources/public/js/datagrid/cell/ProposalDiff/ProposalDiffString.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import {diffChars} from 'diff';
+import {diffChars, Change} from 'diff';
 import {ProposalChangeAccessor} from '../ProposalChange';
+import styled from 'styled-components';
+import {useTranslate} from '@akeneo-pim-community/shared';
+import {ArrowDownIcon, LoaderIcon} from 'akeneo-design-system';
+
+const CHAR_LIMIT = 512;
+
+const ExpandButton = styled.span`
+  cursor: pointer;
+`;
 
 type ProposalDiffStringProps = {
   accessor: ProposalChangeAccessor;
@@ -10,21 +19,85 @@ type ProposalDiffStringProps = {
   };
 };
 
+/**
+ * collapsed: the text to display is too long, we truncate it to compute the diff to save time
+ * expand: the text to display is ok, but the other text to compare is truncated to save time
+ * force_expand: no matter the text lengths, we calculate the full diff (can be long)
+ */
+type DiffState = 'collapsed' | 'expand' | 'force_expand';
+
+const computeDiffState = (
+  change: {
+    before: string | null;
+    after: string | null;
+  },
+  accessor: ProposalChangeAccessor
+): DiffState => {
+  let before = change.before || '';
+  let after = change.after || '';
+  if (before.length > CHAR_LIMIT || after.length > CHAR_LIMIT) {
+    before = before.substring(0, CHAR_LIMIT - 3);
+    after = after.substring(0, CHAR_LIMIT - 3);
+  }
+
+  return (accessor === 'before' && before !== (change.before || '')) ||
+    (accessor === 'after' && after !== (change.after || ''))
+    ? 'collapsed'
+    : 'expand';
+};
+
 const ProposalDiffString: React.FC<ProposalDiffStringProps> = ({accessor, change, ...rest}) => {
+  const [changes, setChanges] = React.useState<Change[] | null>(null);
+  const [diffState, setDiffState] = React.useState<DiffState>(computeDiffState(change, accessor));
+  const translate = useTranslate();
+
+  React.useEffect(() => {
+    let before = change.before || '';
+    let after = change.after || '';
+    switch (diffState) {
+      case 'collapsed':
+        before = before.substring(0, CHAR_LIMIT - 3);
+        after = after.substring(0, CHAR_LIMIT - 3);
+        break;
+      case 'expand':
+        before = accessor === 'after' ? before.substring(0, CHAR_LIMIT) : before;
+        after = accessor === 'before' ? after.substring(0, CHAR_LIMIT) : after;
+        break;
+    }
+    setChanges(diffChars(before, after));
+  }, [diffState]);
+
+  const forceExpand = () => {
+    setChanges(null);
+    setDiffState('force_expand');
+  };
+
   return (
     <span {...rest}>
-      {diffChars(change.before || '', change.after || '').map((change, i) => {
-        if (accessor === 'before' && change.removed) {
-          return <del key={i}>{change.value}</del>;
-        }
-        if (accessor === 'after' && change.added) {
-          return <ins key={i}>{change.value}</ins>;
-        }
-        if ((accessor === 'before' && !change.added) || (accessor === 'after' && !change.removed)) {
-          return change.value;
-        }
-        return null;
-      })}
+      {!changes && <LoaderIcon />}
+      {changes &&
+        changes.map((change, i) => {
+          if (accessor === 'before' && change.removed) {
+            return <del key={i}>{change.value}</del>;
+          }
+          if (accessor === 'after' && change.added) {
+            return <ins key={i}>{change.value}</ins>;
+          }
+          if ((accessor === 'before' && !change.added) || (accessor === 'after' && !change.removed)) {
+            return change.value;
+          }
+          return null;
+        })}
+      {changes && 'collapsed' === diffState && (
+        <>
+          ...
+          <div>
+            <ExpandButton onClick={forceExpand}>
+              <ArrowDownIcon size={10} /> {translate('pim_datagrid.workflow.see_more')}
+            </ExpandButton>
+          </div>
+        </>
+      )}
     </span>
   );
 };
