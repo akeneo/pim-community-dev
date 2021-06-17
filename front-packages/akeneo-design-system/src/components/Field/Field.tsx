aea0d33185963f0f1996ff3954d91f5163420d2c
Julien Sanchez <sumbobyboys@gmail.com>
2021-06-17 17:49:59 +0200

    RAC-727: fix Field child props

--- front-packages/akeneo-design-system/src/components/Field/Field.tsx
+++ front-packages/akeneo-design-system/src/components/Field/Field.tsx
@@ -31,7 +31,7 @@ const HelperContainer = styled.div`
   max-width: 460px;
 `;
 
-type FieldChild = ReactElement<InputProps<unknown>> | ReactElement<HelperProps> | FieldChild[] | false | null;
+type FieldChild = ReactElement<InputProps<unknown>> | ReactElement<HelperProps> | FieldChild[] | false | null | undefined;
 
 type FieldProps = {
   /**
