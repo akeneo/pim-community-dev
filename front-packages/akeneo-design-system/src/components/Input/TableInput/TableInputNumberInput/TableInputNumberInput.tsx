4c143b17e5e28cb22b4b00a6df73f06ccb019515
pierallard <pierallard@gmail.com>
2021-06-18 17:33:34 +0200

    CPM-179: Fix search style

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumberInput/TableInputNumberInput.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumberInput/TableInputNumberInput.tsx
@@ -1,13 +1,20 @@
 import { TableInputRow } from "../TableInputRow/TableInputRow";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { NumberInput } from "../../NumberInput/NumberInput";
+import { AkeneoThemedProps, getColor } from "../../../../theme";
 
-const TableInputNumberInput = styled(NumberInput)`
-  height: 36px;
-  border-width: 0;
-  background: none;
+const TableInputNumberInput = styled(NumberInput)<{highlighted: boolean} & AkeneoThemedProps>`
+  height: 39px;
   padding-left: 10px;
   padding-right: 10px;
+  border-radius: 0;
+  ${({highlighted}) => (highlighted ? css`
+    background: ${getColor('green', 10)};
+    border-color: ${getColor('green', 80)};
+  ` : css`
+    background: none;
+    border-color: rgba(0,0,0,0);
+  `)};
 `;
 
 TableInputRow.displayName = 'TableInput.NumberInput';
