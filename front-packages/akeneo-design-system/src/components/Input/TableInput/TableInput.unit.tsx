bd26582b2710e97cab1a5db73e6257f121855f52
pierallard <pierallard@gmail.com>
2021-06-21 14:07:33 +0200

    CPM-179: Add tests

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.unit.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.unit.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import {TableInput} from './TableInput';
-import {render, screen} from '../../../storybook/test-util';
+import {fireEvent, render, screen} from '../../../storybook/test-util';
 
 test('it renders its children properly', () => {
-  render(<TableInput>Table content</TableInput>);
+  render(
+    <TableInput>
+      <tbody>
+        <tr>
+          <td>Table content</td>
+        </tr>
+      </tbody>
+    </TableInput>
+  );
 
   expect(screen.getByText('Table content')).toBeInTheDocument();
 });
 
-// Those tests should pass directly if you follow the contributing guide.
-// If you add required props to your Component, these tests will fail
-// and you will need to add these required props here as well
-test('TableInput supports forwardRef', () => {
-  const ref = {current: null};
+test('it scrolls', () => {
+  render(
+    <TableInput data-testid="tableContainer">
+      <tbody>
+        <tr>
+          <td>Table content</td>
+        </tr>
+      </tbody>
+    </TableInput>
+  );
 
-  render(<TableInput ref={ref} />);
-  expect(ref.current).not.toBe(null);
+  fireEvent.scroll(screen.getByTestId('tableContainer'), 100);
 });
 
 test('TableInput supports ...rest props', () => {
--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputHeaderCell/TableInputHeaderCell.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputHeaderCell/TableInputHeaderCell.tsx
@@ -1,7 +1,7 @@
-import React, { Ref } from "react";
-import styled from "styled-components";
-import { Override } from "../../../../shared";
-import { getColor } from "../../../../theme";
+import React, {Ref} from 'react';
+import styled from 'styled-components';
+import {Override} from '../../../../shared';
+import {getColor} from '../../../../theme';
 
 const TableInputTh = styled.th`
   font-weight: normal;
@@ -11,17 +11,18 @@ const TableInputTh = styled.th`
   font-weight: bold;
 `;
 
-type TableInputHeaderCellProps = Override<
-  React.TdHTMLAttributes<HTMLTableCellElement>,
-  {}
->;
-
-const TableInputHeaderCell = React.forwardRef<HTMLTableHeaderCellElement, TableInputHeaderCellProps>(
-  ({children, ...rest}: TableInputHeaderCellProps, forwardedRef: Ref<HTMLTableHeaderCellElement>) => {
-  return <TableInputTh ref={forwardedRef} {...rest}>
-    {children}
-  </TableInputTh>;
-});
+const TableInputHeaderCell = React.forwardRef<HTMLTableHeaderCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
+  (
+    {children, ...rest}: React.TdHTMLAttributes<HTMLTableCellElement>,
+    forwardedRef: Ref<HTMLTableHeaderCellElement>
+  ) => {
+    return (
+      <TableInputTh ref={forwardedRef} {...rest}>
+        {children}
+      </TableInputTh>
+    );
+  }
+);
 
 TableInputHeaderCell.displayName = 'TableInput.HeaderCell';
 
