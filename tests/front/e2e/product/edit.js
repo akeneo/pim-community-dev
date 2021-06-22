c4944617194cfca63074da2eb1f0ee63c9fb1568
jmleroux <jmleroux.pro@gmail.com>
2021-06-22 16:17:19 +0200

    BH-626: Use 'admin' user for front e2e tests

--- tests/front/e2e/product/edit.js
+++ tests/front/e2e/product/edit.js
@@ -1,6 +1,6 @@
 describe('edit product sanity check', () => {
   it('User can enrich the first product of the products grid', () => {
-    cy.login('julia', 'julia');
+    cy.login('admin', 'admin');
     cy.goToProductsGrid();
     cy.selectFirstProductInDatagrid();
     cy.updateField('Name', 'updated product');
