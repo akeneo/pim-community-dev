d70821bb3ba6aab832633fa626fa3fd117cab7fe
anaisbaunelemaire <anais.lemaire@akeneo.com>
2021-06-22 09:14:34 +0000

    Preparing version 5.0.34

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.33';
+    const VERSION = '5.0.34';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
