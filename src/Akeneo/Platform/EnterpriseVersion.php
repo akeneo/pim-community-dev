5e233931c9e5bf43efc8defbd720cf9e3bcdf273
Michel Tag <micheltag@akeneo.com>
2021-06-22 17:02:32 +0200

    Preparing version 5.0.35 (#11119)
    Preparing version 5.0.35
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.34';
+    const VERSION = '5.0.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buttercup';
