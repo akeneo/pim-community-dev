d8e4171b23b3cdbce13a40640f3f161ca2205252
jmleroux <jmleroux.pro@gmail.com>
2021-06-22 17:42:03 +0200

    BH-626: add job name in log context

--- src/Akeneo/Tool/Bundle/BatchBundle/spec/EventListener/LoggerSubscriberSpec.php
+++ src/Akeneo/Tool/Bundle/BatchBundle/spec/EventListener/LoggerSubscriberSpec.php
@@ -4,30 +4,52 @@ namespace spec\Akeneo\Tool\Bundle\BatchBundle\EventListener;
 
 use Akeneo\Tool\Component\Batch\Event\JobExecutionEvent;
 use Akeneo\Tool\Component\Batch\Model\JobExecution;
+use Akeneo\Tool\Component\Batch\Model\JobInstance;
 use PhpSpec\ObjectBehavior;
 use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoggerSubscriberSpec extends ObjectBehavior
 {
-    function let(LoggerInterface $logger, TranslatorInterface $translator)
-    {
+    function let(
+        LoggerInterface $logger,
+        TranslatorInterface $translator,
+        JobExecution $jobExecution,
+        JobInstance $jobInstance
+    ) {
+        $jobExecution->getJobInstance()->willReturn($jobInstance);
+
+        $jobInstance->getConnector()->willReturn('my_connector');
+        $jobInstance->getJobName()->willReturn('my_jobname');
+
         $this->beConstructedWith($logger, $translator);
     }
 
-    function it_logs_job_execution_created($logger, JobExecutionEvent $event, JobExecution $jobExecution)
-    {
+    function it_logs_job_execution_created(
+        $logger,
+        JobExecutionEvent $event,
+        JobExecution $jobExecution
+    ) {
         $event->getJobExecution()->willReturn($jobExecution);
         $jobExecution->__toString()->willReturn('job exec');
-        $logger->debug('Job execution is created: job exec')->shouldBeCalled();
+        $logger->debug(
+            'Job execution is created: job exec',
+            ["connector" => "my_connector", "jobname" => "my_jobname"]
+        )->shouldBeCalled();
         $this->jobExecutionCreated($event);
     }
 
-    function it_logs_before_job_execution($logger, JobExecutionEvent $event, JobExecution $jobExecution)
-    {
+    function it_logs_before_job_execution(
+        $logger,
+        JobExecutionEvent $event,
+        JobExecution $jobExecution
+    ) {
         $event->getJobExecution()->willReturn($jobExecution);
         $jobExecution->__toString()->willReturn('job exec');
-        $logger->debug('Job execution starting: job exec')->shouldBeCalled();
+        $logger->debug(
+            'Job execution starting: job exec',
+            ["connector" => "my_connector", "jobname" => "my_jobname"]
+        )->shouldBeCalled();
         $this->beforeJobExecution($event);
     }
 }
