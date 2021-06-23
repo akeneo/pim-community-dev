bf9ede9e102cc0b8044842c8619ebca9b809a1f6
Olivier Pontier <olivier.pontier@akeneo.com>
2021-06-23 09:16:40 +0200

    Update style for the navigation

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/PimNavigation.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/PimNavigation.tsx
@@ -1,11 +1,37 @@
 import React, {FC, useState} from 'react';
 import styled from 'styled-components';
 import {PimView, useTranslate} from '@akeneo-pim-community/shared';
-import {IconProps, MainNavigationItem} from 'akeneo-design-system';
+import {IconProps, MainNavigationItem, SubNavigationPanel} from 'akeneo-design-system';
 
-const LogoContainer = styled.div``;
+const NavContainer = styled.nav`
+  display: flex;
+  height: 100%;
+`;
+
+const MainNavContainer = styled.div`
+  display: flex;
+  width: 80px;
+  flex-direction: column;
+  justify-content: start;
+  height: 100%;
+  border-right: 1px solid ${({theme}) => theme.color.grey80};
+  z-index: 803;
+  background: white;
+`;
+const SubNavContainer = styled.div``;
+
+const LogoContainer = styled.div`
+  height: 80px;
+  min-height: 80px;
+  position: relative;
+`;
 const MenuContainer = styled.div``;
-const HelpContainer = styled.div``;
+const HelpContainer = styled.div`
+  height: 80px;
+  min-height: 80px;
+  position: relative;
+  margin-top: auto;
+`;
 
 type SubNavigationEntry = {
   position: number;
@@ -39,6 +65,10 @@ const PimNavigation: FC<Props> = ({entries}) => {
   const [activeEntry, setActiveEntry] = useState<string>(entries[0].code || '');
   const isActiveEntry = (code: string) => code === activeEntry;
 
+  // @todo read default value by main navigation entry from Session storage
+  // @example: collapsedColumn_pim-menu-settings: 1
+  const isSubNavigationOpened = true;
+
   const getEntrySubNavigation = () => {
     const entry = entries.find((entry: NavigationEntry) => entry.code === activeEntry);
 
@@ -52,35 +82,39 @@ const PimNavigation: FC<Props> = ({entries}) => {
   const subNavigationItems = getEntrySubNavigation();
 
   return (
-    <nav>
-      <LogoContainer>
-        <PimView viewName="pim-menu-logo" />
-      </LogoContainer>
-      <MenuContainer>
-        {entries.map(({code, label, disabled, icon}) => (
-          <MainNavigationItem
-            key={code}
-            active={isActiveEntry(code)}
-            disabled={disabled}
-            icon={icon}
-            onClick={() => handleFollowEntry(code)}
-          >
-            {translate(label)}
-          </MainNavigationItem>
-        ))}
-      </MenuContainer>
-      <HelpContainer>
-        <PimView viewName="pim-menu-help" />
-      </HelpContainer>
-      <div>
-        {activeEntry}
-        {subNavigationItems.map(item => (
-          <div key={`${item.target}-${item.label}`}>
-            {item.label} {JSON.stringify(item.section)}
-          </div>
-        ))}
-      </div>
-    </nav>
+    <NavContainer aria-label="Main Navigation">
+      <MainNavContainer>
+        <LogoContainer>
+          <PimView viewName="pim-menu-logo" />
+        </LogoContainer>
+        <MenuContainer>
+          {entries.map(({code, label, disabled, icon}) => (
+            <MainNavigationItem
+              key={code}
+              active={isActiveEntry(code)}
+              disabled={disabled}
+              icon={icon}
+              onClick={() => handleFollowEntry(code)}
+            >
+              {translate(label)}
+            </MainNavigationItem>
+          ))}
+        </MenuContainer>
+        <HelpContainer>
+          <PimView viewName="pim-menu-help" />
+        </HelpContainer>
+      </MainNavContainer>
+      <SubNavContainer>
+        <SubNavigationPanel isOpen={isSubNavigationOpened}>
+          {activeEntry}
+          {subNavigationItems.map(item => (
+            <div key={`${item.target}-${item.label}`}>
+              {item.label} {JSON.stringify(item.section)}
+            </div>
+          ))}
+        </SubNavigationPanel>
+      </SubNavContainer>
+    </NavContainer>
   );
 };
 
