039009d1732bd84dbf6f705426e5be4271130cb3
pierallard <pierallard@gmail.com>
2021-06-24 10:00:08 +0200

    CPM-193: Fix boolean input

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputBoolean/TableInputBoolean.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputBoolean/TableInputBoolean.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Badge } from "../../../Badge/Badge";
 import { SelectInput } from "../../SelectInput/SelectInput";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { AkeneoThemedProps, getColor } from "../../../../theme";
 
-const BooleanSelectInput = styled(SelectInput)`
+const BooleanSelectInput = styled(SelectInput)<{highlighted: boolean} & AkeneoThemedProps>`
+  ${({highlighted}) => highlighted && css`
+    & > div:first-child > div:first-child { background: ${getColor('green', 10)}; }
+  `}
   input {
-    border-width: 0;
+    height: 39px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 0;
+    border: none;
+
+    ${({highlighted}) => highlighted ? css`
+      box-shadow: 0 0 0 1px ${getColor('green', 80)};
+    ` : css`
+      background: none;
+    `};
+
+    &:focus {
+      box-shadow: 0 0 0 1px ${getColor('grey', 100)};
+    }
   }
 `;
 
@@ -14,10 +32,18 @@ type TableInputBooleanProps = {
   onChange: (value: boolean) => void;
   yesLabel: string;
   noLabel: string;
+  highlighted: boolean;
 }
 
 const TableInputBoolean = React.forwardRef<HTMLDivElement, TableInputBooleanProps>(
-  ({value, onChange, yesLabel, noLabel, ...rest}, forwardedRef: React.Ref<HTMLDivElement>) => {
+  ({
+     value,
+     onChange,
+     yesLabel,
+     noLabel,
+     highlighted = false,
+     ...rest
+   }, forwardedRef: React.Ref<HTMLDivElement>) => {
     const label = typeof value === 'undefined' ? '' : value ? yesLabel : noLabel;
     const handleChange = (value) => {
       if (value === null) {
@@ -28,6 +54,7 @@ const TableInputBoolean = React.forwardRef<HTMLDivElement, TableInputBooleanProp
     }
     return (
       <BooleanSelectInput
+        highlighted={highlighted}
         onChange={handleChange}
         value={value === null ? null : value ? 'true' : 'false'}
         {...rest}
