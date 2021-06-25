244a75b8f8fe352d439801178879acdf727e28ad
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-06-25 14:15:44 +0200

    Feature job messenger (#14351)
    * CPM-155: add config
    
    * CPM-155: add job execution message normalizer
    
    * CPM-155: add messenger queue service
    
    * CPM-155: add normalizer services
    
    * CPM-155: add handler to execute job
    
    * CPM-155: fix log config
    
    * CPM-155: improve log messages
    
    * CPM-155: improve logger
    
    * CPM-155: add filter capability to gps client
    
    * CPM-155: add job message ui, import, export and backend
    
    * CPM-155: make JobExecutionMessage abstract
    
    * CPM-155: add integration test
    
    * CPM-155: update config
    
    * CPM-155: add readme
    
    * CPM-155: replace backend by data maintenance
    
    * CPM-155: add topics config
    
    * CPM-155: fix reviews
    
    * CPM-155: add documentation
    
    * CPM-155: add todo
    
    * CPM-155: fix flush queue in tests
    
    * CPM-155: use OptionsResolver in PubSub Client
    
    * CPM-155: complete the readme and add diagram
    
    * CPM-155: update readme (review)
    
    * CPM-155: add comment by Alex
    
    * CPM-155: fix english
    
    * CPM-156: fix english
    
    Co-authored-by: Alexandre Hocquard <alexandre.hocquard@akeneo.com>
    
    * CPM-156: fix english
    
    Co-authored-by: Alexandre Hocquard <alexandre.hocquard@akeneo.com>
    
    * CPM-156: fix english
    
    Co-authored-by: Alexandre Hocquard <alexandre.hocquard@akeneo.com>
    
    * CPM-156: use messenger queue
    
    * CPM-156: update tests to use messenger
    
    * CPM-156: add ordering key
    
    * CPM-156: fix ordering key
    
    * CPM-156: update tests with job
    
    * CPM-156: fix job consumer for behats
    
    * CPM-156: fix integration tests
    
    * CPM-156: add spec
    
    * CPM-156: fix HookContext constant
    
    * CPM-156: add migration
    
    * CPM-154: clean useless classes and services
    
    * CPM-156: fix migration test
    
    * CPM-156: remove JobExecutionMessageRepository and create a query instead
    
    * CPM-156: clean and rename class
    
    * CPM-156: fix doc
    
    * CPM-156: add make target to start job worker
    
    * CPM-156: rename method
    
    * CPM-156: remove useless consumer info
    
    * CPM-156: add changelog
    
    * CPM-156: Fix review
    
    * CPM-156: refactor ordering key
    
    * CPM-156: add note in readme
    
    * CPM-156: keep queue table if we have to rollback
    
    * POC: ack job message after pull in all cases (#14606)
    
    * CPM: ack job message after pull in all cases
    
    * Update docs
    
    * Add acceptance tests
    
    * Remove silent exception
    
    * CPM: fix unit test after rebase
    
    * CPM-156: replace migration by SF command (#14694)
    
    * CPM-154: Clean old job queue (#14709)
    
    * CPM-154: remove old queue table for new installs
    
    * CPM-154: remove useless consumer on message
    
    * Fix integration test
    
    * CPM-156 stabilize job tests (#14816)
    
    Co-authored-by: Alexandre Hocquard <alexandre.hocquard@akeneo.com>

--- src/Akeneo/Tool/Bundle/BatchQueueBundle/tests/integration/Queue/ConsumeJobMessageIntegration.php
+++ src/Akeneo/Tool/Bundle/BatchQueueBundle/tests/integration/Queue/ConsumeJobMessageIntegration.php
@@ -1,27 +1,27 @@
 <?php
-
 declare(strict_types=1);
 
-namespace Akeneo\Tool\Bundle\BatchQueueBundle\tests\integration\Command;
+namespace Akeneo\Tool\Bundle\BatchQueueBundle\tests\integration\Queue;
 
-use Akeneo\Tool\Bundle\BatchQueueBundle\Command\JobQueueConsumerCommand;
+use Akeneo\Platform\Bundle\ImportExportBundle\Repository\InternalApi\JobExecutionRepository;
+use Akeneo\Test\Integration\TestCase;
+use Akeneo\Test\IntegrationTestsBundle\Launcher\JobLauncher;
 use Akeneo\Tool\Bundle\BatchQueueBundle\Manager\JobExecutionManager;
+use Akeneo\Tool\Bundle\BatchQueueBundle\MessageHandler\JobExecutionMessageHandler;
 use Akeneo\Tool\Component\Batch\Job\BatchStatus;
 use Akeneo\Tool\Component\Batch\Job\ExitStatus;
 use Akeneo\Tool\Component\Batch\Model\JobExecution;
 use Akeneo\Tool\Component\Batch\Model\JobInstance;
-use Akeneo\Tool\Component\BatchQueue\Queue\JobExecutionMessage;
+use Akeneo\Tool\Component\BatchQueue\Queue\DataMaintenanceJobExecutionMessage;
 use Akeneo\Tool\Component\BatchQueue\Queue\JobExecutionQueueInterface;
-use Akeneo\Test\Integration\TestCase;
-use Akeneo\Test\IntegrationTestsBundle\Launcher\JobLauncher;
-use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Assert;
 use Symfony\Component\Process\Process;
 
-class JobQueueConsumerCommandIntegration extends TestCase
+class ConsumeJobMessageIntegration extends TestCase
 {
-    /** @var JobLauncher */
-    protected $jobLauncher;
+    private JobLauncher $jobLauncher;
+    private JobExecutionRepository $jobExecutionRepository;
 
     /**
      * {@inheritdoc}
@@ -36,9 +36,10 @@ class JobQueueConsumerCommandIntegration extends TestCase
         $jobInstanceSaver->save($jobInstance);
 
         $this->jobLauncher = $this->get('akeneo_integration_tests.launcher.job_launcher');
+        $this->jobExecutionRepository = $this->get('pim_enrich.repository.job_execution');
     }
 
-    public function testLaunchAJobExecution()
+    public function testLaunchAJobExecution(): void
     {
         $jobExecution = $this->createJobExecutionInQueue('csv_product_export');
 
@@ -50,63 +51,27 @@ class JobQueueConsumerCommandIntegration extends TestCase
         Assert::assertEquals(ExitStatus::COMPLETED, $row['exit_code']);
         Assert::assertNotNull($row['health_check_time']);
 
-        $jobExecution = $this->get('pim_enrich.repository.job_execution')->findBy(['id' => $jobExecution->getId()]);
+        $this->jobExecutionRepository->clear();
+        $jobExecution = $this->jobExecutionRepository->findBy(['id' => $jobExecution->getId()]);
         $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution[0]);
 
         Assert::assertEquals(BatchStatus::COMPLETED, $jobExecution->getStatus()->getValue());
         Assert::assertEquals(ExitStatus::COMPLETED, $jobExecution->getExitStatus()->getExitCode());
-
-        $stmt = $this->getConnection()->prepare('SELECT consumer from akeneo_batch_job_execution_queue');
-        $stmt->execute();
-        $row = $stmt->fetch();
-
-        Assert::assertNotEmpty($row['consumer']);
     }
 
-    public function testLaunchFilteredJobExecution()
-    {
-        $jobExecution = $this->createJobExecutionInQueue('csv_product_export');
-
-        $this->jobLauncher->launchConsumerOnce(['-j' => ['csv_product_export']]);
-
-        $row = $this->getJobExecutionDatabaseRow($jobExecution);
-
-        Assert::assertEquals(BatchStatus::COMPLETED, $row['status']);
-        Assert::assertEquals(ExitStatus::COMPLETED, $row['exit_code']);
-        Assert::assertNotNull($row['health_check_time']);
-
-        $jobExecution = $this->get('pim_enrich.repository.job_execution')->findBy(['id' => $jobExecution->getId()]);
-        $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution[0]);
-
-        Assert::assertEquals(BatchStatus::COMPLETED, $jobExecution->getStatus()->getValue());
-        Assert::assertEquals(ExitStatus::COMPLETED, $jobExecution->getExitStatus()->getExitCode());
-
-        $stmt = $this->getConnection()->prepare('SELECT consumer from akeneo_batch_job_execution_queue');
-        $stmt->execute();
-        $row = $stmt->fetch();
-
-        Assert::assertNotEmpty($row['consumer']);
-    }
-
-    public function testStatusOfACrashedJobExecution()
+    public function testStatusOfACrashedJobExecution(): void
     {
         $jobExecution = $this->createJobExecutionInQueue('infinite_loop_job');
 
-        $options = ['email' => 'ziggy@akeneo.com', 'env' => $this->getParameter('kernel.environment')];
-        $jobExecutionMessage = JobExecutionMessage::createJobExecutionMessage($jobExecution->getId(), $options);
-
-        $this->getQueue()->publish($jobExecutionMessage);
-
         $daemonProcess = $this->jobLauncher->launchConsumerOnceInBackground();
 
-        $jobExecutionProcessPid = $this->getJobExecutionProcessPid($daemonProcess);
+        $jobExecutionProcessPid = $this->getJobExecutionProcessPid($daemonProcess->getPid());
 
-        // wait update of the job execution status in database
         sleep(5);
 
-        $killJobExecution = new Process(sprintf('kill -9 %s', $jobExecutionProcessPid));
+        $killJobExecution = new Process(['kill', '-9', $jobExecutionProcessPid]);
         $killJobExecution->run();
-        sleep(JobQueueConsumerCommand::HEALTH_CHECK_INTERVAL + 5);
+        sleep(JobExecutionMessageHandler::HEALTH_CHECK_INTERVAL + 5);
 
         $row = $this->getJobExecutionDatabaseRow($jobExecution);
 
@@ -114,32 +79,35 @@ class JobQueueConsumerCommandIntegration extends TestCase
         Assert::assertEquals(ExitStatus::FAILED, $row['exit_code']);
         Assert::assertNotNull($row['health_check_time']);
 
-        $jobExecution = $this->get('pim_enrich.repository.job_execution')->findBy(['id' => $jobExecution->getId()]);
-        $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution[0]);
+        $this->jobExecutionRepository->clear();
+        $jobExecution = $this->jobExecutionRepository->find($jobExecution->getId());
+        $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution);
 
         Assert::assertEquals(BatchStatus::FAILED, $jobExecution->getStatus()->getValue());
         Assert::assertEquals(ExitStatus::FAILED, $jobExecution->getExitStatus()->getExitCode());
     }
 
-    public function testJobExecutionStatusResolverWhenDaemonAndJobExecutionCrash()
+    public function testJobExecutionStatusResolverWhenDaemonAndJobExecutionCrash(): void
     {
         $jobExecution = $this->createJobExecutionInQueue('infinite_loop_job');
 
         $daemonProcess = $this->jobLauncher->launchConsumerOnceInBackground();
 
-        $jobExecutionProcessPid = $this->getJobExecutionProcessPid($daemonProcess);
+        $jobExecutionProcessPid = $this->getJobExecutionProcessPid($daemonProcess->getPid());
 
-        $killDaemon = new Process(sprintf('kill -9 %s', $daemonProcess->getPid()));
-        $killDaemon->run();
+        $daemonProcess->stop(3,9);
 
         // wait update of the job execution status in database
-        sleep(5);
+        while ($daemonProcess->isRunning()) {
+            sleep(1);
+        }
+        sleep(2);
 
         $killJobExecution = new Process(sprintf('kill -9 %s', $jobExecutionProcessPid));
         $killJobExecution->run();
 
         // wait healtch check date expiration
-        sleep(JobQueueConsumerCommand::HEALTH_CHECK_INTERVAL + 10);
+        sleep(JobExecutionMessageHandler::HEALTH_CHECK_INTERVAL + 2);
 
         $row = $this->getJobExecutionDatabaseRow($jobExecution);
 
@@ -147,24 +115,19 @@ class JobQueueConsumerCommandIntegration extends TestCase
         Assert::assertEquals(ExitStatus::UNKNOWN, $row['exit_code']);
         Assert::assertNotNull($row['health_check_time']);
 
-        $jobExecution = $this->get('pim_enrich.repository.job_execution')->findBy(['id' => $jobExecution->getId()]);
-        $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution[0]);
+        $this->jobExecutionRepository->clear();
+        $jobExecution = $this->jobExecutionRepository->find($jobExecution->getId());
+        $jobExecution = $this->getJobExecutionManager()->resolveJobExecutionStatus($jobExecution);
 
         Assert::assertEquals(BatchStatus::FAILED, $jobExecution->getStatus()->getValue());
         Assert::assertEquals(ExitStatus::FAILED, $jobExecution->getExitStatus()->getExitCode());
     }
 
-    /**
-     * @param string $jobInstanceCode
-     * @param string $user
-     * @param array  $configuration
-     *
-     * @throws \RuntimeException
-     *
-     * @return JobExecution
-     */
-    protected function createJobExecution(string $jobInstanceCode, ?string $user, array $configuration = []) : JobExecution
-    {
+    protected function createJobExecution(
+        string $jobInstanceCode,
+        ?string $user,
+        array $configuration = []
+    ) : JobExecution {
         $jobInstanceClass = $this->getParameter('akeneo_batch.entity.job_instance.class');
         $jobInstance = $this
             ->get('doctrine.orm.default_entity_manager')
@@ -194,7 +157,7 @@ class JobQueueConsumerCommandIntegration extends TestCase
     {
         $jobExecution = $this->createJobExecution($jobInstanceCode, 'mary');
         $options = ['email' => 'ziggy@akeneo.com', 'env' => $this->getParameter('kernel.environment')];
-        $jobExecutionMessage = JobExecutionMessage::createJobExecutionMessage($jobExecution->getId(), $options);
+        $jobExecutionMessage = DataMaintenanceJobExecutionMessage::createJobExecutionMessage($jobExecution->getId(), $options);
         $this->getQueue()->publish($jobExecutionMessage);
 
         return $jobExecution;
@@ -202,21 +165,17 @@ class JobQueueConsumerCommandIntegration extends TestCase
 
     /**
      * Returns the PID of the job execution process launched by the daemon process.
-     *
-     * @param $daemonProcess
-     *
-     * @throws \Exception
-     *
-     * @return string
      */
-    protected function getJobExecutionProcessPid(Process $daemonProcess): string
+    protected function getJobExecutionProcessPid(int $processPid): string
     {
         $count = 0;
         do {
-            $pgrep = new Process(sprintf('pgrep -P %s', $daemonProcess->getPid()));
+            $pgrep = new Process(sprintf('pgrep -P %d', $processPid));
             $pgrep->run();
             $output = trim($pgrep->getOutput());
-            $isJobLaunched = '' !== $output;
+            if ('' !== $output) {
+                return $output;
+            }
 
             $count++;
             if ($count > 30) {
@@ -224,45 +183,27 @@ class JobQueueConsumerCommandIntegration extends TestCase
             }
 
             sleep(1);
-        } while (false === $isJobLaunched);
-
-        return $output;
+        } while (true);
     }
 
-    /**
-     * @param JobExecution $jobExecution
-     *
-     * @return array
-     */
     protected function getJobExecutionDatabaseRow(JobExecution $jobExecution): array
     {
-        $stmt = $this->getConnection()->prepare('SELECT status, exit_code, health_check_time from akeneo_batch_job_execution where id = :id');
-        $stmt->bindValue('id', $jobExecution->getId());
-        $stmt->execute();
-        $row = $stmt->fetch();
-
-        return $row;
+        return $this->getConnection()->executeQuery(
+            'SELECT status, exit_code, health_check_time from akeneo_batch_job_execution where id = :id',
+            ['id' => $jobExecution->getId()]
+        )->fetch();
     }
 
-    /**
-     * @return Connection
-     */
     protected function getConnection(): Connection
     {
-        return $this->get('doctrine.orm.entity_manager')->getConnection();
+        return $this->get('doctrine.dbal.default_connection');
     }
 
-    /**
-     * @return JobExecutionQueueInterface
-     */
     protected function getQueue(): JobExecutionQueueInterface
     {
-        return $this->get('akeneo_batch_queue.queue.database_job_execution_queue');
+        return $this->get('akeneo_batch_queue.queue.job_execution_queue');
     }
 
-    /**
-     * @return JobExecutionManager
-     */
     protected function getJobExecutionManager(): JobExecutionManager
     {
         return $this->get('akeneo_batch_queue.manager.job_execution_manager');
