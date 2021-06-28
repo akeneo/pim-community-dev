a0f89e18b63d13a8c632b4e6cf8c58316f2d8c5d
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2021-06-28 20:27:46 +0200

    API-1669 remove scale up after no-zdd (helm3 does it for us) (#11146)
    * API-1669 remove scale up after no-zdd (helm3 does it for us)
    * API-1669 scale down API for no ZDD

--- deployments/automation/steps/_no-zdd-compliant.sh
+++ deployments/automation/steps/_no-zdd-compliant.sh
@@ -7,7 +7,7 @@ if [ ${STEP} == "PRE_APPLY" ]; then
 
     if [ ! -z ${SKIP_SHUTDOWN} ] && [ ${SKIP_SHUTDOWN} == "false" ]; then
         echo "[INFO] NO ZDD COMPLIANT RELEASE, we are turning off the application ${PFID}"
-        kubectl scale -n ${PFID} deploy/pim-web deploy/pim-daemon-default --replicas=0 || true
+        kubectl scale -n ${PFID} deploy/pim-web deploy/pim-api deploy/pim-daemon-default --replicas=0 || true
         kubectl scale -n ${PFID} deploy/pim-daemon-webhook-consumer-process --replicas=0 || true
         kubectl scale -n ${PFID} deploy/pim-daemon-all-but-linking-assets-to-products --replicas=0 || true
     else
@@ -15,16 +15,3 @@ if [ ${STEP} == "PRE_APPLY" ]; then
     fi
 fi
 
-if [ ${STEP} == "POST_APPLY" ]; then
-    if [ ! -z ${SKIP_SHUTDOWN} ] && [ ${SKIP_SHUTDOWN} == "false" ]; then
-        PARALLEL_WEB=$(yq m -x ${TF_PATH_PIM_MODULE}/pim/values.yaml tf-helm-pim-values.yaml values.yaml | yq r - pim.replicas)
-        PARALLEL_DAEMON_DEFAULT=$(yq m -x ${TF_PATH_PIM_MODULE}/pim/values.yaml tf-helm-pim-values.yaml values.yaml | yq r - pim.daemons.default.replicas)
-        PARALLEL_DAEMON_WEBHOOK=$(yq m -x ${TF_PATH_PIM_MODULE}/pim/values.yaml tf-helm-pim-values.yaml values.yaml | yq r - pim.daemons.webhook-consumer-process.replicas)
-        PARALLEL_DAEMON_ASSET=$(yq m -x ${TF_PATH_PIM_MODULE}/pim/values.yaml tf-helm-pim-values.yaml values.yaml | yq r - pim.daemons.all-but-linking-assets-to-products.replicas)
-
-        kubectl scale -n ${PFID} deploy/pim-web --replicas=${PARALLEL_WEB} || true
-        kubectl scale -n ${PFID} deploy/pim-daemon-default --replicas=${PARALLEL_DAEMON_DEFAULT} || true
-        kubectl scale -n ${PFID} deploy/pim-daemon-webhook-consumer-process --replicas=${PARALLEL_DAEMON_WEBHOOK} || true
-        kubectl scale -n ${PFID} deploy/pim-daemon-all-but-linking-assets-to-products --replicas=${PARALLEL_DAEMON_ASSET} || true
-    fi
-fi
