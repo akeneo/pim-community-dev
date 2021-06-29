038036bb5d69a874a933fe8bcc3f1ba30afd66d2
Riff <riff.ventura@gmail.com>
2021-06-29 13:41:50 +0200

    CXP-782 Created marketplace without user profile page

---
+++ src/Akeneo/Connectivity/Connection/front/src/connect/pages/MarketplaceWithoutUserProfile.tsx
@@ -0,0 +1,5 @@
+import React, {FC, useContext, useEffect, useState} from 'react';
+
+export const MarketplaceWithoutUserProfile: FC = () => {
+    return (<> <h1>Marketplace without user profile</h1></>);
+};
