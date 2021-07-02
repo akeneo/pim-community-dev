dff284e3c0dbafd4c7d8c316a0c89f1a9cff205b
Benoit Jacquemont <benoit@akeneo.com>
2021-07-02 14:49:53 +0200

    BH-641 Adds Symfony route identifier to FPM & Apache logs (#11173)
    * BH-641 Adds Symfony route identifier to FPM & Apache logs

--- tests/back/Platform/Specification/Bundle/FrameworkBundle/EventListener/AddContextHeaderResponseListenerSpec.php
+++ tests/back/Platform/Specification/Bundle/FrameworkBundle/EventListener/AddContextHeaderResponseListenerSpec.php
@@ -8,6 +8,7 @@ use PhpSpec\ObjectBehavior;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class AddContextHeaderResponseListenerSpec extends ObjectBehavior
@@ -17,12 +18,15 @@ class AddContextHeaderResponseListenerSpec extends ObjectBehavior
         Request $request,
         ResponseEvent $event,
         Response $response,
-        ResponseHeaderBag $headers
+        ResponseHeaderBag $headers,
+        ParameterBag $requestAttributes
     ) {
         $boundedContextResolver->fromRequest($request)->shouldBeCalled()->willReturn('my_context');
 
         $event->getRequest()->shouldBeCalled()->willReturn($request);
         $request->getPathInfo()->willReturn('my_path_info');
+        $requestAttributes->get('_route', 'undefined')->willReturn('my_symfony_route');
+        $request->attributes = $requestAttributes;
 
         $event->getResponse()->shouldBeCalled()->willReturn($response);
 
@@ -33,6 +37,7 @@ class AddContextHeaderResponseListenerSpec extends ObjectBehavior
 
         $headers->set('x-akeneo-context', 'my_context')->shouldBeCalled();
         $headers->set('x-request-path', 'my_path_info')->shouldBeCalled();
+        $headers->set('x-symfony-route', 'my_symfony_route')->shouldBeCalled();
 
         $this->injectAkeneoContextHeader($event);
     }
