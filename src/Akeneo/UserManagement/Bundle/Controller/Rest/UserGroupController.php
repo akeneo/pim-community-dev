f0e70bdb1a6c6d1ba25f98afc1779782bc7d415d
Florian Lasdoulours <florian.lasdoulours@akeneo.com>
2021-07-05 23:59:53 +0200

    add new private rest endpoints for reading and writing user roles and user groups

--- src/Akeneo/UserManagement/Bundle/Controller/Rest/UserGroupController.php
+++ src/Akeneo/UserManagement/Bundle/Controller/Rest/UserGroupController.php
@@ -2,9 +2,22 @@
 
 namespace Akeneo\UserManagement\Bundle\Controller\Rest;
 
+use Akeneo\Tool\Component\Connector\Processor\Denormalization\AbstractProcessor;
+use Akeneo\Tool\Component\StorageUtils\Factory\SimpleFactoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Saver\BulkSaverInterface;
+use Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface;
+use Akeneo\UserManagement\Bundle\Context\UserContext;
 use Akeneo\UserManagement\Bundle\Doctrine\ORM\Repository\GroupRepository;
+use Akeneo\UserManagement\Component\Model\Group;
 use Akeneo\UserManagement\Component\Model\GroupInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 
 /**
  * User group rest controller
@@ -17,14 +30,105 @@ class UserGroupController
 {
     /** @var GroupRepository */
     protected $groupRepository;
+    /** @var NormalizerInterface */
+    protected $normalizer;
+
+    /** @var UserContext */
+    protected $userContext;
+
+    /** @var BulkSaverInterface */
+    private BulkSaverInterface $saver;
+
+    /**  @var SimpleFactoryInterface */
+    private SimpleFactoryInterface $factory;
+
+    /** @var ObjectUpdaterInterface */
+    private ObjectUpdaterInterface $updater;
+
+    /** @var ValidatorInterface */
+    private ValidatorInterface $validator;
+
+    /** @var NormalizerInterface */
+    private NormalizerInterface $constraintViolationNormalizer;
 
     /**
      * @param GroupRepository $groupRepository
+     * @param NormalizerInterface $normalizer
+     * @param UserContext $userContext
+     * @param SimpleFactoryInterface $factory
+     * @param ObjectUpdaterInterface $updater
+     * @param BulkSaverInterface $saver
+     * @param ValidatorInterface $validator
+     * @param NormalizerInterface $constraintViolationNormalizer
      */
-    public function __construct(GroupRepository $groupRepository)
+    public function __construct(
+        GroupRepository $groupRepository,
+        NormalizerInterface $normalizer,
+        AbstractProcessor $denormalizer,
+        UserContext $userContext,
+        SimpleFactoryInterface $factory,
+        ObjectUpdaterInterface $updater,
+        BulkSaverInterface $saver,
+        ValidatorInterface $validator,
+        NormalizerInterface $constraintViolationNormalizer
+    )
     {
         $this->groupRepository = $groupRepository;
+        $this->normalizer = $normalizer;
+        $this->denormalizer = $denormalizer;
+        $this->userContext = $userContext;
+        $this->factory = $factory;
+        $this->updater = $updater;
+        $this->saver = $saver;
+        $this->validator = $validator;
+        $this->constraintViolationNormalizer = $constraintViolationNormalizer;
     }
+
+    /**
+     * @AclAncestor("pim_user_group_create")
+     */
+    public function createAction(Request $request): Response
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return new RedirectResponse('/');
+        }
+
+        $userGroup = $this->factory->create();
+        $content = json_decode($request->getContent(), true);
+
+        $this->updater->update($userGroup, $content);
+        $violations = $this->validator->validate($userGroup);
+
+        if ($violations->count() > 0) {
+            $normalizedViolations = [];
+            foreach ($violations as $violation) {
+                $normalizedViolations[] = $this->constraintViolationNormalizer->normalize(
+                    $violation,
+                    'internal_api',
+                    ['userGroup' => $userGroup]
+                );
+            }
+
+            return new JsonResponse(['values' => $normalizedViolations], Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->saver->save($userGroup);
+
+        return new JsonResponse($this->normalizer->normalize($userGroup, 'internal_api'));
+    }
+
+    /**
+     * @param int $identifier
+     * @AclAncestor("pim_user_group_edit")
+     * @return JsonResponse
+     */
+    public function getAction(int $identifier): JsonResponse
+    {
+        $userGroup = $this->getUserGroupOr404($identifier);
+
+        return new JsonResponse($this->normalizer->normalize($userGroup, 'internal_api'));
+    }
+
 
     /**
      * Get the list of all user groups
@@ -44,5 +148,22 @@ class UserGroupController
         }, $this->groupRepository->findAll());
 
         return new JsonResponse($userGroups);
+    }
+
+    /**
+     * @param $identifier
+     * @return Group
+     */
+    private function getUserGroupOr404($identifier):Group
+    {
+        $userGroup = $this->groupRepository->findOneBy(['id' => $identifier]);
+
+        if (null === $userGroup) {
+            throw new NotFoundHttpException(
+                sprintf('User Group with id "%s" not found', $identifier)
+            );
+        }
+
+        return $userGroup;
     }
 }
--- src/Akeneo/UserManagement/Bundle/Controller/Rest/UserRoleController.php
+++ src/Akeneo/UserManagement/Bundle/Controller/Rest/UserRoleController.php
@@ -2,10 +2,22 @@
 
 namespace Akeneo\UserManagement\Bundle\Controller\Rest;
 
+use Akeneo\Tool\Component\Connector\ArrayConverter\ArrayConverterInterface;
+use Akeneo\Tool\Component\Connector\Processor\Normalization\Processor;
+use Akeneo\Tool\Component\StorageUtils\Factory\SimpleFactoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Saver\BulkSaverInterface;
+use Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface;
 use Akeneo\UserManagement\Bundle\Context\UserContext;
 use Akeneo\UserManagement\Bundle\Doctrine\ORM\Repository\RoleRepository;
+use Akeneo\UserManagement\Component\Model\Role;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Role controller
@@ -25,20 +37,114 @@ class UserRoleController
     /** @var UserContext */
     protected $userContext;
 
+    /** @var BulkSaverInterface */
+    private BulkSaverInterface $saver;
+
+    /**  @var SimpleFactoryInterface */
+    private SimpleFactoryInterface $factory;
+
+    /** @var ObjectUpdaterInterface */
+    private ObjectUpdaterInterface $updater;
+
+    /** @var ValidatorInterface */
+    private ValidatorInterface $validator;
+
+    /** @var NormalizerInterface */
+    private NormalizerInterface $constraintViolationNormalizer;
+    private NormalizerInterface $basicNormalizer;
+    private ArrayConverterInterface $arrayConverter;
+    private ArrayConverterInterface $standardToFlatArrayConverter;
+    private ArrayConverterInterface $flatToStandardArrayConverter;
+
     /**
-     * @param RoleRepository      $roleRepository
-     * @param NormalizerInterface $normalizer
-     * @param UserContext         $userContext
+     * @param RoleRepository $roleRepository
+     * @param Processor $normalizer
+     * @param ArrayConverterInterface $standardToFlatArrayConverter
+     * @param ArrayConverterInterface $flatToStandardArrayConverter
+     * @param NormalizerInterface $basicNormalizer
+     * @param UserContext $userContext
+     * @param SimpleFactoryInterface $factory
+     * @param ObjectUpdaterInterface $updater
+     * @param BulkSaverInterface $saver
+     * @param ValidatorInterface $validator
+     * @param NormalizerInterface $constraintViolationNormalizer
      */
     public function __construct(
         RoleRepository $roleRepository,
-        NormalizerInterface $normalizer,
-        UserContext $userContext
+        Processor $normalizer,
+        ArrayConverterInterface $standardToFlatArrayConverter,
+        ArrayConverterInterface $flatToStandardArrayConverter,
+        NormalizerInterface $basicNormalizer,
+        UserContext $userContext,
+        SimpleFactoryInterface $factory,
+        ObjectUpdaterInterface $updater,
+        BulkSaverInterface $saver,
+        ValidatorInterface $validator,
+        NormalizerInterface $constraintViolationNormalizer
+
     ) {
         $this->roleRepository = $roleRepository;
         $this->normalizer = $normalizer;
+        $this->standardToFlatArrayConverter = $standardToFlatArrayConverter;
+        $this->flatToStandardArrayConverter = $flatToStandardArrayConverter;
+        $this->basicNormalizer = $basicNormalizer;
         $this->userContext = $userContext;
+        $this->factory = $factory;
+        $this->updater = $updater;
+        $this->saver = $saver;
+        $this->validator = $validator;
+        $this->constraintViolationNormalizer = $constraintViolationNormalizer;
     }
+
+    /**
+     * @AclAncestor("pim_user_role_create")
+     */
+    public function createAction(Request $request): Response
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return new RedirectResponse('/');
+        }
+
+        $userRole = $this->factory->create();
+        $content = json_decode($request->getContent(), true);
+
+        $datas = $this->flatToStandardArrayConverter->convert($content);
+
+        $this->updater->update($userRole, $datas);
+        $violations = $this->validator->validate($userRole);
+
+        if ($violations->count() > 0) {
+            $normalizedViolations = [];
+            foreach ($violations as $violation) {
+                $normalizedViolations[] = $this->constraintViolationNormalizer->normalize(
+                    $violation,
+                    'internal_api',
+                    ['userRole' => $userRole]
+                );
+            }
+
+            return new JsonResponse(['values' => $normalizedViolations], Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->saver->saveAll([$userRole]);
+
+        $userRole = $this->roleRepository->findOneByIdentifier($userRole->role()->getRole());
+
+        return new JsonResponse($this->standardToFlatArrayConverter->convert($this->normalizer->process($userRole)));
+    }
+
+    /**
+     * @param int $identifier
+     * @AclAncestor("pim_user_role_edit")
+     * @return JsonResponse
+     */
+    public function getAction(int $identifier): JsonResponse
+    {
+        $userRole = $this->getUserRoleOr404($identifier);
+
+        return new JsonResponse($this->standardToFlatArrayConverter->convert($this->normalizer->process($userRole)));
+    }
+
 
     /**
      * @return JsonResponse
@@ -48,10 +154,28 @@ class UserRoleController
         $queryBuildder = $this->roleRepository->getAllButAnonymousQB();
         $roles = $queryBuildder->getQuery()->execute();
 
-        return new JsonResponse($this->normalizer->normalize(
+        return new JsonResponse($this->basicNormalizer->normalize(
             $roles,
             'internal_api',
             $this->userContext->toArray()
         ));
     }
+
+    /**
+     * @param $identifier
+     * @return Role
+     */
+    private function getUserRoleOr404($identifier):Role
+    {
+        $userRole = $this->roleRepository->findOneBy(['id' => $identifier]);
+
+        if (null === $userRole) {
+            throw new NotFoundHttpException(
+                sprintf('User Role with id "%s" not found', $identifier)
+            );
+        }
+
+        return $userRole;
+    }
+
 }
