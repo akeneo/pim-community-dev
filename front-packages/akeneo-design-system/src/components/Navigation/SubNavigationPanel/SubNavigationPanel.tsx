f9ccf1dba45a6d0dddb3ba01e82be2b0ffe0df72
Julian Prud'homme <julian.prudhomme@akeneo.com>
2021-07-05 14:17:45 +0200

    PLG-352: fix sub menu container

--- front-packages/akeneo-design-system/src/components/Navigation/SubNavigationPanel/SubNavigationPanel.tsx
+++ front-packages/akeneo-design-system/src/components/Navigation/SubNavigationPanel/SubNavigationPanel.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import {useBooleanState} from '../../../hooks';
 import {PanelCloseIcon, PanelOpenIcon} from '../../../icons';
 import {AkeneoThemedProps, getColor} from '../../../theme';
 
 const Panel = styled.div<{isOpen: boolean} & AkeneoThemedProps>`
-  background-color: ${getColor('grey', 20)};
-  border-right: 1px solid ${getColor('grey', 80)};
   display: flex;
   flex-direction: column;
-  height: 100%;
-  max-width: ${({isOpen}) => (isOpen ? '280px' : '40px')};
-  transition: max-width 0.3s ease-in-out;
+  height: calc(100% - 54px);
+  width: 100%;
+  position: absolute;
+  overflow-x: ${({isOpen}) => (isOpen ? 'auto' : 'none')};
 `;
 
 const PanelContent = styled.div<{isOpen: boolean} & AkeneoThemedProps>`
   flex-grow: 1;
-  opacity: ${({isOpen}) => (isOpen ? 1 : 0)};
-  padding: 20px;
+  //opacity: ${({isOpen}) => (isOpen ? 1 : 0)};
+  padding: ${({isOpen}) => (isOpen ? '30px' : '10px 5px')};
   transition: opacity 0.3s;
   transition-delay: 0.3s;
 `;
@@ -33,11 +31,27 @@ const ToggleButton = styled.button<{isOpen: boolean} & AkeneoThemedProps>`
   padding: ${({isOpen}) => (isOpen ? '0' : '0 12.5px')};
   transition: margin 0.3s ease-in-out;
   text-align: left;
+  position: absolute;
+  bottom: 0;
+  width: ${({isOpen}) => (isOpen ? '240px' : '40px')};;
 
   svg {
     color: ${getColor('grey', 100)};
     width: 15px;
   }
+`;
+
+const Container = styled.div<{isOpen: boolean} & AkeneoThemedProps>`
+  width: ${({isOpen}) => (isOpen ? '280px' : '40px')};
+  transition: width 0.3s ease-in-out;
+  position: relative;
+  order: -10;
+  background-color: ${getColor('grey', 20)};
+  border-right: 1px solid ${getColor('grey', 80)};
+  flex-shrink: 0;
+  height: 100%;
+  z-index: 802;
+  overflow: hidden;
 `;
 
 type SubNavigationPanelProps = {
@@ -50,6 +64,16 @@ type SubNavigationPanelProps = {
    * Whether or not the panel is open.
    */
   isOpen?: boolean;
+
+  /**
+   * Callback to open the sub navigation panel
+   */
+  open: () => void;
+
+  /**
+   * Callback to close the sub navigation panel
+   */
+  close: () => void;
 };
 
 /**
@@ -57,19 +81,19 @@ type SubNavigationPanelProps = {
  */
 const SubNavigationPanel = React.forwardRef<HTMLDivElement, SubNavigationPanelProps>(
   (
-    {children, isOpen: defaultIsOpen = true, ...rest}: SubNavigationPanelProps,
+    {children, isOpen: isOpen = true, open, close, ...rest}: SubNavigationPanelProps,
     forwardedRef: React.Ref<HTMLDivElement>
   ) => {
-    const [isOpen, open, close] = useBooleanState(defaultIsOpen);
 
     return (
-      <Panel ref={forwardedRef} isOpen={isOpen} {...rest}>
-        <PanelContent isOpen={isOpen}>{isOpen && children}</PanelContent>
-
+      <Container isOpen={isOpen}>
+        <Panel ref={forwardedRef} isOpen={isOpen} {...rest}>
+          <PanelContent isOpen={isOpen}>{children}</PanelContent>
+        </Panel>
         <ToggleButton isOpen={isOpen} onClick={() => (isOpen ? close() : open())} title={isOpen ? 'Close' : 'Open'}>
           {isOpen ? <PanelCloseIcon /> : <PanelOpenIcon />}
         </ToggleButton>
-      </Panel>
+      </Container>
     );
   }
 );
