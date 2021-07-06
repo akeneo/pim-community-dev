bfbb107f5bda2c965cabafa5db1780f05de471b0
Paul Chasle <pchasle@users.noreply.github.com>
2021-07-06 10:14:18 +0200

    Fix missing phpspec on Context obj

--- tests/back/Platform/Specification/Component/Webhook/ContextSpec.php
+++ tests/back/Platform/Specification/Component/Webhook/ContextSpec.php
@@ -11,10 +11,25 @@ use PhpSpec\ObjectBehavior;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class EventDataCollectionSpec extends ObjectBehavior
+class ContextSpec extends ObjectBehavior
 {
+    public function let(): void
+    {
+        $this->beConstructedWith('username_0000', 10);
+    }
+
     public function it_is_a_context(): void
     {
         $this->shouldBeAnInstanceOf(Context::class);
+    }
+
+    public function it_returns_a_username(): void
+    {
+        $this->getUsername()->shouldReturn('username_0000');
+    }
+
+    public function it_returns_a_user_id(): void
+    {
+        $this->getUserId()->shouldReturn(10);
     }
 }
