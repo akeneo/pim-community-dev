5011532dd7bea5518bd22c450364af2a55577cfd
Steven VAIDIE <steven.vaidie@gmail.com>
2021-07-07 14:45:06 +0200

    RAC-602: Add association and attribute public API

---
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/Association/AssociationType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Association;
+
+class AssociationType
+{
+    public string $code;
+
+    public string $label;
+}
---
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/Attribute/GroupedAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Attribute;
+
+class GroupedAttribute
+{
+    public string $code;
+    public string $label;
+    public string $attributeGroupCode;
+    public string $attributeGroupLabel;
+}
