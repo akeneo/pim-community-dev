eb098426fb0516f193c73cdfbe4c1aad3a030fb8
Laurent Petard <laurent.petard@akeneo.com>
2021-07-09 14:41:24 +0200

    PIM-9961: Make job profiles files field read-only instead of hide them

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/edit/field/allow-file-upload.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/edit/field/allow-file-upload.ts
@@ -3,11 +3,11 @@ const editionProvider = require('pim/edition');
 
 class AllowFileUpload extends BaseField {
   render() {
-    if (editionProvider.isCloudEdition() === false) {
-      super.render();
+    if (true === editionProvider.isCloudEdition()) {
+      this.config.readOnly = true;
     }
 
-    return this;
+    super.render();
   }
 }
 
