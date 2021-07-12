f769e342bcc7e0917cb502e7b25c3ccf8afebb54
pierallard <pierallard@gmail.com>
2021-07-12 16:36:43 +0200

    CPM-251: Add tests and mock for LocaleSwitcher

---
+++ src/Akeneo/Pim/TableAttribute/front/__tests__/src/attributes/LocaleSwitcher.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {renderWithProviders} from '@akeneo-pim-community/legacy-bridge/tests/front/unit/utils';
+import {LocaleSwitcher} from '../../../src/attribute/LocaleSwitcher';
+import {act, fireEvent, screen} from '@testing-library/react';
+import {getLocales} from "../factories/Locales";
+jest.mock('../../../src/fetchers/LocaleFetcher');
+
+describe('LocaleSwitcher', () => {
+  it('should render a locale', () => {
+    act(() => {
+      renderWithProviders(<LocaleSwitcher localeCode={'en_US'} onChange={jest.fn()} locales={getLocales()} />);
+    });
+
+    expect(screen.getByText('English')).toBeInTheDocument();
+  });
+
+  it ('should display nothing if locale does not exists', async () => {
+    act(() => {
+      renderWithProviders(<LocaleSwitcher localeCode={'en_US'} onChange={jest.fn()} locales={[]} />);
+    });
+
+    expect(screen.queryByText('English')).not.toBeInTheDocument();
+  });
+
+  it ('should change a locale',  async() => {
+    const handleChange = jest.fn();
+    act(() => {
+      renderWithProviders(<LocaleSwitcher localeCode={'en_US'} onChange={handleChange} locales={getLocales()} />);
+    });
+
+    act(() => {
+      fireEvent.click(screen.getByText('English'));
+    });
+    expect(await screen.findByText('French')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('French'));
+    expect(handleChange).toBeCalledWith('fr_FR');
+  });
+});
---
+++ src/Akeneo/Pim/TableAttribute/front/src/attribute/__mocks__/LocaleSwitcher.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Locale, LocaleCode} from '@akeneo-pim-community/shared';
+
+type LocaleSwitcherProps = {
+  locales: Locale[];
+  localeCode: LocaleCode;
+  onChange: (localeCode: LocaleCode) => void;
+};
+
+const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({locales, onChange}) => {
+  return <>
+    {locales.map(locale => <button key={locale.code} onClick={() => onChange(locale.code)}>
+      Fake LocaleSwitcher {locale.code}
+    </button>)}
+  </>
+};
+
+export {LocaleSwitcher};
