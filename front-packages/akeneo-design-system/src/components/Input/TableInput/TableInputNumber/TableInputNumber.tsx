05f2a9a07f5252e9cea084f06b2cbca4f531ea9a
pierallard <pierallard@gmail.com>
2021-07-13 16:30:38 +0200

    CPM-232: Add errored parameter for fields

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.tsx
@@ -3,18 +3,28 @@ import styled, {css} from 'styled-components';
 import {NumberInput} from '../../NumberInput/NumberInput';
 import {AkeneoThemedProps, getColor} from '../../../../theme';
 
-const TableInputNumber = styled(NumberInput)<{highlighted: boolean} & AkeneoThemedProps>`
+const TableInputNumber = styled(NumberInput)<{highlighted: boolean; inError: boolean} & AkeneoThemedProps>`
   height: 39px;
   padding-left: 10px;
   padding-right: 10px;
   border-radius: 0;
   border: none;
 
-  ${({highlighted}) =>
-    highlighted
+  ${({highlighted, inError}) =>
+    highlighted && !inError
       ? css`
           background: ${getColor('green', 10)};
           box-shadow: 0 0 0 1px ${getColor('green', 80)};
+        `
+      : css`
+          background: none;
+        `};
+
+  ${({inError}) =>
+    inError
+      ? css`
+          background: ${getColor('red', 10)};
+          box-shadow: 0 0 0 1px ${getColor('red', 80)};
         `
       : css`
           background: none;
--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputText/TableInputText.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputText/TableInputText.tsx
@@ -3,18 +3,28 @@ import styled, {css} from 'styled-components';
 import {TextInput} from '../../TextInput/TextInput';
 import {AkeneoThemedProps, getColor} from '../../../../theme';
 
-const TableInputText = styled(TextInput)<{highlighted: boolean} & AkeneoThemedProps>`
+const TableInputText = styled(TextInput)<{highlighted: boolean; inError: boolean} & AkeneoThemedProps>`
   height: 39px;
   padding-left: 10px;
   padding-right: 10px;
   border-radius: 0;
   border: none;
 
-  ${({highlighted}) =>
-    highlighted
+  ${({highlighted, inError}) =>
+    highlighted && !inError
       ? css`
           background: ${getColor('green', 10)};
           box-shadow: 0 0 0 1px ${getColor('green', 80)};
+        `
+      : css`
+          background: none;
+        `};
+
+  ${({inError}) =>
+    inError
+      ? css`
+          background: ${getColor('red', 10)};
+          box-shadow: 0 0 0 1px ${getColor('red', 80)};
         `
       : css`
           background: none;
