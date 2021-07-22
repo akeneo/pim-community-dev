64479c6f7b7b0eef89bcf31b207f3a806a469f5d
Samir <samir.boulil@akeneo.com>
2021-07-22 14:09:02 +0200

    RAC-743: Check channel is active for scope key in context

--- src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Infrastructure/Connector/ConstraintCollectionProvider.php
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Infrastructure/Connector/ConstraintCollectionProvider.php
@@ -18,6 +18,7 @@ use Akeneo\Platform\TailoredExport\Infrastructure\Validation\Columns;
 use Akeneo\Platform\TailoredExport\Infrastructure\Validation\LocaleShouldBeActive;
 use Akeneo\Tool\Component\Batch\Job\JobInterface;
 use Akeneo\Tool\Component\Batch\Job\JobParameters\ConstraintCollectionProviderInterface;
+use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ConstraintCollectionProvider implements ConstraintCollectionProviderInterface
@@ -58,7 +59,7 @@ class ConstraintCollectionProvider implements ConstraintCollectionProviderInterf
                                 'context' => new Assert\Optional([
                                     new Assert\Collection([
                                         'fields' => [
-                                            'scope' => new Assert\Optional(),
+                                            'scope' => new Assert\Optional(new ChannelShouldExist()),
                                             'locale' => new Assert\Optional(),
                                             'locales' => new Assert\Optional(
                                                 [
