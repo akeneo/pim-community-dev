00d3fd7a068386e25be556bf4e44223caf869012
Romain D <romain.delouard@akeneo.com>
2021-07-22 14:43:08 +0200

    CXP-800 Pair review: Define final class for GetAllApps and GetAllAppsQuery

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/InternalApi/Controller/Marketplace/GetAllApps.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/InternalApi/Controller/Marketplace/GetAllApps.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class GetAllApps
+final class GetAllApps
 {
     private GetAllAppsQueryInterface $getAllAppsQuery;
     private MarketplaceAnalyticsGenerator $marketplaceAnalyticsGenerator;
--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Marketplace/GetAllAppsQuery.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Marketplace/GetAllAppsQuery.php
@@ -12,7 +12,7 @@ use Akeneo\Connectivity\Connection\Domain\Marketplace\Model\App;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class GetAllAppsQuery implements GetAllAppsQueryInterface
+final class GetAllAppsQuery implements GetAllAppsQueryInterface
 {
     private const MAX_REQUESTS = 10;
 
