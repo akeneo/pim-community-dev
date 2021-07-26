4a18b0838486cd1385e2e6532e968ffbd8022c9d
Julien Sanchez <sumbobyboys@gmail.com>
2021-07-26 17:19:33 +0200

    RAC-751: move permission interfaces in CE

---
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/Permission/GetViewableAttributeCodesForUserInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/*
+* This file is part of the Akeneo PIM Enterprise Edition.
+*
+* (c) 2014 Akeneo SAS (http://www.akeneo.com)
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Permission;
+
+interface GetViewableAttributeCodesForUserInterface
+{
+    public function forAttributeCodes(array $attributeCodes, int $userId): array;
+}
