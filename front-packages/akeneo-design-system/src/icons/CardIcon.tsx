d2d4e2b63fa318806dbc67daa5ec1831e79d141b
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-07-26 14:52:38 +0200

    PLG-392: Update navigation icons

--- front-packages/akeneo-design-system/src/icons/CardIcon.tsx
+++ front-packages/akeneo-design-system/src/icons/CardIcon.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import {IconProps} from './IconProps';
 
 const CardIcon = ({title, size = 24, color = 'currentColor', ...props}: IconProps) => (
-  <svg viewBox="0 0 24 24" width={size} height={size} {...props}>
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width={size} height={size} {...props}>
     {title && <title>{title}</title>}
     <path
-      d="M11 13v9H2v-9h9zm11 0v9h-9v-9h9zM11 2v9H2V2h9zm11 0v9h-9V2h9z"
+      d="M10.5 13.5v8h-8v-8h8zm11 0v8h-8v-8h8zm-11-11v8h-8v-8h8zm11 0v8h-8v-8h8z"
       stroke={color}
       fill="none"
       fillRule="evenodd"
       strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+      strokeLinejoin="round"/>
   </svg>
-);
+)
+;
 
 export {CardIcon};
