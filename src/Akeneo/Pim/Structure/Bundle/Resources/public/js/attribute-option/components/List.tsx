1aa0f200dcacce232386a1626fd52de633a84034
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-07-27 17:09:33 +0200

    PLG-302: Add Label column

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/List.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/List.tsx
@@ -8,6 +8,8 @@ import {useSortedAttributeOptions} from '../hooks/useSortedAttributeOptions';
 import ToggleButton from './ToggleButton';
 import ListItem, {DragItem} from './ListItem';
 import NewOptionPlaceholder from './NewOptionPlaceholder';
+import {Table} from 'akeneo-design-system';
+import styled from 'styled-components';
 
 interface ListProps {
   selectAttributeOption: (selectedOptionId: number | null) => void;
@@ -82,30 +84,65 @@ const List = ({
       <ToggleButton />
 
       <div className="AknAttributeOption-list-optionsList" role="attribute-options-list">
-        {sortedAttributeOptions !== null &&
-          sortedAttributeOptions.map((attributeOption: AttributeOption, index: number) => {
-            return (
-              <ListItem
-                key={attributeOption.code}
-                data={attributeOption}
-                selectAttributeOption={onSelectItem}
-                isSelected={selectedOptionId === attributeOption.id}
-                deleteAttributeOption={deleteAttributeOption}
-                moveAttributeOption={moveAttributeOption}
-                validateMoveAttributeOption={validateMoveAttributeOption}
-                dragItem={dragItem}
-                setDragItem={setDragItem}
-                index={index}
-              >
-                {extraData[attributeOption.code]}
-              </ListItem>
-            );
-          })}
+        <>
+          <SpacedTable>
+            <Table.Header sticky={44}>
+              <TableHeaderFirstCell>&nbsp;</TableHeaderFirstCell>
+              <TableHeaderLabelCell>{translate('pim_common.label')}</TableHeaderLabelCell>
+              <TableHeaderCodeCell>{translate('pim_common.code')}</TableHeaderCodeCell>
+            </Table.Header>
+          </SpacedTable>
+          {sortedAttributeOptions !== null &&
+            sortedAttributeOptions.map((attributeOption: AttributeOption, index: number) => {
+              return (
+                <ListItem
+                  key={attributeOption.code}
+                  data={attributeOption}
+                  selectAttributeOption={onSelectItem}
+                  isSelected={selectedOptionId === attributeOption.id}
+                  deleteAttributeOption={deleteAttributeOption}
+                  moveAttributeOption={moveAttributeOption}
+                  validateMoveAttributeOption={validateMoveAttributeOption}
+                  dragItem={dragItem}
+                  setDragItem={setDragItem}
+                  index={index}
+                >
+                  {extraData[attributeOption.code]}
+                </ListItem>
+              );
+            })}
+        </>
 
         {showNewOptionPlaceholder && <NewOptionPlaceholder cancelNewOption={cancelNewOption} />}
       </div>
     </div>
   );
 };
+
+const SpacedTable = styled(Table)`
+  th {
+    padding-top: 15px;
+  }
+`;
+
+const TableHeaderFirstCell = styled(Table.HeaderCell)`
+  width: 42px;
+`;
+
+const TableHeaderLabelCell = styled(Table.HeaderCell)`
+  width: 40%;
+  @media (max-width: 1440px) {
+    width: 32%;
+  }
+  & > span {
+    padding: 0;
+  }
+`;
+
+const TableHeaderCodeCell = styled(Table.HeaderCell)`
+  & > span {
+    padding: 0;
+  }
+`;
 
 export default List;
