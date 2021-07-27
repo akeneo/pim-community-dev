d36343c872629b78c9b1521e2640e4cf56422a3c
pierallard <pierallard@gmail.com>
2021-07-27 14:46:29 +0200

    CPM-257: Refactor everything

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/pages/attributes/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './CreateAttributeModal';
-export * from './SelectAttributeTypeModal';
--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/pages/index.ts
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/pages/index.ts
@@ -2,4 +2,4 @@ export * from './AttributeGroupsIndex';
 export * from './LocalesIndex';
 export * from './AssociationTypesIndex';
 export * from './categories';
-export * from './attributes';
+export * from './CreateAttributeData';
