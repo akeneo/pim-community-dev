c6573c6533da95c1cdfd056b7da3e1917d8b89d3
pierallard <pierallard@gmail.com>
2021-07-27 14:46:29 +0200

    CPM-257: Refactor everything

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/attribute/form/CreateAttributeButtonApp.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import SelectAttributeType from "./SelectAttributeType";
+import {Button, useBooleanState} from "akeneo-design-system";
+
+export type CreateAttributeButtonStepProps = {
+  onClose: () => void;
+  onStepConfirm: (data: AttributeData) => void;
+  initialData?: AttributeData;
+}
+
+export type CreateAttributeButtonAppProps = {
+  buttonTitle: string;
+  iconsMap: {[attributeType: string]: string};
+  steps: {[attributeType: string]: { view: React.FC<CreateAttributeButtonStepProps> }[]};
+  isModalOpen?: boolean;
+  onClick: (data: AttributeData) => void;
+  initialData?: AttributeData;
+}
+
+type AttributeType = string;
+
+type AttributeData = {
+  attribute_type?: AttributeType
+} & {[key: string]: any};
+
+const CreateAttributeButtonApp: React.FC<CreateAttributeButtonAppProps> = ({
+  buttonTitle,
+  steps,
+  iconsMap,
+  isModalOpen = false,
+  onClick,
+  initialData = {},
+}) => {
+  const [isOpen, open, close] = useBooleanState(isModalOpen);
+  const [attributeData, setAttributeData] = React.useState<AttributeData>(initialData);
+  const [currentStepIndex, setCurrentStepIndex] = React.useState<number>(-1);
+
+  let stepsForAttributeType = steps['default'];
+  if (attributeData.attribute_type) {
+    if (attributeData.attribute_type in steps) {
+      stepsForAttributeType = steps[attributeData.attribute_type];
+    }
+  }
+
+  const handleStepConfirm = (data: AttributeData) => {
+    const newData = {...attributeData, ...data};
+    setAttributeData(newData);
+    if (currentStepIndex + 1 === Object.keys(stepsForAttributeType).length) {
+      close();
+      onClick(newData);
+
+      return;
+    }
+
+    setCurrentStepIndex(currentStepIndex + 1);
+  }
+
+  const handleClose = () => {
+    setCurrentStepIndex(-1);
+    setAttributeData(initialData);
+    close();
+  }
+
+  return <>
+    {isOpen && <>
+      {(currentStepIndex === -1) && <SelectAttributeType onClose={handleClose} iconsMap={iconsMap} onStepConfirm={handleStepConfirm}/>}
+      {stepsForAttributeType.map((step, stepIndex) => {
+        const Component = step.view;
+        return stepIndex === currentStepIndex && <Component
+          key={stepIndex}
+          onClose={handleClose}
+          onStepConfirm={handleStepConfirm}
+          initialData={attributeData}
+        />
+      })}
+    </>}
+    <Button id="attribute-create-button" onClick={open}>
+      {buttonTitle}
+    </Button>
+  </>
+}
+
+export {CreateAttributeButtonApp};
