dbef15d20a3a663dbca25e27e842471690393f20
Laurent Petard <laurent.petard@akeneo.com>
2021-07-28 14:46:12 +0200

    Fix copyrights in Free-Trial

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Controller/RetrieveUsersController.php
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Controller/RetrieveUsersController.php
@@ -2,16 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Akeneo\FreeTrial\Infrastructure\Controller;
 
 use Akeneo\FreeTrial\Domain\Model\InvitedUser;
 use Akeneo\FreeTrial\Domain\Query\GetInvitedUsersQuery;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-/**
- * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 class RetrieveUsersController
 {
     private GetInvitedUsersQuery $getInvitedUsersQuery;
--- tria/src/Akeneo/FreeTrial/tests/back/Integration/MySql/MySqlGetInvitedUsersQueryIntegration.php
+++ tria/src/Akeneo/FreeTrial/tests/back/Integration/MySql/MySqlGetInvitedUsersQueryIntegration.php
@@ -2,16 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Akeneo\Test\FreeTrial\Integration\MySql;
 
 use Akeneo\FreeTrial\Domain\Model\InvitedUser;
 use Akeneo\FreeTrial\Domain\ValueObject\InvitedUserStatus;
 use Akeneo\Test\Integration\TestCase;
 
-/**
- * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 final class MySqlGetInvitedUsersQueryIntegration extends TestCase
 {
     protected function getConfiguration()
