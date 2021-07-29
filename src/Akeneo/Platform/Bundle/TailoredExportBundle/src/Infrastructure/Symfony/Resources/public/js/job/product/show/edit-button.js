0d3de3cedad9b47d511110c65124b3d8ece85da5
Julien Sanchez <sumbobyboys@gmail.com>
2021-07-29 17:34:05 +0200

    RAC-751: working simple solution

--- src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Infrastructure/Symfony/Resources/public/js/job/product/show/edit-button.js
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Infrastructure/Symfony/Resources/public/js/job/product/show/edit-button.js
@@ -6,36 +6,36 @@
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-define(['underscore', 'pim/common/redirect', 'pim/fetcher-registry'], function(_, BaseRedirect, FetcherRegistry) {
+define(['underscore', 'oro/translator', 'pim/common/redirect', 'pim/fetcher-registry'], function(_, __, BaseRedirect, FetcherRegistry) {
   return BaseRedirect.extend({
+
+    /**
+     * {@inheritdoc}
+     */
+    render: function () {
+      this.isVisible().then(
+        function (isVisible) {
+          this.$el.html(
+            this.template({
+              label: __(this.config.label),
+              isVisible,
+              buttonClass: (this.config.buttonClass || 'AknButton--action') + (isVisible ? '' : ' AknButton--disabled'),
+            })
+          );
+        }.bind(this)
+      );
+
+      return this;
+    },
+
     /**
      * {@inheritdoc}
      */
     isVisible: function() {
-      const promise = $.Deferred();
-
-      const fetchCanEditTailoredExport = async (jobInstanceCode) => {
-        const response = await fetch(router.generate('', {jobInstanceCode}));
-        const {canEditJob} = response.json();
-
-        return canEditJob;
-      }
-
-      (() => {
-        const canEditJob = await fetchCanEditTailoredExport(this.getFormData().code);
-
-        promise.resolve(canEditJob);
-      })();
-
-      return promise.promise();
-
-      return $.deferred.allFetcherRegistry.getFetcher('permission')
-        .fetchAll()
+      return FetcherRegistry.getFetcher(`job-instance-${this.getFormData().type}`).fetch(this.getFormData().code)
         .then(
-          (permissions) => {
-            var permission = _.findWhere(permissions.job_instances, {code: this.getFormData().code});
-
-            return permission.edit;
+          (jobInstance) => {
+            return jobInstance.permissions.edit_tailored_export;
           }
         );
     },
