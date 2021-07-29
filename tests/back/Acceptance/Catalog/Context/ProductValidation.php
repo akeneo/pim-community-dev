8292a401fbce066bc2ee63fe7af5a598d4bd8f25
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-07-29 10:46:18 +0200

    CPM-278: update validation context

--- tests/back/Acceptance/Catalog/Context/ProductValidation.php
+++ tests/back/Acceptance/Catalog/Context/ProductValidation.php
@@ -22,17 +22,10 @@ use Webmozart\Assert\Assert;
  */
 final class ProductValidation implements Context
 {
-    /** @var ProductInterface */
-    private $updatedProduct;
-
-    /** @var Builder\Product */
-    private $productBuilder;
-
-    /** @var ValidatorInterface */
-    private $productValidator;
-
-    /** @var InMemoryProductRepository */
-    private $productRepository;
+    private Builder\Product $productBuilder;
+    private ValidatorInterface $productValidator;
+    private InMemoryProductRepository $productRepository;
+    private ProductInterface $updatedProduct;
 
     public function __construct(
         Builder\Product $productBuilder,
@@ -75,14 +68,35 @@ final class ProductValidation implements Context
     }
 
     /**
+     * @Then :violationCount violation is raised
+     * @Then :violationCount violations are raised
+     *
+     * @throws \Exception
+     */
+    public function violationsAreRaised(int $violationCount): void
+    {
+        $violations = $this->productValidator->validate($this->updatedProduct);
+        if ($violationCount !== $violations->count()) {
+            throw new \Exception(sprintf(
+                '%d violation%s expected, %d violation%s found: %s',
+                $violationCount,
+                $violationCount > 1 ? 's' : '',
+                $violations->count(),
+                $violations->count() > 1 ? 's' : '',
+                $violations->__toString()
+            ));
+        }
+    }
+
+    /**
      * @Then the error :errorMessage is raised
+     * @Then the violation :errorMessage is raised
      *
      * @throws \Exception
      */
     public function theErrorIsRaised(string $errorMessage): void
     {
         $violations = $this->productValidator->validate($this->updatedProduct);
-
         if ($violations->count() === 0) {
             throw new \Exception(
                 sprintf('Expected error message "%s" but no violation was found', $errorMessage)
@@ -121,11 +135,52 @@ final class ProductValidation implements Context
     }
 
     /**
+     * @Then the error :errorMessage is raised at path :errorPath
+     * @Then the violation :errorMessage is raised at path :errorPath
+     *
+     * @throws \Exception
+     */
+    public function theErrorIsRaisedAtPath(string $errorMessage, string $errorPath): void
+    {
+        $violations = $this->productValidator->validate($this->updatedProduct);
+        if ($violations->count() === 0) {
+            throw new \Exception(
+                sprintf('Expected violation message "%s" but no violation was found', $errorMessage)
+            );
+        }
+
+        $messages = [];
+        $isFoundMessage = false;
+
+        foreach ($violations as $violation) {
+            print_r(get_class($violation));
+            $message = $violation->getMessage();
+            $path = $violation->getPropertyPath();
+            $messages[] = sprintf('path: %s, message: %s', $path, $message);
+            if ($message === $errorMessage && $path === $errorPath) {
+                $isFoundMessage = true;
+            }
+        }
+
+        if (!$isFoundMessage) {
+            throw new \Exception(
+                sprintf(
+                    'Expected violation message "%s" at path "%s" was not found, %s given',
+                    $errorMessage,
+                    $errorPath,
+                    implode(',', $messages)
+                )
+            );
+        }
+    }
+
+    /**
      * @Then no error is raised
+     * @Then no product violation is raised
      */
     public function noErrorIsRaised()
     {
         $violations = $this->productValidator->validate($this->updatedProduct);
-        Assert::count($violations, 0);
+        Assert::count($violations, 0, 'No violation should be raised, found: ' . $violations->__toString());
     }
 }
