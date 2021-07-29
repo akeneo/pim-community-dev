02e103357a3ef8cdccb4ece81ad73100722ae362
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-07-29 12:11:40 +0200

    PLG-308: Adapt List.unit.tsx

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/List.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/List.tsx
@@ -83,38 +83,36 @@ const List = ({
       </label>
       <ToggleButton />
 
-      <>
-        <SpacedTable>
-          <Table.Header sticky={44}>
-            <TableHeaderFirstCell>&nbsp;</TableHeaderFirstCell>
-            <TableHeaderLabelCell>{translate('pim_common.label')}</TableHeaderLabelCell>
-            <TableHeaderCodeCell>{translate('pim_common.code')}</TableHeaderCodeCell>
-          </Table.Header>
-        </SpacedTable>
-        <div className="AknAttributeOption-list-optionsList" role="attribute-options-list">
-          {sortedAttributeOptions !== null &&
-            sortedAttributeOptions.map((attributeOption: AttributeOption, index: number) => {
-              return (
-                <ListItem
-                  key={attributeOption.code}
-                  data={attributeOption}
-                  selectAttributeOption={onSelectItem}
-                  isSelected={selectedOptionId === attributeOption.id}
-                  deleteAttributeOption={deleteAttributeOption}
-                  moveAttributeOption={moveAttributeOption}
-                  validateMoveAttributeOption={validateMoveAttributeOption}
-                  dragItem={dragItem}
-                  setDragItem={setDragItem}
-                  index={index}
-                >
-                  {extraData[attributeOption.code]}
-                </ListItem>
-              );
-            })}
+      <SpacedTable>
+        <Table.Header sticky={44}>
+          <TableHeaderFirstCell>&nbsp;</TableHeaderFirstCell>
+          <TableHeaderLabelCell>{translate('pim_common.label')}</TableHeaderLabelCell>
+          <TableHeaderCodeCell>{translate('pim_common.code')}</TableHeaderCodeCell>
+        </Table.Header>
+      </SpacedTable>
+      <div className="AknAttributeOption-list-optionsList" role="attribute-options-list">
+        {sortedAttributeOptions !== null &&
+          sortedAttributeOptions.map((attributeOption: AttributeOption, index: number) => {
+            return (
+              <ListItem
+                key={attributeOption.code}
+                data={attributeOption}
+                selectAttributeOption={onSelectItem}
+                isSelected={selectedOptionId === attributeOption.id}
+                deleteAttributeOption={deleteAttributeOption}
+                moveAttributeOption={moveAttributeOption}
+                validateMoveAttributeOption={validateMoveAttributeOption}
+                dragItem={dragItem}
+                setDragItem={setDragItem}
+                index={index}
+              >
+                {extraData[attributeOption.code]}
+              </ListItem>
+            );
+          })}
 
-          {showNewOptionPlaceholder && <NewOptionPlaceholder cancelNewOption={cancelNewOption} />}
-        </div>
-      </>
+        {showNewOptionPlaceholder && <NewOptionPlaceholder cancelNewOption={cancelNewOption} />}
+      </div>
     </div>
   );
 };
