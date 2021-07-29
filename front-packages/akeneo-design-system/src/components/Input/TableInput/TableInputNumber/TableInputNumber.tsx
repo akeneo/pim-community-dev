ee8bfb4a0223e426b44af45b68367eaa85f166af
pierallard <pierallard@gmail.com>
2021-07-29 17:45:58 +0200

    CPM-181: Update Readonly on TableInput cells

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import {TableInputRow} from '../TableInputRow/TableInputRow';
 import styled, {css} from 'styled-components';
 import {NumberInput} from '../../NumberInput/NumberInput';
 import {AkeneoThemedProps, getColor} from '../../../../theme';
+import {Override} from '../../../../shared';
+import {InputProps} from '../../InputProps';
+import {TableInputReadOnlyCell} from '../TableInputReadOnlyCell';
+import {TableInputContext} from '../TableInputContext';
 
-const TableInputNumber = styled(NumberInput)<{highlighted: boolean; inError: boolean} & AkeneoThemedProps>`
+const EditableTableInputNumber = styled(NumberInput)<{highlighted: boolean; inError: boolean} & AkeneoThemedProps>`
   height: 39px;
   padding-left: 10px;
   padding-right: 35px;
@@ -30,6 +35,26 @@ const TableInputNumber = styled(NumberInput)<{highlighted: boolean; inError: boo
     box-shadow: 0 0 0 1px ${getColor('grey', 100)};
   }
 `;
+
+type TableInputNumberProps = Override<
+  Override<React.InputHTMLAttributes<HTMLInputElement>, InputProps<string>>,
+  {
+    highlighted?: boolean;
+    inError?: boolean;
+  }
+>;
+
+const TableInputNumber = ({children, value, ...rest}: TableInputNumberProps) => {
+  const {readOnly} = React.useContext(TableInputContext);
+  if (readOnly) {
+    return <TableInputReadOnlyCell title={value}>{value}</TableInputReadOnlyCell>;
+  } else
+    return (
+      <EditableTableInputNumber value={value} {...rest}>
+        {children}
+      </EditableTableInputNumber>
+    );
+};
 
 TableInputRow.displayName = 'TableInput.NumberInput';
 
