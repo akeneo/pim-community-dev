d5bcd95ad0d1ed79e4e084bac3b4a536cfe25269
pierallard <pierallard@gmail.com>
2021-07-29 17:45:58 +0200

    CPM-181: Update Readonly on TableInput cells

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.unit.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputNumber/TableInputNumber.unit.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import {TableInputNumber} from './TableInputNumber';
 import {render, screen} from '../../../../storybook/test-util';
-
-describe('TableInputNumber supports forwardRef', () => {
-  const handleChange = jest.fn();
-  const ref = {current: null};
-
-  render(<TableInputNumber id="myInput" value="12" onChange={handleChange} ref={ref} />);
-  expect(ref.current).not.toBe(null);
-});
+import {TableInput} from '../TableInput';
 
 test('TableInputNumber supports ...rest props', () => {
   const handleChange = jest.fn();
@@ -17,4 +10,21 @@ test('TableInputNumber supports ...rest props', () => {
     <TableInputNumber id="myInput" value="12" onChange={handleChange} data-testid="my_value" highlighted={true} />
   );
   expect(screen.getByTestId('my_value')).toBeInTheDocument();
+});
+
+test('it displays input in readonly mode', () => {
+  render(
+    <TableInput readOnly={true}>
+      <tbody>
+        <tr>
+          <td>
+            <TableInputNumber value="42" />
+          </td>
+        </tr>
+      </tbody>
+    </TableInput>
+  );
+
+  expect(screen.getByText('42')).toBeInTheDocument();
+  expect(screen.getByTitle('42')).toBeInTheDocument();
 });
