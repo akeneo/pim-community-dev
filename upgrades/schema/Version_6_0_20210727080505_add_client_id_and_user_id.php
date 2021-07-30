3543392d88e64a481aa8ac090c15a9a70c1010a3
Riff <riff.ventura@gmail.com>
2021-07-30 12:30:35 +0200

    CXP-820 Added integration test for the migration

--- upgrades/schema/Version_6_0_20210727080505_add_client_id_and_user_id.php
+++ upgrades/schema/Version_6_0_20210727080505_add_client_id_and_user_id.php
@@ -13,15 +13,14 @@ final class Version_6_0_20210727080505_add_client_id_and_user_id extends Abstrac
     public function up(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE pim_api_auth_code ADD client_id INT DEFAULT NULL, ADD user_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE pim_api_auth_code ADD CONSTRAINT FK_AD5DC7C619EB6921 FOREIGN KEY (client_id) REFERENCES pim_api_client (id)');
-        $this->addSql('ALTER TABLE pim_api_auth_code ADD CONSTRAINT FK_AD5DC7C6A76ED395 FOREIGN KEY (user_id) REFERENCES oro_user (id)');
-        $this->addSql('CREATE INDEX IDX_AD5DC7C619EB6921 ON pim_api_auth_code (client_id)');
-        $this->addSql('CREATE INDEX IDX_AD5DC7C6A76ED395 ON pim_api_auth_code (user_id)');
+        $this->addSql('ALTER TABLE pim_api_auth_code ADD CONSTRAINT FK_API_AUTH_CODE_CLIENT_ID FOREIGN KEY (client_id) REFERENCES pim_api_client (id)');
+        $this->addSql('ALTER TABLE pim_api_auth_code ADD CONSTRAINT FK_API_AUTH_CODE_USER_ID FOREIGN KEY (user_id) REFERENCES oro_user (id)');
+        $this->addSql('CREATE INDEX IDX_CLIENT_ID ON pim_api_auth_code (client_id)');
+        $this->addSql('CREATE INDEX IDX_USER_ID ON pim_api_auth_code (user_id)');
     }
 
     public function down(Schema $schema) : void
     {
         $this->throwIrreversibleMigrationException();
-
     }
 }
