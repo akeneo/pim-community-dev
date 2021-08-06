5056bd379bf0636c1b4751f7dc113ccdde99064a
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-08-06 17:08:25 +0200

    RAC-652: Add default value operation for measurement

--- src/Akeneo/Platform/Bundle/TailoredExportBundle/tests/Acceptance/UseCases/Attribute/HandleMeasurementValueTest.php
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/tests/Acceptance/UseCases/Attribute/HandleMeasurementValueTest.php
@@ -13,11 +13,13 @@ declare(strict_types=1);
 
 namespace Akeneo\Platform\TailoredExport\Test\Acceptance\UseCases\Attribute;
 
+use Akeneo\Platform\TailoredExport\Application\Query\Operation\DefaultValueOperation;
 use Akeneo\Platform\TailoredExport\Application\Query\Selection\Measurement\MeasurementUnitCodeSelection;
 use Akeneo\Platform\TailoredExport\Application\Query\Selection\Measurement\MeasurementUnitLabelSelection;
 use Akeneo\Platform\TailoredExport\Application\Query\Selection\Measurement\MeasurementValueSelection;
 use Akeneo\Platform\TailoredExport\Application\Query\Selection\SelectionInterface;
 use Akeneo\Platform\TailoredExport\Domain\SourceValue\MeasurementValue;
+use Akeneo\Platform\TailoredExport\Domain\SourceValue\NullValue;
 use Akeneo\Platform\TailoredExport\Domain\SourceValueInterface;
 use Akeneo\Platform\TailoredExport\Test\Acceptance\FakeServices\Measurement\InMemoryFindUnitLabel;
 use PHPUnit\Framework\Assert;
@@ -47,29 +49,49 @@ final class HandleMeasurementValueTest extends AttributeTestCase
     public function provider(): array
     {
         return [
-            [
+            'it selects the value' => [
                 'operations' => [],
                 'selection' => new MeasurementValueSelection(),
                 'value' => new MeasurementValue('10', 'KILOGRAM'),
                 'expected' => [self::TARGET_NAME => '10']
             ],
-            [
+            'it selects the unit code' => [
                 'operations' => [],
                 'selection' => new MeasurementUnitCodeSelection(),
                 'value' => new MeasurementValue('10', 'KILOGRAM'),
                 'expected' => [self::TARGET_NAME => 'KILOGRAM']
             ],
-            [
+            'it selects the unit label' => [
                 'operations' => [],
                 'selection' => new MeasurementUnitLabelSelection('weight', 'fr_FR'),
                 'value' => new MeasurementValue('10', 'KILOGRAM'),
                 'expected' => [self::TARGET_NAME => 'Kilogramme']
             ],
-            [
+            'it fallbacks on unit code when unit label is not found' => [
                 'operations' => [],
                 'selection' => new MeasurementUnitLabelSelection('weight', 'fr_FR'),
                 'value' => new MeasurementValue('10', 'GRAM'),
                 'expected' => [self::TARGET_NAME => '[GRAM]']
+            ],
+            'it applies default value operation when value is null' => [
+                'operations' => [
+                    DefaultValueOperation::createFromNormalized([
+                        'value' => 'n/a'
+                    ])
+                ],
+                'selection' => new MeasurementValueSelection(),
+                'value' => new NullValue(),
+                'expected' => [self::TARGET_NAME => 'n/a']
+            ],
+            'it does not apply default value operation when value is not null' => [
+                'operations' => [
+                    DefaultValueOperation::createFromNormalized([
+                        'value' => 'n/a'
+                    ])
+                ],
+                'selection' => new MeasurementValueSelection(),
+                'value' => new MeasurementValue('10', 'KILOGRAM'),
+                'expected' => [self::TARGET_NAME => '10']
             ],
         ];
     }
