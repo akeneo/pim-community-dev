21ff20c89c16b3775122a55564529d5174e292e7
Vincent <vincent.gelot@akeneo.com>
2021-08-06 16:01:44 +0200

    change return value

--- src/Akeneo/Platform/Bundle/InstallerBundle/FixtureLoader/FixturePathProvider.php
+++ src/Akeneo/Platform/Bundle/InstallerBundle/FixtureLoader/FixturePathProvider.php
@@ -36,7 +36,8 @@ class FixturePathProvider
         }
 
         if (null === $installerDataDir || !is_dir($installerDataDir)) {
-            throw new \RuntimeException('Installer data directory cannot be found.', 1);
+            throw new \RuntimeException('Installer data directory cannot be found.');
+            return 1;
         }
 
         if (DIRECTORY_SEPARATOR !== substr($installerDataDir, -1, 1)) {
