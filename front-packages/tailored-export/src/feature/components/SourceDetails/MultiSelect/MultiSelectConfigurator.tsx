698f3f3df7a237fdffdd645d0caa0e7c75ab226e
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-08-06 17:58:13 +0200

    RAC-629: Add default value operation for multi select

--- front-packages/tailored-export/src/feature/components/SourceDetails/MultiSelect/MultiSelectConfigurator.tsx
+++ front-packages/tailored-export/src/feature/components/SourceDetails/MultiSelect/MultiSelectConfigurator.tsx
@@ -4,6 +4,7 @@ import {AttributeConfiguratorProps} from '../../../models';
 import {CodeLabelCollectionSelector} from '../common/CodeLabelCollectionSelector';
 import {isMultiSelectSource} from './model';
 import {InvalidAttributeSourceError} from '../error';
+import {DefaultValue, Operations} from '../common';
 
 const MultiSelectConfigurator = ({source, validationErrors, onSourceChange}: AttributeConfiguratorProps) => {
   if (!isMultiSelectSource(source)) {
@@ -11,11 +12,20 @@ const MultiSelectConfigurator = ({source, validationErrors, onSourceChange}: Att
   }
 
   return (
-    <CodeLabelCollectionSelector
-      selection={source.selection}
-      validationErrors={filterErrors(validationErrors, '[selection]')}
-      onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
-    />
+    <Operations>
+      <DefaultValue
+        operation={source.operations.default_value}
+        validationErrors={filterErrors(validationErrors, '[operations][default_value]')}
+        onOperationChange={updatedOperation =>
+          onSourceChange({...source, operations: {...source.operations, default_value: updatedOperation}})
+        }
+      />
+      <CodeLabelCollectionSelector
+        selection={source.selection}
+        validationErrors={filterErrors(validationErrors, '[selection]')}
+        onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
+      />
+    </Operations>
   );
 };
 
