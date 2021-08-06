e74f498b98ff307d8fe3b4b983691ff2d00bf448
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-08-06 18:17:19 +0200

    RAC-644: Add default value operation for reference entity multi

--- front-packages/tailored-export/src/feature/components/SourceDetails/ReferenceEntity/ReferenceEntityConfigurator.tsx
+++ front-packages/tailored-export/src/feature/components/SourceDetails/ReferenceEntity/ReferenceEntityConfigurator.tsx
@@ -4,6 +4,7 @@ import {AttributeConfiguratorProps} from '../../../models';
 import {CodeLabelSelector} from '../common/CodeLabelSelector';
 import {isReferenceEntitySource} from './model';
 import {InvalidAttributeSourceError} from '../error';
+import {DefaultValue, Operations} from '../common';
 
 const ReferenceEntityConfigurator = ({source, validationErrors, onSourceChange}: AttributeConfiguratorProps) => {
   if (!isReferenceEntitySource(source)) {
@@ -11,11 +12,20 @@ const ReferenceEntityConfigurator = ({source, validationErrors, onSourceChange}:
   }
 
   return (
-    <CodeLabelSelector
-      selection={source.selection}
-      validationErrors={filterErrors(validationErrors, '[selection]')}
-      onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
-    />
+    <Operations>
+      <DefaultValue
+        operation={source.operations.default_value}
+        validationErrors={filterErrors(validationErrors, '[operations][default_value]')}
+        onOperationChange={updatedOperation =>
+          onSourceChange({...source, operations: {...source.operations, default_value: updatedOperation}})
+        }
+      />
+      <CodeLabelSelector
+        selection={source.selection}
+        validationErrors={filterErrors(validationErrors, '[selection]')}
+        onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
+      />
+    </Operations>
   );
 };
 
