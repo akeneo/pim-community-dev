7ccb79a8ed1a36f2c58c34eb18fda223fac9b88b
Pierre Jolly <pie.jolly@gmail.com>
2021-08-09 11:13:32 +0200

    RAC-688: Add default value operation

--- front-packages/tailored-export/src/feature/components/SourceDetails/FamilyVariant/FamilyVariantConfigurator.tsx
+++ front-packages/tailored-export/src/feature/components/SourceDetails/FamilyVariant/FamilyVariantConfigurator.tsx
@@ -4,6 +4,7 @@ import {PropertyConfiguratorProps} from '../../../models';
 import {CodeLabelSelector} from '../common/CodeLabelSelector';
 import {isFamilyVariantSource} from './model';
 import {InvalidPropertySourceError} from '../error';
+import {DefaultValue, Operations} from "../common";
 
 const FamilyVariantConfigurator = ({source, validationErrors, onSourceChange}: PropertyConfiguratorProps) => {
   if (!isFamilyVariantSource(source)) {
@@ -11,11 +12,20 @@ const FamilyVariantConfigurator = ({source, validationErrors, onSourceChange}: P
   }
 
   return (
-    <CodeLabelSelector
-      selection={source.selection}
-      validationErrors={filterErrors(validationErrors, '[selection]')}
-      onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
-    />
+    <Operations>
+      <DefaultValue
+        operation={source.operations.default_value}
+        validationErrors={filterErrors(validationErrors, '[operations][default_value]')}
+        onOperationChange={updatedOperation =>
+          onSourceChange({...source, operations: {...source.operations, default_value: updatedOperation}})
+        }
+      />
+      <CodeLabelSelector
+        selection={source.selection}
+        validationErrors={filterErrors(validationErrors, '[selection]')}
+        onSelectionChange={updatedSelection => onSourceChange({...source, selection: updatedSelection})}
+      />
+    </Operations>
   );
 };
 
