45f9a99b7e822c6212ae27df3b2cf9e8a6f89585
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-08-10 16:46:36 +0200

    RAC-797: Move selection to domain

---
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Domain/Model/Selection/Parent/ParentLabelSelection.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\TailoredExport\Domain\Model\Selection\Parent;
+
+final class ParentLabelSelection implements ParentSelectionInterface
+{
+    public const TYPE = 'label';
+
+    private string $locale;
+    private string $channel;
+
+    public function __construct(
+        string $locale,
+        string $channel
+    ) {
+        $this->locale = $locale;
+        $this->channel = $channel;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function getChannel(): string
+    {
+        return $this->channel;
+    }
+}
---
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/src/Domain/Model/Selection/ReferenceEntityCollection/ReferenceEntityCollectionLabelSelection.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\TailoredExport\Domain\Model\Selection\ReferenceEntityCollection;
+
+final class ReferenceEntityCollectionLabelSelection implements ReferenceEntityCollectionSelectionInterface
+{
+    public const TYPE = 'label';
+
+    private string $separator;
+    private string $locale;
+    private string $referenceEntityCode;
+
+    public function __construct(
+        string $separator,
+        string $locale,
+        string $referenceEntityCode
+    ) {
+        $this->separator = $separator;
+        $this->locale = $locale;
+        $this->referenceEntityCode = $referenceEntityCode;
+    }
+
+    public function getSeparator(): string
+    {
+        return $this->separator;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function getReferenceEntityCode(): string
+    {
+        return $this->referenceEntityCode;
+    }
+}
