d56f7816c011cc33c94c44797581ccfa5bd91e77
pierallard <pierallard@gmail.com>
2021-08-16 14:02:43 +0200

    CPM-195: Add drag and drop for table input

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputContext.ts
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInputContext.ts
@@ -2,10 +2,14 @@ import {createContext} from 'react';
 
 type TableInputContextType = {
   readOnly: boolean;
+  isDragAndDroppable: boolean;
+  onReorder: ((reorderedIndices: number[]) => void) | undefined;
 };
 
 const TableInputContext = createContext<TableInputContextType>({
   readOnly: false,
+  isDragAndDroppable: false,
+  onReorder: undefined,
 });
 
 export {TableInputContext};
