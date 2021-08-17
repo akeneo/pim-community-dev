a0ba955d8351408ad6764620f037ad257d66ff9e
Samir <samir.boulil@akeneo.com>
2021-08-17 17:53:48 +0200

    RAC-841: Add unit test for Infrastructure/Query/Enrichment/FindProductModelLabels

---
+++ src/Akeneo/Platform/Bundle/TailoredExportBundle/tests/Specification/Infrastructure/Query/Enrichment/FindProductModelLabelsSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Specification\Akeneo\Platform\TailoredExport\Infrastructure\Query\Enrichment;
+
+use Akeneo\Pim\Enrichment\Component\Product\Query\GetProductModelLabelsInterface;
+use Akeneo\Platform\TailoredExport\Domain\Query\FindProductModelLabelsInterface;
+use PhpSpec\ObjectBehavior;
+
+class FindProductModelLabelsSpec extends ObjectBehavior
+{
+    public function let(
+        GetProductModelLabelsInterface $getProductModelLabels
+    ): void {
+        $this->beConstructedWith($getProductModelLabels);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->beAnInstanceOf(FindProductLabels::class);
+    }
+
+    public function it_finds_product_labels(
+        GetProductModelLabelsInterface $getProductModelLabels
+    ): void {
+        $productModelCodes = ['vneck', 'watch'];
+        $channelCode = 'ecommerce';
+        $localeCode = 'fr_FR';
+
+        $expectedLabel = ['vneck' => 'V-Neck', 'watch' => 'Montre'];
+        $getProductModelLabels->byCodesAndLocaleAndScope($productModelCodes, $localeCode, $channelCode)
+            ->willReturn($expectedLabel);
+
+        $this->byCodes($productModelCodes, $channelCode, $localeCode)->shouldReturn($expectedLabel);
+    }
+}
