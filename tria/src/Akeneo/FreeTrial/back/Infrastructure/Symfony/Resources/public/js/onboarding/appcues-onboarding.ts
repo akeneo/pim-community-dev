ad37b740950306c2a89862d583365ac9e94ecbcb
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-08-17 10:40:25 +0200

    PLG-314: remove comments

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
@@ -32,19 +32,9 @@ const AppcuesOnboarding: PimOnboarding = {
       }
 
       appcues.loadLaunchpad(element, {
-        //Optionally specify the position of the content relative to the Launchpad icon. Possible values are as followed:
-        //	- center (default value, i.e. bottom-center)
-        //	- left (i.e. bottom-left)
-        //	- right (i.e. bottom-right)
-        //	- top (i.e. top-center)
-        //	- top-left
-        //	- top-right
         position: "left",
-        // Optionally add a header or footer. This must be HTML.
         header: "<h1>Tutorials</h1>",
         footer: "<p>Your footer here</p>",
-        // And if you'd prefer to use a custom icon rather than the default "bell" icon, you can optionally pass
-        // in an icon as well. Make sure that src is set to the right resource, whether it's in your site's directory or hosted
         icon: '/bundles/akeneofreetrial/icons/LaunchpadIcon.svg'
       });
     });
