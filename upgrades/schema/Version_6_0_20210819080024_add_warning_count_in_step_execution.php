f6886335c77c8b389393a85996f836ece9424e03
Laurent Petard <laurent.petard@akeneo.com>
2021-08-19 14:31:47 +0200

    PIM-10004: Add column to store warnings count in step executions

---
+++ upgrades/schema/Version_6_0_20210819080024_add_warning_count_in_step_execution.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_6_0_20210819080024_add_warning_count_in_step_execution extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql(<<<SQL
+ALTER TABLE akeneo_batch_step_execution 
+ADD COLUMN warning_count INT NOT NULL;
+SQL
+        );
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
