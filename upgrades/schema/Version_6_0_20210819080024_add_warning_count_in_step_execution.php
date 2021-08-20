e079ba8a74ba351382fad32b4e860a250e5efa7d
Laurent Petard <laurent.petard@akeneo.com>
2021-08-20 14:47:37 +0200

    PIM-10004: Add table to persist migration status

--- upgrades/schema/Version_6_0_20210819080024_add_warning_count_in_step_execution.php
+++ upgrades/schema/Version_6_0_20210819080024_add_warning_count_in_step_execution.php
@@ -14,6 +14,17 @@ ALTER TABLE akeneo_batch_step_execution
 ADD COLUMN warning_count INT NOT NULL DEFAULT 0;
 SQL
         );
+
+        $this->addSql(<<<SQL
+CREATE TABLE IF NOT EXISTS pim_one_time_task (
+    `code` VARCHAR(100) PRIMARY KEY,
+    `status` VARCHAR(100) NOT NULL,
+    `start_time` DATETIME,
+    `end_time` DATETIME,
+    `values` JSON NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+SQL
+        );
     }
 
     public function down(Schema $schema) : void
--- upgrades/test_schema/Version_6_0_20210819080024_add_warning_count_in_step_execution_Integration.php
+++ upgrades/test_schema/Version_6_0_20210819080024_add_warning_count_in_step_execution_Integration.php
@@ -17,25 +17,23 @@ final class Version_6_0_20210819080024_add_warning_count_in_step_execution_Integ
         return $this->catalog->useMinimalCatalog();
     }
 
-    public function test_it_adds_warning_count_column_in_step_execution_table()
+    public function test_it_adds_warning_count_column_in_step_execution_table_and_create_one_time_task_table()
     {
         $dbConnection = $this->get('database_connection');
 
-        $dbConnection->executeQuery(
-            "ALTER TABLE akeneo_batch_step_execution DROP COLUMN warning_count;"
+        $dbConnection->executeQuery(<<<SQL
+ALTER TABLE akeneo_batch_step_execution DROP COLUMN warning_count;
+DROP TABLE pim_one_time_task;
+SQL
         );
 
         $this->reExecuteMigration(self::MIGRATION_LABEL);
 
-        $this->assertWarningCountColumnExists();
-    }
+        $schemaManager = $this->get('database_connection')->getSchemaManager();
 
-    private function assertWarningCountColumnExists(): void
-    {
-        $columns = $this->get('database_connection')
-            ->getSchemaManager()
-            ->listTableColumns('akeneo_batch_step_execution');
-
+        $columns = $schemaManager->listTableColumns('akeneo_batch_step_execution');
         $this->assertArrayHasKey('warning_count', $columns);
+
+        $this->assertTrue($schemaManager->tablesExist('pim_one_time_task'));
     }
 }
