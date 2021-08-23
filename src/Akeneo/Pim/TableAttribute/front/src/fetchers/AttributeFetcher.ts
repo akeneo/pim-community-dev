345107d90f62270ce946af2b929a8077c9be876a
pierallard <pierallard@gmail.com>
2021-08-23 11:41:11 +0200

    CPM-182: Fix import

--- src/Akeneo/Pim/TableAttribute/front/src/fetchers/AttributeFetcher.ts
+++ src/Akeneo/Pim/TableAttribute/front/src/fetchers/AttributeFetcher.ts
@@ -1,8 +1,7 @@
 import {Router} from '@akeneo-pim-community/shared';
 import {Attribute} from '../models/Attribute';
-import {AttributeCode} from '../../../../../AssetManager/front/platform/model/structure/attribute';
 
-const fetchAttribute = async (router: Router, attributeCode: AttributeCode): Promise<Attribute> => {
+const fetchAttribute = async (router: Router, attributeCode: string): Promise<Attribute> => {
   const url = router.generate('pim_enrich_attribute_rest_get', {identifier: attributeCode});
   const response = await fetch(url);
 
