d225d58b673ccacff0c06e77778d36649a621787
Steven VAIDIE <steven.vaidie@gmail.com>
2021-08-23 10:55:31 +0200

    RAC-768: Refactor

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/duplicate/DuplicateJob.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/duplicate/DuplicateJob.tsx
@@ -1,147 +1,130 @@
-import React, {useCallback, useRef, useState} from 'react';
-import {Button, ProductsIllustration, Modal, useAutoFocus} from 'akeneo-design-system';
+import React, {useRef, useState} from 'react';
+import {Button, ProductsIllustration, Modal, useAutoFocus, useBooleanState} from 'akeneo-design-system';
 import {
-    filterErrors,
-    TextField,
-    useTranslate,
-    Section,
-    useRoute,
-    NotificationLevel,
-    useRouter,
-    useNotify,
-    ValidationError,
+  filterErrors,
+  TextField,
+  useTranslate,
+  sanitize,
+  Section,
+  NotificationLevel,
+  useRouter,
+  useNotify,
+  ValidationError,
 } from '@akeneo-pim-community/shared';
 
 type DuplicateModalProps = {
-  title: string;
   subTitle: string;
   jobCodeToDuplicate: string;
   successRedirectRoute: string;
 };
 
-type DuplicateJobRequest = {
-    label: string;
-    code: string;
-};
-
 const DuplicateJob = ({
-    title,
-    subTitle,
-    successRedirectRoute,
-    jobCodeToDuplicate,
+  subTitle,
+  successRedirectRoute,
+  jobCodeToDuplicate,
 }: DuplicateModalProps) => {
-    const translate = useTranslate();
-    const notify = useNotify();
+  const translate = useTranslate();
+  const notify = useNotify();
+  const router = useRouter();
 
-    const duplicateRoute = useRoute('pim_enrich_job_instance_rest_duplicate', {code: jobCodeToDuplicate});
-    const router = useRouter();
-    const [duplicateJobRequest, setDuplicateJobRequest] = useState<DuplicateJobRequest>({label: '', code: ''});
-    const [isModalOpen, setModalOpen] = useState<boolean>(false);
-    const canDuplicate = '' !== duplicateJobRequest.label && '' !== duplicateJobRequest.code;
-    const labelInputRef = useRef(null);
-    useAutoFocus(labelInputRef);
-    const [validationErrors, setValidationErrors] = useState<ValidationError[]>([]);
+  const [label, setLabel] = useState('');
+  const [code, setCode] = useState('');
+  const labelInputRef = useRef(null);
+  const [isModalOpen, openModal, closeModal] = useBooleanState(false);
+  const [validationErrors, setValidationErrors] = useState<ValidationError[]>([]);
+  const canDuplicate = '' !== label && '' !== code;
 
-    const onCancel = () => {
-        setDuplicateJobRequest({label: '', code: ''});
-        setModalOpen(false);
-    };
+  useAutoFocus(labelInputRef);
 
-    const onConfirm = async () => {
-        const response = await fetch(
-            duplicateRoute,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest',
-                },
-                body: JSON.stringify(duplicateJobRequest),
-            }
-        );
+  const onCancel = () => {
+    setCode('');
+    setLabel('');
+    closeModal();
+  };
 
-        if (!response.ok) {
-            setValidationErrors([]);
-
-            try {
-                const json = await response.json();
-                setValidationErrors(json.normalized_errors);
-            } catch (error) {}
-
-            notify(NotificationLevel.ERROR, translate('pim_import_export.entity.job_instance.flash.duplicate.fail'));
-        } else {
-            const json = await response.json();
-
-            setModalOpen(false);
-            notify(NotificationLevel.SUCCESS, translate('pim_import_export.entity.job_instance.flash.duplicate.success'));
-            router.redirect(router.generate(successRedirectRoute, {code: json.code}));
-        }
-    };
-
-    const sanitizeCode = (value: string): string => {
-        const regex = /[a-zA-Z0-9_]/;
-
-        return value
-            .split('')
-            .filter((char: string) => char !== ' ')
-            .map((char: string) => (char.match(regex) ? char : '_'))
-            .join('');
-    };
-
-    const handleLabelChange = useCallback(
-        (newLabel: string) => {
-            const expectedSanitizedCode = sanitizeCode(duplicateJobRequest.label);
-            const newCode = expectedSanitizedCode === duplicateJobRequest.code
-                ? sanitizeCode(newLabel)
-                : duplicateJobRequest.code;
-            setDuplicateJobRequest({code: newCode, label: newLabel});
+  const onConfirm = async () => {
+    const response = await fetch(
+      router.generate('pim_enrich_job_instance_rest_duplicate', {code: jobCodeToDuplicate}),
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest',
         },
-        [duplicateJobRequest]
+        body: JSON.stringify({
+          code,
+          label
+        }),
+      }
     );
 
-    return (
-        <>
-            <div onClick={() => setModalOpen(true)}>{translate('pim_common.duplicate')}</div>
-            {isModalOpen && (
-                <Modal
-                    closeTitle={translate('pim_common.close')}
-                    onClose={onCancel}
-                    illustration={<ProductsIllustration />}
-                >
-                    <Modal.SectionTitle color="brand">{translate(subTitle)}</Modal.SectionTitle>
-                    <Modal.Title>
-                        {translate('pim_import_export.entity.job_instance.duplicate.title', {job_code: jobCodeToDuplicate})}
-                    </Modal.Title>
-                    <Section>
-                        <TextField
-                            label={translate('pim_common.label')}
-                            value={duplicateJobRequest.label}
-                            errors={filterErrors(validationErrors, 'label')}
-                            required={true}
-                            ref={labelInputRef}
-                            onChange={handleLabelChange}
-                        />
-                        <TextField
-                            label={translate('pim_common.code')}
-                            value={duplicateJobRequest.code}
-                            errors={filterErrors(validationErrors, 'code')}
-                            required={true}
-                            onChange={code =>
-                                setDuplicateJobRequest({...duplicateJobRequest, code})
-                            }
-                        />
-                    </Section>
-                    <Modal.BottomButtons>
-                        <Button level="tertiary" onClick={onCancel} >
-                            {translate('pim_common.cancel')}
-                        </Button>
-                        <Button level="primary" disabled={!canDuplicate} onClick={onConfirm}>
-                            {translate('pim_common.save')}
-                        </Button>
-                    </Modal.BottomButtons>
-                </Modal>)}
-        </>
-    );
+    setValidationErrors([]);
+
+    if (response.ok) {
+      closeModal();
+
+      const json = await response.json();
+      notify(NotificationLevel.SUCCESS, translate('pim_importexport.job_profile.edit.duplicate.flash.success'));
+      router.redirect(router.generate(successRedirectRoute, {code: json.code}));
+    } else {
+      try {
+        const json = await response.json();
+        setValidationErrors(json.normalized_errors);
+      } catch (error) {}
+
+      notify(NotificationLevel.ERROR, translate('pim_import_export.entity.job_instance.flash.update.fail'));
+    }
+  };
+
+  const handleLabelChange = (newLabel: string) => {
+    const expectedSanitizedCode = sanitize(label);
+    const newCode = expectedSanitizedCode === code ? sanitize(newLabel) : code;
+
+    setCode(newCode);
+    setLabel(newLabel);
+  };
+
+  return (
+    <>
+      <div onClick={openModal}>{translate('pim_common.duplicate')}</div>
+      {isModalOpen && (
+        <Modal
+          closeTitle={translate('pim_common.close')}
+          onClose={onCancel}
+          illustration={<ProductsIllustration />}
+        >
+          <Modal.SectionTitle color="brand">{translate(subTitle)}</Modal.SectionTitle>
+          <Modal.Title>
+            {translate('pim_import_export.entity.job_instance.duplicate.title', {job_code: jobCodeToDuplicate})}
+          </Modal.Title>
+          <Section>
+            <TextField
+              label={translate('pim_common.label')}
+              value={label}
+              errors={filterErrors(validationErrors, 'label')}
+              required={true}
+              ref={labelInputRef}
+              onChange={handleLabelChange}
+            />
+            <TextField
+              label={translate('pim_common.code')}
+              value={code}
+              errors={filterErrors(validationErrors, 'code')}
+              required={true}
+              onChange={setCode}
+            />
+          </Section>
+          <Modal.BottomButtons>
+            <Button level="tertiary" onClick={onCancel} >
+              {translate('pim_common.cancel')}
+            </Button>
+            <Button level="primary" disabled={!canDuplicate} onClick={onConfirm}>
+              {translate('pim_common.save')}
+            </Button>
+          </Modal.BottomButtons>
+        </Modal>)}
+    </>
+  );
 };
 
 export {DuplicateJob};
