01c8974f30fe369140013a2c9639bd40be1ccae0
Julian Prud'homme <rouliane@users.noreply.github.com>
2021-08-24 16:36:06 +0200

    Revert "Revert "PLG-297: Options - IMP UX""

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/Edit.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/Edit.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {useTranslate} from '@akeneo-pim-community/shared';
 import {AttributeOption, Locale} from '../model';
@@ -19,10 +19,9 @@ const Edit = ({option, saveAttributeOption}: EditProps) => {
     setUpdatedOption(option);
   }, [option]);
 
-  const onUpdateOptionLabel = (event: ChangeEvent<HTMLInputElement>, localeCode: string) => {
-    event.persist();
+  const onUpdateOptionLabel = (newLabel: string, localeCode: string) => {
     let updatedOption: AttributeOption = {...option};
-    updatedOption.optionValues[localeCode].value = event.target.value;
+    updatedOption.optionValues[localeCode].value = newLabel;
     setUpdatedOption(updatedOption);
   };
 
