cb21042b9213630daf7c3ac39f1b90ad661a3aa5
Julian Prud'homme <rouliane@users.noreply.github.com>
2021-08-24 12:19:40 +0200

    Revert "PLG-297: Options - IMP UX"

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/EmptyAttributeOptionsList.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute-option/components/EmptyAttributeOptionsList.tsx
@@ -17,7 +17,7 @@ const EmptyAttributeOptionsList = ({showNewOptionForm}: EmptyAttributeOptionsLis
       <div
         className="AknAttributeOption-emptyList-addLink"
         onClick={() => showNewOptionForm(true)}
-        data-testid="add-new-attribute-option-button"
+        role="add-new-attribute-option-button"
       >
         {translate('pim_enrich.entity.attribute_option.module.edit.add_option')}
       </div>
--- tests/front/unit/structure/attribute-option/components/AttributeOptionForm.unit.tsx
+++ tests/front/unit/structure/attribute-option/components/AttributeOptionForm.unit.tsx
@@ -1,103 +1,98 @@
-import React from 'react';
+import React from "react";
 import {fireEvent, render} from '@testing-library/react';
 
-import AttributeOptionForm from 'akeneopimstructure/js/attribute-option/components/AttributeOptionForm';
-import {EditingOptionContext} from 'akeneopimstructure/js/attribute-option/contexts/EditingOptionContext';
-import {pimTheme} from 'akeneo-design-system';
-import {ThemeProvider} from 'styled-components';
+import AttributeOptionForm from "akeneopimstructure/js/attribute-option/components/AttributeOptionForm";
+import {EditingOptionContext} from "akeneopimstructure/js/attribute-option/contexts/EditingOptionContext";
 
 describe('AttributeOptionForm', () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
-  const addRefMockFn = jest.fn();
-  const removeRefMockFn = jest.fn();
-  const onUpdateOptionLabelMockFn = jest.fn();
+    const addRefMockFn = jest.fn();
+    const removeRefMockFn = jest.fn();
+    const onUpdateOptionLabelMockFn = jest.fn();
 
-  const renderAttributeOptionFormWithContext = () => {
-    const {option, locale, onUpdateOptionLabel} = givenProps();
-    const providerState = {
-      option,
-      addRef: addRefMockFn,
-      removeRef: removeRefMockFn,
+    const renderAttributeOptionFormWithContext = () => {
+        const {option, locale, onUpdateOptionLabel} = givenProps();
+        const providerState = {
+            option,
+            addRef: addRefMockFn,
+            removeRef: removeRefMockFn
+        };
+
+        return render(
+            <EditingOptionContext.Provider value={providerState}>
+                <AttributeOptionForm option={option} locale={locale} onUpdateOptionLabel={onUpdateOptionLabel} />
+            </EditingOptionContext.Provider>
+        );
     };
 
-    return render(
-      <ThemeProvider theme={pimTheme}>
-        <EditingOptionContext.Provider value={providerState}>
-          <AttributeOptionForm option={option} locale={locale} onUpdateOptionLabel={onUpdateOptionLabel} />
-        </EditingOptionContext.Provider>
-      </ThemeProvider>
-    );
-  };
-
-  const givenProps = () => {
-    return {
-      option: {
-        id: 80,
-        code: 'black',
-        optionValues: {
-          en_US: {
-            id: 1,
-            value: 'Black 2',
-            locale: 'en_US',
-          },
-          fr_FR: {
-            id: 2,
-            value: 'Noir',
-            locale: 'fr_FR',
-          },
-        },
-      },
-      locale: {code: 'en_US', label: 'English (United States)'},
-      onUpdateOptionLabel: onUpdateOptionLabelMockFn,
+    const givenProps = () => {
+        return {
+            option: {
+                id: 80,
+                code: 'black',
+                optionValues: {
+                    en_US: {
+                        id: 1,
+                        value: 'Black 2',
+                        locale: 'en_US',
+                    },
+                    fr_FR: {
+                        id: 2,
+                        value: 'Noir',
+                        locale: 'fr_FR',
+                    },
+                },
+            },
+            locale: {code: 'en_US', label: 'English (United States)'},
+            onUpdateOptionLabel: onUpdateOptionLabelMockFn
+        };
     };
-  };
 
-  describe('on mount', () => {
-    it('should display label and input', () => {
-      const {queryByTestId, queryByText} = renderAttributeOptionFormWithContext();
-      const input = queryByTestId(/attribute-option-label/i);
+    describe('on mount', () => {
+        it('should display label and input', () => {
+            const {queryByRole, queryByText} = renderAttributeOptionFormWithContext();
+            const input = queryByRole(/attribute-option-label/i);
 
-      expect(input).not.toBeNull();
-      expect(queryByText('English (United States)')).not.toBeNull();
+            expect(input).not.toBeNull();
+            expect(queryByText('English (United States)')).not.toBeNull();
+        });
+
+        it('should dispatch the input was added', () => {
+            const {queryByRole} = renderAttributeOptionFormWithContext();
+            const input = queryByRole(/attribute-option-label/i);
+
+            expect(input).not.toBeNull();
+            expect(addRefMockFn).toHaveBeenCalledWith('en_US', {current: input});
+        });
     });
 
-    it('should dispatch the input was added', () => {
-      const {queryByTestId} = renderAttributeOptionFormWithContext();
-      const input = queryByTestId(/attribute-option-label/i);
+    describe('on unmount', () => {
+        it('should remove label and input', () => {
+            const {unmount, queryByRole, queryByText} = renderAttributeOptionFormWithContext();
 
-      expect(input).not.toBeNull();
-      expect(addRefMockFn).toHaveBeenCalledWith('en_US', {current: input});
+            unmount();
+
+            expect(queryByRole(/attribute-option-label/i)).toBeNull();
+            expect(queryByText('English (United States)')).toBeNull();
+        });
+
+        it('should dispatch the input was removed', () => {
+            const {unmount} = renderAttributeOptionFormWithContext();
+
+            unmount();
+            expect(removeRefMockFn).toHaveBeenCalledWith('en_US', {current:  null});
+        });
     });
-  });
 
-  describe('on unmount', () => {
-    it('should remove label and input', () => {
-      const {unmount, queryByTestId, queryByText} = renderAttributeOptionFormWithContext();
+    it('should dispatch the option was updated when the user changes the value', () => {
+        const {queryByRole} = renderAttributeOptionFormWithContext();
+        const input = queryByRole(/attribute-option-label/i);
 
-      unmount();
+        fireEvent.change(input, {target: {value: 'Black'}});
 
-      expect(queryByTestId(/attribute-option-label/i)).toBeNull();
-      expect(queryByText('English (United States)')).toBeNull();
+        expect(onUpdateOptionLabelMockFn).toHaveBeenCalled();
     });
-
-    it('should dispatch the input was removed', () => {
-      const {unmount} = renderAttributeOptionFormWithContext();
-
-      unmount();
-      expect(removeRefMockFn).toHaveBeenCalledWith('en_US', {current: null});
-    });
-  });
-
-  it('should dispatch the option was updated when the user changes the value', () => {
-    const {queryByTestId} = renderAttributeOptionFormWithContext();
-    const input = queryByTestId(/attribute-option-label/i);
-
-    // @ts-ignore
-    fireEvent.change(input, {target: {value: 'Black'}});
-
-    expect(onUpdateOptionLabelMockFn).toHaveBeenCalled();
-  });
 });
