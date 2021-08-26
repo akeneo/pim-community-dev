62696106cad0df37b428cfcc8cb4be1da04f5946
Julien Sanchez <sumbobyboys@gmail.com>
2021-08-26 10:40:37 +0200

    RAC-849: fix review commands

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/breadcrumb/JobBreadcrumb.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/breadcrumb/JobBreadcrumb.tsx
@@ -2,10 +2,11 @@ import {useRoute, useTranslate} from '@akeneo-pim-community/shared';
 import {Breadcrumb} from 'akeneo-design-system';
 import React from 'react';
 
-type JobBreadcrumbProps = {jobLabel: string, jobType: string};
+type JobBreadcrumbProps = {isEdit: boolean, jobCode: string, jobLabel: string, jobType: string};
 
-const JobBreadcrumb = ({jobLabel, jobType}: JobBreadcrumbProps) => {
+const JobBreadcrumb = ({isEdit, jobCode, jobLabel, jobType}: JobBreadcrumbProps) => {
   const jobIndexUrl = useRoute(`pim_importexport_${jobType}_profile_index`);
+  const jobShowUrl = useRoute(`pim_importexport_${jobType}_profile_edit`, {code: jobCode});
   const translate = useTranslate();
 
   return (
@@ -13,9 +14,14 @@ const JobBreadcrumb = ({jobLabel, jobType}: JobBreadcrumbProps) => {
       <Breadcrumb.Step href={`#${jobIndexUrl}`}>
         {translate(`pim_menu.tab.${jobType}s`)}
       </Breadcrumb.Step>
-      <Breadcrumb.Step>
+      <Breadcrumb.Step href={`#${jobShowUrl}`}>
         {jobLabel}
       </Breadcrumb.Step>
+      {isEdit && (
+        <Breadcrumb.Step>
+          {translate('pim_common.edit')}
+        </Breadcrumb.Step>
+      )}
     </Breadcrumb>
   )
 }
--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/breadcrumb/breadcrumb.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/job/common/breadcrumb/breadcrumb.ts
@@ -1,11 +1,24 @@
 import {JobBreadcrumb} from './JobBreadcrumb';
 import BaseForm = require('../../../view/base');
 
+type Config = {
+  isEdit: boolean;
+};
+
 class Breadcrumb extends BaseForm {
+  private readonly config: Config;
+
+  constructor(options: {config: Config}) {
+    super({...options});
+
+    this.config = options.config;
+  }
+
   public render(): BaseForm {
     this.renderReact(
       JobBreadcrumb,
       {
+        isEdit: this.config.isEdit,
         jobLabel: this.getFormData().label,
         jobType: this.getFormData().type
       },
