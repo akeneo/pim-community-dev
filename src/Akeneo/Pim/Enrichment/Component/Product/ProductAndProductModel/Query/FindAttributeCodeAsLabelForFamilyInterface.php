e794498061434e577caaed7724f93abea4096254
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-08-27 12:07:09 +0200

    PLG-10019: fix phpdoc

--- src/Akeneo/Pim/Enrichment/Component/Product/ProductAndProductModel/Query/FindAttributeCodeAsLabelForFamilyInterface.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/ProductAndProductModel/Query/FindAttributeCodeAsLabelForFamilyInterface.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Enrichment\Component\Product\ProductAndProductModel\Query;
 
 /**
- * Count the total number of variant products for a given list of product models.
- *
- * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 interface FindAttributeCodeAsLabelForFamilyInterface
