cf7529239395736b45a9931a5c992a2d92896359
Olivier Pontier <olivier.pontier@akeneo.com>
2021-08-30 19:02:20 +0200

    Add Subscriber on Db creation for extacting data from the reference PIM before installing the catalog

---
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Install/EventSubscriber/ExtractCatalogSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2021 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\FreeTrial\Infrastructure\Install\EventSubscriber;
+
+use Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvent;
+use Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExtractCatalogSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            InstallerEvents::POST_DB_CREATE => 'extractCatalogOnDbCreate'
+        ];
+    }
+
+    public function extractCatalogOnDbCreate(InstallerEvent $event): void
+    {
+        $event->getCommandExecutor()->runCommand('akeneo:free-trial:extract-catalog');
+    }
+}
