863714d3cc144e5d829230220ade2bb492f9c487
pierallard <pierallard@gmail.com>
2021-08-31 10:07:25 +0200

    CPM-289: Fix DSM drag & drop

--- front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.unit.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TableInput/TableInput.unit.tsx
@@ -57,13 +57,24 @@ test('it can drag and drop', () => {
     </TableInput>
   );
 
+  let dataTransferred = '';
+  const dataTransfer = {
+    getData: (_format: string) => {
+      return dataTransferred;
+    },
+    setData: (_format: string, data: string) => {
+      dataTransferred = data;
+    },
+  };
+
+  // Move 2nd column to 4th place
   fireEvent.mouseDown(screen.getAllByTestId('dragAndDrop')[1]);
-  fireEvent.dragStart(screen.getAllByRole('row')[1]);
-  fireEvent.dragEnter(screen.getAllByRole('row')[2]);
-  fireEvent.dragLeave(screen.getAllByRole('row')[2]);
-  fireEvent.dragEnter(screen.getAllByRole('row')[3]);
-  fireEvent.drop(screen.getAllByRole('row')[3]);
-  fireEvent.dragEnd(screen.getAllByRole('row')[1]);
+  fireEvent.dragStart(screen.getAllByRole('row')[1], {dataTransfer});
+  fireEvent.dragEnter(screen.getAllByRole('row')[2], {dataTransfer});
+  fireEvent.dragLeave(screen.getAllByRole('row')[2], {dataTransfer});
+  fireEvent.dragEnter(screen.getAllByRole('row')[3], {dataTransfer});
+  fireEvent.drop(screen.getAllByRole('row')[3], {dataTransfer});
+  fireEvent.dragEnd(screen.getAllByRole('row')[1], {dataTransfer});
 
   expect(handleReorder).toHaveBeenCalledWith([1, 2, 0]);
 });
