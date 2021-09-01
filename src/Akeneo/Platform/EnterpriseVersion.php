2bca7b3d90b18a0847d481f4e1e732566d46d20c
Michel Tag <micheltag@akeneo.com>
2021-09-01 10:06:51 +0200

    Preparing version 4.0.123 (#11659)
    Preparing version 4.0.123
    
    
    Co-authored-by: alemaire <anaislem@gmail.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.122';
+    const VERSION = '4.0.123';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Daisy';
