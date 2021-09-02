3d7f0974cff090c0d403aeb2d169e5a4d4d78b95
Willy Mesnage <willy.mesnage@gmail.com>
2021-09-02 17:03:02 +0200

    CXP-723: fix test

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/AppWizardWithSteps/Permissions.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/AppWizardWithSteps/Permissions.test.tsx
@@ -6,5 +6,4 @@ import {Permissions} from '@src/connect/components/AppWizardWithSteps/Permission
 test('The permissions step renders without error', async () => {
     render(<Permissions />);
     await waitForElement(() => screen.getByText('Hello permissions!'));
-    expect(screen.getByText('Hello permissions!')).toBeInTheDocument();
 });
