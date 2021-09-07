baf868fa13b967edfcb4e4b0cefdf524d51e17c9
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-09-07 18:02:45 +0200

    PLG-541: Checklist #1 - Create an event view "furniture - to enrich" is saved

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
@@ -40,6 +40,11 @@ const AppcuesOnboarding: PimOnboarding = {
       }
 
       switch (event) {
+        case 'product-grid:view:saved':
+          if (eventOptions && eventOptions.name === 'Furniture - To enrich') {
+            appcues.track('View "Furniture - To enrich" saved');
+          }
+          break;
         case 'product-grid:view:selected':
           if (eventOptions && eventOptions.name === 'Furniture - To enrich') {
             appcues.track('View "Furniture - To enrich" selected');
