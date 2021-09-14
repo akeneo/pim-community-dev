a68fc10742a16d84592ba241980bfbbac3ab343e
pierallard <pierallard@gmail.com>
2021-09-14 16:47:43 +0200

    CPM-357: Refactor PDF renderer

---
+++ src/Akeneo/Pim/Enrichment/Bundle/PdfGeneration/Renderer/ProductValueRenderer/DefaultProductValueRenderer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Bundle\PdfGeneration\Renderer\ProductValueRenderer;
+
+use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
+use Twig\Environment;
+
+class DefaultProductValueRenderer implements ProductValueRenderer
+{
+    public function render(Environment $environment, AttributeInterface $attribute, $value): ?string
+    {
+        return twig_escape_filter($environment, $value);
+    }
+
+    public function supports($attributeType): bool
+    {
+        return true;
+    }
+}
