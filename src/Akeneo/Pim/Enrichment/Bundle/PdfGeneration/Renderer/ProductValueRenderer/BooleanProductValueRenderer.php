8599f1a90eec5a64531c63df22f9db523faf6c49
pierallard <pierallard@gmail.com>
2021-09-14 16:47:43 +0200

    CPM-357: Refactor PDF renderer

---
+++ src/Akeneo/Pim/Enrichment/Bundle/PdfGeneration/Renderer/ProductValueRenderer/BooleanProductValueRenderer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Akeneo\Pim\Enrichment\Bundle\PdfGeneration\Renderer\ProductValueRenderer;
+
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+
+class BooleanProductValueRenderer implements ProductValueRenderer
+{
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function render(Environment $environment, AttributeInterface $attribute, $value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return $this->translator->trans($value->getData() ? 'Yes' : 'No');
+    }
+
+    public function supports($attributeType): bool
+    {
+        return $attributeType === AttributeTypes::BOOLEAN;
+    }
+}
