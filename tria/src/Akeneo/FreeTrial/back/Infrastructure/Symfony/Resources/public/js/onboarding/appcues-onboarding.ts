0ba9013f4e9c003081495c5fb24613d5fe03c69d
Bryan Stéphan <bryan.stephan@akeneo.com>
2021-09-15 14:58:29 +0200

    PLG-497: Checklist #3 - Create an event on fill the attribute "Description" for the product "PLG513725"

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/onboarding/appcues-onboarding.ts
@@ -93,6 +93,10 @@ const AppcuesOnboarding: PimOnboarding = {
             appcues.track('Attribute "Designer" filled with "Studio Plumen"');
           }
 
+          if (eventOptions && eventOptions.attribute === 'description') {
+            appcues.track('Attribute "Description" filled in product edit form');
+          }
+
           appcues.track('Attribute filled in product edit form');
           break;
         case 'product:form:saved':
