655449632517a6f8292c5627966e5b99f8c61b13
Steven VAIDIE <steven.vaidie@gmail.com>
2021-09-15 16:45:31 +0200

    PIM-10073: DQI de-activation on attribute group is not fully taken into account

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/tests/front/unit/infrastructure/hooks/AttributeGroup/useFetchAllAttributeGroupsStatus.unit.ts
@@ -0,0 +1,30 @@
+import {fetchAllAttributeGroupsDqiStatus} from '@akeneo-pim-community/data-quality-insights/src/infrastructure/fetcher/AttributeGroup/attributeGroupDqiStatusFetcher';
+import {renderHook, act} from '@testing-library/react-hooks';
+import {useFetchAllAttributeGroupsStatus} from '@akeneo-pim-community/data-quality-insights/src/infrastructure/hooks';
+
+jest.mock(
+  '@akeneo-pim-community/data-quality-insights/src/infrastructure/fetcher/AttributeGroup/attributeGroupDqiStatusFetcher'
+);
+
+describe('useFetchAllAttributeGroupsStatus', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('it loads all attribute groups status for DQI', async () => {
+    fetchAllAttributeGroupsDqiStatus.mockResolvedValueOnce({erp: true, technical: true, marketing: true});
+
+    const {result} = renderHook(() => useFetchAllAttributeGroupsStatus());
+
+    expect(result.current.status).toEqual({});
+
+    await act(async () => result.current.load());
+
+    expect(result.current.status).toEqual({erp: true, technical: true, marketing: true});
+  });
+});
