d4e5a3950f6458f043726d84b2945ddcc3ce0724
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-09-21 12:02:30 +0200

    RAC-785: Add id on button to be able to get it by label text

--- front-packages/akeneo-design-system/src/components/SwitcherButton/SwitcherButton.unit.tsx
+++ front-packages/akeneo-design-system/src/components/SwitcherButton/SwitcherButton.unit.tsx
@@ -3,7 +3,7 @@ import {SwitcherButton} from './SwitcherButton';
 import {fireEvent, render, screen} from '../../storybook/test-util';
 
 test('it renders its children properly', () => {
-  render(<SwitcherButton label={'A label'}>A value</SwitcherButton>);
+  render(<SwitcherButton label="A label">A value</SwitcherButton>);
 
   expect(screen.getByText('A label:')).toBeInTheDocument();
   expect(screen.getByText('A value')).toBeInTheDocument();
@@ -12,29 +12,33 @@ test('it renders its children properly', () => {
 test('SwitcherButton supports forwardRef', () => {
   const ref = {current: null};
 
-  render(<SwitcherButton label={'A label'} ref={ref} />);
+  render(<SwitcherButton label="A label" ref={ref} />);
+
   expect(ref.current).not.toBe(null);
 });
 
 test('SwitcherButton supports ...rest props', () => {
-  render(<SwitcherButton label={'A label'} data-testid="my_value" />);
+  render(<SwitcherButton label="A label" data-testid="my_value" />);
+
   expect(screen.getByTestId('my_value')).toBeInTheDocument();
 });
 
 test('It calls deletion', () => {
   const onDelete = jest.fn();
 
-  render(<SwitcherButton label={'A label'} inline={false} deletable={true} onDelete={onDelete} />);
+  render(<SwitcherButton label="A label" inline={false} deletable={true} onDelete={onDelete} />);
 
   fireEvent.click(screen.getAllByRole('button')[1]);
+
   expect(onDelete).toBeCalledTimes(1);
 });
 
 test('It calls click', () => {
   const onClick = jest.fn();
 
-  render(<SwitcherButton label={'A label'} onClick={onClick} />);
+  render(<SwitcherButton label="A label" onClick={onClick} />);
 
-  fireEvent.click(screen.getAllByRole('button')[0]);
+  fireEvent.click(screen.getByLabelText('A label:'));
+
   expect(onClick).toBeCalledTimes(1);
 });
