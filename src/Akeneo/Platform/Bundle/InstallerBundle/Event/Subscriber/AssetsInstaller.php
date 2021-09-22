528c0e902fa8d84b691449ab867529744562c03e
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-09-22 14:50:45 +0200

    CPM-262: add copyright and licence

--- src/Akeneo/Platform/Bundle/InstallerBundle/Event/Subscriber/AssetsInstaller.php
+++ src/Akeneo/Platform/Bundle/InstallerBundle/Event/Subscriber/AssetsInstaller.php
@@ -8,6 +8,10 @@ use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class AssetsInstaller
 {
     private Filesystem $filesystem;
