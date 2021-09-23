9e89301d1ee29e40e71cd744f2d5bd236c424cc3
anaisbaunelemaire <anais.lemaire@akeneo.com>
2021-09-23 07:10:17 +0000

    Preparing version 4.0.124

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.123';
+    const VERSION = '4.0.124';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Tonic';
