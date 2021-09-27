b70448a1304cc1c49506df84305c585563b7755a
Tseho <quentin.favrie@gmail.com>
2021-09-27 14:36:36 +0200

    CXP-841: fix coupling issue

--- src/Akeneo/Pim/Permission/Bundle/Persistence/ORM/Category/GetRootCategoryReferenceFromCode.php
+++ src/Akeneo/Pim/Permission/Bundle/Persistence/ORM/Category/GetRootCategoryReferenceFromCode.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Permission\Bundle\Persistence\ORM\Category;
 
-use Akeneo\Pim\Enrichment\Component\Category\Model\Category;
 use Akeneo\Pim\Enrichment\Component\Category\Model\CategoryInterface;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\DBAL\Connection;
@@ -14,13 +13,16 @@ class GetRootCategoryReferenceFromCode
 {
     private Connection $connection;
     private Registry $doctrine;
+    private string $class;
 
     public function __construct(
         Connection $connection,
-        Registry $doctrine
+        Registry $doctrine,
+        string $class
     ) {
         $this->connection = $connection;
         $this->doctrine = $doctrine;
+        $this->class = $class;
     }
 
     /**
@@ -48,6 +50,6 @@ SQL;
             throw new \LogicException(sprintf('Expected %s, got %s', EntityManagerInterface::class, get_class($em)));
         }
 
-        return $em->getReference(Category::class, (int) $id);
+        return $em->getReference($this->class, (int) $id);
     }
 }
