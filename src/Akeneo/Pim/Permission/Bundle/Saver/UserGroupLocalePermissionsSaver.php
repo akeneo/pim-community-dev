8fe6f05a43de3b00c754a723aca45d505ea1bcc4
Riff <riff.ventura@gmail.com>
2021-09-30 05:57:57 +0200

    CXP-881 Added locale permissions saver

---
+++ src/Akeneo/Pim/Permission/Bundle/Saver/UserGroupLocalePermissionsSaver.php
@@ -0,0 +1,276 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Permission\Bundle\Saver;
+
+use Akeneo\Pim\Permission\Bundle\Manager\LocaleAccessManager;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetAllLocalesCodes;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetLocaleReferenceFromCode;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetLocalesAccessesWithHighestLevel;
+use Akeneo\Pim\Permission\Component\Attributes;
+use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\UserManagement\Bundle\Doctrine\ORM\Repository\GroupRepository;
+use Akeneo\UserManagement\Component\Model\GroupInterface;
+
+class UserGroupLocalePermissionsSaver
+{
+    private const DEFAULT_PERMISSION_EDIT = 'locale_edit';
+    private const DEFAULT_PERMISSION_VIEW = 'locale_view';
+
+    private const PERMISSION_EDIT = Attributes::EDIT_ITEMS;
+    private const PERMISSION_VIEW = Attributes::VIEW_ITEMS;
+
+    private LocaleAccessManager $localeAccessManager;
+    private GroupRepository $groupRepository;
+    private SaverInterface $groupSaver;
+    private GetAllLocalesCodes $getAllLocalesCodes;
+    private GetLocalesAccessesWithHighestLevel $getLocalesAccessesWithHighestLevel;
+    private GetLocaleReferenceFromCode $getLocaleReferenceFromCode;
+
+    public function __construct(
+        LocaleAccessManager $localeAccessManager,
+        GroupRepository $groupRepository,
+        SaverInterface $groupSaver,
+        GetAllLocalesCodes $getAllLocalesCodes,
+        GetLocalesAccessesWithHighestLevel $getLocalesAccessesWithHighestLevel,
+        GetLocaleReferenceFromCode $getLocaleReferenceFromCode
+    ) {
+        $this->groupRepository = $groupRepository;
+        $this->groupSaver = $groupSaver;
+        $this->localeAccessManager = $localeAccessManager;
+        $this->getAllLocalesCodes = $getAllLocalesCodes;
+        $this->getLocalesAccessesWithHighestLevel = $getLocalesAccessesWithHighestLevel;
+        $this->getLocaleReferenceFromCode = $getLocaleReferenceFromCode;
+    }
+
+    /**
+     * @param string $groupName
+     * @param array{
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     *
+     * @throws \LogicException
+     */
+    public function save(string $groupName, array $permissions): void
+    {
+        $group = $this->getGroup($groupName);
+        $this->updateDefaultPermissions($group, $permissions);
+
+        $affectedLocalesCodes = $this->getAffectedLocalesCodes($permissions);
+        $highestAccessLevelIndexedByLocaleCode = $this->getHighestAccessLevelIndexedByLocaleCode($affectedLocalesCodes, $permissions);
+        $existingHighestAccessLevelIndexedByLocaleCode = $this->getLocalesAccessesWithHighestLevel->execute($group->getId());
+
+        $removedCategoryCodes = array_diff(array_keys($existingHighestAccessLevelIndexedByLocaleCode), $affectedLocalesCodes);
+        $this->revokeAccesses($removedCategoryCodes, $group);
+
+        $this->updateAccesses($highestAccessLevelIndexedByLocaleCode, $existingHighestAccessLevelIndexedByLocaleCode, $group);
+
+    }
+
+    private function getGroup(string $groupName): GroupInterface
+    {
+        $group = $this->groupRepository->findOneByIdentifier($groupName);
+
+        if (null === $group) {
+            throw new \LogicException('User group not found');
+        }
+
+        return $group;
+    }
+
+    /**
+     * @param array{
+     *      own: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     */
+    private function updateDefaultPermissions(GroupInterface $group, array $permissions): void
+    {
+        $defaultPermissions = $group->getDefaultPermissions();
+
+        $currentHighestAll = $this->getCurrentHighestAll($defaultPermissions);
+        $submittedHighestAll = $this->getSubmittedHighestAll($permissions);
+
+        if ($currentHighestAll === $submittedHighestAll) {
+            return;
+        }
+
+        $group->setDefaultPermission(
+            self::DEFAULT_PERMISSION_VIEW,
+            in_array($submittedHighestAll, [self::PERMISSION_EDIT, self::PERMISSION_VIEW])
+        );
+        $group->setDefaultPermission(
+            self::DEFAULT_PERMISSION_EDIT,
+            $submittedHighestAll === self::PERMISSION_EDIT
+        );
+
+        $this->groupSaver->save($group);
+    }
+
+    /**
+     * @param array<string, bool>|null $defaultPermissions
+     */
+    private function getCurrentHighestAll(?array $defaultPermissions): ?string
+    {
+        if (true === ($defaultPermissions[self::DEFAULT_PERMISSION_EDIT] ?? null)) {
+            return self::PERMISSION_EDIT;
+        } elseif (true === ($defaultPermissions[self::DEFAULT_PERMISSION_VIEW] ?? null)) {
+            return self::PERMISSION_VIEW;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array{
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     */
+    private function getSubmittedHighestAll(array $permissions): ?string
+    {
+        if (true === $permissions['edit']['all']) {
+            return self::PERMISSION_EDIT;
+        } elseif (true === $permissions['view']['all']) {
+            return self::PERMISSION_VIEW;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array{
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     *
+     * @return string[]
+     */
+    private function getAffectedLocalesCodes(array $permissions): array
+    {
+        if ($permissions['edit']['all'] || $permissions['view']['all']) {
+            return $this->getAllLocalesCodes->execute();
+        }
+
+        return array_values(array_unique(array_merge(
+            $permissions['edit']['identifiers'],
+            $permissions['view']['identifiers'],
+        )));
+    }
+
+    /**
+     * @param string[] $affectedLocalesCodes
+     * @param array{
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     *
+     * @return array<string, string>
+     */
+    private function getHighestAccessLevelIndexedByLocaleCode(array $affectedLocalesCodes, array $permissions): array
+    {
+        $highestAccessLevelIndexedByLocaleCode = [];
+
+        foreach ($affectedLocalesCodes as $code) {
+            $highestAccessLevelIndexedByLocaleCode[$code] = $this->getHighestAccessLevelFromPermissions($code, $permissions);
+        }
+
+        return $highestAccessLevelIndexedByLocaleCode;
+    }
+
+    /**
+     * @param array{
+     *      edit: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      },
+     *      view: array{
+     *          all: bool,
+     *          identifiers: string[],
+     *      }
+     * } $permissions
+     */
+    private function getHighestAccessLevelFromPermissions(string $localeCode, array $permissions): string
+    {
+        if (true === $permissions['edit']['all'] || in_array($localeCode, $permissions['edit']['identifiers'])) {
+            return self::PERMISSION_EDIT;
+        } elseif (true === $permissions['view']['all'] || in_array($localeCode, $permissions['view']['identifiers'])) {
+            return self::PERMISSION_VIEW;
+        }
+
+        throw new \LogicException('Locale code is not covered by submitted permissions');
+    }
+
+    /**
+     * @param string[] $removedLocaleCodes
+     * @param GroupInterface $group
+     */
+    private function revokeAccesses(array $removedLocaleCodes, GroupInterface $group): void
+    {
+        foreach ($removedLocaleCodes as $localeCode) {
+            $locale = $this->getLocaleReferenceFromCode->execute($localeCode);
+            if (null === $locale) {
+                continue;
+            }
+            $this->localeAccessManager->revokeGroupAccess($locale, $group);
+        }
+    }
+
+    /**
+     * @param array<string, string> $highestAccessLevelIndexedByLocaleCode
+     * @param array<string, string> $existingHighestAccessLevelIndexedByLocaleCode
+     */
+    private function updateAccesses(
+        array $highestAccessLevelIndexedByLocaleCode,
+        array $existingHighestAccessLevelIndexedByLocaleCode,
+        GroupInterface $group
+    ): void {
+        foreach ($highestAccessLevelIndexedByLocaleCode as $localeCode => $newLevel) {
+            $existingLevel = $existingHighestAccessLevelIndexedByLocaleCode[$localeCode] ?? null;
+
+            if ($existingLevel !== $newLevel) {
+                $locale = $this->getLocaleReferenceFromCode->execute($localeCode);
+                if (null === $locale) {
+                    continue;
+                }
+                $this->localeAccessManager->grantAccess($locale, $group, $newLevel);
+            }
+        }
+    }
+}
---
+++ tests/back/Pim/Permission/Specification/Bundle/Saver/UserGroupLocalePermissionsSaverSpec.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Specification\Akeneo\Pim\Permission\Bundle\Saver;
+
+use Akeneo\Channel\Component\Model\LocaleInterface;
+use Akeneo\Pim\Permission\Bundle\Manager\LocaleAccessManager;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetAllLocalesCodes;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetLocaleReferenceFromCode;
+use Akeneo\Pim\Permission\Bundle\Persistence\ORM\Locale\GetLocalesAccessesWithHighestLevel;
+use Akeneo\Pim\Permission\Component\Attributes;
+use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
+use Akeneo\UserManagement\Bundle\Doctrine\ORM\Repository\GroupRepository;
+use Akeneo\UserManagement\Component\Model\GroupInterface;
+use PhpSpec\ObjectBehavior;
+
+class UserGroupLocalePermissionsSaverSpec extends ObjectBehavior
+{
+    function let(
+        LocaleAccessManager $localeAccessManager,
+        GroupRepository $groupRepository,
+        SaverInterface $groupSaver,
+        GetAllLocalesCodes $getAllLocalesCodes,
+        GetLocalesAccessesWithHighestLevel $getLocalesAccessesWithHighestLevel,
+        GetLocaleReferenceFromCode $getLocaleReferenceFromCode,
+        GroupInterface $group,
+        LocaleInterface $localeA,
+        LocaleInterface $localeB,
+        LocaleInterface $localeC
+    ) {
+        $group->getId()->willReturn(42);
+        $groupRepository->findOneByIdentifier('Redactor')->willReturn($group);
+
+        $localeA->getId()->willReturn(1);
+        $localeA->getCode()->willReturn('locale_a');
+        $localeB->getId()->willReturn(2);
+        $localeB->getCode()->willReturn('locale_b');
+        $localeC->getId()->willReturn(3);
+        $localeC->getCode()->willReturn('locale_c');
+
+        $getAllLocalesCodes->execute()->willReturn(['locale_a', 'locale_b', 'locale_c']);
+
+        $getLocaleReferenceFromCode->execute('locale_a')->willReturn($localeA);
+        $getLocaleReferenceFromCode->execute('locale_b')->willReturn($localeB);
+        $getLocaleReferenceFromCode->execute('locale_c')->willReturn($localeC);
+
+        $getLocalesAccessesWithHighestLevel->execute(42)->willReturn([]);
+
+        $this->beConstructedWith(
+            $localeAccessManager,
+            $groupRepository,
+            $groupSaver,
+            $getAllLocalesCodes,
+            $getLocalesAccessesWithHighestLevel,
+            $getLocaleReferenceFromCode,
+        );
+    }
+
+    public function it_throws_logic_exception_when_group_is_not_found(GroupRepository $groupRepository)
+    {
+        $groupRepository->findOneByIdentifier('Redactor')->willReturn(null);
+        $this->shouldThrow(\LogicException::class)->during('save', ['Redactor', []]);
+    }
+
+    /**
+     * FROM nothing
+     * TO {"edit":{"all":true,"identifiers":[]},"view":{"all":true,"identifiers":[]}}
+     */
+    public function it_GrantPermissionsOnExistingLocalesWhenTheAllByDefaultOptionIsEnabled(
+        SaverInterface $groupSaver,
+        LocaleAccessManager $localeAccessManager,
+        GroupInterface $group,
+        LocaleInterface $localeA,
+        LocaleInterface $localeB,
+        LocaleInterface $localeC
+    ) {
+        $group->getDefaultPermissions()->willReturn([]);
+        $group->setDefaultPermission('locale_view', true)->shouldBeCalled();
+        $group->setDefaultPermission('locale_edit', true)->shouldBeCalled();
+        $groupSaver->save($group)->shouldBeCalled();
+
+        $localeAccessManager->grantAccess($localeA, $group, Attributes::EDIT_ITEMS)->shouldBeCalled();
+        $localeAccessManager->grantAccess($localeB, $group, Attributes::EDIT_ITEMS)->shouldBeCalled();
+        $localeAccessManager->grantAccess($localeC, $group, Attributes::EDIT_ITEMS)->shouldBeCalled();
+
+        $this->save('Redactor', [
+            'edit' => [
+                'all' => true,
+                'identifiers' => [],
+            ],
+            'view' => [
+                'all' => true,
+                'identifiers' => [],
+            ],
+        ]);
+    }
+}
