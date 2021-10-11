2ab02a0707dafedf41ba526cceb6d66d88860e64
Steven VAIDIE <steven.vaidie@gmail.com>
2021-10-11 14:32:47 +0200

    RAC-univ: Fix error

--- front-packages/akeneo-design-system/src/storybook/test-util.tsx
+++ front-packages/akeneo-design-system/src/storybook/test-util.tsx
@@ -1,10 +1,10 @@
 import React, {ComponentType, ReactElement} from 'react';
-import {render, RenderOptions} from '@testing-library/react';
+import {render, RenderResult, RenderOptions} from '@testing-library/react';
 import {ThemeProvider} from 'styled-components';
 import {pimTheme} from '../theme/pim';
 
 const wrapper: ComponentType = ({children}) => <ThemeProvider theme={pimTheme}>{children}</ThemeProvider>;
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) => render(ui, {wrapper, ...options});
+const customRender:(ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) => RenderResult = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) => render(ui, {wrapper, ...options});
 
 export * from '@testing-library/react';
 export {customRender as render};
