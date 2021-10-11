20fb39628ef083d5ffdbd95c74b6360e4e4318e9
Steven VAIDIE <steven.vaidie@gmail.com>
2021-10-11 15:47:24 +0200

    RAC-UNIV: fix @types/react-draft-wysiwyg and @types/draft-js to be sure that the packages are compatible

--- front-packages/akeneo-design-system/src/components/Input/TextAreaInput/RichTextEditor.tsx
+++ front-packages/akeneo-design-system/src/components/Input/TextAreaInput/RichTextEditor.tsx
@@ -46,9 +46,9 @@ const RichTextEditor = ({value, readOnly = false, onChange, ...rest}: RichTextEd
           options: ['bold', 'italic'],
         },
       }}
-      onEditorStateChange={handleChange as any}
+      onEditorStateChange={handleChange}
       {...rest}
-      editorState={editorState as any}
+      editorState={editorState}
     />
   );
 };
