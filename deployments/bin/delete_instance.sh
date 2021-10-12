e354ce1c6ebdfbc07bebbc9cd425afd689988f7c
adesaegher <adesaegher@gmail.com>
2021-10-12 12:14:55 +0200

    add target on srnt_es_bucket (#11982)

--- deployments/bin/delete_instance.sh
+++ deployments/bin/delete_instance.sh
@@ -74,8 +74,8 @@ terraform init
 find ${NAMESPACE_PATH}/../../  -name "*.tf" -type f | xargs sed -i "s/prevent_destroy = true/prevent_destroy = false/g"
 yq w -j -P -i ${PWD}/main.tf.json module.pim.force_destroy_storage true
 export TF_VAR_force_destroy_storage=true
-terraform plan -target=module.pim.local_file.kubeconfig -target=module.pim.google_storage_bucket.srnt_bucket
-terraform apply ${TF_INPUT_FALSE} ${TF_AUTO_APPROVE} -target=module.pim.local_file.kubeconfig -target=module.pim.google_storage_bucket.srnt_bucket
+terraform plan -target=module.pim.local_file.kubeconfig -target=module.pim.google_storage_bucket.srnt_bucket -target=module.pim.google_storage_bucket.srnt_es_bucket
+terraform apply ${TF_INPUT_FALSE} ${TF_AUTO_APPROVE} -target=module.pim.local_file.kubeconfig -target=module.pim.google_storage_bucket.srnt_bucket -target=module.pim.google_storage_bucket.srnt_es_bucket
 
 echo "2 - removing deployment and terraform resources"
 export KUBECONFIG=.kubeconfig
