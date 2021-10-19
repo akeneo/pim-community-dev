48d6ee31259b6a69bf90178ea71c370377a0e835
Willy Mesnage <willy.mesnage@gmail.com>
2021-10-19 16:00:29 +0200

    CXP-870: Init RedeemCodeForToken

---
+++ src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Application/Apps/Service/RedeemCodeForTokenSpec.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Akeneo\Connectivity\Connection\Application\Apps\Service;
+
+use Akeneo\Connectivity\Connection\Application\Apps\Service\RedeemCodeForToken;
+use PhpSpec\ObjectBehavior;
+
+class RedeemCodeForTokenSpec extends ObjectBehavior
+{
+    public function it_is_a_redeem_code_for_token(): void
+    {
+        $this->shouldHaveType(RedeemCodeForToken::class);
+    }
+}
