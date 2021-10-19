8de7373046fa2f6af16d0590eb7325478bb3c21b
Willy Mesnage <willy.mesnage@gmail.com>
2021-10-19 16:04:00 +0200

    CXP-870: Implements interface

--- src/Akeneo/Connectivity/Connection/back/Application/Apps/Service/RedeemCodeForToken.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Apps/Service/RedeemCodeForToken.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Akeneo\Connectivity\Connection\Application\Apps\Service;
 
-class RedeemCodeForToken
+class RedeemCodeForToken implements RedeemCodeForTokenInterface
 {
-
+    public function redeem(string $code): string
+    {
+        // TODO: Implement redeem() method.
+    }
 }
--- src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Application/Apps/Service/RedeemCodeForTokenSpec.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Application/Apps/Service/RedeemCodeForTokenSpec.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace spec\Akeneo\Connectivity\Connection\Application\Apps\Service;
 
 use Akeneo\Connectivity\Connection\Application\Apps\Service\RedeemCodeForToken;
+use Akeneo\Connectivity\Connection\Application\Apps\Service\RedeemCodeForTokenInterface;
 use PhpSpec\ObjectBehavior;
 
 class RedeemCodeForTokenSpec extends ObjectBehavior
@@ -12,5 +13,6 @@ class RedeemCodeForTokenSpec extends ObjectBehavior
     public function it_is_a_redeem_code_for_token(): void
     {
         $this->shouldHaveType(RedeemCodeForToken::class);
+        $this->shouldImplement(RedeemCodeForTokenInterface::class);
     }
 }
