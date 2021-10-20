d0f809867508c81f27647e43d899f4719f7d8a0f
Steven VAIDIE <steven.vaidie@gmail.com>
2021-10-20 08:52:39 +0200

    RAC-929: Remove copyright on CE files

--- src/Akeneo/Platform/Job/back/Infrastructure/Symfony/JobBundle.php
+++ src/Akeneo/Platform/Job/back/Infrastructure/Symfony/JobBundle.php
@@ -5,10 +5,6 @@ namespace Akeneo\Platform\Job\Infrastructure\Symfony;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * @copyright 2021 Akeneo SAS (https://www.akeneo.com)
- * @license https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 class JobBundle extends Bundle
 {
     /**
