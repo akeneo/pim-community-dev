d830d92152278f94cccb6adeeab29d83807be572
Grégoire Houssard <gregoire.houssard@akeneo.com>
2021-10-26 17:13:22 +0200

    RAC-942: Fix warning

--- src/Akeneo/Platform/Job/front/process-tracker/src/feature/hooks/useJobSearchResult.ts
+++ src/Akeneo/Platform/Job/front/process-tracker/src/feature/hooks/useJobSearchResult.ts
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {JobSearchResult} from '../models/JobSearchResult';
 import {useRoute, useIsMounted} from '@akeneo-pim-community/shared';
 
