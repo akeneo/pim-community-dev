55f150e35a4d0bad2cd1b109f8405bb5aae8170f
jmleroux <jmleroux.pro@gmail.com>
2021-11-07 17:00:11 +0100

    Use PhpStan deprecation extension

--- make-file/test.mk
+++ make-file/test.mk
@@ -124,3 +124,6 @@ endif
 test-database-structure: #Doc: test database structure
 	$(DOCKER_COMPOSE) run -e APP_DEBUG=1 -u www-data --rm php bash -c 'bin/console pimee:database:inspect -f --env=test && bin/console pimee:database:diff --env=test'
 
+.PHONY: find-deprecated
+find-deprecated:
+	$(DOCKER_COMPOSE) run -u www-data --rm php php -d memory_limit=1G vendor/bin/phpstan analyse src/Akeneo -c phpstan-deprecations.neon --level 1
