65b260f581d902ac20dfedfb70f8d83cea1dc015
vgelot <vincent.gelot@akeneo.com>
2021-11-08 09:56:53 +0100

    BH-68 - change source for srnt (#12064)
    * change source for srnt from github to GCP bucket
    * remove github tag for release

---
+++ deployments/automation/steps/20211103-srnt-change-source-github-to-bucket.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ ${STEP} == "PRE_INIT" ]; then
+    if [[ $(yq r "${PWD}/main.tf.json" 'module.pim.product_reference_type') == "serenity_instance" ]]; then
+        if [[ $(yq r "${PWD}/main.tf.json" 'module.pim.source') =~ gcs::https:\/\/www.googleapis.com ]]; then
+            echo "[INFO] Path for SRNT is already correct. Nothing to do!"
+        else
+            echo "[INFO] Changing the path for srnt in main.tf.json"
+            PIM_VERSION=$(yq r "${PWD}/main.tf.json" 'module.pim.pim_version')
+            if [[ $(yq r "${PWD}/main.tf.json" 'module.pim.product_reference_code') =~ serenity_dev ]]; then
+                # Dev mode
+                yq w -j -P -i ${PWD}/main.tf.json 'module.pim.source' "gcs::https://www.googleapis.com/storage/v1/akecld-terraform-modules/serenity-edition-dev/${PIM_VERSION}//deployments/terraform"
+                yq w -j -P -i ${PWD}/main.tf.json 'module.pim-monitoring.source' "gcs::https://www.googleapis.com/storage/v1/akecld-terraform-modules/serenity-edition-dev/${PIM_VERSION}//deployments/terraform/monitoring"
+            else
+                # Prod mode
+                yq w -j -P -i ${PWD}/main.tf.json 'module.pim.source' "gcs::https://www.googleapis.com/storage/v1/akecld-terraform-modules/serenity-edition/${PIM_VERSION}//deployments/terraform"
+                yq w -j -P -i ${PWD}/main.tf.json 'module.pim-monitoring.source' "gcs::https://www.googleapis.com/storage/v1/akecld-terraform-modules/serenity-edition/${PIM_VERSION}//deployments/terraform/monitoring"
+            fi
+        fi
+    else
+        echo "[INFO] Not a SRNT release. Nothing to do!"
+    fi
+fi
