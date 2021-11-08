8c4acae9f626169af3cba4523cbcca08f750bf23
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-11-08 17:26:31 +0100

    CPM-261: add migration to create job

--- upgrades/schema/Version_6_0_20211108135619_create_clean_table_values_following_deleted_options_job.php
+++ upgrades/schema/Version_6_0_20211108135619_create_clean_table_values_following_deleted_options_job.php
@@ -7,20 +7,24 @@ namespace Pim\Upgrade\Schema;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version6020211108135619Create extends AbstractMigration
+final class Version_6_0_20211108135619_create_clean_table_values_following_deleted_options_job extends AbstractMigration
 {
     private const JOB_CODE = 'clean_table_values_following_deleted_options';
 
     public function getDescription() : string
     {
-        return '';
+        return sprintf('Create the "%s" job instance', self::JOB_CODE);
     }
 
     public function up(Schema $schema) : void
     {
+        if ($this->jobAlreadyExists()) {
+            $this->write('Job already exists');
+            $this->disableMigrationWarning();
+
+            return;
+        }
+
         $createJobSql = <<<SQL
         INSERT INTO `akeneo_batch_job_instance` (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
         VALUES (
@@ -42,17 +46,37 @@ final class Version6020211108135619Create extends AbstractMigration
 
         $updatePermissionSql = <<<SQL
         INSERT INTO pimee_security_job_profile_access (job_profile_id, user_group_id, execute_job_profile, edit_job_profile)
-        SELECT ji.id as job_profile_id, oap.id as user_group_id, 1, 0
-        FROM akeneo_batch_job_instance ji
-            CROSS JOIN oro_access_group oap
-            LEFT JOIN pimee_security_job_profile_access pa On pa.user_group_id = oap.id AND pa.job_profile_id = ji.id
-        WHERE oap.name = 'All' AND pa.id IS NULL AND ji.code = :jobCode
-        SQL;
-        $this->addSql($updatePermissionSql, ['jobCode' => self::JOB_CODE]);
+            SELECT j.id AS job_profile_id, g.id AS user_group_id, 1, 1
+            FROM akeneo_batch_job_instance as j
+                JOIN oro_access_group AS g ON g.name = 'All'
+            WHERE j.code = :code
+        ;
+SQL;
+
+        $this->addSql($updatePermissionSql, ['code' => static::JOB_CODE]);
     }
 
     public function down(Schema $schema) : void
     {
         $this->throwIrreversibleMigrationException();
+    }
+
+    private function jobAlreadyExists(): bool
+    {
+        $result = $this->connection->executeQuery(
+            'SELECT id FROM akeneo_batch_job_instance WHERE code = :code',
+            ['code' => self::JOB_CODE]
+        )->fetchOne();
+
+        return false !== $result;
+    }
+
+    /**
+     * Function that does a non altering operation on the DB using SQL to hide the doctrine warning stating that no
+     * sql query has been made to the db during the migration process.
+     */
+    private function disableMigrationWarning(): void
+    {
+        $this->addSql('SELECT 1');
     }
 }
