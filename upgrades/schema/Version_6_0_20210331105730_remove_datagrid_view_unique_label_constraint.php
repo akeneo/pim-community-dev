5c3d35487436fc5bbf4c8f84881840035a2e5459
jean-marie leroux <jmleroux.pro@gmail.com>
2021-11-08 10:34:11 +0100

    Skip Version_6_0_20210331105730 if unique constraint is up to date (#15660)

--- upgrades/schema/Version_6_0_20210331105730_remove_datagrid_view_unique_label_constraint.php
+++ upgrades/schema/Version_6_0_20210331105730_remove_datagrid_view_unique_label_constraint.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pim\Upgrade\Schema;
 
 use Doctrine\DBAL\Connection as DbalConnection;
-use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -29,16 +28,14 @@ final class Version_6_0_20210331105730_remove_datagrid_view_unique_label_constra
     public function up(Schema $schema): void
     {
         $selectUniqueConstraint = <<< SQL
-SELECT DISTINCT CONSTRAINT_NAME
-FROM information_schema.TABLE_CONSTRAINTS
-WHERE table_name = 'pim_datagrid_view' AND constraint_type = 'UNIQUE' AND TABLE_SCHEMA = 'akeneo_pim';
-SQL;
+            SELECT DISTINCT CONSTRAINT_NAME
+            FROM information_schema.TABLE_CONSTRAINTS
+            WHERE table_name = 'pim_datagrid_view' AND constraint_type = 'UNIQUE' AND TABLE_SCHEMA = 'akeneo_pim';
+        SQL;
         $uniqueConstraintKeyName = $this->dbalConnection()->executeQuery($selectUniqueConstraint)->fetchOne();
 
-        $this->addSql(<<<SQL
-ALTER TABLE pim_datagrid_view
-DROP index $uniqueConstraintKeyName
-SQL);
+        $this->skipIf(!$uniqueConstraintKeyName, 'pim_datagrid_view unique constraint is already up to date');
+        $this->addSql("ALTER TABLE pim_datagrid_view DROP index $uniqueConstraintKeyName");
     }
 
     public function down(Schema $schema): void
