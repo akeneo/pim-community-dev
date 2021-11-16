3f954dcfba08bc44dfb81af2cb2037738bbd2a2e
willy mesnage <willy.mesnage@gmail.com>
2021-11-16 12:02:57 +0100

    CXP-751: disable migration warning

--- upgrades/schema/Version_6_0_20211115143942_add_user_group_type_column.php
+++ upgrades/schema/Version_6_0_20211115143942_add_user_group_type_column.php
@@ -19,11 +19,23 @@ final class Version_6_0_20211115143942_add_user_group_type_column extends Abstra
         $table = $schema->getTable('oro_access_group');
         if (!$table->hasColumn('type')) {
             $this->addSql('ALTER TABLE oro_access_group ADD type VARCHAR(30) NOT NULL DEFAULT "default"');
+
+            return;
         }
+        $this->disableMigrationWarning();
     }
 
     public function down(Schema $schema) : void
     {
         $this->throwIrreversibleMigrationException();
+    }
+
+    /**
+     * Function that does a non altering operation on the DB using SQL to hide the doctrine warning stating that no
+     * sql query has been made to the db during the migration process.
+     */
+    private function disableMigrationWarning(): void
+    {
+        $this->addSql('SELECT 1');
     }
 }
