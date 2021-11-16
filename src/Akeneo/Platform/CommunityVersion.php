ecb14d3faf8d414f7d52298c6931cecedcc75383
Michel Tag <micheltag@akeneo.com>
2021-11-16 10:51:33 +0100

    Preparing version 4.0.126 (#15709)
    Preparing version 4.0.126
    
    
    Co-authored-by: mmetayer <mathias.metayer@akeneo.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '4.0.125';
+    const VERSION = '4.0.126';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Hare Tonic';
