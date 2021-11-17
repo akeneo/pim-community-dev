43cfcc5f5326958bf8df76f59dbf61e5f0fa1b91
Guillaume Fer <guillaume.fer@akeneo.com>
2021-11-17 10:58:45 +0100

    improve spec items's data

--- src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Infrastructure/Marketplace/GetAllAppsQuerySpec.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Infrastructure/Marketplace/GetAllAppsQuerySpec.php
@@ -28,12 +28,12 @@ class GetAllAppsQuerySpec extends ObjectBehavior
         $this->items = [
             [
                 'id' => '90741597-54c5-48a1-98da-a68e7ee0a715',
-                'name' => 'Akeneo Shopware 6 Connector by EIKONA Media',
-                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/extension_logo_large/public/extension-logos/akeneo-to-shopware6-eimed_0.jpg?itok=InguS-1N',
+                'name' => 'Akeneo Shopware 6 App by EIKONA Media',
+                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/app_logo_large/public/app-logos/akeneo-to-shopware6-eimed_0.jpg?itok=InguS-1N',
                 'author' => 'EIKONA Media GmbH',
                 'partner' => 'Akeneo Preferred Partner',
-                'description' => 'With the new "Akeneo-Shopware-6-Connector" from EIKONA Media, you can smoothly export all your product data from Akeneo to Shopware. The connector uses the standard interfaces provided for data exchange. Benefit from up-to-date product data in all your e-commerce channels and be faster on the market.',
-                'url' => 'https://marketplace.akeneo.com/extension/akeneo-shopware-6-connector-eikona-media',
+                'description' => 'With the new "Akeneo-Shopware-6-App" from EIKONA Media, you can smoothly export all your product data from Akeneo to Shopware. The app uses the standard interfaces provided for data exchange. Benefit from up-to-date product data in all your e-commerce channels and be faster on the market.',
+                'url' => 'https://marketplace.akeneo.com/app/akeneo-shopware-6-app-eikona-media',
                 'categories' => [
                     'E-commerce',
                 ],
@@ -43,12 +43,12 @@ class GetAllAppsQuerySpec extends ObjectBehavior
             ],
             [
                 'id' => 'b18561ff-378e-41a5-babb-ca0ec0af569a',
-                'name' => 'Akeneo PIM Connector for Shopify',
-                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/extension_logo_large/public/extension-logos/shopify-connector-logo-1200x.png?itok=mASOVlwC',
+                'name' => 'Akeneo PIM App for Shopify',
+                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/app_logo_large/public/app-logos/shopify-app-logo-1200x.png?itok=mASOVlwC',
                 'author' => 'StrikeTru',
                 'partner' => 'Akeneo Partner',
                 'description' => 'SaaS software from StrikeTru that seamlessly connects Akeneo PIM to the Shopify platform. It allows Shopify users to quickly setup a link to Akeneo PIM and sync all product catalog data to Shopify within minutes. It eliminates a lot of manual and repetitive work involved in updating the product catalog of a Shopify store. You can send and receive products, variations, modifiers, categories, standard and custom attributes, images and more from Akeneo PIM into your Shopify store. Compatible with all Akeneo PIM editions – Community, Growth, Enterprise (On-Premise, Cloud Flexibility, and Cloud Serenity) and StrikeTru\'s smallPIM.',
-                'url' => 'https://marketplace.akeneo.com/extension/akeneo-pim-connector-shopify',
+                'url' => 'https://marketplace.akeneo.com/app/akeneo-pim-app-shopify',
                 'categories' => [
                     'E-commerce',
                 ],
@@ -58,12 +58,12 @@ class GetAllAppsQuerySpec extends ObjectBehavior
             ],
             [
                 'id' => 'b18561ff-378e-41a5-babb-ca0ec0af569b',
-                'name' => 'Akeneo PIM Connector for Shopify',
-                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/extension_logo_large/public/extension-logos/shopify-connector-logo-1200x.png?itok=mASOVlwC',
+                'name' => 'Akeneo PIM App for Shopify',
+                'logo' => 'https://marketplace.akeneo.com/sites/default/files/styles/app_logo_large/public/app-logos/shopify-app-logo-1200x.png?itok=mASOVlwC',
                 'author' => 'StrikeTru',
                 'partner' => 'Akeneo Partner',
                 'description' => 'SaaS software from StrikeTru that seamlessly connects Akeneo PIM to the Shopify platform. It allows Shopify users to quickly setup a link to Akeneo PIM and sync all product catalog data to Shopify within minutes. It eliminates a lot of manual and repetitive work involved in updating the product catalog of a Shopify store. You can send and receive products, variations, modifiers, categories, standard and custom attributes, images and more from Akeneo PIM into your Shopify store. Compatible with all Akeneo PIM editions – Community, Growth, Enterprise (On-Premise, Cloud Flexibility, and Cloud Serenity) and StrikeTru\'s smallPIM.',
-                'url' => 'https://marketplace.akeneo.com/extension/akeneo-pim-connector-shopify',
+                'url' => 'https://marketplace.akeneo.com/app/akeneo-pim-app-shopify',
                 'categories' => [
                     'E-commerce',
                 ],
@@ -102,7 +102,7 @@ class GetAllAppsQuerySpec extends ObjectBehavior
         $this->shouldHaveType(GetAllAppsQuery::class);
     }
 
-    public function it_execute_and_returns_extension_result()
+    public function it_execute_and_returns_app_result()
     {
         $this->execute()->shouldBeLike(GetAllAppsResult::create(3, array_map(function ($item) {
             return App::fromWebMarketplaceValues($item);
