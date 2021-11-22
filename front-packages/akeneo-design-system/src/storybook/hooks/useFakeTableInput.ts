300a542fd2a5150c9491c6776ce75ae666c241e6
Celine Morin <celine.morin@akeneo.com>
2021-11-22 17:37:44 +0100

    CPM-430 Add annotation on dropdown

--- front-packages/akeneo-design-system/src/storybook/hooks/useFakeTableInput.ts
+++ front-packages/akeneo-design-system/src/storybook/hooks/useFakeTableInput.ts
@@ -5,10 +5,11 @@ type Row = {
   part: string;
   quantity: number;
   is_allergenic: boolean | null;
+  origin: 'french' | 'english' | 'german' | null;
 };
 
-type RowCode = 'nutritionScore' | 'part' | 'quantity' | 'is_allergenic';
-type RowValue = '1' | '2' | '3' | string | number | boolean | null;
+type RowCode = 'nutritionScore' | 'part' | 'quantity' | 'is_allergenic' | 'origin';
+type RowValue = '1' | '2' | '3' | string | number | boolean | null | 'french' | 'english' | 'german';
 
 const useFakeTableInput = (linesCount: number) => {
   const [state, setState] = useState<Row[]>(
@@ -18,6 +19,7 @@ const useFakeTableInput = (linesCount: number) => {
         part: `${lineIndex * 100}g`,
         quantity: lineIndex * 10,
         is_allergenic: [true, false, null][lineIndex % 3],
+        origin: ['french', 'english', 'german', null][lineIndex % 4] as 'french' | 'english' | 'german' | null,
       };
     })
   );
@@ -33,7 +35,11 @@ const useFakeTableInput = (linesCount: number) => {
     setState([...state]);
   };
 
-  return {getValue, setValue, searchValue, setSearchValue};
+  const match: (text: string) => boolean = text => {
+    return text.toLowerCase().includes(searchValue.toLowerCase());
+  };
+
+  return {getValue, setValue, searchValue, setSearchValue, match};
 };
 
 export {useFakeTableInput};
