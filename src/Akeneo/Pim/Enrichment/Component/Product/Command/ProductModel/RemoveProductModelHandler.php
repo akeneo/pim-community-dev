d99e4b1bee7d67506b0c75a57b4ff6ee386f27c0
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-11-26 11:52:53 +0100

    PIM-10098: fix headers

--- src/Akeneo/Pim/Enrichment/Component/Product/Command/ProductModel/RemoveProductModelHandler.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Command/ProductModel/RemoveProductModelHandler.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Akeneo PIM Enterprise Edition.
- *
- * (c) 2021 Akeneo SAS (https://www.akeneo.com)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Akeneo\Pim\Enrichment\Component\Product\Command\ProductModel;
 
 use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductModelRepositoryInterface;
@@ -18,6 +9,10 @@ use Akeneo\Tool\Bundle\ElasticsearchBundle\Client;
 use Akeneo\Tool\Component\StorageUtils\Remover\RemoverInterface;
 use Webmozart\Assert\Assert;
 
+/**
+ * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class RemoveProductModelHandler
 {
     private ProductModelRepositoryInterface $productModelRepository;
