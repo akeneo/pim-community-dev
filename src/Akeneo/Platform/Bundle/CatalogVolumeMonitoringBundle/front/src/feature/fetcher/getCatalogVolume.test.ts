9c37151ae866d949c2b78a76b2ff5a85c93d6cee
Antoine Trouve <antoine.trouve@akeneo.com>
2021-11-26 11:54:07 +0100

    PLG-646: Add test on fetcher

--- src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/front/src/feature/fetcher/getCatalogVolume.test.ts
+++ src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/front/src/feature/fetcher/getCatalogVolume.test.ts
@@ -5,40 +5,51 @@ import {mockedDependencies} from "@akeneo-pim-community/shared";
 
 jest.mock("./catalogVolumeWrapper")
 
-beforeEach(() => {
-    fetchMock.resetMocks();
-});
-
-test('get Catalog volume with error', async () => {
-
-    // Given
-    const volumesResponse = {
-        count_products: {
-            value: 1389,
-            has_warning: false,
-            type: 'count',
-        },
-        average_max_attributes_per_family: {
-            value: {
-                average: 4,
-                max: 43,
-            },
-            has_warning: false,
-            type: 'average_max',
-        }
-    };
+const mockRouter = (relativeUrl: string) => {
     // @ts-ignore
     const mockFn = jest.mock();
     mockFn
         .spyOn(mockedDependencies.router, 'generate')
-        .mockReturnValue('http://localhost:8080/catalog-volume-monitoring/volumes');
+        .mockReturnValue(relativeUrl);
+}
 
-    fetchMock.mockResponseOnce(JSON.stringify(volumesResponse), {status: 404});
+const volumesResponse = {
+    count_products: {
+        value: 1389,
+        has_warning: false,
+        type: 'count',
+    },
+    average_max_attributes_per_family: {
+        value: {
+            average: 4,
+            max: 43,
+        },
+        has_warning: false,
+        type: 'average_max',
+    }
+};
+
+beforeEach(() => {
+    fetchMock.resetMocks();
+    mockRouter('http://localhost:8080/catalog-volume-monitoring/volumes')
+});
+
+test('get Catalog volume with success', async () => {
+    // Given
+    fetchMock.mockResponseOnce(JSON.stringify(volumesResponse), {status: 200});
 
     // When
     await getCatalogVolume(mockedDependencies.router)
 
     // Then
-    expect(transformVolumesToAxis).not.toBeCalled()
+    expect(transformVolumesToAxis).toHaveBeenCalledTimes(1);
+});
+
+test('get Catalog volume with error', async () => {
+    // Given
+    fetchMock.mockResponseOnce(JSON.stringify(volumesResponse), {status: 404});
+
+    // Then
+    expect(await getCatalogVolume(mockedDependencies.router)).toThrowError()
 });
 
