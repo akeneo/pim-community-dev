522e3c287491796f00e5eaf54b642f54fe8bbfc4
Adrien Migaire <adrien.migaire@akeneo.com>
2021-11-29 16:45:45 +0100

    CPM-432 fix namespace after review

--- src/Akeneo/ReferenceEntity/back/Infrastructure/Application/ReferenceEntityFeature.php
+++ src/Akeneo/ReferenceEntity/back/Infrastructure/Application/ReferenceEntityFeature.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Akeneo\ReferenceEntity\back\Infrastructure\Application;
+namespace Akeneo\ReferenceEntity\Infrastructure\Application;
 
 use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
 
