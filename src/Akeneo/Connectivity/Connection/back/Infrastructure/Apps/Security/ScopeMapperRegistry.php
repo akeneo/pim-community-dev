9a9e3f978ee3d06f20374b9784a0b528ad48e52a
Tseho <quentin.favrie@gmail.com>
2021-11-29 13:40:57 +0100

    CXP-942: linter

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Security/ScopeMapperRegistry.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Security/ScopeMapperRegistry.php
@@ -114,7 +114,7 @@ final class ScopeMapperRegistry
     private function filterByKeepingHighestLevels(array $scopeList, array $lowerLevelScopes): array
     {
         return \array_values(
-            \array_filter($scopeList, fn(string $scope) => !\in_array($scope, $lowerLevelScopes))
+            \array_filter($scopeList, fn (string $scope) => !\in_array($scope, $lowerLevelScopes))
         );
     }
 
