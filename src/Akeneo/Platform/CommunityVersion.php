fdb5c0cb0722876c0bab5446ddc092e6526b892f
Pierre Jolly <pie.jolly@gmail.com>
2021-11-30 14:15:00 +0100

    Preparing version 5.0.60

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.59';
+    const VERSION = '5.0.60';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
