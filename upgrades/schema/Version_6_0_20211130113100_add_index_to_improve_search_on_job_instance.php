0800a2a7cdfffeb3e89a039456f77d80da1fa78f
Steven VAIDIE <steven.vaidie@gmail.com>
2021-11-30 14:25:17 +0100

    RAC-1083: Fix migration test

--- upgrades/schema/Version_6_0_20211130113100_add_index_to_improve_search_on_job_instance.php
+++ upgrades/schema/Version_6_0_20211130113100_add_index_to_improve_search_on_job_instance.php
@@ -9,7 +9,7 @@ final class Version_6_0_20211130113100_add_index_to_improve_search_on_job_instan
 {
     public function up(Schema $schema) : void
     {
-        $this->skipIf($this->indexExist('code_idx'), 'Index code_idx already exist in akeneo_batch_job_instance');
+        $this->skipIf($this->indexExist(), 'Index code_idx already exist in akeneo_batch_job_instance');
 
         $this->addSql('CREATE INDEX code_idx ON akeneo_batch_job_instance (code)');
     }
@@ -19,13 +19,13 @@ final class Version_6_0_20211130113100_add_index_to_improve_search_on_job_instan
         $this->throwIrreversibleMigrationException();
     }
 
-    private function indexesExist(string $indexName): bool
+    private function indexExist(): bool
     {
         $indexes = $this->connection->executeQuery('SHOW INDEX FROM akeneo_batch_job_instance')->fetchAllAssociative();
         $indexIndexedByName = array_column($indexes, null, 'Key_name');
 
         return isset(
-            $indexIndexedByName[$indexName],
+            $indexIndexedByName['code_idx'],
         );
     }
 }
