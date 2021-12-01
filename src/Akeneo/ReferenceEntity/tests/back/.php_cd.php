fea57dbf3c681c524cdcac7fc38c6e9851d27d6d
Tseho <quentin.favrie@gmail.com>
2021-12-01 09:17:38 +0100

    CXP-914: fix phpcd

--- src/Akeneo/ReferenceEntity/tests/back/.php_cd.php
+++ src/Akeneo/ReferenceEntity/tests/back/.php_cd.php
@@ -47,6 +47,7 @@ $rules = [
         'Akeneo\Pim\Enrichment\ReferenceEntity\Component',
         'Akeneo\Platform\Component\CatalogVolumeMonitoring\Volume\ReadModel\AverageMaxVolumes',
         'Akeneo\Platform\Component\CatalogVolumeMonitoring\Volume\Query\AverageMaxQuery',
+        'Akeneo\Connectivity\Connection\Infrastructure\Apps\Security\ScopeMapperInterface',
     ])->in('Akeneo\ReferenceEntity\Infrastructure'),
 ];
 
