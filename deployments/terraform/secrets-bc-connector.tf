03575b78fa96af48d3648483ad9646616c4ab912
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2021-12-02 12:12:43 +0100

    BCAPP-XXX: fix use of papo secret for onboarder dev project (#12466)

--- deployments/terraform/secrets-bc-connector.tf
+++ deployments/terraform/secrets-bc-connector.tf
@@ -1,12 +1,12 @@
 data "google_secret_manager_secret_version" "bigcommerce_connector_akeneo_connect_bot_password" {
   count = 1
-  secret  = var.google_project_id != "akecld-saas-dev" ? "connector-bigcommerce-prod-portal-client-password" : "connector-bigcommerce-dev-portal-client-password"
+  secret  = !contains([ "akecld-onboarder-dev", "akecld-saas-dev"], var.google_project_id) ? "connector-bigcommerce-prod-portal-client-password" : "connector-bigcommerce-dev-portal-client-password"
   project = "akeneo-cloud"
 }
 
 data "google_secret_manager_secret_version" "bigcommerce_connector_akeneo_connect_bot_client_secret" {
   count = 1
-  secret  = var.google_project_id != "akecld-saas-dev" ? "connector-bigcommerce-prod-portal-client-secret" : "connector-bigcommerce-dev-portal-client-secret"
+  secret  = !contains([ "akecld-onboarder-dev", "akecld-saas-dev"], var.google_project_id) ? "connector-bigcommerce-prod-portal-client-secret" : "connector-bigcommerce-dev-portal-client-secret"
   project = "akeneo-cloud"
 }
 
