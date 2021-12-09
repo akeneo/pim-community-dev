ef7ff8801315c3f9490be223d256fc9466e35143
Antoine Trouve <antoine.trouve@akeneo.com>
2021-12-09 10:00:46 +0100

    PLG-681: remove old form extension

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/controller/catalog-volume.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/controller/catalog-volume.tsx
@@ -3,13 +3,9 @@ import {DependenciesProvider} from '@akeneo-pim-community/legacy-bridge';
 import {ThemeProvider} from 'styled-components';
 import {pimTheme} from 'akeneo-design-system';
 import React from 'react';
-import * as $ from 'jquery';
-import BaseView = require('pimui/js/view/base');
 import {CatalogVolumeMonitoringApp, getCatalogVolume} from '@akeneo-pim-community/catalog-volume-monitoring';
 
 const mediator = require('oro/mediator');
-const FormBuilder = require('pim/form-builder');
-const Routing = require('routing');
 
 class CatalogVolumeController extends ReactController {
 
@@ -32,22 +28,6 @@ class CatalogVolumeController extends ReactController {
       mediator.trigger('pim_menu:highlight:item', {extension: 'pim-menu-system-catalog-volume'});
 
     return super.renderRoute();
-  }
-
-  renderForm(): JQueryPromise<BaseView> {
-    return $.when(
-      FormBuilder.build('pim-catalog-volume-index'),
-      $.get(Routing.generate('pim_volume_monitoring_get_volumes'))
-    ).then((form: BaseView, data = []) => {
-      this.on('pim:controller:can-leave', (event: {canLeave: true}) => {
-        form.trigger('pim_enrich:form:can-leave', event);
-      });
-
-      form.setData(data[0]);
-      form.setElement(this.$el).render();
-
-      return form;
-    });
   }
 }
 
