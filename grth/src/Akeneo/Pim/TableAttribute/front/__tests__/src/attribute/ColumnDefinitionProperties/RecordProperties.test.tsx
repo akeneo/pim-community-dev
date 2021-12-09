4613428921a7b5390bfa5ffc7e77efcb4f9bffcc
pierallard <pierallard@gmail.com>
2021-12-09 14:40:38 +0100

    CPM-436: Fix after rebase

--- grth/src/Akeneo/Pim/TableAttribute/front/__tests__/src/attribute/ColumnDefinitionProperties/RecordProperties.test.tsx
+++ grth/src/Akeneo/Pim/TableAttribute/front/__tests__/src/attribute/ColumnDefinitionProperties/RecordProperties.test.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 import {renderWithProviders} from '@akeneo-pim-community/legacy-bridge/tests/front/unit/utils';
 import {screen} from '@testing-library/react';
 import {getComplexTableAttribute} from '../../../factories';
-import {RecordColumnDefinition} from '../../../../src/models';
 import RecordProperties from '../../../../src/attribute/ColumDefinitionProperties/RecordProperties';
 
 jest.mock('../../../../src/fetchers/ReferenceEntityFetcher');
 
 describe('RecordProperties', () => {
   it('should render the component', async () => {
-    const recordColumn: RecordColumnDefinition = {
-      code: 'city_column',
-      reference_entity_identifier: 'city',
-      validations: {},
-      data_type: 'record',
-      labels: {},
-      is_required_for_completeness: false,
-    };
-    const tableAttribute = getComplexTableAttribute();
-    tableAttribute.table_configuration.push(recordColumn);
-
+    const tableAttribute = getComplexTableAttribute('record');
     renderWithProviders(
-      <RecordProperties attribute={tableAttribute} selectedColumn={recordColumn} handleChange={jest.fn()} />
+      <RecordProperties
+        attribute={tableAttribute}
+        selectedColumn={tableAttribute.table_configuration[0]}
+        handleChange={jest.fn()}
+      />
     );
 
     expect(screen.getByText('pim_table_attribute.form.attribute.reference_entity')).toBeInTheDocument();
