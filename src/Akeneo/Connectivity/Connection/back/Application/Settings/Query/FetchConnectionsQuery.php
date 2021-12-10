e769227e447f9d5bb585c73ed8bd4d79f3d9d39c
Guillaume Fer <guillaume.fer@akeneo.com>
2021-12-10 17:06:15 +0100

    CXP-755: add a final class

--- src/Akeneo/Connectivity/Connection/back/Application/Settings/Query/FetchConnectionsQuery.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Settings/Query/FetchConnectionsQuery.php
@@ -8,7 +8,7 @@ namespace Akeneo\Connectivity\Connection\Application\Settings\Query;
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class FetchConnectionsQuery
+final class FetchConnectionsQuery
 {
     /** @var array<string, mixed> */
     private array $types;
