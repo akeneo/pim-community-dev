42c38c3a579338ff510a5feb685e1c86d49760a4
Paul Chasle <pchasle@users.noreply.github.com>
2021-12-15 17:30:32 +0100

    CXP-989: remove fk between consent & app

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Install/Query/CreateUserConsentTable.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Install/Query/CreateUserConsentTable.php
@@ -11,15 +11,14 @@ namespace Akeneo\Connectivity\Connection\Infrastructure\Install\Query;
 class CreateUserConsentTable
 {
     const QUERY = <<<'SQL'
-    CREATE TABLE IF NOT EXISTS akeneo_connectivity_user_consent(
-        user_id INT NOT NULL,
-        app_id VARCHAR(36) NOT NULL,
-        scopes JSON NOT NULL,
-        uuid CHAR(36) CHARACTER SET ascii NOT NULL,
-        consent_date DATETIME NOT NULL,
-        PRIMARY KEY (user_id, app_id),
-        CONSTRAINT FK_CONNECTIVITY_CONNECTION_user_consent_user_id FOREIGN KEY (user_id) REFERENCES oro_user (id) ON DELETE CASCADE,
-        CONSTRAINT FK_CONNECTIVITY_CONNECTION_user_consent_app_id FOREIGN KEY (app_id) REFERENCES akeneo_connectivity_connected_app (id)
-    ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
-    SQL;
+        CREATE TABLE IF NOT EXISTS akeneo_connectivity_user_consent(
+            user_id INT NOT NULL,
+            app_id VARCHAR(36) NOT NULL,
+            scopes JSON NOT NULL,
+            uuid CHAR(36) CHARACTER SET ascii NOT NULL,
+            consent_date DATETIME NOT NULL,
+            PRIMARY KEY (user_id, app_id),
+            CONSTRAINT FK_CONNECTIVITY_CONNECTION_user_consent_user_id FOREIGN KEY (user_id) REFERENCES oro_user (id) ON DELETE CASCADE,
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC
+        SQL;
 }
