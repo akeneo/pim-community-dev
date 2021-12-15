2f46d5a374fee29817c4748b9e3789429a026fe3
Tseho <quentin.favrie@gmail.com>
2021-12-15 16:56:20 +0100

    CXP-758: reduce the number of E2E tests

--- src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Apps/InternalApi/UpdateConnectedAppMonitoringSettingsActionEndToEnd.php
+++ src/Akeneo/Connectivity/Connection/back/tests/EndToEnd/Apps/InternalApi/UpdateConnectedAppMonitoringSettingsActionEndToEnd.php
@@ -10,9 +10,7 @@ use Akeneo\Connectivity\Connection\Domain\Settings\Model\ValueObject\FlowType;
 use Akeneo\Connectivity\Connection\Tests\CatalogBuilder\ConnectionLoader;
 use Akeneo\Connectivity\Connection\Tests\Integration\Mock\FakeFeatureFlag;
 use Akeneo\Test\Integration\Configuration;
-use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\Assert;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -35,81 +33,6 @@ class UpdateConnectedAppMonitoringSettingsActionEndToEnd extends WebTestCase
     protected function getConfiguration(): Configuration
     {
         return $this->catalog->useMinimalCatalog();
-    }
-
-    public function test_it_throws_not_found_exception_with_feature_flag_disabled(): void
-    {
-        $this->featureFlagMarketplaceActivate->disable();
-        $this->authenticateAsAdmin();
-
-        $this->connectionLoader->createConnection('connectionCodeA', 'Connector A', FlowType::DATA_SOURCE, true);
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings'
-        );
-        $response = $this->client->getResponse();
-
-        Assert::assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
-    }
-
-    public function test_it_redirects_on_missing_xmlhttprequest_header(): void
-    {
-        $this->featureFlagMarketplaceActivate->enable();
-        $this->authenticateAsAdmin();
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings'
-        );
-
-        $response = $this->client->getResponse();
-
-        Assert::assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
-        assert($response instanceof RedirectResponse);
-        Assert::assertEquals('/', $response->getTargetUrl());
-    }
-
-    public function test_it_throws_access_denied_exception_with_missing_acl(): void
-    {
-        $this->featureFlagMarketplaceActivate->enable();
-        $this->authenticateAsAdmin();
-        $this->removeAclFromRole('ROLE_ADMINISTRATOR', 'akeneo_connectivity_connection_manage_apps');
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings',
-            [],
-            [],
-            [
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-            ]
-        );
-        $response = $this->client->getResponse();
-
-        Assert::assertEquals(Response::HTTP_FORBIDDEN, $response->getStatusCode());
-    }
-
-    public function test_it_throws_not_found_exception_with_wrong_connection_code(): void
-    {
-        $this->featureFlagMarketplaceActivate->enable();
-        $this->authenticateAsAdmin();
-        $this->addAclToRole('ROLE_ADMINISTRATOR', 'akeneo_connectivity_connection_manage_apps');
-
-        $this->connectionLoader->createConnection('connectionCodeA', 'Connector A', FlowType::DATA_SOURCE, true);
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings',
-            [],
-            [],
-            [
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-            ]
-        );
-        $response = $this->client->getResponse();
-
-        Assert::assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
     }
 
     public function test_it_updates_connected_app_monitoring_settings(): void
@@ -149,95 +72,6 @@ class UpdateConnectedAppMonitoringSettingsActionEndToEnd extends WebTestCase
         Assert::assertNull($result);
         Assert::assertEquals($data, $this->getConnectionMonitoringSettings('connectionCodeA'));
     }
-
-    public function test_it_throws_unprocessed_entity_on_update_with_unknown_flow_type_value(): void
-    {
-        $this->featureFlagMarketplaceActivate->enable();
-        $this->authenticateAsAdmin();
-        $this->addAclToRole('ROLE_ADMINISTRATOR', 'akeneo_connectivity_connection_manage_apps');
-
-        $this->connectionLoader->createConnection(
-            'connectionCodeA',
-            'Connector A',
-            FlowType::DATA_DESTINATION,
-            false,
-            ConnectionType::APP_TYPE
-        );
-
-        $data = [
-            'flowType' => 'some_unknown_flow_type',
-            'auditable' => false,
-        ];
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings',
-            [],
-            [],
-            [
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-                'CONTENT_TYPE' => 'application/json'
-            ],
-            \json_encode($data)
-        );
-        $response = $this->client->getResponse();
-        $result = \json_decode($response->getContent(), true);
-
-        $expectedResult = [
-            "message" => "akeneo_connectivity.connection.constraint_violation_list_exception",
-            "errors" => [
-                [
-                    "name" => "flowType",
-                    "reason" => "akeneo_connectivity.connection.connection.constraint.flow_type.invalid"
-                ]
-            ]
-        ];
-
-        Assert::assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
-        Assert::assertEquals($expectedResult, $result);
-    }
-
-    public function test_it_throws_unprocessed_entity_on_update_with_wrong_type(): void
-    {
-        $this->featureFlagMarketplaceActivate->enable();
-        $this->authenticateAsAdmin();
-        $this->addAclToRole('ROLE_ADMINISTRATOR', 'akeneo_connectivity_connection_manage_apps');
-
-        $this->connectionLoader->createConnection(
-            'connectionCodeA',
-            'Connector A',
-            FlowType::DATA_DESTINATION,
-            false,
-            ConnectionType::APP_TYPE
-        );
-
-        $data = [
-            'flowType' => FlowType::DATA_SOURCE,
-            'auditable' => 'should be a bool',
-        ];
-
-        $this->client->request(
-            'POST',
-            '/rest/apps/connected-apps/connectionCodeA/monitoring-settings',
-            [],
-            [],
-            [
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-                'CONTENT_TYPE' => 'application/json'
-            ],
-            \json_encode($data)
-        );
-        $response = $this->client->getResponse();
-        $result = \json_decode($response->getContent(), true);
-
-        $expectedResult = [
-            "error" => "Wrong type for parameters",
-        ];
-
-        Assert::assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
-        Assert::assertEquals($expectedResult, $result);
-    }
-
 
     private function getConnectionMonitoringSettings(string $connectionCode): array
     {
