b086e58bb3967d9c46d64f70e6f0f9218e29760a
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-12-16 16:51:41 +0100

    RAC-1055: Remove BEM usage in Asset Library placeholder

--- src/Akeneo/AssetManager/front/application/component/library/components/AssetLibraryPlaceholder.tsx
+++ src/Akeneo/AssetManager/front/application/component/library/components/AssetLibraryPlaceholder.tsx
@@ -1,39 +1,30 @@
-import {AssetFamily} from 'akeneoassetmanager/domain/model/asset-family/asset-family';
 import React from 'react';
 import styled from 'styled-components';
+import {Card, CardGrid, SkeletonPlaceholder} from 'akeneo-design-system';
+import {AssetFamily} from 'akeneoassetmanager/domain/model/asset-family/asset-family';
 
-const AssetCardPlaceholderGrid = styled.div`
-  margin-top: 20px;
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
-`;
-
-const AssetCardPlaceholder = styled.div`
-  width: 100%;
-  padding-top: 100%; /* 1:1 Aspect Ratio */
-  position: relative;
-  margin-bottom: 6px;
-  min-height: 140px;
-`;
-
-const SearchBarPlaceholder = styled.div`
+const SearchBarPlaceholder = styled(SkeletonPlaceholder)`
   height: 45px;
   width: 100%;
+  margin-bottom: 20px;
 `;
 
-const AssetLibraryPlaceholder = ({assetFamily}: {assetFamily: AssetFamily | null}) => {
-  return (
-    <>
-      <div className={`AknLoadingPlaceHolderContainer`}>
-        <SearchBarPlaceholder />
-      </div>
-      <AssetCardPlaceholderGrid className={`AknLoadingPlaceHolderContainer`}>
-        {undefined !== assetFamily?.assetCount &&
-          [...Array(Math.min(assetFamily.assetCount, 50))].map((_e, i) => <AssetCardPlaceholder key={i} />)}
-      </AssetCardPlaceholderGrid>
-    </>
-  );
+type AssetLibraryPlaceholderProps = {
+  assetFamily: AssetFamily | null;
 };
+
+const AssetLibraryPlaceholder = ({assetFamily}: AssetLibraryPlaceholderProps) => (
+  <>
+    <SearchBarPlaceholder>{assetFamily?.code}</SearchBarPlaceholder>
+    <CardGrid>
+      {undefined !== assetFamily?.assetCount &&
+        [...Array(Math.min(assetFamily.assetCount, 50))].map((_e, i) => (
+          <Card key={i} src={null}>
+            {i}
+          </Card>
+        ))}
+    </CardGrid>
+  </>
+);
 
 export {AssetLibraryPlaceholder};
