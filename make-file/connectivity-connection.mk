dbf5d9ee3b33ebf0ba69d77d8ca6cb53e1d1b496
Tseho <quentin.favrie@gmail.com>
2021-12-17 14:12:32 +0100

    CXP-1033: Add php-cs-fixer to connectivity-connection-lint-back

--- make-file/connectivity-connection.mk
+++ make-file/connectivity-connection.mk
@@ -63,8 +63,12 @@ connectivity-connection-coupling-back:
 connectivity-connection-lint-back:
 	$(DOCKER_COMPOSE) run -u www-data --rm php rm -rf var/cache/dev
 	APP_ENV=dev $(DOCKER_COMPOSE) run -e APP_DEBUG=1 -u www-data --rm php bin/console cache:warmup
+	$(PHP_RUN) vendor/bin/php-cs-fixer fix --diff --dry-run --config=src/Akeneo/Connectivity/Connection/back/tests/.php_cs.php
 	$(PHP_RUN) vendor/bin/phpstan analyse --level=8 src/Akeneo/Connectivity/Connection/back/Application src/Akeneo/Connectivity/Connection/back/Domain
 	$(PHP_RUN) vendor/bin/phpstan analyse --level=5 src/Akeneo/Connectivity/Connection/back/Infrastructure
+
+connectivity-connection-lint-back_fix:
+	$(PHP_RUN) vendor/bin/php-cs-fixer fix --config=src/Akeneo/Connectivity/Connection/back/tests/.php_cs.php
 
 connectivity-connection-unit-back:
 	$(PHP_RUN) vendor/bin/phpspec run src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/
