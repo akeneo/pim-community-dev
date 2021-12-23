88b09a60652ae4738e4bcf4602ada7f59f37480d
Pierre Maraître <pierre.maraitre@akeneo.com>
2021-12-23 09:39:33 +0100

    PIM-10212: Validate new locales more strictly

--- src/Akeneo/Channel/Component/Validator/Constraint/LocaleCodeValidator.php
+++ src/Akeneo/Channel/Component/Validator/Constraint/LocaleCodeValidator.php
@@ -9,7 +9,16 @@ final class LocaleCodeValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint): void
     {
-        if (! preg_match('#^[a-z0-9_]*[a-z]{2,3}_[a-z0-9_]{2,}[a-z0-9_]*$#i', $value)) {
+        // PIM-10212 For newly created locales, we do a proper check, for existing ones, we keep the old check to avoid BC breaks
+        $locale = $this->context->getRoot();
+        if (null !== $locale && null === $locale->getId()) {
+            if (! preg_match('#^[a-z0-9]{2,}_[a-z0-9_]{2,}$#i', $value)) {
+                $this->context->buildViolation($constraint->message)->addViolation();
+            }
+            return;
+        }
+
+        if (! preg_match('#[a-z]{2,3}_[a-z0-9_]{2,}#i', $value)) {
             $this->context->buildViolation($constraint->message)->addViolation();
         }
     }
--- tests/back/Channel/Specification/Component/Validator/Constraint/LocaleCodeValidatorSpec.php
+++ tests/back/Channel/Specification/Component/Validator/Constraint/LocaleCodeValidatorSpec.php
@@ -2,6 +2,7 @@
 
 namespace Specification\Akeneo\Channel\Component\Validator\Constraint;
 
+use Akeneo\Channel\Component\Model\Locale;
 use Akeneo\Channel\Component\Validator\Constraint\LocaleCode;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -15,10 +16,12 @@ class LocaleCodeValidatorSpec extends ObjectBehavior
         $this->initialize($context);
     }
 
-    function it_validates_a_valid_locale_code($context) {
+    function it_validates_a_valid_locale_code($context, Locale $contextLocale) {
         $constraint = new LocaleCode();
 
         $context->buildViolation(Argument::cetera())->shouldNotBeCalled();
+        $context->getRoot()->willReturn($contextLocale);
+        $contextLocale->getId()->willReturn(null);
 
         $this->validate('aa_AA', $constraint);
         $this->validate('aaa_AAA', $constraint);
@@ -29,21 +32,36 @@ class LocaleCodeValidatorSpec extends ObjectBehavior
         $this->validate('en_029', $constraint);
     }
 
-    function it_add_a_violation_with_an_invalid_locale_code(ConstraintViolationBuilderInterface $violation, $context) {
+    function it_add_a_violation_with_an_invalid_locale_code(ConstraintViolationBuilderInterface $violation, $context, Locale $contextLocale) {
         $constraint = new LocaleCode();
         $context->buildViolation($constraint->message)->willReturn($violation);
+        $context->getRoot()->willReturn($contextLocale);
+        $contextLocale->getId()->willReturn(null);
 
         $violation->addViolation()->shouldBeCalledTimes(10);
 
         $this->validate('aa', $constraint);
         $this->validate('aa_', $constraint);
-        $this->validate('aa_a', $constraint);
         $this->validate('a_aa', $constraint);
         $this->validate('aA', $constraint);
         $this->validate('AA_', $constraint);
         $this->validate('aA_A', $constraint);
         $this->validate('A_AA', $constraint);
         $this->validate(' aa_AA', $constraint);
-        $this->validate('aa_AA ', $constraint);
+        $this->validate('aa_AA-test ', $constraint);
+        $this->validate('aa AA ', $constraint);
+    }
+
+    function it_validates_an_old_pattern_if_locale_already_exists(ConstraintViolationBuilderInterface $violation, $context, Locale $contextLocale) {
+        $constraint = new LocaleCode();
+
+        $context->buildViolation(Argument::cetera())->shouldNotBeCalled();
+        $context->getRoot()->willReturn($contextLocale);
+        $contextLocale->getId()->willReturn('fr_FR');
+
+        $violation->addViolation()->shouldNotBeCalled();
+
+        $this->validate('aa_AA-test', $constraint);
+        $this->validate(' aa_AA', $constraint);
     }
 }
