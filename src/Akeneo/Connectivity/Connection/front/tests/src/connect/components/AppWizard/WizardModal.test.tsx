df3d4602aac37dc7f0df2fdfb8387062939e3504
Paul Chasle <pchasle@users.noreply.github.com>
2022-01-11 17:37:50 +0100

    CXP-1027: Add app wizard tests

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/AppWizard/WizardModal.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/AppWizard/WizardModal.test.tsx
@@ -1,8 +1,9 @@
 import {WizardModal} from '@src/connect/components/AppWizard/WizardModal';
 import '@testing-library/jest-dom/extend-expect';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import {pimTheme} from 'akeneo-design-system';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import {ThemeProvider} from 'styled-components';
 
 test('it renders a single step wizard modal', async () => {
@@ -36,13 +37,22 @@ test('it renders a single step wizard modal', async () => {
 
     expect(screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.cancel')).toBeInTheDocument();
     expect(screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.confirm')).toBeInTheDocument();
-
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.previous')
     ).not.toBeInTheDocument();
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.progress.authentication')
     ).not.toBeInTheDocument();
+
+    act(() => {
+        fireEvent.click(screen.getByText('akeneo_connectivity.connection.connect.apps.wizard.action.cancel'));
+    })
+    expect(handleClose).toBeCalled();
+
+    act(() => {
+        fireEvent.click(screen.getByText('akeneo_connectivity.connection.connect.apps.wizard.action.confirm'));
+    })
+    expect(handleConfirm).toBeCalled();
 });
 
 test('it renders a multi steps wizard modal', async () => {
@@ -80,16 +90,43 @@ test('it renders a multi steps wizard modal', async () => {
 
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.previous')
-    ).toBeInTheDocument();
+    ).not.toBeInTheDocument();
     expect(screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.next')).toBeInTheDocument();
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.progress.authentication')
     ).toBeInTheDocument();
-
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.cancel')
     ).not.toBeInTheDocument();
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.confirm')
     ).not.toBeInTheDocument();
+
+    act(() => {
+        fireEvent.click(screen.getByText('akeneo_connectivity.connection.connect.apps.wizard.action.next'));
+    })
+
+    expect(renderChildren).toBeCalledWith({
+        name: 'authorizations',
+        action: 'confirm',
+    });
+
+    expect(
+        screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.previous')
+    ).toBeInTheDocument();
+    expect(screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.next')).not.toBeInTheDocument();
+    expect(
+        screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.progress.authorizations')
+    ).toBeInTheDocument();
+    expect(
+        screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.cancel')
+    ).not.toBeInTheDocument();
+    expect(
+        screen.queryByText('akeneo_connectivity.connection.connect.apps.wizard.action.confirm')
+    ).toBeInTheDocument();
+
+    act(() => {
+        fireEvent.click(screen.getByText('akeneo_connectivity.connection.connect.apps.wizard.action.confirm'));
+    })
+    expect(handleConfirm).toBeCalled();
 });
