949b9e86afcb61688e5ccc701d8e3025cac05e64
Thomas Galvaing <thomas.galvaing@akeneo.com>
2022-01-11 16:38:25 +0100

    add ConfirmAuthorizationActionSpec

--- src/Akeneo/Connectivity/Connection/back/Application/Apps/Command/ConsentAppAuthenticationHandler.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Apps/Command/ConsentAppAuthenticationHandler.php
@@ -17,7 +17,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class ConsentAppAuthenticationHandler
+class ConsentAppAuthenticationHandler
 {
     private GetAppConfirmationQueryInterface $getAppConfirmationQuery;
     private AppAuthorizationSessionInterface $appAuthorizationSession;
