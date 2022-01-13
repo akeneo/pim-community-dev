ebb27af0d864f5f5a143ce5812d23db7b616c152
jmleroux <jmleroux.pro@gmail.com>
2022-01-13 16:33:18 +0100

    BH-1056: Upgrade phpseclib to v3.0

--- upgrades/schema/Version_6_0_20211214000000_add_openid_keys_into_pim_configuration.php
+++ upgrades/schema/Version_6_0_20211214000000_add_openid_keys_into_pim_configuration.php
@@ -3,21 +3,27 @@ declare(strict_types=1);
 
 namespace Pim\Upgrade\Schema;
 
-use Akeneo\Connectivity\Connection\Domain\Apps\DTO\AsymmetricKeys;
+use Akeneo\Connectivity\Connection\Infrastructure\Apps\AsymmetricKeysGenerator;
 use Akeneo\Connectivity\Connection\Infrastructure\Apps\Persistence\SaveAsymmetricKeysQuery;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\Migrations\AbstractMigration;
-use phpseclib\Crypt\RSA;
-use phpseclib\File\X509;
-
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Version_6_0_20211214000000_add_openid_keys_into_pim_configuration extends AbstractMigration
+class Version_6_0_20211214000000_add_openid_keys_into_pim_configuration extends AbstractMigration implements ContainerAwareInterface
 {
+    private ContainerInterface $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function up(Schema $schema): void
     {
         $query = <<<SQL
@@ -26,7 +32,8 @@ class Version_6_0_20211214000000_add_openid_keys_into_pim_configuration extends 
             ON DUPLICATE KEY UPDATE `values`= :asymmetricKeys
             SQL;
 
-        $asymmetricKeys = $this->generate();
+        $generator = $this->container->get(AsymmetricKeysGenerator::class);
+        $asymmetricKeys = $generator->generate();
 
         $now = new \DateTime('now', new \DateTimeZone('UTC'));
 
@@ -45,31 +52,5 @@ class Version_6_0_20211214000000_add_openid_keys_into_pim_configuration extends 
     public function down(Schema $schema): void
     {
         $this->throwIrreversibleMigrationException();
-    }
-
-    private function generate(): AsymmetricKeys
-    {
-        $privKey = new RSA();
-        $keys = $privKey->createKey();
-        $privateKey = $keys['privatekey'];
-        $publicKey = $keys['publickey'];
-
-        $pubKey = new RSA();
-        $pubKey->loadKey($publicKey);
-        $pubKey->setPublicKey();
-
-        $subject = new X509();
-        $subject->setEndDate('99991231235959Z');
-        $subject->setDNProp('id-at-organizationName', 'Akeneo');
-        $subject->setPublicKey($pubKey);
-
-        $issuer = new X509();
-        $issuer->setPrivateKey($privKey);
-        $issuer->setDn($subject->getDN());
-
-        $x509 = new X509();
-        $result = $x509->sign($issuer, $subject);
-
-        return AsymmetricKeys::create($x509->saveX509($result), $privateKey);
     }
 }
