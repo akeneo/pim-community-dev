647c7da45776083588936efa27bff538d5143137
Riff <riff.ventura@gmail.com>
2022-01-13 16:37:27 +0100

    CXP-1038 typo fix

--- src/Akeneo/Connectivity/Connection/back/Application/Marketplace/TestApps/Command/DeleteTestAppHandler.php
+++ src/Akeneo/Connectivity/Connection/back/Application/Marketplace/TestApps/Command/DeleteTestAppHandler.php
@@ -8,7 +8,7 @@ use Akeneo\Connectivity\Connection\Domain\Marketplace\TestApps\Persistence\Delet
 use Akeneo\Connectivity\Connection\Domain\Marketplace\TestApps\Persistence\GetTestAppQueryInterface;
 
 /**
- * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class DeleteTestAppHandler
--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Marketplace/TestApps/Controller/InternalApi/DeleteTestAppAction.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Marketplace/TestApps/Controller/InternalApi/DeleteTestAppAction.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
- * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 final class DeleteTestAppAction
