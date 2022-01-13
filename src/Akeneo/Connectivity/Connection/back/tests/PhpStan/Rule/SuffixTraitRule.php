f44ea3396c38e2850cf15fdaac13e6138d5a1ac6
Thomas Ramel <thomas@thomasramel.com>
2022-01-13 11:04:45 +0100

    CXP-957: [rule] Each backend trait should have the Trait suffix

---
+++ src/Akeneo/Connectivity/Connection/back/tests/PhpStan/Rule/SuffixTraitRule.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Tests\PhpStan\Rule;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\Trait_;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+
+final class SuffixTraitRule implements Rule
+{
+    private const ERROR_MESSAGE = 'Trait must be suffixed with "Trait" exclusively';
+
+    public function getNodeType(): string
+    {
+        return Node::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!\is_a($node, ClassLike::class, true)) {
+            return [];
+        }
+
+        if (\str_ends_with((string) $node->name, 'Trait')) {
+            if (!$node instanceof Trait_) {
+                return [self::ERROR_MESSAGE];
+            }
+
+            return [];
+        }
+
+        if ($node instanceof Trait_) {
+            return [self::ERROR_MESSAGE];
+        }
+
+        return [];
+    }
+}
