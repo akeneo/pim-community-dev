37bdf93694469c3e6212dfe9dbe4dc4da9810b73
Antoine Trouve <antoine.trouve@akeneo.com>
2022-01-14 16:09:57 +0100

    PLG-692 : Update password review

---
+++ src/Akeneo/UserManagement/Bundle/Notification/MailResetNotifier.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\UserManagement\Bundle\Notification;
+
+use Akeneo\Platform\Bundle\NotificationBundle\Email\MailNotifierInterface as MailNotification;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Throwable;
+use Twig\Environment;
+
+/**
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class MailResetNotifier
+{
+    public function __construct(
+        protected LoggerInterface $logger,
+        protected Environment $twig,
+        protected MailNotification $mailer
+    ) {
+    }
+
+    public function notify(UserInterface $user)
+    {
+        $parameters = ['user' => $user];
+
+        try {
+            $txtBody = $this->twig->render('@PimUser/Mail/reset.txt.twig', $parameters);
+            $htmlBody = $this->twig->render('@PimUser/Mail/reset.html.twig', $parameters);
+            $this->mailer->notifyByEmail($user->getEmail(), 'Reset password', $txtBody, $htmlBody);
+        } catch (Throwable $exception) {
+            $this->logger->error(
+                MailResetNotifier::class . ' - Unable to send email : ' . $exception->getMessage(),
+                ['Exception' => $exception]
+            );
+            return;
+        }
+    }
+
+}
