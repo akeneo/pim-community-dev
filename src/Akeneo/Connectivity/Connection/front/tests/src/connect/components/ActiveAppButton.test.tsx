5c0aaef3e020c329bb9a364bf76481bb82dc8364
Riff <riff.ventura@gmail.com>
2022-01-19 16:17:11 +0100

    CXP-928: Removed outdated test

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/ActiveAppButton.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/ActiveAppButton.test.tsx
@@ -28,12 +28,3 @@ test('button renders for non connected app', () => {
     expect(button).toHaveAttribute('href', '#akeneo_connectivity_connection_connect_apps_activate?id=appId');
     expect(button).not.toHaveAttribute('disabled');
 });
-
-test('button renders disabled for non connected app when limit is reached', () => {
-    renderWithProviders(<ActivateAppButton id='appId' isConnected={false} isLimitReached={true} />);
-
-    const button = screen.queryByText('akeneo_connectivity.connection.connect.marketplace.card.connect');
-
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveAttribute('disabled');
-});
