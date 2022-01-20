1300aeb1069fd40559ff553d08947080c05a95d2
Celine Morin <celine.morin@akeneo.com>
2022-01-20 11:58:10 +0100

    CPM-483: Fix useRecords using cache

--- grth/src/Akeneo/Pim/TableAttribute/front/src/repositories/ReferenceEntityRecord.ts
+++ grth/src/Akeneo/Pim/TableAttribute/front/src/repositories/ReferenceEntityRecord.ts
@@ -2,7 +2,26 @@ import {ChannelCode, LocaleCode, Router} from '@akeneo-pim-community/shared';
 import {RecordCode, ReferenceEntityIdentifierOrCode, ReferenceEntityRecord} from '../models';
 import {RecordFetcher} from '../fetchers';
 
+type Params = {
+  search?: string;
+  page?: number;
+  itemsPerPage?: number;
+  channel: ChannelCode;
+  locale: LocaleCode;
+  codes?: RecordCode[];
+};
+
+type CallParam = Params & {
+  referenceEntityIdentifier: ReferenceEntityIdentifierOrCode;
+};
+
 const referenceEntityRecordsCache: {[key: string]: ReferenceEntityRecord | null} = {};
+const referenceEntityRecordsCalls: {method: string; params: CallParam; result: Promise<ReferenceEntityRecord[]>}[] = [];
+
+const clearCache = () => {
+  referenceEntityRecordsCalls.splice(0, referenceEntityRecordsCalls.length);
+  Object.entries(referenceEntityRecordsCache).forEach(([key]) => delete referenceEntityRecordsCache[key]);
+};
 
 const getKey: (referenceEntityIdentifier: ReferenceEntityIdentifierOrCode, recordCode: RecordCode) => string = (
   referenceEntityIdentifier,
@@ -11,22 +30,53 @@ const getKey: (referenceEntityIdentifier: ReferenceEntityIdentifierOrCode, recor
   return `${referenceEntityIdentifier}-${recordCode}`;
 };
 
+const getCachedCall = (
+  referenceEntityIdentifier: ReferenceEntityIdentifierOrCode,
+  params: Params
+): Promise<ReferenceEntityRecord[]> | undefined => {
+  const found = referenceEntityRecordsCalls.find(item => {
+    const {search, page, itemsPerPage, codes} = params;
+    return (
+      referenceEntityIdentifier === item.params.referenceEntityIdentifier &&
+      search === item.params.search &&
+      page === item.params.page &&
+      itemsPerPage === item.params.itemsPerPage &&
+      JSON.stringify(codes) === JSON.stringify(params.codes)
+    );
+  });
+  return found?.result;
+};
+
+const saveCall = (
+  referenceEntityIdentifier: ReferenceEntityIdentifierOrCode,
+  params: Params,
+  result: Promise<ReferenceEntityRecord[]>
+): void => {
+  referenceEntityRecordsCalls.push({
+    method: 'search',
+    params: {
+      ...params,
+      referenceEntityIdentifier,
+    },
+    result,
+  });
+};
+
 const search: (
   router: Router,
   referenceEntityIdentifier: ReferenceEntityIdentifierOrCode,
-  props: {
-    search?: string;
-    page?: number;
-    itemsPerPage?: number;
-    channel: ChannelCode;
-    locale: LocaleCode;
-    codes?: RecordCode[];
-  }
+  props: Params
 ) => Promise<ReferenceEntityRecord[]> = async (router, referenceEntityIdentifier, props) => {
   let codesToFetch: RecordCode[] | undefined = undefined;
   const alreadyFetchedRecords: ReferenceEntityRecord[] = [];
+
+  // check if promise has already been called or if it is already loading
+  const cachedCall = getCachedCall(referenceEntityIdentifier, props);
+  if (cachedCall) return cachedCall;
+
   if (props.codes) {
     const nonFetchedRecords: RecordCode[] = [];
+
     props.codes.forEach(recordCode => {
       const record = referenceEntityRecordsCache[getKey(referenceEntityIdentifier, recordCode)];
       if (typeof record !== 'undefined') {
@@ -44,7 +94,10 @@ const search: (
     }
   }
 
-  const records = await RecordFetcher.search(router, referenceEntityIdentifier, {...props, codes: codesToFetch});
+  const recordsPromise = RecordFetcher.search(router, referenceEntityIdentifier, {...props, codes: codesToFetch});
+  saveCall(referenceEntityIdentifier, props, recordsPromise);
+
+  const records = await recordsPromise;
 
   records.forEach(record => {
     if (!(getKey(referenceEntityIdentifier, record.code) in referenceEntityRecordsCache)) {
@@ -58,7 +111,7 @@ const search: (
     referenceEntityRecordsCache[getKey(referenceEntityIdentifier, recordCode)] = null;
   });
 
-  return records.concat(alreadyFetchedRecords);
+  return Promise.resolve(records.concat(alreadyFetchedRecords));
 };
 
 const findByCode: (
@@ -88,6 +141,7 @@ const ReferenceEntityRecordRepository = {
   search,
   findByCode,
   getCachedByCode,
+  clearCache,
 };
 
 export {ReferenceEntityRecordRepository};
