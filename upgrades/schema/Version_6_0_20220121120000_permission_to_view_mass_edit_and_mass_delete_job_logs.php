852eb2ed3998a555f46a6b01b15a0feeabdcb152
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-01-21 14:31:05 +0100

    PIM-10238: Handle mass delete records and mass edit assets

--- upgrades/schema/Version_6_0_20220121120000_permission_to_view_mass_edit_and_mass_delete_job_logs.php
+++ upgrades/schema/Version_6_0_20220121120000_permission_to_view_mass_edit_and_mass_delete_job_logs.php
@@ -2,15 +2,20 @@
 
 namespace Pim\Upgrade\Schema;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Add the permission to download logs for the "asset_manager_mass_delete_assets" job.
+ * Add the permission to download logs for the "asset_manager_mass_delete_assets", "asset_manager_mass_edit_assets" and "reference_entity_mass_delete_records" jobs.
  */
-final class Version_6_0_20220121120000_permission_to_view_asset_manager_mass_delete_assets_job_logs extends AbstractMigration
+final class Version_6_0_20220121120000_permission_to_view_mass_edit_and_mass_delete_job_logs extends AbstractMigration
 {
-    private const JOB_CODE = 'asset_manager_mass_delete_assets';
+    private const JOB_CODES = [
+        'asset_manager_mass_delete_assets',
+        'asset_manager_mass_edit_assets',
+        'reference_entity_mass_delete_records',
+    ];
 
     public function up(Schema $schema) : void
     {
@@ -20,9 +25,9 @@ SELECT ji.id as job_profile_id, oap.id as user_group_id, 1, 0
 FROM akeneo_batch_job_instance ji
     CROSS JOIN oro_access_group oap
     LEFT JOIN pimee_security_job_profile_access pa On pa.user_group_id = oap.id AND pa.job_profile_id = ji.id
-WHERE oap.name = 'All' AND pa.id IS NULL AND ji.code = :jobCode
+WHERE oap.name = 'All' AND pa.id IS NULL AND ji.code IN (:jobCodes)
 SQL;
-        $this->addSql($sql, ['jobCode' => self::JOB_CODE]);
+        $this->addSql($sql, ['jobCodes' => self::JOB_CODES], ['jobCodes' => Connection::PARAM_STR_ARRAY]);
     }
 
     public function down(Schema $schema) : void
