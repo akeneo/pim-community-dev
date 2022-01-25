0b1f2da40534731da3159dea707591c5f0068762
Steven VAIDIE <steven.vaidie@gmail.com>
2022-01-25 17:08:21 +0100

    YARN3-POC1: RAC: test

--- front-packages/akeneo-design-system/src/components/Modal/Modal.visual.tsx
+++ front-packages/akeneo-design-system/src/components/Modal/Modal.visual.tsx
@@ -4,19 +4,38 @@ expect.extend({toMatchImageSnapshot});
 
 const storyIds = ['standard', 'with-illustration'];
 test.each(storyIds)('Test modal %s is displayed correctly', async storyId => {
+  console.log('1');
+
   await page.goto(`http://localhost:6006/iframe.html?id=components-modal--${storyId}`);
+  console.log('2');
+
   const root = await page.waitFor('#root');
+  console.log('3');
+
   if (null === root) throw new Error('Cannot find root element');
+  console.log('4');
 
   const openButton = await page.$('button');
+  console.log('5');
   if (null === openButton) throw new Error('Cannot find button');
+  console.log('6');
+  console.log(page.viewport());
+  console.log(openButton);
+
   await openButton.click();
+  console.log('7');
 
   await new Promise(resolve => setTimeout(resolve, 500));
+  console.log('8');
 
   const modalRoot = await page.$('#modal-root > :first-child');
+  console.log('9');
+
   if (null === modalRoot) throw new Error('Cannot find modal root');
+  console.log('10');
+
   const image = await modalRoot.screenshot();
+  console.log('11');
 
   expect(image).toMatchImageSnapshot({
     failureThreshold: 1,
