b2c941c9e055c531811c345e207df0fd2333c665
Thomas Ramel <thomas@thomasramel.com>
2022-01-25 11:25:40 +0100

    CXP-1068: fix GetAppActivateUrlAction Spec

--- src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Infrastructure/InternalApi/Controller/Apps/GetAppActivateUrlActionSpec.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Unit/spec/Infrastructure/InternalApi/Controller/Apps/GetAppActivateUrlActionSpec.php
@@ -6,6 +6,7 @@ namespace spec\Akeneo\Connectivity\Connection\Infrastructure\InternalApi\Control
 
 use Akeneo\Connectivity\Connection\Application\Marketplace\AppUrlGenerator;
 use Akeneo\Connectivity\Connection\Domain\Marketplace\GetAppQueryInterface;
+use Akeneo\Connectivity\Connection\Domain\Settings\Persistence\Query\IsConnectionsNumberLimitReachedQueryInterface;
 use Akeneo\Connectivity\Connection\Infrastructure\Apps\OAuth\ClientProviderInterface;
 use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
 use Akeneo\Platform\Bundle\FrameworkBundle\Service\PimUrl;
@@ -14,15 +15,17 @@ use PhpSpec\ObjectBehavior;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class GetAppActivateUrlControllerSpec extends ObjectBehavior
+class GetAppActivateUrlActionSpec extends ObjectBehavior
 {
     public function let(
         GetAppQueryInterface $getAppQuery,
         ClientProviderInterface $clientProvider,
         SecurityFacade $security,
         FeatureFlag $featureFlag,
+        IsConnectionsNumberLimitReachedQueryInterface $isConnectionsNumberLimitReachedQuery,
     ): void {
         $this->beConstructedWith(
             $getAppQuery,
@@ -30,6 +33,7 @@ class GetAppActivateUrlControllerSpec extends ObjectBehavior
             new AppUrlGenerator(new PimUrl('https://some_pim_url')),
             $security,
             $featureFlag,
+            $isConnectionsNumberLimitReachedQuery,
         );
     }
 
@@ -67,15 +71,33 @@ class GetAppActivateUrlControllerSpec extends ObjectBehavior
             ->during('__invoke', [$request, 'foo']);
     }
 
+    public function it_throws_bad_request_exception_when_too_much_apps(
+        FeatureFlag $featureFlag,
+        SecurityFacade $security,
+        IsConnectionsNumberLimitReachedQueryInterface $isConnectionsNumberLimitReachedQuery,
+        Request $request,
+    ): void {
+        $request->isXmlHttpRequest()->willReturn(true);
+        $featureFlag->isEnabled()->willReturn(true);
+        $security->isGranted('akeneo_connectivity_connection_manage_apps')->willReturn(true);
+        $isConnectionsNumberLimitReachedQuery->execute()->willReturn(true);
+
+        $this
+            ->shouldThrow(new BadRequestHttpException('App and connections limit reached'))
+            ->during('__invoke', [$request, 'foo']);
+    }
+
     public function it_throws_not_found_exception_with_wrong_app_identifier(
         FeatureFlag $featureFlag,
         SecurityFacade $security,
+        IsConnectionsNumberLimitReachedQueryInterface $isConnectionsNumberLimitReachedQuery,
         GetAppQueryInterface $getAppQuery,
         Request $request,
     ): void {
         $request->isXmlHttpRequest()->willReturn(true);
         $featureFlag->isEnabled()->willReturn(true);
         $security->isGranted('akeneo_connectivity_connection_manage_apps')->willReturn(true);
+        $isConnectionsNumberLimitReachedQuery->execute()->willReturn(false);
         $getAppQuery->execute('foo')->willReturn(null);
 
         $this
