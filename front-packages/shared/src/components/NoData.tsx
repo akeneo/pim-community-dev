24d85c7ea27ee2bb2996cf1069172b6ec5db626b
pierallard <pierallard@gmail.com>
2022-01-25 13:50:12 +0100

    Make pim-dev work

--- front-packages/shared/src/components/NoData.tsx
+++ front-packages/shared/src/components/NoData.tsx
@@ -1,19 +1,19 @@
-import styled from 'styled-components';
-import {getColor, getFontSize} from 'akeneo-design-system';
+import styled, {StyledComponent, ThemeProps} from 'styled-components';
+import {getColor, getFontSize, Theme} from 'akeneo-design-system';
 
 const NoDataSection = styled.div`
   text-align: center;
   margin-top: 70px;
 `;
 
-const NoDataTitle = styled.div`
+const NoDataTitle: StyledComponent<"div", any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
   color: ${getColor('grey', 140)};
   font-size: ${getFontSize('title')};
   text-align: center;
   margin: 30px 0 20px 0;
 `;
 
-const NoDataText = styled.div`
+const NoDataText: StyledComponent<"div", any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
   color: ${getColor('grey', 120)};
   font-size: ${getFontSize('bigger')};
   text-align: center;
