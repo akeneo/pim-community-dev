e36f0843f83a4c952972e0e7b8f7c1983bb49ff4
pierallard <pierallard@gmail.com>
2022-01-26 08:02:47 +0100

    make lint fix OK

--- front-packages/shared/src/components/NoData.tsx
+++ front-packages/shared/src/components/NoData.tsx
@@ -6,14 +6,14 @@ const NoDataSection = styled.div`
   margin-top: 70px;
 `;
 
-const NoDataTitle: StyledComponent<"div", any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
+const NoDataTitle: StyledComponent<'div', any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
   color: ${getColor('grey', 140)};
   font-size: ${getFontSize('title')};
   text-align: center;
   margin: 30px 0 20px 0;
 `;
 
-const NoDataText: StyledComponent<"div", any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
+const NoDataText: StyledComponent<'div', any, Record<string, unknown> & ThemeProps<Theme>, never> = styled.div`
   color: ${getColor('grey', 120)};
   font-size: ${getFontSize('bigger')};
   text-align: center;
