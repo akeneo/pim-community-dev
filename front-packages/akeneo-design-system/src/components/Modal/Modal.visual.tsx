6caf7c8fc2dcb68b8d3f8f7cbd21c1a32773c4e4
Steven VAIDIE <steven.vaidie@gmail.com>
2022-01-26 09:13:06 +0100

    YARN3-POC1: WIP

--- front-packages/akeneo-design-system/src/components/Modal/Modal.visual.tsx
+++ front-packages/akeneo-design-system/src/components/Modal/Modal.visual.tsx
@@ -4,35 +4,20 @@ expect.extend({toMatchImageSnapshot});
 
 const storyIds = ['standard', 'with-illustration'];
 test.each(storyIds)('Test modal %s is displayed correctly', async storyId => {
-  console.log('1');
-
   await page.goto(`http://localhost:6006/iframe.html?id=components-modal--${storyId}`);
-  console.log('2');
-
-  const root = await page.waitFor('#root');
-  console.log('3');
-
+  const root = await page.waitForSelector('#root');
   if (null === root) throw new Error('Cannot find root element');
-  console.log('4');
 
-  await page.waitForSelector('button');
-  console.log('5');
-  await root.screenshot({path: 'buddy-screenshot.png'});
-
+//  const openButton = await page.$('button');
+//  if (null === openButton) throw new Error('Cannot find button');
+//  await openButton.click();
   await page.click('button');
-  console.log('6');
 
   await new Promise(resolve => setTimeout(resolve, 500));
-  console.log('8');
 
   const modalRoot = await page.$('#modal-root > :first-child');
-  console.log('9');
-
   if (null === modalRoot) throw new Error('Cannot find modal root');
-  console.log('10');
-
   const image = await modalRoot.screenshot();
-  console.log('11');
 
   expect(image).toMatchImageSnapshot({
     failureThreshold: 1,
