3cbb519da11b64bba1f7755bc273bc3cbf2c9515
pierallard <pierallard@gmail.com>
2022-01-26 14:56:29 +0100

    CPM-486: Fix tests

--- grth/src/Akeneo/Pim/TableAttribute/front/__tests__/factories/index.ts
+++ grth/src/Akeneo/Pim/TableAttribute/front/__tests__/factories/index.ts
@@ -1,6 +1,5 @@
 export * from './Attributes';
 export * from './ColumnDefinition';
-export * from './ColumnDefinitionProperties';
 export * from './Locales';
 export * from './TableConfiguration';
 export * from './TableValue';
