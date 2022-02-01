8efab25818d06eb0b289cb54c5f40c5b15b9179e
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-02-01 15:11:28 +0100

    RAB-490: Fix copyright

--- components/tailored-import/back/tests/Integration/Infrastructure/Validation/IsValidAttributeValidatorTest.php
+++ components/tailored-import/back/tests/Integration/Infrastructure/Validation/IsValidAttributeValidatorTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the Akeneo PIM Enterprise Edition.
  *
- * (c) 2021 Akeneo SAS (https://www.akeneo.com)
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@ use Akeneo\Platform\TailoredImport\Infrastructure\Validation\IsValidAttribute;
 use Akeneo\Test\Integration\Configuration;
 
 /**
- * @copyright 2021 Akeneo SAS (https://www.akeneo.com)
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  */
 final class IsValidAttributeValidatorTest extends AbstractValidationTest
 {
