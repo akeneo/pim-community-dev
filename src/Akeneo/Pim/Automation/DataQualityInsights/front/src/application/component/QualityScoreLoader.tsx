f72e72ce6b4337f07327ce63474899580c3534a1
Antoine Trouve <antoine.trouve@akeneo.com>
2022-02-01 15:25:46 +0100

    PLG-718 : Update loading quality score with skeleton.

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScoreLoader.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScoreLoader.tsx
@@ -1,28 +1,35 @@
 import styled from 'styled-components';
 import React from 'react';
-import {getColor, LoaderIcon} from 'akeneo-design-system';
+import {SkeletonPlaceholder} from 'akeneo-design-system';
 
 const QualityScoreLoader = () => {
   return (
     <Container>
-      <QualityScoreLoaderIcon />
+      <Skeleton />
+      <Border />
     </Container>
   );
 };
 
-const QualityScoreLoaderIcon = styled(LoaderIcon)`
-  color: ${getColor('grey100')};
+const Container = styled.div`
+  display: inline-flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: center;
+  height: 25px;
+  top: 1px;
+  padding-top: 2px;
+  margin-right: 20px;
 `;
 
-const Container = styled.div`
-  display: flex;
-  position: relative;
-  top: 1px;
+const Border = styled.div`
   border-right: 1px ${({theme}) => theme.color.grey80} solid;
-  padding-right: 20px;
+`;
+
+const Skeleton = styled(SkeletonPlaceholder)`
+  width: 102px;
   margin-right: 20px;
-  padding-top: 2px;
-  height: 25px;
 `;
 
 export {QualityScoreLoader};
