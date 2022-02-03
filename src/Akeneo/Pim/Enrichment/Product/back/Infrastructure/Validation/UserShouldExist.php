b4b215f0418640eb6cbe1d750c547268aa62927f
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-03 17:16:03 +0100

    CPM-493: add validation to check the user

--- src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Validation/UserShouldExist.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Validation/UserShouldExist.php
@@ -10,15 +10,7 @@ use Symfony\Component\Validator\Constraint;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UserCategoryShouldBeGranted extends Constraint
+class UserShouldExist extends Constraint
 {
-    public string $message = 'pim_enrich.product.validation.upsert.category_no_access_to_products';
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getTargets()
-    {
-        return self::CLASS_CONSTRAINT;
-    }
+    public string $message = 'pim_enrich.product.validation.upsert.unknown_user';
 }
