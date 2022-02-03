03ce0cc107a9f9d773b9258ab99cd8b6af493e53
Laurent Petard <laurent.petard@akeneo.com>
2022-02-03 17:00:26 +0100

    PLG-679: Use a hook to generate the product evaluation URL

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/hooks/ProductEditForm/useEvaluateProduct.ts
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/hooks/ProductEditForm/useEvaluateProduct.ts
@@ -1,14 +1,18 @@
 import {useCallback} from 'react';
 import {Product} from '../../../domain';
-
-const Routing = require('routing');
+import {useRoute} from '@akeneo-pim-community/shared';
 
 const useEvaluateProduct = (product: Product) => {
+  if (null === product.meta.id) {
+    return;
+  }
+
   const routeName =
     'product_model' === product.meta.model_type
       ? 'akeneo_data_quality_insights_evaluate_product_model'
       : 'akeneo_data_quality_insights_evaluate_product';
-  const url = Routing.generate(routeName, {productId: product.meta.id});
+
+  const url = useRoute(routeName, {productId: product.meta.id.toString()});
 
   return useCallback(async () => {
     await fetch(url, {
