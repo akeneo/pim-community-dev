19aefea3bca0f95a348c87780f298bd2c62a20f6
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-03 18:19:01 +0100

    CPM-493: add tests

--- src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/AntiCorruptionLayer/AclIsUserCategoryGranted.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/AntiCorruptionLayer/AclIsUserCategoryGranted.php
@@ -13,7 +13,7 @@ use Webmozart\Assert\Assert;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AclIsUserCategoryGranted implements IsUserCategoryGranted
+final class AclIsUserCategoryGranted implements IsUserCategoryGranted
 {
     public function __construct(private ?ProductCategoryAccessQueryInterface $productCategoryAccessQuery)
     {
--- src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Validation/UserShouldExist.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Validation/UserShouldExist.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraint;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UserShouldExist extends Constraint
+final class UserShouldExist extends Constraint
 {
     public string $message = 'pim_enrich.product.validation.upsert.unknown_user';
 }
