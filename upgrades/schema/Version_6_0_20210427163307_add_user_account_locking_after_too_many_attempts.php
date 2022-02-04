34949da4c9b964167e2d7ac938616a23ab659ea7
Steven VAIDIE <steven.vaidie@gmail.com>
2022-02-04 18:29:43 +0100

    PIM-10250: Remove pull up TODO and made Version_6_0_20210427163307_add_user_account_locking_after_too_many_attempts idempotent

--- upgrades/schema/Version_6_0_20210427163307_add_user_account_locking_after_too_many_attempts.php
+++ upgrades/schema/Version_6_0_20210427163307_add_user_account_locking_after_too_many_attempts.php
@@ -15,6 +15,10 @@ final class Version_6_0_20210427163307_add_user_account_locking_after_too_many_a
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->skipIf(
+            $this->hasColumn('consecutive_authentication_failure_counter') && $this->hasColumn('authentication_failure_reset_date'),
+            'consecutive_authentication_failure_counter & authentication_failure_reset_date column already exists in oro_user'
+        );
 
         $this->addSql('alter table oro_user add consecutive_authentication_failure_counter int default 0');
         $this->addSql('alter table oro_user add authentication_failure_reset_date datetime  default null ');
@@ -23,5 +27,10 @@ final class Version_6_0_20210427163307_add_user_account_locking_after_too_many_a
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+    }
+
+    private function hasColumn(string $columnName)
+    {
+        return $schema->getTable('oro_user')->hasColumn($columnName);
     }
 }
