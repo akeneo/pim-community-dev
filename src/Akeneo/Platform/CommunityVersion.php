b3506806fadbab472b43d44b35c1a86cb9012b99
Guillaume Fer <guillaume.fer@akeneo.com>
2022-02-04 09:18:00 +0000

    Preparing version 5.0.72

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.71';
+    const VERSION = '5.0.72';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
