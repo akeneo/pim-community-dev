0e905ee77bae611fad52669cdf6e498284710c73
anthony-grandin <anthony.grandin@getakeneo.com>
2022-02-07 17:41:28 +0100

    RAB-485: implements logic to construct user intent in ProductProcessor.php

--- components/tailored-import/back/src/Infrastructure/Connector/Processor/ProductProcessor.php
+++ components/tailored-import/back/src/Infrastructure/Connector/Processor/ProductProcessor.php
@@ -2,6 +2,8 @@
 
 namespace Akeneo\Platform\TailoredImport\Infrastructure\Connector\Processor;
 
+use Akeneo\Pim\Enrichment\Product\Api\Command\UserIntent\SetTextValue;
+use Akeneo\Platform\TailoredImport\Application\Common\DataMapping;
 use Akeneo\Platform\TailoredImport\Application\Common\DataMappingCollection;
 use Akeneo\Platform\TailoredImport\Application\Common\Row;
 use Akeneo\Tool\Component\Batch\Item\ItemProcessorInterface;
@@ -20,6 +22,17 @@ class ProductProcessor implements ItemProcessorInterface, StepExecutionAwareInte
 
         $dataMappingCollection = DataMappingCollection::createFromNormalized($this->stepExecution->getJobParameters()->get('import_structure')['data_mappings']);
 
+        $userIntents = [];
+
+        /** @var DataMapping $dataMapping */
+        foreach ($dataMappingCollection->getIterator() as $dataMapping) {
+            $cellData = implode("", array_map(static fn(string $uuid) => $item->getCellData($uuid), $dataMapping->getSources()));
+            /** TODO Iterate over operation */
+            $target = $dataMapping->getTarget();
+            $userIntents[] = new SetTextValue($target->getCode(), $target->getLocale(), $target->getChannel(), $cellData);
+        }
+
+        return $userIntents;
     }
 
     public function setStepExecution(StepExecution $stepExecution)
