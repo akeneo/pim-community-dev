1318552697622536ac6d10a6828668bba29429cf
Steven VAIDIE <steven.vaidie@gmail.com>
2022-02-09 14:38:35 +0100

    PIM-10263: Add missing migration test

--- upgrades/schema/Version_6_0_20210427163307_add_user_account_locking_after_too_many_attempts.php
+++ upgrades/schema/Version_6_0_20210427163307_add_user_account_locking_after_too_many_attempts.php
@@ -22,6 +22,6 @@ final class Version_6_0_20210427163307_add_user_account_locking_after_too_many_a
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->throwIrreversibleMigrationException();
     }
 }
