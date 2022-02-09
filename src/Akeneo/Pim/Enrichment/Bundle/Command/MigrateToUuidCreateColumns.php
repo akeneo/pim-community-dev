ca07153483d8577290eceab0463906143f0f06f4
pierallard <pierallard@gmail.com>
2022-02-09 17:11:41 +0100

    CPM-503: WIP

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidCreateColumns.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidCreateColumns.php
@@ -5,22 +5,23 @@ namespace Akeneo\Pim\Enrichment\Bundle\Command;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MigrateToUuidCreateColumns
+class MigrateToUuidCreateColumns implements MigrateToUuidStep
 {
-    private const TABLES = [
-        'pim_catalog_product' => 'id',
-        'pim_catalog_association' => 'owner_id',
-        'pim_catalog_association_product' => 'product_id',
-        'pim_catalog_association_product_model_to_product' => 'product_id',
-        'pim_catalog_category_product' => 'product_id',
-        'pim_catalog_group_product' => 'product_id',
-        'pim_catalog_product_unique_data' => 'product_id',
-        'pim_data_quality_insights_product_criteria_evaluation' => 'product_id',
-        'pim_data_quality_insights_product_score' => 'product_id',
-        'pimee_teamwork_assistant_completeness_per_attribute_group' => 'product_id',
-        'pimee_teamwork_assistant_project_product' => 'product_id',
-        'pimee_workflow_product_draft' => 'product_id',
-        'pimee_workflow_published_product' => 'original_product_id'
+    public const TABLES = [
+        'pim_catalog_product' => ['id', 'uuid'],
+        'pim_catalog_association' => ['owner_id', 'owner_uuid'],
+        'pim_catalog_association_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_association_product_model_to_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_category_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_group_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_product_unique_data' => ['product_id', 'product_uuid'],
+        'pim_data_quality_insights_product_criteria_evaluation' => ['product_id', 'product_uuid'],
+        'pim_data_quality_insights_product_score' => ['product_id', 'product_uuid'],
+        'pimee_teamwork_assistant_completeness_per_attribute_group' => ['product_id', 'product_uuid'],
+        'pimee_teamwork_assistant_project_product' => ['product_id', 'product_uuid'],
+        'pimee_workflow_product_draft' => ['product_id', 'product_uuid'],
+        'pimee_workflow_published_product' => ['original_product_id', 'original_product_uuid'],
+        'pim_versioning_version' => ['resource_id', 'resource_uuid'],
     ];
 
     public function __construct(private Connection $dbConnection)
@@ -30,9 +31,8 @@ class MigrateToUuidCreateColumns
     public function getMissingCount(OutputInterface $output): int
     {
         $count = 0;
-        foreach (self::TABLES as $tableName => $idColumnName) {
-            $uuidColumnName = $this->getUuidColumnName($idColumnName);
-            if ($this->tableExists($tableName) && !$this->columnExists($tableName, $uuidColumnName)) {
+        foreach (self::TABLES as $tableName => $columnNames) {
+            if ($this->tableExists($tableName) && !$this->columnExists($tableName, $columnNames[1])) {
                 $output->writeln(sprintf('... missing %s', $tableName));
                 $count++;
             }
@@ -43,13 +43,12 @@ class MigrateToUuidCreateColumns
 
     public function addMissing(OutputInterface $output): void
     {
-        foreach (self::TABLES as $tableName => $idColumnName) {
-            $uuidColumnName = $this->getUuidColumnName($idColumnName);
-            if ($this->tableExists($tableName) && !$this->columnExists($tableName, $uuidColumnName)) {
+        foreach (self::TABLES as $tableName => $columnNames) {
+            if ($this->tableExists($tableName) && !$this->columnExists($tableName, $columnNames[1])) {
                 $output->writeln(sprintf('... add %s', $tableName));
                 $addUuidColumnQuery = sprintf(<<<SQL
     ALTER TABLE `%s` ADD `%s` BINARY(16) DEFAULT NULL AFTER `%s`, LOCK=NONE, ALGORITHM=INPLACE;
-    SQL, $tableName, $uuidColumnName, $idColumnName);
+    SQL, $tableName, $columnNames[1], $columnNames[0]);
 
                 $this->dbConnection->executeQuery($addUuidColumnQuery);
             }
@@ -76,10 +75,5 @@ class MigrateToUuidCreateColumns
         );
 
         return count($rows) >= 1;
-    }
-
-    private function getUuidColumnName(string $columnName): string
-    {
-        return preg_replace('/(.*)id$/', '$1uuid', $columnName);
     }
 }
