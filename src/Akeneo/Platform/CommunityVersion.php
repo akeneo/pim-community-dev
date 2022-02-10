2e9d11d5def0ac7350b199a71941a4e1c39c3148
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-02-10 12:18:34 +0000

    Preparing version 6.0.4

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.3';
+    public const VERSION = '6.0.4';
 
     /** @staticvar string */
     public const VERSION_CODENAME = 'Buccaneer Bunny';
