9c3439d19e07e3c2f7e93474c935fec5ca2ab288
pierallard <pierallard@gmail.com>
2022-02-10 12:26:43 +0100

    wip2

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidStep.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidStep.php
@@ -6,7 +6,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 interface MigrateToUuidStep
 {
-    public function getMissingCount(OutputInterface $output): int;
+    public function getMissingCount(): int;
 
-    public function addMissing(OutputInterface $output): void;
+    public function addMissing(bool $dryRun, OutputInterface $output): void;
+
+    public function getDescription(): string;
 }
