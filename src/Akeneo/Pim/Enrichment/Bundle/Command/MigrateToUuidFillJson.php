d4b1ac36aec03e3a4ea1c126d86fd6bc23e9e793
pierallard <pierallard@gmail.com>
2022-02-10 12:26:43 +0100

    wip2

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidFillJson.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidFillJson.php
@@ -11,26 +11,49 @@ class MigrateToUuidFillJson implements MigrateToUuidStep
     {
     }
 
-    public function getMissingCount(OutputInterface $output): int
+    public function getDescription(): string
+    {
+        return 'Adds product_uuid field in JSON objects';
+    }
+
+    public function getMissingCount(): int
     {
         $sql = "
 SELECT COUNT(1)
-FROM pim_catalog_product 
+FROM pim_catalog_product
 WHERE JSON_CONTAINS_PATH(quantified_associations, 'one', '$.*.products[*].id')
-  AND NOT JSON_CONTAINS_PATH(quantified_associations, 'one', '$.*.products[*].uuid');";
+AND NOT JSON_CONTAINS_PATH(quantified_associations, 'one', '$.*.products[*].uuid');";
 
         $result = $this->connection->fetchOne($sql);
 
         return (int) $result;
     }
 
-    public function addMissing(OutputInterface $output): void
+    public function addMissing(bool $dryRun, OutputInterface $output): void
     {
-        $sql = "
-SELECT id, quantified_associations
-FROM pim_catalog_product 
-WHERE JSON_CONTAINS_PATH(quantified_associations, 'one', '$.*.products[*].id')
-  AND NOT JSON_CONTAINS_PATH(quantified_associations, 'one', '$.*.products[*].uuid') LIMIT 1;";
+        $output->writeln('TODO');
 
+        $sql = '
+SELECT *,
+       JSON_EXTRACT(
+    pim_catalog_product.quantified_associations,
+    CONCAT("$[", association_code, "]")
+) assocation_values
+       
+       FROM (
+    SELECT id AS product_id, 
+    association_codes.association_code
+    FROM pim_catalog_product,
+    JSON_TABLE(
+        JSON_KEYS(pim_catalog_product.quantified_associations),
+        "$[*]"
+        COLUMNS (
+            association_code varchar(100) PATH "$")
+        ) association_codes
+) toto
+
+';
     }
+
 }
+
