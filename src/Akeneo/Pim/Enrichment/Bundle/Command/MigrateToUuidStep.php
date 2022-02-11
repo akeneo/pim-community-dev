19494ff51742c39bbee9b5bfafdb09197097fef0
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-11 17:22:36 +0100

    CPM-503: add triggers WIP

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidStep.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidStep.php
@@ -6,6 +6,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 interface MigrateToUuidStep
 {
+    public const TABLES = [
+        'pim_catalog_product' => ['id', 'uuid'],
+        'pim_catalog_association' => ['owner_id', 'owner_uuid'],
+        'pim_catalog_association_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_association_product_model_to_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_category_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_group_product' => ['product_id', 'product_uuid'],
+        'pim_catalog_product_unique_data' => ['product_id', 'product_uuid'],
+        'pim_data_quality_insights_product_criteria_evaluation' => ['product_id', 'product_uuid'],
+        'pim_data_quality_insights_product_score' => ['product_id', 'product_uuid'],
+        'pimee_teamwork_assistant_completeness_per_attribute_group' => ['product_id', 'product_uuid'],
+        'pimee_teamwork_assistant_project_product' => ['product_id', 'product_uuid'],
+        'pimee_workflow_product_draft' => ['product_id', 'product_uuid'],
+        'pimee_workflow_published_product' => ['original_product_id', 'original_product_uuid'],
+        'pim_versioning_version' => ['resource_id', 'resource_uuid'],
+    ];
+    public const ID_COLUMN_INDEX = 0;
+    public const UUID_COLUMN_INDEX = 1;
+
     public function getMissingCount(): int;
 
     public function addMissing(bool $dryRun, OutputInterface $output): void;
