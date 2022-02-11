090eacadd98935c3af402b36ae24129ad649bb7c
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-11 17:22:36 +0100

    CPM-503: add triggers WIP

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidCreateColumns.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidCreateColumns.php
@@ -9,23 +9,6 @@ class MigrateToUuidCreateColumns implements MigrateToUuidStep
 {
     use MigrateToUuidTrait;
 
-    public const TABLES = [
-        'pim_catalog_product' => ['id', 'uuid'],
-        'pim_catalog_association' => ['owner_id', 'owner_uuid'],
-        'pim_catalog_association_product' => ['product_id', 'product_uuid'],
-        'pim_catalog_association_product_model_to_product' => ['product_id', 'product_uuid'],
-        'pim_catalog_category_product' => ['product_id', 'product_uuid'],
-        'pim_catalog_group_product' => ['product_id', 'product_uuid'],
-        'pim_catalog_product_unique_data' => ['product_id', 'product_uuid'],
-        'pim_data_quality_insights_product_criteria_evaluation' => ['product_id', 'product_uuid'],
-        'pim_data_quality_insights_product_score' => ['product_id', 'product_uuid'],
-        'pimee_teamwork_assistant_completeness_per_attribute_group' => ['product_id', 'product_uuid'],
-        'pimee_teamwork_assistant_project_product' => ['product_id', 'product_uuid'],
-        'pimee_workflow_product_draft' => ['product_id', 'product_uuid'],
-        'pimee_workflow_published_product' => ['original_product_id', 'original_product_uuid'],
-        'pim_versioning_version' => ['resource_id', 'resource_uuid'],
-    ];
-
     public function __construct(private Connection $connection)
     {
     }
@@ -46,7 +29,7 @@ class MigrateToUuidCreateColumns implements MigrateToUuidStep
     public function getMissingCount(): int
     {
         $count = 0;
-        foreach (self::TABLES as $tableName => $columnNames) {
+        foreach (MigrateToUuidStep::TABLES as $tableName => $columnNames) {
             if ($this->tableExists($tableName) && !$this->columnExists($tableName, $columnNames[1])) {
                 $count++;
             }
@@ -57,7 +40,7 @@ class MigrateToUuidCreateColumns implements MigrateToUuidStep
 
     public function addMissing(bool $dryRun, OutputInterface $output): void
     {
-        foreach (self::TABLES as $tableName => $columnNames) {
+        foreach (MigrateToUuidStep::TABLES as $tableName => $columnNames) {
             if ($this->tableExists($tableName) && !$this->columnExists($tableName, $columnNames[1])) {
                 $output->writeln(sprintf('    Will add %s', $tableName));
                 if (!$dryRun) {
@@ -74,15 +57,5 @@ class MigrateToUuidCreateColumns implements MigrateToUuidStep
                 }
             }
         }
-    }
-
-    private function columnExists(string $tableName, string $columnName): bool
-    {
-        $rows = $this->connection->fetchAllAssociative(sprintf('SHOW COLUMNS FROM %s LIKE :columnName', $tableName),
-            [
-                'columnName' => $columnName,
-            ]);
-
-        return count($rows) >= 1;
     }
 }
