decc232638b9a67b2e8e69ba4b9e00c05536d814
Frederic Sarron <frederic.sarron@getakeneo.com>
2022-02-14 09:26:14 +0100

    feat(DQI): PLG-747 Stacked apperearance of the DQI scores

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScore.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScore.tsx
@@ -3,36 +3,91 @@ import styled, {css} from 'styled-components';
 
 type Props = {
   score: string | null;
+  appearance: 'regular' | 'stacked';
   className?: string;
+  isSelected?: boolean;
 };
 
-const QualityScore: FC<Props> = ({score, className}) => {
+// TODO remove className prop ? (seems unused)
+
+const QualityScore: FC<Props> = ({score, className, isSelected, appearance = 'regular'}) => {
   if (score === 'N/A' || score === null) {
     return <>N/A</>;
   }
 
+  const topStackStyle = isSelected
+    ? {
+        top: -2,
+        left: 0,
+      }
+    : {top: 0, left: 0};
+
   return (
-    <Container score={score} className={className}>
-      {score}
-    </Container>
+    <Wrapper isSelected={isSelected}>
+      {appearance === 'stacked' && isSelected && (
+        <>
+          <Container
+            isSelected
+            className={className}
+            style={{top: topStackStyle.top - 4, left: topStackStyle.left + 4}}
+          />
+          <Container
+            isSelected
+            className={className}
+            style={{top: topStackStyle.top - 2, left: topStackStyle.left + 2}}
+          />
+        </>
+      )}
+      <Container isSelected={isSelected} score={score} className={className} style={topStackStyle}>
+        {score}
+      </Container>
+    </Wrapper>
   );
 };
 
-const Container = styled.div<{score: string}>`
-  width: 20px;
-  height: 20px;
-  font-size: 13px;
+const Wrapper = styled.div<{isSelected?: boolean}>`
+  position: relative;
+  ${props => (props.isSelected ? bigSize : standardSize)};
+`;
+
+const Container = styled.div<{score?: string; isSelected?: boolean}>`
+  position: absolute;
   text-align: center;
   display: inline-block;
   text-transform: uppercase;
   font-weight: bold;
   border-radius: 4px;
 
+  ${props => (props.isSelected ? bigSizeContainer : standardSizeContainer)}
+
   ${props => props.score === 'A' && AScore}
   ${props => props.score === 'B' && BScore}
   ${props => props.score === 'C' && CScore}
   ${props => props.score === 'D' && DScore}
   ${props => props.score === 'E' && EScore}
+`;
+
+const standardSize = css`
+  width: 20px;
+  height: 20px;
+`;
+
+const bigSize = css`
+  width: 25px;
+  height: 25px;
+`;
+
+const standardSizeContainer = css`
+  ${standardSize}
+  font-size: 13px;
+`;
+
+const bigSizeContainer = css`
+  ${bigSize}
+  font-size: 15px;
+  lineHeight: 25px,
+  border-radius: 4px !important;
+  border: thin solid black;
 `;
 
 const AScore = css`
--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScoreBar.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/QualityScoreBar.tsx
@@ -5,26 +5,34 @@ import {DATA_QUALITY_INSIGHTS_REDIRECT_TO_DQI_TAB} from '../listener';
 
 type Props = {
   currentScore: string | null;
+  appearance: 'regular' | 'stacked';
 };
 
-const QualityScoreBar: FC<Props> = ({currentScore}) => {
+const QualityScoreBar: FC<Props> = props => {
+  const {
+    currentScore,
+    // for testing PLG-747, should be 'regular' by default
+    // TODO change this in last commit
+    appearance = 'stacked',
+  } = props;
   return (
     <Container
       currentScore={currentScore}
       onClick={() => window.dispatchEvent(new CustomEvent(DATA_QUALITY_INSIGHTS_REDIRECT_TO_DQI_TAB))}
     >
-      {['A', 'B', 'C', 'D', 'E'].map((score: string) => {
-        return score === currentScore ? (
-          <SelectedScore key={`ranking-score-${currentScore}`} score={currentScore} />
-        ) : (
-          <QualityScore key={`ranking-score-${score}`} score={score} />
-        );
-      })}
+      {['A', 'B', 'C', 'D', 'E'].map((score: string) => (
+        <QualityScore
+          key={`ranking-score-${score}`}
+          isSelected={score === currentScore}
+          score={score}
+          appearance={appearance}
+        />
+      ))}
     </Container>
   );
 };
 
-const Container = styled.div<Props>`
+const Container = styled.div<{currentScore: string | null}>`
   display: flex;
   position: relative;
   top: 1px;
@@ -51,17 +59,6 @@ const Container = styled.div<Props>`
 
 const NoScoreStyle = css`
   opacity: 0.3;
-`;
-
-const SelectedScore = styled(QualityScore)`
-  width: 25px;
-  height: 25px;
-  border-radius: 4px !important;
-  line-height: 25px;
-  font-size: 15px;
-  top: -2px;
-  position: relative;
-  margin: 0 -2px 0 -2px;
 `;
 
 export {QualityScoreBar};
