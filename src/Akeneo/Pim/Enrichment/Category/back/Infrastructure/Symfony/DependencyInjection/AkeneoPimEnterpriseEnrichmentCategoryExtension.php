90f5bbc3a80d69e462b331ea462c02fc86141961
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-15 14:39:00 +0100

    CPM-493: add user category permission validation (#12859)

---
+++ src/Akeneo/Pim/Enrichment/Category/back/Infrastructure/Symfony/DependencyInjection/AkeneoPimEnterpriseEnrichmentCategoryExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AkeneoEnterprise\Pim\Enrichment\Category\Infrastructure\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class AkeneoPimEnterpriseEnrichmentCategoryExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('queries.yml');
+    }
+}
