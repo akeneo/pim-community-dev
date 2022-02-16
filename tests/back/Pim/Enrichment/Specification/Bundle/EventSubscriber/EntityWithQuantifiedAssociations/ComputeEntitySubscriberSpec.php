25f0206d38a647bceea47cc569bd968e8b349dd7
pierallard <pierallard@gmail.com>
2022-02-16 11:39:33 +0100

    CPM-503: Add uuid in quantified associations on save

--- tests/back/Pim/Enrichment/Specification/Bundle/EventSubscriber/EntityWithQuantifiedAssociations/ComputeEntitySubscriberSpec.php
+++ tests/back/Pim/Enrichment/Specification/Bundle/EventSubscriber/EntityWithQuantifiedAssociations/ComputeEntitySubscriberSpec.php
@@ -5,8 +5,10 @@ namespace Specification\Akeneo\Pim\Enrichment\Bundle\EventSubscriber\EntityWithQ
 use Akeneo\Pim\Enrichment\Bundle\EventSubscriber\EntityWithQuantifiedAssociations\ComputeEntitySubscriber;
 use Akeneo\Pim\Enrichment\Component\Product\Model\EntityWithQuantifiedAssociationsInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\QuantifiedAssociation\IdMapping;
+use Akeneo\Pim\Enrichment\Component\Product\Model\QuantifiedAssociation\UuidMapping;
 use Akeneo\Pim\Enrichment\Component\Product\Query\QuantifiedAssociation\GetIdMappingFromProductIdentifiersQueryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\QuantifiedAssociation\GetIdMappingFromProductModelCodesQueryInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\QuantifiedAssociation\GetUuidMappingFromProductIdentifiersQueryInterface;
 use Akeneo\Tool\Component\StorageUtils\StorageEvents;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -16,9 +18,14 @@ class ComputeEntitySubscriberSpec extends ObjectBehavior
 {
     function let(
         GetIdMappingFromProductIdentifiersQueryInterface $getIdMappingFromProductIdentifiers,
+        GetUuidMappingFromProductIdentifiersQueryInterface $getUuidMappingFromProductIdentifiers,
         GetIdMappingFromProductModelCodesQueryInterface $getIdMappingFromProductModelCodes
     ) {
-        $this->beConstructedWith($getIdMappingFromProductIdentifiers, $getIdMappingFromProductModelCodes);
+        $this->beConstructedWith(
+            $getIdMappingFromProductIdentifiers,
+            $getUuidMappingFromProductIdentifiers,
+            $getIdMappingFromProductModelCodes
+        );
     }
 
     function it_is_initializable()
@@ -35,6 +42,7 @@ class ComputeEntitySubscriberSpec extends ObjectBehavior
 
     function it_computes_quantified_associations(
         $getIdMappingFromProductIdentifiers,
+        $getUuidMappingFromProductIdentifiers,
         $getIdMappingFromProductModelCodes,
         GenericEvent $event,
         EntityWithQuantifiedAssociationsInterface $entityWithQuantifiedAssociations
@@ -43,13 +51,17 @@ class ComputeEntitySubscriberSpec extends ObjectBehavior
         $productModelCodes = ['product_1', 'product_2'];
         $productIdentifiers = ['product_model_1', 'product_model_2'];
         $productIdMapping = $this->anIdMapping();
+        $productUuidMapping = $this->aUuidMapping();
         $productModelIdMapping = $this->anIdMapping();
 
         $entityWithQuantifiedAssociations->getQuantifiedAssociationsProductIdentifiers()->willReturn($productIdentifiers);
         $entityWithQuantifiedAssociations->getQuantifiedAssociationsProductModelCodes()->willReturn($productModelCodes);
-        $entityWithQuantifiedAssociations->updateRawQuantifiedAssociations($productIdMapping, $productModelIdMapping);
+        $entityWithQuantifiedAssociations
+            ->updateRawQuantifiedAssociations($productIdMapping, $productUuidMapping, $productModelIdMapping)
+            ->shouldBeCalledOnce();
 
         $getIdMappingFromProductIdentifiers->execute($productIdentifiers)->willReturn($productIdMapping);
+        $getUuidMappingFromProductIdentifiers->execute($productIdentifiers)->willReturn($productUuidMapping);
         $getIdMappingFromProductModelCodes->execute($productModelCodes)->willReturn($productModelIdMapping);
 
         $this->computeRawQuantifiedAssociations($event);
@@ -66,5 +78,13 @@ class ComputeEntitySubscriberSpec extends ObjectBehavior
     private function anIdMapping(): IdMapping
     {
         return IdMapping::createFromMapping([1 => 'entity_1', 2 => 'entity_2']);
+    }
+
+    private function aUuidMapping(): UuidMapping
+    {
+        return UuidMapping::createFromMapping([
+            '3f090f5e-3f54-4f34-879c-87779297d130' => 'entity_1',
+            '52254bba-a2c8-40bb-abe1-195e3970bd93' => 'entity_2'
+        ]);
     }
 }
