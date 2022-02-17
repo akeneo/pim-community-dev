f7337fb68f4ee8b430fb111ba7381bba4c7b598d
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2022-02-17 15:46:29 +0100

    PLG-732 : Calculate and consolidate Product Model Score

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Application/ProductEvaluation/EvaluateProductModels.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Application/ProductEvaluation/EvaluateProductModels.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\DataQualityInsights\Application\ProductEvaluation;
 
+use Akeneo\Pim\Automation\DataQualityInsights\Application\Consolidation\ConsolidateProductScores;
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\Event\ProductModelsEvaluated;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductIdCollection;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
@@ -13,12 +16,16 @@ use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductIdCollec
 class EvaluateProductModels
 {
     public function __construct(
-        private EvaluatePendingCriteria $evaluatePendingProductModelCriteria
+        private EvaluatePendingCriteria $evaluatePendingProductModelCriteria,
+        private ConsolidateProductScores $consolidateProductScores,
+        private EventDispatcherInterface $eventDispatcher
     ) {
     }
 
     public function __invoke(ProductIdCollection $productModelIdCollection): void
     {
         $this->evaluatePendingProductModelCriteria->evaluateAllCriteria($productModelIdCollection);
+        $this->consolidateProductScores->consolidate($productModelIdCollection);
+        $this->eventDispatcher->dispatch(new ProductModelsEvaluated($productModelIdCollection));
     }
 }
