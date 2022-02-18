fa1f27da9a2beeb43130161fcf774126a7d6e2c9
Willy Mesnage <willy.mesnage@akeneo.com>
2022-02-18 13:15:20 +0000

    Preparing version 5.0.78

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.77';
+    const VERSION = '5.0.78';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
