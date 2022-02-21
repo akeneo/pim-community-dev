c572942c861a394476bfed915ad88821484f250a
Valentin Merlet <valentin.merlet@akeneo.com>
2022-02-21 17:25:21 +0100

    OB2-19: Add phpunit configuration and unit test on Supplier/Code VO

---
+++ tests/back/Onboarder/Unit/Domain/Supplier/CodeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Onboarder\Test\Unit\Domain\Supplier;
+
+use Akeneo\Onboarder\Domain\Supplier\Code;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class CodeTest extends KernelTestCase
+{
+    /** @test */
+    public function itDoesNotCreateASupplierCodeIfItsEmpty(): void
+    {
+        static::expectExceptionObject(new \InvalidArgumentException('The supplier code cannot be empty.'));
+
+        Code::fromString('');
+    }
+
+    /** @test */
+    public function itDoesNotCreateASupplierCodeIfItExceedsTheMaxLength(): void
+    {
+        static::expectExceptionObject(
+            new \InvalidArgumentException(
+                sprintf('The supplier code is too long. It should have %d characters or less.', 200)
+            )
+        );
+
+        Code::fromString(str_repeat('a', 201));
+    }
+
+    /** @test */
+    public function itDoesNotCreateASupplierCodeIfItContainsForbiddenCharacters(): void
+    {
+        static::expectExceptionObject(
+            new \InvalidArgumentException(
+                'The supplier code can only contain lowercase letters, numbers and underscores.'
+            )
+        );
+
+        Code::fromString('$uppli€rCØde');
+    }
+
+    /** @test */
+    public function itCreatesAndGetsASupplierCodeIfItsValid(): void
+    {
+        $code = Code::fromString('valid_supplier_code');
+
+        static::assertSame('valid_supplier_code', (string) $code);
+    }
+}
