1f8da62057dc9da96b2f49aac380195d73f88f25
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-21 11:23:34 +0100

    CPM-509: rename legacy bundle

--- src/Akeneo/Channel/Bundle/DependencyInjection/AkeneoLegacyChannelExtension.php
+++ src/Akeneo/Channel/Bundle/DependencyInjection/AkeneoLegacyChannelExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AkeneoChannelExtension extends Extension
+class AkeneoLegacyChannelExtension extends Extension
 {
     /**
      * {@inheritdoc}
