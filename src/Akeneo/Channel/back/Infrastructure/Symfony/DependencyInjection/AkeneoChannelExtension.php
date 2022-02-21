bbf198ccf56ec9015099b5fe57dfbf16bb2f8f17
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-02-21 11:24:58 +0100

    CPM-509: rename new channel bundle

--- src/Akeneo/Channel/back/Infrastructure/Symfony/DependencyInjection/AkeneoChannelExtension.php
+++ src/Akeneo/Channel/back/Infrastructure/Symfony/DependencyInjection/AkeneoChannelExtension.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class AkeneoChannelApiExtension extends Extension
+final class AkeneoChannelExtension extends Extension
 {
     /**
      * {@inheritDoc}
