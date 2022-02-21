d4f91c745818c150202e0d1e2f81bce9009244a8
Riff <riff.ventura@gmail.com>
2022-02-21 13:35:10 +0000

    Preparing version 6.0.6

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.5';
+    public const VERSION = '6.0.6';
 
     /** @staticvar string */
     public const VERSION_CODENAME = 'Buccaneer Bunny';
