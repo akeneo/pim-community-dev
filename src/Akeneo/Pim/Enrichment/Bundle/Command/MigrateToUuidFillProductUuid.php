4b846a6a6a200397532e6a46edd2dd588a3c00c2
pierallard <pierallard@gmail.com>
2022-02-22 18:02:33 +0100

    CPM-502: Clean before review

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidFillProductUuid.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuidFillProductUuid.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MigrateToUuidFillProductUuid implements MigrateToUuidStep
 {
+    use MigrateToUuidTrait;
+
     private const BATCH_SIZE = 1000;
 
     public function __construct(private Connection $connection)
@@ -19,18 +21,29 @@ class MigrateToUuidFillProductUuid implements MigrateToUuidStep
         return 'Generates uuid4 for every product';
     }
 
+    public function getMissingCount(): int
+    {
+        return $this->getMissingProductUuidCount();
+    }
     /**
      * {@inheritDoc}
      */
     public function shouldBeExecuted(): bool
     {
-        // @todo: improve
-        return 0 < $this->getMissingCount();
-    }
+        if (!$this->columnExists('pim_catalog_product', 'uuid')) {
+            return false;
+        }
 
-    public function getMissingCount(): int
-    {
-        return $this->getMissingProductUuidCount();
+        $sql = <<<SQL
+            SELECT EXISTS (
+                SELECT 1
+                FROM pim_catalog_product
+                WHERE uuid IS NULL
+                LIMIT 1
+            ) AS missing
+        SQL;
+
+        return (bool) $this->connection->fetchOne(\sprintf($sql, 'uuid'));
     }
 
     public function addMissing(bool $dryRun, OutputInterface $output): void
@@ -39,7 +52,7 @@ class MigrateToUuidFillProductUuid implements MigrateToUuidStep
         while ($count > 0) {
             $output->writeln(sprintf('    Will add %d uuids (still missing: %d)', min(self::BATCH_SIZE, $count), $count));
             if (!$dryRun) {
-                $this->fillMissingProductUuidsInsert();
+                $this->fillMissingProductUuids();
                 $count = $this->getMissingProductUuidCount();
             } else {
                 $output->writeln(sprintf('    Option --dry-run is set, will continue to next step.'));
@@ -48,58 +61,71 @@ class MigrateToUuidFillProductUuid implements MigrateToUuidStep
         }
     }
 
-    private function getMissingUuidCount(string $tableName, string $uuidColumnName): int
-    {
-        $sql = sprintf('SELECT COUNT(*) FROM %s WHERE %s IS NULL', $tableName, $uuidColumnName);
-
-        $result = $this->connection->fetchOne($sql);
-
-        return (int) $result;
-    }
-
-    private function getIdCount(string $tableName): int
-    {
-        $sql = sprintf('SELECT COUNT(*) FROM %s', $tableName);
-
-        $result = $this->connection->fetchOne($sql);
-
-        return (int) $result;
-    }
-
     private function getMissingProductUuidCount(): int
     {
-        if (!$this->columnExists('pim_catalog_product', 'uuid')) {
-            return $this->getIdCount('pim_catalog_product');
-        } else {
-            return $this->getMissingUuidCount('pim_catalog_product', 'uuid');
-        }
+        return $this->columnExists('pim_catalog_product', 'uuid') ?
+            $this->getNullUuidCount('uuid') :
+            $this->getProductCount();
     }
 
-    private function fillMissingProductUuidsInsert(): void
+    private function getNullUuidCount(string $uuidColumnName): int
     {
-        // TODO CREATE INDEX toto ON pim_catalog_product (uuid);
+        $sql = <<<SQL
+            SELECT COUNT(*) 
+            FROM pim_catalog_product
+            WHERE %s IS NULL
+        SQL;
 
+        return (int) $this->connection->fetchOne(sprintf($sql, $uuidColumnName));
+    }
+
+    private function getProductCount(): int
+    {
+        $sql = <<<SQL
+            SELECT COUNT(*)
+            FROM pim_catalog_product
+        SQL;
+
+        return (int) $this->connection->fetchOne($sql);
+    }
+
+    private function fillMissingProductUuids(): void
+    {
         $this->connection->beginTransaction();
-        $productIdsWithoutUuids = $this->connection->fetchFirstColumn(sprintf('SELECT id FROM pim_catalog_product WHERE uuid is NULL LIMIT %d', self::BATCH_SIZE));
-        $values = array_map(fn (string $productId): string => sprintf('(%s, UUID_TO_BIN("%s"), 1, CONCAT(md5(rand()), md5(rand())), "{}", NOW(), NOW())', $productId, Uuid::uuid4()->toString()), $productIdsWithoutUuids);
 
-        $sql = sprintf('
-INSERT INTO pim_catalog_product (id, uuid, is_enabled, identifier, raw_values, created, updated)
-VALUES %s ON DUPLICATE KEY UPDATE uuid=VALUES(uuid)', implode(', ', $values));
+        $productIdsWithNullUuids = $this->getProductIdsWithNullUuids();
 
-        $this->connection->executeQuery($sql);
+        /**
+         * We need to insert mysql valid row, even if the ON DUPLICATE KEY updates only uuid.
+         * The double md5(rand()) is here to be sure there will not be any collision on identifier.
+         */
+        $values = array_map(fn (string $productId): string => sprintf(
+            '(%s, UUID_TO_BIN("%s"), 1, CONCAT(md5(rand()), md5(rand())), "{}", NOW(), NOW())',
+            $productId,
+            Uuid::uuid4()->toString()
+        ), $productIdsWithNullUuids);
+
+        $insertSql = <<<SQL
+            INSERT INTO pim_catalog_product (id, uuid, is_enabled, identifier, raw_values, created, updated)
+            VALUES %s
+            ON DUPLICATE KEY UPDATE uuid=VALUES(uuid)
+        SQL;
+
+        $this->connection->executeQuery(sprintf($insertSql, implode(', ', $values)));
         $this->connection->commit();
     }
 
-    private function columnExists(string $tableName, string $columnName): bool
+    private function getProductIdsWithNullUuids(): array
     {
-        $rows = $this->connection->fetchAllAssociative(
-            sprintf('SHOW COLUMNS FROM %s LIKE :columnName', $tableName),
-            [
-                'columnName' => $columnName,
-            ]
-        );
+        $sqlProductIdsWithNullUuids = <<<SQL
+            SELECT id
+            FROM pim_catalog_product 
+            WHERE uuid is NULL
+            LIMIT %d
+        SQL;
 
-        return count($rows) >= 1;
+        return $this->connection->fetchFirstColumn(
+            sprintf($sqlProductIdsWithNullUuids, self::BATCH_SIZE)
+        );
     }
 }
