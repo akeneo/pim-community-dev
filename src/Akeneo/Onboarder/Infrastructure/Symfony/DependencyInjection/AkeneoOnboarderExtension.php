455f0ff6adc6a51df7aebeea97a9a86d406a265e
Valentin Merlet <valentin.merlet@akeneo.com>
2022-02-22 07:41:55 +0100

    OB2-19: Add AkeneoOnboarderExtension dependency injection extension file

---
+++ src/Akeneo/Onboarder/Infrastructure/Symfony/DependencyInjection/AkeneoOnboarderExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Onboarder\Infrastructure\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class AkeneoOnboarderExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('supplier/services.yml');
+    }
+}
