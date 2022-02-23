7a6e88b33fa161779deb645b9c74ff7a87a8a0a0
pierallard <pierallard@gmail.com>
2022-02-23 10:29:15 +0100

    CPM-502: Clean before review (2)

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidStep.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidStep.php
@@ -1,9 +1,13 @@
 <?php
 
-namespace Akeneo\Pim\Enrichment\Bundle\Command;
+namespace Akeneo\Pim\Enrichment\Bundle\Command\MigrateToUuid;
 
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 interface MigrateToUuidStep
 {
     public const TABLES = [
