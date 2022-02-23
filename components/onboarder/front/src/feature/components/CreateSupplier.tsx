fcf1445e0f03edf907acefebe25dc122229c0a25
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-02-23 15:50:55 +0100

    OB-27: add onboarder V2 microfrontend

---
+++ components/onboarder/front/src/feature/components/CreateSupplier.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Button, Modal, useBooleanState} from 'akeneo-design-system';
+import {useTranslate} from '@akeneo-pim-community/shared';
+
+type CreateSupplierProps = {
+    onSupplierCreated: () => void;
+};
+
+const CreateSupplier = ({onSupplierCreated}: CreateSupplierProps) => {
+    const [isOpen, openModal, closeModal] = useBooleanState(false);
+    const translate = useTranslate();
+
+    return (
+        <>
+            <Button onClick={openModal}>Create</Button>
+            {isOpen && (<Modal
+                onClose={closeModal}
+                closeTitle={translate('pim_common.close')}
+            >
+                <Modal.TopLeftButtons>
+                    <Button>Top left button</Button>
+                </Modal.TopLeftButtons>
+                <Modal.TopRightButtons>
+                    <Button>Top right button</Button>
+                </Modal.TopRightButtons>
+            </Modal>)}
+        </>
+    );
+};
+
+export {CreateSupplier};
---
+++ components/onboarder/front/src/feature/components/SupplierList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {SupplierRow} from "../hooks/useSuppliers";
+import {Table} from 'akeneo-design-system';
+
+type SupplierListProps = {
+    suppliers: SupplierRow[];
+};
+
+const SupplierList = ({suppliers}: SupplierListProps) => {
+    return (
+        <Table>
+            <Table.Header>
+                <Table.HeaderCell>
+                    Supplier
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                    Number of contributors
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                    Actions
+                </Table.HeaderCell>
+            </Table.Header>
+            <Table.Body>
+                {suppliers.map((supplier: SupplierRow) => (
+                    <Table.Row key={supplier.code}>
+                        <Table.Cell>
+                            {supplier.label}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {supplier.contributorsCount}
+                        </Table.Cell>
+                        <Table.ActionCell>
+
+                        </Table.ActionCell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table>
+    );
+};
+
+export {SupplierList};
