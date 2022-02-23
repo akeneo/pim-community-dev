e02e88d4455ce8526688bf02c9059a9d075ed144
Valentin Merlet <valentin.merlet@akeneo.com>
2022-02-23 15:18:56 +0100

    OB2-29: Setup Onboarder Serenity Makefile
    - Also add documentation for testing

---
+++ make-file/onboarder-serenity.mk
@@ -0,0 +1,10 @@
+DOCKER_COMPOSE_RUN_PHP = $(DOCKER_COMPOSE) run -u www-data --rm php
+DOCKER_COMPOSE_RUN_PHP_TEST_ENV = $(DOCKER_COMPOSE) run -u www-data --rm -e APP_ENV=test php
+
+.PHONY: onboarder-unit-tests
+onboarder-unit-tests:
+	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Onboarder_Serenity_Unit_Test --configuration tests/back/OnboarderSerenity/phpunit.xml.dist ${IO}
+
+.PHONY: onboarder-integration-tests
+onboarder-integration-tests:
+	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Onboarder_Serenity_Integration_Test --configuration tests/back/OnboarderSerenity/phpunit.xml.dist ${IO}
