9cf9988912dc1f2cdeaf129daae925f2fddba766
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-02-24 17:29:16 +0100

    OB2-27: continue suppliers list & creation

--- components/onboarder/front/src/feature/hooks/useSuppliers.ts
+++ components/onboarder/front/src/feature/hooks/useSuppliers.ts
@@ -1,11 +1,36 @@
+import React, {useEffect, useState} from "react";
+
 type SupplierRow = {
     code: string;
     label: string;
     contributorsCount: number;
 };
 
-const useSuppliers = (search: string, page: number): [SupplierRow[], () => void] => {
-    return [[], () => {}];
+export const SUPPLIERS_PER_PAGE = 50;
+
+const useSuppliers = (search: string, page: number): [SupplierRow[], number, () => void] => {
+    const [suppliers, setSuppliers] = useState<SupplierRow[]>(
+        [...Array(152)].map(
+            (_e, i) => (
+                {code: `supplier-${i}`, label: `Supplier ${i}`, contributorsCount: Math.floor(Math.random() * 10)}
+            )
+        )
+    );
+    const [filteredSuppliers, setFilteredSuppliers] = useState<SupplierRow[]>(suppliers);
+
+    useEffect(() => {
+        console.log(page * SUPPLIERS_PER_PAGE);
+        setFilteredSuppliers(suppliers
+            .filter(supplier => supplier.label.toLocaleLowerCase().includes(search.trim().toLocaleLowerCase()))
+            .slice(page === 1 ? 0 : (page - 1) * SUPPLIERS_PER_PAGE, ((page - 1) * SUPPLIERS_PER_PAGE) + SUPPLIERS_PER_PAGE)
+        );
+    }, [search, page]);
+
+    return [
+        filteredSuppliers,
+        suppliers.length,
+        () => setSuppliers([{code: 'toto', label: "Supplier 1", contributorsCount: 10}])
+    ];
 };
 
 export type {SupplierRow};
