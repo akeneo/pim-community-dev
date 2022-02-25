cbcae3c7f8e2bf85192540667b403013eae77121
Steven VAIDIE <steven.vaidie@gmail.com>
2022-02-25 09:51:09 +0000

    Preparing version 5.0.82

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.81';
+    const VERSION = '5.0.82';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
