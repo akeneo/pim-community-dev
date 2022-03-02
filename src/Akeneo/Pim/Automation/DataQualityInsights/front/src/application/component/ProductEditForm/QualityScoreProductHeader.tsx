366d0027edee004f909192f1b082d79b8e3595ed
Madeline Joubert <madeline.joubert@akeneo.com>
2022-03-02 15:53:44 +0100

    WIP

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/QualityScoreProductHeader.tsx
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/application/component/ProductEditForm/QualityScoreProductHeader.tsx
@@ -3,17 +3,46 @@ import {QualityScoreBar} from '../QualityScoreBar';
 import {QualityScoreLoader} from '../QualityScoreLoader';
 import {DATA_QUALITY_INSIGHTS_REDIRECT_TO_DQI_TAB} from '../../listener';
 import {useCatalogContext, useFetchQualityScore} from '../../../infrastructure/hooks';
+import {Badge} from 'akeneo-design-system';
+import styled from 'styled-components';
+
+const Border = styled.div`
+  border-right: 1px ${({theme}) => theme.color.grey80} solid;
+`;
+
+const Wrapper = styled.div`
+  margin-right: 20px;
+`;
 
 const QualityScoreProductHeader = () => {
   const {channel, locale} = useCatalogContext();
   const {score, productType, isLoading} = useFetchQualityScore(channel, locale);
   const redirectToDqiTab = () => window.dispatchEvent(new CustomEvent(DATA_QUALITY_INSIGHTS_REDIRECT_TO_DQI_TAB));
 
+  let qualityScoreComponent;
+  if (isLoading) {
+    qualityScoreComponent = <QualityScoreLoader />;
+  } else if (score === 'N/A' || score === null) {
+    qualityScoreComponent = <Badge level="tertiary">in progress</Badge>;
+  } else {
+    qualityScoreComponent = (
+      <QualityScoreBar currentScore={score} stacked={productType === 'product_model'} onClick={redirectToDqiTab} />
+    );
+  }
+
+  return (
+    <Wrapper>
+      {qualityScoreComponent}
+      <Border />
+    </Wrapper>
+  );
+
+  /*
   return isLoading ? (
     <QualityScoreLoader />
   ) : (
     <QualityScoreBar currentScore={score} stacked={productType === 'product_model'} onClick={redirectToDqiTab} />
-  );
+  );*/
 };
 
 export {QualityScoreProductHeader};
