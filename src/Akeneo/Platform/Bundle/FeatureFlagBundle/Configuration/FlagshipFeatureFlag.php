59b3d466ec79e3c42e627f5f4524bba09d22bf4c
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-03-03 13:40:13 +0100

    OB2-41: add cache behavior

--- src/Akeneo/Platform/Bundle/FeatureFlagBundle/Configuration/FlagshipFeatureFlag.php
+++ src/Akeneo/Platform/Bundle/FeatureFlagBundle/Configuration/FlagshipFeatureFlag.php
@@ -3,47 +3,66 @@
 namespace Akeneo\Platform\Bundle\FeatureFlagBundle\Configuration;
 
 use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FlagshipFeatureFlag implements FeatureFlag
 {
-    private HttpClientInterface $flagshipClient;
-    private string $flagName;
-    private bool $defaultValue;
-    private string $visitorId;
+    private const CACHE_DURATION_IN_SECONDS = 3600;
 
-    public function __construct(HttpClientInterface $flagshipClient, string $visitorId, string $flagName, bool $defaultValue)
-    {
-        $this->flagshipClient = $flagshipClient;
-        $this->visitorId = $visitorId;
-        $this->flagName = $flagName;
-        $this->defaultValue = $defaultValue;
+    public function __construct(
+        private HttpClientInterface $flagshipClient,
+        private CacheInterface $cache,
+        private string $visitorId,
+        private string $flagName,
+        private bool $defaultValue,
+        private LoggerInterface $logger,
+    ) {
     }
 
     public function isEnabled(): bool
     {
         try {
-            $response = $this->flagshipClient->request('POST', '', [
-                'json' => [
-                    'visitor_id' => $this->visitorId,
-                ],
-            ]);
+            $value = $this->cache->get($this->generateCacheKey(), function (ItemInterface $item) {
+                $item->expiresAfter(self::CACHE_DURATION_IN_SECONDS);
 
-            $responseBody = $response->toArray();
-        } catch (\Exception $e) {
-            //Log errors
-            dump($e->getMessage());
+                return $this->retrieveFlagValue();
+            });
+        } catch (ExceptionInterface $e) {
+            $this->logger->error(sprintf('An error occured while calling Flagship : %s', $e->getMessage()));
+
+            return $this->defaultValue;
         }
 
-        if (!array_key_exists('mergedModifications', $responseBody) || !is_array($responseBody['mergedModifications'])) {
+        return $value;
+    }
+
+    private function retrieveFlagValue(): bool
+    {
+        $response = $this->flagshipClient->request('POST', '', [
+            'json' => [
+                'visitor_id' => $this->visitorId,
+            ],
+        ]);
+
+        $responseBody = $response->toArray();
+
+        if (! array_key_exists('mergedModifications', $responseBody) || ! is_array($responseBody['mergedModifications'])) {
             throw new \Exception('Unexpected response from Flagship : key "mergedModifications" was not found');
         }
 
-        if (array_key_exists($this->flagName, $responseBody['mergedModifications'])) {
-            //The flagvalue can be null or a boolean value
-            return boolval($responseBody['mergedModifications'][$this->flagName]);
+        if (!array_key_exists($this->flagName, $responseBody['mergedModifications']) || $responseBody['mergedModifications'][$this->flagName] === null) {
+            return $this->defaultValue;
         }
 
-        return $this->defaultValue;
+        return $responseBody['mergedModifications'][$this->flagName];
+    }
+
+    private function generateCacheKey(): string
+    {
+        return $this->visitorId . '-' . $this->flagName;
     }
 }
