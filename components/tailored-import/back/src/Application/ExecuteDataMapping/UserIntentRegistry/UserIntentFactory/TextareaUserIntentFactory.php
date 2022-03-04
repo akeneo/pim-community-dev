6578081be65d4ceed3ea006e5883471af744a047
Steven VAIDIE <steven.vaidie@gmail.com>
2022-03-04 11:00:46 +0100

    Fix invalid return type since master have been merged in the feature branch

--- components/tailored-import/back/src/Application/ExecuteDataMapping/UserIntentRegistry/UserIntentFactory/TextareaUserIntentFactory.php
+++ components/tailored-import/back/src/Application/ExecuteDataMapping/UserIntentRegistry/UserIntentFactory/TextareaUserIntentFactory.php
@@ -14,14 +14,14 @@ declare(strict_types=1);
 namespace Akeneo\Platform\TailoredImport\Application\ExecuteDataMapping\UserIntentRegistry\UserIntentFactory;
 
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetTextareaValue;
-use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\UserIntent;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\ValueUserIntent;
 use Akeneo\Platform\TailoredImport\Application\ExecuteDataMapping\UserIntentRegistry\UserIntentFactoryInterface;
 use Akeneo\Platform\TailoredImport\Domain\Model\TargetAttribute;
 use Akeneo\Platform\TailoredImport\Domain\Model\TargetInterface;
 
 class TextareaUserIntentFactory implements UserIntentFactoryInterface
 {
-    public function create(TargetInterface $target, string $value): UserIntent
+    public function create(TargetInterface $target, string $value): ValueUserIntent
     {
         if (!$target instanceof TargetAttribute) {
             throw new \InvalidArgumentException('The target must be a TargetAttribute');
