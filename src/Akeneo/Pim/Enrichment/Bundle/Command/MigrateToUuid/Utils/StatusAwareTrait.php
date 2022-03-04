c03f34c50a4238151180f346fcc64659a3c690cf
Adrien Migaire <adrien.migaire@akeneo.com>
2022-03-04 10:41:32 +0100

    CPM-542 Refacto to ease log context use

---
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/Utils/StatusAwareTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Bundle\Command\MigrateToUuid\Utils;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+trait StatusAwareTrait
+{
+    protected string $status = 'none';
+    private ?float $startTime = null;
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatusInProgress(): void
+    {
+        $this->startTime = microtime(true);
+        $this->status = 'in_progress';
+    }
+
+    public function setStatusDone(): void
+    {
+        $this->status = 'done';
+    }
+
+    public function setStatusInError(): void
+    {
+        $this->status = 'in_error';
+    }
+
+    public function setStatusCountMissing(): void
+    {
+        $this->status = 'count_missing';
+    }
+
+    public function setStatusSkipped(): void
+    {
+        $this->status = 'skipped';
+    }
+
+    public function getDuration(): ?float
+    {
+        if ($this->startTime === null) {
+            return null;
+        }
+        return microtime(true) - $this->startTime;
+    }
+}
