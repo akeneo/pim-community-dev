4cbadec43fb3f90227ef67439b0768e6c2aafabe
ValentinMerlet <merlet.valentin@gmail.com>
2022-03-07 14:19:23 +0100

    OB2-48: Add some PHP-CS-Fixer rules:
    - cast_spaces, class_reference_name_casing,
    no_blank_lines_after_class_opening, no_whitespace_in_blank_line,
    return_type_declaration and single_quote

--- components/onboarder/back/tests/.php_cs.php
+++ components/onboarder/back/tests/.php_cs.php
@@ -11,6 +11,16 @@ return (new PhpCsFixer\Config())
                 'always_move_variable' => true, // Whether variables should always be on non assignable side when applying Yoda style.
             ],
             'protected_to_private' => true,
+            'cast_spaces' => true,
+            'class_reference_name_casing' => true,
+            'no_blank_lines_after_class_opening' => true,
+            'no_whitespace_in_blank_line' => true,
+            'return_type_declaration' => [
+                'space_before' => 'none',
+            ],
+            'single_quote' => [
+                'strings_containing_single_quote_chars' => true,
+            ]
         ]
     )
     ->setCacheFile('var/php_cs.cache')
