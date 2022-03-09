aaec5837b73b66c3bcb2e73d80af4e0c2816336a
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-03-09 15:24:43 +0100

    PIM-10309: Backport Removing a locale from a channel does not affect the table attribute (#13115)

--- grth/src/Akeneo/Pim/TableAttribute/front/src/attribute/TableStructureApp.tsx
+++ grth/src/Akeneo/Pim/TableAttribute/front/src/attribute/TableStructureApp.tsx
@@ -82,6 +82,7 @@ const TableStructureApp: React.FC<TableStructureAppProps> = ({
   const [savedColumnIds, setSavedColumnIds] = React.useState<string[]>([]);
 
   React.useEffect(() => {
+    LocaleRepository.clearCache();
     LocaleRepository.findActivated(router).then((activeLocales: Locale[]) => setActiveLocales(activeLocales));
   }, [router]);
 
