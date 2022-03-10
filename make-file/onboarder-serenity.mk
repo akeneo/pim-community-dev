0bc5aa328095b54c42b1e5b88cc6016cb5c75f11
ValentinMerlet <merlet.valentin@gmail.com>
2022-03-10 14:23:21 +0100

    OBS-51: Change owner yarn cache directory and create output directory for Behat acceptance

--- make-file/onboarder-serenity.mk
+++ make-file/onboarder-serenity.mk
@@ -2,19 +2,19 @@ DOCKER_COMPOSE_RUN_PHP_TEST_ENV = $(DOCKER_COMPOSE) run -u www-data --rm -e APP_
 DOCKER_COMPOSE_RUN_PHP_TEST_FAKE_ENV = $(DOCKER_COMPOSE) run -u www-data --rm -e APP_ENV=test_fake php
 
 .PHONY: unit-back
-unit-back: #Doc: Run unit back tests for Onboarder
+unit-back: #Doc: Run unit back tests for Onboarder Serenity
 	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Onboarder_Serenity_Unit_Test --configuration components/onboarder/back/tests/phpunit.xml.dist ${ARGS}
 
 .PHONY: integration
-integration: #Doc: Run integration tests for Onboarder
+integration: #Doc: Run integration tests for Onboarder Serenity
 	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Onboarder_Serenity_Integration_Test --configuration components/onboarder/back/tests/phpunit.xml.dist ${ARGS}
 
 .PHONY: coupling
-coupling: #Doc: Run coupling detector for Onboarder
+coupling: #Doc: Run coupling detector for Onboarder Serenity
 	$(PHP_RUN) vendor/bin/php-coupling-detector detect --config-file=components/onboarder/back/tests/.php_cd.php components/onboarder/back
 
 .PHONY: coupling-list-unused-requirements
-coupling-list-unused-requirements: #Doc: List unused coupling detector requirements
+coupling-list-unused-requirements: #Doc: List unused coupling detector requirements for Onboarder Serenity
 	$(PHP_RUN) vendor/bin/php-coupling-detector list-unused-requirements --config-file=components/onboarder/back/tests/.php_cd.php components/onboarder/back
 
 .PHONY: lint-back
@@ -27,9 +27,9 @@ fix-phpcs: #Doc: Run PHP-CS-Fixer for Onboarder Serenity
 	${PHP_RUN} vendor/bin/php-cs-fixer fix --diff --config=components/onboarder/back/tests/.php_cs.php components/onboarder/back
 
 .PHONY: unit-front
-unit-front: #Doc: Run unit front tests for Onboarder
+unit-front: #Doc: Run unit front tests for Onboarder Serenity
 	$(YARN_RUN) run --cwd=components/onboarder/front test:unit:run
 
 .PHONY: acceptance-back
-acceptance-back: var/tests/behat/onboarder-serenity-acceptance #Doc: launch Behat acceptance tests for Onboarder Serenity
+acceptance-back: var/tests/behat/onboarder-serenity-acceptance #Doc: Run Behat acceptance tests for Onboarder Serenity
 	$(DOCKER_COMPOSE_RUN_PHP_TEST_FAKE_ENV) vendor/bin/behat --config components/onboarder/back/tests/behat.yml --profile acceptance --format pim --out var/tests/behat/onboarder-serenity-acceptance --format progress --out std --colors $(O)
