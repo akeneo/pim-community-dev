57ec6e248d8d2968bd5f9e25952319e0b27e6045
Adrien Migaire <adrien.migaire@akeneo.com>
2022-03-15 16:08:33 +0100

    CPM-547 set real uuid v4 instead of using v1 mysql uuid()

--- upgrades/test_schema/Version_6_0_20210527144217_dqi_init_recompute_products_scores_Integration.php
+++ upgrades/test_schema/Version_6_0_20210527144217_dqi_init_recompute_products_scores_Integration.php
@@ -30,8 +30,8 @@ final class Version_6_0_20210527144217_dqi_init_recompute_products_scores_Integr
     {
         $insertProducts = <<<SQL
 INSERT INTO pim_catalog_product (id, uuid, family_id, product_model_id, family_variant_id, is_enabled, identifier, raw_values, created, updated) VALUES
-    (1, UUID_TO_BIN(UUID()), NULL, NULL, NULL, 1, 'product1', '{"sku": {"<all_channels>": {"<all_locales>": "product1"}}}', '2021-05-31 10:42:00', '2021-05-31 10:42:00'),
-    (2, UUID_TO_BIN(UUID()), NULL, NULL, NULL, 1, 'product2', '{"sku": {"<all_channels>": {"<all_locales>": "product2"}}}', '2021-05-31 10:42:01', '2021-05-31 10:42:01')
+    (1, UUID_TO_BIN('99c8c1aa-fb62-400c-acbd-d686a25989f4'), NULL, NULL, NULL, 1, 'product1', '{"sku": {"<all_channels>": {"<all_locales>": "product1"}}}', '2021-05-31 10:42:00', '2021-05-31 10:42:00'),
+    (2, UUID_TO_BIN('4ef70193-3d39-4122-8229-a42fa5b036b6'), NULL, NULL, NULL, 1, 'product2', '{"sku": {"<all_channels>": {"<all_locales>": "product2"}}}', '2021-05-31 10:42:01', '2021-05-31 10:42:01')
 SQL;
 
         $this->get('database_connection')->executeQuery($insertProducts);
