30aac6bdc2d76daba9363201aef24a4f4d37121c
Ronan Fauglas <ronan.fauglas@akeneo.com>
2022-03-16 16:57:57 +0000

    Preparing version 6.0.11

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.10';
+    public const VERSION = '6.0.11';
 
     /** @staticvar string */
     public const VERSION_CODENAME = 'Buccaneer Bunny';
