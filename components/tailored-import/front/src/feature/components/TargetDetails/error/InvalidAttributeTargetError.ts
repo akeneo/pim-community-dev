2b64ee1115d4632249728ab557cf7babc491646a
Adrien Pétremann <hello@grena.fr>
2022-03-18 14:20:46 +0100

    Remove front files

--- components/tailored-import/front/src/feature/components/TargetDetails/error/InvalidAttributeTargetError.ts
+++
@@ -1,9 +0,0 @@
-class InvalidAttributeTargetError extends Error {
-  constructor(msg: string) {
-    super(msg);
-
-    Object.setPrototypeOf(this, InvalidAttributeTargetError.prototype);
-  }
-}
-
-export {InvalidAttributeTargetError};
