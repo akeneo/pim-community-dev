5e0a802f41a5b4f09ccccd4cd9b5a77271928bd8
Steven VAIDIE <steven.vaidie@gmail.com>
2022-03-21 12:17:38 +0100

    PIM-10345: Add some tests

--- tests/back/Pim/Enrichment/Specification/Component/Product/Connector/ArrayConverter/FlatToStandard/ProductModelSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Product/Connector/ArrayConverter/FlatToStandard/ProductModelSpec.php
@@ -70,7 +70,10 @@ class ProductModelSpec extends ObjectBehavior
             'categories' => 'tshirt,pull',
             'name-en_US' => 'name',
             'description-en_US-ecommerce' => 'description',
-            123456 => "numerical code name field"
+            123456 => 'numerical as field code',
+            '0123456' => 'numerical with trailing zero as field code',
+            '12.33' => 'string float as field code',
+            '1234567' => 'numerical as field code',
         ];
 
         $columnsMapper->map($flatProductModel, [
@@ -80,7 +83,7 @@ class ProductModelSpec extends ObjectBehavior
         $columnsMerger->merge($flatProductModel)->willReturn($flatProductModel);
 
         $fieldsRequirementChecker->checkFieldsPresence($flatProductModel, ['code'])->shouldBeCalled();
-        $attributeColumnsResolver->resolveAttributeColumns()->willReturn(['name-en_US', 'description-en_US-ecommerce', '123456']);
+        $attributeColumnsResolver->resolveAttributeColumns()->willReturn(['name-en_US', 'description-en_US-ecommerce', '123456', '0123456', '12.33', '1234567']);
 
         $fieldConverter->supportsColumn('code')->willreturn(true);
         $fieldConverter->convert('code', 'code')->willreturn($identifierConverter);
@@ -121,34 +124,64 @@ class ProductModelSpec extends ObjectBehavior
         ]);
 
         $fieldConverter->supportsColumn('123456')->willreturn(false);
+        $fieldConverter->supportsColumn('0123456')->willreturn(false);
+        $fieldConverter->supportsColumn('12.33')->willreturn(false);
+        $fieldConverter->supportsColumn('1234567')->willreturn(false);
+
         $fieldConverter->supportsColumn('name-en_US')->willreturn(false);
         $fieldConverter->supportsColumn('description-en_US-ecommerce')->willreturn(false);
 
-        $productValueConverter->convert(["name-en_US" => "name", "description-en_US-ecommerce" => "description", 123456 => "numerical code name field"])
-            ->willReturn([
-                    'name' => [
-                        [
-                            'locale' => 'en_US',
-                            'scope' => null,
-                            'data' => 'name',
-                        ],
-                    ],
-                    'description' => [
-                        [
-                            'locale' => 'en_US',
-                            'scope' => 'ecommerce',
-                            'data' => 'description',
-                        ],
-                    ],
-                    123456 => [
-                        [
-                            'locale' => null,
-                            'scope' => null,
-                            'data' => 'numerical code name field'
-                        ]
-                    ]
-                ]
-            );
+        $productValueConverter->convert([
+            "name-en_US" => "name",
+            "description-en_US-ecommerce" => "description",
+            123456 => 'numerical as field code',
+            '0123456' => 'numerical with trailing zero as field code',
+            '12.33' => 'string float as field code',
+            '1234567' => 'numerical as field code',
+        ])->willReturn([
+            'name' => [
+                [
+                    'locale' => 'en_US',
+                    'scope' => null,
+                    'data' => 'name',
+                ],
+            ],
+            'description' => [
+                [
+                    'locale' => 'en_US',
+                    'scope' => 'ecommerce',
+                    'data' => 'description',
+                ],
+            ],
+            123456 => [
+                [
+                    'locale' => null,
+                    'scope' => null,
+                    'data' => 'numerical as field code',
+                ],
+            ],
+            '0123456' => [
+                [
+                    'locale' => null,
+                    'scope' => null,
+                    'data' => 'numerical with trailing zero as field code',
+                ],
+            ],
+            '12.33' => [
+                [
+                    'locale' => null,
+                    'scope' => null,
+                    'data' => 'string float as field code',
+                ],
+            ],
+            '1234567' => [
+                [
+                    'locale' => null,
+                    'scope' => null,
+                    'data' => 'numerical as field code',
+                ],
+            ],
+        ]);
 
         $this->convert($flatProductModel, [
             'mapping' => [
@@ -182,9 +215,30 @@ class ProductModelSpec extends ObjectBehavior
                     [
                         'locale' => null,
                         'scope' => null,
-                        'data' => 'numerical code name field'
-                    ]
-                ]
+                        'data' => 'numerical as field code',
+                    ],
+                ],
+                '0123456' => [
+                    [
+                        'locale' => null,
+                        'scope' => null,
+                        'data' => 'numerical with trailing zero as field code',
+                    ],
+                ],
+                '12.33' => [
+                    [
+                        'locale' => null,
+                        'scope' => null,
+                        'data' => 'string float as field code',
+                    ],
+                ],
+                '1234567' => [
+                    [
+                        'locale' => null,
+                        'scope' => null,
+                        'data' => 'numerical as field code',
+                    ],
+                ],
             ],
         ]);
     }
