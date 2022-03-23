2a723561d7294b099f210233271436b838a07dfc
pierallard <pierallard@gmail.com>
2022-03-23 14:01:02 +0100

    CPM-574: Add constraints

--- src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidTrait.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidTrait.php
@@ -37,21 +37,6 @@ trait MigrateToUuidTrait
         return count($rows) >= 1;
     }
 
-    protected function indexExists(string $tableName, string $indexName): bool
-    {
-        $rows = $this->connection->executeQuery(
-            \strtr(
-                'SHOW INDEX FROM {table_name} WHERE KEY_NAME = :index',
-                ['{table_name}' => $tableName]
-            ),
-            [
-                'index' => $indexName,
-            ]
-        )->fetchAllAssociative();
-
-        return count($rows) > 0;
-    }
-
     protected function triggerExists(string $triggerName): bool
     {
         $schema = $this->connection->getDatabase();
@@ -82,6 +67,42 @@ trait MigrateToUuidTrait
                 'schema' => $this->connection->getDatabase(),
                 'table_name' => $tableName,
                 'constraint_name' => $constraintName,
+            ]
+        )->fetchOne();
+    }
+
+    protected function getIndexName(string $tableName, string $columnName): ?string
+    {
+        $sql = <<<SQL
+            SELECT INDEX_NAME FROM information_schema.STATISTICS
+            WHERE INDEX_SCHEMA = :schema AND TABLE_NAME = :table_name AND COLUMN_NAME = :column_name
+            SQL;
+
+        return $this->connection->executeQuery(
+            $sql,
+            [
+                'schema' => $this->connection->getDatabase(),
+                'table_name' => $tableName,
+                'column_name' => $columnName,
+            ]
+        )->fetchOne();
+    }
+
+    protected function indexExists(string $tableName, string $indexName): bool
+    {
+        $sql = <<<SQL
+            SELECT EXISTS (
+                SELECT INDEX_NAME FROM information_schema.STATISTICS
+                WHERE INDEX_SCHEMA = :schema AND TABLE_NAME = :table_name AND INDEX_NAME = :index_name
+            ) as is_existing
+        SQL;
+
+        return (bool) $this->connection->executeQuery(
+            $sql,
+            [
+                'schema' => $this->connection->getDatabase(),
+                'table_name' => $tableName,
+                'index_name' => $indexName,
             ]
         )->fetchOne();
     }
