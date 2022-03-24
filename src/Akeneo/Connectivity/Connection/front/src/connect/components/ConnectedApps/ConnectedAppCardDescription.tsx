0f8e49a6d3a4f3723ba9272a0abd4813009f3ef6
Tseho <quentin.favrie@gmail.com>
2022-03-24 20:42:44 +0100

    CXP-1138: display warning when the connected app is not listed on the app store

---
+++ src/Akeneo/Connectivity/Connection/front/src/connect/components/ConnectedApps/ConnectedAppCardDescription.tsx
@@ -0,0 +1,112 @@
+import React, {FC} from 'react';
+import {ConnectedApp} from '../../../model/Apps/connected-app';
+import {useTranslate} from '../../../shared/translate';
+import styled from 'styled-components';
+import {DangerIcon, getColor, getFontSize} from 'akeneo-design-system';
+
+const Warning = styled.div`
+    color: ${getColor('grey', 120)};
+    font-size: ${getFontSize('small')};
+    font-weight: normal;
+    margin: 0;
+    margin-bottom: 5px;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis; ;
+`;
+
+const WarningIcon = styled(DangerIcon)`
+    color: ${getColor('yellow', 100)};
+    vertical-align: middle;
+    margin-right: 5px;
+`;
+
+const Error = styled.div`
+    color: ${getColor('red', 100)};
+    font-size: ${getFontSize('small')};
+    font-weight: normal;
+    margin: 0;
+    margin-bottom: 5px;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis; ;
+`;
+
+const ErrorIcon = styled(DangerIcon)`
+    color: ${getColor('red', 100)};
+    vertical-align: middle;
+    margin-right: 5px;
+`;
+
+const Tag = styled.span`
+    color: ${getColor('grey', 120)};
+    font-size: ${getFontSize('small')};
+    text-transform: uppercase;
+    font-weight: normal;
+
+    border: 1px ${getColor('grey', 120)} solid;
+    background: ${getColor('white')};
+    border-radius: 2px;
+
+    display: inline-block;
+    line-height: ${getFontSize('small')};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    padding: 2px 5px;
+    margin-right: 5px;
+`;
+
+const Author = styled.div`
+    color: ${getColor('grey', 120)};
+    font-size: ${getFontSize('big')};
+    font-weight: normal;
+    margin: 0;
+    margin-bottom: 5px;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
+
+type Props = {
+    connectedApp: ConnectedApp;
+};
+
+const ConnectedAppCardDescription: FC<Props> = ({connectedApp}) => {
+    const translate = useTranslate();
+    const author =
+        connectedApp.author ??
+        translate('akeneo_connectivity.connection.connect.connected_apps.list.test_apps.removed_user');
+
+    if (true !== connectedApp.is_loaded) {
+        return null;
+    }
+
+    if (false === connectedApp.is_listed_on_the_appstore) {
+        return (
+            <Error>
+                <ErrorIcon size={14} />
+                {translate(
+                    'akeneo_connectivity.connection.connect.connected_apps.list.card.not_listed_on_the_appstore'
+                )}
+            </Error>
+        );
+    }
+
+    return (
+        <>
+            <Author>
+                {translate('akeneo_connectivity.connection.connect.connected_apps.list.card.developed_by', {
+                    author,
+                })}
+            </Author>
+            {connectedApp.categories.length > 0 && <Tag>{connectedApp.categories[0]}</Tag>}
+        </>
+    );
+};
+
+export default ConnectedAppCardDescription;
