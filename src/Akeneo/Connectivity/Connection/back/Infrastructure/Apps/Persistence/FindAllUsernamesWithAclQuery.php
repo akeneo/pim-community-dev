fbed97bb192269a6230f91a37cf0f43195ec036b
Riff <riff.ventura@gmail.com>
2022-03-25 11:08:22 +0100

    CXP-1129: Find all usernames with acl query adjusted

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Persistence/FindAllUsernamesWithAclQuery.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Persistence/FindAllUsernamesWithAclQuery.php
@@ -30,6 +30,22 @@ class FindAllUsernamesWithAclQuery implements FindAllUsernamesWithAclQueryInterf
         JOIN oro_user_access_role on oro_access_role.id = oro_user_access_role.role_id
         JOIN oro_user on oro_user_access_role.user_id = oro_user.id
         WHERE acl_classes.class_type = :acl
+        AND acl_entries.mask = 1
+
+        UNION DISTINCT
+
+        SELECT oro_user.username
+        FROM acl_entries
+        JOIN acl_security_identities ON acl_security_identities.id = acl_entries.security_identity_id
+        JOIN acl_classes ON acl_entries.class_id = acl_classes.id
+        LEFT JOIN acl_object_identities on acl_object_identities.id = acl_entries.object_identity_id
+        JOIN oro_access_role ON oro_access_role.role = acl_security_identities.identifier
+        JOIN oro_user_access_role on oro_access_role.id = oro_user_access_role.role_id
+        JOIN oro_user on oro_user_access_role.user_id = oro_user.id
+        WHERE (acl_classes.class_type = "(root)" AND acl_object_identities.object_identifier = "action")
+        OR acl_classes.class_type = :acl
+        GROUP BY oro_user.username
+        HAVING COUNT(acl_classes.class_type) = 1
         SQL;
 
         $results = $this->connection->executeQuery($selectSQL, ['acl' => $acl])->fetchFirstColumn();
