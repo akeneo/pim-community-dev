4d1e2b01c824860b16803ae7a55cef199fe809f3
ahocquard <alexandre.hocquard@akeneo.com>
2022-03-25 23:10:56 +0100

    TIP-1556: add e2e test for activation of the assets

--- src/Akeneo/AssetManager/tests/back/Integration/Connector/Api/Context/ActivateAssetManagerBeforeScenario.php
+++ src/Akeneo/AssetManager/tests/back/Integration/Connector/Api/Context/ActivateAssetManagerBeforeScenario.php
@@ -13,41 +13,9 @@ declare(strict_types=1);
 
 namespace Akeneo\AssetManager\Integration\Connector\Api\Context;
 
-use Akeneo\AssetManager\Common\Fake\Connector\InMemoryFindConnectorAssetByAssetFamilyAndCode;
-use Akeneo\AssetManager\Common\Fake\InMemoryMediaFileRepository;
-use Akeneo\AssetManager\Common\Helper\OauthAuthenticatedClientFactory;
-use Akeneo\AssetManager\Common\Helper\WebClientHelper;
-use Akeneo\AssetManager\Domain\Filesystem\Storage;
-use Akeneo\AssetManager\Domain\Model\Asset\AssetCode;
-use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamily;
-use Akeneo\AssetManager\Domain\Model\AssetFamily\AssetFamilyIdentifier;
-use Akeneo\AssetManager\Domain\Model\AssetFamily\RuleTemplateCollection;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeAllowedExtensions;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeCode;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeIdentifier;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeIsReadOnly;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeIsRequired;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeMaxFileSize;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeMaxLength;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeOrder;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeRegularExpression;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeValidationRule;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeValuePerChannel;
-use Akeneo\AssetManager\Domain\Model\Attribute\AttributeValuePerLocale;
-use Akeneo\AssetManager\Domain\Model\Attribute\MediaFile\MediaType;
-use Akeneo\AssetManager\Domain\Model\Attribute\MediaFileAttribute;
-use Akeneo\AssetManager\Domain\Model\Attribute\TextAttribute;
-use Akeneo\AssetManager\Domain\Model\Image;
-use Akeneo\AssetManager\Domain\Model\LabelCollection;
-use Akeneo\AssetManager\Domain\Query\Asset\Connector\ConnectorAsset;
-use Akeneo\AssetManager\Domain\Repository\AssetFamilyRepositoryInterface;
-use Akeneo\AssetManager\Domain\Repository\AttributeRepositoryInterface;
+
 use Akeneo\Platform\Bundle\FeatureFlagBundle\Internal\InMemoryFeatureFlags;
-use Akeneo\ReferenceEntity\Common\Fake\InMemoryFilesystemProviderStub;
-use Akeneo\Tool\Component\FileStorage\Model\FileInfo;
-use AkeneoEnterprise\Test\Acceptance\Permission\InMemory\SecurityFacadeStub;
 use Behat\Behat\Context\Context;
-use Symfony\Component\HttpFoundation\Response;
 
 class ActivateAssetManagerBeforeScenario implements Context
 {
