f7d20b85279bcfa9067be62222dee2522787a180
Pierre Maraître <pierre.maraitre@akeneo.com>
2022-03-31 17:58:09 +0200

    CPM-558: Change create product method in tests to use service API

--- src/Akeneo/Pim/Enrichment/Product/back/Test/Integration/EnrichmentProductTestCase.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Test/Integration/EnrichmentProductTestCase.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Akeneo\Test\Pim\Enrichment\Product\Integration;
 
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductModelInterface;
+use Akeneo\Pim\Enrichment\Product\API\Command\UpsertProductCommand;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetCategories;
 use Akeneo\Pim\Structure\Component\AttributeTypes;
 use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
 use Akeneo\Pim\Structure\Component\Model\FamilyInterface;
@@ -14,9 +16,22 @@ use Akeneo\Test\Integration\TestCase;
 use Akeneo\Test\Pim\Enrichment\Product\Helper\FeatureHelper;
 use Akeneo\UserManagement\Component\Model\UserInterface;
 use PHPUnit\Framework\Assert;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 abstract class EnrichmentProductTestCase extends TestCase
 {
+    private MessageBusInterface $messageBus;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->messageBus = $this->get('pim_enrich.product.message_bus');
+    }
+
     protected function getConfiguration(): Configuration
     {
         return $this->catalog->useMinimalCatalog();
@@ -72,13 +87,16 @@ abstract class EnrichmentProductTestCase extends TestCase
         ]);
     }
 
-    protected function createProduct(string $identifier, array $data): void
+    protected function createProduct(string $identifier, array $userIntents): void
     {
-        $product = $this->get('pim_catalog.builder.product')->createProduct($identifier);
-        $this->get('pim_catalog.updater.product')->update($product, $data);
-        $violations = $this->get('pim_catalog.validator.product')->validate($product);
-        Assert::assertSame(0, $violations->count(), (string) $violations);
-        $this->get('pim_catalog.saver.product')->save($product);
+        $command = UpsertProductCommand::createFromCollection(
+            userId: $this->getUserId('peter'),
+            productIdentifier: $identifier,
+            userIntents: $userIntents
+        );
+        $this->messageBus->dispatch($command);
+        $this->getContainer()->get('pim_catalog.validator.unique_value_set')->reset();
+        $this->clearDoctrineUoW();
     }
 
     protected function createProductModel(string $code, string $familyVariantCode, array $data): ProductModelInterface
