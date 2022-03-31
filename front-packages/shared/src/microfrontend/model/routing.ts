d9199671a45b06bd4bcdd09ec6af3a4fa5616409
Steven VAIDIE <steven.vaidie@gmail.com>
2022-03-31 12:01:44 +0200

    RAB-584: Handle array object in fake generateRoute

---
+++ front-packages/shared/src/microfrontend/model/routing.ts
@@ -0,0 +1,17 @@
+const createQueryParam = (parameters?: object) => {
+  if (!parameters) return '';
+
+  const queryParameters =
+    Object.entries(parameters)
+      .map(([key, val]) => {
+        if (Array.isArray(val)) {
+          return val.map((value) => `${key}[]=${decodeURIComponent(value)}`).join('&')
+        }
+
+        return `${key}=${decodeURIComponent(val)}`
+      });
+
+  return queryParameters.length > 0 ? '?' + queryParameters.join('&'): '';
+}
+
+export {createQueryParam}
