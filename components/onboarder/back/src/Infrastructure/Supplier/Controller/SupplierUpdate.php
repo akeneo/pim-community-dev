316cfa1e46b49ffbde7ea53dd1a31759ac358743
Philippe Mossiere <philippe.mossiere@akeneo.com>
2022-03-31 12:34:51 +0200

    OBS-34: Add route and controller services and handle responses in controller

--- components/onboarder/back/src/Infrastructure/Supplier/Controller/SupplierUpdate.php
+++ components/onboarder/back/src/Infrastructure/Supplier/Controller/SupplierUpdate.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Akeneo\OnboarderSerenity\Infrastructure\Supplier\Controller;
 
+use Akeneo\OnboarderSerenity\Application\Supplier\Exceptions\InvalidDataException;
 use Akeneo\OnboarderSerenity\Application\Supplier\UpdateSupplier;
 use Akeneo\OnboarderSerenity\Application\Supplier\UpdateSupplierHandler;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class SupplierUpdate
 {
@@ -17,9 +19,20 @@ final class SupplierUpdate
 
     public function __invoke(Request $request, string $identifier): JsonResponse
     {
-        $contributorEmails = $request->request->get('contributorEmails');
-        $supplierLabel = $request->request->get('label');
+        $requestContent = json_decode($request->getContent(), true);
 
-        ($this->updateSupplierHandler)(new UpdateSupplier($identifier, $supplierLabel, $contributorEmails));
+        if (!isset($requestContent['label']) && !isset($requestContent['contributorEmails'])){
+            return new JsonResponse(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            ($this->updateSupplierHandler)(
+                new UpdateSupplier($identifier, $requestContent['label'], $requestContent['contributorEmails'])
+            );
+        } catch (InvalidDataException $e) {
+            return new JsonResponse(null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
