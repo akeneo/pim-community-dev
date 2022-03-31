48988907076648950d9d9d6f9418b9576e6df8e3
Anne-Laure Jouhanneau <75078262+niarcotyk@users.noreply.github.com>
2022-03-31 14:37:18 +0200

    Update src/Akeneo/Platform/Bundle/FrameworkBundle/Logging/ContextLogProcessor.php
    Co-authored-by: jean-marie leroux <jmleroux.pro@gmail.com>

--- src/Akeneo/Platform/Bundle/FrameworkBundle/Logging/ContextLogProcessor.php
+++ src/Akeneo/Platform/Bundle/FrameworkBundle/Logging/ContextLogProcessor.php
@@ -57,7 +57,7 @@ class ContextLogProcessor
         $this->cachedContext['akeneo_context']= $this->boundedContextResolver->fromCommand($cmd)?: "Unknown context";
     }
 
-    public function insertContext(string $key, string $value)
+    public function insertContext(string $key, string $value): void
     {
         $this->cachedContext[$key] = $value;
     }
