7bd453726bd2796a0ba505ee04be6984de78e649
Steven VAIDIE <steven.vaidie@gmail.com>
2022-04-01 15:56:13 +0200

    RAB-584: Fix unit test

--- components/tailored-import/front/src/feature/components/DataMappingDetails/Operations.test.tsx
+++ components/tailored-import/front/src/feature/components/DataMappingDetails/Operations.test.tsx
@@ -38,7 +38,7 @@ test('it display preview if sample data is provided', () => {
   expect(screen.queryByText('product_3')).toBeInTheDocument();
 });
 
-test('it call refresh sample data handler when user click on refresh', () => {
+test('it call refresh sample data handler when user refresh a not empty data', () => {
   const handleRefreshSampleData = jest.fn();
   const dataMapping = {
     uuid: '1ff0d8da-2438-4f2a-ae29-2ea513100924',
@@ -56,7 +56,25 @@ test('it call refresh sample data handler when user click on refresh', () => {
   expect(handleRefreshSampleData).toBeCalledWith(0);
 });
 
-test('it cannot refresh sample data handler while sample data is reloading', () => {
+test('it call refresh sample data handler when user refresh an empty data', () => {
+  const handleRefreshSampleData = jest.fn();
+  const dataMapping = {
+    uuid: '1ff0d8da-2438-4f2a-ae29-2ea513100924',
+    target: {} as AttributeTarget,
+    sources: ['8cb22256-3bc7-494e-9b24-33a1c52fe758'],
+    operations: [],
+    sample_data: ['product_1', 'product_2', null],
+  } as DataMapping;
+
+  renderWithProviders(
+    <Operations dataMapping={dataMapping} loadingSampleData={[]} onRefreshSampleData={handleRefreshSampleData} />
+  );
+
+  userEvent.click(screen.getAllByTitle('akeneo.tailored_import.data_mapping.preview.refresh')[2]);
+  expect(handleRefreshSampleData).toBeCalledWith(2);
+});
+
+test('it cannot refresh sample data while non empty data is reloading', () => {
   const handleRefreshSampleData = jest.fn();
   const dataMapping = {
     uuid: '1ff0d8da-2438-4f2a-ae29-2ea513100924',
@@ -71,5 +89,23 @@ test('it cannot refresh sample data handler while sample data is reloading', () 
   );
 
   userEvent.click(screen.getAllByTitle('akeneo.tailored_import.data_mapping.preview.refresh')[0]);
+  expect(handleRefreshSampleData).not.toBeCalled();
+});
+
+test('it cannot refresh sample data while empty data is reloading', () => {
+  const handleRefreshSampleData = jest.fn();
+  const dataMapping = {
+    uuid: '1ff0d8da-2438-4f2a-ae29-2ea513100924',
+    target: {} as AttributeTarget,
+    sources: ['8cb22256-3bc7-494e-9b24-33a1c52fe758'],
+    operations: [],
+    sample_data: ['product_1', 'product_2', null],
+  } as DataMapping;
+
+  renderWithProviders(
+    <Operations dataMapping={dataMapping} loadingSampleData={[2]} onRefreshSampleData={handleRefreshSampleData} />
+  );
+
+  userEvent.click(screen.getAllByTitle('akeneo.tailored_import.data_mapping.preview.refresh')[2]);
   expect(handleRefreshSampleData).not.toBeCalled();
 });
