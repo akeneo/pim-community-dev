ec53b49f576b51e482882acdcd9f87b40e83f032
Pierre Maraître <pierre.maraitre@akeneo.com>
2022-04-01 17:22:07 +0200

    CPM-558: Add query to fetch non viewable products in list

---
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Query/GetNonViewableProducts.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Product\Infrastructure\Query;
+
+use Akeneo\Pim\Enrichment\Product\Domain\Query\GetNonViewableProducts as GetNonViewableProductsInterface;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GetNonViewableProducts implements GetNonViewableProductsInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function fromProductIdentifiers(array $productIdentifiers, int $userId): array
+    {
+        return [];
+    }
+}
