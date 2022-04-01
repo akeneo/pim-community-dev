07ffe6419e6b8344f97ce1ab7ca79e84116b60c6
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-04-01 11:23:46 +0200

    OBS-34: fix useFilteredContributors unit test

--- components/onboarder/front/src/feature/hooks/useFilteredContributors.test.ts
+++ components/onboarder/front/src/feature/hooks/useFilteredContributors.test.ts
@@ -1,11 +1,11 @@
 import {renderHook, act} from '@testing-library/react-hooks';
 import {useFilteredContributors} from './useFilteredContributors';
 
-const contributors = {
-    1: 'contributor1@example.com',
-    2: 'contributor2@example.com',
-    3: 'another@example.com',
-};
+const contributors = [
+    'contributor1@example.com',
+    'contributor2@example.com',
+    'another@example.com',
+];
 
 test('it returns all contributors by default', () => {
     const {result} = renderHook(() => useFilteredContributors(contributors));
@@ -15,14 +15,14 @@ test('it returns all contributors by default', () => {
 test('we can search contributors by email', async () => {
     const {result} = renderHook(() => useFilteredContributors(contributors));
     await act(async () => result.current.search('contributor'));
-    expect(result.current.filteredContributors).toEqual({
-        1: 'contributor1@example.com',
-        2: 'contributor2@example.com',
-    });
+    expect(result.current.filteredContributors).toEqual([
+       'contributor1@example.com',
+       'contributor2@example.com',
+    ]);
 });
 
 test('it returns no result if the search does not match any email', async () => {
     const {result} = renderHook(() => useFilteredContributors(contributors));
     await act(async () => result.current.search('test'));
-    expect(result.current.filteredContributors).toEqual({});
+    expect(result.current.filteredContributors).toEqual([]);
 });
