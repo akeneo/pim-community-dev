40628632a83c8fe2fcbf6afcc3671d9f204ad6ae
Tseho <quentin.favrie@gmail.com>
2022-04-04 15:14:21 +0200

    CXP-916: remove warning logs of live ACLs

--- tests/back/Pim/Enrichment/EndToEnd/Product/Product/ExternalApi/ListProducts/SuccessListProductEndToEnd.php
+++ tests/back/Pim/Enrichment/EndToEnd/Product/Product/ExternalApi/ListProducts/SuccessListProductEndToEnd.php
@@ -975,14 +975,6 @@ JSON;
         $client->request('GET', 'api/rest/v1/products');
         $response = $client->getResponse();
 
-        $logger = self::$container->get('monolog.logger.pim_api_acl');
-        assert($logger instanceof TestLogger);
-
-        $this->assertTrue(
-            $logger->hasWarning('User "admin" with roles ROLE_ADMINISTRATOR is not granted "pim_api_product_list"'),
-            'Expected warning not found in the logs.'
-        );
-
         $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
     }
 
