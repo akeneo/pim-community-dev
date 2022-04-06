a8422fd1afd7c2b21cb11fbb28a52bf8156663ce
Celine Morin <celine.morin@akeneo.com>
2022-04-06 17:33:31 +0200

    CPM-558: Rename GetNonViewableProducts into GetViewableProducts

--- src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Query/GetViewableProducts.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Query/GetViewableProducts.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Enrichment\Product\Infrastructure\Query;
 
-use Akeneo\Pim\Enrichment\Product\Domain\Query\GetNonViewableProducts as GetNonViewableProductsInterface;
+use Akeneo\Pim\Enrichment\Product\Domain\Query\GetViewableProducts as GetViewableProductsInterface;
 
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class GetNonViewableProducts implements GetNonViewableProductsInterface
+final class GetViewableProducts implements GetViewableProductsInterface
 {
     /**
      * {@inheritDoc}
      */
     public function fromProductIdentifiers(array $productIdentifiers, int $userId): array
     {
-        return [];
+        return $productIdentifiers;
     }
 }
