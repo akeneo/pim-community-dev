5e67dd7b2e0b2c51530159f39c868643c0c2ae1e
Valentin Dijkstra <valentindijkstra@gmail.com>
2022-04-06 13:58:05 +0200

    RAB-654: Limit sample data length to 100 characters

--- components/tailored-import/back/src/Domain/SampleData/SelectSampleData.php
+++ components/tailored-import/back/src/Domain/SampleData/SelectSampleData.php
@@ -11,29 +11,43 @@ namespace Akeneo\Platform\TailoredImport\Domain\SampleData;
 final class SelectSampleData
 {
     public const NUMBER_OF_VALUES = 3;
+    public const SAMPLE_DATA_MAX_LENGTH = 100;
 
-    public static function fromExtractedColumn(array $extractedColumn, int $length = SelectSampleData::NUMBER_OF_VALUES): array
+    public static function fromExtractedColumn(array $extractedColumn, int $length = self::NUMBER_OF_VALUES): array
     {
-        $reducedValue = self::filterUniqueValues($extractedColumn);
-        $sampleData = self::pickRandomValues($reducedValue, $length);
+        $uniqueValues = self::filterUniqueValues($extractedColumn);
+        $pickedValues = self::pickRandomValues($uniqueValues, $length);
+        $sampleData = self::formatValues($pickedValues);
 
         return self::fillBlankValues($sampleData, $length);
     }
 
     private static function fillBlankValues(array $sampleData, int $length): array
     {
-        return array_pad($sampleData, $length, null);
+        return \array_pad($sampleData, $length, null);
     }
 
     private static function filterUniqueValues(array $sampleData): array
     {
-        return array_unique($sampleData);
+        return \array_unique($sampleData);
+    }
+
+    private static function formatValues(array $sampleData): array
+    {
+        return \array_map(
+            static fn (string $value): string => \mb_substr(
+                $value,
+                0,
+                self::SAMPLE_DATA_MAX_LENGTH,
+            ),
+            $sampleData,
+        );
     }
 
     private static function pickRandomValues(array $sampleData, int $length): array
     {
-        shuffle($sampleData);
+        \shuffle($sampleData);
 
-        return array_slice($sampleData, 0, min(count($sampleData), $length));
+        return \array_slice($sampleData, 0, \min(\count($sampleData), $length));
     }
 }
