02b3b69f823b0a27425021ef4cc353c5c12e9998
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-04-06 16:01:03 +0200

    PLG-469: Fix migration script

--- upgrades/schema/Version_7_0_20220404152647_dqi_purge_product_score_and_modify_pk.php
+++ upgrades/schema/Version_7_0_20220404152647_dqi_purge_product_score_and_modify_pk.php
@@ -12,18 +12,11 @@ final class Version_7_0_20220404152647_dqi_purge_product_score_and_modify_pk ext
 {
     public function up(Schema $schema): void
     {
-        $purgeProductScoresQuery = <<<SQL
-DELETE old_scores
+        $this->addSql('DELETE old_scores
 FROM pim_data_quality_insights_product_score AS old_scores
 INNER JOIN pim_data_quality_insights_product_score AS younger_scores
     ON younger_scores.product_id = old_scores.product_id
-    AND younger_scores.evaluated_at > old_scores.evaluated_at;
-SQL;
-
-        $date = new \DateTime();
-        $this->addSql($purgeProductScoresQuery, [
-            'purge_date' =>  $date->format('Y-m-d'),
-        ]);
+    AND younger_scores.evaluated_at > old_scores.evaluated_at;');
 
         $this->addSql('ALTER TABLE pim_data_quality_insights_product_score DROP PRIMARY KEY, ADD PRIMARY KEY (product_id), ALGORITHM=INPLACE, LOCK=NONE;');
     }
