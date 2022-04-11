df2c89b8db465d4b06ade624a9ba0c1889b32e19
Pierre Jolly <pie.jolly@gmail.com>
2022-04-11 16:12:20 +0200

    RAB-153: Add support for simple select attribute (back)

---
+++ components/tailored-import/back/src/Application/ExecuteDataMapping/UserIntentRegistry/UserIntentFactory/SimpleSelectUserIntentFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\TailoredImport\Application\ExecuteDataMapping\UserIntentRegistry\UserIntentFactory;
+
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetSimpleSelectValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\ValueUserIntent;
+use Akeneo\Platform\TailoredImport\Application\ExecuteDataMapping\UserIntentRegistry\UserIntentFactoryInterface;
+use Akeneo\Platform\TailoredImport\Domain\Model\Target\AttributeTarget;
+use Akeneo\Platform\TailoredImport\Domain\Model\Target\TargetInterface;
+
+class SimpleSelectUserIntentFactory implements UserIntentFactoryInterface
+{
+    public function create(TargetInterface $target, string $value): ValueUserIntent
+    {
+        if (!$target instanceof AttributeTarget) {
+            throw new \InvalidArgumentException('The target must be a AttributeTarget');
+        }
+
+        return new SetSimpleSelectValue(
+            $target->getCode(),
+            $target->getChannel(),
+            $target->getLocale(),
+            $value,
+        );
+    }
+
+    public function supports(TargetInterface $target): bool
+    {
+        return $target instanceof AttributeTarget && 'pim_catalog_simpleselect' === $target->getType();
+    }
+}
