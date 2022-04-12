ebe9fc98b1b67d86d789cfdc56c555f5dce800ac
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-04-12 15:52:11 +0200

    CPM-545: fix specs

--- src/Akeneo/Pim/Enrichment/Component/Error/Normalizer/ProductDomainErrorNormalizer.php
+++ src/Akeneo/Pim/Enrichment/Component/Error/Normalizer/ProductDomainErrorNormalizer.php
@@ -55,7 +55,7 @@ class ProductDomainErrorNormalizer implements NormalizerInterface, CacheableSupp
             }
 
             $data['product'] = [
-                'uuid' => $product->getUuid(),
+                'uuid' => $product->getUuid()->toString(),
                 'identifier' => $product->getIdentifier(),
                 'label' => $product->getLabel(),
                 'family' => null !== $product->getFamily() ? $product->getFamily()->getCode() : null,
