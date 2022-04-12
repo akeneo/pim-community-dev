9c59b41f47e73d460987cbbfcf184785e673220d
anthony <12048285+MadeWilson@users.noreply.github.com>
2022-04-12 16:44:04 +0200

    RAB-633 (#13437)
    * RAB-633: add Helper
    
    * RAB-633: remove useless method from Xlsx reader
    
    * RAB-633: clean error when handling previous
    
    * RAB-633: lint-fix
    
    * RAB-633: wrappe helper, don't use inline helper
    
    * RAB-633: remove useless import
    
    * RAB-633: fix lint front
    
    * RAB-633: fix test grth
    
    * Update components/tailored-import/back/src/Infrastructure/Validation/ColumnsValidator.php
    
    Co-authored-by: pierrejolly <47548830+pierrejolly@users.noreply.github.com>
    
    * Fix reviews
    
    * Early return if missing param
    
    * Add frontend test
    
    * Fix lint front
    
    * Restore deleted files
    
    * Fix reviews
    
    Co-authored-by: Adrien Pétremann <grena@users.noreply.github.com>
    Co-authored-by: pierrejolly <47548830+pierrejolly@users.noreply.github.com>
    Co-authored-by: Adrien Pétremann <adrien.petremann@getakeneo.com>

---
+++ components/tailored-import/back/src/Infrastructure/Validation/ReadColumns.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Akeneo\Platform\TailoredImport\Infrastructure\Validation;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ReadColumns extends Constraint
+{
+    public const FIRST_PRODUCT_ROW_AFTER_HEADER = 'akeneo.tailored_import.validation.file_structure.first_product_row_should_be_after_header_row';
+    public const UNIQUE_IDENTIFIER_AFTER_FIRST_COLUMN = 'akeneo.tailored_import.validation.file_structure.unique_identifier_column_should_be_after_first_column_message';
+    public const MAX_COUNT_REACHED = 'akeneo.tailored_import.validation.columns.max_count_reached';
+}
---
+++ components/tailored-import/back/src/Infrastructure/Validation/ReadColumnsValidator.php
@@ -0,0 +1,91 @@
+<?php
+
+
+namespace Akeneo\Platform\TailoredImport\Infrastructure\Validation;
+
+use Akeneo\Platform\TailoredImport\Domain\Query\Filesystem\XlsxFileReaderFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class ReadColumnsValidator extends ConstraintValidator
+{
+    private const MAX_COLUMN_COUNT = 500;
+
+    public function __construct(
+        private XlsxFileReaderFactoryInterface $xlsxFileReaderFactory,
+    ) {
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof ReadColumns) {
+            throw new UnexpectedTypeException($constraint, ReadColumns::class);
+        }
+
+        if (!$value instanceof Request) {
+            return;
+        }
+
+        $requiredParams = [
+            'file_key',
+            'file_structure',
+        ];
+
+        $missingParams = array_filter($requiredParams, fn ($param) => null === $value->get($param));
+
+        if (count($missingParams) > 0) {
+            $this->context->buildViolation(
+                RefreshSampleDataQuery::MISSING_QUERY_PARAMS,
+                [
+                    '{{ missing_params }}' => implode(', ', $missingParams),
+                ]
+            )->addViolation();
+
+            return;
+        }
+
+        $requestParams = [
+            'file_key' => $value->get('file_key'),
+            'file_structure' => $value->get('file_structure'),
+        ];
+
+        $validator = $this->context->getValidator()->inContext($this->context);
+        $validator->atPath('[params]')->validate($requestParams, new Collection([
+            'fields' => [
+                'file_key' => [
+                    new FileKey(),
+                ],
+                'file_structure' => [
+                    new IsValidFileStructure(),
+                ],
+            ],
+        ]));
+
+        if ($validator->getViolations()->count() > 0) {
+            return;
+        }
+
+        $fileStructure = $value->get('file_structure');
+        $reader = $this->xlsxFileReaderFactory->create($value->get('file_key'));
+        $row = current($reader->readRows(
+            $fileStructure['sheet_name'],
+            $fileStructure['header_row'],
+            1
+        ));
+
+        $this->validateLessThan500Column($row);
+    }
+
+    private function validateLessThan500Column(
+        array $columns
+    ): void {
+        if (count($columns) > 500) {
+            $this->context->buildViolation(ReadColumns::MAX_COUNT_REACHED)
+                ->setParameter('limit', strval(self::MAX_COLUMN_COUNT))
+                ->addViolation();
+        }
+    }
+}
