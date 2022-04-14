523fc637071ce5781e011f077035aeeb3a1f1791
Laurent Petard <laurent.petard@akeneo.com>
2022-04-14 12:14:59 +0200

    PLG-834: Add feature-flag service

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/FeatureFlag/AllCriteriaFeature.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\DataQualityInsights\Infrastructure\FeatureFlag;
+
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlags;
+
+/**
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class AllCriteriaFeature implements FeatureFlag
+{
+    public const NAME = 'data_quality_insights_all_criteria';
+
+    public function __construct(
+        private FeatureFlags $featureFlags
+    ) {
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->featureFlags->isEnabled(self::NAME);
+    }
+}
