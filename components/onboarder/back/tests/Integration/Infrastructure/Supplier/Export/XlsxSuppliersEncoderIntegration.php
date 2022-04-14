8125b280444db7d902b8e2649028b589543a7477
Philippe Mossiere <philippe.mossiere@akeneo.com>
2022-04-14 15:48:14 +0200

    OBS-71: Rework test in order to remove call of `GetAllSuppliersWithContributors` query

--- components/onboarder/back/tests/Integration/Infrastructure/Supplier/Export/XlsxSuppliersEncoderIntegration.php
+++ components/onboarder/back/tests/Integration/Infrastructure/Supplier/Export/XlsxSuppliersEncoderIntegration.php
@@ -4,19 +4,18 @@ declare(strict_types=1);
 
 namespace Akeneo\OnboarderSerenity\Test\Integration\Infrastructure\Supplier\Export;
 
-use Akeneo\OnboarderSerenity\Domain\Read\Supplier\GetAllSuppliersWithContributors;
+use Akeneo\OnboarderSerenity\Domain\Read\Supplier\Model\SupplierWithContributors;
 use Akeneo\OnboarderSerenity\Infrastructure\Supplier\Encoder\SuppliersEncoder;
-use Akeneo\OnboarderSerenity\Test\Integration\SqlIntegrationTestCase;
 use Box\Spout\Common\Type;
 use Box\Spout\Reader\Common\Creator\ReaderFactory;
-use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-final class XlsxSuppliersEncoderIntegration extends SqlIntegrationTestCase
+final class XlsxSuppliersEncoderIntegration extends KernelTestCase
 {
     /** @test */
     public function itExportsAXlsxFileContainingHeadersOnlyWhenThereIsNoSuppliers(): void
     {
-        $filepath = $this->get(SuppliersEncoder::class)([]);
+        $filepath = $this->getContainer()->get(SuppliersEncoder::class)([]);
 
         $xlsxReader = ReaderFactory::createFromType(Type::XLSX);
         $xlsxReader->open($filepath);
@@ -36,22 +35,22 @@ final class XlsxSuppliersEncoderIntegration extends SqlIntegrationTestCase
     /** @test */
     public function itExportsAXlsxFileContainingAllTheSuppliers(): void
     {
-        $this->createSupplier(
-            '44ce8069-8da1-4986-872f-311737f46f02',
-            'supplier_a',
-            'Supplier A',
-        );
-        $this->createSupplier(
-            '0d321e66-7858-4db6-89bb-9c99283e5c58',
-            'supplier_b',
-            'Supplier B',
-        );
+        $suppliers = [
+            new SupplierWithContributors(
+                '44ce8069-8da1-4986-872f-311737f46f02',
+                'supplier_a',
+                'Supplier A',
+                ['foo@foo.foo', 'bar@bar.bar'],
+            ),
+            new SupplierWithContributors(
+                '0d321e66-7858-4db6-89bb-9c99283e5c58',
+                'supplier_b',
+                'Supplier B',
+                [],
+            ),
+        ];
 
-        $this->createContributor('foo@foo.foo', '44ce8069-8da1-4986-872f-311737f46f02');
-        $this->createContributor('bar@bar.bar', '44ce8069-8da1-4986-872f-311737f46f02');
-
-        $suppliers = $this->get(GetAllSuppliersWithContributors::class)();
-        $filepath = $this->get(SuppliersEncoder::class)($suppliers);
+        $filepath = $this->getContainer()->get(SuppliersEncoder::class)($suppliers);
 
         $xlsxReader = ReaderFactory::createFromType(Type::XLSX);
         $xlsxReader->open($filepath);
@@ -72,39 +71,6 @@ final class XlsxSuppliersEncoderIntegration extends SqlIntegrationTestCase
                 ['supplier_b', 'Supplier B', ''],
             ],
             $rows,
-        );
-    }
-
-    private function createSupplier(string $identifier, string $code, string $label): void
-    {
-        $sql = <<<SQL
-            INSERT INTO `akeneo_onboarder_serenity_supplier` (identifier, code, label)
-            VALUES (:identifier, :code, :label)
-        SQL;
-
-        $this->get(Connection::class)->executeQuery(
-            $sql,
-            [
-                'identifier' => $identifier,
-                'code' => $code,
-                'label' => $label,
-            ],
-        );
-    }
-
-    private function createContributor(string $email, string $supplierIdentifier): void
-    {
-        $sql = <<<SQL
-            INSERT INTO `akeneo_onboarder_serenity_supplier_contributor` (email, supplier_identifier)
-            VALUES (:email, :supplierIdentifier)
-        SQL;
-
-        $this->get(Connection::class)->executeQuery(
-            $sql,
-            [
-                'email' => $email,
-                'supplierIdentifier' => $supplierIdentifier,
-            ],
         );
     }
 }
