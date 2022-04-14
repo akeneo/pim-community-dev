806fb43fc8d572b3d2aa03d6f4b1019f0e3220bd
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-04-14 16:04:36 +0200

    PIM-10396: Adapt non required attributes queries to handle table attribute

--- src/Akeneo/Pim/Automation/DataQualityInsights/tests/back/Integration/Infrastructure/Persistence/Query/Completeness/GetNonRequiredProductModelAttributesMaskQueryIntegration.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/tests/back/Integration/Infrastructure/Persistence/Query/Completeness/GetNonRequiredProductModelAttributesMaskQueryIntegration.php
@@ -43,6 +43,51 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
             // Attributes non required but deactivated from their attribute group
             ['code' => 'a_deactivated_text', 'type' => AttributeTypes::TEXT, 'group' => 'erp'],
             ['code' => 'a_deactivated_variant_text', 'type' => AttributeTypes::TEXT, 'group' => 'erp'],
+            // A table attribute because the handling is different than other attribute
+            [
+                'code' => 'a_localizable_scopable_table',
+                'type' => AttributeTypes::TABLE,
+                'localizable' => true,
+                'scopable' => true,
+                'table_configuration' => [
+                    [
+                        'code' => 'column_1',
+                        'data_type' => 'select',
+                        'labels' => [],
+                        'options' => [
+                            ['code' => 'option_1'],
+                            ['code' => 'option_2'],
+                            ['code' => 'option_3'],
+                        ],
+                    ],
+                    [
+                        'code' => 'column_2',
+                        'data_type' => 'text',
+                        'labels' => [],
+                    ],
+                ],
+            ],
+            [
+                'code' => 'a_non_localizable_scopable_table',
+                'type' => AttributeTypes::TABLE,
+                'table_configuration' => [
+                    [
+                        'code' => 'column_1',
+                        'data_type' => 'select',
+                        'labels' => [],
+                        'options' => [
+                            ['code' => 'option_1'],
+                            ['code' => 'option_2'],
+                            ['code' => 'option_3'],
+                        ],
+                    ],
+                    [
+                        'code' => 'column_2',
+                        'data_type' => 'text',
+                        'labels' => [],
+                    ],
+                ],
+            ]
         ]);
 
         $this->givenFamilies([
@@ -67,6 +112,8 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
                     'a_required_variant_text',
                     'a_deactivated_text',
                     'a_deactivated_variant_text',
+                    'a_localizable_scopable_table',
+                    'a_non_localizable_scopable_table'
                 ],
                 'attribute_requirements' => [
                     'ecommerce' => [
@@ -115,6 +162,12 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
             'a_non_localizable_scopable_text-ecommerce-<all_locales>',
             'a_localizable_non_scopable_locale_specific_us-<all_channels>-en_US',
             'a_non_localizable_scopable_locale_specific_fr_us-ecommerce-<all_locales>',
+            \sprintf(
+                'a_localizable_scopable_table-%s-ecommerce-en_US',
+                $this->getColumnId('a_localizable_scopable_table', 'column_2')
+            ),
+            \sprintf('a_non_localizable_scopable_table-%s-<all_channels>-<all_locales>',
+                $this->getColumnId('a_non_localizable_scopable_table', 'column_2')),
         ], $ecommerceEnUsMask->mask());
 
         $this->assertEqualsCanonicalizing([
@@ -127,6 +180,12 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
             'a_localizable_scopable_text-tablet-en_US',
             'a_localizable_non_scopable_locale_specific_us-<all_channels>-en_US',
             'a_non_localizable_scopable_locale_specific_fr_us-tablet-<all_locales>',
+            \sprintf(
+                'a_localizable_scopable_table-%s-tablet-en_US',
+                $this->getColumnId('a_localizable_scopable_table', 'column_2')
+            ),
+            \sprintf('a_non_localizable_scopable_table-%s-<all_channels>-<all_locales>',
+                $this->getColumnId('a_non_localizable_scopable_table', 'column_2')),
         ], $tabletEnUS->mask());
 
         $this->assertEqualsCanonicalizing([
@@ -139,7 +198,13 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
             'a_localizable_scopable_text-tablet-fr_FR',
             'a_non_localizable_non_scopable_locale_specific_fr-<all_channels>-<all_locales>',
             'a_non_localizable_scopable_locale_specific_fr_us-tablet-<all_locales>',
-            'a_localizable_scopable_locale_specific_fr-tablet-fr_FR'
+            'a_localizable_scopable_locale_specific_fr-tablet-fr_FR',
+            \sprintf(
+                'a_localizable_scopable_table-%s-tablet-fr_FR',
+                $this->getColumnId('a_localizable_scopable_table', 'column_2')
+            ),
+            \sprintf('a_non_localizable_scopable_table-%s-<all_channels>-<all_locales>',
+                $this->getColumnId('a_non_localizable_scopable_table', 'column_2')),
         ], $tabletFrFr->mask());
 
         $unknownProductId = $this->get(ProductModelIdFactory::class)->create('42');
@@ -211,5 +276,19 @@ class GetNonRequiredProductModelAttributesMaskQueryIntegration extends Completen
             'a_product_model_text-ecommerce-en_US',
             'a_sub_product_model_text-<all_channels>-<all_locales>'
         ], $attributesMask->requiredAttributesMaskForChannelAndLocale('ecommerce', 'en_US')->mask());
+    }
+
+    private function getColumnId(string $attributeCode, string $columnCode): string
+    {
+        $query = <<<SQL
+SELECT c.id
+FROM pim_catalog_table_column c
+    JOIN pim_catalog_attribute a ON a.id = c.attribute_id
+WHERE a.code = :attribute_code AND c.code = :column_code
+SQL;
+        return $this->get('database_connection')->executeQuery($query, [
+            'attribute_code' => $attributeCode,
+            'column_code' => $columnCode,
+        ])->fetchOne();
     }
 }
