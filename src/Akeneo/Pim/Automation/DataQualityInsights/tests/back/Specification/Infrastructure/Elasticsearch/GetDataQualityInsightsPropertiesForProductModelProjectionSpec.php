48229b452bf03fea641a8699a376d6698f43f4f0
Laurent Petard <laurent.petard@akeneo.com>
2022-04-19 14:40:55 +0200

    Fix specs

--- src/Akeneo/Pim/Automation/DataQualityInsights/tests/back/Specification/Infrastructure/Elasticsearch/GetDataQualityInsightsPropertiesForProductModelProjectionSpec.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/tests/back/Specification/Infrastructure/Elasticsearch/GetDataQualityInsightsPropertiesForProductModelProjectionSpec.php
@@ -7,6 +7,7 @@ namespace Specification\Akeneo\Pim\Automation\DataQualityInsights\Infrastructure
 use Akeneo\Pim\Automation\DataQualityInsights\Application\ComputeProductsKeyIndicators;
 use Akeneo\Pim\Automation\DataQualityInsights\Application\ProductEntityIdFactoryInterface;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\Model\ChannelLocaleRateCollection;
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\Model\Read;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\Query\ProductEnrichment\GetProductModelIdsFromProductModelCodesQueryInterface;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\Query\ProductEvaluation\GetProductModelScoresQueryInterface;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ChannelCode;
@@ -55,12 +56,22 @@ final class GetDataQualityInsightsPropertiesForProductModelProjectionSpec extend
         $localeFr = new LocaleCode('fr_FR');
 
         $getProductModelScoresQuery->byProductModelIds($collection)->willReturn([
-            42 => (new ChannelLocaleRateCollection)
-                ->addRate($channelMobile, $localeEn, new Rate(81))
-                ->addRate($channelMobile, $localeFr, new Rate(30))
-                ->addRate($channelEcommerce, $localeEn, new Rate(73)),
-            123 => (new ChannelLocaleRateCollection)
-                ->addRate($channelMobile, $localeEn, new Rate(66)),
+            42 => new Read\Scores(
+                (new ChannelLocaleRateCollection)
+                    ->addRate($channelMobile, $localeEn, new Rate(81))
+                    ->addRate($channelMobile, $localeFr, new Rate(30))
+                    ->addRate($channelEcommerce, $localeEn, new Rate(73)),
+                (new ChannelLocaleRateCollection)
+                    ->addRate($channelMobile, $localeEn, new Rate(78))
+                    ->addRate($channelMobile, $localeFr, new Rate(46))
+                    ->addRate($channelEcommerce, $localeEn, new Rate(81))
+            ),
+            123 => new Read\Scores(
+                (new ChannelLocaleRateCollection)
+                    ->addRate($channelMobile, $localeEn, new Rate(66)),
+                (new ChannelLocaleRateCollection)
+                    ->addRate($channelMobile, $localeEn, new Rate(74)),
+            )
         ]);
 
         $productModelsKeyIndicators = [
@@ -116,6 +127,15 @@ final class GetDataQualityInsightsPropertiesForProductModelProjectionSpec extend
                             'en_US' => 3,
                         ],
                     ],
+                    'scores_partial_criteria' => [
+                        'mobile' => [
+                            'en_US' => 3,
+                            'fr_FR' => 5,
+                        ],
+                        'ecommerce' => [
+                            'en_US' => 2,
+                        ],
+                    ],
                     'key_indicators' => $productModelsKeyIndicators[42]
                 ],
             ],
@@ -126,11 +146,16 @@ final class GetDataQualityInsightsPropertiesForProductModelProjectionSpec extend
                             'en_US' => 4,
                         ],
                     ],
+                    'scores_partial_criteria' => [
+                        'mobile' => [
+                            'en_US' => 3,
+                        ],
+                    ],
                     'key_indicators' => $productModelsKeyIndicators[123]
                 ],
             ],
             'product_model_without_rates' => [
-                'data_quality_insights' => ['scores' => [], 'key_indicators' => []],
+                'data_quality_insights' => ['scores' => [], 'scores_partial_criteria' => [], 'key_indicators' => []],
             ],
         ]);
     }
