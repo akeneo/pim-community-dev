bef98467b005b9c57a208d7bf8aca7fb7ebed8d9
ValentinMerlet <merlet.valentin@gmail.com>
2022-04-20 18:01:32 +0200

    OBS-79: Add integration test supplier import

---
+++ components/onboarder/back/tests/Integration/Infrastructure/Supplier/Import/ImportSupplierTaskletIntegration.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Test\Integration\Infrastructure\Supplier\Import;
+
+use Akeneo\OnboarderSerenity\Domain\Supplier\Write\Model\Supplier;
+use Akeneo\OnboarderSerenity\Test\Integration\SqlIntegrationTestCase;
+use Akeneo\Tool\Component\Batch\Job\JobParameters;
+use Akeneo\Tool\Component\Batch\Model\JobExecution;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+final class ImportSupplierTaskletIntegration extends SqlIntegrationTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        parent::bootKernel();
+    }
+
+    /** @test */
+    public function itImportsSupplierFromAXlsxFile(): void
+    {
+        $filePath = sprintf(
+            '%s/components/onboarder/back/tests/Integration/files/suppliers_import.xlsx',
+            static::$kernel->getProjectDir(),
+        );
+
+        $this->runOnboarderSerenityXlsxSupplierImportJob($filePath);
+        $supplier = $this->findSupplierByCode('supplier_1');
+
+        static::assertNotNull($supplier);
+        static::assertSame('Supplier 1', $supplier->label());
+        static::assertCount(2, $supplier->contributors());
+    }
+
+    /** @test */
+    public function itImportsNothingIfThereIsNoSuppliersInTheXlsxFile(): void
+    {
+        $filePath = sprintf(
+            '%s/components/onboarder/back/tests/Integration/files/suppliers_import_empty.xlsx',
+            static::$kernel->getProjectDir(),
+        );
+
+        $this->runOnboarderSerenityXlsxSupplierImportJob($filePath);
+
+        static::assertSame(0, $this->countSuppliers());
+    }
+
+    private function runOnboarderSerenityXlsxSupplierImportJob(string $filePath): void
+    {
+        $jobInstanceRepository = $this->get('pim_enrich.repository.job_instance');
+        $jobInstance = $jobInstanceRepository->findOneBy(['code' => 'onboarder_serenity.job.xlsx_supplier_import']);
+
+        $jobParameters = new JobParameters(['filePath' => $filePath]);
+        $jobExecution = new JobExecution();
+        $jobExecution->setJobParameters($jobParameters);
+        $jobExecution->setJobInstance($jobInstance);
+
+        $em = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $em->persist($jobExecution);
+        $em->flush();
+
+        $application = new Application(static::$kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'akeneo:batch:job',
+            'code' => 'onboarder_serenity.job.xlsx_supplier_import',
+            'execution' => $jobExecution->getId(),
+        ]);
+
+        $application->run($input, new BufferedOutput());
+    }
+
+    private function findSupplierByCode(string $code): ?Supplier
+    {
+        $sql = <<<SQL
+            WITH contributor AS (
+                SELECT supplier_identifier, JSON_ARRAYAGG(email) as contributor_emails
+                FROM `akeneo_onboarder_serenity_supplier_contributor` contributor
+                GROUP BY contributor.supplier_identifier
+            )
+            SELECT identifier, code, label, contributor.contributor_emails
+            FROM `akeneo_onboarder_serenity_supplier` supplier
+            LEFT JOIN contributor ON contributor.supplier_identifier = supplier.identifier
+            WHERE code = :code
+        SQL;
+
+        $row = static::$kernel->getContainer()->get('database_connection')->executeQuery(
+            $sql,
+            [
+                'code' => $code,
+            ],
+        )->fetchAssociative();
+
+        return false !== $row ? Supplier::create(
+            $row['identifier'],
+            $row['code'],
+            $row['label'],
+            null !== $row['contributor_emails'] ? json_decode($row['contributor_emails'], true) : [],
+        ) : null;
+    }
+
+    private function countSuppliers(): int
+    {
+        $sql = <<<SQL
+            SELECT COUNT(*)
+            FROM `akeneo_onboarder_serenity_supplier`
+        SQL;
+
+        return (int) static::$kernel->getContainer()->get('database_connection')->executeQuery($sql)->fetchOne();
+    }
+}
