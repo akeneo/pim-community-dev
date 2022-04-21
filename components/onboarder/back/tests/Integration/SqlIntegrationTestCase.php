50804f0925e500578e4d0b81c45268ef87f0a0ae
ValentinMerlet <merlet.valentin@gmail.com>
2022-04-21 15:26:44 +0200

    OBS-79: Add onboarder_serenity_xlsx_supplier_import job instance creation in SqlIntegrationTestCase

--- components/onboarder/back/tests/Integration/SqlIntegrationTestCase.php
+++ components/onboarder/back/tests/Integration/SqlIntegrationTestCase.php
@@ -25,6 +25,8 @@ abstract class SqlIntegrationTestCase extends KernelTestCase
             DELETE FROM `akeneo_onboarder_serenity_supplier_contributor`;
             DELETE FROM `akeneo_batch_job_execution`;
         SQL);
+
+        $this->addOnboarderSerenityXlsxSupplierImportJob();
     }
 
     protected function get(string $service): ?object
@@ -38,5 +40,43 @@ abstract class SqlIntegrationTestCase extends KernelTestCase
         $connectionCloser->closeConnections();
 
         $this->ensureKernelShutdown();
+    }
+
+    private function addOnboarderSerenityXlsxSupplierImportJob(): void
+    {
+        if ($this->onboarderSerenityXlsxSupplierImportJobExists()) {
+            return;
+        }
+
+        $sql = <<<SQL
+            INSERT INTO akeneo_batch_job_instance (code, label, job_name, status, connector, raw_parameters, type)
+            VALUES (:code, :label, :code, 0, :connector, :rawParameters, :type);
+        SQL;
+
+        $this->connection->executeStatement(
+            $sql,
+            [
+                'code' => 'onboarder_serenity_xlsx_supplier_import',
+                'label' => 'Onboarder Serenity XLSX Supplier Import',
+                'connector' => 'Onboarder Serenity',
+                'rawParameters' => 'a:0:{}',
+                'type' => 'import',
+            ],
+        );
+    }
+
+    private function onboarderSerenityXlsxSupplierImportJobExists(): bool
+    {
+        $sql = <<<SQL
+            SELECT COUNT(*)
+            FROM `akeneo_batch_job_instance`
+            WHERE code = :code
+        SQL;
+
+        return 1 === (int) $this
+                ->connection
+                ->executeQuery($sql, ['code' => 'onboarder_serenity_xlsx_supplier_import'])
+                ->fetchOne()
+            ;
     }
 }
