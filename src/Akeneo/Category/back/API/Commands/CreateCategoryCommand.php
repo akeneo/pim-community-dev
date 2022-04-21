30925b74f6930e8e138dc8e465b6fcde0d40ddd7
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2022-04-21 16:36:20 +0200

    Command creation

---
+++ src/Akeneo/Category/back/API/Commands/CreateCategoryCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Category\API\Commands;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CreateCategoryCommand
+{
+    private function __construct(
+        private string  $code,
+        private ?string $parent,
+        private array   $labels,
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        Assert::keyExists($data, 'code');
+        Assert::string($data['code']);
+
+        $parent = $data['parent'] ?? null;
+        Assert::nullOrString($parent);
+
+        $labels = $data['labels'] ?? [];
+        Assert::isArray($labels);
+
+        return new self($data['code'], $parent, $labels);
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function getParent(): ?string
+    {
+        return $this->parent;
+    }
+
+    public function getLabels(): array
+    {
+        return $this->labels;
+    }
+}
