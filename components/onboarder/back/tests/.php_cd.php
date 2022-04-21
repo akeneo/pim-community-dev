efc559aeba5959f298a33f2244572766ed252bb3
ValentinMerlet <merlet.valentin@gmail.com>
2022-04-21 15:54:34 +0200

    OBS-79: Fix php coupling detector

--- components/onboarder/back/tests/.php_cd.php
+++ components/onboarder/back/tests/.php_cd.php
@@ -50,6 +50,20 @@ $rules = [
             'Akeneo\OnboarderSerenity\Domain',
             'Akeneo\OnboarderSerenity\Application',
             // PIM coupling
+            'Akeneo\Tool\Component\Batch\Event\EventInterface',
+            'Akeneo\Tool\Component\Batch\Event\InvalidItemEvent',
+            'Akeneo\Tool\Component\Batch\Item\FileInvalidItem',
+            'Akeneo\Tool\Component\Batch\Item\InvalidItemException',
+            'Akeneo\Tool\Component\Batch\Item\ItemReaderInterface',
+            'Akeneo\Tool\Component\Batch\Job\JobRepositoryInterface',
+            'Akeneo\Tool\Component\Batch\Model\StepExecution',
+            'Akeneo\Tool\Component\Batch\Model\Warning',
+            'Akeneo\Tool\Component\Batch\Step\StepExecutionAwareInterface',
+            'Akeneo\Tool\Component\Connector\Exception\InvalidItemFromViolationsException',
+            'Akeneo\Tool\Component\Connector\Step\TaskletInterface',
+            'Akeneo\Tool\Component\StorageUtils\Cache\EntityManagerClearerInterface',
+            'Akeneo\Tool\Component\Connector\ArrayConverter\ArrayConverterInterface',
+            'Akeneo\Tool\Component\Connector\ArrayConverter\FieldsRequirementChecker',
             'Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvents',
             // External dependencies coupling
             'Box\Spout\Common\Type',
@@ -69,6 +83,8 @@ $rules = [
             'Akeneo\OnboarderSerenity\Domain',
             'Akeneo\OnboarderSerenity\Infrastructure\Supplier\Encoder\SuppliersEncoder',
             // PIM coupling
+            'Akeneo\Tool\Component\Batch\Job\JobParameters',
+            'Akeneo\Tool\Component\Batch\Model\JobExecution',
             // External dependencies coupling
             'Box\Spout\Common\Type',
             'Box\Spout\Reader\Common\Creator\ReaderFactory',
