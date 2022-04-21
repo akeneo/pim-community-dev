bbbf163201d614ebd88eecf6316c0bf1a79da65d
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-04-21 15:40:01 +0200

    PLG-841: Move Category connector & routing from Enrichment to Category

--- src/Akeneo/Category/back/Infrastructure/Symfony/DependencyInjection/AkeneoCategoryExtension.php
+++ src/Akeneo/Category/back/Infrastructure/Symfony/DependencyInjection/AkeneoCategoryExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages your bundle configuration.
  *
  * @author    Weasels
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
@@ -23,12 +23,15 @@ class AkeneoCategoryExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('connector/flat_translators.yml');
+        $loader->load('connector/use_cases.yml');
         $loader->load('entities.yml');
         $loader->load('models.yml');
         $loader->load('controllers.yml');
         $loader->load('product_grid_category_tree.yml');
         $loader->load('category_counters.yml');
         $loader->load('repositories.yml');
+        $loader->load('routing.yml');
     }
 }
