3ef63cce58d797ba5262dab6010c38e485a35613
Olivier Pontier <olivier.pontier@akeneo.com>
2022-04-26 16:36:21 +0200

    Move unit tests

--- make-file/category.mk
+++ make-file/category.mk
@@ -7,6 +7,10 @@ category-lint-back: #Doc: launch PHPStan for category bounded context
 category-lint-fix-back: #Doc: launch PHPStan for category bounded context
 	${PHP_RUN} vendor/bin/php-cs-fixer fix --diff --config=src/Akeneo/Category/back/tests/.php_cs.php
 
+.PHONY: category-unit-back
+category-unit-back: #Doc: launch PHPSpec for category bounded context
+	$(PHP_RUN) vendor/bin/phpspec run src/Akeneo/Category/back/tests/Specification $(O)
+
 .PHONY: category-integration-back
 category-integration-back: #Doc: launch PHPUnit integration tests for category bounded context
 	APP_ENV=test $(PHP_RUN) vendor/bin/phpunit -c src/Akeneo/Category/back/tests --testsuite Category_Integration_Test $(O)
@@ -17,10 +21,10 @@ category-end-to-end: #Doc: launch PHPUnit end to end tests for category bounded 
 
 .PHONY: category-end-to-end-behat
 category-end-to-end-behat: #Doc: launch Behat end to end tests for category bounded context
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat tests/legacy/features/pim/enrichment/category/ -p legacy -s all ${O}
+	APP_ENV=behat $(PHP_RUN) vendor/bin/behat tests/legacy/features/pim/enrichment/category/ -p legacy -s all $(O)
 
 .PHONY: category-ci-back
-category-ci-back: category-lint-back category-integration-back category-end-to-end category-end-to-end-behat
+category-ci-back: category-lint-back category-unit-back category-integration-back category-end-to-end category-end-to-end-behat
 
 .PHONY: category-ci
 category-ci: category-ci-back
---
+++ src/Akeneo/Category/back/tests/Specification/Infrastructure/Component/CategoryTree/UseCase/ListChildrenCategoriesWithCountHandlerSpec.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Specification\Akeneo\Category\Infrastructure\Component\CategoryTree\UseCase;
+
+use Akeneo\Category\Infrastructure\Component\CategoryTree\Query;
+use Akeneo\Category\Infrastructure\Component\CategoryTree\ReadModel\ChildCategory;
+use Akeneo\Category\Infrastructure\Component\CategoryTree\UseCase\ListChildrenCategoriesWithCount;
+use Akeneo\Category\Infrastructure\Component\CategoryTree\UseCase\ListChildrenCategoriesWithCountHandler;
+use Akeneo\Tool\Component\Classification\Model\CategoryInterface;
+use Akeneo\Tool\Component\Classification\Repository\CategoryRepositoryInterface;
+use Akeneo\UserManagement\Bundle\Context\UserContext;
+use PhpSpec\ObjectBehavior;
+
+class ListChildrenCategoriesWithCountHandlerSpec extends ObjectBehavior
+{
+    function let(
+        CategoryRepositoryInterface                                    $categoryRepository,
+        UserContext                                                    $userContext,
+        Query\ListChildrenCategoriesWithCountIncludingSubCategories    $listIncludingSubCategories,
+        Query\ListChildrenCategoriesWithCountNotIncludingSubCategories $listNotIncludingSubCategories
+    )
+    {
+        $this->beConstructedWith(
+            $categoryRepository,
+            $userContext,
+            $listIncludingSubCategories,
+            $listNotIncludingSubCategories
+        );
+    }
+
+    function it_is_an_handler()
+    {
+        $this->shouldHaveType(ListChildrenCategoriesWithCountHandler::class);
+    }
+
+    function it_handles_children_categories_with_count_including_sub_categories(
+        $categoryRepository,
+        $listIncludingSubCategories,
+        CategoryInterface $categoryToExpand
+    )
+    {
+        $categoryRepository->find(1)->willReturn($categoryToExpand);
+        $categoryToExpand->getId()->willReturn(1);
+
+        $listIncludingSubCategories->list('en_US', 1, 1, null)->willReturn([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+
+        $query = new ListChildrenCategoriesWithCount(1, -1, true, 1, 'en_US');
+        $this->handle($query)->shouldBeLike([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+    }
+
+    function it_handles_children_categories_with_count_not_including_sub_categories(
+        $categoryRepository,
+        $listNotIncludingSubCategories,
+        CategoryInterface $categoryToExpand
+    )
+    {
+        $categoryRepository->find(1)->willReturn($categoryToExpand);
+        $categoryToExpand->getId()->willReturn(1);
+
+        $listNotIncludingSubCategories->list('en_US', 1, 1, null)->willReturn([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+
+        $query = new ListChildrenCategoriesWithCount(1, -1, false, 1, 'en_US');
+        $this->handle($query)->shouldBeLike([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+    }
+
+    function it_handles_children_categories_of_user_product_category_tree_when_no_category_selected_as_filter(
+        $userContext,
+        $listNotIncludingSubCategories,
+        CategoryInterface $treeToExpand
+    )
+    {
+        $userContext->getUserProductCategoryTree()->willReturn($treeToExpand);
+        $treeToExpand->getId()->willReturn(1);
+
+        $listNotIncludingSubCategories->list('en_US', 1, 1, null)->willReturn([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+
+        $query = new ListChildrenCategoriesWithCount(-1, -1, false, 1, 'en_US');
+        $this->handle($query)->shouldBeLike([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+    }
+
+    function it_handles_children_categories_with_category_selected_as_filter(
+        $categoryRepository,
+        $listNotIncludingSubCategories,
+        CategoryInterface $categoryToExpand,
+        CategoryInterface $categoryToFilterWith
+    )
+    {
+        $categoryRepository->find(1)->willReturn($categoryToExpand);
+        $categoryToExpand->getId()->willReturn(1);
+
+        $categoryRepository->isAncestor($categoryToExpand, $categoryToFilterWith)->willReturn(true);
+        $categoryRepository->find(3)->willReturn($categoryToFilterWith);
+        $categoryToFilterWith->getId()->willReturn(3);
+
+        $listNotIncludingSubCategories->list('en_US', 1, 1, 3)->willReturn([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+
+        $query = new ListChildrenCategoriesWithCount(1, 3, false, 1, 'en_US');
+        $this->handle($query)->shouldBeLike([
+            new ChildCategory(1, 'code', 'label', true, true, 10, [])
+        ]);
+    }
+}
