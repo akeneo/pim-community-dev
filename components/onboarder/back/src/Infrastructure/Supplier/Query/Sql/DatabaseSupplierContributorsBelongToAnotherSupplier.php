eb99eea99174bddbed7beb857968f0104de0f5a0
ValentinMerlet <merlet.valentin@gmail.com>
2022-04-26 11:33:01 +0200

    OBS-78: Remove unnecessary comment

--- components/onboarder/back/src/Infrastructure/Supplier/Query/Sql/DatabaseSupplierContributorsBelongToAnotherSupplier.php
+++ components/onboarder/back/src/Infrastructure/Supplier/Query/Sql/DatabaseSupplierContributorsBelongToAnotherSupplier.php
@@ -18,17 +18,16 @@ final class DatabaseSupplierContributorsBelongToAnotherSupplier implements Suppl
 
     public function __invoke(string $supplierIdentifier, array $emails): array
     {
-        //An empty array would generate en invalid mysql query, IN operator does not accept an empty array
         if (empty($emails)) {
             return [];
         }
 
         $query = <<<SQL
-SELECT email
-FROM akeneo_onboarder_serenity_supplier_contributor
-WHERE email IN (:emails)
-AND supplier_identifier != :supplierIdentifier
-SQL;
+            SELECT email
+            FROM akeneo_onboarder_serenity_supplier_contributor
+            WHERE email IN (:emails)
+            AND supplier_identifier != :supplierIdentifier
+        SQL;
 
         $statement = $this->connection->executeQuery(
             $query,
