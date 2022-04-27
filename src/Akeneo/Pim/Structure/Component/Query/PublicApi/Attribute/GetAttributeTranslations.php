6b0651401ce4c1adcedbf37d67e4b9737867416a
Steven VAIDIE <steven.vaidie@gmail.com>
2022-04-27 14:31:08 +0200

    RAC-1234: Fix lint

--- src/Akeneo/Pim/Structure/Component/Query/PublicApi/Attribute/GetAttributeTranslations.php
+++ src/Akeneo/Pim/Structure/Component/Query/PublicApi/Attribute/GetAttributeTranslations.php
@@ -7,12 +7,12 @@ namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Attribute;
 interface GetAttributeTranslations
 {
     /**
-     * @return array<string: attributeCode, string: label>
+     * @return array<string, string>
      */
     public function byAttributeCodesAndLocale(array $attributeCodes, string $locale): array;
 
     /**
-     * @return array<string: attributeCode, array<string: locale, string: label>>
+     * @return array<string , array<string, string>>
      */
     public function byAttributeCodes(array $attributeCodes): array;
 }
