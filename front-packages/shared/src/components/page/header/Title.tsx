aa1381627a41015f32af9c696e4bc23355ee8888
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-04-28 15:24:40 +0200

    PIM-10422: add option to not transform text in title

--- front-packages/shared/src/components/page/header/Title.tsx
+++ front-packages/shared/src/components/page/header/Title.tsx
@@ -2,7 +2,7 @@ import React, {FC} from 'react';
 import {SkeletonPlaceholder} from 'akeneo-design-system';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Container = styled.div<{noTextTransform: boolean}>`
   color: ${({theme}) => theme.color.purple100};
   font-size: ${({theme}) => theme.fontSize.title};
   line-height: 34px;
@@ -14,17 +14,20 @@ const Container = styled.div`
   flex-grow: 1;
 
   &:first-letter {
-    text-transform: uppercase;
+    text-transform: ${({noTextTransform}) => (noTextTransform ? 'initial' : 'capitalize')};
   }
 `;
 
 type Props = {
   showPlaceholder?: boolean;
+  noTextTransform?: boolean;
 };
 
-const Title: FC<Props> = ({children, showPlaceholder}) => {
+const Title: FC<Props> = ({children, showPlaceholder, noTextTransform}) => {
   return (
-    <Container>{showPlaceholder ? <SkeletonPlaceholder>{children}</SkeletonPlaceholder> : <>{children}</>}</Container>
+    <Container noTextTransform={noTextTransform ?? false}>
+      {showPlaceholder ? <SkeletonPlaceholder>{children}</SkeletonPlaceholder> : <>{children}</>}
+    </Container>
   );
 };
 
