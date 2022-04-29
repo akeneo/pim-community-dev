b43c4401191570754e2bf9bb77f404f5a4148161
pierallard <pierallard@gmail.com>
2022-04-29 19:29:01 +0200

    CPM-577: Run linter

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/fetcher/ProductEditForm/fetchProductModelEvaluation.ts
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/fetcher/ProductEditForm/fetchProductModelEvaluation.ts
@@ -5,7 +5,9 @@ const Routing = require('routing');
 
 const ROUTE_NAME = 'akeneo_data_quality_insights_product_model_evaluation';
 
-const fetchProductModelEvaluation: ProductEvaluationFetcher = async (productModelId: string): Promise<ProductEvaluation> => {
+const fetchProductModelEvaluation: ProductEvaluationFetcher = async (
+  productModelId: string
+): Promise<ProductEvaluation> => {
   const response = await fetch(
     Routing.generate(ROUTE_NAME, {
       productModelId,
