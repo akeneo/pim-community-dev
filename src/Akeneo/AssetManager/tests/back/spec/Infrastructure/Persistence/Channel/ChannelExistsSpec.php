ade70e98aa51b9652c3cad486192d292d8243abe
Adrien Pétremann <adrien.petremann@getakeneo.com>
2022-05-02 11:02:12 +0200

    Update queries of AssetManager

---
+++ src/Akeneo/AssetManager/tests/back/spec/Infrastructure/Persistence/Channel/ChannelExistsSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Akeneo\AssetManager\Infrastructure\Persistence\Channel;
+
+use Akeneo\AssetManager\Domain\Model\ChannelIdentifier;
+use Akeneo\Channel\API\Query\Channel;
+use Akeneo\Channel\API\Query\FindChannels;
+use Akeneo\Channel\API\Query\LabelCollection;
+use PhpSpec\ObjectBehavior;
+
+class ChannelExistsSpec extends ObjectBehavior
+{
+    public function let(FindChannels $findChannels)
+    {
+        $channelEcommerce = new Channel('ecommerce', [], LabelCollection::fromArray([]), []);
+        $channelPrint = new Channel('print', [], LabelCollection::fromArray([]), []);
+
+        $findChannels->findAll()->willReturn([
+            $channelEcommerce,
+            $channelPrint,
+        ]);
+
+        $this->beConstructedWith($findChannels);
+    }
+
+    public function it_tells_if_a_channel_exists()
+    {
+        $ecommerceIdentifier = ChannelIdentifier::fromCode('ecommerce');
+        $this->exists($ecommerceIdentifier)->shouldReturn(true);
+
+        $printIdentifier = ChannelIdentifier::fromCode('print');
+        $this->exists($printIdentifier)->shouldReturn(true);
+
+        $unknownIdentifier = ChannelIdentifier::fromCode('unknown');
+        $this->exists($unknownIdentifier)->shouldReturn(false);
+    }
+
+    public function it_is_case_insensitive()
+    {
+        $ecommerceIdentifier = ChannelIdentifier::fromCode('eCoMmErCe');
+        $this->exists($ecommerceIdentifier)->shouldReturn(true);
+
+        $ecommerceIdentifier = ChannelIdentifier::fromCode('ECOMMERCE');
+        $this->exists($ecommerceIdentifier)->shouldReturn(true);
+    }
+}
---
+++ src/Akeneo/AssetManager/tests/back/spec/Infrastructure/Persistence/Channel/FindActivatedLocalesByIdentifiersSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Akeneo\AssetManager\Infrastructure\Persistence\Channel;
+
+use Akeneo\AssetManager\Domain\Model\LocaleIdentifierCollection;
+use Akeneo\Channel\API\Query\FindLocales;
+use Akeneo\Channel\API\Query\Locale;
+use PhpSpec\ObjectBehavior;
+
+class FindActivatedLocalesByIdentifiersSpec extends ObjectBehavior
+{
+    public function let(FindLocales $findLocales)
+    {
+        $frLocale = new Locale('fr_FR', true);
+        $deLocale = new Locale('de_DE', true);
+
+        $findLocales->findAllActivated()->willReturn([$frLocale, $deLocale]);
+
+        $this->beConstructedWith($findLocales);
+    }
+
+    public function it_fetches_activated_locale_codes_from_identifiers()
+    {
+        $localeIdentifiers = LocaleIdentifierCollection::fromNormalized(['fr_FR', 'en_US']);
+        $expectedIdentifiers = LocaleIdentifierCollection::fromNormalized(['fr_FR']);
+
+        $result = $this->find($localeIdentifiers);
+
+        $result->shouldBeLike($expectedIdentifiers);
+    }
+
+    public function it_is_case_insensitive()
+    {
+        $localeIdentifiers = LocaleIdentifierCollection::fromNormalized(['FR_fR', 'de_dE']);
+        $expectedIdentifiers = LocaleIdentifierCollection::fromNormalized(['fr_FR', 'de_DE']);
+
+        $result = $this->find($localeIdentifiers);
+
+        $result->shouldBeLike($expectedIdentifiers);
+    }
+}
