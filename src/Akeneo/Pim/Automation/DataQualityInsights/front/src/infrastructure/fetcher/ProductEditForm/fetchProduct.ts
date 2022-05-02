46b0313160f88cf39076d13e42b7d9c1336d141d
pierallard <pierallard@gmail.com>
2022-05-02 11:16:11 +0200

    CPM-577: Fix navigation

--- src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/fetcher/ProductEditForm/fetchProduct.ts
+++ src/Akeneo/Pim/Automation/DataQualityInsights/front/src/infrastructure/fetcher/ProductEditForm/fetchProduct.ts
@@ -5,7 +5,7 @@ const ROUTE_NAME = 'pim_enrich_product_rest_get';
 const fetchProduct = async (id: string) => {
   const response = await fetch(
     Routing.generate(ROUTE_NAME, {
-      id: id,
+      uuid: id,
     })
   );
 
