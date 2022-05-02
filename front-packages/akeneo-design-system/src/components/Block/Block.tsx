89f407bb170f6872995a6bc4a889970f48827d54
axelhms <hemissi.axel@gmail.com>
2022-05-02 11:33:30 +0200

    RAB-713: Handle collapse

--- front-packages/akeneo-design-system/src/components/Block/Block.tsx
+++ front-packages/akeneo-design-system/src/components/Block/Block.tsx
@@ -1,8 +1,8 @@
-import React, {cloneElement, isValidElement, ReactNode, Ref} from 'react';
-import styled from 'styled-components';
+import React, {cloneElement, isValidElement, ReactNode, Ref, useEffect, useRef, useState} from 'react';
+import styled, {css} from 'styled-components';
 import {AkeneoThemedProps, getColor, getFontSize} from '../../theme';
 import {Override} from '../../shared';
-import {IconProps} from '../../icons';
+import {ArrowDownIcon, ArrowUpIcon, IconProps} from '../../icons';
 import {IconButton, IconButtonProps} from '../IconButton/IconButton';
 
 type BlockProps = Override<
@@ -28,28 +28,87 @@ type BlockProps = Override<
      */
     ariaDescribedBy?: string;
 
+    // /**
+    //  * Block is open.
+    //  */
+    // open?: boolean;
+
+    // /**
+    //  * Block is collapsable.
+    //  */
+    // isCollapsable?: boolean;
+
     /**
      * Children of the button.
      */
     children?: ReactNode;
-  }
+  } & (
+    | {
+        /**
+         * Whether or not the Preview is open.
+         */
+        isOpen: boolean;
+
+        /**
+         * Label of the collapse button.
+         */
+        collapseButtonLabel: string;
+
+        /**
+         * Handler called when the collapse button is clicked.
+         */
+        onCollapse: (isOpen: boolean) => void;
+      }
+    | {
+        isOpen?: undefined;
+        collapseButtonLabel?: undefined;
+        onCollapse?: undefined;
+      }
+  )
 >;
+
+const ANIMATION_DURATION = 100;
 
 const ActionsContainer = styled.div`
   display: none;
   align-items: center;
 `;
 
+const BlockTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 24px;
+`;
+
+const BlockContent = styled.div<
+  {isCollapsable: boolean; $height: number; $overflow: string; shouldAnimate: boolean} & AkeneoThemedProps
+>`
+  overflow-wrap: break-word;
+  white-space: break-spaces;
+  color: ${getColor('grey', 140)};
+  margin-top: ${({$height, isCollapsable}) => (0 === $height && isCollapsable ? 0 : 5)}px;
+  ${({isCollapsable, $height, $overflow, shouldAnimate}) =>
+    isCollapsable &&
+    css`
+      max-height: ${$height}px;
+      overflow: ${$overflow};
+      ${shouldAnimate &&
+      css`
+        transition: all ${ANIMATION_DURATION}ms ease-in-out;
+        transition-property: max-height, margin-top;
+      `}
+    `}
+`;
+
 const Container = styled.div<AkeneoThemedProps>`
   box-sizing: border-box;
-  padding: 0 20px;
+  padding: 10px 15px;
   border-style: solid;
   border-width: 1px;
   border-radius: 2px;
-  height: 50px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
   font-family: inherit;
   font-size: ${getFontSize('default')};
   font-weight: 400;
@@ -68,9 +127,39 @@ const Container = styled.div<AkeneoThemedProps>`
 
 const Block = React.forwardRef<HTMLButtonElement, BlockProps>(
   (
-    {actions, ariaDescribedBy, ariaLabel, ariaLabelledBy, children, ...rest}: BlockProps,
+    {actions, ariaDescribedBy, ariaLabel, ariaLabelledBy, isOpen, onCollapse, children, ...rest}: BlockProps,
     forwardedRef: Ref<HTMLButtonElement>
   ) => {
+    // isCollapsable = true;
+    // const [isOpen, setIsOpen] = useState(open);
+    // const handleCollapse = () => setIsOpen(!isOpen);
+
+    const [contentHeight, setContentHeight] = useState<number>(0);
+    const [shouldAnimate, setShouldAnimate] = useState<boolean>(false);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    const isCollapsable = undefined !== onCollapse && undefined !== isOpen;
+
+    const handleCollapse = () => onCollapse?.(!isOpen);
+
+    useEffect(() => {
+      if (!isCollapsable) return;
+
+      setContentHeight(contentHeight => {
+        const scrollHeight = contentRef.current?.scrollHeight ?? 0;
+
+        return 0 === scrollHeight ? contentHeight : scrollHeight;
+      });
+
+      const shouldAnimateTimeoutId = window.setTimeout(() => {
+        setShouldAnimate(true);
+      }, ANIMATION_DURATION);
+
+      return () => {
+        window.clearTimeout(shouldAnimateTimeoutId);
+      };
+    }, [children]);
+
     return (
       <Container
         aria-describedby={ariaDescribedBy}
@@ -79,7 +168,7 @@ const Block = React.forwardRef<HTMLButtonElement, BlockProps>(
         ref={forwardedRef}
         {...rest}
       >
-        <div>
+        <BlockTitle>
           {React.Children.map(children, child => {
             if (isValidElement<IconProps>(child)) {
               return React.cloneElement(child, {size: child.props.size ?? 18});
@@ -87,8 +176,31 @@ const Block = React.forwardRef<HTMLButtonElement, BlockProps>(
 
             return child;
           })}
-        </div>
-        <ActionsContainer>{actions}</ActionsContainer>
+
+          <ActionsContainer>
+            {isCollapsable ? (
+              <IconButton
+                icon={isOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}
+                title="Collapse"
+                level="tertiary"
+                ghost="borderless"
+                size="small"
+                onClick={handleCollapse}
+              />
+            ) : null}
+            {actions}
+          </ActionsContainer>
+        </BlockTitle>
+        <BlockContent
+          ref={contentRef}
+          isCollapsable={isCollapsable}
+          $overflow={shouldAnimate || !isOpen ? 'hidden' : 'inherit'}
+          $height={true === isOpen ? contentHeight : 0}
+          shouldAnimate={shouldAnimate}
+          aria-hidden={!isOpen}
+        >
+          <div>Example content</div>
+        </BlockContent>
       </Container>
     );
   }
