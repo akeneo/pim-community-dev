d8e22841bfe7111d9cb5f5315567aa9c506e313f
Adrien Migaire <adrien.migaire@akeneo.com>
2022-05-03 08:05:35 +0000

    Preparing version 5.0.93

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.92';
+    const VERSION = '5.0.93';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
