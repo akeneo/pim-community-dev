64ec574a3521cf8554f487c10f0ff49199171cce
Tseho <quentin.favrie@gmail.com>
2022-05-03 09:35:45 +0200

    wip

--- components/catalogs/front/product-selection-builder/src/dependencies/require-context.ts
+++
@@ -1,5 +0,0 @@
-/* istanbul ignore file */
-
-const requireContext = require('require-context');
-
-export default requireContext;
--- components/catalogs/front/product-selection-builder/src/dependencies/translate.ts
+++
@@ -1,5 +0,0 @@
-/* istanbul ignore file */
-
-const translate = require('oro/translator');
-
-export default translate;
