ec8976ccd9712d90f9e692f84d7f0e491ef52f63
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-05-05 10:14:31 +0200

    CPM-609: Update pim_comment_comment table creation and fix code to use uuid (#16948)
    * CPM-609 add resourceUuid to Product's comment
    
    * CPM-609: make columns nullable
    
    * CPM-609: clean and fix normalizer
    
    * CPM-609: fix behats
    
    Co-authored-by: Adrien Migaire <adrien.migaire@akeneo.com>

--- tests/back/Pim/Enrichment/Specification/Component/Comment/Normalizer/Standard/CommentNormalizerSpec.php
+++ tests/back/Pim/Enrichment/Specification/Component/Comment/Normalizer/Standard/CommentNormalizerSpec.php
@@ -7,6 +7,7 @@ use Akeneo\Pim\Enrichment\Component\Comment\Normalizer\Standard\CommentNormalize
 use Akeneo\UserManagement\Component\Model\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -59,6 +60,7 @@ class CommentNormalizerSpec extends ObjectBehavior
                 'id'           => 2,
                 'resourceName' => 'Product',
                 'resourceId'   => '100',
+                'resourceUuid' => 'b7d38dc6-5166-46c5-ae7f-2643d8ef4edc',
                 'author'       => [
                     'username' => 'mary',
                     'fullName' => 'Mary Dia',
@@ -77,6 +79,7 @@ class CommentNormalizerSpec extends ObjectBehavior
         $comment->getId()->willReturn(1);
         $comment->getResourceName()->willReturn('Product');
         $comment->getResourceId()->willReturn('100');
+        $comment->getResourceUuid()->willReturn(Uuid::fromString('b7d38dc6-5166-46c5-ae7f-2643d8ef4edc'));
         $comment->getAuthor()->willReturn($author);
         $comment->getBody()->willReturn('Body of the comment');
         $comment->getCreatedAt()->willReturn($dateTime);
@@ -87,6 +90,7 @@ class CommentNormalizerSpec extends ObjectBehavior
             'id'           => 1,
             'resourceName' => 'Product',
             'resourceId'   => '100',
+            'resourceUuid'   => 'b7d38dc6-5166-46c5-ae7f-2643d8ef4edc',
             'author'       => [
                 'username' => 'julia',
                 'fullName' => 'Julia Doe',
@@ -100,6 +104,7 @@ class CommentNormalizerSpec extends ObjectBehavior
                     'id'           => 2,
                     'resourceName' => 'Product',
                     'resourceId'   => '100',
+                    'resourceUuid'   => 'b7d38dc6-5166-46c5-ae7f-2643d8ef4edc',
                     'author'       => [
                         'username' => 'mary',
                         'fullName' => 'Mary Dia',
@@ -125,6 +130,7 @@ class CommentNormalizerSpec extends ObjectBehavior
         $comment->getId()->willReturn(42);
         $comment->getResourceName()->willReturn('Product');
         $comment->getResourceId()->willReturn('100');
+        $comment->getResourceUuid()->willReturn(Uuid::fromString('b7d38dc6-5166-46c5-ae7f-2643d8ef4edc'));
         $comment->getAuthor()->willReturn(null);
         $comment->getBody()->willReturn('Lorem ipsum dolor sit amet');
         $comment->getCreatedAt()->willReturn($dateTime);
@@ -136,6 +142,7 @@ class CommentNormalizerSpec extends ObjectBehavior
                 'id' => 42,
                 'resourceName' => 'Product',
                 'resourceId' => '100',
+                'resourceUuid' => 'b7d38dc6-5166-46c5-ae7f-2643d8ef4edc',
                 'author' => [
                     'username' => null,
                     'fullName' => null,
