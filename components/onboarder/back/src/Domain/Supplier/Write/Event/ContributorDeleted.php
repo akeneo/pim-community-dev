7489e4f8ec514fcba079bc6f8dbd3ee039aaed33
ValentinMerlet <merlet.valentin@gmail.com>
2022-05-06 16:47:31 +0200

    OBS-77: Add ContributorDeleted event

---
+++ components/onboarder/back/src/Domain/Supplier/Write/Event/ContributorDeleted.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Domain\Supplier\Write\Event;
+
+use Akeneo\OnboarderSerenity\Domain\Supplier\Write\ValueObject\Identifier;
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class ContributorDeleted extends Event
+{
+    public function __construct(private Identifier $supplierIdentifier, private string $contributorEmail)
+    {
+    }
+
+    public function supplierIdentifier(): Identifier
+    {
+        return $this->supplierIdentifier;
+    }
+
+    public function contributorEmail(): string
+    {
+        return $this->contributorEmail;
+    }
+}
