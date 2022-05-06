77d7b966ef772ce6509fb7cadb37f60c0053e4ca
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-05-06 15:48:10 +0200

    CPM: fix behat

--- src/Akeneo/Tool/Component/Batch/Model/StepExecution.php
+++ src/Akeneo/Tool/Component/Batch/Model/StepExecution.php
@@ -471,10 +471,19 @@ class StepExecution
             $data = [];
         }
 
+        $id = '[unknown]';
+        if (\method_exists($data, 'getUuid')
+            && get_class($data) !== 'Akeneo\Pim\WorkOrganization\Workflow\Component\Model\PublishedProductInterface'
+        ) {
+            $id = $data->getUuid()->toString();
+        } elseif (\method_exists($data, 'getId')) {
+            $id = $data->getId();
+        }
+
         if (is_object($data)) {
             $data = [
                 'class'  => ClassUtils::getClass($data),
-                'id'     => method_exists($data, 'getId') ? $data->getId() : '[unknown]',
+                'id'     => $id,
                 'string' => method_exists($data, '__toString') ? (string) $data : '[unknown]',
             ];
         }
