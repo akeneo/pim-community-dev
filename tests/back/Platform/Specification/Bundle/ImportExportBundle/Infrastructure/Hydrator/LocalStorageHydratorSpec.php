dc056328b4a2dcc070989a17cf1a22ca162e2aa5
Steven VAIDIE <steven.vaidie@gmail.com>
2022-05-10 16:11:53 +0200

    RAB-683: Add tests

---
+++ tests/back/Platform/Specification/Bundle/ImportExportBundle/Infrastructure/Hydrator/LocalStorageHydratorSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Specification\Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\Hydrator;
+
+use Akeneo\Platform\Bundle\ImportExportBundle\Domain\LocalStorage;
+use Akeneo\Platform\VersionProviderInterface;
+use PhpSpec\ObjectBehavior;
+
+class LocalStorageHydratorSpec extends ObjectBehavior
+{
+    public function let(VersionProviderInterface $versionProvider)
+    {
+        $versionProvider->isSaaSVersion()->willReturn(false);
+        $this->beConstructedWith($versionProvider);
+    }
+
+    public function it_supports_only_local_storage()
+    {
+        $this->supports(['type' => 'local', 'file_path' => 'a_file_path'])->shouldReturn(true);
+        $this->supports(['type' => 'none'])->shouldReturn(false);
+        $this->supports(['type' => 'unknown'])->shouldReturn(false);
+    }
+
+    public function it_hydrates_a_local_storage()
+    {
+        $this->hydrate(['type' => 'local', 'file_path' => 'a_file_path'])->shouldBeLike(new LocalStorage('a_file_path'));
+    }
+
+    public function it_throws_an_exception_on_saas_environment(VersionProviderInterface $versionProvider)
+    {
+        $versionProvider->isSaaSVersion()->willReturn(true);
+
+        $this->shouldThrow(\InvalidArgumentException::class)->during('hydrate', [['type' => 'local', 'file_path' => 'a_file_path']]);
+    }
+}
---
+++ tests/back/Platform/Specification/Bundle/ImportExportBundle/Infrastructure/Hydrator/NoneStorageHydratorSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Specification\Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\Hydrator;
+
+use Akeneo\Platform\Bundle\ImportExportBundle\Domain\NoneStorage;
+use PhpSpec\ObjectBehavior;
+
+class NoneStorageHydratorSpec extends ObjectBehavior
+{
+    public function it_supports_only_local_storage()
+    {
+        $this->supports(['type' => 'none'])->shouldReturn(true);
+        $this->supports(['type' => 'local'])->shouldReturn(false);
+        $this->supports(['type' => 'unknown'])->shouldReturn(false);
+    }
+
+    public function it_hydrates_a_none_storage()
+    {
+        $this->hydrate(['type' => 'local', 'file_path' => 'a_file_path'])->shouldBeLike(new NoneStorage());
+    }
+}
