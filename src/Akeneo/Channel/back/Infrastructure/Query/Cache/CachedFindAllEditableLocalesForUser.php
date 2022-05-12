e098308d86f463211941fb8867a4316f3d98d40a
Pierre Jolly <pierre.jolly@akeneo.com>
2022-05-12 16:59:47 +0200

    RAB-617: Retrieve all editable locales for user

---
+++ src/Akeneo/Channel/back/Infrastructure/Query/Cache/CachedFindAllEditableLocalesForUser.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Channel\Infrastructure\Query\Cache;
+
+use Akeneo\Channel\API\Query\FindAllEditableLocalesForUser;
+use Akeneo\Tool\Component\StorageUtils\Cache\CachedQueryInterface;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class CachedFindAllEditableLocalesForUser implements FindAllEditableLocalesForUser, CachedQueryInterface
+{
+    private ?array $cache = null;
+
+    public function __construct(
+        private FindAllEditableLocalesForUser $findAllEditableLocalesForUser
+    ) {
+    }
+
+    public function findAll(int $userId): array
+    {
+        if (empty($this->cache[$userId])) {
+            $this->cache[$userId] = $this->findAllEditableLocalesForUser->findAll($userId);
+        }
+
+        return $this->cache[$userId];
+    }
+
+    public function clearCache(): void
+    {
+        $this->cache = null;
+    }
+}
