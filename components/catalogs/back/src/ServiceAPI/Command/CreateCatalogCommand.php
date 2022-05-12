70ded588828c14e2b4ade39d3afb0d8140b74682
Willy Mesnage <willy.mesnage@akeneo.com>
2022-05-12 18:00:42 +0200

    CXP-1182: Add CommandInterface

--- components/catalogs/back/src/ServiceAPI/Command/CreateCatalogCommand.php
+++ components/catalogs/back/src/ServiceAPI/Command/CreateCatalogCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class CreateCatalogCommand
+final class CreateCatalogCommand implements CommandInterface
 {
     public function __construct(
         #[Assert\NotBlank]
