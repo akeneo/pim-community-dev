4ca7b2697e1c033be9d8f7a2a410f54dfbb927fd
Willy Mesnage <willy.mesnage@akeneo.com>
2022-05-13 11:57:47 +0200

    Update components/catalogs/back/src/ServiceAPI/Query/GetCatalogQuery.php
    Co-authored-by: Quentin Favrie <quentin.favrie@akeneo.com>

--- components/catalogs/back/src/ServiceAPI/Query/GetCatalogQuery.php
+++ components/catalogs/back/src/ServiceAPI/Query/GetCatalogQuery.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
- * @implements QueryInterface<Catalog>
+ * @implements QueryInterface<Catalog|null>
  */
 final class GetCatalogQuery implements QueryInterface
 {
