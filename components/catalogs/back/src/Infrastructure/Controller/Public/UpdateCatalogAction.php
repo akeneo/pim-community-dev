aa6f987e745f119547e9d2be09371df1e82f53d4
Guillaume Fer <guillaume.fer@akeneo.com>
2022-05-16 18:00:40 +0200

    CXP-1184: UPDATE catalogs/{id} endpoint

--- components/catalogs/back/src/Infrastructure/Controller/Public/UpdateCatalogAction.php
+++ components/catalogs/back/src/Infrastructure/Controller/Public/UpdateCatalogAction.php
@@ -6,16 +6,16 @@ namespace Akeneo\Catalogs\Infrastructure\Controller\Public;
 
 use Akeneo\Catalogs\Infrastructure\Security\DenyAccessUnlessGrantedTrait;
 use Akeneo\Catalogs\Infrastructure\Security\GetCurrentUserIdTrait;
-use Akeneo\Catalogs\ServiceAPI\Command\CreateCatalogCommand;
+use Akeneo\Catalogs\ServiceAPI\Command\UpdateCatalogCommand;
 use Akeneo\Catalogs\ServiceAPI\Messenger\CommandBus;
 use Akeneo\Catalogs\ServiceAPI\Messenger\QueryBus;
 use Akeneo\Catalogs\ServiceAPI\Query\GetCatalogQuery;
 use Akeneo\Platform\Bundle\FrameworkBundle\Security\SecurityFacadeInterface;
 use Akeneo\Tool\Component\Api\Exception\ViolationHttpException;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Messenger\Exception\ValidationFailedException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -24,7 +24,7 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class CreateCatalogAction
+final class UpdateCatalogAction
 {
     use DenyAccessUnlessGrantedTrait;
     use GetCurrentUserIdTrait;
@@ -38,31 +38,45 @@ final class CreateCatalogAction
     ) {
     }
 
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, string $id): Response
     {
         $this->denyAccessUnlessGrantedToEditCatalogs();
+
+        try {
+            $catalog = $this->queryBus->execute(new GetCatalogQuery($id));
+        } catch (ValidationFailedException $e) {
+            throw $this->notFound($id);
+        }
+
+        $userId = $this->getCurrentUserId();
+        if (null === $catalog || $catalog->getOwnerId() !== $userId) {
+            throw $this->notFound($id);
+        }
 
         /** @var array{name?: string} $payload */
         $payload = \json_decode((string) $request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-        $catalogId = Uuid::uuid4()->toString();
-        $userId = $this->getCurrentUserId();
-
         try {
-            $this->commandBus->execute(new CreateCatalogCommand(
-                $catalogId,
-                $payload['name'] ?? '',
-                $userId,
+            $this->commandBus->execute(new UpdateCatalogCommand(
+                $id,
+                $payload['name'] ?? ''
             ));
         } catch (ValidationFailedException $e) {
             throw new ViolationHttpException($e->getViolations());
         }
 
-        $catalog = $this->queryBus->execute(new GetCatalogQuery($catalogId));
+        $catalog = $this->queryBus->execute(new GetCatalogQuery($id));
         if (null === $catalog) {
-            throw new \LogicException('The catalog must exist after its creation');
+            throw new \LogicException('The catalog must exist after its update');
         }
 
-        return new JsonResponse($this->normalizer->normalize($catalog, 'external_api'), Response::HTTP_CREATED);
+        return new JsonResponse($this->normalizer->normalize($catalog, 'external_api'), Response::HTTP_OK);
+    }
+
+    private function notFound(string $id): NotFoundHttpException
+    {
+        return new NotFoundHttpException(
+            \sprintf('Catalog "%s" does not exist or you can\'t access it.', $id)
+        );
     }
 }
--- components/catalogs/back/src/ServiceAPI/Command/UpdateCatalogCommand.php
+++ components/catalogs/back/src/ServiceAPI/Command/UpdateCatalogCommand.php
@@ -2,18 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Catalogs\ServiceAPI\Model;
+namespace Akeneo\Catalogs\ServiceAPI\Command;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class Catalog
+final class UpdateCatalogCommand implements CommandInterface
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
         private string $id,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 1, max: 255)]
         private string $name,
-        private int $ownerId,
     ) {
     }
 
@@ -25,19 +30,5 @@ final class Catalog
     public function getName(): string
     {
         return $this->name;
-    }
-
-    public function getOwnerId(): int
-    {
-        return $this->ownerId;
-    }
-
-    public function withNewName(string $name): self
-    {
-        return new self(
-            $this->id,
-            $name,
-            $this->ownerId,
-        );
     }
 }
