ff0737683afd628d055409adfaf850dd7bbe56cd
pierallard <pierallard@gmail.com>
2022-05-17 17:17:16 +0200

    [adr-uuid-doctrine] Fix rebase

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Domain/Query/ProductEvaluation/GetProductModelScoresQueryInterface.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Domain/Query/ProductEvaluation/GetProductModelScoresQueryInterface.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\DataQualityInsights\Domain\Query\ProductEvaluation;
 
+use Akeneo\Pim\Automation\DataQualityInsights\Domain\Model\Read;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductModelId;
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\ValueObject\ProductModelIdCollection;
-use Akeneo\Pim\Automation\DataQualityInsights\Domain\Model\Read;
 
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
