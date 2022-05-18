b1fae8454d4bc16b75d9ee2f616184b177ba08fe
ahocquard <alexandre.hocquard@akeneo.com>
2022-05-18 17:28:20 +0200

    Initialize the script to migrate GE instances with incremental progress

--- grth/migration_srnt_infra/migrate/backup_data_from_instance.sh
+++ grth/migration_srnt_infra/migrate/backup_data_from_instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euox pipefail
+set -euo pipefail
 
 if [[ -z "${SOURCE_PFID:-}" ]]; then
     echo "ERROR: environment variable SOURCE_PFID is not set." >&2
@@ -9,7 +9,7 @@ fi
 readonly MYSQL_SOURCE_POD=$(kubectl get pods --namespace=${SOURCE_PFID} -l component=mysql | awk '/mysql/ {print $1}')
 
 echo "Removing backup data from Bucket if it exists"
-gsutil ls gs://mig-ge-to-srnt/${SOURCE_PFID}/backup && gsutil rm -r gs://mig-ge-to-srnt/${SOURCE_PFID}/backup || echo "Nothing to delete."
+gsutil ls gs://mig-ge-to-srnt/${SOURCE_PFID}/backup && gsutil -m rm -r gs://mig-ge-to-srnt/${SOURCE_PFID}/backup || echo "Nothing to delete."
 
 echo "Generate Customer SQL dump"
 kubectl exec -i --namespace ${SOURCE_PFID} ${MYSQL_SOURCE_POD} -- /bin/bash -c 'mysqldump -u root -p$(cat /mysql_temp/root_password.txt) akeneo_pim | gzip' | gsutil cp - gs://mig-ge-to-srnt/${SOURCE_PFID}/backup/mysql/dump_customer_data_ge.sql.gz
