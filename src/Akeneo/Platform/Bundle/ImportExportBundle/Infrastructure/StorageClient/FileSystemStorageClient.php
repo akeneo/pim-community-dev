505facc041a2bf4a12941c877dfad665006c682d
Steven VAIDIE <steven.vaidie@gmail.com>
2022-05-19 09:57:07 +0200

    RAB-684: Fix review

--- src/Akeneo/Platform/Bundle/ImportExportBundle/Infrastructure/StorageClient/FileSystemStorageClient.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Infrastructure/StorageClient/FileSystemStorageClient.php
@@ -2,20 +2,14 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Akeneo PIM Enterprise Edition.
- *
- * (c) 2022 Akeneo SAS (https://www.akeneo.com)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 namespace Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\StorageClient;
 
-use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemOperator;
-use League\Flysystem\Local\LocalFilesystemAdapter;
 
 final class FileSystemStorageClient implements StorageClientInterface
 {
--- src/Akeneo/Platform/Bundle/ImportExportBundle/Infrastructure/StorageClient/StorageClientProvider.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/Infrastructure/StorageClient/StorageClientProvider.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Akeneo PIM Enterprise Edition.
- *
- * (c) 2022 Akeneo SAS (https://www.akeneo.com)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 namespace Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\StorageClient;
@@ -16,8 +12,6 @@ namespace Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\StorageClient
 use Akeneo\Platform\Bundle\ImportExportBundle\Application\TransferFilesToStorage\FileToTransfer;
 use Akeneo\Platform\Bundle\ImportExportBundle\Domain\Model\StorageInterface;
 use Akeneo\Tool\Component\FileStorage\FilesystemProvider;
-use League\Flysystem\Filesystem;
-use League\Flysystem\Local\LocalFilesystemAdapter;
 
 final class StorageClientProvider
 {
