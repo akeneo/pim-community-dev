0f94ca212361ad2fee06aa4382dabf04635cc03a
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-05-25 12:43:37 +0000

    Preparing version 6.0.29

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.28';
+    public const VERSION = '6.0.29';
 
     /** @staticvar string */
     public const VERSION_CODENAME = 'Buccaneer Bunny';
