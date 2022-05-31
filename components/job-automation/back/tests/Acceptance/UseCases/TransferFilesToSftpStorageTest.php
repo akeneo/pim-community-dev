86ef706f3ed4828b1b4459f16a742164f90fe97c
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-05-31 11:49:39 +0200

    RAB-687: add acceptance tests

---
+++ components/job-automation/back/tests/Acceptance/UseCases/TransferFilesToSftpStorageTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\JobAutomation\Test\Acceptance\UseCases;
+
+use Akeneo\Platform\Bundle\ImportExportBundle\Application\TransferFilesToStorage\FileToTransfer;
+use Akeneo\Platform\Bundle\ImportExportBundle\Application\TransferFilesToStorage\TransferFilesToStorageCommand;
+use Akeneo\Platform\Bundle\ImportExportBundle\Application\TransferFilesToStorage\TransferFilesToStorageHandler;
+use Akeneo\Platform\JobAutomation\Test\Acceptance\AcceptanceTestCase;
+use League\Flysystem\Filesystem;
+
+class TransferFilesToSftpStorageTest extends AcceptanceTestCase
+{
+    /**
+     * @test
+     */
+    public function itTransfersFilesToSftpStorage(): void
+    {
+        $this->getLocalFilesystem()->write('file_key1', 'file1 content');
+        $this->getCatalogFilesystem()->write('file_key2', 'file2 content');
+
+        $storage = [
+            'type' => 'sftp',
+            'file_path' => 'a_file_path',
+            'host' => 'localhost',
+            'port' => 22,
+            'username' => 'root',
+            'password' => 'root',
+        ];
+
+        $filesToTransfer = [
+            new FileToTransfer('file_key1', 'localFilesystem', 'filename1.csv'),
+            new FileToTransfer('file_key2', 'catalogStorage', 'filename2.csv'),
+        ];
+
+        $this->getHandler()->handle(new TransferFilesToStorageCommand($filesToTransfer, $storage));
+
+        $this->assertTrue($this->getSftpFilesystem()->fileExists('filename1.csv'));
+        $this->assertTrue($this->getSftpFilesystem()->fileExists('filename2.csv'));
+
+        $this->assertEquals('file1 content', $this->getSftpFilesystem()->read('filename1.csv'));
+        $this->assertEquals('file2 content', $this->getSftpFilesystem()->read('filename2.csv'));
+    }
+
+    private function getHandler(): TransferFilesToStorageHandler
+    {
+        return $this->get('Akeneo\Platform\Bundle\ImportExportBundle\Application\TransferFilesToStorage\TransferFilesToStorageHandler');
+    }
+
+    private function getCatalogFilesystem(): Filesystem
+    {
+        return $this->get('oneup_flysystem.catalog_storage_filesystem');
+    }
+
+    private function getLocalFilesystem(): Filesystem
+    {
+        return $this->get('oneup_flysystem.local_storage_filesystem');
+    }
+
+    private function getSftpFilesystem(): Filesystem
+    {
+        return $this->get('oneup_flysystem.sftp_storage_filesystem');
+    }
+}
