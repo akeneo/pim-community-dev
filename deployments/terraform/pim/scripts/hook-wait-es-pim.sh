7e3daca9887290f230443df6e95c5581505c4418
mstiri <mstiri@users.noreply.github.com>
2022-05-31 16:28:19 +0200

    SDO-33: Jsonify PIM jobs logs' (#13806)

--- deployments/terraform/pim/scripts/hook-wait-es-pim.sh
+++ deployments/terraform/pim/scripts/hook-wait-es-pim.sh
@@ -5,16 +5,22 @@ MAX_COUNTER=120
 COUNTER=1
 SLEEP_TIME=5
 
-echo "Checking Elasticsearch connectivity..."
-while ! curl --fail "http://elasticsearch-client:9200/_cluster/health?wait_for_status=yellow&timeout=1s&pretty"; do
-    curl "http://elasticsearch-client:9200/_cluster/health?pretty" || true
+log_json() {
+    currentDate=$(date +"%G-%m-%d %T.%N")
+    logWithMessage=$(printf '{"channel": "hook-wait-es-pim","context": {"message": "%s"},"datetime": {"date": "%s","timezone": "Etc/UTC","timezone_type": "3"}, "level_name": "%s"}' "$2" "${currentDate}" "$1")
+    echo "${logWithMessage}" >&2
+}
+
+log_json "INFO" "Checking Elasticsearch connectivity..."
+while ! curl --fail -s "http://elasticsearch-client:9200/_cluster/health?wait_for_status=yellow&timeout=1s"; do
+    curl -s "http://elasticsearch-client:9200/_cluster/health" || true
     COUNTER=$((${COUNTER} + 1))
     if [ ${COUNTER} -gt ${MAX_COUNTER} ]; then
         TIME_WAITED=$(( COUNTER*SLEEP_TIME ))
-        echo "We have been waiting for Elasticsearch for too long: ${TIME_WAITED} seconds; failing." >&2
+        log_json "WARNING" "We have been waiting for Elasticsearch for too long: ${TIME_WAITED} seconds; failing."
         exit 1
     fi;
     sleep ${SLEEP_TIME}
 done
 TIME_WAITED=$(( COUNTER*SLEEP_TIME ))
-echo "We have been waiting for Elasticsearch ${TIME_WAITED} seconds!"
+log_json "INFO" "We have been waiting for Elasticsearch ${TIME_WAITED} seconds!"
