8fb0ca5abb2f25dc9ec2c198e6294c26ed234647
Tseho <quentin.favrie@gmail.com>
2022-05-31 18:07:36 +0200

    test new translator in microfrontend

--- components/catalogs/front/src/components/CatalogList/CatalogList.tsx
+++ components/catalogs/front/src/components/CatalogList/CatalogList.tsx
@@ -1,6 +1,8 @@
 import React, {FC, PropsWithChildren} from 'react';
 import {List} from './components/List';
 import {ErrorBoundary} from '../ErrorBoundary';
+import {Badge} from 'akeneo-design-system';
+import {translate} from '@akeneo-pim-community/shared';
 
 type Props = {
     owner: string;
@@ -10,6 +12,7 @@ type Props = {
 const CatalogList: FC<PropsWithChildren<Props>> = ({owner, onCatalogClick}) => {
     return (
         <ErrorBoundary>
+            <Badge level='primary'>{translate('akeneo_connectivity.connection.connect.connected_apps.edit.tabs.settings')}</Badge>
             <List owner={owner} onCatalogClick={onCatalogClick} />
         </ErrorBoundary>
     );
