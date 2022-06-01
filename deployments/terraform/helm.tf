3027b89beab234afb0fa477a5b8fe6dc2d87b2bf
adesaegher <adesaegher@gmail.com>
2022-06-01 10:19:56 +0200

    Fix bad innodb_buffer_pool_size (#13884)

--- deployments/terraform/helm.tf
+++ deployments/terraform/helm.tf
@@ -239,7 +239,7 @@ resource "local_file" "helm_pim_config" {
         innodb_buffer_pool_size = format(
           "%d%s", ceil(
             trimsuffix(local.helm-default-values.mysql.mysql.innodbBufferPoolSize, local.mysql-memory-unit) +
-            758 * local.tshirt-size-index
+            1024 * local.tshirt-size-index
           ),
           local.mysql-memory-unit
         )
@@ -248,7 +248,7 @@ resource "local_file" "helm_pim_config" {
             memory = format(
               "%d%s", ceil(
                 trimsuffix(local.helm-default-values.mysql.mysql.resources.limits.memory, local.k8s-memory-unit) +
-                1024 * local.tshirt-size-index
+                1024 * local.tshirt-size-index * 130 / 100
               ),
               local.k8s-memory-unit
             )
