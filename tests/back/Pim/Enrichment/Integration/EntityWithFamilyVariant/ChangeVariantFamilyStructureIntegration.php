c261116c41394a129b928e612a478546b69e60ac
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-06-01 21:24:17 +0200

    PIM-10462: update integration tests

--- tests/back/Pim/Enrichment/Integration/EntityWithFamilyVariant/ChangeVariantFamilyStructureIntegration.php
+++ tests/back/Pim/Enrichment/Integration/EntityWithFamilyVariant/ChangeVariantFamilyStructureIntegration.php
@@ -3,7 +3,6 @@
 namespace AkeneoTest\Pim\Enrichment\Integration\EntityWithFamilyVariant;
 
 use Akeneo\Pim\Enrichment\Component\Product\Model\ValueInterface;
-use Akeneo\Pim\Structure\Component\Model\AttributeOptionInterface;
 use Akeneo\Test\Integration\TestCase;
 use Akeneo\Test\IntegrationTestsBundle\Jobs\JobExecutionObserver;
 use Akeneo\Test\IntegrationTestsBundle\Launcher\JobLauncher;
@@ -279,18 +278,42 @@ class ChangeVariantFamilyStructureIntegration extends TestCase
         );
     }
 
-    public function testItDoesNotRunBackgroundJobWhenAFamilyVariantHasNotChanged()
+    public function testItDoesNotRunBackgroundJobWhenAttributesHaveNotChanged(): void
     {
-        // is this test relevant?
+        $this->assertCount(
+            0,
+            $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
+        );
+
         $familyVariant = $this->get('pim_catalog.repository.family_variant')->findOneByIdentifier('shoes_size_color');
         $this->get('pim_catalog.saver.family_variant')->save($familyVariant);
+
         $this->assertCount(
-            1,
+            0,
             $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
         );
     }
 
-    public function testBulkMoveAnAttributeFromItsLevelDoesNotRunBackgroundJobs()
+    public function testItDoesNotRunBackgroundJobWhenAJobIsAlreadyCreated(): void
+    {
+        $this->assertCount(
+            0,
+            $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
+        );
+
+        $familyVariant = $this->get('pim_catalog.repository.family_variant')->findOneByIdentifier('shoes_size_color');
+        $this->get('pim_catalog.updater.family_variant')->update($familyVariant, [
+            'labels' => ['en_US' => 'test'],
+        ]);
+        $this->get('pim_catalog.saver.family_variant')->save($familyVariant);
+
+        $this->assertCount(
+            0,
+            $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
+        );
+    }
+
+    public function testBulkMoveAnAttributeFromItsLevelRunBackgroundJobs(): void
     {
         $product = $this->get('pim_catalog.repository.product')->findOneByIdentifier('1111111287');
         $this->assertInstanceOf(ValueInterface::class, $product->getValuesForVariation()->getByCodes('weight'));
@@ -316,11 +339,13 @@ class ChangeVariantFamilyStructureIntegration extends TestCase
         );
 
         $violationList = $this->get('validator')->validate($familyVariant);
-        if (0 !== $violationList->count()) {
-            throw new \LogicException('The family is not valid');
-        }
-
+        self::assertCount(0, $violationList, (string) $violationList);
         $this->get('pim_catalog.saver.family_variant')->saveAll([$familyVariant]);
+
+        $this->assertCount(
+            1,
+            $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
+        );
 
         while ($this->jobLauncher->hasJobInQueue()) {
             $this->jobLauncher->launchConsumerOnce();
@@ -328,15 +353,8 @@ class ChangeVariantFamilyStructureIntegration extends TestCase
 
         $this->get('doctrine.orm.default_entity_manager')->clear();
 
-        $product = $this->get('pim_catalog.repository.product')
-            ->findOneByIdentifier('1111111287');
-
-        $this->assertNotNull($product->getValuesForVariation()->getByCodes('weight'));
-
-        $this->assertCount(
-            0,
-            $this->jobExecutionObserver->jobExecutionsWithJobName('compute_family_variant_structure_changes')
-        );
+        $product = $this->get('pim_catalog.repository.product')->findOneByIdentifier('1111111287');
+        $this->assertNull($product->getValuesForVariation()->getByCodes('weight'));
     }
 
     protected function setUp(): void
@@ -345,6 +363,7 @@ class ChangeVariantFamilyStructureIntegration extends TestCase
 
         $this->purgeJobExecutions('compute_family_variant_structure_changes');
         $this->jobLauncher = $this->get('akeneo_integration_tests.launcher.job_launcher');
+        $this->jobLauncher->flushJobQueue();
         $this->jobExecutionObserver = $this->get(
             'akeneo_integration_tests.launcher.job_execution_observer'
         );
