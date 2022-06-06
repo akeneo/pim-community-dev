3b06c5a2a23b4574ce704356798677691440f492
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-06 11:12:30 +0200

    OBS-160: Add GetContributorAccountByAccessToken query + associated read model

---
+++ components/onboarder-retailer/back/tests/Unit/Domain/Authentication/ContributorAccount/Read/Model/ContributorAccountUnit.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Retailer\Test\Unit\Domain\Authentication\ContributorAccount\Read\Model;
+
+use Akeneo\OnboarderSerenity\Retailer\Domain\Authentication\ContributorAccount\Read\Model\ContributorAccount;
+use PHPUnit\Framework\TestCase;
+
+final class ContributorAccountUnit extends TestCase
+{
+    /** @test */
+    public function itCanBeNormalized(): void
+    {
+        $sut = new ContributorAccount('9f4c017c-7682-4f83-9099-dd9afcada1a2', 'foo', true);
+
+        static::assertSame(
+            ['id' => '9f4c017c-7682-4f83-9099-dd9afcada1a2', 'accessToken' => 'foo', 'isAccessTokenValid' => true],
+            $sut->toArray(),
+        );
+    }
+}
--- components/onboarder-supplier/back/src/Infrastructure/ContributorAccount/Authentication/Controller/SetUpPassword.php
+++ components/onboarder-supplier/back/src/Infrastructure/ContributorAccount/Authentication/Controller/SetUpPassword.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Akeneo\OnboarderSerenity\Supplier\Infrastructure\Authentication\ContributorAccount\Controller;
 
+use Akeneo\OnboarderSerenity\Retailer\Domain\Authentication\ContributorAccount\Read\GetContributorAccountIdByValidAccessToken;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class SetUpPassword
 {
+    public function __construct(private GetContributorAccountIdByValidAccessToken $isAccessTokenValid)
+    {
+    }
+
     public function __invoke(): JsonResponse
     {
         return new JsonResponse();
