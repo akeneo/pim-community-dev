300d72cc0ac7a87552f3768de07665a062972f6a
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-06 17:53:54 +0200

    OBS-162: Add ContributorAccountDoesNotExist exception

---
+++ components/onboarder-retailer/back/src/Application/Authentication/ContributorAccount/Exception/ContributorAccountDoesNotExist.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Retailer\Application\Authentication\ContributorAccount\Exception;
+
+final class ContributorAccountDoesNotExist extends \Exception
+{
+}
