7b6a500966a3b4cf560f278dc6d7c894b698158b
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-06 15:31:11 +0200

    OBS-161: Add a find method to the in memory implem of the ContributorAccount repository

--- components/onboarder-retailer/back/tests/Unit/Infrastructure/Authentication/ContributorAccount/Repository/InMemory/InMemoryRepositoryTest.php
+++ components/onboarder-retailer/back/tests/Unit/Infrastructure/Authentication/ContributorAccount/Repository/InMemory/InMemoryRepositoryTest.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Akeneo\OnboarderSerenity\Retailer\Test\Unit\Infrastructure\Authentication\ContributorAccount\Repository\InMemory;
 
 use Akeneo\OnboarderSerenity\Retailer\Domain\Authentication\ContributorAccount\Write\Model\ContributorAccount;
+use Akeneo\OnboarderSerenity\Retailer\Domain\Authentication\ContributorAccount\Write\ValueObject\Identifier;
 use Akeneo\OnboarderSerenity\Retailer\Infrastructure\Authentication\ContributorAccount\Repository\InMemory\InMemoryRepository;
 use PHPUnit\Framework\TestCase;
 
 final class InMemoryRepositoryTest extends TestCase
 {
     /** @test */
-    public function itSavesAndFindsAContributorAccount(): void
+    public function itSavesAndFindsAContributorAccountByItsEmail(): void
     {
         $contributorAccountRepository = new InMemoryRepository();
 
@@ -31,5 +32,36 @@ final class InMemoryRepositoryTest extends TestCase
         $contributorAccountRepository->save(ContributorAccount::fromEmail('momoss@example.com'));
 
         $this->assertNull($contributorAccountRepository->findByEmail('yolo@example.com'));
+    }
+
+    /** @test */
+    public function itFindsAContributorAccountByItsId(): void
+    {
+        $contributorAccountRepository = new InMemoryRepository();
+
+        $testContributor = ContributorAccount::fromEmail('test@example.com');
+        $contributorAccountRepository->save($testContributor);
+        $contributorAccountRepository->save(ContributorAccount::fromEmail('contributor@example.com'));
+
+        $contributorAccount = $contributorAccountRepository->find(
+            Identifier::fromString($testContributor->identifier()),
+        );
+
+        static::assertSame($contributorAccount->email(), 'test@example.com');
+    }
+
+    /** @test */
+    public function itReturnsNullIfAContributorAccountCannotBeFindByItsId(): void
+    {
+        $contributorAccountRepository = new InMemoryRepository();
+
+        $testContributor = ContributorAccount::fromEmail('test@example.com');
+        $contributorAccountRepository->save($testContributor);
+
+        $contributorAccount = $contributorAccountRepository->find(
+            Identifier::fromString('9e42ded3-1085-441e-b89c-b4681602ac10'),
+        );
+
+        static::assertNull($contributorAccount);
     }
 }
