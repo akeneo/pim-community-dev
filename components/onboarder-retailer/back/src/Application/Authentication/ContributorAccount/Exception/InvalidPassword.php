c475476853ed64ef621ff9118b6473a0853f8a57
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-07 16:21:50 +0200

    OBS-166: Add InvalidPassword exception

---
+++ components/onboarder-retailer/back/src/Application/Authentication/ContributorAccount/Exception/InvalidPassword.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Retailer\Application\Authentication\ContributorAccount\Exception;
+
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+final class InvalidPassword extends \Exception
+{
+    public function __construct(private ConstraintViolationListInterface $violations)
+    {
+        parent::__construct();
+    }
+
+    public function violations(): ConstraintViolationListInterface
+    {
+        return $this->violations;
+    }
+}
