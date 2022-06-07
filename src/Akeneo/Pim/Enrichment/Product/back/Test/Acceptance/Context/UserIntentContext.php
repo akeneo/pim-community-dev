5fef1cfa3a3df40e0098a143de89be332d5b6b79
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-06-07 12:01:06 +0200

    CPM-625: add tests for all attribute types

--- src/Akeneo/Pim/Enrichment/Product/back/Test/Acceptance/Context/UserIntentContext.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Test/Acceptance/Context/UserIntentContext.php
@@ -7,10 +7,24 @@ namespace Akeneo\Pim\Enrichment\Product\Test\Acceptance\Context;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\Association\ReplaceAssociatedGroups;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\Association\ReplaceAssociatedProductModels;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\Association\ReplaceAssociatedProducts;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\PriceValue;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\QuantifiedAssociation\QuantifiedEntity;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\QuantifiedAssociation\ReplaceAssociatedQuantifiedProductModels;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\QuantifiedAssociation\ReplaceAssociatedQuantifiedProducts;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetBooleanValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetDateValue;
 use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetFamily;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetFileValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetIdentifierValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetImageValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetMeasurementValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetMultiSelectValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetNumberValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetPriceCollectionValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetSimpleSelectValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetTableValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetTextareaValue;
+use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetTextValue;
 use Akeneo\Pim\Enrichment\Product\API\Query\GetUserIntentsFromStandardFormat;
 use Behat\Behat\Context\Context;
 use Symfony\Component\Messenger\Envelope;
@@ -67,16 +81,56 @@ final class UserIntentContext implements Context
                     'product_models' => [['identifier' => 'associated_product_model', 'quantity' => 21]],
                 ]
             ],
-//            'values' => [
-//                'name' => [['data' => 'Bonjour', 'scope' => null, 'locale' => null]],
-//                'measurement' => [['data' => ['amount' => 10, 'unit' => 'KILOGRAM'], 'scope' => 'e-commerce', 'locale' => 'fr_FR']],
-//            ],
+            'values' => [
+                'ean' => [['data' => '123456789', 'scope' => null, 'locale' => null]],
+                'name' => [['data' => 'Bonjour', 'scope' => null, 'locale' => null]],
+                'a_textarea' => [['data' => '<p>textarea text</p>', 'scope' => 'ecommerce', 'locale' => 'en_US']],
+                'a_boolean' => [
+                    ['data' => false, 'scope' => null, 'locale' => 'en_US'],
+                    ['data' => true, 'scope' => null, 'locale' => 'fr_FR'],
+                ],
+                'a_metric' => [['data' => ['amount' => 10, 'unit' => 'KILOGRAM'], 'scope' => 'ecommerce', 'locale' => 'fr_FR']],
+                'a_date' => [['data' => '2020-01-01', 'scope' => 'ecommerce', 'locale' => 'en_US']],
+                'a_file' => [['data' => 'a/b/file.pdf', 'scope' => 'mobile', 'locale' => null]],
+                'an_image' => [['data' => 'a/b/image.png', 'scope' => 'mobile', 'locale' => null]],
+                'a_number' => [['data' => '4.5000', 'scope' => null, 'locale' => null]],
+                'a_multiselect' => [['data' => ['codeA', 'codeB'], 'scope' => null, 'locale' => null]],
+                'a_simpleselect' => [['data' => 'the_code', 'scope' => null, 'locale' => 'en_US']],
+                'a_price' => [[
+                    'data' => [['amount' => '10.55', 'currency' => 'EUR'], ['amount' => '11', 'currency' => 'USD']],
+                    'scope' => null,
+                    'locale' => 'en_US',
+                ]],
+                'a_table' => [[
+                    'data' => [
+                        ['ingredient' => 'butter', 'quantity' => 2],
+                        ['ingredient' => 'salt'],
+                    ],
+                    'scope' => 'mobile',
+                    'locale' => null,
+                ]],
+            ],
         ]);
 
         $this->expectedResult = [
             new SetFamily('accessories'),
-//            new SetMeasurementValue('measurement', 'e-commerce', 'fr_FR', 10, 'KILOGRAM'),
-//            new SetTextValue('name', null, null, 'Bonjour'),
+            new SetIdentifierValue('ean', '123456789'),
+            new SetTextValue('name', null, null, 'Bonjour'),
+            new SetTextareaValue('a_textarea', 'ecommerce', 'en_US', '<p>textarea text</p>'),
+            new SetBooleanValue('a_boolean', null, 'en_US', false),
+            new SetBooleanValue('a_boolean', null, 'fr_FR', true),
+            new SetMeasurementValue('a_metric', 'ecommerce', 'fr_FR', 10, 'KILOGRAM'),
+            new SetDateValue('a_date', 'ecommerce', 'en_US', \DateTime::createFromFormat('Y-m-d', '2020-01-01')),
+            new SetFileValue('a_file', 'mobile', null, 'a/b/file.pdf'),
+            new SetImageValue('an_image', 'mobile', null, 'a/b/image.png'),
+            new SetNumberValue('a_number', null, null, '4.5000'),
+            new SetMultiSelectValue('a_multiselect', null, null, ['codeA', 'codeB']),
+            new SetSimpleSelectValue('a_simpleselect', null, 'en_US', 'the_code'),
+            new SetPriceCollectionValue('a_price', null, 'en_US', [new PriceValue('10.55', 'EUR'), new PriceValue('11', 'USD')]),
+            new SetTableValue('a_table', 'mobile', null, [
+                ['ingredient' => 'butter', 'quantity' => 2],
+                ['ingredient' => 'salt'],
+            ]),
             new ReplaceAssociatedProducts('PACK', ['associated_product']),
             new ReplaceAssociatedProductModels('PACK', []),
             new ReplaceAssociatedGroups('PACK', ['RELATED']),
@@ -108,7 +162,32 @@ final class UserIntentContext implements Context
         $handledStamp = $this->returnedEnvelope->last(HandledStamp::class);
         Assert::notNull($handledStamp, 'The command bus does not return any result');
 
-        Assert::eq($handledStamp->getResult(), $this->expectedResult);
+        $notFound = [];
+        $expectedResult = $this->expectedResult;
+        $results = $handledStamp->getResult();
+        foreach ($expectedResult as $expectedResultIndex => $expectedUserIntent) {
+            $found = false;
+            foreach ($results as $resultIndex => $userIntent) {
+                if ($userIntent == $expectedUserIntent) {
+                    $found = true;
+                    unset($this->expectedResult[$expectedResultIndex]);
+                    unset($results[$resultIndex]);
+                    break;
+                }
+            }
+            if (!$found) {
+                $notFound[] = $expectedUserIntent;
+            }
+        }
+
+        Assert::isEmpty(
+            $notFound,
+            \sprintf("These user intents are not found:\n%s\nResults were:\n%s\n", print_r($notFound, true), print_r($handledStamp->getResult(), true))
+        );
+        Assert::isEmpty(
+            $results,
+            \sprintf("There is some extra user intent:\n%s\nResults were:\n%s", print_r($results, true), print_r($handledStamp->getResult(), true))
+        );
     }
 
     private function dispatchToConverter(array $standardFormat): void
--- src/Akeneo/Pim/Enrichment/Product/back/Test/Feature/StandardFormat/convert_standard_format_to_user_intents.feature
+++ src/Akeneo/Pim/Enrichment/Product/back/Test/Feature/StandardFormat/convert_standard_format_to_user_intents.feature
@@ -6,9 +6,24 @@ Feature: Convert standard format to user intents
 
   Background:
     Given the following attributes:
-      | code                      | type                   |
-      | sku                       | pim_catalog_identifier |
-      | name                      | pim_catalog_text       |
+      | code                | type                               |
+      | sku                 | pim_catalog_identifier             |
+      | ean                 | pim_catalog_identifier             |
+      | name                | pim_catalog_text                   |
+      | a_boolean           | pim_catalog_boolean                |
+      | a_date              | pim_catalog_date                   |
+      | a_file              | pim_catalog_file                   |
+      | an_image            | pim_catalog_image                  |
+      | a_metric            | pim_catalog_metric                 |
+      | a_number            | pim_catalog_number                 |
+      | a_multiselect       | pim_catalog_multiselect            |
+      | a_simpleselect      | pim_catalog_simpleselect           |
+      | a_price             | pim_catalog_price_collection       |
+      | a_textarea          | pim_catalog_textarea               |
+      | a_record            | akeneo_reference_entity            |
+      | a_record_collection | akeneo_reference_entity_collection |
+      | an_asset_collection | pim_catalog_asset_collection       |
+      | a_table             | pim_catalog_table                  |
 
   Scenario: Convert successfully a complete standard format in user intents
     When I ask to convert standard format into user intents
@@ -18,8 +33,3 @@ Feature: Convert standard format to user intents
   Scenario: Error when bad family data
     When I ask to convert standard format with an invalid family data
     Then there is an exception with message: Property "family" expects a string as data, "array" given.
-
-  Scenario: Error when bad value formatting
-    When I ask to convert standard format into user intents
-    Then there is no exception
-    And I obtain all expected user intents
