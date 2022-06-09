55083155d6323d7c942486465dd4154399bf9143
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-06-09 15:23:11 +0200

    OBS-168: extract duplicated redirect logic in a dedicated function

--- components/onboarder-supplier/front/src/api/client.ts
+++ components/onboarder-supplier/front/src/api/client.ts
@@ -1,24 +1,22 @@
 import {QueryClient} from 'react-query';
 import {UnauthorizedError} from './UnauthorizedError';
 
+function redirectToLoginPage(error: unknown) {
+    if (error instanceof UnauthorizedError) {
+        window.location.href = '/login';
+    }
+}
+
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             retry: false,
             refetchOnWindowFocus: false,
-            onError: error => {
-                if (error instanceof UnauthorizedError) {
-                    window.location.href = '/login';
-                }
-            },
+            onError: redirectToLoginPage,
         },
         mutations: {
             retry: false,
-            onError: error => {
-                if (error instanceof UnauthorizedError) {
-                    window.location.href = '/login';
-                }
-            },
+            onError: redirectToLoginPage,
         },
     },
 });
