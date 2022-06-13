470685db6da77f331d107aed8369eb7b4ef6d394
Thomas Ramel <thomas@thomasramel.com>
2022-06-13 17:41:59 +0200

    CXP-1215: fix front coverage

--- components/catalogs/front/src/components/CatalogEdit/components/Settings.test.tsx
+++ components/catalogs/front/src/components/CatalogEdit/components/Settings.test.tsx
@@ -1,7 +1,9 @@
+import userEvent from '@testing-library/user-event';
+
 jest.unmock('./Settings');
 
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {act, render, screen} from '@testing-library/react';
 import {ThemeProvider} from 'styled-components';
 import {pimTheme} from 'akeneo-design-system';
 import {Settings} from './Settings';
@@ -16,4 +18,24 @@ test('it renders without error', () => {
     expect(screen.getByText('akeneo_catalogs.settings.fields.enabled')).toBeInTheDocument();
     expect(screen.getByText('akeneo_catalogs.settings.inputs.no')).toBeInTheDocument();
     expect(screen.getByText('akeneo_catalogs.settings.inputs.yes')).toBeInTheDocument();
+});
+
+test('it enables the catalog', () => {
+    render(
+        <ThemeProvider theme={pimTheme}>
+            <Settings />
+        </ThemeProvider>
+    );
+
+    act(() => {
+        userEvent.click(screen.getByText('akeneo_catalogs.settings.inputs.yes'));
+    });
+
+    expect(screen.getByText('akeneo_catalogs.settings.inputs.yes')).toHaveAttribute('value', 'true');
+
+    act(() => {
+        userEvent.click(screen.getByText('akeneo_catalogs.settings.inputs.no'));
+    });
+
+    expect(screen.getByText('akeneo_catalogs.settings.inputs.yes')).toHaveAttribute('value', 'false');
 });
