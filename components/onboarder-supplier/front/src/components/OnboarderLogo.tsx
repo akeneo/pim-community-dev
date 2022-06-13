84fb8d819ef76d71f8f7c3597e4935c8cd6da38a
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-06-13 19:01:59 +0200

    OBS-170: fix assets path

--- components/onboarder-supplier/front/src/components/OnboarderLogo.tsx
+++ components/onboarder-supplier/front/src/components/OnboarderLogo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import logoImage from "../assets/images/onboarderlogo.svg";
 
 const OnboarderLogo = () => {
-    return <Logo src="/assets/images/onboarderlogo.svg" />;
+    return <Logo src={logoImage} />;
 };
 
 const Logo = styled.img`
