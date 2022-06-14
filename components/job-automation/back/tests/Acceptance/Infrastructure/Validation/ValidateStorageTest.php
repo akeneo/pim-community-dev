e3832bd6465db16f09cb789b18208a641edc19d6
Steven VAIDIE <steven.vaidie@gmail.com>
2022-06-14 19:47:02 +0200

    Add tests

--- components/job-automation/back/tests/Acceptance/Infrastructure/Validation/ValidateStorageTest.php
+++ components/job-automation/back/tests/Acceptance/Infrastructure/Validation/ValidateStorageTest.php
@@ -3,25 +3,41 @@
 namespace Akeneo\Platform\JobAutomation\Test\Acceptance\Infrastructure\Validation;
 
 use Akeneo\Platform\Bundle\ImportExportBundle\Infrastructure\Validation\Storage;
+use Akeneo\Platform\JobAutomation\Infrastructure\Validation\Storage\Sftp\SftpStorage;
 use AkeneoTest\Platform\Acceptance\ImportExport\Infrastructure\Validation\AbstractValidationTest;
 
 class ValidateStorageTest extends AbstractValidationTest
 {
-    public function test_it_does_not_build_violations_when_storage_is_valid(): void
+    /**
+     * @dataProvider validSftpStorage
+     */
+    public function test_it_does_not_build_violations_when_sftp_storage_is_valid(array $value): void
     {
-        $violations = $this->getValidator()->validate(
-            [
-                'type' => 'sftp',
-                'file_path' => '/tmp/file.xlsx',
-                'host' => 'example.com',
-                'port' => 22,
-                'username' => 'ziggy',
-                'password' => 'MySecretPassword',
-            ],
-            new Storage(['xlsx', 'xls'])
-        );
+        $violations = $this->getValidator()->validate($value, new Storage(['xlsx', 'xls']));
 
         $this->assertNoViolation($violations);
+    }
+
+    public function validSftpStorage(): array
+    {
+        return [
+            'Valid local storage configuration' => [
+                [
+                    'type' => 'local',
+                    'file_path' => '/tmp/file.xlsx',
+                ],
+            ],
+            'Valid sftp storage configuration' => [
+                [
+                    'type' => 'sftp',
+                    'file_path' => '/tmp/file.xlsx',
+                    'host' => 'example.com',
+                    'port' => 22,
+                    'username' => 'ziggy',
+                    'password' => 'MySecretPassword',
+                ],
+            ],
+        ];
     }
 
     public function test_it_build_violations_when_storage_is_invalid(): void
