18b9e4c01a77646d9931811e17cebfccabdc69ce
jean-marie leroux <jmleroux.pro@gmail.com>
2022-06-14 08:27:03 +0200

    Hotfix CriterionEvaluationRepository

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Persistence/Repository/CriterionEvaluationRepository.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Persistence/Repository/CriterionEvaluationRepository.php
@@ -16,15 +16,10 @@ use Doctrine\DBAL\Exception\DeadlockException;
  */
 class CriterionEvaluationRepository
 {
-    private Connection $dbConnection;
-
-    private TransformCriterionEvaluationResultCodes $transformCriterionEvaluationResult;
-
-    public function __construct(Connection $dbConnection, TransformCriterionEvaluationResultCodes $transformCriterionEvaluationResult)
-    {
-        $this->dbConnection = $dbConnection;
-        $this->transformCriterionEvaluationResult = $transformCriterionEvaluationResult;
-    }
+    public function __construct(
+        private Connection $dbConnection, 
+        private TransformCriterionEvaluationResultCodes $transformCriterionEvaluationResult
+    ) {}
 
     public function createCriterionEvaluationsForProducts(Write\CriterionEvaluationCollection $criteriaEvaluations): void
     {
@@ -122,7 +117,7 @@ SQL;
     private function executeWithLock(string $query, array $queryParametersValues, array $queryParametersTypes): void
     {
         $value = $this->dbConnection->executeQuery('SELECT @@autocommit')->fetchAssociative();
-        if (!isset($value['@@autocommit']) && ((int) $value['@@autocommit'] !== 1 || (int) $value['@@autocommit'] !== 0)) {
+        if (!isset($value['@@autocommit']) || ((int) $value['@@autocommit'] !== 1 && (int) $value['@@autocommit'] !== 0)) {
             throw new \LogicException('Error when getting autocommit parameter from Mysql.');
         }
 
