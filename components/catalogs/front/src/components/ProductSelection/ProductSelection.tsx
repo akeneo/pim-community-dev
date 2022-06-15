94b60d59d9e869e795e3939d200211176344f9a9
Quentin Favrie <quentin.favrie@akeneo.com>
2022-06-15 18:00:20 +0200

    CXP-1196: Add and remove criteria (#17209)

--- components/catalogs/front/src/components/ProductSelection/ProductSelection.tsx
+++ components/catalogs/front/src/components/ProductSelection/ProductSelection.tsx
@@ -1,9 +1,18 @@
-import React, {FC, useState} from 'react';
-import {List} from 'akeneo-design-system';
-import {CriterionState} from './models/Criterion';
-import {useCatalogCriteria} from './hooks/useCatalogCriteria';
-import {Empty} from './components/Empty';
+import React, {FC, useCallback, useState} from 'react';
+import {getColor} from 'akeneo-design-system';
+import {Criterion, CriterionState} from './models/Criterion';
 import {Criteria} from './models/Criteria';
+import {useCatalogCriteria} from './hooks/useCatalogCriteria';
+import {AddCriterionDropdown} from './components/AddCriterionDropdown';
+import styled from 'styled-components';
+import {Empty} from './components/Empty';
+
+const Header = styled.div`
+    border-bottom: 1px solid ${getColor('grey', 60)};
+    display: flex;
+    justify-content: end;
+    padding: 10px 0;
+`;
 
 type Props = {
     id: string;
@@ -14,31 +23,52 @@ const ProductSelection: FC<Props> = ({id}) => {
 
     const [criteria, setCriteria] = useState<Criteria>(backend);
 
-    if (0 === criteria.length) {
-        return <Empty />;
-    }
+    const addCriterion = useCallback(
+        (criterion: Criterion<CriterionState>) => {
+            setCriteria(state => [...state, criterion]);
+        },
+        [setCriteria]
+    );
+
+    const updateCriterion = useCallback(
+        (criterion: Criterion<CriterionState>, newState: CriterionState) => {
+            setCriteria(state =>
+                state.map(old =>
+                    criterion.id !== old.id
+                        ? old
+                        : {
+                              ...old,
+                              state: newState,
+                          }
+                )
+            );
+        },
+        [setCriteria]
+    );
+
+    const removeCriterion = useCallback(
+        (criterion: Criterion<CriterionState>) => {
+            setCriteria(state => state.filter(old => old.id !== criterion.id));
+        },
+        [setCriteria]
+    );
+
+    const list = criteria.map(criterion => {
+        const Module = criterion.module;
+
+        const handleChange = (newState: CriterionState) => updateCriterion(criterion, newState);
+        const handleRemove = () => removeCriterion(criterion);
+
+        return <Module key={criterion.id} state={criterion.state} onChange={handleChange} onRemove={handleRemove} />;
+    });
 
     return (
-        <List>
-            {criteria.map(criterion => {
-                const Module = criterion.module;
-
-                const handleChange = (criterionState: CriterionState) => {
-                    setCriteria(state =>
-                        state.map(old =>
-                            criterion.id !== old.id
-                                ? old
-                                : {
-                                      ...old,
-                                      state: criterionState,
-                                  }
-                        )
-                    );
-                };
-
-                return <Module key={criterion.id} state={criterion.state} onChange={handleChange} />;
-            })}
-        </List>
+        <>
+            <Header>
+                <AddCriterionDropdown onNewCriterion={addCriterion} />
+            </Header>
+            {criteria.length ? list : <Empty />}
+        </>
     );
 };
 
