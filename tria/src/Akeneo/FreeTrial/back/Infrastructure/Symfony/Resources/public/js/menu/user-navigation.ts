f7a0e6ca670de992e148d407fbc873bddb7f6949
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-06-16 15:58:39 +0200

    GRF-65: Feature flag on invite user button

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/menu/user-navigation.ts
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Symfony/Resources/public/js/menu/user-navigation.ts
@@ -1,9 +1,14 @@
 import {InviteUserButton} from 'akeneo-pim-free-trial';
 
 const UserNavigation = require('pimui/js/menu/user-navigation');
+const FeatureFlags = require('pim/feature-flags');
 
 class FreeTrialUserNavigation extends UserNavigation {
   render() {
+    if (!FeatureFlags.isEnabled('free_trial')) {
+      return super.render();
+    }
+
     super.render();
 
     this.renderReact(InviteUserButton, {}, this.el.querySelector('#invite-user-btn'));
