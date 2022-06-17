7c58b6fd55dde16567154a12ad78c3cdea60eaab
LevFlavien <flavien.levesque@akeneo.com>
2022-06-17 11:48:31 +0200

    refact(API-1824): Get firewall config name from new Firewall service

--- src/Akeneo/Tool/Bundle/ApiBundle/EventSubscriber/ApiRequestLogSubscriber.php
+++ src/Akeneo/Tool/Bundle/ApiBundle/EventSubscriber/ApiRequestLogSubscriber.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Bundle\ApiBundle\EventSubscriber;
 
+use Akeneo\Tool\Bundle\ApiBundle\Security\Firewall;
 use Psr\Log\LoggerInterface;
-use Symfony\Bundle\SecurityBundle\Security\FirewallConfig;
-use Symfony\Bundle\SecurityBundle\Security\FirewallMap;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Http\FirewallMapInterface;
 
 class ApiRequestLogSubscriber implements EventSubscriberInterface
 {
-    private const API_FIREWALL = 'api';
-
     public function __construct(
-        private FirewallMapInterface $firewall,
+        private Firewall $firewall,
         private TokenStorageInterface $tokenStorage,
         private LoggerInterface $logger,
     ) {
@@ -41,7 +37,7 @@ class ApiRequestLogSubscriber implements EventSubscriberInterface
 
             $request = $event->getRequest();
 
-            if (!$this->isCurrentRequestInsideTheApiFirewall($request)) {
+            if (!$this->firewall->isCurrentRequestInsideTheApiFirewall()) {
                 return;
             }
 
@@ -68,27 +64,5 @@ class ApiRequestLogSubscriber implements EventSubscriberInterface
     private function getCurrentUrl(Request $request): string
     {
         return sprintf('%s%s', $request->getSchemeAndHttpHost(), $request->getPathInfo());
-    }
-
-    private function isCurrentRequestInsideTheApiFirewall(Request $request): bool
-    {
-        return self::API_FIREWALL === $this->getFirewallName($request);
-    }
-
-    private function getFirewallName(Request $request): ?string
-    {
-        // The method getFirewallConfig is only part of Symfony\Bundle\SecurityBundle\Security\FirewallMap,
-        // not in the FirewallMapInterface.
-        // In EE, we override the "@security.firewall.map" service with another class that is not extending
-        // Symfony\Bundle\SecurityBundle\Security\FirewallMap but still provide getFirewallConfig.
-        if ($this->firewall instanceof FirewallMap || method_exists($this->firewall, 'getFirewallConfig')) {
-            $firewallConfig = $this->firewall->getFirewallConfig($request);
-
-            if ($firewallConfig instanceof FirewallConfig) {
-                return $firewallConfig->getName();
-            }
-        }
-
-        return null;
     }
 }
--- src/Akeneo/Tool/Bundle/ApiBundle/EventSubscriber/ValidateApiRequestQueryParametersSubscriber.php
+++ src/Akeneo/Tool/Bundle/ApiBundle/EventSubscriber/ValidateApiRequestQueryParametersSubscriber.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Bundle\ApiBundle\EventSubscriber;
 
-use Symfony\Bundle\SecurityBundle\Security\FirewallConfig;
-use Symfony\Bundle\SecurityBundle\Security\FirewallMap;
+use Akeneo\Tool\Bundle\ApiBundle\Security\Firewall;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Security\Http\FirewallMapInterface;
 
 final class ValidateApiRequestQueryParametersSubscriber implements EventSubscriberInterface
 {
-    private const API_FIREWALL = 'api';
-
     public function __construct(
-        private FirewallMapInterface $firewall
+        private Firewall $firewall
     ) {
     }
 
@@ -37,7 +32,7 @@ final class ValidateApiRequestQueryParametersSubscriber implements EventSubscrib
 
         $request = $event->getRequest();
 
-        if (!$this->isCurrentRequestInsideTheApiFirewall($request)) {
+        if (!$this->firewall->isCurrentRequestInsideTheApiFirewall()) {
             return;
         }
 
@@ -52,27 +47,5 @@ final class ValidateApiRequestQueryParametersSubscriber implements EventSubscrib
                 throw new BadRequestHttpException('Bracket syntax is not supported in query parameters.');
             }
         }
-    }
-
-    private function isCurrentRequestInsideTheApiFirewall(Request $request): bool
-    {
-        return self::API_FIREWALL === $this->getFirewallName($request);
-    }
-
-    private function getFirewallName(Request $request): ?string
-    {
-        // The method getFirewallConfig is only part of Symfony\Bundle\SecurityBundle\Security\FirewallMap,
-        // not in the FirewallMapInterface.
-        // In EE, we override the "@security.firewall.map" service with another class that is not extending
-        // Symfony\Bundle\SecurityBundle\Security\FirewallMap but still provide getFirewallConfig.
-        if ($this->firewall instanceof FirewallMap || method_exists($this->firewall, 'getFirewallConfig')) {
-            $firewallConfig = $this->firewall->getFirewallConfig($request);
-
-            if ($firewallConfig instanceof FirewallConfig) {
-                return $firewallConfig->getName();
-            }
-        }
-
-        return null;
     }
 }
