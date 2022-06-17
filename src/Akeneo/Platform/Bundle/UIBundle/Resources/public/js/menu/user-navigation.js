2ffb5b8d97b387c2bde265a4c401011adadefb6c
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-06-17 15:39:19 +0200

    GRF-65: Feature flag on user navigation

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
@@ -20,6 +20,7 @@ define([
   'akeneo/communication-channel',
   'pim/media-url-generator',
   'pim/template/menu/user-navigation',
+  'pim/free-trial/template/menu/user-navigation',
   'pim/feature-flags',
 ], function (
   _,
@@ -31,11 +32,12 @@ define([
   CommunicationChannel,
   MediaUrlGenerator,
   template,
+  templateFreeTrial,
   FeatureFlags
 ) {
   return BaseForm.extend({
     className: 'AknTitleContainer-userMenu',
-    template: _.template(template),
+    template: _.template(!FeatureFlags.isEnabled('free_trial') ? template : templateFreeTrial),
     events: {
       'click .logout': 'logout',
       'click .user-account': 'userAccount',
