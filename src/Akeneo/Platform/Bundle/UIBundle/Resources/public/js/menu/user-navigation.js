8528c1562682e637823dcda35b161663d1b50266
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-06-20 17:03:24 +0200

    GRF-65: Fix free trial template on user navigation

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
@@ -20,7 +20,6 @@ define([
   'akeneo/communication-channel',
   'pim/media-url-generator',
   'pim/template/menu/user-navigation',
-  'pim/free-trial/template/menu/user-navigation',
   'pim/feature-flags',
 ], function (
   _,
@@ -32,12 +31,11 @@ define([
   CommunicationChannel,
   MediaUrlGenerator,
   template,
-  templateFreeTrial,
   FeatureFlags
 ) {
   return BaseForm.extend({
     className: 'AknTitleContainer-userMenu',
-    template: _.template(!FeatureFlags.isEnabled('free_trial') ? template : templateFreeTrial),
+    template: _.template(template),
     events: {
       'click .logout': 'logout',
       'click .user-account': 'userAccount',
@@ -48,6 +46,11 @@ define([
      */
     initialize: function (config) {
       this.config = config.config;
+
+      if (FeatureFlags.isEnabled('free_trial')) {
+        const freeTrialTemplate = require('pim/free-trial/template/menu/user-navigation');
+        this.template = _.template(freeTrialTemplate);
+      }
 
       BaseForm.prototype.initialize.apply(this, arguments);
     },
