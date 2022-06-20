5bb85747fba60df42d5fed26ff5e53afd00e04e3
Frederic Sarron <frederic.sarron@getakeneo.com>
2022-06-20 11:40:11 +0200

    category front unit test moving

--- src/Akeneo/Category/front/src/feature/helpers/permissionsHelper.test.ts
+++ src/Akeneo/Category/front/src/feature/helpers/permissionsHelper.test.ts
@@ -60,6 +60,7 @@ const formData: EditCategoryForm = {
   },
   errors: [],
   _token: {
+    //label: 'token',
     value: 'XFC_DnwJvzF5TsnB2-MbiPUjKqmUtZTJt0O1CTQLqMs',
     fullName: 'pim_category[_token]',
   },
@@ -80,6 +81,7 @@ const formData: EditCategoryForm = {
       choices: [],
     },
     apply_on_children: {
+      //label: '',
       value: '1',
       fullName: 'pim_category[permissions][apply_on_children]',
     },
