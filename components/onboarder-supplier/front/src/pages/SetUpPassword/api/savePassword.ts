84007f0cf654b77de76b61c6fa9e571ed1b99590
Philippe Mossiere <philippe.mossiere@akeneo.com>
2022-06-21 16:56:21 +0200

    OBS-171: remove `/v2` from onboarder-supplier routing

--- components/onboarder-supplier/front/src/pages/SetUpPassword/api/savePassword.ts
+++ components/onboarder-supplier/front/src/pages/SetUpPassword/api/savePassword.ts
@@ -1,7 +1,7 @@
 import {apiFetch} from '../../../api/apiFetch';
 
 const savePassword = async (data: {contributorAccountIdentifier: string; plainTextPassword: string}) => {
-    return await apiFetch(`/onboarder-supplier/v2/authentication/set-up-password`, {
+    return await apiFetch(`/onboarder-supplier/authentication/set-up-password`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
