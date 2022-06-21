260465d3fe8a552838fb385255d957d2c2c6e948
Willy Mesnage <willy.mesnage@akeneo.com>
2022-06-21 11:36:35 +0200

    CXP-1233: Fix lint and phpstan

--- components/catalogs/back/src/Infrastructure/Validation/Criteria.php
+++ components/catalogs/back/src/Infrastructure/Validation/Criteria.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Akeneo\Catalogs\Infrastructure\Validation;
 
-use Symfony\Component\Validator\Constraints\Compound;
+use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Compound;
 
 /**
  * @psalm-suppress PropertyNotSetInConstructor
@@ -13,6 +14,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[\Attribute]
 class Criteria extends Compound
 {
+    /**
+     * @param array<array-key, mixed> $options
+     *
+     * @return array<Constraint>
+     */
     protected function getConstraints(array $options): array
     {
         return [
