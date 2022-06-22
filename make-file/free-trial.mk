fb75ab69c04ad6f39475fbd042926c85504835f9
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-06-22 17:32:29 +0200

    GRF-65: Integrate Free Trial make command to launch tests

--- make-file/free-trial.mk
+++ make-file/free-trial.mk
@@ -11,3 +11,14 @@ trial-dev: #Doc: run docker-compose up, clean symfony cache, run webpack dev & i
 	cd $(PIM_SRC_PATH) && docker/wait_docker_up.sh
 	APP_ENV=dev O="--catalog $(DATABASE_CATALOG_FREE_TRIAL)" $(MAKE) database
 	APP_ENV=dev $(PHP_RUN) bin/console pim:user:create --admin -n -- admin admin admin@example.com John Doe en_US
+
+.PHONY: free-trial-unit-back
+free-trial-unit-back: #Doc: launch PHPSec for the free trial
+	$(DOCKER_COMPOSE) run --rm php sh -c "cd tria && php ../vendor/bin/phpspec run --config=phpspec.yml.dist $(O)"
+
+.PHONY: free-trial-integration-back
+free-trial-integration-back: #Doc: launch PHPUnit integration test for the free trial
+	APP_ENV=test ${PHP_RUN} vendor/bin/phpunit --configuration phpunit.xml.dist --testsuite FreeTrial_Integration_EE $(O)
+
+.PHONY: free-trial-tests
+free-trial-tests: free-trial-unit-back free-trial-integration-back
