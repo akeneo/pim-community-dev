719552e929099a94e64ce8e776ed9f6b79d7ecc3
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-06-22 19:32:30 +0200

    OBS-171: add unit test on useContributorAccount hook

--- components/onboarder-supplier/front/src/tests/index.tsx
+++ components/onboarder-supplier/front/src/tests/index.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import '@testing-library/jest-dom/extend-expect';
 import {render} from '@testing-library/react';
+import {renderHook} from '@testing-library/react-hooks';
 import {onboarderTheme} from 'akeneo-design-system';
 import {IntlProvider} from 'react-intl';
 import {MemoryRouter} from 'react-router-dom';
+import {ToastProvider} from '../utils/toaster';
+import {QueryClientProvider} from 'react-query';
+import {queryClient} from '../api';
 
 // eslint-disable-next-line
 const DefaultProviders = ({children}: any) => (
     <ThemeProvider theme={onboarderTheme}>
         <IntlProvider locale="en" defaultLocale="en" messages={{}}>
-            <MemoryRouter>{children}</MemoryRouter>
+            <ToastProvider>
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>{children}</MemoryRouter>
+                </QueryClientProvider>
+            </ToastProvider>
         </IntlProvider>
     </ThemeProvider>
 );
 const renderWithProviders = (ui: React.ReactElement) => render(ui, {wrapper: DefaultProviders});
 
-export {renderWithProviders};
+// eslint-disable-next-line
+const renderHookWithProviders = (hook: () => any) =>
+    renderHook(hook, {
+        // eslint-disable-next-line
+        wrapper: ({children}: any) => (
+            <ThemeProvider theme={onboarderTheme}>
+                <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+            </ThemeProvider>
+        ),
+    });
+
+export {renderWithProviders, renderHookWithProviders};
