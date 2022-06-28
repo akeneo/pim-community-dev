893b494aca4978214c732a1dc9e5762a07c8aa8e
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-28 15:27:12 +0200

    OBS-179: Add findByEmail method to contributor account repository

--- components/onboarder-supplier/back/src/Infrastructure/Authentication/ContributorAccount/Repository/Sql/DatabaseRepository.php
+++ components/onboarder-supplier/back/src/Infrastructure/Authentication/ContributorAccount/Repository/Sql/DatabaseRepository.php
@@ -6,6 +6,7 @@ namespace Akeneo\OnboarderSerenity\Supplier\Infrastructure\Authentication\Contri
 
 use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\ContributorAccountRepository;
 use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\Model\ContributorAccount;
+use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\ValueObject\Email;
 use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\ValueObject\Identifier;
 use Doctrine\DBAL\Connection;
 
@@ -68,5 +69,33 @@ class DatabaseRepository implements ContributorAccountRepository
             )
             : null
         ;
+    }
+
+    public function findByEmail(Email $email): ?ContributorAccount
+    {
+        $sql = <<<SQL
+            SELECT id, email, created_at, password, access_token, access_token_created_at, last_logged_at
+            FROM akeneo_onboarder_serenity_contributor_account
+            WHERE email = :email
+        SQL;
+
+        $result = $this
+            ->connection
+            ->executeQuery($sql, ['email' => $email])
+            ->fetchAssociative()
+        ;
+
+        return false !== $result
+            ? ContributorAccount::hydrate(
+                $result['id'],
+                $result['email'],
+                $result['created_at'],
+                $result['password'],
+                $result['access_token'],
+                $result['access_token_created_at'],
+                $result['last_logged_at'],
+            )
+            : null
+            ;
     }
 }
