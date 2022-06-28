68b4a8f4d674004a7ea502a58ee77f345c5fbbc0
Michel Tag <micheltag@akeneo.com>
2022-06-28 15:12:11 +0200

    Preparing version 6.0.32 (#17331)
    Preparing version 6.0.32
    
    
    Co-authored-by: BryanS29-akeneo <bryan.stephan@akeneo.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.31';
+    public const VERSION = '6.0.32';
 
     /** @staticvar string */
     public const VERSION_CODENAME = 'Buccaneer Bunny';
