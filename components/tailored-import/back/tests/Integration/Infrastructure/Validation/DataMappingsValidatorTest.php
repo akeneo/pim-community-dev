380da5c64263d0666748debb158048c71e2a07a1
Steven VAIDIE <steven.vaidie@gmail.com>
2022-06-28 16:40:59 +0200

    RAB-733: Prevent user to have data mappings to the same target (#14109)

--- components/tailored-import/back/tests/Integration/Infrastructure/Validation/DataMappingsValidatorTest.php
+++ components/tailored-import/back/tests/Integration/Infrastructure/Validation/DataMappingsValidatorTest.php
@@ -94,6 +94,64 @@ final class DataMappingsValidatorTest extends AbstractValidationTest
                     ]
                 ],
             ],
+            'valid data mapping on the same attribute but on different channel' => [
+                [
+                    [
+                        'uuid' => '018e1a5e-4d77-4a15-add8-f142111d4cd0',
+                        'target' => [
+                            'code' => 'sku',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_identifier',
+                            'channel' => null,
+                            'locale' => null,
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc69'
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => '018e1a5e-4d77-4a15-add8-f142111d4cd1',
+                        'target' => [
+                            'code' => 'a_localized_and_scopable_text_area',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_text',
+                            'channel' => 'ecommerce',
+                            'locale' => 'en_US',
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc63',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => 'ca37e4ad-a637-441c-8b88-7675dd83806e',
+                        'target' => [
+                            'code' => 'a_localized_and_scopable_text_area',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_text',
+                            'channel' => 'tablet',
+                            'locale' => 'fr_FR',
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc63',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                ],
+            ]
         ];
     }
 
@@ -155,6 +213,118 @@ final class DataMappingsValidatorTest extends AbstractValidationTest
                         'sources' => [
                             '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc63',
                             '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc03',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ]
+                ],
+            ],
+            'data mapping with the same attribute target' => [
+                'akeneo.tailored_import.validation.data_mappings.target_should_unique',
+                '[c401bbcc-fd42-4dfb-8d64-1c955e964ee3][target][code]',
+                [
+                    [
+                        'uuid' => '018e1a5e-4d77-4a15-add8-f142111d4cd0',
+                        'target' => [
+                            'code' => 'sku',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_identifier',
+                            'channel' => null,
+                            'locale' => null,
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc69'
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => 'a60c2c22-1611-46f6-8dfc-bdfeeb04c9c5',
+                        'target' => [
+                            'code' => 'a_text',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_text',
+                            'locale' => null,
+                            'channel' => null,
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc63',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => 'c401bbcc-fd42-4dfb-8d64-1c955e964ee3',
+                        'target' => [
+                            'code' => 'a_text',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_text',
+                            'locale' => null,
+                            'channel' => null,
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc03',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                ],
+            ],
+            'data mapping with the same property target' => [
+                'akeneo.tailored_import.validation.data_mappings.target_should_unique',
+                '[34db97c5-da4a-4bf9-ba2a-72972f79684d][target][code]',
+                [
+                    [
+                        'uuid' => '018e1a5e-4d77-4a15-add8-f142111d4cd0',
+                        'target' => [
+                            'code' => 'sku',
+                            'type' => 'attribute',
+                            'attribute_type' => 'pim_catalog_identifier',
+                            'channel' => null,
+                            'locale' => null,
+                            'action_if_not_empty' => 'set',
+                            'action_if_empty' => 'skip',
+                            'source_configuration' => null,
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc69'
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => '8cc8b642-f1ac-47b4-86f4-acc3ef67ed30',
+                        'target' => [
+                            'code' => 'categories',
+                            'type' => 'property',
+                            'action_if_not_empty' => 'add',
+                            'action_if_empty' => 'skip',
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc03',
+                        ],
+                        'operations' => [],
+                        'sample_data' => [],
+                    ],
+                    [
+                        'uuid' => '34db97c5-da4a-4bf9-ba2a-72972f79684d',
+                        'target' => [
+                            'code' => 'categories',
+                            'type' => 'property',
+                            'action_if_not_empty' => 'add',
+                            'action_if_empty' => 'skip',
+                        ],
+                        'sources' => [
+                            '9cecaeaf-d4d0-40be-9b78-53d5a1a5fc63',
                         ],
                         'operations' => [],
                         'sample_data' => [],
