186901c270aeea04d489cb7eca7be89d8c892d63
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-06-28 17:07:24 +0200

    GRF-65: Add akeneo_free_trial_invited_user table only on FT & remove useless migration script

--- tria/src/Akeneo/FreeTrial/back/Infrastructure/Install/EventSubscriber/InitFreeTrialDbSchemaSubscriber.php
+++ tria/src/Akeneo/FreeTrial/back/Infrastructure/Install/EventSubscriber/InitFreeTrialDbSchemaSubscriber.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Akeneo\FreeTrial\Infrastructure\Install\EventSubscriber;
 
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlags;
 use Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvent;
 use Akeneo\Platform\Bundle\InstallerBundle\Event\InstallerEvents;
 use Doctrine\DBAL\Connection;
@@ -20,11 +21,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class InitFreeTrialDbSchemaSubscriber implements EventSubscriberInterface
 {
-    private Connection $dbalConnection;
-
-    public function __construct(Connection $dbalConnection)
+    public function __construct(private Connection $dbalConnection, private FeatureFlags $featureFlags)
     {
-        $this->dbalConnection = $dbalConnection;
     }
 
     public static function getSubscribedEvents()
@@ -36,6 +34,10 @@ class InitFreeTrialDbSchemaSubscriber implements EventSubscriberInterface
 
     public function initDbSchema(InstallerEvent $event): void
     {
+        if (!$this->featureFlags->isEnabled('free_trial')) {
+            return;
+        }
+
         $query = <<<'SQL'
 CREATE TABLE IF NOT EXISTS akeneo_free_trial_invited_user (
     email VARCHAR(255) NOT NULL PRIMARY KEY,
