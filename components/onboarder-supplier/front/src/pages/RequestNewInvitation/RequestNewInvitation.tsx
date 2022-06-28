4fc31de854fbaea3840af01e438a50d42c3f7419
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-28 16:32:10 +0200

    OBS-179: Remove toaster and display static message when the user submit the request new invitation form

--- components/onboarder-supplier/front/src/pages/RequestNewInvitation/RequestNewInvitation.tsx
+++ components/onboarder-supplier/front/src/pages/RequestNewInvitation/RequestNewInvitation.tsx
@@ -1,53 +1,67 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useMutation} from 'react-query';
-import {Button, Field, TextInput} from 'akeneo-design-system';
+import {Button, Field, Helper, TextInput} from 'akeneo-design-system';
 import {OnboarderLogo, UnauthenticatedContainer} from '../../components';
 import {FormattedMessage} from 'react-intl';
 import {requestNewInvitation} from './api/requestNewInvitation';
-import {useToaster} from '../../utils/toaster';
-import {useIntl} from 'react-intl';
+import {BadRequestError} from '../../api/BadRequestError';
+import {NotFoundError} from '../../api/NotFoundError';
 
 const RequestNewInvitation = () => {
     const [email, setEmail] = useState('');
-    const notify = useToaster();
-    const intl = useIntl();
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
     const mutation = useMutation(requestNewInvitation);
 
     const submit = async (email: string) => {
+        setIsFormSubmitted(true);
         try {
             await mutation.mutateAsync({email: email});
-            notify(
-                intl.formatMessage({
-                    id: '6xalb8',
-                    defaultMessage:
-                        'Your new request invitation have been taken into account, you should have received a new email.',
-                }),
-                'success'
-            );
         } catch (error) {
-            console.error(error);
-            // @todo
+            if (error instanceof BadRequestError || error instanceof NotFoundError) {
+                setHasError(true);
+            }
         }
     };
 
     return (
         <UnauthenticatedContainer>
             <OnboarderLogo />
-            <InvitationHasExpiredMessage>
-                <FormattedMessage
-                    defaultMessage="Your invitation has expired. Please enter your email address to receive a new one."
-                    id="sYYIml"
-                />
-            </InvitationHasExpiredMessage>
-            <Field label={'Email'}>
-                <TextInput onChange={setEmail} value={email} data-testid="email-input" />
-            </Field>
 
-            <SubmitButton type="button" onClick={async () => await submit(email)}>
-                <FormattedMessage defaultMessage="Receive a new invitation" id="tq8W8G" />
-            </SubmitButton>
+            {!isFormSubmitted ? (
+                <>
+                    {hasError && (
+                        <Helper level="error">
+                            <FormattedMessage
+                                defaultMessage="An error occurred during the form submission, please try again."
+                                id="Z7xDKR"
+                            />
+                        </Helper>
+                    )}
+                    <InvitationHasExpiredMessage>
+                        <FormattedMessage
+                            defaultMessage="Your invitation has expired. Please enter your email address to receive a new one."
+                            id="sYYIml"
+                        />
+                    </InvitationHasExpiredMessage>
+                    <Field label={'Email'}>
+                        <TextInput onChange={setEmail} value={email} data-testid="email-input" />
+                    </Field>
+
+                    <SubmitButton type="button" onClick={async () => await submit(email)}>
+                        <FormattedMessage defaultMessage="Receive a new invitation" id="tq8W8G" />
+                    </SubmitButton>
+                </>
+            ) : (
+                <div>
+                    <FormattedMessage
+                        defaultMessage="An email will be send in a few moments. Please check your emails to access the service."
+                        id="SHw26+"
+                    />
+                </div>
+            )}
         </UnauthenticatedContainer>
     );
 };
