17ff84b74e8076a0c85d06baf42a98a82e8a4275
jean-marie leroux <jmleroux.pro@gmail.com>
2022-06-29 09:11:52 +0200

    PH-20: UCS - Do not persist tenant ID in JobMessage (#17284)
    * PH-20: Do not persist tenant ID in JobMessage
    
    * PH-20: Fix UCS middleware

--- src/Akeneo/Tool/Bundle/MessengerBundle/Middleware/UcsMiddleware.php
+++ src/Akeneo/Tool/Bundle/MessengerBundle/Middleware/UcsMiddleware.php
@@ -10,6 +10,10 @@ use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 
+/**
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
 class UcsMiddleware implements MiddlewareInterface
 {
     public function __construct(private ?string $pimTenantId)
@@ -18,15 +22,22 @@ class UcsMiddleware implements MiddlewareInterface
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        if (!$this->pimTenantId) {
-            return $stack->next()->handle($envelope, $stack);
+        // No tenant ID in the env, but existing in the received message
+        // We are in a daemon long-running process
+        if (null === $this->pimTenantId && null !== $envelope->last(TenantIdStamp::class)) {
+            /** @var TenantIdStamp $stamp */
+            $stamp = $envelope->last(TenantIdStamp::class);
+            $this->pimTenantId = $stamp->pimTenantId();
         }
 
-        if (null === $envelope->last(TenantIdStamp::class)) {
+        // Tenant ID exists in this env
+        // We are in a contextualized process
+        if ($this->pimTenantId && null === $envelope->last(TenantIdStamp::class)) {
             $envelope = $envelope->with(new TenantIdStamp($this->pimTenantId));
         }
 
-        if ($envelope->getMessage() instanceof TenantAwareInterface) {
+        // Enrich the message with the tenant ID
+        if ($this->pimTenantId && $envelope->getMessage() instanceof TenantAwareInterface) {
             $envelope->getMessage()->setTenantId($this->pimTenantId);
         }
 
