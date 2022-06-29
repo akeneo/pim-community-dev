2e96286cd03a7a8849600c5a40adb7af78399934
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-06-29 17:27:15 +0200

    RAB-754: move storage form inside CE and use it on every profiles

---
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/components/model.test.ts
@@ -0,0 +1,36 @@
+import {isValidStorageType, getDefaultStorage, isExport} from './model';
+
+test('it says if a storage type is valid', () => {
+  expect(isValidStorageType('none')).toBe(true);
+  expect(isValidStorageType('local')).toBe(true);
+  expect(isValidStorageType('sftp')).toBe(true);
+  expect(isValidStorageType('invalid')).toBe(false);
+});
+
+test('it returns the default local storage', () => {
+  expect(getDefaultStorage('export', 'local')).toEqual({
+    type: 'local',
+    file_path: '/tmp/export_%job_label%_%datetime%.xlsx',
+  });
+
+  expect(getDefaultStorage('export', 'sftp')).toEqual({
+    type: 'sftp',
+    file_path: 'export_%job_label%_%datetime%.xlsx',
+    host: '',
+    port: 22,
+    username: '',
+    password: '',
+  });
+
+  expect(getDefaultStorage('export', 'none')).toEqual({
+    type: 'none',
+  });
+
+  // @ts-expect-error invalid storage type
+  expect(() => getDefaultStorage('export', 'invalid')).toThrowError('Unknown storage type: invalid');
+});
+
+test('it says if a job is an export', () => {
+  expect(isExport('export')).toBe(true);
+  expect(isExport('import')).toBe(false);
+})
