4ccccc7387f87517d8f025fde0e61be5b808b1a5
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-30 14:50:22 +0200

    OBS-178: Add a ResetPasswordRequested domain event

---
+++ components/onboarder-supplier/back/src/Domain/Authentication/ContributorAccount/Write/Event/ResetPasswordRequested.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\Event;
+
+final class ResetPasswordRequested
+{
+    public function __construct(public string $contributorAccountEmail)
+    {
+    }
+}
---
+++ components/onboarder-supplier/back/tests/Unit/Domain/Authentication/ContributorAccount/Write/Event/ResetPasswordRequestedTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Supplier\Test\Unit\Domain\Authentication\ContributorAccount\Write\Event;
+
+use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\Event\ResetPasswordRequested;
+use PHPUnit\Framework\TestCase;
+
+final class ResetPasswordRequestedTest extends TestCase
+{
+    /** @test */
+    public function itOnlyContainsTheContributorAccountEmail(): void
+    {
+        $resetPasswordRequestedReflectionClass = new \ReflectionClass(ResetPasswordRequested::class);
+        $properties = $resetPasswordRequestedReflectionClass->getProperties();
+
+        static::assertCount(1, $properties);
+        static::assertSame(
+            'contributorAccountEmail',
+            $properties[0]->getName(),
+        );
+    }
+}
