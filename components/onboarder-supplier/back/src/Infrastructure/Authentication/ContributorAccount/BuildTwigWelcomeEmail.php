28d0bfc2418be5c5343de392490e0a18666c5f91
ValentinMerlet <merlet.valentin@gmail.com>
2022-06-30 16:07:59 +0200

    OBS-178: Move SET_UP_PASSWORD_URL constant from BuildTwigWelcomeEmail service to a class containing the constant (SetUpPasswordUrl) as it will be reused in another service

--- components/onboarder-supplier/back/src/Infrastructure/Authentication/ContributorAccount/BuildTwigWelcomeEmail.php
+++ components/onboarder-supplier/back/src/Infrastructure/Authentication/ContributorAccount/BuildTwigWelcomeEmail.php
@@ -6,12 +6,11 @@ namespace Akeneo\OnboarderSerenity\Supplier\Infrastructure\Authentication\Contri
 
 use Akeneo\OnboarderSerenity\Supplier\Domain\Authentication\ContributorAccount\Write\BuildWelcomeEmail;
 use Akeneo\OnboarderSerenity\Supplier\Domain\Mailer\ValueObject\EmailContent;
+use Akeneo\OnboarderSerenity\Supplier\Infrastructure\SetUpPasswordUrl;
 use Twig\Environment;
 
 class BuildTwigWelcomeEmail implements BuildWelcomeEmail
 {
-    private const SET_UP_PASSWORD_URL = '%s/onboarder/supplier/index.html/#/set-up-password/%s';
-
     public function __construct(
         private Environment $twig,
         private string $domain,
@@ -20,7 +19,7 @@ class BuildTwigWelcomeEmail implements BuildWelcomeEmail
 
     public function __invoke(string $accessToken, string $email): EmailContent
     {
-        $setUpPasswordUrl = sprintf(self::SET_UP_PASSWORD_URL, $this->domain, $accessToken);
+        $setUpPasswordUrl = sprintf(SetUpPasswordUrl::VALUE, $this->domain, $accessToken);
 
         $htmlContent = $this->twig->render(
             '@AkeneoOnboarderSerenitySupplier/Email/contributor-invitation.html.twig',
---
+++ components/onboarder-supplier/back/src/Infrastructure/SetUpPasswordUrl.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\OnboarderSerenity\Supplier\Infrastructure;
+
+final class SetUpPasswordUrl
+{
+    public const VALUE = '%s/onboarder/supplier/index.html/#/set-up-password/%s';
+}
