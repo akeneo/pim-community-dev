c2abc18cdd9cd6b1f6466ba8fa0bc18865558c51
Thomas Ramel <thomas@thomasramel.com>
2022-07-01 14:47:25 +0200

    OCT-79: update coupling dependencies

--- src/Akeneo/Connectivity/Connection/back/tests/.php_cd.php
+++ src/Akeneo/Connectivity/Connection/back/tests/.php_cd.php
@@ -151,6 +151,9 @@ $rules = [
             'Akeneo\Platform\Component\EventQueue\BulkEvent',
             'Akeneo\Platform\Bundle\PimVersionBundle\VersionProviderInterface',
 
+            'Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface',
+            'Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface',
+
             'Psr\Log\LoggerInterface',
             'Psr\Http\Message\ResponseInterface',
 
@@ -189,7 +192,12 @@ $rules = [
 
             'Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag',
 
+            'Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface',
+            'Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface',
+
             'Akeneo\UserManagement\Component\Model\RoleInterface',
+            'Akeneo\UserManagement\Component\Model\UserInterface',
+            'Akeneo\UserManagement\Component\Repository\UserRepositoryInterface',
 
             'Symfony\Component\EventDispatcher\EventDispatcherInterface',
         ]
