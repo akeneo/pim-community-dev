b780bf58b6cc31c0e5f28a526b0410e24347453c
anthony <12048285+MadeWilson@users.noreply.github.com>
2022-07-04 16:38:18 +0200

    RAB-792: Import/export error step in process tracker (#17342)
    * use step instead of subscriber to manage download and upload of file in import/export job
    
    * clean old implementation
    
    * add translations
    
    * fix lint
    
    * make service public in order to reach them in test
    
    * RAB-792: Fix copyright
    
    * add step to export and import xlsx product
    
    * RAB-792: Hide steps on already launched jobs
    
    Co-authored-by: Pierre Jolly <pierre.jolly@akeneo.com>
    Co-authored-by: Valentin Dijkstra <valentindijkstra@gmail.com>

--- src/Akeneo/Platform/Bundle/ImportExportBundle/DependencyInjection/PimImportExportExtension.php
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/DependencyInjection/PimImportExportExtension.php
@@ -28,7 +28,6 @@ class PimImportExportExtension extends Extension
         $loader->load('connector.yml');
         $loader->load('controllers.yml');
         $loader->load('datagrid_listeners.yml');
-        $loader->load('event_subscribers.yml');
         $loader->load('factory.yml');
         $loader->load('grid.yml');
         $loader->load('handlers.yml');
@@ -37,6 +36,7 @@ class PimImportExportExtension extends Extension
         $loader->load('queries.yml');
         $loader->load('repositories.yml');
         $loader->load('services.yml');
+        $loader->load('step.yml');
         $loader->load('storage_client.yml');
         $loader->load('validations.yml');
         $loader->load('widget.yml');
