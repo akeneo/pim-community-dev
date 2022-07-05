9aba15965d34bc6e65305d4e21756fc3241b853d
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-07-05 12:12:57 +0200

    SP-185: rename some routes, translations and commands to be consistent in naming

--- make-file/onboarder-serenity.mk
+++ make-file/onboarder-serenity.mk
@@ -78,7 +78,7 @@ unit-back-retailer: #Doc: Run unit back tests for the retailer part of Onboarder
 
 .PHONY: unit-back-supplier
 unit-back-supplier: #Doc: Run unit back tests for the supplier part of Onboarder Serenity
-	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Supplier_Portal_Unit_Test --configuration components/onboarder-supplier/back/tests/phpunit.xml.dist ${ARGS}
+	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Supplier_Portal_Supplier_Unit_Test --configuration components/onboarder-supplier/back/tests/phpunit.xml.dist ${ARGS}
 
 .PHONY: unit-back
 unit-back: unit-back-retailer unit-back-supplier #Doc: Run unit back tests for Onboarder Serenity
@@ -132,9 +132,9 @@ endif
 .PHONY: integration-back-supplier
 integration-back-supplier: #Doc: Run integration back tests for the supplier part of Onboarder Serenity
 ifeq ($(CI),true)
-	vendor/akeneo/pim-community-dev/.circleci/run_phpunit.sh . vendor/akeneo/pim-community-dev/.circleci/find_phpunit.php Supplier_Portal_Integration_Test
+	vendor/akeneo/pim-community-dev/.circleci/run_phpunit.sh . vendor/akeneo/pim-community-dev/.circleci/find_phpunit.php Supplier_Portal_Supplier_Integration_Test
 else
-	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Supplier_Portal_Integration_Test --configuration components/onboarder-supplier/back/tests/phpunit.xml.dist ${ARGS}
+	$(DOCKER_COMPOSE_RUN_PHP_TEST_ENV) vendor/bin/phpunit --testsuite Supplier_Portal_Supplier_Integration_Test --configuration components/onboarder-supplier/back/tests/phpunit.xml.dist ${ARGS}
 endif
 
 .PHONY: integration-back
