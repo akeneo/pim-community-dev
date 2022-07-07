0c867ec26db46b540f882759fe66b7866cf64d72
Philippe Mossiere <philippe.mossiere@akeneo.com>
2022-07-07 11:26:56 +0200

    SP-185: Fix entry path for `supplier-portal` supplier part

--- deployments-ucs/pim-saas-service/pim-web/scripts/copy-web-entrypoint-files-to-httpd-mouting-point.sh
+++ deployments-ucs/pim-saas-service/pim-web/scripts/copy-web-entrypoint-files-to-httpd-mouting-point.sh
@@ -6,6 +6,6 @@ cp -p -r -f /srv/pim/public/* /srv/pim/public/.htaccess /web-src/.
 
 #Onboarder serenity front build
 if [[ -d '/srv/pim/components/supplier-portal-supplier/front/build' ]];then
-  mkdir -p /web-src/supplier-portal/supplier/
-  cp -p -r -f /srv/pim/components/supplier-portal-supplier/front/build/* /web-src/supplier-portal/supplier/
+  mkdir -p /web-src/supplier-portal/
+  cp -p -r -f /srv/pim/components/supplier-portal-supplier/front/build/* /web-src/supplier-portal/
 fi
