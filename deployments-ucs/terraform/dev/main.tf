14621c4b03815817d0b406764b127fd493102f27
Gabriel Magniez <gabriel.magniez@getakeneo.com>
2022-07-11 18:15:40 +0200

    PH-9 GKE Cluster module for UCS (thanks to @JulienBaudon

--- deployments-ucs/terraform/dev/main.tf
+++ deployments-ucs/terraform/dev/main.tf
@@ -1,14 +1,29 @@
 locals {
-  project_id = "akecld-prd-pim-saas-dev"
-  ci_sa      = "main-service-account@${local.project_id}.iam.gserviceaccount.com"
-  admins     = ["group:phoenix-squad@akeneo.com"]
+  host_project_id = "akecld-prd-shared-vpc-dev"
+  shared_vpc_name = "akecld-prd-shared-vpc-dev-xpn"
+  project_id      = "akecld-prd-pim-saas-dev"
+  ci_sa           = "main-service-account@${local.project_id}.iam.gserviceaccount.com"
+  admins          = ["group:phoenix-squad@akeneo.com"]
+  viewers         = ["group:phoenix-squad@akeneo.com"]
+  regions         = ["europe-west1"]
 }
 
 module "registry" {
   source         = "../modules/registry"
   project_id     = local.project_id
   admin_members  = concat(["serviceAccount:${local.ci_sa}"], local.admins)
-  viewer_members = []
+  viewer_members = local.viewers
+}
+
+module "gke" {
+  source             = "../modules/gke"
+  project            = local.project_id
+  host_project_id    = local.host_project_id
+  shared_vpc_name    = local.shared_vpc_name
+  regions            = local.regions
+  cluster_developers = concat(["serviceAccount:${local.ci_sa}"], local.admins)
+  viewer_members     = local.viewers
+  admin_members      = local.admins
 }
 
 terraform {
