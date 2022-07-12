8fcfb19749112de3f8addff6f4ed5964a23190d5
Adam <101580975+adam-akeneo@users.noreply.github.com>
2022-07-12 16:47:50 +0200

    SDO-117 GE2EE Fully destroy old GE instances (#14166)
    * Fully destroy and remove old instances from Cloud-Customers
    * Some fixes for CI and Jenkins interoperability
    * Split cloud-customers file modifications from recreate_instance.sh as these instructions should not be skipable.

--- deployments/ge2srnt/migrate/restore_data.sh
+++ deployments/ge2srnt/migrate/restore_data.sh
@@ -11,20 +11,47 @@ if [[ -z "${TARGET_PFID:-}" ]]; then
     exit 1
 fi
 
-readonly MYSQL_TARGET_POD=$(kubectl get pods --namespace=${TARGET_PFID} -l component=mysql | awk '/mysql/ {print $1}')
+# Configure Kubeconfig via terraform target
+if [ ! -r ".kubeconfig" ]; then
+    terraform init
+    terraform apply -input=false -auto-approve -target=module.pim.local_file.kubeconfig
+fi
+export KUBECONFIG=.kubeconfig
+
+#Need to add retry loop because Helm finish before MySQL pod readiness - Retry every 5s for 120s
+RETRY_NUM=1
+RETRY_EVERY=5
+MYSQL_TARGET_POD=""
+
+while [[ -z $MYSQL_TARGET_POD ]]
+do
+    MYSQL_TARGET_POD=$(kubectl get pods --namespace=${TARGET_PFID} -l component=mysql -o jsonpath="{.items[0].metadata.name}" --ignore-not-found)
+
+    if [[ -z $MYSQL_TARGET_POD ]]
+    then
+        ((RETRY_NUM++))
+        echo "WARNING: Retrieving MYSQL_TARGET_POD was not successful after ${RETRY_NUM} tries...Retry in 5s"
+        sleep ${RETRY_EVERY}
+    fi
+    if [ ${RETRY_NUM} -eq 24 ]
+    then
+        echo "ERROR: Retrieve MYSQL_TARGET_POD was not successful after ${RETRY_NUM} tries"
+        exit 1
+    fi
+done
 
 echo "Load SQL dump"
-kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -e "drop database akeneo_pim; create database akeneo_pim"'
-gsutil cat gs://mig-ge-to-srnt/${SOURCE_PFID}/backup/mysql/dump_customer_data_ge.sql.gz | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -- /bin/bash -c 'gunzip | mysql -u root -p$(cat /mysql_temp/root_password.txt) akeneo_pim'
+kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -c mysql-server -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -e "drop database akeneo_pim; create database akeneo_pim"'
+gsutil cat gs://mig-ge-to-srnt/${SOURCE_PFID}/backup/mysql/dump_customer_data_ge.sql.gz | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -c mysql-server -- /bin/bash -c 'gunzip | mysql -u root -p$(cat /mysql_temp/root_password.txt) akeneo_pim'
 
 echo "Add missing tables"
-gsutil cat gs://mig-ge-to-srnt/dump_schema_ge_to_ee.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
+gsutil cat gs://mig-ge-to-srnt/dump_schema_ge_to_ee.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -c mysql-server -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
 
 echo "Add missing jobs"
-gsutil cat gs://mig-ge-to-srnt/dump_data_job_instance_ee.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
+gsutil cat gs://mig-ge-to-srnt/dump_data_job_instance_ee.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -c mysql-server -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
 
 echo "Add right for jobs, categories, locales and attributes to User Group 'All'"
-gsutil cat gs://mig-ge-to-srnt/add_missing_permissions.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
+gsutil cat gs://mig-ge-to-srnt/add_missing_permissions.sql | kubectl exec -i --namespace ${TARGET_PFID} ${MYSQL_TARGET_POD} -c mysql-server -- /bin/bash -c 'mysql -u root -p$(cat /mysql_temp/root_password.txt) -D akeneo_pim '
 
 echo "Add all missing doctrine migrations"
 readonly FPM_TARGET_POD=$(kubectl get pods --no-headers --namespace=${TARGET_PFID} -l component=pim-web | awk 'NR==1{print $1}')
