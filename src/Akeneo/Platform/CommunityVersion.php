e94b53416fba7c7ffc0bcbf816f950718f5da1a5
LevFlavien <flavien.levesque@akeneo.com>
2022-07-12 08:07:07 +0000

    Preparing version 6.0.35

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.34';
+    public const VERSION = '6.0.35';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
