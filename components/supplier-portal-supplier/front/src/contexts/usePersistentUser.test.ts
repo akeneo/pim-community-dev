92e6afa880f3e97fe852bb2c51f2f931ba09760b
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-07-13 18:09:50 +0200

    SP: check is the user is really authenticated when loading him from local storage

--- components/supplier-portal-supplier/front/src/contexts/usePersistentUser.test.ts
+++ components/supplier-portal-supplier/front/src/contexts/usePersistentUser.test.ts
@@ -1,11 +1,15 @@
-import {renderHook} from '@testing-library/react-hooks';
+import {act, renderHook} from '@testing-library/react-hooks';
 import {usePersistentUser, User} from './usePersistentUser';
+import {apiFetch} from '../api/apiFetch';
+import {UnauthorizedError} from '../api';
 
 const user: User = {email: 'burger@example.com'};
 
 afterEach(() => {
     localStorage.clear();
 });
+
+jest.mock('../api/apiFetch');
 
 test('it returns no user by default', () => {
     const {result} = renderHook(() => usePersistentUser());
@@ -14,9 +18,14 @@ test('it returns no user by default', () => {
     expect(localStorage.getItem('supplier-portal-contributor-account')).toBeNull();
 });
 
-test('it is able to load a user from local storage', () => {
+test('it is able to load a user from local storage', async () => {
+    apiFetch.mockResolvedValue({});
+
     localStorage.setItem('supplier-portal-contributor-account', JSON.stringify(user));
-    const {result} = renderHook(() => usePersistentUser());
+    const {result, waitForNextUpdate} = renderHook(() => usePersistentUser());
+    await act(async () => {
+        await waitForNextUpdate();
+    });
     expect(result.current.isInitialized).toBe(true);
     expect(result.current.user).toStrictEqual(user);
 });
@@ -34,11 +43,15 @@ test('it can persist the user in the local storage', async () => {
 });
 
 test('it can remove a logged out user from the local storage', async () => {
+    apiFetch.mockRejectedValue(new UnauthorizedError());
+
     localStorage.setItem('supplier-portal-contributor-account', JSON.stringify(user));
 
-    const {result, waitFor} = renderHook(() => usePersistentUser());
-    expect(result.current.isInitialized).toBe(true);
-    expect(result.current.user).toStrictEqual(user);
+    const {result, waitFor, waitForNextUpdate} = renderHook(() => usePersistentUser());
+
+    await act(async () => {
+        await waitForNextUpdate();
+    });
 
     result.current.updateUser(null);
     expect(result.current.user).toBeNull();
--- components/supplier-portal-supplier/front/src/contexts/usePersistentUser.ts
+++ components/supplier-portal-supplier/front/src/contexts/usePersistentUser.ts
@@ -1,4 +1,6 @@
 import {useEffect, useState} from 'react';
+import {apiFetch} from '../api/apiFetch';
+import {UnauthorizedError} from '../api';
 
 type User = {
     email: string;
@@ -11,12 +13,28 @@ const usePersistentUser = () => {
     const [isInitialized, setIsInitialized] = useState(false);
 
     useEffect(() => {
-        const contributorAccount = localStorage.getItem(localStorageKey);
-        if (null !== contributorAccount) {
-            const user = JSON.parse(contributorAccount);
-            null !== user && updateUser(user);
-        }
-        setIsInitialized(true);
+        (async () => {
+            const contributorAccount = localStorage.getItem(localStorageKey);
+            if (null !== contributorAccount) {
+                const user = JSON.parse(contributorAccount);
+                if (null !== user) {
+                    try {
+                        await apiFetch(`/supplier-portal/check-authentication`, {
+                            method: 'POST',
+                            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                            body: `email=${user.email}`,
+                        });
+                        updateUser(user);
+                    } catch (error) {
+                        if (error instanceof UnauthorizedError) {
+                            localStorage.removeItem(localStorageKey);
+                            updateUser(null);
+                        }
+                    }
+                }
+            }
+            setIsInitialized(true);
+        })();
     }, [updateUser]);
 
     useEffect(() => {
