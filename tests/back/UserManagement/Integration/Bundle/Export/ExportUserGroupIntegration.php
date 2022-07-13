ab8c66394b72d20fc5d59fdd446450ac0e9e4720
Steven VAIDIE <steven.vaidie@gmail.com>
2022-07-13 20:02:51 +0200

    RAB-753: Fix review

--- tests/back/UserManagement/Integration/Bundle/Export/ExportUserGroupIntegration.php
+++ tests/back/UserManagement/Integration/Bundle/Export/ExportUserGroupIntegration.php
@@ -29,7 +29,7 @@ final class ExportUserGroupIntegration extends TestCase
             'job_name' => static::CSV_EXPORT_JOB_CODE,
             'status' => 0,
             'type' => 'export',
-            'raw_parameters' => 'a:5:{s:9:"delimiter";s:1:";";s:9:"enclosure";s:1:""";s:10:"withHeader";b:1;s:14:"user_to_notify";N;s:21:"is_user_authenticated";b:0;}',
+            'raw_parameters' => 'a:6:{s:7:"storage";a:2:{s:4:"type";s:5:"local";s:9:"file_path";s:38:"/tmp/export_%job_label%_%datetime%.csv";}s:9:"delimiter";s:1:";";s:9:"enclosure";s:1:""";s:10:"withHeader";b:1;s:14:"user_to_notify";N;s:21:"is_user_authenticated";b:0;}',
         ]);
         $this->get(SqlCreateJobInstance::class)->createJobInstance([
             'code' => static::XLSX_EXPORT_JOB_CODE,
@@ -37,7 +37,7 @@ final class ExportUserGroupIntegration extends TestCase
             'job_name' => static::XLSX_EXPORT_JOB_CODE,
             'status' => 0,
             'type' => 'export',
-            'raw_parameters' => 'a:4:{s:10:"withHeader";b:1;s:12:"linesPerFile";i:10000;s:14:"user_to_notify";N;s:21:"is_user_authenticated";b:0;}',
+            'raw_parameters' => 'a:5:{s:7:"storage";a:2:{s:4:"type";s:5:"local";s:9:"file_path";s:39:"/tmp/export_%job_label%_%datetime%.xlsx";}s:10:"withHeader";b:1;s:12:"linesPerFile";i:10000;s:14:"user_to_notify";N;s:21:"is_user_authenticated";b:0;}',
         ]);
     }
 
