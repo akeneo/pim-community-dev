eefb7d9ddf38e581304c27f0851563adab26c118
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-07-18 15:30:48 +0200

    SP-115: add a dummy controller for the file dropping

---
+++ components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Controller/UploadProductFile.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\SupplierPortal\Supplier\Infrastructure\ProductFileDropping\Controller;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+final class UploadProductFile
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        /** @var $file UploadedFile */
+        $file = $request->files->has('file') ? $request->files->get('file') : null;
+
+        if (null === $file) {
+            return new JsonResponse(null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            //Call handler
+        } catch (\Exception) {
+            return new JsonResponse(['error' => 'Sample error message'], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return new JsonResponse(null, Response::HTTP_ACCEPTED);
+    }
+}
