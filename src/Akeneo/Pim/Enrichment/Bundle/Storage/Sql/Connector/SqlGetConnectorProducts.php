9e195e957926ba94e6cd97030e476a3e36c6adbd
Pierre Maraitre <pierre.maraitre@akeneo.com>
2022-07-19 11:15:02 +0200

    Fix review
    Co-authored-by: Pierre Allard <pierallard@gmail.com>

--- src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Connector/SqlGetConnectorProducts.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Connector/SqlGetConnectorProducts.php
@@ -145,7 +145,7 @@ class SqlGetConnectorProducts implements Query\GetConnectorProducts
 
         $products = [];
         foreach ($productUuids as $productUuid) {
-            if (!isset($rows[$productUuid->toString()]['uuid'])) {
+            if (!isset($rows[$productUuid->toString()])) {
                 continue;
             }
             $row = $rows[$productUuid->toString()];
