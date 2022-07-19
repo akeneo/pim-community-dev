d479c9b7c369e8f849e44f479967861debbd869b
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-07-19 17:42:17 +0200

    Revert "RAB-782: Add a public api to search families"
    This reverts commit b86edc093301f6c13db54b44a3b87c6adfbba5d4.

--- src/Akeneo/Pim/Structure/Component/Query/PublicApi/Family/SearchFamiliesInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Akeneo\Pim\Structure\Component\Query\PublicApi\Family;
-
-interface SearchFamiliesInterface
-{
-    public function search(SearchFamiliesParameters $searchFamiliesParameters): SearchFamiliesResult;
-}
