6320f75b5adbf7ad5c3416f1e6df1df20c24d8d1
Adrien Migaire <adrien.migaire@akeneo.com>
2022-07-20 15:17:11 +0200

    GRF-68 fix lint error

--- src/Akeneo/Pim/Enrichment/Product/back/Test/Acceptance/Context/CategoryContext.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Test/Acceptance/Context/CategoryContext.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Enrichment\Product\Test\Acceptance\Context;
 
+use Akeneo\Category\Infrastructure\Component\Model\Category;
 use Akeneo\Pim\Enrichment\Category\Test\Acceptance\InMemory\InMemoryGetOwnedCategories;
-use Akeneo\Pim\Enrichment\Component\Category\Model\Category;
 use Akeneo\Test\Acceptance\Category\InMemoryCategoryRepository;
 use Behat\Behat\Context\Context;
 
