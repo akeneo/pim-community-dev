c4d5089b04f2e794801ebaffefe32824d69bf2b8
ahocquard <alexandre.hocquard@akeneo.com>
2022-07-20 15:23:36 +0200

    TIP-1597: test that a job execution is not visible if the feature is disabled

--- src/Akeneo/Tool/Bundle/BatchBundle/tests/Integration/Job/DoctrineJobRepositoryIntegration.php
+++ src/Akeneo/Tool/Bundle/BatchBundle/tests/Integration/Job/DoctrineJobRepositoryIntegration.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Bundle\BatchBundle\tests\Integration\Job;
 
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
+use Akeneo\Platform\Bundle\ImportExportBundle\Repository\InternalApi\JobExecutionRepository;
+use Akeneo\Test\IntegrationTestsBundle\Launcher\JobLauncher;
 use Akeneo\Tool\Bundle\BatchBundle\Job\DoctrineJobRepository;
 use Akeneo\Tool\Bundle\BatchBundle\Job\JobInstanceRepository;
 use Akeneo\Tool\Component\Batch\Job\BatchStatus;
@@ -11,16 +14,18 @@ use Akeneo\Tool\Component\Batch\Job\ExitStatus;
 use Akeneo\Tool\Component\Batch\Job\JobParameters;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
+use Akeneo\Tool\Component\Batch\Model\JobInstance;
 use Akeneo\Tool\Component\Batch\Model\StepExecution;
 use Akeneo\Tool\Component\Batch\Model\Warning;
+use Doctrine\ORM\EntityManager;
 
 class DoctrineJobRepositoryIntegration extends TestCase
 {
-    public function testCreateJobExecutionWithJobParameters()
+    public function test_create_job_execution_with_job_parameters()
     {
-        $jobInstanceCode = 'csv_product_quick_export';
-        $jobInstance = $this->getJobInstanceRepository()->findOneByIdentifier($jobInstanceCode);
-        $job = $this->get('akeneo_batch.job.job_registry')->get($jobInstanceCode);
+        $jobInstance = $this->createJobInstance('stoppable_dumb_job');
+
+        $job = $this->get('akeneo_batch.job.job_registry')->get('stoppable_dumb_job');
         $jobParameters =  new JobParameters(['foo' => 'bar']);
         $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
 
@@ -31,19 +36,21 @@ class DoctrineJobRepositoryIntegration extends TestCase
             'exit_code' => 'UNKNOWN',
             'raw_parameters' => '{"foo": "bar"}',
             'is_stoppable' => '1',
+            'is_visible' => '1',
         ];
 
         $this->assertEquals($expectedResult['status'], $result['status']);
         $this->assertEquals($expectedResult['exit_code'], $result['exit_code']);
         $this->assertJsonStringEqualsJsonString($expectedResult['raw_parameters'], $result['raw_parameters']);
         $this->assertEquals($expectedResult['is_stoppable'], $result['is_stoppable']);
+        $this->assertEquals($expectedResult['is_visible'], $result['is_visible']);
     }
 
-    public function testCreateNonStoppableJobExecution()
+
+    public function test_create_non_stoppable_job_execution()
     {
-        $jobInstanceCode = 'clean_removed_attribute_job';
-        $jobInstance = $this->getJobInstanceRepository()->findOneByIdentifier($jobInstanceCode);
-        $job = $this->get('akeneo_batch.job.job_registry')->get($jobInstanceCode);
+        $jobInstance = $this->createJobInstance('non_stoppable_dumb_job');
+        $job = $this->get('akeneo_batch.job.job_registry')->get('non_stoppable_dumb_job');
 
         $jobParameters =  new JobParameters([]);
         $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
@@ -53,11 +60,11 @@ class DoctrineJobRepositoryIntegration extends TestCase
         $this->assertEquals('0', $result['is_stoppable']);
     }
 
-    public function testGetLastJobExecutionWithJobParameters()
+    public function test_get_last_job_execution_with_job_parameters()
     {
-        $jobInstanceCode = 'csv_product_quick_export';
-        $jobInstance = $this->getJobInstanceRepository()->findOneByIdentifier($jobInstanceCode);
-        $job = $this->get('akeneo_batch.job.job_registry')->get($jobInstanceCode);
+        $jobInstance = $this->createJobInstance('stoppable_dumb_job');
+        $job = $this->get('akeneo_batch.job.job_registry')->get('stoppable_dumb_job');
+
         $jobParameters =  new JobParameters(['foo' => 'bar']);
         $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
 
@@ -72,11 +79,10 @@ class DoctrineJobRepositoryIntegration extends TestCase
         $this->assertEquals(['foo' => 'bar'], $lastJobExecution->getJobParameters()->all());
     }
 
-    public function testAddWarnings()
+    public function test_add_warnings()
     {
-        $jobInstanceCode = 'csv_product_quick_export';
-        $jobInstance = $this->getJobInstanceRepository()->findOneByIdentifier($jobInstanceCode);
-        $job = $this->get('akeneo_batch.job.job_registry')->get($jobInstanceCode);
+        $jobInstance = $this->createJobInstance('stoppable_dumb_job');
+        $job = $this->get('akeneo_batch.job.job_registry')->get('stoppable_dumb_job');
         $jobParameters =  new JobParameters([]);
         $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
 
@@ -113,6 +119,44 @@ class DoctrineJobRepositoryIntegration extends TestCase
         $this->assertEquals(['name' => 'Invalid item 0'], $firstWarning->getItem());
     }
 
+    public function test_job_execution_is_visible_when_feature_of_the_job_is_activated()
+    {
+        $this->addNewFeatureFlag('coffee_maker');
+        $this->get('feature_flags')->enable('coffee_maker');
+
+        $jobInstance = $this->createJobInstance('visible_coffee_maker_job');
+
+        $job = $this->get('akeneo_batch.job.job_registry')->get('visible_coffee_maker_job');
+
+        $jobParameters =  new JobParameters([]);
+        $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
+
+        $result = $this->selectJobExecution($jobExecution->getId());
+
+        $this->assertEquals('1', $result['is_visible']);
+    }
+
+    /**
+     * A job is not visible if the feature is not activated. If the feature is activated afterward, due to an upgrade in the offer,
+     * then the job executed before the activation should still be not visible.
+     */
+    public function test_that_job_execution_is_not_visible_when_feature_of_the_job_is_not_activated()
+    {
+        $this->addNewFeatureFlag('coffee_maker');
+        $this->get('feature_flags')->disable('coffee_maker');
+
+        $jobInstance = $this->createJobInstance('visible_coffee_maker_job');
+
+        $job = $this->get('akeneo_batch.job.job_registry')->get('visible_coffee_maker_job');
+
+        $jobParameters =  new JobParameters([]);
+        $jobExecution = $this->getDoctrineJobRepository()->createJobExecution($job, $jobInstance, $jobParameters);
+
+        $result = $this->selectJobExecution($jobExecution->getId());
+
+        $this->assertEquals('0', $result['is_visible']);
+    }
+
     protected function getDoctrineJobRepository(): DoctrineJobRepository
     {
         return $this->get('akeneo_batch.job_repository');
@@ -128,7 +172,7 @@ class DoctrineJobRepositoryIntegration extends TestCase
      */
     protected function getConfiguration()
     {
-        return $this->catalog->useTechnicalSqlCatalog();
+        return $this->catalog->useMinimalCatalog();
     }
 
     private function selectJobExecution(int $id): array {
@@ -138,5 +182,27 @@ class DoctrineJobRepositoryIntegration extends TestCase
         $stmt->execute();
         return $stmt->fetch();
 
+    }
+
+    private function createJobInstance(string $jobName): JobInstance
+    {
+        $jobInstance = new JobInstance('import', 'test', $jobName);
+        $jobInstance->setCode('stoppable_dumb_job_instance');
+        $jobInstanceSaver = $this->get('akeneo_batch.saver.job_instance');
+        $jobInstanceSaver->save($jobInstance);
+
+        return $jobInstance;
+    }
+
+    /**
+     * It's not possible to activate a feature flag that does not exist, to avoid to mock a non existing flag in production
+     * Therefore we create a fake service in the feature flag registry. The fake implementation is not used.
+     */
+    private function addNewFeatureFlag(string $featureFlag): void
+    {
+        $featureFlagService = new class implements FeatureFlag {
+            public function isEnabled(): bool { return false; }
+        };
+        $this->get('akeneo.feature_flag.service.registry')->add($featureFlag, $featureFlagService);
     }
 }
