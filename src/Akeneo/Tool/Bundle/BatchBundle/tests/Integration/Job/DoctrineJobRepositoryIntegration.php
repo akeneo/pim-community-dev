e525942d11cde474fdc796a01d31fc46c5930da7
ahocquard <alexandre.hocquard@akeneo.com>
2022-07-20 11:05:40 +0200

    TIP-1597: move test into the directory of the bundle

--- src/Akeneo/Tool/Bundle/BatchBundle/tests/Integration/Job/DoctrineJobRepositoryIntegration.php
+++ src/Akeneo/Tool/Bundle/BatchBundle/tests/Integration/Job/DoctrineJobRepositoryIntegration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Test\Integration\integration\BatchBundle\Job;
+namespace Akeneo\Tool\Bundle\BatchBundle\tests\Integration\Job;
 
 use Akeneo\Tool\Bundle\BatchBundle\Job\DoctrineJobRepository;
 use Akeneo\Tool\Bundle\BatchBundle\Job\JobInstanceRepository;
