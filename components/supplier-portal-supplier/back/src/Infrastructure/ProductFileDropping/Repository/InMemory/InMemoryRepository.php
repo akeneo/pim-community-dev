25ed60261f3cb70eb779f7767beb28764d99c7e4
Philippe Mossiere <philippe.mossiere@akeneo.com>
2022-07-20 09:25:45 +0200

    SP-116: Add find method in InMemory repo

--- components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Repository/InMemory/InMemoryRepository.php
+++ components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Repository/InMemory/InMemoryRepository.php
@@ -6,6 +6,7 @@ namespace Akeneo\SupplierPortal\Supplier\Infrastructure\ProductFileDropping\Repo
 
 use Akeneo\SupplierPortal\Supplier\Domain\ProductFileDropping\Write\Model\SupplierFile;
 use Akeneo\SupplierPortal\Supplier\Domain\ProductFileDropping\Write\SupplierFileRepository;
+use Akeneo\SupplierPortal\Supplier\Domain\ProductFileDropping\Write\ValueObject\Identifier;
 
 final class InMemoryRepository implements SupplierFileRepository
 {
@@ -14,5 +15,16 @@ final class InMemoryRepository implements SupplierFileRepository
     public function save(SupplierFile $supplierFile): void
     {
         $this->supplierFiles[$supplierFile->identifier()] = $supplierFile;
+    }
+
+    public function find(Identifier $identifier): ?SupplierFile
+    {
+        foreach ($this->supplierFiles as $supplierFile) {
+            if ((string) $identifier === $supplierFile->identifier()) {
+                return $supplierFile;
+            }
+        }
+
+        return null;
     }
 }
