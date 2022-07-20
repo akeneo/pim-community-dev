9b9aa81b176239c4f9cd92f3e8b56426709e9745
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-07-20 15:49:41 +0200

    SP-115: fix CS

--- components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Controller/UploadProductFile.php
+++ components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Controller/UploadProductFile.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Akeneo\SupplierPortal\Supplier\Infrastructure\ProductFileDropping\Controller;
 
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 
 final class UploadProductFile
 {
@@ -18,18 +16,17 @@ final class UploadProductFile
 
     public function __invoke(Request $request): JsonResponse
     {
-        /** @var $file UploadedFile */
         $file = $request->files->has('file') ? $request->files->get('file') : null;
 
         if (null === $file) {
             return new JsonResponse(null, Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
-        try {
-            //Call handler
-        } catch (\Exception) {
-            return new JsonResponse(['error' => 'Sample error message'], Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+//        try {
+//            //Call handler
+//        } catch (\Exception) {
+//            return new JsonResponse(['error' => 'Sample error message'], Response::HTTP_UNPROCESSABLE_ENTITY);
+//        }
 
         return new JsonResponse(null, Response::HTTP_ACCEPTED);
     }
