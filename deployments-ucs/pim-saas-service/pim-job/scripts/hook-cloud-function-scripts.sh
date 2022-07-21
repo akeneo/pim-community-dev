c7fe9c6dc901bd6bb7f07adb9e0a0a20a2abac7b
Ahmed Romdhani <ahmed.romdhani@gmail.com>
2022-07-21 17:15:04 +0200

    delete token and use SA

--- deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-tar cvf ucs-function-cron-transformer.zip /tmp/ucs-function-cron-transformer
-CLOUD_FUNCTION_SOURCE_ZIP=/tmp/ucs-function-cron-transformer.zip
-ls -lrt /tmp/
-echo ${TOKEN_OATH2}
-curl -X POST --data-binary $CLOUD_FUNCTION_SOURCE_ZIP \
-    -H "Content-Type: application/zip" \
-    "https://storage.googleapis.com/upload/storage/v1/b/srnt-ucs-pim-cron/o?uploadType=media&name=ucs-function-cron-transformer.zip" 
+cd /tmp
+tar -cvf  ucs-function-cron-transformer.zip /ucs-function-cron-transformer
+curl -X POST  -v  --data-binary @ucs-function-cron-transformer.zip  \
+      "https://storage.googleapis.com/upload/storage/v1/b/srnt-ucs-pim-cron/o?uploadType=media&name=ucs-function-cron-transformer.zip"
