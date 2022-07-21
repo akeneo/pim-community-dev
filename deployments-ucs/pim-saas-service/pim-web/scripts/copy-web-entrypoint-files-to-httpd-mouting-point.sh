8b3c8902fce420fdc3a19a46aa4adfe6ab16d37a
ValentinMerlet <merlet.valentin@gmail.com>
2022-07-21 09:29:16 +0200

    SP-189: Rename onboarder serenity occurences in .circleCI and deployments directories

--- deployments-ucs/pim-saas-service/pim-web/scripts/copy-web-entrypoint-files-to-httpd-mouting-point.sh
+++ deployments-ucs/pim-saas-service/pim-web/scripts/copy-web-entrypoint-files-to-httpd-mouting-point.sh
@@ -4,7 +4,7 @@ set -e
 #Copy PIM entrypoint files into web-src volume
 cp -p -r -f /srv/pim/public/* /srv/pim/public/.htaccess /web-src/.
 
-#Onboarder serenity front build
+# Supplier Portal Supplier front build
 if [[ -d '/srv/pim/components/supplier-portal-supplier/front/build' ]];then
   mkdir -p /web-src/supplier-portal/
   cp -p -r -f /srv/pim/components/supplier-portal-supplier/front/build/* /web-src/supplier-portal/
