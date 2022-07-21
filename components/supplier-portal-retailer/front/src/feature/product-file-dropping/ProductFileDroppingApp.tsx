d7245de088a69872fbf4c751f4a8049a8ba5e5e2
Julian Prud'homme <julian.prudhomme@akeneo.com>
2022-07-21 17:14:20 +0200

    SP-126: prepare retailer microfrontend for a new product file dropping application

---
+++ components/supplier-portal-retailer/front/src/feature/product-file-dropping/ProductFileDroppingApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {HashRouter as Router} from 'react-router-dom';
+
+const ProductFileDroppingApp = () => {
+    return (
+        <Router basename="/retailer-portal/product-file-dropping">
+            <div>Product File Dropping</div>
+        </Router>
+    );
+};
+
+export {ProductFileDroppingApp};
