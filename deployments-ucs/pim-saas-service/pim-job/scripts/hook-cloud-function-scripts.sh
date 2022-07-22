edd08624eeacc7f5a9ef99ff362002b93747bd82
Ahmed Romdhani <ahmed.romdhani@gmail.com>
2022-07-22 12:40:55 +0200

    rename files configmaps and inject vars to script

--- deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
 cd /tmp
-tar -cvf  ucs-function-cron-transformer.zip /ucs-function-cron-transformer
-curl -X POST  -v  --data-binary @ucs-function-cron-transformer.zip  \
-      "https://storage.googleapis.com/upload/storage/v1/b/srnt-ucs-pim-cron/o?uploadType=media&name=ucs-function-cron-transformer.zip"
+BUCKET_NAME="$env{BUCKET_NAME}"
+SOURCE_CODE_ZIP="$env{SOURCE_CODE_ZIP}"
+tar -cvf  SOURCE_CODE_ZIP /ucs-function-cron-transformer
+curl -X POST  -v  --data-binary @SOURCE_CODE_ZIP  \
+      "https://storage.googleapis.com/upload/storage/v1/b/$BUCKET_NAME/o?uploadType=media&name=$SOURCE_CODE_ZIP"
