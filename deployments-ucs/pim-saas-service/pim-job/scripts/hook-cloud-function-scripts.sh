4e2cefe45ae638523a492e92b17bf18277bb18bb
Ahmed Romdhani <ahmed.romdhani@gmail.com>
2022-07-22 13:27:40 +0200

    quick fix

--- deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-BUCKET_NAME="$env{BUCKET_NAME}"
-SOURCE_CODE_ZIP="$env{SOURCE_CODE_ZIP}"
-tar -cvf  SOURCE_CODE_ZIP /ucs-function-cron-transformer
-curl -X POST  -v  --data-binary @SOURCE_CODE_ZIP  \
-      "https://storage.googleapis.com/upload/storage/v1/b/$BUCKET_NAME/o?uploadType=media&name=$SOURCE_CODE_ZIP"
+tar -cvf  $SOURCE_CODE_ZIP /ucs-function-cron-transformer
+curl -X POST  -v  --data-binary @$SOURCE_CODE_ZIP  "https://storage.googleapis.com/upload/storage/v1/b/"$BUCKET_NAME"/o?uploadType=media&name="$SOURCE_CODE_ZIP
+
