ec1f3e7ca98f4fd20c0e4a235de6ba2adecdd135
Vincent <vincent.gelot@akeneo.com>
2022-07-25 18:06:43 +0200

    get token to push to bucket

--- deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
@@ -3,4 +3,5 @@ set -ex
 
 cd /tmp
 tar -cvf ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer
-curl -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
+SA_TOKEN=$(curl -v -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token | jq '.access_token')
+curl -H "Authorization: OAuth ${SA_TOKEN}" -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
