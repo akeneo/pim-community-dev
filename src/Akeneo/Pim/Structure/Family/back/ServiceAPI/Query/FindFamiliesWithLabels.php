b9dc575b6a35bff52efa3b34c3eaee623b1a72f7
Adrien Pétremann <adrien.petremann@getakeneo.com>
2022-07-25 16:06:51 +0200

    Add query to fetch families with their labels

---
+++ src/Akeneo/Pim/Structure/Family/back/ServiceAPI/Query/FindFamiliesWithLabels.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Akeneo\Pim\Structure\Family\ServiceAPI\Query;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface FindFamiliesWithLabels
+{
+    /**
+     * @return FamilyWithLabels[]
+     */
+    public function fromQuery(FamilyQuery $query): array;
+}
