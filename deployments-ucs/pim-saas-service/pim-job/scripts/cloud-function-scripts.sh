7de86411a19810f2fce62dc1d98956f86057f7ba
Vincent <vincent.gelot@akeneo.com>
2022-07-25 16:55:23 +0200

    add init container

--- deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-set -uo pipefail
+set -ex
 
 cd /tmp
-tar -cvf  ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer
-curl -X POST  -v  --data-binary @${SOURCE_CODE_ZIP}  "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
+tar -cvf ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer
+curl -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
