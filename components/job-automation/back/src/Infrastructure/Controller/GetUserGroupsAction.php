946a95b7c606e266b7f573062e4cfeeed929a01a
axelhms <hemissi.axel@gmail.com>
2022-07-25 17:10:55 +0200

    RAB-933: Add user groups controller with mocked value

---
+++ components/job-automation/back/src/Infrastructure/Controller/GetUserGroupsAction.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Platform\JobAutomation\Infrastructure\Controller;
+
+use Akeneo\Platform\JobAutomation\Application\GetUserGroups\GetUserGroupsHandler;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class GetUserGroupsAction
+{
+    public function __construct(
+        private GetUserGroupsHandler $getUserGroupsHandler,
+    ) {
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        // if (!$request->isXmlHttpRequest()) {
+        //     return new RedirectResponse('/');
+        // }
+
+        return new JsonResponse($this->getUserGroupsHandler->handle(), Response::HTTP_OK);
+    }
+}
