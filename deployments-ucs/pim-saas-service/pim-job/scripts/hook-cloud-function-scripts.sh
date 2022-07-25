680d573a58847b39b9b0e7f05787c7d88708cf73
Vincent <vincent.gelot@akeneo.com>
2022-07-25 08:52:51 +0200

    use brackets variables

--- deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/hook-cloud-function-scripts.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cd /tmp
-tar -cvf  $SOURCE_CODE_ZIP /ucs-function-cron-transformer
-curl -X POST  -v  --data-binary @$SOURCE_CODE_ZIP  "https://storage.googleapis.com/upload/storage/v1/b/"$BUCKET_NAME"/o?uploadType=media&name="$SOURCE_CODE_ZIP
-
+tar -cvf  ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer
+curl -X POST  -v  --data-binary @${SOURCE_CODE_ZIP}  "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
