7e030b364b85a0aa97e5f37e36f05744a1b2de4b
pierallard <pierallard@gmail.com>
2022-07-27 17:33:03 +0200

    CPM-638: Fix reviews

--- src/Akeneo/Pim/Enrichment/Bundle/Elasticsearch/Sorter/Field/IdSorter.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Elasticsearch/Sorter/Field/IdSorter.php
@@ -6,9 +6,9 @@ namespace Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Sorter\Field;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UuidSorter extends BaseFieldSorter
+class IdSorter extends BaseFieldSorter
 {
-    public function __construct(array $supportedFields = [])
+    public function __construct()
     {
         parent::__construct(['id']);
     }
