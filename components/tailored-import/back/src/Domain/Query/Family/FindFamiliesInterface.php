20939e91b2453d0f9c764853eea7a880852fe5c7
anthony-grandin <anthony.grandin@getakeneo.com>
2022-07-28 11:36:20 +0200

    RAB-783: fix lint back

--- components/tailored-import/back/src/Domain/Query/Family/FindFamiliesInterface.php
+++ components/tailored-import/back/src/Domain/Query/Family/FindFamiliesInterface.php
@@ -2,8 +2,6 @@
 
 namespace Akeneo\Platform\TailoredImport\Domain\Query\Family;
 
-use Akeneo\Platform\TailoredImport\Domain\Query\Family\FindFamiliesResult;
-
 interface FindFamiliesInterface
 {
     public function execute(
