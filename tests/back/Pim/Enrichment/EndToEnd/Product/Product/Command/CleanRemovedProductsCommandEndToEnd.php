3dd1cdcf681b6a1922c817da0d83fa6241d98191
anne-laurej <anne-laure.jouhanneau@akeneo.com>
2022-07-28 15:09:01 +0200

    Add tests existing in DB and not existing in DB

--- tests/back/Pim/Enrichment/EndToEnd/Product/Product/Command/CleanRemovedProductsCommandEndToEnd.php
+++ tests/back/Pim/Enrichment/EndToEnd/Product/Product/Command/CleanRemovedProductsCommandEndToEnd.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace AkeneoTest\Pim\Enrichment\EndToEnd\Product\Product\Command;
 
 use Akeneo\Pim\Enrichment\Bundle\Command\CleanRemovedProductsCommand;
+use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Model\ElasticsearchProductProjection;
 use Akeneo\Pim\Enrichment\Product\API\Command\UpsertProductCommand;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Akeneo\Test\IntegrationTestsBundle\Launcher\CommandLauncher;
 use Akeneo\Test\IntegrationTestsBundle\Messenger\AssertEventCountTrait;
 use Akeneo\Tool\Bundle\ElasticsearchBundle\Client;
+use Akeneo\Tool\Bundle\ElasticsearchBundle\Refresh;
 use AkeneoTest\Pim\Enrichment\EndToEnd\Product\Product\ExternalApi\AbstractProductTestCase;
+use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Assert;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
@@ -33,85 +36,96 @@ class CleanRemovedProductsCommandEndToEnd extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->get('akeneo_integration_tests.helper.authenticator')->logIn('admin');
         //Create a product
-        /*$command = UpsertProductCommand::createFromCollection(
+        $command = UpsertProductCommand::createFromCollection(
             userId: $this->getUserId('admin'),
             productIdentifier: 'existing_product_in_db',
             userIntents: []
         );
         $this->get('pim_enrich.product.message_bus')->dispatch($command);
-        $productUuid = $this->getProductUuidFromIdentifier('existing_product_in_db');
-        $this->get('akeneo.pim.enrichment.product.query.get_elasticsearch_product_projection')->fromProductUuids([$productUuid]);*/
-        //$this->command = $this->application->find('pim:product:clean-removed-products');
-        //$this->esProductClient = $this->get('akeneo_elasticsearch.client.product_and_product_model');
-        //$this->get('pim:product:clean-removed-products')->load();
-
-        //$this->runResetIndexesCommand();
+        $this->esProductClient = $this->get('akeneo_elasticsearch.client.product_and_product_model');
+        /*$productUuid = $this->getProductUuidFromIdentifier('existing_product_in_db');
+        $this->get('akeneo.pim.enrichment.product.query.get_elasticsearch_product_projection')->fromProductUuids([$productUuid]);
+        $this->esProductClient = $this->get('akeneo_elasticsearch.client.product_and_product_model');*/
+        // $this->get('akeneo_elasticsearch.client.product_and_product_model')->refreshIndex();
     }
 
-    private function runResetIndexesCommand(): void
+    public function test_getProduct_Existing_in_db(): void
     {
-        $commandLauncher = new CommandLauncher(static::$kernel);
-        $exitCode = $commandLauncher->execute('pim:product:clean-removed-products', null, ['inputs' => ['yes']]);
-        $this->assertSame(0, $exitCode);
+        $product = 'existing_product_in_db';
+        $query = <<<SQL
+            SELECT BIN_TO_UUID(uuid) AS uuid FROM pim_catalog_product WHERE identifier = :productIdentifier
+        SQL;
+
+        //retrieve data from db
+        $stmt = $this->get('database_connection')->executeQuery($query, ['productIdentifier' => $product]);
+        $id = $stmt->fetchOne();
+        Assert::assertNotNull($id);
+        $idDB = ElasticsearchProductProjection::INDEX_PREFIX_ID . $id;
+
+        //retrieve data from Elasticsearch
+        $params = [
+            'query' => [
+                'match' => [
+                        'identifier' => $product
+                ]
+            ]
+        ];
+        $this->esProductClient->refreshIndex();
+        $result = $this->esProductClient->search($params);
+        Assert::assertNotNull($result);
+        $doc = $result["hits"]["hits"];
+        foreach ($doc as $information) {
+            $docId = $information["_source"]["id"];
+        }
+
+        Assert::assertEquals($idDB, $docId);
     }
 
-    public function test_getAncestorsFromProductsIds_result(): void
+    public function test_getProduct_Not_Existing_In_DB(): void
     {
-        //$commandTester = new CommandTester($this->command);
-        //$commandTester->execute();
-        //$commandDiff = $this->get('pim:product:clean-removed-products');
+        //create a product only in Elasticsearch
+        $product = [
+            'identifier' => 'product_not_exiting_product_in_db',
+            'values'     => [
+                'name-text' => [
+                    '<all_channels>' => [
+                        '<all_locales>' => '2015/01/01',
+                    ],
+                ],
+            ]
+        ];
+        $this->esProductClient->index($product['identifier'], $product);
 
-        //$commandDiff->getAncestorsFromProductsIds();
+        $this->esProductClient->refreshIndex();
+        $params = [
+            'query' => [
+                'match' => [
+                    'identifier' => $product['identifier']
+                ]
+            ]
+        ];
+        $result = $this->esProductClient->search($params);
+        $doc = $result["hits"]["hits"];
+        foreach ($doc as $information) {
+            $identifierDoc = $information["_source"]["identifier"];
+        }
+
+        //search in DB
+        $query = <<<SQL
+            SELECT identifier FROM pim_catalog_product WHERE identifier = :productIdentifier
+        SQL;
+
+        //retrieve data from db
+        $stmt = $this->get('database_connection')->executeQuery($query, ['productIdentifier' => $product['identifier']]);
+        $identifier = $stmt->fetchOne();
+        $identifierDB = ElasticsearchProductProjection::INDEX_PREFIX_ID . $identifier;
+
+        Assert::assertNotEquals($identifierDoc, $identifierDB);
 
     }
-
-    /*public function testDeleteAProduct()
-    {
-        $this->createAdminUser();
-        $client = $this->createAuthenticatedClient();
-
-        $this->assertCount(7, $this->get('pim_catalog.repository.product')->findAll());
-
-        $this->get('pim_catalog.elasticsearch.indexer.product')->indexFromProductIdentifier('foo');
-        $client->request('DELETE', 'api/rest/v1/products/foo');
-
-        $response = $client->getResponse();
-        $this->assertSame(Response::HTTP_NO_CONTENT, $response->getStatusCode());
-
-        $this->assertCount(6, $this->get('pim_catalog.repository.product')->findAll());
-        $this->assertNull($this->get('pim_catalog.repository.product')->findOneByIdentifier('foo'));
-
-        $this->assertEventCount(1, ProductRemoved::class);
-    }
-
-    public function testNotFoundAProduct()
-    {
-        $this->createAdminUser();
-        $client = $this->createAuthenticatedClient();
-
-        $client->request('DELETE', 'api/rest/v1/products/not_found');
-
-        $response = $client->getResponse();
-        $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
-        $content = json_decode($response->getContent(), true);
-        $this->assertCount(2, $content, 'response contains 2 items');
-        $this->assertSame(Response::HTTP_NOT_FOUND, $content['code']);
-        $this->assertSame('The not_found product does not exist in your PIM or you do not have permission to access it.', $content['message']);
-    }
-
-    public function testAccessDeniedWhenDeletingProductWithoutTheAcl()
-    {
-        $this->createAdminUser();
-        $client = $this->createAuthenticatedClient();
-        $this->removeAclFromRole('action:pim_api_product_remove');
-
-        $client->request('DELETE', 'api/rest/v1/products/foo');
-        $response = $client->getResponse();
-
-        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
-    }*/
-
     private function getUserId(string $username): int
     {
         $query = <<<SQL
