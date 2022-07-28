42d0a3bfdef9c89e4138db3ba40281b629eab3f1
Vincent <vincent.gelot@akeneo.com>
2022-07-28 17:40:30 +0200

    test cloud function with php format

--- deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
@@ -2,6 +2,7 @@
 set -ex
 
 cd /tmp
-zip ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer/*.js /ucs-function-cron-transformer/*.json
+zip ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer/*.php
+# zip ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer/*.js /ucs-function-cron-transformer/*.json
 SA_TOKEN=$(curl -v -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token | jq '.access_token')
 curl -f -H "Authorization: OAuth ${SA_TOKEN}" -H "Content-Type: application/zip" -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
---
+++ deployments-ucs/pim-saas-service/pim-job/ucs-function-cron-transformer/index.php
@@ -0,0 +1,8 @@
+<?php
+
+function testPHP()
+{
+header("Location: https://akeneo.com/");
+exit();
+}
+?>
