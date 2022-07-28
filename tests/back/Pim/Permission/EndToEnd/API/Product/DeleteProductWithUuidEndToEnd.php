d20b98d5d96cceb95157f84a5d5af067eace4e39
pierallard <pierallard@gmail.com>
2022-07-28 14:27:14 +0200

    CPM-638: Add tests for LIST

--- tests/back/Pim/Permission/EndToEnd/API/Product/DeleteProductWithUuidEndToEnd.php
+++ tests/back/Pim/Permission/EndToEnd/API/Product/DeleteProductWithUuidEndToEnd.php
@@ -14,33 +14,23 @@ use Symfony\Component\HttpFoundation\Response;
  * | Manager  | View,Edit,Own | View,Edit,Own | View,Edit,Own |
  * +----------+-----------------------------------------------+
  */
-class DeleteProductEndToEnd extends AbstractProductTestCase
+class DeleteProductWithUuidEndToEnd extends AbstractProductTestCase
 {
-    public function testDeleteProductSuccessful()
+    public function testDeleteProductByUuidSuccessful()
     {
+        $product = $this->createProduct('test_identifier', []);
         $client = $this->createAuthenticatedClient([], [], null, null, 'julia', 'julia');
-        $client->request('DELETE', 'api/rest/v1/products/product_viewable_by_everybody_1');
+        $client->request('DELETE', 'api/rest/v1/products-uuid/' . $product->getUuid()->toString());
 
         $response = $client->getResponse();
         $this->assertSame(Response::HTTP_NO_CONTENT, $response->getStatusCode());
         $this->assertSame('', $response->getContent());
-        $this->assertProductDeleted('product_viewable_by_everybody_1');
+        $this->assertProductDeleted('test_identifier');
     }
 
-    public function testDeleteProductWithoutCategory()
+    public function testProductNotDeletableByUuidByUserWhoCanEditButIsNotOwner()
     {
-        $client = $this->createAuthenticatedClient([], [], null, null, 'mary', 'mary');
-        $client->request('DELETE', 'api/rest/v1/products/product_without_category');
-
-        $response = $client->getResponse();
-        $this->assertSame(Response::HTTP_NO_CONTENT, $response->getStatusCode());
-        $this->assertSame('', $response->getContent());
-        $this->assertProductDeleted('product_without_category');
-    }
-
-    public function testProductNotDeletableByUserWhoCanEditBuIsNotOwner()
-    {
-        $this->createProduct('product_not_owned_by_redactor', [
+        $product = $this->createProduct('product_not_owned_by_redactor', [
             'categories' => ['categoryA']
         ]);
 
@@ -50,7 +40,7 @@ class DeleteProductEndToEnd extends AbstractProductTestCase
 JSON;
 
         $client = $this->createAuthenticatedClient([], [], null, null, 'mary', 'mary');
-        $client->request('DELETE', 'api/rest/v1/products/product_not_owned_by_redactor');
+        $client->request('DELETE', 'api/rest/v1/products-uuid/' . $product->getUuid()->toString());
 
         $response = $client->getResponse();
         $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
@@ -58,9 +48,9 @@ JSON;
         $this->assertProductNotDeleted('product_not_owned_by_redactor');
     }
 
-    public function testProductNotDeletableByUserWhoCanOnlyViewProduct()
+    public function testProductNotDeletableByUuidByUserWhoCanOnlyViewProduct()
     {
-        $this->createProduct('product_not_owned_by_redactor', [
+        $product = $this->createProduct('product_not_owned_by_redactor', [
             'categories' => ['categoryA1']
         ]);
 
@@ -70,7 +60,7 @@ JSON;
 JSON;
 
         $client = $this->createAuthenticatedClient([], [], null, null, 'mary', 'mary');
-        $client->request('DELETE', 'api/rest/v1/products/product_not_owned_by_redactor');
+        $client->request('DELETE', 'api/rest/v1/products-uuid/' . $product->getUuid()->toString());
 
         $response = $client->getResponse();
         $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
@@ -78,20 +68,24 @@ JSON;
         $this->assertProductNotDeleted('product_not_owned_by_redactor');
     }
 
-    public function testProductNotDeletableByUserWhoHasNoPermissions()
+    public function testProductNotDeletableByUuidByUserWhoHasNoPermissions()
     {
+        $product = $this->createProduct('second_product_not_viewable_by_redactor', [
+            'categories' => ['categoryB']
+        ]);
+
         $expectedResponseContent =
 <<<JSON
-{"code":404,"message":"Product \"product_not_viewable_by_redactor\" does not exist or you do not have permission to access it."}
+{"code":404,"message":"Product \"second_product_not_viewable_by_redactor\" does not exist or you do not have permission to access it."}
 JSON;
 
         $client = $this->createAuthenticatedClient([], [], null, null, 'mary', 'mary');
-        $client->request('DELETE', 'api/rest/v1/products/product_not_viewable_by_redactor');
+        $client->request('DELETE', 'api/rest/v1/products-uuid/' . $product->getUuid()->toString());
 
         $response = $client->getResponse();
         $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
         $this->assertSame($expectedResponseContent, $response->getContent());
-        $this->assertProductNotDeleted('product_not_viewable_by_redactor');
+        $this->assertProductNotDeleted('second_product_not_viewable_by_redactor');
     }
 
     /**
