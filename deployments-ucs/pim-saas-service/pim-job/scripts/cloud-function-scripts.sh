cf28c06d57601054698b3071ccf5ef340316b5b0
Vincent <vincent.gelot@akeneo.com>
2022-07-29 13:43:40 +0200

    use node instead of php

--- deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
+++ deployments-ucs/pim-saas-service/pim-job/scripts/cloud-function-scripts.sh
@@ -2,8 +2,8 @@
 set -ex
 
 cd /tmp
-cp /ucs-function-cron-transformer/*.php .
-zip ${SOURCE_CODE_ZIP} *.php
-# zip ${SOURCE_CODE_ZIP} /ucs-function-cron-transformer/*.js /ucs-function-cron-transformer/*.json
+cp /ucs-function-cron-transformer/*.js .
+cp /ucs-function-cron-transformer/*.json .
+zip ${SOURCE_CODE_ZIP} *.js *.json
 SA_TOKEN=$(curl -v -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token | jq '.access_token')
 curl -f -H "Authorization: OAuth ${SA_TOKEN}" -H "Content-Type: application/zip" -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
--- deployments-ucs/pim-saas-service/pim-job/ucs-function-cron-transformer/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function testPHP()
-{
-header("Location: https://akeneo.com/");
-exit();
-}
-?>
