c22f37b7aeb09dc459a8bba2890caacb0c3831e9
Steven VAIDIE <steven.vaidie@gmail.com>
2022-07-29 14:13:17 +0200

    RAB-919: fix unit test

--- components/job-automation/front/src/model/Automation.test.ts
+++ components/job-automation/front/src/model/Automation.test.ts
@@ -2,7 +2,7 @@ import {filterDefaultUserGroup} from './Automation';
 
 test('it remove the default user group', async () => {
   expect(filterDefaultUserGroup([])).toEqual([]);
-  expect(filterDefaultUserGroup(['All'])).toEqual(['All']);
+  expect(filterDefaultUserGroup(['All'])).toEqual([]);
   expect(filterDefaultUserGroup(['All', 'Support IT'])).toEqual(['Support IT']);
   expect(filterDefaultUserGroup(['Manager', 'Support IT'])).toEqual(['Manager', 'Support IT']);
 });
