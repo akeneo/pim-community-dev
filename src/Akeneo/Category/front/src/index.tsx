9a89b71d70d8dd89e436b432f9ad33d0f8570203
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-07-29 16:27:59 +0200

    GRF-91: Decouple the Categories Microfrontend from the PIM

--- src/Akeneo/Category/front/src/index.tsx
+++ src/Akeneo/Category/front/src/index.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {ThemeProvider} from 'styled-components';
 import {pimTheme} from 'akeneo-design-system';
-import {MicroFrontendDependenciesProvider, Routes, Translations} from '@akeneo-pim-community/shared';
+import {Routes, Translations} from '@akeneo-pim-community/shared';
 import {routes} from './routes.json';
 import translations from './translations.json';
-import {CategoriesApp} from './feature/CategoriesApp';
+import {CategoriesApp} from './feature';
+import {ConfigurationProvider, Page} from './configuration';
+import {MicroFrontendDependenciesProvider} from './microfrontend';
 import {FakePIM} from './FakePIM';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 ReactDOM.render(
   <ThemeProvider theme={pimTheme}>
-    <MicroFrontendDependenciesProvider routes={routes as Routes} translations={translations as Translations}>
-      <FakePIM>
-        <CategoriesApp setCanLeavePage={() => true} />
-      </FakePIM>
-    </MicroFrontendDependenciesProvider>
+    <ConfigurationProvider>
+      <MicroFrontendDependenciesProvider routes={routes as Routes} translations={translations as Translations}>
+        <FakePIM>
+          <Router basename="/">
+            <Switch>
+              <Route path="/configuration">
+                <Page />
+              </Route>
+              <Route path="/">
+                <CategoriesApp setCanLeavePage={() => true} />
+              </Route>
+            </Switch>
+          </Router>
+        </FakePIM>
+      </MicroFrontendDependenciesProvider>
+    </ConfigurationProvider>
   </ThemeProvider>,
   document.getElementById('root')
 );
