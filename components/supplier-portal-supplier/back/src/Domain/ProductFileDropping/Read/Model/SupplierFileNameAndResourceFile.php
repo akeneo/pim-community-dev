e837fc74894a4a4a0f27df7a6c5208a2ca96f162
ValentinMerlet <merlet.valentin@gmail.com>
2022-08-01 18:14:02 +0200

    SP-202: Add GetProductFilePathAndFileName query

--- components/supplier-portal-supplier/back/src/Domain/ProductFileDropping/Read/Model/SupplierFileNameAndResourceFile.php
+++ components/supplier-portal-supplier/back/src/Domain/ProductFileDropping/Read/Model/SupplierFileNameAndResourceFile.php
@@ -6,6 +6,7 @@ namespace Akeneo\SupplierPortal\Supplier\Domain\ProductFileDropping\Read\Model;
 
 final class SupplierFileNameAndResourceFile
 {
+    //@phpstan-ignore-next-line
     public function __construct(public string $originalFilename, public $file)
     {
     }
