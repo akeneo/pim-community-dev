33f6edea21234b2a3a18ee2b53149a436e0e09d7
jean-marie leroux <jmleroux.pro@gmail.com>
2022-08-03 17:39:18 +0200

    Update .circleci/configure_docker_mirrors.sh
    Co-authored-by: Alexandre Hocquard <alexandre.hocquard@akeneo.com>

--- .circleci/configure_docker_mirrors.sh
+++ .circleci/configure_docker_mirrors.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 # see https://cloud.google.com/container-registry/docs/pulling-cached-images#configure
 echo '{"registry-mirrors": ["https://mirror.gcr.io"]}' >> /etc/docker/daemon.json
