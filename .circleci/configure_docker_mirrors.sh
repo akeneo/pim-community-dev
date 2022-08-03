a20b8e371d69005f8b14aa98d6e382a12be64068
jean-marie leroux <jmleroux.pro@gmail.com>
2022-08-04 00:07:44 +0200

    Use Google mirror cache to pull Docker images (#14484)

---
+++ .circleci/configure_docker_mirrors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# see https://cloud.google.com/container-registry/docs/pulling-cached-images#configure
+echo '{"registry-mirrors": ["https://mirror.gcr.io"]}' >> /etc/docker/daemon.json
+
+service docker restart
