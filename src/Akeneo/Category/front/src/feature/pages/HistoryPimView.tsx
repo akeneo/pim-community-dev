3b019b7d06dcf6e93c74220f2cfdb547477a292b
Antoine Trouve <antoine.trouve@akeneo.com>
2022-08-05 11:26:44 +0200

    PIM-10566 : fix display history tab data when refreshing browser and multiple http request on refreshing.

--- src/Akeneo/Category/front/src/feature/pages/HistoryPimView.tsx
+++ src/Akeneo/Category/front/src/feature/pages/HistoryPimView.tsx
@@ -4,10 +4,11 @@ import {useIsMounted, useViewBuilder, View} from '@akeneo-pim-community/shared';
 type Props = {
   viewName: string;
   className?: string;
-  onBuild?: (view: View) => Promise<View>;
+  onBuild?: (view: View) => Promise<View|null>;
+  version: number;
 };
 
-const HistoryPimView = ({viewName, className, onBuild}: Props) => {
+const HistoryPimView = ({viewName, className, onBuild, version = 0}: Props) => {
   const el = useRef<HTMLDivElement>(null);
   const [view, setView] = useState<View | null>(null);
 
@@ -26,13 +27,13 @@ const HistoryPimView = ({viewName, className, onBuild}: Props) => {
         }
         return view;
       })
-      .then((view: View) => {
-        if (isMounted()) {
-          view.setElement(el.current).render();
+      .then((view: View|null) => {
+        if (isMounted() && view !== null) {
           setView(view);
+          view.setElement(el.current).render();
         }
       });
-  }, [viewBuilder, viewName, isMounted, onBuild]);
+  }, [viewBuilder, viewName, isMounted, onBuild, version]);
 
   useEffect(
     () => () => {
