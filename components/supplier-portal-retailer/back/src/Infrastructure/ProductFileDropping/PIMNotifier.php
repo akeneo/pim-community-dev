7622a1c2fad600f28c347fe89c861e7937513ca1
ValentinMerlet <merlet.valentin@gmail.com>
2022-08-09 14:13:44 +0200

    SP-105: Add ACL for PIM Notifier

---
+++ components/supplier-portal-retailer/back/src/Infrastructure/ProductFileDropping/PIMNotifier.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\SupplierPortal\Retailer\Infrastructure\ProductFileDropping;
+
+use Akeneo\Platform\Bundle\NotificationBundle\Entity\Notification;
+use Akeneo\Platform\Bundle\NotificationBundle\NotifierInterface;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Notifier;
+use Akeneo\UserManagement\Component\Repository\UserRepositoryInterface;
+
+final class PIMNotifier implements Notifier
+{
+    public function __construct(private NotifierInterface $notifier, private UserRepositoryInterface $userRepository)
+    {
+    }
+
+    public function notifyUsersForSupplierFileAdding(string $contributorEmail): void
+    {
+        $notification = new Notification();
+        $notification
+            ->setType('add')
+            ->setMessage('supplier_portal.product_file_dropping.pim_notification.content')
+            ->setMessageParams(['{{ contributorEmail }}' => $contributorEmail])
+            ->setRoute('supplier_portal_retailer_product_files_list')
+            ->setContext([
+                'actionType' => 'new_product_file',
+                'buttonLabel' => 'pim_notification.supplier_portal.new_product_file.button_label',
+            ])
+        ;
+
+        $this->notifier->notify($notification, $this->userRepository->findAll());
+    }
+}
