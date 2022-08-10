039dc6c9c41e9d8aa11b61318a8925412c28255a
ManuelActis <manuel.actis-cassa@akeneo.com>
2022-08-10 11:08:50 +0200

    PIM-10512: Fix empty group labels format for attribute endpoints

--- tests/back/Pim/Structure/Specification/Component/Normalizer/ExternalApi/AttributeNormalizerSpec.php
+++ tests/back/Pim/Structure/Specification/Component/Normalizer/ExternalApi/AttributeNormalizerSpec.php
@@ -10,7 +10,7 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AttributeNormalizerSpec extends ObjectBehavior
 {
-    function let(NormalizerInterface $stdNormalizer,NormalizerInterface $translationNormalizer)
+    function let(NormalizerInterface $stdNormalizer, NormalizerInterface $translationNormalizer)
     {
         $this->beConstructedWith($stdNormalizer);
         $this->beConstructedWith($stdNormalizer, $translationNormalizer);
@@ -29,36 +29,62 @@ class AttributeNormalizerSpec extends ObjectBehavior
         $this->supportsNormalization($attribute, 'external_api')->shouldReturn(true);
     }
 
-    function it_normalizes_an_attribute($stdNormalizer, AttributeInterface $attribute)
+    function it_normalizes_an_attribute(NormalizerInterface $stdNormalizer, AttributeInterface $attribute)
     {
-        $data = ['code' => 'my_attribute', 'labels' => []];
+        $attribute->getGroup()->willReturn(null);
 
-        $stdNormalizer->normalize($attribute, 'standard', [])->willReturn($data);
-
-        $normalizedAttribute = $this->normalize($attribute, 'external_api', []);
-        $normalizedAttribute->shouldHaveLabels($data);
-    }
-
-    function it_normalizes_an_attribute_with_his_group_labels($stdNormalizer, AttributeInterface $attribute)
-    {
-        $data = ['code' => 'my_attribute', 'labels' => [], 'group' => 'attributeGroupA'];
-
-        $stdNormalizer->normalize($attribute, 'standard', [])->willReturn($data);
-        $normalizedAttribute = $this->normalize($attribute, 'external_api', []);
-
-        $normalizedAttribute->shouldHaveLabels($data);
-        $normalizedAttribute->shouldHaveGroupLabels($data);
-    }
-
-    public function getMatchers(): array
-    {
-        return [
-            'haveLabels' => function ($subject) {
-                return is_object($subject['labels']);
-            },
-            'haveGroupLabels' => function ($subject) {
-                return array_key_exists('group_labels', $subject);
-            }
+        $data = [
+            'code' => 'my_attribute',
+            'labels' => ['en_US' => 'english_label'],
         ];
+        $stdNormalizer->normalize($attribute, 'standard', [])->shouldBeCalled()->willReturn($data);
+
+        $this->normalize($attribute, 'external_api', [])->shouldBeLike(array_merge($data, ['group_labels' => null]));
+    }
+
+    function it_normalizes_an_attribute_with_its_group_labels(
+        NormalizerInterface $stdNormalizer,
+        NormalizerInterface $translationNormalizer,
+        AttributeInterface $attribute,
+        AttributeInterface $group
+    ) {
+        $attribute->getGroup()->willReturn($group);
+
+        $data = [
+            'code' => 'my_attribute',
+            'labels' => ['en_US' => 'english_label'],
+            'group' => 'attributeGroupA',
+        ];
+        $stdNormalizer->normalize($attribute, 'standard', [])->willReturn($data);
+
+        $translationNormalizer->normalize($group, 'external_api', [])
+            ->shouldBeCalled()
+            ->willReturn(['en_US' => 'attribute group A']);
+
+        $this->normalize($attribute, 'external_api', [])
+            ->shouldBeLike(array_merge($data, ['group_labels' => ['en_US' => 'attribute group A']]));
+    }
+
+    function it_normalizes_an_attribute_with_empty_labels(
+        NormalizerInterface $stdNormalizer,
+        NormalizerInterface $translationNormalizer,
+        AttributeInterface $attribute,
+        AttributeInterface $group
+    ) {
+        $attribute->getGroup()->willReturn($group);
+
+        $data = ['code' => 'my_attribute', 'labels' => [], 'group' => 'attributeGroupA'];
+        $stdNormalizer->normalize($attribute, 'standard', [])->willReturn($data);
+
+        $translationNormalizer->normalize($group, 'external_api', [])->shouldBeCalled()->willReturn([]);
+
+        $this->normalize($attribute, 'external_api', [])->shouldBeLike(
+            [
+                'code' => 'my_attribute',
+                'labels' => (object)[],
+                'group' => 'attributeGroupA',
+                'group_labels' => (object)[],
+            ]
+        );
     }
 }
