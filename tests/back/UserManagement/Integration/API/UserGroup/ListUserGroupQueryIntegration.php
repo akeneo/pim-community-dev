d90f97a28d34ed34a89b01bc444a3ccabe19d272
Olivier Pontier <olivier.pontier@akeneo.com>
2022-08-10 16:06:02 +0200

    add integration tests

---
+++ tests/back/UserManagement/Integration/API/UserGroup/ListUserGroupQueryIntegration.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AkeneoTest\UserManagement\Integration\API\UserGroup;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use Akeneo\UserManagement\API\UserGroup\ListUserGroupQuery;
+use Akeneo\UserManagement\API\UserGroup\UserGroup;
+use Akeneo\UserManagement\Application\Handler\ListUserGroupHandler;
+use PHPUnit\Framework\Assert;
+
+class ListUserGroupQueryIntegration extends TestCase
+{
+    public function testItListsTheUserGroups(): void
+    {
+        $userGroups = ($this->get(ListUserGroupHandler::class))(new ListUserGroupQuery());
+
+        Assert::assertCount(4, $userGroups);
+        Assert::containsOnlyInstancesOf(UserGroup::class);
+    }
+
+    public function testItFiltersTheUserGroupsOnLabel(): void
+    {
+        $userGroups = ($this->get(ListUserGroupHandler::class))(new ListUserGroupQuery('support'));
+
+        Assert::assertCount(1, $userGroups);
+        Assert::containsOnlyInstancesOf(UserGroup::class);
+
+        Assert::assertMatchesRegularExpression('/it support/i', $userGroups[0]->getLabel());
+    }
+
+    public function testItListsTheUserGroupsWithPagination(): void
+    {
+        $userGroups = ($this->get(ListUserGroupHandler::class))(new ListUserGroupQuery(
+            null,
+            null,
+            2
+        ));
+
+        Assert::assertCount(2, $userGroups);
+        Assert::assertLessThan(2, $userGroups[0]->getId());
+        Assert::assertEquals(2, $userGroups[1]->getId());
+
+        $userGroups = ($this->get(ListUserGroupHandler::class))(new ListUserGroupQuery(
+            null,
+            2,
+            2
+        ));
+
+        Assert::assertCount(2, $userGroups);
+        Assert::assertGreaterThan(2, $userGroups[0]->getId());
+        Assert::assertGreaterThan(2, $userGroups[1]->getId());
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+}
