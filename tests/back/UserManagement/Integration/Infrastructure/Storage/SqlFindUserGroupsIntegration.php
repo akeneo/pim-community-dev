c759fd0398b0340c9be00e5623393b789074a976
Olivier Pontier <olivier.pontier@akeneo.com>
2022-08-10 19:38:38 +0200

    Add domain model for group

---
+++ tests/back/UserManagement/Integration/Infrastructure/Storage/SqlFindUserGroupsIntegration.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AkeneoTest\UserManagement\Integration\Infrastructure\Storage;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use Akeneo\UserManagement\Domain\Model\Group;
+use Akeneo\UserManagement\Infrastructure\Storage\SqlFindUserGroups;
+use PHPUnit\Framework\Assert;
+
+class SqlFindUserGroupsIntegration extends TestCase
+{
+    public function testItListsTheUserGroups(): void
+    {
+        $userGroups = ($this->get(SqlFindUserGroups::class))();
+
+        Assert::assertCount(4, $userGroups);
+        Assert::containsOnlyInstancesOf(Group::class);
+    }
+
+    public function testItFiltersTheUserGroupsOnLabel(): void
+    {
+        $userGroups = ($this->get(SqlFindUserGroups::class))('support');
+
+        Assert::assertCount(1, $userGroups);
+        Assert::containsOnlyInstancesOf(Group::class);
+
+        Assert::assertMatchesRegularExpression('/it support/i', $userGroups[0]->getName());
+    }
+
+    public function testItListsTheUserGroupsWithPagination(): void
+    {
+        $userGroups = ($this->get(SqlFindUserGroups::class))(
+            null,
+            null,
+            2
+        );
+
+        Assert::assertCount(2, $userGroups);
+        Assert::assertLessThan(2, $userGroups[0]->getId());
+        Assert::assertEquals(2, $userGroups[1]->getId());
+
+        $userGroups = ($this->get(SqlFindUserGroups::class))(
+            null,
+            2,
+            2
+        );
+
+        Assert::assertCount(2, $userGroups);
+        Assert::assertGreaterThan(2, $userGroups[0]->getId());
+        Assert::assertGreaterThan(2, $userGroups[1]->getId());
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
+    }
+
+}
