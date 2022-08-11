179be2a1562f96bac221d29814b7dd9c72d98a36
Tseho <quentin.favrie@gmail.com>
2022-08-11 15:57:56 +0200

    CXP-1245: improve useFindCriterionByField unit test

--- components/catalogs/front/src/components/ProductSelection/hooks/useFindCriterionByField.unit.ts
+++ components/catalogs/front/src/components/ProductSelection/hooks/useFindCriterionByField.unit.ts
@@ -2,16 +2,19 @@ jest.unmock('./useFindCriterionByField');
 
 import {renderHook} from '@testing-library/react-hooks';
 import {useFindCriterionByField} from './useFindCriterionByField';
+import {AnyCriterion} from '../models/Criterion';
+import StatusCriterion from '../criteria/StatusCriterion';
+import FamilyCriterion from '../criteria/FamilyCriterion';
 
-const criterions: string[] = ['enabled', 'family'];
+const criterions: [string, AnyCriterion][] = [
+    ['enabled', StatusCriterion],
+    ['family', FamilyCriterion],
+];
 
-test.each(criterions)('it returns a criterion when searching for "%s"', async field => {
+test.each(criterions)('it returns a criterion when searching for "%s"', async (field, criterion) => {
     const {result} = renderHook(() => useFindCriterionByField());
 
-    expect(await result.current(field)).toMatchObject({
-        component: expect.any(Function),
-        factory: expect.any(Function),
-    });
+    expect(await result.current(field)).toMatchObject(criterion);
 });
 
 test('it throws when searching for an unknown field', async () => {
