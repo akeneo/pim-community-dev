0433d5cc4dfe6a85f71944c619e80ef58a7415b1
Antoine Trouve <antoine.trouve@akeneo.com>
2022-08-11 10:30:44 +0200

    GRF-121 : Create Template Models.

---
+++ src/Akeneo/Category/back/Domain/Model/Template.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Category\Domain\Model;
+
+use Akeneo\Category\Domain\ValueObject\CategoryId;
+use Akeneo\Category\Domain\ValueObject\LabelCollection;
+use Akeneo\Category\Domain\ValueObject\TemplateCode;
+use Akeneo\Category\Domain\ValueObject\TemplateId;
+
+/**
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class Template
+{
+    public function __construct(
+        private TemplateId $id,
+        private TemplateCode $code,
+        private LabelCollection $labelCollection,
+        private ?CategoryId $parentId
+    ) {
+    }
+
+    public function getId(): TemplateId
+    {
+        return $this->id;
+    }
+
+    public function getCode(): TemplateCode
+    {
+        return $this->code;
+    }
+
+    public function getLabelCollection(): LabelCollection
+    {
+        return $this->labelCollection;
+    }
+
+    public function getParentId(): ?CategoryId
+    {
+        return $this->parentId;
+    }
+}
