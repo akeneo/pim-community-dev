1bc81f71660d611336598267927ca9f8a5f456a7
Antoine T <antoine.trouve@akeneo.com>
2022-08-11 16:07:01 +0200

    GRF-220 : Upsert Category command handler (#17631)
    * GRF-220 : create query get category by code
    
    * GRF-220 : create category events
    
    * GRF-220 : Validate the command
    
    * GRF-220 : Mock category updater
    
    * GRF-121 : Delete SetCode user intent which will be not an intent.

---
+++ src/Akeneo/Category/back/Application/UpsertCategoryCommandHandler.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Category\Application;
+
+use Akeneo\Category\Api\Command\Exceptions\ViolationsException;
+use Akeneo\Category\Api\Command\UpsertCategoryCommand;
+use Akeneo\Category\Api\Event\CategoryCreatedEvent;
+use Akeneo\Category\Api\Event\CategoryUpdatedEvent;
+use Akeneo\Category\Application\Applier\UserIntentApplierRegistry;
+use Akeneo\Category\Application\Query\FindCategoryByCode;
+use Akeneo\Category\Application\Storage\ProcessCategoryUpdateMock;
+use Akeneo\Category\Domain\Model\Category;
+use Exception;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class UpsertCategoryCommandHandler
+{
+    public function __construct(
+        private ValidatorInterface $validator,
+        private FindCategoryByCode $findCategoryByCode,
+        private UserIntentApplierRegistry $applierRegistry,
+        private EventDispatcherInterface $eventDispatcher,
+        private ProcessCategoryUpdateMock $saver
+    ) {
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function __invoke(UpsertCategoryCommand $command): void
+    {
+        $this->validateCommand($command);
+
+        $category = ($this->findCategoryByCode)($command->categoryCode());
+
+        $isCreation = null === $category;
+        if ($isCreation) {
+            throw new Exception("Command to create a category is in progress.");
+        }
+
+        $this->updateCategory($category, $command);
+
+        $this->saver->update($category, $command->userIntents());
+
+        if ($isCreation) {
+            $this->eventDispatcher->dispatch(new CategoryCreatedEvent((string)$category->getCode()));
+        } else {
+            $this->eventDispatcher->dispatch(new CategoryUpdatedEvent((string)$category->getCode()));
+        }
+    }
+
+    private function updateCategory(Category $category, UpsertCategoryCommand $command): void
+    {
+        foreach ($command->userIntents() as $userIntent) {
+            $applier = $this->applierRegistry->getApplier($userIntent);
+            if (null === $applier) {
+                throw new \InvalidArgumentException(
+                    \sprintf('The "%s" intent cannot be handled.', get_class($userIntent))
+                );
+            }
+
+            try {
+                $applier->apply($userIntent, $category);
+            } catch (\LogicException $exception) {
+                $violations = new ConstraintViolationList([
+                    new ConstraintViolation(
+                        message: $exception->getMessage(),
+                        messageTemplate: $exception->getMessage(),
+                        parameters: [],
+                        root: $command,
+                        propertyPath: $applier::class,
+                        invalidValue: $userIntent
+                    ),
+                ]);
+
+                throw new ViolationsException($violations);
+            }
+        }
+    }
+
+    private function validateCommand(UpsertCategoryCommand $command): void
+    {
+        $violations = $this->validator->validate($command);
+        if (0 < $violations->count()) {
+            throw new ViolationsException($violations);
+        }
+    }
+}
