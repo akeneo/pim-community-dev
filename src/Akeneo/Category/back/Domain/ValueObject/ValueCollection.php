51604dc12ebf7165cd7f1c10c0a8f63a74505bc1
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-08-12 11:12:07 +0200

    GRF-234: Remove useless use

--- src/Akeneo/Category/back/Domain/ValueObject/ValueCollection.php
+++ src/Akeneo/Category/back/Domain/ValueObject/ValueCollection.php
@@ -2,8 +2,6 @@
 
 namespace Akeneo\Category\Domain\ValueObject;
 
-use Webmozart\Assert\Assert;
-
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
