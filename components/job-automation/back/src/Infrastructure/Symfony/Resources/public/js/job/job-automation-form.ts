a0bbbe5284afbc0526f358db11802f47543c9324
Grégoire Houssard <35272857+ghoussard@users.noreply.github.com>
2022-08-12 14:17:56 +0200

    Fix front type checking (#14555)

--- components/job-automation/back/src/Infrastructure/Symfony/Resources/public/js/job/job-automation-form.ts
+++ components/job-automation/back/src/Infrastructure/Symfony/Resources/public/js/job/job-automation-form.ts
@@ -65,7 +65,6 @@ class JobAutomationFormController extends BaseView {
 
   getDefaultAutomation(): Automation {
     return {
-      is_enabled: false,
       cron_expression: '0 0 * * *',
       running_user_groups: userContext.get('groups'),
       notification_user_groups: [],
