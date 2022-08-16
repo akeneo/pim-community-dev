2f227e9f3540a9bb1491d1c0b6d272bba4608893
Samir <samir.boulil@akeneo.com>
2022-08-16 17:33:48 +0200

    CPM-691: quick fix

--- upgrades/test_schema/Version_7_0_20220728121643_remove_uuid_triggers_Integration.php
+++ upgrades/test_schema/Version_7_0_20220728121643_remove_uuid_triggers_Integration.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Pim\Upgrade\Schema;
+namespace Pim\Upgrade\Schema\Tests;
 
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Doctrine\DBAL\Connection;
-use Pim\Upgrade\Schema\Tests\ExecuteMigrationTrait;
-
+use Pim\Upgrade\Schema\Version_7_0_20220728121643_remove_uuid_triggers;
 
 /**
  * Remove triggers added in the UUID migration which add triggers on foreign uuid column.
@@ -18,6 +17,38 @@ final class Version_7_0_20220728121643_remove_uuid_triggers_Integration extends 
 {
     use ExecuteMigrationTrait;
     private const MIGRATION_LABEL = '_7_0_20220728121643_remove_uuid_triggers';
+    public const TRIGGERS_TO_REMOVE = [
+        'pim_catalog_association_uuid_insert',
+        'pim_catalog_association_uuid_update',
+        'pim_catalog_association_product_uuid_insert',
+        'pim_catalog_association_product_uuid_update',
+        'pim_catalog_association_product_model_to_product_uuid_insert',
+        'pim_catalog_association_product_model_to_product_uuid_update',
+        'pim_catalog_category_product_uuid_insert',
+        'pim_catalog_category_product_uuid_update',
+        'pim_catalog_group_product_uuid_insert',
+        'pim_catalog_group_product_uuid_update',
+        'pim_catalog_product_unique_data_uuid_insert',
+        'pim_catalog_product_unique_data_uuid_update',
+        'pim_catalog_completeness_uuid_insert',
+        'pim_catalog_completeness_uuid_update',
+        'pim_dqi_product_criteria_evaluation_uuid_insert',
+        'pim_dqi_product_criteria_evaluation_uuid_update',
+        'pim_dqi_product_score_uuid_insert',
+        'pim_dqi_product_score_uuid_update',
+        'pim_versioning_version_uuid_insert',
+        'pim_versioning_version_uuid_update',
+        'pim_comment_comment_uuid_insert',
+        'pim_comment_comment_uuid_update',
+        'pimee_workflow_product_draft_uuid_insert',
+        'pimee_workflow_product_draft_uuid_update',
+        'pimee_workflow_published_product_uuid_insert',
+        'pimee_workflow_published_product_uuid_update',
+        'pimee_twa_completeness_per_attribute_group_uuid_insert',
+        'pimee_twa_completeness_per_attribute_group_uuid_update',
+        'pimee_twa_project_product_uuid_insert',
+        'pimee_twa_project_product_uuid_update',
+    ];
 
     private Connection $connection;
 
@@ -49,7 +80,7 @@ IF NEW.product_uuid IS NULL THEN SET NEW.category_id = 1;
 END IF;  
 END
 SQL;
-        foreach(Version_7_0_20220728121643_remove_uuid_triggers::TRIGGERS_TO_REMOVE as $triggerName) {
+        foreach(self::TRIGGERS_TO_REMOVE as $triggerName) {
             $query = \strtr($createDummyTriggerQuery, ['{trigger_name}' => $triggerName]);
             $this->connection->executeStatement($query);
         }
