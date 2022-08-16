55b447d88b886297a03d6fb8f137c038d6d95bd4
ValentinMerlet <merlet.valentin@gmail.com>
2022-08-16 12:44:20 +0200

    SP-238: Remove contributor authentication feature flag

--- components/supplier-portal-supplier/back/src/Application/Authentication/ContributorAccount/Subscriber/CreateContributorAccountOnContributorAdded.php
+++ components/supplier-portal-supplier/back/src/Application/Authentication/ContributorAccount/Subscriber/CreateContributorAccountOnContributorAdded.php
@@ -2,7 +2,6 @@
 
 namespace Akeneo\SupplierPortal\Supplier\Application\Authentication\ContributorAccount\Subscriber;
 
-use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlags;
 use Akeneo\SupplierPortal\Retailer\Domain\Supplier\Write\Event\ContributorAdded;
 use Akeneo\SupplierPortal\Supplier\Application\Authentication\ContributorAccount\CreateContributorAccount;
 use Akeneo\SupplierPortal\Supplier\Application\Authentication\ContributorAccount\CreateContributorAccountHandler;
@@ -10,10 +9,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class CreateContributorAccountOnContributorAdded implements EventSubscriberInterface
 {
-    public function __construct(
-        private CreateContributorAccountHandler $createContributorAccountHandler,
-        private FeatureFlags $featureFlags,
-    ) {
+    public function __construct(private CreateContributorAccountHandler $createContributorAccountHandler)
+    {
     }
 
     public static function getSubscribedEvents(): array
@@ -25,10 +22,6 @@ class CreateContributorAccountOnContributorAdded implements EventSubscriberInter
 
     public function contributorAdded(ContributorAdded $contributorAdded): void
     {
-        if (!$this->featureFlags->isEnabled('supplier_portal_contributor_authentication')) {
-            return;
-        }
-
         ($this->createContributorAccountHandler)(new CreateContributorAccount($contributorAdded->contributorEmail()));
     }
 }
