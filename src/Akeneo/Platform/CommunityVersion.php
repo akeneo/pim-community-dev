ff47a20b1b591b1bb6431472f4c3229d1b5c1d00
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-08-23 14:57:27 +0000

    Preparing version 5.0.110

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.109';
+    const VERSION = '5.0.110';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
