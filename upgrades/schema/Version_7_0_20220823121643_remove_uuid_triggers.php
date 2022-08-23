89428cf4fece018384600408e11b238985efbbe1
Samir <samir.boulil@akeneo.com>
2022-08-23 15:13:26 -0400

    CPM-691: Remove product id related columns

--- upgrades/schema/Version_7_0_20220823121643_remove_uuid_triggers.php
+++ upgrades/schema/Version_7_0_20220823121643_remove_uuid_triggers.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Pim\Upgrade\Schema;
 
+use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Exception\IrreversibleMigration;
@@ -12,7 +13,7 @@ use Doctrine\Migrations\Exception\IrreversibleMigration;
  * Remove triggers added in the UUID migration which add triggers on foreign uuid column.
  * @see https://github.com/akeneo/pim-community-dev/blob/77be7d26721554834bbbabae39bf6f11a90f77ac/src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidAddTriggers.php#L15
  */
-final class Version_7_0_20220728121643_remove_uuid_triggers extends AbstractMigration
+final class Version_7_0_20220823121643_remove_uuid_triggers extends AbstractMigration
 {
     public const TRIGGERS_TO_REMOVE = [
         'pim_catalog_association_uuid_insert',
@@ -46,21 +47,60 @@ final class Version_7_0_20220728121643_remove_uuid_triggers extends AbstractMigr
         'pimee_twa_project_product_uuid_insert',
         'pimee_twa_project_product_uuid_update',
     ];
+    private const TABLES_TO_UPDATE = [
+        'pim_catalog_association' => 'owner_id',
+        'pim_catalog_association_product' => 'product_id',
+        'pim_catalog_association_product_model_to_product' => 'product_id',
+        'pim_catalog_category_product' => 'product_id',
+        'pim_catalog_group_product' => 'product_id',
+        'pim_catalog_product_unique_data' => 'product_id',
+        'pim_catalog_completeness' => 'product_id',
+        'pim_data_quality_insights_product_criteria_evaluation' => 'product_id',
+        'pim_data_quality_insights_product_score' => 'product_id',
+        'pimee_teamwork_assistant_completeness_per_attribute_group' => 'product_id',
+        'pimee_teamwork_assistant_project_product' => 'product_id',
+        'pimee_workflow_product_draft' => 'product_id',
+        'pimee_workflow_published_product' => 'original_product_id',
+    ];
 
     public function getDescription(): string
     {
-        return 'Remove UUID triggers';
+        return 'Remove UUID triggers and product id related columns';
     }
 
     public function up(Schema $schema): void
     {
-        foreach(self::TRIGGERS_TO_REMOVE as $triggerToRemove) {
-            $this->addSql(sprintf('DROP TRIGGER IF EXISTS %s', $triggerToRemove));
-        }
+        $this->dropTriggers();
+        $this->dropProductIdColumns();
     }
 
     public function down(Schema $schema): void
     {
         throw new IrreversibleMigration();
+    }
+
+    private function dropTriggers(): void
+    {
+        foreach (self::TRIGGERS_TO_REMOVE as $triggerToRemove) {
+            $this->addSql(sprintf('DROP TRIGGER IF EXISTS %s', $triggerToRemove));
+        }
+    }
+
+    private function dropProductIdColumns(): void
+    {
+        foreach (self::TABLES_TO_UPDATE as $table => $column) {
+            if ($this->columnExists($table, $column)) {
+                $this->addSql(sprintf('ALTER TABLE %s DROP COLUMN %s', $table, $column));
+            }
+        }
+    }
+
+    private function columnExists(string $table, string $columnName): bool
+    {
+        $tableColumnNames = array_map(
+            static fn(Column $column) => $column->getName(),
+            $this->connection->getSchemaManager()->listTableColumns($table)
+        );
+        return in_array($columnName, $tableColumnNames);
     }
 }
--- upgrades/test_schema/Version_7_0_20220823121643_remove_uuid_triggers_Integration.php
+++ upgrades/test_schema/Version_7_0_20220823121643_remove_uuid_triggers_Integration.php
@@ -7,16 +7,20 @@ namespace Pim\Upgrade\Schema\Tests;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Doctrine\DBAL\Connection;
-use Pim\Upgrade\Schema\Version_7_0_20220728121643_remove_uuid_triggers;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\TableDiff;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
+use Pim\Upgrade\Schema\Version_7_0_20220823121643_remove_uuid_triggers;
 
 /**
  * Remove triggers added in the UUID migration which add triggers on foreign uuid column.
  * @see https://github.com/akeneo/pim-community-dev/blob/77be7d26721554834bbbabae39bf6f11a90f77ac/src/Akeneo/Pim/Enrichment/Bundle/Command/MigrateToUuid/MigrateToUuidAddTriggers.php#L15
  */
-final class Version_7_0_20220728121643_remove_uuid_triggers_Integration extends TestCase
+final class Version_7_0_20220823121643_remove_uuid_triggers_Integration extends TestCase
 {
     use ExecuteMigrationTrait;
-    private const MIGRATION_LABEL = '_7_0_20220728121643_remove_uuid_triggers';
+    private const MIGRATION_LABEL = '_7_0_20220823121643_remove_uuid_triggers';
     public const TRIGGERS_TO_REMOVE = [
         'pim_catalog_association_uuid_insert',
         'pim_catalog_association_uuid_update',
@@ -49,6 +53,21 @@ final class Version_7_0_20220728121643_remove_uuid_triggers_Integration extends 
         'pimee_twa_project_product_uuid_insert',
         'pimee_twa_project_product_uuid_update',
     ];
+    private const TABLES_TO_UPDATE = [
+        'pim_catalog_association' => 'owner_id',
+        'pim_catalog_association_product' => 'product_id',
+        'pim_catalog_association_product_model_to_product' => 'product_id',
+        'pim_catalog_category_product' => 'product_id',
+        'pim_catalog_group_product' => 'product_id',
+        'pim_catalog_product_unique_data' => 'product_id',
+        'pim_catalog_completeness' => 'product_id',
+        'pim_data_quality_insights_product_criteria_evaluation' => 'product_id',
+        'pim_data_quality_insights_product_score' => 'product_id',
+        'pimee_teamwork_assistant_completeness_per_attribute_group' => 'product_id',
+        'pimee_teamwork_assistant_project_product' => 'product_id',
+        'pimee_workflow_product_draft' => 'product_id',
+        'pimee_workflow_published_product' => 'original_product_id',
+    ];
 
     private Connection $connection;
 
@@ -58,11 +77,15 @@ final class Version_7_0_20220728121643_remove_uuid_triggers_Integration extends 
         $this->connection = $this->get('database_connection');
     }
 
-    public function test_it_removes_the_triggers(): void
+    public function test_it_removes_the_triggers_and_the_product_id_columns(): void
     {
         $this->createDummyTriggersToBeRemoved();
+        $this->createDummyColumnsToBeRemoved();
+
         $this->reExecuteMigration(self::MIGRATION_LABEL);
+
         $this->assertTriggersRemoved();
+        $this->assertColumnsRemoved();
     }
 
     protected function getConfiguration(): Configuration
@@ -91,5 +114,30 @@ SQL;
         $stmt = $this->connection->executeQuery('SHOW TRIGGERS;');
         $result = $stmt->fetchFirstColumn();
         $this->assertEmpty($result);
+    }
+
+    private function assertColumnsRemoved(): void
+    {
+        foreach (self::TABLES_TO_UPDATE as $table => $productIdColumnName) {
+            $tableColumnNames = array_map(
+                static fn(Column $column) => $column->getName(),
+                $this->connection->getSchemaManager()->listTableColumns($table)
+            );
+            $this->assertNotContains(
+                $productIdColumnName,
+                $tableColumnNames,
+                sprintf('Expected column "%s" to not exist on table "%s". Column found.', $productIdColumnName, $table)
+            );
+        }
+    }
+
+    private function createDummyColumnsToBeRemoved(): void
+    {
+        foreach (self::TABLES_TO_UPDATE as $table => $productIdColumnName) {
+            if ($this->connection->getSchemaManager()->tablesExist($table)) {
+                $query = sprintf('ALTER TABLE %s ADD COLUMN %s VARCHAR(255)', $table, $productIdColumnName);
+                $this->connection->executeStatement($query);
+            }
+        }
     }
 }
