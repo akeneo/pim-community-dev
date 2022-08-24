7e58fb449c88954cf76ae0bdfc2f1f19480a4d25
pierallard <pierallard@gmail.com>
2022-08-24 09:00:15 +0200

    PIM-10577: Move summernote into our lib folder

--- frontend/webpack/requirejs-utils.js
+++ frontend/webpack/requirejs-utils.js
@@ -81,7 +81,6 @@ const utils = {
             baseDir,
             './vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.js'
           ),
-          summernote: path.resolve(baseDir, 'node_modules/summernote/dist/summernote.js'),
           jquery: require.resolve('jquery'),
           underscore: require.resolve('underscore'),
           'pim/dropzonejs': require.resolve('dropzone'),
