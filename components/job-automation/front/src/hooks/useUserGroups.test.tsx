f21f6376d633486ce9f947a3c325be0c4b10dffc
anthony-grandin <anthony.grandin@getakeneo.com>
2022-08-24 10:51:36 +0200

    RAB-968: test load next page in hooks

--- components/job-automation/front/src/hooks/useUserGroups.test.tsx
+++ components/job-automation/front/src/hooks/useUserGroups.test.tsx
@@ -1,5 +1,9 @@
 import {renderHookWithProviders} from '@akeneo-pim-community/shared';
 import {useUserGroups} from './useUserGroups';
+import {useUsers} from "./useUsers";
+import TestRenderer from 'react-test-renderer';
+
+const {act} = TestRenderer;
 
 test('it return fetched user groups', async () => {
   global.fetch = jest.fn().mockImplementation(async () => ({
@@ -20,3 +24,28 @@ test('it return fetched user groups', async () => {
     method: 'GET',
   });
 });
+
+test('it load next page', async () => {
+  global.fetch = jest.fn().mockImplementation(async () => ({
+    ok: true,
+    json: async () => [{'id': 1, 'label': 'IT Support'}, {'id': 2, 'label': 'Manager'}],
+  }));
+
+  const {result, waitForNextUpdate} = renderHookWithProviders(() => useUserGroups());
+  await waitForNextUpdate();
+
+  global.fetch = jest.fn().mockImplementation(async () => ({
+    ok: true,
+    json: async () => [{'id': 3, 'label': 'Redactor'}, {'id': 4, 'label': 'QA'}],
+  }));
+
+  const loadNexPage = result.current.loadNextPage;
+  await act(async () => await loadNexPage());
+
+  expect(result.current.availableUserGroups).toEqual([
+    {'id': 1, 'label': 'IT Support'},
+    {'id': 2, 'label': 'Manager'},
+    {'id': 3, 'label': 'Redactor'},
+    {'id': 4, 'label': 'QA'}
+  ]);
+})
--- components/job-automation/front/src/hooks/useUsers.test.tsx
+++ components/job-automation/front/src/hooks/useUsers.test.tsx
@@ -1,5 +1,8 @@
 import {renderHookWithProviders} from '@akeneo-pim-community/shared';
 import {useUsers} from './useUsers';
+import TestRenderer from 'react-test-renderer';
+
+const {act} = TestRenderer;
 
 test('it return fetched users', async () => {
   global.fetch = jest.fn().mockImplementation(async () => ({
@@ -20,3 +23,28 @@ test('it return fetched users', async () => {
     method: 'GET',
   });
 });
+
+test('it load next page', async () => {
+  global.fetch = jest.fn().mockImplementation(async () => ({
+    ok: true,
+    json: async () => [{'id': 1, 'username': 'Admin'}, {'id': 2, 'username': 'Julia'}],
+  }));
+
+  const {result, waitForNextUpdate} = renderHookWithProviders(() => useUsers());
+  await waitForNextUpdate();
+
+  global.fetch = jest.fn().mockImplementation(async () => ({
+    ok: true,
+    json: async () => [{'id': 3, 'username': 'Jean'}, {'id': 4, 'username': 'Michel'}],
+  }));
+
+  const loadNexPage = result.current.loadNextPage;
+  await act(async () => await loadNexPage());
+
+  expect(result.current.availableUsers).toEqual([
+    {'id': 1, 'username': 'Admin'},
+    {'id': 2, 'username': 'Julia'},
+    {'id': 3, 'username': 'Jean'},
+    {'id': 4, 'username': 'Michel'},
+  ]);
+})
