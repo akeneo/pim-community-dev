0bfcf1776bacf3961b6113b3ade7e88ea2f7b7c6
Adrien Pétremann <grena@users.noreply.github.com>
2022-08-24 15:03:49 +0200

    RAB-928: Launch valid scheduled jobs (#17649)
    * Bring back RAB-970 changes
    
    * Bring back RAB-941 changes
    
    * RAB-928: Add a spec for JobInstanceUpdater
    
    * RAB-928: refresh job instance automation
    
    * RAB-928: move scheduled validation inside JobInstanceValidator
    
    * RAB-928: validate job inside publisher
    
    * RAB-928: fix tests
    
    * RAB-928: fix FixturesJobHelper
    
    * RAB-928: return types
    
    * RAB-928: fix lint
    
    * RAB-928: add return types
    
    * RAB-928: add copyright & final
    
    * RAB-928: fix JobInstanceUpdater
    
    * RAB-928: add a spec
    
    * RAB-928: fix lint
    
    * RAB-928: Fix reviews
    
    * RAB-928: Fixing
    
    * RAB-928: Fix merge
    
    * RAB-928: Fix tests
    
    Co-authored-by: Grégoire Houssard <gregoire.houssard@akeneo.com>
    Co-authored-by: axelhms <hemissi.axel@gmail.com>

--- src/Akeneo/Tool/Component/Batch/Clock/SystemClock.php
+++ src/Akeneo/Tool/Component/Batch/Clock/SystemClock.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Platform\Job\Infrastructure\Clock;
+namespace Akeneo\Tool\Component\Batch\Clock;
 
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class SystemClock implements ClockInterface
+final class SystemClock implements ClockInterface
 {
     public function now(): \DateTimeImmutable
     {
