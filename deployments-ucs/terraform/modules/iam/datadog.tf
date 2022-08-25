ed348ac4aaff1c33c6dac8cfd4a14486f9ad0fc9
Vincent <vincent.gelot@akeneo.com>
2022-08-25 12:05:54 +0200

    add datadog_api_key empty creation

---
+++ deployments-ucs/terraform/modules/iam/datadog.tf
@@ -0,0 +1,28 @@
+resource "google_secret_manager_secret" "datadog_api_key" {
+  project   = var.project_id
+  secret_id = "DATADOG_API_KEY"
+
+  labels = {
+    usage = "datadog"
+  }
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_iam_binding" "datadog_api_key" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.datadog_api_key.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  members   = [
+    "serviceAccount:main-service-account@${var.project_id}.iam.gserviceaccount.com",
+  ]
+}
+
+resource "google_secret_manager_secret_iam_binding" "datadog_api_key" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.datadog_api_key.secret_id
+  role      = "roles/secretmanager.secretVersionManager"
+  members   = var.secrets_admins
+}
