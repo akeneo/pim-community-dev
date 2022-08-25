3548540d7a2bca6d18db28898de2ba33fc0847b1
jmleroux <jmleroux.pro@gmail.com>
2022-08-25 11:03:32 +0200

    PH-172: Purge versioning batch job

--- upgrades/schema/Version_7_0_20220823094700_add_scheduled_jobs.php
+++ upgrades/schema/Version_7_0_20220823094700_add_scheduled_jobs.php
@@ -4,21 +4,11 @@ declare(strict_types=1);
 
 namespace Pim\Upgrade\Schema;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-final class Version_7_0_20220822173400_add_scheduled_jobs extends AbstractMigration implements ContainerAwareInterface
+final class Version_7_0_20220822173400_add_scheduled_jobs extends AbstractMigration
 {
-    private ContainerInterface $container;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
     public function up(Schema $schema): void
     {
         $this->addScheduledJob('versioning_purge', 'Purge versions of entities', []);
@@ -26,46 +16,31 @@ final class Version_7_0_20220822173400_add_scheduled_jobs extends AbstractMigrat
 
     private function addScheduledJob(string $jobCode, string $label, array $rawParameters): void
     {
-        if (!$this->jobInstanceExists($jobCode)) {
-            $sql = <<<SQL
+        $sql = <<<SQL
             INSERT INTO akeneo_batch_job_instance 
                 (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
             VALUES
             (
                 :code,
                 :label,
+             
                 :code,
                 0,
                 'internal',
                 :raw_parameters,
                 'scheduled_job'
-            );
+            )
+            ON DUPLICATE KEY UPDATE code = code;
         SQL;
 
-            $this->addSql(
-                $sql,
-                ['code' => $jobCode, 'label' => $label, 'raw_parameters' => \serialize($rawParameters)]
-            );
-        }
+        $this->addSql(
+            $sql,
+            ['code' => $jobCode, 'label' => $label, 'raw_parameters' => \serialize($rawParameters)]
+        );
     }
 
     public function down(Schema $schema): void
     {
         $this->throwIrreversibleMigrationException();
-    }
-
-    private function jobInstanceExists(string $jobCode): bool
-    {
-        $sql = "SELECT id FROM akeneo_batch_job_instance WHERE code = :jobCode";
-
-        $jobId = $this->dbalConnection()->executeQuery($sql, ['jobCode' => $jobCode])
-                ->fetchFirstColumn()[0] ?? null;
-
-        return $jobId !== null;
-    }
-
-    private function dbalConnection(): Connection
-    {
-        return $this->container->get('database_connection');
     }
 }
