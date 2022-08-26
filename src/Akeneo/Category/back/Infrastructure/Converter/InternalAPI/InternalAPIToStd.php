3943ba4f550b54f99f4ef2f332ee88a572ab109a
Antoine Trouve <antoine.trouve@akeneo.com>
2022-08-26 10:14:50 +0200

    Create RichText value user intent applier

--- src/Akeneo/Category/back/Infrastructure/Converter/InternalAPI/InternalAPIToStd.php
+++ src/Akeneo/Category/back/Infrastructure/Converter/InternalAPI/InternalAPIToStd.php
@@ -15,14 +15,18 @@ use Webmozart\Assert\Assert;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
+ * @phpstan-type InternalApi array{
+ *     properties: PropertyApi,
+ *     attributes: array<string, AttributeCodeApi|AttributeValueApi>
+ * }
+ * @phpstan-type StandardInternalApi array{
+ *     code: string,
+ *     labels: array<string, string>
+ *     values: array<string, array<AttributeCodeApi|AttributeValueApi>>
+ * }
+ * @phpstan-type PropertyApi array{code: string, labels: array<string, string>}
  * @phpstan-type AttributeCodeApi array<string>
  * @phpstan-type AttributeValueApi array{data: string, locale: string|null, attribute_code: string}
- * @phpstan-type PropertyApi array{code: string, labels: array<string, string>}
- * @phpstan-type StandardInternalApi array{
- *      code: string,
- *      labels: array<string, string>
- *      values: array<string, array<AttributeCodeApi|AttributeValueApi>>
- * }
  */
 class InternalAPIToStd implements ConverterInterface
 {
@@ -38,11 +42,7 @@ class InternalAPIToStd implements ConverterInterface
      *     attributes: array<string, AttributeCodeApi|AttributeValueApi>
      * } $data
      *
-     * @retrun array{
-     *      code: string,
-     *      labels: array<string, string>
-     *      values: array<string, array<AttributeCodeApi|AttributeValueApi>>
-     * }
+     * @retrun array|StandardInternalApi
      */
     public function convert(array $data): array
     {
