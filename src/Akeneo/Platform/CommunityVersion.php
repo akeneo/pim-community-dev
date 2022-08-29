cdca125a91c6bb4ce92eebdcf2b45ee9bb777fa2
Michel Tag <micheltag@akeneo.com>
2022-08-29 15:31:38 +0200

    Preparing version 6.0.41 (#17785)
    Preparing version 6.0.41
    
    
    Co-authored-by: LevFlavien <flavien.levesque@akeneo.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.40';
+    public const VERSION = '6.0.41';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
