58c85ab32cfcfedd663fcfb790ad8a942fecf1ed
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-08-31 11:12:39 +0200

    Try to fix CI

--- .circleci/configure_docker_mirrors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-
-# see https://cloud.google.com/container-registry/docs/pulling-cached-images#configure
-echo '{"registry-mirrors": ["https://mirror.gcr.io"]}' >> /etc/docker/daemon.json
-
-service docker restart
