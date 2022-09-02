bbc7c86331d77cd66f08ad90072a201943791202
vgelot <vincent.gelot@akeneo.com>
2022-09-02 12:16:42 +0200

    PH-63 - Unify UCS pim namespace (#14717)

--- deployments-ucs/pim-saas-service/pim/scripts/pim-job-push-function-bucket.sh
+++ deployments-ucs/pim-saas-service/pim/scripts/pim-job-push-function-bucket.sh
@@ -2,8 +2,6 @@
 set -ex
 
 cd /tmp
-cp /ucs-function-cron-transformer/*.js .
-cp /ucs-function-cron-transformer/*.json .
-zip ${SOURCE_CODE_ZIP} *.js *.json
+zip -j ${SOURCE_CODE_ZIP} ${MOUNT_PATH}/*.js ${MOUNT_PATH}/*.json
 SA_TOKEN=$(curl -v -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token | jq '.access_token')
 curl -f -H "Authorization: OAuth ${SA_TOKEN}" -H "Content-Type: application/zip" -X POST -v --data-binary @${SOURCE_CODE_ZIP} "https://storage.googleapis.com/upload/storage/v1/b/"${BUCKET_NAME}"/o?uploadType=media&name="${SOURCE_CODE_ZIP}
