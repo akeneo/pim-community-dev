dc29f23dc8f5226597692894a7534995bfe960db
Valentin Dijkstra <valentindijkstra@gmail.com>
2022-09-02 15:48:23 -0400

    RAB-1039: Fix user normalizer sending visible groups ids as object

--- src/Akeneo/UserManagement/Component/Normalizer/UserNormalizer.php
+++ src/Akeneo/UserManagement/Component/Normalizer/UserNormalizer.php
@@ -169,7 +169,10 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
     /** @return int[] */
     private function getVisibleGroupIds(UserInterface $user): array
     {
-        $visibleGroups = array_filter($user->getGroups()->toArray(), static fn (Group $group) => $group->getName() !== 'All');
+        $visibleGroups = array_values(array_filter(
+            $user->getGroups()->toArray(),
+            static fn (Group $group) => $group->getName() !== 'All',
+        ));
 
         return array_map(static fn (Group $group) => $group->getId(), $visibleGroups);
     }
