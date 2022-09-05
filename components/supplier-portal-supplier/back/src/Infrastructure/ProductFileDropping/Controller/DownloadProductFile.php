70afc78a42d2dad2f0733896f8fb2216dcc1e1e9
ValentinMerlet <merlet.valentin@gmail.com>
2022-09-05 09:04:44 +0200

    SP-254: Fix the add of a .xlsx file extension when downloading a product file

--- components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Controller/DownloadProductFile.php
+++ components/supplier-portal-supplier/back/src/Infrastructure/ProductFileDropping/Controller/DownloadProductFile.php
@@ -30,7 +30,7 @@ final class DownloadProductFile
         }
 
         $headers['Content-Disposition'] = sprintf(
-            'attachment; filename="%s.xlsx"',
+            'attachment; filename="%s"',
             $productFile->originalFilename,
         );
 
