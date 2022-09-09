13b753ad238c9d5795558ec32128b9763e8cb002
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-09-09 13:29:06 +0200

    PIM-10568: fix migration

--- upgrades/schema/Version_7_0_20220629142647_dqi_update_pk_on_product_score.php
+++ upgrades/schema/Version_7_0_20220629142647_dqi_update_pk_on_product_score.php
@@ -11,14 +11,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
-
-final class Version_7_0_20220826101252_dqi_update_pk_on_product_score extends AbstractMigration
+final class Version_7_0_20220629142647_dqi_update_pk_on_product_score extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         $this->skipIf(
-            $this->isProductUuidAlreadyPrimaryAndNotEvaluatedAt($schema),
-            'product_uuid column is already the primary key of pim_data_quality_insights_product_score and no more evaluated_at'
+            $this->isProductUuidAlreadyPrimary($schema),
+            'product_uuid column is already the primary key of pim_data_quality_insights_product_score'
         );
 
         $this->runCommand('pim:data-quality-insights:clean-product-scores');
@@ -28,14 +27,14 @@ final class Version_7_0_20220826101252_dqi_update_pk_on_product_score extends Ab
         $this->runCommand('pim:data-quality-insights:populate-product-models-scores-and-ki');
     }
 
-    private function isProductUuidAlreadyPrimaryAndNotEvaluatedAt(Schema $schema): bool
+    private function isProductUuidAlreadyPrimary(Schema $schema): bool
     {
         $productScoreTable = $schema->getTable('pim_data_quality_insights_product_score');
 
-        $isProductUuidPrimary = in_array('product_uuid', $productScoreTable->getPrimaryKeyColumns());
-        $isEvaluatedAtNotAPrimary = !in_array('evaluated_at', $productScoreTable->getPrimaryKeyColumns());
+        $hasUuidColumn = $productScoreTable->hasColumn('product_uuid');
+        $isPrimary = in_array('product_uuid', $productScoreTable->getPrimaryKeyColumns());
 
-        return $isProductUuidPrimary && $isEvaluatedAtNotAPrimary;
+        return $hasUuidColumn && $isPrimary;
     }
 
     private function runCommand(string $commandName) {
