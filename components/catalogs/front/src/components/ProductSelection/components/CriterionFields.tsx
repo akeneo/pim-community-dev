25424b05bd2cb5ea8211134ade4f178e2fab4a93
Riff <riff.ventura@gmail.com>
2022-09-13 17:40:41 +0200

    OCT-118: Fixed width issues for multiple date and background color for measurement unit

--- components/catalogs/front/src/components/ProductSelection/components/CriterionFields.tsx
+++ components/catalogs/front/src/components/ProductSelection/components/CriterionFields.tsx
@@ -10,4 +10,5 @@ export const CriterionFields = styled.div`
 export const CriterionField = styled.div<{width?: number}>`
     flex-basis: ${({width = 160}) => `${width}px`};
     flex-shrink: 0;
+    width: ${({width = 160}) => `${width}px`};
 `;
