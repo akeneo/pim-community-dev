61410d8b1d02847245cded9df1685d317d8a985d
anthony-grandin <anthony.grandin@getakeneo.com>
2022-09-14 17:44:49 +0200

    RAB-959: implements operation and applier

---
+++ components/tailored-import/back/src/Domain/Model/Operation/SimpleReferenceEntityReplacementOperation.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Akeneo\Platform\TailoredImport\Domain\Model\Operation;
+
+final class SimpleReferenceEntityReplacementOperation extends AbstractReplacementOperation
+{
+    public const TYPE = 'simple_reference_entity_replacement';
+
+    public function normalize(): array
+    {
+        return [
+            'type' => self::TYPE,
+            'uuid' => $this->uuid,
+            'mapping' => $this->getMapping()
+        ];
+    }
+}
