bb557dfc19d5aee8652f16384a5d8982f6d76eda
ValentinMerlet <merlet.valentin@gmail.com>
2022-09-16 12:01:23 +0200

    SP-294: Add front unit tests

---
+++ components/supplier-portal-retailer/front/src/feature/product-file-dropping/components/CommentList.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {screen} from '@testing-library/react';
+import {renderWithProviders} from '@akeneo-pim-community/shared';
+import {CommentList} from './CommentList';
+import {Comment} from '../models/read/Comment';
+
+const comments: Comment[] = [
+    {
+        authorEmail: 'julia@akeneo.com',
+        content: 'This file is outdated, please send 2022 version instead.',
+        createdAt: '2022-09-22T04:08:00+00:00',
+        isRetailer: true,
+    },
+    {
+        authorEmail: 'jimmy@supplier.com',
+        content: 'Can you explain a bit more? I’m sure this is the right file.',
+        createdAt: '2022-09-22T10:32:00+00:00',
+        isRetailer: false,
+    },
+    {
+        authorEmail: 'julia@akeneo.com',
+        content: 'It does not fit our 2022 company standards.',
+        createdAt: '2022-09-22T14:34:00+00:00',
+        isRetailer: true,
+    },
+];
+
+test('it renders a list of comments', () => {
+    renderWithProviders(<CommentList comments={comments} />);
+
+    expect(screen.queryAllByText('julia@akeneo.com').length).toBe(2);
+    expect(screen.queryAllByText('jimmy@supplier.com').length).toBe(1);
+    expect(screen.getByText('09/22/2022, 04:08 AM')).toBeInTheDocument();
+    expect(screen.getByText('09/22/2022, 10:32 AM')).toBeInTheDocument();
+    expect(screen.getByText('09/22/2022, 02:34 PM')).toBeInTheDocument();
+    expect(screen.getByText('"This file is outdated, please send 2022 version instead."')).toBeInTheDocument();
+    expect(screen.getByText('"Can you explain a bit more? I’m sure this is the right file."')).toBeInTheDocument();
+    expect(screen.getByText('"It does not fit our 2022 company standards."')).toBeInTheDocument();
+});
+
+test('it displays nothing when there is no comments', () => {
+    renderWithProviders(<CommentList comments={[]} />);
+
+    expect(
+        screen.queryByText('supplier_portal.product_file_dropping.supplier_files.discussion.discussion_title')
+    ).not.toBeInTheDocument();
+});
---
+++ components/supplier-portal-retailer/front/src/feature/product-file-dropping/components/Discussion.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {act, fireEvent, screen} from '@testing-library/react';
+import {renderWithProviders} from '@akeneo-pim-community/shared';
+import {Discussion} from './Discussion';
+import userEvent from '@testing-library/user-event';
+
+const productFile = {
+    identifier: '037455b4-24a3-4404-a721-aca6f06d6293',
+    originalFilename: 'file.xlsx',
+    uploadedAt: '09/22/2022, 04:08 AM',
+    contributor: 'julia@akeneo.com',
+    supplier: 'ffa51317-e609-481e-b6a3-63991b4e6dbe',
+    retailerComments: [],
+    supplierComments: [],
+};
+
+test('it does not enable the send button if the comment textarea is not fulfilled', () => {
+    renderWithProviders(
+        <Discussion
+            productFile={productFile}
+            saveComment={(content: string, authorEmail: string) => {
+                return {};
+            }}
+            validationErrors={[]}
+        />
+    );
+
+    expect(
+        screen.getByText('supplier_portal.product_file_dropping.supplier_files.discussion.submit_button_label')
+    ).toBeDisabled();
+});
+
+test('it enables the send button enabled if the comment textarea is fulfilled', () => {
+    renderWithProviders(
+        <Discussion
+            productFile={productFile}
+            saveComment={(content: string, authorEmail: string) => {
+                return {};
+            }}
+            validationErrors={[]}
+        />
+    );
+
+    const commentInput = screen.getByLabelText(
+        'supplier_portal.product_file_dropping.supplier_files.discussion.comment_input_label'
+    );
+
+    fireEvent.change(commentInput, {target: {value: 'This file is outdated, please send 2022 version instead.'}});
+
+    expect(
+        screen.getByText('supplier_portal.product_file_dropping.supplier_files.discussion.submit_button_label')
+    ).not.toBeDisabled();
+});
+
+test('it can save a comment', async () => {
+    global.fetch = jest.fn().mockImplementation(async () => ({
+        ok: true,
+        json: async () => {},
+    }));
+
+    const saveComment = jest.fn();
+
+    renderWithProviders(<Discussion productFile={productFile} saveComment={saveComment} validationErrors={[]} />);
+
+    userEvent.type(
+        screen.getByLabelText('supplier_portal.product_file_dropping.supplier_files.discussion.comment_input_label'),
+        'This file is outdated, please send 2022 version instead.'
+    );
+
+    await act(async () => {
+        userEvent.click(
+            screen.getByText('supplier_portal.product_file_dropping.supplier_files.discussion.submit_button_label')
+        );
+    });
+
+    expect(saveComment).toHaveBeenCalledTimes(1);
+});
