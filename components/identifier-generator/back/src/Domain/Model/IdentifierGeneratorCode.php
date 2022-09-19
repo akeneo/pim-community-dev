47f33a0f7ed157254ead06c49858b4e3109c0eb6
antoinearrive <antoine.arrive@gmail.com>
2022-09-19 14:15:42 +0200

    CPM-726: Create Domain Objects

---
+++ components/identifier-generator/back/src/Domain/Model/IdentifierGeneratorCode.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class IdentifierGeneratorCode
+{
+    private function __construct(
+        private string $code,
+    ) {
+    }
+
+    public static function fromString(string $code): self
+    {
+        Assert::stringNotEmpty($code);
+
+        return new self($code);
+    }
+
+    public function asString(): string
+    {
+        return $this->code;
+    }
+}
---
+++ components/identifier-generator/back/src/Domain/Model/Target.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class Target
+{
+    private function __construct(
+        private string $attributeCode,
+    ) {
+    }
+
+    public static function fromString(string $attributeCode): self
+    {
+        Assert::stringNotEmpty($attributeCode);
+
+        return new self($attributeCode);
+    }
+
+    public function asString(): string
+    {
+        return $this->attributeCode;
+    }
+}
