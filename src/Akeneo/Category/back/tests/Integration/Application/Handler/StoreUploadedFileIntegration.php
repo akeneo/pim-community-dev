ad749afc9552f82505bd1d5b7a0bbd11d81994d5
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-09-20 15:17:24 +0200

    GRF-315: Rename FileHandler to StoreUploadedFile

--- src/Akeneo/Category/back/tests/Integration/Application/Handler/StoreUploadedFileIntegration.php
+++ src/Akeneo/Category/back/tests/Integration/Application/Handler/StoreUploadedFileIntegration.php
@@ -9,13 +9,13 @@ declare(strict_types=1);
 
 namespace Akeneo\Category\back\tests\Integration\Application\Handler;
 
-use Akeneo\Category\Application\Handler\FileHandler;
+use Akeneo\Category\Application\Handler\StoreUploadedFile;
 use Akeneo\Category\Domain\Filesystem\Storage;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class FileHandlerIntegration extends TestCase
+class StoreUploadedFileIntegration extends TestCase
 {
     /** @test */
     public function it_stores_file(): void
@@ -24,7 +24,7 @@ class FileHandlerIntegration extends TestCase
 
         $fileToUpload = new UploadedFile($fileInfo->getPathname(), $fileInfo->getFilename(), 'image/jpg');
 
-        $file = $this->get(FileHandler::class)->storeFile($fileToUpload);
+        $file = $this->get(StoreUploadedFile::class)->__invoke($fileToUpload);
 
         $fileStorage = $this->get('akeneo_file_storage.file_storage.filesystem_provider')->getFilesystem(Storage::CATEGORY_STORAGE_ALIAS);
         $this->assertTrue($fileStorage->fileExists($file->getKey()));
