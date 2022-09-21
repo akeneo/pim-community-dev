1d717e28d5220a014eb5f14a48197f7a7873c7d8
vgelot <vincent.gelot@akeneo.com>
2022-09-21 15:34:25 +0200

    PH-91 - Add trigger cloud build to deploy infra (#14940)

---
+++ deployments-ucs/terraform/modules/cloudbuild/cloudbuild.tf
@@ -0,0 +1,48 @@
+resource "google_cloudbuild_trigger" "pr-trigger" {
+  name            = "${var.trigger_name}-plan"
+  project         = var.project_id
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.cloudbuild_service_account}"
+  filename        = var.cloudbuild_filename
+  included_files  = var.cloudbuild_included_files
+
+  github {
+    owner = var.cloudbuild_github_org
+    name  = var.cloudbuild_github_repository
+    pull_request {
+      branch = "^${var.cloudbuild_github_branch}$"
+    }
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
+resource "google_cloudbuild_trigger" "master-trigger" {
+  name            = "${var.trigger_name}-apply"
+  project         = var.project_id
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.cloudbuild_service_account}"
+  filename        = var.cloudbuild_filename
+  included_files  = var.cloudbuild_included_files
+
+  github {
+    owner = var.cloudbuild_github_org
+    name  = var.cloudbuild_github_repository
+    push {
+      branch = "^${var.cloudbuild_github_branch}$"
+    }
+  }
+
+  approval_config {
+    approval_required = true
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
+resource "google_storage_bucket" "cloudbuild_logs" {
+  name          = "${var.project_id_target}-cloudbuild-logs"
+  location      = "EU"
+  force_destroy = true
+  project       = var.project_id
+
+  uniform_bucket_level_access = true
+}
--- deployments-ucs/terraform/shared/main.tf
+++ deployments-ucs/terraform/shared/main.tf
@@ -1,18 +1,19 @@
 locals {
-  project_id   = "akecld-prd-pim-saas-shared"
-  ci_sa        = "main-service-account@akecld-prd-pim-saas-dev.iam.gserviceaccount.com"
-  main_sa      = "main-service-account@${local.project_id}.iam.gserviceaccount.com"
-  admins       = [
+  project_id     = "akecld-prd-pim-saas-shared"
+  project_id_dev = "akecld-prd-pim-saas-dev"
+  ci_sa          = "main-service-account@akecld-prd-pim-saas-dev.iam.gserviceaccount.com"
+  main_sa        = "main-service-account@${local.project_id}.iam.gserviceaccount.com"
+  admins         = [
     "group:phoenix-squad@akeneo.com",
-    "serviceAccount:ci-service-account@akecld-prd-pim-saas-dev.iam.gserviceaccount.com" # because of the makefile
+    "serviceAccount:ci-service-account@akecld-prd-pim-saas-dev.iam.gserviceaccount.com" # To be able to push image in the registry
   ]
-  viewers      = [
+  viewers        = [
     "serviceAccount:argocd@akecld-prd-pim-saas-dev.iam.gserviceaccount.com",
     "serviceAccount:gke-sa@akecld-prd-pim-saas-dev.iam.gserviceaccount.com",
     "serviceAccount:cluster-bootstrap@akecld-prd-pim-saas-dev.iam.gserviceaccount.com",
   ]
-  region       = "europe-west1"
-  multi_region = "EU"
+  region         = "europe-west1"
+  multi_region   = "EU"
 }
 
 module "registry_dev" {
@@ -53,6 +54,18 @@ module "storage_bucket_prod" {
   viewer_members = concat(local.admins, ["serviceAccount:${local.ci_sa}"], local.viewers)
   bucket_name    = "akeneo-pim-saas-charts-prod"
   bucket_region  = local.multi_region
+}
+
+module "cloud_build_infra_dev" {
+  source                       = "../modules/cloudbuild"
+  project_id                   = local.project_id
+  project_id_target            = local.project_id_dev
+  trigger_name                 = local.project_id_dev
+  cloudbuild_filename          = ".cloudbuild/infra/${local.project_id_dev}.yaml"
+  cloudbuild_included_files    = [".cloudbuild/infra/${local.project_id_dev}.yaml", "deployments-ucs/terraform/dev/**", "deployments-ucs/terraform/modules/**"]
+  cloudbuild_github_repository = "pim-enterprise-dev"
+  cloudbuild_github_branch     = "master"
+  cloudbuild_service_account   = local.main_sa
 }
 
 terraform {
