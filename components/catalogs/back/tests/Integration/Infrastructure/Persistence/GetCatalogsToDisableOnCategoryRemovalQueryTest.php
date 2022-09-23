aa125de6b2583836b6f1d6d964fd080ac295ca7c
Guillaume Fer <guillaume.fer@akeneo.com>
2022-09-23 15:19:41 +0200

    CXP-1298: fix conflict

--- components/catalogs/back/tests/Integration/Infrastructure/Persistence/GetCatalogsToDisableOnCategoryRemovalQueryTest.php
+++ components/catalogs/back/tests/Integration/Infrastructure/Persistence/GetCatalogsToDisableOnCategoryRemovalQueryTest.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace Akeneo\Catalogs\Test\Integration\Infrastructure\Persistence;
 
 use Akeneo\Catalogs\Domain\Operator;
-use Akeneo\Catalogs\Infrastructure\Persistence\GetCatalogsToDisableOnCategoryRemovalQuery;
+use Akeneo\Catalogs\Infrastructure\Persistence\GetCatalogIdsContainingCategoryQuery;
 use Akeneo\Catalogs\Test\Integration\IntegrationTestCase;
 
 /**
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
- * @covers \Akeneo\Catalogs\Infrastructure\Persistence\GetCatalogsToDisableOnCategoryRemovalQuery
+ * @covers \Akeneo\Catalogs\Infrastructure\Persistence\GetCatalogIdsContainingCategoryQuery
  */
 class GetCatalogsToDisableOnCategoryRemovalQueryTest extends IntegrationTestCase
 {
-    private ?GetCatalogsToDisableOnCategoryRemovalQuery $query;
+    private ?GetCatalogIdsContainingCategoryQuery $query;
 
     protected function setUp(): void
     {
@@ -24,7 +24,7 @@ class GetCatalogsToDisableOnCategoryRemovalQueryTest extends IntegrationTestCase
 
         $this->purgeDataAndLoadMinimalCatalog();
 
-        $this->query = self::getContainer()->get(GetCatalogsToDisableOnCategoryRemovalQuery::class);
+        $this->query = self::getContainer()->get(GetCatalogIdsContainingCategoryQuery::class);
     }
 
     public function testItGetsCatalogsByCategory(): void
