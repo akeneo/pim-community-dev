b412db6e2b4ec7f34a6765d58977cb152351bd46
Antoine ARRIVE <antoine.arrive@gmail.com>
2022-09-26 16:12:44 +0200

    CPM-742: Add a short description for domain objects

--- components/identifier-generator/back/src/Domain/Model/Delimiter.php
+++ components/identifier-generator/back/src/Domain/Model/Delimiter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model;
 
 /**
+ * String between each Properties chosen to create the Structure of the identifier
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
