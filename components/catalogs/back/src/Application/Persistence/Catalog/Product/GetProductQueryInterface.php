00fdb7e784c0a87832b81054230b408e4cb2969d
Riff <riff.ventura@gmail.com>
2022-09-27 17:24:20 +0200

    CXP-1307: added a query to fetch a single product belonging to the catalog

---
+++ components/catalogs/back/src/Application/Persistence/Catalog/Product/GetProductQueryInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Catalogs\Application\Persistence\Catalog\Product;
+
+/**
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @phpstan-type ProductValue array{
+ *      scope: string|null,
+ *      locale: string|null,
+ *      data: mixed,
+ * }
+ *
+ * @phpstan-type Product array{
+ *      uuid: string,
+ *      enabled: boolean,
+ *      family: string,
+ *      categories: array<string>,
+ *      groups: array<string>,
+ *      parent: string|null,
+ *      values: array<string, ProductValue>,
+ *      associations: array<string, array{groups: array<string>, products: array<string>, product_models: array<string>}>,
+ *      quantified_associations: array<string, array{products: array<string>, product_models: array<string>}>,
+ *      created: string,
+ *      updated: string,
+ * }
+ */
+interface GetProductQueryInterface
+{
+    /**
+     * @return Product|null
+     */
+    public function execute(string $catalogId, string $productUuid): ?array;
+}
