dba1bbd37cea346efde61816a8a21d1f8c508c2b
Tseho <quentin.favrie@gmail.com>
2022-09-28 11:16:52 +0200

    OCT-141: rewrite translator & user-context from AMD to CommonJS

--- front-packages/shared/legacy/user-context.js
+++ front-packages/shared/legacy/user-context.js
@@ -1,58 +1,57 @@
 'use strict';
 
-define(['jquery', 'backbone', 'underscore', 'routing'], ($, Backbone, _, Routing) => {
-  var contextData = {};
+const $ = require('jquery');
+const Backbone = require('backbone');
+const _ = require('underscore');
 
-  return _.extend(
-    {
-      /**
-       * Fetches data from the back then stores it.
-       *
-       * @returns {Promise}
-       */
-      initialize: () => {
-        return $.get(Routing.generate('pim_user_user_rest_get_current')).then(response => {
-          contextData = response;
-          contextData.uiLocale = contextData.user_default_locale;
-          contextData.catalogLocale = contextData.catalog_default_locale;
-          contextData.catalogScope = contextData.catalog_default_scope;
-        });
-      },
+var contextData = {};
 
-      refresh: () => {
-        return $.get(Routing.generate('pim_user_user_rest_get_current')).then(response => {
-          contextData = response;
-          contextData.uiLocale = contextData.user_default_locale;
-          contextData.catalogLocale = contextData.catalog_default_locale;
-          contextData.catalogScope = contextData.catalog_default_scope;
-        });
-      },
+export default _.extend({
+  /**
+   * Fetches data from the back then stores it.
+   *
+   * @returns {Promise}
+   */
+  initialize: () => {
+    return $.get('/rest/user/').then(response => {
+      contextData = response;
+      contextData.uiLocale = contextData.user_default_locale;
+      contextData.catalogLocale = contextData.catalog_default_locale;
+      contextData.catalogScope = contextData.catalog_default_scope;
+    });
+  },
 
-      /**
-       * Returns the value corresponding to the specified key.
-       *
-       * @param {String} key
-       *
-       * @returns {*}
-       */
-      get: key => contextData[key],
+  refresh: () => {
+    return $.get('/rest/user/').then(response => {
+      contextData = response;
+      contextData.uiLocale = contextData.user_default_locale;
+      contextData.catalogLocale = contextData.catalog_default_locale;
+      contextData.catalogScope = contextData.catalog_default_scope;
+    });
+  },
 
-      /**
-       * Sets a new value at the specified key.
-       *
-       * @param {String} key
-       * @param {String} value
-       * @param {Object} options
-       */
-      set: function (key, value, options) {
-        options = options || {};
-        contextData[key] = value;
+  /**
+   * Returns the value corresponding to the specified key.
+   *
+   * @param {String} key
+   *
+   * @returns {*}
+   */
+  get: key => contextData[key],
 
-        if (!options.silent) {
-          this.trigger('change:' + key);
-        }
-      },
-    },
-    Backbone.Events
-  );
-});
+  /**
+   * Sets a new value at the specified key.
+   *
+   * @param {String} key
+   * @param {String} value
+   * @param {Object} options
+   */
+  set: function (key, value, options) {
+    options = options || {};
+    contextData[key] = value;
+
+    if (!options.silent) {
+      this.trigger('change:' + key);
+    }
+  },
+}, Backbone.Events);
