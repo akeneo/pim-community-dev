6eb0fadf23958609ddd2f49c0ac018a3d232d699
Tseho <quentin.favrie@gmail.com>
2022-09-28 12:04:11 +0200

    OCT-141: exposes dependencies in @akeneo-pim-community/shared

---
+++ front-packages/shared/src/dependencies/user-context.ts
@@ -0,0 +1,7 @@
+import {UserContext} from '../DependenciesProvider.type';
+// @ts-ignore
+import legacy from '../../legacy/user-context';
+
+const userContext: UserContext = legacy;
+
+export {userContext};
