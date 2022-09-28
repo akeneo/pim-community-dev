6270aac8a7bcb75228fde3a0bbbe1da76d03c208
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-09-28 16:46:36 +0200

    Use env variableto find non executed specs

--- .circleci/find_non_executed_phpspec.sh
+++ .circleci/find_non_executed_phpspec.sh
@@ -6,8 +6,18 @@ EXECUTED_PHPSPEC_FILE=$(mktemp --suffix "executed")
 EXISTING_PHPSPEC_FILE=$(mktemp --suffix "existing")
 cat var/tests/phpspec/specs.xml | grep -Po '(?<=classname\=").*(?=\" status)' | sort | uniq > $EXECUTED_PHPSPEC_FILE
 
+if [ $# -eq 0 ]; then
+  ENV=
+else
+  ENV="$1"
+fi
+
 if [ -d components ]; then
-    find src tests components -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
+    if [ "grth" = $ENV -o  "tria" = $ENV ]; then
+        find src tests components -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -not -path "components/performance-analytics/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
+    else
+        find src tests components -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
+    fi
 else
     find src tests -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
 fi
