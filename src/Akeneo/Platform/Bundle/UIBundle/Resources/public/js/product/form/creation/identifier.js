aa3e720b003676c11d64da8e9b880854cbf725b4
pierallard <pierallard@gmail.com>
2022-09-30 17:04:22 +0200

    CPM-688: Add helper

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/creation/identifier.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/creation/identifier.js
@@ -42,6 +42,7 @@ define([
                 this.template({
                   identifier: this.identifier,
                   label: i18n.getLabel(identifier.labels, UserContext.get('catalogLocale'), identifier.code),
+                  requiredLabel: '',
                   errors: this.getRoot().validationErrors,
                   value: this.getFormData()[this.identifier],
                 })
