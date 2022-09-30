9eb12bf02a38c08cb4c33e8cc04ca2e2642701e9
pierallard <pierallard@gmail.com>
2022-09-30 10:22:01 +0200

    CPM-594: Add identifier under family condition

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/creation/identifier.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/creation/identifier.js
@@ -25,28 +25,41 @@ define([
      * @return {Promise}
      */
     render: function () {
-      return FetcherRegistry.getFetcher('attribute')
-        .getIdentifierAttribute()
-        .then(
-          function (identifier) {
-            this.$el.html(
-              this.template({
-                identifier: this.identifier,
-                label: i18n.getLabel(identifier.labels, UserContext.get('catalogLocale'), identifier.code),
-                requiredLabel: __('pim_common.required_label'),
-                errors: this.getRoot().validationErrors,
-                value: this.getFormData()[this.identifier],
-              })
-            );
+      return this.shouldDisplay()
+        .then((shouldDisplay) => {
+        if (!shouldDisplay) {
+          this.$el.html('');
 
-            this.delegateEvents();
+          return this;
+        }
 
-            return this;
-          }.bind(this)
-        )
-        .fail(() => {
-          this.$el.html(this.errorTemplate({message: __('pim_enrich.entity.product.flash.create.fail')}));
-        });
+        return FetcherRegistry.getFetcher('attribute')
+          .getIdentifierAttribute()
+          .then(
+            function (identifier) {
+              this.$el.html(
+                this.template({
+                  identifier: this.identifier,
+                  label: i18n.getLabel(identifier.labels, UserContext.get('catalogLocale'), identifier.code),
+                  requiredLabel: __('pim_common.required_label'),
+                  errors: this.getRoot().validationErrors,
+                  value: this.getFormData()[this.identifier],
+                })
+              );
+
+              this.delegateEvents();
+
+              return this;
+            }.bind(this)
+          )
+          .fail(() => {
+            this.$el.html(this.errorTemplate({message: __('pim_enrich.entity.product.flash.create.fail')}));
+          });
+      });
     },
+
+    shouldDisplay: async function () {
+      return new Promise(resolve => resolve(true));
+    }
   });
 });
