2d802fc2fd7298b864851dc121ef3c44f14675ad
Riff <riff.ventura@gmail.com>
2022-10-03 14:28:20 +0200

    CXP-1288: Adjusted placeholder to use DSM component

--- components/catalogs/front/src/components/ProductMapping/components/SourcePlaceholder.tsx
+++ components/catalogs/front/src/components/ProductMapping/components/SourcePlaceholder.tsx
@@ -1,28 +1,10 @@
 import React, {FC} from 'react';
 import styled from 'styled-components';
-import {SectionTitle, AttributesIllustration, Link, getColor, getFontSize} from 'akeneo-design-system';
+import {AttributesIllustration, Link, Placeholder, SectionTitle} from 'akeneo-design-system';
 import {useTranslate} from '@akeneo-pim-community/shared';
 
-const Placeholder = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+const SectionContent = styled.div`
     padding: 30px 0;
-    gap: 5px;
-`;
-const Title = styled.div`
-    font-size: ${getFontSize('big')};
-    text-align: center;
-    color: ${getColor('grey', 140)};
-    font-weight: 400;
-    line-height: 18px;
-`;
-const SubTitle = styled.div`
-    font-size: ${getFontSize('default')};
-    color: ${getColor('grey', 120)};
-    font-weight: 400;
-    line-height: 16px;
     text-align: center;
 `;
 
@@ -36,14 +18,17 @@ export const SourcePlaceholder: FC = () => {
                     {translate('akeneo_catalogs.product_mapping.source.placeholder.title')}
                 </SectionTitle.Title>
             </SectionTitle>
-            <Placeholder>
-                <AttributesIllustration size={128} />
-                <Title>{translate('akeneo_catalogs.product_mapping.source.placeholder.illustration_title')}</Title>
-                <SubTitle>{translate('akeneo_catalogs.product_mapping.source.placeholder.subtitle')}</SubTitle>
-                <Link href={'https://help.akeneo.com/'} target='_blank'>
-                    {translate('akeneo_catalogs.product_mapping.source.placeholder.link')}
-                </Link>
-            </Placeholder>
+            <SectionContent>
+                <Placeholder
+                    illustration={<AttributesIllustration />}
+                    title={translate('akeneo_catalogs.product_mapping.source.placeholder.illustration_title')}
+                >
+                    <div>{translate('akeneo_catalogs.product_mapping.source.placeholder.subtitle')}</div>
+                    <Link href={'https://help.akeneo.com/'} target='_blank'>
+                        {translate('akeneo_catalogs.product_mapping.source.placeholder.link')}
+                    </Link>
+                </Placeholder>
+            </SectionContent>
         </>
     );
 };
