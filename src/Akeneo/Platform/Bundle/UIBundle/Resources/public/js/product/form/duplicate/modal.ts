b441cd8d138cab10ee89e648bf0fa48d7bf6063c
pierallard <pierallard@gmail.com>
2022-10-04 15:06:39 +0200

    CPM-594: Fix Duplicate modal

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/duplicate/modal.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/duplicate/modal.ts
@@ -63,7 +63,7 @@ class DuplicateModal extends BaseView {
 
   public open() {
     const modal = new (Backbone as any).BootstrapModal({
-      title: __(this.config.labels.title, {product_identifier: this.productIdentifierToDuplicate}),
+      title: __(this.config.labels.title, {product_identifier: this.productIdentifierToDuplicate || this.productUuidToDuplicate}),
       subtitle: __(this.config.labels.subTitle),
       picture: this.config.picture,
       content: '',
