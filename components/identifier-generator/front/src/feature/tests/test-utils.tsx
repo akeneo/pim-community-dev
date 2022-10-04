4fd834def5fcd488f1cbab6b8fd6d618ac986428
Celine Morin <celine.morin@akeneo.com>
2022-10-04 11:57:50 +0200

    CPM-732: Add IdentifierAttributeSelector

---
+++ components/identifier-generator/front/src/feature/tests/test-utils.tsx
@@ -0,0 +1,38 @@
+import React, {FC} from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {pimTheme} from 'akeneo-design-system';
+import {MicroFrontendDependenciesProvider, Routes} from '@akeneo-pim-community/shared';
+import {ThemeProvider} from 'styled-components';
+import {render, RenderOptions} from '@testing-library/react';
+
+const AllTheProviders: FC<{children: React.ReactNode}> = ({children}) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // turns retries off because we don't need it inside tests
+        retry: false,
+      },
+    },
+  });
+
+  const fakeRoutes = {
+    pim_user_security_rest_get: {tokens: []},
+  };
+
+  return (
+    <ThemeProvider theme={pimTheme}>
+      <MicroFrontendDependenciesProvider routes={fakeRoutes as Routes}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </MicroFrontendDependenciesProvider>
+    </ThemeProvider>
+  );
+};
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+  // @ts-ignore
+) => render(ui, {wrapper: AllTheProviders, ...options});
+
+export * from '@testing-library/react';
+export {customRender as render};
