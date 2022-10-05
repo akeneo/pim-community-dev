93e161fc128c796783e2142c57aab45cec11ea5c
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2022-10-05 15:41:44 +0200

    CXP-1317: add final to CatalogNotFoundException

--- components/catalogs/back/src/Application/Exception/CatalogNotFoundException.php
+++ components/catalogs/back/src/Application/Exception/CatalogNotFoundException.php
@@ -8,6 +8,6 @@ namespace Akeneo\Catalogs\Application\Exception;
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CatalogNotFoundException extends \Exception
+final class CatalogNotFoundException extends \Exception
 {
 }
