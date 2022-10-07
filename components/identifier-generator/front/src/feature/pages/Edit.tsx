5060439376517870bd2f4aff1dd245e1e784cf58
pierallard <pierallard@gmail.com>
2022-10-07 17:27:43 +0200

    CPM-734: Create pages

---
+++ components/identifier-generator/front/src/feature/pages/Edit.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+
+const Edit: React.FC<{}> = () => {
+  const {identifierGeneratorCode} = useParams<{identifierGeneratorCode: string}>();
+
+  return <div>
+    Edit TODO
+    {identifierGeneratorCode}
+  </div>;
+};
+
+export {Edit};
