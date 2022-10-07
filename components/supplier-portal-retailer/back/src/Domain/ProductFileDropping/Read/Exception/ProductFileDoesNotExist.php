bbea9f2d743097030393c0cd8278e11607bf43c9
ValentinMerlet <merlet.valentin@gmail.com>
2022-10-07 10:43:12 +0200

    SP-359: Delete Application ProductFileIsNotDownloadable exception
    - Move ProductFileDoesNotExist and UnableToStoreProductFile from Application Layer to Domain
    - Create 2 new Domain exception for UnableToReadProductFile and
    ProductFileDoesNotExist (dedicated to writes)

--- components/supplier-portal-retailer/back/src/Domain/ProductFileDropping/Read/Exception/ProductFileDoesNotExist.php
+++ components/supplier-portal-retailer/back/src/Domain/ProductFileDropping/Read/Exception/ProductFileDoesNotExist.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\SupplierPortal\Retailer\Application\ProductFileDropping\Exception;
+namespace Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Read\Exception;
 
 final class ProductFileDoesNotExist extends \Exception
 {
--- components/supplier-portal-retailer/back/src/Domain/ProductFileDropping/Write/Exception/UnableToStoreProductFile.php
+++ components/supplier-portal-retailer/back/src/Domain/ProductFileDropping/Write/Exception/UnableToStoreProductFile.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\SupplierPortal\Retailer\Application\ProductFileDropping\Exception;
+namespace Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Write\Exception;
 
 final class UnableToStoreProductFile extends \Exception
 {
