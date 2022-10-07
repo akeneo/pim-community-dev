10bcdc193e04614a6c206ed3bb26873f902b3290
Pierre Jolly <pie.jolly@gmail.com>
2022-10-07 11:11:06 +0200

    RAB-1120: fix validator

--- components/tailored-import/back/tests/Integration/Infrastructure/Validation/GetRecordsValidatorTest.php
+++ components/tailored-import/back/tests/Integration/Infrastructure/Validation/GetRecordsValidatorTest.php
@@ -158,57 +158,6 @@ final class GetRecordsValidatorTest extends AbstractValidationTest
         ];
     }
 
-
-
-
-
-
-
-
-
-
-//    public function test_it_build_violation_if_no_locale(): void
-//    {
-//        $request = new Request(
-//            request: [
-//                'channel' => 'ecommerce',
-//                'locale' => null,
-//                'include_codes' => null,
-//                'exclude_codes' => null,
-//                'search' => null,
-//                'limit' => 25,
-//                'page' => 1
-//            ],
-//            attributes: [
-//                'reference_entity_code' => 'brand'
-//            ]
-//        );
-//        $violations = $this->getValidator()->validate($request, new GetRecords());
-//
-//        $this->assertHasValidationError('This value should not be null.', '[locale]',$violations);
-//    }
-//
-//    public function test_it_build_violation_if_no_channel(): void
-//    {
-//        $request = new Request(
-//            request: [
-//                'channel' => null,
-//                'locale' => 'en_Us',
-//                'include_codes' => null,
-//                'exclude_codes' => null,
-//                'search' => null,
-//                'limit' => 25,
-//                'page' => 1
-//            ],
-//            attributes: [
-//                'reference_entity_code' => 'brand'
-//            ]
-//        );
-//        $violations = $this->getValidator()->validate($request, new GetRecords());
-//
-//        $this->assertHasValidationError('This value should not be null.', '[channel]',$violations);
-//    }
-
     protected function getConfiguration(): Configuration
     {
         return $this->catalog->useMinimalCatalog();
