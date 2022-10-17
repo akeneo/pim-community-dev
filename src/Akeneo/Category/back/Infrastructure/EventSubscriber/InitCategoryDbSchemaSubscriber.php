f292f672d5c5e2e046ab597940dfed9b1d328fac
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-10-17 16:00:59 +0200

    GRF-357: Create category template table for pim-dev

--- src/Akeneo/Category/back/Infrastructure/EventSubscriber/InitCategoryDbSchemaSubscriber.php
+++ src/Akeneo/Category/back/Infrastructure/EventSubscriber/InitCategoryDbSchemaSubscriber.php
@@ -24,6 +24,12 @@ class InitCategoryDbSchemaSubscriber implements EventSubscriberInterface
 
     public function initDbSchema(InstallerEvent $event): void
     {
+        $this->addValueCollectionInCategoryTable();
+        $this->addCategoryTemplateTable();
+    }
+
+    private function addValueCollectionInCategoryTable()
+    {
         $columns = $this->dbalConnection->getSchemaManager()->listTableColumns('pim_catalog_category');
         if (array_key_exists('value_collection', $columns)) {
             return;
@@ -34,5 +40,18 @@ ALTER TABLE pim_catalog_category ADD value_collection JSON;
 SQL;
 
         $this->dbalConnection->executeQuery($addCategoryValueCollectionQuery);
+    }
+
+    private function addCategoryTemplateTable()
+    {
+        $query = <<<SQL
+CREATE TABLE IF NOT EXISTS pim_catalog_category_template (
+    uuid binary(16) PRIMARY KEY,
+    code VARCHAR(100) NOT NULL,
+    labels JSON NOT NULL DEFAULT ('{}')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+SQL;
+
+        $this->dbalConnection->executeQuery($query);
     }
 }
