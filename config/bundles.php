c0dbddc3e6488e880aaad20a95e12da15f655c55
Grégoire Houssard <ghoussard@protonmail.com>
2022-10-24 12:31:34 +0200

    RAB-1132: Replace swiftmailer by symfony/mailer

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\AclBundle\AclBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
