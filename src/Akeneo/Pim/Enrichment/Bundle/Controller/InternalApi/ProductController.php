cfc9d842106ad020dadfa5015b69931132867132
pierallard <pierallard@gmail.com>
2022-10-24 16:34:52 +0200

    CPM-771: Revert previous and set product dirty on create

--- src/Akeneo/Pim/Enrichment/Bundle/Controller/InternalApi/ProductController.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Controller/InternalApi/ProductController.php
@@ -148,7 +148,7 @@ class ProductController
         $violations = $this->validator->validate($product);
 
         if (0 === $violations->count()) {
-            $this->productSaver->save($product, ['force_save' => true]);
+            $this->productSaver->save($product);
 
             return new JsonResponse($this->normalizer->normalize(
                 $product,
