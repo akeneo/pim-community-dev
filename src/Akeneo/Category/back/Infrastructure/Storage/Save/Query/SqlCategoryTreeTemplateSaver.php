2938869c6a9313f6b85098dfc764250aba60ffa0
Adrien Migaire <adrien.migaire@akeneo.com>
2022-10-24 18:09:05 +0200

    GRF-361 add integration test of ActivateTemplate service

--- src/Akeneo/Category/back/Infrastructure/Storage/Save/Query/SqlCategoryTreeTemplateSaver.php
+++ src/Akeneo/Category/back/Infrastructure/Storage/Save/Query/SqlCategoryTreeTemplateSaver.php
@@ -27,20 +27,20 @@ class SqlCategoryTreeTemplateSaver implements CategoryTreeTemplateSaver
     {
         $query = <<< SQL
             INSERT INTO pim_catalog_category_tree_template
-                (template_uuid, category_tree_id)
+                (category_template_uuid, category_tree_id)
             VALUES
-                (:template_identifier, :category_tree_id)
+                (UUID_TO_BIN(:template_uuid), :category_tree_id)
             ;
         SQL;
 
         $this->connection->executeQuery(
             $query,
             [
-                'template_identifier' => (string) $templateModel->getUuid(),
+                'template_uuid' => (string) $templateModel->getUuid(),
                 'category_tree_id' => $templateModel->getCategoryTreeId()->getValue(),
             ],
             [
-                'template_identifier' => \PDO::PARAM_STR,
+                'template_uuid' => \PDO::PARAM_STR,
                 'category_tree_id' => \PDO::PARAM_INT,
             ]
         );
@@ -49,25 +49,5 @@ class SqlCategoryTreeTemplateSaver implements CategoryTreeTemplateSaver
     public function update(Template $templateModel)
     {
         // TODO: Implement update() method.
-    }
-
-    public function linkAlreadyExists($templateModel): bool
-    {
-        $query = <<< SQL
-            SELECT * FROM pim_catalog_category_tree_template
-            WHERE template_uuid=:template_uuid 
-                AND category_tree_id NOT NULL
-            ;
-        SQL;
-
-        return $this->connection->fetchOne(
-            $query,
-            [
-                'template_uuid' => (string) $templateModel->getUuid()
-            ],
-            [
-                'template_uuid' => \PDO::PARAM_STR
-            ]
-        );
     }
 }
