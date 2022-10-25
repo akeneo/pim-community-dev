97e15c6e1e485b63f2f26f99af7ec57edfbb61c2
jmleroux <jmleroux.pro@gmail.com>
2022-10-25 16:13:55 +0200

    PH-269: Extract `akeneo:connectivity-audit:update-data` logic into a dedicated service

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Audit/Command/UpdateAuditDataCommand.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Audit/Command/UpdateAuditDataCommand.php
@@ -4,26 +4,22 @@ declare(strict_types=1);
 
 namespace Akeneo\Connectivity\Connection\Infrastructure\Audit\Command;
 
-use Akeneo\Connectivity\Connection\Application\Audit\Command\UpdateDataSourceProductEventCountCommand;
-use Akeneo\Connectivity\Connection\Application\Audit\Command\UpdateDataSourceProductEventCountHandler;
-use Akeneo\Connectivity\Connection\Domain\Audit\Persistence\PurgeAuditProductQueryInterface;
-use Akeneo\Connectivity\Connection\Domain\ValueObject\HourlyInterval;
-use Akeneo\Connectivity\Connection\Infrastructure\Audit\Persistence\DbalSelectHourlyIntervalsToRefreshQuery;
 use Akeneo\Connectivity\Connection\Infrastructure\Audit\UpdateAuditData;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @author Romain Monceau <romain@akeneo.com>
+ * @author    Romain Monceau <romain@akeneo.com>
  * @copyright 2019 Akeneo SAS (http://www.akeneo.com)
- * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 class UpdateAuditDataCommand extends Command
 {
     protected static $defaultName = 'akeneo:connectivity-audit:update-data';
 
-    public function __construct(private UpdateAuditData $updateAuditData) {
+    public function __construct(private UpdateAuditData $updateAuditData)
+    {
         parent::__construct();
     }
 
--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Audit/UpdateAuditData.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Audit/UpdateAuditData.php
@@ -12,9 +12,9 @@ use Akeneo\Connectivity\Connection\Infrastructure\Audit\Persistence\DbalSelectHo
 use Psr\Log\LoggerInterface;
 
 /**
- * @author JM Leroux <jean-marie.leroux@akneo.com>
+ * @author    JM Leroux <jean-marie.leroux@akneo.com>
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
- * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 final class UpdateAuditData
 {
