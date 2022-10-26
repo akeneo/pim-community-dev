e0777bd1cf66afa9f5dc56724a7a4e83af834455
pierallard <pierallard@gmail.com>
2022-10-26 14:30:38 +0200

    CPM-767: Remove order as the normalizer order them already

--- src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Product/GetAssociatedProductUuidsByProductFromDB.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Product/GetAssociatedProductUuidsByProductFromDB.php
@@ -23,8 +23,7 @@ SELECT DISTINCT(BIN_TO_UUID(p.uuid)) as uuid
 FROM pim_catalog_association a
     INNER JOIN pim_catalog_association_product ap ON a.id = ap.association_id
     INNER JOIN pim_catalog_product p ON p.uuid = ap.product_uuid
-WHERE a.owner_uuid = UUID_TO_BIN(:ownerUuid) AND a.association_type_id = :associationTypeId
-ORDER BY uuid ASC;
+WHERE a.owner_uuid = UUID_TO_BIN(:ownerUuid) AND a.association_type_id = :associationTypeId;
 SQL;
         $stmt = $this->connection->executeQuery(
             $sql,
