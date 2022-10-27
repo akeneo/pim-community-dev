968092d0aeafb99be71a28d3e4c80e99d8e0c30e
Adrien Migaire <adrien.migaire@akeneo.com>
2022-10-27 11:44:22 +0200

    GRF-361 refacto insert query of category template attributes

--- src/Akeneo/Category/back/Infrastructure/Storage/Save/Query/SqlCategoryTemplateAttributeSaver.php
+++ src/Akeneo/Category/back/Infrastructure/Storage/Save/Query/SqlCategoryTemplateAttributeSaver.php
@@ -18,99 +18,79 @@ use Doctrine\DBAL\Types\Types;
  */
 class SqlCategoryTemplateAttributeSaver implements CategoryTemplateAttributeSaver
 {
-    private array $defaultParams;
-
-    private array $defaultTypes;
-
     public function __construct(
         private Connection $connection,
     ) {
     }
 
     /**
-     * @throws Exception
+     * @param TemplateUuid $templateUuid
+     * @param AttributeCollection $attributeCollection
+     * @return void
      */
     public function insert(TemplateUuid $templateUuid, AttributeCollection $attributeCollection): void
     {
         $this->defaultParams['template_uuid'] = (string) $templateUuid;
         $this->defaultTypes['template_uuid'] = \PDO::PARAM_STR;
 
-        foreach ($attributeCollection->getAttributes() as $attribute) {
-            $this->insertAttribute($attribute);
-        }
+        $this->insertAttributes($attributeCollection->getAttributes());
     }
 
+    /**
+     * @param TemplateUuid $templateUuid
+     * @param AttributeCollection $attributeCollection
+     * @return void
+     */
     public function update(TemplateUuid $templateUuid, AttributeCollection $attributeCollection): void
     {
         // TODO: Implement update() method.
     }
 
-    private function insertAttribute(Attribute $attribute): void
+    /**
+     * @param array<Attribute> $attributes
+     * @return void
+     * @throws Exception
+     * @throws \Doctrine\DBAL\Driver\Exception
+     */
+    private function insertAttributes(array $attributes)
     {
-        $query = <<<SQL
+        $placeholders = \implode(
+            ',',
+            \array_fill(0, \count($attributes), '(UUID_TO_BIN(?), ?, UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?, ?)')
+        );
+        $statement = $this->connection->prepare(
+            <<<SQL
             INSERT INTO pim_catalog_category_attribute
-                (
-                    uuid,
-                    code,
-                    category_template_uuid,
-                    labels,
-                    attribute_type,
-                    attribute_order,
-                    is_required,
-                    is_scopable,
-                    is_localizable,
-                    additional_properties
-                )
-            VALUES
-                (
-                    UUID_TO_BIN(:uuid),
-                    :code,
-                    UUID_TO_BIN(:template_uuid),
-                    :labels,
-                    :attribute_type,
-                    :attribute_order,
-                    :is_required,
-                    :is_scopable,
-                    :is_localizable,
-                    :additional_properties
-                 )
-            ;
-SQL;
-
-        $params = array_merge(
-            $this->defaultParams,
-            [
-                'uuid' => (string) $attribute->getUuid(),
-                'code' => (string) $attribute->getCode(),
-                'labels' => $attribute->getLabelCollection()->normalize(),
-                'attribute_type' => (string) $attribute->getType(),
-                'attribute_order' => $attribute->getOrder()->intValue(),
-                'is_required' => false, // TODO
-                'is_scopable' => false, // TODO
-                'is_localizable' => false, // TODO
-                'additional_properties' => [], // TODO
-            ]
+            (
+                uuid,
+                code,
+                category_template_uuid,
+                labels,
+                attribute_type,
+                attribute_order,
+                is_required,
+                is_scopable,
+                is_localizable,
+                additional_properties
+            )
+            VALUES {$placeholders} ;
+            SQL
         );
 
-        $types = array_merge(
-            $this->defaultTypes,
-            [
-                'uuid'=> \PDO::PARAM_STR,
-                'code'=> \PDO::PARAM_STR,
-                'labels'=> Types::JSON,
-                'attribute_type'=> \PDO::PARAM_STR,
-                'attribute_order'=> \PDO::PARAM_INT,
-                'is_required'=> \PDO::PARAM_BOOL,
-                'is_scopable'=> \PDO::PARAM_BOOL,
-                'is_localizable'=> \PDO::PARAM_BOOL,
-                'additional_properties'=> Types::JSON,
-            ]
-        );
+        $placeholderIndex = 0;
+        foreach ($attributes as $attribute) {
+            $statement->bindValue(++$placeholderIndex, (string) $attribute->getUuid(), \PDO::PARAM_STR);
+            $statement->bindValue(++$placeholderIndex, (string) $attribute->getCode(), \PDO::PARAM_STR);
+            $statement->bindValue(++$placeholderIndex, (string) $attribute->getTemplateUuid(), \PDO::PARAM_STR);
+            $statement->bindValue(++$placeholderIndex, $attribute->getLabelCollection()->normalize(), Types::JSON);
+            $statement->bindValue(++$placeholderIndex, (string) $attribute->getType(), \PDO::PARAM_STR);
+            $statement->bindValue(++$placeholderIndex, $attribute->getOrder()->intValue(), \PDO::PARAM_INT);
+            $statement->bindValue(++$placeholderIndex, $attribute->isRequired()->getValue(), \PDO::PARAM_BOOL);
+            $statement->bindValue(++$placeholderIndex, $attribute->isScopable()->getValue(), \PDO::PARAM_BOOL);
+            $statement->bindValue(++$placeholderIndex, $attribute->isLocalizable()->getValue(), \PDO::PARAM_BOOL);
+            $statement->bindValue(++$placeholderIndex, $attribute->getAdditionalProperties()->normalize(), Types::JSON);
+        }
 
-        $this->connection->executeQuery(
-            $query,
-            $params,
-            $types
-        );
+        $statement->executeQuery();
     }
 }
