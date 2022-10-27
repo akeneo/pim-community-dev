319c44a21e8beb409ac4e65134cc906aee6d339c
pierallard <pierallard@gmail.com>
2022-10-27 15:22:15 +0200

    CPM-767: Fix order in associations

--- src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Product/GetAssociatedProductUuidsByProductFromDB.php
+++ src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Product/GetAssociatedProductUuidsByProductFromDB.php
@@ -19,12 +19,12 @@ final class GetAssociatedProductUuidsByProductFromDB implements GetAssociatedPro
     public function getIdentifiers(UuidInterface $productUuid, AssociationInterface $association): array
     {
         $sql = <<<SQL
-SELECT DISTINCT(p.identifier) as identifier
+SELECT DISTINCT(p.identifier) as identifier, p.uuid AS p_uuid
 FROM pim_catalog_association a
     INNER JOIN pim_catalog_association_product ap ON a.id = ap.association_id
     INNER JOIN pim_catalog_product p ON p.uuid = ap.product_uuid
 WHERE a.owner_uuid = UUID_TO_BIN(:ownerUuid) AND a.association_type_id = :associationTypeId
-ORDER BY p.uuid;
+ORDER BY p_uuid;
 SQL;
         $stmt = $this->connection->executeQuery(
             $sql,
@@ -43,12 +43,12 @@ SQL;
     public function getUuids(UuidInterface $productUuid, AssociationInterface $association): array
     {
         $sql = <<<SQL
-SELECT DISTINCT(BIN_TO_UUID(p.uuid)) as uuid
+SELECT DISTINCT(BIN_TO_UUID(p.uuid)) as uuid, p.uuid AS p_uuid
 FROM pim_catalog_association a
     INNER JOIN pim_catalog_association_product ap ON a.id = ap.association_id
     INNER JOIN pim_catalog_product p ON p.uuid = ap.product_uuid
 WHERE a.owner_uuid = UUID_TO_BIN(:ownerUuid) AND a.association_type_id = :associationTypeId
-ORDER BY p.uuid;
+ORDER BY p_uuid;
 SQL;
         $stmt = $this->connection->executeQuery(
             $sql,
