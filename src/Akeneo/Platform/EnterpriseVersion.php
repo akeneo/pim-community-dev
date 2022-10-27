aca815ec2c7036bab9476bd131700b0abade59ef
Michel Tag <micheltag@akeneo.com>
2022-10-27 16:45:57 +0200

    Preparing version 6.0.47 (#15323)
    Preparing version 6.0.47
    
    
    Co-authored-by: StevenVAIDIE <no-reply@akeneo.com>

--- src/Akeneo/Platform/EnterpriseVersion.php
+++ src/Akeneo/Platform/EnterpriseVersion.php
@@ -19,7 +19,7 @@ namespace Akeneo\Platform;
 class EnterpriseVersion
 {
     /** @staticvar string */
-    const VERSION = '6.0.46';
+    const VERSION = '6.0.47';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Maple';
