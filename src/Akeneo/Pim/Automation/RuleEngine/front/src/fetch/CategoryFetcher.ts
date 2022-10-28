d430f3ba48fd488302fe32dcc3ecfd70a4e85ace
Celine Morin <celine.morin@akeneo.com>
2022-10-28 09:54:35 +0200

    PIM-10691: Change method type for categories related method in rule engine to support very large number of categories

--- src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/CategoryFetcher.ts
+++ src/Akeneo/Pim/Automation/RuleEngine/front/src/fetch/CategoryFetcher.ts
@@ -1,15 +1,15 @@
 import {Router} from '../dependenciesTools';
-import {httpGet} from './fetch';
+import {httpPost} from './fetch';
 import {Category, CategoryCode} from '../models/Category';
 
 const fetchCategoriesByIdentifiers = async (
   categoryCodes: CategoryCode[],
   router: Router
 ): Promise<Category[]> => {
-  const url = router.generate('pimee_enrich_rule_definition_get_categories', {
-    identifiers: categoryCodes,
+  const url = router.generate('pimee_enrich_rule_definition_get_categories');
+  const response = await httpPost(url, {
+    body: {identifiers: categoryCodes},
   });
-  const response = await httpGet(url);
 
   return response.status === 404 ? null : await response.json();
 };
