a216350bccd9f941dd52081ea0324cdd474aeb2d
Sébastien DESTRE <sebastien.destre@akeneo.com>
2022-10-28 18:56:35 +0200

    PH-283: Refactor CRON job akeneo:reference-entity:refresh-records (#15310)
    * PH-283: Refactor CRON job akeneo:reference-entity:refresh-records
    
    * Fix codestyle violations
    
    * Fix codestyle violations
    
    * Don't remove CronJobs yet

---
+++ upgrades/schema/Version_7_0_20221026163100_add_scheduled_jobs_reference_entity_refresh_records.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version_7_0_20221026163100_add_scheduled_jobs_reference_entity_refresh_records extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addScheduledJob(
+            'reference_entity_refresh_records',
+            'Refresh all records referencing a deleted record or a deleted attribute option.',
+            []
+        );
+    }
+
+    private function addScheduledJob(string $jobCode, string $label, array $rawParameters): void
+    {
+        $sql = <<<SQL
+            INSERT INTO akeneo_batch_job_instance 
+                (`code`, `label`, `job_name`, `status`, `connector`, `raw_parameters`, `type`)
+            VALUES
+            (
+                :code,
+                :label,
+             
+                :code,
+                0,
+                'internal',
+                :raw_parameters,
+                'scheduled_job'
+            )
+            ON DUPLICATE KEY UPDATE code = code;
+        SQL;
+
+        $this->addSql(
+            $sql,
+            ['code' => $jobCode, 'label' => $label, 'raw_parameters' => \serialize($rawParameters)]
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
