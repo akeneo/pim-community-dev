ce87a8a40a699dfb3632e651dfedb36b20e11458
ValentinMerlet <merlet.valentin@gmail.com>
2022-10-31 11:01:32 +0100

    SP-374: Add unit test for CleanSupplierProductFiles tasklet

---
+++ components/supplier-portal-retailer/back/tests/Unit/Infrastructure/ProductFileDropping/Job/CleanSupplierProductFilesTest.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\SupplierPortal\Retailer\Test\Unit\Infrastructure\ProductFileDropping\Job;
+
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\DeleteOldProductFiles;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\DeleteProductFilesFromPaths;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\GetProductFilePathsOfOldProductFiles;
+use Akeneo\SupplierPortal\Retailer\Infrastructure\ProductFileDropping\GoogleCloudStorage\DeleteUnknownSupplierDirectoriesInGCSBucket;
+use Akeneo\SupplierPortal\Retailer\Infrastructure\ProductFileDropping\Job\CleanSupplierProductFiles;
+use Akeneo\Tool\Component\Batch\Model\StepExecution;
+use League\Flysystem\UnableToDeleteFile;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\Test\TestLogger;
+
+final class CleanSupplierProductFilesTest extends TestCase
+{
+    /** @test */
+    public function itCleansTheProductFiles(): void
+    {
+        $getProductFilePathsOfOldProductFiles = $this->createMock(
+            GetProductFilePathsOfOldProductFiles::class,
+        );
+        $deleteProductFilesFromPaths = $this->createMock(DeleteProductFilesFromPaths::class);
+        $deleteOldProductFiles = $this->createMock(DeleteOldProductFiles::class);
+        $deleteUnknownSupplierDirectoriesInGCSBucket = $this->createMock(
+            DeleteUnknownSupplierDirectoriesInGCSBucket::class,
+        );
+
+        $getProductFilePathsOfOldProductFiles
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn(['path/to/product_file_1.xlsx', 'path/to/product_file_2.xlsx'])
+        ;
+
+        $deleteProductFilesFromPaths
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with(['path/to/product_file_1.xlsx', 'path/to/product_file_2.xlsx'])
+        ;
+
+        $deleteOldProductFiles
+            ->expects($this->once())
+            ->method('__invoke')
+        ;
+
+        $deleteUnknownSupplierDirectoriesInGCSBucket
+            ->expects($this->once())
+            ->method('__invoke')
+        ;
+
+        $sut = new CleanSupplierProductFiles(
+            $getProductFilePathsOfOldProductFiles,
+            $deleteProductFilesFromPaths,
+            $deleteOldProductFiles,
+            $deleteUnknownSupplierDirectoriesInGCSBucket,
+            new TestLogger(),
+        );
+
+        $sut->execute();
+    }
+
+    /** @test */
+    public function itLogsAnErrorIfTheDeletionFailed(): void
+    {
+        $logger = new TestLogger();
+
+        $getProductFilePathsOfOldProductFiles = $this->createMock(
+            GetProductFilePathsOfOldProductFiles::class,
+        );
+        $deleteProductFilesFromPaths = $this->createMock(DeleteProductFilesFromPaths::class);
+        $deleteOldProductFiles = $this->createMock(DeleteOldProductFiles::class);
+        $deleteUnknownSupplierDirectoriesInGCSBucket = $this->createMock(
+            DeleteUnknownSupplierDirectoriesInGCSBucket::class,
+        );
+        $stepExecution = $this->createMock(StepExecution::class);
+
+        $getProductFilePathsOfOldProductFiles
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willReturn(['path/to/product_file_1.xlsx', 'path/to/product_file_2.xlsx'])
+        ;
+
+        $raisedException = new UnableToDeleteFile();
+
+        $deleteProductFilesFromPaths
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with(['path/to/product_file_1.xlsx', 'path/to/product_file_2.xlsx'])
+            ->willThrowException($raisedException)
+        ;
+
+        $stepExecution
+            ->expects($this->once())
+            ->method('addFailureException')
+            ->with($raisedException)
+        ;
+
+        $sut = new CleanSupplierProductFiles(
+            $getProductFilePathsOfOldProductFiles,
+            $deleteProductFilesFromPaths,
+            $deleteOldProductFiles,
+            $deleteUnknownSupplierDirectoriesInGCSBucket,
+            $logger,
+        );
+
+        $sut->setStepExecution($stepExecution);
+        $sut->execute();
+
+        static::assertTrue($logger->hasError([
+            'message' => 'An error occurred during the cleaning of old supplier product files',
+            'context' => [
+                'data' => [
+                    'step_execution_id' => null,
+                    'message' => '',
+                ],
+            ],
+        ]));
+    }
+}
