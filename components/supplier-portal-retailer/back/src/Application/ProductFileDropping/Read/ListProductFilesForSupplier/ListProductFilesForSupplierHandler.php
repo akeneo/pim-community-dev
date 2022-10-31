c543e8aa54bf5dd54327f82e06919fef6ad6dfdc
ValentinMerlet <merlet.valentin@gmail.com>
2022-10-31 17:59:27 +0100

    SP-348: Add ListProductFiles query/handler for supplier use case

---
+++ components/supplier-portal-retailer/back/src/Application/ProductFileDropping/Read/ListProductFilesForSupplier/ListProductFilesForSupplierHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\SupplierPortal\Retailer\Application\ProductFileDropping\Read\ListProductFilesForSupplier;
+
+use Akeneo\SupplierPortal\Retailer\Application\ProductFileDropping\Read\ListProductFilesForSupplier\ListProductFilesForSupplier as ListProductFilesForSupplierQuery;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\GetProductFilesCount;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\ListProductFilesForSupplier;
+use Akeneo\SupplierPortal\Retailer\Domain\Supplier\Read\GetSupplierFromContributorEmail;
+
+final class ListProductFilesForSupplierHandler
+{
+    public function __construct(
+        private ListProductFilesForSupplier $listProductFilesForSupplier,
+        private GetSupplierFromContributorEmail $getSupplierFromContributorEmail,
+        private GetProductFilesCount $getProductFilesCount,
+    ) {
+    }
+
+    public function __invoke(ListProductFilesForSupplierQuery $listProductFiles): ProductFiles
+    {
+        $supplier = ($this->getSupplierFromContributorEmail)($listProductFiles->contributorEmail);
+
+        if (null === $supplier) {
+            return new ProductFiles([], 0);
+        }
+
+        return new ProductFiles(
+            ($this->listProductFilesForSupplier)($supplier->identifier, $listProductFiles->page),
+            ($this->getProductFilesCount)($supplier->identifier),
+        );
+    }
+}
