5d41e0cffd67d2856cd2d15f4049594a65ef2b62
Ahmed Romdhani <6512508+aromdhani-code@users.noreply.github.com>
2022-11-08 18:08:50 +0100

    PH-441-bis2: Add new module Cloud-Build for Timmy (#15447)
    * add cloud build for timmy
    
    * fix tf command
    
    * add timmy cloudbuild trigger
    
    * rename file and add module in shared
    
    * fix region
    
    * delete empty line
    
    * rename files
    
    * rename tirigger
    
    * delete unused code
    
    * add env and region
    
    * add target_project_id
    
    * delete empty line
    
    * fix logs
    
    * modify tf plan
    
    * add apply step
    
    * debug terrafrom
    
    * fix branch name
    
    * create new module for timmy
    
    * delete unused code

---
+++ deployments-ucs/infra/terraform/modules/cloudbuild-timmy/cloudbuild.tf
@@ -0,0 +1,62 @@
+resource "google_cloudbuild_trigger" "pr-trigger" {
+  name            = "${var.trigger_name}-plan"
+  project         = var.project_id
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.cloudbuild_service_account}"
+  filename        = var.cloudbuild_filename
+  included_files  = var.cloudbuild_included_files
+
+  github {
+    owner = var.cloudbuild_github_org
+    name  = var.cloudbuild_github_repository
+    pull_request {
+      branch = "^${var.cloudbuild_github_branch}$"
+    }
+  }
+
+ substitutions = {
+    _CLUSTER_ENV             = var.env
+    _GOOGLE_CLUSTER_REGION   = var.region
+    _GOOGLE_PROJECT_ID       = var.target_project_id
+    _TARGET_IMPERSONATE      = "${var.impersonate}@${var.target_project_id}.iam.gserviceaccount.com"
+ }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
+resource "google_cloudbuild_trigger" "master-trigger" {
+  name            = "${var.trigger_name}-apply"
+  project         = var.project_id
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.cloudbuild_service_account}"
+  filename        = var.cloudbuild_filename
+  included_files  = var.cloudbuild_included_files
+
+  github {
+    owner = var.cloudbuild_github_org
+    name  = var.cloudbuild_github_repository
+    push {
+      branch = "^${var.cloudbuild_github_branch}$"
+    }
+  }
+
+  approval_config {
+    approval_required = true
+  }
+
+ substitutions = {
+    _CLUSTER_ENV             = var.env
+    _GOOGLE_CLUSTER_REGION   = var.region
+    _GOOGLE_PROJECT_ID       = var.target_project_id
+    _TARGET_IMPERSONATE      = "${var.impersonate}@${var.target_project_id}.iam.gserviceaccount.com"
+ }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
+resource "google_storage_bucket" "cloudbuild_logs" {
+  name          = "${var.trigger_name}-cloudbuild-logs"
+  location      = "EU"
+  force_destroy = true
+  project       = var.project_id
+
+  uniform_bucket_level_access = true
+}
