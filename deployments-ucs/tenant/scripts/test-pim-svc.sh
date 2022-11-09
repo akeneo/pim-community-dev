a6d7a8a44872cced689f3b39c444f1c59f770352
vgelot <vincent.gelot@akeneo.com>
2022-11-09 14:05:55 +0100

    PH-426 - Readd cronjob for tenants (#15430)

--- deployments-ucs/tenant/scripts/test-pim-svc.sh
+++ deployments-ucs/tenant/scripts/test-pim-svc.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -ex
 
-# Workarround for self signed certificate
-CURL="curl -k --connect-timeout 10 --retry 5 --retry-delay 5 --retry-connrefused"
-# CURL="curl --connect-timeout 10 --retry 5 --retry-delay 5 --retry-connrefused"
+CURL="curl -k --connect-timeout 10 --retry 5 --retry-delay 5 --retry-connrefused --http1.1"
 
 echo "Testing ${TARGET}"
-timeout 500 curl --retry 300 --retry-delay 30 -k ${TARGET} > /dev/null
+timeout 500 curl --retry 300 --retry-delay 30 -k ${TARGET} --http1.1 > /dev/null
 
 ${CURL} --output - --header "${MONITORING_AUTHENTICATION_TOKEN}" ${TARGET}
 
