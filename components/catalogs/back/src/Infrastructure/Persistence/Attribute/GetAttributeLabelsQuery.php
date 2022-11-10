549b55b628a8f11904df2f72ce5389ff26c58547
Willy Mesnage <willy.mesnage@akeneo.com>
2022-11-10 12:01:05 +0100

    CXP-1289: Translates product mapping attribute codes into label

---
+++ components/catalogs/back/src/Infrastructure/Persistence/Attribute/GetAttributeLabelsQuery.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Catalogs\Infrastructure\Persistence\Attribute;
+
+use Akeneo\Catalogs\Application\Persistence\Attribute\GetAttributeLabelsQueryInterface;
+use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
+use Akeneo\Tool\Component\StorageUtils\Repository\SearchableRepositoryInterface;
+
+/**
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GetAttributeLabelsQuery implements GetAttributeLabelsQueryInterface
+{
+    public function __construct(
+        private SearchableRepositoryInterface $searchableAttributeRepository,
+    ) {
+    }
+
+    public function execute(array $attributeCodes): array
+    {
+        $attributes = $this->searchableAttributeRepository->findBySearch(options: ['identifiers' => $attributeCodes, 'locale' => null]);
+
+        return \array_reduce(
+            $attributes,
+            static function (array $normalized, AttributeInterface $attribute): array {
+                $normalized[$attribute->getCode()] = [
+                    'code' => $attribute->getCode(),
+                    'label' => $attribute->getLabel(),
+                ];
+
+                return $normalized;
+            },
+            []
+        );
+    }
+}
