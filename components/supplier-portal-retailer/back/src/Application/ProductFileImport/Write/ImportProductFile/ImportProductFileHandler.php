b72190ede842d2cbcafa0658befb48bfd5d68a90
kévin <kevin.petiteau@akeneo.com>
2022-11-10 17:06:15 +0100

    SP-289 : correction import Identifier

--- components/supplier-portal-retailer/back/src/Application/ProductFileImport/Write/ImportProductFile/ImportProductFileHandler.php
+++ components/supplier-portal-retailer/back/src/Application/ProductFileImport/Write/ImportProductFile/ImportProductFileHandler.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Akeneo\SupplierPortal\Retailer\Application\ProductFileImport\Write\ImportProductFile;
 
 use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\StreamStoredProductFile;
+use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Write\Model\ProductFile\Identifier;
 use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Write\ProductFileRepository;
-use Akeneo\SupplierPortal\Retailer\Domain\ProductFileDropping\Write\ValueObject\Identifier;
 use Akeneo\SupplierPortal\Retailer\Domain\ProductFileImport\Write;
 use Akeneo\SupplierPortal\Retailer\Domain\ProductFileImport\Write\Exception\ProductFileDoesNotExist;
 use Akeneo\SupplierPortal\Retailer\Domain\ProductFileImport\Write\Model\ProductFileImport;
