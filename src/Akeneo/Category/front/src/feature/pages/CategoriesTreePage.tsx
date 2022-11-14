92f9ae2972f09aa41f29c50954ecc3f2c28e0e47
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2022-11-14 17:54:51 +0100

    GRF-200: Fix missing parameter

--- src/Akeneo/Category/front/src/feature/pages/CategoriesTreePage.tsx
+++ src/Akeneo/Category/front/src/feature/pages/CategoriesTreePage.tsx
@@ -1,22 +1,27 @@
 import React, {FC, useEffect, useState} from 'react';
 import {useParams} from 'react-router';
-import {Breadcrumb, SectionTitle, SkeletonPlaceholder, useBooleanState} from 'akeneo-design-system';
+import {Breadcrumb, SectionTitle, SkeletonPlaceholder, useBooleanState, Button} from 'akeneo-design-system';
 import {
   FullScreenError,
+  NotificationLevel,
   PageContent,
   PageHeader,
   PimView,
   useFeatureFlags,
   useRouter,
+  useNotify,
   useSecurity,
   useSessionStorageState,
   useSetPageTitle,
   useTranslate,
+  useUserContext,
 } from '@akeneo-pim-community/shared';
 import {CategoryToDelete, useCategoryTree, useDeleteCategory} from '../hooks';
 import {CategoryTree} from '../components';
 import {NewCategoryModal} from './NewCategoryModal';
 import {DeleteCategoryModal} from '../components/datagrids/DeleteCategoryModal';
+import {createTemplate} from '../components/templates/createTemplate';
+import {CategoryTreeModel, Template} from '../models';
 
 type Params = {
   treeId: string;
@@ -36,6 +41,7 @@ const CategoriesTreePage: FC = () => {
   let {treeId} = useParams<Params>();
   const router = useRouter();
   const translate = useTranslate();
+  const notify = useNotify();
   const {isGranted} = useSecurity();
   const featureFlags = useFeatureFlags();
   const [lastSelectedCategory] = useSessionStorageState<lastSelectedCategory>(
@@ -55,6 +61,8 @@ const CategoriesTreePage: FC = () => {
   const [isDeleteCategoryModalOpen, openDeleteCategoryModal, closeDeleteCategoryModal] = useBooleanState();
   const [categoryToDelete, setCategoryToDelete] = useState<CategoryToDelete | null>(null);
   const {isCategoryDeletionPossible, handleDeleteCategory} = useDeleteCategory();
+  const userContext = useUserContext();
+  const catalogLocale = userContext.get('catalogLocale');
 
   useSetPageTitle(translate('pim_title.pim_enrich_categorytree_tree', {'category.label': treeLabel}));
 
@@ -92,6 +100,30 @@ const CategoriesTreePage: FC = () => {
   const handleCloseDeleteCategoryModal = () => {
     setCategoryToDelete(null);
     closeDeleteCategoryModal();
+  };
+
+  const onCreateTemplate = (categoryTree: CategoryTreeModel) => {
+    createTemplate(categoryTree, catalogLocale, router)
+      .then(response => {
+        response.json().then((template: Template) => {
+          if (template) {
+            notify(NotificationLevel.SUCCESS, translate('akeneo.category.template.notification_success'));
+            redirectToTemplate(categoryTree.id, template.uuid);
+          }
+        });
+      })
+      .catch(() => {
+        notify(NotificationLevel.ERROR, translate('akeneo.category.template.notification_error'));
+      });
+  };
+
+  const redirectToTemplate = (treeId: number, templateId: string) => {
+    router.redirect(
+      router.generate('pim_category_template_edit', {
+        treeId: treeId,
+        templateId: templateId,
+      })
+    );
   };
 
   useEffect(() => {
@@ -132,6 +164,21 @@ const CategoriesTreePage: FC = () => {
             className="AknTitleContainer-userMenuContainer AknTitleContainer-userMenu"
           />
         </PageHeader.UserActions>
+        {featureFlags.isEnabled('enriched_category') && isGranted('pim_enrich_product_category_template') && (
+          <PageHeader.Actions>
+            {tree && (
+              <Button
+                onClick={() =>
+                  tree.templateUuid ? redirectToTemplate(tree.id, tree.templateUuid) : onCreateTemplate(tree)
+                }
+                level="tertiary"
+                ghost
+              >
+                {translate(tree.templateUuid ? 'akeneo.category.template.edit' : 'akeneo.category.template.create')}
+              </Button>
+            )}
+          </PageHeader.Actions>
+        )}
         <PageHeader.Title>{tree?.label ?? treeId}</PageHeader.Title>
       </PageHeader>
       <PageContent>
