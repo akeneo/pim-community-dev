b7926fa54fab5737fd6b3ffdf37abcf3a2775e8d
Benjamin Letellier <letellier.benjamin@gmail.com>
2022-11-15 10:29:07 +0100

    CPM-814 : add "void_return" rule to php-cs-fixer

--- components/identifier-generator/back/tests/Specification/Infrastructure/Subscriber/SetIdentifiersSubscriberSpec.php
+++ components/identifier-generator/back/tests/Specification/Infrastructure/Subscriber/SetIdentifiersSubscriberSpec.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Specification\Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Subscriber;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\Application\Generate\GenerateIdentifierHandler;
@@ -31,7 +33,7 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
     public function let(
         IdentifierGeneratorRepository $identifierGeneratorRepository,
         ValidatorInterface $validator,
-    ) {
+    ): void {
         $this->beConstructedWith(
             $identifierGeneratorRepository,
             new GenerateIdentifierHandler(new \ArrayIterator([
@@ -41,17 +43,17 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
         );
     }
 
-    public function it_should_be_an_event_subscriber()
+    public function it_should_be_an_event_subscriber(): void
     {
         $this->beAnInstanceOf(SetIdentifiersSubscriber::class);
         $this->beAnInstanceOf(EventSubscriberInterface::class);
     }
 
-    public function it_should_listen_to_pre_save_events()
+    public function it_should_listen_to_pre_save_events(): void
     {
         Assert::assertSame(\array_keys($this->getSubscribedEvents()->getWrappedObject()), [
             StorageEvents::PRE_SAVE,
-            StorageEvents::PRE_SAVE_ALL
+            StorageEvents::PRE_SAVE_ALL,
         ]);
     }
 
@@ -59,7 +61,7 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
         IdentifierGeneratorRepository $identifierGeneratorRepository,
         ValidatorInterface $validator,
         ProductInterface $product,
-    ) {
+    ): void {
         $product->getValue('sku')->shouldBeCalled()->willReturn(null);
         $identifierGeneratorRepository->getAll()->shouldBeCalled()->willReturn([$this->getIdentifierGenerator()]);
         $value = ScalarValue::value('sku', 'AKN');
@@ -75,14 +77,14 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
         ValidatorInterface $validator,
         ProductInterface $product,
         ConstraintViolationInterface $constraintViolation,
-    ) {
+    ): void {
         $product->getValue('sku')->shouldBeCalled()->willReturn(null);
         $identifierGeneratorRepository->getAll()->shouldBeCalled()->willReturn([$this->getIdentifierGenerator()]);
         $value = ScalarValue::value('sku', 'AKN');
         $product->addValue($value)->shouldBeCalled();
         $product->setIdentifier('AKN')->shouldBeCalled();
         $validator->validate($product)->shouldBeCalled()->willReturn(new ConstraintViolationList([
-            $constraintViolation->getWrappedObject()
+            $constraintViolation->getWrappedObject(),
         ]));
         $product->removeValue($value)->shouldBeCalled();
         $product->setIdentifier(null)->shouldBeCalled();
@@ -96,7 +98,7 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
         ProductInterface $product1,
         ProductInterface $product2,
         ConstraintViolationInterface $constraintViolation,
-    ) {
+    ): void {
         $identifierGeneratorRepository->getAll()->shouldBeCalled()->willReturn([$this->getIdentifierGenerator()]);
         $value = ScalarValue::value('sku', 'AKN');
 
@@ -109,27 +111,27 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
         $product2->addValue($value)->shouldBeCalled();
         $product2->setIdentifier('AKN')->shouldBeCalled();
         $validator->validate($product2)->shouldBeCalled()->willReturn(new ConstraintViolationList([
-            $constraintViolation->getWrappedObject()
+            $constraintViolation->getWrappedObject(),
         ]));
         $product2->removeValue($value)->shouldBeCalled();
         $product2->setIdentifier(null)->shouldBeCalled();
 
         $this->setIdentifiers(new GenericEvent([
             $product1->getWrappedObject(),
-            $product2->getWrappedObject()
+            $product2->getWrappedObject(),
         ]));
     }
 
     public function it_should_do_nothing_if_subject_is_not_a_product(
         IdentifierGeneratorRepository $identifierGeneratorRepository,
-    ) {
+    ): void {
         $this->setIdentifier(new GenericEvent(new \stdClass()));
         $identifierGeneratorRepository->getAll()->shouldNotBeCalled();
     }
 
     public function it_should_do_nothing_if_subject_is_not_an_array(
         IdentifierGeneratorRepository $identifierGeneratorRepository,
-    ) {
+    ): void {
         $this->setIdentifiers(new GenericEvent(new \stdClass()));
         $identifierGeneratorRepository->getAll()->shouldNotBeCalled();
     }
@@ -137,7 +139,7 @@ class SetIdentifiersSubscriberSpec extends ObjectBehavior
     public function it_should_do_nothing_if_a_subject_is_not_a_product(
         IdentifierGeneratorRepository $identifierGeneratorRepository,
         ProductInterface $product,
-    ) {
+    ): void {
         $this->setIdentifiers(new GenericEvent([$product->getWrappedObject(), new \stdClass()]));
         $identifierGeneratorRepository->getAll()->shouldNotBeCalled();
     }
