3cdfe8e41fb1f8306d90996210bb8c43803233f4
Adam <101580975+adam-akeneo@users.noreply.github.com>
2022-11-16 10:11:58 +0100

    SDO-262-pim Use new version of cloud-deployer on PIM CI(#14814)

--- deployments/terraform/kubeconfig.tf
+++ deployments/terraform/kubeconfig.tf
@@ -14,12 +14,23 @@ apiVersion: v1
 kind: Config
 current-context: ${data.google_container_cluster.main.name}
 contexts: [{name: ${data.google_container_cluster.main.name}, context: {cluster: ${data.google_container_cluster.main.name}, user: user}}]
-users: [{name: user, user: {auth-provider: {name: gcp}}}]
+users:
+- name: user
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      args: null
+      command: gke-gcloud-auth-plugin
+      env: null
+      installHint: Install gke-gcloud-auth-plugin for use with kubectl by following
+        https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+      interactiveMode: Never
+      provideClusterInfo: true
 clusters:
 - name: ${data.google_container_cluster.main.name}
   cluster:
     server: https://${data.google_container_cluster.main.endpoint}
-    certificate-authority-data: ${data.google_container_cluster.main.master_auth[0].cluster_ca_certificate}                                                                                                 
+    certificate-authority-data: ${data.google_container_cluster.main.master_auth[0].cluster_ca_certificate}
 EOF
 
 }
