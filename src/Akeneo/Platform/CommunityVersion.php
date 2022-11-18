1b8e0b378238e6a1a77c2b9fd6693731f974ac5a
Michel Tag <micheltag@akeneo.com>
2022-11-18 10:06:33 +0100

    Preparing version 5.0.118 (#18452)
    Preparing version 5.0.118
    
    
    Co-authored-by: jmleroux <no-reply@akeneo.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    const VERSION = '5.0.117';
+    const VERSION = '5.0.118';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Bully for Bugs';
