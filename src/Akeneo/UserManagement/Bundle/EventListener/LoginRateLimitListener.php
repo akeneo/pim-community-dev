f322b4241d6820de9682dc23ce4b12b9e375bfe1
Steven VAIDIE <steven.vaidie@gmail.com>
2022-11-21 10:26:14 +0100

    RAB-1149: Implement new authenticator manager

--- src/Akeneo/UserManagement/Bundle/EventListener/LoginRateLimitListener.php
+++ src/Akeneo/UserManagement/Bundle/EventListener/LoginRateLimitListener.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class RateLimitLoginThrottlingListener implements EventSubscriberInterface
+final class LoginRateLimitListener implements EventSubscriberInterface
 {
     public function __construct(
         private UserManager $userManager,
