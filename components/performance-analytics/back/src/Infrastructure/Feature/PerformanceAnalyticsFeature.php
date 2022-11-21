f8904f2c184a2272c88fd7aeb19ae4068f296661
Laurent Pétard <laurent.petard@akeneo.com>
2022-11-21 15:49:08 +0100

    JEL: Fix activation of Performance-Analytics feature (#15566)
    * JEL: Fix activation of Performance Analytics in configmap
    
    * JEL: Add specific feature flag service for Performance-Analytics

---
+++ components/performance-analytics/back/src/Infrastructure/Feature/PerformanceAnalyticsFeature.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (http://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\PerformanceAnalytics\Infrastructure\Feature;
+
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlag;
+
+/**
+ * To remove when Performance-Analytics will be enabled on serenity edition by default
+ * Then we should not need to rely on an additional env var, but use directly the service OnlySerenityFeatureFlag.
+ */
+final class PerformanceAnalyticsFeature implements FeatureFlag
+{
+    public function __construct(
+        private FeatureFlag $onlySerenityFeature,
+        private mixed $performanceAnalyticsEnabledEnvVar
+    ) {
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->onlySerenityFeature->isEnabled() && \boolval($this->performanceAnalyticsEnabledEnvVar);
+    }
+}
