57ecb1229b088d0a275231d31f9feb848e81f8eb
Adrien Migaire <adrien.migaire@akeneo.com>
2022-11-22 15:25:30 +0100

    GRF-394 use DSM props instead of custom selected management and style

--- src/Akeneo/Category/front/src/feature/components/channel/ChannelSelector.tsx
+++ src/Akeneo/Category/front/src/feature/components/channel/ChannelSelector.tsx
@@ -1,47 +1,20 @@
-import {Channel, ChannelCode, useTranslate} from '@akeneo-pim-community/shared';
+import {Channel, ChannelCode, useTranslate, useUserContext} from '@akeneo-pim-community/shared';
 import {
-  AkeneoThemedProps,
   Dropdown,
-  getColor,
-  getFontSize,
-  Pill,
   SwitcherButton,
   useBooleanState
 } from 'akeneo-design-system';
 import React, {forwardRef, Ref} from 'react';
-import styled, {css} from "styled-components";
-
-const DropdownContainer = styled(Dropdown)`
-  text-transform: none;
-  font-size: ${getFontSize('default')};
-  color: ${getColor('grey', 120)};
-`;
 
 const ChannelSpan = forwardRef<HTMLSpanElement, ChannelProps>(
     ({code, label, ...rest}: ChannelProps, forwardedRef: Ref<HTMLSpanElement>) => {
       return (
           <span ref={forwardedRef} {...rest}>
-          {label || code}
+          {label || "["+code+"]"}
         </span>
       );
     }
 );
-
-const HighlightChannel = styled(ChannelSpan)<{selected?: boolean} & AkeneoThemedProps>`
-  ${({selected}) =>
-    selected &&
-    css`
-      color: ${getColor('purple100')};
-      font-style: italic;
-      font-weight: bold;
-    `}
-`;
-
-const ChannelDropdownItem = styled(Dropdown.Item)`
-  justify-content: space-between;
-  align-items: center;
-  white-space: nowrap;
-`;
 
 type ChannelProps = {
   code: string;
@@ -51,20 +24,21 @@ type ChannelProps = {
 type ChannelSelectorProps = {
   value: ChannelCode;
   values: Channel[];
-  completeValues?: ChannelCode[]
   onChange: (value: ChannelCode) => void;
 }
-const ChannelSelector = ({value, values, completeValues, onChange}: ChannelSelectorProps) => {
+const ChannelSelector = ({value, values, onChange}: ChannelSelectorProps) => {
   const translate = useTranslate();
+  const userContext = useUserContext();
+  const catalogLocale = userContext.get('catalogLocale');
   const [isOpen, open, close] = useBooleanState(false);
   const selectedChannel: Channel = values.find(channel => channel.code === value) || values[0];
 
   const handleChange = (channelCode: ChannelCode) => onChange?.(channelCode);
 
   return (
-    <DropdownContainer>
+    <Dropdown>
       <SwitcherButton inline onClick={() => {open();}} label={translate('pim_common.channel')}>
-        <HighlightChannel code={"["+selectedChannel.code+"]"} label={selectedChannel.labels['en_US']} />
+        <ChannelSpan code={"["+selectedChannel.code+"]"} label={selectedChannel.labels[catalogLocale]} />
       </SwitcherButton>
       {isOpen && (
         <Dropdown.Overlay verticalPosition="down" onClose={close}>
@@ -74,31 +48,28 @@ const ChannelSelector = ({value, values, completeValues, onChange}: ChannelSelec
           <Dropdown.ItemCollection>
             {values.map(channel => {
                 return (
-                  <ChannelDropdownItem
+                  <Dropdown.Item
                     aria-selected={channel.code === value}
                     key={channel.code}
                     onClick={() => {
                       close();
                       handleChange(channel.code);
                     }}
+                    isActive={channel.code === selectedChannel.code}
                   >
-                    <HighlightChannel
-                      code={"["+channel.code+"]"}
-                      label={channel.labels['en_US']}
-                      selected={channel.code === value}
+                    <ChannelSpan
+                      code={channel.code}
+                      label={channel.labels[catalogLocale]}
                       key={channel.code}
                     />
-                    {completeValues && !completeValues.includes(channel.code) && (
-                        <Pill level="warning" data-testid={`ChannelSelector.incomplete.${channel.code}`} />
-                    )}
-                  </ChannelDropdownItem>
+                  </Dropdown.Item>
                 );
               }
             )}
           </Dropdown.ItemCollection>
         </Dropdown.Overlay>
       )}
-    </DropdownContainer>
+    </Dropdown>
   );
 }
 
