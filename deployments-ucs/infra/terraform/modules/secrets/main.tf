979f044a8a4c90afd8b50cdecb59891c6c7b8290
Ahmed Romdhani <6512508+aromdhani-code@users.noreply.github.com>
2022-11-23 15:07:21 +0100

    Ph 519 (#15598)
    * add viewr role to secret
    
    * quick fix
    
    * fix the role

--- deployments-ucs/infra/terraform/modules/secrets/main.tf
+++ deployments-ucs/infra/terraform/modules/secrets/main.tf
@@ -28,3 +28,11 @@ resource "google_secret_manager_secret_iam_binding" "secret_version_manager" {
   role      = "roles/secretmanager.secretVersionManager"
   members   = local.secrets[each.value.name].members
 }
+
+resource "google_secret_manager_secret_iam_binding" "secret_viewer" {
+  for_each  = local.secrets
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.this[each.value.name].secret_id
+  role      = "roles/secretmanager.viewer"
+  members   = local.secrets[each.value.name].members
+}
