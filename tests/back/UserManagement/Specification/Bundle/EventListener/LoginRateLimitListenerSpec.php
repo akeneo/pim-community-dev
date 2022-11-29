9d2f55e2128639fd8cbccde7de3e6f057e96a9a8
Grégoire Houssard <ghoussard@protonmail.com>
2022-11-29 12:07:31 +0100

    RAB-1149: Fix spec

--- tests/back/UserManagement/Specification/Bundle/EventListener/LoginRateLimitListenerSpec.php
+++ tests/back/UserManagement/Specification/Bundle/EventListener/LoginRateLimitListenerSpec.php
@@ -1,205 +1,149 @@
 <?php
 
-namespace Specification\Akeneo\UserManagement\Bundle\Provider;
+namespace Specification\Akeneo\UserManagement\Bundle\EventListener;
 
+use Akeneo\UserManagement\Bundle\EventListener\LoginRateLimitListener;
 use Akeneo\UserManagement\Bundle\Manager\UserManager;
 use Akeneo\UserManagement\Bundle\Model\LockedAccountException;
-use Akeneo\UserManagement\Bundle\Provider\CustomDaoAuthenticationProvider;
 use Akeneo\UserManagement\Component\Model\UserInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
-use Symfony\Component\Security\Core\User\UserCheckerInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginRateLimitListenerSpec extends ObjectBehavior
 {
-    const ACCOUNT_LOCK_DURATION = 2;
-    const ALLOWED_FAILED_ATTEMPTS = 10;
-    const PROVIDER_KEY = "providerKey";
-    const USERNAME = "username";
+    private const ACCOUNT_LOCK_DURATION = 2;
+    private const ALLOWED_FAILED_ATTEMPTS = 10;
 
-    public function let(UserProviderInterface $userProvider, UserCheckerInterface $userChecker, PasswordHasherFactoryInterface $hasherFactory, UserManager $userManager)
+    public function let(UserManager $userManager)
     {
-        $this->beConstructedWith($userProvider, $userChecker, self::PROVIDER_KEY, $hasherFactory, $userManager, self::ACCOUNT_LOCK_DURATION, self::ALLOWED_FAILED_ATTEMPTS, false);
+        $this->beConstructedWith($userManager, self::ACCOUNT_LOCK_DURATION, self::ALLOWED_FAILED_ATTEMPTS, false);
     }
 
     public function it_is_initializable()
     {
-        $this->shouldHaveType(CustomDaoAuthenticationProvider::class);
+        $this->shouldHaveType(LoginRateLimitListener::class);
     }
 
-    public function it_can_authenticate_when_counter_is_reset(UserManager $userManager, UserInterface $user, UsernamePasswordToken $usernamePasswordToken)
-    {
+    public function it_can_authenticate_when_counter_is_reset(
+        UserManager $userManager,
+        Passport $passport,
+        UserBadge $badge,
+        UserInterface $user,
+        CheckPassportEvent $event,
+    ): void {
         $this->initUser($user, 0, null);
-        $this->initUserNamePasswordTokenWithUser($usernamePasswordToken, $user);
+        $this->initPassport($passport, $badge, $user);
+        $event->getPassport()->willReturn($passport);
 
-        $this->authenticate($usernamePasswordToken);
+        $this->checkPassport($event);
 
         $userManager->updateUser()->shouldNotHaveBeenCalled();
     }
 
-    public function it_can_authenticate_under_max_limit_counter(UserManager $userManager, UserInterface $user, UsernamePasswordToken $usernamePasswordToken)
-    {
-        $this->initUser(
-            $user,
-            self::ALLOWED_FAILED_ATTEMPTS - 1,
-            $this->initialFailedAttemptDateBackFromNow(1)
-        );
+    public function it_can_authenticate_under_max_limit_counter(
+        Passport $passport,
+        UserBadge $badge,
+        UserInterface $user,
+        CheckPassportEvent $event,
+    ): void {
+        $this->initUser($user, self::ALLOWED_FAILED_ATTEMPTS - 1, $this->getAuthenticationFailureResetDateFromNow(1));
+        $this->initPassport($passport, $badge, $user);
+        $event->getPassport()->willReturn($passport);
 
-        $this->initUserNamePasswordTokenWithUser($usernamePasswordToken, $user);
-
-        $this->authenticate($usernamePasswordToken);
-
-        $this->shouldResetlockState($user, $userManager);
+        $this->checkPassport($event);
     }
 
-    public function it_rejects_authentication_when_limit_is_reached(UserManager $userManager, UserInterface $user, UsernamePasswordToken $usernamePasswordToken)
-    {
-        $this->initUser($user, self::ALLOWED_FAILED_ATTEMPTS, $this->initialFailedAttemptDateBackFromNow(self::ACCOUNT_LOCK_DURATION - 1));
-        $this->initUserNamePasswordTokenWithUser($usernamePasswordToken, $user);
+    public function it_rejects_authentication_when_limit_is_reached(
+        Passport $passport,
+        UserBadge $badge,
+        UserInterface $user,
+        CheckPassportEvent $event,
+    ): void {
+        $this->initUser($user, self::ALLOWED_FAILED_ATTEMPTS, $this->getAuthenticationFailureResetDateFromNow(self::ACCOUNT_LOCK_DURATION - 1));
+        $this->initPassport($passport, $badge, $user);
+        $event->getPassport()->willReturn($passport);
 
         $this->shouldThrow(new LockedAccountException(self::ACCOUNT_LOCK_DURATION))
-            ->duringAuthenticate($usernamePasswordToken);
-        $this->shouldNotChangeState($userManager);
+            ->duringCheckPassport($event);
     }
 
     public function it_rejects_authentication_when_user_just_reach_max_attempt(
+        Passport $passport,
+        UserBadge $badge,
         UserInterface $user,
-        UserProviderInterface $userProvider,
-        UsernamePasswordToken $usernamePasswordToken,
-        PasswordHasherInterface $passwordHasher,
-        PasswordHasherFactoryInterface $hasherFactory
-    ) {
-        $this->initUser(
-            $user,
-            self::ALLOWED_FAILED_ATTEMPTS - 1,
-            $this->initialFailedAttemptDateBackFromNow(self::ACCOUNT_LOCK_DURATION - 1)
-        );
-
-        $user->setConsecutiveAuthenticationFailureCounter(10)->shouldBeCalled()->will(function () {
-            $this->getConsecutiveAuthenticationFailureCounter()->willReturn(20);
-        });
-
-        $this->initUsernamePasswordToken($usernamePasswordToken);
-        $userProvider->loadUserByIdentifier(self::USERNAME)->shouldBeCalled()->willReturn($user);
-        $passwordHasher->verify(Argument::any(), Argument::any(), Argument::any())->willReturn(false);
-        $hasherFactory->getPasswordHasher(Argument::any())->willReturn($passwordHasher);
+        CheckPassportEvent $event,
+    ): void {
+        $this->initUser($user, self::ALLOWED_FAILED_ATTEMPTS, $this->getAuthenticationFailureResetDateFromNow(self::ACCOUNT_LOCK_DURATION - 1));
+        $this->initPassport($passport, $badge, $user);
+        $event->getPassport()->willReturn($passport);
 
         $this->shouldThrow(LockedAccountException::class)
-            ->duringAuthenticate($usernamePasswordToken);
+            ->duringCheckPassport($event);
     }
 
-    public function it_increase_failed_attempts_counter(
-        PasswordHasherFactoryInterface $hasherFactory,
-        UserManager $userManager,
-        UserProviderInterface $userProvider,
-        PasswordHasherInterface $passwordHasher,
+    public function it_increase_failed_attempts_counter_on_login_failure(
+        Passport $passport,
+        UserBadge $badge,
         UserInterface $user,
-        UsernamePasswordToken $usernamePasswordToken
-    ) {
-        $this->initUser(
-            $user,
-            self::ALLOWED_FAILED_ATTEMPTS - 1,
-            $this->initialFailedAttemptDateBackFromNow(self::ACCOUNT_LOCK_DURATION - 1)
-        );
+        LoginFailureEvent $event,
+        UserManager $userManager,
+    ): void {
+        $this->initUser($user,self::ALLOWED_FAILED_ATTEMPTS - 1, $this->getAuthenticationFailureResetDateFromNow(self::ACCOUNT_LOCK_DURATION - 1));
+        $this->initPassport($passport, $badge, $user);
+        $event->getPassport()->willReturn($passport);
 
-        $this->initUsernamePasswordToken($usernamePasswordToken);
-        $userProvider->loadUserByIdentifier(self::USERNAME)->shouldBeCalled()->willReturn($user);
-        $passwordHasher->verify(Argument::any(), Argument::any(), Argument::any())->willReturn(false);
-        $hasherFactory->getPasswordHasher(Argument::any())->willReturn($passwordHasher);
-        $this->shouldThrow(BadCredentialsException::class)
-            ->duringAuthenticate($usernamePasswordToken);
+        $this->onFailureLogin($event);
 
-        $this->checkLockStateUpdated($userManager, $user, self::ALLOWED_FAILED_ATTEMPTS);
+        $this->lockStateShouldBeUpdated($userManager, $user, self::ALLOWED_FAILED_ATTEMPTS);
     }
 
-    public function it_initialize_failed_attempts_after_reset(UserManager $userManager, UserProviderInterface $userProvider, PasswordHasherFactoryInterface $hasherFactory, PasswordHasherInterface $passwordHasher, UserInterface $user, UsernamePasswordToken $usernamePasswordToken)
-    {
-        $this->initUser(
-            $user,
-            self::ALLOWED_FAILED_ATTEMPTS - 1,
-            self::initialFailedAttemptDateBackFromNow(self::ACCOUNT_LOCK_DURATION + 1)
-        );
-        $user->getConsecutiveAuthenticationFailureCounter()->willReturn(0);
-        $this->initUsernamePasswordToken($usernamePasswordToken);
-        $userProvider->loadUserByIdentifier(self::USERNAME)->willReturn($user);
-        $hasherFactory->getPasswordHasher(Argument::any())->willReturn($passwordHasher);
+    public function it_reset_failed_attempts_on_login_success(
+        UserInterface $user,
+        LoginSuccessEvent $event,
+        UserManager $userManager,
+    ): void {
+        $this->initUser($user, 0, $this->getAuthenticationFailureResetDateFromNow(self::ACCOUNT_LOCK_DURATION + 1));
+        $event->getUser()->willReturn($user);
 
-        $this->shouldThrow(BadCredentialsException::class)
-            ->duringAuthenticate($usernamePasswordToken);
+        $this->onSuccessfulLogin($event);
 
-        $this->checkLockStateReset($user, $userManager);
-
-        $this->checkLockStateInitialized($userManager, $user);
+        $this->lockStateShouldBeReset($user, $userManager);
     }
 
-    private static function initialFailedAttemptDateBackFromNow(int $secondsBehind): \DateTime
+    private function initUser(UserInterface $user, int $consecutiveAuthenticationFailureCounter, \DateTime $authenticationFailureResetDate): void
     {
-        return (new \DateTime())->modify("-{$secondsBehind} minute");
-    }
-
-    private function checkLockStateUpdated(UserManager $userManager, UserInterface $user, int $fal): void
-    {
-        $user->setConsecutiveAuthenticationFailureCounter($fal)->shouldHaveBeenCalledOnce();
-        $user->setAuthenticationFailureResetDate()->shouldNotBeCalled();
-        $userManager->updateUser(Argument::any())->shouldHaveBeenCalled();
-    }
-
-    private function checkLockStateInitialized(UserManager $userManager, UserInterface $user)
-    {
-        $user->setConsecutiveAuthenticationFailureCounter(1)->shouldHaveBeenCalledOnce();
-        $user->setAuthenticationFailureResetDate()->shouldNotBeCalled();
-        $userManager->updateUser(Argument::any())->shouldHaveBeenCalled();
-    }
-
-    private function initUsernamePasswordToken(UsernamePasswordToken $usernamePasswordToken)
-    {
-        $usernamePasswordToken->getUser()->willReturn(self::USERNAME);
-        $usernamePasswordToken->getFirewallName()->willReturn(self::PROVIDER_KEY);
-        $usernamePasswordToken->getRoleNames(false)->willReturn([]);
-        $usernamePasswordToken->getAttributes()->willReturn([]);
-        $usernamePasswordToken->getUserIdentifier()->willReturn(self::USERNAME);
-        $usernamePasswordToken->getCredentials()->willReturn('');
-    }
-
-    private function initUser(UserInterface $user, int $i, \DateTime $initialFailureDate): void
-    {
-        $user->getConsecutiveAuthenticationFailureCounter()->willReturn($i);
-        $user->getAuthenticationFailureResetDate()->willReturn($initialFailureDate);
+        $user->getConsecutiveAuthenticationFailureCounter()->willReturn($consecutiveAuthenticationFailureCounter);
+        $user->getAuthenticationFailureResetDate()->willReturn($authenticationFailureResetDate);
         $user->getRoles()->willReturn([]);
-        $user->getPassword()->willReturn("");
-        $user->getSalt()->willReturn("");
+        $user->getPassword()->willReturn('');
+        $user->getSalt()->willReturn('');
     }
 
-    private function shouldResetlockState($user, $userManager): void
+    private function initPassport(Passport $passport, UserBadge $badge, UserInterface $user): void
     {
-        $user->setConsecutiveAuthenticationFailureCounter(0)->shouldHaveBeenCalledOnce();
-        $user->setAuthenticationFailureResetDate(null)->shouldHaveBeenCalledOnce();
-        $userManager->updateUser(Argument::any())->shouldHaveBeenCalled();
+        $passport->hasBadge(UserBadge::class)->willReturn(true);
+        $passport->getBadge(UserBadge::class)->willReturn($badge);
+        $badge->getUser()->willReturn($user);
     }
 
-    private function shouldNotChangeState($userManager): \Prophecy\Prophecy\MethodProphecy
+    private function getAuthenticationFailureResetDateFromNow(int $minutesBehind): \DateTime
     {
-        return $userManager->updateUser()->shouldNotHaveBeenCalled();
+        return (new \DateTime())->modify("-{$minutesBehind} minute");
     }
 
-    private function initUserNamePasswordTokenWithUser(UsernamePasswordToken $usernamePasswordToken, UserInterface $user): void
+    private function lockStateShouldBeUpdated(UserManager $userManager, UserInterface $user, int $expectedConsecutiveAuthenticationFailureCounter): void
     {
-        $usernamePasswordToken->getUser()->willReturn($user);
-        $usernamePasswordToken->getFirewallName()->willReturn(self::PROVIDER_KEY);
-        $usernamePasswordToken->getRoleNames()->willReturn([]);
-        $usernamePasswordToken->getAttributes()->willReturn([]);
-        $usernamePasswordToken->getUserIdentifier()->willReturn(self::USERNAME);
-        $usernamePasswordToken->getCredentials()->willReturn([]);
+        $user->setConsecutiveAuthenticationFailureCounter($expectedConsecutiveAuthenticationFailureCounter)->shouldHaveBeenCalledOnce();
+        $user->setAuthenticationFailureResetDate()->shouldNotBeCalled();
+        $userManager->updateUser($user)->shouldHaveBeenCalled();
     }
 
-    private function checkLockStateReset(UserInterface $user, UserManager $userManager): void
+    private function lockStateShouldBeReset(UserInterface $user, UserManager $userManager): void
     {
         $user->setConsecutiveAuthenticationFailureCounter(0)->shouldHaveBeenCalledOnce();
         $user->setAuthenticationFailureResetDate(null)->shouldHaveBeenCalled();
