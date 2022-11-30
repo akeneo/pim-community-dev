761eec6b2a5ecec306765b293bdc25e2d656aec2
pierallard <pierallard@gmail.com>
2022-11-30 14:49:10 +0100

    CPM-817: Remove condition

--- upgrades/schema/Version_7_0_20221130130031_update_identifier_generator_prefix_number_type.php
+++ upgrades/schema/Version_7_0_20221130130031_update_identifier_generator_prefix_number_type.php
@@ -20,29 +20,14 @@ final class Version_7_0_20221130130031_update_identifier_generator_prefix_number
 
     public function up(Schema $schema): void
     {
-        $this->addSql('SELECT 1');
-        if ($this->getColumnType() !== 'bigint unsigned') {
-            $this->addSql(<<<SQL
+        $this->addSql(<<<SQL
 ALTER TABLE pim_catalog_identifier_generator_prefixes 
     MODIFY `number` BIGINT UNSIGNED NOT NULL;
 SQL);
-        }
     }
 
     public function down(Schema $schema): void
     {
         $this->throwIrreversibleMigrationException();
-    }
-
-    private function getColumnType(): string
-    {
-        $sql = <<<SQL
-SELECT COLUMN_TYPE from information_schema.COLUMNS 
-WHERE TABLE_SCHEMA='%s' 
-  AND TABLE_NAME='pim_catalog_identifier_generator_prefixes'
-  AND COLUMN_NAME='number';
-SQL;
-
-        return $this->connection->fetchOne(\sprintf($sql, $this->connection->getDatabase()));
     }
 }
