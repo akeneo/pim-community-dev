dba6cda23a9ceccb9bbf823dade03f501887fab6
Willy Mesnage <willy.mesnage@akeneo.com>
2022-11-30 15:39:47 +0100

    CXP-1333: Be able to count the number of products in the selection criteria

---
+++ components/catalogs/front/src/components/ProductSelection/components/CountSelectedProductsByCriteria.tsx
@@ -0,0 +1,42 @@
+import React, {FC} from 'react';
+import {ProductSelectionValues} from '../models/ProductSelectionValues';
+import {getColor, getFontSize} from 'akeneo-design-system';
+import {useCountProductsInSelectionCriteria} from '../hooks/useCountProductsInSelectionCriteria';
+import styled from 'styled-components';
+import {useTranslate} from '@akeneo-pim-community/shared';
+
+const Container = styled.div`
+    border-right: 1px solid ${getColor('grey', 100)};
+    padding: 5px 20px 5px 0;
+    margin: 0 20px 0 0;
+    text-align: center;
+`;
+
+const Count = styled.span`
+    color: ${getColor('brand', 100)};
+    font-size: ${getFontSize('default')};
+`;
+
+type Props = {
+    criteria: ProductSelectionValues;
+};
+
+export const CountSelectedProductsByCriteria: FC<Props> = ({criteria}) => {
+    const translate = useTranslate();
+    const {data: count} = useCountProductsInSelectionCriteria(criteria);
+
+    let label = '';
+    if (null === count || undefined === count) {
+        label = translate('akeneo_catalogs.product_selection.count.error');
+    } else if (0 === count) {
+        label = count.toString() + ' ' + translate('akeneo_catalogs.product_selection.count.no_products');
+    } else {
+        label = count.toString() + ' ' + translate('akeneo_catalogs.product_selection.count.products');
+    }
+
+    return (
+        <Container>
+            <Count>{label}</Count>
+        </Container>
+    );
+};
