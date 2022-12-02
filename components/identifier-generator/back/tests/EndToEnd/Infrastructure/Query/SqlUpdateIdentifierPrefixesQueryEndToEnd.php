6763e678d079ad472417bf971f5baf8304b7f5cf
pierallard <pierallard@gmail.com>
2022-12-02 09:56:57 +0100

    CPM-817: Split endtoend tests

--- components/identifier-generator/back/tests/EndToEnd/Infrastructure/Query/SqlUpdateIdentifierPrefixesQueryEndToEnd.php
+++ components/identifier-generator/back/tests/EndToEnd/Infrastructure/Query/SqlUpdateIdentifierPrefixesQueryEndToEnd.php
@@ -5,32 +5,15 @@ declare(strict_types=1);
 namespace Akeneo\Test\Pim\Automation\IdentifierGenerator\EndToEnd\Infrastructure\Subscriber;
 
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductRepositoryInterface;
-use Akeneo\Pim\Enrichment\Product\API\Command\UpsertProductCommand;
-use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetIdentifierValue;
-use Akeneo\Pim\Enrichment\Product\API\ValueObject\ProductUuid;
-use Akeneo\Pim\Enrichment\Product\Application\UpsertProductHandler;
 use Akeneo\Test\Integration\Configuration;
-use Akeneo\Test\Integration\TestCase;
-use Akeneo\Test\IntegrationTestsBundle\Helper\AuthenticatorHelper;
+use Akeneo\Test\Pim\Automation\IdentifierGenerator\EndToEnd\Infrastructure\EndToEndTestCase;
 use Akeneo\Tool\Component\StorageUtils\Remover\RemoverInterface;
-use Akeneo\UserManagement\Component\Model\UserInterface;
 use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Assert;
-use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
-class SqlUpdateIdentifierPrefixesQueryEndToEnd extends TestCase
+class SqlUpdateIdentifierPrefixesQueryEndToEnd extends EndToEndTestCase
 {
-    private UserInterface $admin;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->admin = $this->createAdminUser();
-    }
-
     protected function getConfiguration(): Configuration
     {
         return $this->catalog->useMinimalCatalog();
@@ -51,7 +34,7 @@ class SqlUpdateIdentifierPrefixesQueryEndToEnd extends TestCase
     public function it_should_update_prefixes_on_update(): void
     {
         $productFromDatabase = $this->createProduct('my_identifier_123');
-        $this->setIdentifier($productFromDatabase, 'my_new_identifier_234');
+        $this->updateProductIdentifier($productFromDatabase, 'my_new_identifier_234');
         Assert::assertEquals($this->getPrefixes($productFromDatabase->getUuid(), $this->getIdentifierId()), [
             'my_new_identifier_' => '234',
             'my_new_identifier_2' => '34',
@@ -65,55 +48,6 @@ class SqlUpdateIdentifierPrefixesQueryEndToEnd extends TestCase
         $productFromDatabase = $this->createProduct('my_identifier_123');
         $this->deleteProduct($productFromDatabase);
         Assert::assertEquals($this->getPrefixes($productFromDatabase->getUuid(), $this->getIdentifierId()), []);
-    }
-
-    private function createProduct(?string $identifier = null): ProductInterface
-    {
-        $uuid = Uuid::uuid4();
-        $this->getAuthenticator()->logIn('admin');
-
-        $userIntents = [];
-        if (null !== $identifier) {
-            $userIntents = [new SetIdentifierValue('sku', $identifier)];
-        }
-
-        $command = UpsertProductCommand::createWithUuid(
-            $this->admin->getId(),
-            ProductUuid::fromUuid($uuid),
-            $userIntents
-        );
-        ($this->getUpsertProductHandler())($command);
-
-        return $this->getProductRepository()->find($uuid);
-    }
-
-    private function setIdentifier(ProductInterface $product, ?string $identifier = null): ProductInterface
-    {
-        $command = UpsertProductCommand::createWithUuid(
-            $this->admin->getId(),
-            ProductUuid::fromUuid($product->getUuid()),
-            [
-                new SetIdentifierValue('sku', $identifier),
-            ]
-        );
-        ($this->getUpsertProductHandler())($command);
-
-        return $this->getProductRepository()->find($product->getUuid());
-    }
-
-    private function getProductRepository(): ProductRepositoryInterface
-    {
-        return $this->get('pim_catalog.repository.product');
-    }
-
-    private function getUpsertProductHandler(): UpsertProductHandler
-    {
-        return $this->get('Akeneo\Pim\Enrichment\Product\Application\UpsertProductHandler');
-    }
-
-    private function getAuthenticator(): AuthenticatorHelper
-    {
-        return $this->get('akeneo_integration_tests.helper.authenticator');
     }
 
     private function getConnection(): Connection
