cb973f3ecdae27db9119762badc3a9d684f34d17
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2022-12-05 17:09:18 +0100

    CXP-1325: cancel refacto

--- components/catalogs/front/src/components/CatalogEdit/utils/findFirstError.ts
+++ components/catalogs/front/src/components/CatalogEdit/utils/findFirstError.ts
@@ -1,4 +1,4 @@
-import {CatalogFormErrors} from '../components/CatalogEdit/models/CatalogFormErrors';
+import {CatalogFormErrors} from '../models/CatalogFormErrors';
 
 export const findFirstError = (errors: CatalogFormErrors, path: string): string | undefined =>
     errors.filter(error => error.propertyPath.startsWith(path)).shift()?.message || undefined;
