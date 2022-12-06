f17c3e71e78a655c8ee393fb55e07c2c8bd500a2
Florian HEGRON <florian.hegron@akeneo.com>
2022-12-06 12:50:55 +0100

    CXP-1353: prettier

--- front-packages/akeneo-design-system/src/icons/ExternalLinkIcon.tsx
+++ front-packages/akeneo-design-system/src/icons/ExternalLinkIcon.tsx
@@ -10,13 +10,7 @@ const ExternalLinkIcon = ({title, size = 24, color = 'currentColor', ...props}: 
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-    <path
-      d="M21 12.1088V21H3V3H12.0679"
-      stroke={color}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      fill="none"
-    />
+    <path d="M21 12.1088V21H3V3H12.0679" stroke={color} strokeLinecap="round" strokeLinejoin="round" fill="none" />
   </svg>
 );
 
