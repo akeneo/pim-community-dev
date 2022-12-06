0adaf22f95d4b79068874095223861c4605531f0
Florian HEGRON <florian.hegron@akeneo.com>
2022-12-06 14:27:45 +0100

    CXP-1353: miss arrow fill attribute

--- front-packages/akeneo-design-system/src/icons/ExternalLinkIcon.tsx
+++ front-packages/akeneo-design-system/src/icons/ExternalLinkIcon.tsx
@@ -9,6 +9,7 @@ const ExternalLinkIcon = ({title, size = 24, color = 'currentColor', ...props}: 
       stroke={color}
       strokeLinecap="round"
       strokeLinejoin="round"
+      fill="none"
     />
     <path d="M21 12.1088V21H3V3H12.0679" stroke={color} strokeLinecap="round" strokeLinejoin="round" fill="none" />
   </svg>
