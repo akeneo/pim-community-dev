12c4e2df52824faf9796af6bd49f56aefb129c52
Celine Morin <celine.morin@akeneo.com>
2022-12-07 17:45:50 +0100

    CPM-818: Refactor migration to use a generator and have better performances

--- components/identifier-generator/back/src/Infrastructure/ZddMigrations/V20221205153905FillIdentifierPrefixesZddMigration.php
+++ components/identifier-generator/back/src/Infrastructure/ZddMigrations/V20221205153905FillIdentifierPrefixesZddMigration.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\ZddMigrations;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\API\Query\UpdateIdentifierPrefixesQuery;
+use Akeneo\Pim\Enrichment\Component\Product\Factory\WriteValueCollectionFactory;
+use Akeneo\Pim\Enrichment\Component\Product\Model\Product;
 use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductRepositoryInterface;
 use Akeneo\Platform\Bundle\InstallerBundle\Command\ZddMigration;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Types\Types;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 use Webmozart\Assert\Assert;
 
 /**
@@ -19,32 +20,24 @@ use Webmozart\Assert\Assert;
  */
 class V20221205153905FillIdentifierPrefixesZddMigration implements ZddMigration
 {
-    public const BULK_SIZE = 100;
+    /**
+     * @var string[]|null
+     */
+    private ?array $identifierAttributeCodes = null;
 
     public function __construct(
         private readonly Connection $connection,
         private readonly UpdateIdentifierPrefixesQuery $updateIdentifierPrefixesQuery,
-        private readonly ProductRepositoryInterface $productRepository,
+        private readonly WriteValueCollectionFactory $writeValueCollectionFactory,
         private readonly LoggerInterface $logger,
     ) {
     }
 
     public function migrate(): void
     {
-        $lastProductUuidAsBytes = '';
-        $productUuids = $this->getProductsByBatch($lastProductUuidAsBytes);
-
-        while (\count($productUuids) > 0) {
-            $this->logger->info(\sprintf('%s products found to fill prefixes', \count($productUuids)));
-
-            $productUuidsAsBytes = \array_map(fn (string $uuid): string => Uuid::fromString($uuid)->getBytes(), $productUuids);
-            $products = $this->productRepository->findBy(['uuid' => $productUuidsAsBytes]);
-            Assert::allIsInstanceOf($products, ProductInterface::class);
-
+        foreach ($this->getProductsByBatch() as $products) {
+            $this->logger->notice(\sprintf('%d products found to fill prefixes', \count($products)));
             $this->updateIdentifierPrefixesQuery->updateFromProducts($products);
-
-            $lastProductUuidAsBytes = Uuid::fromString(\end($productUuids))->getBytes();
-            $productUuids = $this->getProductsByBatch($lastProductUuidAsBytes);
         }
     }
 
@@ -54,26 +47,77 @@ class V20221205153905FillIdentifierPrefixesZddMigration implements ZddMigration
     }
 
     /**
-     * @return string[]
+     * @return iterable<ProductInterface[]>
      */
-    private function getProductsByBatch(string $lastProductUuid): array
+    private function getProductsByBatch(): iterable
     {
+        $lastProductUuid = '';
+
         $query = <<<SQL
-        SELECT DISTINCT BIN_TO_UUID(pcp.uuid) as uuid 
-        FROM pim_catalog_product as pcp
-            LEFT JOIN pim_catalog_identifier_generator_prefixes pcigf on pcp.uuid = pcigf.product_uuid
-        WHERE pcigf.product_uuid IS NULL
-        AND pcp.uuid > :lastUuid
+        SELECT BIN_TO_UUID(uuid) as uuid_string, raw_values
+        FROM pim_catalog_product product
+        WHERE NOT EXISTS(
+            SELECT * FROM pim_catalog_identifier_generator_prefixes WHERE product_uuid = product.uuid
+        )
+            AND uuid > :lastUuid
         ORDER BY uuid
-        LIMIT :limit
+        LIMIT 100
 SQL;
 
-        $result = $this->connection->executeQuery(
-            $query,
-            ['lastUuid' => $lastProductUuid, 'limit' => self::BULK_SIZE],
-            ['lastUuid' => \PDO::PARAM_STR, 'limit' => \PDO::PARAM_INT]
-        )->fetchFirstColumn();
+        while (true) {
+            $rows = $this->connection->fetchAllKeyValue(
+                $query,
+                ['lastUuid' => $lastProductUuid],
+                ['lastUuid' => Types::BINARY],
+            );
 
-        return \array_map('strval', $result);
+            if (empty($rows)) {
+                return;
+            }
+
+            $values = $this->writeValueCollectionFactory->createMultipleFromStorageFormat(
+                \array_map(
+                    static function (string $rawValues): array {
+                        $values = \json_decode($rawValues, true);
+                        Assert::isArray($values);
+
+                        return \array_filter(
+                            $values,
+                            fn (string $key): bool => \in_array($key, $this->getIdentifierAttributeCodes()),
+                            ARRAY_FILTER_USE_KEY
+                        );
+                    },
+                    $rows
+                )
+            );
+            $products = [];
+            foreach ($values as $uuid => $valueCollection) {
+                $product = new Product($uuid);
+                $product->setValues($valueCollection);
+                $products[] = $product;
+            }
+            $lastProduct = \end($products);
+            if ($lastProduct instanceof ProductInterface) {
+                $lastProductUuid = $lastProduct->getUuid()->getBytes();
+            }
+
+            yield $products;
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getIdentifierAttributeCodes(): array
+    {
+        if (null === $this->identifierAttributeCodes) {
+            $result = $this->connection->fetchFirstColumn(
+                "SELECT code FROM pim_catalog_attribute WHERE attribute_type = 'pim_catalog_identifier'"
+            );
+            Assert::allString($result);
+            $this->identifierAttributeCodes = $result;
+        }
+
+        return $this->identifierAttributeCodes;
     }
 }
