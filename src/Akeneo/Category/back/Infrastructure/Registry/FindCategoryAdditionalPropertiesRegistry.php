c8005683f805eaf13b99a767192f797f020ca853
amigaire <94356747+amigaire@users.noreply.github.com>
2022-12-08 10:07:22 +0100

    GRF-340 : Load permission for edited category (#18564)
    * GRF-340 : Load permission for edited category
    Co-authored-by: Antoine Trouve <antoine.trouve@akeneo.com>

---
+++ src/Akeneo/Category/back/Infrastructure/Registry/FindCategoryAdditionalPropertiesRegistry.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Akeneo\Category\Infrastructure\Registry;
+
+use Akeneo\Category\Application\Registry\FindCategoryAdditionalProperties;
+use Akeneo\Category\Domain\Model\Enrichment\Category;
+use Akeneo\Category\ServiceApi\Handler\CategoryAdditionalPropertiesFinder;
+
+class FindCategoryAdditionalPropertiesRegistry implements FindCategoryAdditionalProperties
+{
+    /**
+     * @param iterable<string, CategoryAdditionalPropertiesFinder> $additionalPropertiesFinder
+     */
+    public function __construct(
+        private iterable $additionalPropertiesFinder,
+    ) {
+    }
+
+    public function forCategory(Category $category): Category
+    {
+        foreach ($this->additionalPropertiesFinder as $finder) {
+            if ($finder->isSupportedAdditionalProperties() === true) {
+                $category = $finder->execute($category);
+            }
+        }
+
+        return $category;
+    }
+}
