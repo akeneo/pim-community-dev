4abeaf446763d672cbce88e99da3f2c7f74591d7
Bryan Stéphan <bryan.stephan@akeneo.com>
2022-12-08 11:12:34 +0100

    GRF-168: fix phpstan errors

--- src/Akeneo/Category/back/Infrastructure/Converter/InternalApi/InternalApiToStd.php
+++ src/Akeneo/Category/back/Infrastructure/Converter/InternalApi/InternalApiToStd.php
@@ -14,12 +14,14 @@ use Akeneo\Category\Infrastructure\Exception\ArrayConversionException;
  *
  * @phpstan-type InternalApi array{
  *     properties: PropertyApi,
- *     attributes: array<string, AttributeValueApi>
+ *     attributes: array<string, AttributeValueApi>,
+ *     permissions: array<string, array<int>>|null
  * }
  * @phpstan-type StandardInternalApi array{
  *     code: string,
- *     labels: array<string, string>
- *     values: array<string, array<AttributeValueApi>>
+ *     labels: array<string, string>,
+ *     values: array<string, array<AttributeValueApi>>,
+ *     permissions: array<string, array<int>>|null
  * }
  * @phpstan-type PropertyApi array{code: string, labels: array<string, string>}
  * @phpstan-type AttributeValueApi array{data: string, channel: string|null, locale: string|null, attribute_code: string}
