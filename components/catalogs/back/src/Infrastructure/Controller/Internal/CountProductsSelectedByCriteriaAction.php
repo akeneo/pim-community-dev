d000460137ef75a97d760ee39f8a30bbb517ff46
Riff <riff.ventura@gmail.com>
2022-12-13 11:06:18 +0100

    fake commit

--- components/catalogs/back/src/Infrastructure/Controller/Internal/CountProductsSelectedByCriteriaAction.php
+++ components/catalogs/back/src/Infrastructure/Controller/Internal/CountProductsSelectedByCriteriaAction.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
+
 /**
  * @author    Willy Mesnage <willy.mesnage@akeneo.com>
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
