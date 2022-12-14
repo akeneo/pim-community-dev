75da3327ac56bbeaf4201ca666841c3d47985d14
pierallard <pierallard@gmail.com>
2022-12-14 09:19:52 +0100

    CPM-860: Add validation for unknown family operators

---
+++ components/identifier-generator/back/tests/Specification/Infrastructure/Validation/FamilyShouldBeValidValidatorSpec.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Specification\Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation;
+
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\FamilyShouldBeValidValidator;
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\FamilyShouldBeValid;
+use Akeneo\Platform\Component\Webhook\Context;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContext;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class FamilyShouldBeValidValidatorSpec extends ObjectBehavior
+{
+    public function let(ExecutionContext $context): void
+    {
+        $this->initialize($context);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(FamilyShouldBeValidValidator::class);
+    }
+
+    public function it_can_only_validate_the_right_constraint(): void
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->during('validate', [['type' => 'enabled', 'operator' => 'IN', 'value' => ['shirts']], new NotBlank()]);
+    }
+
+    public function it_should_not_build_violation_when_family_constraint_is_valid(
+        ExecutionContext $context
+    ): void {
+        $context->buildViolation(Argument::any())->shouldNotBeCalled();
+        $this->validate(['type' => 'family', 'operator' => 'IN', 'value' => ['shirts']], new FamilyShouldBeValid());
+    }
+
+    public function it_should_not_validate_something_else_than_an_array(ExecutionContext $context): void
+    {
+        $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
+
+        $this->validate(new \stdClass(), new FamilyShouldBeValid());
+    }
+
+    public function it_should_not_validate_a_condition_which_is_not_a_family(ExecutionContext $context): void
+    {
+        $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
+
+        $this->validate(['type' => 'something_else', 'operator' => 'IN', 'value' => ['shirts']], new FamilyShouldBeValid());
+    }
+
+    public function it_should_build_violation_when_operator_is_unknown(
+        ExecutionContext $context,
+        ConstraintViolationBuilderInterface $violationBuilder
+    ): void
+    {
+        $context->buildViolation(Argument::type('string'))->shouldBeCalled()->willReturn($violationBuilder);
+        $violationBuilder->atPath('operator')->shouldBeCalled()->willReturn($violationBuilder);
+        $violationBuilder->setParameters([
+            '{{ value }}' => '"unknown"',
+            '{{ choices }}' => '"IN", "NOT IN", "EMPTY", "NOT EMPTY"',
+        ])->shouldBeCalled()->willReturn($violationBuilder);
+        $violationBuilder->addViolation()->shouldBeCalled();
+
+        $this->validate(['type' => 'family', 'operator' => 'unknown', 'value' => ['shirts']], new FamilyShouldBeValid());
+    }
+}
