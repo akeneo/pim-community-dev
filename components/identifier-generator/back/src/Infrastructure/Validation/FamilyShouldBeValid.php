e6fef9e638edfc50226015ccaf09bfda3399a67e
pierallard <pierallard@gmail.com>
2022-12-14 09:19:52 +0100

    CPM-860: Add validation for unknown family operators

---
+++ components/identifier-generator/back/src/Infrastructure/Validation/FamilyShouldBeValid.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class FamilyShouldBeValid extends Constraint
+{
+    public string $unknownOperator = 'Operator {{ value }} can only be one of the following: {{ choices }}';
+
+    /**
+     * @inerhitDoc
+     */
+    public function getTargets(): string
+    {
+        return self::PROPERTY_CONSTRAINT;
+    }
+}
---
+++ components/identifier-generator/back/src/Infrastructure/Validation/FamilyShouldBeValidValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class FamilyShouldBeValidValidator extends ConstraintValidator
+{
+    public function validate($condition, Constraint $constraint): void
+    {
+        Assert::isInstanceOf($constraint, FamilyShouldBeValid::class);
+
+        if (!\is_array($condition)) {
+            return;
+        }
+
+        if (!\array_key_exists('operator', $condition)) {
+            return;
+        }
+
+        if (!\in_array($condition['operator'], ['IN', 'NOT IN', 'EMPTY', 'NOT EMPTY'])) {
+            $this->context
+                ->buildViolation($constraint->unknownOperator)
+                ->atPath('operator')
+                ->setParameters([
+                    '{{ value }}' => \json_encode($condition['operator']),
+                    '{{ choices }}' => \implode(', ', \array_map(fn (string $value): string => \json_encode($value), ['IN', 'NOT IN', 'EMPTY', 'NOT EMPTY']))
+                ])
+                ->addViolation();
+        }
+    }
+}
