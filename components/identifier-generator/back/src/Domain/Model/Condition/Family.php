33287b7ce493f6f780d1925925a375defa3016d5
pierallard <pierallard@gmail.com>
2022-12-14 08:57:02 +0100

    CPM-860: Add Family condition

---
+++ components/identifier-generator/back/src/Domain/Model/Condition/Family.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Condition;
+
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\ProductProjection;
+
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Family implements ConditionInterface
+{
+    static function type(): string
+    {
+        return 'family';
+    }
+
+    static function fromNormalized(): self
+    {
+        return new self();
+    }
+
+    public function normalize(): array
+    {
+        // TODO: Implement normalize() method.
+    }
+
+    public function match(ProductProjection $productProjection): bool
+    {
+        // TODO: Implement match() method.
+    }
+}
