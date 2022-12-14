9139dc2a2989ab2b68dd189d775d02e09b07149c
pierallard <pierallard@gmail.com>
2022-12-14 11:54:16 +0100

    CPM-860: Add validation for family condition count

--- components/identifier-generator/back/tests/Specification/Infrastructure/Validation/ConditionsShouldNotContainMultipleConditionValidatorSpec.php
+++ components/identifier-generator/back/tests/Specification/Infrastructure/Validation/ConditionsShouldNotContainMultipleConditionValidatorSpec.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Specification\Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation;
 
-use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\ConditionsShouldNotContainMultipleEnabled;
-use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\ConditionsShouldNotContainMultipleEnabledValidator;
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\ConditionsShouldNotContainMultipleCondition;
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\ConditionsShouldNotContainMultipleConditionValidator;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Context\ExecutionContext;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ConditionsShouldNotContainMultipleEnabledValidatorSpec extends ObjectBehavior
+class ConditionsShouldNotContainMultipleConditionValidatorSpec extends ObjectBehavior
 {
     public function let(ExecutionContext $context): void
     {
@@ -24,7 +24,7 @@ class ConditionsShouldNotContainMultipleEnabledValidatorSpec extends ObjectBehav
 
     public function it_is_initializable(): void
     {
-        $this->shouldHaveType(ConditionsShouldNotContainMultipleEnabledValidator::class);
+        $this->shouldHaveType(ConditionsShouldNotContainMultipleConditionValidator::class);
     }
 
     public function it_can_only_validate_the_right_constraint(): void
@@ -37,21 +37,21 @@ class ConditionsShouldNotContainMultipleEnabledValidatorSpec extends ObjectBehav
     {
         $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
 
-        $this->validate(new \stdClass(), new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate(new \stdClass(), new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 
     public function it_should_not_validate_something_else_than_an_array_of_array(ExecutionContext $context): void
     {
         $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
 
-        $this->validate([new \stdClass()], new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate([new \stdClass()], new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 
     public function it_should_not_validate_something_else_than_an_array_of_property(ExecutionContext $context): void
     {
         $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
 
-        $this->validate([[]], new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate([[]], new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 
     public function it_should_not_validate_conditions_without_enabled(ExecutionContext $context): void
@@ -62,7 +62,7 @@ class ConditionsShouldNotContainMultipleEnabledValidatorSpec extends ObjectBehav
             ['type' => 'free_text', 'string' => 'abcdef'],
             ['type' => 'free_text', 'string' => 'ghijkl'],
         ];
-        $this->validate($conditions, new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate($conditions, new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 
     public function it_should_build_violation_when_conditions_contains_multiple_enabled(ExecutionContext $context): void
@@ -73,23 +73,24 @@ class ConditionsShouldNotContainMultipleEnabledValidatorSpec extends ObjectBehav
         ];
 
         $context
-            ->buildViolation('validation.identifier_generator.conditions_enabled_limit_reached', [
+            ->buildViolation('validation.identifier_generator.conditions_limit_reached', [
                 '{{limit}}' => 1,
+                '{{type}}' => 'enabled',
             ])
             ->shouldBeCalled();
 
-        $this->validate($conditions, new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate($conditions, new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 
     public function it_should_be_valid_when_enabled_is_under_limit(ExecutionContext $context): void
     {
         $conditions = [
-            ['type' => 'free_text', 'string' => 'abcdef'],
+            ['type' => 'family', 'IN' => ['shirts']],
             ['type' => 'enabled', 'numberMin' => 3, 'digitsMin' => 2],
         ];
 
         $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
 
-        $this->validate($conditions, new ConditionsShouldNotContainMultipleEnabled());
+        $this->validate($conditions, new ConditionsShouldNotContainMultipleCondition(['enabled', 'family']));
     }
 }
