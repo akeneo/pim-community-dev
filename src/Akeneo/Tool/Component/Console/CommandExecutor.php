cac8f3a6e87b549c3b23215f273ded7beef2e57f
jmleroux <jmleroux.pro@gmail.com>
2022-12-14 11:46:51 +0100

    PH-430: Use Application::find() to run the command in CommandExecutor

--- src/Akeneo/Tool/Component/Console/CommandExecutor.php
+++ src/Akeneo/Tool/Component/Console/CommandExecutor.php
@@ -5,45 +5,39 @@ namespace Akeneo\Tool\Component\Console;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Command executor
  *
  * @author    Romain Monceau <romain@akeneo.com>
+ * @author    JM Leroux <jean-marie.leroux@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class CommandExecutor
 {
     public function __construct(
-        protected InputInterface $input,
-        protected OutputInterface $output,
-        protected Application $application
+        protected readonly InputInterface $input,
+        protected readonly OutputInterface $output,
+        protected readonly Application $application
     ) {
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function runCommand($command, $params = []): static
+    public function runCommand(string $commandName, array $params = []): static
     {
         $params = array_merge(
-            ['command' => $command],
             $params,
             $this->getDefaultParams()
         );
 
-        $this->application->setAutoExit(false);
-        $exitCode = $this->application->run(new ArrayInput($params), $this->output);
+        $command = $this->application->find($commandName);
 
-        if (0 !== $exitCode) {
-            $this->application->setAutoExit(true);
-            $errorMessage = sprintf('The command terminated with an error code: %u.', $exitCode);
-            $this->output->writeln("<error>$errorMessage</error>");
-            $e = new \Exception($errorMessage, $exitCode);
-            throw $e;
-        }
+        $command->run(
+            new ArrayInput($params),
+            $this->input->hasOption('quiet') ? new NullOutput() : $this->output
+        );
 
         return $this;
     }
