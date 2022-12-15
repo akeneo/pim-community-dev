68158b8314d67f1eb3151958c8de0455a2ad6f9c
Florian HEGRON <florian.hegron@akeneo.com>
2022-12-15 17:20:47 +0100

    CXP-1355 : Disable Catalogs when Locales are removed

--- components/catalogs/back/tests/Integration/Infrastructure/Job/DisableCatalogsOnLocaleDeactivationTaskletTest.php
+++ components/catalogs/back/tests/Integration/Infrastructure/Job/DisableCatalogsOnLocaleDeactivationTaskletTest.php
@@ -34,11 +34,18 @@ class DisableCatalogsOnLocaleDeactivationTaskletTest extends IntegrationTestCase
             ownerUsername: 'magenta',
             catalogProductValueFilters: ['locales' => ['en_US']]
         );
+        $this->createCatalog(
+            id: 'af80cdbe-3e5b-4f47-acef-8878bee72dfb',
+            name: 'Store BE',
+            ownerUsername: 'magenta',
+            catalogProductMapping: ['meta_title' => ['scope' => null, 'locale' => 'fr_FR', 'source' => 'meta_title']]
+        );
 
         $this->disableLocale('fr_FR');
         $this->waitForQueuedJobs();
 
         $this->assertCatalogIsDisabled('975fef57-aa2c-42ad-a0ba-003af694ce11');
+        $this->assertCatalogIsDisabled('af80cdbe-3e5b-4f47-acef-8878bee72dfb');
         $this->assertCatalogIsEnabled('29655723-2b78-4ae0-9ac2-2d00799db8df');
     }
 
