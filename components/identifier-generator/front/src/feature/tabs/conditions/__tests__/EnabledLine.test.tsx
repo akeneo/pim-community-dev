af443217fb621340a0df7816062b9f9f39331623
Benjamin Letellier <letellier.benjamin@gmail.com>
2022-12-15 17:09:16 +0100

    CPM-856: Remove a condition + tests

--- components/identifier-generator/front/src/feature/tabs/conditions/__tests__/EnabledLine.test.tsx
+++ components/identifier-generator/front/src/feature/tabs/conditions/__tests__/EnabledLine.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {fireEvent, render, screen} from '../../../tests/test-utils';
+import {fireEvent, render, screen, waitFor} from '../../../tests/test-utils';
 import {EnabledLine} from '../EnabledLine';
 import {CONDITION_NAMES} from '../../../models';
 
@@ -8,7 +8,7 @@ describe('EnabledLine', () => {
     render(
       <table>
         <tbody>
-          <EnabledLine condition={{type: CONDITION_NAMES.ENABLED, value: true, id: 'enabledId'}} onChange={jest.fn()} />
+          <EnabledLine condition={{type: CONDITION_NAMES.ENABLED, value: true, id: 'enabledId'}} onChange={jest.fn()} onDelete={jest.fn()} />
         </tbody>
       </table>
     );
@@ -23,13 +23,31 @@ describe('EnabledLine', () => {
     render(
       <table>
         <tbody>
-          <EnabledLine condition={{type: CONDITION_NAMES.ENABLED, value: false, id: 'enabledId'}} onChange={onChange} />
+          <EnabledLine condition={{type: CONDITION_NAMES.ENABLED, value: false, id: 'enabledId'}} onChange={onChange} onDelete={jest.fn()} />
         </tbody>
       </table>
     );
 
-    fireEvent.click(screen.getByRole('button'));
+    const buttons = screen.getAllByRole('button');
+    const openButton = buttons.find(button => button.title === 'pim_common.open') as HTMLElement;
+    fireEvent.click(openButton);
     fireEvent.click(screen.getByTitle('pim_common.enabled'));
     expect(onChange).toBeCalledWith({type: CONDITION_NAMES.ENABLED, value: true, id: 'enabledId'});
+  });
+
+  it('should callback on delete', () => {
+    const onDelete = jest.fn();
+    render(
+      <table>
+        <tbody>
+        <EnabledLine condition={{type: CONDITION_NAMES.ENABLED, value: false, id: 'enabledId'}} onChange={jest.fn()} onDelete={onDelete} />
+        </tbody>
+      </table>
+    );
+
+    const deleteButton = screen.getByText('pim_common.delete');
+    expect(deleteButton).toBeInTheDocument();
+    fireEvent.click(deleteButton);
+    expect(onDelete).toBeCalled();
   });
 });
