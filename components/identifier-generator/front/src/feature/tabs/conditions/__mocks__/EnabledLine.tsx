d84d585da54b767402056f39e2f9dfdf486b5c4d
Benjamin Letellier <letellier.benjamin@gmail.com>
2022-12-15 17:09:16 +0100

    CPM-856: Remove a condition + tests

--- components/identifier-generator/front/src/feature/tabs/conditions/__mocks__/EnabledLine.tsx
+++ components/identifier-generator/front/src/feature/tabs/conditions/__mocks__/EnabledLine.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import {Condition, Enabled} from '../../../models';
+import {Button} from "akeneo-design-system";
 
 type EnabledLineProps = {
   condition: Enabled & {id: string};
   onChange: (condition: Condition & {id: string}) => void;
+  onDelete: (conditionId: string) => void;
 };
 
-const EnabledLine: React.FC<EnabledLineProps> = ({condition, onChange}) => {
+const EnabledLine: React.FC<EnabledLineProps> = ({condition, onChange, onDelete}) => {
   const handleChange = () => {
     onChange({...condition, value: !condition.value});
+  };
+
+  const handleDelete = (conditionId: string) => () => {
+    onDelete(conditionId);
   };
 
   return (
     <>
       EnabledLineMock
       <button onClick={handleChange}>Update value</button>
+      <Button onClick={handleDelete(condition.id)}>Delete Enabled</Button>
     </>
   );
 };
