5058b87bd806e6248557f8cb3937a030839efec9
pierallard <pierallard@gmail.com>
2022-12-15 14:59:21 +0100

    CPM-861: Update match method for family

--- components/identifier-generator/back/src/Domain/Model/Condition/Family.php
+++ components/identifier-generator/back/src/Domain/Model/Condition/Family.php
@@ -5,15 +5,27 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Condition;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\ProductProjection;
+use Webmozart\Assert\Assert;
 
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
- * @phpstan-type FamilyNormalized array{type: string, operator: string, value?: string[]}
+ * @phpstan-type FamilyOperator 'IN'|'NOT IN'|'EMPTY'|'NOT EMPTY'
+ * @phpstan-type FamilyNormalized array{type: string, operator: FamilyOperator, value?: string[]}
  */
 final class Family implements ConditionInterface
 {
+    /**
+     * @param FamilyOperator $operator
+     * @param array|null $value
+     */
+    private function __construct(
+        private readonly string $operator,
+        private readonly ?array $value = null,
+    ) {
+    }
+
     public static function type(): string
     {
         return 'family';
@@ -24,8 +36,20 @@ final class Family implements ConditionInterface
      */
     public static function fromNormalized(array $normalizedProperty): self
     {
-        // TODO: CPM-861
-        return new self();
+        Assert::eq($normalizedProperty['type'], self::type());
+        Assert::keyExists($normalizedProperty, 'operator');
+        Assert::string($normalizedProperty['operator']);
+        if (\in_array($normalizedProperty['operator'], ['IN', 'NOT IN'])) {
+            Assert::keyExists($normalizedProperty, 'value');
+            Assert::allStringNotEmpty($normalizedProperty['value']);
+            Assert::minCount($normalizedProperty['value'], 1);
+
+            return new self($normalizedProperty['operator'], $normalizedProperty['value']);
+        }
+
+        Assert::keyNotExists($normalizedProperty, 'value');
+
+        return new self($normalizedProperty['operator']);
     }
 
     /**
@@ -33,13 +57,25 @@ final class Family implements ConditionInterface
      */
     public function normalize(): array
     {
-        // TODO: CPM-861
-        throw new \Exception('not implemented');
+        $result = [
+            'type' => self::type(),
+            'operator' => $this->operator,
+        ];
+        if (null !== $this->value) {
+            $result['value'] = $this->value;
+        }
+
+        return $result;
     }
 
     public function match(ProductProjection $productProjection): bool
     {
-        // TODO: CPM-861
-        throw new \Exception('not implemented');
+        return match ($this->operator) {
+            'IN' => \in_array($productProjection->familyCode(), $this->value),
+            'NOT IN' => null !== $productProjection->familyCode() &&
+                !\in_array($productProjection->familyCode(), $this->value),
+            'EMPTY' => null === $productProjection->familyCode(),
+            default => null !== $productProjection->familyCode(),
+        };
     }
 }
