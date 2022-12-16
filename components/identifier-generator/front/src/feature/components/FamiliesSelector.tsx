cad4e3d6a4dc27e120be132f5190222e2c569551
pierallard <pierallard@gmail.com>
2022-12-16 15:12:16 +0100

    CPM-859: Manage errors

--- components/identifier-generator/front/src/feature/components/FamiliesSelector.tsx
+++ components/identifier-generator/front/src/feature/components/FamiliesSelector.tsx
@@ -1,8 +1,9 @@
 import React, {FC} from 'react';
-import {MultiSelectInput} from 'akeneo-design-system';
+import {Helper, MultiSelectInput} from 'akeneo-design-system';
 import {useGetFamilies, usePaginatedFamilies} from '../hooks/useFamilies';
 import {getLabel, useTranslate, useUserContext} from '@akeneo-pim-community/shared';
 import {Family, FamilyCode} from '../models';
+import {Unauthorized} from '../errors';
 
 type FamiliesSelectorProps = {
   familyCodes: FamilyCode[],
@@ -14,7 +15,7 @@ const FamiliesSelector: FC<FamiliesSelectorProps> = ({familyCodes, onChange}) =>
   const userContext = useUserContext();
   const catalogLocale = userContext.get('catalogLocale');
   const {families, handleNextPage, handleSearchChange} = usePaginatedFamilies();
-  const {data: selectedFamilies, isLoading} = useGetFamilies({codes: familyCodes});
+  const {data: selectedFamilies, isLoading, error} = useGetFamilies({codes: familyCodes});
 
   // Avoid blinking of values when selecting a new one
   const [debouncedSelectedFamilies, setDebouncedSelectedFamilies] = React.useState<Family[]>([]);
@@ -26,16 +27,23 @@ const FamiliesSelector: FC<FamiliesSelectorProps> = ({familyCodes, onChange}) =>
 
   const [debouncedInvalidFamilyCodes, setDebouncedInvalidFamilyCodes] = React.useState<FamilyCode[]>([]);
   React.useEffect(() => {
-    if (!isLoading) {
+    if (!isLoading && !error) {
       setDebouncedInvalidFamilyCodes(familyCodes
         .filter(code => !(selectedFamilies as Family[]).map(f => f.code).includes(code)));
     }
-  }, [selectedFamilies, isLoading]);
+  }, [selectedFamilies, isLoading, error]);
 
   const getFamiliesList = [
     ...(families || []),
     ...(debouncedSelectedFamilies || []).filter(family => !(families || []).map(f => f.code).includes(family.code))
   ];
+
+  if (error) {
+    if (error instanceof Unauthorized) {
+      return <Helper level={'error'}>{translate('pim_error.unauthorized_list_families')}</Helper>;
+    }
+    return <Helper level={'error'}>{translate('pim_error.general')}</Helper>;
+  }
 
   return <MultiSelectInput
     emptyResultLabel={translate('pim_common.no_result')}
