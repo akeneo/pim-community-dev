ef1dc6543d9a41298ebc4f3876af79502e164675
pierallard <pierallard@gmail.com>
2022-12-16 11:50:06 +0100

    CPM-859: Add invalid values

--- front-packages/akeneo-design-system/src/components/Input/MultiSelectInput/ChipInput.tsx
+++ front-packages/akeneo-design-system/src/components/Input/MultiSelectInput/ChipInput.tsx
@@ -24,17 +24,17 @@ const Container = styled.ul<AkeneoThemedProps & {invalid: boolean}>`
   }
 `;
 
-const Chip = styled.li<AkeneoThemedProps & {isSelected: boolean; readOnly: boolean}>`
+const Chip = styled.li<AkeneoThemedProps & {isSelected: boolean; readOnly: boolean; isErrored: boolean}>`
   list-style-type: none;
   padding: 3px 15px;
   padding-left: ${({readOnly}) => (readOnly ? '15px' : '4px')};
-  border: 1px ${getColor('grey', 80)} solid;
-  background-color: ${({isSelected}) => (isSelected ? getColor('grey', 40) : getColor('grey', 20))};
+  border: 1px ${({isErrored}) => isErrored ? getColor('red', 80) : getColor('grey', 80)} solid;
+  background-color: ${({isSelected, isErrored}) => (isErrored ? getColor('red', 20) : isSelected ? getColor('grey', 40) : getColor('grey', 20))};
   display: flex;
   align-items: center;
   height: 30px;
   box-sizing: border-box;
-  color: ${({readOnly}) => (readOnly ? getColor('grey', 100) : getColor('grey', 140))};
+  color: ${({readOnly, isErrored}) => (isErrored ? getColor('red', 100) : readOnly ? getColor('grey', 100) : getColor('grey', 140))};
 `;
 
 const Input = styled.input`
@@ -74,11 +74,11 @@ const ReadOnlyIcon = styled(LockIcon)`
   color: ${getColor('grey', 100)};
 `;
 
-const RemoveButton = styled(IconButton)`
+const RemoveButton = styled(IconButton)<AkeneoThemedProps & {isErrored: boolean}>`
   background-color: transparent;
   margin-left: -3px;
   margin-right: 1px;
-  color: ${getColor('grey', 100)};
+  color: ${({isErrored}) => (isErrored ? getColor('red', 100) : getColor('grey', 100))};
 `;
 
 type ChipValue = {
@@ -89,6 +89,7 @@ type ChipValue = {
 type ChipInputProps = {
   id?: string;
   value: ChipValue[];
+  invalidValue: string[];
   invalid?: boolean;
   placeholder?: string;
   readOnly?: boolean;
@@ -104,6 +105,7 @@ const ChipInput = React.forwardRef<HTMLInputElement, ChipInputProps>(
     {
       id,
       value,
+      invalidValue,
       invalid,
       readOnly,
       placeholder,
@@ -140,15 +142,16 @@ const ChipInput = React.forwardRef<HTMLInputElement, ChipInputProps>(
     return (
       <Container invalid={invalid} readOnly={readOnly}>
         {value.map((chip, index) => (
-          <Chip key={chip.code} readOnly={readOnly} isSelected={index === value.length - 1 && isLastSelected}>
+          <Chip key={chip.code} readOnly={readOnly} isErrored={invalidValue.includes(chip.code)} isSelected={index === value.length - 1 && isLastSelected}>
             {!readOnly && (
               <RemoveButton
                 title={removeLabel}
                 ghost="borderless"
                 size="small"
                 level="tertiary"
-                icon={<CloseIcon />}
+                icon={<CloseIcon color={invalidValue.includes(chip.code) ? 'red' : 'grey'} />}
                 onClick={() => onRemove(chip.code)}
+                isErrored={invalidValue.includes(chip.code)}
               />
             )}
             {chip.label}
