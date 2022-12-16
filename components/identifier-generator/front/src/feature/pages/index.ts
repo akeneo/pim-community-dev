af4a665e4bf3ae4748029488fb902008326f9479
Benjamin Letellier <letellier.benjamin@gmail.com>
2022-12-16 15:28:29 +0100

    CPM-856: Rename DeletePropertyModal to SimpleDeleteModal

--- components/identifier-generator/front/src/feature/pages/index.ts
+++ components/identifier-generator/front/src/feature/pages/index.ts
@@ -2,5 +2,5 @@ export * from './CreateGeneratorModal';
 export * from './CreateOrEditGeneratorPage';
 export * from './CreateGeneratorPage';
 export * from './DeleteGeneratorModal';
-export * from './DeletePropertyModal';
+export * from './SimpleDeleteModal';
 export * from './EditGeneratorPage';
