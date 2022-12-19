c5e76803e724803bd48865cd335ff91aedf1bc2d
pierallard <pierallard@gmail.com>
2022-12-19 18:00:36 +0100

    CPM-862: Move operatorselector

--- components/identifier-generator/front/src/feature/components/index.ts
+++ components/identifier-generator/front/src/feature/components/index.ts
@@ -4,5 +4,6 @@ export * from './Header';
 export * from './IdentifierAttributeSelector';
 export * from './LabelTranslations';
 export * from './ListSkeleton';
+export * from './OperatorSelector';
 export * from './TranslationWithLink';
 export * from './LabelTranslationsSkeleton';
