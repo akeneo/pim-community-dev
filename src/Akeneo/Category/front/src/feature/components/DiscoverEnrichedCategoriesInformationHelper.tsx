88fa31a7c1e151cbeeac8238672be9d2e51390b1
Olivier Pontier <olivier.pontier@akeneo.com>
2022-12-26 18:13:21 +0100

    Display the component when the Enriched categories feature flga is enabled

--- src/Akeneo/Category/front/src/feature/components/DiscoverEnrichedCategoriesInformationHelper.tsx
+++ src/Akeneo/Category/front/src/feature/components/DiscoverEnrichedCategoriesInformationHelper.tsx
@@ -1,7 +1,12 @@
 import React, {FC} from 'react';
+import {useFeatureFlags} from '@akeneo-pim-community/shared';
 
 const DiscoverEnrichedCategoriesInformationHelper: FC = () => {
-  return <>Discover Enriched Categories</>;
+  const featureFlags = useFeatureFlags();
+  if (!featureFlags.isEnabled('enriched_category')) {
+    return <></>;
+  }
+  return <div data-testid="discover-enriched-categories-information-helper">Discover Enriched Categories</div>;
 };
 
 export {DiscoverEnrichedCategoriesInformationHelper};
