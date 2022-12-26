fa0e0b1a1bbc3ea60929f621ddba7648aceb4437
Olivier Pontier <olivier.pontier@akeneo.com>
2022-12-26 16:45:20 +0100

    initiate DiscoverEnrichedCategories commponent

---
+++ src/Akeneo/Category/front/src/feature/components/DiscoverEnrichedCategoriesInformationHelper.tsx
@@ -0,0 +1,7 @@
+import React, {FC} from 'react';
+
+const DiscoverEnrichedCategoriesInformationHelper: FC = () => {
+  return <>Discover Enriched Categories</>;
+};
+
+export {DiscoverEnrichedCategoriesInformationHelper};
