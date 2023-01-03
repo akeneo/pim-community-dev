cb6b409fc6ac487db7446740aa9dd58dfe51317e
pierallard <pierallard@gmail.com>
2023-01-03 17:04:43 +0100

    CPM-888: Add final

--- src/Akeneo/Pim/Structure/Bundle/Query/InternalApi/AttributeOption/FindAttributeOptions.php
+++ src/Akeneo/Pim/Structure/Bundle/Query/InternalApi/AttributeOption/FindAttributeOptions.php
@@ -3,8 +3,8 @@
 namespace Akeneo\Pim\Structure\Bundle\Query\InternalApi\AttributeOption;
 
 /**
- * @author    Pierre Allard <pierre.allard@akeneo.com>
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 interface FindAttributeOptions
 {
