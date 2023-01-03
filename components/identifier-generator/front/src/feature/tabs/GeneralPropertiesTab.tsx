348365abc4476616b276680504e5dd394a85df6f
Benjamin Letellier <letellier.benjamin@gmail.com>
2023-01-03 16:48:54 +0100

    CPM-901: create new component for error display

--- components/identifier-generator/front/src/feature/tabs/GeneralPropertiesTab.tsx
+++ components/identifier-generator/front/src/feature/tabs/GeneralPropertiesTab.tsx
@@ -1,7 +1,7 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback} from 'react';
 import {IdentifierGenerator, LabelCollection} from '../models';
 import {Field, SectionTitle, TextInput} from 'akeneo-design-system';
-import {IdentifierAttributeSelector, LabelTranslations} from '../components';
+import {IdentifierAttributeSelector, LabelTranslations, TabValidationErrors} from '../components';
 import {Styled} from '../components/Styled';
 import {useTranslate} from '@akeneo-pim-community/shared';
 import {Violation} from '../validators';
@@ -20,27 +20,14 @@ const GeneralPropertiesTab: React.FC<GeneralPropertiesProps> = ({generator, onGe
     [onGeneratorChange, generator]
   );
 
-  const displayedErrors = useMemo(
-    () => validationErrors?.map(({message}) => message).filter((value, index, self) => self.indexOf(value) === index),
-    [validationErrors]
-  );
-
   return (
     <>
-      {displayedErrors?.length > 0 && (
-        <Styled.MainErrorHelper level="error">
-          <Styled.ErrorList>
-            {displayedErrors.map(message => (
-              <li key={message}>{message}</li>
-            ))}
-          </Styled.ErrorList>
-        </Styled.MainErrorHelper>
-      )}
+      <TabValidationErrors errors={validationErrors} />
       <SectionTitle>
         <SectionTitle.Title>{translate('pim_identifier_generator.general.title')}</SectionTitle.Title>
       </SectionTitle>
       <Styled.FormContainer>
-        <Field label={'pim_common.code'}>
+        <Field label={translate('pim_common.code')}>
           <TextInput value={generator.code} readOnly={true} />
         </Field>
         <IdentifierAttributeSelector code={generator.target || ''} />
