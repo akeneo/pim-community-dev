30a7b9c9d9ad91835ed0ee49ec614ce1eabc2b94
Celine Morin <celine.morin@akeneo.com>
2023-01-05 15:41:34 +0100

    CPM-889: Remove useless type for now

--- components/identifier-generator/front/src/feature/models/conditions/simpleSelectOptionCondition.ts
+++
@@ -1,9 +0,0 @@
-import {CONDITION_NAMES} from './conditions';
-import {OptionCode} from '../option';
-
-type SimpleSelectOptionCondition = {
-  type: CONDITION_NAMES.SELECT_OPTION;
-  value: OptionCode[] | null;
-};
-
-export type {SimpleSelectOptionCondition};
