eb898fda009fa4dedae7569edfd0fdc7f8183de6
pierallard <pierallard@gmail.com>
2023-01-05 16:30:27 +0100

    CPM-892: Add validation for attribute type

--- components/identifier-generator/back/tests/Specification/Infrastructure/Validation/AttributeShouldHaveTypeValidatorSpec.php
+++ components/identifier-generator/back/tests/Specification/Infrastructure/Validation/AttributeShouldHaveTypeValidatorSpec.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Specification\Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation;
 
-use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\TargetAttributeShouldBeAnIdentifier;
-use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\TargetAttributeShouldBeAnIdentifierValidator;
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\AttributeShouldHaveType;
+use Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Validation\AttributeShouldHaveTypeValidator;
 use Akeneo\Pim\Structure\Component\AttributeTypes;
 use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\Attribute;
 use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\GetAttributes;
@@ -18,7 +18,7 @@ use Symfony\Component\Validator\Context\ExecutionContext;
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class TargetAttributeShouldBeAnIdentifierValidatorSpec extends ObjectBehavior
+class AttributeShouldHaveTypeValidatorSpec extends ObjectBehavior
 {
     public function let(GetAttributes $getAttributes, ExecutionContext $context): void
     {
@@ -28,7 +28,7 @@ class TargetAttributeShouldBeAnIdentifierValidatorSpec extends ObjectBehavior
 
     public function it_is_initializable(GetAttributes $getAttributes): void
     {
-        $this->shouldHaveType(TargetAttributeShouldBeAnIdentifierValidator::class);
+        $this->shouldHaveType(AttributeShouldHaveTypeValidator::class);
     }
 
     public function it_can_only_validate_the_right_constraint(): void
@@ -36,7 +36,7 @@ class TargetAttributeShouldBeAnIdentifierValidatorSpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->during('validate', ['code', new NotBlank()]);
     }
 
-    public function it_should_build_violation_when_target_attribute_is_not_an_identifier(
+    public function it_should_build_violation_when_attribute_should_have_type(
         GetAttributes $getAttributes,
         ExecutionContext $context
     ): void {
@@ -57,11 +57,11 @@ class TargetAttributeShouldBeAnIdentifierValidatorSpec extends ObjectBehavior
             ));
 
         $context->buildViolation(
-            'validation.identifier_generator.target_attribute_is_not_an_identifier',
+            'validation.identifier_generator.attribute_should_have_type',
             ['{{code}}' => 'sku', '{{type}}' => 'pim_catalog_text']
         )->shouldBeCalled();
 
-        $this->validate('sku', new TargetAttributeShouldBeAnIdentifier());
+        $this->validate('sku', new AttributeShouldHaveType());
     }
 
     public function it_should_be_valid_when_target_attribute_is_an_identifier(
@@ -86,6 +86,6 @@ class TargetAttributeShouldBeAnIdentifierValidatorSpec extends ObjectBehavior
 
         $context->buildViolation((string)Argument::any())->shouldNotBeCalled();
 
-        $this->validate('sku', new TargetAttributeShouldBeAnIdentifier());
+        $this->validate('sku', new AttributeShouldHaveType());
     }
 }
