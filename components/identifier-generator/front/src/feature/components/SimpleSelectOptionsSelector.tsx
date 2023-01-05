80c5b40b40808ae6b5b239e1eb6b28821c5c0d95
Celine Morin <celine.morin@akeneo.com>
2023-01-05 15:36:43 +0100

    CPM-889: Add SimpleSelectIotuibsSelector and its hook

---
+++ components/identifier-generator/front/src/feature/components/SimpleSelectOptionsSelector.tsx
@@ -0,0 +1,75 @@
+import React, {FC, useEffect, useMemo, useState} from 'react';
+import {Helper, MultiSelectInput} from 'akeneo-design-system';
+import {getLabel, useTranslate, useUserContext} from '@akeneo-pim-community/shared';
+import {Unauthorized} from '../errors';
+import {OptionCode} from '../models/option';
+import {useGetSelectOptions, usePaginatedOptions} from '../hooks/useGetSelectOptions';
+
+type FamiliesSelectorProps = {
+  attributeCode: string;
+  optionCodes: OptionCode[];
+  onChange: (optionCodes: OptionCode[]) => void;
+};
+
+const SimpleSelectOptionsSelector: FC<FamiliesSelectorProps> = ({attributeCode, optionCodes, onChange}) => {
+  const translate = useTranslate();
+  const userContext = useUserContext();
+  const catalogLocale = userContext.get('catalogLocale');
+  const [invalidOptionCodes, setInvalidOptionCodes] = useState<OptionCode[]>([]);
+
+  const {
+    data: selectedOptions,
+    error: errorDuringGet,
+    isLoading: isGetLoading
+  } = useGetSelectOptions({attributeCode, codes: optionCodes});
+  const {
+    options: paginatedOptions,
+    handleNextPage,
+    handleSearchChange,
+    error: errorDuringPagination,
+    isLoading: isPaginationLoading
+  } = usePaginatedOptions(attributeCode);
+
+  useEffect(() => {
+    if (!isGetLoading && !isPaginationLoading && !errorDuringGet && !errorDuringPagination) {
+      const result = optionCodes?.filter(code => !selectedOptions?.map(({code}) => code).includes(code));
+      setInvalidOptionCodes(result);
+    }
+  }, [errorDuringGet, errorDuringPagination, isGetLoading, isPaginationLoading, optionCodes, selectedOptions]);
+
+  const optionsList = useMemo(() => {
+    const missingSelectedOptions = selectedOptions?.filter(
+      ({code: selectedCode}) => !paginatedOptions?.some(({code}) => selectedCode  === code)
+    ) || [];
+    return (paginatedOptions || []).concat(missingSelectedOptions);
+  }, [paginatedOptions, selectedOptions]);
+
+  if (errorDuringGet || errorDuringPagination) {
+    if (errorDuringGet instanceof Unauthorized || errorDuringPagination instanceof Unauthorized) {
+      return <Helper level={'error'}>{translate('pim_error.unauthorized_list_families')}</Helper>;
+    }
+    return <Helper level={'error'}>{translate('pim_error.general')}</Helper>;
+  }
+
+  return (
+    <MultiSelectInput
+      emptyResultLabel={translate('pim_common.no_result')}
+      placeholder={translate('pim_identifier_generator.selection.settings.family.placeholder')}
+      removeLabel={translate('pim_common.remove')}
+      openLabel={translate('pim_common.open')}
+      onNextPage={handleNextPage}
+      onSearchChange={handleSearchChange}
+      onChange={onChange}
+      value={optionCodes}
+      invalidValue={invalidOptionCodes}
+    >
+      {optionsList?.map(option => (
+        <MultiSelectInput.Option value={option.code} key={option.code}>
+          {getLabel(option.labels, catalogLocale, option.code)}
+        </MultiSelectInput.Option>
+      ))}
+    </MultiSelectInput>
+  );
+};
+
+export {SimpleSelectOptionsSelector};
