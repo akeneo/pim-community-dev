114786a9fc0745746d18f45dd9ddaee6052ad263
Bryan Stéphan <bryan.stephan@akeneo.com>
2023-01-09 10:21:47 +0100

    GRF-667: fix copyrights

--- src/Akeneo/Category/back/Infrastructure/EventSubscriber/RemoveCategoryTreeTemplateSubscriber.php
+++ src/Akeneo/Category/back/Infrastructure/EventSubscriber/RemoveCategoryTreeTemplateSubscriber.php
@@ -14,10 +14,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
- * Storage event subscriber that update channel locales
- *
- * @author    Clement Gautier <clement.gautier@akeneo.com>
- * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class RemoveCategoryTreeTemplateSubscriber implements EventSubscriberInterface
--- src/Akeneo/Category/back/tests/Specification/Infrastructure/EventSubscriber/RemoveCategoryTreeTemplateSubscriberSpec.php
+++ src/Akeneo/Category/back/tests/Specification/Infrastructure/EventSubscriber/RemoveCategoryTreeTemplateSubscriberSpec.php
@@ -17,6 +17,10 @@ use Prophecy\Argument;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
+/**
+ * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class RemoveCategoryTreeTemplateSubscriberSpec extends ObjectBehavior
 {
     function let(
