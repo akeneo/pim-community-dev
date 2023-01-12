697b576b1e1f2ddb8b3f4342f411a3b4308580d1
hogren <hogren@iiiha.com>
2023-01-12 16:32:03 +0100

    Update copyright year components/catalogs/back/src/Infrastructure/Validation/ProductMapping/AttributeSource/AttributeIdentifierSource.php
    Co-authored-by: guillaumefer-akeneo <93598018+guillaumefer-akeneo@users.noreply.github.com>

--- components/catalogs/back/src/Infrastructure/Validation/ProductMapping/AttributeSource/AttributeIdentifierSource.php
+++ components/catalogs/back/src/Infrastructure/Validation/ProductMapping/AttributeSource/AttributeIdentifierSource.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Constraints\Compound;
 
 /**
- * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @psalm-suppress PropertyNotSetInConstructor
