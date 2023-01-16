5baad7d24d9d5d2fff8e1642cca4c27dc37532f2
Celine Morin <celine.morin@akeneo.com>
2023-01-16 09:18:36 +0100

    CPM-891: Use back validation for attribute related violations

--- components/identifier-generator/front/src/feature/validators/validateSimpleSelect.ts
+++ components/identifier-generator/front/src/feature/validators/validateSimpleSelect.ts
@@ -8,21 +8,7 @@ const validateSimpleSelect: Validator<SimpleSelectCondition> = (condition, path)
   if ([Operator.IN, Operator.NOT_IN].includes(condition?.operator) && condition.value?.length === 0) {
     violations.push({
       path,
-      message: `A value is required for the ${condition.label} attribute`,
-    });
-  }
-
-  if (condition?.scopable && !condition.scope) {
-    violations.push({
-      path,
-      message: `A channel is required for the ${condition.label} attribute`,
-    });
-  }
-
-  if (condition?.localizable && !condition.locale) {
-    violations.push({
-      path,
-      message: `A locale is required for the ${condition.label} attribute`,
+      message: `A value is required for the ${condition.attributeCode} attribute`,
     });
   }
 
