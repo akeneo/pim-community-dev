1ce0e2db25074a5e2cb8f401b4530cca6ea3876c
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-01-17 09:52:30 +0100

    CXP-1373: pr changes

--- components/catalogs/back/src/Infrastructure/Validation/ProductMapping/IsActivatedLocaleValidator.php
+++ components/catalogs/back/src/Infrastructure/Validation/ProductMapping/IsActivatedLocaleValidator.php
@@ -15,21 +15,8 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @psalm-suppress PropertyNotSetInConstructor
- *
- * @phpstan-type AttributeSource array{source: string, scope: string|null, locale: string|null}
- * @phpstan-type Attribute array{
- *    attribute_group_code: string,
- *    attribute_group_label: string,
- *    code: string,
- *    default_measurement_unit?: string,
- *    label: string,
- *    localizable: bool,
- *    measurement_family?: string,
- *    scopable: bool,
- *    type: string
- * }
  */
-final class IsValidLocaleValidator extends ConstraintValidator
+final class IsActivatedLocaleValidator extends ConstraintValidator
 {
     public function __construct(
         private GetLocalesQueryInterface $getLocalesQuery,
@@ -38,8 +25,8 @@ final class IsValidLocaleValidator extends ConstraintValidator
 
     public function validate($value, Constraint $constraint): void
     {
-        if (!$constraint instanceof IsValidLocale) {
-            throw new UnexpectedTypeException($constraint, IsValidLocale::class);
+        if (!$constraint instanceof IsActivatedLocale) {
+            throw new UnexpectedTypeException($constraint, IsActivatedLocale::class);
         }
 
         if (!\is_string($value)) {
