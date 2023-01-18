09e430bdfc1b09bffe8f8f44af6c355e717e5b06
Celine Morin <celine.morin@akeneo.com>
2023-01-18 10:18:09 +0100

    CPM-891: Fix tests and types

--- components/identifier-generator/front/src/feature/models/conditions/simpleSelectCondition.ts
+++ components/identifier-generator/front/src/feature/models/conditions/simpleSelectCondition.ts
@@ -2,17 +2,18 @@ import {CONDITION_NAMES} from './conditions';
 import {Operator} from './operator';
 import {OptionCode} from '../option';
 import {ChannelCode, LocaleCode} from '@akeneo-pim-community/shared';
+import {AttributeCode} from '../attribute';
 
 type SimpleSelectCondition = {
   type: CONDITION_NAMES.SIMPLE_SELECT;
-  attributeCode: string;
+  attributeCode: AttributeCode;
   locale?: LocaleCode | null;
   scope?: ChannelCode | null;
   value?: OptionCode[];
 } & (
   | {
       type: CONDITION_NAMES.SIMPLE_SELECT;
-      attributeCode: string;
+      attributeCode: AttributeCode;
       locale?: LocaleCode | null;
       scope?: ChannelCode | null;
       operator: Operator.IN | Operator.NOT_IN;
@@ -20,7 +21,7 @@ type SimpleSelectCondition = {
     }
   | {
       type: CONDITION_NAMES.SIMPLE_SELECT;
-      attributeCode: string;
+      attributeCode: AttributeCode;
       locale?: LocaleCode | null;
       scope?: ChannelCode | null;
       operator: Operator.EMPTY | Operator.NOT_EMPTY;
