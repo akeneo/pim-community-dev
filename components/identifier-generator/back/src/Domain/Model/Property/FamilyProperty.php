0ad96a6a0017c4e958970fe63e9859f8e88a97a4
Benjamin Letellier <letellier.benjamin@gmail.com>
2023-01-19 11:16:37 +0100

    CPM-907: add family in structure property

---
+++ components/identifier-generator/back/src/Domain/Model/Property/FamilyProperty.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Property;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @phpstan-type FamilyPropertyNormalized array{type: 'family', process: ProcessNormalized}
+ */
+final class FamilyProperty implements PropertyInterface
+{
+    public function __construct(
+        private readonly Process $process,
+    ) {
+    }
+
+    public static function type(): string
+    {
+        return 'family';
+    }
+
+    public function process(): Process
+    {
+        return $this->process;
+    }
+
+    /**
+     * @return FamilyPropertyNormalized
+     */
+    public function normalize(): array
+    {
+        return [
+            'type' => $this->type(),
+            'process' => $this->process->normalize(),
+        ];
+    }
+
+    public static function fromNormalized(array $normalizedProperty): PropertyInterface
+    {
+        Assert::keyExists($normalizedProperty, 'type');
+        Assert::same($normalizedProperty['type'], self::type());
+        Assert::keyExists($normalizedProperty, 'process');
+
+        return new self(Process::fromNormalized($normalizedProperty['process']));
+    }
+}
