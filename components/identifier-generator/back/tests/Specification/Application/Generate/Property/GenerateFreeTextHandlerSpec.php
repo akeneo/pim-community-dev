13611bb90983af7b763ac37ff4a30564e89a422e
pierallard <pierallard@gmail.com>
2023-01-23 09:40:53 +0100

    CPM-926: Add specs

--- components/identifier-generator/back/tests/Specification/Application/Generate/Property/GenerateFreeTextHandlerSpec.php
+++ components/identifier-generator/back/tests/Specification/Application/Generate/Property/GenerateFreeTextHandlerSpec.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Specification\Akeneo\Pim\Automation\IdentifierGenerator\Application\Generate\Property;
 
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Condition\Conditions;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Delimiter;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\IdentifierGenerator;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\IdentifierGeneratorCode;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\IdentifierGeneratorId;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\LabelCollection;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Property\AutoNumber;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Property\FreeText;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Structure;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Target;
 use PhpSpec\ObjectBehavior;
 
@@ -22,7 +29,21 @@ class GenerateFreeTextHandlerSpec extends ObjectBehavior
 
     public function it_should_throw_exception_when_invoked_with_something_else_than_free_text(): void
     {
-        $target = Target::fromString('sku');
+        $identifierGenerator = new IdentifierGenerator(
+            IdentifierGeneratorId::fromString('d556e59e-d46c-465e-863d-f4a39d0b7485'),
+            IdentifierGeneratorCode::fromString('my_generator'),
+            Conditions::fromArray([]),
+            Structure::fromArray([
+                FreeText::fromNormalized([
+                    'type' => FreeText::type(),
+                    'string' => 'AKN-',
+                ]),
+            ]),
+            LabelCollection::fromNormalized(['en_US' => 'MyGenerator']),
+            Target::fromString('sku'),
+            Delimiter::fromString(null),
+        );
+
         $autoNumber = AutoNumber::fromNormalized([
             'type' => AutoNumber::type(),
             'numberMin' => 0,
@@ -31,17 +52,26 @@ class GenerateFreeTextHandlerSpec extends ObjectBehavior
 
         $this
             ->shouldThrow(\InvalidArgumentException::class)
-            ->during('__invoke', [$autoNumber, $target, 'AKN-']);
+            ->during('__invoke', [$autoNumber, $identifierGenerator, 'AKN-']);
     }
 
     public function it_should_return_string(): void
     {
-        $target = Target::fromString('sku');
         $freeText = FreeText::fromNormalized([
             'type' => FreeText::type(),
             'string' => 'AKN-',
         ]);
 
-        $this->__invoke($freeText, $target, 'AKN-')->shouldReturn('AKN-');
+        $identifierGenerator = new IdentifierGenerator(
+            IdentifierGeneratorId::fromString('d556e59e-d46c-465e-863d-f4a39d0b7485'),
+            IdentifierGeneratorCode::fromString('my_generator'),
+            Conditions::fromArray([]),
+            Structure::fromArray([$freeText]),
+            LabelCollection::fromNormalized(['en_US' => 'MyGenerator']),
+            Target::fromString('sku'),
+            Delimiter::fromString(null),
+        );
+
+        $this->__invoke($freeText, $identifierGenerator, 'AKN-')->shouldReturn('AKN-');
     }
 }
