4a383cb58e0fc28106db46c8398303ff8108e3bf
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-01-30 16:40:04 +0100

    GRF-300: Add intagration test

--- src/Akeneo/Category/back/tests/Integration/Infrastructure/Storage/Sql/DeleteTemplateAndAttributesSqlIntegration.php
+++ src/Akeneo/Category/back/tests/Integration/Infrastructure/Storage/Sql/DeleteTemplateAndAttributesSqlIntegration.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Akeneo\Category\back\tests\Integration\Infrastructure\Storage\Sql;
 
+use Akeneo\Category\Application\ActivateTemplate;
+use Akeneo\Category\Application\Query\DeleteCategoryTreeTemplate;
+use Akeneo\Category\Application\Query\DeleteTemplateAndAttributes;
+use Akeneo\Category\Application\Query\GetTemplate;
 use Akeneo\Category\back\tests\Integration\Helper\CategoryTestCase;
+use Akeneo\Category\Domain\Model\Enrichment\Template;
 use Akeneo\Category\Domain\ValueObject\Code;
+use Akeneo\Category\Domain\ValueObject\Template\TemplateUuid;
 
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
@@ -13,12 +19,48 @@ use Akeneo\Category\Domain\ValueObject\Code;
  */
 class DeleteTemplateAndAttributesSqlIntegration extends CategoryTestCase
 {
-    public function TestDeleteTemplateAndAttributes(): void
+    public function testDeleteTemplateAndAttributes(): void
     {
         $category = $this->insertBaseCategory(new Code('template_deletion'));
-        $template = $this->givenTemplateWithAttributes(
-            '7b26de6a-9e64-11ed-a8fc-0242ac120002',
-            $category->getId()
+
+        $activateTemplateService = $this->get(ActivateTemplate::class);
+        $templateModel = $this->generateMockedCategoryTemplateModel(
+            categoryTreeId: $category->getId()->getValue()
         );
+
+        // Create template with dedicated service
+        $templateUuid = ($activateTemplateService)(
+            $templateModel->getCategoryTreeId(),
+            $templateModel->getCode(),
+            $templateModel->getLabelCollection()
+        );
+
+        /** @var Template $template */
+        $template = $this->get(GetTemplate::class)->byUuid($templateUuid);
+        self::assertNotNull($template);
+        self::assertEquals($templateUuid, $template->getUuid()->getValue());
+
+        // Call DeleteCategoryTreeTemplate to remove association with pim_catalog_category_tree_template before removing template
+        $deleteCategoryTreeTemplateService = $this->get(DeleteCategoryTreeTemplate::class);
+        ($deleteCategoryTreeTemplateService)($category->getId()->getValue(), $templateUuid);
+        $deleteTemplateAndAttributesService = $this->get(DeleteTemplateAndAttributes::class);
+        ($deleteTemplateAndAttributesService)($templateUuid);
+        $template = $this->get(GetTemplate::class)->byUuid($templateUuid);
+        self::assertNull($template);
+    }
+
+    public function testDeleteTemplateAndAttributesWithoutTemplateUuid(): void
+    {
+        // Non existing template in base
+        $templateUuid = TemplateUuid::fromString('02274dac-e99a-4e1d-8f9b-794d4c3ba330');
+
+        /** @var Template $template */
+        $template = $this->get(GetTemplate::class)->byUuid($templateUuid);
+        self::assertNull($template);
+
+        $deleteTemplateAndAttributesService = $this->get(DeleteTemplateAndAttributes::class);
+        ($deleteTemplateAndAttributesService)($templateUuid);
+        $template = $this->get(GetTemplate::class)->byUuid($templateUuid);
+        self::assertNull($template);
     }
 }
