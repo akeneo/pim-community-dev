1f93279e887a2736a2e0bf8008c1bf9cfb96ce88
pierallard <pierallard@gmail.com>
2023-01-31 18:13:14 +0100

    CPM-944: Add Controller and EndToEnd

---
+++ components/identifier-generator/back/src/Infrastructure/Repository/SqlNomenclatureDefinitionRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Repository;
+
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\NomenclatureDefinition;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\NomenclatureDefinitionRepository;
+use Doctrine\DBAL\Connection;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class SqlNomenclatureDefinitionRepository implements NomenclatureDefinitionRepository
+{
+
+    public function __construct(
+        private readonly Connection $connection,
+    ) {
+    }
+
+    public function get(string $propertyCode): NomenclatureDefinition
+    {
+        $sql = <<<SQL
+SELECT definition
+FROM pim_catalog_identifier_generator_nomenclature_definition
+WHERE property_code=:property_code
+SQL;
+        $result = $this->connection->fetchOne($sql, [
+            'property_code' => $propertyCode
+        ]);
+
+        if (false === $result) {
+            return NomenclatureDefinition::default();
+        }
+
+        $jsonResult = \json_decode($result, true);
+        // TODO Create a FromNormalized + check data
+        return new NomenclatureDefinition(
+            $jsonResult['operator'],
+            $jsonResult['value'],
+        );
+    }
+
+    public function create(string $propertyCode, NomenclatureDefinition $nomenclatureDefinition): void
+    {
+        // TODO This method should be removed
+        throw new \LogicException('Not implemented');
+    }
+
+    public function update(string $propertyCode, NomenclatureDefinition $nomenclatureDefinition): void
+    {
+        $sql = <<<SQL
+INSERT INTO pim_catalog_identifier_generator_nomenclature_definition (property_code, definition)
+VALUES(:property_code, :definition)
+ON DUPLICATE KEY UPDATE definition = :definition
+SQL;
+
+        $this->connection->executeStatement($sql, [
+            'property_code' => $propertyCode,
+            'definition' => \json_encode($nomenclatureDefinition->normalize()),
+        ]);
+    }
+}
