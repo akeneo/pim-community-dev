16bb34d53066613f8d7400c2bfb9ce050c79ec19
Celine Morin <celine.morin@akeneo.com>
2023-02-01 16:22:04 +0100

    CPM-944: Merge repositories

--- components/identifier-generator/back/src/Infrastructure/Repository/InMemoryNomenclatureRepository.php
+++ components/identifier-generator/back/src/Infrastructure/Repository/InMemoryNomenclatureRepository.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Repository;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\NomenclatureDefinition;
-use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\NomenclatureDefinitionRepository;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\NomenclatureRepository;
 
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class InMemoryNomenclatureDefinitionRepository implements NomenclatureDefinitionRepository
+class InMemoryNomenclatureRepository implements NomenclatureRepository
 {
     /** @var array<string, NomenclatureDefinition> */
     private array $nomenclatureDefinitions = [];
+    /**
+     * @var array<string, string>
+     */
+    private array $values = [];
 
     public function get(string $propertyCode): ?NomenclatureDefinition
     {
@@ -24,5 +28,18 @@ class InMemoryNomenclatureDefinitionRepository implements NomenclatureDefinition
     public function update(string $propertyCode, NomenclatureDefinition $nomenclatureDefinition): void
     {
         $this->nomenclatureDefinitions[$propertyCode] = $nomenclatureDefinition;
+
+        foreach ($nomenclatureDefinition->values() as $familyCode => $value) {
+            if (null === $value) {
+                unset($this->values[$familyCode]);
+            } else {
+                $this->values[$familyCode] = $value;
+            }
+        }
+    }
+
+    public function getValue(string $familyCode): ?string
+    {
+        return $this->values[$familyCode] ?? null;
     }
 }
