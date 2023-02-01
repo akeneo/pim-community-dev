ecd0d7fe2c11bc493db680cfed3d24e1395fd8ae
pierallard <pierallard@gmail.com>
2023-02-01 15:10:18 +0100

    CPM-944: Fix CI

--- components/identifier-generator/back/src/Application/Update/UpdateNomenclatureCommand.php
+++ components/identifier-generator/back/src/Application/Update/UpdateNomenclatureCommand.php
@@ -1,18 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Application\Update;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\Application\CommandInterface;
 
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class UpdateNomenclatureCommand implements CommandInterface
 {
     /**
      * @param array<string, ?string> $values
      */
     public function __construct(
-        private readonly ?string $propertyCode,
+        private readonly string $propertyCode,
         private readonly ?string $operator,
-        private readonly string|int|null $value,
+        private readonly ?int $value,
         private readonly ?bool $generateIfEmpty,
         private readonly ?array $values = [],
     ) {
@@ -23,21 +29,26 @@ class UpdateNomenclatureCommand implements CommandInterface
      */
     public function getValues(): array
     {
-        return $this->values;
+        return $this->values ?? [];
     }
 
-    public function getOperator(): string
+    public function getOperator(): ?string
     {
         return $this->operator;
     }
 
-    public function getValue(): string
+    public function getValue(): ?int
     {
         return $this->value;
     }
 
-    public function getGenerateIfEmpty(): bool
+    public function getGenerateIfEmpty(): ?bool
     {
         return $this->generateIfEmpty;
+    }
+
+    public function getPropertyCode(): string
+    {
+        return $this->propertyCode;
     }
 }
--- components/identifier-generator/back/src/Infrastructure/Repository/SqlNomenclatureDefinitionRepository.php
+++ components/identifier-generator/back/src/Infrastructure/Repository/SqlNomenclatureDefinitionRepository.php
@@ -15,7 +15,6 @@ use Webmozart\Assert\Assert;
  */
 class SqlNomenclatureDefinitionRepository implements NomenclatureDefinitionRepository
 {
-
     public function __construct(
         private readonly Connection $connection,
     ) {
@@ -28,18 +27,21 @@ SELECT definition
 FROM pim_catalog_identifier_generator_nomenclature_definition
 WHERE property_code=:property_code
 SQL;
-        $result = $this->connection->fetchOne($sql, [
-            'property_code' => $propertyCode
+        $definition = $this->connection->fetchOne($sql, [
+            'property_code' => $propertyCode,
         ]);
 
-        if (false === $result) {
+        if (false === $definition) {
             return null;
         }
+        Assert::string($definition);
 
-        $jsonResult = \json_decode($result, true);
+        $jsonResult = \json_decode($definition, true);
+        Assert::isArray($jsonResult, sprintf('Invalid JSON: "%s"', $definition));
 
-        return $this->denormalizeNomenclatureDefinition($jsonResult);
+        return $this->fromNormalize($jsonResult);
     }
+
     public function update(string $propertyCode, NomenclatureDefinition $nomenclatureDefinition): void
     {
         $sql = <<<SQL
@@ -54,7 +56,14 @@ SQL;
         ]);
     }
 
-    private function denormalizeNomenclatureDefinition(mixed $jsonResult): NomenclatureDefinition
+    /**
+     * @param array{
+     *     operator?: string,
+     *     value?: int,
+     *     generate_if_empty?: bool
+     * } $jsonResult
+     */
+    private function fromNormalize(array $jsonResult): NomenclatureDefinition
     {
         return new NomenclatureDefinition(
             $jsonResult['operator'] ?? null,
