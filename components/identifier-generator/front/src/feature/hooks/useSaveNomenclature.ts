cc939ed2b410779b73871a1d666d1ae08dfe60b9
Celine Morin <celine.morin@akeneo.com>
2023-02-06 18:02:49 +0100

    CPM-945: Add useSaveNomenclature hook

---
+++ components/identifier-generator/front/src/feature/hooks/useSaveNomenclature.ts
@@ -0,0 +1,39 @@
+import {Nomenclature} from '../models';
+import {useMutation, useQueryClient} from 'react-query';
+import {useRouter} from '@akeneo-pim-community/shared';
+import {UseMutateFunction} from 'react-query/types/react/types';
+import {Violation} from '../validators';
+
+const useSaveNomenclature: () => {save: UseMutateFunction<void, Violation[], Nomenclature>} = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const {mutate: save} = useMutation<void, Violation[], Nomenclature>(
+    async (nomenclature: Nomenclature) => {
+      const response = await fetch(
+        router.generate('akeneo_identifier_generator_nomenclature_rest_update', {
+          propertyCode: nomenclature.propertyCode,
+        }),
+        {
+          method: 'PATCH',
+          headers: [
+            ['Content-type', 'application/json'],
+            ['X-Requested-With', 'XMLHttpRequest'],
+          ],
+          body: JSON.stringify(nomenclature),
+        }
+      );
+
+      const data = await response.json();
+
+      return response.ok ? data : Promise.reject(data);
+    },
+    {
+      onSuccess: () => queryClient.invalidateQueries('getNomenclature'),
+    }
+  );
+
+  return {save};
+};
+
+export {useSaveNomenclature};
