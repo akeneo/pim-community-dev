464408d113c8464561a37a3ebef948ad16dbfd0b
Celine Morin <celine.morin@akeneo.com>
2023-02-07 15:02:40 +0100

    CPM-943: Add final to new classes

--- components/identifier-generator/back/src/Application/Get/GetNomenclatureCommand.php
+++ components/identifier-generator/back/src/Application/Get/GetNomenclatureCommand.php
@@ -8,7 +8,7 @@ namespace Akeneo\Pim\Automation\IdentifierGenerator\Application\Get;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class GetNomenclatureCommand
+final class GetNomenclatureCommand
 {
     public function __construct(private readonly string $propertyCode)
     {
