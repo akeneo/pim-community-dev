55d372616d85babee820553973e367f046d3f6dc
Riff <riff.ventura@gmail.com>
2023-02-07 18:34:58 +0100

    Moved and renamed GetAllTestAppsAction

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/CustomApps/Controller/Internal/GetAllCustomAppsAction.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/CustomApps/Controller/Internal/GetAllCustomAppsAction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Connectivity\Connection\Infrastructure\Marketplace\Controller\Internal;
+namespace Akeneo\Connectivity\Connection\Infrastructure\CustomApps\Controller\Internal;
 
 use Akeneo\Connectivity\Connection\Application\Marketplace\AppUrlGenerator;
 use Akeneo\Connectivity\Connection\Domain\CustomApps\DTO\GetAllCustomAppsResult;
@@ -17,12 +17,12 @@ use Symfony\Component\HttpFoundation\Response;
  * @copyright 2021 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class GetAllTestAppsAction
+final class GetAllCustomAppsAction
 {
     public function __construct(
-        private FeatureFlag $appDeveloperMode,
-        private AppUrlGenerator $appUrlGenerator,
-        private GetAllCustomAppsQueryInterface $getAllTestAppsQuery
+        private readonly FeatureFlag $appDeveloperMode,
+        private readonly AppUrlGenerator $appUrlGenerator,
+        private readonly GetAllCustomAppsQueryInterface $getAllCustomAppsQuery
     ) {
     }
 
@@ -36,7 +36,7 @@ final class GetAllTestAppsAction
             return new JsonResponse(GetAllCustomAppsResult::create(0, [])->normalize());
         }
 
-        $result = $this->getAllTestAppsQuery->execute();
+        $result = $this->getAllCustomAppsQuery->execute();
         $resultWithPimUrl = $result->withPimUrlSource($this->appUrlGenerator->getAppQueryParameters());
 
         return new JsonResponse($resultWithPimUrl->normalize());
