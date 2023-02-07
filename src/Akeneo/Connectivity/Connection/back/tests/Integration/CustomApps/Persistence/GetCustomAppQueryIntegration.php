4fa6c4333fcf7dcb6d61280c18c721580bde17f3
Riff <riff.ventura@gmail.com>
2023-02-07 15:25:09 +0100

    CXP-1419: removed integration test duplicate

--- src/Akeneo/Connectivity/Connection/back/tests/Integration/CustomApps/Persistence/GetCustomAppQueryIntegration.php
+++ src/Akeneo/Connectivity/Connection/back/tests/Integration/CustomApps/Persistence/GetCustomAppQueryIntegration.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Connectivity\Connection\Tests\Integration\CustomApps\Persistence;
 
 use Akeneo\Connectivity\Connection\Infrastructure\CustomApps\Persistence\GetCustomAppQuery;
+use Akeneo\Connectivity\Connection\Tests\CatalogBuilder\ConnectedAppLoader;
 use Akeneo\Connectivity\Connection\Tests\CatalogBuilder\CustomAppLoader;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
@@ -18,6 +19,7 @@ use Akeneo\Test\Integration\TestCase;
 class GetCustomAppQueryIntegration extends TestCase
 {
     private ?CustomAppLoader $customAppLoader;
+    private ?ConnectedAppLoader $connectedAppLoader;
     private ?GetCustomAppQuery $getCustomAppQuery;
 
     protected function setUp(): void
@@ -25,6 +27,7 @@ class GetCustomAppQueryIntegration extends TestCase
         parent::setUp();
 
         $this->customAppLoader = $this->get(CustomAppLoader::class);
+        $this->connectedAppLoader = $this->get('akeneo_connectivity.connection.fixtures.connected_app_loader');
         $this->getCustomAppQuery = $this->get(GetCustomAppQuery::class);
         $this->connection = $this->get('database_connection');
     }
@@ -49,6 +52,27 @@ class GetCustomAppQueryIntegration extends TestCase
                 'activate_url' => 'http://activate.test',
                 'callback_url' => 'http://callback.test',
                 'connected' => false,
+            ],
+            $result
+        );
+    }
+
+    public function test_it_returns_a_custom_app_which_is_connected(): void
+    {
+        $user = $this->createAdminUser();
+        $this->customAppLoader->create('100eedac-ff5c-497b-899d-e2d64b6c59f9', $user->getId());
+        $this->connectedAppLoader->createConnectedAppWithUserAndTokens('100eedac-ff5c-497b-899d-e2d64b6c59f9', 'foo');
+
+        $result = $this->getCustomAppQuery->execute('100eedac-ff5c-497b-899d-e2d64b6c59f9');
+
+        $this->assertEquals(
+            [
+                'id' => '100eedac-ff5c-497b-899d-e2d64b6c59f9',
+                'name' => 'custom_app_name',
+                'author' => 'John Doe',
+                'activate_url' => 'http://activate.test',
+                'callback_url' => 'http://callback.test',
+                'connected' => true,
             ],
             $result
         );
