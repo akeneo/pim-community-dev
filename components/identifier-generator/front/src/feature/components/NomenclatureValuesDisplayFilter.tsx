b5b5950471668b0aa46ed24c6cbc47e8e57fe921
pierallard <pierallard@gmail.com>
2023-02-07 12:14:04 +0100

    CPM-945: Add translations and style for nomenclatures

--- components/identifier-generator/front/src/feature/components/NomenclatureValuesDisplayFilter.tsx
+++ components/identifier-generator/front/src/feature/components/NomenclatureValuesDisplayFilter.tsx
@@ -1,46 +1,46 @@
-import React, {useState} from 'react';
-import {Dropdown, SwitcherButton} from 'akeneo-design-system';
+import React, {useCallback} from 'react';
+import {Dropdown, SwitcherButton, useBooleanState} from 'akeneo-design-system';
 import {NomenclatureFilter} from '../models';
+import {useTranslate} from '@akeneo-pim-community/shared';
 
 type Props = {
   filter: NomenclatureFilter;
   onChange: (value: NomenclatureFilter) => void;
 };
 
-const values: {code: NomenclatureFilter; label: string}[] = [
-  {code: 'all', label: 'All'},
-  {code: 'error', label: 'Error'},
-  {code: 'empty', label: 'Missing'},
-  {code: 'filled', label: 'Filled'},
-];
+const FILTERS: NomenclatureFilter[] = ['all', 'error', 'empty', 'filled'];
 
 const NomenclatureValuesDisplayFilter: React.FC<Props> = ({filter, onChange}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const open = () => setIsOpen(true);
-  const close = () => setIsOpen(false);
+  const translate = useTranslate();
+  const [isOpen, open, close] = useBooleanState();
+
+  const getLabel = useCallback(
+    codeFilter => translate(`pim_identifier_generator.nomenclature.filters.${codeFilter}`),
+    [translate]
+  );
 
   return (
     <Dropdown>
-      <SwitcherButton inline onClick={open} label={'DIsplay TODO'}>
-        <span>{filter}</span>
+      <SwitcherButton inline onClick={open} label={translate('pim_identifier_generator.nomenclature.display')}>
+        <span>{getLabel(filter)}</span>
       </SwitcherButton>
       {isOpen && (
         <Dropdown.Overlay verticalPosition="down" onClose={close}>
           <Dropdown.Header>
-            <Dropdown.Title>Title TODO</Dropdown.Title>
+            <Dropdown.Title>{translate('pim_identifier_generator.nomenclature.operator_placeholder')}</Dropdown.Title>
           </Dropdown.Header>
           <Dropdown.ItemCollection>
-            {values.map(({code, label}) => (
+            {FILTERS.map(myFilter => (
               <Dropdown.Item
-                aria-selected={code === filter}
-                key={code}
+                aria-selected={myFilter === filter}
+                key={myFilter}
                 onClick={() => {
                   close();
-                  onChange(code);
+                  onChange(myFilter);
                 }}
-                isActive={code === filter}
+                isActive={myFilter === filter}
               >
-                <span key={code}>{label} TODO</span>
+                <span>{getLabel(myFilter)}</span>
               </Dropdown.Item>
             ))}
           </Dropdown.ItemCollection>
