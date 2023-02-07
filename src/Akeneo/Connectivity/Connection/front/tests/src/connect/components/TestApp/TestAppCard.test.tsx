f06d7e3249389dd072c5c03d6d5d954cf450a10e
Guillaume Fer <guillaume.fer@akeneo.com>
2023-02-07 16:50:10 +0100

    CXP-1424: add and modify tests

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/TestApp/TestAppCard.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/TestApp/TestAppCard.test.tsx
@@ -3,6 +3,7 @@ import {screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import {renderWithProviders} from '../../../../test-utils';
 import {TestAppCard} from '@src/connect/components/TestApp/TestAppCard';
+import {SecurityContext} from '@src/shared/security';
 
 test('it displays test app', () => {
     const testApp = {
@@ -42,4 +43,36 @@ test('it displays test app with removed author', () => {
             'akeneo_connectivity.connection.connect.marketplace.card.developed_by?author=akeneo_connectivity.connection.connect.marketplace.test_apps.removed_user'
         )
     ).toBeInTheDocument();
+});
+
+test('it not displays the delete button when the user doesnt have the permission to delete custom Apps', () => {
+    const isGranted = jest.fn(acl => {
+        if (acl === 'akeneo_connectivity_connection_manage_test_apps') {
+            return false;
+        }
+        return true;
+    });
+
+    const testApp = {
+        id: 'id1',
+        name: 'Name of the test app',
+        logo: null,
+        author: null,
+        url: null,
+        activate_url: 'test_app_1_activate_url',
+        callback_url: 'test_app_1_callback_url',
+        connected: false,
+    };
+
+    renderWithProviders(
+        <SecurityContext.Provider value={{isGranted}}>
+            <TestAppCard testApp={testApp} />
+        </SecurityContext.Provider>
+    );
+
+    expect(screen.queryByText('Name of the test app')).toBeInTheDocument();
+
+    expect(
+        screen.queryByTitle('pim_common.delete')
+    ).not.toBeInTheDocument();
 });
