f2bfa6bcc296273da512501ace5f9e28a7450787
Pierre Allard <pierallard@gmail.com>
2023-02-08 15:40:51 +0100

    Apply suggestions from code review
    Co-authored-by: antoinearrive <antoine.arrive@gmail.com>

--- components/identifier-generator/back/src/Application/Update/UpdateNomenclatureCommand.php
+++ components/identifier-generator/back/src/Application/Update/UpdateNomenclatureCommand.php
@@ -10,7 +10,7 @@ use Akeneo\Pim\Automation\IdentifierGenerator\Application\CommandInterface;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UpdateNomenclatureCommand implements CommandInterface
+final class UpdateNomenclatureCommand implements CommandInterface
 {
     /**
      * @param array<string, ?string> $values
