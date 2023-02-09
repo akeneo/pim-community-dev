b057468d56181a767628464deffc9ddd982b3925
Riff <riff.ventura@gmail.com>
2023-02-09 15:48:05 +0100

    CXP-1419: Renamed create test app page

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/pages/CreateCustomAppPage.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/pages/CreateCustomAppPage.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import {screen} from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
 import {renderWithProviders, historyMock} from '../../../test-utils';
-import {TestAppCreatePage} from '@src/connect/pages/TestAppCreatePage';
+import {CreateCustomAppPage} from '@src/connect/pages/CreateCustomAppPage';
 import {CreateTestAppForm} from '@src/connect/components/TestApp/CreateTestAppForm';
 import {CreateTestAppCredentials} from '@src/connect/components/TestApp/CreateTestAppCredentials';
 import {TestAppCredentials} from '@src/model/Apps/test-app-credentials';
@@ -45,7 +45,7 @@ jest.mock('@src/connect/components/TestApp/CreateTestAppCredentials', () => ({
 }));
 
 test('it renders the form without credentials and display them when form is submitted', () => {
-    renderWithProviders(<TestAppCreatePage />);
+    renderWithProviders(<CreateCustomAppPage />);
 
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.marketplace.test_apps.modal.subtitle')
