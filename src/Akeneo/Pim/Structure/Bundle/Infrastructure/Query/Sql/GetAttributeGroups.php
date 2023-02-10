5c2651b3494552c3bf83caf264a999bf4345074d
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-02-10 09:54:05 +0100

    RAB-1293: Add attribute count in GetAttributeGroups sql query

--- src/Akeneo/Pim/Structure/Bundle/Infrastructure/Query/Sql/GetAttributeGroups.php
+++ src/Akeneo/Pim/Structure/Bundle/Infrastructure/Query/Sql/GetAttributeGroups.php
@@ -24,12 +24,19 @@ class GetAttributeGroups implements GetAttributeGroupsInterface
     {
         $sql = <<<SQL
 WITH attribute_group_labels AS (
-    SELECT foreign_key as attribute_group_id, JSON_OBJECTAGG(locale, label) as labels
-    FROM pim_catalog_attribute_group_translation
+    SELECT foreign_key AS attribute_group_id, JSON_OBJECTAGG(locale, label) AS labels
+    FROM `pim_catalog_attribute_group_translation`
     GROUP BY foreign_key
+),
+attribute_group_count AS (
+	SELECT group_id, COUNT(id) AS attribute_count
+	FROM `pim_catalog_attribute`
+	GROUP BY group_id
 )
-SELECT code, sort_order, labels FROM pim_catalog_attribute_group
-LEFT JOIN attribute_group_labels ON attribute_group_id = id
+SELECT attribute_group.code, attribute_group.sort_order, labels, attribute_count
+FROM `pim_catalog_attribute_group` attribute_group
+LEFT JOIN `attribute_group_labels` ON attribute_group_labels.attribute_group_id = attribute_group.id
+LEFT JOIN `attribute_group_count` ON attribute_group_count.group_id = attribute_group.id
 ORDER BY sort_order;
 SQL;
 
@@ -39,10 +46,13 @@ SQL;
         return array_map(function (array $attributeGroup) use ($dqiFeatureIsEnabled) {
             $attributeGroup['sort_order'] = (int) $attributeGroup['sort_order'];
             $attributeGroup['labels'] = null !== $attributeGroup['labels'] ? json_decode($attributeGroup['labels'], true) : [];
+            $attributeGroup['attribute_count'] = (int) $attributeGroup['attribute_count'] ?? 0;
+
             if ($dqiFeatureIsEnabled) {
                 // @TODO RAB-1274 call ServiceAPI
                 $attributeGroup['is_dqi_activated'] = true;
             }
+
             return $attributeGroup;
         }, $attributeGroups);
     }
--- tests/back/Pim/Structure/Integration/AttributeGroup/Infrastructure/Query/Sql/GetAttributeGroupsIntegration.php
+++ tests/back/Pim/Structure/Integration/AttributeGroup/Infrastructure/Query/Sql/GetAttributeGroupsIntegration.php
@@ -8,19 +8,24 @@ use Akeneo\Pim\Structure\Bundle\Infrastructure\Query\Sql\GetAttributeGroups;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Types\Types;
 
-class SqlGetAttributeGroupsIntegration extends TestCase
+class GetAttributeGroupsIntegration extends TestCase
 {
     public function testItReturnsAttributeGroups()
     {
-        $this->createAttributeGroup('an_attribute_group', 1, []);
-        $this->createAttributeGroup('another_attribute_group', 2, ['en_US' => 'Another attribute group']);
+        $attributeGroupId1 = $this->createAttributeGroup('an_attribute_group', 1, [], true);
+        $this->createAttributeGroup('another_attribute_group', 2, ['en_US' => 'Another attribute group'], false);
+
+        $this->createAttribute('nice_attribute1', 'pim_catalog_text', $attributeGroupId1);
+        $this->createAttribute('nice_attribute2', 'pim_catalog_text', $attributeGroupId1);
 
         $this->assertSame([
             [
                 'code' => 'an_attribute_group',
                 'sort_order' => 1,
                 'labels' => [],
+                'attribute_count' => 2,
             ],
             [
                 'code' => 'another_attribute_group',
@@ -28,6 +33,7 @@ class SqlGetAttributeGroupsIntegration extends TestCase
                 'labels' => [
                     'en_US' => 'Another attribute group',
                 ],
+                'attribute_count' => 0,
             ],
             [
                 'code' => 'other',
@@ -36,6 +42,7 @@ class SqlGetAttributeGroupsIntegration extends TestCase
                     'en_US' => 'Other',
                     'fr_FR' => 'Autre',
                 ],
+                'attribute_count' => 1,
             ],
         ], $this->getQuery()->all());
     }
@@ -44,14 +51,18 @@ class SqlGetAttributeGroupsIntegration extends TestCase
     {
         $this->get('feature_flags')->enable('data_quality_insights');
 
-        $this->createAttributeGroup('an_attribute_group', 1, []);
-        $this->createAttributeGroup('another_attribute_group', 2, ['en_US' => 'Another attribute group']);
+        $attributeGroupId1 = $this->createAttributeGroup('an_attribute_group', 1, [], true);
+        $this->createAttributeGroup('another_attribute_group', 2, ['en_US' => 'Another attribute group'], false);
+
+        $this->createAttribute('nice_attribute1', 'pim_catalog_text', $attributeGroupId1);
+        $this->createAttribute('nice_attribute2', 'pim_catalog_text', $attributeGroupId1);
 
         $this->assertSame([
             [
                 'code' => 'an_attribute_group',
                 'sort_order' => 1,
                 'labels' => [],
+                'attribute_count' => 2,
                 'is_dqi_activated' => true,
             ],
             [
@@ -60,7 +71,8 @@ class SqlGetAttributeGroupsIntegration extends TestCase
                 'labels' => [
                     'en_US' => 'Another attribute group',
                 ],
-                'is_dqi_activated' => true,
+                'attribute_count' => 0,
+                'is_dqi_activated' => false,
             ],
             [
                 'code' => 'other',
@@ -69,6 +81,7 @@ class SqlGetAttributeGroupsIntegration extends TestCase
                     'en_US' => 'Other',
                     'fr_FR' => 'Autre',
                 ],
+                'attribute_count' => 1,
                 'is_dqi_activated' => true,
             ],
         ], $this->getQuery()->all());
@@ -84,7 +97,7 @@ class SqlGetAttributeGroupsIntegration extends TestCase
         return $this->get('Akeneo\Pim\Structure\Bundle\Infrastructure\Query\Sql\GetAttributeGroups');
     }
 
-    private function createAttributeGroup(string $code, int $sortOrder, array $labels): void
+    private function createAttributeGroup(string $code, int $sortOrder, array $labels, ?bool $isDqiActivated): int
     {
         /** @var Connection $connection */
         $connection = $this->get('database_connection');
@@ -94,7 +107,7 @@ INSERT INTO pim_catalog_attribute_group (code, sort_order, created, updated) VAL
 SQL;
 
         $connection->executeQuery($attributeGroupSql, ['code' => $code, 'sort_order' => $sortOrder]);
-        $attributeGroupId = $connection->lastInsertId();
+        $attributeGroupId = (int) $connection->lastInsertId();
 
         $attributeGroupTranslationSql = <<<SQL
 INSERT INTO pim_catalog_attribute_group_translation (foreign_key, label, locale) VALUES (:attribute_group_id, :label, :locale);
@@ -103,5 +116,31 @@ SQL;
         foreach ($labels as $locale => $label) {
             $connection->executeQuery($attributeGroupTranslationSql, ['attribute_group_id' => $attributeGroupId, 'label' => $label, 'locale' => $locale]);
         }
+
+        if (null !== $isDqiActivated) {
+            $attributeGroupActivationSql = <<<SQL
+INSERT INTO pim_data_quality_insights_attribute_group_activation (attribute_group_code, activated, updated_at) VALUES (:attribute_group_code, :is_dqi_activated, NOW());
+SQL;
+            $connection->executeQuery($attributeGroupActivationSql, ['attribute_group_code' => $code, 'is_dqi_activated' => $isDqiActivated], ['is_dqi_activated' => Types::BOOLEAN]);
+        }
+
+        return $attributeGroupId;
+    }
+
+    private function createAttribute(string $code, string $attributeType, int $attributeGroupId): void
+    {
+        /** @var Connection $connection */
+        $connection = $this->get('database_connection');
+
+        $attributeSql = <<<SQL
+INSERT INTO `pim_catalog_attribute` ( `group_id`, `sort_order`, `is_required`, `is_unique`, `is_localizable`, `is_scopable`, `code`, `entity_type`, `attribute_type`, `backend_type`, `created`, `updated`)
+VALUES
+	(:attribute_group_id, 1, 1, 1, 1, 0, :code, 'Akeneo\\Pim\\Enrichment\\Component\\Product\\Model\\Product', :attribute_type, 'text', NOW(), NOW());
+SQL;
+        $connection->executeQuery($attributeSql, [
+            'code' => $code,
+            'attribute_type' => $attributeType,
+            'attribute_group_id' => $attributeGroupId,
+        ]);
     }
 }
