230b7048a0121d854d4d0a0d7943ea9c4301c083
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-02-13 14:58:19 +0100

    CXP-1422: Credentials regeneration

---
+++ src/Akeneo/Connectivity/Connection/back/Application/CustomApps/Command/RegenerateCustomAppSecretHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Application\CustomApps\Command;
+
+use Akeneo\Connectivity\Connection\Application\RandomCodeGeneratorInterface;
+use Akeneo\Connectivity\Connection\Domain\CustomApps\Persistence\UpdateCustomAppSecretQueryInterface;
+
+/**
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class RegenerateCustomAppSecretHandler
+{
+    public function __construct(
+        private readonly UpdateCustomAppSecretQueryInterface $regenerateCustomAppSecretQuery,
+        private readonly RandomCodeGeneratorInterface $randomCodeGenerator,
+    ) {
+    }
+
+    public function handle(RegenerateCustomAppSecretCommand $command): void
+    {
+        $customAppId = $command->customAppId;
+
+        $clientSecret = \substr($this->randomCodeGenerator->generate(), 0, 100);
+
+        $this->regenerateCustomAppSecretQuery->execute($customAppId, $clientSecret);
+    }
+}
