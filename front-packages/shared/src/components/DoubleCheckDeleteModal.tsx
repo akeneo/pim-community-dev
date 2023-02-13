af4a448ab882b46f42d1debe493feb8c89483030
Pierre Jolly <pie.jolly@gmail.com>
2023-02-13 10:06:41 +0100

    RAB-1259: add tests

--- front-packages/shared/src/components/DoubleCheckDeleteModal.tsx
+++ front-packages/shared/src/components/DoubleCheckDeleteModal.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {DeleteModal, DeleteModalProps} from './DeleteModal';
+import {TextField} from './TextField';
+import {useTranslate} from '../hooks';
 
-type DoubleCheckDeleteModalProps = DeleteModalProps & {
-  textToCheck: string;
-};
+type DoubleCheckDeleteModalProps = DeleteModalProps & {textToCheck: string};
 
 const DoubleCheckDeleteModal = ({children, textToCheck, ...deleteModalProps}: DoubleCheckDeleteModalProps) => {
+  const translate = useTranslate();
+  const [value, setValue] = useState('');
+  const canConfirmDelete = value === textToCheck;
+
   return (
-    <DeleteModal {...deleteModalProps} canConfirmDelete={true}>
+    <DeleteModal {...deleteModalProps} canConfirmDelete={canConfirmDelete}>
       {children}
-      <div>Please type "{textToCheck}"</div>
+      <TextField
+        value={value}
+        label={translate('pim_enrich.entity.attribute.module.mass_delete.modal.label', {textToCheck})}
+        onChange={setValue}
+      />
     </DeleteModal>
   );
 };
