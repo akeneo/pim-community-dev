eb6c587627c92b2c12dd7a4a58d28d3e70cb32b3
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-02-13 15:23:04 +0100

    CXP-1422: pr fixes

--- src/Akeneo/Connectivity/Connection/back/Application/CustomApps/Command/RegenerateCustomAppSecretHandler.php
+++ src/Akeneo/Connectivity/Connection/back/Application/CustomApps/Command/RegenerateCustomAppSecretHandler.php
@@ -8,8 +8,7 @@ use Akeneo\Connectivity\Connection\Application\RandomCodeGeneratorInterface;
 use Akeneo\Connectivity\Connection\Domain\CustomApps\Persistence\UpdateCustomAppSecretQueryInterface;
 
 /**
- * @author    Willy Mesnage <willy.mesnage@akeneo.com>
- * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class RegenerateCustomAppSecretHandler
--- src/Akeneo/Connectivity/Connection/back/Domain/CustomApps/Persistence/UpdateCustomAppSecretQueryInterface.php
+++ src/Akeneo/Connectivity/Connection/back/Domain/CustomApps/Persistence/UpdateCustomAppSecretQueryInterface.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Akeneo\Connectivity\Connection\Domain\CustomApps\Persistence;
 
 /**
- * @author    Willy Mesnage <willy.mesnage@akeneo.com>
- * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 interface UpdateCustomAppSecretQueryInterface
