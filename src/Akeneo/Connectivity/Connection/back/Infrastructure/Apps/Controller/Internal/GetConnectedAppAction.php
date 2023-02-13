c220288d466977559f33bcfc466ef205ae4ca27a
Guillaume Fer <guillaume.fer@akeneo.com>
2023-02-13 11:48:09 +0100

    CXP-1424: fix acls

--- src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Controller/Internal/GetConnectedAppAction.php
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/Apps/Controller/Internal/GetConnectedAppAction.php
@@ -54,9 +54,5 @@ final class GetConnectedAppAction
         if (!$connectedApp->isCustomApp() && !$this->security->isGranted('akeneo_connectivity_connection_manage_apps')) {
             throw new AccessDeniedHttpException();
         }
-
-        if ($connectedApp->isCustomApp() && !$this->security->isGranted('akeneo_connectivity_connection_manage_test_apps')) {
-            throw new AccessDeniedHttpException();
-        }
     }
 }
