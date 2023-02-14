d3a43e03faaa13710e0acc0e4d6c42ffcc7b215b
Riff <riff.ventura@gmail.com>
2023-02-14 14:33:54 +0100

    CXP-1419: Updated hook to delete a custom app

--- src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-delete-custom-app.ts
+++ src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-delete-custom-app.ts
@@ -1,15 +1,14 @@
 import {useRoute} from '../../shared/router';
 import {useCallback} from 'react';
+import {useMutation} from 'react-query';
 
-type CallbackType = () => Promise<void>;
-
-export const useDeleteTestApp = (customAppId: string): CallbackType => {
+type DeleteCustomApp = () => Promise<void>;
+export const useDeleteCustomApp = (customAppId: string): DeleteCustomApp => {
     const url = useRoute('akeneo_connectivity_connection_custom_apps_rest_delete', {customAppId});
-
-    return useCallback(async () => {
+    const request = useCallback(async () => {
         const response = await fetch(url, {
             method: 'DELETE',
-            headers: [['X-Requested-With', 'XMLHttpRequest']],
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
         });
 
         if (!response.ok) {
@@ -18,4 +17,6 @@ export const useDeleteTestApp = (customAppId: string): CallbackType => {
 
         return Promise.resolve();
     }, [url]);
+
+    return useMutation(request).mutateAsync;
 };
