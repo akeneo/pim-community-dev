3bd9898afe1dfb15d430541f9de0cd83f99bc7fc
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-02-14 09:48:37 +0100

    CXP-1420: Limit the number of custom apps (connected)

--- src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/TestApp/TestAppList.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/connect/components/TestApp/TestAppList.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import {renderWithProviders} from '../../../../test-utils';
+import {mockFetchResponses, renderWithProviders} from '../../../../test-utils';
 import {TestAppList} from '@src/connect/components/TestApp/TestAppList';
 import {SecurityContext} from '@src/shared/security';
 
@@ -45,7 +45,7 @@ test('it displays test app', () => {
         total: 2,
         apps: [testApp1, testApp2],
     };
-    renderWithProviders(<TestAppList testApps={testApps} />);
+    renderWithProviders(<TestAppList testApps={testApps} isLimitReached={false} />);
 
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.marketplace.test_apps.title')
@@ -63,7 +63,7 @@ test('it displays nothing when total is 0', () => {
         total: 0,
         apps: [],
     };
-    renderWithProviders(<TestAppList testApps={testApps} />);
+    renderWithProviders(<TestAppList testApps={testApps} isLimitReached={false} />);
 
     expect(
         screen.queryByText('akeneo_connectivity.connection.connect.marketplace.test_apps.title')
@@ -85,7 +85,7 @@ test('it disabled the connect button when the user doesnt have the permission to
 
     renderWithProviders(
         <SecurityContext.Provider value={{isGranted}}>
-            <TestAppList testApps={testApps} />
+            <TestAppList testApps={testApps} isLimitReached={false} />
         </SecurityContext.Provider>
     );
 
@@ -95,4 +95,35 @@ test('it disabled the connect button when the user doesnt have the permission to
 
     connectButton.toHaveAttribute('disabled');
     connectButton.toHaveAttribute('aria-disabled', 'true');
+});
+
+test('it disabled the connect button and show a warning when the limit of connected app is reached', () => {
+    const isGranted = jest.fn(acl => {
+        if (acl === 'akeneo_connectivity_connection_manage_apps') {
+            return true;
+        }
+        return true;
+    });
+
+    const testApps = {
+        total: 1,
+        apps: [testApp1],
+    };
+
+    renderWithProviders(
+        <SecurityContext.Provider value={{isGranted}}>
+            <TestAppList testApps={testApps} isLimitReached={true} />
+        </SecurityContext.Provider>
+    );
+
+    expect(screen.queryByText('testApp1')).toBeInTheDocument();
+
+    const connectButton = expect(screen.getByText('akeneo_connectivity.connection.connect.marketplace.card.connect'));
+
+    connectButton.toHaveAttribute('disabled');
+    connectButton.toHaveAttribute('aria-disabled', 'true');
+
+    expect(
+        screen.queryByText('akeneo_connectivity.connection.connection.constraint.connections_number_limit_reached')
+    ).toBeInTheDocument();
 });
