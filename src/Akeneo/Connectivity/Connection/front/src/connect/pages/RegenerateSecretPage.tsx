ebf1e1fa2a7857eedfa49c372e42c1710f4174ef
Riff <riff.ventura@gmail.com>
2023-02-15 17:51:01 +0100

    CXP-1418: Added test for custom app secret regeneration hook

--- src/Akeneo/Connectivity/Connection/front/src/connect/pages/RegenerateSecretPage.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/connect/pages/RegenerateSecretPage.tsx
@@ -4,7 +4,7 @@ import {useHistory, useParams} from 'react-router';
 import {useTranslate} from '../../shared/translate';
 import {NotificationLevel, useNotify} from '../../shared/notify';
 import {useRouter} from '../../shared/router/use-router';
-import {useCustomAppRegenerateSecret} from '../hooks/use-custom-app-regenerate-secret';
+import {useRegenerateCustomAppSecret} from '../hooks/use-regenerate-custom-app-secret';
 import {useConnectedApp} from '../hooks/use-connected-app';
 import {RegenerateSecretConfirm} from '../components/CustomApps/RegenerateSecret/RegenerateSecretConfirm';
 import {RegenerateSecretNewCredentials} from '../components/CustomApps/RegenerateSecret/RegenerateSecretNewCredentials';
@@ -22,7 +22,7 @@ export const RegenerateSecretPage = () => {
 
     const {loading: connectedAppLoading, payload: connectedApp} = useConnectedApp(connectionCode);
 
-    const mutationRegenerateSecret = useCustomAppRegenerateSecret();
+    const mutationRegenerateSecret = useRegenerateCustomAppSecret();
 
     const handleRedirect = () => {
         history.push(
@@ -67,14 +67,14 @@ export const RegenerateSecretPage = () => {
             closeTitle={translate('pim_common.close')}
             illustration={<SettingsIllustration />}
         >
-            {step == 'confirm' && (
+            {step === 'confirm' && (
                 <RegenerateSecretConfirm
                     handleRedirect={handleRedirect}
                     handleRegenerate={handleRegenerate}
                     buttonDisabled={mutationRegenerateSecret.isLoading}
                 />
             )}
-            {step == 'new_credentials' && (
+            {step === 'new_credentials' && (
                 <RegenerateSecretNewCredentials
                     handleRedirect={handleRedirect}
                     clientId={connectedApp.id}
