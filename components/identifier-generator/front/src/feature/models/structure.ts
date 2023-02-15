2dc4e8d7f23c7ca178d8c7fac3a9f56d6e4da70f
Celine Morin <celine.morin@akeneo.com>
2023-02-15 14:45:27 +0100

    CPM-962: Add hook to retrieve list of available properties

--- components/identifier-generator/front/src/feature/models/structure.ts
+++ components/identifier-generator/front/src/feature/models/structure.ts
@@ -4,6 +4,7 @@ enum PROPERTY_NAMES {
   AUTO_NUMBER = 'auto_number',
   FREE_TEXT = 'free_text',
   FAMILY = 'family',
+  SIMPLE_SELECT = 'simple_select',
 }
 
 const ALLOWED_PROPERTY_NAMES = [PROPERTY_NAMES.FREE_TEXT, PROPERTY_NAMES.AUTO_NUMBER, PROPERTY_NAMES.FAMILY];
