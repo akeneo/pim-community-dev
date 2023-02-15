ceb14a16a7f4918726c99993d1fafd344b2fbbc2
Grégoire Houssard <ghoussard@protonmail.com>
2023-02-15 09:54:35 +0100

    RAB-1260: Rename listener for attribute used as label

--- tests/back/Pim/Structure/Specification/Bundle/EventSubscriber/CheckAttributeIsNotUsedAsLabelInAFamilyOnDeletionSubscriberSpec.php
+++ tests/back/Pim/Structure/Specification/Bundle/EventSubscriber/CheckAttributeIsNotUsedAsLabelInAFamilyOnDeletionSubscriberSpec.php
@@ -10,7 +10,7 @@ use Doctrine\DBAL\Statement;
 use Prophecy\Argument;
 use PhpSpec\ObjectBehavior;
 
-class CheckAttributeOnDeletionSubscriberSpec extends ObjectBehavior
+class CheckAttributeIsNotUsedAsLabelInAFamilyOnDeletionSubscriberSpec extends ObjectBehavior
 {
     public function let(
         Connection $dbConnection
