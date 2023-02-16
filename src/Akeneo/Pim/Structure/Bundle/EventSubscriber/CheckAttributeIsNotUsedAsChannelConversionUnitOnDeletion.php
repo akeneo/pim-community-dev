50ee56e140878d1dfd08ad8e503ebce1995ada70
Grégoire Houssard <ghoussard@protonmail.com>
2023-02-16 15:06:31 +0100

    RAB-1265: Fix lint

--- src/Akeneo/Pim/Structure/Bundle/EventSubscriber/CheckAttributeIsNotUsedAsChannelConversionUnitOnDeletion.php
+++ src/Akeneo/Pim/Structure/Bundle/EventSubscriber/CheckAttributeIsNotUsedAsChannelConversionUnitOnDeletion.php
@@ -25,11 +25,12 @@ class CheckAttributeIsNotUsedAsChannelConversionUnitOnDeletion implements EventS
     public static function getSubscribedEvents(): array
     {
         return [
-            StorageEvents::PRE_REMOVE => 'onPreRemove'
+            StorageEvents::PRE_REMOVE => 'onPreRemove',
         ];
     }
 
-    public function onPreRemove(RemoveEvent $event): void {
+    public function onPreRemove(RemoveEvent $event): void
+    {
         $attribute = $event->getSubject();
 
         if (!$attribute instanceof AttributeInterface) {
