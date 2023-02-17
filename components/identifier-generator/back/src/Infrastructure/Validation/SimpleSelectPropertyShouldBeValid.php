234c0970f0a1ba68f3d2d7f5e73264908041a762
Pierre Maraître <pierre.maraitre@akeneo.com>
2023-02-17 16:10:27 +0100

    CPM-959: Add Classes and validation to create simple select properties in identifier generators

--- components/identifier-generator/back/src/Infrastructure/Validation/SimpleSelectPropertyShouldBeValid.php
+++ components/identifier-generator/back/src/Infrastructure/Validation/SimpleSelectPropertyShouldBeValid.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraint;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class FamilyPropertyShouldBeValid extends Constraint
+final class SimpleSelectPropertyShouldBeValid extends Constraint
 {
-    public string $fieldsRequired = 'validation.identifier_generator.family_property_fields_required';
+    public string $fieldsRequired = 'validation.identifier_generator.simple_select_property_fields_required';
 }
