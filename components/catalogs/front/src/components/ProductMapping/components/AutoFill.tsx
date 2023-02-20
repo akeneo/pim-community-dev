e0696ea151bd0d28374923ee3aaa621e85d5636b
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-02-20 10:04:20 +0100

    add loading

--- components/catalogs/front/src/components/ProductMapping/components/AutoFill.tsx
+++ components/catalogs/front/src/components/ProductMapping/components/AutoFill.tsx
@@ -1,8 +1,16 @@
-import React, {useState} from 'react';
-import {Button, List, Modal, SelectInput, SettingsIllustration} from 'akeneo-design-system';
+import React, {useCallback, useState} from 'react';
+import {
+    Button,
+    Field,
+    List,
+    Locale as LocaleLabel,
+    Modal,
+    ProgressBar,
+    SelectInput,
+    SettingsIllustration
+} from 'akeneo-design-system';
 import {useTranslate} from '@akeneo-pim-community/shared';
 import styled from 'styled-components';
-import {Channel} from '../../../models/Channel';
 
 
 type Props = {
@@ -10,7 +18,7 @@ type Props = {
 };
 
 export type Step = {
-    name: 'catalogs' | 'scope_and_locale';
+    name: 'catalogs' | 'scope_and_locale' | 'autofill_loading';
 };
 
 const ListWrapper = styled.div`
@@ -18,11 +26,16 @@ const ListWrapper = styled.div`
     overflow-y: scroll;
 `;
 
+const DropdownField = styled(Field)`
+    margin-top: 10px;
+`;
+
 export const AutoFill = ({closeModal}: Props) => {
     const translate = useTranslate();
 
     const [step, setStep] = useState<Step>({name: 'catalogs'});
     const [selectedCatalogId, setSelectedCatalogId] = useState<Number | null>(null);
+    const [loadingPercentage, setLoadingPercentage] = useState<Number>(0);
 
     const catalogList = [
         {
@@ -39,25 +52,36 @@ export const AutoFill = ({closeModal}: Props) => {
         },
     ];
 
-    const channels = [
-        {
-            label : 'Ecommerce',
-            code : 'ecom',
-        },
-        {
-            label : 'Print',
-            code : 'print',
-        }
-    ];
-
     const selectCatalog = function (catalogId : number) {
-        console.log(catalogId);
         setSelectedCatalogId(catalogId)
     };
 
     const applyAutofill = function (catalogId : number) {
-        closeModal();
+        console.log('ouioui');
+        setStep({name:'autofill_loading'});
+        // startLoading()
     };
+    // const startLoading = function () {
+    //     console.log(loadingPercentage);
+    //     if (loadingPercentage > 100) {
+    //         return;
+    //     }
+    //     setTimeout(() => {
+    //         const percentage = parseInt(loadingPercentage.toString()) + 1;
+    //         console.log(percentage);
+    //         setLoadingPercentage(percentage);
+    //         startLoading();
+    //     }, 50);
+    // }
+    //
+    // const handleClick = useCallback(
+    //     (targetCode, source) => {
+    //         setSelectedTarget(targetCode);
+    //         setSelectedTargetLabel(productMappingSchema?.properties[targetCode]?.title ?? targetCode);
+    //         setSelectedSource(source);
+    //     },
+    //     [productMappingSchema]
+    // );
 
     return (
         <Modal onClose={closeModal} closeTitle={translate('pim_common.close')} illustration={<SettingsIllustration />}>
@@ -89,26 +113,64 @@ export const AutoFill = ({closeModal}: Props) => {
                     <p>Choose the default channel and scope for the attribute that will need one, if some association
                     are not possible, an error will appear upon saving</p>
 
-        {/*<SelectInput*/}
-        {/*    value={source.scope}*/}
-        {/*    onChange={newChannel => onChange({...source, scope: newChannel, locale: null})}*/}
-        {/*    onNextPage={fetchNextPage}*/}
-        {/*    clearable={false}*/}
-        {/*    invalid={error !== undefined}*/}
-        {/*    emptyResultLabel={translate('akeneo_catalogs.common.select.no_matches')}*/}
-        {/*    openLabel={translate('akeneo_catalogs.common.select.open')}*/}
-        {/*    placeholder={translate('akeneo_catalogs.product_mapping.source.parameters.channel.placeholder')}*/}
-        {/*    data-testid='source-parameter-channel-dropdown'*/}
-        {/*>*/}
-        {/*    {channels?.map(channel => (*/}
-        {/*        <SelectInput.Option key={channel.code} title={channel.label} value={channel.code}>*/}
-        {/*            {channel.label}*/}
-        {/*        </SelectInput.Option>*/}
-        {/*    ))}*/}
-        {/*</SelectInput>*/}
+                    <DropdownField label={translate('akeneo_catalogs.product_mapping.source.parameters.channel.label')}>
+                        <SelectInput
+                            onChange={() => null}
+                            value='ecommerce'
+                            clearable={false}
+                            invalid={undefined}
+                            emptyResultLabel={translate('akeneo_catalogs.common.select.no_matches')}
+                            openLabel={translate('akeneo_catalogs.common.select.open')}
+                            placeholder={translate('akeneo_catalogs.product_mapping.source.parameters.channel.placeholder')}
+                            data-testid='source-parameter-channel-dropdown'
+                        >
+                            <SelectInput.Option key='ecommerce' title='Ecommerce' value='ecommerce'>
+                                Ecommerce
+                            </SelectInput.Option>
+                        </SelectInput>
+                    </DropdownField>
+                    <DropdownField label={translate('akeneo_catalogs.product_mapping.source.parameters.locale.label')}>
+                        <SelectInput
+                            onChange={() => null}
+                            value='en-US'
+                            clearable={false}
+                            invalid={undefined}
+                            emptyResultLabel={translate('akeneo_catalogs.common.select.no_matches')}
+                            openLabel={translate('akeneo_catalogs.common.select.open')}
+                            placeholder={translate('akeneo_catalogs.product_mapping.source.parameters.channel.placeholder')}
+                            data-testid='source-parameter-channel-dropdown'
+                        >
+                            <SelectInput.Option key='en-US' title='English (United States)' value='en-US'>
+                                <LocaleLabel code='en-US' languageLabel='English (United States)' />
+                            </SelectInput.Option>
+                            <SelectInput.Option key='fr-Fr' title='French (France)' value='fr-Fr'>
+                                <LocaleLabel code='fr-Fr' languageLabel='French (France)' />
+                            </SelectInput.Option>
+                        </SelectInput>
+                    </DropdownField>
                     <Modal.BottomButtons>
-                        <Button level="primary" onClick={() => applyAutofill}>
-                            Autofill my mapping
+                        <Button level="tertiary" onClick={() => setStep({name:'catalogs'})}>
+                            Change the selected Catalog
+                        </Button>
+                        <Button level="primary" onClick={() => applyAutofill(1)}>
+                            Autofill
+                        </Button>
+                    </Modal.BottomButtons>
+                </>
+            }
+            { step.name == 'autofill_loading' &&
+                <>
+                    <p>Wait a moment while we are filling your mapping</p>
+                    <ProgressBar
+                        level="primary"
+                        // percent={parseInt(loadingPercentage.toString())}
+                        percent={30}
+                        progressLabel=""
+                        title=""
+                    />
+                    <Modal.BottomButtons>
+                        <Button level="primary" onClick={() => closeModal}>
+                            Done
                         </Button>
                     </Modal.BottomButtons>
                 </>
