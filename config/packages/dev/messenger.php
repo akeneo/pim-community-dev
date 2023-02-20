9d3d18913f27e62360d3c0b8584c19485181399b
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-20 18:19:47 +0100

    Add config builder

--- config/packages/dev/messenger.php
+++ config/packages/dev/messenger.php
@@ -2,39 +2,15 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Akeneo\Pim\Platform\Messaging\Domain\Config\MessengerConfigBuilder;
+use Akeneo\Pim\Platform\Messaging\Domain\Config\TransportType;
 use Symfony\Component\Yaml\Yaml;
-
-function createTransportConfig($queueName): array {
-    return [
-        'dsn' => 'doctrine://default',
-        'options' => [
-            'table_name' => 'messenger_messages',
-            'queue_name' => $queueName,
-            'redeliver_timeout' => 86400,
-            'auto_setup' => false,
-        ],
-        'serializer' => 'akeneo_batch_queue.messenger.serializer',
-    ];
-}
 
 return static function (ContainerConfigurator $containerConfigurator) {
     $messagingConfigs = Yaml::parse(file_get_contents(__DIR__ . '/../../messaging.yml'));
 
-    $transports = [];
-    $routing = [];
-
-    foreach ($messagingConfigs['queues'] as $name => $pimMessageConfig) {
-        $transportName = $name;
-        $transports[$transportName] = createTransportConfig($name);
-        $routing[$pimMessageConfig['messageClass']] = $transportName;
-    }
-
-    $config = [
-        'transports' => $transports,
-        'routing' => $routing,
-    ];
-
-    print_r($config);
+    $configBuilder = new MessengerConfigBuilder($containerConfigurator->env());
+    $config = $configBuilder->build($messagingConfigs, TransportType::DOCTRINE);
 
     $containerConfigurator->extension('framework', ['messenger' => $config]);
 };
