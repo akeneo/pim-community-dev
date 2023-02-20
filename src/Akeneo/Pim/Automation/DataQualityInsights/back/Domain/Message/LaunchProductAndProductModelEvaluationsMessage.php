149c37629aa2e0292a3021b9cb70932be3fd50ef
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-20 11:17:47 +0100

    Generic handler for all handlers

--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Domain/Message/LaunchProductAndProductModelEvaluationsMessage.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Domain/Message/LaunchProductAndProductModelEvaluationsMessage.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\DataQualityInsights\Domain\Message;
 
+use Akeneo\Pim\Platform\Messaging\Domain\PimMessageInterface;
+
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class LaunchProductAndProductModelEvaluationsMessage
+final class LaunchProductAndProductModelEvaluationsMessage implements PimMessageInterface
 {
     public function __construct(public readonly string $text)
     {
--- src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Handler/LaunchProductAndProductModelEvaluationsHandler.php
+++ src/Akeneo/Pim/Automation/DataQualityInsights/back/Infrastructure/Handler/LaunchProductAndProductModelEvaluationsHandler.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\DataQualityInsights\Infrastructure\Handler;
 
 use Akeneo\Pim\Automation\DataQualityInsights\Domain\Message\LaunchProductAndProductModelEvaluationsMessage;
-use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Akeneo\Pim\Platform\Messaging\Domain\PimMessageHandlerInterface;
+use Akeneo\Pim\Platform\Messaging\Domain\PimMessageInterface;
+use Webmozart\Assert\Assert;
 
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class LaunchProductAndProductModelEvaluationsHandler implements MessageHandlerInterface
+final class LaunchProductAndProductModelEvaluationsHandler implements PimMessageHandlerInterface
 {
-    public function __invoke(LaunchProductAndProductModelEvaluationsMessage $message)
+    public function __invoke(PimMessageInterface $message)
     {
+        Assert::isInstanceOf($message, LaunchProductAndProductModelEvaluationsMessage::class);
         print_r(get_class($this) . ': ' . $message->text . "\n");
     }
 }
