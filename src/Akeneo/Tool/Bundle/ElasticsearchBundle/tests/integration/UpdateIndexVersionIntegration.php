d86d642225fa2a118c0d3ed171345bd2dfcd6253
Steven VAIDIE <steven.vaidie@gmail.com>
2023-02-20 17:02:59 +0100

    RAB-1353: Add script to check elasticsearch index have been created with Elasticsearch 7

---
+++ src/Akeneo/Tool/Bundle/ElasticsearchBundle/tests/integration/UpdateIndexVersionIntegration.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Akeneo\Tool\Bundle\ElasticsearchBundle\tests\integration;
+
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\Integration\TestCase;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class UpdateIndexVersionIntegration extends TestCase
+{
+    public function testCommandResetsAllIndexes()
+    {
+        $productIndexNameBeforeMigration = $this->getProductAndProductModelIndexName();
+        $numberOfDocumentBeforeMigration = $this->getNumberOfIndexedProductAndProductModel();
+        $firstDocumentBeforeMigration = $this->getFirstProductAndProductModel();
+
+        $this->runUpdateIndexVersionCommand();
+
+        $productIndexNameAfterMigration = $this->getProductAndProductModelIndexName();
+        $numberOfDocumentAfterMigration = $this->getNumberOfIndexedProductAndProductModel();
+        $firstDocumentAfterMigration = $this->getFirstProductAndProductModel();
+
+        $this->assertNotEquals($productIndexNameBeforeMigration, $productIndexNameAfterMigration);
+        $this->assertEquals($numberOfDocumentBeforeMigration, $numberOfDocumentAfterMigration);
+        $this->assertEquals($firstDocumentBeforeMigration, $firstDocumentAfterMigration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useFunctionalCatalog('catalog_modeling');
+    }
+
+    private function runUpdateIndexVersionCommand(): void
+    {
+        $application = new Application(self::$kernel);
+        $command = $application->find('akeneo:elasticsearch:update-index-version');
+        $commandTester = new CommandTester($command);
+        $exitCode = $commandTester->execute([
+            'command' => $command->getName(),
+            'indices' => ['akeneo_pim_product_and_product_model']
+        ]);
+
+        $this->assertSame(0, $exitCode);
+    }
+
+    private function getClient(): Client
+    {
+        $clientBuilder = new ClientBuilder();
+        $clientBuilder->setHosts([$this->getParameter('index_hosts')]);
+
+        return $clientBuilder->build();
+    }
+
+    private function getProductAndProductModelIndexName(): string
+    {
+        $clientBuilder = new ClientBuilder();
+        $clientBuilder->setHosts([$this->getParameter('index_hosts')]);
+        $client = $clientBuilder->build();
+
+        $aliasConfiguration = $client->indices()->get(['index' => 'akeneo_pim_product_and_product_model']);
+        $indexNames = array_keys($aliasConfiguration);
+
+        if (count($indexNames) !== 1) {
+            throw new \Exception('There is multiple index behind the index alias or there is no alias behind the alias');
+        }
+
+        return $indexNames[0];
+    }
+
+    private function getNumberOfIndexedProductAndProductModel(): int
+    {
+        $response = $this->getClient()->count(['index' => 'akeneo_pim_product_and_product_model']);
+
+        return $response['count'];
+    }
+
+    private function getFirstProductAndProductModel()
+    {
+        $response = $this->getClient()->search(['index' => 'akeneo_pim_product_and_product_model', "size" => 1]);
+
+        return $response['hits']['hits'][0]['_source'];
+    }
+}
