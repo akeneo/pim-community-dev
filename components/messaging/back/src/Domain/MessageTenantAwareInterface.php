23e6b54fddb97445c820cf80323a62237ff1947c
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-21 12:02:35 +0100

    JEL-227: use one handler class for all messages

---
+++ components/messaging/back/src/Domain/MessageTenantAwareInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Platform\Messaging\Domain;
+
+use Akeneo\Tool\Component\Messenger\Tenant\TenantAwareInterface;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface MessageTenantAwareInterface extends TenantAwareInterface
+{
+
+}
