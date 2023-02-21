e37e470bdce822a465153b25539e52d92558797d
Laurent Petard <laurent.petard@akeneo.com>
2023-02-21 16:37:01 +0100

    Register handlers automatically

--- components/messaging/back/src/Infrastructure/Symfony/DependencyInjection/AkeneoMessagingExtension.php
+++ components/messaging/back/src/Infrastructure/Symfony/DependencyInjection/AkeneoMessagingExtension.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Platform\Messaging\Infrastructure\Symfony\DependencyInjection;
 
+use Akeneo\Pim\Platform\Messaging\Domain\MessageTenantAwareInterface;
 use Akeneo\Pim\Platform\Messaging\Infrastructure\MessageHandler;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
@@ -25,6 +27,22 @@ final class AkeneoMessagingExtension  extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-//        $container->setDefinition('a', new Definition(MessageHandler::class, [[]]))
+        // @fixme: Find a better way to load the config file
+        $messagingConfigs = Yaml::parse(file_get_contents(__DIR__ . '/../../../../../../../config/messaging.yml'));
+
+        // Register a handler for each consumer of each queue
+        foreach ($messagingConfigs['queues'] as $queueConfig) {
+            foreach ($queueConfig['consumers'] as $consumerConfig) {
+                $container->register('akeneo.messaging.handler.'.$consumerConfig['name'], MessageHandler::class)
+                    ->setArguments([
+                        new Reference('akeneo_batch_queue.messenger.serializer'),
+                        $consumerConfig['name'],
+                    ])
+                    ->addTag('messenger.message_handler', [
+                        'handles' => MessageTenantAwareInterface::class,
+                        'from_transport' => $consumerConfig['name'],
+                    ]);
+            }
+        }
     }
 }
