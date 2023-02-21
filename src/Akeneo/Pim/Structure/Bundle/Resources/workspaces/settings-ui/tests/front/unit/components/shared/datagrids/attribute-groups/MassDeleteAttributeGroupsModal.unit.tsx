306fbc1ce8edd75a1fdf472ea4da245a3bf4f347
anthony-grandin <anthony.grandin@getakeneo.com>
2023-02-21 15:38:13 +0100

    RAB-1294: test display of number of affected attributes

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/tests/front/unit/components/shared/datagrids/attribute-groups/MassDeleteAttributeGroupsModal.unit.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/tests/front/unit/components/shared/datagrids/attribute-groups/MassDeleteAttributeGroupsModal.unit.tsx
@@ -5,7 +5,7 @@ import {MassDeleteAttributeGroupsModal} from '../../../../../../../src';
 import {fireEvent, screen, act} from '@testing-library/react';
 
 test('it renders a confirm modal delete', () => {
-  renderWithProviders(<MassDeleteAttributeGroupsModal attributeGroups={[]} onConfirm={() => {}} />);
+  renderWithProviders(<MassDeleteAttributeGroupsModal selectedAttributeGroups={[]} unselectAttributeGroups={[]} onConfirm={() => {}} />);
 
   expect(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.button')).toBeInTheDocument();
 });
@@ -13,7 +13,11 @@ test('it renders a confirm modal delete', () => {
 test('it display number of attribute groups to delete', async () => {
   renderWithProviders(
     <MassDeleteAttributeGroupsModal
-      attributeGroups={[{code: 'attribute_group_1', labels: {}, sort_order: 1, is_dqi_activated: false}]}
+      selectedAttributeGroups={[
+        {code: 'attribute_group_1', labels: {}, sort_order: 1, is_dqi_activated: false, attribute_count: 0},
+      ]}
+      unselectAttributeGroups={[]}
+      onConfirm={() => {}}
       onConfirm={() => {}}
     />
   );
@@ -22,5 +26,28 @@ test('it display number of attribute groups to delete', async () => {
     fireEvent.click(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.button'));
   });
 
+  expect(
+    screen.queryByText('pim_enrich.entity.attribute_group.mass_delete.select_attribute_group')
+  ).not.toBeInTheDocument();
+  expect(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.confirm')).toBeInTheDocument();
+});
+
+test('it display number of attribute affected', async () => {
+  renderWithProviders(
+    <MassDeleteAttributeGroupsModal
+      selectedAttributeGroups={[
+        {code: 'attribute_group_1', labels: {}, sort_order: 1, is_dqi_activated: false, attribute_count: 4},
+        {code: 'attribute_group_2', labels: {}, sort_order: 2, is_dqi_activated: false, attribute_count: 5},
+      ]}
+      unselectAttributeGroups={[]}
+      onConfirm={() => {}}
+    />
+  );
+
+  await act(async () => {
+    fireEvent.click(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.button'));
+  });
+
+  expect(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.select_attribute_group')).toBeInTheDocument();
   expect(screen.getByText('pim_enrich.entity.attribute_group.mass_delete.confirm')).toBeInTheDocument();
 });
