eaaf44813af3f95620571cb206c86dbe5cf9025a
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-22 12:04:09 +0100

    JEL-227: add documentation

--- components/messaging/back/src/Domain/TenantAwareTrait.php
+++ components/messaging/back/src/Domain/TenantAwareTrait.php
@@ -8,7 +8,7 @@ namespace Akeneo\Pim\Platform\Messaging\Domain;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-trait TenantAware
+trait TenantAwareTrait
 {
     private ?string $tenantId = null;
 
