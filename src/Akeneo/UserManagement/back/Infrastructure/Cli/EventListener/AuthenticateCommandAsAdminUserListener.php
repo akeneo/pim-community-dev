d33d4c4d56bdc977023c1a00a1153953cd352903
Laurent Petard <laurent.petard@akeneo.com>
2023-02-27 17:45:30 +0100

    JEL-239: Optimization and renaming

--- src/Akeneo/UserManagement/back/Infrastructure/Cli/EventListener/AuthenticateCommandAsAdminUserListener.php
+++ src/Akeneo/UserManagement/back/Infrastructure/Cli/EventListener/AuthenticateCommandAsAdminUserListener.php
@@ -19,7 +19,7 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AuthenticateCommandAsAdminUser
+class AuthenticateCommandAsAdminUserListener
 {
     public function __construct(
         private readonly TokenStorageInterface $tokenStorage,
