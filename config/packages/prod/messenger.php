d174aeded0be3deadf30b8e5d80b359202921005
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-27 12:04:17 +0100

    JEL-227: add config for other environments

---
+++ config/packages/prod/messenger.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Akeneo\Tool\Bundle\MessengerBundle\Config\MessengerConfigBuilder;
+use Akeneo\Tool\Component\Messenger\Config\TransportType;
+
+return static function (ContainerConfigurator $containerConfigurator) {
+    $configBuilder = new MessengerConfigBuilder($containerConfigurator->env());
+    $config = $configBuilder->build(__DIR__ . '/../../..', TransportType::DOCTRINE);
+
+    $containerConfigurator->extension('framework', ['messenger' => $config]);
+};
