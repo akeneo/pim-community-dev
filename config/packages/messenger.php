6f2885c5ad78a11f157d166f3604c6a6530fee5c
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-02-28 17:22:02 +0100

    JEL-227: one config file for all

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -6,8 +6,14 @@ use Akeneo\Tool\Bundle\MessengerBundle\Config\MessengerConfigBuilder;
 use Akeneo\Tool\Component\Messenger\Config\TransportType;
 
 return static function (ContainerConfigurator $containerConfigurator) {
+    $transportType = match ($containerConfigurator->env()) {
+        'behat', 'test' => TransportType::PUB_SUB,
+        'test_fake' => TransportType::IN_MEMORY,
+        default => TransportType::DOCTRINE,
+    };
+
     $configBuilder = new MessengerConfigBuilder($containerConfigurator->env());
-    $config = $configBuilder->build(__DIR__ . '/../../..', TransportType::PUB_SUB);
+    $config = $configBuilder->build(__DIR__ . '/../..', $transportType);
 
     $containerConfigurator->extension('framework', ['messenger' => $config]);
 };
