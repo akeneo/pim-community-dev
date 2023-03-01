b84ab140080dba38108efd9ec5eeaa3178e80990
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-03-01 20:49:38 +0100

    JEL-227: add comment

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -5,6 +5,10 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use Akeneo\Tool\Bundle\MessengerBundle\Config\MessengerConfigBuilder;
 use Akeneo\Tool\Component\Messenger\Config\TransportType;
 
+/**
+ * The goal of this file is to generate Symfony Messenger config from
+ * the queues/consumers defined into config/messages*.yml
+ */
 return static function (ContainerConfigurator $containerConfigurator) {
     $transportType = match ($containerConfigurator->env()) {
         'behat', 'test' => TransportType::PUB_SUB,
