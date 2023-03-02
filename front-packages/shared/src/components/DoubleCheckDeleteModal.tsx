f0f5489ca176c897a17a03933c7bcafb4988e614
Steven VAIDIE <steven.vaidie@gmail.com>
2023-03-02 16:49:20 +0100

    RAB-1287: Move delete attribute groups error into a dedicated subscriber

--- front-packages/shared/src/components/DoubleCheckDeleteModal.tsx
+++ front-packages/shared/src/components/DoubleCheckDeleteModal.tsx
@@ -11,15 +11,16 @@ const DoubleCheckDeleteModal = ({
   children,
   doubleCheckInputLabel,
   textToCheck,
+  onConfirm,
   ...deleteModalProps
 }: DoubleCheckDeleteModalProps) => {
   const [value, setValue] = useState('');
   const canConfirmDelete = value === textToCheck;
 
   return (
-    <DeleteModal {...deleteModalProps} canConfirmDelete={canConfirmDelete}>
+    <DeleteModal {...deleteModalProps} canConfirmDelete={canConfirmDelete} onConfirm={onConfirm}>
       {children}
-      <TextField value={value} label={doubleCheckInputLabel} onChange={setValue} />
+      <TextField value={value} label={doubleCheckInputLabel} onChange={setValue} onSubmit={onConfirm} />
     </DeleteModal>
   );
 };
