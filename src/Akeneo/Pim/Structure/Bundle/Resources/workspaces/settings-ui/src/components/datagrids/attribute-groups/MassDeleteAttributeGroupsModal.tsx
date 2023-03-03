f3bd5debadfec53322645d31ea7a0217bcfd4fe6
Steven VAIDIE <steven.vaidie@gmail.com>
2023-03-03 10:21:45 +0100

    RAB-1287: Plurialization does not work

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/datagrids/attribute-groups/MassDeleteAttributeGroupsModal.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/datagrids/attribute-groups/MassDeleteAttributeGroupsModal.tsx
@@ -71,12 +71,12 @@ const MassDeleteAttributeGroupsModal = ({
                 <Helper level="error">
                   {translate('pim_enrich.entity.attribute_group.mass_delete.attribute_warning', {
                     number_of_attribute: numberOfAttribute,
-                  })}
+                  }, numberOfAttribute)}
                 </Helper>
                 <Field
                   label={translate('pim_enrich.entity.attribute_group.mass_delete.select_attribute_group', {
                     number_of_attribute: numberOfAttribute,
-                  })}
+                  }, numberOfAttribute)}
                 >
                   <SelectInput
                     emptyResultLabel={translate('pim_enrich.entity.attribute_group.mass_delete.empty_result_label')}
