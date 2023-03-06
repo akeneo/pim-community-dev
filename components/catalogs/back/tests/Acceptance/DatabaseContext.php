ce44bf35f5363c7aec09cc6eba8a91529ac6e575
Guillaume Fer <guillaume.fer@akeneo.com>
2023-03-06 15:23:27 +0100

    CXP-1374: fix flaky tests

--- components/catalogs/back/tests/Acceptance/DatabaseContext.php
+++ components/catalogs/back/tests/Acceptance/DatabaseContext.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Akeneo\Catalogs\Test\Acceptance;
 
 use Behat\Behat\Context\Context;
+use League\Flysystem\FileAttributes;
+use League\Flysystem\Filesystem;
+use League\Flysystem\StorageAttributes;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -27,9 +30,26 @@ class DatabaseContext implements Context
      */
     public function loadFixtures(): void
     {
+        $this->resetCatalogMappingFilesystem();
         $catalog = $this->container->get('akeneo_integration_tests.catalogs');
         $configuration = $catalog->useMinimalCatalog();
         $fixturesLoader = $this->container->get('akeneo_integration_tests.loader.fixtures_loader');
         $fixturesLoader->load($configuration);
+    }
+
+    private function resetCatalogMappingFilesystem(): void
+    {
+        /** @var Filesystem $catalogMappingFilesystem */
+        $catalogMappingFilesystem = $this->container->get('oneup_flysystem.catalogs_mapping_filesystem');
+
+        $paths = $catalogMappingFilesystem->listContents('/')->filter(
+            fn(StorageAttributes $attributes): bool => $attributes instanceof FileAttributes,
+        )->map(
+            fn(FileAttributes $attributes): string => $attributes->path(),
+        );
+
+        foreach ($paths as $path) {
+            $catalogMappingFilesystem->delete($path);
+        }
     }
 }
