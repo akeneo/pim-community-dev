ee9d95bd8af189f18a57f0e6d0b2fdae4b60717e
Grégoire Houssard <ghoussard@protonmail.com>
2023-03-07 12:21:00 +0100

    RAB-1255: Check ACL in launch job endpoint

--- src/Akeneo/Pim/Structure/Bundle/Controller/InternalApi/MassDeleteAttributeController.php
+++ src/Akeneo/Pim/Structure/Bundle/Controller/InternalApi/MassDeleteAttributeController.php
@@ -9,11 +9,13 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Structure\Bundle\Controller\InternalApi;
 
+use Akeneo\Platform\Bundle\FrameworkBundle\Security\SecurityFacadeInterface;
 use Akeneo\Tool\Bundle\BatchBundle\Launcher\JobLauncherInterface;
 use Akeneo\Tool\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -23,11 +25,16 @@ class MassDeleteAttributeController
         private TokenStorageInterface $tokenStorage,
         private JobLauncherInterface $jobLauncher,
         private IdentifiableObjectRepositoryInterface $jobInstanceRepository,
+        private SecurityFacadeInterface $securityFacade,
     ) {
     }
 
     public function launchAction(Request $request): Response
     {
+        if (!$this->securityFacade->isGranted('pim_enrich_attribute_mass_delete')) {
+            throw new AccessDeniedHttpException();
+        }
+
         $jobInstance = $this->jobInstanceRepository->findOneByIdentifier('delete_attributes');
         $user = $this->tokenStorage->getToken()->getUser();
         if (!$user instanceof UserInterface) {
