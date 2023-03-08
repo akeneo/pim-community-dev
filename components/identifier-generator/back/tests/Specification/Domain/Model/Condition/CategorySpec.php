81a3a82b8680b8688659301d0e4eb8cfa9ec3a6c
Benjamin Letellier <letellier.benjamin@gmail.com>
2023-03-08 15:10:35 +0100

    CPM-968: add validators
    CPM-968: declare validator

--- components/identifier-generator/back/tests/Specification/Domain/Model/Condition/CategorySpec.php
+++ components/identifier-generator/back/tests/Specification/Domain/Model/Condition/CategorySpec.php
@@ -24,7 +24,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldBeAnInstanceOf(Category::class);
     }
 
-    public function it_cant_be_instanciated_if_type_is_not_category()
+    public function it_cant_be_instanciated_if_type_is_not_category(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'bad',
@@ -34,7 +34,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_no_operator_is_defined()
+    public function it_cant_be_instanciated_if_no_operator_is_defined(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -43,7 +43,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_operator_is_not_a_string()
+    public function it_cant_be_instanciated_if_operator_is_not_a_string(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -53,7 +53,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_operator_is_unknown()
+    public function it_cant_be_instanciated_if_operator_is_unknown(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -63,7 +63,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_value_is_not_defined()
+    public function it_cant_be_instanciated_if_value_is_not_defined(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -72,7 +72,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_value_is_not_an_array_of_strings()
+    public function it_cant_be_instanciated_if_value_is_not_an_array_of_strings(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -82,7 +82,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_cant_be_instanciated_if_value_is_empty()
+    public function it_cant_be_instanciated_if_value_is_empty(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -92,7 +92,7 @@ class CategorySpec extends ObjectBehavior
         $this->shouldThrow(\InvalidArgumentException::class)->duringInstantiation();
     }
 
-    public function it_can_be_normalized_with_value_and_in_operator()
+    public function it_can_be_normalized_with_value_and_in_operator(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
@@ -102,11 +102,11 @@ class CategorySpec extends ObjectBehavior
         $this->normalize()->shouldReturn([
             'type' => 'category',
             'operator' => 'IN',
-            'value' => ['pants', 'shoes']
+            'value' => ['pants', 'shoes'],
         ]);
     }
 
-    public function it_can_be_normalized_without_value_and_classified_operator()
+    public function it_can_be_normalized_without_value_and_classified_operator(): void
     {
         $this->beConstructedThrough('fromNormalized', [[
             'type' => 'category',
