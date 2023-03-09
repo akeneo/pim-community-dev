eb0cac9d1fb218360f18553a7e054c7d862f5f06
Celine Morin <celine.morin@akeneo.com>
2023-03-09 15:36:24 +0100

    CPM-963: Revert sanitize in front

--- components/identifier-generator/front/src/feature/hooks/useSaveGenerator.ts
+++ components/identifier-generator/front/src/feature/hooks/useSaveGenerator.ts
@@ -10,28 +10,17 @@ type HookResponse = {
   error: Violation[];
 };
 
-const sanitizeValues = (generator: IdentifierGenerator) => {
-  const sanitizedStructure = generator.structure.map(property => {
-    const newObject = {...property};
-    if ('locale' in newObject && newObject.locale === null) delete newObject.locale;
-    if ('scope' in newObject && newObject.scope === null) delete newObject.scope;
-    return newObject;
-  });
-  return {...generator, structure: sanitizedStructure};
-};
-
 const useSaveGenerator = (): HookResponse => {
   const router = useRouter();
 
   const callSave = async (generator: IdentifierGenerator) => {
-    const sanitizedGenerator = sanitizeValues(generator);
     const res = await fetch(router.generate('akeneo_identifier_generator_rest_update', {code: generator.code}), {
       method: 'PATCH',
       headers: [
         ['Content-type', 'application/json'],
         ['X-Requested-With', 'XMLHttpRequest'],
       ],
-      body: JSON.stringify(sanitizedGenerator),
+      body: JSON.stringify(generator),
     });
     const data = await res.json();
 
