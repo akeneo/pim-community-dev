276e40b2633d945db4aefb0a4dfdf40f52162a0b
Steven VAIDIE <steven.vaidie@gmail.com>
2023-03-14 10:28:16 +0100

    RAB-1300: Disable create button when user hit the atribute group limit

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/buttons/AttributeGroupsCreateButton.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/components/buttons/AttributeGroupsCreateButton.tsx
@@ -1,8 +1,36 @@
-import React, {FC} from 'react';
-import {PimView} from '@akeneo-pim-community/shared';
+import React from 'react';
+import {Button} from "akeneo-design-system";
+import {useRouter, useSecurity, useTranslate} from '@akeneo-pim-community/shared';
 
-const AttributeGroupsCreateButton: FC = () => {
-  return <PimView viewName="pim-attribute-group-index-create-button" />;
+const MAX_ATTRIBUTE_GROUPS = 50;
+
+type AttributeGroupsCreateButtonProps = {
+  attributeGroupCount: number;
+}
+
+const AttributeGroupsCreateButton = ({attributeGroupCount}: AttributeGroupsCreateButtonProps) => {
+  const {isGranted} = useSecurity();
+  const router = useRouter();
+  const translate = useTranslate();
+  const handleCreateAttributeGroup = () => {
+    router.redirectToRoute('pim_enrich_attributegroup_create');
+  };
+
+  const limitIsReached = MAX_ATTRIBUTE_GROUPS <= attributeGroupCount;
+  if (!isGranted('pim_enrich_attributegroup_create')) {
+    return null;
+  }
+
+  return (
+    <Button
+      onClick={handleCreateAttributeGroup}
+      level="primary"
+      disabled={limitIsReached}
+      title={limitIsReached ? translate('pim_enrich.entity.attribute_group.limit_reached', {max: MAX_ATTRIBUTE_GROUPS}) : undefined}
+    >
+      {translate('pim_common.create')}
+    </Button>
+  );
 };
 
 export {AttributeGroupsCreateButton};
