ee0ce9b7da89192955a053d2a11135a04340f011
Steven VAIDIE <steven.vaidie@gmail.com>
2023-03-15 12:44:21 +0100

    RAB-1301: Remove usless components

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/hooks/attribute-groups/index.ts
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/hooks/attribute-groups/index.ts
@@ -1,6 +1,3 @@
-export * from './useAttributeGroupsIndexState';
-export * from './useAttributeGroupPermissions';
 export * from './useAttributeGroups';
 export * from './useMassDeleteAttributeGroups';
-export * from './useFilteredAttributeGroups';
 export * from './useRedirectToAttributeGroup';
