40632e9e5addf3b58ec5eae98b79a9e8543a0d81
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-03-15 15:05:31 +0100

    fix-clean-ec-feature-flag: fix front unit test

--- make-file/enrichment-category.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: enrichment-category-acceptance-back
-enrichment-category-acceptance-back: var/tests/behat/enrichment-category
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/create_a_category.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/edit_a_category.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/export_categories_csv.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/export_categories_xlsx.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/import_categories.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/list_categories.feature
-	APP_ENV=behat $(PHP_RUN) vendor/bin/behat --config behat.yml -p legacy tests/legacy/features/pim/enrichment/category/remove_a_category.feature
