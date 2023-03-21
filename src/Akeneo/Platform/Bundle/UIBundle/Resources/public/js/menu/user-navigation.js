c483b3e26d1c9f076e86b4f6ffcaeb94b4448c00
Bryan Stéphan <bryan.stephan@akeneo.com>
2023-03-21 14:48:56 +0100

    GROWTH: POC segement consent manager

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/menu/user-navigation.js
@@ -62,6 +62,7 @@ define([
           logoutLabel: __(this.config.logout),
           userAccountLabel: __(this.config.userAccount),
           freeTrialEnabled: FeatureFlags.isEnabled('free_trial'),
+          segmentEnabled: FeatureFlags.isEnabled('segment_integration'),
         })
       );
 
