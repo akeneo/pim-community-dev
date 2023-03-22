f80044064f2d490c12d2462d5d686dc1b130597f
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2023-03-22 08:54:04 +0100

    CXP-1412: Fix copyright date

--- components/catalogs/back/tests/Integration/Infrastructure/Persistence/Category/GetProductCategoriesLabelsQueryTest.php
+++ components/catalogs/back/tests/Integration/Infrastructure/Persistence/Category/GetProductCategoriesLabelsQueryTest.php
@@ -10,7 +10,7 @@ use Akeneo\Pim\Enrichment\Product\API\Command\UserIntent\SetCategories;
 use Ramsey\Uuid\Uuid;
 
 /**
- * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
  * @covers \Akeneo\Catalogs\Infrastructure\Persistence\Category\GetProductCategoriesLabelsQuery
