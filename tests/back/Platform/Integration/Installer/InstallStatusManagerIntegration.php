2aa3bbe8745709dd1e024cd74681cf3750d346b3
jmleroux <jmleroux.pro@gmail.com>
2023-03-27 13:58:54 +0200

    PH-908: Use pim_configuration install data to check database installed

--- tests/back/Platform/Integration/Installer/InstallStatusManagerIntegration.php
+++ tests/back/Platform/Integration/Installer/InstallStatusManagerIntegration.php
@@ -2,17 +2,41 @@
 
 namespace AkeneoTest\Platform\Integration\Installer;
 
+use Akeneo\Platform\Bundle\InstallerBundle\Persistence\Sql\InstallData;
 use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 
+/**
+ * @author  JM Leroux <jmleroux.pro@gmail.com>
+ * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class InstallStatusManagerIntegration extends TestCase
 {
     public function testGetPimInstallTimestamp()
     {
+        $intallDataQuery = $this->get(InstallData::class);
+        $intallDataQuery->withDatetime(new \DateTimeImmutable('2022-12-13'));
+
         $installStatusManager = $this->get('pim_installer.install_status_manager');
         $timestamp = $installStatusManager->getPimInstallDateTime();
         $this->assertNotNull($timestamp);
         $this->assertInstanceOf(\DateTime::class, $timestamp);
+    }
+
+    public function test_pim_installed()
+    {
+        $intallDataQuery = $this->get(InstallData::class);
+        $intallDataQuery->withDatetime(new \DateTimeImmutable('2022-12-13'));
+
+        $installStatusManager = $this->get('pim_installer.install_status_manager');
+        $this->assertTrue($installStatusManager->isPimInstalled());
+    }
+
+    public function test_pim_not_installed()
+    {
+        $installStatusManager = $this->get('pim_installer.install_status_manager');
+        $this->assertFalse($installStatusManager->isPimInstalled());
     }
 
     /**
