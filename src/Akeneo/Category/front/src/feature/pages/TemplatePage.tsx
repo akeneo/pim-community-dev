cf48518f86453e19c15438a720f8cc9464639a58
Etienne Kerbastard <42570470+etiennekerbastard@users.noreply.github.com>
2023-03-29 17:30:18 +0200

    GRF-794: No template attributes (#19516)
    * GRF-794: no attributes on template page

--- src/Akeneo/Category/front/src/feature/pages/TemplatePage.tsx
+++ src/Akeneo/Category/front/src/feature/pages/TemplatePage.tsx
@@ -15,6 +15,7 @@ import {cloneDeep, set} from 'lodash/fp';
 import {FC, useCallback, useEffect, useState} from 'react';
 import {useParams} from 'react-router';
 import {EditTemplateAttributesForm} from '../components/templates/EditTemplateAttributesForm';
+import {NoTemplateAttributeOnTemplatePage} from '../components/templates/NoTemplateAttributeOnTemplatePage';
 import {EditTemplatePropertiesForm} from '../components/templates/EditTemplatePropertiesForm';
 import {TemplateOtherActions} from '../components/templates/TemplateOtherActions';
 import {useCategoryTree, useTemplateByTemplateUuid} from '../hooks';
@@ -100,6 +101,11 @@ const TemplatePage: FC = () => {
 
   const [isDeactivateTemplateModelOpen, openDeactivateTemplateModal, closeDeactivateTemplateModal] = useBooleanState();
 
+  const templateHasAttribute = () =>
+  {
+    return templateEdited?.attributes.length != 0;
+  }
+
   return (
     <>
       <PageHeader>
@@ -150,7 +156,11 @@ const TemplatePage: FC = () => {
           </TabBar.Tab>
         </TabBar>
 
-        {isCurrent(Tabs.ATTRIBUTE) && tree && templateEdited && (
+        {isCurrent(Tabs.ATTRIBUTE) && tree && templateEdited && !templateHasAttribute() && (
+            <NoTemplateAttributeOnTemplatePage templateId={templateEdited.uuid}/>
+        )}
+
+        {isCurrent(Tabs.ATTRIBUTE) && tree && templateEdited && templateHasAttribute() && (
           <EditTemplateAttributesForm attributes={templateEdited.attributes} templateId={templateEdited.uuid} />
         )}
 
