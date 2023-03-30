744477bb5b90b52c6635db6735477123573a1d76
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-03-30 15:59:21 +0200

    RAB-1331: Remove useless datagrid

--- front-packages/shared/src/components/page/SandboxHelper.tsx
+++ front-packages/shared/src/components/page/SandboxHelper.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Helper, InfoIcon, Link, getColor} from 'akeneo-design-system';
+import {Helper, InfoIcon, getColor} from 'akeneo-design-system';
 import {useFeatureFlags, useTranslate} from '../../hooks';
 
-const HELPER_LINK = 'https://docs.akeneo.com/latest/cloud_edition/serenity_mode/index.html';
 const HELPER_BACKGROUND_COLOR = '#5e63b6';
 
-const StyledInfoIcon = styled(InfoIcon)`
+const WhiteInfoIcon = styled(InfoIcon)`
   color: ${getColor('white')};
 `;
 
-const StyledHelper = styled(Helper)`
+const DarkBlueHelper = styled(Helper)`
   background-color: ${HELPER_BACKGROUND_COLOR};
   color: ${getColor('white')};
-
-  a,
-  a:hover {
-    color: ${getColor('white')};
-  }
 `;
 
 const SandboxHelper = () => {
@@ -29,12 +23,9 @@ const SandboxHelper = () => {
   }
 
   return (
-    <StyledHelper level="info" icon={<StyledInfoIcon />}>
-      {translate('pim_system.sandbox.helper.label')}&nbsp;
-      <Link href={HELPER_LINK} target="_blank">
-        {translate('pim_system.sandbox.helper.link')}
-      </Link>
-    </StyledHelper>
+    <DarkBlueHelper level="info" icon={<WhiteInfoIcon />}>
+      {translate('pim_system.sandbox.helper.text')}&nbsp;
+    </DarkBlueHelper>
   );
 };
 
