ec19f1e79a86e62de3e43aaed06ff329d9d01fc7
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-03-31 09:43:15 +0200

    RAB-1331: Add featureFlags context value

--- components/identifier-generator/front/src/feature/tests/test-utils.tsx
+++ components/identifier-generator/front/src/feature/tests/test-utils.tsx
@@ -20,7 +20,9 @@ const AllTheProviders: FC<{children: React.ReactNode}> = ({children}) => {
 
   return (
     <ThemeProvider theme={pimTheme}>
-      <DependenciesContext.Provider value={{translate: k => k, security: {isGranted: () => true}}}>
+      <DependenciesContext.Provider
+        value={{translate: k => k, security: {isGranted: () => true}, featureFlags: {isEnabled: () => true}}}
+      >
         <QueryClientProvider client={queryClient}>
           <IdentifierGeneratorContextProvider>
             <IdentifierGeneratorAclContextProvider>{children}</IdentifierGeneratorAclContextProvider>
