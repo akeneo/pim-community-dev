d9e12c9e18e26df747a54f8807e0acd73ac2e122
amigaire <adrien.migaire@akeneo.com>
2023-04-05 16:58:41 +0200

    GRF-793 clean debug logs and improve code

--- src/Akeneo/Category/front/src/feature/components/templates/AttributeSettings.tsx
+++ src/Akeneo/Category/front/src/feature/components/templates/AttributeSettings.tsx
@@ -18,10 +18,10 @@ export const AttributeSettings = ({attribute}: Props) => {
           {attribute.labels[catalogLocale]} {translate('akeneo.category.template.attribute.settings.title')}
         </SectionTitle.Title>
       </SectionTitle>
-      <Footer sticky={0}>
-        <DeleteButton level="danger" ghost>
+      <Footer>
+        <Button level="danger" ghost>
           {translate('akeneo.category.template.attribute.delete_button')}
-        </DeleteButton>
+        </Button>
       </Footer>
     </SettingsContainer>
   );
@@ -35,17 +35,12 @@ const SettingsContainer = styled.div`
   overflow-y: auto;
 `;
 
-const DeleteButton = styled(Button)`
-  float: right;
-`;
-
-const Footer = styled(SectionTitle)`
-  border-bottom: 0;
-  padding: 10px 0 20px;
+const Footer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  padding: 5px 0 5px;
   margin-top: 2px;
-  justify-content: flex-end;
   position: sticky;
   bottom: 0;
   background-color: #ffffff;
-  z-index: 10;
 `;
