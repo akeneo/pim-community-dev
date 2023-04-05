fb7c4fdc9c9a19283ebc005d7f0062a60103231c
Helene Gonzalez <helene.gonzalez@akeneo.com>
2023-04-05 16:07:25 +0200

    CXP-1410: fix wrong type in extractor

--- components/catalogs/back/src/Application/Mapping/ValueExtractor/Extractor/StringUri/StringFromUriFromAssetCollectionMediaLinkAttributeValueExtractor.php
+++ components/catalogs/back/src/Application/Mapping/ValueExtractor/Extractor/StringUri/StringFromUriFromAssetCollectionMediaLinkAttributeValueExtractor.php
@@ -25,11 +25,11 @@ class StringFromUriFromAssetCollectionMediaLinkAttributeValueExtractor implement
 
     public function getSupportedTargetType(): string
     {
-        return self::TARGET_TYPE_STRING;
+        return self::TARGET_TYPE_ARRAY_OF_STRING_URIS;
     }
 
     public function getSupportedTargetFormat(): ?string
     {
-        return self::TARGET_FORMAT_URI;
+        return null;
     }
 }
