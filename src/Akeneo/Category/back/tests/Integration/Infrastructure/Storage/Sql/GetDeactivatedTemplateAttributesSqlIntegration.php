2793ab057715adbd800e8a9c3de5b60713073bf9
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2023-04-11 16:46:18 +0200

    GRF-782: Service for retrieving deactivated attributes

---
+++ src/Akeneo/Category/back/tests/Integration/Infrastructure/Storage/Sql/GetDeactivatedTemplateAttributesSqlIntegration.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Category\back\tests\Integration\Infrastructure\Storage\Sql;
+
+use Akeneo\Category\back\tests\Integration\Helper\CategoryTestCase;
+use Akeneo\Category\Domain\Query\GetDeactivatedTemplateAttributes;
+use Akeneo\Category\Application\Query\GetAttribute;
+use Akeneo\Category\Domain\ValueObject\Template\TemplateUuid;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GetDeactivatedTemplateAttributesSqlIntegration extends CategoryTestCase
+{
+    public const TEMPLATE_UUID = '8a2ee006-0459-42e3-a8fc-07d32b547b95';
+
+    public function testGetAllDeactivatedTemplateAttributes(): void
+    {
+        $this->useTemplateFunctionalCatalog(self::TEMPLATE_UUID, 'test');
+
+        $getDeactivatedTemplateAttributes = $this->get(GetDeactivatedTemplateAttributes::class);
+        $deactivatedAttributes = $getDeactivatedTemplateAttributes->execute();
+        $this->assertEmpty($deactivatedAttributes);
+
+        $getAttributes = $this->get(GetAttribute::class);
+        $attributes = $getAttributes->byTemplateUuid(TemplateUuid::fromString(self::TEMPLATE_UUID))->getAttributes();
+        $firstAttributeUuid = $attributes[0]->normalize()['uuid'];
+        $this->deactivateAttribute($firstAttributeUuid);
+
+        $deactivatedAttributes = $getDeactivatedTemplateAttributes->execute();
+        $this->assertArrayHasKey($firstAttributeUuid, $deactivatedAttributes);
+    }
+}
