966df7c711c977e928da2c3bb0e6d121ffda2e42
Bryan Stéphan <bryan.stephan@akeneo.com>
2023-04-11 17:56:37 +0200

    GROWTH-71: Remove feature from non executed phpspecs script

--- .circleci/find_non_executed_phpspec.sh
+++ .circleci/find_non_executed_phpspec.sh
@@ -7,7 +7,7 @@ EXISTING_PHPSPEC_FILE=$(mktemp --suffix "existing")
 cat var/tests/phpspec/specs.xml | grep -Po '(?<=classname\=").*(?=\" status)' | sort | uniq > $EXECUTED_PHPSPEC_FILE
 
 if [ -d components ]; then
-    find src tests components -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -not -path "components/performance-analytics/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
+    find src tests components -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -not -path "components/performance-analytics/*" -not -path "components/customer-data-platform/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
 else
     find src tests -not -path "src/Akeneo/ReferenceEntity/*" -not -path "src/Akeneo/AssetManager/*" -name "*Spec.php" -type f | xargs grep "^namespace" |  tr "/" "\n" | grep namespace | sed -e 's/\([^/:]\+\):\([^:;]\+\);$/\2\\\1/' | cut -d " " -f 2 | cut -d "." -f 1 | sort | uniq > $EXISTING_PHPSPEC_FILE
 fi
