e5d4173055b7e601628a6ac02d82f4dc6f12d39d
amigaire <adrien.migaire@akeneo.com>
2023-04-13 17:51:41 +0200

    Update src/Akeneo/Category/back/Infrastructure/Controller/InternalApi/GetActivatedLocalesController.php
    Co-authored-by: Paul <pchasle@users.noreply.github.com>

--- src/Akeneo/Category/back/Infrastructure/Controller/InternalApi/GetActivatedLocalesController.php
+++ src/Akeneo/Category/back/Infrastructure/Controller/InternalApi/GetActivatedLocalesController.php
@@ -13,14 +13,14 @@ use Symfony\Component\HttpFoundation\Response;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class GetActivatedLocalesController
+final class GetActivatedLocalesController
 {
     public function __construct(
         private readonly FindLocales $findLocales,
     ) {
     }
 
-    public function __invoke()
+    public function __invoke(): JsonResponse
     {
         $locales = $this->findLocales->findAllActivated();
         $localeCodes = array_map(fn (Locale $locale) => $locale->getCode(), $locales);
