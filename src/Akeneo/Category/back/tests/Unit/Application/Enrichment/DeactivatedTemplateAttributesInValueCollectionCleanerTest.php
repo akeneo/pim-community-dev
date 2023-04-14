3bf6c793eaa843a80dec34fb121539b9f50190d3
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2023-04-14 15:26:53 +0200

    GRF-782: Add test case

--- src/Akeneo/Category/back/tests/Unit/Application/Enrichment/DeactivatedTemplateAttributesInValueCollectionCleanerTest.php
+++ src/Akeneo/Category/back/tests/Unit/Application/Enrichment/DeactivatedTemplateAttributesInValueCollectionCleanerTest.php
@@ -38,4 +38,27 @@ class DeactivatedTemplateAttributesInValueCollectionCleanerTest extends Category
 
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testItRemovesNothingFromValueCollectionWhenNoDeactivatedAttributes(): void
+    {
+        $deactivatedAttributes = [];
+        $rawCategory = [
+            'id' => '1',
+            'code' => 'master',
+            'value_collection' => '{"photo|8587cda6-58c8-47fa-9278-033e1d8c735c":{"data":{"size":168107,"extension":"jpg","file_path":"shoes.jpg","mime_type":"jpeg","original_filename":"shoes.jpg"},"type":"image","locale":null,"channel":null,"attribute_code":"photo|8587cda6-58c8-47fa-9278-033e1d8c735c"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|en_US":{"data":"All the shoes you need!","type":"text","locale":"en_US","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|fr_FR":{"data":"Les chaussures dont vous avez besoin !","type":"text","locale":"fr_FR","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"}}',
+        ];
+        $expected = [
+            'id' => '1',
+            'code' => 'master',
+            'value_collection' => '{"photo|8587cda6-58c8-47fa-9278-033e1d8c735c":{"data":{"size":168107,"extension":"jpg","file_path":"shoes.jpg","mime_type":"jpeg","original_filename":"shoes.jpg"},"type":"image","locale":null,"channel":null,"attribute_code":"photo|8587cda6-58c8-47fa-9278-033e1d8c735c"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|en_US":{"data":"All the shoes you need!","type":"text","locale":"en_US","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|fr_FR":{"data":"Les chaussures dont vous avez besoin !","type":"text","locale":"fr_FR","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"}}',
+        ];
+
+        $deactivatedAttributesCleaner = $this->get(DeactivatedTemplateAttributesInValueCollectionCleaner::class);
+        $result = ($deactivatedAttributesCleaner)($deactivatedAttributes, $rawCategory);
+
+        $this->assertEquals($expected, $result);
+    }
 }
