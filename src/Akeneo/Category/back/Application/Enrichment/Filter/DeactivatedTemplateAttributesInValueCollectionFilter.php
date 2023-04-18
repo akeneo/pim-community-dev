1af4cb720abdfd60e97fe1b484bfeccd3fbba086
etienne-kerbastard <etienne.kerbastard@akeneo.com>
2023-04-18 12:39:27 +0200

    GRF-782: PR comments

--- src/Akeneo/Category/back/Application/Enrichment/Filter/DeactivatedTemplateAttributesInValueCollectionFilter.php
+++ src/Akeneo/Category/back/Application/Enrichment/Filter/DeactivatedTemplateAttributesInValueCollectionFilter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Akeneo\Category\Application\Enrichment;
+namespace Akeneo\Category\Application\Enrichment\Filter;
 
 use Akeneo\Category\Domain\Query\DeactivatedTemplateAttributes\DeactivatedTemplateAttributeIdentifier;
 use Akeneo\Category\Domain\ValueObject\Attribute\Value\AbstractValue;
@@ -11,7 +11,7 @@ use Akeneo\Category\Domain\ValueObject\Attribute\Value\AbstractValue;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DeactivatedTemplateAttributesInValueCollectionCleaner
+class DeactivatedTemplateAttributesInValueCollectionFilter
 {
     /**
      * @param array<DeactivatedTemplateAttributeIdentifier> $deactivatedAttributes
@@ -26,21 +26,17 @@ class DeactivatedTemplateAttributesInValueCollectionCleaner
         if (empty($deactivatedAttributes) || empty($rawCategory['value_collection'])) {
             return $rawCategory;
         }
-        $decodedRawValueCollection = json_decode(
-            $rawCategory['value_collection'],
-            true,
-            512,
-            JSON_THROW_ON_ERROR,
-        );
+
+        $valueCollection = json_decode($rawCategory['value_collection'], true, 512, JSON_THROW_ON_ERROR);
+
         foreach ($deactivatedAttributes as $deactivatedAttribute) {
             $attributeCode = $deactivatedAttribute->code.AbstractValue::SEPARATOR.$deactivatedAttribute->uuid;
-            foreach ($decodedRawValueCollection as $key => $rawValue) {
-                if ($rawValue['attribute_code'] === $attributeCode) {
-                    unset($decodedRawValueCollection[$key]);
-                }
-            }
+            $valueCollection = array_filter($valueCollection, static function ($rawValue) use ($attributeCode) {
+                return $rawValue['attribute_code'] !== $attributeCode;
+            });
         }
-        $rawCategory['value_collection'] = json_encode($decodedRawValueCollection, JSON_THROW_ON_ERROR);
+
+        $rawCategory['value_collection'] = json_encode($valueCollection, JSON_THROW_ON_ERROR);
 
         return $rawCategory;
     }
--- src/Akeneo/Category/back/tests/Unit/Application/Enrichment/DeactivatedTemplateAttributesInValueCollectionCleanerTest.php
+++ src/Akeneo/Category/back/tests/Unit/Application/Enrichment/DeactivatedTemplateAttributesInValueCollectionCleanerTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Akeneo\Category\back\tests\Unit\Application\Enrichment;
 
-use Akeneo\Category\Application\Enrichment\DeactivatedTemplateAttributesInValueCollectionCleaner;
+use Akeneo\Category\Application\Enrichment\Filter\DeactivatedTemplateAttributesInValueCollectionFilter;
 use Akeneo\Category\back\tests\Integration\Helper\CategoryTestCase;
 use Akeneo\Category\Domain\Query\DeactivatedTemplateAttributes\DeactivatedTemplateAttributeIdentifier;
 
@@ -33,7 +33,7 @@ class DeactivatedTemplateAttributesInValueCollectionCleanerTest extends Category
             'value_collection' => '{"photo|8587cda6-58c8-47fa-9278-033e1d8c735c":{"data":{"size":168107,"extension":"jpg","file_path":"shoes.jpg","mime_type":"jpeg","original_filename":"shoes.jpg"},"type":"image","locale":null,"channel":null,"attribute_code":"photo|8587cda6-58c8-47fa-9278-033e1d8c735c"}}',
         ];
 
-        $deactivatedAttributesCleaner = $this->get(DeactivatedTemplateAttributesInValueCollectionCleaner::class);
+        $deactivatedAttributesCleaner = $this->get(DeactivatedTemplateAttributesInValueCollectionFilter::class);
         $result = ($deactivatedAttributesCleaner)($deactivatedAttributes, $rawCategory);
 
         $this->assertEquals($expected, $result);
@@ -56,7 +56,7 @@ class DeactivatedTemplateAttributesInValueCollectionCleanerTest extends Category
             'value_collection' => '{"photo|8587cda6-58c8-47fa-9278-033e1d8c735c":{"data":{"size":168107,"extension":"jpg","file_path":"shoes.jpg","mime_type":"jpeg","original_filename":"shoes.jpg"},"type":"image","locale":null,"channel":null,"attribute_code":"photo|8587cda6-58c8-47fa-9278-033e1d8c735c"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|en_US":{"data":"All the shoes you need!","type":"text","locale":"en_US","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"},"title|87939c45-1d85-4134-9579-d594fff65030|ecommerce|fr_FR":{"data":"Les chaussures dont vous avez besoin !","type":"text","locale":"fr_FR","channel":"ecommerce","attribute_code":"title|87939c45-1d85-4134-9579-d594fff65030"}}',
         ];
 
-        $deactivatedAttributesCleaner = $this->get(DeactivatedTemplateAttributesInValueCollectionCleaner::class);
+        $deactivatedAttributesCleaner = $this->get(DeactivatedTemplateAttributesInValueCollectionFilter::class);
         $result = ($deactivatedAttributesCleaner)($deactivatedAttributes, $rawCategory);
 
         $this->assertEquals($expected, $result);
