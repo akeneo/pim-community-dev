2c97d22ed1fe1688c21701d0b19f7e24ce6714ba
Riff <riff.ventura@gmail.com>
2023-04-18 14:38:56 +0200

    OCT-245: removed never type from mock catalogs exports

--- components/catalogs/mock/src/exports.ts
+++ components/catalogs/mock/src/exports.ts
@@ -1,39 +1,32 @@
-type CatalogList = (props: {
-    owner: string;
-    onCatalogClick: (id: string) => void;
-}) => null;
-type CatalogEdit = (props: {
-    id: string;
-    form: never;
-    headerContextContainer: HTMLDivElement | undefined;
-}) => null;
+type CatalogForm = {
+  values: {};
+  dispatch: () => void;
+  errors: {};
+};
+
+type CatalogList = (props: {owner: string; onCatalogClick: (id: string) => void}) => null;
+type CatalogEdit = (props: {id: string; form: CatalogForm; headerContextContainer: HTMLDivElement | undefined}) => null;
 type useCatalog = (id: string) => {
-    isLoading: boolean;
-    isError: boolean;
-    data: undefined | {
+  isLoading: boolean;
+  isError: boolean;
+  data:
+    | undefined
+    | {
         id: string;
         name: string;
         enabled: boolean;
         owner_username: string;
-    };
-    error: null;
+      };
+  error: null;
 };
-type useCatalogForm = (id: string) => [
-    {
-        values: {},
-        dispatch: () => void,
-        errors: {},
-    } | undefined,
-    () => Promise<boolean>,
-    boolean,
-];
+type useCatalogForm = (id: string) => [CatalogForm | undefined, () => Promise<boolean>, boolean];
 
 export const CatalogList: CatalogList = () => null;
 export const CatalogEdit: CatalogEdit = () => null;
 export const useCatalog: useCatalog = () => ({
-    isLoading: false,
-    isError: true,
-    data: undefined,
-    error: null,
+  isLoading: false,
+  isError: true,
+  data: undefined,
+  error: null,
 });
 export const useCatalogForm: useCatalogForm = () => [undefined, () => Promise.reject(), false];
