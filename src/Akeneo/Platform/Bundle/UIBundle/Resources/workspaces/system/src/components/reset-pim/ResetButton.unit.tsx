94030a380371c564ba3aea1b33850ac9100da19d
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-04-19 17:01:33 +0200

    RAB-1326: unit

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/system/src/components/reset-pim/ResetButton.unit.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/workspaces/system/src/components/reset-pim/ResetButton.unit.tsx
@@ -7,9 +7,9 @@ import {ResetButton} from './ResetButton';
 test('it opens a modal when clicking on the button', () => {
   renderWithProviders(<ResetButton />);
 
-  expect(screen.queryByText('pim_system.reset_pim.modal.steps.impact.title')).not.toBeInTheDocument();
+  expect(screen.queryByText('pim_system.reset_pim.modal.impact.title')).not.toBeInTheDocument();
 
   userEvent.click(screen.getByText('pim_system.reset_pim.button.label'));
 
-  expect(screen.getByText('pim_system.reset_pim.modal.steps.impact.title')).toBeInTheDocument();
+  expect(screen.getByText('pim_system.reset_pim.modal.impact.title')).toBeInTheDocument();
 });
