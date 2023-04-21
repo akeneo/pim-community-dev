1b672039b7106c7f040e4c2fd63da9d56f624fb6
Riff <riff.ventura@gmail.com>
2023-04-21 14:51:11 +0200

    OCT-245: Removed remaining Catalogs feature files

--- components/catalogs/front/src/components/ProductMapping/components/TargetSourceAssociation/SourceLabel.tsx
+++
@@ -1,7 +0,0 @@
-import React, {FC} from 'react';
-import {useAttribute} from '../../../../hooks/useAttribute';
-
-export const SourceLabel: FC<{sourceCode: string}> = ({sourceCode}) => {
-    const {data: attribute} = useAttribute(sourceCode);
-    return <>{attribute?.label ?? `[${sourceCode}]`}</>;
-};
