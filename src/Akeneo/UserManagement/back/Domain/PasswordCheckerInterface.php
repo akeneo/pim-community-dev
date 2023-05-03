abc0d0d5c1b0c40cef18632ba2af741bbe01dbf2
jeremyBeaucousinAkeneo <114410848+jeremyBeaucousinAkeneo@users.noreply.github.com>
2023-05-03 13:21:18 +0200

    Update PasswordCheckerInterface.php

--- src/Akeneo/UserManagement/back/Domain/PasswordCheckerInterface.php
+++ src/Akeneo/UserManagement/back/Domain/PasswordCheckerInterface.php
@@ -14,7 +14,6 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 interface PasswordCheckerInterface
 {
-
     /** @param array<mixed> $data */
     public function validatePassword(UserInterface $user, array $data): ConstraintViolationListInterface;
     public function validatePasswordMatch(string $password, string $passwordRepeat, string $propertyPath): ConstraintViolationListInterface;
