5cdc2b4056eaaf6049fee19ff9abb207f0649265
Céline Morin <celine.morin@akeneo.com>
2023-05-04 11:33:46 +0200

    CPM-1027: Add specific exceptions on get nomenclature

--- components/identifier-generator/back/src/Application/Exception/UndefinedAttributeException.php
+++ components/identifier-generator/back/src/Application/Exception/UndefinedAttributeException.php
@@ -8,6 +8,15 @@ namespace Akeneo\Pim\Automation\IdentifierGenerator\Application\Exception;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class UndefinedAttributeException extends \Exception
+class UndefinedAttributeException extends \RuntimeException
 {
+    private function __construct(private readonly string $attributeCode)
+    {
+        parent::__construct(\sprintf('The %s attribute does not exist', $this->attributeCode));
+    }
+
+    public static function withAttributeCode(string $attributeCode): self
+    {
+        return new self($attributeCode);
+    }
 }
--- components/identifier-generator/back/src/Infrastructure/Controller/GetNomenclatureController.php
+++ components/identifier-generator/back/src/Infrastructure/Controller/GetNomenclatureController.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Infrastructure\Controller;
 
 use Akeneo\Pim\Automation\IdentifierGenerator\Application\Exception\UndefinedAttributeException;
+use Akeneo\Pim\Automation\IdentifierGenerator\Application\Exception\UnexpectedAttributeTypeException;
 use Akeneo\Pim\Automation\IdentifierGenerator\Application\Get\GetNomenclatureCommand;
 use Akeneo\Pim\Automation\IdentifierGenerator\Application\Get\GetNomenclatureHandler;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -36,6 +38,8 @@ final class GetNomenclatureController
             return new JsonResponse(($this->getNomenclatureHandler)($command));
         } catch (UndefinedAttributeException $e) {
             throw new NotFoundHttpException($e->getMessage());
+        } catch (UnexpectedAttributeTypeException $e) {
+            throw new BadRequestHttpException($e->getMessage());
         }
     }
 }
