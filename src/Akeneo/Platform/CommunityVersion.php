d5f219a7a92d70bb01626e4aa8f6fc73224dbc48
Michel Tag <micheltag@akeneo.com>
2023-05-16 15:50:06 +0200

    Preparing version 6.0.84 (#19731)
    Preparing version 6.0.84
    
    
    Co-authored-by: ghoussard <ghoussard@protonmail.com>

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.83';
+    public const VERSION = '6.0.84';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
