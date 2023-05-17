slongiaru-dmind/akeneo-pim-community-dev

CPM-1027: Handle ref entities nomenclatures in GetNomenclatureController and UpdateNomenclatureController

components/identifier-generator/back/src/Application/Get/GetNomenclatureHandler.php
@@ -7,7 +7,11 @@ namespace Akeneo\Pim\Automation\IdentifierGenerator\Application\Get;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\NomenclatureDefinition;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Property\FamilyProperty;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\FamilyNomenclatureRepository;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\ReferenceEntityNomenclatureRepository;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\SimpleSelectNomenclatureRepository;
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\GetAttributes;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Webmozart\Assert\Assert;
 
 /**
@@ -19,6 +23,8 @@ final class GetNomenclatureHandler
     public function __construct(
         private readonly FamilyNomenclatureRepository $familyNomenclatureRepository,
         private readonly SimpleSelectNomenclatureRepository $simpleSelectNomenclatureRepository,
+        private readonly GetAttributes $getAttributes,
+        private readonly ReferenceEntityNomenclatureRepository $referenceEntityNomenclatureRepository,
     ) {
     }
 
@@ -35,7 +41,18 @@ final class GetNomenclatureHandler
         if ($command->propertyCode() === FamilyProperty::TYPE) {
             $nomenclature = $this->familyNomenclatureRepository->get() ?? new NomenclatureDefinition();
         } else {
-            $nomenclature = $this->simpleSelectNomenclatureRepository->get($command->propertyCode()) ?? new NomenclatureDefinition();
+            $attribute = $this->getAttributes->forCode($command->propertyCode());
+
+            if (null === $attribute) {
+                throw new NotFoundHttpException(
+                    sprintf('The "%s" attribute is not found', $command->propertyCode())
+                );
+            }
+            if ($attribute->type() === AttributeTypes::OPTION_SIMPLE_SELECT) {
+                $nomenclature = $this->simpleSelectNomenclatureRepository->get($command->propertyCode()) ?? new NomenclatureDefinition();
+            } else {
+                $nomenclature = $this->referenceEntityNomenclatureRepository->get($command->propertyCode()) ?? new NomenclatureDefinition();
+            }
         }
 
         Assert::allNotNull($nomenclature->values());

akeneo/pim-community-dev

components/identifier-generator/back/src/Application/Get/GetNomenclatureHandler.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Akeneo\Pim\Automation\IdentifierGenerator\Application\Get;
 
+use Akeneo\Pim\Automation\IdentifierGenerator\Application\Exception\UndefinedAttributeException;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\NomenclatureDefinition;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Model\Property\FamilyProperty;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\FamilyNomenclatureRepository;
+use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\ReferenceEntityNomenclatureRepository;
 use Akeneo\Pim\Automation\IdentifierGenerator\Domain\Repository\SimpleSelectNomenclatureRepository;
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+use Akeneo\Pim\Structure\Component\Query\PublicApi\AttributeType\GetAttributes;
 use Webmozart\Assert\Assert;
 
 /**
@@ -19,6 +23,8 @@ final class GetNomenclatureHandler
     public function __construct(
         private readonly FamilyNomenclatureRepository $familyNomenclatureRepository,
         private readonly SimpleSelectNomenclatureRepository $simpleSelectNomenclatureRepository,
+        private readonly GetAttributes $getAttributes,
+        private readonly ReferenceEntityNomenclatureRepository $referenceEntityNomenclatureRepository,
     ) {
     }
 
@@ -35,7 +41,18 @@ final class GetNomenclatureHandler
         if ($query->propertyCode() === FamilyProperty::TYPE) {
             $nomenclature = $this->familyNomenclatureRepository->get() ?? new NomenclatureDefinition();
         } else {
-            $nomenclature = $this->simpleSelectNomenclatureRepository->get($query->propertyCode()) ?? new NomenclatureDefinition();
+            $attribute = $this->getAttributes->forCode($query->propertyCode());
+
+            if (null === $attribute) {
+                throw new UndefinedAttributeException(
+                    \sprintf('The "%s" attribute is not found', $query->propertyCode())
+                );
+            }
+            if ($attribute->type() === AttributeTypes::OPTION_SIMPLE_SELECT) {
+                $nomenclature = $this->simpleSelectNomenclatureRepository->get($query->propertyCode()) ?? new NomenclatureDefinition();
+            } else {
+                $nomenclature = $this->referenceEntityNomenclatureRepository->get($query->propertyCode()) ?? new NomenclatureDefinition();
+            }
         }
 
         Assert::allNotNull($nomenclature->values());