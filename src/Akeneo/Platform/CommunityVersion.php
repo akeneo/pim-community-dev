127e34f52c256cb05f4656a2ee26cdac7cbdf2d5
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-05-23 11:44:27 +0000

    Preparing version 6.0.85

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.84';
+    public const VERSION = '6.0.85';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
