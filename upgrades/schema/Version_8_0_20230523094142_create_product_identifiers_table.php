5e2a35ce5e9ff76565ee75fe79f5177eef5a04b8
Pierre Maraître <pierre.maraitre@akeneo.com>
2023-05-23 14:25:16 +0200

    CPM-1030: Add migration to create table pim_catalog_product_identifiers

---
+++ upgrades/schema/Version_8_0_20230523094142_create_product_identifiers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pim\Upgrade\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class Version_8_0_20230523094142_create_product_identifiers_table extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create the pim_catalog_product_identifiers table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            <<<SQL
+            CREATE TABLE IF NOT EXISTS pim_catalog_product_identifiers(
+                uuid BINARY(16) NOT NULL PRIMARY KEY,
+                identifiers JSON DEFAULT NULL COMMENT '(DC2Type:json_array)',
+                CONSTRAINT pim_catalog_product_identifiers_pim_catalog_product_uuid_fk
+                    FOREIGN KEY (uuid) REFERENCES `pim_catalog_product` (uuid)
+                        ON DELETE CASCADE,
+                INDEX idx_identifiers ( (CAST(identifiers AS CHAR(255) array)) )
+            )
+            SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
