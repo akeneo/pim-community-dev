75c3c6d2101b10fb1c368aa881824f2d8035590b
Antoine ARRIVE <antoine.arrive@gmail.com>
2023-05-25 09:21:14 +0200

    CPM-1041: Get Completenesses from new table

---
+++ src/Akeneo/Pim/Enrichment/Bundle/Storage/Sql/Completeness/SwitchSqlGetCompleteness.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Bundle\Storage\Sql\Completeness;
+
+use Akeneo\Pim\Enrichment\Component\Product\Completeness\Model\ProductCompletenessCollection;
+use Akeneo\Pim\Enrichment\Component\Product\Query\GetProductCompletenesses;
+use Doctrine\DBAL\Connection;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class SwitchSqlGetCompleteness implements GetProductCompletenesses
+{
+    private const TABLE_NAME = 'pim_catalog_product_completeness';
+
+    public function __construct(
+        private readonly GetProductCompletenesses $legacyGetProductCompletenesses,
+        private readonly GetProductCompletenesses $getProductCompletenesses,
+        private readonly Connection $connection,
+    ) {
+    }
+
+    public function fromProductUuid(UuidInterface $productUuid): ProductCompletenessCollection
+    {
+        return $this->fromProductUuids([$productUuid])[$productUuid->toString()];
+    }
+
+    public function fromProductUuids(array $productUuids, ?string $channel = null, array $locales = []): array
+    {
+        if ($this->newTableExists()) {
+            return $this->getProductCompletenesses->fromProductUuids($productUuids, $channel, $locales);
+        }
+        return $this->legacyGetProductCompletenesses->fromProductUuids($productUuids, $channel, $locales);
+    }
+
+    private function newTableExists(): bool
+    {
+        return $this->connection->executeQuery(
+            'SHOW TABLES LIKE :tableName',
+            [
+                'tableName' => self::TABLE_NAME,
+            ]
+        )->rowCount() >= 1;
+    }
+}
