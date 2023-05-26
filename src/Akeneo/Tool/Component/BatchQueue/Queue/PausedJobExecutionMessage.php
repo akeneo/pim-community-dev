54ca13d575e15c00b95eb9b95deba8047e3d17db
Grégoire Houssard <ghoussard@protonmail.com>
2023-05-26 15:44:30 +0200

    RAB-1392: Fix copyright

--- src/Akeneo/Tool/Component/BatchQueue/Queue/PausedJobExecutionMessage.php
+++ src/Akeneo/Tool/Component/BatchQueue/Queue/PausedJobExecutionMessage.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace Akeneo\Tool\Component\BatchQueue\Queue;
 
 /**
- * @copyright 2023 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 final class PausedJobExecutionMessage extends JobExecutionMessage
 {
