2e7f42254977992874e08980de850df4915290fa
Laurent Petard <laurent.petard@akeneo.com>
2023-05-26 15:06:18 +0200

    JEL-340: Adapt integration tests

--- src/Akeneo/Tool/Bundle/MessengerBundle/tests/config/Handler1ForMessage1.php
+++ src/Akeneo/Tool/Bundle/MessengerBundle/tests/config/Handler1ForMessage1.php
@@ -4,23 +4,20 @@ declare(strict_types=1);
 
 namespace Akeneo\Tool\Bundle\MessengerBundle\tests\config;
 
-use Akeneo\Tool\Component\Messenger\UcsMessageHandlerInterface;
-use Akeneo\Tool\Component\Messenger\TraceableMessageInterface;
-use Webmozart\Assert\Assert;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-final class Handler1ForMessage1 implements UcsMessageHandlerInterface
+final class Handler1ForMessage1 implements MessageHandlerInterface
 {
     public function __construct(private readonly HandlerObserver $handlerObserver)
     {
     }
 
-    public function __invoke(TraceableMessageInterface $message): void
+    public function __invoke(Message1 $message): void
     {
-        Assert::isInstanceOf($message, Message1::class);
         $this->handlerObserver->handlerWasExecuted(self::class, $message);
     }
 }
