1b6c9e1eef11d2970f438c25b86dab7ed4223bb1
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-05-29 15:52:15 +0200

    Merge remote-tracking branch 'origin/master' into PIM-10982

--- front-packages/akeneo-design-system/src/GlobalStyle.tsx
+++ front-packages/akeneo-design-system/src/GlobalStyle.tsx
@@ -7,8 +7,4 @@ export const GlobalStyle = createGlobalStyle`
       unicode-range: U+1F1E6-1F1FF;
       src: url(${TwemojiCountryFlags}) format('woff2');
     }
-    
-    .windows-display-flags {
-      font-family: "Windows Flag Emoji";
-    }
 `;
