a0bf55882b4a03faa2b63ee18eae18c8531b3850
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-06-02 09:04:39 +0200

    PIM-11012 : Add helper if selected UI language is not sufficiently supported

--- src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning-view.tsx
+++ src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning-view.tsx
@@ -3,18 +3,18 @@ import {ThemeProvider} from 'styled-components';
 import {pimTheme} from 'akeneo-design-system';
 import {ReactView} from '@akeneo-pim-community/legacy-bridge/src/bridge/react';
 import {DependenciesProvider} from '@akeneo-pim-community/legacy-bridge';
-import {UnsupportedUiLocaleWarning} from "./unsupported-ui-locale-warning";
-import {QueryClient, QueryClientProvider} from "react-query";
+import {UnsupportedUiLocaleWarning} from './unsupported-ui-locale-warning';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 class UnsupportedUiLocaleWarningView extends ReactView {
-    reactElementToMount() {
+  reactElementToMount() {
     const queryClient = new QueryClient();
     return (
       <DependenciesProvider>
         <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={pimTheme}>
-                <UnsupportedUiLocaleWarning />
-            </ThemeProvider>
+          <ThemeProvider theme={pimTheme}>
+            <UnsupportedUiLocaleWarning />
+          </ThemeProvider>
         </QueryClientProvider>
       </DependenciesProvider>
     );
--- src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning.tsx
+++ src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning.tsx
@@ -3,18 +3,20 @@ import {Helper} from 'akeneo-design-system';
 import {useTranslate, userContext} from '@akeneo-pim-community/shared';
 import {useUiLocales} from '../hooks/useUiLocales';
 
-const UnsupportedUiLocaleWarning: FC = () =>{
+const UnsupportedUiLocaleWarning: FC = () => {
   const translate = useTranslate();
   const userDefaultLocaleCode = userContext.get('user_default_locale');
   const locales = useUiLocales();
   const userLocalFound = locales?.find(locale => locale.code === userDefaultLocaleCode);
 
   return (
-      <>
-        {!userLocalFound && (
-            <Helper level="warning">{translate('pim_user_management.entity.user.properties.helper', {code: userDefaultLocaleCode})}</Helper>
-        )}
-      </>
+    <>
+      {!userLocalFound && (
+        <Helper level="warning">
+          {translate('pim_user_management.entity.user.properties.helper', {code: userDefaultLocaleCode})}
+        </Helper>
+      )}
+    </>
   );
 };
 
