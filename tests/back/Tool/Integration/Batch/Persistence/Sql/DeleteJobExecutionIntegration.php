498c8fb03485701e8c954ad511b4a74983788420
Steven VAIDIE <steven.vaidie@gmail.com>
2023-06-05 10:28:44 +0200

    PIM-10931: Refactor tests

--- tests/back/Tool/Integration/Batch/Persistence/Sql/DeleteJobExecutionIntegration.php
+++ tests/back/Tool/Integration/Batch/Persistence/Sql/DeleteJobExecutionIntegration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Akeneo\Test\Tool\Integration\Batch\Persistence\Sql;
 
+use Akeneo\Test\Integration\Configuration;
 use Akeneo\Test\Integration\TestCase;
 use Akeneo\Tool\Bundle\BatchBundle\Persistence\Sql\DeleteJobExecution;
 use Doctrine\DBAL\Connection;
@@ -16,36 +17,28 @@ class DeleteJobExecutionIntegration extends TestCase
 {
     public function test_that_it_deletes_all_jobs_older_than_one_day_except_the_most_recent_one_for_each_job_instance()
     {
-        $this->launchJob();
-        $this->launchJob();
-        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
-        Assert::assertSame(2, $numberOfJobs);
+        $this->launchJob('csv_product_export');
+        $this->launchJob('csv_product_export');
+        $this->assertJobExecutionCount(2);
 
-        $this->getConnection()->executeUpdate('UPDATE akeneo_batch_job_execution SET create_time = Date_ADD(end_time, INTERVAL -10 day)');
+        $this->getConnection()->executeStatement('UPDATE akeneo_batch_job_execution SET create_time = Date_ADD(end_time, INTERVAL -10 day)');
 
-        $deleteJobExecution = $this->getDeleteQuery();
-        $deleteJobExecution->olderThanDays(1);
+        $this->getDeleteQuery()->olderThanDays(1, [], null);
 
-        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
-        Assert::assertSame(1, $numberOfJobs);
-
+        $this->assertJobExecutionCount(1);
     }
 
     public function test_that_it_deletes_all_jobs()
     {
-        $this->launchJob();
-        $this->launchJob();
-        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
-        Assert::assertSame(2, $numberOfJobs);
+        $this->launchJob('csv_product_export');
+        $this->launchJob('csv_product_export');
+        $this->assertJobExecutionCount(2);
 
-        $this->getConnection()->executeUpdate('UPDATE akeneo_batch_job_execution SET create_time = Date_ADD(end_time, INTERVAL -10 day)');
+        $this->getConnection()->executeStatement('UPDATE akeneo_batch_job_execution SET create_time = Date_ADD(end_time, INTERVAL -10 day)');
 
-        $deleteJobExecution = $this->getDeleteQuery();
-        $deleteJobExecution->all();
+        $this->getDeleteQuery()->all([], null);
 
-        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) as number_of_jobs FROM akeneo_batch_job_execution')->fetch()['number_of_jobs'];
-        Assert::assertSame(0, $numberOfJobs);
-
+        $this->assertJobExecutionCount(0);
     }
 
     private function getDeleteQuery(): DeleteJobExecution
@@ -53,26 +46,25 @@ class DeleteJobExecutionIntegration extends TestCase
         return $this->get('akeneo_batch.delete_job_execution');
     }
 
-    private function launchJob(array $arrayInput = []): BufferedOutput
+    private function launchJob(string $jobInstanceCode): void
     {
         $application = new Application(static::$kernel);
         $application->setAutoExit(false);
 
-        $defaultArrayInput = [
+        $parameters = [
             'command'  => 'akeneo:batch:job',
-            'code'     => 'csv_product_export',
+            'code'     => $jobInstanceCode,
         ];
 
-        $arrayInput = array_merge($defaultArrayInput, $arrayInput);
-        if (isset($arrayInput['--config'])) {
-            $arrayInput['--config'] = json_encode($arrayInput['--config']);
-        }
-
-        $input = new ArrayInput($arrayInput);
+        $input = new ArrayInput($parameters);
         $output = new BufferedOutput();
         $application->run($input, $output);
+    }
 
-        return $output;
+    private function assertJobExecutionCount(int $expectedCount): void
+    {
+        $numberOfJobs = (int) $this->getConnection()->executeQuery('SELECT COUNT(*) FROM akeneo_batch_job_execution')->fetchOne();
+        Assert::assertSame($expectedCount, $numberOfJobs);
     }
 
     private function getConnection(): Connection
@@ -83,7 +75,7 @@ class DeleteJobExecutionIntegration extends TestCase
     /**
      * {@inheritdoc}
      */
-    protected function getConfiguration()
+    protected function getConfiguration(): Configuration
     {
         return $this->catalog->useTechnicalCatalog();
     }
