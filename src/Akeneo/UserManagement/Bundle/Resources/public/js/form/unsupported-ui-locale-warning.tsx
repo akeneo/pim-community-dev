3d7778e8b8d292414338163d73c7ed738d8f005b
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-06-05 09:15:26 +0200

    GRF-859: Add link to crowdin on unsuported UI local warning

--- src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning.tsx
+++ src/Akeneo/UserManagement/Bundle/Resources/public/js/form/unsupported-ui-locale-warning.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {Helper} from 'akeneo-design-system';
+import {Helper, Link} from 'akeneo-design-system';
 import {useTranslate, userContext} from '@akeneo-pim-community/shared';
 import {useUiLocales} from '../hooks/useUiLocales';
 
@@ -13,7 +13,9 @@ const UnsupportedUiLocaleWarning: FC = () => {
     <>
       {!userLocalFound && (
         <Helper level="warning">
-          {translate('pim_user_management.entity.user.properties.helper', {code: userDefaultLocaleCode})}
+          <span style={{marginRight: '5px'}}>{translate('pim_user_management.entity.user.properties.not_fully_supported_locale_start', {code: userDefaultLocaleCode})}</span>
+          <Link href="https://crowdin.com/project/akeneo">Crowdin</Link>
+          <span style={{marginLeft: '5px'}}>{translate('pim_user_management.entity.user.properties.not_fully_supported_locale_end', {code: userDefaultLocaleCode})}</span>
         </Helper>
       )}
     </>
