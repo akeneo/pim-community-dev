b29ae11744fa04fb9e0b15f8f4f9fc1b8328a6b0
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-06-12 15:01:27 +0200

    RAB-1329: Fix inte test

--- src/Akeneo/Platform/Installer/back/tests/Integration/Controller/CheckInstanceCanBeResetActionTest.php
+++ src/Akeneo/Platform/Installer/back/tests/Integration/Controller/CheckInstanceCanBeResetActionTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Akeneo\Platform\Installer\Test\Integration\Controller;
 
-use Akeneo\Platform\Job\Test\Integration\ControllerIntegrationTestCase;
+use Akeneo\Platform\Installer\Test\Integration\ControllerIntegrationTestCase;
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Tool\Component\Batch\Job\BatchStatus;
+use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\Assert;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,11 +15,15 @@ class CheckInstanceCanBeResetActionTest extends ControllerIntegrationTestCase
 {
     private const ROUTE = 'akeneo_installer_check_reset_instance';
 
+    private Connection $connection;
+
     public function setUp(): void
     {
         parent::setUp();
+        $this->connection = $this->get('database_connection');
 
         $this->logAs('julia');
+        $this->featureFlags->enable('reset_pim');
     }
 
     public function test_it_returns_ok_when_no_job_is_queued_or_running(): void
@@ -24,12 +31,12 @@ class CheckInstanceCanBeResetActionTest extends ControllerIntegrationTestCase
         $this->webClientHelper->callApiRoute($this->client, self::ROUTE);
 
         $response = $this->client->getResponse();
-        Assert::assertSame($response->getStatusCode(), Response::HTTP_OK);
+        Assert::assertSame($response->getStatusCode(), Response::HTTP_NO_CONTENT);
     }
 
     public function test_it_returns_failure_when_a_job_is_queued(): void
     {
-        //given a queued job execution queued
+        $this->given_a_job_with_status(BatchStatus::STARTING);
 
         $this->webClientHelper->callApiRoute($this->client, self::ROUTE);
 
@@ -39,11 +46,26 @@ class CheckInstanceCanBeResetActionTest extends ControllerIntegrationTestCase
 
     public function test_it_returns_failure_when_a_job_is_running(): void
     {
-        //given a queued job execution running
+        $this->given_a_job_with_status(BatchStatus::STARTED);
 
         $this->webClientHelper->callApiRoute($this->client, self::ROUTE);
 
         $response = $this->client->getResponse();
         Assert::assertSame($response->getStatusCode(), Response::HTTP_BAD_REQUEST);
+    }
+
+    private function given_a_job_with_status(int $status): void
+    {
+        $jobInstanceId = $this->connection->executeQuery('SELECT id FROM akeneo_batch_job_instance WHERE code = "csv_product_quick_export";')->fetchOne();
+        $insertJobExecution = <<<SQL
+INSERT INTO `akeneo_batch_job_execution` (job_instance_id, pid, user, status, start_time, end_time, create_time, updated_time, health_check_time, exit_code, exit_description, failure_exceptions, log_file, raw_parameters)
+VALUES (:job_instance_id, null, 'admin', $status, null, null, '2020-10-16 09:38:16', null, null, 'UNKNOWN', '', 'a:0:{}', null, '{}');
+SQL;
+        $this->connection->executeUpdate($insertJobExecution, ['job_instance_id' => $jobInstanceId]);
+    }
+
+    protected function getConfiguration(): Configuration
+    {
+        return $this->catalog->useMinimalCatalog();
     }
 }
--- src/Akeneo/Platform/Installer/back/tests/Integration/ControllerIntegrationTestCase.php
+++ src/Akeneo/Platform/Installer/back/tests/Integration/ControllerIntegrationTestCase.php
@@ -4,22 +4,34 @@ declare(strict_types=1);
 
 namespace Akeneo\Platform\Installer\Test\Integration;
 
+use Akeneo\Platform\Bundle\FeatureFlagBundle\Internal\Test\FilePersistedFeatureFlags;
+use Akeneo\Test\Integration\Configuration;
+use Akeneo\Test\IntegrationTestsBundle\Configuration\CatalogInterface;
+use Akeneo\Test\IntegrationTestsBundle\Helper\WebClientHelper;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ControllerIntegrationTestCase extends WebTestCase
+abstract class ControllerIntegrationTestCase extends WebTestCase
 {
+    protected CatalogInterface $catalog;
     protected KernelBrowser $client;
     protected WebClientHelper $webClientHelper;
+    protected FilePersistedFeatureFlags $featureFlags;
 
     protected function setUp(): void
     {
         parent::setUp();
         static::bootKernel(['debug' => false]);
 
+        $this->catalog = $this->get('akeneo_integration_tests.catalogs');
+        $fixturesLoader = $this->get('akeneo_integration_tests.loader.fixtures_loader');
+        $fixturesLoader->load($this->getConfiguration());
+
         self::ensureKernelShutdown();
         $this->client = static::createClient(['debug' => false]);
         $this->client->disableReboot();
         $this->webClientHelper = $this->get('akeneo_integration_tests.helper.web_client');
+        $this->featureFlags = $this->get('feature_flags');
     }
 
     protected function get(string $service): ?object
@@ -39,4 +51,6 @@ class ControllerIntegrationTestCase extends WebTestCase
     {
         $this->get('akeneo_integration_tests.helper.authenticator')->logIn($username, $this->client);
     }
+
+    abstract protected function getConfiguration(): Configuration;
 }
