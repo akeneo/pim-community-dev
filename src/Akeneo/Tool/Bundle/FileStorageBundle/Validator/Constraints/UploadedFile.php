e220a0ee13ab3bfd245c96d62f8b41af75c74e9f
LevFlavien <flavien.levesque@akeneo.com>
2023-06-13 14:24:32 +0200

    PIM-10869: Add new controller responsible for uploading images

---
+++ src/Akeneo/Tool/Bundle/FileStorageBundle/Validator/Constraints/UploadedFile.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Tool\Bundle\FileStorageBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+final class UploadedFile extends Constraint
+{
+    public array $types;
+
+    public string $unsupportedExtensionMessage = 'The extension of the file is invalid ({{ extension }}). Allowed extensions are {{ extensions }}.';
+    public string $unsupportedMimeTypeMessage = 'The mime type of the file is invalid ({{ mimeType }}). Allowed mime types are {{ mimeTypes }}.';
+    public string $invalidExtensionMessage = 'The extension of the file ({{ originalExtension }}) does not match its content.';
+    public string $invalidMimeTypeMessage = 'The mime type of the file ({{ originalMimeType }}) does not match its content.';
+    public string $mimeTypeDoesNotMatchExtensionMessage = 'The mime type of the file ({{ originalMimeType }}) does not match its extension ({{ originalExtension }}).';
+
+    public function getRequiredOptions()
+    {
+        return ['types'];
+    }
+}
