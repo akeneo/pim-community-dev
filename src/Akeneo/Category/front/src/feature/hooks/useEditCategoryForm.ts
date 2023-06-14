d92f12446f344f8a0949ad2218480874f2910bf7
Paul Chasle <pchasle@users.noreply.github.com>
2023-06-14 17:48:13 +0200

    GRF: fix invalid query key

--- src/Akeneo/Category/front/src/feature/hooks/useEditCategoryForm.ts
+++ src/Akeneo/Category/front/src/feature/hooks/useEditCategoryForm.ts
@@ -90,7 +90,7 @@ const useEditCategoryForm = (categoryId: number) => {
       notify(NotificationLevel.SUCCESS, translate('pim_enrich.entity.category.content.edit.success'));
 
       // force to fetch attribute list from template to hide potentially deleted attributes
-      await queryClient.invalidateQueries(['template', fetchedCategory?.template_uuid]);
+      await queryClient.invalidateQueries(['get-template', fetchedCategory?.template_uuid]);
     } else {
       notify(NotificationLevel.ERROR, response.error.message);
       if (response.error.code && response.error.code === DEACTIVATED_TEMPLATE) {
