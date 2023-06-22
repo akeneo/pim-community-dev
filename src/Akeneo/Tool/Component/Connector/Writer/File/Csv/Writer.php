437f3506a1fd5f5aed1494c1dc2acc7c143b2450
Marin Fazio <marin.fazio@akeneo.com>
2023-06-22 11:46:56 +0200

    RAB-1440: Remove useless code

--- src/Akeneo/Tool/Component/Connector/Writer/File/Csv/Writer.php
+++ src/Akeneo/Tool/Component/Connector/Writer/File/Csv/Writer.php
@@ -90,11 +90,6 @@ class Writer extends AbstractFileWriter implements ItemWriterInterface, Initiali
         $this->flatRowBuffer->write($flatItems, $options);
     }
 
-    public function emptyWrite(): void
-    {
-        $this->flatRowBuffer->emptyWrite();
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -113,7 +108,6 @@ class Writer extends AbstractFileWriter implements ItemWriterInterface, Initiali
         $writtenFiles = $this->flusher->flush(
             $this->flatRowBuffer,
             $writerOptions,
-//            str_replace('/tmp//tmp', '/tmp', $this->getPath()),
             $this->getPath(),
             ($parameters->has('linesPerFile') ? $parameters->get('linesPerFile') : -1)
         );
