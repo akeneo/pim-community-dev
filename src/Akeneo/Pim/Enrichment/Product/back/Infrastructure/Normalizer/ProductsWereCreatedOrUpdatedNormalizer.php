b4300c4c0f50e1f42031525e790b72b8a9b43b0e
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2023-06-26 14:01:00 +0200

    JEL-299: add spec for ProductsWereCreatedOrUpdatedNormalizer

--- src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Normalizer/ProductsWereCreatedOrUpdatedNormalizer.php
+++ src/Akeneo/Pim/Enrichment/Product/back/Infrastructure/Normalizer/ProductsWereCreatedOrUpdatedNormalizer.php
@@ -15,8 +15,6 @@ use Webmozart\Assert\Assert;
 /**
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
- * @todo spec
  */
 final class ProductsWereCreatedOrUpdatedNormalizer implements NormalizerInterface, DenormalizerInterface
 {
