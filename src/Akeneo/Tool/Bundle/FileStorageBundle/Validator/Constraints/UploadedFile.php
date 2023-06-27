ba7b6388e3787b4239cd6bcdae7443ea525c690c
LevFlavien <flavien.levesque@akeneo.com>
2023-06-27 11:23:56 +0200

    PIM-10869: Change image upload error messages to give less information (#19944)

--- src/Akeneo/Tool/Bundle/FileStorageBundle/Validator/Constraints/UploadedFile.php
+++ src/Akeneo/Tool/Bundle/FileStorageBundle/Validator/Constraints/UploadedFile.php
@@ -11,10 +11,7 @@ class UploadedFile extends Constraint
     public array $types;
 
     public string $unsupportedExtensionMessage = 'The extension of the file is invalid ({{ extension }}). Allowed extensions are {{ extensions }}.';
-    public string $unsupportedMimeTypeMessage = 'The mime type of the file is invalid ({{ mimeType }}). Allowed mime types are {{ mimeTypes }}.';
-    public string $invalidExtensionMessage = 'The extension of the file ({{ originalExtension }}) does not match its content.';
-    public string $invalidMimeTypeMessage = 'The mime type of the file ({{ originalMimeType }}) does not match its content.';
-    public string $mimeTypeDoesNotMatchExtensionMessage = 'The mime type of the file ({{ originalMimeType }}) does not match its extension ({{ originalExtension }}).';
+    public string $fileIsCorruptedMessage = 'The content of the file is corrupted.';
 
     public function getRequiredOptions()
     {
