0eca0e48b97304568dec62e9e14596d538b1007c
Paul Chasle <pchasle@users.noreply.github.com>
2023-06-27 10:32:35 +0200

    GRF: rollback change on template hook

--- src/Akeneo/Category/front/src/feature/hooks/useTemplateByTemplateUuid.ts
+++ src/Akeneo/Category/front/src/feature/hooks/useTemplateByTemplateUuid.ts
@@ -4,17 +4,18 @@ import {useHistory} from 'react-router';
 import {Template} from '../models';
 import {apiFetch} from '../tools/apiFetch';
 
-export const useTemplateByTemplateUuid = (templateUuid: string | null) => {
+export const useTemplateByTemplateUuid = (uuid: string | null) => {
   const router = useRouter();
   const translate = useTranslate();
   const notify = useNotify();
   const history = useHistory();
 
   const url = router.generate('pim_category_template_rest_get_by_template_uuid', {
-    templateUuid,
+    templateUuid: uuid,
   });
 
-  return useQuery(['get-template', templateUuid], () => apiFetch<Template>(url, {}), {
+  return useQuery(['get-template', uuid], () => apiFetch<Template>(url, {}), {
+    enabled: null !== uuid,
     onError: () => {
       history.push('/');
       notify(NotificationLevel.ERROR, translate('akeneo.category.template.not_found'));
