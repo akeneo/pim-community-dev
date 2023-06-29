a789490d9f52c720e10de0f5e687672ac75a1b74
pierallard <pierallard@gmail.com>
2023-06-29 18:03:43 +0200

    CPM-1067: Add Integration test

--- src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierCommand.php
+++ src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierCommand.php
@@ -2,14 +2,18 @@
 
 namespace Akeneo\Pim\Structure\Bundle\Application\SwitchMainIdentifier;
 
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class SwitchMainIdentifierCommand
 {
     public function __construct(
-        private string $newMainIdentifierCode,
+        private readonly string $newMainIdentifierCode,
     ) {
     }
 
-    public function fromIdentifierCode(string $newMainIdentifierCode)
+    public static function fromIdentifierCode(string $newMainIdentifierCode)
     {
         return new self($newMainIdentifierCode);
     }
