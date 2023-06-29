28915e32b810b0f5ecdaac4e79cda1c062f89644
pierallard <pierallard@gmail.com>
2023-06-29 18:03:43 +0200

    CPM-1067: Add Integration test

--- src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierHandler.php
+++ src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierHandler.php
@@ -5,18 +5,21 @@ namespace Akeneo\Pim\Structure\Bundle\Application\SwitchMainIdentifier;
 use Doctrine\DBAL\Connection;
 use Webmozart\Assert\Assert;
 
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class SwitchMainIdentifierHandler
 {
     public function __construct(
-        private Connection $connection
+        private readonly Connection $connection
     ) {
     }
 
     public function __invoke(SwitchMainIdentifierCommand $command): void
     {
-        $formerMainIdentifierCode = $this->getFormerMainIdentifierCode();
         $this->switchMainIdentifier(
-            $formerMainIdentifierCode,
+            $this->getFormerMainIdentifierCode(),
             $command->getNewMainIdentifierCode()
         );
     }
