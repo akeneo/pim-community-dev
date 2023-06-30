8ac5c8a97257916adf9c9c7df9adc5f3dd0375d0
pierallard <pierallard@gmail.com>
2023-06-30 11:12:24 +0200

    CPM-1067: Add Validation

---
+++ src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierValidator.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Akeneo\Pim\Structure\Bundle\Application\SwitchMainIdentifier;
+
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+use Akeneo\Pim\Structure\Component\Model\Attribute;
+use Akeneo\Pim\Structure\Component\Repository\AttributeRepositoryInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+final class SwitchMainIdentifierValidator
+{
+    private ?Attribute $newMainIdentifier;
+
+    public function __construct(
+        private readonly AttributeRepositoryInterface $attributeRepository,
+    ) {
+    }
+
+    public function validate(
+        SwitchMainIdentifierCommand $command
+    ): void {
+        $this->loadNewMainIdentifier($command->getNewMainIdentifierCode());
+        $this->validateAttributeExists();
+        $this->validateAttributeIsAnIdentifier();
+        $this->validateAttributeIsNotMainIdentifier();
+    }
+
+    private function validateAttributeExists(): void
+    {
+        if (null === $this->newMainIdentifier) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s attribute does not exist',
+                $this->newMainIdentifier
+            ));
+        }
+    }
+
+    private function validateAttributeIsAnIdentifier(): void
+    {
+        Assert::isInstanceOf($this->newMainIdentifier, Attribute::class);
+        if ($this->newMainIdentifier->getType() !== AttributeTypes::IDENTIFIER) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s attribute is not an identifier',
+                $this->newMainIdentifier
+            ));
+        }
+    }
+
+    private function validateAttributeIsNotMainIdentifier(): void
+    {
+        Assert::isInstanceOf($this->newMainIdentifier, Attribute::class);
+        if ($this->newMainIdentifier->isMainIdentifier()) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s attribute is already the main identifier',
+                $this->newMainIdentifier
+            ));
+        }
+    }
+
+    private function loadNewMainIdentifier(
+        string $attributeCode
+    ): void {
+        $this->newMainIdentifier = $this->attributeRepository->findOneByIdentifier($attributeCode);
+    }
+}
