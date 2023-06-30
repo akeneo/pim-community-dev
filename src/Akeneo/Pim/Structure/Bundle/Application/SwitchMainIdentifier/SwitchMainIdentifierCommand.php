7bc06ca472f0f52c5cd6de9b88fb9ebc39ccb2b8
pierallard <pierallard@gmail.com>
2023-06-30 10:31:33 +0200

    CPM-1067: Add Controller to switch main identifier

--- src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierCommand.php
+++ src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/SwitchMainIdentifierCommand.php
@@ -6,7 +6,7 @@ namespace Akeneo\Pim\Structure\Bundle\Application\SwitchMainIdentifier;
  * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class SwitchMainIdentifierCommand
+final class SwitchMainIdentifierCommand
 {
     public function __construct(
         private readonly string $newMainIdentifierCode,
