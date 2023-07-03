776a5bc565796aa19801820e6d08aa8969d0d8cc
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-07-03 17:44:26 +0200

    RAB-1337: Fix spec

--- tests/back/Platform/Specification/Bundle/AnalyticsBundle/DataCollector/VersionDataCollectorSpec.php
+++ tests/back/Platform/Specification/Bundle/AnalyticsBundle/DataCollector/VersionDataCollectorSpec.php
@@ -2,11 +2,12 @@
 
 namespace Specification\Akeneo\Platform\Bundle\AnalyticsBundle\DataCollector;
 
+use Akeneo\Platform\Bundle\AnalyticsBundle\DataCollector\VersionDataCollector;
+use Akeneo\Platform\Bundle\FeatureFlagBundle\FeatureFlags;
+use Akeneo\Platform\Bundle\InstallerBundle\InstallStatusManager\InstallStatusManager;
+use Akeneo\Platform\Bundle\PimVersionBundle\VersionProviderInterface;
 use Akeneo\Tool\Component\Analytics\DataCollectorInterface;
 use PhpSpec\ObjectBehavior;
-use Akeneo\Platform\Bundle\AnalyticsBundle\DataCollector\VersionDataCollector;
-use Akeneo\Platform\Bundle\PimVersionBundle\VersionProviderInterface;
-use Akeneo\Platform\Bundle\InstallerBundle\InstallStatusManager\InstallStatusManager;
 use Prophecy\Argument;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -17,9 +18,10 @@ class VersionDataCollectorSpec extends ObjectBehavior
     function let(
         RequestStack $requestStack,
         VersionProviderInterface $versionProvider,
-        InstallStatusManager $installStatusManager
+        InstallStatusManager $installStatusManager,
+        FeatureFlags $featureFlags,
     ) {
-        $this->beConstructedWith($requestStack, $versionProvider, $installStatusManager, 'prod');
+        $this->beConstructedWith($requestStack, $versionProvider, $installStatusManager, 'prod', $featureFlags);
     }
 
     public function it_is_initializable()
@@ -28,18 +30,20 @@ class VersionDataCollectorSpec extends ObjectBehavior
         $this->shouldImplement(DataCollectorInterface::class);
     }
 
-    public function it_collects_pim_version_edition_and_storage_driver
-    (
-        $requestStack,
-        $versionProvider,
-        $installStatusManager,
+    public function it_collects_pim_version_edition_and_storage_driver(
+        RequestStack $requestStack,
+        VersionProviderInterface $versionProvider,
+        InstallStatusManager $installStatusManager,
         Request $request,
-        ServerBag $serverBag
+        ServerBag $serverBag,
+        FeatureFlags $featureFlags,
     ) {
+        $featureFlags->isEnabled('reset_pim')->willReturn(true);
         $versionProvider->getPatch()->willReturn('1.4.0');
         $versionProvider->getEdition()->willReturn('CE');
         $requestStack->getCurrentRequest()->willReturn($request);
         $installStatusManager->getPimInstallDateTime()->willReturn(new \DateTime('2015-09-16T10:10:32+02:00'));
+        $installStatusManager->getPimResetData()->willReturn(['reset_events' => [['time' => '2015-09-17T10:10:32+02:00']]]);
         $request->server = $serverBag;
         $serverBag->get('SERVER_SOFTWARE')->willReturn('Apache/2.4.12 (Debian)');
 
@@ -50,16 +54,20 @@ class VersionDataCollectorSpec extends ObjectBehavior
                 'pim_environment'    => 'prod',
                 'pim_install_time'   => (new \DateTime('2015-09-16T10:10:32+02:00'))->format(\DateTime::ATOM),
                 'server_version'     => 'Apache/2.4.12 (Debian)',
+                'reset_event_count'  => 1,
+                'last_reset_time'    => '2015-09-17T10:10:32+02:00',
             ]
         );
     }
 
     public function it_does_not_provides_server_version_of_pim_host_if_request_is_null(
-        $requestStack,
-        $versionProvider,
-        $installStatusManager,
-        ServerBag $serverBag
+        RequestStack $requestStack,
+        VersionProviderInterface $versionProvider,
+        InstallStatusManager $installStatusManager,
+        ServerBag $serverBag,
+        FeatureFlags $featureFlags,
     ) {
+        $featureFlags->isEnabled('reset_pim')->willReturn(false);
         $versionProvider->getPatch()->willReturn('1.4.0');
         $versionProvider->getEdition()->willReturn('CE');
         $requestStack->getCurrentRequest()->willReturn(null);
@@ -73,7 +81,7 @@ class VersionDataCollectorSpec extends ObjectBehavior
                 'pim_version'      => '1.4.0',
                 'pim_environment'  => 'prod',
                 'pim_install_time' => (new \DateTime('2015-09-16T10:10:32+02:00'))->format(\DateTime::ATOM),
-                'server_version'     => '',
+                'server_version'   => '',
             ]
         );
     }
