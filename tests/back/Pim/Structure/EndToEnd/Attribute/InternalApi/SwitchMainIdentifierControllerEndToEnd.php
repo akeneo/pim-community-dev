0386c4defb243f2c34c44be53b8a96e7377ba3f8
Céline Morin <celine.morin@akeneo.com>
2023-07-06 15:39:17 +0200

    CPM-1067: Add ACL on attribute edit for SwitchMainIdentifier

--- tests/back/Pim/Structure/EndToEnd/Attribute/InternalApi/SwitchMainIdentifierControllerEndToEnd.php
+++ tests/back/Pim/Structure/EndToEnd/Attribute/InternalApi/SwitchMainIdentifierControllerEndToEnd.php
@@ -5,7 +5,11 @@ namespace AkeneoTest\Pim\Structure\EndToEnd\Attribute\InternalApi;
 use Akeneo\Pim\Structure\Component\AttributeTypes;
 use Akeneo\Pim\Structure\Component\Model\AttributeGroupInterface;
 use Akeneo\Test\Common\EntityBuilder;
+use Akeneo\Test\IntegrationTestsBundle\Helper\AuthenticatorHelper;
 use Akeneo\Test\IntegrationTestsBundle\Helper\WebClientHelper;
+use Akeneo\Tool\Component\StorageUtils\Cache\EntityManagerClearerInterface;
+use Akeneo\Tool\Component\StorageUtils\Repository\IdentifiableObjectRepositoryInterface;
+use Akeneo\Tool\Component\StorageUtils\Saver\BulkSaverInterface;
 use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
 use PHPUnit\Framework\Assert;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -36,6 +40,31 @@ final class SwitchMainIdentifierControllerEndToEnd extends WebTestCase
         );
         $response = $this->client->getResponse();
         Assert::assertTrue($response->isRedirect('/'));
+    }
+
+    public function test_it_should_denied_access_if_user_is_not_granted(): void
+    {
+        $this->createAttribute('newIdentifier', AttributeTypes::IDENTIFIER);
+
+        $roleWithPermissions = $this->getRoleRepository()->findOneByIdentifier('ROLE_CATALOG_MANAGER');
+        $roleWithPermissions->setPermissions(\array_merge($roleWithPermissions->permissions(), [
+            'action:pim_enrich_attribute_edit' => false
+        ]));
+        $this->getRoleSaver()->saveAll([$roleWithPermissions]);
+
+        $this->logIn('Julia');
+
+        $this->webClientHelper->callRoute(
+            $this->client,
+            self::ROUTE,
+            [
+                'attributeCode' => 'newIdentifier',
+            ],
+            'POST',
+            self::HEADERS
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
     }
 
     public function test_it_should_be_a_bad_request_with_unknown_attribute(): void
@@ -107,14 +136,13 @@ final class SwitchMainIdentifierControllerEndToEnd extends WebTestCase
 
         $catalogs = $this->get('akeneo_integration_tests.catalogs');
         $fixturesLoader = $this->get('akeneo_integration_tests.loader.fixtures_loader');
-        $fixturesLoader->load($catalogs->useMinimalCatalog());
+        $fixturesLoader->load($catalogs->useTechnicalCatalog());
 
         $authenticator = $this->get('akeneo_integration_tests.security.system_user_authenticator');
         $authenticator->createSystemUser();
-        $this->get('akeneo_integration_tests.helper.authenticator')->logIn('system', $this->client);
-
-        $this->get('pim_connector.doctrine.cache_clearer')->clear();
-        $this->webClientHelper = $this->get('akeneo_integration_tests.helper.web_client');
+        $this->logIn('admin');
+        $this->getCacheClearer()->clear();
+        $this->webClientHelper = $this->getWebClientHelper();
     }
 
     private function get(string $service)
@@ -143,5 +171,38 @@ final class SwitchMainIdentifierControllerEndToEnd extends WebTestCase
     private function getAttributeBuilder(): EntityBuilder
     {
         return $this->get('akeneo_integration_tests.base.attribute.builder');
+    }
+
+    private function logIn(string $username): void
+    {
+        $this->getAuthenticatorHelper()->logIn($username, $this->client);
+
+        $this->getCacheClearer()->clear();
+        $this->webClientHelper = $this->getWebClientHelper();
+    }
+
+    private function getRoleSaver(): BulkSaverInterface
+    {
+        return $this->get('pim_user.saver.role_with_permissions');
+    }
+
+    public function getRoleRepository(): IdentifiableObjectRepositoryInterface
+    {
+        return $this->get('pim_user.repository.role_with_permissions');
+    }
+
+    public function getAuthenticatorHelper(): AuthenticatorHelper
+    {
+        return $this->get('akeneo_integration_tests.helper.authenticator');
+    }
+
+    public function getCacheClearer(): EntityManagerClearerInterface
+    {
+        return $this->get('pim_connector.doctrine.cache_clearer');
+    }
+
+    public function getWebClientHelper(): WebClientHelper
+    {
+        return $this->get('akeneo_integration_tests.helper.web_client');
     }
 }
