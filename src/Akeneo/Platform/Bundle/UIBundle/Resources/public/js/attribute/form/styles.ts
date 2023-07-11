4d577fd6daecbf70735c6d8cdf62cf6e3589d0a9
Céline Morin <celine.morin@akeneo.com>
2023-07-11 18:05:57 +0200

    CPM-1153: Add tooltip on identifier attribute tile when limit is reached

---
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/attribute/form/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import {getColor} from 'akeneo-design-system';
+
+const TooltipHeader = styled.span`
+  color: ${getColor('blue', 120)};
+  font-weight: 700;
+`;
+
+export {TooltipHeader};
