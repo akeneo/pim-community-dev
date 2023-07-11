7e3bb4241278bbc2afa2c98793074aaf2ac3744b
pierallard <pierallard@gmail.com>
2023-07-11 16:49:33 +0200

    CPM-1152: Fix style and labels

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/AttributeSetupApp.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/AttributeSetupApp.tsx
@@ -1,22 +1,25 @@
 import React, {FC} from 'react';
-import {getColor, Helper, IconButton, List, LockIcon} from 'akeneo-design-system';
+import {getColor, getFontSize, Helper, IconButton, Link, List, LockIcon} from 'akeneo-design-system';
 import {Attribute} from '../models/Attribute';
 import styled from 'styled-components';
-import {useFeatureFlags, useTranslate} from '@akeneo-pim-community/shared';
+import {useTranslate} from '@akeneo-pim-community/shared';
 
-const CustomList = styled(List)`
-  & > * {
-    padding-left: 20px;
-  }
-`
-
-const TitleCell = styled(List.Cell)`
-  font-size: 15px;
-  display: block;
+const ListCellInner = styled.div`
+  padding-left: 10px;
+  flex-direction: column;
+  align-items: baseline;
+  line-height: 22px;
+  color: ${getColor('grey', 120)};
   
-  em {
-    font-style: initial;
-    color: ${getColor('brand', 100)};
+  header {
+    font-size: ${getFontSize('big')};
+    display: block;
+    font-weight: 600;
+
+    em {
+      font-style: initial;
+      color: ${getColor('brand', 100)};
+    }
   }
 `;
 
@@ -26,66 +29,89 @@ type AttributeSetupAppProps = {
 
 const AttributeSetupApp: FC<AttributeSetupAppProps> = ({attribute}) => {
   const translate = useTranslate();
-  const {isEnabled} = useFeatureFlags();
-
-  const isReadOnlyFeatureEnabled = isEnabled('read_only_product_attribute');
+  const urlScopable = 'http://todo';
+  const urlLocalizable = 'http://todo';
 
   return (
     <>
       <Helper level="error">
         Actions taken here could greatly impact your products.
       </Helper>
-      <CustomList>
+      <List>
         <List.Row>
-          <TitleCell width="auto">
-            The attribute is an <em>{translate(`pim_enrich.entity.attribute.property.type.${attribute.type}`)}</em>
-          </TitleCell>
-          <List.RowHelpers>
-            An attribute's type cannot be modified as this would result in the deletion of all its product values
-          </List.RowHelpers>
+          <List.Cell width='auto'>
+            <ListCellInner>
+              <header>
+                This attribute type is <em>{translate(`pim_enrich.entity.attribute.property.type.${attribute.type}`)}</em>
+              </header>
+              An attribute’s type cannot be modified as this would result in the deletion of all its product values.
+            </ListCellInner>
+          </List.Cell>
           <List.RemoveCell>
             <IconButton ghost="borderless" level="tertiary" icon={<LockIcon />} title="" />
           </List.RemoveCell>
         </List.Row>
 
         <List.Row>
-          <TitleCell width="auto">
-            Value par channel ? <em>{JSON.stringify(attribute.scopable)}</em>
-          </TitleCell>
-          <List.RowHelpers>
-            Text value per channel
-          </List.RowHelpers>
+          <List.Cell width='auto'>
+            <ListCellInner>
+              <header>
+                {attribute.unique ? <>
+                    This attribute has a <em>unique value</em>.
+                  </> :
+                  <>This attribute don't have <em>unique value</em>.</>
+                }
+              </header>
+              The value of this attribute must be unique. This cannot be modified. // TODO Have opposite
+            </ListCellInner>
+          </List.Cell>
           <List.RemoveCell>
             <IconButton ghost="borderless" level="tertiary" icon={<LockIcon />} title="" />
           </List.RemoveCell>
         </List.Row>
 
         <List.Row>
-          <TitleCell width="auto">
-            Value par locale ? <em>{JSON.stringify(attribute.localizable)}</em>
-          </TitleCell>
-          <List.RowHelpers>
-            Text value per locale
-          </List.RowHelpers>
+          <List.Cell width='auto'>
+            <ListCellInner>
+              <header>
+                {attribute.scopable ? <>
+                    This attribute is <em>scopable</em>.
+                  </> :
+                  <>This attribute is not <em>scopable</em>.</>
+                }
+              </header>
+              Value per channel determines if an attribute is scopable (different values for different channels) or not (one value for all channels).<br/>
+              A scopable attribute cannot be changed to not scopable.
+              {' '}
+              <Link href={urlScopable} target='_blank'>Learn more</Link>
+            </ListCellInner>
+          </List.Cell>
           <List.RemoveCell>
             <IconButton ghost="borderless" level="tertiary" icon={<LockIcon />} title="" />
           </List.RemoveCell>
         </List.Row>
 
-        {isReadOnlyFeatureEnabled &&
         <List.Row>
-          <TitleCell width="auto">
-            Readonly ? <em>{JSON.stringify(attribute.is_read_only)}</em>
-          </TitleCell>
-          <List.RowHelpers>
-            Text readonly
-          </List.RowHelpers>
+          <List.Cell width='auto'>
+            <ListCellInner>
+              <header>
+                {attribute.localizable ? <>
+                    This attribute is <em>localizable</em>.
+                  </> :
+                  <>This attribute is not <em>localizable</em>.</>
+                }
+              </header>
+              Value per locale determines if an attribute is localizable (different values for different locales) or not (one value for all locales).<br/>
+              A localizable attribute cannot be changed to not localizable.
+              {' '}
+              <Link href={urlLocalizable} target='_blank'>Learn more</Link>
+            </ListCellInner>
+          </List.Cell>
           <List.RemoveCell>
             <IconButton ghost="borderless" level="tertiary" icon={<LockIcon />} title="" />
           </List.RemoveCell>
         </List.Row>
-        }
-      </CustomList>
+      </List>
     </>
   );
 };
