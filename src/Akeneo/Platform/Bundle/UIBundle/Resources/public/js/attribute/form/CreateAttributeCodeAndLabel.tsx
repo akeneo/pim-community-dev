1e82eb685fe736a96f637bf0dba1e6f8237ccc74
Céline Morin <celine.morin@akeneo.com>
2023-07-11 19:28:55 +0200

    CPM-1151: Move CreateAttributeProgressIndicator to be used for table attribute

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/attribute/form/CreateAttributeCodeAndLabel.tsx
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/attribute/form/CreateAttributeCodeAndLabel.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {CreateAttributeButtonStepProps} from './CreateAttributeButtonApp';
 import {CreateAttributeModal} from '@akeneo-pim-community/settings-ui';
-import {CreateAttributeProgressIndicator} from './CreateAttributeProgressIndicator';
+import {CreateAttributeProgressIndicator} from '@akeneo-pim-community/settings-ui';
 
 const CreateAttributeCodeAndLabel: React.FC<CreateAttributeButtonStepProps> = props => {
-  const currentIndex = props?.initialData?.attribute_type === 'table' ? 2 : 1;
-
+  const currentIndex = props?.initialData?.attribute_type === 'pim_catalog_table' ? 2 : 1;
+  console.log('coucou');
   return (
     <CreateAttributeModal {...props}>
       <CreateAttributeProgressIndicator
