659b656a42eec6c49c2df7053f14267b26377a5f
pierallard <pierallard@gmail.com>
2023-07-12 12:21:37 +0200

    CPM-1149: Add button to switch main identifier

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/attribute-setup.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/attribute-setup.tsx
@@ -5,14 +5,26 @@ import {pimTheme} from 'akeneo-design-system';
 import {ThemeProvider} from 'styled-components';
 import {DependenciesProvider} from '@akeneo-pim-community/legacy-bridge';
 import {AttributeSetupApp} from './AttributeSetupApp';
+import {Attribute} from '../models/Attribute';
+const FetcherRegistry = require('pim/fetcher-registry');
 
 class AttributeSetup extends BaseView {
+  mainIdentifierAttribute: Attribute;
+
+  configure(): JQueryPromise<any> {
+    return FetcherRegistry.getFetcher('attribute').getIdentifierAttribute().then((mainIdentifierAttribute: Attribute) => {
+      this.mainIdentifierAttribute = mainIdentifierAttribute;
+
+      return super.configure();
+    });
+  }
+
   render(): any {
     const attribute = this.getFormData();
     ReactDOM.render(
       <DependenciesProvider>
         <ThemeProvider theme={pimTheme}>
-          <AttributeSetupApp attribute={attribute} />
+          <AttributeSetupApp attribute={attribute} originalMainIdentifierAttribute={this.mainIdentifierAttribute} />
         </ThemeProvider>
       </DependenciesProvider>,
       this.el
