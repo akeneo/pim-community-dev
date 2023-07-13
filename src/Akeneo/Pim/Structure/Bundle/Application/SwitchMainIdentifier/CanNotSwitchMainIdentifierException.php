4f7f47f1015666c293cde85b71843ad2cb2b2cf7
pierallard <pierallard@gmail.com>
2023-07-13 10:59:38 +0200

    CPM-1149: Fix linter

--- src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/CanNotSwitchMainIdentifierException.php
+++ src/Akeneo/Pim/Structure/Bundle/Application/SwitchMainIdentifier/CanNotSwitchMainIdentifierException.php
@@ -2,7 +2,10 @@
 
 namespace Akeneo\Pim\Structure\Bundle\Application\SwitchMainIdentifier;
 
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class CanNotSwitchMainIdentifierException extends \InvalidArgumentException
 {
-
 }
