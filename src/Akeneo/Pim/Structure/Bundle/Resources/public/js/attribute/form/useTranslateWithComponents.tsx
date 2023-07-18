4a8aeb657e4be89350cde71c1ea05e63740622d1
pierallard <pierallard@gmail.com>
2023-07-18 09:36:16 +0200

    CPM-1169: Split placeholders

--- src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/useTranslateWithComponents.tsx
+++ src/Akeneo/Pim/Structure/Bundle/Resources/public/js/attribute/form/useTranslateWithComponents.tsx
@@ -33,15 +33,39 @@ const splitPreviousElements: (
   return result;
 };
 
+type OriginalPlaceholders = {[name: string]: string | number};
+type ComponentPlaceholders = {[componentName: string]: ComponentFunction};
+
+function splitComponents(placeholders: {[p: string]: string | number | ComponentFunction}) {
+  return Object.keys(placeholders).reduce<{
+    basePlaceholders: OriginalPlaceholders;
+    componentPlaceholders: ComponentPlaceholders;
+  }>(
+    (previous, name) => {
+      const basePlaceholders = previous.basePlaceholders;
+      const componentPlaceholders = previous.componentPlaceholders;
+      if (typeof placeholders[name] === 'string' || typeof placeholders[name] === 'number') {
+        basePlaceholders[name] = placeholders[name] as string | number;
+      } else {
+        componentPlaceholders[name] = placeholders[name] as ComponentFunction;
+      }
+
+      return {basePlaceholders: basePlaceholders, componentPlaceholders};
+    },
+    {basePlaceholders: {}, componentPlaceholders: {}}
+  );
+}
+
 const useTranslateWithComponents = () => {
   const baseTranslate = useTranslate();
   const translateWithComponents: (
     id: string,
-    placeholders?: {[name: string]: string | number},
-    count?: number,
-    componentPlaceholders?: {[componentName: string]: ComponentFunction}
-  ) => ReactElement<any, any> = (id, placeholders, count, componentPlaceholders = {}) => {
-    const basicTranslation = baseTranslate(id, placeholders, count);
+    placeholders?: {[name: string]: string | number | ComponentFunction},
+    count?: number
+  ) => ReactElement<any, any> = (id, placeholders, count) => {
+    const {basePlaceholders, componentPlaceholders} = splitComponents(placeholders || {});
+
+    const basicTranslation = baseTranslate(id, basePlaceholders, count);
 
     let elements: ComponentsOrStrings = [basicTranslation];
     Object.keys(componentPlaceholders).forEach(componentName => {
