30f1dbe54054190593202be85e2e484949a42ba0
JVerbrugge <julien.verbrugge@akeneo.com>
2023-07-20 14:04:02 +0200

    add local schema

--- src/Akeneo/Channel/back/API/Query/Locale.php
+++ src/Akeneo/Channel/back/API/Query/Locale.php
@@ -2,10 +2,26 @@
 
 namespace Akeneo\Channel\API\Query;
 
+use OpenApi\Attributes as OA;
+
 /**
  * @copyright 2022 Akeneo SAS (https://www.akeneo.com)
  * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+// generate a schema for this class following example in Akeneo\Pim\Enrichment\Product\Domain\Model\ProductIdentifier
+#[OA\Schema(
+    schema: 'Locale',
+    properties: [
+        new OA\Property(
+            property: 'code',
+            type: 'string',
+        ),
+        new OA\Property(
+            property: 'enabled',
+            type: 'boolean',
+        ),
+    ],
+)]
 final class Locale
 {
     public function __construct(
