f185750d161850ad66b650bccb5de0e841b7bb52
LevFlavien <flavien.levesque@akeneo.com>
2023-07-28 14:37:38 +0200

    PIM-11087: Accept more filetypes in Reference Entities (#18230)

--- src/Akeneo/Tool/Bundle/FileStorageBundle/spec/Validator/Constraints/UploadedFileValidatorSpec.php
+++ src/Akeneo/Tool/Bundle/FileStorageBundle/spec/Validator/Constraints/UploadedFileValidatorSpec.php
@@ -13,20 +13,20 @@ use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 final class UploadedFileValidatorSpec extends ObjectBehavior
 {
-    public function let(ExecutionContextInterface $context)
+    public function let(ExecutionContextInterface $context): void
     {
         $this->initialize($context);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable(): void
     {
         $this->shouldHaveType(Constraints\UploadedFileValidator::class);
     }
 
-    public function it_validates_the_file(
+    public function it_validates_a_correct_file(
         File\UploadedFile $file,
         ExecutionContextInterface $context
-    ) {
+    ): void {
         $file->beConstructedWith([__FILE__, 'akeneo.PNG', 'image/png', null, true]);
 
         $file->guessExtension()->willReturn('png');
@@ -43,17 +43,37 @@ final class UploadedFileValidatorSpec extends ObjectBehavior
         $this->validate($file, $constraint);
     }
 
-    public function it_adds_violation_if_extension_is_not_supported(
+    public function it_validates_a_file_with_supported_type_and_extension_even_if_they_dont_match(
+        File\UploadedFile $file,
+        ExecutionContextInterface $context
+    ): void {
+        $file->beConstructedWith([__FILE__, 'akeneo.ai', 'application/illustrator', null, true]);
+
+        $file->guessExtension()->willReturn('pdf');
+        $file->getClientOriginalExtension()->willReturn('ai');
+        $file->getMimeType()->willReturn('application/pdf');
+        $file->getClientMimeType()->willReturn('application/illustrator');
+
+        $constraint = new Constraints\UploadedFile([
+            'types' => ['ai' => ['application/illustrator'], 'pdf' => ['application/pdf']],
+        ]);
+
+        $context->addViolation(Argument::any())->shouldNotBeCalled();
+
+        $this->validate($file, $constraint);
+    }
+
+    public function it_adds_violation_if_client_extension_is_not_supported(
         File\UploadedFile $file,
         ExecutionContextInterface $context,
         ConstraintViolationBuilderInterface $constraintViolationBuilder
-    ) {
-        $file->beConstructedWith([__FILE__, 'akeneo.jpg', 'image/jpg', null, true]);
+    ): void {
+        $file->beConstructedWith([__FILE__, 'akeneo.jpg', 'image/png', null, true]);
 
         $file->guessExtension()->willReturn('jpg');
         $file->getClientOriginalExtension()->willReturn('jpg');
-        $file->getMimeType()->willReturn('image/jpg');
-        $file->getClientMimeType()->willReturn('image/jpg');
+        $file->getMimeType()->willReturn('image/png');
+        $file->getClientMimeType()->willReturn('image/png');
 
         $constraint = new Constraints\UploadedFile([
             'types' => ['png' => ['image/png']],
@@ -67,11 +87,11 @@ final class UploadedFileValidatorSpec extends ObjectBehavior
         $this->validate($file, $constraint);
     }
 
-    public function it_adds_violation_if_mime_type_is_not_supported(
+    public function it_adds_violation_if_client_mime_type_is_not_supported(
         File\UploadedFile $file,
         ExecutionContextInterface $context,
         ConstraintViolationBuilderInterface $constraintViolationBuilder
-    ) {
+    ): void {
         $file->beConstructedWith([__FILE__, 'akeneo.png', 'image/jpg', null, true]);
 
         $file->guessExtension()->willReturn('png');
@@ -89,11 +109,11 @@ final class UploadedFileValidatorSpec extends ObjectBehavior
         $this->validate($file, $constraint);
     }
 
-    public function it_adds_violation_if_extension_does_not_match_file_content(
+    public function it_adds_violation_if_guessed_extension_is_not_supported(
         File\UploadedFile $file,
         ExecutionContextInterface $context,
         ConstraintViolationBuilderInterface $constraintViolationBuilder
-    ) {
+    ): void {
         $file->beConstructedWith([__FILE__, 'akeneo.png', 'image/png', null, true]);
 
         $file->guessExtension()->willReturn('jpg');
@@ -111,11 +131,11 @@ final class UploadedFileValidatorSpec extends ObjectBehavior
         $this->validate($file, $constraint);
     }
 
-    public function it_adds_violation_if_mime_type_does_not_match_file_content(
+    public function it_adds_violation_if_guessed_mime_type_is_not_supported(
         File\UploadedFile $file,
         ExecutionContextInterface $context,
         ConstraintViolationBuilderInterface $constraintViolationBuilder
-    ) {
+    ): void {
         $file->beConstructedWith([__FILE__, 'akeneo.png', 'image/png', null, true]);
 
         $file->guessExtension()->willReturn('png');
@@ -125,31 +145,6 @@ final class UploadedFileValidatorSpec extends ObjectBehavior
 
         $constraint = new Constraints\UploadedFile([
             'types' => ['png' => ['image/png']],
-        ]);
-
-        $context->buildViolation($constraint->fileIsCorruptedMessage)->willReturn($constraintViolationBuilder);
-        $constraintViolationBuilder->addViolation()->shouldBeCalledOnce();
-
-        $this->validate($file, $constraint);
-    }
-
-    public function it_adds_violation_if_mime_type_does_not_match_extension(
-        File\UploadedFile $file,
-        ExecutionContextInterface $context,
-        ConstraintViolationBuilderInterface $constraintViolationBuilder
-    ) {
-        $file->beConstructedWith([__FILE__, 'akeneo.png', 'image/jpg', null, true]);
-
-        $file->guessExtension()->willReturn('png');
-        $file->getClientOriginalExtension()->willReturn('png');
-        $file->getMimeType()->willReturn('image/jpg');
-        $file->getClientMimeType()->willReturn('image/jpg');
-
-        $constraint = new Constraints\UploadedFile([
-            'types' => [
-                'png' => ['image/png'],
-                'jpg' => ['image/jpg'],
-            ],
         ]);
 
         $context->buildViolation($constraint->fileIsCorruptedMessage)->willReturn($constraintViolationBuilder);
