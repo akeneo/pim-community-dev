c7ac94dd56703df5f55f591d19d8faa856d18eb6
jmleroux <jmleroux.pro@gmail.com>
2023-07-31 20:12:51 +0200

    PH-1205: :white_check_mark: Fix specs

--- tests/enterprise/back/Platform/Specification/Component/Tenant/Domain/ContextValueDecrypterSpec.php
+++ tests/enterprise/back/Platform/Specification/Component/Tenant/Domain/ContextValueDecrypterSpec.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Specification\Akeneo\Platform\Component\Tenant;
+namespace Specification\Akeneo\Platform\Component\Tenant\Domain;
 
 use Akeneo\Platform\Component\Tenant\Domain\ContextValueDecrypter;
 use Akeneo\Platform\Component\Tenant\Domain\ContextValueDecrypterInterface;
 use Akeneo\Platform\Component\Tenant\Domain\Exception\TenantContextDecoderException;
 use PhpSpec\ObjectBehavior;
 
-class TenantContextDecoderSpec extends ObjectBehavior
+class ContextValueDecrypterSpec extends ObjectBehavior
 {
     function let()
     {
@@ -34,51 +34,51 @@ class TenantContextDecoderSpec extends ObjectBehavior
             'bar' => 'baz',
         ];
 
-        $encryptedValues =
-            '{"data":"qbbkq1rrnYyj1UkcJ6TR/qTA/ZEd7kPR7Ajyq2vgxUg=","iv":"90d68e58aa2918f137ea2de4c07463ac"}';
+        $encryptedValues = 'qbbkq1rrnYyj1UkcJ6TR/qTA/ZEd7kPR7Ajyq2vgxUg=';
+        $iv = '90d68e58aa2918f137ea2de4c07463ac';
 
-        $this->decode($encryptedValues)->shouldReturn($expectedValues);
+        $this->decode($encryptedValues, $iv)->shouldReturn($expectedValues);
     }
 
     function it_throws_on_invalid_hex_iv()
     {
-        $encryptedValues =
-            '{"data":"qbbkq1rrnYyj1UkcJ6TR/qTA/ZEd7kPR7Ajyq2vgxUg=","iv":"bad_iv"}';
+        $encryptedValues = 'qbbkq1rrnYyj1UkcJ6TR/qTA/ZEd7kPR7Ajyq2vgxUg=';
+        $iv = 'bad_iv';
 
         $this
-            ->shouldThrow(new TenantContextDecoderException('Unable to decode tenant values payload.'))
-            ->during('decode', [$encryptedValues]);
+            ->shouldThrow(TenantContextDecoderException::class)
+            ->during('decode', [$encryptedValues, $iv]);
     }
 
     function it_throws_on_invalid_crypted_data()
     {
-        $encryptedValues =
-            '{"data":"invalid","iv":"90d68e58aa2918f137ea2de4c07463ac"}';
+        $encryptedValues = 'invalid';
+        $iv = '90d68e58aa2918f137ea2de4c07463ac';
 
         $this
             ->shouldThrow(new TenantContextDecoderException('Unable to decrypt tenant values.'))
-            ->during('decode', [$encryptedValues]);
+            ->during('decode', [$encryptedValues, $iv]);
     }
 
     function it_throws_on_invalid_json()
     {
         // clear values = '{"foo": "bar",, "baz": "snafu"}'
-        $encryptedValues =
-            '{"data":"VgvimYiLnlxArlu3RTMbG41dDANh9xne6d71p/AeCQI=","iv":"5f90664b2fca29dd597d1e741a6f8255"}';
+        $encryptedValues = 'VgvimYiLnlxArlu3RTMbG41dDANh9xne6d71p/AeCQI=';
+        $iv = '5f90664b2fca29dd597d1e741a6f8255';
 
         $this
             ->shouldThrow(new TenantContextDecoderException('Decrypted values is not a valid json string.'))
-            ->during('decode', [$encryptedValues]);
+            ->during('decode', [$encryptedValues, $iv]);
     }
 
     function it_throws_on_invalid_json_map()
     {
         // clear values = '{"foo": "bar", "baz": ["in_array"]}'
-        $encryptedValues =
-            '{"data":"8hmjdChBhzysoMDA8a04WsngnpV9EG5CZarPtLOnvY4l4Z9ICRWEoRnLmXnH229x","iv":"25e62a45f89f02f53e5ede9c860f8c16"}';
+        $encryptedValues = '8hmjdChBhzysoMDA8a04WsngnpV9EG5CZarPtLOnvY4l4Z9ICRWEoRnLmXnH229x';
+        $iv = '25e62a45f89f02f53e5ede9c860f8c16';
 
         $this
             ->shouldThrow(new TenantContextDecoderException('Tenant values is not a valid json map.'))
-            ->during('decode', [$encryptedValues]);
+            ->during('decode', [$encryptedValues, $iv]);
     }
 }
