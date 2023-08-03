3ee0595da86091d69c13b37352099a1873f86e8d
Benjamin Letellier <letellier.benjamin@gmail.com>
2023-08-03 09:43:12 +0000

    Preparing version 6.0.96

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.95';
+    public const VERSION = '6.0.96';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
