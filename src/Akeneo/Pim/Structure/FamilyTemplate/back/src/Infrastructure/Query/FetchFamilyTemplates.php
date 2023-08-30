980513a648f929cc96f76a05a2cc264fa8480ab5
marinfazioakeneo <132996137+marinfazioakeneo@users.noreply.github.com>
2023-08-30 09:44:28 +0200

    RAB-1496:  Add controller to fetch family templates (#18715)

--- src/Akeneo/Pim/Structure/FamilyTemplate/back/src/Infrastructure/Query/FetchFamilyTemplates.php
+++ src/Akeneo/Pim/Structure/FamilyTemplate/back/src/Infrastructure/Query/FetchFamilyTemplates.php
@@ -10,6 +10,7 @@ use GuzzleHttp\Client;
 
 class FetchFamilyTemplates implements FetchFamilyTemplatesInterface
 {
+    private const MINIFIED_DIRECTORY = '/dist';
     private const TEMPLATES_DIRECTORY = '/templates/families';
 
     private Client $client;
@@ -31,28 +32,14 @@ class FetchFamilyTemplates implements FetchFamilyTemplatesInterface
 
     public function all(): array
     {
-        $response = $this->client->request('GET', '/repos/'.$this->githubOrgName.'/'.$this->githubRepoName.'/zipball');
+        $response = $this->client->request('GET', '/repos/'.$this->githubOrgName.'/'.$this->githubRepoName.'/contents'.self::MINIFIED_DIRECTORY.'/minified.json');
 
-        $filename = '/tmp/'.uniqid().'_family-templates.zip';
-        file_put_contents($filename, $response->getBody()->getContents());
-
-        $archive = new \ZipArchive();
-        $archive->open($filename);
-
-        $templates = [];
-        for ($i = 0; $i < $archive->numFiles; $i++) {
-            $zipFilePath = $archive->getNameIndex($i);
-            if ($zipFilePath && $archive->getFromIndex($i) && str_contains($zipFilePath, self::TEMPLATES_DIRECTORY) && str_contains($zipFilePath, '.json')) {
-                $pathParts = explode('/', $zipFilePath);
-                $templateName = str_replace('.json', '', $pathParts[count($pathParts)-1]);
-                $arrayTemplate = json_decode($archive->getFromIndex($i), true);
-                $templates[$templateName] = $this->buildReadModel($arrayTemplate);
-            }
+        $content = json_decode($response->getBody()->getContents(), true);
+        if (!isset($content['content'])) {
+            return [];
         }
 
-        unlink($filename);
-
-        return $templates;
+        return json_decode(base64_decode($content['content']), true);
     }
 
     public function byName(string $templateName): FamilyTemplate
--- src/Akeneo/Pim/Structure/FamilyTemplate/back/src/Infrastructure/Symfony/DependencyInjection/AkeneoPimStructureFamilyTemplateExtension.php
+++ src/Akeneo/Pim/Structure/FamilyTemplate/back/src/Infrastructure/Symfony/DependencyInjection/AkeneoPimStructureFamilyTemplateExtension.php
@@ -18,6 +18,7 @@ final class AkeneoPimStructureFamilyTemplateExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('controllers.yml');
         $loader->load('services.yml');
     }
 }
