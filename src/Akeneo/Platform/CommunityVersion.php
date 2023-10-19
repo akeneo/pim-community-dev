4e9fba530605db6d652718c067f49a5572ac6757
Olivier Pontier <olivier.pontier@akeneo.com>
2023-10-19 13:17:16 +0000

    Preparing version 6.0.107

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.106';
+    public const VERSION = '6.0.107';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
