8a9a10d570f6c3a6d60e12427e0e26889d21f012
Mathias Métayer <mathias.metayer@akeneo.com>
2023-11-08 15:43:28 +0000

    Preparing version 6.0.109

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.108';
+    public const VERSION = '6.0.109';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
