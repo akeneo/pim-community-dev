9ad9335622a6c3fc48a43483c2e1be93c4305ae4
LevFlavien <flavien.levesque@akeneo.com>
2023-12-22 15:22:35 +0000

    Preparing version 6.0.111

--- src/Akeneo/Platform/CommunityVersion.php
+++ src/Akeneo/Platform/CommunityVersion.php
@@ -12,7 +12,7 @@ namespace Akeneo\Platform;
 class CommunityVersion
 {
     /** @staticvar string */
-    public const VERSION = '6.0.110';
+    public const VERSION = '6.0.111';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'Buccaneer Bunny';
